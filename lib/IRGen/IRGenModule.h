3b3fb3fcd48180371d9b19b7f3ed34182003fccf
John McCall <rjmccall@apple.com>
2011-08-13 18:55:51 +0000

    Commit missing file.
    Swift SVN r540

--- lib/IRGen/IRGenModule.h
+++ lib/IRGen/IRGenModule.h
@@ -1,9 +1,53 @@
-namespace llvm { class TargetData; }
+//===--- IRGenModule.h - Swift Global IR Generation Module ------*- C++ -*-===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2015 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+//
+// This file defines the interface used 
+// the AST into LLVM IR.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef SWIFT_IRGEN_IRGENMODULE_H
+#define SWIFT_IRGEN_IRGENMODULE_H
+
+namespace llvm {
+  class Module;
+  class TargetData;
+}
 
 namespace swift {
-  struct IRGenModule {
-  IRGenModule(ASTContext &C, irgen::Options &Opts, llvm::Module&, const llvm::TargetData&) {}
-  void emitTranslationUnit(TranslationUnitDecl *TU) ;
-  };
+  class ASTContext;
+  class TranslationUnitDecl;
 
-}
+namespace irgen {
+  class Options;
+
+/// IRGenModule - Primary class for emitting IR for global declarations.
+/// 
+class IRGenModule {
+public:
+  ASTContext &Context;
+  Options &Opts;
+  llvm::Module &Module;
+  const llvm::TargetData &TargetData;
+
+  IRGenModule(ASTContext &Context, Options &Opts, llvm::Module &Module,
+              const llvm::TargetData &TargetData)
+    : Context(Context), Opts(Opts), Module(Module),
+      TargetData(TargetData) {}
+
+  void emitTranslationUnit(TranslationUnitDecl *TU);
+};
+
+} // end namespace irgen
+} // end namespace swift
+
+#endif
