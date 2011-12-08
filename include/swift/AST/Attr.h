3456720d867851a7ff2f00ed9102cab47b9d7a4a
John McCall <rjmccall@apple.com>
2011-12-08 00:22:16 +0000

    Extract decl attributes out into their own file.
    Swift SVN r919

---
+++ include/swift/AST/Attr.h
@@ -0,0 +1,102 @@
+//===--- Attr.h - Swift Language Attribute ASTs -----------------*- C++ -*-===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2015 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+//
+// This file defines classes related to declaration attributes.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef SWIFT_ATTR_H
+#define SWIFT_ATTR_H
+
+#include "swift/Basic/SourceLoc.h"
+
+namespace swift {
+
+/// The associativity of a binary operator.
+enum class Associativity {
+  /// Non-associative operators cannot be written next to other
+  /// operators with the same precedence.  Relational operators are
+  /// typically non-associative.
+  None,
+
+  /// Left-associative operators associate to the left if written next
+  /// to other left-associative operators of the same precedence.
+  Left,
+
+  /// Right-associative operators associate to the right if written
+  /// next to other right-associative operators of the same precedence.
+  Right
+};
+
+class InfixData {
+  unsigned Precedence : 8;
+
+  /// Zero if invalid, or else an Associativity+1.
+  unsigned InvalidOrAssoc : 8;
+public:
+  InfixData() : Precedence(0), InvalidOrAssoc(0) {}
+  InfixData(unsigned prec, Associativity assoc)
+    : Precedence(prec), InvalidOrAssoc(unsigned(assoc) + 1) {}
+
+  bool isValid() const { return InvalidOrAssoc != 0; }
+
+  Associativity getAssociativity() const {
+    assert(isValid());
+    return Associativity(InvalidOrAssoc - 1);
+  }
+  bool isLeftAssociative() const {
+    return getAssociativity() == Associativity::Left;
+  }
+  bool isRightAssociative() const {
+    return getAssociativity() == Associativity::Right;
+  }
+  bool isNonAssociative() const {
+    return getAssociativity() == Associativity::None;
+  }
+
+  unsigned getPrecedence() const {
+    assert(isValid());
+    return Precedence;
+  }
+
+  friend bool operator==(InfixData L, InfixData R) {
+    return L.Precedence == R.Precedence
+        && L.InvalidOrAssoc == R.InvalidOrAssoc;
+  }
+  friend bool operator!=(InfixData L, InfixData R) {
+    return !operator==(L, R);
+  }
+};
+  
+/// DeclAttributes - These are attributes that may be applied to declarations.
+class DeclAttributes {
+public:
+  /// LSquareLoc/RSquareLoc - This is the location of the '[' and ']' in the
+  /// attribute specifier.  If this is an empty attribute specifier, then these
+  /// will be invalid locs.
+  SourceLoc LSquareLoc, RSquareLoc;
+
+  InfixData Infix;
+
+  DeclAttributes() { }
+
+  bool isInfix() const { return Infix.isValid(); }
+  InfixData getInfixData() const { return Infix; }
+    
+  bool empty() const {
+    return !isInfix();
+  }
+};
+  
+} // end namespace swift
+
+#endif
--- include/swift/AST/Decl.h
+++ include/swift/AST/Decl.h
@@ -17,6 +17,7 @@
 #ifndef SWIFT_DECL_H
 #define SWIFT_DECL_H
 
+#include "swift/AST/Attr.h"
 #include "swift/AST/DeclContext.h"
 #include "swift/AST/Identifier.h"
 #include "swift/AST/Type.h"
@@ -40,83 +41,6 @@ enum class DeclKind {
   First_##Id##Decl = FirstId, Last_##Id##Decl = LastId,
 #include "swift/AST/DeclNodes.def"
 };
-
-/// The associativity of a binary operator.
-enum class Associativity {
-  /// Non-associative operators cannot be written next to other
-  /// operators with the same precedence.  Relational operators are
-  /// typically non-associative.
-  None,
-
-  /// Left-associative operators associate to the left if written next
-  /// to other left-associative operators of the same precedence.
-  Left,
-
-  /// Right-associative operators associate to the right if written
-  /// next to other right-associative operators of the same precedence.
-  Right
-};
-
-class InfixData {
-  unsigned Precedence : 8;
-
-  /// Zero if invalid, or else an Associativity+1.
-  unsigned InvalidOrAssoc : 8;
-public:
-  InfixData() : Precedence(0), InvalidOrAssoc(0) {}
-  InfixData(unsigned prec, Associativity assoc)
-    : Precedence(prec), InvalidOrAssoc(unsigned(assoc) + 1) {}
-
-  bool isValid() const { return InvalidOrAssoc != 0; }
-
-  Associativity getAssociativity() const {
-    assert(isValid());
-    return Associativity(InvalidOrAssoc - 1);
-  }
-  bool isLeftAssociative() const {
-    return getAssociativity() == Associativity::Left;
-  }
-  bool isRightAssociative() const {
-    return getAssociativity() == Associativity::Right;
-  }
-  bool isNonAssociative() const {
-    return getAssociativity() == Associativity::None;
-  }
-
-  unsigned getPrecedence() const {
-    assert(isValid());
-    return Precedence;
-  }
-
-  friend bool operator==(InfixData L, InfixData R) {
-    return L.Precedence == R.Precedence
-        && L.InvalidOrAssoc == R.InvalidOrAssoc;
-  }
-  friend bool operator!=(InfixData L, InfixData R) {
-    return !operator==(L, R);
-  }
-};
-  
-/// DeclAttributes - These are attributes that may be applied to declarations.
-class DeclAttributes {
-public:
-  /// LSquareLoc/RSquareLoc - This is the location of the '[' and ']' in the
-  /// attribute specifier.  If this is an empty attribute specifier, then these
-  /// will be invalid locs.
-  SourceLoc LSquareLoc, RSquareLoc;
-
-  InfixData Infix;
-
-  DeclAttributes() { }
-
-  bool isInfix() const { return Infix.isValid(); }
-  InfixData getInfixData() const { return Infix; }
-    
-  bool empty() const {
-    return !isInfix();
-  }
-};
-
   
   
 /// DeclVarName - This is a recursive structure that represents the varname
