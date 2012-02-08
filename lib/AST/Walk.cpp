ca70a2997207deef68f06dd017fd2c7c519d6f11
John McCall <rjmccall@apple.com>
2012-02-08 00:54:11 +0000

    Missing files!  Sorry!
    Swift SVN r1110

---
+++ lib/AST/Walk.cpp
@@ -0,0 +1,377 @@
+//===--- Walk.cpp - AST Traversal -----------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2015 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+//
+//  This file implements Expr::walk and Stmt::walk.
+//
+//===----------------------------------------------------------------------===//
+
+#include "swift/AST/ASTVisitor.h"
+#include "swift/AST/PrettyStackTrace.h"
+using namespace swift;
+
+bool Walker::walkToExprPre(Expr *E) { return true; }
+Expr *Walker::walkToExprPost(Expr *E) { return E; }
+bool Walker::walkToStmtPre(Stmt *S) { return true; }
+Stmt *Walker::walkToStmtPost(Stmt *S) { return S; }
+
+namespace {
+
+/// Traversal - This class implements a simple expression/statement
+/// recursive traverser which queries a user-provided walker class
+/// on every node in an AST.
+class Traversal : public ASTVisitor<Traversal, Expr*, Stmt*> {
+  friend class ASTVisitor<Traversal, Expr*, Stmt*>;
+
+  swift::Walker &Walker;
+
+  typedef ASTVisitor<Traversal, Expr*, Stmt*> inherited;
+  
+  /// \brief RAII object that sets the parent of the walk context 
+  /// appropriately.
+  class SetParentRAII {
+    swift::Walker &Walker;
+    decltype(swift::Walker::Parent) PriorParent;
+    
+  public:
+    template<typename T>
+    SetParentRAII(swift::Walker &walker, T *newParent)
+      : Walker(walker), PriorParent(walker.Parent) {
+      walker.Parent = newParent;
+    }
+    
+    ~SetParentRAII() {
+      Walker.Parent = PriorParent;
+    }
+  };
+  
+  Expr *visit(Expr *E) {
+    SetParentRAII SetParent(Walker, E);
+    return inherited::visit(E);
+  }
+
+  Stmt *visit(Stmt *S) {
+    SetParentRAII SetParent(Walker, S);
+    return inherited::visit(S);
+  }
+  
+  Expr *visitErrorExpr(ErrorExpr *E) { return E; }
+  Expr *visitIntegerLiteralExpr(IntegerLiteralExpr *E) { return E; }
+  Expr *visitFloatLiteralExpr(FloatLiteralExpr *E) { return E; }
+  Expr *visitDeclRefExpr(DeclRefExpr *E) { return E; }
+  Expr *visitOverloadSetRefExpr(OverloadSetRefExpr *E) { return E; }
+  Expr *visitUnresolvedDeclRefExpr(UnresolvedDeclRefExpr *E) { return E; }
+  Expr *visitUnresolvedMemberExpr(UnresolvedMemberExpr *E) { return E; }
+
+  Expr *visitParenExpr(ParenExpr *E) {
+    if (Expr *subExpr = doIt(E->getSubExpr())) {
+      E->setSubExpr(subExpr);
+      return E;
+    }
+    return nullptr;
+  }
+  Expr *visitTupleExpr(TupleExpr *E) {
+    for (unsigned i = 0, e = E->getNumElements(); i != e; ++i)
+      if (E->getElement(i)) {
+        if (Expr *Elt = doIt(E->getElement(i)))
+          E->setElement(i, Elt);
+        else
+          return nullptr;
+      }
+    return E;
+  }
+  Expr *visitUnresolvedDotExpr(UnresolvedDotExpr *E) {
+    if (!E->getBase())
+      return E;
+    
+    if (Expr *E2 = doIt(E->getBase())) {
+      E->setBase(E2);
+      return E;
+    }
+    return nullptr;
+  }
+  
+  Expr *visitLookThroughOneofExpr(LookThroughOneofExpr *E) {
+    if (Expr *E2 = doIt(E->getSubExpr())) {
+      E->setSubExpr(E2);
+      return E;
+    }
+    return nullptr;
+  }
+  
+  Expr *visitTupleElementExpr(TupleElementExpr *E) {
+    if (Expr *E2 = doIt(E->getBase())) {
+      E->setBase(E2);
+      return E;
+    }
+    return nullptr;
+  }
+  
+  Expr *visitTupleShuffleExpr(TupleShuffleExpr *E) {
+    if (Expr *E2 = doIt(E->getSubExpr())) {
+      E->setSubExpr(E2);
+      return E;
+    }
+    return nullptr;
+  }
+  
+  Expr *visitLoadExpr(LoadExpr *E) {
+    if (Expr *E2 = doIt(E->getSubExpr())) {
+      E->setSubExpr(E2);
+      return E;
+    }
+    return nullptr;
+  }
+
+  Expr *visitSequenceExpr(SequenceExpr *E) {
+    for (unsigned i = 0, e = E->getNumElements(); i != e; ++i)
+      if (Expr *Elt = doIt(E->getElement(i)))
+        E->setElement(i, Elt);
+      else
+        return nullptr;
+    return E;
+  }
+  
+  Expr *visitFuncExpr(FuncExpr *E) {
+    if (BraceStmt *S = cast_or_null<BraceStmt>(doIt(E->getBody()))) {
+      E->setBody(S);
+      return E;
+    }
+    return nullptr;
+  }
+  
+  Expr *visitClosureExpr(ClosureExpr *E) {
+    if (Expr *E2 = doIt(E->getInput())) {
+      E->setInput(E2);
+      return E;
+    }
+    return nullptr;
+  }
+  
+  Expr *visitAnonClosureArgExpr(AnonClosureArgExpr *E) { return E; }
+  
+  Expr *visitModuleExpr(ModuleExpr *E) { return E; }
+
+  Expr *visitApplyExpr(ApplyExpr *E) {
+    Expr *E2 = doIt(E->getFn());
+    if (E2 == nullptr) return nullptr;
+    E->setFn(E2);
+    
+    E2 = doIt(E->getArg());
+    if (E2 == nullptr) return nullptr;
+    E->setArg(E2);
+    return E;
+  }
+
+  Expr *visitCallExpr(CallExpr *E) {
+    return visitApplyExpr(E);
+  }
+
+  Expr *visitUnaryExpr(UnaryExpr *E) {
+    return visitApplyExpr(E);
+  }
+
+  Expr *visitBinaryExpr(BinaryExpr *E) {
+    // Visit the arguments to the tuple, but visit the operator in
+    // infix order.
+    TupleExpr *Arg = E->getArgTuple();
+    assert(Arg->getNumElements() == 2);
+    Expr *E2 = doIt(Arg->getElement(0));
+    if (E2 == nullptr) return nullptr;
+    Arg->setElement(0, E2);
+
+    E2 = doIt(E->getFn());
+    if (E2 == 0) return 0;
+    E->setFn(E2);
+    
+    E2 = doIt(Arg->getElement(1));
+    if (E2 == 0) return 0;
+    Arg->setElement(1, E2);
+    return E;
+  }
+
+  Expr *visitConstructorCallExpr(ConstructorCallExpr *E) {
+    return visitApplyExpr(E);
+  }
+
+  Expr *visitDotSyntaxCallExpr(DotSyntaxCallExpr *E) {
+    return visitApplyExpr(E);
+  }
+  
+  Expr *visitDotSyntaxPlusFuncUseExpr(DotSyntaxPlusFuncUseExpr *E) {
+    Expr *E2 = doIt(E->getBaseExpr());
+    if (E2 == nullptr) return nullptr;
+    E->setBaseExpr(E2);
+    
+    E2 = doIt(E->getPlusFuncExpr());
+    if (E2 == nullptr) return nullptr;
+    E->setPlusFuncExpr(cast<DeclRefExpr>(E2));
+    return E;      
+  }    
+
+  Stmt *visitSemiStmt(SemiStmt *SS) {
+    return SS;
+  }
+  
+  Stmt *visitAssignStmt(AssignStmt *AS) {
+    if (Expr *E = doIt(AS->getDest()))
+      AS->setDest(E);
+    else
+      return nullptr;
+
+    if (Expr *E = doIt(AS->getSrc()))
+      AS->setSrc(E);
+    else
+      return nullptr;
+    return AS;
+  }
+  
+  Stmt *visitBraceStmt(BraceStmt *BS) {
+    for (unsigned i = 0, e = BS->getNumElements(); i != e; ++i) {
+      if (Expr *SubExpr = BS->getElement(i).dyn_cast<Expr*>()) {
+        if (Expr *E2 = doIt(SubExpr))
+          BS->setElement(i, E2);
+        else
+          return nullptr;
+        continue;
+      }
+      
+      if (Stmt *S = BS->getElement(i).dyn_cast<Stmt*>()) {
+        if (Stmt *S2 = doIt(S))
+          BS->setElement(i, S2);
+        else
+          return nullptr;
+        continue;
+      }
+
+      if (visitDecl(BS->getElement(i).get<Decl*>()))
+        return nullptr;
+    }
+    
+    return BS;
+  }
+
+  Stmt *visitReturnStmt(ReturnStmt *RS) {
+    if (Expr *E = doIt(RS->getResult()))
+      RS->setResult(E);
+    else
+      return nullptr;
+    return RS;
+  }
+  
+  Stmt *visitIfStmt(IfStmt *IS) {
+    if (Expr *E2 = doIt(IS->getCond()))
+      IS->setCond(E2);
+    else
+      return nullptr;
+    
+    if (Stmt *S2 = doIt(IS->getThenStmt()))
+      IS->setThenStmt(S2);
+    else
+      return nullptr;
+    
+    if (IS->getElseStmt()) {
+      if (Stmt *S2 = doIt(IS->getElseStmt()))
+        IS->setElseStmt(S2);
+      else
+        return nullptr;
+    }
+    return IS;
+  }
+  
+  Stmt *visitWhileStmt(WhileStmt *WS) {
+    if (Expr *E2 = doIt(WS->getCond()))
+      WS->setCond(E2);
+    else
+      return nullptr;
+    
+    if (Stmt *S2 = doIt(WS->getBody()))
+      WS->setBody(S2);
+    else
+      return nullptr;
+    return WS;
+  }
+
+  /// Returns true on failure.
+  bool visitDecl(Decl *D) {
+    if (VarDecl *VD = dyn_cast<VarDecl>(D)) {
+      if (Expr *Init = VD->getInit()) {
+#ifndef NDEBUG
+        PrettyStackTraceDecl debugStack("walking into initializer for", VD);
+#endif
+        if (Expr *E2 = doIt(Init))
+          VD->setInit(E2);
+        else
+          return true;
+      }
+    } else if (FuncDecl *FD = dyn_cast<FuncDecl>(D)) {
+      if (FuncExpr *Body = FD->getBody()) {
+#ifndef NDEBUG
+        PrettyStackTraceDecl debugStack("walking into body of", FD);
+#endif
+        if (FuncExpr *E2 = cast_or_null<FuncExpr>(doIt(Body)))
+          FD->setBody(E2);
+        else
+          return true;
+      }
+
+    } else if (ExtensionDecl *ED = dyn_cast<ExtensionDecl>(D)) {
+      for (Decl *M : ED->getMembers()) {
+        if (visitDecl(M))
+          return true;
+      }
+    }
+    return false;
+  }
+     
+public:
+  Traversal(swift::Walker &walker) : Walker(walker) {}
+
+  Expr *doIt(Expr *E) {
+    // Do the pre-order visitation.  If it returns false, we just
+    // skip entering subnodes of this tree.
+    if (!Walker.walkToExprPre(E))
+      return E;
+
+    // Otherwise, visit the children.
+    E = visit(E);
+
+    // If we didn't bail out, do post-order visitation.
+    if (E) E = Walker.walkToExprPost(E);
+
+    return E;
+  }
+
+  Stmt *doIt(Stmt *S) {
+    // Do the pre-order visitation.  If it returns false, we just
+    // skip entering subnodes of this tree.
+    if (!Walker.walkToStmtPre(S))
+      return S;
+
+    // Otherwise, visit the children.
+    S = visit(S);
+
+    // If we didn't bail out, do post-order visitation.
+    if (S) S = Walker.walkToStmtPost(S);
+
+    return S;
+  }
+};
+
+} // end anonymous namespace.
+
+Expr *Expr::walk(Walker &walker) {
+  return Traversal(walker).doIt(this);
+}
+
+Stmt *Stmt::walk(Walker &walker) {
+  return Traversal(walker).doIt(this);
+}
---
+++ lib/Sema/TypeCheckPattern.cpp
@@ -0,0 +1,193 @@
+//===--- TypeCheckPattern.cpp - Type Checking for Patterns ----------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2015 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+//
+// This file implements semantic analysis for patterns, analysing a
+// pattern tree in both bottom-up and top-down ways.
+//
+//===----------------------------------------------------------------------===//
+
+#include "TypeChecker.h"
+using namespace swift;
+
+static bool hasExplicitType(Pattern *P) {
+  while (ParenPattern *PP = dyn_cast<ParenPattern>(P))
+    P = PP->getSubPattern();
+  return isa<TypedPattern>(P);
+}
+
+/// Set the type of the pattern if it does not already have a type.
+/// Only evaluate the type expression if needed.
+#define SET_TYPE(pattern, type) { \
+  Pattern *PAT = (pattern); \
+  if (PAT->hasType()) \
+    assert(PAT->getType()->isEqual(type)); \
+  else \
+    PAT->setType(type); \
+}
+
+/// Perform bottom-up type-checking on a pattern.  If this returns
+/// false, the type of the pattern will have been set.
+bool TypeChecker::typeCheckPattern(Pattern *P) {
+  switch (P->getKind()) {
+  // Type-check paren patterns by checking the sub-pattern and
+  // propagating that type out.
+  case PatternKind::Paren: {
+    Pattern *SP = cast<ParenPattern>(P)->getSubPattern();
+    if (typeCheckPattern(SP))
+      return true;
+    SET_TYPE(P, SP->getType());
+    return false;
+  }
+
+  // If we see an explicit type annotation, coerce the sub-pattern to
+  // that type.
+  case PatternKind::Typed:
+    if (validateType(P->getType()))
+      return true;
+    if (convertToType(cast<TypedPattern>(P)->getSubPattern(), P->getType()))
+      return true;
+    return false;
+
+  // A wildcard or name pattern cannot appear by itself in a context
+  // which requires an explicit type.
+  case PatternKind::Any:
+  case PatternKind::Named:
+    diagnose(P->getLoc(), diag::cannot_infer_type_for_pattern);
+    return true;
+
+  // A tuple pattern propagates its tuple-ness out.
+  case PatternKind::Tuple: {
+    bool hadError = false;
+    SmallVector<TupleTypeElt, 8> typeElts;
+
+    for (TuplePatternElt &elt : cast<TuplePattern>(P)->getFields()) {
+      Type type;
+      Expr *init = elt.getInit();
+      Pattern *pattern = elt.getPattern();
+
+      // If there's an initializer and the pattern doesn't have an
+      // explicit type, then we want to type-check the initializer and
+      // coerce the pattern to that type.
+      if (init && !hasExplicitType(pattern)) {
+        if (typeCheckExpression(init)) {
+          hadError = true;
+        } else {
+          type = init->getType();
+          elt.setInit(init);
+        }
+
+      // Otherwise, we want to type-check the pattern.
+      } else if (typeCheckPattern(pattern)) {
+        hadError = true;
+      } else {
+        type = pattern->getType();
+
+        // If there's an initializer, use the pattern's type
+        // to type-check the initializer.
+        if (init) {
+          init = convertToType(init, type);
+          if (!init) {
+            hadError = true;
+          } else {
+            elt.setInit(init);
+          }
+        }
+      }
+
+      typeElts.push_back(TupleTypeElt(type, pattern->getBoundName(), init));
+    }
+
+    if (hadError) return true;
+    SET_TYPE(P, TupleType::get(typeElts, Context));
+    return false;
+  }
+  }
+  llvm_unreachable("bad pattern kind!");
+}
+
+/// Perform top-down type coercion on the given pattern.
+bool TypeChecker::convertToType(Pattern *P, Type type) {
+  switch (P->getKind()) {
+  // For parens, just set the type annotation and propagate inwards.
+  case PatternKind::Paren:
+    P->setType(type);
+    return convertToType(cast<ParenPattern>(P)->getSubPattern(), type);
+
+  // If we see an explicit type annotation, coerce the sub-pattern to
+  // that type.
+  case PatternKind::Typed: {
+    TypedPattern *TP = cast<TypedPattern>(P);
+    if (validateType(TP->getType()))
+      return true;
+
+    // Complain if the types don't match exactly.
+    // TODO: allow implicit conversions?
+    if (!type->isEqual(TP->getType())) {
+      diagnose(P->getLoc(), diag::pattern_type_mismatch_context, type);
+      return true;
+    }
+
+    return convertToType(TP->getSubPattern(), TP->getType());
+  }
+
+  // For wildcard and name patterns, just set the type.
+  case PatternKind::Named:
+    cast<NamedPattern>(P)->getDecl()->overwriteType(type);
+    // fallthrough
+  case PatternKind::Any:
+    P->setType(type);
+    return false;
+
+  // We can match a tuple pattern with a tuple type.
+  // TODO: permit implicit conversions?
+  case PatternKind::Tuple: {
+    TuplePattern *TP = cast<TuplePattern>(P);
+
+    // The context type must be a tuple.
+    TupleType *tupleTy = type->getAs<TupleType>();
+    if (!tupleTy) {
+      diagnose(TP->getLParenLoc(), diag::tuple_pattern_in_non_tuple_context,
+               type);
+      return true;
+    }
+
+    // The number of elements must match exactly.
+    // TODO: incomplete tuple patterns, with some syntax.
+    if (tupleTy->getFields().size() != TP->getNumFields()) {
+      diagnose(TP->getLParenLoc(), diag::tuple_pattern_length_mismatch, type);
+      return true;
+    }
+
+    // Coerce each tuple element to the respective type.
+    // TODO: detect and diagnose shuffling
+    // TODO: permit shuffling
+    P->setType(type);
+
+    for (unsigned i = 0, e = TP->getNumFields(); i != e; ++i) {
+      TuplePatternElt &elt = TP->getFields()[i];
+      Pattern *pattern = elt.getPattern();
+      if (elt.getInit()) {
+        diagnose(pattern->getLoc(), diag::coerced_tuple_pattern_has_init)
+          << pattern->getSourceRange();
+        elt.setInit(nullptr);
+      }
+
+      if (convertToType(pattern, tupleTy->getFields()[i].getType()))
+        return true;
+    }
+
+    P->setType(type);
+    return false;
+  }
+  }
+  llvm_unreachable("bad pattern kind!");
+}
