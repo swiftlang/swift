bf2ec19c65b5d527ae42edc67168d47da3b0b707
Howard Hinnant <hhinnant@apple.com>
2012-02-17 14:40:57 +0000

    The very beginnings of a very primitive run time library
    Swift SVN r1118

---
+++ runtime/swift/stdlib.cpp
@@ -0,0 +1,52 @@
+#include <inttypes.h>
+#include <stdio.h>
+
+extern "C" bool _TSsop2leFT3lhsNSs5int643rhsS__NSs4bool(int64_t l, int64_t r) {
+  return l <= r;
+}
+
+extern "C" int64_t _TSsop1sFT3lhsNSs5int643rhsS__S_(int64_t l, int64_t r) {
+  return l - r;
+}
+
+extern "C" int64_t _TSsop1pFT3lhsNSs5int643rhsS__S_(int64_t l, int64_t r) {
+  return l + r;
+}
+
+extern "C" double _TSsop1pFT3lhsNSs6double3rhsS__S_(double l, double r) {
+  return l + r;
+}
+
+extern "C" double _TSsop1sFT1aNSs6double_S_(double x) {
+    return -x;
+}
+
+extern "C" void _T3fib5printFT1iNSs5int64_T_(int64_t l) {
+  printf("%lld\n", l);
+}
+
+extern "C" void _T3fib5printFT1iNS_6double_T_(double l) {
+  printf("%f\n", l);
+}
+
+extern "C" void _T5nbody5printFT1iNSs6double_T_(double l) {
+  printf("%f\n", l);
+}
+
+extern "C" bool _TSs19convertToLogicValueFT1vNSs4bool_i1(bool b) {
+  return b;
+}
+
+extern "C"
+double
+_TNSs6double26convert_from_float_literalFT3valf64_S_(double x)
+{
+    return x;
+}
+
+extern "C"
+double
+_TSsop1mFT3lhsNSs6double3rhsS__S_(double x, double y)
+{
+    return x * y;
+}
