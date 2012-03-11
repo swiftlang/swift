b54d2bc5fbe6aa1c657ef5b4277e75a03c12f3f7
Chris Lattner <clattner@apple.com>
2012-03-11 16:29:47 +0000

    rename Walk.cpp -> ASTWalker.cpp
    Swift SVN r1202

--- lib/AST/ASTWalker.cpp
+++ lib/AST/ASTWalker.cpp
@@ -1,4 +1,4 @@
-//===--- Walk.cpp - AST Traversal -----------------------------------------===//
+//===--- ASTWalker.cpp - AST Traversal ------------------------------------===//
 //
 // This source file is part of the Swift.org open source project
 //
