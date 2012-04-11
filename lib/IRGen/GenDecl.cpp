6b1a0a2de40b0c5753a485b9fd4e5d5f677b5a39
John McCall <rjmccall@apple.com>
2012-04-11 03:39:01 +0000

    Merge GenLocal.cpp and GenGlobal.cpp.
    Swift SVN r1386

--- lib/IRGen/GenDecl.cpp
+++ lib/IRGen/GenDecl.cpp
@@ -1,4 +1,4 @@
-//===--- GenGlobal.cpp - IR Generation for Global Declarations ------------===//
+//===--- GenDecl.cpp - IR Generation for Declarations ---------------------===//
 //
 // This source file is part of the Swift.org open source project
 //
@@ -10,7 +10,8 @@
 //
 //===----------------------------------------------------------------------===//
 //
-//  This file implements IR generation for Swift's global context.
+//  This file implements IR generation for local and global
+//  declarations in Swift.
 //
 //===----------------------------------------------------------------------===//
 
@@ -282,4 +283,62 @@ void IRGenModule::emitExtension(ExtensionDecl *ext) {
     }
     llvm_unreachable("bad extension member kind");
   }
+}
+
+void IRGenFunction::emitLocal(Decl *D) {
+  switch (D->getKind()) {
+  case DeclKind::Import:
+    llvm_unreachable("declaration cannot appear in local scope");
+
+  // Type aliases require IR-gen support if they're really
+  // struct/oneof declarations.
+  case DeclKind::TypeAlias:
+    return IGM.emitTypeAlias(cast<TypeAliasDecl>(D)->getUnderlyingType());
+
+  case DeclKind::OneOfElement:
+    // no IR generation support required.
+    return;
+
+  case DeclKind::Var:
+    // We handle these in pattern-binding.
+    return;
+
+  case DeclKind::Extension:
+    unimplemented(D->getLocStart(), "local extension emission");
+    return;
+
+  case DeclKind::Func:
+    unimplemented(D->getLocStart(), "local function emission");
+    return;
+
+  case DeclKind::PatternBinding:
+    emitPatternBindingDecl(cast<PatternBindingDecl>(D));
+    return;
+  }
+  llvm_unreachable("bad declaration kind!");
+}
+
+OwnedAddress IRGenFunction::getLocal(ValueDecl *D) {
+  auto I = Locals.find(D);
+  assert(I != Locals.end() && "no entry in local map!");
+  return I->second.Var.Addr;
+}
+
+void IRGenFunction::setLocal(ValueDecl *D, OwnedAddress addr) {
+  assert(!Locals.count(D));
+
+  std::pair<ValueDecl*, LocalRecord> entry;
+  entry.first = D;
+  entry.second.Var.Addr = addr;
+
+  Locals.insert(entry);
+}
+
+/// Create an allocation on the stack.
+Address IRGenFunction::createAlloca(llvm::Type *type,
+                                    Alignment alignment,
+                                    const llvm::Twine &name) {
+  llvm::AllocaInst *alloca = new llvm::AllocaInst(type, name, AllocaIP);
+  alloca->setAlignment(alignment.getValue());
+  return Address(alloca, alignment);
 }
