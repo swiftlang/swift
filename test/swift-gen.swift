318e498b34c1f719bc5637c0a0a59b61e9cb0e52
Eli Friedman <eli_friedman@apple.com>
2012-05-03 22:48:41 +0000

    Remove swift.swift from test/.  Yay! :)
    Swift SVN r1729

--- test/swift-gen.swift
+++
@@ -1,5 +0,0 @@
-// RUN: %swift -triple x86_64-apple-darwin10 %S/swift.swift -emit-llvm | FileCheck %s
-
-// This test double-checks that swift.swift can be compiled to IR.
-
-// CHECK: define i1 @_TSsop2aaFT3lhsNSs4bool3rhsFT_S__S_(i1, i8*, i8*)
