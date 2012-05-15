a70b05e9b46c768ab2b0ecf80b1fdd453074290b
Eli Friedman <eli_friedman@apple.com>
2012-05-15 23:30:56 +0000

    Add doxygen comment.
    Swift SVN r1861

--- include/swift/AST/Decl.h
+++ include/swift/AST/Decl.h
@@ -444,6 +444,8 @@ public:
   static bool classof(const TypeAliasDecl *D) { return true; }
 };
 
+/// DistinctTypeDecl - a declaration of a distinct type, like a struct.  This
+/// decl is always a DeclContext.
 class DistinctTypeDecl : public TypeDecl, public DeclContext {
 public:
   DistinctTypeDecl(DeclKind K, DeclContext *DC, Identifier name, Type ty) :
