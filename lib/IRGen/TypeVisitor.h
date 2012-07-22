baae407f276d5041af732e64e9815785d03a4129
John McCall <rjmccall@apple.com>
2012-07-22 07:59:57 +0000

    New kind of visitor for visiting substituted types.
    Swift SVN r2396

--- lib/IRGen/TypeVisitor.h
+++ lib/IRGen/TypeVisitor.h
@@ -48,6 +48,103 @@ public:
     llvm_unreachable("DeducibleGenericParamType should not survive Sema");
   }
 };  
+
+/// SubstTypeVisitor - This is a specialized type visitor for visiting
+/// both a type and the result of substituting it.  The original type
+/// drives the selection, not the substitution result.
+///
+/// For most type kinds, the substitution type preserves the same
+/// structure as the original, and so the methods you declare
+/// should look like:
+///   visitFunctionType(FunctionType *origTy, FunctionType *substTy);
+///
+/// Archetypes are an exception, and the second parameter should just
+/// be a CanType:
+///   visitArchetypeType(FunctionType *origTy, CanType substTy);
+///
+/// In addition, all the leaf type kinds map to the same function:
+///   visitLeafType(CanType origTy, CanType substTy);
+template<typename Impl, typename RetTy = void> class SubstTypeVisitor {
+public:
+  RetTy visit(CanType origTy, CanType substTy) {
+    switch (origTy->getKind()) {
+#define TYPE(Id, Parent)
+#define UNCHECKED_TYPE(Id, Parent) \
+    case TypeKind::Id: \
+      llvm_unreachable(#Id "Type should not survive to IR-gen");
+#define SUGARED_TYPE(Id, Parent) \
+    case TypeKind::Id: \
+      llvm_unreachable(#Id "Type should not survive canonicalization");
+#include "swift/AST/TypeNodes.def"
+
+    case TypeKind::DeducibleGenericParam:
+      llvm_unreachable("DeducibleGenericParamType should not survive Sema");
+
+    case TypeKind::BuiltinFloat:
+    case TypeKind::BuiltinInteger:
+    case TypeKind::BuiltinObjectPointer:
+    case TypeKind::BuiltinObjCPointer:
+    case TypeKind::BuiltinRawPointer:
+    case TypeKind::Class:
+    case TypeKind::Module:
+    case TypeKind::OneOf:
+    case TypeKind::Protocol:
+    case TypeKind::ProtocolComposition:
+    case TypeKind::Struct:
+      return static_cast<Impl*>(this)->visitLeafType(origTy, substTy);
+
+    case TypeKind::Archetype:
+      return static_cast<Impl*>(this)
+        ->visitArchetypeType(cast<ArchetypeType>(origTy),
+                             substTy);
+
+    case TypeKind::Array:
+      return static_cast<Impl*>(this)
+        ->visitArrayType(cast<ArrayType>(origTy),
+                         cast<ArrayType>(substTy));
+
+    case TypeKind::BoundGeneric:
+      return static_cast<Impl*>(this)
+        ->visitBoundGenericType(cast<BoundGenericType>(origTy),
+                                cast<BoundGenericType>(substTy));
+
+    case TypeKind::Function:
+      return static_cast<Impl*>(this)
+        ->visitFunctionType(cast<FunctionType>(origTy),
+                            cast<FunctionType>(substTy));
+
+    case TypeKind::PolymorphicFunction:
+      return static_cast<Impl*>(this)
+        ->visitPolymorphicFunctionType(cast<PolymorphicFunctionType>(origTy),
+                                       cast<PolymorphicFunctionType>(substTy));
+
+    case TypeKind::LValue:
+      return static_cast<Impl*>(this)
+        ->visitLValueType(cast<LValueType>(origTy),
+                          cast<LValueType>(substTy));
+
+    case TypeKind::MetaType:
+      return static_cast<Impl*>(this)
+        ->visitMetaTypeType(cast<MetaTypeType>(origTy),
+                            cast<MetaTypeType>(substTy));
+
+    case TypeKind::Tuple:
+      return static_cast<Impl*>(this)
+        ->visitTupleType(cast<TupleType>(origTy),
+                         cast<TupleType>(substTy));
+    }
+    llvm_unreachable("bad type kind");
+  }
+
+  RetTy visitFunctionType(FunctionType *origTy, FunctionType *substTy) {
+    return static_cast<Impl*>(this)->visitAnyFunctionType(origTy, substTy);
+  }
+
+  RetTy visitPolymorphicFunctionType(PolymorphicFunctionType *origTy,
+                                     PolymorphicFunctionType *substTy) {
+    return static_cast<Impl*>(this)->visitAnyFunctionType(origTy, substTy);
+  }
+};
   
 } // end namespace irgen
 } // end namespace swift
