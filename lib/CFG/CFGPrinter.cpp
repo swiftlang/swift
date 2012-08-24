3930680b839bd495aa720ff91650aa2c152fa7e8
Chris Lattner <clattner@apple.com>
2012-08-24 20:20:32 +0000

    merge CFGPrintContext into CFGPrinter, zap a dead and incorrect << implementation.
    Swift SVN r2754

--- lib/CFG/CFGPrinter.cpp
+++ lib/CFG/CFGPrinter.cpp
@@ -22,23 +22,149 @@
 #include "llvm/ADT/OwningPtr.h"
 using namespace swift;
 
-namespace swift {
-class CFGPrintContext {
+//===----------------------------------------------------------------------===//
+// Pretty-printing
+//===----------------------------------------------------------------------===//
+
+namespace {
+/// CFGPrinter class - This is the internal implementation details of printing
+/// for CFG structures.
+class CFGPrinter : public CFGVisitor<CFGPrinter> {
+  raw_ostream &OS;
+
   typedef llvm::DenseMap<const BasicBlock *, unsigned> BlocksToIdsTy;
   llvm::OwningPtr<BlocksToIdsTy> BlocksToIDs;
+  raw_ostream &printID(const Instruction *I, bool includeBBPrefix = true);
+  raw_ostream &printID(const BasicBlock *B);
+  raw_ostream &printID(const BasicBlockArg *BBarg);
+  raw_ostream &printID(const CFGValue &V);
+
 public:
-  raw_ostream &printID(raw_ostream &OS, const Instruction *I,
-                       bool includeBBPrefix = true);
-  raw_ostream &printID(raw_ostream &OS, const BasicBlock *B);
-  raw_ostream &printID(raw_ostream &OS, const BasicBlockArg *BBarg);
-  raw_ostream &printID(raw_ostream &OS, const CFGValue &V);
+  CFGPrinter(raw_ostream &OS) : OS(OS) {
+  }
 
+  void print(const BasicBlock *BB) {
+    printID(BB) << ":\n";
+
+    for (const Instruction &I : *BB)
+      print(&I);
+
+    OS << "  Preds:";
+    for (const BasicBlock *B : BB->getPreds()) {
+      OS << ' ';
+      printID(B);
+    }
+    OS << '\n';
+    OS << "  Succs:";
+    for (const BasicBlock *B : BB->getSuccs()) {
+      OS << ' ';
+      printID(B);
+    }
+    OS << '\n';
+  }
+
+  //===--------------------------------------------------------------------===//
+  // Instruction Printing Logic
+
+  void print(const Instruction *I) {
+    OS << "  ";
+    printID(I, false) << " = ";
+    visit(const_cast<Instruction*>(I));
+    OS << '\n';
+  }
+
+  void visitInstruction(Instruction *I) {
+    assert(0 && "CFGPrinter not implemented for this instruction!");
+  }
+
+  void visitCallInst(CallInst *CI) {
+    OS << "Call(fn=";
+    printID(CI->function);
+    auto args = CI->arguments();
+    if (!args.empty()) {
+      bool first = true;
+      OS << ",args=(";
+      for (auto arg : args) {
+        if (first)
+          first = false;
+        else
+          OS << ' ';
+        printID(arg);
+      }
+      OS << ')';
+    }
+    OS << ')';
+  }
+
+  void visitDeclRefInst(DeclRefInst *DRI) {
+    OS << "DeclRef(decl=" << DRI->expr->getDecl()->getName() << ')';
+  }
+  void visitIntegerLiteralInst(IntegerLiteralInst *ILI) {
+    const auto &lit = ILI->literal->getValue();
+    OS << "Integer(val=" << lit << ",width=" << lit.getBitWidth() << ')';
+  }
+  void visitLoadInst(LoadInst *LI) {
+    OS << "Load(lvalue=";
+    printID(LI->lvalue);
+    OS << ')';
+  }
+  void visitThisApplyInst(ThisApplyInst *TAI) {
+    OS << "ThisApply(fn=";
+    printID(TAI->function);
+    OS << ",arg=";
+    printID(TAI->argument);
+    OS << ')';
+  }
+  void visitTupleInst(TupleInst *TI) {
+    OS << "Tuple(";
+    bool isFirst = true;
+    for (const auto &Elem : TI->elements()) {
+      if (isFirst)
+        isFirst = false;
+      else
+        OS << ',';
+      printID(Elem);
+    }
+    OS << ')';
+  }
+  void visitTypeOfInst(TypeOfInst *TOI) {
+    OS << "TypeOf(type=" << TOI->Expr->getType().getString() << ')';
+  }
+
+  void visitReturnInst(ReturnInst *RI) {
+    OS << "Return";
+    if (RI->returnValue) {
+      OS << '(';
+      printID(RI->returnValue);
+      OS << ')';
+    }
+  }
+
+  void visitUncondBranchInst(UncondBranchInst *UBI) {
+    OS << "br ";
+    printID(UBI->targetBlock());
+    const UncondBranchInst::ArgsTy Args = UBI->blockArgs();
+    if (!Args.empty()) {
+      OS << '(';
+      for (auto Arg : Args) { OS << "%" << Arg; }
+      OS << ')';
+    }
+  }
+
+  void visitCondBranchInst(CondBranchInst *CBI) {
+    OS << "cond_br(cond=";
+    OS << "?";
+    //      printID(BI.condition);
+    OS << ",branches=(";
+    printID(CBI->branches()[0]);
+    OS << ',';
+    printID(CBI->branches()[1]);
+    OS << "))";
+  }
 };
-}
+} // end anonymous namespace
 
-
-raw_ostream &CFGPrintContext::printID(raw_ostream &OS,
-                                      const BasicBlock *Block) {
+raw_ostream &CFGPrinter::printID(const BasicBlock *Block) {
   // Lazily initialize the Blocks-to-IDs mapping.
   if (!BlocksToIDs) {
     BlocksToIDs.reset(new BlocksToIdsTy());
@@ -56,9 +182,8 @@ raw_ostream &CFGPrintContext::printID(raw_ostream &OS,
   return OS;
 }
 
-raw_ostream &CFGPrintContext::printID(raw_ostream &OS,
-                                      const Instruction *Inst,
-                                      bool includeBBPrefix) {
+raw_ostream &CFGPrinter::printID(const Instruction *Inst,
+                                 bool includeBBPrefix) {
   const BasicBlock *Block = Inst->getParent();
   unsigned count = 1;
   for (const Instruction &I : *Block) {
@@ -68,199 +193,46 @@ raw_ostream &CFGPrintContext::printID(raw_ostream &OS,
   }
   OS << '%';
   if (includeBBPrefix) {
-    printID(OS, Block);
+    printID(Block);
     OS << '.';
   }
   OS << "i" << count;
   return OS;
 }
 
-raw_ostream &CFGPrintContext::printID(raw_ostream &OS,
-                                      const BasicBlockArg *BBArg) {
+raw_ostream &CFGPrinter::printID(const BasicBlockArg *BBArg) {
   OS << "BBArg (unsupported)\n";
   return OS;
 }
 
-raw_ostream &CFGPrintContext::printID(raw_ostream &OS,
-                                      const CFGValue &Val) {
+raw_ostream &CFGPrinter::printID(const CFGValue &Val) {
   if (const Instruction *Inst = Val.dyn_cast<Instruction*>())
-    printID(OS, Inst);
+    printID(Inst);
   else
-    printID(OS, Val.get<BasicBlockArg*>());
+    printID(Val.get<BasicBlockArg*>());
   return OS;
 }
 
 //===----------------------------------------------------------------------===//
-// Pretty-printing
+// Printing for Instruction, BasicBlock, and CFG
 //===----------------------------------------------------------------------===//
-
-namespace {
-/// CFGPrinter class - This is the internal implementation details of printing
-/// for CFG structures.
-class CFGPrinter : public CFGVisitor<CFGPrinter> {
-  raw_ostream &OS;
-  CFGPrintContext &PC;
-public:
-  CFGPrinter(raw_ostream &OS, CFGPrintContext &PC) : OS(OS), PC(PC) {
-  }
-
-  void print(const BasicBlock *BB) {
-    PC.printID(OS, BB) << ":\n";
-
-    for (const Instruction &I : *BB)
-      print(&I);
-
-    OS << "  Preds:";
-    for (const BasicBlock *B : BB->getPreds()) {
-      OS << ' ';
-      PC.printID(OS, B);
-    }
-    OS << '\n';
-    OS << "  Succs:";
-    for (const BasicBlock *B : BB->getSuccs()) {
-      OS << ' ';
-      PC.printID(OS, B);
-    }
-    OS << '\n';
-  }
-
-  //===--------------------------------------------------------------------===//
-  // Instruction Printing Logic
-
-  void print(const Instruction *I) {
-    OS << "  ";
-    PC.printID(OS, I, false) << " = ";
-    visit(const_cast<Instruction*>(I));
-    OS << '\n';
-  }
-
-  void visitInstruction(Instruction *I) {
-    assert(0 && "CFGPrinter not implemented for this instruction!");
-  }
-
-  void visitCallInst(CallInst *CI) {
-    OS << "Call(fn=";
-    PC.printID(OS, CI->function);
-    auto args = CI->arguments();
-    if (!args.empty()) {
-      bool first = true;
-      OS << ",args=(";
-      for (auto arg : args) {
-        if (first)
-          first = false;
-        else
-          OS << ' ';
-        PC.printID(OS, arg);
-      }
-      OS << ')';
-    }
-    OS << ')';
-  }
-
-  void visitDeclRefInst(DeclRefInst *DRI) {
-    OS << "DeclRef(decl=" << DRI->expr->getDecl()->getName() << ')';
-  }
-  void visitIntegerLiteralInst(IntegerLiteralInst *ILI) {
-    const auto &lit = ILI->literal->getValue();
-    OS << "Integer(val=" << lit << ",width=" << lit.getBitWidth() << ')';
-  }
-  void visitLoadInst(LoadInst *LI) {
-    OS << "Load(lvalue=";
-    PC.printID(OS, LI->lvalue);
-    OS << ')';
-  }
-  void visitThisApplyInst(ThisApplyInst *TAI) {
-    OS << "ThisApply(fn=";
-    PC.printID(OS, TAI->function);
-    OS << ",arg=";
-    PC.printID(OS, TAI->argument);
-    OS << ')';
-  }
-  void visitTupleInst(TupleInst *TI) {
-    OS << "Tuple(";
-    bool isFirst = true;
-    for (const auto &Elem : TI->elements()) {
-      if (isFirst)
-        isFirst = false;
-      else
-        OS << ',';
-      PC.printID(OS, Elem);
-    }
-    OS << ')';
-  }
-  void visitTypeOfInst(TypeOfInst *TOI) {
-    OS << "TypeOf(type=" << TOI->Expr->getType().getString() << ')';
-  }
-
-  void visitReturnInst(ReturnInst *RI) {
-    OS << "Return";
-    if (RI->returnValue) {
-      OS << '(';
-      PC.printID(OS, RI->returnValue);
-      OS << ')';
-    }
-  }
-
-  void visitUncondBranchInst(UncondBranchInst *UBI) {
-    OS << "br ";
-    PC.printID(OS, UBI->targetBlock());
-    const UncondBranchInst::ArgsTy Args = UBI->blockArgs();
-    if (!Args.empty()) {
-      OS << '(';
-      for (auto Arg : Args) { OS << "%" << Arg; }
-      OS << ')';
-    }
-  }
-
-  void visitCondBranchInst(CondBranchInst *CBI) {
-    OS << "cond_br(cond=";
-    OS << "?";
-    //      PC.printID(OS, BI.condition);
-    OS << ",branches=(";
-    PC.printID(OS, CBI->branches()[0]);
-    OS << ',';
-    PC.printID(OS, CBI->branches()[1]);
-    OS << "))";
-  }
-};
-} // end anonymous namespace
 
 void Instruction::dump() const {
   print(llvm::errs());
 }
 
 void Instruction::print(raw_ostream &OS) const {
-  CFGPrintContext PC;
-  CFGPrinter(OS, PC).print(this);
+  CFGPrinter(OS).print(this);
 }
 
-
-//===----------------------------------------------------------------------===//
-// Pretty-printing for BasicBlock
-//===----------------------------------------------------------------------===//
-
-/// Pretty-print the BasicBlock.
 void BasicBlock::dump() const {
   print(llvm::errs());
 }
 
 /// Pretty-print the BasicBlock with the designated stream.
 void BasicBlock::print(raw_ostream &OS) const {
-  CFGPrintContext PC;
-  CFGPrinter(OS, PC).print(this);
+  CFGPrinter(OS).print(this);
 }
-
-// FIXME: This is wrong.
-namespace llvm {
-raw_ostream &operator<<(raw_ostream &OS, const ::swift::BasicBlock &B) {
-  OS << 'B' << (void*) &B;
-  return OS;
-}
-} // end namespace llvm
-
-//===----------------------------------------------------------------------===//
-// Pretty-printing for CFG
-//===----------------------------------------------------------------------===//
 
 /// Pretty-print the basic block.
 void CFG::dump() const {
@@ -269,8 +241,7 @@ void CFG::dump() const {
 
 /// Pretty-print the basi block with the designated stream.
 void CFG::print(llvm::raw_ostream &OS) const {
-  CFGPrintContext PC;
-  CFGPrinter Printer(OS, PC);
+  CFGPrinter Printer(OS);
   for (const BasicBlock &B : *this)
     Printer.print(&B);
 }
