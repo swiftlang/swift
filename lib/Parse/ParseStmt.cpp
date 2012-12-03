683ef3548bd0d72cbb129b47184c8650042c98c8
Joe Groff <jgroff@apple.com>
2012-12-03 23:18:50 +0000

    Allow ';' in a top-level form.
    Swift SVN r3338

--- lib/Parse/ParseStmt.cpp
+++ lib/Parse/ParseStmt.cpp
@@ -186,10 +186,13 @@ void Parser::parseBraceItemList(SmallVectorImpl<ExprStmtOrDecl> &Entries,
       skipUntilDeclStmtRBrace();
 
     if (TLCD && !IsMainModule && !NeedParseErrorRecovery) {
-      if (TLCD->getBody().is<Stmt*>()) {
+      if (Stmt *s = TLCD->getBody().dyn_cast<Stmt*>()) {
         // Statements are not allowed at the top level outside the main module.
-        SourceLoc Loc = TLCD->getBody().get<Stmt*>()->getStartLoc();
-        diagnose(Loc, diag::illegal_top_level_stmt);
+        // Empty ';' statements are allowed.
+        if (!isa<SemiStmt>(s)) {
+          SourceLoc Loc = s->getStartLoc();
+          diagnose(Loc, diag::illegal_top_level_stmt);
+        }
       } else {
         // Expressions are not allowed at the top level outside the main module.
         SourceLoc Loc = TLCD->getBody().get<Expr*>()->getStartLoc();
