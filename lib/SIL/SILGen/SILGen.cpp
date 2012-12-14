99c31002e97374bd50793bdf4d504ab3be7e8c3e
Joe Groff <jgroff@apple.com>
2012-12-14 17:49:59 +0000

    SILGen: Back out debug dumps again. Sorry.
    Swift SVN r3494

--- lib/SIL/SILGen/SILGen.cpp
+++ lib/SIL/SILGen/SILGen.cpp
@@ -100,13 +100,9 @@ Function *SILGenModule::emitFunction(SILConstant::Loc decl, FuncExpr *fe) {
   assert(!M.hasFunction(constant) &&
          "already generated function for decl!");
 
-  constant.dump();
-  fe->dump();
-
   Function *f = new (M) Function(M);
   SILGenFunction(*this, *f, fe).visit(fe->getBody());
   
-  f->dump();
   f->verify();
   M.functions[constant] = f;
   
@@ -118,13 +114,9 @@ Function *SILGenModule::emitClosure(ClosureExpr *ce) {
   assert(!M.hasFunction(constant) &&
          "already generated function for closure!");
   
-  constant.dump();
-  ce->dump();
-
   Function *f = new (M) Function(M);
   SILGenFunction(*this, *f, ce).emitClosureBody(ce->getBody());
   
-  f->dump();
   f->verify();
   M.functions[constant] = f;
   
