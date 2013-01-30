cf0d186bf8064e9ee014ac6b5fd00a371d21e95a
Joe Groff <jgroff@apple.com>
2013-01-30 21:47:20 +0000

    IRGen: Undo r3904. It fixed a symptom, not a problem. The actual problem is fixed in r3905.
    Swift SVN r3908

--- lib/IRGen/GenClass.cpp
+++ lib/IRGen/GenClass.cpp
@@ -629,18 +629,15 @@ static void emitClassAllocatingConstructor(IRGenModule &IGM,
   // Gather our arguments.
   Explosion args = IGF.collectParameters();
   
-  // Pick out the metatype we received. There may be witness table
-  // arguments in the way.
-  SmallVector<llvm::Value *, 2> witnessTables;
-  llvm::Value *metadata;
-  while (metadata = args.takeLast().getUnmanagedValue(),
-         metadata->getType() == IGM.WitnessTablePtrTy) {
-    witnessTables.push_back(metadata);
-  }
-  assert(metadata->getType() == IGM.TypeMetadataPtrTy &&
-         "did not find a metadata argument to allocating constructor?");
+  // Pick out the metatype we received and use it as the local type data for
+  // the 'this' type.
+  llvm::Value *metadata = args.takeLast().getUnmanagedValue();
   
-  // Use the metadata we found as the local type data for the 'this' type.
+  // If the argument wasn't a type metadata pointer, something is screwed up in
+  // extracting fulfillments from the class metadata.
+  assert(metadata->getType() == IGM.TypeMetadataPtrTy &&
+         "allocating ctor did not receive class metadata as its last param?!");
+  
   IGF.setUnscopedLocalTypeData(thisType,
                                LocalTypeData::Metatype,
                                metadata);
@@ -664,11 +661,7 @@ static void emitClassAllocatingConstructor(IRGenModule &IGM,
   
   // Pass 'this' on to the initializer.
   argValues.push_back(thisValue);
-  
-  // Pass along the witness tables.
-  argValues.insert(argValues.end(),
-                   witnessTables.begin(), witnessTables.end());
-  
+    
   // Emit a tail call to the initializing constructor.
   llvm::CallSite callSite = IGF.emitInvoke(initFn->getCallingConv(),
                                            initFn, argValues,
