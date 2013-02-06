482826925812c8aa6d7ef861adbe36b26a4dedda
Joe Groff <jgroff@apple.com>
2013-02-06 17:21:00 +0000

    Add a buildbot script.
    This script configures and builds LLVM, Clang, and Swift from a fresh checkout (which should be prepared by Jenkins) and then runs the Swift tests.
    
    Swift SVN r3969

---
+++ utils/buildbot-script.sh
@@ -0,0 +1,64 @@
+#!/bin/sh -ex
+
+# The Jenkins workspace is set up as follows:
+# - LLVM checked out to ${WORKSPACE}/llvm
+# - Clang checked out to ${WORKSPACE}/llvm/tools/clang
+# - Swift checked out to ${WORKSPACE}/swift
+# Jenkins is set up to restore the repositories to pristine state before
+# building, so we rebuild from scratch every time.
+
+# Flags for testing:
+# SKIP_BUILD_LLVM -- set to skip building LLVM/Clang
+# SKIP_BUILD_SWIFT -- set to skip building Swift
+# SKIP_TEST_SWIFT -- set to skip testing Swift
+
+# Set these to the paths of the OS X SDK and toolchain
+SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk
+TOOLCHAIN=/Applications/Xcode.app/Contents/Developer/Toolchains/OSX10.9.xctoolchain
+
+# Make sure the variables and directories we expect to exist actually do.
+test "$WORKSPACE"
+test -d "$WORKSPACE"
+test -d "$WORKSPACE/llvm"
+test -d "$WORKSPACE/llvm/tools/clang"
+test -d "$WORKSPACE/swift"
+
+# Build LLVM and Clang (x86 target only).
+if [ \! "$SKIP_BUILD_LLVM" ]; then
+  echo "--- Building LLVM and Clang ---"
+  mkdir -p "$WORKSPACE/llvm/build"
+  (cd "$WORKSPACE/llvm/build" &&
+    cmake -G "Unix Makefiles" \
+      -DCMAKE_C_COMPILER="$TOOLCHAIN/usr/bin/clang" \
+      -DCMAKE_CXX_COMPILER="$TOOLCHAIN/usr/bin/clang++" \
+      -DCMAKE_CXX_FLAGS="-stdlib=libc++" \
+      -DCMAKE_EXE_LINKER_FLAGS="-stdlib=libc++" \
+      -DCMAKE_SHARED_LINKER_FLAGS="-stdlib=libc++" \
+      -DLLVM_TARGETS_TO_BUILD="X86" \
+      .. &&
+    make -j) || exit 1
+fi
+
+# Build Swift.
+if [ \! "$SKIP_BUILD_SWIFT" ]; then
+  echo "--- Building Swift ---"
+  mkdir -p "$WORKSPACE/swift/build"
+  (cd "$WORKSPACE/swift/build" &&
+    cmake -G "Unix Makefiles" \
+      -DCMAKE_C_COMPILER="$TOOLCHAIN/usr/bin/clang" \
+      -DCMAKE_CXX_COMPILER="$TOOLCHAIN/usr/bin/clang++" \
+      -DSWIFT_PATH_TO_CLANG_SOURCE="$WORKSPACE/llvm/tools/clang" \
+      -DSWIFT_PATH_TO_CLANG_BUILD="$WORKSPACE/llvm/build" \
+      -DSWIFT_PATH_TO_LLVM_SOURCE="$WORKSPACE/llvm" \
+      -DSWIFT_PATH_TO_LLVM_BUILD="$WORKSPACE/llvm/build" \
+      .. &&
+    make -j) || exit 1
+fi
+
+# Run the Swift tests.
+if [ \! "$SKIP_TEST_SWIFT" ]; then
+  export SWIFT="$WORKSPACE/swift/build/bin/swift"
+  echo "--- Running Swift Tests ---"
+  (cd "$WORKSPACE/swift/build" &&
+    "$WORKSPACE/llvm/build/bin/llvm-lit" -sv test) || exit 1
+fi
