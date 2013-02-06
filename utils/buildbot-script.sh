f6a80aecc7a781b2d3b163fb84888a576e39aa67
Joe Groff <jgroff@apple.com>
2013-02-06 19:28:44 +0000

    Tweaks for the buildbot script.
    /usr/local/bin isn't on PATH in the Jenkins environment, so add a variable to point at cmake. Jenkins' "emulate clean checkout" behavior also clobbers clang when it's checked out inside an llvm checkout, so change the script to expect LLVM and Clang checked out side-by-side and create a symlink to Clang from the LLVM tree.
    
    Swift SVN r3972

--- utils/buildbot-script.sh
+++ utils/buildbot-script.sh
@@ -2,7 +2,7 @@
 
 # The Jenkins workspace is set up as follows:
 # - LLVM checked out to ${WORKSPACE}/llvm
-# - Clang checked out to ${WORKSPACE}/llvm/tools/clang
+# - Clang checked out to ${WORKSPACE}/clang
 # - Swift checked out to ${WORKSPACE}/swift
 # Jenkins is set up to restore the repositories to pristine state before
 # building, so we rebuild from scratch every time.
@@ -16,19 +16,26 @@
 SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk
 TOOLCHAIN=/Applications/Xcode.app/Contents/Developer/Toolchains/OSX10.9.xctoolchain
 
+# Set this to the path to the 'cmake' executable.
+CMAKE=/usr/local/bin/cmake
+
 # Make sure the variables and directories we expect to exist actually do.
 test "$WORKSPACE"
 test -d "$WORKSPACE"
 test -d "$WORKSPACE/llvm"
-test -d "$WORKSPACE/llvm/tools/clang"
+test -d "$WORKSPACE/llvm/tools"
+test -d "$WORKSPACE/clang"
 test -d "$WORKSPACE/swift"
+
+# Symlink clang into the llvm tree.
+ln -sf "$WORKSPACE/clang" "$WORKSPACE/llvm/tools/clang"
 
 # Build LLVM and Clang (x86 target only).
 if [ \! "$SKIP_BUILD_LLVM" ]; then
   echo "--- Building LLVM and Clang ---"
   mkdir -p "$WORKSPACE/llvm/build"
   (cd "$WORKSPACE/llvm/build" &&
-    cmake -G "Unix Makefiles" \
+    "$CMAKE" -G "Unix Makefiles" \
       -DCMAKE_C_COMPILER="$TOOLCHAIN/usr/bin/clang" \
       -DCMAKE_CXX_COMPILER="$TOOLCHAIN/usr/bin/clang++" \
       -DCMAKE_CXX_FLAGS="-stdlib=libc++" \
@@ -44,7 +51,7 @@ if [ \! "$SKIP_BUILD_SWIFT" ]; then
   echo "--- Building Swift ---"
   mkdir -p "$WORKSPACE/swift/build"
   (cd "$WORKSPACE/swift/build" &&
-    cmake -G "Unix Makefiles" \
+    "$CMAKE" -G "Unix Makefiles" \
       -DCMAKE_C_COMPILER="$TOOLCHAIN/usr/bin/clang" \
       -DCMAKE_CXX_COMPILER="$TOOLCHAIN/usr/bin/clang++" \
       -DSWIFT_PATH_TO_CLANG_SOURCE="$WORKSPACE/llvm/tools/clang" \
