7afd5f0348f5a2b804f67f43a6454482597c218a
Jordan Rose <jordan_rose@apple.com>
2013-02-14 23:07:26 +0000

    Remove the "no Swift bit" hack entirely.
    As Doug points out, we won't regress here. All Objective-C runtimes we care
    about are correctly ignoring this bit.
    
    <rdar://problem/13065246>
    
    Swift SVN r4052

--- lib/IRGen/GenMeta.cpp
+++ lib/IRGen/GenMeta.cpp
@@ -809,13 +809,10 @@ namespace {
       // Derive the RO-data.
       llvm::Constant *data = emitClassPrivateData(IGM, TargetClass);
 
-      // FIXME: This is not supported on alpha OS builds yet.
-#ifndef SWIFT_DISABLE_SWIFT_BIT_ON_CLASS_DATA
       // We always set the low bit to indicate this is a Swift class.
       data = llvm::ConstantExpr::getPtrToInt(data, IGM.IntPtrTy);
       data = llvm::ConstantExpr::getAdd(data,
                                     llvm::ConstantInt::get(IGM.IntPtrTy, 1));
-#endif
 
       Fields.push_back(data);
     }
