2bf56a3a2eff4a476da9ec77e121f26f23769297
John McCall <rjmccall@apple.com>
2013-03-21 05:29:16 +0000

    Add a Range type which just bundles two iterators together and lets you iterate that.
    Swift SVN r4463

---
+++ include/swift/Basic/Range.h
@@ -0,0 +1,42 @@
+//===- Range.h - A lightweight pair of iterators ----------------*- C++ -*-===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2015 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+//
+//  This file provides Range, a template class which makes it easy to
+//  turn a pair of iterators into a "collection" suitable for C++11's
+//  for loop.
+//
+//  Note that this is kept in Swift because it's really only useful in
+//  C++11, and there aren't any major open-source subprojects of LLVM
+//  that can use C++11 yet.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef SWIFT_BASIC_RANGE_H
+#define SWIFT_BASIC_RANGE_H
+
+#include <utility>
+
+namespace swift {
+  /// A pair of iterators which can be used as the collection of a
+  /// C++11 for-loop.
+  template <typename T> class Range {
+    T Begin;
+    T End;
+  public:
+    Range(const T &begin, const T &end) : Begin(begin), End(end) {}
+    T begin() { return Begin; }
+    T end() { return End; }
+    bool empty() const { return Begin == End; }
+  };
+}
+
+#endif
