e46bb068f190a0dee98b566da60b00869411434f
John McCall <rjmccall@apple.com>
2013-04-12 01:39:50 +0000

    Add ASSERT_IMPLEMENTS_STATIC for overrides of static methods.
    Swift SVN r4704

--- include/swift/Basic/AssertImplements.h
+++ include/swift/Basic/AssertImplements.h
@@ -21,18 +21,42 @@
 #include <type_traits>
 
 namespace swift {
-  template <class ExpectedMemberType, class BaseType>
-  BaseType get_implementing_class_helper(ExpectedMemberType BaseType::*member);
+  template <class ExpectedType, class BaseType>
+  BaseType get_implementing_class_helper(ExpectedType BaseType::*member);
 
 #define GET_IMPLEMENTING_CLASS(DerivedClass, MemberName, ExpectedType)          \
   decltype(::swift::get_implementing_class_helper<ExpectedType>(                \
                                                     &DerivedClass::MemberName))
 
+/// Statically assert that DerivedClass overrides the instance method
+/// MemberName (at overload ExpectedType).  It doesn't have to be
+/// overridden directly in DerivedClass; it just can't still be the
+/// implementation from BaseClass.
 #define ASSERT_IMPLEMENTS(DerivedClass, BaseClass, MemberName, ExpectedType)    \
   static_assert(!::std::is_same<BaseClass,                                      \
                                 GET_IMPLEMENTING_CLASS(DerivedClass, MemberName,\
                                                        ExpectedType)            \
                                >::value,                                        \
+                "" #DerivedClass " does not properly override " #MemberName)
+
+
+  template <class T, T *impl0, T *impl1> struct is_same_pointer {
+    enum { value = false };
+  };
+
+  template <class T, T *impl> struct is_same_pointer<T, impl, impl> {
+    enum { value = true };
+  };
+
+/// Statically assert that DerivedClass overrides the static method
+/// MemberName (at overload ExpectedType).  It doesn't have to be
+/// overridden directly in DerivedClass; it just can't still be the
+/// implementation from BaseClass.
+#define ASSERT_IMPLEMENTS_STATIC(DerivedClass, BaseClass, MemberName,           \
+                                 ExpectedType)                                  \
+  static_assert(!::swift::is_same_pointer<ExpectedType,                         \
+                                          &BaseClass::MemberName,               \
+                                          &DerivedClass::MemberName>::value,    \
                 "" #DerivedClass " does not properly override " #MemberName)
 }
 
