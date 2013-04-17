fc9b9b22a5ac87eb1e60b9041030d96786072f6a
Chris Lattner <clattner@apple.com>
2013-04-17 06:08:34 +0000

    remove IRGen support for byref(heap), part of rdar://11247831.
    Swift SVN r4774

--- lib/IRGen/GenLValue.cpp
+++ lib/IRGen/GenLValue.cpp
@@ -76,116 +76,6 @@ OwnedAddress IRGenFunction::emitAddressForPhysicalLValue(const LValue &lvalue) {
   return address;
 }
 
-namespace {
-  /// The type layout for [byref(heap)] types.
-  class HeapLValueTypeInfo :
-    public ScalarTypeInfo<HeapLValueTypeInfo,FixedTypeInfo> {
-  public:
-    HeapLValueTypeInfo(llvm::StructType *type, Size s, Alignment a)
-      : ScalarTypeInfo(type, s, a, IsNotPOD) {}
-
-    llvm::StructType *getStorageType() const {
-      return cast<llvm::StructType>(TypeInfo::getStorageType());
-    }
-
-    unsigned getExplosionSize(ExplosionKind kind) const {
-      return 2;
-    }
-
-    void getSchema(ExplosionSchema &schema) const {
-      llvm::StructType *ty = getStorageType();
-      assert(ty->getNumElements() == 2);
-      schema.add(ExplosionSchema::Element::forScalar(ty->getElementType(0)));
-      schema.add(ExplosionSchema::Element::forScalar(ty->getElementType(1)));
-    }
-
-    static Address projectReference(IRGenFunction &IGF, Address address) {
-      return IGF.Builder.CreateStructGEP(address, 0, Size(0),
-                                         address->getName() + ".reference");
-    }
-
-    static Address projectOwner(IRGenFunction &IGF, Address address) {
-      return IGF.Builder.CreateStructGEP(address, 1, IGF.IGM.getPointerSize(),
-                                         address->getName() + ".owner");
-    }
-
-    void load(IRGenFunction &IGF, Address address, Explosion &e) const {
-      // Load the reference.
-      Address refAddr = projectReference(IGF, address);
-      e.addUnmanaged(
-            IGF.Builder.CreateLoad(refAddr, refAddr->getName() + ".load"));
-
-      // Load the owner.
-      IGF.emitLoadAndRetain(projectOwner(IGF, address), e);
-    }
-
-    void loadAsTake(IRGenFunction &IGF, Address address, Explosion &e) const {
-      // Load the reference.
-      Address refAddr = projectReference(IGF, address);
-      e.addUnmanaged(IGF.Builder.CreateLoad(refAddr));
-
-      // Load the owner.
-      Address ownerAddr = projectOwner(IGF, address);
-      e.addUnmanaged(IGF.Builder.CreateLoad(ownerAddr));
-    }
-
-    void loadUnmanaged(IRGenFunction &IGF, Address address,
-                       Explosion &e) const {
-      // Load the reference.
-      Address refAddr = projectReference(IGF, address);
-      e.addUnmanaged(IGF.Builder.CreateLoad(refAddr));
-      
-      // Load the owner.
-      Address ownerAddr = projectOwner(IGF, address);
-      e.addUnmanaged(IGF.Builder.CreateLoad(ownerAddr));
-    }
-    
-    void assign(IRGenFunction &IGF, Explosion &e, Address address) const {
-      // Store the reference.
-      IGF.Builder.CreateStore(e.claimUnmanagedNext(),
-                              projectReference(IGF, address));
-
-      // Store the owner.
-      IGF.emitAssignRetained(e.forwardNext(IGF),
-                             projectOwner(IGF, address));
-    }
-
-    void initialize(IRGenFunction &IGF, Explosion &e, Address address) const {
-      // Store the reference.
-      IGF.Builder.CreateStore(e.claimUnmanagedNext(),
-                              projectReference(IGF, address));
-
-      // Store the owner, transferring the +1.
-      IGF.emitInitializeRetained(e.forwardNext(IGF),
-                                 projectOwner(IGF, address));
-    }
-
-    void copy(IRGenFunction &IGF, Explosion &src, Explosion &dest) const {
-      src.transferInto(dest, 1);
-      IGF.emitRetain(src.claimNext().getValue(), dest);
-    }
-
-    void retain(IRGenFunction &IGF, Explosion &e) const {
-      e.claimNext();
-      IGF.emitRetainCall(e.claimNext().getValue());
-    }
-      
-    void release(IRGenFunction &IGF, Explosion &e) const {
-      e.claimNext();
-      IGF.emitRelease(e.claimNext().getValue());
-    }
-      
-    void manage(IRGenFunction &IGF, Explosion &src, Explosion &dest) const {
-      src.transferInto(dest, 1);
-      dest.add(IGF.enterReleaseCleanup(src.claimUnmanagedNext()));
-    }
-
-    void destroy(IRGenFunction &IGF, Address addr) const {
-      IGF.emitRelease(IGF.Builder.CreateLoad(projectOwner(IGF, addr)));
-    }
-  };
-}
-
 /// Convert an l-value type.  For non-heap l-values, this is always
 /// just a bare pointer.  For heap l-values, this is a pair of a bare
 /// pointer with an object reference.
@@ -195,22 +85,9 @@ const TypeInfo *TypeConverter::convertLValueType(LValueType *T) {
 
   // If it's not a heap l-value, just use the reference type as a
   // primitive pointer.
-  if (!T->isHeap()) {
-    return createPrimitive(referenceType, IGM.getPointerSize(),
-                           IGM.getPointerAlignment());
-  }
-
-  // Otherwise, pair up the reference with an owner pointer.  We put
-  // the reference before the owner because, if we do ever see one of
-  // these held in memory, it's more likely that we'll want the
-  // reference by itself than that we'd want the owner by itself, so
-  // it makes sense to give it the trivial offset.  Generally that's
-  // not going to matter too much, though, because the offset will
-  // probably get combined with some base offset.
-  llvm::Type *elts[] = { referenceType, IGM.RefCountedPtrTy };
-  llvm::StructType *pairTy =
-    llvm::StructType::get(IGM.getLLVMContext(), elts, /*packed*/ false);
-  return new HeapLValueTypeInfo(pairTy, IGM.getPointerSize() * 2,
-                                IGM.getPointerAlignment());
+  assert(!T->isHeap());
+  
+  return createPrimitive(referenceType, IGM.getPointerSize(),
+                         IGM.getPointerAlignment());
 }
 
