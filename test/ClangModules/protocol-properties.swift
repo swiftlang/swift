1e1b78ea09443cbe6de2cf415136d4e53f8771aa
Doug Gregor <dgregor@apple.com>
2013-05-09 16:20:24 +0000

    XFAIL'd testcases are uninteresting, especially when they take a long time to crash.
    Swift SVN r5112

--- test/ClangModules/protocol-properties.swift
+++
@@ -1,7 +0,0 @@
-// RUN: rm -rf %t/clang-module-cache
-// RUN: %swift -constraint-checker -emit-llvm -module-cache-path=%t/clang-module-cache -sdk=%S/Inputs -I=%S/Inputs/custom-modules %s
-
-import TestProtocols
-
-// This used to crash IRGen because Foo has a property.
-var p = FooProto
