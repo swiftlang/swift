fec44b22c7f70a6534b28180afaff087b4839775
Chris Lattner <clattner@apple.com>
2013-07-01 16:14:34 +0000

    silence signed comparison warnings.
    Swift SVN r5923

--- unittests/Basic/TreeScopedHashTableTests.cpp
+++ unittests/Basic/TreeScopedHashTableTests.cpp
@@ -16,15 +16,15 @@ TEST(TreeScopedHashTableTest, T1) {
   HT.insertIntoScope(*S2, 3, 2003);
   HT.insertIntoScope(*S2, 4, 2004);
 
-  EXPECT_EQ(1001, HT.lookup(*S1, 1));
-  EXPECT_EQ(1002, HT.lookup(*S1, 2));
-  EXPECT_EQ(0,    HT.lookup(*S1, 3));
-  EXPECT_EQ(0,    HT.lookup(*S1, 4));
+  EXPECT_EQ(1001U, HT.lookup(*S1, 1));
+  EXPECT_EQ(1002U, HT.lookup(*S1, 2));
+  EXPECT_EQ(0U,    HT.lookup(*S1, 3));
+  EXPECT_EQ(0U,    HT.lookup(*S1, 4));
 
-  EXPECT_EQ(1001, HT.lookup(*S2, 1));
-  EXPECT_EQ(1002, HT.lookup(*S2, 2));
-  EXPECT_EQ(2003, HT.lookup(*S2, 3));
-  EXPECT_EQ(2004, HT.lookup(*S2, 4));
+  EXPECT_EQ(1001U, HT.lookup(*S2, 1));
+  EXPECT_EQ(1002U, HT.lookup(*S2, 2));
+  EXPECT_EQ(2003U, HT.lookup(*S2, 3));
+  EXPECT_EQ(2004U, HT.lookup(*S2, 4));
 
   delete S2;
   delete S1;
@@ -47,20 +47,20 @@ TEST(TreeScopedHashTableTest, T2) {
   HT.insertIntoScope(*S3, 1, 3001);
   HT.insertIntoScope(*S3, 4, 3004);
 
-  EXPECT_EQ(1001, HT.lookup(*S1, 1));
-  EXPECT_EQ(1002, HT.lookup(*S1, 2));
-  EXPECT_EQ(0,    HT.lookup(*S1, 3));
-  EXPECT_EQ(0,    HT.lookup(*S1, 4));
+  EXPECT_EQ(1001U, HT.lookup(*S1, 1));
+  EXPECT_EQ(1002U, HT.lookup(*S1, 2));
+  EXPECT_EQ(0U,    HT.lookup(*S1, 3));
+  EXPECT_EQ(0U,    HT.lookup(*S1, 4));
 
-  EXPECT_EQ(1001, HT.lookup(*S2, 1));
-  EXPECT_EQ(1002, HT.lookup(*S2, 2));
-  EXPECT_EQ(2003, HT.lookup(*S2, 3));
-  EXPECT_EQ(2004, HT.lookup(*S2, 4));
+  EXPECT_EQ(1001U, HT.lookup(*S2, 1));
+  EXPECT_EQ(1002U, HT.lookup(*S2, 2));
+  EXPECT_EQ(2003U, HT.lookup(*S2, 3));
+  EXPECT_EQ(2004U, HT.lookup(*S2, 4));
 
-  EXPECT_EQ(3001, HT.lookup(*S3, 1));
-  EXPECT_EQ(1002, HT.lookup(*S3, 2));
-  EXPECT_EQ(0,    HT.lookup(*S3, 3));
-  EXPECT_EQ(3004, HT.lookup(*S3, 4));
+  EXPECT_EQ(3001U, HT.lookup(*S3, 1));
+  EXPECT_EQ(1002U, HT.lookup(*S3, 2));
+  EXPECT_EQ(0U,    HT.lookup(*S3, 3));
+  EXPECT_EQ(3004U, HT.lookup(*S3, 4));
 
   EXPECT_EQ(true,  HT.count(*S3, 1));
   EXPECT_EQ(true,  HT.count(*S3, 2));
@@ -69,28 +69,28 @@ TEST(TreeScopedHashTableTest, T2) {
 
   delete S2;
 
-  EXPECT_EQ(1001, HT.lookup(*S1, 1));
-  EXPECT_EQ(1002, HT.lookup(*S1, 2));
-  EXPECT_EQ(0,    HT.lookup(*S1, 3));
-  EXPECT_EQ(0,    HT.lookup(*S1, 4));
+  EXPECT_EQ(1001U, HT.lookup(*S1, 1));
+  EXPECT_EQ(1002U, HT.lookup(*S1, 2));
+  EXPECT_EQ(0U,    HT.lookup(*S1, 3));
+  EXPECT_EQ(0U,    HT.lookup(*S1, 4));
 
-  EXPECT_EQ(3001, HT.lookup(*S3, 1));
-  EXPECT_EQ(1002, HT.lookup(*S3, 2));
-  EXPECT_EQ(0,    HT.lookup(*S3, 3));
-  EXPECT_EQ(3004, HT.lookup(*S3, 4));
+  EXPECT_EQ(3001U, HT.lookup(*S3, 1));
+  EXPECT_EQ(1002U, HT.lookup(*S3, 2));
+  EXPECT_EQ(0U,    HT.lookup(*S3, 3));
+  EXPECT_EQ(3004U, HT.lookup(*S3, 4));
 
   delete S3;
 
-  EXPECT_EQ(1001, HT.lookup(*S1, 1));
-  EXPECT_EQ(1002, HT.lookup(*S1, 2));
-  EXPECT_EQ(0,    HT.lookup(*S1, 3));
-  EXPECT_EQ(0,    HT.lookup(*S1, 4));
+  EXPECT_EQ(1001U, HT.lookup(*S1, 1));
+  EXPECT_EQ(1002U, HT.lookup(*S1, 2));
+  EXPECT_EQ(0U,    HT.lookup(*S1, 3));
+  EXPECT_EQ(0U,    HT.lookup(*S1, 4));
 
   delete S1;
 }
 
 TEST(TreeScopedHashTableTest, IteratorTest) {
-  using HashtableTy = TreeScopedHashTable<unsigned, unsigned>;
+  using HashtableTy = TreeScopedHashTable<unsigned, int>;
   using ScopeTy = HashtableTy::ScopeTy;
   HashtableTy HT;
 
