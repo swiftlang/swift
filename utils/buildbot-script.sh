f65aed2ccf64fb6ff393ebbd16763b2365f0b84d
Dmitri Hrybenko <dgribenko@apple.com>
2013-07-10 21:44:24 +0000

    Add a %swift-ide-test variable to tests and ensure that it is set on the buildbot
    Swift SVN r6130

--- utils/buildbot-script.sh
+++ utils/buildbot-script.sh
@@ -110,6 +110,7 @@ fi
 # Run the Swift tests.
 if [ \! "$SKIP_TEST_SWIFT" ]; then
   export SWIFT="$WORKSPACE/swift/build/bin/swift"
+  export SWIFT_IDE_TEST="$WORKSPACE/swift/build/bin/swift-ide-test"
   echo "--- Running Swift Tests ---"
   (cd "$WORKSPACE/swift/build" &&
     "$WORKSPACE/llvm/build/bin/llvm-lit" -sv test) || exit 1
