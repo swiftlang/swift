0a6d9df17821f70362077fc45df0bdaf6ec58a5f
Jordan Rose <jordan_rose@apple.com>
2013-07-16 23:11:03 +0000

    Update for LLVM change to raw_fd_ostream's option flags.
    Swift SVN r6305

--- lib/IRGen/IRGen.cpp
+++ lib/IRGen/IRGen.cpp
@@ -187,9 +187,9 @@ void swift::performIRGeneration(Options &Opts, llvm::Module *Module,
   if (!Opts.OutputFilename.empty()) {
     // Try to open the output file.  Clobbering an existing file is fine.
     // Open in binary mode if we're doing binary output.
-    llvm::sys::fs::OpenFlags OSFlags = llvm::sys::fs::F_None;
+    unsigned OSFlags = 0;
     if (isBinaryOutput(Opts.OutputKind))
-      OSFlags |= llvm::sys::fs::F_Binary;
+      OSFlags |= raw_fd_ostream::F_Binary;
     RawOS.reset(new raw_fd_ostream(Opts.OutputFilename.c_str(),
                                    Error, OSFlags));
     if (RawOS->has_error() || !Error.empty()) {
