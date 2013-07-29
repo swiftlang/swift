81a5a41944a95e33d126ab12371acb814bcee327
Doug Gregor <dgregor@apple.com>
2013-07-29 23:50:18 +0000

    Move test for inheritance.
    Swift SVN r6726

--- test/decl/inherit/inherit.swift
+++ test/decl/inherit/inherit.swift
@@ -1,12 +1,19 @@
 // RUN: %swift -parse %s -verify
 
 class A { }
+protocol P { }
 
 // Duplicate inheritance
-class B : A, A { } // expected-error{{duplicate inheritance from class 'A'}}{{12-15=}}
+class B : A, A { } // expected-error{{duplicate inheritance from 'A'}}{{12-15=}}
+
+// Duplicate inheritance from protocol
+class B2 : P, P { } // expected-error{{duplicate inheritance from 'P'}}{{13-16=}}
 
 // Multiple inheritance
 class C : B, A { } // expected-error{{multiple inheritance from classes 'B' and 'A'}}
+
+// Superclass in the wrong position
+class D : P, A { } // expected-error{{superclass 'A' must appear first in the inheritance clause}}{{12-15=}}{{11-11=A, }}
 
 // Struct inheriting a class
 struct S : A { } // expected-error{{non-class type 'S' cannot inherit from class 'A'}}
@@ -16,4 +23,3 @@ protocol Q : A { } // expected-error{{non-class type 'Q' cannot inherit from cla
 
 // Extension inheriting a class
 extension C : A { } // expected-error{{extension of type 'C' cannot inherit from class 'A'}}
-
