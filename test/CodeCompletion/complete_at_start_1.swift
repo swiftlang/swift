334c90dda5ceee338e05d0c252b0d290f87b1c00
Dmitri Hrybenko <dgribenko@apple.com>
2013-07-30 18:25:40 +0000

    Add a disabled failing testcase for rdar://14585108
    Swift SVN r6747

---
+++ test/CodeCompletion/complete_at_start_1.swift
@@ -0,0 +1,20 @@
+#^A^#
+
+// Make sure we can code complete at the beginning of the file.
+// Disabled per rdar://14585108
+// FIXME: %swift-ide-test -code-completion -source-filename %s -code-completion-token=A | FileCheck %s -check-prefix=A
+// Run it anyway, to ensure that we at least don't crash.
+// RUN: %swift-ide-test -code-completion -source-filename %s -code-completion-token=A
+// A: Begin completions
+// A-DAG: SwiftDecl: true[#Bool#]{{$}}
+// A-DAG: SwiftDecl: false[#Bool#]{{$}}
+// A-DAG: SwiftDecl: Int8[#Int8.metatype#]{{$}}
+// A-DAG: SwiftDecl: Int16[#Int16.metatype#]{{$}}
+// A-DAG: SwiftDecl: Int32[#Int32.metatype#]{{$}}
+// A-DAG: SwiftDecl: Int64[#Int64.metatype#]{{$}}
+// A-DAG: SwiftDecl: Int128[#Int128.metatype#]{{$}}
+// A-DAG: SwiftDecl: Bool[#Bool.metatype#]{{$}}
+// A: End completions
+
+// This function just adds more non-comment tokens to ensure that the file is not empty.
+func foo() {}
