e6d197c8f19c21b117e0359b76549bd75c09a975
Chris Lattner <clattner@apple.com>
2013-08-05 14:33:50 +0000

    SILParser support for dealloc_ref.
    Swift SVN r6887

--- lib/Parse/ParseSIL.cpp
+++ lib/Parse/ParseSIL.cpp
@@ -796,6 +796,7 @@ bool SILParser::parseSILOpcode(ValueKind &Opcode, SourceLoc &OpcodeLoc,
     .Case("convert_cc", ValueKind::ConvertCCInst)
     .Case("convert_function", ValueKind::ConvertFunctionInst)
     .Case("copy_addr", ValueKind::CopyAddrInst)
+    .Case("dealloc_ref", ValueKind::DeallocRefInst)
     .Case("dealloc_stack", ValueKind::DeallocStackInst)
     .Case("destroy_addr", ValueKind::DestroyAddrInst)
     .Case("downcast", ValueKind::DowncastInst)
@@ -1269,6 +1270,13 @@ bool SILParser::parseSILInstruction(SILBasicBlock *BB) {
       assert(Opcode == ValueKind::AllocRefInst);
       ResultVal = B.createAllocRef(SILLocation(), Ty);
     }
+    break;
+  }
+  case ValueKind::DeallocRefInst: {
+    if (parseTypedValueRef(Val))
+      return true;
+
+    ResultVal = B.createDeallocRef(SILLocation(), Val);
     break;
   }
   case ValueKind::DeallocStackInst: {
