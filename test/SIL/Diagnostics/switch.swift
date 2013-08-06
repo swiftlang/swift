7a042cf44f5ba5813446a0ad40faf03ac9ad9258
Anna Zaks <ganna@apple.com>
2013-08-06 01:13:48 +0000

    [SIL] Issue a diagnostic on a non-exhaustive switch
    The issue is truly diagnosed in SILGen and an unreachable instruction is
    inserted in cases where the switch is not fully covered. However, we issue
    the diagnostic later on in DataflowDiagnostics pass.
    
    John has suggested that it's best to diagnose this after DCE because
    we should not warn in cases where the switch is not fully covered,
    but we can show that the non-covered cases are not reachable.
    
    Swift SVN r6929

---
+++ test/SIL/Diagnostics/switch.swift
@@ -0,0 +1,17 @@
+// RUN: %swift %s -emit-sil -verify
+
+func non_fully_covered_switch(x : Int) -> Int {
+  switch x {
+    case 0:
+     x++
+    case 3:
+     x--
+  } // expected-error{{switch must be exhaustive}}
+  return x;
+}
+
+func non_fully_covered_switch_no_case_clauses(x : Int) -> Int {
+  switch x {
+  } // expected-error{{switch must be exhaustive}}
+  return x;
+}
