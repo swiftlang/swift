939badfe02011894fdf3f8f6bb4a28c3f1c04553
Anna Zaks <ganna@apple.com>
2013-08-15 01:19:57 +0000

    [SIL] Move recursivelyDeleteTriviallyDeadInstructions so that it could be used by other passes.
    Swift SVN r7250

--- lib/SILPasses/DeadCodeElimination.cpp
+++ lib/SILPasses/DeadCodeElimination.cpp
@@ -12,64 +12,13 @@
 #define DEBUG_TYPE "dead-code-elimination"
 #include "swift/Subsystems.h"
 #include "swift/SIL/SILBuilder.h"
+#include "swift/SILPasses/Utils/Local.h"
 #include "llvm/ADT/Statistic.h"
 #include "llvm/Support/Debug.h"
 using namespace swift;
 
 STATISTIC(NumBlocksRemoved, "Number of unreachable basic blocks removed");
 STATISTIC(NumInstructionsRemoved, "Number of unreachable instructions removed");
-
-/// \brief Perform a fast local check to see if the instruction is dead.
-///
-/// This rutine only examines the state of the instruction at hand.
-static bool isInstructionTriviallyDead(SILInstruction *I) {
-  if (!I->use_empty() || isa<TermInst>(I))
-    return false;
-
-  if (!I->mayHaveSideEffects())
-    return true;
-
-  return false;
-}
-
-/// \brief If the given instruction is dead, delete it along with its dead
-/// operands.
-///
-/// \return Returns true if any instructions were deleted.
-static bool recursivelyDeleteTriviallyDeadInstructions(SILInstruction *I) {
-  // If the instruction is not dead, there is nothing to do.
-  if (!I || !isInstructionTriviallyDead(I))
-    return false;
-
-  // Delete this instruction and others that become dead after it's deleted.
-  SmallVector<SILInstruction*, 16> DeadInsts;
-  DeadInsts.push_back(I);
-  do {
-    I = DeadInsts.pop_back_val();
-
-    // Check if any of the operands will become dead as well.
-    MutableArrayRef<Operand> Ops = I->getAllOperands();
-    for (auto OpI = Ops.begin(), OpE = Ops.end(); OpI != OpE; ++OpI) {
-      Operand &Op = *OpI;
-      SILValue OpVal = Op.get();
-
-      // Remove the reference from the instruction being deleted to this
-      // operand.
-      Op.drop();
-
-      // If the operand is an instruction that is only used by the instruction
-      // being deleted, delete it.
-      if (SILInstruction *OpValInst = dyn_cast<SILInstruction>(OpVal))
-        if (isInstructionTriviallyDead(OpValInst))
-          DeadInsts.push_back(OpValInst);
-    }
-
-    // This will remove this instruction and all its uses.
-    I->eraseFromParent();
-  } while (!DeadInsts.empty());
-  
-  return true;
-}
 
 /// \brief Deletes the instrcutions in the set and any instructions that could
 /// become dead after their removal.
