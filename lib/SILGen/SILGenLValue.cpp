a26584584ef1f78fbc74bc3f40762d5b6becc745
John McCall <rjmccall@apple.com>
2013-09-05 06:44:46 +0000

    Move some basic l-values and address operations into SILGenLValue.cpp.
    Swift SVN r7940

--- lib/SILGen/SILGenLValue.cpp
+++ lib/SILGen/SILGenLValue.cpp
@@ -9,6 +9,11 @@
 // See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
 //
 //===----------------------------------------------------------------------===//
+//
+// Emission of l-value expressions and basic operations on them.
+//
+//===----------------------------------------------------------------------===//
+
 
 #include "SILGen.h"
 #include "swift/AST/AST.h"
@@ -500,3 +505,160 @@ LValue SILGenLValue::visitRequalifyExpr(RequalifyExpr *e) {
          "non-lvalue requalify in lvalue expression");
   return visitRec(e->getSubExpr());
 }
+
+/// Load an r-value out of the given address.
+///
+/// \param rvalueTL - the type lowering for the type-of-rvalue
+///   of the address
+ManagedValue SILGenFunction::emitLoad(SILLocation loc,
+                                      SILValue addr,
+                                      const TypeLowering &rvalueTL,
+                                      SGFContext C,
+                                      IsTake_t isTake) {
+  // Get the lowering for the address type.  We can avoid a re-lookup
+  // in the very common case of this being equivalent to the r-value
+  // type.
+  auto &addrTL =
+    (addr.getType() == rvalueTL.getLoweredType().getAddressType()
+       ? rvalueTL : getTypeLowering(addr.getType()));
+
+  if (rvalueTL.isAddressOnly()) {
+    // Copy the address-only value.
+    SILValue copy = getBufferForExprResult(loc, rvalueTL.getLoweredType(), C);
+    emitSemanticLoadInto(loc, addr, addrTL, copy, rvalueTL,
+                         isTake, IsInitialization);
+    return emitManagedRValueWithCleanup(copy, rvalueTL);
+  }
+  
+  // Load the loadable value, and retain it if we aren't taking it.
+  SILValue loadedV = emitSemanticLoad(loc, addr, addrTL, rvalueTL, isTake);
+  return emitManagedRValueWithCleanup(loadedV, rvalueTL);
+}
+
+/// Load a r-value out of the given address, given that the type is
+/// loadable.
+SILValue SILGenFunction::emitSemanticLoad(SILLocation loc,
+                                          SILValue src,
+                                          const TypeLowering &srcTL,
+                                          const TypeLowering &rvalueTL,
+                                          IsTake_t isTake) {
+  assert(srcTL.getLoweredType().getAddressType() == src.getType());
+  assert(rvalueTL.isLoadable());
+  assert(srcTL.getSemanticType() == rvalueTL.getLoweredType());
+  return srcTL.emitSemanticLoad(B, loc, src, isTake);
+}
+
+/// Load a r-value out of the given address, given that the type is
+/// loadable.
+void SILGenFunction::emitSemanticLoadInto(SILLocation loc,
+                                          SILValue src,
+                                          const TypeLowering &srcTL,
+                                          SILValue dest,
+                                          const TypeLowering &destTL,
+                                          IsTake_t isTake,
+                                          IsInitialization_t isInit) {
+  assert(srcTL.getLoweredType().getAddressType() == src.getType());
+  assert(destTL.getLoweredType().getAddressType() == dest.getType());
+  assert(srcTL.getSemanticType() == destTL.getLoweredType());
+  srcTL.emitSemanticLoadInto(B, loc, src, dest, isTake, isInit);
+}
+
+/// Produce a physical address that corresponds to the given l-value
+/// component.
+static SILValue drillIntoComponent(SILGenFunction &SGF,
+                                   SILLocation loc,
+                                   const PathComponent &component,
+                                   SILValue base) {
+  assert(!base ||
+         base.getType().isAddress() ||
+         base.getType().hasReferenceSemantics());
+
+  SILValue addr;
+  if (component.isPhysical()) {
+    addr = component.asPhysical().offset(SGF, loc, base);
+  } else {
+    auto &lcomponent = component.asLogical();
+    Materialize temporary = lcomponent.getMaterialized(SGF, loc, base);
+    addr = temporary.address;
+  }
+
+  assert(addr.getType().isAddress() ||
+         addr.getType().hasReferenceSemantics());
+  return addr;
+}
+
+/// Find the last component of the given lvalue and derive a base
+/// location for it.
+static const PathComponent &drillToLastComponent(SILGenFunction &SGF,
+                                                 SILLocation loc,
+                                                 const LValue &lv,
+                                                 SILValue &addr) {
+  assert(lv.begin() != lv.end() &&
+         "lvalue must have at least one component");
+
+  auto component = lv.begin(), next = lv.begin(), end = lv.end();
+  ++next;
+  for (; next != end; component = next, ++next) {
+    addr = drillIntoComponent(SGF, loc, *component, addr);
+  }
+
+  return *component;
+}
+
+ManagedValue SILGenFunction::emitLoadOfLValue(SILLocation loc,
+                                              const LValue &src,
+                                              SGFContext C) {
+  // No need to write back to a loaded lvalue.
+  DisableWritebackScope scope(*this);
+
+  SILValue addr;
+  auto &component = drillToLastComponent(*this, loc, src, addr);
+
+  // If the last component is physical, just drill down and load from it.
+  if (component.isPhysical()) {
+    addr = component.asPhysical().offset(*this, loc, addr);
+    return emitLoad(loc, addr, getTypeLowering(src.getTypeOfRValue()),
+                    C, IsNotTake);
+  }
+
+  // If the last component is logical, just emit a get.
+  return component.asLogical().get(*this, loc, addr, C);
+}
+
+ManagedValue SILGenFunction::emitAddressOfLValue(SILLocation loc,
+                                                 const LValue &src) {
+  SILValue addr;
+  
+  assert(src.begin() != src.end() && "lvalue must have at least one component");
+  for (auto &component : src) {
+    addr = drillIntoComponent(*this, loc, component, addr);
+  }
+  assert(addr.getType().isAddress() &&
+         "resolving lvalue did not give an address");
+  return ManagedValue(addr, ManagedValue::LValue);
+}
+
+void SILGenFunction::emitAssignToLValue(SILLocation loc,
+                                        RValue &&src, const LValue &dest) {
+  WritebackScope scope(*this);
+  
+  // Resolve all components up to the last, keeping track of value-type logical
+  // properties we need to write back to.
+  SILValue destAddr;
+  auto &component = drillToLastComponent(*this, loc, dest, destAddr);
+  
+  // Write to the tail component.
+  if (component.isPhysical()) {
+    SILValue finalDestAddr
+      = component.asPhysical().offset(*this, loc, destAddr);
+    
+    std::move(src).getAsSingleValue(*this, loc)
+      .assignInto(*this, loc, finalDestAddr);
+  } else {
+    component.asLogical().set(*this, loc, std::move(src), destAddr);
+  }
+
+  // The writeback scope closing will propagate the value back up through the
+  // writeback chain.
+}
+
