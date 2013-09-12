4a535d44604975d43ad02d2f8909612db073f4f9
Stephen Lin <swlin@apple.com>
2013-09-12 00:06:53 +0000

    Mandatory inlining: clean up alloc_box, partial_apply, etc. after inlining an [auto_closure] argument, in simple cases.
    Swift SVN r8127

--- lib/SILPasses/MandatoryInlining.cpp
+++ lib/SILPasses/MandatoryInlining.cpp
@@ -76,6 +76,127 @@ fixupReferenceCounts(SILBuilder &B, SILBasicBlock::iterator I, SILLocation Loc,
   }
 }
 
+/// \brief Removes instructions that create the callee value if they are no
+/// longer necessary after inlining.
+static void
+cleanupCalleeValue(SILBuilder &B, SILBasicBlock::iterator &I,
+                   SILValue CalleeValue,
+                   SmallVectorImpl<SILValue>& CaptureArgs) {
+  auto eraseInstruction = [&](SILInstruction *Inst) {
+    if (static_cast<SILInstruction*>(I) == Inst)
+      ++I;
+    Inst->eraseFromParent();
+  };
+
+  // Handle the case where the callee of the apply is a load instruction
+  if (LoadInst *LI = dyn_cast<LoadInst>(CalleeValue.getDef())) {
+    assert(CalleeValue.getResultNumber() == 0);
+    SILInstruction *ABI = dyn_cast<AllocBoxInst>(LI->getOperand().getDef());
+    assert(ABI && LI->getOperand().getResultNumber() == 1);
+
+    // The load instruction must have no more uses left to erase it
+    if (!LI->use_empty())
+      return;
+    eraseInstruction(LI);
+
+    // Look through uses of the alloc box the load is loading from to find up to
+    // one store and up to one strong release
+    StoreInst *SI = nullptr;
+    StrongReleaseInst *SRI = nullptr;
+    for (auto UI = ABI->use_begin(), UE = ABI->use_end(); UI != UE; ++UI) {
+      if (SI == nullptr && isa<StoreInst>(UI.getUser())) {
+        SI = cast<StoreInst>(UI.getUser());
+        assert(SI->getDest() == SILValue(ABI, 1));
+      } else if (SRI == nullptr && isa<StrongReleaseInst>(UI.getUser())) {
+        SRI = cast<StrongReleaseInst>(UI.getUser());
+        assert(SRI->getOperand() == SILValue(ABI, 0));
+      } else
+        return;
+    }
+
+    // If we found a store, record its source and erase it
+    if (SI) {
+      CalleeValue = SI->getSrc();
+      eraseInstruction(SI);
+    } else {
+      CalleeValue = SILValue();
+    }
+
+    // If we found a strong release, replace it with a strong release of the
+    // source of the store and erase it
+    if (SRI) {
+      if (CalleeValue.isValid()) {
+        B.setInsertionPoint(SRI);
+        B.createStrongRelease(SRI->getLoc(), CalleeValue);
+      }
+      eraseInstruction(SRI);
+    }
+
+    eraseInstruction(ABI);
+  }
+
+  if (PartialApplyInst *PAI =
+        dyn_cast<PartialApplyInst>(CalleeValue.getDef())) {
+    assert(CalleeValue.getResultNumber() == 0);
+
+    // Look through remaining uses of the partial apply inst to find at most one
+    // strong release instruction
+    StrongReleaseInst *SRI = nullptr;
+    for (auto UI = PAI->use_begin(), UE = PAI->use_end(); UI != UE; ++UI) {
+      if (SRI == nullptr && isa<StrongReleaseInst>(UI.getUser())) {
+        SRI = cast<StrongReleaseInst>(UI.getUser());
+        assert(SRI->getOperand() == SILValue(PAI, 0));
+      } else
+        return;
+    }
+
+    // If there is a strong release of the partial apply, then replace it with
+    // releases of the captured arguments
+    if (SRI) {
+      B.setInsertionPoint(SRI);
+      SILModule &M = B.getFunction().getModule();
+      for (auto &CaptureArg : CaptureArgs) {
+        if (!CaptureArg.getType().isAddress())
+          M.getTypeLowering(CaptureArg.getType()).emitRelease(B, SRI->getLoc(),
+                                                              CaptureArg);
+      }
+      eraseInstruction(SRI);
+    }
+
+    CalleeValue = PAI->getCallee();
+    assert(PAI->use_empty());
+    eraseInstruction(PAI);
+  } else if (ThinToThickFunctionInst *TTTFI =
+               dyn_cast<ThinToThickFunctionInst>(CalleeValue.getDef())) {
+    assert(CalleeValue.getResultNumber() == 0);
+
+    // Look through remaining uses of the thin-to-thick inst to find at most one
+    // strong release instruction
+    StrongReleaseInst *SRI = nullptr;
+    for (auto UI = TTTFI->use_begin(), UE = TTTFI->use_end(); UI != UE; ++UI) {
+      if (SRI == nullptr && isa<StrongReleaseInst>(UI.getUser())) {
+        SRI = cast<StrongReleaseInst>(UI.getUser());
+        assert(SRI->getOperand() == SILValue(TTTFI, 0));
+      } else
+        return;
+    }
+
+    // If there is a strong release of the thin-to-thick function, erase it
+    if (SRI)
+      eraseInstruction(SRI);
+
+    CalleeValue = TTTFI->getOperand();
+    assert(TTTFI->use_empty());
+    eraseInstruction(TTTFI);
+  }
+
+  if (FunctionRefInst *FRI = cast<FunctionRefInst>(CalleeValue)) {
+    assert(CalleeValue.getResultNumber() == 0);
+    if (FRI->use_empty())
+      eraseInstruction(FRI);
+  }
+}
+
 /// \brief Returns the callee SILFunction called at a call site, in the case
 /// that the call is transparent (as in, both that the call is marked
 /// with the transparent flag and that callee function is actually transparently
@@ -83,55 +204,50 @@ fixupReferenceCounts(SILBuilder &B, SILBasicBlock::iterator I, SILLocation Loc,
 /// is already in SSA form.
 ///
 /// In the case that a non-null value is returned, FullArgs contains effective
-/// argument operands for the callee function and PossiblyDeadInsts contains
-/// instructions that might be dead after inlining and removal of any dead
-/// instructions earlier in the vector.
+/// argument operands for the callee function.
 static SILFunction *
 getCalleeFunction(ApplyInst* AI, bool &IsThick,
                   SmallVectorImpl<SILValue>& CaptureArgs,
-                  SmallVectorImpl<SILValue>& FullArgs,
-                  SmallVectorImpl<SILInstruction*>& PossiblyDeadInsts) {
+                  SmallVectorImpl<SILValue>& FullArgs) {
   if (!AI->isTransparent())
     return nullptr;
 
   IsThick = false;
   CaptureArgs.clear();
   FullArgs.clear();
-  PossiblyDeadInsts.clear();
 
   for (const auto &Arg : AI->getArguments())
     FullArgs.push_back(Arg);
-  SILValue Callee = AI->getCallee();
+  SILValue CalleeValue = AI->getCallee();
 
-  if (LoadInst *LI = dyn_cast<LoadInst>(Callee.getDef())) {
-    assert(Callee.getResultNumber() == 0);
+  if (LoadInst *LI = dyn_cast<LoadInst>(CalleeValue.getDef())) {
+    assert(CalleeValue.getResultNumber() == 0);
     // Conservatively only see through alloc_box; we assume this pass is run
     // immediately after SILGen
-    SILInstruction *ABSI = dyn_cast<SILInstruction>(LI->getOperand().getDef());
-    if (!ABSI || !(isa<AllocBoxInst>(ABSI) || isa<AllocStackInst>(ABSI)))
+    SILInstruction *ABI = dyn_cast<AllocBoxInst>(LI->getOperand().getDef());
+    if (!ABI)
       return nullptr;
     assert(LI->getOperand().getResultNumber() == 1);
 
     // Scan forward from the alloc box to find the first store, which
     // (conservatively) must be in the same basic block as the alloc box
     StoreInst *SI = nullptr;
-    for (auto I = SILBasicBlock::iterator(ABSI), E = I->getParent()->end();
+    for (auto I = SILBasicBlock::iterator(ABI), E = I->getParent()->end();
          I != E; ++I) {
       // If we find the load instruction first, then the load is loading from
       // a non-initialized alloc; this shouldn't really happen but I'm not
       // making any assumptions
       if (static_cast<SILInstruction*>(I) == LI)
         return nullptr;
-      if ((SI = dyn_cast<StoreInst>(I)) && SI->getDest().getDef() == ABSI) {
+      if ((SI = dyn_cast<StoreInst>(I)) && SI->getDest().getDef() == ABI) {
         // We found a store that we know dominates the load; now ensure there
         // are no other uses of the alloc other than loads, retains, releases
         // and dealloc stacks
-        for (auto UI = ABSI->use_begin(), UE = ABSI->use_end(); UI != UE;
+        for (auto UI = ABI->use_begin(), UE = ABI->use_end(); UI != UE;
              ++UI)
           if (UI.getUser() != SI && !isa<LoadInst>(UI.getUser()) &&
               !isa<StrongRetainInst>(UI.getUser()) &&
-              !isa<StrongReleaseInst>(UI.getUser()) &&
-              !isa<DeallocStackInst>(UI.getUser()))
+              !isa<StrongReleaseInst>(UI.getUser()))
             return nullptr;
         // We can conservatively see through the store
         break;
@@ -139,42 +255,40 @@ getCalleeFunction(ApplyInst* AI, bool &IsThick,
     }
     if (!SI)
       return nullptr;
-    Callee = SI->getSrc();
+    CalleeValue = SI->getSrc();
   }
 
   // We are allowed to see through exactly one "partial apply" instruction or
   // one "thin to thick function" instructions, since those are the patterns
   // generated when using auto closures
-  if (PartialApplyInst *PAI = dyn_cast<PartialApplyInst>(Callee.getDef())) {
-    assert(Callee.getResultNumber() == 0);
+  if (PartialApplyInst *PAI =
+        dyn_cast<PartialApplyInst>(CalleeValue.getDef())) {
+    assert(CalleeValue.getResultNumber() == 0);
 
     for (const auto &Arg : PAI->getArguments()) {
       CaptureArgs.push_back(Arg);
       FullArgs.push_back(Arg);
     }
 
-    Callee = PAI->getCallee();
+    CalleeValue = PAI->getCallee();
     IsThick = true;
-    PossiblyDeadInsts.push_back(PAI);
   } else if (ThinToThickFunctionInst *TTTFI =
-               dyn_cast<ThinToThickFunctionInst>(Callee.getDef())) {
-    assert(Callee.getResultNumber() == 0);
-    Callee = TTTFI->getOperand();
+               dyn_cast<ThinToThickFunctionInst>(CalleeValue.getDef())) {
+    assert(CalleeValue.getResultNumber() == 0);
+    CalleeValue = TTTFI->getOperand();
     IsThick = true;
-    PossiblyDeadInsts.push_back(TTTFI);
   }
 
-  FunctionRefInst *FRI = dyn_cast<FunctionRefInst>(Callee.getDef());
+  FunctionRefInst *FRI = dyn_cast<FunctionRefInst>(CalleeValue.getDef());
   if (!FRI)
     return nullptr;
-  assert(Callee.getResultNumber() == 0);
+  assert(CalleeValue.getResultNumber() == 0);
 
   SILFunction *CalleeFunction = FRI->getFunction();
   if (!CalleeFunction || CalleeFunction->empty() ||
       (CalleeFunction->getAbstractCC() != AbstractCC::Freestanding &&
        CalleeFunction->getAbstractCC() != AbstractCC::Method))
     return nullptr;
-  PossiblyDeadInsts.push_back(FRI);
   return CalleeFunction;
 }
 
@@ -218,7 +332,6 @@ runOnFunctionRecursively(SILFunction *F, ApplyInst* AI,
 
   SmallVector<SILValue, 16> CaptureArgs;
   SmallVector<SILValue, 32> FullArgs;
-  SmallVector<SILInstruction*, 16> PossiblyDeadInsts;
   SILInliner Inliner(*F);
   SILBuilder Builder(*F);
   for (auto FI = F->begin(), FE = F->end(); FI != FE; ++FI) {
@@ -234,8 +347,7 @@ runOnFunctionRecursively(SILFunction *F, ApplyInst* AI,
       SILValue CalleeValue = InnerAI->getCallee();
       bool IsThick;
       SILFunction *CalleeFunction = getCalleeFunction(InnerAI, IsThick,
-                                                      CaptureArgs, FullArgs,
-                                                      PossiblyDeadInsts);
+                                                      CaptureArgs, FullArgs);
       if (!CalleeFunction) {
         ++I;
         continue;
@@ -272,16 +384,12 @@ runOnFunctionRecursively(SILFunction *F, ApplyInst* AI,
       }
 
       fixupReferenceCounts(Builder, I, Loc, CalleeValue, IsThick, CaptureArgs);
+      cleanupCalleeValue(Builder, I, CalleeValue, CaptureArgs);
 
       // Reposition iterators possibly invalidated by mutation
       FI = SILFunction::iterator(I->getParent());
       FE = F->end();
       E = FI->end();
-
-      for (SILInstruction *PossiblyDeadInst : PossiblyDeadInsts)
-        if (PossiblyDeadInst->use_empty())
-          PossiblyDeadInst->eraseFromParent();
-
       ++NumMandatoryInlines;
     }
   }
