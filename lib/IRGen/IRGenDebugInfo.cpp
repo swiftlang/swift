65f05625e73c0e90862b77af209d332674edb082
Adrian Prantl <aprantl@apple.com>
2013-09-14 00:19:09 +0000

    Debug info: emit typedef types in the appropriate file scope.
    Swift SVN r8233

--- lib/IRGen/IRGenDebugInfo.cpp
+++ lib/IRGen/IRGenDebugInfo.cpp
@@ -1164,8 +1164,9 @@ llvm::DIType IRGenDebugInfo::createType(DebugTypeInfo DbgTy,
         ? Decl->getUnderlyingType()
         : NameAliasTy->getDesugaredType();
       auto DTI = DebugTypeInfo(AliasedTy, DbgTy.size, DbgTy.align);
-      return DBuilder.createTypedef(getOrCreateType(DTI, Scope), Name,
-                                    getOrCreateFile(L.Filename), L.Line, Scope);
+      auto File = getOrCreateFile(L.Filename);
+      return DBuilder.createTypedef(getOrCreateType(DTI, File), Name,
+                                    File, L.Line, File);
     }
     DEBUG(llvm::dbgs() << "Name alias without Decl: ";
           DbgTy.getType()->dump(); llvm::dbgs() << "\n");
