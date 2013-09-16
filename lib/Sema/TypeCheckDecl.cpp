411ce4d69d696bcbaa2a1cefcc36375643cd3425
Chris Lattner <clattner@apple.com>
2013-09-16 14:33:44 +0000

    remove dead code.
    Swift SVN r8274

--- lib/Sema/TypeCheckDecl.cpp
+++ lib/Sema/TypeCheckDecl.cpp
@@ -25,12 +25,6 @@
 #include "llvm/ADT/Twine.h"
 using namespace swift;
 
-// FIXME: This is gross but temporary.
-#include "llvm/Support/CommandLine.h"
-llvm::cl::opt<bool> TLDefiniteInit("enable-top-level-definite-init",
-                                   llvm::cl::Hidden, llvm::cl::init(true));
-
-
 /// \brief Describes the kind of implicit constructor that will be
 /// generated.
 enum class ImplicitConstructorKind {
