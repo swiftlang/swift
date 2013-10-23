49a5f53e19713b899123bf008093b3de462a691b
Argyrios Kyrtzidis <kyrtzidis@apple.com>
2013-10-23 00:41:55 +0000

    [libBasic] Introduce a caching mechanism, that evicts its entries when there is memory pressure.
    The underlying mechanism uses darwin's libcache library (if not building on darwin there is a default implementation).
    
    This was originally introduced in SourceKit, but we are moving it to libswiftBasic so it can be utilized by libswiftIDE
    for caching code-completion results.
    
    Swift SVN r9610

---
+++ include/swift/Basic/Cache.h
@@ -0,0 +1,232 @@
+//===--- Cache.h - Caching mechanism interface -------------------*- C++ -*-==//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2015 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef SWIFT_BASIC_CACHE_H
+#define SWIFT_BASIC_CACHE_H
+
+#include "llvm/ADT/DenseMapInfo.h"
+#include "llvm/ADT/IntrusiveRefCntPtr.h"
+#include "llvm/ADT/StringRef.h"
+#include "llvm/ADT/Optional.h"
+
+namespace swift {
+namespace sys {
+
+template <typename T>
+struct CacheTypeMgmtInfo {
+  static void *enterCache(const T &Val) { return new T(Val); }
+  static void exitCache(void *Ptr) { delete static_cast<T*>(Ptr); }
+  static const T &getFromCache(void *Ptr) { return *static_cast<T*>(Ptr); }
+};
+
+template <typename T>
+struct CacheKeyHashInfo {
+  static uintptr_t getHashValue(const T &Val) {
+    return llvm::DenseMapInfo<T>::getHashValue(Val);
+  }
+  static bool isEqual(const T &LHS, const T &RHS) {
+    return llvm::DenseMapInfo<T>::isEqual(LHS, RHS);
+  }
+};
+
+template <typename T>
+struct CacheValueCostInfo {
+  static size_t getCost(const T &Val) { return sizeof(Val); }
+};
+
+template <typename T>
+struct CacheKeyInfo : public CacheKeyHashInfo<T>,
+                      public CacheTypeMgmtInfo<T> {
+  static const void *getLookupKey(const T *Val) { return Val; }
+};
+
+template <typename T>
+struct CacheValueInfo : public CacheValueCostInfo<T>,
+                        public CacheTypeMgmtInfo<T> {
+};
+
+/// The underlying implementation of the caching mechanism.
+/// It should be inherently thread-safe.
+class CacheImpl {
+public:
+  typedef void *ImplTy;
+
+  struct CallBacks {
+    void *UserData;
+
+    uintptr_t (*keyHashCB)(void *Key, void *UserData);
+    bool (*keyIsEqualCB)(void *Key1, void *Key2, void *UserData);
+
+    void (*keyDestroyCB)(void *Key, void *UserData);
+    void (*valueDestroyCB)(void *Value, void *UserData);
+  };
+
+protected:
+  CacheImpl() = default;
+
+  ImplTy Impl = nullptr;
+
+  static ImplTy create(llvm::StringRef Name, const CallBacks &CBs);
+
+  /// Sets value for key.
+  ///
+  /// \param Key Key to add.  Must not be nullptr.
+  /// \param Value Value to add. If value is nullptr, key is associated with the
+  /// value nullptr.
+  /// \param Cost Cost of maintaining value in cache.
+  ///
+  /// Sets value for key.  Value is retained until released using
+  /// \c releaseValue().
+  ///
+  /// Replaces previous key and value if present.  Invokes the key destroy
+  /// callback immediately for the previous key.  Invokes the value destroy
+  /// callback once the previous value's retain count is zero.
+  ///
+  /// Cost indicates the relative cost of maintaining value in the cache
+  /// (e.g., size of value in bytes) and may be used by the cache under
+  /// memory pressure to select which cache values to evict.  Zero is a
+  /// valid cost.
+  void setAndRetain(void *Key, void *Value, size_t Cost);
+
+  /// Fetches value for key.
+  ///
+  /// \param Key Key used to lookup value.  Must not be nullptr.
+  /// \param Value_out Value is stored here if found.  Must not be nullptr.
+  /// \returns True if the key was found, false otherwise.
+  ///
+  /// Fetches value for key, retains value, and stores value in value_out.
+  /// Caller should release value using \c releaseValue().
+  bool getAndRetain(const void *Key, void **Value_out);
+
+  /// Releases a previously retained cache value.
+  ///
+  /// \param Value Value to release.  Must not be nullptr.
+  ///
+  /// Releases a previously retained cache value. When the reference count
+  /// reaches zero the cache may destroy the value.
+  void releaseValue(void *Value);
+
+  /// Removes a key and its value.
+  ///
+  /// \param Key Key to remove.  Must not be nullptr.
+  /// \returns True if the key was found, false otherwise.
+  ///
+  /// Removes a key and its value from the cache such that \c getAndRetain()
+  /// will return false.  Invokes the key destroy callback immediately.
+  /// Invokes the value destroy callback once value's retain count is zero.
+  bool remove(const void *Key);
+
+  /// Invokes \c remove on all keys.
+  void removeAll();
+
+  /// Destroys cache.
+  void destroy();
+};
+
+/// Caching mechanism, that is thread-safe and can evict its entries when there
+/// is memory pressure.
+///
+/// This works like a dictionary, you use a key to store and retrieve a value.
+/// The value is copied (during storing or retrieval), but an IntrusiveRefCntPtr
+/// can be used directly as a value.
+///
+/// It is important to provide a proper 'cost' function for the value (via
+/// \c CacheValueCostInfo trait); e.g. the cost for an ASTContext would be the
+/// memory usage of the data structures it owns.
+template <typename KeyT, typename ValueT,
+          typename KeyInfoT = CacheKeyInfo<KeyT>,
+          typename ValueInfoT = CacheValueInfo<ValueT> >
+class Cache : CacheImpl {
+public:
+  explicit Cache(llvm::StringRef Name) {
+    CallBacks CBs = {
+      /*UserData=*/nullptr,
+      keyHash,
+      keyIsEqual,
+      keyDestroy,
+      valueDestroy
+    };
+    Impl = create(Name, CBs);
+  }
+
+  ~Cache() {
+    destroy();
+  }
+
+  void set(const KeyT &Key, const ValueT &Value) {
+    void *CacheKeyPtr = KeyInfoT::enterCache(Key);
+    void *CacheValuePtr = ValueInfoT::enterCache(Value);
+    setAndRetain(CacheKeyPtr, CacheValuePtr, ValueInfoT::getCost(Value));
+    releaseValue(CacheValuePtr);
+  }
+
+  llvm::Optional<ValueT> get(const KeyT &Key) {
+    const void *CacheKeyPtr = KeyInfoT::getLookupKey(&Key);
+    void *CacheValuePtr;
+    bool Found = getAndRetain(CacheKeyPtr, &CacheValuePtr);
+    if (!Found)
+      return llvm::None;
+
+    ValueT Val(ValueInfoT::getFromCache(CacheValuePtr));
+    releaseValue(CacheValuePtr);
+    return std::move(Val);
+  }
+
+  /// \returns True if the key was found, false otherwise.
+  bool remove(const KeyT &Key) {
+    const void *CacheKeyPtr = KeyInfoT::getLookupKey(&Key);
+    return CacheImpl::remove(CacheKeyPtr);
+  }
+
+  void clear() {
+    removeAll();
+  }
+
+private:
+  static uintptr_t keyHash(void *Key, void *UserData) {
+    return KeyInfoT::getHashValue(*static_cast<KeyT*>(Key));
+  }
+  static bool keyIsEqual(void *Key1, void *Key2, void *UserData) {
+    return KeyInfoT::isEqual(*static_cast<KeyT*>(Key1),
+                             *static_cast<KeyT*>(Key2));
+  }
+
+  static void keyDestroy(void *Key, void *UserData) {
+    KeyInfoT::exitCache(Key);
+  }
+  static void valueDestroy(void *Value, void *UserData) {
+    ValueInfoT::exitCache(Value);
+  }
+};
+
+template <typename T>
+struct CacheValueInfo<llvm::IntrusiveRefCntPtr<T>>{
+  static void *enterCache(const llvm::IntrusiveRefCntPtr<T> &Val) {
+    T *Ptr = Val.getPtr();
+    Ptr->Retain();
+    return Ptr;
+  }
+  static void exitCache(void *Ptr) {
+    static_cast<T*>(Ptr)->Release();
+  }
+  static llvm::IntrusiveRefCntPtr<T> getFromCache(void *Ptr) {
+    return static_cast<T*>(Ptr);
+  }
+  static size_t getCost(const llvm::IntrusiveRefCntPtr<T> &Val) {
+    return CacheValueCostInfo<T>::getCost(*Val);
+  }
+};
+
+} // namespace sys
+} // namespace swift
+
+#endif
---
+++ lib/Basic/Cache.cpp
@@ -0,0 +1,142 @@
+//===--- Cache.h - Caching mechanism implementation -----------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2015 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===---------------------------------------------------------------------===//
+
+#if defined(__APPLE__)
+#include "Darwin/Cache-Mac.cpp"
+#else
+
+//  This file implements a default caching implementation that never evicts
+//  its entries.
+
+#include "swift/Basic/Cache.h"
+#include "llvm/ADT/DenseMap.h"
+#include "llvm/Support/Mutex.h"
+
+using namespace swift::sys;
+using llvm::StringRef;
+
+namespace {
+struct DefaultCacheKey {
+  void *Key = nullptr;
+  CacheImpl::CallBacks *CBs = nullptr;
+
+  //DefaultCacheKey() = default;
+  DefaultCacheKey(void *Key, CacheImpl::CallBacks *CBs) : Key(Key), CBs(CBs) {}
+};
+
+struct DefaultCache {
+  llvm::sys::Mutex Mux;
+  CacheImpl::CallBacks CBs;
+  llvm::DenseMap<DefaultCacheKey, void *> Entries;
+
+  explicit DefaultCache(CacheImpl::CallBacks CBs) : CBs(std::move(CBs)) { }
+};
+} // end anonymous namespace
+
+namespace llvm {
+template<> struct DenseMapInfo<DefaultCacheKey> {
+  static inline DefaultCacheKey getEmptyKey() {
+    return { DenseMapInfo<void*>::getEmptyKey(), nullptr };
+  }
+  static inline DefaultCacheKey getTombstoneKey() {
+    return { DenseMapInfo<void*>::getTombstoneKey(), nullptr };
+  }
+  static unsigned getHashValue(const DefaultCacheKey &Val) {
+    uintptr_t Hash = Val.CBs->keyHashCB(Val.Key, nullptr);
+    return DenseMapInfo<uintptr_t>::getHashValue(Hash);
+  }
+  static bool isEqual(const DefaultCacheKey &LHS, const DefaultCacheKey &RHS) {
+    if (LHS.Key == RHS.Key)
+      return true;
+    if (LHS.Key == DenseMapInfo<void*>::getEmptyKey() ||
+        LHS.Key == DenseMapInfo<void*>::getTombstoneKey() ||
+        RHS.Key == DenseMapInfo<void*>::getEmptyKey() ||
+        RHS.Key == DenseMapInfo<void*>::getTombstoneKey())
+      return false;
+    return LHS.CBs->keyIsEqualCB(LHS.Key, RHS.Key, nullptr);
+  }
+};
+}
+
+CacheImpl::ImplTy CacheImpl::create(StringRef Name, const CallBacks &CBs) {
+  return new DefaultCache(CBs);
+}
+
+void CacheImpl::setAndRetain(void *Key, void *Value, size_t Cost) {
+  DefaultCache &DCache = *static_cast<DefaultCache*>(Impl);
+  llvm::sys::ScopedLock L(DCache.Mux);
+
+  DefaultCacheKey CKey(Key, &DCache.CBs);
+  auto Entry = DCache.Entries.find(CKey);
+  if (Entry != DCache.Entries.end()) {
+    DCache.CBs.keyDestroyCB(Entry->first.Key, nullptr);
+    DCache.CBs.valueDestroyCB(Entry->second, nullptr);
+    DCache.Entries.erase(Entry);
+  }
+
+  DCache.Entries[CKey] = Value;
+
+  // FIXME: Not thread-safe! It should avoid deleting the value until
+  // 'releaseValue is called on it.
+}
+
+bool CacheImpl::getAndRetain(const void *Key, void **Value_out) {
+  DefaultCache &DCache = *static_cast<DefaultCache*>(Impl);
+  llvm::sys::ScopedLock L(DCache.Mux);
+
+  DefaultCacheKey CKey(const_cast<void*>(Key), &DCache.CBs);
+  auto Entry = DCache.Entries.find(CKey);
+  if (Entry != DCache.Entries.end()) {
+    // FIXME: Not thread-safe! It should avoid deleting the value until
+    // 'releaseValue is called on it.
+    *Value_out = Entry->second;
+    return true;
+  }
+  return false;
+}
+
+void CacheImpl::releaseValue(void *Value) {
+  // FIXME: Implementation.
+}
+
+bool CacheImpl::remove(const void *Key) {
+  DefaultCache &DCache = *static_cast<DefaultCache*>(Impl);
+  llvm::sys::ScopedLock L(DCache.Mux);
+
+  DefaultCacheKey CKey(const_cast<void*>(Key), &DCache.CBs);
+  auto Entry = DCache.Entries.find(CKey);
+  if (Entry != DCache.Entries.end()) {
+    DCache.CBs.keyDestroyCB(Entry->first.Key, nullptr);
+    DCache.CBs.valueDestroyCB(Entry->second, nullptr);
+    DCache.Entries.erase(Entry);
+    return true;
+  }
+  return false;
+}
+
+void CacheImpl::removeAll() {
+  DefaultCache &DCache = *static_cast<DefaultCache*>(Impl);
+  llvm::sys::ScopedLock L(DCache.Mux);
+
+  for (auto Entry : DCache.Entries) {
+    DCache.CBs.keyDestroyCB(Entry.first.Key, nullptr);
+    DCache.CBs.valueDestroyCB(Entry.second, nullptr);
+  }
+  DCache.Entries.clear();
+}
+
+void CacheImpl::destroy() {
+  removeAll();
+  delete static_cast<DefaultCache*>(Impl);
+}
+
+#endif // finish default implementation
