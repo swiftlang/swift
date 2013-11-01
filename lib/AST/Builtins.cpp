915209b4f91f39c2b00032ebf7ca96518b40a617
Mark Lacey <mark.lacey@apple.com>
2013-11-01 05:46:27 +0000

    Add IITDescriptor::VarArg case to DecodeIntrinsicType().
    LLVM rev r193766 adds this new descriptor kind. Update the switch
    to squelch the warning about an unhandled enum value.
    
    Swift SVN r9868

--- lib/AST/Builtins.cpp
+++ lib/AST/Builtins.cpp
@@ -751,6 +751,7 @@ static Type DecodeIntrinsicType(ArrayRef<llvm::Intrinsic::IITDescriptor> &Table,
   case IITDescriptor::Vector:
   case IITDescriptor::ExtendVecArgument:
   case IITDescriptor::TruncVecArgument:
+  case IITDescriptor::VarArg:
     // These types cannot be expressed in swift yet.
     return Type();
 
