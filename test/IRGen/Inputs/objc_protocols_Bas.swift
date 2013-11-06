413e5bfe0f6ff1a84dac986df7c2fbfd6efbcd48
Dmitri Hrybenko <dgribenko@apple.com>
2013-11-06 20:16:46 +0000

    IRGen tests: consistently use a fake Foundation module located in the Inputs directory when using a fake SDK
    Swift SVN r10004

--- test/IRGen/Inputs/objc_protocols_Bas.swift
+++ test/IRGen/Inputs/objc_protocols_Bas.swift
@@ -1,8 +1,10 @@
-// RUN: %swift -parse-as-library -parse %s
-
 // Used by the objc_protocols test to test extensions that add conformances
 // using existing methods on a class.
 
 class Bas {
-  func runce() {}
+  def runce() {}
+}
+
+class Zang {
+  def funge() {}
 }
