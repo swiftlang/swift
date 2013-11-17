5f63cbe37542532530d27252b533c9a7b84de7cb
Dmitri Hrybenko <dgribenko@apple.com>
2013-11-17 09:38:43 +0000

    Move driver test to a correct place
    Swift SVN r10525

--- test/Driver/dump-parse.swift
+++ test/Driver/dump-parse.swift
@@ -1,10 +1,9 @@
 // RUN: %swift -dump-parse %s 2>&1 | FileCheck %s
 
 // CHECK: (func_decl "foo"
-func foo(n : Int) -> Int {
-  // CHECK: (func_expr type='<null>'
+func foo(n: Int) -> Int {
   // CHECK:   (brace_stmt
   // CHECK:     (return_stmt
-  // CHECK:       (integer_literal_expr type='<null>' value=42))))))
+  // CHECK:       (integer_literal_expr type='<null>' value=42)))
   return 42
 }
