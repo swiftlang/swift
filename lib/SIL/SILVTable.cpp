14c858b69a6fadaf1f249b8a1c78eb28895eb184
Chris Lattner <clattner@apple.com>
2013-12-14 07:24:34 +0000

    Fix a nasty memory smasher in SILVTable that was causing one of my innocent (!) tests to fail.  Basically, in the case of an empty vtable, SILVTable::create was allocating space for 0 entries (sensible!) but then the default ctor fires for the 1 element declared in the class, which would write off the end of the memory block, leading to unpleasantness in random places later.
    Swift SVN r11306

--- lib/SIL/SILVTable.cpp
+++ lib/SIL/SILVTable.cpp
@@ -25,7 +25,10 @@ using namespace swift;
 
 SILVTable *SILVTable::create(SILModule &M, ClassDecl *Class,
                              ArrayRef<Pair> Entries) {
-  void *buf = M.allocate(sizeof(SILVTable) + sizeof(Pair) * (Entries.size()-1),
+  // SILVTable contains one element declared in Entries.  We must allocate
+  // space for it, because its default ctor will write to it.
+  unsigned NumTailElements = std::max((unsigned)Entries.size(), 1U)-1;
+  void *buf = M.allocate(sizeof(SILVTable) + NumTailElements,
                          alignof(SILVTable));
   SILVTable *vt = ::new (buf) SILVTable(Class, Entries);
   M.vtables.push_back(vt);
