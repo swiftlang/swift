b0092c4717c038187695623ad28d67f5c28f0a98
Connor Wakamo <cwakamo@apple.com>
2013-12-17 18:35:00 +0000

    [frontend] Initial support for performing non-EmitObject actions in the integrated frontend.
    The integrated frontend now supports:
      - Parse
      - DumpParse
      - DumpAST
      - PrintAST
      - EmitSILGen
      - EmitSIL
      - EmitIR
      - EmitBC
      - EmitAssembly
      - EmitObject
    
    Moved the code for compiling into a separate performCompile() function in frontend_main.cpp, to facilitate using early returns while still supporting the diagnostic verifier.
    Added some error output for the REPL, Immediate, and EmitModuleOnly actions, as the integrated frontend does not yet support these actions.
    
    Swift SVN r11387

--- tools/driver/frontend_main.cpp
+++ tools/driver/frontend_main.cpp
@@ -47,6 +47,142 @@ static std::string displayName(StringRef MainExecutablePath) {
   return Name;
 }
 
+/// Writes SIL out to the given file.
+static bool writeSIL(SILModule &SM, Module *M, bool EmitVerboseSIL,
+                     std::string &OutputFilename) {
+  std::string ErrorInfo;
+  llvm::raw_fd_ostream OS(OutputFilename.c_str(), ErrorInfo);
+  if (!ErrorInfo.empty()) {
+    M->Ctx.Diags.diagnose(SourceLoc(), diag::error_opening_output,
+                          OutputFilename, ErrorInfo);
+    return true;
+  }
+  SM.print(OS, EmitVerboseSIL, M);
+  return false;
+}
+
+/// Performs the compile requested by the user.
+/// \returns true on error
+static bool performCompile(CompilerInstance &Instance,
+                           CompilerInvocation &Invocation) {
+  FrontendOptions::ActionType Action =
+    Invocation.getFrontendOptions().RequestedAction;
+
+  Instance.performParse();
+  ASTContext &Context = Instance.getASTContext();
+  if (Context.hadError())
+    return true;
+
+  // We've just been told to perform a parse, so we can return now.
+  if (Action == FrontendOptions::Parse)
+    return false;
+
+  // We've been told to dump the AST (either after parsing or type-checking,
+  // which is already differentiated in CompilerInstance::performParse()),
+  // so dump the main source file and return.
+  if (Action == FrontendOptions::DumpParse ||
+      Action == FrontendOptions::DumpAST) {
+    SourceFileKind Kind = Invocation.getInputKind();
+    Instance.getMainModule()->getMainSourceFile(Kind).dump();
+    return false;
+  }
+
+  // We've been told to pretty print the AST, so do that and return.
+  if (Action == FrontendOptions::PrintAST) {
+    SourceFileKind Kind = Invocation.getInputKind();
+    SourceFile &SF = Instance.getMainModule()->getMainSourceFile(Kind);
+    SF.print(llvm::outs(), PrintOptions::printEverything());
+    return false;
+  }
+
+  assert(Action >= FrontendOptions::EmitSILGen &&
+         "All actions not requiring SILGen must have been handled!");
+
+  std::unique_ptr<SILModule> SM = Instance.takeSILModule();
+  if (!SM) {
+    SM = performSILGeneration(Instance.getMainModule());
+    performSILLinking(SM.get());
+  }
+
+  // We've been told to emit SIL after SILGen, so write it now.
+  if (Action == FrontendOptions::EmitSILGen) {
+    return writeSIL(*SM, Instance.getMainModule(),
+                    Invocation.getFrontendOptions().EmitVerboseSIL,
+                    Invocation.getFrontendOptions().OutputFilename);
+  }
+
+  assert(Action >= FrontendOptions::EmitSIL &&
+         "All actions not requiring SILPasses must have been handled!");
+
+  // Perform "stable" optimizations that are invariant across compiler versions.
+  if (!Invocation.getDiagnosticOptions().SkipDiagnosticPasses &&
+      runSILDiagnosticPasses(*SM))
+    return true;
+
+  SM->verify();
+
+  // TODO: perform SIL optimization passes, once we know the optimization level.
+  // These may change across compiler versions.
+
+  // TODO: emit module, if requested.
+  if (Action == FrontendOptions::EmitModuleOnly) {
+    llvm::errs() << "error: frontend does not yet support emitting modules\n";
+    return true;
+  }
+
+  // We've been told to write canonical SIL, so write it now.
+  if (Action == FrontendOptions::EmitSIL) {
+    return writeSIL(*SM, Instance.getMainModule(),
+                    Invocation.getFrontendOptions().EmitVerboseSIL,
+                    Invocation.getFrontendOptions().OutputFilename);
+  }
+
+  assert(Action >= FrontendOptions::Immediate &&
+         "All actions not requiring IRGen must have been handled!");
+  assert(Action != FrontendOptions::REPL &&
+         "REPL mode must be handled separate from a normal compile");
+
+  // Check if we had any errors; if we did, don't proceed to IRGen.
+  if (Context.hadError())
+    return true;
+
+  // Cleanup instructions/builtin calls not suitable for IRGen.
+  performSILCleanup(SM.get());
+
+  irgen::Options Options;
+  Options.MainInputFilename = Invocation.getInputFilenames()[0];
+  Options.Triple = Invocation.getTargetTriple();
+  Options.LinkLibraries.append(Invocation.getLinkLibraries().begin(),
+                               Invocation.getLinkLibraries().end());
+  Options.OptLevel = 0;
+  Options.OutputFilename = Invocation.getOutputFilename();
+
+  switch (Action) {
+  case FrontendOptions::EmitIR:
+    Options.OutputKind = irgen::OutputKind::LLVMAssembly;
+    break;
+  case FrontendOptions::EmitBC:
+    Options.OutputKind = irgen::OutputKind::LLVMBitcode;
+    break;
+  case FrontendOptions::EmitAssembly:
+    Options.OutputKind = irgen::OutputKind::NativeAssembly;
+    break;
+  case FrontendOptions::EmitObject:
+    Options.OutputKind = irgen::OutputKind::ObjectFile;
+    break;
+  case FrontendOptions::Immediate:
+    llvm_unreachable("Immediate mode is not yet implemented");
+    return true;
+  default:
+    llvm_unreachable("Unknown ActionType which requires IRGen");
+    return true;
+  }
+
+  performIRGeneration(Options, nullptr, Instance.getMainModule(), SM.get());
+
+  return false;
+}
+
 int frontend_main(ArrayRef<const char *>Args,
                   const char *Argv0, void *MainAddr) {
   llvm::InitializeAllTargets();
@@ -99,53 +235,20 @@ int frontend_main(ArrayRef<const char *>Args,
     return 1;
   }
 
-  Instance.performParse();
+  FrontendOptions::ActionType Action =
+    Invocation.getFrontendOptions().RequestedAction;
+  if (Action == FrontendOptions::REPL || Action == FrontendOptions::Immediate) {
+    // TODO: remove once the integrated frontend supports REPL, Immediate modes
+    llvm::errs() << "error: integrated frontend does not support "
+                 << (Action == FrontendOptions::REPL ? "REPL" :
+                                                       "immediate mode")
+                 << '\n';
 
-  ASTContext &Context = Instance.getASTContext();
-
-  if (!Context.hadError()) {
-    std::unique_ptr<SILModule> SM = Instance.takeSILModule();
-    if (!SM) {
-      SM = performSILGeneration(Instance.getMainModule());
-      performSILLinking(SM.get());
-    }
-
-    if (SM->getStage() != SILStage::Canonical) {
-      if (!Invocation.getDiagnosticOptions().SkipDiagnosticPasses) {
-        performSILMandatoryInlining(SM.get());
-
-        performSILCapturePromotion(SM.get());
-        performSILAllocBoxToStackPromotion(SM.get());
-        performInOutDeshadowing(SM.get());
-        performSILDefiniteInitialization(SM.get());
-
-        performSILConstantPropagation(SM.get());
-        performSILDeadCodeElimination(SM.get());
-
-        emitSILDataflowDiagnostics(SM.get());
-
-        SM->setStage(SILStage::Canonical);
-      }
-    }
-
-    if (!Context.hadError()) {
-      performSILCleanup(SM.get());
-
-      irgen::Options Options;
-      Options.MainInputFilename = Invocation.getInputFilenames()[0];
-      Options.Triple = Invocation.getTargetTriple();
-      Options.LinkLibraries.append(Invocation.getLinkLibraries().begin(),
-                                   Invocation.getLinkLibraries().end());
-      Options.OptLevel = 0;
-      Options.OutputFilename = Invocation.getOutputFilename();
-
-      Options.OutputKind = irgen::OutputKind::ObjectFile;
-
-      performIRGeneration(Options, nullptr, Instance.getMainModule(), SM.get());
-    }
+    return 1;
   }
 
-  bool HadError = Context.hadError();
+  bool HadError = performCompile(Instance, Invocation) ||
+                  Instance.getASTContext().hadError();
 
   if (Invocation.getDiagnosticOptions().VerifyDiagnostics) {
     HadError = verifyDiagnostics(Instance.getSourceMgr(),
