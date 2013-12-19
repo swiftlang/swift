b390ad42791b92c259725a0151f2d58470416dd9
Michael Gottesman <mgottesman@apple.com>
2013-12-19 00:27:29 +0000

    Fixed typo from 11456.
    Swift SVN r11457

--- include/swift/SIL/SILLocation.h
+++ include/swift/SIL/SILLocation.h
@@ -418,8 +418,8 @@ class MandatoryInlinedLocation : public SILLocation {
 public:
   MandatoryInlinedLocation(Expr *CallSite) :
     SILLocation(CallSite, MandatoryInlinedKind) {}
-  MandatoryInlinedLocation(Stmt *S) : SILLocation(S, InlinedKind) {}
-  MandatoryInlinedLocation(Pattern *P) : SILLocation(P, InlinedKind) {}
+  MandatoryInlinedLocation(Stmt *S) : SILLocation(S, MandatoryInlinedKind) {}
+  MandatoryInlinedLocation(Pattern *P) : SILLocation(P, MandatoryInlinedKind) {}
   MandatoryInlinedLocation(Decl *D) : SILLocation(D, MandatoryInlinedKind) {}
 
   /// Constructs an inlined location when the call site is represented by a
