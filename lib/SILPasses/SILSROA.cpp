04f82a72c9f2efc7d303a5838c305cbcd8cb9f12
Michael Gottesman <mgottesman@apple.com>
2013-12-30 00:20:36 +0000

    Fix typo.
    Swift SVN r11729

--- lib/SILPasses/SILSROA.cpp
+++ lib/SILPasses/SILSROA.cpp
@@ -1,4 +1,4 @@
-//===--- SROA.cpp - Scalar Replacement of Aggregates ----------------------===//
+//===--- SILSROA.cpp - Scalar Replacement of Aggregates -------------------===//
 //
 // This source file is part of the Swift.org open source project
 //
