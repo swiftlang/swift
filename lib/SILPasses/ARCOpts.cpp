a3eb838f167bd0ccf76f0ac7a44cae5412802155
Michael Gottesman <mgottesman@apple.com>
2014-01-21 08:56:30 +0000

    [sil-arc-opts] Teach the optimizer that certain instructions whose memory behavior is MayHaveSideEffects do not decrement ref counts.
    The relevant instructions are:
    
    1. DeallocStackInst.
    2. StrongRetainInst.
    3. StrongRetainAutoreleasedInst.
    4. StrongRetainUnownedInst.
    5. UnownedRetainInst.
    6. PartialApplyInst.
    7. CondFailInst.
    8. CopyAddrInst when initialized.
    
    Swift SVN r12639

--- lib/SILPasses/ARCOpts.cpp
+++ lib/SILPasses/ARCOpts.cpp
@@ -37,6 +37,27 @@ STATISTIC(NumIncrementsRemoved, "Total number of increments removed");
 
 /// Could Inst decrement the ref count associated with target?
 static bool cannotDecrementRefCount(SILInstruction *Inst, SILValue Target) {
+  // Clear up some small cases where MayHaveSideEffects is too broad for our
+  // purposes and the instruction does not decrement ref counts.
+  switch (Inst->getKind()) {
+  case ValueKind::DeallocStackInst:
+  case ValueKind::StrongRetainInst:
+  case ValueKind::StrongRetainAutoreleasedInst:
+  case ValueKind::StrongRetainUnownedInst:
+  case ValueKind::UnownedRetainInst:
+  case ValueKind::PartialApplyInst:
+  case ValueKind::CondFailInst:
+    return true;
+  case ValueKind::CopyAddrInst: {
+    auto *CA = cast<CopyAddrInst>(Inst);
+    if (CA->isInitializationOfDest() == IsInitialization_t::IsInitialization)
+      return true;
+  }
+  SWIFT_FALLTHROUGH;
+  default:
+    break;
+  }
+
   // Just make sure that we do not have side effects.
   return Inst->getMemoryBehavior() !=
     SILInstruction::MemoryBehavior::MayHaveSideEffects;
