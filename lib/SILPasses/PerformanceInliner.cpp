acd151f40a1d51043848aa406b5c02080f2fb421
Michael Gottesman <mgottesman@apple.com>
2014-01-21 17:12:11 +0000

    [performance-inliner] A gep of a hasOneUse gep will get folded by LLVM into one gep. In that case the second should be free.
    Swift SVN r12642

--- lib/SILPasses/PerformanceInliner.cpp
+++ lib/SILPasses/PerformanceInliner.cpp
@@ -109,6 +109,16 @@ static unsigned instructionInlineCost(SILInstruction &I) {
     case ValueKind::DebugValueInst:
     case ValueKind::DebugValueAddrInst:
       return 0;
+    case ValueKind::TupleElementAddrInst:
+    case ValueKind::StructElementAddrInst: {
+      // A gep whose operand is a gep with no other users will get folded by
+      // LLVM into one gep implying the second should be free.
+      SILValue Op = I.getOperand(0);
+      if ((Op->getKind() == ValueKind::TupleElementAddrInst ||
+           Op->getKind() == ValueKind::StructElementAddrInst) &&
+          Op->hasOneUse())
+        return 0;
+    }
     default:
       return 1;
   }
