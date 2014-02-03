99a96080dd21078963bf49e5db094ed7827b8b34
Michael Gottesman <mgottesman@apple.com>
2014-02-03 10:51:10 +0000

    [sil-aa] Teach SIL-AA that two "identified objects" can not alias if they do not equal each other.
    Swift SVN r13346

--- lib/SILPasses/Utils/AliasAnalysis.cpp
+++ lib/SILPasses/Utils/AliasAnalysis.cpp
@@ -16,6 +16,10 @@
 
 using namespace swift;
 
+//===----------------------------------------------------------------------===//
+//                             Utility Functions
+//===----------------------------------------------------------------------===//
+
 /// Strip off casts/address projections from V until there is nothing
 /// left to strip.
 static SILValue getUnderlyingObject(SILValue V) {
@@ -52,8 +56,34 @@ static bool isIdentifiableObject(SILValue V) {
   return false;
 }
 
+static bool aliasUnequalObjects(SILValue O1, SILValue O2) {
+  assert(O1 != O2 && "This function should only be called on unequal values.");
+
+  // If O1 and O2 do not equal and they are both values that can be statically
+  // and uniquely identified, they can not alias.
+  if (isIdentifiableObject(O1) && isIdentifiableObject(O2))
+    return true;
+
+  // We failed to prove that the two objects are different.
+  return false;
+}
+
+//===----------------------------------------------------------------------===//
+//                                Entry Points
+//===----------------------------------------------------------------------===//
+
 AliasAnalysis::Result AliasAnalysis::alias(SILValue V1, SILValue V2) {
-  /// FIXME: Fill this out.
+  // Get the underlying objects for V1, V2.
+  SILValue O1 = getUnderlyingObject(V1);
+  SILValue O2 = getUnderlyingObject(V2);
+
+  // If O1 and O2 do not equal, see if we can prove that they can not be the
+  // same object. If we can, return No Alias.
+  if (O1 != O2 && aliasUnequalObjects(O1, O2))
+    return Result::NoAlias;
+
+  // We could not prove anything. Be conservative and return that V1, V2 may
+  // alias.
   return Result::MayAlias;
 }
 
