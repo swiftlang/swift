49614d47174f4903dd77861e67efd25d04b8a4a1
Michael Gottesman <mgottesman@apple.com>
2014-02-03 02:33:59 +0000

    [sil-aa] Initial commit just to set the AA API with alias stubs.
    The idea is for AliasAnalysis to be a wrapper around a Cache of alias analysis
    results that a pass creates when it needs to perform alias analysis queries in
    an analogous manner to how passes use DominanceInfo.
    
    When we get an "analysis" pass infrastructure, this should be made into an
    analysis pass.
    
    Currently the stubs just return AliasAnalysis::Result::MayAlias.
    
    Swift SVN r13327

---
+++ include/swift/SILPasses/Utils/AliasAnalysis.h
@@ -0,0 +1,66 @@
+//===-------------- AliasAnalysis.h - SIL Alias Analysis -*- C++ -*--------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2015 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef SWIFT_SILPASSES_ALIASANALYSIS_H
+#define SWIFT_SILPASSES_ALIASANALYSIS_H
+
+#include "llvm/ADT/DenseMap.h"
+#include "llvm/ADT/SmallVector.h"
+
+namespace swift {
+
+class SILValue;
+class SILInstruction;
+
+/// This class is a simple wrapper around an alias analysis
+/// cache. This is needed since we do not have an "analysis"
+/// infrastructure.
+class AliasAnalysis {
+public:
+  /// The result of an alias query. This is based off of LLVM's alias
+  /// analysis so see LLVM's documentation for more information.
+  ///
+  /// FIXME: PartialAlias?
+  enum class Result : unsigned {
+    NoAlias=0,      ///< The two values have no dependencies on each
+                    ///  other.
+    MayAlias,       ///< The two values can not be proven to alias or
+                    ///  not alias. Anything could happen.
+    MustAlias,      ///< The two values are equal.
+  };
+
+private:
+  using CacheKey = std::pair<SILValue, SILValue>;
+
+  llvm::DenseMap<CacheKey, Result> Cache;
+  llvm::DenseMap<SILValue, llvm::SmallVector<SILValue, 4>> ValueToPairedValue;
+
+public:
+
+  /// Perform an alias query to see if V1, V2 refer to the same values.
+  Result alias(SILValue V1, SILValue V2);
+
+  /// Perform an alias query on Inst to see if any of its operands
+  /// alias V.
+  Result alias(SILInstruction *Inst, SILValue V);
+
+  /// Clear the cache.
+  ///
+  /// FIXME: Add in an API to call to invalidate all of the cache
+  /// entries for one SILValue. It is not needed right now, but it
+  /// could be useful in the future.
+  void invalidateCache() { Cache.clear(); }
+};
+
+} // end namespace swift
+
+#endif
---
+++ lib/SILPasses/Utils/AliasAnalysis.cpp
@@ -0,0 +1,27 @@
+//===-------------- AliasAnalysis.cpp - SIL Alias Analysis ----------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2015 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+#include "swift/SILPasses/Utils/AliasAnalysis.h"
+#include "swift/SIL/SILValue.h"
+
+using namespace swift;
+
+AliasAnalysis::Result AliasAnalysis::alias(SILValue V1, SILValue V2) {
+  /// FIXME: Fill this out.
+  return Result::MayAlias;
+}
+
+AliasAnalysis::Result AliasAnalysis::alias(SILInstruction *Inst,
+                                                SILValue V2) {
+  /// FIXME: Fill this out.
+  return Result::MayAlias;
+}
