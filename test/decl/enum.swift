b000ce2ec836421fcf9e4ec09e418ffabbbaae8b
Chris Lattner <clattner@apple.com>
2014-02-03 16:51:49 +0000

    'data' got renamed to 'enum' a long long time ago, update test to match.
    Swift SVN r13357

--- test/decl/enum.swift
+++ test/decl/enum.swift
@@ -13,7 +13,7 @@ enum unionSearchFlags {
   init() { self = .None }
 }
 
-def test1() -> unionSearchFlags {
+func test1() -> unionSearchFlags {
   var a : unionSearchFlags
   var b = unionSearchFlags.None
   b = unionSearchFlags.Anchored
@@ -21,7 +21,7 @@ def test1() -> unionSearchFlags {
   return unionSearchFlags.Backwards
 }
 
-def test1a() -> unionSearchFlags {
+func test1a() -> unionSearchFlags {
   var a : unionSearchFlags
   var b : unionSearchFlags = .None
   b = .Anchored
@@ -32,7 +32,7 @@ def test1a() -> unionSearchFlags {
   return .Backwards
 }
 
-def test1b() {
+func test1b(b : Bool) {
   var x = 123
   .true == true // should NOT parse as "123.true"
 }
@@ -42,7 +42,7 @@ enum MaybeInt {
   case Some(Int)
 }
 
-def test2(a: Int, b: Int, c: MaybeInt) {
+func test2(a: Int, b: Int, c: MaybeInt) {
   var c = MaybeInt.Some(4)
   var d = MaybeInt.Some  // Bind the function itself.
   var e = MaybeInt.Some(b)
@@ -58,7 +58,7 @@ enum ZeroOneTwoThree {
   case Unknown(MaybeInt, MaybeInt, MaybeInt)
 }
 
-def test3(a: ZeroOneTwoThree) {
+func test3(a: ZeroOneTwoThree) {
   var e = ZeroOneTwoThree.Three(1,2,3)
   var f = ZeroOneTwoThree.Unknown(MaybeInt.None, MaybeInt.Some(4),
                                   MaybeInt.Some(32))
@@ -73,12 +73,12 @@ def test3(a: ZeroOneTwoThree) {
   test3 // expected-error {{expression resolves to an unused function}}
   (ZeroOneTwoThree.Zero)
   
-  var h : ZeroOneTwoThree = ((.One))(4)
+  var h : ZeroOneTwoThree = .One(4)
   
-  var inf : (Int,Int) -> ZeroOneTwoThree = .Two
+  var inf : (Int,Int) -> ZeroOneTwoThree = .Two // expected-error{{'((Int, Int) -> ZeroOneTwoThree).metatype' does not have a member named 'Two'}}
 }
 
-def test3a(a: ZeroOneTwoThree) {
+func test3a(a: ZeroOneTwoThree) {
   var e : ZeroOneTwoThree = (.Three(1, 2, 3))
   var f = ZeroOneTwoThree.Unknown(.None, .Some(4), .Some(32))
 
@@ -98,7 +98,7 @@ def test3a(a: ZeroOneTwoThree) {
 struct CGPoint { var x : Int, y : Int }
 typealias OtherPoint = ( x : Int, y : Int)
 
-def test4() {
+func test4() {
   var a : CGPoint
   // Note: we reject the following because it conflicts with the current
   // "init" hack.
@@ -117,11 +117,11 @@ def test4() {
 struct CGSize { var width : Int, height : Int }
 
 extension CGSize {
-  def area() -> Int {
+  func area() -> Int {
     return width*self.height
   }
   
-  def area_wrapper() -> Int {
+  func area_wrapper() -> Int {
     return area()
   }
 }
@@ -130,21 +130,21 @@ struct CGRect {
   var origin : CGPoint,
   size : CGSize
   
-  def area() -> Int {
+  func area() -> Int {
     return self.size.area()
   }
 }
 
-def area(r: CGRect) -> Int {
+func area(r: CGRect) -> Int {
   return r.size.area()
 }
 
 extension CGRect {
-  def search(x: Int) -> CGSize {}
-  def bad_search(Int) -> CGSize {} // expected-error {{type annotation missing in pattern}}
+  func search(x: Int) -> CGSize {}
+  func bad_search(Int) -> CGSize {} // expected-error {{type annotation missing in pattern}}
 }
 
-def test5(myorigin: CGPoint) {
+func test5(myorigin: CGPoint) {
   var x1 = CGRect(myorigin, CGSize(42, 123))
   var x2 = CGRect(size : CGSize(width : 42, height:123), origin : myorigin)
 
@@ -157,7 +157,7 @@ def test5(myorigin: CGPoint) {
   var x5 = x1.size.area()
   var x5a = (r : x1.size).r.area()
   
-  var x5b = x1.area //expected-error{{partial application of struct or enum method is not allowed}}
+  var x5b = x1.area //expected-error{{partial application of struct method is not allowed}}
 
   var x6 = x1.search(42)
   var x7 = x1.search(42).width
@@ -177,52 +177,52 @@ struct StructTest1 {
 
   typealias ElementType = Int
 }
-def +() {} // expected-error {{operators must have one or two arguments}}
+func +() {} // expected-error {{operators must have one or two arguments}}
 
 enum UnionTest1 {
   case x
   case y(Int)
 
-  def foo() {}
+  func foo() {}
 
   init() { self = .x }
 }
 
 
 extension UnionTest1 {
-  def foo() {}
-  def bar() {}
+  func foo() {}
+  func bar() {}
 
   // metatype method.
-  static def baz() {}
+  type func baz() {}
 }
 
 struct EmptyStruct {
-  def foo() {}
+  func foo() {}
 }
 
-static def global_static_func() {  // expected-error {{'static' functions may only be declared on a type}} {{1-8=}}
+type func global_static_func() {  // expected-error {{type functions may only be declared on a type}} {{1-6=}}
 }
 
-def f() { 
+func f() { 
   var a : UnionTest1
   a.bar()
   UnionTest1.baz()  // dot syntax access to a static method.
   
   // Test that we can get the "address of a member".
   var member_ptr_static : () -> () = UnionTest1.baz
-  var member_ptr : (@inout UnionTest1) -> () -> () = UnionTest1.bar
+  var member_ptr : (UnionTest1) -> () -> () = UnionTest1.bar
 }
 
-def union_error(a: ZeroOneTwoThree) {
-  var t1 : ZeroOneTwoThree = .Zero(1) // expected-error {{expression does not type-check}}
+func union_error(a: ZeroOneTwoThree) {
+  var t1 : ZeroOneTwoThree = .Zero(1) // expected-error {{'$T0 -> ZeroOneTwoThree' is not identical to 'ZeroOneTwoThree'}}
   var t2 : ZeroOneTwoThree = .One // expected-error {{expression does not type-check}}
-  var t3 : ZeroOneTwoThree = .foo // expected-error {{expression does not type-check}}
-  var t4 : ZeroOneTwoThree = .foo() // expected-error {{expression does not type-check}}
+  var t3 : ZeroOneTwoThree = .foo // expected-error {{'ZeroOneTwoThree.metatype' does not have a member named 'foo'}}
+  var t4 : ZeroOneTwoThree = .foo() // expected-error {{'ZeroOneTwoThree.metatype' does not have a member named 'foo'}}
 }
 
-def local_struct() {
-  struct s { def y() {} }
+func local_struct() {
+  struct s { func y() {} }
 }
 
 //===----------------------------------------------------------------------===//
@@ -231,14 +231,14 @@ def local_struct() {
 
 struct distance { var v : Int }
 
-def - (lhs: distance, rhs: distance) -> distance {}
+func - (lhs: distance, rhs: distance) -> distance {}
 
 extension Int {
-  def km() -> distance {}
-  def cm() -> distance {}
+  func km() -> distance {}
+  func cm() -> distance {}
 }
 
-def units(x: Int) -> distance {
+func units(x: Int) -> distance {
   x.km() - 4.cm() - 42.km()
 }
 
@@ -246,7 +246,7 @@ def units(x: Int) -> distance {
 
 var %% : distance -> distance // expected-error {{expected pattern}}
 
-def badTupleElement() {
+func badTupleElement() {
   typealias X = (x : Int, y : Int)
   var y = X.y // expected-error{{'X.metatype' does not have a member named 'y'}}
   var z = X.z // expected-error{{'X.metatype' does not have a member named 'z'}}
