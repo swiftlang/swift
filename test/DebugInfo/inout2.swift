adef88c47f111197cbcbbeee3cfd46fc01fe7453
Adrian Prantl <aprantl@apple.com>
2014-02-06 01:21:29 +0000

    Add a testcase for <rdar://problem/15931767>
    Swift SVN r13535

---
+++ test/DebugInfo/inout2.swift
@@ -0,0 +1,43 @@
+// RUN: %swift -target x86_64-apple-darwin %s -emit-ir -g -o - | FileCheck %s
+// LValues are direct values, too. They are reference types, though.
+class Class {
+	var ivar : Int
+	init() { ivar = 1234 }
+}
+
+class Other : Class {
+	var ovar : Int
+	init () { ovar = 112233
+	    super.init()
+	    ivar = 4321
+	}
+}
+
+struct Struct {
+	var ivar : Int
+	init() { ivar = 4567 }
+}
+
+func foo (x : @inout Class) {
+// CHECK: i32 0, i32 0} ; [ DW_TAG_arg_variable ] [x] [line [[@LINE-1]]]
+	println(x.ivar)
+	x.ivar++ // Set breakpoint here
+}
+
+func foo(x : @inout Struct) {
+// CHECK: i32 0, i32 0} ; [ DW_TAG_arg_variable ] [x] [line [[@LINE-1]]]
+	println(x.ivar)
+	x.ivar++ // Set breakpoint here
+}
+
+func main() {
+	var c : Class = Other()
+	var s = Struct()
+	foo(&c)
+	foo(&s)
+	foo(&c)
+	foo(&s)
+}
+
+main()
+
