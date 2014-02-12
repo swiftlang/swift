e8ea34e24ea481a830e83c28a8d2c80504a97354
Jordan Rose <jordan_rose@apple.com>
2014-02-12 23:57:44 +0000

    Re-add ~= for NSObjects now that that doesn't conflict with swift.~=.
    Now you can use switch to match NSObject values. (This is all Joe's work
    from before. I just uncommented things and wrote some tests.)
    
    Swift SVN r13851

---
+++ test/SILGen/switch_objc.swift
@@ -0,0 +1,29 @@
+// RUN: %swift %clang-importer-sdk -emit-silgen %s | FileCheck %s
+
+import Foundation
+
+// CHECK-LABEL: sil @_TF11switch_objc13matchesEitherFT5inputCSo4Hive1aS0_1bS0__Sb :
+func matchesEither(input: Hive, a: Hive, b: Hive) -> Bool {
+  switch input {
+  // CHECK:   function_ref @_TF10ObjectiveCoi2teFT1xCSo8NSObject1yS0__Sb
+  // CHECK:   cond_br {{%.*}}, [[YES_CASE1:bb[0-9]+]], [[NOT_CASE1:bb[0-9]+]]
+  // CHECK: [[YES_CASE1]]:
+  // CHECK:   br [[RET_TRUE:bb[0-9]+]]
+  // CHECK: [[NOT_CASE1]]:
+  // CHECK:   function_ref @_TF10ObjectiveCoi2teFT1xCSo8NSObject1yS0__Sb
+  // CHECK:   cond_br {{%.*}}, [[YES_CASE2:bb[0-9]+]], [[NOT_CASE2:bb[0-9]+]]
+  // CHECK: [[YES_CASE2]]:
+  // CHECK:   br [[RET_TRUE]]
+  case a, b:
+  // CHECK: [[RET_TRUE]]:
+  // CHECK:   function_ref @_TFSsg4trueSb
+    return true
+
+  // CHECK: [[NOT_CASE2]]:
+  // CHECK:   br [[RET_FALSE:bb[0-9]+]]
+  default:
+  // CHECK: [[RET_FALSE]]:
+  // CHECK:   function_ref @_TFSsg5falseSb
+    return false
+  }
+}
