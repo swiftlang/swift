32aa71412a283a3a70e7546f931670937ccf12d8
Dave Abrahams <dabrahams@apple.com>
2014-02-14 22:23:00 +0000

    Fix the buildbot-script
    oops.
    
    Swift SVN r13926

--- utils/buildbot-script.sh
+++ utils/buildbot-script.sh
@@ -478,7 +478,7 @@ for product in "${SWIFT_TEST_PRODUCTS[@]}" ; do
         _PRODUCT_SOURCE_DIR=${PRODUCT}_SOURCE_DIR
         _PRODUCT_BUILD_DIR=${PRODUCT}_BUILD_DIR
     
-        build_cmd=($DISTCC_PUMP "$CMAKE" --build "${!_PRODUCT_BUILD_DIR}" -- ${BUILD_ARGS})
+        build_cmd=("$CMAKE" --build "${!_PRODUCT_BUILD_DIR}" -- ${BUILD_ARGS})
 
         "${build_cmd[@]}" ${BUILD_TARGET_FLAG} SwiftUnitTests
 
