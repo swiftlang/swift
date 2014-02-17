67fd13ed66f7a69cb0bc78362e328bbd0f78139f
Joe Groff <jgroff@apple.com>
2014-02-17 20:05:46 +0000

    Serialization: Fix a couple of issues with serializing context params of generic SILFunctions.
    - Parameterize maybeReadGenericParams' BitstreamCursor so that we can read from the correct cursor when trying to read the generic params of a SILFunction.
    - Only serialize the context generic params for SILFunctions for which we're serializing a complete definition. This fixes issues with us getting the wrong archetypes forward-declared from references in other modules.
    
    This gets me a clean build when applied against r13984.
    
    Swift SVN r14005

--- lib/Serialization/SerializeSIL.cpp
+++ lib/Serialization/SerializeSIL.cpp
@@ -202,8 +202,11 @@ void SILSerializer::writeSILFunction(const SILFunction &F, bool DeclOnly) {
   if (DeclOnly)
     return;
   
-  if (auto gp = F.getContextGenericParams()) {
-    S.writeGenericParams(gp, SILAbbrCodes);
+  // Write the body's context archetypes, unless we don't actually have a body.
+  if (!F.isExternalDeclaration()) {
+    if (auto gp = F.getContextGenericParams()) {
+      S.writeGenericParams(gp, SILAbbrCodes);
+    }
   }
   
   // Assign a unique ID to each basic block of the SILFunction.
