d9e17b5bb2e54e897566bd6b6bb77b2462bc6421
Joe Pamer <jpamer@apple.com>
2014-03-12 23:45:23 +0000

    Add a targeted test for non-generic classes inheriting from generic super classes.
    Swift SVN r14981

---
+++ test/Generics/non_generic_derived_class.swift
@@ -0,0 +1,11 @@
+// RUN: %swift -parse -verify %s
+
+class Base<T> {
+  class func f(arg: T) -> Int {
+    return 0
+  }
+}
+
+class Derived : Base<Int> {} // expected-error{{classes derived from generic classes must also be generic}}
+
+var a = Derived.f(42)
