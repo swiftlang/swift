01f4b6d9019f75a529395c1421dbf8186609880a
Dave Abrahams <dabrahams@apple.com>
2014-03-12 13:08:45 +0000

    [stdlib] Un-revert r14943: "Use the shims module to bridge NewArray"
    [This time, shims.h doesn't depend even on a system header; hopefully the
    'bot will be happy enough with that until someone fixes the build system
    to allow "#include <stdint.h>" to appear in shims.h]
    
    Now we can essentially send an NSRange instance in a message to a
    CocoaArray without having loaded foundation.
    
    Swift SVN r14957

--- stdlib/shims/shims.h
+++ stdlib/shims/shims.h
@@ -1,3 +1,25 @@
+//===--- shims.h ----------------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2015 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+// This struct is layout-compatible with NSRange.  Using the name
+// "NSRange" here could eliminate some horrible reinterpretCast
+// shenanigans in our briging code, but swift's module importer is not
+// yet tolerant of the same struct coming in from two different Clang
+// modules. <rdar://problem/16294674>
+typedef struct {
+  long location;
+  long length;
+} _SwiftNSRange;
+
 #ifndef __OBJC_SUPERCLASS_HACK__
 #define __OBJC_SUPERCLASS_HACK__
 __attribute__((objc_root_class))
