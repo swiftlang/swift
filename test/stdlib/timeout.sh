bd9a45165750829c21afe686a8e0fb822c25b875
Greg Parker <gparker@apple.com>
2014-03-17 21:39:28 +0000

    [test] Add a timeout because the Reflection test runs for hours under ASan.
    Swift SVN r15159

---
+++ test/stdlib/timeout.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# timeout.sh <seconds> <command...>
+
+# watchdog PID SECONDS
+function watchdog() {
+    (( seconds = "$2" ))
+    while (( seconds > 0 ))
+    do
+        kill -0 $1 || exit
+        sleep 1
+        (( seconds -= 1 ))
+    done
+
+    echo "** timeout.sh: Timed out" 1>&2
+    kill -ALRM $1
+}
+
+watchdog $$ $1 &
+shift
+exec "$@"
