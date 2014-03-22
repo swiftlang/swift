1367e745bd70a18cb4c5857d5e860bf87fe752d3
Jordan Rose <jordan_rose@apple.com>
2014-03-22 00:19:58 +0000

    Rename test/ClangModules/mixed-language.swift to mixed-framework.swift.
    No functionality change.
    
    Swift SVN r15354

--- test/ClangModules/mixed-framework.swift
+++ test/ClangModules/mixed-framework.swift
@@ -1,8 +1,8 @@
 // RUN: rm -rf %t
 // RUN: mkdir -p %t
 
-// RUN: cp -r %S/Inputs/mixed-language/Mixed.framework %t
-// RUN: %swift_driver -target x86_64-apple-macosx10.9 -emit-module -o %t/Mixed.framework/Modules/Mixed.swiftmodule/x86_64.swiftmodule %S/Inputs/mixed-language/Mixed.swift -module-name Mixed
+// RUN: cp -r %S/Inputs/mixed-framework/Mixed.framework %t
+// RUN: %swift_driver -target x86_64-apple-macosx10.9 -emit-module -o %t/Mixed.framework/Modules/Mixed.swiftmodule/x86_64.swiftmodule %S/Inputs/mixed-framework/Mixed.swift -module-name Mixed
 // RUN: %swift %clang-importer-sdk -target x86_64-apple-macosx10.9 -module-cache-path %t -F %t -parse %s -verify
 // REQUIRES: X86
 
