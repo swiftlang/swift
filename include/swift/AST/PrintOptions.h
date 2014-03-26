5e571043d818a381330d4e45dbeb882a183b6cf6
Dmitri Hrybenko <dgribenko@apple.com>
2014-03-26 11:40:30 +0000

    Unindent and fix comment
    Swift SVN r15506

--- include/swift/AST/PrintOptions.h
+++ include/swift/AST/PrintOptions.h
@@ -1,4 +1,4 @@
-//===--- PrintOptions.h - Swift Language Declaration ASTs -------*- C++ -*-===//
+//===--- PrintOptions.h - AST printing options ------------------*- C++ -*-===//
 //
 // This source file is part of the Swift.org open source project
 //
@@ -9,78 +9,75 @@
 // See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
 //
 //===----------------------------------------------------------------------===//
-//
-// This file defines the Decl class and subclasses.
-//
-//===----------------------------------------------------------------------===//
+
 #ifndef SWIFT_AST_PRINTOPTIONS_H
 #define SWIFT_AST_PRINTOPTIONS_H
 
 namespace swift {
-  class GenericParamList;
-  
-  struct PrintOptions {
-    /// \brief The indentation width.
-    unsigned Indent = 2;
+class GenericParamList;
 
-    /// \brief Whether to print function definitions.
-    bool FunctionDefinitions = false;
+struct PrintOptions {
+  /// \brief The indentation width.
+  unsigned Indent = 2;
 
-    /// \brief Whether to print '{ get set }' on readwrite computed properties.
-    bool PrintGetSetOnRWProperties = true;
+  /// \brief Whether to print function definitions.
+  bool FunctionDefinitions = false;
 
-    /// \brief Whether to print type definitions.
-    bool TypeDefinitions = false;
+  /// \brief Whether to print '{ get set }' on readwrite computed properties.
+  bool PrintGetSetOnRWProperties = true;
 
-    /// \brief Whether to print variable initializers.
-    bool VarInitializers = false;
+  /// \brief Whether to print type definitions.
+  bool TypeDefinitions = false;
 
-    /// \brief Whether to prefer printing TypeReprs instead of Types,
-    /// if a TypeRepr is available.  This allows us to print the original
-    /// spelling of the type name.
-    ///
-    /// \note This should be \c true when printing AST with the intention show
-    /// it to the user.
-    bool PreferTypeRepr = true;
+  /// \brief Whether to print variable initializers.
+  bool VarInitializers = false;
 
-    /// \brief Whether to print fully qualified Types.
-    bool FullyQualifiedTypes = false;
+  /// \brief Whether to prefer printing TypeReprs instead of Types,
+  /// if a TypeRepr is available.  This allows us to print the original
+  /// spelling of the type name.
+  ///
+  /// \note This should be \c true when printing AST with the intention show
+  /// it to the user.
+  bool PreferTypeRepr = true;
 
-    /// \brief Print fully qualified types if our heuristics say that a certain
-    /// type might be ambiguous.
-    bool FullyQualifiedTypesIfAmbiguous = false;
+  /// \brief Whether to print fully qualified Types.
+  bool FullyQualifiedTypes = false;
 
-    /// \brief Print Swift.Array and Swift.Optional with sugared syntax
-    /// ([] and ?), even if there are no sugar type nodes.
-    bool SynthesizeSugarOnTypes = false;
+  /// \brief Print fully qualified types if our heuristics say that a certain
+  /// type might be ambiguous.
+  bool FullyQualifiedTypesIfAmbiguous = false;
 
-    /// \brief If true, the printer will explode a pattern like this:
-    /// \code
-    ///   var (a, b) = f()
-    /// \endcode
-    /// into multiple variable declarations.
-    ///
-    /// For this option to work correctly, \c VarInitializers should be
-    /// \c false.
-    bool ExplodePatternBindingDecls = false;
+  /// \brief Print Swift.Array and Swift.Optional with sugared syntax
+  /// ([] and ?), even if there are no sugar type nodes.
+  bool SynthesizeSugarOnTypes = false;
 
-    /// \brief Whether to print implicit parts of the AST.
-    bool SkipImplicit = false;
+  /// \brief If true, the printer will explode a pattern like this:
+  /// \code
+  ///   var (a, b) = f()
+  /// \endcode
+  /// into multiple variable declarations.
+  ///
+  /// For this option to work correctly, \c VarInitializers should be
+  /// \c false.
+  bool ExplodePatternBindingDecls = false;
 
-    /// \brief Print dependent types as references into this generic parameter
-    /// list.
-    GenericParamList *ContextGenericParams = nullptr;
+  /// \brief Whether to print implicit parts of the AST.
+  bool SkipImplicit = false;
 
-    /// \brief Retrieve the set of options that prints everything.
-    static PrintOptions printEverything() {
-      PrintOptions result;
-      result.FunctionDefinitions = true;
-      result.TypeDefinitions = true;
-      result.VarInitializers = true;
-      result.SkipImplicit = false;
-      return result;
-    }
-  };
+  /// \brief Print dependent types as references into this generic parameter
+  /// list.
+  GenericParamList *ContextGenericParams = nullptr;
+
+  /// \brief Retrieve the set of options that prints everything.
+  static PrintOptions printEverything() {
+    PrintOptions result;
+    result.FunctionDefinitions = true;
+    result.TypeDefinitions = true;
+    result.VarInitializers = true;
+    result.SkipImplicit = false;
+    return result;
+  }
+};
 }
 
 #endif // LLVM_SWIFT_AST_PRINTOPTIONS_H
