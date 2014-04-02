30e6d1de9806a8535bdf6b25f55226e3716ed246
Dave Zarzycki <zarzycki@apple.com>
2014-04-02 21:30:31 +0000

    Runtime: remove dead _ObjCSuperClassHack
    Swift SVN r15832

--- stdlib/shims/shims.h
+++ stdlib/shims/shims.h
@@ -31,17 +31,4 @@ typedef struct {
     unsigned long extra[5];
 } _SwiftNSFastEnumerationState;
 
-// A base class we can use to create classes in Swift whose
-// inheritance can be changed at run-time with class_setSuperclass.
-// This technique is used in bridging: for example, to avoid coupling
-// the core standard library to Cocoa, we derive the native Swift
-// Array buffer from this class, and then make that derive from
-// NSArray when Foundation is loaded.
-__attribute__((objc_root_class))
-@interface _ObjCSuperClassHack
-{
-  void *isa;
-}
-@end
-
 #endif
--- test/stdlib/Shims.swift
+++
@@ -1,9 +0,0 @@
-// RUN: %target-run-simple-swift | FileCheck %s
-
-import SwiftShims
-
-class Foo : _ObjCSuperClassHack {
-  var x : Int = 123
-  func bar() {}
-}
-println("hello") // CHECK: hello
