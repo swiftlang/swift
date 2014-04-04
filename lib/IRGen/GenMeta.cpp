d70136a046ce2bf436adbcd7c360d2dbfc1b9ebe
Joe Groff <jgroff@apple.com>
2014-04-04 04:42:39 +0000

    IRGen: Don't eagerly emit protocol descriptors for imported protocols.
    ObjC protocol descriptors are uniqued by the ObjC runtime and lazily emitted by Clang, so we shouldn't burn time eagerly emitting descriptors for every protocol in the universe. Addresses some of the performance issue in <rdar://problem/16520314>.
    
    Swift SVN r15924

--- lib/IRGen/GenMeta.cpp
+++ lib/IRGen/GenMeta.cpp
@@ -3259,6 +3259,11 @@ void IRGenModule::emitProtocolDecl(ProtocolDecl *protocol) {
   // If the protocol is Objective-C-compatible, go through the path that
   // produces an ObjC-compatible protocol_t.
   if (protocol->isObjC()) {
+    // Native ObjC protocols are emitted on-demand in ObjC and uniqued by the
+    // runtime; we don't need to try to emit a unique descriptor symbol for them.
+    if (protocol->hasClangNode())
+      return;
+    
     getObjCProtocolGlobalVars(protocol);
     return;
   }
