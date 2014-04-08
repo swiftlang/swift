329ca68e8c93524d269611d301a4dea908bb33d3
Jordan Rose <jordan_rose@apple.com>
2014-04-08 01:23:24 +0000

    [ClangImporter] Add some more tests for mixing methods and properties.
    The rule right now is basically "the class is most important, followed by
    the superclass, followed by protocols", which I think is the right rule.
    As long as we aren't required to /check/ the conformance to an Objective-C
    protocol, we should be fine.
    
    Swift SVN r16036

--- test/ClangModules/Inputs/custom-modules/ObjCImplicitProperties.h
+++ test/ClangModules/Inputs/custom-modules/ObjCImplicitProperties.h
@@ -28,3 +28,44 @@ __attribute__((objc_root_class))
 
 - (void)setSetterOnly:(int)val;
 @end
+
+
+@protocol PropertiesProto
+- (id)methodInProto;
+@property id propertyInProto;
+@end
+
+__attribute__((objc_root_class))
+@interface Base <PropertiesProto>
+- (id)methodInBase;
+@property(readonly) id propertyInBase;
+
+- (id)methodPairInBase;
+- (void)setMethodPairInBase:(id)value;
+
+- (id)getterOnlyInBase;
+
+- (void)setSetterOnlyInBase:(id)value;
+
+@property id methodInProto;
+- (id)propertyInProto;
+- (id)methodInBaseButPropertyInProto;
+@property id propertyInBaseButMethodInProto;
+@end
+
+@protocol SubProto
+- (id)propertyInBaseButMethodInProto;
+@property id methodInBaseButPropertyInProto;
+@end
+
+@interface Sub : Base <SubProto>
+@property id methodInBase;
+- (id)propertyInBase;
+
+- (void)setMethodPairInBase:(id)value;
+
+- (id)getterOnlyInBase;
+- (void)setGetterOnlyInBase:(id)value;
+
+- (id)setterOnlyInBase;
+@end
