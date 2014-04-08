676c799d71d3f8c58684f087e25e7374eae536a1
Jordan Rose <jordan_rose@apple.com>
2014-04-08 01:23:24 +0000

    [test] Split framework comments test out to a separate file.
    This is unfortunately platform-specific, and swift-ide-test doesn't have a
    -resource-dir flag right now.
    
    <rdar://problem/16335808>
    
    Swift SVN r16037

---
+++ test/Serialization/comments-framework.swift
@@ -0,0 +1,27 @@
+// RUN: rm -rf %t
+// RUN: mkdir -p %t/comments.framework/Modules/comments.swiftmodule/
+// RUN: %swift -module-name comments -emit-module -emit-module-path %t/comments.framework/Modules/comments.swiftmodule/x86_64.swiftmodule -emit-module-doc-path %t/comments.framework/Modules/comments.swiftmodule/x86_64.swiftdoc %s
+// RUN: %swift-ide-test -print-module-comments -module-to-print=comments -source-filename %s -F %t | FileCheck %s
+// REQUIRES: OS=macosx
+
+/// first_decl_class_1 Aaa.
+class first_decl_class_1 {
+
+  /// decl_func_1 Aaa.
+  func decl_func_1() {}
+
+  /**
+   * decl_func_3 Aaa.
+   */
+  func decl_func_2() {}
+
+  /// decl_func_3 Aaa.
+  /** Bbb. */
+  func decl_func_3() {}
+}
+
+// CHECK: first_decl_class_1 RawComment=[/// first_decl_class_1 Aaa.\n]
+// CHECK: decl_func_1 RawComment=[/// decl_func_1 Aaa.\n]
+// CHECK: decl_func_2 RawComment=[/**\n   * decl_func_3 Aaa.\n   */]
+// CHECK: decl_func_3 RawComment=[/// decl_func_3 Aaa.\n/** Bbb. */]
+
