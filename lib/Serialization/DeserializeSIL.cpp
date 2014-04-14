ada9ebb7a9b4b9040b081e9feeef564094059848
Adrian Prantl <aprantl@apple.com>
2014-04-14 22:33:12 +0000

    [CLANG UPDATE!] Update uses of OnDiskChainedHashTable to the OnDiskIterableChainedHashTable interface introduced in CFE 206189.
    Thanks to Justin for guiding me through this!
    
    Swift SVN r16337

--- lib/Serialization/DeserializeSIL.cpp
+++ lib/Serialization/DeserializeSIL.cpp
@@ -172,7 +172,8 @@ SILDeserializer::readFuncTable(ArrayRef<uint64_t> fields, StringRef blobData) {
   auto base = reinterpret_cast<const uint8_t *>(blobData.data());
 
   using OwnedTable = std::unique_ptr<SerializedFuncTable>;
-  return OwnedTable(SerializedFuncTable::Create(base + tableOffset, base));
+  return OwnedTable(SerializedFuncTable::Create(base + tableOffset,
+                                                base + sizeof(uint32_t), base));
 }
 
 /// A high-level overview of how forward references work in serializer and
