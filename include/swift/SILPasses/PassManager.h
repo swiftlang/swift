b902c5e5ebd49fa3a9f7474b7efffb3f11be67ec
Doug Gregor <dgregor@apple.com>
2014-04-14 14:31:15 +0000

    Revert "Limit PassManager to 25 reschedule iterations as a workaround for <rdar://problem/16591598>."
    This reverts commit r16221. We don't need this limit.
    
    Swift SVN r16314

--- include/swift/SILPasses/PassManager.h
+++ include/swift/SILPasses/PassManager.h
@@ -45,9 +45,6 @@ namespace swift {
     /// The number of passes run so far.
     unsigned NumPassesRun = 0;
 
-    /// The number of times we've rescheduled to run again.
-    unsigned NumRescheduleIterations = 0;
-
   public:
     /// C'tor
     SILPassManager(SILModule *M, SILOptions Opts) :
@@ -86,10 +83,7 @@ namespace swift {
     void runOneIteration();
 
     /// \brief Request another invocation of the transformation pipeline.
-    void scheduleAnotherIteration() { 
-      if (!anotherIteration && ++NumRescheduleIterations < 25)
-        anotherIteration = true; 
-    }
+    void scheduleAnotherIteration() { anotherIteration = true; }
 
     ///  \brief Broadcast the invalidation of the module to all analysis.
     void invalidateAnalysis(SILAnalysis::InvalidationKind K) {
