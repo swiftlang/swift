822aa300e85ea57d68cbe9929991f6ea31edd8e5
Joe Groff <jgroff@apple.com>
2014-04-18 01:07:12 +0000

    Remove BlockShims.mm.
    Swift SVN r16485

--- test/stdlib/block-shims.swift
+++
@@ -1,7 +0,0 @@
-// RUN: %swift-demangle `nm %libdir/swift/macosx/libswift_runtime.a | sed -n 's/^[0-9a-f]* T __TTbX/_TTbX/ p'` | FileCheck %s
-// REQUIRES: X86
-
-// We must be able to demangle every block shim in the runtime,
-// and there must be no unmangled suffixes.
-// CHECK-NOT: ---> _
-// CHECK-NOT: unmangled suffix
