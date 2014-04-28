183c3a3b101292f25f32ed746d6ba854a09aa741
Greg Parker <gparker@apple.com>
2014-04-28 22:27:55 +0000

    Revert r16776 (Distributed Objects unavailability).
    This created multiple definitions of the unavailable classes, which broke stuff.
    
    
    Swift SVN r16997

--- test/stdlib/DistributedObjects.swift
+++
@@ -1,7 +0,0 @@
-// RUN: %target-swift-frontend -parse -verify %s
-
-// Distributed Objects was never available on iOS.
-// REQUIRES: OS=macosx
-
-import Foundation
-var x : NSConnection  // expected-error {{'NSConnection' is unavailable}}
