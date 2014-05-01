b56392e643cea665a47197ab392262e972a5daca
Fariborz Jahanian <fjahanian@apple.com>
2014-05-01 21:44:00 +0000

    Overlay for -[NSPredicate predicateWithFormat:] and -[NSExpression expressionWithFormat:] // rdar://16726530
    Swift SVN r17186

---
+++ test/Interpreter/SDK/Foundation_NSExpression.swift
@@ -0,0 +1,12 @@
+// RUN: %target-run-simple-swift | FileCheck %s
+// rdar://16726530
+
+import Foundation
+
+// Test overlain variadic methods.
+let expression = NSExpression.expressionWithFormat("(3 + 2)**2", "LLLL", "BBBB")
+let result = expression.expressionValueWithObject(nil, context:nil) as NSNumber!
+let number = result.stringValue
+println(number)
+
+// CHECK: 25
---
+++ test/Interpreter/SDK/Foundation_NSPredicate.swift
@@ -0,0 +1,10 @@
+// RUN: %target-run-simple-swift | FileCheck %s
+// rdar://16726530
+
+import Foundation
+
+// Test overlain variadic methods.
+let s = NSPredicate.predicateWithFormat("(lastName like[cd] %@) AND (birthday > %@)", "LLLL", "BBBB")
+println(s.predicateFormat)
+
+// CHECK: lastName LIKE[cd] "LLLL" AND birthday > "BBBB"
