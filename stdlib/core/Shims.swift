63c5a145b450db002abf2f9fd7936303247784c8
Dmitri Hrybenko <dgribenko@apple.com>
2014-05-07 12:54:17 +0000

    stdlib: factor out _SwiftNSFastEnumerationState initialization
    Swift SVN r17624

---
+++ stdlib/core/Shims.swift
@@ -0,0 +1,23 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2015 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+///
+/// Additions to 'SwiftShims' that can be written in Swift.
+///
+//===----------------------------------------------------------------------===//
+
+import SwiftShims
+
+func _makeSwiftNSFastEnumerationState() -> _SwiftNSFastEnumerationState {
+  return _SwiftNSFastEnumerationState(state: 0, itemsPtr: nil, mutationsPtr: nil,
+      extra: (0, 0, 0, 0, 0))
+}
+
