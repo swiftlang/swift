e53e815651d0b1e0b37551996d56996cd81003e6
Doug Gregor <dgregor@apple.com>
2014-05-10 20:24:58 +0000

    Add missing ObjectiveC in SILGen's bridging SDK
    Swift SVN r17841

---
+++ test/SILGen/Inputs/usr/include/ObjectiveC.h
@@ -0,0 +1,26 @@
+typedef signed char BOOL;
+@class NSString;
+
+@protocol NSCopying
+- (id) copyWithZone: (void*)zone;
+@end
+
+@interface NSObject
++ (NSObject*) alloc;
+- (NSObject*) init;
++ (NSObject*) new;
++ (void) load;
+
+- (NSString*) description;
+
+@property NSString *stringProperty;
+
+@property Class classProp;
+@end
+
+#define NS_RETURNS_INNER_POINTER __attribute__((objc_returns_inner_pointer))
+#define NS_RETURNS_RETAINED __attribute__((ns_returns_retained))
+#define NS_CONSUMES_SELF __attribute__((ns_consumes_self))
+#define NS_CONSUMED __attribute__((ns_consumed))
+#define OBJC_DESIGNATED_INITIALIZER __attribute__((objc_designated_initializer))
+
