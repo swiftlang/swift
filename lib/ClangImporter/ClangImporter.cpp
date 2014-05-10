63ebd0ef8d6202c8f6d3770ee0b5bf67d1601d91
Argyrios Kyrtzidis <kyrtzidis@apple.com>
2014-05-10 22:31:29 +0000

    [ClangImporter] When creating the FileID for the header import buffer, set the main FileID as include location.
    SourceManager expects that SourceLocations have the main FileID as parent, except for the predefines buffer.
    Fixes assertion hit in a SourceKit test.
    
    Swift SVN r17851

--- lib/ClangImporter/ClangImporter.cpp
+++ lib/ClangImporter/ClangImporter.cpp
@@ -362,7 +362,12 @@ void ClangImporter::importHeader(StringRef header, Module *adapter) {
 
   clang::SourceManager &sourceMgr =
     Impl.getClangASTContext().getSourceManager();
-  clang::FileID bufferID = sourceMgr.createFileIDForMemBuffer(sourceBuffer);
+  clang::FileID bufferID =
+    sourceMgr.createFileIDForMemBuffer(sourceBuffer,
+                                       clang::SrcMgr::C_User,
+                                       /*LoadedID=*/0,
+                                       /*LoadedOffset=*/0,
+      /*IncludeLoc=*/sourceMgr.getLocForStartOfFile(sourceMgr.getMainFileID()));
 
   clang::Preprocessor &pp = Impl.getClangPreprocessor();
   pp.EnterSourceFile(bufferID, /*directoryLookup=*/nullptr, /*loc=*/{});
