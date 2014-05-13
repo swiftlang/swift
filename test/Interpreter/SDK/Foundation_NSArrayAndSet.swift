2fb6e3f8f174f19b34505090b006100534759ca3
Fariborz Jahanian <fjahanian@apple.com>
2014-05-13 19:40:11 +0000

    Test for // rdar://16726444
    Swift SVN r18017

---
+++ test/Interpreter/SDK/Foundation_NSArrayAndSet.swift
@@ -0,0 +1,23 @@
+// RUN: %target-run-simple-swift | FileCheck %s
+// rdar://16726444
+
+import Foundation
+
+var MyArray = NSArray(objects:"A", "B", "C", "D")
+print(MyArray)
+// CHECK: (
+// CHECK: A,
+// CHECK: B,
+// CHECK C,
+// CHECK D
+// CHECK: )
+
+var MySet = NSSet(objects:"a", "b", "c", "42")
+MySet = NSSet(objects:"a", "b", "c", "42")
+print(MySet)
+// CHECK:{(
+// CHECK:    b,
+// CHECK:    42,
+// CHECK:    c,
+// CHECK:    a
+// CHECK:)}
