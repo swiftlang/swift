ae49b8bee2056f3b0ea914accbf3e9ac79ef7620
Chris Lattner <clattner@apple.com>
2014-05-18 04:21:21 +0000

    add an executable test for weak pointers.  It is somewhat concerning to me that without the "w = nil" line that explicitly nil's out the weak pointer, that it fails. I'm not sure what is going on here, but it looks like weak pointers are just broken.
    Swift SVN r18307

---
+++ test/Interpreter/weak.swift
@@ -0,0 +1,43 @@
+// RUN: %target-run-simple-swift | FileCheck %s
+
+import ObjectiveC
+
+class SwiftClassBase {
+  func noop() { println("noop") }
+}
+
+class SwiftClass : SwiftClassBase {
+  init() {
+    println("SwiftClass Created")
+  }
+
+  deinit {
+    println("SwiftClass Destroyed")
+  }
+}
+
+func printState(x : SwiftClassBase?) {
+  println(x ? "is present" : "is nil")
+}
+
+func testSwiftClass() {
+  println("testSwiftClass")               // CHECK: testSwiftClass
+  
+  weak var w : SwiftClassBase?
+  if true {
+    printState(w)                           // CHECK-NEXT: is nil
+    var c : SwiftClassBase = SwiftClass()   // CHECK: SwiftClass Created
+    printState(w)                           // CHECK-NEXT: is nil
+    w = c
+    printState(w)                           // CHECK-NEXT: is present
+    c.noop()                                // CHECK-NEXT: noop
+    c = SwiftClassBase()                    // CHECK-NEXT: SwiftClass Destroyed
+    w = nil
+  }
+  printState(w)                           // CHECK-NEXT: is nil
+}
+
+testSwiftClass()
+
+
+
