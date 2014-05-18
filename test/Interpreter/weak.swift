a3b406a4c477186775937d9e4c10ac32df749737
Chris Lattner <clattner@apple.com>
2014-05-18 05:08:08 +0000

    previous patches fixed: <rdar://problem/16949978> weak implicitly unwrapped optional compiled as optional, not implicitly unwrapped
    add a testcase.
    
    
    
    Swift SVN r18317

--- test/Interpreter/weak.swift
+++ test/Interpreter/weak.swift
@@ -43,6 +43,25 @@ func testSwiftClass() {
 
 testSwiftClass()
 
+
+
+func testSwiftImplicitOptionalClass() {
+  println("testSwiftImplicitOptionalClass") // CHECK: testSwiftImplicitOptionalClass
+  
+  weak var w : SwiftClassBase!
+  printState(w)                           // CHECK-NEXT: is nil
+  var c : SwiftClassBase = SwiftClass()   // CHECK: SwiftClass Created
+  printState(w)                           // CHECK-NEXT: is nil
+  w = c
+  printState(w)                           // CHECK-NEXT: is present
+  c.noop()                                // CHECK-NEXT: noop
+  c = SwiftClassBase()                    // CHECK-NEXT: SwiftClass Destroyed
+  w = nil
+  printState(w)                           // CHECK-NEXT: is nil
+}
+
+testSwiftImplicitOptionalClass()
+
 //========================== Test ObjC classes ==========================
 
 @objc
