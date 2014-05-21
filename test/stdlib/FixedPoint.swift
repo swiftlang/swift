3fab925b773eb8ccc3ed6404bd9fad2866eb3da2
Dmitri Hrybenko <dgribenko@apple.com>
2014-05-21 11:59:01 +0000

    stdlib/BitwiseOperations: add a basic test
    Swift SVN r18492

--- test/stdlib/FixedPoint.swift
+++ test/stdlib/FixedPoint.swift
@@ -1,43 +1,29 @@
-// RUN: %swift -verify -parse %s
+// RUN: %target-run-simple-swift | FileCheck %s
 
-func testUnaryMinusInUnsigned() {
-  var a: UInt8 = -(1) // expected-error {{could not find an overload for '-' that accepts the supplied arguments}}
-  var b: UInt16 = -(1) // expected-error {{could not find an overload for '-' that accepts the supplied arguments}}
-  var c: UInt32 = -(1) // expected-error {{could not find an overload for '-' that accepts the supplied arguments}}
-  var d: UInt64 = -(1) // expected-error {{could not find an overload for '-' that accepts the supplied arguments}}
+func testBitwiseOperationsImpl<T : UnsignedInteger>(_: T.Type) {
+  var x = numericCast(0b11_1010_00) as T
+  var y = numericCast(0b10_1100_10) as T
+
+  assert(x & y == 0b10_1000_00)
+  assert(x | y == 0b11_1110_10)
+  assert(x ^ y == 0b01_0110_10)
+  assert((~x) & 0xff == 0b00_0101_11)
+
+  var z = T.allZeros
+  assert(x | z == x)
+  assert(x ^ z == x)
+  assert(x & z == z)
+  assert(x & ~z == x)
 }
 
-// Int and UInt are not identical to any fixed-size integer type
-var i   :  Int   = 0
-var w   :  Word  = i
-var i64 :  Int64 = i // expected-error {{'Int' is not convertible to 'Int64'}}
-var i32 :  Int32 = i // expected-error {{'Int' is not convertible to 'Int32'}}
-var i16 :  Int16 = i // expected-error {{'Int' is not convertible to 'Int16'}}
-var i8  :  Int8  = i // expected-error {{'Int' is not convertible to 'Int8'}}
+func testBitwiseOperations() {
+  testBitwiseOperationsImpl(UInt8.self)
+  testBitwiseOperationsImpl(UInt16.self)
+  testBitwiseOperationsImpl(UInt32.self)
+  testBitwiseOperationsImpl(UInt64.self)
 
-var u   : UInt   = 0
-var uw  : UWord  = u
-var u64 : UInt64 = u // expected-error {{'UInt' is not convertible to 'UInt64'}}
-var u32 : UInt32 = u // expected-error {{'UInt' is not convertible to 'UInt32'}}
-var u16 : UInt16 = u // expected-error {{'UInt' is not convertible to 'UInt16'}}
-var u8  : UInt8  = u // expected-error {{'UInt' is not convertible to 'UInt8'}}
-
-func testOps<T : IntegerArithmetic>(x: T, y: T) -> T {
-  let a = x + y
-  let s = x - y
-  let m = x * y
-  let d = x / y
-  let r = x % y
-  return a + s + m + d + r
+  println("testBitwiseOperations done")
 }
+testBitwiseOperations()
+// CHECK: testBitwiseOperations done
 
-let   s_ops:    Int = testOps(5, 2)
-let   u_ops:   UInt = testOps(5, 2)
-let  s8_ops:   Int8 = testOps(5, 2)
-let  u8_ops:  UInt8 = testOps(5, 2)
-let s16_ops:  Int16 = testOps(5, 2)
-let u16_ops: UInt16 = testOps(5, 2)
-let s32_ops:  Int32 = testOps(5, 2)
-let u32_ops: UInt32 = testOps(5, 2)
-let s64_ops:  Int64 = testOps(5, 2)
-let u64_ops: UInt64 = testOps(5, 2)
