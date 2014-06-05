649886b142bcf339ac99031a568129086ea7e121
Michael Gottesman <mgottesman@apple.com>
2014-06-05 22:20:43 +0000

    [g-arc-opts] Add a comment to clarify the purpose of BBToPostOrderID.
    This map is just for debugging purposes to give BBs an id # that is easy to
    track in the debug output.
    
    Swift SVN r18714

--- lib/SILAnalysis/GlobalARCSequenceDataflow.h
+++ lib/SILAnalysis/GlobalARCSequenceDataflow.h
@@ -141,7 +141,9 @@ class ARCSequenceDataflowEvaluator {
   /// reverse post order traversals.
   std::vector<SILBasicBlock *> PostOrder;
 
-  /// A map from SIL Basic Blocks to their index in the post order.
+  /// A map from SIL Basic Blocks to their index in the post order. This is just
+  /// to make it easier to debug the optimizer by enabling log print outs of the
+  /// BB# in the traversal.
   llvm::DenseMap<SILBasicBlock *, unsigned> BBToPostOrderID;
 
   /// A map mapping the head to a tail of a backedge. We only compute this once
