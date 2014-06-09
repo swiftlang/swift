02ef6d6554f6a4191ef7bec79433c1da5f048975
Jordan Rose <jordan_rose@apple.com>
2014-06-09 21:29:08 +0000

    Update the "missing SDK" note now that the compiler is almost always in Xcode.
    "xcrun -sdk macosx swift" is much easier than
    "swift -sdk $(xcrun --show-sdk-path --sdk macosx)"
    
    Swift SVN r18758

--- test/Misc/fatal_error.swift
+++ test/Misc/fatal_error.swift
@@ -2,7 +2,7 @@
 
 // CHECK: error: no such module 'NonExistent'
 // CHECK: note: did you forget to set an SDK using -sdk or SDKROOT?
-// CHECK-NEXT: use "-sdk $(xcrun --show-sdk-path --sdk macosx)" to select the default OS X SDK installed with Xcode
+// CHECK-NEXT: use "{{.+}}" to select the default OS X SDK installed with Xcode
 // CHECK-NOT: error
 // CHECK-NOT: warning
 // CHECK-NOT: note
