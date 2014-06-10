4880bceda264a56722dea379a991a7d53e465c89
Greg Parker <gparker@apple.com>
2014-06-10 02:43:26 +0000

    <rdar://17086151> Add overlays for open(), openat(), and struct stat.
    Swift SVN r18777

--- stdlib/objc/Darwin/Darwin.swift
+++ stdlib/objc/Darwin/Darwin.swift
@@ -12,6 +12,11 @@
 
 @exported import Darwin // Clang module
 
+
+//===----------------------------------------------------------------------===//
+// sys/errno.h
+//===----------------------------------------------------------------------===//
+
 var errno : Int32 {
   get {
     return __error().memory
@@ -21,6 +26,10 @@ var errno : Int32 {
   }
 }
 
+
+//===----------------------------------------------------------------------===//
+// stdio.h
+//===----------------------------------------------------------------------===//
 
 var stdin : UnsafePointer<FILE> {
   get {
@@ -49,6 +58,90 @@ var stderr : UnsafePointer<FILE> {
   }   
 }
 
+
+//===----------------------------------------------------------------------===//
+// sys/stat.h
+//===----------------------------------------------------------------------===//
+
+extension stat {
+  init() {
+    st_dev = 0; st_ino = 0; st_mode = 0; st_nlink = 0; 
+    st_uid = 0; st_gid = 0; st_rdev = 0; 
+    st_atimespec = timespec(tv_sec:0, tv_nsec:0); 
+    st_mtimespec = timespec(tv_sec:0, tv_nsec:0); 
+    st_ctimespec = timespec(tv_sec:0, tv_nsec:0); 
+    st_birthtimespec = timespec(tv_sec:0, tv_nsec:0); 
+    st_size = 0; st_blocks = 0; st_blksize = 0; st_flags = 0; st_gen = 0; 
+    st_lspare = 0; st_qspare = (0, 0)
+  }
+}
+
+
+//===----------------------------------------------------------------------===//
+// fcntl.h
+//===----------------------------------------------------------------------===//
+
+@asmname("_swift_Darwin_open") 
+func _swift_Darwin_open(path: CString, oflag: CInt, mode: mode_t) -> CInt
+@asmname("_swift_Darwin_openat") 
+func _swift_Darwin_openat(fd: CInt, path: CString, 
+  oflag: CInt, mode: mode_t) -> CInt
+
+func open(path: CString, oflag: CInt) -> CInt {
+  return _swift_Darwin_open(path, oflag, 0)
+}
+
+func open(path: CString, oflag: CInt, mode: mode_t) -> CInt {
+  return _swift_Darwin_open(path, oflag, mode)
+}
+
+func openat(fd: CInt, path: CString, oflag: CInt) -> CInt {
+  return _swift_Darwin_openat(fd, path, oflag, 0)
+}
+
+func openat(fd: CInt, path: CString, oflag: CInt, mode: mode_t) -> CInt {
+  return _swift_Darwin_openat(fd, path, oflag, mode)
+}
+
+
+let	S_IFMT   = mode_t(0o170000)
+let	S_IFIFO  = mode_t(0o010000)
+let	S_IFCHR  = mode_t(0o020000)
+let	S_IFDIR  = mode_t(0o040000)
+let	S_IFBLK  = mode_t(0o060000)
+let	S_IFREG  = mode_t(0o100000)
+let	S_IFLNK  = mode_t(0o120000)
+let	S_IFSOCK = mode_t(0o140000)
+let	S_IFWHT  = mode_t(0o160000)
+
+let	S_IRWXU  = mode_t(0o000700)
+let	S_IRUSR  = mode_t(0o000400)
+let	S_IWUSR  = mode_t(0o000200)
+let	S_IXUSR  = mode_t(0o000100)
+
+let	S_IRWXG  = mode_t(0o000070)
+let	S_IRGRP  = mode_t(0o000040)
+let	S_IWGRP  = mode_t(0o000020)
+let	S_IXGRP  = mode_t(0o000010)
+
+let	S_IRWXO  = mode_t(0o000007)
+let	S_IROTH  = mode_t(0o000004)
+let	S_IWOTH  = mode_t(0o000002)
+let	S_IXOTH  = mode_t(0o000001)
+
+let	S_ISUID  = mode_t(0o004000)
+let	S_ISGID  = mode_t(0o002000)
+let	S_ISVTX  = mode_t(0o001000)
+
+let	S_ISTXT  = S_ISVTX
+let	S_IREAD  = S_IRUSR
+let	S_IWRITE = S_IWUSR
+let	S_IEXEC  = S_IXUSR
+
+
+//===----------------------------------------------------------------------===//
+// unistd.h
+//===----------------------------------------------------------------------===//
 
 @availability(*, unavailable, message="Please use threads or posix_spawn*()")
 func fork() -> Int32 {
--- test/Interpreter/SDK/libc.swift
+++ test/Interpreter/SDK/libc.swift
@@ -1,9 +1,60 @@
-// RUN: %target-run-simple-swift | FileCheck %s
+/* magic */
+// Do not edit the line above.
+
+// RUN: rm -rf %t  &&  mkdir -p %t
+// RUN: %target-run-simple-swift %s %t | FileCheck %s
 
 import Darwin
+
+let sourcePath = Process.arguments[1]
+let tempPath = Process.arguments[2] + "/libc.txt"
 
 // CHECK: Hello world
 fputs("Hello world", stdout)
 
 // CHECK: 4294967295
 println("\(UINT32_MAX)")
+
+// CHECK: the magic word is /* magic */
+let sourceFile = 
+  sourcePath.withCString { (cstr: CString) -> CInt in
+    return open(cstr, O_RDONLY)
+  }
+assert(sourceFile >= 0)
+var bytes = UnsafePointer<CChar>.alloc(11)
+var readed = read(sourceFile, bytes, 11)
+close(sourceFile)
+assert(readed == 11)
+var magic = String.fromCString(bytes)
+println("the magic word is \(magic) //")
+
+// CHECK: O_CREAT|O_EXCL returned errno *17*
+let errFile = 
+  sourcePath.withCString { (cstr: CString) -> CInt in
+    return open(cstr, O_RDONLY | O_CREAT | O_EXCL)
+  }
+if errFile != -1 { 
+  println("O_CREAT|O_EXCL failed to return an error") 
+} else { 
+  println("O_CREAT|O_EXCL returned errno *\(errno)*") 
+}
+
+// CHECK: created mode *33216*
+let tempFile = 
+  tempPath.withCString { (cstr: CString) -> CInt in
+    return open(cstr, O_RDWR | O_CREAT | O_EXCL, S_IRUSR | S_IWUSR | S_IXUSR)
+  }
+let written = write(tempFile, bytes, 11)
+assert(written == 11)
+close(tempFile)
+var statbuf = stat()
+let err = tempPath.withCString { (cstr: CString) in
+  stat(cstr, &statbuf)
+}
+if err != 0 { 
+  println("stat returned \(err), errno \(errno)") 
+} else { 
+  println("created mode *\(statbuf.st_mode)*") 
+  assert(statbuf.st_mode == S_IFREG | S_IRUSR | S_IWUSR | S_IXUSR)
+}
+
