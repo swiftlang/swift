dcea62e04d97fd7035daf7f6e02514ed5042acb1
Jordan Rose <jordan_rose@apple.com>
2014-06-11 19:02:03 +0000

    [test] "mkdir -p %t" before putting a module cache directory inside it.
    Swift SVN r18805

--- test/ClangModules/clang_builtin.swift
+++ test/ClangModules/clang_builtin.swift
@@ -1,4 +1,4 @@
-// RUN: rm -rf %t/clang-module-cache
+// RUN: rm -rf %t/clang-module-cache && mkdir -p %t
 // RUN: %swift_driver -parse -Xfrontend -verify -module-cache-path %t/clang-module-cache -sdk %sdk -target %target-triple %s
 // RUN: ls -lR %t/clang-module-cache | FileCheck %s
 // CHECK: _Builtin_intrinsics{{.*}}.pcm
