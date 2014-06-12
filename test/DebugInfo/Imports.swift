34a3b8e9c7ea1691f7b8dbe213304ecd6cf567c9
Bob Wilson <bob.wilson@apple.com>
2014-06-12 19:48:44 +0000

    Adjust expected output to match current results on trunk.
    Swift SVN r18836

--- test/DebugInfo/Imports.swift
+++ test/DebugInfo/Imports.swift
@@ -17,9 +17,9 @@ println(basic.foo(1, 2))
 
 // DWARF: .debug_info
 // DWARF: DW_TAG_module
-// DWARF-NEXT: "Swift"
-// DWARF: DW_TAG_module
 // DWARF-NEXT: "Foo"
+// DWARF: DW_TAG_module
+// DWARF-NEXT: "Swift"
 // DWARF: DW_TAG_module
 // DWARF-NEXT: "basic"
 
