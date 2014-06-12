a5a5d8ffcdf0496e00c0d72e3c9533f897035b7f
Joe Groff <jgroff@apple.com>
2014-06-12 23:17:45 +0000

    Add an execution test that exercises metatype-to-object conversions.
    Swift SVN r18846

---
+++ test/Interpreter/SDK/class_as_object.swift
@@ -0,0 +1,23 @@
+// RUN: rm -rf %t  &&  mkdir %t
+// RUN: %target-build-swift -Xfrontend -enable-metatype-object-conversions %s -o %t/a.out
+// RUN: %target-run %t/a.out | FileCheck %s
+
+import Foundation
+
+let classes = NSMutableArray()
+classes.addObject(NSObject.self)
+classes.addObject(NSString.self)
+classes.addObject(NSNumber.self)
+
+for obj: AnyObject in classes {
+  println(obj.description)
+}
+// CHECK: NSObject
+// CHECK-NEXT: NSString
+// CHECK-NEXT: NSNumber
+
+println(NSObject.conformsToProtocol(NSCopying.self))
+// CHECK-NEXT: false
+println(NSString.conformsToProtocol(NSCopying.self))
+// CHECK-NEXT: true
+
