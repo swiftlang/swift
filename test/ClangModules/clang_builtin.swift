61688379ecf6f9a785724ad9f117ad78ac77b771
Greg Parker <gparker@apple.com>
2014-06-12 16:40:01 +0000

    Fix test clang_builtin.swift on iOS.
    Swift SVN r18826

--- test/ClangModules/clang_builtin.swift
+++ test/ClangModules/clang_builtin.swift
@@ -1,5 +1,5 @@
-// RUN: rm -rf %t/clang-module-cache && mkdir -p %t
-// RUN: %swift_driver -parse -Xfrontend -verify -module-cache-path %t/clang-module-cache -sdk %sdk -target %target-triple %s
+// RUN: rm -rf %t/clang-module-cache
+// RUN: %target-build-swift -parse -Xfrontend -verify -module-cache-path %t/clang-module-cache -sdk %sdk -target %target-triple %s
 // RUN: ls -lR %t/clang-module-cache | FileCheck %s
 // CHECK: _Builtin_intrinsics{{.*}}.pcm
 // REQUIRES: sdk
