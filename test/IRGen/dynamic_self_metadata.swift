3fa019f4e83b59eeaf3decfd2638d11e5c0cd8a6
Mark Lacey <mark.lacey@apple.com>
2014-06-17 04:07:00 +0000

    Remove outdated FIXME.
    This was fixed in r18918.
    
    Swift SVN r18947

--- test/IRGen/dynamic_self_metadata.swift
+++ test/IRGen/dynamic_self_metadata.swift
@@ -6,14 +6,10 @@
 class C {
   class func fromMetatype() -> Self? { return nil }
   // CHECK-LABEL: define i64 @_TFC21dynamic_self_metadata1C12fromMetatypefMDS0_FT_GSqDS0__(%swift.type*)
-  // -- FIXME: We should invoke the _Nil-to-T? (Sq) conversion here,
-  //    not _Nil-to-T! (SQ). <rdar://problem/16931392>
   // CHECK:         call void @_TFSq21convertFromNilLiteralU__fMGSqQ__FT_GSqQ__(%Sq.16* noalias sret {{%.*}}, %swift.type* %0)
 
   func fromInstance() -> Self? { return nil }
   // CHECK-LABEL: define i64 @_TFC21dynamic_self_metadata1C12fromInstancefDS0_FT_GSqDS0__(%C21dynamic_self_metadata1C*)
-  // -- FIXME: We should invoke the _Nil-to-T? (Sq) conversion here,
-  //    not _Nil-to-T! (SQ). <rdar://problem/16931392>
   // CHECK:         [[OBJECT:%.*]] = bitcast %C21dynamic_self_metadata1C* %0 to %objc_object*
   // CHECK:         [[TYPE:%.*]] = call %swift.type* @swift_getObjectType(%objc_object* [[OBJECT]])
   // CHECK:         call void @_TFSq21convertFromNilLiteralU__fMGSqQ__FT_GSqQ__(%Sq.16* noalias sret {{%.*}}, %swift.type* [[TYPE]]
