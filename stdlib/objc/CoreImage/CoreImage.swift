a4b945c4862159ec8cb8c1859aa6dfcb03f5342a
Fariborz Jahanian <fjahanian@apple.com>
2014-06-30 19:23:03 +0000

    Overlays for Core Image variadic methods. // rdar://16726822
    Swift SVN r19357

---
+++ stdlib/objc/CoreImage/CoreImage.swift
@@ -0,0 +1,25 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2015 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+@exported import CoreImage  // Clang module
+
+// sdk overlays:
+extension CIFilter {
+  convenience init(name: String!,
+                   elements: (NSCopying, AnyObject)...) {
+    var dict =  NSMutableDictionary()
+        for (key, value) in elements {
+            dict[key] = value
+        }
+    self.init(name: name, withInputParameters: dict)
+  } 
+}
---
+++ stdlib/objc/QuartzCore/QuartzCore.swift
@@ -0,0 +1,54 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2015 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+@exported import QuartzCore // Clang module
+
+// sdk overlays:
+extension CIFilter {
+  // - (CIImage *)apply:(CIKernel *)k, ...
+  // @objc(apply:arguments:options:)
+  // func apply(k: CIKernel!,
+  //            arguments args: [AnyObject]!,
+  //            options dict: Dictionary<NSObject, AnyObject>!) -> CIImage!
+  func apply(k: CIKernel!, args: [AnyObject]!, options: (NSCopying, AnyObject)...) -> CIImage {
+    var dict = NSMutableDictionary()
+    for (key, value) in options {
+      dict[key] = value
+    }
+    return self.apply(k, arguments: args, options: dict)
+  }
+
+  convenience init(name: String!,
+                   elements: (NSCopying, AnyObject)...) {
+    var dict =  NSMutableDictionary()
+        for (key, value) in elements {
+            dict[key] = value
+        }
+    self.init(name: name, withInputParameters: dict)
+  } 
+}
+
+// sdk overlays:
+extension CISampler {
+    // - (id)initWithImage:(CIImage *)im keysAndValues:key0, ...;
+    convenience init(im: CIImage!, elements: (NSCopying, AnyObject)...) {
+        var dict = NSMutableDictionary()
+        for (key, value) in elements {
+            dict[key] = value
+        }
+
+        // @objc(initWithImage:options:)
+        //   init(image im: CIImage!,
+        //        options dict: NSDictionary!)
+        self.init(image: im, options: dict)
+    }
+}
