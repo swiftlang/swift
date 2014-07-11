78fc120c279321c3d224252cc523a526cc99386a
Jordan Rose <jordan_rose@apple.com>
2014-07-11 17:22:49 +0000

    Add a KVO execution test for r19851.
    Swift SVN r19852

---
+++ test/Interpreter/SDK/KVO.swift
@@ -0,0 +1,42 @@
+// RUN: %target-run-simple-swift | FileCheck %s
+
+import Foundation
+
+var kvoContext = 0
+
+class Model : NSObject {
+  dynamic var name = ""
+  dynamic var number = 0
+}
+
+class Observer : NSObject {
+  let model = Model()
+
+  init() {
+    super.init()
+    model.addObserver(self, forKeyPath: "name", options: nil, context: &kvoContext)
+    self.addObserver(self, forKeyPath: "model.number", options: nil, context: &kvoContext)
+  }
+
+  deinit {
+    self.removeObserver(self, forKeyPath: "model.number")
+    model.removeObserver(self, forKeyPath: "name")
+  }
+
+  func test() {
+    model.name = "abc"
+    model.number = 42
+  }
+
+  override func observeValueForKeyPath(keyPath: String!, ofObject object: AnyObject!, change: [NSObject : AnyObject]!, context: UnsafePointer<Void>) {
+    if context != &kvoContext {
+      return super.observeValueForKeyPath(keyPath, ofObject: object, change: change, context: context)
+    }
+
+    println(object.valueForKeyPath(keyPath))
+  }
+}
+
+// CHECK: abc
+// CHECK-NEXT: 42
+Observer().test()
