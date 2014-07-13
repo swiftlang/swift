8b41540d8d9f7d6652dc363445e8ffd1a5074534
Dave Abrahams <dabrahams@apple.com>
2014-07-13 20:54:52 +0000

    [stdlib] Fix a typo Dmitri found
    Thanks, Dmitri!
    
    Swift SVN r19904

--- test/stdlib/RangeTraps.swift
+++ test/stdlib/RangeTraps.swift
@@ -20,7 +20,7 @@
 // RUN: %target-run %t/a.out_Debug OutOfRange 2>&1 | FileCheck %s
 // RUN: %target-run %t/a.out_Release HalfOpen 2>&1 | FileCheck %s
 // RUN: %target-run %t/a.out_Release Closed 2>&1 | FileCheck %s
-// RUN: %target-run %t/a.out_Debug OutOfRange 2>&1 | FileCheck %s
+// RUN: %target-run %t/a.out_Release OutOfRange 2>&1 | FileCheck %s
 
 // CHECK: OK
 // CHECK: CRASHED: SIG{{ILL|TRAP}}
