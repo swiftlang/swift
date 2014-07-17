bfef826414054bc528402c6374b55e2d69a62a38
Ben Langmuir <blangmuir@apple.com>
2014-07-17 03:26:34 +0000

    Combine CHECK-NOTs in print_stdlib test
    Swift SVN r20068

--- test/IDE/print_stdlib.swift
+++ test/IDE/print_stdlib.swift
@@ -1,48 +1,26 @@
 // Make sure we don't crash while printing the standard library.
 //
 // RUN: %swift-ide-test -print-module -module-to-print=Swift -source-filename %s -skip-private-stdlib-decls -fully-qualified-types-if-ambiguous -synthesize-sugar-on-types > %t.txt
+// RUN: FileCheck -check-prefix=CHECK-ARGC -input-file %t.txt %s
 // RUN: FileCheck -input-file %t.txt %s
-// RUN: %swift-ide-test -print-module -module-to-print=Swift -source-filename %s -module-print-submodules | FileCheck %s
-
-// CHECK: var C_ARGC: CInt
-
-// RUN: FileCheck -check-prefix=CHECK-IMPORT -input-file %t.txt %s
-// CHECK-IMPORT-NOT: {{^}}import
-
-// RUN: FileCheck -check-prefix=CHECK-TRANSPARENT -input-file %t.txt %s
-// CHECK-TRANSPARENT-NOT: @transparent
-
-// RUN: FileCheck -check-prefix=CHECK-PRIVATE1 -input-file %t.txt %s
-// CHECK-PRIVATE1-NOT: _Double
-
-// RUN: FileCheck -check-prefix=CHECK-PRIVATE2 -input-file %t.txt %s
-// CHECK-PRIVATE2-NOT: _StringBuffer
-
-// RUN: FileCheck -check-prefix=CHECK-PRIVATE3 -input-file %t.txt %s
-// CHECK-PRIVATE3-NOT: _StringCore
-
-// RUN: FileCheck -check-prefix=CHECK-PRIVATE4 -input-file %t.txt %s
-// CHECK-PRIVATE4-NOT: _ArrayBody
-
-// RUN: FileCheck -check-prefix=CHECK-PRIVATE5 -input-file %t.txt %s
-// CHECK-PRIVATE5-NOT: ~>
-
-// RUN: FileCheck -check-prefix=CHECK-PRIVATE6 -input-file %t.txt %s
-// CHECK-PRIVATE6-NOT: Builtin.
-
-// RUN: FileCheck -check-prefix=CHECK-PRIVATE7 -input-file %t.txt %s
-// CHECK-PRIVATE7-NOT: RawPointer
-
-// RUN: FileCheck -check-prefix=CHECK-ARRAY-SUGAR -input-file %t.txt %s
-// CHECK-ARRAY-SUGAR-NOT: extension [
-
-// RUN: FileCheck -check-prefix=CHECK-OPTIONAL-SUGAR -input-file %t.txt %s
-// CHECK-OPTIONAL-SUGAR-NOT: extension {{.*}}?
-
-// RUN: FileCheck -check-prefix=CHECK-IUO-SUGAR -input-file %t.txt %s
-// CHECK-IUO-SUGAR-NOT: extension {{.*}}!
-
 // RUN: FileCheck -check-prefix=CHECK-SUGAR -input-file %t.txt %s
+// RUN: %swift-ide-test -print-module -module-to-print=Swift -source-filename %s -module-print-submodules | FileCheck -check-prefix=CHECK-ARGC %s
+
+// CHECK-ARGC: var C_ARGC: CInt
+
+// CHECK-NOT: {{^}}import
+// CHECK-NOT: @transparent
+// CHECK-NOT: _Double
+// CHECK-NOT: _StringBuffer
+// CHECK-NOT: _StringCore
+// CHECK-NOT: _ArrayBody
+// CHECK-NOT: ~>
+// CHECK-NOT: Builtin.
+// CHECK-NOT: RawPointer
+// CHECK-NOT: extension [
+// CHECK-NOT: extension {{.*}}?
+// CHECK-NOT: extension {{.*}}!
+
 // CHECK-SUGAR: extension Array :
 // CHECK-SUGAR: extension ImplicitlyUnwrappedOptional :
 // CHECK-SUGAR: extension Optional :
