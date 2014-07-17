9fe6a9998606c68b0f3db8292c383ddbf1fc1732
Argyrios Kyrtzidis <kyrtzidis@apple.com>
2014-07-17 22:56:12 +0000

    [test] Add a test-specific module cache path for test/IDE/print_clang_foundation.swift.
    Swift SVN r20116

--- test/IDE/print_clang_foundation.swift
+++ test/IDE/print_clang_foundation.swift
@@ -1,18 +1,18 @@
 // RUN: rm -rf %t
 // RUN: mkdir -p %t
 
-// RUN: %swift-ide-test -print-module -source-filename %s -module-to-print=Foundation.NSArray -function-definitions=false > %t/Foundation.NSArray.printed.txt
+// RUN: %swift-ide-test -print-module -source-filename %s -module-to-print=Foundation.NSArray -function-definitions=false -module-cache-path=%t/mcp > %t/Foundation.NSArray.printed.txt
 // RUN: FileCheck -input-file %t/Foundation.NSArray.printed.txt -check-prefix=CHECK1 %s
 
 // CHECK1: class NSMutableArray : NSArray
 // CHECK1:   func setArray(otherArray: [AnyObject]!)
 
-// RUN: %swift-ide-test -print-module -source-filename %s -module-to-print=Foundation.NSKeyValueCoding -function-definitions=false -print-regular-comments > %t/Foundation.NSKeyValueCoding.printed.txt
+// RUN: %swift-ide-test -print-module -source-filename %s -module-to-print=Foundation.NSKeyValueCoding -function-definitions=false -print-regular-comments -module-cache-path=%t/mcp > %t/Foundation.NSKeyValueCoding.printed.txt
 // RUN: FileCheck -input-file %t/Foundation.NSKeyValueCoding.printed.txt -check-prefix=CHECK2 %s
 
 // CHECK2: extension NSObject
 
-// RUN: %swift-ide-test -print-module -source-filename %s -module-to-print=Foundation.NSString -function-definitions=false > %t/Foundation.NSString.printed.txt
+// RUN: %swift-ide-test -print-module -source-filename %s -module-to-print=Foundation.NSString -function-definitions=false -module-cache-path=%t/mcp > %t/Foundation.NSString.printed.txt
 // RUN: FileCheck -input-file %t/Foundation.NSString.printed.txt -check-prefix=CHECK_NSSTRING %s
 // RUN: FileCheck -input-file %t/Foundation.NSString.printed.txt -check-prefix=CHECK_DICTIONARY %s
 
