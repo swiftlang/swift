5c71e5075e1a68a6546b12a90ad462af667ce822
Dmitri Hrybenko <dgribenko@apple.com>
2014-07-21 08:44:40 +0000

    stdlib/String: add tests for trapping on out-of-range String.UTF16View indexes
    rdar://17539993
    
    
    Swift SVN r20246

--- test/stdlib/StringTraps.swift
+++ test/stdlib/StringTraps.swift
@@ -8,6 +8,8 @@
 // RUN: %target-run %t/a.out StringCharacterSubscriptEndIndex 2>&1 | FileCheck %s -check-prefix=CHECK
 // RUN: %target-run %t/a.out StringUTF8ViewEndIndexSuccessor 2>&1 | FileCheck %s -check-prefix=CHECK
 // RUN: %target-run %t/a.out StringUTF8ViewSubscriptEndIndex 2>&1 | FileCheck %s -check-prefix=CHECK
+// RUN: %target-run %t/a.out StringUTF16ViewSubscriptDecrementedStartIndex 2>&1 | FileCheck %s -check-prefix=CHECK
+// RUN: %target-run %t/a.out StringUTF16ViewSubscriptEndIndex 2>&1 | FileCheck %s -check-prefix=CHECK
 
 // CHECK: OK
 // CHECK: CRASHED: SIG{{ILL|TRAP|ABRT}}
@@ -64,6 +66,24 @@ if arg == "StringUTF8ViewSubscriptEndIndex" {
   ++i
   println("OK")
   s.utf8[i]
+}
+
+if arg == "StringUTF16ViewSubscriptDecrementedStartIndex" {
+  var s = "abc"
+  var i = s.utf16.startIndex
+  --i
+  println("OK")
+  s.utf16[i]
+}
+
+if arg == "StringUTF16ViewSubscriptEndIndex" {
+  var s = "abc"
+  var i = s.utf16.startIndex
+  ++i
+  ++i
+  ++i
+  println("OK")
+  s.utf16[i]
 }
 
 println("BUSTED: should have crashed already")
