c72fe37e4854e6f2e50b898d17490d182e3082b8
Jordan Rose <jordan_rose@apple.com>
2014-07-23 18:04:08 +0000

    [test] Drop the ability to set a custom 'ld'.
    It wasn't being honored by the Swift driver anyway. Use a custom PATH instead.
    
    Swift SVN r20420

--- test/DebugInfo/ASTSection_linker.swift
+++ test/DebugInfo/ASTSection_linker.swift
@@ -4,6 +4,6 @@
 // RUN: mkdir %t
 
 // RUN: %swift -c -emit-module -o %t %S/ASTSection.swift
-// RUN: %ld %t/ASTSection.o -sectcreate __SWIFT __ast %t/ASTSection.swiftmodule -o %t/ASTSection.dylib -L%libdir/swift/macosx -dylib -lSystem
+// RUN: ld %t/ASTSection.o -sectcreate __SWIFT __ast %t/ASTSection.swiftmodule -o %t/ASTSection.dylib -L%libdir/swift/macosx -dylib -lSystem
 // RUN: lldb-moduleimport-test %t/ASTSection.dylib | FileCheck %S/ASTSection.swift
 // REQUIRES: OS=macosx
