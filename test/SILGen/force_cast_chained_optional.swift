31611db815696d15bb83bbf89425c7c2bba63c3a
Joe Groff <jgroff@apple.com>
2014-07-23 04:11:29 +0000

    SILGen: Update test for implicit final propagation changes.
    Swift SVN r20369

--- test/SILGen/force_cast_chained_optional.swift
+++ test/SILGen/force_cast_chained_optional.swift
@@ -12,7 +12,7 @@ class C {}
 class D: C {}
 
 // CHECK-LABEL: sil @_TF27force_cast_chained_optional4testFCS_3FooCS_1D
-// CHECK:         class_method {{%.*}} : $Foo, #Foo.bar!getter.1
+// CHECK:         function_ref @_TFC27force_cast_chained_optional3Foog3barGSQCS_3Bar_
 // CHECK:         function_ref @_TFSs41_doesImplicitlyUnwrappedOptionalHaveValueU__FRGSQQ__Bi1_
 // CHECK:         cond_br {{%.*}}, [[SOME_BAR:bb[0-9]+]], [[NO_BAR:bb[0-9]+]]
 // CHECK:       [[TRAP:bb[0-9]+]]:
@@ -31,7 +31,7 @@ class D: C {}
 // CHECK:       [[NO_BAR_3]]:
 // CHECK:         br [[TRAP]]
 // CHECK:       [[SOME_BAR_3]]:
-// CHECK:         class_method {{%.*}} : $Bar, #Bar.bas!getter.1
+// CHECK:         function_ref @_TFC27force_cast_chained_optional3Barg3basGSQCS_1C_
 // CHECK:         function_ref @_TFSs36_getImplicitlyUnwrappedOptionalValueU__FGSQQ__Q_
 // CHECK:         unconditional_checked_cast {{%.*}} : $C to $D
 func test(x: Foo) -> D {
