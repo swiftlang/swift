c658caad36361bbf81ffb77a14ff879367b622dd
Anna Zaks <ganna@apple.com>
2014-07-28 18:42:13 +0000

    Test calling setters and getters of the API noted properties.
    Fixit for r20570
    
    Swift SVN r20635

---
+++ test/APINotes/irgen-prop-getter-setter.swift
@@ -0,0 +1,16 @@
+// RUN: rm -rf %t/clang-module-cache
+// RUN: %target-build-swift -Xfrontend %clang-importer-sdk -module-cache-path %t/clang-module-cache %s -emit-ir
+
+// Test that we don't crash when producing IR.
+
+import AppKit
+class MyView: NSView {
+    func drawRect() {
+        var x = self.superview
+        var l = self.layer
+        self.layer = CALayer()
+        self.nextKeyView = nil
+    }    
+}
+var m = MyView()
+m.drawRect()
