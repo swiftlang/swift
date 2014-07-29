657c53fb18c67805ed385863c1812c5b05434155
Doug Gregor <dgregor@apple.com>
2014-07-29 22:43:19 +0000

    Small test of name lookup behavior for extensions with generic parameters.
    Swift SVN r20707

--- test/decl/ext/generic.swift
+++ test/decl/ext/generic.swift
@@ -1,6 +1,6 @@
 // RUN: %swift -parse %s -verify
 
-protocol P1 { }
+protocol P1 { typealias AssocType }
 protocol P2 : P1 { }
 protocol P3 { }
 
@@ -21,3 +21,21 @@ extension X<A, B, C: P3> { } // expected-error{{extension of generic type 'X' ca
 // Bad: wrong number of generic parameters.
 extension X<T> { } // expected-error{{extension of generic type 'X' has too few generic parameters (have 1, expected 3)}}
 extension X<A, B, C, D> { } // expected-error{{extension of generic type 'X' has too many generic parameters (have 4, expected 3)}}
+
+// Name lookup of generic parameters.
+extension X<A, B, C> { // expected-error{{generic arguments are not allowed on an extension}}
+  // Okay: generic parameters from the extension.
+  func foo(x: A) -> (B, C) { }
+
+  // Okay: associated types of the generic parameters 
+  func bar(x: A.AssocType) { }
+
+  // Ill-formed: generic parameters from the extended type.
+  func wibble(x: T) { } // expected-error{{use of undeclared type 'T'}}
+}
+
+// Using generic extensions (basic).
+func f1<A, B, C>(x: X<A, B, C>, a: A, assoc: A.AssocType) {
+  var (b, c): (B, C) = x.foo(a)
+  x.bar(assoc)
+}
