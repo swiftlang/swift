fdcee9c501f7598d5a32fedb52842cf07a8e2036
Ben Langmuir <blangmuir@apple.com>
2014-07-30 22:09:28 +0000

    Clean up the command line interface for 'swift'
    In the interactive driver, disable a bunch of flags that only work with
    swiftc driver (NoInteractiveOption => disallowed, doesn't show up in
    help when invoked as 'swift').  Also move some options to HelpHidden
    (hidden from -h in both 'swift' and 'swiftc') that we don't need to
    advertise.
    
    Swift SVN r20780

--- test/Driver/options-interpreter.swift
+++ test/Driver/options-interpreter.swift
@@ -1,7 +1,7 @@
 // RUN: not %swift_driver -integrated-repl -emit-module 2>&1 | FileCheck -check-prefix=IMMEDIATE_NO_MODULE %s
 // RUN: not %swift_driver -emit-module 2>&1 | FileCheck -check-prefix=IMMEDIATE_NO_MODULE %s
 // REQUIRES: swift_interpreter
-// IMMEDIATE_NO_MODULE: this mode does not support emitting modules
+// IMMEDIATE_NO_MODULE: error: unsupported option '-emit-module'
 
 // RUN: %swift_driver -### %s | FileCheck -check-prefix INTERPRET %s
 // INTERPRET: -interpret
