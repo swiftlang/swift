76fc166cd507b773ec6cbf32a5581c7f441db494
Dmitri Hrybenko <dgribenko@apple.com>
2014-08-02 17:02:07 +0000

    stdlib: add missing file comment
    Swift SVN r20953

--- stdlib/unittest/InterceptTraps.cpp
+++ stdlib/unittest/InterceptTraps.cpp
@@ -1,3 +1,15 @@
+//===--- InterceptTraps.cpp -----------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2015 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
 #include <stdio.h>
 #include <signal.h>
 #include <string.h>
