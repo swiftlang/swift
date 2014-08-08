64a6a7393bd5bcfda50f993e918129463d63bcc4
John McCall <rjmccall@apple.com>
2014-08-08 02:52:42 +0000

    Include information about the Swift ABI version in the objc_imageinfo record.
    rdar://17528908
    
    Swift SVN r21104

--- test/IRGen/objc.swift
+++ test/IRGen/objc.swift
@@ -107,3 +107,11 @@ class WeakObjC {
     var bar: AnyObject? = id
   }
 }
+
+// rdar://17528908
+// CHECK: metadata !{i32 1, metadata !"Objective-C Version", i32 2}
+// CHECK: metadata !{i32 1, metadata !"Swift Version", i32 1}
+// CHECK: metadata !{i32 1, metadata !"Objective-C Image Info Version", i32 0}
+// CHECK: metadata !{i32 1, metadata !"Objective-C Image Info Section", metadata !"__DATA, __objc_imageinfo, regular, no_dead_strip"}
+//   512 == (1 << 8).  1 is the Swift ABI version.
+// CHECK: metadata !{i32 4, metadata !"Objective-C Garbage Collection", i32 512}
