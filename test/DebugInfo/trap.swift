614938400ed9fed7d9736959324f0e0e0eafb65f
Adrian Prantl <aprantl@apple.com>
2014-08-22 22:48:17 +0000

    Debug info: add a test for rdar://problem/16990007.
    Swift SVN r21426

---
+++ test/DebugInfo/trap.swift
@@ -0,0 +1,16 @@
+// RUN: %swift -parse-stdlib -target x86_64-apple-macosx10.9 %s -emit-ir -g -o - | FileCheck %s
+
+import Swift
+func f(x : Int) -> Int {
+  if x < 23 {
+    // CHECK-DAG: call void @llvm.trap(), !dbg ![[LOC1:.*]]
+    // CHECK-DAG: ![[LOC1]] = metadata !{i32 [[@LINE+1]],
+    Builtin.int_trap()
+  }
+  if x > 42 {
+    // CHECK-DAG: ![[LOC2:.*]] = metadata !{i32 [[@LINE+1]],
+    Builtin.int_trap()
+    // CHECK-DAG: call void @llvm.trap(), !dbg ![[LOC2]]
+  }
+  return x
+}
