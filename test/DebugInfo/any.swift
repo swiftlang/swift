bc128218efeaa8d672cb49abe8b18014adc3cfc8
Dmitri Hrybenko <dgribenko@apple.com>
2014-08-22 08:46:50 +0000

    Update tests for a change in debug info metadata format
    Swift SVN r21410

--- test/DebugInfo/any.swift
+++ test/DebugInfo/any.swift
@@ -2,7 +2,7 @@
 func main() {
   // CHECK: call void @llvm.dbg.declare(metadata !{%"protocol<>"* {{.*}}}, metadata ![[S:.*]]), !dbg ![[DBG:.*]]
   // CHECK: ![[S]] ={{.*}}[ DW_TAG_auto_variable ] [s] [line [[@LINE+3]]]
-  // CHECK: ![[SCOPE:.*]] = {{.*}}i32 2, i32 13, i32 0, i32 {{[0-9]+}}} ; [ DW_TAG_lexical_block ]
+  // CHECK: ![[SCOPE:.*]] = {{.*}}i32 2, i32 13, i32 0} ; [ DW_TAG_lexical_block ]
   // CHECK: ![[DBG]] = metadata !{i32 [[@LINE+1]], i32 6, metadata ![[SCOPE]], null}
 	var s : Any = "hello world"
 	var n : Any = 12
