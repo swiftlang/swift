fd28d2c62b457bb6e976af56a6fcc038486fb3af
Greg Parker <gparker@apple.com>
2014-08-26 23:25:56 +0000

    [test] Change the -Xllvm test to use more stable llvm options.
    Swift SVN r21467

--- test/Frontend/Xllvm.swift
+++ test/Frontend/Xllvm.swift
@@ -1,5 +1,5 @@
 // RUN: %swift -Xllvm -help 2>&1 | FileCheck %s --check-prefix=CHECK-HELP
-// CHECK-HELP: -fatal-assembler-warnings
+// CHECK-HELP: -version
 
-// RUN: %swift -Xllvm -fatal-assembler-warnings -emit-sil %s 2>&1 | FileCheck %s -check-prefix=CHECK-SIL
-// CHECK-SIL: top_level_code
+// RUN: %swift -Xllvm -version -emit-sil %s 2>&1 | FileCheck %s -check-prefix=CHECK-SIL
+// CHECK-SIL: LLVM (http://llvm.org/)
