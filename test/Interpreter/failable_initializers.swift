46be672d4f78ccdcf0803932f3487e7b6ee3e3d0
Joe Groff <jgroff@apple.com>
2014-08-26 18:33:19 +0000

    Execution tests for cleanup of partial value initializer failures.
    We get this for free from DI's support for struct/tuple destruction. Put it through its paces to make sure it works in failable value initializers.
    
    Swift SVN r21460

--- test/Interpreter/failable_initializers.swift
+++ test/Interpreter/failable_initializers.swift
@@ -7,9 +7,16 @@ class Canary {
 struct GuineaPig {
   let canary: Canary
 
+  init() { canary = Canary() }
+
   init?(live: Bool) {
     canary = Canary()
     if !live { return nil }
+  }
+
+  init?(failBefore: Bool) {
+    if failBefore { return nil }
+    canary = Canary()
   }
 }
 
@@ -21,6 +28,8 @@ if let x = GuineaPig(live: true) {
 }
 // CHECK-NEXT: died
 
+println("--") // CHECK-NEXT: --
+
 // CHECK-NEXT: died
 // CHECK-NEXT: it's dead
 if let y = GuineaPig(live: false) {
@@ -29,5 +38,111 @@ if let y = GuineaPig(live: false) {
   println("it's dead")
 }
 
+println("--") // CHECK-NEXT: --
+
+// CHECK-NEXT: it's alive
+// CHECK-NEXT: died
+if let a = GuineaPig(failBefore: false) {
+  println("it's alive")
+} else {
+  println("it's dead")
+}
+
+println("--") // CHECK-NEXT: --
+
+// CHECK-NEXT: it's dead
+if let b = GuineaPig(failBefore: true) {
+  println("it's alive")
+} else {
+  println("it's dead")
+}
+
+println("--") // CHECK-NEXT: --
+
+struct Chimera {
+  let canary: Canary
+  let guineaPig: GuineaPig
+
+  init?(failBefore: Bool) {
+    if failBefore { return nil }
+    canary = Canary()
+    guineaPig = GuineaPig()
+  }
+
+  init?(failBetween: Bool) {
+    canary = Canary()
+    if failBetween { return nil }
+    guineaPig = GuineaPig()
+  }
+
+  init?(failAfter: Bool) {
+    canary = Canary()
+    guineaPig = GuineaPig()
+    if failAfter { return nil }
+  }
+}
+
+// CHECK-NEXT: it's dead
+if let q = Chimera(failBefore: true) {
+  println("it's alive")
+} else {
+  println("it's dead")
+}
+
+println("--") // CHECK-NEXT: --
+
+// CHECK-NEXT: it's alive
+// CHECK-NEXT: died
+// CHECK-NEXT: died
+if let r = Chimera(failBefore: false) {
+  println("it's alive")
+} else {
+  println("it's dead")
+}
+
+println("--") // CHECK-NEXT: --
+
+// CHECK-NEXT: died
+// CHECK-NEXT: it's dead
+if let s = Chimera(failBetween: true) {
+  println("it's alive")
+} else {
+  println("it's dead")
+}
+
+println("--") // CHECK-NEXT: --
+
+// CHECK-NEXT: it's alive
+// CHECK-NEXT: died
+// CHECK-NEXT: died
+if let t = Chimera(failBetween: false) {
+  println("it's alive")
+} else {
+  println("it's dead")
+}
+
+println("--") // CHECK-NEXT: --
+
+// CHECK-NEXT: died
+// CHECK-NEXT: died
+// CHECK-NEXT: it's dead
+if let u = Chimera(failAfter: true) {
+  println("it's alive")
+} else {
+  println("it's dead")
+}
+
+println("--") // CHECK-NEXT: --
+
+// CHECK-NEXT: it's alive
+// CHECK-NEXT: died
+// CHECK-NEXT: died
+if let v = Chimera(failAfter: false) {
+  println("it's alive")
+} else {
+  println("it's dead")
+}
+
 // CHECK-NEXT: done
 println("done")
+
