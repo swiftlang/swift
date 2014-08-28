c782dc939f6ce53dc4980f2c7a947fe0e4f753aa
Jordan Rose <jordan_rose@apple.com>
2014-08-28 22:14:47 +0000

    [ClangImporter] FSEventStreamRef is not a CF object.
    Nor is ConstFSEventStreamRef.
    
    Unfortunately, they both get bridged to COpaquePointer, so they're not
    constructible from one another--they're identical.
    
    rdar://problem/18112747
    
    Swift SVN r21546

---
+++ test/Interpreter/SDK/misc_osx.swift
@@ -0,0 +1,16 @@
+// RUN: %target-build-swift -parse %s -Xfrontend -verify
+// REQUIRES: OS=macosx
+
+import CoreServices
+
+func testFSEventStreamRef(stream: FSEventStreamRef) {
+  // FIXME: These should be distinct types, constructible from one another.
+  let _: ConstFSEventStreamRef = stream // works by coincidence because both are currently COpaquePointer
+  let _ = ConstFSEventStreamRef(stream) // expected-error {{cannot invoke 'init' with an argument of type 'FSEventStreamRef'}}
+
+  // This is not a CF object.
+  FSEventStreamRetain(stream) // no-warning
+  FSEventStreamRelease(stream)
+
+  let _: AnyObject = stream // expected-error {{'FSEventStreamRef' does not conform to protocol 'AnyObject'}}
+}
