60bf3c14f2f9e2812548daa193d775a1db70b527
Dmitri Hrybenko <dgribenko@apple.com>
2014-08-28 15:11:44 +0000

    swift-bench: add standard license header
    Swift SVN r21531

--- utils/swift-bench.py
+++ utils/swift-bench.py
@@ -1,3 +1,15 @@
+##===--- swift-bench.py -------------------------------*- coding: utf-8 -*-===##
+##
+## This source file is part of the Swift.org open source project
+##
+## Copyright (c) 2014 - 2015 Apple Inc. and the Swift project authors
+## Licensed under Apache License v2.0 with Runtime Library Exception
+##
+## See http://swift.org/LICENSE.txt for license information
+## See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+##
+##===----------------------------------------------------------------------===##
+
 # This file implements a test harness for running Swift performance benchmarks.
 #
 # Its input is a set of swift files, containing functions named 'bench_*' that
