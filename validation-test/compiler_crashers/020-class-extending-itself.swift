aeef7f736f165fc107bdfc190bbebcbe19ca6c3b
Joe Pamer <jpamer@apple.com>
2014-09-10 00:05:29 +0000

    Now that we now longer crash, reinstate test.
    Swift SVN r21818

--- validation-test/compiler_crashers/020-class-extending-itself.swift
+++ validation-test/compiler_crashers/020-class-extending-itself.swift
@@ -2,8 +2,6 @@
 // Test case submitted to project by https://github.com/practicalswift (practicalswift)
 // http://www.openradar.me/17157251
 
-// XFAIL: asan
-
 // Case 1.
 class A: A { // expected-error {{circular class inheritance A}}
 }
