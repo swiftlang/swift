e3400d6aadd6fd2d7899e662f58072af630984cf
Adrian Prantl <aprantl@apple.com>
2014-09-12 18:18:47 +0000

    swift-mode.el: Add "private" "public" "internal" to the list of keywords.
    Swift SVN r21914

--- utils/swift-mode.el
+++ utils/swift-mode.el
@@ -37,7 +37,8 @@
    ;; Decl and type keywords
    `(,(regexp-opt '("class" "init" "deinit" "extension" "func"
                     "import" "let" "protocol" "static" "struct" "subscript"
-                    "typealias" "enum" "var" "where")
+                    "typealias" "enum" "var" "where"
+                    "private" "public" "internal")
                   'words) . font-lock-keyword-face)
    ;; Statements
    `(,(regexp-opt '("if" "in" "else" "for" "do" "while" "return" "break"
