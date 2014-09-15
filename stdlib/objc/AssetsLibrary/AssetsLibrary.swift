22b4dcda369b08fbf1a6119d9fe2094413327885
Anna Zaks <ganna@apple.com>
2014-09-15 22:10:51 +0000

    Create an overlay for ALAssetsLibrary:enumerateGroupsWithTypes which accepts ALAssetsGroup types.
    This is a case of type mismatch due to the parameter type not being a proper
    NS_OPTIONS enum. However, it crashes at runtime on 32 bit, so let's introduce an
    overlay while waiting for the proper fix.
    
    Addresses radar://18201112
    
    Swift SVN r21957

---
+++ stdlib/objc/AssetsLibrary/AssetsLibrary.swift
@@ -0,0 +1,32 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2015 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+@exported import AssetsLibrary // Clang module
+
+//===----------------------------------------------------------------------===//
+// ALAssetsLibrary.h
+//===----------------------------------------------------------------------===//
+extension ALAssetsLibrary { 
+  public func enumerateGroupsWithTypes(var types: UInt32, 
+      usingBlock enumerationBlock: ALAssetsLibraryGroupsEnumerationResultsBlock!,
+      failureBlock: ALAssetsLibraryAccessFailureBlock!) {
+    if (types == ALAssetsGroupAll) {
+      types = ALAssetsGroupLibrary | ALAssetsGroupAlbum | ALAssetsGroupEvent | 
+              ALAssetsGroupFaces | ALAssetsGroupSavedPhotos | 
+              ALAssetsGroupPhotoStream
+    }
+    return enumerateGroupsWithTypes(ALAssetsGroupType(types), 
+                                    usingBlock: enumerationBlock, 
+                                    failureBlock)
+
+  }
+}
---
+++ test/stdlib/AssetsLibrary.swift
@@ -0,0 +1,15 @@
+// RUN: %target-run-simple-swift
+// REQUIRES: OS=ios
+
+import AssetsLibrary
+
+// Test the enumerateGroupsWithTypes overload that accepts UInt32.
+// This should compile and not crash at runtime.
+
+let library = ALAssetsLibrary()
+library.enumerateGroupsWithTypes(ALAssetsGroupAll,
+  usingBlock: {(group: ALAssetsGroup!, stop: UnsafeMutablePointer<ObjCBool>) -> Void in
+                println("Swift usingBlock")},
+  failureBlock: {(error: NSError!) -> Void in
+                  println("Swift failureBlock")})
+
