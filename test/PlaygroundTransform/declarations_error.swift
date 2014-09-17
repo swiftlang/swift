1ae712e6bba84b4c54038136bc897d131b75bfe9
Jordan Rose <jordan_rose@apple.com>
2014-09-17 19:27:12 +0000

    Fix test broken in r22030.
    The special case for "debugger-but-not-playground" was a staging option for
    Xcode's benefit, but then we had to fix Xcode anyway.
    
    Swift SVN r22032

--- test/PlaygroundTransform/declarations_error.swift
+++ test/PlaygroundTransform/declarations_error.swift
@@ -1,7 +1,6 @@
 // RUN: rm -rf %t && mkdir %t
 // RUN: cp %s %t/main.swift
 // RUN: not %swift -parse -playground %t/main.swift 2>&1 | FileCheck %s
-// RUN: not %swift -parse -playground -debugger-support %t/main.swift 2>&1 | FileCheck %s
 
 // CHECK: error: no such module
 import Nonexistent_Module
