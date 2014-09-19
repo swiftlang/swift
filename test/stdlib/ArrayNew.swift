ae8c52969bc1c56d9b505e4d648378a77541fa63
Dmitri Hrybenko <dgribenko@apple.com>
2014-09-19 14:06:57 +0000

    StdlibUnittest: make testing return-autoreleased optimization reliable
    On some platforms (for example, x86_64), the first call to
    `objc_autoreleaseReturnValue` will always autorelease because it would
    fail to verify the instruction sequence in the caller.  On x86_64
    certain PLT entries would be still pointing to the resolver function,
    and sniffing the call sequence would fail.
    
    This change adds a "warmup" return-autoreleased sequence to the test
    harness.
    
    rdar://18385128
    
    Swift SVN r22127

--- test/stdlib/ArrayNew.swift
+++ test/stdlib/ArrayNew.swift
@@ -583,11 +583,6 @@ ArrayTestSuite.test("BridgedToObjC/Custom/BridgeBack/Cast") {
   expectEqual(idValue0, unsafeBitCast(a.objectAtIndex(0), UWord.self))
   expectEqual(idValue1, unsafeBitCast(a.objectAtIndex(1), UWord.self))
   expectEqual(idValue2, unsafeBitCast(a.objectAtIndex(2), UWord.self))
-
-#if !arch(arm64) && !arch(arm)
-  // FIXME: there should be no autoreleased objects.
-  expectAutoreleasedKeysAndValues(opt: (0, 1), unopt: (0, 3))
-#endif
 }
 
 ArrayTestSuite.test("BridgedToObjC/Custom/BridgeBack/Reallocate") {
