5b29b6ea9e5b43872a4f34157d88b2a00c224346
Joe Groff <jgroff@apple.com>
2014-09-28 18:41:09 +0000

    Revert "SILGen: Upcast existentials using open_existential instead of a bespoke instruction."
    This reverts commit r22332. It somehow exposes a crash bug in LLVM's ARMv7
    AsmPrinter.
    
    Swift SVN r22338

--- test/SILGen/upcast_existential.swift
+++ test/SILGen/upcast_existential.swift
@@ -11,12 +11,7 @@ func useP(x: P) { }
 
 // CHECK-LABEL: sil hidden @_TF18upcast_existential5PQtoPFT_T_ : $@thin () -> () {
 func PQtoP() {
-  // CHECK: [[PQ_PAYLOAD:%.*]] = open_existential [[PQ:%.*]]#1 : $*protocol<P, Q> to $*[[OPENED_TYPE:@opened(.*) protocol<P, Q>]]
-  // CHECK: [[P_PAYLOAD:%.*]] = init_existential [[P:%.*]]#1 : $*P, $[[OPENED_TYPE]]
-  // CHECK: copy_addr [take] [[PQ_PAYLOAD]] to [initialization] [[P_PAYLOAD]]
-  // CHECK-NOT: destroy_addr [[P]]
-  // CHECK-NOT: destroy_addr [[P_PAYLOAD]]
+  // CHECK: upcast_existential [take] [[PQ:%.*]]#1 : $*protocol<P, Q> to {{%.*}}#1 : $*P
   // CHECK-NOT: destroy_addr [[PQ]]
-  // CHECK-NOT: destroy_addr [[PQ_PAYLOAD]]
   useP(makePQ())
 }
