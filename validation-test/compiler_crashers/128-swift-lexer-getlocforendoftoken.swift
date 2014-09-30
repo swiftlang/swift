e3573b2fd2362d3cd7526d5e326ac980aeac33fa
Doug Gregor <dgregor@apple.com>
2014-09-30 22:32:31 +0000

    Drop expected-* annotations from test; it's too messy to -verify. NFC
    Swift SVN r22411

--- validation-test/compiler_crashers/128-swift-lexer-getlocforendoftoken.swift
+++ validation-test/compiler_crashers/128-swift-lexer-getlocforendoftoken.swift
@@ -4,7 +4,6 @@
 // RUN: not %swift %s -parse
 
 struct c<e> {
-    let d: [(  h // expected-note 4{{to match this opening}}
-      // expected-note @-1{{undeclared type}}
+    let d: [(  h
 }
 func b(g: f) -> <e>(()-> e) -> i
