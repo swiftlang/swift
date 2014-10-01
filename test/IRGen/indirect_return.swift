8541ffd006c629e12b645493f0c3b9969569c785
John McCall <rjmccall@apple.com>
2014-10-01 07:09:18 +0000

    Convert UnsafePointer's memory property and subscript operator to be addressed.
    Swift SVN r22424

--- test/IRGen/indirect_return.swift
+++ test/IRGen/indirect_return.swift
@@ -2,6 +2,8 @@
 
 // CHECK: define hidden void @_TF15indirect_return11generic_get
 func generic_get<T>(p: UnsafeMutablePointer<T>) -> T {
-  // CHECK: call void @_TFVSs20UnsafeMutablePointerg6memoryQ_(%swift.opaque* noalias sret %0, i8* %1, %swift.type* %T)
+  // CHECK: [[T0:%.*]] = call i8* @_TFVSs20UnsafeMutablePointerl6memoryQ_(i8* %1, %swift.type* %T)
+  // CHECK: [[T1:%.*]] = bitcast i8* {{%.*}} to %swift.opaque*
+  // CHECK: call %swift.opaque* {{%.*}}(%swift.opaque* %0, %swift.opaque* [[T1]], %swift.type* %T)
   return p.memory
 }
