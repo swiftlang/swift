204e9ee347976e0a321542aa0fb488171917bd07
Adrian Prantl <aprantl@apple.com>
2014-10-02 23:02:48 +0000

    Fix testcase for upstream IR printer change
    Swift SVN r22484

--- test/DebugInfo/closure-multivalue.swift
+++ test/DebugInfo/closure-multivalue.swift
@@ -1,9 +1,9 @@
 // RUN: %swift -target x86_64-apple-macosx10.9 -O %s -emit-ir -g -o - | FileCheck %s
 // CHECK-DAG: ![[A:.*]] = {{.*}} [ DW_TAG_arg_variable ] [a] [line 15]
 // CHECK-DAG: ![[B:.*]] = {{.*}} [ DW_TAG_arg_variable ] [b] [line 15]
-// CHECK-DAG: ![[P1:.*]] = {{.*}}; [ DW_TAG_expression ] [DW_OP_piece offset=0, size= 8]
-// CHECK-DAG: ![[P2:.*]] = {{.*}}; [ DW_TAG_expression ] [DW_OP_piece offset=8, size= 8]
-// CHECK-DAG: ![[P3:.*]] = {{.*}}; [ DW_TAG_expression ] [DW_OP_piece offset=16, size= 8]
+// CHECK-DAG: ![[P1:.*]] = {{.*}}; [ DW_TAG_expression ] [DW_OP_piece offset=0, size=8]
+// CHECK-DAG: ![[P2:.*]] = {{.*}}; [ DW_TAG_expression ] [DW_OP_piece offset=8, size=8]
+// CHECK-DAG: ![[P3:.*]] = {{.*}}; [ DW_TAG_expression ] [DW_OP_piece offset=16, size=8]
 // CHECK-DAG: call void @llvm.dbg.value({{.*}}, metadata ![[A]], metadata ![[P1]])
 // CHECK-DAG: call void @llvm.dbg.value({{.*}}, metadata ![[A]], metadata ![[P2]])
 // CHECK-DAG: call void @llvm.dbg.value({{.*}}, metadata ![[A]], metadata ![[P3]])
