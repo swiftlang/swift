c17c0606ea99c8be3b189bbb4a52d1e852a4d4bb
Joe Groff <jgroff@apple.com>
2014-10-02 21:51:42 +0000

    Verify that a stub initializer appears nowhere within the vtable.
    Swift SVN r22482

--- test/IRGen/class_with_stub_initializers.swift
+++ test/IRGen/class_with_stub_initializers.swift
@@ -3,10 +3,12 @@
 
 import Foundation
 
-// CHECK-LABEL: @_TMdC28class_with_stub_initializers3Foo = 
-// CHECK:         %C28class_with_stub_initializers3Foo* (i64, %C28class_with_stub_initializers3Foo*)*
+// CHECK:       @_TMd[[FOO:C28class_with_stub_initializers3Foo]] = 
 // -- The init() stub should get no vtable entry
-// CHECK-NOT:     %C28class_with_stub_initializers3Foo* (%C28class_with_stub_initializers3Foo*)*
+// CHECK-NOT:     %[[FOO]]* (%[[FOO]]*)*
+// CHECK:         %[[FOO]]* (i64, %[[FOO]]*)*
+// -- The init() stub should get no vtable entry
+// CHECK-NOT:     %[[FOO]]* (%[[FOO]]*)*
 class Foo: NSObject {
   init(x: Int) { super.init() }
 }
