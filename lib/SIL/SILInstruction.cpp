b4c06632e2de81da17f3d481d5f7cbd686922499
Michael Gottesman <mgottesman@apple.com>
2014-10-07 00:53:56 +0000

    Add a helper method SILInstruction::replaceAllUsesWithUndef().
    This should really be on ValueBase, but currently ValueBase does not
    have access to a SILModule.
    
    Swift SVN r22559

--- lib/SIL/SILInstruction.cpp
+++ lib/SIL/SILInstruction.cpp
@@ -138,6 +138,14 @@ void SILInstruction::dropAllReferences() {
   FRI->dropReferencedFunction();
 }
 
+void SILInstruction::replaceAllUsesWithUndef() {
+  SILModule &Mod = getModule();
+  while (!use_empty()) {
+    Operand *Op = *use_begin();
+    Op->set(SILUndef::get(Op->get().getType(), Mod));
+  }
+}
+
 namespace {
   class InstructionDestroyer : public SILVisitor<InstructionDestroyer> {
   public:
