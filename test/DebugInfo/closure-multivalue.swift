ac683e8d3def0705c3d6ec7381f904f6385458c7
Adrian Prantl <aprantl@apple.com>
2014-10-08 00:40:15 +0000

    Make this test a bit more deterministic. <rdar://problem/18559019> DebugInfo/closure-multivalue.swift failing with recent ToT changes
    Swift SVN r22590

--- test/DebugInfo/closure-multivalue.swift
+++ test/DebugInfo/closure-multivalue.swift
@@ -1,4 +1,4 @@
-// RUN: %swift -target x86_64-apple-macosx10.9 -O %s -emit-ir -g -o - | FileCheck %s
+// RUN: %swift -target x86_64-apple-macosx10.9 -O %s -disable-llvm-optzns -emit-ir -g -o - | FileCheck %s
 // CHECK-DAG: ![[A:.*]] = {{.*}} [ DW_TAG_arg_variable ] [a] [line 15]
 // CHECK-DAG: ![[B:.*]] = {{.*}} [ DW_TAG_arg_variable ] [b] [line 15]
 // CHECK-DAG: ![[P1:.*]] = {{.*}}; [ DW_TAG_expression ] [DW_OP_piece offset=0, size=8]
@@ -10,12 +10,16 @@
 // CHECK-DAG: call void @llvm.dbg.value({{.*}}, metadata ![[B]], metadata ![[P1]])
 // CHECK-DAG: call void @llvm.dbg.value({{.*}}, metadata ![[B]], metadata ![[P2]])
 // CHECK-DAG: call void @llvm.dbg.value({{.*}}, metadata ![[B]], metadata ![[P3]])
-func demo () {
+
+
+func sort(a : String, b : String) -> Bool {
+  println("Sorting..\(a) & \(b)")
+  return (a < b)
+}
+
+func demo() {
     var names = ["Sean", "Barry", "Kate"]
-    var sortedNames = sorted(names) {(a, b) in
-        println("Sorting..\(a) & \(b)")
-        return (a < b)
-    }
+    var sortedNames = sorted(names, sort)
     var sortedNamesAsString : String = String()
     for name in sortedNames {
         sortedNamesAsString += ("\(name), ")
