70fd4a4f1d76c9b5873edb66aa1d3198c9ee1ddd
Jordan Rose <jordan_rose@apple.com>
2014-10-10 19:03:48 +0000

    [test] Fix previous commit to pass -tbi instead of +tbi.
    Swift SVN r22673

--- test/Misc/tbi.swift
+++ test/Misc/tbi.swift
@@ -3,7 +3,7 @@
 // RUN:   FileCheck --check-prefix=BOTH --check-prefix=TBI %s
 
 // RUN: %target-build-swift -target arm64-apple-ios8.0 -target-cpu cyclone \
-// RUN:     -Xcc -Xclang -Xcc -target-feature -Xcc -Xclang -Xcc +tbi \
+// RUN:     -Xcc -Xclang -Xcc -target-feature -Xcc -Xclang -Xcc -tbi \
 // RUN:     -O -emit-ir %s -parse-as-library | \
 // RUN:   FileCheck --check-prefix=BOTH --check-prefix=NO_TBI %s
 
