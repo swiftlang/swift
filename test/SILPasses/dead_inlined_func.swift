99c09e07fcd6d6fba5b321f033067d0bb359db7e
Adrian Prantl <aprantl@apple.com>
2014-10-13 16:22:05 +0000

    Massage this testcase so it works on both trunk and 602.
    Swift SVN r22701

--- test/SILPasses/dead_inlined_func.swift
+++ test/SILPasses/dead_inlined_func.swift
@@ -5,11 +5,11 @@
 // The dead inlined function should not be in the SIL
 // CHECK-SIL-NOT: sil {{.*}}to_be_inlined
 
-
 // ... and also not in the llvm IR
 // CHECK-IR-NOT: define {{.*}}to_be_inlined
 
-// CHECK-IR: metadata !{metadata !"{{.*}}to_be_inlined{{.*}}"
+// But: we want debug info for it.
+// CHECK-IR: [ DW_TAG_subprogram ] {{.*}}to_be_inlined
 
 private func to_be_inlined(x: Int) -> Int {
 	return x + 1
