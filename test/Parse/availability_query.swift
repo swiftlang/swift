8f275da82a4620eb6a3d3100f8430ace288b0e77
Devin Coughlin <dcoughlin@apple.com>
2014-10-14 03:56:28 +0000

    [Parser] Update #os(...) parse tests for r22720.
    Swift SVN r22722

--- test/Parse/availability_query.swift
+++ test/Parse/availability_query.swift
@@ -10,28 +10,28 @@ if (#os(OSX >= 10.10)) {
 if #os { // expected-error {{expected availability condition}} expected-error {{braced block of statements is an unused closure}} expected-error {{statement cannot begin with a closure expression}} expected-note {{explicitly discard the result of the closure by assigning to '_'}} expected-error {{type of expression is ambiguous without more context}}
 }
 
-if #os( { // expected-error {{expected platform name}} expected-error {{expected ')'}} expected-note {{to match this opening '('}}
+if #os( { // expected-error {{expected platform name}} expected-error {{expected ')'}} expected-note {{to match this opening '('}} expected-error {{check can only be used as guard of if statement}}
 }
 
-if #os() { // expected-error {{expected platform name}}
+if #os() { // expected-error {{expected platform name}} expected-error {{check can only be used as guard of if statement}}
 }
 
-if #os(OSX { // expected-error {{expected version comparison}} expected-error {{expected ')'}} expected-note {{to match this opening '('}}
+if #os(OSX { // expected-error {{expected version comparison}} expected-error {{expected ')'}} expected-note {{to match this opening '('}} expected-error {{check can only be used as guard of if statement}}
 }
 
-if #os(OSX) { // expected-error {{expected version comparison}}
+if #os(OSX) { // expected-error {{expected version comparison}} expected-error {{check can only be used as guard of if statement}}
 }
 
-if #os(OSX >=  { // expected-error {{expected version number}} expected-error {{expected ')'}} expected-note {{to match this opening '('}} 
+if #os(OSX >=  { // expected-error {{expected version number}} expected-error {{expected ')'}} expected-note {{to match this opening '('}} expected-error {{check can only be used as guard of if statement}}
 }
 
-if #os(OSX >= ) { // expected-error {{expected version number}}
+if #os(OSX >= ) { // expected-error {{expected version number}} expected-error {{check can only be used as guard of if statement}}
 }
 
 if #os(OSX >= 10.10 { // expected-error {{expected ')'}} expected-note {{to match this opening '('}}
 }
 
-if #os(iDishwasherOS >= 10.10) { // expected-error {{unrecognized platform name 'iDishwasherOS'}}
+if #os(iDishwasherOS >= 10.10) { // expected-error {{unrecognized platform name 'iDishwasherOS'}} expected-error {{check can only be used as guard of if statement}}
 }
 
 
