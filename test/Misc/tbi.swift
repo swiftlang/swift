dbd286650bb5109eb7c51652b57c405e14fbb38b
Jordan Rose <jordan_rose@apple.com>
2014-10-15 19:28:13 +0000

    [test] Fix arm64 TBI test to emit assembly again (rather than IR).
    I mistakenly changed this in r22672 but the TBI pass only kicks in when
    lowering IR to a specific target.
    
    Swift SVN r22766

--- test/Misc/tbi.swift
+++ test/Misc/tbi.swift
@@ -1,11 +1,11 @@
 // RUN: %target-build-swift -target arm64-apple-ios8.0 -target-cpu cyclone \
-// RUN:                          -O -emit-ir %s -parse-as-library | \
-// RUN:   FileCheck --check-prefix=BOTH --check-prefix=TBI %s
+// RUN:                          -O -S %s -parse-as-library | \
+// RUN:   FileCheck --check-prefix=TBI %s
 
 // RUN: %target-build-swift -target arm64-apple-ios8.0 -target-cpu cyclone \
 // RUN:     -Xcc -Xclang -Xcc -target-feature -Xcc -Xclang -Xcc -tbi \
-// RUN:     -O -emit-ir %s -parse-as-library | \
-// RUN:   FileCheck --check-prefix=BOTH --check-prefix=NO_TBI %s
+// RUN:     -O -S %s -parse-as-library | \
+// RUN:   FileCheck --check-prefix=NO_TBI %s
 
 // REQUIRES: CPU=arm64, OS=ios
 
