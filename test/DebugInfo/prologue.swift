f80b3f52c7d25aaa9f60ff7b84ca8c7dee029ee0
Erik Eckstein <eeckstein@apple.com>
2014-10-16 08:13:06 +0000

    Make checks invariant to the file ordering in debug info test.
    Swift SVN r22793

--- test/DebugInfo/prologue.swift
+++ test/DebugInfo/prologue.swift
@@ -1,6 +1,7 @@
 // RUN: %swift -target x86_64-apple-macosx10.9 -primary-file %s -S -g -o - | FileCheck %s
+// CHECK: .file [[F:[0-9]+]] "prologue.swift"
 func bar<T, U>(x: T, y: U) { println("bar") }
-// CHECK: .loc	2 [[@LINE-1]] 3{{.}} prologue_end
+// CHECK: .loc	[[F]] [[@LINE-1]] 3{{.}} prologue_end
 // Make sure there is no allocation happening between the end of
 // prologue and the beginning of the function body.
 // CHECK-NOT: callq	*
