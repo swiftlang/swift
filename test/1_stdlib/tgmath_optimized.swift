f88190ab8baecc44b5e2c05e39881135889ba688
Arnold Schwaighofer <aschwaighofer@apple.com>
2014-10-21 23:52:37 +0000

    Reapply "stdlib: libm's sqrt and llvm.sqrt are not semantically equivalent""
    This reapplies commit r22864 - it is not changing the public api as we initially
    thought. sqrt() was never available without importing Darwin.
    
    This change only changes where sqrt() gets "forwarded" to. Before 'sqrt' called
    the builtin '_sqrt' defined in BuiltinMath now it just calls the math library's
    'sqrt' function.
    
    I also added a stdlib test.
    
    rdar://18371371
    
    Swift SVN r22870

---
+++ test/1_stdlib/tgmath_optimized.swift
@@ -0,0 +1,31 @@
+// RUN: mkdir -p %t
+// RUN: %target-build-swift %s -o %t/a.out -Ounchecked
+// RUN: %target-run %t/a.out
+
+
+import Darwin
+import StdlibUnittest
+
+var TGMathTestSuite = TestSuite("tgmath")
+
+let minusOneDouble = Double(-1.0)
+let minusOneFloat = Float(-1.0)
+
+@inline(never)
+func minusOneDoubleFunction() -> Double{
+  return minusOneDouble
+}
+
+@inline(never)
+func minusOneFloatFunction() -> Float {
+  return minusOneFloat
+}
+
+TGMathTestSuite.test("sqrt") {
+  expectTrue(isnan(sqrt(minusOneFloat)))
+  expectTrue(isnan(sqrt(minusOneFloatFunction())))
+  expectTrue(isnan(sqrt(minusOneDouble)))
+  expectTrue(isnan(sqrt(minusOneDoubleFunction())))
+}
+
+runAllTests()
--- test/IRGen/builtin_math.swift
+++ test/IRGen/builtin_math.swift
@@ -2,13 +2,34 @@
 
 import Darwin
 
-// Make sure we use an intrinsic for functions such as sqrt
-// CHECK: call float @llvm.sqrt.f32
+// Make sure we use an intrinsic for functions such as exp.
+
+// CHECK-LABEL: define {{.*}}test1
+// CHECK: call float @llvm.exp.f32
+
 public func test1(f : Float) -> Float {
-  return sqrt(f)
+  return exp(f)
 }
 
-// CHECK: call double @llvm.sqrt.f64
+// CHECK-LABEL: define {{.*}}test2
+// CHECK: call double @llvm.exp.f64
+
 public func test2(f : Double) -> Double {
-  return _sqrt(f)
+  return _exp(f)
+}
+
+// LLVM's sqrt intrinsic does not have the same semantics as libm's sqrt.
+
+// CHECK-LABEL: define {{.*}}test3
+// CHECK: call double @sqrt
+
+public func test3(d : Double) -> Double {
+  return sqrt(d)
+}
+
+// CHECK-LABEL: define {{.*}}test4
+// CHECK: call float @sqrtf
+
+public func test4(f : Float) -> Float {
+  return sqrt(f)
 }
