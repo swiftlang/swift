ce99c049380746ba8d052b359c5c3eb2acf5b628
Dave Abrahams <dabrahams@apple.com>
2014-10-24 15:48:47 +0000

    [stdlib] mv files NSSwiftXXX => SwiftNativeNSXXX
    Swift SVN r22912

--- stdlib/core/SwiftNativeNSArray.swift
+++ stdlib/core/SwiftNativeNSArray.swift
@@ -1,4 +1,4 @@
-//===--- NSSwiftArray.swift - Links NSArray and _ContiguousArrayStorage ---===//
+//===--- SwiftNativeNSArray.swift -----------------------------------------===//
 //
 // This source file is part of the Swift.org open source project
 //
