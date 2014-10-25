44ca2ea31f4324794f47a50b529218c6d9c4d327
Dmitri Hrybenko <dgribenko@apple.com>
2014-10-25 08:26:28 +0000

    Runtime: suppress BridgeObject metadata on platforms without ObjC interop
    Swift SVN r22942

--- stdlib/runtime/KnownMetadata.cpp
+++ stdlib/runtime/KnownMetadata.cpp
@@ -135,11 +135,11 @@ const ExtraInhabitantsValueWitnessTable swift::_TWVMBo =
 /// The basic value-witness table for ObjC object pointers.
 const ExtraInhabitantsValueWitnessTable swift::_TWVBO =
   ValueWitnessTableForBox<ObjCRetainableBox>::table;
-#endif
 
 /// The value-witness table for BridgeObject.
 const ExtraInhabitantsValueWitnessTable swift::_TWVBb =
   ValueWitnessTableForBox<BridgeObjectBox>::table;
+#endif
 
 /*** Functions ***************************************************************/
 
@@ -168,9 +168,9 @@ OPAQUE_METADATA(Bi32_)
 OPAQUE_METADATA(Bi64_)
 OPAQUE_METADATA(Bi128_)
 OPAQUE_METADATA(Bo)
-OPAQUE_METADATA(Bb)
 #if SWIFT_OBJC_INTEROP
 OPAQUE_METADATA(BO)
+OPAQUE_METADATA(Bb)
 #endif
 
 /// The standard metadata for the empty tuple.
