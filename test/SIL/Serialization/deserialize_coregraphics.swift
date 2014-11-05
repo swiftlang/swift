51b74c3be482bcbb10ee85448b83b962ea390207
Dmitri Hrybenko <dgribenko@apple.com>
2014-11-05 04:18:07 +0000

    Run deserialize_coregraphics.swift on iOS and simulator as well
    Swift SVN r23111

--- test/SIL/Serialization/deserialize_coregraphics.swift
+++ test/SIL/Serialization/deserialize_coregraphics.swift
@@ -2,6 +2,5 @@
 // RUN: %sil-opt -verify %libdir/swift/macosx/CoreGraphics.swiftmodule -module-name CoreGraphics -sdk %sdk -target %target-triple > /dev/null
 // RUN: llvm-bcanalyzer %libdir/swift/macosx/CoreGraphics.swiftmodule | FileCheck %s
 // REQUIRES: sdk
-// REQUIRES: OS=macosx
 
 // CHECK-NOT: Unknown
