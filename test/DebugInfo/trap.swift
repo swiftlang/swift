3667f0701da83d70f774cec0751f5d1b0c971ce5
Adrian Prantl <aprantl@apple.com>
2014-11-07 20:54:57 +0000

    Add a test case for <rdar://problem/17308698>
    Swift SVN r23162

--- test/DebugInfo/trap.swift
+++ test/DebugInfo/trap.swift
@@ -1,6 +1,7 @@
 // RUN: %swift -parse-stdlib -target x86_64-apple-macosx10.9 -primary-file %s -emit-ir -g -o - | FileCheck %s
 
 import Swift
+// CHECK: define{{.*}}1f
 func f(x : Int) -> Int {
   if x < 23 {
     // CHECK-DAG: call void @llvm.trap(), !dbg ![[LOC1:.*]]
