f3c093b24167d47b383c1d9a9f91708b116162c5
Adrian Prantl <aprantl@apple.com>
2014-11-11 01:30:51 +0000

    Add a testcase for arithmetic traps in optimized code.
    Swift SVN r23225

---
+++ test/DebugInfo/trap-optimized.swift
@@ -0,0 +1,14 @@
+// RUN: %swift -O -target x86_64-apple-macosx10.9 -primary-file %s -emit-ir -g -o - | FileCheck %s
+
+// CHECK: define{{.*}}2fn
+func fn() {
+  println("two")
+  println(0 - UInt(Process.arguments.count))
+  println("three")
+}
+// All traps should be coalesced at the end.
+// CHECK: ret
+// CHECK-NOT: }
+// CHECK: tail call void @llvm.trap(), !dbg ![[LOC:.*]]
+// CHECK-NEXT: unreachable, !dbg ![[LOC]]
+// CHECK: ![[LOC]] = metadata !{i32 0, i32 0,
