dbcdfff7247853f30e4e200d9bc66bd28fc9f848
Nadav Rotem <nrotem@apple.com>
2014-11-15 07:07:38 +0000

    Remove the locks from getGenericMetadata to accelerate Debug builds.
    This commit removes the locks from the family of getXXXXMetadata APIs in the
    fast path that does not create a new metadata. We still need the lock for the
    cache-miss case because constructing metadata has side effects and we must
    not construct two metadatas at the same time.
    
    I am seeing a 25% - 30% boost in performance on most workloads that are built
    with -Onone.
    
    Swift SVN r23353

--- stdlib/runtime/MetadataCache.h
+++ stdlib/runtime/MetadataCache.h
@@ -12,9 +12,9 @@
 #ifndef SWIFT_RUNTIME_METADATACACHE_H
 #define SWIFT_RUNTIME_METADATACACHE_H
 
-#include "llvm/ADT/DenseMap.h"
-#include "Locks.h"
 #include "llvm/ADT/Hashing.h"
+#include "Locks.h"
+#include "Concurrent.h"
 #include <mutex>
 #include <condition_variable>
 
@@ -38,8 +38,6 @@ class EntryRef {
   EntryRef(const void * const *args, unsigned length) :
     args(args), length(length) {}
 
-  friend struct llvm::DenseMapInfo<EntryRef>;
-
 public:
   static EntryRef forEntry(const Entry *e, unsigned numArguments) {
     return EntryRef(e->getArgumentsBuffer(), numArguments);
@@ -52,6 +50,18 @@ public:
 
   const Entry *getEntry() const {
     return Entry::fromArgumentsBuffer(args, length);
+  }
+
+  bool operator==(EntryRef<Entry>& rhs) const {
+    // Compare the sizes.
+    unsigned asize = size(), bsize = rhs.size();
+    if (asize != bsize) return false;
+
+    // Compare the content.
+    auto abegin = begin(), bbegin = rhs.begin();
+    for (unsigned i = 0; i < asize; ++i)
+      if (abegin[i] != bbegin[i]) return false;
+    return true;
   }
 
   const void * const *begin() const { return args; }
@@ -117,59 +127,36 @@ public:
   }
 };
 
-
 /// The implementation of a metadata cache.  Note that all-zero must
 /// be a valid state for the cache.
 template <class Entry> class MetadataCache {
-  /// Synchronization.
-  ///
-  /// Readers: acquire EntriesLock for reading.
-  ///
-  /// Writers and waiters: Acquire InsertionLock first.
-  /// Use InsertionWaiters to wait for another thread to complete an entry.
-  /// Acquire EntriesLock for writing second when modifying the Entries table.
-  ///
-  /// We need a mutex in addition to the rwlock to make the
-  /// condition variable work.
-  struct MetadataCacheLock {
-    RWMutex EntriesLock;
-    std::mutex InsertionLock;
-    std::condition_variable InsertionWaiters;
+
+  struct EntryPair {
+    EntryPair(EntryRef<Entry> K, Entry* V) : Key(K), Value(V) {}
+    EntryRef<Entry> Key;
+    Entry* Value;
   };
-  MetadataCacheLock *Lock;
+  ConcurrentMap<llvm::hash_code, ConcurrentList<EntryPair>> Map;
+
+  /// Synchronization of metadata creation.
+  std::mutex Lock;
 
   /// The head of a linked list connecting all the metadata cache entries.
   /// TODO: Remove this when LLDB is able to understand the final data
   /// structure for the metadata cache.
   const Entry *Head;
 
-  enum class EntryState : uint8_t { Complete, Building, BuildingWithWaiters };
-
-  /// The lookup table for cached entries.
-  ///
-  /// The EntryRef may be to temporary memory lacking a full backing
-  /// Entry unless the value is Complete.  However, if there is an
-  /// entry with a non-Complete value, there will eventually be a
-  /// notification to the lock's wait list.
-  ///
-  /// TODO: Consider a more tuned hashtable implementation.
-  typedef llvm::DenseMap<EntryRef<Entry>, EntryState> EntriesMapType;
-  typedef typename EntriesMapType::iterator EntriesIteratorType;
-  EntriesMapType Entries;
-
 public:
-  MetadataCache() : Lock(new MetadataCacheLock()) {}
-  ~MetadataCache() { delete Lock; }
+  MetadataCache() : Lock() {}
+  ~MetadataCache() { }
 
   /// Caches are not copyable.
   MetadataCache(const MetadataCache &other) = delete;
   MetadataCache &operator=(const MetadataCache &other) = delete;
 
-  /// Look up a cached metadata entry.
-  /// If a cache match exists, return it.
+  /// Look up a cached metadata entry. If a cache match exists, return it.
   /// Otherwise, call entryBuilder() and add that to the cache.
-  const Entry *findOrAdd(const void * const *arguments,
-                         size_t numArguments,
+  const Entry *findOrAdd(const void * const *arguments, size_t numArguments,
                          std::function<Entry *()> entryBuilder) {
 
 #if SWIFT_DEBUG_RUNTIME
@@ -180,164 +167,52 @@ public:
     }
 #endif
 
-    auto key = EntryRef<Entry>::forArguments(arguments, numArguments);
+    EntryRef<Entry> key = EntryRef<Entry>::forArguments(arguments,numArguments);
+    llvm::hash_code hash = llvm::hash_combine_range(key.begin(), key.end());
+
+#if SWIFT_DEBUG_RUNTIME
+    printf("%s(%p): generated hash %llx\n",
+           Entry::getName(), this, hash);
+#endif
 
     // Look for an existing entry.
-    {
-      ScopedReader readGuard(Lock->EntriesLock);
-      auto found = Entries.find(key);
-      if (found != Entries.end()  &&  found->second == EntryState::Complete) {
-#if SWIFT_DEBUG_RUNTIME
-        printf("%s(%p): found %p already in cache\n",
-               Entry::getName(), this, found->first.getEntry());
-#endif
-        return found->first.getEntry();
-      }
+    // Find the bucket for the metadata entry.
+    ConcurrentList<EntryPair> &Bucket = Map.findOrAllocateNode(hash);
+    // Scan the bucket and return the value if we found the key.
+    for (auto &A : Bucket) {
+      if (A.Key == key) return A.Value;
     }
 
-    // No complete entry found. Insert a new entry or wait for the
-    // existing one to complete.
-
+    // We did not find a key so we will need to create one and store it.
     {
-      std::unique_lock<std::mutex> insertionGuard(Lock->InsertionLock);
+      std::unique_lock<std::mutex> ConstructionGuard(Lock);
 
-      // Try to insert a placeholder so other threads know a new entry
-      // is under construction.
-      std::pair<typename decltype(Entries)::iterator, bool> found;
-      {
-        ScopedWriter writeGuard(Lock->EntriesLock);
-        found = Entries.insert({key, EntryState::Building});
-      }
-      // We no longer hold EntriesLock but `found` remains valid
-      // while we still hold InsertionLock.
-
-      auto it = found.first;
-      bool inserted = found.second;
-
-      if (it->second == EntryState::Complete) {
-        // Some other thread built the entry already. Return it.
-#if SWIFT_DEBUG_RUNTIME
-        printf("%s(%p): found %p already in cache after losing writer race\n",
-               Entry::getName(), this, it->first.getEntry());
-#endif
-        return it->first.getEntry();
+      // Some other thread may have setup the value we are about to construct
+      // while we were asleep so do a search before constructing a new value.
+      for (auto &A : Bucket) {
+        if (A.Key == key) return A.Value;
       }
 
-      if (!inserted) {
-        // Some other thread is currently building the entry.
-        // Wait for it to complete, then return it.
-
-        // Tell the builder that we're here.
-        it->second = EntryState::BuildingWithWaiters;
-
-        // Wait until the entry's state goes to Complete.
-        while (it->second != EntryState::Complete) {
-          Lock->InsertionWaiters.wait(insertionGuard);
-
-          // We dropped InsertionLock so don't trust the existing iterator.
-          it = Entries.find(key);
-          assert(it != Entries.end());
-        }
-
-#if SWIFT_DEBUG_RUNTIME
-        printf("%s(%p): found %p already in cache after waiting\n",
-               Entry::getName(), this, it->first.getEntry());
-#endif
-        return it->first.getEntry();
-      }
-    }
-
-    // Placeholder insertion successful.
-
-    // Build the new cache entry.
-    // For some cache types this call may re-entrantly perform additional
-    // cache lookups.
-    Entry *entry = entryBuilder();
-    assert(entry);
-
-    // Insert the new cache entry.
-    bool shouldNotify;
-    const Entry *result;
-    {
-      std::unique_lock<std::mutex> insertionGuard(Lock->InsertionLock);
+      // Build the new cache entry.
+      // For some cache types this call may re-entrantly perform additional
+      // cache lookups.
+      Entry *entry = entryBuilder();
+      assert(entry);
 
       // Update the linked list.
       entry->Next = Head;
       Head = entry;
 
-      // Find our placeholder.
-      // We hold InsertionLock so nobody can modify the table underneath us.
-      auto it = Entries.find(key);
-      assert(it != Entries.end());
-
-      // Replace the placeholder entry with the real data.
-      {
-        // Acquire EntriesLock to keep readers out while we update.
-        ScopedWriter writeGuard(Lock->EntriesLock);
-
-        // The existing key is a reference to the (probably stack-based)
-        // arguments array, so overwrite it.  Maps don't normally allow
-        // their keys to be overwritten, and doing so isn't officially
-        // allowed, but supposedly it is unofficially guaranteed to
-        // work, at least with the standard containers.
-        key = EntryRef<Entry>::forEntry(entry, entry->getNumArguments());
-        assert(it == Entries.find(key));
-        const_cast<EntryRef<Entry>&>(it->first) = key;
-
-        // Mark the entry as Complete.
-        assert(it->second != EntryState::Complete);
-        shouldNotify = (it->second == EntryState::BuildingWithWaiters);
-        it->second = EntryState::Complete;
-        result = it->first.getEntry();
-      }
-    }
-
-    // Notify any threads that might be waiting for the entry we just built.
-    if (shouldNotify) {
-      Lock->InsertionWaiters.notify_all();
-    }
+      key = EntryRef<Entry>::forEntry(entry, entry->getNumArguments());
+      Bucket.push(EntryPair(key, entry));
 
 #if SWIFT_DEBUG_RUNTIME
-    printf("%s(%p): created %p %p\n",
-           Entry::getName(), this, entry, result);
+      printf("%s(%p): created %p\n",
+             Entry::getName(), this, entry);
 #endif
-    return result;
-  }
-};
-
-namespace llvm {
-template<class Entry>
-struct DenseMapInfo<EntryRef<Entry>> {
-  static inline EntryRef<Entry> getEmptyKey() {
-    // {nullptr, 0} is a legitimate "no arguments" representation.
-    return {(const void * const *)UINTPTR_MAX, 1};
-  }
-
-  static inline EntryRef<Entry> getTombstoneKey() {
-    return {(const void * const *)UINTPTR_MAX, 2};
-  }
-
-  static inline unsigned getHashValue(EntryRef<Entry> val) {
-    llvm::hash_code hash
-      = llvm::hash_combine_range(val.begin(), val.end());
-    return (unsigned)hash;
-  }
-
-  static inline bool isEqual(EntryRef<Entry> a, EntryRef<Entry> b) {
-    unsigned asize = a.size(), bsize = b.size();
-    if (asize != bsize)
-      return false;
-    auto abegin = a.begin(), bbegin = b.begin();
-    if (abegin == (const void * const *)UINTPTR_MAX
-        || bbegin == (const void * const *)UINTPTR_MAX)
-      return abegin == bbegin;
-    for (unsigned i = 0; i < asize; ++i) {
-      if (abegin[i] != bbegin[i])
-        return false;
+      return key.getEntry();
     }
-    return true;
   }
 };
-}
 
 #endif // SWIFT_RUNTIME_METADATACACHE_H
