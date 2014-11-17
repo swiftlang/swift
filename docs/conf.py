0606251fc4218ce56e4da2081aadad17799199c5
Jordan Rose <jordan_rose@apple.com>
2014-11-17 19:42:00 +0000

    [docs] Use the same theme as our internal docs.
    In particular, this uses normal HTML bullets instead of images so that
    rich text copy/paste works correctly.
    
    Swift SVN r23376

--- docs/conf.py
+++ docs/conf.py
@@ -92,6 +92,8 @@ pygments_style = 'friendly'
 # a list of builtin themes.
 html_theme = 'haiku'
 
+html_style = 'swift.css'
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
