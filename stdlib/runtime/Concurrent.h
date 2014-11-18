9bf2d0dae3c838dd9ef60fc59f8e0291bc3e7c24
Nadav Rotem <nrotem@apple.com>
2014-11-18 03:20:32 +0000

    Update the -*- C++ -*- marker
    Swift SVN r23397

--- stdlib/runtime/Concurrent.h
+++ stdlib/runtime/Concurrent.h
@@ -1,4 +1,4 @@
-//===--- Concurrent.h - Concurrent Data Structures  -----------------------===//
+//===--- Concurrent.h - Concurrent Data Structures  ------------*- C++ -*--===//
 //
 // This source file is part of the Swift.org open source project
 //
