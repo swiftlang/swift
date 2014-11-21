007204a59559f6ead725718e3b5af9b1e3de1ea2
Chris Willmore <cwillmore@apple.com>
2014-11-21 23:27:40 +0000

    Crash tests 0730, 0911, 0916, 1660, 2027, 2203 no longer crash.
    Swift SVN r23541

--- validation-test/compiler_crashers/0911-no-stacktrace.swift
+++ validation-test/compiler_crashers/0911-no-stacktrace.swift
@@ -1,5 +1,5 @@
 // RUN: rm -rf %t/clang-module-cache
-// RUN: not --crash %target-swift-frontend %s -module-cache-path %t/clang-module-cache -parse
+// RUN: not %target-swift-frontend %s -module-cache-path %t/clang-module-cache -parse
 
 // Distributed under the terms of the MIT license
 // Test case submitted to project by https://github.com/practicalswift (practicalswift)
