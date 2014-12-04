b73d1e68f1a82b27dedb1fd9cf267c346f048d1b
Jordan Rose <jordan_rose@apple.com>
2014-12-04 00:35:07 +0000

    [test] Rewrite @NSManaged test to use the regular mock SDK instead of SILGen's.
    Swift SVN r23669

--- test/decl/var/NSManaged_properties.swift
+++ test/decl/var/NSManaged_properties.swift
@@ -1,8 +1,7 @@
 // RUN: rm -rf %t/clang-module-cache
-// RUN: %swift -module-cache-path %t/clang-module-cache -target x86_64-apple-macosx10.9 -sdk %S/../../SILGen/Inputs %s -I %S/../../SILGen/Inputs -enable-source-import -parse -verify
+// RUN: %swift -module-cache-path %t/clang-module-cache %clang-importer-sdk -parse %s -verify
 
 import Foundation
-import gizmo
 
 @objc class X {
   func foo() -> X { return self }
@@ -13,7 +12,7 @@ import gizmo
 @NSManaged     // expected-error {{'NSManaged' may only be used on 'var' declarations}}
 func managedFunction() {}
 
-class SwiftGizmo : Gizmo {
+class SwiftGizmo : A {
   @NSManaged var a: X
   @NSManaged var b: Int
   @NSManaged let c: Int  // expected-error {{'NSManaged' attribute not allowed on a 'let' property}}
