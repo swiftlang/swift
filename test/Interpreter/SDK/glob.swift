4456da757e98eac37be6cea8892008e57dec48de
Joe Groff <jgroff@apple.com>
2014-12-09 00:20:47 +0000

    Include "glob_t" test from rdar://problem/17263740.
    Swift SVN r23797

---
+++ test/Interpreter/SDK/glob.swift
@@ -0,0 +1,22 @@
+// RUN: %target-run-simple-swift | FileCheck %s
+
+import Foundation
+
+func Glob(g: String) -> Array<String> {
+	var gt = glob_t()
+	var rv = glob_b((g as NSString).UTF8String, 0, nil, &gt)
+
+	if rv == 0 {
+		var result = Array<String>()
+		for x in 0..<Int(gt.gl_pathc) {
+			var str = String.fromCString(gt.gl_pathv[x])
+			result.append(str!)
+		}
+		globfree(&gt)
+		return result
+	}
+	return []
+}
+
+// CHECK: [{{.*}}]
+println(Glob("/*"))
