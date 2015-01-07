ac30c67fd9c9da1e46c54b90a91472dad7dd270b
Flash Sheridan <flash@apple.com>
2015-01-07 06:37:43 +0000

    Revert the risky part of the change until I can investigate a bot failure report.  The error below sounds like a genuine bug, though not one that I saw, but other errors didn't seem connected to my change.
    /Users/buildslave/jenkins/workspace/swift_tools-RA_stdlib-RD/megaclang/src/tools/swift/validation-test/stdlib/NumericDiagnostics.swift.gyb:29:33: error: expected diagnostic not produced
          x1_UInt8 * x2_Float80  // expected-error{{ }}
    
    
    Swift SVN r24238

--- utils/SwiftIntTypes.py
+++ utils/SwiftIntTypes.py
@@ -78,7 +78,7 @@ def all_integer_type_names():
     return [self_ty.stdlib_name for self_ty in all_integer_types(0)]
 
 def all_real_number_type_names():
-    return ['Float', 'Double', 'Float80']  # Float80 for i386 & x86_64
+    return ['Float', 'Double'] #FIXME , 'Float80' Revert until I figure out a test failure  # Float80 for i386 & x86_64
 
 def all_numeric_type_names():
     return all_integer_type_names() + all_real_number_type_names()
