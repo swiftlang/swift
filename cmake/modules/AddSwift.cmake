387418d03444bd727b1d22a169d8d7fcd632dfad
Mark Lacey <mark.lacey@apple.com>
2015-01-10 01:09:31 +0000

    Document the need to reenable the SLP vectorizer for the stdlib.
    Swift SVN r24345

--- cmake/modules/AddSwift.cmake
+++ cmake/modules/AddSwift.cmake
@@ -334,6 +334,10 @@ function(_compile_swift_files dependency_target_out_var_name)
     list(APPEND swift_flags "-Xfrontend" "-emit-sorted-sil")
   endif()
 
+  # FIXME: Remove -disable-llvm-slp-vectorizer
+  #        (rdar://problem/19433841) once the underlying LLVM issue is
+  #        resolved (rdar://problem/19430955).
+
   # FIXME: Cleaner way to do this?
   if(SWIFTFILE_IS_STDLIB_CORE)
     list(APPEND swift_flags
