6604bf93ffa7c16decd455abd1d2e98ea0b29c5c
Doug Gregor <dgregor@apple.com>
2015-01-14 00:10:23 +0000

    Replace an overly promiscuous FileCheck test
    Swift SVN r24407

--- test/IRGen/objc_bridge.swift
+++ test/IRGen/objc_bridge.swift
@@ -82,9 +82,9 @@ import Foundation
 // CHECK:       i8* bitcast (void ([[OPAQUE:.*]]*, i8*)* @_TToFC11objc_bridge3BasD to i8*)
 // CHECK:     },
 // CHECK:     { i8*, i8*, i8* } {
-// CHECK:       i8* getelementptr inbounds ([11 x i8]* @"\01L_selector_data(acceptSet:)", i64 0, i64 0), 
-// CHECK:       i8* getelementptr inbounds ([11 x i8]* @{{.*}}, i64 0, i64 0), 
-// CHECK:       i8* bitcast (void (%3*, i8*, %4*)* @_TToFC11objc_bridge3Bas9acceptSetfS0_FGVSs3SetS0__T_ to i8*) 
+// CHECK:       i8* getelementptr inbounds ([11 x i8]* @"\01L_selector_data(acceptSet:)", i64 0, i64 0),
+// CHECK:       i8* getelementptr inbounds ([11 x i8]* @{{[0-9]+}}, i64 0, i64 0),
+// CHECK:       i8* bitcast (void (%3*, i8*, %4*)* @_TToFC11objc_bridge3Bas9acceptSetfS0_FGVSs3SetS0__T_ to i8*)
 // CHECK:     }
 // CHECK:     { i8*, i8*, i8* } { 
 // CHECK:       i8* getelementptr inbounds ([14 x i8]* @"\01L_selector_data(.cxx_destruct)", i64 0, i64 0), 
