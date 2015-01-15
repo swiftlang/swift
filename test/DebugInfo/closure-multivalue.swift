1229413d96ad9de8972f83e71f8b637ba9a99d0d
Adrian Prantl <aprantl@apple.com>
2015-01-15 22:55:26 +0000

    Massage this testcase so it works with a debug stdlib, too.
    Swift SVN r24458

--- test/DebugInfo/closure-multivalue.swift
+++ test/DebugInfo/closure-multivalue.swift
@@ -1,16 +1,16 @@
 // RUN: %swift -Xllvm -debug-values-propagate-liveness -target x86_64-apple-macosx10.9 -O %s -disable-llvm-optzns -emit-ir -g -o - | FileCheck %s
-// CHECK-DAG: ![[A:.*]] = {{.*}} [ DW_TAG_arg_variable ] [a] [line 15]
-// CHECK-DAG: ![[B:.*]] = {{.*}} [ DW_TAG_arg_variable ] [b] [line 15]
-// CHECK-DAG: ![[P1:.*]] = {{.*}}; [ DW_TAG_expression ] [DW_OP_piece offset=0, size=8]
-// CHECK-DAG: ![[P2:.*]] = {{.*}}; [ DW_TAG_expression ] [DW_OP_piece offset=8, size=8]
-// CHECK-DAG: ![[P3:.*]] = {{.*}}; [ DW_TAG_expression ] [DW_OP_piece offset=16, size=8]
-// CHECK-DAG: call void @llvm.dbg.value({{.*}}, metadata ![[A]], metadata ![[P1]])
-// CHECK-DAG: call void @llvm.dbg.value({{.*}}, metadata ![[A]], metadata ![[P2]])
-// CHECK-DAG: call void @llvm.dbg.value({{.*}}, metadata ![[A]], metadata ![[P3]])
-// CHECK-DAG: call void @llvm.dbg.value({{.*}}, metadata ![[B]], metadata ![[P1]])
-// CHECK-DAG: call void @llvm.dbg.value({{.*}}, metadata ![[B]], metadata ![[P2]])
-// CHECK-DAG: call void @llvm.dbg.value({{.*}}, metadata ![[B]], metadata ![[P3]])
-
+// CHECK: define {{.*}}4main4sort
+// CHECK: call void @llvm.dbg.value({{.*}}, metadata ![[A:.*]], metadata ![[P1:.*]])
+// CHECK: call void @llvm.dbg.value({{.*}}, metadata ![[A]], metadata ![[P2:.*]])
+// CHECK: call void @llvm.dbg.value({{.*}}, metadata ![[A]], metadata ![[P3:.*]])
+// CHECK: call void @llvm.dbg.value({{.*}}, metadata ![[B:.*]], metadata ![[P1]])
+// CHECK: call void @llvm.dbg.value({{.*}}, metadata ![[B]], metadata ![[P2]])
+// CHECK: call void @llvm.dbg.value({{.*}}, metadata ![[B]], metadata ![[P3]])
+// CHECK-DAG: ![[A]] = {{.*}} [ DW_TAG_a{{.*}}_variable ] [a] [line 15]
+// CHECK-DAG: ![[B]] = {{.*}} [ DW_TAG_a{{.*}}_variable ] [b] [line 15]
+// CHECK-DAG: ![[P1]] = {{.*}}; [ DW_TAG_expression ] [DW_OP_piece offset=0, size=8]
+// CHECK-DAG: ![[P2]] = {{.*}}; [ DW_TAG_expression ] [DW_OP_piece offset=8, size=8]
+// CHECK-DAG: ![[P3]] = {{.*}}; [ DW_TAG_expression ] [DW_OP_piece offset=16, size=8]
 
 func sort(a : String, b : String) -> Bool {
   println("Sorting..\(a) & \(b)")
