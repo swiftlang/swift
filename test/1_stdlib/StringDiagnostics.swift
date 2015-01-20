40884c703040aecfbe80dd79622b86b03ca0474d
Maxwell Swadling <mswadling@apple.com>
2015-01-20 23:31:33 +0000

    [stdlib] added tests for string operators flipped
    Swift SVN r24575

--- test/1_stdlib/StringDiagnostics.swift
+++ test/1_stdlib/StringDiagnostics.swift
@@ -19,4 +19,11 @@ func testAmbiguousStringComparisons(s: String) {
   let a4 = s <= nsString // expected-error{{'<=' is unavailable: Comparing Swift.String and NSString is ambiguous}}
   let a5 = s >= nsString // expected-error{{'>=' is unavailable: Comparing Swift.String and NSString is ambiguous}}
   let a6 = s > nsString // expected-error{{'>' is unavailable: Comparing Swift.String and NSString is ambiguous}}
+  // now the other way
+  let a7 = nsString == s // expected-error{{'==' is unavailable: Comparing Swift.String and NSString is ambiguous}}
+  let a8 = nsString != s // expected-error{{'!=' is unavailable: Comparing Swift.String and NSString is ambiguous}}
+  let a9 = nsString < s // expected-error{{'<' is unavailable: Comparing Swift.String and NSString is ambiguous}}
+  let a10 = nsString <= s // expected-error{{'<=' is unavailable: Comparing Swift.String and NSString is ambiguous}}
+  let a11 = nsString >= s // expected-error{{'>=' is unavailable: Comparing Swift.String and NSString is ambiguous}}
+  let a12 = nsString > s // expected-error{{'>' is unavailable: Comparing Swift.String and NSString is ambiguous}}
 }
