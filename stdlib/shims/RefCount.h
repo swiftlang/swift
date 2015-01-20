80a46cdae23b0c95ced9c0340577bcea6da89577
Arnold Schwaighofer <aschwaighofer@apple.com>
2015-01-20 17:00:12 +0000

    RefCount: Use rotate right in isUniquelyReferencedOrPinned
    This generates more efficient code.
    
    Swift SVN r24555

--- stdlib/shims/RefCount.h
+++ stdlib/shims/RefCount.h
@@ -165,7 +165,13 @@ class StrongRefCount {
   // is set.  During deallocation the reference count is undefined.
   bool isUniquelyReferencedOrPinned() const {
     auto value = __atomic_load_n(&refCount, __ATOMIC_RELAXED);
-    return (value >> RC_FLAGS_COUNT == 1 || value & RC_PINNED_FLAG);
+    // Rotating right by one sets the sign bit to the pinned bit.
+    // The dealloc flag is the least significant bit followed by the reference
+    // count. A reference count of two or higher means that our value is bigger
+    // than 3 if the pinned bit is not set. If the pinned bit is set the value
+    // is negative.
+    auto rotateRightByOne = ((value >> 1) | (value << (sizeof(value) * 8 - 1)));
+    return ((int32_t)rotateRightByOne) < 4;
   }
 
   // Return true if the object is inside deallocation.
