bd1adebdd8715ac3978c4b4d2caac8310999ce88
Dmitri Hrybenko <dgribenko@apple.com>
2015-01-21 08:19:10 +0000

    Adjust DebugInfo test for new version strings
    Swift SVN r24598

--- test/DebugInfo/sdk.swift
+++ test/DebugInfo/sdk.swift
@@ -1,5 +1,5 @@
 // Check that the sdk and resource dirs end up in the debug info.
 // RUN: %target-swiftc_driver %s -emit-ir -g -o - | FileCheck %s
 // RUN: %target-swiftc_driver %s -emit-ir -sdk "/Weird Location/SDK" -g -o - | FileCheck --check-prefix CHECK-EXPLICIT %s
-// CHECK:          \00Swift version {{.*}}\000{{.*}} -resource-dir {{.*}}"
-// CHECK-EXPLICIT: \00Swift version {{.*}}\000{{.*}} -sdk \22/Weird Location/SDK\22{{.*}} -resource-dir {{.*}}"
+// CHECK:          \00{{(Apple )?}}Swift version {{.*}}\000{{.*}} -resource-dir {{.*}}"
+// CHECK-EXPLICIT: \00{{(Apple )?}}Swift version {{.*}}\000{{.*}} -sdk \22/Weird Location/SDK\22{{.*}} -resource-dir {{.*}}"
