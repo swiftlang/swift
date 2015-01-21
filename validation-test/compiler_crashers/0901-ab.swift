d379fd20509e64ab5ad2f4ed44a4ed26cd9bbec0
Maxwell Swadling <mswadling@apple.com>
2015-01-21 23:52:37 +0000

    updated fixed test assertion
    Swift SVN r24617

--- validation-test/compiler_crashers/0901-ab.swift
+++ validation-test/compiler_crashers/0901-ab.swift
@@ -1,5 +1,7 @@
 // RUN: not --crash %target-swift-frontend %s -parse
 
+// XFAIL: no_asserts
+
 // Distributed under the terms of the MIT license
 // Test case submitted to project by https://github.com/practicalswift (practicalswift)
 // Test case found by fuzzing
