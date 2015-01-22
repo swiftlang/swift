9f5d295a81864dce668cefcf39087a820b006ea3
Dmitri Hrybenko <dgribenko@apple.com>
2015-01-22 02:53:32 +0000

    Make test/Serialization/target-incompatible.swift more portable
    It does not require an OS X standard library.
    
    Swift SVN r24629

--- test/Serialization/target-incompatible.swift
+++ test/Serialization/target-incompatible.swift
@@ -1,12 +1,12 @@
 // RUN: rm -rf %t && mkdir -p %t
 
-// RUN: %swift -target x86_64-unknown-darwin14 -o %t/mips-template.swiftmodule -emit-module -module-name mips %S/../Inputs/empty.swift
+// RUN: %swift -target x86_64-unknown-darwin14 -o %t/mips-template.swiftmodule -parse-stdlib -emit-module -module-name mips %S/../Inputs/empty.swift
 // RUN: %S/Inputs/binary_sub.py x86_64-unknown-darwin14 mips64-unknown-darwin14 < %t/mips-template.swiftmodule > %t/mips.swiftmodule
-// RUN: not %target-swift-frontend -I %t -parse %s -DMIPS 2>&1 | FileCheck -check-prefix=CHECK-MIPS %s
+// RUN: not %target-swift-frontend -I %t -parse -parse-stdlib %s -DMIPS 2>&1 | FileCheck -check-prefix=CHECK-MIPS %s
 
-// RUN: %swift -target x86_64-unknown-darwin14 -o %t/solaris-template.swiftmodule -emit-module -module-name solaris %S/../Inputs/empty.swift
+// RUN: %swift -target x86_64-unknown-darwin14 -o %t/solaris-template.swiftmodule -parse-stdlib -emit-module -module-name solaris %S/../Inputs/empty.swift
 // RUN: %S/Inputs/binary_sub.py x86_64-unknown-darwin14 x86_64-unknown-solaris8 < %t/solaris-template.swiftmodule > %t/solaris.swiftmodule
-// RUN: not %target-swift-frontend -I %t -parse %s -DSOLARIS 2>&1 | FileCheck -check-prefix=CHECK-SOLARIS %s
+// RUN: not %target-swift-frontend -I %t -parse -parse-stdlib %s -DSOLARIS 2>&1 | FileCheck -check-prefix=CHECK-SOLARIS %s
 
 #if MIPS
 // CHECK-MIPS: :[[@LINE+1]]:8: error: module file was created for incompatible target mips64-unknown-darwin14: {{.*}}mips.swiftmodule{{$}}
