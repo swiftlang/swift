9257f00d27c9f5c7233f48acc06cd3df77f1f3c7
Dmitri Hrybenko <dgribenko@apple.com>
2015-01-22 06:03:46 +0000

    Update validation tests for compiler changes
    Swift SVN r24633

--- validation-test/stdlib/AssertDiagnostics.swift
+++ validation-test/stdlib/AssertDiagnostics.swift
@@ -1,7 +1,7 @@
 // RUN: %target-parse-verify-swift
 
 func rejectsStringLiteral() {
-  assert("foo") // expected-error {{cannot invoke 'assert' with an argument list of type '(String)'}} expected-note{{expected an argument list of type '(@autoclosure () -> Bool, @autoclosure () -> String, StaticString, UWord)'}}
-  precondition("foo") // expected-error {{cannot invoke 'precondition' with an argument list of type '(String)'}} expected-note{{expected an argument list of type '(@autoclosure () -> Bool, @autoclosure () -> String, StaticString, UWord)'}}
+  assert("foo") // expected-error {{cannot invoke 'assert' with an argument list of type '(String)'}} expected-note {{expected an argument list of type '(@autoclosure () -> Bool, @autoclosure () -> String, file: StaticString, line: UWord)'}}
+  precondition("foo") // expected-error {{cannot invoke 'precondition' with an argument list of type '(String)'}} expected-note {{expected an argument list of type '(@autoclosure () -> Bool, @autoclosure () -> String, file: StaticString, line: UWord)'}}
 }
 
