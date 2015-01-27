e9839523c97931e0584e3d7287cb7e55695515ef
Jordan Rose <jordan_rose@apple.com>
2015-01-27 02:45:25 +0000

    [test] Add a Radar number to crash suite #55.
    Swift SVN r24746

--- validation-test/compiler_crashers/0055-no-stacktrace.swift
+++ validation-test/compiler_crashers/0055-no-stacktrace.swift
@@ -4,6 +4,7 @@
 // Distributed under the terms of the MIT license
 // Test case submitted to project by https://github.com/practicalswift (practicalswift)
 // http://www.reddit.com/r/swift/comments/2gx7m4/psa_nasty_bug_in_the_swift_compiler/cko42gk
+// rdar://problem/18734297
 
 func a() -> Int {
     var b = 0
