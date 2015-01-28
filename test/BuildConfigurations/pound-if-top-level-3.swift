aa366392107948bcd886dbc1dd3d3782bb00afae
Denis Vnukov <dvnukov@apple.com>
2015-01-28 18:54:12 +0000

    [rdar://19605880], minor: review feedback incorporated
    Swift SVN r24800

--- test/BuildConfigurations/pound-if-top-level-3.swift
+++ test/BuildConfigurations/pound-if-top-level-3.swift
@@ -1,7 +1,7 @@
 // RUN: %target-parse-verify-swift
 
 // e xpected-error@+1{{unexpected configuration expression type}}
-#if _runtime(_ObjC)
+#if arch(x86_64)
 // expected-error@+2{{expected '{' in protocol type}}
 // expected-error@+1{{expected #else or #endif at end of configuration block}}
-public protocol _CocoaString}
+public protocol CS}
