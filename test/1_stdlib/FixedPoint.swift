f359945c894597c59f0d8f24685ef034479fe300
Dave Abrahams <dabrahams@apple.com>
2015-01-29 02:05:47 +0000

    [stdlib] Use StdlibUnitTest for FixedPoint
    Swift SVN r24813

--- test/1_stdlib/FixedPoint.swift
+++ test/1_stdlib/FixedPoint.swift
@@ -1,52 +1,57 @@
-// RUN: %target-run-simple-swift | FileCheck %s
+// RUN: %target-run-simple-swift
 
-func assertEq<T1 : Equatable, T2 : Equatable>(a : (T1, T2), b : (T1, T2)) {
-  assert(a.0 == b.0)
-  assert(a.1 == b.1)
-}
+import StdlibUnittest
+
+var tests = TestSuite("FixedPoint")
 
 func testBitwiseOperationsImpl<T : UnsignedIntegerType>(_: T.Type) {
   var x = numericCast(0b11_1010_00) as T
   var y = numericCast(0b10_1100_10) as T
 
-  assert(x & y == 0b10_1000_00)
-  assert(x | y == 0b11_1110_10)
-  assert(x ^ y == 0b01_0110_10)
-  assert((~x) & 0xff == 0b00_0101_11)
+  expectEqual(0b10_1000_00, x & y)
+  expectEqual(0b11_1110_10, x | y)
+  expectEqual(0b01_0110_10, x ^ y)
+  expectEqual(0b00_0101_11, (~x) & 0xff)
 
   var z = T.allZeros
-  assert(x | z == x)
-  assert(x ^ z == x)
-  assert(x & z == z)
-  assert(x & ~z == x)
+  expectEqual(x, x | z)
+  expectEqual(x, x ^ z)
+  expectEqual(z, x & z)
+  expectEqual(x, x & ~z)
 }
 
-func testBitwiseOperations() {
+tests.test("BitwiseOperations/UInt8") {
   testBitwiseOperationsImpl(UInt8.self)
+}
+
+tests.test("BitwiseOperations/UInt16") {
   testBitwiseOperationsImpl(UInt16.self)
+}
+
+tests.test("BitwiseOperations/UInt32") {
   testBitwiseOperationsImpl(UInt32.self)
+}
+
+tests.test("BitwiseOperations/UInt64") {
   testBitwiseOperationsImpl(UInt64.self)
-
-  println("testBitwiseOperations done")
-}
-testBitwiseOperations()
-// CHECK: testBitwiseOperations done
-
-
-func testOverflowCheckOperations() {
-  assertEq(Int8.addWithOverflow(4, 5), (9, false))
-  assertEq(Int8.addWithOverflow(1, 127), (-128, true))
-  assertEq(UInt8.multiplyWithOverflow(2,128), (0, true))
-  println("testOverflowCheckOperations done")
 }
 
-testOverflowCheckOperations()
-// CHECK-NEXT: testOverflowCheckOperations done
+tests.test("OverflowCheck") {
+  expectEqual(Int8.addWithOverflow(4, 5), (9, false))
+  expectEqual(Int8.addWithOverflow(1, 127), (-128, true))
+  expectEqual(UInt8.multiplyWithOverflow(2,128), (0, true))
+}
 
-let x: UInt32 = 0xdeadbeef
-println("0x\(String(x.bigEndian, radix: 16))") // CHECK-NEXT: 0xefbeadde
+tests.test("String.init") {
+  let x: UInt32 = 0xdeadbeef
+  expectEqual("efbeadde", String(x.bigEndian, radix: 16))
 
-let y = UInt32(bigEndian: 0xdeadbeef)
-println("0x\(String(y.bigEndian, radix: 16))") // CHECK-NEXT: 0xdeadbeef
+  let y = UInt32(bigEndian: 0xdeadbeef)
+  expectEqual("deadbeef", String(y.bigEndian, radix: 16))
+}
 
-println(Int64(4).byteSwapped)                  // CHECK-NEXT: 288230376151711744
+tests.test("byteSwapped") {
+  expectEqual(288230376151711744, Int64(4).byteSwapped)
+}
+
+runAllTests()
