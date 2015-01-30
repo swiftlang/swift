1ca5bc414e8cc23a1e7f9cd9c81562e5fdc4506d
Dmitri Hrybenko <dgribenko@apple.com>
2015-01-30 04:17:49 +0000

    stdlib: add an overlay for AudioBufferList
    AudioBufferList contains a fake flexible array member, and is not
    imported in a useable way.
    
    rdar://18536929
    
    Swift SVN r24843

---
+++ stdlib/objc/CoreAudio/CoreAudio.swift
@@ -0,0 +1,170 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2015 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+@exported import CoreAudio // Clang module
+
+extension UnsafeBufferPointer {
+  /// Initialize an `UnsafeBufferPointer<T>` from an `AudioBuffer`.
+  init(_ audioBuffer: AudioBuffer) {
+    // FIXME: this API should be public, but it can't be because of a compiler
+    // limitation: can't declare a public extension on a generic type from
+    // another module.  rdar://16974298
+    self.init(
+      start: UnsafePointer<T>(audioBuffer.mData),
+      count: Int(audioBuffer.mDataByteSize) / strideof(T))
+  }
+}
+
+// The API above can't be public yet.  We expose it through this function for
+// the sake of testing so that the code does not bitrot.
+public // @testable
+func _convertAudioBufferToUnsafeBufferPointer<T>(
+  audioBuffer: AudioBuffer) -> UnsafeBufferPointer<T> {
+  return UnsafeBufferPointer(audioBuffer)
+}
+
+extension UnsafeMutableBufferPointer {
+  /// Initialize an `UnsafeMutableBufferPointer<T>` from an `AudioBuffer`.
+  init(_ audioBuffer: AudioBuffer) {
+    // FIXME: this API should be public, but it can't be because of a compiler
+    // limitation: can't declare a public extension on a generic type from
+    // another module.  rdar://16974298
+    self.init(
+      start: UnsafeMutablePointer<T>(audioBuffer.mData),
+      count: Int(audioBuffer.mDataByteSize) / strideof(T))
+  }
+}
+
+// The API above can't be public yet.  We expose it through this function for
+// the sake of testing so that the code does not bitrot.
+public // @testable
+func _convertAudioBufferToUnsafeMutableBufferPointer<T>(
+  audioBuffer: AudioBuffer) -> UnsafeMutableBufferPointer<T> {
+  return UnsafeMutableBufferPointer(audioBuffer)
+}
+
+extension AudioBuffer {
+  /// Initialize an `AudioBuffer` from an `UnsafeMutableBufferPointer<T>`.
+  public init<T>(
+    _ typedBuffer: UnsafeMutableBufferPointer<T>, numberOfChannels: Int) {
+    self.mNumberChannels = UInt32(numberOfChannels)
+    self.mData = UnsafeMutablePointer<Void>(typedBuffer.baseAddress)
+    self.mDataByteSize = UInt32(typedBuffer.count * strideof(T))
+  }
+}
+
+extension AudioBufferList {
+  /// :returns: the size in bytes of an `AudioBufferList` that can hold up to
+  /// `maximumBuffers` `AudioBuffer`\ s.
+  public static func sizeInBytes(#maximumBuffers: Int) -> Int {
+    _precondition(maximumBuffers >= 1,
+      "AudioBufferList should contain at least one AudioBuffer")
+    return sizeof(AudioBufferList) +
+      (maximumBuffers - 1) * strideof(AudioBuffer)
+  }
+
+  /// Allocate an `AudioBufferList` with a capacity for the specified number of
+  /// `AudioBuffer`\ s.
+  ///
+  /// The `count` property of the new `AudioBufferList` is initialized to
+  /// `maximumBuffers`.
+  ///
+  /// The memory should be freed with `free()`.
+  public static func allocate(#maximumBuffers: Int)
+    -> UnsafeMutableAudioBufferListPointer {
+    let byteSize = UInt(sizeInBytes(maximumBuffers: maximumBuffers))
+    let ablMemory = calloc(byteSize, 1)
+    _precondition(ablMemory != nil,
+      "failed to allocate memory for an AudioBufferList")
+
+    let abl = UnsafeMutableAudioBufferListPointer(
+        UnsafeMutablePointer<AudioBufferList>(ablMemory))
+    abl.count = maximumBuffers
+    return abl
+  }
+}
+
+/// A wrapper for a pointer to an `AudioBufferList`.
+///
+/// Like `UnsafeMutablePointer`, this type provides no automated memory
+/// management and the user must therefore take care to allocate and free
+/// memory appropriately.
+public struct UnsafeMutableAudioBufferListPointer {
+  /// Construct from an `AudioBufferList` pointer.
+  public init(_ p: UnsafeMutablePointer<AudioBufferList>) {
+    unsafeMutablePointer = p
+  }
+
+  /// The number of `AudioBuffer`\ s in the `AudioBufferList`
+  /// (`mNumberBuffers`).
+  public var count: Int {
+    get {
+      return Int(unsafeMutablePointer.memory.mNumberBuffers)
+    }
+    nonmutating set(newValue) {
+      unsafeMutablePointer.memory.mNumberBuffers = UInt32(newValue)
+    }
+  }
+
+  /// The pointer to the first `AudioBuffer` in this `AudioBufferList`.
+  internal var _audioBuffersPointer: UnsafeMutablePointer<AudioBuffer> {
+    // AudioBufferList has one AudioBuffer in a "flexible array member".
+    // Position the pointer after that, and skip one AudioBuffer back.  This
+    // brings us to the start of AudioBuffer array.
+    return UnsafeMutablePointer<AudioBuffer>(unsafeMutablePointer + 1) - 1
+  }
+
+  // FIXME: the properties 'unsafePointer' and 'unsafeMutablePointer' should
+  // be initializers on UnsafePointer and UnsafeMutablePointer, but they can't
+  // because of a compiler limitation: can't declare a public extension on a
+  // generic type from another module.  rdar://16974298
+
+  /// The pointer to the wrapped `AudioBufferList`.
+  public var unsafePointer: UnsafePointer<AudioBufferList> {
+    return UnsafePointer(unsafeMutablePointer)
+  }
+
+  /// The pointer to the wrapped `AudioBufferList`.
+  public var unsafeMutablePointer: UnsafeMutablePointer<AudioBufferList>
+}
+
+extension UnsafeMutableAudioBufferListPointer : MutableCollectionType {
+  public func generate()
+    -> IndexingGenerator<UnsafeMutableAudioBufferListPointer> {
+    return IndexingGenerator(self)
+  }
+
+  /// Always zero, which is the index of the first `AudioBuffer`.
+  public var startIndex: Int {
+    return 0
+  }
+
+  /// The "past the end" position; always identical to `count`.
+  public var endIndex: Int {
+    return count
+  }
+
+  /// Access an indexed `AudioBuffer` (`mBuffers[i]`).
+  public subscript(index: Int) -> AudioBuffer {
+    get {
+      _precondition(index >= 0 && index < self.count,
+        "subscript index out of range")
+      return (_audioBuffersPointer + index).memory
+    }
+    nonmutating set(newValue) {
+      _precondition(index >= 0 && index < self.count,
+        "subscript index out of range")
+      (_audioBuffersPointer + index).memory = newValue
+    }
+  }
+}
+
---
+++ validation-test/stdlib/CoreAudio.swift
@@ -0,0 +1,273 @@
+// RUN: %target-run-simple-swift
+
+import StdlibUnittest
+import CoreAudio
+
+// Used in tests below.
+extension AudioBuffer : Equatable {}
+
+public func == (lhs: AudioBuffer, rhs: AudioBuffer) -> Bool {
+  return lhs.mNumberChannels == rhs.mNumberChannels &&
+    lhs.mDataByteSize == rhs.mDataByteSize &&
+    lhs.mData == rhs.mData
+}
+
+var CoreAudioTestSuite = TestSuite("CoreAudio")
+
+// The size of the non-flexible part of an AudioBufferList.
+#if arch(i386) || arch(arm)
+let ablHeaderSize = 4
+#elseif arch(x86_64) || arch(arm64)
+let ablHeaderSize = 8
+#endif
+
+CoreAudioTestSuite.test("UnsafeBufferPointer.init(_: AudioBuffer)") {
+  if true {
+    let audioBuffer = AudioBuffer(
+      mNumberChannels: 0, mDataByteSize: 0, mData: nil)
+    let result: UnsafeBufferPointer<Float> =
+      _convertAudioBufferToUnsafeBufferPointer(audioBuffer)
+    expectEqual(nil, result.baseAddress)
+    expectEqual(0, result.count)
+  }
+
+  if true {
+    let audioBuffer = AudioBuffer(
+      mNumberChannels: 2, mDataByteSize: 1024,
+      mData: UnsafeMutablePointer<Void>(bitPattern: 0x1234_5678))
+    let result: UnsafeBufferPointer<Float> =
+      _convertAudioBufferToUnsafeBufferPointer(audioBuffer)
+    expectEqual(
+      UnsafePointer<Float>(audioBuffer.mData),
+      result.baseAddress)
+    expectEqual(256, result.count)
+  }
+}
+
+CoreAudioTestSuite.test("UnsafeMutableBufferPointer.init(_: AudioBuffer)") {
+  if true {
+    let audioBuffer = AudioBuffer(
+      mNumberChannels: 0, mDataByteSize: 0, mData: nil)
+    let result: UnsafeMutableBufferPointer<Float> =
+      _convertAudioBufferToUnsafeMutableBufferPointer(audioBuffer)
+    expectEqual(nil, result.baseAddress)
+    expectEqual(0, result.count)
+  }
+
+  if true {
+    let audioBuffer = AudioBuffer(
+      mNumberChannels: 2, mDataByteSize: 1024,
+      mData: UnsafeMutablePointer<Void>(bitPattern: 0x1234_5678))
+    let result: UnsafeMutableBufferPointer<Float> =
+      _convertAudioBufferToUnsafeMutableBufferPointer(audioBuffer)
+    expectEqual(
+      UnsafeMutablePointer<Float>(audioBuffer.mData),
+      result.baseAddress)
+    expectEqual(256, result.count)
+  }
+}
+
+CoreAudioTestSuite.test(
+  "AudioBuffer.init(_: UnsafeMutableBufferPointer, numberOfChannels: Int)") {
+  if true {
+    // NULL pointer.
+    let buffer = UnsafeMutableBufferPointer<Float>(start: nil, count: 0)
+    let result = AudioBuffer(buffer, numberOfChannels: 2)
+    expectEqual(2, result.mNumberChannels)
+    expectEqual(0, result.mDataByteSize)
+    expectEqual(nil, result.mData)
+  }
+  if true {
+    // Non-NULL pointer.
+    let buffer = UnsafeMutableBufferPointer<Float>(
+      start: UnsafeMutablePointer<Float>(bitPattern: 0x1234_5678), count: 0)
+    let result = AudioBuffer(buffer, numberOfChannels: 2)
+    expectEqual(2, result.mNumberChannels)
+    expectEqual(0, result.mDataByteSize)
+    expectEqual(buffer.baseAddress, result.mData)
+  }
+}
+
+CoreAudioTestSuite.test(
+  "AudioBuffer.init(_: UnsafeMutableBufferPointer, numberOfChannels: Int)/trap") {
+#if arch(i386) || arch(arm)
+  let overflowingCount = Int.max
+#elseif arch(x86_64) || arch(arm64)
+  let overflowingCount = Int(UInt32.max)
+#endif
+  let buffer = UnsafeMutableBufferPointer<Float>(
+    start: UnsafeMutablePointer<Float>(bitPattern: 0x1234_5678),
+    count: overflowingCount)
+
+  expectCrashLater()
+  // An overflow happens when we try to compute the value for mDataByteSize.
+  AudioBuffer(buffer, numberOfChannels: 2)
+}
+
+CoreAudioTestSuite.test("AudioBufferList.sizeInBytes(maximumBuffers: Int)") {
+  expectEqual(ablHeaderSize + strideof(AudioBuffer),
+    AudioBufferList.sizeInBytes(maximumBuffers: 1))
+  expectEqual(ablHeaderSize + 16 * strideof(AudioBuffer),
+    AudioBufferList.sizeInBytes(maximumBuffers: 16))
+}
+
+CoreAudioTestSuite.test("AudioBufferList.sizeInBytes(maximumBuffers: Int)/trap/count<0") {
+  expectCrashLater()
+  AudioBufferList.sizeInBytes(maximumBuffers: -1)
+}
+
+CoreAudioTestSuite.test("AudioBufferList.sizeInBytes(maximumBuffers: Int)/trap/count==0") {
+  expectCrashLater()
+  AudioBufferList.sizeInBytes(maximumBuffers: -1)
+}
+
+CoreAudioTestSuite.test("AudioBufferList.sizeInBytes(maximumBuffers: Int)/trap/overflow") {
+  expectCrashLater()
+  AudioBufferList.sizeInBytes(maximumBuffers: Int.max)
+}
+
+CoreAudioTestSuite.test("AudioBufferList.allocate(maximumBuffers: Int)") {
+  if true {
+    let ablPtrWrapper = AudioBufferList.allocate(maximumBuffers: 1)
+    expectEqual(1, ablPtrWrapper.count)
+    free(ablPtrWrapper.unsafeMutablePointer)
+  }
+  if true {
+    let ablPtrWrapper = AudioBufferList.allocate(maximumBuffers: 16)
+    expectEqual(16, ablPtrWrapper.count)
+    free(ablPtrWrapper.unsafeMutablePointer)
+  }
+}
+
+CoreAudioTestSuite.test("AudioBufferList.allocate(maximumBuffers: Int)/trap/count==0") {
+  expectCrashLater()
+  AudioBufferList.allocate(maximumBuffers: 0)
+}
+
+CoreAudioTestSuite.test("AudioBufferList.allocate(maximumBuffers: Int)/trap/count<0") {
+  expectCrashLater()
+  AudioBufferList.allocate(maximumBuffers: -1)
+}
+
+CoreAudioTestSuite.test("AudioBufferList.allocate(maximumBuffers: Int)/trap/overflow") {
+  expectCrashLater()
+  AudioBufferList.allocate(maximumBuffers: Int.max)
+}
+
+
+CoreAudioTestSuite.test(
+  "UnsafeMutableAudioBufferListPointer.init(_: UnsafeMutablePointer<AudioBufferList>)," +
+  "UnsafeMutableAudioBufferListPointer.unsafePointer," +
+  "UnsafeMutableAudioBufferListPointer.unsafeMutablePointer") {
+  if true {
+    let ablPtrWrapper = UnsafeMutableAudioBufferListPointer(nil)
+    expectEqual(nil, ablPtrWrapper.unsafePointer)
+    expectEqual(nil, ablPtrWrapper.unsafeMutablePointer)
+  }
+
+  if true {
+    let ablPtrWrapper = UnsafeMutableAudioBufferListPointer(
+      UnsafeMutablePointer<AudioBufferList>(bitPattern: 0x1234_5678))
+    expectEqual(
+      UnsafePointer<AudioBufferList>(bitPattern: 0x1234_5678),
+      ablPtrWrapper.unsafePointer)
+    expectEqual(
+      UnsafePointer<AudioBufferList>(bitPattern: 0x1234_5678),
+      ablPtrWrapper.unsafeMutablePointer)
+  }
+}
+
+CoreAudioTestSuite.test("UnsafeMutableAudioBufferListPointer.count") {
+  let sizeInBytes = AudioBufferList.sizeInBytes(maximumBuffers: 16)
+  let ablPtr = UnsafeMutablePointer<AudioBufferList>(
+    UnsafeMutablePointer<UInt8>.alloc(sizeInBytes))
+
+  // It is important that 'ablPtrWrapper' is a 'let'.  We are verifying that
+  // the 'count' property has a nonmutating setter.
+  let ablPtrWrapper = UnsafeMutableAudioBufferListPointer(ablPtr)
+
+  // Test getter.
+  UnsafeMutablePointer<UInt32>(ablPtr).memory = 0x1234_5678
+  expectEqual(0x1234_5678, ablPtrWrapper.count)
+
+  // Test setter.
+  ablPtrWrapper.count = 0x8765_4321
+  expectEqual(0x8765_4321, UnsafeMutablePointer<UInt32>(ablPtr).memory)
+
+  ablPtr.dealloc(sizeInBytes)
+}
+
+CoreAudioTestSuite.test("UnsafeMutableAudioBufferListPointer.subscript(_: Int)") {
+  let sizeInBytes = AudioBufferList.sizeInBytes(maximumBuffers: 16)
+  let ablPtr = UnsafeMutablePointer<AudioBufferList>(
+    UnsafeMutablePointer<UInt8>.alloc(sizeInBytes))
+
+  // It is important that 'ablPtrWrapper' is a 'let'.  We are verifying that
+  // the subscript has a nonmutating setter.
+  let ablPtrWrapper = UnsafeMutableAudioBufferListPointer(ablPtr)
+
+  if true {
+    // Test getter.
+    let audioBuffer = AudioBuffer(
+      mNumberChannels: 2, mDataByteSize: 1024,
+      mData: UnsafeMutablePointer<Void>(bitPattern: 0x1234_5678))
+
+    UnsafeMutablePointer<AudioBuffer>(
+      UnsafeMutablePointer<UInt8>(ablPtr) + ablHeaderSize).memory = audioBuffer
+    ablPtrWrapper.count = 1
+
+    expectEqual(2, ablPtrWrapper[0].mNumberChannels)
+    expectEqual(1024, ablPtrWrapper[0].mDataByteSize)
+    expectEqual(audioBuffer.mData, ablPtrWrapper[0].mData)
+  }
+
+  if true {
+    // Test setter.
+    let audioBuffer = AudioBuffer(
+      mNumberChannels: 5, mDataByteSize: 256,
+      mData: UnsafeMutablePointer<Void>(bitPattern: 0x8765_4321))
+
+    ablPtrWrapper.count = 2
+    ablPtrWrapper[1] = audioBuffer
+
+    let audioBufferPtr = UnsafeMutablePointer<AudioBuffer>(
+      UnsafeMutablePointer<UInt8>(ablPtr) + ablHeaderSize) + 1
+    expectEqual(5, audioBufferPtr.memory.mNumberChannels)
+    expectEqual(256, audioBufferPtr.memory.mDataByteSize)
+    expectEqual(audioBuffer.mData, audioBufferPtr.memory.mData)
+  }
+
+  ablPtr.dealloc(sizeInBytes)
+}
+
+CoreAudioTestSuite.test("UnsafeMutableAudioBufferListPointer.subscript(_: Int)/trap") {
+  let ablPtrWrapper = AudioBufferList.allocate(maximumBuffers: 4)
+
+  ablPtrWrapper[0].mNumberChannels = 42
+  ablPtrWrapper[1].mNumberChannels = 42
+  ablPtrWrapper[2].mNumberChannels = 42
+  ablPtrWrapper[3].mNumberChannels = 42
+
+  expectCrashLater()
+  ablPtrWrapper[4].mNumberChannels = 42
+}
+
+CoreAudioTestSuite.test("UnsafeMutableAudioBufferListPointer/Collection") {
+  let ablPtrWrapper = AudioBufferList.allocate(maximumBuffers: 16)
+
+  var expected: [AudioBuffer] = []
+  for i in 0..<16 {
+    let audioBuffer = AudioBuffer(
+      mNumberChannels: UInt32(2 + i), mDataByteSize: UInt32(1024 * i),
+      mData: UnsafeMutablePointer<Void>(bitPattern: 0x1234_5678 + i * 10))
+
+    ablPtrWrapper[i] = audioBuffer
+    expected.append(audioBuffer)
+  }
+
+  checkCollection(expected, ablPtrWrapper, SourceLocStack().withCurrentLoc())
+  free(ablPtrWrapper.unsafeMutablePointer)
+}
+
+runAllTests()
+
