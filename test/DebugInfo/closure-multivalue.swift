7fd7d6f25de69172d444aad9ecee6753b9a0c249
Adrian Prantl <aprantl@apple.com>
2015-01-30 18:17:27 +0000

    Tweak testcase to work with release and debug stdlibs.
    Swift SVN r24849

--- test/DebugInfo/closure-multivalue.swift
+++ test/DebugInfo/closure-multivalue.swift
@@ -1,17 +1,17 @@
 // RUN: %target-swift-frontend -Xllvm -debug-values-propagate-liveness -O %s -disable-llvm-optzns -emit-ir -g -o - | FileCheck %s
-// CHECK: define {{.*}}i1 {{.*}}4main4sort
-// CHECK: call void @llvm.dbg.value({{.*}}, metadata ![[A:.*]], metadata ![[P1:.*]])
-// CHECK: call void @llvm.dbg.value({{.*}}, metadata ![[A]], metadata ![[P2:.*]])
-// CHECK: call void @llvm.dbg.value({{.*}}, metadata ![[A]], metadata ![[P3:.*]])
-// CHECK: call void @llvm.dbg.value({{.*}}, metadata ![[B:.*]], metadata ![[P1]])
-// CHECK: call void @llvm.dbg.value({{.*}}, metadata ![[B]], metadata ![[P2]])
-// CHECK: call void @llvm.dbg.value({{.*}}, metadata ![[B]], metadata ![[P3]])
+// CHECK: define {{.*}}4main4demo
+// CHECK: define {{.*}}hidden i1 {{.*}}4main4sort
+// CHECK: call void @llvm.dbg.value(metadata i8*{{.*}}, metadata ![[A:.*]], metadata ![[P1:.*]])
+// CHECK: call void @llvm.dbg.value(metadata i64{{.*}}, metadata ![[A]], metadata ![[P2:.*]])
+// CHECK: call void @llvm.dbg.value(metadata i64{{.*}}, metadata ![[A]], metadata ![[P3:.*]])
+// CHECK: call void @llvm.dbg.value(metadata i8*{{.*}}, metadata ![[B:.*]], metadata ![[P1]])
+// CHECK: call void @llvm.dbg.value(metadata i64{{.*}}, metadata ![[B]], metadata ![[P2]])
+// CHECK: call void @llvm.dbg.value(metadata i64{{.*}}, metadata ![[B]], metadata ![[P3]])
 // CHECK-DAG: ![[A]] = {{.*}} [ DW_TAG_a{{.*}}_variable ] [a] [line 15]
 // CHECK-DAG: ![[B]] = {{.*}} [ DW_TAG_a{{.*}}_variable ] [b] [line 15]
 // CHECK-DAG: ![[P1]] = {{.*}}; [ DW_TAG_expression ] [DW_OP_piece offset=0, size={{(4|8)}}]
 // CHECK-DAG: ![[P2]] = {{.*}}; [ DW_TAG_expression ] [DW_OP_piece offset={{(4|8)}}, size={{(4|8)}}]
 // CHECK-DAG: ![[P3]] = {{.*}}; [ DW_TAG_expression ] [DW_OP_piece offset={{(8|16)}}, size={{(4|8)}}]
-
 func sort(a : String, b : String) -> Bool {
   println("Sorting..\(a) & \(b)")
   return (a < b)
