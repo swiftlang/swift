0b5843ab43d5f9773d4b9ddfba107ab561b1b5f3
Dmitri Hrybenko <dgribenko@apple.com>
2015-02-04 10:43:56 +0000

    Add XFAILs for tests that started failing under AddressSanitizer
    Swift SVN r24952

--- validation-test/compiler_crashers/0921-resolvetypedecl.swift
+++ validation-test/compiler_crashers/0921-resolvetypedecl.swift
@@ -1,5 +1,6 @@
 // RUN: not --crash %target-swift-frontend %s -parse
 // XFAIL: no_asserts
+// XFAIL: asan
 
 // Distributed under the terms of the MIT license
 // Test case submitted to project by https://github.com/practicalswift (practicalswift)
