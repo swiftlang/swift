eae39871bc351779e1418ec78fa3024f2aef0033
Dmitri Hrybenko <dgribenko@apple.com>
2015-02-05 02:44:55 +0000

    Annotate tests that no longer crash under AddressSanitizer
    Swift SVN r24988

--- validation-test/compiler_crashers/0076-llvm-foldingset-swift-constraints-constraintlocator-nodeequals.swift
+++ validation-test/compiler_crashers/0076-llvm-foldingset-swift-constraints-constraintlocator-nodeequals.swift
@@ -1,5 +1,4 @@
 // RUN: not %target-swift-frontend %s -parse
-// XFAIL: asan
 
 // Distributed under the terms of the MIT license
 // Test case submitted to project by https://github.com/practicalswift (practicalswift)
