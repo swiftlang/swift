97011ee6365131112ae03f2a484e562719c0fe71
Chris Willmore <cwillmore@apple.com>
2015-02-10 02:24:30 +0000

    Update diagnostics in test/1_stdlib/StringDiagnostics.swift for r25104.
    Swift SVN r25110

--- test/1_stdlib/StringDiagnostics.swift
+++ test/1_stdlib/StringDiagnostics.swift
@@ -23,15 +23,15 @@ func testAmbiguousStringComparisons(s: String) {
   let nsString = s as NSString
   let a1 = s == nsString
   let a2 = s != nsString
-  let a3 = s < nsString // expected-error{{'NSString' is not convertible to 'String'; did you mean to use 'as!' to force downcast?}}
-  let a4 = s <= nsString // expected-error{{'NSString' is not convertible to 'String'; did you mean to use 'as!' to force downcast?}}
-  let a5 = s >= nsString // expected-error{{'NSString' is not convertible to 'String'; did you mean to use 'as!' to force downcast?}}
-  let a6 = s > nsString // expected-error{{'NSString' is not convertible to 'String'; did you mean to use 'as!' to force downcast?}}
+  let a3 = s < nsString // expected-error{{'NSString' is not implicitly convertible to 'String'; did you mean to use 'as' to explicitly convert?}}
+  let a4 = s <= nsString // expected-error{{'NSString' is not implicitly convertible to 'String'; did you mean to use 'as' to explicitly convert?}}
+  let a5 = s >= nsString // expected-error{{'NSString' is not implicitly convertible to 'String'; did you mean to use 'as' to explicitly convert?}}
+  let a6 = s > nsString // expected-error{{'NSString' is not implicitly convertible to 'String'; did you mean to use 'as' to explicitly convert?}}
   // now the other way
   let a7 = nsString == s
   let a8 = nsString != s
-  let a9 = nsString < s // expected-error{{'NSString' is not convertible to 'String'; did you mean to use 'as!' to force downcast?}}
-  let a10 = nsString <= s // expected-error{{'NSString' is not convertible to 'String'; did you mean to use 'as!' to force downcast?}}
-  let a11 = nsString >= s // expected-error{{'NSString' is not convertible to 'String'; did you mean to use 'as!' to force downcast?}}
-  let a12 = nsString > s // expected-error{{'NSString' is not convertible to 'String'; did you mean to use 'as!' to force downcast?}}
+  let a9 = nsString < s // expected-error{{'NSString' is not implicitly convertible to 'String'; did you mean to use 'as' to explicitly convert?}}
+  let a10 = nsString <= s // expected-error{{'NSString' is not implicitly convertible to 'String'; did you mean to use 'as' to explicitly convert?}}
+  let a11 = nsString >= s // expected-error{{'NSString' is not implicitly convertible to 'String'; did you mean to use 'as' to explicitly convert?}}
+  let a12 = nsString > s // expected-error{{'NSString' is not implicitly convertible to 'String'; did you mean to use 'as' to explicitly convert?}}
 }
