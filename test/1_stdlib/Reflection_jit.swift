4501aac83019cda180b0d76c2d510062af552d4b
Joe Groff <jgroff@apple.com>
2015-02-11 20:16:43 +0000

    Fix Reflection_jit test to match Reflection again
    Swift SVN r25190

--- test/1_stdlib/Reflection_jit.swift
+++ test/1_stdlib/Reflection_jit.swift
@@ -1,5 +1,5 @@
 // Test Reflection.swift in JIT mode.
-// RUN: %target-jit-run %S/Reflection.swift -- %S/Inputs/shuffle.jpg | FileCheck %S/Reflection.swift
+// RUN: %target-jit-run -parse-stdlib %S/Reflection.swift -- %S/Inputs/shuffle.jpg | FileCheck %S/Reflection.swift
 
 // XFAIL: linux
 // REQUIRES: swift_interpreter
