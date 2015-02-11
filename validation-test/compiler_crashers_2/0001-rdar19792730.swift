b76f6b27ec15c0c31afebabb0843e1e32d968f5d
Dmitri Hrybenko <dgribenko@apple.com>
2015-02-11 07:12:19 +0000

    Add tests for compiler crasher bugs that I filed recently
    If these tests start passing, we would conveniently know which bugs we
    can close.
    
    Swift SVN r25170

---
+++ validation-test/compiler_crashers_2/0001-rdar19792730.swift
@@ -0,0 +1,22 @@
+// RUN: not --crash %target-swift-frontend %s -emit-silgen
+
+// rdar://problem/19792730
+
+public func foo<
+    Expected : SequenceType,
+    Actual : SequenceType,
+    T : Comparable
+    where
+      Expected.Generator.Element == Actual.Generator.Element,
+      Expected.Generator.Element == T
+>(expected: Expected, actual: Actual) {}
+
+public func foo<
+    Expected : SequenceType,
+    Actual : SequenceType,
+    T : Comparable
+    where
+      Expected.Generator.Element == Actual.Generator.Element,
+      Expected.Generator.Element == (T, T)
+>(expected: Expected, actual: Actual) {}
+
---
+++ validation-test/compiler_crashers_2/0002-rdar19792768.swift
@@ -0,0 +1,19 @@
+// RUN: not --crash %target-swift-frontend %s -emit-silgen
+
+// rdar://problem/19792768
+
+public func foo<
+    Expected : SequenceType,
+    Actual : SequenceType,
+    T : Comparable
+    where
+      Expected.Generator.Element == Actual.Generator.Element,
+      Expected.Generator.Element == (T, T)
+>(expected: Expected, actual: Actual) {}
+
+func f() {
+  foo(
+    [ (10, 1010), (20, 1020), (30, 1030) ],
+    [ (10, 1010), (20, 1020), (30, 1030) ])
+}
+
