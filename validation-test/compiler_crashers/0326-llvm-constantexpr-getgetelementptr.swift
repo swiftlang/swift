38a14768e7f4e0bd6d7d4dc107030a3cb4ab612f
Dmitri Hrybenko <dgribenko@apple.com>
2015-02-12 02:29:00 +0000

    Annotate a crasher test that is not crashing anymore
    Swift SVN r25223

--- validation-test/compiler_crashers/0326-llvm-constantexpr-getgetelementptr.swift
+++ validation-test/compiler_crashers/0326-llvm-constantexpr-getgetelementptr.swift
@@ -1,4 +1,4 @@
-// RUN: not --crash %target-swift-frontend %s -emit-ir
+// RUN: %target-swift-frontend %s -emit-ir
 
 // Distributed under the terms of the MIT license
 // Test case submitted to project by https://github.com/practicalswift (practicalswift)
