4c8fad7f40931b763940fa41915f401669687506
Dmitri Hrybenko <dgribenko@apple.com>
2015-02-13 01:14:13 +0000

    tests: add (failing) tests for mandatory inlining of && and ||
    Swift SVN r25261

---
+++ validation-test/stdlib/BoolDiagnostics_Dataflow.swift
@@ -0,0 +1,57 @@
+// RUN: %target-swift-frontend -emit-sil -verify %s
+
+@asmname("opaque")
+func opaque() -> Int
+
+func test_constantFoldAnd1() -> Int {
+  while true && true {
+    return 42
+  }
+  // FIXME: this is a false positive.
+} // expected-error {{missing return in a function expected to return 'Int'}}
+
+func test_constantFoldAnd2() -> Int {
+  while true && false {
+    return 42
+  }
+} // expected-error {{missing return in a function expected to return 'Int'}}
+
+func test_constantFoldAnd3() -> Int {
+  while false && true {
+    return 42
+  }
+} // expected-error {{missing return in a function expected to return 'Int'}}
+
+func test_constantFoldAnd4() -> Int {
+  while false && false {
+    return 42
+  }
+} // expected-error {{missing return in a function expected to return 'Int'}}
+
+func test_constantFoldOr1() -> Int {
+  while true || true {
+    return 42
+  }
+  // FIXME: this is a false positive.
+} // expected-error {{missing return in a function expected to return 'Int'}}
+
+func test_constantFoldOr2() -> Int {
+  while true || false {
+    return 42
+  }
+  // FIXME: this is a false positive.
+} // expected-error {{missing return in a function expected to return 'Int'}}
+
+func test_constantFoldOr3() -> Int {
+  while false || true {
+    return 42
+  }
+  // FIXME: this is a false positive.
+} // expected-error {{missing return in a function expected to return 'Int'}}
+
+func test_constantFoldOr4() -> Int {
+  while false || false {
+    return 42
+  }
+} // expected-error {{missing return in a function expected to return 'Int'}}
+
