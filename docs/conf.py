8c04ba61668a086f04cca3d208763da325832c9e
Dave Abrahams <dabrahams@apple.com>
2015-02-15 22:51:39 +0000

    Docs: switch links to a subtler color
    Red hurts text readability when it occurs inline
    
    Swift SVN r25303

--- docs/conf.py
+++ docs/conf.py
@@ -99,7 +99,12 @@ html_style = 'swift.css'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+  # Red links are a bit too garish
+  "linkcolor" : "#577492",
+  "visitedlinkcolor" : "#577492",
+  "hoverlinkcolor" : "#551A8B"
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = ["."]
