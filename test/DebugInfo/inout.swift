bb63b1eb2ddd46ac153e0c9f9fbdc20deb303404
Adrian Prantl <aprantl@apple.com>
2015-02-17 01:18:14 +0000

    Make this testcase more resilient.
    Swift SVN r25337

--- test/DebugInfo/inout.swift
+++ test/DebugInfo/inout.swift
@@ -1,4 +1,7 @@
-// RUN: %target-swift-frontend %s -emit-ir -g -o - | FileCheck %s
+// RUN: %target-swift-frontend %s -emit-ir -g -module-name inout -o %t.ll
+// RUN: cat %t.ll | FileCheck %s
+// RUN: cat %t.ll | FileCheck %s --check-prefix=PROMO-CHECK
+// RUN: cat %t.ll | FileCheck %s --check-prefix=FOO-CHECK
 
 // LValues are direct values, too. They are reference types, though.
 
@@ -6,26 +9,21 @@ func Close(fn: () -> Int) { fn() }
 typealias MyFloat = Float
 
 // CHECK: define hidden void @_TF5inout13modifyFooHeap
-// CHECK: %[[ALLOCB:.*]] = alloca
-// CHECK: %[[ALLOCA:.*]] = alloca
-// CHECK: %[[ALLOCABOX:.*]] = alloca
-// CHECK:  call void @llvm.dbg.declare(metadata {{.*}} %[[ALLOCB]], metadata ![[B:.*]], metadata !{{[0-9]+}})
-// CHECK:  call void @llvm.dbg.declare(metadata {{.*}} %[[ALLOCA]], metadata
+// CHECK: %[[ALLOCB:.*]] = alloca %Sf
+// CHECK: %[[ALLOCA:.*]] = alloca %Si*
+// CHECK-DAG:  call void @llvm.dbg.declare(metadata {{.*}} %[[ALLOCA]], metadata ![[A:[0-9]+]]
+// CHECK-DAG:  call void @llvm.dbg.declare(metadata {{.*}} %[[ALLOCB]], metadata ![[B:[0-9]+]], metadata !{{[0-9]+}})
 
 // Closure with promoted capture.
-// CHECK: define {{.*}}@_TTSf2d_i___TFF5inout13modifyFooHeapFTRSiSf_T_U_FT_Si
-// CHECK: call void @llvm.dbg.declare(metadata {{(i32|i64)}}* %{{.*}}, metadata ![[A1:[0-9]+]], metadata ![[EMPTY_EXPR:[0-9]+]])
+// PROMO-CHECK: define {{.*}}@_TTSf2d_i___TFF5inout13modifyFooHeapFTRSiSf_T_U_FT_Si
+// PROMO-CHECK: call void @llvm.dbg.declare(metadata {{(i32|i64)}}* %{{.*}}, metadata ![[A1:[0-9]+]], metadata ![[EMPTY_EXPR:[0-9]+]])
 
-// Inout reference type.
-// CHECK: define {{.*}}@_TF5inout9modifyFooFTRSiSf_T_
-// CHECK: call void @llvm.dbg.declare(metadata %Si** %{{.*}}, metadata ![[U:[0-9]+]], metadata ![[EMPTY_EXPR]])
-
-// CHECK-DAG: ![[EMPTY_EXPR]] = !{!"0x102"} ; [ DW_TAG_expression ]
-// CHECK-DAG: ![[REFINT:.*]] = {{.*}}[ DW_TAG_structure_type ] [_TtRSi]
-// CHECK-DAG: ![[A1]] = {{.*}}!"_TtSi"} ; [ DW_TAG_arg_variable ] [a]
-// CHECK-DAG: "_TtRSi"{{.*}}[ DW_TAG_structure_type ] [_TtRSi] [line 0,
+// PROMO-CHECK-DAG: ![[EMPTY_EXPR]] = !{!"0x102"} ; [ DW_TAG_expression ]
+// PROMO-CHECK-DAG: ![[REFINT:.*]] = {{.*}}[ DW_TAG_structure_type ] [_TtRSi]
+// PROMO-CHECK-DAG: ![[A1]] = {{.*}}!"_TtSi"} ; [ DW_TAG_arg_variable ] [a]
+// PROMO-CHECK-DAG: "_TtRSi"{{.*}}[ DW_TAG_structure_type ] [_TtRSi] [line 0,
 func modifyFooHeap(inout a: Int,
-// CHECK-DAG: ![[A:[0-9]+]] = {{.*}}!"_TtRSi"} ; [ DW_TAG_arg_variable ] [a] [line [[@LINE-1]]]
+// CHECK-DAG: ![[A]] = {{.*}}!"_TtRSi"} ; [ DW_TAG_arg_variable ] [a] [line [[@LINE-1]]]
 // CHECK-DAG: ![[B]] = {{.*}}![[MYFLOAT:.*]]} ; [ DW_TAG_arg_variable ] [b] [line [[@LINE+2]]]
 // CHECK-DAG: ![[MYFLOAT]] = {{.*}}[ DW_TAG_typedef ] [_Tta5inout7MyFloat] {{.*}} [from _TtSf]
                    var b: MyFloat)
@@ -38,10 +36,15 @@ func modifyFooHeap(inout a: Int,
     Close({ a })
 }
 
+// Inout reference type.
+// FOO-CHECK: define {{.*}}@_TF5inout9modifyFooFTRSiSf_T_
+// FOO-CHECK: call void @llvm.dbg.declare(metadata %Si** %{{.*}}, metadata ![[U:[0-9]+]], metadata ![[EMPTY_EXPR:.*]])
+// FOO-CHECK-DAG: ![[EMPTY_EXPR]] = !{!"0x102"} ; [ DW_TAG_expression ]
 func modifyFoo(inout u: Int,
-// CHECK-DAG: ![[MYFLOAT]]} ; [ DW_TAG_arg_variable ] [v] [line [[@LINE+2]]]
-// CHECK-DAG: [[U]] = {{.*}}!"_TtRSi"} ; [ DW_TAG_arg_variable ] [u] [line [[@LINE-2]]]
+// FOO-CHECK-DAG: ![[MYFLOAT:[0-9]+]]} ; [ DW_TAG_arg_variable ] [v] [line [[@LINE+2]]]
+// FOO-CHECK-DAG: [[U]] = {{.*}}!"_TtRSi"} ; [ DW_TAG_arg_variable ] [u] [line [[@LINE-2]]]
                var v: MyFloat)
+// CHECK-DAG: ![[MYFLOAT]] = {{.*}}[ DW_TAG_typedef ] [_Tta5inout7MyFloat] {{.*}} [from _TtSf]
 {
     if (v > 2.71) {
       u = u - 41
@@ -49,11 +52,11 @@ func modifyFoo(inout u: Int,
 }
 
 func main() -> Int {
-    var b = 11 as Int
-    modifyFoo(&b, 3.14)
+    var c = 11 as Int
+    modifyFoo(&c, 3.14)
 
-    var c = 64 as Int
-    modifyFooHeap(&c, 1.41)
+    var d = 64 as Int
+    modifyFooHeap(&d, 1.41)
     return 0
 }
 
