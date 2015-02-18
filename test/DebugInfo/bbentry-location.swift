9deae1b6b70869fb74e54ec84c467b02e8060f15
Adrian Prantl <aprantl@apple.com>
2015-02-18 00:46:15 +0000

    Add a testcase I forgot add before committing r25287.
    Swift SVN r25361

---
+++ test/DebugInfo/bbentry-location.swift
@@ -0,0 +1,30 @@
+// REQUIRES: OS=ios
+// REQUIRES: objc_interop
+// RUN: %target-swift-frontend -emit-ir -g %s -o - | FileCheck %s
+
+import UIKit
+class ActionViewController
+{
+  var imageView: UIImageView!
+  func viewDidLoad(inputItems: [AnyObject]) {
+    for item: AnyObject in  inputItems {
+      let inputItem = item as! NSExtensionItem
+      for provider: AnyObject in inputItem.attachments! {
+        let itemProvider = provider as! NSItemProvider
+// CHECK: load {{.*}}selector
+// CHECK:; <label>{{.*}}  ; preds = %{{[0-9]+}}
+// CHECK: @swift_allocObject({{.*}}, !dbg ![[DBG:[0-9]+]]
+// Test that the location is reset at the entry of a new basic block.        
+// CHECK: ![[DBG]] = {{.*}}line: 0
+        if itemProvider.hasItemConformingToTypeIdentifier("") {
+          weak var weakImageView = self.imageView
+          itemProvider.loadItemForTypeIdentifier("", options: nil,
+               completionHandler: { (image, error) in
+              if let imageView = weakImageView {
+              }
+            })
+        }
+      }
+    }
+  }
+}
