1630c074b5abbefedb7808ecd682c55c9c0d8f43
Mark Lacey <mark.lacey@apple.com>
2015-02-25 07:49:17 +0000

    Follow-on to r25524 to fix up iOS tests and another diagnostic.
    Swift SVN r25531

--- test/attr/attr_ibaction_ios.swift
+++ test/attr/attr_ibaction_ios.swift
@@ -7,7 +7,7 @@ struct IntWrapper {
 class IBActionWrapperTy {
   @IBAction func nullary() {}
   // CHECK-ios-NOT: attr_ibaction_ios.swift:[[@LINE-1]]
-  // CHECK-macosx: attr_ibaction_ios.swift:[[@LINE-2]]:18: error: 'IBAction' methods must have a single argument
+  // CHECK-macosx: attr_ibaction_ios.swift:[[@LINE-2]]:18: error: @IBAction methods must have a single argument
   
   @IBAction func reqReq(_: AnyObject, _: AnyObject) {}
   @IBAction func reqOpt(_: AnyObject, _: AnyObject?) {}
@@ -27,32 +27,32 @@ class IBActionWrapperTy {
   // CHECK-ios-NOT: attr_ibaction_ios.swift:[[@LINE-9]]
   // CHECK-ios-NOT: attr_ibaction_ios.swift:[[@LINE-9]]
   // CHECK-ios-NOT: attr_ibaction_ios.swift:[[@LINE-9]]
-  // CHECK-macosx: attr_ibaction_ios.swift:[[@LINE-18]]:18: error: 'IBAction' methods must have a single argument
-  // CHECK-macosx: attr_ibaction_ios.swift:[[@LINE-18]]:18: error: 'IBAction' methods must have a single argument
-  // CHECK-macosx: attr_ibaction_ios.swift:[[@LINE-18]]:18: error: 'IBAction' methods must have a single argument
-  // CHECK-macosx: attr_ibaction_ios.swift:[[@LINE-18]]:18: error: 'IBAction' methods must have a single argument
-  // CHECK-macosx: attr_ibaction_ios.swift:[[@LINE-18]]:18: error: 'IBAction' methods must have a single argument
-  // CHECK-macosx: attr_ibaction_ios.swift:[[@LINE-18]]:18: error: 'IBAction' methods must have a single argument
-  // CHECK-macosx: attr_ibaction_ios.swift:[[@LINE-18]]:18: error: 'IBAction' methods must have a single argument
-  // CHECK-macosx: attr_ibaction_ios.swift:[[@LINE-18]]:18: error: 'IBAction' methods must have a single argument
-  // CHECK-macosx: attr_ibaction_ios.swift:[[@LINE-18]]:18: error: 'IBAction' methods must have a single argument
+  // CHECK-macosx: attr_ibaction_ios.swift:[[@LINE-18]]:18: error: @IBAction methods must have a single argument
+  // CHECK-macosx: attr_ibaction_ios.swift:[[@LINE-18]]:18: error: @IBAction methods must have a single argument
+  // CHECK-macosx: attr_ibaction_ios.swift:[[@LINE-18]]:18: error: @IBAction methods must have a single argument
+  // CHECK-macosx: attr_ibaction_ios.swift:[[@LINE-18]]:18: error: @IBAction methods must have a single argument
+  // CHECK-macosx: attr_ibaction_ios.swift:[[@LINE-18]]:18: error: @IBAction methods must have a single argument
+  // CHECK-macosx: attr_ibaction_ios.swift:[[@LINE-18]]:18: error: @IBAction methods must have a single argument
+  // CHECK-macosx: attr_ibaction_ios.swift:[[@LINE-18]]:18: error: @IBAction methods must have a single argument
+  // CHECK-macosx: attr_ibaction_ios.swift:[[@LINE-18]]:18: error: @IBAction methods must have a single argument
+  // CHECK-macosx: attr_ibaction_ios.swift:[[@LINE-18]]:18: error: @IBAction methods must have a single argument
 
   @IBAction func reqBad(_: AnyObject, _: IBActionWrapperTy) {}
-  // CHECK-ios: attr_ibaction_ios.swift:[[@LINE-1]]:18: error: argument to 'IBAction' method cannot have non-'@objc' class type
-  // CHECK-macosx: attr_ibaction_ios.swift:[[@LINE-2]]:18: error: 'IBAction' methods must have a single argument
+  // CHECK-ios: attr_ibaction_ios.swift:[[@LINE-1]]:18: error: argument to @IBAction method cannot have non-'@objc' class type
+  // CHECK-macosx: attr_ibaction_ios.swift:[[@LINE-2]]:18: error: @IBAction methods must have a single argument
 
   @IBAction func badReq(_: Int, _: AnyObject) {}
-  // CHECK-ios: attr_ibaction_ios.swift:[[@LINE-1]]:18: error: argument to 'IBAction' method cannot have non-object type
-  // CHECK-macosx: attr_ibaction_ios.swift:[[@LINE-2]]:18: error: 'IBAction' methods must have a single argument
+  // CHECK-ios: attr_ibaction_ios.swift:[[@LINE-1]]:18: error: argument to @IBAction method cannot have non-object type
+  // CHECK-macosx: attr_ibaction_ios.swift:[[@LINE-2]]:18: error: @IBAction methods must have a single argument
 
   @IBAction func badBad(_: Int, _: IBActionWrapperTy) {}
-  // CHECK-ios: attr_ibaction_ios.swift:[[@LINE-1]]:18: error: argument to 'IBAction' method cannot have non-object type
-  // CHECK-ios: attr_ibaction_ios.swift:[[@LINE-2]]:18: error: argument to 'IBAction' method cannot have non-'@objc' class type
-  // CHECK-macosx: attr_ibaction_ios.swift:[[@LINE-3]]:18: error: 'IBAction' methods must have a single argument
+  // CHECK-ios: attr_ibaction_ios.swift:[[@LINE-1]]:18: error: argument to @IBAction method cannot have non-object type
+  // CHECK-ios: attr_ibaction_ios.swift:[[@LINE-2]]:18: error: argument to @IBAction method cannot have non-'@objc' class type
+  // CHECK-macosx: attr_ibaction_ios.swift:[[@LINE-3]]:18: error: @IBAction methods must have a single argument
 
   @IBAction func tooManyArgs(_: AnyObject, _: AnyObject, _: AnyObject) {}
-  // CHECK-ios: attr_ibaction_ios.swift:[[@LINE-1]]:18: error: 'IBAction' methods can only have up to 2 arguments
-  // CHECK-macosx: attr_ibaction_ios.swift:[[@LINE-2]]:18: error: 'IBAction' methods must have a single argument
+  // CHECK-ios: attr_ibaction_ios.swift:[[@LINE-1]]:18: error: @IBAction methods can only have up to 2 arguments
+  // CHECK-macosx: attr_ibaction_ios.swift:[[@LINE-2]]:18: error: @IBAction methods must have a single argument
 
   @IBAction func watchKitLike(_: Int) {}
   // CHECK-ios-NOT: attr_ibaction_ios.swift:[[@LINE-1]]
