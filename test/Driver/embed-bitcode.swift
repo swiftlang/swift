849b7fda1f272399398e75b08fde1f7f5bb1241a
Graham Batty <graham.batty@apple.com>
2015-02-27 19:13:38 +0000

    Update new tests for linux.
    Swift SVN r25605

--- test/Driver/embed-bitcode.swift
+++ test/Driver/embed-bitcode.swift
@@ -1,7 +1,12 @@
-// RUN: %swiftc_driver -driver-print-bindings -embed-bitcode %s 2>&1 | FileCheck %s
-// CHECK: "swift", inputs: ["{{.*}}embed-bitcode.swift"], output: {llvm-bc: "[[BC:.*\.bc]]"} 
-// CHECK: "swift", inputs: ["[[BC]]"], output: {object: "[[OBJECT:.*\.o]]"}
-// CHECK: "darwin::Linker", inputs: ["[[OBJECT]]"], output: {image: "embed-bitcode"}
+// RUN: %swiftc_driver -driver-print-bindings -embed-bitcode %s 2>&1 | FileCheck -check-prefix=CHECK-%target-object-format %s
+// CHECK-macho: "swift", inputs: ["{{.*}}embed-bitcode.swift"], output: {llvm-bc: "[[BC:.*\.bc]]"}
+// CHECK-macho: "swift", inputs: ["[[BC]]"], output: {object: "[[OBJECT:.*\.o]]"}
+// CHECK-macho: "darwin::Linker", inputs: ["[[OBJECT]]"], output: {image: "embed-bitcode"}
+
+// CHECK-elf: "swift", inputs: ["{{.*}}embed-bitcode.swift"], output: {llvm-bc: "[[BC:.*\.bc]]"}
+// CHECK-elf: "swift", inputs: ["[[BC]]"], output: {object: "[[OBJECT:.*\.o]]"}
+// CHECK-elf: "swift-autolink-extract", inputs: ["[[OBJECT]]"], output: {autolink: "[[AUTOLINK:.*\.autolink]]"}
+// CHECK-elf: "linux::Linker", inputs: ["[[OBJECT]]", "[[AUTOLINK]]"], output: {image: "main"}
 
 // RUN: %swiftc_driver -embed-bitcode %s 2>&1 -### | FileCheck %s -check-prefix=CHECK-FRONT
 // CHECK-FRONT: -frontend
