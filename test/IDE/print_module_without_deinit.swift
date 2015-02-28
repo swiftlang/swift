8cf7b8b4ae38a8b0c663ec0335f7ef4e8f08cb09
Xi Ge <xi_ge@apple.com>
2015-02-28 00:02:57 +0000

    remove import in test
    Swift SVN r25626

--- test/IDE/print_module_without_deinit.swift
+++ test/IDE/print_module_without_deinit.swift
@@ -4,8 +4,6 @@
 // RUN: %target-swift-ide-test(mock-sdk: %clang-importer-sdk) -print-module -skip-deinit=true -module-to-print=print_module_without_deinit -I %t -source-filename=%s | FileCheck -check-prefix=SKIP1 %s
 // RUN: %target-swift-ide-test(mock-sdk: %clang-importer-sdk) -print-module -skip-deinit=false -module-to-print=print_module_without_deinit -I %t -source-filename=%s | FileCheck -check-prefix=NOSKIP1 %s
 
-import Foundation
-
 // SKIP1: class PropertyOwnership {
 // NOSKIP1: class PropertyOwnership {
 class PropertyOwnership {
