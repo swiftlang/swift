f5432e4a3b1799c05c4897d91ef93b0e42d21284
Adrian Prantl <aprantl@apple.com>
2015-03-04 00:05:26 +0000

    remove stale comment
    Swift SVN r25727

--- test/DebugInfo/basic.swift
+++ test/DebugInfo/basic.swift
@@ -1,4 +1,3 @@
-// See <rdar://problem/16533351>
 // A (no longer) basic test for debug info.
 // --------------------------------------------------------------------
 // Verify that we don't emit any debug info by default.
