1cc7f597519b28d9e542759e4d8ddb118e093886
Joe Groff <jgroff@apple.com>
2015-03-10 17:31:34 +0000

    Add a KnownProtocol kind for ErrorType.
    Swift SVN r25934

--- include/swift/Serialization/ModuleFormat.h
+++ include/swift/Serialization/ModuleFormat.h
@@ -51,7 +51,7 @@ const uint16_t VERSION_MAJOR = 0;
 /// To ensure that two separate changes don't silently get merged into one
 /// in source control, you should also update the comment to briefly
 /// describe what change you made.
-const uint16_t VERSION_MINOR = 178; // Last change: VarPattern
+const uint16_t VERSION_MINOR = 179; // Last change: attr and known protocol for ErrorType
 
 using DeclID = Fixnum<31>;
 using DeclIDField = BCFixed<31>;
---
+++ stdlib/public/core/ErrorType.swift
@@ -0,0 +1,17 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2015 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+// The protocol for error values that can be thrown.
+// TODO: doc comment
+protocol ErrorType {
+  // TODO
+}
