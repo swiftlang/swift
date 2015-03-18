c7da7457e6cd2be5d7d0a38bb0432962b0d8b7db
Ben Langmuir <blangmuir@apple.com>
2015-03-18 17:26:11 +0000

    Update validation/IDE tests missed in previous commit
    Swift SVN r26263

--- validation-test/IDE/complete_from_cocoa.swift
+++ validation-test/IDE/complete_from_cocoa.swift
@@ -12,8 +12,8 @@ import Cocoa
 func testUnqualified() {
   #^T1^#
 // T1: Begin completions
-// T1-DAG: Decl[FreeFunction]/OtherModule: CFArrayCreate({#(allocator): CFAllocator!#}, {#(values): UnsafeMutablePointer<UnsafePointer<Void>>#}, {#(numValues): CFIndex#}, {#(callBacks): UnsafePointer<CFArrayCallBacks>#})[#CFArray!#]{{; name=.+$}}
-// T1-DAG: Decl[FreeFunction]/OtherModule: CFArrayGetCount({#(theArray): CFArray!#})[#CFIndex#]{{; name=.+$}}
-// T1-DAG: Decl[Class]/OtherModule:        NSObject[#NSObject#]{{; name=.+$}}
+// T1-DAG: Decl[FreeFunction]/OtherModule[CoreFoundation.CFArray]: CFArrayCreate({#(allocator): CFAllocator!#}, {#(values): UnsafeMutablePointer<UnsafePointer<Void>>#}, {#(numValues): CFIndex#}, {#(callBacks): UnsafePointer<CFArrayCallBacks>#})[#CFArray!#]{{; name=.+$}}
+// T1-DAG: Decl[FreeFunction]/OtherModule[CoreFoundation.CFArray]: CFArrayGetCount({#(theArray): CFArray!#})[#CFIndex#]{{; name=.+$}}
+// T1-DAG: Decl[Class]/OtherModule[ObjectiveC.NSObject]:        NSObject[#NSObject#]{{; name=.+$}}
 // T1: End completions
 }
