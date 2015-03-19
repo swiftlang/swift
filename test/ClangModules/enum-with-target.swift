5945fab5711f103b0d6182e15181d85013e04933
Jordan Rose <jordan_rose@apple.com>
2015-03-19 02:34:54 +0000

    Add test missing from r26184.
    rdar://problem/17686122, still.
    
    Swift SVN r26295

---
+++ test/ClangModules/enum-with-target.swift
@@ -0,0 +1,11 @@
+// RUN: %swift %clang-importer-sdk -target x86_64-apple-macosx10.9 -parse %s -verify
+// RUN: %swift %clang-importer-sdk -target x86_64-apple-macosx10.10 -parse %s -verify
+
+// REQUIRES: OS=macosx
+import Foundation
+
+// Ignore deprecated constants in prefix stripping, even if they aren't deprecated /yet/.
+let calendarUnits: NSCalendarUnit = .Era | .Year | .Calendar
+let calendarUnits2: NSCalendarUnit = .NSMonthCalendarUnit | .NSYearCalendarUnit // expected-error 2 {{unavailable}}
+  // ...unless they're all deprecated.
+let calendarUnitsDep: NSCalendarUnitDeprecated = .EraCalendarUnitDeprecated | .YearCalendarUnitDeprecated // expected-error 2 {{unavailable}}
