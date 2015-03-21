61ae51da28d3677848bbbe5577b7b192d404bbd5
Dmitri Hrybenko <dgribenko@apple.com>
2015-03-21 05:50:45 +0000

    Fix bad indentation in the NSStringAPI test.
    Swift SVN r26393

--- test/1_stdlib/NSStringAPI.swift
+++ test/1_stdlib/NSStringAPI.swift
@@ -85,16 +85,14 @@ NSStringAPIs.test("NSStringEncoding") {
 NSStringAPIs.test("localizedStringWithFormat(_:...)") {
   var world: NSString = "world"
   expectEqual("Hello, world!%42", String.localizedStringWithFormat(
-      "Hello, %@!%%%ld", world, 42))
+    "Hello, %@!%%%ld", world, 42))
 
   withOverriddenNSLocaleCurrentLocale("en_US") {
-    expectEqual("0.5", String.localizedStringWithFormat(
-        "%g", 0.5))
+    expectEqual("0.5", String.localizedStringWithFormat("%g", 0.5))
   }
 
   withOverriddenNSLocaleCurrentLocale("uk") {
-    expectEqual("0,5", String.localizedStringWithFormat(
-        "%g", 0.5))
+    expectEqual("0,5", String.localizedStringWithFormat("%g", 0.5))
   }
 }
 
