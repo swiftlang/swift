c421c51a424227efb660c14cd67d76de765c059a
Dmitri Hrybenko <dgribenko@apple.com>
2015-03-21 05:41:46 +0000

    StdlibUnittest: add facilities to replace NSLocale.currentLocale()
    Will use this API to write tests for rdar://20171815.
    
    Swift SVN r26391

---
+++ stdlib/private/StdlibUnittestFoundationExtras/StdlibUnittestFoundationExtras.swift
@@ -0,0 +1,59 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2015 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+import ObjectiveC
+import Foundation
+
+internal var _temporaryNSLocaleCurrentLocale: NSLocale? = nil
+
+extension NSLocale {
+  @objc
+  public class func _swiftUnittest_currentLocale() -> NSLocale {
+    return _temporaryNSLocaleCurrentLocale!
+  }
+}
+
+public func withOverriddenNSLocaleCurrentLocale<Result>(
+  temporaryLocale: NSLocale,
+  body: () -> Result
+) -> Result {
+  let oldMethod = class_getClassMethod(
+    NSLocale.self, Selector("currentLocale"))
+  precondition(oldMethod != nil, "could not find +[NSLocale currentLocale]")
+
+  let newMethod = class_getClassMethod(
+    NSLocale.self, Selector("_swiftUnittest_currentLocale"))
+  precondition(newMethod != nil, "could not find +[NSLocale _swiftUnittest_currentLocale]")
+
+  precondition(_temporaryNSLocaleCurrentLocale == nil,
+    "nested calls to withOverriddenNSLocaleCurrentLocale are not supported")
+
+  _temporaryNSLocaleCurrentLocale = temporaryLocale
+  method_exchangeImplementations(oldMethod, newMethod)
+  let result = body()
+  method_exchangeImplementations(newMethod, oldMethod)
+  _temporaryNSLocaleCurrentLocale = nil
+
+  return result
+}
+
+public func withOverriddenNSLocaleCurrentLocale<Result>(
+  temporaryLocaleIdentifier: String,
+  body: () -> Result
+) -> Result {
+  precondition(contains(
+    NSLocale.availableLocaleIdentifiers(), temporaryLocaleIdentifier),
+    "requested locale \(temporaryLocaleIdentifier) is not available")
+
+  return withOverriddenNSLocaleCurrentLocale(
+    NSLocale(localeIdentifier: temporaryLocaleIdentifier), body)
+}
---
+++ validation-test/stdlib/StdlibUnittestFoundationExtras.swift
@@ -0,0 +1,70 @@
+// RUN: %target-run-simple-swift
+
+import StdlibUnittest
+import Foundation
+import StdlibUnittestFoundationExtras
+
+var FoundationExtrasTests = TestSuite("FoundationExtras")
+
+FoundationExtrasTests.test("withOverriddenNSLocaleCurrentLocale(NSLocale)") {
+  // Check two locales to make sure the behavior is correct even if one of
+  // these locales happens to be the same as the actual current locale.
+  if true {
+    let result = withOverriddenNSLocaleCurrentLocale(
+      NSLocale(localeIdentifier: "en_US")) {
+      () -> Int in
+      expectEqual("en_US", NSLocale.currentLocale().localeIdentifier)
+      return 42
+    }
+    expectEqual(42, result)
+  }
+  if true {
+    let result = withOverriddenNSLocaleCurrentLocale(
+      NSLocale(localeIdentifier: "uk")) {
+      () -> Int in
+      expectEqual("uk", NSLocale.currentLocale().localeIdentifier)
+      return 42
+    }
+    expectEqual(42, result)
+  }
+}
+
+FoundationExtrasTests.test("withOverriddenNSLocaleCurrentLocale(NSLocale)/nested") {
+  withOverriddenNSLocaleCurrentLocale(
+    NSLocale(localeIdentifier: "uk")) {
+    () -> () in
+
+    expectCrashLater()
+
+    withOverriddenNSLocaleCurrentLocale(
+      NSLocale(localeIdentifier: "uk")) {
+      () -> () in
+
+      return ()
+    }
+  }
+}
+
+FoundationExtrasTests.test("withOverriddenNSLocaleCurrentLocale(String)") {
+  // Check two locales to make sure the behavior is correct even if one of
+  // these locales happens to be the same as the actual current locale.
+  if true {
+    let result = withOverriddenNSLocaleCurrentLocale("en_US") {
+      () -> Int in
+      expectEqual("en_US", NSLocale.currentLocale().localeIdentifier)
+      return 42
+    }
+    expectEqual(42, result)
+  }
+  if true {
+    let result = withOverriddenNSLocaleCurrentLocale("uk") {
+      () -> Int in
+      expectEqual("uk", NSLocale.currentLocale().localeIdentifier)
+      return 42
+    }
+    expectEqual(42, result)
+  }
+}
+
+runAllTests()
+
