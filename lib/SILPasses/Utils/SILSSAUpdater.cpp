96776e7969165a6584c3d3790aca82532cf3c889
Arnold Schwaighofer <aschwaighofer@apple.com>
2015-03-23 22:02:43 +0000

    Fix the header include
    Swift SVN r26453

--- lib/SILPasses/Utils/SILSSAUpdater.cpp
+++ lib/SILPasses/Utils/SILSSAUpdater.cpp
@@ -12,7 +12,7 @@
 
 #include "llvm/ADT/DenseMap.h"
 #include "llvm/ADT/SmallVector.h"
-#include "llvm/ADT/raw_stream.h"
+#include "llvm/Support/raw_ostream.h"
 #include "llvm/Transforms/Utils/SSAUpdaterImpl.h"
 #include "swift/Basic/Malloc.h"
 #include "swift/SIL/SILArgument.h"
