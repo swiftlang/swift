ad85a0f2df19e29bcd25f562db82433ed7542372
Arnold Schwaighofer <aschwaighofer@apple.com>
2015-03-25 20:22:42 +0000

    Revert "Nuke the intermittently failing test for now: emit-sib"
    Jordan told me I can use "REQUIRES: disabled" instead and so I do.
    
    This reverts commit r26551.
    
    Swift SVN r26552

---
+++ test/Driver/emit-sib-single-file.swift
@@ -0,0 +1,28 @@
+// RUN: %target-swiftc_driver -emit-sib %s -o %t.sib
+// RUN: %target-swiftc_driver %t.sib -o %t
+// RUN: %target-run %t | FileCheck %s
+
+// RUN: %target-swiftc_driver -c %t.sib -o %t.o
+// RUN: %target-swiftc_driver %t.o -o %t
+// RUN: %target-run %t | FileCheck %s
+
+// RUN: %target-swiftc_driver -emit-sibgen %s -o %t.sib
+// RUN: %target-swiftc_driver %t.sib -o %t
+// RUN: %target-run %t | FileCheck %s
+
+// RUN: %target-swiftc_driver -c %t.sib -o %t.o
+// RUN: %target-swiftc_driver %t.o -o %t
+// RUN: %target-run %t | FileCheck %s
+
+// REQUIRES: disabled
+// radar://20295441
+
+// CHECK: Hello World
+// CHECK: Hello Bob, today is Tuesday.
+
+func greet(name: String, day: String) -> String {
+  return "Hello \(name), today is \(day)."
+}
+
+println("Hello World")
+println(greet("Bob", "Tuesday"))
