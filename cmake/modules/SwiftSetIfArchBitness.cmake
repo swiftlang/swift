333447edcc1774911173483290999ea52b5ef70f
Ted Kremenek <kremenek@apple.com>
2015-03-26 01:52:42 +0000

    Add 'armv7s' to SwiftSetIfArchBitness.cmake.
    Swift SVN r26574

--- cmake/modules/SwiftSetIfArchBitness.cmake
+++ cmake/modules/SwiftSetIfArchBitness.cmake
@@ -7,7 +7,8 @@ function(set_if_arch_bitness var_name)
       ${ARGN})
 
   if("${SIA_ARCH}" STREQUAL "i386" OR
-     "${SIA_ARCH}" STREQUAL "armv7")
+     "${SIA_ARCH}" STREQUAL "armv7" OR
+     "${SIA_ARCH}" STREQUAL "armv7s")
     set("${var_name}" "${SIA_CASE_32_BIT}" PARENT_SCOPE)
   elseif("${SIA_ARCH}" STREQUAL "x86_64" OR
          "${SIA_ARCH}" STREQUAL "arm64")
