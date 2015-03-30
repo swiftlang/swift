163c0d1815237896622733e22da26a644925adc0
Nadav Rotem <nrotem@apple.com>
2015-03-30 16:42:20 +0000

    Fix a devirtualization testcase.
    A recent changes in the optimization pipe broke this test because the final
    inlining pass was removed (we rely on LLVM now). I updated this test with the
    new inlining decision and re-enabled the test.
    
    Swift SVN r26718

--- test/SILPasses/devirt_single_module_in_multple_files.swift
+++ test/SILPasses/devirt_single_module_in_multple_files.swift
@@ -1,6 +1,3 @@
-// REQUIRES: disabled
-// rdar://20337676
-
 // RUN: %target-swiftc_driver -module-name devirt_single_module_in_multiple_files -O  %s %S/Inputs/BaseProblem.swift %S/Inputs/Problems.swift -parse-as-library -emit-sil 2>&1 | FileCheck %s
 
 public func test() {
@@ -9,12 +6,12 @@ public func test() {
   e.evaluate(1)
 }
 
-// CHECK-LABEL: sil shared [transparent] [thunk] @_TTSf3cpfr74_TFFC38devirt_single_module_in_multiple_files9EvaluatorcFMS0_FT_S0_U_FT_Si_n_n___TTRXFo__dSi_XFo_iT__iSi_ 
+// CHECK-LABEL: sil shared @_TFFC38devirt_single_module_in_multiple_files9EvaluatorcFMS0_FT_S0_U_FT_Si
 // CHECK: %{{.*}} = class_method %{{.*}} : $Problem1, #Problem1.run!1 : Problem1 -> () -> Int , $@cc(method) @thin (@owned Problem1) -> Int
 // CHECK-NEXT: apply
 // CHECK: return
 
-// CHECK-LABEL: sil shared [transparent] [thunk] @_TTSf3cpfr75_TFFC38devirt_single_module_in_multiple_files9EvaluatorcFMS0_FT_S0_U0_FT_Si_n_n___TTRXFo__dSi_XFo_iT__iSi_
+// CHECK-LABEL: sil shared @_TFFC38devirt_single_module_in_multiple_files9EvaluatorcFMS0_FT_S0_U0_FT_Si 
 // CHECK: %{{.*}} = class_method %{{.*}} : $Problem2, #Problem2.run!1 : Problem2 -> () -> Int , $@cc(method) @thin (@owned Problem2) -> Int
 // CHECK-NEXT: apply
 // CHECK: return
