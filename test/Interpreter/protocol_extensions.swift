66b45990dc9fc333e34396e8da90d61ad69e0b72
Doug Gregor <dgregor@apple.com>
2015-03-30 21:53:44 +0000

    Add some more executable tests for protocol extensions.
    These are simple implementations of some operations we expect to see
    in the standard library, based on some work from Dmitri and Dave.
    
    Swift SVN r26729

--- test/Interpreter/protocol_extensions.swift
+++ test/Interpreter/protocol_extensions.swift
@@ -1,5 +1,6 @@
 // RUN: %target-run-simple-swift | FileCheck %s
 
+// Extend a protocol with a property.
 extension SequenceType {
   var myCount: Int {
     var result = 0
@@ -13,6 +14,7 @@ extension SequenceType {
 // CHECK: 4
 println(["a", "b", "c", "d"].myCount)
 
+// Extend a protocol with a function.
 extension CollectionType {
   var myIndices: Range<Index> {
     return Range(start: startIndex, end: endIndex)
@@ -31,6 +33,8 @@ extension CollectionType {
 // CHECK: 2
 println(["a", "b", "c", "d"].indexMatching({$0 == "c"})!)
 
+// Extend certain instances of a collection (those that have equatable
+// element types) with another algorithm.
 extension CollectionType where Self.Generator.Element : Equatable {
   func myIndexOf(element: Generator.Element) -> Index? {
     for i in indices(self) {
@@ -43,6 +47,67 @@ extension CollectionType where Self.Generator.Element : Equatable {
 
 // CHECK: 3
 println(["a", "b", "c", "d", "e"].myIndexOf("d")!)
+
+extension SequenceType {
+  public func myEnumerate() -> EnumerateSequence<Self> { 
+    return EnumerateSequence(self)
+  }
+}
+
+// CHECK: (0, a)
+// CHECK-NEXT: (1, b)
+// CHECK-NEXT: (2, c)
+for (index, element) in ["a", "b", "c"].myEnumerate() {
+  println("(\(index), \(element))")
+}
+
+extension SequenceType {
+  public func myReduce<T>(
+    initial: T, @noescape combine: (T, Self.Generator.Element) -> T
+  ) -> T { 
+    var result = initial
+    for value in self {
+      result = combine(result, value)
+    }
+    return result
+  }
+}
+
+// CHECK: 15
+println([1, 2, 3, 4, 5].myReduce(0, combine: +))
+
+
+extension SequenceType {
+  public func myZip<S : SequenceType>(s: S) -> Zip2<Self, S> {
+    return Zip2(self, s)
+  }
+}
+
+// CHECK: (1, a)
+// CHECK-NEXT: (2, b)
+// CHECK-NEXT: (3, c)
+for (a, b) in [1, 2, 3].myZip(["a", "b", "c"]) {
+  println("(\(a), \(b))")
+}
+
+// Mutating algorithms.
+extension MutableCollectionType
+  where Self.Index: RandomAccessIndexType, Self.Generator.Element : Comparable {
+
+  public mutating func myPartition(range: Range<Index>) -> Index {
+    return Swift.partition(&self, range)
+  }
+}
+
+// CHECK: 4 3 1 2 | 5 9 8 6 7 6
+var evenOdd = [5, 3, 6, 2, 4, 9, 8, 1, 7, 6]
+var evenOddSplit = evenOdd.myPartition(evenOdd.myIndices)
+for i in evenOdd.myIndices {
+  if i == evenOddSplit { print(" |") }
+  if i > 0 { print(" ") }
+  print(evenOdd[i])
+}
+println()
 
 // CHECK: DONE
 println("DONE")
