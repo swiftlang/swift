c85afb84053ec9a40bdceaf9d943d593d16e8c81
Mark Lacey <mark.lacey@apple.com>
2015-04-02 23:25:24 +0000

    Fix run line of compiler crasher that no longer crashes.
    Apparently fixed by tuple-related fixes that went in today.
    
    Swift SVN r26915

--- validation-test/compiler_crashers/1647-no-stacktrace.swift
+++ validation-test/compiler_crashers/1647-no-stacktrace.swift
@@ -1,4 +1,4 @@
-// RUN: not --crash %target-swift-frontend %s -emit-silgen
+// RUN: not %target-swift-frontend %s -emit-silgen
 // XFAIL: no_asserts
 
 // Distributed under the terms of the MIT license
