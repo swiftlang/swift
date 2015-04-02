0b40374d103b5b853c895cefbafe4728497ae470
Dave Abrahams <dabrahams@apple.com>
2015-04-02 21:55:22 +0000

    [stdlib] [Generator|Sequence]Of => Any[Generator|Sequence]
    Retire the old components now that the new ones have passed API review.
    
    <rdar://20406937> covers the migration fallout of this change.
    
    Swift SVN r26904

--- test/1_stdlib/Collection.swift
+++ test/1_stdlib/Collection.swift
@@ -167,6 +167,11 @@ func testCount() {
 }
 testCount()
 
+struct SequenceOnly<T: SequenceType> : SequenceType {
+  var base: T
+  func generate() -> T.Generator { return base.generate() }
+}
+
 func testUnderestimateCount() {
   // CHECK: testing underestimateCount
   println("testing underestimateCount")
@@ -175,7 +180,7 @@ func testUnderestimateCount() {
   // CHECK-NEXT: bidirectional: 5
   println("bidirectional: \(underestimateCount(dict))")
   // CHECK-NEXT: SequenceType only: 0
-  let s = SequenceOf(array)
+  let s = SequenceOnly(base: array)
   println("SequenceType only: \(underestimateCount(s))")
 }
 testUnderestimateCount()
