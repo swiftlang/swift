0fcea2dbb5cd78f2057d1472d0f56ecc3d9c9d95
Mark Lacey <mark.lacey@apple.com>
2015-04-03 19:05:31 +0000

    Remove XFAIL as this passes with no-asserts builds.
    Swift SVN r26947

--- validation-test/compiler_crashers/1647-no-stacktrace.swift
+++ validation-test/compiler_crashers/1647-no-stacktrace.swift
@@ -1,5 +1,4 @@
 // RUN: %target-swift-frontend %s -emit-silgen
-// XFAIL: no_asserts
 
 // Distributed under the terms of the MIT license
 // Test case submitted to project by https://github.com/radex
