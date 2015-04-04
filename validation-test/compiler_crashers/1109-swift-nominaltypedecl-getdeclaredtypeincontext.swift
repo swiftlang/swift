c3245dac9a27a207c4094ae74d417f4cf372302b
Mark Lacey <mark.lacey@apple.com>
2015-04-04 00:29:08 +0000

    Update tests that pass in no-assertions builds so that they do not run.
    Also remove one 'XFAIL: no_asserts' in a test that no longer fails in
    no-asserts builds.
    
    Swift SVN r26965

--- validation-test/compiler_crashers/1109-swift-nominaltypedecl-getdeclaredtypeincontext.swift
+++ validation-test/compiler_crashers/1109-swift-nominaltypedecl-getdeclaredtypeincontext.swift
@@ -1,5 +1,6 @@
 // RUN: not --crash %target-swift-frontend %s -parse
 // XFAIL: asan
+// REQUIRES: asserts
 
 // Distributed under the terms of the MIT license
 // Test case submitted to project by https://github.com/practicalswift (practicalswift)
--- validation-test/compiler_crashers/1927-swift-type-walk.swift
+++ validation-test/compiler_crashers/1927-swift-type-walk.swift
@@ -1,5 +1,4 @@
 // RUN: not %target-swift-frontend %s -parse
-// XFAIL: no_asserts
 
 // Distributed under the terms of the MIT license
 // Test case submitted to project by https://github.com/practicalswift (practicalswift)
