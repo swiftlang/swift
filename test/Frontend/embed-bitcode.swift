63184b250be375dfb4e468036684f0c042128d48
Manman Ren <mren@apple.com>
2015-04-09 21:57:29 +0000

    Fix testing case for rdar://20485995
    Swift SVN r27182

--- test/Frontend/embed-bitcode.swift
+++ test/Frontend/embed-bitcode.swift
@@ -1,5 +1,5 @@
 // REQUIRES: CPU=x86_64
-// RUN: %swift -target x86_64-apple-macosx10.9 -c -module-name someModule -embed-bitcode-marker  -o %t.o %s
+// RUN: %target-swift-frontend -c -module-name someModule -embed-bitcode-marker  -o %t.o %s
 // RUN: llvm-objdump -m -section __LLVM,__bitcode %t.o | FileCheck -check-prefix=MARKER %s
 // RUN: llvm-objdump -m -section __LLVM,__swift_cmdline %t.o | FileCheck -check-prefix=MARKER-CMD %s
 
