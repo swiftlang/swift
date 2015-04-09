857e32d9f5ae91c10ebc6bfec505d950c29d0cf4
Jordan Rose <jordan_rose@apple.com>
2015-04-09 18:25:14 +0000

    [test] Use %target-jit-run instead of %target-swift-frontend -interpret.
    rdar://problem/20474905
    
    Swift SVN r27174

--- test/Interpreter/SDK/autolinking.swift
+++ test/Interpreter/SDK/autolinking.swift
@@ -2,12 +2,12 @@
 // RUN: echo "int global() { return 42; }" | %clang -dynamiclib -o %t/libLinkMe.dylib -x c -
 // RUN: %target-swift-frontend -emit-module -parse-stdlib -o %t -module-name LinkMe -module-link-name LinkMe %S/../../Inputs/empty.swift
 
-// RUN: %target-swift-frontend -interpret -DIMPORT %s -I %t -L %t 2>&1
-// RUN: %target-swift-frontend -interpret -lLinkMe %s -L %t 2>&1
-// RUN: not %target-swift-frontend -interpret -lLinkMe %s 2>&1
+// RUN: %target-jit-run -DIMPORT %s -I %t -L %t 2>&1
+// RUN: %target-jit-run -lLinkMe %s -L %t 2>&1
+// RUN: not %target-jit-run -lLinkMe %s 2>&1
 
-// RUN: %target-swift-frontend -interpret -lLinkMe -DUSE_DIRECTLY %s -L %t 2>&1
-// RUN: not %target-swift-frontend -interpret -DUSE_DIRECTLY -lLinkMe %s 2>&1
+// RUN: %target-jit-run -lLinkMe -DUSE_DIRECTLY %s -L %t 2>&1
+// RUN: not %target-jit-run -DUSE_DIRECTLY -lLinkMe %s 2>&1
 
 
 // This is specifically testing autolinking for immediate mode. Please do not
