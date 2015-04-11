4fd7e8948fc5872a2ab7bfff829c3db0de3864e4
Greg Parker <gparker@apple.com>
2015-04-11 03:35:14 +0000

    [test] XFAIL StdlibUnittestRunAllTestsCalledTwice.swift on iOS.
    The test's check doesn't work on iOS device or simulator; perhaps 
    `not --crash` isn't doing its thing.
    
    
    Swift SVN r27231

--- validation-test/stdlib/StdlibUnittestRunAllTestsCalledTwice.swift
+++ validation-test/stdlib/StdlibUnittestRunAllTestsCalledTwice.swift
@@ -1,6 +1,9 @@
 // RUN: %target-build-swift -module-name a %s -o %t.out
 // RUN: not --crash %target-run %t.out | FileCheck %s
 
+// FIXME: this check doesn't work in the iOS simulator or device environment.
+// XFAIL: OS=ios
+
 //
 // Check that failures coming from StdlibUnittest are counted as failures by lit.
 //
