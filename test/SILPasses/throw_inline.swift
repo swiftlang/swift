b8538c96e4c89bed10cafca092dec08fdcebdb44
Nadav Rotem <nrotem@apple.com>
2015-04-15 22:37:01 +0000

    Make the testcase portable across 32/64bit targets.
    ssuming --only paths...
    
    Swift SVN r27335

--- test/SILPasses/throw_inline.swift
+++ test/SILPasses/throw_inline.swift
@@ -3,15 +3,15 @@
 // Make sure that we are able to inline try-apply instructions.
 
 @transparent
-func foo() throws -> Int {
+func foo() throws -> Int32 {
   return 999
 }
 
 // CHECK-LABLE: _TF12throw_inline3fooFzT_Si
-// CHECK: %0 = integer_literal $Builtin.Int64, 999
-// CHECK: %1 = struct $Int (%0 : $Builtin.Int64)
-// CHECK: return %1 : $Int
-func bar() throws  -> Int {
+// CHECK: %0 = integer_literal $Builtin.Int32, 999
+// CHECK: %1 = struct $Int32 (%0 : $Builtin.Int32)
+// CHECK: return %1 : $Int32
+func bar() throws  -> Int32 {
   return foo()
 }
 
