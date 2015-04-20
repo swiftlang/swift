af49ec17d2ce3ee9f930b3147ddbf4131f2349b2
Erik Eckstein <eeckstein@apple.com>
2015-04-20 07:24:43 +0000

    Removed unused empty comment lines.
    Swift SVN r27478

--- lib/SILPasses/GlobalPropertyOpt.cpp
+++ lib/SILPasses/GlobalPropertyOpt.cpp
@@ -9,10 +9,6 @@
 // See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
 //
 //===----------------------------------------------------------------------===//
-//
-// 
-//
-//===----------------------------------------------------------------------===//
 
 #define DEBUG_TYPE "globalpropertyopt"
 #include "swift/SILPasses/Passes.h"
