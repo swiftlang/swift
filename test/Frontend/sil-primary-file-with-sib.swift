a33c518e982c6c2395231d69ed5775b34f63aa42
Luqman Aden <luqman@apple.com>
2015-04-23 01:51:47 +0000

    tests: use substitution to pick the right llvm-link.
    Swift SVN r27621

--- test/Frontend/sil-primary-file-with-sib.swift
+++ test/Frontend/sil-primary-file-with-sib.swift
@@ -3,7 +3,7 @@
 // RUN: %target-build-swift -emit-sib %s -module-name test -o %t/test.sib
 // RUN: %target-build-swift -emit-ir %s -module-name test -o %t/test-orig.ll
 // RUN: %target-swift-frontend -emit-ir -primary-file %S/Inputs/sil-primary-file-with-sib.sil %t/test.sib -module-name test -o %t/test-func.ll
-// RUN: llvm-link %t/test-orig.ll -override %t/test-func.ll -o %t/test.bc
+// RUN: %llvm-link %t/test-orig.ll -override %t/test-func.ll -o %t/test.bc
 // RUN: %target-swift-frontend -c %t/test.bc -o %t/test.o
 // RUN: %target-build-swift %t/test.o -o %t/test
 // RUN: %target-run %t/test | FileCheck %s
