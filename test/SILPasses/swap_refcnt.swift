b5c87e715693a67f933a61f0a879470dbe4b464d
Nadav Rotem <nrotem@apple.com>
2015-04-26 22:49:16 +0000

    un-xfail and update two tests.
    Swift SVN r27776

--- test/SILPasses/swap_refcnt.swift
+++ test/SILPasses/swap_refcnt.swift
@@ -1,13 +1,12 @@
 // RUN: %target-swift-frontend -O -emit-sil %s | FileCheck %s
-// REQUIRES: kraken
 
 // Make sure we can swap two values in an array without retaining anything.
 
-//CHECK-LABEL: _TF11swap_refcnt11swapByIndexFT1ARGSaVSs4Int8_1xSi1ySi_T_
+//CHECK-LABEL: _TF11swap_refcnt11swapByIndex
 //CHECK-NOT: strong_retain
 //CHECK-NOT: strong_release
 //CHECK: return
-func swapByIndex(inout A : [Int8], x : Int, y : Int) {
+public func swapByIndex(inout A : [Int8], x : Int, y : Int) {
   swap(&A[x],&A[y])
 }
 
--- test/SILPasses/throw_inline.swift
+++ test/SILPasses/throw_inline.swift
@@ -1,14 +1,13 @@
 // RUN: %target-swift-frontend -emit-sil %s | FileCheck %s
-// REQUIRES: kraken
 
 // Make sure that we are able to inline try-apply instructions.
 
 @transparent
-func foo() throws -> Int32 {
+public func foo() throws -> Int32 {
   return 999
 }
 
-// CHECK-LABEL: _TF12throw_inline3fooFzT_Si
+// CHECK-LABEL: _TF12throw_inline3foo
 // CHECK: %0 = integer_literal $Builtin.Int32, 999
 // CHECK: %1 = struct $Int32 (%0 : $Builtin.Int32)
 // CHECK: return %1 : $Int32
