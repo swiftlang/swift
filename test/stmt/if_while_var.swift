a8b86533d21f2ef592dec62cc85f19c32fc0fa61
Chris Lattner <clattner@apple.com>
2015-04-30 04:40:20 +0000

    testcase for QoI of the new 'if let' model:  - Rewrite "if let x? = foo()" to "if let x = foo()"  - Rewrite "if let <complex refutable pattern> = foo()" to "if case let"  - Handle "if let case" just as well as "if case let" and fixit it to the correct form.
    Swift SVN r27960

--- test/stmt/if_while_var.swift
+++ test/stmt/if_while_var.swift
@@ -76,3 +76,18 @@ if a == 0, where b == 0 {}  // expected-error {{expected 'let' or 'var' in condi
 
 
 
+
+func testIfCase(a : Int?) {
+  if case nil = a where a != nil {}
+  if case let (b?) = a where b != 42 {}
+  
+  if let case (b?) = a where b != 42 {}  // expected-error {{pattern matching binding is spelled with 'case let', not 'let case'}} {{6-9=}} {{14-14= let}}
+  
+  if a != nil, let c = a, case nil = a { }
+  
+  if let p? = a {}  // expected-error {{pattern matching in a condition implicitly unwraps optionals}} {{11-12=}}
+  
+  
+  if let .Some(x) = a {}  // expected-error {{pattern matching in a condition requires the 'case' keyword}} {{6-6=case }}
+}
+
