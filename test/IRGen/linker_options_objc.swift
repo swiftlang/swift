2d872dc7d2ed65806ab4bf8c7307082cc41d4abc
David Farler <dfarler@apple.com>
2015-05-01 23:35:56 +0000

    Fix linker_options_objc test: incorrect REQUIRES and options
    Swift SVN r28056

--- test/IRGen/linker_options_objc.swift
+++ test/IRGen/linker_options_objc.swift
@@ -1,15 +1,15 @@
 // RUN: rm -rf %t
 // RUN: mkdir %t
-// RUN: %swift -target x86_64-unknown-linux-gnu -emit-module -parse-stdlib -o %t -module-name Empty -module-link-name swiftEmpty %S/../Inputs/empty.swift
-// RUN: %swift -target x86_64-unknown-linux-gnu %s -I %t -parse-stdlib -disable-objc-interop -module-name main -emit-ir -o - | FileCheck %s
+// RUN: %target-swiftc_driver -emit-module -o %t -module-name Empty -module-link-name swiftEmpty %S/../Inputs/empty.swift
+// RUN: %target-swiftc_driver %s -I %t -emit-ir | FileCheck %s
 
-// REQUIRES: objc
+// REQUIRES: objc_interop
 
 import Empty
 
 // Check that libobjc is always autolinked together with libswiftCore on
 // platforms that support Objective-C.
 
-// CHECK: {{.*}} = !{i32 6, !"Linker Options", !3}
-// CHECK: {{.*}} = !{!"-lswiftCore"}
-// CHECK: {{.*}} = !{!"-lobjc"}
+// CHECK: !{{.*}} = !{i32 6, !"Linker Options", !3}
+// CHECK-DAG: !{{.*}} = !{!"-lswiftCore"}
+// CHECK-DAG: !{{.*}} = !{!"-lobjc"}
