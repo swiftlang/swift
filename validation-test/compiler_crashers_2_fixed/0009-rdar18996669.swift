1c64eee1c428d01166efffd70bf0060217ba8c6a
Slava Pestov <spestov@apple.com>
2015-05-02 19:31:29 +0000

    Add testcase for <rdar://18996669> which seems to be fixed already
    Swift SVN r28091

---
+++ validation-test/compiler_crashers_2_fixed/0009-rdar18996669.swift
@@ -0,0 +1,25 @@
+// RUN: %target-swift-frontend %s -emit-ir -o /dev/null
+
+protocol P {
+  static var name: String { get }
+  init(i:Int)
+}
+
+class A: P {
+  class var name: String {
+    get { return "A" }
+  }
+  required init(i:Int) {}
+}
+class B: P {
+  class var name: String {
+    get { return "A" }
+  }
+  required init(i:Int) {}
+}
+
+let cls:P.Type = A.self
+
+let p:P = cls(i:1)
+
+println(p)
