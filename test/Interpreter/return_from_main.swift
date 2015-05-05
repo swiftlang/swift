3abc8c41a25902d00f118128f28e2275bd255189
Greg Parker <gparker@apple.com>
2015-05-05 09:42:23 +0000

    [test] Interpreter/return_from_main.swift requires the interpreter.
    Swift SVN r28163

--- test/Interpreter/return_from_main.swift
+++ test/Interpreter/return_from_main.swift
@@ -1,4 +1,6 @@
+// This test specifically exercises the interpreter's top-level error handler.
 // RUN: not %target-jit-run %s
+// REQUIRES: swift_interpreter
 
 // rdar://20809122
 enum Error : _ErrorType { case Foo }
