f99da03d47d6ccffdbd6025e21cb20ca3765e29c
Adrian Prantl <aprantl@apple.com>
2015-05-07 15:54:31 +0000

    Remove a redundant debug info test.
    Swift SVN r28265

--- test/DebugInfo/class.swift
+++
@@ -1,9 +0,0 @@
-// RUN: %target-swift-frontend -primary-file %s -emit-ir -g -o - | FileCheck %s
-
-// FIXME: This is more of a crash test than anything else.
-// CHECK: !DICompositeType(tag: DW_TAG_structure_type, name: "Tree",{{.*}} line: [[@LINE+1]]
-class Tree<T> {
-  var l, r: Tree<T>?
-}
-
-var T : Tree<Int>
