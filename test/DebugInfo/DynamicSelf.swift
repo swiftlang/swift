94834dccdfe2c666f0ac6e17e9ff9234c0394032
Adrian Prantl <aprantl@apple.com>
2015-05-07 18:36:30 +0000

    Debug info: Cleanup. Actually test for the type + sugar instead of just casually mentioning it in a comment.
    Thanks Dmitri for finding this!
    
    <rdar://problem/20856385> CHECK lines are superficial in DebugInfo tests
    
    Swift SVN r28278

--- test/DebugInfo/DynamicSelf.swift
+++ test/DebugInfo/DynamicSelf.swift
@@ -10,7 +10,8 @@ class C {
 extension C {
   class func Factory() -> Self {
     // Currently we emit the static type C for r.
-    // CHECK: !DILocalVariable(tag: DW_TAG_auto_variable, name: "r", {{.*}}line: [[@LINE+1]]
+    // CHECK: !DILocalVariable(tag: DW_TAG_auto_variable, name: "r", {{.*}}line: [[@LINE+2]], type: ![[SELFTY:[0-9]+]])
+    // CHECK: ![[SELFTY]] = !DIDerivedType(tag: DW_TAG_typedef, name: "_TtDC11DynamicSelf1C", {{.*}}, baseType: !"_TtC11DynamicSelf1C")
     let r = self(number: 0)
     return r
   }
