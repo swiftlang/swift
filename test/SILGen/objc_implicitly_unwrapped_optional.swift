98d69ab089a4aaf6d97ab2b005d7c8554b515e33
Mark Lacey <mark.lacey@apple.com>
2015-05-09 01:30:26 +0000

    Move ObjC portion of test to a separate file to unbreak Linux testing.
    Swift SVN r28356

---
+++ test/SILGen/objc_implicitly_unwrapped_optional.swift
@@ -0,0 +1,19 @@
+// RUN: %target-swift-frontend(mock-sdk: %clang-importer-sdk) -emit-silgen %s | FileCheck %s
+
+// REQUIRES: objc_interop
+
+import Foundation
+
+class C : Hive {}
+
+// CHECK: sil private @_TTVFC34objc_implicitly_unwrapped_optional1DcfMS0_FT5coderCSo7NSCoder_S0_
+// CHECK: bb0(%0 : $ImplicitlyUnwrappedOptional<NSCoder>, %1 : $D):
+// CHECK:   [[THUNK:%.*]] = function_ref @_TFC34objc_implicitly_unwrapped_optional1DcfMS0_FT5coderCSo7NSCoder_S0_
+// CHECK:   [[REF:%.*]] = apply [[THUNK]]
+// CHECK:   [[RESULT:%.*]] = enum $ImplicitlyUnwrappedOptional<D>, #ImplicitlyUnwrappedOptional.Some!enumelt.1, [[REF]] : $D
+// CHECK:   return [[RESULT]] : $ImplicitlyUnwrappedOptional<D>
+class D : C {
+  override init(coder aCoder: NSCoder) {
+    super.init(coder: aCoder)
+  }
+}
