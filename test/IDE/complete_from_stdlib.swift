1d8620326d36fa8a7df68c757e376245fb3d0818
Ben Langmuir <blangmuir@apple.com>
2015-05-14 22:46:58 +0000

    [CodeCompletion] Add some tests and FIXMEs for protocol extensions in the stdlib
    For rdar://problem/20305938
    
    Swift SVN r28587

--- test/IDE/complete_from_stdlib.swift
+++ test/IDE/complete_from_stdlib.swift
@@ -2,9 +2,23 @@
 // RUN: FileCheck %s -check-prefix=PLAIN_TOP_LEVEL < %t.toplevel.txt
 // RUN: FileCheck %s -check-prefix=NO_STDLIB_PRIVATE < %t.toplevel.txt
 
-// RUN: %target-swift-ide-test -code-completion -source-filename %s -code-completion-token=PRIVATE_NOMINAL_MEMBERS_1 > %t.members.txt
-// RUN: FileCheck %s -check-prefix=PRIVATE_NOMINAL_MEMBERS_1 < %t.members.txt
-// RUN: FileCheck %s -check-prefix=NO_STDLIB_PRIVATE < %t.members.txt
+// RUN: %target-swift-ide-test -code-completion -source-filename %s -code-completion-token=PRIVATE_NOMINAL_MEMBERS_1 > %t.members1.txt
+// RUN: FileCheck %s -check-prefix=PRIVATE_NOMINAL_MEMBERS_1 < %t.members1.txt
+// RUN: FileCheck %s -check-prefix=NO_STDLIB_PRIVATE < %t.members1.txt
+
+// RUN: %target-swift-ide-test -code-completion -source-filename %s -code-completion-token=PRIVATE_NOMINAL_MEMBERS_2 > %t.members2.txt
+// RUN: FileCheck %s -check-prefix=PRIVATE_NOMINAL_MEMBERS_2 < %t.members2.txt
+// FIXME: _prext_filter?
+// RUN-disabled: FileCheck %s -check-prefix=NO_STDLIB_PRIVATE < %t.members2.txt
+
+// RUN: %target-swift-ide-test -code-completion -source-filename %s -code-completion-token=PRIVATE_NOMINAL_MEMBERS_3 > %t.members3.txt
+// RUN: FileCheck %s -check-prefix=PRIVATE_NOMINAL_MEMBERS_3 < %t.members3.txt
+// FIXME: _prext_filter?
+// RUN-disabled: FileCheck %s -check-prefix=NO_STDLIB_PRIVATE < %t.members3.txt
+
+// RUN: %target-swift-ide-test -code-completion -source-filename %s -code-completion-token=PRIVATE_NOMINAL_MEMBERS_4 > %t.members4.txt
+// RUN: FileCheck %s -check-prefix=PRIVATE_NOMINAL_MEMBERS_4 < %t.members4.txt
+// RUN: FileCheck %s -check-prefix=NO_STDLIB_PRIVATE < %t.members4.txt
 
 // NO_STDLIB_PRIVATE: Begin completions
 // NO_STDLIB_PRIVATE-NOT: Decl[{{.*}}]{{[^:]*}}: _
@@ -27,3 +41,33 @@ func privateNominalMembers(a: String) {
 // PRIVATE_NOMINAL_MEMBERS_1-DAG: Decl[InstanceVar]/CurrNominal: startIndex[#Index#]{{; name=.+$}}
 // PRIVATE_NOMINAL_MEMBERS_1: End completions
 
+// FIXME: if we change this to (a: CollectionType) it crashes.
+func protocolExtCollection1<C : CollectionType>(a: C) {
+  a.#^PRIVATE_NOMINAL_MEMBERS_2^#
+}
+
+// PRIVATE_NOMINAL_MEMBERS_2: Begin completions
+// PRIVATE_NOMINAL_MEMBERS_2-DAG: Decl[InstanceMethod]/Super: map({#({{.*}}): (Self.Generator.Element) -> T##(Self.Generator.Element) -> T#})[#[T]#]{{; name=.+}}
+// PRIVATE_NOMINAL_MEMBERS_2-DAG-NOT: Decl{{.*}}: last
+// PRIVATE_NOMINAL_MEMBERS_2: End completions
+
+func protocolExtCollection2<C : CollectionType where C.Index : BidirectionalIndexType>(a: C) {
+  a.#^PRIVATE_NOMINAL_MEMBERS_3^#
+}
+
+// PRIVATE_NOMINAL_MEMBERS_3: Begin completions
+// PRIVATE_NOMINAL_MEMBERS_3-DAG: Decl[InstanceMethod]/Super: map({#({{.*}}): (Self.Generator.Element) -> T##(Self.Generator.Element) -> T#})[#[T]#]{{; name=.+}}
+// PRIVATE_NOMINAL_MEMBERS_3-DAG: Decl[InstanceVar]/Super:            last[#Self.Generator.Element?#]{{; name=.+}}
+// PRIVATE_NOMINAL_MEMBERS_3-DAG-NOT: Decl{{.*}}:         indexOf({#({{.*}}): Self.Generator.Element
+// PRIVATE_NOMINAL_MEMBERS_3-DAG: Decl[InstanceMethod]/Super:         indexOf({#({{.*}}): (Self.Generator.Element) -> Bool##(Self.Generator.Element) -> Bool#})[#Self.Index?#]{{; name=.+}}
+// PRIVATE_NOMINAL_MEMBERS_3: End completions
+
+func protocolExtArray<T : Equatable>(a: [T]) {
+  a.#^PRIVATE_NOMINAL_MEMBERS_4^#
+}
+// PRIVATE_NOMINAL_MEMBERS_4: Begin completions
+// PRIVATE_NOMINAL_MEMBERS_4-DAG: Decl[InstanceMethod]/Super: map({#({{.*}}): (Self.Generator.Element) -> T##(Self.Generator.Element) -> T#})[#[T]#]{{; name=.+}}
+// PRIVATE_NOMINAL_MEMBERS_4-DAG: Decl[InstanceVar]/Super:            last[#Self.Generator.Element?#]{{; name=.+}}
+// PRIVATE_NOMINAL_MEMBERS_4-DAG: Decl[InstanceMethod]/Super:         indexOf({#({{.*}}): Self.Generator.Element#})[#Self.Index?#]{{; name=.+}}
+// PRIVATE_NOMINAL_MEMBERS_4-DAG: Decl[InstanceMethod]/Super:         indexOf({#({{.*}}): (Self.Generator.Element) -> Bool##(Self.Generator.Element) -> Bool#})[#Self.Index?#]{{; name=.+}}
+// PRIVATE_NOMINAL_MEMBERS_4: End completions
