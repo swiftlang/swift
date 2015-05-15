da2ad487d7a050d32a34fcda82c395f31e96fefe
Adrian Prantl <aprantl@apple.com>
2015-05-15 17:19:36 +0000

    Cleanup: Remove the dependency on SwiftShims from this testcase and relax the prerequisites.
    Thanks Dmitri for noticing!
    
    Swift SVN r28628

---
+++ test/DebugInfo/Inputs/ClangModule.h
@@ -0,0 +1,2 @@
+@interface Foo
+@end
--- test/DebugInfo/modulecache.swift
+++ test/DebugInfo/modulecache.swift
@@ -1,25 +1,23 @@
-// UNSUPPORTED: OS=ios
-// REQUIRES: objc_interop
 // RUN: rm -rf %t && mkdir -p %t
 
 // Clang-import a module.
-import SwiftShims
+import ClangModule
 
 // Note: This test is highly dependent on the clang module cache
 // format, but it is testing specifics of the module cache.
 
 // 1. Test that swift-ide-test creates a thin module without debug info.
 
-// RUN: %swift-ide-test_plain -print-usrs -target %target-triple -module-cache-path %t -source-filename %s
-// RUN: %target-swift-frontend %s -c -g -o %t.o -module-cache-path %t
-// RUN: file %t/*/SwiftShims-*.pcm | grep data
+// RUN: %swift-ide-test_plain -print-usrs -target %target-triple -module-cache-path %t  -I %S/Inputs -source-filename %s
+// RUN: %target-swift-frontend %s -c -g -o %t.o -module-cache-path %t -I %S/Inputs
+// RUN: file %t/*/ClangModule-*.pcm | grep data
 
 // 2. Test that swift is creating clang modules with debug info.
 
 // RUN: rm -rf %t && mkdir -p %t
-// RUN: %target-swift-frontend %s -c -g -o %t.o -module-cache-path %t
-// RUN: file %t/*/SwiftShims-*.pcm | grep Mach-O
+// RUN: %target-swift-frontend %s -c -g -o %t.o -module-cache-path %t -I %S/Inputs
+// RUN: file %t/*/ClangModule-*.pcm | grep Mach-O
 
 // 3. Test that swift-ide-check will not share swiftc's module cache.
 
-// RUN: %swift-ide-test_plain -generate-module-api-description -- -target %target-triple -module-cache-path %t %s
+// RUN: %swift-ide-test_plain -print-usrs -target %target-triple -module-cache-path %t  -I %S/Inputs -source-filename %s
