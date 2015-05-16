eac42662dcbc048783b314db376fa85888342061
Dmitri Hrybenko <dgribenko@apple.com>
2015-05-16 00:29:15 +0000

    Add a failing test case for rdar://20985062
    Swift SVN r28653

---
+++ validation-test/compiler_crashers_2/0011-rdar20985062.swift
@@ -0,0 +1,14 @@
+// RUN: not --crash %target-swift-frontend %s -emit-ir
+
+// rdar://20985062
+
+class A {
+  func foo(a: [(x: Int, y: Double)]) {}
+}
+
+func accept<T>(t: T.Type, inout _ value: T) {}
+
+typealias TheType = (A) -> ([(x: Int, y: Double)]) -> ()
+var curried = A.foo
+accept(TheType.self, &curried)
+
