7ff1e80dacd52aadd25c16f7eef84fe9c6642e73
Chris Lattner <clattner@apple.com>
2015-05-20 23:04:18 +0000

    make these error message checks more specific, NFC.
    Swift SVN r28856

--- test/Parse/optional_lvalues.swift
+++ test/Parse/optional_lvalues.swift
@@ -23,34 +23,34 @@ mutT! = T()
 mutT!.mutS = S()
 mutT!.mutS! = S()
 mutT!.mutS!.x = 0
-mutT!.mutS!.y = 0 // expected-error{{cannot assign}}
-mutT!.immS = S() // expected-error{{cannot assign}}
-mutT!.immS! = S() // expected-error{{cannot assign}}
-mutT!.immS!.x = 0 // expected-error{{cannot assign}}
-mutT!.immS!.y = 0 // expected-error{{cannot assign}}
+mutT!.mutS!.y = 0 // expected-error{{cannot assign to 'let' property 'y'}}
+mutT!.immS = S() // expected-error{{cannot assign to 'let' property 'immS'}}
+mutT!.immS! = S() // expected-error{{cannot assign to the result of this expression}}
+mutT!.immS!.x = 0 // expected-error{{cannot assign to 'x', base has immutable type 'S'}}
+mutT!.immS!.y = 0 // expected-error{{cannot assign to 'let' property 'y'}}
 
-immT! = T() // expected-error{{cannot assign}}
-immT!.mutS = S() // expected-error{{cannot assign}}
-immT!.mutS! = S() // expected-error{{cannot assign}}
-immT!.mutS!.x = 0 // expected-error{{cannot assign}}
-immT!.mutS!.y = 0 // expected-error{{cannot assign}}
-immT!.immS = S() // expected-error{{cannot assign}}
-immT!.immS! = S() // expected-error{{cannot assign}}
-immT!.immS!.x = 0 // expected-error{{cannot assign}}
-immT!.immS!.y = 0 // expected-error{{cannot assign}}
+immT! = T() // expected-error{{cannot assign to the result of this expression}}
+immT!.mutS = S() // expected-error{{cannot assign to 'mutS', base has immutable type 'T'}}
+immT!.mutS! = S() // expected-error{{cannot assign to the result of this expression}}
+immT!.mutS!.x = 0 // expected-error{{cannot assign to 'x', base has immutable type 'S'}}
+immT!.mutS!.y = 0 // expected-error{{cannot assign to 'let' property 'y'}}
+immT!.immS = S() // expected-error{{cannot assign to 'let' property 'immS'}}
+immT!.immS! = S() // expected-error{{cannot assign to the result of this expression}}
+immT!.immS!.x = 0 // expected-error{{cannot assign to 'x', base has immutable type 'S'}}
+immT!.immS!.y = 0 // expected-error{{cannot assign to 'let' property 'y'}}
 
 var mutIUO: T! = nil
 let immIUO: T! = nil
 
 mutIUO!.mutS = S()
-mutIUO!.immS = S() // expected-error{{cannot assign}}
-immIUO!.mutS = S() // expected-error{{cannot assign}}
-immIUO!.immS = S() // expected-error{{cannot assign}}
+mutIUO!.immS = S() // expected-error{{cannot assign to 'let' property 'immS'}}
+immIUO!.mutS = S() // expected-error{{cannot assign to 'mutS', base has immutable type 'T'}}
+immIUO!.immS = S() // expected-error{{cannot assign to 'let' property 'immS'}}
 
 mutIUO.mutS = S()
-mutIUO.immS = S() // expected-error{{cannot assign}}
-immIUO.mutS = S() // expected-error{{cannot assign}}
-immIUO.immS = S() // expected-error{{cannot assign}}
+mutIUO.immS = S() // expected-error{{cannot assign to 'let' property 'immS'}}
+immIUO.mutS = S() // expected-error{{cannot assign to 'mutS', base has immutable type 'T!'}}
+immIUO.immS = S() // expected-error{{cannot assign to 'let' property 'immS'}}
 
 func foo(x: Int) {}
 
