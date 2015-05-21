6e7d6b980bba65fab7a4ed8468cfc25100efe9f6
Dave Abrahams <dabrahams@apple.com>
2015-05-21 23:39:10 +0000

    [stdlib] Eliminate OptionSetDispatchType
    Iteration N+1 of the default implementations hack applied!
    Also clean up the code voice in many comments.
    
    Swift SVN r28895

--- stdlib/public/core/SetAlgebra.swift
+++ stdlib/public/core/SetAlgebra.swift
@@ -45,13 +45,14 @@
 ///   - `x.isSubsetOf(y)` iff `y.isSupersetOf(x)`
 ///   - `x.isStrictSupersetOf(y)` iff `x.isSupersetOf(y) && x != y`
 ///   - `x.isStrictSubsetOf(y)` iff `x.isSubsetOf(y) && x != y`
-public protocol SetAlgebraType : Equatable, ArrayLiteralConvertible,
-    SetAlgebraDispatchType {
+public protocol SetAlgebraType : Equatable, ArrayLiteralConvertible {
+
+  typealias DefaultImplementations = ()
 
   /// A type for which `Self` provides a containment test.
   typealias Element
   
-  /// Create an empty instance.
+  /// Creates an empty set.
   ///
   /// - Equivalent to `[] as Self`
   init()
@@ -72,14 +73,14 @@ public protocol SetAlgebraType : Equatable, ArrayLiteralConvertible,
   /// but not in both `self` and `other`.
   func exclusiveOr(other: Self) -> Self
 
-  /// If `member` is not already contained in `self`, insert it.
+  /// If `member` is not already contained in `self`, inserts it.
   ///
   /// - Equivalent to `self.unionInPlace([member])`
   /// - Postcondition: `self.contains(member)`
   mutating func insert(member: Element)
   
-  /// If `member` is contained in `self`, remove and return it.
-  /// Otherwise, remove all elements subsumed by `member` and return
+  /// If `member` is contained in `self`, removes and returns it.
+  /// Otherwise, removes all elements subsumed by `member` and returns
   /// `nil`.
   ///
   /// - Postcondition: `self.intersect([member]).isEmpty`
@@ -91,18 +92,51 @@ public protocol SetAlgebraType : Equatable, ArrayLiteralConvertible,
   /// - Postcondition: `self.isSupersetOf(other)`
   mutating func unionInPlace(other: Self)
 
-  /// Remove all elements of `self` that are not also present in
+  /// Removes all elements of `self` that are not also present in
   /// `other`.
   ///
   /// - Equivalent to replacing `self` with `self.intersect(other)`
   /// - Postcondition: `self.isSubsetOf(other)`
   mutating func intersectInPlace(other: Self)
 
-  /// Replace `self` with a set containing all elements contained in
+  /// Replaces `self` with a set containing all elements contained in
   /// either `self` or `other`, but not both.
   ///
   /// - Equivalent to replacing `self` with `self.exclusiveOr(other)`
   mutating func exclusiveOrInPlace(other: Self)  
+
+  //===--- Requirements with default implementations ----------------------===//
+  /// Return true iff `self.intersect(other).isEmpty`.
+  func subtract(other: Self) -> Self
+  /// Return true iff every element of `self` is contained in `other`.
+  func isSubsetOf(other: Self) -> Bool
+  /// Return true iff `self.intersect(other).isEmpty`.
+  func isDisjointWith(other: Self) -> Bool
+  /// Return true iff every element of `other` is contained in `self`.
+  func isSupersetOf(other: Self) -> Bool
+  /// Return true iff `self.contains(e)` is `false` for all `e`.
+  var isEmpty: Bool { get }
+  
+  /// Creates the set containing all elements of `sequence`.
+  init<S : SequenceType where S.Generator.Element == Element>(_ sequence: S)
+
+  /// Removes all elements of `other` from `self`.
+  ///
+  /// - Equivalent to replacing `self` with `self.subtract(other)`.
+  mutating func subtractInPlace(other: Self)
+
+  /// Returns `true` iff `a` subsumes `b`.
+  ///
+  /// - Equivalent to `([a] as Self).isSupersetOf([b])`
+  static func element(a: Element, subsumes b: Element) -> Bool
+
+  /// Returns `true` iff `a` is disjoint with `b`.
+  ///
+  /// Two elements are disjoint when neither one subsumes the other.
+  ///
+  /// - SeeAlso: `Self.element(_, subsumes:_)`
+  static func element(a: Element, isDisjointWith b: Element) -> Bool
+  
 }
 
 /// `SetAlgebraType` requirements for which default implementations
@@ -111,8 +145,8 @@ public protocol SetAlgebraType : Equatable, ArrayLiteralConvertible,
 /// - Note: A type conforming to `SetAlgebraType` can implement any of
 ///   these initializers or methods, and those implementations will be
 ///   used in lieu of these defaults.
-extension SetAlgebraType {
-  /// The set containing all elements of `sequence`.
+extension SetAlgebraType where DefaultImplementations == () {
+  /// Creates the set containing all elements of `sequence`.
   public init<
     S : SequenceType where S.Generator.Element == Element
   >(_ sequence: S) {
@@ -120,7 +154,7 @@ extension SetAlgebraType {
     for e in sequence { insert(e) }
   }
 
-  /// The set containing all elements of the given `arrayLiteral`.
+  /// Creates a set containing all elements of the given `arrayLiteral`.
   ///
   /// This initializer allows an array literal containing
   /// `Self.Element` to represent an instance of the set, wherever it
@@ -129,7 +163,7 @@ extension SetAlgebraType {
     self.init(arrayLiteral)
   }
 
-  /// Remove all elements of `other` from `self`.
+  /// Removes all elements of `other` from `self`.
   ///
   /// - Equivalent to replacing `self` with `self.subtract(other)`.
   final
@@ -137,37 +171,37 @@ extension SetAlgebraType {
     self.intersectInPlace(self.exclusiveOr(other))
   }
 
-  /// Return true iff every element of `self` is contained in `other`.
+  /// Returns true iff every element of `self` is contained in `other`.
   final
   public func isSubsetOf(other: Self) -> Bool {
     return self.intersect(other) == self
   }
 
-  /// Return true iff every element of `other` is contained in `self`.
+  /// Returns true iff every element of `other` is contained in `self`.
   final
   public func isSupersetOf(other: Self) -> Bool {
     return other.isSubsetOf(self)
   }
 
-  /// Return true iff `self.intersect(other).isEmpty`.
+  /// Returns true iff `self.intersect(other).isEmpty`.
   final
   public func isDisjointWith(other: Self) -> Bool {
     return self.intersect(other).isEmpty
   }
 
-  /// Return true iff `self.intersect(other).isEmpty`.
+  /// Returns true iff `self.intersect(other).isEmpty`.
   final
   public func subtract(other: Self) -> Self {
     return self.intersect(self.exclusiveOr(other))
   }
 
-  /// Return true iff `self.contains(e)` is `false` for all `e`.
+  /// Returns true iff `self.contains(e)` is `false` for all `e`.
   final
   public var isEmpty: Bool {
     return self == Self()
   }
 
-  /// Return true iff every element of `other` is contained in `self`
+  /// Returns true iff every element of `other` is contained in `self`
   /// and `self` contains an element that is not contained in `other`.
   final
   public func isStrictSupersetOf(other: Self) -> Bool {
@@ -198,46 +232,4 @@ extension SetAlgebraType {
   public static func element(a: Element, isDisjointWith b: Element) -> Bool {
     return ([a] as Self).isDisjointWith([b])
   }
-}
-
-/// Dispatching protocol for `SetAlgebraType`.
-///
-/// For all intents and purposes, you can and should ignore this
-/// protocol.  All of its requirements are restated in the derived
-/// `SetAlgebraType` protocol, either as requirements, or as
-/// implementations in its protocol extension.
-/// `SetAlgebraDispatchType` exists merely so that default generic
-/// algorithms on `SetAlgebraType` will dispatch to more specific
-/// operations given by a type that conforms to `SetAlgebraType`:
-///
-///     /// exercise two SetAlgebraType operations
-///     func differenceIsEmpty<S: SetAlgebraType>(s1: S, s2: S) -> Bool {
-///       return s1.subtract(s2).isEmpty
-///     }
-///
-///     /// A type that supports set algebra
-///     struct MySet : SetAlgebraType {
-///       var isEmpty: Bool { /* specialized isEmpty implementation */ }
-///       ...                 /* no subtract implementation given */
-///     }
-///
-///     // calls the **specialized implementation of isEmpty** given above,
-///     // and the **default implementation of subtract** given in the
-///     // SetAlgebraType protocol extension.
-///     differenceIsEmpty(MySet(), MySet()) 
-///
-/// - SeeAlso: `SetAlgebraType` for details of the APIs required here.
-public protocol SetAlgebraDispatchType {
-  typealias Element
-  func subtract(other: Self) -> Self
-  func isSubsetOf(other: Self) -> Bool
-  func isDisjointWith(other: Self) -> Bool
-  func isSupersetOf(other: Self) -> Bool
-  var isEmpty: Bool { get }
-  
-  init<S : SequenceType where S.Generator.Element == Element>(_ sequence: S)
-  mutating func subtractInPlace(other: Self)
-
-  static func element(a: Element, subsumes b: Element) -> Bool  
-  static func element(a: Element, isDisjointWith b: Element) -> Bool
 }
