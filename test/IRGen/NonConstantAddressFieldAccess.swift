73e2dbababceb8a40d54b4d05e995680f3a40887
Arnold Schwaighofer <aschwaighofer@apple.com>
2015-05-21 21:46:38 +0000

    Fix test case for 32 bit
    Swift SVN r28888

--- test/IRGen/NonConstantAddressFieldAccess.swift
+++ test/IRGen/NonConstantAddressFieldAccess.swift
@@ -2,8 +2,8 @@
 
 // REQUIRES: objc_interop
 
-// CHECK: @_TWvdvC1a12ObjCSubclass5fieldSi = global i64 16
-// CHECK: @_TWvdvC1a12ObjCSubclass5fieldSi = external global i64
+// CHECK: @_TWvdvC1a12ObjCSubclass5fieldSi = global i{{[0-9]+}}
+// CHECK: @_TWvdvC1a12ObjCSubclass5fieldSi = external global i{{[0-9]+}}
 
 func test(o: ObjCSubclass) {
   o.field = 10
