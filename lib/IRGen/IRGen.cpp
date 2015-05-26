4b5217ec7829e0dc2bbb21aab70364c587e6623f
Arnold Schwaighofer <aschwaighofer@apple.com>
2015-05-26 23:55:39 +0000

    Revert "Disable LLVM's MergeFunctions pass"
    Renable the merge function pass. The bug in LLVM was fixed.
    
    This reverts commit r28483.
    
    Swift SVN r29045

--- lib/IRGen/IRGen.cpp
+++ lib/IRGen/IRGen.cpp
@@ -181,6 +181,7 @@ static bool performLLVM(IRGenOptions &Opts, DiagnosticEngine &Diags,
     PMBuilder.Inliner = llvm::createFunctionInliningPass(200);
     PMBuilder.SLPVectorize = !Opts.DisableLLVMSLPVectorizer;
     PMBuilder.LoopVectorize = true;
+    PMBuilder.MergeFunctions = true;
   } else {
     PMBuilder.OptLevel = 0;
     if (!Opts.DisableLLVMOptzns)
