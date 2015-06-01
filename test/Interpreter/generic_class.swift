efcbf64f47999c77c7b9ab765b0c8ebf2161b33d
Arnold Schwaighofer <aschwaighofer@apple.com>
2015-06-01 21:23:33 +0000

    Interpreter/generic_class.swift test fails in optimize mode
    rdar://21188939
    
    Swift SVN r29207

--- test/Interpreter/generic_class.swift
+++ test/Interpreter/generic_class.swift
@@ -1,6 +1,8 @@
 // Also run this test in optimize test modes.
 // REQUIRES: optimize_test
 
+// XFAIL: swift_test_mode_optimize
+
 // RUN: rm -rf %t  &&  mkdir %t
 // RUN: %target-build-swift %s -o %t/a.out
 // RUN: %target-run %t/a.out | FileCheck %s
