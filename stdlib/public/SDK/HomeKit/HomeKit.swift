cd968aa16476c71c35cd5af38a09ae7ba587c2a9
Dmitri Hrybenko <dgribenko@apple.com>
2015-06-02 20:59:01 +0000

    HomeKit overlay: adjust for new SDK
    Swift SVN r29251

--- stdlib/public/SDK/HomeKit/HomeKit.swift
+++ stdlib/public/SDK/HomeKit/HomeKit.swift
@@ -1,10 +1,9 @@
 @exported import HomeKit
 import Foundation
 
-// FIXME: HMErrorDomain has @available(iOS 8.0), but NMErrorCode does not.
+@available(iOS 8.0, *)
 extension HMErrorCode : _BridgedNSError {
   public static var _NSErrorDomain: String {
-    guard #available(iOS 8.0, *) else { return "" }
     return HMErrorDomain
   }
 }
