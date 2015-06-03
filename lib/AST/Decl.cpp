ec6b6b586c38915ca1982afdf5cb087e68ce8e34
Slava Pestov <spestov@apple.com>
2015-06-03 00:01:30 +0000

    IRGen: No longer refuses to emit @objc thunks for stuff in generic context
    For example, overriding methods in a generic subclass of an @objc
    class works.
    
    Swift SVN r29261

--- lib/AST/Decl.cpp
+++ lib/AST/Decl.cpp
@@ -3055,13 +3055,6 @@ Pattern *VarDecl::getParentPattern() const {
 /// Return true if this stored property needs to be accessed with getters and
 /// setters for Objective-C.
 bool AbstractStorageDecl::hasObjCGetterAndSetter() const {
-  // We don't export generic methods or subclasses to IRGen yet.
-  auto *DC = getDeclContext();
-  if (DC->getDeclaredTypeInContext() &&
-      DC->getDeclaredTypeInContext()->is<BoundGenericType>() &&
-      !isa<ProtocolDecl>(DC))
-    return false;
-
   if (auto override = getOverriddenDecl())
     return override->hasObjCGetterAndSetter();
 
