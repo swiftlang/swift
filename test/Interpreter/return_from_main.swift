492b52f6f2446905b116424d76bd4d67eb144858
Joe Groff <jgroff@apple.com>
2015-06-16 20:12:06 +0000

    Test that message throwing from main identifies error.
    Swift SVN r29402

--- test/Interpreter/return_from_main.swift
+++ test/Interpreter/return_from_main.swift
@@ -1,7 +1,8 @@
 // This test specifically exercises the interpreter's top-level error handler.
-// RUN: not --crash %target-jit-run %s
+// RUN: not --crash %target-jit-run %s 2>&1 | FileCheck %s
 // REQUIRES: swift_interpreter
 
 // rdar://20809122
+// CHECK: Error raised at top level: return_from_main.Error.Foo
 enum Error : ErrorType { case Foo }
 throw Error.Foo
