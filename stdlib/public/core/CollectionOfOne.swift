d0136445880d69eef4a2487d1e14f6daed228475
Dmitri Hrybenko <dgribenko@apple.com>
2015-06-24 20:41:52 +0000

    stdlib: rename generic parameters from T to Element
    ... for GeneratorOfOne and CollectionOfOne.
    
    Part of rdar://21429126
    
    Swift SVN r29621

--- stdlib/public/core/CollectionOfOne.swift
+++ stdlib/public/core/CollectionOfOne.swift
@@ -10,11 +10,11 @@
 //
 //===----------------------------------------------------------------------===//
 
-/// A generator that produces one or fewer instances of `T`.
-public struct GeneratorOfOne<T> : GeneratorType, SequenceType {
+/// A generator that produces one or fewer instances of `Element`.
+public struct GeneratorOfOne<Element> : GeneratorType, SequenceType {
   /// Construct an instance that generates `element!`, or an empty
   /// sequence if `element == nil`.
-  public init(_ element: T?) {
+  public init(_ element: Element?) {
     self.elements = element
   }
 
@@ -30,16 +30,16 @@ public struct GeneratorOfOne<T> : GeneratorType, SequenceType {
   /// - Requires: `next()` has not been applied to a copy of `self`
   ///   since the copy was made, and no preceding call to `self.next()`
   ///   has returned `nil`.
-  public mutating func next() -> T? {
+  public mutating func next() -> Element? {
     let result = elements
     elements = .None
     return result
   }
-  var elements: T?
+  var elements: Element?
 }
 
-/// A collection containing a single element of type `T`.
-public struct CollectionOfOne<T> : CollectionType {
+/// A collection containing a single element of type `Element`.
+public struct CollectionOfOne<Element> : CollectionType {
   /// A type that represents a valid position in the collection.
   ///
   /// Valid indices consist of the position of every element and a
@@ -47,7 +47,7 @@ public struct CollectionOfOne<T> : CollectionType {
   public typealias Index = Bit
 
   /// Construct an instance containing just `element`.
-  public init(_ element: T) {
+  public init(_ element: Element) {
     self.element = element
   }
 
@@ -67,14 +67,14 @@ public struct CollectionOfOne<T> : CollectionType {
   /// Return a *generator* over the elements of this *sequence*.
   ///
   /// - Complexity: O(1).
-  public func generate() -> GeneratorOfOne<T> {
+  public func generate() -> GeneratorOfOne<Element> {
     return GeneratorOfOne(element)
   }
 
   /// Access the element at `position`.
   ///
   /// - Requires: `position == .Zero`.
-  public subscript(position: Index) -> T {
+  public subscript(position: Index) -> Element {
     _precondition(position == .Zero, "Index out of range")
     return element
   }
@@ -84,6 +84,6 @@ public struct CollectionOfOne<T> : CollectionType {
     return 1
   }
 
-  let element: T
+  let element: Element
 }
 
