1b79092e24f8f8ae1f509164b819aeec58c2afb8
Dave Abrahams <dabrahams@apple.com>
2015-06-26 01:15:50 +0000

    [stdlib] Use StdlibUnittest to test Generator
    Swift SVN r29723

--- test/1_stdlib/Generator.swift
+++ test/1_stdlib/Generator.swift
@@ -1,48 +1,34 @@
-// RUN: %target-run-simple-swift | FileCheck %s
+// RUN: %target-run-stdlib-swift
 // REQUIRES: executable_test
+
+import StdlibUnittest
+var tests = TestSuite("Generator")
 
 // Check to make sure we are actually getting Optionals out of this
 // GeneratorType
-var w = (1..<2).generate()
-var maybe_one = w.next()
-
-var is_ = "is"
-var is_not = "is not"
-
-print("maybe_one \(maybe_one == nil ? is_ : is_not) None")
-// CHECK: maybe_one is not None
-
-switch maybe_one {
-case .Some(var one):
-  print("one \(one == 1 ? is_ : is_not) 1")
-  // CHECK: one is 1
-case .None:
-  ()
+tests.test("Range") {
+  var w = (1..<2).generate()
+  var maybe_one = w.next()
+  expectType(Optional<Int>.self, &maybe_one)
+  expectEqual(1, maybe_one)
+  expectEmpty(w.next())
 }
 
-print("w.next() \(w.next() == nil ? is_ : is_not) None")
-// CHECK: w.next() is None
-
-// Test SequenceType protocol
-w = (1..<2).generate()
-for x in w { 
-  print("x is \(x)") 
+tests.test("RangeGeneratorConformsToSequence") {
+  for x in (1..<2).generate() { 
+    expectEqual(1, x)
+  }
 }
 
 // Test round-trip GeneratorType/GeneratorType adaptation
-var x = (1..<7).generate()
-var y = x
-var z = Zip2Generator(y, (1..<7).generate())
-
-for a in GeneratorSequence(z) {
-  print("\(a.0), \(a.1)")
+tests.test("GeneratorSequence") {
+  var r = 1..<7
+  var x = MinimalGenerator(Array(r))
+  for a in GeneratorSequence(x) {
+    expectEqual(r.startIndex, a)
+    ++r.startIndex
+  }
+  expectEqual(r.startIndex, r.endIndex)
 }
-// CHECK:      1, 1
-// CHECK-NEXT: 2, 2
-// CHECK-NEXT: 3, 3
-// CHECK-NEXT: 4, 4
-// CHECK-NEXT: 5, 5
-// CHECK-NEXT: 6, 6
 
-print("done.")
-// CHECK: done.
+runAllTests()
