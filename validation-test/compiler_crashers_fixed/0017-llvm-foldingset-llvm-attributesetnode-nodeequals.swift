913a03cb0c1745a7b8bbc2185d2daa69fa497762
Slava Pestov <spestov@apple.com>
2015-06-26 08:10:30 +0000

    Update test for r29737
    Swift SVN r29739

--- validation-test/compiler_crashers_fixed/0017-llvm-foldingset-llvm-attributesetnode-nodeequals.swift
+++ validation-test/compiler_crashers_fixed/0017-llvm-foldingset-llvm-attributesetnode-nodeequals.swift
@@ -4,6 +4,6 @@
 // Test case submitted to project by https://github.com/jvasileff (John Vasileff)
 // This bug is NOT triggered when compiling with -O.
 
-func f<T : BooleanType>(b: T) { // expected-note {{in call to function 'f'}}
+func f<T : BooleanType>(b: T) {
 }
-f(true as BooleanType) // expected-error {{generic parameter 'T' is constrained to a non-@objc protocol type 'BooleanType' and cannot be bound to an existential type}}
+f(true as BooleanType) // expected-error {{cannot invoke 'f' with an argument list of type '(BooleanType)'}} // expected-note {{expected an argument list of type '(T)'}}
