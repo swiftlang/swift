c8241b065ac9bd8ed1919469b24ab4e14673c807
Dave Abrahams <dabrahams@apple.com>
2015-06-27 02:31:23 +0000

    [emacs support] Fix regexps for matching StdlibUnittest failures
    Swift SVN r29751

--- utils/swift-project-settings.el
+++ utils/swift-project-settings.el
@@ -237,12 +237,12 @@ takes precedence for files in the Swift project"
     ad-do-it))
 
 (push 'swift-stdlibunittest1 compilation-error-regexp-alist)
-(push `(swift-stdlibunittest1 "^\\(?:out\\|err\\)>>>.* \\(?:failed\\(?: at\\|.*file\\)\\|.*: file\\) \\(.*[^,]\\), line \\([0-9]+\\)$"
+(push `(swift-stdlibunittest1 "^.*\\(?:failed\\(?: at\\|.*file\\)\\|.*: file\\) \\([^,]*\\), line \\([0-9]+\\)$"
               1 2 ,(not :column) ,(not :just-a-warning))
       compilation-error-regexp-alist-alist)
 
 (push 'swift-stdlibunittest2 compilation-error-regexp-alist)
-(push `(swift-stdlibunittest2 "^\\(?:out\\|err\\)>>> *#[0-9]+: \\(.+\\):\\([0-9]+\\)\\(?: +.*\\)?$"
+(push `(swift-stdlibunittest2 "^\\(?:\\(?:out\\|err\\)>>>* \\)?#[0-9]+: \\(.+\\):\\([0-9]+\\)\\(?: +.*\\)?$"
               1 2 ,(not :column) ,(not :just-a-warning))
       compilation-error-regexp-alist-alist)
     
