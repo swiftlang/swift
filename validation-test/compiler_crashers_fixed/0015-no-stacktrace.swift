24eee303dc21eca4f8325b561154377f8c09003c
Chris Lattner <clattner@apple.com>
2015-07-02 03:25:12 +0000

    update for diagnostic change.  Remove -verify, validation tests should not be checking diagnostics.
    Swift SVN r29880

--- validation-test/compiler_crashers_fixed/0015-no-stacktrace.swift
+++ validation-test/compiler_crashers_fixed/0015-no-stacktrace.swift
@@ -1,9 +1,9 @@
-// RUN: %target-swift-frontend %s -emit-silgen -verify
+// RUN: not %target-swift-frontend %s -emit-silgen 
 
 // Distributed under the terms of the MIT license
 // Test case submitted to project by https://github.com/practicalswift (practicalswift)
 // http://www.openradar.me/17225563
 
-enum a<T> { // expected-error {{recursive value type 'a<T>' is not allowed}}
+enum a<T> {
     case s(T, a)
 }
