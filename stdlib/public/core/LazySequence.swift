88aaed0a0cb48a053a898df32d2be338b99d61a1
Dave Abrahams <dabrahams@apple.com>
2015-07-13 15:36:17 +0000

    [stdlib] Move most of the lazy prototype into the stdlib
    Swift SVN r30144

--- stdlib/public/core/LazySequence.swift
+++ stdlib/public/core/LazySequence.swift
@@ -10,6 +10,70 @@
 //
 //===----------------------------------------------------------------------===//
 
+public protocol _prext_LazySequenceType : SequenceType {
+  /// A SequenceType that can contain the same elements as this one,
+  /// possibly with a simpler type.
+  ///
+  /// This associated type is used to keep the result type of
+  /// `lazy(x).operation` from growing a `_prext_LazySequence` layer.
+  typealias Elements: SequenceType = Self
+
+  /// A sequence containing the same elements as this one, possibly with
+  /// a simpler type.
+  ///
+  /// When implementing lazy operations, wrapping `elements` instead
+  /// of `self` can prevent result types from growing a `_prext_LazySequence`
+  /// layer.
+  ///
+  /// Note: this property need not be implemented by conforming types,
+  /// it has a default implementation in a protocol extension that
+  /// just returns `self`.
+  var elements: Elements {get} 
+  
+  /// An Array, created on-demand, containing the elements of this
+  /// lazy SequenceType.
+  ///
+  /// Note: this property need not be implemented by conforming types, it has a
+  /// default implementation in a protocol extension.
+  var array: [Generator.Element] {get}
+}
+
+extension _prext_LazySequenceType {
+  /// an Array, created on-demand, containing the elements of this
+  /// lazy SequenceType.
+  public var array: [Generator.Element] {
+    return Array(self)
+  }
+}
+
+extension _prext_LazySequenceType where Elements == Self {
+  public var elements: Self { return self }
+}
+
+/// A sequence that forwards its implementation to an underlying
+/// sequence instance while exposing lazy computations as methods.
+public struct _prext_LazySequence<Base_ : SequenceType>
+  : _prext_LazySequenceType, _SequenceWrapperType {
+  public var _base: Base_
+  public var elements: Base_ { return _base }
+}
+
+extension SequenceType {
+  public var _prext_lazy: _prext_LazySequence<Self> {
+    return _prext_LazySequence(_base: self)
+  }
+}
+
+extension _prext_LazySequenceType {
+  public var _prext_lazy: Self { // Don't re-wrap already-lazy sequences
+    return self
+  }
+}
+
+//===----------------------------------------------------------------------===//
+// Implementations we are replacing
+//===----------------------------------------------------------------------===//
+
 /// A sequence that forwards its implementation to an underlying
 /// sequence instance while exposing lazy computations as methods.
 public struct LazySequence<Base : SequenceType> : SequenceType {
