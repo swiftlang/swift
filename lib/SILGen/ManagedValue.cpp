bd1c3ba0fc1e441facf7fcf9ee99189a15805530
Andrew Trick <atrick@apple.com>
2015-07-17 06:52:04 +0000

    Revert "ManagedValue forwardInto and assignInto should not always "take" their value."
    This reverts commit r30272.
    
    There's a way to fix Builtin.reinterpretCast that preserves SILGen
    RValue invariants better according to JohnM. So although these changes
    are valid, there will be no way for me to test them.
    
    After reverting this, I will roll back in the change to
    emitImplicitValueConstructor because I think the old code was
    violating SILGen invariants.
    
    Swift SVN r30306

--- lib/SILGen/ManagedValue.cpp
+++ lib/SILGen/ManagedValue.cpp
@@ -92,24 +92,16 @@ void ManagedValue::forwardInto(SILGenFunction &gen, SILLocation loc,
                                SILValue address) {
   if (hasCleanup())
     forwardCleanup(gen);
-
-  // Do not take this value unless it has an active cleanup. For example,
-  // unchecked_addr_cast of trivial to nontrivial type produces a value that
-  // would normally require a release but is returned as +0.
-  IsTake_t isTake = isPlusZeroRValueOrTrivial() ? IsNotTake : IsTake;
   auto &addrTL = gen.getTypeLowering(address.getType());
-  gen.emitSemanticStore(loc, getValue(), address, addrTL, isTake,
-                        IsInitialization);
+  gen.emitSemanticStore(loc, getValue(), address, addrTL, IsInitialization);
 }
 
 void ManagedValue::assignInto(SILGenFunction &gen, SILLocation loc,
                               SILValue address) {
   if (hasCleanup())
     forwardCleanup(gen);
-
-  // As with forwardInto, do not take this value unless its cleanup is active.
-  IsTake_t isTake = isPlusZeroRValueOrTrivial() ? IsNotTake : IsTake;
+  
   auto &addrTL = gen.getTypeLowering(address.getType());
-  gen.emitSemanticStore(loc, getValue(), address, addrTL, isTake,
+  gen.emitSemanticStore(loc, getValue(), address, addrTL,
                         IsNotInitialization);
 }
