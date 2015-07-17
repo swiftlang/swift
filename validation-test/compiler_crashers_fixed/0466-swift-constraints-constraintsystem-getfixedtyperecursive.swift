6765a765a355340ec62fc71708467daa4a54dd3b
Jordan Rose <jordan_rose@apple.com>
2015-07-17 00:22:24 +0000

    Validate decls before doing redeclaration checking.
    This fixes a surprising number of compiler crashers. The reason this is an issue
    at all is because we visit variables before their parent pattern binding decls.
    (At least, that's one of the reasons...)
    
    Swift SVN r30293

---
+++ validation-test/compiler_crashers_fixed/0466-swift-constraints-constraintsystem-getfixedtyperecursive.swift
@@ -0,0 +1,20 @@
+// RUN: not %target-swift-frontend %s -parse
+
+// Distributed under the terms of the MIT license
+// Test case submitted to project by https://github.com/practicalswift (practicalswift)
+// Test case found by fuzzing
+
+print()
+protocol A : P {
+protocol P {
+protocol a {
+}("""\()
+print() {
+protocol A {
+}
+}
+protocol d {
+struct D : P {
+}
+}
+typealias e : e!.
---
+++ validation-test/compiler_crashers_fixed/0967-getselftypeforcontainer.swift
@@ -0,0 +1,13 @@
+// RUN: not %target-swift-frontend %s -parse
+
+// Distributed under the terms of the MIT license
+// Test case submitted to project by https://github.com/practicalswift (practicalswift)
+// Test case found by fuzzing
+
+protocol B {
+protocol b : a {
+func a() ->() -> String {
+}
+}
+}
+static let f = B
