03f879bb87185dc3555ebce04f0c825b9535767f
Dave Abrahams <dabrahams@apple.com>
2015-07-18 06:50:27 +0000

    [stdlib] Lazy flatMap
    Addresses <rdar://problem/20138472>, can close after API review.
    
    Swift SVN r30370

---
+++ stdlib/public/core/FlatMap.swift
@@ -0,0 +1,50 @@
+//===--- FlatMap.swift ----------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2015 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+extension _prext_LazySequenceType where
+Generator.Element == Elements.Generator.Element {
+  public func flatMap<Intermediate: SequenceType>(
+    transform: (Generator.Element)->Intermediate
+  ) -> _prext_LazySequence<
+    _prext_FlattenSequence<_prext_LazyMapSequence<Elements, Intermediate>>> {
+    return self.map(transform)._prext_flatten
+  }
+}
+
+extension _prext_LazyCollectionType
+where Generator.Element == Elements.Generator.Element{
+  public func flatMap<Intermediate: CollectionType>(
+    transform: (Generator.Element)->Intermediate
+  ) -> _prext_LazyCollection<
+    _prext_FlattenCollection<
+      _prext_LazyMapCollection<Elements, Intermediate>>
+  > {
+    return self.map(transform)._prext_flatten
+  }
+}
+
+extension _prext_LazyCollectionType
+where Generator.Element == Elements.Generator.Element,
+Elements.Index : BidirectionalIndexType
+{
+  public func flatMap<
+    Intermediate: CollectionType
+    where Intermediate.Index : BidirectionalIndexType
+  >(
+    transform: (Generator.Element)->Intermediate
+  ) -> _prext_LazyCollection<
+    _prext_FlattenBidirectionalCollection<
+      _prext_LazyMapCollection<Elements, Intermediate>
+  >> {
+    return self.map(transform)._prext_flatten
+  }
+}
