db49e9fe49451f467e3108c2c0606775c2df54f1
Chris Lattner <clattner@apple.com>
2015-07-21 04:44:38 +0000

    -verify mode in compiler_crashers isn't a good use of broken bots.
    Swift SVN r30440

--- validation-test/compiler_crashers_fixed/0045-swift-lowering-adjustfunctiontype.swift
+++ validation-test/compiler_crashers_fixed/0045-swift-lowering-adjustfunctiontype.swift
@@ -1,9 +1,9 @@
-// RUN: %target-swift-frontend %s -parse -verify
+// RUN: not %target-swift-frontend %s -parse 
 
 // Distributed under the terms of the MIT license
 // Test case submitted to project by https://github.com/practicalswift (practicalswift)
 // http://www.openradar.me/17317691
 
 func f() {
-    ({}) // expected-error {{expression resolves to an unused function}}
+    ({}) 
 }
--- validation-test/compiler_crashers_fixed/0049-swift-nominaltypedecl-getmembers.swift
+++ validation-test/compiler_crashers_fixed/0049-swift-nominaltypedecl-getmembers.swift
@@ -1,4 +1,4 @@
-// RUN: %target-swift-frontend %s -emit-sil -verify
+// RUN: not %target-swift-frontend %s -emit-sil 
 
 // Distributed under the terms of the MIT license
 // Test case submitted to project by https://github.com/rnapier (Rob Napier)
@@ -7,5 +7,5 @@ var f = 1
 var e: Int -> Int = {
     return $0
 }
-let d: Int =  { c, b in // expected-error {{'()' is not convertible to 'Int'}}
+let d: Int =  { c, b in 
 }(f, e)
