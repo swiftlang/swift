eff5cb3d4887b8aceffb4baef292b9c7f091903a
Chris Lattner <clattner@apple.com>
2015-07-26 21:47:27 +0000

    remove a -verify line from a validation test.
    Swift SVN r30663

--- validation-test/compiler_crashers_fixed/0022-no-stacktrace.swift
+++ validation-test/compiler_crashers_fixed/0022-no-stacktrace.swift
@@ -1,9 +1,9 @@
-// RUN: not %target-swift-frontend %s -parse -verify
+// RUN: not %target-swift-frontend %s -parse
 
 // Distributed under the terms of the MIT license
 // Test case submitted to project by https://github.com/practicalswift (practicalswift)
 // Based on the stack trace this bug is actually triggering a different code path
 // compared to case #021. so these appears not to be dupes(!).
 
-class A<T where T : A> { // expected-error {{reference to generic type 'A' requires arguments in <...>}} expected-note {{generic type 'A' declared here}}
+class A<T where T : A> { 
 }
