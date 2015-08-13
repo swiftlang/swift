e922008fa4456a43a7c22d182bac1eff89bb7f74
Chris Lattner <clattner@apple.com>
2015-08-13 05:39:23 +0000

    update test for diagnostic changes, not handling sametype constraints well is a known issue.
    Swift SVN r31209

--- validation-test/stdlib/StdlibUnittestStaticAssertions.swift
+++ validation-test/stdlib/StdlibUnittestStaticAssertions.swift
@@ -8,11 +8,11 @@ struct S2 {}
 func test_expectType() {
   var s1 = S1()
   expectType(S1.self, &s1)
-  expectType(S2.self, &s1) // expected-error {{cannot convert value of type 'S2.Type' to expected argument type '_.Type'}}
+  expectType(S2.self, &s1) // expected-error {{cannot convert value of type 'inout S1' to expected argument type 'inout _'}}
 }
 
 func test_expectEqualType() {
   expectEqualType(S1.self, S1.self)
-  expectEqualType(S1.self, S2.self) // expected-error {{cannot convert value of type 'S1.Type' to expected argument type '_.Type'}}
+  expectEqualType(S1.self, S2.self) // expected-error {{cannot invoke 'expectEqualType' with an argument list of type '(S1.Type, S2.Type)'}}
 }
 
