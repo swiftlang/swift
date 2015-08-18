49970aa7cd6c281ce9211792b9bb31e413654a04
Jordan Rose <jordan_rose@apple.com>
2015-08-18 21:14:41 +0000

    [test] Tests for __attribute__((overloadable)) support in r31249.
    Swift SVN r31309

---
+++ test/IRGen/Inputs/c_functions.h
@@ -0,0 +1,4 @@
+// This header is included on non-ObjC platforms.
+
+void overloaded(void) __attribute__((overloadable));
+void overloaded(int) __attribute__((overloadable));
---
+++ test/IRGen/c_functions.swift
@@ -0,0 +1,12 @@
+// RUN: rm -rf %t && mkdir -p %t
+// RUN: %target-swift-frontend -import-objc-header %S/Inputs/c_functions.h -primary-file %s -emit-ir | FileCheck %s
+
+// This is deliberately not a SIL test so that we can test SILGen too.
+
+// CHECK-LABEL: define hidden void @_TF11c_functions14testOverloadedFT_T_
+func testOverloaded() {
+  // CHECK: call void @_Z10overloadedv()
+  overloaded()
+  // CHECK: call void @_Z10overloadedi(i32 42)
+  overloaded(42)
+} // CHECK: {{^}$}}
