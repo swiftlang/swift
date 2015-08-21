c93bf3bbf0c49e942a4a5687ec8cbb3a33a39df1
Dmitri Hrybenko <dgribenko@apple.com>
2015-08-21 21:05:38 +0000

    stdlib: remove 58 global initializers from the Darwin overlay
    This change removes the global initializers, but the constants are
    still not inlineable.  Filed rdar://22384493 for that.
    
    Swift SVN r31390

--- stdlib/public/SDK/Darwin/Darwin.swift
+++ stdlib/public/SDK/Darwin/Darwin.swift
@@ -16,7 +16,8 @@
 //===----------------------------------------------------------------------===//
 // MacTypes.h
 //===----------------------------------------------------------------------===//
-public let noErr: OSStatus = 0
+
+public var noErr: OSStatus { return 0 }
 
 /// The `Boolean` type declared in MacTypes.h and used throughout Core
 /// Foundation.
@@ -176,41 +177,39 @@ public func openat(fd: CInt, _ path: UnsafePointer<CChar>,
   return _swift_Darwin_openat(fd, path, oflag, mode)
 }
 
+public var S_IFMT: mode_t   { return mode_t(0o170000) }
+public var S_IFIFO: mode_t  { return mode_t(0o010000) }
+public var S_IFCHR: mode_t  { return mode_t(0o020000) }
+public var S_IFDIR: mode_t  { return mode_t(0o040000) }
+public var S_IFBLK: mode_t  { return mode_t(0o060000) }
+public var S_IFREG: mode_t  { return mode_t(0o100000) }
+public var S_IFLNK: mode_t  { return mode_t(0o120000) }
+public var S_IFSOCK: mode_t { return mode_t(0o140000) }
+public var S_IFWHT: mode_t  { return mode_t(0o160000) }
 
-public let	S_IFMT   = mode_t(0o170000)
-public let	S_IFIFO  = mode_t(0o010000)
-public let	S_IFCHR  = mode_t(0o020000)
-public let	S_IFDIR  = mode_t(0o040000)
-public let	S_IFBLK  = mode_t(0o060000)
-public let	S_IFREG  = mode_t(0o100000)
-public let	S_IFLNK  = mode_t(0o120000)
-public let	S_IFSOCK = mode_t(0o140000)
-public let	S_IFWHT  = mode_t(0o160000)
+public var S_IRWXU: mode_t  { return mode_t(0o000700) }
+public var S_IRUSR: mode_t  { return mode_t(0o000400) }
+public var S_IWUSR: mode_t  { return mode_t(0o000200) }
+public var S_IXUSR: mode_t  { return mode_t(0o000100) }
 
-public let	S_IRWXU  = mode_t(0o000700)
-public let	S_IRUSR  = mode_t(0o000400)
-public let	S_IWUSR  = mode_t(0o000200)
-public let	S_IXUSR  = mode_t(0o000100)
+public var S_IRWXG: mode_t  { return mode_t(0o000070) }
+public var S_IRGRP: mode_t  { return mode_t(0o000040) }
+public var S_IWGRP: mode_t  { return mode_t(0o000020) }
+public var S_IXGRP: mode_t  { return mode_t(0o000010) }
 
-public let	S_IRWXG  = mode_t(0o000070)
-public let	S_IRGRP  = mode_t(0o000040)
-public let	S_IWGRP  = mode_t(0o000020)
-public let	S_IXGRP  = mode_t(0o000010)
+public var S_IRWXO: mode_t  { return mode_t(0o000007) }
+public var S_IROTH: mode_t  { return mode_t(0o000004) }
+public var S_IWOTH: mode_t  { return mode_t(0o000002) }
+public var S_IXOTH: mode_t  { return mode_t(0o000001) }
 
-public let	S_IRWXO  = mode_t(0o000007)
-public let	S_IROTH  = mode_t(0o000004)
-public let	S_IWOTH  = mode_t(0o000002)
-public let	S_IXOTH  = mode_t(0o000001)
+public var S_ISUID: mode_t  { return mode_t(0o004000) }
+public var S_ISGID: mode_t  { return mode_t(0o002000) }
+public var S_ISVTX: mode_t  { return mode_t(0o001000) }
 
-public let	S_ISUID  = mode_t(0o004000)
-public let	S_ISGID  = mode_t(0o002000)
-public let	S_ISVTX  = mode_t(0o001000)
-
-public let	S_ISTXT  = S_ISVTX
-public let	S_IREAD  = S_IRUSR
-public let	S_IWRITE = S_IWUSR
-public let	S_IEXEC  = S_IXUSR
-
+public var S_ISTXT: mode_t  { return S_ISVTX }
+public var S_IREAD: mode_t  { return S_IRUSR }
+public var S_IWRITE: mode_t { return S_IWUSR }
+public var S_IEXEC: mode_t  { return S_IXUSR }
 
 //===----------------------------------------------------------------------===//
 // unistd.h
@@ -231,9 +230,11 @@ public func vfork() -> Int32 {
 //===----------------------------------------------------------------------===//
 
 #if os(OSX) || os(iOS) || os(watchOS) || os(tvOS)
-public let SIG_DFL: sig_t? = nil
-public let SIG_IGN = unsafeBitCast(1, sig_t.self)
-public let SIG_ERR = unsafeBitCast(-1, sig_t.self)
-public let SIG_HOLD = unsafeBitCast(5, sig_t.self)
+public var SIG_DFL: sig_t? { return nil }
+public var SIG_IGN: sig_t { return unsafeBitCast(1, sig_t.self) }
+public var SIG_ERR: sig_t { return unsafeBitCast(-1, sig_t.self) }
+public var SIG_HOLD: sig_t { return unsafeBitCast(5, sig_t.self) }
+#else
+internal var _ignore = _UnsupportedPlatformError()
 #endif
 
