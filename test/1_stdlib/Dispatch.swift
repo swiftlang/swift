802995d0b5ac4b19f0e7dd05eb78a3bb1cc1739b
Jordan Rose <jordan_rose@apple.com>
2015-09-02 18:31:50 +0000

    [test] Switch Dispatch tests over to StdlibUnittest.
    Swift SVN r31633

--- test/1_stdlib/Dispatch.swift
+++ test/1_stdlib/Dispatch.swift
@@ -1,21 +1,31 @@
-// RUN: %target-run-simple-swift | FileCheck %s
+// RUN: %target-run-simple-swift
 // REQUIRES: executable_test
 
 // REQUIRES: objc_interop
 
 import Dispatch
 import Foundation
+import StdlibUnittest
 
-// Be paranoid about the value of large 32-bit UInts on 32-bit architectures.
-print(DISPATCH_PROC_EXIT)
-// CHECK: 2147483648
+defer { runAllTests() }
 
-print(DISPATCH_QUEUE_CONCURRENT.description)
-// CHECK-NEXT: OS_dispatch_queue_attr
+var DispatchAPI = TestSuite("DispatchAPI")
 
-print(dispatch_data_empty.description)
-// CHECK-NEXT: <>
+DispatchAPI.test("constants") {
+  expectEqual(2147483648, DISPATCH_PROC_EXIT)
+  expectEqual("<>", dispatch_data_empty.description)
 
-// Check upcasts and downcasts.
-let mainQueue = dispatch_get_main_queue() as AnyObject
-print(mainQueue is dispatch_queue_t)
+  // This is a lousy test, but really we just care that
+  // DISPATCH_QUEUE_CONCURRENT comes through at all.
+  _ = DISPATCH_QUEUE_CONCURRENT as AnyObject
+  _ = DISPATCH_QUEUE_CONCURRENT.description
+}
+
+DispatchAPI.test("OS_OBJECT support") {
+  let mainQueue = dispatch_get_main_queue() as AnyObject
+  expectTrue(mainQueue is dispatch_queue_t)
+
+  // This should not be optimized out, and should succeed.
+  expectNotEmpty(mainQueue as? dispatch_queue_t)
+}
+
