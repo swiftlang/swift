f6862e27934fadfdcfa74d603806bf3f304f61a4
Doug Gregor <dgregor@apple.com>
2015-09-03 06:09:34 +0000

    Omit needless words script: set up target appropriately for the selected SDK
    Swift SVN r31661

--- utils/omit-needless-words.py
+++ utils/omit-needless-words.py
@@ -19,6 +19,9 @@ def help():
     print('\t-s <sdkname>\t\t\tThe SDK to use (e.g., macosx)')
     print("\t--sdk=<sdkname>'\t\tDefaults to 'macosx'")
     print('')
+    print('\t-t <triple>\t\t\tThe target triple to use (e.g., x86_64-apple-macosx10.10)')
+    print("\t--target=<triple>'\t\tDefaults to 'x86_64-apple-macosx10.10'")
+    print('')
     print('\t-i <executable>\t\t\tThe swift-ide-test executable')
     print("\t--swift-ide-test=<executable>\tDefaults to 'swift-ide-test'")
     print('')
@@ -30,6 +33,7 @@ def help():
 
 # Configuration information
 sdk = 'macosx'
+target = ''
 module = ''
 source_filename = 'omit-needless-words.swift'
 swift_ide_test = 'swift-ide-test'
@@ -37,9 +41,9 @@ diff_tool = 'opendiff'
 
 # Parse command-line arguments.
 try:
-    opts, args = getopt.getopt(sys.argv[1:], 'hs:m:i:d:',
-                               ['help', 'sdk=', 'module=','swift-ide-test=',
-                                'diff_tool='])
+    opts, args = getopt.getopt(sys.argv[1:], 'hs:t:m:i:d:',
+                               ['help', 'sdk=', 'target=', 'module=',
+                                'swift-ide-test=','diff_tool='])
 except getopt.GetoptError:
     help()
     sys.exit(2)
@@ -51,6 +55,10 @@ for opt, arg in opts:
 
     if opt in ('-s', '--sdk'):
         sdk = arg
+        continue
+
+    if opt in ('-t', '--target'):
+        target = arg
         continue
 
     if opt in ('-m', '--module'):
@@ -71,12 +79,24 @@ for opt, arg in opts:
 if module == '':
     help()
     sys.exit(2)
-    
+
+if target == '':
+    if sdk in ('macosx', 'macosx.internal'):
+        target = 'x86_64-apple-macosx10.10'
+    if sdk in ('iphoneos', 'iphoneos.internal'):
+        target = 'arm64-apple-ios8.0'
+    if sdk == 'iphonesimulator':
+        target = 'x86_64-apple-ios8.0'
+    if sdk in ('watchos', 'watchos.internal'):
+        target = 'armv7k-apple-watchos2.0'
+    if sdk == 'watchos.simulator':
+        target = 'i386-apple-watchos2.0'
+
 # Figure out the SDK root for the requested SDK
 sdkroot = subprocess.check_output(['xcrun', '--show-sdk-path', '--sdk', sdk]).rstrip()
 print('SDK Root = %s' % (sdkroot))
 
-swift_ide_test_cmd = [swift_ide_test, '-print-module', '-source-filename', source_filename, '-sdk', sdkroot, '-module-print-skip-overlay', '-skip-unavailable', '-module-print-submodules', '-skip-parameter-names', '-module-to-print=%s' % (module)]
+swift_ide_test_cmd = [swift_ide_test, '-print-module', '-source-filename', source_filename, '-sdk', sdkroot, '-target', target, '-module-print-skip-overlay', '-skip-unavailable', '-module-print-submodules', '-skip-parameter-names', '-module-to-print=%s' % (module)]
 omit_needless_words_args = ['-enable-omit-needless-words']
 
 # Determine the output files.
