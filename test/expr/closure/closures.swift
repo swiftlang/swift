f8b12c60957528547d1998fedd9456a7006713e5
Chris Lattner <clattner@apple.com>
2015-09-05 23:39:57 +0000

    merge two tests
    Swift SVN r31721

--- test/expr/closure/closures.swift
+++ test/expr/closure/closures.swift
@@ -307,3 +307,7 @@ class r22344208 {
   }
 }
 
+var f = { (s: Undeclared)-> Int in 0 } // expected-error {{use of undeclared type 'Undeclared'}}
+
+
+
--- test/expr/closure/invalid_parameter_type.swift
+++
@@ -1,3 +0,0 @@
-// RUN: %target-parse-verify-swift
-
-var f = { (s: Undeclared)-> Int in 0 } // expected-error {{use of undeclared type 'Undeclared'}}
