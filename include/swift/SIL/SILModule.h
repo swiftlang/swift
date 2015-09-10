66e13af73e93d4c433f170c1923a6a171d934220
Roman Levenstein <rlevenstein@apple.com>
2015-09-10 22:26:35 +0000

    Add support for whole-module optimizations for SIL files.
    To invoke the front-end on a SIL with whole-module optimizations enabled, execute:
    swiftc -frontend myfile.sil
    
    To invoke the front-end on a SIL without whole-module optimizations enabled, add a -primary-file option:
    swiftc -frontend -primary-file myfile.sil
    
    To invoke a sil-opt with whole-module optimizations enabled, use the -wmo option:
    sil-opt myfile.sil -wmo
    
    This change was need to be able to write SIL unit tests which should be compiled in the WMO mode.
    
    Swift SVN r31862

--- include/swift/SIL/SILModule.h
+++ include/swift/SIL/SILModule.h
@@ -232,8 +232,9 @@ public:
   /// \brief Create and return an empty SIL module that we can
   /// later parse SIL bodies directly into, without converting from an AST.
   static std::unique_ptr<SILModule> createEmptyModule(Module *M,
-                                                      SILOptions &Options) {
-    return std::unique_ptr<SILModule>(new SILModule(M, Options, M, false));
+                                                      SILOptions &Options,
+                                                      bool WholeModule = false) {
+    return std::unique_ptr<SILModule>(new SILModule(M, Options, M, WholeModule));
   }
 
   /// Get the Swift module associated with this SIL module.
