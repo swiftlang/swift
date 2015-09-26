c22ab440137349e4f5ff5b6249bdc25f0c48fb1c
Xi Ge <xi_ge@apple.com>
2015-09-26 05:54:43 +0000

    Re-apply "[CodeComplete] Make sure the lookup API can find closure parameters. rdar://17968255"
    Swift SVN r32257

--- lib/AST/LookupVisibleDecls.cpp
+++ lib/AST/LookupVisibleDecls.cpp
@@ -740,6 +740,13 @@ void swift::lookupVisibleDecls(VisibleDeclConsumer &Consumer,
       if (Loc.isValid()) {
         auto CE = cast<ClosureExpr>(ACE);
         namelookup::FindLocalVal(SM, Loc, Consumer).visit(CE->getBody());
+        for (auto Pattern : CE->getParamPatterns()) {
+          if (!Pattern)
+            continue;
+          Pattern->forEachVariable([&](VarDecl *VD) {
+            Consumer.foundDecl(VD, Reason);
+          });
+        }
       }
     } else if (auto ED = dyn_cast<ExtensionDecl>(DC)) {
       ExtendedType = ED->getExtendedType();
