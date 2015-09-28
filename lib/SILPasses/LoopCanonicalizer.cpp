4f38366d24595a2aa81248d6417e5f753daec383
Michael Gottesman <mgottesman@apple.com>
2015-09-28 02:08:23 +0000

    Create a new pass LoopCanonicalizer.
    This enables loop canonicalizations to be tested independently of loop rotation.
    I also moved the loop canonicalization tests that were in loop rotate into loop
    canonicalization.
    
    Swift SVN r32270

---
+++ lib/SILPasses/LoopCanonicalizer.cpp
@@ -0,0 +1,83 @@
+//===--- LoopCanonicalizer.cpp --------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2015 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// This is a simple pass that can be used to apply loop canonicalizations to a
+/// cfg. It also enables loop canonicalizations to be tested via FileCheck.
+///
+//===----------------------------------------------------------------------===//
+
+#define DEBUG_TYPE "sil-loop-canonicalizer"
+#include "swift/SILPasses/Passes.h"
+#include "swift/SILAnalysis/Analysis.h"
+#include "swift/SILAnalysis/DominanceAnalysis.h"
+#include "swift/SILAnalysis/LoopAnalysis.h"
+#include "swift/SILPasses/Transforms.h"
+#include "swift/SILPasses/Utils/LoopUtils.h"
+
+using namespace swift;
+
+namespace {
+
+class LoopCanonicalizer : public SILFunctionTransform {
+
+  void run() override {
+    SILFunction *F = getFunction();
+
+    DEBUG(llvm::dbgs() << "Attempt to canonicalize loops in " << F->getName()
+                       << "\n");
+
+    auto *LA = PM->getAnalysis<SILLoopAnalysis>();
+    auto *DA = PM->getAnalysis<DominanceAnalysis>();
+    auto *LI = LA->getLoopInfo(F);
+    auto *DT = DA->get(F);
+
+    if (LI->empty()) {
+      DEBUG(llvm::dbgs() << "    No loops to canonicalize!\n");
+      return;
+    }
+
+    bool Changed = false;
+    std::vector<SILLoop *> Worklist;
+    for (auto *L : LI->getTopLevelLoops())
+      Worklist.push_back(L);
+    while (Worklist.size()) {
+      auto *L = Worklist.back();
+      Worklist.pop_back();
+      if (canonicalizeLoop(L, DT, LI)) {
+        DEBUG(llvm::dbgs() << "    Canonicalized: " << *L);
+        Changed = true;
+      }
+    }
+
+    if (Changed) {
+      // We preserve loop info and the dominator tree.
+      DA->lockInvalidation();
+      LA->lockInvalidation();
+      PM->invalidateAnalysis(F, SILAnalysis::PreserveKind::Nothing);
+      DA->unlockInvalidation();
+      LA->unlockInvalidation();
+    }
+  }
+
+  StringRef getName() override { return "Loop Canonicalizer"; }
+};
+
+} // end anonymous namespace
+
+//===----------------------------------------------------------------------===//
+//                            Top Level Entrypoint
+//===----------------------------------------------------------------------===//
+
+SILTransform *swift::createLoopCanonicalizer() {
+  return new LoopCanonicalizer();
+}
