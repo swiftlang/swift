21e7b06e77cf7dab2be19e95b0ab69d0ae0ee576
Andrew Trick <atrick@apple.com>
2015-09-28 01:58:30 +0000

    Migrate the unsafeBitCast API to use Builtin.reinterpretCast.
    This avoids unnecessary address-taking. Instead use the builtin that directly
    supports unsafe casts. The optimizer now follows clear rules for
    Builtin.reinterpretCast (so it's safe to use) and knows how to optimize it.
    
    Swift SVN r32268

--- stdlib/public/core/Builtin.swift
+++ stdlib/public/core/Builtin.swift
@@ -106,9 +106,7 @@ func _canBeClass<T>(_: T.Type) -> Int8 {
 public func unsafeBitCast<T, U>(var x: T, _: U.Type) -> U {
   _precondition(sizeof(T.self) == sizeof(U.self),
     "can't unsafeBitCast between types of different sizes")
-  // FIXME: <rdar://problem/18574301> Builtin.reinterpretCast is broken
-  // return Builtin.reinterpretCast(x)
-  return UnsafeMutablePointer<U>(Builtin.addressof(&x)).memory
+  return Builtin.reinterpretCast(x)
 }
 
 /// `unsafeBitCast` something to `AnyObject`.
