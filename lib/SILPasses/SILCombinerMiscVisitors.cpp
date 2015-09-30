95da3d2e6d0aad19a918e2a83a0ac785cda113b2
Michael Gottesman <mgottesman@apple.com>
2015-09-30 00:35:56 +0000

    Fix header of SILCombinerMiscVisitors.cpp to have the right filename. NFC.
    Swift SVN r32329

--- lib/SILPasses/SILCombinerMiscVisitors.cpp
+++ lib/SILPasses/SILCombinerMiscVisitors.cpp
@@ -1,4 +1,4 @@
-//===---- SILCombinerVisitors.cpp - SILCombiner Visitor Impl -*- C++ -*----===//
+//===---- SILCombinerMiscVisitors.cpp -------------------------------------===//
 //
 // This source file is part of the Swift.org open source project
 //
