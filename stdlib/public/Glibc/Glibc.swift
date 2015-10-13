8f089d20b2b023916ba2c82c93ba14260bba89bc
Daniel Dunbar <daniel_dunbar@apple.com>
2015-10-13 14:31:34 +0000

    [Linux] Add Glibc overlay support for fcntl stat macros.
    Swift SVN r32655

--- stdlib/public/Glibc/Glibc.swift
+++ stdlib/public/Glibc/Glibc.swift
@@ -26,6 +26,38 @@ public var errno: Int32 {
 }
 
 //===----------------------------------------------------------------------===//
+// fcntl.h
+//===----------------------------------------------------------------------===//
+
+public var S_IFMT: mode_t   { return mode_t(0o170000) }
+public var S_IFIFO: mode_t  { return mode_t(0o010000) }
+public var S_IFCHR: mode_t  { return mode_t(0o020000) }
+public var S_IFDIR: mode_t  { return mode_t(0o040000) }
+public var S_IFBLK: mode_t  { return mode_t(0o060000) }
+public var S_IFREG: mode_t  { return mode_t(0o100000) }
+public var S_IFLNK: mode_t  { return mode_t(0o120000) }
+public var S_IFSOCK: mode_t { return mode_t(0o140000) }
+
+public var S_IRWXU: mode_t  { return mode_t(0o000700) }
+public var S_IRUSR: mode_t  { return mode_t(0o000400) }
+public var S_IWUSR: mode_t  { return mode_t(0o000200) }
+public var S_IXUSR: mode_t  { return mode_t(0o000100) }
+
+public var S_IRWXG: mode_t  { return mode_t(0o000070) }
+public var S_IRGRP: mode_t  { return mode_t(0o000040) }
+public var S_IWGRP: mode_t  { return mode_t(0o000020) }
+public var S_IXGRP: mode_t  { return mode_t(0o000010) }
+
+public var S_IRWXO: mode_t  { return mode_t(0o000007) }
+public var S_IROTH: mode_t  { return mode_t(0o000004) }
+public var S_IWOTH: mode_t  { return mode_t(0o000002) }
+public var S_IXOTH: mode_t  { return mode_t(0o000001) }
+
+public var S_ISUID: mode_t  { return mode_t(0o004000) }
+public var S_ISGID: mode_t  { return mode_t(0o002000) }
+public var S_ISVTX: mode_t  { return mode_t(0o001000) }
+
+//===----------------------------------------------------------------------===//
 // signal.h
 //===----------------------------------------------------------------------===//
 
