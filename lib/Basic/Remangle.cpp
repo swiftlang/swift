21e01730f4588fc9ed7e7c7766846640a885d446
Slava Pestov <spestov@apple.com>
2015-10-28 18:39:54 +0000

    Revert "Don't need to mangle @nonobjc, NFC"
    This reverts r32940. In reality this is not dead code, because
    foreign to native thunks have the _TTO mangling. We need better
    tests, which I will add in an upcoming commit.
    
    Swift SVN r32945

--- lib/Basic/Remangle.cpp
+++ lib/Basic/Remangle.cpp
@@ -569,6 +569,10 @@ void Remangler::mangleObjCAttribute(Node *node) {
   Out << "To";
 }
 
+void Remangler::mangleNonObjCAttribute(Node *node) {
+  Out << "TO";
+}
+
 void Remangler::mangleDirectMethodReferenceAttribute(Node *node) {
   Out << "Td";
 }
