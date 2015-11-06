d7b7fc76ea95d69fe65a6f5ae90ceebc4a7161c9
Erik Eckstein <eeckstein@apple.com>
2015-11-05 21:34:05 -0800

    Revert "Add a stack promotion optimization."
    This reverts commit 0dd045ca04dcc10a33abf57f7e1b08260c4e3de1.
    
    StackPromotion crashes when compiling MatMult in stdlib-asserts configuration

--- include/swift/LLVMPasses/PassesFwd.h
+++ include/swift/LLVMPasses/PassesFwd.h
@@ -22,13 +22,11 @@ namespace llvm {
   void initializeSwiftRCIdentityPass(PassRegistry &);
   void initializeSwiftARCOptPass(PassRegistry &);
   void initializeSwiftARCContractPass(PassRegistry &);
-  void initializeSwiftStackPromotionPass(PassRegistry &);
 }
 
 namespace swift {
   llvm::FunctionPass *createSwiftARCOptPass();
   llvm::FunctionPass *createSwiftARCContractPass();
-  llvm::FunctionPass *createSwiftStackPromotionPass();
   llvm::ImmutablePass *createSwiftAAWrapperPass();
   llvm::ImmutablePass *createSwiftRCIdentityPass();
 } // end namespace swift
