swiftlang/swift

commit 971a6801994920ea441f282601f0e8fa1d0c424a

Support for stack promotion of array buffers in the stdlib. The basic idea is to move the allocation of the buffer out of @_semantics("array.uninitialized") so that it can be inlined without inlining the semantics function (on high-level SIL).
This change in the stdlib also requires an adaption of the dead area elimination in DeadObjectElimination.
Otherwise it would just remove the semantics function but not the allocation and we would leak memory.

lib/SILPasses/DeadObjectElimination.cpp
@@ -33,6 +33,7 @@
 #include "swift/SIL/SILInstruction.h"
 #include "swift/SIL/SILModule.h"
 #include "swift/SIL/SILUndef.h"
+#include "swift/SIL/DebugUtils.h"
 #include "swift/SILAnalysis/ArraySemantic.h"
 #include "swift/SILPasses/Utils/Local.h"
 #include "swift/SILPasses/Utils/SILSSAUpdater.h"
@@ -773,6 +774,30 @@ bool DeadObjectElimination::processAllocApply(ApplyInst *AI) {
   if (ArraySemanticsCall(AI).getKind() != ArrayCallKind::kArrayUninitialized)
     return false;
 
+  ApplyInst *AllocBufferAI = nullptr;
+  SILValue Arg0 = AI->getArgument(0);
+  if (Arg0.getType().isExistentialType()) {
+    // This is a version of the initializer which receives a pre-allocated
+    // buffer as first argument. If we want to delete the initializer we also
+    // have to delete the allocation.
+    // But we have to check tbhe argument is really a buffer allocation call.
+    AllocBufferAI = dyn_cast<ApplyInst>(Arg0);
+    if (!AllocBufferAI)
+      return false;
+
+    auto *FRI = dyn_cast<FunctionRefInst>(AllocBufferAI->getCallee());
+    if (!FRI)
+      return false;
+
+    StringRef AllocFuncName = FRI->getReferencedFunction()->getName();
+    if (AllocFuncName != "swift_bufferAllocate" &&
+        AllocFuncName != "swift_bufferAllocateOnStack")
+      return false;
+
+    if (!hasOneNonDebugUse(*AllocBufferAI))
+      return false;
+  }
+
   if (!removeAndReleaseArray(AI))
     return false;
 
@@ -781,6 +806,8 @@ bool DeadObjectElimination::processAllocApply(ApplyInst *AI) {
   eraseUsesOfInstruction(AI);
   assert(AI->use_empty() && "All users should have been removed.");
   recursivelyDeleteTriviallyDeadInstructions(AI, true);
+  if (AllocBufferAI)
+    recursivelyDeleteTriviallyDeadInstructions(AllocBufferAI, true);
   ++DeadAllocApplyEliminated;
   return true;
 }

stdlib/public/core/Arrays.swift.gyb
@@ -450,16 +450,35 @@ extension ${Self} : ArrayLiteralConvertible {
 %end
 }
 
+%if Self == 'Array':
+
 // Referenced by the compiler to allocate array literals.
+//
+/// Returns an Array of `count` uninitialized elements using the
+/// given `storage`, and a pointer to uninitialized memory for the
+/// first element.
+///
+/// - Requires: `storage` is _ContiguousArrayStorage.
 @warn_unused_result
-@_semantics("array.uninitialized")
-public func _allocateUninitialized${Self}<Element>(count: Builtin.Word)
-    -> (${Self}<Element>, Builtin.RawPointer) {
-  let (array, ptr) = ${Self}<Element>._allocateUninitialized(Int(count))
+@inline(__always)
+public func _allocateUninitializedArray<Element>(_count: Builtin.Word)
+    -> (Array<Element>, Builtin.RawPointer) {
+  let count = Int(_count)
+  if count > 0 {
+    // Doing the actual buffer alloction outside of the array.uninitialized
+    // semantics function enables stack propagation of the buffer.
+    let bufferObject = ManagedBufferPointer<_ArrayBody, Element>(
+      _uncheckedBufferClass: _ContiguousArrayStorage<Element>.self,
+      minimumCapacity: count)
+
+    let (array, ptr) = Array<Element>._adoptStorage(bufferObject.buffer, count: count)
+    return (array, ptr._rawValue)
+  }
+  // For an empty array no buffer allocation is needed.
+  let (array, ptr) = Array<Element>._allocateUninitialized(count)
   return (array, ptr._rawValue)
 }
 
-%if Self == 'Array':
 // Referenced by the compiler to deallocate array literals on the
 // error path.
 @warn_unused_result
@@ -522,6 +541,7 @@ extension ${Self} : _ArrayType {
   /// Entry point for `Array` literal construction; builds and returns
   /// a ${Self} of `count` uninitialized elements.
   @warn_unused_result
+  @_semantics("array.uninitialized")
   internal static func _allocateUninitialized(
     count: Int
   ) -> (${Self}, UnsafeMutablePointer<Element>) {
@@ -530,6 +550,29 @@ extension ${Self} : _ArrayType {
   }
 
 %if Self == 'Array':
+
+  /// Returns an Array of `count` uninitialized elements using the
+  /// given `storage`, and a pointer to uninitialized memory for the
+  /// first element.
+  ///
+  /// - Requires: `storage is _ContiguousArrayStorage`.
+  @warn_unused_result
+  @_semantics("array.uninitialized")
+  internal static func _adoptStorage(
+    storage: AnyObject, count: Int
+  ) -> (Array, UnsafeMutablePointer<Element>) {
+    
+    _sanityCheck(
+      storage is _ContiguousArrayStorage<Element>, "Invalid array storage type")
+    
+    let innerBuffer = _ContiguousArrayBuffer<Element>(
+      count: count, storage: unsafeDowncast(storage))
+    
+    return (
+      Array(_Buffer(innerBuffer, shiftedToStartIndex: 0)),
+      innerBuffer.firstElementAddress)
+  }
+
   /// Entry point for aborting literal construction: deallocates
   /// a ${Self} containing only uninitialized elements.
   internal mutating func _deallocateUninitialized() {

stdlib/public/core/ContiguousArrayBuffer.swift
@@ -199,25 +199,49 @@ public struct _ContiguousArrayBuffer<Element> : _ArrayBufferType {
         _uncheckedBufferClass: _ContiguousArrayStorage<Element>.self,
         minimumCapacity: realMinimumCapacity)
 
-#if _runtime(_ObjC)
-      let verbatim = _isBridgedVerbatimToObjectiveC(Element.self)
-#else
-      let verbatim = false
-#endif
+      _initStorageHeader(count, capacity: __bufferPointer.allocatedElementCount)
 
-      __bufferPointer._valuePointer.initialize(
-        _ArrayBody(
-          count: count,
-          capacity: __bufferPointer.allocatedElementCount,
-          elementTypeIsBridgedVerbatim: verbatim))
-      
       _fixLifetime(__bufferPointer)
     }
   }
 
-  init(_ storage: _ContiguousArrayStorageBase) {
+  /// Initialize using the given uninitialized `storage`.
+  /// The storage is assumed to be uninitialized. The returned buffer has the
+  /// body part of the storage initialized, but not the elements.
+  ///
+  /// - Warning: The result has uninitialized elements.
+  /// 
+  /// - Warning: storage may have been stack-allocated, so it's
+  ///   crucial not to call, e.g., `malloc_size` on it.
+  internal init(count: Int, storage: _ContiguousArrayStorage<Element>) {
     __bufferPointer = ManagedBufferPointer(
       _uncheckedUnsafeBufferObject: storage)
+
+    _initStorageHeader(count, capacity: count)
+
+    _fixLifetime(__bufferPointer)
+  }
+
+  internal init(_ storage: _ContiguousArrayStorageBase) {
+    __bufferPointer = ManagedBufferPointer(
+      _uncheckedUnsafeBufferObject: storage)
+  }
+
+  /// Initialize the body part of our storage.
+  ///
+  /// - Warning: does not initialize elements
+  func _initStorageHeader(count: Int, capacity: Int) {
+#if _runtime(_ObjC)
+    let verbatim = _isBridgedVerbatimToObjectiveC(Element.self)
+#else
+    let verbatim = false
+#endif
+
+    __bufferPointer._valuePointer.initialize(
+      _ArrayBody(
+        count: count,
+        capacity: capacity,
+        elementTypeIsBridgedVerbatim: verbatim))
   }
 
   var arrayPropertyIsNative : Bool {

test/SILPasses/unused_containers.swift
@@ -1,5 +1,7 @@
 // RUN: %target-swift-frontend -primary-file %s -O -emit-sil | FileCheck %s
 
+// REQUIRES: swift_stdlib_no_asserts
+
 //CHECK-LABEL: @_TF17unused_containers16empty_array_testFT_T_
 //CHECK: bb0:
 //CHECK-NEXT: tuple

commit da794b876a5bb324b47522a0b0dc4d7800146723

Update dead object elimination to maintain the call graph.
Fixes a recent bot breakage caused by
971a6801994920ea441f282601f0e8fa1d0c424a.

lib/SILPasses/DeadObjectElimination.cpp
@@ -35,6 +35,7 @@
 #include "swift/SIL/SILUndef.h"
 #include "swift/SIL/DebugUtils.h"
 #include "swift/SILAnalysis/ArraySemantic.h"
+#include "swift/SILAnalysis/CallGraphAnalysis.h"
 #include "swift/SILPasses/Utils/Local.h"
 #include "swift/SILPasses/Utils/SILSSAUpdater.h"
 #include "swift/SILPasses/Transforms.h"
@@ -662,6 +663,7 @@ namespace {
 class DeadObjectElimination : public SILFunctionTransform {
   llvm::DenseMap<SILType, bool> DestructorAnalysisCache;
   llvm::SmallVector<SILInstruction*, 16> Allocations;
+  CallGraph *CG;
 
   void collectAllocations(SILFunction &Fn) {
     for (auto &BB : Fn)
@@ -697,6 +699,9 @@ class DeadObjectElimination : public SILFunctionTransform {
   }
 
   void run() override {
+    auto *CGA = PM->getAnalysis<CallGraphAnalysis>();
+    CG = CGA->getCallGraphOrNull();
+
     if (processFunction(*getFunction()))
       invalidateAnalysis(SILAnalysis::PreserveKind::ProgramFlow);
   }
@@ -805,9 +810,12 @@ bool DeadObjectElimination::processAllocApply(ApplyInst *AI) {
 
   eraseUsesOfInstruction(AI);
   assert(AI->use_empty() && "All users should have been removed.");
+  CallGraphEditor(CG).removeEdgesForInstruction(AI);
   recursivelyDeleteTriviallyDeadInstructions(AI, true);
-  if (AllocBufferAI)
+  if (AllocBufferAI) {
+    CallGraphEditor(CG).removeEdgesForInstruction(AllocBufferAI);
     recursivelyDeleteTriviallyDeadInstructions(AllocBufferAI, true);
+  }
   ++DeadAllocApplyEliminated;
   return true;
 }