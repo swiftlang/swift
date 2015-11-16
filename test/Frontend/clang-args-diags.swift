d380e0e69e0846e2d683f72de10dd3c378f81fcd
Jordan Rose <jordan_rose@apple.com>
2015-11-16 14:00:17 -0800

    Fix clang-args-diags.swift test for public Clang's output.
    Public Clang calls itself "Clang"; Apple Clang calls itself "Apple LLVM".
    The test doesn't actually care what the name is; it's just trying to make
    sure that the importer is failing for the reason we expect, rather than
    something unrelated.
    
    rdar://problem/23506925

--- test/Frontend/clang-args-diags.swift
+++ test/Frontend/clang-args-diags.swift
@@ -1,4 +1,3 @@
-// REQUIRES: rdar23506925
 // RUN: not %swift -Xcc -fake-argument -parse %s 2>&1 | FileCheck %s -check-prefix=CHECK-UNKNOWN-ARG
 // CHECK-UNKNOWN-ARG: unknown argument: '-fake-argument'
 
@@ -6,6 +5,6 @@
 // CHECK-VFS-NONEXISTENT: virtual filesystem overlay file '{{.*}}' not found
 
 // RUN: not %swift -Xcc --version -parse %s 2>&1 | FileCheck %s -check-prefix=CHECK-DRIVER-OPTION
-// CHECK-DRIVER-OPTION-DAG: LLVM version {{[0-9]+\.[0-9]+}}
+// CHECK-DRIVER-OPTION-DAG: {{clang|LLVM}} version {{[0-9]+\.[0-9]+}}
 // CHECK-DRIVER-OPTION-DAG: error: unable to handle compilation, expected exactly one compiler job
 // CHECK-DRIVER-OPTION-DAG: error: clang importer creation failed
