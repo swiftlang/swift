fed7f79147ed0978f77f4226360cd0c12a8d178c
Joe Groff <jgroff@apple.com>
2015-11-30 17:01:07 -0800

    Runtime: Static-ize swift_unsafeReflectAny.
    The _unsafeReflect entry point never ended up getting used on the Swift side, so don't export it, and remove the _silgen_name'd declaration.

--- include/swift/Runtime/Reflection.h
+++ include/swift/Runtime/Reflection.h
@@ -49,16 +49,6 @@ struct MirrorReturn {
 /// in the runtime that structurally reflects values of any type.
 extern "C" MirrorReturn
 swift_reflectAny(OpaqueValue *value, const Metadata *T);
-  
-/// func unsafeReflect<T>(owner: Builtin.NativeObject,
-///                       x: UnsafeMutablePointer<T>) -> Mirror
-///
-/// Produce a mirror for any value. If the value's type conforms to _Reflectable,
-/// invoke its _getMirror() method; otherwise, fall back to an implementation
-/// in the runtime that structurally reflects values of any type.
-extern "C" MirrorReturn
-swift_unsafeReflectAny(HeapObject *owner,
-                       const OpaqueValue *value, const Metadata *T);
 
 #pragma clang diagnostic pop
 
