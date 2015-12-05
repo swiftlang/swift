40584f830abf06f1ffd542a8d755663619c389f9
Jordan Rose <jordan_rose@apple.com>
2015-12-04 18:36:04 -0800

    [test] Add a Radar to the tests I disabled in f869e9e6.
    I filed this under Radar rather than bugs.swift.org because it's /only/ our
    CI system that's failing, and external contributors have no insight into
    that anyway. Hopefully I'll be able to get to the bottom of this soon.

--- test/Driver/environment.swift
+++ test/Driver/environment.swift
@@ -1,4 +1,5 @@
 // FIXME: This is failing on some of Apple's internal CI.
+// FIXME: <rdar://problem/23771412> Fix test/Driver/{environment.swift,options-interpreter.swift}
 // REQUIRES: disabled
 
 // RUN: %swift_driver -target x86_64-apple-macosx10.9 -L/foo/ -driver-use-frontend-path %S/Inputs/print-var.sh %s DYLD_LIBRARY_PATH | FileCheck %s
