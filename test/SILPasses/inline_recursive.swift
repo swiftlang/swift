b71171a6d085204e5bb1101d963b252f993b3f6b
Mark Lacey <mark.lacey@apple.com>
2015-12-05 02:19:37 -0800

    Revert "Do not inline self-recursive functions into other functions."
    This reverts commit dc65f701e9274d9284ec5537ee1f7929291af591 since it
    looks like the updated test is broken on some configurations.
    
    I'll investigate, fix, and reapply soon.

--- test/SILPasses/inline_recursive.swift
+++ test/SILPasses/inline_recursive.swift
@@ -7,17 +7,13 @@ private func recFunc(x: Int) -> Int {
   return 0
 }
 
-// Ensure that we do not inline self-recursive functions into other
-// functions since doing so can result in large code growth if we run
-// the inlining pass multiple times.
-
-// CHECK-LABEL: sil hidden @_TF16inline_recursive6callitFT_Si
+//CHECK-LABEL: sil {{.*}}callit
 // CHECK: bb0:
-// CHECK: [[FN:%.*]] = function_ref @_TF16inline_recursiveP33_38E63D320CFF538A1F98BBC31453B1EB7recFuncFSiSi
-// CHECK: [[BUILTIN_INT:%.*]] = integer_literal $Builtin.Int64, 3
-// CHECK: [[INT:%.*]] = struct $Int ([[BUILTIN_INT]] : $Builtin.Int64)
-// CHECK: [[APPLY:%.*]] = apply [[FN]]([[INT]])
-// CHECK: return [[APPLY]]
+// CHECK-NEXT: integer_literal {{.*}}, 0
+// CHECK-NEXT: struct
+// CHECK-NEXT: return
+
 func callit() -> Int {
   return recFunc(3)
 }
+
