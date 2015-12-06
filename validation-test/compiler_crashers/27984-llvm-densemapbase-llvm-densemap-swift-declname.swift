f781fbb820bb94fe441f2e7e44453ddc57c285c4
practicalswift <practicalswift@users.noreply.github.com>
2015-12-06 05:34:53 -0500

    Sync with https://github.com/practicalswift/swift-compiler-crashes
    Add 342 compiler crashes.

---
+++ validation-test/compiler_crashers/27984-llvm-densemapbase-llvm-densemap-swift-declname.swift
@@ -0,0 +1,11 @@
+// RUN: not --crash %target-swift-frontend %s -parse
+
+// Distributed under the terms of the MIT license
+// Test case submitted to project by https://github.com/practicalswift (practicalswift)
+// Test case found by fuzzing
+
+class B<T{enum S<h{protocol A{{
+}
+func g:A
+struct A:B<T>
+struct B<a
