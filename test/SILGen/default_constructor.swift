977188bf06fca5f0fa3b7278be3af82921040334
David Farler <dfarler@apple.com>
2015-12-06 14:03:17 -0800

    Update test/SILGen/default_constructor.swift for native super dispatch
    NFC.

--- test/SILGen/default_constructor.swift
+++ test/SILGen/default_constructor.swift
@@ -1,4 +1,4 @@
-// RUN: %target-swift-frontend -emit-silgen %s | FileCheck %s
+// RUN: %target-swift-frontend -use-native-super-method -emit-silgen %s | FileCheck %s
 
 struct B {
   var i : Int, j : Float
@@ -52,7 +52,7 @@ class F : E { }
 // CHECK-NEXT: store [[ORIGSELF]] to [[SELF]] : $*F
 // CHECK-NEXT: [[SELFP:%[0-9]+]] = load [[SELF]] : $*F
 // CHECK-NEXT: [[E:%[0-9]]] = upcast [[SELFP]] : $F to $E
-// CHECK: [[E_CTOR:%[0-9]+]] = function_ref @_TFC19default_constructor1Ec{{.*}} : $@convention(method) (@owned E) -> @owned E
+// CHECK: [[E_CTOR:%[0-9]+]] = super_method [[SELFP]] : $F, #E.init!initializer.1 : E.Type -> () -> E , $@convention(method) (@owned E) -> @owned E
 // CHECK-NEXT: [[ESELF:%[0-9]]] = apply [[E_CTOR]]([[E]]) : $@convention(method) (@owned E) -> @owned E
 
 // CHECK-NEXT: [[ESELFW:%[0-9]+]] = unchecked_ref_cast [[ESELF]] : $E to $F
