swiftlang/swift

commit 96a1e96dea9505c271dca7e75d69baec04c576e1

Improve printing of "too few" or "too many" arguments in generic or overloaded argument list mismatches.  We printed them in simple cases due to "Failure" detecting them in trivial situations.  Instead of doing that, let CSDiags do it, which allows us to pick things out of overload sets and handle the more complex cases well.
This is a progression across the board except for a couple of cases
where we now produce "cannot convert value of type 'whatever' to
expected argument type '(arglist)'", this is a known issue that I'll
fix in a subsequent commit.

lib/Sema/CSDiag.cpp
@@ -643,45 +643,12 @@ static bool diagnoseFailure(ConstraintSystem &cs, Failure &failure,
     // FIXME: diagnose other cases
     return false;
 
-  case Failure::MissingArgument: {
-    Identifier name;
-    unsigned idx = failure.getValue();
-    if (auto tupleTy = failure.getFirstType()->getAs<TupleType>()) {
-      name = tupleTy->getElement(idx).getName();
-    } else {
-      // Scalar.
-      assert(idx == 0);
-    }
-
-    if (name.empty())
-      tc.diagnose(loc, diag::missing_argument_positional, idx+1);
-    else
-      tc.diagnose(loc, diag::missing_argument_named, name);
-    return true;
-  }
-    
-  case Failure::ExtraArgument: {
-    if (auto tuple = dyn_cast_or_null<TupleExpr>(anchor)) {
-      unsigned firstIdx = failure.getValue();
-      auto name = tuple->getElementName(firstIdx);
-      Expr *arg = tuple->getElement(firstIdx);
-      
-      if (firstIdx == tuple->getNumElements()-1 &&
-          tuple->hasTrailingClosure())
-        tc.diagnose(arg->getLoc(), diag::extra_trailing_closure_in_call)
-          .highlight(arg->getSourceRange());
-      else if (name.empty())
-        tc.diagnose(loc, diag::extra_argument_positional)
-          .highlight(arg->getSourceRange());
-      else
-        tc.diagnose(loc, diag::extra_argument_named, name)
-          .highlight(arg->getSourceRange());
-      return true;
-    }
-
+  case Failure::MissingArgument:
     return false;
-  }
-      
+    
+  case Failure::ExtraArgument:
+    return false;
+    
   case Failure::NoPublicInitializers: {
     tc.diagnose(loc, diag::no_accessible_initializers, failure.getFirstType())
       .highlight(range);
@@ -3580,24 +3547,33 @@ bool FailureDiagnosis::visitApplyExpr(ApplyExpr *callExpr) {
   // If we filtered this down to exactly one candidate, see if we can produce
   // an extremely specific error about it.
   if (calleeInfo.size() == 1) {
-    if (calleeInfo.closeness == CC_ArgumentLabelMismatch) {
+    if (calleeInfo.closeness == CC_ArgumentLabelMismatch ||
+        calleeInfo.closeness == CC_ArgumentCountMismatch) {
       auto args = decomposeArgParamType(argExpr->getType());
       SmallVector<Identifier, 4> correctNames;
       unsigned OOOArgIdx = ~0U, OOOPrevArgIdx = ~0U;
+      unsigned extraArgIdx = ~0U, missingParamIdx = ~0U;
       
       // If we have a single candidate that failed to match the argument list,
       // attempt to use matchCallArguments to diagnose the problem.
       struct OurListener : public MatchCallArgumentListener {
         SmallVectorImpl<Identifier> &correctNames;
         unsigned &OOOArgIdx, &OOOPrevArgIdx;
+        unsigned &extraArgIdx, &missingParamIdx;
 
       public:
         OurListener(SmallVectorImpl<Identifier> &correctNames,
-                    unsigned &OOOArgIdx, unsigned &OOOPrevArgIdx)
+                    unsigned &OOOArgIdx, unsigned &OOOPrevArgIdx,
+                    unsigned &extraArgIdx, unsigned &missingParamIdx)
            : correctNames(correctNames),
-             OOOArgIdx(OOOArgIdx), OOOPrevArgIdx(OOOPrevArgIdx) {}
-        void extraArgument(unsigned argIdx) override {}
-        void missingArgument(unsigned paramIdx) override {}
+             OOOArgIdx(OOOArgIdx), OOOPrevArgIdx(OOOPrevArgIdx),
+             extraArgIdx(extraArgIdx), missingParamIdx(missingParamIdx) {}
+        void extraArgument(unsigned argIdx) override {
+          extraArgIdx = argIdx;
+        }
+        void missingArgument(unsigned paramIdx) override {
+          missingParamIdx = paramIdx;
+        }
         void outOfOrderArgument(unsigned argIdx, unsigned prevArgIdx) override{
           OOOArgIdx = argIdx;
           OOOPrevArgIdx = prevArgIdx;
@@ -3606,7 +3582,8 @@ bool FailureDiagnosis::visitApplyExpr(ApplyExpr *callExpr) {
           correctNames.append(newNames.begin(), newNames.end());
           return true;
         }
-      } listener(correctNames, OOOArgIdx, OOOPrevArgIdx);
+      } listener(correctNames, OOOArgIdx, OOOPrevArgIdx,
+                 extraArgIdx, missingParamIdx);
 
       // Use matchCallArguments to determine how close the argument list is (in
       // shape) to the specified candidates parameters.  This ignores the
@@ -3616,6 +3593,38 @@ bool FailureDiagnosis::visitApplyExpr(ApplyExpr *callExpr) {
       if (matchCallArguments(args, params, hasTrailingClosure,
                              /*allowFixes:*/true, listener, paramBindings)) {
        
+        // If we are missing an parameter, diagnose that.
+        if (missingParamIdx != ~0U) {
+          Identifier name = params[missingParamIdx].Label;
+          auto loc = callExpr->getArg()->getStartLoc();
+          if (name.empty())
+            diagnose(loc, diag::missing_argument_positional,
+                        missingParamIdx+1);
+          else
+            diagnose(loc, diag::missing_argument_named, name);
+          return true;
+        }
+        
+        if (extraArgIdx != ~0U) {
+          auto name = args[extraArgIdx].Label;
+          Expr *arg = argExpr;
+          auto tuple = dyn_cast<TupleExpr>(argExpr);
+          if (tuple)
+            arg = tuple->getElement(extraArgIdx);
+          auto loc = arg->getLoc();
+          if (tuple && extraArgIdx == tuple->getNumElements()-1 &&
+              tuple->hasTrailingClosure())
+            diagnose(loc, diag::extra_trailing_closure_in_call)
+              .highlight(arg->getSourceRange());
+          else if (name.empty())
+            diagnose(loc, diag::extra_argument_positional)
+              .highlight(arg->getSourceRange());
+          else
+            diagnose(loc, diag::extra_argument_named, name)
+              .highlight(arg->getSourceRange());
+          return true;
+        }
+        
         // If this is a argument label mismatch, then diagnose that error now.
         if (!correctNames.empty() &&
             CS->diagnoseArgumentLabelError(argExpr, correctNames,

test/Constraints/diagnostics.swift
@@ -42,8 +42,8 @@ f1(
    ) 
 
 // Tuple element unused.
-f0(i, i, // expected-error{{extra argument in call}}
-   i)
+f0(i, i,
+   i) // expected-error{{extra argument in call}}
 
 
 // Position mismatch
@@ -362,7 +362,7 @@ extension CurriedClass {
     method3(1, b: 2)
     method3()            // expected-error {{missing argument for parameter #1 in call}}
     method3(42)          // expected-error {{cannot convert value of type 'Int' to expected argument type '(Int, b: Int)'}}
-    method3(self)        // expected-error {{missing argument for parameter 'b' in call}}
+    method3(self)        // expected-error {{cannot convert value of type 'CurriedClass' to expected argument type '(Int, b: Int)'}}
   }
 }
 

test/expr/closure/closures.swift
@@ -38,7 +38,7 @@ func funcdecl5(a: Int, _ y: Int) {
 
   var testfunc : ((), Int) -> Int
   testfunc(          
-           {$0+1})  // expected-error {{missing argument for parameter #2 in call}}
+           {$0+1})  // expected-error {{cannot convert value of type '(Int) -> Int' to expected argument type '((), Int)'}}
 
   funcdecl5(1, 2) // recursion.
 

test/expr/closure/trailing.swift
@@ -68,7 +68,7 @@ var c3 = C().map // expected-note{{parsing trailing closure for this call}}
 // <rdar://problem/16835718> Ban multiple trailing closures
 func multiTrailingClosure(a : () -> (), b : () -> ()) {
   multiTrailingClosure({}) {} // ok
-  multiTrailingClosure {} {}   // expected-error {{missing argument for parameter #1 in call}} expected-error {{consecutive statements on a line must be separated by ';'}} {{26-26=;}} expected-error {{braced block of statements is an unused closure}} expected-error{{expression resolves to an unused function}}
+  multiTrailingClosure {} {}   // expected-error {{cannot convert value of type '() -> ()' to expected argument type '(() -> (), b: () -> ())'}} expected-error {{consecutive statements on a line must be separated by ';'}} {{26-26=;}} expected-error {{braced block of statements is an unused closure}} expected-error{{expression resolves to an unused function}}
   
   
 }

commit aad3cf9e10c63e2e8d1b415c6c36e7b81e66461a

Factor out structural error diagnostics out to a helper method (allowing the code to be actually readable since it unnests it greatly), and call it both before and after argument type validation.  This allows us to capture many more structural errors than before, leading to much better diagnostics in a lot of cases.  This also fixes the specific regressions introduced by 96a1e96.

lib/Sema/CSDiag.cpp
@@ -1190,6 +1190,11 @@ namespace {
     /// overloads.
     void suggestPotentialOverloads(SourceLoc loc, bool isResult = false);
 
+    /// If the candidate set has been narrowed down to a specific structural
+    /// problem, e.g. that there are too few parameters specified or that
+    /// argument labels don't match up, diagnose that error and return true.
+    bool diagnoseAnyStructuralArgumentError(Expr *argExpr);
+    
     void dump() const LLVM_ATTRIBUTE_USED;
     
   private:
@@ -1684,6 +1689,140 @@ suggestPotentialOverloads(SourceLoc loc, bool isResult) {
                     suggestionText);
   }
 }
+
+
+/// If the candidate set has been narrowed down to a specific structural
+/// problem, e.g. that there are too few parameters specified or that argument
+/// labels don't match up, diagnose that error and return true.
+bool CalleeCandidateInfo::diagnoseAnyStructuralArgumentError(Expr *argExpr) {
+  // TODO: We only handle the situation where there is exactly one candidate
+  // here.
+  if (size() != 1) return false;
+  
+  // We only handle structural errors here.
+  if (closeness != CC_ArgumentLabelMismatch &&
+      closeness != CC_ArgumentCountMismatch)
+    return false;
+    
+  auto args = decomposeArgParamType(argExpr->getType());
+  SmallVector<Identifier, 4> correctNames;
+  unsigned OOOArgIdx = ~0U, OOOPrevArgIdx = ~0U;
+  unsigned extraArgIdx = ~0U, missingParamIdx = ~0U;
+  
+  // If we have a single candidate that failed to match the argument list,
+  // attempt to use matchCallArguments to diagnose the problem.
+  struct OurListener : public MatchCallArgumentListener {
+    SmallVectorImpl<Identifier> &correctNames;
+    unsigned &OOOArgIdx, &OOOPrevArgIdx;
+    unsigned &extraArgIdx, &missingParamIdx;
+    
+  public:
+    OurListener(SmallVectorImpl<Identifier> &correctNames,
+                unsigned &OOOArgIdx, unsigned &OOOPrevArgIdx,
+                unsigned &extraArgIdx, unsigned &missingParamIdx)
+    : correctNames(correctNames),
+    OOOArgIdx(OOOArgIdx), OOOPrevArgIdx(OOOPrevArgIdx),
+    extraArgIdx(extraArgIdx), missingParamIdx(missingParamIdx) {}
+    void extraArgument(unsigned argIdx) override {
+      extraArgIdx = argIdx;
+    }
+    void missingArgument(unsigned paramIdx) override {
+      missingParamIdx = paramIdx;
+    }
+    void outOfOrderArgument(unsigned argIdx, unsigned prevArgIdx) override{
+      OOOArgIdx = argIdx;
+      OOOPrevArgIdx = prevArgIdx;
+    }
+    bool relabelArguments(ArrayRef<Identifier> newNames) override {
+      correctNames.append(newNames.begin(), newNames.end());
+      return true;
+    }
+  } listener(correctNames, OOOArgIdx, OOOPrevArgIdx,
+             extraArgIdx, missingParamIdx);
+  
+  // Use matchCallArguments to determine how close the argument list is (in
+  // shape) to the specified candidates parameters.  This ignores the
+  // concrete types of the arguments, looking only at the argument labels.
+  SmallVector<ParamBinding, 4> paramBindings;
+  auto params = decomposeArgParamType(candidates[0].getArgumentType());
+  if (!matchCallArguments(args, params, hasTrailingClosure,
+                          /*allowFixes:*/true, listener, paramBindings))
+    return false;
+  
+  
+  // If we are missing an parameter, diagnose that.
+  if (missingParamIdx != ~0U) {
+    Identifier name = params[missingParamIdx].Label;
+    auto loc = argExpr->getStartLoc();
+    if (name.empty())
+      CS->TC.diagnose(loc, diag::missing_argument_positional,
+                      missingParamIdx+1);
+    else
+      CS->TC.diagnose(loc, diag::missing_argument_named, name);
+    return true;
+  }
+  
+  if (extraArgIdx != ~0U) {
+    auto name = args[extraArgIdx].Label;
+    Expr *arg = argExpr;
+    auto tuple = dyn_cast<TupleExpr>(argExpr);
+    if (tuple)
+      arg = tuple->getElement(extraArgIdx);
+    auto loc = arg->getLoc();
+    if (tuple && extraArgIdx == tuple->getNumElements()-1 &&
+        tuple->hasTrailingClosure())
+      CS->TC.diagnose(loc, diag::extra_trailing_closure_in_call)
+        .highlight(arg->getSourceRange());
+    else if (params.empty())
+      CS->TC.diagnose(loc, diag::extra_argument_to_nullary_call)
+        .highlight(argExpr->getSourceRange());
+    else if (name.empty())
+      CS->TC.diagnose(loc, diag::extra_argument_positional)
+        .highlight(arg->getSourceRange());
+    else
+      CS->TC.diagnose(loc, diag::extra_argument_named, name)
+        .highlight(arg->getSourceRange());
+    return true;
+  }
+  
+  // If this is a argument label mismatch, then diagnose that error now.
+  if (!correctNames.empty() &&
+      CS->diagnoseArgumentLabelError(argExpr, correctNames,
+                                     /*isSubscript=*/false))
+    return true;
+  
+  // If we have an out-of-order argument, diagnose it as such.
+  if (OOOArgIdx != ~0U && isa<TupleExpr>(argExpr)) {
+    auto tuple = cast<TupleExpr>(argExpr);
+    Identifier first = tuple->getElementName(OOOArgIdx);
+    Identifier second = tuple->getElementName(OOOPrevArgIdx);
+    
+    SourceLoc diagLoc;
+    if (!first.empty())
+      diagLoc = tuple->getElementNameLoc(OOOArgIdx);
+    else
+      diagLoc = tuple->getElement(OOOArgIdx)->getStartLoc();
+    
+    if (!second.empty()) {
+      CS->TC.diagnose(diagLoc, diag::argument_out_of_order, first, second)
+        .highlight(tuple->getElement(OOOArgIdx)->getSourceRange())
+        .highlight(SourceRange(tuple->getElementNameLoc(OOOPrevArgIdx),
+                               tuple->getElement(OOOPrevArgIdx)->getEndLoc()));
+      return true;
+    }
+    
+    CS->TC.diagnose(diagLoc, diag::argument_out_of_order_named_unnamed, first,
+                    OOOPrevArgIdx)
+      .highlight(tuple->getElement(OOOArgIdx)->getSourceRange())
+      .highlight(tuple->getElement(OOOPrevArgIdx)->getSourceRange());
+    return true;
+  }
+  return false;
+}
+
+
+
+
 
 /// Flags that can be used to control name lookup.
 enum TCCFlags {
@@ -3505,7 +3644,9 @@ bool FailureDiagnosis::visitApplyExpr(ApplyExpr *callExpr) {
   
   // Filter the candidate list based on the argument we may or may not have.
   calleeInfo.filterContextualMemberList(callExpr->getArg());
-  
+
+  if (calleeInfo.diagnoseAnyStructuralArgumentError(callExpr->getArg()))
+    return true;
   
   Type argType;  // Type of the argument list, if knowable.
   if (auto FTy = fnType->getAs<AnyFunctionType>())
@@ -3529,124 +3670,8 @@ bool FailureDiagnosis::visitApplyExpr(ApplyExpr *callExpr) {
 
   calleeInfo.filterList(argExpr->getType());
 
-
-  // If we filtered this down to exactly one candidate, see if we can produce
-  // an extremely specific error about it.
-  if (calleeInfo.size() == 1) {
-    if (calleeInfo.closeness == CC_ArgumentLabelMismatch ||
-        calleeInfo.closeness == CC_ArgumentCountMismatch) {
-      auto args = decomposeArgParamType(argExpr->getType());
-      SmallVector<Identifier, 4> correctNames;
-      unsigned OOOArgIdx = ~0U, OOOPrevArgIdx = ~0U;
-      unsigned extraArgIdx = ~0U, missingParamIdx = ~0U;
-      
-      // If we have a single candidate that failed to match the argument list,
-      // attempt to use matchCallArguments to diagnose the problem.
-      struct OurListener : public MatchCallArgumentListener {
-        SmallVectorImpl<Identifier> &correctNames;
-        unsigned &OOOArgIdx, &OOOPrevArgIdx;
-        unsigned &extraArgIdx, &missingParamIdx;
-
-      public:
-        OurListener(SmallVectorImpl<Identifier> &correctNames,
-                    unsigned &OOOArgIdx, unsigned &OOOPrevArgIdx,
-                    unsigned &extraArgIdx, unsigned &missingParamIdx)
-           : correctNames(correctNames),
-             OOOArgIdx(OOOArgIdx), OOOPrevArgIdx(OOOPrevArgIdx),
-             extraArgIdx(extraArgIdx), missingParamIdx(missingParamIdx) {}
-        void extraArgument(unsigned argIdx) override {
-          extraArgIdx = argIdx;
-        }
-        void missingArgument(unsigned paramIdx) override {
-          missingParamIdx = paramIdx;
-        }
-        void outOfOrderArgument(unsigned argIdx, unsigned prevArgIdx) override{
-          OOOArgIdx = argIdx;
-          OOOPrevArgIdx = prevArgIdx;
-        }
-        bool relabelArguments(ArrayRef<Identifier> newNames) override {
-          correctNames.append(newNames.begin(), newNames.end());
-          return true;
-        }
-      } listener(correctNames, OOOArgIdx, OOOPrevArgIdx,
-                 extraArgIdx, missingParamIdx);
-
-      // Use matchCallArguments to determine how close the argument list is (in
-      // shape) to the specified candidates parameters.  This ignores the
-      // concrete types of the arguments, looking only at the argument labels.
-      SmallVector<ParamBinding, 4> paramBindings;
-      auto params = decomposeArgParamType(calleeInfo[0].getArgumentType());
-      if (matchCallArguments(args, params, hasTrailingClosure,
-                             /*allowFixes:*/true, listener, paramBindings)) {
-       
-        // If we are missing an parameter, diagnose that.
-        if (missingParamIdx != ~0U) {
-          Identifier name = params[missingParamIdx].Label;
-          auto loc = callExpr->getArg()->getStartLoc();
-          if (name.empty())
-            diagnose(loc, diag::missing_argument_positional,
-                        missingParamIdx+1);
-          else
-            diagnose(loc, diag::missing_argument_named, name);
-          return true;
-        }
-        
-        if (extraArgIdx != ~0U) {
-          auto name = args[extraArgIdx].Label;
-          Expr *arg = argExpr;
-          auto tuple = dyn_cast<TupleExpr>(argExpr);
-          if (tuple)
-            arg = tuple->getElement(extraArgIdx);
-          auto loc = arg->getLoc();
-          if (tuple && extraArgIdx == tuple->getNumElements()-1 &&
-              tuple->hasTrailingClosure())
-            diagnose(loc, diag::extra_trailing_closure_in_call)
-              .highlight(arg->getSourceRange());
-          else if (name.empty())
-            diagnose(loc, diag::extra_argument_positional)
-              .highlight(arg->getSourceRange());
-          else
-            diagnose(loc, diag::extra_argument_named, name)
-              .highlight(arg->getSourceRange());
-          return true;
-        }
-        
-        // If this is a argument label mismatch, then diagnose that error now.
-        if (!correctNames.empty() &&
-            CS->diagnoseArgumentLabelError(argExpr, correctNames,
-                                           /*isSubscript=*/false))
-          return true;
-        
-        // If we have an out-of-order argument, diagnose it as such.
-        if (OOOArgIdx != ~0U && isa<TupleExpr>(callExpr->getArg())) {
-          auto tuple = cast<TupleExpr>(callExpr->getArg());
-          Identifier first = tuple->getElementName(OOOArgIdx);
-          Identifier second = tuple->getElementName(OOOPrevArgIdx);
-
-          SourceLoc diagLoc;
-          if (!first.empty())
-            diagLoc = tuple->getElementNameLoc(OOOArgIdx);
-          else
-            diagLoc = tuple->getElement(OOOArgIdx)->getStartLoc();
-          
-          if (!second.empty()) {
-            diagnose(diagLoc,
-                     diag::argument_out_of_order, first, second)
-            .highlight(tuple->getElement(OOOArgIdx)->getSourceRange())
-            .highlight(SourceRange(tuple->getElementNameLoc(OOOPrevArgIdx),
-                               tuple->getElement(OOOPrevArgIdx)->getEndLoc()));
-            return true;
-          }
-          
-          diagnose(diagLoc, diag::argument_out_of_order_named_unnamed, first,
-                   OOOPrevArgIdx)
-            .highlight(tuple->getElement(OOOArgIdx)->getSourceRange())
-            .highlight(tuple->getElement(OOOPrevArgIdx)->getSourceRange());
-          return true;
-        }
-      }
-    }
-  }
+  if (calleeInfo.diagnoseAnyStructuralArgumentError(argExpr))
+    return true;
 
   // If we have a failure where the candidate set differs on exactly one
   // argument, and where we have a consistent mismatch across the candidate set

test/ClangModules/cfuncs_parse.swift
@@ -12,8 +12,8 @@ func test_cfunc1(i: Int) {
 func test_cfunc2(i: Int) {
   let f = cfunc2(i, 17)
   _ = f as Float
-  // FIXME: Should report this error: {{cannot convert the expression's type '$T3' to type 'CLong'}}
-  cfunc2(b:17, a:i) // expected-error{{cannot convert value of type 'Int' to expected argument type 'Int32'}}
+  cfunc2(b:17, a:i) // expected-error{{extraneous argument labels 'b:a:' in call}}
+  cfunc2(17, i) // expected-error{{cannot convert value of type 'Int' to expected argument type 'Int32'}}
 }
 
 func test_cfunc3_a() {

test/Constraints/closures.swift
@@ -98,7 +98,7 @@ func testMap() {
 }
 
 // <rdar://problem/22414757> "UnresolvedDot" "in wrong phase" assertion from verifier
-[].reduce { $0 + $1 }  // expected-error {{cannot convert value of type '(_, _) -> _' to expected argument type '(_, combine: @noescape (_, _) throws -> _)'}}
+[].reduce { $0 + $1 }  // expected-error {{missing argument for parameter #1 in call}}
 
 
 

test/Constraints/diagnostics.swift
@@ -308,12 +308,15 @@ func f7(a: Int)(b : Int) -> Int { // expected-warning{{curried function declarat
 f7(1)(b: 1)
 f7(1.0)(2)       // expected-error {{cannot convert value of type 'Double' to expected argument type 'Int'}}
 
-f7(1)(1.0)       // expected-error {{cannot convert value of type 'Double' to expected argument type 'Int'}}
+f7(1)(1.0)       // expected-error {{missing argument label 'b:' in call}}
+f7(1)(b: 1.0)       // expected-error {{cannot convert value of type 'Double' to expected argument type 'Int'}}
 
 let f8 = f7(2)
 f8(b: 1)
 f8(10)          // expected-error {{missing argument label 'b:' in call}} {{4-4=b: }}
-f8(1.0)         // expected-error {{cannot convert value of type 'Double' to expected argument type 'Int'}}
+f8(1.0)         // expected-error {{missing argument label 'b:' in call}}
+f8(b: 1.0)         // expected-error {{cannot convert value of type 'Double' to expected argument type 'Int'}}
+
 
 class CurriedClass {
   func method1() {}
@@ -352,7 +355,7 @@ _ = CurriedClass.method3(c)(1, 2)        // expected-error {{missing argument la
 _ = CurriedClass.method3(c)(1, b: 2)(32) // expected-error {{cannot call value of non-function type '()'}}
 _ = CurriedClass.method3(1, 2)           // expected-error {{extra argument in call}}
 CurriedClass.method3(c)(1.0, b: 1)       // expected-error {{cannot convert value of type 'Double' to expected argument type 'Int'}}
-CurriedClass.method3(c)(1)               // expected-error {{cannot convert value of type 'Int' to expected argument type '(Int, b: Int)'}}
+CurriedClass.method3(c)(1)               // expected-error {{missing argument for parameter 'b' in call}}
 
 CurriedClass.method3(c)(c: 1.0)          // expected-error {{missing argument for parameter 'b' in call}}
 
@@ -361,8 +364,8 @@ extension CurriedClass {
   func f() {
     method3(1, b: 2)
     method3()            // expected-error {{missing argument for parameter #1 in call}}
-    method3(42)          // expected-error {{cannot convert value of type 'Int' to expected argument type '(Int, b: Int)'}}
-    method3(self)        // expected-error {{cannot convert value of type 'CurriedClass' to expected argument type '(Int, b: Int)'}}
+    method3(42)          // expected-error {{missing argument for parameter 'b' in call}}
+    method3(self)        // expected-error {{missing argument for parameter 'b' in call}}
   }
 }
 
@@ -513,7 +516,8 @@ class B {
 
 func test(a : B) {
   B.f1(nil)    // expected-error {{nil is not compatible with expected argument type 'AOpts'}}
-  a.function(42, nil) //expected-error {{nil is not compatible with expected argument type 'AOpts'}}
+  a.function(42, a: nil) //expected-error {{nil is not compatible with expected argument type 'AOpts'}}
+  a.function(42, nil) //expected-error {{missing argument label 'a:' in call}}
   a.f2(nil)  // expected-error {{nil is not compatible with expected argument type 'AOpts'}}
 }
 

test/Generics/generic_types.swift
@@ -195,7 +195,8 @@ func useNested(ii: Int, hni: HasNested<Int>,
   typealias HNI = HasNested<Int>
   var id = hni.f(1, u: 3.14159)
   id = (2, 3.14159)
-  hni.f(1.5, 3.14159) // expected-error{{cannot convert value of type 'Double' to expected argument type 'Int'}}
+  hni.f(1.5, 3.14159) // expected-error{{missing argument label 'u:' in call}}
+  hni.f(1.5, u: 3.14159) // expected-error{{cannot convert value of type 'Double' to expected argument type 'Int'}}
 
   // Generic constructor of a generic struct
   HNI(1, 2.71828) // expected-warning{{unused}}

test/TypeCoercion/overload_member.swift
@@ -71,7 +71,7 @@ func test_mixed_overload(a: A, x: X, y: Y) {
   x1 = x
   var y1 = a.mixed(y: y) // expected-error{{incorrect argument label in call (have 'y:', expected 'x:')}}
   
-  A.mixed(x) // expected-error{{cannot convert value of type 'X' to expected argument type 'Y'}}
+  A.mixed(x) // expected-error{{missing argument label 'y:' in call}}
   var x2 = A.mixed(a)(x: x)
   x2 = x
   var y2 = A.mixed(y: y)
@@ -157,7 +157,7 @@ extension A {
   }
 
   class func test_mixed_overload_static(a a: A, x: X, y: Y) {
-    mixed(x) // expected-error{{cannot convert value of type 'X' to expected argument type 'Y'}}
+    mixed(x) // expected-error{{missing argument label 'y:' in call}}
     var x2 = mixed(a)(x: x)
     x2 = x
     var y2 = mixed(y: y)

test/decl/func/complete_object_init.swift
@@ -94,7 +94,7 @@ class B2 : A {
 
 func testConstructB2(i: Int) {
   var b2a = B2()
-  var b2b = B2(int: i) // expected-error{{extra argument 'int' in call}}
+  var b2b = B2(int: i) // expected-error{{argument passed to call that takes no arguments}}
 
   var b2: B2 = b2a
 }

test/decl/var/properties.swift
@@ -1138,7 +1138,7 @@ extension rdar17391625derived {
 struct r19874152S1 {
   let number : Int = 42
 }
-_ = r19874152S1(number:64)  // expected-error {{extra argument 'number' in call}}
+_ = r19874152S1(number:64)  // expected-error {{argument passed to call that takes no arguments}}
 _ = r19874152S1()  // Ok
 
 struct r19874152S2 {
@@ -1159,7 +1159,7 @@ _ = r19874152S3()  // expected-error {{missing argument for parameter 'flavour' 
 struct r19874152S4 {
   let number : Int? = nil
 }
-_ = r19874152S4(number:64)  // expected-error {{extra argument 'number' in call}}
+_ = r19874152S4(number:64)  // expected-error {{argument passed to call that takes no arguments}}
 _ = r19874152S4()  // Ok
 
 

test/expr/closure/closures.swift
@@ -37,8 +37,7 @@ func funcdecl5(a: Int, _ y: Int) {
 
 
   var testfunc : ((), Int) -> Int
-  testfunc(          
-           {$0+1})  // expected-error {{cannot convert value of type '(Int) -> Int' to expected argument type '((), Int)'}}
+  testfunc({$0+1})  // expected-error {{missing argument for parameter #2 in call}}
 
   funcdecl5(1, 2) // recursion.
 

test/expr/closure/trailing.swift
@@ -68,7 +68,7 @@ var c3 = C().map // expected-note{{parsing trailing closure for this call}}
 // <rdar://problem/16835718> Ban multiple trailing closures
 func multiTrailingClosure(a : () -> (), b : () -> ()) {
   multiTrailingClosure({}) {} // ok
-  multiTrailingClosure {} {}   // expected-error {{cannot convert value of type '() -> ()' to expected argument type '(() -> (), b: () -> ())'}} expected-error {{consecutive statements on a line must be separated by ';'}} {{26-26=;}} expected-error {{braced block of statements is an unused closure}} expected-error{{expression resolves to an unused function}}
+  multiTrailingClosure {} {}   // expected-error {{missing argument for parameter #1 in call}} expected-error {{consecutive statements on a line must be separated by ';'}} {{26-26=;}} expected-error {{braced block of statements is an unused closure}} expected-error{{expression resolves to an unused function}}
   
   
 }

test/expr/expressions.swift
@@ -534,7 +534,7 @@ struct Rule {
 }
 
 var ruleVar: Rule
-ruleVar = Rule("a") // expected-error {{cannot convert value of type 'String' to expected argument type '(target: String, dependencies: String)'}}
+ruleVar = Rule("a") // expected-error {{missing argument for parameter 'dependencies' in call}}
 
 
 class C {
@@ -544,7 +544,8 @@ class C {
   func method() {}
 }
 
-var c = C(3) // expected-error {{cannot convert value of type 'Int' to expected argument type 'C?'}}
+_ = C(3) // expected-error {{missing argument label 'other:' in call}}
+_ = C(other: 3) // expected-error {{cannot convert value of type 'Int' to expected argument type 'C?'}}
 
 //===----------------------------------------------------------------------===//
 // Unary Operators