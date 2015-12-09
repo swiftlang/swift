cdaaca1929ef0f8799a1e64651ce03a19a5c7c05
Adrian Prantl <aprantl@apple.com>
2015-12-08 16:11:48 -0800

    Tweak this test run work on Linux and Darwin. https://bugs.swift.org/browse/SR-149

--- test/DebugInfo/linetable.swift
+++ test/DebugInfo/linetable.swift
@@ -3,9 +3,6 @@
 
 // REQUIRES: CPU=i386_or_x86_64
 
-// FIXME: ASM-CHECK: for end-of-prologue fails on Linux. SR-149
-// XFAIL: linux
-
 import Swift
 func markUsed<T>(t: T) {}
 
@@ -29,9 +26,9 @@ func main(x: Int64) -> Void
 {
     var my_class = MyClass(input: 10)
 // Linetable continuity. Don't go into the closure expression.
-// ASM-CHECK: .loc	[[FILEID:[0-9]]] [[@LINE+1]] 5
+// ASM-CHECK: .loc [[FILEID:[0-9]]] [[@LINE+1]] 5
     call_me (
-// ASM-CHECK-NOT: .loc	[[FILEID]] [[@LINE+1]] 5
+// ASM-CHECK-NOT: .loc [[FILEID]] [[@LINE+1]] 5
 // CHECK: @_TTSf2i_n___TFF9linetable4mainFVs5Int64T_U_FT_T_
         {
             var result = my_class.do_something(x)
@@ -45,15 +42,15 @@ func main(x: Int64) -> Void
 
 // The swift_releases at the end should not jump to the point where
 // that memory was retained/allocated and also not to line 0.
-// ASM-CHECK-NOT: .loc	[[FILEID]] 0 0
-// ASM-CHECK: .loc	[[FILEID]] [[@LINE+2]] 1
+// ASM-CHECK-NOT: .loc [[FILEID]] 0 0
+// ASM-CHECK: .loc [[FILEID]] [[@LINE+2]] 1
 // ASM-CHECK: ret
 }
 
-// ASM-CHECK:__TTSf2i_n___TFF9linetable4mainFVs5Int64T_U_FT_T_:
+// ASM-CHECK: {{^_?_TTSf2i_n___TFF9linetable4mainFVs5Int64T_U_FT_T_:}}
 // ASM-CHECK-NOT: retq
 // The end-of-prologue should have a valid location (0 is ok, too).
-// ASM-CHECK: .loc	[[FILEID]] 0 {{[0-9]+}} prologue_end
-// ASM-CHECK: .loc	[[FILEID]] 33 {{[0-9]+}}
+// ASM-CHECK: .loc [[FILEID]] 0 {{[0-9]+}} prologue_end
+// ASM-CHECK: .loc [[FILEID]] 34 {{[0-9]+}}
 
 main(30)
