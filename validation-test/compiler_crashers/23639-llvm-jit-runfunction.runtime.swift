159d58dd8c41b7f58c6f8b64f2f33ad9c2c126a6
practicalswift <practicalswift@users.noreply.github.com>
2015-12-10 15:44:25 +0100

    Sync with https://github.com/practicalswift/swift-compiler-crashes
    Add 5 compiler crashes.

---
+++ validation-test/compiler_crashers/23639-llvm-jit-runfunction.runtime.swift
@@ -0,0 +1,16 @@
+// RUN: not --crash %target-swift-frontend %s -emit-silgen
+// Distributed under the terms of the MIT license
+// Test case submitted to project by https://github.com/practicalswift (practicalswift)
+// http://www.openradar.me/19423783
+
+protocol A {
+    var a: Int {
+        get
+    }
+}
+class B: A {
+    let a = 42
+}
+let b = B()
+let c = [b]
+let d = c as [A]
