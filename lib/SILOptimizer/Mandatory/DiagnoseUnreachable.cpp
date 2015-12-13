749c82c042e83215502bb80391531dcd1a63ec87
practicalswift <practicalswift@users.noreply.github.com>
2015-12-14 00:11:54 +0100

    Fix typo: incomming → incoming

--- lib/SILOptimizer/Mandatory/DiagnoseUnreachable.cpp
+++ lib/SILOptimizer/Mandatory/DiagnoseUnreachable.cpp
@@ -145,7 +145,7 @@ static void propagateBasicBlockArgs(SILBasicBlock &BB) {
   }
 
   // If we've reached this point, the optimization is valid, so optimize.
-  // We know that the incomming arguments from all predecessors are the same,
+  // We know that the incoming arguments from all predecessors are the same,
   // so just use them directly and remove the basic block paramters.
 
   // Drop the arguments from the branch instructions by creating a new branch
