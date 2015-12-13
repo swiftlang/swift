67f1c5646c4d244d224559067147297695964a14
practicalswift <practicalswift@users.noreply.github.com>
2015-12-13 22:30:09 +0100

    Fix typo: initiailized → initialized

--- lib/Sema/TypeCheckAttr.cpp
+++ lib/Sema/TypeCheckAttr.cpp
@@ -426,7 +426,7 @@ void AttributeEarlyChecker::visitLazyAttr(LazyAttr *attr) {
   if (!VD->hasStorage())
     return diagnoseAndRemoveAttr(attr, diag::lazy_not_on_computed);
 
-  // lazy is not allowed on a lazily initiailized global variable or on a
+  // lazy is not allowed on a lazily initialized global variable or on a
   // static property (which is already lazily initialized).
   if (VD->isStatic() ||
       (varDC->isModuleScopeContext() &&
