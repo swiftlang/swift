11d9be1117f017c621207dc37070677b70d008a4
Arsen Gasparyan <to.arsen.gasparyan@gmail.com>
2015-12-15 22:20:34 +0300

    [tests] add import statements to prevent unresolved symbols

--- test/1_stdlib/PrintString.swift
+++ test/1_stdlib/PrintString.swift
@@ -9,6 +9,14 @@ import Swift
 import Darwin
 import StdlibUnittest
 
+// Also import modules which are used by StdlibUnittest internally. This
+// workaround is needed to link all required libraries in case we compile
+// StdlibUnittest with -sil-serialize-all.
+import SwiftPrivate
+#if _runtime(_ObjC)
+import ObjectiveC
+#endif
+
 struct MyString : StringLiteralConvertible, StringInterpolationConvertible {
   init(str: String) {
     value = str
