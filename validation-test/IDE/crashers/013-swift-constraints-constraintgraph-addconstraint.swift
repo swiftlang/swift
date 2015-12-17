fa76f20016e7e707dc3f110d8626313748da21b9
Dmitri Gribenko <gribozavr@gmail.com>
2015-12-17 13:09:19 -0700

    Annotate new tests for the --no-assertions build

--- validation-test/IDE/crashers/013-swift-constraints-constraintgraph-addconstraint.swift
+++ validation-test/IDE/crashers/013-swift-constraints-constraintgraph-addconstraint.swift
@@ -1,4 +1,5 @@
 // XFAIL: *
 // FIXME: xfailing the test as there is no longer a free `map` function
 // RUN: not --crash %target-swift-ide-test -code-completion -code-completion-token=A -source-filename=%s
+// REQUIRES: asserts
 [[map)#^A^#
--- validation-test/IDE/crashers/022-swift-typechecker-applygenericarguments.swift
+++ validation-test/IDE/crashers/022-swift-typechecker-applygenericarguments.swift
@@ -1,4 +1,5 @@
 // RUN: not --crash %target-swift-ide-test -code-completion -code-completion-token=A -source-filename=%s
+// REQUIRES: asserts
 protocol a{
 func g:P
 protocol P{
