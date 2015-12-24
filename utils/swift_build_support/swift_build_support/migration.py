ca3e11b2d6529f6a6d7cc49f0c5be324f1fd8dd5
Brian Gesiak <modocache@gmail.com>
2015-12-23 23:44:38 -0500

    [build-script] Determine HOST_CC in build-script
    https://bugs.swift.org/browse/SR-237 calls for `build-script` and
    `build-script-impl` to be merged. This commit takes another step towards
    that goal by moving the logic that finds the path to the `clang` and
    `clang++` executables up into Python-land.
    
    Rather than simply moving all of the logic into `utils/build-script`,
    this commit moves relevant functions into a new Python module, named
    `swift_build_support`. This has several benefits:
    
    - The logic can be tested. Whereas `build-script-impl` needed to be run
      in order to verify its behavior, the logic extracted out of it into
      `swift_build_support` can be tested in isolation.
    - The logic can be split up into several files without polluting the
      `utils` directory, which now contains many different files that are
      unrelated to `build-script`.

---
+++ utils/swift_build_support/swift_build_support/migration.py
@@ -0,0 +1,56 @@
+# swift_build_support/migration.py - Migrating build-script -*- python -*-
+#
+# This source file is part of the Swift.org open source project
+#
+# Copyright (c) 2014 - 2015 Apple Inc. and the Swift project authors
+# Licensed under Apache License v2.0 with Runtime Library Exception
+#
+# See http://swift.org/LICENSE.txt for license information
+# See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+#
+# ----------------------------------------------------------------------------
+#
+# utils/build-script takes arguments for its argument parser, as well as
+# arguments that are meant to be passed directly to utils/build-script-impl.
+# In order to gradually migrate away from build-script-impl, this module
+# provides tools to handle parsing of these args.
+#
+# ----------------------------------------------------------------------------
+
+
+def migrate_impl_args(argv, migrate_args):
+    """
+    Given a list of arguments of the form:
+
+        --foo --bar=baz -- --flim=flam
+
+    And a list of arguments to migrate, return a list in which the arguments
+    to migrate come before the '--' separator. For example, were we to migrate
+    '--flim', we would return:
+
+        --foo --bar=baz --flim=flam --
+
+    Note that we do not attempt to remove the '--' separator if it is no longer
+    necessary, nor do we replace '--flim' if it already appears before the
+    separator. In these cases, argparse "does the right thing": it can handle
+    a trailing separator, and when options that are specified twice argparse
+    uses the second value.
+    """
+    try:
+        split_index = argv.index('--')
+    except ValueError:
+        # If there is no separator, then we have nothing to migrate.
+        return argv
+
+    args = argv[:split_index]
+    impl_args = argv[split_index:]
+    impl_args_to_remove = []
+    for index, impl_arg in enumerate(impl_args):
+        if impl_arg.split('=')[0] in migrate_args:
+            args.append(impl_arg)
+            impl_args_to_remove.append(impl_arg)
+
+    for impl_arg_to_remove in impl_args_to_remove:
+        impl_args.remove(impl_arg_to_remove)
+
+    return args + impl_args
---
+++ utils/swift_build_support/swift_build_support/which.py
@@ -0,0 +1,36 @@
+# swift_build_support/which.py - shutil.which() for Python 2.7 -*- python -*-
+#
+# This source file is part of the Swift.org open source project
+#
+# Copyright (c) 2014 - 2015 Apple Inc. and the Swift project authors
+# Licensed under Apache License v2.0 with Runtime Library Exception
+#
+# See http://swift.org/LICENSE.txt for license information
+# See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+#
+# ----------------------------------------------------------------------------
+#
+# A naive reimplementation of shutil.which() for Python 2.7. This can be
+# removed if shutil.which() is backported, or if the Swift build toolchain
+# migrates completely to Python 3.3+.
+#
+# ----------------------------------------------------------------------------
+
+import subprocess
+
+
+def which(cmd):
+    """
+    Return the path to an executable which would be run if
+    the given cmd was called. If no cmd would be called, return None.
+
+    Python 3.3+ provides this behavior via the shutil.which() function;
+    see: https://docs.python.org/3.3/library/shutil.html#shutil.which
+
+    We provide our own implementation because shutil.which() has not
+    been backported to Python 2.7, which we support.
+    """
+    try:
+        return subprocess.check_output(['which', cmd]).rstrip()
+    except subprocess.CalledProcessError:
+        return None
