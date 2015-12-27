e7184aaac5a4b4d281ece631110baf15142c32e1
Brian Gesiak <modocache@gmail.com>
2015-12-27 15:46:30 -0500

    [cmpcodesize] Add code headers
    Add code headers missing from the Python files in utils/cmpcodesize, as per
    the template from #762.

--- utils/cmpcodesize/cmpcodesize.py
+++ utils/cmpcodesize/cmpcodesize.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python
+# cmpcodesize.py - Compare sizes of built products -*- python -*-
+#
+# This source file is part of the Swift.org open source project
+#
+# Copyright (c) 2014 - 2015 Apple Inc. and the Swift project authors
+# Licensed under Apache License v2.0 with Runtime Library Exception
+#
+# See http://swift.org/LICENSE.txt for license information
+# See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
 
 from cmpcodesize.main import main
 
--- utils/cmpcodesize/cmpcodesize/compare.py
+++ utils/cmpcodesize/cmpcodesize/compare.py
@@ -1,3 +1,13 @@
+# cmpcodesize/compare.py - Compare sizes of built products -*- python -*-
+#
+# This source file is part of the Swift.org open source project
+#
+# Copyright (c) 2014 - 2015 Apple Inc. and the Swift project authors
+# Licensed under Apache License v2.0 with Runtime Library Exception
+#
+# See http://swift.org/LICENSE.txt for license information
+# See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+
 from __future__ import print_function
 
 import re
