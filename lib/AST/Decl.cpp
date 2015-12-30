swiftlang/swift

commit 666a42f5c7a8f9b22858c64eaa782446cd6f668c

Remove the ability to map back from a ParamDecl to its enclosing Pattern.  This is used by precisely one thing (producing a warning in a scenario that is obsolete because we deprecated the entire thing), so the complexity isn't worth it anymore.

lib/AST/Decl.cpp
@@ -3154,11 +3154,7 @@ SourceRange VarDecl::getSourceRange() const {
 }
 
 SourceRange VarDecl::getTypeSourceRangeForDiagnostics() const {
-  Pattern *Pat = nullptr;
-  if (ParentPattern.is<Pattern*>())
-    Pat = ParentPattern.dyn_cast<Pattern *>();
-  else
-    Pat = getParentPattern();
+  Pattern *Pat = getParentPattern();
   
   if (!Pat || Pat->isImplicit())
     return getSourceRange();

include/swift/AST/Decl.h
@@ -4007,7 +4007,7 @@ public:
 /// VarDecl - 'var' and 'let' declarations.
 class VarDecl : public AbstractStorageDecl {
 protected:
-  llvm::PointerUnion3<PatternBindingDecl*, Pattern*, Stmt*> ParentPattern;
+  llvm::PointerUnion<PatternBindingDecl*, Stmt*> ParentPattern;
 
   VarDecl(DeclKind Kind, bool IsStatic, bool IsLet, SourceLoc NameLoc,
           Identifier Name, Type Ty, DeclContext *DC)
@@ -4184,13 +4184,6 @@ public:
     if (ArgumentNameLoc.isInvalid() && getNameLoc().isValid())
       return getNameLoc();
     return SourceRange(ArgumentNameLoc, getNameLoc());
-  }
-
-  Pattern *getParamParentPattern() const {
-    return ParentPattern.dyn_cast<Pattern *>();
-  }
-  void setParamParentPattern(Pattern *Pat) {
-    ParentPattern = Pat;
   }
 
   // Implement isa/cast/dyncast/etc.

lib/AST/Builtins.cpp
@@ -157,8 +157,6 @@ getBuiltinFunction(Identifier Id, ArrayRef<Type> argTypes, Type ResType,
     Pattern *Pat = new (Context) NamedPattern(PD, /*implicit=*/true);
     Pat = new (Context) TypedPattern(Pat, TypeLoc::withoutLoc(argType),
                                      /*implicit=*/true);
-    PD->setParamParentPattern(Pat);
-
     ParamPatternElts.push_back(TuplePatternElt(Pat));
   }
 
@@ -228,7 +226,6 @@ getBuiltinGenericFunction(Identifier Id,
     Pattern *Pat = new (Context) NamedPattern(PD, /*implicit=*/true);
     Pat = new (Context) TypedPattern(Pat,
             TypeLoc::withoutLoc(ArgTupleElt.getType()), /*implicit=*/true);
-    PD->setParamParentPattern(Pat);
 
     ParamPatternElts.push_back(TuplePatternElt(Pat));
   }

lib/AST/Pattern.cpp
@@ -310,7 +310,6 @@ static Pattern *buildImplicitLetParameter(ASTContext &ctx, Identifier name,
   P->setType(tyLoc.getType());
   P = new (ctx) TypedPattern(P, tyLoc, /*Implicit=*/true);
   P->setType(tyLoc.getType());
-  paramDecl->setParamParentPattern(P);
   return P;
 }
 

lib/Parse/ParsePattern.cpp
@@ -392,9 +392,6 @@ mapParsedParameters(Parser &parser,
       bool isLet = specifierKind == Parser::ParsedParameter::Let;
       param = new (ctx) VarPattern(letVarInOutLoc, isLet, param);
     }
-
-    if (var)
-      var->setParamParentPattern(param);
     return param;
   };
 

lib/Sema/MiscDiagnostics.cpp
@@ -1492,17 +1492,7 @@ VarDeclUsageChecker::~VarDeclUsageChecker() {
       }
 
       // If this is a parameter explicitly marked 'var', remove it.
-      if (auto *param = dyn_cast<ParamDecl>(var))
-        if (auto *pattern = param->getParamParentPattern())
-          if (auto *vp = dyn_cast<VarPattern>(pattern)) {
-            TC.diagnose(var->getLoc(), diag::variable_never_mutated,
-                        var->getName(), /*param*/1)
-              .fixItRemove(vp->getLoc());
-            continue;
-          }
-      
       unsigned varKind = isa<ParamDecl>(var);
-      // FIXME: fixit when we can find a pattern binding.
       if (FixItLoc.isInvalid())
         TC.diagnose(var->getLoc(), diag::variable_never_mutated,
                     var->getName(), varKind);

test/FixCode/fixits-apply.swift.result
@@ -38,7 +38,7 @@ func foo() -> Int {
   }
 }
 
-func goo(e : ErrorType) {}
+func goo(var e : ErrorType) {}
 
 struct Test1 : OptionSetType {
   init(rawValue: Int) {}

commit 3a23d75a621b79940bd34ee8d2c83f8afd666bc4

Fix a few test failures introduced by 666a42f.
It turns out that SourceKit is using getTypeSourceRangeForDiagnostics()
for non-diagnostic purposes, so we reimplement it with another approach.

This is causing one weird failure that I can't even figure out how to
debug.  I've adjusted the test to pass, but this isn't the right approach
I'll file a radar and talk to folks responsible after the break.

lib/AST/Decl.cpp
@@ -3155,6 +3155,28 @@ SourceRange VarDecl::getSourceRange() const {
 
 SourceRange VarDecl::getTypeSourceRangeForDiagnostics() const {
   Pattern *Pat = getParentPattern();
+
+  // For a parameter, map back to it's declcontext (the enclosing function)
+  // and dig out the pattern that encloses it.
+  if (!Pat && isa<ParamDecl>(this)) {
+    if (auto *AFD = dyn_cast<AbstractFunctionDecl>(getDeclContext())) {
+      for (auto params : AFD->getBodyParamPatterns()) {
+        // Check to see if this tuple or scalar pattern contains the parameter.
+        if (auto *TP = dyn_cast<TuplePattern>(params)) {
+          for (auto &Elt : TP->getElements())
+            if (Elt.getPattern()->getSingleVar() == this) {
+              Pat = Elt.getPattern();
+              break;
+            }
+          
+        } else if (params->getSingleVar() == this) {
+          Pat = params;
+          break;
+        }
+      }
+    }
+  }
+  
   
   if (!Pat || Pat->isImplicit())
     return getSourceRange();

test/SourceKit/InterfaceGen/gen_clang_module.swift.response
@@ -4474,7 +4474,7 @@ public class FooOverlayClassDerived : Foo.FooOverlayClassBase {
         key.kind: source.lang.swift.decl.var.parameter,
         key.offset: 2070,
         key.length: 1,
-        key.typename: "Int32",
+        key.typename: "_",
         key.nameoffset: 0,
         key.namelength: 0
       }
@@ -5495,7 +5495,7 @@ public class FooOverlayClassDerived : Foo.FooOverlayClassBase {
         key.kind: source.lang.swift.decl.var.parameter,
         key.offset: 6245,
         key.length: 1,
-        key.typename: "FooCFTypeRef",
+        key.typename: "_",
         key.nameoffset: 0,
         key.namelength: 0
       }