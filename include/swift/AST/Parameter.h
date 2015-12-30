fb276d30162e5a8be1e0f704c6a1452043eb0cd9
Chris Lattner <sabre@iMac.local>
2015-12-29 21:40:45 -0800

    fill out the Parameter and ParameterList APIs and implementation to be a lot closer to what we need.  NFC since there are no clients yet.

--- include/swift/AST/Parameter.h
+++ include/swift/AST/Parameter.h
@@ -20,6 +20,8 @@
 
 #include "swift/AST/DefaultArgumentKind.h"
 #include "swift/AST/TypeLoc.h"
+#include "swift/AST/Decl.h"
+#include "swift/Basic/OptionSet.h"
 
 namespace swift {
   class ParamDecl;
@@ -33,8 +35,8 @@ namespace swift {
 ///   a : Int = 42        //< Default value.
 ///   a : Int...          //< Varargs parameter.
 ///
-/// Parameters are stored in parameter lists, and multiple levels of currying
-/// are represented with multiple parameter lists.
+/// A parameter is stored as ParamDecls with additional information in the
+/// Parameter struct to represent source information as well as extra semantics.
 ///
 struct Parameter {
   /// The decl keeps track of the internal and external parameter name, as well
@@ -51,16 +53,50 @@ struct Parameter {
   // an Expr* here, along with a "is checked" bit.
 
   /// The default value, if any, along with whether this is varargs.
-  llvm::PointerIntPair<ExprHandle *, 1, bool> defaultValueAndIsVarargs;
+  llvm::PointerIntPair<ExprHandle *, 1, bool> defaultValueAndIsVariadic;
 
+  void setDefaultValue(ExprHandle *H) {
+    defaultValueAndIsVariadic.setPointer(H);
+  }
   ExprHandle *getDefaultValue() const {
-    return defaultValueAndIsVarargs.getPointer();
+    return defaultValueAndIsVariadic.getPointer();
   }
   /// Whether or not this parameter is varargs.
-  bool isVarargs() const { return defaultValueAndIsVarargs.getInt(); }
+  bool isVariadic() const { return defaultValueAndIsVariadic.getInt(); }
+  void setVariadic(bool value = true) {defaultValueAndIsVariadic.setInt(value);}
   
   /// Information about a symbolic default argument, like __FILE__.
   DefaultArgumentKind defaultArgumentKind;
+  
+  /// Remove the type of this varargs element designator, without the array
+  /// type wrapping it.  A parameter like "Int..." will have formal parameter
+  /// type of "[Int]" and this returns "Int".
+  static Type getVarargBaseTy(Type VarArgT);
+  
+  /// Remove the type of this varargs element designator, without the array
+  /// type wrapping it.
+  Type getVarargBaseTy() const {
+    assert(isVariadic());
+    return getVarargBaseTy(decl->getType());
+  }
+  
+  /// Create a simple parameter without location information.
+  static Parameter withoutLoc(ParamDecl *decl) {
+    Parameter result;
+    result.decl = decl;
+    result.type = TypeLoc::withoutLoc(decl->getType());
+    return result;
+  }
+  
+  /// Return the full source range of this parameter.
+  SourceRange getSourceRange() const;
+  SourceLoc getStartLoc() const { return getSourceRange().Start; }
+  SourceLoc getEndLoc() const { return getSourceRange().End; }
+
+  void dump() const;
+  void dump(raw_ostream &OS, unsigned Indent = 0) const;
+  
+  //  void print(raw_ostream &OS) const;
 };
 
 
@@ -73,12 +109,49 @@ class alignas(alignof(Parameter)) ParameterList {
   void *operator new(size_t Bytes, ASTContext &C,
                      unsigned Alignment = 8);
 
+  SourceLoc LParenLoc, RParenLoc;
   size_t numParameters;
 
   ParameterList(size_t numParameters) : numParameters(numParameters) {}
   void operator=(const ParameterList&) = delete;
 public:
-  static ParameterList *create(ASTContext &Context, ArrayRef<Parameter> Params);
+  /// Create a parameter list with the specified parameters.
+  static ParameterList *create(const ASTContext &C, SourceLoc LParenLoc,
+                               ArrayRef<Parameter> params,
+                               SourceLoc RParenLoc);
+
+  /// Create a parameter list with the specified parameters, with no location
+  /// info for the parens.
+  static ParameterList *create(const ASTContext &C,
+                               ArrayRef<Parameter> params) {
+    return create(C, SourceLoc(), params, SourceLoc());
+  }
+ 
+  /// Create an empty parameter list.
+  static ParameterList *createEmpty(const ASTContext &C,
+                                    SourceLoc LParenLoc = SourceLoc(),
+                                    SourceLoc RParenLoc = SourceLoc()) {
+    return create(C, LParenLoc, {}, RParenLoc);
+  }
+  
+  /// Create a parameter list for a single parameter lacking location info.
+  static ParameterList *createWithoutLoc(ParamDecl *decl) {
+    return create(decl->getASTContext(), Parameter::withoutLoc(decl));
+  }
+  
+  /// Create an implicit 'self' decl for a method in the specified decl context.
+  /// If 'static' is true, then this is self for a static method in the type.
+  ///
+  /// Note that this decl is created, but it is returned with an incorrect
+  /// DeclContext that needs to be set correctly.  This is automatically handled
+  /// when a function is created with this as part of its argument list.
+  ///
+  static ParameterList *createSelf(SourceLoc loc, DeclContext *DC,
+                                   bool isStaticMethod = false,
+                                   bool isInOut = false);
+
+  SourceLoc getLParenLoc() const { return LParenLoc; }
+  SourceLoc getRParenLoc() const { return RParenLoc; }
   
   size_t getNumParameters() const {
     return numParameters;
@@ -100,6 +173,46 @@ public:
   Parameter &getParameter(unsigned i) {
     return getParameters()[i];
   }
+  
+  /// Change the DeclContext of any contained parameters to the specified
+  /// DeclContext.
+  void setDeclContextOfParamDecls(DeclContext *DC);
+  
+  
+  /// Flags used to indicate how ParameterList cloning should operate.
+  enum CloneFlags {
+    /// The cloned ParamDecls should be marked implicit.
+    Implicit = 0x01,
+    /// The cloned pattern is for an inherited constructor; mark default
+    /// arguments as inherited, and mark unnamed arguments as named.
+    Inherited = 0x02
+  };
+  
+  /// Make a duplicate copy of this parameter list.  This allocates copies of
+  /// the ParamDecls, so they can be reparented into a new DeclContext.
+  ParameterList *clone(const ASTContext &C,
+                       OptionSet<CloneFlags> options = None) const;
+  
+  /// Return a TupleType or ParenType for this parameter list.  This returns a
+  /// null type if one of the ParamDecls does not have a type set for it yet.
+  Type getType(const ASTContext &C) const;
+  
+  /// Return the full function type for a set of curried parameter lists that
+  /// returns the specified result type.  This returns a null type if one of the
+  /// ParamDecls does not have a type set for it yet.
+  ///
+  static Type getFullType(Type resultType, ArrayRef<ParameterList*> PL);
+  
+  
+  /// Return the full source range of this parameter.
+  SourceRange getSourceRange() const;
+  SourceLoc getStartLoc() const { return getSourceRange().Start; }
+  SourceLoc getEndLoc() const { return getSourceRange().End; }
+
+  void dump() const;
+  void dump(raw_ostream &OS, unsigned Indent = 0) const;
+  
+  //  void print(raw_ostream &OS) const;
 };
 
 } // end namespace swift.
--- lib/AST/Parameter.cpp
+++ lib/AST/Parameter.cpp
@@ -17,14 +17,58 @@
 
 #include "swift/AST/Parameter.h"
 #include "swift/AST/ASTContext.h"
+#include "swift/AST/Expr.h"
+#include "swift/AST/ExprHandle.h"
 using namespace swift;
+
+/// Return the full source range of this parameter.
+SourceRange Parameter::getSourceRange() const {
+  SourceRange range = decl->getSourceRange();
+  if (range.isInvalid()) return range;
+  
+  // It would be nice to extend the front of the range to show where inout is,
+  // but we don't have that location info.  Extend the back of the range to the
+  // location of the default argument, or the typeloc if they are valid.
+  if (auto expr = getDefaultValue()) {
+    auto endLoc = expr->getExpr()->getEndLoc();
+    if (endLoc.isValid())
+      return SourceRange(range.Start, endLoc);
+  }
+
+  // If the typeloc has a valid location, use it to end the range.
+  if (auto typeRepr = type.getTypeRepr()) {
+    auto endLoc = typeRepr->getEndLoc();
+    if (endLoc.isValid())
+      return SourceRange(range.Start, endLoc);
+  }
+  
+  // Otherwise, just return the info we have about the parameter.
+  return range;
+}
+
+Type Parameter::getVarargBaseTy(Type VarArgT) {
+  TypeBase *T = VarArgT.getPointer();
+  if (ArraySliceType *AT = dyn_cast<ArraySliceType>(T))
+    return AT->getBaseType();
+  if (BoundGenericType *BGT = dyn_cast<BoundGenericType>(T)) {
+    // It's the stdlib Array<T>.
+    return BGT->getGenericArgs()[0];
+  }
+  assert(isa<ErrorType>(T));
+  return T;
+}
+
 
 /// TODO: unique and reuse the () parameter list in ASTContext, as well as the
 /// "(self : T)" parameter lists common to many methods.
 ParameterList *
-ParameterList::create(ASTContext &context, ArrayRef<Parameter> params) {
+ParameterList::create(const ASTContext &C, SourceLoc LParenLoc,
+                      ArrayRef<Parameter> params, SourceLoc RParenLoc) {
+  assert(LParenLoc.isValid() == RParenLoc.isValid() &&
+         "Either both paren locs are valid or neither are");
+  
   auto byteSize = sizeof(ParameterList)+params.size()*sizeof(Parameter);
-  auto rawMem = context.Allocate(byteSize, alignof(ParameterList));
+  auto rawMem = C.Allocate(byteSize, alignof(ParameterList));
   
   //  Placement initialize the ParameterList and the Parameter's.
   auto PL = ::new (rawMem) ParameterList(params.size());
@@ -34,3 +78,121 @@ ParameterList::create(ASTContext &context, ArrayRef<Parameter> params) {
   
   return PL;
 }
+
+/// Create an implicit 'self' decl for a method in the specified decl context.
+/// If 'static' is true, then this is self for a static method in the type.
+///
+/// Note that this decl is created, but it is returned with an incorrect
+/// DeclContext that needs to be set correctly.  This is automatically handled
+/// when a function is created with this as part of its argument list.
+///
+ParameterList *ParameterList::createSelf(SourceLoc loc, DeclContext *DC,
+                                         bool isStaticMethod, bool isInOut) {
+  auto *param = ParamDecl::createSelf(loc, DC, isStaticMethod, isInOut);
+  return create(DC->getASTContext(), Parameter::withoutLoc(param));
+}
+
+/// Change the DeclContext of any contained parameters to the specified
+/// DeclContext.
+void ParameterList::setDeclContextOfParamDecls(DeclContext *DC) {
+  for (auto &P : getParameters())
+    P.decl->setDeclContext(DC);
+}
+
+
+
+/// Make a duplicate copy of this parameter list.  This allocates copies of
+/// the ParamDecls, so they can be reparented into a new DeclContext.
+ParameterList *ParameterList::clone(const ASTContext &C,
+                                    OptionSet<CloneFlags> options) const {
+  // If this list is empty, don't actually bother with a copy.
+  if (getNumParameters() == 0)
+    return const_cast<ParameterList*>(this);
+  
+  SmallVector<Parameter, 8> params(getParameters().begin(),
+                                   getParameters().end());
+
+  // Remap the ParamDecls inside of the ParameterList.
+  for (auto &param : params) {
+    auto decl = param.decl;
+    auto name = decl->getName();
+    // If the argument isn't named, and we're cloning for an inherited
+    // constructor, give the parameter a name so that silgen will produce a
+    // value for it.
+    if (name.empty() && (options & Inherited))
+      name = C.getIdentifier("argument");
+    
+    param.decl = new (C) ParamDecl(decl->isLet(),
+                                   decl->getArgumentNameLoc(),
+                                   decl->getArgumentName(),
+                                   decl->getLoc(), name,
+                                   decl->hasType() ? decl->getType() : Type(),
+                                   decl->getDeclContext());
+    if ((options & Implicit) || decl->isImplicit())
+      param.decl->setImplicit();
+
+    // If we're inheriting a default argument, mark it as such.
+    if (param.defaultArgumentKind != DefaultArgumentKind::None &&
+        (options & Inherited)) {
+      param.defaultArgumentKind = DefaultArgumentKind::Inherited;
+      param.setDefaultValue(nullptr);
+    }
+  }
+  
+  return create(C, getParameters());
+}
+
+/// Return a TupleType or ParenType for this parameter list.  This returns a
+/// null type if one of the ParamDecls does not have a type set for it yet.
+Type ParameterList::getType(const ASTContext &C) const {
+  if (getNumParameters() == 0)
+    return TupleType::getEmpty(C);
+  
+  SmallVector<TupleTypeElt, 8> argumentInfo;
+  
+  for (auto &P : getParameters()) {
+    if (!P.decl->hasType()) return Type();
+    
+    argumentInfo.push_back({
+      P.decl->getType(), P.decl->getArgumentName(), P.defaultArgumentKind,
+      P.isVariadic()
+    });
+  }
+  
+  return TupleType::get(argumentInfo, C);
+}
+
+
+/// Return the full function type for a set of curried parameter lists that
+/// returns the specified result type.  This returns a null type if one of the
+/// ParamDecls does not have a type set for it yet.
+///
+Type ParameterList::getFullType(Type resultType, ArrayRef<ParameterList*> PLL) {
+  auto result = resultType;
+  auto &C = result->getASTContext();
+  
+  for (auto PL : reversed(PLL)) {
+    auto paramType = PL->getType(C);
+    if (!paramType) return Type();
+    result = FunctionType::get(paramType, result);
+  }
+  return result;
+}
+
+/// Return the full source range of this parameter list.
+SourceRange ParameterList::getSourceRange() const {
+  // If we have locations for the parens, then they define our range.
+  if (LParenLoc.isValid())
+    return { LParenLoc, RParenLoc };
+  
+  // Otherwise, try the first and last parameter.
+  if (getNumParameters() != 0) {
+    auto Start = getParameter(0).getStartLoc();
+    auto End = getParameters().back().getEndLoc();
+    if (Start.isValid() && End.isValid())
+      return { Start, End };
+  }
+
+  return SourceRange();
+}
+
