7e5efb48ae567fbd727eb8bdae615ac53bed23d6
Nadav Rotem <nrotem@apple.com>
2016-01-01 21:40:16 -0800

    [swift-compress] Add flags to select one of the two compression independently.
    Add the flags "-cbc-only" (to enable only the dictionary-based codebook
    compression), and "-huff-only" to enable only the variable length encoding.

--- tools/swift-compress/swift-compress.cpp
+++ tools/swift-compress/swift-compress.cpp
@@ -25,19 +25,46 @@
 #include <iostream>
 #include <string>
 
+using EncodingKind = swift::Compress::EncodingKind;
+
 static llvm::cl::opt<bool>
-DecompressMode("decompress",
-llvm::cl::desc("Decompress the incoming strings."));
+DecompressMode("decompress", llvm::cl::desc("Decompress the input."));
+// Decompress only using the (CBC code book compression) dictionary
+// compression.
+static llvm::cl::opt<bool>
+CBCOnly("cbc-only", llvm::cl::desc("Only use CBC compression."));
+// Decompress only using the (CBC code book compression) dictionary
+// compression.
+static llvm::cl::opt<bool>
+HuffOnly("huff-only", llvm::cl::desc("Only use variable length encoding."));
 
 static std::string Compress(const std::string &In, bool Compress) {
-    // Only inspect Swift mangled names.
-    if (In.substr(0, 2) != "_T") return In;
+  // Only inspect Swift mangled names.
+  if (In.substr(0, 2) != "_T") return In;
 
-    return std::string("_T") + (Compress ?
-     swift::Compress::CompressName(In.substr(2, std::string::npos)) :
-     swift::Compress::DecompressName(In.substr(2, std::string::npos)));
+  auto Prefix = std::string("_T");
+  auto Payload = In.substr(2, std::string::npos);
+
+  if (CBCOnly) {
+    return Prefix + (Compress ?
+                     swift::Compress::EncodeCBCString(Payload) :
+                     swift::Compress::DecodeCBCString(Payload));
+  }
+
+  if (HuffOnly) {
+    if (Compress) {
+      llvm::APInt num = EncodeStringAsNumber(Payload, EncodingKind::Variable);
+      return Prefix + DecodeStringFromNumber(num, EncodingKind::Fixed);
+    } else {
+      llvm::APInt num = EncodeStringAsNumber(Payload, EncodingKind::Fixed);
+      return Prefix + DecodeStringFromNumber(num, EncodingKind::Variable);
+    }
+  }
+
+  return Prefix + (Compress ?
+                   swift::Compress::CompressName(Payload) :
+                   swift::Compress::DecompressName(Payload));
 }
-
 static llvm::StringRef substrBefore(llvm::StringRef whole,
                                     llvm::StringRef part) {
   return whole.slice(0, part.data() - whole.data());
@@ -51,11 +78,18 @@ static llvm::StringRef substrAfter(llvm::StringRef whole,
 int main(int argc, char **argv) {
   llvm::cl::ParseCommandLineOptions(argc, argv);
 
+  if (HuffOnly && CBCOnly) {
+    llvm::errs() << "Can't select two compression mode at once." << '\n';
+    return EXIT_FAILURE;
+  }
+
   auto input = llvm::MemoryBuffer::getSTDIN();
+
   if (!input) {
     llvm::errs() << input.getError().message() << '\n';
     return EXIT_FAILURE;
   }
+
   llvm::StringRef inputContents = input.get()->getBuffer();
 
   llvm::Regex maybeSymbol("_T[_a-zA-Z0-9$]+");
