e1ef60987c3ad46ad348521b5e9074fe3bdc9d9d
Michael Gottesman <mgottesman@apple.com>
2016-01-03 18:39:49 -0600

    Revert "[codegardening] Move MemBehaviorDumper from AADumper.cpp into its own file MemoryBehaviorDumper.cpp." Revert "Make AADumper and MemoryBehaviorDumper function passes. They do not need to be module passes."
    This reverts commit a503269e2d4a8739818002de3bc0f31a2814b071.
    This reverts commit 375f525c51ef56a9a76918d57060778daefd9e74.
    
    Turns out we /do/ want these two passes to be module passes so that their output
    is independent of how the pass manager schedules function passes.
    
    I tried to just fix the issue in MemBehaviorDumper/AADumper without reverting,
    but somehow this caused their tests to start failing?!
    
    I will try separating them again in a subsequent commit.

--- lib/SILOptimizer/UtilityPasses/AADumper.cpp
+++ lib/SILOptimizer/UtilityPasses/AADumper.cpp
@@ -16,7 +16,7 @@
 //
 //===----------------------------------------------------------------------===//
 
-#define DEBUG_TYPE "sil-aa-dumper"
+#define DEBUG_TYPE "sil-aa-evaluator"
 #include "swift/SILOptimizer/PassManager/Passes.h"
 #include "swift/SIL/SILArgument.h"
 #include "swift/SIL/SILFunction.h"
@@ -53,51 +53,106 @@ static bool gatherValues(SILFunction &Fn, std::vector<SILValue> &Values) {
 namespace {
 
 /// Dumps the alias relations between all instructions of a function.
-class SILAADumper : public SILFunctionTransform {
+class SILAADumper : public SILModuleTransform {
 
   void run() override {
-    auto &Fn = *getFunction();
-    llvm::outs() << "@" << Fn.getName() << "\n";
-    // Gather up all Values in Fn.
-    std::vector<SILValue> Values;
-    if (!gatherValues(Fn, Values))
-      return;
+    for (auto &Fn: *getModule()) {
+      llvm::outs() << "@" << Fn.getName() << "\n";
+      // Gather up all Values in Fn.
+      std::vector<SILValue> Values;
+      if (!gatherValues(Fn, Values))
+        continue;
 
-    AliasAnalysis *AA = PM->getAnalysis<AliasAnalysis>();
+      AliasAnalysis *AA = PM->getAnalysis<AliasAnalysis>();
 
-    // A cache
-    llvm::DenseMap<uint64_t, AliasAnalysis::AliasResult> Results;
+      // A cache
+      llvm::DenseMap<uint64_t, AliasAnalysis::AliasResult> Results;
 
-    // Emit the N^2 alias evaluation of the values.
-    unsigned PairCount = 0;
-    for (unsigned i1 = 0, e1 = Values.size(); i1 != e1; ++i1) {
-      for (unsigned i2 = 0, e2 = Values.size(); i2 != e2; ++i2) {
-        auto V1 = Values[i1];
-        auto V2 = Values[i2];
+      // Emit the N^2 alias evaluation of the values.
+      unsigned PairCount = 0;
+      for (unsigned i1 = 0, e1 = Values.size(); i1 != e1; ++i1) {
+        for (unsigned i2 = 0, e2 = Values.size(); i2 != e2; ++i2) {
+          auto V1 = Values[i1];
+          auto V2 = Values[i2];
 
-        auto Result =
-            AA->alias(V1, V2, computeTBAAType(V1), computeTBAAType(V2));
+          auto Result =
+              AA->alias(V1, V2, computeTBAAType(V1), computeTBAAType(V2));
 
-        // Results should always be the same. But if they are different print
-        // it out so we find the error. This should make our test results less
-        // verbose.
-        uint64_t Key = uint64_t(i1) | (uint64_t(i2) << 32);
-        uint64_t OpKey = uint64_t(i2) | (uint64_t(i1) << 32);
-        auto R = Results.find(OpKey);
-        if (R != Results.end() && R->second == Result)
-          continue;
+          // Results should always be the same. But if they are different print
+          // it out so we find the error. This should make our test results less
+          // verbose.
+          uint64_t Key = uint64_t(i1) | (uint64_t(i2) << 32);
+          uint64_t OpKey = uint64_t(i2) | (uint64_t(i1) << 32);
+          auto R = Results.find(OpKey);
+          if (R != Results.end() && R->second == Result)
+            continue;
 
-        Results[Key] = Result;
-        llvm::outs() << "PAIR #" << PairCount++ << ".\n" << V1 << V2 << Result
-                     << "\n";
+          Results[Key] = Result;
+          llvm::outs() << "PAIR #" << PairCount++ << ".\n" << V1 << V2 << Result
+                      << "\n";
+        }
       }
+          llvm::outs() << "\n";
     }
-    llvm::outs() << "\n";
   }
 
   StringRef getName() override { return "AA Dumper"; }
 };
 
+/// Dumps the memory behavior of instructions in a function.
+class MemBehaviorDumper : public SILModuleTransform {
+  
+  // To reduce the amount of output, we only dump the memory behavior of
+  // selected types of instructions.
+  static bool shouldTestInstruction(SILInstruction *I) {
+    // Only consider function calls.
+    if (FullApplySite::isa(I))
+      return true;
+    
+    return false;
+  }
+  
+  void run() override {
+    for (auto &Fn: *getModule()) {
+      llvm::outs() << "@" << Fn.getName() << "\n";
+      // Gather up all Values in Fn.
+      std::vector<SILValue> Values;
+      if (!gatherValues(Fn, Values))
+        continue;
+
+      AliasAnalysis *AA = PM->getAnalysis<AliasAnalysis>();
+
+      unsigned PairCount = 0;
+      for (auto &BB : Fn) {
+        for (auto &I : BB) {
+          if (shouldTestInstruction(&I)) {
+
+            // Print the memory behavior in relation to all other values in the
+            // function.
+            for (auto &V : Values) {
+              bool Read = AA->mayReadFromMemory(&I, V);
+              bool Write = AA->mayWriteToMemory(&I, V);
+              bool SideEffects = AA->mayHaveSideEffects(&I, V);
+              llvm::outs() <<
+              "PAIR #" << PairCount++ << ".\n" <<
+              "  " << SILValue(&I) <<
+              "  " << V <<
+              "  r=" << Read << ",w=" << Write << ",se=" << SideEffects << "\n";
+            }
+          }
+        }
+      }
+      llvm::outs() << "\n";
+    }
+  }
+  
+  StringRef getName() override { return "Memory Behavior Dumper"; }
+};
+        
 } // end anonymous namespace
 
 SILTransform *swift::createAADumper() { return new SILAADumper(); }
+
+SILTransform *swift::createMemBehaviorDumper() {
+  return new MemBehaviorDumper();
+}
