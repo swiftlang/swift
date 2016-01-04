fd608f3d85a085a65bec6fb33f78f40b3d27fa1b
practicalswift <practicalswift@users.noreply.github.com>
2016-01-04 10:02:46 +0100

    Fix incorrect file names in file headers.

--- lib/SILOptimizer/IPO/GlobalOpt.cpp
+++ lib/SILOptimizer/IPO/GlobalOpt.cpp
@@ -1,4 +1,4 @@
-//===---------- SILGlobalOpt.cpp - Optimize global initializers -----------===//
+//===--- GlobalOpt.cpp - Optimize global initializers ---------------------===//
 //
 // This source file is part of the Swift.org open source project
 //
