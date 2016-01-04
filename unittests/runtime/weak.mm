022efe94c4aed10515fe561cefb425cb5097f065
practicalswift <practicalswift@users.noreply.github.com>
2016-01-04 09:56:23 +0100

    Reference file names (and not paths) in file headers.

--- unittests/runtime/weak.mm
+++ unittests/runtime/weak.mm
@@ -1,4 +1,4 @@
-//===- swift/unittests/runtime/weak.mm - Weak-pointer tests ---------------===//
+//===- weak.mm - Weak-pointer tests ---------------------------------------===//
 //
 // This source file is part of the Swift.org open source project
 //
