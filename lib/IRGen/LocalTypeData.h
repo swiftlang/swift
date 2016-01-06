0f3179d37932ba3b21dfde66ade6cd5348ce8534
practicalswift <practicalswift@users.noreply.github.com>
2016-01-06 13:30:05 +0100

    Fix formatting for some newly introduced file headers.
    The expected formatting is described in PR #875.

--- lib/IRGen/LocalTypeData.h
+++ lib/IRGen/LocalTypeData.h
@@ -1,4 +1,4 @@
-//===--- LocalTypeData.h - Dominance-scoped type data -------------*- C++ -*-=//
+//===--- LocalTypeData.h - Dominance-scoped type data -----------*- C++ -*-===//
 //
 // This source file is part of the Swift.org open source project
 //
