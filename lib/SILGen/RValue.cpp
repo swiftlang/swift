941e57c0d90744514a6600a6beeab40e276cfd98
Slava Pestov <spestov@apple.com>
2016-01-07 14:32:46 -0800

    SILGen: Revert accidental edit from alloc_global patch
    Today is a really bad day as far as me not breaking the build goes.
    Sorry folks!

--- lib/SILGen/RValue.cpp
+++ lib/SILGen/RValue.cpp
@@ -310,7 +310,7 @@ static void copyOrInitValuesInto(Initialization *init,
   bool implodeTuple = false;
 
   if (auto Address = init->getAddressOrNull()) {
-    if (isa<AllocGlobalInst>(Address) &&
+    if (isa<GlobalAddrInst>(Address) &&
         gen.getTypeLowering(type).getLoweredType().isTrivial(gen.SGM.M)) {
       // Implode tuples in initialization of globals if they are
       // of trivial types.
