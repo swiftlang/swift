7382b469b59f4be7b76db60abbec65642667b65c
practicalswift <practicalswift@users.noreply.github.com>
2016-01-08 22:28:29 +0100

    Update RUN line to reflect current non-crash status.

--- validation-test/IDE/crashers/020-swift-typechecker-validategenericfuncsignature.swift
+++ validation-test/IDE/crashers/020-swift-typechecker-validategenericfuncsignature.swift
@@ -1,2 +1,2 @@
-// RUN: not --crash %target-swift-ide-test -code-completion -code-completion-token=A -source-filename=%s
+// RUN: not %target-swift-ide-test -code-completion -code-completion-token=A -source-filename=%s
 protocol a{struct B<e{let a{protocol a{func a<T:s#^A^#
