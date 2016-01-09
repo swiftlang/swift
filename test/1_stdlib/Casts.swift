swiftlang/swift

commit e09027ca04e63a1ba79b54ea8716d51733170469

Fix the error message when a cast to a protocol type fails.
rdar://22936612

stdlib/public/runtime/Casting.cpp
@@ -934,7 +934,7 @@ static bool _dynamicCastToExistential(OpaqueValue *dest,
     if (!_conformsToProtocols(srcDynamicValue, srcDynamicType,
                               targetType->Protocols,
                               destExistential->getWitnessTables())) {
-      return _fail(src, srcType, targetType, flags);
+      return _fail(srcDynamicValue, srcDynamicType, targetType, flags);
     }
 
     auto object = *(reinterpret_cast<HeapObject**>(srcDynamicValue));
@@ -953,7 +953,7 @@ static bool _dynamicCastToExistential(OpaqueValue *dest,
     if (!_conformsToProtocols(srcDynamicValue, srcDynamicType,
                               targetType->Protocols,
                               destExistential->getWitnessTables()))
-      return _fail(src, srcType, targetType, flags);
+      return _fail(srcDynamicValue, srcDynamicType, targetType, flags);
 
     // Fill in the type and value.
     destExistential->Type = srcDynamicType;
@@ -977,7 +977,7 @@ static bool _dynamicCastToExistential(OpaqueValue *dest,
     if (!_conformsToProtocols(srcDynamicValue, srcDynamicType,
                               targetType->Protocols,
                               &errorWitness))
-      return _fail(src, srcType, targetType, flags);
+      return _fail(srcDynamicValue, srcDynamicType, targetType, flags);
     
     BoxPair destBox = swift_allocError(srcDynamicType, errorWitness,
                                        srcDynamicValue,

test/1_stdlib/CastTraps.swift.gyb
@@ -36,6 +36,10 @@ class ObjCClass2 : NSObject { }
 % objectTypes.append(['NSDateFormatter', 'NSNumberFormatter'])
 // non-Swift Objective-C class
 
+% protocolTypes.append('a.Proto1')
+% protocolTypes.append('a.Proto2')
+protocol Proto1 { }
+protocol Proto2 { }
 % protocolTypes.append('NSURLSessionDelegate')
 // non-Swift Objective-C protocol
 

commit 30224016c503ddae5c2eccd894458029b1b535b1

[SR-392][Runtime] Avoid overrelease when failing cast to protocol
Fixes a regression introduced in e09027ca which causes us to release
the wrong value when a cast fails.

stdlib/public/runtime/Casting.cpp
@@ -469,9 +469,14 @@ static bool _dynamicCastClassToValueViaObjCBridgeable(
 
 /// A convenient method for failing out of a dynamic cast.
 static bool _fail(OpaqueValue *srcValue, const Metadata *srcType,
-                  const Metadata *targetType, DynamicCastFlags flags) {
-  if (flags & DynamicCastFlags::Unconditional)
-    swift_dynamicCastFailure(srcType, targetType);
+                  const Metadata *targetType, DynamicCastFlags flags,
+                  const Metadata *srcDynamicType = nullptr) {
+  if (flags & DynamicCastFlags::Unconditional) {
+    const Metadata *srcTypeToReport =
+        srcDynamicType ? srcDynamicType
+                       : srcType;
+    swift_dynamicCastFailure(srcTypeToReport, targetType);
+  }
   if (flags & DynamicCastFlags::DestroyOnFailure)
     srcType->vw_destroy(srcValue);
   return false;
@@ -986,7 +991,7 @@ static bool _dynamicCastToExistential(OpaqueValue *dest,
     if (!_conformsToProtocols(srcDynamicValue, srcDynamicType,
                               targetType->Protocols,
                               destExistential->getWitnessTables())) {
-      return _fail(srcDynamicValue, srcDynamicType, targetType, flags);
+      return _fail(src, srcType, targetType, flags, srcDynamicType);
     }
 
     auto object = *(reinterpret_cast<HeapObject**>(srcDynamicValue));
@@ -1005,7 +1010,7 @@ static bool _dynamicCastToExistential(OpaqueValue *dest,
     if (!_conformsToProtocols(srcDynamicValue, srcDynamicType,
                               targetType->Protocols,
                               destExistential->getWitnessTables()))
-      return _fail(srcDynamicValue, srcDynamicType, targetType, flags);
+      return _fail(src, srcType, targetType, flags, srcDynamicType);
 
     // Fill in the type and value.
     destExistential->Type = srcDynamicType;
@@ -1029,7 +1034,7 @@ static bool _dynamicCastToExistential(OpaqueValue *dest,
     if (!_conformsToProtocols(srcDynamicValue, srcDynamicType,
                               targetType->Protocols,
                               &errorWitness))
-      return _fail(srcDynamicValue, srcDynamicType, targetType, flags);
+      return _fail(src, srcType, targetType, flags, srcDynamicType);
     
     BoxPair destBox = swift_allocError(srcDynamicType, errorWitness,
                                        srcDynamicValue,

test/1_stdlib/Casts.swift
@@ -54,4 +54,23 @@ CastsTests.test("No leak for failed tuple casts") {
     expectTrue(deinitRan)
 }
 
+protocol P {}
+class ErrClass : ErrorType { }
+
+CastsTests.test("No overrelease of existential boxes in failed casts") {
+    // Test for crash from SR-392
+    // We fail casts of an existential box repeatedly
+    // to ensure it does not get over-released.
+    func bar<T>(t: T) {
+        for i in 0..<10 {
+            if case let a as P = t {
+                _ = a
+            }
+        }
+    }
+    
+    let err: ErrorType = ErrClass()
+    bar(err)
+}
+
 runAllTests()