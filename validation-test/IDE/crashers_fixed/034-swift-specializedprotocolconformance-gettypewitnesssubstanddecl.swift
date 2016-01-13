8bac0868c3cc680dbde9c451c5c9f76d940dd5c4
Chris Lattner <clattner@apple.com>
2016-01-12 16:04:12 -0800

    My changes fixed this test, and swift-ide-test is returning a successful exit code, I should have removed the "not" as well as the --crash.

--- validation-test/IDE/crashers_fixed/034-swift-specializedprotocolconformance-gettypewitnesssubstanddecl.swift
+++ validation-test/IDE/crashers_fixed/034-swift-specializedprotocolconformance-gettypewitnesssubstanddecl.swift
@@ -1,8 +1,4 @@
-// RUN: not %target-swift-ide-test -code-completion -code-completion-token=A -source-filename=%s
-
-// Test is just disabled to unblock the bots. TODO: enable the test and fix it.
-
-// REQUIRES: FIXME
+// RUN: %target-swift-ide-test -code-completion -code-completion-token=A -source-filename=%s
 
 struct A<c{class a<A{enum S:CollectionType
 #^A^#
