2874743528a5de47c065a2d792c8992cc19f096e
Dmitri Gribenko <gribozavr@gmail.com>
2016-01-19 13:37:22 -0700

    Annotate a crash test as crashing in the verifier

--- validation-test/compiler_crashers/28215-swift-normalprotocolconformance-getwitness.swift
+++ validation-test/compiler_crashers/28215-swift-normalprotocolconformance-getwitness.swift
@@ -1,5 +1,6 @@
 // RUN: not --crash %target-swift-frontend %s -parse
 // REQUIRES: asserts
+// REQUIRES: swift_ast_verifier
 
 // Distributed under the terms of the MIT license
 // Test case submitted to project by https://github.com/practicalswift (practicalswift)
