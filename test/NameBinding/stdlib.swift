61d56576da937b1064b61078d2e7bc078a4426fb
Slava Pestov <spestov@apple.com>
2016-01-19 22:50:23 -0800

    Tweak some -parse-stdlib tests
    While making some changes that resulted in materializeForSet getting
    synthesized more often, I noticed that some tests started failing
    because they could not find the Optional type, which appears in the
    type of materializeForSet. Fix these tests by defining an Optional
    and passing in '-module-name Swift' so that we can find it.
    
    This feels a little bit gross, but Dmitri says it is preferrable to
    importing Swift from a test.

--- test/NameBinding/stdlib.swift
+++ test/NameBinding/stdlib.swift
@@ -1,5 +1,9 @@
 // RUN: %target-parse-verify-swift -parse-stdlib -module-name Swift
 
+enum Optional<T> {
+  case Some(T), None
+}
+
 // <rdar://problem/15593704>
 struct X {
   // This is in parse-stdlib mode with no default literal type.
