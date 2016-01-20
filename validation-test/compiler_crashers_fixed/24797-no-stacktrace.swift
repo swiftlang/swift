e9c44f2877c790f88f9cd07719a168008eadd0dd
Joe Pamer <jpamer@apple.com>
2016-01-20 14:04:56 -0800

    Resolve three crashers that were fixed by the last set of changes.

--- validation-test/compiler_crashers_fixed/24797-no-stacktrace.swift
+++ validation-test/compiler_crashers_fixed/24797-no-stacktrace.swift
@@ -1,4 +1,4 @@
-// RUN: not --crash %target-swift-frontend %s -parse
+// RUN: not %target-swift-frontend %s -parse
 
 // Distributed under the terms of the MIT license
 // Test case found by https://github.com/neilpa (neilpa)
