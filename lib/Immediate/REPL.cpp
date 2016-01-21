8013baeda91166f25eb0e577fc69e5f16113fe23
Michael Gottesman <mgottesman@apple.com>
2016-01-20 19:45:36 -0800

    Pass in the actual unique_ptr of the submodule to llvm::Linker::linkModules to deal with an API that was updated upstream.
    What is important to note is that the submodule needs to be forwarded via a move
    constructor to make sure that we do not destroy the value early.

--- lib/Immediate/REPL.cpp
+++ lib/Immediate/REPL.cpp
@@ -871,11 +871,7 @@ private:
     // Make a copy of it to be able to correct produce DumpModule.
     std::unique_ptr<llvm::Module> SaveLineModule(CloneModule(LineModule.get()));
     
-    if (!linkLLVMModules(Module, LineModule.get()
-                         // TODO: reactivate the linker mode if it is
-                         // supported in llvm again. Otherwise remove the
-                         // commented code completely.
-                         /*, llvm::Linker::PreserveSource */)) {
+    if (!linkLLVMModules(&Module, std::move(LineModule))) {
       return false;
     }
 
@@ -885,7 +881,7 @@ private:
 
     stripPreviouslyGenerated(*NewModule);
 
-    if (!linkLLVMModules(&DumpModule, SaveLineModule.get()
+    if (!linkLLVMModules(&DumpModule, std::move(SaveLineModule),
                          // TODO: reactivate the linker mode if it is
                          // supported in llvm again. Otherwise remove the
                          // commented code completely.
