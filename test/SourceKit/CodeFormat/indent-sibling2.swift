afa32c3a141fd4f1561ab13e9fd69f48b23c9bfd
Xi Ge <xi_ge@apple.com>
2016-01-21 13:57:30 -0800

    [SourceKit][CodeFormat] Indent case label items of a case statement as siblings. rdar://24279926

--- test/SourceKit/CodeFormat/indent-sibling2.swift
+++ test/SourceKit/CodeFormat/indent-sibling2.swift
@@ -7,10 +7,29 @@ foo(0,
       baz: 2,
           buzz: 3)
 
+public enum ProtobufJSONToken: Equatable {
+  case COLON
+  case COMMA
+  case BEGIN_OBJECT
+}
+
+public func ==(lhs: ProtobufJSONToken, rhs: ProtobufJSONToken) -> Bool {
+  switch (lhs, rhs) {
+  case (.COLON, .COLON),
+          (.COMMA, .COMMA),
+              (.BEGIN_OBJECT, .BEGIN_OBJECT),
+    return true
+  default:
+    return false
+  }
+}
+
 // RUN: %sourcekitd-test -req=format -line=6 -length=1 %s >%t.response
 // RUN: %sourcekitd-test -req=format -line=7 -length=1 %s >>%t.response
 // RUN: %sourcekitd-test -req=format -line=8 -length=1 %s >>%t.response
 // RUN: %sourcekitd-test -req=format -line=9 -length=1 %s >>%t.response
+// RUN: %sourcekitd-test -req=format -line=19 -length=1 %s >>%t.response
+// RUN: %sourcekitd-test -req=format -line=20 -length=1 %s >>%t.response
 // RUN: FileCheck --strict-whitespace %s <%t.response
 
 //                        "foo(0,"
@@ -21,3 +40,8 @@ foo(0,
 
 //                        "      baz: 2,"
 // CHECK: key.sourcetext: "      buzz: 3)"
+
+//                        "  case (.COLON, .COLON),"
+// CHECK: key.sourcetext: "       (.COMMA, .COMMA),"
+//                        "          (.COMMA, .COMMA),"
+// CHECK: key.sourcetext: "          (.BEGIN_OBJECT, .BEGIN_OBJECT),"
