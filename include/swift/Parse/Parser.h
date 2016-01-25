14df00766fe140ffe1600bff31172fb02350ab80
Denis Vnukov <dvnukov@apple.com>
2016-01-25 12:31:54 -0800

    Properly handle tokens split in parser inside swift::tokenize(...)
    Swift parser splits tokens in few cases, but it swift::tokenize(...) does not know
    about that. In order to reconstruct token stream as it was seen by the parser,
    we need to collect the tokens it decided to split and use this information
    in swift::tokenize(...).

--- include/swift/Parse/Parser.h
+++ include/swift/Parse/Parser.h
@@ -71,6 +71,7 @@ class Parser {
 
   bool IsInputIncomplete = false;
   SourceLoc DelayedDeclEnd;
+  std::vector<Token> SplitTokens;
 
 public:
   SourceManager &SourceMgr;
@@ -154,6 +155,8 @@ public:
   bool allowTopLevelCode() const {
     return SF.isScriptMode();
   }
+
+  std::vector<Token> &getSplitTokens() { return SplitTokens; }
 
   /// Returns true if the parser reached EOF with incomplete source input, due
   /// for example, a missing right brace.
--- lib/Parse/Parser.cpp
+++ lib/Parse/Parser.cpp
@@ -228,7 +228,8 @@ std::vector<Token> swift::tokenize(const LangOptions &LangOpts,
                                    const SourceManager &SM, unsigned BufferID,
                                    unsigned Offset, unsigned EndOffset,
                                    bool KeepComments,
-                                   bool TokenizeInterpolatedString) {
+                                   bool TokenizeInterpolatedString,
+                                   ArrayRef<Token> SplitTokens) {
   if (Offset == 0 && EndOffset == 0)
     EndOffset = SM.getRangeForBuffer(BufferID).getByteLength();
 
@@ -236,10 +237,34 @@ std::vector<Token> swift::tokenize(const LangOptions &LangOpts,
           KeepComments ? CommentRetentionMode::ReturnAsTokens
                        : CommentRetentionMode::AttachToNextToken,
           Offset, EndOffset);
+
+  auto TokComp = [&] (const Token &A, const Token &B) {
+    return SM.isBeforeInBuffer(A.getLoc(), B.getLoc());
+  };
+
+  std::set<Token, decltype(TokComp)> ResetTokens(TokComp);
+  for (auto C = SplitTokens.begin(), E = SplitTokens.end(); C != E; ++C) {
+    ResetTokens.insert(*C);
+  }
+
   std::vector<Token> Tokens;
   do {
     Tokens.emplace_back();
     L.lex(Tokens.back());
+
+    // If the token has the same location as a reset location,
+    // reset the token stream
+    auto F = ResetTokens.find(Tokens.back());
+    if (F != ResetTokens.end()) {
+      Tokens.back() = *F;
+      assert(Tokens.back().isNot(tok::string_literal));
+
+      auto NewState = L.getStateForBeginningOfTokenLoc(
+                                    F->getLoc().getAdvancedLoc(F->getLength()));
+      L.restoreState(NewState);
+      continue;
+    }
+
     if (Tokens.back().is(tok::string_literal) && TokenizeInterpolatedString) {
       Token StrTok = Tokens.back();
       Tokens.pop_back();
@@ -334,6 +359,9 @@ SourceLoc Parser::consumeStartingCharacterOfCurrentToken() {
   if (Tok.getLength() == 1) {
     return consumeToken();
   }
+
+  SplitTokens.emplace_back();
+  SplitTokens.back().setToken(tok::oper_postfix, Tok.getText().substr(0, 1));
 
   // ... or a multi-character token with the first character being the one that
   // we want to consume as a separate token.
