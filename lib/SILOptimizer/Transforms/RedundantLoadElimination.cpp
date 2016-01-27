swiftlang/swift

commit 925eb2e0d9717bb9a659bb36b8bbe6831c13d428

Correct an inefficiency in initial state of the data flow in RLE

lib/SILOptimizer/Transforms/RedundantLoadElimination.cpp
@@ -324,7 +324,9 @@ public:
     ForwardSetIn.resize(LocationNum, false);
     ForwardSetOut.resize(LocationNum, optimistic);
 
-    ForwardSetMax.resize(LocationNum, true);
+    // If we are running an optimsitic data flow, set forward max to true
+    // initially.
+    ForwardSetMax.resize(LocationNum, optimistic);
 
     BBGenSet.resize(LocationNum, false);
     BBKillSet.resize(LocationNum, false);

commit 5c96bc4945447d39cf2ca2b0f32c36fd5bf04f71

RLE marks the LiveOut of unreachable block as 0. This is done to simplfy the SSAupdate etc, i.e. we do not need to place bogus value in the unreachable blocks in case a SILArgument needs to be constrcuted for this  block's successors.
This relies on simplifycfg or other passes to clean up the CFG before RLE is ran.

isReachable logic is incorrect. This make RLE too conservative in some cases and incorrect in
others .

This fixed ASAN build break caused by commit 925eb2e0d9717bb9a659bb36b8bbe6831c13d428

I see more redundant loads elim'ed, but I do not see a performance difference with this change.

lib/SILOptimizer/Transforms/RedundantLoadElimination.cpp
@@ -155,30 +155,6 @@ static bool isRLEInertInstruction(SILInstruction *Inst) {
   }
 }
 
-/// Returns true if the given basic block is reachable from the entry block.
-///
-/// TODO: this is very inefficient, can we make use of the domtree.
-static bool isReachable(SILBasicBlock *Block) {
-  SmallPtrSet<SILBasicBlock *, 16> Visited;
-  llvm::SmallVector<SILBasicBlock *, 16> Worklist;
-  SILBasicBlock *EntryBB = &*Block->getParent()->begin();
-  Worklist.push_back(EntryBB);
-  Visited.insert(EntryBB);
-
-  while (!Worklist.empty()) {
-    auto *CurBB = Worklist.back();
-    Worklist.pop_back();
-
-    if (CurBB == Block)
-      return true;
-
-    for (auto &Succ : CurBB->getSuccessors())
-      if (!Visited.insert(Succ).second)
-        Worklist.push_back(Succ);
-  }
-  return false;
-}
-
 //===----------------------------------------------------------------------===//
 //                       Basic Block Location State
 //===----------------------------------------------------------------------===//
@@ -1371,12 +1347,19 @@ bool RLEContext::run() {
   bool MultiIteration = Kind == ProcessKind::ProcessMultipleIterations ?
                         true : false;
 
+  // These are a list of basic blocks that we actually processed.
+  // We do not process unreachable block, instead we set their liveouts to nil.
+  llvm::DenseSet<SILBasicBlock *> BBToProcess;
+  for (auto X : PO->getPostOrder()) 
+    BBToProcess.insert(X);
+
   // For all basic blocks in the function, initialize a BB state. Since we
   // know all the locations accessed in this function, we can resize the bit
   // vector to the appropriate size.
   for (auto &B : *Fn) {
     BBToLocState[&B] = BlockState();
-    BBToLocState[&B].init(&B, LocationVault.size(), MultiIteration && isReachable(&B));
+    BBToLocState[&B].init(&B, LocationVault.size(), MultiIteration &&
+                          BBToProcess.find(&B) != BBToProcess.end());
   }
 
   if (MultiIteration)

test/SILOptimizer/redundant_load_elim.sil
@@ -1047,3 +1047,51 @@ bb3:
   %44 = tuple ()
   return %44 : $()
 }
+
+// Make sure the first load in bb1 is not eliminated as we have
+// this unreachable block which will have a liveout of nil.
+// we make this in the context of a loop, because we want to run an
+// optimistic data flow.
+// 
+// CHECK-LABEL: sil @load_to_load_loop_with_unreachable_block
+// CHECK: bb1:
+// CHECK: load
+// CHECK: cond_br
+sil @load_to_load_loop_with_unreachable_block : $@convention(thin) () -> () {
+bb0:
+  %101 = alloc_stack $Int32
+  %102 = alloc_stack $Int32
+  %0 = struct_element_addr %101 : $*Int32, #Int32.value
+  %1 = struct_element_addr %102 : $*Int32, #Int32.value
+  %2 = load %0 : $*Builtin.Int32
+  %99 = load %1 : $*Builtin.Int32
+  %125 = function_ref @use : $@convention(thin) (Builtin.Int32) -> () // user: %23 
+  %126 = apply %125(%2) : $@convention(thin) (Builtin.Int32) -> ()
+  %127 = apply %125(%99) : $@convention(thin) (Builtin.Int32) -> ()
+  br bb1
+
+bb20:
+  %44 = load %0 : $*Builtin.Int32
+  br bb1
+
+bb1:
+  %4 = load %0 : $*Builtin.Int32
+  %5 = integer_literal $Builtin.Int32, 2
+  %1125 = function_ref @use : $@convention(thin) (Builtin.Int32) -> () // user: %23 
+  %1126 = apply %1125(%4) : $@convention(thin) (Builtin.Int32) -> ()
+  store %5 to %0 : $*Builtin.Int32
+  builtin "trunc_Int32_Int1"(%4 : $Builtin.Int32) : $Builtin.Int1
+  %6 = load %0 : $*Builtin.Int32
+  %11125 = function_ref @use : $@convention(thin) (Builtin.Int32) -> () // user: %23 
+  %11126 = apply %11125(%6) : $@convention(thin) (Builtin.Int32) -> ()
+  cond_br undef, bb1, bb2
+
+bb2:
+  %7 = load %0 : $*Builtin.Int32
+  %111125 = function_ref @use : $@convention(thin) (Builtin.Int32) -> () // user: %23 
+  %111126 = apply %111125(%7) : $@convention(thin) (Builtin.Int32) -> ()
+  dealloc_stack %102 : $*Int32
+  dealloc_stack %101 : $*Int32
+  %9999 = tuple()
+  return %9999 : $()
+}