312e51ad8c73c5fc84fffe16aa42c8e2217b43a4
Erik Eckstein <eeckstein@apple.com>
2016-01-28 16:21:03 -0800

    Inliner: introduce a new limit for the number of caller blocks.
    This avoids too much inlining into a caller function (so far we only had limits based on the callee).
    rdar://problem/23228386

--- test/SILOptimizer/specialize_apply_conf.swift
+++ test/SILOptimizer/specialize_apply_conf.swift
@@ -1,4 +1,4 @@
-// RUN: %target-swift-frontend -O -sil-inline-threshold 1 -emit-sil -primary-file %s | FileCheck %s
+// RUN: %target-swift-frontend -O -sil-inline-threshold 0 -emit-sil -primary-file %s | FileCheck %s
 
 // We can't deserialize apply_inst with subst lists. When radar://14443304
 // is fixed then we should convert this test to a SIL test.
@@ -19,7 +19,7 @@ func main_func<T>(In In : T) {
 }
 
 //CHECK: sil hidden @_TF21specialize_apply_conf11interestingFT_T_
-//CHECK: function_ref @_TTSf4d___TTSg5Si___TF21specialize_apply_conf9main_funcurFT2Inx_T_
+//CHECK: function_ref @{{.*}}_TTSg5Si___TF21specialize_apply_conf9main_funcurFT2Inx_T_
 //CHECK-NEXT: apply
 //CHECK: return
 func interesting() {
