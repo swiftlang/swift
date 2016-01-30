d7b05d647bc13b62c2981aa74bb1972145f1b5a3
Dmitri Gribenko <gribozavr@gmail.com>
2016-01-29 19:38:24 -0700

    Really mark the crash test as fixed

--- validation-test/IDE/crashers_fixed/019-swift-vardecl-emitlettovarnoteifsimple.swift
+++ validation-test/IDE/crashers_fixed/019-swift-vardecl-emitlettovarnoteifsimple.swift
@@ -1,2 +1,2 @@
-// RUN: not %target-swift-ide-test -code-completion -code-completion-token=A -source-filename=%s
+// RUN: %target-swift-ide-test -code-completion -code-completion-token=A -source-filename=%s
 class l{func a=let A{class Q{var _=a={#^A^#
