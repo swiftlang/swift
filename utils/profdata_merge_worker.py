f99fd759f4d4d2f08ce10ed249095caf25d23e73
Harlan Haskins <harlan@apple.com>
2016-02-01 15:09:22 -0800

    [coverage] Converted global non-constants to explicitly passed parameters

--- utils/profdata_merge_worker.py
+++ utils/profdata_merge_worker.py
@@ -28,9 +28,6 @@ import argparse
 
 SERVER_ADDRESS = ('localhost', 12400)
 TESTS_FINISHED_SENTINEL = "PROFDATA_MERGE_WORKER_TESTS_FINISHED_SENTINEL"
-FILE_QUEUE = JoinableQueue()
-FILES_MERGED = set()
-CONFIG = None
 
 class Config():
     """A class to store configuration information specified by command-line arguments.
@@ -44,15 +41,16 @@ class Config():
         self.remove_files = not no_remove_files
 
 printlock = Lock()
-def printsync(msg):
-    if CONFIG and CONFIG.debug:
-        with printlock:
-            print >>sys.stderr, msg
+def printsync(msg, config=None):
+    if not config.debug:
+        return
+    with printlock:
+        print >>sys.stderr, msg
 
 class ProfdataTCPHandler(SocketServer.StreamRequestHandler):
     def report(self, msg):
         """Convenience method for reporting status from the workers."""
-        printsync("\n===== ProfdataTCPHandler =====\n%s\n" % msg)
+        printsync("\n===== ProfdataTCPHandler =====\n%s\n" % msg, self.server.config)
 
     def handle(self):
         """Receive a newline-separated list of filenames from a TCP connection
@@ -74,21 +72,30 @@ class ProfdataTCPHandler(SocketServer.StreamRequestHandler):
             # Add all the files to the queue
             for f in data.splitlines():
                 f = f.strip()
-                if f in FILES_MERGED:
+                if f in self.server.files_merged:
                     return
-                FILES_MERGED.add(f)
-                FILE_QUEUE.put(f)
+                self.server.files_merged.add(f)
+                self.server.file_queue.put(f)
+
+class ProfdataServer(SocketServer.TCPServer, object):
+    def __init__(self, config, file_queue):
+        super(ProfdataServer, self).__init__(SERVER_ADDRESS, ProfdataTCPHandler)
+        self.config = config
+        self.file_queue = file_queue
+        self.files_merged = set()
 
 class ProfdataMergerProcess(Process):
-    def __init__(self):
+    def __init__(self, config, file_queue):
         Process.__init__(self)
+        self.config = config
+        self.file_queue = file_queue
         self.filename_buffer = []
-        self.profdata_path = os.path.join(CONFIG.tmp_dir, "%s.profdata" % self.name)
+        self.profdata_path = os.path.join(config.tmp_dir, "%s.profdata" % self.name)
         self.profdata_tmp_path = self.profdata_path + ".copy"
 
     def report(self, msg):
         """Convenience method for reporting status from the workers."""
-        printsync("\n===== %s =====\n%s\n" % (self.name, msg))
+        printsync("\n===== %s =====\n%s\n" % (self.name, msg), self.config)
 
     def merge_file_buffer(self):
         """Merge all files in this worker's buffer and clear them.
@@ -111,7 +118,7 @@ class ProfdataMergerProcess(Process):
         if ret != 0:
             self.report("llvm profdata command failed -- Exited with code %d"
                 % ret)
-        if CONFIG.remove_files:
+        if self.config.remove_files:
             for f in self.filename_buffer:
                 if os.path.exists(f):
                     os.remove(f)
@@ -122,54 +129,56 @@ class ProfdataMergerProcess(Process):
         and execute merges. If it finds None in the queue, then it knows to
         stop waiting for the queue, merge its current buffer, and kill itself"""
         while True:
-            filename = FILE_QUEUE.get()
+            filename = self.file_queue.get()
             self.report("received filename: %s" % filename)
             if filename is None:
                 self.report("received sentinel; merging...")
                 self.merge_file_buffer()
-                FILE_QUEUE.task_done()
+                self.file_queue.task_done()
                 break
             self.filename_buffer.append(filename)
             self.report("Adding %s to filename_buffer." % filename)
             if len(self.filename_buffer) >= 10:
                 self.merge_file_buffer()
-                FILE_QUEUE.task_done()
+                self.file_queue.task_done()
 
-def run_server():
+def run_server(config):
     pid = os.getpid()
-    if os.path.exists(CONFIG.pid_file_path):
-        with open(CONFIG.pid_file_path) as pidfile:
+    if os.path.exists(config.pid_file_path):
+        with open(config.pid_file_path) as pidfile:
             pid = pidfile.read()
             printsync(("existing process found with pid %s." +
                        "Ensure there are no other test runners running," +
                        "and delete the file at %s")
-                       % (pid, CONFIG.pid_file_path))
+                       % (pid, config.pid_file_path), config)
         return
 
-    with open(CONFIG.pid_file_path, "w") as pidfile:
+    with open(config.pid_file_path, "w") as pidfile:
         pidfile.write(str(pid))
 
-    processes = [ProfdataMergerProcess() for _ in range(10)]
+    file_queue = JoinableQueue()
+
+    processes = [ProfdataMergerProcess(config, file_queue) for _ in range(10)]
     for p in processes:
         p.start()
 
-    server = SocketServer.TCPServer(SERVER_ADDRESS, ProfdataTCPHandler)
+    server = ProfdataServer(config, file_queue)
     server.serve_forever()
 
     for p in processes:
         # force each merge worker to gracefully exit
-        FILE_QUEUE.put(None)
+        file_queue.put(None)
 
     for p in processes:
-        printsync("waiting for %s to finish..." % p.name)
+        printsync("waiting for %s to finish..." % p.name, config)
         p.join()
 
     # now that all workers have completed, merge all their files
-    merge_final = ProfdataMergerProcess()
-    merge_final.profdata_path = CONFIG.final_profdata_path
+    merge_final = ProfdataMergerProcess(config, file_queue)
+    merge_final.profdata_path = config.final_profdata_path
     for p in processes:
         if os.path.exists(p.profdata_path):
-            printsync("merging " + p.profdata_path + "...")
+            printsync("merging " + p.profdata_path + "...", config)
             merge_final.filename_buffer.append(p.profdata_path)
     merge_final.merge_file_buffer()
 
@@ -180,19 +189,18 @@ def stop_server(args):
     sock.close()
 
 def start_server(args):
-    global CONFIG
-    CONFIG = Config(args.debug, args.output_dir, args.no_remove)
-    if not CONFIG.debug:
+    config = Config(args.debug, args.output_dir, args.no_remove)
+    if not config.debug:
         pid = os.fork()
         if pid != 0:
             sys.exit(0) # kill the parent process we forked from.
     try:
-        run_server()
+        run_server(config)
     finally:
-        if os.path.exists(CONFIG.pid_file_path):
-            os.remove(CONFIG.pid_file_path)
-        if os.path.exists(CONFIG.tmp_dir):
-            shutil.rmtree(CONFIG.tmp_dir, ignore_errors=True)
+        if os.path.exists(config.pid_file_path):
+            os.remove(config.pid_file_path)
+        if os.path.exists(config.tmp_dir):
+            shutil.rmtree(config.tmp_dir, ignore_errors=True)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
