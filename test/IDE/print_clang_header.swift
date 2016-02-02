e4b7de6dee08b0c1af21b7730c60f1ae3db6a66a
Michael Gottesman <mgottesman@apple.com>
2016-02-01 17:38:08 -0800

    Make two IDE tests only run on macosx. Argyrios is going to fix them after the merge.

--- test/IDE/print_clang_header.swift
+++ test/IDE/print_clang_header.swift
@@ -1,5 +1,6 @@
 // FIXME: rdar://problem/19648117 Needs splitting objc parts out
 // XFAIL: linux
+// REQUIRES: OS=macosx
 
 // RUN: echo '#include "header-to-print.h"' > %t.m
 // RUN: %target-swift-ide-test -source-filename %s -print-header -header-to-print %S/Inputs/print_clang_header/header-to-print.h -print-regular-comments --cc-args %target-cc-options -isysroot %clang-importer-sdk-path -fsyntax-only %t.m -I %S/Inputs/print_clang_header > %t.txt
