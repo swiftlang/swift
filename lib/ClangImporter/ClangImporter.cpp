1396251f3dba450bffe0c0d9197a07b9f5c6151a
Slava Pestov <spestov@apple.com>
2016-02-03 16:41:50 -0800

    ClangImporter: Fix a warning, NFC

--- lib/ClangImporter/ClangImporter.cpp
+++ lib/ClangImporter/ClangImporter.cpp
@@ -2067,7 +2067,7 @@ static bool shouldIgnoreMacro(const clang::IdentifierInfo *identifier,
 
   // Currently we only convert non-function-like macros.
   if (macro->isFunctionLike())
-    return nullptr;
+    return false;
 
   // Consult the blacklist of macros to suppress.
   auto suppressMacro =
