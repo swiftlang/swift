50f0d0a7bd504c85f3a378d6c488540412deda4a
practicalswift <practicalswift@users.noreply.github.com>
2016-02-05 16:29:39 +0100

    [gardening] Fix header consistency in recently introduced files

--- include/swift/Sema/IDETypeChecking.h
+++ include/swift/Sema/IDETypeChecking.h
@@ -1,4 +1,4 @@
-//===-------- IDETypeChecking.h - Type-check entry points -*- C++ -*-------===//
+//===--- IDETypeChecking.h - Type-check entry points ------------*- C++ -*-===//
 //
 // This source file is part of the Swift.org open source project
 //
