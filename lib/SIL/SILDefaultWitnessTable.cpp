5695545c7dcbcf4f2515005da3a0b7b2e4a1771d
practicalswift <practicalswift@users.noreply.github.com>
2016-02-06 10:23:56 +0100

    [gardening] Use standard file header format in recently added file.

--- lib/SIL/SILDefaultWitnessTable.cpp
+++ lib/SIL/SILDefaultWitnessTable.cpp
@@ -1,4 +1,4 @@
-//===--- SILDefaultWitnessTable.cpp --------------------------------------===//
+//===--- SILDefaultWitnessTable.cpp ---------------------------------------===//
 //
 // This source file is part of the Swift.org open source project
 //
