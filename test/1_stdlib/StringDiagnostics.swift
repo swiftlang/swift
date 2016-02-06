56690e82f76b9fbc68b11d9211302e8bbb6a35e3
Nate Cook <nmersethcook@apple.com>
2016-02-05 21:03:40 -0600

    Add tests for string index IndexType conformance.

--- test/1_stdlib/StringDiagnostics.swift
+++ test/1_stdlib/StringDiagnostics.swift
@@ -58,3 +58,21 @@ func testStringDeprecation(hello: String) {
 
 
 }
+
+// Positive and negative tests for String index types
+func acceptsForwardIndex<I: ForwardIndexType>(index: I) {}
+func acceptsBidirectionalIndex<I: BidirectionalIndexType>(index: I) {}
+func acceptsRandomAccessIndex<I: RandomAccessIndexType>(index: I) {}
+
+func testStringIndexTypes(s: String) {
+  acceptsForwardIndex(s.utf8.startIndex)
+  acceptsBidirectionalIndex(s.utf8.startIndex) // expected-error{{argument type 'String.UTF8View.Index' does not conform to expected type 'BidirectionalIndexType'}}
+  acceptsBidirectionalIndex(s.unicodeScalars.startIndex)
+  acceptsRandomAccessIndex(s.unicodeScalars.startIndex) // expected-error{{argument type 'String.UnicodeScalarView.Index' does not conform to expected type 'RandomAccessIndexType'}}
+  acceptsBidirectionalIndex(s.characters.startIndex)
+  acceptsRandomAccessIndex(s.characters.startIndex) // expected-error{{argument type 'String.CharacterView.Index' does not conform to expected type 'RandomAccessIndexType'}}
+  
+  // UTF16View.Index is random-access with Foundation, bidirectional without
+  acceptsRandomAccessIndex(s.utf16.startIndex)
+}
+
