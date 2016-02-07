c937e06b459d9172094d4a22da8eecb7ac323cb8
Doug Gregor <dgregor@apple.com>
2016-02-06 23:52:11 -0800

    [Omit needless words] Don't split setters, "action-like" methods.
    Suggested by Brent Royal-Gordon.

--- test/IDE/Inputs/custom-modules/OmitNeedlessWords.h
+++ test/IDE/Inputs/custom-modules/OmitNeedlessWords.h
@@ -4,4 +4,6 @@
 -(void)jumpToUrl:(nonnull NSURL *)url;
 -(BOOL)objectIsCompatibleWithObject:(nonnull id)other;
 -(void)insetByX:(NSInteger)x y:(NSInteger)y;
+-(void)setIndirectlyToValue:(nonnull id)object;
+-(void)jumpToTop:(nonnull id)sender;
 @end
