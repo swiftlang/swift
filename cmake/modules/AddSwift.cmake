6012485a248e38f271c512160c5b36a44453aa4c
Jordan Rose <jordan_rose@apple.com>
2016-02-09 16:25:04 -0800

    [CMake] Remove old experimental option SWIFT_STDLIB_USE_ASSERT_CONFIG_RELEASE.
    This is now controlled by SWIFT_STDLIB_ASSERTIONS.

--- cmake/modules/AddSwift.cmake
+++ cmake/modules/AddSwift.cmake
@@ -322,10 +322,6 @@ function(_compile_swift_files dependency_target_out_var_name)
     list(APPEND swift_flags "-Xfrontend" "-sil-verify-all")
   endif()
 
-  if(SWIFT_STDLIB_USE_ASSERT_CONFIG_RELEASE)
-    list(APPEND swift_flags "-assert-config" "Release")
-  endif()
-
   if(SWIFT_STDLIB_ENABLE_RESILIENCE AND SWIFTFILE_IS_STDLIB)
     list(APPEND swift_flags "-Xfrontend" "-enable-resilience")
   endif()
