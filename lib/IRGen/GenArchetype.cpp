94a21ced5d96a86ebed8c80dc209936766cdc009
Joe Groff <jgroff@apple.com>
2016-02-10 12:55:21 -0800

    IRGen: Work around crash lowering generic base class-constrained type parameter.
    Type lowering a type with abstract type parameters requires a generic signature, but type lowering does not ever affect class types, so we can be unprincipled and bypass the problem. Work around for rdar://problem/24590570.

--- lib/IRGen/GenArchetype.cpp
+++ lib/IRGen/GenArchetype.cpp
@@ -247,7 +247,11 @@ const TypeInfo *TypeConverter::convertArchetypeType(ArchetypeType *archetype) {
       ClassDecl *superClass = super->getClassOrBoundGenericClass();
       refcount = getReferenceCountingForClass(IGM, superClass);
 
-      auto &superTI = IGM.getTypeInfoForUnlowered(super);
+      // FIXME: Bypass type lowering here, since lowering a generic superclass
+      // requirement triggers a crash when it's dependent on generic params
+      // (rdar://problem/24590570). Nominal class types are currently
+      // unaffected by type lowering (and are likely to remain so).
+      auto &superTI = IGM.getTypeInfoForLowered(super->getCanonicalType());
       reprTy = cast<llvm::PointerType>(superTI.StorageType);
     }
 
