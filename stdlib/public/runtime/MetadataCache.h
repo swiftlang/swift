422764545c720f696bf7061513eac30941d39cf4
Nadav Rotem <nrotem@apple.com>
2016-02-10 00:07:42 -0800

    [Runtime] Improve the metadata hashing function.
    The inputs to the hash function is pointers that have a predictable patten. The
    hashes that we were generating and using for the metadata caches were not very
    good, and as a result we generated very deep search trees. A small change that
    improved the utilization of the 'length' field and another bit-rotate round
    improved the quality of the hash function.
    
    I am going to attach to the github commit two pictures. The first picture is the
    binary with the old hash. The first tree is very deep and sparse. The second
    picture is with the new hash function and the tree is very wide and uniform.  I
    used the benchmark 'TypeFlood' in debug mode to generate huge amounts of
    metadata.

--- stdlib/public/runtime/MetadataCache.h
+++ stdlib/public/runtime/MetadataCache.h
@@ -66,12 +66,13 @@ public:
   }
 
   size_t hash() {
-    size_t H = 0x56ba80d1 ^ length ;
+    size_t H = 0x56ba80d1 * length ;
     for (unsigned i = 0; i < length; i++) {
       H = (H >> 10) | (H << ((sizeof(size_t) * 8) - 10));
       H ^= ((size_t)args[i]) ^ ((size_t)args[i] >> 19);
     }
-    return H * 0x27d4eb2d;
+    H *= 0x27d4eb2d;
+    return (H >> 10) | (H << ((sizeof(size_t) * 8) - 10));
   }
 
   const void * const *begin() const { return args; }
