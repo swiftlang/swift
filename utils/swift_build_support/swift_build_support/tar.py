120ec99be5fd251085dd13a7cbfe8f57e38232f5
Brian Gesiak <modocache@gmail.com>
2016-02-10 22:03:24 -0500

    [build-script] Archive symbols in Python
    Rather than archiving symbols at the very end of the build-script-impl
    shellscript, do so at the end of the Python build-script. A small step towards
    achieving SR-237.

---
+++ utils/swift_build_support/swift_build_support/tar.py
@@ -0,0 +1,31 @@
+# swift_build_support/tar.py - Call tar from Python -*- python -*-
+#
+# This source file is part of the Swift.org open source project
+#
+# Copyright (c) 2014 - 2016 Apple Inc. and the Swift project authors
+# Licensed under Apache License v2.0 with Runtime Library Exception
+#
+# See http://swift.org/LICENSE.txt for license information
+# See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+
+import platform
+import subprocess
+
+
+def tar(source, destination):
+    """
+    Create a gzip archive of the file at 'source' at the given
+    'destination' path.
+    """
+    # We do not use `tarfile` here because:
+    #  - We wish to support LZMA2 compression while also supporting Python 2.7.
+    #  - We wish to explicitly set the owner and group of the archive.
+    args = ['tar', '-c', '-z', '-f', destination]
+
+    if platform.system() != 'Darwin':
+        args += ['--owner=0', '--group=0']
+
+    # Capture stderr output such as 'tar: Failed to open ...'. We'll detect
+    # these cases using the exit code, which should cause 'check_call' to
+    # raise.
+    subprocess.check_call(args + [source], stderr=subprocess.PIPE)
