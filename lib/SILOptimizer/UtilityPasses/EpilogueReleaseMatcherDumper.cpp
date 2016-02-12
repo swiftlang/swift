d1c75976c9bb3a382454b5e1c073b1a799f2b374
Xin Tong <xin_tong@apple.com>
2016-02-12 09:57:14 -0800

    Add a epilogue release dumper. This pass tests/prints what epilogue releases for function arguments are found and dump them.
    This is important as we are trying to improve EpilogueReleaseMatcher.

---
+++ lib/SILOptimizer/UtilityPasses/EpilogueReleaseMatcherDumper.cpp
@@ -0,0 +1,71 @@
+//===--- EpilogueReleaseMatcherDumper.cpp - Find Epilogue Releases  -------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2016 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// This pass finds the epilogue releases matched to each argument of the
+/// function.
+///
+//===----------------------------------------------------------------------===//
+
+#define DEBUG_TYPE "sil-epilogue-release-dumper"
+#include "swift/SILOptimizer/PassManager/Passes.h"
+#include "swift/SIL/SILArgument.h"
+#include "swift/SIL/SILFunction.h"
+#include "swift/SIL/SILValue.h"
+#include "swift/SILOptimizer/Analysis/RCIdentityAnalysis.h"
+#include "swift/SILOptimizer/Analysis/ARCAnalysis.h"
+#include "swift/SILOptimizer/Analysis/Analysis.h"
+#include "swift/SILOptimizer/PassManager/Transforms.h"
+
+using namespace swift;
+
+//===----------------------------------------------------------------------===//
+//                              Top Level Driver
+//===----------------------------------------------------------------------===//
+
+namespace {
+
+/// Find and dump the epilogue release instructions for the arguments.
+class SILEpilogueReleaseMatcherDumper : public SILModuleTransform {
+
+  void run() override {
+    auto *RCIA = getAnalysis<RCIdentityAnalysis>();
+    for (auto &Fn: *getModule()) {
+      // Function is not definition.
+      if (!Fn.isDefinition())
+        continue;
+
+      llvm::outs() << "START: sil @" << Fn.getName() << "\n";
+      ConsumedArgToEpilogueReleaseMatcher ArgToRetRelMap(RCIA->get(&Fn), &Fn); 
+
+      // Iterate over arguments and dump their epilogue releases.
+      for (auto Arg : Fn.getArguments()) {
+        llvm::outs() << *Arg;
+        auto *RI = ArgToRetRelMap.releaseForArgument(Arg);
+        // Can not find an epilogue release instruction for the argument.
+        if (!RI)
+          continue;
+        llvm::outs() << *RI;
+      }
+
+      llvm::outs() << "END: sil @" << Fn.getName() << "\n";
+    }
+  }
+
+  StringRef getName() override { return "Epilogue Release Matcher Dumper"; }
+};
+        
+} // end anonymous namespace
+
+SILTransform *swift::createEpilogueReleaseMatcherDumper() {
+  return new SILEpilogueReleaseMatcherDumper();
+}
