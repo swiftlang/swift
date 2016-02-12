970216434d67e7a3899023c09267cf6b8f278ed5
practicalswift <practicalswift@users.noreply.github.com>
2016-02-12 22:59:57 +0100

    [Python] Fix "too many leading '#' for block comment"
    See discussion with @gribozavr in https://github.com/apple/swift/pull/1287#issuecomment-183413319

--- utils/SwiftIntTypes.py
+++ utils/SwiftIntTypes.py
@@ -1,14 +1,12 @@
 ##===--- SwiftIntTypes.py -----------------------------*- coding: utf-8 -*-===##
-##
-## This source file is part of the Swift.org open source project
-##
-## Copyright (c) 2014 - 2016 Apple Inc. and the Swift project authors
-## Licensed under Apache License v2.0 with Runtime Library Exception
-##
-## See http://swift.org/LICENSE.txt for license information
-## See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
-##
-##===----------------------------------------------------------------------===##
+#
+# This source file is part of the Swift.org open source project
+#
+# Copyright (c) 2014 - 2016 Apple Inc. and the Swift project authors
+# Licensed under Apache License v2.0 with Runtime Library Exception
+#
+# See http://swift.org/LICENSE.txt for license information
+# See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
 
 # Bit counts for all int types
 _all_integer_type_bitwidths = [8, 16, 32, 64]
