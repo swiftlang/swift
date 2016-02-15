2f547e9356958c634bd313e0c46937b968c67a85
practicalswift <practicalswift@users.noreply.github.com>
2016-02-15 16:47:48 +0100

    [gardening] Use consistent header structure in newly introduced files

--- include/swift/Basic/ImmutablePointerSet.h
+++ include/swift/Basic/ImmutablePointerSet.h
@@ -1,4 +1,4 @@
-//===--- ImmutablePointerSet.h --------------------------------------------===//
+//===--- ImmutablePointerSet.h ----------------------------------*- C++ -*-===//
 //
 // This source file is part of the Swift.org open source project
 //
