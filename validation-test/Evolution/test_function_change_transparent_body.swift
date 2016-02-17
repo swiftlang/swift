f53db0ebd3c29b2a06ea70ee5ed8a8ccc7ac1f40
Luke Larson <llarson@apple.com>
2016-02-16 18:24:46 -0800

    [rth] Add resilience test helper utility

--- validation-test/Evolution/test_function_change_transparent_body.swift
+++ validation-test/Evolution/test_function_change_transparent_body.swift
@@ -1,24 +1,4 @@
-// RUN: rm -rf %t && mkdir -p %t/before && mkdir -p %t/after
-
-// RUN: %target-build-swift -whole-module-optimization -parse-as-library -emit-library -Xfrontend -enable-resilience -D BEFORE -c %S/Inputs/function_change_transparent_body.swift -o %t/before/function_change_transparent_body.o
-// RUN: %target-build-swift -whole-module-optimization -parse-as-library -emit-module -Xfrontend -enable-resilience -D BEFORE -c %S/Inputs/function_change_transparent_body.swift -o %t/before/function_change_transparent_body.o
-
-// RUN: %target-build-swift -whole-module-optimization -parse-as-library -emit-library -Xfrontend -enable-resilience -D AFTER -c %S/Inputs/function_change_transparent_body.swift -o %t/after/function_change_transparent_body.o
-// RUN: %target-build-swift -whole-module-optimization -parse-as-library -emit-module -Xfrontend -enable-resilience -D AFTER -c %S/Inputs/function_change_transparent_body.swift -o %t/after/function_change_transparent_body.o
-
-// RUN: %target-build-swift -D BEFORE -c %s -I %t/before -o %t/before/main.o
-// RUN: %target-build-swift -D AFTER -c %s -I %t/after -o %t/after/main.o
-
-// RUN: %target-build-swift %t/before/function_change_transparent_body.o %t/before/main.o -o %t/before_before
-// RUN: %target-build-swift %t/before/function_change_transparent_body.o %t/after/main.o -o %t/before_after
-// RUN: %target-build-swift %t/after/function_change_transparent_body.o %t/before/main.o -o %t/after_before
-// RUN: %target-build-swift %t/after/function_change_transparent_body.o %t/after/main.o -o %t/after_after
-
-// RUN: %target-run %t/before_before
-// RUN: %target-run %t/before_after
-// RUN: %target-run %t/after_before
-// RUN: %target-run %t/after_after
-
+// RUN: %target-resilience-test-wmo
 // REQUIRES: executable_test
 
 // FIXME: shouldn't need -whole-module-optimization here; we need to fix the
