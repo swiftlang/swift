efc6cc3e2a383dd33d37ce1557499700bd5d22f8
Todd Fiala <todd.fiala@gmail.com>
2016-02-22 21:19:32 -0800

    Revert "Most swift targets should depend on SwiftOnoneSupport, except Core and SwiftOnoneSupport"
    This reverts commit a374d1b0fa06d0ccd2d18a904379b8ac73ce8362.

--- cmake/modules/AddSwift.cmake
+++ cmake/modules/AddSwift.cmake
@@ -1344,18 +1344,13 @@ function(add_swift_library name)
   # All Swift code depends on the standard library, except for the standard
   # library itself.
   if(SWIFTLIB_TARGET_LIBRARY AND NOT SWIFTLIB_IS_STDLIB_CORE)
-    list(APPEND SWIFTLIB_SWIFT_MODULE_DEPENDS Core SwiftOnoneSupport)
-  endif()
-
-  if("${name}" STREQUAL "swiftSwiftOnoneSupport")
-    list(REMOVE_ITEM SWIFTLIB_SWIFT_MODULE_DEPENDS
-      SwiftOnoneSupport)
+    list(APPEND SWIFTLIB_SWIFT_MODULE_DEPENDS Core)
   endif()
 
   if((NOT "${SWIFT_BUILD_STDLIB}") AND
      (NOT "${SWIFTLIB_SWIFT_MODULE_DEPENDS}" STREQUAL ""))
     list(REMOVE_ITEM SWIFTLIB_SWIFT_MODULE_DEPENDS
-      Core SwiftOnoneSupport)
+        Core)
   endif()
 
   translate_flags(SWIFTLIB "${SWIFTLIB_options}")
