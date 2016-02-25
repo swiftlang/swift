aec916c212470692dcadb57e03ea4af623dce7af
Michael Gottesman <mgottesman@apple.com>
2016-02-24 17:43:57 -0800

    [dtrace] Add dtrace for support for counting the number of retains/releases applied to specific classes as well as computing total retain/release counts.

--- utils/runtime_statistics.d
+++ utils/runtime_statistics.d
@@ -1,20 +1,54 @@
 
-pid$target:*:swift_retain:entry
+
+typedef struct HeapObject {
+  uintptr_t M; /* Metadata */
+} HeapObject;
+
+typedef struct NominalTypeDescriptor {
+  uintptr_t Zero;
+  uintptr_t Name;
+} NominalTypeDescriptor;
+
+typedef struct ClassMetadata {
+  uintptr_t Kind;
+  struct ClassMetadata *super;
+  void *data[2];
+  uintptr_t data2;
+  uint32_t InstanceAddressPoint;
+  uint32_t InstanceSize;
+  uint16_t InstanceAlignMask;
+  uint16_t Reserved;
+  uint32_t ClassSize;
+  uint32_t ClassAddressPoint;
+  NominalTypeDescriptor *Descriptor;
+} ClassMetadata;
+
+pid$target:libswiftCore:swift_retain:entry / arg0 != 0 /
 {
-        @counts["swift_retain"] = count();
+    this->ptr = (HeapObject *)copyin(arg0, sizeof(HeapObject));
+    this->metadata = (ClassMetadata *)copyin(this->ptr->M, sizeof(ClassMetadata));
+    this->descriptor = (NominalTypeDescriptor *)copyin((uintptr_t)this->metadata->Descriptor, sizeof(NominalTypeDescriptor));
+    this->clsname = copyinstr(this->descriptor->Name);
+    @counts["swift_retain", this->clsname] = count();
+    @counts["swift_retain"] = count();
 }
 
 pid$target:*:swift_release:entry
 {
-        @counts["swift_release"] = count();
+    this->ptr = (HeapObject *)copyin(arg0, sizeof(HeapObject));
+    this->metadata = (ClassMetadata *)copyin(this->ptr->M, sizeof(ClassMetadata));
+    this->descriptor = (NominalTypeDescriptor *)copyin((uintptr_t)this->metadata->Descriptor, sizeof(NominalTypeDescriptor));
+    this->clsname = copyinstr(this->descriptor->Name);
+    @counts["swift_release", this->clsname] = count();
+    @counts["swift_release"] = count();
 }
 
 pid$target:*:objc_retain:entry
 {
-        @counts["objc_retain"] = count();
+    @counts["objc_retain"] = count();
 }
 
 pid$target:*:objc_release:entry
 {
-        @counts["objc_release"] = count();
+    @counts["objc_release"] = count();
 }
