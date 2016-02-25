268c4fe0177d8bb54ac1d45dd0789fa00e4b8a6d
Dmitri Gribenko <gribozavr@gmail.com>
2016-02-24 21:02:59 -0800

    stdlib: migration attributes for UnicodeDecodingResult.isEmptyInput

--- stdlib/public/core/Unicode.swift
+++ stdlib/public/core/Unicode.swift
@@ -30,7 +30,7 @@ public enum UnicodeDecodingResult {
 
   /// Returns `true` if `self` indicates no more unicode scalars are
   /// available.
-  @swift3_migration(renamedToProperty="isEmptyInput")
+  @swift3_migration(message="use 'x == .emptyInput' instead")
   @warn_unused_result
   public func isEmptyInput() -> Bool {
     switch self {
