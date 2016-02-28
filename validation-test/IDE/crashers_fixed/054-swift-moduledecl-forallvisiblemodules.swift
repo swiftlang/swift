615d4cb7567ba353cb179a32a06f54dcf71d847f
Xi Ge <xi_ge@apple.com>
2016-02-27 22:50:47 -0800

    [test] Remove 'not' because code completion prints result.

--- validation-test/IDE/crashers_fixed/054-swift-moduledecl-forallvisiblemodules.swift
+++ validation-test/IDE/crashers_fixed/054-swift-moduledecl-forallvisiblemodules.swift
@@ -1,3 +1,3 @@
-// RUN: not %target-swift-ide-test -code-completion -code-completion-token=A -source-filename=%s
+// RUN: %target-swift-ide-test -code-completion -code-completion-token=A -source-filename=%s
 // REQUIRES: asserts
 extension String->[Void{#^A^#
