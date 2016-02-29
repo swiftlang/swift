71384c7322d5ab26abf411bfbcaffceb3ac8a52c
Max Moiseev <moiseev@apple.com>
2016-02-29 12:13:01 -0800

    Removing unnecessary test command

--- test/SourceKit/DocSupport/doc_clang_module.swift
+++ test/SourceKit/DocSupport/doc_clang_module.swift
@@ -1,4 +1,3 @@
 // RUN: %sourcekitd-test -req=doc-info -module Foo -- -F %S/../Inputs/libIDE-mock-sdk \
 // RUN:         %mcp_opt %clang-importer-sdk | %sed_clean > %t.response
-// RUN: cp -f %t.response %s.response 
 // RUN: diff -u %s.response %t.response
