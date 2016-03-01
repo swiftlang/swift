0b307a313cb9e1d9453af074a1a0717e42d79a18
Patrick Pijnappel <patrickpijnappel@gmail.com>
2016-03-01 20:31:32 +1100

    Make UTF-8 decode benchmark use more realistic cases

--- benchmark/single-source/UTF8Decode.swift
+++ benchmark/single-source/UTF8Decode.swift
@@ -14,13 +14,24 @@ import TestsUtils
 
 @inline(never)
 public func run_UTF8Decode(N: Int) {
-  // Use test data with UTF-8 sequences of all lengths, but mainly ASCII.
-  let string = "Mainly ASCII but also some 2-byte sequences (пример),"
-    + " some 3-bytes sequences (성능 테스트) and occasionally a 🐼 (4 bytes)"
-  let data = Array(string.utf8)
+  // 1-byte sequences
+  // This test case is the longest as it's the most perfomance sensitive.
+  let ascii = "Swift is a multi-paradigm, compiled programming language created for iOS, OS X, watchOS, tvOS and Linux development by Apple Inc. Swift is designed to work with Apple's Cocoa and Cocoa Touch frameworks and the large body of existing Objective-C code written for Apple products. Swift is intended to be more resilient to erroneous code (\"safer\") than Objective-C and also more concise. It is built with the LLVM compiler framework included in Xcode 6 and later and uses the Objective-C runtime, which allows C, Objective-C, C++ and Swift code to run within a single program."
+  // 2-byte sequences
+  let russian = "Ру́сский язы́к один из восточнославянских языков, национальный язык русского народа."
+  // 3-byte sequences
+  let japanese = "日本語（にほんご、にっぽんご）は、主に日本国内や日本人同士の間で使われている言語である。"
+  // 4-byte sequences
+  // Most commonly emoji, which are usually mixed with other text.
+  let emoji = "Panda 🐼, Dog 🐶, Cat 🐱, Mouse 🐭."
+
+  let strings = [ ascii, russian, japanese, emoji ].map { Array($0.utf8) }
+
   for _ in 1...N {
-    var generator = data.generate()
-    var utf8 = UTF8()
-    while !utf8.decode(&generator).isEmptyInput() { }
+    for string in strings {
+      var generator = string.generate()
+      var utf8 = UTF8()
+      while !utf8.decode(&generator).isEmptyInput() { }
+    }
   }
 }
