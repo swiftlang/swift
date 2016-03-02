da6f2f3a9e6c4df315cc092922daef4ee0718d8f
practicalswift <practicalswift@users.noreply.github.com>
2016-03-02 09:50:27 +0100

    [gardening] Fix formatting of recently introduced file headers

--- include/swift/Runtime/RuntimeFnWrappersGen.h
+++ include/swift/Runtime/RuntimeFnWrappersGen.h
@@ -1,4 +1,4 @@
-//===-- RuntimeFnWrapperGen.h - LLVM IR Generation for runtime functions --===//
+//===--- RuntimeFnWrappersGen.h - LLVM IR Generation for runtime functions ===//
 //
 // This source file is part of the Swift.org open source project
 //
