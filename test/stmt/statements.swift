1763a435ac3e054ffad9b2d6a2dcbea9534b2226
Devin Coughlin <dcoughlin@apple.com>
2016-03-03 13:16:24 -0800

    [tests] Update #available test to be independent of the deployment target.
    rdar://problem/24954576

--- test/stmt/statements.swift
+++ test/stmt/statements.swift
@@ -471,8 +471,8 @@ func f21080671() {
 // <rdar://problem/24467411> QoI: Using "&& #available" should fixit to comma
 // https://twitter.com/radexp/status/694561060230184960
 func f(x : Int, y : Int) {
-  if x == y && #available(iOS 9, *) {}  // expected-error {{expected ',' joining parts of a multi-clause condition}} {{13-15=,}}
-  if #available(iOS 9, *) && x == y {}  // expected-error {{expected ',' joining parts of a multi-clause condition}} {{27-29=,}}
+  if x == y && #available(iOS 52, *) {}  // expected-error {{expected ',' joining parts of a multi-clause condition}} {{13-15=,}}
+  if #available(iOS 52, *) && x == y {}  // expected-error {{expected ',' joining parts of a multi-clause condition}} {{28-30=,}}
 
   // https://twitter.com/radexp/status/694790631881883648
   if x == y && let _ = Optional(y) {}  // expected-error {{expected ',' joining parts of a multi-clause condition}} {{13-15=,}}
