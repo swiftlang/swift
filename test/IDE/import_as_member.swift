196c1fbd7aadcb3113d08ddb7169426748f373d0
Michael Ilseman <milseman@apple.com>
2016-03-03 13:07:11 -0800

    [Clang Importer] import as member: test typechecking

--- test/IDE/import_as_member.swift
+++ test/IDE/import_as_member.swift
@@ -1,12 +1,6 @@
 // RUN: %target-swift-ide-test(mock-sdk: -sdk %S/../Inputs/clang-importer-sdk -I %t -I %S/Inputs/custom-modules) -print-module -source-filename %s -module-to-print=ImportAsMember -enable-omit-needless-words -always-argument-labels > %t.printed.txt
 // RUN: FileCheck %s -check-prefix=PRINT -strict-whitespace < %t.printed.txt
 
-import ImportAsMember
-
-let iamStruct = IAMStruct1CreateSimple(1)
-let i = Struct1.globalVar
-let i = IAMStruct1GlobalVar
-
 // PRINT: struct Struct1 {
 // PRINT-NEXT:   var x: Double
 // PRINT-NEXT:   var y: Double
@@ -18,3 +12,16 @@ let i = IAMStruct1GlobalVar
 // PRINT-NEXT:   static var globalVar: Int32
 // PRINT-NEXT: }
 
+// RUN: %target-swift-frontend %s -parse -I %S/Inputs/custom-modules -verify
+
+import ImportAsMember
+
+let iamStructFail = IAMStruct1CreateSimple()
+  // expected-error@-1{{use of unresolved identifier 'IAMStruct1CreateSimple'}}
+let iamStruct = Struct1(x: 1.0, y: 1.0, z: 1.0)
+
+let gVarFail = IAMStruct1GlobalVar
+  // expected-error@-1{{use of unresolved identifier 'IAMStruct1GlobalVar'}}
+let gVar = Struct1.globalVar
+
+print("\(gVar)")
