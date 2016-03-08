6f64082bc59f4403163078984ec267a16aeae12a
Michael Gottesman <mgottesman@apple.com>
2016-03-08 13:36:47 -0800

    Clean up count_if/none in STLExtras.
    Specifically this change:
    
    1. Changed the input collection to be a const T & instead of T.
    2. The return type of count_if is now properly the difference_type of the input range's iterators.

--- include/swift/Basic/Range.h
+++ include/swift/Basic/Range.h
@@ -270,14 +270,17 @@ template <class T> EnumeratorRange<T> enumerate(T Begin, T End) {
 
 /// An adaptor of std::none_of for ranges.
 template <class Range, class Predicate>
-inline
-bool
-none_of(Range R, Predicate P) {
+inline bool none_of(const Range &R, Predicate P) {
   return std::none_of(R.begin(), R.end(), P);
 }
 
+/// An adaptor of std::count_if for ranges.
+///
+/// We use std::result_of on Range::begin since llvm::iterator_range does not
+/// have a public typedef set to what is the underlying iterator.
 template <class Range, class Predicate>
-inline unsigned count_if(Range R, Predicate P) {
+inline auto count_if(const Range &R, Predicate P)
+  -> typename std::iterator_traits<decltype(R.begin())>::difference_type {
   return std::count_if(R.begin(), R.end(), P);
 }
 
