e9796f1c3d6b61fe0c3e27d9124605f219b145c9
Dmitri Gribenko <gribozavr@gmail.com>
2016-03-09 18:40:59 -0800

    Rename UnicodeCodecType to UnicodeCodec in a new test

--- validation-test/stdlib/UnicodeUTFEncoders.swift
+++ validation-test/stdlib/UnicodeUTFEncoders.swift
@@ -18,7 +18,7 @@ import Foundation
 @_silgen_name("random") func random() -> UInt32
 @_silgen_name("srandomdev") func srandomdev()
 
-protocol TestableUnicodeCodec : UnicodeCodecType {
+protocol TestableUnicodeCodec : UnicodeCodec {
   associatedtype CodeUnit : Integer
   static func encodingId() -> NSStringEncoding
   static func name() -> NSString
