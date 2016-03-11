d576abf1a767efe3cd433609da2d18489adeb9f8
Michael Ilseman <milseman@apple.com>
2016-03-11 10:35:35 -0800

    [Clang importer] Allow ObjC categories as decl contexts.
    ObjC containers and categories are valid effective Clang decl
    contexts, but do not have a canonical declaration. Permit them
    alongside translation units.

--- lib/ClangImporter/SwiftLookupTable.h
+++ lib/ClangImporter/SwiftLookupTable.h
@@ -92,8 +92,9 @@ public:
     } else if (auto fDecl = dyn_cast<clang::FunctionDecl>(dc)) {
       DC = fDecl->getCanonicalDecl();
     } else {
-      assert(isa<clang::TranslationUnitDecl>(dc) &&
-             "No other kinds of effective Clang contexts");
+      assert(isa<clang::TranslationUnitDecl>(dc) ||
+             isa<clang::ObjCContainerDecl>(dc) &&
+                 "No other kinds of effective Clang contexts");
       DC = dc;
     }
   }
