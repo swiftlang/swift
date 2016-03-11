4ad5ed4ada67bf9bbef37339f28d18dacf01312b
practicalswift <practicalswift@users.noreply.github.com>
2016-03-11 22:25:20 +0100

    [crashers] License: Apache License v2.0 with Runtime Library Exception

--- validation-test/compiler_crashers/08008-swift-typechecker-typecheckexpression.swift
+++ validation-test/compiler_crashers/08008-swift-typechecker-typecheckexpression.swift
@@ -1,9 +1,10 @@
+// This source file is part of the Swift.org open source project
+// Copyright (c) 2014 - 2016 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+
 // RUN: not --crash %target-swift-frontend %s -parse
-
-// Distributed under the terms of the MIT license
-// Test case submitted to project by https://github.com/practicalswift (practicalswift)
-// Test case found by fuzzing
-
 // ASAN Output: stack-overflow on address 0x7fffe2a98fd8 (pc 0x000001e12adb bp 0x7fffe2a992d0 sp 0x7fffe2a98f60 T0)
-
 class A:A.b{let b=Void{
