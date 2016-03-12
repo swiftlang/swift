9633cf0fe7aa49e1b5ace7af576706e3ff456f3d
practicalswift <practicalswift@users.noreply.github.com>
2016-03-12 10:03:00 +0100

    [crashers] Remove 91 fixed crashers that lack explicit Apache license text
    Prior to this commit:
    
    ```
    $ git grep -E '[^a-zA-Z]MIT[^a-zA-Z]' | grep /compiler_crashers_fixed/ | wc -l
    76
    (+ 15 without the "MIT" string)
    $ ls validation-test/compiler_crashers_fixed/*.swift | wc -l
    5083
    ```
    
    After this commit:
    
    ```
    $ git grep -E '[^a-zA-Z]MIT[^a-zA-Z]' | grep /compiler_crashers_fixed/ | wc -l
    0
    $ ls validation-test/compiler_crashers_fixed/*.swift | wc -l
    4992
    ```

--- validation-test/compiler_crashers_fixed/00053-std-function-func-swift-type-subst.swift
+++
@@ -1,7 +0,0 @@
-// RUN: %target-swift-frontend %s -parse -verify
-
-// Distributed under the terms of the MIT license
-// Test case submitted to project by https://github.com/julasamer (julasamer)
-
-struct c<d, e: b where d.c == e> { // expected-error {{use of undeclared type 'b'}} expected-error {{'c' is not a member type of 'd'}}
-}
