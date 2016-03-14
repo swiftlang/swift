8bc375b04663d714d3908dae1adf7388a5b167b5
practicalswift <practicalswift@users.noreply.github.com>
2016-03-14 13:43:28 +0100

    [crashers] Use standard licensing preamble

--- validation-test/compiler_crashers_fixed/25612-swift-patternbindingdecl-create.swift
+++ validation-test/compiler_crashers_fixed/25612-swift-patternbindingdecl-create.swift
@@ -1,9 +1,5 @@
 // This source file is part of the Swift.org open source project
-// Copyright (c) 2014 - 2016 Apple Inc. and the Swift project authors
-// Licensed under Apache License v2.0 with Runtime Library Exception
-//
 // See http://swift.org/LICENSE.txt for license information
-// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
 
 // RUN: not %target-swift-frontend %s -parse
 func C{func a{func f{func f:a}}func f:Boolean,class a
