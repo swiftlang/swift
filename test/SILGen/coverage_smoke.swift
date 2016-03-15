dc1cacc1d76c068aa39f6b6b08d2a0c606164c8a
Vedant Kumar <vsk@apple.com>
2016-03-15 13:44:47 -0700

    Use inferSwiftBinary to grab llvm-{cov,profdata}

--- test/SILGen/coverage_smoke.swift
+++ test/SILGen/coverage_smoke.swift
@@ -1,9 +1,9 @@
 // RUN: rm -rf %t && mkdir %t
 // RUN: %target-build-swift %s -profile-generate -profile-coverage-mapping -o %t/main
 // RUN: env LLVM_PROFILE_FILE=%t/default.profraw %target-run %t/main
-// RUN: %target-llvm-profdata merge %t/default.profraw -o %t/default.profdata
-// RUN: %target-llvm-profdata show %t/default.profdata -function=main | FileCheck %s --check-prefix=CHECK-PROF
-// RUN: %target-llvm-cov show %t/main -instr-profile=%t/default.profdata | FileCheck %s --check-prefix=CHECK-COV
+// RUN: %llvm-profdata merge %t/default.profraw -o %t/default.profdata
+// RUN: %llvm-profdata show %t/default.profdata -function=main | FileCheck %s --check-prefix=CHECK-PROF
+// RUN: %llvm-cov show %t/main -instr-profile=%t/default.profdata | FileCheck %s --check-prefix=CHECK-COV
 // RUN: rm -rf %t
 // REQUIRES: profile_runtime
 
