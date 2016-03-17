9b0727f6aa51e917d00d8e8b74671d61fee72103
Doug Gregor <dgregor@apple.com>
2016-03-17 16:25:26 -0700

    Use API Notes to map APPRefrigerator bridge to Refrigerator.
    This isolates us from %target-clang's not having picked up my
    Clang-side fix.

--- test/Inputs/ObjCBridging/Appliances.h
+++ test/Inputs/ObjCBridging/Appliances.h
@@ -4,7 +4,6 @@
 #import <Foundation/Foundation.h>
 #endif
 
-__attribute__((swift_bridge("Refrigerator")))
 @interface APPRefrigerator : NSObject <NSCopying>
 -(nonnull instancetype)initWithTemperature:(double)temperature __attribute__((objc_designated_initializer));
 @property (nonatomic) double temperature;
