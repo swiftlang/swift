a942bb76d00a92d6b91de83482ed2f8932b68647
practicalswift <practicalswift@users.noreply.github.com>
2016-03-19 00:37:37 +0100

    [gardening] Fix formatting of recently introduced headers.

--- benchmark/single-source/unit-tests/ObjectiveCBridgingStubs.swift
+++ benchmark/single-source/unit-tests/ObjectiveCBridgingStubs.swift
@@ -1,4 +1,4 @@
-//===---------- ObjectiveCBridgingStubs.swift -----------------------------===//
+//===--- ObjectiveCBridgingStubs.swift ------------------------------------===//
 //
 // This source file is part of the Swift.org open source project
 //
--- lib/SILOptimizer/IPO/EagerSpecializer.cpp
+++ lib/SILOptimizer/IPO/EagerSpecializer.cpp
@@ -1,4 +1,4 @@
-//===------- EagerSpecializer.cpp - Performs Eager Specialization ---------===//
+//===--- EagerSpecializer.cpp - Performs Eager Specialization -------------===//
 //
 // This source file is part of the Swift.org open source project
 //
