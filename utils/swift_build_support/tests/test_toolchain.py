af6f844ec968b9a9e8ac24a015c0ba4d5d1d46fd
Harlan Haskins <harlan@apple.com>
2016-03-18 16:38:53 -0700

    [build_support] Added generic 'toolchain' lookup

--- utils/swift_build_support/tests/test_toolchain.py
+++ utils/swift_build_support/tests/test_toolchain.py
@@ -1,4 +1,5 @@
-# test_clang.py - Unit tests for swift_build_support.clang -*- python -*-
+# -*- python -*-
+# test_toolchain.py - Unit tests for swift_build_support.toolchain
 #
 # This source file is part of the Swift.org open source project
 #
@@ -11,7 +12,7 @@
 import os
 import unittest
 
-from swift_build_support.clang import host_clang
+from swift_build_support.toolchain import host_clang, host_toolchain
 
 
 class HostClangTestCase(unittest.TestCase):
@@ -25,6 +26,18 @@ class HostClangTestCase(unittest.TestCase):
         # number.
         self.assertTrue(os.path.split(clang.cc)[-1].startswith('clang'))
         self.assertTrue(os.path.split(clang.cxx)[-1].startswith('clang++'))
+
+    def test_found_executables_match(self):
+        # Test that the raw incovation of _first_common_executables
+        # either returns None or matching paths.
+        suffixes = ['', '-3.8', '-3.7', '-3.6']
+        toolchain = host_toolchain(suffixes=suffixes)
+        self.assertTrue(len(toolchain.tools) == 2)
+
+        exec_names = {'foo': 'a-tool-that-does-not-exist'}
+        toolchain = host_toolchain(tools=exec_names,
+                                   suffixes=suffixes)
+        self.assertIsNone(toolchain)
 
 
 if __name__ == '__main__':
