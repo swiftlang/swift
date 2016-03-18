8cb669dd566b9660e57c4a1a1fa1cd91e663f386
Michael Ilseman <milseman@apple.com>
2016-03-18 16:53:00 -0700

    Disable crasher test that ASAN somehow fixes.
    The disabled crasher test does not crash with ASAN enabled, which is
    particularly odd. Disable the test to unblock builders and testers
    while diagnosing.

--- validation-test/IDE/crashers/063-swift-generictypeparamtype-get.swift
+++ validation-test/IDE/crashers/063-swift-generictypeparamtype-get.swift
@@ -1,5 +1,6 @@
 // RUN: not --crash %target-swift-ide-test -code-completion -code-completion-token=A -source-filename=%s
 // REQUIRES: asserts
+// REQUIRES: disabled
 {protocol A{func b
 }enum B:A{let s=b
 let A{#^A^#
