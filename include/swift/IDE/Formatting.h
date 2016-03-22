ac87f428eac52817dc9e6427c5bced5323a64385
practicalswift <practicalswift@users.noreply.github.com>
2016-03-22 21:36:09 +0100

    [gardening] Fix formatting of recently introduced headers.

--- include/swift/IDE/Formatting.h
+++ include/swift/IDE/Formatting.h
@@ -1,4 +1,4 @@
-//===--- Formatting.h --------------------------------------------------===//
+//===--- Formatting.h -------------------------------------------*- C++ -*-===//
 //
 // This source file is part of the Swift.org open source project
 //
