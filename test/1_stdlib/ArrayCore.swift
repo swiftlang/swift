a46597fa77f23684c6a9d037c47a9ce4c45fdf3d
Dmitri Gribenko <gribozavr@gmail.com>
2016-03-21 17:55:18 -0700

    ArrayCore test: also import StdlibUnittest dependencies
    This is required to run the test in optimized mode.

--- test/1_stdlib/ArrayCore.swift
+++ test/1_stdlib/ArrayCore.swift
@@ -15,6 +15,14 @@
 import StdlibUnittest
 import Swift
 
+// Also import modules which are used by StdlibUnittest internally. This
+// workaround is needed to link all required libraries in case we compile
+// StdlibUnittest with -sil-serialize-all.
+import SwiftPrivate
+#if _runtime(_ObjC)
+import ObjectiveC
+#endif
+
 //===--- class Tracked ----------------------------------------------------===//
 // Instead of testing with Int elements, we use this wrapper class
 // that can help us track allocations and find issues with object
