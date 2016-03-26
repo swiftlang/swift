4a34aa78951a0781887a1e46a0a3d06d175ca1c5
Slava Pestov <spestov@apple.com>
2016-03-25 23:33:22 -0700

    Re-apply "stdlib: Add minimum set of @_versioned attributes to build without -sil-serialize-all"
    This reverts commit 22864721ca7cccede9229a7dbe164bcd8b808b37.

--- stdlib/public/core/ManagedBuffer.swift
+++ stdlib/public/core/ManagedBuffer.swift
@@ -220,6 +220,7 @@ public struct ManagedBufferPointer<Value, Element> : Equatable {
   /// _debugPreconditions in _checkValidBufferClass for any array. Since we know
   /// for the _ContiguousArrayBuffer that this check must always succeed we omit
   /// it in this specialized constructor.
+  @_versioned
   internal init(_uncheckedUnsafeBufferObject buffer: AnyObject) {
     ManagedBufferPointer._sanityCheckValidBufferClass(buffer.dynamicType)
     self._nativeBuffer = Builtin.castToNativeObject(buffer)
@@ -327,6 +328,7 @@ public struct ManagedBufferPointer<Value, Element> : Equatable {
 
   /// Internal version for use by _ContiguousArrayBuffer.init where we know that
   /// we have a valid buffer class and that the capacity is >= 0.
+  @_versioned
   internal init(
     _uncheckedBufferClass: AnyClass,
     minimumCapacity: Int
