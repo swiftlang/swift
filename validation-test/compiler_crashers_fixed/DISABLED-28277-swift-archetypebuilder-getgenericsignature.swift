59f96454c95521f423d0e3142dec09a3b606b377
Ted Kremenek <kremenek@apple.com>
2016-03-25 18:48:20 -0700

    Note that test is disabled due to ASan failure, tracked by SR-1070.

--- validation-test/compiler_crashers_fixed/DISABLED-28277-swift-archetypebuilder-getgenericsignature.swift
+++ validation-test/compiler_crashers_fixed/DISABLED-28277-swift-archetypebuilder-getgenericsignature.swift
@@ -1,3 +1,5 @@
+// DISABLED because of Address Sanitizer failure: https://bugs.swift.org/browse/SR-1070
+
 // This source file is part of the Swift.org open source project
 // Copyright (c) 2014 - 2016 Apple Inc. and the Swift project authors
 // Licensed under Apache License v2.0 with Runtime Library Exception
