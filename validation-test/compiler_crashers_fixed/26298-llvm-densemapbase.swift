3bf92167649ef24aeb7eecf594aaaf2bfb472e72
practicalswift <practicalswift@users.noreply.github.com>
2016-03-31 20:59:48 +0200

    [crashers] Add license headers.

--- validation-test/compiler_crashers_fixed/26298-llvm-densemapbase.swift
+++ validation-test/compiler_crashers_fixed/26298-llvm-densemapbase.swift
@@ -1,5 +1,11 @@
+// This source file is part of the Swift.org open source project
+// Copyright (c) 2014 - 2016 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+
 // RUN: %target-swift-frontend %s -emit-silgen
-// Distributed under the terms of the MIT license
 // Test case submitted to project by https://github.com/airspeedswift (airspeedswift)
 
 ["1"].map { String($0) }
