6db942d0d1d72b424b92281a36b27c268dcbf06b
Chris Willmore <cwillmore@apple.com>
2016-04-01 13:23:58 -0700

    Add test for calling func of type '(Any) -> ()' with ().
    Tests SR-1028. This issue was already resolved by SR-590 but there was
    no test addressing this particular SR.

--- test/Constraints/function.swift
+++ test/Constraints/function.swift
@@ -76,6 +76,7 @@ print(r22451001(5))  // expected-error {{argument passed to call that takes no a
 
 
 // SR-590 Passing two parameters to a function that takes one argument of type Any crashes the compiler
+// SR-1028: Segmentation Fault: 11 when superclass init takes parameter of type 'Any'
 func sr590(x: Any) {}
 sr590(3,4) // expected-error {{extra argument in call}}
-
+sr590() // expected-error {{missing argument for parameter #1 in call}}
