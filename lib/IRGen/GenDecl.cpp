3e12eccb565314bbf5793490a095b95aeaf379a2
Slava Pestov <spestov@apple.com>
2016-04-03 21:14:21 -0700

    SIL: Fix miscompiles with @effects(readonly) functions that have indirect results
    When resilience is enabled, some functions in the standard library that
    are marked @effects(readonly) now have indirect results.
    
    The SILCombiner pass doesn't handle @effects(readonly) with @out results
    correctly, so just disable the optimizations temporarily to avoid a
    mis-compile when resilience is enabled.
    
    The LLVM readonly attribute can never be applied to such functions, so
    don't set it either.
    
    Should not have an effect when resilience is disabled.

--- lib/IRGen/GenDecl.cpp
+++ lib/IRGen/GenDecl.cpp
@@ -1652,7 +1652,7 @@ static bool isReadOnlyFunction(SILFunction *f) {
   // Check if the function has any 'owned' parameters. Owned parameters may
   // call the destructor of the object which could violate the readonly-ness
   // of the function.
-  if (f->hasOwnedParameters())
+  if (f->hasOwnedParameters() || f->hasIndirectResults())
     return false;
 
   auto Eff = f->getEffectsKind();
