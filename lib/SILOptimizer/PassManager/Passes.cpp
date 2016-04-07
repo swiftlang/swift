f73bc9da7d10754b2974a3504e5cdc16b87ceb40
Roman Levenstein <rlevenstein@apple.com>
2016-04-07 10:08:32 -0700

    The initial version of the NonAtomicRC optimization pass.

--- lib/SILOptimizer/PassManager/Passes.cpp
+++ lib/SILOptimizer/PassManager/Passes.cpp
@@ -297,6 +297,10 @@ void swift::runSILOptimizationPasses(SILModule &Module) {
   // Do the first stack promotion on high-level SIL.
   PM.addStackPromotion();
 
+  // Replace atomic RC instructions by non-atomic RC
+  // whenever possible.
+  PM.addNonAtomicRC();
+
   PM.runOneIteration();
   PM.resetAndRemoveTransformations();
 
@@ -329,6 +333,12 @@ void swift::runSILOptimizationPasses(SILModule &Module) {
 
   // Do the second stack promotion on low-level SIL.
   PM.addStackPromotion();
+
+  // Replace atomic RC instructions by non-atomic RC
+  // whenever possible.
+  PM.addNonAtomicRC();
+
+
 
   // Speculate virtual call targets.
   PM.addSpeculativeDevirtualization();
