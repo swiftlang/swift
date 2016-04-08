563057ca980fb9a8fce899184761a812164dc9d2
John McCall <rjmccall@apple.com>
2016-04-08 09:21:00 -0700

    When performing member lookup into a type during type-checking, immediately discard as non-viable any declarations that cannot be called due to argument-label mismatch.
    This heuristic already existed, but it was badly out-of-date vs.
    the current language rules on argument-passing.  Change it to use
    the standard argument matching algorithm.
    
    This greatly reduces the number of overloads we consider for certain
    kinds of expression, most importantly explicit initialization syntax
    ('T(x)').  Ordinary type-matching will quickly reject such calls,
    but backtracking will discard this rejection.  Thus this heuristic
    can greatly decrease the total work done by the type-checker when
    something else in the system is causing a combinatorial explosion.
    
    The diagnostic changes in the test-suite seem acceptable to me.
    
    Shout-out to Doug for pointing out multiple places where I didn't
    need to reinvent the wheel.

--- lib/Sema/CSGen.cpp
+++ lib/Sema/CSGen.cpp
@@ -2910,7 +2910,9 @@ namespace {
     ArgumentLabelWalker(ConstraintSystem &cs, Expr *expr) 
       : CS(cs), ParentMap(expr->getParentMap()) { }
 
-    void associateArgumentLabels(Expr *arg, ArrayRef<Identifier> labels,
+    using State = ConstraintSystem::ArgumentLabelState;
+
+    void associateArgumentLabels(Expr *arg, State labels,
                                  bool labelsArePermanent) {
       // Our parent must be a call.
       auto call = dyn_cast_or_null<CallExpr>(ParentMap[arg]);
@@ -2941,21 +2943,28 @@ namespace {
 
       // Record the labels.
       if (!labelsArePermanent)
-        labels = CS.allocateCopy(labels);
+        labels.Labels = CS.allocateCopy(labels.Labels);
       CS.ArgumentLabels[CS.getConstraintLocator(fn)] = labels;
     }
 
     std::pair<bool, Expr *> walkToExprPre(Expr *expr) override {
       if (auto tuple = dyn_cast<TupleExpr>(expr)) {
         if (tuple->hasElementNames())
-          associateArgumentLabels(expr, tuple->getElementNames(), true);
+          associateArgumentLabels(expr,
+                                  { tuple->getElementNames(),
+                                    tuple->hasTrailingClosure() },
+                                  /*labelsArePermanent*/ true);
         else {
           llvm::SmallVector<Identifier, 4> names(tuple->getNumElements(),
                                                  Identifier()); 
-          associateArgumentLabels(expr, names, false);
+          associateArgumentLabels(expr, { names, tuple->hasTrailingClosure() },
+                                  /*labelsArePermanent*/ false);
         }
       } else if (auto paren = dyn_cast<ParenExpr>(expr)) {
-        associateArgumentLabels(paren, { Identifier() }, false);
+        associateArgumentLabels(paren,
+                                { { Identifier() },
+                                  paren->hasTrailingClosure() },
+                                /*labelsArePermanent*/ false);
       }
 
       return { true, expr };
