1c3501ffb61920f721eeeebfe3ad3f2f4ca13e30
Dave Abrahams <dabrahams@apple.com>
2016-04-07 22:22:21 -0700

    [emacs support] Add swift-hide-doc-comment-detail
    When explanatory text in doc comments grows large, it can be hard to
    take in an API at a glance.  This command collapses everything but the
    summary line(s) of all the documentation comments in the file.

--- utils/swift-mode.el
+++ utils/swift-mode.el
@@ -149,6 +149,32 @@
   (set (make-local-variable 'font-lock-defaults) 
        '(swift-font-lock-keywords) ))
 
+(defvar swift-doc-comment-detail-re
+  (let ((just-space "[ \t\n]*")
+        (not-just-space ".*?[^ \t\n].*")
+        (eol "\\(?:$\\)")
+        (continue "\n\\1"))
+    
+    (concat "^\\([ \t]*///\\)" not-just-space eol
+            "\\(?:" continue not-just-space eol "\\)*"
+            "\\(" continue just-space eol
+            "\\(?:" continue ".*" eol "\\)*"
+            "\\)"))
+  "regexp that finds the non-summary part of a swift doc comment as subexpression 2")
+
+(defun swift-hide-doc-comment-detail ()
+  "Hide everything but the summary part of doc comments.
+
+Use `M-x hs-show-all' to show them again."
+    (interactive)
+  (hs-minor-mode)
+  (save-excursion
+    (save-match-data
+      (goto-char (point-min))
+      (while (search-forward-regexp swift-doc-comment-detail-re (point-max) :noerror)
+        (hs-hide-comment-region (match-beginning 2) (match-end 2))
+        (goto-char (match-end 2))))))
+
 (defun swift-indent-line ()
   (interactive)
   (let (indent-level target-column)
