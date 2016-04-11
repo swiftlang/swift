1fdd1535d198f9d40222d3dbce146ca77e0e027d
Michael Ilseman <milseman@apple.com>
2016-04-11 15:29:23 -0700

    [Import as member] Simplified SILGen test for global assert

---
+++ test/SILGen/import_as_member_cf.swift
@@ -0,0 +1,14 @@
+// RUN: %target-swift-frontend -emit-silgen -I %S/../IDE/Inputs/custom-modules %s 2>&1 | FileCheck --check-prefix=SIL %s
+// REQUIRES: objc_interop
+
+// XFAIL:
+
+import ImportAsMember.C
+
+// SIL-LABEL: sil {{.*}}readSemiModularPowerSupply{{.*}}
+public func readSemiModularPowerSupply() -> CCPowerSupply {
+  // TODO: actual body
+  // FIXME: this asserts
+  return CCPowerSupply.semiModular
+}
+
