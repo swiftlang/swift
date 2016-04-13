7feb60da04ad394bdc745cc930b680de3f7f4bb8
Joe Groff <jgroff@apple.com>
2016-04-12 20:54:40 -0700

    SILGen: Handle C functions imported as labeled unit constructors.

--- test/IDE/import_as_member_cf.swift
+++ test/IDE/import_as_member_cf.swift
@@ -7,6 +7,7 @@
 // PRINTC:      extension CCPowerSupply {
 // PRINTC-NEXT:   /*not inherited*/ init(watts watts: Double)
 // PRINTC-NEXT:   class let semiModular: CCPowerSupply!
+// PRINTC-NEXT:   /*not inherited*/ init(dangerous dangerous: ())
 // PRINTC-NEXT:   class let defaultPower: Double
 // PRINTC-NEXT:   class let AC: CCPowerSupply
 // PRINTC-NEXT:   class let DC: CCPowerSupply?
