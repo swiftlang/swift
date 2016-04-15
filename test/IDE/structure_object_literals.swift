00274b958343cc122b1107f1213fb011da61a11b
Ted Kremenek <kremenek@apple.com>
2016-04-15 13:57:25 -0700

    Update test to look for 'colorLiteral' instead of 'Color'.

--- test/IDE/structure_object_literals.swift
+++ test/IDE/structure_object_literals.swift
@@ -4,7 +4,7 @@ struct S: _ColorLiteralConvertible {
   init(red: Float, green: Float, blue: Float, alpha: Float) {}
 }
 
-// CHECK: <gvar>let <name>y</name>: S = <object-literal-expression>[#<name>Color</name>(<param><name>red</name>: 1</param>, <param><name>green</name>: 0</param>, <param><name>blue</name>: 0</param>, <param><name>alpha</name>: 1</param>)#]</object-literal-expression></gvar>
+// CHECK: <gvar>let <name>y</name>: S = <object-literal-expression>[#<name>colorLiteral</name>(<param><name>red</name>: 1</param>, <param><name>green</name>: 0</param>, <param><name>blue</name>: 0</param>, <param><name>alpha</name>: 1</param>)#]</object-literal-expression></gvar>
 let y: S = [#colorLiteral(red: 1, green: 0, blue: 0, alpha: 1)#]
 
 struct I: _ImageLiteralConvertible {
