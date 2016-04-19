2520703f58e6df20418582ebf87d14a0e61c3a6e
Arnold Schwaighofer <aschwaighofer@apple.com>
2016-04-19 07:55:30 -0700

    Add String.hasPrefix/hasSuffix benchmark

--- benchmark/single-source/StringTests.swift
+++ benchmark/single-source/StringTests.swift
@@ -9,10 +9,59 @@
 // See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
 //
 //===----------------------------------------------------------------------===//
+import TestsUtils
 
 public func run_StringWithCString(_ N: Int) {
   let str = String(repeating: "x" as UnicodeScalar, count: 100 * (1 << 16))
   for _ in 0 ..< N {
     str.withCString { _ in }
+  }
+}
+
+public func run_StringHasPrefix(_ N: Int) {
+  let prefix = "prefix"
+  let testString = "prefixedString"
+  for _ in 0 ..< N {
+    for _ in 0 ..< 100_000 {
+      if !testString.hasPrefix(prefix) {
+        CheckResults(false, "prefix check failed")
+      }
+    }
+  }
+}
+
+public func run_StringHasSuffix(_ N: Int) {
+  let suffix = "Suffixed"
+  let testString = "StringSuffixed"
+  for _ in 0 ..< N {
+    for _ in 0 ..< 100_000 {
+      if !testString.hasSuffix(suffix) {
+        CheckResults(false, "suffix check failed")
+      }
+    }
+  }
+}
+
+public func run_StringHasPrefixUnicode(_ N: Int) {
+  let prefix = "❄️prefix"
+  let testString = "❄️prefixedString"
+  for _ in 0 ..< N {
+    for _ in 0 ..< 100_000 {
+      if !testString.hasPrefix(prefix) {
+        CheckResults(false, "prefix check failed")
+      }
+    }
+  }
+}
+
+public func run_StringHasSuffixUnicode(_ N: Int) {
+  let suffix = "❄️Suffixed"
+  let testString = "String❄️Suffixed"
+  for _ in 0 ..< N {
+    for _ in 0 ..< 100_000 {
+      if !testString.hasSuffix(suffix) {
+        CheckResults(false, "suffix check failed")
+      }
+    }
   }
 }
