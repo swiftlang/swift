f5091136595d8e8465c1b8e6f44dd83d9922995b
Jordan Rose <jordan_rose@apple.com>
2016-04-21 10:18:36 -0700

    [test] Add a test for SR-156.

---
+++ test/multifile/protocol-conformance-member.swift
@@ -0,0 +1,14 @@
+// RUN: rm -rf %t && mkdir %t
+// RUN: %target-build-swift -emit-library %s %S/Inputs/protocol-conformance-member-helper.swift -o %t/libTest.dylib -module-name Test
+// RUN: nm %t/libTest.dylib | FileCheck %s
+
+// CHECK: _TFV4Test10CoolStructg10coolFactorSd
+
+// SR-156: Make sure we synthesize getters for members used as protocol
+// witnesses. Check that we link correctly; we don't care which file
+// synthesizes it.
+
+protocol CoolStructProtocol {
+  var coolFactor: Double { get }
+}
+extension CoolStruct : CoolStructProtocol {}
