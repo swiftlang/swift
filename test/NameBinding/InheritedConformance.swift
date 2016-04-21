502db52fc2b5a12a3e47728650aed230859ab0c0
Mark Sands <marksands07@gmail.com>
2016-04-21 11:47:44 -0500

    Reduces the test case to a more simplified version

--- test/NameBinding/InheritedConformance.swift
+++ test/NameBinding/InheritedConformance.swift
@@ -4,17 +4,7 @@
 // RUN: %target-swift-frontend -parse -primary-file %t/main.swift -emit-reference-dependencies-path - > %t.swiftdeps
 
 // SR-1267, SR-1270
-class TypeType<T> { }
-protocol ProtocolType {}
-
-struct StructType<T> { }
-extension StructType where T : ProtocolType {
-    func testCase<T>() -> TypeType<T> {
-        return TypeType<T>()
-    }
-}
-
-class TestView : ProtocolType {}
-extension StructType where T : TestView {
-    var typeType : TypeType<T> { return testCase() }
-}
+protocol Protocol {}
+class ConformingClass: Protocol {}
+class BaseClass<T: Protocol> {}
+class ConcreteClass<T: ConformingClass> : BaseClass<T> {}
