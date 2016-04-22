5b24b191ee7a9b0160b98ee2b8d3978fcc2b91f7
Nate Cook <nmersethcook@apple.com>
2016-04-22 11:37:32 -0500

    Expanded string slicing test

--- test/1_stdlib/subString.swift
+++ test/1_stdlib/subString.swift
@@ -1,15 +1,110 @@
-// RUN: %target-run-simple-swift | FileCheck %s
+// RUN: rm -rf %t
+// RUN: mkdir -p %t
+// RUN: %target-build-swift %s -o %t/a.out
+// RUN: %target-run %t/a.out
 // REQUIRES: executable_test
 
-func test(_ s: String) {
-  print(s)
-  var s2 = s[s.index(s.startIndex, offsetBy: 2)..<s.index(s.startIndex, offsetBy: 4)]
-  print(s2)
-  var s3 = s2[s2.startIndex..<s2.startIndex]
-  var s4 = s3[s2.startIndex..<s2.startIndex]
+import StdlibUnittest
+
+var SubstringTests = TestSuite("SubstringTests")
+
+func checkMatch<S: Collection, T: Collection
+  where S.Index == T.Index, S.Iterator.Element == T.Iterator.Element,
+  S.Iterator.Element: Equatable>(
+  _ x: S, _ y: T, _ i: S.Index) {
+  
+  expectEqual(x[i], y[i])
 }
 
-test("some text")
+let s = "abcdefg"
 
-// CHECK: some text
-// CHECK: me
+SubstringTests.test("String") {
+  let s1 = s[s.index(s.startIndex, offsetBy: 2) ..<
+    s.index(s.startIndex, offsetBy: 4)]
+  let s2 = s1[s1.startIndex..<s1.endIndex]
+  let s3 = s2[s1.startIndex..<s1.endIndex]
+  
+  expectEqual(s1, "cd")
+  expectEqual(s2, "cd")
+  expectEqual(s3, "cd")
+}
+
+SubstringTests.test("CharacterView")
+  .xfail(.always("CharacterView slices don't share indices"))
+  .code {
+  var t = s.characters.dropFirst(2)
+  var u = t.dropFirst(2)
+  
+  checkMatch(s.characters, t, t.startIndex)
+  checkMatch(s.characters, t, t.index(after: t.startIndex))
+  checkMatch(s.characters, t, t.index(before: t.endIndex))
+  
+  t.replaceSubrange(t.startIndex...t.startIndex, with: ["C"])
+  expectEqual(String(t), "Cdefg")
+  expectEqual(s, "abcdefg")
+  
+  checkMatch(s.characters, t, u.startIndex)
+  checkMatch(t, u, u.startIndex)
+  checkMatch(t, u, u.index(after: u.startIndex))
+  checkMatch(t, u, u.index(before: u.endIndex))
+  
+  u.replaceSubrange(u.startIndex...u.startIndex, with: ["E"])
+  expectEqual(String(u), "Efg")
+  expectEqual(String(t), "Cdefg")
+  expectEqual(s, "abcdefg")
+}
+
+SubstringTests.test("UnicodeScalars")
+  .xfail(.always("UnicodeScalarsView slices don't share indices"))
+  .code {
+  var t = s.unicodeScalars.dropFirst(2)
+  var u = t.dropFirst(2)
+  
+  checkMatch(s.unicodeScalars, t, t.startIndex)
+  checkMatch(s.unicodeScalars, t, t.index(after: t.startIndex))
+  checkMatch(s.unicodeScalars, t, t.index(before: t.endIndex))
+  
+  t.replaceSubrange(t.startIndex...t.startIndex, with: ["C"])
+  expectEqual(String(t), "Cdefg")
+  expectEqual(s, "abcdefg")
+  
+  checkMatch(s.unicodeScalars, t, u.startIndex)
+  checkMatch(t, u, u.startIndex)
+  checkMatch(t, u, u.index(after: u.startIndex))
+  checkMatch(t, u, u.index(before: u.endIndex))
+  
+  u.replaceSubrange(u.startIndex...u.startIndex, with: ["E"])
+  expectEqual(String(u), "Efg")
+  expectEqual(String(t), "Cdefg")
+  expectEqual(s, "abcdefg")
+}
+
+SubstringTests.test("UTF16View")
+  .xfail(.always("UTF16View slices don't share indices"))
+  .code {
+  var t = s.utf16.dropFirst(2)
+  var u = t.dropFirst(2)
+  
+  checkMatch(s.utf16, t, t.startIndex)
+  checkMatch(s.utf16, t, t.index(after: t.startIndex))
+  checkMatch(s.utf16, t, t.index(before: t.endIndex))
+  
+  checkMatch(s.utf16, t, u.startIndex)
+  checkMatch(t, u, u.startIndex)
+  checkMatch(t, u, u.index(after: u.startIndex))
+  checkMatch(t, u, u.index(before: u.endIndex))
+}
+
+SubstringTests.test("UTF8View") {
+  var t = s.utf8.dropFirst(2)
+  var u = t.dropFirst(2)
+  
+  checkMatch(s.utf8, t, t.startIndex)
+  checkMatch(s.utf8, t, t.index(after: t.startIndex))
+  
+  checkMatch(s.utf8, t, u.startIndex)
+  checkMatch(t, u, u.startIndex)
+  checkMatch(t, u, u.index(after: u.startIndex))
+}
+
+runAllTests()
