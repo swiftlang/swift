20e1131b5689edc5077132a28bd25b959328026b
Doug Gregor <dgregor@apple.com>
2016-04-25 16:21:24 -0700

    [Linux] This test requires Objective-C interoperability.

--- test/IRGen/newtype.swift
+++ test/IRGen/newtype.swift
@@ -1,6 +1,8 @@
 // RUN// RUN: %target-swift-frontend -I %S/../IDE/Inputs/custom-modules %s -emit-ir | FileCheck %s
 import Newtype
 
+// REQUIRES: objc_interop
+
 // CHECK-LABEL: define %CSo8NSString* @_TF7newtype14getErrorDomainFT_VSC11ErrorDomain()
 public func getErrorDomain() -> ErrorDomain {
   // CHECK: load %CSo8NSString*, %CSo8NSString** getelementptr inbounds (%VSC11ErrorDomain, %VSC11ErrorDomain* @SNTErrOne
