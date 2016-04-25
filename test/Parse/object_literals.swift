08ec512d470e7fa621761059b7e92bd7d0a3b4bd
Ted Kremenek <kremenek@apple.com>
2016-04-24 20:56:54 -0700

    Add tests for migration fixits.

--- test/Parse/object_literals.swift
+++ test/Parse/object_literals.swift
@@ -1,6 +1,6 @@
 // RUN: %target-parse-verify-swift
 
-let _ = [##] // expected-error{{expected identifier after '#' in object literal expression}} expected-error{{object literal syntax no longer uses '[# ... #]'}}
-let _ = [#what#] // expected-error{{object literal syntax no longer uses '[# ... #]'}}
-let _ = [#what()#] // expected-error{{object literal syntax no longer uses '[# ... #]'}}
+let _ = [##] // expected-error{{expected identifier after '#' in object literal expression}} expected-error{{object literal syntax no longer uses '[# ... #]'}} {{9-10=}} {{11-13=}}
+let _ = [#what#] // expected-error{{object literal syntax no longer uses '[# ... #]'}} {{9-10=}} {{15-17=}}
+let _ = [#what()#] // expected-error{{object literal syntax no longer uses '[# ... #]'}} {{9-10=}} {{17-19=}}
 let _ = [#colorLiteral( // expected-error{{expected ',' separator}} expected-error{{expected expression in list of expressions}}
