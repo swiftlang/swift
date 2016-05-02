7b8c4fca8e67eac7a48b02a916623a04345e682b
Jordan Rose <jordan_rose@apple.com>
2016-05-02 10:24:26 -0700

    [test] Disable Reflection/typeref_decoding on non-OS-X.
    ...until Slava has a chance to look at it. This should bring the
    buildbots back.

--- test/Reflection/typeref_decoding.swift
+++ test/Reflection/typeref_decoding.swift
@@ -2,3 +2,5 @@
 // RUN: %target-build-swift %S/Inputs/ConcreteTypes.swift %S/Inputs/GenericTypes.swift %S/Inputs/Protocols.swift %S/Inputs/Extensions.swift %S/Inputs/Closures.swift -parse-as-library -emit-module -emit-library -module-name TypesToReflect -Xfrontend -enable-reflection-metadata -Xfrontend -enable-reflection-names -o %t/libTypesToReflect.%target-dylib-extension
 // RUN: %target-swift-reflection-dump -binary-filename %t/libTypesToReflect.%target-dylib-extension > %t/typeref_decoding.txt
 // RUN: diff -u %S/typeref_decoding.result.txt %t/typeref_decoding.txt
+
+// REQUIRES: OS=macosx
