a7ccda59fd53fa916c3fe4d7bf3ef8e88f7d9c0a
Slava Pestov <spestov@apple.com>
2016-05-02 21:26:02 -0700

    IRGen: Fixes for capture descriptor emission
    - Fix caller/callee confusion, and use the right SIL function type
      for obtaining the generic signature.
    - Correctly interpret the NecessaryBindings structure and the
      substitutions therein.
    - Fix alignment for capture and builtin type descriptors
    - Put capture descriptor typerefs in the correct section
    
    Add new SIL-level tests to precisely trigger various scenarios.

--- lib/IRGen/GenReflection.cpp
+++ lib/IRGen/GenReflection.cpp
@@ -184,18 +184,15 @@ protected:
   }
 
   /// Add a 32-bit relative offset to a mangled typeref string
-  /// in the typeref reflection section, or globally if 'global' is 'true'.
-  void addTypeRef(Module *ModuleContext, CanType type, bool global = false) {
+  /// in the typeref reflection section.
+  void addTypeRef(Module *ModuleContext, CanType type) {
     assert(type);
     Mangle::Mangler mangler(/*DWARFMangling*/false,
                             /*usePunyCode*/ true,
                             /*OptimizeProtocolNames*/ false);
     mangler.setModuleContext(ModuleContext);
     mangler.mangleType(type, 0);
-    auto mangledName = global
-     ? IGM.getAddrOfGlobalString(mangler.finalize(),
-                                 /*willBeRelativelyAddressed*/ true)
-     : IGM.getAddrOfStringForTypeRef(mangler.finalize());
+    auto mangledName = IGM.getAddrOfStringForTypeRef(mangler.finalize());
     addRelativeAddress(mangledName);
   }
 
@@ -476,7 +473,7 @@ public:
                                         init,
                                         "\x01l__swift3_builtin_metadata");
     var->setSection(IGM.getBuiltinTypeMetadataSectionName());
-    var->setAlignment(IGM.getPointerAlignment().getValue());
+    var->setAlignment(4);
 
     auto replacer = llvm::ConstantExpr::getBitCast(var, IGM.Int8PtrTy);
     tempBase->replaceAllUsesWith(replacer);
@@ -490,18 +487,28 @@ public:
 /// captures are generic.
 class CaptureDescriptorBuilder : public ReflectionMetadataBuilder {
   swift::reflection::MetadataSourceBuilder SourceBuilder;
-  SILFunction &Callee;
+  SILFunction &Caller;
+  CanSILFunctionType OrigCalleeType;
+  CanSILFunctionType SubstCalleeType;
+  ArrayRef<Substitution> Subs;
   HeapLayout &Layout;
+  unsigned FirstCaptureIndex;
 public:
   CaptureDescriptorBuilder(IRGenModule &IGM,
                            llvm::SetVector<CanType> &BuiltinTypes,
-                           SILFunction &Callee,
-                           HeapLayout &Layout)
+                           SILFunction &Caller,
+                           CanSILFunctionType OrigCalleeType,
+                           CanSILFunctionType SubstCalleeType,
+                           ArrayRef<Substitution> Subs,
+                           HeapLayout &Layout,
+                           unsigned FirstCaptureIndex)
     : ReflectionMetadataBuilder(IGM, BuiltinTypes),
-      Callee(Callee), Layout(Layout) {}
+      Caller(Caller), OrigCalleeType(OrigCalleeType),
+      SubstCalleeType(SubstCalleeType), Subs(Subs),
+      Layout(Layout), FirstCaptureIndex(FirstCaptureIndex) {}
 
   using MetadataSourceMap
-    = llvm::SetVector<std::pair<CanType, const reflection::MetadataSource*>>;
+    = std::vector<std::pair<CanType, const reflection::MetadataSource*>>;
 
   void addMetadataSource(const reflection::MetadataSource *Source) {
     if (Source == nullptr) {
@@ -512,35 +519,16 @@ public:
       MetadataSourceEncoder Encoder(OS);
       Encoder.visit(Source);
 
-      auto EncodedSource = IGM.getAddrOfGlobalString(OS.str(),
-        /*willBeRelativelyAddressed*/ true);
+      auto EncodedSource = IGM.getAddrOfStringForTypeRef(OS.str());
       addRelativeAddress(EncodedSource);
     }
   }
 
-  const reflection::MetadataSource *searchBindingsForMetadata(CanType type) {
-    auto &Bindings = Layout.getBindings();
-    for (unsigned i = 0; i < Bindings.size(); ++i) {
-      if (Bindings[i].TypeParameter == type) {
-        return SourceBuilder.createClosureBinding(i);
-      }
-    }
-    return nullptr;
-  }
-
-  llvm::Optional<unsigned> indexOfCaptureWithType(CanType interfaceType) {
-    auto ElementTypes = Layout.getElementTypes();
-    for (unsigned i = 0; i < ElementTypes.size(); ++i) {
-      auto ElementType = ElementTypes[i];
-      if (!ElementType)
-        continue;
-      auto ElementInterfaceType
-        = Callee.mapTypeOutOfContext(ElementType.getSwiftRValueType())
-          ->getCanonicalType();
-      if (ElementInterfaceType == interfaceType)
-        return llvm::Optional<unsigned>(i);
-    }
-    return None;
+  /// Slice off the NecessaryBindings struct at the beginning, if it's there.
+  /// We'll keep track of how many things are in the bindings struct with its
+  /// own count in the capture descriptor.
+  ArrayRef<SILType> getElementTypes() {
+    return Layout.getElementTypes().slice(Layout.hasBindings() ? 1 : 0);
   }
 
   /// Build a map from generic parameter -> source of its metadata at runtime.
@@ -550,83 +538,62 @@ public:
   MetadataSourceMap getMetadataSourceMap() {
     MetadataSourceMap SourceMap;
 
-    auto CalleeType = Callee.getLoweredFunctionType();
-
-    if (!CalleeType->isPolymorphic())
+    if (!OrigCalleeType->isPolymorphic())
       return SourceMap;
 
-    PolymorphicConvention Convention(IGM, CalleeType);
+    // Any generic parameters that are not fulfilled are passed in via the
+    // bindings. Structural types are decomposed, so emit the contents of
+    // the bindings structure directly.
+    auto &Bindings = Layout.getBindings();
+    for (unsigned i = 0; i < Bindings.size(); ++i) {
+      auto Source = SourceBuilder.createClosureBinding(i);
+      auto BindingType = Caller.mapTypeOutOfContext(Bindings[i].TypeParameter);
+      SourceMap.push_back({BindingType->getCanonicalType(), Source});
+    }
+
+    PolymorphicConvention Convention(IGM, OrigCalleeType);
 
     using SourceKind = PolymorphicConvention::SourceKind;
 
-    auto Generics = Callee.getContextGenericParams()->getNestedGenericParams();
-    for (auto GenericParam : Generics) {
-      // The generic type parameter (depth, index) serves as the key to the
-      // metadata source map.
-      const auto GenericParamType
-        = GenericParam->getDeclaredType()->getCanonicalType();
+    // Check if any requirements were fulfilled by metadata stored inside a
+    // captured value.
+    auto GenericSig = OrigCalleeType->getGenericSignature();
+    auto SubstMap = GenericSig->getSubstitutionMap(Subs);
+    auto Generics = GenericSig->getGenericParams();
 
-      // Check to see if the convention fulfills a source of the metadata we
-      // need.
+    for (auto GenericParam : Generics) {
+      auto GenericParamType = GenericParam->getCanonicalType();
+
       auto Fulfillment
         = Convention.getFulfillmentForTypeMetadata(GenericParamType);
 
       if (Fulfillment != nullptr) {
-        // The convention fulfills the requirement, so record how to get
-        // to the metadata.
-
         auto ConventionSource = Convention.getSource(Fulfillment->SourceIndex);
-        if (ConventionSource.getKind() == SourceKind::SelfMetadata) {
-          SourceMap.insert({GenericParamType, SourceBuilder.createSelf()});
-          continue;
-        } else if (ConventionSource.getKind() == SourceKind::SelfWitnessTable) {
-          SourceMap.insert({
-            GenericParamType,
-            SourceBuilder.createSelfWitnessTable()
-          });
+
+        if (ConventionSource.getKind() == SourceKind::SelfMetadata ||
+            ConventionSource.getKind() == SourceKind::SelfWitnessTable) {
+          // Handled as part of bindings
           continue;
         }
 
-        // Since captures are created via partial_apply instructions, we need
-        // to see which function parameter fulfilled this metadata need and
-        // grab its type.
-        auto FnParameterIndex = ConventionSource.getParamIndex();
-        auto FnParameter = CalleeType->getParameters()[FnParameterIndex];
-        auto ParameterType = FnParameter.getType()->getCanonicalType();
+        // The metadata might be reached via a non-trivial path (eg,
+        // dereferencing an isa pointer or a generic argument). Record
+        // the path. We assume captured values map 1-1 with function
+        // parameters.
+        auto ParamIndex = ConventionSource.getParamIndex();
+        auto Index = ParamIndex - FirstCaptureIndex;
 
-        // Now we need to get the index of the captured value with that type so
-        // we know where to start the search at runtime.
-        //
-        // For example, if we capture an object MyClass<T> and we need T, we can
-        // get it by finding that captured MyClass<T> in the closure, following
-        // it's metadata pointer, and getting its 0th generic argument. We can
-        // do that particular trick because class instances' metadata have their
-        // generic parameters instantiated with real metadata.
-        auto CaptureIndex = indexOfCaptureWithType(ParameterType);
-        if (CaptureIndex.hasValue()) {
-          auto Root
-            = SourceBuilder.createReferenceCapture(CaptureIndex.getValue());
-          auto Src = Fulfillment->Path.getMetadataSource(SourceBuilder, Root);
-          SourceMap.insert({GenericParamType, Src});
-        }
-      } else {
-        // The convention didn't provide a source of the metadata, so we'll
-        // want to check the necessary bindings structure to see if it was
-        // stored there (it most likely is).
-        //
-        // We need to pull the generic parameters from the callee's interface
-        // type back into context because NecessaryBindings speaks in terms of
-        // archetypes.
-        auto Archetype
-          = Callee.mapTypeIntoContext(GenericParamType)->getCanonicalType();
+        auto ParamType = SubstCalleeType->getParameters()[ParamIndex].getSILType();
+        auto CaptureType = getElementTypes()[Index];
+        assert(ParamType == CaptureType);
+        (void) ParamType;
+        (void) CaptureType;
 
-        if (auto Source = searchBindingsForMetadata(Archetype)) {
-          SourceMap.insert({GenericParamType, Source});
-        } else {
-          // We couldn't find a source of metadata even in the bindings, so
-          // we won't be able to get to this metadata at runtime.
-          SourceMap.insert({GenericParamType, nullptr});
-        }
+        auto Root = SourceBuilder.createReferenceCapture(Index);
+        auto Src = Fulfillment->Path.getMetadataSource(SourceBuilder, Root);
+
+        auto SubstType = Caller.mapTypeOutOfContext(SubstMap[GenericParam]);
+        SourceMap.push_back({SubstType->getCanonicalType(), Src});
       }
     }
 
@@ -638,15 +605,9 @@ public:
   std::vector<CanType> getCaptureTypes() {
     std::vector<CanType> CaptureTypes;
 
-    // Slice off the NecessaryBindings struct at the beginning, if it's there.
-    // We'll keep track of how many things are in the bindings struct with its
-    // own count in the capture descriptor.
-    auto ElementTypes = Layout.getElementTypes()
-      .slice(Layout.hasBindings() ? 1 : 0);
-
-    for (auto ElementType : ElementTypes) {
+    for (auto ElementType : getElementTypes()) {
       auto SwiftType = ElementType.getSwiftRValueType();
-      auto InterfaceType = Callee.mapTypeOutOfContext(SwiftType);
+      auto InterfaceType = Caller.mapTypeOutOfContext(SwiftType);
       CaptureTypes.push_back(InterfaceType->getCanonicalType());
     }
 
@@ -663,8 +624,7 @@ public:
 
     // Now add typerefs of all of the captures.
     for (auto CaptureType : CaptureTypes) {
-      addTypeRef(Callee.getModule().getSwiftModule(), CaptureType,
-                 /*global*/ true);
+      addTypeRef(Caller.getModule().getSwiftModule(), CaptureType);
       addBuiltinTypeRefs(CaptureType);
     }
 
@@ -673,7 +633,8 @@ public:
     for (auto GenericAndSource : MetadataSources) {
       auto GenericParam = GenericAndSource.first->getCanonicalType();
       auto Source = GenericAndSource.second;
-      addTypeRef(nullptr, GenericParam, /*global*/ true);
+
+      addTypeRef(nullptr, GenericParam);
       addMetadataSource(Source);
     }
   }
@@ -696,7 +657,7 @@ public:
                                         init,
                                         "\x01l__swift3_capture_descriptor");
     var->setSection(IGM.getCaptureDescriptorMetadataSectionName());
-    var->setAlignment(IGM.getPointerAlignment().getValue());
+    var->setAlignment(4);
 
     auto replacer = llvm::ConstantExpr::getBitCast(var, IGM.Int8PtrTy);
     tempBase->replaceAllUsesWith(replacer);
@@ -769,13 +730,20 @@ llvm::Constant *IRGenModule::getAddrOfStringForTypeRef(StringRef Str) {
   return entry.second;
 }
 
-llvm::Constant *IRGenModule::getAddrOfCaptureDescriptor(SILFunction &SILFn,
-                                                        HeapLayout &Layout) {
+llvm::Constant *
+IRGenModule::getAddrOfCaptureDescriptor(SILFunction &Caller,
+                                        CanSILFunctionType OrigCalleeType,
+                                        CanSILFunctionType SubstCalleeType,
+                                        ArrayRef<Substitution> Subs,
+                                        HeapLayout &Layout,
+                                        unsigned FirstCaptureIndex) {
   if (!IRGen.Opts.EnableReflectionMetadata)
     return llvm::Constant::getNullValue(CaptureDescriptorPtrTy);
 
   llvm::SetVector<CanType> BuiltinTypes;
-  CaptureDescriptorBuilder builder(*this, BuiltinTypes, SILFn, Layout);
+  CaptureDescriptorBuilder builder(*this, BuiltinTypes, Caller,
+                                   OrigCalleeType, SubstCalleeType, Subs,
+                                   Layout, FirstCaptureIndex);
 
   auto var = builder.emit();
   if (var)
