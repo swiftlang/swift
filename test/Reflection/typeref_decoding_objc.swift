5f34cda9ed6876cf846148ebed89be8fe911fb2b
Mark Lacey <mark.lacey@apple.com>
2016-05-05 11:13:39 -0700

    Temporarily disable failing reflection test.
    It's currently failing on the master-next branch and needs
    investigation.

--- test/Reflection/typeref_decoding_objc.swift
+++ test/Reflection/typeref_decoding_objc.swift
@@ -1,3 +1,4 @@
+// REQUIRES: rdar26118423
 // RUN: rm -rf %t && mkdir -p %t
 // RUN: %target-build-swift %S/Inputs/ObjectiveCTypes.swift -parse-as-library -emit-module -emit-library -module-name TypesToReflect -Xfrontend -enable-reflection-metadata -Xfrontend -enable-reflection-names -o %t/libTypesToReflect.%target-dylib-extension
 // RUN: %target-swift-reflection-dump -binary-filename %t/libTypesToReflect.%target-dylib-extension > %t/typeref_decoding.txt
