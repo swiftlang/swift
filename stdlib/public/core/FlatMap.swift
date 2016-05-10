1afa93b646c37e5c0a5ce94c1580c0d7f21b153f
Russ Bishop <russ@plangrid.com>
2016-05-10 02:42:57 -0700

    [stdlib] SR-361: Implement SE008: Add Lazy flatMap for Seq of Optionals
    https://bugs.swift.org/browse/SR-361
    
    Adds required LazyFilterRandomAccessCollection and the flatMap extensions.

--- stdlib/public/core/FlatMap.swift
+++ stdlib/public/core/FlatMap.swift
@@ -24,6 +24,26 @@ extension LazySequenceProtocol {
     FlattenSequence<LazyMapSequence<Elements, SegmentOfResult>>> {
     return self.map(transform).flatten()
   }
+  
+  /// Returns a `LazyMapSequence` containing the concatenated non-nil
+  /// results of mapping transform over this `Sequence`.  The elements of
+  /// the result are computed lazily, each time they are read.
+  ///
+  /// Use this method to receive only nonoptional values when your
+  /// transformation produces an optional value.
+  ///
+  /// - Parameter transform: A closure that accepts an element of this
+  /// sequence as its argument and returns an optional value.
+  @warn_unused_result
+  public func flatMap<U>(
+    _ transform: (Self.Elements.Iterator.Element) -> U?
+  ) -> LazyMapSequence<
+    LazyFilterSequence<
+      LazyMapSequence<Self.Elements, U?>>,
+    U
+  > {
+    return self.map(transform).filter { $0 != nil }.map { $0! }
+  }
 }
 
 extension LazyCollectionProtocol {
@@ -41,6 +61,26 @@ extension LazyCollectionProtocol {
       LazyMapCollection<Elements, SegmentOfResult>>
   > {
     return self.map(transform).flatten()
+  }
+  
+  /// Returns a `LazyMapCollection` containing the concatenated non-nil
+  /// results of mapping transform over this collection.  The elements of
+  /// the result are computed lazily, each time they are read.
+  ///
+  /// Use this method to receive only nonoptional values when your
+  /// transformation produces an optional value.
+  ///
+  /// - Parameter transform: A closure that accepts an element of this
+  /// collection as its argument and returns an optional value.
+  @warn_unused_result
+  public func flatMap<U>(
+    _ transform: (Self.Elements.Iterator.Element) -> U?
+  ) -> LazyMapCollection<
+    LazyFilterCollection<
+      LazyMapCollection<Self.Elements, U?>>,
+    U
+  > {
+    return self.map(transform).filter { $0 != nil }.map { $0! }
   }
 }
 
@@ -66,5 +106,49 @@ extension LazyCollectionProtocol
       LazyMapBidirectionalCollection<Elements, SegmentOfResult>
   >> {
     return self.map(transform).flatten()
+  }
+  
+  /// Returns a `LazyMapBidirectionalCollection` containing the concatenated non-nil
+  /// results of mapping transform over this collection.  The elements of
+  /// the result are computed lazily, each time they are read.
+  ///
+  /// Use this method to receive only nonoptional values when your
+  /// transformation produces an optional value.
+  ///
+  /// - Parameter transform: A closure that accepts an element of this
+  /// collection as its argument and returns an optional value.
+  public func flatMap<U>(
+    _ transform: (Self.Elements.Iterator.Element) -> U?
+  ) -> LazyMapBidirectionalCollection<
+    LazyFilterBidirectionalCollection<
+      LazyMapBidirectionalCollection<Self.Elements, U?>>,
+    U
+  > {
+    return self.map(transform).filter { $0 != nil }.map { $0! }
+  }
+}
+
+extension LazyCollectionProtocol
+  where
+  Self : RandomAccessCollection,
+  Elements : RandomAccessCollection
+{
+  /// Returns a `LazyMapRandomAccessCollection` containing the concatenated non-nil
+  /// results of mapping transform over this collection.  The elements of
+  /// the result are computed lazily, each time they are read.
+  ///
+  /// Use this method to receive only nonoptional values when your
+  /// transformation produces an optional value.
+  ///
+  /// - Parameter transform: A closure that accepts an element of this
+  /// collection as its argument and returns an optional value.
+  public func flatMap<U>(
+    _ transform: (Self.Elements.Iterator.Element) -> U?
+  ) -> LazyMapRandomAccessCollection<
+    LazyFilterRandomAccessCollection<
+      LazyMapRandomAccessCollection<Self.Elements, U?>>,
+    U
+  > {
+    return self.map(transform).filter { $0 != nil }.map { $0! }
   }
 }
