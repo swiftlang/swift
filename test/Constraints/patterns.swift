073f4279422163af4f71d15067c6ecf48b6dc54e
Ted Kremenek <kremenek@apple.com>
2016-05-11 15:53:24 -0700

    Revert "Don't diagnose failures to call symmetrically-typed binary operators"
    This reverts commit 35ba809fd0665eba2bbe5d0f1cca2f37d00ae620.

--- test/Constraints/patterns.swift
+++ test/Constraints/patterns.swift
@@ -140,7 +140,7 @@ default: break
 
 // <rdar://problem/21995744> QoI: Binary operator '~=' cannot be applied to operands of type 'String' and 'String?'
 switch ("foo" as String?) {
-case "what": break // expected-error{{expression pattern of type 'String' cannot match values of type 'String?'}}
+case "what": break // expected-error{{value of optional type 'String?' not unwrapped; did you mean to use '!' or '?'?}}
 default: break
 }
 
