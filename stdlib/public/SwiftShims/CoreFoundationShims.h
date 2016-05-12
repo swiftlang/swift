780746669f97b2084b993bf85e2c40c5cca64d6b
Dmitri Gribenko <gribozavr@gmail.com>
2016-05-12 13:25:50 -0700

    stdlib: add SWIFT_RUNTIME_STDLIB_INTERFACE attribute where necessary
    ... and remove some redundant ones.
    
    This change fixes the RuntimeObjC.swift test.
    
    rdar://problem/26239602

--- stdlib/public/SwiftShims/CoreFoundationShims.h
+++ stdlib/public/SwiftShims/CoreFoundationShims.h
@@ -20,6 +20,7 @@
 #define SWIFT_STDLIB_SHIMS_COREFOUNDATIONSHIMS_H
 
 #include "SwiftStdint.h"
+#include "Visibility.h"
 
 #ifdef __cplusplus
 namespace swift { extern "C" {
@@ -54,37 +55,46 @@ typedef unsigned char _swift_shims_Boolean;
 typedef __swift_uint16_t _swift_shims_UniChar;
 
 // Buffer is nullable in case the string is zero-length.
+SWIFT_RUNTIME_STDLIB_INTERFACE
 void _swift_stdlib_CFStringGetCharacters(_swift_shims_CFStringRef __nonnull theString,
                            _swift_shims_CFRange range,
                            _swift_shims_UniChar *__nullable buffer);
 
+SWIFT_RUNTIME_STDLIB_INTERFACE
 const _swift_shims_UniChar *__nullable _swift_stdlib_CFStringGetCharactersPtr(
     _swift_shims_CFStringRef __nonnull theString);
 
+SWIFT_RUNTIME_STDLIB_INTERFACE
 _swift_shims_CFIndex
     _swift_stdlib_CFStringGetLength(_swift_shims_CFStringRef __nonnull theString);
 
+SWIFT_RUNTIME_STDLIB_INTERFACE
 _swift_shims_CFStringRef __nonnull _swift_stdlib_CFStringCreateWithSubstring(
     _swift_shims_CFAllocatorRef __nullable alloc,
     _swift_shims_CFStringRef __nonnull str, _swift_shims_CFRange range);
 
+SWIFT_RUNTIME_STDLIB_INTERFACE
 _swift_shims_UniChar
 _swift_stdlib_CFStringGetCharacterAtIndex(_swift_shims_CFStringRef __nonnull theString,
                             _swift_shims_CFIndex idx);
 
+SWIFT_RUNTIME_STDLIB_INTERFACE
 _swift_shims_CFStringRef __nonnull _swift_stdlib_CFStringCreateCopy(
     _swift_shims_CFAllocatorRef __nullable alloc,
     _swift_shims_CFStringRef __nonnull theString);
 
+SWIFT_RUNTIME_STDLIB_INTERFACE
 const char *__nullable _swift_stdlib_CFStringGetCStringPtr(
     _swift_shims_CFStringRef __nonnull theString,
     _swift_shims_CFStringEncoding encoding);
 
+SWIFT_RUNTIME_STDLIB_INTERFACE
 _swift_shims_CFComparisonResult
 _swift_stdlib_CFStringCompare(_swift_shims_CFStringRef __nonnull theString1,
                 _swift_shims_CFStringRef __nonnull theString2,
                 _swift_shims_CFStringCompareFlags compareOptions);
 
+SWIFT_RUNTIME_STDLIB_INTERFACE
 _swift_shims_Boolean
 _swift_stdlib_CFStringFindWithOptions(_swift_shims_CFStringRef __nonnull theString,
                         _swift_shims_CFStringRef __nonnull stringToFind,
@@ -92,6 +102,7 @@ _swift_stdlib_CFStringFindWithOptions(_swift_shims_CFStringRef __nonnull theStri
                         _swift_shims_CFStringCompareFlags searchOptions,
                         _swift_shims_CFRange *__nullable result);
 
+SWIFT_RUNTIME_STDLIB_INTERFACE
 _swift_shims_CFStringRef __nonnull _swift_stdlib_objcDebugDescription(id __nonnull nsObject);
 #endif // __OBJC2__
 
