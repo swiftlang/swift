940a3a7094c54f5d67f0e1f93371af17d56ee751
Luke Larson <llarson@apple.com>
2016-05-16 14:37:36 -0700

    Reflection: Update tests for NS types

--- validation-test/Reflection/reflect_NSArray.swift
+++ validation-test/Reflection/reflect_NSArray.swift
@@ -23,7 +23,9 @@ reflect(object: obj)
 // CHECK-64: (class reflect_NSArray.TestClass)
 
 // CHECK-64: Type info:
-// CHECK-64: <null type info>
+// CHECK-64: (class_instance size=24 alignment=16 stride=32 num_extra_inhabitants=0
+// CHECK-64:   (field name=t offset=16
+// CHECK-64:     (reference kind=strong refcounting=unknown)))
 
 // CHECK-32: Reflecting an object.
 // CHECK-32: Instance pointer in child address space: 0x{{[0-9a-fA-F]+}}
@@ -31,7 +33,9 @@ reflect(object: obj)
 // CHECK-32: (class reflect_NSArray.TestClass)
 
 // CHECK-32: Type info:
-// CHECK-32: <null type info>
+// CHECK-32: (class_instance size=16 alignment=16 stride=16 num_extra_inhabitants=0
+// CHECK-32:   (field name=t offset=12
+// CHECK-32:     (reference kind=strong refcounting=unknown)))
 
 reflect(any: obj)
 
