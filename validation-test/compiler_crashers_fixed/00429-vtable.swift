e50069221a5c8198379662b58080ed279a46c66c
Ted Kremenek <kremenek@apple.com>
2016-05-21 11:37:49 -0700

    XFAIL 00429-vtable.swift because of an assertion failure (SR-1584).

--- validation-test/compiler_crashers_fixed/00429-vtable.swift
+++ validation-test/compiler_crashers_fixed/00429-vtable.swift
@@ -6,6 +6,11 @@
 // See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
 
 // RUN: not %target-swift-frontend %s -parse
+
+// REQUIRES: asserts
+// SR-1584
+// XFAIL: *
+
 }
 struct S {
 static let i: B<Q<T where A"""
