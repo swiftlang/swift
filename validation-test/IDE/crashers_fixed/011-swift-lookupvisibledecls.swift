5c83792a8f7bbe0ac76a9c011b0e9674db3972bc
John McCall <rjmccall@apple.com>
2016-05-24 11:15:29 -0700

    Pad my commit count by carefully splitting the actual test change from the commit that moved the test to the fixed directory.

--- validation-test/IDE/crashers_fixed/011-swift-lookupvisibledecls.swift
+++ validation-test/IDE/crashers_fixed/011-swift-lookupvisibledecls.swift
@@ -1,4 +1,3 @@
-// RUN: not --crash %target-swift-ide-test -code-completion -code-completion-token=A -source-filename=%s
+// RUN: %target-swift-ide-test -code-completion -code-completion-token=A -source-filename=%s
 // REQUIRES: asserts
-// XFAIL: *
 enum S<T where g:A{struct T enum S:#^A^#
