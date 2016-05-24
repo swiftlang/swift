ccf9a35ac54cdd23f927e642b0b7b338d06a40ad
David Farler <dfarler@apple.com>
2016-05-23 22:34:16 -0700

    Enable reflection metadata by default in Swift 3 (#2659)
    * Move test/Reflection tests to FileCheck instead of diff
    
    This lets us test for lines specific to architecture, for example.
    
    rdar://problem/26306303
    
    * SwiftRemoteMirror: Turn on reflection metadata by default
    
    Flip the enable flags to disable and make these IRGen options
    true by default.
    
    rdar://problem/26206547

--- validation-test/Reflection/example.swift
+++ validation-test/Reflection/example.swift
@@ -1,5 +1,5 @@
 // RUN: rm -rf %t && mkdir -p %t
-// RUN: %target-build-swift -Xfrontend -enable-reflection-metadata -Xfrontend -enable-reflection-names -lswiftSwiftReflectionTest %s -o %t/example
+// RUN: %target-build-swift -lswiftSwiftReflectionTest %s -o %t/example
 // RUN: %target-run %target-swift-reflection-test %t/example | FileCheck %s --check-prefix=CHECK-%target-ptrsize
 // REQUIRES: objc_interop
 
@@ -47,45 +47,45 @@ reflect(object: mc)
 // CHECK-64: Reflecting an object.
 // CHECK-64: Instance pointer in child address space: 0x{{[0-9a-fA-F]+}}
 
-// CHECK-64: Type reference:
-// CHECK-64: (bound_generic_class example.MyClass
-// CHECK-64:   (class example.Container)
-// CHECK-64:   (class example.Container))
+// CHECK-64:        Type reference:
+// CHECK-64:        (bound_generic_class example.MyClass
+// CHECK-64-NEXT:   (class example.Container)
+// CHECK-64-NEXT:   (class example.Container))
 
-// CHECK-64: Type info:
-// CHECK-64: (class_instance size=48 alignment=16 stride=48 num_extra_inhabitants=0
-// CHECK-64:   (field name=f offset=16
-// CHECK-64:     (thick_function size=16 alignment=8 stride=16 num_extra_inhabitants=0
-// CHECK-64:       (field name=function offset=0
-// CHECK-64:         (builtin size=8 alignment=8 stride=8 num_extra_inhabitants=1))
-// CHECK-64:       (field name=context offset=8
-// CHECK-64:         (reference kind=strong refcounting=native))))
-// CHECK-64:   (field name=x offset=32
-// CHECK-64:     (reference kind=strong refcounting=native))
-// CHECK-64:   (field name=y offset=40
-// CHECK-64:     (reference kind=strong refcounting=native)))
+// CHECK-64:        Type info:
+// CHECK-64:        (class_instance size=48 alignment=16 stride=48 num_extra_inhabitants=0
+// CHECK-64-NEXT:   (field name=f offset=16
+// CHECK-64-NEXT:     (thick_function size=16 alignment=8 stride=16 num_extra_inhabitants=0
+// CHECK-64-NEXT:       (field name=function offset=0
+// CHECK-64-NEXT:         (builtin size=8 alignment=8 stride=8 num_extra_inhabitants=1))
+// CHECK-64-NEXT:       (field name=context offset=8
+// CHECK-64-NEXT:         (reference kind=strong refcounting=native))))
+// CHECK-64-NEXT:   (field name=x offset=32
+// CHECK-64-NEXT:     (reference kind=strong refcounting=native))
+// CHECK-64-NEXT:   (field name=y offset=40
+// CHECK-64-NEXT:     (reference kind=strong refcounting=native)))
 
 
 // CHECK-32: Reflecting an object.
 // CHECK-32: Instance pointer in child address space: 0x{{[0-9a-fA-F]+}}
 
-// CHECK-32: Type reference:
-// CHECK-32: (bound_generic_class example.MyClass
-// CHECK-32:   (class example.Container)
-// CHECK-32:   (class example.Container))
+// CHECK-32:      Type reference:
+// CHECK-32:      (bound_generic_class example.MyClass
+// CHECK-32-NEXT:   (class example.Container)
+// CHECK-32-NEXT:   (class example.Container))
 
-// CHECK-32: Type info:
-// CHECK-32: (class_instance size=28 alignment=16 stride=32 num_extra_inhabitants=0
-// CHECK-32:   (field name=f offset=12
-// CHECK-32:     (thick_function size=8 alignment=4 stride=8 num_extra_inhabitants=0
-// CHECK-32:       (field name=function offset=0
-// CHECK-32:         (builtin size=4 alignment=4 stride=4 num_extra_inhabitants=1))
-// CHECK-32:       (field name=context offset=4
-// CHECK-32:         (reference kind=strong refcounting=native))))
-// CHECK-32:   (field name=x offset=20
-// CHECK-32:     (reference kind=strong refcounting=native))
-// CHECK-32:   (field name=y offset=24
-// CHECK-32:     (reference kind=strong refcounting=native)))
+// CHECK-32:      Type info:
+// CHECK-32:      (class_instance size=28 alignment=16 stride=32 num_extra_inhabitants=0
+// CHECK-32-NEXT:   (field name=f offset=12
+// CHECK-32-NEXT:     (thick_function size=8 alignment=4 stride=8 num_extra_inhabitants=0
+// CHECK-32-NEXT:       (field name=function offset=0
+// CHECK-32-NEXT:         (builtin size=4 alignment=4 stride=4 num_extra_inhabitants=1))
+// CHECK-32-NEXT:       (field name=context offset=4
+// CHECK-32-NEXT:         (reference kind=strong refcounting=native))))
+// CHECK-32-NEXT:   (field name=x offset=20
+// CHECK-32-NEXT:     (reference kind=strong refcounting=native))
+// CHECK-32-NEXT:   (field name=y offset=24
+// CHECK-32-NEXT:     (reference kind=strong refcounting=native)))
 
 reflect(any: mc)
 
@@ -93,19 +93,19 @@ reflect(any: mc)
 // CHECK-64: Instance pointer in child address space: 0x{{[0-9a-fA-F]+}}
 // CHECK-64: Type reference:
 // CHECK-64: (bound_generic_class example.MyClass
-// CHECK-64:   (class example.Container)
-// CHECK-64:   (class example.Container))
+// CHECK-64-NEXT:   (class example.Container)
+// CHECK-64-NEXT:   (class example.Container))
 
 // CHECK-64: Type info:
 // CHECK-64: (reference kind=strong refcounting=native)
 
 
-// CHECK-32: Reflecting an existential.
-// CHECK-32: Instance pointer in child address space: 0x{{[0-9a-fA-F]+}}
-// CHECK-32: Type reference:
-// CHECK-32: (bound_generic_class example.MyClass
-// CHECK-32:   (class example.Container)
-// CHECK-32:   (class example.Container))
+// CHECK-32:      Reflecting an existential.
+// CHECK-32:      Instance pointer in child address space: 0x{{[0-9a-fA-F]+}}
+// CHECK-32:      Type reference:
+// CHECK-32-NEXT: (bound_generic_class example.MyClass
+// CHECK-32-NEXT:   (class example.Container)
+// CHECK-32-NEXT:   (class example.Container))
 
 // CHECK-32: Type info:
 // CHECK-32: (reference kind=strong refcounting=native)
