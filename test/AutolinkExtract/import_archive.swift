f221499bcca10bdb689070ccf03f680370a0dde2
Saleem Abdulrasool <compnerd@compnerd.org>
2016-06-03 10:17:14 -0700

    test: use llvm tools
    Rather than relying on the system to provide ar and nm, use the LLVM equivalent
    tools instead.  Since the build is already dependent on llvm and its tools, this
    just ensures that the full set is self-contained.  NFC.

--- test/AutolinkExtract/import_archive.swift
+++ test/AutolinkExtract/import_archive.swift
@@ -2,7 +2,7 @@
 // RUN: mkdir -p %t
 // RUN: %target-swiftc_driver -emit-library -emit-module -emit-module-path %t/empty.swiftmodule -module-name empty -module-link-name empty %S/empty.swift
 // RUN: %target-swiftc_driver -c %s -I %t -o %t/import_experimental.o
-// RUN: ar cr %t/import_experimental.a %t/import_experimental.o
+// RUN: llvm-ar cr %t/import_experimental.a %t/import_experimental.o
 // RUN: %target-swift-autolink-extract %t/import_experimental.a -o - | FileCheck --check-prefix CHECK-%target-object-format %s
 
 // REQUIRES: autolink-extract
