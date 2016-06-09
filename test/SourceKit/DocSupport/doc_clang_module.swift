d5157c85da1b9069c73da3221505043f6b92c61b
Dmitri Gribenko <gribozavr@gmail.com>
2016-06-09 07:57:28 -0700

    Fix test SourceKit/DocSupport/doc_clang_module.swift

--- test/SourceKit/DocSupport/doc_clang_module.swift
+++ test/SourceKit/DocSupport/doc_clang_module.swift
@@ -1,4 +1,3 @@
-// XFAIL: *
 // RUN: %sourcekitd-test -req=doc-info -module Foo -- -F %S/../Inputs/libIDE-mock-sdk \
 // RUN:         %mcp_opt %clang-importer-sdk | %sed_clean > %t.response
 // RUN: diff -u %s.response %t.response
