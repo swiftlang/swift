c74273dfd119292ef466b04e618e06487a5bde8a
practicalswift <practicalswift@users.noreply.github.com>
2016-06-12 19:03:54 +0200

    [gardening] Remove "-*- swift -*-" from non-.gyb file.

--- stdlib/private/StdlibCollectionUnittest/RangeSelection.swift
+++ stdlib/private/StdlibCollectionUnittest/RangeSelection.swift
@@ -1,4 +1,4 @@
-//===--- RangeSelection.swift ---------------------------------*- swift -*-===//
+//===--- RangeSelection.swift ---------------------------------------------===//
 //
 // This source file is part of the Swift.org open source project
 //
