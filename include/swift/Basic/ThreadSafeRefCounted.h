a113666cb09718ec9b9b05e79707608fba481905
Saleem Abdulrasool <compnerd@compnerd.org>
2016-06-13 13:52:47 -0700

    Basic: use std::atomic instead of home-grown synch
    SVN r271540 removed the LLVM atomics.  Use std::atomic instead.  NFC.

--- include/swift/Basic/ThreadSafeRefCounted.h
+++ include/swift/Basic/ThreadSafeRefCounted.h
@@ -13,7 +13,7 @@
 #ifndef SWIFT_BASIC_THREADSAFEREFCOUNTED_H
 #define SWIFT_BASIC_THREADSAFEREFCOUNTED_H
 
-#include "llvm/Support/Atomic.h"
+#include <atomic>
 #include <cassert>
 
 namespace swift {
@@ -28,18 +28,19 @@ namespace swift {
 /// FIXME: This should eventually move to llvm.
 template <class Derived>
 class ThreadSafeRefCountedBase {
-  mutable llvm::sys::cas_flag ref_cnt;
+  mutable std::atomic<unsigned> ref_cnt;
 
 protected:
   ThreadSafeRefCountedBase() : ref_cnt(0) {}
 
 public:
   void Retain() const {
-    llvm::sys::AtomicIncrement(&ref_cnt);
+    ref_cnt.fetch_add(1, std::memory_order_acq_rel);
   }
 
   void Release() const {
-    int refCount = static_cast<int>(llvm::sys::AtomicDecrement(&ref_cnt));
+    int refCount =
+        static_cast<int>(ref_cnt.fetch_sub(1, std::memory_order_acq_rel));
     assert(refCount >= 0 && "Reference count was already zero.");
     if (refCount == 0) delete static_cast<const Derived*>(this);
   }
@@ -52,7 +53,7 @@ public:
 /// already have virtual methods to enforce dynamic allocation via 'new'.
 /// FIXME: This should eventually move to llvm.
 class ThreadSafeRefCountedBaseVPTR {
-  mutable llvm::sys::cas_flag ref_cnt;
+  mutable std::atomic<unsigned> ref_cnt;
   virtual void anchor();
 
 protected:
@@ -61,11 +62,12 @@ protected:
 
 public:
   void Retain() const {
-    llvm::sys::AtomicIncrement(&ref_cnt);
+    ref_cnt.fetch_add(1, std::memory_order_acq_rel);
   }
 
   void Release() const {
-    int refCount = static_cast<int>(llvm::sys::AtomicDecrement(&ref_cnt));
+    int refCount =
+        static_cast<int>(ref_cnt.fetch_sub(1, std::memory_order_acq_rel));
     assert(refCount >= 0 && "Reference count was already zero.");
     if (refCount == 0) delete this;
   }
