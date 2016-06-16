6554344d307beb43bb19100aa66193e4412e3943
Slava Pestov <spestov@apple.com>
2016-06-15 23:54:01 -0700

    AST: getInnermostDeclContext() - SubscriptDecls are DeclContexts too
    NFC for now, but will be more useful when we do generic subscripts.

--- lib/AST/Decl.cpp
+++ lib/AST/Decl.cpp
@@ -286,8 +286,10 @@ llvm::raw_ostream &swift::operator<<(llvm::raw_ostream &OS,
 DeclContext *Decl::getInnermostDeclContext() const {
   if (auto func = dyn_cast<AbstractFunctionDecl>(this))
     return const_cast<AbstractFunctionDecl*>(func);
-  if (auto nominal = dyn_cast<GenericTypeDecl>(this))
-    return const_cast<GenericTypeDecl*>(nominal);
+  if (auto subscript = dyn_cast<SubscriptDecl>(this))
+    return const_cast<SubscriptDecl*>(subscript);
+  if (auto type = dyn_cast<GenericTypeDecl>(this))
+    return const_cast<GenericTypeDecl*>(type);
   if (auto ext = dyn_cast<ExtensionDecl>(this))
     return const_cast<ExtensionDecl*>(ext);
   if (auto topLevel = dyn_cast<TopLevelCodeDecl>(this))
