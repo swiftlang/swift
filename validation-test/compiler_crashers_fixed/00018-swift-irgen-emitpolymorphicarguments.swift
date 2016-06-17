8e9734081694e71bea5db925a881b4ae33ea0db2
Slava Pestov <spestov@apple.com>
2016-06-16 19:20:16 -0700

    Fix a compiler_crasher test to not depend on specific diagnostics

--- validation-test/compiler_crashers_fixed/00018-swift-irgen-emitpolymorphicarguments.swift
+++ validation-test/compiler_crashers_fixed/00018-swift-irgen-emitpolymorphicarguments.swift
@@ -6,12 +6,12 @@
 // See http://swift.org/LICENSE.txt for license information
 // See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
 
-// RUN: %target-swift-frontend %s -parse -verify
+// RUN: not %target-swift-frontend %s -parse
 
 // Issue found by https://github.com/owensd (David Owens II)
 
-func a<T>() { // expected-error {{generic parameter 'T' is not used in function signature}}
-    enum b { // expected-error {{type 'b' nested in generic function 'a' is not allowed}}
+func a<T>() {
+    enum b {
         case c
     }
 }
