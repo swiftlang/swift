d88bfb609224b028f853f09ac6fbacac9a4f4360
Brian Croom <brian.s.croom@gmail.com>
2016-06-20 09:34:53 -0400

    [SourceKit] Rename Concurrency-Mac.cpp to Concurrency-libdispatch.cpp
    This file builds fine as long as libdispatch is available, even on
    non-Darwin platforms.

--- tools/SourceKit/lib/Support/Concurrency-libdispatch.cpp
+++ tools/SourceKit/lib/Support/Concurrency-libdispatch.cpp
@@ -1,4 +1,4 @@
-//===--- Concurrency-Mac.cpp ----------------------------------------------===//
+//===--- Concurrency-libdispatch.cpp --------------------------------------===//
 //
 // This source file is part of the Swift.org open source project
 //
