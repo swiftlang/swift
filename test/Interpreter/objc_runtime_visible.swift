945b6c445307b25cd83054ab5fe4a77a0c348ead
Jordan Rose <jordan_rose@apple.com>
2016-07-01 14:04:51 -0700

    [test] Further tweaks to objc_runtime_visible.swift.
    Speculative fix for Apple-internal bots.

--- test/Interpreter/objc_runtime_visible.swift
+++ test/Interpreter/objc_runtime_visible.swift
@@ -1,9 +1,9 @@
 // RUN: rm -rf %t && mkdir -p %t
 
-// RUN: %clang %target-cc-options -isysroot %sdk -fobjc-arc %S/Inputs/objc_runtime_visible.m -fmodules -nodefaultlibs -lc -dynamiclib -o %t/libobjc_runtime_visible.dylib
+// RUN: %clang %target-cc-options -isysroot %sdk -fobjc-arc %S/Inputs/objc_runtime_visible.m -fmodules -nodefaultlibs -lc -dynamiclib -o %t/libobjc_runtime_visible.dylib -install_name @executable_path/libobjc_runtime_visible.dylib
 // RUN: nm -g %t/libobjc_runtime_visible.dylib | FileCheck %s
 // RUN: %target-build-swift -import-objc-header %S/Inputs/objc_runtime_visible.h %t/libobjc_runtime_visible.dylib %s -o %t/main
-// RUN: %target-run %t/main
+// RUN: %target-run %t/main %t/libobjc_runtime_visible.dylib
 
 // REQUIRES: executable_test
 // REQUIRES: objc_interop
