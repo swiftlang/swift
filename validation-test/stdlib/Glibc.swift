98829a29f75f30f860e5da2647a2da473ea227a8
Ben Langmuir <blangmuir@apple.com>
2016-07-05 10:21:27 -0700

    Revert "Glibc validation test now performed also on FreeBSD"
    This broke the test on OS X bots.
    
    This reverts commit c145ac2424ea95291aedec2c38b58e7344075ce3.

--- validation-test/stdlib/Glibc.swift
+++ validation-test/stdlib/Glibc.swift
@@ -1,34 +1,27 @@
 // RUN: %target-run-stdlib-swift
 // REQUIRES: executable_test
 
+// REQUIRES: OS=linux-gnu
+
 import Swift
 import StdlibUnittest
 
-#if os(Linux) || os(FreeBSD)
+
 import Glibc
-#endif
 
 var GlibcTestSuite = TestSuite("Glibc")
 
-GlibcTestSuite.test("errno")
-  .skip(.custom({
-    !( linuxAny(reason: "").evaluate() || freeBSDAny(reason: "").evaluate() ) 
-  }, reason: "Only Linux and FreeBSD are supported")).code {
-    errno = 0
-    expectEqual(0, errno)
-    close(-1)
-    expectEqual(EBADF, errno)
+GlibcTestSuite.test("errno") {
+  errno = 0
+  expectEqual(0, errno)
+  close(-1)
+  expectEqual(EBADF, errno)
 }
-
 
 var GlibcIoctlConstants = TestSuite("GlibcIoctlConstants")
 
-GlibcIoctlConstants.test("tty ioctl constants availability") 
-  .skip(.custom({
-    !( linuxAny(reason: "").evaluate() || freeBSDAny(reason: "").evaluate() ) 
-  }, reason: "Only Linux and FreeBSD are supported")).code {
-    let aConstant = TIOCGWINSZ
+GlibcIoctlConstants.test("tty ioctl constants availability") {
+  let aConstant = TIOCGWINSZ
 }
-
 
 runAllTests()
