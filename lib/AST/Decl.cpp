d4a2e633cb170758bc65113976d94619efe4e60e
Ben Langmuir <blangmuir@apple.com>
2016-07-07 13:51:03 -0700

    [FixIt] Insert '@objc' before 'optional' on protocol property requirements
    rdar://problem/26831078

--- lib/AST/Decl.cpp
+++ lib/AST/Decl.cpp
@@ -1794,9 +1794,15 @@ bool ValueDecl::canInferObjCFromRequirement(ValueDecl *requirement) {
 
 SourceLoc ValueDecl::getAttributeInsertionLoc(bool forModifier) const {
   if (auto var = dyn_cast<VarDecl>(this)) {
-    if (auto pbd = var->getParentPatternBinding()) {
-      SourceLoc resultLoc = pbd->getAttrs().getStartLoc(forModifier);
-      return resultLoc.isValid() ? resultLoc : pbd->getStartLoc();
+    // [attrs] var ...
+    // The attributes are part of the VarDecl, but the 'var' is part of the PBD.
+    SourceLoc resultLoc = var->getAttrs().getStartLoc(forModifier);
+    if (resultLoc.isValid()) {
+      return resultLoc;
+    } else if (auto pbd = var->getParentPatternBinding()) {
+      return pbd->getStartLoc();
+    } else {
+      return var->getStartLoc();
     }
   }
 
