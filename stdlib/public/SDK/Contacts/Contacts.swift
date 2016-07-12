5a144281522492932d9490c3a95712261f0b9668
Jacob Mizraji <jmizraji@apple.com>
2016-07-12 15:58:39 -0700

    comment out NSError bridging in Contacts Overlay

--- stdlib/public/SDK/Contacts/Contacts.swift
+++ stdlib/public/SDK/Contacts/Contacts.swift
@@ -13,9 +13,9 @@
 @_exported import Contacts
 import Foundation
 
-@available(OSX, introduced: 10.11)
-@available(iOS, introduced: 9.0)
-extension CNErrorCode : _BridgedNSError {
-  public static var _nsErrorDomain: String { return CNErrorDomain }
-}
+// @available(OSX, introduced: 10.11)
+// @available(iOS, introduced: 9.0)
+// extension CNErrorCode : _BridgedNSError {
+//   public static var _nsErrorDomain: String { return CNErrorDomain }
+// }
 
