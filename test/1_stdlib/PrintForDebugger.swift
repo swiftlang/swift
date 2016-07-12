1eb200c1db712e98aad1875351e10926a3d97ae8
Enrico Granata <egranata@apple.com>
2016-07-11 17:55:00 -0700

    Use StdlibUnittest

--- test/1_stdlib/PrintForDebugger.swift
+++ test/1_stdlib/PrintForDebugger.swift
@@ -1,23 +1,17 @@
-// RUN: %target-run-simple-swift | FileCheck %s
+// RUN: %target-run-simple-swift
 // REQUIRES: executable_test
+
+import StdlibUnittest
 
 struct StructWithMembers {
   var a = 1
   var b = "Hello World"
 }
 
-print(_PrintForDebugger.printForDebugger(value: StructWithMembers()))
-// CHECK: StructWithMembers
-// CHECK: a : 1
-// CHECK: b : "Hello World"
-
 class ClassWithMembers {
   var a = 1
   var b = "Hello World"
 }
-
-print(_PrintForDebugger.printForDebugger(value: ClassWithMembers()))
-// CHECK: <ClassWithMembers: 0x
 
 class ClassWithMirror: CustomReflectable {
   var customMirror: Mirror {
@@ -25,27 +19,40 @@ class ClassWithMirror: CustomReflectable {
   }
 }
 
-print(_PrintForDebugger.printForDebugger(value: ClassWithMirror()))
-// CHECK: ClassWithMirror
-// CHECK: a : 1
-// CHECK: b : "Hello World"
+let PrintForDebuggerTests = TestSuite("PrintForDebugger")
+PrintForDebuggerTests.test("StructWithMembers") {
+  let printed = _PrintForDebugger.printForDebugger(value: StructWithMembers())
+  expectEqual(printed, "▿ StructWithMembers\n  - a : 1\n  - b : \"Hello World\"\n")
+}
 
-print(_PrintForDebugger.printForDebugger(value: [1,2,3,4]))
-// CHECK: 4 elements
-// CHECK: - 0 : 1
-// CHECK: - 1 : 2
-// CHECK: - 2 : 3
-// CHECK: - 3 : 4
+PrintForDebuggerTests.test("ClassWithMembers") {
+  let printed = _PrintForDebugger.printForDebugger(value: ClassWithMembers())
+  expectTrue(printed.hasPrefix("<ClassWithMembers: 0x"))
+}
 
-print(_PrintForDebugger.printForDebugger(value: [1:2, 3:4]))
-// CHECK: 2 elements
-// CHECK: 0 : 2 elements
-// CHECK: 1 : 2 elements
+PrintForDebuggerTests.test("ClassWithMirror") {
+  let printed = _PrintForDebugger.printForDebugger(value: ClassWithMirror())
+  expectEqual(printed, "▿ ClassWithMirror\n  - a : 1\n  - b : \"Hello World\"\n")
+}
 
-print(_PrintForDebugger.printForDebugger(value: nil as Int?))
-// CHECK: nil
+PrintForDebuggerTests.test("Array") {
+  let printed = _PrintForDebugger.printForDebugger(value: [1,2,3,4])
+  expectEqual(printed, "▿ 4 elements\n  - 0 : 1\n  - 1 : 2\n  - 2 : 3\n  - 3 : 4\n")
+}
 
-print(_PrintForDebugger.printForDebugger(value: 3 as Int?))
-// CHECK: Optional<Int>
-// CHECK: - some : 3
+PrintForDebuggerTests.test("Dictionary") {
+  let printed = _PrintForDebugger.printForDebugger(value: [1:2])
+  expectEqual(printed, "▿ 1 elements\n  ▿ 0 : 2 elements\n    - .0 : 1\n    - .1 : 2\n")
+}
 
+PrintForDebuggerTests.test("NilOptional") {
+  let printed = _PrintForDebugger.printForDebugger(value: nil as Int?)
+  expectTrue(printed.hasPrefix("nil"))
+}
+
+PrintForDebuggerTests.test("SomeOptional") {
+  let printed = _PrintForDebugger.printForDebugger(value: 3 as Int?)
+  expectEqual(printed, "▿ Optional<Int>\n  - some : 3\n")
+}
+
+runAllTests()
