5008f4de3f3a35aaa848862b0f7ad329d68cd1de
Andrew Trick <atrick@apple.com>
2016-07-15 13:47:16 -0700

    Change the SIL Module Format version.
    This module version belongs with:
    
    commit c47687da2cd1e085abf75a531c49494342fbbfa4
    Author: Andrew Trick <atrick@apple.com>
    Date:   Fri Jul 15 13:04:02 2016
    
        Add an isStrict flag to SIL pointer_to_address. (#3529)

--- include/swift/Serialization/ModuleFormat.h
+++ include/swift/Serialization/ModuleFormat.h
@@ -53,7 +53,7 @@ const uint16_t VERSION_MAJOR = 0;
 /// in source control, you should also update the comment to briefly
 /// describe what change you made. The content of this comment isn't important;
 /// it just ensures a conflict if two people change the module format.
-const uint16_t VERSION_MINOR = 252; // Last change: remove class's "foreign" field
+const uint16_t VERSION_MINOR = 253; // Last change: Add an isStrict flag to SIL pointer_to_address.
 
 using DeclID = PointerEmbeddedInt<unsigned, 31>;
 using DeclIDField = BCFixed<31>;
