5558ea0f92b0175d4789092d5f31efc12663c081
practicalswift <practicalswift@users.noreply.github.com>
2016-07-15 10:30:34 +0200

    [gardening] Header length should be 80 chars.

--- lib/IDE/TypeReconstruction.cpp
+++ lib/IDE/TypeReconstruction.cpp
@@ -1,4 +1,4 @@
-//===--- TypeReconstruction.cpp ----------------------------------------------===//
+//===--- TypeReconstruction.cpp -------------------------------------------===//
 //
 // This source file is part of the Swift.org open source project
 //
