3fbd61291dfec3d7ad42bc8a92ac5e24d23cec6a
Dmitri Gribenko <gribozavr@gmail.com>
2016-07-17 22:46:48 -0700

    tests: adapt to removal of Boolean protocol

--- validation-test/compiler_crashers_fixed/00017-llvm-foldingset-llvm-attributesetnode-nodeequals.swift
+++ validation-test/compiler_crashers_fixed/00017-llvm-foldingset-llvm-attributesetnode-nodeequals.swift
@@ -6,11 +6,17 @@
 // See http://swift.org/LICENSE.txt for license information
 // See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
 
-// RUN: not %target-swift-frontend %s -parse -verify
+// RUN: %target-swift-frontend %s -parse -verify
 
 // Issue found by https://github.com/jvasileff (John Vasileff)
 // This bug is NOT triggered when compiling with -O.
 
-func f<T : Boolean>(_ b: T) {
+protocol BooleanProtocol {
+  var boolValue: Bool { get }
 }
-f(true as Boolean) // expected-error {{cannot invoke 'f' with an argument list of type '(Boolean)'}} // expected-note {{expected an argument list of type '(T)'}}
+extension Bool : BooleanProtocol {
+  var boolValue: Bool { return self }
+}
+func f<T : BooleanProtocol>(_ b: T) {
+}
+f(true as BooleanProtocol) // expected-error {{cannot invoke 'f' with an argument list of type '(BooleanProtocol)'}} // expected-note {{expected an argument list of type '(T)'}}
