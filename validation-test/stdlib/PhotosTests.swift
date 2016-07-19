616f58c73ec51df125ddf7eb412bf10743a42ad6
Max Moiseev <moiseev@apple.com>
2016-07-19 12:18:14 -0700

    Photos overlay

---
+++ validation-test/stdlib/PhotosTests.swift
@@ -0,0 +1,42 @@
+// RUN: %target-parse-verify-swift
+
+// REQUIRES: objc_interop
+// UNSUPPORTED: OS=watchos
+// UNSUPPORTED: OS=macosx
+
+import Photos
+
+if #available(iOS 8.0, tvOS 10.0, *) {
+  // compile time only validation test for the SDK overlay,
+  // because PHChange pretty much requires a GUI app
+  struct GenericPHChangeTest {
+    let asset: PHAsset!
+    let collection: PHAssetCollection!
+    let list: PHCollectionList!
+    let assetsFetch: PHFetchResult<PHAsset>!
+    let collectionsFetch: PHFetchResult<PHAssetCollection>!
+    let listsFetch: PHFetchResult<PHCollectionList>!
+
+    func testPHChange(change: PHChange) {
+      let assetDetails: PHObjectChangeDetails<PHAsset>?
+        = change.changeDetails(for: asset)
+      let collectionDetails: PHObjectChangeDetails<PHAssetCollection>?
+        = change.changeDetails(for: collection)
+      let listDetails: PHObjectChangeDetails<PHCollectionList>?
+        = change.changeDetails(for: list)
+
+      let assetsFetchDetails: PHFetchResultChangeDetails<PHAsset>?
+        = change.changeDetails(for: assetsFetch)
+      let collectionsFetchDetails: PHFetchResultChangeDetails<PHAssetCollection>?
+        = change.changeDetails(for: collectionsFetch)
+      let listsFetchDetails: PHFetchResultChangeDetails<PHCollectionList>?
+        = change.changeDetails(for: listsFetch)
+      assert(assetDetails != nil)
+      assert(collectionDetails != nil)
+      assert(listDetails != nil)
+      assert(assetsFetchDetails != nil)
+      assert(collectionsFetchDetails != nil)
+      assert(listsFetchDetails != nil)
+    }
+  }
+}
