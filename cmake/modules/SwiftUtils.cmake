d417b48c77e4dc59636b9dc21fe7e24c390cdb93
Michael Gottesman <mgottesman@apple.com>
2016-07-21 13:54:52 -0700

    [cmake] Move SwiftBuildType.cmake into SwiftUtils.cmake. NFC.
    The contents of that .cmake file are cmake utility functions. Move them into
    SwiftUtils.cmake, a file that contains cmake utility functions. This is just a
    quick cleanup.

--- cmake/modules/SwiftUtils.cmake
+++ cmake/modules/SwiftUtils.cmake
@@ -31,3 +31,27 @@ function(precondition_translate_flag invar outvar)
   precondition(${invar})
   set(${outvar} "${${invar}}" PARENT_SCOPE)
 endfunction()
+
+function(is_build_type_optimized build_type result_var_name)
+  if("${build_type}" STREQUAL "Debug")
+    set("${result_var_name}" FALSE PARENT_SCOPE)
+  elseif("${build_type}" STREQUAL "RelWithDebInfo" OR
+         "${build_type}" STREQUAL "Release" OR
+         "${build_type}" STREQUAL "MinSizeRel")
+    set("${result_var_name}" TRUE PARENT_SCOPE)
+  else()
+    message(FATAL_ERROR "Unknown build type: ${build_type}")
+  endif()
+endfunction()
+
+function(is_build_type_with_debuginfo build_type result_var_name)
+  if("${build_type}" STREQUAL "Debug" OR
+     "${build_type}" STREQUAL "RelWithDebInfo")
+    set("${result_var_name}" TRUE PARENT_SCOPE)
+  elseif("${build_type}" STREQUAL "Release" OR
+         "${build_type}" STREQUAL "MinSizeRel")
+    set("${result_var_name}" FALSE PARENT_SCOPE)
+  else()
+    message(FATAL_ERROR "Unknown build type: ${build_type}")
+  endif()
+endfunction()
