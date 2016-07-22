fe0ca8c9333568929cf926361eab05ff2252ab1d
David Farler <dfarler@apple.com>
2016-07-22 14:58:00 -0700

    [SILGen] Utilize _silgen_name for implicit global variables (#3641)
    The known __dso_handle symbol name wasn't getting used when emitting
    references to #dsohandle, causing link time failures for a mangled
    name that didn't exist.
    
    rdar://problem/26565092

--- test/SILGen/default_arguments.swift
+++ test/SILGen/default_arguments.swift
@@ -193,7 +193,7 @@ func takeDSOHandle(_ handle: UnsafeMutablePointer<Void> = #dsohandle) { }
 
 // CHECK-LABEL: sil hidden @_TF17default_arguments13testDSOHandleFT_T_
 func testDSOHandle() {
-  // CHECK: [[DSO_HANDLE:%[0-9]+]] = global_addr @_Tv17default_arguments12__dso_handleGSpT__ : $*UnsafeMutablePointer<()>
+  // CHECK: [[DSO_HANDLE:%[0-9]+]] = global_addr @__dso_handle : $*UnsafeMutablePointer<()>
   takeDSOHandle()
 }
 
