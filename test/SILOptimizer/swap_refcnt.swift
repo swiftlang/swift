bb9c44f3f18cb086c2abfc133df3e7eb1c612044
Slava Pestov <spestov@apple.com>
2016-07-23 17:21:48 -0600

    SILOptimizer: Fix a broken test
    The test only passed on accident with -O prior to the
    '@noreturn -> Never' change, because '@noreturn' in a
    function_ref matched the 'CHECK: return' line.
    
    Make the CHECK: more specific, which even before my
    changes exposes that there was a strong_retain in
    there in optimized builds.
    
    Fixes <rdar://problem/27506150>.

--- test/SILOptimizer/swap_refcnt.swift
+++ test/SILOptimizer/swap_refcnt.swift
@@ -1,12 +1,25 @@
 // RUN: %target-swift-frontend -O -emit-sil %s | FileCheck %s
-// REQUIRES: rdar:27506150> SILOptimizer/swap_refcnt.swift failes after noreturn -> Never changes
 
 // Make sure we can swap two values in an array without retaining anything.
+
+// FIXME: In optimized builds, we inline some stuff, and end up with
+// the following instruction sequence -- %130 is a never-returning
+// function:
+//
+// strong_retain %23 : $Builtin.NativeObject
+// strong_retain %23 : $Builtin.NativeObject
+// strong_retain %23 : $Builtin.NativeObject
+// %130 = apply %121(%125, %114, %118, %120, %126)
+//
+// But really the strong_retains here should be optimized away.
+
+// REQUIRES: swift_test_mode_optimize_none
+
 
 // CHECK-LABEL: sil @_TF11swap_refcnt11swapByIndex
 // CHECK-NOT: strong_retain
 // CHECK-NOT: strong_release
-// CHECK: return
+// CHECK: return %
 public func swapByIndex(A: inout [Int8], x : Int, y : Int) {
   swap(&A[x],&A[y])
 }
