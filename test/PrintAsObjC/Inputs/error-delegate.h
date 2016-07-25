cd8a417931d16f8fe9aa2d9441dcadd076fa77ae
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-07-25 12:05:26 -0500

    Added test for SR-2159

---
+++ test/PrintAsObjC/Inputs/error-delegate.h
@@ -0,0 +1,8 @@
+// This file is meant to be used with the mock SDK, not the real one.
+#import <Foundation.h>
+
+@protocol ABCErrorProtocol <NSObject>
+
+- (void)didFail:(NSError * _Nonnull)error;
+
+@end
