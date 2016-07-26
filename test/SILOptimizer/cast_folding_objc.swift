11a5c71160d604766f2e36ec87484bf0af5d0d84
Arnold Schwaighofer <aschwaighofer@apple.com>
2016-07-26 10:51:45 -0700

    Fix ASTContext::getBridgedToObjC to not return None under id-as-any
    This would lead us to conclude that a check-cast-branch fails when it does not.
    
    rdar://27536049

--- test/SILOptimizer/cast_folding_objc.swift
+++ test/SILOptimizer/cast_folding_objc.swift
@@ -205,7 +205,7 @@ print("test0=\(test0())")
 // CHECK:         unconditional_checked_cast_addr
 
 // CHECK-LABEL: sil [noinline] @{{.*}}testCastAnyObjectToNonClassType
-// CHECK:         builtin "int_trap"
+// CHECK-NOT:         builtin "int_trap"
 
 // CHECK-LABEL: sil [noinline] @{{.*}}testCastAnyToAnyClass{{.*}}
 // CHECK:         unconditional_checked_cast_addr
