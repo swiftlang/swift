7d0f4e2b7fc50775e70338cd8b0366e51184ed03
Andrew Trick <atrick@apple.com>
2016-08-02 23:56:01 -0700

    Add some availability attributes for UnsafePointer conversion.
    This introduces some important diagnostics to help migration.
    
    Some of the diagnostics that we would like to provide are impossible
    on a generic class because they introduce ambiguity.
    
    Renaming UnsafePointer<Void> to UnsafeRawPointer makes extensions and
    global operators ambiguous.
    
    We would also like to provide this fix-it on conversion from
    UnsafePointer<Void> or UnsafeRawPointer to UnsafePointer<T>
    with this message:
      Conversion restricted. Use 'assumingMemoryBound(to:)' or 'bindMemory(to:capacity:)'}}
    
    However, that introduces ambiguous overloads that defeat other hueristics.

--- test/Sema/diag_init.swift
+++ test/Sema/diag_init.swift
@@ -1,5 +1,5 @@
 // RUN: %target-parse-verify-swift
 
 func foo(a :  UnsafePointer<Void>)->UnsafeMutablePointer<Void> {
-  return UnsafeMutablePointer(a) // expected-error {{cannot invoke initializer for type 'UnsafeMutablePointer<_>' with an argument list of type '(UnsafePointer<Void>)'}} // expected-note {{do you want to add 'mutating'}}{{31-31=mutating: }} // expected-note {{overloads for 'UnsafeMutablePointer<_>' exist with these partially matching parameter lists: (RawPointer), (OpaquePointer), (OpaquePointer?), (UnsafeMutablePointer<Pointee>), (UnsafeMutablePointer<Pointee>?)}}
+  return UnsafeMutablePointer(a) // expected-error {{'init' has been renamed to 'init(mutating:)'}}
 }
