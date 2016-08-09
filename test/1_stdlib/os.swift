0015fa57bb951cbb552f1aac1784109c976beaa9
Matt Wright <mww@apple.com>
2016-08-09 13:35:55 -0700

    [oslog-overlay-test] Fix missing %@ in test and remove previous workaround (#4161)
    rdar://problem/27659079

--- test/1_stdlib/os.swift
+++ test/1_stdlib/os.swift
@@ -14,7 +14,6 @@ var osAPI = TestSuite("osAPI")
 if #available(OSX 10.12, iOS 10.0, watchOS 3.0, tvOS 10.0, *) {
 	osAPI.test("log") {
 		os_log("test: %d", 42)
-		// FIXME: cast to NSString because of <rdar://problem/27651717> CVarArg does not work with String
-		os_log("test2: %2", "test" as! NSString)
+		os_log("test2: %@", "test")
 	}
 }
