111db9ef302f420b854e1c332b0e1d617fe493e0
Jordan Rose <jordan_rose@apple.com>
2016-08-24 16:39:34 -0700

    [APINotes] Protocols cannot use import-as-member. (#4484)
    We could support this in the future but right now it's causing problems.
    There's also a potential ambiguity issue here where a protocol and class
    could have the same name.
    
    In addition to updating the importer, remove the two entries from the
    CryptoTokenKit API notes that were trying to use this feature.
    
    rdar://problem/27990168

--- validation-test/stdlib/CryptoTokenKitTests.swift
+++ validation-test/stdlib/CryptoTokenKitTests.swift
@@ -69,8 +69,8 @@ if #available(OSX 10.12, *) {
 
   struct TKTokenTest {
     func f1(session: TKTokenSession, sessionDelegate: TKTokenSessionDelegate,
-            algorithm: TKTokenSessionDelegate.KeyAlgorithm,
-            parameters: TKTokenSessionDelegate.KeyExchangeParameters) throws {
+            algorithm: TKTokenKeyAlgorithm,
+            parameters: TKTokenKeyExchangeParameters) throws {
       let _: Bool = sessionDelegate.tokenSession!(session, supports: .none,
            keyObjectID: "", algorithm: algorithm)
       let _: Data = try sessionDelegate.tokenSession!(session, sign: Data(),
