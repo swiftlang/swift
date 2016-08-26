a24094247d06655cead9712c2bb16d9fdb74c727
Nicholas Maccharoli <nmaccharoli@gmail.com>
2016-08-27 02:37:39 +0900

    [stdlib] cleanup UnicodeScalar initializer for UInt16 (#4504)

--- stdlib/public/core/UnicodeScalar.swift
+++ stdlib/public/core/UnicodeScalar.swift
@@ -127,11 +127,7 @@ public struct UnicodeScalar :
   ///       // Do something else
   ///     }
   public init?(_ v: UInt16) {
-    if let us = UnicodeScalar(UInt32(v)) {
-      self = us
-    } else {
-      return nil
-    }
+    self.init(UInt32(v))
   }
 
   /// Creates a Unicode scalar with the specified numeric value.
