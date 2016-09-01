5573313df2b4165fc1cbdd8549717342e6340155
Vedant Kumar <vsk@apple.com>
2016-09-01 15:57:01 -0700

    [SwiftPGO] Make profile counts available to SILGenProfiler
    This makes it possible to look up the execution count corresponding to
    an ASTNode through SILGenFunction. The profile reader itself is stored
    in a SILGenModule: this doesn't seem like the best place for it, so
    suggestions for improvement are welcome!
    
    Next, we'll actually attach this data to SIL objects and pass it all
    down to IRGen.

--- lib/SILGen/SILGen.h
+++ lib/SILGen/SILGen.h
@@ -24,6 +24,7 @@
 #include "swift/SIL/SILModule.h"
 #include "swift/SIL/TypeLowering.h"
 #include "llvm/ADT/DenseMap.h"
+#include "llvm/ProfileData/InstrProfReader.h"
 #include <deque>
 
 namespace swift {
@@ -59,6 +60,9 @@ public:
   /// The profiler for instrumentation based profiling, or null if profiling is
   /// disabled.
   std::unique_ptr<SILGenProfiling> Profiler;
+
+  /// The indexed profile data to be used for PGO, or nullptr.
+  std::unique_ptr<llvm::IndexedInstrProfReader> PGOReader;
 
   /// Mapping from SILDeclRefs to emitted SILFunctions.
   llvm::DenseMap<SILDeclRef, SILFunction*> emittedFunctions;
