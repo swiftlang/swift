00d2d37c2f3a7e806cc236087ab30dfe30e8b735
Slava Pestov <spestov@apple.com>
2016-08-31 17:43:44 -0700

    Resolve some crashers

--- validation-test/compiler_crashers_fixed/28403-swift-genericsignature-getsubstitutionmap.swift
+++ validation-test/compiler_crashers_fixed/28403-swift-genericsignature-getsubstitutionmap.swift
@@ -5,6 +5,13 @@
 // See http://swift.org/LICENSE.txt for license information
 // See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
 
-// RUN: not --crash %target-swift-frontend %s -parse
+// RUN: not %target-swift-frontend %s -parse
 // REQUIRES: asserts
-protocol A{class A{var _=c<}protocol A{extension{func<:a
+{
+extension{
+protocol A{{
+}
+typealias e struct b{func a{
+enum b{
+let f=F
+class b:A class A:b
