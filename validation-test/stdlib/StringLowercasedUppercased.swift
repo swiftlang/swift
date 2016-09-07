5434ccc20a8e390e18cac16677e4e5a6a7edcded
Dmitri Gribenko <gribozavr@gmail.com>
2016-09-06 21:39:00 -0600

    stdlib: disable an incorrect String comparison optimization on platforms that call ICU directly
    ... and add a test to ensure that the next attempt at writing this
    optimization won't introduce a regression.

---
+++ validation-test/stdlib/StringLowercasedUppercased.swift
@@ -0,0 +1,20 @@
+// RUN: %target-run-simple-swiftgyb
+// REQUIRES: executable_test
+
+// This test requires that the standard library calls ICU
+// directly.  It is not specific to Linux, it is just that on
+// Apple platforms we are using the NSString bridge right now.
+
+// REQUIRES: OS=linux-gnu
+
+import StdlibUnittest
+
+var StringTests = TestSuite("StringTests")
+
+StringTests.test("smoketest") {
+  expectEqual("i\u{0307}", "\u{0130}".lowercased())
+  expectEqual("SS", "\u{00df}".uppercased())
+}
+
+runAllTests()
+
