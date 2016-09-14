9185c052a9d404ce06dcbd4ee95aed0e7f81cb28
practicalswift <practicalswift@users.noreply.github.com>
2016-09-14 20:18:23 +0200

    [gardening] Fix inconsistent headers

--- lib/SILOptimizer/Transforms/ARCCodeMotion.cpp
+++ lib/SILOptimizer/Transforms/ARCCodeMotion.cpp
@@ -1,4 +1,4 @@
-//===------------------------ ARCCodeMotion.cpp - SIL ARC Code Motion ----===//
+//===--- ARCCodeMotion.cpp - SIL ARC Code Motion --------------------------===//
 //
 // This source file is part of the Swift.org open source project
 //
