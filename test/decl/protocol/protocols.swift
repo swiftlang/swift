1116f48a4ccf868826e5f43b1bf5e422216aa157
Slava Pestov <spestov@apple.com>
2016-09-14 19:44:38 -0700

    Don't drop requirements from potential archetypes if they came from a child
    It was possible to create a same-type requirement between a type and
    one of its associated types. If one of those associated types had
    inferred conformance constraints, we would drop them from the parent
    type.
    
    This is not actually correct because then we have no way to recover
    metadata for the parent type.
    
    The underlying problem has been present for a long time, however this
    particular failure mode was new to Swift 3, when we stopped passing
    metadata for associated types.
    
    Fixes <rdar://problem/27018457>.

--- test/decl/protocol/protocols.swift
+++ test/decl/protocol/protocols.swift
@@ -235,7 +235,7 @@ struct WrongIsEqual : IsEqualComparable { // expected-error{{type 'WrongIsEqual'
 //===----------------------------------------------------------------------===//
 
 func existentialSequence(_ e: Sequence) { // expected-error{{has Self or associated type requirements}}
-  var x = e.makeIterator() // expected-error{{'Sequence' is not convertible to '<<error type>>'}}
+  var x = e.makeIterator() // expected-error{{type 'Sequence' does not conform to protocol 'IteratorProtocol'}}
   x.next()
   x.nonexistent()
 }
