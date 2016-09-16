642117b5511276c2da8ea64e36bac8ed30ff9572
Huon Wilson <huon@apple.com>
2016-09-16 16:19:01 -0700

    [IRGen] Streamline integer literal emission.

--- lib/IRGen/GenConstant.cpp
+++ lib/IRGen/GenConstant.cpp
@@ -32,16 +32,14 @@ llvm::Constant *irgen::emitConstantInt(IRGenModule &IGM,
     = ILI->getType().castTo<BuiltinIntegerType>()->getWidth();
 
   // The value may need truncation if its type had an abstract size.
-  if (width.isFixedWidth()) {
-    // nothing to do
-  } else if (width.isPointerWidth()) {
+  if (!width.isFixedWidth()) {
+    assert(width.isPointerWidth() && "impossible width value");
+
     unsigned pointerWidth = IGM.getPointerSize().getValueInBits();
     assert(pointerWidth <= value.getBitWidth()
            && "lost precision at AST/SIL level?!");
     if (pointerWidth < value.getBitWidth())
       value = value.trunc(pointerWidth);
-  } else {
-    llvm_unreachable("impossible width value");
   }
 
   return llvm::ConstantInt::get(IGM.LLVMContext, value);
