cccfe7ab1fb9617d1cfdeba9a88024a624b8d099
Doug Gregor <dgregor@apple.com>
2016-09-20 20:55:18 -0700

    [Printing] Print tuple labels when available.
    When printing a tuple via print(...), print tuple labels when they are
    available. This is possible now that the runtime metadata properly
    stores tuple labels. Fixes rdar://problem/23130016.

--- test/Interpreter/tuple_casts.swift
+++ test/Interpreter/tuple_casts.swift
@@ -15,16 +15,21 @@ func anyToInt2(_ x: Any) -> (Int, Int) {
   return x as! (Int, Int)
 }
 
+func anyToPartlyLabeled(_ x: Any) -> (first: Int, Int, third: Int) {
+  return x as! (first: Int, Int, third: Int)
+}
+
 // Labels can be added/removed.
-print("Label add/remove")          // CHECK: Label add/remove
-print(anyToIntPoint((x: 1, y: 2))) // CHECK-NEXT: (1, 2)
-print(anyToIntPoint((3, 4)))       // CHECK-NEXT: (3, 4)
-print(anyToIntPoint((x: 5, 6)))    // CHECK-NEXT: (5, 6)
-print(anyToInt2((1, 2)))           // CHECK-NEXT: (1, 2)
-print(anyToInt2((x: 3, y: 4)))     // CHECK-NEXT: (3, 4)
-print(anyToInt2((x: 5, 6)))        // CHECK-NEXT: (5, 6)
+print("Label add/remove")            // CHECK: Label add/remove
+print(anyToIntPoint((x: 1, y: 2)))   // CHECK-NEXT: (x: 1, y: 2)
+print(anyToIntPoint((3, 4)))         // CHECK-NEXT: (x: 3, y: 4)
+print(anyToIntPoint((x: 5, 6)))      // CHECK-NEXT: (x: 5, y: 6)
+print(anyToInt2((1, 2)))             // CHECK-NEXT: (1, 2)
+print(anyToInt2((x: 3, y: 4)))       // CHECK-NEXT: (3, 4)
+print(anyToInt2((x: 5, 6)))          // CHECK-NEXT: (5, 6)
+print(anyToPartlyLabeled((1, 2, 3))) // CHECK-NEXT: (first: 1, 2, third: 3)
 
 // Labels cannot be wrong.
 print("Wrong labels")                 // CHECK: Wrong labels
 print(anyToIntPointOpt((x: 1, z: 2))) // CHECK-NEXT: nil
-print(anyToIntPointOpt((x: 1, y: 2))) // CHECK-NEXT: Optional((1, 2))
+print(anyToIntPointOpt((x: 1, y: 2))) // CHECK-NEXT: Optional((x: 1, y: 2))
