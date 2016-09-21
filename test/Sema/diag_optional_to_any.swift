d8135798ebf95fa3d386b5064562fa894f075df7
Mark Lacey <mark.lacey@apple.com>
2016-09-21 13:25:35 -0700

    Improvements to optional-to-Any coercion warning.
    Don't explicitly desguar types when it is not needed and/or results in
    worse types displayed in diagnostics.
    
    Tweak the warning messages to use "this warning" rather than "the
    warning".
    
    Addresses feedback from Jordan on commit 401ca2453284a990b59bef2e649dcedef05a2b00.

--- test/Sema/diag_optional_to_any.swift
+++ test/Sema/diag_optional_to_any.swift
@@ -7,61 +7,54 @@ func takeAny(_ left: Any, _ right: Any) -> Int? {
 func throwing() throws -> Int? {}
 
 func warnOptionalToAnyCoercion(value x: Int?) -> Any {
-  let a: Any = x // expected-warning {{expression implicitly coerced from 'Optional<Int>' to Any}}
-  // expected-note@-1 {{provide a default value to avoid the warning}}
-  // expected-note@-2 {{force-unwrap the value to avoid the warning}}
-  // expected-note@-3 {{explicitly cast to Any with 'as Any' to silence}}
+  let a: Any = x // expected-warning {{expression implicitly coerced from 'Int?' to Any}}
+  // expected-note@-1 {{provide a default value to avoid this warning}}
+  // expected-note@-2 {{force-unwrap the value to avoid this warning}}
+  // expected-note@-3 {{explicitly cast to Any with 'as Any' to silence this warning}}
 
   let b: Any = x as Any
 
-  let c: Any = takeAny(a, b) // expected-warning {{expression implicitly coerced from 'Optional<Int>' to Any}}
-  // expected-note@-1 {{provide a default value to avoid the warning}}
-  // expected-note@-2 {{force-unwrap the value to avoid the warning}}
-  // expected-note@-3 {{explicitly cast to Any with 'as Any' to silence}}
+  let c: Any = takeAny(a, b) // expected-warning {{expression implicitly coerced from 'Int?' to Any}}
+  // expected-note@-1 {{provide a default value to avoid this warning}}
+  // expected-note@-2 {{force-unwrap the value to avoid this warning}}
+  // expected-note@-3 {{explicitly cast to Any with 'as Any' to silence this warning}}
 
-  let d: Any = takeAny(c, c) as Any
+  let _: Any = takeAny(c, c) as Any
 
-  let e: Any = (x)  // expected-warning {{expression implicitly coerced from 'Optional<Int>' to Any}}
-  // expected-note@-1 {{provide a default value to avoid the warning}}
-  // expected-note@-2 {{force-unwrap the value to avoid the warning}}
-  // expected-note@-3 {{explicitly cast to Any with 'as Any' to silence}}
-
-  _ = takeAny(d, e)
+  let _: Any = (x)  // expected-warning {{expression implicitly coerced from 'Int?' to Any}}
+  // expected-note@-1 {{provide a default value to avoid this warning}}
+  // expected-note@-2 {{force-unwrap the value to avoid this warning}}
+  // expected-note@-3 {{explicitly cast to Any with 'as Any' to silence this warning}}
 
   let f: Any = (x as Any)
   let g: Any = (x) as (Any)
 
-  _ = takeAny(f as? Int, g) // expected-warning {{expression implicitly coerced from 'Optional<Int>' to Any}}
-  // expected-note@-1 {{provide a default value to avoid the warning}}
-  // expected-note@-2 {{force-unwrap the value to avoid the warning}}
-  // expected-note@-3 {{explicitly cast to Any with 'as Any' to silence}}
+  _ = takeAny(f as? Int, g) // expected-warning {{expression implicitly coerced from 'Int?' to Any}}
+  // expected-note@-1 {{provide a default value to avoid this warning}}
+  // expected-note@-2 {{force-unwrap the value to avoid this warning}}
+  // expected-note@-3 {{explicitly cast to Any with 'as Any' to silence this warning}}
 
-  let h: Any = takeAny(f as? Int, g) as Any // expected-warning {{expression implicitly coerced from 'Optional<Int>' to Any}}
-  // expected-note@-1 {{provide a default value to avoid the warning}}
-  // expected-note@-2 {{force-unwrap the value to avoid the warning}}
-  // expected-note@-3 {{explicitly cast to Any with 'as Any' to silence}}
+  let _: Any = takeAny(f as? Int, g) as Any // expected-warning {{expression implicitly coerced from 'Int?' to Any}}
+  // expected-note@-1 {{provide a default value to avoid this warning}}
+  // expected-note@-2 {{force-unwrap the value to avoid this warning}}
+  // expected-note@-3 {{explicitly cast to Any with 'as Any' to silence this warning}}
 
-  let i: Any = takeAny(f as? Int as Any, g) as Any
+  let _: Any = takeAny(f as? Int as Any, g) as Any
 
-  _ = takeAny(h, i)
+  let _: Any = x! == x! ? 1 : x // expected-warning {{expression implicitly coerced from 'Int?' to Any}}
+  // expected-note@-1 {{provide a default value to avoid this warning}}
+  // expected-note@-2 {{force-unwrap the value to avoid this warning}}
+  // expected-note@-3 {{explicitly cast to Any with 'as Any' to silence this warning}}
 
-  let j: Any = x! == x! ? 1 : x // expected-warning {{expression implicitly coerced from 'Optional<Int>' to Any}}
-  // expected-note@-1 {{provide a default value to avoid the warning}}
-  // expected-note@-2 {{force-unwrap the value to avoid the warning}}
-  // expected-note@-3 {{explicitly cast to Any with 'as Any' to silence}}
-
-  let k: Any
   do {
-    k = try throwing() // expected-warning {{expression implicitly coerced from 'Optional<Int>' to Any}}
-    // expected-note@-1 {{provide a default value to avoid the warning}}
-    // expected-note@-2 {{force-unwrap the value to avoid the warning}}
-    // expected-note@-3 {{explicitly cast to Any with 'as Any' to silence}}
+    let _: Any = try throwing() // expected-warning {{expression implicitly coerced from 'Int?' to Any}}
+    // expected-note@-1 {{provide a default value to avoid this warning}}
+    // expected-note@-2 {{force-unwrap the value to avoid this warning}}
+    // expected-note@-3 {{explicitly cast to Any with 'as Any' to silence this warning}}
   } catch {}
 
-  _ = takeAny(j, k)
-
-  return x // expected-warning {{expression implicitly coerced from 'Optional<Int>' to Any}}
-  // expected-note@-1 {{provide a default value to avoid the warning}}
-  // expected-note@-2 {{force-unwrap the value to avoid the warning}}
-  // expected-note@-3 {{explicitly cast to Any with 'as Any' to silence}}
+  return x // expected-warning {{expression implicitly coerced from 'Int?' to Any}}
+  // expected-note@-1 {{provide a default value to avoid this warning}}
+  // expected-note@-2 {{force-unwrap the value to avoid this warning}}
+  // expected-note@-3 {{explicitly cast to Any with 'as Any' to silence this warning}}
 }
