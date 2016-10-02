9508fdc8a7730782c3726ded371824288ba9a3c2
gregomni <gregomni@gmail.com>
2016-10-02 15:25:55 -0700

    Simpler implementation for multiple matching patterns in case with variable bindings
    Backed off of the implementation with a shared body block, which got
    messy really fast with multiple patterns containing address-only values
    to pass in, as it led to separated stack alloc/cleanup requirements and
    passing stack alloc’d values as block args, aliasing of addresses for
    stack cleanup verifying, etc. A mess.
    
    This now does the conservative thing of repeating the body in each
    matching pattern block.

--- lib/SILGen/SILGenPattern.cpp
+++ lib/SILGen/SILGenPattern.cpp
@@ -1017,15 +1017,15 @@ void PatternMatchEmission::emitWildcardDispatch(ClauseMatrix &clauses,
   // Bind the rest of the patterns.
   bindIrrefutablePatterns(clauses[row], args, !hasGuard, hasMultipleItems);
 
-  // Emit the guard branch, if it exists.
-  if (guardExpr) {
-    SGF.usingImplicitVariablesForPattern(clauses[row].getCasePattern(), dyn_cast<CaseStmt>(stmt), [&]{
+  SGF.usingImplicitVariablesForPattern(clauses[row].getCasePattern(), dyn_cast<CaseStmt>(stmt), [&]{
+    // Emit the guard branch, if it exists.
+    if (guardExpr) {
       this->emitGuardBranch(guardExpr, guardExpr, failure);
-    });
-  }
+    }
 
-  // Enter the row.
-  CompletionHandler(*this, args, clauses[row]);
+    // Enter the row.
+    CompletionHandler(*this, args, clauses[row]);
+  });
   assert(!SGF.B.hasValidInsertionPoint());
 }
 
@@ -1168,13 +1168,6 @@ void PatternMatchEmission::bindVariable(SILLocation loc, VarDecl *var,
                                         CanType formalValueType,
                                         bool isIrrefutable,
                                         bool hasMultipleItems) {
-  // If this binding is one of multiple patterns, each individual binding
-  // will just be let, and then the chosen value will get forwarded into
-  // a var box in the final shared case block.
-  bool forcedLet = hasMultipleItems && !var->isLet();
-  if (forcedLet)
-    var->setLet(true);
-  
   // Initialize the variable value.
   InitializationPtr init = SGF.emitInitializationForVarDecl(var);
 
@@ -1184,9 +1177,6 @@ void PatternMatchEmission::bindVariable(SILLocation loc, VarDecl *var,
   } else {
     std::move(rv).copyInto(SGF, loc, init.get());
   }
-  
-  if (forcedLet)
-    var->setLet(false);
 }
 
 /// Evaluate a guard expression and, if it returns false, branch to
@@ -2099,7 +2089,7 @@ void SILGenFunction::usingImplicitVariablesForPattern(Pattern *pattern, CaseStmt
 
   ArrayRef<CaseLabelItem> labelItems = stmt->getCaseLabelItems();
   auto expectedPattern = labelItems[0].getPattern();
-  
+
   if (labelItems.size() <= 1 || pattern == expectedPattern) {
     f();
     return;
@@ -2107,27 +2097,28 @@ void SILGenFunction::usingImplicitVariablesForPattern(Pattern *pattern, CaseStmt
 
   // Remap vardecls that the case body is expecting to the pattern var locations
   // for the given pattern, emit whatever, and switch back.
-  SmallVector<VarDecl *, 4> Vars;
-  expectedPattern->collectVariables(Vars);
+  SmallVector<VarDecl *, 4> expectedVars;
+  SmallVector<VarLoc, 4> savedVarLocs;
+  expectedPattern->collectVariables(expectedVars);
+
+  for (auto expected : expectedVars)
+    savedVarLocs.push_back(VarLocs[expected]);
   
-  auto variableSwapper = [&] {
-    pattern->forEachVariable([&](VarDecl *VD) {
-      if (!VD->hasName())
+  pattern->forEachVariable([&](VarDecl *VD) {
+    if (!VD->hasName())
+      return;
+    for (auto expected : expectedVars) {
+      if (expected->hasName() && expected->getName() == VD->getName()) {
+        VarLocs[expected] = VarLocs[VD];
         return;
-      for (auto expected : Vars) {
-        if (expected->hasName() && expected->getName() == VD->getName()) {
-          auto swap = VarLocs[expected];
-          VarLocs[expected] = VarLocs[VD];
-          VarLocs[VD] = swap;
-          return;
-        }
       }
-    });
-  };
+    }
+  });
   
-  variableSwapper();
   f();
-  variableSwapper();
+
+  for (unsigned index = 0; index < savedVarLocs.size(); index++)
+    VarLocs[expectedVars[index]] = savedVarLocs[index];
 }
 
 void SILGenFunction::emitSwitchStmt(SwitchStmt *S) {
@@ -2154,41 +2145,6 @@ void SILGenFunction::emitSwitchStmt(SwitchStmt *S) {
       JumpDest sharedDest = emission.getSharedCaseBlockDest(caseBlock,
                                                         row.hasFallthroughTo());
       Cleanups.emitBranchAndCleanups(sharedDest, caseBlock);
-    } else if (caseBlock->getCaseLabelItems().size() > 1) {
-      JumpDest sharedDest = emission.getSharedCaseBlockDest(caseBlock,
-                                                            row.hasFallthroughTo());
-      
-      // Generate the arguments from this row's pattern in the case block's expected order,
-      // and keep those arguments from being cleaned up, as we're passing the +1 along to
-      // the shared case block dest. (The cleanups still happen, as they are threaded through
-      // here messily, but the explicit retains here counteract them, and then the
-      // retain/release pair gets optimized out.)
-      ArrayRef<CaseLabelItem> labelItems = caseBlock->getCaseLabelItems();
-      SmallVector<SILValue, 4> args;
-      SmallVector<VarDecl *, 4> expectedVarOrder;
-      SmallVector<VarDecl *, 4> Vars;
-      labelItems[0].getPattern()->collectVariables(expectedVarOrder);
-      row.getCasePattern()->collectVariables(Vars);
-      
-      for (auto expected : expectedVarOrder) {
-        if (!expected->hasName())
-        continue;
-        for (auto var : Vars) {
-          if (var->hasName() && var->getName() == expected->getName()) {
-            auto value = VarLocs[var].value;
-            args.push_back(value);
-            
-            for (auto cmv : argArray) {
-              if (cmv.getValue() == value) {
-                B.createRetainValue(CurrentSILLoc, value, Atomicity::Atomic);
-                break;
-              }
-            }
-            break;
-          }
-        }
-      }
-      Cleanups.emitBranchAndCleanups(sharedDest, caseBlock, args);
     } else {
       // However, if we don't have a fallthrough or a multi-pattern 'case', we
       // can just emit the body inline and save some dead blocks.
