183d4763f1e3654893cbff7e586b36bf0da4e6e3
Rintaro Ishizaki <fs.output@gmail.com>
2016-10-05 23:28:03 +0900

    [QoI][Parse] Improve recovering from unknown attribute
    By consuming parens.
    As for type attributes, handle `@unknownAttribute(Arg) -> Ret` case.
    
    Improves diagnostic QoI. For example, on:
    func foo(x: @unknown(x) Int) {}
    
    Before:
    test.swift:1:14: error: unknown attribute 'unknown'
    test.swift:1:25: error: expected ',' separator
    test.swift:1:25: error: unnamed parameters must be written with the empty name '_'
    test.swift:1:22: error: use of undeclared type 'x'
    
    Now, we just emit the first one:
    test.swift:1:14: error: unknown attribute 'unknown'
    func foo(x: @unknown(x) Int) {}
                 ^

--- lib/Parse/ParseDecl.cpp
+++ lib/Parse/ParseDecl.cpp
@@ -1249,8 +1249,11 @@ bool Parser::parseDeclAttribute(DeclAttributes &Attributes, SourceLoc AtLoc) {
     diagnose(Tok, diag::type_attribute_applied_to_decl);
   else
     diagnose(Tok, diag::unknown_attribute, Tok.getText());
-  // Recover by eating @foo when foo is not known.
+
+  // Recover by eating @foo(...) when foo is not known.
   consumeToken();
+  if (Tok.is(tok::l_paren))
+    skipSingle();
 
   return true;
 }
@@ -1312,15 +1315,16 @@ bool Parser::parseTypeAttribute(TypeAttributes &Attributes, bool justChecking) {
       }
     }
     
-    // Recover by eating @foo when foo is not known.
+    // Recover by eating @foo(...) when foo is not known.
     consumeToken();
-      
-    // Recovery by eating "@foo=bar" if present.
-    if (consumeIf(tok::equal)) {
-      if (Tok.is(tok::identifier) ||
-          Tok.is(tok::integer_literal) ||
-          Tok.is(tok::floating_literal))
-        consumeToken();
+    if (Tok.is(tok::l_paren) && getEndOfPreviousLoc() == Tok.getLoc()) {
+      ParserPosition LParenPosition = getParserPosition();
+      skipSingle();
+      // If we found '->', or 'throws' after paren, it's likely a parameter
+      // of function type.
+      if (Tok.isAny(tok::arrow, tok::kw_throws, tok::kw_rethrows,
+                    tok::kw_throw))
+        backtrackToPosition(LParenPosition);
     }
     return true;
   }
