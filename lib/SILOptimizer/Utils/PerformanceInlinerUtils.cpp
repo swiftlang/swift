265a4605a0e1341ea6c4d4c13440ed03e715727d
practicalswift <practicalswift@users.noreply.github.com>
2016-10-16 12:53:49 +0200

    [gardening] Fix typos.

--- lib/SILOptimizer/Utils/PerformanceInlinerUtils.cpp
+++ lib/SILOptimizer/Utils/PerformanceInlinerUtils.cpp
@@ -1,4 +1,4 @@
-//===--- PerformanceInlinerUtils.cpp - Peformance inliner utilities. ------===//
+//===--- PerformanceInlinerUtils.cpp - Performance inliner utilities. -----===//
 //
 // This source file is part of the Swift.org open source project
 //
