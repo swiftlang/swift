abaed64e2047415279f51b7d8c93931135c68c89
Michael Gottesman <mgottesman@apple.com>
2016-10-19 23:43:57 -0700

    [semantic-arc] Refactor the ownership model eliminator to use a visitor type.
    rdar://28685236

--- lib/SILOptimizer/Transforms/OwnershipModelEliminator.cpp
+++ lib/SILOptimizer/Transforms/OwnershipModelEliminator.cpp
@@ -26,6 +26,7 @@
 #include "swift/SILOptimizer/PassManager/Transforms.h"
 #include "swift/SIL/SILBuilder.h"
 #include "swift/SIL/SILFunction.h"
+#include "swift/SIL/SILVisitor.h"
 
 using namespace swift;
 
@@ -35,73 +36,93 @@ using namespace swift;
 
 namespace {
 
+struct OwnershipModelEliminatorVisitor
+    : SILInstructionVisitor<OwnershipModelEliminatorVisitor, bool> {
+  SILBuilder &B;
+
+  OwnershipModelEliminatorVisitor(SILBuilder &B) : B(B) {}
+
+  bool visitValueBase(ValueBase *V) { return false; }
+  bool visitLoadInst(LoadInst *LI);
+  bool visitStoreInst(StoreInst *SI);
+};
+
+} // end anonymous namespace
+
+bool OwnershipModelEliminatorVisitor::visitLoadInst(LoadInst *LI) {
+  auto Qualifier = LI->getOwnershipQualifier();
+
+  // If the qualifier is unqualified, there is nothing further to do
+  // here. Just return.
+  if (Qualifier == LoadOwnershipQualifier::Unqualified)
+    return false;
+
+  // Otherwise, we need to break down the load inst into its unqualified
+  // components.
+  B.setInsertionPoint(LI);
+  B.setCurrentDebugScope(LI->getDebugScope());
+  auto *UnqualifiedLoad = B.createLoad(LI->getLoc(), LI->getOperand());
+
+  // If we have a copy, insert a retain_value. All other copies do not require
+  // more work.
+  if (Qualifier == LoadOwnershipQualifier::Copy) {
+    B.createRetainValue(UnqualifiedLoad->getLoc(), UnqualifiedLoad,
+                        Atomicity::Atomic);
+  }
+
+  // Then remove the qualified load and use the unqualified load as the def of
+  // all of LI's uses.
+  LI->replaceAllUsesWith(UnqualifiedLoad);
+  LI->eraseFromParent();
+  return true;
+}
+
+bool OwnershipModelEliminatorVisitor::visitStoreInst(StoreInst *SI) {
+  auto Qualifier = SI->getOwnershipQualifier();
+
+  // If the qualifier is unqualified, there is nothing further to do
+  // here. Just return.
+  if (Qualifier == StoreOwnershipQualifier::Unqualified)
+    return false;
+
+  // Otherwise, we need to break down the store.
+  B.setInsertionPoint(SI);
+  B.setCurrentDebugScope(SI->getDebugScope());
+
+  if (Qualifier != StoreOwnershipQualifier::Assign) {
+    // If the ownership qualifier is not an assign, we can just emit an
+    // unqualified store.
+    B.createStore(SI->getLoc(), SI->getSrc(), SI->getDest());
+  } else {
+    // If the ownership qualifier is [assign], then we need to eliminate the
+    // old value.
+    //
+    // 1. Load old value.
+    // 2. Store new value.
+    // 3. Release old value.
+    auto *Old = B.createLoad(SI->getLoc(), SI->getDest());
+    B.createStore(SI->getLoc(), SI->getSrc(), SI->getDest());
+    B.createReleaseValue(SI->getLoc(), Old, Atomicity::Atomic);
+  }
+
+  // Then remove the qualified store.
+  SI->eraseFromParent();
+  return true;
+}
+
+//===----------------------------------------------------------------------===//
+//                           Top Level Entry Point
+//===----------------------------------------------------------------------===//
+
+namespace {
+
 struct OwnershipModelEliminator : SILFunctionTransform {
-
-  bool processLoad(SILBuilder &B, LoadInst *LI) {
-    auto Qualifier = LI->getOwnershipQualifier();
-
-    // If the qualifier is unqualified, there is nothing further to do
-    // here. Just return.
-    if (Qualifier == LoadOwnershipQualifier::Unqualified)
-      return false;
-
-    // Otherwise, we need to break down the load inst into its unqualified
-    // components.
-    B.setInsertionPoint(LI);
-    B.setCurrentDebugScope(LI->getDebugScope());
-    auto *UnqualifiedLoad = B.createLoad(LI->getLoc(), LI->getOperand());
-
-    // If we have a copy, insert a retain_value. All other copies do not require
-    // more work.
-    if (Qualifier == LoadOwnershipQualifier::Copy) {
-      B.createRetainValue(UnqualifiedLoad->getLoc(), UnqualifiedLoad,
-                          Atomicity::Atomic);
-    }
-
-    // Then remove the qualified load and use the unqualified load as the def of
-    // all of LI's uses.
-    LI->replaceAllUsesWith(UnqualifiedLoad);
-    LI->removeFromParent();
-    return true;
-  }
-
-  bool processStore(SILBuilder &B, StoreInst *SI) {
-    auto Qualifier = SI->getOwnershipQualifier();
-
-    // If the qualifier is unqualified, there is nothing further to do
-    // here. Just return.
-    if (Qualifier == StoreOwnershipQualifier::Unqualified)
-      return false;
-
-    // Otherwise, we need to break down the load.
-    B.setInsertionPoint(SI);
-    B.setCurrentDebugScope(SI->getDebugScope());
-
-    if (Qualifier != StoreOwnershipQualifier::Assign) {
-      // If the ownership qualifier is not an assign, we can just emit an
-      // unqualified store.
-      B.createStore(SI->getLoc(), SI->getSrc(), SI->getDest());
-    } else {
-      // If the ownership qualifier is [assign], then we need to eliminate the
-      // old value.
-      //
-      // 1. Load old value.
-      // 2. Store new value.
-      // 3. Release old value.
-      auto *Old = B.createLoad(SI->getLoc(), SI->getDest());
-      B.createStore(SI->getLoc(), SI->getSrc(), SI->getDest());
-      B.createReleaseValue(SI->getLoc(), Old, Atomicity::Atomic);
-    }
-
-    // Then remove the qualified store.
-    SI->removeFromParent();
-    return true;
-  }
-
   void run() override {
     bool MadeChange = false;
     SILFunction *F = getFunction();
     SILBuilder B(*F);
+    OwnershipModelEliminatorVisitor Visitor(B);
+
     for (auto &BB : *F) {
       for (auto II = BB.begin(), IE = BB.end(); II != IE;) {
         // Since we are going to be potentially removing instructions, we need
@@ -109,16 +130,7 @@ struct OwnershipModelEliminator : SILFunctionTransform {
         SILInstruction *I = &*II;
         ++II;
 
-        if (auto *LI = dyn_cast<LoadInst>(I)) {
-          MadeChange |= processLoad(B, LI);
-          continue;
-        }
-
-        auto *SI = dyn_cast<StoreInst>(I);
-        if (!SI)
-          continue;
-
-        MadeChange |= processStore(B, SI);
+        MadeChange |= Visitor.visit(I);
       }
     }
 
@@ -133,10 +145,6 @@ struct OwnershipModelEliminator : SILFunctionTransform {
 };
 
 } // end anonymous namespace
-
-//===----------------------------------------------------------------------===//
-//                           Top Level Entry Point
-//===----------------------------------------------------------------------===//
 
 SILTransform *swift::createOwnershipModelEliminator() {
   return new OwnershipModelEliminator();
