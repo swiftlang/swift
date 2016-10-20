87d22acb676cc03fae99b44daff800eb5f98b7c6
Xi Ge <xi_ge@apple.com>
2016-10-20 12:02:37 -0700

    [swift-api-digester] Ensure we exclude decls of low accessibility from the API comparison. (#5382)

--- test/api-digester/Inputs/cake.swift
+++ test/api-digester/Inputs/cake.swift
@@ -1,4 +1,11 @@
 public struct S1 {
   public func foo1() {}
   mutating public func foo2() {}
+  internal func foo3() {}
+  private func foo4() {}
+  fileprivate func foo5() {}
+}
+
+public class C1 {
+	open func foo1() {}
 }
