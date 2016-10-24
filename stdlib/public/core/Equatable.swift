d2851584fc88df54603c0ea09a00ff93e7247d50
Max Moiseev <moiseev@apple.com>
2016-10-24 11:40:37 -0700

    Operators from free functions to static methods

--- stdlib/public/core/Equatable.swift
+++ stdlib/public/core/Equatable.swift
@@ -165,19 +165,22 @@ public protocol Equatable {
   static func == (lhs: Self, rhs: Self) -> Bool
 }
 
-/// Returns a Boolean value indicating whether two values are not equal.
-///
-/// Inequality is the inverse of equality. For any values `a` and `b`, `a != b`
-/// implies that `a == b` is `false`.
-///
-/// This is the default implementation of the not-equal-to operator (`!=`)
-/// for any type that conforms to `Equatable`.
-///
-/// - Parameters:
-///   - lhs: A value to compare.
-///   - rhs: Another value to compare.
-public func != <T : Equatable>(lhs: T, rhs: T) -> Bool {
-  return !(lhs == rhs)
+extension Equatable {
+  /// Returns a Boolean value indicating whether two values are not equal.
+  ///
+  /// Inequality is the inverse of equality. For any values `a` and `b`, `a != b`
+  /// implies that `a == b` is `false`.
+  ///
+  /// This is the default implementation of the not-equal-to operator (`!=`)
+  /// for any type that conforms to `Equatable`.
+  ///
+  /// - Parameters:
+  ///   - lhs: A value to compare.
+  ///   - rhs: Another value to compare.
+  @_transparent
+  public static func != (lhs: Self, rhs: Self) -> Bool {
+    return !(lhs == rhs)
+  }
 }
 
 //===----------------------------------------------------------------------===//
