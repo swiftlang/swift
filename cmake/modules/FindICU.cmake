97d3fbaffbb7545337bf71266051cb73ce7eca07
K Staring <qdk@quickdekay.net>
2016-10-24 08:23:22 +0200

    change some variables to make it more clear - when comparing with swift/master - what exactly changed. In short: the first loop used ${MODULE} in uppercase when setting the INCLUDE paths, while the second loop used ${MODULE} in lowercase to retrieve the INCLUDE paths. This can't have worked. Also, there was a discrepancy for the first and second loop where *_INCLUDE_DIR vs *_INCLUDE_DIRS was used. Again, this cannot have worked (just try in the currenct swift/master code, surround the code with some message(STATUS ...) lines)

--- cmake/modules/FindICU.cmake
+++ cmake/modules/FindICU.cmake
@@ -5,34 +5,34 @@ include(FindPackageHandleStandardArgs)
 find_package(PkgConfig)
 
 set(ICU_REQUIRED)
-foreach(FIND_COMP_ITEM ${ICU_FIND_COMPONENTS})
-  string(TOUPPER "${FIND_COMP_ITEM}" UMODULE)
-  string(TOLOWER "${FIND_COMP_ITEM}" LMODULE)
+foreach(MODULE ${ICU_FIND_COMPONENTS})
+  string(TOUPPER "${MODULE}" MODULE)
+  string(TOLOWER "${MODULE}" module)
   list(APPEND ICU_REQUIRED 
-    ICU_${UMODULE}_INCLUDE_DIR ICU_${UMODULE}_LIBRARIES)
+    ICU_${MODULE}_INCLUDE_DIR ICU_${MODULE}_LIBRARIES)
 
-  pkg_check_modules(PC_ICU_${UMODULE} QUIET icu-${LMODULE})
-  if(${PC_ICU_${UMODULE}_FOUND})
-    set(ICU_${UMODULE}_DEFINITIONS ${PC_ICU_${UMODULE}_CFLAGS_OTHER})
+  pkg_check_modules(PC_ICU_${MODULE} QUIET icu-${module})
+  if(${PC_ICU_${MODULE}_FOUND})
+    set(ICU_${MODULE}_DEFINITIONS ${PC_ICU_${MODULE}_CFLAGS_OTHER})
 
-    find_path(ICU_${UMODULE}_INCLUDE_DIR unicode
-      HINTS ${PC_ICU_${UMODULE}_INCLUDEDIR} ${PC_ICU_${UMODULE}_INCLUDE_DIRS})
-    set(ICU_${UMODULE}_INCLUDE_DIRS ${ICU_${UMODULE}_INCLUDE_DIR})
+    find_path(ICU_${MODULE}_INCLUDE_DIR unicode
+      HINTS ${PC_ICU_${MODULE}_INCLUDEDIR} ${PC_ICU_${MODULE}_INCLUDE_DIRS})
+    set(ICU_${MODULE}_INCLUDE_DIRS ${ICU_${MODULE}_INCLUDE_DIR})
 
-    find_library(ICU_${UMODULE}_LIBRARY NAMES icu${LMODULE}
-      HINTS ${PC_ICU_${UMODULE}_LIBDIR} ${PC_ICU_${UMODULE}_LIBRARY_DIRS})
-    set(ICU_${UMODULE}_LIBRARIES ${ICU_${UMODULE}_LIBRARY})
+    find_library(ICU_${MODULE}_LIBRARY NAMES icu${module}
+      HINTS ${PC_ICU_${MODULE}_LIBDIR} ${PC_ICU_${MODULE}_LIBRARY_DIRS})
+    set(ICU_${MODULE}_LIBRARIES ${ICU_${MODULE}_LIBRARY})
   endif()
 endforeach()
 
 foreach(sdk ANDROID;FREEBSD;LINUX;WINDOWS)
-  foreach(ITEM ${ICU_FIND_COMPONENTS})
-    string(TOUPPER "${ITEM}" UMODULE)
-    if("${SWIFT_${sdk}_ICU_${UMODULE}_INCLUDE}" STREQUAL "")
-      set(SWIFT_${sdk}_ICU_${UMODULE}_INCLUDE ${ICU_${UMODULE}_INCLUDE_DIRS})
+  foreach(MODULE ${ICU_FIND_COMPONENTS})
+    string(TOUPPER "${MODULE}" MODULE)
+    if("${SWIFT_${sdk}_ICU_${MODULE}_INCLUDE}" STREQUAL "")
+      set(SWIFT_${sdk}_ICU_${MODULE}_INCLUDE ${ICU_${MODULE}_INCLUDE_DIRS})
     endif()
-    if("${SWIFT_${sdk}_ICU_${UMODULE}}" STREQUAL "")
-      set(SWIFT_${sdk}_ICU_${UMODULE} ${ICU_${UMODULE}_LIBRARY})
+    if("${SWIFT_${sdk}_ICU_${MODULE}}" STREQUAL "")
+      set(SWIFT_${sdk}_ICU_${MODULE} ${ICU_${MODULE}_LIBRARY})
     endif()
   endforeach()
 endforeach()
