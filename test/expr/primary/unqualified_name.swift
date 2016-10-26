47e07bc6112b4e04dce7687781f5af5d2bdbc914
Rintaro Ishizaki <fs.output@gmail.com>
2016-10-26 21:41:23 +0900

    [Parse] Create DiagnosticTransaction along with BacktrackingScope
    This prevents duplicated diagnostics.

--- test/expr/primary/unqualified_name.swift
+++ test/expr/primary/unqualified_name.swift
@@ -73,3 +73,14 @@ class C1 : C0 {
   }
 }
 
+struct S1 {
+  init(x: Int) {} // expected-note {{'init(x:)' declared here}}
+
+  func testS1() {
+    _ = S1.init(x:)(1)
+    _ = S1.init(`x`: 1)  // expected-warning {{keyword 'x' does not need to be escaped in argument list}} {{17-18=}} {{19-20=}}
+
+    // Test for unknown token.
+    _ = S1.init(x: 0xG) // expected-error {{expected a digit after integer literal prefix}} expected-error {{missing argument for parameter 'x' in call}}
+  }
+}
