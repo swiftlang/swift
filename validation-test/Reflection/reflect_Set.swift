eda76c8384892d0e524c993d9e0fca69ef7ecbeb
Slava Pestov <spestov@apple.com>
2016-11-09 18:36:01 -0800

    Reflection: Fix class layout start offset calculations
    Fixes <rdar://problem/29115967>.

--- validation-test/Reflection/reflect_Set.swift
+++ validation-test/Reflection/reflect_Set.swift
@@ -23,7 +23,7 @@ reflect(object: obj)
 // CHECK-64: (class reflect_Set.TestClass)
 
 // CHECK-64: Type info:
-// CHECK-64: (class_instance size=25 alignment=16 stride=32 num_extra_inhabitants=0
+// CHECK-64: (class_instance size=25 alignment=8 stride=32 num_extra_inhabitants=0
 // CHECK-64:   (field name=t offset=16
 // CHECK-64:     (struct size=9 alignment=8 stride=16 num_extra_inhabitants=0
 // CHECK-64:       (field name=_variantStorage offset=0
@@ -41,7 +41,7 @@ reflect(object: obj)
 // CHECK-32: (class reflect_Set.TestClass)
 
 // CHECK-32: Type info:
-// CHECK-32: (class_instance size=17 alignment=16 stride=32 num_extra_inhabitants=0
+// CHECK-32: (class_instance size=17 alignment=4 stride=20 num_extra_inhabitants=0
 // CHECK-32:   (field name=t offset=12
 // CHECK-32:     (struct size=5 alignment=4 stride=8 num_extra_inhabitants=0
 // CHECK-32:       (field name=_variantStorage offset=0
