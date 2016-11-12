a76c35a75217ca8675a352c65e587439ca280914
Alexis Beingessner <a.beingessner@gmail.com>
2016-11-11 19:17:11 -0500

    reenable collection reflection tests, with impl details ignored

--- validation-test/Reflection/reflect_String.swift
+++ validation-test/Reflection/reflect_String.swift
@@ -4,9 +4,6 @@
 // REQUIRES: objc_interop
 // REQUIRES: executable_test
 
-// FIXME(ABI): This test is too fragile while this type isn't ABI stable
-// REQUIRES: rdar29139967
-
 import SwiftReflectionTest
 
 class TestClass {
@@ -29,24 +26,7 @@ reflect(object: obj)
 // CHECK-64-NEXT: (class_instance size=40 alignment=8 stride=40
 // CHECK-64-NEXT:   (field name=t offset=16
 // CHECK-64-NEXT:     (struct size=24 alignment=8 stride=24 num_extra_inhabitants=0
-// CHECK-64-NEXT:       (field name=_core offset=0
-// CHECK-64-NEXT:         (struct size=24 alignment=8 stride=24 num_extra_inhabitants=0
-// CHECK-64-NEXT:           (field name=_baseAddress offset=0
-// CHECK-64-NEXT:             (single_payload_enum size=8 alignment=8 stride=8 num_extra_inhabitants=0
-// CHECK-64-NEXT:               (field name=some offset=0
-// CHECK-64-NEXT:                 (struct size=8 alignment=8 stride=8 num_extra_inhabitants=1
-// CHECK-64-NEXT:                   (field name=_rawValue offset=0
-// CHECK-64-NEXT:                     (builtin size=8 alignment=8 stride=8 num_extra_inhabitants=1))))))
-// CHECK-64-NEXT:           (field name=_countAndFlags offset=8
-// CHECK-64-NEXT:             (struct size=8 alignment=8 stride=8 num_extra_inhabitants=0
-// CHECK-64-NEXT:               (field name=_value offset=0
-// CHECK-64-NEXT:                 (builtin size=8 alignment=8 stride=8 num_extra_inhabitants=0))))
-// CHECK-64-NEXT:           (field name=_owner offset=16
-// CHECK-64-NEXT:             (single_payload_enum size=8 alignment=8 stride=8 num_extra_inhabitants=2147483646
-// CHECK-64-NEXT:               (field name=some offset=0
-// CHECK-64-NEXT:                 (class_existential size=8 alignment=8 stride=8 num_extra_inhabitants=2147483647
-// CHECK-64-NEXT:                   (field name=object offset=0
-// CHECK-64-NEXT:                     (reference kind=strong refcounting=unknown)))))))))))
+// (unstable implementation details omitted)
 
 // CHECK-32: Reflecting an object.
 // CHECK-32: Instance pointer in child address space: 0x{{[0-9a-fA-F]+}}
@@ -57,24 +37,7 @@ reflect(object: obj)
 // CHECK-32-NEXT: (class_instance size=24 alignment=4 stride=32
 // CHECK-32-NEXT:   (field name=t offset=12
 // CHECK-32-NEXT:     (struct size=12 alignment=4 stride=12 num_extra_inhabitants=0
-// CHECK-32-NEXT:       (field name=_core offset=0
-// CHECK-32-NEXT:         (struct size=12 alignment=4 stride=12 num_extra_inhabitants=0
-// CHECK-32-NEXT:           (field name=_baseAddress offset=0
-// CHECK-32-NEXT:             (single_payload_enum size=4 alignment=4 stride=4 num_extra_inhabitants=0
-// CHECK-32-NEXT:               (field name=some offset=0
-// CHECK-32-NEXT:                 (struct size=4 alignment=4 stride=4 num_extra_inhabitants=1
-// CHECK-32-NEXT:                   (field name=_rawValue offset=0
-// CHECK-32-NEXT:                     (builtin size=4 alignment=4 stride=4 num_extra_inhabitants=1))))))
-// CHECK-32-NEXT:           (field name=_countAndFlags offset=4
-// CHECK-32-NEXT:             (struct size=4 alignment=4 stride=4 num_extra_inhabitants=0
-// CHECK-32-NEXT:               (field name=_value offset=0
-// CHECK-32-NEXT:                 (builtin size=4 alignment=4 stride=4 num_extra_inhabitants=0))))
-// CHECK-32-NEXT:           (field name=_owner offset=8
-// CHECK-32-NEXT:             (single_payload_enum size=4 alignment=4 stride=4 num_extra_inhabitants=4095
-// CHECK-32-NEXT:               (field name=some offset=0
-// CHECK-32-NEXT:                 (class_existential size=4 alignment=4 stride=4 num_extra_inhabitants=4096
-// CHECK-32-NEXT:                   (field name=object offset=0
-// CHECK-32-NEXT:                     (reference kind=strong refcounting=unknown)))))))))))
+// (unstable implementation details omitted)
 
 doneReflecting()
 
