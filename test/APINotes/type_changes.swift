7c81c367423ad71eb09c457c377a28ab8fc74688
Doug Gregor <dgregor@apple.com>
2016-11-11 23:16:18 -0800

    [API Notes] Add tests for API notes that change types of Clang declarations.
    Testing for rdar://problem/28943642.

---
+++ test/APINotes/type_changes.swift
@@ -0,0 +1,8 @@
+// RUN: %target-parse-verify-swift -I %S/Inputs/custom-modules -F %S/Inputs/custom-frameworks
+import APINotesTest
+import APINotesFrameworkTest
+
+func testChangedTypes(tc: TypeChanges, a: A, i: Int) {
+  _ = tc.method(with: i) // expected-error{{cannot convert value of type 'Int' to expected argument type 'A?'}}
+  let _: Int = tc.method(with: a) // expected-error{{cannot convert value of type 'A' to specified type 'Int'}}
+}
