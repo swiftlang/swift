a1760161c56a3105a21d278ce6b842d2301d4875
Rintaro Ishizaki <fs.output@gmail.com>
2016-11-23 20:34:01 +0900

    [Parse] Don't parse 'throws' or 'rethrows' as identifiers
    It seems there is no reason to accept them.
    Why we want to accept `class C<throws> { ... }`?

---
+++ test/Compatibility/throws_identifier.swift
@@ -0,0 +1,14 @@
+// RUN: rm -rf %t && mkdir -p %t
+// RUN: %utils/split_file.py -o %t %s
+// RUN: %target-swift-frontend -parse -primary-file %t/swift3.swift -verify -swift-version 3
+// RUN: %target-swift-frontend -parse -primary-file %t/swift4.swift -verify -swift-version 4
+
+// 'throws' or 'rethrows' are allowed as an identifier in Swift 3.
+
+// BEGIN swift3.swift
+class C<throws> {}
+precedencegroup rethrows {}
+
+// BEGIN swift4.swift
+class C<throws> {} // expected-error {{expected an identifier to name generic parameter}}
+precedencegroup rethrows {} // expected-error {{expected identifier after 'precedencegroup'}}
