ddfe0c4b2af21daaa6f9f3889f18cd43865d8be6
practicalswift <practicalswift@users.noreply.github.com>
2016-11-30 18:58:06 +0100

    [gardening] Fix headers.

--- lib/Basic/Edit.cpp
+++ lib/Basic/Edit.cpp
@@ -1,4 +1,4 @@
-//===--- Edit.cpp - Misc edit utilities -----------------------===//
+//===--- Edit.cpp - Misc edit utilities -----------------------------------===//
 //
 // This source file is part of the Swift.org open source project
 //
