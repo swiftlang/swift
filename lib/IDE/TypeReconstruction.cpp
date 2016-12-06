2e2c8da5d445e03bd64771e3dcb06ecdbef13c52
Erik Eckstein <eeckstein@apple.com>
2016-12-06 15:34:47 -0800

    Mangling: handle new mangling suffix in type reconstruction

--- lib/IDE/TypeReconstruction.cpp
+++ lib/IDE/TypeReconstruction.cpp
@@ -15,6 +15,7 @@
 #include "swift/AST/GenericSignature.h"
 #include "swift/AST/Mangle.h"
 #include "swift/Basic/Mangler.h"
+#include "swift/Basic/ManglingMacros.h"
 #include "swift/AST/NameLookup.h"
 #include "swift/Basic/Demangle.h"
 #include "swift/ClangImporter/ClangImporter.h"
@@ -2164,7 +2165,8 @@ Decl *ide::getDeclFromUSR(ASTContext &context, StringRef USR,
   // This relies on USR generation being very close to symbol mangling; if we
   // need to support entities with customized USRs (e.g. extensions), we will
   // need to do something smarter here.
-  mangledName.replace(0, 2, "_T");
+  mangledName.replace(0, 2, NewMangling::useNewMangling() ?
+                             MANGLING_PREFIX_STR : "_T");
 
   return getDeclFromMangledSymbolName(context, mangledName, error);
 }
