712052bc9ae6a53502f2de35698f1329c8d86116
Pavel Yaskevich <xedin@apache.org>
2016-12-05 20:24:35 -0800

    [QoI] Mark inactive constraints as retired after removing
    This makes sure that removed constraints are returned back to the
    system after current run, otherwise only constraint graph would
    get them back since it has its own scope.

--- validation-test/compiler_crashers_fixed/28247-swift-constraints-constraintsystem-solverscope-solverscope.swift
+++ validation-test/compiler_crashers_fixed/28247-swift-constraints-constraintsystem-solverscope-solverscope.swift
@@ -5,5 +5,5 @@
 // See https://swift.org/LICENSE.txt for license information
 // See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
 
-// RUN: not --crash %target-swift-frontend %s -typecheck
+// RUN: not %target-swift-frontend %s -typecheck
 {func a(a)class a{var _=a(a{{a{
