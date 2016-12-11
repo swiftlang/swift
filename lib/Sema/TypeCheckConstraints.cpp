294f90a68b862f85e5f2c4e603553237c539895f
Mark Lacey <rudkx@icloud.com>
2016-12-10 21:32:56 -0700

    Re-apply: Cache types in the constraint system expression type map.
    I've been unable to reproduce the issue that hit on the builder, and
    still expect this change to be NFC, so trying it out again. If it
    fails, I'll revert again and try another shot at reproducing.
    
    Original commit message:
    
    We create new expressions that have the type on the expression
    set. Make sure we capture these types in the constraint system type
    map so that we can refer to types uniformally by consulting the map.
    
    NFC.
    
    (cherry picked from commit 57d5d974ff4d0922abba2e49f72de4e68578e2ab)

--- lib/Sema/TypeCheckConstraints.cpp
+++ lib/Sema/TypeCheckConstraints.cpp
@@ -2491,6 +2491,8 @@ bool TypeChecker::convertToType(Expr *&expr, Type type, DeclContext *dc,
   ConstraintSystem cs(*this, dc, ConstraintSystemFlags::AllowFixes);
   CleanupIllFormedExpressionRAII cleanup(Context, expr);
 
+  cs.cacheExprTypes(expr);
+
   // If there is a type that we're expected to convert to, add the conversion
   // constraint.
   cs.addConstraint(ConstraintKind::ExplicitConversion, expr->getType(), type,
