59ce1ff73c81065430dddeabdbfb17dfde21fd9e
Mark Lacey <mark.lacey@apple.com>
2016-12-12 23:51:14 -0800

    Add a function parameter to Expr::propagateLValueAccessKind.
    This parameter implements getType() for the given expression, making
    it possible to use this from within the constraint system, which now
    has it's own side map for types of expressions.

--- lib/Sema/ConstraintSystem.h
+++ lib/Sema/ConstraintSystem.h
@@ -1764,6 +1764,13 @@ public:
   /// \brief Determine if the type in question is AnyHashable.
   bool isAnyHashableType(Type t);
 
+  /// Call Expr::propagateLValueAccessKind on the given expression,
+  /// using a custom accessor for the type on the expression which
+  /// reads the type from the ConstraintSystem expression type map.
+  void propagateLValueAccessKind(Expr *E,
+                                 AccessKind accessKind,
+                                 bool allowOverwrite = false);
+
 private:
   /// Introduce the constraints associated with the given type variable
   /// into the worklist.
