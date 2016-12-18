15a24c111778a96089ccad00be316b5b4306ee36
Saleem Abdulrasool <compnerd@compnerd.org>
2016-12-17 16:38:34 -0800

    stdlib: include Glibc on cygwin
    cygwin uses glibc, so we should use the glibc module when working with
    cygwin.

--- stdlib/private/SwiftPrivateLibcExtras/SwiftPrivateLibcExtras.swift
+++ stdlib/private/SwiftPrivateLibcExtras/SwiftPrivateLibcExtras.swift
@@ -13,7 +13,7 @@
 import SwiftPrivate
 #if os(OSX) || os(iOS) || os(watchOS) || os(tvOS)
 import Darwin
-#elseif os(Linux) || os(FreeBSD) || os(PS4) || os(Android)
+#elseif os(Linux) || os(FreeBSD) || os(PS4) || os(Android) || CYGWIN
 import Glibc
 #endif
 
