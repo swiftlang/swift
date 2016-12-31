3313e5611ac51712ea4b345fdd9996c1744b003a
Xiaodi Wu <xiaodi.wu@gmail.com>
2016-12-30 23:17:26 -0500

    Zap copypasta

--- test/Sema/object_literals_osx.swift
+++ test/Sema/object_literals_osx.swift
@@ -1,4 +1,4 @@
-// RUN: %target-typecheck-verify-swift -swift-version 4 %s
+// RUN: %target-typecheck-verify-swift -swift-version 4
 // REQUIRES: OS=macosx
 
 struct S: _ExpressibleByColorLiteral {
--- test/Sema/object_literals_v3.swift
+++ test/Sema/object_literals_v3.swift
@@ -1,4 +1,4 @@
-// RUN: %target-typecheck-verify-swift -swift-version 3 %s
+// RUN: %target-typecheck-verify-swift -swift-version 3
 
 struct Path: _ExpressibleByFileReferenceLiteral {
   init(fileReferenceLiteralResourceName: String) {}
