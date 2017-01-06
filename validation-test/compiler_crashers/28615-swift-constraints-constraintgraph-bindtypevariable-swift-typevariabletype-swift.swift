d359668922bec6291c1631046a66896d2eab351a
practicalswift <practicalswift@users.noreply.github.com>
2017-01-06 11:51:39 +0100

    Add "// REQUIRES: OS=linux-gnu"

--- validation-test/compiler_crashers/28615-swift-constraints-constraintgraph-bindtypevariable-swift-typevariabletype-swift.swift
+++ validation-test/compiler_crashers/28615-swift-constraints-constraintgraph-bindtypevariable-swift-typevariabletype-swift.swift
@@ -5,5 +5,6 @@
 // See https://swift.org/LICENSE.txt for license information
 // See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
 
+// REQUIRES: OS=linux-gnu
 // RUN: not --crash %target-swift-frontend %s -emit-ir
 print(_===#keyPath(t>c
