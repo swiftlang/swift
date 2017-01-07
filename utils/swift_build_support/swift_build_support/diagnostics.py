8c2b87bce0c7c32af8c0fdc450e97c7dfa5b55ad
practicalswift <practicalswift@users.noreply.github.com>
2017-01-07 20:32:18 +0100

    [gardening] Add correct copyright notices

--- utils/swift_build_support/swift_build_support/diagnostics.py
+++ utils/swift_build_support/swift_build_support/diagnostics.py
@@ -2,7 +2,7 @@
 #
 # This source file is part of the Swift.org open source project
 #
-# Copyright (c) 2016 Apple Inc. and the Swift project authors
+# Copyright (c) 2014 - 2017 Apple Inc. and the Swift project authors
 # Licensed under Apache License v2.0 with Runtime Library Exception
 #
 # See https://swift.org/LICENSE.txt for license information
