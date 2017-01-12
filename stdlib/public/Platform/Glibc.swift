2daf136ab5cff42a933d8d0520ce8ee4134b20e2
Max Moiseev <moiseev@apple.com>
2017-01-12 14:21:14 -0800

    Deprecating M_PI and a few other constants in favor of better API
    Deprecating `M_PI`, `M_PI_2`, `M_PI_4`, `M_SQRT2`, `M_SQRT1_2`.
    Sugesting using `Double.pi` or even just `.pi` to allow type be inferred
    and avoid a type-cast.
    
    Fixes: <rdar://problem/26602190>

--- stdlib/public/Platform/Glibc.swift
+++ stdlib/public/Platform/Glibc.swift
@@ -14,3 +14,18 @@
 
 public let MAP_FAILED =
   UnsafeMutableRawPointer(bitPattern: -1)! as UnsafeMutableRawPointer!
+
+@available(swift, deprecated: 3.0, message: "Please use '.pi' to get the value of correct type and avoid casting.")
+public let M_PI = Double.pi
+
+@available(swift, deprecated: 3.0, message: "Please use '.pi / 2' to get the value of correct type and avoid casting.")
+public let M_PI_2 = Double.pi / 2
+
+@available(swift, deprecated: 3.0, message: "Please use '.pi / 4' to get the value of correct type and avoid casting.")
+public let M_PI_4 = Double.pi / 4
+
+@available(swift, deprecated: 3.0, message: "Please use '2.squareRoot()'.")
+public let M_SQRT2 = 2.0.squareRoot()
+
+@available(swift, deprecated: 3.0, message: "Please use '0.5.squareRoot()'.")
+public let M_SQRT1_2 = 0.5.squareRoot()
