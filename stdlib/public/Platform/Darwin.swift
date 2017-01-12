0f691a829bad6603ea71744dba70ffe715b9015b
Max Moiseev <moiseev@apple.com>
2017-01-12 15:16:36 -0800

    Removing SQRT2 and SQRT1_2 for now

--- stdlib/public/Platform/Darwin.swift
+++ stdlib/public/Platform/Darwin.swift
@@ -23,9 +23,3 @@ public let M_PI_2 = Double.pi / 2
 
 @available(swift, deprecated: 3.0, message: "Please use 'Double.pi / 4' or '.pi / 4' to get the value of correct type and avoid casting.")
 public let M_PI_4 = Double.pi / 4
-
-@available(swift, deprecated: 3.0, message: "Please use '2.squareRoot()'.")
-public let M_SQRT2 = 2.0.squareRoot()
-
-@available(swift, deprecated: 3.0, message: "Please use '0.5.squareRoot()'.")
-public let M_SQRT1_2 = 0.5.squareRoot()
--- test/MathConstants.swift
+++ test/MathConstants.swift
@@ -9,5 +9,3 @@ import Glibc
 _ = M_PI // expected-warning {{is deprecated}}
 _ = M_PI_2 // expected-warning {{is deprecated}}
 _ = M_PI_4 // expected-warning {{is deprecated}}
-_ = M_SQRT2 // expected-warning {{is deprecated}}
-_ = M_SQRT1_2 // expected-warning {{is deprecated}}
