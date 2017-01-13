a1c5a0b52836337d31bc3d2220daf7fedc64c769
Max Moiseev <moiseev@apple.com>
2017-01-13 10:40:28 -0800

    Deprecating M_SQRT2 and M_SQRT1_2
    Now that square root is being propoerly inlined and optimized away, it
    is safe to deprecate these two constants in favor of a Swiftier API.
    
    Fixes: <rdar://problem/30003973>

--- test/MathConstants.swift
+++ test/MathConstants.swift
@@ -9,3 +9,6 @@ import Glibc
 _ = M_PI // expected-warning {{is deprecated}}
 _ = M_PI_2 // expected-warning {{is deprecated}}
 _ = M_PI_4 // expected-warning {{is deprecated}}
+
+_ = M_SQRT2 // expected-warning {{is deprecated}}
+_ = M_SQRT1_2 // expected-warning {{is deprecated}}
