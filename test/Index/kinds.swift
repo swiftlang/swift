cfa1749836c83d93262eb03382181b227cec1d96
Argyrios Kyrtzidis <kyrtzidis@apple.com>
2017-01-24 08:58:51 -0800

    [index] Report @IB and @GKInspectable properties in the index info.

--- test/Index/kinds.swift
+++ test/Index/kinds.swift
@@ -196,3 +196,15 @@ class NonTestCase {
   func testMeNot() {}
   // CHECK: [[@LINE-1]]:8 | instance-method/Swift | testMeNot() |
 }
+
+@objc class TargetForIBAction {}
+// CHECK: [[@LINE-1]]:13 | class/Swift | TargetForIBAction | [[TargetForIBAction_USR:.*]] | Def |
+class AttrAnnots {
+  @IBOutlet var iboutletString: AnyObject?
+  // CHECK: [[@LINE-1]]:17 | instance-property(IB)/Swift | iboutletString |
+  @IBAction func someibaction(o: TargetForIBAction) {}
+  // CHECK: [[@LINE-1]]:18 | instance-method(IB)/Swift | someibaction(o:) | {{.*}} | Def,RelChild,RelIBType | rel: 2
+  // CHECK-NEXT: RelIBType | TargetForIBAction | [[TargetForIBAction_USR]]
+  @GKInspectable var gkString = "gk"
+  // CHECK: [[@LINE-1]]:22 | instance-property(GKI)/Swift | gkString |
+}
