9a8cba1579f96cf8e38b46a5fe741a5a0342b7d4
practicalswift <practicalswift@users.noreply.github.com>
2017-01-25 08:37:04 +0100

    Add "// REQUIRES: OS=linux-gnu"

--- validation-test/compiler_crashers/28666-conformingreplacementtype-is-substitutabletype-conformingreplacementtype-is-depe.swift
+++ validation-test/compiler_crashers/28666-conformingreplacementtype-is-substitutabletype-conformingreplacementtype-is-depe.swift
@@ -5,6 +5,7 @@
 // See https://swift.org/LICENSE.txt for license information
 // See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
 
+// REQUIRES: OS=linux-gnu
 // REQUIRES: asserts
 // RUN: not --crash %target-swift-frontend %s -emit-ir
 struct A:RangeReplaceableCollection{var f=max
