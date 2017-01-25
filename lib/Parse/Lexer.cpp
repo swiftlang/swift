d5a27a4307395941028919e023e8032b11d89886
David Farler <dfarler@apple.com>
2017-01-25 13:47:32 -0800

    Lexer: Don't split an operator '.<' from '.<#placeholder#>'
    '.<#placeholder#>' is actually an unresolved reference where the name is
    an editor placeholder, not the operator '.<' followed by #placeholder#>.
    
    rdar://problem/28457876

--- lib/Parse/Lexer.cpp
+++ lib/Parse/Lexer.cpp
@@ -678,6 +678,19 @@ static bool isRightBound(const char *tokEnd, bool isLeftBound,
   }
 }
 
+static bool rangeContainsPlaceholderEnd(const char *CurPtr,
+                                        const char *End) {
+  while (CurPtr++ < End - 1) {
+    if (*CurPtr== '\n') {
+      return false;
+    }
+    if (CurPtr[0] == '#' && CurPtr[1] == '>') {
+      return true;
+    }
+  }
+  return false;
+}
+
 /// lexOperatorIdentifier - Match identifiers formed out of punctuation.
 void Lexer::lexOperatorIdentifier() {
   const char *TokStart = CurPtr-1;
@@ -697,6 +710,10 @@ void Lexer::lexOperatorIdentifier() {
     // started with a '.'.
     if (*CurPtr == '.' && *TokStart != '.')
       break;
+    if (Identifier::isEditorPlaceholder(StringRef(CurPtr, 2)) &&
+        rangeContainsPlaceholderEnd(CurPtr + 2, BufferEnd)) {
+      break;
+    }
   } while (advanceIfValidContinuationOfOperator(CurPtr, BufferEnd));
 
   if (CurPtr-TokStart > 2) {
