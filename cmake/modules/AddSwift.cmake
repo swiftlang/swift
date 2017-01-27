038303b1b1263b00906037018d03946d633e3125
John McCall <rjmccall@apple.com>
2017-01-27 17:52:24 -0500

    Switch MetadataCache to use a global slab allocator.
    This seems to more than fix a performance regression that we
    detected on a metadata-allocation microbenchmark.
    
    A few months ago, I improved the metadata cache representation
    and changed the metadata allocation scheme to primarily use malloc.
    Previously, we'd been using malloc in the concurrent tree data
    structure but a per-cache slab allocator for the metadata itself.
    At the time, I was concerned about the overhead of per-cache
    allocators, since many metadata patterns see only a small number
    of instantiations.  That's still an important factor, so in the
    new scheme we're using a global allocator; but instead of using
    malloc for individual allocations, we're using a slab allocator,
    which should have better peak, single-thread performance, at the
    cost of not easily supporting deallocation.  Deallocation is
    only used for metadata when there's contention on the cache, and
    specifically only when there's contention for the same key, so
    leaking a little isn't the worst thing in the world.
    
    The initial slab is a 64K globally-allocated buffer.
    Successive slabs are 16K and allocated with malloc.
    
    rdar://28189496

--- cmake/modules/AddSwift.cmake
+++ cmake/modules/AddSwift.cmake
@@ -328,7 +328,7 @@ function(_add_variant_link_flags)
     RESULT_VAR_NAME result)
 
   if("${LFLAGS_SDK}" STREQUAL "LINUX")
-    list(APPEND result "-lpthread" "-ldl")
+    list(APPEND result "-lpthread" "-latomic" "-ldl")
   elseif("${LFLAGS_SDK}" STREQUAL "FREEBSD")
     list(APPEND result "-lpthread")
   elseif("${LFLAGS_SDK}" STREQUAL "CYGWIN")
