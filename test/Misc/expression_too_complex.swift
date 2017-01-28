b5d25cc92fabac4a872746f37b018dd2ddc37d39
Max Moiseev <moiseev@apple.com>
2017-01-27 17:49:32 -0800

    Fixing the tests (again)

--- test/Misc/expression_too_complex.swift
+++ test/Misc/expression_too_complex.swift
@@ -1,7 +1,3 @@
-// FIXME(integers): new integer protocols change the way + is resolved, which
-// breaks this test. <rdar://problem/29956472>
-// XFAIL: *
-
 // RUN: %target-typecheck-verify-swift -solver-memory-threshold 4000
 
 var x = [1, 2, 3, 4.5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18 ,19] // expected-error{{expression was too complex to be solved in reasonable time; consider breaking up the expression into distinct sub-expressions}}
