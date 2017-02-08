5c4185a33f8fd7347864ee4b246fc23da84985ec
Jacob Bandes-Storch <jacob@bandes-stor.ch>
2017-02-07 23:34:50 -0800

    [Parse] Fix crash in conditional compilation parsing

--- test/Parse/ConditionalCompilation/no_configuration_error1.swift
+++ test/Parse/ConditionalCompilation/no_configuration_error1.swift
@@ -2,6 +2,6 @@
 
 // With a space next to the '#if'
 #if 
-// expected-error@-1 {{expected a condition to follow #if}}
-class C {} //expected-error {{expected #else or #endif at end of conditional compilation block}}
+// expected-error@-1 {{incomplete condition in conditional compilation directive}}
+class C {}
 #endif  // expected-error {{unexpected conditional compilation block terminator}}
