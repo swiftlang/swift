e678f4979bad7b847593cca2a1d865c7a19654f2
Michael Gottesman <mgottesman@apple.com>
2017-02-09 22:02:42 -0800

    [gardening] Add swift license headers to all files in ./unittests/Basic/*.

--- unittests/Basic/OptionSetTest.cpp
+++ unittests/Basic/OptionSetTest.cpp
@@ -1,3 +1,15 @@
+//===--- OptionSetTest.cpp ------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2017 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
 #include "swift/Basic/OptionSet.h"
 #include "swift/Basic/Range.h"
 #include "swift/Basic/ValueEnumerator.h"
