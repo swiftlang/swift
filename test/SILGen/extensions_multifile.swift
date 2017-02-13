e561b774ec55e4bd6e24b2bcf2a88ac200f691df
Slava Pestov <spestov@apple.com>
2017-02-13 15:52:29 -0800

    SILGen: Fix corner-case in emission of extension initializer
    If the initializer is in a different file than the original
    type, and the original type contains a stored property that
    has both an initializer *and* a declared type, then the
    initializer expression will not have been type checked.
    
    So we must look at the type of pattern, which should still
    be correct.
    
    Fixes <https://bugs.swift.org/browse/SR-3942> and
    <rdar://problem/25705157>.

--- test/SILGen/extensions_multifile.swift
+++ test/SILGen/extensions_multifile.swift
@@ -1,10 +1,10 @@
 // RUN: %target-swift-frontend -emit-silgen -primary-file %s %S/Inputs/struct_with_initializer.swift -module-name extensions_multifile | tee /tmp/xxx | %FileCheck %s
 
-// CHECK-LABEL: sil hidden @_TFV20extensions_multifile12HasInitValueCfT1xSi_S0_ : $@convention(method) (Int, @thin HasInitValue.Type) -> HasInitValue {
+// CHECK-LABEL: sil hidden @_TFV20extensions_multifile12HasInitValueCfT1zSi_S0_ : $@convention(method) (Int, @thin HasInitValue.Type) -> @owned HasInitValue {
 // CHECK: function_ref @_TIvV20extensions_multifile12HasInitValue1xSii : $@convention(thin) () -> Int
 
 // CHECK-LABEL: sil hidden_external [transparent] @_TIvV20extensions_multifile12HasInitValue1xSii : $@convention(thin) () -> Int
 
 extension HasInitValue {
-  init(x: Int) {}
+  init(z: Int) {}
 }
