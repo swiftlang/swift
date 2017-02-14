1c64f04997ad532367a55348143bce4556d3d005
practicalswift <practicalswift@users.noreply.github.com>
2017-02-14 19:23:23 +0100

    [gardening] Fix header inconsistencies

--- lib/SIL/TransitivelyUnreachableBlocks.h
+++ lib/SIL/TransitivelyUnreachableBlocks.h
@@ -1,5 +1,4 @@
-//===--- TransitivelyUnreachableBlocksInfo.h
-//----------------------------------===//
+//===--- TransitivelyUnreachableBlocks.h ------------------------*- C++ -*-===//
 //
 // This source file is part of the Swift.org open source project
 //
