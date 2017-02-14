ab1fd78ffe799871d6bbf78dcfa5224d520b6e7c
Roman Levenstein <rlevenstein@apple.com>
2017-02-14 11:22:15 -0800

    [sil-devirtualizer] Introduce a helper method canDevirtualizeApply
    This predicate can be used to check if a given call can be devirtualized. One of the clients of this new API will be the inliner, which may want to check if a given method call becomes devirtualizable after inlining.

--- include/swift/SILOptimizer/Utils/Devirtualize.h
+++ include/swift/SILOptimizer/Utils/Devirtualize.h
@@ -46,6 +46,7 @@ typedef std::pair<ValueBase *, ApplySite> DevirtualizationResult;
 DevirtualizationResult tryDevirtualizeApply(FullApplySite AI);
 DevirtualizationResult tryDevirtualizeApply(FullApplySite AI,
                                             ClassHierarchyAnalysis *CHA);
+bool canDevirtualizeApply(FullApplySite AI, ClassHierarchyAnalysis *CHA);
 bool isNominalTypeWithUnboundGenericParameters(SILType Ty, SILModule &M);
 bool canDevirtualizeClassMethod(FullApplySite AI, SILType ClassInstanceType);
 SILFunction *getTargetClassMethod(SILModule &M, SILType ClassOrMetatypeType,
