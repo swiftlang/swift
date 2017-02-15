757125ab10d94873bd9e6a750594a8cff7b4c0b1
Vedant Kumar <vsk@apple.com>
2017-02-15 13:57:45 -0800

    [Coverage] Pass each prof name var to the lowering pass just once
    While invoking the instrprof_increment intrinsic, we used to pass the
    profile name variable by using a GEP instruction to get to the string
    constant. That prevented llvm from uniquing the names. Use a constexpr
    GEP to fix the issue.

--- test/IRGen/coverage.swift
+++ test/IRGen/coverage.swift
@@ -1,6 +1,8 @@
 // RUN: %target-swift-frontend -Xllvm -new-mangling-for-tests -assume-parsing-unqualified-ownership-sil %s -profile-generate -profile-coverage-mapping -emit-sil -o - | %FileCheck %s --check-prefix=SIL
 // RUN: %target-swift-frontend -Xllvm -new-mangling-for-tests -assume-parsing-unqualified-ownership-sil %s -profile-generate -profile-coverage-mapping -emit-ir -o - | %FileCheck %s --check-prefix=IR
 
+// IR-NOT: __llvm_coverage_names
+
 // SIL-DAG: sil hidden @_T08coverage2f1yyF
 // SIL-DAG: string_literal utf8 "{{.*}}coverage.swift:{{.*}}_T08coverage2f1yyF"
 // IR-DAG: @"__profn_{{.*}}coverage.swift:_T08coverage2f1yyF" {{.*}} c"{{.*}}coverage.swift:_T08coverage2f1yyF"
