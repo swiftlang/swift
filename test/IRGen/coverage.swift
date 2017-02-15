2fabfe0e962f5ac36786bfeb0e15a7e4f58ea6bb
Vedant Kumar <vsk@apple.com>
2017-02-15 14:16:02 -0800

    [Coverage] Prepare for an upcoming llvm fix (NFC)
    Temporarily loosen this test so I can cherry-pick an llvm fix that
    throws away dead profile name variables. I will tighten it up again when
    the llvm change lands.
    
    (cherry picked from commit 9c205c1728233ed91792356523ddcb4d9f543c64)
    
     Conflicts:
    	test/IRGen/coverage.swift

--- test/IRGen/coverage.swift
+++ test/IRGen/coverage.swift
@@ -5,17 +5,14 @@
 
 // SIL-DAG: sil hidden @_TF8coverage2f1FT_T_
 // SIL-DAG: string_literal utf8 "{{.*}}coverage.swift:{{.*}}_TF8coverage2f1FT_T_"
-// IR-DAG: @"__profn_{{.*}}coverage.swift:_TF8coverage2f1FT_T_" {{.*}} c"{{.*}}coverage.swift:_TF8coverage2f1FT_T_"
 internal func f1() {}
 
 // SIL-DAG: sil private @_TF8coverageP33_[[F2HASH:[_a-zA-Z0-9]+]]
 // SIL-DAG: string_literal utf8 "{{.*}}coverage.swift:_TF8coverageP33_[[F2HASH]]"
-// IR-DAG: @"__profn_{{.*}}coverage.swift:_TF8coverageP33_[[F2HASH:[_a-zA-Z0-9]+]]" {{.*}} c"{{.*}}coverage.swift:_TF8coverageP33_[[F2HASH]]"
 private func f2() {}
 
 // SIL-DAG: sil @_TF8coverage2f3FT_T_
 // SIL-DAG: string_literal utf8 "_TF8coverage2f3FT_T_"
-// IR-DAG: @__profn__TF8coverage2f3FT_T_ {{.*}} c"_TF8coverage2f3FT_T_"
 public func f3() {
   f1();
   f2();
