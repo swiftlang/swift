8c1ae56c7c3a2c967d9f9d2f6d867497d115a8f6
practicalswift <practicalswift@users.noreply.github.com>
2017-02-21 11:52:41 +0100

    [gardening] Add missing copyright header.

--- lib/Sema/Semantics.cpp
+++ lib/Sema/Semantics.cpp
@@ -1,3 +1,15 @@
+//===--- Semantics.cpp - Semantics manager --------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2017 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
 #include "swift/Sema/Semantics.h"
 #include "swift/AST/Expr.h"
 #include "swift/AST/Decl.h"
