5f8a8944f21892c8fb5135bcca8e6e838878eff6
Michael Gottesman <mgottesman@apple.com>
2017-02-21 12:33:19 -0800

    [upsteam-update] Remove upstream removed clang::BuiltinType::OCLNDRange.
    This is an opencl type that we did not use in the compiler anyways. Our only use
    was to make sure that we had covered switches on clang builtin types (and
    asserted if we ever hit that case in a switch).

--- lib/IRGen/GenCall.cpp
+++ lib/IRGen/GenCall.cpp
@@ -655,7 +655,6 @@ namespace {
       case clang::BuiltinType::OCLEvent:
       case clang::BuiltinType::OCLClkEvent:
       case clang::BuiltinType::OCLQueue:
-      case clang::BuiltinType::OCLNDRange:
       case clang::BuiltinType::OCLReserveID:
         llvm_unreachable("OpenCL type in ABI lowering");
 
