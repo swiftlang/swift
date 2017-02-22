1a8ba93e59d04f56b2eb69fe52f85f546b57e7af
Slava Pestov <spestov@apple.com>
2017-02-21 20:40:45 -0800

    Sema: Refactor getTypeOfMemberReference() for generic subscripts
    Subscripts in generic context will soon have a
    GenericFunctionType, so get ready to handle that.

--- lib/Sema/TypeCheckExpr.cpp
+++ lib/Sema/TypeCheckExpr.cpp
@@ -591,10 +591,17 @@ Type TypeChecker::getUnopenedTypeOfReference(ValueDecl *value, Type baseType,
       // Subscript decls have function type.  For the purposes of later type
       // checker consumption, model this as returning an lvalue.
       assert(isa<SubscriptDecl>(storage));
-      auto *RFT = requestedType->castTo<FunctionType>();
+      auto *RFT = requestedType->castTo<AnyFunctionType>();
       return FunctionType::get(RFT->getInput(),
                                LValueType::get(RFT->getResult()),
                                RFT->getExtInfo());
+    }
+
+    // FIXME: Fix downstream callers.
+    if (auto *genericFn = requestedType->getAs<GenericFunctionType>()) {
+      return FunctionType::get(genericFn->getInput(),
+                               genericFn->getResult(),
+                               genericFn->getExtInfo());
     }
   }
 
