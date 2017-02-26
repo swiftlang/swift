412c71d8c246c6089db8f78d1dbfeb1861e8f560
Slava Pestov <spestov@apple.com>
2017-02-25 16:03:19 -0800

    Add some crashers
    One is already fixed so let's make sure it won't regress again; the
    other two are pending some improvements to GenericSignatureBuilder.

---
+++ validation-test/compiler_crashers_2/0076-sr3500.swift
@@ -0,0 +1,12 @@
+// RUN: not --crash %target-swift-frontend %s -emit-ir
+
+protocol A {
+  associatedtype Coordinate: Strideable
+  func doSomething(_: Range<Coordinate>) -> Coordinate.Stride
+}
+
+extension A where Coordinate == Int {
+  func extensionFunc(_ range: Range<Coordinate>) {
+    _ = doSomething(range)
+  }
+}
