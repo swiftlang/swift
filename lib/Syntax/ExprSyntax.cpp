57fa66e9fed76e000d0adf95a2b16dcf334a4794
practicalswift <practicalswift@users.noreply.github.com>
2017-02-28 17:15:39 +0100

    [gardening] Fix inconsistent headers

--- lib/Syntax/ExprSyntax.cpp
+++ lib/Syntax/ExprSyntax.cpp
@@ -1,4 +1,4 @@
-//===--- ExprSyntax.cpp - Swift Expression Syntax Impl. ---------*- C++ -*-===//
+//===--- ExprSyntax.cpp - Swift Expression Syntax Impl. -------------------===//
 //
 // This source file is part of the Swift.org open source project
 //
