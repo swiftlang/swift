06eadd7fe67a9b855ea88a2a3c445cff4c877817
therealbnut <andrew.charles.bennett@gmail.com>
2017-03-05 21:12:52 +1100

    clean up test

---
+++ test/expr/primary/literal/string.swift
@@ -0,0 +1,35 @@
+// RUN: %target-typecheck-verify-swift
+
+struct CustomString<T> : ExpressibleByStringLiteral {
+  init(stringLiteral value: T) {}
+}
+struct CustomExtendedGraphemeCluster<T> : ExpressibleByExtendedGraphemeCluster {
+  init(extendedGraphemeClusterLiteral value: T) {}
+}
+struct CustomUnicodeScalar<T> : ExpressibleByUnicodeScalarLiteral {
+  init(unicodeScalarLiteral value: T) {}
+}
+
+func verify<T>(stringLiteral value: CustomString<T>, with: T.Type) {}
+func verify<T>(
+  extendedGraphemeClusterLiteral value: CustomExtendedGraphemeCluster<T>, 
+  with: T.Type) {
+}
+func verify<T>(
+  unicodeScalarLiteral value: CustomUnicodeScalar<T>, 
+  with: T.Type) {
+}
+
+verify(unicodeScalarLiteral: "å", with: UnicodeScalar.self)
+verify(unicodeScalarLiteral: "ß", with: Character.self)
+verify(unicodeScalarLiteral: "c", with: String.self)
+verify(unicodeScalarLiteral: "∂", with: StaticString.self)
+
+verify(extendedGraphemeClusterLiteral: "a", with: Character.self)
+verify(extendedGraphemeClusterLiteral: "❄︎", with: String.self)
+verify(extendedGraphemeClusterLiteral: "김", with: StaticString.self)
+
+verify(stringLiteral: "abc", with: String.self)
+verify(stringLiteral: "∂éƒg", with: StaticString.self)
+
+test this is compiled
