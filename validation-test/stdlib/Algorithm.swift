bd42a6714a9ba73c0c66187c6bf7c647f35a17c7
Nate Cook <natecook@gmail.com>
2017-03-07 10:38:51 -0600

    Update tests for de-gybbed sort

--- validation-test/stdlib/Algorithm.swift
+++ validation-test/stdlib/Algorithm.swift
@@ -229,7 +229,7 @@ Algorithm.test("sort3/simple")
     [1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]
   ]) {
     var input = $0
-    _sort3(&input, 0, 1, 2)
+    _sort3(&input, 0, 1, 2, by: <)
     expectEqual([1, 2, 3], input)
 }
 
