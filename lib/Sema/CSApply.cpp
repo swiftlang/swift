3f8e1fec4ac9bf8561dd3c2206281a9afde306b8
Mark Lacey <mark.lacey@apple.com>
2017-03-10 16:51:03 -0800

    [Constraint solver] Cache a the types of a few AST nodes we create.
    Another small step toward moving to using the side map for all types in
    the constraint solver.

--- lib/Sema/CSApply.cpp
+++ lib/Sema/CSApply.cpp
@@ -1519,18 +1519,20 @@ namespace {
       auto bridgeFnTy = FunctionType::get(valueParenTy, bridgeTy);
       // FIXME: Wrap in ParenExpr to prevent bogus "tuple passed as argument"
       // errors.
-      auto valueParen = new (tc.Context) ParenExpr(SourceLoc(),
-                                                   value,
-                                                   SourceLoc(),
-                                                   /*trailing closure*/ false,
-                                                   valueParenTy);
+      auto valueParen = cs.cacheType(
+          new (tc.Context) ParenExpr(SourceLoc(),
+                                     value,
+                                     SourceLoc(),
+                                     /*trailing closure*/ false,
+                                     valueParenTy));
 
       valueParen->setImplicit();
-      auto fnRef = new (tc.Context) DeclRefExpr(bridgeAnythingRef,
-                                                DeclNameLoc(),
-                                                /*implicit*/ true,
-                                                AccessSemantics::Ordinary,
-                                                bridgeFnTy);
+      auto fnRef = cs.cacheType(
+          new (tc.Context) DeclRefExpr(bridgeAnythingRef,
+                                       DeclNameLoc(),
+                                       /*implicit*/ true,
+                                       AccessSemantics::Ordinary,
+                                       bridgeFnTy));
 
       auto getType = [&](const Expr *E) -> Type {
         return cs.getType(E);
@@ -1629,13 +1631,14 @@ namespace {
 
       // Form the arguments.
       Expr *args[2] = {
-        object,
-        new (tc.Context) DotSelfExpr(
-                             TypeExpr::createImplicitHack(object->getLoc(),
-                                                          valueType,
-                                                          tc.Context),
-                             object->getLoc(), object->getLoc(),
-                             MetatypeType::get(valueType))
+        cs.cacheType(object),
+        cs.cacheType(
+            new (tc.Context) DotSelfExpr(
+                TypeExpr::createImplicitHack(object->getLoc(),
+                                             valueType,
+                                             tc.Context),
+                object->getLoc(), object->getLoc(),
+                MetatypeType::get(valueType)))
       };
       args[1]->setImplicit();
 
