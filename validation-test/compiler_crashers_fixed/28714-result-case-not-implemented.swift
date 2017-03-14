6543405e2dee0cf3c48269fa1a9f89b1f0590550
Robert Widmann <devteam.codafi@gmail.com>
2017-03-14 00:05:45 -0400

    [SR-4219] Resolve non-deterministic compiler crashes
    These were leftover from the #8059, which plugged a type variable leak
    caused by type checking invalid default argument expressions.

--- validation-test/compiler_crashers_fixed/28714-result-case-not-implemented.swift
+++ validation-test/compiler_crashers_fixed/28714-result-case-not-implemented.swift
@@ -5,7 +5,5 @@
 // See https://swift.org/LICENSE.txt for license information
 // See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
 
-// REQUIRES: deterministic-behavior
-// REQUIRES: asserts
-// RUN: not --crash %target-swift-frontend %s -emit-ir
-{P{}func b(UInt=1 + 1 as?Int){
+// RUN: not %target-swift-frontend %s -emit-ir
+{extension{func a(UInt=1 + 1 as?Int){a
