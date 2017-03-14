56c1e2310ead22d3b10fc5d0cfcd98432eddcbb3
David Ronnqvist <david.ronnqvist@skype.net>
2017-03-15 00:00:41 +0100

    [SR-2409] Rephrase diagnostic to consider Objective-C interop
    When Objective-C interop is enabled, modules will continue to be referred to as “Objective-C modules”. But when interop is disabled, they will be referred to as “C modules”.
    
    Updated the existing test for the current behavior that doesn’t expect to fail on linux and doesn’t require objc_interop, so that it checks to different diagnostics on the two systems.

--- test/ClangImporter/non-modular-include.swift
+++ test/ClangImporter/non-modular-include.swift
@@ -1,8 +1,9 @@
 // RUN: rm -rf %t && mkdir -p %t
-// RUN: not %target-swift-frontend -typecheck %s -I %S/Inputs/non-modular -F %S/Inputs/non-modular 2>&1 | %FileCheck %s
+// RUN: not %target-swift-frontend -typecheck %s -I %S/Inputs/non-modular -F %S/Inputs/non-modular 2>&1 | %FileCheck --check-prefix=CHECK --check-prefix=CHECK-%target-os %s
 
 // CHECK: {{.+}}/non-modular/Foo.framework/Headers/Foo.h:1:9: error: include of non-modular header inside framework module 'Foo'
-// CHECK: error: could not build Objective-C module 'Foo'
+// CHECK-macosx: error: could not build Objective-C module 'Foo'
+// CHECK-linux: error: could not build C module 'Foo'
 // CHECK-NOT: error
 
 import Foo
