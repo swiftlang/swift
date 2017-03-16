1a2703307ee42318ccbe729cb99cf2562bb22bd8
TheOtherDave <davesweeris@mac.com>
2017-03-15 17:54:32 -0700

    Add files via upload
    "@objc is not supported within extensions of generic classes, or classes that inherit from generic classes"

--- test/decl/ext/extension-generic-objc.swift
+++ test/decl/ext/extension-generic-objc.swift
@@ -2,18 +2,46 @@
 
 import Foundation
 
-class X<T> : NSObject {
-    init(x: ()) {
+// Tests for correct detection of the "objc_in_generic_extension" error,
+// declared in {Swift Source}/include/swift/AST/DiagnosticsSema.def
+
+// Test "0 levels" deep
+class A<T> : NSObject {
+    init(a: ()) {
         super.init()
     }
 }
+extension A {
+    // This should throw an error
+    @objc func a1() {} // expected-error{{@objc is not supported within extensions of generic classes, or classes that inherit from generic classes}}
+    // This should *not* throw an error
+    func a2() {}
+}
 
-class Y : X<Int> {
-    init(y: ()) {
-        super.init(x: ())
+// Test "1 level" deep
+class B : A<Int> {
+    init(b: ()) {
+        super.init(a: ())
     }
 }
-
-extension Y {
-    @objc func f() {} // expected-error{{@objc is not supported within extensions of generic classes, or classes that inherit from generic classes}}
+extension B {
+    // This should throw an error
+    @objc func b1() {} // expected-error{{@objc is not supported within extensions of generic classes, or classes that inherit from generic classes}}
+    // This should *not* throw an error
+    func b2() {}
 }
+
+// Test "many levels" deep
+class C : B {}
+class D : C {
+    init(d: ()) {
+        super.init(b: ())
+    }
+}
+extension D {
+    // This should throw an error
+    @objc func d1() {} // expected-error{{@objc is not supported within extensions of generic classes, or classes that inherit from generic classes}}
+    // This should *not* throw an error
+    func d2() {}
+}
+
