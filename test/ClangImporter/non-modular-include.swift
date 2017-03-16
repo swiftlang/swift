d7c1f57184c7b0ee39250248d2388d5cd55f96ec
David Rönnqvist <david.ronnqvist@skype.net>
2017-03-16 13:10:10 +0100

    Conditional checks using %target-runtime instead of %target-os

--- test/ClangImporter/non-modular-include.swift
+++ test/ClangImporter/non-modular-include.swift
@@ -1,9 +1,9 @@
 // RUN: rm -rf %t && mkdir -p %t
-// RUN: not %target-swift-frontend -typecheck %s -I %S/Inputs/non-modular -F %S/Inputs/non-modular 2>&1 | %FileCheck --check-prefix=CHECK --check-prefix=CHECK-%target-os %s
+// RUN: not %target-swift-frontend -typecheck %s -I %S/Inputs/non-modular -F %S/Inputs/non-modular 2>&1 | %FileCheck --check-prefix=CHECK --check-prefix=CHECK-%target-runtime %s
 
 // CHECK: {{.+}}/non-modular/Foo.framework/Headers/Foo.h:1:9: error: include of non-modular header inside framework module 'Foo'
-// CHECK-macosx: error: could not build Objective-C module 'Foo'
-// CHECK-linux-gnu: error: could not build C module 'Foo'
+// CHECK-objc: error: could not build Objective-C module 'Foo'
+// CHECK-native: error: could not build C module 'Foo'
 // CHECK-NOT: error
 
 import Foo
