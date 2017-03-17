732ae1b2c2873ab4556ab0c2dad4e476a3296339
David Hart <david@hartbit.com>
2017-03-17 07:37:44 +0100

    Removed requires assert

--- validation-test/compiler_crashers_fixed/28707-false-encountered-error-in-diagnostic-text.swift
+++ validation-test/compiler_crashers_fixed/28707-false-encountered-error-in-diagnostic-text.swift
@@ -5,7 +5,6 @@
 // See https://swift.org/LICENSE.txt for license information
 // See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
 
-// REQUIRES: asserts
 // RUN: not %target-swift-frontend %s -emit-ir
 class a{@_versioned
 public
