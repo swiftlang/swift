c17cfb69d8d0bfa0b74fb2c03f99949bafb73b06
Greg Parker <gparker@apple.com>
2017-03-16 21:11:58 -0700

    Revert "SILGen: Add experimental TSan instrumentation for inout accesses. (#7736)"
    This reverts commit 52d5178a3e3c9e7edab093efd2c3cc8334ae5012.

--- lib/SILGen/LValue.h
+++ lib/SILGen/LValue.h
@@ -156,9 +156,7 @@ public:
   /// base value.
   virtual AccessKind getBaseAccessKind(SILGenFunction &SGF,
                                        AccessKind accessKind) const = 0;
-
-  virtual bool isRValue() const { return false; }
-
+  
   /// Returns the logical type-as-rvalue of the value addressed by the
   /// component.  This is always an object type, never an address.
   SILType getTypeOfRValue() const { return TypeData.TypeOfRValue; }
