aa5795f1a710db74af3fdef1a4cff21934a536d0
Dave Abrahams <dabrahams@apple.com>
2017-03-18 14:11:05 -0700

    [stdlib] Try a less-aggressive cleanup
    [SR-4285](https://bugs.swift.org/browse/SR-4285) seems to get in the way

--- stdlib/public/core/BidirectionalCollection.swift
+++ stdlib/public/core/BidirectionalCollection.swift
@@ -101,6 +101,25 @@ public protocol BidirectionalCollection
   // This is dependent on both recursive protocol constraints AND associated 
   // types with where clauses.
   // associatedtype Indices : BidirectionalCollection
+
+  /// The indices that are valid for subscripting the collection, in ascending
+  /// order.
+  ///
+  /// A collection's `indices` property can hold a strong reference to the
+  /// collection itself, causing the collection to be non-uniquely referenced.
+  /// If you mutate the collection while iterating over its indices, a strong
+  /// reference can cause an unexpected copy of the collection. To avoid the
+  /// unexpected copy, use the `index(after:)` method starting with
+  /// `startIndex` to produce indices instead.
+  ///
+  ///     var c = MyFancyCollection([10, 20, 30, 40, 50])
+  ///     var i = c.startIndex
+  ///     while i != c.endIndex {
+  ///         c[i] /= 5
+  ///         i = c.index(after: i)
+  ///     }
+  ///     // c == MyFancyCollection([2, 4, 6, 8, 10])
+  var indices: Indices { get }
   
   // TODO: swift-3-indexing-model: tests.
   /// The last element of the collection.
@@ -293,4 +312,3 @@ extension BidirectionalCollection {
     return self[start..<endIndex]
   }
 }
-
