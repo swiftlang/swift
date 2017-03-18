c0128c1da2a546c69e2ae19c2333f75f41015de4
Graydon Hoare <ghoare@apple.com>
2017-03-17 17:32:59 -0700

    [Metrics] Add sys::ResourceStats for tracking rusage from subprocesses.

--- include/swift/Basic/Statistic.h
+++ include/swift/Basic/Statistic.h
@@ -1,8 +1,8 @@
-//===--- Statistic.h - Helpers for llvm::Statistic --------------*- C++ -*-===//
+//===--- Statistic.h - Helpers for gathering stats --------------*- C++ -*-===//
 //
 // This source file is part of the Swift.org open source project
 //
-// Copyright (c) 2014 - 2017 Apple Inc. and the Swift project authors
+// Copyright (c) 2016 - 2017 Apple Inc. and the Swift project authors
 // Licensed under Apache License v2.0 with Runtime Library Exception
 //
 // See https://swift.org/LICENSE.txt for license information
@@ -21,5 +21,29 @@
       {DEBUG_TYPE, __func__, __func__, {0}, false};                     \
     ++FStat;                                                            \
   } while (0)
+
+namespace swift {
+namespace sys {
+
+/// A JSONSerialization-friendly summary of resource-usage data we're interested
+/// in from self and subprocesses.
+///
+/// Note: we do not use llvm::TimeRecord here for a few reasons:
+///
+///    1. It only measures own-process times, not subprocesses. We want both, so
+///       the code to produce it is sunk into swift::sys::TaskQueue anyway.
+///
+///    2. Its memory-measuring code is controlled by an option we don't want to
+///       be controlled-by
+///
+///    3. It measures the wrong thing anyways (malloc heap, not peak RSS)
+struct ResourceStats {
+  int64_t UserTimeUsec;       ///< User time elapsed in microseconds.
+  int64_t SystemTimeUsec;     ///< System time elapsed in microseconds.
+  int64_t MaxResidentBytes;   ///< Peak resident-set / working-set size.
+};
+
+} // namespace sys
+} // namespace swift
 
 #endif // SWIFT_BASIC_STATISTIC_H
