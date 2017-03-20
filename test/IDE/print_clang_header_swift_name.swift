1432f8492922f41c5353c29a6b258d0f4dffa9a5
Jordan Rose <jordan_rose@apple.com>
2017-03-20 11:48:50 -0700

    [ASTPrinter] Stop skipping unavailable-in-Swift-version decls. (#8181)
    We already have an option to skip /all/ unavailable decls. Singling
    out the ones that are specifically unavailable-in-Swift just makes
    testing harder.
    
    This will not affect interface generation in Xcode, which sets that
    option to skip all unavailable decls.

--- test/IDE/print_clang_header_swift_name.swift
+++ test/IDE/print_clang_header_swift_name.swift
@@ -1,6 +1,6 @@
 // RUN: echo '#include "print_clang_header_swift_name.h"' > %t.m
 // RUN: %target-swift-ide-test -source-filename %s -print-header -header-to-print \
-// RUN:     %S/Inputs/print_clang_header_swift_name.h --cc-args %target-cc-options \
+// RUN:     %S/Inputs/print_clang_header_swift_name.h -skip-unavailable --cc-args %target-cc-options \
 // RUN:     -isysroot %clang-importer-sdk-path -fsyntax-only %t.m -I %S/Inputs | %FileCheck %s
 
 // REQUIRES: objc_interop
