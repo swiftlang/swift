a3a290eb607e240e1644fb8733d760a62fb9a07d
Slava Pestov <spestov@apple.com>
2017-03-20 23:42:05 -0700

    Mangling: Fix bug in the logic for dropping same-type constraints
    It's not correct to drop a constraint if one of the two types
    structurally contains generic parameters at the method depth.

--- validation-test/compiler_crashers_2_fixed/0083-rdar31163470-1.swift
+++ validation-test/compiler_crashers_2_fixed/0083-rdar31163470-1.swift
@@ -1,5 +1,4 @@
-// RUN: not --crash %target-swift-frontend -primary-file %s -emit-ir
-// REQUIRES: asserts
+// RUN: %target-swift-frontend -primary-file %s -emit-ir
 
 struct First<T> {}
 struct Second<T> {}
