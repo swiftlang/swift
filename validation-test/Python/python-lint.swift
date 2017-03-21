fbad5fdfa68e5b55a86dfb70a7bdb275edda888f
Hugh Bellamy <hughbellars@gmail.com>
2017-03-21 19:28:31 +0700

    Integrate python linting into validation tests

---
+++ validation-test/Python/python-lint.swift
@@ -0,0 +1,6 @@
+// RUN: %{python} %utils/python-lint
+// Continuous integration for the OS X Platform also runs the tests in the iPhone, Apple TV and Apple Watch simulators.
+// We only need to run python linting once per OSX Platform test run, rather than once for each supported Apple device.
+// UNSUPPORTED: OS=watchos
+// UNSUPPORTED: OS=ios
+// UNSUPPORTED: OS=tvos
