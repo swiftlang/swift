0d49529395324c202d33416b777720c088cce477
Jordan Rose <jordan_rose@apple.com>
2017-03-24 16:00:50 -0700

    [ClangImporter] Make sure fake locations are always in /some/ buffer. (#8303)
    We synthesize fake source locations for module imports that come from
    Swift code; these locations have to be both valid and distinct for
    Clang to use. We've mostly been getting away with simply making fake
    offsets from the main file, but after the offsets start exceeding the
    size of the buffer they start pointing into some /other/ file, and
    then if Clang's SourceManager tries to order those locations it gets
    /very/ confused.
    
    This commit fixes that by allocating a 256K buffer of zeros and using
    offsets into that instead. The hope is that a read-only mmap'd buffer
    of zeros that never gets read (except possibly to look for newlines)
    will be cheap to allocate.
    
    (Why not just make the main file buffer 256K? Because we actually try
    to parse that, and there's really no reason for the lexer to go crawl
    through that file eagerly.)
    
    This test case isn't the best because it doesn't actually fail in the
    old code, but if only the assertion was added we at least hit that.
    I did verify with the reproducing project I have that we no longer
    hit this issue.
    
    rdar://problem/30924269

---
+++ test/ClangImporter/diags-with-many-imports.swift
@@ -0,0 +1,110 @@
+// RUN: not %target-swift-frontend -typecheck %s -I %S/Inputs/many-imports -import-objc-header %S/Inputs/many-imports/obsoleted.h 2>&1 | %FileCheck %s
+// RUN: %target-swift-frontend -emit-pch %S/Inputs/many-imports/obsoleted.h -o %t.pch
+// RUN: not %target-swift-frontend -typecheck %s -I %S/Inputs/many-imports -import-objc-header %t.pch 2>&1 | %FileCheck %s
+
+import Module1
+import Module2
+import Module3
+import Module4
+import Module5
+import Module6
+import Module7
+import Module8
+import Module9
+import Module10
+import Module11
+import Module12
+import Module13
+import Module14
+import Module15
+import Module16
+import Module17
+import Module18
+import Module19
+import Module20
+import Module21
+import Module22
+import Module23
+import Module24
+import Module25
+import Module26
+import Module27
+import Module28
+import Module29
+import Module30
+import Module31
+import Module32
+import Module33
+import Module34
+import Module35
+import Module36
+import Module37
+import Module38
+import Module39
+import Module40
+import Module41
+import Module42
+import Module43
+import Module44
+import Module45
+import Module46
+import Module47
+import Module48
+import Module49
+import Module50
+import Module51
+import Module52
+import Module53
+import Module54
+import Module55
+import Module56
+import Module57
+import Module58
+import Module59
+import Module60
+import Module61
+import Module62
+import Module63
+import Module64
+import Module65
+import Module66
+import Module67
+import Module68
+import Module69
+import Module70
+import Module71
+import Module72
+import Module73
+import Module74
+import Module75
+import Module76
+import Module77
+import Module78
+import Module79
+import Module80
+import Module81
+import Module82
+import Module83
+import Module84
+import Module85
+import Module86
+import Module87
+import Module88
+import Module89
+import Module90
+import Module91
+import Module92
+import Module93
+import Module94
+import Module95
+import Module96
+import Module97
+import Module98
+import Module99
+import Module100
+
+// Trigger the diagnostic.
+obsoleted()
+// CHECK: [[@LINE-1]]:1: error:
+// CHECK: explicitly marked unavailable here
+// CHECK-NEXT: func obsoleted()
