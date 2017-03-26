e43bda2491d86d9f0204c0a90c129f7b530cb7f2
Brent Royal-Gordon <brent@architechies.com>
2017-03-26 06:27:04 -0700

    Restore original interpolation_segments test
    We actualy want to end up with a constraint system more like this one, so let’s keep the old test.

--- test/Constraints/interpolation_segments.swift
+++ test/Constraints/interpolation_segments.swift
@@ -3,27 +3,43 @@
 
 // Make sure that the interpolation segments get placed into separate connected
 // components.
-// The literal segments, however, are in one component.
 // CHECK: ---Connected components---
-// CHECK-NEXT:  0: $T{{[0-9]+}} $T{{[0-9]+}} $T{{[0-9]+}} $T{{[0-9]+}} $T{{[0-9]+}} $T{{[0-9]+}} $T{{[0-9]+}}
-// CHECK-NEXT:  1: $T{{[0-9]+}}
-// CHECK-NEXT:  2: $T{{[0-9]+}}
-// CHECK-NEXT:  3: $T{{[0-9]+}}
-// CHECK-NEXT:  4: $T{{[0-9]+}}
+// CHECK-NEXT:  0: 
+// CHECK-NEXT:  1: 
+// CHECK-NEXT:  2: 
+// CHECK-NEXT:  3: 
+// CHECK-NEXT:  4: 
+// CHECK-NEXT:  5: 
+// CHECK-NEXT:  6: 
+// CHECK-NEXT:  7: 
+// CHECK-NEXT:  8: 
+// CHECK-NEXT:  9:
 
-// CHECK: (solving component #0
-// CHECK-NEXT: literal=3 involves_type_vars bindings=(subtypes of) (default from ExpressibleByStringLiteral) String)
+// CHECK: (solving component #
+// CHECK: literal=3 bindings=(subtypes of) (default from ExpressibleByStringLiteral) String)
 
-// CHECK: (solving component #1
-// CHECK-NEXT: literal=3 bindings=(subtypes of) (default from ExpressibleByIntegerLiteral) Int)
+// CHECK: (solving component #
+// CHECK: literal=3 bindings=(subtypes of) (default from ExpressibleByIntegerLiteral) Int)
 
-// CHECK: (solving component #2
-// CHECK-NEXT: literal=3 bindings=(subtypes of) (default from ExpressibleByIntegerLiteral) Int)
+// CHECK: (solving component #
+// CHECK: literal=3 bindings=(subtypes of) (default from ExpressibleByStringLiteral) String)
 
-// CHECK: (solving component #3
-// CHECK-NEXT: literal=3 bindings=(subtypes of) (default from ExpressibleByIntegerLiteral) Int)
+// CHECK: (solving component #
+// CHECK: literal=3 bindings=(subtypes of) (default from ExpressibleByIntegerLiteral) Int)
 
-// CHECK: (solving component #4
-// CHECK-NEXT: literal=3 bindings=(subtypes of) (default from ExpressibleByIntegerLiteral) Int)
+// CHECK: (solving component #
+// CHECK: literal=3 bindings=(subtypes of) (default from ExpressibleByStringLiteral) String)
+
+// CHECK: (solving component #
+// CHECK: literal=3 bindings=(subtypes of) (default from ExpressibleByIntegerLiteral) Int)
+
+// CHECK: (solving component #
+// CHECK: literal=3 bindings=(subtypes of) (default from ExpressibleByStringLiteral) String)
+
+// CHECK: (solving component #
+// CHECK: literal=3 bindings=(subtypes of) (default from ExpressibleByIntegerLiteral) Int)
+
+// CHECK: (solving component #
+// CHECK: literal=3 bindings=(subtypes of) (default from ExpressibleByStringLiteral) String)
 
 _ = "\(1), \(2), \(3), \(4)"
