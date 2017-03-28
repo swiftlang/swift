138debee8a4e924488cb9ef1a8ed3d66734378bf
Hugh Bellamy <hughbellars@gmail.com>
2017-03-28 09:13:46 +0700

    Make sure to lint the interpreted, not compiled python file

--- utils/gyb.py
+++ utils/gyb.py
@@ -1102,7 +1102,7 @@ def main():
     """
     Lint this file.
     >>> import sys
-    >>> gyb_path = os.path.realpath(__file__)
+    >>> gyb_path = os.path.realpath(__file__).replace('.pyc', '.py')
     >>> sys.path.append(os.path.dirname(gyb_path))
     >>> import python_lint
     >>> python_lint.lint([gyb_path], verbose=False)
