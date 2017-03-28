b4cc8b1e457ba73751d4a47f40abe9677f980114
Xi Ge <xi_ge@apple.com>
2017-03-28 14:27:07 -0700

    [SourceKit] DocSupport: constraint extensions provide default implementation too. rdar://25692947

--- test/SourceKit/DocSupport/Inputs/cake1.swift
+++ test/SourceKit/DocSupport/Inputs/cake1.swift
@@ -3,6 +3,7 @@ public protocol P1 {
   var Ins : Int { get set }
   func foo2(a : Int, b: Int)
   subscript(_ a : Int) -> Int { get set }
+  func fooConstraint()
 }
 
 public protocol P2 : P1 {
@@ -15,4 +16,12 @@ public extension P2 {
   var Ins : Int { get { return 1 } set {}}
   func foo2(a : Int, b: Int) {}
   subscript(_ a : Int) -> Int { get {return 1} set {} }
+}
+
+public protocol P3 {
+  func p3Required()
+}
+
+public extension P2 where Self : P3 {
+  func fooConstraint() {}
 }
