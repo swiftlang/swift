c7ef60c7ed9994494c3d6f658877165c18cdb444
Erik Eckstein <eeckstein@apple.com>
2017-03-30 07:59:05 -0700

    Revert "AllocBoxToStack: Improve alloc_stack/dealloc_stack scoping"
    It broke some bots.
    
    This reverts commit 43ddc2a643948347c8f88748b901f1d695818515.

--- test/DebugInfo/protocolarg.swift
+++ test/DebugInfo/protocolarg.swift
@@ -8,21 +8,21 @@ public protocol IGiveOutInts {
 }
 
 // CHECK: define {{.*}}@_T011protocolarg16printSomeNumbersyAA12IGiveOutInts_pF
-// CHECK: @llvm.dbg.declare(metadata %T11protocolarg12IGiveOutIntsP** %
-// CHECK-SAME:              metadata ![[ARG:.*]], metadata ![[DEREF:.*]])
 // CHECK: @llvm.dbg.declare(metadata %T11protocolarg12IGiveOutIntsP* %
 // CHECK-SAME:              metadata ![[VAR:.*]], metadata ![[EMPTY:.*]])
+// CHECK: @llvm.dbg.declare(metadata %T11protocolarg12IGiveOutIntsP** %
+// CHECK-SAME:              metadata ![[ARG:.*]], metadata ![[DEREF:.*]])
 
 // CHECK: ![[EMPTY]] = !DIExpression()
 
 public func printSomeNumbers(_ gen: IGiveOutInts) {
   var gen = gen
+  // CHECK: ![[VAR]] = !DILocalVariable(name: "gen", {{.*}} line: [[@LINE-1]]
   // FIXME: Should be DW_TAG_interface_type
+  // CHECK: ![[PT:.*]] = !DICompositeType(tag: DW_TAG_structure_type, name: "IGiveOutInts"
   // CHECK: ![[ARG]] = !DILocalVariable(name: "gen", arg: 1,
-  // CHECK-SAME:                        line: [[@LINE-4]], type: ![[PT:[0-9]+]]
-  // CHECK: ![[PT]] = !DICompositeType(tag: DW_TAG_structure_type, name: "IGiveOutInts"
+  // CHECK-SAME:                        line: [[@LINE-6]], type: ![[PT]]
   // CHECK: ![[DEREF]] = !DIExpression(DW_OP_deref)
-  // CHECK: ![[VAR]] = !DILocalVariable(name: "gen", {{.*}} line: [[@LINE-6]]
   markUsed(gen.callMe())
   use(&gen)
 }
--- test/SILOptimizer/sroa_unreferenced_members.swift
+++ test/SILOptimizer/sroa_unreferenced_members.swift
@@ -3,7 +3,7 @@
 import gizmo
 
 // CHECK: ModifyStruct
-// CHECK:   = alloc_stack $Drill
+// CHECK: %1 = alloc_stack $Drill
 // CHECK: ret
 func ModifyStruct(inDrill : Drill) -> Int32 {
   var D : Drill = inDrill
