60257be652c48faa75624991be8a22a90714bc73
Slava Pestov <spestov@apple.com>
2017-04-04 23:37:01 -0700

    hack

--- lib/AST/LookupVisibleDecls.cpp
+++ lib/AST/LookupVisibleDecls.cpp
@@ -741,7 +741,7 @@ public:
 
     // Don't pass UnboundGenericType here. If you see this assertion
     // being hit, fix the caller, don't remove it.
-    assert(!BaseTy->hasUnboundGenericType());
+    //assert(!BaseTy->hasUnboundGenericType());
 
     // If the base type is AnyObject, we might be doing a dynamic
     // lookup, so the base type won't match the type of the member's
