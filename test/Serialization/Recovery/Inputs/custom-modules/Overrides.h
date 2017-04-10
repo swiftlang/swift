a2e0e27624926a39260606cb3c67147e98aaf306
Jordan Rose <jordan_rose@apple.com>
2017-04-10 11:28:11 -0700

    [test] Add some more tests for 73d4526.
    There's still nothing really complicated going on here, such as an
    override that previously satisfied a protocol conformance.

--- test/Serialization/Recovery/Inputs/custom-modules/Overrides.h
+++ test/Serialization/Recovery/Inputs/custom-modules/Overrides.h
@@ -3,7 +3,33 @@
 @end
 
 @interface Base : Object
-#ifndef BAD
-- (void)method;
+#if !BAD
+- (void)disappearingMethod;
+- (nullable id)nullabilityChangeMethod;
+- (nonnull id)typeChangeMethod;
+#else
+//- (void)disappearingMethod;
+- (nonnull id)nullabilityChangeMethod;
+- (nonnull Base *)typeChangeMethod;
+#endif
+@end
+
+@interface Base (ExtraMethodsToTriggerCircularReferences)
+#if !BAD
+- (void)disappearingMethodWithOverload;
+#else
+//- (void)disappearingMethodWithOverload;
+#endif
+@end
+
+@interface GenericBase<Element>: Object
+#if !BAD
+- (void)disappearingMethod;
+- (nullable Element)nullabilityChangeMethod;
+- (nonnull id)typeChangeMethod;
+#else
+//- (void)disappearingMethod;
+- (nonnull Element)nullabilityChangeMethod;
+- (nonnull Element)typeChangeMethod;
 #endif
 @end
--- test/Serialization/Recovery/overrides.swift
+++ test/Serialization/Recovery/overrides.swift
@@ -9,15 +9,47 @@
 
 import Overrides
 
-public class Sub: Base {
-  public override func method() {}
+// Please use prefixes to keep the printed parts of this file in alphabetical
+// order.
+
+public class SwiftOnlyClass {}
+
+public class A_Sub: Base {
+  public override func disappearingMethod() {}
+  public override func nullabilityChangeMethod() -> Any? { return nil }
+  public override func typeChangeMethod() -> Any { return self }
+  public override func disappearingMethodWithOverload() {}
 }
 
-// CHECK-LABEL: class Sub : Base {
-// CHECK-NEXT: func method()
+// CHECK-LABEL: class A_Sub : Base {
+// CHECK-NEXT: func disappearingMethod()
+// CHECK-NEXT: func nullabilityChangeMethod() -> Any?
+// CHECK-NEXT: func typeChangeMethod() -> Any
+// CHECK-NEXT: func disappearingMethodWithOverload()
 // CHECK-NEXT: init()
 // CHECK-NEXT: {{^}$}}
 
-// CHECK-RECOVERY-LABEL: class Sub : Base {
+// CHECK-RECOVERY-LABEL: class A_Sub : Base {
+// CHECK-RECOVERY-NEXT: init()
+// CHECK-RECOVERY-NEXT: {{^}$}}
+
+extension Base {
+  @nonobjc func disappearingMethodWithOverload() -> SwiftOnlyClass? { return nil }
+}
+
+public class B_GenericSub : GenericBase<Base> {
+  public override func disappearingMethod() {}
+  public override func nullabilityChangeMethod() -> Base? { return nil }
+  public override func typeChangeMethod() -> Any { return self }
+}
+
+// CHECK-LABEL: class B_GenericSub : GenericBase<Base> {
+// CHECK-NEXT: func disappearingMethod()
+// CHECK-NEXT: func nullabilityChangeMethod() -> Base?
+// CHECK-NEXT: func typeChangeMethod() -> Any
+// CHECK-NEXT: init()
+// CHECK-NEXT: {{^}$}}
+
+// CHECK-RECOVERY-LABEL: class B_GenericSub : GenericBase<Base> {
 // CHECK-RECOVERY-NEXT: init()
 // CHECK-RECOVERY-NEXT: {{^}$}}
