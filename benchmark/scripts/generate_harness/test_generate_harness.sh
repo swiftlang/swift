857745fbf102d768bf016b535201357283341851
Pavol Vaskovic <pali@pali.sk>
2017-04-13 21:38:21 +0200

    Added usage comment to test driver. Renamed validation-test/[benchmarks -> benchmark].

--- benchmark/scripts/generate_harness/test_generate_harness.sh
+++ benchmark/scripts/generate_harness/test_generate_harness.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# This script is invoked by `lit` on all smoke test runs from the
+# validation-test/benchmark/generate-harness.test-sh.
+# It ensures that the files checked in the benchmark suite that are generated
+# from templates always match what would be regenerated if one
+# re-ran the relevant scripts. This is to catch accidental manual edits.
 
 SWIFT_SRC_DIR="$1"
 BENCHMARK_DIR="${SWIFT_SRC_DIR}/benchmark"
