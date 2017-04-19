da9a77eb485f7129d5c73759c97bd361d9528951
Xi Ge <xi_ge@apple.com>
2017-04-19 14:59:46 -0700

    test requires objc_interop.

--- test/migrator/member.swift
+++ test/migrator/member.swift
@@ -1,3 +1,4 @@
+// REQUIRES: objc_interop
 // RUN: %target-swiftc_driver -update-code -F %S/mock-sdk -api-diff-data-file %S/API.json %s -o %t.member.result
 // RUN: %FileCheck %s -check-prefix=SIMPLE_REPLACE < %t.member.result
 
