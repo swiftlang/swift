c44eaccc9118b68a41ed2785d8616a7057412256
Xi Ge <xi_ge@apple.com>
2017-04-19 13:37:20 -0700

    avoid hard code path

--- test/migrator/member.swift
+++ test/migrator/member.swift
@@ -1,8 +1,10 @@
 // RUN: %target-swiftc_driver -update-code -F %S/mock-sdk -api-diff-data-file %S/API.json %s -o %t.member.result
-// RUN: diff -u %S/Outputs/member.result %t.member.result
+// RUN: %FileCheck %s -check-prefix=SIMPLE_REPLACE < %t.member.result
 
 import Bar
 
 func foo(_ b: BarForwardDeclaredClass) -> Int32 {
   return barGlobalVariable
 }
+
+// SIMPLE_REPLACE: "text": "bar.memberVariable"
