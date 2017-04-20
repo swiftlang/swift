9701d94f15c6a98b27f46a68d4f02dd480c79250
David Farler <dfarler@apple.com>
2017-04-19 19:18:32 -0700

    [Migrator] Add adapter for clang lib/Edit textual edits
    This adds an adapter class that wraps Clang's lib/Edit
    Commit and EditedSource classes for use in the initial
    "syntactic" passes. Once the passes have completed,
    the resulting source file is then passed to the Swift compiler
    fix-it passes.
    
    rdar://problem/30926261

--- include/swift/Migrator/MigrationState.h
+++ include/swift/Migrator/MigrationState.h
@@ -18,7 +18,6 @@
 #ifndef SWIFT_MIGRATOR_MIGRATIONSTATE_H
 #define SWIFT_MIGRATOR_MIGRATIONSTATE_H
 
-#include "swift/Migrator/Replacement.h"
 #include "swift/Syntax/References.h"
 #include "llvm/ADT/IntrusiveRefCntPtr.h"
 #include "llvm/ADT/StringRef.h"
@@ -30,45 +29,34 @@ class SourceManager;
 namespace migrator {
 
 enum class MigrationKind {
+  /// The start state of the migrator.
+  Start,
+
+  /// A syntactic pass. This is generic for now because there isn't a good
+  /// way to separate out and compose syntactic passes until lib/Syntax is
+  /// integrated.
+  Syntactic,
+
   /// The compiler has made several passes over the input file and we
   /// applied the suggested fix-its we deemed appropriate.
   CompilerFixits,
 };
 
 struct MigrationState : public llvm::ThreadSafeRefCountedBase<MigrationState> {
-  const MigrationKind Kind;
+  MigrationKind Kind;
   SourceManager &SrcMgr;
+  unsigned InputBufferID;
+  unsigned OutputBufferID;
 
-  MigrationState(const MigrationKind Kind, SourceManager &SrcMgr)
-    : Kind(Kind), SrcMgr(SrcMgr) {}
+  MigrationState(const MigrationKind Kind, SourceManager &SrcMgr,
+                 const unsigned InputBufferID, const unsigned OutputBufferID)
+    : Kind(Kind), SrcMgr(SrcMgr),
+      InputBufferID(InputBufferID),
+      OutputBufferID(OutputBufferID) {}
 
   MigrationKind getKind() const {
     return Kind;
   }
-
-  std::string getInputText() const;
-  std::string getOutputText() const;
-
-  /// Write all relevant information about the state to OutDir, such as the
-  /// input file, output file, replacements, syntax trees, etc.
-  bool print(size_t StateNumber, StringRef OutDir) const;
-};
-
-struct FixitMigrationState final : public MigrationState {
-  const unsigned InputBufferID;
-  const unsigned OutputBufferID;
-  const std::vector<Replacement> Replacements;
-
-  // Other information about the migration?
-
-  FixitMigrationState(SourceManager &SrcMgr,
-                      const unsigned InputBufferID,
-                      const unsigned OutputBufferID,
-                      const std::vector<Replacement> &Replacements)
-    : MigrationState(MigrationKind::CompilerFixits, SrcMgr),
-      InputBufferID(InputBufferID),
-      OutputBufferID(OutputBufferID),
-      Replacements(Replacements) {}
 
   std::string getInputText() const;
 
@@ -82,37 +70,35 @@ struct FixitMigrationState final : public MigrationState {
     return OutputBufferID;
   }
 
+  /// Write all relevant information about the state to OutDir, such as the
+  /// input file, output file, replacements, syntax trees, etc.
   bool print(size_t StateNumber, StringRef OutDir) const;
 
   bool outputDiffersFromInput() const {
     return InputBufferID != OutputBufferID;
   }
 
-  /// Dump the remap between the current input and output text.
-  void dumpRemap() const;
-
-  static RC<FixitMigrationState>
-  make(SourceManager &SrcMgr, const unsigned InputBufferID,
-       const unsigned OutputBufferID,
-       const std::vector<Replacement> &Replacements) {
-    return RC<FixitMigrationState> {
-      new FixitMigrationState {
-        SrcMgr,
-        InputBufferID,
-        // The input is the output here, because nothing happened yet.
-        OutputBufferID,
-        Replacements
+  static RC<MigrationState>
+  start(SourceManager &SrcMgr, const unsigned InputBufferID) {
+    return RC<MigrationState> {
+      new MigrationState {
+        MigrationKind::Start, SrcMgr, InputBufferID, InputBufferID
       }
     };
   }
 
-  static RC<FixitMigrationState>
-  start(SourceManager &SrcMgr, const unsigned InputBufferID) {
-    return make(SrcMgr, InputBufferID, InputBufferID, {});
-  }
-
-  static bool classof(const MigrationState *MS) {
-    return MS->getKind() == MigrationKind::CompilerFixits;
+  static RC<MigrationState>
+  make(MigrationKind Kind, SourceManager &SrcMgr, const unsigned InputBufferID,
+       const unsigned OutputBufferID) {
+    return RC<MigrationState> {
+      new MigrationState {
+        Kind,
+        SrcMgr,
+        InputBufferID,
+        // The input is the output here, because nothing happened yet.
+        OutputBufferID
+      }
+    };
   }
 };
 
