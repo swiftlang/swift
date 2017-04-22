202de40f053b466b66ad606c8e123174ea47e3c2
Roman Levenstein <rlevenstein@apple.com>
2017-04-22 01:09:47 -0700

    [sil-opened-archetype-tracker] Improve tracking of archetypes in SILBuilder
    Fixes rdar://problem/31749245

--- include/swift/SIL/SILBuilder.h
+++ include/swift/SIL/SILBuilder.h
@@ -153,8 +153,7 @@ public:
     if (InsertPt == BB->end())
       return;
     // Set the opened archetype context from the instruction.
-    this->getOpenedArchetypes().addOpenedArchetypeOperands(
-        InsertPt->getTypeDependentOperands());
+    addOpenedArchetypeOperands(&*InsertPt);
   }
 
   /// setInsertionPoint - Set the insertion point to insert before the specified
@@ -200,10 +199,7 @@ public:
   //===--------------------------------------------------------------------===//
   // Opened archetypes handling
   //===--------------------------------------------------------------------===//
-  void addOpenedArchetypeOperands(SILInstruction *I) {
-    getOpenedArchetypes().addOpenedArchetypeOperands(
-        I->getTypeDependentOperands());
-  }
+  void addOpenedArchetypeOperands(SILInstruction *I);
 
   //===--------------------------------------------------------------------===//
   // Type remapping
