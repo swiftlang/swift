6fb6cdcc61c7334429bbbf45b637b9457694c340
Erik Eckstein <eeckstein@apple.com>
2017-04-21 21:08:33 -0700

    Disabled 2 driver tests because they are failing
    rdar://problem/31771633

--- test/Driver/loaded_module_trace_foundation.swift
+++ test/Driver/loaded_module_trace_foundation.swift
@@ -1,5 +1,8 @@
 // RUN: %target-build-swift -o %t -module-name loaded_module_trace_foundation %s -emit-loaded-module-trace -emit-loaded-module-trace-path - 2>&1 | %FileCheck %s
 
+// Disabled for now
+// REQUIRES: rdar://problem/31771633
+
 // REQUIRES: objc_interop
 
 // CHECK: {
--- test/Driver/loaded_module_trace_header.swift
+++ test/Driver/loaded_module_trace_header.swift
@@ -1,6 +1,9 @@
 // RUN: env SWIFT_LOADED_MODULE_TRACE_PATH=%t %target-build-swift -module-name loaded_module_trace_header %s -o- -import-objc-header %S/Inputs/loaded_module_trace_header.h > /dev/null
 // RUN: %FileCheck %s < %t
 
+// Disabled for now
+// REQUIRES: rdar://problem/31771633
+
 // REQUIRES: objc_interop
 
 // CHECK: {
