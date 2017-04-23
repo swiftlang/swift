7eae042f5b448f89fa1a207c039ef1a140223b80
ben-cohen <ben_cohen@apple.com>
2017-04-23 12:57:34 -0700

    XFAIL a few tests that need further investigation

--- validation-test/compiler_crashers_2_fixed/0066-sr3687-updated.swift
+++ validation-test/compiler_crashers_2_fixed/0066-sr3687-updated.swift
@@ -1,9 +1,7 @@
 // RUN: %target-swift-frontend %s -emit-ir
-// <rdar://problem/31798398>
 
 public protocol QHash : Collection, ExpressibleByArrayLiteral {
-  associatedtype Key
-  typealias Element = Key
+  associatedtype Key where Key == Element
 
   init()
 }
