a4a8600f6205c35a2f45bee8c68f176ca8c9c48e
Huon Wilson <huon@apple.com>
2017-04-24 11:24:08 -0700

    [test] rdar://problem/31771633 stop looking for target dependent libraries.

--- test/Driver/loaded_module_trace_foundation.swift
+++ test/Driver/loaded_module_trace_foundation.swift
@@ -1,8 +1,5 @@
 // RUN: %target-build-swift -o %t -module-name loaded_module_trace_foundation %s -emit-loaded-module-trace -emit-loaded-module-trace-path - 2>&1 | %FileCheck %s
 
-// Disabled for now
-// REQUIRES: rdar://problem/31771633
-
 // REQUIRES: objc_interop
 
 // CHECK: {
@@ -14,7 +11,6 @@
 // CHECK: "{{[^"]*}}/Darwin.swiftmodule"
 // CHECK: "{{[^"]*}}/Foundation.swiftmodule"
 // CHECK: "{{[^"]*}}/Swift.swiftmodule"
-// CHECK: "{{[^"]*}}/IOKit.swiftmodule"
 // CHECK: "{{[^"]*}}/SwiftOnoneSupport.swiftmodule"
 // CHECK: ]
 // CHECK: }
