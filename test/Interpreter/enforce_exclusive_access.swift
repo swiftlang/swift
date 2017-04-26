f3bf3d111239e4397cbd7d627973a0d8e36d6ceb
Andrew Trick <atrick@apple.com>
2017-04-26 15:49:24 -0700

    [Exclusivity] Update tests for access markers.

--- test/Interpreter/enforce_exclusive_access.swift
+++ test/Interpreter/enforce_exclusive_access.swift
@@ -1,6 +1,6 @@
 // RUN: rm -rf %t
 // RUN: mkdir -p %t
-// RUN: %target-build-swift %s -o %t/a.out -enforce-exclusivity=checked
+// RUN: %target-build-swift %s -o %t/a.out -enforce-exclusivity=checked -Onone
 //
 // RUN: %target-run %t/a.out
 // REQUIRES: executable_test
