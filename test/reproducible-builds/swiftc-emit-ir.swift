3e27346eb336dfce32089a9cd371bea1ae5066d7
practicalswift <practicalswift@users.noreply.github.com>
2017-04-26 17:16:12 +0200

    Add reproducibility tests for -emit-assembly, -emit-ir and -emit-silgen

---
+++ test/reproducible-builds/swiftc-emit-ir.swift
@@ -0,0 +1,5 @@
+// RUN: rm -rf %t && mkdir -p %t
+// RUN: %target-build-swift -O -g -module-name foo -emit-ir %s > %t/run-1.ir
+// RUN: %target-build-swift -O -g -module-name foo -emit-ir %s > %t/run-2.ir
+// RUN: diff -u %t/run-1.ir %t/run-2.ir
+print("foo")
