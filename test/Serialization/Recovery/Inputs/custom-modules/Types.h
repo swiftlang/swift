93963c58ed32147bc2ebbfe9be6ac776c396b9f4
Jordan Rose <jordan_rose@apple.com>
2017-04-26 13:06:55 -0700

    [Serialization] Look through unavailable compatibility aliases.
    This keeps us from showing Swift 3 names in Swift 4 code;
    unfortunately, as the test case shows, we still have a few cases where
    Swift /4/ names will leak into Swift /3/ code. I'm considering this an
    acceptable state of events for now.

---
+++ test/Serialization/Recovery/Inputs/custom-modules/Types.h
@@ -0,0 +1,27 @@
+@interface Object
+- (nonnull instancetype)init;
+@end
+
+@interface RenamedClass : Object
+@end
+
+@interface RenamedGenericClass<Element> : Object
+@end
+
+typedef int RenamedTypedef;
+
+struct RenamedStruct {
+  int value;
+};
+
+#define CF_ENUM(_type, _name) enum _name : _type _name; enum _name : _type
+
+typedef CF_ENUM(long, RenamedEnum) {
+  RenamedEnumA
+};
+
+@protocol RenamedProtocol
+@end
+
+@interface RenamedClass (RenamedProtocol) <RenamedProtocol>
+@end
