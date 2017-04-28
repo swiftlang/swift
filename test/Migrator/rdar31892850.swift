005f85cf3cd993b1accbd55e85175370383cd282
Arnold Schwaighofer <aschwaighofer@apple.com>
2017-04-28 15:12:58 -0700

    Disable test/Migrator/rdar31892850.swift rdar://31897099

--- test/Migrator/rdar31892850.swift
+++ test/Migrator/rdar31892850.swift
@@ -1,6 +1,7 @@
 // RUN: rm -rf %t && mkdir -p %t && %target-swift-frontend -typecheck -primary-file %s -module-cache-path %t/mcp -emit-remap-file-path %t/edits.remap
 // RUN: diff -u %t/edits.remap %S/rdar31892850.remap
 
+// REQUIRES: rdar_31897099
 enum SomeStringEnum : String {
   case val = ""
 }
