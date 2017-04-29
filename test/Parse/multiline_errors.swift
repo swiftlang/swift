0f19bb6bdbfa615df5906a9fa89c18e7a0c30cd4
Brent Royal-Gordon <brent@architechies.com>
2017-04-29 00:11:48 -0700

    Add tests for improved multiline string errors

--- test/Parse/multiline_errors.swift
+++ test/Parse/multiline_errors.swift
@@ -4,35 +4,61 @@ import Swift
 
 // ===---------- Multiline --------===
 
+// expecting at least 4 columns of leading indentation
 _ = """
     Eleven
   Mu
-    """ // expected-error@-1{{invalid mix of multi-line string literal indentation}}
-// expecting at least 4 columns of leading indentation
+    """ // expected-error@-1{{string literal content is insufficiently indented}}
+        // expected-note@-2{{change indentation to match last line}} {{1-3=    }}
+        // expected-note@-2{{change last line's indentation to match this line}} {{1-5=  }}
 
+// expecting at least 4 columns of leading indentation
 _ = """
     Eleven
    Mu
-    """ // expected-error@-1{{invalid mix of multi-line string literal indentation}}
-// expecting at least 4 columns of leading indentation
+    """ // expected-error@-1{{string literal content is insufficiently indented}}
+        // expected-note@-2{{change indentation to match last line}} {{1-4=    }}
+        // expected-note@-2{{change last line's indentation to match this line}} {{1-5=   }}
 
+// \t is not the same as an actual tab for de-indentation
 _ = """
 	Twelve
 \tNu
-	""" // expected-error@-1{{invalid mix of multi-line string literal indentation}}
-// \t is not the same as an actual tab for de-indentation
+	""" // expected-error@-1{{string literal content is insufficiently indented}}
+      // expected-note@-2{{change indentation to match last line}} {{1-1=	}}
+      // expected-note@-2{{change last line's indentation to match this line}} {{1-2=}}
 
+// a tab is not the same as multiple spaces for de-indentation
 _ = """
   Thirteen
 	Xi
-  """ // expected-error@-1{{invalid mix of multi-line string literal indentation}}
-// a tab is not the same as multiple spaces for de-indentation
+  """ // expected-error@-1{{string literal content is indented with a tab where a space is expected}}
+      // expected-note@-2{{change indentation to match last line}} {{1-2=  }}
+      // expected-note@-2{{change last line's indentation to match this line}} {{1-3=	}}
 
+// a tab is not the same as multiple spaces for de-indentation
 _ = """
     Fourteen
   	Pi
-    """ // expected-error@-1{{invalid mix of multi-line string literal indentation}}
-// a tab is not the same as multiple spaces for de-indentation
+    """ // expected-error@-1{{string literal content is indented with a tab where a space is expected}}
+        // expected-note@-2{{change indentation to match last line}} {{1-4=    }}
+        // expected-note@-2{{change last line's indentation to match this line}} {{1-5=  	}}
+
+// multiple spaces are not the same as a tab for de-indentation
+_ = """
+	Thirteen 2
+  Xi 2
+	""" // expected-error@-1{{string literal content is indented with a space where a tab is expected}}
+      // expected-note@-2{{change indentation to match last line}} {{1-2=	}}
+      // expected-note@-2{{change last line's indentation to match this line}} {{1-3=  }}
+
+// multiple spaces are not the same as a tab for de-indentation
+_ = """
+		Fourteen 2
+	  Pi 2
+		""" // expected-error@-1{{string literal content is indented with a space where a tab is expected}}
+        // expected-note@-2{{change indentation to match last line}} {{1-4=		}}
+        // expected-note@-2{{change last line's indentation to match this line}} {{1-5=	  }}
 
 _ = """Fourteen
     Pi
