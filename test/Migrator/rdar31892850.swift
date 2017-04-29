d14aa19884212416907bcab4dca850a1f65d191a
Nathan Hawes <nhawes@apple.com>
2017-04-28 17:08:39 -0700

    [test] Fix and re-enable test/Migrator/rdar31892850.swift

--- test/Migrator/rdar31892850.swift
+++ test/Migrator/rdar31892850.swift
@@ -1,7 +1,6 @@
 // RUN: rm -rf %t && mkdir -p %t && %target-swift-frontend -typecheck -primary-file %s -module-cache-path %t/mcp -emit-remap-file-path %t/edits.remap
-// RUN: diff -u %t/edits.remap %S/rdar31892850.remap
+// RUN: %FileCheck %s -input-file=%t/edits.remap
 
-// REQUIRES: rdar_31897099
 enum SomeStringEnum : String {
   case val = ""
 }
@@ -11,3 +10,16 @@ func foo() {
   let e : SomeStringEnum = "aa"
 }
 #endif
+
+// CHECK:[
+// CHECK:  {
+// CHECK:    "file": "{{.*}}rdar31892850.swift",
+// CHECK:    "offset": 305,
+// CHECK:    "text": "SomeStringEnum(rawValue: "
+// CHECK:  },
+// CHECK:  {
+// CHECK:    "file": "{{.*}}rdar31892850.swift",
+// CHECK:    "offset": 309,
+// CHECK:    "text": ")!"
+// CHECK:  }
+// CHECK:]
