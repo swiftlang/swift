6e6804d92035ccb659eaaa588470eef4bf5da600
Doug Gregor <dgregor@apple.com>
2017-05-02 11:16:10 -0700

    [Type checker] Warn about NSCoding-conforming classes with unstable mangled names.
    The name mangling changed from Swift 3 to Swift 4, and may get slight
    tweaks as we lock down ABI stability. Identify and warn about (in
    Swift 3) or error about (in Swift 4) the cases where we don't have
    obviously-stable name mangling, e.g.,
    
    * private/fileprivate classes (whose mangled names involve the file name)
    * nested classes (whose mangled names depend on their enclosing type)
    * generic classes (whose mangled names involve the type arguments)

---
+++ test/decl/protocol/conforms/nscoding.swift
@@ -0,0 +1,74 @@
+// RUN: %target-swift-frontend(mock-sdk: %clang-importer-sdk) -typecheck -parse-as-library -swift-version 4 %s -verify
+
+// REQUIRES: objc_interop
+
+import Foundation
+
+// Top-level classes
+class CodingA : NSObject, NSCoding {
+  required init(coder: NSCoder) { }
+  func encode(coder: NSCoder) { }
+  
+}   // okay
+
+// Nested classes
+extension CodingA {
+  class NestedA : NSObject, NSCoding { // expected-error{{nested class 'CodingA.NestedA' has an unstable name when archiving via 'NSCoding'}}
+    required init(coder: NSCoder) { }
+    func encode(coder: NSCoder) { }
+  }
+
+  class NestedB : NSObject {
+    required init(coder: NSCoder) { }
+    func encode(coder: NSCoder) { }
+  }
+
+  @objc(CodingA_NestedC)
+  class NestedC : NSObject, NSCoding {
+    required init(coder: NSCoder) { }
+    func encode(coder: NSCoder) { }
+  }
+
+  @objc(CodingA_NestedD)
+  class NestedD : NSObject {
+    required init(coder: NSCoder) { }
+    func encode(coder: NSCoder) { }
+  }
+}
+
+extension CodingA.NestedB: NSCoding { // expected-error{{nested class 'CodingA.NestedB' has an unstable name when archiving via 'NSCoding'}}
+}
+
+extension CodingA.NestedD: NSCoding { // okay
+}
+
+// Generic classes
+class CodingB<T> : NSObject, NSCoding {   // expected-error{{generic class 'CodingB<T>' has an unstable name when archiving via 'NSCoding'}}
+  required init(coder: NSCoder) { }
+  func encode(coder: NSCoder) { }
+}
+
+extension CodingB {
+  class NestedA : NSObject, NSCoding { // expected-error{{generic class 'CodingB<T>.NestedA' has an unstable name when archiving via 'NSCoding'}}
+    required init(coder: NSCoder) { }
+    func encode(coder: NSCoder) { }
+  }
+}
+
+// Fileprivate classes.
+fileprivate class CodingC : NSObject, NSCoding {    // expected-error{{fileprivate class 'CodingC' has an unstable name when archiving via 'NSCoding'}}
+  required init(coder: NSCoder) { }
+  func encode(coder: NSCoder) { }
+}
+
+// Private classes
+private class CodingD : NSObject, NSCoding {       // expected-error{{private class 'CodingD' has an unstable name when archiving via 'NSCoding'}}
+  required init(coder: NSCoder) { }
+  func encode(coder: NSCoder) { }
+}
+
+// Inherited conformances.
+class CodingE<T> : CodingB<T> {   // expected-error{{generic class 'CodingE<T>' has an unstable name when archiving via 'NSCoding'}}
+  required init(coder: NSCoder) { super.init(coder: coder) }
+  override func encode(coder: NSCoder) { }
+}
