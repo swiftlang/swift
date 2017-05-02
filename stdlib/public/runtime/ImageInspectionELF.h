43c5acbb195cd117f8dd70110215dce5797bb321
Michael Gottesman <mgottesman@apple.com>
2017-05-02 13:53:11 -0700

    [gardening] Fix doxygen headers in ImageInspection files.

--- stdlib/public/runtime/ImageInspectionELF.h
+++ stdlib/public/runtime/ImageInspectionELF.h
@@ -9,9 +9,11 @@
 // See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
 //
 //===----------------------------------------------------------------------===//
-//
-// ELF specific image inspection routines.
-//
+///
+/// \file
+///
+/// ELF specific image inspection routines.
+///
 //===----------------------------------------------------------------------===//
 
 #ifndef SWIFT_RUNTIME_IMAGE_INSPECTION_ELF_H
