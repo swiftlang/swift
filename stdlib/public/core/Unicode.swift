40b66e5935f39901d0e2a0f1fa9227a3498fad17
Dave Abrahams <dabrahams@apple.com>
2017-05-02 01:01:59 -0700

    [stdlib] Implement legacy UTF8 APIs in terms of new components

--- stdlib/public/core/Unicode.swift
+++ stdlib/public/core/Unicode.swift
@@ -145,24 +145,9 @@ public protocol UnicodeCodec {
 
 /// A codec for translating between Unicode scalar values and UTF-8 code
 /// units.
-public struct UTF8 : UnicodeCodec {
-  // See Unicode 8.0.0, Ch 3.9, UTF-8.
-  // http://www.unicode.org/versions/Unicode8.0.0/ch03.pdf
-
-  /// A type that can hold code unit values for this encoding.
-  public typealias CodeUnit = UInt8
-
+extension _Unicode.UTF8 : UnicodeCodec {
   /// Creates an instance of the UTF-8 codec.
-  public init() {}
-
-  /// Lookahead buffer used for UTF-8 decoding.  New bytes are inserted at MSB,
-  /// and bytes are read at LSB.  Note that we need to use a buffer, because
-  /// in case of invalid subsequences we sometimes don't know whether we should
-  /// consume a certain byte before looking at it.
-  internal var _decodeBuffer: UInt32 = 0
-
-  /// The number of bits in `_decodeBuffer` that are current filled.
-  internal var _bitsInBuffer: UInt8 = 0
+  public init() { self = ._swift3Buffer(ForwardParser()) }
 
   /// Starts or continues decoding a UTF-8 sequence.
   ///
@@ -208,60 +193,16 @@ public struct UTF8 : UnicodeCodec {
   public mutating func decode<I : IteratorProtocol>(
     _ input: inout I
   ) -> UnicodeDecodingResult where I.Element == CodeUnit {
-
-    // Bufferless ASCII fastpath.
-    if _fastPath(_bitsInBuffer == 0) {
-      guard let codeUnit = input.next() else { return .emptyInput }
-      // ASCII, return immediately.
-      if codeUnit & 0x80 == 0 {
-        return .scalarValue(UnicodeScalar(
-          _unchecked: UInt32(extendingOrTruncating: codeUnit)))
-      }
-      // Non-ASCII, proceed to buffering mode.
-      _decodeBuffer = UInt32(extendingOrTruncating: codeUnit)
-      _bitsInBuffer = 8
-    } else if _decodeBuffer & 0x80 == 0 {
-      // ASCII in buffer.  We don't refill the buffer so we can return
-      // to bufferless mode once we've exhausted it.
-      let codeUnit = _decodeBuffer & 0xff
-      _decodeBuffer &>>= 8
-      _bitsInBuffer = _bitsInBuffer &- 8
-      return .scalarValue(UnicodeScalar(_unchecked: codeUnit))
+    guard case ._swift3Buffer(var parser) = self else {
+      fatalError("unreachable")
     }
-    // Buffering mode.
-    // Fill buffer back to 4 bytes (or as many as are left in the iterator).
-    _sanityCheck(_bitsInBuffer < 32)
-    repeat {
-      if let codeUnit = input.next() {
-        // We know _bitsInBuffer < 32 so we use `& 0x1f` (31) to make the
-        // compiler omit a bounds check branch for the bitshift.
-        _decodeBuffer |=
-          (UInt32(extendingOrTruncating: codeUnit) &<<
-          UInt32(extendingOrTruncating: _bitsInBuffer & 0x1f))
-        _bitsInBuffer = _bitsInBuffer &+ 8
-      } else {
-        if _bitsInBuffer == 0 { return .emptyInput }
-        break // We still have some bytes left in our buffer.
-      }
-    } while _bitsInBuffer < 32
+    defer { self = ._swift3Buffer(parser) }
 
-    // Decode one unicode scalar.
-    // Note our empty bytes are always 0x00, which is required for this call.
-    let (result, length) = UTF8._decodeOne(_decodeBuffer)
-
-    // Consume the decoded bytes (or maximal subpart of ill-formed sequence).
-    let bitsConsumed = 8 &* length
-    _sanityCheck(1...4 ~= length && bitsConsumed <= _bitsInBuffer)
-    // Swift doesn't allow shifts greater than or equal to the type width.
-    // _decodeBuffer >>= UInt32(bitsConsumed) // >>= 32 crashes.
-    // Mask with 0x3f (63) to let the compiler omit the '>= 64' bounds check.
-    _decodeBuffer = UInt32(extendingOrTruncating:
-      UInt64(extendingOrTruncating: _decodeBuffer) &>>
-      (UInt64(extendingOrTruncating: bitsConsumed) & 0x3f))
-    _bitsInBuffer = _bitsInBuffer &- bitsConsumed
-
-    guard _fastPath(result != nil) else { return .error }
-    return .scalarValue(UnicodeScalar(_unchecked: result!))
+    switch parser.parseScalar(from: &input) {
+    case .valid(let s): return .scalarValue(UTF8.decode(s))
+    case .invalid: return .error
+    case .emptyInput: return .emptyInput
+    }
   }
 
   /// Attempts to decode a single UTF-8 code unit sequence starting at the LSB
@@ -288,80 +229,18 @@ public struct UTF8 : UnicodeCodec {
       let value = buffer & 0xff
       return (value, 1)
     }
-
-    // Determine sequence length using high 5 bits of 1st byte.  We use a
-    // look-up table to branch less.  1-byte sequences are handled above.
-    //
-    //  case | pattern | description
-    // ----------------------------
-    //   00  |  110xx  | 2-byte sequence
-    //   01  |  1110x  | 3-byte sequence
-    //   10  |  11110  | 4-byte sequence
-    //   11  |  other  | invalid
-    //
-    //                     11xxx      10xxx      01xxx      00xxx
-    let lut0: UInt32 = 0b1011_0000__1111_1111__1111_1111__1111_1111
-    let lut1: UInt32 = 0b1100_0000__1111_1111__1111_1111__1111_1111
-
-    let index: UInt32 = (buffer &>> (3 as UInt32)) & 0x1f
-    let bit0 = (lut0 &>> index) & 1
-    let bit1 = (lut1 &>> index) & 1
-
-    switch (bit1, bit0) {
-    case (0, 0): // 2-byte sequence, buffer: [ ... ... CU1 CU0 ].
-      // Require 10xx xxxx  110x xxxx.
-      if _slowPath(buffer & 0xc0e0 != 0x80c0) { return (nil, 1) }
-      // Disallow xxxx xxxx  xxx0 000x (<= 7 bits case).
-      if _slowPath(buffer & 0x001e == 0x0000) { return (nil, 1) }
-      // Extract data bits.
-      // FIXME(integers): split into multiple expressions to help the typechecker
-      var value = (buffer & 0x3f00) &>> (8 as UInt32)
-      value    |= (buffer & 0x001f) &<< (6 as UInt32)
-      return (value, 2)
-
-    case (0, 1): // 3-byte sequence, buffer: [ ... CU2 CU1 CU0 ].
-      // Disallow xxxx xxxx  xx0x xxxx  xxxx 0000 (<= 11 bits case).
-      if _slowPath(buffer & 0x00200f == 0x000000) { return (nil, 1) }
-      // Disallow xxxx xxxx  xx1x xxxx  xxxx 1101 (surrogate code points).
-      if _slowPath(buffer & 0x00200f == 0x00200d) { return (nil, 1) }
-      // Require 10xx xxxx  10xx xxxx  1110 xxxx.
-      if _slowPath(buffer & 0xc0c0f0 != 0x8080e0) {
-        if buffer & 0x00c000 != 0x008000 { return (nil, 1) }
-        return (nil, 2) // All checks on CU0 & CU1 passed.
-      }
-      // Extract data bits.
-      // FIXME(integers): split into multiple expressions to help the typechecker
-      var value = (buffer & 0x3f0000) &>> (16 as UInt32)
-      value    |= (buffer & 0x003f00) &>> (2  as UInt32)
-      value    |= (buffer & 0x00000f) &<< (12 as UInt32)
-      return (value, 3)
-
-    case (1, 0): // 4-byte sequence, buffer: [ CU3 CU2 CU1 CU0 ].
-      // Disallow xxxx xxxx  xxxx xxxx  xx00 xxxx  xxxx x000 (<= 16 bits case).
-      if _slowPath(buffer & 0x00003007 == 0x00000000) { return (nil, 1) }
-      // If xxxx xxxx  xxxx xxxx  xxxx xxxx  xxxx x1xx.
-      if buffer & 0x00000004 == 0x00000004 {
-        // Require xxxx xxxx  xxxx xxxx  xx00 xxxx  xxxx xx00 (<= 0x10FFFF).
-        if _slowPath(buffer & 0x00003003 != 0x00000000) { return (nil, 1) }
-      }
-      // Require 10xx xxxx  10xx xxxx  10xx xxxx  1111 0xxx.
-      if _slowPath(buffer & 0xc0c0c0f8 != 0x808080f0) {
-        if buffer & 0x0000c000 != 0x00008000 { return (nil, 1) }
-        // All other checks on CU0, CU1 & CU2 passed.
-        if buffer & 0x00c00000 != 0x00800000 { return (nil, 2) }
-        return (nil, 3)
-      }
-      // Extract data bits.
-      // FIXME(integers): remove extra type hints
-      // FIXME(integers): split into multiple expressions to help the typechecker
-      var value = (buffer & 0x3f000000) &>> (24 as UInt32)
-      value    |= (buffer & 0x003f0000) &>> (10 as UInt32)
-      value    |= (buffer & 0x00003f00) &<< (4  as UInt32)
-      value    |= (buffer & 0x00000007) &<< (18 as UInt32)
-      return (value, 4)
-
-    default: // Invalid sequence (CU0 invalid).
-      return (nil, 1)
+    var p = ForwardParser()
+    p._buffer._storage = buffer
+    p._buffer._bitCount = 32
+    var i = EmptyCollection<UInt8>().makeIterator()
+    switch p.parseScalar(from: &i) {
+    case .valid(let s):
+      return (
+        result: UTF8.decode(s).value,
+        length: UInt8(extendingOrTruncating: s.count))
+    case .invalid(let l):
+      return (result: nil, length: UInt8(extendingOrTruncating: l))
+    case .emptyInput: Builtin.unreachable()
     }
   }
 
@@ -385,33 +264,17 @@ public struct UTF8 : UnicodeCodec {
     _ input: UnicodeScalar,
     into processCodeUnit: (CodeUnit) -> Void
   ) {
-    var c = UInt32(input)
-    var buf3 = UInt8(extendingOrTruncating: c & 0xFF)
-
-    if c >= ((1 as UInt32) &<< 7) {
-      c &>>= 6
-      buf3 = (buf3 & 0x3F) | 0x80 // 10xxxxxx
-      var buf2 = UInt8(extendingOrTruncating: c & 0xFF)
-      if c < ((1 as UInt32) &<< 5) {
-        buf2 |= 0xC0              // 110xxxxx
-      }
-      else {
-        c &>>= 6
-        buf2 = (buf2 & 0x3F) | 0x80 // 10xxxxxx
-        var buf1 = UInt8(extendingOrTruncating: c & 0xFF)
-        if c < UInt32(1 &<< 4) {
-          buf1 |= 0xE0              // 1110xxxx
-        }
-        else {
-          c &>>= 6
-          buf1 = (buf1 & 0x3F) | 0x80 // 10xxxxxx
-          processCodeUnit(UInt8(extendingOrTruncating: c | 0xF0)) // 11110xxx
-        }
-        processCodeUnit(buf1)
-      }
-      processCodeUnit(buf2)
-    }
-    processCodeUnit(buf3)
+    var s = encode(input)._storage
+    processCodeUnit(UInt8(extendingOrTruncating: s))
+    s &>>= 8
+    if _fastPath(s == 0) { return }
+    processCodeUnit(UInt8(extendingOrTruncating: s))
+    s &>>= 8
+    if _fastPath(s == 0) { return }
+    processCodeUnit(UInt8(extendingOrTruncating: s))
+    s &>>= 8
+    if _fastPath(s == 0) { return }
+    processCodeUnit(UInt8(extendingOrTruncating: s))
   }
 
   /// Returns a Boolean value indicating whether the specified code unit is a
@@ -447,6 +310,7 @@ public struct UTF8 : UnicodeCodec {
     return Int(_swift_stdlib_strlen(input))
   }
 }
+public typealias UTF8 = _Unicode.UTF8
 
 /// A codec for translating between Unicode scalar values and UTF-16 code
 /// units.
@@ -1194,7 +1058,6 @@ extension UTF16 {
 
 /// A namespace for Unicode utilities.
 public enum _Unicode {
-  public typealias UTF8 = Swift.UTF8
   public typealias UTF16 = Swift.UTF16
   public typealias UTF32 = Swift.UTF32
 }
