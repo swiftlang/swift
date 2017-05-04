4a17449d0247f1aac7ee8e635d1be40e32f172ae
Michael Ilseman <milseman@apple.com>
2017-05-03 18:52:52 -0700

    [stdlib] Shims for UBreakIterator and thread local storage.
    Introduce shims for using UBreakIterators from ICU. Also introduce
    shims for using thread local storage via pthreads.
    
    We will be relying on ICU and UBreakIterators for grapheme
    breaking. But, UBreakIterators are very expensive to create,
    especially for the way we do grapheme breaking, which is relatively
    stateless. Thus, we will stash one or more into thread local storage
    and reset it as needed.
    
    Note: Currently, pthread_key_t is hard coded for a single platform
    (Darwin), but I have a static_assert alongside directions on how to
    adapt it to any future platforms who differ in key type.

--- stdlib/public/SwiftShims/LibcShims.h
+++ stdlib/public/SwiftShims/LibcShims.h
@@ -111,6 +111,29 @@ double _swift_stdlib_squareRoot(double _self) {
   return __builtin_sqrt(_self);
 }
 
+// TLS - thread local storage
+
+// FIXME: multi-platform
+typedef unsigned long __swift_pthread_key_t;
+
+SWIFT_RUNTIME_STDLIB_INTERFACE
+int _swift_stdlib_pthread_key_create(
+  __swift_pthread_key_t * _Nonnull key, void
+  (* _Nullable destructor)(void * _Nullable )
+);
+
+SWIFT_RUNTIME_STDLIB_INTERFACE
+void * _Nullable _swift_stdlib_pthread_getspecific(__swift_pthread_key_t key);
+
+SWIFT_RUNTIME_STDLIB_INTERFACE
+int _swift_stdlib_pthread_setspecific(
+  __swift_pthread_key_t key, const void * _Nullable value
+);
+
+SWIFT_RUNTIME_STDLIB_INTERFACE
+__swift_pthread_key_t _swift_stdlib_PTHREAD_KEYS_MAX(void);
+
+
 // TODO: Remove horrible workaround when importer does Float80 <-> long double.
 #if (defined __i386__ || defined __x86_64__) && !defined _MSC_VER
 static inline SWIFT_ALWAYS_INLINE
