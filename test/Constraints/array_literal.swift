e0c013c531c66bec18dc8573990ea336575aa7c0
ben-cohen <ben_cohen@apple.com>
2017-05-05 19:53:27 -0700

    Fix up more tests

--- test/Constraints/array_literal.swift
+++ test/Constraints/array_literal.swift
@@ -112,7 +112,7 @@ func rdar25563498<T : ExpressibleByArrayLiteral>(t: T) {
 }
 
 func rdar25563498_ok<T : ExpressibleByArrayLiteral>(t: T) -> T
-     where T.Element : ExpressibleByIntegerLiteral {
+     where T.ArrayLiteralElement : ExpressibleByIntegerLiteral {
   let x: T = [1]
   return x
 }
