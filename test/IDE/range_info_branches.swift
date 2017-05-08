38991ca83fe98747484a984d1107abc273768710
Xi Ge <xi_ge@apple.com>
2017-05-07 19:08:47 -0700

    RangeInfo: unbox ifstmt to get the agreed-upon return type from selected statements. rdar://31691723 (#9380)

---
+++ test/IDE/range_info_branches.swift
@@ -0,0 +1,17 @@
+func foo(_ a: Bool) -> Int{
+  if a {
+    return 1
+  } else {
+  }
+  if a {
+    return 0
+  } else {
+    return 1
+  }
+}
+
+// RUN: %target-swift-ide-test -range -pos=2:1 -end-pos 5:4 -source-filename %s | %FileCheck %s -check-prefix=CHECK-ERR
+// RUN: %target-swift-ide-test -range -pos=6:1 -end-pos 10:4 -source-filename %s | %FileCheck %s -check-prefix=CHECK-INT
+
+// CHECK-ERR: <Type><<error type>></Type>
+// CHECK-INT: <Type>Int</Type>
