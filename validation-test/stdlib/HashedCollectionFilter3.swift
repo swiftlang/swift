dbe2b2d5883c95849c0664ba2036de157850f180
Nate Cook <natecook1000@users.noreply.github.com>
2017-05-10 15:37:39 -0500

    Merge pull request #9462 from natecook1000/nc-dict-keys-compat
    [stdlib] Provide Swift 3 compatible Dict.keys/values

--- validation-test/stdlib/HashedCollectionFilter3.swift
+++ validation-test/stdlib/HashedCollectionFilter3.swift
@@ -1,4 +1,4 @@
-// RUN: %target-run-stdlib-swift
+// RUN: %target-run-stdlib-swift -swift-version 3
 // REQUIRES: executable_test
 
 import StdlibUnittest
@@ -15,6 +15,22 @@ FilterTestSuite.test("Set.filter(_:) -> [Element]") {
   let s: Set = [10, 20, 30, 40]
   let f: Any = s.filter { $0 > 20 }
   expectTrue(f is [Int])
+}
+
+FilterTestSuite.test("Dictionary.keys -> LazyMapCollection") {
+  let d = [10: 1010, 20: 1020, 30: 1030, 40: 1040]
+  // .keys should produce a LazyMapCollection in Swift 3
+  let f: Any = d.keys
+  let g = f as! LazyMapCollection<[Int: Int], Int>
+  expectEqual(4, g.count)
+}
+
+FilterTestSuite.test("Dictionary.values -> LazyMapCollection") {
+  let d = [10: 1010, 20: 1020, 30: 1030, 40: 1040]
+  // .values should produce a LazyMapCollection in Swift 3
+  let f: Any = d.values
+  let g = f as! LazyMapCollection<[Int: Int], Int>
+  expectEqual(4, g.count)
 }
 
 runAllTests()
