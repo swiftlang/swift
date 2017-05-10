288f4750928325dff5788afeb1b90076a8be2df3
Xi Ge <xi_ge@apple.com>
2017-05-10 16:20:50 -0700

    [Migrator] Add a stub for authored API change list to handle API changes that are not detected automatically.

--- lib/Frontend/CompilerInvocation.cpp
+++ lib/Frontend/CompilerInvocation.cpp
@@ -1621,7 +1621,7 @@ bool ParseMigratorArgs(MigratorOptions &Opts, llvm::Triple &Triple,
   }
 
   if (auto DataPath = Args.getLastArg(OPT_api_diff_data_file)) {
-    Opts.APIDigesterDataStorePath = DataPath->getValue();
+    Opts.APIDigesterDataStorePaths.push_back(DataPath->getValue());
   } else {
     bool Supported = true;
     llvm::SmallString<128> dataPath(ResourcePath);
@@ -1636,8 +1636,13 @@ bool ParseMigratorArgs(MigratorOptions &Opts, llvm::Triple &Triple,
       llvm::sys::path::append(dataPath, "watchos.json");
     else
       Supported = false;
-    if (Supported)
-      Opts.APIDigesterDataStorePath = dataPath.str();
+    if (Supported) {
+      llvm::SmallString<128> authoredDataPath(ResourcePath);
+      llvm::sys::path::append(authoredDataPath, "overlay.json");
+      // Add authored list first to take higher priority.
+      Opts.APIDigesterDataStorePaths.push_back(authoredDataPath.str())
+      Opts.APIDigesterDataStorePaths.push_back(dataPath.str());
+    }
   }
 
   return false;
