d92fe872c5d6d287e6209e7e1e373a15530699c4
Doug Gregor <dgregor@apple.com>
2017-05-11 09:26:14 -0700

    [SILGen] Only emit Builtin.swift3ImplicitObjCEntrypoint() calls in Swift 4
    Only emit calls to Builtin.swift3ImplicitObjCEntrypoint() when we are
    in Swift 4 mode with `-enable-swift3-objc-inference`, which is a
    transitional state in which one is debugging the use of the
    deprecated @objc entrypoints. Fixes rdar://problem/32122408.

--- test/IRGen/objc_deprecated_objc_thunks.swift
+++ test/IRGen/objc_deprecated_objc_thunks.swift
@@ -1,4 +1,6 @@
-// RUN: %target-swift-frontend -assume-parsing-unqualified-ownership-sil %s -emit-ir -disable-objc-attr-requires-foundation-module | %FileCheck %s
+// RUN: %target-swift-frontend -assume-parsing-unqualified-ownership-sil %s -emit-ir -disable-objc-attr-requires-foundation-module -enable-swift3-objc-inference -swift-version 4 | %FileCheck -check-prefix CHECK-SWIFT4 %s
+
+// RUN: %target-swift-frontend -assume-parsing-unqualified-ownership-sil %s -emit-ir -disable-objc-attr-requires-foundation-module -swift-version 3 | %FileCheck -check-prefix CHECK-SWIFT3 %s
 
 // REQUIRES: CPU=x86_64
 // REQUIRES: objc_interop
@@ -9,7 +11,11 @@ class ObjCSubclass : NSObject {
   func foo() { }
 }
 
-// CHECK-LABEL: define hidden void @_T0016objc_deprecated_A7_thunks12ObjCSubclassC3fooyyFTo(%0*, i8*)
-// CHECK: entry:
-// CHECK: [[SELF:%[0-9]+]] = bitcast %0* %0 to %objc_object*
-// CHECK-NEXT: call void @swift_objc_swift3ImplicitObjCEntrypoint(%objc_object* [[SELF]], i8* %1)
+// CHECK-SWIFT4-LABEL: define hidden void @_T0016objc_deprecated_A7_thunks12ObjCSubclassC3fooyyFTo(%0*, i8*)
+// CHECK-SWIFT4: entry:
+// CHECK-SWIFT4: [[SELF:%[0-9]+]] = bitcast %0* %0 to %objc_object*
+// CHECK-SWIFT4-NEXT: call void @swift_objc_swift3ImplicitObjCEntrypoint(%objc_object* [[SELF]], i8* %1)
+
+// CHECK-SWIFT3-LABEL: define hidden void @_T0016objc_deprecated_A7_thunks12ObjCSubclassC3fooyyFTo(%0*, i8*)
+// CHECK-SWIFT3: entry:
+// CHECK-SWIFT3-NOT: call void @swift_objc_swift3ImplicitObjCEntrypoint(%objc_object* [[SELF]], i8* %1)
