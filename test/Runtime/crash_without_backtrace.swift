43660db26317e25b645f675f4085968ae5263aaf
Kuba Mracek <mracek@apple.com>
2017-05-11 17:53:19 -0700

    Add back the crash_without_backtrace.swift testcase.

---
+++ test/Runtime/crash_without_backtrace.swift
@@ -0,0 +1,23 @@
+// RUN: rm -rf %t
+// RUN: mkdir %t
+// RUN: %target-build-swift %s -o %t/out
+// RUN: not --crash %t/out 2>&1 | %FileCheck %s
+
+// UNSUPPORTED: OS=watchos
+// UNSUPPORTED: OS=ios
+// UNSUPPORTED: OS=tvos
+// REQUIRES: swift_stdlib_no_asserts
+// REQUIRES: executable_test
+
+// This file just causes a crash in the runtime to check whether or not a stack
+// trace is produced from the runtime.
+
+// CHECK-NOT: Current stack trace:
+
+import Swift
+
+func foo() -> Int {
+  return UnsafePointer<Int>(bitPattern: 0)!.pointee
+}
+
+foo()
