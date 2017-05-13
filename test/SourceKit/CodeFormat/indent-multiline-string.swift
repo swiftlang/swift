c0d6e994da4ea2f4985c05ee556beb1ca41336e8
Xi Ge <xi_ge@apple.com>
2017-05-13 16:28:29 -0700

    Formatting: Aoid formatting multi-line string literals.

---
+++ test/SourceKit/CodeFormat/indent-multiline-string.swift
@@ -0,0 +1,16 @@
+func foo() {
+  let s1 = """
+this is line1,
+     this is line2,
+"""
+}
+
+// RUN: %sourcekitd-test -req=format -line=3 -length=1 %s >%t.response
+// RUN: %sourcekitd-test -req=format -line=4 -length=1 %s >>%t.response
+// RUN: %sourcekitd-test -req=format -line=5 -length=1 %s >>%t.response
+
+// RUN: %FileCheck --strict-whitespace %s <%t.response
+
+// CHECK: key.sourcetext: "this is line1,"
+// CHECK: key.sourcetext: "     this is line2,"
+// CHECK: key.sourcetext: "\"\"\""
