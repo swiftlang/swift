180afe2aa8429077088594ce955442d9c0ae2e04
Doug Gregor <dgregor@apple.com>
2017-05-15 17:11:18 -0700

    [GSB] Only finalize a canonical GSB when performing expensive assertions.
    A `GenericSignatureBuilder` only needs to be finalized when we need to
    compute a generic signature from it or otherwise require diagnostics.
    Canonical generic signature builders don’t need either of those, so unless we
    are performing the expensive idempotency checking, only do the minimal work
    to “process delayed requirements”.
    
    This gives a 3x speedup in type-checking time for the “trivial” example
    
        let x = [1, 2]
    
    because we do a lot less work on deserialization of generic environments.
    
    Part of rdar://problem/32116933.
    (cherry picked from commit 4f8f86ab2f4521bf70f42822d7ea1232429d5474)

--- lib/AST/ASTContext.cpp
+++ lib/AST/ASTContext.cpp
@@ -51,6 +51,10 @@
 
 using namespace swift;
 
+/// Define this to 1 to enable expensive assertions of the
+/// GenericSignatureBuilder.
+#define SWIFT_GSB_EXPENSIVE_ASSERTIONS 0
+
 LazyResolver::~LazyResolver() = default;
 DelegatingLazyResolver::~DelegatingLazyResolver() = default;
 void ModuleLoader::anchor() {}
@@ -1383,12 +1387,10 @@ GenericSignatureBuilder *ASTContext::getOrCreateGenericSignatureBuilder(
 
   builder->addGenericSignature(sig);
 
+#if SWIFT_GSB_EXPENSIVE_ASSERTIONS
   auto builderSig =
     builder->computeGenericSignature(SourceLoc(),
                                      /*allowConcreteGenericParams=*/true);
-  (void)builderSig;
-  
-#ifndef NDEBUG
   if (builderSig->getCanonicalSignature() != sig) {
     llvm::errs() << "ERROR: generic signature builder is not idempotent.\n";
     llvm::errs() << "Original generic signature   : ";
@@ -1439,6 +1441,10 @@ GenericSignatureBuilder *ASTContext::getOrCreateGenericSignatureBuilder(
 
     llvm_unreachable("idempotency problem with a generic signature");
   }
+#else
+  // FIXME: This should be handled lazily in the future, and therefore not
+  // required.
+  builder->processDelayedRequirements();
 #endif
 
   return builder;
