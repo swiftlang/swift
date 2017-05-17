012ea9373b5d2f72c59e16e18bd077c3ee86c745
Itai Ferber <iferber@apple.com>
2017-05-17 16:01:20 -0700

    Add Codable conformance to common Foundation types
    Add conformances + unit tests for
    
    * CGFloat
    * AffineTransform
    * Calendar
    * CharacterSet
    * DateComponents
    * DateInterval
    * Decimal
    * IndexPath
    * IndexSet
    * Locale
    * Measurement
    * NSRange
    * PersonNameComponents
    * TimeZone
    * URL
    * UUID
    
    along with some unit tests for each.

--- stdlib/public/SDK/Foundation/IndexSet.swift
+++ stdlib/public/SDK/Foundation/IndexSet.swift
@@ -917,3 +917,38 @@ private final class _MutablePairHandle<ImmutableType : NSObject, MutableType : N
         }
     }
 }
+
+extension IndexSet : Codable {
+    private enum CodingKeys : Int, CodingKey {
+        case indexes
+    }
+
+    private enum RangeCodingKeys : Int, CodingKey {
+        case location
+        case length
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        var indexesContainer = try container.nestedUnkeyedContainer(forKey: .indexes)
+        self.init()
+
+        while !indexesContainer.isAtEnd {
+            let rangeContainer = try indexesContainer.nestedContainer(keyedBy: RangeCodingKeys.self)
+            let startIndex = try rangeContainer.decode(Int.self, forKey: .location)
+            let count = try rangeContainer.decode(Int.self, forKey: .length)
+            self.insert(integersIn: startIndex ..< (startIndex + count))
+        }
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        var indexesContainer = container.nestedUnkeyedContainer(forKey: .indexes)
+
+        for range in self.rangeView {
+            var rangeContainer = indexesContainer.nestedContainer(keyedBy: RangeCodingKeys.self)
+            try rangeContainer.encode(range.startIndex, forKey: .location)
+            try rangeContainer.encode(range.count, forKey: .length)
+        }
+    }
+}
--- stdlib/public/SDK/Foundation/NSRange.swift
+++ stdlib/public/SDK/Foundation/NSRange.swift
@@ -172,3 +172,17 @@ extension NSRange : CustomPlaygroundQuickLookable {
     }
 }
 
+extension NSRange : Codable {
+    public init(from decoder: Decoder) throws {
+        var container = try decoder.unkeyedContainer()
+        let location = try container.decode(Int.self)
+        let length = try container.decode(Int.self)
+        self.init(location: location, length: length)
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.unkeyedContainer()
+        try container.encode(self.location)
+        try container.encode(self.length)
+    }
+}
