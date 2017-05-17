aab6603616f549c3010147a697bfc9b38f1eb5e6
John McCall <rjmccall@apple.com>
2017-05-16 22:03:07 -0400

    Don't mirror-import a protocol method if there's another method in the class's protocol hierarchy that overrides it.
    rdar://31471034

---
+++ test/ClangImporter/Inputs/mirror_import_overrides_2.h
@@ -0,0 +1,23 @@
+@protocol Context
+- (void) operate;
+@end
+
+@protocol A
+- (void) use: (nonnull void(^)(__nonnull id)) callback;
+@end
+
+@protocol B<A>
+@end
+
+@protocol C<A>
+- (void) use: (nonnull void(^)(__nonnull id<Context>)) callback;
+@end
+
+@protocol D<B, C>
+@end
+
+@interface NSObject
+@end
+
+@interface Widget : NSObject<D>
+@end
