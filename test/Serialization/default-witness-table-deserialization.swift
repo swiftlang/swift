47d8e74819c282026d1950987f197a35f5b44fda
Jordan Rose <jordan_rose@apple.com>
2017-05-17 19:29:37 -0700

    Add -disable-sil-linking to four simple SIL serialization tests. (#9723)
    What these tests are testing has nothing to do with whether or not we
    deserialize anything from the standard library, and not doing so saves
    more than two minutes (single-threaded) on my machine from each test.

--- test/Serialization/default-witness-table-deserialization.swift
+++ test/Serialization/default-witness-table-deserialization.swift
@@ -1,4 +1,4 @@
-// RUN: %target-swift-frontend %s -module-name Test -sil-serialize-all -emit-module -emit-module-path - -o /dev/null | %target-sil-opt -enable-sil-verify-all -module-name="Test" | %FileCheck %s
+// RUN: %target-swift-frontend %s -module-name Test -sil-serialize-all -emit-module -emit-module-path - -o /dev/null | %target-sil-opt -enable-sil-verify-all -disable-sil-linking -module-name="Test" | %FileCheck %s
 
 // Check that default witness tables are properly deserialized.
 // rdar://problem/29173229
