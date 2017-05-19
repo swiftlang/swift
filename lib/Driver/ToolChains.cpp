4640246263f3fbada37a631280a94b1360a80636
Michael Ilseman <milseman@apple.com>
2017-05-18 19:41:55 -0700

    Revert "[stdlib] Static stdlib requires explicit -licucore"
    This reverts commit 0de242511201d409a3c36fc2011a988dd85c1e4a.

--- lib/Driver/ToolChains.cpp
+++ lib/Driver/ToolChains.cpp
@@ -1240,7 +1240,6 @@ toolchains::Darwin::constructInvocation(const LinkJobAction &job,
     Arguments.push_back("-framework");
     Arguments.push_back("Foundation");
     Arguments.push_back("-force_load_swift_libs");
-    Arguments.push_back("-licucore");
   } else {
     Arguments.push_back(context.Args.MakeArgString(RuntimeLibPath));
   }
