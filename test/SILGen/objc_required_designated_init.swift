6a95c8628ef0236f31c7dd9ebad529c095b16a00
Joe Groff <jgroff@apple.com>
2017-05-19 17:55:50 -0700

    SILGen: Use VarDecl::getInterfaceType instead of getType in emitForeignToNativeThunk.
    getType() only gets set if the decl is type-checked, and is deprecated moreover. Fixes rdar://problem/32280288.

---
+++ test/SILGen/objc_required_designated_init.swift
@@ -0,0 +1,12 @@
+// RUN: rm -rf %t && mkdir -p %t
+// RUN: %target-swift-frontend -emit-module %S/Inputs/objc_required_designated_init_2.swift -module-name Booms -o %t/Booms.swiftmodule
+// RUN: %target-swift-frontend -I %t -emit-silgen -verify %s
+
+import Booms
+
+class Baboom: Boom {
+  required init() {
+    super.init()
+  }
+}
+
