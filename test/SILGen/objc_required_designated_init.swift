e8245256cc48f47f065b5f8e561f2ee1ad2b1f4f
Joe Groff <arcata@gmail.com>
2017-05-20 12:22:35 -0700

    Test requires objc_interop

--- test/SILGen/objc_required_designated_init.swift
+++ test/SILGen/objc_required_designated_init.swift
@@ -1,6 +1,7 @@
 // RUN: rm -rf %t && mkdir -p %t
 // RUN: %target-swift-frontend -emit-module %S/Inputs/objc_required_designated_init_2.swift -module-name Booms -o %t/Booms.swiftmodule
 // RUN: %target-swift-frontend -I %t -emit-silgen -verify %s
+// REQUIRES: objc_interop
 
 import Booms
 
