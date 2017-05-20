36ac387b6acd703755a08437c5ff597a81cd09f8
George Karpenkov <ekarpenkov@apple.com>
2017-05-19 17:10:19 -0700

    Fuzzing tests

---
+++ test/Fuzzing/fuzzer_test.swift
@@ -0,0 +1,21 @@
+// RUN: %target-swift-frontend(mock-sdk: %clang-importer-sdk) -parse-as-library -sanitize=fuzzer,address -c %s -o %S/file.o
+// RUN: %clang %target-cc-options -isysroot %sdk -fobjc-arc -fsanitize-coverage=edge,trace-pc-guard -c %S/objcwrapper.m -c -o %S/objcwrapper.o
+// RUN: %target-swiftc_driver -sanitize=fuzzer,address %S/objcwrapper.o %S/file.o -o %S/fuzztarget
+// RUN: not %S/fuzztarget -only_ascii=1 -max_len=3 | %FileCheck -check-prefix=CRASH %s
+// CRASH: Crash!
+
+// Cleanup.
+// RUN: rm -f %S/*.o %S/fuzztarget
+
+// REQUIRES: objc_interop
+
+import Foundation
+
+@objc(Fuzzer) class Fuzzer : NSObject {
+  @objc static func fuzzOneInput(Data: UnsafePointer<CChar>, Size: CLong) {
+    if (Size >= 3 && Data[0] == 65 && Data[1] == 66 && Data[2] == 67) {
+        print("Crash!");
+        assert(false);
+    }
+  }
+}
