8c43b67cebbb89f4d5bf823f5b5299a79f95002b
Ewa Matejska <ematejska@Ewas-MBP.attlocal.net>
2017-05-19 20:36:18 -0700

    requiring objc interop to avoid linux test failure

--- test/SILGen/objc_required_designated_init.swift
+++ test/SILGen/objc_required_designated_init.swift
@@ -2,6 +2,8 @@
 // RUN: %target-swift-frontend -emit-module %S/Inputs/objc_required_designated_init_2.swift -module-name Booms -o %t/Booms.swiftmodule
 // RUN: %target-swift-frontend -I %t -emit-silgen -verify %s
 
+// REQUIRES: objc_interop
+
 import Booms
 
 class Baboom: Boom {
