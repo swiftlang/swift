02f30cf520b524a78bb27f45b5e90a0da3051cc6
George Karpenkov <ekarpenkov@apple.com>
2017-05-24 14:12:11 -0700

    Simpler tests for libFuzzer, without Objective-C bridge!

--- test/Fuzzing/fuzzer_test.swift
+++ test/Fuzzing/fuzzer_test.swift
@@ -1,21 +1,13 @@
-// RUN: %target-swift-frontend(mock-sdk: %clang-importer-sdk) -parse-as-library -sanitize=fuzzer,address -c %s -o %S/file.o
-// RUN: %clang %target-cc-options -isysroot %sdk -fobjc-arc -fsanitize-coverage=edge,trace-pc-guard -c %S/objcwrapper.m -c -o %S/objcwrapper.o
-// RUN: %target-swiftc_driver -sanitize=fuzzer,address %S/objcwrapper.o %S/file.o -o %S/fuzztarget
-// RUN: not %S/fuzztarget -only_ascii=1 -max_len=3 | %FileCheck -check-prefix=CRASH %s
-// CRASH: Crash!
-
-// Cleanup.
-// RUN: rm -f %S/*.o %S/fuzztarget
-
-// REQUIRES: objc_interop
+// RUN: %target-build-swift -parse-as-library -sanitize=fuzzer %s -o %t
+// RUN: not %t -only_ascii=1 -max_len=3 | %FileCheck %s
+// CHECK: Crash!
 
 import Foundation
 
-@objc(Fuzzer) class Fuzzer : NSObject {
-  @objc static func fuzzOneInput(Data: UnsafePointer<CChar>, Size: CLong) {
-    if (Size >= 3 && Data[0] == 65 && Data[1] == 66 && Data[2] == 67) {
-        print("Crash!");
-        assert(false);
-    }
+@_cdecl("LLVMFuzzerTestOneInput") public func fuzzOneInput(Data: UnsafePointer<CChar>, Size: CLong) -> CInt {
+  if (Size >= 3 && Data[0] == 65 && Data[1] == 66 && Data[2] == 67) {
+    print("Crash!");
+    exit(1);
   }
+  return 0;
 }
