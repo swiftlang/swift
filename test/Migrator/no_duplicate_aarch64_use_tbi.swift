0bbd73af98164746ef1aa6ddf64fcca916092ccc
Ben Langmuir <blangmuir@apple.com>
2017-05-30 13:43:20 -0700

    [test] Fix REQUIRES lines in test
    Attempt to fix rdar://problem/32470725

--- test/Migrator/no_duplicate_aarch64_use_tbi.swift
+++ test/Migrator/no_duplicate_aarch64_use_tbi.swift
@@ -1,5 +1,5 @@
-// REQUIRES OS=ios
-// REQUIRES CPU=arm64
+// REQUIRES: OS=ios
+// REQUIRES: CPU=arm64
 // RUN: %target-swift-frontend -typecheck %s -swift-version 3
 // RUN: rm -rf %t && mkdir -p %t && cd %t && %swiftc_driver -c -update-code -target arm64-apple-ios10.3 -output-file-map %S/Inputs/no_duplicate_aarch64_use_tbi_ofm.json -swift-version 3 %s -v
 // RUN: cd %t && %swiftc_driver -c -update-code -target arm64-apple-ios10.3 -output-file-map %S/Inputs/no_duplicate_aarch64_use_tbi_ofm.json -swift-version 3 %s -### > %t/driver_actions.txt
