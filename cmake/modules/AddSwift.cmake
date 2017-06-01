304f556f5d438b217a3c7d7fd485ad7fe8166521
John Holdsworth <github@johnholdsworth.com>
2017-06-01 01:53:29 +0100

    Cmake changes for -llog and -latomic V

--- cmake/modules/AddSwift.cmake
+++ cmake/modules/AddSwift.cmake
@@ -359,7 +359,7 @@ function(_add_variant_link_flags)
     endif()
   elseif("${LFLAGS_SDK}" STREQUAL "ANDROID")
     list(APPEND result
-        "-ldl" "-llog" "-latomic"
+        "-ldl" "-llog" "-latomic" "-licudata" "-licui18n" "-licuuc"
         "${SWIFT_ANDROID_NDK_PATH}/sources/cxx-stl/llvm-libc++/libs/armeabi-v7a/libc++_shared.so")
     list(APPEND library_search_directories
         "${SWIFT_ANDROID_PREBUILT_PATH}/arm-linux-androideabi/lib/armv7-a"
