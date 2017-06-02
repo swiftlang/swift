fa78d9259ec4b62c7768c99398502c9edd81ccc4
Xi Ge <xi_ge@apple.com>
2017-06-02 14:20:35 -0700

    migrator/test: move all API diff data files to Inputs directory. NFC (#10074)

--- test/Migrator/member.swift
+++ test/Migrator/member.swift
@@ -1,5 +1,5 @@
 // REQUIRES: objc_interop
-// RUN: rm -rf %t && mkdir -p %t && %target-swift-frontend -c -update-code -primary-file %s -F %S/mock-sdk -api-diff-data-file %S/API.json -emit-migrated-file-path %t/member.swift.result -emit-remap-file-path %t/member.swift.remap -o /dev/null
+// RUN: rm -rf %t && mkdir -p %t && %target-swift-frontend -c -update-code -primary-file %s -F %S/mock-sdk -api-diff-data-file %S/Inputs/API.json -emit-migrated-file-path %t/member.swift.result -emit-remap-file-path %t/member.swift.remap -o /dev/null
 // RUN: diff -u %S/member.swift.expected %t/member.swift.result
 
 import Bar
