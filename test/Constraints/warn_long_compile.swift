a0ae313c2075d933a9eae57e9a3dc2f64c39c343
Mark Lacey <mark.lacey@apple.com>
2017-06-13 00:00:43 -0700

    Fix test failure by removing function bodies.

--- test/Constraints/warn_long_compile.swift
+++ test/Constraints/warn_long_compile.swift
@@ -1,6 +1,9 @@
 // RUN: %target-typecheck-verify-swift -warn-long-expression-type-checking=1 -warn-long-function-bodies=1 %s
-func foo<T>(_ x: T) -> T { return x }
-func foo(_ x: Int) -> Int { return x }
+@_silgen_name("generic_foo")
+func foo<T>(_ x: T) -> T
+
+@_silgen_name("foo_of_int")
+func foo(_ x: Int) -> Int
 
 func test(m: Double) -> Int {
   // expected-warning@-1 {{global function 'test(m:)' took}}
