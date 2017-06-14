4c6ece6fe0199a0948781cbef1911c4fd8d42982
Rintaro Ishizaki <fs.output@gmail.com>
2017-06-14 15:09:24 +0900

    [Parse] Don't invalidate compilation condition with invalid platform condition argument (#10221)
    It's just a warning.

--- test/Parse/ConditionalCompilation/basicParseErrors.swift
+++ test/Parse/ConditionalCompilation/basicParseErrors.swift
@@ -114,3 +114,27 @@ undefinedFunc() // ignored.
 #else
 undefinedFunc() // expected-error {{use of unresolved identifier 'undefinedFunc'}}
 #endif
+
+/// Invalid platform condition arguments don't invalidate the whole condition.
+#if !arch(tecture) && !os(ystem) && !_endian(ness)
+// expected-warning@-1 {{unknown architecture for build configuration 'arch'}}
+// expected-note@-2 {{did you mean 'arm'?}} {{11-18=arm}}
+// expected-warning@-3 {{unknown operating system for build configuration 'os'}}
+// expected-note@-4 {{did you mean 'OSX'?}} {{27-32=OSX}}
+// expected-note@-5 {{did you mean 'PS4'?}} {{27-32=PS4}}
+// expected-warning@-6 {{unknown endianness for build configuration '_endian'}}
+// expected-note@-7 {{did you mean 'big'?}} {{46-50=big}}
+func fn_k() {}
+#endif
+fn_k()
+
+#if os(cillator) || arch(ive)
+// expected-warning@-1 {{unknown operating system for build configuration 'os'}}
+// expected-note@-2 {{did you mean 'macOS'?}} {{8-16=macOS}}
+// expected-note@-3 {{did you mean 'iOS'?}} {{8-16=iOS}}
+// expected-warning@-4 {{unknown architecture for build configuration 'arch'}}
+// expected-note@-5 {{did you mean 'arm'?}} {{26-29=arm}}
+// expected-note@-6 {{did you mean 'i386'?}} {{26-29=i386}}
+func undefinedFunc() // ignored.
+#endif
+undefinedFunc() // expected-error {{use of unresolved identifier 'undefinedFunc'}}
