fc3de906809d60e38bff11d11035cedcce425dde
Roman Levenstein <rlevenstein@apple.com>
2017-06-16 09:33:44 -0700

    Fix SIL serialization of witness tables and protocol witness thunks
    Serialize witness tables only if it -sil-serialize-all or -sil-serialize-witness-tables options are provided, which should happen only when building the stdlib and overlays, or it is a compilation with explicitly enabled resilience support. Disable serialization of witness tables in all other cases.
    
    Disabling the serialization of witness tables also avoids serialization of protocol witness thunks, whose serialization in 3rd party code sometimes resulted in performance degradation, because resilience checking rules used by many parts of the compiler would prohibit certain optimizations like inlining or specialization, if the caller was serialized and the callee would not be serialized.
    
    This should fix rdar://32718853   and address rdar://32743391

--- lib/SILGen/SILGenType.cpp
+++ lib/SILGen/SILGenType.cpp
@@ -315,11 +315,14 @@ public:
 
     // Serialize the witness table if we're serializing everything with
     // -sil-serialize-all, or if the conformance itself thinks it should be.
-    if (SGM.isMakeModuleFragile())
+    if (SGM.isMakeModuleFragile() ||
+        SGM.M.getOptions().SILSerializeWitnessTables)
       Serialized = IsSerialized;
 
     auto *nominal = Conformance->getType()->getAnyNominal();
-    if (nominal->hasFixedLayout() &&
+    if (SGM.M.getSwiftModule()->getResilienceStrategy() ==
+            ResilienceStrategy::Resilient &&
+        nominal->hasFixedLayout() &&
         proto->getEffectiveAccess() >= Accessibility::Public &&
         nominal->getEffectiveAccess() >= Accessibility::Public)
       Serialized = IsSerialized;
