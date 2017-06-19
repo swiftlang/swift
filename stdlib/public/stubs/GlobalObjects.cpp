2cf7a565bd35399d0f6446f2be325f403b495a91
Jordan Rose <jordan_rose@apple.com>
2017-06-19 09:25:49 -0700

    Revert "Merge pull request #10357 from aschwaighofer/irgen_cleanup_code"
    This reverts commit 45dda950b1da6e992e9ce764323e5fd13cacfc57, reversing
    changes made to 48e889b51ba4302f948c5d91d148b96a1919dae6.

--- stdlib/public/stubs/GlobalObjects.cpp
+++ stdlib/public/stubs/GlobalObjects.cpp
@@ -35,6 +35,10 @@ ClassMetadata CLASS_METADATA_SYM(s27_RawNativeDictionaryStorage);
 // _direct type metadata for Swift._RawNativeSetStorage
 SWIFT_RUNTIME_STDLIB_INTERFACE
 ClassMetadata CLASS_METADATA_SYM(s20_RawNativeSetStorage);
+
+// _direct type metadata for Swift._EmptyBoxStorage
+SWIFT_RUNTIME_STDLIB_INTERFACE
+ClassMetadata CLASS_METADATA_SYM(s16_EmptyBoxStorage);
 } // namespace swift
 
 swift::_SwiftEmptyArrayStorage swift::_swiftEmptyArrayStorage = {
@@ -134,6 +138,7 @@ swift::_SwiftEmptyBoxStorage swift::_EmptyBoxStorage = {
  // HeapObject header;
   {
     &_emptyBoxStorageMetadata,
+    //&swift::CLASS_METADATA_SYM(s16_EmptyBoxStorage), // isa pointer
   }
 };
 
