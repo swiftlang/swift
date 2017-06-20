83807a05fef02245f33c8d8082d7293849813c3b
Xi Ge <xi_ge@apple.com>
2017-06-20 12:59:07 -0700

    sema: missing switch case fixits should print payload names explicitly instead of printing underscores. rdar://32121806

--- lib/Sema/TypeCheckSwitchStmt.cpp
+++ lib/Sema/TypeCheckSwitchStmt.cpp
@@ -77,6 +77,10 @@ namespace {
       SpaceKind Kind;
       llvm::PointerIntPair<Type, 1, bool> TypeAndVal;
       Identifier Head;
+
+      // This is for type space only to help us print meaningful name, e.g.,
+      // tuple element name in fixits.
+      Identifier NameForPrinting;
       std::forward_list<Space> Spaces;
 
       // NB: This constant is arbitrary.  Anecdotally, the Space Engine is
@@ -134,9 +138,9 @@ namespace {
       }
       
     public:
-      explicit Space(Type T)
+      explicit Space(Type T, Identifier NameForPrinting)
         : Kind(SpaceKind::Type), TypeAndVal(T, false), Head(Identifier()),
-          Spaces({}){}
+          NameForPrinting(NameForPrinting), Spaces({}){}
       explicit Space(Type T, Identifier H, bool downgrade,
                      SmallVectorImpl<Space> &SP)
         : Kind(SpaceKind::Constructor), TypeAndVal(T, downgrade), Head(H),
@@ -750,7 +754,10 @@ namespace {
           if (!forDisplay) {
             getType()->print(buffer);
           }
-          buffer << "_";
+          if (NameForPrinting.empty())
+            buffer << "_";
+          else
+            buffer << tok::kw_let << " " << NameForPrinting.str();
           break;
         }
       }
@@ -865,10 +872,11 @@ namespace {
                                TTy->getElements().end(),
                                std::back_inserter(constElemSpaces),
                                [&](TupleTypeElt ty){
-                                 return Space(ty.getType());
+                                 return Space(ty.getType(), ty.getName());
                                });
               } else if (auto *TTy = dyn_cast<ParenType>(eedTy.getPointer())) {
-                constElemSpaces.push_back(Space(TTy->getUnderlyingType()));
+                constElemSpaces.push_back(Space(TTy->getUnderlyingType(),
+                                                Identifier()));
               }
             }
             return Space(tp, eed->getName(),
@@ -882,7 +890,7 @@ namespace {
           std::transform(TTy->getElements().begin(), TTy->getElements().end(),
                          std::back_inserter(constElemSpaces),
                          [&](TupleTypeElt ty){
-            return Space(ty.getType());
+            return Space(ty.getType(), ty.getName());
           });
           // Create an empty constructor head for the tuple space.
           arr.push_back(Space(tp, Identifier(), /*canDowngrade*/false,
@@ -943,7 +951,7 @@ namespace {
         }
       }
       
-      Space totalSpace(Switch->getSubjectExpr()->getType());
+      Space totalSpace(Switch->getSubjectExpr()->getType(), Identifier());
       Space coveredSpace(spaces);
       size_t totalSpaceSize = totalSpace.getSize(TC);
       if (totalSpaceSize > Space::getMaximumSize()) {
@@ -1218,8 +1226,9 @@ namespace {
                                 bool &sawDowngradablePattern) {
       switch (item->getKind()) {
       case PatternKind::Any:
+        return Space(item->getType(), Identifier());
       case PatternKind::Named:
-        return Space(item->getType());
+        return Space(item->getType(), cast<NamedPattern>(item)->getBoundName());
       case PatternKind::Bool: {
         return Space(cast<BoolPattern>(item)->getValue());
       }
@@ -1231,7 +1240,7 @@ namespace {
           // These coercions are irrefutable.  Project with the original type
           // instead of the cast's target type to maintain consistency with the
           // scrutinee's type.
-          return Space(IP->getType());
+          return Space(IP->getType(), Identifier());
         case CheckedCastKind::Unresolved:
         case CheckedCastKind::ValueCast:
         case CheckedCastKind::ArrayDowncast:
@@ -1316,7 +1325,7 @@ namespace {
               || SP->getKind() == PatternKind::Tuple) {
             if (auto *TTy = SP->getType()->getAs<TupleType>()) {
               for (auto ty : TTy->getElements()) {
-                conArgSpace.push_back(Space(ty.getType()));
+                conArgSpace.push_back(Space(ty.getType(), ty.getName()));
               }
             } else {
               conArgSpace.push_back(projectPattern(TC, SP,
