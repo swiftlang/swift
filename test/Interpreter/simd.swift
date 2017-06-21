cf2e2123a59fce26ee25fb819f77d55d233f3f59
Arnold Schwaighofer <aschwaighofer@apple.com>
2017-06-21 12:06:09 -0700

    IRGen: Use LLVM's alloc size to compute padding between types
    We need to subtract alignment padding when doing type layout in terms of llvm types
    
    rdar://32618125
    SR-5137

---
+++ test/Interpreter/simd.swift
@@ -0,0 +1,30 @@
+// RUN: %target-run-simple-swift | %FileCheck %s
+// REQUIRES: executable_test
+
+import simd
+
+public struct Vector3f {
+  var f3: float3
+  init() {
+    f3 = float3(0, 1, 2)
+  }
+}
+
+public struct TwoFloat {
+  var f0 : Float
+  var f1 : Float
+  init() {
+    f0 = 0.0
+    f1 = 1.0
+  }
+}
+
+public func test() -> (Vector3f, TwoFloat) {
+  let v = Vector3f()
+  let y = TwoFloat()
+  let r = (v, y)
+  return r
+}
+
+// CHECK: (main.Vector3f(f3: float3(0.0, 1.0, 2.0)), main.TwoFloat(f0: 0.0, f1: 1.0))
+print(test())
