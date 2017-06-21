c2aa020bfceb0bdfbff19c9f0c0ea0c554251aae
Jordan Rose <jordan_rose@apple.com>
2017-06-20 18:12:35 -0700

    Fix up 08c2a7a3ac for 32-bit platforms.
    ...where swift::TwoWordPair is defined a little less freely.

--- stdlib/public/SDK/Foundation/CheckClass.mm
+++ stdlib/public/SDK/Foundation/CheckClass.mm
@@ -56,8 +56,9 @@ namespace {
     template <size_t N>
     StringRefLite(const char (&staticStr)[N]) : data(staticStr), length(N) {}
 
-    StringRefLite(swift::TwoWordPair<const char *, uintptr_t> pair)
-        : data(pair.first), length(pair.second) {}
+    StringRefLite(swift::TwoWordPair<const char *, uintptr_t>::Return rawValue)
+        : data(swift::TwoWordPair<const char *, uintptr_t>(rawValue).first),
+          length(swift::TwoWordPair<const char *, uintptr_t>(rawValue).second){}
 
     NS_RETURNS_RETAINED
     NSString *newNSStringNoCopy() const {
