2af1a77fadb6b3e8dda290b0b8e9bbfea3b40f42
Harlan Haskins <harlan@apple.com>
2017-06-23 12:55:50 -0700

    UnknownSyntax now accounts for non-token children.

--- lib/Syntax/UnknownSyntax.cpp
+++ lib/Syntax/UnknownSyntax.cpp
@@ -23,10 +23,32 @@ void UnknownSyntax::validate() const {
 #pragma mark - unknown-syntax API
 
 size_t UnknownSyntax::getNumChildren() const {
-  return Data->getNumChildren();
+  size_t NonTokenChildren = 0;
+  for (auto Child : getRaw()->Layout) {
+    if (!Child->isToken()) {
+      ++NonTokenChildren;
+    }
+  }
+  return NonTokenChildren;
 }
 
 Syntax UnknownSyntax::getChild(const size_t N) const {
-  return Syntax { Root, Data->getChild(N).get() };
+  // The actual index of the Nth non-token child.
+  size_t ActualIndex = 0;
+  // The number of non-token children we've seen.
+  size_t NumNonTokenSeen = 0;
+  for (auto Child : getRaw()->Layout) {
+    // If we see a child that's not a token, count it.
+    if (!Child->isToken()) {
+      ++NumNonTokenSeen;
+    }
+    // If the number of children we've seen indexes the same (count - 1) as
+    // the number we're looking for, then we're done.
+    if (NumNonTokenSeen == N + 1) { break; }
+
+    // Otherwise increment the actual index and keep searching.
+    ++ActualIndex;
+  }
+  return Syntax { Root, Data->getChild(ActualIndex).get() };
 }
 
