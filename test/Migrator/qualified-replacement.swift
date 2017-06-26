db605623bb1d1cdd17cfea4e716e7e598e43a7c1
Xi Ge <xi_ge@apple.com>
2017-06-26 15:47:17 -0700

    migrator: handle qualified replacement for member reference expression. rdar://32845918 (#10593)
    Previously, we only handle dot syntax call expression for qualified
    replacement, i.e. changing from A.a to B.b. This patch teaches the tool
    to handle the migration of member reference expression tool.

--- test/Migrator/qualified-replacement.swift
+++ test/Migrator/qualified-replacement.swift
@@ -1,11 +1,15 @@
 // REQUIRES: objc_interop
-// RUN: %empty-directory(%t) && %target-swift-frontend -c -update-code -primary-file %s -F %S/mock-sdk -api-diff-data-file %S/Inputs/qualified.json -emit-migrated-file-path %t/qualified-replacement.swift.result -emit-remap-file-path %t/qualified-replacement.swift.remap -o /dev/null
+// RUN: %empty-directory(%t.mod)
+// RUN: %target-swift-frontend -emit-module -o %t.mod/Cities.swiftmodule %S/Inputs/Cities.swift -module-name Cities -parse-as-library
+// RUN: %empty-directory(%t) && %target-swift-frontend -c -update-code -primary-file %s -F %S/mock-sdk -I %t.mod -api-diff-data-file %S/Inputs/qualified.json -emit-migrated-file-path %t/qualified-replacement.swift.result -emit-remap-file-path %t/qualified-replacement.swift.remap -o /dev/null
 // RUN: diff -u %S/qualified-replacement.swift.expected %t/qualified-replacement.swift.result
 
+import Cities
 import Bar
 func foo() {
   _ = PropertyUserInterface.fieldPlus
   PropertyUserInterface.methodPlus(1)
   _ = FooComparisonResult.orderedSame
   let _ : FooComparisonResult = .orderedSame
+  _ = Cities.CityKind.Town
 }
