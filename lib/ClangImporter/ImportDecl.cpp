c22be13c13f5f89b2aa64e959d52718dd8cf7050
Doug Gregor <dgregor@apple.com>
2017-06-28 15:48:55 -0700

    [Clang importer] Compute requirement signature before setting environment.
    Match what the type checker is doing.
    
    (cherry picked from commit 75f3c99382211c23946a2ba15b5d540c7f317a1e)

--- lib/ClangImporter/ImportDecl.cpp
+++ lib/ClangImporter/ImportDecl.cpp
@@ -4159,12 +4159,12 @@ namespace {
       result->setInherited(Impl.SwiftContext.AllocateCopy(inheritedTypes));
       result->setCheckedInheritanceClause();
 
-      auto *env = Impl.buildGenericEnvironment(result->getGenericParams(), dc);
-      result->setGenericEnvironment(env);
-
       // Compute the requirement signature.
       if (!result->isRequirementSignatureComputed())
         result->computeRequirementSignature();
+
+      auto *env = Impl.buildGenericEnvironment(result->getGenericParams(), dc);
+      result->setGenericEnvironment(env);
 
       result->setMemberLoader(&Impl, 0);
 
