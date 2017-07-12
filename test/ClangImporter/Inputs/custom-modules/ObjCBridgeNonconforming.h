1c84e5de4c67a7d16f3dce1f58efb1b08d27a9fe
Jordan Rose <jordan_rose@apple.com>
2017-07-11 19:12:47 -0700

    [ClangImporter] Generic parameters can be Hashable too! (#10897)
    aa215e7e54 made sure we didn't try to construct Sets and Dictionaries
    with a non-Hashable key. However, that commit was a little too
    restrictive: there was no handling for imported generic parameters
    that were constrained to inherit from NSObject. Fortunately,
    recovering that information is fairly straightforward.
    
    rdar://problem/33222646

--- test/ClangImporter/Inputs/custom-modules/ObjCBridgeNonconforming.h
+++ test/ClangImporter/Inputs/custom-modules/ObjCBridgeNonconforming.h
@@ -4,3 +4,26 @@
 @interface ObjCBridgeNonconforming
 @property NSSet<NSDictionary<NSString *, id> *> * _Nonnull foo;
 @end
+
+@interface ObjCBridgeGeneric<Element> : NSObject
+@property NSSet<Element> * _Nonnull foo;
+@end
+
+@interface ElementBase : NSObject
+@end
+@protocol ExtraElementProtocol
+@end
+@interface ElementConcrete : ElementBase <ExtraElementProtocol>
+@end
+
+@interface ObjCBridgeGenericConstrained<Element: ElementBase *> : NSObject
+@property NSSet<Element> * _Nonnull foo;
+@end
+
+@interface ObjCBridgeGenericInsufficientlyConstrained<Element: id <NSObject>> : NSObject
+@property NSSet<Element> * _Nonnull foo;
+@end
+
+@interface ObjCBridgeGenericConstrainedExtra<Element: NSObject <ExtraElementProtocol> *> : NSObject
+@property NSSet<Element> * _Nonnull foo;
+@end
