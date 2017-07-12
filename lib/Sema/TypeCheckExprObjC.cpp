3b9481f0e5d5340a83e24c995862fd882d4c6252
Pavel Yaskevich <pyaskevich@apple.com>
2017-07-12 14:04:10 -0700

    [QoI] Don't try to lookup members on incorrect type during Objective-C KeyPath validation
    While trying to validate Objective-C keypath components
    don't assume that type of the component is always correct, check
    before trying to see if it's bridged type or has members.
    
    Resolves: rdar://problem/33044867
    (cherry picked from commit 481401f6c3c754c5e88b496b2353f2488fb4b584)

--- lib/Sema/TypeCheckExprObjC.cpp
+++ lib/Sema/TypeCheckExprObjC.cpp
@@ -166,6 +166,8 @@ Optional<Type> TypeChecker::checkObjCKeyPathExpr(DeclContext *dc,
       return lookupUnqualified(dc, componentName, componentNameLoc);
 
     assert(currentType && "Non-beginning state must have a type");
+    if (!currentType->mayHaveMembers())
+      return LookupResult();
 
     // Determine the type in which the lookup should occur. If we have
     // a bridged value type, this will be the Objective-C class to
