ecfa9ebfca05b67f60471accc53670989a68f2bf
Nathan Hawes <nhawes@apple.com>
2017-07-13 15:37:17 -0700

    [interface-gen] Rename AreSwiftArgs -> UsingSwiftArgs throughout

--- tools/SourceKit/tools/sourcekitd-test/sourcekitd-test.cpp
+++ tools/SourceKit/tools/sourcekitd-test/sourcekitd-test.cpp
@@ -580,8 +580,8 @@ static int handleTestInvocation(ArrayRef<const char *> Args,
       sourcekitd_request_dictionary_set_uid(Req, KeyRequest,
                                             RequestEditorOpenSwiftSourceInterface);
     } else {
-      if (Opts.AreSwiftArgs)
-          sourcekitd_request_dictionary_set_int64(Req, KeyAreSwiftArgs, true);
+      if (Opts.UsingSwiftArgs)
+          sourcekitd_request_dictionary_set_int64(Req, KeyUsingSwiftArgs, true);
       sourcekitd_request_dictionary_set_uid(Req, KeyRequest,
                                             RequestEditorOpenHeaderInterface);
     }
