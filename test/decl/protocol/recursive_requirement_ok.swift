ded45a6e1c498e69e56f38ddf32c8942f407db8c
Doug Gregor <dgregor@apple.com>
2017-07-12 17:11:13 -0700

    [GSB] SE-0157: Reprocess delayed requirements when we need a complete PA.
    Whenever we need a complete, well-formed potential archetype,
    reprocess any delayed requirements, so that we pick up additional
    requirements on that potential archetype.
    
    This relies on us tracking a generation count for the GSB instance as
    a whole, which gets bumped each time we add some new requirement or
    create a new potential archetype, and only actually reprocessing
    delayed requirements when the generation count exceeds the point at
    which we last processed delayed requirements.
    
    This gets the most basic recursive protocol constraint working
    end-to-end and doesn't seem to break things.

--- test/decl/protocol/recursive_requirement_ok.swift
+++ test/decl/protocol/recursive_requirement_ok.swift
@@ -9,5 +9,6 @@ protocol P {
 func testP<T: P>(_ t: T) {
   testP(t.assoc)
   testP(t.assoc.assoc)
-  testP(t.assoc.assoc.assoc) // FIXME: expected-error{{argument type 'T.Assoc.Assoc.Assoc' does not conform to expected type 'P'}}
+  testP(t.assoc.assoc.assoc)
+  testP(t.assoc.assoc.assoc.assoc.assoc.assoc.assoc)
 }
