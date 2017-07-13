fc93261c1551b09932f8e24a705698a416d0e63d
Simon Evans <si@si.org>
2017-07-14 00:08:36 +0100

    Update the linker -Xlinker argument reordering test.
    - Use -driver-print-jobs to avoid actually building the test file.

--- test/Driver/linker-args-order-linux.swift
+++ test/Driver/linker-args-order-linux.swift
@@ -1,11 +1,8 @@
 // Statically link a "hello world" program
-// REQUIRES: OS=linux-gnu
 // REQUIRES: static_stdlib
 print("hello world!")
 // RUN: %empty-directory(%t)
-// RUN: %target-swiftc_driver -v -static-stdlib -o %t/static-stdlib %s -Xlinker --no-allow-multiple-definition 2>&1| %FileCheck %s
-// CHECK: Swift version
-// CHECK: Target: x86_64-unknown-linux-gnu
+// RUN: %target-swiftc_driver -driver-print-jobs -static-stdlib -o %t/static-stdlib %s -Xlinker --no-allow-multiple-definition 2>&1| %FileCheck %s
 // CHECK: {{.*}}/swift -frontend -c -primary-file {{.*}}/linker-args-order-linux.swift -target x86_64-unknown-linux-gnu -disable-objc-interop
 // CHECK: {{.*}}/swift-autolink-extract{{.*}}
 // CHECK: {{.*}}swift_begin.o /{{.*}}/linker-args-order-linux-{{[a-z0-9]+}}.o @/{{.*}}/linker-args-order-linux-{{[a-z0-9]+}}.autolink {{.*}} @{{.*}}/static-stdlib-args.lnk {{.*}} -Xlinker --no-allow-multiple-definition {{.*}}/swift_end.o
