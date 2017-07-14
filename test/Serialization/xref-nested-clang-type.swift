ac567ac0ab1cad7c26fe5777a6a519454e9d409d
Jordan Rose <jordan_rose@apple.com>
2017-07-13 18:00:45 -0700

    [test] Test previous commits with bridging headers too.
    (both precompiled and non-precompiled)

--- test/Serialization/xref-nested-clang-type.swift
+++ test/Serialization/xref-nested-clang-type.swift
@@ -1,8 +1,16 @@
 // RUN: %empty-directory(%t)
-
 // RUN: %target-swift-frontend(mock-sdk: %clang-importer-sdk) -emit-module -o %t -I %S/Inputs/xref-nested-clang-type/ %s -module-name Lib
-
 // RUN: %target-swift-frontend(mock-sdk: %clang-importer-sdk) -typecheck -I %t -I %S/Inputs/xref-nested-clang-type/ %s -DCLIENT -verify
+
+// RUN: %empty-directory(%t)
+// RUN: cp %S/Inputs/xref-nested-clang-type/NestedClangTypes.h %t
+// RUN: %target-swift-frontend(mock-sdk: %clang-importer-sdk) -emit-module -o %t -import-objc-header %t/NestedClangTypes.h %s -module-name Lib -DNO_MODULE
+// RUN: %target-swift-frontend(mock-sdk: %clang-importer-sdk) -typecheck -I %t -import-objc-header %t/NestedClangTypes.h %s -DCLIENT -verify
+
+// RUN: %empty-directory(%t)
+// RUN: cp %S/Inputs/xref-nested-clang-type/NestedClangTypes.h %t
+// RUN: %target-swift-frontend(mock-sdk: %clang-importer-sdk) -emit-module -o %t -import-objc-header %t/NestedClangTypes.h -pch-output-dir %t/PCHCache %s -module-name Lib -DNO_MODULE
+// RUN: %target-swift-frontend(mock-sdk: %clang-importer-sdk) -typecheck -I %t -pch-output-dir %t/PCHCache -import-objc-header %t/NestedClangTypes.h %s -DCLIENT -verify
 
 #if CLIENT
 
@@ -17,7 +25,9 @@ func test(x: MyCode) {}
 
 #else // CLIENT
 
+#if !NO_MODULE
 import NestedClangTypes
+#endif
 
 public typealias MyOuter = Outer
 public typealias MyInner = Outer.InterestingValue
