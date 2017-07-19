8e65747e531fcf3097ba39c5070030a8fe7ffdd8
Itai Ferber <iferber@apple.com>
2017-07-18 17:03:56 -0700

    Add notes to suggest overriding init(from:)
    Classes which inherit from Codable classes inherit their Codable implementation (as opposed to synthesizing their own). In cases where they add their own storage, they will fail to inherit their superclass’s init(from:). In this case, we provide a diagnostic suggesting that init(from:) be overridden.

--- test/decl/protocol/special/coding/class_codable_inheritance_diagnostics.swift
+++ test/decl/protocol/special/coding/class_codable_inheritance_diagnostics.swift
@@ -30,3 +30,42 @@ class InaccessibleNonDecodableSuper {
 
 class InaccessibleNonDecodableSub : InaccessibleNonDecodableSuper, Decodable { // expected-error {{type 'InaccessibleNonDecodableSub' does not conform to protocol 'Decodable'}}
 }
+
+// Subclasses of Decodable classes which can't inherit their initializers should
+// produce diagnostics.
+class DecodableSuper : Decodable {
+  var value = 5
+}
+
+class DecodableSubWithoutInitialValue : DecodableSuper { // expected-error {{class 'DecodableSubWithoutInitialValue' has no initializers}}
+  // expected-note@-1 {{did you mean to override 'init(from:)'?}}
+  var value2: Int // expected-note {{stored property 'value2' without initial value prevents synthesized initializers}}
+}
+
+class DecodableSubWithInitialValue : DecodableSuper {
+  var value2 = 10
+}
+
+// Subclasses of Codable classes which can't inherit their initializers should
+// produce diagnostics.
+class CodableSuper : Codable {
+  var value = 5
+}
+
+class CodableSubWithoutInitialValue : CodableSuper { // expected-error {{class 'CodableSubWithoutInitialValue' has no initializers}}
+  // expected-note@-1 {{did you mean to override 'init(from:)' and 'encode(to:)'?}}
+  var value2: Int // expected-note {{stored property 'value2' without initial value prevents synthesized initializers}}
+}
+
+// We should only mention encode(to:) in the diagnostic if the subclass does not
+// override it.
+class EncodableSubWithoutInitialValue : CodableSuper { // expected-error {{class 'EncodableSubWithoutInitialValue' has no initializers}}
+  // expected-note@-1 {{did you mean to override 'init(from:)'?}}
+  var value2: Int // expected-note {{stored property 'value2' without initial value prevents synthesized initializers}}
+
+  override func encode(to: Encoder) throws {}
+}
+
+class CodableSubWithInitialValue : CodableSuper {
+  var value2 = 10
+}
