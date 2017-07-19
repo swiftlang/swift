c431d5bbc0fc2f087f9b789efaac901d3c72e5d8
Pavel Yaskevich <pyaskevich@apple.com>
2017-07-19 15:04:32 -0700

    [QoI] Add test-case for SR-5505 (attempting to use function name as a type)

--- test/type/types.swift
+++ test/type/types.swift
@@ -176,3 +176,8 @@ class C {
 let _ : inout @convention(c) Int -> Int // expected-error {{'inout' may only be used on parameters}}
 func foo3(inout a: Int -> Void) {} // expected-error {{'inout' before a parameter name is not allowed, place it before the parameter type instead}} {{11-16=}} {{20-20=inout }}
                                    // expected-error @-1 {{single argument function types require parentheses}} {{20-20=(}} {{23-23=)}}
+
+func sr5505(arg: Int) -> String {
+  return "hello"
+}
+var _: sr5505 = sr5505 // expected-error {{use of undeclared type 'sr5505'}}
