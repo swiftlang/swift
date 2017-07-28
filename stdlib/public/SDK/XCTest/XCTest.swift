ba9481f37ab683423b81b2b3b98653a53b769ada
Mishal Shah <mishal_shah@apple.com>
2017-07-28 11:11:49 -0700

    Update swift 4.0 to build with Xcode 9 beta 4, macOS 10.13, iOS 11, tvOS 11, and watchOS 4 SDKs.

--- stdlib/public/SDK/XCTest/XCTest.swift
+++ stdlib/public/SDK/XCTest/XCTest.swift
@@ -20,25 +20,34 @@ import _SwiftXCTestOverlayShims
 public extension XCTContext {
 
   /// Create and run a new activity with provided name and block.
-  public class func runActivity(named name: String, block: (XCTActivity) throws -> ()) rethrows {
+  public class func runActivity<Result>(named name: String, block: (XCTActivity) throws -> Result) rethrows -> Result {
     let context = _XCTContextCurrent()
 
     if _XCTContextShouldStartActivity(context, XCTActivityTypeUserCreated) {
-      try autoreleasepool {
+      return try autoreleasepool {
         let activity = _XCTContextWillStartActivity(context, name, XCTActivityTypeUserCreated)
-        do {
-          try block(activity)
+        defer {
           _XCTContextDidFinishActivity(context, activity)
-        } catch {
-          _XCTContextDidFinishActivity(context, activity)
-          throw error
         }
+        return try block(activity)
       }
     } else {
-      XCTFail("XCTContext.runActivity(named:block:) failed because activities are disallowed in the current configuration.")
+      fatalError("XCTContext.runActivity(named:block:) failed because activities are disallowed in the current configuration.")
     }
   }
 }
+
+#if os(macOS)
+@available(swift 4.0)
+@available(macOS 10.11, *)
+public extension XCUIElement {
+  /// Types a single key from the XCUIKeyboardKey enumeration with the specified modifier flags.
+  @nonobjc public func typeKey(_ key: XCUIKeyboardKey, modifierFlags: XCUIKeyModifierFlags) {
+    // Call the version of the method defined in XCTest.framework.
+    typeKey(key.rawValue, modifierFlags: modifierFlags)
+  }
+}
+#endif
 
 // --- Failure Formatting ---
 
