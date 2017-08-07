4adfcabe9ab2107655be85155d5df2dc0ae38353
George Karpenkov <ekarpenkov@apple.com>
2017-08-07 13:06:33 -0700

    Test should not be ignored on Linux

--- test/Driver/fuzzer.swift
+++ test/Driver/fuzzer.swift
@@ -1,7 +1,4 @@
-// XFAIL: linux
-
 // RUN: %swiftc_driver -driver-print-jobs -sanitize=fuzzer,address %s | %FileCheck -check-prefix=LIBFUZZER %s
-
 
 // LIBFUZZER: libLLVMFuzzer.a
 int LLVMFuzzerTestOneInput(const char *Data, long Size) {
