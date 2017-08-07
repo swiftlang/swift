235178c8501034e45f44b5dd85a9e1c5279a4442
George Karpenkov <ekarpenkov@apple.com>
2017-08-07 13:35:11 -0700

    Use valid Swift code even for the driver test.

--- test/Driver/fuzzer.swift
+++ test/Driver/fuzzer.swift
@@ -1,6 +1,6 @@
 // RUN: %swiftc_driver -driver-print-jobs -sanitize=fuzzer,address %s | %FileCheck -check-prefix=LIBFUZZER %s
 
 // LIBFUZZER: libLLVMFuzzer.a
-int LLVMFuzzerTestOneInput(const char *Data, long Size) {
+@_cdecl("LLVMFuzzerTestOneInput") public func fuzzOneInput(Data: UnsafePointer<CChar>, Size: CLong) -> CInt {
   return 0;
 }
