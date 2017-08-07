2c61ed5c9947156cc74c00ac0d95b36a5f5847ca
Slava Pestov <spestov@apple.com>
2017-08-06 19:52:21 -0700

    Sema: Fix pre-check expression folding nested types of generic parameters
    This allows [Foo.Bar](), [Foo.Bar?]() etc to type check if Bar is
    an associated type of a generic parameter Foo.
    
    Fixes <rdar://problem/27631137>.

--- validation-test/compiler_crashers_fixed/28811-gpdecl-getdepth-generictypeparamdecl-invaliddepth-parameter-hasnt-been-validated.swift
+++ validation-test/compiler_crashers_fixed/28811-gpdecl-getdepth-generictypeparamdecl-invaliddepth-parameter-hasnt-been-validated.swift
@@ -5,7 +5,7 @@
 // See https://swift.org/LICENSE.txt for license information
 // See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
 
-// RUN: not --crash %target-swift-frontend %s -emit-ir
-{
-f
-typealias a:b{}var f=a.a
+// REQUIRES: asserts
+// RUN: not %target-swift-frontend %s -emit-ir
+{<a.h=a
+class a<H
