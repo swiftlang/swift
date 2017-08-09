7c705c3a7562d0a203335d5de2999575cf902a15
Michael Ilseman <milseman@apple.com>
2017-08-09 15:46:52 -0700

    [stdlib] Deprecate String/Substring.CharacterView
    CharacterView is now entirely redundant in Swift 4. Deprecate its
    use. This also allows us to schedule the unbreaking of
    String.CharacterView leakiness without a hard source break.

--- test/stdlib/StringCompatibilityDiagnostics3.swift
+++ test/stdlib/StringCompatibilityDiagnostics3.swift
@@ -3,14 +3,22 @@
 func testPopFirst() {
   var str = "abc"
   _ = str.popFirst() // expected-warning{{'popFirst()' is deprecated: Please use 'first', 'dropFirst()', or 'Substring.popFirst()'}}
-  _ = str.characters.popFirst() // expected-warning{{'popFirst()' is deprecated: Please use 'first', 'dropFirst()', or 'Substring.CharacterView.popFirst()'}}
-    // TODO: ^^^ deprecate the view, and update the warning here
+  _ = str.characters.popFirst() // expected-warning{{'characters' is deprecated: Please use String or Substring directly}}
+    // expected-warning@-1{{'popFirst()' is deprecated: Please use 'first', 'dropFirst()', or 'Substring.CharacterView.popFirst()'}}
   _ = str.unicodeScalars.popFirst() // expected-warning{{'popFirst()' is deprecated: Please use 'first', 'dropFirst()', or 'Substring.UnicodeScalarView.popFirst()'}}
+
+  var charView: String.CharacterView // expected-warning{{'CharacterView' is deprecated: Please use String or Substring directly}}
+  charView = str.characters // expected-warning{{'characters' is deprecated: Please use String or Substring directly}}
+  dump(charView)
 
   var substr = str[...]
   _ = substr.popFirst() // ok
-  _ = substr.characters.popFirst() // ok
+  _ = substr.characters.popFirst() // expected-warning{{'characters' is deprecated: Please use String or Substring directly}}
   _ = substr.unicodeScalars.popFirst() // ok
+
+  var charSubView: Substring.CharacterView // expected-warning{{'CharacterView' is deprecated: Please use String or Substring directly}}
+  charSubView = substr.characters // expected-warning{{'characters' is deprecated: Please use String or Substring directly}}
+  dump(charSubView)
 }
 
 
