599ea03fbdf7e10ed41209bdd4a7ad45d6a882eb
Mark Lacey <mark.lacey@apple.com>
2017-08-10 10:25:24 -0700

    Add comment to test CODEOWNERS.

--- lib/SIL/SILOwnershipVerifier.cpp
+++ lib/SIL/SILOwnershipVerifier.cpp
@@ -11,7 +11,7 @@
 //===----------------------------------------------------------------------===//
 
 #define DEBUG_TYPE "sil-ownership-verifier"
-
+//test CODEOWNERS
 #include "UseOwnershipRequirement.h"
 #include "swift/AST/ASTContext.h"
 #include "swift/AST/AnyFunctionRef.h"
