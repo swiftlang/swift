64e0e1859ed20df96a131d0ecd5cfcc0ab7b03cf
Michael Ilseman <milseman@apple.com>
2017-08-10 15:27:05 -0700

    [stdlib] Deprecate popFirst in Swift 3.2
    Add a deprecation warning for use of popFirst even with -swift-version
    3, as it's prone to leaks under the old model. Test cases added.

--- test/stdlib/StringCompatibilityDiagnostics.swift
+++ test/stdlib/StringCompatibilityDiagnostics.swift
@@ -3,7 +3,7 @@
 func testPopFirst() {
   var str = "abc"
   _ = str.popFirst() // expected-error{{'popFirst()' is unavailable: Please use 'first', 'dropFirst()', or 'Substring.popFirst()'}}
-  _ = str.characters.popFirst() // TODO: deprecate the view, and update the error here
+  _ = str.characters.popFirst() // FIXME: deprecate CharacterView. This call currently gets paired with default popFirst from Collection :-(
   _ = str.unicodeScalars.popFirst() // expected-error{{'popFirst()' is unavailable: Please use 'first', 'dropFirst()', or 'Substring.UnicodeScalarView.popFirst()'}}
 
   var substr = str[...]
