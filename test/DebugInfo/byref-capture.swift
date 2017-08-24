dc24c29bc82994cd0b6e616fd6efeb29b2fba176
Adrian Prantl <aprantl@apple.com>
2017-08-24 08:53:22 -0700

    Adapt to upstream LLVM changes in pretty-printing DIExpression.

--- test/DebugInfo/byref-capture.swift
+++ test/DebugInfo/byref-capture.swift
@@ -7,12 +7,11 @@ func makeIncrementor(_ inc : Int64) -> () -> Int64
   func inner() -> Int64 {
     // CHECK: call void @llvm.dbg.declare(metadata %Ts5Int64V**
     // CHECK-SAME:                        metadata ![[SUM_CAPTURE:[0-9]+]],
-    // CHECK-SAME:                        metadata ![[EMPTY:.*]])
-    // CHECK: ![[EMPTY]] = !DIExpression()
+    // CHECK-SAME:                        metadata !DIExpression())
     // CHECK: ![[INOUTTY:[0-9]+]] = !DICompositeType({{.*}}identifier: "_T0s5Int64VzD"
     //                                                              ^ inout type.
     // CHECK: ![[SUM_CAPTURE]] = !DILocalVariable(name: "sum", arg: 1,
-    // CHECK-SAME:     line: [[@LINE-10]], type: ![[INOUTTY]]
+    // CHECK-SAME:     line: [[@LINE-9]], type: ![[INOUTTY]]
     sum += inc
     return sum
   }
