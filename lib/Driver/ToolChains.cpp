bfa7b9f130f4b3a4f922ed2af0b7358e5aafecf7
John Holdsworth <github@johnholdsworth.com>
2017-08-26 02:57:37 +0100

    Allows shared libraries on Linux

--- lib/Driver/ToolChains.cpp
+++ lib/Driver/ToolChains.cpp
@@ -1597,7 +1597,7 @@ toolchains::GenericUnix::constructInvocation(const LinkJobAction &job,
     Arguments.push_back(context.Args.MakeArgString(A->getValue()));
   }
 
-  if (getTriple().getOS() == llvm::Triple::Linux && !getTriple().isAndroid()) {
+  if (getTriple().getOS() == llvm::Triple::Linux && job.getKind() != LinkKind::DynamicLibrary) {
     Arguments.push_back("-pie");
   }
 
