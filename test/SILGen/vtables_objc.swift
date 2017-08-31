d0862d9fb53c06bc8fa8c53e913ee7dc4c3617cd
Slava Pestov <spestov@apple.com>
2017-08-31 00:39:20 -0700

    Sema: Fix assertion failure when overriding a dynamic method with a final method
    Fixes <https://bugs.swift.org/browse/SR-5703>, <rdar://problem/33944463>.

--- test/SILGen/vtables_objc.swift
+++ test/SILGen/vtables_objc.swift
@@ -9,11 +9,48 @@ import gizmo
 class Hoozit : Gizmo {
   // Overrides Gizmo.frob
   override func frob() {}
+
   // Overrides Gizmo.funge
   override func funge() {}
 
+  // Overrides Gizmo.foo
+  final override func foo() {}
+
   func anse() {}
   func incorrige() {}
+}
+
+// CHECK-LABEL: sil hidden @_T012vtables_objc10callHoozityAA0D0CF : $@convention(thin) (@owned Hoozit) -> ()
+func callHoozit(_ h: Hoozit) {
+  // CHECK: class_method [volatile] {{.*}} : $Hoozit, #Hoozit.frob!1.foreign
+  h.frob()
+  // CHECK: function_ref @_T012vtables_objc6HoozitC3fooyyF
+  h.foo()
+  // CHECK: class_method {{.*}} : $Hoozit, #Hoozit.anse!1
+  h.anse()
+  // CHECK: return
+}
+
+class Wotsit : Hoozit {
+  // Overrides Gizmo.funge
+  override func funge() {}
+
+  // Overrides Hoozit.incorrige
+  override func incorrige() {}
+
+  // Overrides Gizmo.frob
+  final override func frob() {}
+}
+
+// CHECK-LABEL: sil hidden @_T012vtables_objc10callWotsityAA0D0CF : $@convention(thin) (@owned Wotsit) -> ()
+func callWotsit(_ w: Wotsit) {
+  // CHECK: class_method [volatile] {{.*}} : $Wotsit, #Wotsit.funge!1.foreign
+  w.funge()
+  // CHECK: class_method {{.*}} : $Wotsit, #Wotsit.incorrige!1
+  w.incorrige()
+  // CHECK: function_ref @_T012vtables_objc6WotsitC4frobyyF
+  w.frob()
+  // CHECK: return
 }
 
 // Entries only exist for native Swift methods
@@ -25,11 +62,6 @@ class Hoozit : Gizmo {
 // CHECK-NEXT:   #Hoozit.init!initializer.1: (Hoozit.Type) -> (Int) -> Hoozit! : _T012vtables_objc6HoozitCSQyACGSi7bellsOn_tcfc
 // CHECK-NEXT:   #Hoozit.deinit!deallocator: _T012vtables_objc6HoozitCfD
 // CHECK-NEXT: }
-
-class Wotsit : Hoozit {
-  override func funge() {}
-  override func incorrige() {}
-}
 
 // CHECK: sil_vtable Wotsit {
 // CHECK-NEXT:   #Hoozit.anse!1: {{.*}} : _T012vtables_objc6HoozitC4anse{{[_0-9a-zA-Z]*}}F
