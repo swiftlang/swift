swiftlang/swift

commit 5afa52684d073f1835a06c4375bae88b13c7df50

[Constraint system] Dump potential type variable bindings in CS dump.
Debugging aid. NFC.

lib/Sema/ConstraintSystem.h
@@ -2603,47 +2603,57 @@ private:
       }
     }
 
+    void dump(llvm::raw_ostream &out, unsigned indent =0) const {
+      out.indent(indent);
+      if (FullyBound)
+        out << "fully_bound ";
+      if (SubtypeOfExistentialType)
+        out << "subtype_of_existential ";
+      if (LiteralBinding != LiteralBindingKind::None)
+        out << "literal=" << static_cast<int>(LiteralBinding) << " ";
+      if (InvolvesTypeVariables)
+        out << "involves_type_vars ";
+      if (NumDefaultableBindings > 0)
+        out << "#defaultable_bindings=" << NumDefaultableBindings << " ";
+
+      out << "bindings=";
+      if (!Bindings.empty()) {
+        interleave(Bindings,
+                   [&](const PotentialBinding &binding) {
+                     auto type = binding.BindingType;
+                     auto &ctx = type->getASTContext();
+                     llvm::SaveAndRestore<bool> debugConstraints(
+                         ctx.LangOpts.DebugConstraintSolver, true);
+                     switch (binding.Kind) {
+                     case AllowedBindingKind::Exact:
+                       break;
+
+                     case AllowedBindingKind::Subtypes:
+                       out << "(subtypes of) ";
+                       break;
+
+                     case AllowedBindingKind::Supertypes:
+                       out << "(supertypes of) ";
+                       break;
+                     }
+                     if (binding.DefaultedProtocol)
+                       out << "(default from "
+                           << (*binding.DefaultedProtocol)->getName() << ") ";
+                     out << type.getString();
+                   },
+                   [&]() { out << " "; });
+      } else {
+        out << "{}";
+      }
+    }
+
     void dump(TypeVariableType *typeVar, llvm::raw_ostream &out,
-              unsigned indent) const {
+              unsigned indent =0) const {
       out.indent(indent);
       out << "(";
       if (typeVar)
         out << "$T" << typeVar->getImpl().getID();
-      if (FullyBound)
-        out << " fully_bound";
-      if (SubtypeOfExistentialType)
-        out << " subtype_of_existential";
-      if (LiteralBinding != LiteralBindingKind::None)
-        out << " literal=" << static_cast<int>(LiteralBinding);
-      if (InvolvesTypeVariables)
-        out << " involves_type_vars";
-      if (NumDefaultableBindings > 0)
-        out << " defaultable_bindings=" << NumDefaultableBindings;
-      out << " bindings=";
-      interleave(Bindings,
-                 [&](const PotentialBinding &binding) {
-                   auto type = binding.BindingType;
-                   auto &ctx = type->getASTContext();
-                   llvm::SaveAndRestore<bool> debugConstraints(
-                       ctx.LangOpts.DebugConstraintSolver, true);
-                   switch (binding.Kind) {
-                   case AllowedBindingKind::Exact:
-                     break;
-
-                   case AllowedBindingKind::Subtypes:
-                     out << "(subtypes of) ";
-                     break;
-
-                   case AllowedBindingKind::Supertypes:
-                     out << "(supertypes of) ";
-                     break;
-                   }
-                   if (binding.DefaultedProtocol)
-                     out << "(default from "
-                         << (*binding.DefaultedProtocol)->getName() << ") ";
-                   out << type.getString();
-                 },
-                 [&]() { out << " "; });
+      dump(out);
       out << ")\n";
     }
   };

lib/Sema/TypeCheckConstraints.cpp
@@ -3111,6 +3111,8 @@ void ConstraintSystem::print(raw_ostream &out) {
       if (auto fixed = getFixedType(tv)) {
         out << " as ";
         fixed->print(out);
+      } else {
+        getPotentialBindings(tv).dump(out, 1);
       }
     } else {
       out << " equivalent to ";

commit f9b9f4f68f44d127bc0cb1e16abd974615c09cd6

[Constraint system] Fix debug output which is now missing a space.
This was a dumper bug introduced in 5afa52684d073f1835a06c4375bae88b13c7df50.

lib/Sema/ConstraintSystem.h
@@ -2665,7 +2665,7 @@ private:
       out << "(";
       if (typeVar)
         out << "$T" << typeVar->getImpl().getID();
-      dump(out);
+      dump(out, 1);
       out << ")\n";
     }
   };