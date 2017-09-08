0f2ec380c9e82acba44ecd06fc9814a4d22e275b
Mark Lacey <mark.lacey@apple.com>
2017-09-07 23:10:17 -0700

    Add LLVM_ATTRIBUTE_USED to some dumpers.

--- lib/Sema/ConstraintSystem.h
+++ lib/Sema/ConstraintSystem.h
@@ -2611,7 +2611,8 @@ private:
       }
     }
 
-    void dump(llvm::raw_ostream &out, unsigned indent =0) const {
+    void dump(llvm::raw_ostream &out,
+              unsigned indent = 0) const LLVM_ATTRIBUTE_USED {
       out.indent(indent);
       if (FullyBound)
         out << "fully_bound ";
@@ -2655,12 +2656,13 @@ private:
       }
     }
 
-    void dump(ConstraintSystem *cs, unsigned indent =0) const {
+    void dump(ConstraintSystem *cs,
+              unsigned indent = 0) const LLVM_ATTRIBUTE_USED {
       dump(cs->getASTContext().TypeCheckerDebug->getStream());
     }
 
     void dump(TypeVariableType *typeVar, llvm::raw_ostream &out,
-              unsigned indent =0) const {
+              unsigned indent = 0) const LLVM_ATTRIBUTE_USED {
       out.indent(indent);
       out << "(";
       if (typeVar)
