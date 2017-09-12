6b12d7d6cffff0304c0d0d41d31df5b499d54f3f
Michael Gottesman <mgottesman@apple.com>
2017-09-12 14:49:21 -0700

    [mandatory-inline] Move fixupReferenceCounts to /before/ inlining.
    Once this lands, I can land my commit that turns on ownership for mandatory
    inlining.
    
    rdar://31521023

--- test/IRGen/big_types_corner_cases.swift
+++ test/IRGen/big_types_corner_cases.swift
@@ -152,7 +152,7 @@ public func enumCallee(_ x: LargeEnum) {
 // CHECK-64: [[ALLOC2:%.*]] = alloca %T22big_types_corner_cases9BigStructV
 // CHECK-64: [[ALLOC3:%.*]] = alloca %T22big_types_corner_cases9BigStructVSg
 // CHECK-64: [[ALLOC4:%.*]] = alloca %T22big_types_corner_cases9BigStructVSg
-// CHECK-64: call swiftcc void @_T022big_types_corner_cases8SuperSubC1fyyFAA9BigStructVycfU_AFyKXKfu_TA(%T22big_types_corner_cases9BigStructV* noalias nocapture sret [[ALLOC1]], %swift.refcounted* swiftself {{.*}}, %swift.error** nocapture swifterror %swifterror)
+// CHECK-64: call swiftcc void @_T022big_types_corner_cases9SuperBaseC4boomAA9BigStructVyF(%T22big_types_corner_cases9BigStructV* noalias nocapture sret [[ALLOC1]], %T22big_types_corner_cases9SuperBaseC* swiftself {{.*}})
 // CHECK: ret void
 class SuperBase {
   func boom() -> BigStruct {
