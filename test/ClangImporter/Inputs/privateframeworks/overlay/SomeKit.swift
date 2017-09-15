e4c653640f03453bd0c8cbc44ed3c31f7640a96f
Doug Gregor <dgregor@apple.com>
2017-09-15 13:46:35 -0700

    Improve testing for re-exported modules.
    Test more kinds of entities, including through serialized SIL, and
    verify that other combinations work (e.g., building the overlay
    against the public module but using it with the private version).

--- test/ClangImporter/Inputs/privateframeworks/overlay/SomeKit.swift
+++ test/ClangImporter/Inputs/privateframeworks/overlay/SomeKit.swift
@@ -1,4 +1,5 @@
 @_exported import SomeKit
+import Foundation
 
 extension SKWidget {
   public func extensionMethod() -> ExtensionType { return ExtensionType() }
@@ -6,4 +7,43 @@ extension SKWidget {
   public struct ExtensionType { }
 }
 
+extension SKWidget.ExtensionType {
+  public func foo() { }
+}
 
+extension NSObject {
+  public func doSomethingElse(_: SKWidget) { }
+}
+
+extension SKWidgetError {
+  public func getCode(from widget: SKWidget) -> SKWidgetError.Code {
+    return widget.getCurrentError()
+  }
+}
+
+extension SKWidgetError.Code {
+  public var isBoom: Bool {
+    return self == .boom
+  }
+}
+
+public protocol HasAnObject {
+  var anObject: NSObject { get set }
+}
+
+extension SKWidget : HasAnObject { }
+
+@inline(__always)
+public func inlineWidgetOperations(_ widget: SKWidget) {
+  widget.extensionMethod().foo()
+  widget.someObjCMethod()
+  widget.someObjCExtensionMethod()
+  widget.doSomething(with: widget)
+  widget.doSomethingElse(widget)
+  let obj = widget.anObject
+  widget.anObject = obj
+  _ = SKWidgetError(.boom).getCode(from: widget).isBoom
+  var hao: HasAnObject = widget
+  someKitGlobalFunc()
+  hao.anObject = widget
+}
