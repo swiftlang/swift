0aa935c77de6aa7f9f130dafe37a94e55079665f
Manav Rathi <mnvrth@gmail.com>
2017-09-23 16:50:22 +0530

    Improve error messages in utils/python_lint.py

--- utils/python_lint.py
+++ utils/python_lint.py
@@ -23,8 +23,16 @@ def lint(arguments, verbose=True):
     )
     if flake8_result != 0:
         if verbose:
-            print("Missing modules flake8 or flake8-import-order. Please be"
-                  " sure to install these python packages before linting.")
+            print("""
+The flake8 and flake8-import-order Python packages are required for linting,
+but these were not found on your system.
+
+You can install these using:
+
+    python -m pip install flake8
+    python -m pip install flake8-import-order
+
+For more help, see http://flake8.pycqa.org.""")
         return 0
 
     utils_directory = os.path.dirname(os.path.abspath(__file__))
