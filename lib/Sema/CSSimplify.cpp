80e4a2226b4c11cffeef49b9ab58dc3336644544
Pavel Yaskevich <pyaskevich@apple.com>
2017-09-22 17:23:08 -0700

    [ConstraintGraph] Don't try to contract edge of parameter bindings with `inout` attribute
    Currently edge related to the parameter bindings is contracted
    without properly checking if newly created equivalence class has
    the same inout & l-value requirements. This patch improves the
    situation by disallowing contraction of the edges related to parameter
    binding constraint where left-hand side has `inout` attribute set.
    
    Such guarantees that parameter can get `inout` type assigned when
    argument gets `l-value` type.
    
    Resolves: rdar://problem/33429010

--- lib/Sema/CSSimplify.cpp
+++ lib/Sema/CSSimplify.cpp
@@ -1676,11 +1676,27 @@ ConstraintSystem::matchTypes(Type type1, Type type2, ConstraintKind kind,
         if (!isBindable(typeVar1, type2))
           return formUnsolvedResult();
 
+        // If the right-hand side of the BindParam constraint
+        // is `lvalue` type, we'll have to make sure that
+        // left-hand side is bound to type variable which
+        // is wrapped in `inout` type to preserve inout/lvalue pairing.
         if (auto *lvt = type2->getAs<LValueType>()) {
-          assignFixedType(typeVar1, InOutType::get(lvt->getObjectType()));
-        } else {
-          assignFixedType(typeVar1, type2);
+          auto *tv = createTypeVariable(typeVar1->getImpl().getLocator(),
+                                        /*options=*/0);
+          assignFixedType(typeVar1, InOutType::get(tv));
+
+          typeVar1 = tv;
+          type2 = lvt->getObjectType();
         }
+
+        // If we have a binding for the right-hand side
+        // (argument type) don't try to bind it to the left-hand
+        // side (parameter type) directly, because their
+        // relationship is contravariant and the actual
+        // binding can only come from the left-hand side.
+        addUnsolvedConstraint(
+            Constraint::create(*this, ConstraintKind::ArgumentConversion, type2,
+                               typeVar1, getConstraintLocator(locator)));
         return SolutionKind::Solved;
       }
 
