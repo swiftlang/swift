93c517bf7605f50d11212113d5f6558406a59c4e
Jordan Rose <jordan_rose@apple.com>
2017-09-28 16:39:29 -0700

    [ClangImporter] Prefer later enum_extensibility over earlier ones
    Since NS_ENUM has an enum_extensibility(open) in it already, we want
    to allow people to undo that by sticking enum_extensibility(closed) on
    the end of their enum. Furthermore, we actually want to ignore the
    enum_extensibility(open) that's in there now, so as not to introduce
    new warnings in Swift 4.1.

--- test/ClangImporter/enum-inferred-exhaustivity.swift
+++ test/ClangImporter/enum-inferred-exhaustivity.swift
@@ -1,4 +1,4 @@
-// RUN: %target-swift-frontend -typecheck %s -import-objc-header %S/Inputs/enum-inferred-exhaustivity.h -swift-version 4
+// RUN: %target-swift-frontend -typecheck %s -import-objc-header %S/Inputs/enum-inferred-exhaustivity.h -swift-version 4 -warnings-as-errors
 // RUN: %target-swift-frontend -typecheck %s -import-objc-header %S/Inputs/enum-inferred-exhaustivity.h -swift-version 5 -verify
 
 // This is testing what happens with a CF_ENUM definition that doesn't include
@@ -9,6 +9,13 @@ func test(_ value: EnumWithDefaultExhaustivity) {
   // In Swift 4 this should be accepted as is, without even getting warnings,
   // until we actually ship Swift 5. Then it should be a warning in Swift 4
   // mode and an error in Swift 5.
+  switch value { // expected-error {{switch must be exhaustive}} expected-note {{do you want to add a default clause?}}
+  case .loneCase: break
+  }
+}
+
+func test(_ value: EnumWithSpecialAttributes) {
+  // Same, but with the attributes macro shipped in the Xcode 9 SDKs.
   switch value { // expected-error {{switch must be exhaustive}} expected-note {{do you want to add a default clause?}}
   case .loneCase: break
   }
