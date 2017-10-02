ad509d7c9141aa2e2beff40efc482744cb8ec63b
Rintaro Ishizaki <fs.output@gmail.com>
2017-10-02 21:56:49 +0900

    [IDE/SourceKit] Enable refactoring tests in Linux

--- test/SourceKit/Refactoring/semantic-refactoring/extract-func-default.swift
+++ test/SourceKit/Refactoring/semantic-refactoring/extract-func-default.swift
@@ -7,6 +7,3 @@ func foo() -> Int {
 // RUN: rm -rf %t.result && mkdir -p %t.result
 // RUN: %sourcekitd-test -req=extract-func -pos=2:1 -end-pos 4:11 %s -- %s > %t.result/extract-func-default.swift.expected
 // RUN: diff -u %S/extract-func-default.swift.expected %t.result/extract-func-default.swift.expected
-
-// FIXME: Fails on linux with assertion: "!GlibcModuleMapPath.empty()"" failed
-// REQUIRES-ANY: OS=macosx
