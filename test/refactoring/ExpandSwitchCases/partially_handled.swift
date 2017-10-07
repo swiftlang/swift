42d38b260e4280c96245f6228e525ea7789ea64d
Kacper Harasim <kacper.harasim@gmail.com>
2017-10-07 12:44:43 +0200

    Added test case for situation where enum is partially handled by the user at the moment  refactoring is invoked

--- test/refactoring/ExpandSwitchCases/partially_handled.swift
+++ test/refactoring/ExpandSwitchCases/partially_handled.swift
@@ -6,8 +6,10 @@ enum E {
 }
 
 func foo(e: E) -> Int {
-  switch e { }
+  switch e {
+    case .e1: return 5
+   }
 }
 // RUN: rm -rf %t.result && mkdir -p %t.result
-// RUN: %refactor -expand-switch-cases -source-filename %s -pos=9:8 >> %t.result/L10.swift
-// RUN: diff -u %S/Outputs/partially_handled/L10.swift.expected %t.result/L10.swift
+// RUN: %refactor -expand-switch-cases -source-filename %s -pos=9:8 >> %t.result/L11.swift
+// RUN: diff -u %S/Outputs/partially_handled/L11.swift.expected %t.result/L11.swift
