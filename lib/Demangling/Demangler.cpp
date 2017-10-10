1b2c2269122776ccf633e2a8adbca31a1647e050
Andrew Trick <atrick@apple.com>
2017-10-10 05:35:25 -0700

    Add mangling for @noescape/@escaping.

--- lib/Demangling/Demangler.cpp
+++ lib/Demangling/Demangler.cpp
@@ -1012,6 +1012,9 @@ NodePointer Demangler::demangleImplFunctionType() {
   if (GenSig && nextIf('P'))
     GenSig = changeKind(GenSig, Node::Kind::DependentPseudogenericSignature);
 
+  if (nextIf('e'))
+    type->addChild(createNode(Node::Kind::ImplEscaping), *this);
+
   const char *CAttr = nullptr;
   switch (nextChar()) {
     case 'y': CAttr = "@callee_unowned"; break;
