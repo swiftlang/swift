a70e857d594405f2d6a41c1d42b6e31bd36da232
Ben Cohen <airspeedswift@users.noreply.github.com>
2017-10-16 13:44:51 -0700

    [stdlib] Fix issue with UTF16 index(_:offsetBy:limitedBy) (#12378)
    * Fix issue with empty string ranges
    
    * Add tests for basic offsetBy operation on UTF16View.Index

--- test/Interpreter/SDK/Foundation_test.swift
+++ test/Interpreter/SDK/Foundation_test.swift
@@ -177,6 +177,14 @@ FoundationTestSuite.test("RangeConversion") {
   let nsrFrom = NSRange(b..., in: s)
   expectEqual(nsrFrom.location,5)
   expectEqual(nsrFrom.length, 5)
+  
+  expectNil(Range(NSRange(location: 100, length: 0), in: s))
+  expectNil(Range(NSRange(location: 0, length: 100), in: s))
+  
+  let empty = ""
+  expectNil(Range(NSRange(location: 1, length: 0), in: empty))  
+  expectNil(Range(NSRange(location: 0, length: 1), in: empty))
+  expectNotNil(Range(NSRange(location: 0, length: 0), in: empty))
 
   // FIXME: enable once indices conform to RangeExpression
   // let nsrFull = NSRange(s.indices, in: s)
