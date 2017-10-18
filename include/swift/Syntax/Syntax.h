5ece69154948d3d49728cb2b77fb2a8051b27a27
Xi Ge <xi_ge@apple.com>
2017-10-18 11:55:18 -0700

    remove merging relics.

--- include/swift/Syntax/Syntax.h
+++ include/swift/Syntax/Syntax.h
@@ -127,6 +127,7 @@ public:
   /// if it has one, otherwise 0.
   CursorIndex getIndexInParent() const;
 
+  /// Returns true if this syntax node represents a token.
   bool isToken() const;
 
   /// Returns true if this syntax node represents a statement.
@@ -153,9 +154,6 @@ public:
 
   /// Returns true if the node is "present" in the source.
   bool isPresent() const;
-
-
-  bool isToken() const;
 
   /// Print the syntax node with full fidelity to the given output stream.
   void print(llvm::raw_ostream &OS) const;
