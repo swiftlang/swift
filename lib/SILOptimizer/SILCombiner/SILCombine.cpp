98f137ed3ee5fbf891342b547e199c3c4107b98c
Chris Lattner <clattner@nondot.org>
2017-10-22 16:32:18 -0700

    Fix a comment header, trim #includes.  NFC.

--- lib/SILOptimizer/SILCombiner/SILCombine.cpp
+++ lib/SILOptimizer/SILCombiner/SILCombine.cpp
@@ -1,4 +1,4 @@
-//===--- SILCombine -------------------------------------------------------===//
+//===--- SILCombine.cpp ---------------------------------------------------===//
 //
 // This source file is part of the Swift.org open source project
 //
