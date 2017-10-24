0c66643576b2b916fb65ee31f758ddd87458434e
Slava Pestov <spestov@apple.com>
2017-10-23 22:11:40 -0700

    Serialization: Serialize FuncDecl::hasForcedStaticDispatch()
    Otherwise, a protocol conformance where the witness was a dynamic
    property in another module would trigger an assertion while building
    the materializeForSet witness, or miscompile and fail at runtime
    if asserts are off.

--- lib/Serialization/Deserialization.cpp
+++ lib/Serialization/Deserialization.cpp
@@ -2872,7 +2872,7 @@ ModuleFile::getDeclChecked(DeclID DID, Optional<DeclContext *> ForcedContext) {
     bool isImplicit;
     bool isStatic;
     uint8_t rawStaticSpelling, rawAccessLevel, rawAddressorKind, rawMutModifier;
-    bool isObjC, hasDynamicSelf, throws;
+    bool isObjC, hasDynamicSelf, hasForcedStaticDispatch, throws;
     unsigned numParamPatterns, numNameComponentsBiased;
     GenericEnvironmentID genericEnvID;
     TypeID interfaceTypeID;
@@ -2885,7 +2885,8 @@ ModuleFile::getDeclChecked(DeclID DID, Optional<DeclContext *> ForcedContext) {
 
     decls_block::FuncLayout::readRecord(scratch, contextID, isImplicit,
                                         isStatic, rawStaticSpelling, isObjC,
-                                        rawMutModifier, hasDynamicSelf, throws,
+                                        rawMutModifier, hasDynamicSelf,
+                                        hasForcedStaticDispatch, throws,
                                         numParamPatterns, genericEnvID,
                                         interfaceTypeID,
                                         associatedDeclID, overriddenID,
@@ -3030,6 +3031,7 @@ ModuleFile::getDeclChecked(DeclID DID, Optional<DeclContext *> ForcedContext) {
     if (isImplicit)
       fn->setImplicit();
     fn->setDynamicSelf(hasDynamicSelf);
+    fn->setForcedStaticDispatch(hasForcedStaticDispatch);
     fn->setNeedsNewVTableEntry(needsNewVTableEntry);
 
     if (auto defaultArgumentResilienceExpansion = getActualResilienceExpansion(
