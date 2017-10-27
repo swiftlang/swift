65fc0f492ae870bbfcbd064516dbf13f1550052b
Graydon Hoare <ghoare@apple.com>
2017-10-26 18:15:25 -0700

    [NamedLazyMemberLoading] Add @interface test.

--- test/NameBinding/named_lazy_member_loading_objc_interface.swift
+++ test/NameBinding/named_lazy_member_loading_objc_interface.swift
@@ -6,21 +6,18 @@
 // RUN: %target-swift-frontend -typecheck -I %S/Inputs/NamedLazyMembers -typecheck %s
 //
 // Check that without lazy named member loading, we're importing too much.
-// RUN: %target-swift-frontend -typecheck -I %S/Inputs/NamedLazyMembers -typecheck -stats-output-dir %t/stats-pre %s
-// RUN: %utils/process-stats-dir.py --evaluate 'NumTotalClangImportedEntities > 35' %t/stats-pre
+// RUN: %target-swift-frontend -typecheck -I %S/Inputs/NamedLazyMembers -stats-output-dir %t/stats-pre %s
+// RUN: %utils/process-stats-dir.py --evaluate 'NumTotalClangImportedEntities > 20' %t/stats-pre
 //
 // Check that with lazy named member loading, we're importing less.
 // RUN: %target-swift-frontend -typecheck -I %S/Inputs/NamedLazyMembers -enable-named-lazy-member-loading -stats-output-dir %t/stats-post %s
-// RUN: %utils/process-stats-dir.py --evaluate 'NumTotalClangImportedEntities < 20' %t/stats-post
+// RUN: %utils/process-stats-dir.py --evaluate 'NumTotalClangImportedEntities < 10' %t/stats-post
 
 import NamedLazyMembers
 
-public func foo(d: Doer) {
-  d.doSomeWork()
-  d.doSomeWork(withSpeed:10)
-  d.doVeryImportantWork(speed:10, thoroughness:12)
-  d.doSomeWorkWithSpeed(speed:10, levelOfAlacrity:12)
-
-  let _ = SimpleDoer()
-  let _ = SimpleDoer.ofNoWork()
+public func foo(d: SimpleDoer) {
+  let _ = d.simplyDoSomeWork()
+  let _ = d.simplyDoSomeWork(withSpeed:10)
+  let _ = d.simplyDoVeryImportantWork(speed:10, thoroughness:12)
+  let _ = d.simplyDoSomeWorkWithSpeed(speed:10, levelOfAlacrity:12)
 }
