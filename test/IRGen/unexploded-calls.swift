95d41168b3edec39f70046a0cc765abf884ff825
Saleem Abdulrasool <compnerd@compnerd.org>
2017-10-27 22:57:25 -0700

    test: ensure that the ARM codegenerator is present
    These tests require the ARM codegenerator as they exercise the ARM
    triples.  This should fix the buildbot testing.

--- test/IRGen/unexploded-calls.swift
+++ test/IRGen/unexploded-calls.swift
@@ -2,6 +2,8 @@
 // RUN: %swift -target thumbv7-unknown-linux-gnueabihf -parse-stdlib -parse-as-library -I %S/Inputs/usr/include -module-name Swift -S -emit-ir -o - %s | %FileCheck %s
 // RUN: %swift -target thumbv7-unknown-linux-gnueabi -Xcc -mfloat-abi=hard -parse-stdlib -parse-as-library -I %S/Inputs/usr/include -module-name Swift -S -emit-ir -o - %s | %FileCheck %s
 
+// REQUIRES: CODEGENERATOR=ARM
+
 struct Float {
   let _value: Builtin.FPIEEE32
 }
