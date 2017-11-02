ae03d05b5c6ccd72b7bbfaf43f626d5459bc8ea5
Karoy Lorentey <klorentey@apple.com>
2017-11-02 17:24:03 +0000

    [DO NOT MERGE]: String._dump() and StringGuts.checkInvariants()

--- stdlib/public/stubs/LibcShims.cpp
+++ stdlib/public/stubs/LibcShims.cpp
@@ -329,3 +329,15 @@ swift::_stdlib_cxx11_mt19937_uniform(__swift_uint32_t upper_bound) {
   std::uniform_int_distribution<__swift_uint32_t> RandomUniform(0, upper_bound);
   return RandomUniform(getGlobalMT19937());
 }
+
+// FIXME remove
+SWIFT_RUNTIME_STDLIB_INTERFACE
+void swift::_swift_stdlib_print_hex(__swift_uintptr_t value, int newline) {
+  if (newline != 0) {
+    printf("%016tX\n", value);
+  }
+  else {
+    printf("%016tX", value);
+  }
+}
+
