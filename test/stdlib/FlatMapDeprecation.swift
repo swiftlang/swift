d19f9fa1d29da732e095a15686a200d96bc3cbc4
Max Moiseev <moiseev@apple.com>
2017-11-08 11:06:09 -0800

    [stdlib] Introduce compactMap and deprecate one flatMap variant
    This is the implementation of
    [SE-0187](https://github.com/apple/swift-evolution/blob/master/proposals/0187-introduce-filtermap.md)
    
    <rdar://problem/34918180>
    
    Cherrypicked and squashed from the following commits:
    6f7aecd fb49b3e 8ec6c45 96d3439 128092a

---
+++ test/stdlib/FlatMapDeprecation.swift
@@ -0,0 +1,32 @@
+// RUN: %target-typecheck-verify-swift %s
+
+func flatMapOnSequence<
+  S : Sequence
+>(xs: S, f: (S.Element) -> S.Element?) {
+  _ = xs.flatMap(f) // expected-warning {{deprecated}} expected-note {{compactMap}}
+}
+
+func flatMapOnLazySequence<
+  S : LazySequenceProtocol
+>(xs: S, f: (S.Element) -> S.Element?) {
+  _ = xs.flatMap(f) // expected-warning {{deprecated}} expected-note {{compactMap}}
+}
+
+func flatMapOnLazyCollection<
+  C : LazyCollectionProtocol
+>(xs: C, f: (C.Element) -> C.Element?) {
+  _ = xs.flatMap(f) // expected-warning {{deprecated}} expected-note {{compactMap}}
+}
+
+func flatMapOnLazyBidirectionalCollection<
+  C : LazyCollectionProtocol & BidirectionalCollection
+>(xs: C, f: (C.Element) -> C.Element?)
+where C.Elements : BidirectionalCollection {
+  _ = xs.flatMap(f) // expected-warning {{deprecated}} expected-note {{compactMap}}
+}
+
+func flatMapOnCollectinoOfStrings<
+  C : Collection
+>(xs: C, f: (C.Element) -> String?) {
+  _ = xs.flatMap(f) // expected-warning {{deprecated}} expected-note {{compactMap}}
+}
