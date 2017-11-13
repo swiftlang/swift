37c986361ca525c8b25a8f137f079adda9be460a
David Ungar <ungar@mac.com>
2017-11-13 09:33:51 -0800

    Undo adding three lines that doesn’t work

--- lib/FrontendTool/FrontendTool.cpp
+++ lib/FrontendTool/FrontendTool.cpp
@@ -512,9 +512,6 @@ static bool performCompile(CompilerInstance &Instance,
   FrontendOptions opts = Invocation.getFrontendOptions();
   FrontendOptions::ActionType Action = opts.RequestedAction;
   
-  if (Action == FrontendOptions::ActionType::EmitSyntax)
-    Instance.getASTContext().LangOpts.KeepSyntaxInfoInSourceFile = true;
-
   // We've been asked to precompile a bridging header; we want to
   // avoid touching any other inputs and just parse, emit and exit.
   if (Action == FrontendOptions::ActionType::EmitPCH) {
