0e535c24dc1cd5291796ea167eedf92d8602136a
Doug Gregor <dgregor@apple.com>
2017-11-13 15:54:19 -0800

    Fix some tests due to Equatable Optional/Array/Dictionary change.

--- test/Constraints/patterns.swift
+++ test/Constraints/patterns.swift
@@ -163,9 +163,8 @@ case nil?: break // expected-warning {{case is already handled by previous patte
 default: break
 }
 
-// <rdar://problem/21995744> QoI: Binary operator '~=' cannot be applied to operands of type 'String' and 'String?'
 switch ("foo" as String?) {
-case "what": break // expected-error{{expression pattern of type 'String' cannot match values of type 'String?'}} {{12-12=?}}
+case "what": break
 default: break
 }
 
