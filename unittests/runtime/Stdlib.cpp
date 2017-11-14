c759b510d545533423a8c8006bd2309c58e3eb0f
Greg Parker <gparker@apple.com>
2017-11-13 23:27:03 -0800

    [tests] Add more stub functions for tests that link directly to the runtime.

--- unittests/runtime/Stdlib.cpp
+++ unittests/runtime/Stdlib.cpp
@@ -17,6 +17,8 @@ using namespace swift;
 // Placeholders for Swift functions that the C++ runtime references
 // but that the test code does not link to.
 
+// AnyHashable
+
 SWIFT_CC(swift) SWIFT_RUNTIME_STDLIB_INTERNAL
 void _swift_makeAnyHashableUsingDefaultRepresentation(
   const OpaqueValue *value,
@@ -26,6 +28,8 @@ void _swift_makeAnyHashableUsingDefaultRepresentation(
 ) {
   abort();
 }
+
+// SwiftHashableSupport
 
 SWIFT_CC(swift) SWIFT_RUNTIME_STDLIB_INTERNAL
 bool _swift_stdlib_Hashable_isEqual_indirect(
@@ -37,5 +41,81 @@ bool _swift_stdlib_Hashable_isEqual_indirect(
 SWIFT_CC(swift) SWIFT_RUNTIME_STDLIB_INTERNAL
 intptr_t _swift_stdlib_Hashable_hashValue_indirect(
   const void *value, const Metadata *type, const void *wt) {
+  abort();
+}
+
+SWIFT_CC(swift) SWIFT_RUNTIME_STDLIB_INTERNAL
+void _swift_convertToAnyHashableIndirect(
+  OpaqueValue *source, OpaqueValue *destination, const Metadata *sourceType,
+  const void *sourceConformance) {
+  abort();
+}
+
+SWIFT_CC(swift) SWIFT_RUNTIME_STDLIB_INTERNAL
+bool _swift_anyHashableDownCastConditionalIndirect(
+  OpaqueValue *source, OpaqueValue *destination, const Metadata *targetType) {
+  abort();
+}
+
+
+// Casting
+
+SWIFT_CC(swift) SWIFT_RUNTIME_STDLIB_INTERNAL
+void _swift_arrayDownCastIndirect(OpaqueValue *destination,
+                                  OpaqueValue *source,
+                                  const Metadata *sourceValueType,
+                                  const Metadata *targetValueType) {
+  abort();
+}
+
+SWIFT_CC(swift) SWIFT_RUNTIME_STDLIB_INTERNAL
+bool _swift_arrayDownCastConditionalIndirect(OpaqueValue *destination,
+                                             OpaqueValue *source,
+                                             const Metadata *sourceValueType,
+                                             const Metadata *targetValueType) {
+  abort();
+}
+
+SWIFT_CC(swift) SWIFT_RUNTIME_STDLIB_INTERNAL
+void _swift_setDownCastIndirect(OpaqueValue *destination,
+                                OpaqueValue *source,
+                                const Metadata *sourceValueType,
+                                const Metadata *targetValueType,
+                                const void *sourceValueHashable,
+                                const void *targetValueHashable) {
+  abort();
+}
+
+SWIFT_CC(swift) SWIFT_RUNTIME_STDLIB_INTERNAL
+bool _swift_setDownCastConditionalIndirect(OpaqueValue *destination,
+                                           OpaqueValue *source,
+                                           const Metadata *sourceValueType,
+                                           const Metadata *targetValueType,
+                                           const void *sourceValueHashable,
+                                           const void *targetValueHashable) {
+  abort();
+}
+
+SWIFT_CC(swift) SWIFT_RUNTIME_STDLIB_INTERNAL
+void _swift_dictionaryDownCastIndirect(OpaqueValue *destination,
+                                       OpaqueValue *source,
+                                       const Metadata *sourceKeyType,
+                                       const Metadata *sourceValueType,
+                                       const Metadata *targetKeyType,
+                                       const Metadata *targetValueType,
+                                       const void *sourceKeyHashable,
+                                       const void *targetKeyHashable) {
+  abort();
+}
+
+SWIFT_CC(swift) SWIFT_RUNTIME_STDLIB_INTERNAL
+bool _swift_dictionaryDownCastConditionalIndirect(OpaqueValue *destination,
+                                        OpaqueValue *source,
+                                        const Metadata *sourceKeyType,
+                                        const Metadata *sourceValueType,
+                                        const Metadata *targetKeyType,
+                                        const Metadata *targetValueType,
+                                        const void *sourceKeyHashable,
+                                        const void *targetKeyHashable) {
   abort();
 }
