afd9903509928578bac147c5a95fc94b442ae62a
Slava Pestov <spestov@apple.com>
2017-11-14 00:17:41 -0800

    Fixed crashers don't require asserts

--- validation-test/compiler_crashers_fixed/28775-gpdecl-getdepth-generictypeparamdecl-invaliddepth-parameter-hasnt-been-validated.swift
+++ validation-test/compiler_crashers_fixed/28775-gpdecl-getdepth-generictypeparamdecl-invaliddepth-parameter-hasnt-been-validated.swift
@@ -5,6 +5,5 @@
 // See https://swift.org/LICENSE.txt for license information
 // See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
 
-// REQUIRES: asserts
 // RUN: not %target-swift-frontend %s -emit-ir
 protocol P{{}protocol A{typealias e{}}typealias a=A}extension P.a{protocol P
