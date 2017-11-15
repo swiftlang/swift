swiftlang/swift

commit e74367f2b377ed7ef09861c256287639ecc94aba

[pred-memopt] Only promote destroy_addr if we know that otherwise the allocation is dead.
Previously, we just always promoted destroy_addr. With ownership, this does not
work as well since we need to be able to reason about the take operation that we
are performing. In the general case, this would require adding code to
pred-memopts for tracking reads and for compensation store code since we would
need to eliminate the store of the taken value to prevent a double use.

I am going to loop back around later in the year and add back this code once the
time is available. I filed SR-6341. In case any third party contributor is
interested in looking at re-enabling this optimization before I get back to it.

rdar://31521023

lib/SILOptimizer/Mandatory/PredictableMemOpt.cpp
@@ -149,7 +149,11 @@ static unsigned computeSubelement(SILValue Pointer,
 
 namespace {
 
+class AvailableValueAggregator;
+
 struct AvailableValue {
+  friend class AvailableValueAggregator;
+
   /// If this gets too expensive in terms of copying, we can use an arena and a
   /// FrozenPtrSet like we do in ARC.
   using SetVector = llvm::SmallSetVector<SILInstruction *, 1>;
@@ -157,6 +161,9 @@ struct AvailableValue {
   SILValue Value;
   unsigned SubElementNumber;
   SetVector InsertionPoints;
+
+  /// Just for updating.
+  SmallVectorImpl<DIMemoryUse> *Uses;
 
 public:
   AvailableValue() = default;
@@ -236,7 +243,7 @@ public:
   void print(llvm::raw_ostream &os) const;
 
 private:
-  /// Private constructor for use by emitStructExtract and emitTupleExtract.
+  /// Private constructor.
   AvailableValue(SILValue Value, unsigned SubElementNumber,
                  const SetVector &InsertPoints)
       : Value(Value), SubElementNumber(SubElementNumber),
@@ -345,12 +352,14 @@ class AvailableValueAggregator {
   SILBuilderWithScope B;
   SILLocation Loc;
   MutableArrayRef<AvailableValue> AvailableValueList;
+  SmallVectorImpl<DIMemoryUse> &Uses;
 
 public:
   AvailableValueAggregator(SILInstruction *Inst,
-                           MutableArrayRef<AvailableValue> AvailableValueList)
+                           MutableArrayRef<AvailableValue> AvailableValueList,
+                           SmallVectorImpl<DIMemoryUse> &Uses)
       : M(Inst->getModule()), B(Inst), Loc(Inst->getLoc()),
-        AvailableValueList(AvailableValueList) {}
+        AvailableValueList(AvailableValueList), Uses(Uses) {}
 
   // This is intended to be passed by reference only once constructed.
   AvailableValueAggregator(const AvailableValueAggregator &) = delete;
@@ -495,9 +504,13 @@ SILValue AvailableValueAggregator::handlePrimitiveValue(SILType LoadTy,
                                                         unsigned FirstElt) {
   auto &Val = AvailableValueList[FirstElt];
 
-  // If the value is not available, load the value.
+  // If the value is not available, load the value and update our use list.
   if (!Val) {
-    return B.createLoad(Loc, Address, LoadOwnershipQualifier::Unqualified);
+    auto *Load =
+        B.createLoad(Loc, Address, LoadOwnershipQualifier::Unqualified);
+    Uses.push_back(DIMemoryUse(Load, DIUseKind::Load, FirstElt,
+                               getNumSubElements(Load->getType(), M)));
+    return Load;
   }
 
   // If we have 1 insertion point, just extract the value and return.
@@ -570,7 +583,11 @@ public:
 
 private:
   bool promoteLoad(SILInstruction *Inst);
-  bool promoteDestroyAddr(DestroyAddrInst *DAI);
+  void promoteDestroyAddr(DestroyAddrInst *DAI,
+                          MutableArrayRef<AvailableValue> Values);
+  bool
+  canPromoteDestroyAddr(DestroyAddrInst *DAI,
+                        llvm::SmallVectorImpl<AvailableValue> &AvailableValues);
 
   // Load promotion.
   bool hasEscapedAt(SILInstruction *I);
@@ -867,7 +884,6 @@ static SILValue tryFindSrcAddrForLoad(SILInstruction *Inst) {
 /// cross element accesses have been scalarized.
 ///
 /// This returns true if the load has been removed from the program.
-///
 bool AllocOptimize::promoteLoad(SILInstruction *Inst) {
   // Note that we intentionally don't support forwarding of weak pointers,
   // because the underlying value may drop be deallocated at any time.  We would
@@ -940,7 +956,7 @@ bool AllocOptimize::promoteLoad(SILInstruction *Inst) {
   // type as the load did, and emit smaller) loads for any subelements that were
   // not available.
   auto *Load = cast<LoadInst>(Inst);
-  AvailableValueAggregator Agg(Load, AvailableValues);
+  AvailableValueAggregator Agg(Load, AvailableValues, Uses);
   SILValue NewVal = Agg.aggregateValues(LoadTy, Load->getOperand(), FirstElt);
 
   ++NumLoadPromoted;
@@ -957,15 +973,12 @@ bool AllocOptimize::promoteLoad(SILInstruction *Inst) {
   return true;
 }
 
-/// promoteDestroyAddr - DestroyAddr is a composed operation merging
-/// load+strong_release.  If the implicit load's value is available, explode it.
-///
-/// Note that we handle the general case of a destroy_addr of a piece of the
-/// memory object, not just destroy_addrs of the entire thing.
-///
-bool AllocOptimize::promoteDestroyAddr(DestroyAddrInst *DAI) {
+/// Return true if we can promote the given destroy.
+bool AllocOptimize::canPromoteDestroyAddr(
+    DestroyAddrInst *DAI,
+    llvm::SmallVectorImpl<AvailableValue> &AvailableValues) {
   SILValue Address = DAI->getOperand();
-  
+
   // We cannot promote destroys of address-only types, because we can't expose
   // the load.
   SILType LoadTy = Address->getType().getObjectType();
@@ -987,24 +1000,46 @@ bool AllocOptimize::promoteDestroyAddr(DestroyAddrInst *DAI) {
   llvm::SmallBitVector RequiredElts(NumMemorySubElements);
   RequiredElts.set(FirstElt, FirstElt+NumLoadSubElements);
 
-  SmallVector<AvailableValue, 8> AvailableValues;
-  AvailableValues.resize(NumMemorySubElements);
-  
   // Find out if we have any available values.  If no bits are demanded, we
   // trivially succeed. This can happen when there is a load of an empty struct.
-  if (NumLoadSubElements != 0) {
-    computeAvailableValues(DAI, RequiredElts, AvailableValues);
+  if (NumLoadSubElements == 0)
+    return true;
 
-    // If some value is not available at this load point, then we fail.
-    if (llvm::any_of(range(FirstElt, FirstElt + NumLoadSubElements),
-                     [&](unsigned i) -> bool { return !AvailableValues[i]; }))
-      return false;
-  }
-  
+  llvm::SmallVector<AvailableValue, 8> TmpList;
+  TmpList.resize(NumMemorySubElements);
+  computeAvailableValues(DAI, RequiredElts, TmpList);
+
+  // If some value is not available at this load point, then we fail.
+  if (llvm::any_of(range(FirstElt, FirstElt + NumLoadSubElements),
+                   [&](unsigned i) -> bool { return !TmpList[i]; }))
+    return false;
+
+  // Now that we have our final list, move the temporary lists contents into
+  // AvailableValues.
+  std::move(TmpList.begin(), TmpList.end(),
+            std::back_inserter(AvailableValues));
+
+  return true;
+}
+
+/// promoteDestroyAddr - DestroyAddr is a composed operation merging
+/// load+strong_release.  If the implicit load's value is available, explode it.
+///
+/// Note that we handle the general case of a destroy_addr of a piece of the
+/// memory object, not just destroy_addrs of the entire thing.
+void AllocOptimize::promoteDestroyAddr(
+    DestroyAddrInst *DAI, MutableArrayRef<AvailableValue> AvailableValues) {
+  SILValue Address = DAI->getOperand();
+  SILType LoadTy = Address->getType().getObjectType();
+
+  // Compute the access path down to the field so we can determine precise
+  // def/use behavior.
+  unsigned FirstElt = computeSubelement(Address, TheMemory);
+
   // Aggregate together all of the subelements into something that has the same
   // type as the load did, and emit smaller) loads for any subelements that were
   // not available.
-  AvailableValueAggregator Agg(DAI, AvailableValues);
+  AvailableValueAggregator Agg(DAI, AvailableValues, Uses);
   SILValue NewVal = Agg.aggregateValues(LoadTy, Address, FirstElt);
 
   ++NumDestroyAddrPromoted;
@@ -1014,10 +1049,7 @@ bool AllocOptimize::promoteDestroyAddr(DestroyAddrInst *DAI) {
 
   SILBuilderWithScope(DAI).emitDestroyValueOperation(DAI->getLoc(), NewVal);
   DAI->eraseFromParent();
-  return true;
 }
-
-
 
 /// Explode a copy_addr instruction of a loadable type into lower level
 /// operations like loads, stores, retains, releases, retain_value, etc.
@@ -1171,15 +1203,53 @@ bool AllocOptimize::tryToRemoveDeadAllocation() {
 
   // If the memory object has non-trivial type, then removing the deallocation
   // will drop any releases.  Check that there is nothing preventing removal.
+  llvm::SmallVector<unsigned, 8> DestroyAddrIndices;
+  llvm::SmallVector<AvailableValue, 32> AvailableValueList;
+  llvm::SmallVector<unsigned, 8> AvailableValueStartOffsets;
+
   if (!MemoryType.isTrivial(Module)) {
-    for (auto *R : Releases) {
+    for (auto P : llvm::enumerate(Releases)) {
+      auto *R = P.value();
       if (R == nullptr || isa<DeallocStackInst>(R) || isa<DeallocBoxInst>(R))
         continue;
+
+      // We stash all of the destroy_addr that we see.
+      if (auto *DAI = dyn_cast<DestroyAddrInst>(R)) {
+        AvailableValueStartOffsets.push_back(AvailableValueList.size());
+        // Make sure we can actually promote this destroy addr. If we can not,
+        // then we must bail. In order to not gather available values twice, we
+        // gather the available values here that we will use to promote the
+        // values.
+        if (!canPromoteDestroyAddr(DAI, AvailableValueList))
+          return false;
+        DestroyAddrIndices.push_back(P.index());
+        continue;
+      }
 
       DEBUG(llvm::dbgs() << "*** Failed to remove autogenerated alloc: "
             "kept alive by release: " << *R);
       return false;
     }
+  }
+
+  // If we reached this point, we can promote all of our destroy_addr.
+  for (auto P : llvm::enumerate(DestroyAddrIndices)) {
+    unsigned DestroyAddrIndex = P.value();
+    unsigned AvailableValueIndex = P.index();
+    unsigned StartOffset = AvailableValueStartOffsets[AvailableValueIndex];
+    unsigned Count;
+
+    if ((AvailableValueStartOffsets.size() - 1) != AvailableValueIndex) {
+      Count = AvailableValueStartOffsets[AvailableValueIndex + 1] - StartOffset;
+    } else {
+      Count = AvailableValueList.size() - StartOffset;
+    }
+
+    MutableArrayRef<AvailableValue> Values(&AvailableValueList[StartOffset],
+                                           Count);
+    auto *DAI = cast<DestroyAddrInst>(Releases[DestroyAddrIndex]);
+    promoteDestroyAddr(DAI, Values);
+    Releases[DestroyAddrIndex] = nullptr;
   }
 
   DEBUG(llvm::dbgs() << "*** Removing autogenerated alloc_stack: "<<*TheMemory);
@@ -1214,16 +1284,6 @@ bool AllocOptimize::doIt() {
     }
   }
   
-  // destroy_addr(p) is strong_release(load(p)), try to promote it too.
-  for (unsigned i = 0; i != Releases.size(); ++i) {
-    if (auto *DAI = dyn_cast_or_null<DestroyAddrInst>(Releases[i]))
-      if (promoteDestroyAddr(DAI)) {
-        // remove entry if destroy_addr got deleted.
-        Releases[i] = nullptr;
-        Changed = true;
-      }
-  }
-
   // If this is an allocation, try to remove it completely.
   Changed |= tryToRemoveDeadAllocation();
 

test/DebugInfo/linetable-cleanups.swift
@@ -27,7 +27,8 @@ func main() {
 // CHECK:  call void @swift_bridgeObjectRelease({{.*}}) {{#[0-9]+}}, !dbg ![[CLEANUPS:.*]]
 // CHECK-NEXT:  !dbg ![[CLEANUPS]]
 // CHECK-NEXT:  llvm.lifetime.end
-// CHECK-NEXT:  bitcast
+// CHECK-NEXT:  load
+// CHECK-NEXT:  swift_rt_swift_release
 // CHECK-NEXT:  bitcast
 // CHECK-NEXT:  llvm.lifetime.end
 // CHECK-NEXT:  ret void, !dbg ![[CLEANUPS]]

test/DebugInfo/return.swift
@@ -7,16 +7,19 @@ class X {
 
 // CHECK: define {{.*}}ifelseexpr
 public func ifelseexpr() -> Int64 {
-  var x = X(i:0) 
+  var x = X(i:0)
+  // CHECK: [[ALLOCA:%.*]] = alloca %T6return1XC*
   // CHECK: [[META:%.*]] = call %swift.type* @_T06return1XCMa()
   // CHECK: [[X:%.*]] = call {{.*}}%T6return1XC* @_T06return1XCACs5Int64V1i_tcfC(
   // CHECK-SAME:                                  i64 0, %swift.type* swiftself [[META]])
+  // CHECK:  store %T6return1XC* [[X]], %T6return1XC** [[ALLOCA]]
   // CHECK:  @swift_rt_swift_release to void (%T6return1XC*)*)(%T6return1XC* [[X]])
   if true {
     x.x += 1
   } else {
     x.x -= 1
   }
+  // CHECK:  [[X:%.*]] = load %T6return1XC*, %T6return1XC** [[ALLOCA]]
   // CHECK:  @swift_rt_swift_release to void (%T6return1XC*)*)(%T6return1XC* [[X]])
   // CHECK-SAME:                    , !dbg ![[RELEASE:.*]]
 

test/IRGen/superclass_constraint.swift
@@ -8,9 +8,9 @@ public class CVC<A1: AC> where A1: A {
   // CHECK-LABEL: define{{.*}} @_T021superclass_constraint3CVCCACyxGycfc
   public init() {
     // CHECK: [[A:%.*]] = alloca %T21superclass_constraint3CVCC*
-    // CHECK-NOT: ret
+    // CHECK-NOT: ret %T21superclass_constraint3CVCC*
     // CHECK: store %T21superclass_constraint3CVCC* %0, %T21superclass_constraint3CVCC** [[A]]
-    // CHECK: ret
+    // CHECK: ret %T21superclass_constraint3CVCC*
     var a = self
   }
 }

commit 78e074dbf6b57110e15e4159b84b32ca8d5208d8

Fix the debug locations of inserted operations in AvailableValueAggregator.
This mandatory pass inserts struct_extract operations before earlier
stores to the aggregate but didn't set the debug location of those new
instructions to the location of the store, which caused unexpected
stepping behavior in the debugger.

Fixes a regression from e74367f2b377ed7ef09861c256287639ecc94aba.
<rdar://problem/35459092>

lib/SILOptimizer/Mandatory/PredictableMemOpt.cpp
@@ -519,8 +519,10 @@ SILValue AvailableValueAggregator::handlePrimitiveValue(SILType LoadTy,
   // case.
   ArrayRef<SILInstruction *> InsertPts = Val.getInsertionPoints();
   if (InsertPts.size() == 1) {
-    SavedInsertionPointRAII SavedInsertPt(B, InsertPts[0]);
-    SILValue EltVal = nonDestructivelyExtractSubElement(Val, B, Loc);
+    // Use the scope and location of the store at the insertion point.
+    SILBuilderWithScope Builder(InsertPts[0]);
+    SILLocation Loc = InsertPts[0]->getLoc();
+    SILValue EltVal = nonDestructivelyExtractSubElement(Val, Builder, Loc);
     assert(EltVal->getType() == LoadTy && "Subelement types mismatch");
     return EltVal;
   }
@@ -530,8 +532,10 @@ SILValue AvailableValueAggregator::handlePrimitiveValue(SILType LoadTy,
   SILSSAUpdater Updater;
   Updater.Initialize(LoadTy);
   for (auto *I : Val.getInsertionPoints()) {
-    SavedInsertionPointRAII SavedInsertPt(B, I);
-    SILValue EltVal = nonDestructivelyExtractSubElement(Val, B, Loc);
+    // Use the scope and location of the store at the insertion point.
+    SILBuilderWithScope Builder(I);
+    SILLocation Loc = I->getLoc();
+    SILValue EltVal = nonDestructivelyExtractSubElement(Val, Builder, Loc);
     Updater.AddAvailableValue(I->getParent(), EltVal);
   }
 

test/SILOptimizer/predictable_memopt_locs.swift
@@ -1,0 +1,22 @@
+// RUN: %target-swift-frontend -emit-sil -Xllvm -sil-print-debuginfo %s \
+// RUN:  | %FileCheck %s
+
+struct MyStruct {
+	var a = 12
+}
+
+public func use<T>(_ t : T){}
+
+public func main() {
+	var a = MyStruct() // line 11
+  // Verify that the inserted struct_extract has the same location as the store.
+  // CHECK:  %[[A:.*]] = apply {{.*}} -> MyStruct,
+  // CHECK-SAME: loc {{.*}}:11:10, scope [[S:[0-9]+]]
+  // CHECK-NEXT:  %[[I:.*]] = struct_extract %[[A]]
+  // CHECK-SAME:  loc {{.*}}:11:10, scope [[S]]
+  // CHECK-NEXT:  struct_extract %[[I]]
+  // CHECK-SAME:  loc {{.*}}:11:10, scope [[S]]
+  // CHECK:  store %[[A]] to %0 : $*MyStruct,
+  // CHECK-SAME:  loc {{.*}}:11:10, scope [[S]]
+	use(a.a)
+}