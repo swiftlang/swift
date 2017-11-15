d3613b80175577315e18c142743734e7f6cd1cbc
Graydon Hoare <ghoare@apple.com>
2017-11-15 10:20:16 -0800

    [Platform] Support triple.isSimulatorEnvironment()

--- test/Parse/ConditionalCompilation/simulatorTargetEnv.swift
+++ test/Parse/ConditionalCompilation/simulatorTargetEnv.swift
@@ -1,4 +1,5 @@
 // RUN: %swift -typecheck %s -verify -target x86_64-apple-ios7.0 -parse-stdlib
+// RUN: %swift -typecheck %s -verify -target x86_64-unknown-linux-simulator -parse-stdlib
 // RUN: %swift-ide-test -test-input-complete -source-filename=%s -target x86_64-apple-ios7.0
 
 #if !targetEnvironment(simulator)
