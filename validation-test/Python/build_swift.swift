779eb6dd2a1673d99a4543ca9352d68e3dca9e92
Ross bayer <ross.m.bayer@gmail.com>
2017-11-16 14:34:34 -0800

    Added validation test to run the build_swift unit-tests.

---
+++ validation-test/Python/build_swift.swift
@@ -0,0 +1,7 @@
+// RUN: %{python} -m unittest discover -s utils/build_swift/ -t utils/
+
+// Continuous integration for the OS X Platform also runs the tests in the iPhone, Apple TV and Apple Watch simulators.
+// We only need to run python linting once per OSX Platform test run, rather than once for each supported Apple device.
+// UNSUPPORTED: OS=watchos
+// UNSUPPORTED: OS=ios
+// UNSUPPORTED: OS=tvos
