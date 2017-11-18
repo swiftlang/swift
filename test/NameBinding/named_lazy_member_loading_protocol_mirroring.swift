db06618b9732872f4291d399691328a594c3341f
Graydon Hoare <ghoare@apple.com>
2017-11-17 21:56:51 -0800

    [NamedLazyMemberLoading] Bail on cases that interact with protocol member mirroring.

--- test/NameBinding/named_lazy_member_loading_protocol_mirroring.swift
+++ test/NameBinding/named_lazy_member_loading_protocol_mirroring.swift
@@ -8,16 +8,19 @@
 // Check that named-lazy-member-loading reduces the number of Decls deserialized
 // RUN: %target-swift-frontend -typecheck -I %S/Inputs/NamedLazyMembers -disable-named-lazy-member-loading -stats-output-dir %t/stats-pre %s
 // RUN: %target-swift-frontend -typecheck -I %S/Inputs/NamedLazyMembers -stats-output-dir %t/stats-post %s
-// RUN: %utils/process-stats-dir.py --evaluate-delta 'NumTotalClangImportedEntities < -9' %t/stats-pre %t/stats-post
 
 import NamedLazyMembers
 
-public func foo(d: Doer) {
-  d.doSomeWork()
-  d.doSomeWork(withSpeed:10)
-  d.doVeryImportantWork(speed:10, thoroughness:12)
-  d.doSomeWorkWithSpeed(speed:10, levelOfAlacrity:12)
+public func foo(d: MirroringDoer) {
+  let _ = MirroringDoer.mirroredBaseClassMethod()
+  let _ = MirroringDoer.mirroredDerivedClassMethod()
+  let _ = d.mirroredBaseInstanceMethod()
+  let _ = d.mirroredDerivedInstanceMethod()
+}
 
-  let _ = SimpleDoer()
-  let _ = SimpleDoer.ofNoWork()
+public func foo(d: DerivedFromMirroringDoer) {
+  let _ = MirroringDoer.mirroredBaseClassMethod()
+  let _ = MirroringDoer.mirroredDerivedClassMethod()
+  let _ = d.mirroredBaseInstanceMethod()
+  let _ = d.mirroredDerivedInstanceMethod()
 }
