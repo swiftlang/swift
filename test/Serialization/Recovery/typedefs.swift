b562663c5c76f03888e05388b27779d4744fea50
Jordan Rose <jordan_rose@apple.com>
2017-11-27 18:27:40 -0800

    [test] Add deserialization recovery test for 'override required init'
    This could have had different behavior from normal 'required', but it
    doesn't. Everything seems to work.

--- test/Serialization/Recovery/typedefs.swift
+++ test/Serialization/Recovery/typedefs.swift
@@ -29,11 +29,11 @@ func testSymbols() {
 
 // CHECK-IR-LABEL: define{{.*}} void @_T08typedefs18testVTableBuildingy3Lib4UserC4user_tF
 public func testVTableBuilding(user: User) {
-  // The important thing in this CHECK line is the "i64 28", which is the offset
+  // The important thing in this CHECK line is the "i64 29", which is the offset
   // for the vtable slot for 'lastMethod()'. If the layout here
-  // changes, please check that offset 28 is still correct.
+  // changes, please check that offset 29 is still correct.
   // CHECK-IR-NOT: ret
-  // CHECK-IR: getelementptr inbounds void (%T3Lib4UserC*)*, void (%T3Lib4UserC*)** %{{[0-9]+}}, {{i64 28|i32 31}}
+  // CHECK-IR: getelementptr inbounds void (%T3Lib4UserC*)*, void (%T3Lib4UserC*)** %{{[0-9]+}}, {{i64 29|i32 32}}
   _ = user.lastMethod()
 } // CHECK-IR: ret void
 
@@ -168,6 +168,10 @@ open class User {
   // CHECK-RECOVERY: /* placeholder for init(wrappedRequired:) */
   public required init(wrappedRequired: WrappedInt) {}
 
+  // CHECK: {{^}} init(wrappedRequiredInSub: WrappedInt)
+  // CHECK-RECOVERY: /* placeholder for init(wrappedRequiredInSub:) */
+  public init(wrappedRequiredInSub: WrappedInt) {}
+
   public func lastMethod() {}
 }
 // CHECK: {{^}$}}
@@ -195,7 +199,8 @@ open class User {
 // 25 CHECK-VTABLE-NEXT: #User.init!initializer.1:
 // 26 CHECK-VTABLE-NEXT: #User.init!allocator.1:
 // 27 CHECK-VTABLE-NEXT: #User.init!initializer.1:
-// 28 CHECK-VTABLE-NEXT: #User.lastMethod!1:
+// 28 CHECK-VTABLE-NEXT: #User.init!initializer.1:
+// 29 CHECK-VTABLE-NEXT: #User.lastMethod!1:
 // CHECK-VTABLE: }
 
 
@@ -264,6 +269,10 @@ open class UserSub : User {
   // CHECK: required init(wrappedRequired: WrappedInt?)
   // CHECK-RECOVERY: /* placeholder for init(wrappedRequired:) */
   public required init(wrappedRequired: WrappedInt?) { super.init() }
+
+  // CHECK: required init(wrappedRequiredInSub: WrappedInt?)
+  // CHECK-RECOVERY: /* placeholder for init(wrappedRequiredInSub:) */
+  public required override init(wrappedRequiredInSub: WrappedInt?) { super.init() }
 }
 // CHECK: {{^}$}}
 // CHECK-RECOVERY: {{^}$}}
