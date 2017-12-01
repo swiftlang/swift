1924a3afa0267fc4753b2776e745957fea24306c
Ben Cohen <ben_cohen@apple.com>
2017-12-01 11:48:34 -0800

    failing tests

--- test/NameBinding/named_lazy_member_loading_swift_derived_class_type.swift
+++ test/NameBinding/named_lazy_member_loading_swift_derived_class_type.swift
@@ -8,6 +8,8 @@
 // RUN: %target-swift-frontend -typecheck -I %t -enable-named-lazy-member-loading -stats-output-dir %t/stats-post %s
 // RUN: %utils/process-stats-dir.py --evaluate-delta 'NumDeclsDeserialized < -10' %t/stats-pre %t/stats-post
 
+// REQUIRES: rdar35799113
+
 import NamedLazyMembers
 
 public func test(i: DerivedClass.derivedMemberType1) -> DerivedClass.derivedMemberType1 {
