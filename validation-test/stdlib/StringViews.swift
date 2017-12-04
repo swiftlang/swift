c41ba7e5d273a6217595b67c404d6395bdf0b900
Karoy Lorentey <klorentey@apple.com>
2017-12-04 16:38:34 +0000

    [test] Migrate glass box tests from _LegacyStringCore to _StringGuts.

--- validation-test/stdlib/StringViews.swift
+++ validation-test/stdlib/StringViews.swift
@@ -36,7 +36,9 @@ let replacementCharacter = Character(replacementScalar)
 // Unicode scalars that must be represented with a surrogate pair in
 // UTF16, grapheme clusters composed of multiple Unicode scalars, and
 // invalid UTF16 that should be replaced with replacement characters.
-let winter = String(_fixmeLegacyCore: "🏂☃❅❆❄︎⛄️❄️"._core + [0xD83C, 0x0020, 0xDF67, 0xD83C])
+let winterUTF16 = Array("🏂☃❅❆❄︎⛄️❄️".utf16) + [0xD83C, 0x0020, 0xDF67, 0xD83C]
+var winter = ""
+winter._guts.append(contentsOf: winterUTF16)
 let winterInvalidUTF8: [UTF8.CodeUnit] = replacementUTF8 + ([0x20] as [UTF8.CodeUnit]) + replacementUTF8 + replacementUTF8
 let winterUTF8: [UTF8.CodeUnit] = [
   0xf0, 0x9f, 0x8f, 0x82, 0xe2, 0x98, 0x83, 0xe2, 0x9d, 0x85, 0xe2,
