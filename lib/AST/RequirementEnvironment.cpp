1579938793d460153500974a75c5fdb0a4872a01
Doug Gregor <dgregor@apple.com>
2017-12-11 17:03:07 -0800

    [NFC] Fix new file headers
    (cherry picked from commit beed602bdc4eb7eeaa56fb637e4660451fb26879)

--- lib/AST/RequirementEnvironment.cpp
+++ lib/AST/RequirementEnvironment.cpp
@@ -1,4 +1,4 @@
-//===--- RequirementEnvironment.cpp - ASTContext Implementation -----------===//
+//===--- RequirementEnvironment.cpp - Requirement Environments ------------===//
 //
 // This source file is part of the Swift.org open source project
 //
