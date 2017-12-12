d1bb36425d91b6025ea30378180a732a798d6979
Mark Lacey <mark.lacey@apple.com>
2017-12-12 10:32:45 -0800

    IUO: Add overload choice kind for IUO decls.
    Add a new OverloadChoiceKind for decls that are either IUO-typed, or
    function-typed meaning that the function result type is IUO-typed.
    
    Not currently used, so NFC.

--- lib/Sema/TypeCheckConstraints.cpp
+++ lib/Sema/TypeCheckConstraints.cpp
@@ -3019,6 +3019,7 @@ void Solution::dump(raw_ostream &out) const {
     case OverloadChoiceKind::DeclViaDynamic:
     case OverloadChoiceKind::DeclViaBridge:
     case OverloadChoiceKind::DeclViaUnwrappedOptional:
+    case OverloadChoiceKind::DeclForImplicitlyUnwrappedOptional:
       choice.getDecl()->dumpRef(out);
       out << " as ";
       if (choice.getBaseType())
@@ -3195,6 +3196,7 @@ void ConstraintSystem::print(raw_ostream &out) {
       case OverloadChoiceKind::DeclViaDynamic:
       case OverloadChoiceKind::DeclViaBridge:
       case OverloadChoiceKind::DeclViaUnwrappedOptional:
+      case OverloadChoiceKind::DeclForImplicitlyUnwrappedOptional:
         if (choice.getBaseType())
           out << choice.getBaseType()->getString() << ".";
         out << choice.getDecl()->getBaseName() << ": "
