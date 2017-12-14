1bb1b5de684c07623d80bbec885a103af49560fb
Joe Shajrawi <shajrawi@apple.com>
2017-12-14 13:15:39 -0800

    Add CHECK lines to 0134-rdar35947198 validation-test

--- validation-test/compiler_crashers_2_fixed/0134-rdar35947198.swift
+++ validation-test/compiler_crashers_2_fixed/0134-rdar35947198.swift
@@ -1,4 +1,13 @@
-// RUN: %target-swift-frontend %s -emit-ir -o -
+// RUN: %target-swift-frontend %s -O -emit-sil | %FileCheck %s
+
+// CHECK-LABEL: sil shared [transparent] [thunk] @_T0Sf4main7NumProtA2aBP5valueSdyFTW : $@convention(witness_method: NumProt) (@in_guaranteed Float) -> Double
+// CHECK-NOT: %1 = load %0 : $*Float
+// CHECK-NOT: function_ref @_T0Sf4mainE5valueSdyF : $@convention(method) (Float) -> Double
+// CHECK: %1 = struct_element_addr %0 : $*Float, #Float._value // user: %2
+// CHECK: %2 = load %1 : $*Builtin.FPIEEE32               // user: %3
+// CHECK: %3 = builtin "fpext_FPIEEE32_FPIEEE64"(%2 : $Builtin.FPIEEE32) : $Builtin.FPIEEE64 // user: %4
+// CHECK: %4 = struct $Double (%3 : $Builtin.FPIEEE64)    // user: %5
+// CHECK: return %4 : $Double
 
 public protocol NumProt: Prot {
     func value() -> Double
