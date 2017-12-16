c27b366f46c809406dc6f202139f9ddedf887848
Max Moiseev <moiseev@users.noreply.github.com>
2017-12-16 10:57:02 -0800

    [stdlib] Utilize conditional conformances for FlattenCollection (#13476)
    * [stdlib] Utilize conditional conformances for FlattenCollection
    
    * Fix indentation [NFC]

--- stdlib/public/core/Flatten.swift
+++ stdlib/public/core/Flatten.swift
@@ -1,4 +1,4 @@
-//===--- Flatten.swift.gyb ------------------------------------*- swift -*-===//
+//===--- Flatten.swift ----------------------------------------*- swift -*-===//
 //
 // This source file is part of the Swift.org open source project
 //
@@ -9,13 +9,6 @@
 // See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
 //
 //===----------------------------------------------------------------------===//
-
-%{
-from gyb_stdlib_support import (
-    TRAVERSALS,
-    collectionForTraversal,
-)
-}%
 
 /// An iterator that produces the elements contained in each segment
 /// produced by some `Base` Iterator.
@@ -149,19 +142,12 @@ extension LazySequenceProtocol where Element : Sequence {
   }
 }
 
-% for traversal in ['Forward', 'Bidirectional']:
-%   Collection = 'Flatten' + collectionForTraversal(traversal)
-%   if traversal == 'Forward':
-%     constraints = '%(Base)sIterator.Element : Collection'
-%   if traversal == 'Bidirectional':
-%     constraints = '%(Base)sIterator.Element : BidirectionalCollection'
-%   Index = Collection + 'Index'
-/// A position in a `${Collection}`.
+/// A position in a FlattenCollection
 @_fixed_layout // FIXME(sil-serialize-all)
-public struct ${Index}<BaseElements>
+public struct FlattenCollectionIndex<BaseElements>
   where
-  BaseElements : ${collectionForTraversal(traversal)},
-  ${constraints % {'Base': 'BaseElements.'}} {
+  BaseElements : Collection,
+  BaseElements.Element : Collection {
 
   @_inlineable // FIXME(sil-serialize-all)
   @_versioned // FIXME(sil-serialize-all)
@@ -181,24 +167,26 @@ public struct ${Index}<BaseElements>
   ///
   /// When `_inner != nil`, `_inner!` is a valid subscript of `base[_outer]`;
   /// when `_inner == nil`, `_outer == base.endIndex` and this index is
-  /// `endIndex` of the `${Collection}`.
+  /// `endIndex` of the `FlattenCollection`.
   @_versioned // FIXME(sil-serialize-all)
   internal let _inner: BaseElements.Element.Index?
 }
 
-extension ${Index} : Comparable {
+extension FlattenCollectionIndex : Equatable {
   @_inlineable // FIXME(sil-serialize-all)
   public static func == (
-    lhs: ${Index}<BaseElements>,
-    rhs: ${Index}<BaseElements>
+    lhs: FlattenCollectionIndex<BaseElements>,
+    rhs: FlattenCollectionIndex<BaseElements>
   ) -> Bool {
     return lhs._outer == rhs._outer && lhs._inner == rhs._inner
   }
+}
 
+extension FlattenCollectionIndex : Comparable {
   @_inlineable // FIXME(sil-serialize-all)
   public static func < (
-    lhs: ${Index}<BaseElements>,
-    rhs: ${Index}<BaseElements>
+    lhs: FlattenCollectionIndex<BaseElements>,
+    rhs: FlattenCollectionIndex<BaseElements>
   ) -> Bool {
     // FIXME: swift-3-indexing-model: tests.
     if lhs._outer != rhs._outer {
@@ -218,9 +206,8 @@ extension ${Index} : Comparable {
   }
 }
 
-extension ${Index} : Hashable
-  where BaseElements.Index : Hashable, BaseElements.Element.Index : Hashable
-{
+extension FlattenCollectionIndex : Hashable
+  where BaseElements.Index : Hashable, BaseElements.Element.Index : Hashable {
   public var hashValue: Int {
     return _mixInt(_inner?.hashValue ?? 0) ^ _outer.hashValue
   }
@@ -240,126 +227,36 @@ extension ${Index} : Hashable
 /// * `c.lazy.joined().map(f)` maps lazily and returns a `LazyMapCollection`
 ///
 /// - Note: The performance of accessing `startIndex`, `first`, any methods
-///   that depend on `startIndex`, or of advancing a `${Collection}Index`
+///   that depend on `startIndex`, or of advancing a `FlattenCollectionIndex`
 ///   depends on how many empty subcollections are found in the base
-///   collection, and may not offer the usual performance given by
-///   `Collection` or `${traversal}Index`. Be aware, therefore, that
-///   general operations on `${Collection}` instances may not have the
-///   documented complexity.
+///   collection, and may not offer the usual performance given by `Collection`
+///   or `Index`. Be aware, therefore, that general operation on
+///   `FlattenCollection` instances may not have the documented complexity.
 ///
 /// - See also: `FlattenSequence`
 @_fixed_layout // FIXME(sil-serialize-all)
-public struct ${Collection}<Base> : ${collectionForTraversal(traversal)}
-  where
-  Base : ${collectionForTraversal(traversal)},
-  ${constraints % {'Base': 'Base.'}} {
+public struct FlattenCollection<Base>
+  where Base : Collection, Base.Element : Collection {
   // FIXME: swift-3-indexing-model: check test coverage for collection.
 
-  /// A type that represents a valid position in the collection.
-  ///
-  /// Valid indices consist of the position of every element and a
-  /// "past the end" position that's not valid for use as a subscript.
-  public typealias Index = ${Index}<Base>
+  @_versioned // FIXME(sil-serialize-all)
+  internal var _base: Base
 
   /// Creates a flattened view of `base`.
   @_inlineable // FIXME(sil-serialize-all)
   public init(_ base: Base) {
     self._base = base
   }
+}
 
+extension FlattenCollection : Sequence {
+  public typealias SubSequence = Slice<FlattenCollection>
   /// Returns an iterator over the elements of this sequence.
   ///
   /// - Complexity: O(1).
   @_inlineable // FIXME(sil-serialize-all)
   public func makeIterator() -> FlattenIterator<Base.Iterator> {
     return FlattenIterator(_base: _base.makeIterator())
-  }
-
-  /// The position of the first element in a non-empty collection.
-  ///
-  /// In an empty collection, `startIndex == endIndex`.
-  @_inlineable // FIXME(sil-serialize-all)
-  public var startIndex: Index {
-    let end = _base.endIndex
-    var outer = _base.startIndex
-    while outer != end {
-      let innerCollection = _base[outer]
-      if !innerCollection.isEmpty {
-        return ${Index}(outer, innerCollection.startIndex)
-      }
-      _base.formIndex(after: &outer)
-    }
-
-    return endIndex
-  }
-
-  /// The collection's "past the end" position.
-  ///
-  /// `endIndex` is not a valid argument to `subscript`, and is always
-  /// reachable from `startIndex` by zero or more applications of
-  /// `index(after:)`.
-  @_inlineable // FIXME(sil-serialize-all)
-  public var endIndex: Index {
-    return ${Index}(_base.endIndex, nil)
-  }
-
-  // TODO: swift-3-indexing-model - add docs
-  @_inlineable // FIXME(sil-serialize-all)
-  public func index(after i: Index) -> Index {
-    let innerCollection = _base[i._outer]
-    let nextInner = innerCollection.index(after: i._inner!)
-    if _fastPath(nextInner != innerCollection.endIndex) {
-      return ${Index}(i._outer, nextInner)
-    }
-
-    var nextOuter = _base.index(after: i._outer)
-    while nextOuter != _base.endIndex {
-      let nextInnerCollection = _base[nextOuter]
-      if !nextInnerCollection.isEmpty {
-        return ${Index}(nextOuter, nextInnerCollection.startIndex)
-      }
-      _base.formIndex(after: &nextOuter)
-    }
-
-    return endIndex
-  }
-
-% if traversal == 'Bidirectional':
-  // TODO: swift-3-indexing-model - add docs
-  @_inlineable // FIXME(sil-serialize-all)
-  public func index(before i: Index) -> Index {
-    var prevOuter = i._outer
-    if prevOuter == _base.endIndex {
-      prevOuter = _base.index(before: prevOuter)
-    }
-    var prevInnerCollection = _base[prevOuter]
-    var prevInner = i._inner ?? prevInnerCollection.endIndex
-
-    while prevInner == prevInnerCollection.startIndex {
-      prevOuter = _base.index(before: prevOuter)
-      prevInnerCollection = _base[prevOuter]
-      prevInner = prevInnerCollection.endIndex
-    }
-
-    return ${Index}(prevOuter, prevInnerCollection.index(before: prevInner))
-  }
-% end
-
-  /// Accesses the element at `position`.
-  ///
-  /// - Precondition: `position` is a valid position in `self` and
-  ///   `position != endIndex`.
-  @_inlineable // FIXME(sil-serialize-all)
-  public subscript(
-    position: Index
-  ) -> Base.Element.Element {
-    return _base[position._outer][position._inner!]
-  }
-
-  @_inlineable // FIXME(sil-serialize-all)
-  public subscript(bounds: Range<Index>)
-    -> Slice<${Collection}> {
-    return Slice(base: self, bounds: bounds)
   }
 
   // To return any estimate of the number of elements, we have to start
@@ -388,16 +285,226 @@ public struct ${Collection}<Base> : ${collectionForTraversal(traversal)}
       try innerCollection.forEach(body)
     }
   }
+}
+
+extension FlattenCollection : Collection {
+  /// A type that represents a valid position in the collection.
+  ///
+  /// Valid indices consist of the position of every element and a
+  /// "past the end" position that's not valid for use as a subscript.
+  public typealias Index = FlattenCollectionIndex<Base>
+
+  /// The position of the first element in a non-empty collection.
+  ///
+  /// In an empty collection, `startIndex == endIndex`.
+  @_inlineable // FIXME(sil-serialize-all)
+  public var startIndex: Index {
+    let end = _base.endIndex
+    var outer = _base.startIndex
+    while outer != end {
+      let innerCollection = _base[outer]
+      if !innerCollection.isEmpty {
+        return FlattenCollectionIndex(outer, innerCollection.startIndex)
+      }
+      _base.formIndex(after: &outer)
+    }
+
+    return endIndex
+  }
+
+  /// The collection's "past the end" position.
+  ///
+  /// `endIndex` is not a valid argument to `subscript`, and is always
+  /// reachable from `startIndex` by zero or more applications of
+  /// `index(after:)`.
+  @_inlineable // FIXME(sil-serialize-all)
+  public var endIndex: Index {
+    return FlattenCollectionIndex(_base.endIndex, nil)
+  }
+
+  @_inlineable // FIXME(sil-serialize-all)
+  @_versioned // FIXME(sil-serialize-all)
+  internal func _index(after i: Index) -> Index {
+    let innerCollection = _base[i._outer]
+    let nextInner = innerCollection.index(after: i._inner!)
+    if _fastPath(nextInner != innerCollection.endIndex) {
+      return FlattenCollectionIndex(i._outer, nextInner)
+    }
+
+    var nextOuter = _base.index(after: i._outer)
+    while nextOuter != _base.endIndex {
+      let nextInnerCollection = _base[nextOuter]
+      if !nextInnerCollection.isEmpty {
+        return FlattenCollectionIndex(
+          nextOuter, nextInnerCollection.startIndex)
+      }
+      _base.formIndex(after: &nextOuter)
+    }
+
+    return endIndex
+  }
+
+  @_inlineable // FIXME(sil-serialize-all)
+  @_versioned // FIXME(sil-serialize-all)
+  internal func _index(before i: Index) -> Index {
+    var prevOuter = i._outer
+    if prevOuter == _base.endIndex {
+      prevOuter = _base.index(prevOuter, offsetBy: -1)
+    }
+    var prevInnerCollection = _base[prevOuter]
+    var prevInner = i._inner ?? prevInnerCollection.endIndex
+
+    while prevInner == prevInnerCollection.startIndex {
+      prevOuter = _base.index(prevOuter, offsetBy: -1)
+      prevInnerCollection = _base[prevOuter]
+      prevInner = prevInnerCollection.endIndex
+    }
+
+    return FlattenCollectionIndex(
+      prevOuter, prevInnerCollection.index(prevInner, offsetBy: -1))
+  }
+
+  // TODO: swift-3-indexing-model - add docs
+  @_inlineable // FIXME(sil-serialize-all)
+  public func index(after i: Index) -> Index {
+    return _index(after: i)
+  }
+
+  @_inlineable // FIXME(sil-serialize-all)
+  public func formIndex(after i: inout Index) {
+    i = index(after: i)
+  }
+
+  @_inlineable // FIXME(sil-serialize-all)
+  public func distance(from start: Index, to end: Index) -> Int {
+    // The following line makes sure that distance(from:to:) is invoked on the
+    // _base at least once, to trigger a _precondition in forward only
+    // collections.
+    var _start: Index
+    let _end: Index
+    let step: Int
+    if start > end {
+      _start = end
+      _end = start
+      step = -1
+    }
+    else {
+      _start = start
+      _end = end
+      step = 1
+    }
+    var count = 0
+    while _start != _end {
+      count += step
+      formIndex(after: &_start)
+    }
+    return count
+  }
+
+  @inline(__always)
+  @_inlineable // FIXME(sil-serialize-all)
+  @_versioned // FIXME(sil-serialize-all)
+  internal func _advanceIndex(_ i: inout Index, step: Int) {
+    _sanityCheck(-1...1 ~= step, "step should be within the -1...1 range")
+    i = step < 0 ? _index(before: i) : _index(after: i)
+  }
+
+  @inline(__always)
+  @_inlineable // FIXME(sil-serialize-all)
+  @_versioned // FIXME(sil-serialize-all)
+  internal func _ensureBidirectional(step: Int) {
+    // FIXME: This seems to be the best way of checking whether _base is
+    // forward only without adding an extra protocol requirement.
+    // index(_:offsetBy:limitedBy:) is chosen becuase it is supposed to return
+    // nil when the resulting index lands outside the collection boundaries,
+    // and therefore likely does not trap in these cases.
+    if step < 0 {
+      _ = _base.index(
+        _base.endIndex, offsetBy: step, limitedBy: _base.startIndex)
+    }
+  }
+
+  @_inlineable // FIXME(sil-serialize-all)
+  public func index(_ i: Index, offsetBy n: Int) -> Index {
+    var i = i
+    let step = n.signum()
+    _ensureBidirectional(step: step)
+    for _ in 0 ..< abs(n) {
+      _advanceIndex(&i, step: step)
+    }
+    return i
+  }
+
+  @_inlineable // FIXME(sil-serialize-all)
+  public func formIndex(_ i: inout Index, offsetBy n: Int) {
+    i = index(i, offsetBy: n)
+  }
+
+  @_inlineable // FIXME(sil-serialize-all)
+  public func index(
+    _ i: Index, offsetBy n: Int, limitedBy limit: Index
+  ) -> Index? {
+    var i = i
+    let step = n.signum()
+    // The following line makes sure that index(_:offsetBy:limitedBy:) is
+    // invoked on the _base at least once, to trigger a _precondition in
+    // forward only collections.
+    _ensureBidirectional(step: step)
+    for _ in 0 ..< abs(n) {
+      if i == limit {
+        return nil
+      }
+      _advanceIndex(&i, step: step)
+    }
+    return i
+  }
+
+  @_inlineable // FIXME(sil-serialize-all)
+  public func formIndex(
+    _ i: inout Index, offsetBy n: Int, limitedBy limit: Index
+  ) -> Bool {
+    if let advancedIndex = index(i, offsetBy: n, limitedBy: limit) {
+      i = advancedIndex
+      return true
+    }
+    i = limit
+    return false
+  }
+
+  /// Accesses the element at `position`.
+  ///
+  /// - Precondition: `position` is a valid position in `self` and
+  ///   `position != endIndex`.
+  @_inlineable // FIXME(sil-serialize-all)
+  public subscript(position: Index) -> Base.Element.Element {
+    return _base[position._outer][position._inner!]
+  }
+
+  @_inlineable // FIXME(sil-serialize-all)
+  public subscript(bounds: Range<Index>) -> SubSequence {
+    return Slice(base: self, bounds: bounds)
+  }
+}
+
+extension FlattenCollection : BidirectionalCollection
+  where Base : BidirectionalCollection, Base.Element : BidirectionalCollection {
 
   // FIXME(performance): swift-3-indexing-model: add custom advance/distance
   // methods that skip over inner collections when random-access
 
-  @_versioned // FIXME(sil-serialize-all)
-  internal var _base: Base
+  // TODO: swift-3-indexing-model - add docs
+  @_inlineable // FIXME(sil-serialize-all)
+  public func index(before i: Index) -> Index {
+    return _index(before: i)
+  }
+
+  @_inlineable // FIXME(sil-serialize-all)
+  public func formIndex(before i: inout Index) {
+    i = index(before: i)
+  }
 }
 
-extension ${collectionForTraversal(traversal)}
-  where ${constraints % {'Base': ''}} {
+extension Collection where Element : Collection {
   /// Returns the elements of this collection of collections, concatenated.
   ///
   /// In this example, an array of three ranges is flattened so that the
@@ -422,24 +529,60 @@ extension ${collectionForTraversal(traversal)}
   /// - Returns: A flattened view of the elements of this
   ///   collection of collections.
   @_inlineable // FIXME(sil-serialize-all)
-  public func joined() -> ${Collection}<Self> {
-    return ${Collection}(self)
+  public func joined() -> FlattenCollection<Self> {
+    return FlattenCollection(self)
+  }
+}
+
+extension BidirectionalCollection where Element : BidirectionalCollection {
+  /// Returns the elements of this collection of collections, concatenated.
+  ///
+  /// In this example, an array of three ranges is flattened so that the
+  /// elements of each range can be iterated in turn.
+  ///
+  ///     let ranges = [0..<3, 8..<10, 15..<17]
+  ///
+  ///     // A for-in loop over 'ranges' accesses each range:
+  ///     for range in ranges {
+  ///       print(range)
+  ///     }
+  ///     // Prints "0..<3"
+  ///     // Prints "8..<10"
+  ///     // Prints "15..<17"
+  ///
+  ///     // Use 'joined()' to access each element of each range:
+  ///     for index in ranges.joined() {
+  ///         print(index, terminator: " ")
+  ///     }
+  ///     // Prints: "0 1 2 8 9 15 16"
+  ///
+  /// - Returns: A flattened view of the elements of this
+  ///   collection of collections.
+  @_inlineable // FIXME(sil-serialize-all)
+  public func joined() -> FlattenCollection<Self> {
+    return FlattenCollection(self)
   }
 }
 
 extension LazyCollectionProtocol
-  where
-%   if traversal != 'Forward':
-  Self : ${collectionForTraversal(traversal)},
-  Elements : ${collectionForTraversal(traversal)},
-%   end
-  ${constraints % {'Base': 'Elements.'}}
-{
+  where Self : Collection, Element : Collection {
   /// A concatenation of the elements of `self`.
   @_inlineable // FIXME(sil-serialize-all)
-  public func joined() -> LazyCollection<${Collection}<Elements>> {
-    return ${Collection}(elements).lazy
+  public func joined() -> LazyCollection<FlattenCollection<Elements>> {
+    return FlattenCollection(elements).lazy
   }
 }
 
-% end
+extension LazyCollectionProtocol
+  where Self : BidirectionalCollection, Element : BidirectionalCollection {
+  /// A concatenation of the elements of `self`.
+  @_inlineable // FIXME(sil-serialize-all)
+  public func joined() -> LazyCollection<FlattenCollection<Elements>> {
+    return FlattenCollection(elements).lazy
+  }
+}
+
+@available(*, deprecated, renamed: "FlattenCollectionIndex")
+public typealias FlattenBidirectionalCollectionIndex<T> = FlattenCollectionIndex<T> where T : BidirectionalCollection, T.Element : BidirectionalCollection
+@available(*, deprecated, renamed: "FlattenCollection")
+public typealias FlattenBidirectionalCollection<T> = FlattenCollection<T> where T : BidirectionalCollection, T.Element : BidirectionalCollection
