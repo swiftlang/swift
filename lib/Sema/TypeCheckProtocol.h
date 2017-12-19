d9095b1079b58196f9fedd1f0b5c9b935ad00b71
Doug Gregor <dgregor@apple.com>
2017-12-18 16:33:33 -0800

    [Conformance checking] Don't suppress substitution failures during checking.
    When checking whether a particular protocol conformance satisfies all of
    the protocol's requirements, we were suppressing substitution failures.
    In some cases, this would mean that we marked a conformance "invalid"
    without ever emitting a diagnostic, which would lead to downstream crashes.
    
    Instead, treat substitution failures somewhat more lazily. If we encounter
    one while performing the checking, put the conformance into a "delayed" list
    rather than failing immediately. Teach the top-level type checking
    loop to re-check these conformances, emitting a diagnostic if they
    fail the second time around.
    
    Fixes rdar://problem/35082483 and likely other issues that slipped
    through the type checker or blew up in unpredictable ways.

--- lib/Sema/TypeCheckProtocol.h
+++ lib/Sema/TypeCheckProtocol.h
@@ -636,12 +636,18 @@ public:
 
   /// Check all of the protocols requirements are actually satisfied by a
   /// the chosen type witnesses.
-  void ensureRequirementsAreSatisfied();
+  ///
+  /// \param failUnsubstituted Whether to fail when the requirements of the
+  /// protocol could not be substituted (e.g., due to missing information).
+  /// When true, emits a diagnostic in such cases; when false, enqueues the
+  /// conformance for later checking.
+  void ensureRequirementsAreSatisfied(bool failUnsubstituted);
 
   /// Check the entire protocol conformance, ensuring that all
   /// witnesses are resolved and emitting any diagnostics.
   void checkConformance(MissingWitnessDiagnosisKind Kind);
 };
+
 /// Captures the state needed to infer associated types.
 class AssociatedTypeInference {
   /// The type checker we'll need to validate declarations etc.
