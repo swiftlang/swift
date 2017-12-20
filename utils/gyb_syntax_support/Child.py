674945b8acaa8210fa8be9e7f5e383ba0ed8cbbc
Xi Ge <xi_ge@apple.com>
2017-12-20 12:21:45 -0800

    libSyntax: allow children of syntax nodes to have multiple choices. rdar://35879331
    Some structures of syntax nodes can have children choices, e.g. a
    dictionary expression can either contain a single ':' token or a list of
    key-value pairs.
    
    This patch gives the existing code generation infrastructure a way to
    specify such node choices. Node choices are specified under a child
    declaration with two constraints: a choice cannot be declared as
    optional, and a choice cannot have further recursive choices.
    
    Since we don't have too many node structures with choices, part of the
    SyntaxFactory code for these nodes is manually typed.
    
    This patch also teaches AccessorBlock to use node choices.

--- utils/gyb_syntax_support/Child.py
+++ utils/gyb_syntax_support/Child.py
@@ -8,8 +8,8 @@ class Child(object):
     A child of a node, that may be declared optional or a token with a
     restricted subset of acceptable kinds or texts.
     """
-    def __init__(self, name, kind, is_optional=False,
-                 token_choices=None, text_choices=None):
+    def __init__(self, name, kind, either_group=None, is_optional=False,
+                 token_choices=None, text_choices=None, node_choices=None):
         self.name = name
         self.swift_name = lowercase_first_word(name)
         self.syntax_kind = kind
@@ -38,6 +38,19 @@ class Child(object):
         # This will force validation logic to check the text passed into the
         # token against the choices.
         self.text_choices = text_choices or []
+
+        # A list of valid choices for a child
+        self.node_choices = node_choices or []
+
+        # Check the choices are either empty or multiple
+        assert len(self.node_choices) != 1
+
+        # Check node choices are well-formed
+        for choice in self.node_choices:
+            assert not choice.is_optional, \
+                "node choice %s cannot be optional" % choice.name
+            assert not choice.node_choices, \
+                "node choice %s cannot have further choices" % choice.name
 
     def is_token(self):
         """
