353eb77e19e90b2fe51aad4a7c0e197947f1cc80
clattner <clattner@google.com>
2017-12-21 11:31:06 -0500

    Initial checkin of the TFCanonicalizeCFG pass.
    This introduces the (still in early development) pass that canonicalizes the
    CFG of a SIL function into a well structured single-entry-single-exit regions.
    The SILPass that wraps it won't be immediately used by the compiler itself, but
    is used to allow us to write testcases.  An initial test is included but more
    are necessary and will be added over time.
    
    PiperOrigin-RevId: 179817076

---
+++ lib/SILOptimizer/Mandatory/TFCanonicalizeCFG.h
@@ -0,0 +1,147 @@
+//===--- TFCanonicalizeCFG.h - Transform CFG into SESE regions ------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2017 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+//
+// This defines the data structures used to represent single-entry-single-exit
+// (SESE) regions, and defines the function that canonicalizes a SIL function
+// into this form.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef SWIFT_SILOPTIMIZER_TFCANONICALIZECFG_H
+#define SWIFT_SILOPTIMIZER_TFCANONICALIZECFG_H
+
+#include "swift/SIL/SILBasicBlock.h"
+
+namespace swift {
+namespace tf {
+  /// This is the base class for the nodes that make up the "Single-Entry
+  /// Single-Exit" (SESE) region tree that canonicalizeCFGForXLA produces.  As
+  /// such, each node in the tree has a single start node and a single end node.
+  class SESERegionTree {
+  public:
+    enum KindTy {
+      SingleBlock,
+      Sequence,
+      WhileLoop,
+      If
+    };
+  protected:
+    KindTy kind;
+    SESERegionTree(KindTy kind) : kind(kind) {}
+  public:
+    virtual ~SESERegionTree();
+    KindTy getKind() const { return kind; }
+
+
+    void dump() const;
+    void print(llvm::raw_ostream &OS, unsigned indent = 0) const;
+  };
+
+  /// This region subset is a single basic block.
+  class SingleBlockSESERegion : public SESERegionTree {
+    SILBasicBlock *BB;
+  public:
+    SingleBlockSESERegion(SILBasicBlock *BB)
+    : SESERegionTree(SingleBlock), BB(BB) {
+    }
+
+    SILBasicBlock *getBB() const { return BB; }
+
+    void print(llvm::raw_ostream &OS, unsigned indent = 0) const;
+
+    static bool classof(const SESERegionTree *n) {
+      return n->getKind() == SingleBlock;
+    }
+  };
+
+  /// This region represents a sequential series of regions, e.g. a single block
+  /// followed by an 'if' diamond, followed by a while.
+  class SequenceSESERegion : public SESERegionTree {
+    SmallVector<std::unique_ptr<SESERegionTree>, 4> nodes;
+  public:
+    SequenceSESERegion(MutableArrayRef<std::unique_ptr<SESERegionTree>> nodes)
+      : SESERegionTree(Sequence) {
+      this->nodes.reserve(nodes.size());
+      for (auto &n : nodes)
+        this->nodes.push_back(std::move(n));
+    }
+
+    ArrayRef<std::unique_ptr<SESERegionTree>> getNodes() const {
+      return nodes;
+    }
+
+    MutableArrayRef<std::unique_ptr<SESERegionTree>> takeNodes() {
+      return nodes;
+    }
+
+    void print(llvm::raw_ostream &OS, unsigned indent = 0) const;
+
+    static bool classof(const SESERegionTree *n) {
+      return n->getKind() == Sequence;
+    }
+  };
+
+  /// This represents an XLA-style while loop.  We know that it is properly
+  /// structured for conversion to XLA.  This means that the while loop has a
+  /// header block.  That header block ends with a conditional jump to the exit
+  /// block and the body of the loop.  The body of the loop ends with a backedge
+  /// to the header.
+  class WhileLoopSESERegion : public SESERegionTree {
+    SILBasicBlock *header, *exit;
+    std::unique_ptr<SESERegionTree> body;
+  public:
+    WhileLoopSESERegion(SILBasicBlock *header, SILBasicBlock *exit,
+                        std::unique_ptr<SESERegionTree> body)
+      : SESERegionTree(WhileLoop), header(header), exit(exit),
+        body(std::move(body)) {
+    }
+
+    SILBasicBlock *getHeader() const { return header; }
+    SILBasicBlock *getExit() const { return exit; }
+    SESERegionTree *getBody() const { return body.get(); }
+
+    void print(llvm::raw_ostream &OS, unsigned indent = 0) const;
+
+    static bool classof(const SESERegionTree *n) {
+      return n->getKind() == WhileLoop;
+    }
+  };
+
+
+  class IfSESERegion : public SESERegionTree {
+    SILBasicBlock *header;
+    // TODO: NOT REALLY IMPLEMENTED.
+  public:
+    IfSESERegion(SILBasicBlock *header) : SESERegionTree(If), header(header) {
+    }
+
+    SILBasicBlock *getHeader() const { return header; }
+    //SESERegionTree *getTrue() const { return trueRegion.get(); }
+    //SESERegionTree *getFalse() const { return falseRegion.get(); }
+    //SILBasicBlock *getExit() const { return exit; }
+
+    void print(llvm::raw_ostream &OS, unsigned indent = 0) const;
+
+    static bool classof(const SESERegionTree *n) {
+      return n->getKind() == If;
+    }
+  };
+
+
+  /// Transform the function into a properly nested series of
+  /// single-entry-single-exit regions and return the data structure that
+  /// reflects it.
+  std::unique_ptr<SESERegionTree> canonicalizeCFGForXLA(SILFunction *F);
+
+} // end namespace tf
+} // end namespace swift
+#endif
