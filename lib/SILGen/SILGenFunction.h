710e13b96bb70d718a01eb12d3615ad4968f25f2
Vedant Kumar <vsk@apple.com>
2017-12-21 10:51:56 -0800

    [Coverage] Refactor SIL generation for profiling
    This patch moves the ownership of profiling state from SILGenProfiling
    to SILFunction, where it always belonged. Similarly, it moves ownership
    of the profile reader from SILGenModule to SILModule.
    
    The refactor sets us up to fix a few outstanding code coverage bugs and
    does away with sad hacks like ProfilerRAII. It also allows us to locally
    guarantee that a profile counter increment actually corresponds to the
    SILFunction at hand.
    
    That local guarantee causes a bugfix to accidentally fall out of this
    refactor: we now set up the profiling state for delayed functions
    correctly. Previously, we would set up a ProfilerRAII for the delayed
    function, but its counter increment would never be emitted :(. This fix
    constitutes the only functional change in this patch -- the rest is NFC.
    
    As a follow-up, I plan on removing some dead code in the profiling
    logic and fixing a few naming inconsistencies. I've left that for later
    to keep this patch simple.
    
    (cherry picked from commit eb21693893403618e13dc11823038600fd62faf1)

--- lib/SILGen/SILGenFunction.h
+++ lib/SILGen/SILGenFunction.h
@@ -370,11 +370,14 @@ public:
   bool allowsVoidReturn() const { return ReturnDest.getBlock()->args_empty(); }
 
   /// Emit code to increment a counter for profiling.
-  void emitProfilerIncrement(ASTNode N) {
-    if (SGM.Profiler && SGM.Profiler->hasRegionCounters() &&
-        SGM.M.getOptions().UseProfile.empty())
-      SGM.Profiler->emitCounterIncrement(B, N);
-  }
+  void emitProfilerIncrement(ASTNode Node);
+
+  /// Load the profiled execution count corresponding to \p Node, if one is
+  /// available.
+  ProfileCounter loadProfilerCount(ASTNode Node) const;
+
+  /// Get the PGO node's parent.
+  Optional<ASTNode> getPGOParent(ASTNode Node) const;
 
   SILGenFunction(SILGenModule &SGM, SILFunction &F);
   ~SILGenFunction();
