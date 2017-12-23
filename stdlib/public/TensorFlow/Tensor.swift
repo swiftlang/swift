b6f96f8a97c97d9dc69781aa25125602b86bdf04
clattner <clattner@google.com>
2017-12-23 11:37:34 -0500

    Check in the implementation of the Tensor type, adapted from prior work where it was implemented in terms of NumPy.  This are lots of caveats to this, but this is enough to get the bodies of these values serialized into the TensorFlow SIL module and inlined into client code (as shown in the testcase).
    That said, this isn't enough to get the tf-partition pass to light up: building
    the testcase with the optimizer on causes the compiler to crash.
    
    Next steps include debugging the crash, and checking in the higher level tensor
    APIs like Tensor2D.
    
    PiperOrigin-RevId: 180008060

--- stdlib/public/TensorFlow/Tensor.swift
+++ stdlib/public/TensorFlow/Tensor.swift
@@ -16,26 +16,348 @@
 //
 //===----------------------------------------------------------------------===//
 
+// NOTE: Pretty much everything here is marked @_inlineable/@_versioned.  This
+// causes the body to be serialized into the generated Swift module, which
+// allows it to be inlined into the user's code during deabstraction.  This is
+// really gross, and will get better one way or the other, through compiler
+// changes.
+
 import Swift
+
+// Python PEP 465 makes a compelling argument that matrix multiplication should
+// not be spelled with the standard * operator, so we need a new one.  We'll use
+// the operator, though it is defensible to use a variety of other ones as well.
+infix operator • : MultiplicationPrecedence
 
 public struct Tensor<Element : TensorElementProtocol> {
   // State owned by this tensor.
-  fileprivate let core : TensorCore<Element>
+  @_versioned
+  internal // FIXME: Should be fileprivate, but Swift doesn't allow that yet.
+  let core : TensorCore<Element>
 
+  @_versioned
   internal init(_ c : TensorCore<Element>) {
     core = c
   }
+
+  @_inlineable
+  public var rank : Int { return tfop_rank(core) }
+  @_inlineable
+  public var shape : [Int] { return tfop_shape(core) }
+
+  /// Indicate that this tensor is being moved to the accelerator.
+  @_inlineable
+  public func toDevice() -> Tensor<Element> { return Tensor(tfop_send(core)) }
+
+  /// Indicate that this tensor is being moved to the host.
+  @_inlineable
+  public func toHost() -> Tensor<Element> { return Tensor(tfop_receive(core)) }
 }
 
 // Initializers
 public extension Tensor {
   /// Perform an element conversion from Tensor<U> to Tensor<T>.
+  @_inlineable
   public init<FromType>(_ t : Tensor<FromType>) {
-    fatalError("tensor conversion unimplemented")
+    core = tfop_elt_convert(t.core)
   }
 
   // zeroD initializer, takes exactly one value.
+  @_inlineable
   init(zeroD value: Element) {
-    self.init(TensorCore<Element>())
+    self.init(tfop_literal_0d(value))
+  }
+
+  // oneD initializer, takes an array of values.
+  @_inlineable
+  init(oneD values: [Element]) {
+    self.init(tfop_literal_1d(values))
+  }
+
+  // oneD initializer, takes a vararg list of values.
+  @_inlineable
+  init(oneD values: Element...) {
+    self.init(oneD: values)
+  }
+
+  @_inlineable
+  init(twoD values: [[Element]]) {
+    self.init(tfop_literal_2d(values))
+  }
+
+  @_inlineable
+  init(twoD values: [Element]...) {
+    self.init(twoD: values)
+  }
+
+  /// Zero initializer, takes a list of dimensions.
+  @_inlineable
+  init(zeros dimensions: Int...) {
+    self.init(zeros: dimensions)
+  }
+  @inline(never)  // FIXME: Remove this.
+  init(zeros dimensions: [Int]) {
+    self.init(dimensions.withUnsafeBufferPointer { dimensions in
+      tfop_init_zeros(dimensions)
+    })
+  }
+  /// Ones initializer, takes a list of dimensions.
+  @_inlineable
+  init(ones dimensions: Int...) {
+    self.init(ones: dimensions)
+  }
+  @inline(never)  // FIXME: remove this.
+  init(ones dimensions: [Int]) {
+    self.init(dimensions.withUnsafeBufferPointer { dimensions in
+      tfop_init_ones(dimensions)
+    })
+  }
+
+  // def tf.random_normal(shape, mean=0.0, stddev=1.0, dtype=dtypes.float32,
+  //                      seed=None, name=None):
+  @inline(never)  // FIXME: Remove this.
+  public init(randomNormal dimensions: Int...,
+    mean: Double = 0, stddev: Double = 1) {
+    self.init(tfop_init_random_normal(dimensions, mean, stddev))
+  }
+
+  // def tf.eye(shape)
+  @inline(never)  // FIXME: Remove this.
+  public init(eye dimensions: [Int]) {
+    self.init(tfop_init_eye(dimensions))
   }
 }
+
+// Subscripting a tensor produces a smaller tensor.
+public extension Tensor {
+  @_inlineable
+  subscript(tensor indices : Int...) -> Tensor {
+    return Tensor(tfop_subscript_tensor(core, indices))
+  }
+
+  // Subscript to get a scalar.
+  @_inlineable
+  subscript(scalar indices : Int...) -> Element {
+    return tfop_subscript_scalar(core, indices)
+  }
+
+  // Slicing out a range of elements.
+  // TODO: begin/end are vectors in general.
+  // tfop_slice(tensor, begin, end) -> tensor
+  @_inlineable
+  subscript(slice : Range<Int>) -> Tensor {
+    return Tensor(tfop_slice(core, start: slice.lowerBound,
+                             end: slice.upperBound))
+  }
+}
+
+extension TensorElementProtocol {
+  // Scalar to Tensor broadcast
+  @_versioned @_inlineable
+  func broadcastTo(rank: Int) -> Tensor<Self> {
+    return Tensor(tfop_broadcast_scalar(self, rank))
+  }
+}
+public extension Tensor {
+  // Tensor to Tensor broadcast
+  @_inlineable
+  func broadcastTo(rank: Int) -> Tensor {
+    return Tensor(tfop_broadcast(core, rank))
+  }
+
+  @_inlineable
+  func broadcastTo(tensor: Tensor) -> Tensor {
+    return Tensor(tfop_broadcast_to(core, tensor.core))
+  }
+}
+
+
+/// Arithmetic Operators.
+public extension Tensor {
+  @_inlineable
+  static func +(lhs: Tensor, rhs: Tensor) -> Tensor {
+    return Tensor(tfop_elt_add(lhs.core, rhs.core))
+  }
+  @_inlineable
+  static func +(lhs: Tensor, rhs: Element) -> Tensor {
+    return lhs + rhs.broadcastTo(rank: lhs.rank)
+  }
+  @_inlineable
+  static func +(lhs: Element, rhs: Tensor) -> Tensor {
+    return lhs.broadcastTo(rank: rhs.rank) + rhs
+  }
+  @_inlineable
+  static func +=(lhs: inout Tensor, rhs: Tensor) {
+    lhs = lhs + rhs
+  }
+
+  @_inlineable
+  static func -(lhs: Tensor, rhs: Tensor) -> Tensor {
+    return Tensor(tfop_elt_subtract(lhs.core, rhs.core))
+  }
+  @_inlineable
+  static func -(lhs: Tensor, rhs: Element) -> Tensor {
+    return lhs - rhs.broadcastTo(rank: lhs.rank)
+  }
+  @_inlineable
+  static func -(lhs: Element, rhs: Tensor) -> Tensor {
+    return lhs.broadcastTo(rank: rhs.rank) - rhs
+  }
+  @_inlineable
+  static func -=(lhs: inout Tensor, rhs: Tensor) {
+    lhs = lhs - rhs
+  }
+  @_inlineable
+  static func -=(lhs: inout Tensor, rhs: Element) {
+    lhs = lhs - rhs
+  }
+
+  @_inlineable
+  static func *(lhs: Tensor, rhs: Tensor) -> Tensor {
+    return Tensor(tfop_elt_multiply(lhs.core, rhs.core))
+  }
+  @_inlineable
+  static func *(lhs: Element, rhs: Tensor) -> Tensor {
+    return lhs.broadcastTo(rank: rhs.rank) * rhs
+  }
+  @_inlineable
+  static func *(lhs: Tensor, rhs: Element) -> Tensor {
+    return lhs * rhs.broadcastTo(rank: lhs.rank)
+  }
+
+  @_inlineable
+  static func /(lhs: Tensor, rhs: Tensor) -> Tensor {
+    return Tensor(tfop_elt_divide(lhs.core, rhs.core))
+  }
+  @_inlineable
+  static func /(lhs: Tensor, rhs: Element) -> Tensor {
+    return lhs / rhs.broadcastTo(rank: lhs.rank)
+  }
+  @_inlineable
+  static func /(lhs: Element, rhs: Tensor) -> Tensor {
+    return lhs.broadcastTo(rank: rhs.rank) / rhs
+  }
+  @_inlineable
+  static func /=(lhs: inout Tensor, rhs: Tensor) {
+    lhs = lhs / rhs
+  }
+  @_inlineable
+  static func /=(lhs: inout Tensor, rhs: Element) {
+    lhs = lhs / rhs
+  }
+
+
+  @_inlineable
+  static func •(lhs: Tensor, rhs: Tensor) -> Tensor {
+    return Tensor(tfop_elt_matmul(lhs.core, rhs.core))
+  }
+
+  @_inlineable
+  static func •(lhs: Element, rhs: Tensor) -> Tensor {
+    return lhs.broadcastTo(rank: rhs.rank) • rhs
+  }
+  @_inlineable
+  static func •(lhs: Tensor, rhs: Element) -> Tensor {
+    return lhs • rhs.broadcastTo(rank: lhs.rank)
+  }
+
+  @_inlineable
+  static func < (lhs: Tensor, rhs: Tensor) -> Tensor<Bool> {
+    return Tensor<Bool>(tfop_elt_less(lhs.core, rhs.core))
+  }
+  @_inlineable
+  static func < (lhs: Tensor, rhs: Element) -> Tensor<Bool> {
+    return lhs < rhs.broadcastTo(rank: lhs.rank)
+  }
+
+  // FIXME: Other comparisons as well.  Should Tensor conform to Numeric or
+  // some other standard protocols?
+}
+
+/// Arithmetic methods.
+public extension Tensor {
+  @_inlineable
+  func transpose() -> Tensor {
+    return Tensor(tfop_transpose(self.core))
+  }
+
+  @_inlineable
+  func mean() -> Element {
+    return tfop_mean(self.core)
+  }
+  @_inlineable
+  func min() -> Element {
+    return tfop_min(self.core)
+  }
+  @_inlineable
+  func max() -> Element {
+    return tfop_max(self.core)
+  }
+  @_inlineable
+  func max(axis: Int...) -> Tensor {
+    return Tensor(tfop_max_axis(self.core, axis))
+  }
+  @_inlineable
+  func argmax() -> Int64 {
+    return tfop_argmax(self.core)
+  }
+  // Sum entire tensor to produce a scalar value.
+  @_inlineable
+  func sum() -> Element {
+    return tfop_sum(self.core)
+  }
+  // Sum tensor along one or more axis, producing a Tensor.
+  @_inlineable
+  func sum(axis: Int...) -> Tensor {
+    return Tensor(tfop_sum_axis(self.core, axis))
+  }
+
+  @_inlineable
+  func square() -> Tensor {
+    // TODO: Is the TF op for this more efficient?
+    return self•self.transpose()
+  }
+
+  func reduceMean() -> Element {
+    // FIXME: Implement!
+    preconditionFailure()
+  }
+
+  @_inlineable
+  func log() -> Tensor {
+    return Tensor(tfop_log(self.core))
+  }
+
+  @_inlineable
+  func tanh() -> Tensor {
+    return Tensor(tfop_tanh(self.core))
+  }
+
+  @_inlineable
+  func exp() -> Tensor {
+    return Tensor(tfop_exp(self.core))
+  }
+
+  @_inlineable
+  func concat(rhs: Tensor) -> Tensor {
+    return Tensor(tfop_concat(self.core, rhs.core))
+  }
+}
+
+
+/// TODO: When TensorCore can produce a summary, we should wire this in.
+#if false
+/// Make "print(someTensor)" print a pretty form of the tensor.
+extension Tensor : CustomStringConvertible {
+  public var description: String {
+    return core.description
+  }
+}
+
+// Make Tensors show up nicely in the Xcode Playground results sidebar.
+extension Tensor : CustomPlaygroundQuickLookable {
+  public var customPlaygroundQuickLook: PlaygroundQuickLook {
+    return core.customPlaygroundQuickLook
+  }
+}
+#endif
--- stdlib/public/TensorFlow/TensorCore.swift
+++ stdlib/public/TensorFlow/TensorCore.swift
@@ -22,6 +22,313 @@ import Swift
 /// Tensor.  It is the primitive thing that reflects the state managed by the
 /// tf-compiler partitioner.
 struct TensorCore<Element : TensorElementProtocol> {
-  // FIXME: Implement
+  // FIXME: Replace the internals of this with a builtin type.
   private var tmpState : UnsafePointer<Int>? = nil
 }
+
+// For "print", REPL, and Playgrounds integeration, we'll eventually want to
+// implement this, probably in terms of fetching a summary.  For now, this is
+// disabled.
+#if false
+/// Make "print(someTensor)" print a pretty form of the tensor.
+extension TensorCore : CustomStringConvertible {
+  public var description: String {
+    fatalError("unimplemented")
+  }
+}
+
+// Make Tensors show up nicely in the Xcode Playground results sidebar.
+extension TensorCore : CustomPlaygroundQuickLookable {
+  public var customPlaygroundQuickLook: PlaygroundQuickLook {
+    return .text(description)
+  }
+}
+#endif
+
+//===----------------------------------------------------------------------===//
+// Primitive "Ops"
+//===----------------------------------------------------------------------===//
+//
+// Ops should eventually have a new attribute like @tf.op(...) that encapsulates
+// their magic and weird behavior, including specifying the TensorFlow op they
+// correspond to and associating a derivative function.  That said, for now we
+// are doing hacky things to get the prototype off the ground, as described
+// below.
+//
+// The implementation of these functions are all marked @inline(never) as an
+// implementation hack that allows the tf-compiler partitioning pass to see
+// the ops instead of having them get inlined away.
+//
+// These each get a @_versioned attribute, which allows the symbol to be
+// accessible outside of the module even though it doesn't have public access
+// control.  This is required because the Tensor type and higher level
+// operations are all @_inlinable.  Trivia: @_versioned and @_in_inlinable are
+// going to get renamed in mainline in the next couple months.
+//
+// The body of these ops just abort, since they are never actually run: these
+// are just markers in the code that are seen by the tf-compiler partitioning
+// pass and then removed, so the bodies are never used.
+//
+// These ops are also marked with @_silgen_name, which gives them simple and
+// predictable names that are easy to match in the partitioner and then lower
+// into graphs.  Op functions that correspond to TensorFlow ops are expected to
+// be of the form:
+//   @inline(never) @_silgen_name("__tfop_<OPNAME>__<OPERANDINFO>__")
+//
+// The <OPNAME> corresponds to the TensorFlow op name, e.g. Add or Const.  The
+// operand info is an encoding of the operands to the function with instructions
+// on how to transform them into in the graph node.  Here are the codes that are
+// recognized so far:
+//
+//    t: the next operand is a TensorCore, and is an "input" to the TF node.
+//    d: the dtype attribute must be added, corresponding to the last TensorCore
+//       operand we saw, or the result type of the function if first.
+//    c: the next operand is a standard library integer or FP type.  We should
+//       pass the value(s) as the 'value' attribute.
+//
+// The tf-compiler expects that all operands passed to the op function will be
+// described by the codes above.  The node created will have results equal to
+// the results of the function, currently they must all be TensorCore results.
+//
+// Here are some example encodings:
+//   1) 'Add' is @_silgen_name("__tfop_Add__tt__").
+//   2) Literal0d form of 'Const' is @_silgen_name("__tfop_Const__dc__").
+//
+
+// A simple wrapper to make the bodies of the op functions more concise.
+func opBody() -> Never {
+  Builtin.unreachable()
+}
+
+/// tfop_send marks the specified Tensor source as intentionally being on the
+/// host, and that the result Tensor is intended to be on the accelerator.  This
+/// enables warnings in the compiler partitioning algorithm about cases of
+/// unanticipated copies back and forth.
+@_versioned @inline(never)
+@_silgen_name("__tfop_send") // Special name, not a TF op!
+func tfop_send<T>(_ c : TensorCore<T>) -> TensorCore<T> {
+  return c
+}
+
+/// tfop_receive marks the specified Tensor source as intentionally being on the
+/// accelerator, and that the result Tensor is intended to be on the host.  This
+/// enables warnings in the compiler partitioning algorithm about cases of
+/// unanticipated copies back and forth.
+@_versioned @inline(never)
+@_silgen_name("__tfop_receive") // Special name, not a TF op!
+func tfop_receive<T>(_ c : TensorCore<T>) -> TensorCore<T> {
+  return c
+}
+
+@_versioned @inline(never) @_silgen_name("_tfop_rank")
+func tfop_rank<T>(_ c : TensorCore<T>) -> Int { opBody() }
+
+
+@_versioned @inline(never) @_silgen_name("_tfop_shape")
+func tfop_shape<T>(_ c : TensorCore<T>) -> [Int] { opBody() }
+
+@_versioned @inline(never) @_silgen_name("__tfop_Const__dc__")
+func tfop_literal_0d<T>(_ value: T) -> TensorCore<T> { opBody() }
+
+
+// FIXME: Teach the partitioner about this.  For now, this executes on the host.
+// FIXME: The issue is that we can't reason about array parameters.
+@_versioned @inline(never) @_silgen_name("_tfop_literal_1d_")
+func tfop_literal_1d<T>(_ values: [T]) -> TensorCore<T> {
+  fatalError("this should become an op")
+}
+
+// FIXME: Teach the partitioner about this.  For now, this executes on the host.
+// FIXME: The issue is that we can't reason about array parameters.
+@_versioned @inline(never) @_silgen_name("_tfop_literal_2d_")
+func tfop_literal_2d<T>(_ values: [[T]]) -> TensorCore<T> {
+  fatalError("this should become an op")
+}
+
+// FIXME: Teach the partitioner about this.  For now, this executes on the host.
+// FIXME: The issue is that we can't reason about array parameters.
+@_versioned @inline(never) @_silgen_name("_tfop_init_zeros")
+func tfop_init_zeros<T>(_ dimensions: UnsafeBufferPointer<Int>)
+  -> TensorCore<T> {
+  fatalError("this should become an op")
+}
+
+// FIXME: Teach the partitioner about this.  For now, this executes on the host.
+// FIXME: The issue is that we can't reason about array parameters.
+@_versioned @inline(never) @_silgen_name("_tfop_init_ones")
+func tfop_init_ones<T>(_ dimensions: UnsafeBufferPointer<Int>) -> TensorCore<T>{
+  fatalError("this should become an op")
+}
+
+
+// def tf.random_normal(shape, mean=0.0, stddev=1.0, dtype=dtypes.float32,
+//                      seed=None, name=None):
+// FIXME: Teach the partitioner about this.  For now, this executes on the host.
+// FIXME: The issue is that we can't reason about array parameters.
+@_versioned @inline(never) @_silgen_name("_tfop_init_random_normal")
+func tfop_init_random_normal<T>(_ dimensions: [Int], _ mean: Double,
+                                _ stddev: Double) -> TensorCore<T> {
+  fatalError("this should become an op")
+}
+
+// def tf.eye(shape)
+// FIXME: Teach the partitioner about this.  For now, this executes on the host.
+// FIXME: The issue is that we can't reason about array parameters.
+@_versioned @inline(never) @_silgen_name("_tfop_init_eye")
+func tfop_init_eye<T>(_ dimensions: [Int]) -> TensorCore<T> {
+  fatalError("this should become an op")
+}
+
+// FIXME: Teach the partitioner about this.  For now, this executes on the host.
+// FIXME: The issue is that we can't reason about array parameters.
+@_versioned @inline(never) @_silgen_name("_tfop_subscript_tensor")
+func tfop_subscript_tensor<T>(_ c : TensorCore<T>,
+                              _ indices : [Int]) -> TensorCore<T> {
+  fatalError("this should become an op")
+}
+
+// FIXME: Teach the partitioner about this.  For now, this executes on the host.
+// FIXME: The issue is that we can't reason about array parameters.
+@_versioned @inline(never) @_silgen_name("_tfop_subscript_scalar_")
+func tfop_subscript_scalar<T>(_ c : TensorCore<T>, _ indices : [Int]) -> T {
+  fatalError("this should become an op")
+}
+
+// FIXME: Bind this to a proper TensorFlow op.
+@_versioned @inline(never) @_silgen_name("_tfop_slice")
+func tfop_slice<T>(_ c : TensorCore<T>, start: Int, end: Int) -> TensorCore<T> {
+  fatalError("this should become an op")
+}
+
+/// Broadcast the specified Tensor to a rank >= its current size, filling in the
+/// new dimensions with rank = 1.
+/// FIXME: Bind this to a proper TensorFlow op.
+@_versioned @inline(never) @_silgen_name("_tfop_broadcast")
+func tfop_broadcast<T>(_ c : TensorCore<T>, _ rank: Int) -> TensorCore<T> {
+  fatalError("this should become an op")
+}
+
+/// Broadcast tensor A to the same shape as B
+/// FIXME: Bind this to a proper TensorFlow op.
+@_versioned @inline(never) @_silgen_name("_tfop_broadcast_to")
+func tfop_broadcast_to<T>(_ a : TensorCore<T>,
+                          _ b: TensorCore<T>) -> TensorCore<T> {
+  fatalError("this should become an op")
+}
+
+
+/// Broadcast the specified scalar value to be a tensor with the same rank as
+/// the specified other tensor, but with dimension=1 for each rank.
+/// FIXME: Bind this to a proper TensorFlow op.
+@_versioned @inline(never) @_silgen_name("_tfop_broadcast_scalar_")
+func tfop_broadcast_scalar<T>(_ value : T, _ rank: Int) -> TensorCore<T> {
+  fatalError("this should become an op")
+}
+
+/// FIXME: Bind this to a proper TensorFlow op.
+@_versioned @inline(never) @_silgen_name("_tfop_transpose")
+func tfop_transpose<T>(_ value : TensorCore<T>) -> TensorCore<T> {
+  fatalError("this should become an op")
+}
+
+@_versioned @inline(never) @_silgen_name("__tfop_Tanh__t__")
+func tfop_tanh<T>(_ value : TensorCore<T>) -> TensorCore<T> { opBody() }
+
+@_versioned @inline(never) @_silgen_name("__tfop_Log__t__")
+func tfop_log<T>(_ value : TensorCore<T>) -> TensorCore<T> { opBody() }
+
+@_versioned @inline(never) @_silgen_name("__tfop_Exp__t__")
+func tfop_exp<T>(_ value : TensorCore<T>) -> TensorCore<T> { opBody() }
+
+/// FIXME: Bind this to a proper TensorFlow op.
+@_versioned @inline(never) @_silgen_name("_tfop_sum_")
+func tfop_sum<T>(_ c : TensorCore<T>) -> T {
+  fatalError("this should become an op")
+}
+
+// FIXME: Teach the partitioner about this.  For now, this executes on the host.
+// FIXME: The issue is that we can't reason about array parameters.
+// TODO: Push the indices array out of this.  Use UnsafeBufferPointer instead.
+@_versioned @inline(never) @_silgen_name("_tfop_sum_axis")
+func tfop_sum_axis<T>(_ c : TensorCore<T>, _ axis : [Int]) -> TensorCore<T> {
+  fatalError("this should become an op")
+}
+
+// FIXME: Teach the partitioner about this.  For now, this executes on the host.
+// FIXME: The issue is that we can't reason about array parameters.
+@_versioned @inline(never) @_silgen_name("_tfop_max_axis")
+func tfop_max_axis<T>(_ c : TensorCore<T>, _ axis : [Int]) -> TensorCore<T> {
+  fatalError("this should become an op")
+}
+
+
+/// FIXME: Bind this to a proper TensorFlow op.
+@_versioned @inline(never) @_silgen_name("_tfop_mean_")
+func tfop_mean<T>(_ c : TensorCore<T>) -> T {
+  fatalError("this should become an op")
+}
+
+/// FIXME: Bind this to a proper TensorFlow op.
+/// TODO: Should this return a Tensor0D to keep it in the guaranteed tensor
+/// domain?
+@_versioned @inline(never) @_silgen_name("_tfop_max_")
+func tfop_max<T>(_ c : TensorCore<T>) -> T {
+  fatalError("this should become an op")
+}
+
+/// FIXME: Bind this to a proper TensorFlow op.
+/// TODO: Should this return a Tensor0D to keep it in the guaranteed tensor
+/// domain?
+@_versioned @inline(never) @_silgen_name("_tfop_argmax_")
+func tfop_argmax<T>(_ c : TensorCore<T>) -> Int64 {
+  fatalError("this should become an op")
+}
+
+/// FIXME: Bind this to a proper TensorFlow op.
+/// TODO: Should this return a Tensor0D to keep it in the guaranteed tensor
+/// domain?
+@_versioned @inline(never) @_silgen_name("_tfop_min_")
+func tfop_min<T>(_ c : TensorCore<T>) -> T {
+  fatalError("this should become an op")
+}
+
+/// FIXME: Bind this to a proper TensorFlow op.
+@_versioned @inline(never) @_silgen_name("_tfop_elt_convert")
+func tfop_elt_convert<From, To>(_ c : TensorCore<From>) -> TensorCore<To> {
+  fatalError("this should become an op")
+}
+
+@_versioned @inline(never) @_silgen_name("__tfop_Add__tt__")
+func tfop_elt_add<T>(_ lhs : TensorCore<T>, _ rhs : TensorCore<T>)
+  -> TensorCore<T> { opBody() }
+
+@_versioned @inline(never) @_silgen_name("__tfop_Sub__tt__")
+func tfop_elt_subtract<T>(_ lhs : TensorCore<T>, _ rhs : TensorCore<T>)
+  -> TensorCore<T> { opBody() }
+
+@_versioned @inline(never) @_silgen_name("__tfop_Mul__tt__")
+func tfop_elt_multiply<T>(_ lhs : TensorCore<T>, _ rhs : TensorCore<T>)
+  -> TensorCore<T> { opBody() }
+
+@_versioned @inline(never) @_silgen_name("__tfop_Div__tt__")
+func tfop_elt_divide<T>(_ lhs : TensorCore<T>, _ rhs : TensorCore<T>)
+  -> TensorCore<T> { opBody() }
+
+/// FIXME: Bind this to a proper TensorFlow op.
+@_versioned @inline(never) @_silgen_name("_tfop_elt_matmul")
+func tfop_elt_matmul<T>(_ lhs : TensorCore<T>,
+                        _ rhs : TensorCore<T>) -> TensorCore<T> {
+  fatalError("this should become an op")
+}
+
+@_versioned @inline(never) @_silgen_name("__tfop_Less__tt__")
+func tfop_elt_less<T>(_ lhs : TensorCore<T>, _ rhs : TensorCore<T>)
+  -> TensorCore<Bool> { opBody() }
+
+/// FIXME: Bind this to a proper TensorFlow op.
+@_versioned @inline(never) @_silgen_name("_tfop_concat")
+func tfop_concat<T>(_ lhs : TensorCore<T>, _ rhs : TensorCore<T>)
+  -> TensorCore<T> {
+  fatalError("this should become an op")
+}
+
