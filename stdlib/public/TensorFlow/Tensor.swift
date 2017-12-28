6da375b5e1c939812d08b1a43c50571951cefdb1
clattner <clattner@google.com>
2017-12-28 13:45:12 -0500

    Now that #tfop exists, there is no reason for the old __tfop functions to persist.  Banish them by inlining them into Tensor.
    PiperOrigin-RevId: 180280371

--- stdlib/public/TensorFlow/Tensor.swift
+++ stdlib/public/TensorFlow/Tensor.swift
@@ -30,49 +30,129 @@ import Swift
 infix operator • : MultiplicationPrecedence
 
 public struct Tensor<Element : TensorElementProtocol> {
-  // State owned by this tensor.
-  @_versioned
-  internal // FIXME: Should be fileprivate, but Swift doesn't allow that yet.
-  let core : TensorHandle<Element>
+  /// A tensor just contains a TensorHandle under the covers.  This is public to
+  /// allow user defined ops, but shouldn't normally be used otherwise.
+  public let handle : TensorHandle<Element>
 
-  @_versioned
   @_inlineable
-  internal init(_ c : TensorHandle<Element>) {
-    core = c
+  public init(_ c : TensorHandle<Element>) {
+    handle = c
   }
+}
 
-  @_inlineable
-  public var rank : Int { return tfop_rank(core) }
-  @_inlineable
-  public var shape : [Int] { return tfop_shape(core) }
+//===----------------------------------------------------------------------===//
+// Shape and Rank accessors
+//===----------------------------------------------------------------------===//
+//
+// TODO: If these are only needed on the host, we could implement it in terms
+// of the TF_TensorHandle API.  In that case, we should sink them to be defined
+// on TensorHandle though, because we don't want people to think that they can
+// call this and have it run on the accelerator.
 
+public extension Tensor {
+  // make @_inlinable when implemented.
+  var rank : Int {
+    fatalError("FIXME: implement rank")
+  }
+  // make @_inlinable when implemented.
+  var shape : [Int] {
+    fatalError("FIXME: implement shape")
+  }
+}
+
+
+//===----------------------------------------------------------------------===//
+// Send and receive magic.
+//===----------------------------------------------------------------------===//
+//
+// By default, when a tensor value is implicitly passed between host and tensor
+// code, the partitioning pass will generate a warning.  Users can indicate that
+// they are doing something intentional by using these methods, which silences
+// the warning.
+
+// TODO: These would be nicer if defined as builtins rather than being "well
+// known functions".
+@_versioned @inline(never)
+@_silgen_name("__tfop_send") // Magic name, not a TF op!
+func tfop_send<T>(_ c : TensorHandle<T>) -> TensorHandle<T> {
+  return c
+}
+@_versioned @inline(never)
+@_silgen_name("__tfop_receive") // Magic name, not a TF op!
+func tfop_receive<T>(_ c : TensorHandle<T>) -> TensorHandle<T> {
+  return c
+}
+
+public extension Tensor {
   /// Indicate that this tensor is being moved to the accelerator.
   @_inlineable
-  public func toDevice() -> Tensor<Element> { return Tensor(tfop_send(core)) }
+  public func toDevice() -> Tensor<Element> { return Tensor(tfop_send(handle)) }
 
   /// Indicate that this tensor is being moved to the host.
   @_inlineable
-  public func toHost() -> Tensor<Element> { return Tensor(tfop_receive(core)) }
+  public func toHost() -> Tensor<Element> { return Tensor(tfop_receive(handle)) }
 }
+
+
+//===----------------------------------------------------------------------===//
+// Ops and Convenience Methods
+//===----------------------------------------------------------------------===//
+//
+// The majority of the Tensor API is implemented in terms of 'ops' that are
+// partitioned out to the TensorFlow graph when the compiler runs.  These
+// ops are intentially designed to reflect TensorFlow ops, but provide nicer
+// Swift syntax for accessing them.  In addition to the core ops themselves,
+// we also define some helper function wrappers, e.g. to make things symmetric
+// and generally feel nice to use.
+//
+// The ops themselves are defined by the primitive #tfop(...) syntax, here are
+// some examples:
+//     result = #tfop("Add", "tt:t", lhs, rhs)
+//     result = #tfop("Const", "dc:t", Float.self, 4.0)
+//
+// The first two parameters to this syntax are the TensorFlow op name as a
+// string, and then a constraint string - which specifies information about the
+// operands and result type of the op.  The inputs are specified as additional
+// arguments that follow.
+//
+// The constraint string is specified as two colon separated lists:
+// "<OPERANDS>:<RESULTS>".  Here are the codes that are recognized for operands
+// so far:
+//
+//    t: the next operand is a TensorHandle, and is an "input" to the TF node.
+//    d: the next operand is a metatype value, and is added as a 'dtype'
+//       attribute to the TF node. [TODO: Use param label to genericize name].
+//    c: the next operand is a standard library integer or FP type.  We should
+//       pass the value(s) as the 'value' attribute.  [TODO: Use param label to
+//       genericize to names other than 'value'].
+//
+// The codes for the results are currently:
+//
+//    t: the result is a TensorHandle<T>, where the T is the same type as one
+//       of the tensor input operands, or the type of the last dtype specified.
+//    t<type>: the result is a TensorHandle<T>, where T is written out manually
+//       using the same type names that TensorFlow ops use.
+//
+
 
 // Initializers
 public extension Tensor {
   /// Perform an element conversion from Tensor<U> to Tensor<T>.
-  @_inlineable
-  public init<FromType>(_ t : Tensor<FromType>) {
-    core = tfop_elt_convert(t.core)
+  @inline(never) // make @_inlinable when implemented.
+  init<FromType>(_ t : Tensor<FromType>) {
+    fatalError("FIXME: Implement element conversion")
   }
 
   // zeroD initializer, takes exactly one value.
   @_inlineable
   init(zeroD value: Element) {
-    self.init(tfop_literal_0d(value))
+    handle = #tfop("Const", "dc:t", Element.self, value)
   }
 
   // oneD initializer, takes an array of values.
-  @_inlineable
+  @inline(never) // make @_inlinable when implemented.
   init(oneD values: [Element]) {
-    self.init(tfop_literal_1d(values))
+    fatalError("FIXME: implement init(oneD:)")
   }
 
   // oneD initializer, takes a vararg list of values.
@@ -81,9 +161,9 @@ public extension Tensor {
     self.init(oneD: values)
   }
 
-  @_inlineable
+  @inline(never) // make @_inlinable when implemented.
   init(twoD values: [[Element]]) {
-    self.init(tfop_literal_2d(values))
+    fatalError("FIXME: implement init(twoD:)")
   }
 
   @_inlineable
@@ -96,79 +176,78 @@ public extension Tensor {
   init(zeros dimensions: Int...) {
     self.init(zeros: dimensions)
   }
-  @inline(never)  // FIXME: Remove this.
+  @inline(never) // make @_inlinable when implemented.
   init(zeros dimensions: [Int]) {
-    self.init(dimensions.withUnsafeBufferPointer { dimensions in
-      tfop_init_zeros(dimensions)
-    })
+    fatalError("FIXME: implement zeros")
   }
   /// Ones initializer, takes a list of dimensions.
   @_inlineable
   init(ones dimensions: Int...) {
     self.init(ones: dimensions)
   }
-  @inline(never)  // FIXME: remove this.
+  @inline(never) // make @_inlinable when implemented.
   init(ones dimensions: [Int]) {
-    self.init(dimensions.withUnsafeBufferPointer { dimensions in
-      tfop_init_ones(dimensions)
-    })
+    fatalError("FIXME: implement ones")
   }
 
   // def tf.random_normal(shape, mean=0.0, stddev=1.0, dtype=dtypes.float32,
   //                      seed=None, name=None):
-  @inline(never)  // FIXME: Remove this.
+  @inline(never) // make @_inlinable when implemented.
   public init(randomNormal dimensions: Int...,
     mean: Double = 0, stddev: Double = 1) {
-    self.init(tfop_init_random_normal(dimensions, mean, stddev))
+    fatalError("FIXME: implement randomNormal")
   }
 
   // def tf.eye(shape)
-  @inline(never)  // FIXME: Remove this.
+  @inline(never) // make @_inlinable when implemented.
   public init(eye dimensions: [Int]) {
-    self.init(tfop_init_eye(dimensions))
+    fatalError("FIXME: implement eye init")
   }
 }
 
 // Subscripting a tensor produces a smaller tensor.
 public extension Tensor {
-  @_inlineable
+  // make @_inlinable when implemented.
   subscript(tensor indices : Int...) -> Tensor {
-    return Tensor(tfop_subscript_tensor(core, indices))
+    fatalError("FIXME: implement subscript to tensor")
   }
 
   // Subscript to get a scalar.
-  @_inlineable
+  // make @_inlinable when implemented.
   subscript(scalar indices : Int...) -> Element {
-    return tfop_subscript_scalar(core, indices)
+    fatalError("FIXME: implement subscript to scalar")
   }
 
   // Slicing out a range of elements.
   // TODO: begin/end are vectors in general.
   // tfop_slice(tensor, begin, end) -> tensor
-  @_inlineable
+  // make @_inlinable when implemented.
   subscript(slice : Range<Int>) -> Tensor {
-    return Tensor(tfop_slice(core, start: slice.lowerBound,
-                             end: slice.upperBound))
+    fatalError(
+      "FIXME: implement slice \(slice.lowerBound) ... \(slice.upperBound)")
   }
 }
 
-extension TensorElementProtocol {
-  // Scalar to Tensor broadcast
-  @_versioned @_inlineable
+public extension TensorElementProtocol {
+  /// Broadcast the specified scalar value to be a tensor with the same rank as
+  /// the specified other tensor, but with dimension=1 for each rank.
+  @inline(never) // make @_inlinable when implemented.
   func broadcastTo(rank: Int) -> Tensor<Self> {
-    return Tensor(tfop_broadcast_scalar(self, rank))
+    fatalError("FIXME: implement scalar broadcast")
   }
 }
 public extension Tensor {
-  // Tensor to Tensor broadcast
-  @_inlineable
+  /// Broadcast the specified Tensor to a rank >= its current size, filling in
+  /// the new dimensions with rank = 1.
+  @inline(never) // make @_inlinable when implemented.
   func broadcastTo(rank: Int) -> Tensor {
-    return Tensor(tfop_broadcast(core, rank))
+    fatalError("FIXME: implement broadcast")
   }
 
-  @_inlineable
+  /// Broadcast self tensor to the same shape as the specified one.
+  @inline(never) // make @_inlinable when implemented.
   func broadcastTo(tensor: Tensor) -> Tensor {
-    return Tensor(tfop_broadcast_to(core, tensor.core))
+    fatalError("FIXME: implement broadcast")
   }
 }
 
@@ -177,7 +256,7 @@ public extension Tensor {
 public extension Tensor {
   @_inlineable
   static func +(lhs: Tensor, rhs: Tensor) -> Tensor {
-    return Tensor(tfop_elt_add(lhs.core, rhs.core))
+    return Tensor(#tfop("Add", "tt:t", lhs.handle, rhs.handle))
   }
   @_inlineable
   static func +(lhs: Tensor, rhs: Element) -> Tensor {
@@ -194,7 +273,7 @@ public extension Tensor {
 
   @_inlineable
   static func -(lhs: Tensor, rhs: Tensor) -> Tensor {
-    return Tensor(tfop_elt_subtract(lhs.core, rhs.core))
+    return Tensor(#tfop("Sub", "tt:t", lhs.handle, rhs.handle))
   }
   @_inlineable
   static func -(lhs: Tensor, rhs: Element) -> Tensor {
@@ -215,7 +294,7 @@ public extension Tensor {
 
   @_inlineable
   static func *(lhs: Tensor, rhs: Tensor) -> Tensor {
-    return Tensor(tfop_elt_multiply(lhs.core, rhs.core))
+    return Tensor(#tfop("Mul", "tt:t", lhs.handle, rhs.handle))
   }
   @_inlineable
   static func *(lhs: Element, rhs: Tensor) -> Tensor {
@@ -228,7 +307,7 @@ public extension Tensor {
 
   @_inlineable
   static func /(lhs: Tensor, rhs: Tensor) -> Tensor {
-    return Tensor(tfop_elt_divide(lhs.core, rhs.core))
+    return Tensor(#tfop("Div", "tt:t", lhs.handle, rhs.handle))
   }
   @_inlineable
   static func /(lhs: Tensor, rhs: Element) -> Tensor {
@@ -248,9 +327,9 @@ public extension Tensor {
   }
 
 
-  @_inlineable
+  @inline(never) // make @_inlinable when implemented.
   static func •(lhs: Tensor, rhs: Tensor) -> Tensor {
-    return Tensor(tfop_elt_matmul(lhs.core, rhs.core))
+    fatalError("FIXME: implement matmul")
   }
 
   @_inlineable
@@ -264,7 +343,7 @@ public extension Tensor {
 
   @_inlineable
   static func < (lhs: Tensor, rhs: Tensor) -> Tensor<Bool> {
-    return Tensor<Bool>(tfop_elt_less(lhs.core, rhs.core))
+    return Tensor<Bool>(#tfop("Less", "tt:t<bool>", lhs.handle, rhs.handle))
   }
   @_inlineable
   static func < (lhs: Tensor, rhs: Element) -> Tensor<Bool> {
@@ -277,40 +356,42 @@ public extension Tensor {
 
 /// Arithmetic methods.
 public extension Tensor {
-  @_inlineable
+  @inline(never) // make @_inlinable when implemented.
   func transpose() -> Tensor {
-    return Tensor(tfop_transpose(self.core))
+    fatalError("FIXME: implement transpose")
   }
 
-  @_inlineable
+  @inline(never) // make @_inlinable when implemented.
   func mean() -> Element {
-    return tfop_mean(self.core)
+    fatalError("FIXME: implement mean")
   }
-  @_inlineable
+  @inline(never) // make @_inlinable when implemented.
   func min() -> Element {
-    return tfop_min(self.core)
+    fatalError("FIXME: implement min")
   }
-  @_inlineable
+  @inline(never) // make @_inlinable when implemented.
   func max() -> Element {
-    return tfop_max(self.core)
+    fatalError("FIXME: implement max")
   }
-  @_inlineable
+  @inline(never) // make @_inlinable when implemented.
   func max(axis: Int...) -> Tensor {
-    return Tensor(tfop_max_axis(self.core, axis))
+    fatalError("FIXME: implement max axis")
   }
-  @_inlineable
+
+  /// TODO: Should this return a Tensor0D to keep it in the tensor domain?
+  @inline(never) // make @_inlinable when implemented.
   func argmax() -> Int64 {
-    return tfop_argmax(self.core)
+    fatalError("FIXME: implement argmax")
   }
   // Sum entire tensor to produce a scalar value.
-  @_inlineable
+  @inline(never) // make @_inlinable when implemented.
   func sum() -> Element {
-    return tfop_sum(self.core)
+    fatalError("FIXME: implement sum")
   }
   // Sum tensor along one or more axis, producing a Tensor.
-  @_inlineable
+  @inline(never) // make @_inlinable when implemented.
   func sum(axis: Int...) -> Tensor {
-    return Tensor(tfop_sum_axis(self.core, axis))
+    fatalError("FIXME: implement sum axis")
   }
 
   @_inlineable
@@ -326,22 +407,22 @@ public extension Tensor {
 
   @_inlineable
   func log() -> Tensor {
-    return Tensor(tfop_log(self.core))
+    return Tensor(#tfop("Log", "t:t", self.handle))
   }
 
   @_inlineable
   func tanh() -> Tensor {
-    return Tensor(tfop_tanh(self.core))
+    return Tensor(#tfop("Tanh", "t:t", self.handle))
   }
 
   @_inlineable
   func exp() -> Tensor {
-    return Tensor(tfop_exp(self.core))
+    return Tensor(#tfop("Exp", "t:t", self.handle))
   }
 
-  @_inlineable
+  @inline(never) // make @_inlinable when implemented.
   func concat(rhs: Tensor) -> Tensor {
-    return Tensor(tfop_concat(self.core, rhs.core))
+    fatalError("FIXME: implement concat")
   }
 }
 
@@ -351,14 +432,14 @@ public extension Tensor {
 /// Make "print(someTensor)" print a pretty form of the tensor.
 extension Tensor : CustomStringConvertible {
   public var description: String {
-    return core.description
+    return handle.description
   }
 }
 
 // Make Tensors show up nicely in the Xcode Playground results sidebar.
 extension Tensor : CustomPlaygroundQuickLookable {
   public var customPlaygroundQuickLook: PlaygroundQuickLook {
-    return core.customPlaygroundQuickLook
+    return handle.customPlaygroundQuickLook
   }
 }
 #endif
