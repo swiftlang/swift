7d49bea6efb9b2bc46e43716b583c55ea70ca481
clattner <clattner@google.com>
2018-01-02 18:46:16 -0500

    Change the partitioning pass to generate a call to the "start" function defined in the runtime:  - We generate the list of inputs as a tuple on the stack, and pass the address    of that tuple into the runtime call.  - This adds logic to find the runtime call from the TensorFlow module.  - This rearranges the CompilerRuntime.swift code a bit so that the very    carefully crafted entrypoints the compiler uses are separated from the    implementation logic.
    PiperOrigin-RevId: 180603715

--- stdlib/public/TensorFlow/CompilerRuntime.swift
+++ stdlib/public/TensorFlow/CompilerRuntime.swift
@@ -19,93 +19,133 @@ import CTensorFlow
 // The C type is TF_Status*
 public typealias CTF_Status = OpaquePointer?
 
-public class TensorProgram {
-  // Kept for debugging. Currently unused.
-  let inputTensors: [AnyTensorHandle]
-
-  let outputTensors: [AnyTensorHandle]
-
-  init(inputs: [AnyTensorHandle], outputs: [AnyTensorHandle]) {
-    inputTensors = inputs
-    outputTensors = outputs
-  }
-}
-
 func checkOk(_ s: CTF_Status) {
   precondition(TF_GetCode(s) == TF_OK, String(cString: TF_Message(s)))
 }
 
 // The call sequence for the APIs below must be one of the two:
-// _TFCStartTensorProgram() -> _TFCTerminateTensorProgram()
-// _TFCStartTensorProgram() -> _TFCFinishTensorProgram()
+// init -> terminate()
+// init -> finish()
+public final class TensorProgram {
+  // inputTensors is kept for debugging. Currently unused.
+  let inputTensors: [AnyTensorHandle]
+  let outputTensors: [AnyTensorHandle]
+
+  // Load the TF computation from a binary TF FunctionDef proto given by 'bytes'
+  // and 'size', start the computation, and return a state object as a unique
+  // identifier for that computation.
+  //
+  // For now we only support programs that output 1 tensor. To support multiple
+  // output tensors, we may need to extend this API to pass in a
+  // outputTensorCount.
+  @_versioned
+  init(_ bytes: UnsafeRawPointer, _ size: Int,
+       _ inputTensorPtr: UnsafePointer<AnyTensorHandle>,
+       _ inputTensorCount: Int) {
+    inputTensors =
+      Array(UnsafeBufferPointer(start: inputTensorPtr, count: inputTensorCount))
+
+    let s = TF_NewStatus()
+
+    let tfFunc = TF_FunctionImportFunctionDef(bytes, size, s)
+    checkOk(s)
+
+    // Now we start the graph computation.
+    let opts = TFE_NewContextOptions()
+    let ctx = TFE_NewContext(opts, s)
+    checkOk(s)
+    TFE_DeleteContextOptions(opts)
+
+    TFE_ContextAddFunction(ctx, tfFunc, s)
+    checkOk(s)
+    TF_DeleteFunction(tfFunc)
+
+    let op = TFE_NewOp(ctx, "the_function", s)
+    checkOk(s)
+
+    for inputTensor in inputTensors {
+      let cTensorHandle = inputTensor.cTensorHandle
+      TFE_OpAddInput(op, cTensorHandle, s)
+      checkOk(s)
+    }
+
+    var retValCount: CInt = 1
+    var retVals = [CTensorHandle](repeating: nil, count: Int(retValCount))
+    TFE_Execute(op, &retVals, &retValCount, s)
+    checkOk(s)
+    precondition(retValCount == 1)
+    TFE_DeleteOp(op)
+
+    let outputTensors = retVals.map(AnyTensorHandle.init)
+
+    TFE_DeleteContext(ctx, s)
+    checkOk(s)
+    TF_DeleteStatus(s)
+
+    self.outputTensors = outputTensors
+  }
+
+  // Terminate the computation as given by 'program', and clean up the state.
+  //
+  // TODO(hongm): add real logic, including handling input/output and errors.
+  @_versioned
+  func terminate() {
+  }
+
+  // Wait for completion the computation as given by 'program', and returns output
+  // handles.
+  //
+  // TODO(hongm): add real logic, including handling input/output and errors.
+  @_versioned
+  func finish() -> [AnyTensorHandle] {
+    return outputTensors
+  }
+}
+
+
+
+//===----------------------------------------------------------------------===//
+//  Compiler runtime entrypoints
+//===----------------------------------------------------------------------===//
+
+// These are the entrypoints that are well-known to the compiler internals.  The
+// signatures and forms must not be changed without updating the compiler.  Any
+// code put into the body of these functions will end up being inlined into the
+// user code, so they are generally just wrappers around the implementation
+// above.
 
 // Load the TF computation from a binary TF FunctionDef proto given by 'bytes'
 // and 'size', start the computation, and return a state object as a unique
 // identifier for that computation.
 //
-// For now we only support programs that output 1 tensor. To support multiple
-// output tensors, we may need to extend this API to pass in a
-// outputTensorCount.
-public // COMPILER_INTRINSIC
-func _TFCStartTensorProgram(
+@_inlineable
+@_silgen_name("_swift_tfc_StartTensorProgram")
+public func _TFCStartTensorProgram(
   _ bytes: UnsafeRawPointer,
   _ size: Int,
-  _ inputTensors: UnsafePointer<AnyTensorHandle>,
-  _ numInputTensors: Int
+  _ inputTensorPtr: UnsafePointer<AnyTensorHandle>,
+  _ inputTensorCount: Int
 ) -> TensorProgram {
-  let s = TF_NewStatus()
-
-  let tfFunc = TF_FunctionImportFunctionDef(bytes, size, s)
-  checkOk(s)
-
-  // Now we start the graph computation.
-  let opts = TFE_NewContextOptions()
-  let ctx = TFE_NewContext(opts, s)
-  checkOk(s)
-  TFE_DeleteContextOptions(opts)
-
-  TFE_ContextAddFunction(ctx, tfFunc, s)
-  checkOk(s)
-  TF_DeleteFunction(tfFunc)
-
-  let op = TFE_NewOp(ctx, "the_function", s)
-  checkOk(s)
-  let inputTensorArray : [AnyTensorHandle] =
-    Array(UnsafeBufferPointer(start: inputTensors, count: numInputTensors))
-  for inputTensor in inputTensorArray {
-    let cTensorHandle = inputTensor.cTensorHandle
-    TFE_OpAddInput(op, cTensorHandle, s)
-    checkOk(s)
-  }
-
-  var retValCount: CInt = 1
-  var retVals = [CTensorHandle](repeating: nil, count: Int(retValCount))
-  TFE_Execute(op, &retVals, &retValCount, s)
-  checkOk(s)
-  precondition(retValCount == 1)
-  TFE_DeleteOp(op)
-
-  let outputTensors = retVals.map(AnyTensorHandle.init)
-
-  TFE_DeleteContext(ctx, s)
-  checkOk(s)
-  TF_DeleteStatus(s)
-
-  return TensorProgram(inputs: inputTensorArray, outputs: outputTensors)
+  return TensorProgram(bytes, size, inputTensorPtr, inputTensorCount)
 }
 
 // Terminate the computation as given by 'program', and clean up the state.
-public // COMPILER_INTRINSIC
-func _TFCTerminateTensorProgram(program: TensorProgram) {
-  // Since we current do all the tensor computation in _TFCStartTensorProgram(),
-  // this function is a no op.
+//
+@_inlineable
+@_silgen_name("_swift_tfc_TerminateTensorProgram")
+public func _TFCTerminateTensorProgram(_ program: TensorProgram) {
+  program.terminate()
 }
 
 // Wait for completion the computation as given by 'program', and returns output
 // handles.
-public // COMPILER_INTRINSIC
-func _TFCFinishTensorProgram(
+//
+@_inlineable
+@_silgen_name("_swift_tfc_FinishTensorProgram")
+public func _TFCFinishTensorProgram(
   _ program: TensorProgram
-) -> [AnyTensorHandle] {
-  return program.outputTensors
+  ) -> [AnyTensorHandle] {
+  return program.finish()
 }
+
+
