ff56f11b3929f037ee265eec475d20068d4a2e1c
hongm <hongm@google.com>
2018-01-02 16:50:53 -0500

    git multi showInitial Swift TF runtime API impl based on TF eager C API, with some unit testing on Tanh and Add functions.
    PiperOrigin-RevId: 180588105

--- stdlib/public/TensorFlow/CompilerRuntime.swift
+++ stdlib/public/TensorFlow/CompilerRuntime.swift
@@ -16,14 +16,23 @@
 
 import CTensorFlow
 
-// TODO(hongm): replace the dummy impl below with a real one.
-public class TensorProgram {
-  let inputTensors: [AnyTensorHandle]
-  // let session_handle: UnsafePointer<CChar>
+// The C type is TF_Status*
+public typealias CTF_Status = OpaquePointer?
 
-  init(_ input: [AnyTensorHandle]) {
-      inputTensors = input
+public class TensorProgram {
+  // Kept for debugging. Currently unused.
+  let inputTensors: [AnyTensorHandle]
+
+  let outputTensors: [AnyTensorHandle]
+
+  init(inputs: [AnyTensorHandle], outputs: [AnyTensorHandle]) {
+    inputTensors = inputs
+    outputTensors = outputs
   }
+}
+
+func checkOk(_ s: CTF_Status) {
+  precondition(TF_GetCode(s) == TF_OK, String(cString: TF_Message(s)))
 }
 
 // The call sequence for the APIs below must be one of the two:
@@ -34,39 +43,69 @@ public class TensorProgram {
 // and 'size', start the computation, and return a state object as a unique
 // identifier for that computation.
 //
-// TODO(hongm): add real logic, including handling input/output and errors.
-public func _TFCStartTensorProgram(
+// For now we only support programs that output 1 tensor. To support multiple
+// output tensors, we may need to extend this API to pass in a
+// outputTensorCount.
+public // COMPILER_INTRINSIC
+func _TFCStartTensorProgram(
   _ bytes: UnsafeRawPointer,
   _ size: Int,
   _ inputTensors: UnsafePointer<AnyTensorHandle>,
   _ numInputTensors: Int
 ) -> TensorProgram {
-  // This place holder call confirms we are able to call the TF C APIs.
-  // It is currently working for swift/src/swift/test/TensorFlowRuntime:tf_main,
-  // but not swift/src/swift/test/TensorFlowRuntime/RunTime.swift.test.
-  let s /*TF_Status**/ = TF_NewStatus()
+  let s = TF_NewStatus()
 
-  // The compiler wants to pass us the input tensors as an
-  // unsafepointer + length, but we can transform that into an Array or any
-  // other type if that is convenient.
-  let inputTensorArray = Array(UnsafeBufferPointer(start: inputTensors,
-                                                   count: numInputTensors))
-  let program = TensorProgram(inputTensorArray)
-  return program
+  let tfFunc = TF_FunctionImportFunctionDef(bytes, size, s)
+  checkOk(s)
+
+  // Now we start the graph computation.
+  let opts = TFE_NewContextOptions()
+  let ctx = TFE_NewContext(opts, s)
+  checkOk(s)
+  TFE_DeleteContextOptions(opts)
+
+  TFE_ContextAddFunction(ctx, tfFunc, s)
+  checkOk(s)
+  TF_DeleteFunction(tfFunc)
+
+  let op = TFE_NewOp(ctx, "the_function", s)
+  checkOk(s)
+  let inputTensorArray : [AnyTensorHandle] =
+    Array(UnsafeBufferPointer(start: inputTensors, count: numInputTensors))
+  for inputTensor in inputTensorArray {
+    let cTensorHandle = inputTensor.cTensorHandle
+    TFE_OpAddInput(op, cTensorHandle, s)
+    checkOk(s)
+  }
+
+  var retValCount: CInt = 1
+  var retVals = [CTensorHandle](repeating: nil, count: Int(retValCount))
+  TFE_Execute(op, &retVals, &retValCount, s)
+  checkOk(s)
+  precondition(retValCount == 1)
+  TFE_DeleteOp(op)
+
+  let outputTensors = retVals.map(AnyTensorHandle.init)
+
+  TFE_DeleteContext(ctx, s)
+  checkOk(s)
+  TF_DeleteStatus(s)
+
+  return TensorProgram(inputs: inputTensorArray, outputs: outputTensors)
 }
 
 // Terminate the computation as given by 'program', and clean up the state.
-//
-// TODO(hongm): add real logic, including handling input/output and errors.
-public func _TFCTerminateTensorProgram(program: TensorProgram) {
+public // COMPILER_INTRINSIC
+func _TFCTerminateTensorProgram(program: TensorProgram) {
+  // Since we current do all the tensor computation in _TFCStartTensorProgram(),
+  // this function is a no op.
 }
 
 // Wait for completion the computation as given by 'program', and returns output
 // handles.
-//
-// TODO(hongm): add real logic, including handling input/output and errors.
-public func _TFCFinishTensorProgram(
+public // COMPILER_INTRINSIC
+func _TFCFinishTensorProgram(
   _ program: TensorProgram
 ) -> [AnyTensorHandle] {
-  return program.inputTensors
+  return program.outputTensors
 }
