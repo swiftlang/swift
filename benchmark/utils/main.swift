1eaf1f5571a14bcbc59d4b33881d93c29b04999e
Michael Ilseman <milseman@apple.com>
2018-01-05 11:04:18 -0800

    [benchmark] Hook up CSVParsing to suite

--- benchmark/utils/main.swift
+++ benchmark/utils/main.swift
@@ -30,6 +30,7 @@ import ArraySubscript
 import BitCount
 import ByteSwap
 import CString
+import CSVParsing
 import Calculator
 import CaptureProp
 import CharacterLiteralsLarge
@@ -151,6 +152,7 @@ registerBenchmark(ArraySubscript)
 registerBenchmark(BitCount)
 registerBenchmark(ByteSwap)
 registerBenchmark(CString)
+registerBenchmark(CSVParsing)
 registerBenchmark(Calculator)
 registerBenchmark(CaptureProp)
 registerBenchmark(CharacterLiteralsLarge)
