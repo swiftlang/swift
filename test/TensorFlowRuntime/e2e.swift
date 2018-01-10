76d127cc39fe1d32d03dd6c46cf9066000f33690
hongm <hongm@google.com>
2018-01-10 13:18:29 -0500

    Bug fixes and performance improvements.
    PiperOrigin-RevId: 181482526

---
+++ test/TensorFlowRuntime/e2e.swift
@@ -0,0 +1,14 @@
+// RUN: %target-run-simple-swift
+// REQUIRES: executable_test
+
+// A simple end-to-end test. Currently segfaults.
+
+import TensorFlow
+
+// @inline(never)
+// public func testTensorExample() {
+//   let x = Tensor<Float>(2.0)
+//   // FIXME: add code to verify y's content when we can read a tensor locally.
+// }
+
+// testTensorExample()
