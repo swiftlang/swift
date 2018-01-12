6001c835237e2d4948a0643bdb74f0d68cccc7ba
Davide Italiano <ditaliano@apple.com>
2018-01-12 13:37:47 -0800

    [SILOptimizer] Relax CHECK lines to work on a debug stdlib.
    <rdar://problem/36475591>

--- test/SILOptimizer/constantprop-wrongscope.swift
+++ test/SILOptimizer/constantprop-wrongscope.swift
@@ -4,7 +4,6 @@
 // RUN:   %S/../ClangImporter/Inputs/custom-modules -emit-sil -o /dev/null \
 // RUN:   -primary-file %s -Xllvm -sil-print-debuginfo -module-name patatino \
 // RUN:   -Onone -Xllvm -sil-print-after=diagnostic-constant-propagation \
-// RUN:   -Xllvm -sil-print-only-functions=$S8patatino19indexedSubscripting1b3idx1aySo1BC_SiSo1ACtF \
 // RUN:   2>&1 | %FileCheck %s
 
 // REQUIRES: objc_interop
@@ -13,11 +12,10 @@
 // Make sure that the destroy_addr instruction has the same scope of the
 // instructions surrounding it.
 
-// CHECK:   %60 = unchecked_take_enum_data_addr %12 : $*Optional<Any>, #Optional.some!enumelt.1, loc {{.*}}:24:13, scope 2
-// CHECK:   destroy_addr %7 : $*Any, loc {{.*}}:24:19, scope 2
-// CHECK:   dealloc_stack %12 : $*Optional<Any>, loc {{.*}}:24:23, scope 2
-// CHECK:   dealloc_stack %7 : $*Any, loc {{.*}}:24:23, scope 2
-// CHECK:   dealloc_stack %6 : $*A, loc {{.*}}:24:7, scope 2
+// CHECK:   destroy_addr %7 : $*Any, loc {{.*}}:22:19, scope 2
+// CHECK:   dealloc_stack %12 : $*Optional<Any>, loc {{.*}}:22:23, scope 2
+// CHECK:   dealloc_stack %7 : $*Any, loc {{.*}}:22:23, scope 2
+// CHECK:   dealloc_stack %6 : $*A, loc {{.*}}:22:7, scope 2
 
 import Foundation
 func indexedSubscripting(b b: B, idx: Int, a: A) {
