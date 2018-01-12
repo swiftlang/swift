d9860edff33c825ee62889565db7d53dbefe6812
Michael Ilseman <milseman@apple.com>
2018-01-11 20:08:32 -0800

    Revert "[tests] Tighten up DebugInfo test with more labels/dags"
    This reverts commit c11ec78a65c31904396b311696bfb4fab8a9615f.

--- test/DebugInfo/letstring.swift
+++ test/DebugInfo/letstring.swift
@@ -1,26 +1,27 @@
-// RUN: %target-swift-frontend %s -emit-ir -g -module-name main -o %t.ll
+// RUN: %target-swift-frontend %s -emit-ir -g -o %t.ll
 // RUN: %FileCheck %s < %t.ll
 
 class UIWindow {}
 class AppDelegate {
   var window: UIWindow?
-  // CHECK-LABEL: define hidden {{.*}}i1 {{.*}}11AppDelegateC1f
+  // CHECK: define hidden {{.*}}i1 {{.*}}11AppDelegateC1f
   func f() -> Bool {
     // Test for -O0 shadow copies.
-    // CHECK-DAG: call void @llvm.dbg.declare(metadata %T4main11AppDelegateC** {{.*}}, metadata ![[SELF:.*]], metadata !DIExpression())
-    // CHECK-DAG: call void @llvm.dbg.declare(metadata %TSS* {{.*}}, metadata ![[A:.*]], metadata !DIExpression())
+    // CHECK: call void @llvm.dbg.declare({{.*}}, metadata ![[SELF:.*]], metadata !DIExpression())
+    // CHECK-NOT: call void @llvm.dbg.value
+    // CHECK: call void @llvm.dbg.declare({{.*}}, metadata ![[A:.*]], metadata !DIExpression())
     let a = "let"
     // CHECK-NOT: call void @llvm.dbg.value
     // CHECK: call void @llvm.dbg.declare({{.*}}, metadata ![[B:.*]], metadata !DIExpression())
     // CHECK-NOT: call void @llvm.dbg.value
+    // CHECK: ret
+    // CHECK-DAG: ![[SELF]] = !DILocalVariable(name: "self", arg: 1{{.*}} line: [[@LINE-10]],
+    // CHECK-DAG: ![[A]] = !DILocalVariable(name: "a",{{.*}} line: [[@LINE-6]],
+    // CHECK-DAG: ![[B]] = !DILocalVariable(name: "b",{{.*}} line: [[@LINE+1]],
     var b = "var"
     self.window = UIWindow()
-    // CHECK-LABEL: ret i1 true
     return true
   }
-  // CHECK-DAG: ![[SELF]] = !DILocalVariable(name: "self", arg: 1{{.*}} line: 8,
-  // CHECK-DAG: ![[A]] = !DILocalVariable(name: "a",{{.*}} line: 12,
-  // CHECK-DAG: ![[B]] = !DILocalVariable(name: "b",{{.*}} line: 16,
 }
 
 // End-to-end test:
