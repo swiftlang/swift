76604e74da5f6a8687ba1f39b675af115370386a
Chris Eidhof <chris@eidhof.nl>
2018-01-13 19:18:10 +0100

    CSV alt example

---
+++ benchmark/single-source/CSVParsingAlt.swift
@@ -0,0 +1,28 @@
+extension String {
+    func parseAlt() -> [[String]] {
+        var result: [[String]] = [[]]
+        var currentField = "".unicodeScalars
+        var inQuotes = false
+        
+        @inline(__always) func flush() {
+            result[result.endIndex-1].append(String(currentField))
+            currentField.removeAll()
+        }
+        
+        for c in self.unicodeScalars {
+            switch (c, inQuotes) {
+            case (",", false):
+                flush()
+            case ("\n", false):
+                flush()
+                result.append([])
+            case ("\"", _):
+                inQuotes = !inQuotes
+            default:
+                currentField.append(c)
+            }
+        }
+        flush()
+        return result
+    }
+}
