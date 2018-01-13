7f3368ef69a0bfeb36902ff9389d66601718d0bc
Argyrios Kyrtzidis <akyrtzi@gmail.com>
2018-01-12 22:38:28 -0800

    Merge pull request #13916 from akyrtzi/fix-clang-headers-order
    [ClangImporter] Make sure that headers from the bridging header are considered 'isBeforeInTranslationUnit' compared to headers imported from swift code
    
    (cherry picked from commit f0c04053537b980e813d380f62bab4049189061f)

--- lib/IDE/CodeCompletion.cpp
+++ lib/IDE/CodeCompletion.cpp
@@ -5514,6 +5514,11 @@ void PrintingCodeCompletionConsumer::handleResults(
     Result->getCompletionString()->getName(NameOs);
     OS << "; name=" << Name;
 
+    StringRef comment = Result->getBriefDocComment();
+    if (IncludeComments && !comment.empty()) {
+      OS << "; comment=" << comment;
+    }
+
     OS << "\n";
   }
   OS << "End completions\n";
