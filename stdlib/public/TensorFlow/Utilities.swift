ade7180dd1d62a8597e13ec15ede481e0b217bd7
rxwei <rxwei@google.com>
2018-01-17 19:58:28 -0500

    Complete a style pass and add documentation to compiler runtime.
    Minor changes:
    - Extract stuff that's used across TensorFlow stdlib to Utilities.swift.
    - Change _TFCRuntimeConfig to an enum so that it can never be instantiated.
    - Use `withMemoryRebound(to:_:)` instead of `bindMemory(to:capacity:_:)` becase the result buffer of TF_AllocateTensor is already considered bound to the data type.
    - Add utility functions to log to stderr.
    - Nuke `_TFCCreateHostTensorCopy` as it's no longer needed.
    - Rename `CTFOperator` to `CTFEOp` to reflect the correct C name.
    - Rename `operator` to `op`.
    
    PiperOrigin-RevId: 182292587

---
+++ stdlib/public/TensorFlow/Utilities.swift
@@ -0,0 +1,81 @@
+//===-- Utilities.swift ---------------------------------------*- swift -*-===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2017 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+//
+// This file defines the utilities functions and common type alises.
+//
+//===----------------------------------------------------------------------===//
+
+import Glibc
+import CTensorFlow
+
+//===----------------------------------------------------------------------===//
+// - MARK: Runtime checkers
+//===----------------------------------------------------------------------===//
+
+/// These checks run in both debug and release modes (while assert() only runs in
+/// debug mode), to help shake out more bugs and facilitate debugging in the
+/// early project phases. It can be replaced with plain assert() later, when we
+/// have a more mature code base.
+@_versioned
+func internalConsistencyCheck(
+  _ predicate: Bool,
+  _ errMessage: String = "TF runtime assertion failure",
+  file: StaticString = #file,
+  line: UInt = #line
+) {
+  guard predicate else {
+    fatalError(errMessage, file: file, line: line)
+  }
+}
+
+@_versioned
+func checkOk(_ s: CTFStatus?, file: StaticString = #file, line: UInt = #line) {
+  internalConsistencyCheck(TF_GetCode(s) == TF_OK,
+                           String(cString: TF_Message(s)),
+                           file: file, line: line)
+}
+
+//===----------------------------------------------------------------------===//
+// - MARK: Type aliases
+//===----------------------------------------------------------------------===//
+
+/// TF_Status* type.
+typealias CTFStatus = OpaquePointer
+
+/// TF_Function* type
+typealias CTFFunction = OpaquePointer
+
+/// TF_Tensor* type.
+typealias CTensor = OpaquePointer
+
+/// TF_Context* type.
+typealias CTFContext = OpaquePointer
+
+/// TFE_Op* type.
+typealias CTFEOp = OpaquePointer
+
+//===----------------------------------------------------------------------===//
+// - MARK: Logging
+//===----------------------------------------------------------------------===//
+
+/// Log to standard error.
+func logToStderr(_ message: StaticString) {
+  message.utf8Start
+    .withMemoryRebound(to: Int8.self, capacity: message.utf8CodeUnitCount) {
+    _ = fputs($0, stderr)
+  }
+}
+
+/// Log to standard error.
+func logToStderr(_ message: String) {
+  _ = fputs(message, stderr)
+}
