33a5a2151de892dc83227a4c5bb2f0a5cd12ade6
Luke Larson <llarson@apple.com>
2018-01-22 14:24:47 -0800

    Disable type_checker_perf/slow/rdar23620262.swift since its flaky

--- validation-test/Sema/type_checker_perf/slow/rdar23620262.swift
+++ validation-test/Sema/type_checker_perf/slow/rdar23620262.swift
@@ -1,6 +1,6 @@
 // RUN: %target-typecheck-verify-swift -solver-expression-time-threshold=1
 // REQUIRES: tools-release,no_asserts
-// XFAIL: rdar35803518
+// REQUIRES: disabled-rdar35803518
 
 let a: [Double] = []
 _ = a.map { $0 - 1.0 }
