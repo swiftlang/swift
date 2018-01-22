bf6fc3e90f2bb6e6a668631b7fcc5a2da761aea0
Davide Italiano <ditaliano@apple.com>
2018-01-22 15:54:00 -0800

    [SILOptimizer] Require Obj-C interoperability for this test.
    Also fixup check lines.

--- test/SILOptimizer/di-conditional-destroy-scope.swift
+++ test/SILOptimizer/di-conditional-destroy-scope.swift
@@ -3,10 +3,12 @@
 // RUN:   -sil-print-only-functions=$S2fs36RecursibleDirectoryContentsGeneratorC4path10fileSystemAcA12AbsolutePathV_AA04FileH0_ptKc33_F8B132991B28208F48606E87DC165393Llfc \
 // RUN:   -Xllvm -sil-print-debuginfo -o /dev/null 2>&1 | %FileCheck %s
 
-// CHECK: %48 = begin_borrow %2 : $RecursibleDirectoryContentsGenerator, loc {{.*}}:36:5, scope 2
-// CHECK: %49 = ref_element_addr %48 : $RecursibleDirectoryContentsGenerator, #RecursibleDirectoryContentsGenerator.fileSystem, loc {{.*}}:36:5, scope 2
-// CHECK:  destroy_addr %49 : $*FileSystem, loc {{.*}}:36:5, scope 2
-// CHECK:  end_borrow %48 from %2 : $RecursibleDirectoryContentsGenerator, $RecursibleDirectoryContentsGenerator, loc {{.*}}:36:5, scope 2
+// REQUIRES: objc_interop
+
+
+// CHECK: %49 = ref_element_addr %48 : $RecursibleDirectoryContentsGenerator, #RecursibleDirectoryContentsGenerator.fileSystem, loc {{.*}}:38:5, scope 2
+// CHECK:  destroy_addr %49 : $*FileSystem, loc {{.*}}:38:5, scope 2
+
 
 import Foundation
 
