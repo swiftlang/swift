979fd613f9e67a7ba1c0625fc2d2dd065c7c67c2
Kuba Mracek <mracek@apple.com>
2018-01-28 10:05:20 -0800

    Use '(void)' in Metadata.cpp to avoid "variable unused" warning in non-assert builds.

--- stdlib/public/runtime/Metadata.cpp
+++ stdlib/public/runtime/Metadata.cpp
@@ -2741,6 +2741,7 @@ allocateWitnessTable(GenericWitnessTable *genericTable,
   // default implementations.
   size_t numMandatoryRequirements = protocol->NumMandatoryRequirements;
   assert(numPatternWitnesses >= numMandatoryRequirements);
+  (void)numMandatoryRequirements;
 
   // The total number of requirements.
   size_t numRequirements = protocol->NumRequirements;
