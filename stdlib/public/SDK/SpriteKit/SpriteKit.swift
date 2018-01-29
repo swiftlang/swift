a94a9d0909136db16af67c970c632cee00ef7d47
Connor Wakamo <cwakamo@apple.com>
2018-01-29 02:00:39 -0500

    [SpriteKit] Removed a no-longer-needed shim class from the SpriteKit overlay.
    The CustomPlaygroundQuickLookable conformances have been deleted, so this is no longer required.

--- stdlib/public/SDK/SpriteKit/SpriteKit.swift
+++ stdlib/public/SDK/SpriteKit/SpriteKit.swift
@@ -21,14 +21,6 @@ public typealias SKColor = NSColor
 public typealias SKColor = UIColor
 #endif
 
-// this class only exists to allow AnyObject lookup of _copyImageData
-// since that method only exists in a private header in SpriteKit, the lookup
-// mechanism by default fails to accept it as a valid AnyObject call
-@objc class _SpriteKitMethodProvider : NSObject {
-  override init() { _sanityCheckFailure("don't touch me") }
-  @objc func _copyImageData() -> NSData! { return nil }
-}
-
 @available(iOS, introduced: 10.0)
 @available(OSX, introduced: 10.12)
 @available(tvOS, introduced: 10.0)
