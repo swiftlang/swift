80229c763c1641036495d0be3584ec6bd23c5f89
Pavel Yaskevich <pyaskevich@apple.com>
2018-01-31 11:42:20 -0800

    [CSSolver] Fix performance regression related to contraction of closure parameters
    Improve situation around closure parameter/argument contractions
    by allowing such action if it can be proved that none of the bindings
    would result in solver attempting to bind parameter to `inout` type.
    
    Resolves: rdar://problem/36838495
    (cherry picked from commit 3b7e555c7ebf91e30a90dfd1383b41921df59275)

--- lib/Sema/CSSolver.cpp
+++ lib/Sema/CSSolver.cpp
@@ -1787,9 +1787,11 @@ static bool shouldSkipDisjunctionChoice(ConstraintSystem &cs,
     auto &score = bestNonGenericScore->Data;
     // Let's skip generic overload choices only in case if
     // non-generic score indicates that there were no forced
-    // unwrappings of optional(s) and no unavailable overload
-    // choices present in the solution.
-    if (score[SK_ForceUnchecked] == 0 && score[SK_Unavailable] == 0)
+    // unwrappings of optional(s), no unavailable overload
+    // choices present in the solution, and there are no
+    // non-trivial function conversions.
+    if (score[SK_ForceUnchecked] == 0 && score[SK_Unavailable] == 0 &&
+        score[SK_FunctionConversion] == 0)
       return true;
   }
 
