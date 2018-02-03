93666e7b1dbc5ea825d7a1653467d5b5bfab5a65
rxwei <rxwei@google.com>
2018-02-02 20:24:59 -0500

    Fix type checking and improve diagnostics for '@differentiable' attribute.
    PiperOrigin-RevId: 184357835

--- test/TensorFlow/differentiable_type_checking.swift
+++ test/TensorFlow/differentiable_type_checking.swift
@@ -10,12 +10,40 @@ func dfoo(_ x: Float, primal: Float, seed: Float) -> Float {
   return 2 * x
 }
 
-@differentiable(gradient: dfoo(_:primal:_:)) // expected-error {{cannot resolve gradient function 'dfoo(_:primal:_:)'}}
+@differentiable(gradient: dfoo(_:primal:seed:)) // ok!
+func foo(_ x: Float) -> Float {
+  return x * x
+}
+
+func dbar(_ x: Float, _ y: Float, primal: Float, seed: Float) -> (Float, Float) {
+  return (1, 1)
+}
+
+@differentiable(gradient: dbar(_:_:primal:seed:)) // ok!
+func bar(_ x: Float, _ y: Float) -> Float {
+  return x + y
+}
+
+func dfoo2_wrong_type(_ x: Float, primal: Float, seed: Double) -> Float {
+  return 2 * x
+}
+
+@differentiable(gradient: dfoo2_wrong_type(_:primal:seed:)) // expected-error {{'dfoo2_wrong_type(_:primal:seed:)' does not have an overload with expected type '(Float, Float, Float) -> Float'}}
 func foo2(_ x: Float) -> Float {
   return x * x
 }
 
-@differentiable(gradient: meow) // expected-error {{cannot resolve gradient function 'meow'}}
+@differentiable(gradient: dfoo(_:primal:_:)) // expected-error {{undefined identifier 'dfoo(_:primal:_:)' as gradients function}}
 func foo3(_ x: Float) -> Float {
   return x * x
+}
+
+@differentiable(gradient: meow) // expected-error {{undefined identifier 'meow' as gradients function}}
+func foo4(_ x: Float) -> Float {
+  return x * x
+}
+
+@differentiable(gradient: woof) // expected-error {{'foo5' has no arguments to differentiate with respect to}}
+func foo5() -> Float {
+  return 1
 }
