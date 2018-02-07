cb34826dc72cc3d0329472032df2cc38129ffcf7
Rintaro Ishizaki <rishizaki@apple.com>
2018-02-07 18:29:03 +0900

    [Syntax] Introduce CodeBlockItem
    CodeBlockItem represents Decl, Stmt or Expr that optionally followed by
    semi-colon.
    SourceFile syntax holds a list of CodeBlockItem.

--- utils/gyb_syntax_support/CommonNodes.py
+++ utils/gyb_syntax_support/CommonNodes.py
@@ -1,4 +1,5 @@
-from Node import Node
+from Child import Child
+from Node import Node  # noqa: I201
 
 COMMON_NODES = [
     Node('Decl', kind='Syntax'),
@@ -11,4 +12,29 @@ COMMON_NODES = [
     Node('UnknownStmt', kind='Stmt'),
     Node('UnknownType', kind='Type'),
     Node('UnknownPattern', kind='Pattern'),
+
+    # code-block-item = (decl | stmt | expr) ';'?
+    Node('CodeBlockItem', kind='Syntax',
+         children=[
+             Child('Item', kind='Syntax',
+                   node_choices=[
+                       Child('Decl', kind='Decl'),
+                       Child('Stmt', kind='Stmt'),
+                       Child('Expr', kind='Expr'),
+                   ]),
+             Child('Semicolon', kind='SemicolonToken',
+                   is_optional=True),
+         ]),
+
+    # code-block-item-list -> code-block-item code-block-item-list?
+    Node('CodeBlockItemList', kind='SyntaxCollection',
+         element='CodeBlockItem'),
+
+    # code-block -> '{' stmt-list '}'
+    Node('CodeBlock', kind='Syntax',
+         children=[
+             Child('OpenBrace', kind='LeftBraceToken'),
+             Child('Statements', kind='CodeBlockItemList'),
+             Child('CloseBrace', kind='RightBraceToken'),
+         ]),
 ]
