e1ed8788bf361b756782ae3b4b33e9b541510587
rxwei <rxwei@google.com>
2018-02-08 14:46:54 -0500

    Rename 'Unit' to 'Scalar'. Rename 'AccelerableTensorUnit' to 'AccelerableByTensorFlow'.
    Note:
    - AccelerableByTensorFlow is arguably a great name. It conveys the pragmatic meaning "this scalar is accelerable by TensorFlow".
    - Don't worry, I didn't rename unit tests to scalar tests.
    PiperOrigin-RevId: 185027603

--- test/TensorFlow/diagnostics.swift
+++ test/TensorFlow/diagnostics.swift
@@ -23,7 +23,7 @@ public func nonConstantAttribute(x: Tensor<Float>, someBool: Bool) {
 }
 
 public func shapeError() {
-  // expected-error @+1 {{tensor literal should have 9 units for this shape, but has 8}}
+  // expected-error @+1 {{tensor literal should have 9 scalars for this shape, but has 8}}
   let _ = Tensor<Float>(shape: [1, 3, 3, 1],
-                        units: [0, 1, 0, 1, 1, 1, 0, 1])
+                        scalars: [0, 1, 0, 1, 1, 1, 0, 1])
 }
