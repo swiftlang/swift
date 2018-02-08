ab893a736d91428bc3d6c80661d4f0b07e410544
Max Moiseev <moiseev@apple.com>
2018-02-08 14:38:24 -0800

    Implement an opaque Index type for StrideTo Collection conformance

--- validation-test/stdlib/Stride.swift
+++ validation-test/stdlib/Stride.swift
@@ -16,13 +16,13 @@ StrideTestSuite.test("through") {
   checkSequence(Array(0...5).reversed(), stride(from: 5, through: 0, by: -1))
 }
 
-#if false
 StrideTestSuite.test("StrideToCollection/Int") {
   let odds = stride(from: 1, to: 10, by: 2)
   let expected = (1..<10).filter { $0 % 2 != 0 }
-  checkForwardCollection(expected, odds)
+  checkRandomAccessCollection(expected, odds)
 }
 
+#if false
 StrideTestSuite.test("StrideThroughCollection/Int") {
   let odds = stride(from: 1, through: 10, by: 2)
   let expected = (1...10).filter { $0 % 2 != 0 }
