c6ff7b40cc25ccc9eb7e895db7ca40a31add0019
Pavel Yaskevich <pyaskevich@apple.com>
2018-02-08 01:17:31 -0800

    [CSBindings] Look through optional types when trying to validate l-valueness of the new bindings
    When bindings are picked for particular type variable, right-hand
    side of the binding might be another type variable wrapped into optional
    type, when trying to determine if both sides of the binding have the
    same l-valueness it's imperative to look throught optional type of the
    right-hand side. Otherwise new binding might be effectively unsolvable.
    
    Resolves: rdar://problem/37291371

--- validation-test/compiler_crashers_fixed/28625-destoptionals-size-destextraoptionals-srcoptionals-size.swift
+++ validation-test/compiler_crashers_fixed/28625-destoptionals-size-destextraoptionals-srcoptionals-size.swift
@@ -5,7 +5,6 @@
 // See https://swift.org/LICENSE.txt for license information
 // See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
 
-// RUN: not --crash %target-swift-frontend %s -emit-ir
-[.a
-[Int?as?Int
-nil?
+// RUN: not %target-swift-frontend %s -emit-ir
+
+nil?as?Int??
