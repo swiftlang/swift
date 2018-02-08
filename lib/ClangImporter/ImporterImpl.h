78784a6723c38997453167a0ae8a569529c5d189
Jordan Rose <jordan_rose@apple.com>
2018-02-08 11:29:06 -0800

    [ClangImporter] Handle submodule imports in bridging headers
    If a bridging header imported a submodule but /not/ the top-level
    module, the top-level module would never be made visible. This is a
    problem since the Clang importer makes all top-level decls available
    through their enclosing top-level modules, not the individual
    submodules.
    
    To fix this, stop special-casing the way we handle bridging header
    imports; we can translate them from Clang Modules to Swift ModuleDecls
    the same way we do for regular imports.
    
    rdar://problem/37355259
    (cherry picked from commit 6754cc6dd0c88864fe508bdea55f31b0504dbfe7)

--- lib/ClangImporter/ImporterImpl.h
+++ lib/ClangImporter/ImporterImpl.h
@@ -336,7 +336,6 @@ public:
 
   bool IsReadingBridgingPCH;
   llvm::SmallVector<clang::serialization::SubmoduleID, 2> PCHImportedSubmodules;
-  llvm::SmallVector<const clang::Module*, 2> DeferredHeaderImports;
 
   const Version CurrentVersion;
 
@@ -529,7 +528,7 @@ public:
   ClangModuleUnit *ImportedHeaderUnit = nullptr;
 
   /// The modules re-exported by imported headers.
-  llvm::SmallVector<ModuleDecl::ImportedModule, 8> ImportedHeaderExports;
+  llvm::SmallVector<clang::Module *, 8> ImportedHeaderExports;
 
   /// The modules that requested imported headers.
   ///
