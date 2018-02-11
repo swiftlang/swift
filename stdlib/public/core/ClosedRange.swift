83343e8c9df937ba591bf53e4e995325f9e059b9
Karoy Lorentey <klorentey@apple.com>
2018-02-11 02:44:50 +0000

    [stdlib] ClosedRange: Describe availability of Hashable conformance

--- stdlib/public/core/ClosedRange.swift
+++ stdlib/public/core/ClosedRange.swift
@@ -383,6 +383,7 @@ extension ClosedRange: Equatable {
   }
 }
 
+@available(swift, introduced: 4.1) // FIXME(conformance-availability)
 extension ClosedRange : Hashable where Bound : Hashable {
   /// The hash value for the range.
   ///
@@ -391,6 +392,7 @@ extension ClosedRange : Hashable where Bound : Hashable {
   /// Hash values are not guaranteed to be equal across different executions of
   /// your program. Do not save hash values to use during a future execution.
   @_inlineable // FIXME(sil-serialize-all)
+  @available(swift, introduced: 4.1)
   public var hashValue: Int {
     return _combineHashValues(lowerBound.hashValue, upperBound.hashValue)
   }
