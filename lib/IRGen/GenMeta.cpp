034489f141be5ba263bbe3dfe93a201d2ff4b37d
rxwei <rxwei@google.com>
2018-02-11 19:15:28 -0500

    Add TensorProtocol to the compiler-known protocol list. This will be used later when we make #tfop consume T : TensorProtocol instead of TensorHandle. Also remove unused AccelerableByTensorFlow.
    PiperOrigin-RevId: 185319791

--- lib/IRGen/GenMeta.cpp
+++ lib/IRGen/GenMeta.cpp
@@ -3472,7 +3472,7 @@ SpecialProtocol irgen::getSpecialProtocolID(ProtocolDecl *P) {
   case KnownProtocolKind::Encodable:
   case KnownProtocolKind::Decodable:
   // SWIFT_ENABLE_TENSORFLOW
-  case KnownProtocolKind::AccelerableByTensorFlow:
+  case KnownProtocolKind::TensorProtocol:
     return SpecialProtocol::None;
   }
 
