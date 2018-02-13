dc8c88058fa17b56ffcdafec48b24663f9cd06cf
Mark Lacey <mark.lacey@apple.com>
2018-02-12 22:09:43 -0800

    Fix an issue with the constraint favoring code in the constraint optimizer.
    The code was favoring overloads where *either* argument matched its
    parameter type and where both parameter types were the same. This
    is really overly broad and can result in bugs like the one here, where
    we only attempt the
      (Int, Int) -> Bool
    overload of '!=' despite the fact that it means forcing an
    optional. We should select the
      <T>(T?, T?) -> Bool
    overload instead, and inject the non-optional operand into an optional.
    
    Making this require *both* arguments to match mostly disables this
    optimization, though, since for somethin like "a"+"b"+"c" the second
    add has a type variable and string literal as arugments.
    
    So instead, we'll just disable this in cases where one argument is the
    optional of the other, to ensure we never try forcing one side. This
    also means we'll disable the optimization in cases where we would
    inject one operand into an optional, but that's likely find as we
    don't have long chains of expressions where that happens.
    
    Fixes rdar://problem/37073401.

--- test/stdlib/ImplicitlyUnwrappedOptional.swift
+++ test/stdlib/ImplicitlyUnwrappedOptional.swift
@@ -57,5 +57,18 @@ ImplicitlyUnwrappedOptionalTests.test("flatMap") {
   // expectNil((3 as Int!).flatMap(half))
 }
 
-runAllTests()
+infix operator *^* : ComparisonPrecedence
 
+func *^*(lhs: Int?, rhs: Int?) -> Bool { return true }
+func *^*(lhs: Int, rhs: Int) -> Bool { return true }
+
+ImplicitlyUnwrappedOptionalTests.test("preferOptional") {
+  let i: Int! = nil
+  let j: Int = 1
+  if i != j {} // we should choose != for Optionals rather than forcing i
+  if i == j {} // we should choose == for Optionals rather than forcing i
+  // FIXME: https://bugs.swift.org/browse/SR-6988
+  //  if i *^* j {} // we should choose *^* for Optionals rather than forcing i
+}
+
+runAllTests()
