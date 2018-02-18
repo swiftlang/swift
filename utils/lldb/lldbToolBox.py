8b960718031a5bb16cd09e2f342568332e3be22d
Michael Gottesman <mgottesman@apple.com>
2018-02-17 21:56:23 -0800

    [lldb-with-tools] Add a new utility script called lldb-with-tools.
    This is a simple tool that starts lldb, but before it runs your commands, uses
    the -O command to load lldbToolBox.py. This provide sthe llvm data formatters as
    well as potential future lldb extensions that we write for swift itself.
    
    A better name for this utility would be much appreciated, but I think for now
    the name "lldb-with-tools" is at least self explanatory.

--- utils/lldb/lldbToolBox.py
+++ utils/lldb/lldbToolBox.py
@@ -10,7 +10,7 @@ to the swift checkout.
 import os
 
 REPO_BASE = os.path.abspath(os.path.join(__file__, os.pardir, os.pardir,
-                                         os.pardir))
+                                         os.pardir, os.pardir))
 SWIFT_REPO = os.path.join(REPO_BASE, "swift")
 LLVM_REPO = os.path.join(REPO_BASE, "llvm")
 LLVM_DATAFORMATTER_PATH = os.path.join(LLVM_REPO, "utils",
