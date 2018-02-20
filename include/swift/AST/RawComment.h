swiftlang/swift

commit d16cce3a948082a242d289263dc496e4d46ee6c8

[SourceKit] Add documentation range in structure (SR-2487) (#11264)

include/swift/AST/RawComment.h
@@ -67,6 +67,8 @@ struct RawComment {
   bool isEmpty() const {
     return Comments.empty();
   }
+
+  CharSourceRange getCharSourceRange();
 };
 
 struct CommentInfo {

lib/AST/RawComment.cpp
@@ -247,3 +247,18 @@ StringRef Decl::getBriefComment() const {
   Context.setBriefComment(this, Result);
   return Result;
 }
+
+CharSourceRange RawComment::getCharSourceRange() {
+  if (this->isEmpty()) {
+    return CharSourceRange();
+  }
+
+  auto Start = this->Comments.front().Range.getStart();
+  if (Start.isInvalid()) {
+    return CharSourceRange();
+  }
+  auto End = this->Comments.back().Range.getEnd();
+  auto Length = (char *)End.getOpaquePointerValue() -
+                (char* )Start.getOpaquePointerValue();
+  return CharSourceRange(Start, Length);
+}

include/swift/IDE/SyntaxModel.h
@@ -137,6 +137,7 @@ struct SyntaxStructureNode {
   CharSourceRange BodyRange;
   CharSourceRange NameRange;
   CharSourceRange TypeRange;
+  CharSourceRange DocRange;
   std::vector<CharSourceRange> InheritedTypeRanges;
   std::vector<SyntaxStructureElement> Elements;
 

lib/IDE/SyntaxModel.cpp
@@ -418,6 +418,12 @@ CharSourceRange parameterNameRangeOfCallArg(const TupleExpr *TE,
   return CharSourceRange();
 }
 
+static void setDecl(SyntaxStructureNode &N, Decl *D) {
+  N.Dcl = D;
+  N.Attrs = D->getAttrs();
+  N.DocRange = D->getRawComment().getCharSourceRange();
+}
+
 } // anonymous namespace
 
 bool SyntaxModelContext::walk(SyntaxModelWalker &Walker) {
@@ -753,7 +759,7 @@ bool ModelASTWalker::walkToDeclPre(Decl *D) {
     } else {
       // Pass Function / Method structure node.
       SyntaxStructureNode SN;
-      SN.Dcl = D;
+      setDecl(SN, D);
       const DeclContext *DC = AFD->getDeclContext();
       if (DC->isTypeContext()) {
         if (FD && FD->isStatic()) {
@@ -772,12 +778,11 @@ bool ModelASTWalker::walkToDeclPre(Decl *D) {
                                                      AFD->getBodySourceRange());
       SN.NameRange = charSourceRangeFromSourceRange(SM,
                           AFD->getSignatureSourceRange());
-      SN.Attrs = AFD->getAttrs();
       pushStructureNode(SN, AFD);
     }
   } else if (auto *NTD = dyn_cast<NominalTypeDecl>(D)) {
     SyntaxStructureNode SN;
-    SN.Dcl = D;
+    setDecl(SN, D);
     SN.Kind = syntaxStructureKindFromNominalTypeDecl(NTD);
     SN.Range = charSourceRangeFromSourceRange(SM, NTD->getSourceRange());
     SN.BodyRange = innerCharSourceRangeFromSourceRange(SM, NTD->getBraces());
@@ -792,12 +797,11 @@ bool ModelASTWalker::walkToDeclPre(Decl *D) {
       SN.Elements.emplace_back(SyntaxStructureElementKind::TypeRef, TR);
     }
 
-    SN.Attrs = NTD->getAttrs();
     pushStructureNode(SN, NTD);
 
   } else if (auto *ED = dyn_cast<ExtensionDecl>(D)) {
     SyntaxStructureNode SN;
-    SN.Dcl = D;
+    setDecl(SN, D);
     SN.Kind = SyntaxStructureKind::Extension;
     SN.Range = charSourceRangeFromSourceRange(SM, ED->getSourceRange());
     SN.BodyRange = innerCharSourceRangeFromSourceRange(SM, ED->getBraces());
@@ -811,7 +815,6 @@ bool ModelASTWalker::walkToDeclPre(Decl *D) {
       SN.Elements.emplace_back(SyntaxStructureElementKind::TypeRef, TR);
     }
 
-    SN.Attrs = ED->getAttrs();
     pushStructureNode(SN, ED);
 
   } else if (auto *PD = dyn_cast<ParamDecl>(D)) {
@@ -832,7 +835,7 @@ bool ModelASTWalker::walkToDeclPre(Decl *D) {
     const DeclContext *DC = VD->getDeclContext();
     if (DC->isTypeContext() || DC->isModuleScopeContext()) {
       SyntaxStructureNode SN;
-      SN.Dcl = D;
+      setDecl(SN, D);
       SourceRange SR;
       if (auto *PBD = VD->getParentPatternBinding())
         SR = PBD->getSourceRange();
@@ -863,7 +866,6 @@ bool ModelASTWalker::walkToDeclPre(Decl *D) {
       } else {
         SN.Kind = SyntaxStructureKind::GlobalVariable;
       }
-      SN.Attrs = VD->getAttrs();
       pushStructureNode(SN, VD);
     }
 
@@ -915,7 +917,7 @@ bool ModelASTWalker::walkToDeclPre(Decl *D) {
 
   } else if (auto *EnumCaseD = dyn_cast<EnumCaseDecl>(D)) {
     SyntaxStructureNode SN;
-    SN.Dcl = D;
+    setDecl(SN, D);
     SN.Kind = SyntaxStructureKind::EnumCase;
     SN.Range = charSourceRangeFromSourceRange(SM, D->getSourceRange());
 
@@ -939,7 +941,7 @@ bool ModelASTWalker::walkToDeclPre(Decl *D) {
         if (EnumElemD->getName().empty())
           continue;
         SyntaxStructureNode SN;
-        SN.Dcl = EnumElemD;
+        setDecl(SN, EnumElemD);
         SN.Kind = SyntaxStructureKind::EnumElement;
         SN.Range = charSourceRangeFromSourceRange(SM,
                                                   EnumElemD->getSourceRange());
@@ -956,23 +958,21 @@ bool ModelASTWalker::walkToDeclPre(Decl *D) {
     }
   } else if (auto *TypeAliasD = dyn_cast<TypeAliasDecl>(D)) {
     SyntaxStructureNode SN;
-    SN.Dcl = TypeAliasD;
+    setDecl(SN, D);
     SN.Kind = SyntaxStructureKind::TypeAlias;
     SN.Range = charSourceRangeFromSourceRange(SM,
                                               TypeAliasD->getSourceRange());
     SN.NameRange = CharSourceRange(TypeAliasD->getNameLoc(),
                                    TypeAliasD->getName().getLength());
-    SN.Attrs = TypeAliasD->getAttrs();
     pushStructureNode(SN, TypeAliasD);
   } else if (auto *SubscriptD = dyn_cast<SubscriptDecl>(D)) {
     SyntaxStructureNode SN;
-    SN.Dcl = SubscriptD;
+    setDecl(SN, D);
     SN.Kind = SyntaxStructureKind::Subscript;
     SN.Range = charSourceRangeFromSourceRange(SM,
                                               SubscriptD->getSourceRange());
     SN.BodyRange = innerCharSourceRangeFromSourceRange(SM,
                                                SubscriptD->getBracesRange());
-    SN.Attrs = SubscriptD->getAttrs();
     pushStructureNode(SN, SubscriptD);
   }
 

test/SourceKit/InterfaceGen/gen_clang_module.swift.response
@@ -4116,6 +4116,8 @@ public class FooOverlayClassDerived : Foo.FooOverlayClassBase {
     key.namelength: 8,
     key.bodyoffset: 237,
     key.bodylength: 106,
+    key.docoffset: 157,
+    key.doclength: 26,
     key.inheritedtypes: [
       {
         key.name: "RawRepresentable"
@@ -4199,7 +4201,9 @@ public class FooOverlayClassDerived : Foo.FooOverlayClassBase {
     key.length: 31,
     key.typename: "FooEnum1",
     key.nameoffset: 384,
-    key.namelength: 9
+    key.namelength: 9,
+    key.docoffset: 346,
+    key.doclength: 27
   },
   {
     key.kind: source.lang.swift.decl.struct,
@@ -4423,6 +4427,8 @@ public class FooOverlayClassDerived : Foo.FooOverlayClassBase {
     key.namelength: 19,
     key.bodyoffset: 970,
     key.bodylength: 133,
+    key.docoffset: 894,
+    key.doclength: 37,
     key.inheritedtypes: [
       {
         key.name: "Int"
@@ -4502,6 +4508,8 @@ public class FooOverlayClassDerived : Foo.FooOverlayClassBase {
     key.namelength: 17,
     key.bodyoffset: 1186,
     key.bodylength: 210,
+    key.docoffset: 1106,
+    key.doclength: 35,
     key.inheritedtypes: [
       {
         key.name: "OptionSet"
@@ -4803,7 +4811,9 @@ public class FooOverlayClassDerived : Foo.FooOverlayClassBase {
     key.offset: 1974,
     key.length: 29,
     key.nameoffset: 1984,
-    key.namelength: 11
+    key.namelength: 11,
+    key.docoffset: 1938,
+    key.doclength: 29
   },
   {
     key.kind: source.lang.swift.decl.var.global,
@@ -4814,7 +4824,9 @@ public class FooOverlayClassDerived : Foo.FooOverlayClassBase {
     key.length: 20,
     key.typename: "Int32",
     key.nameoffset: 2043,
-    key.namelength: 9
+    key.namelength: 9,
+    key.docoffset: 2005,
+    key.doclength: 27
   },
   {
     key.kind: source.lang.swift.decl.function.free,
@@ -4824,6 +4836,8 @@ public class FooOverlayClassDerived : Foo.FooOverlayClassBase {
     key.length: 34,
     key.nameoffset: 2099,
     key.namelength: 20,
+    key.docoffset: 2061,
+    key.doclength: 26,
     key.substructure: [
       {
         key.kind: source.lang.swift.decl.var.parameter,
@@ -4967,7 +4981,9 @@ public class FooOverlayClassDerived : Foo.FooOverlayClassBase {
     key.offset: 2627,
     key.length: 26,
     key.nameoffset: 2632,
-    key.namelength: 21
+    key.namelength: 21,
+    key.docoffset: 2557,
+    key.doclength: 62
   },
   {
     key.kind: source.lang.swift.decl.function.free,
@@ -4985,7 +5001,9 @@ public class FooOverlayClassDerived : Foo.FooOverlayClassBase {
     key.offset: 2800,
     key.length: 26,
     key.nameoffset: 2805,
-    key.namelength: 21
+    key.namelength: 21,
+    key.docoffset: 2733,
+    key.doclength: 59
   },
   {
     key.kind: source.lang.swift.decl.function.free,
@@ -4994,7 +5012,9 @@ public class FooOverlayClassDerived : Foo.FooOverlayClassBase {
     key.offset: 2888,
     key.length: 26,
     key.nameoffset: 2893,
-    key.namelength: 21
+    key.namelength: 21,
+    key.docoffset: 2828,
+    key.doclength: 53
   },
   {
     key.kind: source.lang.swift.decl.function.free,
@@ -5003,7 +5023,9 @@ public class FooOverlayClassDerived : Foo.FooOverlayClassBase {
     key.offset: 2982,
     key.length: 26,
     key.nameoffset: 2987,
-    key.namelength: 21
+    key.namelength: 21,
+    key.docoffset: 2916,
+    key.doclength: 59
   },
   {
     key.kind: source.lang.swift.decl.function.free,
@@ -5013,6 +5035,8 @@ public class FooOverlayClassDerived : Foo.FooOverlayClassBase {
     key.length: 58,
     key.nameoffset: 3072,
     key.namelength: 44,
+    key.docoffset: 3010,
+    key.doclength: 50,
     key.substructure: [
       {
         key.kind: source.lang.swift.decl.var.parameter,
@@ -5036,6 +5060,8 @@ public class FooOverlayClassDerived : Foo.FooOverlayClassBase {
     key.namelength: 15,
     key.bodyoffset: 3193,
     key.bodylength: 545,
+    key.docoffset: 3127,
+    key.doclength: 33,
     key.substructure: [
       {
         key.kind: source.lang.swift.decl.function.method.instance,
@@ -5044,7 +5070,9 @@ public class FooOverlayClassDerived : Foo.FooOverlayClassBase {
         key.offset: 3258,
         key.length: 19,
         key.nameoffset: 3263,
-        key.namelength: 14
+        key.namelength: 14,
+        key.docoffset: 3204,
+        key.doclength: 43
       },
       {
         key.kind: source.lang.swift.decl.function.method.instance,
@@ -5053,7 +5081,9 @@ public class FooOverlayClassDerived : Foo.FooOverlayClassBase {
         key.offset: 3363,
         key.length: 40,
         key.nameoffset: 3368,
-        key.namelength: 35
+        key.namelength: 35,
+        key.docoffset: 3288,
+        key.doclength: 64
       },
       {
         key.kind: source.lang.swift.decl.function.method.instance,
@@ -5062,7 +5092,9 @@ public class FooOverlayClassDerived : Foo.FooOverlayClassBase {
         key.offset: 3503,
         key.length: 40,
         key.nameoffset: 3508,
-        key.namelength: 35
+        key.namelength: 35,
+        key.docoffset: 3414,
+        key.doclength: 77
       },
       {
         key.kind: source.lang.swift.decl.function.method.static,
@@ -5243,6 +5275,8 @@ public class FooOverlayClassDerived : Foo.FooOverlayClassBase {
     key.namelength: 15,
     key.bodyoffset: 4192,
     key.bodylength: 422,
+    key.docoffset: 4096,
+    key.doclength: 33,
     key.inheritedtypes: [
       {
         key.name: "FooClassBase"

test/SourceKit/InterfaceGen/gen_header.swift.response
@@ -222,7 +222,9 @@ public protocol SameNameProtocol {
     key.nameoffset: 179,
     key.namelength: 8,
     key.bodyoffset: 189,
-    key.bodylength: 1
+    key.bodylength: 1,
+    key.docoffset: 106,
+    key.doclength: 62
   },
   {
     key.kind: source.lang.swift.decl.protocol,

test/SourceKit/InterfaceGen/gen_swift_source.swift.response
@@ -390,6 +390,8 @@ internal enum Colors {
     key.namelength: 19,
     key.bodyoffset: 200,
     key.bodylength: 51,
+    key.docoffset: 139,
+    key.doclength: 27,
     key.substructure: [
       {
         key.kind: source.lang.swift.decl.function.method.instance,
@@ -498,7 +500,9 @@ internal enum Colors {
         key.length: 13,
         key.typename: "Int",
         key.nameoffset: 512,
-        key.namelength: 4
+        key.namelength: 4,
+        key.docoffset: 473,
+        key.doclength: 22
       },
       {
         key.kind: source.lang.swift.decl.var.instance,
@@ -509,7 +513,9 @@ internal enum Colors {
         key.length: 24,
         key.typename: "(Int, Int)",
         key.nameoffset: 564,
-        key.namelength: 2
+        key.namelength: 2,
+        key.docoffset: 527,
+        key.doclength: 19
       }
     ]
   },

tools/SourceKit/include/SourceKit/Core/LangSupport.h
@@ -211,6 +211,8 @@ public:
                                          unsigned NameLength,
                                          unsigned BodyOffset,
                                          unsigned BodyLength,
+                                         unsigned DocOffset,
+                                         unsigned DocLength,
                                          StringRef DisplayName,
                                          StringRef TypeName,
                                          StringRef RuntimeName,

commit dd9b2098d5f371ef0ec59a6088e86fa8785df70d

[AST] Retrive EndLine after StartStartLineAndColumn for SingleRawComment
llvm::SourceMgr caches the line and column of the last query. It
usually scans from that position for subsequent queries. However, if the
query is for a position ahead of the last query, it re-scan from the top
of the whole buffer. This significally slows down the performance.

This change effectively mitigate performance regression introduced in
https://github.com/apple/swift/pull/11264 without functional changes.

include/swift/AST/RawComment.h
@@ -32,7 +32,7 @@ struct SingleRawComment {
   unsigned Kind : 8;
   unsigned StartColumn : 16;
   unsigned StartLine;
-  const unsigned EndLine;
+  unsigned EndLine;
 
   SingleRawComment(CharSourceRange Range, const SourceManager &SourceMgr);
   SingleRawComment(StringRef RawText, unsigned StartColumn);

lib/AST/RawComment.cpp
@@ -57,11 +57,11 @@ static SingleRawComment::CommentKind getCommentKind(StringRef Comment) {
 SingleRawComment::SingleRawComment(CharSourceRange Range,
                                    const SourceManager &SourceMgr)
     : Range(Range), RawText(SourceMgr.extractText(Range)),
-      Kind(static_cast<unsigned>(getCommentKind(RawText))),
-      EndLine(SourceMgr.getLineNumber(Range.getEnd())) {
+      Kind(static_cast<unsigned>(getCommentKind(RawText))) {
   auto StartLineAndColumn = SourceMgr.getLineAndColumn(Range.getStart());
   StartLine = StartLineAndColumn.first;
   StartColumn = StartLineAndColumn.second;
+  EndLine = SourceMgr.getLineNumber(Range.getEnd());
 }
 
 SingleRawComment::SingleRawComment(StringRef RawText, unsigned StartColumn)