d68ba606c8da8391b0d03ede925421d7490d38ac
hongm <hongm@google.com>
2018-02-21 00:30:54 -0500

    Bug fixes and performance improvements.
    PiperOrigin-RevId: 186409458

--- stdlib/ctensorflow_init.cpp
+++ stdlib/ctensorflow_init.cpp
@@ -2,11 +2,13 @@
 
 #include "tensorflow/core/platform/init_main.h"
 
+#include <assert.h>
 #include <vector>
 
 extern "C" {
 
-void InitTensorFlowRuntime(unsigned char enable_debug_logging) {
+void InitTensorFlowRuntime(unsigned char enable_debug_logging,
+                           int verbose_level) {
   // Synthesize argc and argv
   char arg0[] = "dummyProgramName";
   std::vector<char*> my_argv;
@@ -16,6 +18,12 @@ void InitTensorFlowRuntime(unsigned char enable_debug_logging) {
   char arg1[] = "--alsologtostderr";
   if (enable_debug_logging > 0) {
     my_argv.push_back(&arg1[0]);
+  }
+  char arg2[] = "--v=?";
+  if (verbose_level > 0) {
+    assert(verbose_level <= 4);
+    arg2[4] = verbose_level + '0';
+    my_argv.push_back(&arg2[0]);
   }
   int my_argc = my_argv.size();
   char** tmpArgv = my_argv.data();
