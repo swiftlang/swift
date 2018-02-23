3f232e753bfe65284b66b8320020145fe41af035
Rintaro Ishizaki <rishizaki@apple.com>
2018-02-23 11:32:39 +0900

    [SourceKit] Add defensive guard for invalid offset
    Invalid offset used to cause a inifinite loop in Lexer in some race
    condition.
    
    This is a quick fix until we fix underlying problem in SourceKit.

---
+++ test/SourceKit/CursorInfo/invalid_offset.swift
@@ -0,0 +1,12 @@
+let a = 12
+
+// rdar://problem/30346106
+// Invalid offset should trigger a crash.
+
+// RUN: %sourcekitd-test \
+// RUN:   -req=open %s -- %s == \
+// RUN:   -req=edit -async -offset=0 -length=200 -replace='' %s -- %s == \
+// RUN:   -req=cursor -offset=250 %s -- %s \
+// RUN: | %FileCheck %s
+
+// CHECK: <empty cursor info>
