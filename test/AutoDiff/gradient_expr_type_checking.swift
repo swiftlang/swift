7e25be3789de347e62fef5be2a6c9774566b02ca
danielzheng <danielzheng@google.com>
2018-03-03 23:12:22 -0500

    Improve sema for #gradient expression. - Differentiation arguments must conform to FloatingPoint. (to be generalized) - #gradient expressions with generic primal must have explicit, convertible contextual type.
    Diagnostics can be vastly improved, but I'm still figuring out how to leverage the constraint system naturally.
    The goal in this CL is just to ensure *valid expressions definitely pass sema and invalid ones definitely fail*.
    
    PiperOrigin-RevId: 187759414

--- test/AutoDiff/gradient_expr_type_checking.swift
+++ test/AutoDiff/gradient_expr_type_checking.swift
@@ -13,6 +13,7 @@ let _: (Float, Float) -> (Float, Float) = #gradient(of: foo, withRespectTo: .0, 
 let _: (Float, Float) -> (Float, Float) = #gradient(of: foo, withRespectTo: .0, .1) // ok
 let _: (Float, Float) -> (Float, Float, Float) = #gradient(of: foo, withRespectTo: .0, .1, .2) // expected-error {{the argument index is out of bounds for type}}
 
+// S is not a valid differentiation argument because it does not conform to FloatingPoint.
 struct S {
   func a(_ x: Float) -> Float {}
 
@@ -20,7 +21,7 @@ struct S {
 
   func b() {
     let _: (Float) -> Float = #gradient(of: a) // ok
-    let _: (Float) -> S = #gradient(of: a, withRespectTo: self) // ok
+    let _: (Float) -> S = #gradient(of: a, withRespectTo: self) // expected-error {{type of gradient expression could not be resolved}}
   }
 
   static func c(_ x: Float) -> Float {}
@@ -30,9 +31,16 @@ struct S {
   }
 }
 
-func e<T>(_ x: T) -> T {}
-
 let s = S()
 let _: (Float) -> Float = #gradient(of: s.a)
-// TODO: Uncomment when we support generics.
-// let _: (Float) -> Float = #gradient(of: e)
+let _: (Double) -> Double = #gradient(of: s.a) // expected-error {{type of gradient expression could not be resolved}}
+
+// Gradient expressions with generic primal.
+func e<T>(_ x: T) -> T {}
+
+let _: (Float) -> Float = #gradient(of: e)
+let _: (Double) -> Double = #gradient(of: e)
+let _ = #gradient(of: e) // expected-error {{cannot infer type for generic gradient expression without a contextual type}}
+let _: ((Float, Float)) -> ((Float, Float)) = #gradient(of: e) // expected-error {{type of gradient expression could not be resolved}}
+let _: (Int) -> (Int) = #gradient(of: e) // expected-error {{type of gradient expression could not be resolved}}
+let _: (Float) -> Double = #gradient(of: e) // expected-error {{type of gradient expression could not be resolved}}
