17330077171bceb26b45a46f57989808750b0cfc
rxwei <rxwei@google.com>
2018-03-05 05:42:56 -0500

    Introduce RandomState API. This includes
    - Introduce class RandomState, implemented using the BSD random generator. Users can initialze a state using
      a seed and pass that to various Tensor initializers to get reproducible pesudorandom tensors.
    - Introduce struct RandomNumberSequence, a non-terminating sequence that can be iterated over. It can also be
      initialized with a random seed.
    
    PiperOrigin-RevId: 187841503

--- stdlib/public/TensorFlow/Utilities.swift
+++ stdlib/public/TensorFlow/Utilities.swift
@@ -14,10 +14,10 @@
 //
 //===----------------------------------------------------------------------===//
 
-#if os(Linux) || os(FreeBSD)
-  import Glibc
+#if os(macOS) || os(iOS) || os(watchOS) || os(tvOS)
+import Darwin
 #else
-  import Darwin
+import Glibc
 #endif
 import CTensorFlow
 
@@ -115,8 +115,7 @@ func logToStderr(_ message: String) {
 @_versioned
 func debugLog(_ message: @autoclosure () -> String,
               file: StaticString = #file,
-              line: UInt = #line
-) {
+              line: UInt = #line) {
   if _RuntimeConfig.printsDebugLog {
     print("[\(file):\(line)] \(message())")
     // This helps dump more log before a crash.
@@ -124,3 +123,129 @@ func debugLog(_ message: @autoclosure () -> String,
   }
 }
 
+//===----------------------------------------------------------------------===//
+// Pseudorandom number generation
+//===----------------------------------------------------------------------===//
+
+#if os(macOS) || os(iOS) || os(watchOS) || os(tvOS)
+/// The BSD random number function.
+///
+/// - Returns: A random 32-bit integer.
+/// - Note:
+///   - On Darwin platforms, this function is hidden to encourage the use of
+///     arc4random for cryptographically secure random numbers. However,
+///     reproduciblity through seeding is required by our users.
+///   - Although the corresponding C function returns `long int`, aka `Int`, its
+///     maximum is `Int32.max`. Therefore it is safe and recommended to cast it
+///     to `Int32` without truncation.
+///
+@_silgen_name("random")
+private func random() -> Int
+#endif
+
+open class RandomState {
+  /// The global pseudorandom state.
+  public static let global = RandomState(seed: UInt32(time(nil)),
+                                         bufferSize: 256)
+
+  /// The start address of a memory buffer that stores the internal state
+  /// information.
+  private var bufferAddress: UnsafeMutablePointer<Int8>
+  /// The size of the state buffer.
+  private var bufferSize: Int
+
+  deinit {
+    bufferAddress.deinitialize(count: bufferSize)
+    bufferAddress.deallocate()
+  }
+
+  /// Create a pseudorandom state with the given seed in a buffer of the given
+  /// size.
+  ///
+  /// - Parameters:
+  ///   - seed: A number used to initialize the pseudorandom state.
+  ///   - bufferSize: The size of the buffer used to hold the internal state
+  ///     information. It is initialized based on `seed`. `size` must be
+  ///     between 8 and 256, and should be a power of two.
+  ///
+  fileprivate init(seed: UInt32, bufferSize: Int = 64) {
+    self.bufferAddress = .allocate(capacity: bufferSize)
+    self.bufferSize = bufferSize
+    self.seed(with: seed)
+  }
+
+  /// Create a pseudorandom state by copying from another pseudorandom state.
+  ///
+  /// - Parameter other: The pseudorandom state to copy from.
+  ///
+  public init(_ other: RandomState) {
+    bufferSize = other.bufferSize
+    bufferAddress = UnsafeMutablePointer<Int8>.allocate(capacity: bufferSize)
+    bufferAddress.initialize(from: other.bufferAddress, count: bufferSize)
+  }
+
+  /// Create a pseudorandom state with the given seed.
+  ///
+  /// - Parameter seed: A number used to initialize the pseudorandom state.
+  ///
+  public convenience init(seed: UInt32) {
+    self.init(seed: seed, bufferSize: 64)
+  }
+
+  /// Seed the pseudorandom state.
+  ///
+  /// - Parameter seed: The seed to be used to initialize the internal
+  ///   psuedorandom state.
+  /// - Note: Users should not re-seed the `global` state unless there is a
+  ///   compelling reason to do so.
+  ///
+  open func seed(with seed: UInt32) {
+    initstate(seed, bufferAddress, bufferSize)
+  }
+
+  /// Returns the next pseudorandom number and updates the internal state.
+  ///
+  /// - Returns: The next pseudorandom number.
+  ///
+  open func generate() -> Int32 {
+    let previousState = setstate(bufferAddress)
+    defer { setstate(previousState!) }
+    return Int32(random())
+  }
+
+  /// Returns the next few pseudorandom numbers and updates the internal state.
+  ///
+  /// - Parameter count: The number of pseudonumbers to generate.
+  /// - Returns: The next pseudorandom number.
+  ///
+  open func generate(_ count: Int) -> [Int32] {
+    let previousState = setstate(bufferAddress)
+    defer { setstate(previousState!) }
+    return (0..<count).map { _ in Int32(random()) }
+  }
+}
+
+/// A sequence of pseudorandom numbers.
+public struct RandomNumberSequence : Sequence {
+  public let seed: UInt32?
+
+  /// Creates a sequence of pseudorandom numbers. If a seed is given, a random
+  /// state will be created and initialized based on the seed value. Otherwise
+  /// the global pseudorandom state, i.e. `RandomState.global`, will be used and
+  /// updated. Use a seed if the use case requires the random sequence to be
+  /// reproducible.
+  ///
+  /// - Parameter seed: The seed value that the random sequence is being
+  ///   generated based on.
+  ///
+  public init(seed: UInt32? = nil) {
+    self.seed = seed
+  }
+
+  public func makeIterator() -> AnyIterator<Int32> {
+    return AnyIterator {
+      let state = self.seed.flatMap(RandomState.init) ?? .global
+      return state.generate()
+    }
+  }
+}
