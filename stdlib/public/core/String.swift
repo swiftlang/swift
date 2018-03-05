c356fb45b5d1d99c9c793ce32ae0c7bc3f2ae92d
Paul Hudson <paul.hudson@gmail.com>
2018-03-05 10:44:28 +0000

    [Gardening] Changed lowercaseString to lowercased() in comments.

--- stdlib/public/core/String.swift
+++ stdlib/public/core/String.swift
@@ -1337,7 +1337,7 @@ extension String {
       var guts = _guts
       guts.withMutableASCIIStorage(unusedCapacity: 0) { storage in
         for i in 0..<storage._value.count {
-          // See the comment above in lowercaseString.
+          // See the comment above in lowercased.
           let value = storage._value.start[i]
           let isLower =
             _asciiLowerCaseTable &>>
