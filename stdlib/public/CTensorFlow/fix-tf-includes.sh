04c10d1f66caac4ca3d6ef10404dfa36dea7ab31
Googler <noreply@google.com>
2018-03-07 12:55:20 -0500

    Fix toolchain builds.
    With this change, the full toolchain build, including codesigning executes properly.  Command line:
    
    SWIFT_PACKAGE=tensorflow_osx HOST_CXX_HEADERS_DIR=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/c++/ ./swift/utils/build-toolchain local.swift
    
    * Modulemap includes don't support sub-directories.
    Fix this by rewriting the TF include files during build time.
    
    * Copy header files to the target directory to allow for toolchain support and `swift` repl.
    
    * Add TF library includes to the lldb xcode project.
    
    The swift toolchain build doesn't support building LLDB via CMake, so we instead need to adjust the xcode project directly.
    
    PiperOrigin-RevId: 188190653

---
+++ stdlib/public/CTensorFlow/fix-tf-includes.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -x
+# Copies $1 to $2, adjusting the #include
+# lines to be compatible with modulemaps (removing tensorflow/...)
+
+src=$1
+dst=$2
+
+mkdir -p $(dirname $2)
+cp $1 $2
+sed -i -e 's#include "tensorflow/c/c_api.h"#include "c_api.h:#g' $2
+sed -i -e 's#include "tensorflow/c/c_api_experimental.h"#include "c_api_experimental.h"#g' $2
+sed -i -e 's#include "tensorflow/c/eager/c_api.h"#include "c_api_eager.h"#g' $2
