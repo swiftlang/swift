c22c96b9c5af50ce9d8e6cda557cec91631c0a65
Joe Shajrawi <joeshajrawi@iMac-2.local>
2018-03-07 16:45:17 -0800

    [Devirtualize] Support devirtualization of witness methods when we have a polymorphic type and no substitutions required
    SILGen is generating code with “shadow” requirements:
    convention(witness_method: ApplyRegStruct) <τ_0_0 where τ_0_0 == Type> (@inout Array<Type>) -> () {
    // no use of τ_0_0 inside
    
    In this case we have an empty substitution map but a polymorphic function type
    
    We should relax the assert when creating apply instructions to support this case.

--- lib/SIL/SILFunctionType.cpp
+++ lib/SIL/SILFunctionType.cpp
@@ -2438,7 +2438,9 @@ CanSILFunctionType
 SILFunctionType::substGenericArgs(SILModule &silModule,
                                   SubstitutionList subs) {
   if (subs.empty()) {
-    assert(!isPolymorphic() && "no args for polymorphic substitution");
+    assert(
+        (!isPolymorphic() || getGenericSignature()->areAllParamsConcrete()) &&
+        "no args for non-concrete polymorphic substitution");
     return CanSILFunctionType(this);
   }
 
