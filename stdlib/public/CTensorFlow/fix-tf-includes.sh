05afa556362993203ffdbcf2a6a4fb460a07bead
danielzheng <danielzheng@google.com>
2018-03-07 21:12:51 -0500

    Patch CTensorFlow header rewrite script.
    Fix missing quotation mark typo.
    
    PiperOrigin-RevId: 188266364

--- stdlib/public/CTensorFlow/fix-tf-includes.sh
+++ stdlib/public/CTensorFlow/fix-tf-includes.sh
@@ -9,6 +9,6 @@ dst=$2
 
 mkdir -p $(dirname $2)
 cp $1 $2
-sed -i -e 's#include "tensorflow/c/c_api.h"#include "c_api.h:#g' $2
+sed -i -e 's#include "tensorflow/c/c_api.h"#include "c_api.h":#g' $2
 sed -i -e 's#include "tensorflow/c/c_api_experimental.h"#include "c_api_experimental.h"#g' $2
 sed -i -e 's#include "tensorflow/c/eager/c_api.h"#include "c_api_eager.h"#g' $2
