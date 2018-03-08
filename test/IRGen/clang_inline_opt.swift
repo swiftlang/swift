c75887d4b18ca4cef351ea89cd54f3a8e0b5d784
asowani <sowania@us.ibm.com>
2018-03-08 10:05:33 +0530

    ppc64le port.

--- test/IRGen/clang_inline_opt.swift
+++ test/IRGen/clang_inline_opt.swift
@@ -1,4 +1,5 @@
 // RUN: %target-swift-frontend -import-objc-header %S/Inputs/c_functions.h -primary-file %s -O -emit-ir -disable-llvm-optzns | %FileCheck %s
+// UNSUPPORTED: CPU=powerpc64le
 
 func return10() -> UInt32 {
 	return return7() + 3
@@ -7,7 +8,7 @@ func return10() -> UInt32 {
 // Sanity check that we tell Clang to generate optimizable code when
 // we're optimizing.
 
-// CHECK: define internal {{(zeroext)?}} i32 @return7() [[CLANG_ATTRS:#[0-9]+]] {
+// CHECK: define internal i32 @return7() [[CLANG_ATTRS:#[0-9]+]] {
 
 // CHECK: attributes [[CLANG_ATTRS]] = {
 // CHECK-NOT: noinline
