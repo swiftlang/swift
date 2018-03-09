45cdec4007295487cd99dd13cf4ce728f01100aa
Tim Kientzle <tkientzle@apple.com>
2018-03-09 13:50:03 -0800

    Benchmark {Float,Double,Float80}.description
    This just tests how fast we can format the standard
    floating-point types.  It also includes a test that
    uses the result, to ensure that future optimized
    _creation_ of the string doesn't incidentally pessimize
    _use_ of the results.

--- benchmark/utils/main.swift
+++ benchmark/utils/main.swift
@@ -62,6 +62,7 @@ import ErrorHandling
 import Exclusivity
 import ExistentialPerformance
 import Fibonacci
+import FloatingPointPrinting
 import Hanoi
 import Hash
 import HashQuadratic
@@ -211,6 +212,7 @@ registerBenchmark(ErrorHandling)
 registerBenchmark(Exclusivity)
 registerBenchmark(ExistentialPerformance)
 registerBenchmark(Fibonacci)
+registerBenchmark(FloatingPointPrinting)
 registerBenchmark(Hanoi)
 registerBenchmark(HashTest)
 registerBenchmark(HashQuadratic)
