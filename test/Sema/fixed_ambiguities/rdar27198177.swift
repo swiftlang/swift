fa3f351747b26329039e92c29b1a73bfeaca2fd7
Tellow Krinkle <tellowkrinkle@gmail.com>
2018-03-13 23:16:01 -0500

    Revert "Unbroke tests"
    This reverts commit 5da0b079e19e3857bc85abedd281c54a1928dd79.

--- test/Sema/fixed_ambiguities/rdar27198177.swift
+++ test/Sema/fixed_ambiguities/rdar27198177.swift
@@ -2,5 +2,5 @@
 
 let arr = ["A", "B", "C"]
 let lazy: LazyMapCollection = arr.lazy.map { $0 }
-// CHECK: function_ref @$Ss17LazyMapCollectionV6filterys0a7CompactbC0Vyxq_GSbq_cF : $@convention(method) <τ_0_0, τ_0_1 where τ_0_0 : Collection> (@owned @callee_guaranteed (@in τ_0_1) -> Bool, @in_guaranteed LazyMapCollection<τ_0_0, τ_0_1>) -> @out LazyCompactMapCollection<τ_0_0, τ_0_1>
+// CHECK: function_ref @$Ss22LazyCollectionProtocolPsE6filterys0a6FilterB0Vy8ElementsQzGSb7ElementQzcF : $@convention(method) <τ_0_0 where τ_0_0 : LazyCollectionProtocol> (@owned @callee_guaranteed (@in τ_0_0.Element) -> Bool, @in_guaranteed τ_0_0) -> @out LazyFilterCollection<τ_0_0.Elements>
 _ = lazy.filter { $0 > "A" }.count
