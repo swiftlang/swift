0c11e1a0a3dff4a79f782e7d8a997cf080cf5d10
Arnold Schwaighofer <aschwaighofer@apple.com>
2018-03-14 10:04:52 -0700

    Mark swift sections as swift4 for Swift 4.2
    rdar://36363251

--- stdlib/public/runtime/SwiftRT-ELF.cpp
+++ stdlib/public/runtime/SwiftRT-ELF.cpp
@@ -24,14 +24,14 @@
   __attribute__((__visibility__("hidden"))) extern const char __stop_##name;
 
 extern "C" {
-DECLARE_SWIFT_SECTION(swift5_protocols)
-DECLARE_SWIFT_SECTION(swift5_protocol_conformances)
-DECLARE_SWIFT_SECTION(swift5_type_metadata)
+DECLARE_SWIFT_SECTION(swift4_protocols)
+DECLARE_SWIFT_SECTION(swift4_protocol_conformances)
+DECLARE_SWIFT_SECTION(swift4_type_metadata)
 
-DECLARE_SWIFT_SECTION(swift5_typeref)
-DECLARE_SWIFT_SECTION(swift5_reflstr)
-DECLARE_SWIFT_SECTION(swift5_fieldmd)
-DECLARE_SWIFT_SECTION(swift5_assocty)
+DECLARE_SWIFT_SECTION(swift4_typeref)
+DECLARE_SWIFT_SECTION(swift4_reflstr)
+DECLARE_SWIFT_SECTION(swift4_fieldmd)
+DECLARE_SWIFT_SECTION(swift4_assocty)
 }
 
 #undef DECLARE_SWIFT_SECTION
@@ -53,14 +53,14 @@ static void swift_image_constructor() {
       nullptr,
       nullptr,
 
-      SWIFT_SECTION_RANGE(swift5_protocols),
-      SWIFT_SECTION_RANGE(swift5_protocol_conformances),
-      SWIFT_SECTION_RANGE(swift5_type_metadata),
+      SWIFT_SECTION_RANGE(swift4_protocols),
+      SWIFT_SECTION_RANGE(swift4_protocol_conformances),
+      SWIFT_SECTION_RANGE(swift4_type_metadata),
 
-      SWIFT_SECTION_RANGE(swift5_typeref),
-      SWIFT_SECTION_RANGE(swift5_reflstr),
-      SWIFT_SECTION_RANGE(swift5_fieldmd),
-      SWIFT_SECTION_RANGE(swift5_assocty),
+      SWIFT_SECTION_RANGE(swift4_typeref),
+      SWIFT_SECTION_RANGE(swift4_reflstr),
+      SWIFT_SECTION_RANGE(swift4_fieldmd),
+      SWIFT_SECTION_RANGE(swift4_assocty),
   };
 
 #undef SWIFT_SECTION_RANGE
