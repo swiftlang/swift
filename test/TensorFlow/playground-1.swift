521308b11fe2227481c804d0226314e47d93b085
clattner <clattner@google.com>
2018-03-16 02:04:29 -0400

    Add a testcase that explores how the playground transform interacts with the tensorflow passes.  This tests the recent LLDB logger changes to not log tensor values.  This also exposes that my previous approach of "forcibly flattening" functions into playground top level code is not going to work: it generates exponential code blowup, by inlining the playground logger calls, array gunk, tons of unicode and other unpleastant things.
    This needs to be fixed properly, but in the meantime we can just avoid using
    functions that refer to variables defined at the top level.  With this change,
    tensorflow should be more usable within Xcode playgrounds.
    
    PiperOrigin-RevId: 189298574

---
+++ test/TensorFlow/playground-1.swift
@@ -0,0 +1,47 @@
+// Test that the playground transform + tensor operations play well together,
+// that we are able to promote things to graph properly, that the playground
+// logger gets the right stuff, and we're not generating implicit sends
+// unexpectedly.
+
+// RUN: %empty-directory(%t)
+// RUN: cp %s %t/main.swift
+// RUN: %target-swift-frontend -debugger-support -dump-ast -playground %t/main.swift %S/PlaygroundsRuntime.swift
+// RUN: %target-swift-frontend -debugger-support -Xllvm -tf-dump-intermediates -O -emit-sil -playground %t/main.swift %S/PlaygroundsRuntime.swift
+// RUN: %target-swift-frontend -debugger-support -Xllvm -tf-dump-intermediates -O -emit-sil -playground %t/main.swift %S/PlaygroundsRuntime.swift -verify | FileCheck %s
+
+import TensorFlow
+
+// Check that logging is happening for simple things.
+let x = 12345678  // a distinctive number to filecheck for.
+
+// CHECK: [[INT:%.*]] = integer_literal $Builtin.Int64, 12345678
+// CHECK: [[INT2:%.*]] = struct $Int ([[INT]] : $Builtin.Int64)
+// CHECK: store [[INT2]] to [[INTP:%.*]] : $*Int
+// CHECK: [[INT:%.*]] = load [[INTP]] : $*Int
+// CHECK: [[INTP:%.*]] = alloc_stack $Int
+// CHECK: store [[INT]] to [[INTP]] : $*Int
+// CHECK: [[LOGFN:%.*]] = function_ref @{{.*}}$builtin_log_with_id{{.*}}
+// CHECK: apply [[LOGFN]]<Int>([[INTP]],
+
+
+let a = Tensor<Float>([1,2,3])
+var b = a+a
+b -= a
+
+
+func someLocalFunctionThatShouldBeForceInlined() {
+  b *= a
+}
+
+
+// FIXME: Enable this when we handle function calls in playgrounds correctly.
+//someLocalFunctionThatShouldBeForceInlined()
+
+// CHECK-LABEL: --- TFPartition Accelerator Result: main
+// CHECK: sil private @main.tf_partition : $@callee_owned ()
+// CHECK:   builtin "__tfop_Const,
+// CHECK:   builtin "__tfop_Add,
+// CHECK:   builtin "__tfop_Sub,
+// CHECK:   return
+
+// CHECK-LABEL: --- XLA CFG Canonicalize: main
