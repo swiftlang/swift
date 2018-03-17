f1676d3b88aa3e6787a59882659b6e1fb0ecd886
Xi Ge <xi_ge@apple.com>
2018-03-16 19:22:42 -0700

    [test] Avoid creating new tmp directory for syntax round-trip testing. rdar://38567449

--- test/Syntax/round_trip_parse_gen.swift
+++ test/Syntax/round_trip_parse_gen.swift
@@ -1,15 +1,13 @@
+// RUN: rm -rf %t
 // RUN: %swift-syntax-test -input-source-filename %s -parse-gen > %t
 // RUN: diff -u %s %t
 // RUN: %swift-syntax-test -input-source-filename %s -parse-gen -print-node-kind > %t.withkinds
 // RUN: diff -u %S/Outputs/round_trip_parse_gen.swift.withkinds %t.withkinds
 // RUN: %swift-syntax-test -input-source-filename %s -eof > %t
 // RUN: diff -u %s %t
-// RUN: %empty-directory(%t)
-// RUN: %swift-syntax-test -serialize-raw-tree -input-source-filename %s > %t/serialized.json
-// RUN: %swift-syntax-test -deserialize-raw-tree -input-source-filename %t/serialized.json -output-filename %t/output.swift
-// RUN: diff -u %s %t/output.swift
-
-// REQUIRES: rdar38567449
+// RUN: %swift-syntax-test -serialize-raw-tree -input-source-filename %s > %t.dump
+// RUN: %swift-syntax-test -deserialize-raw-tree -input-source-filename %t.dump -output-filename %t
+// RUN: diff -u %s %t
 
 import ABC
 import A.B.C
