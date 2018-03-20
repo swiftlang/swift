e00eaba75e5e2a6549dc5ebedfc8c3da220c4d63
David Ungar <dungar@apple.com>
2018-03-20 11:41:34 -0700

    Add more unicode characters to one test, and add a new test for escaping them in the supplementary output map.

--- test/Driver/batch_mode_with_supplementary_filelist_execution.swift
+++ test/Driver/batch_mode_with_supplementary_filelist_execution.swift
@@ -1,12 +1,12 @@
 // REQUIRES: executable_test
 // RUN: %empty-directory(%t)
 // RUN: echo 'print("Hello, World!")' >%t/main.swift
-// RUN: touch %t/file-01.swift %t/file-02.swift %t/Ωfile-03.swift
+// RUN: touch %t/𝔼-file-01.swift %t/😂-file-02.swift %t/Ω-file-03.swift
 //
-// Ensure that the supplementary output filelist argument is passed to the frontend:
+// Ensure that the supplementary output filelist argument is passed to the frontend.
+// Also use some characters outside the BMP.
 //
-// RUN: %target-build-swift -save-temps -enable-batch-mode -driver-use-filelists -j2 %t/main.swift  %t/file-01.swift %t/file-02.swift %t/Ωfile-03.swift  -o %t/a.out 2> %t/shouldBeEmpty
-// Check for errors such as "Unrecognized escape code".
+// RUN: %target-build-swift -save-temps -emit-dependencies -emit-loaded-module-trace -emit-loaded-module-trace-path %t/lmt -serialize-diagnostics -driver-use-filelists -j2 %t/main.swift  %t/𝔼-file-01.swift %t/😂-file-02.swift %t/Ω-file-03.swift  -o %t/a.out 2> %t/shouldBeEmpty
 // RUN: test -z "`cat %t/shouldBeEmpty`"
 // RUN: %target-run %t/a.out | %FileCheck %s -check-prefix=CHECK-HELLO-WORLD
 // CHECK-HELLO-WORLD: Hello, World!
