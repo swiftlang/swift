6a8056a4ad4fca09c833c4d8655c139df474235d
rxwei <rxwei@google.com>
2018-03-20 18:24:01 -0400

    Update SILGen to generate the correct SIL argument indices for autodiff_reverse declarations.
    PiperOrigin-RevId: 189822144

--- test/AutoDiff/gradient_expr_silgen.swift
+++ test/AutoDiff/gradient_expr_silgen.swift
@@ -6,17 +6,29 @@ public func foo(_ x: Float) -> Float {
   return x * x
 }
 
-public func bar() {
-  let _ = #gradient(of: foo)
+/// FIXME: Test tuple flattening once Sema supports tuple differentiation arguments.
+// @_silgen_name("foo_tuple")
+// public func foo_tuple(_ x: ((Float, Float)), _ y: (Float, (Float, (Float)))) -> Float {
+//   return 0
+// }
+
+@_silgen_name("foo_indir_ret")
+public func foo_indir_ret<T>(x: Float, y: Float, t: T) -> (T, T) {
+  return (t, t)
 }
 
-public func bar2() {
-  let _ = #gradient(of: foo)
+@_silgen_name("foo_generic_diff")
+public func foo_generic_diff<T : Differentiable>(x: T, y: T) -> T {
+  return x
 }
 
-public func bar3() {
-  let _ = #gradient(of: foo, withRespectTo: .0)
-}
+let _ = #gradient(of: foo)
+
+let _ = #gradient(of: foo, withRespectTo: .0)
+
+// TODO: Uncomment these when generics are supported.
+// let _: (Float, Float) -> (Float, Float) = #gradient(of: foo_generic_diff)
+// let _: (Float, Float, [Int]) -> (Float, Float) = #gradient(of: foo_indir_ret, withRespectTo: .0, .1)
 
 // CHECK-LABEL: sil @foo__grad_all : $@convention(thin) (Float) -> Float
 // CHECK-NEXT: bb0
