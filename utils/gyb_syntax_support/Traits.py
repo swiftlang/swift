780508f01ba1d76d6bfc76b6a72d2de88f511499
ortem <ortem00@gmail.com>
2018-03-25 19:18:22 +0300

    Add ModifierList to DeclGroup trait

--- utils/gyb_syntax_support/Traits.py
+++ utils/gyb_syntax_support/Traits.py
@@ -14,6 +14,8 @@ TRAITS = [
               Child('Attributes', kind='AttributeList', is_optional=True),
               Child('AccessLevelModifier', kind='DeclModifier',
                     is_optional=True),
+              Child('Modifiers', kind='ModifierList',
+                   is_optional=True),
               Child('Members', kind='MemberDeclBlock'),
           ]),
 
