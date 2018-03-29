ddb559de0d18e8b86e47e616d12729a8f76bc8e6
danielzheng <danielzheng@google.com>
2018-03-29 16:54:39 -0400

    Clean up CMake for Python/TensorFlow. - Copy Python 2.7 headers into toolchain. This is a step towards making packages more distributable. - Rename SwiftCPython module to CPython. - Add FindTensorFlow CMake module.
    This is part 1/2 of creating fully-bundled toolchain installers.
    TODO: Copy Python/TensorFlow libraries into toolchain. Edit installer script.
    PiperOrigin-RevId: 190981007

---
+++ cmake/modules/FindTensorFlow.cmake
@@ -0,0 +1,19 @@
+# SWIFT_ENABLE_TENSORFLOW
+# Find TensorFlow.
+
+include(FindPackageHandleStandardArgs)
+
+find_path(TF_INCLUDE_DIR
+  NAMES tensorflow/c
+  HINTS ${SWIFT_TENSORFLOW_TARGET_INCLUDE_DIR} /usr/include /usr/local/include)
+
+find_library(TF_LIBRARY
+  NAMES tensorflow
+  HINTS ${SWIFT_TENSORFLOW_TARGET_LIB_DIR} /usr/lib /usr/local/lib)
+find_library(TF_FRAMEWORK_LIBRARY
+  NAMES tensorflow_framework
+  HINTS ${SWIFT_TENSORFLOW_TARGET_LIB_DIR} /usr/lib /usr/local/lib)
+set(TF_LIBRARIES ${TF_LIBRARY} ${TF_FRAMEWORK_LIBRARY})
+
+find_package_handle_standard_args(TensorFlow DEFAULT_MSG TF_INCLUDE_DIR TF_LIBRARIES)
+mark_as_advanced(${TF_INCLUDE_DIR} ${TF_LIBRARIES})
