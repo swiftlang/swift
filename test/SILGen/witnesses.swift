32f2ff85d664e3b4d714bfe635436f7e2827e348
Arnold Schwaighofer <aschwaighofer@apple.com>
2018-03-30 13:32:38 -0700

    Update tests for branch

--- test/SILGen/witnesses.swift
+++ test/SILGen/witnesses.swift
@@ -519,8 +519,11 @@ protocol EscapingReq {
 
 // CHECK-LABEL: sil private [transparent] [thunk] @$S9witnesses18EscapingCovarianceVAA0B3ReqA2aDP1fyyS2icFTW : $@convention(witness_method: EscapingReq) (@owned @callee_guaranteed (Int) -> Int, @in_guaranteed EscapingCovariance) -> ()
 // CHECK-NOT: return
-// CHECK: [[COPIED:%.*]] = copy_value %0
-// CHECK: convert_escape_to_noescape [[COPIED]]
+// CHECK-NOT: copy_value
+// CHECK: convert_escape_to_noescape %0
+// CHECK-NOT: copy_value
+// CHECK: destroy_value %0
+// CHECK: return
 struct EscapingCovariance: EscapingReq {
   func f(_: (Int) -> Int) { }
 }
--- test/SILOptimizer/definite-init-convert-to-escape.swift
+++ test/SILOptimizer/definite-init-convert-to-escape.swift
@@ -8,25 +8,36 @@ import Foundation
 // Make sure that we keep the escaping closures alive accross the ultimate call.
 // CHECK-LABEL: sil @$S1A19bridgeNoescapeBlock5optFn0D3Fn2yySSSgcSg_AFtF
 // CHECK: bb0
-// CHECK:  retain_value %0
+// CHECK: alloc_stack
+// CHECK: alloc_stack
 // CHECK:  retain_value %0
 // CHECK: bb2
+// CHECK:  destroy_addr
+// CHECK:  enum
+// CHECK:  store
 // CHECK:  convert_escape_to_noescape %
-// CHECK:  strong_release
+// CHECK-NOT:  strong_release
 // CHECK: bb6
 // CHECK:  retain_value %1
-// CHECK:  retain_value %1
 // CHECK: bb8
+// CHECK:  destroy_addr
+// CHECK:  enum
+// CHECK:  store
 // CHECK:  convert_escape_to_noescape %
-// CHECK:  strong_release
+// CHECK-NOT:  strong_release
+// CHECK: br
 // CHECK: bb12
 // CHECK:  [[F:%.*]] = function_ref @noescapeBlock3
 // CHECK:  apply [[F]]
 // CHECK:  release_value {{.*}} : $Optional<NSString>
-// CHECK:  release_value %1 : $Optional<@callee_guaranteed (@guaranteed Optional<String>) -> ()>
 // CHECK:  release_value {{.*}} : $Optional<@convention(block) @noescape (Optional<NSString>) -> ()>
-// CHECK:  release_value %0 : $Optional<@callee_guaranteed (@guaranteed Optional<String>) -> ()>
 // CHECK:  release_value {{.*}} : $Optional<@convention(block) @noescape (Optional<NSString>)
+// CHECK:  release_value %1 : $Optional<@callee_guaranteed (@owned Optional<String>) -> ()>
+// CHECK:  release_value %0 : $Optional<@callee_guaranteed (@owned Optional<String>) -> ()>
+// CHECK:  destroy_addr {{.*}}Optional<@callee_guaranteed
+// CHECK:  dealloc_stack
+// CHECK:  destroy_addr {{.*}}Optional<@callee_guaranteed
+// CHECK:  dealloc_stack
 public func bridgeNoescapeBlock( optFn: ((String?) -> ())?, optFn2: ((String?) -> ())?) {
   noescapeBlock3(optFn, optFn2, "Foobar")
 }
@@ -39,17 +50,24 @@ public func returnOptionalEscape() -> (() ->())?
 
 // CHECK-LABEL: sil @$S1A19bridgeNoescapeBlockyyF : $@convention(thin) () -> () {
 // CHECK: bb0:
+// CHECK:  [[SLOT:%.*]] = alloc_stack $Optional<@callee_guaranteed () -> ()>
+// CHECK:  [[NONE:%.*]] = enum $Optional
+// CHECK:  store [[NONE]] to [[SLOT]]
 // CHECK:  [[V0:%.*]] = function_ref @_returnOptionalEscape
 // CHECK:  [[V1:%.*]] = apply [[V0]]
-// CHECK:  retain_value [[V1]]
-// CHECK:  switch_enum {{.*}}bb2
-// CHECK: bb2([[V2:%.*]]: $@callee_guaranteed () -> ()):
+// CHECK: bb2:
+// CHECK:  [[V2:%.*]] = unchecked_enum_data
+// CHECK:  destroy_addr [[SLOT]]
+// CHECK:  [[SOME:%.*]] = enum $Optional<@callee_guaranteed () -> ()>, #Optional.some!enumelt.1, [[V2]]
+// CHECK:  store [[SOME]] to [[SLOT]]
 // CHECK:  convert_escape_to_noescape %
-// CHECK:  strong_release [[V2]]
+// CHECK-NOT:  strong_release
+// CHECK:  br
 // CHECK: bb6({{.*}} : $Optional<@convention(block) @noescape () -> ()>)
 // CHECK:  [[F:%.*]] = function_ref @noescapeBlock
 // CHECK:  apply [[F]]({{.*}})
-// CHECK:  release_value [[V1]] : $Optional<@callee_guaranteed () -> ()>
+// CHECK:  destroy_addr [[SLOT]]
+// CHECK:  dealloc_stack  [[SLOT]]
 
 // NOPEEPHOLE-LABEL: sil @$S1A19bridgeNoescapeBlockyyF : $@convention(thin) () -> () {
 // NOPEEPHOLE: bb0:
@@ -58,8 +76,8 @@ public func returnOptionalEscape() -> (() ->())?
 // NOPEEPHOLE:  store [[NONE]] to [[SLOT]]
 // NOPEEPHOLE:  [[V0:%.*]] = function_ref @_returnOptionalEscape
 // NOPEEPHOLE:  [[V1:%.*]] = apply [[V0]]
-// NOPEEPHOLE:  switch_enum {{.*}}bb2
-// NOPEEPHOLE: bb2([[V2:%.*]]: $@callee_guaranteed () -> ()):
+// NOPEEPHOLE: bb2:
+// NOPEEPHOLE:  [[V2:%.*]] = unchecked_enum_data
 // NOPEEPHOLE:  destroy_addr [[SLOT]]
 // NOPEEPHOLE:  [[SOME:%.*]] = enum $Optional<@callee_guaranteed () -> ()>, #Optional.some!enumelt.1, [[V2]]
 // NOPEEPHOLE:  store [[SOME]] to [[SLOT]]
