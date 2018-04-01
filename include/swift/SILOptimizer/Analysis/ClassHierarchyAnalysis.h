3e0df78578f9ffad8ea69d9cd1d4b179bc713cab
Slava Pestov <spestov@apple.com>
2018-03-31 23:40:09 -0700

    Revert "SIL: Remove CHA's unused 'protocol implementations' analysis"
    This reverts commit 3e24fadf8df2cfebcc68ea7f376d013ce454973b.

--- include/swift/SILOptimizer/Analysis/ClassHierarchyAnalysis.h
+++ include/swift/SILOptimizer/Analysis/ClassHierarchyAnalysis.h
@@ -80,6 +80,10 @@ public:
     return IndirectSubclassesCache[C];
   }
 
+  const NominalTypeList &getProtocolImplementations(ProtocolDecl *P) {
+    return ProtocolImplementationsCache[P];
+  }
+
   /// Returns true if the class is inherited by another class in this module.
   bool hasKnownDirectSubclasses(ClassDecl *C) {
     return DirectSubclassesCache.count(C);
@@ -90,6 +94,11 @@ public:
   bool hasKnownIndirectSubclasses(ClassDecl *C) {
     return IndirectSubclassesCache.count(C) &&
            !IndirectSubclassesCache[C].empty();
+  }
+
+  /// Returns true if the protocol is implemented by any class in this module.
+  bool hasKnownImplementations(ProtocolDecl *C) {
+    return ProtocolImplementationsCache.count(C);
   }
 
 private:
@@ -105,6 +114,9 @@ private:
 
   /// A cache that maps a class to all of its known indirect subclasses.
   llvm::DenseMap<ClassDecl*, ClassList> IndirectSubclassesCache;
+
+  /// A cache that maps a protocol to all of its known implementations.
+  ProtocolImplementations ProtocolImplementationsCache;
 };
 
 }
