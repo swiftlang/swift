911d201d51f7c3579d9f87cfba1e554f8f4b8060
Slava Pestov <spestov@apple.com>
2018-04-01 22:06:05 -0700

    ClangImporter: Remove RegisteredExternalDecls and finishPendingActions() logic

--- lib/ClangImporter/ClangImporter.cpp
+++ lib/ClangImporter/ClangImporter.cpp
@@ -1661,7 +1661,6 @@ ClangImporter::Implementation::Implementation(ASTContext &ctx,
       nameImporter() {}
 
 ClangImporter::Implementation::~Implementation() {
-  assert(NumCurrentImportingEntities == 0);
 #ifndef NDEBUG
   SwiftContext.SourceMgr.verifyAllBuffers();
 #endif
