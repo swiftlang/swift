d1b51b1cf9216779f058a9aac8fd72b53979269e
Rintaro Ishizaki <rishizaki@apple.com>
2018-04-09 11:40:17 +0900

    [IDE] Don't emit “Convert to Trailing Closure” for trailing closure
    rdar://problem/39253640

--- test/refactoring/RefactoringKind/trailingclosure.swift
+++ test/refactoring/RefactoringKind/trailingclosure.swift
@@ -12,6 +12,10 @@ func testTrailingClosure() -> String {
   [1,2,3]
     .filter({ $0 % 2 == 0 })
     .map({ $0 + 1 })
+
+  Foo.foo { 1 }
+  Foo.bar { print(3); return 1 }
+  Foo().qux(x: 1) { 1 }
 }
 
 // RUN: %refactor -source-filename %s -pos=7:3 | %FileCheck %s -check-prefix=CHECK-TRAILING-CLOSURE
@@ -38,6 +42,10 @@ func testTrailingClosure() -> String {
 // RUN: %refactor -source-filename %s -pos=12:4 | %FileCheck %s -check-prefix=CHECK-NO-TRAILING-CLOSURE
 // RUN: %refactor -source-filename %s -pos=13:5 | %FileCheck %s -check-prefix=CHECK-TRAILING-CLOSURE
 // RUN: %refactor -source-filename %s -pos=14:5 | %FileCheck %s -check-prefix=CHECK-TRAILING-CLOSURE
+
+// RUN: %refactor -source-filename %s -pos=16:7 | %FileCheck %s -check-prefix=CHECK-NO-TRAILING-CLOSURE
+// RUN: %refactor -source-filename %s -pos=17:7 | %FileCheck %s -check-prefix=CHECK-NO-TRAILING-CLOSURE
+// RUN: %refactor -source-filename %s -pos=18:9 | %FileCheck %s -check-prefix=CHECK-NO-TRAILING-CLOSURE
 
 // CHECK-TRAILING-CLOSURE: Convert To Trailing Closure
 
