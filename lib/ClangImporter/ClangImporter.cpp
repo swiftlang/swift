8cb008481ca1c99c8025d65704ab1a160d255bb0
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2018-04-11 15:46:45 -0700

    [ClangImporter] Use appropriate link name for export_as
    rdar://problem/38269782

--- lib/ClangImporter/ClangImporter.cpp
+++ lib/ClangImporter/ClangImporter.cpp
@@ -2837,6 +2837,11 @@ void ClangModuleUnit::collectLinkLibraries(
     ModuleDecl::LinkLibraryCallback callback) const {
   if (!clangModule)
     return;
+
+  // Skip this lib name in favor of export_as name.
+  if (clangModule->UseExportAsModuleLinkName)
+    return;
+
   for (auto clangLinkLib : clangModule->LinkLibraries) {
     LibraryKind kind;
     if (clangLinkLib.IsFramework)
