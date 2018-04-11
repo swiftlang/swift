af0a1083acf6738bdb368447ced87498293efd3b
clattner <clattner@google.com>
2018-04-11 02:36:20 -0400

    Fix b/77437755: Remove a blob of code that was accidentally copied over from findPromotableMemoryFromValue that was causing us to miscompile scalar promoted builtin operators, transforming them into their LHS.   Thanks to mazare@ for tracking this down.
    This does introduce a new regression in the no-copy test where we're getting a
    copy.  This looks like the performance inliner not inlining something. Instead
    of patching this one case, I'd like to get back to working on deabstraction and
    fixing this stuff properly someday.
    
    PiperOrigin-RevId: 192407601

--- test/TensorFlow/no-copy.swift
+++ test/TensorFlow/no-copy.swift
@@ -207,6 +207,7 @@ struct Classifier {
 
 public func mnist() {
   // Training data
+  // expected-warning @+1 {{'Tensor<Float>' implicitly copied to the accelerator, use .toDevice}}
   let images = Tensor<Float>(randomNormal: [10, 784])
   let labels = Tensor<Float>(randomNormal: [10, 10])
   var classifier = Classifier()
