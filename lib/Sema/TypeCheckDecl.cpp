b3cd0507fb133fb22a4ac69d12f2a4b5e2ecc048
Doug Gregor <dgregor@apple.com>
2018-04-20 10:13:33 -0700

    [AST] Make TypeBase::adjustSuperclassMemberDeclType() more robust.
    When performing substitutions in this function, allow them to fail
    more gracefully by producing error types. Fixes SR-7364 /
    rdar://problem/39239629.

--- lib/Sema/TypeCheckDecl.cpp
+++ lib/Sema/TypeCheckDecl.cpp
@@ -5331,6 +5331,7 @@ public:
         // Check whether the types are identical.
         auto parentDeclTy = owningTy->adjustSuperclassMemberDeclType(
             parentDecl, decl, parentDecl->getInterfaceType());
+        if (parentDeclTy->hasError()) continue;
         parentDeclTy = parentDeclTy->getUnlabeledType(TC.Context);
         if (method) {
           // For methods, strip off the 'Self' type.
