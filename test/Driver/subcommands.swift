10b376e9da1042dcd82369c91dccd77cb61d082e
danielzheng <danielzheng@google.com>
2018-04-20 23:41:42 -0400

    Mark tests that hardlink/copy Swift executables as unsupported when running with "tensorflow" configuration.
    Some Driver tests copy Swift executables to different directories without also copying the Swift libraries directory.
    This fails because the dynamic linker is no longer able to find the TensorFlow libraries.
    
    Driver/static-stdlib.swift fails on Mac for some reason, I'll look into it.
    
    PiperOrigin-RevId: 193758421

--- test/Driver/subcommands.swift
+++ test/Driver/subcommands.swift
@@ -1,3 +1,6 @@
+// SWIFT_ENABLE_TENSORFLOW: This test is unsupported because moving Swift executables without the TensorFlow libraries causes dynamic linking to fail.
+// UNSUPPORTED: tensorflow
+
 // Check that 'swift' and 'swift repl' invoke the REPL.
 //
 // REQUIRES: swift_interpreter
