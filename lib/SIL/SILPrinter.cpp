033444c58fbdbb2f6a91aa9c7fcd54e2a643eef4
rxwei <rxwei@google.com>
2018-04-24 00:54:06 -0400

    [AD] Fix a crasher when the differentiation currency type is generic. Add tensor test.
    PiperOrigin-RevId: 194033776

--- lib/SIL/SILPrinter.cpp
+++ lib/SIL/SILPrinter.cpp
@@ -2695,8 +2695,9 @@ void SILModule::print(SILPrintContext &PrintCtx, ModuleDecl *M,
     for (const Decl *D : topLevelDecls) {
       if (!WholeModuleMode && !(D->getDeclContext() == AssociatedDeclContext))
           continue;
+      // SWIFT_ENABLE_TENSORFLOW
       if ((isa<ValueDecl>(D) || isa<OperatorDecl>(D) ||
-           isa<ExtensionDecl>(D)) &&
+           isa<ExtensionDecl>(D) || isa<ImportDecl>(D)) &&
           !D->isImplicit()) {
         if (isa<AccessorDecl>(D))
           continue;
