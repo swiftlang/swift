d222804680c2546ab26e516ce6a76b6583a8f6fe
Rintaro Ishizaki <rishizaki@apple.com>
2018-04-25 11:30:18 +0900

    [IDE] Use getStartLoc() for matching pattern ranges
    For Example `getLoc()` for `foo as Ty`(`IsPattern`) is at 'as'. We
    should try to resolve this at position of 'foo'.

--- lib/IDE/SwiftSourceDocInfo.cpp
+++ lib/IDE/SwiftSourceDocInfo.cpp
@@ -581,7 +581,7 @@ std::pair<bool, Pattern*> NameMatcher::walkToPatternPre(Pattern *P) {
   if (isDone() || shouldSkip(P->getSourceRange()))
     return std::make_pair(false, P);
 
-  tryResolve(ASTWalker::ParentTy(P), P->getLoc());
+  tryResolve(ASTWalker::ParentTy(P), P->getStartLoc());
   return std::make_pair(!isDone(), P);
 }
 
