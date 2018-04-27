swiftlang/swift

commit 4732be155a711d9f5e65edd93196144b094fe6fe

[Reflection] NFC: Enable building after clang r331013 (#16212)

include/swift/Reflection/TypeRef.h
@@ -42,18 +42,23 @@ enum class TypeRefKind {
 // Clang reports an error if we don't use "template"
 #if defined(__clang__) || defined(__GNUC__)
 #define DEPENDENT_TEMPLATE template
+#if __clang_major__ >= 7
+#define DEPENDENT_TEMPLATE2
+#else
+#define DEPENDENT_TEMPLATE2 template
+#endif
 #else
 #define DEPENDENT_TEMPLATE
 #endif
 
 #define FIND_OR_CREATE_TYPEREF(Allocator, TypeRefTy, ...)                      \
   auto ID = Profile(__VA_ARGS__);                                              \
-  const auto Entry = Allocator.DEPENDENT_TEMPLATE TypeRefTy##s.find(ID);       \
-  if (Entry != Allocator.DEPENDENT_TEMPLATE TypeRefTy##s.end())                \
+  const auto Entry = Allocator.DEPENDENT_TEMPLATE2 TypeRefTy##s.find(ID);      \
+  if (Entry != Allocator.DEPENDENT_TEMPLATE2 TypeRefTy##s.end())               \
     return Entry->second;                                                      \
   const auto TR =                                                              \
       Allocator.DEPENDENT_TEMPLATE makeTypeRef<TypeRefTy>(__VA_ARGS__);        \
-  Allocator.DEPENDENT_TEMPLATE TypeRefTy##s.insert({ID, TR});                  \
+  Allocator.DEPENDENT_TEMPLATE2 TypeRefTy##s.insert({ID, TR});                 \
   return TR;
 
 /// An identifier containing the unique bit pattern made up of all of the

commit 7ea49bf907b8e1baef4b4cf03b6dfa8b7ac4bad0

Fix macOS build regression after #16212

include/swift/Reflection/TypeRef.h
@@ -40,15 +40,23 @@ enum class TypeRefKind {
 
 // MSVC reports an error if we use "template"
 // Clang reports an error if we don't use "template"
-#if defined(__clang__) || defined(__GNUC__)
-#define DEPENDENT_TEMPLATE template
-#if __clang_major__ >= 7
-#define DEPENDENT_TEMPLATE2
+#if defined(__APPLE_CC__)
+#  define DEPENDENT_TEMPLATE template
+#  if __APPLE_CC__ >= 7000
+#    define DEPENDENT_TEMPLATE2
+#  else
+#    define DEPENDENT_TEMPLATE2 template
+#  endif
+#elif defined(__clang__) || defined(__GNUC__)
+#  define DEPENDENT_TEMPLATE template
+#  if __clang_major__ >= 7
+#    define DEPENDENT_TEMPLATE2
+#  else
+#    define DEPENDENT_TEMPLATE2 template
+#  endif
 #else
-#define DEPENDENT_TEMPLATE2 template
-#endif
-#else
-#define DEPENDENT_TEMPLATE
+#  define DEPENDENT_TEMPLATE template
+#  define DEPENDENT_TEMPLATE2
 #endif
 
 #define FIND_OR_CREATE_TYPEREF(Allocator, TypeRefTy, ...)                      \