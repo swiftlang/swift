5a93195949867d13850c9e40dcf39e30bddda0c4
Slava Pestov <spestov@apple.com>
2018-04-27 23:03:53 -0700

    Sema: Refactor applyUnboundGenericArguments() to take an array of Types not TypeLocs
    Most callers were using TypeLoc::withoutLoc() to wrap a Type,
    and the one place that still had TypeReprs can resolve the
    TypeReprs right there instead.

--- lib/Sema/TypeChecker.h
+++ lib/Sema/TypeChecker.h
@@ -1186,7 +1186,6 @@ public:
   /// \param loc The source location for diagnostic reporting.
   /// \param dc The context where the arguments are applied.
   /// \param genericArgs The list of generic arguments to apply to the type.
-  /// \param options The type resolution context.
   /// \param resolver The generic type resolver.
   ///
   /// \returns A BoundGenericType bound to the given arguments, or null on
@@ -1196,8 +1195,7 @@ public:
   Type applyUnboundGenericArguments(UnboundGenericType *unboundType,
                                     GenericTypeDecl *decl,
                                     SourceLoc loc, DeclContext *dc,
-                                    MutableArrayRef<TypeLoc> genericArgs,
-                                    TypeResolutionOptions options,
+                                    ArrayRef<Type> genericArgs,
                                     GenericTypeResolver *resolver,
                                     UnsatisfiedDependency *unsatisfiedDependency);
 
