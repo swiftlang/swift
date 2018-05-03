e432971a1a2b4f26470e8e96c0a5739e1c32d474
Jordan Rose <jordan_rose@apple.com>
2018-05-03 10:25:41 -0700

    Revert "Sema: Infer '@_usableFromInline' imports by looking at references from inlinable functions (#16326)"
    This reverts commit ee6e190e09013bff9c10460f0c346119532fe6bc. It's not
    sufficient to solve the problem, and the choices were to do something
    more complicated, or just take a simple brute force approach. We're
    going with the latter.

--- lib/AST/Module.cpp
+++ lib/AST/Module.cpp
@@ -1287,14 +1287,6 @@ bool SourceFile::hasTestableImport(const swift::ModuleDecl *module) const {
   });
 }
 
-void SourceFile::markUsableFromInlineImport(const ModuleDecl *module) {
-  for (auto &Import : Imports) {
-    if (Import.first.second == module) {
-      Import.second |= ImportFlags::UsableFromInline;
-    }
-  }
-}
-
 void SourceFile::clearLookupCache() {
   if (!Cache)
     return;
--- test/Serialization/Inputs/autolinking_module_inferred.swift
+++
@@ -1,7 +0,0 @@
-@_exported import autolinking_public
-import autolinking_other // inferred as @_usableFromInline
-import autolinking_private
-
-public func bfunc(x: Int = afunc()) {
-  cfunc()
-}
