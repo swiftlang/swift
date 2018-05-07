0480cc74aacbe72cd356ba143b0c7d81fb86a037
Rintaro Ishizaki <rishizaki@apple.com>
2018-05-07 16:23:36 +0900

    [Syntax] Add test case for '-verify-syntax-tree'

---
+++ test/Syntax/diagnostics_verify.swift
@@ -0,0 +1,15 @@
+// RUN: %target-typecheck-verify-swift -verify-syntax-tree
+// FIXME: Too many "unknown syntax" errors.
+
+if true {
+  try
+  // expected-error@-1 4 {{unknown expression syntax exists in the source}}
+  // expected-error@-2 2 {{unknown statement syntax exists in the source}}
+} // expected-error {{expected expression}} expected-error 2 {{unknown statement syntax exists in the source}}
+
+if false {
+  [.]
+  // expected-error@-1 {{expected identifier after '.' expression}}
+  // expected-error@-2 2 {{unknown expression syntax exists in the source}}
+  // expected-error@-3 {{unknown statement syntax exists in the source}}
+} // expected-error {{unknown statement syntax exists in the source}}
