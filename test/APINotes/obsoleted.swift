e8de4ca55d53f2db3bdd577f6a0aff246960318f
Ted Kremenek <kremenek@apple.com>
2018-05-09 11:55:58 -0700

    Swift 4.x (x < 2) names are obsoleted in 4.2, not 5.

---
+++ test/APINotes/obsoleted.swift
@@ -0,0 +1,7 @@
+// RUN: not %target-swift-frontend -typecheck -verify -I %S/Inputs/custom-modules -F %S/Inputs/custom-frameworks -swift-version 3 %s
+// RUN: %target-swift-frontend -typecheck -verify -I %S/Inputs/custom-modules -F %S/Inputs/custom-frameworks -swift-version 4.2 %s
+// RUN: %target-swift-frontend -typecheck -verify -I %S/Inputs/custom-modules -F %S/Inputs/custom-frameworks -swift-version 5 %s
+import ObsoletedAPINotesTest
+
+let _: FooID // expected-error{{'FooID' has been renamed to 'Foo_ID'}}
+let _: Foo_ID
