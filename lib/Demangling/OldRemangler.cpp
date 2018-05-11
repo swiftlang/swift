f6659435ad7e7380226f3ebfee1132e97704cc8f
Davide Italiano <ditaliano@apple.com>
2018-05-10 17:42:21 -0700

    [ASTMangler] Add support for mangling generic typealiases.
    <rdar://problem/39915946>

--- lib/Demangling/OldRemangler.cpp
+++ lib/Demangling/OldRemangler.cpp
@@ -1875,6 +1875,11 @@ void Remangler::mangleBoundGenericOtherNominalType(Node *node) {
   mangleAnyNominalType(node, ctx);
 }
 
+void Remangler::mangleBoundGenericTypeAlias(Node *node) {
+  EntityContext ctx;
+  mangleAnyNominalType(node, ctx);
+}
+
 void Remangler::mangleTypeList(Node *node) {
   mangleChildNodes(node); // all types
   Out << '_';
