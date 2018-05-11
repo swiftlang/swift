b137b088222dc836429e18be7f58a72f5231e267
Slava Pestov <spestov@apple.com>
2018-05-11 15:53:16 -0700

    Sema: Type aliases referenced from @inlinable functions must be public or @usableFromInline
    This is a source breaking change, so emit a warning in Swift 4 mode,
    and an error in Swift 5 mode.

--- test/Compatibility/attr_inlinable_typealias.swift
+++ test/Compatibility/attr_inlinable_typealias.swift
@@ -1,4 +1,4 @@
-// RUN: %target-typecheck-verify-swift -swift-version 5
+// RUN: %target-typecheck-verify-swift -swift-version 4
 
 private typealias PrivateAlias = Int
 // expected-note@-1 {{type alias 'PrivateAlias' is not '@usableFromInline' or public}}
@@ -12,10 +12,10 @@ public typealias PublicAlias = Int
 
 @inlinable public func f() {
   _ = PrivateAlias.self
-  // expected-error@-1 {{type alias 'PrivateAlias' is private and cannot be referenced from an '@inlinable' function}}
+  // expected-warning@-1 {{type alias 'PrivateAlias' is private and should not be referenced from an '@inlinable' function}}
 
   _ = InternalAlias.self
-  // expected-error@-1 {{type alias 'InternalAlias' is internal and cannot be referenced from an '@inlinable' function}}
+  // expected-warning@-1 {{type alias 'InternalAlias' is internal and should not be referenced from an '@inlinable' function}}
 
   _ = UsableFromInlineAlias.self
 
