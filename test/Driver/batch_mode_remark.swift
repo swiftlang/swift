f94610b43e96912e691bf85b489bf27fcf2fc8af
David Ungar <dungar@apple.com>
2018-05-13 19:12:59 -0700

    test fixes

--- test/Driver/batch_mode_remark.swift
+++ test/Driver/batch_mode_remark.swift
@@ -1,12 +1,12 @@
 // Ensure that driver issues a remark iff in batch mode.
 //
-// RUN: %swiftc_driver -whole-module-optimization -enable-batch-mode   %S/../Inputs/empty.swift  2>&1 | %FileCheck %s
-// RUN: %swiftc_driver -whole-module-optimization -enable-batch-mode   %S/../Inputs/empty.swift  2>&1 | %FileCheck %s
-// RUN: (%swiftc_driver -enable-batch-mode        -disable-batch-mode  %S/../Inputs/empty.swift  2>&1; echo stop-empty-complaint) | %FileCheck %s
+// RUN: %swiftc_driver -whole-module-optimization -enable-batch-mode   %S/../Inputs/empty.swift -### 2>&1 | %FileCheck %s
+// RUN: %swiftc_driver                                                 %S/../Inputs/empty.swift -### 2>&1 | %FileCheck %s
+// RUN: %swiftc_driver -enable-batch-mode        -disable-batch-mode   %S/../Inputs/empty.swift -### 2>&1 | %FileCheck -allow-empty %s
 //
 // CHECK-NOT: remark: using batch mode
 //
 //
-// RUN: %swiftc_driver -enable-batch-mode  %S/../Inputs/empty.swift 2>&1 | %FileCheck %s -check-prefix USING-BATCH-MODE
+// RUN: %swiftc_driver -enable-batch-mode  %S/../Inputs/empty.swift -### 2>&1 | %FileCheck %s -check-prefix USING-BATCH-MODE
 //
 // USING-BATCH-MODE: remark: using batch mode
