62813f4cde3eaa1b4a95255f46343b79bd7c1260
Austin Belknap <dabelknap@google.com>
2018-05-14 10:16:47 -0700

    Reorganize response file tests.

--- test/Driver/response-file.swift
+++ test/Driver/response-file.swift
@@ -6,35 +6,31 @@
 // RUN: %target-build-swift -typecheck @%t.1.resp %s 2>&1 | %FileCheck %s -check-prefix=MULTIPLE
 // MULTIPLE: warning: result of call to 'abs' is unused
 
-// RUN: echo "-DTEST2A -DTEST2B" > %t.2.resp
-// RUN: echo "-DTEST2C -DTEST2D" > %t.3.resp
-// RUN: %target-build-swift -typecheck @%t.2.resp %s @%t.3.resp 2>&1 | %FileCheck %s -check-prefix=MIXED
+// RUN: echo "-DTEST2A -DTEST2B" > %t.2A.resp
+// RUN: echo "-DTEST2C -DTEST2D" > %t.2B.resp
+// RUN: %target-build-swift -typecheck @%t.2A.resp %s @%t.2B.resp 2>&1 | %FileCheck %s -check-prefix=MIXED
 // MIXED: warning: result of call to 'abs' is unused
 
-// RUN: echo "-DTEST3A" > %t.4.resp
-// RUN: echo "%s" >> %t.4.resp
-// RUN: echo "-DTEST3B" >> %t.4.resp
-// RUN: %target-build-swift -typecheck @%t.4.resp 2>&1 | %FileCheck %s -check-prefix=RESPONLY
+// RUN: echo "-DTEST3A" > %t.3.resp
+// RUN: echo "%s" >> %t.3.resp
+// RUN: echo "-DTEST3B" >> %t.3.resp
+// RUN: %target-build-swift -typecheck @%t.3.resp 2>&1 | %FileCheck %s -check-prefix=RESPONLY
 // RESPONLY: warning: result of call to 'abs' is unused
 
-// RUN: echo "-DTEST4A" > %t.5.resp
-// RUN: echo "%s" >> %t.5.resp
-// RUN: echo "@%t.5.resp" > %t.6.resp
-// RUN: echo "-DTEST4B" >> %t.6.resp
-// RUN: %target-build-swift -typecheck @%t.6.resp 2>&1 | %FileCheck %s -check-prefix=RECURSIVE
+// RUN: echo "-DTEST4A" > %t.4A.resp
+// RUN: echo "%s" >> %t.4A.resp
+// RUN: echo "@%t.4A.resp" > %t.4B.resp
+// RUN: echo "-DTEST4B" >> %t.4B.resp
+// RUN: %target-build-swift -typecheck @%t.4B.resp 2>&1 | %FileCheck %s -check-prefix=RECURSIVE
 // RECURSIVE: warning: result of call to 'abs' is unused
 
-// RUN: python -c 'for i in range(500001): print "-DTEST5_" + str(i)' > %t.7.resp
-// RUN: %target-build-swift -typecheck @%t.7.resp %s 2>&1 | %FileCheck %s -check-prefix=LONG
+// RUN: python -c 'for i in range(500001): print "-DTEST5_" + str(i)' > %t.5.resp
+// RUN: %target-build-swift -typecheck @%t.5.resp %s 2>&1 | %FileCheck %s -check-prefix=LONG
 // LONG: warning: result of call to 'abs' is unused
-
-// RUN: python -c 'for i in range(500001): print "-DTEST6_" + str(i)' > %t.8.resp
 // RUN: %empty-directory(%t/tmp)
-// RUN: env TMPDIR=%t/tmp/ %target-swiftc_driver %s @%t.8.resp -save-temps
+// RUN: env TMPDIR=%t/tmp/ %target-swiftc_driver %s @%t.5.resp -save-temps
 // RUN: ls %t/tmp/arguments-*.resp
-
-// RUN: python -c 'for i in range(500001): print "-DTEST7_" + str(i)' > %t.9.resp
-// RUN: not %target-swiftc_driver %s @%t.9.resp -Xfrontend -debug-crash-immediately 2>&1 | %FileCheck %s -check-prefix=CRASH
+// RUN: not %target-swiftc_driver %s @%t.5.resp -Xfrontend -debug-crash-immediately 2>&1 | %FileCheck %s -check-prefix=CRASH
 // CRASH: @{{[^ ]*}}arguments-{{[0-9a-zA-Z]+}}.resp
 
 #if TEST0
