6bb13b7cfae9ddb45715b2d38344d28d63daa0a1
Arnold Schwaighofer <aschwaighofer@apple.com>
2018-05-15 08:40:02 -0700

    Add codesign

--- test/multifile/multiconformanceimpls/main.swift
+++ test/multifile/multiconformanceimpls/main.swift
@@ -1,7 +1,10 @@
 // RUN: %empty-directory(%t)
 // RUN: %target-build-swift -emit-library -emit-module %S/Inputs/A.swift -o %t/libA.%target-dylib-extension -emit-module-path %t/A.swiftmodule -module-name A
+// RUN: %target-codesign %t/libA.%target-dylib-extension
 // RUN: %target-build-swift -emit-library -emit-module %S/Inputs/B.swift -o %t/libB.%target-dylib-extension -emit-module-path %t/B.swiftmodule -module-name B -I %t %t/libA.%target-dylib-extension
+// RUN: %target-codesign %t/libB.%target-dylib-extension
 // RUN: %target-build-swift -emit-library -emit-module %S/Inputs/C.swift -o %t/libC.%target-dylib-extension -emit-module-path %t/C.swiftmodule -module-name C -I %t %t/libA.%target-dylib-extension
+// RUN: %target-codesign %t/libC.%target-dylib-extension
 // RUN: %target-build-swift %s -I %t %t/libA.%target-dylib-extension %t/libB.%target-dylib-extension %t/libC.%target-dylib-extension -o %t/a.out
 // RUN: %target-run %t/a.out |  %FileCheck %s
 
