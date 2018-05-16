eaef4f2016ef85346d52766a21d70d4dfb8ae533
Arnold Schwaighofer <aschwaighofer@apple.com>
2018-05-16 10:16:33 -0700

    Fix the test case to work on the simulator

--- test/multifile/multiconformanceimpls/main.swift
+++ test/multifile/multiconformanceimpls/main.swift
@@ -1,12 +1,12 @@
 // RUN: %empty-directory(%t)
-// RUN: %target-build-swift -emit-library -emit-module %S/Inputs/A.swift -o %t/libA.%target-dylib-extension -emit-module-path %t/A.swiftmodule -module-name A
+// RUN: %target-build-swift-dylib(%t/libA.%target-dylib-extension) %S/Inputs/A.swift -emit-module -emit-module-path %t/A.swiftmodule -module-name A
 // RUN: %target-codesign %t/libA.%target-dylib-extension
-// RUN: %target-build-swift -emit-library -emit-module %S/Inputs/B.swift -o %t/libB.%target-dylib-extension -emit-module-path %t/B.swiftmodule -module-name B -I %t %t/libA.%target-dylib-extension
+// RUN: %target-build-swift-dylib(%t/libB.%target-dylib-extension) %S/Inputs/B.swift -emit-module -emit-module-path %t/B.swiftmodule -module-name B -I%t -L%t -lA
 // RUN: %target-codesign %t/libB.%target-dylib-extension
-// RUN: %target-build-swift -emit-library -emit-module %S/Inputs/C.swift -o %t/libC.%target-dylib-extension -emit-module-path %t/C.swiftmodule -module-name C -I %t %t/libA.%target-dylib-extension
+// RUN: %target-build-swift-dylib(%t/libC.%target-dylib-extension) %S/Inputs/C.swift -emit-module -emit-module-path %t/C.swiftmodule -module-name C -I%t -L%t -lA
 // RUN: %target-codesign %t/libC.%target-dylib-extension
-// RUN: %target-build-swift %s -I %t %t/libA.%target-dylib-extension %t/libB.%target-dylib-extension %t/libC.%target-dylib-extension -o %t/a.out
-// RUN: %target-run %t/a.out |  %FileCheck %s
+// RUN: %target-build-swift %s -I %t -o %t/a.out -L %t -Xlinker -rpath -Xlinker %t -lA -lB -lC
+// RUN: %target-run %t/a.out %t/libA.%target-dylib-extension %t/libB.%target-dylib-extension %t/libC.%target-dylib-extension |  %FileCheck %s
 
 import A
 import B
