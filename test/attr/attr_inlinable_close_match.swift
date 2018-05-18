f5f15c51fec088e44aae2144cc2b261441e9d1f9
Wes Wickwire <wickwirew@gmail.com>
2018-05-18 12:12:37 -0400

    [SR-7501] corrected the error message to be lowercased (#16703)
    (cherry picked from commit 3d1bfcb8d10a9b68ba8ce17449141760054a3c5d)

--- test/attr/attr_inlinable_close_match.swift
+++ test/attr/attr_inlinable_close_match.swift
@@ -1,4 +1,4 @@
 // RUN: %target-typecheck-verify-swift
 
 @inlineable public func misspelledInlinable() {}
-// expected-error@-1 {{No attribute named '@inlineable', did you mean '@inlinable'?}}{{2-12=inlinable}}
+// expected-error@-1 {{no attribute named '@inlineable'; did you mean '@inlinable'?}}{{2-12=inlinable}}
