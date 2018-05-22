db30959a9db24d8afbce40bf199249aea19b5f19
Michael Gottesman <mgottesman@apple.com>
2018-05-22 12:32:12 -0700

    [pred-memopt] Replace remaining occurances of prefix DI with PMO prefix.
    I am going to DI and predictable mem opts have been split for a long time and
    their subroutines aren't going to be joined in the future... so replace the DI
    prefixes in pred-mem-opts impl with PMO and rename DIMemoryUseCollector =>
    PMOUseCollector.
    
    Been sitting on this for a long time... just happy to get it in.

--- lib/SILOptimizer/Mandatory/PMOMemoryUseCollector.h
+++ lib/SILOptimizer/Mandatory/PMOMemoryUseCollector.h
@@ -1,4 +1,4 @@
-//===--- DIMemoryUseCollector.h - Memory use information for DI -*- C++ -*-===//
+//===- PMOMemoryUseCollector.h - Memory use information for PMO -*- C++ -*-===//
 //
 // This source file is part of the Swift.org open source project
 //
@@ -17,8 +17,8 @@
 //
 //===----------------------------------------------------------------------===//
 
-#ifndef SWIFT_SILOPTIMIZER_MANDATORY_DIMEMORYUSECOLLECTOR_H
-#define SWIFT_SILOPTIMIZER_MANDATORY_DIMEMORYUSECOLLECTOR_H
+#ifndef SWIFT_SILOPTIMIZER_MANDATORY_PMOMEMORYUSECOLLECTOR_H
+#define SWIFT_SILOPTIMIZER_MANDATORY_PMOMEMORYUSECOLLECTOR_H
 
 #include "swift/Basic/LLVM.h"
 #include "swift/SIL/SILInstruction.h"
@@ -30,7 +30,7 @@ namespace swift {
 
 class SILBuilder;
 
-/// DIMemoryObjectInfo - This struct holds information about the memory object
+/// PMOMemoryObjectInfo - This struct holds information about the memory object
 /// being analyzed that is required to correctly break it down into elements.
 ///
 /// This includes a collection of utilities for reasoning about (potentially
@@ -47,7 +47,7 @@ class SILBuilder;
 ///
 /// Derived classes have an additional field at the end that models whether or
 /// not super.init() has been called or not.
-class DIMemoryObjectInfo {
+class PMOMemoryObjectInfo {
 public:
   /// This is the instruction that represents the memory.  It is either an
   /// allocation (alloc_box, alloc_stack) or a mark_uninitialized.
@@ -55,7 +55,7 @@ public:
 
   /// This is the base type of the memory allocation.
   SILType MemorySILType;
-  
+
   /// True if the memory object being analyzed represents a 'let', which is
   /// initialize-only (reassignments are not allowed).
   bool IsLet = false;
@@ -64,9 +64,9 @@ public:
   /// tuples, this is the flattened element count.  For 'self' members in init
   /// methods, this is the local field count (+1 for derive classes).
   unsigned NumElements;
-public:
 
-  DIMemoryObjectInfo(SingleValueInstruction *MemoryInst);
+public:
+  PMOMemoryObjectInfo(SingleValueInstruction *MemoryInst);
 
   SILLocation getLoc() const { return MemoryInst->getLoc(); }
   SILFunction &getFunction() const { return *MemoryInst->getFunction(); }
@@ -74,9 +74,7 @@ public:
   /// Return the first instruction of the function containing the memory object.
   SILInstruction *getFunctionEntryPoint() const;
 
-  CanType getType() const {
-    return MemorySILType.getASTType();
-  }
+  CanType getType() const { return MemorySILType.getASTType(); }
 
   SingleValueInstruction *getAddress() const {
     if (isa<AllocStackInst>(MemoryInst))
@@ -108,32 +106,31 @@ public:
   bool isElementLetProperty(unsigned Element) const;
 };
 
-
-enum DIUseKind {
+enum PMOUseKind {
   /// The instruction is a Load.
   Load,
-  
+
   /// The instruction is either an initialization or an assignment, we don't
   /// know which.  This classification only happens with values of trivial type
   /// where the different isn't significant.
   InitOrAssign,
-  
+
   /// The instruction is an initialization of the tuple element.
   Initialization,
-  
+
   /// The instruction is an assignment, overwriting an already initialized
   /// value.
   Assign,
-  
+
   /// The instruction is a store to a member of a larger struct value.
   PartialStore,
-  
+
   /// An indirect 'inout' parameter of an Apply instruction.
   InOutUse,
-  
+
   /// An indirect 'in' parameter of an Apply instruction.
   IndirectIn,
-  
+
   /// This instruction is a general escape of the value, e.g. a call to a
   /// closure that captures it.
   Escape,
@@ -148,51 +145,52 @@ enum DIUseKind {
 
 /// This struct represents a single classified access to the memory object
 /// being analyzed, along with classification information about the access.
-struct DIMemoryUse {
+struct PMOMemoryUse {
   /// This is the instruction accessing the memory.
   SILInstruction *Inst;
-  
+
   /// This is what kind of access it is, load, store, escape, etc.
-  DIUseKind Kind;
-  
+  PMOUseKind Kind;
+
   /// For memory objects of (potentially recursive) tuple type, this keeps
   /// track of which tuple elements are affected.
   unsigned short FirstElement, NumElements;
-  
-  DIMemoryUse(SILInstruction *Inst, DIUseKind Kind, unsigned FE, unsigned NE)
-  : Inst(Inst), Kind(Kind), FirstElement(FE), NumElements(NE) {
+
+  PMOMemoryUse(SILInstruction *Inst, PMOUseKind Kind, unsigned FE, unsigned NE)
+      : Inst(Inst), Kind(Kind), FirstElement(FE), NumElements(NE) {
     assert(FE == FirstElement && NumElements == NE &&
            "more than 64K elements not supported yet");
   }
-  
-  DIMemoryUse() : Inst(nullptr) {}
-  
+
+  PMOMemoryUse() : Inst(nullptr) {}
+
   bool isInvalid() const { return Inst == nullptr; }
   bool isValid() const { return Inst != nullptr; }
 
   bool usesElement(unsigned i) const {
-    return i >= FirstElement && i < static_cast<unsigned>(FirstElement+NumElements);
+    return i >= FirstElement &&
+           i < static_cast<unsigned>(FirstElement + NumElements);
   }
-  
+
   /// onlyTouchesTrivialElements - Return true if all of the accessed elements
   /// have trivial type.
-  bool onlyTouchesTrivialElements(const DIMemoryObjectInfo &MemoryInfo) const;
-  
+  bool onlyTouchesTrivialElements(const PMOMemoryObjectInfo &MemoryInfo) const;
+
   /// getElementBitmask - Return a bitmask with the touched tuple elements
   /// set.
   APInt getElementBitmask(unsigned NumMemoryTupleElements) const {
     return APInt::getBitsSet(NumMemoryTupleElements, FirstElement,
-                             FirstElement+NumElements);
+                             FirstElement + NumElements);
   }
 };
 
-/// collectDIElementUsesFrom - Analyze all uses of the specified allocation
+/// collectPMOElementUsesFrom - Analyze all uses of the specified allocation
 /// instruction (alloc_box, alloc_stack or mark_uninitialized), classifying them
 /// and storing the information found into the Uses and Releases lists.
 LLVM_NODISCARD bool
-collectDIElementUsesFrom(const DIMemoryObjectInfo &MemoryInfo,
-                         SmallVectorImpl<DIMemoryUse> &Uses,
-                         SmallVectorImpl<SILInstruction *> &Releases);
+collectPMOElementUsesFrom(const PMOMemoryObjectInfo &MemoryInfo,
+                          SmallVectorImpl<PMOMemoryUse> &Uses,
+                          SmallVectorImpl<SILInstruction *> &Releases);
 
 } // end namespace swift
 
