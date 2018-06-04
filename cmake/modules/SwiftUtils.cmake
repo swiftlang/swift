ba6c5a6a07455c905c9632ccb8d4db7108efc6f3
Ross Bayer <rbayer@apple.com>
2018-06-04 01:29:50 -0700

    First part of the CMake Gardening Project (TM), cleaning up the SwiftUtils.cmake module to conform to proper CMake styling conventions and use the module import guard pattern (similar to header guards in C).

--- cmake/modules/SwiftUtils.cmake
+++ cmake/modules/SwiftUtils.cmake
@@ -1,53 +1,71 @@
+# Module import guard
+if(DEFINED SWIFT_UTILS_MODULE_LOADED)
+  return()
+endif()
+set(SWIFT_UTILS_MODULE_LOADED TRUE)
+
+
 include(CMakeParseArguments)
 
+
 function(precondition var)
-  cmake_parse_arguments(
-    PRECONDITION # prefix
-    "NEGATE" # options
-    "MESSAGE" # single-value args
-    "" # multi-value args
+  set(options NEGATE)
+  set(single_value_args MESSAGE)
+  set(multi_value_args)
+
+  cmake_parse_arguments(PRE
+    "${options}"
+    "${single_value_args}"
+    "${multi_value_args}"
     ${ARGN})
 
-  if (PRECONDITION_NEGATE)
-    if (${var})
-      if (PRECONDITION_MESSAGE)
-        message(FATAL_ERROR "Error! ${PRECONDITION_MESSAGE}")
+  if(PRE_NEGATE)
+    if(${var})
+      if(PRE_MESSAGE)
+        message(FATAL_ERROR "Error! ${PRE_MESSAGE}")
       else()
-        message(FATAL_ERROR "Error! Variable ${var} is true or not empty. The value of ${var} is ${${var}}.")
+        message(FATAL_ERROR
+          "Error! Variable ${var} is true or not empty. "
+          "The value of ${var} is ${${var}}.")
       endif()
     endif()
   else()
-    if (NOT ${var})
-      if (PRECONDITION_MESSAGE)
-        message(FATAL_ERROR "Error! ${PRECONDITION_MESSAGE}")
+    if(NOT ${var})
+      if(PRE_MESSAGE)
+        message(FATAL_ERROR "Error! ${PRE_MESSAGE}")
       else()
-        message(FATAL_ERROR "Error! Variable ${var} is false, empty or not set.")
+        message(FATAL_ERROR
+          "Error! Variable ${var} is false, empty or not set.")
       endif()
     endif()
   endif()
 endfunction()
 
+
 # Assert is 'NOT ${LHS} ${OP} ${RHS}' is true.
 function(precondition_binary_op OP LHS RHS)
-  cmake_parse_arguments(
-    PRECONDITIONBINOP # prefix
-    "NEGATE" # options
-    "MESSAGE" # single-value args
-    "" # multi-value args
+  set(options NEGATE)
+  set(single_value_args MESSAGE)
+  set(multi_value_args)
+
+  cmake_parse_arguments(PRE_BIN_OP
+    "${options}"
+    "${single_value_args}"
+    "${multi_value_args}"
     ${ARGN})
 
-  if (PRECONDITIONBINOP_NEGATE)
-    if (${LHS} ${OP} ${RHS})
-      if (PRECONDITIONBINOP_MESSAGE)
-        message(FATAL_ERROR "Error! ${PRECONDITIONBINOP_MESSAGE}")
+  if(PRE_BIN_OP_NEGATE)
+    if(${LHS} ${OP} ${RHS})
+      if(PRE_BIN_OP_MESSAGE)
+        message(FATAL_ERROR "Error! ${PRE_BIN_OP_MESSAGE}")
       else()
         message(FATAL_ERROR "Error! ${LHS} ${OP} ${RHS} is true!")
       endif()
     endif()
   else()
-    if (NOT ${LHS} ${OP} ${RHS})
-      if (PRECONDITIONBINOP_MESSAGE)
-        message(FATAL_ERROR "Error! ${PRECONDITIONBINOP_MESSAGE}")
+    if(NOT ${LHS} ${OP} ${RHS})
+      if(PRE_BIN_OP_MESSAGE)
+        message(FATAL_ERROR "Error! ${PRE_BIN_OP_MESSAGE}")
       else()
         message(FATAL_ERROR "Error! ${LHS} ${OP} ${RHS} is false!")
       endif()
@@ -55,21 +73,23 @@ function(precondition_binary_op OP LHS RHS)
   endif()
 endfunction()
 
+
 # Translate a yes/no variable to the presence of a given string in a
 # variable.
 #
 # Usage:
-#   translate_flag(is_set flag_name var_name)
+#   translate_flag(is_set flag_name result_var)
 #
-# If is_set is true, sets ${var_name} to ${flag_name}. Otherwise,
-# unsets ${var_name}.
-function(translate_flag is_set flag_name var_name)
+# If is_set is true, sets ${result_var} to ${flag_name}. Otherwise,
+# unsets ${result_var}.
+function(translate_flag is_set flag_name result_var)
   if(${is_set})
-    set("${var_name}" "${flag_name}" PARENT_SCOPE)
+    set(${result_var} "${flag_name}" PARENT_SCOPE)
   else()
-    set("${var_name}" "" PARENT_SCOPE)
+    set(${result_var} "" PARENT_SCOPE)
   endif()
 endfunction()
+
 
 macro(translate_flags prefix options)
   foreach(var ${options})
@@ -77,60 +97,78 @@ macro(translate_flags prefix options)
   endforeach()
 endmacro()
 
+
 # Set ${outvar} to ${${invar}}, asserting if ${invar} is not set.
 function(precondition_translate_flag invar outvar)
   precondition(${invar})
   set(${outvar} "${${invar}}" PARENT_SCOPE)
 endfunction()
 
-function(is_build_type_optimized build_type result_var_name)
+
+function(is_build_type_optimized build_type result_var)
   if("${build_type}" STREQUAL "Debug")
-    set("${result_var_name}" FALSE PARENT_SCOPE)
-  elseif("${build_type}" STREQUAL "RelWithDebInfo" OR
-         "${build_type}" STREQUAL "Release" OR
-         "${build_type}" STREQUAL "MinSizeRel")
-    set("${result_var_name}" TRUE PARENT_SCOPE)
+    set(${result_var} FALSE PARENT_SCOPE)
+  elseif("${build_type}" STREQUAL "RelWithDebInfo"
+      OR "${build_type}" STREQUAL "Release"
+      OR "${build_type}" STREQUAL "MinSizeRel")
+    set(${result_var} TRUE PARENT_SCOPE)
   else()
     message(FATAL_ERROR "Unknown build type: ${build_type}")
   endif()
 endfunction()
 
-function(is_build_type_with_debuginfo build_type result_var_name)
-  if("${build_type}" STREQUAL "Debug" OR
-     "${build_type}" STREQUAL "RelWithDebInfo")
-    set("${result_var_name}" TRUE PARENT_SCOPE)
-  elseif("${build_type}" STREQUAL "Release" OR
-         "${build_type}" STREQUAL "MinSizeRel")
-    set("${result_var_name}" FALSE PARENT_SCOPE)
+
+function(is_build_type_with_debuginfo build_type result_var)
+  if("${build_type}" STREQUAL "Debug"
+      OR "${build_type}" STREQUAL "RelWithDebInfo")
+    set(${result_var} TRUE PARENT_SCOPE)
+  elseif("${build_type}" STREQUAL "Release"
+      OR "${build_type}" STREQUAL "MinSizeRel")
+    set(${result_var} FALSE PARENT_SCOPE)
   else()
     message(FATAL_ERROR "Unknown build type: ${build_type}")
   endif()
 endfunction()
+
 
 # Set variable to value if value is not null or false. Otherwise set variable to
 # default_value.
-function(set_with_default variable value)
-  cmake_parse_argument(
-    SWD
-    ""
-    "DEFAULT"
-    "" ${ARGN})
+function(set_with_default var value)
+  set(options)
+  set(single_value_args DEFAULT)
+  set(multi_value_args)
+
+  cmake_parse_argument(SWD
+    "${options}"
+    "${single_value_args}"
+    "${multi_value_args}"
+    ${ARGN})
+
   precondition(SWD_DEFAULT
     MESSAGE "Must specify a default argument")
-  if (value)
-    set(${variable} ${value} PARENT_SCOPE)
+
+  if("${value}")
+    set(${var} "${value}" PARENT_SCOPE)
   else()
-    set(${variable} ${SWD_DEFAULT} PARENT_SCOPE)
+    set(${var} "${SWD_DEFAULT}" PARENT_SCOPE)
   endif()
 endfunction()
 
+
 function(swift_create_post_build_symlink target)
-  set(options IS_DIRECTORY)
-  set(oneValueArgs SOURCE DESTINATION WORKING_DIRECTORY COMMENT)
+  set(options
+    IS_DIRECTORY)
+  set(single_value_args
+    COMMENT
+    DESTINATION
+    SOURCE
+    WORKING_DIRECTORY)
+  set(multi_value_args)
+
   cmake_parse_arguments(CS
     "${options}"
-    "${oneValueArgs}"
-    ""
+    "${single_value_args}"
+    "${multi_value_args}"
     ${ARGN})
 
   if("${CMAKE_SYSTEM_NAME}" STREQUAL "Windows")
@@ -140,37 +178,49 @@ function(swift_create_post_build_symlink target)
       set(cmake_symlink_option "copy_if_different")
     endif()
   else()
-      set(cmake_symlink_option "create_symlink")
+    set(cmake_symlink_option "create_symlink")
   endif()
 
   add_custom_command(TARGET "${target}" POST_BUILD
     COMMAND
-      "${CMAKE_COMMAND}" "-E" "${cmake_symlink_option}"
+      "${CMAKE_COMMAND}"
+      "-E" "${cmake_symlink_option}"
       "${CS_SOURCE}"
       "${CS_DESTINATION}"
     WORKING_DIRECTORY "${CS_WORKING_DIRECTORY}"
     COMMENT "${CS_COMMENT}")
 endfunction()
 
+
 function(dump_swift_vars)
   set(SWIFT_STDLIB_GLOBAL_CMAKE_CACHE)
-  get_cmake_property(variableNames VARIABLES)
-  foreach(variableName ${variableNames})
-    if(variableName MATCHES "^SWIFT")
-      set(SWIFT_STDLIB_GLOBAL_CMAKE_CACHE "${SWIFT_STDLIB_GLOBAL_CMAKE_CACHE}set(${variableName} \"${${variableName}}\")\n")
-      message("set(${variableName} \"${${variableName}}\")")
+
+  get_cmake_property(variable_names VARIABLES)
+  foreach(variable_name ${variable_names})
+    if("${variable_name}" MATCHES "^SWIFT")
+      set(set_string "set(${variable_name} \"${${variable_name}}\")")
+
+      # FIXME: Not sure if this was originally meant to export the variables
+      # to the parent scope or not. This variable isn't used anywhere else
+      # otherwise.
+      string(CONCAT SWIFT_STDLIB_GLOBAL_CMAKE_CACHE
+        "${SWIFT_STDLIB_GLOBAL_CMAKE_CACHE}"
+        "${set_string}\n")
+
+      message("${set_string}")
     endif()
   endforeach()
 endfunction()
 
-function(is_sdk_requested name result_var_name)
-  if("${SWIFT_HOST_VARIANT_SDK}" STREQUAL "${name}")
-    set("${result_var_name}" "TRUE" PARENT_SCOPE)
+
+function(is_sdk_requested name result_var)
+  if(SWIFT_HOST_VARIANT_SDK STREQUAL "${name}")
+    set(${result_var} TRUE PARENT_SCOPE)
   else()
     if("${name}" IN_LIST SWIFT_SDKS)
-      set("${result_var_name}" "TRUE" PARENT_SCOPE)
+      set(${result_var} TRUE PARENT_SCOPE)
     else()
-      set("${result_var_name}" "FALSE" PARENT_SCOPE)
+      set(${result_var} FALSE PARENT_SCOPE)
     endif()
   endif()
 endfunction()
