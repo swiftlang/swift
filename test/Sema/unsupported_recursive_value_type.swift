b576a40428f4187931ac1f92f24b5f4ce54e4f10
fischertony <aqamoss3fan2010@gmail.com>
2018-06-05 03:22:11 +0300

    message amendments

--- test/Sema/unsupported_recursive_value_type.swift
+++ test/Sema/unsupported_recursive_value_type.swift
@@ -42,7 +42,7 @@ enum RecursiveByGenericSubstitutionEnum<T> {
   case A(T)
 }
 
-enum InconstructibleEnum1 { // expected-warning {{enum with recursive-only cases is not constructible}}
+enum InconstructibleEnum1 { // expected-warning {{enum containing only recursive cases is not constructible}}
   indirect case A(InconstructibleEnum1)
 }
 enum InconstructibleEnum2 { // OK
@@ -50,13 +50,13 @@ enum InconstructibleEnum2 { // OK
   case B(Bool)
   indirect case C(Int, InconstructibleEnum2)
 }
-enum InconstructibleEnum3 { // expected-warning {{enum with recursive-only cases is not constructible}}
+enum InconstructibleEnum3 { // expected-warning {{enum containing only recursive cases is not constructible}}
   indirect case B(Int, InconstructibleEnum3)
 }
-indirect enum InconstructibleEnum4 { // expected-warning {{enum with recursive-only cases is not constructible}}
+indirect enum InconstructibleEnum4 { // expected-warning {{enum containing only recursive cases is not constructible}}
   case A(InconstructibleEnum4)
 }
-indirect enum InconstructibleEnum5 { // expected-warning {{enum with recursive-only cases is not constructible}}
+indirect enum InconstructibleEnum5 { // expected-warning {{enum containing only recursive cases is not constructible}}
   case B(Int, InconstructibleEnum5)
 }
 indirect enum InconstructibleEnum6 { // OK
