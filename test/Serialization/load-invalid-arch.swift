4b9262e4f4ea2d02da2824cfd9b49241963dbb89
Tapan Thaker <tapanth@uber.com>
2018-06-12 10:11:19 -0700

    Remove import search path for load-invalid-arch framework test

--- test/Serialization/load-invalid-arch.swift
+++ test/Serialization/load-invalid-arch.swift
@@ -11,7 +11,7 @@
 // RUN: mkdir -p %t/new_module.framework/Modules/new_module.swiftmodule/
 // RUN: touch %t/new_module.framework/Modules/new_module.swiftmodule/powerpc64.swiftmodule
 // RUN: touch %t/new_module.framework/Modules/new_module.swiftmodule/arm64.swiftmodule
-// RUN: %target-swift-frontend %s -F %t -typecheck -I %t -verify -show-diagnostics-after-fatal
+// RUN: %target-swift-frontend %s -F %t -typecheck -verify -show-diagnostics-after-fatal
 
 import new_module // expected-error {{no such module 'new_module'}} expected-error {{could not find module 'new_module' for architecture 'x86_64' found: powerpc64, arm64}}
 
