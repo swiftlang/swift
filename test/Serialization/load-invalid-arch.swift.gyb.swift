b6eb1d5f68e49b201e3863ad3a971d575d3c2247
Tapan Thaker <tapanth@uber.com>
2018-06-12 19:50:59 -0700

    Make load-invalid-arch test pass for all target cpu

--- test/Serialization/load-invalid-arch.swift.gyb.swift
+++ test/Serialization/load-invalid-arch.swift.gyb.swift
@@ -5,14 +5,16 @@
 // RUN: touch %t/new_module.swiftmodule/powerpc64.swiftmodule
 // RUN: touch %t/new_module.swiftmodule/arm64.swiftdoc
 // RUN: touch %t/new_module.swiftmodule/powerpc64.swiftdoc
-// RUN: %target-swift-frontend %s -typecheck -I %t -verify -show-diagnostics-after-fatal
+// RUN: %gyb -DTARGET_ARCHITECTURE=%target-cpu %s -o %t/load-invalid-arc.swift
+// RUN: %target-swift-frontend %t/load-invalid-arc.swift -typecheck -I %t -verify -show-diagnostics-after-fatal
 
 // RUN: %empty-directory(%t)
 // RUN: mkdir -p %t/new_module.framework/Modules/new_module.swiftmodule/
 // RUN: touch %t/new_module.framework/Modules/new_module.swiftmodule/powerpc64.swiftmodule
 // RUN: touch %t/new_module.framework/Modules/new_module.swiftmodule/arm64.swiftmodule
-// RUN: %target-swift-frontend %s -F %t -typecheck -verify -show-diagnostics-after-fatal
+// RUN: %gyb -DTARGET_ARCHITECTURE=%target-cpu %s -o %t/load-invalid-arc.swift
+// RUN: %target-swift-frontend %t/load-invalid-arc.swift -F %t -typecheck -verify -show-diagnostics-after-fatal
 
-import new_module // expected-error {{no such module 'new_module'}} expected-error {{could not find module 'new_module' for architecture 'x86_64' found: powerpc64, arm64}}
+import new_module // expected-error {{no such module 'new_module'}} expected-error {{could not find module 'new_module' for architecture '${TARGET_ARCHITECTURE}' found: powerpc64, arm64}}
 
 new_module.foo() // expected-error {{use of unresolved identifier 'new_module'}}
