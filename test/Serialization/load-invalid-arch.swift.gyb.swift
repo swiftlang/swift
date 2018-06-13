3e938a9c2421c02e28b84611b946768dc33a7214
Tapan Thaker <tapanth@uber.com>
2018-06-13 09:59:09 -0700

    Use non-existing achitectures for the test to make it more robust

--- test/Serialization/load-invalid-arch.swift.gyb.swift
+++ test/Serialization/load-invalid-arch.swift.gyb.swift
@@ -1,20 +1,20 @@
 
 // RUN: %empty-directory(%t)
 // RUN: mkdir %t/new_module.swiftmodule
-// RUN: touch %t/new_module.swiftmodule/arm64.swiftmodule
-// RUN: touch %t/new_module.swiftmodule/powerpc64.swiftmodule
-// RUN: touch %t/new_module.swiftmodule/arm64.swiftdoc
-// RUN: touch %t/new_module.swiftmodule/powerpc64.swiftdoc
+// RUN: touch %t/new_module.swiftmodule/i387.swiftmodule
+// RUN: touch %t/new_module.swiftmodule/ppc65.swiftmodule
+// RUN: touch %t/new_module.swiftmodule/i387.swiftdoc
+// RUN: touch %t/new_module.swiftmodule/ppc65.swiftdoc
 // RUN: %gyb -DTARGET_ARCHITECTURE=%target-cpu %s -o %t/load-invalid-arc.swift
 // RUN: %target-swift-frontend %t/load-invalid-arc.swift -typecheck -I %t -verify -show-diagnostics-after-fatal
 
 // RUN: %empty-directory(%t)
 // RUN: mkdir -p %t/new_module.framework/Modules/new_module.swiftmodule/
-// RUN: touch %t/new_module.framework/Modules/new_module.swiftmodule/powerpc64.swiftmodule
-// RUN: touch %t/new_module.framework/Modules/new_module.swiftmodule/arm64.swiftmodule
+// RUN: touch %t/new_module.framework/Modules/new_module.swiftmodule/i387.swiftmodule
+// RUN: touch %t/new_module.framework/Modules/new_module.swiftmodule/ppc65.swiftmodule
 // RUN: %gyb -DTARGET_ARCHITECTURE=%target-cpu %s -o %t/load-invalid-arc.swift
 // RUN: %target-swift-frontend %t/load-invalid-arc.swift -F %t -typecheck -verify -show-diagnostics-after-fatal
 
-import new_module // expected-error {{no such module 'new_module'}} expected-error {{could not find module 'new_module' for architecture '${TARGET_ARCHITECTURE}' found: powerpc64, arm64}}
+import new_module // expected-error {{no such module 'new_module'}} expected-error {{could not find module 'new_module' for architecture '${TARGET_ARCHITECTURE}'; found: ppc65, i387}}
 
 new_module.foo() // expected-error {{use of unresolved identifier 'new_module'}}
