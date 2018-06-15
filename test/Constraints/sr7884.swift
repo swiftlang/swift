eb0d5b66e909ed0e4cdadca086125090b36b38e0
Mark Lacey <mark.lacey@apple.com>
2018-06-14 20:08:10 -0700

    Simplify a test by removing a couple extraneous expressions.

--- test/Constraints/sr7884.swift
+++ test/Constraints/sr7884.swift
@@ -1,3 +1,4 @@
+
 // RUN: %target-typecheck-verify-swift
 
 // SR-7884
@@ -13,7 +14,7 @@ let r = f(1)
 let _ = r! // expected-error {{cannot force unwrap value of non-optional type 'Int'}}
 
 // SR-7899
-let optSeq: LazySequence<[Int]>? = nil
-let lazySequence = optSeq?.lazy
-let value = lazySequence?.compactMap({ $0 as? Int }).first // expected-warning {{conditional cast from 'Int' to 'Int' always succeeds}}
-let _: Int = value!
+func testLazySequence(_ lazySequence: LazySequence<[Int]>?) {
+  let value = lazySequence?.compactMap({ $0 as? Int }).first // expected-warning {{conditional cast from 'Int' to 'Int' always succeeds}}
+  let _: Int = value!
+}
