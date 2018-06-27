71e2a014fea805d8bb4daad6a92170ad81c93c8d
Doug Gregor <dgregor@apple.com>
2018-06-27 10:43:33 -0700

    [Type checker] Make sure we get all conformances for extended nominal types.
    Index-while-building ends up querying some conformances that aren’t
    strictly needed by the type-checker, but which require us to pre-check all
    conformances for any extension we have. Long-term, this is wasted effort, but
    for now it eliminates a recent regression caused by the request-evaluator,
    which (intentionally) tries to perform less work for non-primary files.
    
    Fixes rdar://problem/41392714.

---
+++ test/Index/Store/cross-file-extension-crash.swift
@@ -0,0 +1,11 @@
+// Ensure that we don't crash due to resolving an extension in a non-primary
+// file.
+
+// XFAIL: linux
+
+// RUN: %empty-directory(%t)
+// RUN: %target-swift-frontend -index-store-path %t/idx -o %t/file.o -typecheck -primary-file %s %S/Inputs/cross-file-extension-crash-other.swift -verify
+
+extension X {
+  func foo() { }
+}
