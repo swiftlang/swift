d48fea83dd9be44e3227d97263b7db666e300826
Mark Lacey <mark.lacey@apple.com>
2018-06-26 17:13:13 -0700

    [ConstraintSystem] Use the type cache to read types in constraint generation.
    We should pretty much always be reading
    expression types out of the type cache in the constraint system.
    
    We only see key path applications here on the path where we are
    diagnosing a failure.
    
    I haven't yet managed to come up with a test case that reproduces a crash
    here.
    
    Fixes: rdar://problem/41306933

--- lib/Sema/CSGen.cpp
+++ lib/Sema/CSGen.cpp
@@ -2799,8 +2799,8 @@ namespace {
       auto locator = CS.getConstraintLocator(expr);
       auto projectedTy = CS.createTypeVariable(locator,
                                                TVO_CanBindToLValue);
-      CS.addKeyPathApplicationConstraint(expr->getKeyPath()->getType(),
-                                         expr->getBase()->getType(),
+      CS.addKeyPathApplicationConstraint(CS.getType(expr->getKeyPath()),
+                                         CS.getType(expr->getBase()),
                                          projectedTy,
                                          locator);
       return projectedTy;
