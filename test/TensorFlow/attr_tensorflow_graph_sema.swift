75028fdc258fcf01b4fbd76661a2bac12022ebe2
Richard Wei <rxwei@google.com>
2018-07-01 13:38:15 +0100

    [Sema] Reject usage of @TensorFlowGraph on generic functions.

--- test/TensorFlow/attr_tensorflow_graph_sema.swift
+++ test/TensorFlow/attr_tensorflow_graph_sema.swift
@@ -23,6 +23,10 @@ func tensors(_ x: Tensor<Float>) -> Tensor<Int32> {} // okay
 @TensorFlowGraph
 func multiRetTensors(_ x: Tensor<Int32>) -> (Tensor<Double>, Tensor<Float>) {} // okay
 
+// expected-error @+1 {{@TensorFlowGraph cannot be applied to generic functions}}
+@TensorFlowGraph
+func generic<T>(_ x: Tensor<T>) -> Tensor<T> {}
+
 // expected-error @+1 {{@TensorFlowGraph can only be applied to functions whose parameters and return values are TensorFlow values or aggregates of TensorFlow values}}
 @TensorFlowGraph
 func wrongInput(_ x: Int32) -> ResourceHandle {}
