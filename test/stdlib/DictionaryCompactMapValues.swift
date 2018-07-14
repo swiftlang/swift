d8e0c68e090207fb5b0b97695e0e598b794d1ff8
Daiki Matsudate <d.matsudate@gmail.com>
2018-07-14 16:47:06 +0900

    fix format

--- test/stdlib/DictionaryCompactMapValues.swift
+++ test/stdlib/DictionaryCompactMapValues.swift
@@ -13,11 +13,11 @@ tests.test("DefaultReturnType") {
 }
 
 tests.test("ExplicitTypeContext") {
-  expectEqual(["a":"1","c":"3"],
-    ["a":"1","b":nil,"c":"3"].compactMapValues({$0})
+  expectEqual(["a": "1", "c": "3"],
+    ["a": "1", "b": nil, "c": "3"].compactMapValues({$0})
   )
   expectEqual(["a": 1, "b": 2],
-    ["a":"1","b":"2", "c":"three"].compactMapValues(Int.init)
+    ["a": "1", "b": "2", "c": "three"].compactMapValues(Int.init)
   )
 }
 
