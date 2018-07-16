b28225403a0e5bd760c31cae379b6d3ba6a1b38c
Richard Wei <rxwei@google.com>
2018-07-16 15:50:51 -0700

    Fix formatting.

--- include/swift/SIL/SILBuilder.h
+++ include/swift/SIL/SILBuilder.h
@@ -2099,9 +2099,8 @@ private:
         unsigned NumBits = BuiltinIntTy->getWidth().getFixedWidth();
         Name += "_Int" + llvm::utostr(NumBits);
       }
-    }
-    else if (auto BuiltinFloatTy =
-                 dyn_cast<BuiltinFloatType>(OpdTy.getASTType())) {
+    } else if (auto BuiltinFloatTy =
+                   dyn_cast<BuiltinFloatType>(OpdTy.getASTType())) {
       Name += "_FP";
       switch (BuiltinFloatTy->getFPKind()) {
       case BuiltinFloatType::IEEE16: Name += "IEEE16"; break;
