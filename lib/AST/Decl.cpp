0966cd990a744176c083d47b5eb9bec77944da53
Doug Gregor <dgregor@apple.com>
2018-07-18 14:50:25 -0700

    [AST] Performance hack for AssociatedTypeDecl::getOverriddenDecls().
    The GenericSignatureBuilder is generating a *huge* number of invocations
    of AssociatedTypeDecl::getOverriddenDecls(), causing contention in
    the request-evaluator’s core data structures and a significant
    slow-down in compile-time performance when the request-evaluator is
    handling this computation. 
    
    Those data structures need to be optimized, but in the meantime, 
    introduce a performance hack to use the cached entry without going
    through the request-evaluator. This will MISS dependencies and needs
    to go away quickly.

--- lib/AST/Decl.cpp
+++ lib/AST/Decl.cpp
@@ -2894,8 +2894,17 @@ SourceRange AssociatedTypeDecl::getSourceRange() const {
 
 SmallVector<AssociatedTypeDecl *, 1>
 AssociatedTypeDecl::getOverriddenDecls() const {
+  // FIXME: Performance hack because we end up looking at the overridden
+  // declarations of an associated type a *lot*.
+  OverriddenDeclsRequest request{const_cast<AssociatedTypeDecl *>(this)};
+  SmallVector<ValueDecl *, 1> overridden;
+  if (auto cached = request.getCachedResult())
+    overridden = std::move(*cached);
+  else
+    overridden = AbstractTypeParamDecl::getOverriddenDecls();
+
   SmallVector<AssociatedTypeDecl *, 1> assocTypes;
-  for (auto decl : AbstractTypeParamDecl::getOverriddenDecls()) {
+  for (auto decl : overridden) {
     assocTypes.push_back(cast<AssociatedTypeDecl>(decl));
   }
   return assocTypes;
