ea39a73b39f254c92c66bc660d3a4cef5f265d43
Gogul Balakrishnan <bgogul@google.com>
2018-07-20 16:15:38 -0700

    Bug fixes in SESE loop canonicalization that was triggered by nested loops.
      - Avoid invalidating the use_iterator when updating use of escaping values.
      - Recompute dominator information after every loop change as it stale.

--- test/TensorFlowRuntime/sese_loop_canonicalization.swift
+++ test/TensorFlowRuntime/sese_loop_canonicalization.swift
@@ -45,4 +45,51 @@ ControlFlowTests.testAllBackends("natSumWithBreak") {
   expectEqualWithScalarTensor(5050, natSumWithBreak(200))
 }
 
+func sumOfProducts(_ M : Int32, _ N : Int32) -> Tensor<Float> {
+  // Effectively computes natSum(M)*natSum(N)
+  var sum = Tensor<Float>(0)
+  for i in 0..<M {
+    for j in 0..<N {
+      // i + 1 and j + 1 is a workaround for
+      // https://bugs.swift.org/browse/SR-8256
+      let prod = Tensor<Float>(Float((i + 1) * (j + 1)));
+      sum += prod
+    }
+  }
+  return sum
+}
+
+ControlFlowTests.testAllBackends("sumOfProducts") {
+  expectNearlyEqualWithScalarTensor(18.0, sumOfProducts(2, 3))
+  expectNearlyEqualWithScalarTensor(1980.0,sumOfProducts(10, 8))
+  expectNearlyEqualWithScalarTensor(11550.0,sumOfProducts(20, 10))
+}
+
+// A contrived example to test labeled breaks
+func sumOfProductsWithBound(_ M : Int32, _ N : Int32, _ Bound : Float) -> Tensor<Float> {
+  // Effectively computes natSum(M)*natSum(N) upto Bound
+  var sum = Tensor<Float>(0)
+outer_loop:for i in 0..<M {
+    for j in 0..<N {
+      // i + 1 and j + 1 is a workaround for
+      // https://bugs.swift.org/browse/SR-8256
+      let prod = Tensor<Float>(Float((i + 1) * (j + 1)));
+      sum += prod
+      if (sum.scalarized() >= Bound) {
+        break outer_loop;
+      }
+    }
+  }
+  return sum
+}
+
+ControlFlowTests.testAllBackends("sumOfProductsWithBound") {
+  expectNearlyEqualWithScalarTensor(6, sumOfProductsWithBound(3, 3, 4))
+  expectNearlyEqualWithScalarTensor(8, sumOfProductsWithBound(2, 3, 7))
+  expectNearlyEqualWithScalarTensor(8, sumOfProductsWithBound(3, 3, 7))
+  expectNearlyEqualWithScalarTensor(18, sumOfProductsWithBound(3, 3, 18))
+  // Effectively no bound as natSum(3) * natSum(3) is 36.
+  expectNearlyEqualWithScalarTensor(36, sumOfProductsWithBound(3, 3, 100))
+}
+
 runAllTests()
