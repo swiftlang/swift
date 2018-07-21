106878005e32abdf2e690b5904f607b45093849c
Slava Pestov <spestov@apple.com>
2018-07-21 05:16:30 -0700

    AST: Use AbstractFunctionDecl::computeType()

--- lib/AST/Decl.cpp
+++ lib/AST/Decl.cpp
@@ -3006,18 +3006,7 @@ void ClassDecl::addImplicitDestructor() {
     recordObjCMethod(DD);
 
   // Assign DD the interface type (Self) -> () -> ()
-  ArrayRef<AnyFunctionType::Param> noParams;
-  AnyFunctionType::ExtInfo info;
-  Type selfTy = selfDecl->getInterfaceType();
-  Type voidTy = TupleType::getEmpty(ctx);
-  Type funcTy = FunctionType::get(noParams, voidTy, info);
-  if (auto *sig = DD->getGenericSignature()) {
-    DD->setInterfaceType(
-      GenericFunctionType::get(sig, {selfTy}, funcTy, info));
-  } else {
-    DD->setInterfaceType(
-      FunctionType::get({selfTy}, funcTy, info));
-  }
+  DD->computeType();
 }
 
 
