9392fee535cd37b4936f3ff65ef7035dbb543c5b
Mingsheng Hong <hongm@google.com>
2018-07-31 06:36:49 -0700

    Added a unit test on tuples of tensor values.

--- test/TensorFlow/no_copy.swift
+++ test/TensorFlow/no_copy.swift
@@ -212,3 +212,11 @@ public func SR8395() {
   let x: Tensor<Float> = [[1, 2], [3, 4]]
   print(matmul(x, x) + x)
 }
+
+// Tuple `d` should be deabstracted away.
+public func noTupleExtractOverTensorValues(d: (Tensor<Float>, Tensor<Float>)) {
+  let (a, b) = (Tensor<Float>(1.0), Tensor<Float>(2.0))
+  let c = a + 5
+  let e = c + d.1
+  _hostOp(e)
+}
