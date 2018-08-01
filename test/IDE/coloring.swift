df043f739fec3ab7fc3aeac6c810711076d0e3ed
Argyrios Kyrtzidis <kyrtzidis@apple.com>
2018-07-31 21:44:30 -0700

    [test] Move syntactic coloring tests related to '#' configs and other pound-related keywords to coloring_configs.swift, NFC

--- test/IDE/coloring.swift
+++ test/IDE/coloring.swift
@@ -5,16 +5,6 @@
 // RUN: %swift-swiftsyntax-test -classify-syntax -source-file %s | %FileCheck %s --check-prefixes CHECK,CHECK-NEW
 // XFAIL: broken_std_regex
 
-#line 17 "abc.swift"
-// CHECK: <kw>#line</kw> <int>17</int> <str>"abc.swift"</str>
-
-@available(iOS 8.0, OSX 10.10, *)
-// CHECK: <attr-builtin>@available</attr-builtin>(<kw>iOS</kw> <float>8.0</float>, <kw>OSX</kw> <float>10.10</float>, *)
-func foo() {
-// CHECK: <kw>if</kw> <kw>#available</kw> (<kw>OSX</kw> <float>10.10</float>, <kw>iOS</kw> <float>8.01</float>, *) {<kw>let</kw> <kw>_</kw> = <str>"iOS"</str>}
-  if #available (OSX 10.10, iOS 8.01, *) {let _ = "iOS"}
-}
-
 enum List<T> {
   case Nil
   // rdar://21927124
@@ -299,16 +289,6 @@ func test3(o: AnyObject) {
   let x = o as! MyCls
 }
 
-// CHECK: <kw>func</kw> test4(<kw>inout</kw> a: <type>Int</type>) {{{$}}
-func test4(inout a: Int) {
-  // CHECK-OLD: <kw>if</kw> <kw>#available</kw> (<kw>OSX</kw> >= <float>10.10</float>, <kw>iOS</kw> >= <float>8.01</float>) {<kw>let</kw> OSX = <str>"iOS"</str>}}{{$}}
-  // CHECK-NEW: <kw>if</kw> <kw>#available</kw> (OSX >= <float>10.10</float>, iOS >= <float>8.01</float>) {<kw>let</kw> OSX = <str>"iOS"</str>}}{{$}}
-  if #available (OSX >= 10.10, iOS >= 8.01) {let OSX = "iOS"}}
-
-// CHECK: <kw>func</kw> test4b(a: <kw>inout</kw> <type>Int</type>) {{{$}}
-func test4b(a: inout Int) {
-}
-
 // CHECK: <kw>class</kw> MySubClass : <type>MyCls</type> {
 class MySubClass : MyCls {
     // CHECK: <attr-builtin>override</attr-builtin> <kw>func</kw> foo(x: <type>Int</type>) {}
@@ -362,35 +342,6 @@ _ = -3.1e-5
 // CHECK-OLD: <float>-3.1e-5</float>
 // CHECK-NEW: <float>3.1e-5</float>
 
-let filename = #file
-// CHECK: <kw>let</kw> filename = <kw>#file</kw>
-let line = #line
-// CHECK: <kw>let</kw> line = <kw>#line</kw>
-let column = #column
-// CHECK: <kw>let</kw> column = <kw>#column</kw>
-let function = #function
-// CHECK: <kw>let</kw> function = <kw>#function</kw>
-
-let image = #imageLiteral(resourceName: "cloud.png")
-// CHECK-OLD: <kw>let</kw> image = <object-literal>#imageLiteral(resourceName: "cloud.png")</object-literal>
-// CHECK-NEW: <kw>let</kw> image = <object-literal>#imageLiteral</object-literal>(resourceName: <str>"cloud.png"</str>)
-let file = #fileLiteral(resourceName: "cloud.png")
-// CHECK-OLD: <kw>let</kw> file = <object-literal>#fileLiteral(resourceName: "cloud.png")</object-literal>
-// CHECK-NEW: <kw>let</kw> file = <object-literal>#fileLiteral</object-literal>(resourceName: <str>"cloud.png"</str>)
-let black = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
-// CHECK-OLD: <kw>let</kw> black = <object-literal>#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)</object-literal>
-// CHECK-NEW: <kw>let</kw> black = <object-literal>#colorLiteral</object-literal>(red: <int>0</int>, green: <int>0</int>, blue: <int>0</int>, alpha: <int>1</int>)
-
-let rgb = [#colorLiteral(red: 1, green: 0, blue: 0, alpha: 1),
-           #colorLiteral(red: 0, green: 1, blue: 0, alpha: 1),
-           #colorLiteral(red: 0, green: 0, blue: 1, alpha: 1)]
-// CHECK-OLD: <kw>let</kw> rgb = [<object-literal>#colorLiteral(red: 1, green: 0, blue: 0, alpha: 1)</object-literal>,
-// CHECK-OLD:                     <object-literal>#colorLiteral(red: 0, green: 1, blue: 0, alpha: 1)</object-literal>,
-// CHECK-OLD:                     <object-literal>#colorLiteral(red: 0, green: 0, blue: 1, alpha: 1)</object-literal>]
-// CHECK-NEW: <kw>let</kw> rgb = [<object-literal>#colorLiteral</object-literal>(red: <int>1</int>, green: <int>0</int>, blue: <int>0</int>, alpha: <int>1</int>),
-// CHECK-NEW:                     <object-literal>#colorLiteral</object-literal>(red: <int>0</int>, green: <int>1</int>, blue: <int>0</int>, alpha: <int>1</int>),
-// CHECK-NEW:                     <object-literal>#colorLiteral</object-literal>(red: <int>0</int>, green: <int>0</int>, blue: <int>1</int>, alpha: <int>1</int>)]
-
 "--\"\(x) --"
 // CHECK: <str>"--\"</str>\<anchor>(</anchor>x<anchor>)</anchor><str> --"</str>
 
@@ -439,7 +390,3 @@ func keywordInCaseAndLocalArgLabel(_ for: Int, for in: Int, class _: Int) {
     print(x, y)
   }
 }
-
-#if os(macOS)
-#endif
-// CHECK: <#kw>#if</#kw> <#id>os</#id>(<#id>macOS</#id>)
