12948f254ac2e1fc72cfd086ad38842b714aa44c
Mingsheng Hong <hongm@google.com>
2018-08-02 15:11:43 -0700

    Added a simple fix to reading global vars based on discussion with @lattner.
    This is done by adding a new flag to protect the code that promotes global vars
    to SSA values, as a performance optimization. It used to be required before we
    support sends/recvs.

---
+++ test/TensorFlowRuntime/top_level_2.swift
@@ -0,0 +1,26 @@
+// RUN: %target-run-simple-swift
+// REQUIRES: executable_test
+// REQUIRES: swift_test_mode_optimize
+
+// This file contains stubs for functions that the playground transform in
+// lib/Sema/PlaygroundTransform.cpp generates calls into.
+
+import TensorFlow
+import TensorFlowUnittest
+import StdlibUnittest
+
+var TopLevelTests = TestSuite("TopLevel")
+
+let g = Tensor<Float>(1.0)
+
+// This function is not inlined into main(). Confirm that it 
+func SR8405() {
+  // _hostOp(g)
+  expectNearlyEqualWithScalarTensor(1.0, g)
+}
+
+TopLevelTests.testCPUOrGPU("TopLevel") {
+  SR8405()
+}
+
+runAllTests()
