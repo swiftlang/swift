0b990a82a5fc2c8bbd1d149976aaf702a4b3a3db
Pavol Vaskovic <pali@pali.sk>
2018-08-07 22:15:52 +0200

    [benchmark] Extracted test_utils.py
    Moving the `captured_output` function to own file.
    
    Adding homegrown unit testing helper classes `Stub` and `Mock`.
    
    The issue is that the unittest.mock was added in Python 3.3 and we need to run on Python 2.7. `Stub` and `Mock` were organically developed as minimal implementations to support the common testing patterns used on the original branch, but since I’m rewriting the commit history to provide an easily digestible narrative, it makes sense to introduce them here in one step as a custom unit testing library.

--- benchmark/scripts/test_compare_perf_tests.py
+++ benchmark/scripts/test_compare_perf_tests.py
@@ -19,9 +19,6 @@ import sys
 import tempfile
 import unittest
 
-from StringIO import StringIO
-from contextlib import contextmanager
-
 from compare_perf_tests import PerformanceTestResult
 from compare_perf_tests import ReportFormatter
 from compare_perf_tests import ResultComparison
@@ -29,16 +26,7 @@ from compare_perf_tests import TestComparator
 from compare_perf_tests import main
 from compare_perf_tests import parse_args
 
-
-@contextmanager
-def captured_output():
-    new_out, new_err = StringIO(), StringIO()
-    old_out, old_err = sys.stdout, sys.stderr
-    try:
-        sys.stdout, sys.stderr = new_out, new_err
-        yield sys.stdout, sys.stderr
-    finally:
-        sys.stdout, sys.stderr = old_out, old_err
+from test_utils import captured_output
 
 
 class TestPerformanceTestResult(unittest.TestCase):
