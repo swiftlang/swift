5ebc6e77e78082dcfd3d77b8a427326367194024
Arnold Schwaighofer <aschwaighofer@apple.com>
2018-08-08 07:11:06 -0700

    Codesign the reflection tests

--- validation-test/Reflection/reflect_existential.swift
+++ validation-test/Reflection/reflect_existential.swift
@@ -1,5 +1,6 @@
 // RUN: %empty-directory(%t)
 // RUN: %target-build-swift -lswiftSwiftReflectionTest %s -o %t/reflect_existential
+// RUN: %target-codesign %t/reflect_existential
 // RUN: %target-run %target-swift-reflection-test %t/reflect_existential 2>&1 | %FileCheck %s --check-prefix=CHECK-%target-ptrsize
 // REQUIRES: objc_interop
 // REQUIRES: executable_test
