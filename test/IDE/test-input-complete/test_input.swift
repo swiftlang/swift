82be1a5d4717608a8e1c2944ebd10554cb35eaa6
Argyrios Kyrtzidis <kyrtzidis@apple.com>
2018-08-09 16:55:42 -0700

    [parser] Make sure the isInputIncomplete() function works as expected when there is an empty string interpolation segment
    Previously it was erroneously treating such invalid interpolation segment as 'incomplete',
    even though additional user input, will not 'complete' it.
    
    rdar://28498239

--- test/IDE/test-input-complete/test_input.swift
+++ test/IDE/test-input-complete/test_input.swift
@@ -30,6 +30,7 @@
 // RUN: %swift-ide-test -test-input-complete -source-filename %S/Inputs/type_incomplete2.swift | %FileCheck %s -check-prefix=INCOMPLETE
 // RUN: %swift-ide-test -test-input-complete -source-filename %S/Inputs/type_incomplete3.swift | %FileCheck %s -check-prefix=INCOMPLETE
 // RUN: %swift-ide-test -test-input-complete -source-filename %S/Inputs/type_incomplete4.swift | %FileCheck %s -check-prefix=INCOMPLETE
+// RUN: %swift-ide-test -test-input-complete -source-filename %S/Inputs/invalid_interpolation1.swift | %FileCheck %s -check-prefix=COMPLETE
 
 // INCOMPLETE: IS_INCOMPLETE
 // COMPLETE: IS_COMPLETE
