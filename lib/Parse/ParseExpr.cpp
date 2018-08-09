669c83e73092d89f389aa9fe49a08be714aa1758
Brent Royal-Gordon <brent@architechies.com>
2018-08-08 23:17:48 -0700

    Warn about strange interpolations in Swift 4.2

--- lib/Parse/ParseExpr.cpp
+++ lib/Parse/ParseExpr.cpp
@@ -1919,6 +1919,12 @@ parseStringSegments(SmallVectorImpl<Lexer::StringSegment> &Segments,
       // the label (at best), and multiple arguments would form a tuple.
       if (!Context.isSwiftVersionAtLeast(5)) {
         if (args.size() > 1) {
+          diagnose(args[1]->getLoc(), diag::string_interpolation_list_changing)
+            .highlightChars(args[1]->getLoc(), rParen);
+          diagnose(args[1]->getLoc(), diag::string_interpolation_list_insert_parens)
+            .fixItInsertAfter(lParen, "(")
+            .fixItInsert(rParen, ")");
+          
           args = {
             TupleExpr::create(Context,
                               lParen, args, argLabels, argLabelLocs, rParen,
@@ -1929,6 +1935,11 @@ parseStringSegments(SmallVectorImpl<Lexer::StringSegment> &Segments,
           argLabelLocs = { SourceLoc() };
         }
         else if (args.size() == 1 && argLabels[0] != Identifier()) {
+          diagnose(argLabelLocs[0], diag::string_interpolation_label_changing)
+            .highlightChars(argLabelLocs[0], args[0]->getStartLoc());
+          diagnose(argLabelLocs[0], diag::string_interpolation_remove_label, argLabels[0])
+            .fixItRemoveChars(argLabelLocs[0], args[0]->getStartLoc());
+          
           argLabels[0] = Identifier();
         }
       }
