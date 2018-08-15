fc2ff48b814b99cb1e92c60106d90bb40c04ee66
Harlan Haskins <harlan@apple.com>
2018-08-15 10:35:37 -0700

    Add test for invalid version string

--- test/TBD/dylib-version.swift
+++ test/TBD/dylib-version.swift
@@ -2,6 +2,7 @@
 // RUN: %target-swift-frontend -emit-ir -o /dev/null %s -tbd-current-version 2.0.3 -tbd-compatibility-version 1.7 -emit-tbd -emit-tbd-path %t/both_provided.tbd
 // RUN: %target-swift-frontend -emit-ir -o /dev/null %s -tbd-current-version 2.0 -emit-tbd -emit-tbd-path %t/only_current_provided.tbd
 // RUN: %target-swift-frontend -emit-ir -o /dev/null %s -tbd-compatibility-version 2 -emit-tbd -emit-tbd-path %t/only_compat_provided.tbd
+// RUN: not %target-swift-frontend -emit-ir -o /dev/null %s -tbd-compatibility-version not_a_version_string -emit-tbd -emit-tbd-path /dev/null 2>&1 | %FileCheck %s --check-prefix BOGUS
 
 // RUN: %FileCheck %s --check-prefix BOTH < %t/both_provided.tbd
 // RUN: %FileCheck %s --check-prefix CURRENT < %t/only_current_provided.tbd
@@ -21,3 +22,5 @@
 // Same as above -- current version defaults to 1 and is not present in
 // emitted TBD file if it's 1.
 // COMPAT-NOT: current-version: 1
+
+// BOGUS: version component contains non-numeric characters
