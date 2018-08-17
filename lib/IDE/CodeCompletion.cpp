2f260f1000a6a613f329a41479ad3d5b612dd82c
Brent Royal-Gordon <brent@architechies.com>
2018-08-16 18:48:15 -0700

    Fix issues from rebasing

--- lib/IDE/CodeCompletion.cpp
+++ lib/IDE/CodeCompletion.cpp
@@ -5042,7 +5042,8 @@ class CodeCompletionTypeContextAnalyzer {
   ExprParentFinder Finder;
 
 public:
-  CodeCompletionTypeContextAnalyzer(DeclContext *DC, Expr *ParsedExpr) : DC(DC),
+  CodeCompletionTypeContextAnalyzer(DeclContext *DC, Expr *ParsedExpr,
+                                    bool AncestorsWithinBrace = false) : DC(DC),
     ParsedExpr(ParsedExpr), SM(DC->getASTContext().SourceMgr),
     Context(DC->getASTContext()),
     Finder(ParsedExpr,  [](ASTWalker::ParentTy Node) {
@@ -5084,7 +5085,7 @@ public:
         }
       } else
         return false;
-    }, /*AncestorsWithinBrace=*/true) {}
+    }, /*AncestorsWithinBrace=*/AncestorsWithinBrace) {}
 
   void analyzeExpr(Expr *Parent, llvm::function_ref<void(Type)> Callback,
                    SmallVectorImpl<StringRef> &PossibleNames) {
@@ -5521,8 +5522,9 @@ void CodeCompletionCallbacksImpl::doneParsing() {
   case CompletionKind::UnresolvedMember: {
     Lookup.setHaveDot(DotLoc);
     SmallVector<Type, 2> PossibleTypes;
-    ::CodeCompletionTypeContextAnalyzer TypeAnalyzer(CurDeclContext,
-                                                     CodeCompleteTokenExpr);
+    ::CodeCompletionTypeContextAnalyzer
+        TypeAnalyzer(CurDeclContext, CodeCompleteTokenExpr,
+                     /*AncestorsWithinBrace=*/true);
     if (TypeAnalyzer.Analyze(PossibleTypes))
       Lookup.setExpectedTypes(PossibleTypes);
     Lookup.getUnresolvedMemberCompletions(PossibleTypes);
