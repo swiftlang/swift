ccbc3f7dcc8bb41f3ab588b3167f1052875b4095
Daniel Martín <mardani29@yahoo.es>
2018-08-26 15:40:17 +0200

    Add some tests for #if compiler

---
+++ test/Parse/ConditionalCompilation/compiler.swift
@@ -0,0 +1,29 @@
+// RUN: %target-typecheck-verify-swift
+
+#if !compiler(>=4.1)
+ // There should be no error here.
+ foo bar
+#else
+ let _: Int = 1
+#endif
+
+#if compiler(<4.1)
+ // There should be no error here.
+ foo bar
+#else
+ let _: Int = 1
+#endif
+
+#if (compiler(>=4.1))
+ let _: Int = 1
+#else
+ // There should be no error here.
+ foo bar
+#endif
+
+#if !compiler(<4.1)
+ let _: Int = 1
+#else
+ // There should be no error here.
+ foo bar
+#endif
