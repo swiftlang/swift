e7903f95c292f299f83805b22ce6044b109db0b4
Graydon Hoare <ghoare@apple.com>
2018-08-28 14:00:19 -0700

    [benchmark] Compensate for missing header in some SDKs.

--- benchmark/utils/LibProc/LibProcIncludeSystemHeader.h
+++ benchmark/utils/LibProc/LibProcIncludeSystemHeader.h
@@ -10,6 +10,13 @@
 //
 //===----------------------------------------------------------------------===//
 
-// This file exists to include the following not-yet-modularized system header:
+// This file exists to include the not-yet-modularized libproc.h system header.
 
+#if __has_include(<libproc.h>)
 #include <libproc.h>
+#else
+#include <Availability.h>
+#include <sys/resource.h>
+// Some SDKs are missing the libproc.h header, despite this symbol being present.
+int proc_pid_rusage(int pid, int flavor, rusage_info_t *buffer) __OSX_AVAILABLE_STARTING(__MAC_10_9, __IPHONE_7_0);
+#endif
