427fe53b21021fa7878fbd1e8a6d88865c230db9
Davide Italiano <ditaliano@apple.com>
2018-08-28 08:41:31 -0700

    [LoadableByAddress] Relax test, the IR produced might be platform dependent.

--- test/SILOptimizer/di-loadable-by-addr-scope.swift
+++ test/SILOptimizer/di-loadable-by-addr-scope.swift
@@ -2,7 +2,7 @@
 // RUN: %target-swift-frontend -emit-ir %s -Onone -sil-verify-all \
 // RUN:   -Xllvm -sil-print-debuginfo -o - 2>&1 | %FileCheck %s
 
-// CHECK: define hidden swiftcc %Ts15_AnySequenceBoxC* @"$S4main18DependencyResolverC14resolveSubtree_9excludings11AnySequenceVyAA20VersionAssignmentSetVy9ContainerQzGGAK_SDyAJ_10IdentifierQZShyAA0I0VGGtF"(%swift.opaque* noalias nocapture, i64, %T4main18DependencyResolverC* swiftself)
+// CHECK: define hidden swiftcc {{.*}} @"$S4main18DependencyResolverC14resolveSubtree_9excludings11AnySequenceVyAA20VersionAssignmentSetVy9ContainerQzGGAK_SDyAJ_10IdentifierQZShyAA0I0VGGtF"
 
 public struct Version {
     public let major: Int
