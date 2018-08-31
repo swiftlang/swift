swiftlang/swift

commit 61152e658561edec7cff85049d872abf31c766cc

Sema: Move getUnlabeledType() to CSRanking and simplify it

lib/Sema/CSRanking.cpp
@@ -726,6 +726,19 @@ Comparison TypeChecker::compareDeclarations(DeclContext *dc,
   return decl1Better? Comparison::Better : Comparison::Worse;
 }
 
+static Type getUnlabeledType(Type type, ASTContext &ctx) {
+  if (auto *tupleType = dyn_cast<TupleType>(type.getPointer())) {
+    SmallVector<TupleTypeElt, 8> elts;
+    for (auto elt : tupleType->getElements()) {
+      elts.push_back(elt.getWithoutName());
+    }
+
+    return TupleType::get(elts, ctx);
+  }
+
+  return type;
+}
+
 SolutionCompareResult ConstraintSystem::compareSolutions(
     ConstraintSystem &cs, ArrayRef<Solution> solutions,
     const SolutionDiff &diff, unsigned idx1, unsigned idx2,
@@ -1033,8 +1046,8 @@ SolutionCompareResult ConstraintSystem::compareSolutions(
         ++score2;
 
       // Prefer the unlabeled form of a type.
-      auto unlabeled1 = type1->getUnlabeledType(cs.getASTContext());
-      auto unlabeled2 = type2->getUnlabeledType(cs.getASTContext());
+      auto unlabeled1 = getUnlabeledType(type1, cs.getASTContext());
+      auto unlabeled2 = getUnlabeledType(type2, cs.getASTContext());
       if (unlabeled1->isEqual(unlabeled2)) {
         if (type1->isEqual(unlabeled1)) {
           ++score1;

include/swift/AST/Types.h
@@ -907,14 +907,6 @@ public:
   /// argument labels removed.
   Type removeArgumentLabels(unsigned numArgumentLabels);
 
-  /// getUnlabeledType - Retrieve a version of this type with all labels
-  /// removed at every level. For example, given a tuple type 
-  /// \code
-  /// (p : (x : int, y : int))
-  /// \endcode
-  /// the result would be the (parenthesized) type ((int, int)).
-  Type getUnlabeledType(ASTContext &Context);
-
   /// Retrieve the type without any parentheses around it.
   Type getWithoutParens();
 

lib/AST/Type.cpp
@@ -680,49 +680,6 @@ bool TypeBase::isExistentialWithError() {
   return layout.isExistentialWithError(getASTContext());
 }
 
-
-static Type getStrippedType(const ASTContext &context, Type type,
-                            bool stripLabels) {
-  return type.transform([&](Type type) -> Type {
-    auto *tuple = dyn_cast<TupleType>(type.getPointer());
-    if (!tuple)
-      return type;
-
-    SmallVector<TupleTypeElt, 4> elements;
-    bool anyChanged = false;
-    unsigned idx = 0;
-    for (const auto &elt : tuple->getElements()) {
-      Type eltTy = getStrippedType(context, elt.getRawType(),
-                                   stripLabels);
-      if (anyChanged || eltTy.getPointer() != elt.getRawType().getPointer() ||
-          (elt.hasName() && stripLabels)) {
-        if (!anyChanged) {
-          elements.reserve(tuple->getNumElements());
-          for (unsigned i = 0; i != idx; ++i) {
-            const TupleTypeElt &elt = tuple->getElement(i);
-            Identifier newName = stripLabels? Identifier() : elt.getName();
-            elements.push_back(elt.getWithName(newName));
-          }
-          anyChanged = true;
-        }
-
-        Identifier newName = stripLabels? Identifier() : elt.getName();
-        elements.emplace_back(eltTy, newName, elt.getParameterFlags());
-      }
-      ++idx;
-    }
-
-    if (!anyChanged)
-      return type;
-
-    return TupleType::get(elements, context);
-  });
-}
-
-Type TypeBase::getUnlabeledType(ASTContext &Context) {
-  return getStrippedType(Context, Type(this), /*stripLabels=*/true);
-}
-
 /// Remove argument labels from the function type.
 Type TypeBase::removeArgumentLabels(unsigned numArgumentLabels) {
   // If there is nothing to remove, don't.

commit 6b13734da3e7c33392681665b75171eb6d8f414f

Sema: Fix regression with tuple conversions from recent changes
61152e658561edec7cff85049d872abf31c766cc introduced a behavior change
that was caught neither by our test suite nor the source compatibility
tests.

lib/Sema/CSRanking.cpp
@@ -758,16 +758,18 @@ Comparison TypeChecker::compareDeclarations(DeclContext *dc,
 }
 
 static Type getUnlabeledType(Type type, ASTContext &ctx) {
-  if (auto *tupleType = dyn_cast<TupleType>(type.getPointer())) {
-    SmallVector<TupleTypeElt, 8> elts;
-    for (auto elt : tupleType->getElements()) {
-      elts.push_back(elt.getWithoutName());
+  return type.transform([&](Type type) -> Type {
+    if (auto *tupleType = dyn_cast<TupleType>(type.getPointer())) {
+      SmallVector<TupleTypeElt, 8> elts;
+      for (auto elt : tupleType->getElements()) {
+        elts.push_back(elt.getWithoutName());
+      }
+
+      return TupleType::get(elts, ctx);
     }
 
-    return TupleType::get(elts, ctx);
-  }
-
-  return type;
+    return type;
+  });
 }
 
 SolutionCompareResult ConstraintSystem::compareSolutions(

test/Constraints/tuple.swift
@@ -245,3 +245,10 @@ func microwave() -> Dinner {
   let d: Dinner? = nil
   return (n: d) // expected-error{{cannot convert return expression of type '(n: Dinner?)' to return type 'Dinner'}}
 }
+
+// Tuple conversion with an optional
+func f(b: Bool) -> (a: Int, b: String)? {
+  let x = 3
+  let y = ""
+  return b ? (x, y) : nil
+}