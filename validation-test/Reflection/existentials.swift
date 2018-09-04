e3f3e939cb67a68b45d30792101c0e05e3710f8e
Jordan Rose <jordan_rose@apple.com>
2018-09-04 08:45:25 -0700

    [test] Fix target-based reflection tests for real (#19098)
    - Hard-link %target-swift-reflection-test into %t/ for %target-run
      implementations that copy executables, like remote-run.
    - Use DYLD_LIBRARY_PATH to undo that for %target-run implementations
      that run the executable in place (host and simulators)
    
    rdar://problem/43809613

--- validation-test/Reflection/existentials.swift
+++ validation-test/Reflection/existentials.swift
@@ -1,17 +1,10 @@
 // RUN: %empty-directory(%t)
 // RUN: %target-build-swift -lswiftSwiftReflectionTest %s -o %t/existentials
 // RUN: %target-codesign %t/existentials
-
-// Link %target-swift-reflection-test into %t to convince %target-run to copy
-// it.
-// RUN: ln -s %target-swift-reflection-test %t/swift-reflection-test
-// RUN: %target-run %t/swift-reflection-test %t/existentials | %FileCheck %s --check-prefix=CHECK-%target-ptrsize
+// RUN: %target-run-reflection-test %t/existentials | %FileCheck %s --check-prefix=CHECK-%target-ptrsize
 
 // REQUIRES: objc_interop
 // REQUIRES: executable_test
-
-// FIXME: Handle different forms of %target-run more robustly
-// REQUIRES: OS=macosx
 
 /*
    This file pokes at the swift_reflection_projectExistential API
