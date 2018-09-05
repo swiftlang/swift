054e53ea49e7adc5d90d667e66d0f3b6cefd6123
Richard Wei <rxrwei@gmail.com>
2018-09-05 11:35:26 -0700

    Enable tensor_autodiff test with workaround for @convention(method)

--- test/TensorFlow/tensor_autodiff.swift
+++ test/TensorFlow/tensor_autodiff.swift
@@ -1,11 +1,10 @@
 // RUN: %target-swift-frontend -emit-sil %s | %FileCheck %s
-// XFAIL: *
 
 import TensorFlow
 
 public func test1() {
   func addSelf(_ x: Tensor<Float>) -> Tensor<Float> {
-    return x + log(x)
+    return log(x)
   }
   _ = #gradient(addSelf)
 }
