d50a3e804e71d7edefeaeae0e20d926a024a4f2b
Pavel Yaskevich <pyaskevich@apple.com>
2018-09-07 11:56:39 -0700

    [CSStep] Add License header to `CSStep.cpp`

--- lib/Sema/CSStep.cpp
+++ lib/Sema/CSStep.cpp
@@ -1,3 +1,20 @@
+//===--- CSStep.cpp - Constraint Solver Steps -----------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2018 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+//
+// This file implements the \c SolverStep class and its related types,
+// which is used by constraint solver to do iterative solving.
+//
+//===----------------------------------------------------------------------===//
+
 #include "CSStep.h"
 #include "ConstraintSystem.h"
 #include "swift/AST/Types.h"
