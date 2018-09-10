ea495e17e0e96368dcc32041d4fd721cec5d59bd
Richard Wei <rxrwei@gmail.com>
2018-09-09 23:14:09 -0700

    Revert "[AutoDiff] Temporarily disable object outliner to unblock AD user code (#19163)"
    This reverts commit 37f2e5fbb22f5ae2c8c02079cdb5793e475eb9bd.

--- test/TensorFlow/tensor_autodiff.swift
+++ test/TensorFlow/tensor_autodiff.swift
@@ -11,14 +11,3 @@ public func test1() {
 }
 
 // CHECK: @{{.*}}matsquare{{.*}}__grad_src_0_wrt_0
-
-// SR-8709
-public func selfmin(_ x: Tensor<Float>) -> Tensor<Float> {
-  return min(x, x)
-}
-
-public func test2() {
-  _ = #gradient(selfmin)(Tensor(1))
-}
-
-// CHECK: @{{.*}}selfmin{{.*}}__grad_src_0_wrt_0
