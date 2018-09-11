42543e7bba8076d1c65a6f9b288472d65b8cd476
Nate Cook <natecook@apple.com>
2018-09-11 11:31:53 -0500

    Point the 'sort3/simple' test back at sort3

--- validation-test/stdlib/Algorithm.swift
+++ validation-test/stdlib/Algorithm.swift
@@ -227,7 +227,7 @@ Algorithm.test("sort3/simple")
     [1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]
   ]) {
     var input = $0
-    input.sort()
+    input._sort3(0, 1, 2, by: <)
     expectEqual([1, 2, 3], input)
 }
 
