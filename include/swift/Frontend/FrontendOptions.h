73c2ae066ad1cbb7ca170e2cac1ec3ee45646ed0
Harlan Haskins <harlan@apple.com>
2018-09-12 14:30:27 -0700

    Undo Frontend/FileTypes -> Basic/FileTypes change

--- include/swift/Frontend/FrontendOptions.h
+++ include/swift/Frontend/FrontendOptions.h
@@ -13,8 +13,8 @@
 #ifndef SWIFT_FRONTEND_FRONTENDOPTIONS_H
 #define SWIFT_FRONTEND_FRONTENDOPTIONS_H
 
-#include "swift/Basic/FileTypes.h"
 #include "swift/Basic/Version.h"
+#include "swift/Frontend/FileTypes.h"
 #include "swift/Frontend/FrontendInputsAndOutputs.h"
 #include "swift/Frontend/InputFile.h"
 #include "llvm/ADT/Hashing.h"
