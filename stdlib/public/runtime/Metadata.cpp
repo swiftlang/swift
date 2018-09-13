824851926421f2e0d5fae17ce5f24e378dee71fb
Doug Gregor <dgregor@apple.com>
2018-09-12 23:21:57 -0700

    [ABI] Emit associated type witnesses resiliently.
    Emit associated type witnesses into resilient conformance tables, so they
    can be re-ordered within the protocol without breaking clients. This will
    (eventually) permit adding new, defaulted associated types to protocols
    resiliently.

--- stdlib/public/runtime/Metadata.cpp
+++ stdlib/public/runtime/Metadata.cpp
@@ -3647,8 +3647,7 @@ static void initializeResilientWitnessTable(GenericWitnessTable *genericTable,
   for (size_t i = 0, e = protocol->NumRequirements; i < e; ++i) {
     auto &reqt = requirements[i];
 
-    // Only function-like requirements are filled in from the
-    // resilient witness table.
+    // Only certain requirements are filled in from the resilient witness table.
     switch (reqt.Flags.getKind()) {
     case ProtocolRequirementFlags::Kind::Method:
     case ProtocolRequirementFlags::Kind::Init:
@@ -3656,9 +3655,9 @@ static void initializeResilientWitnessTable(GenericWitnessTable *genericTable,
     case ProtocolRequirementFlags::Kind::Setter:
     case ProtocolRequirementFlags::Kind::ReadCoroutine:
     case ProtocolRequirementFlags::Kind::ModifyCoroutine:
+    case ProtocolRequirementFlags::Kind::AssociatedTypeAccessFunction:
       break;
     case ProtocolRequirementFlags::Kind::BaseProtocol:
-    case ProtocolRequirementFlags::Kind::AssociatedTypeAccessFunction:
     case ProtocolRequirementFlags::Kind::AssociatedConformanceAccessFunction:
       continue;
     }
