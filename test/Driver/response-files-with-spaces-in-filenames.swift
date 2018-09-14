b36eede8b11e7cca614d77df5f6fb0d6511e4c09
David Ungar <dungar@apple.com>
2018-09-14 10:16:57 -0700

    Use %target-build-swift and -parse.

--- test/Driver/response-files-with-spaces-in-filenames.swift
+++ test/Driver/response-files-with-spaces-in-filenames.swift
@@ -1,4 +1,4 @@
 // RUN: %empty-directory(%t)
 // RUN: touch "%t/f i l e.swift"
 //
-// RUN: %swiftc_driver -driver-force-response-files -typecheck  "%t/f i l e.swift"
+// RUN: %target-build-swift -driver-force-response-files -parse  "%t/f i l e.swift"
