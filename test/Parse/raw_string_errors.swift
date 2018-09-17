50497ff5a9ab50a7b5982684c47d1f90f9676a55
Rintaro Ishizaki <rishizaki@apple.com>
2018-09-18 08:44:20 +0900

    [Lexer] Add a couple of test cases for extended escaping str literal

--- test/Parse/raw_string_errors.swift
+++ test/Parse/raw_string_errors.swift
@@ -16,3 +16,9 @@ let _ = ###"invalid"######
 // expected-error@-1{{too many '#' characters in closing delimiter}}{{24-27=}}
 // expected-error@-2{{consecutive statements on a line must be separated by ';'}}
 // expected-error@-3{{expected expression}}
+
+let _ = ##"""##
+  foobar
+  ##"""##
+// expected-error@-3{{multi-line string literal content must begin on a new line}}{{14-14=\n}}
+// expected-error@-2{{multi-line string literal closing delimiter must begin on a new line}}{{5-5=\n}}
