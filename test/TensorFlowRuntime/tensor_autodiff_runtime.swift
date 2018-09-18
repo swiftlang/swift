857131bd4f0db03b8c037cbb1c0a66a7bdfba541
marcrasi <marc.rasi@gmail.com>
2018-09-17 19:10:08 -0700

    Move tensor ops back into extensions (#19354)
    * Revert "move some tensor ops out of extensions (#19201)"
    
    This reverts commit 02a81acb43faeb6e033f45a9f87dc645d03d2183.
    
    * fix matmul op
    
    * add more tests

--- test/TensorFlowRuntime/tensor_autodiff_runtime.swift
+++ test/TensorFlowRuntime/tensor_autodiff_runtime.swift
@@ -11,14 +11,9 @@ import TensorFlowUnittest
 var TensorADTests = TestSuite("TensorAD")
 
 TensorADTests.testAllBackends("SimpleAdjointCall") {
-  let adjMatmul: (
-    Tensor<Float>,
-    Tensor<Float>,
-    Tensor<Float>,
-    Tensor<Float>
-  ) -> (Tensor<Float>, Tensor<Float>) = #adjoint(matmul)
-  let x = Tensor<Float>([[1]])
-  let (d0, d1) = adjMatmul(x, x, x, x)
+  let adjPlus = #adjoint(Tensor<Float>.+)
+  let x = Tensor<Float>(1)
+  let (d0, d1) = adjPlus(x, x, x + x, x)
   expectNearlyEqual(1, d0.scalarized())
   expectNearlyEqual(1, d1.scalarized())
 }
@@ -29,6 +24,47 @@ TensorADTests.testAllBackends("TestSimpleGrad") {
   }
   expectTrue(#gradient(square)([0.1, 0.2, 0.3]) == [0.2, 0.4, 0.6])
   expectTrue(#gradient(square)([[10], [20]]) == [[20], [40]])
+}
+
+TensorADTests.testAllBackends("+") {
+  let grad = #gradient({ (a: Tensor<Float>, b: Tensor<Float>) in a + b })
+  expectTrue(([1], [1]) == grad([0], [0]))
+  expectTrue(([1], [1]) == grad([1], [10]))
+}
+
+TensorADTests.testAllBackends("-") {
+  let grad = #gradient({ (a: Tensor<Float>, b: Tensor<Float>) in a - b })
+  expectTrue(([1], [-1]) == grad([0], [0]))
+  expectTrue(([1], [-1]) == grad([1], [10]))
+}
+
+TensorADTests.testAllBackends("*") {
+  let grad = #gradient({ (a: Tensor<Float>, b: Tensor<Float>) in a * b })
+  expectTrue(([0], [0]) == grad([0], [0]))
+  expectTrue(([10], [1]) == grad([1], [10]))
+}
+
+TensorADTests.testAllBackends("/") {
+  let grad = #gradient({ (a: Tensor<Float>, b: Tensor<Float>) in a / b })
+  expectTrue(([0.1], [-0.01]) == grad([1], [10]))
+}
+
+TensorADTests.testAllBackends("matmul") {
+  let grad = #gradient({ (a: Tensor<Float>, b: Tensor<Float>) in matmul(a, b) })
+  expectTrue(([[0]], [[0]]) == grad([[0]], [[0]]))
+  expectTrue(([[10]], [[1]]) == grad([[1]], [[10]]))
+}
+
+TensorADTests.testAllBackends("•") {
+  let grad = #gradient({ (a: Tensor<Float>, b: Tensor<Float>) in a • b })
+  expectTrue(([[0]], [[0]]) == grad([[0]], [[0]]))
+  expectTrue(([[10]], [[1]]) == grad([[1]], [[10]]))
+}
+
+TensorADTests.testAllBackends("negate") {
+  let grad = #gradient({ (a: Tensor<Float>) in -a })
+  expectTrue([-1] == grad([0]))
+  expectTrue([-1] == grad([10]))
 }
 
 runAllTests()
