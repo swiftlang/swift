5c34c0073ecb956191978e9da7a736ab3e055ae5
Ben Cohen <ben_cohen@apple.com>
2018-09-20 11:04:59 -0700

    Refactor ArrayTraps tests

--- validation-test/stdlib/ArrayTraps.swift
+++ validation-test/stdlib/ArrayTraps.swift
@@ -1,262 +1,128 @@
 // RUN: %empty-directory(%t)
-// RUN: %gyb %s -o %t/ArrayTraps.swift
-// RUN: %line-directive %t/ArrayTraps.swift -- %target-build-swift %t/ArrayTraps.swift -o %t/a.out_Debug -Onone
-// RUN: %line-directive %t/ArrayTraps.swift -- %target-build-swift %t/ArrayTraps.swift -o %t/a.out_Release -O
-//
-// RUN: %target-codesign %t/a.out_Debug
-// RUN: %target-codesign %t/a.out_Release
-// RUN: %line-directive %t/ArrayTraps.swift -- %target-run %t/a.out_Debug
-// RUN: %line-directive %t/ArrayTraps.swift -- %target-run %t/a.out_Release
+// RUN: %target-build-swift %s -o %t/a.out_Debug -Onone -swift-version 4.2 && %target-codesign %t/a.out_Debug && %target-run %t/a.out_Debug
+// RUN: %target-build-swift %s -o %t/a.out_Release -O -swift-version 4.2 && %target-codesign %t/a.out_Release && %target-run %t/a.out_Release
 // REQUIRES: executable_test
 
 import StdlibUnittest
 
 let testSuiteSuffix = _isDebugAssertConfiguration() ? "_debug" : "_release"
 
-var ArrayTraps = TestSuite("ArrayTraps" + testSuiteSuffix)
-var ArrayLowerBoundOutOfRangeErrorMsg = (
-    "Out of bounds: range begins before startIndex"
-)
+let ArrayTraps = TestSuite("ArrayTraps" + testSuiteSuffix)
+let ArrayLowerBoundOutOfRangeErrorMsg = "Out of bounds: range begins before startIndex"
+let ArrayBoundsErrorMsg = "Index out of range"
 
-var ArrayBoundsErrorMsg = (
-    "Index out of range"
-)
-
-
-ArrayTraps.test("bounds1/read")
-  .skip(.custom(
-    { _isFastAssertConfiguration() },
-    reason: "this trap is not guaranteed to happen in -Ounchecked"))
-  .crashOutputMatches(_isDebugAssertConfiguration() ?
-    ArrayBoundsErrorMsg : "")
+for i in [0,100] {
+  ArrayTraps.test("bounds/readEmpty/\(i)")
+  .skip(.custom(_isFastAssertConfiguration, reason: "this trap is not guaranteed to happen in -Ounchecked"))
+  .crashOutputMatches(_isDebugAssertConfiguration() ? ArrayBoundsErrorMsg : "")
   .code {
-  var a: Array<Int> = []
-  expectCrashLater()
-let x = a[0]
+    let a: Array<Int> = []
+    expectCrashLater()
+    _ = a[i]
+  }
+  
+  ArrayTraps.test("bounds/writeEmpty/\(i)")
+  .skip(.custom(_isFastAssertConfiguration, reason: "this trap is not guaranteed to happen in -Ounchecked"))
+  .crashOutputMatches(_isDebugAssertConfiguration() ? ArrayBoundsErrorMsg : "")
+  .code {
+    var a: Array<Int> = []
+    expectCrashLater()
+    a[i] = 1
+  }
 }
 
-ArrayTraps.test("bounds2/read")
-  .skip(.custom(
-    { _isFastAssertConfiguration() },
-    reason: "this trap is not guaranteed to happen in -Ounchecked"))
-  .crashOutputMatches(_isDebugAssertConfiguration() ?
-    ArrayBoundsErrorMsg : "")
-  .code {
-  var a: Array<Int> = []
+ArrayTraps.test("bounds/read/end")
+.skip(.custom(_isFastAssertConfiguration, reason: "this trap is not guaranteed to happen in -Ounchecked"))
+.crashOutputMatches(_isDebugAssertConfiguration() ? ArrayBoundsErrorMsg : "")
+.code {
+  let a: Array = [ 10, 20, 30 ]
   expectCrashLater()
-let x = a[100]
+  _ = a[3]
 }
 
-ArrayTraps.test("bounds3/read")
-  .skip(.custom(
-    { _isFastAssertConfiguration() },
-    reason: "this trap is not guaranteed to happen in -Ounchecked"))
-  .crashOutputMatches(_isDebugAssertConfiguration() ?
-    ArrayBoundsErrorMsg : "")
-  .code {
+ArrayTraps.test("bounds/write/end")
+.skip(.custom(_isFastAssertConfiguration, reason: "this trap is not guaranteed to happen in -Ounchecked"))
+.crashOutputMatches(_isDebugAssertConfiguration() ? ArrayBoundsErrorMsg : "")
+.code {
   var a: Array = [ 10, 20, 30 ]
   expectCrashLater()
-let x = a[3]
+  a[3] = 1
 }
 
-ArrayTraps.test("sliceBounds0/read")
-  .skip(.custom(
-    { _isFastAssertConfiguration() },
-    reason: "this trap is not guaranteed to happen in -Ounchecked"))
-  .crashOutputMatches(_isDebugAssertConfiguration() ?
-    ArrayLowerBoundOutOfRangeErrorMsg : "")
+for r in [-1..<1,0..<2,1..<2] {
+  let crashText = _isDebugAssertConfiguration() 
+    ? (r.lowerBound < 0 ? "Negative " : "") + "Array index is out of range"
+    : ""
+  
+  ArrayTraps.test("sliceBounds/empty/read/\(r)")
+  .skip(.custom(_isFastAssertConfiguration, reason: "this trap is not guaranteed to happen in -Ounchecked"))
+  .crashOutputMatches(crashText)
   .code {
-  var a: Array<Int> = []
-  expectCrashLater()
-let x = a[-1..<1]
+    let a: Array<Int> = []
+    expectCrashLater()
+    _ = a[-1..<1]
+  }
+
+  ArrayTraps.test("sliceBounds/empty/write/\(r)")
+  .skip(.custom(_isFastAssertConfiguration, reason: "this trap is not guaranteed to happen in -Ounchecked"))
+  .crashOutputMatches(crashText)
+  .code {
+    var a: Array<Int> = []
+    expectCrashLater()
+    a[-1..<1] = []
+  }
+
+  ArrayTraps.test("sliceBounds/single/read/\(r)")
+  .skip(.custom(_isFastAssertConfiguration, reason: "this trap is not guaranteed to happen in -Ounchecked"))
+  .crashOutputMatches(crashText)
+  .code {
+    let a: Array = [ 1 ]
+    expectCrashLater()
+    _ = a[r]
+  }
+
+  ArrayTraps.test("sliceBounds/single/write/\(r)")
+  .skip(.custom(_isFastAssertConfiguration, reason: "this trap is not guaranteed to happen in -Ounchecked"))
+  .crashOutputMatches(crashText)
+  .code {
+    var a: Array = [ 1 ]
+    expectCrashLater()
+    a[r] = []
+  }
 }
 
-ArrayTraps.test("sliceBounds1/read")
-  .skip(.custom(
-    { _isFastAssertConfiguration() },
-    reason: "this trap is not guaranteed to happen in -Ounchecked"))
-  .crashOutputMatches(_isDebugAssertConfiguration() ?
-    ArrayLowerBoundOutOfRangeErrorMsg : "")
-  .code {
-  var a: Array = [ 1 ]
-  expectCrashLater()
-let x = a[-1..<1]
-}
-
-ArrayTraps.test("sliceBounds2/read")
-  .skip(.custom(
-    { _isFastAssertConfiguration() },
-    reason: "this trap is not guaranteed to happen in -Ounchecked"))
-  .crashOutputMatches(_isDebugAssertConfiguration() ?
-    "Out of bounds: range begins after bounds.upperBound" : "")
-  .code {
-  var a: Array = [ 1 ]
-  expectCrashLater()
-let x = a[0..<2]
-}
-
-ArrayTraps.test("sliceBounds3/read")
-  .skip(.custom(
-    { _isFastAssertConfiguration() },
-    reason: "this trap is not guaranteed to happen in -Ounchecked"))
-  .crashOutputMatches(_isDebugAssertConfiguration() ?
-    "Out of bounds: range begins after bounds.upperBound" : "")
-  .code {
-  var a: Array = [ 1 ]
-  expectCrashLater()
-let x = a[1..<2]
-}
-
-
-ArrayTraps.test("bounds1/write")
-  .skip(.custom(
-    { _isFastAssertConfiguration() },
-    reason: "this trap is not guaranteed to happen in -Ounchecked"))
-  .crashOutputMatches(_isDebugAssertConfiguration() ?
-    ArrayBoundsErrorMsg : "")
-  .code {
-  var a: Array<Int> = []
-  expectCrashLater()
-a[0] = 1
-}
-
-ArrayTraps.test("bounds2/write")
-  .skip(.custom(
-    { _isFastAssertConfiguration() },
-    reason: "this trap is not guaranteed to happen in -Ounchecked"))
-  .crashOutputMatches(_isDebugAssertConfiguration() ?
-    ArrayBoundsErrorMsg : "")
-  .code {
-  var a: Array<Int> = []
-  expectCrashLater()
-a[100] = 1
-}
-
-ArrayTraps.test("bounds3/write")
-  .skip(.custom(
-    { _isFastAssertConfiguration() },
-    reason: "this trap is not guaranteed to happen in -Ounchecked"))
-  .crashOutputMatches(_isDebugAssertConfiguration() ?
-    ArrayBoundsErrorMsg : "")
-  .code {
-  var a: Array = [ 10, 20, 30 ]
-  expectCrashLater()
-a[3] = 1
-}
-
-ArrayTraps.test("sliceBounds0/write")
-  .skip(.custom(
-    { _isFastAssertConfiguration() },
-    reason: "this trap is not guaranteed to happen in -Ounchecked"))
-  .crashOutputMatches(_isDebugAssertConfiguration() ?
-    ArrayLowerBoundOutOfRangeErrorMsg : "")
-  .code {
-  var a: Array<Int> = []
-  expectCrashLater()
-a[-1..<1] = []
-}
-
-ArrayTraps.test("sliceBounds1/write")
-  .skip(.custom(
-    { _isFastAssertConfiguration() },
-    reason: "this trap is not guaranteed to happen in -Ounchecked"))
-  .crashOutputMatches(_isDebugAssertConfiguration() ?
-    ArrayLowerBoundOutOfRangeErrorMsg : "")
-  .code {
-  var a: Array = [ 1 ]
-  expectCrashLater()
-a[-1..<1] = []
-}
-
-ArrayTraps.test("sliceBounds2/write")
-  .skip(.custom(
-    { _isFastAssertConfiguration() },
-    reason: "this trap is not guaranteed to happen in -Ounchecked"))
-  .crashOutputMatches(_isDebugAssertConfiguration() ?
-    "Out of bounds: range begins after bounds.upperBound" : "")
-  .code {
-  var a: Array = [ 1 ]
-  expectCrashLater()
-a[0..<2] = []
-}
-
-ArrayTraps.test("sliceBounds3/write")
-  .skip(.custom(
-    { _isFastAssertConfiguration() },
-    reason: "this trap is not guaranteed to happen in -Ounchecked"))
-  .crashOutputMatches(_isDebugAssertConfiguration() ?
-    "Out of bounds: range begins after bounds.upperBound" : "")
-  .code {
-  var a: Array = [ 1 ]
-  expectCrashLater()
-a[1..<2] = []
-}
-
-
-ArrayTraps.test("PopFromEmpty")
-  .crashOutputMatches(_isDebugAssertConfiguration() ?
-    "Can't remove last element from an empty collection" : "")
-  .code {
+ArrayTraps.test("RemoveLastFromEmpty")
+.crashOutputMatches(_isDebugAssertConfiguration() ? "Can't remove last element from an empty collection" : "")
+.code {
   var a: Array<Int> = []
   expectCrashLater()
   a.removeLast()
 }
 
-ArrayTraps.test("insert(_:at:)/-1")
-  .skip(.custom(
-    { _isFastAssertConfiguration() },
-    reason: "this trap is not guaranteed to happen in -Ounchecked"))
-  .crashOutputMatches(_isDebugAssertConfiguration() ?
-    "Negative Array index is out of range"
-    : "")
+for i in [-1,2] {
+  let crashText = _isDebugAssertConfiguration() 
+    ? (i < 0 ? "Negative " : "") + "Array index is out of range"
+    : ""
+
+  ArrayTraps.test("insert(_:at:)/\(i)")
+  .skip(.custom(_isFastAssertConfiguration, reason: "this trap is not guaranteed to happen in -Ounchecked"))
+  .crashOutputMatches(crashText)
   .code {
-  var a: Array<Int> = [42]
-  expectCrashLater()
-  a.insert(3, at: -1)
-}
-ArrayTraps.test("insert(_:at:)/2")
-  .skip(.custom(
-    { _isFastAssertConfiguration() },
-    reason: "this trap is not guaranteed to happen in -Ounchecked"))
-  .crashOutputMatches(_isDebugAssertConfiguration() ?
-    "Array index is out of range"
-    : "")
-  .code {
-  var a: Array<Int> = [42]
-  expectCrashLater()
-  a.insert(3, at: 2)
+    var a: Array<Int> = [42]
+    expectCrashLater()
+    a.insert(3, at: i)
+  }  
 }
 
-ArrayTraps.test("remove(at:)/-1")
-  .skip(.custom(
-    { _isFastAssertConfiguration() },
-    reason: "this trap is not guaranteed to happen in -Ounchecked"))
-  .crashOutputMatches(_isDebugAssertConfiguration() ?
-    ArrayBoundsErrorMsg : "")
+for i in [-1,1,2] {
+  ArrayTraps.test("remove(at:)/\(i)")
+  .skip(.custom(_isFastAssertConfiguration, reason: "this trap is not guaranteed to happen in -Ounchecked"))
+  .crashOutputMatches(_isDebugAssertConfiguration() ? ArrayBoundsErrorMsg : "")
   .code {
-  var a: Array<Int> = [42]
-  expectCrashLater()
-  a.remove(at: -1)
+    var a: Array<Int> = [42]
+    expectCrashLater()
+    a.remove(at: i)
+  }
 }
-ArrayTraps.test("remove(at:)/1")
-  .skip(.custom(
-    { _isFastAssertConfiguration() },
-    reason: "this trap is not guaranteed to happen in -Ounchecked"))
-  .crashOutputMatches(_isDebugAssertConfiguration() ?
-    ArrayBoundsErrorMsg : "")
-  .code {
-  var a: Array<Int> = [42]
-  expectCrashLater()
-  a.remove(at: 1)
-}
-ArrayTraps.test("remove(at:)/2")
-  .skip(.custom(
-    { _isFastAssertConfiguration() },
-    reason: "this trap is not guaranteed to happen in -Ounchecked"))
-  .crashOutputMatches(_isDebugAssertConfiguration() ?
-    ArrayBoundsErrorMsg : "")
-  .code {
-  var a: Array<Int> = [42]
-  expectCrashLater()
-  a.remove(at: 2)
-}
+
+runAllTests()
--- validation-test/stdlib/ContiguousArrayTraps.swift
+++ validation-test/stdlib/ContiguousArrayTraps.swift
@@ -1,262 +1,128 @@
 // RUN: %empty-directory(%t)
-// RUN: %gyb %s -o %t/ArrayTraps.swift
-// RUN: %line-directive %t/ArrayTraps.swift -- %target-build-swift %t/ArrayTraps.swift -o %t/a.out_Debug -Onone
-// RUN: %line-directive %t/ArrayTraps.swift -- %target-build-swift %t/ArrayTraps.swift -o %t/a.out_Release -O
-//
-// RUN: %target-codesign %t/a.out_Debug
-// RUN: %target-codesign %t/a.out_Release
-// RUN: %line-directive %t/ArrayTraps.swift -- %target-run %t/a.out_Debug
-// RUN: %line-directive %t/ArrayTraps.swift -- %target-run %t/a.out_Release
+// RUN: %target-build-swift %s -o %t/a.out_Debug -Onone -swift-version 4.2 && %target-codesign %t/a.out_Debug && %target-run %t/a.out_Debug
+// RUN: %target-build-swift %s -o %t/a.out_Release -O -swift-version 4.2 && %target-codesign %t/a.out_Release && %target-run %t/a.out_Release
 // REQUIRES: executable_test
 
 import StdlibUnittest
 
 let testSuiteSuffix = _isDebugAssertConfiguration() ? "_debug" : "_release"
 
-var ContiguousArrayTraps = TestSuite("ContiguousArrayTraps" + testSuiteSuffix)
-var ContiguousArrayLowerBoundOutOfRangeErrorMsg = (
-    "Out of bounds: range begins before startIndex"
-)
+let ContiguousArrayTraps = TestSuite("ContiguousArrayTraps" + testSuiteSuffix)
+let ContiguousArrayLowerBoundOutOfRangeErrorMsg = "Out of bounds: range begins before startIndex"
+let ContiguousArrayBoundsErrorMsg = "Index out of range"
 
-var ContiguousArrayBoundsErrorMsg = (
-    "Index out of range"
-)
-
-
-ContiguousArrayTraps.test("bounds1/read")
-  .skip(.custom(
-    { _isFastAssertConfiguration() },
-    reason: "this trap is not guaranteed to happen in -Ounchecked"))
-  .crashOutputMatches(_isDebugAssertConfiguration() ?
-    ContiguousArrayBoundsErrorMsg : "")
+for i in [0,100] {
+  ContiguousArrayTraps.test("bounds/readEmpty/\(i)")
+  .skip(.custom(_isFastAssertConfiguration, reason: "this trap is not guaranteed to happen in -Ounchecked"))
+  .crashOutputMatches(_isDebugAssertConfiguration() ? ContiguousArrayBoundsErrorMsg : "")
   .code {
-  var a: ContiguousArray<Int> = []
-  expectCrashLater()
-let x = a[0]
+    let a: ContiguousArray<Int> = []
+    expectCrashLater()
+    _ = a[i]
+  }
+  
+  ContiguousArrayTraps.test("bounds/writeEmpty/\(i)")
+  .skip(.custom(_isFastAssertConfiguration, reason: "this trap is not guaranteed to happen in -Ounchecked"))
+  .crashOutputMatches(_isDebugAssertConfiguration() ? ContiguousArrayBoundsErrorMsg : "")
+  .code {
+    var a: ContiguousArray<Int> = []
+    expectCrashLater()
+    a[i] = 1
+  }
 }
 
-ContiguousArrayTraps.test("bounds2/read")
-  .skip(.custom(
-    { _isFastAssertConfiguration() },
-    reason: "this trap is not guaranteed to happen in -Ounchecked"))
-  .crashOutputMatches(_isDebugAssertConfiguration() ?
-    ContiguousArrayBoundsErrorMsg : "")
-  .code {
-  var a: ContiguousArray<Int> = []
+ContiguousArrayTraps.test("bounds/read/end")
+.skip(.custom(_isFastAssertConfiguration, reason: "this trap is not guaranteed to happen in -Ounchecked"))
+.crashOutputMatches(_isDebugAssertConfiguration() ? ContiguousArrayBoundsErrorMsg : "")
+.code {
+  let a: ContiguousArray = [ 10, 20, 30 ]
   expectCrashLater()
-let x = a[100]
+  _ = a[3]
 }
 
-ContiguousArrayTraps.test("bounds3/read")
-  .skip(.custom(
-    { _isFastAssertConfiguration() },
-    reason: "this trap is not guaranteed to happen in -Ounchecked"))
-  .crashOutputMatches(_isDebugAssertConfiguration() ?
-    ContiguousArrayBoundsErrorMsg : "")
-  .code {
+ContiguousArrayTraps.test("bounds/write/end")
+.skip(.custom(_isFastAssertConfiguration, reason: "this trap is not guaranteed to happen in -Ounchecked"))
+.crashOutputMatches(_isDebugAssertConfiguration() ? ContiguousArrayBoundsErrorMsg : "")
+.code {
   var a: ContiguousArray = [ 10, 20, 30 ]
   expectCrashLater()
-let x = a[3]
+  a[3] = 1
 }
 
-ContiguousArrayTraps.test("sliceBounds0/read")
-  .skip(.custom(
-    { _isFastAssertConfiguration() },
-    reason: "this trap is not guaranteed to happen in -Ounchecked"))
-  .crashOutputMatches(_isDebugAssertConfiguration() ?
-    ContiguousArrayLowerBoundOutOfRangeErrorMsg : "")
+for r in [-1..<1,0..<2,1..<2] {
+  let crashText = _isDebugAssertConfiguration() 
+    ? (r.lowerBound < 0 ? "Negative " : "") + "ContiguousArray index is out of range"
+    : ""
+  
+  ContiguousArrayTraps.test("sliceBounds/empty/read/\(r)")
+  .skip(.custom(_isFastAssertConfiguration, reason: "this trap is not guaranteed to happen in -Ounchecked"))
+  .crashOutputMatches(crashText)
   .code {
-  var a: ContiguousArray<Int> = []
-  expectCrashLater()
-let x = a[-1..<1]
+    let a: ContiguousArray<Int> = []
+    expectCrashLater()
+    _ = a[-1..<1]
+  }
+
+  ContiguousArrayTraps.test("sliceBounds/empty/write/\(r)")
+  .skip(.custom(_isFastAssertConfiguration, reason: "this trap is not guaranteed to happen in -Ounchecked"))
+  .crashOutputMatches(crashText)
+  .code {
+    var a: ContiguousArray<Int> = []
+    expectCrashLater()
+    a[-1..<1] = []
+  }
+
+  ContiguousArrayTraps.test("sliceBounds/single/read/\(r)")
+  .skip(.custom(_isFastAssertConfiguration, reason: "this trap is not guaranteed to happen in -Ounchecked"))
+  .crashOutputMatches(crashText)
+  .code {
+    let a: ContiguousArray = [ 1 ]
+    expectCrashLater()
+    _ = a[r]
+  }
+
+  ContiguousArrayTraps.test("sliceBounds/single/write/\(r)")
+  .skip(.custom(_isFastAssertConfiguration, reason: "this trap is not guaranteed to happen in -Ounchecked"))
+  .crashOutputMatches(crashText)
+  .code {
+    var a: ContiguousArray = [ 1 ]
+    expectCrashLater()
+    a[r] = []
+  }
 }
 
-ContiguousArrayTraps.test("sliceBounds1/read")
-  .skip(.custom(
-    { _isFastAssertConfiguration() },
-    reason: "this trap is not guaranteed to happen in -Ounchecked"))
-  .crashOutputMatches(_isDebugAssertConfiguration() ?
-    ContiguousArrayLowerBoundOutOfRangeErrorMsg : "")
-  .code {
-  var a: ContiguousArray = [ 1 ]
-  expectCrashLater()
-let x = a[-1..<1]
-}
-
-ContiguousArrayTraps.test("sliceBounds2/read")
-  .skip(.custom(
-    { _isFastAssertConfiguration() },
-    reason: "this trap is not guaranteed to happen in -Ounchecked"))
-  .crashOutputMatches(_isDebugAssertConfiguration() ?
-    "Out of bounds: range begins after bounds.upperBound" : "")
-  .code {
-  var a: ContiguousArray = [ 1 ]
-  expectCrashLater()
-let x = a[0..<2]
-}
-
-ContiguousArrayTraps.test("sliceBounds3/read")
-  .skip(.custom(
-    { _isFastAssertConfiguration() },
-    reason: "this trap is not guaranteed to happen in -Ounchecked"))
-  .crashOutputMatches(_isDebugAssertConfiguration() ?
-    "Out of bounds: range begins after bounds.upperBound" : "")
-  .code {
-  var a: ContiguousArray = [ 1 ]
-  expectCrashLater()
-let x = a[1..<2]
-}
-
-
-ContiguousArrayTraps.test("bounds1/write")
-  .skip(.custom(
-    { _isFastAssertConfiguration() },
-    reason: "this trap is not guaranteed to happen in -Ounchecked"))
-  .crashOutputMatches(_isDebugAssertConfiguration() ?
-    ContiguousArrayBoundsErrorMsg : "")
-  .code {
-  var a: ContiguousArray<Int> = []
-  expectCrashLater()
-a[0] = 1
-}
-
-ContiguousArrayTraps.test("bounds2/write")
-  .skip(.custom(
-    { _isFastAssertConfiguration() },
-    reason: "this trap is not guaranteed to happen in -Ounchecked"))
-  .crashOutputMatches(_isDebugAssertConfiguration() ?
-    ContiguousArrayBoundsErrorMsg : "")
-  .code {
-  var a: ContiguousArray<Int> = []
-  expectCrashLater()
-a[100] = 1
-}
-
-ContiguousArrayTraps.test("bounds3/write")
-  .skip(.custom(
-    { _isFastAssertConfiguration() },
-    reason: "this trap is not guaranteed to happen in -Ounchecked"))
-  .crashOutputMatches(_isDebugAssertConfiguration() ?
-    ContiguousArrayBoundsErrorMsg : "")
-  .code {
-  var a: ContiguousArray = [ 10, 20, 30 ]
-  expectCrashLater()
-a[3] = 1
-}
-
-ContiguousArrayTraps.test("sliceBounds0/write")
-  .skip(.custom(
-    { _isFastAssertConfiguration() },
-    reason: "this trap is not guaranteed to happen in -Ounchecked"))
-  .crashOutputMatches(_isDebugAssertConfiguration() ?
-    ContiguousArrayLowerBoundOutOfRangeErrorMsg : "")
-  .code {
-  var a: ContiguousArray<Int> = []
-  expectCrashLater()
-a[-1..<1] = []
-}
-
-ContiguousArrayTraps.test("sliceBounds1/write")
-  .skip(.custom(
-    { _isFastAssertConfiguration() },
-    reason: "this trap is not guaranteed to happen in -Ounchecked"))
-  .crashOutputMatches(_isDebugAssertConfiguration() ?
-    ContiguousArrayLowerBoundOutOfRangeErrorMsg : "")
-  .code {
-  var a: ContiguousArray = [ 1 ]
-  expectCrashLater()
-a[-1..<1] = []
-}
-
-ContiguousArrayTraps.test("sliceBounds2/write")
-  .skip(.custom(
-    { _isFastAssertConfiguration() },
-    reason: "this trap is not guaranteed to happen in -Ounchecked"))
-  .crashOutputMatches(_isDebugAssertConfiguration() ?
-    "Out of bounds: range begins after bounds.upperBound" : "")
-  .code {
-  var a: ContiguousArray = [ 1 ]
-  expectCrashLater()
-a[0..<2] = []
-}
-
-ContiguousArrayTraps.test("sliceBounds3/write")
-  .skip(.custom(
-    { _isFastAssertConfiguration() },
-    reason: "this trap is not guaranteed to happen in -Ounchecked"))
-  .crashOutputMatches(_isDebugAssertConfiguration() ?
-    "Out of bounds: range begins after bounds.upperBound" : "")
-  .code {
-  var a: ContiguousArray = [ 1 ]
-  expectCrashLater()
-a[1..<2] = []
-}
-
-
-ContiguousArrayTraps.test("PopFromEmpty")
-  .crashOutputMatches(_isDebugAssertConfiguration() ?
-    "Can't remove last element from an empty collection" : "")
-  .code {
+ContiguousArrayTraps.test("RemoveLastFromEmpty")
+.crashOutputMatches(_isDebugAssertConfiguration() ? "Can't remove last element from an empty collection" : "")
+.code {
   var a: ContiguousArray<Int> = []
   expectCrashLater()
   a.removeLast()
 }
 
-ContiguousArrayTraps.test("insert(_:at:)/-1")
-  .skip(.custom(
-    { _isFastAssertConfiguration() },
-    reason: "this trap is not guaranteed to happen in -Ounchecked"))
-  .crashOutputMatches(_isDebugAssertConfiguration() ?
-    "Negative ContiguousArray index is out of range"
-    : "")
+for i in [-1,2] {
+  let crashText = _isDebugAssertConfiguration() 
+    ? (i < 0 ? "Negative " : "") + "ContiguousArray index is out of range"
+    : ""
+
+  ContiguousArrayTraps.test("insert(_:at:)/\(i)")
+  .skip(.custom(_isFastAssertConfiguration, reason: "this trap is not guaranteed to happen in -Ounchecked"))
+  .crashOutputMatches(crashText)
   .code {
-  var a: ContiguousArray<Int> = [42]
-  expectCrashLater()
-  a.insert(3, at: -1)
-}
-ContiguousArrayTraps.test("insert(_:at:)/2")
-  .skip(.custom(
-    { _isFastAssertConfiguration() },
-    reason: "this trap is not guaranteed to happen in -Ounchecked"))
-  .crashOutputMatches(_isDebugAssertConfiguration() ?
-    "ContiguousArray index is out of range"
-    : "")
-  .code {
-  var a: ContiguousArray<Int> = [42]
-  expectCrashLater()
-  a.insert(3, at: 2)
+    var a: ContiguousArray<Int> = [42]
+    expectCrashLater()
+    a.insert(3, at: i)
+  }  
 }
 
-ContiguousArrayTraps.test("remove(at:)/-1")
-  .skip(.custom(
-    { _isFastAssertConfiguration() },
-    reason: "this trap is not guaranteed to happen in -Ounchecked"))
-  .crashOutputMatches(_isDebugAssertConfiguration() ?
-    ContiguousArrayBoundsErrorMsg : "")
+for i in [-1,1,2] {
+  ContiguousArrayTraps.test("remove(at:)/\(i)")
+  .skip(.custom(_isFastAssertConfiguration, reason: "this trap is not guaranteed to happen in -Ounchecked"))
+  .crashOutputMatches(_isDebugAssertConfiguration() ? ContiguousArrayBoundsErrorMsg : "")
   .code {
-  var a: ContiguousArray<Int> = [42]
-  expectCrashLater()
-  a.remove(at: -1)
+    var a: ContiguousArray<Int> = [42]
+    expectCrashLater()
+    a.remove(at: i)
+  }
 }
-ContiguousArrayTraps.test("remove(at:)/1")
-  .skip(.custom(
-    { _isFastAssertConfiguration() },
-    reason: "this trap is not guaranteed to happen in -Ounchecked"))
-  .crashOutputMatches(_isDebugAssertConfiguration() ?
-    ContiguousArrayBoundsErrorMsg : "")
-  .code {
-  var a: ContiguousArray<Int> = [42]
-  expectCrashLater()
-  a.remove(at: 1)
-}
-ContiguousArrayTraps.test("remove(at:)/2")
-  .skip(.custom(
-    { _isFastAssertConfiguration() },
-    reason: "this trap is not guaranteed to happen in -Ounchecked"))
-  .crashOutputMatches(_isDebugAssertConfiguration() ?
-    ContiguousArrayBoundsErrorMsg : "")
-  .code {
-  var a: ContiguousArray<Int> = [42]
-  expectCrashLater()
-  a.remove(at: 2)
-}
+
+runAllTests()
