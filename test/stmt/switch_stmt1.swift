0f00e31bfa6109f2f3ec1a014349f6d763acac55
Pavel Yaskevich <pyaskevich@apple.com>
2018-09-25 15:57:39 -0700

    [Sema] Diagnose 'init' use as identifier in "case" statements
    Provide a diagnostic which points out that 'init' can only
    be used in "case" statements when properly escaped.
    
    Resolves: rdar://problem/44751408

--- test/stmt/switch_stmt1.swift
+++ test/stmt/switch_stmt1.swift
@@ -5,9 +5,35 @@ enum E {
   case e2
 }
 
+enum E2 {
+  case `init`, `subscript`, foo, bar
+}
+
 func foo1(e : E, i : Int) {
   switch e {} // expected-error{{switch must be exhaustive}}
   // expected-note@-1 {{missing case: '.e1'}}
   // expected-note@-2 {{missing case: '.e2'}}
   switch i {} // expected-error{{'switch' statement body must have at least one 'case' or 'default' block; do you want to add a default case?}}{{13-13=default:\n<#code#>\n}}
+}
+
+func foo2(e: E2) {
+  switch (e) {
+  case .init: break
+  // expected-error@-1 {{keyword 'init' cannot be used as an identifier here}}
+  // expected-note@-2 {{use backticks to escape it}} {{9-13=`init`}}
+  case .subscript: break // Ok
+  default: break
+  }
+
+  switch (e) {
+  case .init, .foo: break
+  // expected-error@-1 {{keyword 'init' cannot be used as an identifier here}}
+  // expected-note@-2 {{use backticks to escape it}} {{9-13=`init`}}
+  case .subscript, .bar: break // Ok
+  }
+
+  switch (e) {
+  case .`init`, .subscript: break // Ok
+  case .foo, .bar: break
+  }
 }
