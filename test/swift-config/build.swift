70252d21f06920e7fcf4e3f6ba18ea3dd367d43e
Marcin Krzyzanowski <marcin.krzyzanowski@gmail.com>
2018-09-30 10:31:27 +0200

    Basic tests for swift-config output

---
+++ test/swift-config/build.swift
@@ -0,0 +1,5 @@
+// REQUIRES: executable_test
+// REQUIRES: OS=macosx
+
+// RUN: swift-config --obj-root | %FileCheck %s
+// CHECK: BUILD_DIR
---
+++ test/swift-config/source.swift
@@ -0,0 +1,5 @@
+// REQUIRES: executable_test
+// REQUIRES: OS=macosx
+
+// RUN: swift-config --src-root | %FileCheck %s
+// CHECK: SOURCE_DIR
