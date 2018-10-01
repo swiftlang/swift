a56c621c777adb26d8707e8b75b89e8387fc9504
Jordan Rose <jordan_rose@apple.com>
2018-09-30 19:53:36 -0700

    [test] Disable TSan tests over remote-run
    Ideally lit.cfg would conditionalize this based on the remote OS,
    but that's tricky. Meanwhile, let's not fail on old versions of
    Apple OSs that don't support TSan.
    
    rdar://problem/44802353

--- test/SILGen/tsan_instrumentation.swift
+++ test/SILGen/tsan_instrumentation.swift
@@ -1,6 +1,10 @@
 // RUN: %target-swift-emit-silgen -sanitize=thread %s | %FileCheck %s
 // REQUIRES: tsan_runtime
 
+// FIXME: This should be covered by "tsan_runtime"; older versions of Apple OSs
+// don't support TSan.
+// UNSUPPORTED: remote_run
+
 func takesInout(_ p: inout Int) { }
 func takesInout(_ p: inout MyStruct) { }
 
