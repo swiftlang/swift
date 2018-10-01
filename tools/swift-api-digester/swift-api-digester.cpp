a96948693212e6cdd470559a5c31f4d27f12bca2
Xi Ge <xi_ge@apple.com>
2018-10-01 16:00:34 -0700

    swift-module-digester: diagnose added associated types as ABI breakages even they have default type.

--- tools/swift-api-digester/swift-api-digester.cpp
+++ tools/swift-api-digester/swift-api-digester.cpp
@@ -942,14 +942,7 @@ public:
       }
       // Complain about added protocol requirements
       if (auto *D = dyn_cast<SDKNodeDecl>(Right)) {
-        if (D->isProtocolRequirement()) {
-          bool ShouldComplain = !D->isOverriding();
-          // We should allow added associated types with default.
-          if (auto ATD = dyn_cast<SDKNodeDeclAssociatedType>(D)) {
-            if (ATD->getDefault())
-              ShouldComplain = false;
-          }
-          if (ShouldComplain)
+        if (D->isProtocolRequirement() && !D->isOverriding()) {
             Ctx.getDiags().diagnose(SourceLoc(), diag::protocol_req_added,
                                     D->getScreenInfo());
         }
