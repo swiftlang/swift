cbf174d7f6677a4fff1001b7215e6e8a0b32e6d2
Nate Cook <natecook@apple.com>
2018-10-03 01:12:48 -0500

    [stdlib] Add stable sort to mutable collections
    This switches the standard library's sort algorithm from an in-place
    introsort to use a modified timsort, a stable, adaptive sort that
    merges runs using a temporary buffer. This implementation performs
    straight merges instead of adopting timsort's galloping strategy.
    
    In addition to maintaining the relative order of equal/non-comparable
    elements, this algorithm outperforms the introsort on data with any
    intrinsic structure, such as runs of ascending or descending elements
    or a significant number of equality collisions.

--- stdlib/public/core/Sort.swift
+++ stdlib/public/core/Sort.swift
@@ -249,64 +249,443 @@ extension MutableCollection where Self: RandomAccessCollection {
   ) rethrows {
     let didSortUnsafeBuffer = try _withUnsafeMutableBufferPointerIfSupported {
       buffer -> Void? in
-        try buffer.sort(by: areInIncreasingOrder)
+        try buffer._stableSortImpl(by: areInIncreasingOrder)
     }
     if didSortUnsafeBuffer == nil {
-      try _introSort(within: startIndex..<endIndex, by: areInIncreasingOrder)
+      try _stableSortImpl(by: areInIncreasingOrder)
     }
   }
 }
 
 extension MutableCollection where Self: BidirectionalCollection {
+  /// Sorts `self[range]` according to `areInIncreasingOrder`. Stable.
+  ///
+  /// - Precondition: `sortedEnd != range.lowerBound`
+  /// - Precondition: `elements[..<sortedEnd]` are already in order.
   @inlinable
   internal mutating func _insertionSort(
-    within range: Range<Index>, 
+    within range: Range<Index>,
+    sortedEnd: Index,
     by areInIncreasingOrder: (Element, Element) throws -> Bool
   ) rethrows {
-
-    guard !range.isEmpty else { return }
-
-    let start = range.lowerBound
-
-    // Keep track of the end of the initial sequence of sorted elements. One
-    // element is trivially already-sorted, thus pre-increment Continue until
-    // the sorted elements cover the whole sequence
-    var sortedEnd = index(after: start)
-
+    var sortedEnd = sortedEnd
+    
+    // Continue sorting until the sorted elements cover the whole sequence.
     while sortedEnd != range.upperBound {
-      // get the first unsorted element
-      // FIXME: by stashing the element, instead of using indexing and swapAt,
-      // this method won't work for collections of move-only types.
-      let x = self[sortedEnd]
-
-      // Look backwards for x's position in the sorted sequence,
-      // moving elements forward to make room.
       var i = sortedEnd
+      // Look backwards for `self[i]`'s position in the sorted sequence,
+      // moving each element forward to make room.
       repeat {
         let j = index(before: i)
-        let predecessor = self[j]
-
-        // If closure throws, put the element at right place and rethrow.
-        do {
-          // if x doesn't belong before y, we've found its position
-          if try !areInIncreasingOrder(x, predecessor) {
-            break
-          }
-        } catch {
-          self[i] = x
-          throw error
+        
+        // If `self[i]` doesn't belong before `self[j]`, we've found
+        // its position.
+        if try !areInIncreasingOrder(self[i], self[j]) {
+          break
         }
-
-        // Move y forward
-        self[i] = predecessor
+        
+        // Swap the elements at `i` and `j`.
+        swapAt(i, j)
         i = j
-      } while i != start
-
-      if i != sortedEnd {
-        // Plop x into position
-        self[i] = x
-      }
+      } while i != range.lowerBound
+      
       formIndex(after: &sortedEnd)
+    }
+  }
+  
+  /// Sorts `self[range]` according to `areInIncreasingOrder`. Stable.
+  @inlinable
+  internal mutating func _insertionSort(
+    within range: Range<Index>,
+    by areInIncreasingOrder: (Element, Element) throws -> Bool
+  ) rethrows {
+    if range.isEmpty {
+      return
+    }
+    
+    // One element is trivially already-sorted, so the actual sort can
+    // start on the second element.
+    let sortedEnd = index(after: range.lowerBound)
+    try _insertionSort(
+      within: range, sortedEnd: sortedEnd, by: areInIncreasingOrder)
+  }
+  
+  /// Reverses the elements in the given range.
+  @inlinable
+  internal mutating func _reverse(
+    within range: Range<Index>
+  ) {
+    var f = range.lowerBound
+    var l = range.upperBound
+    while f < l {
+      formIndex(before: &l)
+      swapAt(f, l)
+      formIndex(after: &f)
+    }
+  }
+}
+
+/// Merges the elements in the ranges `lo..<mid` and `mid..<hi` using `buffer`
+/// as out-of-place storage. Stable.
+///
+/// - Precondition: `lo..<mid` and `mid..<hi` must already be sorted according
+///   to `areInIncreasingOrder`.
+/// - Precondition: `buffer` must point to a region of memory at least as large
+///   as `min(mid - lo, hi - mid)`.
+/// - Postcondition: `lo..<hi` is sorted according to `areInIncreasingOrder`.
+@inlinable
+internal func _merge<Element>(
+  low: UnsafeMutablePointer<Element>,
+  mid: UnsafeMutablePointer<Element>,
+  high: UnsafeMutablePointer<Element>,
+  buffer: UnsafeMutablePointer<Element>,
+  by areInIncreasingOrder: (Element, Element) throws -> Bool
+) rethrows {
+  let lowCount = mid - low
+  let highCount = high - mid
+  
+  var destLow = low         // Lower bound of uninitialized storage
+  var bufferLow = buffer    // Lower bound of the initialized buffer
+  var bufferHigh = buffer   // Upper bound of the initialized buffer
+
+  // When we exit the merge, move any remaining elements from the buffer back
+  // into `destLow` (aka the collection we're sorting). The buffer can have
+  // remaining elements if `areIncreasingOrder` throws, or more likely if the
+  // merge runs out of elements from the array before exhausting the buffer.
+  defer {
+    destLow.moveInitialize(from: bufferLow, count: bufferHigh - bufferLow)
+  }
+  
+  if lowCount < highCount {
+    // Move the lower group of elements into the buffer, then traverse from
+    // low to high in both the buffer and the higher group of elements.
+    //
+    // After moving elements, the storage and buffer look like this, where
+    // `x` is uninitialized memory:
+    //
+    // Storage: [x, x, x, x, x, 6, 8, 8, 10, 12, 15]
+    //           ^              ^
+    //        destLow        srcLow
+    //
+    // Buffer:  [4, 4, 7, 8, 9, x, ...]
+    //           ^              ^
+    //        bufferLow     bufferHigh
+    buffer.moveInitialize(from: low, count: lowCount)
+    bufferHigh = bufferLow + lowCount
+    
+    var srcLow = mid
+
+    // Each iteration moves the element that compares lower into `destLow`,
+    // preferring the buffer when equal to maintain stability. Elements are
+    // moved from either `bufferLow` or `srcLow`, with those pointers
+    // incrementing as elements are moved.
+    while bufferLow < bufferHigh && srcLow < high {
+      if try areInIncreasingOrder(srcLow.pointee, bufferLow.pointee) {
+        destLow.moveInitialize(from: srcLow, count: 1)
+        srcLow += 1
+      } else {
+        destLow.moveInitialize(from: bufferLow, count: 1)
+        bufferLow += 1
+      }
+      destLow += 1
+    }
+  } else {
+    // Move the higher group of elements into the buffer, then traverse from
+    // high to low in both the buffer and the lower group of elements.
+    //
+    // After moving elements, the storage and buffer look like this, where
+    // `x` is uninitialized memory:
+    //
+    // Storage: [4, 4, 7, 8, 9, 6, x, x,  x,  x,  x]
+    //                          ^  ^                 ^
+    //                    srcHigh  destLow        destHigh (past the end)
+    //
+    // Buffer:                    [8, 8, 10, 12, 15, x, ...]
+    //                             ^                 ^
+    //                          bufferLow        bufferHigh
+    buffer.moveInitialize(from: mid, count: highCount)
+    bufferHigh = bufferLow + highCount
+    
+    var destHigh = high
+    var srcHigh = mid
+    destLow = mid
+
+    // Each iteration moves the element that compares higher into `destHigh`,
+    // preferring the buffer when equal to maintain stability. Elements are
+    // moved from either `bufferHigh - 1` or `srcHigh - 1`, with those
+    // pointers decrementing as elements are moved.
+    //
+    // Note: At the start of each iteration, each `...High` pointer points one
+    // past the element they're referring to.
+    while bufferHigh > bufferLow && srcHigh > low {
+      destHigh -= 1
+      if try areInIncreasingOrder(
+        (bufferHigh - 1).pointee, (srcHigh - 1).pointee
+      ) {
+        srcHigh -= 1
+        destHigh.moveInitialize(from: srcHigh, count: 1)
+        
+        // Moved an element from the lower initialized portion to the upper,
+        // sorted, initialized portion, so `destLow` moves down one.
+        destLow -= 1
+      } else {
+        bufferHigh -= 1
+        destHigh.moveInitialize(from: bufferHigh, count: 1)
+      }
+    }
+  }
+}
+
+/// Calculates an optimal minimum run length for sorting a collection.
+///
+/// "... pick a minrun in range(32, 65) such that N/minrun is exactly a power
+/// of 2, or if that isn't possible, is close to, but strictly less than, a
+/// power of 2. This is easier to do than it may sound: take the first 6 bits
+/// of N, and add 1 if any of the remaining bits are set."
+/// - From the Timsort introduction, at
+///   https://svn.python.org/projects/python/trunk/Objects/listsort.txt
+///
+/// - Parameter c: The number of elements in a collection.
+/// - Returns: If `c <= 64`, returns `c`. Otherwise, returns a value in
+///   `32...64`.
+@inlinable
+internal func _minimumMergeRunLength(_ c: Int) -> Int {
+  // Max out at `2^6 == 64` elements
+  let bitsToUse = 6
+  
+  if c < 1 << bitsToUse {
+    return c
+  }
+  let offset = (MemoryLayout<Int>.size * 8 - bitsToUse) - c.leadingZeroBitCount
+  let mask = (1 << offset) - 1
+  return c >> offset + (c & mask == 0 ? 0 : 1)
+}
+
+/// Returns the end of the next in-order run along with a Boolean value
+/// indicating whether the elements in `start..<end` are in increasing order.
+@inlinable
+internal func _findNextRun<C: RandomAccessCollection>(
+  in elements: C,
+  from start: C.Index,
+  by areInIncreasingOrder: (C.Element, C.Element) throws -> Bool
+) rethrows -> (end: C.Index, ascending: Bool) {
+  var next = start
+  if next < elements.endIndex {
+    var current = next
+    elements.formIndex(after: &next)
+    if try next < elements.endIndex &&
+      areInIncreasingOrder(elements[next], elements[current])
+    {
+      // The elements of this run are in descending order. Equal elements
+      // cannot be included in the run, since the run will be reversed, which
+      // would break stability.
+      repeat {
+        current = next
+        elements.formIndex(after: &next)
+      } while try next < elements.endIndex &&
+        areInIncreasingOrder(elements[next], elements[current])
+      
+      return(next, false)
+    } else if next < elements.endIndex {
+      // The elements in this run are in ascending order. Equal elements can
+      // be included in the run.
+      repeat {
+        current = next
+        elements.formIndex(after: &next)
+      } while try next < elements.endIndex &&
+        !areInIncreasingOrder(elements[next], elements[current])
+    }
+  }
+  return (next, true)
+}
+
+extension UnsafeMutableBufferPointer {
+  /// Merges the elements at `runs[i]` and `runs[i - 1]`, using `buffer` as
+  /// out-of-place storage.
+  ///
+  /// - Precondition: `runs.count > 1` and `i > 0`
+  /// - Precondition: `buffer` must have at least
+  ///   `min(runs[i].count, runs[i - 1].count)` uninitialized elements.
+  @inlinable
+  public mutating func _mergeRuns(
+    _ runs: inout [Range<Index>],
+    at i: Int,
+    buffer: UnsafeMutablePointer<Element>,
+    by areInIncreasingOrder: (Element, Element) throws -> Bool
+  ) rethrows {
+    precondition(i > 0)
+    let low = runs[i - 1].lowerBound
+    precondition(runs[i - 1].upperBound == runs[i].lowerBound)
+    let middle = runs[i].lowerBound
+    let high = runs[i].upperBound
+    
+    try _merge(
+      low: baseAddress! + low,
+      mid: baseAddress! + middle,
+      high: baseAddress! + high,
+      buffer: buffer,
+      by: areInIncreasingOrder)
+    
+    runs[i - 1] = low..<high
+    runs.remove(at: i)
+  }
+  
+  /// Merges upper elements of `runs` until the required invariants are
+  /// satisfied.
+  ///
+  /// - Precondition: `buffer` must have at least
+  ///   `min(runs[i].count, runs[i - 1].count)` uninitialized elements.
+  /// - Precondition: The ranges in `runs` must be consecutive, such that for
+  ///   any i, `runs[i].upperBound == runs[i + 1].lowerBound`.
+  @inlinable
+  public mutating func _mergeTopRuns(
+    _ runs: inout [Range<Index>],
+    buffer: UnsafeMutablePointer<Element>,
+    by areInIncreasingOrder: (Element, Element) throws -> Bool
+  ) rethrows {
+    // The invariants for the `runs` array are:
+    // (a) - for all i in 2..<runs.count:
+    //         - runs[i - 2].count > runs[i - 1].count + runs[i].count
+    // (b) - for c = runs.count - 1:
+    //         - runs[i - 1].count > runs[i].count
+    //
+    // Loop until the invariant is satisified for the top four elements of
+    // `runs`. Because this method is called for every added run, and only
+    // the top three runs are ever merged, this guarantees the invariant holds
+    // for the whole array.
+    //
+    // At all times, `runs` is one of the following, where W, X, Y, and Z are
+    // the counts of their respective ranges:
+    // - [ ...?, W, X, Y, Z ]
+    // - [ X, Y, Z ]
+    // - [ Y, Z ]
+    //
+    // If W > X + Y, X > Y + Z, and Y > Z, then the invariants are satisfied
+    // for the entirety of `runs`.
+    
+    // The invariant is always in place for a single element.
+    while runs.count > 1 {
+      var lastIndex = runs.count - 1
+      
+      if lastIndex >= 3 &&
+        (runs[lastIndex - 3].count <=
+          runs[lastIndex - 2].count + runs[lastIndex - 1].count)
+      {
+        // Second-to-last three runs do not follow W > X + Y.
+        // Always merge Y with the smaller of X or Z.
+        if runs[lastIndex - 2].count < runs[lastIndex].count {
+          lastIndex -= 1
+        }
+      } else if lastIndex >= 2 &&
+        (runs[lastIndex - 2].count <=
+          runs[lastIndex - 1].count + runs[lastIndex].count)
+      {
+        // Last three runs do not follow X > Y + Z.
+        // Always merge Y with the smaller of X or Z.
+        if runs[lastIndex - 2].count < runs[lastIndex].count {
+          lastIndex -= 1
+        }
+      } else if runs[lastIndex - 1].count <= runs[lastIndex].count {
+        // Last two runs do not follow Y > Z.
+        // Merge Y and Z.
+      } else {
+        // All invariants satisfied!
+        break
+      }
+      
+      // Merge the runs at `i` and `i - 1`.
+      try _mergeRuns(
+        &runs, at: lastIndex, buffer: buffer, by: areInIncreasingOrder)
+    }
+  }
+  
+  /// Merges elements of `runs` until only one run remains.
+  ///
+  /// - Precondition: `buffer` must have at least
+  ///   `min(runs[i].count, runs[i - 1].count)` uninitialized elements.
+  /// - Precondition: The ranges in `runs` must be consecutive, such that for
+  ///   any i, `runs[i].upperBound == runs[i + 1].lowerBound`.
+  @inlinable
+  public mutating func _finalizeRuns(
+    _ runs: inout [Range<Index>],
+    buffer: UnsafeMutablePointer<Element>,
+    by areInIncreasingOrder: (Element, Element) throws -> Bool
+  ) rethrows {
+    while runs.count > 1 {
+      try _mergeRuns(
+        &runs, at: runs.count - 1, buffer: buffer, by: areInIncreasingOrder)
+    }
+  }
+  
+  /// Sorts the elements of this buffer according to `areInIncreasingOrder`,
+  /// using a stable, adaptive merge sort.
+  ///
+  /// The adaptive algorithm used is Timsort, modified to perform a straight
+  /// merge of the elements using a temporary buffer.
+  @inlinable
+  public mutating func _stableSortImpl(
+    by areInIncreasingOrder: (Element, Element) throws -> Bool
+  ) rethrows {
+    let minimumRunLength = _minimumMergeRunLength(count)
+    if count <= minimumRunLength {
+      try _insertionSort(
+        within: startIndex..<endIndex, by: areInIncreasingOrder)
+      return
+    }
+    
+    // Use array's allocating initializer to create a temporary buffer---this
+    // keeps the buffer allocation going through the same tail-allocated path
+    // as other allocating methods.
+    //
+    // There's no need to set the initialized count within the initializing
+    // closure, since the buffer is guaranteed to be uninitialized at exit.
+    _ = try Array<Element>(_unsafeUninitializedCapacity: count / 2) {
+      buffer, _ in
+      var runs: [Range<Index>] = []
+      
+      var start = startIndex
+      while start < endIndex {
+        // Find the next consecutive run, reversing it if necessary.
+        var (end, ascending) =
+          try _findNextRun(in: self, from: start, by: areInIncreasingOrder)
+        if !ascending {
+          _reverse(within: start..<end)
+        }
+        
+        // If the current run is shorter than the minimum length, use the
+        // insertion sort to extend it.
+        if end < endIndex && end - start < minimumRunLength {
+          let newEnd = Swift.min(endIndex, start + minimumRunLength)
+          try _insertionSort(
+            within: start..<newEnd, sortedEnd: end, by: areInIncreasingOrder)
+          end = newEnd
+        }
+        
+        // Append this run and merge down as needed to maintain the `runs`
+        // invariants.
+        runs.append(start..<end)
+        try _mergeTopRuns(
+          &runs, buffer: buffer.baseAddress!, by: areInIncreasingOrder)
+        start = end
+      }
+      
+      try _finalizeRuns(
+        &runs, buffer: buffer.baseAddress!, by: areInIncreasingOrder)
+      assert(runs.count == 1, "Didn't complete final merge")
+    }
+  }
+}
+
+extension MutableCollection {
+  /// This unconstrained default implementation is a fallback stable sort that
+  /// sorts into an outside array, then copies elements back in.
+  @inlinable
+  public mutating func _stableSortImpl(
+    by areInIncreasingOrder: (Element, Element) throws -> Bool
+  ) rethrows {
+    let sortedElements = try sorted(by: areInIncreasingOrder)
+    for (i, j) in zip(indices, sortedElements.indices) {
+      self[i] = sortedElements[j]
     }
   }
 }
@@ -402,6 +781,9 @@ extension MutableCollection where Self: RandomAccessCollection {
     let half = distance(from: lo, to: hi) / 2
     let mid = index(lo, offsetBy: half)
     try _sort3(lo, mid, hi, by: areInIncreasingOrder)
+    
+    // FIXME: Stashing the pivot element instead of using the index won't work
+    // for move-only types.
     let pivot = self[mid]
 
     // Loop invariants:
