48c6cb40cf646b3159db2b28165ddf98ab152130
Mark Lacey <mark.lacey@apple.com>
2018-10-03 16:53:04 -0700

    Fix release-build test failure due to unaccounted-for warning.

--- validation-test/Sema/type_checker_perf/fast/rdar35213699.swift
+++ validation-test/Sema/type_checker_perf/fast/rdar35213699.swift
@@ -2,6 +2,6 @@
 // REQUIRES: tools-release,no_asserts
 
 func test() {
-  let x: UInt = 1 * 2 + 3 * 4 + 5 * 6 + 7 * 8 + 9 * 10 + 11 * 12 + 13 * 14
+  let _: UInt = 1 * 2 + 3 * 4 + 5 * 6 + 7 * 8 + 9 * 10 + 11 * 12 + 13 * 14
 }
 
