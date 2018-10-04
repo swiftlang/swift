9ca89e496c6477fd8ce3a41871ec1a3524095b5c
mzp <mzpppp@gmail.com>
2018-10-04 11:57:12 +0900

    fixup! [Sema] Add fix-it for property in protocol

--- test/decl/var/lazy_properties.swift
+++ test/decl/var/lazy_properties.swift
@@ -10,9 +10,8 @@ struct S {
 
 protocol SomeProtocol {
   lazy var x : Int  // expected-error {{'lazy' isn't allowed on a protocol requirement}} {{3-8=}}
-  // expected-error@-1 {{property in protocol must have explicit { get } or { get set } specifier}}
+  // expected-error@-1 {{property in protocol must have explicit { get } or { get set } specifier}} {{19-19= { get <#set#> \}}}
   // expected-error@-2 {{lazy properties must have an initializer}}
-  // {{18-18= { get set \}}}
   lazy var y : Int { get } // expected-error {{'lazy' isn't allowed on a protocol requirement}} {{3-8=}}
   // expected-error@-1 {{'lazy' must not be used on a computed property}}
   // expected-error@-2 {{lazy properties must have an initializer}}
