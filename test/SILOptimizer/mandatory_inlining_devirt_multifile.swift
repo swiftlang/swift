bc2dea5ae7892fb2876e2e97df36bad35b7ed2d0
John McCall <rjmccall@apple.com>
2018-10-09 19:46:45 -0400

    Walk through subclasses in other files in class hierarchy analysis.
    rdar://45110471

---
+++ test/SILOptimizer/mandatory_inlining_devirt_multifile.swift
@@ -0,0 +1,21 @@
+// RUN: %target-swift-frontend -enable-sil-ownership -module-name test -primary-file %s %S/Inputs/mandatory_inlining_devirt_other.swift -emit-sil -o - | %FileCheck %s
+
+// rdar://45110471
+
+// We only generate SIL v-tables for these two classes in this file, but
+// class hierarchy analysis needs to walk through the third class in the
+// helper file.
+
+class Base {
+  fileprivate func foo() { print("base") }
+  func callit() { foo() }
+}
+
+class Derived : Middle {
+  fileprivate override func foo() { print("derived") }
+}
+
+// CHECK-LABEL: sil hidden @$s4test4BaseC6callityyF
+// CHECK:         [[T0:%.*]] = class_method %0 : $Base, #Base.foo!1
+// CHECK-NEXT:    apply [[T0]](%0)
+// CHECK-LABEL: } // end sil function '$s4test4BaseC6callityyF'
