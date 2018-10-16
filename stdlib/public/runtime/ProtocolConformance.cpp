f66a3c1046feb4e6d802571dc65d05b3467e4180
Doug Gregor <dgregor@apple.com>
2018-10-16 16:02:51 -0700

    [Runtime] Use CF superclass metadata in runtime lookups.
    When searching the superclasses at runtime, e.g., to find a suitable
    protocol conformance record, also consider the superclasses of CF
    types, which were recorded in the metadata but otherwise unused.
    
    (cherry picked from commit c5e226bb16cf8c9dcf634f1a27c55c940d71d5f4)

--- stdlib/public/runtime/ProtocolConformance.cpp
+++ stdlib/public/runtime/ProtocolConformance.cpp
@@ -463,12 +463,10 @@ recur:
     }
   }
 
-  // If the type is a class, try its superclass.
-  if (const ClassMetadata *classType = type->getClassObject()) {
-    if (classHasSuperclass(classType)) {
-      type = getMetadataForClass(classType->Superclass);
-      goto recur;
-    }
+  // If there is a superclass, look there.
+  if (auto superclass = _swift_class_getSuperclass(type)) {
+    type = superclass;
+    goto recur;
   }
 
   // We did not find an up-to-date cache entry.
@@ -506,12 +504,10 @@ bool isRelatedType(const Metadata *type, const void *candidate,
         return true;
     }
 
-    // If the type is a class, try its superclass.
-    if (const ClassMetadata *classType = type->getClassObject()) {
-      if (classHasSuperclass(classType)) {
-        type = getMetadataForClass(classType->Superclass);
-        continue;
-      }
+    // If there is a superclass, look there.
+    if (auto superclass = _swift_class_getSuperclass(type)) {
+      type = superclass;
+      continue;
     }
 
     break;
