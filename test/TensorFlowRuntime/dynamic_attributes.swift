e7fa0d0be13c7fb69b484bde578ca568bd4bcd18
Marc Rasi <marcrasi@google.com>
2018-10-16 11:08:50 -0700

    make the test not use python

--- test/TensorFlowRuntime/dynamic_attributes.swift
+++ test/TensorFlowRuntime/dynamic_attributes.swift
@@ -6,49 +6,31 @@
 import TensorFlow
 import TensorFlowUnittest
 import StdlibUnittest
-import Python
 
 var DynamicAttributeTests = TestSuite("DynamicAttribute")
 
-/// Loads an attribute value from a file. This ensures that this test actually
-/// exercises dynamic attributes.
-func loadAttribute<T: PythonConvertible>(_ key: String) -> T {
-  let attributesFilenameIndex =
-      CommandLine.arguments.firstIndex(of: "--attributes-json")! + 1
-  let attributesFilename = CommandLine.arguments[attributesFilenameIndex]
-  let json = Python.import("json")
-  let f = Python.open(attributesFilename, "r")
-  defer { f.close() }
-  return T(json.load(f)[PythonObject(key)])!
+// These global vars with @inline(never) loaders ensure that the compiler
+// can't find the constant value of the attributes.
+
+var dtypeInt32 = Int32.tensorFlowDataType
+@inline(never)
+func loadDtypeInt32() -> TensorDataType {
+  return dtypeInt32
 }
 
-extension TensorDataType : PythonConvertible {
-  public var pythonObject: PythonObject {
-    fatalError("unimplemented")
-  }
-
-  public init?(_ pythonObject: PythonObject) {
-    guard let str = String(pythonObject) else { return nil }
-    switch str {
-    case "Double":
-      self = Double.tensorFlowDataType
-    case "Int32":
-      self = Int32.tensorFlowDataType
-    default:
-      return nil
-    }
-  }
+var dtypeDouble = Double.tensorFlowDataType
+@inline(never)
+func loadDtypeDouble() -> TensorDataType {
+  return dtypeDouble
 }
 
 DynamicAttributeTests.test("single TFDataTypeAttribute") {
   let t1 = Tensor<Int32>(-1)
-  let t1Result: Tensor<Int32> = #tfop(
-    "Abs", t1, T$dtype: loadAttribute("dtype_Int32") as TensorDataType)
+  let t1Result: Tensor<Int32> = #tfop("Abs", t1, T$dtype: loadDtypeInt32())
   expectEqual(1, t1Result.scalar!)
 
   let t2 = Tensor<Double>(-2)
-  let t2Result: Tensor<Double> = #tfop(
-    "Abs", t2, T$dtype: loadAttribute("dtype_Double") as TensorDataType)
+  let t2Result: Tensor<Double> = #tfop("Abs", t2, T$dtype: loadDtypeDouble())
   expectEqual(2, t2Result.scalar!)
 }
 
