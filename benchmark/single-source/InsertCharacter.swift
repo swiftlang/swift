f886075673f8688fc1c39fb7eda84d8835a71ceb
Patrick Balestra <me@patrickbalestra.com>
2018-10-23 08:52:38 +0200

    Fix indentation level

--- benchmark/single-source/InsertCharacter.swift
+++ benchmark/single-source/InsertCharacter.swift
@@ -13,86 +13,86 @@
 import TestsUtils
 
 public let InsertCharacter = [
-    BenchmarkInfo(name: "InsertCharacterEndIndex", runFunction: run_InsertCharacterEndIndex, tags: [.validation, .api], setUpFunction: buildWorkload),
-    BenchmarkInfo(name: "InsertCharacterTowardsEndIndex", runFunction: run_InsertCharacterTowardsEndIndex, tags: [.validation, .api], setUpFunction: buildWorkload),
-    BenchmarkInfo(name: "InsertCharacterStartIndex", runFunction: run_InsertCharacterStartIndex, tags: [.validation, .api], setUpFunction: buildWorkload),
-    BenchmarkInfo(name: "InsertCharacterEndIndexNonASCII", runFunction: run_InsertCharacterEndIndexNonASCII, tags: [.validation, .api], setUpFunction: buildWorkload),
-    BenchmarkInfo(name: "InsertCharacterTowardsEndIndexNonASCII", runFunction: run_InsertCharacterTowardsEndIndexNonASCII, tags: [.validation, .api], setUpFunction: buildWorkload),
-    BenchmarkInfo(name: "InsertCharacterStartIndexNonASCII", runFunction: run_InsertCharacterStartIndexNonASCII, tags: [.validation, .api], setUpFunction: buildWorkload)
+  BenchmarkInfo(name: "InsertCharacterEndIndex", runFunction: run_InsertCharacterEndIndex, tags: [.validation, .api], setUpFunction: buildWorkload),
+  BenchmarkInfo(name: "InsertCharacterTowardsEndIndex", runFunction: run_InsertCharacterTowardsEndIndex, tags: [.validation, .api], setUpFunction: buildWorkload),
+  BenchmarkInfo(name: "InsertCharacterStartIndex", runFunction: run_InsertCharacterStartIndex, tags: [.validation, .api], setUpFunction: buildWorkload),
+  BenchmarkInfo(name: "InsertCharacterEndIndexNonASCII", runFunction: run_InsertCharacterEndIndexNonASCII, tags: [.validation, .api], setUpFunction: buildWorkload),
+  BenchmarkInfo(name: "InsertCharacterTowardsEndIndexNonASCII", runFunction: run_InsertCharacterTowardsEndIndexNonASCII, tags: [.validation, .api], setUpFunction: buildWorkload),
+  BenchmarkInfo(name: "InsertCharacterStartIndexNonASCII", runFunction: run_InsertCharacterStartIndexNonASCII, tags: [.validation, .api], setUpFunction: buildWorkload)
 ]
 
 let str = String(repeating: "A very long ASCII string.", count: 200)
 
 func buildWorkload() {
-    blackHole(str)
+  blackHole(str)
 }
 
 // Insert towards end index
 
 @inline(__always)
 func insertTowardsEndIndex(_ c: Character, in string: String, count: Int) {
-	var workload = string
-	var index = workload.endIndex
-	for i in 0..<count {
-		workload.insert(identity(c), at: index)
-		if i % 1000 == 0 {
-			index = workload.endIndex
-		}
-	}
-	blackHole(workload)
+  var workload = string
+  var index = workload.endIndex
+  for i in 0..<count {
+    workload.insert(identity(c), at: index)
+    if i % 1000 == 0 {
+      index = workload.endIndex
+    }
+  }
+  blackHole(workload)
 }
 
 @inline(never)
 func run_InsertCharacterTowardsEndIndex(_ N: Int) {
-	insertTowardsEndIndex("s", in: str, count: N * 3000)
+  insertTowardsEndIndex("s", in: str, count: N * 3000)
 }
 
 @inline(never)
 func run_InsertCharacterTowardsEndIndexNonASCII(_ N: Int) {
-	insertTowardsEndIndex("👩🏼‍💻", in: str, count: N * 1000)
+  insertTowardsEndIndex("👩🏼‍💻", in: str, count: N * 1000)
 }
 
 // Insert at end index
 
 @inline(__always)
 func insertAtEndIndex(_ c: Character, in string: String, count: Int) {
-	var workload = string
-	for _ in 0..<count {
-		workload.insert(identity(c), at: workload.endIndex)
-	}
-	blackHole(workload)
+  var workload = string
+  for _ in 0..<count {
+    workload.insert(identity(c), at: workload.endIndex)
+  }
+  blackHole(workload)
 }
 
 @inline(never)
 func run_InsertCharacterEndIndex(_ N: Int) {
-	insertAtEndIndex("s", in: str, count: N * 3000)
+  insertAtEndIndex("s", in: str, count: N * 3000)
 }
 
 @inline(never)
 func run_InsertCharacterEndIndexNonASCII(_ N: Int) {
-	insertAtEndIndex("👩🏾‍🏫", in: str, count: N * 1000)
+  insertAtEndIndex("👩🏾‍🏫", in: str, count: N * 1000)
 }
 
 // Insert at start index
 
 @inline(__always)
 func insertAtStartIndex(_ c: Character, in string: String, count: Int, insertions: Int) {
-	var workload = str
-    for _ in 0..<count {
-        for _ in 0..<insertions {
-            workload.insert(identity(c), at: workload.startIndex)	
-        }
-        workload = str
+  var workload = str
+  for _ in 0..<count {
+    for _ in 0..<insertions {
+      workload.insert(identity(c), at: workload.startIndex)
     }
-    blackHole(workload)
+    workload = str
+  }
+  blackHole(workload)
 }
 
 @inline(never)
 func run_InsertCharacterStartIndex(_ N: Int) {
-	insertAtStartIndex("w", in: str, count: N * 75, insertions: 50)
+  insertAtStartIndex("w", in: str, count: N * 75, insertions: 50)
 }
 
 @inline(never)
 func run_InsertCharacterStartIndexNonASCII(_ N: Int) {
-	insertAtStartIndex("👩🏾‍🏫", in: str, count: N * 75, insertions: 25)
+  insertAtStartIndex("👩🏾‍🏫", in: str, count: N * 75, insertions: 25)
 }
