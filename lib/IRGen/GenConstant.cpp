a9bdc07d115cb744f04d024dafea0161a3691745
Erik Eckstein <eeckstein@apple.com>
2018-10-25 10:12:00 -0700

    SILOptimizer: support static large string literal generation for the new UTF8 String implementation
    In the new string implementation there is a subtract of a (small) constant from the literal pointer. We convert
    
    ((ptr - offset) | bits
    
    to
    
    (ptr + (bits - offset))
    
    which can still be represented as a relocation.

--- lib/IRGen/GenConstant.cpp
+++ lib/IRGen/GenConstant.cpp
@@ -94,11 +94,23 @@ static llvm::Constant *emitConstantValue(IRGenModule &IGM, SILValue operand) {
         return llvm::ConstantExpr::getZExtOrBitCast(value, IGM.getStorageType(BI->getType()));
       }
       case BuiltinValueKind::StringObjectOr: {
-        llvm::Constant *lhs = emitConstantValue(IGM, BI->getArguments()[0]);
-        llvm::Constant *rhs = emitConstantValue(IGM, BI->getArguments()[1]);
         // It is a requirement that the or'd bits in the left argument are
         // initialized with 0. Therefore the or-operation is equivalent to an
         // addition. We need an addition to generate a valid relocation.
+        llvm::Constant *rhs = emitConstantValue(IGM, BI->getArguments()[1]);
+        if (auto *TE = dyn_cast<TupleExtractInst>(BI->getArguments()[0])) {
+          // Handle StringObjectOr(tuple_extract(usub_with_overflow(x, offset)), bits)
+          // This pattern appears in UTF8 String literal construction.
+          // Generate the equivalent: add(x, sub(bits - offset)
+          BuiltinInst *SubtrBI =
+            SILGlobalVariable::getOffsetSubtract(TE, IGM.getSILModule());
+          assert(SubtrBI && "unsupported argument of StringObjectOr");
+          auto *ptr = emitConstantValue(IGM, SubtrBI->getArguments()[0]);
+          auto *offset = emitConstantValue(IGM, SubtrBI->getArguments()[1]);
+          auto *totalOffset = llvm::ConstantExpr::getSub(rhs, offset);
+          return llvm::ConstantExpr::getAdd(ptr, totalOffset);
+        }
+        llvm::Constant *lhs = emitConstantValue(IGM, BI->getArguments()[0]);
         return llvm::ConstantExpr::getAdd(lhs, rhs);
       }
       default:
