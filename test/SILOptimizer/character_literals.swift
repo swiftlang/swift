e6582c37eec7b4a27b2800aec234070b415f89d4
Michael Ilseman <milseman@apple.com>
2018-10-28 17:15:17 -0700

    [test] Adjust String tests for UTF-8 representation.
    Adjust tests for the UTF-8 representation, in preparation for 32-bit
    support. Includes UTF-8 literal update.

--- test/SILOptimizer/character_literals.swift
+++ test/SILOptimizer/character_literals.swift
@@ -17,21 +17,30 @@ public func charArray(_ i: Int) -> [Character] {
   return [ "a", "b", "c", "d" ]
 }
 
+// NOTE: 97 = 'a'
+// NOTE: -2233785415175766016 = 0xE1 = 0xE0 (ASCII discrim) | 0x01 (count)
+//
 // CHECK-LABEL: define {{.*}}singleChar
-// CHECK: tail call swiftcc {{.*}}singleChar
-// CHECK-LABEL: define {{.*}}singleNonAsciiChar
-// CHECK: tail call swiftcc {{.*}}singleNonAsciiChar
-
-// CHECK-LABEL: define linkonce_odr hidden {{.*}}singleChar
-// CHECK: ret {{.*}} 97
+// CHECK-NEXT: ret { i64, %swift.bridge* } { i64 97, %swift.bridge* inttoptr (i64 -2233785415175766016 to %swift.bridge*) }
 public func singleChar() -> Character {
   return "a"
 }
 
-// NOTE: 10852326 is base-10 little-endian for E6 97 A5, the encoding of U+65E5
+// NOTE: 10852326 = 0xE6 0x97 0xA5 (little endian), the encoding of U+65E5
+// NOTE: -6701356245527298048 = 0xA3 = 0xA0 (non-ASCII discrim) | 0x03 (count)
 //
-// CHECK-LABEL: define linkonce_odr hidden {{.*}}singleNonAsciiChar
-// CHECK: ret {{.*}} 10852326
+// CHECK-LABEL: define {{.*}}singleNonAsciiChar
+// CHECK-NEXT: ret { i64, %swift.bridge* } { i64 10852326, %swift.bridge* inttoptr (i64 -6701356245527298048 to %swift.bridge*) }
 public func singleNonAsciiChar() -> Character {
   return "日"
 }
+
+// NOTE: -9223372036854775808 = 0x80 = immortal large discrim
+// NOTE: 25 = length in UTF-8 code units
+//
+// CHECK-LABEL: define {{.*}}singleNonSmolChar
+// CHECK:   ret { i64, %swift.bridge* } { i64 25, %swift.bridge* {{.*}}@0{{.*}}i64 -9223372036854775808
+public func singleNonSmolChar() -> Character {
+  return "👩‍👩‍👦‍👦"
+}
+
--- validation-test/Reflection/reflect_Character.swift
+++ validation-test/Reflection/reflect_Character.swift
@@ -24,16 +24,24 @@ reflect(object: obj)
 // CHECK-64: Type reference:
 // CHECK-64: (class reflect_Character.TestClass)
 
-// CHECK-64: Type info:
-// CHECK-64: (class_instance size=24 alignment=8 stride=24 num_extra_inhabitants=0
-// CHECK-64-NEXT:  (field name=t offset=16
-// CHECK-64-NEXT:    (struct size=8 alignment=8 stride=8 num_extra_inhabitants=0
-// CHECK-64-NEXT:      (field name=_representation offset=0
-// CHECK-64-NEXT:        (multi_payload_enum size=8 alignment=8 stride=8 num_extra_inhabitants=0
-// CHECK-64-NEXT:          (field name=smallUTF16 offset=0
-// CHECK-64-NEXT:            (builtin size=8 alignment=8 stride=8 num_extra_inhabitants=2147483647))
-// CHECK-64-NEXT:          (field name=large offset=0
-// CHECK-64-NEXT:            (reference kind=strong refcounting=native)))))))
+// CHECK-64-LABEL: Type info:
+// CHECK-64: (class_instance size=32 alignment=8 stride=32 num_extra_inhabitants=0
+// CHECK-64-NEXT: (field name=t offset=16
+// CHECK-64-NEXT:   (struct size=16 alignment=8 stride=16 num_extra_inhabitants=1
+// CHECK-64-NEXT:     (field name=_str offset=0
+// CHECK-64-NEXT:       (struct size=16 alignment=8 stride=16 num_extra_inhabitants=1
+// CHECK-64-NEXT:         (field name=_guts offset=0
+// CHECK-64-NEXT:           (struct size=16 alignment=8 stride=16 num_extra_inhabitants=1
+// CHECK-64-NEXT:             (field name=_object offset=0
+// CHECK-64-NEXT:               (struct size=16 alignment=8 stride=16 num_extra_inhabitants=1
+// CHECK-64-NEXT:                 (field name=_countAndFlags offset=0
+// CHECK-64-NEXT:                   (struct size=8 alignment=8 stride=8 num_extra_inhabitants=0
+// CHECK-64-NEXT:                     (field name=_storage offset=0
+// CHECK-64-NEXT:                       (struct size=8 alignment=8 stride=8 num_extra_inhabitants=0
+// CHECK-64-NEXT:                         (field name=_value offset=0
+// CHECK-64-NEXT:                           (builtin size=8 alignment=8 stride=8 num_extra_inhabitants=0))))))
+// CHECK-64-NEXT:                 (field name=_object offset=8
+// CHECK-64-NEXT:                   (builtin size=8 alignment=8 stride=8 num_extra_inhabitants=1)))))))))))
 
 // CHECK-32: Reflecting an object.
 // CHECK-32: Type reference:
