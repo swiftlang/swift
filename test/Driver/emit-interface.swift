3ce15d12ad56c7ee928eb1fc62dba65b2dcfac94
Harlan Haskins <harlan@apple.com>
2018-10-31 10:16:45 -0700

    Add test to make sure interfaces get arguments with filelists

--- test/Driver/emit-interface.swift
+++ test/Driver/emit-interface.swift
@@ -19,3 +19,21 @@
 // CHECK-EXPLICIT-PATH: swift -frontend
 // CHECK-EXPLICIT-PATH-SAME: emit-interface.swift
 // CHECK-EXPLICIT-PATH-SAME: -emit-parseable-module-interface-path {{.+}}/unrelated.swiftinterface
+
+// Ensure that we emit arguments when we force filelists as well
+// RUN: %swiftc_driver -driver-print-jobs -target x86_64-apple-macosx10.9 %s -emit-parseable-module-interface -o %t/foo -module-name foo -force-single-frontend-invocation -driver-filelist-threshold=0 2>&1 | %FileCheck -check-prefix=CHECK-FILELIST %s
+
+// CHECK-FILELIST: swift -frontend
+// CHECK-FILELIST-SAME: -supplementary-output-file-map
+// CHECK-FILELIST-NOT: emit-interface.swift{{ }}
+
+
+// Actually run this and make sure the flags show up in the interface
+
+// RUN: %swiftc_driver -target x86_64-apple-macosx10.9 %s -emit-parseable-module-interface -o %t/foo -module-name foo -force-single-frontend-invocation -driver-filelist-threshold=0 2>&1
+// RUN: %FileCheck %s < %t/foo.swiftinterface --check-prefix CHECK-FILELIST-INTERFACE
+
+// CHECK-FILELIST-INTERFACE: swift-module-flags:
+// CHECK-FILELIST-INTERFACE-SAME: -target x86_64-apple-macosx10.9
+// CHECK-FILELIST-INTERFACE-SAME: -module-name foo
+
