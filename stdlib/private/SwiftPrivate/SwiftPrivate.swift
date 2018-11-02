e6fea9e6eddfb40caeaed8961199f6ebdb1550aa
Arthur Guiot <arguiot@gmail.com>
2018-11-02 23:46:19 +0800

    Fixed header in SwiftPrivate.swift

--- stdlib/private/SwiftPrivate/SwiftPrivate.swift
+++ stdlib/private/SwiftPrivate/SwiftPrivate.swift
@@ -2,12 +2,6 @@
 //
 // This source file is part of the Swift.org open source project
 //
-// Copyright (c) 2014 - 2017 Apple Inc. and the Swift project authors
-// Licensed under Apache License v2.0 with Runtime Library Exception
-////===----------------------------------------------------------------------===//
-//
-// This source file is part of the Swift.org open source project
-//
 // Copyright (c) 2014 - 2018 Apple Inc. and the Swift project authors
 // Licensed under Apache License v2.0 with Runtime Library Exception
 //
