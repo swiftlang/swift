ed635e5e7ab4aa6200349ef27636580ac0ff0df1
Xi Ge <xi_ge@apple.com>
2018-11-04 19:20:31 -0800

    [test] disable SILOptimizer/unused_containers.swift to unblock CI. rdar://45797168

--- test/SILOptimizer/unused_containers.swift
+++ test/SILOptimizer/unused_containers.swift
@@ -4,9 +4,9 @@
 
 // FIXME: https://bugs.swift.org/browse/SR-7806
 // REQUIRES: CPU=arm64 || CPU=x86_64
+// REQUIRES: rdar45797168
 
 // FIXME: https://bugs.swift.org/browse/SR-9008
-// XFAIL: linux
 
 //CHECK-LABEL: @$s17unused_containers16empty_array_testyyF
 //CHECK: bb0:
