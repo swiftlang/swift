eeccb29587df3585f5526e8814e100ccfcb96497
Graydon Hoare <ghoare@apple.com>
2018-11-08 16:21:18 -0800

    [ModuleInterface] Compensate for silly source of order variation in .d files.

--- test/ParseableInterface/ModuleCache/module-cache-init.swift
+++ test/ParseableInterface/ModuleCache/module-cache-init.swift
@@ -38,7 +38,6 @@
 // RUN: test -f %t/modulecache/OtherModule-*.swiftmodule
 // RUN: test -f %t/TestModule.d
 // RUN: llvm-bcanalyzer -dump %t/modulecache/OtherModule-*.swiftmodule | %FileCheck %s -check-prefix=CHECK-OTHERMODULE
-// RUN: %FileCheck %s -check-prefix=CHECK-DEPENDS <%t/TestModule.d
 // CHECK-OTHERMODULE: {{MODULE_NAME.*blob data = 'OtherModule'}}
 // CHECK-OTHERMODULE: {{FILE_DEPENDENCY.*Swift.swiftmodule'}}
 // CHECK-OTHERMODULE: {{FILE_DEPENDENCY.*SwiftOnoneSupport.swiftmodule'}}
@@ -46,13 +45,29 @@
 // CHECK-OTHERMODULE: {{FILE_DEPENDENCY.*LeafModule-.*.swiftmodule'}}
 // CHECK-OTHERMODULE: {{FILE_DEPENDENCY.*OtherModule.swiftinterface'}}
 // CHECK-OTHERMODULE: FUNC_DECL
+//
+// Quirk: because the cached .swiftmodules have a hash name component that
+// integrates target, and we sort the contents of lines in a .d file by the
+// dependency's reverse-name (for reasons), the order in which the cached
+// .swiftmodules are listed in the .d file will vary _by target_.
+//
+// So we cannot write a single set of CHECK-SAME lines here that will work
+// for all targets: some will have LeafModule first, some OtherModule
+// first. So instead, we write two sets of patterns, and run FileCheck
+// twice. Yes this is silly.
+//
+// RUN: %FileCheck %s -check-prefix=CHECK-DEPENDS <%t/TestModule.d
+// RUN: %FileCheck %s -check-prefix=CHECK-DEPENDSAGAIN <%t/TestModule.d
+//
 // CHECK-DEPENDS: TestModule.swiftmodule :
 // CHECK-DEPENDS-SAME: LeafModule.swiftinterface
 // CHECK-DEPENDS-SAME: OtherModule.swiftinterface
-// CHECK-DEPENDS-SAME: {{LeafModule-[^ ]+.swiftmodule}}
 // CHECK-DEPENDS-SAME: {{OtherModule-[^ ]+.swiftmodule}}
 // CHECK-DEPENDS-SAME: Swift.swiftmodule
 // CHECK-DEPENDS-SAME: SwiftOnoneSupport.swiftmodule
+//
+// CHECK-DEPENDSAGAIN: TestModule.swiftmodule :
+// CHECK-DEPENDSAGAIN-SAME: {{LeafModule-[^ ]+.swiftmodule}}
 
 import OtherModule
 
