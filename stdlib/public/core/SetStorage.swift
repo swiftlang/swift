671de071c66220367ea378732399df012c2a0c78
Karoy Lorentey <klorentey@apple.com>
2018-11-12 20:53:10 +0000

    [stdlib] Audit @_fixed_layout usage in collection storage classes
    - Remove __SwiftNativeNSEnumerator from the ABI. (It’s only used in internal classes and in the Objective-C runtime.)
    - Remove inlinability of init() and deinit for all the __SwiftNativeNSFoo superclasses except Array’s and Data’s.

--- stdlib/public/core/SetStorage.swift
+++ stdlib/public/core/SetStorage.swift
@@ -16,7 +16,7 @@ import SwiftShims
 /// Enough bytes are allocated to hold the bitmap for marking valid entries,
 /// keys, and values. The data layout starts with the bitmap, followed by the
 /// keys, followed by the values.
-@_fixed_layout // FIXME(sil-serialize-all)
+@_fixed_layout
 @usableFromInline
 @_objc_non_lazy_realization
 internal class _RawSetStorage: __SwiftNativeNSSet {
