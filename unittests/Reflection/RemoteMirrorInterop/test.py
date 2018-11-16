4ae12f3c3be949667ff7a3833a7d6704b06b57a1
Mike Ash <mikeash@apple.com>
2018-11-16 11:22:38 -0500

    [RemoteMirror] Fix lint errors in RemoteMirrorInterop/test.py, and make relative path args work when the working directory is not the script's enclosing directory.

--- unittests/Reflection/RemoteMirrorInterop/test.py
+++ unittests/Reflection/RemoteMirrorInterop/test.py
@@ -14,13 +14,16 @@ import sys
 
 args = sys.argv[1:]
 if len(args) == 0:
-  print >> sys.stderr, "Usage:", sys.argv[0], "swift-build-dirs..."
-  print >> sys.stderr, "Note: pass paths to the swift-macosx-x86_64 directories."
-  sys.exit(1)
+    print >> sys.stderr, "Usage:", sys.argv[0], "swift-build-dirs..."
+    print >> sys.stderr, ("Note: pass paths to the swift-macosx-x86_64"
+                          " directories.")
+    sys.exit(1)
 
-swiftcs = [os.path.join(arg, 'bin', 'swiftc') for arg in args]
-mirrorlibs = [os.path.join(arg, 'lib', 'swift', 'macosx', 'libswiftRemoteMirror.dylib')
-              for arg in args]
+absoluteArgs = [os.path.abspath(arg) for arg in args]
+swiftcs = [os.path.join(arg, 'bin', 'swiftc') for arg in absoluteArgs]
+mirrorlibs = [os.path.join(arg, 'lib', 'swift', 'macosx',
+                           'libswiftRemoteMirror.dylib')
+              for arg in absoluteArgs]
 
 os.chdir(os.path.dirname(sys.argv[0]))
 
@@ -35,20 +38,23 @@ subprocess.check_call(['clang',
 
 # Build a test library with each Swift compiler passed in.
 for i, swiftc in enumerate(swiftcs):
-  subprocess.check_call(['xcrun', swiftc, '-emit-library', 'test.swift',
-                         '-o', os.path.join('/tmp', 'libtest' + str(i) + '.dylib')])
+    subprocess.check_call(
+        ['xcrun', swiftc, '-emit-library', 'test.swift',
+         '-o', os.path.join('/tmp', 'libtest' + str(i) + '.dylib')])
 
 # Run the test harness with all combinations of the remote mirror libraries.
 for i in range(len(swiftcs) + 1):
-  for localMirrorlibs in itertools.combinations(mirrorlibs, i):
-    for i, arg in enumerate(args):
-      print 'Testing', arg, 'with mirror libs:'
-      for l in localMirrorlibs:
-        print '\t', l
-      callArgs = (['/tmp/test', os.path.join('/tmp', 'libtest' + str(i) + '.dylib')]
-                  + list(localMirrorlibs))
-      print ' '.join(callArgs)
-      subprocess.call(callArgs)
-      print 'DONE'
-      print ''
-    print localMirrorlibs
+    for localMirrorlibs in itertools.combinations(mirrorlibs, i):
+        for i, arg in enumerate(absoluteArgs):
+            print 'Testing', arg, 'with mirror libs:'
+            for l in localMirrorlibs:
+                print '\t', l
+            callArgs = ['/tmp/test']
+            dylibPath = os.path.join('/tmp', 'libtest' + str(i) + '.dylib')
+            callArgs.append(dylibPath)
+            callArgs += list(localMirrorlibs)
+            print ' '.join(callArgs)
+            subprocess.call(callArgs)
+            print 'DONE'
+            print ''
+        print localMirrorlibs
