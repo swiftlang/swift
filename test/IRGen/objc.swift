3a8d4a8e36b9047f8fb1c371f72dc735b0964caf
Mike Ash <mikeash@apple.com>
2018-11-16 12:53:40 -0500

    Fix comment in ObjC test file to match swift version change.
    Followup to https://github.com/apple/swift/pull/20638.

--- test/IRGen/objc.swift
+++ test/IRGen/objc.swift
@@ -147,7 +147,7 @@ class WeakObjC {
 // CHECK:  i32 1, !"Objective-C Version", i32 2}
 // CHECK:  i32 1, !"Objective-C Image Info Version", i32 0}
 // CHECK:  i32 1, !"Objective-C Image Info Section", !"__DATA,__objc_imageinfo,regular,no_dead_strip"}
-//   67241472 == (4 << 24) | (2 << 16) | (6 << 8). 
-//     4 and 2 is the current major.minor version. 6 is the Swift ABI version.
+//   67241472 == (5 << 24) | (0 << 16) | (6 << 8). 
+//     5 and 0 is the current major.minor version. 6 is the Swift ABI version.
 // CHECK:  i32 4, !"Objective-C Garbage Collection", i32 83887616}
 // CHECK:  i32 1, !"Swift Version", i32 6}
