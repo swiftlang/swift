e8d3caaac004755ea31275472131b43e824c8dca
Suyash Srijan <suyashsrijan@outlook.com>
2018-11-19 22:59:57 +0000

    [test] update expected-note in test

--- test/SILGen/functions_uninhabited_param.swift
+++ test/SILGen/functions_uninhabited_param.swift
@@ -2,7 +2,7 @@
 
 //===--- Function declaration with uninhabited parameter type
                                    
-func foo(baz: Never) -> Int { // expected-note {{the function body will never be executed}}
+func foo(baz: Never) -> Int { // expected-note {{the function body will never be executed. remove the function body to dismiss the warning}}
   print("I can't be called!") // expected-warning{{will never be executed}}
   return 0
 }
