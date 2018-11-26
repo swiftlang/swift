f0aa5abbf753e7bbf20a44e6e6c8e129ec0e770e
Mark Lacey <mark.lacey@apple.com>
2018-11-26 14:28:37 -0800

    Re-enable a test that had been disabled at one point.
    We saw a failure of this test on Linux at some point, and disabled
    it. It looks like it may have been due to the fact that the
    `%target-typecheck-verify-swift` provides the filename, but we
    explicitly provide the filename in the test invocation here as well.
    
    I'm going to re-enable and see if we can get through tests on all
    supported platforms.

--- test/Constraints/warn_long_compile.swift
+++ test/Constraints/warn_long_compile.swift
@@ -1,5 +1,5 @@
 // RUN: %target-typecheck-verify-swift -warn-long-expression-type-checking=1 -warn-long-function-bodies=1 %s
-// REQUIRES: rdar44305428
+
 @_silgen_name("generic_foo")
 func foo<T>(_ x: T) -> T
 
