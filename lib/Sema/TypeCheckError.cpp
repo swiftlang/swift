9099ad919192d2f02b2b10a7fc103bc389ca62ed
Mark Lacey <mark.lacey@apple.com>
2018-11-27 08:29:00 -0800

    [TypeChecker] Fix a problem with rethrows classification.
    We failed to properly classify arguments that are of optional function
    type, in this case leading to a verification failure due to the throws
    attribute on the apply expression not being set to some value.
    
    Fixes: SR-9102 / rdar://problem/45615204
    (cherry picked from commit 5a498db041b185ad3f92716303fce99ef61e3c7c)

--- lib/Sema/TypeCheckError.cpp
+++ lib/Sema/TypeCheckError.cpp
@@ -713,7 +713,10 @@ private:
 
     // If it doesn't have function type, we must have invalid code.
     Type argType = fn.getType();
-    auto argFnType = (argType ? argType->getAs<AnyFunctionType>() : nullptr);
+    if (!argType) return Classification::forInvalidCode();
+
+    auto argFnType =
+        argType->lookThroughAllOptionalTypes()->getAs<AnyFunctionType>();
     if (!argFnType) return Classification::forInvalidCode();
 
     // If it doesn't throw, this argument does not cause the call to throw.
