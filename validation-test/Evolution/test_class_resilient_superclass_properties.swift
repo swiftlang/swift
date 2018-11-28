9af20c71f09f068701990b4b3ee9a5db4bdbdce9
Slava Pestov <spestov@apple.com>
2018-11-28 14:45:40 -0500

    Evolution: Clean up superclass insertion tests a bit
    - We don't want to support changing a root class of a class, so don't
      pretend that works. Some of these tests got removed recently in
      d8104e7e43fe3266247285e40b59caae27dd9110 but one still remained.
    
    - For the tests that insert a non-root superclass in the inheritance
      hierarchy, also test calling a method of the derived class. This
      works now that we no longer hardcode vtable offsets and instead use
      dispatch thunks.

--- validation-test/Evolution/test_class_resilient_superclass_properties.swift
+++ validation-test/Evolution/test_class_resilient_superclass_properties.swift
@@ -16,6 +16,7 @@ SuperclassPropertiesTest.test("AddInterposingProperty") {
       override class var classProperty: String {
         return super.classProperty
       }
+      var newProperty: String = "still works"
     }
     if getVersion() == 0 {
       expectEqual(Leaf().property, "Base.property")
@@ -24,6 +25,7 @@ SuperclassPropertiesTest.test("AddInterposingProperty") {
       expectEqual(Leaf().property, "AddInterposingProperty.property")
       expectEqual(Leaf.classProperty, "AddInterposingProperty.classProperty")
     }
+    expectEqual(Leaf().newProperty, "still works")
   }
 }
 
@@ -36,6 +38,7 @@ SuperclassPropertiesTest.test("RemoveInterposingProperty") {
       override class var classProperty: String {
         return super.classProperty
       }
+      var newProperty: String = "still works"
     }
     if getVersion() == 0 {
       expectEqual(Leaf().property, "RemoveInterposingProperty.property")
@@ -44,6 +47,7 @@ SuperclassPropertiesTest.test("RemoveInterposingProperty") {
       expectEqual(Leaf().property, "Base.property")
       expectEqual(Leaf.classProperty, "Base.classProperty")
     }
+    expectEqual(Leaf().newProperty, "still works")
   }
 }
 
@@ -56,6 +60,7 @@ SuperclassPropertiesTest.test("InsertSuperclass") {
       override class var classProperty: String {
         return super.classProperty
       }
+      var newProperty: String = "still works"
     }
     if getVersion() == 0 {
       expectEqual(Leaf().property, "Base.property")
@@ -66,6 +71,7 @@ SuperclassPropertiesTest.test("InsertSuperclass") {
       expectEqual(Leaf().nonOverriddenProperty, "Base.nonOverriddenProperty")
       expectEqual(Leaf.classProperty, "InBetween.classProperty")
     }
+    expectEqual(Leaf().newProperty, "still works")
   }
 }
 
