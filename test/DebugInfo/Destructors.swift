0ac9a7dc771c90e077cd1c63197d558286d1b82f
Adrian Prantl <aprantl@apple.com>
2018-11-29 09:53:13 -0800

    Update tests for upstream LLVM IR assembler changes

--- test/DebugInfo/Destructors.swift
+++ test/DebugInfo/Destructors.swift
@@ -3,7 +3,7 @@
 public class Foo {
   // CHECK: !DISubprogram(name: "deinit", linkageName: "$s11Destructors3FooCfD"
   // CHECK-SAME:          line: [[@LINE-2]]
-  // CHECK-SAME:          isDefinition: true
+  // CHECK-SAME:          DISPFlagDefinition
   var x : Int64
   init(x: Int64) { self.x = x }
 }
