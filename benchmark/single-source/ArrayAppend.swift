150d748cab4a3b00c052b380a35956f4abed2d98
Pavol Vaskovic <pali@pali.sk>
2018-11-29 10:38:12 +0100

    [benchmark] ArrayAppend Legacy Factor
    Cleaned up doubled inner loops and applied legacyFactor to the resulting, more rational, workload sizes.
    
    Factors for string appending benchmarks are not 10, but 11 to compensate for smaller amortized Array resizes. Similarily ArrayPlusEqualFiveElementCollection’s factor is 49.

--- benchmark/single-source/ArrayAppend.swift
+++ benchmark/single-source/ArrayAppend.swift
@@ -16,40 +16,40 @@ import TestsUtils
 
 let t: [BenchmarkCategory] = [.validation, .api, .Array]
 public let ArrayAppend = [
-  BenchmarkInfo(name: "ArrayAppend", runFunction: run_ArrayAppend, tags: t),
+  BenchmarkInfo(name: "ArrayAppend", runFunction: run_ArrayAppend, tags: t, legacyFactor: 10),
   BenchmarkInfo(name: "ArrayAppendArrayOfInt", runFunction: run_ArrayAppendArrayOfInt, tags: t,
-    setUpFunction: ones, tearDownFunction: releaseOnes),
-  BenchmarkInfo(name: "ArrayAppendAscii", runFunction: run_ArrayAppendAscii, tags: t),
-  BenchmarkInfo(name: "ArrayAppendAsciiSubstring", runFunction: run_ArrayAppendAsciiSubstring, tags: t),
+    setUpFunction: ones, tearDownFunction: releaseOnes, legacyFactor: 10),
+  BenchmarkInfo(name: "ArrayAppendAscii", runFunction: run_ArrayAppendAscii, tags: t, legacyFactor: 11),
+  BenchmarkInfo(name: "ArrayAppendAsciiSubstring", runFunction: run_ArrayAppendAsciiSubstring, tags: t, legacyFactor: 11),
   BenchmarkInfo(name: "ArrayAppendFromGeneric", runFunction: run_ArrayAppendFromGeneric, tags: t,
-    setUpFunction: ones, tearDownFunction: releaseOnes),
+    setUpFunction: ones, tearDownFunction: releaseOnes, legacyFactor: 10),
   BenchmarkInfo(name: "ArrayAppendGenericStructs", runFunction: run_ArrayAppendGenericStructs, tags: t,
     setUpFunction: { otherStructs = Array(repeating: S(x: 3, y: 4.2), count: 10_000) },
-    tearDownFunction: {  otherStructs = nil } ),
-  BenchmarkInfo(name: "ArrayAppendLatin1", runFunction: run_ArrayAppendLatin1, tags: t),
-  BenchmarkInfo(name: "ArrayAppendLatin1Substring", runFunction: run_ArrayAppendLatin1Substring, tags: t),
+    tearDownFunction: {  otherStructs = nil }, legacyFactor: 10),
+  BenchmarkInfo(name: "ArrayAppendLatin1", runFunction: run_ArrayAppendLatin1, tags: t, legacyFactor: 11),
+  BenchmarkInfo(name: "ArrayAppendLatin1Substring", runFunction: run_ArrayAppendLatin1Substring, tags: t, legacyFactor: 11),
   BenchmarkInfo(name: "ArrayAppendLazyMap", runFunction: run_ArrayAppendLazyMap, tags: t,
-    setUpFunction: { blackHole(array) }),
+    setUpFunction: { blackHole(array) }, legacyFactor: 10),
   BenchmarkInfo(name: "ArrayAppendOptionals", runFunction: run_ArrayAppendOptionals, tags: t,
     setUpFunction: { otherOptionalOnes = Array(repeating: 1, count: 10_000) },
-    tearDownFunction: {  otherOptionalOnes = nil } ),
-  BenchmarkInfo(name: "ArrayAppendRepeatCol", runFunction: run_ArrayAppendRepeatCol, tags: t),
-  BenchmarkInfo(name: "ArrayAppendReserved", runFunction: run_ArrayAppendReserved, tags: t),
-  BenchmarkInfo(name: "ArrayAppendSequence", runFunction: run_ArrayAppendSequence, tags: t),
+    tearDownFunction: {  otherOptionalOnes = nil }, legacyFactor: 10),
+  BenchmarkInfo(name: "ArrayAppendRepeatCol", runFunction: run_ArrayAppendRepeatCol, tags: t, legacyFactor: 10),
+  BenchmarkInfo(name: "ArrayAppendReserved", runFunction: run_ArrayAppendReserved, tags: t, legacyFactor: 10),
+  BenchmarkInfo(name: "ArrayAppendSequence", runFunction: run_ArrayAppendSequence, tags: t, legacyFactor: 10),
   BenchmarkInfo(name: "ArrayAppendStrings", runFunction: run_ArrayAppendStrings, tags: t,
     setUpFunction: { otherStrings = stride(from: 0, to: 10_000, by: 1).map { "\($0)" } },
-    tearDownFunction: {  otherStrings = nil } ),
+    tearDownFunction: {  otherStrings = nil }, legacyFactor: 10),
   BenchmarkInfo(name: "ArrayAppendToFromGeneric", runFunction: run_ArrayAppendToFromGeneric, tags: t,
-    setUpFunction: ones, tearDownFunction: releaseOnes),
+    setUpFunction: ones, tearDownFunction: releaseOnes, legacyFactor: 10),
   BenchmarkInfo(name: "ArrayAppendToGeneric", runFunction: run_ArrayAppendToGeneric, tags: t,
-    setUpFunction: ones, tearDownFunction: releaseOnes),
-  BenchmarkInfo(name: "ArrayAppendUTF16", runFunction: run_ArrayAppendUTF16, tags: t),
-  BenchmarkInfo(name: "ArrayAppendUTF16Substring", runFunction: run_ArrayAppendUTF16Substring, tags: t),
+    setUpFunction: ones, tearDownFunction: releaseOnes, legacyFactor: 10),
+  BenchmarkInfo(name: "ArrayAppendUTF16", runFunction: run_ArrayAppendUTF16, tags: t, legacyFactor: 11),
+  BenchmarkInfo(name: "ArrayAppendUTF16Substring", runFunction: run_ArrayAppendUTF16Substring, tags: t, legacyFactor: 11),
   BenchmarkInfo(name: "ArrayPlusEqualArrayOfInt", runFunction: run_ArrayPlusEqualArrayOfInt, tags: t,
-    setUpFunction: ones, tearDownFunction: releaseOnes),
-  BenchmarkInfo(name: "ArrayPlusEqualFiveElementCollection", runFunction: run_ArrayPlusEqualFiveElementCollection, tags: t),
-  BenchmarkInfo(name: "ArrayPlusEqualSingleElementCollection", runFunction: run_ArrayPlusEqualSingleElementCollection, tags: t),
-  BenchmarkInfo(name: "ArrayPlusEqualThreeElements", runFunction: run_ArrayPlusEqualThreeElements, tags: t),
+    setUpFunction: ones, tearDownFunction: releaseOnes, legacyFactor: 10),
+  BenchmarkInfo(name: "ArrayPlusEqualFiveElementCollection", runFunction: run_ArrayPlusEqualFiveElementCollection, tags: t, legacyFactor: 49),
+  BenchmarkInfo(name: "ArrayPlusEqualSingleElementCollection", runFunction: run_ArrayPlusEqualSingleElementCollection, tags: t, legacyFactor: 40),
+  BenchmarkInfo(name: "ArrayPlusEqualThreeElements", runFunction: run_ArrayPlusEqualThreeElements, tags: t, legacyFactor: 10),
 ]
 
 var otherOnes: [Int]!
@@ -65,11 +65,9 @@ func releaseOnes() { otherOnes = nil }
 @inline(never)
 public func run_ArrayAppend(_ N: Int) {
   for _ in 0..<N {
-    for _ in 0..<10 {
-       var nums = [Int]()
-       for _ in 0..<40000 {
-         nums.append(1)
-       }
+    var nums = [Int]()
+    for _ in 0..<40000 {
+      nums.append(1)
     }
   }
 }
@@ -78,12 +76,10 @@ public func run_ArrayAppend(_ N: Int) {
 @inline(never)
 public func run_ArrayAppendReserved(_ N: Int) {
   for _ in 0..<N {
-    for _ in 0..<10 {
-       var nums = [Int]()
-       nums.reserveCapacity(40000)
-       for _ in 0..<40000 {
-         nums.append(1)
-       }
+    var nums = [Int]()
+    nums.reserveCapacity(40000)
+    for _ in 0..<40000 {
+      nums.append(1)
     }
   }
 }
@@ -95,11 +91,9 @@ public func run_ArrayAppendSequence(_ N: Int) {
   let seq = stride(from: 0, to: 10_000, by: 1)
 
   for _ in 0..<N {
-    for _ in 0..<10 {
-      var nums = [Int]()
-      for _ in 0..<8 {
-        nums.append(contentsOf: seq)
-      }
+    var nums = [Int]()
+    for _ in 0..<8 {
+      nums.append(contentsOf: seq)
     }
   }
 }
@@ -111,11 +105,9 @@ public func run_ArrayAppendArrayOfInt(_ N: Int) {
   let other: [Int] = otherOnes
 
   for _ in 0..<N {
-    for _ in 0..<10 {
-      var nums = [Int]()
-      for _ in 0..<8 {
-        nums.append(contentsOf: other)
-      }
+    var nums = [Int]()
+    for _ in 0..<8 {
+      nums.append(contentsOf: other)
     }
   }
 }
@@ -127,11 +119,9 @@ public func run_ArrayPlusEqualArrayOfInt(_ N: Int) {
   let other: [Int] = otherOnes
 
   for _ in 0..<N {
-    for _ in 0..<10 {
-      var nums = [Int]()
-      for _ in 0..<8 {
-        nums += other
-      }
+    var nums = [Int]()
+    for _ in 0..<8 {
+      nums += other
     }
   }
 }
@@ -143,12 +133,10 @@ public func run_ArrayAppendStrings(_ N: Int) {
   let other: [String] = otherStrings
 
   for _ in 0..<N {
-    for _ in 0..<10 {
-      var nums = [String]()
-      // lower inner count due to string slowness
-      for _ in 0..<4 {
-        nums += other
-      }
+    var nums = [String]()
+    // lower inner count due to string slowness
+    for _ in 0..<4 {
+      nums += other
     }
   }
 }
@@ -165,11 +153,9 @@ public func run_ArrayAppendGenericStructs(_ N: Int) {
   let other: [S<Int, Double>] = otherStructs
 
   for _ in 0..<N {
-    for _ in 0..<10 {
-      var nums = [S<Int, Double>]()
-      for _ in 0..<8 {
-        nums += other
-      }
+    var nums = [S<Int,Double>]()
+    for _ in 0..<8 {
+      nums += other
     }
   }
 }
@@ -181,11 +167,9 @@ public func run_ArrayAppendOptionals(_ N: Int) {
   let other: [Int?] = otherOptionalOnes
 
   for _ in 0..<N {
-    for _ in 0..<10 {
-      var nums = [Int?]()
-      for _ in 0..<8 {
-        nums += other
-      }
+    var nums = [Int?]()
+    for _ in 0..<8 {
+      nums += other
     }
   }
 }
@@ -198,11 +182,9 @@ public func run_ArrayAppendLazyMap(_ N: Int) {
   let other = array.lazy.map { $0 * 2 }
 
   for _ in 0..<N {
-    for _ in 0..<10 {
-      var nums = [Int]()
-      for _ in 0..<8 {
-        nums += other
-      }
+    var nums = [Int]()
+    for _ in 0..<8 {
+      nums += other
     }
   }
 }
@@ -215,11 +197,9 @@ public func run_ArrayAppendRepeatCol(_ N: Int) {
   let other = repeatElement(1, count: 10_000)
 
   for _ in 0..<N {
-    for _ in 0..<10 {
-      var nums = [Int]()
-      for _ in 0..<8 {
-        nums += other
-      }
+    var nums = [Int]()
+    for _ in 0..<8 {
+      nums += other
     }
   }
 }
@@ -238,11 +218,9 @@ public func run_ArrayAppendFromGeneric(_ N: Int) {
   let other: [Int] = otherOnes
 
   for _ in 0..<N {
-    for _ in 0..<10 {
-      var nums = [Int]()
-      for _ in 0..<8 {
-        appendFromGeneric(array: &nums, sequence: other)
-      }
+    var nums = [Int]()
+    for _ in 0..<8 {
+      appendFromGeneric(array: &nums, sequence: other)
     }
   }
 }
@@ -260,11 +238,9 @@ public func run_ArrayAppendToGeneric(_ N: Int) {
   let other: [Int] = otherOnes
 
   for _ in 0..<N {
-    for _ in 0..<10 {
-      var nums = [Int]()
-      for _ in 0..<8 {
-        appendToGeneric(collection: &nums, array: other)
-      }
+    var nums = [Int]()
+    for _ in 0..<8 {
+      appendToGeneric(collection: &nums, array: other)
     }
   }
 }
@@ -284,11 +260,9 @@ public func run_ArrayAppendToFromGeneric(_ N: Int) {
   let other: [Int] = otherOnes
 
   for _ in 0..<N {
-    for _ in 0..<10 {
-      var nums = [Int]()
-      for _ in 0..<8 {
-        appendToFromGeneric(collection: &nums, sequence: other)
-      }
+    var nums = [Int]()
+    for _ in 0..<8 {
+      appendToFromGeneric(collection: &nums, sequence: other)
     }
   }
 }
@@ -297,11 +271,9 @@ public func run_ArrayAppendToFromGeneric(_ N: Int) {
 @inline(never)
 public func run_ArrayPlusEqualSingleElementCollection(_ N: Int) {
   for _ in 0..<N {
-    for _ in 0..<10 {
-       var nums = [Int]()
-       for _ in 0..<40_000 {
-         nums += [1]
-       }
+    var nums = [Int]()
+    for _ in 0..<10_000 {
+      nums += [1]
     }
   }
 }
@@ -310,11 +282,9 @@ public func run_ArrayPlusEqualSingleElementCollection(_ N: Int) {
 @inline(never)
 public func run_ArrayPlusEqualFiveElementCollection(_ N: Int) {
   for _ in 0..<N {
-    for _ in 0..<10 {
-       var nums = [Int]()
-       for _ in 0..<40_000 {
-         nums += [1, 2, 3, 4, 5]
-       }
+    var nums = [Int]()
+    for _ in 0..<10_000 {
+      nums += [1, 2, 3, 4, 5]
     }
   }
 }
@@ -326,7 +296,7 @@ public func appendThreeElements(_ a: inout [Int]) {
 
 @inline(never)
 public func run_ArrayPlusEqualThreeElements(_ N: Int) {
-  for _ in 0..<(10_000 * N) {
+  for _ in 0..<(1_000 * N) {
     var a: [Int] = []
     appendThreeElements(&a)
   }
@@ -337,11 +307,9 @@ public func run_ArrayPlusEqualThreeElements(_ N: Int) {
 public func run_ArrayAppendAscii(_ N: Int) {
   let s = "the quick brown fox jumps over the lazy dog!"
   for _ in 0..<N {
-    for _ in 0..<10 {
-       var nums = [UInt8]()
-       for _ in 0..<10_000 {
-         nums += getString(s).utf8
-       }
+    var nums = [UInt8]()
+    for _ in 0..<10_000 {
+      nums += getString(s).utf8
     }
   }
 }
@@ -351,11 +319,9 @@ public func run_ArrayAppendAscii(_ N: Int) {
 public func run_ArrayAppendLatin1(_ N: Int) {
   let s = "the quick brown fox jumps over the lazy dog\u{00A1}"
   for _ in 0..<N {
-    for _ in 0..<10 {
-       var nums = [UInt8]()
-       for _ in 0..<10_000 {
-         nums += getString(s).utf8
-       }
+    var nums = [UInt8]()
+    for _ in 0..<10_000 {
+      nums += getString(s).utf8
     }
   }
 }
@@ -365,11 +331,9 @@ public func run_ArrayAppendLatin1(_ N: Int) {
 public func run_ArrayAppendUTF16(_ N: Int) {
   let s = "the quick brown 🦊 jumps over the lazy dog"
   for _ in 0..<N {
-    for _ in 0..<10 {
-       var nums = [UInt8]()
-       for _ in 0..<10_000 {
-         nums += getString(s).utf8
-       }
+    var nums = [UInt8]()
+    for _ in 0..<10_000 {
+      nums += getString(s).utf8
     }
   }
 }
@@ -379,11 +343,9 @@ public func run_ArrayAppendUTF16(_ N: Int) {
 public func run_ArrayAppendAsciiSubstring(_ N: Int) {
   let s = "the quick brown fox jumps over the lazy dog!"[...]
   for _ in 0..<N {
-    for _ in 0..<10 {
-       var nums = [UInt8]()
-       for _ in 0..<10_000 {
-         nums += getSubstring(s).utf8
-       }
+    var nums = [UInt8]()
+    for _ in 0..<10_000 {
+      nums += getSubstring(s).utf8
     }
   }
 }
@@ -393,11 +355,9 @@ public func run_ArrayAppendAsciiSubstring(_ N: Int) {
 public func run_ArrayAppendLatin1Substring(_ N: Int) {
   let s = "the quick brown fox jumps over the lazy dog\u{00A1}"[...]
   for _ in 0..<N {
-    for _ in 0..<10 {
-       var nums = [UInt8]()
-       for _ in 0..<10_000 {
-         nums += getSubstring(s).utf8
-       }
+    var nums = [UInt8]()
+    for _ in 0..<10_000 {
+      nums += getSubstring(s).utf8
     }
   }
 }
@@ -407,11 +367,9 @@ public func run_ArrayAppendLatin1Substring(_ N: Int) {
 public func run_ArrayAppendUTF16Substring(_ N: Int) {
   let s = "the quick brown 🦊 jumps over the lazy dog"[...]
   for _ in 0..<N {
-    for _ in 0..<10 {
-       var nums = [UInt8]()
-       for _ in 0..<10_000 {
-         nums += getSubstring(s).utf8
-       }
+    var nums = [UInt8]()
+    for _ in 0..<10_000 {
+      nums += getSubstring(s).utf8
     }
   }
 }
