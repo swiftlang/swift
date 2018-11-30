98d71ff38f4e375569ab4780aaa38af2567e8079
Saleem Abdulrasool <compnerd@compnerd.org>
2018-11-29 22:15:00 -0800

    SwiftPrivateThreadExtras: clean up using WinSDK SDK overlay
    Use the constants rather than hardcoding values.

--- stdlib/private/SwiftPrivateThreadExtras/ThreadBarriers.swift
+++ stdlib/private/SwiftPrivateThreadExtras/ThreadBarriers.swift
@@ -122,9 +122,8 @@ public func _stdlib_thread_barrier_wait(
   if barrier.pointee.numThreadsWaiting < barrier.pointee.count {
     // Put the thread to sleep.
 #if os(Windows)
-    // TODO(compnerd) modularize rpc.h to get INFIITE (0xffffffff)
     if SleepConditionVariableSRW(barrier.pointee.cond!, barrier.pointee.mutex!,
-                                 0xffffffff, 0) == 0 {
+                                 INFINITE, 0) == 0 {
       return -1
     }
     ReleaseSRWLockExclusive(barrier.pointee.mutex!)
