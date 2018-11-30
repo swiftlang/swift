28962b575463b56b2d561507ef87325b1a9b5065
Stephen Canon <stephentyrone@gmail.com>
2018-11-30 16:30:15 -0500

    Move most of the simd operators into an optional module (#20914)
    * Move most of the simd operators into an optional module
    
    Adding simd to the stdlib caused some typechecker regressions. We can resolve them in the near-term by making the types universally available, but moving the arithmetic operators into a separate module that must be explicitly imported.
    
    * Move two fuzzing tests back to fixed.
    
    * Add SIMDOperators as a dependency for MediaPlayer.
    
    * Move the .-prefixed operator declarations back into the stdlib.

--- validation-test/compiler_crashers_fixed/28606-swift-typebase-getcanonicaltype.swift
+++ validation-test/compiler_crashers_fixed/28606-swift-typebase-getcanonicaltype.swift
@@ -6,5 +6,5 @@
 // See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
 
 // REQUIRES: asserts
-// RUN: not --crash %target-swift-frontend %s -emit-ir
+// RUN: not %target-swift-frontend %s -emit-ir
 print([.h.h=#keyPath(n&_=(){
