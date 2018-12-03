e24b16651da34259226c4a767505fd3b17c3a596
David Nadoba <dnadoba@gmail.com>
2018-12-03 02:45:06 +0100

    Add test for just a too large array as a function parameter

--- test/Inputs/clang-importer-sdk/usr/include/ctypes.h
+++ test/Inputs/clang-importer-sdk/usr/include/ctypes.h
@@ -273,7 +273,9 @@ typedef struct ModRM {
 //===---
 void useArray(char x[4], char y[], char z[][8]);
 void staticBoundsArray(const char x[static 4]);
-void useBigArray(char max_size[][4096], char max_size_plus_one[][4097]);
+
+void useBigArray(char max_size[4096], char max_size_plus_one[4097]);
+void useBigArray2d(char max_size[][4096], char max_size_plus_one[][4097]);
 
 typedef const int FourConstInts[4];
 void nonnullArrayParameters(const char x[_Nonnull], void * const _Nullable y[_Nonnull], _Nonnull FourConstInts z);
