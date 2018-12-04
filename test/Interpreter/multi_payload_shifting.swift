11f9d19cec6bf7c81342339501961fcd621ae0c9
Arnold Schwaighofer <aschwaighofer@apple.com>
2018-12-04 08:40:36 -0800

    IRGen: gatherbits - extend the bits before we shift left
    SR-9335
    rdar://46264497

---
+++ test/Interpreter/multi_payload_shifting.swift
@@ -0,0 +1,53 @@
+// RUN: %empty-directory(%t)
+
+// RUN: %target-build-swift -o %t/a.out %s
+// RUN: %target-run %t/a.out 2>&1
+
+// REQUIRES: executable_test
+
+import Swift
+import StdlibUnittest
+
+class Tag {}
+
+struct Scalar {
+  var str = ""
+  var x = Tag()
+  var style: BinaryChoice  = .zero
+  enum BinaryChoice: UInt32 {
+    case zero = 0
+    case one
+  }
+}
+
+public struct Sequence {
+  var tag: Tag = Tag()
+  var tag2: Tag = Tag()
+}
+
+enum Node {
+  case scalar(Scalar)
+  case sequence(Sequence)
+}
+
+func createOptionalNodeNil<T>(_ t: T) -> T? {
+  return nil
+}
+
+func isNil<T>(_ t: T?) -> Bool {
+  return t == nil
+}
+
+var tests = TestSuite("extra inhabitants shifts")
+
+
+tests.test("test-shift-fix") {
+  let opt = createOptionalNodeNil(Node.scalar(Scalar()))
+  var res = false
+  if isNil(opt) {
+    res = true
+  }
+  expectEqual(true, res)
+}
+
+runAllTests()
