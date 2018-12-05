e043e2b2b3214c197056dfbccb3beaaf2b51c3c9
Pavel Yaskevich <pyaskevich@apple.com>
2018-12-04 17:55:00 -0800

    [CSFix] Add a fix to remove invalid optional unwrap
    If the base type is not optional, trying to unwrap it is
    incorrect. Introduce a fix to make it look like base was
    an optional type which leads solver to move forward
    towards possible solution.

--- lib/Sema/CSFix.cpp
+++ lib/Sema/CSFix.cpp
@@ -213,3 +213,12 @@ AutoClosureForwarding *AutoClosureForwarding::create(ConstraintSystem &cs,
                                                      ConstraintLocator *locator) {
   return new (cs.getAllocator()) AutoClosureForwarding(cs, locator);
 }
+
+bool RemoveUnwrap::diagnose(Expr *root, bool asNote) const {
+  return false;
+}
+
+RemoveUnwrap *RemoveUnwrap::create(ConstraintSystem &cs, Type baseType,
+                                   ConstraintLocator *locator) {
+  return new (cs.getAllocator()) RemoveUnwrap(cs, baseType, locator);
+}
