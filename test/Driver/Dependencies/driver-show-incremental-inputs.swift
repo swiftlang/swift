ea8465db36fa80fed7f2e75ed132597b882c92c5
Saleem Abdulrasool <compnerd@compnerd.org>
2018-12-05 11:43:32 -0800

    test: invoke touch.py with the interpreter
    Ensure that we use the interpreter explicitly when running the touch.py
    script.  This is important for platforms that do not support shebangs
    for interpreter execution.

--- test/Driver/Dependencies/driver-show-incremental-inputs.swift
+++ test/Driver/Dependencies/driver-show-incremental-inputs.swift
@@ -10,7 +10,7 @@
 
 // RUN: %empty-directory(%t)
 // RUN: cp -r %S/Inputs/one-way/* %t
-// RUN: %S/Inputs/touch.py 443865900 %t/*
+// RUN: %{python} %S/Inputs/touch.py 443865900 %t/*
 // RUN: echo '{version: "'$(%swiftc_driver_plain -version | head -n1)'", inputs: {"./main.swift": [443865900, 0], "./other.swift": [443865900, 0]}}' > %t/main~buildrecord.swiftdeps
 
 // RUN: cd %t && %swiftc_driver -driver-use-frontend-path %S/Inputs/update-dependencies.py -c ./main.swift ./other.swift -module-name main -incremental -v -driver-show-incremental -output-file-map %t/output.json | %FileCheck --check-prefix CHECK-INCREMENTAL %s
