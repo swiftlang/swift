a16c28dbdeb67eb95edbd3bf3bbd0abdd22436b8
Doug Gregor <dgregor@apple.com>
2018-12-05 21:05:52 -0800

    [stdlib] Teach abs(_:) to use magnitude when it can.
    Within the (single) implementation of abs(_:), dynamically check whether
    the numeric type and its `Magnitude` are of the same type and, if so,
    return the result of `magnitude`. This ensures that we do the right thing
    with respect to (e.g.) floating point values like -0.0, without resorting
    to overloading of abs(_:).
    
    (cherry picked from commit ab755770799f39055b066adc7829b0dd57de5f65)

--- stdlib/public/core/Integers.swift
+++ stdlib/public/core/Integers.swift
@@ -333,6 +333,10 @@ extension SignedNumeric {
 /// - Returns: The absolute value of `x`.
 @inlinable
 public func abs<T : SignedNumeric & Comparable>(_ x: T) -> T {
+  if T.self == T.Magnitude.self {
+    return unsafeBitCast(x.magnitude, to: T.self)
+  }
+
   return x < (0 as T) ? -x : x
 }
 
