a49e2ddde7a1246c3e84776e442e95470e57267f
Mike Ash <mikeash@apple.com>
2018-12-07 17:17:07 -0500

    [Stdlib] Rename AnyKeyPath's ObjC name and _VaListBuilder to avoid conflicts with older stdlibs.
    rdar://problem/46546165

--- stdlib/public/core/KeyPath.swift
+++ stdlib/public/core/KeyPath.swift
@@ -26,7 +26,11 @@ internal func _abstract(
 
 // MARK: Type-erased abstract base classes
 
-/// A type-erased key path, from any root type to any resulting value type.
+/// A type-erased key path, from any root type to any resulting value
+/// type. NOTE: older runtimes had Swift.AnyKeyPath as the ObjC name.
+/// The two must coexist, so it was renamed. The old name must not be
+/// used in the new runtime.
+@objc(_SwiftAnyKeyPath)
 public class AnyKeyPath: Hashable, _AppendKeyPath {
   /// The root type for this key path.
   @inlinable
