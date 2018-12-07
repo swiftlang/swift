5c11c108679b8760c962b79049a4e3a857f1728b
Slava Pestov <spestov@apple.com>
2018-12-07 15:57:34 -0500

    Sema: Fix order dependency between lazy getter body synthesis and capture computation
    Fixes <rdar://problem/46735170>.
    
    If we computed captures before completing a lazy getter body, we would fail to
    consider the 'self' capture properly. Instead make it resilient to such ordering
    issues by checking in capture computation if the lazy property has a getter yet
    or not.

--- test/multifile/lazy.swift
+++ test/multifile/lazy.swift
@@ -1,6 +1,11 @@
 // RUN: %target-swift-frontend -emit-sil -verify -primary-file %s %S/Inputs/external_lazy_property.swift
+// RUN: %target-swift-frontend -emit-sil -verify -primary-file %s -primary-file %S/Inputs/external_lazy_property.swift
 
 // rdar://45712204
 func test1(s: Test1) {
-  s.property // expected-error {{cannot use mutating getter on immutable value: 's' is a 'let' constant}}
+  _ = s.property // expected-error {{cannot use mutating getter on immutable value: 's' is a 'let' constant}}
+}
+
+func test2(s: Test2) {
+  _ = s.property
 }
