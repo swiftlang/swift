13f7a219aa90955570fdfd92f4e8ec6d7d3c6cff
Arnold Schwaighofer <aschwaighofer@apple.com>
2018-12-10 10:33:47 -0800

    IRGen: Drop the witness table pointer in visitThickToObjCMetatypeInst
    Otherwise, we will assert in an assert build of the compiler.
    
    rdar://46563206

--- lib/IRGen/IRGenSIL.cpp
+++ lib/IRGen/IRGenSIL.cpp
@@ -4697,6 +4697,8 @@ void IRGenSILFunction::visitThinToThickFunctionInst(
 void IRGenSILFunction::visitThickToObjCMetatypeInst(ThickToObjCMetatypeInst *i){
   Explosion from = getLoweredExplosion(i->getOperand());
   llvm::Value *swiftMeta = from.claimNext();
+  // Claim any conformances.
+  (void)from.claimAll();
   CanType instanceType(i->getType().castTo<AnyMetatypeType>().getInstanceType());
   Explosion to;
   llvm::Value *classPtr =
