6618ec17dde2231bd246f814a789510a78fc40eb
Saleem Abdulrasool <compnerd@compnerd.org>
2018-12-10 23:09:50 -0800

    stdlib: replace shell script with python
    Replace timeout.sh with timeout.py.  This makes it portable to other
    platforms like Windows.

--- test/stdlib/Reflection_objc.swift
+++ test/stdlib/Reflection_objc.swift
@@ -3,15 +3,16 @@
 // RUN: %target-clang %S/Inputs/Mirror/Mirror.mm -c -o %t/Mirror.mm.o -g
 // RUN: %target-build-swift -parse-stdlib %s -module-name Reflection -I %S/Inputs/Mirror/ -Xlinker %t/Mirror.mm.o -o %t/a.out
 // RUN: %target-codesign %t/a.out
-// RUN: %S/timeout.sh 360 %target-run %t/a.out %S/Inputs/shuffle.jpg | %FileCheck %s
-// REQUIRES: executable_test
+// RUN: %{python} %S/timeout.py 360 %target-run %t/a.out %S/Inputs/shuffle.jpg | %FileCheck %s
 // FIXME: timeout wrapper is necessary because the ASan test runs for hours
+
+// REQUIRES: executable_test
+// REQUIRES: objc_interop
 
 //
 // DO NOT add more tests to this file.  Add them to test/1_stdlib/Runtime.swift.
 //
 
-// XFAIL: linux
 
 import Swift
 import Foundation
