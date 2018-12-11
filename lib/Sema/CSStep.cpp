4120976ce21b1849aca2eb2b14ca74b5ad8e33f9
Doug Gregor <dgregor@apple.com>
2018-12-11 15:14:24 -0800

    SIMDOperators hack failure

--- lib/Sema/CSStep.cpp
+++ lib/Sema/CSStep.cpp
@@ -490,6 +490,15 @@ bool DisjunctionStep::shouldStopAt(const DisjunctionChoice &choice) const {
           shortCircuitDisjunctionAt(choice, lastChoice));
 }
 
+bool swift::isSIMDOperator(ValueDecl *value) {
+  auto func = dyn_cast<FuncDecl>(value);
+  if (!func)
+    return false;
+
+  return func->isOperator() &&
+         func->getParentModule()->getName().is("SIMDOperators");
+}
+
 bool DisjunctionStep::shortCircuitDisjunctionAt(
     Constraint *currentChoice, Constraint *lastSuccessfulChoice) const {
   auto &ctx = CS.getASTContext();
@@ -534,6 +543,17 @@ bool DisjunctionStep::shortCircuitDisjunctionAt(
   // Implicit conversions are better than checked casts.
   if (currentChoice->getKind() == ConstraintKind::CheckedCast)
     return true;
+
+  // If we have an operator from the SIMDOperators module, and the prior
+  // choice was not from the SIMDOperators module, we're done.
+  if (currentChoice->getKind() == ConstraintKind::BindOverload &&
+      currentChoice->getOverloadChoice().isDecl() &&
+      isSIMDOperator(currentChoice->getOverloadChoice().getDecl()) &&
+      lastSuccessfulChoice->getKind() == ConstraintKind::BindOverload &&
+      lastSuccessfulChoice->getOverloadChoice().isDecl() &&
+      !isSIMDOperator(lastSuccessfulChoice->getOverloadChoice().getDecl())) {
+    return true;
+  }
 
   return false;
 }
