4b033ac6571110ad177f9db69f54fe9d7de9b3c0
Pavel Yaskevich <pyaskevich@apple.com>
2018-12-14 17:48:07 -0800

    [TypeChecker] NFC: Add for test-cases for rdar://problem/46742002

--- test/Constraints/rdar46742002.swift
+++ test/Constraints/rdar46742002.swift
@@ -1,12 +1,28 @@
 // RUN: %target-typecheck-verify-swift
 
+prefix  operator +++
+postfix operator ---
+
 protocol P : class {}
 
 class F<T> {
   func wait() throws -> T { fatalError() }
 }
 
-func bar(_ a: F<P>, _ b: F<P>) throws {
-  _ = (try  a.wait()) === (try  b.wait()) // Ok
-  _ = (try? a.wait()) === (try? b.wait()) // Ok
+func foo(_ v: AnyObject?) {}
+func bar(_ a: AnyObject?, _ b: AnyObject?) {}
+
+prefix  func +++(_ v: AnyObject?) {}
+postfix func ---(_ v: AnyObject?) {}
+
+func baz(_ a: F<P>, _ b: F<P>) throws {
+  _ = (try  a.wait()) === (try  b.wait())   // Ok
+  _ = (try? a.wait()) === (try? b.wait())   // Ok
+  _ = foo(try? a.wait())                    // Ok
+  _ = foo((try? a.wait()))                  // Ok
+  _ = bar((try? a.wait()), (try? b.wait())) // Ok
+  _ = +++(try? a.wait())                    // Ok
+  _ = +++((try? a.wait()))                  // Ok
+  _ = (try? a.wait())---                    // Ok
+  _ = ((try? a.wait()))---                  // Ok
 }
