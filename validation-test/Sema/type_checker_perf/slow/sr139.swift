e21a96f4ab06a1e240d7e51ad72fab182d81ea0b
Doug Gregor <dgregor@apple.com>
2018-12-17 11:43:45 -0800

    [SIMD] Update tests to reflect the SIMD operators in the standard library
    Introducing the SIMD operators back into the standard library regresses
    one test (SR-139 goes exponential against without the designated types
    for operatores feature). Split that part of the test out.

---
+++ validation-test/Sema/type_checker_perf/slow/sr139.swift
@@ -0,0 +1,7 @@
+// RUN: %target-typecheck-verify-swift
+// RUN: %target-swift-frontend -typecheck %s -solver-enable-operator-designated-types
+
+// SR-139:
+// Infinite recursion parsing bitwise operators
+let x = UInt32(0x1FF)&0xFF << 24 | UInt32(0x1FF)&0xFF << 16 | UInt32(0x1FF)&0xFF << 8 | (UInt32(0x1FF)&0xFF);  // expected-error {{reasonable time}}
+
