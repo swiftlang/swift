4164600bede963272ae5cf0689980879e69843de
Xi Ge <xi_ge@apple.com>
2018-12-18 11:40:15 -0800

    migrator: set up placeholder migration scripts for swift 4.2

--- test/Migrator/stdlib_rename.swift
+++ test/Migrator/stdlib_rename.swift
@@ -1,6 +1,8 @@
 // REQUIRES: objc_interop
 // RUN: %empty-directory(%t) && %target-swift-frontend -c -update-code -primary-file %s -F %S/mock-sdk -emit-migrated-file-path %t/stdlib_rename.swift.result -emit-remap-file-path %t/stdlib_rename.swift.remap -o /dev/null
 // RUN: diff -u %S/stdlib_rename.swift.expected %t/stdlib_rename.swift.result
+// RUN: %empty-directory(%t) && %target-swift-frontend -c -update-code -primary-file %s -F %S/mock-sdk -emit-migrated-file-path %t/stdlib_rename.swift.result -emit-remap-file-path %t/stdlib_rename.swift.remap -o /dev/null -swift-version 4.2
+// RUN: diff -u %S/stdlib_rename.swift.expected %t/stdlib_rename.swift.result
 
 func test1(_ a: [String], s: String) {
   _ = a.index(of: s)
