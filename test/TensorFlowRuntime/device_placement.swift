480b2bd9b30198da2aae887d592f084e6ee852c8
Parker Schuh <parkers@google.com>
2018-12-17 17:53:42 -0800

    Adding support for setting the device id at runtime.

---
+++ test/TensorFlowRuntime/device_placement.swift
@@ -0,0 +1,28 @@
+// RUN: %target-run-simple-swift %swift-tensorflow-test-run-extra-options
+// RUN: %target-run-dynamic-compilation-swift %swift-tensorflow-test-run-extra-options
+// REQUIRES: executable_test
+// REQUIRES: swift_test_mode_optimize
+
+// Test device placement API.
+
+import TensorFlow
+#if TPU
+import TensorFlowUnittestTPU
+#else
+import TensorFlowUnittest
+#endif
+import StdlibUnittest
+
+var DevicePlacementTests = TestSuite("DevicePlacement")
+
+DevicePlacementTests.testAllBackends("GroupWithSize2") {
+  // Need 2 or more CPU devices for this test.
+  let x = Tensor<Float>(1.0)
+
+  withDefaultDevice() {
+    let t = Tensor<Float>(1.0) + Tensor<Float>(2.0)
+    expectEqualWithScalarTensor(3, t)
+  }
+}
+
+runAllTests()
