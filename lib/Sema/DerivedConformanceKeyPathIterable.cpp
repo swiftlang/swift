89b3fe2df4f5ea61c66ea7649176ec3fe6bb56b5
Dan Zheng <fangda.zheng@gmail.com>
2018-12-26 02:36:41 -0500

    Add `KeyPathIterable` protocol and synthesis.
    The `KeyPathIterable` protocol represents types whose values provide
    custom key paths to properties or elements.
    
    For types that conform to `KeyPathIterable`, the compiler can synthesize
    a default implementation of `allKeyPaths` based on the type's stored
    properties.
    
    ---
    
    `KeyPathIterable` enables generic machine learning optimizers and will
    be used to replace `ParameterGroup`.
    
    The `ParameterGroup` protocol requirements are not sufficiently general
    for many use cases: it is limited to a single parameter type and does not
    support joint iteration over parameters for multiple `ParameterGroup`
    instances.
    
    `KeyPathIterable` solves these problems. In addition, it is a generally
    useful language feature that can model both static stored properties
    and custom dynamic properties.

---
+++ lib/Sema/DerivedConformanceKeyPathIterable.cpp
@@ -0,0 +1,134 @@
+//===--- DerivedConformanceKeyPathIterable.cpp ----------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2017 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+//
+// This file implements explicit derivation of the KeyPathIterable protocol for
+// a nominal type.
+//
+//===----------------------------------------------------------------------===//
+
+#include "CodeSynthesis.h"
+#include "TypeChecker.h"
+#include "swift/AST/Decl.h"
+#include "swift/AST/Expr.h"
+#include "swift/AST/GenericSignature.h"
+#include "swift/AST/Module.h"
+#include "swift/AST/ParameterList.h"
+#include "swift/AST/Pattern.h"
+#include "swift/AST/ProtocolConformance.h"
+#include "swift/AST/Stmt.h"
+#include "swift/AST/Types.h"
+#include "DerivedConformances.h"
+
+using namespace swift;
+
+bool DerivedConformance::canDeriveKeyPathIterable(NominalTypeDecl *nominal) {
+  return !nominal->getStoredProperties().empty();
+}
+
+// Compute `PartialKeyPathType<Nominal>`, bound to the given nominal
+// declaration's type.
+static Type computePartialKeyPathType(NominalTypeDecl *nominal) {
+  auto &C = nominal->getASTContext();
+  auto nominalType = nominal->getDeclaredInterfaceType();
+  if (!nominalType || nominalType->hasError())
+    return nullptr;
+  auto *partialKeyPathDecl = cast<ClassDecl>(C.getPartialKeyPathDecl());
+  return BoundGenericClassType::get(partialKeyPathDecl, /*parent*/ Type(),
+                                    {nominal->getDeclaredInterfaceType()});
+}
+
+// Compute `AllKeyPaths` associated type for the given nominal declaration.
+// It should be `[PartialKeyPath<Nominal>]`.
+static ArraySliceType *computeAllKeyPathsType(NominalTypeDecl *nominal) {
+  auto partialKeyPathType = computePartialKeyPathType(nominal);
+  return ArraySliceType::get(partialKeyPathType);
+}
+
+// Synthesize body for the `allKeyPaths` computed property getter.
+static void
+deriveBodyKeyPathIterable_allKeyPaths(AbstractFunctionDecl *funcDecl) {
+  auto *nominal = funcDecl->getDeclContext()->getSelfNominalTypeDecl();
+  auto &C = nominal->getASTContext();
+
+  auto *nominalTypeExpr = TypeExpr::createForDecl(SourceLoc(), nominal,
+                                                  funcDecl, /*Implicit*/ true);
+
+  // Create array of key path expressions to stored properties.
+  llvm::SmallVector<Expr *, 2> keyPathExprs;
+  for (auto member : nominal->getStoredProperties()) {
+    auto *dotExpr = new (C)
+        UnresolvedDotExpr(nominalTypeExpr, SourceLoc(), member->getFullName(),
+                          DeclNameLoc(), /*Implicit*/ true);
+    auto *keyPathExpr =
+        new (C) KeyPathExpr(SourceLoc(), dotExpr, nullptr, /*Implicit*/ true);
+    keyPathExprs.push_back(keyPathExpr);
+  }
+  // Return array of all key path expressions.
+  auto keyPathsArrayExpr =
+      ArrayExpr::create(C, SourceLoc(), keyPathExprs, {}, SourceLoc());
+  auto *returnStmt = new (C) ReturnStmt(SourceLoc(), keyPathsArrayExpr);
+  auto *body = BraceStmt::create(C, SourceLoc(), {returnStmt}, SourceLoc(),
+                                 /*Implicit*/ true);
+  funcDecl->setBody(BraceStmt::create(C, SourceLoc(), {body}, SourceLoc(),
+                                      /*Implicit*/ true));
+}
+
+// Synthesize the `allKeyPaths` computed property declaration.
+static ValueDecl *
+deriveKeyPathIterable_allKeyPaths(DerivedConformance &derived) {
+  auto nominal = derived.Nominal;
+  auto &C = derived.TC.Context;
+
+  auto returnInterfaceTy = computeAllKeyPathsType(nominal);
+  auto returnTy =
+      derived.getConformanceContext()->mapTypeIntoContext(returnInterfaceTy);
+
+  // Create `allKeyPaths` property declaration.
+  VarDecl *allKeyPathsDecl;
+  PatternBindingDecl *pbDecl;
+  std::tie(allKeyPathsDecl, pbDecl) = derived.declareDerivedProperty(
+      C.Id_allKeyPaths, returnInterfaceTy, returnTy, /*isStatic*/ false,
+      /*isFinal*/ true);
+
+  // Create `allKeyPaths` getter.
+  auto *getterDecl = derived.declareDerivedPropertyGetter(
+      derived.TC, allKeyPathsDecl, returnTy);
+  getterDecl->setBodySynthesizer(deriveBodyKeyPathIterable_allKeyPaths);
+  allKeyPathsDecl->setAccessors(StorageImplInfo::getImmutableComputed(),
+                                SourceLoc(), {getterDecl}, SourceLoc());
+  derived.addMembersToConformanceContext({getterDecl, allKeyPathsDecl, pbDecl});
+  return allKeyPathsDecl;
+}
+
+static Type deriveKeyPathIterable_AllKeyPaths(DerivedConformance &derived) {
+  auto *rawInterfaceType = computeAllKeyPathsType(derived.Nominal);
+  return derived.getConformanceContext()->mapTypeIntoContext(rawInterfaceType);
+}
+
+ValueDecl *DerivedConformance::deriveKeyPathIterable(ValueDecl *requirement) {
+  if (requirement->getBaseName() == TC.Context.Id_allKeyPaths) {
+    return deriveKeyPathIterable_allKeyPaths(*this);
+  }
+  TC.diagnose(requirement->getLoc(),
+              diag::broken_key_path_iterable_requirement);
+  return nullptr;
+}
+
+Type DerivedConformance::deriveKeyPathIterable(
+    AssociatedTypeDecl *requirement) {
+  if (requirement->getBaseName() == TC.Context.Id_AllKeyPaths) {
+    return deriveKeyPathIterable_AllKeyPaths(*this);
+  }
+  TC.diagnose(requirement->getLoc(),
+              diag::broken_key_path_iterable_requirement);
+  return nullptr;
+}
---
+++ test/Sema/key_path_iterable.swift
@@ -0,0 +1,140 @@
+// SWIFT_ENABLE_TENSORFLOW
+// RUN: %target-swift-frontend -typecheck -verify %s -verify-ignore-unknown
+// REQUIRES: tensorflow
+
+import TensorFlow
+
+struct Parameters : KeyPathIterable {
+  var w: Float
+  var b: Float
+}
+var params = Parameters(w: 1, b: 2)
+assert(params.allKeyPaths.count == 2)
+assert(params.allKeyPaths(to: Float.self).count == 2)
+assert(params.allKeyPaths(to: Int.self).count == 0)
+for kp in params.allWritableKeyPaths(to: Float.self) {
+  params[keyPath: kp] *= 2
+}
+
+struct TensorParameters : KeyPathIterable {
+  var w: Tensor<Float>
+  var b: Tensor<Float>
+
+  // Non-stored-property members should not affect synthesis.
+  var computed: Float {
+    return (w + b).scalarized()
+  }
+  func foo() {}
+  typealias Foo = Int
+}
+
+extension TensorParameters : VectorNumeric {
+  static var zero: TensorParameters {
+    return TensorParameters(w: Tensor(0), b: Tensor(0))
+  }
+  static func + (lhs: TensorParameters, rhs: TensorParameters) -> TensorParameters {
+    return TensorParameters(w: lhs.w + rhs.w, b: lhs.b + rhs.b)
+  }
+  static func - (lhs: TensorParameters, rhs: TensorParameters) -> TensorParameters {
+    return TensorParameters(w: lhs.w + rhs.w, b: lhs.b + rhs.b)
+  }
+  typealias Scalar = Tensor<Float>
+  static func * (lhs: Scalar, rhs: TensorParameters) -> TensorParameters { 
+    return TensorParameters(w: lhs + rhs.w, b: lhs + rhs.b)
+  }
+}
+
+struct HeterogeneousParameters : KeyPathIterable {
+  var float: Float
+  var double: Double
+  var tensor: Tensor<Float>
+  var params: Parameters
+}
+let hetero = HeterogeneousParameters(float: 0, double: 0,
+                                     tensor: Tensor(0), params: params)
+assert(hetero.allKeyPaths.count == 4)
+assert(hetero.recursivelyAllKeyPaths.count == 6)
+assert(hetero.allKeyPaths(to: Float.self).count == 1)
+assert(hetero.recursivelyAllKeyPaths(to: Float.self).count == 3)
+assert(hetero.allKeyPaths(to: Tensor<Float>.self).count == 1)
+assert(hetero.allKeyPaths(to: Parameters.self).count == 1)
+assert(hetero.allKeyPaths(to: Int.self).count == 0)
+
+// Test type in generic context.
+struct A<T> {
+  struct B<U, V> {
+    struct GenericContextParams : KeyPathIterable {
+      var params: Parameters
+      var float: Float
+    }
+  }
+}
+
+// Test generic optimizer.
+
+// `pow` is defined in Darwin on `Float` and `Double`, but there doesn't exist
+// a generic version for `FloatingPoint`.
+// This is a manual definition.
+func pow<T : BinaryFloatingPoint>(_ x: T, _ y: T) -> T {
+  // return T(pow(Float(x), Float(y)))
+  return T(pow(Double(x), Double(y)))
+}
+
+struct AdamOptimizer<P : KeyPathIterable, Scalar : BinaryFloatingPoint>
+  where P : VectorNumeric, P.Scalar == Tensor<Scalar>
+{
+  let learningRate: Scalar
+  var beta1: Scalar
+  var beta2: Scalar
+  var epsilon: Scalar
+
+  init(
+    learningRate: Scalar = 1e-3,
+    beta1: Scalar = 0.9,
+    beta2: Scalar = 0.999,
+    epsilon: Scalar = 1e-8
+  ) {
+    self.learningRate = learningRate
+    self.beta1 = beta1
+    self.beta2 = beta2
+    self.epsilon = epsilon
+  }
+
+  var step: Scalar = 0
+  var firstMoments: P = P.zero
+  var secondMoments: P = P.zero
+
+  mutating func fitParameters(
+    parameters: inout P, withGradients gradients: P
+  ) {
+    for kp in parameters.recursivelyAllWritableKeyPaths(to: Tensor<Scalar>.self) {
+      firstMoments[keyPath: kp] =
+        firstMoments[keyPath: kp] * beta1 + (1 - beta1) * gradients[keyPath: kp]
+      secondMoments[keyPath: kp] =
+        firstMoments[keyPath: kp] * beta2 + (1 - beta2) * gradients[keyPath: kp] * gradients[keyPath: kp]
+
+      let denominator = sqrt(secondMoments[keyPath: kp]) + epsilon
+      step += 1
+      let biasCorrection1 = 1 - pow(beta1, step)
+      let biasCorrection2 = 1 - pow(beta2, step)
+      let stepSize = learningRate * sqrt(biasCorrection2) / biasCorrection1
+      parameters[keyPath: kp] -= stepSize * firstMoments[keyPath: kp] / denominator
+    }
+  }
+}
+
+func testOptimizer<P : KeyPathIterable, Scalar : BinaryFloatingPoint>(
+  parameters: inout P, withGradients gradients: P
+)
+  where P : VectorNumeric, P.Scalar == Tensor<Scalar>
+{
+  var optimizer = AdamOptimizer<P, Scalar>()
+  print(parameters)
+  for _ in 0..<5 {
+    optimizer.fitParameters(parameters: &parameters, withGradients: gradients)
+    print(parameters)
+  }
+}
+var tensorParams = TensorParameters.zero
+let gradients = TensorParameters(w: Tensor(10), b: Tensor(10))
+testOptimizer(parameters: &tensorParams, withGradients: gradients)
