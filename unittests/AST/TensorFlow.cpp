7f5468eff77761751f1acb709ff349c11af5cf7c
Gogul Balakrishnan <bgogul@google.com>
2018-12-28 16:02:51 -0800

    Updated the unit test with different scenarios

--- unittests/AST/TensorFlow.cpp
+++ unittests/AST/TensorFlow.cpp
@@ -21,18 +21,81 @@
 using namespace swift;
 using namespace swift::unittest;
 
-TEST(TensorFlow, ContainsTensorValueWorks) {
-  TestContext C;
-  auto *structDecl = C.makeNominal<StructDecl>("PairedStructure");
-  structDecl->dump();
-  auto *structType = StructType::get(structDecl, Type(), C.Ctx);
-  structType->dump();
-  auto *structType2 = StructType::get(structDecl, structType, C.Ctx);
-  structType2->dump();
-  // auto* blah = ArraySliceType::get(structType);
-  // blah->dump();
-  // auto *arrayDecl = C.makeNominal<StructDecl>("Array", Type(), {structDecl});
-  // arrayDecl->dump();
-  // auto
-  EXPECT_TRUE(1 == 1);
+class TypeContainsTensorFlowValueTest : public ::testing::Test {
+protected:
+  TestContext testContext;
+  ASTContext& Ctx;
+  Type floatType;
+  BoundGenericClassType *tensorHandleType;
+  tf::TypeContainsTensorFlowValue tctf;
+
+  TypeContainsTensorFlowValueTest()
+      : Ctx(testContext.Ctx), floatType(createFloatType()),
+        tensorHandleType(createTensorHandleType(floatType)) {}
+
+  Type createStructWithField(const char *name, Type fieldType) {
+    auto *structDecl = testContext.makeNominal<StructDecl>(name);
+    structDecl->computeType();
+    auto *structType = StructType::get(structDecl, Type(), Ctx);
+    auto *varDecl = new (Ctx) VarDecl(
+        /*IsStatic*/ false, VarDecl::Specifier::Var,
+        /*IsCaptureList*/ false, SourceLoc(), Ctx.getIdentifier("field"),
+        structDecl);
+    varDecl->setInterfaceType(fieldType);
+    structDecl->addMember(varDecl);
+    return structType;
+  }
+
+  bool containsTensorFlowValue(Type t) {
+    // TODO: Add tests with CheckHigherOrderFunctions set to true.
+    return tctf.containsTensorFlowValue(t, /*CheckHigherOrderFunctions*/false);
+  }
+
+
+private:
+  Type createFloatType() {
+    // Float type.
+    auto *floatDecl = testContext.makeNominal<StructDecl>("Float");
+    return StructType::get(floatDecl, Type(), Ctx);
+  }
+
+  BoundGenericClassType *createTensorHandleType(Type genericType) {
+    auto *tensorDecl = testContext.makeNominal<ClassDecl>("TensorHandle");
+
+    // Generic parameter list.
+    auto *floatGenericParamDecl = new (Ctx)
+        GenericTypeParamDecl(tensorDecl->getDeclContext(),
+                             Ctx.getIdentifier("Scalar"), SourceLoc(), 0, 0);
+    auto *paramList = GenericParamList::create(
+        Ctx, SourceLoc(), {floatGenericParamDecl}, SourceLoc());
+    tensorDecl->setGenericParams(paramList);
+
+    return BoundGenericClassType::get(tensorDecl, Type(), {genericType});
+  }
+};
+
+TEST_F(TypeContainsTensorFlowValueTest, ClassifiesCorrectly) {
+  EXPECT_TRUE(containsTensorFlowValue(tensorHandleType));
+  EXPECT_TRUE(containsTensorFlowValue(
+      createStructWithField("StructWithTensor", tensorHandleType)));
+  EXPECT_FALSE(containsTensorFlowValue(
+      createStructWithField("StructWithNoTensor", floatType)));
+}
+
+TEST_F(TypeContainsTensorFlowValueTest, WorksForRecursiveTypes) {
+  // Creates a recursive type for testing purposes. Note that this is not a
+  // valid swift type, but should suffice for the purposes of this unit test.
+  //
+  auto *recursiveDecl = testContext.makeNominal<StructDecl>("RecursiveStruct");
+  recursiveDecl->computeType();
+  auto *recursiveType = StructType::get(recursiveDecl, Type(), Ctx);
+  // Add a field of the recursiveType.
+  // (This is not possible in swift, but ok for tests.)
+  auto *varDecl = new (Ctx) VarDecl(
+      /*IsStatic*/ false, VarDecl::Specifier::Var,
+      /*IsCaptureList*/ false, SourceLoc(), Ctx.getIdentifier("someField"),
+      recursiveDecl);
+  varDecl->setInterfaceType(recursiveType);
+  recursiveDecl->addMember(varDecl);
+  EXPECT_FALSE(containsTensorFlowValue(recursiveType));
 }
