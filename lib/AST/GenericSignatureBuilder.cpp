98287ca2399ae51b2808607c97d139215e4f3584
Slava Pestov <spestov@apple.com>
2019-01-07 14:20:00 -0500

    AST: Fix failure to diagnose with redundant same-type constraints
    This test case used to crash because the source-location-based order
    here did not handle invalid source locations, but Doug fixed it already
    in #21656. However, fixing it to sort invalid locations after valid
    locations meant that the diagnostic code would pick the invalid location
    to emit the diagnostic on, so no diagnostic was emitted.
    
    This is not a big deal since the diagnostics in question are warnings,
    but we do want to emit these redundant constraint warnings to avoid
    confusing users if possible so let's fix it to do the right thing.
    
    More completely fixes rdar://problem/46848889 (but it was already not
    crashing after Doug's fix).

--- lib/AST/GenericSignatureBuilder.cpp
+++ lib/AST/GenericSignatureBuilder.cpp
@@ -6838,7 +6838,7 @@ void GenericSignatureBuilder::checkSameTypeConstraints(
           // Put invalid locations after valid ones.
           if (locA.isInvalid() || locB.isInvalid()) {
             if (locA.isInvalid() != locB.isInvalid())
-              return locA.isInvalid() ? 1 : -1;
+              return locA.isValid() ? 1 : -1;
 
             return 0;
           }
