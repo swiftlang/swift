15aaa1e777970fa03c381067674f5ffd3be692ce
Lance Parker <lance_parker@apple.com>
2019-01-08 13:55:29 -0800

    [stdlib]String normalization functions (#21026)
    * fast/foreignNormalize functions

--- validation-test/stdlib/String.swift
+++ validation-test/stdlib/String.swift
@@ -2199,10 +2199,30 @@ for test in comparisonTestCases {
   }
 
   StringTests.test("Comparison.OpaqueSubstring.\(test.strings)")
-    .skip(.linuxAny(reason: "NSSlowString requires ObjC interop"))
-    .code {
+  .skip(.linuxAny(reason: "NSSlowString requires ObjC interop"))
+  .code {
     test.testOpaqueSubstrings()
   }
+}
+
+StringTests.test("Comparison.Substrings") {
+  let str = "abcdefg"
+  let expectedStr = "bcdef"
+  let substring = str.dropFirst().dropLast()
+  
+  expectEqual(expectedStr, substring)
+}
+
+StringTests.test("Comparison.Substrings/Opaque")
+.skip(.linuxAny(reason: "NSSlowString requires ObjC interop"))
+.code {
+#if _runtime(_ObjC)
+  let str = NSSlowString(string: "abcdefg") as String
+  let expectedStr = NSSlowString(string: "bcdef") as String
+  let substring = str.dropFirst().dropLast()
+  
+  expectEqual(expectedStr, substring)
+#endif
 }
 
 StringTests.test("NormalizationBufferCrashRegressionTest") {
@@ -2216,8 +2236,21 @@ StringTests.test("NormalizationCheck") {
   let str = "\u{0336}\u{0344}\u{0357}\u{0343}\u{0314}\u{0351}\u{0340}\u{0300}\u{0340}\u{0360}\u{0314}\u{0357}\u{0315}\u{0301}\u{0344}a"
   let nfcCodeUnits = str._nfcCodeUnits
   let expectedCodeUnits: [UInt8] = [0xCC, 0xB6, 0xCC, 0x88, 0xCC, 0x81, 0xCD, 0x97, 0xCC, 0x93, 0xCC, 0x94, 0xCD, 0x91, 0xCC, 0x80, 0xCC, 0x80, 0xCC, 0x80, 0xCC, 0x94, 0xCD, 0x97, 0xCC, 0x81, 0xCC, 0x88, 0xCC, 0x81, 0xCC, 0x95, 0xCD, 0xA0, 0x61]
-  
+
   expectEqual(expectedCodeUnits, nfcCodeUnits)
+}
+
+StringTests.test("NormalizationCheck/Opaque")
+.skip(.linuxAny(reason: "NSSlowString requires ObjC interop"))
+.code {
+#if _runtime(_ObjC)
+  let str = "\u{0336}\u{0344}\u{0357}\u{0343}\u{0314}\u{0351}\u{0340}\u{0300}\u{0340}\u{0360}\u{0314}\u{0357}\u{0315}\u{0301}\u{0344}a"
+  let opaqueString = NSSlowString(string: str) as String
+  let nfcCodeUnits = opaqueString._nfcCodeUnits
+  let expectedCodeUnits: [UInt8] = [0xCC, 0xB6, 0xCC, 0x88, 0xCC, 0x81, 0xCD, 0x97, 0xCC, 0x93, 0xCC, 0x94, 0xCD, 0x91, 0xCC, 0x80, 0xCC, 0x80, 0xCC, 0x80, 0xCC, 0x94, 0xCD, 0x97, 0xCC, 0x81, 0xCC, 0x88, 0xCC, 0x81, 0xCC, 0x95, 0xCD, 0xA0, 0x61]
+
+  expectEqual(expectedCodeUnits, nfcCodeUnits)
+#endif
 }
 
 runAllTests()
