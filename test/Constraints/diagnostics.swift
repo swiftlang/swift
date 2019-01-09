50ab6523252a6ee4207666b02962694a0a00aa71
Steve (Numerics) Canon <scanon@apple.com>
2019-01-09 08:30:12 -0500

    Implement Float16

--- test/Constraints/diagnostics.swift
+++ test/Constraints/diagnostics.swift
@@ -742,7 +742,7 @@ example21890157.property = "confusing"  // expected-error {{value of optional ty
 struct UnaryOp {}
 
 _ = -UnaryOp() // expected-error {{unary operator '-' cannot be applied to an operand of type 'UnaryOp'}}
-// expected-note@-1 {{overloads for '-' exist with these partially matching parameter lists: (Double), (Float)}}
+// expected-note@-1 {{overloads for '-' exist with these partially matching parameter lists: (Double), (Float), (Float16), (Float80)}}
 
 
 // <rdar://problem/23433271> Swift compiler segfault in failure diagnosis
