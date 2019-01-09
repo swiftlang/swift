0d8091082d1d91cd6f3ef589f2dbe3e81f6c9caa
Gogul Balakrishnan <bgogul@google.com>
2019-01-09 00:04:41 -0800

    Remove warnings from affected runtime tests.

--- test/TensorFlowRuntime/sese_regions.swift
+++ test/TensorFlowRuntime/sese_regions.swift
@@ -20,8 +20,6 @@ public func testSharedRegion(_ count : Int32) -> Tensor<Int32> {
   do {
     if count > 0 {
       if count < 100 {
-        // expected-warning @+2 {{value implicitly copied to the host}}
-        // expected-warning @+1 {{value implicitly copied to the host}}
         result += 1
       } else {
         result += 2
@@ -29,26 +27,18 @@ public func testSharedRegion(_ count : Int32) -> Tensor<Int32> {
       }
     } else {
       if count > -100 {
-        // expected-warning @+2 {{value implicitly copied to the host}}
-        // expected-warning @+1 {{value implicitly copied to the host}}
         result += 3
       } else {
         result += 4
         throw TestError.NegativeOutOfBound
       }
-    } // expected-note {{value used here}}
-    // expected-note @+1 {{value used here}}
-  } catch { // expected-note {{value used here}}
-    // expected-warning @+2 {{value implicitly copied to the host}}
-    // expected-warning @+1 {{value implicitly copied to the host}}
+    }
+  } catch {
     result += 5
   }
   return result
 }
 
-// expected-note @+3 {{value used here}}
-// expected-note @+2 {{value used here}}
-// expected-note @+1 {{value used here}}
 SESERegionTests.testAllBackends("testSharedRegion") { 
   expectEqualWithScalarTensor(1, testSharedRegion(99))
   expectEqualWithScalarTensor(7, testSharedRegion(101))
@@ -81,8 +71,6 @@ public func testSharedRegionWithLoop(_ count : Int32) -> Tensor<Int32> {
   do {
     if count > 0 {
       if count < 100 {
-        // expected-warning @+2 {{value implicitly copied to the host}}
-        // expected-warning @+1 {{value implicitly copied to the host}}
         result += 1
       } else {
         result += 2
@@ -90,20 +78,15 @@ public func testSharedRegionWithLoop(_ count : Int32) -> Tensor<Int32> {
       }
     } else {
       if count > -100 {
-        // expected-warning @+2 {{value implicitly copied to the host}}
-        // expected-warning @+1 {{value implicitly copied to the host}}
         result += 3
       } else {
         result += 4
         throw TestError.NegativeOutOfBound
       }
-    } // expected-note {{value used here}}
-    // expected-note @+1 {{value used here}}
-  } catch { // expected-note {{value used here}}
+    }
+  } catch {
     var i: Int32 = 0
     while i < 2 {
-      // expected-warning @+2 {{value implicitly copied to the host}}
-      // expected-warning @+1 {{value implicitly copied to the host}}
       result += 5
       i += 1
     }
@@ -111,9 +94,6 @@ public func testSharedRegionWithLoop(_ count : Int32) -> Tensor<Int32> {
   return result
 }
 
-// expected-note @+3 {{value used here}}
-// expected-note @+2 {{value used here}}
-// expected-note @+1 {{value used here}}
 SESERegionTests.testAllBackends("testSharedRegionWithLoop") { 
   expectEqualWithScalarTensor(1, testSharedRegionWithLoop(99))
 #if !CUDA
