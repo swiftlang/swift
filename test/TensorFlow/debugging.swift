9a2f437e1dd20702cfff4fe52d3b4b8ff0d5d797
Gogul Balakrishnan <bgogul@google.com>
2019-01-10 09:58:37 -0800

    Remove irrelvant checks in debugging.swift test.

--- test/TensorFlow/debugging.swift
+++ test/TensorFlow/debugging.swift
@@ -1,6 +1,7 @@
 // RUN: %target-swift-frontend -Xllvm -tf-dynamic-compilation=false -Xllvm -tf-dump-intermediates -emit-sil %s | %FileCheck %s
 
 import TensorFlow
+import CTensorFlow
 
 public func basicDebugValues(_ x: Tensor<Float>) {
   let y = x + 1
@@ -24,38 +25,16 @@ public func noCopyForOpaqueHandles() {
           Toutput_types$dtype: [Float.tensorFlowDataType], output_shapes: [TensorShape()])
 }
 
-// CHECK-LABEL: --- TFPartition Accelerator Result: {{.*}}basicDebugValues
-// CHECK: @{{.*}}basicDebugValues{{.*}}.tf
-// CHECK: [[ONE:%.*]] = graph_op "Const"
-// CHECK: [[ADD_RESULT:%.*]] = graph_op "Add"
-// CHECK: graph_op "Square"([[ADD_RESULT]] : $TensorHandle<Float>) {T$dtype: i32 1, __device: "/job:localhost/replica:0/task:0/device:CPU:0"} : $TensorHandle<Float>
-
-
-// CHECK-LABEL: --- TFPartition Accelerator Result: {{.*}}debugValuesInLoop
-// CHECK: bb0
-// CHECK:   [[SQUARED:%.*]] = graph_op "Square"
-// CHECK-NEXT:   br bb1([[SQUARED]] : $TensorHandle<Float>)
-// CHECK: bb1
-// CHECK:   graph_op "tfc.RecvFromHost"()
-// CHECK:   [[COND:%.*]] = graph_op "tf_tensor_to_i1"
-// CHECK:   cond_br [[COND]], bb2, bb3
-// CHECK: bb2:
-// CHECK:   [[ADD_RESULT:%.*]] = graph_op "Add"
-// CHECK-NEXT:   br bb1([[ADD_RESULT]]
-
-
-// CHECK-LABEL: ---- PARTITION STATE FOR FUNCTION ${{.*}}noCopyForOpaqueHandle
-// CHECK: result values:
-// CHECK-NOT: graph_op "TensorSliceDataset"([%4 : $TensorHandle<Float>])
-
-
-// CHECK-LABEL: --- TFPartition Host Result: {{.*}}basicDebugValues
+// CHECK-LABEL: // basicDebugValues(_:)
+// CHECK-NEXT: sil @{{.*}}basicDebugValues{{.*}} {
 // CHECK: debug_value %{{.*}} : $Tensor<Float>, let, name "x", argno 1
 // CHECK: debug_value %{{.*}} : $Tensor<Float>, let, name "y"
 // CHECK: debug_value %{{.*}} : $Tensor<Float>, let, name "z"
 
 
-// CHECK-LABEL: --- TFPartition Host Result: {{.*}}debugValuesInLoop{{.*}}
-// CHECK: bb0(%0 : $Tensor<Float>):
+
+// CHECK-LABEL: // debugValuesInLoop(_:_:)
+// CHECK-NEXT: sil @{{.*}}debugValuesInLoop{{.*}} {
+// CHECK: bb0(%0 : $Tensor<Float>, %1 : $Int):
 // CHECK:   debug_value %{{.*}} : $Tensor<Float>, let, name "x", argno 1
 // CHECK:   debug_value %{{.*}} : $Int, let, name "i"
