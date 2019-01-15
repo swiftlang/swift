152fab22a1c93cf947ab158f9505263bba41d9d3
Slava Pestov <sviatoslav.pestov@gmail.com>
2019-01-15 01:05:06 -0500

    Revert "IRGen: Ensure that default witness thunks are emitted in the same thread as the protocol descriptor"

--- lib/IRGen/IRGen.cpp
+++ lib/IRGen/IRGen.cpp
@@ -964,11 +964,11 @@ static void performParallelIRGeneration(
   }
 
   // Emit the module contents.
-  irgen.emitGlobalTopLevel();
+  irgen.emitGlobalTopLevel(true /*emitForParallelEmission*/);
 
   for (auto *File : M->getFiles()) {
     if (auto *SF = dyn_cast<SourceFile>(File)) {
-      CurrentIGMPtr IGM = irgen.getGenModule(SF);
+      IRGenModule *IGM = irgen.getGenModule(SF);
       IGM->emitSourceFile(*SF);
     } else {
       File->collectLinkLibraries([&](LinkLibrary LinkLib) {
