5a9a59ec6af67e9662706ee7a8acc5ec986dd98a
Adrian Prantl <aprantl@apple.com>
2019-01-15 11:01:10 -0800

    Add missing enum case

--- lib/Basic/Platform.cpp
+++ lib/Basic/Platform.cpp
@@ -140,6 +140,7 @@ StringRef swift::getPlatformNameForTriple(const llvm::Triple &triple) {
   case llvm::Triple::AMDPAL:
   case llvm::Triple::HermitCore:
   case llvm::Triple::Hurd:
+  case llvm::Triple::COWS:
     return "";
   case llvm::Triple::Darwin:
   case llvm::Triple::MacOSX:
