e9b064d0fcdbe2640db7901aa3e6f9de73bd08e3
Joe Groff <jgroff@apple.com>
2019-01-14 18:01:26 -0800

    IRGen: Substitute out opaque types when emitting associated types.

--- lib/IRGen/GenProto.cpp
+++ lib/IRGen/GenProto.cpp
@@ -1468,6 +1468,19 @@ void WitnessTableBuilder::defineAssociatedTypeWitnessTableAccessFunction(
                                 AssociatedConformance requirement,
                                 CanType associatedType,
                                 ProtocolConformanceRef associatedConformance) {
+  // Substitute out opaque types.
+  auto substAssocType = associatedType
+    .substOpaqueTypesWithUnderlyingTypes(ResilienceExpansion::Minimal)
+    ->getCanonicalType();
+  if (substAssocType != associatedType) {
+    auto substAssocConformance = associatedConformance
+      .substOpaqueTypesWithUnderlyingTypes(associatedType,
+                                           ResilienceExpansion::Minimal);
+    
+    associatedType = substAssocType;
+    associatedConformance = substAssocConformance;
+  }
+  
   bool hasArchetype = associatedType->hasArchetype();
 
   assert(isa<NormalProtocolConformance>(Conformance) && "has associated type");
