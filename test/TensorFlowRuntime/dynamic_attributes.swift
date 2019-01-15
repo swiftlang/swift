7dab8739751063a248a35ddc1e40bcbafb594eb6
Gogul Balakrishnan <bgogul@google.com>
2019-01-15 10:24:01 -0800

    Reduce flakyness in dynamic_attributes.swift test (#21809)

--- test/TensorFlowRuntime/dynamic_attributes.swift
+++ test/TensorFlowRuntime/dynamic_attributes.swift
@@ -238,38 +238,32 @@ DynamicAttributeTests.testAllBackends("NormalAttribute Float") {
   expectEqual(false, result2.scalar!)
 }
 
-#if !CUDA
 DynamicAttributeTests.testAllBackends("NormalAttribute String") {
   let result: Tensor<Float> = #tfop("Conv2D", convImage, convFilter,
                                     T$dtype: Float.tensorFlowDataType,
                                     strides: [1, 1, 1, 1] as [Int32],
                                     padding: loadVALIDString())
-  expectEqual(convExpectedResult, result.array)
+  expectPointwiseNearlyEqual(convExpectedResult, result.array)
 }
-#endif //!CUDA
 
 DynamicAttributeTests.testAllBackends("NormalAttribute Array<Bool>") {
   // There aren't any ops that take bool list attributes!
 }
 
-#if !CUDA
 DynamicAttributeTests.testAllBackends("NormalAttribute Array<Int32>") {
   let result = convImage.convolved2D(withFilter: convFilter,
                                      strides: loadStridesInt32(),
                                      padding: .valid)
-  expectEqual(convExpectedResult, result.array)
+  expectPointwiseNearlyEqual(convExpectedResult, result.array)
 }
-#endif //!CUDA
 
-#if !CUDA
 DynamicAttributeTests.testAllBackends("NormalAttribute Array<Int64>") {
   let result: Tensor<Float> = #tfop("Conv2D", convImage, convFilter,
                                     T$dtype: Float.tensorFlowDataType,
                                     strides: loadStridesInt64(),
                                     padding: "VALID")
-  expectEqual(convExpectedResult, result.array)
+  expectPointwiseNearlyEqual(convExpectedResult, result.array)
 }
-#endif //!CUDA
 
 DynamicAttributeTests.testAllBackends("NormalAttribute Array<Double>") {
   let input = Tensor<Double>([-1, 0.1, 4.3, 1.2])
