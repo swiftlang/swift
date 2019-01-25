3501568fc058a5062f56008d6fc0b645e0cd604d
Stephen Canon <stephentyrone@gmail.com>
2019-01-24 23:08:29 -0500

    Make SIMD types Codable. (#22092)
    * Make SIMD types codable. We're considering this a bugfix.
    
    This is a very tiny ABI change, in that user-defined SIMD types compiled with an earlier version of 5.0 will be missing the necessary conformance to Codable. Discussed with Ben, and we're OK with this because we don't think there are such types yet, and it can be fixed with a recompile.
    
    * Add basic tests

---
+++ test/stdlib/SIMD.swift
@@ -0,0 +1,71 @@
+// RUN: %target-run-simple-swift
+// REQUIRES: executable_test
+// REQUIRES: objc_interop
+
+import Foundation
+import StdlibUnittest
+
+let SIMDCodableTests = TestSuite("SIMDCodable")
+
+func testRoundTrip<T>(_ for: T.Type)
+where T : SIMD, T.Scalar : FixedWidthInteger {
+  let input = T.random(in: T.Scalar.min ... T.Scalar.max)
+  let encoder = JSONEncoder()
+  let decoder = JSONDecoder()
+  do {
+    let data = try encoder.encode(input)
+    let output = try decoder.decode(T.self, from: data)
+    expectEqual(input, output)
+  }
+  catch {
+    expectUnreachableCatch(error)
+  }
+}
+
+func testRoundTrip<T>(_ for: T.Type)
+where T : SIMD, T.Scalar : BinaryFloatingPoint,
+      T.Scalar.RawSignificand : FixedWidthInteger {
+  let input = T.random(in: -16 ..< 16)
+  let encoder = JSONEncoder()
+  let decoder = JSONDecoder()
+  do {
+    let data = try encoder.encode(input)
+    let output = try decoder.decode(T.self, from: data)
+    expectEqual(input, output)
+  }
+  catch {
+    expectUnreachableCatch(error)
+  }
+}
+
+// Very basic round-trip test. We can be much more sophisticated in the future,
+// but we want to at least exercise the API. Also need to add some negative
+// tests for the error paths, and test a more substantial set of types.
+SIMDCodableTests.test("roundTrip") {
+  testRoundTrip(SIMD2<Int8>.self)
+  testRoundTrip(SIMD3<Int8>.self)
+  testRoundTrip(SIMD4<Int8>.self)
+  testRoundTrip(SIMD2<UInt8>.self)
+  testRoundTrip(SIMD3<UInt8>.self)
+  testRoundTrip(SIMD4<UInt8>.self)
+  testRoundTrip(SIMD2<Int32>.self)
+  testRoundTrip(SIMD3<Int32>.self)
+  testRoundTrip(SIMD4<Int32>.self)
+  testRoundTrip(SIMD2<UInt32>.self)
+  testRoundTrip(SIMD3<UInt32>.self)
+  testRoundTrip(SIMD4<UInt32>.self)
+  testRoundTrip(SIMD2<Int>.self)
+  testRoundTrip(SIMD3<Int>.self)
+  testRoundTrip(SIMD4<Int>.self)
+  testRoundTrip(SIMD2<UInt>.self)
+  testRoundTrip(SIMD3<UInt>.self)
+  testRoundTrip(SIMD4<UInt>.self)
+  testRoundTrip(SIMD2<Float>.self)
+  testRoundTrip(SIMD3<Float>.self)
+  testRoundTrip(SIMD4<Float>.self)
+  testRoundTrip(SIMD2<Double>.self)
+  testRoundTrip(SIMD3<Double>.self)
+  testRoundTrip(SIMD4<Double>.self)
+}
+
+runAllTests()
