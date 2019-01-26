cc35f385edcd739ccb9068b69f63beec62c2cf40
Erik Eckstein <eeckstein@apple.com>
2019-01-25 16:48:41 -0800

    Runtime: fix mangling of nested private generic classes
    The generated mangled names were wrong and couldn't be demangled.
    For example the runtime generated class name for PrivateGeneric was wrong:
    
    class OuterClass {
        private class PrivateGeneric<T, U> { }
    }
    
    rdar://problem/47529629

--- lib/Demangling/OldRemangler.cpp
+++ lib/Demangling/OldRemangler.cpp
@@ -1910,6 +1910,7 @@ void Remangler::mangleGenericArgs(Node *node, EntityContext &ctx) {
     break;
   }
 
+  case Node::Kind::AnonymousContext:
   case Node::Kind::Extension: {
     mangleGenericArgs(node->getChild(1), ctx);
     break;
