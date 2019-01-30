bcd022a4669e266c1fb9cce020bf1eba9cb5eb2f
John McCall <rjmccall@apple.com>
2019-01-30 01:01:45 -0500

    Protocol requirement overrides must match in mutating-ness.
    Without this change, SILGen will crash when compiling a use of the
    derived protocol's requirement: it will instead attempt to use
    the base protocol's requirement, but the code will have been
    type-checked incorrectly for that.
    
    This has a potential for source-compatibility impact if anyone's
    using explicit override checking for their protocol requirements:
    reasonable idioms like overriding a mutating requirement with a
    non-mutating one will no longer count as an override.  However,
    this is arguably a bug-fix, because the current designed intent
    of protocol override checking is to not allow any differences in
    type, even "covariant" changes like making a mutating requirement
    non-mutating.  Moreover, we believe explicit override checking in
    protocols is quite uncommon, so the overall compatibility impact
    will be low.
    
    This also has a potential for ABI impact whenever something that
    was once an override becomes a non-override and thus requires a
    new entry.  It might require a contrived test case to demonstrate
    that while using the derived entry, but it's quite possible to
    imagine a situation where the derived entry is not used directly
    but nonetheless has ABI impact.
    
    Furthermore, as part of developing this patch (earlier versions of
    which used stricter rules in places), I discovered a number of
    places where the standard library was unintentionally introducing
    a new requirement in a derived protocol when it intended only to
    guide associated type deduction.  Fixing that (as I have in this
    patch) *definitely* has ABI impact.

---
+++ test/SILGen/protocol_overrides.swift
@@ -0,0 +1,107 @@
+// RUN: %target-swift-emit-silgen -module-name main %s | %FileCheck %s
+
+// rdar://47470420
+
+/*****************************************************************************/
+/* Methods *******************************************************************/
+/*****************************************************************************/
+
+protocol HasMutatingMethod {
+  mutating func foo()
+}
+
+protocol HasMutatingMethodClone: HasMutatingMethod {
+  mutating func foo()
+}
+extension HasMutatingMethodClone {
+// CHECK-LABEL: sil hidden @$s4main22HasMutatingMethodClonePAAE10performFooyyF :
+// CHECK:         witness_method $Self, #HasMutatingMethod.foo!1
+  mutating func performFoo() {
+    foo()
+  }
+}
+
+protocol HasNonMutatingMethod: HasMutatingMethod {
+  func foo()
+}
+extension HasNonMutatingMethod {
+// CHECK-LABEL: sil hidden @$s4main20HasNonMutatingMethodPAAE10performFooyyF :
+// CHECK:         witness_method $Self, #HasNonMutatingMethod.foo!1 :
+  func performFoo() {
+    foo()
+  }
+}
+
+/*****************************************************************************/
+/* Getters *******************************************************************/
+/*****************************************************************************/
+
+protocol HasMutatingGetter {
+  var foo: Int { mutating get }
+}
+
+protocol HasMutatingGetterClone: HasMutatingGetter {
+  var foo: Int { mutating get }
+}
+extension HasMutatingGetterClone {
+// CHECK-LABEL: sil hidden @$s4main22HasMutatingGetterClonePAAE11readFromFooSiyF :
+// CHECK:         witness_method $Self, #HasMutatingGetter.foo!getter.1 :
+  mutating func readFromFoo() -> Int {
+    return foo
+  }
+}
+
+protocol HasNonMutatingGetter: HasMutatingGetter {
+  var foo: Int { get }
+}
+extension HasNonMutatingGetter {
+// CHECK-LABEL: sil hidden @$s4main20HasNonMutatingGetterPAAE11readFromFooSiyF :
+// CHECK:         witness_method $Self, #HasNonMutatingGetter.foo!getter.1 :
+  func readFromFoo() -> Int {
+    return foo
+  }
+}
+
+/*****************************************************************************/
+/* Setters *******************************************************************/
+/*****************************************************************************/
+
+protocol HasMutatingSetter {
+  var foo: Int { get set }
+}
+
+protocol HasMutatingSetterClone: HasMutatingSetter {
+  var foo: Int { get set }
+}
+extension HasMutatingSetterClone {
+// CHECK-LABEL: sil hidden @$s4main22HasMutatingSetterClonePAAE11readFromFooSiyF :
+// CHECK:         witness_method $Self, #HasMutatingSetter.foo!getter.1 :
+  func readFromFoo() -> Int {
+    return foo
+  }
+
+// CHECK-LABEL: sil hidden @$s4main22HasMutatingSetterClonePAAE10writeToFooyySiF :
+// CHECK:         witness_method $Self, #HasMutatingSetter.foo!setter.1 :
+  mutating func writeToFoo(_ x: Int) {
+    foo = x
+  }
+}
+
+protocol HasNonMutatingSetter: HasMutatingSetter {
+  var foo: Int { get nonmutating set }
+}
+extension HasNonMutatingSetter {
+//   It's unfortunate that this uses a new witness table entry,
+//   but we can live with it.
+// CHECK-LABEL: sil hidden @$s4main20HasNonMutatingSetterPAAE11readFromFooSiyF :
+// CHECK:         witness_method $Self, #HasNonMutatingSetter.foo!getter.1 :
+  func readFromFoo() -> Int {
+    return foo
+  }
+
+// CHECK-LABEL: sil hidden @$s4main20HasNonMutatingSetterPAAE10writeToFooyySiF :
+// CHECK:         witness_method $Self, #HasNonMutatingSetter.foo!setter.1 :
+  func writeToFoo(_ x: Int) {
+    foo = x
+  }
+}
