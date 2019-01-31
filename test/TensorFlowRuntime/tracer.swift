2e50a93786c5e177c07c987983fb6c291434b518
Gogul Balakrishnan <bgogul@google.com>
2019-01-30 18:40:04 -0800

    Add unittest for dtypes.

--- test/TensorFlowRuntime/tracer.swift
+++ test/TensorFlowRuntime/tracer.swift
@@ -60,6 +60,59 @@ TracerTests.testAllBackends("Basic") {
   expectNearlyEqualWithScalarTensor(1.0, result2)
 }
 
+TracerTests.testAllBackends("AllDtypeSupport") {
+  func tracee<S: TensorGroup>(state: S, data: S) -> (S, S) {
+    return (state, data)
+  }
+
+  func traceAndRun<T>(_ v: T) -> (Tensor<T>, Tensor<T>)
+  where T: TensorFlowScalar & Numeric {
+    let state = Tensor<T>(v)
+    let data = Tensor<T>(v + 4)
+    let tracedFn = _graph(with: state, in: tracee)
+    return tracedFn(state, data)
+  }
+
+  func traceAndCheckNearlyEqual<T>(_ v: T)
+  where T: TensorFlowScalar & FloatingPoint & ExpressibleByFloatLiteral {
+    let (newState, newData) = traceAndRun(v)
+
+    expectNearlyEqualWithScalarTensor(v, newState)
+    expectNearlyEqualWithScalarTensor(v + 4, newData)
+  }
+
+  func traceAndCheckEqual<T>(_ v: T)
+  where T: TensorFlowScalar & Numeric & Comparable {
+    let (newState, newData) = traceAndRun(v)
+    expectEqualWithScalarTensor(v, newState)
+    expectEqualWithScalarTensor(v + 4, newData)
+  }
+
+  // FloatingPoint
+  let f: Float = 1.0
+  traceAndCheckNearlyEqual(f)
+  let d: Double = 2.0
+  traceAndCheckNearlyEqual(d)
+
+  // Int variants
+  let ui8: UInt8 = 1
+  traceAndCheckEqual(ui8)
+  let i8: Int8 = 2
+  traceAndCheckEqual(i8)
+  let ui16: UInt16 = 3
+  traceAndCheckEqual(ui16)
+  let i16: Int16 = 4
+  traceAndCheckEqual(i16)
+  let ui32: UInt32 = 5
+  traceAndCheckEqual(ui32)
+  let i32: Int32 = 6
+  traceAndCheckEqual(i32)
+  let ui64: UInt64 = 7
+  traceAndCheckEqual(ui64)
+  let i64: Int64 = 8
+  traceAndCheckEqual(i64)
+}
+
 TracerTests.testAllBackends("Basic_IntermediateTensors") {
   func tracee(state: Tensor<Float>, data: Data) -> (Tensor<Float>, Result) {
     // Create an intermediate tensor value, which the tracing infra needs to
