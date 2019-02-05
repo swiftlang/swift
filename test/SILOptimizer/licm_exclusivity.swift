ae6b5129d342793de8f2bc2e7b831dee8bef6708
Joe Shajrawi <shajrawi@apple.com>
2019-02-05 13:40:29 -0800

    update licm_exclusivity.swift test for String view changes

--- test/SILOptimizer/licm_exclusivity.swift
+++ test/SILOptimizer/licm_exclusivity.swift
@@ -35,14 +35,13 @@ func run_ReversedArray(_ N: Int) {
 // TEST2: Hoist and Sink pairs attempt
 // TEST2: Hoisted
 
-// FIXME: <rdar://problem/45931225> Re-enable the below
-//
-// xTESTSIL-LABEL: sil @$s16licm_exclusivity20count_unicodeScalarsyySS17UnicodeScalarViewVF : $@convention(thin) (@guaranteed String.UnicodeScalarView) -> () {
-// xTESTSIL: bb0(%0 : $String.UnicodeScalarView)
-// xTESTSIL-NEXT: %1 = global_addr @$s16licm_exclusivity5countSivp : $*Int
-// xTESTSIL: begin_access [modify] [dynamic] [no_nested_conflict] %1 : $*Int
-// xTESTSIL: end_access
-// xTESTSIL: return
+// TESTSIL-LABEL: sil @$s16licm_exclusivity20count_unicodeScalarsyySS17UnicodeScalarViewVF : $@convention(thin) (@guaranteed String.UnicodeScalarView) -> () {
+// TESTSIL: bb0(%0 : $String.UnicodeScalarView)
+// TESTSIL: bb5:
+// TESTSIL-NEXT: [[A1:%.*]] = global_addr @$s16licm_exclusivity5countSivp : $*Int
+// TESTSIL: begin_access [modify] [dynamic] [no_nested_conflict] [[A1]] : $*Int
+// TESTSIL: end_access
+// TESTSIL: return
 var count: Int = 0
 public func count_unicodeScalars(_ s: String.UnicodeScalarView) {
   for _ in s {
