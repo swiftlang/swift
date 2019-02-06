d08b46c47e8c3086523c6d5873b209e26237197e
Daniel Rodríguez Troitiño <danielrodriguez@fb.com>
2019-02-06 10:51:55 -0800

    [tests] Standarize the checks for Darwin, Glibc and MSVCRT.
    Different tests used different os checks for importing Darwin, Glibc and
    MSVCRT. This commit use the same pattern for importing those libraries,
    in order to avoid the #else branches of the incorrect patterns to be
    applied to the wrong platform. This was very normal for Android, which
    normally should follow the Linux branches, but sometimes was trying to
    import Darwin or not importing anything.
    
    The standarized pattern imports Darwin for macOS, iOS, tvOS and watchOS.
    It imports Glibc for Linux, FreeBSD, PS4, Android, Cygwin and Haiku; and
    imports MSVCRT for Windows. If a new platform is introduced, the else
    branch will report an error, so the new platform can be added to one of
    the branches (or maybe add a new specific branch).
    
    In some cases  the standard pattern was modified because some test required
    it (importing extra modules, or extra type aliases), and in some other
    cases some branches were removed because the test will not have used
    them (but it is not exhaustive, so there might be some unnecessary
    branches).
    
    This should, at least, fix three tests for Android (the three
    dynamic_replacement*.swift ones).

--- test/stdlib/mmap.swift
+++ test/stdlib/mmap.swift
@@ -3,10 +3,12 @@
 // UNSUPPORTED: OS=windows-msvc
 
 import StdlibUnittest
-#if os(Linux) || os(Android)
+#if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
+  import Darwin
+#elseif os(Linux) || os(FreeBSD) || os(PS4) || os(Android) || os(Cygwin) || os(Haiku)
   import Glibc
 #else
-  import Darwin
+#error("Unsupported platform")
 #endif
 
 var MMapTests = TestSuite("MMaptests")
