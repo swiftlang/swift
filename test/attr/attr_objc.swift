swiftlang/swift

commit a8fb416f9e34ca8ce86b547d7cca77a8fc55a970

[Type checker] Don't implicitly propagate @objc to read/modify accessors.
Fixes rdar://problem/46699152.

lib/Sema/TypeCheckDeclObjC.cpp
@@ -1066,6 +1066,21 @@ Optional<ObjCReason> shouldMarkAsObjC(const ValueDecl *VD, bool allowImplicit) {
     if (VD->getFormalAccess() <= AccessLevel::FilePrivate)
       return false;
 
+    if (auto accessor = dyn_cast<AccessorDecl>(VD)) {
+      switch (accessor->getAccessorKind()) {
+      case AccessorKind::DidSet:
+      case AccessorKind::Modify:
+      case AccessorKind::Read:
+      case AccessorKind::WillSet:
+        return false;
+
+      case AccessorKind::MutableAddress:
+      case AccessorKind::Address:
+      case AccessorKind::Get:
+      case AccessorKind::Set:
+        break;
+      }
+    }
     return true;
   };
 
@@ -1106,7 +1121,7 @@ Optional<ObjCReason> shouldMarkAsObjC(const ValueDecl *VD, bool allowImplicit) {
        cast<ExtensionDecl>(VD->getDeclContext())->getAttrs()
         .hasAttribute<NonObjCAttr>()))
     return None;
-  if (isMemberOfObjCClassExtension(VD))
+  if (isMemberOfObjCClassExtension(VD) && canInferImplicitObjC())
     return ObjCReason(ObjCReason::MemberOfObjCExtension);
   if (isMemberOfObjCMembersClass(VD) && canInferImplicitObjC())
     return ObjCReason(ObjCReason::MemberOfObjCMembersClass);

test/attr/attr_objc.swift
@@ -2312,3 +2312,18 @@ protocol ObjCProtocolWithWeakProperty {
 protocol ObjCProtocolWithUnownedProperty {
    unowned var unownedProp: AnyObject { get set } // okay
 }
+
+// rdar://problem/46699152: errors about read/modify accessors being implicitly
+// marked @objc.
+@objc class MyObjCClass: NSObject {}
+
+@objc
+extension MyObjCClass {
+    @objc
+    static var objCVarInObjCExtension: Bool {
+        get {
+            return true
+        }
+        set {}
+    }
+}

commit 5bc7a69546004dc1117802c5795e60c3ebfb6983

`@objc extension` makes even 'private' members '@objc'
This regressed in a8fb416f9e, when we started using a helper function
in more places.

rdar://problem/47869562

lib/Sema/TypeCheckDeclObjC.cpp
@@ -1054,7 +1054,7 @@ Optional<ObjCReason> shouldMarkAsObjC(const ValueDecl *VD, bool allowImplicit) {
       protocolContext && protocolContext->isObjC();
 
   // Local function to determine whether we can implicitly infer @objc.
-  auto canInferImplicitObjC = [&] {
+  auto canInferImplicitObjC = [&](bool allowAnyAccess) {
     if (VD->isInvalid())
       return false;
     if (VD->isOperator())
@@ -1064,7 +1064,7 @@ Optional<ObjCReason> shouldMarkAsObjC(const ValueDecl *VD, bool allowImplicit) {
     if (!allowImplicit && VD->isImplicit())
       return false;
 
-    if (VD->getFormalAccess() <= AccessLevel::FilePrivate)
+    if (!allowAnyAccess && VD->getFormalAccess() <= AccessLevel::FilePrivate)
       return false;
 
     if (auto accessor = dyn_cast<AccessorDecl>(VD)) {
@@ -1115,6 +1115,7 @@ Optional<ObjCReason> shouldMarkAsObjC(const ValueDecl *VD, bool allowImplicit) {
   // A member of an @objc protocol is implicitly @objc.
   if (isMemberOfObjCProtocol)
     return ObjCReason(ObjCReason::MemberOfObjCProtocol);
+
   // A @nonobjc is not @objc, even if it is an override of an @objc, so check
   // for @nonobjc first.
   if (VD->getAttrs().hasAttribute<NonObjCAttr>() ||
@@ -1122,10 +1123,14 @@ Optional<ObjCReason> shouldMarkAsObjC(const ValueDecl *VD, bool allowImplicit) {
        cast<ExtensionDecl>(VD->getDeclContext())->getAttrs()
         .hasAttribute<NonObjCAttr>()))
     return None;
-  if (isMemberOfObjCClassExtension(VD) && canInferImplicitObjC())
+
+  if (isMemberOfObjCClassExtension(VD) && 
+      canInferImplicitObjC(/*allowAnyAccess*/true))
     return ObjCReason(ObjCReason::MemberOfObjCExtension);
-  if (isMemberOfObjCMembersClass(VD) && canInferImplicitObjC())
+  if (isMemberOfObjCMembersClass(VD) && 
+      canInferImplicitObjC(/*allowAnyAccess*/false))
     return ObjCReason(ObjCReason::MemberOfObjCMembersClass);
+
   // An override of an @objc declaration is implicitly @objc.
   if (VD->getOverriddenDecl() && VD->getOverriddenDecl()->isObjC())
     return ObjCReason(ObjCReason::OverridesObjC);
@@ -1177,7 +1182,7 @@ Optional<ObjCReason> shouldMarkAsObjC(const ValueDecl *VD, bool allowImplicit) {
   // (and extensions thereof) whose class hierarchies originate in Objective-C,
   // e.g., which derive from NSObject, so long as the members have internal
   // access or greater.
-  if (!canInferImplicitObjC())
+  if (!canInferImplicitObjC(/*allowAnyAccess*/false))
     return None;
 
   // If this declaration is part of a class with implicitly @objc members,

test/attr/attr_objc.swift
@@ -2325,4 +2325,18 @@ extension MyObjCClass {
         }
         set {}
     }
+
+    // CHECK: {{^}} @objc private dynamic func stillExposedToObjCDespiteBeingPrivate()
+    private func stillExposedToObjCDespiteBeingPrivate() {}
 }
+
+@objc private extension MyObjCClass {
+  // CHECK: {{^}} @objc dynamic func alsoExposedToObjCDespiteBeingPrivate()
+  func alsoExposedToObjCDespiteBeingPrivate() {}
+}
+
+@objcMembers class VeryObjCClass: NSObject {
+  // CHECK: {{^}} private func notExposedToObjC()
+  private func notExposedToObjC() {}
+}
+