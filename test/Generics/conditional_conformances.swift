6fd1600534b9252b4a87dbf90b63d1449bc377eb
Pavel Yaskevich <pyaskevich@apple.com>
2019-02-08 11:16:54 -0800

    [ConstraintSystem] Diagnose conditional requirement failures via fixes
    Extend existing `RequirementFailure` functionality to support
    conditional requirement failures. Such fixes are introduced
    only if the parent type requirement has been matched successfully.
    
    Resolves: rdar://problem/47871590

--- test/Generics/conditional_conformances.swift
+++ test/Generics/conditional_conformances.swift
@@ -12,55 +12,36 @@ protocol P6: P2 {}
 protocol Assoc { associatedtype AT }
 
 func takes_P2<X: P2>(_: X) {}
-// FIXME: "requirement specified as..." isn't accurate below
-// expected-note@-2{{candidate requires that the types 'U' and 'V' be equivalent (requirement specified as 'U' == 'V')}}
-// expected-note@-3{{requirement from conditional conformance of 'SameTypeGeneric<U, V>' to 'P2'}}
-// expected-note@-4{{candidate requires that the types 'U' and 'Int' be equivalent (requirement specified as 'U' == 'Int')}}
-// expected-note@-5{{requirement from conditional conformance of 'SameTypeGeneric<U, Int>' to 'P2'}}
-// expected-note@-6{{candidate requires that the types 'Int' and 'Float' be equivalent (requirement specified as 'Int' == 'Float')}}
-// expected-note@-7{{requirement from conditional conformance of 'SameTypeGeneric<Int, Float>' to 'P2'}}
-// expected-note@-8{{candidate requires that 'C1' inherit from 'U' (requirement specified as 'U' : 'C1')}}
-// expected-note@-9{{requirement from conditional conformance of 'ClassFree<U>' to 'P2'}}
-// expected-note@-10{{candidate requires that 'C3' inherit from 'U' (requirement specified as 'U' : 'C3')}}
-// expected-note@-11{{requirement from conditional conformance of 'ClassMoreSpecific<U>' to 'P2'}}
-// expected-note@-12{{candidate requires that 'C1' inherit from 'Int' (requirement specified as 'Int' : 'C1')}}
-// expected-note@-13{{requirement from conditional conformance of 'SubclassBad' to 'P2'}}
-// expected-note@-14{{candidate requires that the types 'Float' and 'Int' be equivalent (requirement specified as 'Float' == 'Int')}}
-// expected-note@-15{{requirement from conditional conformance of 'Infer<Constrained<U>, V>' to 'P2'}}
-// expected-note@-16{{candidate requires that the types 'Constrained<U>' and 'Constrained<V>' be equivalent (requirement specified as 'Constrained<U>' == 'Constrained<V>')}}
-// expected-note@-17{{candidate requires that the types 'U' and 'Int' be equivalent (requirement specified as 'U' == 'Int')}}
-// expected-note@-18{{requirement from conditional conformance of 'SameType<Float>' to 'P2'}}
-// expected-note@-19{{requirement from conditional conformance of 'SameType<U>' to 'P2'}}
+// expected-note@-1 {{candidate requires that the types 'U' and 'V' be equivalent (requirement specified as 'U' == 'V')}}
+// expected-note@-2 {{requirement from conditional conformance of 'SameTypeGeneric<U, V>' to 'P2'}}
+// expected-note@-3 {{requirement from conditional conformance of 'SubclassBad' to 'P2'}}
+// expected-note@-4 {{candidate requires that 'C1' inherit from 'U' (requirement specified as 'U' : 'C1')}}
+// expected-note@-5 {{requirement from conditional conformance of 'ClassFree<U>' to 'P2'}}
+// expected-note@-6 {{candidate requires that 'C1' inherit from 'Int' (requirement specified as 'Int' : 'C1')}}
 func takes_P5<X: P5>(_: X) {}
 
 struct Free<T> {}
 // CHECK-LABEL: ExtensionDecl line={{.*}} base=Free<T>
 // CHECK-NEXT: (normal_conformance type=Free<T> protocol=P2
 // CHECK-NEXT:   conforms_to: T P1)
-extension Free: P2 where T: P1 {}
+extension Free: P2 where T: P1 {} // expected-note {{requirement from conditional conformance of 'Free<U>' to 'P2'}}
 func free_good<U: P1>(_: U) {
     takes_P2(Free<U>())
 }
 func free_bad<U>(_: U) {
-    takes_P2(Free<U>()) // expected-error{{type 'U' does not conform to protocol 'P1'}}}}
-    // expected-error@-1{{'<X where X : P2> (X) -> ()' requires that 'U' conform to 'P1'}}
-    // expected-note@-2{{requirement specified as 'U' : 'P1'}}
-    // expected-note@-3{{requirement from conditional conformance of 'Free<U>' to 'P2'}}
+    takes_P2(Free<U>()) // expected-error{{global function 'takes_P2' requires that 'U' conform to 'P1'}}
 }
 
 struct Constrained<T: P1> {}
 // CHECK-LABEL: ExtensionDecl line={{.*}} base=Constrained<T>
 // CHECK-NEXT: (normal_conformance type=Constrained<T> protocol=P2
 // CHECK-NEXT:   conforms_to: T P3)
-extension Constrained: P2 where T: P3 {}
+extension Constrained: P2 where T: P3 {} // expected-note {{requirement from conditional conformance of 'Constrained<U>' to 'P2'}}
 func constrained_good<U: P1 & P3>(_: U) {
     takes_P2(Constrained<U>())
 }
 func constrained_bad<U: P1>(_: U) {
-    takes_P2(Constrained<U>()) // expected-error{{type 'U' does not conform to protocol 'P3'}}
-    // expected-error@-1{{'<X where X : P2> (X) -> ()' requires that 'U' conform to 'P3'}}
-    // expected-note@-2{{requirement specified as 'U' : 'P3'}}
-    // expected-note@-3{{requirement from conditional conformance of 'Constrained<U>' to 'P2'}}
+  takes_P2(Constrained<U>()) // expected-error{{global function 'takes_P2' requires that 'U' conform to 'P3'}}
 }
 
 struct RedundantSame<T: P1> {}
@@ -77,15 +58,12 @@ struct OverlappingSub<T: P1> {}
 // CHECK-LABEL: ExtensionDecl line={{.*}} base=OverlappingSub<T>
 // CHECK-NEXT: (normal_conformance type=OverlappingSub<T> protocol=P2
 // CHECK-NEXT:   conforms_to: T P4)
-extension OverlappingSub: P2 where T: P4 {}
+extension OverlappingSub: P2 where T: P4 {} // expected-note {{requirement from conditional conformance of 'OverlappingSub<U>' to 'P2'}}
 func overlapping_sub_good<U: P4>(_: U) {
     takes_P2(OverlappingSub<U>())
 }
 func overlapping_sub_bad<U: P1>(_: U) {
-    takes_P2(OverlappingSub<U>()) // expected-error{{type 'U' does not conform to protocol 'P4'}}
-    // expected-error@-1{{'<X where X : P2> (X) -> ()' requires that 'U' conform to 'P4'}}
-    // expected-note@-2{{requirement specified as 'U' : 'P4'}}
-    // expected-note@-3{{requirement from conditional conformance of 'OverlappingSub<U>' to 'P2'}}
+  takes_P2(OverlappingSub<U>()) // expected-error{{global function 'takes_P2' requires that 'U' conform to 'P4'}}
 }
 
 
@@ -94,12 +72,14 @@ struct SameType<T> {}
 // CHECK-NEXT: (normal_conformance type=SameType<T> protocol=P2
 // CHECK-NEXT:   same_type: T Int)
 extension SameType: P2 where T == Int {}
+// expected-note@-1 {{requirement from conditional conformance of 'SameType<U>' to 'P2'}}
+// expected-note@-2 {{requirement from conditional conformance of 'SameType<Float>' to 'P2'}}
 func same_type_good() {
     takes_P2(SameType<Int>())
 }
 func same_type_bad<U>(_: U) {
-    takes_P2(SameType<U>()) // expected-error{{cannot invoke 'takes_P2(_:)' with an argument list of type '(SameType<U>)'}}
-    takes_P2(SameType<Float>()) // expected-error{{cannot invoke 'takes_P2(_:)' with an argument list of type '(SameType<Float>)'}}
+  takes_P2(SameType<U>()) // expected-error{{global function 'takes_P2' requires the types 'U' and 'Int' be equivalent}}
+  takes_P2(SameType<Float>()) // expected-error{{global function 'takes_P2' requires the types 'Float' and 'Int' be equivalent}}
 }
 
 
@@ -108,6 +88,8 @@ struct SameTypeGeneric<T, U> {}
 // CHECK-NEXT: (normal_conformance type=SameTypeGeneric<T, U> protocol=P2
 // CHECK-NEXT:   same_type: T U)
 extension SameTypeGeneric: P2 where T == U {}
+// expected-note@-1 {{requirement from conditional conformance of 'SameTypeGeneric<U, Int>' to 'P2'}}
+// expected-note@-2 {{requirement from conditional conformance of 'SameTypeGeneric<Int, Float>' to 'P2'}}
 func same_type_generic_good<U, V>(_: U, _: V)
   where U: Assoc, V: Assoc, U.AT == V.AT
 {
@@ -116,12 +98,12 @@ func same_type_generic_good<U, V>(_: U, _: V)
     takes_P2(SameTypeGeneric<U.AT, V.AT>())
 }
 func same_type_bad<U, V>(_: U, _: V) {
-    takes_P2(SameTypeGeneric<U, V>())
-    // expected-error@-1{{cannot invoke 'takes_P2(_:)' with an argument list of type '(SameTypeGeneric<U, V>)'}}
-    takes_P2(SameTypeGeneric<U, Int>())
-    // expected-error@-1{{cannot invoke 'takes_P2(_:)' with an argument list of type '(SameTypeGeneric<U, Int>)'}}
-    takes_P2(SameTypeGeneric<Int, Float>())
-    // expected-error@-1{{cannot invoke 'takes_P2(_:)' with an argument list of type '(SameTypeGeneric<Int, Float>)'}}
+  takes_P2(SameTypeGeneric<U, V>())
+  // expected-error@-1{{cannot invoke 'takes_P2(_:)' with an argument list of type '(SameTypeGeneric<U, V>)'}}
+  takes_P2(SameTypeGeneric<U, Int>())
+  // expected-error@-1{{global function 'takes_P2' requires the types 'U' and 'Int' be equivalent}}
+  takes_P2(SameTypeGeneric<Int, Float>())
+  // expected-error@-1{{global function 'takes_P2' requires the types 'Int' and 'Float' be equivalent}}
 }
 
 
@@ -131,14 +113,16 @@ struct Infer<T, U> {}
 // CHECK-NEXT:   same_type: T Constrained<U>
 // CHECK-NEXT:   conforms_to:  U P1)
 extension Infer: P2 where T == Constrained<U> {}
+// expected-note@-1 2 {{requirement from conditional conformance of 'Infer<Constrained<U>, V>' to 'P2'}}
 func infer_good<U: P1>(_: U) {
     takes_P2(Infer<Constrained<U>, U>())
 }
 func infer_bad<U: P1, V>(_: U, _: V) {
-    takes_P2(Infer<Constrained<U>, V>())
-    // expected-error@-1{{cannot invoke 'takes_P2(_:)' with an argument list of type '(Infer<Constrained<U>, V>)'}}
-    takes_P2(Infer<Constrained<V>, V>())
-    // expected-error@-1{{type 'V' does not conform to protocol 'P1'}}
+  takes_P2(Infer<Constrained<U>, V>())
+  // expected-error@-1{{global function 'takes_P2' requires the types 'Constrained<U>' and 'Constrained<V>' be equivalent}}
+  // expected-error@-2{{global function 'takes_P2' requires that 'V' conform to 'P1'}}
+  takes_P2(Infer<Constrained<V>, V>())
+  // expected-error@-1{{type 'V' does not conform to protocol 'P1'}}
 }
 
 struct InferRedundant<T, U: P1> {}
@@ -178,13 +162,13 @@ struct ClassMoreSpecific<T: C1> {}
 // CHECK-LABEL: ExtensionDecl line={{.*}} base=ClassMoreSpecific<T>
 // CHECK-NEXT: (normal_conformance type=ClassMoreSpecific<T> protocol=P2
 // CHECK-NEXT:   superclass: T C3)
-extension ClassMoreSpecific: P2 where T: C3 {}
+extension ClassMoreSpecific: P2 where T: C3 {} // expected-note {{requirement from conditional conformance of 'ClassMoreSpecific<U>' to 'P2'}}
 func class_more_specific_good<U: C3>(_: U) {
     takes_P2(ClassMoreSpecific<U>())
 }
 func class_more_specific_bad<U: C1>(_: U) {
-    takes_P2(ClassMoreSpecific<U>())
-    // expected-error@-1{{cannot invoke 'takes_P2(_:)' with an argument list of type '(ClassMoreSpecific<U>)'}}
+  takes_P2(ClassMoreSpecific<U>())
+  // expected-error@-1{{global function 'takes_P2' requires that 'U' inherit from 'C3'}}
 }
 
 
@@ -214,26 +198,20 @@ struct InheritEqual<T> {}
 // CHECK-LABEL: ExtensionDecl line={{.*}} base=InheritEqual<T>
 // CHECK-NEXT:  (normal_conformance type=InheritEqual<T> protocol=P2
 // CHECK-NEXT:    conforms_to: T P1)
-extension InheritEqual: P2 where T: P1 {}
+extension InheritEqual: P2 where T: P1 {} // expected-note {{requirement from conditional conformance of 'InheritEqual<U>' to 'P2'}}
 // CHECK-LABEL: ExtensionDecl line={{.*}} base=InheritEqual<T>
 // CHECK-NEXT:  (normal_conformance type=InheritEqual<T> protocol=P5
 // CHECK-NEXT:    (normal_conformance type=InheritEqual<T> protocol=P2
 // CHECK-NEXT:      conforms_to: T P1)
 // CHECK-NEXT:    conforms_to: T P1)
-extension InheritEqual: P5 where T: P1 {}
+extension InheritEqual: P5 where T: P1 {} // expected-note {{requirement from conditional conformance of 'InheritEqual<U>' to 'P5'}}
 func inheritequal_good<U: P1>(_: U) {
   takes_P2(InheritEqual<U>())
   takes_P5(InheritEqual<U>())
 }
 func inheritequal_bad<U>(_: U) {
-  takes_P2(InheritEqual<U>()) // expected-error{{type 'U' does not conform to protocol 'P1'}}
-  // expected-error@-1{{'<X where X : P2> (X) -> ()' requires that 'U' conform to 'P1'}}
-  // expected-note@-2{{requirement specified as 'U' : 'P1'}}
-  // expected-note@-3{{requirement from conditional conformance of 'InheritEqual<U>' to 'P2'}}
-  takes_P5(InheritEqual<U>()) // expected-error{{type 'U' does not conform to protocol 'P1'}}
-  // expected-error@-1{{'<X where X : P5> (X) -> ()' requires that 'U' conform to 'P1'}}
-  // expected-note@-2{{requirement specified as 'U' : 'P1'}}
-  // expected-note@-3{{requirement from conditional conformance of 'InheritEqual<U>' to 'P5'}}
+  takes_P2(InheritEqual<U>()) // expected-error{{global function 'takes_P2' requires that 'U' conform to 'P1'}}
+  takes_P5(InheritEqual<U>()) // expected-error{{global function 'takes_P5' requires that 'U' conform to 'P1'}}
 }
 
 struct InheritLess<T> {}
@@ -248,33 +226,24 @@ struct InheritMore<T> {}
 // CHECK-LABEL: ExtensionDecl line={{.*}} base=InheritMore<T>
 // CHECK-NEXT:  (normal_conformance type=InheritMore<T> protocol=P2
 // CHECK-NEXT:    conforms_to: T P1)
-extension InheritMore: P2 where T: P1 {}
+extension InheritMore: P2 where T: P1 {} // expected-note {{requirement from conditional conformance of 'InheritMore<U>' to 'P2'}}
 // CHECK-LABEL: ExtensionDecl line={{.*}} base=InheritMore<T>
 // CHECK-NEXT:  (normal_conformance type=InheritMore<T> protocol=P5
 // CHECK-NEXT:    (normal_conformance type=InheritMore<T> protocol=P2
 // CHECK-NEXT:      conforms_to: T P1)
 // CHECK-NEXT:    conforms_to: T P4)
-extension InheritMore: P5 where T: P4 {}
+extension InheritMore: P5 where T: P4 {} // expected-note 2 {{requirement from conditional conformance of 'InheritMore<U>' to 'P5'}}
 func inheritequal_good_good<U: P4>(_: U) {
   takes_P2(InheritMore<U>())
   takes_P5(InheritMore<U>())
 }
 func inheritequal_good_bad<U: P1>(_: U) {
   takes_P2(InheritMore<U>())
-  takes_P5(InheritMore<U>()) // expected-error{{type 'U' does not conform to protocol 'P4'}}
-  // expected-error@-1{{'<X where X : P5> (X) -> ()' requires that 'U' conform to 'P4'}}
-  // expected-note@-2{{requirement specified as 'U' : 'P4'}}
-  // expected-note@-3{{requirement from conditional conformance of 'InheritMore<U>' to 'P5'}}
+  takes_P5(InheritMore<U>()) // expected-error{{global function 'takes_P5' requires that 'U' conform to 'P4'}}
 }
 func inheritequal_bad_bad<U>(_: U) {
-  takes_P2(InheritMore<U>()) // expected-error{{type 'U' does not conform to protocol 'P1'}}
-  // expected-error@-1{{'<X where X : P2> (X) -> ()' requires that 'U' conform to 'P1'}}
-  // expected-note@-2{{requirement specified as 'U' : 'P1'}}
-  // expected-note@-3{{requirement from conditional conformance of 'InheritMore<U>' to 'P2'}}
-  takes_P5(InheritMore<U>()) // expected-error{{type 'U' does not conform to protocol 'P4'}}
-  // expected-error@-1{{'<X where X : P5> (X) -> ()' requires that 'U' conform to 'P4'}}
-  // expected-note@-2{{requirement specified as 'U' : 'P4'}}
-  // expected-note@-3{{requirement from conditional conformance of 'InheritMore<U>' to 'P5'}}
+  takes_P2(InheritMore<U>()) // expected-error{{global function 'takes_P2' requires that 'U' conform to 'P1'}}
+  takes_P5(InheritMore<U>()) // expected-error{{global function 'takes_P5' requires that 'U' conform to 'P4'}}
 }
 
 struct InheritImplicitOne<T> {}
@@ -379,14 +348,11 @@ extension X1: P8 {
 
 struct X2<T> { }
 
-extension X2: P7 where T: P8, T.A: P7 { }
+extension X2: P7 where T: P8, T.A: P7 { } // expected-note {{requirement from conditional conformance of 'X2<X1>' to 'P7'}}
 
 func takesF7<T: P7>(_: T) { }
 func passesConditionallyNotF7(x21: X2<X1>) {
-  takesF7(x21) // expected-error{{type 'X1.A' (aka 'X0') does not conform to protocol 'P7'}}
-  // expected-error@-1{{'<T where T : P7> (T) -> ()' requires that 'X1.A' (aka 'X0') conform to 'P7'}}
-  // expected-note@-2{{requirement specified as 'X1.A' (aka 'X0') : 'P7'}}
-  // expected-note@-3{{requirement from conditional conformance of 'X2<X1>' to 'P7'}}
+  takesF7(x21) // expected-error{{global function 'takesF7' requires that 'X1.A' (aka 'X0') conform to 'P7'}}
 }
 
 
@@ -424,4 +390,15 @@ protocol P {
 
 extension Foo: P where Bar: P {
   var foo: Foo { return self }
+}
+
+// rdar://problem/47871590
+
+extension BinaryInteger {
+  var foo: Self {
+    return self <= 1
+            ? 1
+            : (2...self).reduce(1, *)
+            // expected-error@-1 {{instance method 'reduce' requires that 'Self.Stride' conform to 'SignedInteger'}}
+  }
 }
