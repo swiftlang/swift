5e5cd76e8d8d379e580c30cb25eac668c754d482
Steve (Numerics) Canon <scanon@apple.com>
2019-02-11 22:09:44 -0800

    Work in progress towards "Mathsable"
    Fundamental decision to be made; should implementation hooks present like `Float.exp(_ x: Float) -> Float` or like `Float.Maths.exp(_ x: Float) -> Float`? Having the intermediate namespace to group them as in the second is definitely nicer, but it requires a little bit of extra machinery, and much more importantly, there doesn't seem to be any way to make access to the static `Maths` associatedtype transparent.

---
+++ utils/SwiftMathsFunctions.py
@@ -0,0 +1,37 @@
+from SwiftFloatingPointTypes import all_floating_point_types
+
+class SwiftMathsFunction(object):
+  def __init__(self, name, cName=None, intrinsic=False, args="x"):
+    self.name = name
+    self.cName = cName if cName is not None else name
+    self.intrinsic = intrinsic
+    self.args = args
+
+  def params(self, prefix="", suffix=""):
+    return ", ".join(
+      map(lambda a: prefix + a + suffix, self.args)
+    )
+  
+  def decl(self, type):
+    return self.name + "(" + self.params("_ ", ": " + type) + ") -> " + type
+  
+  def free_decl(self):
+    return self.name + "<T>(" + self.params("_ ", ": T") + ") -> T where T: Mathsable"
+  
+  def impl(self, type):
+    if self.intrinsic:
+      builtin = "Builtin.int_" + self.name + "_FPIEEE" + str(type.bits)
+      return type.stdlib_name + "(" + builtin + "(" + self.params("","._value") + "))"
+    return self.cName + type.cFuncSuffix + "(" + self.params() + ")"
+
+MathFunctions = [
+  SwiftMathsFunction(name="cos", intrinsic=True),
+  SwiftMathsFunction(name="sin", intrinsic=True),
+  SwiftMathsFunction(name="exp", intrinsic=True),
+  SwiftMathsFunction(name="exp2", intrinsic=True),
+  SwiftMathsFunction(name="log", intrinsic=True),
+  SwiftMathsFunction(name="log10", intrinsic=True),
+  SwiftMathsFunction(name="log2", intrinsic=True),
+]
+
+
