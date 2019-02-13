3fac766a23a77ebd0640296bfd7fc116ea60a4e0
marcrasi <marcrasi@google.com>
2019-02-12 21:59:59 -0800

    Update 28653-child-source-range-not-contained-within-its-parent.swift

--- validation-test/compiler_crashers/28653-child-source-range-not-contained-within-its-parent.swift
+++ validation-test/compiler_crashers/28653-child-source-range-not-contained-within-its-parent.swift
@@ -5,5 +5,9 @@
 // See https://swift.org/LICENSE.txt for license information
 // See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
 
+// This test no longer crashes the compiler, but it does produce an AST that
+// the verifier doesn't like.
+// REQUIRES: swift_ast_verifier
+
 // RUN: not --crash %target-swift-frontend %s -emit-ir
 switch{case.b(u){
