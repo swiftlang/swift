f41eb87c00132753589e9681d4c5b89d5e615bee
Pavel Yaskevich <pyaskevich@apple.com>
2019-02-13 21:06:11 -0800

    [TypeChecker] Disable perf test-case for rdar://problem/21398466
    Looks like obsoleted initializer has been removed,
    and there are no solutions available for 'init(truncatingBitPattern:)'
    which leads to performance regression with designated types enabled.
    
    Resolves: rdar://problem/48061151
    (cherry picked from commit 43c547b0b89bf06eef2f98284638b6d18cbf9ae6)

--- validation-test/Sema/type_checker_perf/fast/rdar21398466.swift
+++ validation-test/Sema/type_checker_perf/fast/rdar21398466.swift
@@ -1,5 +1,7 @@
 // RUN: %target-typecheck-verify-swift -solver-expression-time-threshold=1 -swift-version 5 -solver-disable-shrink -disable-constraint-solver-performance-hacks -solver-enable-operator-designated-types
-// REQUIRES: tools-release,no_asserts
+// REQUIRES: OS=macosx
+// REQUIRES: asserts
+// REQUIRES: rdar48061151
 
 // This problem is related to renaming,
 // as soon as `init(truncatingBitPattern:)` is changed
