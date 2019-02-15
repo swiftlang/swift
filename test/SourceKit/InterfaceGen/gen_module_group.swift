2cc3a2704bdb88bc3bca3511c934cd923db65705
Xi Ge <xi_ge@apple.com>
2019-02-15 12:28:12 -0800

    [test] Add -target %target-triple to sourcekitd-test invocation

--- test/SourceKit/InterfaceGen/gen_module_group.swift
+++ test/SourceKit/InterfaceGen/gen_module_group.swift
@@ -1,7 +1,7 @@
 // RUN: %empty-directory(%t.mod)
 // RUN: %target-swift-frontend -module-name MyModule -emit-module -emit-module-path %t.mod/MyModule.swiftmodule  -emit-module-doc -emit-module-doc-path %t.mod/MyModule.swiftdoc -group-info-path %S/Inputs/group.json %S/Inputs/swift_mod.swift %S/Inputs/swift_mod_syn.swift
-// RUN: %sourcekitd-test -req=interface-gen -module MyModule -group-name A -- -I %t.mod | %FileCheck -check-prefix=GROUPA %s
-// RUN: %sourcekitd-test -req=interface-gen -module MyModule -group-name B -- -I %t.mod | %FileCheck -check-prefix=GROUPB %s
+// RUN: %sourcekitd-test -req=interface-gen -module MyModule -group-name A -- -I %t.mod -target %target-triple | %FileCheck -check-prefix=GROUPA %s
+// RUN: %sourcekitd-test -req=interface-gen -module MyModule -group-name B -- -I %t.mod -target %target-triple | %FileCheck -check-prefix=GROUPB %s
 
 // GROUPA: MyClass
 // GROUPA-NOT: P1
