a769917a1070160af2cc46cd2cdbac0de0d82fb7
Suyash Srijan <suyashsrijan@TABLT0177-SuyashSrijan.local>
2019-02-19 18:59:18 +0000

    [test] add more test cases

--- test/decl/accessors/throwable_accessors.swift
+++ test/decl/accessors/throwable_accessors.swift
@@ -2,6 +2,8 @@
 
 extension String: Error {}
 
+/// Throwing properties and subscripts ///
+
 struct Foo {
     var _arr: [Int] = [2, 3, 4]
 
@@ -64,7 +66,7 @@ func testThrowableSubscript() {
 
     let _ = try instance[0] // expected-error {{error is not handled because the enclosing function is not declared 'throws'}}
     let _ = try? instance[0] // Okay
-    let _ = try! instance[0] // Ok
+    let _ = try! instance[0] // Okay
 
     try instance[0] = 1 // expected-error {{error is not handled because the enclosing function is not declared 'throws'}}
     try? instance[0] = 1 // Okay
@@ -76,7 +78,7 @@ func testThrowableProperty() {
 
     let _ = try instance.arr // expected-error {{error is not handled because the enclosing function is not declared 'throws'}}
     let _ = try? instance.arr  // Okay
-    let _ = try! instance.arr  // Ok
+    let _ = try! instance.arr  // Okay
 
     try instance.arr = [1, 2, 3] // expected-error {{error is not handled because the enclosing function is not declared 'throws'}}
     try? instance.arr = [1, 2, 3] // Okay
@@ -88,7 +90,7 @@ func testMutatingProperty() {
 
     let _ = try anotherInstance.arr2 // expected-error {{error is not handled because the enclosing function is not declared 'throws'}}
     let _ = try? anotherInstance.arr2  // Okay
-    let _ = try! anotherInstance.arr2  // Ok
+    let _ = try! anotherInstance.arr2  // Okay
 
     try anotherInstance.arr2 = ["Foo"] // expected-error {{error is not handled because the enclosing function is not declared 'throws'}}
     try? anotherInstance.arr2 = ["Foo"] // Okay
@@ -108,3 +110,98 @@ testThrowingAccessor() // expected-error {{call can throw but is not marked with
 try testThrowingAccessor() // Okay
 try? testThrowingAccessor() // Okay
 try! testThrowingAccessor() // Okay
+
+func testNestedFunctionThatContainsThrowingAccessor() {
+    func nested() {
+        var instance = Foo()
+        let _ = try instance.arr // expected-error {{error is not handled because the enclosing function is not declared 'throws'}}
+        let _ = try? instance.arr // Okay
+        let _ = try! instance.arr // Okay
+    }
+}
+
+func testNestedFunctionThatThrowsAndContainsThrowingAccessor() {
+    func nested() throws {
+        var instance = Foo()
+        let _ = try instance.arr
+    }
+
+    nested() // expected-error {{call can throw, but it is not marked with 'try' and the error is not handled}}
+    try nested() // expected-error {{errors thrown from here are not handled}}
+    try? nested() // Okay
+    try! nested() // Okay
+}
+
+func testFunctionThatTakesInOut(_ f: inout Foo) {
+    let _ = try f.arr // expected-error {{error is not handled because the enclosing function is not declared 'throws'}}
+    let _ = try? f.arr // Okay
+    let _ = try! f.arr // Okay
+}
+
+func testThrowingFunctionThatTakesInOut(_ f: inout Foo) throws {
+    let _ = try f.arr // Okay
+    let _ = try? f.arr // Okay
+    let _ = try! f.arr // Okay
+}
+
+var instance = Foo()
+testThrowingFunctionThatTakesInOut(&instance) // expected-error {{call can throw but is not marked with 'try'}} // expected-note {{did you mean to use 'try'?}} // expected-note {{did you mean to handle error as optional value?}} // expected-note {{did you mean to disable error propagation?}}
+try testThrowingFunctionThatTakesInOut(&instance) // Okay
+try? testThrowingFunctionThatTakesInOut(&instance) // Okay
+try! testThrowingFunctionThatTakesInOut(&instance) // Okay
+
+/// Throwing properties and subscripts in protocols ///
+
+protocol P {
+    var a: Int { get throws set throws } // Okay
+    var b: Int { get throws set } // Okay
+    var c: Int { get set throws } // Okay
+    var d: Int { get set } // Okay
+
+    subscript(e: Int) -> Int { get throws set throws } // Okay
+    subscript(f: String) -> String { get throws set } // Okay
+    subscript(g: Float) -> Float { get set throws } // Okay
+    subscript(h: Double) -> Double { get set } // Okay
+}
+
+class S: P {
+    var a: Int {
+        get throws {} // Okay
+        set throws {} // Okay
+    }
+
+    var b: Int {
+        get throws {} // Okay
+        set {} // Okay
+    }
+
+    var c: Int {
+        get {} // Okay
+        set throws {} // Okay
+    }
+    
+    var d: Int {
+        get throws {} // FIXME: A non-throwing requirement can be fulfilled by a throwing property in the conforming type
+        set {} // Okay
+    }
+
+    subscript(e: Int) -> Int {
+        get throws {} // Okay
+        set throws {} // Okay
+    }
+
+    subscript(f: String) -> String {
+        get throws {} // Okay
+        set {} // Okay
+    }
+
+    subscript(g: Float) -> Float {
+        get {} // Okay
+        set throws {} // Okay
+    }
+
+    subscript(h: Double) -> Double {
+        get {} // Okay
+        set throws {} // FIXME: A non-throwing requirement can be fulfilled by a throwing subscript in the conforming type
+    }
+}
