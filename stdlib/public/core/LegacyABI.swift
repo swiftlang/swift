397de3e57a0539589a09e8606f4e6fcf530ba62d
Michael Ilseman <milseman@apple.com>
2019-03-01 10:46:52 -0800

    [stdlib] Add LegacyABI.swift for legacy non-API declarations

---
+++ stdlib/public/core/LegacyABI.swift
@@ -0,0 +1,15 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2019 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+// This file contains non-API (or underscored) declarations that are needed to
+// be kept around for ABI compatibility
+
