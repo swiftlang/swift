cad116a12b94fe98bce5d768043fa800fe14aa1b
Steve (Numerics) Canon <scanon@apple.com>
2019-03-02 20:13:00 -0500

    Revert "build: improve the builds with Visual Studio"
    This reverts commit d9dde28bf183535f03813bc3a5852d8354c918fa.

--- cmake/modules/ClangClCompileRules.cmake
+++ cmake/modules/ClangClCompileRules.cmake
@@ -1,10 +1,8 @@
 
 # clang-cl interprets paths starting with /U as macro undefines, so we need to
 # put a -- before the input file path to force it to be treated as a path.
-if(NOT MSVC AND "${CMAKE_SIMULATE_ID}" STREQUAL "MSVC")
-  string(REPLACE "-c <SOURCE>" "-c -- <SOURCE>" CMAKE_C_COMPILE_OBJECT "${CMAKE_C_COMPILE_OBJECT}")
-  string(REPLACE "-c <SOURCE>" "-c -- <SOURCE>" CMAKE_CXX_COMPILE_OBJECT "${CMAKE_CXX_COMPILE_OBJECT}")
-endif()
+string(REPLACE "-c <SOURCE>" "-c -- <SOURCE>" CMAKE_C_COMPILE_OBJECT "${CMAKE_C_COMPILE_OBJECT}")
+string(REPLACE "-c <SOURCE>" "-c -- <SOURCE>" CMAKE_CXX_COMPILE_OBJECT "${CMAKE_CXX_COMPILE_OBJECT}")
 
 # NOTE(compnerd) incremental linking is known to cause corruption in the
 # protocol conformance tables.  Avoid using incremental links with Visual
