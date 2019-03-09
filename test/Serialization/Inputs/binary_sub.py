5b639d20511903d00ec2c22839330bdb0501e1e0
Daniel Rodríguez Troitiño <danielrodriguez@fb.com>
2019-03-08 17:47:57 -0800

    [test] Make binary_sub.py compatible with Python 3

--- test/Serialization/Inputs/binary_sub.py
+++ test/Serialization/Inputs/binary_sub.py
@@ -5,5 +5,14 @@ import sys
 (_, old, new) = sys.argv
 assert(len(old) == len(new))
 
-data = sys.stdin.read()
-sys.stdout.write(data.replace(old, new))
+if sys.version_info[0] == 2:
+    stdin = sys.stdin
+    stdout = sys.stdout
+else:
+    stdin = sys.stdin.buffer
+    stdout = sys.stdout.buffer
+    old = old.encode()
+    new = new.encode()
+
+data = stdin.read()
+stdout.write(data.replace(old, new))
