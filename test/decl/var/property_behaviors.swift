0661d1b8e4bd5e79e0562d4626659314c9da5f11
Doug Gregor <dgregor@apple.com>
2019-03-09 23:24:02 -0800

    [Type checker] Apply property behavior to the storage type of a variable.
    When a variable has a property behavior, wrap the type of the variable in
    the property behavior type. For example, a property behavior “Lazy”
    should be written as Lazy<T>. A variable declared as
    
      var someLazyInt: Int by Lazy
    
    will have a storage type of Lazy<Int>.

--- test/decl/var/property_behaviors.swift
+++ test/decl/var/property_behaviors.swift
@@ -2,10 +2,6 @@
 
 struct Wrapper<T> {
   var value: T
-
-  init(initialValue: T) {
-    self.value = initialValue
-  }
 }
 
 // ---------------------------------------------------------------------------
@@ -14,8 +10,8 @@ struct Wrapper<T> {
 
 func testParsing() {
   let wrapped1: Int by Wrapper
-  let wrapped2: Int by Wrapper = 5
-  let wrapped3 by Wrapper = 5
+  let wrapped2: Int by Wrapper = Wrapper(value: 5)
+  let wrapped3 by Wrapper = Wrapper(value: 5)
 
   _ = wrapped1
   _ = wrapped2
@@ -30,4 +26,34 @@ func testParseError() {
   _ = b
   _ = c
   _ = d
+}
+
+// ---------------------------------------------------------------------------
+// Type formation
+// ---------------------------------------------------------------------------
+func testExplicitWrapperType() {
+  var wrapped1: Int by Wrapper
+
+  wrapped1 = "Hello" // expected-error{{cannot assign value of type 'String' to type 'Wrapper<Int>'}}
+}
+
+func testImplicitWrapperType() {
+  var wrapped1 by Wrapper = .init(value: 5)
+
+  wrapped1 = "Hello" // expected-error{{cannot assign value of type 'String' to type 'Wrapper<Int>'}}
+}
+
+struct NonGenericWrapper { }
+struct TwoParameterWrapper<T, U> { } // expected-note{{generic struct 'TwoParameterWrapper' declared here}}
+
+func testBadWrapperTypes() {
+  let wrapped1: Int by NonGenericWrapper // expected-error{{property behavior must name a generic type without any generic arguments}}
+  let wrapped2: Int by TwoParameterWrapper // expected-error{{property behavior must refer to a single-parameter generic type}}
+  let wrapped3: Int by TwoParameterWrapper<Int, Int> // expected-error{{property behavior must name a generic type without any generic arguments}}
+  let wrapped4: Int by (Int) // expected-error{{property behavior must name a generic type without any generic arguments}}
+
+  _ = wrapped1
+  _ = wrapped2
+  _ = wrapped3
+  _ = wrapped4
 }
