dd5a874d73de24ed0ecbab78e333dd2618e241b4
Nate Chandler <nate_chandler@apple.com>
2019-03-11 13:21:58 -0700

    Corrected argument capitalization.

--- include/swift/Parse/Parser.h
+++ include/swift/Parse/Parser.h
@@ -1378,7 +1378,7 @@ public:
 
   Expr *parseExprAnonClosureArg();
   ParserResult<Expr> parseExprList(tok LeftTok, tok RightTok, 
-                                   bool allowSepOmission,
+                                   bool AllowSepOmission,
                                    syntax::SyntaxKind Kind);
 
   /// Parse an expression list, keeping all of the pieces separated.
