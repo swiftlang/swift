d80065d5c6cf1a706824f41bff641ccea1c9cb45
simon gladman <sgladman@gmail.com>
2019-03-12 11:06:09 +0000

    Swift overlays to Accelerate.vDSP clipping, limit, and threshold operations.

--- test/stdlib/Accelerate.swift
+++ test/stdlib/Accelerate.swift
@@ -69,4 +69,288 @@ if #available(iOS 10.0, OSX 10.12, tvOS 10.0, watchOS 4.0, *) {
   
 }
 
+
+//===----------------------------------------------------------------------===//
+//
+//  vDSP clipping, limit, and threshold tests; single-precision.
+//
+//===----------------------------------------------------------------------===//
+
+if #available(iOS 9999, macOS 9999, tvOS 9999, watchOS 9999, *) {
+    
+    let count = 256
+    let n = vDSP_Length(256)
+    let bounds = Float(-0.5) ... Float(0.5)
+    let outputConstant: Float = 99
+    
+    let source: [Float] = (0 ..< 256).map { i in
+        return sin(Float(i) * 0.05) + sin(Float(i) * 0.025)
+    }
+    
+    AccelerateTests.test("vDSP/SinglePrecisionClipping") {
+        var result = [Float](repeating: 0,
+                             count: count)
+        
+        vDSP.clip(source,
+                  to: bounds,
+                  result: &result)
+        
+        var legacyResult = [Float](repeating: -1,
+                                   count: count)
+        
+        vDSP_vclip(source, 1,
+                   [bounds.lowerBound],
+                   [bounds.upperBound],
+                   &legacyResult, 1,
+                   n)
+        
+        expectTrue(result.elementsEqual(legacyResult))
+    }
+    
+    AccelerateTests.test("vDSP/SinglePrecisionInvertedClipping") {
+        var result = [Float](repeating: 0,
+                             count: count)
+        
+        vDSP.invertedClip(source,
+                          to: bounds,
+                          result: &result)
+        
+        var legacyResult = [Float](repeating: -1,
+                                   count: count)
+        
+        vDSP_viclip(source, 1,
+                    [bounds.lowerBound],
+                    [bounds.upperBound],
+                    &legacyResult, 1,
+                    n)
+        
+        expectTrue(result.elementsEqual(legacyResult))
+    }
+    
+    AccelerateTests.test("vDSP/SinglePrecisionThreshold") {
+        var result = [Float](repeating: 0,
+                             count: count)
+        
+        vDSP.threshold(source,
+                       to: bounds.lowerBound,
+                       with: .clampToThreshold,
+                       result: &result)
+        
+        var legacyResult = [Float](repeating: -1,
+                                   count: count)
+        
+        vDSP_vthr(source, 1,
+                  [bounds.lowerBound],
+                  &legacyResult, 1,
+                  n)
+        
+        expectTrue(result.elementsEqual(legacyResult))
+    }
+    
+    AccelerateTests.test("vDSP/SinglePrecisionThresholdWithConstant") {
+        var result = [Float](repeating: 0,
+                             count: count)
+        
+        vDSP.threshold(source,
+                       to: bounds.lowerBound,
+                       with: .signedConstant(outputConstant),
+                       result: &result)
+        
+        var legacyResult = [Float](repeating: -1,
+                                   count: count)
+        
+        vDSP_vthrsc(source, 1,
+                    [bounds.lowerBound],
+                    [outputConstant],
+                    &legacyResult, 1,
+                    n)
+        
+        expectTrue(result.elementsEqual(legacyResult))
+    }
+    
+    AccelerateTests.test("vDSP/SinglePrecisionThresholdWithZeroFill") {
+        var result = [Float](repeating: 0,
+                             count: count)
+        
+        vDSP.threshold(source,
+                       to: bounds.lowerBound,
+                       with: .zeroFill,
+                       result: &result)
+        
+        var legacyResult = [Float](repeating: -1,
+                                   count: count)
+        
+        vDSP_vthres(source, 1,
+                    [bounds.lowerBound],
+                    &legacyResult, 1,
+                    n)
+        
+        expectTrue(result.elementsEqual(legacyResult))
+    }
+    
+    AccelerateTests.test("vDSP/SinglePrecisionLimit") {
+        var result = [Float](repeating: 0,
+                             count: count)
+        
+        vDSP.limit(source,
+                   limit: bounds.upperBound,
+                   withOutputConstant: outputConstant,
+                   result: &result)
+        
+        var legacyResult = [Float](repeating: -1,
+                                   count: count)
+        
+        vDSP_vlim(source, 1,
+                  [bounds.upperBound],
+                  [outputConstant],
+                  &legacyResult, 1,
+                  n)
+        
+        expectTrue(result.elementsEqual(legacyResult))
+    }
+}
+
+//===----------------------------------------------------------------------===//
+//
+//  vDSP clipping, limit, and threshold tests; double-precision.
+//
+//===----------------------------------------------------------------------===//
+
+if #available(iOS 9999, macOS 9999, tvOS 9999, watchOS 9999, *) {
+    
+    let count = 256
+    let n = vDSP_Length(256)
+    let bounds = Double(-0.5) ... Double(0.5)
+    let outputConstant: Double = 99
+    
+    let source: [Double] = (0 ..< 256).map { i in
+        return sin(Double(i) * 0.05) + sin(Double(i) * 0.025)
+    }
+    
+    AccelerateTests.test("vDSP/DoublePrecisionClipping") {
+        var result = [Double](repeating: 0,
+                              count: count)
+        
+        vDSP.clip(source,
+                  to: bounds,
+                  result: &result)
+        
+        var legacyResult = [Double](repeating: -1,
+                                    count: count)
+        
+        vDSP_vclipD(source, 1,
+                    [bounds.lowerBound],
+                    [bounds.upperBound],
+                    &legacyResult, 1,
+                    n)
+        
+        expectTrue(result.elementsEqual(legacyResult))
+    }
+    
+    AccelerateTests.test("vDSP/DoublePrecisionInvertedClipping") {
+        var result = [Double](repeating: 0,
+                              count: count)
+        
+        vDSP.invertedClip(source,
+                          to: bounds,
+                          result: &result)
+        
+        var legacyResult = [Double](repeating: -1,
+                                    count: count)
+        
+        vDSP_viclipD(source, 1,
+                     [bounds.lowerBound],
+                     [bounds.upperBound],
+                     &legacyResult, 1,
+                     n)
+        
+        expectTrue(result.elementsEqual(legacyResult))
+    }
+    
+    AccelerateTests.test("vDSP/DoublePrecisionThreshold") {
+        var result = [Double](repeating: 0,
+                              count: count)
+        
+        vDSP.threshold(source,
+                       to: bounds.lowerBound,
+                       with: .clampToThreshold,
+                       result: &result)
+        
+        var legacyResult = [Double](repeating: -1,
+                                    count: count)
+        
+        vDSP_vthrD(source, 1,
+                   [bounds.lowerBound],
+                   &legacyResult, 1,
+                   n)
+        
+        expectTrue(result.elementsEqual(legacyResult))
+    }
+    
+    AccelerateTests.test("vDSP/DoublePrecisionThresholdWithConstant") {
+        var result = [Double](repeating: 0,
+                              count: count)
+        
+        vDSP.threshold(source,
+                       to: bounds.lowerBound,
+                       with: .signedConstant(outputConstant),
+                       result: &result)
+        
+        var legacyResult = [Double](repeating: -1,
+                                    count: count)
+        
+        vDSP_vthrscD(source, 1,
+                     [bounds.lowerBound],
+                     [outputConstant],
+                     &legacyResult, 1,
+                     n)
+        
+        expectTrue(result.elementsEqual(legacyResult))
+    }
+    
+    AccelerateTests.test("vDSP/DoublePrecisionThresholdWithZeroFill") {
+        var result = [Double](repeating: 0,
+                              count: count)
+        
+        vDSP.threshold(source,
+                       to: bounds.lowerBound,
+                       with: .zeroFill,
+                       result: &result)
+        
+        var legacyResult = [Double](repeating: -1,
+                                    count: count)
+        
+        vDSP_vthresD(source, 1,
+                     [bounds.lowerBound],
+                     &legacyResult, 1,
+                     n)
+        
+        expectTrue(result.elementsEqual(legacyResult))
+    }
+    
+    AccelerateTests.test("vDSP/DoublePrecisionLimit") {
+        var result = [Double](repeating: 0,
+                              count: count)
+        
+        vDSP.limit(source,
+                   limit: bounds.upperBound,
+                   withOutputConstant: outputConstant,
+                   result: &result)
+        
+        var legacyResult = [Double](repeating: -1,
+                                    count: count)
+        
+        vDSP_vlimD(source, 1,
+                   [bounds.upperBound],
+                   [outputConstant],
+                   &legacyResult, 1,
+                   n)
+        
+        expectTrue(result.elementsEqual(legacyResult))
+    }
+}
+
+runAllTests()
+
+
 runAllTests()
