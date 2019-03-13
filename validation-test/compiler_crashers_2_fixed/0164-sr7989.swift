ec32f041924e40d87edba6f27ba6b465376fa1d6
John Holdsworth <github@johnholdsworth.com>
2019-03-13 19:50:18 +0000

    Fix validation-test/compiler_crashers_2_fixed/0164-sr7989.swift

--- validation-test/compiler_crashers_2_fixed/0164-sr7989.swift
+++ validation-test/compiler_crashers_2_fixed/0164-sr7989.swift
@@ -8,4 +8,4 @@ struct Var<N> {}
 extension Var : P2 where N : P1 { }
 
 protocol P3 {}
-extension Var : P3 where Self : P2 {} // expected-error {{'Self' is only available in a protocol or as the result of a method in a class; did you mean 'Var'?}}
+extension Var : P3 where Self : P2 {} // expected-error {{type 'Var<N>' in conformance requirement does not refer to a generic parameter or associated type}}
