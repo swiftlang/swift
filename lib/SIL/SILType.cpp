d069cc414774464e5c26932f1ef35d0d6604a6a5
Slava Pestov <spestov@apple.com>
2019-03-13 02:10:26 -0400

    SIL: Remove SILType::isAddressOnly()/isLoadable() overloads that take a SILModule
    Finally we can remove the old overloads.

--- lib/SIL/SILType.cpp
+++ lib/SIL/SILType.cpp
@@ -175,28 +175,13 @@ SILType SILType::getEnumElementType(EnumElementDecl *elt, SILModule &M) const {
   return SILType(loweredTy, getCategory());
 }
 
-bool SILType::isLoadableOrOpaque(SILModule &M) const {
-  return isLoadable(M) || !SILModuleConventions(M).useLoweredAddresses();
-}
-
 bool SILType::isLoadableOrOpaque(const SILFunction &F) const {
   SILModule &M = F.getModule();
-  return isLoadable(F) ||
-         !SILModuleConventions(M).useLoweredAddresses();
-}
-
-/// True if the type, or the referenced type of an address type, is
-/// address-only. For example, it could be a resilient struct or something of
-/// unknown size.
-bool SILType::isAddressOnly(SILModule &M) const {
-  return M.Types.getTypeLowering(*this, ResilienceExpansion::Minimal)
-    .isAddressOnly();
+  return isLoadable(F) || !SILModuleConventions(M).useLoweredAddresses();
 }
 
 bool SILType::isAddressOnly(const SILFunction &F) const {
-  // FIXME: Should just call F.getTypeLowering()
-  return F.getModule().Types.getTypeLowering(*this,
-                                   F.getResilienceExpansion()).isAddressOnly();
+  return F.getTypeLowering(*this).isAddressOnly();
 }
 
 SILType SILType::substGenericArgs(SILModule &M,
