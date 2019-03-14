018ba70698ef1485c4e8a47f94b4e1a91314481b
Nate Chandler <nate_chandler@apple.com>
2019-03-14 12:05:01 -0700

    Just revert return insertion if inappropriate.
    Because the BodyStmts are revered to their original contents, they are
    no longer implicit.

--- lib/Sema/TypeCheckStmt.cpp
+++ lib/Sema/TypeCheckStmt.cpp
@@ -1923,16 +1923,11 @@ bool TypeChecker::typeCheckFunctionBodyUntil(FuncDecl *FD,
     //           }
 
     if (fnTypeLoc.isNull() || fnType->isVoid()) {
-      auto voidExpr = TupleExpr::createEmpty(Context,
-                                             E->getStartLoc(),
-                                             E->getEndLoc(),
-                                             /*implicit*/true);
-      returnStmt->setResult(voidExpr);
       BS = BraceStmt::create(Context,
                              BS->getStartLoc(),
-                             { E, returnStmt },
+                             { E },
                              BS->getEndLoc(),
-                             /*implicit=*/true);
+                             /*implicit=*/false);
     } else {
       // FIXME: FIXME_NOW: DETERMINE: Is this the appropriate mechanism to use
       //        to divine the type of E?
@@ -1951,7 +1946,7 @@ bool TypeChecker::typeCheckFunctionBodyUntil(FuncDecl *FD,
                                  BS->getStartLoc(),
                                  { E },
                                  BS->getEndLoc(),
-                                 /*implicit=*/true);
+                                 /*implicit=*/false);
         }
       }
     }
