6850a0ef1de6ce6c08377008285b5febdb61fff2
Dan Zheng <danielzheng@google.com>
2019-03-17 03:41:45 -0700

    [AutoDiff] Fix `@differentiating` type-checking for differentials. (#23353)
    * [AutoDiff] Fix `@differentiating` type-checking for differentials.
    
    Fix type-checking for differential functions.
    Simplify pullback/differential type-checking diagnostics.
    
    Resolves TF-366.
    
    * Revamp differential/pullback type mismatch diagnostics.
    
    - Revamp differential/pullback type mismatch diagnostics.
      - Emit type mismatch error on `@differentiating` attribute.
      - Emit note with expected differential/pullback type on actual type location.
      - Emit note showing original function location, if possible.
    - Perform original function lookup before checking `wrt` parameters.
    - Change error message for `differentiating_attr_overload_not_found`.

--- lib/Sema/TypeCheckAttr.cpp
+++ lib/Sema/TypeCheckAttr.cpp
@@ -2893,10 +2893,10 @@ makeFunctionType(AnyFunctionType *copy, ArrayRef<AnyFunctionType::Param> params,
 }
 
 // SWIFT_ENABLE_TENSORFLOW
-// Return the original function type corresponding to the given derivative
+// Compute the original function type corresponding to the given derivative
 // function type.
 static AnyFunctionType *
-getAutoDiffOriginalFunctionType(AnyFunctionType *derivativeType) {
+computeAutoDiffOriginalFunctionType(AnyFunctionType *derivativeType) {
   // Unwrap curry levels.
   SmallVector<AnyFunctionType *, 2> curryLevels;
   auto *currentLevel = derivativeType;
@@ -2937,6 +2937,14 @@ void AttributeChecker::visitDifferentiatingAttr(DifferentiatingAttr *attr) {
       LookUpConformanceInModule(D->getDeclContext()->getParentModule());
   auto original = attr->getOriginal();
 
+  auto *derivativeInterfaceType = derivative->getInterfaceType()
+      ->eraseDynamicSelfType()->castTo<AnyFunctionType>();
+  auto *derivativeType = isMethod
+      ? derivative->getMethodInterfaceType()->castTo<AnyFunctionType>()
+      : derivativeInterfaceType;
+
+  // Perform preliminary checks.
+
   // If the original function has no parameters or returns the empty tuple
   // type, there's nothing to differentiate from or with-respect-to.
   auto &derivativeParams = *derivative->getParameters();
@@ -2968,7 +2976,7 @@ void AttributeChecker::visitDifferentiatingAttr(DifferentiatingAttr *attr) {
   Identifier autoDiffAssocTyId;
   if (valueResultElt.getName().str() != "value") {
     TC.diagnose(attr->getLocation(),
-                diag::differentiating_attr_expected_result_tuple_value_label);
+                diag::differentiating_attr_invalid_result_tuple_value_label);
     attr->setInvalid();
     return;
   }
@@ -2980,7 +2988,7 @@ void AttributeChecker::visitDifferentiatingAttr(DifferentiatingAttr *attr) {
     autoDiffAssocTyId = ctx.Id_CotangentVector;
   } else {
     TC.diagnose(attr->getLocation(),
-                diag::differentiating_attr_expected_result_tuple_func_label);
+                diag::differentiating_attr_invalid_result_tuple_func_label);
     attr->setInvalid();
     return;
   }
@@ -2999,80 +3007,10 @@ void AttributeChecker::visitDifferentiatingAttr(DifferentiatingAttr *attr) {
     attr->setInvalid();
     return;
   }
-  // Pullback must take one parameter with type `R.CotangentVector`.
-  // FIXME(TF-363): Fix type-checking for differentials. The current logic is
-  // completely wrong.
-  auto seedTy = ProtocolConformanceRef::getTypeWitnessByName(
-      valueResultType, *valueResultConf, autoDiffAssocTyId,
-      ctx.getLazyResolver());
-  auto funcEltType = funcResultElt.getType()->getAs<AnyFunctionType>();
-  if (funcEltType->hasTypeParameter())
-    funcEltType = derivative->mapTypeIntoContext(
-        funcResultElt.getType())->getAs<AnyFunctionType>();
-  if (!funcEltType || funcEltType->getNumParams() != 1 ||
-      !funcEltType->getParams().front().getPlainType()->isEqual(seedTy)) {
-    TC.diagnose(attr->getLocation(),
-                diag::differentiating_attr_result_func_invalid_parameter,
-                funcResultElt.getName(), seedTy);
-    attr->setInvalid();
-    return;
-  }
 
-  // Gather inferred differentiation parameters.
-  SmallVector<TupleTypeElt, 4> diffParams;
-  auto addDiffParam = [&](Type paramType) {
-    auto conf = TC.conformsToProtocol(paramType, diffableProto, derivative,
-                                      ConformanceCheckFlags::Used);
-    if (!conf)
-      return;
-    auto diffParamType = ProtocolConformanceRef::getTypeWitnessByName(
-        paramType, *conf, autoDiffAssocTyId, ctx.getLazyResolver());
-    diffParams.push_back(TupleTypeElt(diffParamType));
-  };
-
-  auto *derivativeInterfaceType =
-      derivative->getInterfaceType()->castTo<AnyFunctionType>();
-  auto *derivativeType = isMethod
-      ? derivative->getMethodInterfaceType()->castTo<AnyFunctionType>()
-      : derivativeInterfaceType;
-  // If `derivative` is an instance method, check whether `Self` conforms to
-  // `Differentiable`.
-  if (isInstanceMethod) {
-    auto selfType = derivative->getImplicitSelfDecl()->getInterfaceType();
-    if (selfType->hasTypeParameter())
-      selfType = derivative->getParent()->mapTypeIntoContext(selfType);
-    addDiffParam(selfType);
-  }
-  // Check whether every parameter conforms to `Differentiable`.
-  for (auto param : derivativeType->getParams()) {
-    auto paramType = param.getPlainType();
-    if (param.isNonDifferentiable())
-      continue;
-    if (paramType->hasTypeParameter())
-      paramType = derivative->mapTypeIntoContext(paramType);
-    addDiffParam(paramType);
-  }
-  // There must be at least one differentiation parameter.
-  if (diffParams.empty()) {
-    TC.diagnose(attr->getLocation(),
-                diag::differentiating_attr_no_diff_parameters);
-    attr->setInvalid();
-    return;
-  }
-
-  // Check returned parameter derivatives type against expected type.
-  auto expectedDiffParamsType = TupleType::get(diffParams, ctx);
-  auto diffParamsType = funcEltType->getResult();
-  if (!diffParamsType || !diffParamsType->isEqual(expectedDiffParamsType)) {
-    TC.diagnose(attr->getLocation(),
-                diag::differentiating_attr_unexpected_diff_params_type,
-                diffParamsType, expectedDiffParamsType);
-    attr->setInvalid();
-    return;
-  }
-
+  // Compute expected original function type and look up original function.
   auto *originalFnType =
-      getAutoDiffOriginalFunctionType(derivativeInterfaceType);
+      computeAutoDiffOriginalFunctionType(derivativeInterfaceType);
 
   std::function<bool(GenericSignature *, GenericSignature *)>
     checkGenericSignatureSatisfied =
@@ -3102,6 +3040,8 @@ void AttributeChecker::visitDifferentiatingAttr(DifferentiatingAttr *attr) {
         checkGenericSignatureSatisfied);
   };
 
+  // TODO: Do not reuse incompatible `@differentiable` attribute diagnostics.
+  // Rename compatible diagnostics so that they're not attribute-specific.
   auto overloadDiagnostic = [&]() {
     TC.diagnose(original.Loc, diag::differentiating_attr_overload_not_found,
                 original.Name, originalFnType);
@@ -3155,6 +3095,87 @@ void AttributeChecker::visitDifferentiatingAttr(DifferentiatingAttr *attr) {
     return;
   }
   attr->setOriginalFunction(originalFn);
+
+  // Gather inferred differentiation parameters.
+  SmallVector<TupleTypeElt, 4> diffParamElts;
+  auto addDiffParam = [&](Type paramType) {
+    auto conf = TC.conformsToProtocol(paramType, diffableProto, derivative,
+                                      ConformanceCheckFlags::Used);
+    if (!conf)
+      return;
+    auto diffParamType = ProtocolConformanceRef::getTypeWitnessByName(
+        paramType, *conf, autoDiffAssocTyId, ctx.getLazyResolver());
+    diffParamElts.push_back(TupleTypeElt(diffParamType));
+  };
+
+  // If `derivative` is an instance method, check whether `Self` conforms to
+  // `Differentiable`.
+  if (isInstanceMethod) {
+    auto selfType = derivative->getImplicitSelfDecl()->getInterfaceType();
+    if (selfType->hasTypeParameter())
+      selfType = derivative->getParent()->mapTypeIntoContext(selfType);
+    addDiffParam(selfType);
+  }
+  // Check whether every parameter conforms to `Differentiable`.
+  for (auto param : derivativeType->getParams()) {
+    auto paramType = param.getPlainType();
+    if (param.isNonDifferentiable())
+      continue;
+    if (paramType->hasTypeParameter())
+      paramType = derivative->mapTypeIntoContext(paramType);
+    addDiffParam(paramType);
+  }
+  // There must be at least one differentiation parameter.
+  if (diffParamElts.empty()) {
+    TC.diagnose(attr->getLocation(),
+                diag::differentiating_attr_no_diff_parameters);
+    attr->setInvalid();
+    return;
+  }
+
+  // Check differentiation parameters.
+  // Get vector type: the associated type of the value result type.
+  auto vectorTy = ProtocolConformanceRef::getTypeWitnessByName(
+      valueResultType, *valueResultConf, autoDiffAssocTyId,
+      ctx.getLazyResolver());
+
+  // Compute expected differential/pullback type.
+  auto funcEltType = funcResultElt.getType();
+  Type expectedFuncEltType;
+  if (kind == AutoDiffAssociatedFunctionKind::JVP) {
+    auto diffParams = map<SmallVector<AnyFunctionType::Param, 4>>(
+        diffParamElts, [&](TupleTypeElt elt) {
+          return AnyFunctionType::Param(elt.getType());
+        });
+    expectedFuncEltType = FunctionType::get(diffParams, vectorTy);
+  } else {
+    expectedFuncEltType = FunctionType::get({AnyFunctionType::Param(vectorTy)},
+                                            TupleType::get(diffParamElts, ctx));
+  }
+  expectedFuncEltType = expectedFuncEltType->mapTypeOutOfContext();
+  // Check if differential/pullback type matches expected type.
+  if (!funcEltType->isEqual(expectedFuncEltType)) {
+    // Emit differential/pullback type mismatch error on attribute.
+    TC.diagnose(attr->getLocation(),
+                diag::differentiating_attr_result_func_type_mismatch,
+                funcResultElt.getName(), originalFn->getFullName());
+    // Emit note with expected differential/pullback type on actual type
+    // location.
+    auto *tupleReturnTypeRepr =
+        cast<TupleTypeRepr>(derivative->getReturnTypeLoc().getTypeRepr());
+    auto *funcEltTypeRepr = tupleReturnTypeRepr->getElementType(1);
+    TC.diagnose(funcEltTypeRepr->getStartLoc(),
+                diag::differentiating_attr_result_func_type_mismatch_note,
+                funcResultElt.getName(), expectedFuncEltType)
+        .highlight(funcEltTypeRepr->getSourceRange());
+    // Emit note showing original function location, if possible.
+    if (originalFn->getLoc().isValid())
+      TC.diagnose(originalFn->getLoc(),
+                  diag::differentiating_attr_result_func_original_note,
+                  originalFn->getFullName());
+    attr->setInvalid();
+    return;
+  }
 
   // Reject different-file retroactive derivatives.
   // TODO(TF-136): Full support for cross-file/cross-module retroactive
--- test/AutoDiff/differentiating_attr_type_checking.swift
+++ test/AutoDiff/differentiating_attr_type_checking.swift
@@ -2,6 +2,7 @@
 
 // Test top-level functions.
 
+// expected-note @+1 {{'sin' defined here}}
 func sin(_ x: Float) -> Float {
   return x // dummy implementation
 }
@@ -14,19 +15,24 @@ func vjpSin(x: Float) -> (value: Float, pullback: (Float) -> Float) {
 func jvpSin(x: @nondiff Float) -> (value: Float, differential: (Float) -> (Float)) {
   return (x, { $0 })
 }
-@differentiating(sin) // expected-error {{'@differentiating' attribute requires function to return a two-element tuple of type '(value: T..., pullback: (U.CotangentVector) -> T.CotangentVector...)' or '(value: T..., differential: (T.TangentVector...) -> U.TangentVector)'}}
+// expected-error @+1 {{'@differentiating' attribute requires function to return a two-element tuple of type '(value: T..., pullback: (U.CotangentVector) -> T.CotangentVector...)' or '(value: T..., differential: (T.TangentVector...) -> U.TangentVector)'}}
+@differentiating(sin)
 func jvpSinResultInvalid(x: @nondiff Float) -> Float {
   return x
 }
-@differentiating(sin) // expected-error {{'@differentiating' attribute requires function to return a two-element tuple (second element must have label 'pullback:' or 'differential:')}}
+// expected-error @+1 {{'@differentiating' attribute requires function to return a two-element tuple (second element must have label 'pullback:' or 'differential:')}}
+@differentiating(sin)
 func vjpSinResultWrongLabel(x: Float) -> (value: Float, (Float) -> Float) {
   return (x, { $0 })
 }
-@differentiating(sin) // expected-error {{'@differentiating' attribute requires function to return a two-element tuple (first element type 'Int' must conform to 'Differentiable')}}
+// expected-error @+1 {{'@differentiating' attribute requires function to return a two-element tuple (first element type 'Int' must conform to 'Differentiable')}}
+@differentiating(sin)
 func vjpSinResultNotDifferentiable(x: Int) -> (value: Int, pullback: (Int) -> Int) {
   return (x, { $0 })
 }
-@differentiating(sin) // expected-error {{expected 'pullback' to be a function with a single parameter of type 'Float.CotangentVector' (aka 'Float')}}
+// expected-error @+2 {{function result's 'pullback' type does not match 'sin'}}
+// expected-note @+2 {{'pullback' does not have expected type '(Float.CotangentVector) -> (Float.CotangentVector)' (aka '(Float) -> Float')}}
+@differentiating(sin)
 func vjpSinResultInvalidSeedType(x: Float) -> (value: Float, pullback: (Double) -> Double) {
   return (x, { $0 })
 }
@@ -39,14 +45,16 @@ func vjpGeneric<T : Differentiable>(x: T, y: T) -> (value: T, pullback: (T.Cotan
   return (x, { ($0, $0) })
 }
 @differentiating(generic)
-func jvpGeneric<T : Differentiable>(x: T, y: T) -> (value: T, differential: (T.TangentVector) -> (T.TangentVector, T.TangentVector)) {
-  return (x, { ($0, $0) })
+func jvpGeneric<T : Differentiable>(x: T, y: T) -> (value: T, differential: (T.TangentVector, T.TangentVector) -> T.TangentVector) {
+  return (x, { $0 + $1 })
 }
-@differentiating(generic) // expected-error {{'@differentiating' attribute requires function to return a two-element tuple (second element must have label 'pullback:' or 'differential:')}}
+// expected-error @+1 {{'@differentiating' attribute requires function to return a two-element tuple (second element must have label 'pullback:' or 'differential:')}}
+@differentiating(generic)
 func vjpGenericWrongLabel<T : Differentiable>(x: T, y: T) -> (value: T, (T) -> (T, T)) {
   return (x, { ($0, $0) })
 }
-@differentiating(generic) // expected-error {{unexpected differentiation parameters type; got '(T, T)', but expected 'T'}}
+// expected-error @+1 {{could not find function 'generic' with expected type '<T where T : Differentiable, T == T.CotangentVector> (x: T) -> T'}}
+@differentiating(generic)
 func vjpGenericDiffParamMismatch<T : Differentiable>(x: T) -> (value: T, pullback: (T) -> (T, T)) where T == T.CotangentVector {
   return (x, { ($0, $0) })
 }
@@ -58,7 +66,7 @@ func vjpGenericExtraGenericRequirements<T : Differentiable & FloatingPoint>(x: T
 func foo<T : FloatingPoint & Differentiable>(_ x: T) -> T { return x }
 
 // expected-error @+2 {{type 'T' does not conform to protocol 'FloatingPoint'}}
-// expected-error @+1 {{'foo' does not have expected type '<T where T : AdditiveArithmetic, T : Differentiable> (T) -> T'}}
+// expected-error @+1 {{could not find function 'foo' with expected type '<T where T : AdditiveArithmetic, T : Differentiable> (T) -> T'}}
 @differentiating(foo)
 func vjpFoo<T : AdditiveArithmetic & Differentiable>(_ x: T) -> (value: T, pullback: (T.CotangentVector) -> (T.CotangentVector)) {
   return (x, { $0 })
@@ -99,7 +107,7 @@ extension Differentiable where Self : AdditiveArithmetic {
 }
 
 extension AdditiveArithmetic where Self : Differentiable, Self == Self.CotangentVector {
-  // expected-error @+1 {{unexpected differentiation parameters type; got '(Self, Self)', but expected '(Self, Self, Self)'}}
+  // expected-error @+1 {{could not find function '+' with expected type '<Self where Self : Differentiable, Self == Self.CotangentVector> (Self) -> (Self, Self) -> Self'}}
   @differentiating(+)
   func vjpPlusInstanceMethod(x: Self, y: Self) -> (value: Self, pullback: (Self) -> (Self, Self)) {
     return (x + y, { v in (v, v) })
@@ -109,12 +117,16 @@ extension AdditiveArithmetic where Self : Differentiable, Self == Self.Cotangent
 // Test instance methods.
 
 protocol InstanceMethod : Differentiable {
+  // expected-note @+1 {{'foo' defined here}}
   func foo(_ x: Self) -> Self
+  // expected-note @+1 {{'bar' defined here}}
+  func bar<T : Differentiable>(_ x: T) -> Self
 }
 
 extension InstanceMethod {
   // If `Self` conforms to `Differentiable`, then `Self` is currently always inferred to be a differentiation parameter.
-  // expected-error @+1 {{unexpected differentiation parameters type; got 'Self.CotangentVector', but expected '(Self.CotangentVector, Self.CotangentVector)'}}
+  // expected-error @+2 {{function result's 'pullback' type does not match 'foo'}}
+  // expected-note @+2 {{'pullback' does not have expected type '(Self.CotangentVector) -> (Self.CotangentVector, Self.CotangentVector)'}}
   @differentiating(foo)
   func vjpFoo(x: Self) -> (value: Self, pullback: (Self.CotangentVector) -> Self.CotangentVector) {
     return (x, { $0 })
@@ -126,10 +138,44 @@ extension InstanceMethod {
   }
 }
 
-extension InstanceMethod where Self == Self.CotangentVector {
+extension InstanceMethod {
+  // expected-error @+2 {{function result's 'pullback' type does not match 'bar'}}
+  // expected-note @+2 {{'pullback' does not have expected type '(Self.CotangentVector) -> (Self.CotangentVector, T.CotangentVector)'}}
+  @differentiating(bar)
+  func vjpBar<T : Differentiable>(_ x: T) -> (value: Self, pullback: (Self.CotangentVector) -> T.CotangentVector) {
+    return (self, { _ in .zero })
+  }
+
+  @differentiating(bar)
+  func vjpBar<T : Differentiable>(_ x: T) -> (value: Self, pullback: (Self.CotangentVector) -> (Self.CotangentVector, T.CotangentVector)) {
+    return (self, { ($0, .zero) })
+  }
+
+  @differentiating(bar)
+  func jvpBar<T : Differentiable>(_ x: T) -> (value: Self, differential: (Self.TangentVector, T.TangentVector) -> Self.TangentVector) {
+    return (self, { dself, dx in dself })
+  }
+}
+
+extension InstanceMethod where Self == Self.TangentVector, Self == Self.CotangentVector {
   @differentiating(foo)
   func vjpFooExtraRequirements(x: Self) -> (value: Self, pullback: (Self) -> (Self, Self)) {
     return (x, { ($0, $0) })
+  }
+
+  @differentiating(foo)
+  func jvpFooExtraRequirements(x: Self) -> (value: Self, differential: (Self, Self) -> (Self)) {
+    return (x, { $0 + $1 })
+  }
+
+  @differentiating(bar)
+  func vjpBarExtraRequirements<T : Differentiable>(x: T) -> (value: Self, pullback: (Self) -> (Self, T.CotangentVector)) {
+    return (self, { ($0, .zero) })
+  }
+
+  @differentiating(bar)
+  func jvpBarExtraRequirements<T : Differentiable>(_ x: T) -> (value: Self, differential: (Self, T.TangentVector) -> Self) {
+    return (self, { dself, dx in dself })
   }
 }
 
