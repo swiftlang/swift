0dfaa19f9f637fc43fd3d7ae0388f1687b55385f
Michael Gottesman <mgottesman@apple.com>
2019-03-18 00:54:02 -0700

    [ownership] Rename enable-sil-ownership => verify-sil-ownership.
    I have been meaning to do this change for a minute, but kept on putting it off.
    This describes what is actually happening and is a better name for the option.

--- benchmark/cmake/modules/AddSwiftBenchmarkSuite.cmake
+++ benchmark/cmake/modules/AddSwiftBenchmarkSuite.cmake
@@ -314,7 +314,7 @@ function (swift_benchmark_compile_archopts)
 
   set(common_options
       "-c"
-      "-Xfrontend" "-enable-sil-ownership"
+      "-Xfrontend" "-verify-sil-ownership"
       "-target" "${target}"
       "-${BENCH_COMPILE_ARCHOPTS_OPT}" ${PAGE_ALIGNMENT_OPTION})
 
@@ -344,7 +344,7 @@ function (swift_benchmark_compile_archopts)
 
   set(common_options_driver
       "-c"
-      "-Xfrontend" "-enable-sil-ownership"
+      "-Xfrontend" "-verify-sil-ownership"
       "-target" "${target}"
       "-${driver_opt}")
 
