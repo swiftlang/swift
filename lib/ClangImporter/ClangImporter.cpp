8a015a54ffc1d3967af3af3ab952cd890ff5b70b
Davide Italiano <ditaliano@apple.com>
2019-03-18 11:30:43 -0700

    Revert "ClangImporter: Use the new CacheGeneratedPCH in Clang"
    This reverts commit da8a1c57e88263c582252f37bc57cfdf04aa53b6.

--- lib/ClangImporter/ClangImporter.cpp
+++ lib/ClangImporter/ClangImporter.cpp
@@ -1451,7 +1451,6 @@ ClangImporter::emitBridgingPCH(StringRef headerPath,
   invocation->getFrontendOpts().ProgramAction = clang::frontend::GeneratePCH;
   invocation->getPreprocessorOpts().resetNonModularOptions();
   invocation->getLangOpts()->NeededByPCHOrCompilationUsesPCH = true;
-  invocation->getLangOpts()->CacheGeneratedPCH = true;
 
   clang::CompilerInstance emitInstance(
     Impl.Instance->getPCHContainerOperations());
