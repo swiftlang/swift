fb410f9c50d38a25808c78bec35d3fc1be8d965d
Brent Royal-Gordon <broyalgordon@apple.com>
2019-03-20 09:59:43 -0700

    Fix miscellaneous “subscripts can’t be static” assumptions

--- test/decl/subscript/static.swift
+++ test/decl/subscript/static.swift
@@ -3,10 +3,24 @@
 // FIXME: Write proper tests (this file is mainly suitable for interactive testing)
 
 struct MyStruct {
-    static subscript(_ i: Int) -> String {
-        return "success"
-    }
+  static subscript(_ i: Int) -> String {
+    get { return "get \(i)" }
+    set { print("set \(i)") }
+  }
 }
 
 print(MyStruct.self[0])
 print(MyStruct[0])
+
+MyStruct.self[1] = "zyzyx"
+MyStruct[2] = "asdfg"
+
+@dynamicMemberLookup
+class Dyn {
+  static subscript(dynamicMember name: String) -> String {
+    return "Dyn.\(name)"
+  }
+}
+
+print(Dyn.foo)
+print(Dyn.bar)
