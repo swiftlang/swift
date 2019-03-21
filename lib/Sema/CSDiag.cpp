d470e9df4de8d97fe3a4dd9684d59a817c997bfc
Slava Pestov <spestov@apple.com>
2019-03-21 00:51:41 -0400

    AST: Split off ArgumentShuffleExpr from TupleShuffleExpr
    Right now we use TupleShuffleExpr for two completely different things:
    
    - Tuple conversions, where elements can be re-ordered and labels can be
      introduced/eliminated
    - Complex argument lists, involving default arguments or varargs
    
    The first case does not allow default arguments or varargs, and the
    second case does not allow re-ordering or introduction/elimination
    of labels. Furthermore, the first case has a representation limitation
    that prevents us from expressing tuple conversions that change the
    type of tuple elements.
    
    For all these reasons, it is better if we use two separate Expr kinds
    for these purposes. For now, just make an identical copy of
    TupleShuffleExpr and call it ArgumentShuffleExpr. In CSApply, use
    ArgumentShuffleExpr when forming the arguments to a call, and keep
    using TupleShuffleExpr for tuple conversions. Each usage of
    TupleShuffleExpr has been audited to see if it should instead look at
    ArgumentShuffleExpr.
    
    In sequent commits I plan on redesigning TupleShuffleExpr to correctly
    represent all tuple conversions without any unnecessary baggage.
    
    Longer term, we actually want to change the representation of CallExpr
    to directly store an argument list; then instead of a single child
    expression that must be a ParenExpr, TupleExpr or ArgumentShuffleExpr,
    all CallExprs will have a uniform representation and ArgumentShuffleExpr
    will go away altogether. This should reduce memory usage and radically
    simplify parts of SILGen.

--- lib/Sema/CSDiag.cpp
+++ lib/Sema/CSDiag.cpp
@@ -3533,7 +3533,7 @@ public:
              "unexpected argument expression type");
       insertLoc = ArgExpr->getLoc();
 
-      // Can't be TupleShuffleExpr because this argExpr is not yet resolved.
+      // Can't be ArgumentShuffleExpr because this argExpr is not yet resolved.
     }
 
     assert(insertLoc.isValid() && "missing argument after trailing closure?");
