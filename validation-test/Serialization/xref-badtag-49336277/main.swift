swiftlang/swift

commit ec95e68ab91dd57dae9c7d2729484501bfc37d98

[Serialization] Collapse one level of helper function I didn't need
No functionality change.

lib/Serialization/Deserialization.cpp
@@ -2309,7 +2309,6 @@ public:
   /// passing each one to AddAttribute.
   llvm::Error deserializeDeclAttributes();
 
-  static Expected<Decl *> getDeclCheckedImpl(ModuleFile &MF, DeclID DID);
   Expected<Decl *> getDeclCheckedImpl();
 
   Expected<Decl *> deserializeTypeAlias(ArrayRef<uint64_t> scratch,
@@ -3767,10 +3766,23 @@ public:
 Expected<Decl *>
 ModuleFile::getDeclChecked(DeclID DID) {
   // Tag every deserialized ValueDecl coming out of getDeclChecked with its ID.
-  Expected<Decl *> deserialized =
-      DeclDeserializer::getDeclCheckedImpl(*this, DID);
-  if (deserialized && deserialized.get()) {
-    if (auto *IDC = dyn_cast<IterableDeclContext>(deserialized.get())) {
+  if (DID == 0)
+    return nullptr;
+
+  assert(DID <= Decls.size() && "invalid decl ID");
+  auto &declOrOffset = Decls[DID-1];
+
+  if (declOrOffset.isComplete())
+    return declOrOffset;
+
+  ++NumDeclsLoaded;
+  BCOffsetRAII restoreOffset(DeclTypeCursor);
+  DeclTypeCursor.JumpToBit(declOrOffset);
+
+  SWIFT_DEFER {
+    if (!declOrOffset.isComplete())
+      return;
+    if (auto *IDC = dyn_cast_or_null<IterableDeclContext>(declOrOffset.get())) {
       // Only set the DeclID on the returned Decl if it's one that was loaded
       // and _wasn't_ one that had its DeclID set elsewhere (a followed XREF).
       if (IDC->wasDeserialized() &&
@@ -3778,8 +3790,10 @@ ModuleFile::getDeclChecked(DeclID DID) {
         IDC->setDeclID(DID);
       }
     }
-  }
-  return deserialized;
+  };
+
+  ModuleFile::DeserializingEntityRAII deserializingEntity(*this);
+  return DeclDeserializer(*this, declOrOffset).getDeclCheckedImpl();
 }
 
 llvm::Error DeclDeserializer::deserializeDeclAttributes() {
@@ -4052,25 +4066,6 @@ llvm::Error DeclDeserializer::deserializeDeclAttributes() {
     restoreOffset.cancel();
     scratch.clear();
   }
-}
-
-Expected<Decl *>
-DeclDeserializer::getDeclCheckedImpl(ModuleFile &MF, DeclID DID) {
-  if (DID == 0)
-    return nullptr;
-
-  assert(DID <= MF.Decls.size() && "invalid decl ID");
-  auto &declOrOffset = MF.Decls[DID-1];
-
-  if (declOrOffset.isComplete())
-    return declOrOffset;
-
-  ++NumDeclsLoaded;
-  BCOffsetRAII restoreOffset(MF.DeclTypeCursor);
-  MF.DeclTypeCursor.JumpToBit(declOrOffset);
-
-  ModuleFile::DeserializingEntityRAII deserializingEntity(MF);
-  return DeclDeserializer(MF, declOrOffset).getDeclCheckedImpl();
 }
 
 Expected<Decl *>

commit 1b2cb9c72327451e710e415843cf476841c850c0

[Serialization] Fix bad tagging of deserialized decls that can result in failed member lookups
The issue was introduced in https://github.com/apple/swift/pull/22610/commits/ec95e68ab91dd57dae9c7d2729484501bfc37d98 which changed the behavior of `ModuleFile::getDeclChecked()` slightly.

rdar://49336277

lib/Serialization/Deserialization.cpp
@@ -3751,35 +3751,35 @@ public:
 
 Expected<Decl *>
 ModuleFile::getDeclChecked(DeclID DID) {
-  // Tag every deserialized ValueDecl coming out of getDeclChecked with its ID.
   if (DID == 0)
     return nullptr;
 
   assert(DID <= Decls.size() && "invalid decl ID");
   auto &declOrOffset = Decls[DID-1];
 
-  if (declOrOffset.isComplete())
-    return declOrOffset;
+  if (!declOrOffset.isComplete()) {
+    ++NumDeclsLoaded;
+    BCOffsetRAII restoreOffset(DeclTypeCursor);
+    DeclTypeCursor.JumpToBit(declOrOffset);
 
-  ++NumDeclsLoaded;
-  BCOffsetRAII restoreOffset(DeclTypeCursor);
-  DeclTypeCursor.JumpToBit(declOrOffset);
+    ModuleFile::DeserializingEntityRAII deserializingEntity(*this);
+    Expected<Decl *> deserialized =
+      DeclDeserializer(*this, declOrOffset).getDeclCheckedImpl();
+    if (!deserialized)
+      return deserialized;
+  }
 
-  SWIFT_DEFER {
-    if (!declOrOffset.isComplete())
-      return;
-    if (auto *IDC = dyn_cast_or_null<IterableDeclContext>(declOrOffset.get())) {
-      // Only set the DeclID on the returned Decl if it's one that was loaded
-      // and _wasn't_ one that had its DeclID set elsewhere (a followed XREF).
-      if (IDC->wasDeserialized() &&
-          static_cast<uint32_t>(IDC->getDeclID()) == 0) {
-        IDC->setDeclID(DID);
-      }
+  // Tag every deserialized ValueDecl coming out of getDeclChecked with its ID.
+  assert(declOrOffset.isComplete());
+  if (auto *IDC = dyn_cast_or_null<IterableDeclContext>(declOrOffset.get())) {
+    // Only set the DeclID on the returned Decl if it's one that was loaded
+    // and _wasn't_ one that had its DeclID set elsewhere (a followed XREF).
+    if (IDC->wasDeserialized() &&
+        static_cast<uint32_t>(IDC->getDeclID()) == 0) {
+      IDC->setDeclID(DID);
     }
-  };
-
-  ModuleFile::DeserializingEntityRAII deserializingEntity(*this);
-  return DeclDeserializer(*this, declOrOffset).getDeclCheckedImpl();
+  }
+  return declOrOffset;
 }
 
 llvm::Error DeclDeserializer::deserializeDeclAttributes() {

validation-test/Serialization/xref-badtag-49336277/Inputs/basemod.swift
@@ -1,0 +1,13 @@
+import itermod
+
+public protocol MySequence {
+  associatedtype Element
+  associatedtype Iterator: MyIter where Iterator.Element == Element
+}
+
+public struct MyBar: MySequence, MyIter {
+  public func callme() {}
+
+  public typealias Element = UInt8
+  public typealias Iterator = MyBar
+}

validation-test/Serialization/xref-badtag-49336277/Inputs/dependmod.swift
@@ -1,0 +1,4 @@
+import itermod
+import basemod
+
+extension MyIter where Element == MyBar {}

validation-test/Serialization/xref-badtag-49336277/Inputs/itermod.swift
@@ -1,0 +1,3 @@
+public protocol MyIter {
+  associatedtype Element
+}

validation-test/Serialization/xref-badtag-49336277/main.swift
@@ -1,0 +1,14 @@
+// RUN: %empty-directory(%t)
+// RUN: %target-swift-frontend %S/Inputs/itermod.swift -module-name itermod -emit-module -o %t/itermod.swiftmodule
+// RUN: %target-swift-frontend %S/Inputs/basemod.swift -module-name basemod -emit-module -o %t/basemod.swiftmodule -I %t
+// RUN: %target-swift-frontend %S/Inputs/dependmod.swift -module-name dependmod -emit-module -o %t/dependmod.swiftmodule -I %t
+// RUN: %target-swift-frontend -typecheck %s -I %t -verify
+
+import basemod
+import dependmod
+
+func test(b: MyBar) {
+  b.callme()
+  // Check that there was typechecking.
+  b.non_existent() // expected-error {{no member 'non_existent'}}
+}