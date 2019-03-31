33fe71f0c2b04c7b91acd954f626944ad2c5b3fa
Suyash Srijan <suyashsrijan@outlook.com>
2019-03-31 20:03:43 +0100

    [test] fix indentation

--- test/expr/expressions.swift
+++ test/expr/expressions.swift
@@ -244,7 +244,7 @@ func test_lambda() {
   var a = { (value: Int) -> () in markUsed(value+1) }
 
   // A recursive lambda.
-	var fib: (Int) -> Int = { (n: Int) -> Int in
+  var fib: (Int) -> Int = { (n: Int) -> Int in
     if (n < 2) {
       return n
     }
