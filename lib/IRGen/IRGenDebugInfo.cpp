f3a624a5ea986563750003d34fa337fe7d4f565f
Adrian Prantl <aprantl@apple.com>
2019-04-03 15:39:58 -0700

    Add an option to disable DWARF skeleton CU breadcrumbs for Clang module imports. This is useful when building redistributable static archives, since any pointers into the CLang module cache won't be portable.
    When using this option the Clang type fallback path in LLDB will be
    less useful since DWARF type information from those modules will not
    be available unless another object file compiled without the option
    imported the same modules.
    
    rdar://problem/48827784
    (cherry picked from commit 4a2351206802063593e855e8adea96197800d279)

--- lib/IRGen/IRGenDebugInfo.cpp
+++ lib/IRGen/IRGenDebugInfo.cpp
@@ -640,16 +640,19 @@ private:
     std::string RemappedIncludePath = DebugPrefixMap.remapPath(IncludePath);
 
     // For Clang modules / PCH, create a Skeleton CU pointing to the PCM/PCH.
-    bool CreateSkeletonCU = !ASTFile.empty();
-    bool IsRootModule = !Parent;
-    if (CreateSkeletonCU && IsRootModule) {
-      llvm::DIBuilder DIB(M);
-      DIB.createCompileUnit(IGM.ObjCInterop ? llvm::dwarf::DW_LANG_ObjC
-                                            : llvm::dwarf::DW_LANG_C99,
-                            DIB.createFile(Name, RemappedIncludePath),
-                            TheCU->getProducer(), true, StringRef(), 0, ASTFile,
-                            llvm::DICompileUnit::FullDebug, Signature);
-      DIB.finalize();
+    if (!Opts.DisableClangModuleSkeletonCUs) {
+      bool CreateSkeletonCU = !ASTFile.empty();
+      bool IsRootModule = !Parent;
+      if (CreateSkeletonCU && IsRootModule) {
+        llvm::DIBuilder DIB(M);
+        DIB.createCompileUnit(IGM.ObjCInterop ? llvm::dwarf::DW_LANG_ObjC
+                                              : llvm::dwarf::DW_LANG_C99,
+                              DIB.createFile(Name, RemappedIncludePath),
+                              TheCU->getProducer(), true, StringRef(), 0,
+                              ASTFile, llvm::DICompileUnit::FullDebug,
+                              Signature);
+        DIB.finalize();
+      }
     }
 
     StringRef Sysroot = IGM.Context.SearchPathOpts.SDKPath;
