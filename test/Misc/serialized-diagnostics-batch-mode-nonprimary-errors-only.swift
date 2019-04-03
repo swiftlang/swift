bd7a7cfcb2ec424b47da99051b7ef154dc09fa39
David Ungar <dungar@apple.com>
2019-04-03 13:53:27 -0700

    Update test

--- test/Misc/serialized-diagnostics-batch-mode-nonprimary-errors-only.swift
+++ test/Misc/serialized-diagnostics-batch-mode-nonprimary-errors-only.swift
@@ -2,9 +2,19 @@
 //
 // RUN: rm -f %t.*
 
+// First, put the causal primary first:
+
 // RUN: not %target-swift-frontend -typecheck  -primary-file %s  -serialize-diagnostics-path %t.main.dia -primary-file %S/../Inputs/empty.swift  -serialize-diagnostics-path %t.empty.dia %S/Inputs/serialized-diagnostics-batch-mode-suppression-helper.swift  2> %t.stderr.txt
 
-// RUN: test -e %t.main.dia -a ! -s %t.empty.dia
+// RUN: test -e %t.main.dia -a -s %t.main.dia
+// RUN: test -e %t.empty.dia -a ! -s %t.empty.dia
+
+// Now, put the causal primary second:
+// (No matter which order, the error message goes with the causal primary.)
+
+// RUN: not %target-swift-frontend -typecheck  -primary-file %S/../Inputs/empty.swift  -serialize-diagnostics-path %t.empty.dia -primary-file %s  -serialize-diagnostics-path %t.main.dia  %S/Inputs/serialized-diagnostics-batch-mode-suppression-helper.swift  2> %t.stderr.txt
+
+// RUN: test -e %t.main.dia -a -s %t.main.dia
 // RUN: test -e %t.empty.dia -a ! -s %t.empty.dia
 
 func test(x: SomeType) {
