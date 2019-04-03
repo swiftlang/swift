d9732a050fbb40f5bcf2f0dfe4b963f6f5955cf7
Brent Royal-Gordon <brentdax@apple.com>
2019-04-02 18:26:03 -0700

    Allow the declaration of static subscripts
    In this commit, MyStruct.self[0] parses and typechecks but the solution doesn’t apply correctly. MyStruct[0] gets diagnosed as an error.

---
+++ test/decl/subscript/static.swift
@@ -0,0 +1,12 @@
+// RUN: %target-typecheck-verify-swift
+
+// FIXME: Write proper tests (this file is mainly suitable for interactive testing)
+
+struct MyStruct {
+    static subscript(_ i: Int) -> String {
+        return "success"
+    }
+}
+
+//print(MyStruct.self[0])
+//print(MyStruct[0])
