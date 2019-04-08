145e6d9b7bf599a02083f7270f08b1c71d12a0d2
Pavel Yaskevich <pyaskevich@apple.com>
2019-04-08 01:24:38 -0700

    [ConstraintLocator] Add a special locator element to denote contextual type of single expr function

--- lib/Sema/ConstraintLocator.cpp
+++ lib/Sema/ConstraintLocator.cpp
@@ -82,6 +82,7 @@ void ConstraintLocator::Profile(llvm::FoldingSetNodeID &id, Expr *anchor,
     case KeyPathType:
     case KeyPathRoot:
     case KeyPathValue:
+    case SingleExprFuncResultType:
       if (unsigned numValues = numNumericValuesInPathElement(elt.getKind())) {
         id.AddInteger(elt.getValue());
         if (numValues > 1)
@@ -360,8 +361,11 @@ void ConstraintLocator::dump(SourceManager *sm, raw_ostream &out) {
     case KeyPathValue:
       out << " keypath value";
       break;
+
+    case SingleExprFuncResultType:
+      out << " expected result type of the function with a single expression";
+      break;
     }
   }
-
   out << ']';
 }
