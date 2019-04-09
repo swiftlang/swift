da317a5a215669555f69a37b4cf56fad68d573d7
Doug Friedman <dfriedm3@gmail.com>
2019-04-09 11:09:49 -0400

    [TF-76][Python Interop] add PythonConvertible conformance to TensorShape (#23762)
    * [TF-76] add TensorShape conformance to PythonConvertible

--- test/TensorFlowRuntime/numpy_conversion.swift
+++ test/TensorFlowRuntime/numpy_conversion.swift
@@ -52,6 +52,18 @@ NumpyConversionTests.test("shaped-array-conversion") {
                 array)
   }
 
+  let reshaped = np.reshape(numpyArrayInt32, [2, 3] as TensorShape)
+  if let array = expectNotNil(ShapedArray<Int32>(numpy: reshaped)) {
+    expectEqual(ShapedArray(shape: [2, 3], scalars: [1, 2, 3, 4, 5, 6]),
+                array)
+  }
+
+  let numpyArray1D = np.ones(28)
+  let reshaped3D = np.reshape(numpyArray1D, [2, 7, 2] as TensorShape)
+  expectEqual(TensorShape(reshaped3D.shape), [2, 7, 2])
+  let reshaped2D = np.reshape(reshaped3D, [14, 2] as TensorShape)
+  expectEqual(TensorShape(reshaped2D.shape), [14, 2])
+
   let numpyArrayStrided = np.array([[1, 2], [1, 2]], dtype: np.int32)[
       Python.slice(Python.None), 1]
   // Assert that the array has a stride, so that we're certainly testing a
@@ -95,6 +107,12 @@ NumpyConversionTests.test("tensor-conversion") {
                 tensor.array)
   }
 
+  let reshaped = np.reshape(numpyArrayInt32, [2, 3] as TensorShape)
+  if let tensor = expectNotNil(Tensor<Int32>(numpy: reshaped)) {
+    expectEqual(ShapedArray(shape: [2, 3], scalars: [1, 2, 3, 4, 5, 6]),
+                tensor.array)
+  }
+
   let numpyArrayStrided = np.array([[1, 2], [1, 2]], dtype: np.int32)[
       Python.slice(Python.None), 1]
   // Assert that the array has a stride, so that we're certainly testing a
@@ -117,6 +135,12 @@ NumpyConversionTests.test("tensor-round-trip") {
 
   let t3 = Tensor<Int32>(repeating: 30, shape: [8,5,4])
   expectEqual(t3, Tensor<Int32>(numpy: t3.makeNumpyArray())!)
+}
+
+NumpyConversionTests.test("tensor-shape") {
+  let pyArray = [2, 3].pythonObject
+  expectEqual(pyArray, TensorShape(2, 3).pythonObject)
+  expectEqual(TensorShape(2, 3), TensorShape(pyArray))
 }
 #endif
 
