df61110ef9d6d4a23131e6da991e5cc1a81b1aae
Kuba Mracek <mracek@apple.com>
2019-04-11 09:49:42 -0700

    Undo "Disable TSan in coroutine functions" (0ca3f79).
    This is no longer needed because we now make sure to run the coroutine lowering pass before ASan/TSan instrumentation passes.
    
    Also fixes a typo in the test.

--- test/IRGen/tsan-attributes.swift
+++ test/IRGen/tsan-attributes.swift
@@ -1,6 +1,6 @@
 // This test verifies that we add the function attributes used by TSan.
 
-// RUN: %target-swift-frontend -emit-ir -disable-llvm-optzns -sanitize=thread %s | %FileCheck %s -check-prefix=TSAN
+// RUN: %target-swift-frontend -emit-ir -sanitize=thread %s | %FileCheck %s -check-prefix=TSAN
 
 // TSan is currently only supported on 64 bit mac and simulators.
 // (We do not test the simulators here.)
@@ -22,5 +22,5 @@ public var x: Int {
 // TSAN-SAME: }
 
 // TSAN: attributes [[COROUTINE_ATTRS]] =
-// TSAN-NOT: sanitize_address
+// TSAN-SAME: sanitize_thread
 // TSAN-SAME: }
