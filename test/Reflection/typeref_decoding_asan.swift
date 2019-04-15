7b413be79a7a73cfc13d9339750a0103d905a810
sarveshtamba <37213716+sarveshtamba@users.noreply.github.com>
2019-04-15 13:53:59 +0530

    Update typeref_decoding_asan.swift

--- test/Reflection/typeref_decoding_asan.swift
+++ test/Reflection/typeref_decoding_asan.swift
@@ -2,6 +2,8 @@
 // RUN: %empty-directory(%t)
 // RUN: %target-build-swift %S/Inputs/ConcreteTypes.swift %S/Inputs/GenericTypes.swift %S/Inputs/Protocols.swift %S/Inputs/Extensions.swift %S/Inputs/Closures.swift -parse-as-library -emit-module -emit-library -module-name TypesToReflect -sanitize=address -o %t/%target-library-name(TypesToReflect)
 // RUN: %target-swift-reflection-dump -binary-filename %t/%target-library-name(TypesToReflect) | %FileCheck %s
+// XFAIL: CPU=powerpc64le
+// SR-10341
 
 // CHECK: FIELDS:
 // CHECK: =======
