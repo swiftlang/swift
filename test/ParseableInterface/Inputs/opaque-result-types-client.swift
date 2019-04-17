6500ef339208ef007e3aca8e4e79b433e65879c5
Joe Groff <jgroff@apple.com>
2019-04-17 13:37:33 -0700

    Track var/subscript opaque return decls for type reconstruction.

--- test/ParseableInterface/Inputs/opaque-result-types-client.swift
+++ test/ParseableInterface/Inputs/opaque-result-types-client.swift
@@ -3,6 +3,12 @@ import OpaqueResultTypes
 func getAssocType<T: AssocTypeInference>(_ x: T) -> T.Assoc {
   return x.foo(0)
 }
+func getAssocPropType<T: AssocTypeInference>(_ x: T) -> T.AssocProperty {
+  return x.prop
+}
+func getAssocSubscriptType<T: AssocTypeInference>(_ x: T) -> T.AssocSubscript {
+  return x[]
+}
 
 struct MyFoo: Foo {}
 struct YourFoo: Foo {}
@@ -29,6 +35,18 @@ func someTypeIsTheSame() {
   d = barString.foo(0) // expected-error{{cannot assign}}
   d = getAssocType(barInt)
   d = getAssocType(barString) // expected-error{{cannot assign}}
+  
+  var d2 = barInt.prop
+  d2 = barInt.prop
+  d2 = barString.prop // expected-error{{cannot assign}}
+  d2 = getAssocPropType(barInt)
+  d2 = getAssocPropType(barString) // expected-error{{cannot assign}}
+
+  var d3 = barInt[]
+  d3 = barInt[]
+  d3 = barString[] // expected-error{{cannot assign}}
+  d3 = getAssocSubscriptType(barInt)
+  d3 = getAssocSubscriptType(barString) // expected-error{{cannot assign}}
 
   var e = barString.foo(0)
   e = barInt.foo(0) // expected-error{{cannot assign}}
