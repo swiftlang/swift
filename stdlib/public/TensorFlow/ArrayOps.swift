726ef7e834f390f702558a4234ec01e597ba6496
Richard Wei <rxwei@google.com>
2019-04-17 12:23:53 -0700

    Update stdlib/public/TensorFlow/ArrayOps.swift
    Co-Authored-By: pschuh <parkers@google.com>

--- stdlib/public/TensorFlow/ArrayOps.swift
+++ stdlib/public/TensorFlow/ArrayOps.swift
@@ -114,7 +114,7 @@ public extension Raw {
   ///
   /// - Parameters:
   ///   - value: The tensor to split.
-  ///   - size_splits: list containing the sizes of each output tensor along the split
+  ///   - sizeSplits: list containing the sizes of each output tensor along the split
   ///     dimension. Must sum to the dimension of value along split_dim.
   ///     Can contain one -1 indicating that dimension is to be inferred.
   ///   - split_dim: 0-D.  The dimension along which to split.  Must be in the range
