swiftlang/swift

commit 7566f98a454e4f2eba4c1800b2fdc338e4df2e44

Sema: Diagnose enum inheritance clause containing subclass existential
Also, tidy up the code a bit and stop emitting redundant diagnostics for
associated types.

Fixes <https://bugs.swift.org/browse/SR-10232>.

lib/Sema/TypeCheckDecl.cpp
@@ -215,7 +215,6 @@ static void validateAttributes(TypeChecker &TC, Decl *D);
 /// file.
 static void checkInheritanceClause(
                     llvm::PointerUnion<TypeDecl *, ExtensionDecl *> declUnion) {
-  TypeResolutionOptions options = None;
   DeclContext *DC;
   MutableArrayRef<TypeLoc> inheritedClause;
   ExtensionDecl *ext = nullptr;
@@ -224,7 +223,6 @@ static void checkInheritanceClause(
   if ((ext = declUnion.dyn_cast<ExtensionDecl *>())) {
     decl = ext;
     DC = ext;
-    options |= TypeResolutionFlags::AllowUnavailableProtocol;
 
     inheritedClause = ext->getInherited();
 
@@ -243,13 +241,16 @@ static void checkInheritanceClause(
     decl = typeDecl;
     if (auto nominal = dyn_cast<NominalTypeDecl>(typeDecl)) {
       DC = nominal;
-      options |= TypeResolutionFlags::AllowUnavailableProtocol;
     } else {
       DC = typeDecl->getDeclContext();
     }
 
     inheritedClause = typeDecl->getInherited();
   }
+
+  bool canHaveSuperclass = (isa<ClassDecl>(decl) ||
+                            (isa<ProtocolDecl>(decl) &&
+                             !cast<ProtocolDecl>(decl)->isObjC()));
 
   ASTContext &ctx = decl->getASTContext();
   auto &diags = ctx.Diags;
@@ -258,13 +259,6 @@ static void checkInheritanceClause(
   auto getStartLocOfInheritanceClause = [&] {
     if (ext)
       return ext->getSourceRange().End;
-
-    if (auto genericTypeDecl = dyn_cast<GenericTypeDecl>(typeDecl)) {
-      if (auto genericParams = genericTypeDecl->getGenericParams())
-        return genericParams->getSourceRange().End;
-
-      return genericTypeDecl->getNameLoc();
-    }
 
     return typeDecl->getNameLoc();
   };
@@ -316,6 +310,12 @@ static void checkInheritanceClause(
     if (!inheritedTy || inheritedTy->hasError())
       continue;
 
+    // For generic parameters and associated types, the GSB checks constraints;
+    // however, we still want to fire off the requests to produce diagnostics
+    // in some circular validation cases.
+    if (isa<AbstractTypeParamDecl>(decl))
+      continue;
+
     // Check whether we inherited from 'AnyObject' twice.
     // Other redundant-inheritance scenarios are checked below, the
     // GenericSignatureBuilder (for protocol inheritance) or the
@@ -351,41 +351,36 @@ static void checkInheritanceClause(
     if (inheritedTy->isExistentialType()) {
       auto layout = inheritedTy->getExistentialLayout();
 
-      // @objc protocols cannot have superclass constraints.
+      // Inheritance from protocol compositions that do not contain classes
+      // or AnyObject is always OK.
+      if (!layout.hasExplicitAnyObject &&
+          !layout.explicitSuperclass)
+        continue;
+
+      // Protocols can inherit from AnyObject.
+      if (layout.hasExplicitAnyObject &&
+          isa<ProtocolDecl>(decl))
+        continue;
+
+      // Class-constrained protocol compositions are not allowed except in
+      // special cases.
       if (layout.explicitSuperclass) {
-        if (auto *protoDecl = dyn_cast<ProtocolDecl>(decl)) {
-          if (protoDecl->isObjC()) {
-            protoDecl->diagnose(diag::objc_protocol_with_superclass,
-                                protoDecl->getName());
-            continue;
-          }
+        if (!canHaveSuperclass) {
+          decl->diagnose(diag::inheritance_from_protocol_with_superclass,
+                         inheritedTy);
+          continue;
+        }
+
+        // Classes can inherit from protocol compositions that contain a
+        // superclass, but not AnyObject.
+        if (isa<ClassDecl>(decl) &&
+            !layout.hasExplicitAnyObject) {
+          // Superclass inheritance is handled below.
+          inheritedTy = layout.explicitSuperclass;
         }
       }
-
-      // Protocols, generic parameters and associated types can inherit
-      // from subclass existentials, which are "exploded" into their
-      // corresponding requirements.
-      //
-      // Extensions, structs and enums can only inherit from protocol
-      // compositions that do not contain AnyObject or class members.
-      if (isa<ProtocolDecl>(decl) ||
-          isa<AbstractTypeParamDecl>(decl) ||
-          (!layout.hasExplicitAnyObject &&
-           !layout.explicitSuperclass)) {
-        continue;
-      }
-
-      // Classes can inherit from subclass existentials as long as they
-      // do not contain an explicit AnyObject member.
-      if (isa<ClassDecl>(decl) &&
-          !layout.hasExplicitAnyObject) {
-        // Superclass inheritance is handled below.
-        inheritedTy = layout.explicitSuperclass;
-        if (!inheritedTy)
-          continue;
-      }
     }
-    
+
     // If this is an enum inheritance clause, check for a raw type.
     if (isa<EnumDecl>(decl)) {
       // Check if we already had a raw type.
@@ -445,29 +440,6 @@ static void checkInheritanceClause(
         continue;
       }
 
-      // @objc protocols cannot have superclass constraints.
-      if (auto *protoDecl = dyn_cast<ProtocolDecl>(decl)) {
-        if (protoDecl->isObjC()) {
-          protoDecl->diagnose(diag::objc_protocol_with_superclass,
-                              protoDecl->getName());
-          continue;
-        }
-      }
-
-      // If the declaration we're looking at doesn't allow a superclass,
-      // complain.
-      if (isa<StructDecl>(decl) || isa<ExtensionDecl>(decl)) {
-        decl->diagnose(isa<ExtensionDecl>(decl)
-                         ? diag::extension_class_inheritance
-                         : diag::non_class_inheritance,
-                       isa<ExtensionDecl>(decl)
-                         ? cast<ExtensionDecl>(decl)->getDeclaredInterfaceType()
-                         : cast<TypeDecl>(decl)->getDeclaredInterfaceType(),
-                       inheritedTy)
-          .highlight(inherited.getSourceRange());
-        continue;
-      }
-
       // If this is not the first entry in the inheritance clause, complain.
       if (isa<ClassDecl>(decl) && i > 0) {
         auto removeRange = getRemovalRange(i);
@@ -480,21 +452,18 @@ static void checkInheritanceClause(
         // Fall through to record the superclass.
       }
 
-      // Record the superclass.
-      superclassTy = inheritedTy;
-      superclassRange = inherited.getSourceRange();
-      continue;
+      if (canHaveSuperclass) {
+        // Record the superclass.
+        superclassTy = inheritedTy;
+        superclassRange = inherited.getSourceRange();
+        continue;
+      }
     }
 
-    // The GenericSignatureBuilder diagnoses problems with generic type
-    // parameters.
-    if (isa<GenericTypeParamDecl>(decl))
-      continue;
-
     // We can't inherit from a non-class, non-protocol type.
-    decl->diagnose((isa<StructDecl>(decl) || isa<ExtensionDecl>(decl))
-                     ? diag::inheritance_from_non_protocol
-                     : diag::inheritance_from_non_protocol_or_class,
+    decl->diagnose(canHaveSuperclass
+                   ? diag::inheritance_from_non_protocol_or_class
+                   : diag::inheritance_from_non_protocol,
                    inheritedTy);
     // FIXME: Note pointing to the declaration 'inheritedTy' references?
   }

test/decl/protocol/protocol_with_superclass_objc.swift
@@ -3,11 +3,11 @@
 class Base {}
 
 @objc protocol Protocol1 : Base {}
-// expected-error@-1 {{protocol 'Protocol1' is '@objc' and cannot have a superclass constraint}}
+// expected-error@-1 {{inheritance from non-protocol type 'Base'}}
 
 @objc protocol OtherProtocol {}
 
 typealias Composition = OtherProtocol & Base
 
 @objc protocol Protocol2 : Composition {}
-// expected-error@-1 {{protocol 'Protocol2' is '@objc' and cannot have a superclass constraint}}
+// expected-error@-1 {{inheritance from class-constrained protocol composition type 'Composition' (aka 'Base & OtherProtocol')}}

include/swift/AST/DiagnosticsSema.def
@@ -2268,14 +2268,10 @@ ERROR(duplicate_inheritance,none,
   "duplicate inheritance from %0", (Type))
 WARNING(duplicate_anyobject_class_inheritance,none,
   "redundant inheritance from 'AnyObject' and Swift 3 'class' keyword", ())
-ERROR(objc_protocol_with_superclass,none,
-  "protocol %0 is '@objc' and cannot have a superclass constraint", (Identifier))
+ERROR(inheritance_from_protocol_with_superclass,none,
+  "inheritance from class-constrained protocol composition type %0", (Type))
 ERROR(multiple_inheritance,none,
   "multiple inheritance from classes %0 and %1", (Type, Type))
-ERROR(non_class_inheritance,none,
-  "non-class type %0 cannot inherit from class %1", (Type, Type))
-ERROR(extension_class_inheritance,none,
-  "extension of type %0 cannot inherit from class %1", (Type, Type))
 ERROR(inheritance_from_non_protocol_or_class,none,
   "inheritance from non-protocol, non-class type %0", (Type))
 ERROR(inheritance_from_non_protocol,none,

test/Constraints/generics.swift
@@ -672,7 +672,7 @@ protocol SR_7984_P {
 struct A<T: String> {} // expected-error {{type 'T' constrained to non-protocol, non-class type 'String'}}
 struct B<T> where T: String {} // expected-error {{type 'T' constrained to non-protocol, non-class type 'String'}}
 protocol C {
-  associatedtype Foo: String // expected-error {{inheritance from non-protocol, non-class type 'String'}} expected-error {{type 'Self.Foo' constrained to non-protocol, non-class type 'String'}}
+  associatedtype Foo: String // expected-error {{type 'Self.Foo' constrained to non-protocol, non-class type 'String'}}
 }
 protocol D {
   associatedtype Foo where Foo: String // expected-error {{type 'Self.Foo' constrained to non-protocol, non-class type 'String'}}

test/Generics/superclass_constraint.swift
@@ -105,9 +105,8 @@ func takeP5<T: P5>(_ t: T) {
 
 protocol P7 {
 	associatedtype Assoc: A, Other 
-	// FIXME: expected-error@-1{{multiple inheritance from classes 'A' and 'Other'}}
-	// expected-note@-2{{superclass constraint 'Self.Assoc' : 'A' written here}}
-	// expected-error@-3{{'Self.Assoc' cannot be a subclass of both 'Other' and 'A'}}
+	// expected-note@-1{{superclass constraint 'Self.Assoc' : 'A' written here}}
+	// expected-error@-2{{'Self.Assoc' cannot be a subclass of both 'Other' and 'A'}}
 }
 
 // CHECK: superclassConformance4

test/NameBinding/scope_map_lookup.swift
@@ -25,8 +25,7 @@ protocol P1 {
 // Protocols involving associated types.
 protocol AProtocol {
   associatedtype e : e
-  // expected-error@-1 {{inheritance from non-protocol, non-class type 'Self.e'}}
-  // expected-error@-2 {{type 'Self.e' constrained to non-protocol, non-class type 'Self.e'}}
+  // expected-error@-1 {{type 'Self.e' constrained to non-protocol, non-class type 'Self.e'}}
 }
 
 // Extensions.

test/Sema/circular_decl_checking.swift
@@ -43,11 +43,9 @@ func TopLevelGenericFunc2<T : TopLevelGenericFunc2>(x: T) -> T { return x} // ex
 var TopLevelVar: TopLevelVar? { return nil } // expected-error {{use of undeclared type 'TopLevelVar'}}
 
 
-// FIXME: The first error is redundant and isn't correct in what it states.
 protocol AProtocol {
   associatedtype e : e
   // expected-error@-1 {{type 'Self.e' constrained to non-protocol, non-class type 'Self.e'}}
-  // expected-error@-2 {{inheritance from non-protocol, non-class type 'Self.e'}}
 }
 
 

test/decl/enum/enumtest.swift
@@ -436,3 +436,9 @@ let _: NestedBazEnumGeneric.Baz<Int> = .none // ok
 let _: NestedBazEnumGeneric.Baz<Int> = .one // ok
 let _: NestedBazEnumGeneric.Baz<Int>? = .one // ok
 let _: NestedBazEnumGeneric.Baz<Int>?? = .one // ok
+
+class C {}
+protocol P {}
+
+enum E : C & P {}
+// expected-error@-1 {{inheritance from class-constrained protocol composition type 'C & P'}}

commit 06c819d46e273db8374f5777f0d2bcc067f7ccff

Sema: Allow protocols to inherit from subclass existentials
This was a regression from 7566f98a454e4f2eba4c1800b2fdc338e4df2e44.

Fixes <rdar://problem/50022955>.

lib/Sema/TypeCheckDecl.cpp
@@ -248,6 +248,8 @@ static void checkInheritanceClause(
     inheritedClause = typeDecl->getInherited();
   }
 
+  // Can this declaration's inheritance clause contain a class or
+  // subclass existential?
   bool canHaveSuperclass = (isa<ClassDecl>(decl) ||
                             (isa<ProtocolDecl>(decl) &&
                              !cast<ProtocolDecl>(decl)->isObjC()));
@@ -351,34 +353,38 @@ static void checkInheritanceClause(
     if (inheritedTy->isExistentialType()) {
       auto layout = inheritedTy->getExistentialLayout();
 
-      // Inheritance from protocol compositions that do not contain classes
-      // or AnyObject is always OK.
-      if (!layout.hasExplicitAnyObject &&
-          !layout.explicitSuperclass)
+      // Subclass existentials are not allowed except on classes and
+      // non-@objc protocols.
+      if (layout.explicitSuperclass &&
+          !canHaveSuperclass) {
+        decl->diagnose(diag::inheritance_from_protocol_with_superclass,
+                       inheritedTy);
         continue;
-
-      // Protocols can inherit from AnyObject.
-      if (layout.hasExplicitAnyObject &&
-          isa<ProtocolDecl>(decl))
-        continue;
-
-      // Class-constrained protocol compositions are not allowed except in
-      // special cases.
-      if (layout.explicitSuperclass) {
-        if (!canHaveSuperclass) {
-          decl->diagnose(diag::inheritance_from_protocol_with_superclass,
-                         inheritedTy);
-          continue;
-        }
-
-        // Classes can inherit from protocol compositions that contain a
-        // superclass, but not AnyObject.
-        if (isa<ClassDecl>(decl) &&
-            !layout.hasExplicitAnyObject) {
-          // Superclass inheritance is handled below.
-          inheritedTy = layout.explicitSuperclass;
-        }
       }
+
+      // AnyObject is not allowed except on protocols.
+      if (layout.hasExplicitAnyObject &&
+          !isa<ProtocolDecl>(decl)) {
+        decl->diagnose(canHaveSuperclass
+                       ? diag::inheritance_from_non_protocol_or_class
+                       : diag::inheritance_from_non_protocol,
+                       inheritedTy);
+        continue;
+      }
+
+      // If the existential did not have a class constraint, we're done.
+      if (!layout.explicitSuperclass)
+        continue;
+
+      // Classes and protocols can inherit from subclass existentials.
+      // For classes, we check for a duplicate superclass below.
+      // For protocols, the GSB emits its own warning instead.
+      if (isa<ProtocolDecl>(decl))
+        continue;
+
+      assert(isa<ClassDecl>(decl));
+      assert(canHaveSuperclass);
+      inheritedTy = layout.explicitSuperclass;
     }
 
     // If this is an enum inheritance clause, check for a raw type.
@@ -418,7 +424,9 @@ static void checkInheritanceClause(
       continue;
     }
 
-    // If this is a class type, it may be the superclass.
+    // If this is a class type, it may be the superclass. We end up here when
+    // the inherited type is either itself a class, or when it is a subclass
+    // existential via the existential type path above.
     if (inheritedTy->getClassOrBoundGenericClass()) {
       // First, check if we already had a superclass.
       if (superclassTy) {

test/decl/protocol/protocol_with_superclass.swift
@@ -340,3 +340,15 @@ extension Amb { // expected-error {{'Amb' is ambiguous for type lookup in this c
     _ = ConcreteAlias.self
   }
 }
+
+protocol ProtoRefinesClassComposition : Generic<Int> & BaseProto {}
+
+func usesProtoRefinesClass1(_ t: ProtoRefinesClassComposition) {
+  t.genericMethod((1, 2))
+  let _: BaseProto = t
+}
+
+func usesProtoRefinesClass2<T : ProtoRefinesClassComposition>(_ t: T) {
+  t.genericMethod((1, 2))
+  let _: BaseProto = t
+}

test/decl/protocol/protocol_with_superclass_objc.swift
@@ -11,3 +11,6 @@ typealias Composition = OtherProtocol & Base
 
 @objc protocol Protocol2 : Composition {}
 // expected-error@-1 {{inheritance from class-constrained protocol composition type 'Composition' (aka 'Base & OtherProtocol')}}
+
+@objc protocol Protocol3 : OtherProtocol & Base {}
+// expected-error@-1 {{inheritance from class-constrained protocol composition type 'Base & OtherProtocol'}}