3ac59cddd7914a2d61bc06514ff7e38042a4a3da
Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
2019-04-19 15:36:34 -0400

    [TF] Fix gradients in sum(squeezinAxes:) and mean(squeezinAxes:)
    sum(squeezinAxes:) and mean(squeezinAxes:) were throwing an error during the bawckward pass because the gradients weren't unsqueezed before being broadcast.
    Note that this could be refactored nicely if we had a function that took a list of ints for `expandingShape`.
    Second note: I may be wrong, but it seems like `_vjpMean(squeezingAxes axes: [Int])` is never used and only the Tensor<Int32> version is.

--- stdlib/public/TensorFlow/Gradients.swift
+++ stdlib/public/TensorFlow/Gradients.swift
@@ -579,7 +579,11 @@ extension Tensor where Scalar : TensorFlowFloatingPoint {
     squeezingAxes axes: Tensor<Int32>
   ) -> (Tensor, (Tensor) -> Tensor) {
     let value = sum(squeezingAxes: axes)
-    return (value, { [shape = shapeTensor] in $0.broadcast(toShape: shape) })
+    return (value, { [shape = shapeTensor] in 
+      var res = $0 
+      for i in axes.array.scalars { res = res.expandingShape(at: Int(i)) }
+      return res.broadcast(toShape: shape) 
+    })
   }
 
   @inlinable
@@ -596,7 +600,9 @@ extension Tensor where Scalar : TensorFlowFloatingPoint {
     let value = mean(squeezingAxes: axes)
     return (value, { [shape = shapeTensor,
                       count = axes.map { shape[$0] }.reduce(1, *)] in
-      $0.broadcast(toShape: shape) / Tensor(Scalar(count))
+      var res = $0
+      for i in axes { res = res.expandingShape(at: Int(i)) }              
+      return res.broadcast(toShape: shape) / Tensor(Scalar(count))
     })
   }
 
@@ -607,7 +613,9 @@ extension Tensor where Scalar : TensorFlowFloatingPoint {
     let value = mean(squeezingAxes: axes)
     let count = Raw.gather(params: shapeTensor, indices: axes).product()
     return (value, { [shape = shapeTensor] in
-      $0.broadcast(toShape: shape) / Tensor(count)
+      var res = $0
+      for i in axes.array.scalars { res = res.expandingShape(at: Int(i)) }              
+      return res.broadcast(toShape: shape) / Tensor(count)
     })
   }
 }
