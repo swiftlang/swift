28152154842c076af735ef4452550adb76087803
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2019-04-19 14:33:11 -0700

    fix wasi sdk path

--- vvv.sh
+++ vvv.sh
@@ -1,7 +1,7 @@
 utils/build-script --release-debuginfo --wasm \
 	--llvm-targets-to-build "X86;ARM;AArch64;PowerPC;SystemZ;WebAssembly" \
 	--llvm-max-parallel-lto-link-jobs 1 --swift-tools-max-parallel-lto-link-jobs 1 \
-	--wasm-wasi-sdk "/home/zhuowei/Downloads/wasi-sdk-3.0-linux/wasi-sdk-3.0/opt/wasi-sdk" \
+	--wasm-wasi-sdk "/home/zhuowei/wasi-sdk" \
 	--wasm-icu-uc "todo" \
 	--wasm-icu-uc-include "$PWD/NO" \
 	--wasm-icu-i18n "todo" \
