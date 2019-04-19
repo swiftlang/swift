199e5a765d53488f27d63dceac13b38c0da46521
Mike Ash <mikeash@apple.com>
2019-04-19 11:00:56 -0400

    [Tests] Add `-dump-input fail` to environment-mac.swift's FileCheck.

--- test/Driver/environment-mac.swift
+++ test/Driver/environment-mac.swift
@@ -1,5 +1,5 @@
 // REQUIRES: OS=macosx
 
-// RUN: %swift_driver -driver-use-frontend-path %S/Inputs/print-var.sh %s DYLD_LIBRARY_PATH | %FileCheck %s
+// RUN: %swift_driver -driver-use-frontend-path %S/Inputs/print-var.sh %s DYLD_LIBRARY_PATH | %FileCheck %s --dump-input fail
 
 // CHECK: {{^/usr/lib/swift:}}
