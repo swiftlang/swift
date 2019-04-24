5ce06a349f9d709bcc513e6e3c2bf74706e2d8d8
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2019-04-23 22:33:43 -0700

    attempt to fix swift_once; doesn't work

--- stdlib/public/runtime/Once.cpp
+++ stdlib/public/runtime/Once.cpp
@@ -52,7 +52,28 @@ void swift::swift_once(swift_once_t *predicate, void (*fn)(void *),
   dispatch_once_f(predicate, context, fn);
 #elif defined(__CYGWIN__)
   _swift_once_f(predicate, context, fn);
+#elif defined(__wasm__)
+  // WebAssembly: hack: Swift compiler passes in a fn that doesn't take a parameter,
+  // which is invalid in WebAssembly. So swift_once casts the function.
+  // The correct way to fix this is to change 
+  // SILGenModule::emitLazyGlobalInitializer
+  // but this is OK as a proof of concept.
+  // Keep a copy of the unmodified swift_once function below.
+  std::call_once(*predicate, [fn, context]() { ((void (*)())fn)(); });
 #else
   std::call_once(*predicate, [fn, context]() { fn(context); });
 #endif
 }
+
+#ifdef __wasm__
+void swift::swift_once_real(swift_once_t *predicate, void (*fn)(void *),
+                       void *context) {
+#if defined(__APPLE__)
+  dispatch_once_f(predicate, context, fn);
+#elif defined(__CYGWIN__)
+  _swift_once_f(predicate, context, fn);
+#else
+  std::call_once(*predicate, [fn, context]() { fn(context); });
+#endif
+}
+#endif
