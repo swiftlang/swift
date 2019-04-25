3e99d1142895d54e602a4f1b386d530719e2fe7e
Slava Pestov <spestov@apple.com>
2019-04-25 00:29:25 -0400

    SILGen: Stop forcing lazy conformances in SILGenSILBuilder

--- lib/SILGen/SILGenSILBuilder.cpp
+++ lib/SILGen/SILGenSILBuilder.cpp
@@ -47,128 +47,29 @@ SILGenSILBuilder::SILGenSILBuilder(SILGenFunction &SGF, SILBasicBlock *insertBB,
 // SILBuilder APIs directly.
 //
 
-MetatypeInst *SILGenSILBuilder::createMetatype(SILLocation loc,
-                                               SILType metatype) {
-  auto theMetatype = metatype.castTo<MetatypeType>();
-  // Getting a nontrivial metatype requires forcing any conformances necessary
-  // to instantiate the type.
-  switch (theMetatype->getRepresentation()) {
-  case MetatypeRepresentation::Thin:
-    break;
-  case MetatypeRepresentation::Thick:
-  case MetatypeRepresentation::ObjC: {
-    // Walk the type recursively to look for substitutions we may need.
-    theMetatype.getInstanceType().findIf([&](Type t) -> bool {
-      auto *decl = t->getAnyNominal();
-      if (!decl)
-        return false;
-
-      if (isa<ProtocolDecl>(decl))
-        return false;
-
-      auto *genericSig = decl->getGenericSignature();
-      if (!genericSig)
-        return false;
-
-      auto subMap =
-          t->getContextSubstitutionMap(getSILGenModule().SwiftModule, decl);
-      getSILGenModule().useConformancesFromSubstitutions(subMap);
-      return false;
-    });
-
-    break;
-  }
-  }
-
-  return SILBuilder::createMetatype(loc, metatype);
-}
-
 ApplyInst *SILGenSILBuilder::createApply(SILLocation loc, SILValue fn,
                                          SILType substFnTy, SILType result,
                                          SubstitutionMap subs,
                                          ArrayRef<SILValue> args) {
-  getSILGenModule().useConformancesFromSubstitutions(subs);
   return SILBuilder::createApply(loc, fn, subs, args, false);
 }
 
 TryApplyInst *SILGenSILBuilder::createTryApply(
     SILLocation loc, SILValue fn, SILType substFnTy, SubstitutionMap subs,
     ArrayRef<SILValue> args, SILBasicBlock *normalBB, SILBasicBlock *errorBB) {
-  getSILGenModule().useConformancesFromSubstitutions(subs);
   return SILBuilder::createTryApply(loc, fn, subs, args, normalBB, errorBB);
 }
 
 BeginApplyInst *SILGenSILBuilder::createBeginApply(SILLocation loc, SILValue fn,
                                                    SubstitutionMap subs,
                                                    ArrayRef<SILValue> args) {
-  getSILGenModule().useConformancesFromSubstitutions(subs);
   return SILBuilder::createBeginApply(loc, fn, subs, args, false);
 }
 
 PartialApplyInst *SILGenSILBuilder::createPartialApply(
     SILLocation loc, SILValue fn, SILType substFnTy, SubstitutionMap subs,
     ArrayRef<SILValue> args, SILType closureTy) {
-  getSILGenModule().useConformancesFromSubstitutions(subs);
   return SILBuilder::createPartialApply(
       loc, fn, subs, args,
       closureTy.getAs<SILFunctionType>()->getCalleeConvention());
-}
-
-BuiltinInst *SILGenSILBuilder::createBuiltin(SILLocation loc, Identifier name,
-                                             SILType resultTy,
-                                             SubstitutionMap subs,
-                                             ArrayRef<SILValue> args) {
-  getSILGenModule().useConformancesFromSubstitutions(subs);
-  return SILBuilder::createBuiltin(loc, name, resultTy, subs, args);
-}
-
-InitExistentialAddrInst *SILGenSILBuilder::createInitExistentialAddr(
-    SILLocation loc, SILValue existential, CanType formalConcreteType,
-    SILType loweredConcreteType,
-    ArrayRef<ProtocolConformanceRef> conformances) {
-  for (auto conformance : conformances)
-    getSILGenModule().useConformance(conformance);
-
-  return SILBuilder::createInitExistentialAddr(
-      loc, existential, formalConcreteType, loweredConcreteType, conformances);
-}
-
-InitExistentialValueInst *SILGenSILBuilder::createInitExistentialValue(
-    SILLocation Loc, SILType ExistentialType, CanType FormalConcreteType,
-    SILValue Concrete, ArrayRef<ProtocolConformanceRef> Conformances) {
-  for (auto conformance : Conformances)
-    getSILGenModule().useConformance(conformance);
-
-  return SILBuilder::createInitExistentialValue(
-      Loc, ExistentialType, FormalConcreteType, Concrete, Conformances);
-}
-
-InitExistentialMetatypeInst *SILGenSILBuilder::createInitExistentialMetatype(
-    SILLocation loc, SILValue metatype, SILType existentialType,
-    ArrayRef<ProtocolConformanceRef> conformances) {
-  for (auto conformance : conformances)
-    getSILGenModule().useConformance(conformance);
-
-  return SILBuilder::createInitExistentialMetatype(
-      loc, metatype, existentialType, conformances);
-}
-
-InitExistentialRefInst *SILGenSILBuilder::createInitExistentialRef(
-    SILLocation loc, SILType existentialType, CanType formalConcreteType,
-    SILValue concreteValue, ArrayRef<ProtocolConformanceRef> conformances) {
-  for (auto conformance : conformances)
-    getSILGenModule().useConformance(conformance);
-
-  return SILBuilder::createInitExistentialRef(
-      loc, existentialType, formalConcreteType, concreteValue, conformances);
-}
-
-AllocExistentialBoxInst *SILGenSILBuilder::createAllocExistentialBox(
-    SILLocation loc, SILType existentialType, CanType concreteType,
-    ArrayRef<ProtocolConformanceRef> conformances) {
-  for (auto conformance : conformances)
-    getSILGenModule().useConformance(conformance);
-
-  return SILBuilder::createAllocExistentialBox(loc, existentialType,
-                                               concreteType, conformances);
 }
--- lib/SILGen/SILGenSILBuilder.h
+++ lib/SILGen/SILGenSILBuilder.h
@@ -39,12 +39,6 @@ public:
 
   SILGenModule &getSILGenModule() const;
 
-  // Metatype instructions use the conformances necessary to instantiate the
-  // type.
-
-  MetatypeInst *createMetatype(SILLocation loc, SILType metatype);
-  // Generic apply instructions use the conformances necessary to form the call.
-
   using SILBuilder::createApply;
 
   ApplyInst *createApply(SILLocation loc, SILValue fn, SILType SubstFnTy,
@@ -63,37 +57,6 @@ public:
                                        SILType substFnTy, SubstitutionMap subs,
                                        ArrayRef<SILValue> args,
                                        SILType closureTy);
-  BuiltinInst *createBuiltin(SILLocation loc, Identifier name, SILType resultTy,
-                             SubstitutionMap subs, ArrayRef<SILValue> args);
-
-  // Existential containers use the conformances needed by the existential
-  // box.
-
-  InitExistentialAddrInst *
-  createInitExistentialAddr(SILLocation loc, SILValue existential,
-                            CanType formalConcreteType,
-                            SILType loweredConcreteType,
-                            ArrayRef<ProtocolConformanceRef> conformances);
-
-  InitExistentialValueInst *
-  createInitExistentialValue(SILLocation loc, SILType existentialType,
-                             CanType formalConcreteType, SILValue concrete,
-                             ArrayRef<ProtocolConformanceRef> conformances);
-
-  InitExistentialMetatypeInst *
-  createInitExistentialMetatype(SILLocation loc, SILValue metatype,
-                                SILType existentialType,
-                                ArrayRef<ProtocolConformanceRef> conformances);
-
-  InitExistentialRefInst *
-  createInitExistentialRef(SILLocation loc, SILType existentialType,
-                           CanType formalConcreteType, SILValue concreteValue,
-                           ArrayRef<ProtocolConformanceRef> conformances);
-
-  AllocExistentialBoxInst *
-  createAllocExistentialBox(SILLocation loc, SILType existentialType,
-                            CanType concreteType,
-                            ArrayRef<ProtocolConformanceRef> conformances);
 };
 
 } // namespace Lowering
