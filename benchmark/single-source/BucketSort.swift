abb3143aa0f483507a4de4fcd64dfa4cad9a9475
Pavol Vaskovic <pali@pali.sk>
2019-04-29 21:59:30 +0200

    [benchmark] BucketSort: tag .existential

--- benchmark/single-source/BucketSort.swift
+++ benchmark/single-source/BucketSort.swift
@@ -31,7 +31,7 @@ import Foundation
 public let BucketSort = BenchmarkInfo(
   name: "BucketSort",
   runFunction: run_BucketSort,
-  tags: [.validation, .algorithm],
+  tags: [.existential, .algorithm],
   setUpFunction: { blackHole(buckets) }
 )
 
