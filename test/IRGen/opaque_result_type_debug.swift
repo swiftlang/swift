2edc957f8aaa66f2af23c7231aebdd17cfbffd02
Joe Groff <jgroff@apple.com>
2019-04-30 13:44:59 -0700

    IRGen: Render opaque types like other archetypes in debug info.

--- test/IRGen/opaque_result_type_debug.swift
+++ test/IRGen/opaque_result_type_debug.swift
@@ -1,6 +1,6 @@
-// RUN: %target-swift-frontend -emit-ir -enable-anonymous-context-mangled-names %s | %FileCheck %s
+// RUN: %target-swift-frontend -g -emit-ir -enable-anonymous-context-mangled-names %s | %FileCheck %s
 
-protocol P {}
+public protocol P {}
 extension Int: P {} 
 
 // CHECK: [[DEFINER_NAME:@.*]] = {{.*}}constant [{{[0-9]+}} x i8] c"$s24opaque_result_type_debug3fooQryF\00"
@@ -9,6 +9,21 @@ extension Int: P {}
 
 // CHECK: @"\01l_type_metadata_table" = {{.*}} @"$s24opaque_result_type_debug3fooQryFQOMQ"
 
-func foo() -> some P {
+public func foo() -> some P {
   return 0
 }
+
+@_silgen_name("use") public func use<T: P>(_: T)
+
+public func bar<T: P>(genericValue: T) {
+  use(genericValue)
+
+  let intValue = 0
+  use(intValue)
+
+  let opaqueValue = foo()
+  use(opaqueValue)
+}
+
+// CHECK: [[OPAQUE_TYPE:![0-9]+]] = !DICompositeType({{.*}} name: "$s24opaque_result_type_debug3fooQryFQOyQo_D"
+// CHECK: {{![0-9]+}} = !DILocalVariable(name: "opaqueValue",{{.*}} type: [[OPAQUE_TYPE]])
