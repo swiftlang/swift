3b6ec6c651429b1b56a9de0562f7cf9641463a9f
Slava Pestov <spestov@apple.com>
2019-05-01 13:32:41 -0400

    IRGen: Move some deployment target checks to LangOptions

--- include/swift/Basic/LangOptions.h
+++ include/swift/Basic/LangOptions.h
@@ -385,6 +385,15 @@ namespace swift {
       return EffectiveLanguageVersion.isVersionAtLeast(major, minor);
     }
 
+    // The following deployment targets ship an Objective-C runtime supporting
+    // the class metadata update callback mechanism:
+    //
+    // - macOS 10.14.4
+    // - iOS 12.2
+    // - tvOS 12.2
+    // - watchOS 5.2
+    bool doesTargetSupportObjCMetadataUpdateCallback() const;
+
     /// Returns true if the given platform condition argument represents
     /// a supported target operating system.
     ///
