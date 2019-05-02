34063e06c2c22f61eea0f9caa45e6f8054598152
Arnold Schwaighofer <aschwaighofer@apple.com>
2019-05-02 05:21:00 -0700

    Add copyright notice

--- lib/SILOptimizer/Transforms/SpecializeOpaqueArchetypes.cpp
+++ lib/SILOptimizer/Transforms/SpecializeOpaqueArchetypes.cpp
@@ -1,3 +1,19 @@
+//===--- SpecializeOpaqueArchetypes.cpp -  Specialize opaque archetypes ---===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2019 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+//
+// A pass to specialize opaque archetypes
+//
+//===----------------------------------------------------------------------===//
+
 #define DEBUG_TYPE "opaque-archetype-specializer"
 
 #include "swift/AST/Types.h"
