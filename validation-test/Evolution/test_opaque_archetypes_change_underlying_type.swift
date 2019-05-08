670ce52b49593d01078e2f1c5732644b797a7440
Arnold Schwaighofer <aschwaighofer@apple.com>
2019-05-08 08:49:58 -0700

    [5.1] Opaque types require the next Swift runtime
    rdar://50503909

--- validation-test/Evolution/test_opaque_archetypes_change_underlying_type.swift
+++ validation-test/Evolution/test_opaque_archetypes_change_underlying_type.swift
@@ -10,13 +10,15 @@ import StdlibUnittest
 var OpaqueArchetypes = TestSuite("OpaqueArchetypes")
 
 OpaqueArchetypes.test("test1") {
-  let o = resilientFunction()
-  expectEqual(o.getValue(), expectedResult())
-  expectEqual(MemoryLayout.size(ofValue: o), expectedSize())
+  if #available(iOS 9999, macOS 9999, tvOS 9999, watchOS 9999, *) {
+    let o = resilientFunction()
+    expectEqual(o.getValue(), expectedResult())
+    expectEqual(MemoryLayout.size(ofValue: o), expectedSize())
 
-  let c = Container()
-  expectEqual(c.property.getValue(), c.expectedResult())
-  expectEqual(MemoryLayout.size(ofValue: c.property), c.expectedSize())
+    let c = Container()
+    expectEqual(c.property.getValue(), c.expectedResult())
+    expectEqual(MemoryLayout.size(ofValue: c.property), c.expectedSize())
+  }
 }
 
 runAllTests()
