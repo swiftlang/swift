0e1840f101faeb82f02a319d72a54369679fe7fe
David Smith <david_smith@apple.com>
2019-05-09 14:11:41 -0700

    Revert "SR-10294: convertBoolToDarwinBool and friends should be inlined"
    This reverts commit b055bfda170b1fea00611b416e440c22ddfb6870.

--- stdlib/public/Darwin/ObjectiveC/ObjectiveC.swift
+++ stdlib/public/Darwin/ObjectiveC/ObjectiveC.swift
@@ -27,30 +27,26 @@ import _SwiftObjectiveCOverlayShims
 public struct ObjCBool : ExpressibleByBooleanLiteral {
 #if os(macOS) || (os(iOS) && (arch(i386) || arch(arm)))
   // On OS X and 32-bit iOS, Objective-C's BOOL type is a "signed char".
-  @usableFromInline var _value: Int8
+  var _value: Int8
 
-  @_transparent
   init(_ value: Int8) {
     self._value = value
   }
 
-  @_transparent
   public init(_ value: Bool) {
     self._value = value ? 1 : 0
   }
 
 #else
   // Everywhere else it is C/C++'s "Bool"
-  @usableFromInline var _value: Bool
+  var _value: Bool
 
-  @_transparent
   public init(_ value: Bool) {
     self._value = value
   }
 #endif
 
   /// The value of `self`, expressed as a `Bool`.
-  @_transparent
   public var boolValue: Bool {
 #if os(macOS) || (os(iOS) && (arch(i386) || arch(arm)))
     return _value != 0
@@ -82,13 +78,11 @@ extension ObjCBool : CustomStringConvertible {
 
 // Functions used to implicitly bridge ObjCBool types to Swift's Bool type.
 
-@_transparent
 public // COMPILER_INTRINSIC
 func _convertBoolToObjCBool(_ x: Bool) -> ObjCBool {
   return ObjCBool(x)
 }
 
-@_transparent
 public // COMPILER_INTRINSIC
 func _convertObjCBoolToBool(_ x: ObjCBool) -> Bool {
   return x.boolValue
