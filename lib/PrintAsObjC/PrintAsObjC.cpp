f340906a83885380b3c9664b2e73262e54308808
Slava Pestov <spestov@apple.com>
2019-05-08 22:00:29 -0400

    Fix REQUIRES lines on class stubs tests
    We use the just-built clang without any -target or -sdk flags. Keep things
    simple and limit the test to macOS for now.
    
    Fixes <rdar://problem/50586614>.

--- lib/PrintAsObjC/PrintAsObjC.cpp
+++ lib/PrintAsObjC/PrintAsObjC.cpp
@@ -322,6 +322,13 @@ private:
   void visitClassDecl(ClassDecl *CD) {
     printDocumentationComment(CD);
 
+    // This is just for testing, so we check explicitly for the attribute instead
+    // of asking if the class is weak imported. If the class has availablility,
+    // we'll print a SWIFT_AVAIALBLE() which implies __attribute__((weak_imported))
+    // already.
+    if (CD->getAttrs().hasAttribute<WeakLinkedAttr>())
+      os << "SWIFT_WEAK_IMPORT\n";
+
     bool hasResilientAncestry =
       CD->checkAncestry().contains(AncestryFlags::ResilientOther);
     if (hasResilientAncestry) {
@@ -2716,6 +2723,9 @@ public:
            "#endif\n"
            "#if !defined(SWIFT_AVAILABILITY)\n"
            "# define SWIFT_AVAILABILITY(plat, ...) __attribute__((availability(plat, __VA_ARGS__)))\n"
+           "#endif\n"
+           "#if !defined(SWIFT_WEAK_IMPORT)\n"
+           "# define SWIFT_WEAK_IMPORT __attribute__((weak_import))\n"
            "#endif\n"
            "#if !defined(SWIFT_DEPRECATED)\n"
            "# define SWIFT_DEPRECATED __attribute__((deprecated))\n"
