107297b2025b350be7def5ed6c5fc60f79c84e9a
Mike Ash <mikeash@apple.com>
2019-05-16 15:20:24 -0400

    Revert "[Tests] Add `-dump-input fail` to environment-mac.swift's FileCheck."
    This reverts commit 199e5a765d53488f27d63dceac13b38c0da46521.

--- test/Driver/environment-mac.swift
+++ test/Driver/environment-mac.swift
@@ -1,5 +1,5 @@
 // REQUIRES: OS=macosx
 
-// RUN: %swift_driver -driver-use-frontend-path %S/Inputs/print-var.sh %s DYLD_LIBRARY_PATH | %FileCheck %s --dump-input fail
+// RUN: %swift_driver -driver-use-frontend-path %S/Inputs/print-var.sh %s DYLD_LIBRARY_PATH | %FileCheck %s
 
 // CHECK: {{^/usr/lib/swift:}}
