f4f4c70f4306b28b1fbc551b2ddd44869e6bc60a
Mike Ash <mikeash@apple.com>
2019-05-16 15:20:23 -0400

    Revert "[Tests] Make environment-mac.swift pass if DYLD_LIBRARY_PATH is not set."
    This reverts commit 92fa5efafdf0525e266ecebbf758e5e665c9d35b.

--- test/Driver/environment-mac.swift
+++ test/Driver/environment-mac.swift
@@ -2,7 +2,4 @@
 
 // RUN: %swift_driver -driver-use-frontend-path %S/Inputs/print-var.sh %s DYLD_LIBRARY_PATH | %FileCheck %s --dump-input fail
 
-// Pass if the variable is not set at all. Something causes the
-// variable to get lost in PR testing. Accept that for now. This is
-// a temporary solution and so this will go away soon in any case.
-// CHECK: {{(^/usr/lib/swift:)|(^NO_VALUE$)}}
+// CHECK: {{^/usr/lib/swift:}}
