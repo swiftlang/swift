d37fd2f85dae7f375ad2b84af052038689834bcc
David Ungar <dungar@apple.com>
2019-05-20 11:33:53 -0700

    Revised test.

--- test/Parse/interpolated_trailing_quote.swift
+++
@@ -1,6 +0,0 @@
-// Check the trailingQuoteLoc field of InterpolatedStringLiteral
-
-"\("abc")"
-
-// RUN: %target-swift-frontend -dump-ast %s | %FileCheck %s
-// CHECK: (interpolated_string_literal_expr {{.*}} trailing_quote_loc={{.*}}:3:10 {{.*}}
---
+++ test/Parse/source_locs.swift
@@ -0,0 +1,8 @@
+// Check source locations (only InterpolatedStringLiteral for now).
+
+func string_interpolation() {
+  "\("abc")"
+}
+
+// RUN: %target-swift-frontend -dump-ast %s | %FileCheck %s
+// CHECK: (interpolated_string_literal_expr {{.*}} trailing_quote_loc=SOURCE_DIR/test/Parse/source_locs.swift:4:12 {{.*}}
