502fcee378455cb761c5890a12de76147925ee03
Daniil Tiganov <tiganov@ualberta.ca>
2019-05-21 16:58:51 -0600

    [Tests] Adding full unit test for each FrontendObserver method

--- unittests/FrontendTool/FrontendObserverTests.cpp
+++ unittests/FrontendTool/FrontendObserverTests.cpp
@@ -10,16 +10,15 @@
 //
 //===----------------------------------------------------------------------===//
 ///
-/// This file exists to make sure methods of FrontendObserver are not removed
+/// This test exists to make sure methods of FrontendObserver are not removed
 /// because external clients may be using them.
-///
-/// Successful compilation -> successful test.
 ///
 //===----------------------------------------------------------------------===//
 
 #include "gtest/gtest.h"
 #include "swift/SIL/SILModule.h"
 #include "swift/Frontend/Frontend.h"
+#include "swift/AST/Module.h"
 #include "swift/FrontendTool/FrontendTool.h"
 
 using namespace swift;
@@ -30,20 +29,72 @@ class TestObserver : public testing::Test, public FrontendObserver {
 
 public:
 
-  void parsedArgs(CompilerInvocation &invocation) override { }
+  // convenient fields are grabbed from the given object at each method
+  // to assert that the passed object is valid and usable
 
-  void configuredCompiler(CompilerInstance &instance) override { }
+  // fields to assert
+  std::string parsedArgsTest = "";
+  std::string configuredCompilerTest = "";
+  std::string performedSemanticAnalysisTest = "";
+  bool performedSILGenerationTest = true;
+  bool performedSILProcessingTest = true;
 
-  void performedSemanticAnalysis(CompilerInstance &instance) override { }
+  void parsedArgs(CompilerInvocation &invocation) override {
+    parsedArgsTest = invocation.getModuleName();
+  }
 
-  void performedSILGeneration(SILModule &module) override { }
+  void configuredCompiler(CompilerInstance &instance) override {
+    configuredCompilerTest = instance.getMainModule()->getName().str();
+  }
 
-  void performedSILProcessing(SILModule &module) override { }
+  void performedSemanticAnalysis(CompilerInstance &instance) override {
+    performedSemanticAnalysisTest = instance.getMainModule()->getName().str();
+  }
+
+  void performedSILGeneration(SILModule &module) override {
+    performedSILGenerationTest = module.isSerialized();
+  }
+
+  void performedSILProcessing(SILModule &module) override {
+    performedSILProcessingTest = module.hasFunction("random");
+  }
 
 };
 
 TEST_F(TestObserver, MethodsExistTest) {
-  // If this file compiles, then the "test" is successful.
+  std::string testName = "testName";
+
+  // set up CompilerInvocation
+  CompilerInvocation TestCompilerInovcation;
+  TestCompilerInovcation.setModuleName(testName);
+
+  // set up CompilerInstance
+  CompilerInstance TestCompilerInstance;
+  if (TestCompilerInstance.setup(TestCompilerInovcation))
+    FAIL() << "Failed to setup CompilerInstance.";
+  TestCompilerInstance.performSema();
+  TestCompilerInstance.setSILModule(SILModule::createEmptyModule(
+      TestCompilerInstance.getMainModule(),
+      TestCompilerInstance.getSILOptions()));
+
+  // perform calls, then assert that appropriate TestObserver property has been
+  // set from given object, for each method
+
+  parsedArgs(TestCompilerInovcation);
+  ASSERT_EQ(parsedArgsTest, testName);
+
+  configuredCompiler(TestCompilerInstance);
+  ASSERT_EQ(configuredCompilerTest, testName);
+
+  performedSemanticAnalysis(TestCompilerInstance);
+  ASSERT_EQ(performedSemanticAnalysisTest, testName);
+
+  performedSILGeneration(*TestCompilerInstance.getSILModule());
+  ASSERT_EQ(performedSILGenerationTest, false);
+
+  performedSILProcessing(*TestCompilerInstance.getSILModule());
+  ASSERT_EQ(performedSILProcessingTest, false);
+
 }
 
 } // end anonymous namespace
