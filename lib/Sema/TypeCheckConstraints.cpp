2ef18488d65486ddaec924baeac97bb10e9f2dfb
Luciano Almeida <passos.luciano@outlook.com>
2019-05-27 22:57:56 -0300

    Walking through the expr to set the types and type variables in the constraint system before add convertion constraint

--- lib/Sema/TypeCheckConstraints.cpp
+++ lib/Sema/TypeCheckConstraints.cpp
@@ -3323,6 +3323,10 @@ bool TypeChecker::convertToType(Expr *&expr, Type type, DeclContext *dc,
   // TODO: need to add kind arg?
   // Construct a constraint system from this expression.
   ConstraintSystem cs(*this, dc, ConstraintSystemFlags::AllowFixes);
+  
+  // We need to walk through the expression to set the type variables.
+  cs.generateConstraints(expr);
+
   // If there is a type that we're expected to convert to, add the conversion
   // constraint.
   cs.addConstraint(ConstraintKind::Conversion, expr->getType(), type,
