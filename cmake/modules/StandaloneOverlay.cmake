be903d2d4763fef7e1ebfc08464d27e0ed3a1c9d
Ross Bayer <rbayer@apple.com>
2019-05-29 13:19:32 -0700

    [Build System: CMake] Add new SWIFT_DARWIN_MODULE_ARCHS CMake cache variable which allows users to specify architectures they wish to configure and build _just_ the .swiftmodule content, even if they are not 'supported' via SWIFT_DARWIN_SUPPORTED_ARCHS.
    (cherry picked from commit 4ec60332f9aa824c8ec30333260170fdc1ee84a6)

--- cmake/modules/StandaloneOverlay.cmake
+++ cmake/modules/StandaloneOverlay.cmake
@@ -73,7 +73,13 @@ set(SWIFT_DARWIN_SUPPORTED_ARCHS "" CACHE STRING
   "Space-separated list of architectures to configure on Darwin platforms. \
 If left empty all default architectures are configured.")
 
+set(SWIFT_DARWIN_MODULE_ARCHS "" CACHE STRING
+  "Space-separated list of architectures to configure Swift module-only \
+targets on Darwin platforms. These targets are in addition to the full \
+library targets.")
+
 separate_arguments(SWIFT_DARWIN_SUPPORTED_ARCHS)
+separate_arguments(SWIFT_DARWIN_MODULE_ARCHS)
 
 
 # -----------------------------------------------------------------------------
