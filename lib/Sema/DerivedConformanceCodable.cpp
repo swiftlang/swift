b9b6055cda840c3e1c1dfaa08a3a9c21e337f106
Doug Gregor <dgregor@apple.com>
2019-05-29 15:47:03 -0700

    [SE-0258] Adjust name for CodingKeys property to drop the '$'.
    When synthesizing Codable conformances, the enum case name for a
    property with an attached delegate does not have the '$', i.e., it's
    the name of the originally-declared property.

--- lib/Sema/DerivedConformanceCodable.cpp
+++ lib/Sema/DerivedConformanceCodable.cpp
@@ -142,6 +142,14 @@ static CodableConformanceType varConformsToCodable(TypeChecker &tc,
   return typeConformsToCodable(tc, context, varDecl->getType(), isIUO, proto);
 }
 
+/// Retrieve the variable name for the purposes of encoding/decoding.
+static Identifier getVarNameForCoding(VarDecl *var) {
+  if (auto originalVar = var->getOriginalDelegatedProperty())
+    return originalVar->getName();
+
+  return var->getName();
+}
+
 /// Validates the given CodingKeys enum decl by ensuring its cases are a 1-to-1
 /// match with the stored vars of the given type.
 ///
@@ -168,7 +176,7 @@ static bool validateCodingKeysEnum(DerivedConformance &derived,
     if (varDecl->getAttrs().hasAttribute<LazyAttr>())
       continue;
 
-    properties[varDecl->getName()] = varDecl;
+    properties[getVarNameForCoding(varDecl)] = varDecl;
   }
 
   bool propertiesAreValid = true;
@@ -371,7 +379,8 @@ static EnumDecl *synthesizeCodingKeysEnum(DerivedConformance &derived) {
     switch (conformance) {
       case Conforms:
       {
-        auto *elt = new (C) EnumElementDecl(SourceLoc(), varDecl->getName(),
+        auto *elt = new (C) EnumElementDecl(SourceLoc(),
+                                            getVarNameForCoding(varDecl),
                                             nullptr, SourceLoc(), nullptr,
                                             enumDecl);
         elt->setImplicit();
@@ -526,6 +535,11 @@ lookupVarDeclForCodingKeysCase(DeclContext *conformanceDC,
 
   for (auto decl : targetDecl->lookupDirect(DeclName(elt->getName()))) {
     if (auto *vd = dyn_cast<VarDecl>(decl)) {
+      // If we found a property with an attached delegate, retrieve the
+      // backing property.
+      if (auto backingVar = vd->getPropertyDelegateBackingProperty())
+        vd = backingVar;
+
       if (!vd->isStatic()) {
         // This is the VarDecl we're looking for.
 
