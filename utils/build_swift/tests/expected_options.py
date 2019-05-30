43f002cdfb58f3076b67ca08ef19b10b997c2e7b
Anthony Platanios <e.a.platanios@gmail.com>
2019-05-30 12:03:49 -0400

    Fixed failing tests.

--- utils/build_swift/tests/expected_options.py
+++ utils/build_swift/tests/expected_options.py
@@ -213,7 +213,6 @@ EXPECTED_DEFAULTS = {
     'tensorflow_host_include_dir': None,
     'tensorflow_target_lib_dir': None,
     'tensorflow_target_include_dir': None,
-    'tensorflow_swift_bindings': None,
     'tensorflow_swift_apis': None,
     'host_bazel': None,
     'tensorflow_bazel_options': [],
