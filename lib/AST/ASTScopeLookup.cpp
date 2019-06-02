013a45a9a5b184e37f7f862d3eca23dc176fe304
David Ungar <dungar@apple.com>
2019-06-01 21:35:23 -0700

    Get fileScope sooner for debugging convenience

--- lib/AST/ASTScopeLookup.cpp
+++ lib/AST/ASTScopeLookup.cpp
@@ -57,9 +57,9 @@ const ASTScopeImpl *ASTScopeImpl::findStartingScopeForLookup(
   if (startingContext->getContextKind() == DeclContextKind::Module)
     return nullptr;
 
+  auto *const fileScope = sourceFile->getScope()->impl;
   // Parser may have added decls to source file, since previous lookup
   sourceFile->getScope()->addAnyNewScopesToTree();
-  auto *const fileScope = sourceFile->getScope()->impl;
   if (name.isOperator())
     return fileScope; // operators always at file scope
 
