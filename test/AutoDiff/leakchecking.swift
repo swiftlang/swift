902357711c2b332a7b0f996396a22d70b436a423
Dan Zheng <danielzheng@google.com>
2019-06-04 16:18:57 -0700

    [AutoDiff] Add control flow AD leak checking tests.

--- test/AutoDiff/leakchecking.swift
+++ test/AutoDiff/leakchecking.swift
@@ -1,7 +1,7 @@
 // RUN: %target-run-simple-swift-control-flow-differentiation
 // REQUIRES: executable_test
 
-// A test that we can properly differentiate types that require refcounting.
+// Test differentiation-related memory leaks.
 
 import StdlibUnittest
 import DifferentiationUnittest
@@ -9,9 +9,14 @@ import DifferentiationUnittest
 var LeakCheckingTests = TestSuite("LeakChecking")
 
 /// Execute body, check expected leak count, and reset global leak count.
-func testWithLeakChecking(expectedLeakCount: Int = 0, _ body: () -> Void) {
+func testWithLeakChecking(
+  expectedLeakCount: Int = 0, file: String = #file, line: UInt = #line,
+  _ body: () -> Void
+) {
   body()
-  expectEqual(expectedLeakCount, _GlobalLeakCount.count, "Leak detected.")
+  expectEqual(
+    expectedLeakCount, _GlobalLeakCount.count, "Leak detected.",
+    file: file, line: line)
   _GlobalLeakCount.count = 0
 }
 
@@ -23,19 +28,82 @@ struct ExampleLeakModel : Differentiable {
   }
 }
 
+struct FloatPair : Differentiable & AdditiveArithmetic {
+  var first, second: Tracked<Float>
+  init(_ first: Tracked<Float>, _ second: Tracked<Float>) {
+    self.first = first
+    self.second = second
+  }
+}
+
+struct Pair<T : Differentiable, U : Differentiable> : Differentiable
+  where T == T.AllDifferentiableVariables, T == T.TangentVector,
+        U == U.AllDifferentiableVariables, U == U.TangentVector
+{
+  var first: Tracked<T>
+  var second: Tracked<U>
+  init(_ first: Tracked<T>, _ second: Tracked<U>) {
+    self.first = first
+    self.second = second
+  }
+}
+
 LeakCheckingTests.test("BasicVarLeakChecking") {
-  do {
+  testWithLeakChecking(expectedLeakCount: 0) {
     var model = ExampleLeakModel()
     let x: Tracked<Float> = 1.0
-    let _ = model.gradient(at: x) { m, x in m.applied(to: x) }
+    _ = model.gradient(at: x) { m, x in m.applied(to: x) }
   }
-  expectEqual(0, _GlobalLeakCount.count, "Leak detected.")
 }
 
 LeakCheckingTests.test("ControlFlow") {
-  // TODO: Add more `var` + control flow tests.
-  // Porting tests from test/AutoDiff/control_flow.swift requires more support
-  // for `Tracked<Float>`.
+  // FIXME: Fix control flow AD memory leaks.
+  // See related FIXME comments in adjoint value/buffer propagation in
+  // lib/SILOptimizer/Mandatory/Differentiation.cpp.
+  testWithLeakChecking(expectedLeakCount: 105) {
+    func cond_nestedtuple_var(_ x: Tracked<Float>) -> Tracked<Float> {
+      // Convoluted function returning `x + x`.
+      var y = (x + x, x - x)
+      var z = (y, x)
+      if x > 0 {
+        var w = (x, x)
+        y.0 = w.1
+        y.1 = w.0
+        z.0.0 = z.0.0 - y.0
+        z.0.1 = z.0.1 + y.0
+      } else {
+        z = ((y.0 - x, y.1 + x), x)
+      }
+      return y.0 + y.1 - z.0.0 + z.0.1
+    }
+    expectEqual((8, 2), Tracked<Float>(4).valueWithGradient(in: cond_nestedtuple_var))
+    expectEqual((-20, 2), Tracked<Float>(-10).valueWithGradient(in: cond_nestedtuple_var))
+    expectEqual((-2674, 2), Tracked<Float>(-1337).valueWithGradient(in: cond_nestedtuple_var))
+  }
+
+  // FIXME: Fix control flow AD memory leaks.
+  // See related FIXME comments in adjoint value/buffer propagation in
+  // lib/SILOptimizer/Mandatory/Differentiation.cpp.
+  testWithLeakChecking(expectedLeakCount: 379) {
+    func cond_nestedstruct_var(_ x: Tracked<Float>) -> Tracked<Float> {
+      // Convoluted function returning `x + x`.
+      var y = FloatPair(x + x, x - x)
+      var z = Pair(Tracked(y), x)
+      if x > 0 {
+        var w = FloatPair(x, x)
+        y.first = w.second
+        y.second = w.first
+        z.first = Tracked(FloatPair(z.first.value.first - y.first,
+                                    z.first.value.second + y.first))
+      } else {
+        z = Pair(Tracked(FloatPair(y.first - x, y.second + x)), x)
+      }
+      return y.first + y.second - z.first.value.first + z.first.value.second
+    }
+    expectEqual((8, 2), Tracked<Float>(4).valueWithGradient(in: cond_nestedstruct_var))
+    expectEqual((-20, 2), Tracked<Float>(-10).valueWithGradient(in: cond_nestedstruct_var))
+    expectEqual((-2674, 2), Tracked<Float>(-1337).valueWithGradient(in: cond_nestedstruct_var))
+  }
 
   // FIXME: Fix control flow AD memory leaks.
   // See related FIXME comments in adjoint value/buffer propagation in
