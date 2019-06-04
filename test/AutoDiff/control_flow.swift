9af6170f2cdd94813e272298d8845cdbdd7e7e47
Dan Zheng <danielzheng@google.com>
2019-06-04 11:06:12 -0700

    Fix tests.
    - Make sure `gradient` calls test the right function.
    - Add struct `let` and `var` tests.
    
    Note: tuple/struct tests are designed to compute `x + x`
    (in convoluted ways) for easy verification.
    
    Corresponding tuple/struct tests have essentially the same body
    (e.g. `cond_nestedtuple_var` ~= `cond_nestedstruct_var`).

--- test/AutoDiff/control_flow.swift
+++ test/AutoDiff/control_flow.swift
@@ -57,19 +57,25 @@ ControlFlowTests.test("Conditionals") {
   expectEqual((-1, 1), gradient(at: -3, -2, in: cond3))
 
   func cond_tuple(_ x: Float) -> Float {
+    // Convoluted function returning `x + x`.
     let y: (Float, Float) = (x, x)
-    return y.0 + y.1
+    if x > 0 {
+      return y.0 + y.1
+    }
+    return y.0 + y.0 - y.1 + y.0
   }
-  expectEqual(8, gradient(at: 4, in: cond2))
-  expectEqual(2, gradient(at: -10, in: cond2))
-  expectEqual(0, gradient(at: -1337, in: cond2))
+  expectEqual((8, 2), valueWithGradient(at: 4, in: cond_tuple))
+  expectEqual((-20, 2), valueWithGradient(at: -10, in: cond_tuple))
+  expectEqual((-2674, 2), valueWithGradient(at: -1337, in: cond_tuple))
 
   func cond_tuple_var(_ x: Float) -> Float {
+    // Convoluted function returning `x + x`.
     var y: (Float, Float) = (x, x)
     var z: (Float, Float) = (x + x, x - x)
     if x > 0 {
-      y.0 = x
-      y.1 = x
+      var w = (x, x)
+      y.0 = w.1
+      y.1 = w.0
       z.0 = z.0 - y.0
       z.1 = z.1 + y.0
     } else {
@@ -77,9 +83,95 @@ ControlFlowTests.test("Conditionals") {
     }
     return y.0 + y.1 - z.0 + z.1
   }
-  expectEqual(8, gradient(at: 4, in: cond2_var))
-  expectEqual(2, gradient(at: -10, in: cond2_var))
-  expectEqual(0, gradient(at: -1337, in: cond2_var))
+  expectEqual((8, 2), valueWithGradient(at: 4, in: cond_tuple_var))
+  expectEqual((-20, 2), valueWithGradient(at: -10, in: cond_tuple_var))
+  expectEqual((-2674, 2), valueWithGradient(at: -1337, in: cond_tuple_var))
+
+  func cond_nestedtuple_var(_ x: Float) -> Float {
+    // Convoluted function returning `x + x`.
+    var y: (Float, Float) = (x + x, x - x)
+    var z: ((Float, Float), Float) = (y, x)
+    if x > 0 {
+      var w = (x, x)
+      y.0 = w.1
+      y.1 = w.0
+      z.0.0 = z.0.0 - y.0
+      z.0.1 = z.0.1 + y.0
+    } else {
+      z = ((y.0 - x, y.1 + x), x)
+    }
+    return y.0 + y.1 - z.0.0 + z.0.1
+  }
+  expectEqual((8, 2), valueWithGradient(at: 4, in: cond_nestedtuple_var))
+  expectEqual((-20, 2), valueWithGradient(at: -10, in: cond_nestedtuple_var))
+  expectEqual((-2674, 2), valueWithGradient(at: -1337, in: cond_nestedtuple_var))
+
+  struct FloatPair : Differentiable {
+    var first, second: Float
+    init(_ first: Float, _ second: Float) {
+      self.first = first
+      self.second = second
+    }
+  }
+
+  struct Pair<T : Differentiable, U : Differentiable> : Differentiable {
+    var first: T
+    var second: U
+    init(_ first: T, _ second: U) {
+      self.first = first
+      self.second = second
+    }
+  }
+
+  func cond_struct(_ x: Float) -> Float {
+    // Convoluted function returning `x + x`.
+    let y = FloatPair(x, x)
+    if x > 0 {
+      return y.first + y.second
+    }
+    return y.first + y.first - y.second + y.first
+  }
+  expectEqual((8, 2), valueWithGradient(at: 4, in: cond_struct))
+  expectEqual((-20, 2), valueWithGradient(at: -10, in: cond_struct))
+  expectEqual((-2674, 2), valueWithGradient(at: -1337, in: cond_struct))
+
+  func cond_struct_var(_ x: Float) -> Float {
+    // Convoluted function returning `x + x`.
+    var y = FloatPair(x, x)
+    var z = FloatPair(x + x, x - x)
+    if x > 0 {
+      var w = y
+      y.first = w.second
+      y.second = w.first
+      z.first = z.first - y.first
+      z.second = z.second + y.first
+    } else {
+      z = FloatPair(x, x)
+    }
+    return y.first + y.second - z.first + z.second
+  }
+  expectEqual((8, 2), valueWithGradient(at: 4, in: cond_struct_var))
+  expectEqual((-20, 2), valueWithGradient(at: -10, in: cond_struct_var))
+  expectEqual((-2674, 2), valueWithGradient(at: -1337, in: cond_struct_var))
+
+  func cond_nestedstruct_var(_ x: Float) -> Float {
+    // Convoluted function returning `x + x`.
+    var y = FloatPair(x + x, x - x)
+    var z = Pair(y, x)
+    if x > 0 {
+      var w = FloatPair(x, x)
+      y.first = w.second
+      y.second = w.first
+      z.first.first = z.first.first - y.first
+      z.first.second = z.first.second + y.first
+    } else {
+      z = Pair(FloatPair(y.first - x, y.second + x), x)
+    }
+    return y.first + y.second - z.first.first + z.first.second
+  }
+  expectEqual((8, 2), valueWithGradient(at: 4, in: cond_nestedstruct_var))
+  expectEqual((-20, 2), valueWithGradient(at: -10, in: cond_nestedstruct_var))
+  expectEqual((-2674, 2), valueWithGradient(at: -1337, in: cond_nestedstruct_var))
 
   func guard1(_ x: Float, _ y: Float) -> Float {
     guard x > 0 else {
