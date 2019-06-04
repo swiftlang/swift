045e192e26a2e8d44718d0da06ac89582db348bc
Dan Zheng <danielzheng@google.com>
2019-06-04 14:55:12 -0700

    [AutoDiff] Support differentiation of conditionals. (#25057)
    - Support control flow in adjoint generation.
      - Make adjoint value/buffer mappings be per basic block.
      - Change `AdjointValue` to not be move-only. Original values from
        different basic blocks may share the same `AdjointValue`.
      - Propagate adjoint values from active bb arguments to predecessor
        terminator operands.
      - Propagate adjoint values/buffers of dominated active values/buffers
        to predecessor blocks.
        - For active values: propagate adjoint values as adjoint bb arguments.
        - For active buffers: propagate adjoint buffers via `copy_addr`.
    - Revamp `AdjointEmitter` handling of `begin_access` and `end_access`.
      - `getAdjointValue` of `begin_access` now returns the adjoint base buffer.
        Previously, it returned a `begin_access` of the adjoint base buffer
        without generating a corresponding `end_access`.
      - `AdjointEmitter::visitBeginAccessInst` now generates no code.
      - `AdjointEmitter::visitEndAccessInst` now does nothing.
    - Add various control flow differentiation tests.
      - Test differentiation of conditionals (nested), recursion,
        `var` allocations (tuples, structs).
      - Add negative leakchecking tests.
    
    Todos:
    - Fix adjoint value/buffer propagation memory leaks.
    - Add more tests (adjoint SIL, leakchecking).
    - Support differentiation of enum-related instructions and loops.

--- test/AutoDiff/leakchecking.swift
+++ test/AutoDiff/leakchecking.swift
@@ -1,4 +1,4 @@
-// RUN: %target-run-simple-swift
+// RUN: %target-run-simple-swift-control-flow-differentiation
 // REQUIRES: executable_test
 
 // A test that we can properly differentiate types that require refcounting.
@@ -7,6 +7,13 @@ import StdlibUnittest
 import DifferentiationUnittest
 
 var LeakCheckingTests = TestSuite("LeakChecking")
+
+/// Execute body, check expected leak count, and reset global leak count.
+func testWithLeakChecking(expectedLeakCount: Int = 0, _ body: () -> Void) {
+  body()
+  expectEqual(expectedLeakCount, _GlobalLeakCount.count, "Leak detected.")
+  _GlobalLeakCount.count = 0
+}
 
 struct ExampleLeakModel : Differentiable {
   var bias: Tracked<Float> = 2.0
@@ -22,7 +29,45 @@ LeakCheckingTests.test("BasicVarLeakChecking") {
     let x: Tracked<Float> = 1.0
     let _ = model.gradient(at: x) { m, x in m.applied(to: x) }
   }
-  expectEqual(0, _GlobalLeakCount.count, "Leak Detected.")
+  expectEqual(0, _GlobalLeakCount.count, "Leak detected.")
+}
+
+LeakCheckingTests.test("ControlFlow") {
+  // TODO: Add more `var` + control flow tests.
+  // Porting tests from test/AutoDiff/control_flow.swift requires more support
+  // for `Tracked<Float>`.
+
+  // FIXME: Fix control flow AD memory leaks.
+  // See related FIXME comments in adjoint value/buffer propagation in
+  // lib/SILOptimizer/Mandatory/Differentiation.cpp.
+  testWithLeakChecking(expectedLeakCount: 9) {
+    var model = ExampleLeakModel()
+    let x: Tracked<Float> = 1.0
+    let _ = model.gradient(at: x) { m, x in
+      let result: Tracked<Float>
+      if x > 0 {
+        result = m.applied(to: x)
+      } else {
+        result = x
+      }
+      return result
+    }
+  }
+
+  // FIXME: Fix control flow AD memory leaks.
+  // See related FIXME comments in adjoint value/buffer propagation in
+  // lib/SILOptimizer/Mandatory/Differentiation.cpp.
+  testWithLeakChecking(expectedLeakCount: 14) {
+    var model = ExampleLeakModel()
+    let x: Tracked<Float> = 1.0
+    let _ = model.gradient(at: x) { m, x in
+      var result: Tracked<Float> = x
+      if x > 0 {
+        result = result + m.applied(to: x)
+      }
+      return result
+    }
+  }
 }
 
 runAllTests()
