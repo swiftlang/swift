a82d33ff4fc79d965b77429da62f44b157bfbeda
Doug Gregor <dgregor@apple.com>
2019-06-05 14:05:24 -0700

    [SE-0258] Fix crash with implicitly-initialized backing storage.
    Fixes SR-10830 / rdar://problem/51443591

--- lib/AST/Decl.cpp
+++ lib/AST/Decl.cpp
@@ -5478,6 +5478,11 @@ bool VarDecl::isPropertyWrapperInitializedWithInitialValue() const {
   // If there is no initializer, the initialization form depends on
   // whether the property wrapper type has an init(initialValue:).
   if (!isParentInitialized()) {
+    // If it's default-initializable, that doesn't use an initial value.
+    if (auto *PBD = getParentPatternBinding())
+      if (PBD->isDefaultInitializable())
+        return false;
+
     auto wrapperTypeInfo = getAttachedPropertyWrapperTypeInfo();
     return wrapperTypeInfo.initialValueInit != nullptr;
   }
