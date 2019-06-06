cbb9c7089aa394ece74c5998d33b3b6301ff0616
Dan Zheng <danielzheng@google.com>
2019-06-06 00:01:54 -0700

    Use `llvm::SmallDenseMap`.
    Use `llvm::SmallDenseMap` for deterministic insertion order iteration.

--- lib/SILOptimizer/Mandatory/Differentiation.cpp
+++ lib/SILOptimizer/Mandatory/Differentiation.cpp
@@ -54,6 +54,7 @@ using namespace swift;
 using llvm::DenseMap;
 using llvm::SmallDenseMap;
 using llvm::SmallDenseSet;
+using llvm::SmallMapVector;
 using llvm::SmallSet;
 
 /// This flag is used to disable `autodiff_function_extract` instruction folding
@@ -844,7 +845,9 @@ private:
   SmallPtrSet<AutoDiffFunctionInst *, 32> processedAutoDiffFunctionInsts;
 
   /// Mapping from `[differentiable]` attributes to invokers.
-  DenseMap<SILDifferentiableAttr *, DifferentiationInvoker> invokers;
+  /// `SmallMapVector` is used for deterministic insertion order iteration.
+  SmallMapVector<SILDifferentiableAttr *, DifferentiationInvoker, 32>
+      invokers;
 
   /// Mapping from `autodiff_function` instructions to result indices.
   DenseMap<AutoDiffFunctionInst *, unsigned> resultIndices;
@@ -902,7 +905,8 @@ public:
     return processedAutoDiffFunctionInsts;
   }
 
-  DenseMap<SILDifferentiableAttr *, DifferentiationInvoker> &getInvokers() {
+  llvm::SmallMapVector<SILDifferentiableAttr *, DifferentiationInvoker, 32> &
+  getInvokers() {
     return invokers;
   }
 
@@ -957,8 +961,8 @@ public:
   }
 
   void cleanUp() {
-    for (auto invokerInfo : invokers) {
-      auto *attr = invokerInfo.getFirst();
+    for (auto invokerPair : invokers) {
+      auto *attr = std::get<0>(invokerPair);
       auto *original = attr->getOriginal();
       LLVM_DEBUG(getADDebugStream()
                  << "Removing [differentiable] attribute for "
@@ -6357,21 +6361,14 @@ void Differentiation::run() {
   // A global differentiation context.
   ADContext context(*this);
 
-  // Use a temporary list to store `@differentiable` attributes and invokers
-  // for deterministic iteration order.
-  SmallVector<std::pair<SILDifferentiableAttr *, DifferentiationInvoker>, 32>
-      invokerList;
-
   // Register all `@differentiable` attributes and `autodiff_function`
   // instructions in the module that trigger differentiation.
   for (SILFunction &f : module) {
     for (auto *diffAttr : f.getDifferentiableAttrs()) {
       DifferentiationInvoker invoker(diffAttr);
-      auto insertion =
-          context.getInvokers().try_emplace(diffAttr, invoker);
-      assert(insertion.second &&
+      assert(!context.getInvokers().count(diffAttr) &&
              "[differentiable] attribute already has an invoker");
-      invokerList.push_back({diffAttr, invoker});
+      context.getInvokers().insert({diffAttr, invoker});
       continue;
     }
     for (SILBasicBlock &bb : f)
@@ -6396,7 +6393,7 @@ void Differentiation::run() {
   bool errorOccurred = false;
 
   // Process all `[differentiable]` attributes.
-  for (auto invokerPair : invokerList) {
+  for (auto invokerPair : context.getInvokers()) {
     auto *attr = invokerPair.first;
     auto *original = attr->getOriginal();
     auto invoker = invokerPair.second;
