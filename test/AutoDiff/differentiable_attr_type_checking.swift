48f7718a3fb53701951c6ecd4c4595e3d3a7aeff
Dan Zheng <danielzheng@google.com>
2019-06-10 15:56:37 -0700

    [AutoDiff] Diagnose duplicate `@differentiable` attributes on properties. (#25293)
    - Diagnose duplicate `@differentiable` attributes on computed property
      and getter. Resolves TF-544.
    - During SILGen, assert no duplicate `@differentiable` attributes.
    - Improve duplicate `@differentiable` attribute diagnostics.
    - Fix `@differentiable` attribute creation in `Differentiable` derived
      conformances for stored properties.

--- test/AutoDiff/differentiable_attr_type_checking.swift
+++ test/AutoDiff/differentiable_attr_type_checking.swift
@@ -29,15 +29,36 @@ func no_jvp_or_vjp(_ x: Float) -> Float {
   return x * x
 }
 
+// Test duplicated `@differentiable` attributes.
+
 @differentiable // expected-error {{duplicate '@differentiable' attribute}}
-@differentiable
+@differentiable // expected-note {{other attribute declared here}}
 func dupe_attributes(arg: Float) -> Float { return arg }
 
-@differentiable(wrt: arg1) // expected-error {{duplicate '@differentiable' attribute}}
 @differentiable(wrt: arg1)
 @differentiable(wrt: arg2) // expected-error {{duplicate '@differentiable' attribute}}
-@differentiable(wrt: arg2)
+@differentiable(wrt: arg2) // expected-note {{other attribute declared here}}
 func dupe_attributes(arg1: Float, arg2: Float) -> Float { return arg1 }
+
+struct ComputedPropertyDupeAttributes<T : Differentiable> : Differentiable {
+  var value: T
+
+  @differentiable // expected-error {{duplicate '@differentiable' attribute}}
+  var computed1: T {
+    @differentiable // expected-note {{other attribute declared here}}
+    get { value }
+    set { value = newValue }
+  }
+
+  // TODO(TF-482): Remove diagnostics when `@differentiable` attributes are
+  // also uniqued based on generic requirements.
+  @differentiable(where T == Float) // expected-error {{duplicate '@differentiable' attribute}}
+  @differentiable(where T == Double) // expected-note {{other attribute declared here}}
+  var computed2: T {
+    get { value }
+    set { value = newValue }
+  }
+}
 
 class Class {}
 // expected-error @+1 {{class objects and protocol existentials ('Class') cannot be differentiated with respect to}}
