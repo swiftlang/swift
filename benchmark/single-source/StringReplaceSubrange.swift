4d86f3fcf7f7625d41a2f323bd04510ca0aefd51
Keita Ito <keitaitok@gmail.com>
2019-06-11 01:37:39 -0700

    Rename benchmarks based on the benchmark naming convention
    Reference: https://github.com/apple/swift/blob/master/benchmark/Naming.md

--- benchmark/single-source/StringReplaceSubrange.swift
+++ benchmark/single-source/StringReplaceSubrange.swift
@@ -13,12 +13,12 @@
 import TestsUtils
 
 public let StringReplaceSubrange = [
-  BenchmarkInfo(name: "ReplaceSubrangeWithSmallLiteralString", runFunction: run_ReplaceSubrangeWithSmallLiteralString, tags: [.validation, .api]),
-  BenchmarkInfo(name: "ReplaceSubrangeWithLargeLiteralString", runFunction: run_ReplaceSubrangeWithLargeLiteralString, tags: [.validation, .api]),
-  BenchmarkInfo(name: "ReplaceSubrangeWithLargeManagedString", runFunction: run_ReplaceSubrangeWithLargeManagedString, tags: [.validation, .api], setUpFunction: setupLargeManagedString),
-  BenchmarkInfo(name: "ReplaceSubrangeWithSmallLiteralSubstring", runFunction: run_ReplaceSubrangeWithSmallLiteralSubstring, tags: [.validation, .api]),
-  BenchmarkInfo(name: "ReplaceSubrangeWithLargeLiteralSubstring", runFunction: run_ReplaceSubrangeWithLargeLiteralSubstring, tags: [.validation, .api]),
-  BenchmarkInfo(name: "ReplaceSubrangeWithLargeManagedSubstring", runFunction: run_ReplaceSubrangeWithLargeManagedSubstring, tags: [.validation, .api], setUpFunction: setupLargeManagedSubstring),
+  BenchmarkInfo(name: "Str.replaceSubrange.SmallLiteralString", runFunction: run_StrReplaceSubrangeSmallLiteralString, tags: [.validation, .api]),
+  BenchmarkInfo(name: "Str.replaceSubrange.LargeLiteralString", runFunction: run_StrReplaceSubrangeLargeLiteralString, tags: [.validation, .api]),
+  BenchmarkInfo(name: "Str.replaceSubrange.LargeManagedString", runFunction: run_StrReplaceSubrangeLargeManagedString, tags: [.validation, .api], setUpFunction: setupLargeManagedString),
+  BenchmarkInfo(name: "Str.replaceSubrange.SmallLiteralSubstr", runFunction: run_StrReplaceSubrangeSmallLiteralSubstr, tags: [.validation, .api]),
+  BenchmarkInfo(name: "Str.replaceSubrange.LargeLiteralSubstr", runFunction: run_StrReplaceSubrangeLargeLiteralSubstr, tags: [.validation, .api]),
+  BenchmarkInfo(name: "Str.replaceSubrange.LargeManagedSubstr", runFunction: run_StrReplaceSubrangeLargeManagedSubstr, tags: [.validation, .api], setUpFunction: setupLargeManagedSubstring),
 ]
 
 // MARK: - Privates for String
@@ -62,7 +62,7 @@ private func setupLargeManagedSubstring() {
 }
 
 @inline(never)
-public func run_ReplaceSubrangeWithSmallLiteralString(N: Int) {
+public func run_StrReplaceSubrangeSmallLiteralString(N: Int) {
 
     for _ in 0 ..< N {
         var string = smallLiteralString()
@@ -76,7 +76,7 @@ public func run_ReplaceSubrangeWithSmallLiteralString(N: Int) {
 }
 
 @inline(never)
-public func run_ReplaceSubrangeWithLargeLiteralString(N: Int) {
+public func run_StrReplaceSubrangeLargeLiteralString(N: Int) {
 
     for _ in 0 ..< N {
         var string = largeLiteralString()
@@ -90,7 +90,7 @@ public func run_ReplaceSubrangeWithLargeLiteralString(N: Int) {
 }
 
 @inline(never)
-public func run_ReplaceSubrangeWithLargeManagedString(N: Int) {
+public func run_StrReplaceSubrangeLargeManagedString(N: Int) {
 
     for _ in 0 ..< N {
         var string = largeManagedString
@@ -104,7 +104,7 @@ public func run_ReplaceSubrangeWithLargeManagedString(N: Int) {
 }
 
 @inline(never)
-public func run_ReplaceSubrangeWithSmallLiteralSubstring(N: Int) {
+public func run_StrReplaceSubrangeSmallLiteralSubstr(N: Int) {
 
     for _ in 0 ..< N {
         var string = smallLiteralSubstring()
@@ -118,7 +118,7 @@ public func run_ReplaceSubrangeWithSmallLiteralSubstring(N: Int) {
 }
 
 @inline(never)
-public func run_ReplaceSubrangeWithLargeLiteralSubstring(N: Int) {
+public func run_StrReplaceSubrangeLargeLiteralSubstr(N: Int) {
 
     for _ in 0 ..< N {
         var string = largeLiteralSubstring()
@@ -132,7 +132,7 @@ public func run_ReplaceSubrangeWithLargeLiteralSubstring(N: Int) {
 }
 
 @inline(never)
-public func run_ReplaceSubrangeWithLargeManagedSubstring(N: Int) {
+public func run_StrReplaceSubrangeLargeManagedSubstr(N: Int) {
 
     for _ in 0 ..< N {
         var string = largeManagedSubstring
