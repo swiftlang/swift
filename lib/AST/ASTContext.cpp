2df8a691d1dd05fccba37bb48cf7a4f47eaea789
Scott Perry <dev@numist.net>
2019-06-10 17:14:26 -0700

    [Foundation] Bridge difference APIs between Foundation and swift stdlib
    rdar://problem/51604652
    (cherry picked from commit b10632811f3a8d7b8e87fd4b65cc667234684cb2)

--- lib/AST/ASTContext.cpp
+++ lib/AST/ASTContext.cpp
@@ -4981,6 +4981,10 @@ bool ASTContext::isTypeBridgedInExternalModule(
           nominal == getFloatDecl() ||
           nominal == getDoubleDecl() ||
           nominal == getArrayDecl() ||
+          nominal == getCollectionDifferenceDecl() ||
+          (nominal->getDeclContext()->getAsDecl() ==
+            getCollectionDifferenceDecl() &&
+            nominal->getBaseName() == Id_Change) ||
           nominal == getDictionaryDecl() ||
           nominal == getSetDecl() ||
           nominal == getStringDecl() ||
