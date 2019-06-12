587cd97a9537d8df57582542fdf2f9a18db11ae5
Doug Gregor <dgregor@apple.com>
2019-06-12 11:01:28 -0700

    [SE-0258] Warn about (but still use) 'delegateValue' in lieu of 'wrapperValue'.
    Provide some backward compatibilty for property delegates written before
    they became property wrapper, picking up 'delegateValue' (if present)
    but warning about it.
    
    (cherry picked from commit 3ac754075f5f48eb28f2f3a420c51b5b958b7097)

--- test/decl/var/property_wrapper_aliases.swift
+++ test/decl/var/property_wrapper_aliases.swift
@@ -5,6 +5,18 @@
 @propertyDelegate
 struct Delegate<T> {
   var value: T
+
+  var delegateValue: Wrapper<T> { // expected-warning{{property wrapper's `delegateValue` property should be renamed to 'wrapperValue'; use of 'delegateValue' is deprecated}}{{7-20=wrapperValue}}
+    return Wrapper(value: value)
+  }
+}
+
+struct TestDelegateValue {
+  @Delegate var foo: String
+
+  func test() -> Wrapper<String> {
+    return $foo
+  }
 }
 
 @propertyWrapper
