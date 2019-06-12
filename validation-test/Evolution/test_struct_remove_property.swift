5ba3abf1d4d0e7342d20eac40a1863fa2074e2f6
Arnold Schwaighofer <aschwaighofer@apple.com>
2019-06-12 15:51:42 -0700

    Remove the remaining swift_test_mode_optimize_none_with_implicit_dynamic
    It is expected that under enable-private-import internal/private symbols
    become public. So that symbol-diffing would fail. Disable symbol diffing
    under that test mode.
    
    rdar://51304243

--- validation-test/Evolution/test_struct_remove_property.swift
+++ validation-test/Evolution/test_struct_remove_property.swift
@@ -1,7 +1,6 @@
 // RUN: %target-resilience-test
 // REQUIRES: executable_test
 
-// UNSUPPORTED: swift_test_mode_optimize_none_with_implicit_dynamic
 
 import StdlibUnittest
 import struct_remove_property
