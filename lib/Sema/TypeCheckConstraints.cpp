20ddd244abc2c339af8e2371af6c3e98319a2c26
Pavel Yaskevich <pyaskevich@apple.com>
2019-06-13 00:56:00 -0700

    [ConstraintSystem] Don't track captures if solver was in diagnostic mode
    If solution application is attempted for one of the sub-expressions,
    while diagnostics are trying to narrow down where the failure is
    located, don't record that captures need to be computed for closures,
    because that could fail later on as in such conditions expressions
    are not guaranteed to have correct types (e.g. some types could be
    set to "unresolved").
    
    (cherry picked from commit 59bc60eb201e35a2cf90a5a286c78d97500f8111)

--- lib/Sema/TypeCheckConstraints.cpp
+++ lib/Sema/TypeCheckConstraints.cpp
@@ -2190,6 +2190,9 @@ Type TypeChecker::typeCheckExpressionImpl(Expr *&expr, DeclContext *dc,
   if (options.contains(TypeCheckExprFlags::ConvertTypeIsOpaqueReturnType))
     csOptions |= ConstraintSystemFlags::UnderlyingTypeForOpaqueReturnType;
 
+  if (options.contains(TypeCheckExprFlags::SubExpressionDiagnostics))
+    csOptions |= ConstraintSystemFlags::SubExpressionDiagnostics;
+
   ConstraintSystem cs(*this, dc, csOptions, expr);
   cs.baseCS = baseCS;
 
