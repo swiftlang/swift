2dc61c502ce2ff4ccab4c4477731a38649521c12
Arnold Schwaighofer <aschwaighofer@apple.com>
2019-06-13 09:00:50 -0700

    Fix typo in diagnostic

--- test/attr/dynamicReplacement_library_evolution.swift
+++ test/attr/dynamicReplacement_library_evolution.swift
@@ -2,13 +2,13 @@
 // RUN: %target-typecheck-verify-swift -swift-version 5 -enable-library-evolution  -I %t
 
 struct Container {
-  dynamic var property: Int { return 1 } // expected-error{{marking non-'@objc' Swift declaration 'dynamic' in library evolution mode not supported}}
-  dynamic func foo() {} // expected-error{{marking non-'@objc' Swift declaration 'dynamic' in library evolution mode not supported}}
+  dynamic var property: Int { return 1 } // expected-error{{marking non-'@objc' Swift declaration 'dynamic' in library evolution mode is not supported}}
+  dynamic func foo() {} // expected-error{{marking non-'@objc' Swift declaration 'dynamic' in library evolution mode is not supported}}
 }
 
 class AClass {
-  dynamic var property: Int { return 1 } // expected-error{{marking non-'@objc' Swift declaration 'dynamic' in library evolution mode not supported}}
-  dynamic func foo() {} // expected-error{{marking non-'@objc' Swift declaration 'dynamic' in library evolution mode not supported}}
+  dynamic var property: Int { return 1 } // expected-error{{marking non-'@objc' Swift declaration 'dynamic' in library evolution mode is not supported}}
+  dynamic func foo() {} // expected-error{{marking non-'@objc' Swift declaration 'dynamic' in library evolution mode is not supported}}
 
   @objc dynamic func allowed() {}
 
