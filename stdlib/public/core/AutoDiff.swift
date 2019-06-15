fdef58426d312afed2c10a1253eeaf674fbffe19
Richard Wei <rxwei@google.com>
2019-06-14 17:34:52 -0700

    Update AutoDiff.swift

--- stdlib/public/core/AutoDiff.swift
+++ stdlib/public/core/AutoDiff.swift
@@ -52,7 +52,7 @@ public extension VectorProtocol {
 }
 
 extension VectorProtocol where VectorSpaceScalar: SignedNumeric {
-  prefix static func - (x: Self) -> Self {
+  static prefix func - (x: Self) -> Self {
     .zero - x
   }
 }
