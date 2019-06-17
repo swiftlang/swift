8bb91443a9e81bbfac92a2621a0af887a1da8dbf
Saleem Abdulrasool <compnerd@compnerd.org>
2019-06-17 15:07:04 -0700

    build: dereference variable for some versions of CMake
    CMake quoting is odd and in some versions this would compare the wrong
    strings.

--- cmake/modules/AddSwift.cmake
+++ cmake/modules/AddSwift.cmake
@@ -1383,7 +1383,7 @@ function(_add_swift_library_single target name)
   # NOTE(compnerd) use the C linker language to invoke `clang` rather than
   # `clang++` as we explicitly link against the C++ runtime.  We were previously
   # actually passing `-nostdlib++` to avoid the C++ runtime linkage.
-  if(SWIFTLIB_SINGLE_SDK STREQUAL ANDROID)
+  if("${SWIFTLIB_SINGLE_SDK}" STREQUAL "ANDROID")
     set_property(TARGET "${target}" PROPERTY
       LINKER_LANGUAGE "C")
   else()
