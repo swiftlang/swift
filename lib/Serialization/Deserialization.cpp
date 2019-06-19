fe74cb401708fef384da368e9e768e172c97bbe9
Joe Groff <jgroff@apple.com>
2019-06-18 21:46:45 -0700

    Serialization: Deserialize opaque type xrefs from the right extension module.
    When deserializing an opaque type xref inside an extension context, we were looking
    incorrectly in the base module of the type being extended, rather than in the module
    of the extension, where the opaque type would really be. Fixes rdar://problem/51775500.
    
    This includes a small refactoring of OpaqueTypeDecl deserialization to break the inevitable
    cycle between deserializing the namingDecl, and the namingDecl turning around and re-
    deserializing its opaque return type. This is NFC but avoids some unnecessary work.

--- lib/Serialization/Deserialization.cpp
+++ lib/Serialization/Deserialization.cpp
@@ -1724,9 +1724,10 @@ giveUpFastPath:
       
       auto name = getIdentifier(DefiningDeclNameID);
       pathTrace.addOpaqueReturnType(name);
-      
-      if (auto opaqueTy = baseModule->lookupOpaqueResultType(name.str(),
-                                                             nullptr)) {
+    
+      auto lookupModule = M ? M : baseModule;
+      if (auto opaqueTy = lookupModule->lookupOpaqueResultType(name.str(),
+                                                               nullptr)) {
         values.push_back(opaqueTy);
       }
       break;
@@ -3168,7 +3169,6 @@ public:
                                               interfaceTypeID, genericEnvID,
                                               underlyingTypeID);
     
-    auto namingDecl = cast<ValueDecl>(MF.getDecl(namingDeclID));
     auto declContext = MF.getDeclContext(contextID);
     auto sig = MF.getGenericSignature(interfaceSigID);
     auto interfaceType = MF.getType(interfaceTypeID)
@@ -3180,9 +3180,12 @@ public:
       
     // Create the decl.
     auto opaqueDecl =
-      new (ctx) OpaqueTypeDecl(namingDecl, nullptr, declContext,
+      new (ctx) OpaqueTypeDecl(nullptr, nullptr, declContext,
                                sig, interfaceType);
     declOrOffset = opaqueDecl;
+
+    auto namingDecl = cast<ValueDecl>(MF.getDecl(namingDeclID));
+    opaqueDecl->setNamingDecl(namingDecl);
 
     if (auto genericParams = MF.maybeReadGenericParams(opaqueDecl))
       opaqueDecl->setGenericParams(genericParams);
