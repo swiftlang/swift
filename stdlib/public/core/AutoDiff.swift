0d589e759151f0ee77e52f06ba14779d24a303ac
Anthony Platanios <e.a.platanios@gmail.com>
2019-06-23 11:27:51 -0400

    Added support for 'withoutDerivative(at:)'.

--- stdlib/public/core/AutoDiff.swift
+++ stdlib/public/core/AutoDiff.swift
@@ -139,12 +139,14 @@ public extension Differentiable where TangentVector == Self {
   }
 }
 
-public extension Differentiable {
-  /// Identity function that stops derivatives from propagating.
-  @inlinable
-  @inline(__always)
-  @_semantics("autodiff.nonvarying")
-  func withoutDerivative() -> Self { return self }
+/// Acts as an identity function. When used in a context where `x` is being
+/// differentiated with respect to, this function will not produce any 
+/// derivative at `x`.
+@inlinable
+@inline(__always)
+@_semantics("autodiff.nonvarying")
+public func withoutDerivative<T>(at x: T) -> T {
+  x
 }
 
 /// Applies the given closure `body` to `x`. When used in a context where `x` is
