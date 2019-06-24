f53525b0a1132fe40b26ff51019a900ff571b72f
Xi Ge <xi_ge@apple.com>
2019-06-24 11:02:44 -0700

    Revert "[ownership] Ignore non-consuming uses in dead end blocks when we are analyzing objects that are consumed once in the same block in which they are defined."
    This reverts commit 9a67f2d3bad8b96c049932d7c7edeffe0e391d41.

--- lib/SIL/LinearLifetimeChecker.cpp
+++ lib/SIL/LinearLifetimeChecker.cpp
@@ -486,12 +486,11 @@ LinearLifetimeError swift::valueHasLinearLifetime(
   // have been detected by initializing our consuming uses. So we are done.
   if (consumingUses.size() == 1 &&
       consumingUses[0].getParent() == value->getParentBlock()) {
-    // Check if any of our non consuming uses are not in the parent block and
-    // are reachable. We flag those as additional use after frees. Any in the
-    // same block, we would have flagged.
+    // Check if any of our non consuming uses are not in the parent block. We
+    // flag those as additional use after frees. Any in the same block, we would
+    // have flagged.
     if (llvm::any_of(nonConsumingUses, [&](BranchPropagatedUser user) {
-          return user.getParent() != value->getParentBlock() &&
-            !deBlocks.isDeadEnd(user.getParent());
+          return user.getParent() != value->getParentBlock();
         })) {
       state.error.handleUseAfterFree([&] {
         llvm::errs() << "Function: '" << value->getFunction()->getName()
