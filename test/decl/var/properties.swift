9f3cdb92b20222df77840b75273616804a09f560
Suyash Srijan <suyashsrijan@outlook.com>
2019-06-25 01:07:14 +0100

    [Test] Update tests

--- test/decl/var/properties.swift
+++ test/decl/var/properties.swift
@@ -1285,8 +1285,17 @@ class SR_10995 {
     return nil
   }
 
+  func makeSingleOptionalNever() -> Never? {
+    return nil
+  }
+
   func sr_10995_foo() {
-    let doubleOptionalNever = makeDoubleOptionalNever() // expected-warning {{Constant 'doubleOptionalNever' inferred to have type 'Never??', which is an enum with no cases}}
+    let doubleOptionalNever = makeDoubleOptionalNever() // expected-warning {{constant 'doubleOptionalNever' inferred to have type 'Never??', which may be unexpected}} 
+    // expected-note@-1 {{add an explicit type annotation to silence this warning}} 
+    // expected-warning@-2 {{initialization of immutable value 'doubleOptionalNever' was never used; consider replacing with assignment to '_' or removing it}}
+    let singleOptionalNever = makeSingleOptionalNever() // expected-warning {{constant 'singleOptionalNever' inferred to have type 'Never?', which may be unexpected}} 
+    // expected-note@-1 {{add an explicit type annotation to silence this warning}} 
+    // expected-warning@-2 {{initialization of immutable value 'singleOptionalNever' was never used; consider replacing with assignment to '_' or removing it}}
   }
 }
 
