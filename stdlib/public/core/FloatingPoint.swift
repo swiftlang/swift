d13a9074466923fac27b485e6f3754a122a56ea9
Steve (Numerics) Canon <scanon@apple.com>
2019-06-26 17:28:40 -0400

    Switch transparent to inlinable+inline(__always).

--- stdlib/public/core/FloatingPoint.swift
+++ stdlib/public/core/FloatingPoint.swift
@@ -2031,7 +2031,7 @@ extension BinaryFloatingPoint {
   /// following formula, where `**` is exponentiation:
   ///
   ///     let magnitude = x.significand * F.radix ** x.exponent
-  @_transparent
+  @inlinable @inline(__always)
   public static var radix: Int { return 2 }
 
   /// Creates a new floating-point value using the sign of one value and the
