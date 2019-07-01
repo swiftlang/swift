e99fac01edc6484e397deb890df53998ef1ad41e
Xi Ge <xi_ge@apple.com>
2019-07-01 11:57:55 -0700

    ABI Checker: diagnose underlying type changes of opaque result types from inlinable decls.
    Inlinable decls from swift interface files allow us to construct the underlying types
    of opaque result types, whose changes can break ABI.
    
    rdar://52273137

--- test/api-digester/Inputs/cake_baseline/cake.swift
+++ test/api-digester/Inputs/cake_baseline/cake.swift
@@ -175,3 +175,21 @@ public protocol HasMutatingMethodClone: HasMutatingMethod {
 public extension Int {
   public func IntEnhancer() {}
 }
+
+public protocol Animal {}
+public class Cat: Animal { public init() {} }
+public class Dog: Animal { public init() {} }
+
+public class Zoo {
+  public init() {}
+  @inlinable
+  @available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
+  public var current: some Animal {
+    return Cat()
+  }
+  @inlinable
+  @available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
+  public func getCurrentAnimalInlinable() -> some Animal {
+    return Cat()
+  }
+}
