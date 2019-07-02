6d4d8caf95b84bb3b6b421b376a64b621e8d3a12
Michael Ilseman <milseman@apple.com>
2019-07-02 13:09:55 -0700

    [String] Switch scalar-aligned bit to a reserved bit.
    Since scalar-alignment is set in inlinable code, switch the alignment
    bit to one of the previously-reserved bits rather than a grapheme
    cache bit. Setting a grapheme cache bit in inlinable would break
    backward deployment, as older versions would interpret it as a cached
    value.
    
    Also adjust the name to "scalar-aligned", which is clearer, and
    removed assertion (which should be a real precondition).

--- stdlib/public/core/StringIndex.swift
+++ stdlib/public/core/StringIndex.swift
@@ -16,25 +16,36 @@ import SwiftShims
 
 String's Index has the following layout:
 
- ┌──────────┬───────────────────┬─────────╥────────────────┬──────────┐
- │ b63:b16  │      b15:b14      │   b13   ║     b12:b8     │  b6:b0   │
- ├──────────┼───────────────────┼─────────╫────────────────┼──────────┤
- │ position │ transcoded offset │ aligned ║ grapheme cache │ reserved │
- └──────────┴───────────────────┴─────────╨────────────────┴──────────┘
+ ┌──────────┬───────────────────╥────────────────┬──────────╥────────────────┐
+ │ b63:b16  │      b15:b14      ║     b13:b8     │  b7:b1   ║       b0       │
+ ├──────────┼───────────────────╫────────────────┼──────────╫────────────────┤
+ │ position │ transcoded offset ║ grapheme cache │ reserved ║ scalar aligned │
+ └──────────┴───────────────────╨────────────────┴──────────╨────────────────┘
+                                └──────── resilient ────────┘
 
-Position, transcoded offset, and aligned are fully exposed in the ABI. Grapheme
-cache and reserved are partially resilient: the fact that there are 13 bits with
-a default value of `0` is ABI, but not the layout, construction, or
+Position, transcoded offset, and scalar aligned are fully exposed in the ABI.
+Grapheme cache and reserved are partially resilient: the fact that there are 13
+bits with a default value of `0` is ABI, but not the layout, construction, or
 interpretation of those bits. All use of grapheme cache should be behind
-non-inlinable function calls.
+non-inlinable function calls. Inlinable code should not set a non-zero value to
+grapheme cache bits: doing so breaks back deployment as they will be interpreted
+as a set cache.
 
 - position aka `encodedOffset`: A 48-bit offset into the string's code units
+
 - transcoded offset: a 2-bit sub-scalar offset, derived from transcoding
-- aligned, whether this index is known to be scalar-aligned (see below)
+
 <resilience barrier>
-- grapheme cache: A 5-bit value remembering the distance to the next grapheme
-boundary
-- reserved: 8-bit for future use.
+
+- grapheme cache: A 6-bit value remembering the distance to the next grapheme
+boundary.
+
+- reserved: 7-bit for future use.
+
+<resilience barrier>
+
+- scalar aligned, whether this index is known to be scalar-aligned (see below)
+
 
 */
 extension String {
@@ -86,7 +97,7 @@ extension String.Index {
 
   @usableFromInline
   internal var characterStride: Int? {
-    let value = (_rawBits & 0x1F00) &>> 8
+    let value = (_rawBits & 0x3F00) &>> 8
     return value > 0 ? Int(truncatingIfNeeded: value) : nil
   }
 
@@ -136,7 +147,7 @@ extension String.Index {
     encodedOffset: Int, transcodedOffset: Int, characterStride: Int
   ) {
     self.init(encodedOffset: encodedOffset, transcodedOffset: transcodedOffset)
-    if _slowPath(characterStride > 0x1F) { return }
+    if _slowPath(characterStride > 0x3F) { return }
     self._rawBits |= UInt64(truncatingIfNeeded: characterStride &<< 8)
     self._invariantCheck()
   }
@@ -152,7 +163,7 @@ extension String.Index {
   @usableFromInline @inline(never) @_effects(releasenone)
   internal func _invariantCheck() {
     _internalInvariant(_encodedOffset >= 0)
-    if self._isAligned {
+    if self._isScalarAligned {
       _internalInvariant(transcodedOffset == 0)
     }
   }
@@ -209,35 +220,35 @@ extension String.Index {
 }
 
 /*
-  Index Alignment
+  Index Scalar Alignment
 
   SE-0180 unifies the Index type of String and all its views and allows
   non-scalar-aligned indices to be used across views. In order to guarantee
   behavior, we often have to check and perform scalar alignment. To speed up
-  these checks, we allocate a bit denoting known-to-be-aligned, so that the
-  alignment check can skip the load. The below shows what views need to check
-  for alignment before they can operate, and whether the indices they produce
-  are aligned.
+  these checks, we allocate a bit denoting known-to-be-scalar-aligned, so that
+  the alignment check can skip the load. The below shows what views need to
+  check for alignment before they can operate, and whether the indices they
+  produce are aligned.
 
-  ┌───────────────╥────────────────────┬──────────────────────────┐
-  │ View          ║ Requires Alignment │ Produces Aligned Indices │
-  ╞═══════════════╬════════════════════╪══════════════════════════╡
-  │ Native UTF8   ║ no                 │ no                       │
-  ├───────────────╫────────────────────┼──────────────────────────┤
-  │ Native UTF16  ║ yes                │ no                       │
-  ╞═══════════════╬════════════════════╪══════════════════════════╡
-  │ Foreign UTF8  ║ yes                │ no                       │
-  ├───────────────╫────────────────────┼──────────────────────────┤
-  │ Foreign UTF16 ║ no                 │ no                       │
-  ╞═══════════════╬════════════════════╪══════════════════════════╡
-  │ UnicodeScalar ║ yes                │ yes                      │
-  ├───────────────╫────────────────────┼──────────────────────────┤
-  │ Character     ║ yes                │ yes                      │
-  └───────────────╨────────────────────┴──────────────────────────┘
+  ┌───────────────╥───────────────────────────┬─────────────────────────┐
+  │ View          ║ Requires Scalar Alignment │ Produces Scalar Aligned │
+  ╞═══════════════╬═══════════════════════════╪═════════════════════════╡
+  │ Native UTF8   ║ no                        │ no                      │
+  ├───────────────╫───────────────────────────┼─────────────────────────┤
+  │ Native UTF16  ║ yes                       │ no                      │
+  ╞═══════════════╬═══════════════════════════╪═════════════════════════╡
+  │ Foreign UTF8  ║ yes                       │ no                      │
+  ├───────────────╫───────────────────────────┼─────────────────────────┤
+  │ Foreign UTF16 ║ no                        │ no                      │
+  ╞═══════════════╬═══════════════════════════╪═════════════════════════╡
+  │ UnicodeScalar ║ yes                       │ yes                     │
+  ├───────────────╫───────────────────────────┼─────────────────────────┤
+  │ Character     ║ yes                       │ yes                     │
+  └───────────────╨───────────────────────────┴─────────────────────────┘
 
-  The "requires alignment" applies to any operation taking a String.Index that's
-  not defined entirely in terms of other operations taking a String.Index. These
-  include:
+  The "requires scalar alignment" applies to any operation taking a String.Index
+  that's not defined entirely in terms of other operations taking a
+  String.Index. These include:
 
   * index(after:)
   * index(before:)
@@ -249,13 +260,13 @@ extension String.Index {
 extension String.Index {
   @_alwaysEmitIntoClient // Swift 5.1
   @inline(__always)
-  internal var _isAligned: Bool { return 0 != _rawBits & 0x2000 }
+  internal var _isScalarAligned: Bool { return 0 != _rawBits & 0x1 }
 
   @_alwaysEmitIntoClient // Swift 5.1
   @inline(__always)
-  internal var _aligned: String.Index {
+  internal var _scalarAligned: String.Index {
     var idx = self
-    idx._rawBits |= 0x2000
+    idx._rawBits |= 0x1
     idx._invariantCheck()
     return idx
   }
