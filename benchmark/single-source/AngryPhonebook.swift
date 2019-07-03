560cdca452379526e925c791b2cce46a01d15a6e
Pavol Vaskovic <pali@pali.sk>
2019-07-03 18:49:51 +0200

    [benchmark] Large variants of new AngryPhonebook
    Add case conversion benchmarks that operate on one large string (precomposed phonebook, comma separated names).

--- benchmark/single-source/AngryPhonebook.swift
+++ benchmark/single-source/AngryPhonebook.swift
@@ -23,6 +23,8 @@ public let AngryPhonebook = [
     runFunction: run_AngryPhonebook,
     tags: t,
     legacyFactor: 7),
+
+  // Small String Workloads
   BenchmarkInfo(
     name: "AngryPhonebook.Latin",
     runFunction: { angryPhonebook($0, latin) },
@@ -37,7 +39,24 @@ public let AngryPhonebook = [
     name: "AngryPhonebook.Cyrillic",
     runFunction: { angryPhonebook($0, cyrillic) },
     tags: t,
-    setUpFunction: { blackHole(cyrillic) })
+    setUpFunction: { blackHole(cyrillic) }),
+
+  // Large String Workloads
+  BenchmarkInfo(
+    name: "AngryPhonebook.Latin.Large",
+    runFunction: { largeAngryPhonebook($0, latinLarge) },
+    tags: t,
+    setUpFunction: { blackHole(latinLarge) }),
+  BenchmarkInfo(
+    name: "AngryPhonebook.Armenian.Large",
+    runFunction: { largeAngryPhonebook($0, armenianLarge) },
+    tags: t,
+    setUpFunction: { blackHole(armenianLarge) }),
+  BenchmarkInfo(
+    name: "AngryPhonebook.Cyrillic.Large",
+    runFunction: { largeAngryPhonebook($0, cyrillicLarge) },
+    tags: t,
+    setUpFunction: { blackHole(cyrillicLarge) })
 ]
 
 let words = [
@@ -64,17 +83,29 @@ public func run_AngryPhonebook(_ N: Int) {
 // a total length of approximately 120 characters.
 // E.g.: `latin.joined(separator: "").count == 124`
 
+/// Precompose the phonebook into one large string of comma separated names.
+func large(_ names: [String]) -> String {
+  names.map { firstName in
+    names.map { lastName in
+      firstName + " " + lastName }
+      .joined(separator: ", ")
+    }.joined(separator: ", ")
+}
+
 let latin = Array(words.prefix(20))
+let latinLarge = large(latin)
 
 let armenian = [
   "Արմեն", "Աննա", "Հարութ", "Միքայել", "Մարիա", "Դավիթ", "Վարդան",
   "Նարինե", "Տիգրան", "Տաթևիկ", "Թագուհի", "Թամարա", "Ազնաուր", "Գրիգոր",
   "Կոմիտաս", "Հայկ", "Գառնիկ", "Վահրամ", "Վահագն", "Գևորգ"]
+let armenianLarge = large(armenian)
 
 let cyrillic = [
   "Ульяна", "Аркадий", "Аня", "Даниил", "Дмитрий", "Эдуард", "Юрій", "Давид",
   "Анна", "Дмитрий", "Евгений", "Борис", "Ксения", "Артур", "Аполлон",
   "Соломон", "Николай", "Кристи", "Надежда", "Спартак"]
+let cyrillicLarge = large(cyrillic)
 
 @inline(never)
 public func angryPhonebook(_ N: Int, _ names: [String]) {
@@ -86,5 +117,12 @@ public func angryPhonebook(_ N: Int, _ names: [String]) {
         blackHole((firstname.uppercased(), lastname.lowercased()))
       }
     }
+  }
+}
+
+@inline(never)
+public func largeAngryPhonebook(_ N: Int, _ names: String) {
+  for _ in 1...N {
+    blackHole((names.uppercased(), names.lowercased()))
   }
 }
