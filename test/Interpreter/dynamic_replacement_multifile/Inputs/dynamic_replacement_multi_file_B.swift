ca1e808bde54310d0a2e7160ff1375507fd0101c
Stephen Canon <scanon@apple.com>
2019-07-12 14:48:37 -0400

    Replace all 9999 availability in non-stdlib tests. (#26109)
    Replace all 9999 availability in non-stdlib tests with the appropriate platform availability.

--- test/Interpreter/dynamic_replacement_multifile/Inputs/dynamic_replacement_multi_file_B.swift
+++ test/Interpreter/dynamic_replacement_multifile/Inputs/dynamic_replacement_multi_file_B.swift
@@ -7,12 +7,12 @@ func replaceable2_r() -> Int {
   return 3
 }
 
-@available(macOS 9999, iOS 9999, tvOS 9999, watchOS 9999, *)
+@available(macOS 10.15, iOS 13, tvOS 13, watchOS 6, *)
 dynamic func bar3(_ x: Int) -> some P {
   return x
 }
 
-@available(macOS 9999, iOS 9999, tvOS 9999, watchOS 9999, *)
+@available(macOS 10.15, iOS 13, tvOS 13, watchOS 6, *)
 @_dynamicReplacement(for: bar3(_:))
 func bar3_r(_ x: Int) -> some P {
   return Pair()
