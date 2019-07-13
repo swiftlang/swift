eccee1debdc4e603cd87816b5ce00c64d3dbe623
Owen Voorhees <owen@squibner.com>
2019-07-12 21:05:53 -0700

    Update interpreter test to use the new as T... syntax for varargs expansion

--- test/Interpreter/varargs.swift
+++ test/Interpreter/varargs.swift
@@ -18,45 +18,45 @@ vf(s as NSString)
 // CHECK: OK
 
 func f(_ x: Int...) {
-  print(#variadic(x), separator: ",")
+  print(x as Any..., separator: ",")
 }
 
 func f(_ x: [Int]...) {
-  print(#variadic(x), separator: ",")
+  print(x as Any..., separator: ",")
 }
 
 let closure: ((Int...) -> Void) = {
   (args: Int...) in
-  print(#variadic(args), separator: ",")
+  print(args as Any..., separator: ",")
 }
 
 f(1,2,3) // CHECK: 1,2,3
-f(#variadic([1,2,3])) // CHECK: 1,2,3
-closure(#variadic([1,2,3])) // CHECK: 1,2,3
+f([1,2,3] as Int...) // CHECK: 1,2,3
+closure([1,2,3] as Int...) // CHECK: 1,2,3
 let x = [1,2,3].map { $0 + 1 }
-f(#variadic(x)) // CHECK: 2,3,4
-f(#variadic([x, x])) // CHECK: [2, 3, 4],[2, 3, 4]
+f(x as Int...) // CHECK: 2,3,4
+f([x, x] as [Int]...) // CHECK: [2, 3, 4],[2, 3, 4]
 
 func overloaded(x: [Int]) { print("incorrect") }
 func overloaded(x: Int...) { print("correct") }
-overloaded(x: #variadic(x)) // CHECK: correct
+overloaded(x: x as Int...) // CHECK: correct
 
 struct HasVariadicSubscript {
   subscript(bar: Int...) -> Int {
     get {
-      print(#variadic(bar), separator: ",")
+      print(bar as Any..., separator: ",")
       return 42
     }
   }
 }
-_ = HasVariadicSubscript()[9,8,7] // CHECK: 9,8,7
+_ = HasVariadicSubscript()[[9,8,7] as Int...] // CHECK: 9,8,7
 
 func triangle(_ x: Int...) {
   if x.isEmpty {
     return
   }
-  print(#variadic(x), separator: ",")
-  triangle(#variadic(x.dropLast()))
+  print(x as Any..., separator: ",")
+  triangle(x.dropLast() as Int...)
 }
 
 triangle(1,2,3,4,5)
