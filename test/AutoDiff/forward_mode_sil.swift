0973f3f06302f742b10cfe1be432ce25d20f4684
Bart Chrzaszcz <bart.chr@gmail.com>
2019-07-17 10:41:41 -0700

    Fix SIL tests.

--- test/AutoDiff/forward_mode_sil.swift
+++ test/AutoDiff/forward_mode_sil.swift
@@ -18,40 +18,82 @@ func unary(_ x: Float) -> Float {
 // CHECK-DATA-STRUCTURES: enum _AD__unary_bb0__Succ__src_0_wrt_0 {
 // CHECK-DATA-STRUCTURES: }
 
-
 // CHECK-SIL-LABEL: sil hidden @AD__unary__jvp_src_0_wrt_0 : $@convention(thin) (Float) -> (Float, @owned @callee_guaranteed (Float) -> Float) {
-// CHECK-SIL: // %0
-// CHECK-SIL: bb0(%0 : $Float):
-// CHECK-SIL:   debug_value %0 : $Float, let, name "x", argno 1 // id: %1
-// CHECK-SIL:   %2 = metatype $@thin Float.Type                 // user: %18
-// CHECK-SIL:   %3 = metatype $@thin Float.Type                 // user: %9
-// CHECK-SIL:   %4 = function_ref @$sSf1moiyS2f_SftFZ : $@convention(method) (Float, Float, @thin Float.Type) -> Float // users: %8, %5
-// CHECK-SIL:   retain_value %4 : $@convention(method) (Float, Float, @thin Float.Type) -> Float // id: %5
-// CHECK-SIL:   // function_ref static Float._vjpMultiply(lhs:rhs:)
-// CHECK-SIL:   %6 = function_ref @AD__$sSf1moiyS2f_SftFZ__jvp_src_0_wrt_0_1 : $@convention(method) (Float, Float, @thin Float.Type) -> (Float, @owned @callee_guaranteed (Float, Float) -> Float) // users: %9, %8
-// CHECK-SIL:   // function_ref static Float._vjpMultiply(lhs:rhs:)
-// CHECK-SIL:   %7 = function_ref @$sSf12_vjpMultiply3lhs3rhsSf5value_Sf_SftSfc8pullbacktSf_SftFZ : $@convention(method) (Float, Float, @thin Float.Type) -> (Float, @owned @callee_guaranteed (Float) -> (Float, Float)) // user: %8
-// CHECK-SIL:   %8 = autodiff_function [wrt 0 1] [order 1] %4 : $@convention(method) (Float, Float, @thin Float.Type) -> Float with {%6 : $@convention(method) (Float, Float, @thin Float.Type) -> (Float, @owned @callee_guaranteed (Float, Float) -> Float), %7 : $@convention(method) (Float, Float, @thin Float.Type) -> (Float, @owned @callee_guaranteed (Float) -> (Float, Float))} // user: %10
-// CHECK-SIL:   %9 = apply %6(%0, %0, %3) : $@convention(method) (Float, Float, @thin Float.Type) -> (Float, @owned @callee_guaranteed (Float, Float) -> Float) // users: %12, %11
-// CHECK-SIL:   release_value %8 : $@differentiable @convention(method) (Float, Float, @nondiff @thin Float.Type) -> Float // id: %10
-// CHECK-SIL:   %11 = tuple_extract %9 : $(Float, @callee_guaranteed (Float, Float) -> Float), 0 // user: %18
-// CHECK-SIL:   %12 = tuple_extract %9 : $(Float, @callee_guaranteed (Float, Float) -> Float), 1 // user: %22
-// CHECK-SIL:   // function_ref static Float.* infix(_:_:)
-// CHECK-SIL:   %13 = function_ref @$sSf1moiyS2f_SftFZ : $@convention(method) (Float, Float, @thin Float.Type) -> Float // users: %17, %14
-// CHECK-SIL:   retain_value %13 : $@convention(method) (Float, Float, @thin Float.Type) -> Float // id: %14
-// CHECK-SIL:   // function_ref AD__$sSf1moiyS2f_SftFZ__jvp_src_0_wrt_0_1
-// CHECK-SIL:   %15 = function_ref @AD__$sSf1moiyS2f_SftFZ__jvp_src_0_wrt_0_1 : $@convention(method) (Float, Float, @thin Float.Type) -> (Float, @owned @callee_guaranteed (Float, Float) -> Float) // users: %18, %17
-// CHECK-SIL:   // function_ref static Float._vjpMultiply(lhs:rhs:)
-// CHECK-SIL:   %16 = function_ref @$sSf12_vjpMultiply3lhs3rhsSf5value_Sf_SftSfc8pullbacktSf_SftFZ : $@convention(method) (Float, Float, @thin Float.Type) -> (Float, @owned @callee_guaranteed (Float) -> (Float, Float)) // user: %17
-// CHECK-SIL:   %17 = autodiff_function [wrt 0 1] [order 1] %13 : $@convention(method) (Float, Float, @thin Float.Type) -> Float with {%15 : $@convention(method) (Float, Float, @thin Float.Type) -> (Float, @owned @callee_guaranteed (Float, Float) -> Float), %16 : $@convention(method) (Float, Float, @thin Float.Type) -> (Float, @owned @callee_guaranteed (Float) -> (Float, Float))} // user: %19
-// CHECK-SIL:   %18 = apply %15(%11, %0, %2) : $@convention(method) (Float, Float, @thin Float.Type) -> (Float, @owned @callee_guaranteed (Float, Float) -> Float) // users: %21, %20
-// CHECK-SIL:   release_value %17 : $@differentiable @convention(method) (Float, Float, @nondiff @thin Float.Type) -> Float // id: %19
-// CHECK-SIL:   %20 = tuple_extract %18 : $(Float, @callee_guaranteed (Float, Float) -> Float), 0 // user: %25
-// CHECK-SIL:   %21 = tuple_extract %18 : $(Float, @callee_guaranteed (Float, Float) -> Float), 1 // user: %22
-// CHECK-SIL:   %22 = struct $_AD__unary_bb0__DF__src_0_wrt_0 (%12 : $@callee_guaranteed (Float, Float) -> Float, %21 : $@callee_guaranteed (Float, Float) -> Float) // user: %24
-// CHECK-SIL:   // function_ref AD__unary__differential_src_0_wrt_0
-// CHECK-SIL:   %23 = function_ref @AD__unary__differential_src_0_wrt_0 : $@convention(thin) (Float, @guaranteed _AD__unary_bb0__DF__src_0_wrt_0) -> Float // user: %24
-// CHECK-SIL:   %24 = partial_apply [callee_guaranteed] %23(%22) : $@convention(thin) (Float, @guaranteed _AD__unary_bb0__DF__src_0_wrt_0) -> Float // user: %25
-// CHECK-SIL:   %25 = tuple (%20 : $Float, %24 : $@callee_guaranteed (Float) -> Float) // user: %26
-// CHECK-SIL:   return %25 : $(Float, @callee_guaranteed (Float) -> Float) // id: %26
-// CHECK-SIL: } // end sil function 'AD__unary__jvp_src_0_wrt_0'
+// CHECK-SIL: bb0([[X_ARG:%.*]] : $Float):
+// CHECK-SIL:   [[MULT_FUNC_1:%.*]] = function_ref @$sSf1moiyS2f_SftFZ : $@convention(method) (Float, Float, @thin Float.Type) -> Float
+// CHECK-SIL:   retain_value [[MULT_FUNC_1]] : $@convention(method) (Float, Float, @thin Float.Type) -> Float
+// CHECK-SIL:   [[MULT_FUNC_JVP_1:%.*]] = function_ref @$sSf12_jvpMultiply3lhs3rhsSf5value_S2f_Sftc12differentialtSf_SftFZ : $@convention(method) (Float, Float, @thin Float.Type) -> (Float, @owned @callee_guaranteed (Float, Float) -> Float)
+// CHECK-SIL:   [[MULT_FUNC_VJP_1:%.*]] = function_ref @$sSf12_vjpMultiply3lhs3rhsSf5value_Sf_SftSfc8pullbacktSf_SftFZ : $@convention(method) (Float, Float, @thin Float.Type) -> (Float, @owned @callee_guaranteed (Float) -> (Float, Float))
+// CHECK-SIL:   [[AUTODIFF_INST_1:%.*]] = autodiff_function [wrt 0 1] [order 1] [[MULT_FUNC_1]] : $@convention(method) (Float, Float, @thin Float.Type) -> Float with {[[MULT_FUNC_JVP_1]] : $@convention(method) (Float, Float, @thin Float.Type) -> (Float, @owned @callee_guaranteed (Float, Float) -> Float), [[MULT_FUNC_VJP_1]] : $@convention(method) (Float, Float, @thin Float.Type) -> (Float, @owned @callee_guaranteed (Float) -> (Float, Float))}
+// CHECK-SIL:   [[MULT_JVP_APPLY_TUPLE_1:%.*]] = apply [[MULT_FUNC_JVP_1]]([[X_ARG]], [[X_ARG]], %3) : $@convention(method) (Float, Float, @thin Float.Type) -> (Float, @owned @callee_guaranteed (Float, Float) -> Float)
+// CHECK-SIL:   release_value [[AUTODIFF_INST_1]] : $@differentiable @convention(method) (Float, Float, @nondiff @thin Float.Type) -> Float
+// CHECK-SIL:   [[ORIGINAL_VAL_1:%.*]] = tuple_extract [[MULT_JVP_APPLY_TUPLE_1]] : $(Float, @callee_guaranteed (Float, Float) -> Float), 0
+// CHECK-SIL:   [[MULT_DIFFERENTIAL_1:%.*]] = tuple_extract [[MULT_JVP_APPLY_TUPLE_1]] : $(Float, @callee_guaranteed (Float, Float) -> Float), 1
+// CHECK-SIL:   [[MULT_FUNC_2:%.*]] = function_ref @$sSf1moiyS2f_SftFZ : $@convention(method) (Float, Float, @thin Float.Type) -> Float
+// CHECK-SIL:   retain_value [[MULT_FUNC_2]] : $@convention(method) (Float, Float, @thin Float.Type) -> Float
+// CHECK-SIL:   [[MULT_FUNC_JVP_2:%.*]] = function_ref @$sSf12_jvpMultiply3lhs3rhsSf5value_S2f_Sftc12differentialtSf_SftFZ : $@convention(method) (Float, Float, @thin Float.Type) -> (Float, @owned @callee_guaranteed (Float, Float) -> Float)
+// CHECK-SIL:   [[MULT_FUNC_VJP_2:%.*]] = function_ref @$sSf12_vjpMultiply3lhs3rhsSf5value_Sf_SftSfc8pullbacktSf_SftFZ : $@convention(method) (Float, Float, @thin Float.Type) -> (Float, @owned @callee_guaranteed (Float) -> (Float, Float))
+// CHECK-SIL:   [[AUTODIFF_INST_2:%.*]] = autodiff_function [wrt 0 1] [order 1] [[MULT_FUNC_2]] : $@convention(method) (Float, Float, @thin Float.Type) -> Float with {[[MULT_FUNC_JVP_2]] : $@convention(method) (Float, Float, @thin Float.Type) -> (Float, @owned @callee_guaranteed (Float, Float) -> Float), [[MULT_FUNC_VJP_2]] : $@convention(method) (Float, Float, @thin Float.Type) -> (Float, @owned @callee_guaranteed (Float) -> (Float, Float))}
+// CHECK-SIL:   [[MULT_JVP_APPLY_TUPLE_2:%.*]] = apply [[MULT_FUNC_JVP_2]]([[ORIGINAL_VAL_1]], [[X_ARG]], %2) : $@convention(method) (Float, Float, @thin Float.Type) -> (Float, @owned @callee_guaranteed (Float, Float) -> Float)
+// CHECK-SIL:   release_value [[AUTODIFF_INST_2]] : $@differentiable @convention(method) (Float, Float, @nondiff @thin Float.Type) -> Float
+// CHECK-SIL:   [[ORIGINAL_VAL_2:%.*]] = tuple_extract [[MULT_JVP_APPLY_TUPLE_2]] : $(Float, @callee_guaranteed (Float, Float) -> Float), 0
+// CHECK-SIL:   [[MULT_DIFFERENTIAL_2:%.*]] = tuple_extract [[MULT_JVP_APPLY_TUPLE_2]] : $(Float, @callee_guaranteed (Float, Float) -> Float), 1
+// CHECK-SIL:   [[DIFF_STRUCT:%.*]] = struct $_AD__unary_bb0__DF__src_0_wrt_0 ([[MULT_DIFFERENTIAL_1]] : $@callee_guaranteed (Float, Float) -> Float, [[MULT_DIFFERENTIAL_2]] : $@callee_guaranteed (Float, Float) -> Float)
+// CHECK-SIL:   [[UNARY_DIFFERENTIAL:%.*]] = function_ref @AD__unary__differential_src_0_wrt_0 : $@convention(thin) (Float, @guaranteed _AD__unary_bb0__DF__src_0_wrt_0) -> Float
+// CHECK-SIL:   [[PARTIAL_APP_DIFFERENTIAL:%.*]] = partial_apply [callee_guaranteed] [[UNARY_DIFFERENTIAL]]([[DIFF_STRUCT]]) : $@convention(thin) (Float, @guaranteed _AD__unary_bb0__DF__src_0_wrt_0) -> Float
+// CHECK-SIL:   [[TUPLE_RESULT:%.*]] = tuple ([[ORIGINAL_VAL_2]] : $Float, [[PARTIAL_APP_DIFFERENTIAL]] : $@callee_guaranteed (Float) -> Float)
+// CHECK-SIL:   return [[TUPLE_RESULT]] : $(Float, @callee_guaranteed (Float) -> Float)
+
+// CHECK-SIL-LABEL: sil hidden @AD__unary__differential_src_0_wrt_0 : $@convention(thin) (Float, @guaranteed _AD__unary_bb0__DF__src_0_wrt_0) -> Float {
+// CHECK-SIL: bb0([[X_TAN:%.*]] : $Float, [[DIFF_STRUCT:%.*]] : $_AD__unary_bb0__DF__src_0_wrt_0):
+// CHECK-SIL:   retain_value [[X_TAN]] : $Float
+// CHECK-SIL:   [[MULT_DIFF_1:%.*]] = struct_extract [[DIFF_STRUCT]] : $_AD__unary_bb0__DF__src_0_wrt_0, #_AD__unary_bb0__DF__src_0_wrt_0.differential_0
+// CHECK-SIL:   [[TEMP_TAN_1:%.*]] = apply [[MULT_DIFF_1]]([[X_TAN]], [[X_TAN]]) : $@callee_guaranteed (Float, Float) -> Float
+// CHECK-SIL:   [[MULT_DIFF_2:%.*]] = struct_extract [[DIFF_STRUCT]] : $_AD__unary_bb0__DF__src_0_wrt_0, #_AD__unary_bb0__DF__src_0_wrt_0.differential_1
+// CHECK-SIL:   [[TAN_RESULT:%.*]] = apply [[MULT_DIFF_2]]([[TEMP_TAN_1]], [[X_TAN]]) : $@callee_guaranteed (Float, Float) -> Float
+// CHECK-SIL:   release_value [[TEMP_TAN_1]] : $Float
+// CHECK-SIL:   release_value [[X_TAN]] : $Float
+// CHECK-SIL:   return [[TAN_RESULT]] : $Float
+
+//===----------------------------------------------------------------------===//
+// Binary
+//===----------------------------------------------------------------------===//
+
+@differentiable
+@_silgen_name("binary")
+func binary(x: Float, y: Float) -> Float {
+  return x * y
+}
+
+// CHECK-DATA-STRUCTURES: struct _AD__binary_bb0__DF__src_0_wrt_0_1 {
+// CHECK-DATA-STRUCTURES:   @_hasStorage var differential_0: (Float, Float) -> Float { get set }
+// CHECK-DATA-STRUCTURES: }
+// CHECK-DATA-STRUCTURES: enum _AD__binary_bb0__Succ__src_0_wrt_0_1 {
+// CHECK-DATA-STRUCTURES: }
+
+// CHECK-SIL-LABEL: sil hidden @AD__binary__jvp_src_0_wrt_0_1 : $@convention(thin) (Float, Float) -> (Float, @owned @callee_guaranteed (Float, Float) -> Float) {
+// CHECK-SIL: bb0([[X_ARG:%.*]] : $Float, [[Y_ARG:%.*]] : $Float):
+// CHECK-SIL:   [[MULT_FUNC:%.*]] = function_ref @$sSf1moiyS2f_SftFZ : $@convention(method) (Float, Float, @thin Float.Type) -> Float
+// CHECK-SIL:   retain_value [[MULT_FUNC]] : $@convention(method) (Float, Float, @thin Float.Type) -> Float
+// CHECK-SIL:   [[MULT_FUNC_JVP:%.*]] = function_ref @$sSf12_jvpMultiply3lhs3rhsSf5value_S2f_Sftc12differentialtSf_SftFZ : $@convention(method) (Float, Float, @thin Float.Type) -> (Float, @owned @callee_guaranteed (Float, Float) -> Float)
+// CHECK-SIL:   [[MULT_FUNC_VJP:%.*]] = function_ref @$sSf12_vjpMultiply3lhs3rhsSf5value_Sf_SftSfc8pullbacktSf_SftFZ : $@convention(method) (Float, Float, @thin Float.Type) -> (Float, @owned @callee_guaranteed (Float) -> (Float, Float))
+// CHECK-SIL:   [[AUTODIFF_INST:%.*]] = autodiff_function [wrt 0 1] [order 1] [[MULT_FUNC]] : $@convention(method) (Float, Float, @thin Float.Type) -> Float with {[[MULT_FUNC_JVP]] : $@convention(method) (Float, Float, @thin Float.Type) -> (Float, @owned @callee_guaranteed (Float, Float) -> Float), [[MULT_FUNC_VJP]] : $@convention(method) (Float, Float, @thin Float.Type) -> (Float, @owned @callee_guaranteed (Float) -> (Float, Float))}
+// CHECK-SIL:   [[MULT_JVP_APPLY_TUPLE:%.*]] = apply [[MULT_FUNC_JVP]]([[X_ARG]], [[Y_ARG]], %4) : $@convention(method) (Float, Float, @thin Float.Type) -> (Float, @owned @callee_guaranteed (Float, Float) -> Float)
+// CHECK-SIL:   release_value [[AUTODIFF_INST]] : $@differentiable @convention(method) (Float, Float, @nondiff @thin Float.Type) -> Float
+// CHECK-SIL:   [[ORIGINAL_VAL:%.*]] = tuple_extract [[MULT_JVP_APPLY_TUPLE]] : $(Float, @callee_guaranteed (Float, Float) -> Float), 0
+// CHECK-SIL:   [[MULT_DIFFERENTIAL:%.*]] = tuple_extract [[MULT_JVP_APPLY_TUPLE]] : $(Float, @callee_guaranteed (Float, Float) -> Float), 1
+// CHECK-SIL:   [[DIFF_STRUCT:%.*]] = struct $_AD__binary_bb0__DF__src_0_wrt_0_1 ([[MULT_DIFFERENTIAL]] : $@callee_guaranteed (Float, Float) -> Float)
+// CHECK-SIL:   [[BINARY_DIFFERENTIAL:%.*]] = function_ref @AD__binary__differential_src_0_wrt_0_1 : $@convention(thin) (Float, Float, @guaranteed _AD__binary_bb0__DF__src_0_wrt_0_1) -> Float
+// CHECK-SIL:   [[PARTIAL_APP_DIFFERENTIAL:%.*]] = partial_apply [callee_guaranteed] [[BINARY_DIFFERENTIAL]]([[DIFF_STRUCT]]) : $@convention(thin) (Float, Float, @guaranteed _AD__binary_bb0__DF__src_0_wrt_0_1) -> Float
+// CHECK-SIL:   [[TUPLE_RESULT:%.*]] = tuple ([[ORIGINAL_VAL]] : $Float, [[PARTIAL_APP_DIFFERENTIAL]] : $@callee_guaranteed (Float, Float) -> Float)
+// CHECK-SIL:   return [[TUPLE_RESULT]] : $(Float, @callee_guaranteed (Float, Float) -> Float)
+
+// CHECK-SIL-LABEL: sil hidden @AD__binary__differential_src_0_wrt_0_1 : $@convention(thin) (Float, Float, @guaranteed _AD__binary_bb0__DF__src_0_wrt_0_1) -> Float {
+// CHECK-SIL: bb0([[X_TAN:%.*]] : $Float, [[Y_TAN:%.*]] : $Float, [[DIFF_STRUCT:%.*]] : $_AD__binary_bb0__DF__src_0_wrt_0_1):
+// CHECK-SIL:   retain_value [[X_TAN]] : $Float
+// CHECK-SIL:   retain_value [[Y_TAN]] : $Float
+// CHECK-SIL:   [[MULT_DIFF:%.*]] = struct_extract [[DIFF_STRUCT]] : $_AD__binary_bb0__DF__src_0_wrt_0_1, #_AD__binary_bb0__DF__src_0_wrt_0_1.differential_0
+// CHECK-SIL:   [[TAN_RESULT:%.*]] = apply [[MULT_DIFF]]([[X_TAN]], [[Y_TAN]]) : $@callee_guaranteed (Float, Float) -> Float
+// CHECK-SIL:   release_value [[X_TAN]] : $Float
+// CHECK-SIL:   release_value [[Y_TAN]] : $Float
+// CHECK-SIL:   return [[TAN_RESULT]] : $Float
