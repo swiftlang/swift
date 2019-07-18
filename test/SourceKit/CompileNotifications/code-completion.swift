44cd3ff33d6be5c5a8b35d7fe7c201322187c573
Gwen Mittertreiner <gwen.mittertreiner@gmail.com>
2019-07-17 17:44:07 -0700

    Handle Windows Paths in CompileNotifications Tests
    On Windows, JSON output creates paths with `\\` separators, so pass
    --enable-yaml-compatibility to handle it properly.

--- test/SourceKit/CompileNotifications/code-completion.swift
+++ test/SourceKit/CompileNotifications/code-completion.swift
@@ -1,4 +1,4 @@
-// RUN: %sourcekitd-test -req=track-compiles == -req=complete %s -offset=0 -- %s | %FileCheck %s -check-prefix=COMPILE_1
+// RUN: %sourcekitd-test -req=track-compiles == -req=complete %s -offset=0 -- %s | %FileCheck %s -check-prefix=COMPILE_1 --enable-yaml-compatibility
 // COMPILE_1: {
 // COMPILE_1:  key.notification: source.notification.compile-will-start,
 // COMPILE_1:  key.filepath: "SOURCE_DIR{{.*}}code-completion.swift",
