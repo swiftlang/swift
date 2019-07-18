30788eca5958a64648e4a20e9cb5c1653d94c7bf
Gwen Mittertreiner <gwen.mittertreiner@gmail.com>
2019-07-17 17:46:35 -0700

    [Windows] Create symlinks properly in lit tests
    The mingw `ln -s` doesn't make a proper symlink on Windows. Instead add a
    script to use `cmd.exe /C mklink ...` on Windows.

--- test/SourceKit/Sema/sema_symlink.swift
+++ test/SourceKit/Sema/sema_symlink.swift
@@ -1,6 +1,6 @@
 // RUN: %empty-directory(%t.dir)
 // RUN: echo "let foo: Int = goo" > %t.dir/real.swift
-// RUN: ln -s %t.dir/real.swift %t.dir/linked.swift
+// RUN: %{python} %S/../../Inputs/symlink.py %t.dir/real.swift %t.dir/linked.swift
 // RUN: %sourcekitd-test -req=sema %t.dir/linked.swift -- %t.dir/real.swift | %sed_clean > %t.link.response
 // RUN: diff -u %s.response %t.link.response
 // RUN: %sourcekitd-test -req=sema %t.dir/real.swift -- %t.dir/linked.swift | %sed_clean > %t.real.response
