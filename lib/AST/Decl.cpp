118a557304464a242335aac0b24e75a1d8c3c7de
Doug Gregor <dgregor@apple.com>
2019-07-19 10:26:26 -0700

    [SE-0258] Check for a NULL type.
    Within invalid code, we might encounter expressions without type
    information yet. Check for NULL here.
    
    Fixes crash from rdar://problem/53120878.

--- lib/AST/Decl.cpp
+++ lib/AST/Decl.cpp
@@ -5905,7 +5905,8 @@ Expr *swift::findOriginalPropertyWrapperInitialValue(VarDecl *var,
 
         // ... producing a value of the same nominal type as the innermost
         // property wrapper.
-        if (call->getType()->getAnyNominal() != innermostNominal)
+        if (!call->getType() ||
+            call->getType()->getAnyNominal() != innermostNominal)
           return { true, E };
 
         // Find the implicit initialValue argument.
