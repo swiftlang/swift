b76169c6e08e4faa999b1f8a136a76f1d0660175
Keita Ito <keitaitok@gmail.com>
2019-07-21 12:21:20 -0700

    Update the PR by addressing feedback
    - Re-name benchmark names
    - Remove setupLargeManagedString()
    - Remove unnecessary optimization barrier functions
    - Update benchmark argument string by using smallString and largeString
    - Remove unnecessary optimization barrier function calls
    - Update replaceSubrange(_:_:with) to be generic function

--- benchmark/single-source/StringReplaceSubrange.swift
+++ benchmark/single-source/StringReplaceSubrange.swift
@@ -16,60 +16,51 @@ let tags: [BenchmarkCategory] = [.validation, .api, .String]
 
 public let StringReplaceSubrange = [
   BenchmarkInfo(
-    name: "Str.replaceSubrange.SmallLiteral.String",
-    runFunction: { replaceSubrange($0, "coffee", with: "t") },
+    name: "String.replaceSubrange.String.Small",
+    runFunction: { replaceSubrange($0, smallString, with: "t") },
     tags: tags
   ),
   BenchmarkInfo(
-    name: "Str.replaceSubrange.LargeManaged.String",
-    runFunction: { replaceSubrange($0, largeManagedString, with: "t") },
-    tags: tags,
-    setUpFunction: setupLargeManagedString
-  ),
-  BenchmarkInfo(
-    name: "Str.replaceSubrange.SmallLiteral.Substr",
-    runFunction: { replaceSubrange($0, "coffee", with: getSubstring("t")) },
+    name: "String.replaceSubrange.String",
+    runFunction: { replaceSubrange($0, largeString, with: "t") },
     tags: tags
   ),
   BenchmarkInfo(
-    name: "Str.replaceSubrange.LargeManaged.Substr",
-    runFunction: { replaceSubrange($0, largeManagedString, with: getSubstring("t")) },
-    tags: tags,
-    setUpFunction: setupLargeManagedString
-  ),
-  BenchmarkInfo(
-    name: "Str.replaceSubrange.SmallLiteral.ArrChar",
-    runFunction: { replaceSubrange($0, "coffee", with: getArrayCharacter(Array<Character>(["t"]))) },
+    name: "String.replaceSubrange.Substring.Small",
+    runFunction: { replaceSubrange($0, smallString, with: "t"[...]) },
     tags: tags
   ),
   BenchmarkInfo(
-    name: "Str.replaceSubrange.LargeManaged.ArrChar",
-    runFunction: { replaceSubrange($0, largeManagedString, with: getArrayCharacter(Array<Character>(["t"]))) },
-    tags: tags,
-    setUpFunction: setupLargeManagedString
-  ),
-  BenchmarkInfo(
-    name: "Str.replaceSubrange.SmallLiteral.RepeatedChar",
-    runFunction: { replaceSubrange($0, "coffee", with: getRepeatedCharacter(repeatedCharacter)) },
+    name: "String.replaceSubrange.Substring",
+    runFunction: { replaceSubrange($0, largeString, with: "t"[...]) },
     tags: tags
   ),
   BenchmarkInfo(
-    name: "Str.replaceSubrange.LargeManaged.RepeatedChar",
-    runFunction: { replaceSubrange($0, largeManagedString, with: getRepeatedCharacter(repeatedCharacter)) },
-    tags: tags,
-    setUpFunction: setupLargeManagedString
+    name: "String.replaceSubrange.ArrChar.Small",
+    runFunction: { replaceSubrange($0, smallString, with: Array<Character>(["t"])) },
+    tags: tags
+  ),
+  BenchmarkInfo(
+    name: "String.replaceSubrange.ArrChar",
+    runFunction: { replaceSubrange($0, largeString, with: Array<Character>(["t"])) },
+    tags: tags
+  ),
+  BenchmarkInfo(
+    name: "String.replaceSubrange.RepChar.Small",
+    runFunction: { replaceSubrange($0, smallString, with: repeatedCharacter) },
+    tags: tags
+  ),
+  BenchmarkInfo(
+    name: "String.replaceSubrange.RepChar",
+    runFunction: { replaceSubrange($0, largeString, with: repeatedCharacter) },
+    tags: tags
   ),
 ]
 
 // MARK: - Privates for String
 
-private var largeManagedString: String = {
-    return getString("coffee\u{301}coffeecoffeecoffeecoffee")
-}()
-
-private func setupLargeManagedString() {
-    _ = largeManagedString
-}
+private let smallString = "coffee"
+private let largeString = "coffee\u{301}coffeecoffeecoffeecoffee"
 
 // MARK: - Privates for Repeated<Character>
 
@@ -78,39 +69,12 @@ private let repeatedCharacter: Repeated<Character> = {
   return repeatElement(character, count: 1)
 }()
 
-
 @inline(never)
-private func replaceSubrange(_ N: Int, _ string: String, with replacingString: String) {
-  var copy = getString(string)
-  let range = string.startIndex..<string.index(after: string.startIndex)
-  for _ in 0 ..< 5_000 * N {
-    copy.replaceSubrange(range, with: replacingString)
-  }
-}
-
-@inline(never)
-private func replaceSubrange(_ N: Int, _ string: String, with replacingSubstring: Substring) {
-  var copy = getString(string)
-  let range = string.startIndex..<string.index(after: string.startIndex)
-  for _ in 0 ..< 5_000 * N {
-    copy.replaceSubrange(range, with: replacingSubstring)
-  }
-}
-
-@inline(never)
-private func replaceSubrange(_ N: Int, _ string: String, with replacingArrayCharacter: Array<Character>) {
-  var copy = getString(string)
-  let range = string.startIndex..<string.index(after: string.startIndex)
-  for _ in 0 ..< 5_000 * N {
-    copy.replaceSubrange(range, with: replacingArrayCharacter)
-  }
-}
-
-@inline(never)
-private func replaceSubrange(_ N: Int, _ string: String, with replacingRepeatedCharacter: Repeated<Character>) {
+private func replaceSubrange<C: Collection>(_ N: Int, _ string: String, with newElements: C)
+  where C.Element == Character {
     var copy = getString(string)
     let range = string.startIndex..<string.index(after: string.startIndex)
     for _ in 0 ..< 5_000 * N {
-        copy.replaceSubrange(range, with: replacingRepeatedCharacter)
+      copy.replaceSubrange(range, with: newElements)
     }
 }
