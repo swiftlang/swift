732326328f91394390eb2104fdf4109ece686e20
Xi Ge <xi_ge@apple.com>
2019-07-23 16:15:08 -0700

    AST: avoid unnecessarily calling getStartLoc() to avoid exponential growth of the stack trace
    Issuing multiple getStartLoc() from sub-expression can exponentially grow the stack trace.
    When the expression under analysis is complex enough, this could be a user-noticeable hang.
    This patch fixes UnresolvedDotExpr::getStartLoc() by 'refactoring' the result of SubExpr->getStartLoc()
    to a local variable.
    
    rdar://52982457

--- include/swift/AST/Expr.h
+++ include/swift/AST/Expr.h
@@ -2387,8 +2387,9 @@ public:
   }
 
   SourceLoc getStartLoc() const {
-    if (SubExpr->getStartLoc().isValid())
-      return SubExpr->getStartLoc();
+    auto SubLoc = SubExpr->getStartLoc();
+    if (SubLoc.isValid())
+      return SubLoc;
     else if (DotLoc.isValid())
       return DotLoc;
     else
