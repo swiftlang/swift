0d7049ecb19f3ed1442cafe17c64a5c189d6a5ed
Michael Ilseman <michael.ilseman@gmail.com>
2019-07-25 17:18:05 -0700

    [stdlib] Introduce _invariantCheck_5_1 for 5.1 and later assertions. (#26126)
    Inlinable and non-inlinable code can cause 5.1 code to intermix with
    5.0 code on older OSes. Some (weak) invariants for 5.1 should only be
    checked when the OS's code is 5.1 or later, which is the purpose of
    _invariantCheck_5_1.
    
    Applied to String.Index._isScalarAligned, which is a new bit
    introduced in 5.1 from one of the reserved bits from 5.0. The bit is
    set when the index is proven to be scalar aligned, and we want to
    assert on this liberally in contexts where we expect it to be
    so. However, older OSes might not set this bit when doing scalar
    aligning, depending on exactly what got inlined where/when.

--- stdlib/public/core/Assert.swift
+++ stdlib/public/core/Assert.swift
@@ -297,6 +297,22 @@ internal func _internalInvariant(
 #endif
 }
 
+// Only perform the invariant check on Swift 5.1 and later
+@_alwaysEmitIntoClient // Swift 5.1
+@_transparent
+internal func _internalInvariant_5_1(
+  _ condition: @autoclosure () -> Bool, _ message: StaticString = StaticString(),
+  file: StaticString = #file, line: UInt = #line
+) {
+#if INTERNAL_CHECKS_ENABLED
+  // FIXME: The below won't run the assert on 5.1 stdlib if testing on older
+  // OSes, which means that testing may not test the assertion. We need a real
+  // solution to this.
+  guard #available(macOS 10.15, iOS 13, tvOS 13, watchOS 6, *) else { return }
+  _internalInvariant(condition(), message, file: file, line: line)
+#endif
+}
+
 @usableFromInline @_transparent
 internal func _internalInvariantFailure(
   _ message: StaticString = StaticString(),
