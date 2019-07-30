02efd0a54c7e0cdcfd9857a9e2ada69bbaa0e395
Steve (Numerics) Canon <scanon@apple.com>
2019-07-30 10:11:40 -0400

    Add examples to documentation for FloatingPoint.isCanonical
    In particular, document that subnormal encodings are treated as non-canonical zeros on platforms that flush to zero.

--- stdlib/public/core/FloatingPoint.swift
+++ stdlib/public/core/FloatingPoint.swift
@@ -1197,13 +1197,27 @@ public protocol FloatingPoint: SignedNumeric, Strideable, Hashable
   var floatingPointClass: FloatingPointClassification { get }
 
   /// A Boolean value indicating whether the instance's representation is in
-  /// the canonical form.
+  /// its canonical form.
   ///
   /// The [IEEE 754 specification][spec] defines a *canonical*, or preferred,
-  /// encoding of a floating-point value's representation. Every `Float` or
-  /// `Double` value is canonical, but noncanonical values of the `Float80`
-  /// type exist, and noncanonical values may exist for other types that
-  /// conform to the `FloatingPoint` protocol.
+  /// encoding of a floating-point value. On platforms that fully support
+  /// IEEE 754, every `Float` or `Double` value is canonical, but
+  /// non-canonical values can exist on other platforms or for other types.
+  /// Some examples:
+  ///
+  /// - On platforms that flush subnormal numbers to zero (such as armv7
+  ///   with the default floating-point environment), Swift interprets
+  ///   subnormal `Float` and `Double` values as non-canonical zeros.
+  ///   (In Swift 5.1 and earlier, isCanonical returns `true` for these
+  ///   values. This is a bug.)
+  ///
+  /// - On i386 and x86_64, `Float80` has a number of non-canonical
+  ///   encodings. "Pseudo-NaNs", "pseudo-infinities", and "unnormals" are
+  ///   interpreted as non-canonical NaN encodings. "Pseudo-denormals" are
+  ///   interpreted as non-canonical encodings of subnormal values.
+  ///
+  /// - Decimal floating-point admits a large number of non-canonical
+  ///   encodings. Consult the IEEE 754 standard for additional details.
   ///
   /// [spec]: http://ieeexplore.ieee.org/servlet/opac?punumber=4610933
   var isCanonical: Bool { get }
