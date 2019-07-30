090b74055035a67374e502c49d108741bbb246bd
Arnold Schwaighofer <aschwaighofer@apple.com>
2019-07-30 09:26:08 -0700

    Sema: Don't mark the accessors of the storage wrapper transparent if its backing var has lower visibility than the var
    rdar://53574404

--- lib/Sema/CodeSynthesis.cpp
+++ lib/Sema/CodeSynthesis.cpp
@@ -352,7 +352,8 @@ static void maybeMarkTransparent(AccessorDecl *accessor, ASTContext &ctx) {
 
       if (auto original = var->getOriginalWrappedProperty(
               PropertyWrapperSynthesizedPropertyKind::StorageWrapper)) {
-        if (var->getFormalAccess() < original->getFormalAccess())
+        auto backingVar = original->getPropertyWrapperBackingProperty();
+        if (backingVar->getFormalAccess() < var->getFormalAccess())
           return;
       }
     }
