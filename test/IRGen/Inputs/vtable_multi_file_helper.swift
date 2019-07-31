8bdbc5c793eedd75085ff662c783c3f4e8eb7a8e
Slava Pestov <spestov@apple.com>
2019-07-31 14:47:37 -0400

    Sema: Request member layout for Sequence and IteratorProtocol witnesses used by ForEachStmt

--- test/IRGen/Inputs/vtable_multi_file_helper.swift
+++ test/IRGen/Inputs/vtable_multi_file_helper.swift
@@ -2,3 +2,23 @@ class EmptyBase {}
 class Subclass : EmptyBase {
   class var classProp: String { return "abc" }
 }
+
+class SillySequence : Sequence, IteratorProtocol {
+  typealias Element = Int
+
+  var storedProperty: Int = 0
+
+  func makeIterator() -> SillySequence {
+    return sel
+  }
+
+  func next() -> Int? {
+    return nil
+  }
+}
+
+class Holder {
+  func getSillySequence() -> SillySequence {
+    return SillySequence()
+  }
+}
