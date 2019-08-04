e7a1a1cc3d0222f2fb8090db0c4fd20e93ca13db
Owen Voorhees <owen@squibner.com>
2019-08-03 19:11:26 -0700

    Add -debug-diagnostic-names frontend flag
    This flag adds diagnostic names to the end of their messages, e.g. 'error: cannot convert value of type '[Any]' to specified type '[Int]' [cannot_convert_initializer_value]'. It's intended to be used for debugging purposes when working on the compiler.

--- lib/Frontend/Frontend.cpp
+++ lib/Frontend/Frontend.cpp
@@ -305,6 +305,9 @@ void CompilerInstance::setUpDiagnosticOptions() {
   if (Invocation.getDiagnosticOptions().WarningsAsErrors) {
     Diagnostics.setWarningsAsErrors(true);
   }
+  if (Invocation.getDiagnosticOptions().PrintDiagnosticNames) {
+    Diagnostics.setPrintDiagnosticNames(true);
+  }
 }
 
 bool CompilerInstance::setUpModuleLoaders() {
