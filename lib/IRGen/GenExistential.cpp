swiftlang/swift

commit bb2740e540a4679c26d80d9e58d29ef50a38349f

IRGen: fix enum bit packing on big-endian platforms.
This change modifies spare bit masks so that they are arranged in
the byte order of the target platform. It also modifies and
consolidates the code that gathers and scatters bits into enum
values.

All enum-related validation tests are now passing on IBM Z (s390x)
which is a big-endian platform.

lib/IRGen/GenExistential.cpp
@@ -30,6 +30,7 @@
 #include "llvm/IR/Module.h"
 #include "llvm/Support/raw_ostream.h"
 
+#include "BitPatternBuilder.h"
 #include "EnumPayload.h"
 #include "Explosion.h"
 #include "FixedTypeInfo.h"
@@ -583,8 +584,11 @@ namespace {
                               .getFixedExtraInhabitantMask(IGM);
 
       // Zext out to the size of the existential.
-      bits = bits.zextOrTrunc(asDerived().getFixedSize().getValueInBits());
-      return bits;
+      auto totalSize = asDerived().getFixedSize().getValueInBits();
+      auto mask = BitPatternBuilder(IGM.Triple.isLittleEndian());
+      mask.append(bits);
+      mask.padWithClearBitsTo(totalSize);
+      return mask.build().getValue();
     }
   };
 
@@ -649,8 +653,10 @@ namespace {
                                                      ReferenceOwnership::Name, \
                                                      Refcounting); \
         /* Zext out to the size of the existential. */ \
-        bits = bits.zextOrTrunc(getFixedSize().getValueInBits()); \
-        return bits; \
+        auto mask = BitPatternBuilder(IGM.Triple.isLittleEndian()); \
+        mask.append(bits); \
+        mask.padWithClearBitsTo(getFixedSize().getValueInBits()); \
+        return mask.build().getValue(); \
       } else { \
         return Super::getFixedExtraInhabitantMask(IGM); \
       } \
@@ -922,10 +928,11 @@ public:
     return getHeapObjectFixedExtraInhabitantValue(IGM, bits, index, offset);
   }
   APInt getFixedExtraInhabitantMask(IRGenModule &IGM) const override {
-    auto mask = APInt::getAllOnesValue(IGM.getPointerSize().getValueInBits());
-    mask = mask.zext(getFixedSize().getValueInBits());
-    mask = mask.shl(getLayout().getMetadataRefOffset(IGM).getValueInBits());
-    return mask;
+    auto mask = BitPatternBuilder(IGM.Triple.isLittleEndian());
+    mask.appendClearBits(getLayout().getMetadataRefOffset(IGM).getValueInBits());
+    mask.appendSetBits(IGM.getPointerSize().getValueInBits());
+    mask.padWithClearBitsTo(getFixedSize().getValueInBits());
+    return mask.build().getValue();
   }
   llvm::Value *getExtraInhabitantIndex(IRGenFunction &IGF,
                                        Address src, SILType T,
@@ -1135,17 +1142,20 @@ public:
   const TypeInfo * \
   create##Name##StorageType(TypeConverter &TC, \
                             bool isOptional) const override { \
-    auto spareBits = TC.IGM.getReferenceStorageSpareBits( \
-                                                     ReferenceOwnership::Name, \
-                                                     Refcounting); \
-    for (unsigned i = 0, e = getNumStoredProtocols(); i != e; ++i) \
+    auto spareBits = BitPatternBuilder(TC.IGM.Triple.isLittleEndian()); \
+    auto ref = TC.IGM.getReferenceStorageSpareBits( \
+                                                   ReferenceOwnership::Name, \
+                                                   Refcounting); \
+    spareBits.append(ref); \
+    for (unsigned i = 0, e = getNumStoredProtocols(); i != e; ++i) { \
       spareBits.append(TC.IGM.getWitnessTablePtrSpareBits()); \
+    } \
     auto storageTy = buildReferenceStorageType(TC.IGM, \
                               TC.IGM.Name##ReferencePtrTy->getElementType()); \
     return AddressOnly##Name##ClassExistentialTypeInfo::create( \
                                                  getStoredProtocols(), \
                                                  storageTy, \
-                                                 std::move(spareBits), \
+                                                 spareBits.build(), \
                                                  getFixedSize(), \
                                                  getFixedAlignment(), \
                                                  Refcounting, \
@@ -1157,18 +1167,21 @@ public:
   const TypeInfo * \
   create##Name##StorageType(TypeConverter &TC, \
                             bool isOptional) const override { \
-    auto spareBits = TC.IGM.getReferenceStorageSpareBits( \
-                                                     ReferenceOwnership::Name, \
-                                                     Refcounting); \
-    for (unsigned i = 0, e = getNumStoredProtocols(); i != e; ++i) \
+    auto ref = TC.IGM.getReferenceStorageSpareBits( \
+                                                   ReferenceOwnership::Name, \
+                                                   Refcounting); \
+    auto spareBits = BitPatternBuilder(TC.IGM.Triple.isLittleEndian()); \
+    spareBits.append(ref); \
+    for (unsigned i = 0, e = getNumStoredProtocols(); i != e; ++i) { \
       spareBits.append(TC.IGM.getWitnessTablePtrSpareBits()); \
+    } \
     auto storageTy = buildReferenceStorageType(TC.IGM, \
                               TC.IGM.Name##ReferencePtrTy->getElementType()); \
     if (TC.IGM.isLoadableReferenceAddressOnly(Refcounting)) { \
       return AddressOnly##Name##ClassExistentialTypeInfo::create( \
                                                    getStoredProtocols(), \
                                                    storageTy, \
-                                                   std::move(spareBits), \
+                                                   spareBits.build(), \
                                                    getFixedSize(), \
                                                    getFixedAlignment(), \
                                                    Refcounting, \
@@ -1178,7 +1191,7 @@ public:
                                                    getStoredProtocols(), \
                                                    getValueType(), \
                                                    storageTy, \
-                                                   std::move(spareBits), \
+                                                   spareBits.build(), \
                                                    getFixedSize(), \
                                                    getFixedAlignment(), \
                                                    Refcounting, \
@@ -1191,11 +1204,14 @@ public:
   create##Name##StorageType(TypeConverter &TC, \
                             bool isOptional) const override { \
     assert(Refcounting == ReferenceCounting::Native); \
-    auto spareBits = TC.IGM.getReferenceStorageSpareBits( \
+    auto ref = TC.IGM.getReferenceStorageSpareBits( \
                                                    ReferenceOwnership::Name, \
                                                    ReferenceCounting::Native); \
-    for (unsigned i = 0, e = getNumStoredProtocols(); i != e; ++i) \
+    auto spareBits = BitPatternBuilder(TC.IGM.Triple.isLittleEndian()); \
+    spareBits.append(ref); \
+    for (unsigned i = 0, e = getNumStoredProtocols(); i != e; ++i) { \
       spareBits.append(TC.IGM.getWitnessTablePtrSpareBits()); \
+    } \
     auto storageTy = buildReferenceStorageType(TC.IGM, \
                               TC.IGM.Name##ReferencePtrTy->getElementType()); \
     return Loadable##Name##ClassExistentialTypeInfo::create( \
@@ -1414,7 +1430,7 @@ static const TypeInfo *createExistentialTypeInfo(IRGenModule &IGM, CanType T) {
     Alignment align = IGM.getPointerAlignment();
     Size size = classFields.size() * IGM.getPointerSize();
 
-    SpareBitVector spareBits;
+    auto spareBits = BitPatternBuilder(IGM.Triple.isLittleEndian());
 
     // The class pointer is an unknown heap object, so it may be a tagged
     // pointer, if the platform has those.
@@ -1430,9 +1446,8 @@ static const TypeInfo *createExistentialTypeInfo(IRGenModule &IGM, CanType T) {
     for (unsigned i = 1, e = classFields.size(); i < e; ++i) {
       spareBits.append(IGM.getWitnessTablePtrSpareBits());
     }
-
     return ClassExistentialTypeInfo::create(protosWithWitnessTables, type,
-                                            size, std::move(spareBits), align,
+                                            size, spareBits.build(), align,
                                             refcounting);
   }
 
@@ -1446,23 +1461,11 @@ static const TypeInfo *createExistentialTypeInfo(IRGenModule &IGM, CanType T) {
   Size size = opaque.getSize(IGM);
   // There are spare bits in the metadata pointer and witness table pointers
   // consistent with a native object reference.
-  SpareBitVector spareBits;
-  spareBits.appendClearBits(size.getValueInBits());
-  /* TODO: There are spare bits we could theoretically use in the type metadata
-     and witness table pointers, but opaque existentials are currently address-
-     only, and we can't soundly take advantage of spare bits for in-memory
-     representations.
-   
-  auto metadataOffset = opaque.getMetadataRefOffset(IGM);
-  spareBits.appendClearBits(metadataOffset.getValueInBits());
-  auto typeSpareBits = IGM.getHeapObjectSpareBits();
-  spareBits.append(typeSpareBits);
-  auto witnessSpareBits =
-    IGM.getWitnessTablePtrSpareBits();
-  for (unsigned i = 0, e = protosWithWitnessTables.size(); i < e; ++i)
-    spareBits.append(witnessSpareBits);
-  assert(spareBits.size() == size.getValueInBits());
-   */
+  // TODO: There are spare bits we could theoretically use in the type metadata
+  // and witness table pointers, but opaque existentials are currently address-
+  // only, and we can't soundly take advantage of spare bits for in-memory
+  // representations.
+  auto spareBits = SpareBitVector::getConstant(size.getValueInBits(), false);
   return OpaqueExistentialTypeInfo::create(protosWithWitnessTables, type, size,
                                            std::move(spareBits),
                                            align);
@@ -1491,12 +1494,12 @@ TypeConverter::convertExistentialMetatypeType(ExistentialMetatypeType *T) {
   SmallVector<const ProtocolDecl *, 4> protosWithWitnessTables;
   SmallVector<llvm::Type*, 4> fields;
 
-  SpareBitVector spareBits;
-
   assert(T->getRepresentation() != MetatypeRepresentation::Thin &&
          "existential metatypes cannot have thin representation");
   auto &baseTI = cast<LoadableTypeInfo>(getMetatypeTypeInfo(T->getRepresentation()));
   fields.push_back(baseTI.getStorageType());
+
+  auto spareBits = BitPatternBuilder(IGM.Triple.isLittleEndian());
   spareBits.append(baseTI.getSpareBits());
 
   for (auto protoTy : layout.getProtocols()) {
@@ -1517,7 +1520,7 @@ TypeConverter::convertExistentialMetatypeType(ExistentialMetatypeType *T) {
   Alignment align = IGM.getPointerAlignment();
 
   return ExistentialMetatypeTypeInfo::create(protosWithWitnessTables, type,
-                                             size, std::move(spareBits), align,
+                                             size, spareBits.build(), align,
                                              baseTI);
 }
 

include/swift/Basic/ClusteredBitVector.h
@@ -61,6 +61,16 @@ public:
   ClusteredBitVector(ClusteredBitVector &&other)
     : Bits(std::move(other.Bits)) {}
 
+  /// Create a new ClusteredBitVector from the provided APInt,
+  /// with a size of 0 if the optional does not have a value.
+  ClusteredBitVector(const llvm::Optional<APInt> &bits)
+    : Bits(bits) {}
+
+  /// Create a new ClusteredBitVector from the provided APInt,
+  /// with a size of 0 if the optional does not have a value.
+  ClusteredBitVector(llvm::Optional<APInt> &&bits)
+    : Bits(std::move(bits)) {}
+
   ClusteredBitVector &operator=(const ClusteredBitVector &other) {
     this->Bits = other.Bits;
     return *this;

lib/IRGen/BitPatternBuilder.h
@@ -1,0 +1,170 @@
+//===--- BitPatternBuilder.h - Create masks for composite types -----------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2019 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+#include "swift/Basic/ClusteredBitVector.h"
+
+#include "llvm/ADT/APInt.h"
+#include "llvm/ADT/Optional.h"
+#include "llvm/ADT/SmallVector.h"
+
+namespace swift {
+namespace irgen {
+
+/// BitPatternBuilder is a class to help with the construction of
+/// bit masks for composite types. The class should be constructed
+/// using the byte order of the target platform. Elements added
+/// to the class must mask an entire element in a composite type.
+/// These elements must be byte aligned. For example, if you want
+/// to add a 64-bit integer to the bit pattern with the low 32
+/// bits set to 1 and the high 32 bits set to 0 then you must create
+/// a 64-bit APInt and append it in its entirety rather than calling
+/// the appendSetBits and appendClearBits helper functions which
+/// would not be portable across architectures using different byte
+/// orders.
+///
+/// Example construction of a mask for a struct:
+///
+///   // Type T that we are generating a mask for:
+///   struct T {
+///     uint8_t a;
+///     uint8_t b;
+///     uint16_t c;
+///   };
+///
+///   // Code to generate the mask:
+///   auto mask = BitPatternBuilder(isLittleEndian());
+///   mask.appendSetBits(8);             // mask T.a with 0xff
+///   mask.appendClearBits(8);           // mask T.b with 0x00
+///   mask.append(APInt(16, 0x1177ULL)); // mask T.c with 0x1177
+///
+///   // Little-endian result:
+///   mask.build(); // 0x117700ff [ ff 00 77 11 ]
+///
+///   // Big-endian result:
+///   mask.build(); // 0xff001177 [ ff 00 11 77 ]
+///
+class BitPatternBuilder {
+  using APInt = llvm::APInt;
+
+  // An array of masks that, when combined, will form the mask for a
+  // composite value. Generally these correspond to elements in a
+  // struct (or class, tuple etc.).
+  llvm::SmallVector<APInt, 8> Elements;
+
+  // Little-endian byte order implies that elements should be
+  // appended to the most significant bit. If this flag is false
+  // then elements should be appended to the least signficant
+  // bit (big-endian byte order).
+  bool LittleEndian;
+
+  // The combined size of the elements added so far in bits.
+  unsigned Size = 0;
+public:
+  /// Create a new BitPatternBuilder with either a little-endian
+  /// (true) or big-endian (false) byte order.
+  BitPatternBuilder(bool littleEndian) : LittleEndian(littleEndian) {}
+
+  /// Append the given mask to the bit pattern. The mask should mask
+  /// an entire element type and be byte aligned.
+  void append(const APInt &value) {
+    assert(value.getBitWidth() % 8 == 0);
+    Size += value.getBitWidth();
+    Elements.push_back(value);
+  }
+
+  /// Append the given mask to the bit pattern. The mask should mask
+  /// an entire element type and be byte aligned.
+  void append(APInt &&value) {
+    assert(value.getBitWidth() % 8 == 0);
+    Size += value.getBitWidth();
+    Elements.push_back(std::move(value));
+  }
+
+  /// Append the given mask to the bit pattern. The mask should mask
+  /// an entire element type and be byte aligned.
+  void append(const ClusteredBitVector &value) {
+    assert(value.size() % 8 == 0);
+    if (!value.empty()) {
+      Size += value.size();
+      Elements.push_back(value.asAPInt());
+    }
+  }
+
+  /// Append the given number of set (1) bits to the bit pattern. The
+  /// number of bits must be a multiple of 8 and mask a whole number
+  /// of element types.
+  void appendSetBits(unsigned numBits) {
+    assert(numBits % 8 == 0);
+    if (numBits) {
+      Size += numBits;
+      Elements.push_back(APInt::getAllOnesValue(numBits));
+    }
+  }
+
+  /// Append the given number of clear (0) bits to the bit pattern. The
+  /// number of bits must be a multiple of 8 and mask a whole number
+  /// of element types.
+  void appendClearBits(unsigned numBits) {
+    assert(numBits % 8 == 0);
+    if (numBits) {
+      Size += numBits;
+      Elements.push_back(APInt::getNullValue(numBits));
+    }
+  }
+
+  /// Append set (1) bits to the bit pattern until it reaches the
+  /// given size in bits. The total number of bits must be a
+  /// multiple of 8.
+  void padWithSetBitsTo(unsigned totalSizeInBits) {
+    assert(totalSizeInBits % 8 == 0);
+    assert(totalSizeInBits >= Size);
+    appendSetBits(totalSizeInBits - Size);
+  }
+
+  /// Append clear (0) bits to the bit pattern until it reaches the
+  /// given size in bits. The total number of bits must be a
+  /// multiple of 8.
+  void padWithClearBitsTo(unsigned totalSizeInBits) {
+    assert(totalSizeInBits % 8 == 0);
+    assert(totalSizeInBits >= Size);
+    appendClearBits(totalSizeInBits - Size);
+  }
+
+  /// Build the complete mask for the composite type. If the mask has a
+  /// length of 0 then the optional will not contain a value. Otherwise
+  /// the option will contain a value that is the combined length of
+  /// the elements appended to the builder. The mask represents is an
+  /// integer in the target byte order.
+  llvm::Optional<APInt> build() const {
+    if (Size == 0) {
+      return llvm::Optional<APInt>();
+    }
+    auto result = APInt::getNullValue(Size);
+    unsigned offset = 0;
+    for (const auto &e : Elements) {
+      unsigned index = offset;
+      if (!LittleEndian) {
+        index = Size - offset - e.getBitWidth();
+      }
+      result.insertBits(e, index);
+      offset += e.getBitWidth();
+    }
+    assert(offset == Size);
+    return result;
+  }
+};
+
+} // end namespace irgen
+} // end namespace swift
+

lib/IRGen/BitPatternReader.h
@@ -1,0 +1,82 @@
+//===--- BitPatternReader.h - Split bit patterns into chunks. -------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2019 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+#include "swift/Basic/ClusteredBitVector.h"
+
+#include "llvm/ADT/APInt.h"
+#include "llvm/ADT/Optional.h"
+
+namespace swift {
+namespace irgen {
+
+/// BitPatternReader allows an APInt to be read from in chunks.
+/// Chunks may be read starting from either the least significant bit
+/// (little-endian) or the most significant bit (big-endian).
+///
+/// This is useful when interpreting an APInt as a multi-byte mask
+/// that needs to be applied to a value with a composite type.
+///
+/// Example:
+///
+///   // big-endian
+///   auto x = BitPatternReader(APInt(32, 0x1234), false);
+///   x.read(16) // 0x12
+///   x.read(8)  // 0x3
+///   x.read(8)  // 0x4
+///
+///   // little-endian
+///   auto y = BitPatternReader(APInt(32, 0x1234), true);
+///   y.read(16) // 0x34
+///   y.read(8)  // 0x2
+///   y.read(8)  // 0x1
+///
+class BitPatternReader {
+  using APInt = llvm::APInt;
+
+  const APInt Value;
+  const bool LittleEndian;
+  unsigned Offset = 0;
+public:
+  /// If the reader is in little-endian mode then bits will be read
+  /// from the least significant to the most significant. Otherwise
+  /// they will be read from the most significant to the least
+  /// significant.
+  BitPatternReader(const APInt &value, bool littleEndian) :
+      Value(value),
+      LittleEndian(littleEndian) {}
+
+  /// Read the given number of bits from the unread part of the
+  /// underlying value and adjust the remaining value as appropriate.
+  APInt read(unsigned numBits) {
+    assert(numBits % 8 == 0);
+    assert(Value.getBitWidth() >= Offset + numBits);
+    unsigned offset = Offset;
+    if (!LittleEndian) {
+      offset = Value.getBitWidth() - offset - numBits;
+    }
+    Offset += numBits;
+    return Value.extractBits(numBits, offset);
+  }
+
+  // Skip the number of bits provided.
+  void skip(unsigned numBits) {
+    assert(numBits % 8 == 0);
+    assert(Value.getBitWidth() >= Offset + numBits);
+    Offset += numBits;
+  }
+};
+
+} // end namespace irgen
+} // end namespace swift
+

commit eb1c203cbf0a306cf084579f090b1ea6ebd55125

[IRGen] Fix a missing case of the new bit pattern builder
This fixes a downstream regression introduced by:
bb2740e540a4679c26d80d9e58d29ef50a38349f

Nothing in Swift proper uses this scenario.

lib/IRGen/GenExistential.cpp
@@ -1218,7 +1218,7 @@ public:
                                                   getStoredProtocols(), \
                                                   getValueType(), \
                                                   storageTy, \
-                                                  std::move(spareBits), \
+                                                  spareBits.build(), \
                                                   getFixedSize(), \
                                                   getFixedAlignment(), \
                                                   ReferenceCounting::Native, \