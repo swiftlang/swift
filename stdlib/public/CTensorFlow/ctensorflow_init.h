083af3784e1b369b787c7af9b6ba5814bdf489a2
Gogul Balakrishnan <bgogul@google.com>
2019-08-08 08:37:07 -0700

    Add a temporary interface function to migrate swift-apis. (#26496)

--- stdlib/public/CTensorFlow/ctensorflow_init.h
+++ stdlib/public/CTensorFlow/ctensorflow_init.h
@@ -41,6 +41,21 @@ void *swift_tfc_CreateFloatTensor(int32_t num_dims, int64_t *dims, float *vals,
 void *swift_tfc_CreateScalarStringTensor(char *val, int32_t valLen,
                                          TF_Status *status);
 
+struct TF_Tensor;
+struct TF_ShapeAndTypeList;
+struct TFE_Op;
+
+// We removed a redundant argument in the C shape inference API.
+// This interface function lets us migrate swift-apis without breakages.
+// (This is a temporary function and will be removed.)
+void TFE_InferShapes_Transition(
+  TFE_Op* op, TF_ShapeAndTypeList* input_shapes,
+  TF_Tensor** input_tensors,
+  TF_ShapeAndTypeList* input_tensor_as_shapes,
+  TF_ShapeAndTypeList** input_resource_shapes_and_types,
+  TF_ShapeAndTypeList** output_shapes,
+  TF_ShapeAndTypeList*** output_resource_shapes_and_types, TF_Status* status);
+
 #ifdef __cplusplus
 } /* end extern "C" */
 #endif
