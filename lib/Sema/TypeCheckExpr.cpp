8d028bbf3c1920f66239a6e7ba28da7ccfb9fb4e
Greg Titus <gregomni@gmail.com>
2019-08-11 13:59:33 -0700

    Check for non-associativity at end of chain of right-assoc operators.

--- lib/Sema/TypeCheckExpr.cpp
+++ lib/Sema/TypeCheckExpr.cpp
@@ -522,8 +522,17 @@ static Expr *foldSequence(TypeChecker &TC, DeclContext *DC,
       // If we've drained the entire sequence, we're done.
       if (S.empty()) return LHS;
 
-      // Otherwise, start all over with our new LHS.
-      return foldSequence(TC, DC, LHS, S, precedenceBound);
+      // Otherwise, we have to check that this next operator actually
+      // associates.
+      if ((op2 = getNextOperator()) && op2.precedence) {
+        associativity =
+            TC.Context.associateInfixOperators(op1.precedence, op2.precedence);
+        RHS = S[1];
+      }
+
+      // If so, start all over with our new LHS.
+      if (associativity != Associativity::None)
+        return foldSequence(TC, DC, LHS, S, precedenceBound);
     }
 
     // If we ended up here, it's because we're either:
