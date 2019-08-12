ee6c169c45588f57d2fad28973accfea860c3862
Suyash Srijan <suyashsrijan@outlook.com>
2019-08-12 19:59:17 +0100

    Revert "[Typechecker] Fix fix-it loc when throwing function is called on a protocol typed value"
    This reverts commit 60d52f0f2a446586acba62d3826de648638131e6.

--- lib/Sema/TypeCheckError.cpp
+++ lib/Sema/TypeCheckError.cpp
@@ -215,13 +215,9 @@ public:
     } else if (auto optionalTryExpr = dyn_cast<OptionalTryExpr>(E)) {
       recurse = asImpl().checkOptionalTry(optionalTryExpr);
     } else if (auto apply = dyn_cast<ApplyExpr>(E)) {
-      recurse = asImpl().checkApply(apply, nullptr);
+      recurse = asImpl().checkApply(apply);
     } else if (auto interpolated = dyn_cast<InterpolatedStringLiteralExpr>(E)) {
       recurse = asImpl().checkInterpolatedStringLiteral(interpolated);
-    } else if (auto openExistExpr = dyn_cast<OpenExistentialExpr>(E)) {
-      if (auto applyExpr = dyn_cast<ApplyExpr>(openExistExpr->getSubExpr())) {
-        recurse = asImpl().checkApply(applyExpr, openExistExpr);
-      }
     }
     // Error handling validation (via checkTopLevelErrorHandling) happens after
     // type checking. If an unchecked expression is still around, the code was
@@ -598,7 +594,7 @@ private:
     ShouldRecurse_t checkOptionalTry(OptionalTryExpr *E) {
       return ShouldNotRecurse;
     }
-    ShouldRecurse_t checkApply(ApplyExpr *E, Expr *BaseExpr) {
+    ShouldRecurse_t checkApply(ApplyExpr *E) {
       Result = std::max(Result, Self.classifyApply(E).getResult());
       return ShouldRecurse;
     }
@@ -1020,7 +1016,7 @@ public:
     llvm_unreachable("bad reason kind");
   }
 
-  void diagnoseUncoveredThrowSite(TypeChecker &TC, ASTNode E, Expr *BaseExpr,
+  void diagnoseUncoveredThrowSite(TypeChecker &TC, ASTNode E,
                                   const PotentialReason &reason) {
     auto message = diag::throwing_call_without_try;
     auto loc = E.getStartLoc();
@@ -1034,15 +1030,9 @@ public:
         loc = e->getFn()->getStartLoc();
         message = diag::throwing_operator_without_try;
       }
-
-      if (auto OEE = dyn_cast_or_null<OpenExistentialExpr>(BaseExpr)) {
-        insertLoc = OEE->getExistentialValue()->getStartLoc();
-        highlight = e->getSourceRange();
-      } else {
-        insertLoc = loc;
-        highlight = e->getSourceRange();
-      }
-
+      insertLoc = loc;
+      highlight = e->getSourceRange();
+      
       if (InterpolatedString &&
           e->getCalledValue() &&
           e->getCalledValue()->getBaseName() ==
@@ -1454,12 +1444,12 @@ private:
     CurContext = savedContext;
   }
 
-  ShouldRecurse_t checkApply(ApplyExpr *E, Expr *BaseExpr) {
+  ShouldRecurse_t checkApply(ApplyExpr *E) {
     // An apply expression is a potential throw site if the function throws.
     // But if the expression didn't type-check, suppress diagnostics.
     auto classification = Classifier.classifyApply(E);
 
-    checkThrowSite(E, BaseExpr, /*requiresTry*/ true, classification);
+    checkThrowSite(E, /*requiresTry*/ true, classification);
 
     // HACK: functions can get queued multiple times in
     // definedFunctions, so be sure to be idempotent.
@@ -1520,12 +1510,12 @@ private:
   }
 
   ShouldRecurse_t checkThrow(ThrowStmt *S) {
-    checkThrowSite(S, nullptr, /*requiresTry*/ false,
+    checkThrowSite(S, /*requiresTry*/ false,
                    Classification::forThrow(PotentialReason::forThrow()));
     return ShouldRecurse;
   }
 
-  void checkThrowSite(ASTNode E, Expr *BaseExpr, bool requiresTry,
+  void checkThrowSite(ASTNode E, bool requiresTry,
                       const Classification &classification) {
     MaxThrowingKind = std::max(MaxThrowingKind, classification.getResult());
 
@@ -1561,7 +1551,7 @@ private:
         CurContext.diagnoseUnhandledThrowSite(TC, E, isTryCovered,
                                               classification.getThrowsReason());
       } else if (!isTryCovered) {
-        CurContext.diagnoseUncoveredThrowSite(TC, E, BaseExpr,
+        CurContext.diagnoseUncoveredThrowSite(TC, E,
                                               classification.getThrowsReason());
       }
       return;
