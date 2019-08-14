0b7ef3445eeda7fbe29c0edc91aebc1a846a7755
Doug Gregor <dgregor@apple.com>
2019-08-14 09:20:41 -0700

    [Constraint graph] Make sure we print type variables when dumping components.
    Helps with debugging the constraint graph.

--- lib/Sema/ConstraintGraph.cpp
+++ lib/Sema/ConstraintGraph.cpp
@@ -1211,6 +1211,8 @@ void ConstraintGraph::printConnectedComponents(
 }
 
 void ConstraintGraph::dumpConnectedComponents() {
+  llvm::SaveAndRestore<bool>
+    debug(CS.getASTContext().LangOpts.DebugConstraintSolver, true);
   printConnectedComponents(CS.TypeVariables, llvm::dbgs());
 }
 
