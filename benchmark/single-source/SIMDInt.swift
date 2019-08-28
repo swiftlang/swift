a651a1da335e81f849aae381bc98621673e87d7d
Nikolai Vazquez <nikolai_vazquez@apple.com>
2019-08-28 16:54:13 -0700

    Rename SIMDInt benchmarks prefix to SIMD4Int32

--- benchmark/single-source/SIMDInt.swift
+++ benchmark/single-source/SIMDInt.swift
@@ -15,19 +15,19 @@ import TestsUtils
 let t: [BenchmarkCategory] = [.validation, .api, .simd]
 
 public let SIMDInt = [
-  BenchmarkInfo(name: "SIMDIntAdd",     runFunction: run_SIMDIntAdd,     tags: t),
-  BenchmarkInfo(name: "SIMDIntSub",     runFunction: run_SIMDIntSub,     tags: t),
-  BenchmarkInfo(name: "SIMDIntMul",     runFunction: run_SIMDIntMul,     tags: t),
-  BenchmarkInfo(name: "SIMDIntDiv",     runFunction: run_SIMDIntDiv,     tags: t),
-  BenchmarkInfo(name: "SIMDIntRem",     runFunction: run_SIMDIntRem,     tags: t),
-  BenchmarkInfo(name: "SIMDIntShl",     runFunction: run_SIMDIntShl,     tags: t),
-  BenchmarkInfo(name: "SIMDIntShr",     runFunction: run_SIMDIntShr,     tags: t),
-  BenchmarkInfo(name: "SIMDIntBulkSum", runFunction: run_SIMDIntBulkSum, tags: t,
+  BenchmarkInfo(name: "SIMD4Int32.add",     runFunction: run_SIMD4Int32Add,     tags: t),
+  BenchmarkInfo(name: "SIMD4Int32.sub",     runFunction: run_SIMD4Int32Sub,     tags: t),
+  BenchmarkInfo(name: "SIMD4Int32.mul",     runFunction: run_SIMD4Int32Mul,     tags: t),
+  BenchmarkInfo(name: "SIMD4Int32.div",     runFunction: run_SIMD4Int32Div,     tags: t),
+  BenchmarkInfo(name: "SIMD4Int32.rem",     runFunction: run_SIMD4Int32Rem,     tags: t),
+  BenchmarkInfo(name: "SIMD4Int32.shl",     runFunction: run_SIMD4Int32Shl,     tags: t),
+  BenchmarkInfo(name: "SIMD4Int32.shr",     runFunction: run_SIMD4Int32Shr,     tags: t),
+  BenchmarkInfo(name: "SIMD4Int32.bulkSum", runFunction: run_SIMD4Int32BulkSum, tags: t,
                 setUpFunction: { blackHole(bulkSumInts) }),
 ]
 
 @inline(never)
-public func run_SIMDIntAdd(N: Int) {
+public func run_SIMD4Int32Add(N: Int) {
   var a = identity(SIMD4<Int32>(1, 2, 3, 4))
   let b = identity(SIMD4<Int32>(5, 6, 7, 8))
   for _ in 0..<N {
@@ -37,7 +37,7 @@ public func run_SIMDIntAdd(N: Int) {
 }
 
 @inline(never)
-public func run_SIMDIntSub(N: Int) {
+public func run_SIMD4Int32Sub(N: Int) {
   var a = identity(SIMD4<Int32>(1, 2, 3, 4))
   let b = identity(SIMD4<Int32>(5, 6, 7, 8))
   for _ in 0..<N {
@@ -47,7 +47,7 @@ public func run_SIMDIntSub(N: Int) {
 }
 
 @inline(never)
-public func run_SIMDIntMul(N: Int) {
+public func run_SIMD4Int32Mul(N: Int) {
   var a = identity(SIMD4<Int32>(1, 2, 3, 4))
   let b = identity(SIMD4<Int32>(5, 6, 7, 8))
   for _ in 0..<N {
@@ -57,7 +57,7 @@ public func run_SIMDIntMul(N: Int) {
 }
 
 @inline(never)
-public func run_SIMDIntDiv(N: Int) {
+public func run_SIMD4Int32Div(N: Int) {
   var a = identity(SIMD4<Int32>(1, 2, 3, 4))
   let b = identity(SIMD4<Int32>(5, 6, 7, 8))
   for _ in 0..<N {
@@ -67,7 +67,7 @@ public func run_SIMDIntDiv(N: Int) {
 }
 
 @inline(never)
-public func run_SIMDIntRem(N: Int) {
+public func run_SIMD4Int32Rem(N: Int) {
   var a = identity(SIMD4<Int32>(1, 2, 3, 4))
   let b = identity(SIMD4<Int32>(5, 6, 7, 8))
   for _ in 0..<N {
@@ -77,7 +77,7 @@ public func run_SIMDIntRem(N: Int) {
 }
 
 @inline(never)
-public func run_SIMDIntShl(N: Int) {
+public func run_SIMD4Int32Shl(N: Int) {
   var a = identity(SIMD4<Int32>(1, 2, 3, 4))
   let b = identity(SIMD4<Int32>(5, 6, 7, 8))
   for _ in 0..<N {
@@ -87,7 +87,7 @@ public func run_SIMDIntShl(N: Int) {
 }
 
 @inline(never)
-public func run_SIMDIntShr(N: Int) {
+public func run_SIMD4Int32Shr(N: Int) {
   var a = identity(SIMD4<Int32>(1, 2, 3, 4))
   let b = identity(SIMD4<Int32>(5, 6, 7, 8))
   for _ in 0..<N {
@@ -99,7 +99,7 @@ public func run_SIMDIntShr(N: Int) {
 let bulkSumInts = Array(repeating: SIMD4<Int32>(1, 2, 3, 4), count: 100)
 
 @inline(never)
-public func run_SIMDIntBulkSum(N: Int) {
+public func run_SIMD4Int32BulkSum(N: Int) {
   for _ in 0..<N {
     let ints = identity(bulkSumInts)
     let sum = ints.reduce(SIMD4<Int32>(), &+).wrappedSum()
