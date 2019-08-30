4bd7a89bd64423b439ba4762cdd28bad1b41e9d7
Varun Gandhi <varun_gandhi@apple.com>
2019-08-30 14:29:54 -0700

    Skip module trace emission if we encountered errors when compiling.
    Fixes rdar://problem/54860311.

--- test/Driver/loaded_module_trace_skip.swift
+++ test/Driver/loaded_module_trace_skip.swift
@@ -1,5 +1,5 @@
+// rdar://problem/54860311.
 // RUN: not %target-typecheck-verify-swift -emit-loaded-module-trace -o %t/mytrace -I %S/Inputs 2>&1 | %FileCheck %s
-// XFAIL: *
 
 import IllformedModule
 // CHECK: unexpected error produced: malformed compiled module
