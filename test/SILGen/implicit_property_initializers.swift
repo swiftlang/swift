5e0aa6837d3ca6d4407b274962b388ff61aa65d6
Harlan Haskins <harlan@apple.com>
2019-09-03 16:43:28 -0700

    [test] Update SILGen matching code for 5.1 differences

--- test/SILGen/implicit_property_initializers.swift
+++ test/SILGen/implicit_property_initializers.swift
@@ -21,18 +21,18 @@ struct HasDefaultTupleOfNils {
 
 // CHECK: sil [transparent] [ossa] @$[[X_VALUE_INIT:s30implicit_property_initializers21HasDefaultTupleOfNilsV1xSiSg_AEtvpfi]] : $@convention(thin) () -> (Optional<Int>, Optional<Int>) {
 // CHECK: bb0:
-// CHECK:   %0 = enum $Optional<Int>, #Optional.none!enumelt
-// CHECK:   %1 = enum $Optional<Int>, #Optional.none!enumelt
-// CHECK:   %2 = tuple (%0 : $Optional<Int>, %1 : $Optional<Int>)
-// CHECK:   return %2 : $(Optional<Int>, Optional<Int>)
+// CHECK:   %[[OPT1:[0-9]+]] = enum $Optional<Int>, #Optional.none!enumelt
+// CHECK:   %[[OPT2:[0-9]+]] = enum $Optional<Int>, #Optional.none!enumelt
+// CHECK:   %[[TUPLE:[0-9]+]] = tuple (%[[OPT1]] : $Optional<Int>, %[[OPT2]] : $Optional<Int>)
+// CHECK:   return %[[TUPLE]] : $(Optional<Int>, Optional<Int>)
 // CHECK: }
 
 // The default value initializer for 'y' should have type Optional<Int>
 
 //CHECK: sil [transparent] [ossa] @$s30implicit_property_initializers21HasDefaultTupleOfNilsV1ySiSgvpfi : $@convention(thin) () -> Optional<Int> {
 //CHECK: bb0:
-//CHECK:   %0 = enum $Optional<Int>, #Optional.none!enumelt // user: %1
-//CHECK:   return %0 : $Optional<Int>                      // id: %1
+//CHECK:   %[[OPT:[0-9]+]] = enum $Optional<Int>, #Optional.none!enumelt
+//CHECK:   return %[[OPT]] : $Optional<Int>
 //CHECK: }
 
 // There should not be a default value initializer for 'z'.
@@ -43,23 +43,23 @@ struct HasDefaultTupleOfNils {
 
 //CHECK: sil [transparent] [ossa] @$[[W_VALUE_INIT:s30implicit_property_initializers21HasDefaultTupleOfNilsV1wSiSg_ytAEt_AE_AEttvpfi]] : $@convention(thin) () -> (Optional<Int>, Optional<Int>, Optional<Int>, Optional<Int>) {
 //CHECK: bb0:
-//CHECK:   %0 = enum $Optional<Int>, #Optional.none!enumelt // user: %4
-//CHECK:   %1 = enum $Optional<Int>, #Optional.none!enumelt // user: %4
-//CHECK:   %2 = enum $Optional<Int>, #Optional.none!enumelt // user: %4
-//CHECK:   %3 = enum $Optional<Int>, #Optional.none!enumelt // user: %4
-//CHECK:   %4 = tuple (%0 : $Optional<Int>, %1 : $Optional<Int>, %2 : $Optional<Int>, %3 : $Optional<Int>) // user: %5
-//CHECK:   return %4 : $(Optional<Int>, Optional<Int>, Optional<Int>, Optional<Int>) // id: %5
+//CHECK:   %[[OPT0:[0-9]+]] = enum $Optional<Int>, #Optional.none!enumelt
+//CHECK:   %[[OPT1:[0-9]+]] = enum $Optional<Int>, #Optional.none!enumelt
+//CHECK:   %[[OPT2:[0-9]+]] = enum $Optional<Int>, #Optional.none!enumelt
+//CHECK:   %[[OPT3:[0-9]+]] = enum $Optional<Int>, #Optional.none!enumelt
+//CHECK:   %[[TUPLE:[0-9]+]] = tuple (%[[OPT0]] : $Optional<Int>, %[[OPT1]] : $Optional<Int>, %[[OPT2]] : $Optional<Int>, %[[OPT3]] : $Optional<Int>)
+//CHECK:   return %[[TUPLE]] : $(Optional<Int>, Optional<Int>, Optional<Int>, Optional<Int>)
 //CHECK: }
 
 // The default arg generator for 'x' inside the memberwise init should have type (Optional<Int>, Optional<Int>)
 
 // CHECK: sil [ossa] @$s30implicit_property_initializers21HasDefaultTupleOfNilsV1x1y1z1wACSiSg_AHt_AHSiAH_ytAHt_AH_AHtttcfcfA_ : $@convention(thin) () -> (Optional<Int>, Optional<Int>) {
 // CHECK: bb0:
-// CHECK:   %0 = function_ref @$[[X_VALUE_INIT]] : $@convention(thin) () -> (Optional<Int>, Optional<Int>) // user: %1
-// CHECK:   %1 = apply %0() : $@convention(thin) () -> (Optional<Int>, Optional<Int>)
-// CHECK:   (%2, %3) = destructure_tuple %1 : $(Optional<Int>, Optional<Int>)
-// CHECK:   %4 = tuple (%2 : $Optional<Int>, %3 : $Optional<Int>)
-// CHECK:   return %4 : $(Optional<Int>, Optional<Int>)
+// CHECK:   %[[INIT_FN:[0-9]+]] = function_ref @$[[X_VALUE_INIT]] : $@convention(thin) () -> (Optional<Int>, Optional<Int>)
+// CHECK:   %[[RESULT:[0-9]+]] = apply %[[INIT_FN]]() : $@convention(thin) () -> (Optional<Int>, Optional<Int>)
+// CHECK:   (%[[OPT1:[0-9]+]], %[[OPT2:[0-9]+]]) = destructure_tuple %[[RESULT]] : $(Optional<Int>, Optional<Int>)
+// CHECK:   %[[TUPLE:[0-9]]] = tuple (%[[OPT1]] : $Optional<Int>, %[[OPT2]] : $Optional<Int>)
+// CHECK:   return %[[TUPLE]] : $(Optional<Int>, Optional<Int>)
 // CHECK: }
 
 // There should not be a default arg generator for 'y' because it's just a nil literal and clients construct it directly.
@@ -74,9 +74,9 @@ struct HasDefaultTupleOfNils {
 
 // CHECK: sil [ossa] @$s30implicit_property_initializers21HasDefaultTupleOfNilsV1x1y1z1wACSiSg_AHt_AHSiAH_ytAHt_AH_AHtttcfcfA2_ : $@convention(thin) () -> (Optional<Int>, Optional<Int>, Optional<Int>, Optional<Int>) {
 // CHECK: bb0:
-// CHECK:   %0 = function_ref @$[[W_VALUE_INIT]] : $@convention(thin) () -> (Optional<Int>, Optional<Int>, Optional<Int>, Optional<Int>)
-// CHECK:   %1 = apply %0() : $@convention(thin) () -> (Optional<Int>, Optional<Int>, Optional<Int>, Optional<Int>)
-// CHECK:   (%2, %3, %4, %5) = destructure_tuple %1 : $(Optional<Int>, Optional<Int>, Optional<Int>, Optional<Int>)
-// CHECK:   %6 = tuple (%2 : $Optional<Int>, %3 : $Optional<Int>, %4 : $Optional<Int>, %5 : $Optional<Int>)
-// CHECK:   return %6 : $(Optional<Int>, Optional<Int>, Optional<Int>, Optional<Int>)
+// CHECK:   %[[INIT_FN:[0-9]+]] = function_ref @$[[W_VALUE_INIT]] : $@convention(thin) () -> (Optional<Int>, Optional<Int>, Optional<Int>, Optional<Int>)
+// CHECK:   %[[RESULT:[0-9]+]] = apply %[[INIT_FN:[0-9]+]]() : $@convention(thin) () -> (Optional<Int>, Optional<Int>, Optional<Int>, Optional<Int>)
+// CHECK:   (%[[OPT1:[0-9]+]], %[[OPT2:[0-9]+]], %[[OPT3:[0-9]+]], %[[OPT4:[0-9]+]]) = destructure_tuple %[[RESULT]] : $(Optional<Int>, Optional<Int>, Optional<Int>, Optional<Int>)
+// CHECK:   %[[TUPLE:[0-9]]] = tuple (%[[OPT1]] : $Optional<Int>, %[[OPT2]] : $Optional<Int>, %[[OPT3]] : $Optional<Int>, %[[OPT4]] : $Optional<Int>)
+// CHECK:   return %[[TUPLE]] : $(Optional<Int>, Optional<Int>, Optional<Int>, Optional<Int>)
 // CHECK: }
