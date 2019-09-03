eb045f5b0bf7093c8efd693038eaac7010f65940
Suyash Srijan <suyashsrijan@outlook.com>
2019-09-03 20:03:11 +0100

    [Test] Add a test case for computed property

--- test/decl/protocol/req/witness_fix_its.swift
+++ test/decl/protocol/req/witness_fix_its.swift
@@ -10,6 +10,7 @@ protocol Foo {
   static prefix func ^^^(value: Self) -> Int // expected-note {{protocol requires function '^^^' with type '(ConformsToFoo) -> Int'; do you want to add a stub?}}
   static postfix func ^^^^(value: Self) -> Int // expected-note {{protocol requires function '^^^^' with type '(ConformsToFoo) -> Int'; do you want to add a stub?}}
   func bar4(closure: () throws -> Int) rethrows // expected-note {{protocol requires function 'bar4(closure:)' with type '(() throws -> Int) throws -> ()'; do you want to add a stub?}}
+  var bar5: Int { get set } // expected-note {{protocol requires property 'bar5' with type 'Int'; do you want to add a stub?}}
 }
 
 struct ConformsToFoo: Foo { // expected-error {{type 'ConformsToFoo' does not conform to protocol 'Foo'}}
@@ -21,4 +22,5 @@ struct ConformsToFoo: Foo { // expected-error {{type 'ConformsToFoo' does not co
   static prefix func ^^^^(value: ConformsToFoo) -> Int { return 0 } // expected-error {{operator implementation without matching operator declaration}}
   // expected-note@-1 {{candidate is prefix, not postfix as required}}{{10-16=postfix}}
   func bar4(closure: () throws -> Int) throws {} // expected-note {{candidate is not 'rethrows', but protocol requires it}}{{40-46=rethrows}}
+  var bar5: Int { return 0 } // expected-note {{candidate is not settable, but protocol requires it}}{{none}}
 }
