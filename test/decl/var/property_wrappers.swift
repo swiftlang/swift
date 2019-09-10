9ce12ac42260f8df7c2fb2db148ce62964818308
Doug Gregor <dgregor@apple.com>
2019-09-10 16:32:15 -0700

    [Type checker] Compute "mutating" for projected variables of wrapped properties
    Extend the recently-refactored logic for determining whether the
    getter/setter for a wrapped property is "mutating" to also consider
    the projection variables (e.g., $foo). Fixes SR-11393 /
    rdar://problem/54839570.

--- test/decl/var/property_wrappers.swift
+++ test/decl/var/property_wrappers.swift
@@ -1581,3 +1581,66 @@ protocol SR_11288_P5 {
 struct SR_11288_S5<T>: SR_11288_P5 {
   @SR_11288_Wrapper5 var answer = 42 // Okay
 }
+
+// SR-11393
+protocol Copyable: AnyObject {
+    func copy() -> Self
+}
+
+@propertyWrapper
+struct CopyOnWrite<Value: Copyable> {
+  init(wrappedValue: Value) {
+    self.wrappedValue = wrappedValue
+  }
+
+  var wrappedValue: Value
+
+  var projectedValue: Value {
+    mutating get {
+      if !isKnownUniquelyReferenced(&wrappedValue) {
+        wrappedValue = wrappedValue.copy()
+      }
+      return wrappedValue
+    }
+    set {
+      wrappedValue = newValue
+    }
+  }
+}
+
+final class CopyOnWriteTest: Copyable {
+    let a: Int
+    init(a: Int) {
+        self.a = a
+    }
+
+    func copy() -> Self {
+        Self.init(a: a)
+    }
+}
+
+struct CopyOnWriteDemo1 {
+  @CopyOnWrite var a = CopyOnWriteTest(a: 3)
+
+  func foo() { // expected-note{{mark method 'mutating' to make 'self' mutable}}
+    _ = $a // expected-error{{cannot use mutating getter on immutable value: 'self' is immutable}}
+  }
+}
+
+@propertyWrapper
+struct NonMutatingProjectedValueSetWrapper<Value> {
+  var wrappedValue: Value
+  var projectedValue: Value {
+    get { wrappedValue }
+    nonmutating set { } 
+  }
+}
+
+struct UseNonMutatingProjectedValueSet {
+  @NonMutatingProjectedValueSetWrapper var x = 17
+
+  func test() { // expected-note{{mark method 'mutating' to make 'self' mutable}}
+    $x = 42 // okay
+    x = 42  // expected-error{{cannot assign to property: 'self' is immutable}}
+  }
+}
