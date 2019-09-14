c7e603ddf7d33c523cd2b21652b09026b8f4b3d3
David Ungar <dungar@apple.com>
2019-09-14 13:41:59 -0700

    Fix overly restrictive assertion, for -dump-scope-maps case

--- lib/AST/Module.cpp
+++ lib/AST/Module.cpp
@@ -1745,8 +1745,7 @@ StringRef SourceFile::getFilename() const {
 }
 
 ASTScope &SourceFile::getScope() {
-  assert(getASTContext().LangOpts.EnableASTScopeLookup &&
-         isSuitableForASTScopes() && "Should not be creating scope tree");
+  assert(isSuitableForASTScopes() && "Should not be creating scope tree");
   if (!Scope)
     Scope = std::unique_ptr<ASTScope>(new (getASTContext()) ASTScope(this));
   return *Scope.get();
