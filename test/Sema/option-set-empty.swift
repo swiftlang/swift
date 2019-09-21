2104984cfd041f19b46f2920afa95da820b6065f
Miguel Salinas <mikeys@me.com>
2019-09-21 17:03:45 -0500

    more unit test cases

--- test/Sema/option-set-empty.swift
+++ test/Sema/option-set-empty.swift
@@ -1,6 +1,17 @@
 // RUN: %target-typecheck-verify-swift
 
+struct SomeOptions: OptionSet {
+    var rawValue: Int
+    static let some = MyOptions(rawValue: 4)
+}
+
 struct MyOptions: OptionSet {
+    init() {
+        rawValue = 0
+    }
     var rawValue: Int
     static let none = MyOptions(rawValue: 0) // expected-warning {{static property 'none' produces an empty option set}} expected-note {{use [] to silence this warning}}{{32-45=([])}}
+    static var nothing = MyOptions(rawValue: 0)
+    static let nope = MyOptions()
+    static let other = SomeOptions(rawValue: 8)
 }
