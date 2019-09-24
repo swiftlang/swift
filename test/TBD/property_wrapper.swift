4ba1833ca2cd37bced7e758696c6f1b5792afd9d
Doug Gregor <dgregor@apple.com>
2019-09-23 21:01:10 -0700

    [TBDGen] Emit property wrapper backing initializer symbol

---
+++ test/TBD/property_wrapper.swift
@@ -0,0 +1,15 @@
+// RUN: %target-swift-frontend -emit-ir -o/dev/null -parse-as-library -module-name test -validate-tbd-against-ir=all %s
+
+@propertyWrapper
+public struct Wrapper<Value> {
+  public var wrappedValue: Value
+
+  public init(wrappedValue: Value) {
+    self.wrappedValue = wrappedValue
+  }
+}
+
+public struct UseWrapper {
+  @Wrapper public var string = "hello"
+}
+
