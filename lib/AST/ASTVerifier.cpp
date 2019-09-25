bc804c2d4cb072695f4a67cf231fbaee086e252a
Brent Royal-Gordon <brentdax@apple.com>
2019-09-25 13:43:25 -0700

    Correctly clean interpolation OVE from the AST
    SanitizeExpr was clearing an InterpolatedStringLiteralExpr’s InterpolationExpr (the OVE representing its builder instance) from its TapExpr’s SubExpr, but not from the ISLE itself. This could cause AST validation failures in code in invalid code because the OVE would be introduced by the ISLE but would not actually exist in the AST.
    
    This commit:
    * Improves SanitizeExpr to clean out both references to the OVE.
    * Tweaks ASTVerifier to account for the fact that the InterpolationExpr may not exist.
    * Adds InterpolationExpr to AST dumps to assist in reading interpolated string literals in them.

--- lib/AST/ASTVerifier.cpp
+++ lib/AST/ASTVerifier.cpp
@@ -837,7 +837,11 @@ public:
       if (!expr->getInterpolationExpr())
         return true;
 
-      assert(!OpaqueValues.count(expr->getInterpolationExpr()));
+      // This OpaqueValueExpr is introduced after type checking, so it might not
+      // exist, but if it does, we want to check it.
+      if (auto ove = expr->getInterpolationExpr())
+        assert(!OpaqueValues.count(ove));
+
       return true;
     }
 
@@ -847,7 +851,10 @@ public:
       if (!expr->getInterpolationExpr())
         return;
 
-      assert(OpaqueValues.count(expr->getInterpolationExpr()));
+      // This OpaqueValueExpr is introduced after type checking, so it might not
+      // exist, but if it does, we want to check it.
+      if (auto ove = expr->getInterpolationExpr())
+        assert(OpaqueValues.count(ove));
     }
 
     bool shouldVerify(TapExpr *expr) {
