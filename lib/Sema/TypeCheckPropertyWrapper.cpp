162f86fdcc96c40bef6411c129f2c7dc6b9f0298
Suyash Srijan <suyashsrijan@outlook.com>
2019-10-11 23:11:48 +0100

    [PropertyWrappers] We won't have a valueVar when looking for a default init

--- lib/Sema/TypeCheckPropertyWrapper.cpp
+++ lib/Sema/TypeCheckPropertyWrapper.cpp
@@ -100,8 +100,6 @@ findSuitableWrapperInit(ASTContext &ctx, NominalTypeDecl *nominal,
     Inaccessible,
   };
 
-  auto valueVarType = valueVar->getValueInterfaceType();
-
   SmallVector<std::tuple<ConstructorDecl *, NonViableReason, Type>, 2>
       nonviable;
   SmallVector<ConstructorDecl *, 2> viableInitializers;
@@ -183,7 +181,7 @@ findSuitableWrapperInit(ASTContext &ctx, NominalTypeDecl *nominal,
 
       // The parameter type must be the same as the type of `valueVar` or an
       // autoclosure thereof.
-      if (!paramType->isEqual(valueVarType)) {
+      if (!paramType->isEqual(valueVar->getValueInterfaceType())) {
         nonviable.push_back(std::make_tuple(
             init, NonViableReason::ParameterTypeMismatch, paramType));
         continue;
@@ -214,7 +212,8 @@ findSuitableWrapperInit(ASTContext &ctx, NominalTypeDecl *nominal,
 
       case NonViableReason::ParameterTypeMismatch:
         init->diagnose(diag::property_wrapper_wrong_initial_value_init,
-                       init->getFullName(), paramType, valueVarType);
+                       init->getFullName(), paramType,
+                       valueVar->getValueInterfaceType());
         valueVar->diagnose(diag::decl_declared_here, valueVar->getFullName());
         break;
       }
