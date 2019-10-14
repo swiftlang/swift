ec8538ca034dd9fbf10d4b07416bb61a672397b3
Rintaro Ishizaki <rishizaki@apple.com>
2019-10-14 15:27:32 -0700

    Revert "Revert "Merge pull request #27531 from rintaro/syntaxparse-matchingtoken""
    This reverts commit cd1a2995ff1235a8ee7b97c9463c4982e5ba9316.

--- lib/Parse/Parser.cpp
+++ lib/Parse/Parser.cpp
@@ -1254,22 +1254,26 @@ Optional<ParsedTokenSyntax> Parser::parseTokenSyntax(tok K, SourceLoc &TokLoc,
   return None;
 }
 
-Optional<ParsedTokenSyntax>
-Parser::parseMatchingTokenSyntax(tok K, SourceLoc &TokLoc, Diag<> ErrorDiag, SourceLoc OtherLoc) {
-  Diag<> OtherNote;
-  switch (K) {
-  case tok::r_paren:  OtherNote = diag::opening_paren; break;
-  case tok::r_square: OtherNote = diag::opening_bracket; break;
-  case tok::r_brace:  OtherNote = diag::opening_brace; break;
-  default: llvm_unreachable("unknown matching token!");
-  }
+ParsedSyntaxResult<ParsedTokenSyntax>
+Parser::parseMatchingTokenSyntax(tok K, Diag<> ErrorDiag, SourceLoc OtherLoc,
+                                 bool silenceDiag) {
+  if (Tok.is(K))
+    return makeParsedResult(consumeTokenSyntax(K));
+  checkForInputIncomplete();
 
-  auto Token = parseTokenSyntax(K, TokLoc, ErrorDiag);
-  if (!Token) {
-    TokLoc = getLocForMissingMatchingToken();
+  if (!silenceDiag) {
+    diagnose(Tok, ErrorDiag);
+
+    Diag<> OtherNote;
+    switch (K) {
+    case tok::r_paren:  OtherNote = diag::opening_paren; break;
+    case tok::r_square: OtherNote = diag::opening_bracket; break;
+    case tok::r_brace:  OtherNote = diag::opening_brace; break;
+    default: llvm_unreachable("unknown matching token!");
+    }
     diagnose(OtherLoc, OtherNote);
   }
-  return Token;
+  return makeParserError();
 }
 
 SourceLoc Parser::getLocForMissingMatchingToken() const {
@@ -1315,7 +1319,6 @@ static SyntaxKind getListElementKind(SyntaxKind ListKind) {
 ParserStatus
 Parser::parseListSyntax(tok RightK, SourceLoc LeftLoc,
                         Optional<ParsedTokenSyntax> &LastComma,
-                        SourceLoc &RightLoc,
                         Optional<ParsedTokenSyntax> &Right,
                         SmallVectorImpl<ParsedSyntax>& Junk,
                         bool AllowSepAfterLast, Diag<> ErrorDiag,
@@ -1325,13 +1328,11 @@ Parser::parseListSyntax(tok RightK, SourceLoc LeftLoc,
   };
 
   if (Tok.is(RightK)) {
-    RightLoc = Tok.getLoc();
     Right = consumeTokenSyntax(RightK);
     return makeParserSuccess();
   }
   if (TokIsStringInterpolationEOF()) {
     Tok.setKind(RightK);
-    RightLoc = Tok.getLoc();
     Right = consumeTokenSyntax();
     return makeParserSuccess();
   }
@@ -1354,7 +1355,6 @@ Parser::parseListSyntax(tok RightK, SourceLoc LeftLoc,
     // Just accept the ")" and build the tuple as we usually do.
     if (TokIsStringInterpolationEOF()) {
       Tok.setKind(RightK);
-      RightLoc = Tok.getLoc();
       Right = consumeTokenSyntax();
       return Status;
     }
@@ -1391,20 +1391,10 @@ Parser::parseListSyntax(tok RightK, SourceLoc LeftLoc,
     Status.setIsParseError();
   }
 
-  if (Status.isError()) {
-    // If we've already got errors, don't emit missing RightK diagnostics.
-    if (Tok.is(RightK)) {
-      RightLoc = Tok.getLoc();
-      Right = consumeTokenSyntax(RightK);
-    } else {
-      RightLoc = getLocForMissingMatchingToken();
-    }
-  } else {
-    Right = parseMatchingTokenSyntax(RightK, RightLoc, ErrorDiag, LeftLoc);
-    if (!Right)
-      Status.setIsParseError();
-  }
-
+  auto RightResult = parseMatchingTokenSyntax(RightK, ErrorDiag, LeftLoc,
+                                              Status.isError());
+  Status |= RightResult.getStatus();
+  Right = RightResult.getOrNull();
   return Status;
 }
 
