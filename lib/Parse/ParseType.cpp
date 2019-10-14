5a6e14bee4236a36613026c22114e19dc645bafb
Rintaro Ishizaki <rishizaki@apple.com>
2019-10-14 14:02:05 -0700

    Revert "Merge pull request #26952 from rintaro/syntaxparse-type-failedpostfix"
    This reverts commit 987b856f14fe463f4af9ce4b5ebdb83115bd87dc, reversing
    changes made to 4fa3cef4c422bb73d66f5bb54dffed4de1257a70.

--- lib/Parse/ParseType.cpp
+++ lib/Parse/ParseType.cpp
@@ -210,9 +210,11 @@ Parser::TypeResult Parser::parseTypeSimple(Diag<> MessageID,
   }
 
   // '.Type', '.Protocol', '?', '!', and '[]' still leave us with type-simple.
-  while (Result->isSuccess()) {
-    auto PrevType = Result->getResult();
-
+  while (Result->isSuccess() || !Result->getUnknownNodes().empty()) {
+    auto PrevType = Result->isSuccess()
+                        ? Result->getResult()
+                        : ParsedSyntaxRecorder::makeUnknownType(
+                              Result->getUnknownNodes(), *SyntaxContext);
     if ((Tok.is(tok::period) || Tok.is(tok::period_prefix)) &&
         (peekToken().isContextualKeyword("Type") ||
          peekToken().isContextualKeyword("Protocol"))) {
@@ -235,6 +237,9 @@ Parser::TypeResult Parser::parseTypeSimple(Diag<> MessageID,
         continue;
       }
     }
+    if (!Result->isSuccess())
+      Result =
+          makeParsedResult<ParsedTypeSyntax>({PrevType}, Result->getStatus());
     break;
   }
 
