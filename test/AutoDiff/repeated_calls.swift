f0bd9f81768aeeefe6939ca4ef664a9e15307ce1
Gogul Balakrishnan <bgogul@google.com>
2019-10-16 14:37:08 -0700

    Leak checking in repeated_calls.swift.

--- test/AutoDiff/repeated_calls.swift
+++ test/AutoDiff/repeated_calls.swift
@@ -2,14 +2,15 @@
 // REQUIRES: executable_test
 
 import StdlibUnittest
+import DifferentiationUnittest
 
 var RepeatedCallsTests = TestSuite("RepeatedCalls")
 
-RepeatedCallsTests.test("Repeat") {
-  func mul2(_ x: Float) -> Float {
+RepeatedCallsTests.testWithLeakChecking("Repeat") {
+  func mul2(_ x: Tracked<Float>) -> Tracked<Float> {
     return 2 * x
   }
-  func mul4(_ x: Float) -> Float {
+  func mul4(_ x: Tracked<Float>) -> Tracked<Float> {
     return mul2(mul2(x))
   }
   expectEqual(4, gradient(at: 0, in: mul4))
