af8f48393d751c4bb41e9d1d713885303b5c78a2
Dan Zheng <danielzheng@google.com>
2019-10-17 03:01:02 -0700

    Add clarifying comments.

--- lib/SILGen/SILGenThunk.cpp
+++ lib/SILGen/SILGenThunk.cpp
@@ -86,6 +86,8 @@ SILGenModule::getOrCreateAutoDiffThunk(SILDeclRef derivativeFnDeclRef,
   auto linkage = autodiff::getAutoDiffDerivativeFunctionLinkage(
       originalLinkage, /*isDerivativeFnExported*/ true);
   auto name = derivativeFnDeclRef.mangle();
+  // This thunk is publicly exposed and cannot be transparent.
+  // TODO(TF-925): Mark the thunks as "always inline" for optimization.
   auto *thunk = builder.getOrCreateFunction(
       derivativeFnDecl, name, linkage, derivativeFnTy, IsBare, IsNotTransparent,
       derivativeFnDeclRef.isSerialized(), IsNotDynamic, ProfileCounter(),
