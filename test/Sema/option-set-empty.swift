9cc2d65bb04431bc32f85e3914b449477db196a6
Miguel Salinas <mikeys@me.com>
2019-10-22 17:07:00 -0500

    refine test

--- test/Sema/option-set-empty.swift
+++ test/Sema/option-set-empty.swift
@@ -3,11 +3,13 @@
 struct SomeOptions: OptionSet {
     var rawValue: Int
     
-    let someVal = MyOptions(rawValue: 6)
     static let some = MyOptions(rawValue: 4)
     static let empty = SomeOptions(rawValue: 0) // expected-warning {{static property 'empty' produces an empty option set}} expected-note {{use [] to silence this warning}}{{35-48=([])}}
     static var otherVal = SomeOptions(rawValue: 0)
+    
+    let someVal = MyOptions(rawValue: 6)
     let option = MyOptions(float: Float.infinity)
+    let none = MyOptions(rawValue: 0)
 }
 
 struct MyOptions: OptionSet {
