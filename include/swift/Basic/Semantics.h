00be0b587e449e6ff8bc78772841cd0e04626505
zoecarver <z.zoelec2@gmail.com>
2019-11-02 11:39:55 -0700

    Add comments in both added files

--- include/swift/Basic/Semantics.h
+++ include/swift/Basic/Semantics.h
@@ -1,3 +1,20 @@
+//===--- Semantics.h - Semantics Attribute Definitions -------------*- C++ -*-===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2017 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+//
+// Implementation of the matching definition file.
+// This file holds all semantics attributes as consant stirng literals.
+//
+//===----------------------------------------------------------------------===//
+
 #ifndef SWIFT_SEMANTICS_H
 #define SWIFT_SEMANTICS_H
 
