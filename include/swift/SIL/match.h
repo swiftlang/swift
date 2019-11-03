2910c3cc7be2fa5cfc83d88b5cf6422d5c2b66c3
zoecarver <z.zoelec2@gmail.com>
2019-11-03 15:40:16 -0800

    Document the match function and related classes

--- include/swift/SIL/match.h
+++ include/swift/SIL/match.h
@@ -1,3 +1,22 @@
+//===--- Match.h - Range based matching function -------------*- C++ -*-===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2019 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// This file contains the source for a range based matching function. The function takes a begin and end iterator
+/// which it will use to try to match each element agains a corresponding parameter.
+///
+//===----------------------------------------------------------------------===//
+
+
 #ifndef SWIFT_SIL_MATCH_H
 #define SWIFT_SIL_MATCH_H
 
@@ -12,7 +31,15 @@ struct instance_of : std::false_type {};
 template <template <int...> class _Tp, int... _Args>
 struct instance_of<_Tp, _Tp<_Args...>> : std::true_type {};
 
-template <class T, bool R, class L = char> struct InfoBlock {
+/// InfoBlock is a struct that is used to give info about the element you want to match.
+/// \p T is the type that you want to match. The match function will use
+/// \code dyn_cast \endcode to try to cast the current element to this type. \p R is
+/// a boolean which tells the matching function weather you want the element that you have just
+/// matched back. If this is true, that element will be added to the returned tuple.
+/// \p L is an optional lambda type. If the dynamic cast succeeds the lambda will be passed the
+/// element. If the lambda returns true, the matching succeeds otherwise the matching fails.
+template <class T, bool R, class L = char>
+struct InfoBlock {
   typedef L pred_t;
   typedef T type;
   static constexpr inline bool value = R;
@@ -22,6 +49,10 @@ template <class T, bool R, class L = char> struct InfoBlock {
   InfoBlock(L lambda = 0) : lambda(lambda) {}
 };
 
+/// make_info_block is a helper function for creating an InfoBlock.
+/// make_info_block should be used when you want to supply an optional
+/// lambda to the InfoBlock. \p lambda is the lambda, all other
+/// arguments are the same as InfoBlock.
 template <class T, bool R, class L = char>
 static InfoBlock<T, R, L> make_info_block(L lambda = 0) {
   return InfoBlock<T, R, L>(lambda);
@@ -32,34 +63,89 @@ struct Dummy {
   static bool classof(const SILNode *) { return true; }
 };
 
+/// A range can be used to ignore a certain number of elements. The range will
+/// ignore at least \p Begin elements and will ignore up to \p End elements.
 template <int Begin = 0, int End = std::numeric_limits<int>::max()>
 struct Range {
   static constexpr inline bool value = false;
-  static constexpr inline bool begin = Begin;
-  static constexpr inline bool end = End;
+  static constexpr inline int begin = Begin;
+  static constexpr inline int end = End;
 
   typedef char pred_t;
   typedef Dummy type;
 };
 
+/// Any range indicates that any number of elements may exist
+/// before matching the next element using an InfoBlock.
 using AnyRange = Range<>;
 
-template <typename, typename> struct OutPtr;
+template <class, class>
+struct OutPtr;
 
-template <typename T, typename... Args> struct OutPtr<T, std::tuple<Args...>> {
+template <class T, class... Args>
+struct OutPtr<T, std::tuple<Args...>> {
   using type = std::tuple<typename T::type *, Args...>;
 };
 
-template <typename...> struct filter;
+template <class...>
+struct filter;
 
-template <> struct filter<> { using type = std::tuple<>; };
+template <>
+struct filter<> { using type = std::tuple<>; };
 
-template <typename Head, typename... Tail> struct filter<Head, Tail...> {
+template <class Head, class... Tail>
+struct filter<Head, Tail...> {
   using type = typename std::conditional<
-      Head::value, typename OutPtr<Head, typename filter<Tail...>::type>::type,
-      typename filter<Tail...>::type>::type;
+    Head::value,
+    typename OutPtr<Head, typename filter<Tail...>::type>::type,
+    typename filter<Tail...>::type
+  >::type;
 };
 
+/// match takes a begin and end iterator as arguments \p first and \p last respectively.
+/// Any arguments after that can be used to match the given range. These arguments are known
+/// as \p blocks, and they can be either of type InfoBlock or Range.
+///
+/// Example usage:
+///
+/// \code
+/// // Match test program:
+/// //  func test(_x: Int) -> Int {
+/// //      var x = _x
+/// //      x += 1
+/// //      x &= (x - 1)
+/// //      return x - 1
+/// //  }
+/// ReturnInst *retVal;
+///
+/// for (auto &block : F) {
+///   auto matchedResult = match(
+///       block.begin(), block.end(), AnyRange(),
+///       make_info_block<BuiltinInst, false>([](BuiltinInst *bi) {
+///         return bi->getBuiltinKind().hasValue() &&
+///                bi->getBuiltinKind().getValue() == BuiltinValueKind::SAddOver;
+///       }),
+///       AnyRange(), make_info_block<BuiltinInst, false>([](BuiltinInst *bi) {
+///         return bi->getBuiltinKind().hasValue() &&
+///                bi->getBuiltinKind().getValue() == BuiltinValueKind::SSubOver;
+///       }),
+///       AnyRange(), make_info_block<BuiltinInst, false>([](BuiltinInst *bi) {
+///         return bi->getBuiltinKind().hasValue() &&
+///                bi->getBuiltinKind().getValue() == BuiltinValueKind::And;
+///       }),
+///       AnyRange(), make_info_block<BuiltinInst, false>([](BuiltinInst *bi) {
+///         return bi->getBuiltinKind().hasValue() &&
+///                bi->getBuiltinKind().getValue() == BuiltinValueKind::SSubOver;
+///       }),
+///       AnyRange(), make_info_block<ReturnInst, true>());
+///
+///   if (!matchedResult.hasValue())
+///     continue;
+///
+///   std::tie(retVal) = matchedResult.getValue();
+/// }
+/// \endcode
+///
 template <class T, class B1, class... Blocks>
 Optional<typename filter<B1, Blocks...>::type> match(T first, T last, B1 block,
                                                      Blocks... blocks) {
