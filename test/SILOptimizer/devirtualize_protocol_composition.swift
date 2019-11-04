4b36f3fd69d10fcc2e1ba405847f0da8747359c6
zoecarver <z.zoelec2@gmail.com>
2019-11-04 12:27:35 -0800

    Fix based on review and add more tests

--- test/SILOptimizer/devirtualize_protocol_composition.swift
+++ test/SILOptimizer/devirtualize_protocol_composition.swift
@@ -10,8 +10,8 @@ protocol ProtocolA {
   func foo() -> Int
 }
 
-func shouldOptimizeWitness<T>(_ x: ClassA<T> & ProtocolA) -> Int {
-  return x.foo()
+protocol ProtocolB {
+  func bar() -> Int
 }
 
 public class ClassB: ClassA<String> {
@@ -19,13 +19,116 @@ public class ClassB: ClassA<String> {
     return 10
   }
 }
+
 extension ClassB: ProtocolA { }
 
-//CHECK: witnessEntryPoint
+public class ClassC<T>: ClassA<T> {
+  func foo() -> Int {
+    return 10
+  }
+}
+
+extension ClassC: ProtocolA { }
+
+public class ClassD { }
+public class ClassE : ClassD {
+  func foo() -> Int {
+    return 10
+  }
+}
+
+extension ClassE: ProtocolA { }
+
+public class ClassF {
+  func foo() -> Int {
+    return 10
+  }
+  
+  func bar() -> Int {
+    return 10
+  }
+}
+
+extension ClassF: ProtocolA, ProtocolB { }
+
+public class ClassG <T> {
+  func foo() -> Int {
+    return 10
+  }
+  
+  func bar() -> Int {
+    return 10
+  }
+}
+
+extension ClassG: ProtocolA, ProtocolB { }
+
+public class ClassH {
+  typealias type = ClassD
+}
+
+func shouldOptimize1<T>(_ x: ClassA<T> & ProtocolA) -> Int {
+  return x.foo()
+}
+
+func shouldOptimize2(_ x: ClassD & ProtocolA) -> Int {
+  return x.foo()
+}
+
+func shouldOptimize3(_ x: ProtocolA & ProtocolB) -> Int {
+  return x.foo() + x.bar()
+}
+
+func shouldOptimize4(_ x: ClassH.type & ProtocolA) -> Int {
+  return x.foo()
+}
+
+//CHECK: entryPoint1
 //CHECK-NOT: init_existential_ref
 //CHECK-NOT: open_existential_ref
 //CHECK-NOT: witness_method
 //CHECK: return
-public func witnessEntryPoint(c: ClassB) -> Int {
-  return shouldOptimizeWitness(c)
+public func entryPoint1(c: ClassB) -> Int {
+  return shouldOptimize1(c)
+}
+
+// TODO: is this broken on master too?
+//public func entryPoint2<T>(c: ClassC<T>) -> Int {
+//  return shouldOptimize1(c)
+//}
+
+//CHECK: entryPoint3
+//CHECK-NOT: init_existential_ref
+//CHECK-NOT: open_existential_ref
+//CHECK-NOT: witness_method
+//CHECK: return
+public func entryPoint3(c: ClassE) -> Int {
+  return shouldOptimize2(c)
+}
+
+//CHECK: entryPoint4
+//CHECK-NOT: init_existential_ref
+//CHECK-NOT: open_existential_ref
+//CHECK-NOT: witness_method
+//CHECK: return
+public func entryPoint4(c: ClassF) -> Int {
+  return shouldOptimize3(c)
+}
+
+//CHECK: entryPoint5
+//CHECK-NOT: init_existential_ref
+//CHECK-NOT: open_existential_ref
+//CHECK-NOT: witness_method
+//CHECK: return
+public func entryPoint5<T>(c: ClassG<T>) -> Int {
+  return shouldOptimize3(c)
+}
+
+//CHECK: entryPoint6
+//CHECK-NOT: init_existential_ref
+//CHECK-NOT: open_existential_ref
+//CHECK-NOT: witness_method
+//CHECK: return
+public func entryPoint6(c: ClassE) -> Int {
+  return shouldOptimize4(c)
 }
