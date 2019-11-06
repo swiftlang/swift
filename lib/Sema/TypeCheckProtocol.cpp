1184492d25834627da159beb84d744464f1ffe7c
Luciano Almeida <passos.luciano@outlook.com>
2019-11-06 12:42:48 -0300

    [Diagnostics] SR-11419 Diagnose protocol stub note in editor mode only (#28101)
    * [TypeChecker] Enclosing stubs protocol note within editor mode
    
    * [test] Removing note from test where there is no -diagnostics-editor-mode flag
    
    * Formatting modified code
    
    * [tests] Fixing tests under validation-tests

--- lib/Sema/TypeCheckProtocol.cpp
+++ lib/Sema/TypeCheckProtocol.cpp
@@ -2758,8 +2758,10 @@ diagnoseMissingWitnesses(MissingWitnessDiagnosisKind Kind) {
           // because editor may assume the fixit is in the same file with the note.
           Diags.diagnose(MissingTypeWitness, diag::no_witnesses_type,
                          MissingTypeWitness->getName());
-          Diags.diagnose(ComplainLoc, diag::missing_witnesses_general).
-            fixItInsertAfter(FixitLocation, FixIt);
+          if (EditorMode) {
+            Diags.diagnose(ComplainLoc, diag::missing_witnesses_general)
+              .fixItInsertAfter(FixitLocation, FixIt);
+          }
         }
         continue;
       }
@@ -2772,15 +2774,17 @@ diagnoseMissingWitnesses(MissingWitnessDiagnosisKind Kind) {
           // we can directly associate the fixit with the note issued to the
           // requirement.
           Diags.diagnose(VD, diag::no_witnesses, getRequirementKind(VD),
-            VD->getFullName(), RequirementType, true).
-              fixItInsertAfter(FixitLocation, FixIt);
+            VD->getFullName(), RequirementType, true)
+              .fixItInsertAfter(FixitLocation, FixIt);
         } else {
           // Otherwise, we have to issue another note to carry the fixit,
           // because editor may assume the fixit is in the same file with the note.
           Diags.diagnose(VD, diag::no_witnesses, getRequirementKind(VD),
             VD->getFullName(), RequirementType, false);
-          Diags.diagnose(ComplainLoc, diag::missing_witnesses_general).
-            fixItInsertAfter(FixitLocation, FixIt);
+          if (EditorMode) {
+            Diags.diagnose(ComplainLoc, diag::missing_witnesses_general)
+              .fixItInsertAfter(FixitLocation, FixIt);
+          }
         }
       } else {
         Diags.diagnose(VD, diag::no_witnesses, getRequirementKind(VD),
