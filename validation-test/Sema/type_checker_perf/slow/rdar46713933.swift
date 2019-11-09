db78b3214b6f914daa54f58cb7eef2859f682448
David Zarzycki <dave@znu.io>
2019-11-09 15:21:56 +0200

    [Testing] Disable sema perf tests during ASAN testing
    Also, add `-solver-expression-time-threshold=1` to a few tests.
    
    This forces sr139, rdar25866240, and rdar23327871 to be moved from the
    "fast" directory to the "slow" directory.

--- validation-test/Sema/type_checker_perf/slow/rdar46713933.swift
+++ validation-test/Sema/type_checker_perf/slow/rdar46713933.swift
@@ -1,5 +1,5 @@
 // RUN: %target-typecheck-verify-swift -solver-expression-time-threshold=1
-// REQUIRES: tools-release
+// REQUIRES: tools-release,no_asan
 
 func wrap<T>(_ key: String, _ value: T) -> T { return value }
 func wrap<T: ExpressibleByIntegerLiteral>(_ key: String, _ value: T) -> T { return value }
