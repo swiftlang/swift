824752547129f0cfdbbfd358a164031a9024972d
Michael Gottesman <mgottesman@apple.com>
2019-11-08 17:40:15 -0800

    [irgen] Force emission of objc class refs for non-foreign clang objc classes whose metadata we use.
    Today in far more cases we are using mangled strings to look up metadata at
    runtime. If we do this for an objc class but for whatever reason we do not have
    any other references to the class, the static linker will fail to link in the
    relevant framework. The reason why this happens is that autolinking is treated
    by the static linker as a hint that a framework may be needed rather than as a
    "one must link against the framework". If there aren't any undefined symbols
    needed by the app from that framework, the linker just will ignore the hint. Of
    course this then causes the class lookup to fail at runtime when we use our
    mangled name to try to lookup the class.
    
    I included an Interpreter test as well as IRGen tests to make sure that we do
    not regress here in the future.
    
    NOTE: The test modifications here are due to my moving the ObjCClasses framework
    out of ./test/Interpreters/Inputs => test/Inputs since I am using it in the
    IRGen test along side the interpreter test.
    
    rdar://56136123

--- lib/IRGen/GenDecl.cpp
+++ lib/IRGen/GenDecl.cpp
@@ -1259,6 +1259,27 @@ void IRGenerator::noteUseOfTypeGlobals(NominalTypeDecl *type,
     }
   }
 
+  // Force emission of ObjC class refs used by type refs.
+  //
+  // Otherwise, autolinking can fail if we try to load the class decl from the
+  // name of the class.
+  if (auto *classDecl = dyn_cast<ClassDecl>(type)) {
+    // The logic behind this predicate is that:
+    //
+    // 1. We need to check if a class decl is foreign to exclude CF classes.
+    //
+    // 2. We want to check that the class decl is objc to exclude c++ classes in
+    //    the future.
+    //
+    // 3. We check that we have a clang node since we want to ensure we have
+    //    something coming from clang, rather than from swift which does not
+    //    have this issue.
+    if (classDecl->hasClangNode() && classDecl->isObjC() && !classDecl->isForeign()) {
+      PrimaryIGM->getAddrOfObjCClassRef(classDecl);
+      return;
+    }
+  }
+
   if (!hasLazyMetadata(type))
     return;
 
---
+++ test/Interpreter/autolink_classlookup_test.swift
@@ -0,0 +1,30 @@
+// RUN: %empty-directory(%t)
+//
+// RUN: %target-clang -fobjc-arc %S/../Inputs/ObjCClasses/ObjCClasses.m -c -o %t/ObjCClasses.o
+// RUN: mkdir -p %t/ObjCClasses.framework/Headers
+// RUN: cp %S/../Inputs/ObjCClasses/ObjCClasses.h %t/ObjCClasses.framework/Headers
+// RUN: mkdir -p %t/ObjCClasses.framework/Modules
+// RUN: cp %S/../Inputs/ObjCClasses/framework.module.map %t/ObjCClasses.framework/Modules/module.modulemap
+// RUN: %target-clang -dynamiclib -fobjc-arc -fmodules %t/ObjCClasses.o -o %t/ObjCClasses.framework/ObjCClasses
+// RUN: %target-codesign %t/ObjCClasses.framework/ObjCClasses
+
+// RUN: %target-build-swift -F %t %s -o %t/main
+// RUN: %target-codesign %t/main
+// RUN: %target-run %t/main %t/ObjCClasses.framework
+
+// REQUIRES: executable_test
+// REQUIRES: objc_interop
+
+// Make sure that functionally if we refer to an objc class via autolinking such
+// that the class is not needed by the module directly, we still emit an objc
+// class ref to ensure that the static linker links against the autolinked
+// library.
+
+import ObjCClasses
+
+func main() {
+  let a = Animal()
+  print(a)
+}
+
+main()
