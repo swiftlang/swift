0cd59069d342fb88eb4d8ff397d786322d8cb2c0
Suyash Srijan <suyashsrijan@outlook.com>
2019-11-14 01:42:07 +0000

    [Test] Update a few failing tests

--- test/SILGen/property_wrapper_coroutine.swift
+++ test/SILGen/property_wrapper_coroutine.swift
@@ -19,12 +19,20 @@ struct State1: StateProtocol {
 
 var state = State()
 state.values = Array(repeating: "", count: 20000)
-// CHECK:  [[BEGIN_ACCESS_MODIFY:%.*]] = begin_access [modify] [dynamic] {{%.*}} : $*State
-// CHECK:  [[REF_MODIFY:%.*]] = function_ref @$s26property_wrapper_coroutine5StateV6valuesSaySSGvM : $@yield_once @convention(method) (@inout State) -> @yields @inout Array<String>
-// CHECK:  ([[RES1:%.*]], {{%.*}}) = begin_apply [[REF_MODIFY]]([[BEGIN_ACCESS_MODIFY]]) : $@yield_once @convention(method) (@inout State) -> @yields @inout Array<String>
+state.values[1000] = "foo"
+
+let state1 = State1()
+_ = state1.someValues
+
+// >> Check that the subscript assignment uses the _modify coroutine
+
+// CHECK:  {{%.*}} = begin_access [modify] [dynamic] {{%.*}} : $*State
+// CHECK:  [[REF_VALUES_MODIFY:%.*]] = function_ref @$s26property_wrapper_coroutine5StateV6valuesSaySSGvM : $@yield_once @convention(method) (@inout State) -> @yields @inout Array<String>
+// CHECK:  ([[RES1:%.*]], {{%.*}}) = begin_apply [[REF_VALUES_MODIFY]]({{%.*}}) : $@yield_once @convention(method) (@inout State) -> @yields @inout Array<String>
 // CHECK:  [[REF_ARRAY_SUBSCRIPT:%.*]] = function_ref @$sSayxSiciM : $@yield_once @convention(method) <τ_0_0> (Int, @inout Array<τ_0_0>) -> @yields @inout τ_0_0
 // CHECK:  ({{%.*}}, {{%.*}}) = begin_apply [[REF_ARRAY_SUBSCRIPT]]<String>({{%.*}}, [[RES1]]) : $@yield_once @convention(method) <τ_0_0> (Int, @inout Array<τ_0_0>) -> @yields @inout τ_0_0
-state.values[1000] = "foo"
+
+// >> Check that the _modify coroutine is synthesized properly
 
 // CHECK-LABEL: sil hidden [transparent] [ossa] @$s26property_wrapper_coroutine5StateV6valuesSaySSGvM : $@yield_once @convention(method) (@inout State) -> @yields @inout Array<String> {
 // CHECK: bb0([[STATE:%.*]] : $*State):
@@ -44,15 +52,14 @@ state.values[1000] = "foo"
 // CHECK:  unwind
 // CHECK-END: }
 
-var state1 = State1()
-_ = state1.someValues
+// >> Check that the _read coroutine is synthesized properly
 
 // CHECK-LABEL: sil shared [ossa] @$s26property_wrapper_coroutine6State1V10someValuesSaySSGvr : $@yield_once @convention(method) (@guaranteed State1) -> @yields @guaranteed Array<String> {
 // CHECK: bb0([[STATE1:%.*]] : @guaranteed $State1):
 // CHECK:  debug_value [[SELF:%.*]] : $State1, let, name "self", argno {{.*}}
 // CHECK:  [[EXTRACT_VALUE:%.*]] = struct_extract %0 : $State1, #State1._someValues
 // CHECK:  [[COPY_VALUE:%.*]] = copy_value [[EXTRACT_VALUE]] : $TestWrapper<Array<String>>
-// CHECK:  [[BEGIN_BORROW:%.*]] = begin_borrow [[COPY]] : $TestWrapper<Array<String>>
+// CHECK:  [[BEGIN_BORROW:%.*]] = begin_borrow [[COPY_VALUE]] : $TestWrapper<Array<String>>
 // CHECK:  [[EXTRACT_WRAPPEDVALUE:%.*]] = struct_extract [[BEGIN_BORROW]] : $TestWrapper<Array<String>>, #TestWrapper.wrappedValue
 // CHECK:  yield [[EXTRACT_WRAPPEDVALUE]] : $Array<String>, resume bb1, unwind bb2
 //
