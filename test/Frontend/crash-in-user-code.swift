322ad482b6d053a799255c08a739950e23c23601
Tapan Thaker <tapanth@uber.com>
2019-11-15 07:28:50 -0800

    [FrondEnd] Wrap RunInmediately() in a pretty stack trace indicating we are running user code

---
+++ test/Frontend/crash-in-user-code.swift
@@ -0,0 +1,19 @@
+
+// RUN: echo %s > %t.filelist.txt
+// RUN: not --crash %target-swift-frontend -interpret -filelist %t.filelist.txt 2>&1 | %FileCheck %s
+
+// UNSUPPORTED: OS=ios
+// UNSUPPORTED: OS=tvos
+// UNSUPPORTED: OS=watchos
+
+// CHECK: Stack dump:
+// CHECK-NEXT: Program arguments:
+// CHECK-NEXT: Swift version
+// CHECK-NEXT: Contents of {{.*}}.filelist.txt:
+// CHECK-NEXT: ---
+// CHECK-NEXT: crash-in-user-code.swift
+// CHECK-NEXT: ---
+// CHECK-NEXT: While running user code "{{.*}}crash-in-user-code.swift"
+
+let x: Int? = nil
+x!
