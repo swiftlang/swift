21a7fba89593a6d46c98f4064ce2df711fa6f12d
Doug Gregor <dgregor@apple.com>
2019-11-14 17:21:44 -0800

    More tests for Bool raw values of enums

--- test/decl/enum/bool_raw_value.swift
+++ test/decl/enum/bool_raw_value.swift
@@ -14,3 +14,13 @@ enum IsRecursive : Bool, Equatable, Hashable {
   case recursive=false
   case nonrecursive // expected-error{{enum case must declare a raw value when the preceding raw value is not an integer}}
 }
+
+enum IsRecursiveBad1Integral : Bool, Equatable, Hashable {
+  case recursive = 0
+  case nonrecursive
+}
+
+// expected-error @+1{{'IsRecursiveBad2' declares raw type 'Int', but does not conform to RawRepresentable and conformance could not be synthesized}}
+enum IsRecursiveBad2 : Int, Equatable, Hashable {
+  case recursive = false // expected-error{{cannot convert value of type 'Bool' to raw type 'Int'}}
+}
