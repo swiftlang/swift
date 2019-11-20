8e45f44da4c407c406d12d1930f97ffeb84d09a6
Harlan Haskins <harlan@apple.com>
2019-11-19 16:46:22 -0800

    [Serialization] Serialize hasMissingDesignatedInitializers
    Since this is going to be something modules tell clients, rather than something clients discover about modules, serialize it.

--- lib/Serialization/Serialization.cpp
+++ lib/Serialization/Serialization.cpp
@@ -3139,9 +3139,7 @@ public:
     uint8_t rawAccessLevel =
       getRawStableAccessLevel(theClass->getFormalAccess());
 
-    bool inheritsSuperclassInitializers =
-        const_cast<ClassDecl *>(theClass)->
-          inheritsSuperclassInitializers();
+    auto mutableClass = const_cast<ClassDecl *>(theClass);
 
     unsigned abbrCode = S.DeclTypeAbbrCodes[ClassLayout::Code];
     ClassLayout::emitRecord(S.Out, S.ScratchRecord, abbrCode,
@@ -3149,7 +3147,8 @@ public:
                             contextID.getOpaqueValue(),
                             theClass->isImplicit(),
                             theClass->isObjC(),
-                            inheritsSuperclassInitializers,
+                            mutableClass->inheritsSuperclassInitializers(),
+                            mutableClass->hasMissingDesignatedInitializers(),
                             S.addGenericSignatureRef(
                                              theClass->getGenericSignature()),
                             S.addTypeRef(theClass->getSuperclass()),
