d31f6ed42373ef36f85d8d6fb23f3c52461579dd
Robert Widmann <devteam.codafi@gmail.com>
2019-11-20 14:37:52 -0800

    Drop the ASTContext parameter on FindCapturedVars
    Just get it from the decl context

--- lib/Sema/TypeCheckCaptures.cpp
+++ lib/Sema/TypeCheckCaptures.cpp
@@ -45,13 +45,12 @@ class FindCapturedVars : public ASTWalker {
   bool NoEscape, ObjC, IsGenericFunction;
 
 public:
-  FindCapturedVars(ASTContext &Context,
-                   SourceLoc CaptureLoc,
+  FindCapturedVars(SourceLoc CaptureLoc,
                    DeclContext *CurDC,
                    bool NoEscape,
                    bool ObjC,
                    bool IsGenericFunction)
-      : Context(Context), CaptureLoc(CaptureLoc), CurDC(CurDC),
+      : Context(CurDC->getASTContext()), CaptureLoc(CaptureLoc), CurDC(CurDC),
         NoEscape(NoEscape), ObjC(ObjC), IsGenericFunction(IsGenericFunction) {}
 
   CaptureInfo getCaptureInfo() const {
@@ -595,8 +594,7 @@ void TypeChecker::computeCaptures(AnyFunctionRef AFR) {
     isGeneric = (AFD->getGenericParams() != nullptr);
 
   auto &Context = AFR.getAsDeclContext()->getASTContext();
-  FindCapturedVars finder(Context,
-                          AFR.getLoc(),
+  FindCapturedVars finder(AFR.getLoc(),
                           AFR.getAsDeclContext(),
                           AFR.isKnownNoEscape(),
                           AFR.isObjC(),
@@ -613,8 +611,7 @@ void TypeChecker::computeCaptures(AnyFunctionRef AFR) {
   if (auto *AFD = AFR.getAbstractFunctionDecl()) {
     for (auto *P : *AFD->getParameters()) {
       if (auto E = P->getTypeCheckedDefaultExpr()) {
-        FindCapturedVars finder(Context,
-                                E->getLoc(),
+        FindCapturedVars finder(E->getLoc(),
                                 AFD,
                                 /*isNoEscape=*/false,
                                 /*isObjC=*/false,
@@ -680,14 +677,15 @@ void TypeChecker::checkPatternBindingCaptures(NominalTypeDecl *typeDecl) {
       if (init == nullptr)
         continue;
 
-      FindCapturedVars finder(ctx,
-                              init->getLoc(),
-                              PBD->getInitContext(i),
+      auto *DC = PBD->getInitContext(i);
+      FindCapturedVars finder(init->getLoc(),
+                              DC,
                               /*NoEscape=*/false,
                               /*ObjC=*/false,
                               /*IsGenericFunction*/false);
       init->walk(finder);
 
+      auto &ctx = DC->getASTContext();
       if (finder.getDynamicSelfCaptureLoc().isValid() && !isLazy(PBD)) {
         ctx.Diags.diagnose(finder.getDynamicSelfCaptureLoc(),
                            diag::dynamic_self_stored_property_init);
