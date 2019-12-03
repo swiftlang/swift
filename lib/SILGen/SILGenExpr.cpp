351c94dbf2b814a60a9a321134c759747d6d6c9a
Joe Groff <jgroff@apple.com>
2019-12-03 11:35:48 -0800

    SIL: Distinguish "compatible convention" and "compatible representation" function conversions.
    We want to be able to use different representations for function types with otherwise compatible
    calling conventions. Distinguish these concepts in the `checkForABIDifferences` SIL APIs, so that
    we correctly handle representation-only conversions, which can be handled by `convert_function`,
    from full reabstractions, making sure to note that the representation-only case is not transitive
    for function arguments, since a function that takes a function with a representation change needs
    a thunk to change the argument's representation.

--- lib/SILGen/SILGenExpr.cpp
+++ lib/SILGen/SILGenExpr.cpp
@@ -1463,7 +1463,8 @@ static ManagedValue convertCFunctionSignature(SILGenFunction &SGF,
   // ABI-compatible, since we can't emit a thunk.
   switch (SGF.SGM.Types.checkForABIDifferences(SGF.SGM.M,
                                                loweredResultTy, loweredDestTy)){
-  case TypeConverter::ABIDifference::Trivial:
+  case TypeConverter::ABIDifference::CompatibleRepresentation:
+  case TypeConverter::ABIDifference::CompatibleCallingConvention:
     result = fnEmitter();
     assert(result.getType() == loweredResultTy);
 
@@ -1482,7 +1483,8 @@ static ManagedValue convertCFunctionSignature(SILGenFunction &SGF,
     result = SGF.emitUndef(loweredDestTy);
     break;
 
-  case TypeConverter::ABIDifference::ThinToThick:
+  case TypeConverter::ABIDifference::CompatibleCallingConvention_ThinToThick:
+  case TypeConverter::ABIDifference::CompatibleRepresentation_ThinToThick:
     llvm_unreachable("Cannot have thin to thick conversion here");
   }
 
