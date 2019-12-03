02a4ddd07b677d3172d5d454d022748fa23bd4e4
Gogul Balakrishnan <bgogul@google.com>
2019-12-02 16:11:48 -0800

    More style fixes in comments.

--- lib/SILOptimizer/Analysis/DifferentiableActivityAnalysis.cpp
+++ lib/SILOptimizer/Analysis/DifferentiableActivityAnalysis.cpp
@@ -1,4 +1,4 @@
-//===--- ActivityAnalysis.h - Differentiable Activity Analysis -*- C++ -*-===//
+//===--- DifferentiableActivityAnalysis.h ----------------------*- C++ -*-===//
 //
 // This source file is part of the Swift.org open source project
 //
