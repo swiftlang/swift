4f825124df957985b6d6288a15fed13a2158547a
Yuta Saito <kateinoigakukun@gmail.com>
2019-12-03 06:29:41 +0000

    [WASM] Fix test case for WASI

--- test/Driver/target-triple.swift
+++ test/Driver/target-triple.swift
@@ -1,2 +1,2 @@
-// RUN: %swiftc_driver -target wasm32-unknown-unknown-wasm -sdk %S/Inputs/wasm32.sdk -typecheck %s
-// RUN: %swiftc_driver -target wasm64-unknown-unknown-wasm -sdk %S/Inputs/wasm64.sdk -typecheck %s
+// RUN: %swiftc_driver -target wasm32-unknown-wasi -sdk %S/Inputs/wasm.sdk -typecheck %s
+// RUN: %swiftc_driver -target wasm64-unknown-wasi -sdk %S/Inputs/wasm.sdk -typecheck %s
