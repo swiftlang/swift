e870b6d232e7df80b7edc6b68a6ad94c9d3da350
Dan Zheng <danielzheng@google.com>
2019-12-05 23:18:26 -0800

    Fix test based on upstream changes.

--- test/AutoDiff/reabstraction_thunk.swift
+++ test/AutoDiff/reabstraction_thunk.swift
@@ -8,9 +8,9 @@
 func id<T>(_ x: T) -> T { x }
 let _: @differentiable (Float) -> Float = id
 
-// CHECK-LABEL: sil_differentiability_witness shared [serialized] [parameters 0] [results 0] @$sS2fIegnr_S2fIegyd_TR : $@convention(thin) (Float, @guaranteed @callee_guaranteed (@in_guaranteed Float) -> @out Float) -> Float {
+// CHECK-LABEL: sil_differentiability_witness private [parameters 0] [results 0] @$sS2fIegnr_S2fIegyd_TR : $@convention(thin) (Float, @guaranteed @callee_guaranteed (@in_guaranteed Float) -> @out Float) -> Float {
 // CHECK:   jvp: @AD__$sS2fIegnr_S2fIegyd_TR__jvp_src_0_wrt_0 : $@convention(thin) (Float, @guaranteed @differentiable @callee_guaranteed (@in_guaranteed Float) -> @out Float) -> (Float, @owned @callee_guaranteed (Float) -> Float)
 // CHECK:   vjp: @AD__$sS2fIegnr_S2fIegyd_TR__vjp_src_0_wrt_0 : $@convention(thin) (Float, @guaranteed @differentiable @callee_guaranteed (@in_guaranteed Float) -> @out Float) -> (Float, @owned @callee_guaranteed (Float) -> Float)
 // CHECK: }
 
-// CHECK-LABEL: sil hidden [serializable] @AD__$sS2fIegnr_S2fIegyd_TR__vjp_src_0_wrt_0 : $@convention(thin) (Float, @guaranteed @differentiable @callee_guaranteed (@in_guaranteed Float) -> @out Float) -> (Float, @owned @callee_guaranteed (Float) -> Float) {
+// CHECK-LABEL: sil private @AD__$sS2fIegnr_S2fIegyd_TR__vjp_src_0_wrt_0 : $@convention(thin) (Float, @guaranteed @differentiable @callee_guaranteed (@in_guaranteed Float) -> @out Float) -> (Float, @owned @callee_guaranteed (Float) -> Float) {
