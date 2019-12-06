00407857d030b3427013ea3fd9be0c5ec2ed594d
Robert Widmann <devteam.codafi@gmail.com>
2019-12-06 14:18:31 -0800

    [NFC] Swap CodeCompletion's lookupQualified for TypeChecker's
    This call relied on semantic member synthesis, so now it has to go through TypeChecker::lookupQualified.

--- lib/Sema/TypeChecker.cpp
+++ lib/Sema/TypeChecker.cpp
@@ -711,3 +711,8 @@ TypeChecker::getDeclTypeCheckingSemantics(ValueDecl *decl) {
 void swift::bindExtensions(SourceFile &SF) {
   ::bindExtensions(SF);
 }
+
+LookupResult
+swift::lookupSemanticMember(DeclContext *DC, Type ty, DeclName name) {
+  return TypeChecker::lookupMember(DC, ty, name, None);
+}
