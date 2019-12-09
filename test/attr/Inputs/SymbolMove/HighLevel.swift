6be82b35cbb09df9e2c8256122f4305a9cbf86e9
Xi Ge <xi_ge@apple.com>
2019-12-09 12:23:57 -0800

    test: add more ABI stability test for @_originallyDefinedIn

--- test/attr/Inputs/SymbolMove/HighLevel.swift
+++ test/attr/Inputs/SymbolMove/HighLevel.swift
@@ -3,3 +3,10 @@
 public func printMessage() {
   printMessageMoved()
 }
+public class CandyBox: Box {
+  public typealias Item = Candy
+  public var ItemKind: String { return getItem().kind }
+  let itemInBox: Item
+  public init(_ itemInBox: Item) { self.itemInBox = itemInBox }
+  public func getItem() -> Item { return itemInBox }
+}
