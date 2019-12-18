30876a42dee9161fbf6e5da655c8fde42c851368
Pavel Yaskevich <pyaskevich@apple.com>
2019-12-18 13:06:58 -0800

    [TypeChecker] NFC: Mark test-case for rdar://problem/22022980 as "slow"
    It used to be "fast" before because `selectDisjunction` used to pick
    inner `==`s before outer one (which compares arrays) but now, since
    constraint generation for the closure body is delayed, it looks
    like the opposite is happening which makes this test-case go exponential.

--- validation-test/Sema/type_checker_perf/slow/rdar22022980.swift
+++ validation-test/Sema/type_checker_perf/slow/rdar22022980.swift
@@ -2,3 +2,4 @@
 // REQUIRES: tools-release,no_asan
 
 _ = [1, 3, 5, 7, 11].filter{ $0 == 1 || $0 == 3 || $0 == 11 || $0 == 1 || $0 == 3 || $0 == 11 } == [ 1, 3, 11 ]
+// expected-error@-1 {{unable to type-check}}
