swiftlang/swift

commit 14ee6c5d1ccdd3da0c32323db21d2b988ee22e18

[AutoDiff] Enable `@derivative` attribute qualified declaration names. (#28892)
Enable qualified declaration names in `@derivative` attribute, just like
`@transpose` attribute.

`DerivativeAttr` now stores a base type `TypeRepr *`, which is non-null for
parsed attributes that reference a qualified original declaration.

Add `TypeResolutionFlags::AllowModule` flag to enable module lookup via
`TypeChecker::lookupMember` given a `ModuleType`.

Add tests for type-qualified and module-qualified declaration names.

Resolves TF-1058.

lib/Sema/TypeCheckAttr.cpp
@@ -3547,8 +3547,18 @@ static bool typeCheckDerivativeAttr(ASTContext &Ctx, Decl *D,
         return derivative->getParent() == func->getParent();
       };
 
-  auto lookupOptions =
-      defaultMemberLookupOptions | NameLookupFlags::IgnoreAccessControl;
+  auto resolution = TypeResolution::forContextual(derivative->getDeclContext());
+  Type baseType;
+  if (auto *baseTypeRepr = attr->getBaseTypeRepr()) {
+    TypeResolutionOptions options = None;
+    options |= TypeResolutionFlags::AllowModule;
+    baseType = resolution.resolveType(baseTypeRepr, options);
+  }
+  if (baseType && baseType->hasError())
+    return;
+  auto lookupOptions = attr->getBaseTypeRepr()
+                           ? defaultMemberLookupOptions
+                           : defaultUnqualifiedLookupOptions;
   auto derivativeTypeCtx = derivative->getInnermostTypeContext();
   if (!derivativeTypeCtx)
     derivativeTypeCtx = derivative->getParent();
@@ -3556,7 +3566,7 @@ static bool typeCheckDerivativeAttr(ASTContext &Ctx, Decl *D,
 
   // Look up original function.
   auto *originalAFD = findAbstractFunctionDecl(
-      originalName.Name, originalName.Loc.getBaseNameLoc(), /*baseType*/ Type(),
+      originalName.Name, originalName.Loc.getBaseNameLoc(), baseType,
       derivativeTypeCtx, isValidOriginal, noneValidDiagnostic,
       ambiguousDiagnostic, notFunctionDiagnostic, lookupOptions,
       hasValidTypeContext, invalidTypeContextDiagnostic);
@@ -3678,7 +3688,7 @@ static bool typeCheckDerivativeAttr(ASTContext &Ctx, Decl *D,
   }
 
   // Reject different-file derivative registration.
-  // TODO(TF-1021): Lift this restriction.
+  // TODO(TF-1021): Lift same-file derivative registration restriction.
   if (originalAFD->getParentSourceFile() != derivative->getParentSourceFile()) {
     diags.diagnose(attr->getLocation(),
                    diag::derivative_attr_not_in_same_file_as_original);

include/swift/AST/Attr.h
@@ -1791,6 +1791,11 @@ class DerivativeAttr final
       private llvm::TrailingObjects<DerivativeAttr, ParsedAutoDiffParameter> {
   friend TrailingObjects;
 
+  /// The base type repr for the referenced original function. This field is
+  /// non-null only for parsed attributes that reference a qualified original
+  /// declaration. This field is not serialized; type-checking uses it to
+  /// resolve the original declaration, which is serialized.
+  TypeRepr *BaseTypeRepr;
   /// The original function name.
   DeclNameRefWithLoc OriginalFunctionName;
   /// The original function declaration, resolved by the type checker.
@@ -1803,23 +1808,27 @@ class DerivativeAttr final
   Optional<AutoDiffDerivativeFunctionKind> Kind = None;
 
   explicit DerivativeAttr(bool implicit, SourceLoc atLoc, SourceRange baseRange,
-                          DeclNameRefWithLoc original,
+                          TypeRepr *baseTypeRepr, DeclNameRefWithLoc original,
                           ArrayRef<ParsedAutoDiffParameter> params);
 
   explicit DerivativeAttr(bool implicit, SourceLoc atLoc, SourceRange baseRange,
-                          DeclNameRefWithLoc original, IndexSubset *indices);
+                          TypeRepr *baseTypeRepr, DeclNameRefWithLoc original,
+                          IndexSubset *parameterIndices);
 
 public:
   static DerivativeAttr *create(ASTContext &context, bool implicit,
                                 SourceLoc atLoc, SourceRange baseRange,
+                                TypeRepr *baseTypeRepr,
                                 DeclNameRefWithLoc original,
                                 ArrayRef<ParsedAutoDiffParameter> params);
 
   static DerivativeAttr *create(ASTContext &context, bool implicit,
                                 SourceLoc atLoc, SourceRange baseRange,
+                                TypeRepr *baseTypeRepr,
                                 DeclNameRefWithLoc original,
-                                IndexSubset *indices);
+                                IndexSubset *parameterIndices);
 
+  TypeRepr *getBaseTypeRepr() const { return BaseTypeRepr; }
   DeclNameRefWithLoc getOriginalFunctionName() const {
     return OriginalFunctionName;
   }
@@ -1876,9 +1885,10 @@ class TransposeAttr final
       private llvm::TrailingObjects<TransposeAttr, ParsedAutoDiffParameter> {
   friend TrailingObjects;
 
-  /// The base type of the original function.
-  /// This is non-null only when the original function is not top-level (i.e. it
-  /// is an instance/static method).
+  /// The base type repr for the referenced original function. This field is
+  /// non-null only for parsed attributes that reference a qualified original
+  /// declaration. This field is not serialized; type-checking uses it to
+  /// resolve the original declaration, which is serialized.
   TypeRepr *BaseTypeRepr;
   /// The original function name.
   DeclNameRefWithLoc OriginalFunctionName;
@@ -1895,7 +1905,7 @@ class TransposeAttr final
 
   explicit TransposeAttr(bool implicit, SourceLoc atLoc, SourceRange baseRange,
                          TypeRepr *baseType, DeclNameRefWithLoc original,
-                         IndexSubset *indices);
+                         IndexSubset *parameterIndices);
 
 public:
   static TransposeAttr *create(ASTContext &context, bool implicit,
@@ -1906,7 +1916,7 @@ public:
   static TransposeAttr *create(ASTContext &context, bool implicit,
                                SourceLoc atLoc, SourceRange baseRange,
                                TypeRepr *baseType, DeclNameRefWithLoc original,
-                               IndexSubset *indices);
+                               IndexSubset *parameterIndices);
 
   TypeRepr *getBaseTypeRepr() const { return BaseTypeRepr; }
   DeclNameRefWithLoc getOriginalFunctionName() const {

lib/AST/Attr.cpp
@@ -1518,41 +1518,43 @@ void DifferentiableAttr::print(llvm::raw_ostream &OS, const Decl *D,
 }
 
 DerivativeAttr::DerivativeAttr(bool implicit, SourceLoc atLoc,
-                               SourceRange baseRange,
+                               SourceRange baseRange, TypeRepr *baseTypeRepr,
                                DeclNameRefWithLoc originalName,
                                ArrayRef<ParsedAutoDiffParameter> params)
     : DeclAttribute(DAK_Derivative, atLoc, baseRange, implicit),
-      OriginalFunctionName(std::move(originalName)),
+      BaseTypeRepr(baseTypeRepr), OriginalFunctionName(std::move(originalName)),
       NumParsedParameters(params.size()) {
   std::copy(params.begin(), params.end(),
             getTrailingObjects<ParsedAutoDiffParameter>());
 }
 
 DerivativeAttr::DerivativeAttr(bool implicit, SourceLoc atLoc,
-                               SourceRange baseRange,
+                               SourceRange baseRange, TypeRepr *baseTypeRepr,
                                DeclNameRefWithLoc originalName,
-                               IndexSubset *indices)
+                               IndexSubset *parameterIndices)
     : DeclAttribute(DAK_Derivative, atLoc, baseRange, implicit),
-      OriginalFunctionName(std::move(originalName)), ParameterIndices(indices) {
-}
+      BaseTypeRepr(baseTypeRepr), OriginalFunctionName(std::move(originalName)),
+      ParameterIndices(parameterIndices) {}
 
 DerivativeAttr *
 DerivativeAttr::create(ASTContext &context, bool implicit, SourceLoc atLoc,
-                       SourceRange baseRange, DeclNameRefWithLoc originalName,
+                       SourceRange baseRange, TypeRepr *baseTypeRepr,
+                       DeclNameRefWithLoc originalName,
                        ArrayRef<ParsedAutoDiffParameter> params) {
   unsigned size = totalSizeToAlloc<ParsedAutoDiffParameter>(params.size());
   void *mem = context.Allocate(size, alignof(DerivativeAttr));
-  return new (mem) DerivativeAttr(implicit, atLoc, baseRange,
+  return new (mem) DerivativeAttr(implicit, atLoc, baseRange, baseTypeRepr,
                                   std::move(originalName), params);
 }
 
 DerivativeAttr *DerivativeAttr::create(ASTContext &context, bool implicit,
                                        SourceLoc atLoc, SourceRange baseRange,
+                                       TypeRepr *baseTypeRepr,
                                        DeclNameRefWithLoc originalName,
-                                       IndexSubset *indices) {
+                                       IndexSubset *parameterIndices) {
   void *mem = context.Allocate(sizeof(DerivativeAttr), alignof(DerivativeAttr));
-  return new (mem) DerivativeAttr(implicit, atLoc, baseRange,
-                                  std::move(originalName), indices);
+  return new (mem) DerivativeAttr(implicit, atLoc, baseRange, baseTypeRepr,
+                                  std::move(originalName), parameterIndices);
 }
 
 TransposeAttr::TransposeAttr(bool implicit, SourceLoc atLoc,
@@ -1568,10 +1570,11 @@ TransposeAttr::TransposeAttr(bool implicit, SourceLoc atLoc,
 
 TransposeAttr::TransposeAttr(bool implicit, SourceLoc atLoc,
                              SourceRange baseRange, TypeRepr *baseTypeRepr,
-                             DeclNameRefWithLoc originalName, IndexSubset *indices)
+                             DeclNameRefWithLoc originalName,
+                             IndexSubset *parameterIndices)
     : DeclAttribute(DAK_Transpose, atLoc, baseRange, implicit),
       BaseTypeRepr(baseTypeRepr), OriginalFunctionName(std::move(originalName)),
-      ParameterIndices(indices) {}
+      ParameterIndices(parameterIndices) {}
 
 TransposeAttr *TransposeAttr::create(ASTContext &context, bool implicit,
                                      SourceLoc atLoc, SourceRange baseRange,
@@ -1588,10 +1591,10 @@ TransposeAttr *TransposeAttr::create(ASTContext &context, bool implicit,
                                      SourceLoc atLoc, SourceRange baseRange,
                                      TypeRepr *baseType,
                                      DeclNameRefWithLoc originalName,
-                                     IndexSubset *indices) {
+                                     IndexSubset *parameterIndices) {
   void *mem = context.Allocate(sizeof(TransposeAttr), alignof(TransposeAttr));
   return new (mem) TransposeAttr(implicit, atLoc, baseRange, baseType,
-                                 std::move(originalName), indices);
+                                 std::move(originalName), parameterIndices);
 }
 
 ImplementsAttr::ImplementsAttr(SourceLoc atLoc, SourceRange range,

lib/Parse/ParseDecl.cpp
@@ -1111,7 +1111,8 @@ static bool parseBaseTypeForQualifiedDeclName(Parser &P, TypeRepr *&baseType) {
   return false;
 }
 
-/// parseQualifiedDeclName
+/// Parses an optional base type, followed by a declaration name.
+/// Returns true on error (if declaration name could not be parsed).
 ///
 /// \verbatim
 ///   qualified-decl-name:
@@ -1120,8 +1121,6 @@ static bool parseBaseTypeForQualifiedDeclName(Parser &P, TypeRepr *&baseType) {
 ///     identifier generic-args? ('.' identifier generic-args?)*
 /// \endverbatim
 ///
-/// Parses an optional base type, followed by a declaration name.
-/// Returns true on error (if declaration name could not be parsed).
 // TODO(TF-1066): Use module qualified name syntax/parsing instead of custom
 // qualified name syntax/parsing.
 static bool parseQualifiedDeclName(Parser &P, Diag<> nameParseError,
@@ -1147,7 +1146,8 @@ static bool parseQualifiedDeclName(Parser &P, Diag<> nameParseError,
 ///
 /// \verbatim
 ///   derivative-attribute-arguments:
-///     '(' 'of' ':' decl-name (',' differentiation-params-clause)? ')'
+///     '(' 'of' ':' qualified-decl-name (',' differentiation-params-clause)?
+///     ')'
 /// \endverbatim
 ParserResult<DerivativeAttr> Parser::parseDerivativeAttribute(SourceLoc atLoc,
                                                               SourceLoc loc) {
@@ -1206,16 +1206,16 @@ ParserResult<DerivativeAttr> Parser::parseDerivativeAttribute(SourceLoc atLoc,
              /*DeclModifier*/ false);
     return makeParserError();
   }
-  return ParserResult<DerivativeAttr>(
-      DerivativeAttr::create(Context, /*implicit*/ false, atLoc,
-                             SourceRange(loc, rParenLoc), original, params));
+  return ParserResult<DerivativeAttr>(DerivativeAttr::create(
+      Context, /*implicit*/ false, atLoc, SourceRange(loc, rParenLoc), baseType,
+      original, params));
 }
 
 /// Parse a `@transpose(of:)` attribute, returning true on error.
 ///
 /// \verbatim
 ///   transpose-attribute-arguments:
-///     '(' 'of' ':' decl-name (',' transposed-params-clause)? ')'
+///     '(' 'of' ':' qualified-decl-name (',' transposed-params-clause)? ')'
 /// \endverbatim
 ParserResult<TransposeAttr> Parser::parseTransposeAttribute(SourceLoc atLoc,
                                                             SourceLoc loc) {

lib/Sema/TypeCheckType.cpp
@@ -1644,6 +1644,10 @@ Type TypeChecker::resolveIdentifierType(
   if (!result) return nullptr;
 
   if (auto moduleTy = result->getAs<ModuleType>()) {
+    // Allow module types only if flag is specified.
+    if (options.contains(TypeResolutionFlags::AllowModule))
+      return moduleTy;
+    // Otherwise, emit an error.
     if (!options.contains(TypeResolutionFlags::SilenceErrors)) {
       auto moduleName = moduleTy->getModule()->getName();
       diags.diagnose(Components.back()->getNameLoc(),

lib/Sema/TypeCheckType.h
@@ -66,6 +66,9 @@ enum class TypeResolutionFlags : uint16_t {
 
   /// Whether we should not produce diagnostics if the type is invalid.
   SilenceErrors = 1 << 10,
+
+  /// Whether to allow module declaration types.
+  AllowModule = 1 << 11
 };
 
 /// Type resolution contexts that require special handling.

lib/Serialization/Deserialization.cpp
@@ -4202,11 +4202,12 @@ llvm::Error DeclDeserializer::deserializeDeclAttributes() {
           parametersBitVector[i] = parameters[i];
         auto *indices = IndexSubset::get(ctx, parametersBitVector);
 
-        auto *derivAttr = DerivativeAttr::create(
-            ctx, isImplicit, SourceLoc(), SourceRange(), origName, indices);
-        derivAttr->setOriginalFunction(origDecl);
-        derivAttr->setDerivativeKind(*derivativeKind);
-        Attr = derivAttr;
+        auto *derivativeAttr =
+            DerivativeAttr::create(ctx, isImplicit, SourceLoc(), SourceRange(),
+                                   /*baseType*/ nullptr, origName, indices);
+        derivativeAttr->setOriginalFunction(origDecl);
+        derivativeAttr->setDerivativeKind(*derivativeKind);
+        Attr = derivativeAttr;
         break;
       }
 

test/AutoDiff/Sema/derivative_attr_type_checking.swift
@@ -195,7 +195,8 @@ extension StaticMethod {
     return (x, { $0 })
   }
 
-  @derivative(of: foo)
+  // Test qualified declaration name.
+  @derivative(of: StaticMethod.foo)
   static func vjpFoo(x: Float) -> (value: Float, pullback: (Float) -> Float) {
     return (x, { $0 })
   }
@@ -230,6 +231,14 @@ extension InstanceMethod {
     value: Self, differential: (TangentVector, TangentVector) -> (TangentVector)
   ) {
     return (x, { $0 + $1 })
+  }
+
+  // Test qualified declaration name.
+  @derivative(of: InstanceMethod.foo, wrt: x)
+  func jvpFooWrtX(x: Self) -> (
+    value: Self, differential: (TangentVector) -> (TangentVector)
+  ) {
+    return (x, { $0 })
   }
 
   @derivative(of: generic)

commit 2692abbe2ee08fc9569b9f7eb23442c322c1c4d9

[AutoDiff] Fix build error.
Fix build error introduced in https://github.com/apple/swift/pull/28892.

Caused by not re-running CI after https://github.com/apple/swift/pull/28879
was merged.

lib/Sema/TypeCheckAttr.cpp
@@ -3555,7 +3555,7 @@ static bool typeCheckDerivativeAttr(ASTContext &Ctx, Decl *D,
     baseType = resolution.resolveType(baseTypeRepr, options);
   }
   if (baseType && baseType->hasError())
-    return;
+    return true;
   auto lookupOptions = attr->getBaseTypeRepr()
                            ? defaultMemberLookupOptions
                            : defaultUnqualifiedLookupOptions;