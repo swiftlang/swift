70bcf9a1de3310b851646aff96c7e60f2ac68217
Luciano Almeida <passos.luciano@outlook.com>
2019-12-22 17:29:49 -0300

    [tests][Driver] Adding Debug/Release test cases for warning for -enable-library-evolution and -enable-testing compatibility issues

--- test/Driver/test_mode_library_evolution.swift
+++ test/Driver/test_mode_library_evolution.swift
@@ -1,5 +1,14 @@
-// RUN: %swiftc_driver -driver-print-jobs -enable-testing -enable-library-evolution %s 2>&1 | %FileCheck %s
+// RUN: %swiftc_driver -driver-print-jobs -enable-testing -enable-library-evolution %s 2>&1 | %FileCheck --check-prefix NO_O %s
+// RUN: %swiftc_driver -driver-print-jobs -enable-testing -enable-library-evolution -Osize %s 2>&1 | %FileCheck --check-prefix O_SIZE %s
+// RUN: %swiftc_driver -driver-print-jobs -enable-testing -enable-library-evolution -Onone %s 2>&1 | %FileCheck --check-prefix O_NONE %s
+// RUN: %swiftc_driver -driver-print-jobs -enable-testing -enable-library-evolution -O %s 2>&1 | %FileCheck --check-prefix O %s
+// RUN: %swiftc_driver -driver-print-jobs -enable-testing -enable-library-evolution -Ounchecked %s 2>&1 | %FileCheck --check-prefix O_UNCHECKED %s
 
-// CHECK: warning: specifying '-enable-library-evolution' and '-enable-testing' together will cause program instability
+
+// NO_O: warning: specifying '-enable-library-evolution' and '-enable-testing' together will cause program instability; consider disabling the '-enable-library-evolution' flag
+// O_SIZE: warning: specifying '-enable-library-evolution' and '-enable-testing' together will cause program instability; consider disabling the '-enable-testing' flag
+// O_NONE: warning: specifying '-enable-library-evolution' and '-enable-testing' together will cause program instability; consider disabling the '-enable-library-evolution' flag
+// O: warning: specifying '-enable-library-evolution' and '-enable-testing' together will cause program instability; consider disabling the '-enable-testing' flag
+// O_UNCHECKED: warning: specifying '-enable-library-evolution' and '-enable-testing' together will cause program instability; consider disabling the '-enable-testing' flag
 
 @testable import Module
