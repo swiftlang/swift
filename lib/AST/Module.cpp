3be900b23132aa9e909255b4a45c5edd70b50f43
Azoy <aalonso128@outlook.com>
2020-01-05 13:12:35 -0500

    Select witness based off protocol

--- lib/AST/Module.cpp
+++ lib/AST/Module.cpp
@@ -870,7 +870,7 @@ ProtocolConformanceRef ModuleDecl::lookupConformance(Type type,
     if (protocol == ctx.getProtocol(KnownProtocolKind::Equatable)) {
       // Ensure that every element in this tuple conforms to Equatable.
       for (auto eltTy : tuple->getElementTypes()) {
-        auto conformance = conformsToProtocol(eltTy, protocol);
+        auto conformance = lookupConformance(eltTy, protocol);
 
         if (conformance.isInvalid())
           return ProtocolConformanceRef::forInvalid();
