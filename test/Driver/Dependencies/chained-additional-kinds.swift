2ad443e842df406729ce9014708a42c41f6b4969
David Ungar <dungar@apple.com>
2020-01-05 11:28:35 -0800

    Do check-additional-kinds twice.

--- test/Driver/Dependencies/chained-additional-kinds.swift
+++ test/Driver/Dependencies/chained-additional-kinds.swift
@@ -1,28 +1,56 @@
 // other ==> main ==> yet-another
+// Once with coarse-grained dependencies, once with fine.
 
 // RUN: %empty-directory(%t)
 // RUN: cp -r %S/Inputs/chained-additional-kinds/* %t
 // RUN: touch -t 201401240005 %t/*
 
-// RUN: cd %t && %swiftc_driver -disable-fine-grained-dependencies -c -driver-use-frontend-path "%{python};%S/Inputs/update-dependencies.py" -output-file-map %t/output.json -incremental -driver-always-rebuild-dependents ./main.swift ./other.swift ./yet-another.swift -module-name main -j1 -v 2>&1 | %FileCheck -check-prefix=CHECK-FIRST %s
+// RUN: cd %t && %swiftc_driver -disable-fine-grained-dependencies -c -driver-use-frontend-path "%{python};%S/Inputs/update-dependencies.py" -output-file-map %t/output.json -incremental -driver-always-rebuild-dependents ./main.swift ./other.swift ./yet-another.swift -module-name main -j1 -v 2>&1 | %FileCheck -check-prefix=CHECK-COARSE-FIRST %s
 
-// CHECK-FIRST-NOT: warning
-// CHECK-FIRST: Handled main.swift
-// CHECK-FIRST: Handled other.swift
-// CHECK-FIRST: Handled yet-another.swift
+// CHECK-COARSE-FIRST-NOT: warning
+// CHECK-COARSE-FIRST: Handled main.swift
+// CHECK-COARSE-FIRST: Handled other.swift
+// CHECK-COARSE-FIRST: Handled yet-another.swift
 
-// RUN: cd %t && %swiftc_driver -disable-fine-grained-dependencies -c -driver-use-frontend-path "%{python};%S/Inputs/update-dependencies.py" -output-file-map %t/output.json -incremental -driver-always-rebuild-dependents ./main.swift ./other.swift ./yet-another.swift -module-name main -j1 -v 2>&1 | %FileCheck -check-prefix=CHECK-SECOND %s
+// RUN: cd %t && %swiftc_driver -disable-fine-grained-dependencies -c -driver-use-frontend-path "%{python};%S/Inputs/update-dependencies.py" -output-file-map %t/output.json -incremental -driver-always-rebuild-dependents ./main.swift ./other.swift ./yet-another.swift -module-name main -j1 -v 2>&1 | %FileCheck -check-prefix=CHECK-COARSE-SECOND %s
 
-// CHECK-SECOND-NOT: Handled
+// CHECK-COARSE-SECOND-NOT: Handled
 
 // RUN: touch -t 201401240006 %t/other.swift
-// RUN: cd %t && %swiftc_driver -disable-fine-grained-dependencies -c -driver-use-frontend-path "%{python};%S/Inputs/update-dependencies.py" -output-file-map %t/output.json -incremental -driver-always-rebuild-dependents ./main.swift ./other.swift ./yet-another.swift -module-name main -j1 -v 2>&1 | %FileCheck -check-prefix=CHECK-THIRD %s
+// RUN: cd %t && %swiftc_driver -disable-fine-grained-dependencies -c -driver-use-frontend-path "%{python};%S/Inputs/update-dependencies.py" -output-file-map %t/output.json -incremental -driver-always-rebuild-dependents ./main.swift ./other.swift ./yet-another.swift -module-name main -j1 -v 2>&1 | %FileCheck -check-prefix=CHECK-COARSE-THIRD %s
 
-// CHECK-THIRD-DAG: Handled other.swift
-// CHECK-THIRD-DAG: Handled main.swift
-// CHECK-THIRD-DAG: Handled yet-another.swift
+// CHECK-COARSE-THIRD-DAG: Handled other.swift
+// CHECK-COARSE-THIRD-DAG: Handled main.swift
+// CHECK-COARSE-THIRD-DAG: Handled yet-another.swift
 
 // RUN: touch -t 201401240007 %t/other.swift
-// RUN: cd %t && %swiftc_driver -disable-fine-grained-dependencies -c -driver-use-frontend-path "%{python};%S/Inputs/update-dependencies.py" -output-file-map %t/output.json -incremental -driver-always-rebuild-dependents ./other.swift ./main.swift ./yet-another.swift -module-name main -j1 -v 2>&1 | %FileCheck -check-prefix=CHECK-THIRD %s
+// RUN: cd %t && %swiftc_driver -disable-fine-grained-dependencies -c -driver-use-frontend-path "%{python};%S/Inputs/update-dependencies.py" -output-file-map %t/output.json -incremental -driver-always-rebuild-dependents ./other.swift ./main.swift ./yet-another.swift -module-name main -j1 -v 2>&1 | %FileCheck -check-prefix=CHECK-COARSE-THIRD %s
 
-// RUN: touch -t 201401240008 %t/other.swift
+
+
+// other ==> main ==> yet-another
+
+// RUN: %empty-directory(%t)
+// RUN: cp %S/Inputs/chained-additional-kinds/output.json %t
+// RUN: echo 'func foo() { a; }; struct z {}' >%t/main.swift
+// RUN: echo 'let a = 3' >%t/other.swift
+// RUN: echo 'func bar() { z() }' >%t/yet-another.swift
+// RUN: touch -t 201401240005 %t/{added,main,other,yet-another}.swift
+
+// RUN: cd %t && %swiftc_driver -enable-fine-grained-dependencies -driver-show-incremental -c -output-file-map %t/output.json -incremental -driver-always-rebuild-dependents ./main.swift ./other.swift ./yet-another.swift -module-name main -j1 2>&1 | %FileCheck -check-prefix=CHECK-FINE-FIRST %s
+
+// CHECK-FINE-FIRST: Disabling incremental build: could not read build record
+
+// RUN: cd %t && %swiftc_driver -enable-fine-grained-dependencies -driver-show-incremental -c -output-file-map %t/output.json -incremental -driver-always-rebuild-dependents ./main.swift ./other.swift ./yet-another.swift -module-name main -j1 -v 2>&1 | %FileCheck -check-prefix=CHECK-FINE-SECOND %s
+
+// CHECK-FINE-SECOND-NOT: Queuing
+
+// RUN: touch -t 201401240006 %t/other.swift
+// RUN: cd %t && %swiftc_driver -enable-fine-grained-dependencies -driver-show-incremental -c -output-file-map %t/output.json -incremental -driver-always-rebuild-dependents ./main.swift ./other.swift ./yet-another.swift -module-name main -j1 2>&1 | %FileCheck -check-prefix=CHECK-FINE-THIRD %s
+
+// CHECK-FINE-THIRD-DAG: Queuing {{.*}} {compile: other.o <= other.swift}
+// CHECK-FINE-THIRD-DAG: Queuing {{.*}} {compile: main.o <= main.swift}
+// CHECK-FINE-THIRD-DAG: Queuing {{.*}} {compile: yet-another.o <= yet-another.swift}
+
+// RUN: touch -t 201401240007 %t/other.swift
+// RUN: cd %t && %swiftc_driver -enable-fine-grained-dependencies -driver-show-incremental -c -output-file-map %t/output.json -incremental -driver-always-rebuild-dependents ./other.swift ./main.swift ./yet-another.swift -module-name main -j1 2>&1 | %FileCheck -check-prefix=CHECK-FINE-THIRD %s
