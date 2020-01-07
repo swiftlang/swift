12cd500428db11badf7a7ed98e424da719b890ac
David Ungar <dungar@apple.com>
2020-01-06 21:54:09 -0800

    test fixes

--- test/Driver/Dependencies/driver-show-incremental-arguments.swift
+++ test/Driver/Dependencies/driver-show-incremental-arguments.swift
@@ -14,11 +14,35 @@
 // RUN: %{python} %S/Inputs/touch.py 443865900 %t/*
 // RUN: echo '{version: "'$(%swiftc_driver_plain -version | head -n1)'", inputs: {"./main.swift": [443865900, 0], "./other.swift": [443865900, 0]}}' > %t/main~buildrecord.swiftdeps
 
-// RUN: cd %t && %swiftc_driver -disable-fine-grained-dependencies -driver-use-frontend-path "%{python};%S/Inputs/update-dependencies.py" -c ./main.swift ./other.swift -module-name main -incremental -v -driver-show-incremental -output-file-map %t/output.json | %FileCheck --check-prefix CHECK-INCREMENTAL %s
-// CHECK-INCREMENTAL-NOT: Incremental compilation has been disabled
-// CHECK-INCREMENTAL: Queuing (initial): {compile: main.o <= main.swift}
+// RUN: cd %t && %swiftc_driver -disable-fine-grained-dependencies -driver-use-frontend-path "%{python};%S/Inputs/update-dependencies.py" -c ./main.swift ./other.swift -module-name main -incremental -v -driver-show-incremental -output-file-map %t/output.json | %FileCheck --check-prefix CHECK-COARSE-INCREMENTAL %s
+// CHECK-COARSE-INCREMENTAL-NOT: Incremental compilation has been disabled
+// CHECK-COARSE-INCREMENTAL: Queuing (initial): {compile: main.o <= main.swift}
 
-// RUN: cd %t && %swiftc_driver -disable-fine-grained-dependencies -driver-use-frontend-path "%{python};%S/Inputs/update-dependencies.py" -g -c ./main.swift ./other.swift -module-name main -incremental -v -driver-show-incremental -output-file-map %t/output.json | %FileCheck --check-prefix CHECK-ARGS-MISMATCH %s
-// CHECK-ARGS-MISMATCH: Incremental compilation has been disabled{{.*}}different arguments
-// CHECK-ARGS-MISMATCH-NOT: Queuing (initial): {compile: main.o <= main.swift}
+// RUN: cd %t && %swiftc_driver -disable-fine-grained-dependencies -driver-use-frontend-path "%{python};%S/Inputs/update-dependencies.py" -g -c ./main.swift ./other.swift -module-name main -incremental -v -driver-show-incremental -output-file-map %t/output.json | %FileCheck --check-prefix CHECK-COARSE-ARGS-MISMATCH %s
+// CHECK-COARSE-ARGS-MISMATCH: Incremental compilation has been disabled{{.*}}different arguments
+// CHECK-COARSE-ARGS-MISMATCH-NOT: Queuing (initial): {compile: main.o <= main.swift}
+
+
+
+
+// RUN: %empty-directory(%t)
+// RUN: cp %S/Inputs/one-way-with-swiftdeps/output.json %t
+// RUN: echo 'var V = a' > %t/main.swift
+// RUN: echo 'let a = 34' > %t/other.swift
+// RUN: %{python} %S/Inputs/touch.py 443865900 %t/*
+// RUN: echo '{version: "'$(%swiftc_driver_plain -version | head -n1)'", inputs: {"./main.swift": [443865900, 0], "./other.swift": [443865900, 0]}}' > %t/main~buildrecord.swiftdeps
+
+// create good swiftdeps
+// RUN: cd %t && %swiftc_driver -enable-fine-grained-dependencies -c ./main.swift ./other.swift -module-name main -incremental -v -driver-show-incremental -output-file-map %t/output.json
+
+// RUN: %{python} %S/Inputs/touch.py 443865900 %t/*
+// RUN: echo '{version: "'$(%swiftc_driver_plain -version | head -n1)'", inputs: {"./main.swift": [443865900, 0], "./other.swift": [443865900, 0]}}' > %t/main~buildrecord.swiftdeps
+// RUN: cd %t && %swiftc_driver -enable-fine-grained-dependencies -c ./main.swift ./other.swift -module-name main -incremental -v -driver-show-incremental -output-file-map %t/output.json | %FileCheck --check-prefix CHECK-FINE-INCREMENTAL %s
+// CHECK-FINE-INCREMENTAL-NOT: Disabling incremental build
+// CHECK-FINE-INCREMENTAL: Queuing {{.*}}: {compile: main.o <= main.swift}
+
+// RUN: cd %t && %swiftc_driver -enable-fine-grained-dependencies -g -c ./main.swift ./other.swift -module-name main -incremental -v -driver-show-incremental -output-file-map %t/output.json | %FileCheck --check-prefix CHECK-FINE-ARGS-MISMATCH %s
+// CHECK-FINE-ARGS-MISMATCH: Incremental compilation has been disabled{{.*}}different arguments
+// CHECK-FINE-ARGS-MISMATCH-NOT: Queuing (initial): {compile: main.o <= main.swift}
+
 
