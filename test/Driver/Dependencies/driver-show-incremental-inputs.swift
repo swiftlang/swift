8eab125b425b692166b80ef6c14ae10efcb60194
David Ungar <dungar@apple.com>
2020-01-07 13:57:00 -0800

    Fixed

--- test/Driver/Dependencies/driver-show-incremental-inputs.swift
+++ test/Driver/Dependencies/driver-show-incremental-inputs.swift
@@ -14,12 +14,35 @@
 // RUN: %{python} %S/Inputs/touch.py 443865900 %t/*
 // RUN: echo '{version: "'$(%swiftc_driver_plain -version | head -n1)'", inputs: {"./main.swift": [443865900, 0], "./other.swift": [443865900, 0]}}' > %t/main~buildrecord.swiftdeps
 
-// RUN: cd %t && %swiftc_driver -driver-use-frontend-path "%{python};%S/Inputs/update-dependencies.py" -disable-fine-grained-dependencies -c ./main.swift ./other.swift -module-name main -incremental -v -driver-show-incremental -output-file-map %t/output.json | %FileCheck --check-prefix CHECK-INCREMENTAL %s
-// CHECK-INCREMENTAL-NOT: Incremental compilation has been disabled
-// CHECK-INCREMENTAL: Queuing (initial): {compile: main.o <= main.swift}
+// RUN: cd %t && %swiftc_driver -driver-use-frontend-path "%{python};%S/Inputs/update-dependencies.py" -disable-fine-grained-dependencies -c ./main.swift ./other.swift -module-name main -incremental -v -driver-show-incremental -output-file-map %t/output.json | %FileCheck --check-prefix CHECK-COARSE-INCREMENTAL %s
+// CHECK-COARSE-INCREMENTAL-NOT: Incremental compilation has been disabled
+// CHECK-COARSE-INCREMENTAL: Queuing (initial): {compile: main.o <= main.swift}
 
-// RUN: cd %t && %swiftc_driver -driver-use-frontend-path "%{python};%S/Inputs/update-dependencies.py" -disable-fine-grained-dependencies -c ./main.swift -module-name main -incremental -v -driver-show-incremental -output-file-map %t/output.json | %FileCheck --check-prefix CHECK-INPUTS-MISMATCH %s
-// CHECK-INPUTS-MISMATCH: Incremental compilation has been disabled{{.*}}inputs
-// CHECK-INPUTS-MISMATCH: ./other.swift
-// CHECK-INPUTS-MISMATCH-NOT: Queuing (initial): {compile: main.o <= main.swift}
+// RUN: cd %t && %swiftc_driver -driver-use-frontend-path "%{python};%S/Inputs/update-dependencies.py" -disable-fine-grained-dependencies -c ./main.swift -module-name main -incremental -v -driver-show-incremental -output-file-map %t/output.json | %FileCheck --check-prefix CHECK-COARSE-INPUTS-MISMATCH %s
+// CHECK-COARSE-INPUTS-MISMATCH: Incremental compilation has been disabled{{.*}}inputs
+// CHECK-COARSE-INPUTS-MISMATCH: ./other.swift
+// CHECK-COARSE-INPUTS-MISMATCH-NOT: Queuing (initial): {compile: main.o <= main.swift}
+
+
+
+// RUN: %empty-directory(%t)
+// RUN: cp %S/Inputs/one-way-with-swiftdeps/output.json %t
+// RUN: echo 'var V = 23' >%t/main.swift
+// RUN: echo 'func foo() {}' >%t/other.swift
+// RUN: %{python} %S/Inputs/touch.py 443865900 %t/*
+// RUN: echo '{version: "'$(%swiftc_driver_plain -version | head -n1)'", inputs: {"./main.swift": [443865900, 0], "./other.swift": [443865900, 0]}}' > %t/main~buildrecord.swiftdeps
+
+// Create swiftdeps
+// RUN: cd %t && %swiftc_driver -enable-fine-grained-dependencies  -c ./main.swift ./other.swift -module-name main -incremental -v -driver-show-incremental -output-file-map %t/output.json 2>&1 | %FileCheck --check-prefix CHECK-FINE-CREATING-SWIFTDEPS %s
+// CHECK-FINE-CREATING-SWIFTDEPS: Disabling incremental build: malformed swift dependencies file
+
+// RUN: touch main.swift
+// RUN: cd %t && %swiftc_driver -enable-fine-grained-dependencies -c ./main.swift ./other.swift -module-name main -incremental -v -driver-show-incremental -output-file-map %t/output.json 2>&1 | tee /tmp/1|%FileCheck --check-prefix CHECK-FINE-INCREMENTAL %s
+// CHECK-FINE-INCREMENTAL-NOT: Disabling incremental build
+// CHECK-FINE-INCREMENTAL: Queuing (initial): {compile: main.o <= main.swift}
+
+// RUN: cd %t && %swiftc_driver -enable-fine-grained-dependencies -c ./main.swift -module-name main -incremental -v -driver-show-incremental -output-file-map %t/output.json 2>&1 |tee /tmp/2| %FileCheck --check-prefix CHECK-FINE-INPUTS-MISMATCH %s
+// CHECK-FINE-INPUTS-MISMATCH: Incremental compilation has been disabled{{.*}}inputs
+// CHECK-FINE-INPUTS-MISMATCH: ./other.swift
+// CHECK-FINE-INPUTS-MISMATCH-NOT: Queuing (initial): {compile: main.o <= main.swift}
 
