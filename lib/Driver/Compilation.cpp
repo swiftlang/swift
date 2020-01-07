e9aa095a583ec1df7ce44c60c9bf8f1b66b7bdd8
David Ungar <dungar@apple.com>
2020-01-07 11:54:20 -0800

    Fix malformed swiftdeps message

--- lib/Driver/Compilation.cpp
+++ lib/Driver/Compilation.cpp
@@ -449,7 +449,7 @@ namespace driver {
                                  diag::warn_unable_to_load_dependencies,
                                  DependenciesFile);
       Comp.disableIncrementalBuild(
-          Twine("malformed swift dependencies file ' ") + DependenciesFile +
+          Twine("malformed swift dependencies file '") + DependenciesFile +
           "'");
     }
 
--- test/Driver/Dependencies/driver-show-incremental-conflicting-arguments.swift
+++ test/Driver/Dependencies/driver-show-incremental-conflicting-arguments.swift
@@ -14,20 +14,52 @@
 // RUN: %{python} %S/Inputs/touch.py 443865900 %t/*
 // RUN: echo '{version: "'$(%swiftc_driver_plain -version | head -n1)'", inputs: {"./main.swift": [443865900, 0], "./other.swift": [443865900, 0]}}' > %t/main~buildrecord.swiftdeps
 
-// RUN: cd %t && %swiftc_driver -disable-fine-grained-dependencies -driver-use-frontend-path "%{python};%S/Inputs/update-dependencies.py" -c ./main.swift ./other.swift -module-name main -incremental -v -driver-show-incremental -output-file-map %t/output.json | %FileCheck --check-prefix CHECK-INCREMENTAL %s
-// CHECK-INCREMENTAL-NOT: Incremental compilation has been disabled
-// CHECK-INCREMENTAL: Queuing (initial): {compile: main.o <= main.swift}
+// RUN: cd %t && %swiftc_driver -disable-fine-grained-dependencies -driver-use-frontend-path "%{python};%S/Inputs/update-dependencies.py" -c ./main.swift ./other.swift -module-name main -incremental -v -driver-show-incremental -output-file-map %t/output.json | %FileCheck --check-prefix CHECK-COARSE-INCREMENTAL %s
+// CHECK-COARSE-INCREMENTAL-NOT: Incremental compilation has been disabled
+// CHECK-COARSE-INCREMENTAL: Queuing (initial): {compile: main.o <= main.swift}
 
-// RUN: cd %t && %swiftc_driver -disable-fine-grained-dependencies -driver-use-frontend-path "%{python};%S/Inputs/update-dependencies.py" -c ./main.swift ./other.swift -module-name main -incremental -v -driver-show-incremental -whole-module-optimization -output-file-map %t/output.json | %FileCheck --check-prefix CHECK-WMO %s
-// CHECK-WMO: Incremental compilation has been disabled{{.*}}whole module optimization
-// CHECK-WMO-NOT: Queuing (initial): {compile: main.o <= main.swift}
+// RUN: cd %t && %swiftc_driver -disable-fine-grained-dependencies -driver-use-frontend-path "%{python};%S/Inputs/update-dependencies.py" -c ./main.swift ./other.swift -module-name main -incremental -v -driver-show-incremental -whole-module-optimization -output-file-map %t/output.json | %FileCheck --check-prefix CHECK-COARSE-WMO %s
+// CHECK-COARSE-WMO: Incremental compilation has been disabled{{.*}}whole module optimization
+// CHECK-COARSE-WMO-NOT: Queuing (initial): {compile: main.o <= main.swift}
 
-// RUN: cd %t && %swiftc_driver -disable-fine-grained-dependencies -driver-use-frontend-path "%{python};%S/Inputs/update-dependencies.py" -c ./main.swift ./other.swift -module-name main -incremental -v -driver-show-incremental -embed-bitcode -output-file-map %t/output.json | %FileCheck --check-prefix CHECK-BITCODE %s
-// CHECK-BITCODE: Incremental compilation has been disabled{{.*}}LLVM IR bitcode
-// CHECK-BITCODE-NOT: Queuing (initial): {compile: main.o <= main.swift}
+// RUN: cd %t && %swiftc_driver -disable-fine-grained-dependencies -driver-use-frontend-path "%{python};%S/Inputs/update-dependencies.py" -c ./main.swift ./other.swift -module-name main -incremental -v -driver-show-incremental -embed-bitcode -output-file-map %t/output.json | %FileCheck --check-prefix CHECK-COARSE-BITCODE %s
+// CHECK-COARSE-BITCODE: Incremental compilation has been disabled{{.*}}LLVM IR bitcode
+// CHECK-COARSE-BITCODE-NOT: Queuing (initial): {compile: main.o <= main.swift}
 
-// RUN: cd %t && %swiftc_driver -disable-fine-grained-dependencies -driver-use-frontend-path "%{python};%S/Inputs/update-dependencies.py" -c ./main.swift ./other.swift -module-name main -incremental -v -driver-show-incremental -whole-module-optimization -embed-bitcode -output-file-map %t/output.json | %FileCheck --check-prefix CHECK-WMO-AND-BITCODE %s
-// CHECK-WMO-AND-BITCODE: Incremental compilation has been disabled{{.*}}whole module optimization
-// CHECK-WMO-AND-BITCODE-NOT: Incremental compilation has been disabled
-// CHECK-WMO-AND-BITCODE-NOT: Queuing (initial): {compile: main.o <= main.swift}
+// RUN: cd %t && %swiftc_driver -disable-fine-grained-dependencies -driver-use-frontend-path "%{python};%S/Inputs/update-dependencies.py" -c ./main.swift ./other.swift -module-name main -incremental -v -driver-show-incremental -whole-module-optimization -embed-bitcode -output-file-map %t/output.json | %FileCheck --check-prefix CHECK-COARSE-WMO-AND-BITCODE %s
+// CHECK-COARSE-WMO-AND-BITCODE: Incremental compilation has been disabled{{.*}}whole module optimization
+// CHECK-COARSE-WMO-AND-BITCODE-NOT: Incremental compilation has been disabled
+// CHECK-COARSE-WMO-AND-BITCODE-NOT: Queuing (initial): {compile: main.o <= main.swift}
+
+
+
+// RUN: %empty-directory(%t)
+// RUN: cp %S/Inputs/one-way-with-swiftdeps/output.json %t
+// RUN: echo 'var V = a' >%t/main.swift
+// RUN: echo 'let a = 23' >%t/other.swift
+// RUN: %{python} %S/Inputs/touch.py 443865900 %t/*
+// RUN: echo '{version: "'$(%swiftc_driver_plain -version | head -n1)'", inputs: {"./main.swift": [443865900, 0], "./other.swift": [443865900, 0]}}' > %t/main~buildrecord.swiftdeps
+
+// Create swiftdeps
+// RUN: cd %t && %swiftc_driver -enable-fine-grained-dependencies  -c ./main.swift ./other.swift -module-name main -incremental -v -driver-show-incremental -output-file-map %t/output.json | %FileCheck --check-prefix CHECK-FINE-CREATING-SWIFTDEPS %s
+
+// CHECK-FINE-CREATING-SWIFTDEPS: Disabling incremental build: malformed swift dependencies file
+
+// RUN: touch %t/main.swift
+// RUN: cd %t && %swiftc_driver -enable-fine-grained-dependencies  -c ./main.swift ./other.swift -module-name main -incremental -v -driver-show-incremental -output-file-map %t/output.json 2>&1 | %FileCheck --check-prefix CHECK-FINE-INCREMENTAL %s
+// CHECK-FINE-INCREMENTAL-NOT: Disabling incremental build
+// CHECK-FINE-INCREMENTAL: Queuing (initial): {compile: main.o <= main.swift}
+
+// RUN: cd %t && %swiftc_driver -enable-fine-grained-dependencies  -c ./main.swift ./other.swift -module-name main -incremental -v -driver-show-incremental -whole-module-optimization -output-file-map %t/output.json 2>&1 | %FileCheck --check-prefix CHECK-FINE-WMO %s
+// CHECK-FINE-WMO: Incremental compilation has been disabled{{.*}}whole module optimization
+// CHECK-FINE-WMO-NOT: Queuing (initial): {compile: main.o <= main.swift}
+
+// RUN: cd %t && %swiftc_driver -enable-fine-grained-dependencies  -c ./main.swift ./other.swift -module-name main -incremental -v -driver-show-incremental -embed-bitcode -output-file-map %t/output.json  2>&1 | %FileCheck --check-prefix CHECK-FINE-BITCODE %s
+// CHECK-FINE-BITCODE: Incremental compilation has been disabled{{.*}}LLVM IR bitcode
+// CHECK-FINE-BITCODE-NOT: Queuing (initial): {compile: main.o <= main.swift}
+
+// RUN: cd %t && %swiftc_driver -enable-fine-grained-dependencies  -c ./main.swift ./other.swift -module-name main -incremental -v -driver-show-incremental -whole-module-optimization -embed-bitcode -output-file-map %t/output.json  2>&1 | %FileCheck --check-prefix CHECK-FINE-WMO-AND-BITCODE %s
+// CHECK-FINE-WMO-AND-BITCODE: Incremental compilation has been disabled{{.*}}whole module optimization
+// CHECK-FINE-WMO-AND-BITCODE-NOT: Incremental compilation has been disabled
+// CHECK-FINE-WMO-AND-BITCODE-NOT: Queuing (initial): {compile: main.o <= main.swift}
 
