160eb6cfd879ad222d6ae3521748a9e5cdfe945a
Bruno Rocha <brunorochaesilva@gmail.com>
2020-01-07 07:26:05 -0300

    Update index test response to cover unavailable calls

--- lib/Index/Index.cpp
+++ lib/Index/Index.cpp
@@ -301,6 +301,7 @@ private:
   bool handleSourceOrModuleFile(SourceFileOrModule SFOrMod);
 
   bool walkToDeclPre(Decl *D, CharSourceRange Range) override {
+    // Do not handle unavailable decls from other modules.
     if (IsModuleFile && AvailableAttr::isUnavailable(D))
       return false;
 
