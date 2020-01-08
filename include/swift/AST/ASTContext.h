66c4e4c42ca4c9578e194051d53d14657d170230
Aleksey Zinchenko <a.zintus@gmail.com>
2020-01-08 10:16:27 +0400

    Try to achieve better data locality for statement checker

--- include/swift/AST/ASTContext.h
+++ include/swift/AST/ASTContext.h
@@ -135,7 +135,9 @@ enum class AllocationArena {
   /// lifetime of a particular instance of the constraint solver.
   ///
   /// Any type involving a type variable is allocated in this arena.
-  ConstraintSolver
+  ConstraintSolver,
+
+  Declarations
 };
 
 /// Lists the set of "known" Foundation entities that are used in the
@@ -316,7 +318,7 @@ public:
     if (LangOpts.UseMalloc)
       return AlignedAlloc(bytes, alignment);
 
-    if (arena == AllocationArena::Permanent && Stats)
+    if ((arena == AllocationArena::Permanent || arena == AllocationArena::Declarations) && Stats)
       Stats->getFrontendCounters().NumASTBytesAllocated += bytes;
     return getAllocator(arena).Allocate(bytes, alignment);
   }
