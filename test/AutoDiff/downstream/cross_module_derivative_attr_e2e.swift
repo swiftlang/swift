bea4c8ce99a5d0a64bf57f5a80c850264eec4e06
Marc Rasi <marcrasi@google.com>
2020-01-10 13:54:48 -0800

    [AutoDiff] %target-swift-frontend -> %target-build-swift

--- test/AutoDiff/downstream/cross_module_derivative_attr_e2e.swift
+++ test/AutoDiff/downstream/cross_module_derivative_attr_e2e.swift
@@ -1,5 +1,8 @@
 // RUN: %empty-directory(%t)
-￼// RUN: %target-swift-frontend -I%t -c -parse-as-library -emit-module -module-name module1 -emit-module-path %t/module1.swiftmodule -o %t/module1.o %S/Inputs/cross_module_derivative_attr_e2e/module1/module1.swift %S/Inputs/cross_module_derivative_attr_e2e/module1/module1_other_file.swift -Xllvm -enable-experimental-cross-file-derivative-registration -validate-tbd-against-ir=none
-￼// RUN: %target-build-swift -I%t %S/Inputs/cross_module_derivative_attr_e2e/main/main.swift %t/module1.o -o %t/a.out -lm -Xllvm -enable-experimental-cross-file-derivative-registration -Xfrontend -validate-tbd-against-ir=none
-￼// RUN: %target-run %t/a.out
-￼// REQUIRES: executable_test
+// RUN: %target-build-swift -I%t -parse-as-library -emit-module -module-name module1 -emit-module-path %t/module1.swiftmodule -emit-library -o %t/%target-library-name(module1) %S/Inputs/cross_module_derivative_attr_e2e/module1/module1.swift %S/Inputs/cross_module_derivative_attr_e2e/module1/module1_other_file.swift -Xllvm -enable-experimental-cross-file-derivative-registration -Xfrontend -validate-tbd-against-ir=none
+// RUN: %target-build-swift -I%t -L%t %S/Inputs/cross_module_derivative_attr_e2e/main/main.swift -o %t/a.out -lm -lmodule1 -Xllvm -enable-experimental-cross-file-derivative-registration -Xfrontend -validate-tbd-against-ir=none %target-rpath(%t)
+// RUN: %target-run %t/a.out
+// REQUIRES: executable_test
+
+// TODO(TF-1104): Fix.
+// XFAIL: swift_test_mode_optimize
