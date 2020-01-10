358e72b689b06c5f9593e5f2c621ae0bb73ac9bf
Arnold Schwaighofer <aschwaighofer@apple.com>
2020-01-10 08:02:16 -0800

    Adjust for removal of llvm::make_unique in favor of std::make_unique

--- include/swift/AST/Evaluator.h
+++ include/swift/AST/Evaluator.h
@@ -354,7 +354,7 @@ private:
     // Check for a cycle.
     if (checkDependency(getCanonicalRequest(request))) {
       return llvm::Error(
-        llvm::make_unique<CyclicalRequestError<Request>>(request, *this));
+          std::make_unique<CyclicalRequestError<Request>>(request, *this));
     }
 
     // Make sure we remove this from the set of active requests once we're
