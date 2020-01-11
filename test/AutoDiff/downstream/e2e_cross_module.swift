1b8ecb1a42bc58a60fbf2014429082d145ceb8d5
marcrasi <marcrasi@google.com>
2020-01-10 17:23:25 -0800

    [AutoDiff] %target-swift-frontend -> %target-build-swift (#29131)
    `%target-build-swift` is closer than `%target-swift-frontend` to the normal way you build swift modules, so this change makes the tests a bit more realistic and more likely to catch problems that happen in practice.
    
    For example, `test/AutoDiff/downstream/cross_module_derivative_attr_e2e.swift` now fails in optimize mode. So I made that XFAIL and filed TF-1104.

--- test/AutoDiff/downstream/e2e_cross_module.swift
+++ test/AutoDiff/downstream/e2e_cross_module.swift
@@ -1,7 +1,8 @@
 // RUN: %empty-directory(%t)
-// RUN: %target-swift-frontend -c -parse-as-library -emit-module -module-name e2e_cross_module_external_module -emit-module-path %t/e2e_cross_module_external_module.swiftmodule -o %t/e2e_cross_module_external_module.o %S/Inputs/e2e_cross_module_external_module.swift
-// RUN: %target-build-swift -I%t %s %t/e2e_cross_module_external_module.o -o %t/a.out -lm
+// RUN: %target-build-swift -parse-as-library -emit-module -module-name e2e_cross_module_external_module -emit-module-path %t/e2e_cross_module_external_module.swiftmodule -emit-library -o %t/%target-library-name(e2e_cross_module_external_module) %S/Inputs/e2e_cross_module_external_module.swift
+// RUN: %target-build-swift -I%t -L%t %s -o %t/a.out -lm -le2e_cross_module_external_module  %target-rpath(%t)
 // RUN: %target-run %t/a.out
+// REQUIRES: executable_test
 
 import e2e_cross_module_external_module
 import StdlibUnittest
--- test/AutoDiff/downstream/loadable_by_address_cross_module.swift
+++ test/AutoDiff/downstream/loadable_by_address_cross_module.swift
@@ -9,7 +9,7 @@
 // Compile the module.
 
 // RUN: %empty-directory(%t)
-// RUN: %target-swift-frontend -c -parse-as-library -emit-module -module-name external -emit-module-path %t/external.swiftmodule -o %t/external.o %S/Inputs/loadable_by_address_cross_module.swift
+// RUN: %target-build-swift -parse-as-library -emit-module -module-name external -emit-module-path %t/external.swiftmodule -emit-library -o %t/%target-library-name(external) %S/Inputs/loadable_by_address_cross_module.swift
 
 // Next, check that differentiability_witness_functions in the client get
 // correctly modified by LBA.
@@ -26,7 +26,7 @@
 
 // Finally, execute the test.
 
-// RUN: %target-build-swift -I%t %s %t/external.o -o %t/a.out -lm
+// RUN: %target-build-swift -I%t -L%t %s -o %t/a.out -lm -lexternal %target-rpath(%t)
 // RUN: %target-run %t/a.out
 
 // REQUIRES: executable_test
