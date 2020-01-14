675140e9cae38574c4c7b2c9df9ae00ec0dbdc33
Saleem Abdulrasool <compnerd@compnerd.org>
2020-01-13 18:41:23 -0800

    tests: adjust the SourceKit tests for line endings (NFCI)
    This adjusts the tests for the difference between line endings on
    different platforms.  Windows uses CRLF while most Unicies use LF.  This
    was exposed during the update to the new LLVM snapshot.

--- test/SourceKit/CodeComplete/complete_unresolvedmember.swift
+++ test/SourceKit/CodeComplete/complete_unresolvedmember.swift
@@ -12,5 +12,5 @@ func test() -> Foo {
 }
 
 // RUN: %sourcekitd-test -req=complete -pos=11:11 %s -- %s > %t.response
-// RUN: diff -u %s.response %t.response
+// RUN: diff --strip-trailing-cr -u %s.response %t.response
 
