3d604e2925b713564551f5899ea8f0859f1c8971
simon gladman <sgladman@gmail.com>
2020-01-17 10:42:42 +0000

    Update overlooked availability

--- stdlib/public/Darwin/Accelerate/vDSP_MergeSwapGatherCompress.swift
+++ stdlib/public/Darwin/Accelerate/vDSP_MergeSwapGatherCompress.swift
@@ -192,7 +192,7 @@ extension vDSP {
 
 // MARK: Gather and Compress
 
-@available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
+@available(macOS 9999, iOS 9999, tvOS 9999, watchOS 9999, *)
 extension vDSP {
     
     // MARK: vDSP_vgathr
