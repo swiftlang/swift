7a1d40273b5ae0b5a81526f775ea3d14e14538ff
Suyash Srijan <suyashsrijan@outlook.com>
2020-01-17 22:30:13 +0000

    [Test] Update SILGen test

--- test/SILGen/protocol_enum_witness.swift
+++ test/SILGen/protocol_enum_witness.swift
@@ -8,6 +8,14 @@ enum Bar: Foo {
   case button
 }
 
+protocol AnotherFoo {
+  static func bar(arg: Int) -> Self
+}
+
+enum AnotherBar: AnotherFoo {
+  case bar(arg: Int)
+}
+
 // CHECK-LABEL: sil private [transparent] [thunk] [ossa] @$s21protocol_enum_witness3BarOAA3FooA2aDP6buttonxvgZTW : $@convention(witness_method: Foo) (@thick Bar.Type) -> @out Bar {
 // CHECK-NEXT: bb0([[BAR:%.*]] : $*Bar, [[BAR_TYPE:%.*]] : $@thick Bar.Type):
 // CHECK-NEXT: [[META_TYPE:%.*]] = metatype $@thin Bar.Type
@@ -24,5 +32,18 @@ enum Bar: Foo {
 // CHECK-NEXT: return [[CASE]] : $Bar
 // CHECK-END: }
 
+// CHECK-LABEL: sil private [transparent] [thunk] @$s21protocol_enum_witness10AnotherBarOAA0B3FooA2aDP3bar3argxSi_tFZTW : $@convention(witness_method: AnotherFoo) (Int, @thick AnotherBar.Type) -> @out AnotherBar {
+// CHECK-NEXT: bb0([[ANOTHER_BAR:%.*]] : $*AnotherBar, [[INT_ARG:%.*]] : $Int, [[ANOTHER_BAR_TYPE:%.*]] : $@thick AnotherBar.Type):
+// CHECK-NEXT: [[META_TYPE:%.*]] = metatype $@thin AnotherBar.Type
+// CHECK-NEXT: [[REF:%.*]] = function_ref @$s21protocol_enum_witness10AnotherBarO3baryACSi_tcACmF : $@convention(method) (Int, @thin AnotherBar.Type) -> AnotherBar
+// CHECK-NEXT: [[RESULT:%.*]] = apply [[REF]]([[INT_ARG]], [[META_TYPE]]) : $@convention(method) (Int, @thin AnotherBar.Type) -> AnotherBar
+// CHECK-NEXT: store [[RESULT]] to [[ANOTHER_BAR]] : $*AnotherBar
+// CHECK-NEXT: [[TUPLE:%.*]] = tuple ()
+// CHECK-NEXT: return [[TUPLE]] : $()
+// CHECK-END: }
+
 // CHECK-LABEL: sil_witness_table hidden Bar: Foo module protocol_enum_witness {
 // CHECK: method #Foo.button!getter.1: <Self where Self : Foo> (Self.Type) -> () -> Self : @$s21protocol_enum_witness3BarOAA3FooA2aDP6buttonxvgZTW
+
+// CHECK-LABEL: sil_witness_table hidden AnotherBar: AnotherFoo module protocol_enum_witness {
+// CHECK: method #AnotherFoo.bar!1: <Self where Self : AnotherFoo> (Self.Type) -> (Int) -> Self : @$s21protocol_enum_witness10AnotherBarOAA0B3FooA2aDP3bar3argxSi_tFZTW
