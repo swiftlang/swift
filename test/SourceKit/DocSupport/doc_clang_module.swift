0b93a247c64cd5b19f99b7875a4bb0d61739d493
Ben Langmuir <blangmuir@apple.com>
2020-01-22 11:12:25 -0800

    [test] Remove now-redundant `%mcp_opt` from sourcekit tests
    Remove `%mcp_opt` from commands that use `%sourcekitd-test` and
    `%complete-test`, as they are now redundant with the lit substitution.

--- test/SourceKit/DocSupport/doc_clang_module.swift
+++ test/SourceKit/DocSupport/doc_clang_module.swift
@@ -7,5 +7,5 @@
 // RUN: %build-clang-importer-objc-overlays
 
 // RUN: %sourcekitd-test -req=doc-info -module Foo -- -F %S/../Inputs/libIDE-mock-sdk \
-// RUN:         %mcp_opt -target %target-triple %clang-importer-sdk-nosource -I %t | %sed_clean > %t.response
+// RUN:         -target %target-triple %clang-importer-sdk-nosource -I %t | %sed_clean > %t.response
 // RUN: diff -u %s.response %t.response
