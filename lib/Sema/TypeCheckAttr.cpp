677a67122e6250608d2c0724195cee6a708e060a
Dan Zheng <danielzheng@google.com>
2020-01-23 13:48:52 -0800

    [AutoDiff] Change cross-file derivative registration to a frontend flag. (#29339)
    Change `-enable-experimental-cross-file-derivative-registration` from an
    `-Xllvm` flag to a `-Xfrontend` flag.
    
    This makes the flag consistent with:
    - `-enable-experimental-differentiable-programming`
    - `-enable-experimental-forward-mode-differentiation`

--- lib/Sema/TypeCheckAttr.cpp
+++ lib/Sema/TypeCheckAttr.cpp
@@ -3797,7 +3797,7 @@ static bool typeCheckDerivativeAttr(ASTContext &Ctx, Decl *D,
 
   // Reject different-file derivative registration.
   // TODO(TF-1021): Lift same-file derivative registration restriction.
-  if (!EnableExperimentalCrossFileDerivativeRegistration &&
+  if (!ctx.LangOpts.EnableExperimentalCrossFileDerivativeRegistration &&
       originalAFD->getParentSourceFile() != derivative->getParentSourceFile()) {
     diags.diagnose(attr->getLocation(),
                    diag::derivative_attr_not_in_same_file_as_original);
