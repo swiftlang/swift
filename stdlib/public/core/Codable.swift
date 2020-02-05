29b92073a1e5711832e4829ed064d5a85628283d
Stephen Canon <scanon@apple.com>
2020-02-05 09:30:23 -0500

    Add codable conformance for Float16, remove AnyHashableBox.

--- stdlib/public/core/Codable.swift
+++ stdlib/public/core/Codable.swift
@@ -4592,6 +4592,28 @@ extension RawRepresentable where RawValue == Float, Self: Decodable {
   }
 }
 
+extension Float16: Codable {
+  /// Creates a new instance by decoding from the given decoder.
+  ///
+  /// This initializer throws an error if reading from the decoder fails, or
+  /// if the data read is corrupted or otherwise invalid.
+  ///
+  /// - Parameter decoder: The decoder to read data from.
+  public init(from decoder: Decoder) throws {
+    self = try Self(Float(from: decoder))
+  }
+
+  /// Encodes this value into the given encoder.
+  ///
+  /// This function throws an error if any values are invalid for the given
+  /// encoder's format.
+  ///
+  /// - Parameter encoder: The encoder to write data to.
+  public func encode(to encoder: Encoder) throws {
+    try Float(self).encode(to: encoder)
+  }
+}
+
 extension Int: Codable {
   /// Creates a new instance by decoding from the given decoder.
   ///
