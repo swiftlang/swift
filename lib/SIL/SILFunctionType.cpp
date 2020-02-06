363dc3a8777c6d44947dadae196604a9cf2b5df2
Varun Gandhi <varun_gandhi@apple.com>
2020-02-05 22:04:23 -0800

    [AST] Store Clang type in SILFunctionType for @convention(c) functions.

--- lib/SIL/SILFunctionType.cpp
+++ lib/SIL/SILFunctionType.cpp
@@ -1429,11 +1429,7 @@ static CanSILFunctionType getSILFunctionType(
 
   // NOTE: SILFunctionType::ExtInfo doesn't track everything that
   // AnyFunctionType::ExtInfo tracks. For example: 'throws' or 'auto-closure'
-  auto silExtInfo = SILFunctionType::ExtInfo()
-    .withRepresentation(extInfo.getSILRepresentation())
-    .withIsPseudogeneric(pseudogeneric)
-    .withNoEscape(extInfo.isNoEscape())
-    .withDifferentiabilityKind(extInfo.getDifferentiabilityKind());
+  SILFunctionType::ExtInfo silExtInfo(extInfo, pseudogeneric);
   
   // Build the substituted generic signature we extracted.
   bool impliedSignature = false;
