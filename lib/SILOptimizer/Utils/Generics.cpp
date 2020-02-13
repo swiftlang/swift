5f62183f3c7772fa26b81a05b234f572d4ba9003
Dan Zheng <danielzheng@google.com>
2020-02-12 18:38:03 -0800

    [AutoDiff] Fix `differentiable_function`-related specialization crashes. (#29800)
    Fix crashes related to generic specialization of `partial_apply` operands to
    `differentiable_function` instructions.
    
    `differentiable_function` requires derivative function operand types to match
    expected derivative function types computed from the original function operand's
    type, so operands cannot be specialized individually without specializing the
    others.
    
    Resolves TF-891 and TF-1126.

--- lib/SILOptimizer/Utils/Generics.cpp
+++ lib/SILOptimizer/Utils/Generics.cpp
@@ -531,7 +531,11 @@ bool ReabstractionInfo::prepareAndCheck(ApplySite Apply, SILFunction *Callee,
 
   // SWIFT_ENABLE_TENSORFLOW
   // Disable specialization for instructions that are operands of
-  // `differentiable_function` instructions.
+  // `differentiable_function` instructions. `differentiable_function`
+  // requires derivative function operand types to match expected derivative
+  // function types computed from the original function operand's type, so
+  // operands cannot be specialized individually without specializing the
+  // others.
   if (Apply.getInstruction())
     for (auto result : Apply.getInstruction()->getResults())
       for (auto use : result->getUses())
