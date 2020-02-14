d726532ed47d5c490a45a1705104a1ee5a0df863
Marc Rasi <marcrasi@google.com>
2020-02-13 17:24:50 -0800

    fix test again

--- test/AutoDiff/downstream/compiler_crashers/tf1122-maximally-abstracted-differentiable-function.swift
+++ test/AutoDiff/downstream/compiler_crashers/tf1122-maximally-abstracted-differentiable-function.swift
@@ -1,6 +1,7 @@
 // RUN: %target-build-swift %s -o %t
 // RUN: not --crash %target-run %t
 // REQUIRES: executable_test
+// REQUIRES: swift_test_mode_optimize_none
 
 func id<T>(_ t: T) -> T { t }
 func foo<X: Differentiable>(_ x: X) {
