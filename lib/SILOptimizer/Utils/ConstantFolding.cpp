d58d0817a069a06846488dc2e07dbc33527ba797
Stephen Canon <scanon@apple.com>
2020-02-28 11:22:31 -0500

    More float16 work.

--- lib/SILOptimizer/Utils/ConstantFolding.cpp
+++ lib/SILOptimizer/Utils/ConstantFolding.cpp
@@ -995,6 +995,8 @@ public:
 
 IEEESemantics getFPSemantics(BuiltinFloatType *fpType) {
   switch (fpType->getFPKind()) {
+  case BuiltinFloatType::IEEE16:
+    return IEEESemantics(16, 5, 10, false);
   case BuiltinFloatType::IEEE32:
     return IEEESemantics(32, 8, 23, false);
   case BuiltinFloatType::IEEE64:
