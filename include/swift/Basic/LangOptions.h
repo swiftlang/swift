3904fe83fb1bae773fa5d150f51c0838dc9e8442
Slava Pestov <spestov@apple.com>
2020-03-03 19:03:11 -0500

    AST: Centralize ABI-related deployment target checks
    There were a couple of methods in LangOptions and some related ones in
    Availability and ASTContext that were added more recently.
    
    Refactor the three older checks to the newer scheme.

--- include/swift/Basic/LangOptions.h
+++ include/swift/Basic/LangOptions.h
@@ -390,33 +390,6 @@ namespace swift {
       return EffectiveLanguageVersion.isVersionAtLeast(major, minor);
     }
 
-    // The following deployment targets ship an Objective-C runtime supporting
-    // the class metadata update callback mechanism:
-    //
-    // - macOS 10.14.4
-    // - iOS 12.2
-    // - tvOS 12.2
-    // - watchOS 5.2
-    bool doesTargetSupportObjCMetadataUpdateCallback() const;
-
-    // The following deployment targets ship an Objective-C runtime supporting
-    // the objc_getClass() hook:
-    //
-    // - macOS 10.14.4
-    // - iOS 12.2
-    // - tvOS 12.2
-    // - watchOS 5.2
-    bool doesTargetSupportObjCGetClassHook() const;
-
-    // The following deployment targets ship an Objective-C runtime supporting
-    // the objc_loadClassref() entry point:
-    //
-    // - macOS 10.15
-    // - iOS 13
-    // - tvOS 13
-    // - watchOS 6
-    bool doesTargetSupportObjCClassStubs() const;
-
     /// Returns true if the given platform condition argument represents
     /// a supported target operating system.
     ///
