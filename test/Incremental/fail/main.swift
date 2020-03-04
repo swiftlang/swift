8a141774d51465291224d2e4ab29aac5db197894
Robert Widmann <devteam.codafi@gmail.com>
2020-03-03 22:11:33 -0800

    [DependencyVerifier] XFAIL the failure test for now
    rdar://60030114

--- test/Incremental/fail/main.swift
+++ test/Incremental/fail/main.swift
@@ -1,6 +1,8 @@
+// UNSUPPORTED: OS=windows-msvc
+// rdar://60030114
+
 // RUN: %empty-directory(%t)
 // RUN: %{python} %S/../gen-output-file-map.py -o %t %S/Inputs -r %t.resp
-// RUN: cat %t.resp
 // RUN: cd %t
 // RUN: not %target-swiftc_driver -no-color-diagnostics -typecheck -output-file-map %t/output.json -incremental -module-name main -verify-incremental-dependencies @%t.resp 2>&1 | sort | %FileCheck %s
 
