759926d4e8f3a18ea9b59e07aa3d2ccec6b5c37c
Karoy Lorentey <klorentey@apple.com>
2020-03-04 14:19:15 -0800

    [Atomics] Implement 8/16/32/64-bit atomic integers
    Introduce a new UnsafeAtomic<Value> generic struct; use it to define atomic operations on Int8, Int16, Int32, Int64, UInt8, UInt16, UInt32, UInt64 in additiont to the existing Int/UInt.
    
    Replace UnsafeAtomic[U]Int with UnsafeAtomic<[U]Int>.

---
+++ stdlib/public/Atomics/UnsafeAtomic.swift
@@ -0,0 +1,46 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2019 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+import Swift
+
+@available(macOS 9999, iOS 9999, watchOS 9999, tvOS 9999, *)
+@frozen
+public struct UnsafeAtomic<Value> {
+  @usableFromInline
+  internal let _ptr: UnsafeMutableRawPointer
+
+  @_transparent // Debug performance
+  public init(at address: UnsafeMutablePointer<Value>) {
+    self._ptr = UnsafeMutableRawPointer(address)
+  }
+}
+
+@available(macOS 9999, iOS 9999, watchOS 9999, tvOS 9999, *)
+extension UnsafeAtomic {
+  public static func create(initialValue: __owned Value) -> UnsafeAtomic {
+    let ptr = UnsafeMutablePointer<Value>.allocate(capacity: 1)
+    ptr.initialize(to: initialValue)
+    return UnsafeAtomic(at: ptr)
+  }
+
+  __consuming public func destroy() {
+    _ptr.deallocate()
+  }
+}
+
+@available(macOS 9999, iOS 9999, watchOS 9999, tvOS 9999, *)
+extension UnsafeAtomic {
+  @inlinable
+  public var address: UnsafeMutablePointer<Value> {
+    _ptr.assumingMemoryBound(to: Value.self)
+  }
+}
