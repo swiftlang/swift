078f4a8cbb7e620914f2db985f1f76c01f8ca5a7
Yuta Saito <kateinoigakukun@gmail.com>
2020-03-05 10:19:46 +0000

    Remove packing job from CI

--- utils/webassembly/build-linux-package.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Unpacking Linux prebuilts"
-mkdir -p output
-cd linux
-./unpack-prebuilts.sh
-echo "Compressing"
-tar cJf ../output/swiftwasm-sdk-linux.tar.xz swiftwasm-sdk
-cd ..
--- utils/webassembly/build-mac-package.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Unpacking macOS prebuilts"
-mkdir -p output
-cd macos
-./unpack-prebuilts.sh
-echo "Compressing macOS package"
-tar cJf ../output/swiftwasm-sdk-macos.tar.xz swiftwasm-sdk
-cd ..
