476cea814eab55925e47e9d6b5008d714eff6a84
TJ Usiyan <griotspeak@gmail.com>
2020-03-05 16:01:34 -0800

    [must tail] Implement initial version of must tail semantic attribute
    This commit threads through IRGen for small case of marking something as tail callable where there is no recursion.
    
    ```
    func foo(_ x: Klass) -> Klass {
      @_semantics("musttail")
      func mustTailReturn(_ x: Klass) -> Klass {
        return x
      }
    
      return mustTailReturn(x)
    }
    
    ```
    
    The case involving recursion will require resolving the barrier posed by the presence of multiple returns
    
    ```
    @_semantics("musttail")
    func mustTailRecurse(_ x: Int) -> Int {
      @_semantics("musttail")
      func mustTailReturn(_ x: Int) -> Int {
        return x
      }
    
      if x > 0 {
        return mustTailRecurse(x - 1)
      }
    
      return mustTailReturn(x)
    }
    
    ```

--- lib/IRGen/GenCall.cpp
+++ lib/IRGen/GenCall.cpp
@@ -2767,6 +2767,10 @@ llvm::CallInst *CallEmission::emitCallSite() {
     IGF.Builder.SetInsertPoint(call->getParent());
   }
 
+  if (IsMustTail) {
+    call->setTailCallKind(llvm::CallInst::TailCallKind::TCK_MustTail);
+  }
+
   Args.clear();
 
   // Destroy any temporaries we needed.
