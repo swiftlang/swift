6abf057dc7b8df4cc0dee00280c3f2e20145045b
Robert Widmann <devteam.codafi@gmail.com>
2020-03-09 13:09:05 -0700

    Teach the verifier to emit fixits
    Emit nicely formatted fixits that contain the expectations the user should have written.

--- test/Incremental/fail/main-windows.swift
+++ test/Incremental/fail/main-windows.swift
@@ -1,17 +1,13 @@
-// UNSUPPORTED: OS=windows-msvc
-// See its counterpart main-windows.swift
+// REQUIRES: OS=windows-msvc
+// This test is special-cased on Windows because dependencies in Two.swift don't
+// appear to be registered against the tracker. rdar://60030114
 
 // RUN: %empty-directory(%t)
 // RUN: %{python} %S/../gen-output-file-map.py -o %t %S/Inputs -r %t.resp
 // RUN: cd %t
 // RUN: not %target-swiftc_driver -no-color-diagnostics -typecheck -output-file-map %t/output.json -incremental -module-name main -verify-incremental-dependencies @%t.resp 2>&1 | sort | %FileCheck %s
 
-// CHECK: unexpected dependency exists: main.BaseProtocol
 // CHECK: unexpected cascading member dependency: main.Base.init
-// CHECK: unexpected cascading member dependency: main.Subclass.deinit
-// CHECK: unexpected cascading member dependency: main.Subclass.init
 // CHECK: unexpected cascading potential member dependency: main.Base
 // CHECK: unexpected provided entity: Base
 // CHECK: unexpected provided entity: BaseProtocol
-// CHECK: unexpected provided entity: PublicProtocol
-// CHECK: unexpected provided entity: Subclass
