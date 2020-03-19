4a36a4b6c544b5348c6fa83a65979034d44d9802
3405691582 <dsk@google.com>
2020-03-18 23:14:22 -0400

    [test] Setup testing for OpenBSD.
    This commit focuses the basics: setting up the relevant stanzas in
    lit.cfg and adding platform conditionals for importing Glibc. Future
    commits will deal with other portability fixes.

--- test/stdlib/VarArgs.swift
+++ test/stdlib/VarArgs.swift
@@ -6,7 +6,7 @@ import Swift
 #if _runtime(_ObjC)
   import Darwin
   import CoreGraphics
-#elseif os(Linux) || os(FreeBSD) || os(PS4) || os(Android) || os(Cygwin) || os(Haiku) || os(WASI)
+#elseif os(Linux) || os(FreeBSD) || os(OpenBSD) || os(PS4) || os(Android) || os(Cygwin) || os(Haiku) || os(WASI)
   import Glibc
   typealias CGFloat = Double
 #elseif os(Windows)
