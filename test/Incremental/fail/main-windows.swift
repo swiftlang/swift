589e19f7c4b3218df361b405144fe1077b2b90ab
Robert Widmann <devteam.codafi@gmail.com>
2020-03-19 18:12:46 -0700

    Remove a missing dependency edge on Windows

--- test/Incremental/fail/main-windows.swift
+++ test/Incremental/fail/main-windows.swift
@@ -8,6 +8,5 @@
 // RUN: not %target-swiftc_driver -no-color-diagnostics -typecheck -output-file-map %t/output.json -incremental -module-name main -verify-incremental-dependencies @%t.resp 2>&1 | sort | %FileCheck %s
 
 // CHECK: unexpected cascading member dependency: main.Base.init
-// CHECK: unexpected cascading potential member dependency: main.Base
 // CHECK: unexpected provided entity: Base
 // CHECK: unexpected provided entity: BaseProtocol
