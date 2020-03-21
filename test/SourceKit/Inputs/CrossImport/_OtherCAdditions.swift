a355a56dd41af6b2ccc653a2612e865e101a1c51
Nathan Hawes <nhawes@apple.com>
2020-03-20 21:07:18 -0700

    [SourceKit/InterfaceGen][test] Test that doc comments are printed after the required bystanders comment.

---
+++ test/SourceKit/Inputs/CrossImport/_OtherCAdditions.swift
@@ -0,0 +1,7 @@
+@_exported import Other
+import C
+
+/// This has some interesting documentation that shouldn't be separated from
+/// the decl when we print the comment detailing its required bystanders in the
+/// generated interface of 'Other'.
+public func from_OtherCAdditions() {}
