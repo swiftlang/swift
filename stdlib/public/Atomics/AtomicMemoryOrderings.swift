0723a38427c2846dc2531c4b3a63253397812c33
Karoy Lorentey <klorentey@apple.com>
2020-03-20 22:54:03 -0700

    [stdlib] Add an Atomics module for low-level atomic operations

---
+++ stdlib/public/Atomics/AtomicMemoryOrderings.swift
@@ -0,0 +1,287 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2019 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+import Swift
+
+/// Specifies the memory ordering semantics of an atomic load operation.
+@frozen
+@available(macOS 9999, iOS 9999, watchOS 9999, tvOS 9999, *)
+public struct AtomicLoadOrdering {
+  // This struct works like a non-frozen enum whose cases aren't reorderable.
+
+  @usableFromInline
+  internal var _rawValue: Int
+
+  @_semantics("constant_evaluable")
+  @inlinable @_transparent // Debug performance
+  internal init(_rawValue: Int) {
+    self._rawValue = _rawValue
+  }
+}
+
+@available(macOS 9999, iOS 9999, watchOS 9999, tvOS 9999, *)
+extension AtomicLoadOrdering {
+  // FIXME: Explain these ordering levels in more detail.
+
+  /// Guarantees the atomicity of the specific operation on which it is applied,
+  /// but imposes no ordering constraints on any other reads or writes.
+  @_semantics("constant_evaluable")
+  @_alwaysEmitIntoClient
+  @_transparent // Debug performance
+  public static var relaxed: AtomicLoadOrdering {
+    AtomicLoadOrdering(_rawValue: 0)
+  }
+
+  /// An acquiring load prevents the effects of subsequent load and store
+  /// operations on the current thread from appearing to happen before the
+  /// effect of the atomic operation itself.
+  @_semantics("constant_evaluable")
+  @_alwaysEmitIntoClient
+  @_transparent // Debug performance
+  public static var acquiring: AtomicLoadOrdering {
+    AtomicLoadOrdering(_rawValue: 1)
+  }
+
+  @_semantics("constant_evaluable")
+  @_alwaysEmitIntoClient
+  @_transparent // Debug performance
+  public static var sequentiallyConsistent: AtomicLoadOrdering {
+    AtomicLoadOrdering(_rawValue: 4)
+  }
+}
+
+@available(macOS 9999, iOS 9999, watchOS 9999, tvOS 9999, *)
+extension AtomicLoadOrdering: Equatable {
+  @_transparent // Debug performance
+  public static func ==(
+    left: AtomicLoadOrdering,
+    right: AtomicLoadOrdering
+  ) -> Bool {
+    return left._rawValue == right._rawValue
+  }
+}
+
+@available(macOS 9999, iOS 9999, watchOS 9999, tvOS 9999, *)
+extension AtomicLoadOrdering: Hashable {
+  @inlinable
+  public func hash(into hasher: inout Hasher) {
+    hasher.combine(_rawValue)
+  }
+}
+
+@available(macOS 9999, iOS 9999, watchOS 9999, tvOS 9999, *)
+extension AtomicLoadOrdering: CustomStringConvertible {
+  public var description: String {
+    switch self {
+    case .relaxed: return "relaxed"
+    case .acquiring: return "acquiring"
+    case .sequentiallyConsistent: return "sequentiallyConsistent"
+    default: return "AtomicLoadOrdering(\(_rawValue))"
+    }
+  }
+}
+
+//------------------------------------------------------------------------------
+
+/// Specifies the memory ordering semantics of an atomic store operation.
+@frozen
+@available(macOS 9999, iOS 9999, watchOS 9999, tvOS 9999, *)
+public struct AtomicStoreOrdering {
+  // This struct works like a non-frozen enum whose cases aren't reorderable.
+
+  @usableFromInline
+  internal var _rawValue: Int
+
+  @_semantics("constant_evaluable")
+  @inlinable @_transparent // Debug performance
+  internal init(_rawValue: Int) {
+    self._rawValue = _rawValue
+  }
+}
+
+@available(macOS 9999, iOS 9999, watchOS 9999, tvOS 9999, *)
+extension AtomicStoreOrdering {
+  // FIXME: Explain these ordering levels in more detail.
+
+  /// Guarantees the atomicity of the specific operation on which it is applied,
+  /// but imposes no ordering constraints on any other reads or writes.
+  @_semantics("constant_evaluable")
+  @_alwaysEmitIntoClient
+  @_transparent // Debug performance
+  public static var relaxed: AtomicStoreOrdering {
+    AtomicStoreOrdering(_rawValue: 0)
+  }
+
+  /// A releasing store prevents the effects of previous load and store
+  /// operations on the current thread from appearing to happen after the effect
+  /// of the atomic operation itself.
+  @_semantics("constant_evaluable")
+  @_alwaysEmitIntoClient
+  @_transparent // Debug performance
+  public static var releasing: AtomicStoreOrdering {
+    AtomicStoreOrdering(_rawValue: 2)
+  }
+
+  @_semantics("constant_evaluable")
+  @_alwaysEmitIntoClient
+  @_transparent // Debug performance
+  public static var sequentiallyConsistent: AtomicStoreOrdering {
+    AtomicStoreOrdering(_rawValue: 4)
+  }
+}
+
+@available(macOS 9999, iOS 9999, watchOS 9999, tvOS 9999, *)
+extension AtomicStoreOrdering: Equatable {
+  @_transparent // Debug performance
+  public static func ==(
+    left: AtomicStoreOrdering,
+    right: AtomicStoreOrdering
+  ) -> Bool {
+    return left._rawValue == right._rawValue
+  }
+}
+
+@available(macOS 9999, iOS 9999, watchOS 9999, tvOS 9999, *)
+extension AtomicStoreOrdering: Hashable {
+  @inlinable
+  public func hash(into hasher: inout Hasher) {
+    hasher.combine(_rawValue)
+  }
+}
+
+@available(macOS 9999, iOS 9999, watchOS 9999, tvOS 9999, *)
+extension AtomicStoreOrdering: CustomStringConvertible {
+  public var description: String {
+    switch self {
+    case .relaxed: return "relaxed"
+    case .releasing: return "releasing"
+    case .sequentiallyConsistent: return "sequentiallyConsistent"
+    default: return "AtomicStoreOrdering(\(_rawValue))"
+    }
+  }
+}
+
+//------------------------------------------------------------------------------
+
+/// Specifies the memory ordering semantics of an atomic read-modify-write
+/// operation.
+@frozen
+@available(macOS 9999, iOS 9999, watchOS 9999, tvOS 9999, *)
+public struct AtomicUpdateOrdering {
+  // This struct works like a non-frozen enum whose cases aren't reorderable.
+
+  @usableFromInline
+  internal var _rawValue: Int
+
+  @_semantics("constant_evaluable")
+  @inlinable @_transparent // Debug performance
+  internal init(_rawValue: Int) {
+    self._rawValue = _rawValue
+  }
+}
+
+@available(macOS 9999, iOS 9999, watchOS 9999, tvOS 9999, *)
+extension AtomicUpdateOrdering {
+  // FIXME: Explain these ordering levels in more detail.
+
+  /// Guarantees the atomicity of the specific operation on which it is applied,
+  /// but imposes no ordering constraints on any other reads or writes.
+  @_semantics("constant_evaluable")
+  @_alwaysEmitIntoClient
+  @_transparent // Debug performance
+  public static var relaxed: AtomicUpdateOrdering {
+    AtomicUpdateOrdering(_rawValue: 0)
+  }
+
+  /// An acquiring load prevents the effects of subsequent load and store
+  /// operations on the current thread from appearing to happen before the
+  /// effect of the atomic operation itself.
+  @_semantics("constant_evaluable")
+  @_alwaysEmitIntoClient
+  @_transparent // Debug performance
+  public static var acquiring: AtomicUpdateOrdering {
+    AtomicUpdateOrdering(_rawValue: 1)
+  }
+
+  /// A releasing store prevents the effects of previous load and store
+  /// operations on the current thread from appearing to happen after the effect
+  /// of the atomic operation itself.
+  @_semantics("constant_evaluable")
+  @_alwaysEmitIntoClient
+  @_transparent // Debug performance
+  public static var releasing: AtomicUpdateOrdering {
+    AtomicUpdateOrdering(_rawValue: 2)
+  }
+
+  /// An acquiring-and-releasing operation is a combination of `.acquiring` and
+  /// `.releasing`; it prevents all neighboring load and store operations on the
+  /// current thread from appearing to happen in a different order in relation
+  /// to the atomic operation.
+  @_semantics("constant_evaluable")
+  @_alwaysEmitIntoClient
+  @_transparent // Debug performance
+  public static var acquiringAndReleasing: AtomicUpdateOrdering {
+    AtomicUpdateOrdering(_rawValue: 3)
+  }
+
+  @_semantics("constant_evaluable")
+  @_alwaysEmitIntoClient
+  @_transparent // Debug performance
+  public static var sequentiallyConsistent: AtomicUpdateOrdering {
+    AtomicUpdateOrdering(_rawValue: 4)
+  }
+}
+
+@available(macOS 9999, iOS 9999, watchOS 9999, tvOS 9999, *)
+extension AtomicUpdateOrdering: Equatable {
+  @_transparent // Debug performance
+  public static func ==(
+    left: AtomicUpdateOrdering,
+    right: AtomicUpdateOrdering
+  ) -> Bool {
+    return left._rawValue == right._rawValue
+  }
+}
+
+@available(macOS 9999, iOS 9999, watchOS 9999, tvOS 9999, *)
+extension AtomicUpdateOrdering: Hashable {
+  @inlinable
+  public func hash(into hasher: inout Hasher) {
+    hasher.combine(_rawValue)
+  }
+}
+
+@available(macOS 9999, iOS 9999, watchOS 9999, tvOS 9999, *)
+extension AtomicUpdateOrdering: CustomStringConvertible {
+  public var description: String {
+    switch self {
+    case .relaxed: return "relaxed"
+    case .acquiring: return "acquiring"
+    case .releasing: return "releasing"
+    case .acquiringAndReleasing: return "acquiringAndReleasing"
+    case .sequentiallyConsistent: return "sequentiallyConsistent"
+    default: return "AtomicUpdateOrdering(\(_rawValue))"
+    }
+  }
+}
+
+@available(macOS 9999, iOS 9999, watchOS 9999, tvOS 9999, *)
+@_transparent // Debug performance
+public func atomicMemoryFence(ordering: AtomicUpdateOrdering) {
+  switch ordering {
+  case .relaxed: break
+  case .acquiring: _atomicAcquiringMemoryFence()
+  case .releasing: _atomicReleasingMemoryFence()
+  case .acquiringAndReleasing: _atomicAcquiringAndReleasingMemoryFence()
+  default: _atomicSerializedMemoryFence()
+  }
+}
---
+++ stdlib/public/Atomics/UnsafeAtomicLazyReference.swift
@@ -0,0 +1,112 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2020 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+import Swift
+
+/// An atomic optional strong reference that can be set (initialized) exactly
+/// once, but read many times.
+@available(macOS 9999, iOS 9999, watchOS 9999, tvOS 9999, *)
+@frozen
+public struct UnsafeAtomicLazyReference<Instance: AnyObject> {
+  public typealias Value = Instance?
+
+  @usableFromInline
+  internal let _ptr: UnsafeMutableRawPointer
+
+  @_transparent // Debug performance
+  public init(at address: UnsafeMutablePointer<Value>) {
+    self._ptr = UnsafeMutableRawPointer(address)
+  }
+}
+
+@available(macOS 9999, iOS 9999, watchOS 9999, tvOS 9999, *)
+extension UnsafeAtomicLazyReference {
+  @inlinable
+  public var address: UnsafeMutablePointer<Value> {
+    _ptr.assumingMemoryBound(to: Value.self)
+  }
+
+  @inlinable
+  public static func create() -> UnsafeAtomicLazyReference {
+    let ptr = UnsafeMutablePointer<Value>.allocate(capacity: 1)
+    ptr.initialize(to: nil)
+    return UnsafeAtomicLazyReference(at: ptr)
+  }
+
+  public func destroy() {
+    address.deinitialize(count: 1)
+    address.deallocate()
+  }
+}
+
+@available(macOS 9999, iOS 9999, watchOS 9999, tvOS 9999, *)
+extension UnsafeAtomicLazyReference {
+  /// Atomically initializes this reference if its current value is nil, then
+  /// returns the initialized value. If this reference is already initialized,
+  /// then `initialize(to:)` discards its supplied argument and returns the
+  /// current value without updating it.
+  ///
+  /// The following example demonstrates how this can be used to implement a
+  /// thread-safe lazily initialized reference:
+  ///
+  /// ```
+  /// class Image {
+  ///   var _histogram: UnsafeAtomicLazyReference<Histogram> = ...
+  ///
+  ///   // This is safe to call concurrently from multiple threads.
+  ///   var atomicLazyHistogram: Histogram {
+  ///     if let histogram = _histogram.load() { return foo }
+  ///     // Note that code here may run concurrently on
+  ///     // multiple threads, but only one of them will get to
+  ///     // succeed setting the reference.
+  ///     let histogram = ...
+  ///     return _histogram.initialize(to: foo)
+  /// }
+  /// ```
+  ///
+  /// This operation uses acquiring-and-releasing memory ordering.
+  @_transparent
+  public func initialize(
+    to desired: __owned Instance
+  ) -> Instance {
+    let desiredUnmanaged = Unmanaged.passRetained(desired)
+    let desiredWord = UInt(bitPattern: desiredUnmanaged.toOpaque())
+    var currentWord: UInt = 0
+    var success: Bool
+    (success, currentWord) =
+      _ptr._atomicAcquiringAndReleasingCompareExchangeWord(
+        expected: currentWord,
+        desired: desiredWord)
+    if !success {
+      // The reference has already been initialized. Balance the retain that
+      // we performed on `desired`.
+      desiredUnmanaged.release()
+    }
+    let result = Unmanaged<Instance>.fromOpaque(
+      UnsafeRawPointer(bitPattern: currentWord)!)
+    return result.takeUnretainedValue()
+  }
+}
+
+@available(macOS 9999, iOS 9999, watchOS 9999, tvOS 9999, *)
+extension UnsafeAtomicLazyReference {
+  /// Atomically loads and returns the current value of this reference.
+  ///
+  /// The load operation is performed with the memory ordering
+  /// `AtomicLoadOrdering.acquiring`.
+  @_transparent
+  public func load() -> Instance? {
+    let value = _ptr._atomicAcquiringLoadWord()
+    guard let ptr = UnsafeRawPointer(bitPattern: value) else { return nil }
+    return Unmanaged.fromOpaque(ptr).takeUnretainedValue()
+  }
+}
