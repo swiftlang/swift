b6b1f852d1955b166fd6c5dd228dc0c94f4718bc
Cal Stephens <cal@calstephens.tech>
2020-03-21 21:36:33 -0700

    Drop in CodingKeyPath stdlib implementation from calda/CodingKeyPath

--- stdlib/public/core/Codable.swift
+++ stdlib/public/core/Codable.swift
@@ -91,6 +91,39 @@ extension CodingKey {
 }
 
 //===----------------------------------------------------------------------===//
+// CodingKeyPath
+//===----------------------------------------------------------------------===//
+
+/// A type that can be used as a key path for encoding and decoding.
+public protocol CodingKeyPath: CustomStringConvertible,
+                               CustomDebugStringConvertible {
+  /// The `CodingKey` components that comprise this Key Path.
+  var components: [CodingKey] { get }
+}
+
+extension CodingKey {
+  /// A textual representation of this key path.
+  public var description: String {
+    components.map { $0.stringValue }.joined(by: ".")
+  }
+
+  /// A textual representation of this key path, suitable for debugging.
+  public var debugDescription: String {
+    return description
+  }
+}
+
+/// Default implementation to `CodingKeyPath` for a
+/// String-based `RawRepresentable` enum.
+public extension CodingKeyPath where Self: RawRepresentable, RawValue == String {
+  var components: [CodingKey] {
+    rawValue
+      .components(separatedBy: ".")
+      .map { _CodingKeyPathComponent(stringValue: $0) }
+  }
+}
+
+//===----------------------------------------------------------------------===//
 // Encoder & Decoder
 //===----------------------------------------------------------------------===//
 
@@ -4346,6 +4379,201 @@ internal final class _KeyedDecodingContainerBox<
   override internal func superDecoder(forKey key: Key) throws -> Decoder {
     return try concrete.superDecoder(forKey: key)
   }
+}
+
+//===----------------------------------------------------------------------===//
+// KeyPathEncodingContainer
+//===----------------------------------------------------------------------===//
+
+public struct KeyPathEncodingContainer<K: CodingKeyPath> {
+  
+  public mutating func encode<T>(_ value: T, forKeyPath keyPath: K) throws where T: Encodable {
+    try underlyingContainer.encode(value, forKeyPath: keyPath)
+  }
+  
+  public mutating func encodeIfPresent<T>(_ value: T?, forKeyPath keyPath: K) throws where T: Encodable {
+    try underlyingContainer.encodeIfPresent(value, forKeyPath: keyPath)
+  }
+  
+  private var underlyingContainer: KeyedEncodingContainer<_CodingKeyPathComponent>
+  
+  init(wrapping underlyingContainer: KeyedEncodingContainer<_CodingKeyPathComponent>) {
+    self.underlyingContainer = underlyingContainer
+  }
+  
+}
+
+public extension Encoder {
+  func keyPathContainer<KeyPath>(keyedBy type: KeyPath.Type)
+    -> KeyPathEncodingContainer<KeyPath> where KeyPath: CodingKeyPath
+  {
+    KeyPathEncodingContainer<KeyPath>(wrapping: container(keyedBy: _CodingKeyPathComponent.self))
+  }
+}
+
+public extension KeyedEncodingContainer {
+  mutating func nestedKeyPathContainer<KeyPath>(keyedBy type: KeyPath.Type, forKey key: Key) throws
+    -> KeyPathEncodingContainer<KeyPath> where KeyPath: CodingKeyPath
+  {
+    KeyPathEncodingContainer<KeyPath>(wrapping: nestedContainer(keyedBy: _CodingKeyPathComponent.self, forKey: key))
+  }
+}
+
+public extension UnkeyedEncodingContainer {
+  mutating func nestedKeyPathContainer<KeyPath>(keyedBy type: KeyPath.Type) throws
+    -> KeyPathEncodingContainer<KeyPath> where KeyPath: CodingKeyPath
+  {
+    KeyPathEncodingContainer<KeyPath>(wrapping: nestedContainer(keyedBy: _CodingKeyPathComponent.self))
+  }
+}
+
+//===----------------------------------------------------------------------===//
+// KeyPathDecodingContainer
+//===----------------------------------------------------------------------===//
+
+public struct KeyPathDecodingContainer<K> where K: CodingKeyPath {
+  
+  public func decode<T>(_ type: T.Type, forKeyPath keyPath: K) throws -> T where T: Decodable {
+    try underlyingContainer.decode(T.self, forKeyPath: keyPath)
+  }
+  
+  public func decodeIfPresent<T>(_ type: T.Type, forKeyPath keyPath: K) throws -> T? where T: Decodable {
+    try underlyingContainer.decodeIfPresent(T.self, forKeyPath: keyPath)
+  }
+  
+  private var underlyingContainer: KeyedDecodingContainer<_CodingKeyPathComponent>
+  
+  init(wrapping underlyingContainer: KeyedDecodingContainer<_CodingKeyPathComponent>) {
+    self.underlyingContainer = underlyingContainer
+  }
+  
+}
+
+public extension Decoder {
+  func keyPathContainer<KeyPath>(keyedBy type: KeyPath.Type) throws
+    -> KeyPathDecodingContainer<KeyPath> where KeyPath: CodingKeyPath
+  {
+    try KeyPathDecodingContainer(wrapping: self.container(keyedBy: _CodingKeyPathComponent.self))
+  }
+}
+
+public extension KeyedDecodingContainer {
+  mutating func nestedKeyPathContainer<KeyPath>(keyedBy type: KeyPath.Type, forKey key: Key) throws
+    -> KeyPathDecodingContainer<KeyPath> where KeyPath: CodingKeyPath
+  {
+    try KeyPathDecodingContainer(wrapping: nestedContainer(keyedBy: _CodingKeyPathComponent.self, forKey: key))
+  }
+}
+
+public extension UnkeyedDecodingContainer {
+  mutating func nestedKeyPathContainer<KeyPath>(keyedBy type: KeyPath.Type) throws
+    -> KeyPathDecodingContainer<KeyPath> where KeyPath: CodingKeyPath
+  {
+    try KeyPathDecodingContainer(wrapping: nestedContainer(keyedBy: _CodingKeyPathComponent.self))
+  }
+}
+
+//===----------------------------------------------------------------------===//
+// _CodingKeyPathComponent
+//===----------------------------------------------------------------------===//
+
+struct _CodingKeyPathComponent: CodingKey {
+  public let stringValue: String
+  public let intValue: Int? = nil
+  
+  init(stringValue: String) {
+    self.stringValue = stringValue
+  }
+  
+  init?(intValue: Int) {
+    return nil
+  }
+  
+  init(codingKeyValue: CodingKey) {
+    self.stringValue = codingKeyValue.stringValue
+  }
+}
+
+extension CodingKeyPath {
+  var _components: [_CodingKeyPathComponent] {
+    if let existingKeyPathComponents = components as? [_CodingKeyPathComponent] {
+      return existingKeyPathComponents
+    } else {
+      return components.map { _CodingKeyPathComponent(codingKeyValue: $0) }
+    }
+  }
+}
+
+private struct _CodingKeyPathComponents: CodingKeyPath {
+  var components: [CodingKey]
+  
+  init(_ components: [CodingKey]) {
+    self.components = components
+  }
+}
+
+extension KeyedEncodingContainer where K == _CodingKeyPathComponent {
+  
+  mutating func encode<T: Encodable>(_ value: T, forKeyPath keyPath: CodingKeyPath) throws {
+    let keyPathComponents = keyPath._components
+    
+    if keyPathComponents.isEmpty {
+      throw EncodingError.invalidValue(
+        value,
+        EncodingError.Context(
+          codingPath: codingPath + keyPath.components,
+          debugDescription: "Invalid CodingKeyPath. Components must not be empty."))
+    }
+      
+    else if keyPathComponents.count == 1 {
+      try self.encode(value, forKey: keyPathComponents[0])
+    }
+      
+    else {
+      var container = nestedContainer(keyedBy: _CodingKeyPathComponent.self, forKey: keyPathComponents[0])
+      try container.encode(value, forKeyPath: _CodingKeyPathComponents(Array(keyPathComponents.dropFirst())))
+    }
+  }
+  
+  mutating func encodeIfPresent<T: Encodable>(_ value: T?, forKeyPath keyPath: CodingKeyPath) throws {
+    if let value = value {
+      try encode(value, forKeyPath: keyPath)
+    }
+  }
+  
+}
+
+extension KeyedDecodingContainer where K == _CodingKeyPathComponent {
+  
+  func decode<T: Decodable>(_ type: T.Type, forKeyPath keyPath: CodingKeyPath) throws -> T {
+    if let value = try decodeIfPresent(T.self, forKeyPath: keyPath) {
+      return value
+    } else {
+      throw DecodingError.valueNotFound(T.self, DecodingError.Context(
+        codingPath: self.codingPath + keyPath.components,
+        debugDescription: "Value not found."))
+    }
+  }
+  
+  func decodeIfPresent<T: Decodable>(_ type: T.Type, forKeyPath keyPath: CodingKeyPath) throws -> T? {
+    let keyPathComponents = keyPath._components
+    
+    if keyPathComponents.isEmpty {
+      throw DecodingError.dataCorrupted(DecodingError.Context(
+        codingPath: self.codingPath + keyPath.components,
+        debugDescription: "Invalid CodingKeyPath. Components must not be empty."))
+    }
+      
+    else if keyPathComponents.count == 1 {
+      return try decodeIfPresent(T.self, forKey: keyPathComponents[0])
+    }
+      
+    else {
+      let container = try nestedContainer(keyedBy: _CodingKeyPathComponent.self, forKey: keyPathComponents[0])
+      return try container.decodeIfPresent(T.self, forKeyPath: _CodingKeyPathComponents(Array(keyPathComponents.dropFirst())))
+    }
+  }
+  
 }
 
 //===----------------------------------------------------------------------===//
