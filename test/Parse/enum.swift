572f0544a22a0526736ef043245268cd7da2d163
Anthony Latsis <aqamoss3fan2010@gmail.com>
2020-03-23 08:27:55 +0300

    [DiagQoI] Improve fallback fixits for static member on instance error

--- test/Parse/enum.swift
+++ test/Parse/enum.swift
@@ -473,7 +473,7 @@ enum SE0036 {
 
   func staticReferenceInInstanceMethod() {
     _ = A // expected-error {{enum case 'A' cannot be used as an instance member}} {{9-9=SE0036.}}
-    _ = self.A // expected-error {{enum case 'A' cannot be used as an instance member}} {{9-9=SE0036.}}
+    _ = self.A // expected-error {{enum case 'A' cannot be used as an instance member}} {{9-13=SE0036}}
     _ = SE0036.A
   }
 
