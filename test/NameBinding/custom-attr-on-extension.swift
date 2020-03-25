e3ea72127a8bbd8b746d67563a44a51e23592a51
David Ungar <dungar@apple.com>
2020-03-25 11:29:24 -0700

    Add a test case.

---
+++ test/NameBinding/custom-attr-on-extension.swift
@@ -0,0 +1,8 @@
+// Trips an assertion if an ASTScope lookup is attempted into the innermost
+// DeclContext of the extension.
+
+struct Ty{}
+
+@foo extension Ty {} // expected-error {{unknown attribute 'foo'}}
+
+// RUN: %target-swift-frontend -typecheck %s -verify
