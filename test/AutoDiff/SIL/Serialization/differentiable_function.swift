80e5a51b5ba846060b887bc96db5463ef920e4a7
Dan Zheng <danielzheng@google.com>
2020-03-28 23:07:11 -0700

    [AutoDiff upstream] Add differentiable function type lowering. (#30677)
    Add `@differentiable` and `@differentiable(linear)` type lowering.
    
    Resolves TF-1221.

--- test/AutoDiff/SIL/Serialization/differentiable_function.swift
+++ test/AutoDiff/SIL/Serialization/differentiable_function.swift
@@ -1,11 +1,17 @@
 // RUN: %empty-directory(%t)
-// RUN: %target-sil-opt %s -emit-sib -o %t/tmp.sib -module-name differentiation -enable-experimental-differentiable-programming
-// RUN: %target-sil-opt %t/tmp.sib -o %t/tmp.2.sib -module-name differentiation -enable-experimental-differentiable-programming
-// RUN: %target-sil-opt %t/tmp.2.sib -module-name differentiation -emit-sorted-sil -enable-experimental-differentiable-programming | %FileCheck %s
+// RUN: %target-sil-opt -enable-experimental-differentiable-programming %s -emit-sib -o %t/tmp.sib -module-name main
+// RUN: %target-sil-opt -enable-experimental-differentiable-programming %t/tmp.sib -o %t/tmp.sil -module-name main
+// NOTE(SR-12090): Workaround because import declarations are not preserved in .sib files.
+// RUN: sed -e 's/import Swift$/import Swift; import _Differentiation/' %t/tmp.sil > %t/tmp_fixed.sil
+// RUN: %target-sil-opt -enable-experimental-differentiable-programming %t/tmp_fixed.sil -module-name main -emit-sorted-sil | %FileCheck %s
+
+// NOTE(SR-12090): `shell` is required only to run `sed` as a SR-12090 workaround.
+// REQUIRES: shell
 
 sil_stage raw
 
 import Swift
+import _Differentiation
 
 sil @a : $@convention(thin) (@differentiable (Float) -> Float) -> @differentiable (Float) -> Float {
 bb0(%0 : $@differentiable (Float) -> Float):
