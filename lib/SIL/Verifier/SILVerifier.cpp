96410196acfbc0fc452c2e059db43892d962f122
Michael Gottesman <mgottesman@apple.com>
2020-03-30 16:16:28 -0700

    [gardening] Move maybeScopeLess from SIL/Verifier/SILVerifier.cpp -> SIL/IR/SILDebugScope.cpp.
    This is defined in SILDebugScope.h, so I don't know why it was put into
    SILVerifier.cpp.

--- lib/SIL/Verifier/SILVerifier.cpp
+++ lib/SIL/Verifier/SILVerifier.cpp
@@ -5527,10 +5527,3 @@ void SILModule::verify() const {
     prop.verify(*this);
   }
 }
-
-/// Determine whether an instruction may not have a SILDebugScope.
-bool swift::maybeScopeless(SILInstruction &I) {
-  if (I.getFunction()->isBare())
-    return true;
-  return !isa<DebugValueInst>(I) && !isa<DebugValueAddrInst>(I);
-}
