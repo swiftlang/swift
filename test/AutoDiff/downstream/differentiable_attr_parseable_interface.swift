360e14a8ce1c031ee2dbf1495e8378399d650e89
Marc Rasi <marcrasi@google.com>
2020-03-30 17:29:24 -0700

    [AutoDiff] delete some upstreamed tests

--- test/AutoDiff/downstream/differentiable_attr_parseable_interface.swift
+++
@@ -1,6 +0,0 @@
-// SWIFT_ENABLE_TENSORFLOW
-// RUN: %target-swift-frontend -swift-version 5 -typecheck -emit-parseable-module-interface-path %t.swiftinterface -enable-library-evolution %s
-// RUN: %FileCheck %s < %t.swiftinterface
-
-// CHECK: func testDifferentiableParam(f: @differentiable (Swift.Float) -> Swift.Float)
-public func testDifferentiableParam(f: @differentiable (Float) -> Float) {}
