8d0e38e3641b2816e1d8e6813671a943f1fd09c3
Xi Ge <xi_ge@apple.com>
2020-04-06 10:39:22 -0700

    test: disable Serialization/multi-file-type-eraser.swift for failing in public CI
    rdar://61348443

--- test/Serialization/multi-file-type-eraser.swift
+++ test/Serialization/multi-file-type-eraser.swift
@@ -1,3 +1,4 @@
+// REQUIRES: rdar61348443
 // RUN: %empty-directory(%t)
 // RUN: %target-swift-frontend -emit-module -module-name Multi -o %t/multi-file.swiftmodule -primary-file %s %S/Inputs/multi-file-type-eraser.swift
 // RUN: %target-swift-frontend -emit-module -module-name Multi -o %t/multi-file-2.swiftmodule %s -primary-file %S/Inputs/multi-file-type-eraser.swift
