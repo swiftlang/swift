2d1436e8d21b8d137a92549cdb132d44f59def5d
Marc Rasi <marcrasi@google.com>
2020-04-07 10:26:57 -0700

    fix SR-12526 test

--- test/AutoDiff/Sema/DerivativeRegistrationCrossModule/Inputs/a.swift
+++ test/AutoDiff/Sema/DerivativeRegistrationCrossModule/Inputs/a.swift
@@ -1,6 +1,6 @@
 public struct Struct {
+  public init() {}
   public func method(_ x: Float) -> Float { x }
-
   public static func +(_ lhs: Self, rhs: Self) -> Self {
     lhs
   }
--- test/AutoDiff/Sema/DerivativeRegistrationCrossModule/main.swift
+++ test/AutoDiff/Sema/DerivativeRegistrationCrossModule/main.swift
@@ -1,7 +1,7 @@
 // RUN: %empty-directory(%t)
 // RUN: %target-swift-frontend -emit-module -primary-file %S/Inputs/a.swift -emit-module-path %t/a.swiftmodule
 // RUN: %target-swift-frontend -emit-module -primary-file %S/Inputs/b.swift -emit-module-path %t/b.swiftmodule -I %t
-// RUN: not --crash %target-swift-frontend-typecheck -verify -I %t %s
+// RUN: %target-swift-frontend-typecheck -verify -I %t %s
 
 // SR-12526: Fix cross-module deserialization crash involving `@derivative` attribute.
 
