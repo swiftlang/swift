120840e0927398fae63cb9968a62298053d80cd9
Slava Pestov <spestov@apple.com>
2020-04-10 17:25:05 -0400

    Fix formatting nits in test/Interpreter/dynamic_replacement.swift

--- test/Interpreter/dynamic_replacement.swift
+++ test/Interpreter/dynamic_replacement.swift
@@ -75,46 +75,46 @@ func expectedResult(_ forOriginalLibrary: Bool,  _ expectedOriginalString: Strin
 }
 
 func checkExpectedResults(forOriginalLibrary useOrig: Bool) {
- expectTrue(public_global_var == expectedResult(useOrig, "public_global_var"))
+  expectTrue(public_global_var == expectedResult(useOrig, "public_global_var"))
 
- expectTrue(public_global_func() ==
-            expectedResult(useOrig, "public_global_func"))
- expectTrue(public_global_generic_func(Int.self) ==
-            expectedResult(useOrig, "public_global_generic_func"))
+  expectTrue(public_global_func() ==
+             expectedResult(useOrig, "public_global_func"))
+  expectTrue(public_global_generic_func(Int.self) ==
+             expectedResult(useOrig, "public_global_generic_func"))
 
- expectTrue(PublicClass(x: 0).str ==
-            expectedResult(useOrig, "public_class_init"))
- expectTrue(PublicClass().function() ==
-            expectedResult(useOrig, "public_class_func"))
- expectTrue(PublicClass().finalFunction() ==
-            expectedResult(useOrig, "public_class_final_func"))
- expectTrue(PublicClass().genericFunction(Int.self) ==
-            expectedResult(useOrig, "public_class_generic_func"))
+  expectTrue(PublicClass(x: 0).str ==
+             expectedResult(useOrig, "public_class_init"))
+  expectTrue(PublicClass().function() ==
+             expectedResult(useOrig, "public_class_func"))
+  expectTrue(PublicClass().finalFunction() ==
+             expectedResult(useOrig, "public_class_final_func"))
+  expectTrue(PublicClass().genericFunction(Int.self) ==
+             expectedResult(useOrig, "public_class_generic_func"))
 
- expectTrue(PublicStruct(x: 0).str ==
-            expectedResult(useOrig, "public_struct_init"))
- expectTrue(PublicStruct().function() ==
-            expectedResult(useOrig, "public_struct_func"))
- expectTrue(PublicStruct().genericFunction(Int.self) ==
-            expectedResult(useOrig, "public_struct_generic_func"))
- expectTrue(PublicStruct().public_stored_property ==
-            expectedResult(useOrig, "public_stored_property"))
- expectTrue(PublicStruct()[0] ==
-            expectedResult(useOrig, "public_subscript_get"))
- expectTrue(PublicStruct()[y:0] ==
-            expectedResult(useOrig, "public_subscript_get_modify_read"))
- var testSetter = PublicStruct()
- testSetter[0] = "public_subscript_set"
- expectTrue(testSetter.str ==
-            expectedResult(useOrig, "public_subscript_set"))
- testSetter[y:0] = "public_subscript_set_modify_read"
- expectTrue(testSetter.str ==
-            expectedResult(useOrig, "public_subscript_set_modify_read"))
+  expectTrue(PublicStruct(x: 0).str ==
+             expectedResult(useOrig, "public_struct_init"))
+  expectTrue(PublicStruct().function() ==
+             expectedResult(useOrig, "public_struct_func"))
+  expectTrue(PublicStruct().genericFunction(Int.self) ==
+             expectedResult(useOrig, "public_struct_generic_func"))
+  expectTrue(PublicStruct().public_stored_property ==
+             expectedResult(useOrig, "public_stored_property"))
+  expectTrue(PublicStruct()[0] ==
+             expectedResult(useOrig, "public_subscript_get"))
+  expectTrue(PublicStruct()[y:0] ==
+             expectedResult(useOrig, "public_subscript_get_modify_read"))
+  var testSetter = PublicStruct()
+  testSetter[0] = "public_subscript_set"
+  expectTrue(testSetter.str ==
+             expectedResult(useOrig, "public_subscript_set"))
+  testSetter[y:0] = "public_subscript_set_modify_read"
+  expectTrue(testSetter.str ==
+             expectedResult(useOrig, "public_subscript_set_modify_read"))
 
- expectTrue(PublicEnumeration<Int>.A.function() ==
-            expectedResult(useOrig, "public_enum_func"))
- expectTrue(PublicEnumeration<Int>.B.genericFunction(Int.self) ==
-            expectedResult(useOrig, "public_enum_generic_func"))
+  expectTrue(PublicEnumeration<Int>.A.function() ==
+             expectedResult(useOrig, "public_enum_func"))
+  expectTrue(PublicEnumeration<Int>.B.genericFunction(Int.self) ==
+             expectedResult(useOrig, "public_enum_generic_func"))
 }
 
 private func target_library_name(_ name: String) -> String {
@@ -139,7 +139,7 @@ DynamicallyReplaceable.test("DynamicallyReplaceable") {
 #if os(Linux)
 	_ = dlopen(target_library_name("Module2"), RTLD_NOW)
 #elseif os(Windows)
-        _ = LoadLibraryA(target_library_name("Module2"))
+  _ = LoadLibraryA(target_library_name("Module2"))
 #else
 	_ = dlopen(executablePath+target_library_name("Module2"), RTLD_NOW)
 #endif
