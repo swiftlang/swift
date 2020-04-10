435d13496e80c65cfb974b8b7f13b3fd5e30fc93
Xi Ge <xi_ge@apple.com>
2020-04-10 14:48:54 -0700

    AST: making `export: true` in @_specialized attribute a no-operation
    The client code doesn't actually call into these specialized functions even
    though they have public linkage. This could lead to TBD verification failure
    shown in rdar://44777994.
    
    This patch also warns users' codebase when `export: true` is specified.

--- lib/Parse/ParseDecl.cpp
+++ lib/Parse/ParseDecl.cpp
@@ -626,7 +626,11 @@ bool Parser::parseSpecializeAttributeArguments(
                  ParamLabel);
       }
       if (ParamLabel == "exported") {
+        auto trueLoc = Tok.getLoc();
         bool isTrue = consumeIf(tok::kw_true);
+        if (isTrue) {
+          diagnose(trueLoc, diag::attr_specialize_export_true_no_op);
+        }
         bool isFalse = consumeIf(tok::kw_false);
         if (!isTrue && !isFalse) {
           diagnose(Tok.getLoc(), diag::attr_specialize_expected_bool_value);
