f2e927dc7c62e4edbe41875cf1630ea1372c1c9c
Eric York <eyork@mac.com>
2020-04-11 23:13:39 -0700

    Moved if check

--- lib/Sema/TypeCheckDecl.cpp
+++ lib/Sema/TypeCheckDecl.cpp
@@ -676,13 +676,13 @@ IsStaticRequest::evaluate(Evaluator &evaluator, FuncDecl *decl) const {
       dc->isTypeContext()) {
     const auto operatorName = decl->getBaseIdentifier();
     if (auto ED = dyn_cast<ExtensionDecl>(dc->getAsDecl())) {
-        if (ED->getExtendedTypeRepr() == NULL) {
-            decl->diagnose(diag::nonstatic_operator_in_extension_no_type, operatorName)
+        if (ED->getExtendedTypeRepr()) {
+            decl->diagnose(diag::nonstatic_operator_in_extension,
+                           operatorName, ED->getExtendedTypeRepr())
                 .fixItInsert(decl->getAttributeInsertionLoc(/*forModifier=*/true),
                              "static ");
         } else {
-            decl->diagnose(diag::nonstatic_operator_in_extension,
-                           operatorName, ED->getExtendedTypeRepr())
+            decl->diagnose(diag::nonstatic_operator_in_extension_no_type, operatorName)
                 .fixItInsert(decl->getAttributeInsertionLoc(/*forModifier=*/true),
                              "static ");
         }
