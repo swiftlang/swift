06a0650e50ed64c7aeeb9620aa73166e5ede26c4
Eric York <eyork@mac.com>
2020-04-14 14:19:16 -0700

    Update test file

--- test/decl/ext/SR-12460.swift
+++
@@ -1,8 +0,0 @@
-// RUN: %target-swift-ide-test -print-indexed-symbols -source-filename %s
-
-struct Foo { }
-
-//===--- Test that we don't crash when validating members inside an extension with no type name.
-extension { // expected-error {{expected type name in extension declaration}}
-  static func ==(lhs: Foo, rhs: Foo) -> Bool {} // expected-error {{operator '==' declared in extension must be 'static'}}
-}
