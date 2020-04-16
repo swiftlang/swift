b27f3e0775ebfee1aebb42e165143058ed5349b1
Alexis Laferrière <alaferriere@apple.com>
2020-04-16 13:40:17 -0700

    [Sema] Extract per decl check for -require-explicit-availability

--- lib/Sema/TypeCheckAvailability.cpp
+++ lib/Sema/TypeCheckAvailability.cpp
@@ -2828,6 +2828,23 @@ bool swift::diagnoseDeclAvailability(const ValueDecl *Decl,
   return AW.diagAvailability(const_cast<ValueDecl *>(Decl), R, nullptr, Flags);
 }
 
+/// Should we warn that \p valueDecl needs an explicit availability annotation
+/// in -require-explicit-availaiblity mode?
+static bool declNeedsExplicitAvailability(const ValueDecl *valueDecl) {
+  AccessScope scope =
+    valueDecl->getFormalAccessScope(/*useDC*/nullptr,
+                                  /*treatUsableFromInlineAsPublic*/true);
+  if (!scope.isPublic() ||
+      valueDecl->getAttrs().hasAttribute<AlwaysEmitIntoClientAttr>())
+    return false;
+
+  // Warn on decls without an introduction version.
+  auto &ctx = valueDecl->getASTContext();
+  auto safeRangeUnderApprox = AvailabilityInference::availableRange(valueDecl, ctx);
+  return !safeRangeUnderApprox.getOSVersion().hasLowerEndpoint() &&
+         !valueDecl->getAttrs().isUnavailable(ctx);
+}
+
 void swift::checkExplicitAvailability(Decl *decl) {
   // Check only if the command line option was set.
   if (!decl->getASTContext().LangOpts.RequireExplicitAvailability)
@@ -2840,27 +2857,14 @@ void swift::checkExplicitAvailability(Decl *decl) {
 
   ValueDecl *valueDecl = dyn_cast<ValueDecl>(decl);
   if (valueDecl == nullptr) {
-    // decl should be either a ValueDecl or an ExtensionDecl
+    // decl should be either a ValueDecl or an ExtensionDecl.
     auto extension = cast<ExtensionDecl>(decl);
     valueDecl = extension->getExtendedNominal();
     if (!valueDecl)
       return;
   }
 
-  // Skip decls that are not public and not usable from inline.
-  AccessScope scope =
-    valueDecl->getFormalAccessScope(/*useDC*/nullptr,
-                                  /*treatUsableFromInlineAsPublic*/true);
-  if (!scope.isPublic() ||
-      decl->getAttrs().hasAttribute<AlwaysEmitIntoClientAttr>())
-    return;
-
-  // Warn on decls without an introduction version.
-  auto &ctx = decl->getASTContext();
-  auto safeRangeUnderApprox = AvailabilityInference::availableRange(decl, ctx);
-  if (!safeRangeUnderApprox.getOSVersion().hasLowerEndpoint() &&
-      !decl->getAttrs().isUnavailable(ctx)) {
-
+  if (declNeedsExplicitAvailability(valueDecl)) {
     auto diag = decl->diagnose(diag::public_decl_needs_availability);
 
     auto suggestPlatform = decl->getASTContext().LangOpts.RequireExplicitAvailabilityTarget;
@@ -2876,6 +2880,7 @@ void swift::checkExplicitAvailability(Decl *decl) {
       {
          llvm::raw_string_ostream Out(AttrText);
 
+         auto &ctx = valueDecl->getASTContext();
          StringRef OriginalIndent = Lexer::getIndentationForLine(
            ctx.SourceMgr, InsertLoc);
          Out << "@available(" << suggestPlatform << ", *)\n"
