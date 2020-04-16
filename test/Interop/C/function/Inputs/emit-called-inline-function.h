e9ca0c74dc5cb3d9c2a184e10bc3e8566d791229
Martin Boehme <mboehme@google.com>
2020-04-16 19:24:04 +0200

    Responses to review comments by @gribozavr.

--- test/Interop/C/function/Inputs/emit-called-inline-function.h
+++ test/Interop/C/function/Inputs/emit-called-inline-function.h
@@ -18,6 +18,20 @@ INLINE int calledTransitively() {
   return 42;
 }
 
+#ifdef __cplusplus
+class C {
+ public:
+  int memberFunctionCalledTransitively() {
+    return 42;
+  }
+};
+#endif
+
 INLINE int calledFromSwift() {
+#ifdef __cplusplus
+  C c;
+  return calledTransitively() + c.memberFunctionCalledTransitively();
+#else
   return calledTransitively();
+#endif
 }
--- test/Interop/C/function/emit-called-inline-function-irgen.swift
+++ test/Interop/C/function/emit-called-inline-function-irgen.swift
@@ -8,12 +8,13 @@
 // RUN: %target-swift-frontend %s -I %S/Inputs -Xcc -std=c99 -emit-ir -o - | %FileCheck %s -check-prefix C99 --implicit-check-not notCalled
 // RUN: %target-swift-frontend %s -I %S/Inputs -enable-cxx-interop -emit-ir -o - | %FileCheck %s -check-prefix CXX --implicit-check-not notCalled
 
-import EmitCalledFunction
+import EmitCalledInlineFunction
 
 // C99-DAG: define internal i32 @calledFromSwift() #{{[0-9]+}} {
 // C99-DAG: define internal i32 @calledTransitively() #{{[0-9]+}} {
 
 // CXX-DAG: define linkonce_odr i32 @_Z15calledFromSwiftv() #{{[0-9]+}} comdat {
 // CXX-DAG: define linkonce_odr i32 @_Z18calledTransitivelyv() #{{[0-9]+}} comdat {
+// CXX-DAG: define linkonce_odr i32 @_ZN1C32memberFunctionCalledTransitivelyEv(%class.C* %this)
 
 calledFromSwift()
