6321392e097e4f7f288637bf9dac1ce8dbe5d71f
Marcel Hlopko <hlopko@google.com>
2020-04-20 10:00:38 +0200

    Interleave code and checks

--- test/Interop/Cxx/extern-var/extern-var-irgen.swift
+++ test/Interop/Cxx/extern-var/extern-var-irgen.swift
@@ -2,44 +2,41 @@
 // RUN: %target-build-swift %s -I %S/Inputs -emit-ir -o %t/extern-var.ir -Xfrontend -enable-cxx-interop
 // RUN: %FileCheck < %t/extern-var.ir %s
 
-// CHECK: @counter = external global i32, align 4
-
-// CHECK: define hidden swiftcc i32 @"$s4main10getCounters5Int32VyF"() #0
-// CHECK: [[LOAD:%.*]] = load i32, i32* getelementptr inbounds (%Ts5Int32V, %Ts5Int32V* bitcast (i32* @counter to %Ts5Int32V*), i32 0, i32 0), align 4
-// CHECK: ret i32 [[LOAD]]
-
-// CHECK: define hidden swiftcc void @"$s4main10setCounteryys5Int32VF"(i32 %0) #0
-// CHECK: store i32 %0, i32* getelementptr inbounds (%Ts5Int32V, %Ts5Int32V* bitcast (i32* @counter to %Ts5Int32V*), i32 0, i32 0), align 4
-
-// CHECK: define hidden swiftcc i32 @"$s4main20getNamespacedCounters5Int32VyF"() #0
-//FIXME mangle non-top-level var names to prevent name collisions and check:
-// load i32, i32* getelementptr inbounds (%Ts5Int32V, %Ts5Int32V* bitcast (i32* @Namespaced.counter to %Ts5Int32V*), i32 0, i32 0), align 4
-// CHECK: ret i32 %1
-
-// CHECK: define hidden swiftcc void @"$s4main20setNamespacedCounteryys5Int32VF"(i32 %0) #0
-//FIXME mangle non-top-level var names to prevent name collisions and check:
-// store i32 %0, i32* getelementptr inbounds (%Ts5Int32V, %Ts5Int32V* bitcast (i32* @Namespaced.counter to %Ts5Int32V*), i32 0, i32 0), align 4
-
-// CHECK: define hidden swiftcc void @"$s4main17passingVarAsInoutyyF"() #0
-// CHECK: call swiftcc void @"$s4main11modifyInoutyys5Int32VzF"(%Ts5Int32V* nocapture dereferenceable(4) bitcast (i32* @counter to %Ts5Int32V*))
-
 import ExternVar
 
 func getCounter() -> CInt {
   return counter
 }
 
+// CHECK: @counter = external global i32, align 4
+
+// CHECK: define hidden swiftcc i32 @"$s4main10getCounters5Int32VyF"() #0
+// CHECK: [[LOAD:%.*]] = load i32, i32* getelementptr inbounds (%Ts5Int32V, %Ts5Int32V* bitcast (i32* @counter to %Ts5Int32V*), i32 0, i32 0), align 4
+// CHECK: ret i32 [[LOAD]]
+
 func setCounter(_ c: CInt) {
   counter = c
 }
+
+// CHECK: define hidden swiftcc void @"$s4main10setCounteryys5Int32VF"(i32 %0) #0
+// CHECK: store i32 %0, i32* getelementptr inbounds (%Ts5Int32V, %Ts5Int32V* bitcast (i32* @counter to %Ts5Int32V*), i32 0, i32 0), align 4
 
 func getNamespacedCounter() -> CInt {
   return Namespaced.counter
 }
 
+// CHECK: define hidden swiftcc i32 @"$s4main20getNamespacedCounters5Int32VyF"() #0
+//FIXME mangle non-top-level var names to prevent name collisions and check:
+// load i32, i32* getelementptr inbounds (%Ts5Int32V, %Ts5Int32V* bitcast (i32* @Namespaced.counter to %Ts5Int32V*), i32 0, i32 0), align 4
+// CHECK: ret i32 %1
+
 func setNamespacedCounter(_ c: CInt) {
   Namespaced.counter = c
 }
+
+// CHECK: define hidden swiftcc void @"$s4main20setNamespacedCounteryys5Int32VF"(i32 %0) #0
+//FIXME mangle non-top-level var names to prevent name collisions and check:
+// store i32 %0, i32* getelementptr inbounds (%Ts5Int32V, %Ts5Int32V* bitcast (i32* @Namespaced.counter to %Ts5Int32V*), i32 0, i32 0), align 4
 
 func modifyInout(_ c: inout CInt) {
   c = 42
@@ -48,3 +45,7 @@ func modifyInout(_ c: inout CInt) {
 func passingVarAsInout() {
   modifyInout(&counter)
 }
+
+// CHECK: define hidden swiftcc void @"$s4main17passingVarAsInoutyyF"() #0
+// CHECK: call swiftcc void @"$s4main11modifyInoutyys5Int32VzF"(%Ts5Int32V* nocapture dereferenceable(4) bitcast (i32* @counter to %Ts5Int32V*))
+
--- test/Interop/Cxx/extern-var/extern-var-silgen.swift
+++ test/Interop/Cxx/extern-var/extern-var-silgen.swift
@@ -2,6 +2,12 @@
 // RUN: %target-build-swift %s -I %S/Inputs -emit-sil -o %t/extern-var.sil -Xfrontend -enable-cxx-interop
 // RUN: %FileCheck < %t/extern-var.sil %s
 
+import ExternVar
+
+func getCounter() -> CInt {
+  return counter
+}
+
 // CHECK: sil_global @counter : $Int32
 
 // CHECK: sil hidden @$s4main10getCounters5Int32VyF : $@convention(thin) () -> Int32
@@ -10,10 +16,18 @@
 // CHECK: [[LOAD:%.*]] = load [[ACCESS]] : $*Int32
 // CHECK: return [[LOAD]] : $Int32
 
+func setCounter(_ c: CInt) {
+  counter = c
+}
+
 // CHECK: sil hidden @$s4main10setCounteryys5Int32VF : $@convention(thin) (Int32) -> ()
 // CHECK: [[COUNTER:%.*]] = global_addr @counter : $*Int32
 // CHECK: [[ACCESS:%.*]] = begin_access [modify] [dynamic] [[COUNTER]] : $*Int32
 // CHECK: store %0 to [[ACCESS]] : $*Int32
+
+func getNamespacedCounter() -> CInt {
+  return Namespaced.counter
+}
 
 // sil hidden @$s4main20getNamespacedCounters5Int32VyF : $@convention(thin) () -> Int32
 //FIXME mangle non-top-level var names to prevent name collisions
@@ -22,35 +36,15 @@
 // CHECK: [[LOAD:%.*]] = load [[ACCESS]] : $*Int32
 // CHECK: return [[LOAD]] : $Int32
 
+func setNamespacedCounter(_ c: CInt) {
+  Namespaced.counter = c
+}
+
 // CHECK: sil hidden @$s4main20setNamespacedCounteryys5Int32VF : $@convention(thin) (Int32) -> ()
 //FIXME mangle non-top-level var names to prevent name collisions
 // %1 = global_addr @Namespaced.counter : $*Int32
 // CHECK: [[ACCESS:%.*]] = begin_access [modify] [dynamic] %1 : $*Int32
 // CHECK: store %0 to [[ACCESS]] : $*Int32
-
-// CHECK: sil hidden @$s4main17passingVarAsInoutyyF : $@convention(thin) () -> ()
-// CHECK: [[COUNTER:%.*]] = global_addr @counter : $*Int32
-// CHECK: [[ACCESS:%.*]] = begin_access [modify] [dynamic] [[COUNTER]] : $*Int32
-// CHECK: [[FUNCTION:%.*]] = function_ref @$s4main11modifyInoutyys5Int32VzF : $@convention(thin) (@inout Int32) -> ()
-// CHECK: apply [[FUNCTION]]([[ACCESS]]) : $@convention(thin) (@inout Int32) -> ()
-
-import ExternVar
-
-func getCounter() -> CInt {
-  return counter
-}
-
-func setCounter(_ c: CInt) {
-  counter = c
-}
-
-func getNamespacedCounter() -> CInt {
-  return Namespaced.counter
-}
-
-func setNamespacedCounter(_ c: CInt) {
-  Namespaced.counter = c
-}
 
 func modifyInout(_ c: inout CInt) {
   c = 42
@@ -59,3 +53,9 @@ func modifyInout(_ c: inout CInt) {
 func passingVarAsInout() {
   modifyInout(&counter)
 }
+
+// CHECK: sil hidden @$s4main17passingVarAsInoutyyF : $@convention(thin) () -> ()
+// CHECK: [[COUNTER:%.*]] = global_addr @counter : $*Int32
+// CHECK: [[ACCESS:%.*]] = begin_access [modify] [dynamic] [[COUNTER]] : $*Int32
+// CHECK: [[FUNCTION:%.*]] = function_ref @$s4main11modifyInoutyys5Int32VzF : $@convention(thin) (@inout Int32) -> ()
+// CHECK: apply [[FUNCTION]]([[ACCESS]]) : $@convention(thin) (@inout Int32) -> ()
