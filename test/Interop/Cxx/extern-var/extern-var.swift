774b655b396976a9996cc6ed51d9dc1f6adb35f6
Marcel Hlopko <hlopko@google.com>
2020-04-20 10:57:39 +0200

    Reverse args to expectEqual

--- test/Interop/Cxx/extern-var/extern-var.swift
+++ test/Interop/Cxx/extern-var/extern-var.swift
@@ -12,48 +12,48 @@ import StdlibUnittest
 var ExternVarTestSuite = TestSuite("ExternVarTestSuite")
 
 ExternVarTestSuite.test("read") {
-  expectEqual(counter, 0)
-  expectEqual(getCounterFromCxx(), 0)
+  expectEqual(0, counter)
+  expectEqual(0, getCounterFromCxx())
 }
 
 ExternVarTestSuite.test("write-from-swift") {
   counter = 42
-  expectEqual(counter, 42)
-  expectEqual(getCounterFromCxx(), 42)
+  expectEqual(42, counter)
+  expectEqual(42, getCounterFromCxx())
 }
 
 ExternVarTestSuite.test("write-from-cxx") {
   setCounterFromCxx(84)
-  expectEqual(counter, 84)
-  expectEqual(getCounterFromCxx(), 84)
+  expectEqual(84, counter)
+  expectEqual(84, getCounterFromCxx())
 }
 
 //FIXME mangle non-top-level var names to prevent name collisions
 // ExternVarTestSuite.test("namespaced-read") {
-//   expectEqual(Namespaced.counter, 0)
-//   expectEqual(Namespaced.getCounterFromCxx(), 0)
+//   expectEqual(0, Namespaced.counter)
+//   expectEqual(0, Namespaced.getCounterFromCxx())
 // }
 
 //FIXME mangle non-top-level var names to prevent name collisions
 // ExternVarTestSuite.test("namespaced-write-from-swift") {
 //   Namespaced.counter = 42
-//   expectEqual(Namespaced.counter, 42)
-//   expectEqual(Namespaced.getCounterFromCxx(), 42)
+//   expectEqual(42, Namespaced.counter)
+//   expectEqual(42, amespaced.getCounterFromCxx())
 // }
 
 //FIXME mangle non-top-level var names to prevent name collisions
 // ExternVarTestSuite.test("namespaced-write-from-cxx") {
 //   Namespaced.setCounterFromCxx(84)
-//   expectEqual(Namespaced.counter, 84)
-//   expectEqual(Namespaced.getCounterFromCxx(), 84)
+//   expectEqual(84, Namespaced.counter)
+//   expectEqual(84, Namespaced.getCounterFromCxx())
 // }
 
 //FIXME mangle non-top-level var names to prevent name collisions
 // ExternVarTestSuite.test("no-collisions") {
 //   counter = 12
 //   Namespaced.counter = 42
-//   expectEqual(counter, 12)
-//   expectEqual(Namespaced.counter, 42)
+//   expectEqual(12, counter)
+//   expectEqual(42, Namespaced.counter)
 // }
 
 runAllTests()
