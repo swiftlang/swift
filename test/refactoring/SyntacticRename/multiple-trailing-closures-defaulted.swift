78b7bce3a08c69ceab4d8bf1a8be0bc774b4a95c
Nathan Hawes <nhawes@apple.com>
2020-04-20 19:18:25 -0700

    [IDE][Refactoring] Update syntactic rename to support braceless multiple trailing closures.

---
+++ test/refactoring/SyntacticRename/multiple-trailing-closures-defaulted.swift
@@ -0,0 +1,12 @@
+func /*test:def*/test(x: () -> () = {}, _ xx: () -> () = {}, y: () -> () = {}, z: ()->() = {}) {}
+
+/*test:call*/test(x: {}, {}) {}
+/*test:call*/test(x: {}) {} z: {}
+/*test:call*/test(x: {}, {}) {}
+/*test:call*/test {} y: {}
+/*test:call*/test(y: {}) {}
+
+
+// RUN: %empty-directory(%t.ranges)
+// RUN: %refactor -find-rename-ranges -source-filename %s -pos="test" -is-function-like -old-name "test(x:_:y:z:)" >> %t.ranges/multiple-trailing-closures-defaulted.swift.expected
+// RUN: diff -u %S/FindRangeOutputs/multiple-trailing-closures-defaulted.swift.expected %t.ranges/multiple-trailing-closures-defaulted.swift.expected
