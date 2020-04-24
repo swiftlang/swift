2141514710e5cfbc271f41716cd986e3e2f7493d
Mike Ash <mikeash@apple.com>
2020-04-24 16:22:57 -0400

    [Tools] Simplify swiftdt conformance cache dumping since the conformance cache keys are always metadata pointers.

--- include/swift/Reflection/ReflectionContext.h
+++ include/swift/Reflection/ReflectionContext.h
@@ -802,36 +802,16 @@ public:
     }
   }
 
-  std::string getNameOfMetadataOrContextDescriptor(StoredPointer Ptr, Demangler &dem) {
-    auto TypeDemangled = readDemanglingForContextDescriptor(Ptr, dem);
-    if (TypeDemangled)
-      return mangleNode(TypeDemangled);
-
-    if (auto Metadata = readMetadata(Ptr)) {
-      if (auto ClassWrapper = dyn_cast<TargetObjCClassWrapperMetadata<Runtime>>(Metadata)) {
-        if (auto Class = ClassWrapper->Class) {
-          std::string Name;
-          bool success = readObjCClassName(Class, Name);
-          if (success)
-            return Name;
-        }
-      }
-      auto Desc = getTypeContextDescriptor(Metadata.getLocalBuffer());
-      TypeDemangled = readDemanglingForContextDescriptor(stripSignedPointer(Desc), dem);
-      if (TypeDemangled)
-        return mangleNode(TypeDemangled);
-    }
-    
-    printf("Unable to get name of conformance type %#llx\n", Ptr);
-    return "";
-  }
-
   void dumpConformanceNode(const struct ConformanceNode *Node) {
-    Demangler dem;
-    
-    auto TypeName = getNameOfMetadataOrContextDescriptor(Node->Type, dem);
+    Demangle::Demangler Dem;
 
-    auto ProtocolDemangled = readDemanglingForContextDescriptor(Node->Proto, dem);
+    std::string TypeName = "<unknown>";
+    if (auto TR = readTypeFromMetadata(Node->Type)) {
+      auto Demangling = TR->getDemangling(Dem);
+      TypeName = nodeToString(Demangling);
+    }
+
+    auto ProtocolDemangled = readDemanglingForContextDescriptor(Node->Proto, Dem);
     auto ProtocolName = nodeToString(ProtocolDemangled);
     
     printf("Conformance: %s: %s\n", TypeName.c_str(), ProtocolName.c_str());
