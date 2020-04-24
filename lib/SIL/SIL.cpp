cebe79d4827f7eaddde743adf485ae9cde4bebaa
Saleem Abdulrasool <compnerd@compnerd.org>
2020-04-24 09:50:18 -0700

    SIL: use object libraries instead of globbing
    This simplifies the handling of the subdirectories in the SIL and
    SILOptimizer paths.  Create individual libraries as object libraries
    which allows the analysis of the source changes to be limited in scope.
    Because these are object libraries, this has 0 overhead compared to the
    previous implementation.  However, string operations over the filenames
    are avoided.  The cost for this is that any new sub-library needs to be
    added into the list rather than added with the special local function.

---
+++ lib/SIL/SIL.cpp
@@ -0,0 +1,15 @@
+//===--- SIL.cpp ---------------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2017 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+// DO NOT MODIFY THIS FILE!
+// The SIL library is split into sub-components, modify the respective
+// sub-component.
