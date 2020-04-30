2fbcf184ed0af0d7b872aeef434fe1f66ed52258
Marcel Hlopko <hlopko@google.com>
2020-04-30 16:13:33 +0200

    Add header guards to test/Interop

--- test/Interop/Cxx/extern-var/Inputs/extern-var.h
+++ test/Interop/Cxx/extern-var/Inputs/extern-var.h
@@ -1,3 +1,6 @@
+#ifndef TEST_INTEROP_CXX_EXTERN_VAR_INPUTS_EXTERN_VAR_H
+#define TEST_INTEROP_CXX_EXTERN_VAR_INPUTS_EXTERN_VAR_H
+
 extern int counter;
 
 int getCounterFromCxx();
@@ -9,3 +12,5 @@ extern int counter;
 int getCounterFromCxx();
 void setCounterFromCxx(int);
 } // namespace Namespaced
+
+#endif
