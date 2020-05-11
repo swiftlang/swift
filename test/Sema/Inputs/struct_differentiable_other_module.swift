37dc1b3e3e6e59de2c04368502abae6e6eeea114
Dan Zheng <danielzheng@google.com>
2020-05-10 22:38:21 -0700

    [AutoDiff] Remove upstreamed tensorflow branch tests.
    Remove upstreamed tensorflow branch derived conformances tests for
    `Differentiable` and `AdditiveArithmetic`.
    
    These were upstreamed in `test/AutoDiff/Sema/DerivedConformances`.

--- test/Sema/Inputs/struct_differentiable_other_module.swift
+++
@@ -1,9 +0,0 @@
-// SWIFT_ENABLE_TENSORFLOW
-
-// expected-note @+1 {{type declared here}}
-struct OtherFileNonconforming {}
-
-// expected-note @+1 {{type declared here}}
-struct GenericOtherFileNonconforming<T : Differentiable> {
-  var x: T
-}
