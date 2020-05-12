488333c4a2e6160c7cc928c4e61f1179436cff78
Meghana Gupta <meghanavgupta@gmail.com>
2020-05-12 01:08:19 -0700

    Add new pattern in SILCombine to optimize redundant thick to objc metatype conversions (#31595)
    Add pattern to catch the following redundant conversion:
    
    %tmp1 = thick_to_objc_metatype %x
    %tmp2 = objc_to_thick_metatype %tmp1
    ...
    %tmp3 = <sil operation> %tmp2
    
    to:
    
    %tmp3 = <sil operation> %x
    
    Similarly add pattern for redundant conversion of objc_to_thick_metatype
    followed by thick_to_objc_metatype.
    
    Fixes rdar://62932799

--- lib/SILOptimizer/SILCombiner/SILCombinerCastVisitors.cpp
+++ lib/SILOptimizer/SILCombiner/SILCombinerCastVisitors.cpp
@@ -452,6 +452,10 @@ SILCombiner::visitThickToObjCMetatypeInst(ThickToObjCMetatypeInst *TTOCMI) {
   if (CastOpt.optimizeMetatypeConversion(TTOCMI, MetatypeRepresentation::Thick))
     MadeChange = true;
 
+  if (auto *OCTTMI = dyn_cast<ObjCToThickMetatypeInst>(TTOCMI->getOperand())) {
+    TTOCMI->replaceAllUsesWith(OCTTMI->getOperand());
+    return eraseInstFromFunction(*TTOCMI);
+  }
   return nullptr;
 }
 
@@ -472,6 +476,10 @@ SILCombiner::visitObjCToThickMetatypeInst(ObjCToThickMetatypeInst *OCTTMI) {
   if (CastOpt.optimizeMetatypeConversion(OCTTMI, MetatypeRepresentation::ObjC))
     MadeChange = true;
 
+  if (auto *TTOCMI = dyn_cast<ThickToObjCMetatypeInst>(OCTTMI->getOperand())) {
+    OCTTMI->replaceAllUsesWith(TTOCMI->getOperand());
+    return eraseInstFromFunction(*OCTTMI);
+  }
   return nullptr;
 }
 
