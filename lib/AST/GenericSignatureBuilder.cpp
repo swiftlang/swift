892ded59c1141e0d4b41dea4e86f0fe4fb89ab2d
Slava Pestov <spestov@apple.com>
2020-05-15 21:10:51 -0400

    GSB: Concretize nested types when adding a superclass constraint
    When adding a superclass constraint, we need to find any nested
    types belonging to protocols that the superclass conforms to,
    and introduce implicit same-type constraints between each nested
    type and the corresponding type witness in the superclass's
    conformance to that protocol.
    
    Fixes <rdar://problem/39481178>, <https://bugs.swift.org/browse/SR-11232>.

--- lib/AST/GenericSignatureBuilder.cpp
+++ lib/AST/GenericSignatureBuilder.cpp
@@ -2542,7 +2542,13 @@ static void concretizeNestedTypeFromConcreteParent(
     GenericSignatureBuilder &builder) {
   auto parentEquiv = parent->getEquivalenceClassIfPresent();
   assert(parentEquiv && "can't have a concrete type without an equiv class");
+
+  bool isSuperclassConstrained = false;
   auto concreteParent = parentEquiv->concreteType;
+  if (!concreteParent) {
+    isSuperclassConstrained = true;
+    concreteParent = parentEquiv->superclass;
+  }
   assert(concreteParent &&
          "attempting to resolve concrete nested type of non-concrete PA");
 
@@ -2564,8 +2570,14 @@ static void concretizeNestedTypeFromConcreteParent(
          "No conformance requirement");
   const RequirementSource *parentConcreteSource = nullptr;
   for (const auto &constraint : parentEquiv->conformsTo.find(proto)->second) {
-    if (constraint.source->kind == RequirementSource::Concrete) {
-      parentConcreteSource = constraint.source;
+    if (!isSuperclassConstrained) {
+      if (constraint.source->kind == RequirementSource::Concrete) {
+        parentConcreteSource = constraint.source;
+      }
+    } else {
+      if (constraint.source->kind == RequirementSource::Superclass) {
+        parentConcreteSource = constraint.source;
+      }
     }
   }
 
@@ -4298,6 +4310,15 @@ bool GenericSignatureBuilder::updateSuperclass(
   auto updateSuperclassConformances = [&] {
     for (const auto &conforms : equivClass->conformsTo) {
       (void)resolveSuperConformance(type, conforms.first);
+    }
+
+    // Eagerly resolve any existing nested types to their concrete forms (others
+    // will be "concretized" as they are constructed, in getNestedType).
+    for (auto equivT : equivClass->members) {
+      for (auto nested : equivT->getNestedTypes()) {
+        concretizeNestedTypeFromConcreteParent(equivT, nested.second.front(),
+                                               *this);
+      }
     }
   };
 
@@ -7186,6 +7207,12 @@ void GenericSignatureBuilder::dump(llvm::raw_ostream &out) {
   out << "Potential archetypes:\n";
   for (auto pa : Impl->PotentialArchetypes) {
     pa->dump(out, &Context.SourceMgr, 2);
+  }
+  out << "\n";
+
+  out << "Equivalence classes:\n";
+  for (auto &equiv : Impl->EquivalenceClasses) {
+    equiv.dump(out, this);
   }
   out << "\n";
 }
