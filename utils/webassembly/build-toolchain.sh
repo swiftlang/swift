ae35be09ffa0815c5f5af787efb2f38556341829
Max Desiatov <max@desiatov.com>
2020-05-19 10:13:00 +0100

    Increase cp/rsync verbosity, fix destination path

--- utils/webassembly/build-toolchain.sh
+++ utils/webassembly/build-toolchain.sh
@@ -80,10 +80,10 @@ sed -i -e "s@\".*/include@\"../../../../share/wasi-sysroot/include@g" $SOURCE_PA
 if [[ "$(uname)" == "Linux" ]]; then
   # Avoid copying usr/lib/swift/clang because our toolchain's one is a directory
   # but nightly's one is symbolic link. A simple copy fails to merge them.
-  rsync -a $SOURCE_PATH/install/$TOOLCHAIN_NAME/usr/lib $HOST_TOOLCHAIN_SDK/usr/lib/ --exclude 'swift/clang'
+  rsync -v -a $SOURCE_PATH/install/$TOOLCHAIN_NAME/usr/lib $HOST_TOOLCHAIN_SDK/usr/lib --exclude 'swift/clang'
 else
-  cp -a $SOURCE_PATH/install/$TOOLCHAIN_NAME/usr/lib/swift_static $HOST_TOOLCHAIN_SDK/usr/lib/swift_static
-  cp -a $SOURCE_PATH/install/$TOOLCHAIN_NAME/usr/lib/swift/wasi $HOST_TOOLCHAIN_SDK/usr/lib/swift
+  cp -v -a $SOURCE_PATH/install/$TOOLCHAIN_NAME/usr/lib/swift_static $HOST_TOOLCHAIN_SDK/usr/lib/swift_static
+  cp -v -a $SOURCE_PATH/install/$TOOLCHAIN_NAME/usr/lib/swift/wasi $HOST_TOOLCHAIN_SDK/usr/lib/swift
 fi
 
 cd $HOST_TOOLCHAIN_DESTDIR
