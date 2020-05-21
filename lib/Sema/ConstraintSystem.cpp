fcc4befb21eece8e9b8f2be43621647448f41805
Holly Borla <hborla@apple.com>
2020-05-21 14:30:41 -0700

    [Property Wrappers] For properties that are implicitly default initialized via property wrapper, mark the init call as implicit.

--- lib/Sema/ConstraintSystem.cpp
+++ lib/Sema/ConstraintSystem.cpp
@@ -4251,6 +4251,8 @@ void SolutionApplicationTarget::maybeApplyPropertyWrapper() {
     if (!outermostWrapperType)
       return;
 
+    bool isImplicit = false;
+
     // Retrieve the outermost wrapper argument. If there isn't one, we're
     // performing default initialization.
     auto outermostArg = outermostWrapperAttr->getArg();
@@ -4258,6 +4260,7 @@ void SolutionApplicationTarget::maybeApplyPropertyWrapper() {
       SourceLoc fakeParenLoc = outermostWrapperAttr->getRange().End;
       outermostArg = TupleExpr::createEmpty(
           ctx, fakeParenLoc, fakeParenLoc, /*Implicit=*/true);
+      isImplicit = true;
     }
 
     auto typeExpr = TypeExpr::createImplicitHack(
@@ -4268,7 +4271,7 @@ void SolutionApplicationTarget::maybeApplyPropertyWrapper() {
         outermostWrapperAttr->getArgumentLabels(),
         outermostWrapperAttr->getArgumentLabelLocs(),
         /*hasTrailingClosure=*/false,
-        /*implicit=*/false);
+        /*implicit=*/isImplicit);
   }
   wrapperAttrs[0]->setSemanticInit(backingInitializer);
 
