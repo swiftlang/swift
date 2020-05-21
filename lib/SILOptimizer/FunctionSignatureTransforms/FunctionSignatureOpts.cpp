1f5608a3db1218063a5130e8e464242718d10df1
Michael Gottesman <mgottesman@apple.com>
2020-05-21 14:13:08 -0700

    [fso] Do not try to specialize pseudo-generic functions today.
    This is just fixing an assert violation. I also needed to add support for
    target-sil-opt(mock-sdk: ...) so I added support to lit for that here.
    
    <rdar://problem/62262811>
    
    (cherry picked from commit 6049ce629cdce9851f3c3106f5e11560b5a233e8)

--- lib/SILOptimizer/FunctionSignatureTransforms/FunctionSignatureOpts.cpp
+++ lib/SILOptimizer/FunctionSignatureTransforms/FunctionSignatureOpts.cpp
@@ -641,13 +641,22 @@ bool FunctionSignatureTransform::run(bool hasCaller) {
       TransformDescriptor.hasOnlyDirectInModuleCallers;
   SILFunction *F = TransformDescriptor.OriginalFunction;
 
-
   // If we are asked to assume a caller for testing purposes, set the flag.
   hasCaller |= FSOOptimizeIfNotCalled;
 
   if (!hasCaller && (F->getDynamicallyReplacedFunction() ||
                      canBeCalledIndirectly(F->getRepresentation()))) {
     LLVM_DEBUG(llvm::dbgs() << "  function has no caller -> abort\n");
+    return false;
+  }
+
+  // Bail if we have a pseudo-generic function. We do not handle these today. If
+  // we let it through here we crash when attempting to compute the optimized
+  // function type.
+  //
+  // TODO: Add support for this.
+  if (F->getLoweredFunctionType()->isPseudogeneric()) {
+    LLVM_DEBUG(llvm::dbgs() << "  function is pseudo-generic -> abort\n");
     return false;
   }
 
