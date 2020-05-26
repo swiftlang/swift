c9405fb7fc338f59db4386488245a3ce82227e00
Martin Boehme <mboehme@google.com>
2020-05-26 06:48:17 +0200

    Add Windows-name-mangled version of symbol to SIL test.

--- test/Interop/Cxx/class/constructors-sil.swift
+++ test/Interop/Cxx/class/constructors-sil.swift
@@ -8,6 +8,6 @@ import Constructors
 // CHECK: [[TYPE:%[0-9]+]] = metatype $@thin ConstructorWithParam.Type
 // CHECK: [[LITERAL:%[0-9]+]] = integer_literal $Builtin.Int32, 42
 // CHECK: [[INT:%[0-9]+]] = struct $Int32 ([[LITERAL]] : $Builtin.Int32)
-// CHECK: [[FUNC:%[0-9]+]] = function_ref @_ZN20ConstructorWithParamC1Ei : $@convention(c) (Int32, @thin ConstructorWithParam.Type) -> @out ConstructorWithParam
+// CHECK: [[FUNC:%[0-9]+]] = function_ref @{{_ZN20ConstructorWithParamC1Ei|\?\?0ConstructorWithParam@@QEAA@H@Z}} : $@convention(c) (Int32, @thin ConstructorWithParam.Type) -> @out ConstructorWithParam
 // CHECK: %{{[0-9]+}} = apply [[FUNC]]([[VAR]], [[INT]], [[TYPE]]) : $@convention(c) (Int32, @thin ConstructorWithParam.Type) -> @out ConstructorWithParam
 let _ = ConstructorWithParam(42)
