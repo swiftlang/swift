9ea707dc8ac2d906c5a3127607839b2b8dd878ec
Xi Ge <xi_ge@apple.com>
2020-05-27 08:03:38 -0700

    DependenciesScanner: stop reporting several arguments for building modules
    -o, -fmodule-map-file, and -fmodule-file should be directly chosen and specified by
    the build system. We should exclude them from the reported arguments for building
    modules.

--- lib/Frontend/ModuleInterfaceLoader.cpp
+++ lib/Frontend/ModuleInterfaceLoader.cpp
@@ -1344,10 +1344,6 @@ bool InterfaceSubContextDelegateImpl::runInSubCompilerInstance(StringRef moduleN
   subInvocation.getFrontendOptions().InputsAndOutputs
     .setMainAndSupplementaryOutputs(outputFiles, ModuleOutputPaths);
 
-  // Add -o for building the module explicitly.
-  BuildArgs.push_back("-o");
-  BuildArgs.push_back(outputPath);
-
   SmallVector<const char *, 64> SubArgs;
   std::string CompilerVersion;
   // Extract compiler arguments from the interface file and use them to configure
