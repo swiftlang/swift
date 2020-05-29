23ebf78e0dbcb5a912126bb7b9b597e50482a48c
Ben Cohen <ben_cohen@apple.com>
2020-05-29 08:55:02 -0700

    Add copywrite headers

--- tools/swift-inspect/Sources/swift-inspect/Metadata.swift
+++ tools/swift-inspect/Sources/swift-inspect/Metadata.swift
@@ -1,9 +1,14 @@
+//===----------------------------------------------------------------------===//
 //
-//  File.swift
-//  
+// This source file is part of the Swift.org open source project
 //
-//  Created by Ben Cohen on 5/16/20.
+// Copyright (c) 2014 - 2020 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
 //
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
 
 import SwiftRemoteMirror
 
