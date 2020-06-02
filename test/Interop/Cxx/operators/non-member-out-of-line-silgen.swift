176d7c4d0d055d3ffc926914c4b2209504768707
Michael Forster <forster@google.com>
2020-06-02 09:15:36 +0000

    Test mangled names on Windows

--- test/Interop/Cxx/operators/non-member-out-of-line-silgen.swift
+++ test/Interop/Cxx/operators/non-member-out-of-line-silgen.swift
@@ -4,7 +4,7 @@ import NonMemberOutOfLine
 
 public func add(_ lhs: IntBox, _ rhs: IntBox) -> IntBox { lhs + rhs }
 
-// CHECK: [[COUNTER:%.*]] = function_ref [[NAME:@.*]] : $@convention(c) (IntBox, IntBox) -> IntBox
+// CHECK: [[COUNTER:%.*]] = function_ref [[NAME:@(_Zpl6IntBoxS_|\?\?H@YA\?AUIntBox@@U0@0@Z)]] : $@convention(c) (IntBox, IntBox) -> IntBox
 // CHECK: apply [[COUNTER]](%0, %1) : $@convention(c) (IntBox, IntBox) -> IntBox
 
 // CHECK: sil [serializable] [clang "+"] [[NAME]] : $@convention(c) (IntBox, IntBox) -> IntBox
