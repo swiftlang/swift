89d3e4af4d2c34bb6f32c7322afcc781429fa614
Michael Forster <forster@google.com>
2020-06-04 19:56:53 +0200

    Fix Interop/Cxx/operators/non-member-out-of-line-irgen.swift
    Includes fixes for armv7 and arm64.

--- test/Interop/Cxx/operators/non-member-out-of-line-irgen.swift
+++ test/Interop/Cxx/operators/non-member-out-of-line-irgen.swift
@@ -4,5 +4,5 @@ import NonMemberOutOfLine
 
 public func add(_ lhs: IntBox, _ rhs: IntBox) -> IntBox { lhs + rhs }
 
-// CHECK: call i32 [[NAME:@(_Zpl6IntBoxS_|"\?\?H@YA\?AUIntBox@@U0@0@Z")]](i32 %{{[0-9]+}}, i32 %{{[0-9]+}})
-// CHECK: declare {{(dso_local )?}}i32 [[NAME]](i32, i32)
+// CHECK: call {{i32|i64}} [[NAME:@(_Zpl6IntBoxS_|"\?\?H@YA\?AUIntBox@@U0@0@Z")]]({{i32|\[1 x i32\]|i64}} %{{[0-9]+}}, {{i32|\[1 x i32\]|i64}} %{{[0-9]+}})
+// CHECK: declare {{(dso_local )?}}{{i32|i64}} [[NAME]]({{i32|\[1 x i32\]|i64}}, {{i32|\[1 x i32\]|i64}})
