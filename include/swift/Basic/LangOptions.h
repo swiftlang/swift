0534e8fcae0a71d0645fb8b42e23b78b076eae35
Robert Widmann <devteam.codafi@gmail.com>
2020-06-05 12:37:45 -0700

    XXX

--- include/swift/Basic/LangOptions.h
+++ include/swift/Basic/LangOptions.h
@@ -363,7 +363,7 @@ namespace swift {
 
     /// Whether to enable a more aggressive mode of incremental dependency
     /// gathering that never captures cascading edges.
-    bool EnableExperientalPrivateIntransitiveDependencies = false;
+    bool EnableExperientalPrivateIntransitiveDependencies = true;
 
     /// Enable verification when every SubstitutionMap is constructed.
     bool VerifyAllSubstitutionMaps = false;
