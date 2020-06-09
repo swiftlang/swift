ec059f34de783ccf35e19c6bc175dab5249c59d3
Robert Widmann <devteam.codafi@gmail.com>
2020-06-09 12:16:55 -0700

    Enable ExperientalPrivateIntransitiveDependencies By Default
    "Private Intransitive Dependencies" differ from the status quo by no
    longer requiring the concept of a "cascading dependency edge". This is
    because the request evaluator automatically tracks, records, and replays
    the names looked up while a given file is being processed by the
    frontend. To remove transitivity from the swiftdeps files, each
    primary file processed by the Swift frontend is charged for *all* name
    lookups that occur while it is being processed.  Further, because of
    the replay step, lookups hidden behind cached requests are now entirely
    visible to the dependency tracking code.
    
    The net result is that all formerly implicit transitivity in the
    dependency graph has been made completely explicit and direct. This
    establishes a tighter overall dependency structure for each individual
    file, and results in a remarkable decrease in the amount of
    files that are rebuilt for any particular change.
    
    This feature can be disabled via
    -disable-experimental-private-intransitive-dependencies, which will
    cause a return to the cascading status quo.

--- include/swift/Basic/LangOptions.h
+++ include/swift/Basic/LangOptions.h
@@ -367,7 +367,7 @@ namespace swift {
 
     /// Whether to enable a more aggressive mode of incremental dependency
     /// gathering that never captures cascading edges.
-    bool EnableExperientalPrivateIntransitiveDependencies = false;
+    bool EnableExperientalPrivateIntransitiveDependencies = true;
 
     /// Enable verification when every SubstitutionMap is constructed.
     bool VerifyAllSubstitutionMaps = false;
