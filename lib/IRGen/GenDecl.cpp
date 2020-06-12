76ca05f7b15f38df03c177a76f40e29ffa71073e
Keith Smiley <keithbsmiley@gmail.com>
2020-06-12 11:19:54 -0700

    reland: Emit coverage mappings for all modules
    This reverts commit 499ed05fd9571c2633acc19c30fa36dcb07a6002.
    
    This changes the num-threads passed to 1 to avoid a multithreaded output
    issue on windows

--- lib/IRGen/GenDecl.cpp
+++ lib/IRGen/GenDecl.cpp
@@ -1105,8 +1105,11 @@ void IRGenerator::emitGlobalTopLevel(llvm::StringSet<> *linkerDirectives) {
     IGM->emitSILDifferentiabilityWitness(&dw);
   }
 
-  // Emit code coverage mapping data.
-  PrimaryIGM->emitCoverageMapping();
+  // Emit code coverage mapping data for all modules
+  for (auto Iter : *this) {
+    IRGenModule *IGM = Iter.second;
+    IGM->emitCoverageMapping();
+  }
 
   for (auto Iter : *this) {
     IRGenModule *IGM = Iter.second;
