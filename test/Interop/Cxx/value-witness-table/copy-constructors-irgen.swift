b2b7f7b8536aeeb193eabb8c38074a5712343f8e
zoecarver <z.zoelec2@gmail.com>
2020-06-14 12:26:27 -0700

    [cxx-interop] Use user defined copy constructor to copy C++ objects.
    If a user-defined copy constructor exists, use that to copy imported C++
    types.

---
+++ test/Interop/Cxx/value-witness-table/copy-constructors-irgen.swift
@@ -0,0 +1,45 @@
+// RUN: %target-swift-frontend -enable-cxx-interop -I %S/Inputs %s -emit-ir | %FileCheck %s
+
+// REQUIRES: CPU=x86_64
+// REQUIRES: objc_interop
+
+import CopyConstructors
+
+// CHECK-LABEL: define swiftcc void @"$s4main31testUserProvidedCopyConstructor3objSo03HascdeF0V_AEtAE_tF"
+// CHECK: [[T0_DEST:%.*]] = bitcast %TSo30HasUserProvidedCopyConstructorV* [[ARG0:%[0-9]+]] to %struct.HasUserProvidedCopyConstructor*
+// CHECK: [[T0_SRC:%.*]] = bitcast %TSo30HasUserProvidedCopyConstructorV* [[ARG2:%[0-9]+]] to %struct.HasUserProvidedCopyConstructor*
+// CHECK: call void @_ZN30HasUserProvidedCopyConstructorC1ERKS_(%struct.HasUserProvidedCopyConstructor* [[T0_DEST]], %struct.HasUserProvidedCopyConstructor* [[T0_SRC]])
+// CHECK: [[T1_DEST:%.*]] = bitcast %TSo30HasUserProvidedCopyConstructorV* [[ARG1:%[0-9]+]] to %struct.HasUserProvidedCopyConstructor*
+// CHECK: [[T1_SRC:%.*]] = bitcast %TSo30HasUserProvidedCopyConstructorV* [[ARG2]] to %struct.HasUserProvidedCopyConstructor*
+// CHECK: call void @_ZN30HasUserProvidedCopyConstructorC1ERKS_(%struct.HasUserProvidedCopyConstructor* [[T1_DEST]], %struct.HasUserProvidedCopyConstructor* [[T1_SRC]])
+// CHECK: ret void
+
+// CHECK-LABEL: define linkonce_odr void @_ZN30HasUserProvidedCopyConstructorC1ERKS_
+
+public func testUserProvidedCopyConstructor(obj : HasUserProvidedCopyConstructor) -> (HasUserProvidedCopyConstructor, HasUserProvidedCopyConstructor) {
+  return (obj, obj)
+}
+
+// CHECK-LABEL: define swiftcc void @"$s4main26testDefaultCopyConstructor3defSo013HasNonTrivialcdE0V_AEtAE_tF"
+// CHECK: call void @_ZN35HasNonTrivialDefaultCopyConstructorC1ERKS_
+// CHECK: call void @_ZN35HasNonTrivialDefaultCopyConstructorC1ERKS_
+
+// Make sure we call the copy constructor of our member (HasUserProvidedCopyConstructor)
+// CHECK-LABEL: define linkonce_odr void @_ZN35HasNonTrivialDefaultCopyConstructorC1ERKS_
+// CHECK: call void @_ZN35HasNonTrivialDefaultCopyConstructorC2ERKS_
+
+public func testDefaultCopyConstructor(def : HasNonTrivialDefaultCopyConstructor) -> (HasNonTrivialDefaultCopyConstructor, HasNonTrivialDefaultCopyConstructor) {
+  return (def, def)
+}
+
+// CHECK-LABEL: define swiftcc void @"$s4main27testImplicitCopyConstructor3impSo013HasNonTrivialcdE0V_AEtAE_tF"
+// CHECK: call void @_ZN36HasNonTrivialImplicitCopyConstructorC1ERKS_
+// CHECK: call void @_ZN36HasNonTrivialImplicitCopyConstructorC1ERKS_
+
+// Same as above.
+// CHECK-LABEL: define linkonce_odr void @_ZN36HasNonTrivialImplicitCopyConstructorC1ERKS_
+// CHECK: call void  @_ZN36HasNonTrivialImplicitCopyConstructorC2ERKS_
+
+public func testImplicitCopyConstructor(imp : HasNonTrivialImplicitCopyConstructor) -> (HasNonTrivialImplicitCopyConstructor, HasNonTrivialImplicitCopyConstructor) {
+  return (imp, imp)
+}
