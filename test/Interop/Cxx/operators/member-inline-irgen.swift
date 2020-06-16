f381500e5b9fbf8cc8c24fa092adaf4c1c982dfb
Michael Forster <forster@google.com>
2020-06-16 13:27:29 +0200

    Simplify test to make it pass on Mac

--- test/Interop/Cxx/operators/member-inline-irgen.swift
+++ test/Interop/Cxx/operators/member-inline-irgen.swift
@@ -5,4 +5,4 @@ import MemberInline
 public func add(_ lhs: inout IntBox, _ rhs: IntBox) -> IntBox { lhs + rhs }
 
 // CHECK: call i32 @_ZN6IntBoxplES_(%struct.IntBox* %3, i32 %1)
-// CHECK: define linkonce_odr i32 @_ZN6IntBoxplES_(%struct.IntBox* %this, i32 %rhs.coerce) #2 comdat align 2 {
+// CHECK: define linkonce_odr i32 @_ZN6IntBoxplES_(%struct.IntBox* %this, i32 %rhs.coerce)
