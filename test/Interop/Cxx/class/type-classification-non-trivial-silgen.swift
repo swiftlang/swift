13632d4c9c965bf2c6232c23ff577fedc2ddfd93
zoecarver <z.zoelec2@gmail.com>
2020-06-15 19:49:52 -0700

    [cxx-interop] Mark types with a destructor a non-trivial.
    Any C++ type that isn't trivially copyable is now also not a trivial type. This will preserve the destructor (among other things).

---
+++ test/Interop/Cxx/class/type-classification-non-trivial-silgen.swift
@@ -0,0 +1,25 @@
+// RUN: %target-swift-frontend -I %S/Inputs -enable-cxx-interop -emit-sil %s | %FileCheck %s
+
+import TypeClassification
+
+// Make sure that "StructWithDestructor" is marked as non-trivial by checking for a
+// "destroy_addr".
+// CHECK-LABEL: @$s4main24testStructWithDestructoryyF
+// CHECK: [[AS:%.*]] = alloc_stack $StructWithDestructor
+// CHECK: destroy_addr [[AS]]
+// CHECK-LABEL: end sil function '$s4main24testStructWithDestructoryyF'
+public func testStructWithDestructor() {
+  let d = StructWithDestructor()
+}
+
+// Make sure that "HasMemberWithDestructor" is marked as non-trivial by checking
+// for a "destroy_addr".
+// CHECK-LABEL: @$s4main33testStructWithSubobjectDestructoryyF
+// CHECK: [[AS:%.*]] = alloc_stack $StructWithSubobjectDestructor
+// CHECK: destroy_addr [[AS]]
+// CHECK-LABEL: end sil function '$s4main33testStructWithSubobjectDestructoryyF'
+public func testStructWithSubobjectDestructor() {
+  let d = StructWithSubobjectDestructor()
+}
+
+
