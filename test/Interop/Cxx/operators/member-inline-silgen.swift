a0c9ae52e300067d2ac117258081e5dddbeddf38
Michael Forster <forster@google.com>
2020-06-17 11:50:47 +0200

    Fix tests for Windows and iOS

--- test/Interop/Cxx/operators/member-inline-silgen.swift
+++ test/Interop/Cxx/operators/member-inline-silgen.swift
@@ -7,7 +7,7 @@ public func add(_ lhs: inout IntBox, _ rhs: IntBox) -> IntBox { lhs + rhs }
 // CHECK: bb0([[SELF:%.*]] : $*IntBox, [[RHS:%.*]] : $IntBox):
 
 // CHECK: [[SELFACCESS:%.*]] = begin_access [modify] [static] [[SELF]] : $*IntBox
-// CHECK: [[OP:%.*]] = function_ref [[NAME:@_ZN6IntBoxplES_]] : $@convention(c) (@inout IntBox, IntBox) -> IntBox
+// CHECK: [[OP:%.*]] = function_ref [[NAME:@(_ZN6IntBoxplES_|\?\?HIntBox@@QEAA\?AU0@U0@@Z)]] : $@convention(c) (@inout IntBox, IntBox) -> IntBox
 // CHECK: apply [[OP]]([[SELFACCESS]], [[RHS]]) : $@convention(c) (@inout IntBox, IntBox) -> IntBox
 // CHECK: end_access [[SELFACCESS]] : $*IntBox
 
