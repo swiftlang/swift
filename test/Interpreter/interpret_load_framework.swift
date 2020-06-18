3095deb0f196858881f64240947ab99d1638d586
Doug Gregor <dgregor@apple.com>
2020-06-18 09:48:19 -0700

    [Tests] Update with explicit SDK linking where we missed it.

--- test/Interpreter/interpret_load_framework.swift
+++ test/Interpreter/interpret_load_framework.swift
@@ -1,6 +1,6 @@
 // RUN: %empty-directory(%t)
 // RUN: cp -R %S/Inputs/VerySmallObjCFramework.framework %t
-// RUN: %clang -dynamiclib %S/Inputs/VerySmallObjCFramework.m -fmodules -F %t -o %t/VerySmallObjCFramework.framework/VerySmallObjCFramework
+// RUN: %clang -dynamiclib %S/Inputs/VerySmallObjCFramework.m -fmodules -F %t -o %t/VerySmallObjCFramework.framework/VerySmallObjCFramework -L %sdk/usr/lib
 // RUN: %swift_driver -F %t %s | %FileCheck %s
 
 // REQUIRES: objc_interop 
