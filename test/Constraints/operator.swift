c49f0c4947906e37638be957cc5046f3e1ddfa5a
Pavel Yaskevich <pyaskevich@apple.com>
2020-06-27 11:36:48 -0700

    [TypeChecker] NFC: Clarify flaky diagnostic XFAIL
    Instead of XFAILing whole `test/Constraints/operator.swift`
    let's adjust diagnostic responsible for flakiness.
    
    (cherry picked from commit 171502174613ec1709b2bd16b8e2f5ffa26996fd)

--- test/Constraints/operator.swift
+++ test/Constraints/operator.swift
@@ -216,11 +216,13 @@ func rdar46459603() {
   let e = E.foo(value: "String")
   var arr = ["key": e]
 
+  // FIXME(rdar://problem/64844584) - on iOS simulator this diagnostic is flaky,
+  // either `referencing operator function '==' on 'Equatable'` or `operator function '==' requires`
   _ = arr.values == [e]
-  // expected-error@-1 {{referencing operator function '==' on 'Equatable' requires that 'Dictionary<String, E>.Values' conform to 'Equatable'}}
+  // expected-error@-1 {{requires that 'Dictionary<String, E>.Values' conform to 'Equatable'}}
   // expected-error@-2 {{cannot convert value of type '[E]' to expected argument type 'Dictionary<String, E>.Values'}}
   _ = [arr.values] == [[e]]
-  // expected-error@-1 {{operator function '==' requires that 'Dictionary<String, E>.Values' conform to 'Equatable'}}
+  // expected-error@-1 {{requires that 'Dictionary<String, E>.Values' conform to 'Equatable'}}
   // expected-error@-2 {{cannot convert value of type '[E]' to expected element type 'Dictionary<String, E>.Values'}}
 }
 
