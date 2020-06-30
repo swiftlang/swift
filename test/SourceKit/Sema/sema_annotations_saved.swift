223b5ed4e374b095ba7ac884730e3810d3e28581
Ben Langmuir <blangmuir@apple.com>
2020-06-30 16:08:32 -0700

    [test] use %diff to fix Windows test

--- test/SourceKit/Sema/sema_annotations_saved.swift
+++ test/SourceKit/Sema/sema_annotations_saved.swift
@@ -7,4 +7,4 @@ foo()
 // RUN:    == -req=edit -pos=3:1 -replace="foo()" -length=0 %s \
 // RUN:    == -req=edit -offset=0 -replace="" -length=0 %s \
 // RUN:    == -req=edit -offset=0 -replace="" -length=0 %s > %t.response
-// RUN: diff -u %s.response %t.response
+// RUN: %diff -u %s.response %t.response
