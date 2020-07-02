84489d87b2b493aadce8f9ad871990a10ae32264
Mishal Shah <mishal_shah@apple.com>
2020-07-02 16:26:10 -0700

    [Apple Silicon] Mark several tests that require the Swift interpreter accordingly

--- validation-test/execution/interpret-with-dependencies.swift
+++ validation-test/execution/interpret-with-dependencies.swift
@@ -1,5 +1,6 @@
 // REQUIRES: OS=macosx
 // RUN: %empty-directory(%t)
+// REQUIRES: swift_interpreter
 
 // RUN: echo 'int abc = 42;' | %clang -x c - -dynamiclib -Xlinker -install_name -Xlinker libabc.dylib -o %t/libabc.dylib -L %sdk/usr/lib
 // RUN: echo 'int test() { extern int abc; return abc; }' | %clang -x c - -L%t -dynamiclib -labc -o %t/libfoo.dylib -L %sdk/usr/lib
