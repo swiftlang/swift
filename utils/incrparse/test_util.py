6f97c7a2fb721f5e812965e4b0e81a47989e68fc
Saleem Abdulrasool <compnerd@compnerd.org>
2020-07-09 08:27:12 -0700

    test: make `test_util` more python 3 friendly
    The last set of changes to make it backwards compatible with Python 2
    required converting the arguments.  That is not compatible on Python 3
    unfortunately.  Only perform that on Python 2 to make the utility
    compatible with 2 and 3.

--- utils/incrparse/test_util.py
+++ utils/incrparse/test_util.py
@@ -22,7 +22,8 @@ def escapeCmdArg(arg):
 
 
 def run_command(cmd):
-    cmd = list(map(lambda s: s.encode('utf-8'), cmd))
+    if sys.version_info[0] < 3:
+      cmd = list(map(lambda s: s.encode('utf-8'), cmd))
     print(' '.join([escapeCmdArg(arg) for arg in cmd]))
     return subprocess.check_output(cmd, stderr=subprocess.STDOUT)
 
