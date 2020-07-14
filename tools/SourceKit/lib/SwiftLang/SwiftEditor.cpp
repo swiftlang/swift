bba238b153da8bf9a332edf4aec50d859ccbe668
Ben Langmuir <blangmuir@apple.com>
2020-07-14 16:50:38 -0700

    Revert "Merge pull request #32655 from benlangmuir/sema-repeat-53"
    This reverts commit 46c6eeeeadd38ed1c94498e472763ab68bf2dcd7, reversing
    changes made to eed09ea05011c3d7e08da0724ca7235971eb99fc.
    
    This caused an unexpecte regression.
    
    rdar://65554791

--- tools/SourceKit/lib/SwiftLang/SwiftEditor.cpp
+++ tools/SourceKit/lib/SwiftLang/SwiftEditor.cpp
@@ -660,7 +660,7 @@ public:
   }
 
 private:
-  std::vector<SwiftSemanticToken> getSemanticTokens(
+  std::vector<SwiftSemanticToken> takeSemanticTokens(
       ImmutableTextSnapshotRef NewSnapshot);
 
   Optional<std::vector<DiagnosticEntryInfo>> getSemanticDiagnostics(
@@ -763,12 +763,12 @@ void SwiftDocumentSemanticInfo::readSemanticInfo(
 
   llvm::sys::ScopedLock L(Mtx);
 
-  Tokens = getSemanticTokens(NewSnapshot);
+  Tokens = takeSemanticTokens(NewSnapshot);
   Diags = getSemanticDiagnostics(NewSnapshot, ParserDiags);
 }
 
 std::vector<SwiftSemanticToken>
-SwiftDocumentSemanticInfo::getSemanticTokens(
+SwiftDocumentSemanticInfo::takeSemanticTokens(
     ImmutableTextSnapshotRef NewSnapshot) {
 
   llvm::sys::ScopedLock L(Mtx);
@@ -776,15 +776,13 @@ SwiftDocumentSemanticInfo::getSemanticTokens(
   if (SemaToks.empty())
     return {};
 
-  auto result = SemaToks;
-
   // Adjust the position of the tokens.
   TokSnapshot->foreachReplaceUntil(NewSnapshot,
     [&](ReplaceImmutableTextUpdateRef Upd) -> bool {
-      if (result.empty())
+      if (SemaToks.empty())
         return false;
 
-      auto ReplaceBegin = std::lower_bound(result.begin(), result.end(),
+      auto ReplaceBegin = std::lower_bound(SemaToks.begin(), SemaToks.end(),
           Upd->getByteOffset(),
           [&](const SwiftSemanticToken &Tok, unsigned StartOffset) -> bool {
             return Tok.ByteOffset+Tok.Length < StartOffset;
@@ -794,7 +792,7 @@ SwiftDocumentSemanticInfo::getSemanticTokens(
       if (Upd->getLength() == 0) {
         ReplaceEnd = ReplaceBegin;
       } else {
-        ReplaceEnd = std::upper_bound(ReplaceBegin, result.end(),
+        ReplaceEnd = std::upper_bound(ReplaceBegin, SemaToks.end(),
             Upd->getByteOffset() + Upd->getLength(),
             [&](unsigned EndOffset, const SwiftSemanticToken &Tok) -> bool {
               return EndOffset < Tok.ByteOffset;
@@ -805,14 +803,14 @@ SwiftDocumentSemanticInfo::getSemanticTokens(
       int Delta = InsertLen - Upd->getLength();
       if (Delta != 0) {
         for (std::vector<SwiftSemanticToken>::iterator
-               I = ReplaceEnd, E = result.end(); I != E; ++I)
+               I = ReplaceEnd, E = SemaToks.end(); I != E; ++I)
           I->ByteOffset += Delta;
       }
-      result.erase(ReplaceBegin, ReplaceEnd);
+      SemaToks.erase(ReplaceBegin, ReplaceEnd);
       return true;
     });
 
-  return result;
+  return std::move(SemaToks);
 }
 
 Optional<std::vector<DiagnosticEntryInfo>>
