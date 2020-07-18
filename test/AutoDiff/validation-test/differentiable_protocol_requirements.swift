0aee9efbc051d7069bda3eb18a1280d212f51732
Dan Zheng <danielzheng@google.com>
2020-07-18 15:43:34 -0700

    Update test/AutoDiff/validation-test/differentiable_protocol_requirements.swift
    Co-authored-by: Suyash Srijan <suyashsrijan@outlook.com>

--- test/AutoDiff/validation-test/differentiable_protocol_requirements.swift
+++ test/AutoDiff/validation-test/differentiable_protocol_requirements.swift
@@ -2,7 +2,10 @@
 // REQUIRES: executable_test
 
 // Disabled due to test failure with `-O`: SR-13250.
-// REQUIRES: SR13250
+// SR-13250 is tracking the fix for compiling this test with optimizations.
+// XFAIL: swift_test_mode_optimize
+// XFAIL: swift_test_mode_optimize_size
+// XFAIL: swift_test_mode_optimize_unchecked
 
 import StdlibUnittest
 import DifferentiationUnittest
