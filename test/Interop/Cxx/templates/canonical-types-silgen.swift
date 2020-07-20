d41e7ae18890c706f0ba1348ef40acce5f494953
Marcel Hlopko <hlopko@google.com>
2020-07-20 10:35:58 +0200

    Remove debug test runs

--- test/Interop/Cxx/templates/canonical-types-silgen.swift
+++ test/Interop/Cxx/templates/canonical-types-silgen.swift
@@ -1,7 +1,4 @@
 // RUN: %target-swift-emit-sil %s -I %S/Inputs -enable-cxx-interop | %FileCheck %s
-// RUN: %target-swift-frontend -Xcc -target -Xcc x86_64-unknown-windows-msvc -Xcc -fno-PIC -emit-sil %s -I %S/Inputs -enable-cxx-interop | %FileCheck %s
-// RUN: %target-swift-frontend -Xcc -target -Xcc x86_64-apple-macosx10.9 -Xcc -fno-PIC -emit-sil %s -I %S/Inputs -enable-cxx-interop | %FileCheck %s
-// RUN: %target-swift-frontend -Xcc -target -Xcc arm64-apple-ios11.2.0 -Xcc -fno-PIC -emit-sil %s -I %S/Inputs -enable-cxx-interop | %FileCheck %s
 
 import CanonicalTypes
 
