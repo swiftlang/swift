78e78a23c46e12670127cccfb00a2e712af04560
Robert Widmann <devteam.codafi@gmail.com>
2020-07-23 19:59:29 -0700

    Drop Legacy Tests

--- test/Incremental/PrivateDependencies/private-function-fine.swift
+++ test/Incremental/PrivateDependencies/private-function-fine.swift
@@ -2,11 +2,11 @@
 // Also uses awk:
 // XFAIL OS=windows
 
-// RUN: %target-swift-frontend -emit-silgen -primary-file %s %S/Inputs/InterestingType.swift -DOLD -emit-reference-dependencies-path %t.swiftdeps -module-name main -enable-direct-intramodule-dependencies | %FileCheck %s -check-prefix=CHECK-OLD
+// RUN: %target-swift-frontend -emit-silgen -primary-file %s %S/Inputs/InterestingType.swift -DOLD -emit-reference-dependencies-path %t.swiftdeps -module-name main | %FileCheck %s -check-prefix=CHECK-OLD
 // RUN: %S/../../Inputs/process_fine_grained_swiftdeps.sh %swift-dependency-tool %t.swiftdeps %t-processed.swiftdeps
 // RUN: %FileCheck -check-prefix=CHECK-DEPS %s < %t-processed.swiftdeps
 
-// RUN: %target-swift-frontend -emit-silgen -primary-file %s %S/Inputs/InterestingType.swift -DNEW -emit-reference-dependencies-path %t.swiftdeps -module-name main -enable-direct-intramodule-dependencies | %FileCheck %s -check-prefix=CHECK-NEW
+// RUN: %target-swift-frontend -emit-silgen -primary-file %s %S/Inputs/InterestingType.swift -DNEW -emit-reference-dependencies-path %t.swiftdeps -module-name main | %FileCheck %s -check-prefix=CHECK-NEW
 // RUN: %S/../../Inputs/process_fine_grained_swiftdeps.sh %swift-dependency-tool %t.swiftdeps %t-processed.swiftdeps
 // RUN: %FileCheck -check-prefix=CHECK-DEPS %s < %t-processed.swiftdeps
 
