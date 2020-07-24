ac0ee65bb978bbb589f09b0da4c6d3b3e9b8cd3a
Marcel Hlopko <hlopko@google.com>
2020-07-24 09:24:38 +0200

    Fix i386 iphone target

--- test/Interop/Cxx/templates/canonical-types-irgen.swift
+++ test/Interop/Cxx/templates/canonical-types-irgen.swift
@@ -25,17 +25,17 @@ public func testCanonicalTypes() -> Bool {
 // CHECK: call i32 @{{_ZNK12MagicWrapperI11MagicNumberE10callGetIntEv|"\?callGetInt@\?\$MagicWrapper@UMagicNumber@@@@QEBAHXZ"}}(%struct.MagicWrapper* [[MAGIC_WRAPPER_B]])
 
 // CHECK: define weak_odr{{( dso_local)?}} i32 @{{_ZNK12MagicWrapperI11MagicNumberE10callGetIntEv|"\?callGetInt@\?\$MagicWrapper@UMagicNumber@@@@QEBAHXZ"}}(%struct.MagicWrapper* %this)
-// CHECK: %this.addr = alloca %struct.MagicWrapper*, align 8
-// CHECK: store %struct.MagicWrapper* %this, %struct.MagicWrapper** %this.addr, align 8
-// CHECK: %this1 = load %struct.MagicWrapper*, %struct.MagicWrapper** %this.addr, align 8
+// CHECK: %this.addr = alloca %struct.MagicWrapper*, align {{4|8}}
+// CHECK: store %struct.MagicWrapper* %this, %struct.MagicWrapper** %this.addr, align {{4|8}}
+// CHECK: %this1 = load %struct.MagicWrapper*, %struct.MagicWrapper** %this.addr, align {{4|8}}
 // CHECK: %t = getelementptr inbounds %struct.MagicWrapper, %struct.MagicWrapper* %this1, i32 0, i32 0
 // CHECK: %call = call i32 @{{_ZNK11MagicNumber6getIntEv|"\?getInt@MagicNumber@@QEBAHXZ"}}(%struct.MagicNumber* %t)
 // CHECK: %add = add nsw i32 %call, 5
 // CHECK: ret i32 %add
 
 // CHECK: define linkonce_odr{{( dso_local)?}} i32 @{{_ZNK11MagicNumber6getIntEv|"\?getInt@MagicNumber@@QEBAHXZ"}}(%struct.MagicNumber* %this)
-// CHECK: %this.addr = alloca %struct.MagicNumber*, align 8
-// CHECK: store %struct.MagicNumber* %this, %struct.MagicNumber** %this.addr, align 8
-// CHECK: %this1 = load %struct.MagicNumber*, %struct.MagicNumber** %this.addr, align 8
+// CHECK: %this.addr = alloca %struct.MagicNumber*, align {{4|8}}
+// CHECK: store %struct.MagicNumber* %this, %struct.MagicNumber** %this.addr, align {{4|8}}
+// CHECK: %this1 = load %struct.MagicNumber*, %struct.MagicNumber** %this.addr, align {{4|8}}
 // CHECK: ret i32 24
 
