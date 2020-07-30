806d77cb6a849c8926a3c3a98d7bf51664f419ac
Marcel Hlopko <hlopko@google.com>
2020-07-30 18:32:19 +0200

    Add demangling test

--- test/Interop/Cxx/templates/mangling-irgen.swift
+++ test/Interop/Cxx/templates/mangling-irgen.swift
@@ -2,13 +2,13 @@
 
 import Mangling
 
-public func recvInstantiation(_ i: inout WrappedMagicInt) {}
+public func receiveInstantiation(_ i: inout WrappedMagicInt) {}
 
-// CHECK: define {{(protected |dllexport )?}}swiftcc void @"$s4main17recvInstantiationyySo34__CxxTemplateInst12MagicWrapperIiEVzF"(%TSo34__CxxTemplateInst12MagicWrapperIiEV* nocapture dereferenceable(1) %0)
+// CHECK: define {{(protected |dllexport )?}}swiftcc void @"$s4main20receiveInstantiationyySo34__CxxTemplateInst12MagicWrapperIiEVzF"(%TSo34__CxxTemplateInst12MagicWrapperIiEV* nocapture dereferenceable(1) %0)
 
-public func recvInstantiation(_ i: inout WrappedMagicBool) {}
+public func receiveInstantiation(_ i: inout WrappedMagicBool) {}
 
-// CHECK: define {{(protected |dllexport )?}}swiftcc void @"$s4main17recvInstantiationyySo34__CxxTemplateInst12MagicWrapperIbEVzF"(%TSo34__CxxTemplateInst12MagicWrapperIbEV* nocapture dereferenceable(1) %0)
+// CHECK: define {{(protected |dllexport )?}}swiftcc void @"$s4main20receiveInstantiationyySo34__CxxTemplateInst12MagicWrapperIbEVzF"(%TSo34__CxxTemplateInst12MagicWrapperIbEV* nocapture dereferenceable(1) %0)
 
 public func returnInstantiation() -> WrappedMagicInt {
   return WrappedMagicInt()
