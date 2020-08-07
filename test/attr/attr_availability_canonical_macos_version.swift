19ad53e45f86cf62c98be1319eab2c644eeeef9d
Slava Pestov <spestov@apple.com>
2020-08-07 15:05:33 -0400

    Temporarily disable failing test

--- test/attr/attr_availability_canonical_macos_version.swift
+++ test/attr/attr_availability_canonical_macos_version.swift
@@ -1,5 +1,7 @@
 // RUN: %swift -typecheck -verify -parse-stdlib -module-name Swift -target x86_64-apple-macosx11.0 %s
 
+// REQUIRES: rdar66693249
+
 
 @available(OSX, introduced: 10.5, deprecated: 10.8, obsoleted: 11.0,
               message: "you don't want to do that anyway")
