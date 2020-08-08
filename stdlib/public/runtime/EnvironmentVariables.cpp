36d89a2cdbf7dd3da7c7ef3b3b56ee4e124682ed
Yuta Saito <kateinoigakukun@gmail.com>
2020-08-08 14:34:56 +0000

    [WASM] Add SWIFT_CC(swift) annotation for swift_COWSanityChecksEnabled to sync calling convention

--- stdlib/public/runtime/EnvironmentVariables.cpp
+++ stdlib/public/runtime/EnvironmentVariables.cpp
@@ -189,6 +189,7 @@ void swift::runtime::environment::initialize(void *context) {
 }
 #endif
 
+SWIFT_CC(swift)
 SWIFT_RUNTIME_EXPORT
 bool swift_COWSanityChecksEnabled() {
   return runtime::environment::SWIFT_DEBUG_ENABLE_COW_SANITY_CHECKS();
