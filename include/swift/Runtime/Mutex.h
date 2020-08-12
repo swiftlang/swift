121c7632ed4d5071a940bbcaf113852369fb67a7
Kuba Mracek <mracek@apple.com>
2020-08-12 15:33:10 -0700

    Add a single-threaded stdlib mode, use it for the 'minimal' stdlib

--- include/swift/Runtime/Mutex.h
+++ include/swift/Runtime/Mutex.h
@@ -20,12 +20,14 @@
 
 #include <type_traits>
 
-#if (defined(__APPLE__) || defined(__linux__) || defined(__CYGWIN__) || defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__HAIKU__))
+#ifdef SWIFT_STDLIB_SINGLE_THREADED_RUNTIME
+#include "swift/Runtime/MutexSingleThreaded.h"
+#elif (defined(__APPLE__) || defined(__linux__) || defined(__CYGWIN__) || defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__HAIKU__))
 #include "swift/Runtime/MutexPThread.h"
 #elif defined(_WIN32)
 #include "swift/Runtime/MutexWin32.h"
 #elif defined(__wasi__)
-#include "swift/Runtime/MutexWASI.h"
+#include "swift/Runtime/MutexSingleThreaded.h"
 #else
 #error "Implement equivalent of MutexPThread.h/cpp for your platform."
 #endif
--- stdlib/public/runtime/MutexPThread.cpp
+++ stdlib/public/runtime/MutexPThread.cpp
@@ -15,7 +15,7 @@
 //
 //===----------------------------------------------------------------------===//
 
-#if !defined(_WIN32) && !defined(__wasi__)
+#if !defined(_WIN32) && !defined(__wasi__) && !defined(SWIFT_STDLIB_SINGLE_THREADED_RUNTIME)
 #include "swift/Runtime/Mutex.h"
 
 #include "swift/Runtime/Debug.h"
