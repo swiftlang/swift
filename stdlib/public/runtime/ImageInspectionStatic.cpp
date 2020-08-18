1b66f9276803f3624562ef0c0e4fd8c2ffa99447
Kuba (Brecka) Mracek <mracek@apple.com>
2020-08-18 08:16:45 -0700

    Update header in ImageInspectionStatic.cpp

--- stdlib/public/runtime/ImageInspectionStatic.cpp
+++ stdlib/public/runtime/ImageInspectionStatic.cpp
@@ -1,4 +1,4 @@
-//===--- ImageInspectionMachO.cpp - Mach-O image inspection ---------------===//
+//===--- ImageInspectionStatic.cpp - image inspection for static stdlib ---===//
 //
 // This source file is part of the Swift.org open source project
 //
