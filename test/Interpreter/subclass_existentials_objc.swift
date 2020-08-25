2e42ba358d18fb18a1c2494951f830841f960e3b
Tim Kientzle <tkientzle@apple.com>
2020-08-25 12:38:37 -0700

    Fill out this test further, try to document the failing checks more clearly

--- test/Interpreter/subclass_existentials_objc.swift
+++ test/Interpreter/subclass_existentials_objc.swift
@@ -12,19 +12,19 @@
 //
 // RUN: %empty-directory(%t)
 //
-// RUN: %target-build-swift %s -O -swift-version 5 -o %t/a.swift5.O.out
-// RUN: %target-codesign %t/a.swift5.O.out
-// RUN: %target-run %t/a.swift5.O.out
-//
-// RUN: %target-build-swift %s -Onone -swift-version 5 -o %t/a.swift5.Onone.out
+// RUN: %target-build-swift %s -g -Onone -swift-version 5 -o %t/a.swift5.Onone.out
 // RUN: %target-codesign %t/a.swift5.Onone.out
 // RUN: %target-run %t/a.swift5.Onone.out
 //
-// RUN: %target-build-swift %s -O -swift-version 4 -o %t/a.swift4.O.out
+// RUN: %target-build-swift %s -g -O -swift-version 5 -o %t/a.swift5.O.out
+// RUN: %target-codesign %t/a.swift5.O.out
+// RUN: %target-run %t/a.swift5.O.out
+//
+// RUN: %target-build-swift %s -g -O -swift-version 4 -o %t/a.swift4.O.out
 // RUN: %target-codesign %t/a.swift4.O.out
 // RUN: %target-run %t/a.swift4.O.out
 //
-// RUN: %target-build-swift %s -Onone -swift-version 4 -o %t/a.swift4.Onone.out
+// RUN: %target-build-swift %s -g -Onone -swift-version 4 -o %t/a.swift4.Onone.out
 // RUN: %target-codesign %t/a.swift4.Onone.out
 // RUN: %target-run %t/a.swift4.Onone.out
 //
@@ -34,8 +34,6 @@
 
 import StdlibUnittest
 import Foundation
-
-// FIXME: Actually write proper tests here.
 
 func cast<T, U>(_ t: T, to: U.Type) -> U? {
   return t as? U
@@ -50,23 +48,104 @@ class D : C, OP {}
 
 var SubclassExistentialsTestSuite = TestSuite("SubclassExistentials")
 
-SubclassExistentialsTestSuite.test("Metatype self-conformance") {
-  expectFalse(CP.self is AnyObject.Type)
+SubclassExistentialsTestSuite.test("test1(): Existential basics") {
+  // Making the test body a named function allows you to set a breakpoint more easily:
+  // (lldb) break set -n test1
+  test1()
+}
+
+func test1() {
+  // An OP instance can be cast to OP or AnyObject as expected
+  let op : OP = D()
+  expectTrue(op is OP)
+  expectNotNil(cast(op, to: OP.self))
+  expectTrue(op is D)
+  expectNotNil(cast(op, to: D.self))
+  expectTrue(op is AnyObject)
+  expectNotNil(cast(op, to: AnyObject.self))
+  expectTrue(D.self is AnyObject.Type)
+  expectNotNil(cast(D.self, to: AnyObject.Type.self))
+}
+
+SubclassExistentialsTestSuite.test("test2(): ObjC Metatype self-conformance") {
+  test2()
+}
+
+func test2() {
+  // Obj-C protocols self-conform if they have no static requirements
+  expectTrue(OP.self is OP.Type) // Self-conformance
+
+  // The following should be the same as above, but with a runtime cast instead of compile-time
+  // It fails because the runtime cannot find the necessary protocol conformance
+  // Bug: Compiler isn't emitting these conformances?
+  // In optimized build, this asserts because the casting oracle knows that it succeeds,
+  // so the optimizer converts it into an unconditional cast.
+  //expectFailure { expectNotNil(cast(OP.self, to: OP.Type.self)) }
+
+  // The following cast should succeed, because:
+  // 1. OP.self is OP.Type due to self-conformance above
+  // 2. OP is a sub-type of AnyObject (because OP is implicitly class-constrained)
+  // 3. OP.Type is a sub-type of AnyObject.Type
+  // 4. So OP.self is AnyObject.Type
+  expectFailure { expectTrue(OP.self is AnyObject.Type) }
+  expectFailure { expectNotNil(cast(OP.self, to: AnyObject.Type.self)) } // Ditto
+  // Casting to AnyObject doesn't change the representation, hence this equality
   expectEqual(OP.self, OP.self as AnyObject.Type)
+}
 
+SubclassExistentialsTestSuite.test("test3(): Non-ObjC metatypes do not self-conform") {
+  test3()
+}
+
+func test3() {
+  // Non-ObjC protocols do not generally self-conform
+  expectFalse(CP.self is CP.Type) // No self-conformance
+  expectNil(cast(CP.self, to: CP.Type.self)) // Ditto
+  expectFalse(CP.self is AnyObject.Type)
   expectNil(cast(CP.self, to: AnyObject.Type.self))
-  // FIXME
-  expectNil(cast(OP.self, to: AnyObject.Type.self))
+}
 
-  // FIXME: Sema says 'always true', runtime says false.
-  //
-  // Runtime code had a FIXME in it already, so I think Sema is right.
-  expectFalse(OP.self is AnyObject.Type)
-  expectFalse((C & OP).self is AnyObject.Type)
-  expectFalse((C & OP).self is OP.Type)
+SubclassExistentialsTestSuite.test("test4(): (C & OP) acts like an Obj-C protocol") {
+  test4()
+}
 
-  expectTrue(D.self is (C & OP).Type)
-  expectFalse(OP.self is (C & OP).Type)
+func test4() {
+  // (C & OP) acts like an Obj-C protocol (because OP is)
+  expectFailure { expectTrue((C & OP).self is (C & OP).Type) } // Self-conforms
+  expectFailure { expectNotNil(cast((C & OP).self, to: (C & OP).Type.self)) } // Ditto
+
+  // Casting oracle knows that the next cast must always succeed
+  // (This is why there's a "warning: 'is' test is always true")
+  expectTrue((C & OP).self is OP.Type) // (C & OP) is sub-protocol of OP
+
+  // The following cast is the same as the one above, except in a form that
+  // forces the compiler to use the runtime machinery.  Because the oracle
+  // knows it will succeed, the optimizer legitimately converts it into an
+  // unconditional runtime cast.  Which in turn leads to an assert when it fails at runtime.
+  // Bug: I suspect this is the same bug as above -- the compiler isn't emitting
+  // the protocol conformance information that the runtime requires to verify this cast.
+  //expectFailure { expectNotNil(cast((C & OP).self, to: OP.Type.self)) } // Ditto
+
+  expectFailure { expectTrue((C & OP).self is AnyObject.Type) } // (C & OP) is a sub-protocol of AnyObject
+  expectFailure { expectNotNil(cast((C & OP).self, to: AnyObject.Type.self)) } // Ditto
+}
+
+SubclassExistentialsTestSuite.test("test5(): (C & OP) is a sub-protocol of OP") {
+  test5()
+}
+
+func test5() {
+  expectTrue((C & OP).self is (C & OP).Protocol) // By definition
+  expectNotNil(cast((C & OP).self, to: (C & OP).Protocol.self)) // Ditto
+  expectFalse(D.self is (C & OP).Protocol) // (C & OP).self is only instance of (C&OP).Protocol
+  expectNil(cast(D.self, to: (C & OP).Protocol.self)) // Ditto
+  expectTrue(D.self is (C & OP).Type) // True, since D conforms to (C & OP)
+  expectFalse(OP.self is (C & OP).Protocol) // (C & OP).self is only instance of (C&OP).Protocol
+  expectNil(cast(OP.self, to: (C & OP).Protocol.self)) // Ditto
+  expectFalse(OP.self is (C & OP).Type) // True
+
+  expectFalse((C & OP).self is OP.Protocol) // OP.self is only instance of OP.Protocol
+  expectNil(cast((C & OP).self, to: OP.Protocol.self)) // ditto
 }
 
 runAllTests()
