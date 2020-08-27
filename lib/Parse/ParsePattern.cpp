0f81b400d07a3dd4326de9618c880244df61b487
Jorge Revuelta Herrero <minuscorp@gmail.com>
2020-08-27 13:45:22 +0200

    Backtrack to find the throws keyword

--- lib/Parse/ParsePattern.cpp
+++ lib/Parse/ParsePattern.cpp
@@ -854,10 +854,13 @@ void Parser::parseAsyncThrows(
     
     if (!peekToken().isKeyword()) {
       BacktrackingScope backtrackingScope(*this);
-      ASTContext &Ctx = SF.getASTContext();
-      DiagnosticSuppression SuppressedDiags(Ctx.Diags);
-      ParserResult<TypeRepr> result = parseType();
-      throwsType = result.getPtrOrNull();
+      if (peekToken().is(tok::kw_throws)) {
+        ASTContext &Ctx = SF.getASTContext();
+        DiagnosticSuppression SuppressedDiags(Ctx.Diags);
+        backtrackingScope.cancelBacktrack();
+        ParserResult<TypeRepr> result = parseType();
+        throwsType = result.getPtrOrNull();
+      }
     }
 
     if (existingArrowLoc.isValid()) {
