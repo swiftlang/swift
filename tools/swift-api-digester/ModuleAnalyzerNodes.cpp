13e0e7294857a6d7c5df489dadcbf63f5b18d497
Ben Rimmington <me@benrimmington.com>
2020-08-29 06:08:28 +0100

    Update api-digester tool

--- tools/swift-api-digester/ModuleAnalyzerNodes.cpp
+++ tools/swift-api-digester/ModuleAnalyzerNodes.cpp
@@ -1611,9 +1611,9 @@ SDKContext::shouldIgnore(Decl *D, const Decl* Parent) const {
   } else {
     if (D->isPrivateStdlibDecl(false))
       return true;
+    if (AvailableAttr::isUnavailable(D))
+      return true;
   }
-  if (AvailableAttr::isUnavailable(D))
-     return true;
   if (auto VD = dyn_cast<ValueDecl>(D)) {
     switch (getAccessLevel(VD)) {
     case AccessLevel::Internal:
