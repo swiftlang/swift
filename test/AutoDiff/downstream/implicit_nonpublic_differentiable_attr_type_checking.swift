fa746766c89c0d20e216a47c141fbf3e55235c91
Dan Zheng <danielzheng@google.com>
2020-08-30 17:32:42 +0000

    [TF] Update AutoDiff test.
    `-print-ast` no longer prints the AST if diagnostics are emitted.
    Edit test case to no longer emit diagnostics.

--- test/AutoDiff/downstream/implicit_nonpublic_differentiable_attr_type_checking.swift
+++ test/AutoDiff/downstream/implicit_nonpublic_differentiable_attr_type_checking.swift
@@ -3,24 +3,12 @@
 // Test implicit `@differentiable` attributes for non-public protocol witnesses.
 
 protocol InternalProtocol: Differentiable {
-  // expected-note @+3 {{protocol requires function 'publicMethod' with type '(Float) -> Float'}}
-  @differentiable(wrt: self)
-  @differentiable(wrt: (self, x))
-  func publicMethod(_ x: Float) -> Float
-
   @differentiable(wrt: self)
   @differentiable(wrt: (self, x))
   func internalMethod(_ x: Float) -> Float
 }
 
-// expected-error @+1 {{type 'PublicConformingStruct' does not conform to protocol 'InternalProtocol'}}
 public struct PublicConformingStruct: InternalProtocol {
-  // Expected: error for missing `@differentiable` attribute on public protocol witness.
-  // expected-note @+1 {{candidate is missing attribute '@differentiable'}}
-  public func publicMethod(_ x: Float) -> Float {
-    x
-  }
-
   // Expected: no error for missing `@differentiable` attribute on internal protocol witness.
   // Implicit `@differentiable` attributes should be created.
   func internalMethod(_ x: Float) -> Float {
@@ -29,7 +17,6 @@ public struct PublicConformingStruct: InternalProtocol {
 }
 
 // CHECK-LABEL: public struct PublicConformingStruct : InternalProtocol {
-// CHECK:   public func publicMethod(_ x: Float) -> Float
 // CHECK:   @differentiable(wrt: (self, x))
 // CHECK:   @differentiable(wrt: self)
 // CHECK:   internal func internalMethod(_ x: Float) -> Float
