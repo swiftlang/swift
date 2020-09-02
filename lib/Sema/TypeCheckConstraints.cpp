1131c243e482c45eea293b6baf1d2e26895503cd
Zollerboy1 <josef@walterzollerpiano.com>
2020-09-02 20:04:08 +0200

    Fix FunctionTypeRepr constructor and some code style issues

--- lib/Sema/TypeCheckConstraints.cpp
+++ lib/Sema/TypeCheckConstraints.cpp
@@ -1771,7 +1771,8 @@ TypeExpr *PreCheckExpression::simplifyTypeExpr(Expr *E) {
 
     auto NewTypeRepr = new (ctx)
         FunctionTypeRepr(nullptr, ArgsTypeRepr, AE->getAsyncLoc(),
-                         AE->getThrowsLoc(), AE->getThrowsType(), AE->getArrowLoc(), ResultTypeRepr);
+                         AE->getThrowsLoc(), AE->getThrowsType(),
+                         AE->getArrowLoc(), ResultTypeRepr);
     return new (ctx) TypeExpr(NewTypeRepr);
   }
   
