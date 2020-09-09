3892d9c44db5b2f2094d7c78ed23587e0f43c730
Suyash Srijan <suyashsrijan@outlook.com>
2020-09-09 02:45:36 +0100

    [Test] Fix comments in test file

--- test/decl/var/nan_comparisons.swift
+++ test/decl/var/nan_comparisons.swift
@@ -1,19 +1,19 @@
 // RUN: %target-typecheck-verify-swift
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
-/////// Comparison with '.nan' static property instead of using '.isNan' instance property ///////
+/////// Comparison with '.nan' static property instead of using '.isNaN' instance property ///////
 //////////////////////////////////////////////////////////////////////////////////////////////////
 
 // One side is '.nan' and the other isn't.
-// Using '==' or '!=' for comparison should suggest using '.isNan'.
+// Using '==' or '!=' for comparison should suggest using '.isNaN'.
 
 let double: Double = 0.0
-_ = double == .nan // expected-warning {{comparison with '.nan' using '==' is always false, use 'double.isNan' to check if 'double' is not a number}}
-_ = double != .nan // expected-warning {{comparison with '.nan' using '!=' is always true, use '!double.isNan' to check if 'double' is a number}}
-_ = 0.0 == .nan // // expected-warning {{comparison with '.nan' using '==' is always false, use '0.0.isNan' to check if '0.0' is not a number}}
+_ = double == .nan // expected-warning {{comparison with '.nan' using '==' is always false, use 'double.isNaN' to check if 'double' is not a number}}
+_ = double != .nan // expected-warning {{comparison with '.nan' using '!=' is always true, use '!double.isNaN' to check if 'double' is a number}}
+_ = 0.0 == .nan // // expected-warning {{comparison with '.nan' using '==' is always false, use '0.0.isNaN' to check if '0.0' is not a number}}
 
 // One side is '.nan' and the other isn't. Using '>=', '>', '<', '<=' for comparison:
-// We can't suggest using '.isNan' here.
+// We can't suggest using '.isNaN' here.
 
 _ = 0.0 >= .nan // expected-warning {{comparison with '.nan' using '>=' is always false}}
 _ = .nan > 1.1 // expected-warning {{comparison with '.nan' using '>' is always false}}
@@ -21,7 +21,7 @@ _ = .nan < 2.2 // expected-warning {{comparison with '.nan' using '<' is always 
 _ = 3.3 <= .nan // expected-warning {{comparison with '.nan' using '<=' is always false}}
 
 // Both sides are '.nan':
-// We can't suggest using '.isNan' here.
+// We can't suggest using '.isNaN' here.
 
 _ = Double.nan == Double.nan // expected-warning {{'.nan' == '.nan' is always false}}
 _ = Double.nan != Double.nan // expected-warning {{'.nan' != '.nan' is always true}}
