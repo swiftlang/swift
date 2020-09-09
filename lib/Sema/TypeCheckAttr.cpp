2424b2e275caacf135100341f61ae5b15eacbfc0
Kuba Mracek <mracek@apple.com>
2020-09-08 20:26:32 -0700

    experiment with AttributeChecker::visitFrozenAttr

--- lib/Sema/TypeCheckAttr.cpp
+++ lib/Sema/TypeCheckAttr.cpp
@@ -2874,7 +2874,7 @@ void AttributeChecker::visitImplementsAttr(ImplementsAttr *attr) {
 void AttributeChecker::visitFrozenAttr(FrozenAttr *attr) {
   if (auto *ED = dyn_cast<EnumDecl>(D)) {
     if (!ED->getModuleContext()->isResilient()) {
-      attr->setInvalid();
+      //attr->setInvalid();
       return;
     }
 
