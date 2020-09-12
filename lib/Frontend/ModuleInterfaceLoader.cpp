f9e02946efb0ae103e78740cc4b79ca2deb7a2fb
Robert Widmann <devteam.codafi@gmail.com>
2020-09-12 00:09:05 -0600

    Restore InputMode For Module Interface

--- lib/Frontend/ModuleInterfaceLoader.cpp
+++ lib/Frontend/ModuleInterfaceLoader.cpp
@@ -1142,6 +1142,8 @@ void InterfaceSubContextDelegateImpl::inheritOptionsForBuildingInterface(
     genericSubInvocation.setSDKPath(SearchPathOpts.SDKPath);
   }
 
+  genericSubInvocation.getFrontendOptions().InputMode
+      = FrontendOptions::ParseInputMode::SwiftModuleInterface;
   if (!SearchPathOpts.RuntimeResourcePath.empty()) {
     genericSubInvocation.setRuntimeResourcePath(SearchPathOpts.RuntimeResourcePath);
   }
