67abcb4a9a04994518f939655787bb8335bef643
Suyash Srijan <suyashsrijan@outlook.com>
2020-09-14 20:27:16 +0100

    [Test] Fix a failing SourceKit test

--- test/SourceKit/Sema/educational_note_diags.swift
+++ test/SourceKit/Sema/educational_note_diags.swift
@@ -2,14 +2,14 @@ extension (Int, Int) {}
 
 // RUN: %sourcekitd-test -req=sema %s -- %s | %FileCheck %s -check-prefix=NO_OVERRIDE
 
-// NO_OVERRIDE:      key.description: "non-nominal type
+// NO_OVERRIDE:      key.description: "tuple type
 // NO_OVERRIDE:      key.educational_note_paths: [
 // NO_OVERRIDE-NEXT:   share{{[/\\]+}}doc{{[/\\]+}}swift{{[/\\]+}}diagnostics{{[/\\]+}}nominal-types.md"
 // NO_OVERRIDE-NEXT: ]
 
 // RUN: %sourcekitd-test -req=sema %s -- -Xfrontend -diagnostic-documentation-path -Xfrontend /educational/notes/path/prefix %s | %FileCheck %s -check-prefix=OVERRIDE
 
-// OVERRIDE:      key.description: "non-nominal type
+// OVERRIDE:      key.description: "tuple type
 // OVERRIDE:      key.educational_note_paths: [
 // OVERRIDE-NEXT:   "{{[/\\]+}}educational{{[/\\]+}}notes{{[/\\]+}}path{{[/\\]+}}prefix{{[/\\]+}}nominal-types.md"
 // OVERRIDE-NEXT: ]
