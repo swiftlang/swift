f956e4f7041d48529eee12a87cbfc9b187338099
Doug Gregor <dgregor@apple.com>
2020-09-18 17:40:44 -0700

    [Conformance checker] Eliminate redundant recording of missing witnesses.

--- lib/Sema/TypeCheckProtocol.cpp
+++ lib/Sema/TypeCheckProtocol.cpp
@@ -3800,8 +3800,7 @@ ResolveWitnessResult ConformanceChecker::resolveWitnessViaDefault(
     recordOptionalWitness(requirement);
     return ResolveWitnessResult::Success;
   }
-  // Save the missing requirement for later diagnosis.
-  GlobalMissingWitnesses.insert({requirement, {}});
+
   return ResolveWitnessResult::ExplicitFailed;
 }
 
