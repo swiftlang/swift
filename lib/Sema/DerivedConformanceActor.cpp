8f3c912f46dc6a2ee3734f741de50bdebf803ec0
Doug Gregor <dgregor@apple.com>
2020-09-28 21:16:59 -0700

    [Concurrency] Fix new source file header.

--- lib/Sema/DerivedConformanceActor.cpp
+++ lib/Sema/DerivedConformanceActor.cpp
@@ -1,4 +1,4 @@
-//===--- DerivedConformanceEquatableHashable.cpp - Derived Equatable & co -===//
+//===--- DerivedConformanceActor.cpp - Derived Actor Conformance ----------===//
 //
 // This source file is part of the Swift.org open source project
 //
