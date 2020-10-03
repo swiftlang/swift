81496a14696780357221e92c80b0b7abb8a3d1dc
Yuta Saito <kateinoigakukun@gmail.com>
2020-10-03 12:23:15 +0000

    [WASM] Re-enable all stdlib tests on Linux
    The wasmer cranelift backend is broken since 1.0.0-alpha3, so we use
    singlepass backend instead until the bug will be resolved.
    
    https://github.com/wasmerio/wasmer/issues/1674

--- utils/webassembly/macos/install-dependencies.sh
+++ utils/webassembly/macos/install-dependencies.sh
@@ -3,7 +3,13 @@
 set -ex
 
 brew uninstall $(brew list | grep python@2)
-brew install cmake ninja llvm sccache wasmer
+brew install cmake ninja llvm sccache
+
+# Install latest wasmer
+
+if [ ! -e ~/.wasmer/bin/wasmer ]; then
+  curl https://get.wasmer.io -sSfL | sh
+fi
 
 SOURCE_PATH="$( cd "$(dirname $0)/../../../../" && pwd  )"
 SWIFT_PATH=$SOURCE_PATH/swift
