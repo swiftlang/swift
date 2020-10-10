f57180a68d689eeef6e8eae3a02f20f84782d8f4
Holly Borla <hborla@apple.com>
2020-10-09 21:01:53 -0700

    [Property Wrappers] Don't allow property wrapper attributes to have arguments when applied to parameters.

--- lib/Sema/TypeCheckAttr.cpp
+++ lib/Sema/TypeCheckAttr.cpp
@@ -2995,6 +2995,12 @@ void AttributeChecker::visitCustomAttr(CustomAttr *attr) {
       return;
     }
 
+    if (isa<ParamDecl>(D) && attr->getArg()) {
+      diagnose(attr->getArg()->getLoc(), diag::property_wrapper_param_arg);
+      attr->setInvalid();
+      return;
+    }
+
     return;
   }
 
