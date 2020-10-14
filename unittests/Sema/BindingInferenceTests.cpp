a24383112214c2a23a2008f5b704a0acb513b9fe
Pavel Yaskevich <pyaskevich@apple.com>
2020-10-14 16:15:58 -0700

    [unittest/Sema] Add a test-case for a single hop protocol requirement inference
    In situations like:
    
    `$T0 <convertible> $T1`
    `$T1 <conforms to> P`
    
    `$T0` should know about `P` as a transitive protocol requirement.

--- unittests/Sema/BindingInferenceTests.cpp
+++ unittests/Sema/BindingInferenceTests.cpp
@@ -13,6 +13,7 @@
 #include "SemaFixture.h"
 #include "swift/AST/Expr.h"
 #include "swift/Sema/ConstraintSystem.h"
+#include "llvm/ADT/SmallPtrSet.h"
 
 using namespace swift;
 using namespace swift::unittest;
@@ -43,4 +44,45 @@ TEST_F(SemaTest, TestIntLiteralBindingInference) {
 
   ASSERT_TRUE(binding.BindingType->isEqual(getStdlibType("Int")));
   ASSERT_TRUE(binding.hasDefaultedLiteralProtocol());
+}
+
+TEST_F(SemaTest, TestTransitiveProtocolInference) {
+  ConstraintSystemOptions options;
+  ConstraintSystem cs(DC, options);
+
+  auto *PD1 =
+      new (Context) ProtocolDecl(DC, SourceLoc(), SourceLoc(),
+                                 Context.getIdentifier("P1"), /*Inherited=*/{},
+                                 /*trailingWhere=*/nullptr);
+  PD1->setImplicit();
+
+  auto *protocolTy1 = ProtocolType::get(PD1, Type(), Context);
+
+  auto *GPT = cs.createTypeVariable(cs.getConstraintLocator({}),
+                                    /*options=*/TVO_CanBindToNoEscape);
+
+  cs.addConstraint(
+      ConstraintKind::ConformsTo, GPT, protocolTy1,
+      cs.getConstraintLocator({}, LocatorPathElt::TypeParameterRequirement(
+                                      0, RequirementKind::Conformance)));
+
+  // First, let's try inferring through a single conversion
+  // relationship.
+  {
+    auto *typeVar = cs.createTypeVariable(cs.getConstraintLocator({}),
+                                          /*options=*/0);
+
+    cs.addConstraint(
+        ConstraintKind::Conversion, typeVar, GPT,
+        cs.getConstraintLocator({}, LocatorPathElt::ContextualType()));
+
+    auto bindings = inferBindings(cs, typeVar);
+    ASSERT_TRUE(bindings.Protocols.empty());
+
+    const auto &inferredProtocols = bindings.TransitiveProtocols;
+    ASSERT_TRUE(bool(inferredProtocols));
+    ASSERT_EQ(inferredProtocols->size(), (unsigned)1);
+    ASSERT_TRUE(
+        (*inferredProtocols->begin())->getSecondType()->isEqual(protocolTy1));
+  }
 }
