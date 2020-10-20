05faa07c48ec15cf6fb98b73164c49b6601a20c2
zoecarver <z.zoelec2@gmail.com>
2020-10-20 11:58:46 -0700

    [cxx-interop] Support bool-based enums.
    This is a small fix to prevent a crash. This change simply adds another
    condition for the "bool" branch that checks if "type" is associated with
    a "clang::EnumDecl" with an underlying type of "bool", and if so, treats
    "type" as a "Bool".

---
+++ test/Interop/Cxx/enum/Inputs/bool-enums.h
@@ -0,0 +1,6 @@
+enum Maybe : bool { No, Yes };
+enum BinaryNumbers : bool { One = 1, Zero = 0 };
+enum class EnumClass : bool { Foo, Bar };
+struct WrapperStruct {
+  enum InnerBoolEnum : bool { A, B };
+};
