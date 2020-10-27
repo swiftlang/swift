0ccc5e51459eeb85eb3999a25076c52d3b7f8b16
Konrad `ktoso` Malawski <konrad_malawski@apple.com>
2020-10-27 18:42:53 +0900

    [Concurrency] Add extra test for unsafe continuations; showcasing typical usage

--- test/Concurrency/async_tasks.swift
+++ test/Concurrency/async_tasks.swift
@@ -6,6 +6,40 @@ func someAsyncFunc() async -> String { "" }
 struct MyError: Error {}
 func someThrowingAsyncFunc() async throws -> String { throw MyError() }
 
+// ==== Unsafe Continuations ---------------------------------------------------
+
+struct Vegetable {}
+
+func buyVegetables(
+  shoppingList: [String],
+  // a) if all veggies were in store, this is invoked *exactly-once*
+  onGotAllVegetables: ([Vegetable]) -> (),
+
+  // b) if not all veggies were in store, invoked one by one (one or more times)
+  onGotVegetable: (Vegetable) -> (),
+  // b) if at least one onGotVegetable was called *exactly-once*
+  // this is invoked once no more veggies will be emitted
+  onNoMoreVegetables: () -> (),
+  // c) if no veggies _at all_ were available, this is invoked *exactly once*
+  onNoVegetablesInStore: (Error) -> ()
+) {}
+
+// returns 1 or more vegetables or throws an error
+func buyVegetables(shoppingList: [String]) async throws -> [Vegetable] {
+  await try Task.withUnsafeThrowingContinuation { continuation in
+    var veggies: [Vegetable] = []
+
+    buyVegetables(
+      shoppingList: shoppingList,
+      onGotAllVegetables: { veggies in continuation.resume(returning: veggies) },
+      onGotVegetable: { v in veggies.append(v) },
+      onNoMoreVegetables: { continuation.resume(returning: veggies) },
+      onNoVegetablesInStore: { error in continuation.resume(throwing: error) }
+      )
+  }
+}
+
+
 func test_unsafeContinuations() async {
   // the closure should not allow async operations;
   // after all: if you have async code, just call it directly, without the unsafe continuation
