d5f13c40d26a5d39c398e2cfcd8989cf58ac0724
Artem Chikin <achikin@apple.com>
2020-11-04 10:47:27 -0800

    Revert the Revert of #34545: [Dependency Scanner] Add missing clang overlay dependencies for placeholder modules
    With the added test fixed to be more generic to platforms.
    
    This reverts commit 0f00ea175be1263324092107ca6692e06da988b6, reversing
    changes made to 0092608acc94aa15d830452ab726ad3375aed7ac.

---
+++ test/ScanDependencies/placholder_overlay_deps.swift
@@ -0,0 +1,30 @@
+// RUN: %empty-directory(%t)
+// RUN: mkdir -p %t/clang-module-cache
+// RUN: mkdir -p %t/inputs
+
+// RUN: echo "[{" > %/t/inputs/map.json
+// RUN: echo "\"moduleName\": \"Darwin\"," >> %/t/inputs/map.json
+// RUN: echo "\"modulePath\": \"%/t/inputs/Darwin.swiftmodule\"," >> %/t/inputs/map.json
+// RUN: echo "\"docPath\": \"%/t/inputs/Darwin.swiftdoc\"," >> %/t/inputs/map.json
+// RUN: echo "\"sourceInfoPath\": \"%/t/inputs/Darwin.swiftsourceinfo\"," >> %/t/inputs/map.json
+// RUN: echo "\"isFramework\": false" >> %/t/inputs/map.json
+// RUN: echo "}]" >> %/t/inputs/map.json
+
+// RUN: %target-swift-frontend -scan-dependencies -module-cache-path %t/clang-module-cache %s -placeholder-dependency-module-map-file %t/inputs/map.json -o %t/deps.json
+
+// Check the contents of the JSON output
+// RUN: %FileCheck %s < %t/deps.json
+
+// REQUIRES: executable_test
+// REQUIRES: objc_interop
+// REQUIRES: OS=macosx
+import Metal
+
+// Ensure the dependency on Darwin is captured even though it is a placeholder
+
+// CHECK:   "modulePath": "Metal.swiftmodule",
+// CHECK-NEXT:   "sourceFiles": [
+// CHECK-NEXT:   ],
+// CHECK:     {
+// CHECK:       "swiftPlaceholder": "Darwin"
+// CHECK:     },
