4126fd9b01a18940bb2602880346ae725667d1ce
Marcel Hlopko <hlopko@google.com>
2020-11-06 09:08:14 +0100

    Add CInt test

--- test/Interop/Cxx/templates/Inputs/magic-wrapper.h
+++ test/Interop/Cxx/templates/Inputs/magic-wrapper.h
@@ -1,15 +1,21 @@
 #ifndef TEST_INTEROP_CXX_TEMPLATES_INPUTS_MAGIC_WRAPPER_H
 #define TEST_INTEROP_CXX_TEMPLATES_INPUTS_MAGIC_WRAPPER_H
 
+struct IntWrapper {
+  int value;
+  int getValue() const { return value; }
+};
+
 template<class T>
 struct MagicWrapper {
   T t;
   int getValuePlusArg(int arg) const { return t.getValue() + arg; }
 };
 
-struct IntWrapper {
-  int value;
-  int getValue() const { return value; }
+template<>
+struct MagicWrapper<int> {
+  int i;
+  int getValuePlusArg(int arg) const { return i + arg; }
 };
 
 #endif // TEST_INTEROP_CXX_TEMPLATES_INPUTS_MAGIC_WRAPPER_H
--- test/Interop/Cxx/templates/class-template-instantiation-in-swift.swift
+++ test/Interop/Cxx/templates/class-template-instantiation-in-swift.swift
@@ -2,15 +2,21 @@
 //
 // REQUIRES: executable_test
 
-import UsingDirective
+import MagicWrapper
 import StdlibUnittest
 
 var TemplatesTestSuite = TestSuite("TemplatesTestSuite")
 
-TemplatesTestSuite.test("without-definition") {
-  var intWrapper = IntWrapper(value: 42)
+TemplatesTestSuite.test("with-c++-type") {
+  let intWrapper = IntWrapper(value: 42)
   var wrappedMagicNumber = MagicWrapper<IntWrapper>(t: intWrapper)
   expectEqual(wrappedMagicNumber.getValuePlusArg(8), 50)
+}
+
+TemplatesTestSuite.test("with-swift-type") {
+  let _ = MagicWrapper<CInt>()
+  var wrappedMagicNumber = MagicWrapper<CInt>(i: 13)
+  expectEqual(wrappedMagicNumber.getValuePlusArg(8), 21)
 }
 
 runAllTests()
