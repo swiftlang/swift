72d3d30aef214a3a0d70ab515f127d09b9df60f8
Kavon Farvardin <kfarvardin@apple.com>
2020-11-06 12:51:19 -0800

    add regression test for -disable-implicit-concurrency-module-import

---
+++ test/ImportResolution/disable-implicit-concurrency-module-import.swift
@@ -0,0 +1,5 @@
+// RUN: %target-typecheck-verify-swift -enable-experimental-concurrency -disable-implicit-concurrency-module-import
+// REQUIRES: concurrency
+
+
+protocol P : Actor { } // expected-error{{cannot find type 'Actor' in scope}}
