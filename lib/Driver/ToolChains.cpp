e4916b8c856b00759a6274ca9ef1c5459826157f
Xi Ge <xi_ge@apple.com>
2020-11-09 11:28:47 -0800

    Front-end: add a front-end action to print supported features of the compiler
    This could help swift-driver to check whether a compiler feature is supported so
    it could adjust arguments accordingly.

--- lib/Driver/ToolChains.cpp
+++ lib/Driver/ToolChains.cpp
@@ -599,6 +599,8 @@ const char *ToolChain::JobContext::computeFrontendModeForCompile() const {
     return "-emit-imported-modules";
   case file_types::TY_JSONDependencies:
     return "-scan-dependencies";
+  case file_types::TY_JSONFeatures:
+    return "-emit-supported-features";
   case file_types::TY_IndexData:
     return "-typecheck";
   case file_types::TY_Remapping:
@@ -870,6 +872,7 @@ ToolChain::constructInvocation(const BackendJobAction &job,
     case file_types::TY_ClangModuleFile:
     case file_types::TY_IndexData:
     case file_types::TY_JSONDependencies:
+    case file_types::TY_JSONFeatures:
       llvm_unreachable("Cannot be output from backend job");
     case file_types::TY_Swift:
     case file_types::TY_dSYM:
