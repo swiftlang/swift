d5d7f391305884289267efd1c8c0ad8ff7ea971f
Arnold Schwaighofer <aschwaighofer@apple.com>
2020-11-16 08:20:32 -0800

    Fix test for windows/linux

--- test/IRGen/async/debug.swift
+++ test/IRGen/async/debug.swift
@@ -1,7 +1,7 @@
 // RUN: %target-swift-frontend -primary-file %s -emit-ir -enable-experimental-concurrency -g | %FileCheck %s
 
 // Don't assert on dynamically sized variables.
-// CHECK: define swiftcc void @"$s5debug1fyxxYKlF"
+// CHECK: define{{( dllexport)?}}{{( protected)?}} swiftcc void @"$s5debug1fyxxYKlF"
 
 public func f<Success>(_ value: Success) async throws -> Success {
   switch Result<Success, Error>.success(value) {
