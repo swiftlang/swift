f33f1d3646bd0fed56004abb7370cc10132a559f
Nate Chandler <nate_chandler@apple.com>
2020-11-30 12:56:58 -0800

    [Async CC] Always add full type metadata to bindings.
    NecessaryBindings are used by both async functions and partial apply
    forwarders.  The latter are able to avoid bindings in some cases because
    a new function is generated where the information that would otherwise
    be available in the bindings can be made available.  That is not the
    case for async functions.  A generic async function requires all of the
    metadata and witness tables be passed along to it: unlike a partial
    apply forwarder it isn't in any way specialized so this information
    can't be recovered.
    
    Previously, metadata bindings were always passed along to async
    functions.  However, destructuring that can be done for partial apply
    forwarders was still being applied.  This resulted in an inappropriate
    and unexpected number of bindings in NecessaryBindings.
    
    Here, that destructuring is avoided for metadata passed to async
    functions.
    
    Now, the full metadata required by async functions are passed along to
    them as necessary.
    
    rdar://problem/71816041

---
+++ validation-test/compiler_crashers_2_fixed/rdar71816041.swift
@@ -0,0 +1,8 @@
+// RUN: %target-swift-frontend -emit-ir -primary-file %s -enable-experimental-concurrency
+
+func getIntAndString() async -> (Int, String) { (5, "1") }
+
+func testDecompose() async -> Int {
+  async let (i, s) = await getIntAndString()
+  return await i
+}
