bd96959d145180298459809ed6588a33de10de1e
zoecarver <z.zoelec2@gmail.com>
2020-12-08 09:56:06 -0800

    [cxx-interop] Re-implement namespaces using enums + extensions.
    C++ namespaces are module-independent, but enums are owned by their module's in Swift. So, to prevent declaring two enums with the same name, this patch implements a new approach to namespaces: enums with extensions.
    
    Here's an example:
    ```
    // Module A
    namespace N { void test1(); }
    // Module B
    namespace N { void test2(); }
    // __ObjC module
    enum N { }
    // Swift module A
    extension N { func test1() }
    // Swift module B
    extension N { func test1() }
    ```
    
    Thanks to @gribozavr for the great idea.

---
+++ test/Interop/Cxx/namespace/classes-irgen.swift
@@ -0,0 +1,31 @@
+// RUN: %target-swift-emit-ir -I %S/Inputs -enable-cxx-interop %s | %FileCheck %s
+
+import Classes
+
+// CHECK-LABEL: define {{.*}}void @"$s4main10basicTestsyyF"()
+// CHECK: call i8* @{{_ZN10ClassesNS111BasicStruct11basicMemberEv|"\?basicMember@BasicStruct@ClassesNS1@@QEAAPEBDXZ"}}(%"struct.ClassesNS1::BasicStruct"*
+// CHECK: call i8* @{{_ZN10ClassesNS110ClassesNS211BasicStruct11basicMemberEv|"\?basicMember@BasicStruct@ClassesNS2@ClassesNS1@@QEAAPEBDXZ"}}(%"struct.ClassesNS1::ClassesNS2::BasicStruct"*
+// CHECK: call i8* @{{_ZN10ClassesNS311BasicStruct11basicMemberEv|"\?basicMember@BasicStruct@ClassesNS3@@QEAAPEBDXZ"}}(%"struct.ClassesNS3::BasicStruct"*
+// CHECK: ret void
+public func basicTests() {
+  var basicStructInst = ClassesNS1.BasicStruct()
+  basicStructInst.basicMember()
+
+  var nestedBasicStructInst = ClassesNS1.ClassesNS2.BasicStruct()
+  nestedBasicStructInst.basicMember()
+
+  var siblingBasicStruct = ClassesNS3.BasicStruct()
+  siblingBasicStruct.basicMember()
+}
+
+// CHECK-LABEL: define {{.*}}void @"$s4main15forwardDeclaredyyF"()
+// CHECK: call i8* @{{_ZN10ClassesNS121ForwardDeclaredStruct11basicMemberEv|"\?basicMember@ForwardDeclaredStruct@ClassesNS1@@QEAAPEBDXZ"}}(%"struct.ClassesNS1::ForwardDeclaredStruct"*
+// CHECK: call i8* @{{_ZN10ClassesNS110ClassesNS221ForwardDeclaredStruct11basicMemberEv|"\?basicMember@ForwardDeclaredStruct@ClassesNS2@ClassesNS1@@QEAAPEBDXZ"}}(%"struct.ClassesNS1::ClassesNS2::ForwardDeclaredStruct"*
+// CHECK: ret void
+public func forwardDeclared() {
+  var forwardDeclaredStruct = ClassesNS1.ForwardDeclaredStruct()
+  forwardDeclaredStruct.basicMember()
+
+  var nestedForwardDeclaredStruct = ClassesNS1.ClassesNS2.ForwardDeclaredStruct()
+  nestedForwardDeclaredStruct.basicMember()
+}
---
+++ test/Interop/Cxx/namespace/templates-second-header-module-interface.swift
@@ -0,0 +1,12 @@
+// RUN: %target-swift-ide-test -print-module -module-to-print=TemplatesSecondHeader -I %S/Inputs -source-filename=x -enable-cxx-interop | %FileCheck %s
+
+// CHECK: extension TemplatesNS1 {
+// CHECK-NOT: extension
+// CHECK:   static func basicFunctionTemplateDefinedInDefs<T>(_: T) -> UnsafePointer<CChar>!
+// CHECK:   struct __CxxTemplateInstN12TemplatesNS131BasicClassTemplateDefinedInDefsIcEE {
+// CHECK:     init()
+// CHECK:     mutating func basicMember() -> UnsafePointer<CChar>!
+// CHECK:   }
+// CHECK: }
+
+// CHECK: typealias BasicClassTemplateDefinedInDefsChar = TemplatesNS1.__CxxTemplateInstN12TemplatesNS131BasicClassTemplateDefinedInDefsIcEE
