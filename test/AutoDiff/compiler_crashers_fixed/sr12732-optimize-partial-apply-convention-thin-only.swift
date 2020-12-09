2cf7d63a5e0a47765d6ff1129a729fcf2bda29e1
Dan Zheng <danielzheng@google.com>
2020-12-08 19:48:31 -0500

    [AutoDiff] Fix ownership error in `VJPCloner::visitApplyInst`. (#35003)
    Change the following code pattern:
    
    ```
    %x1 = convert_function %x0 : $@differentiable (...) -> ...
    %x2 = begin_borrow %x1
    ... // use %x2
    %x3 = end_borrow %x2
    destroy_value %x0
    ```
    
    To the following:
    
    ```
    %x1 = begin_borrow %x0 : $@differentiable (...) -> ...
    %x2 = convert_function %x0
    ... // use %x2
    %x3 = end_borrow %x1
    destroy_value %x0
    ```
    
    Resolves SR-13933: "multiple consuming users" ownership error caused by
    `VJPCloner::visitApply` related to `@differentiable`-function-typed callees.
    
    Also upstream test/AutoDiff/SILOptimizer/generics.swift from tensorflow branch.

--- test/AutoDiff/compiler_crashers_fixed/sr12732-optimize-partial-apply-convention-thin-only.swift
+++ test/AutoDiff/compiler_crashers_fixed/sr12732-optimize-partial-apply-convention-thin-only.swift
@@ -6,6 +6,9 @@
 // Do not rewrite `partial_apply` to `thin_to_thick_function` if the specialized
 // callee is not `@convention(thin)`.
 
+// FIXME(SR-13021): Disabled due to flakiness on Linux, likely related to TF-1197.
+// REQUIRES: SR13021
+
 import DifferentiationUnittest
 
 func callback(_ x: inout Tracked<Float>.TangentVector) {}
