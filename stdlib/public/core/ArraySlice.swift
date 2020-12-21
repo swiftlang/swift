be13b470aae8d86163ed175cb476596d321f74f4
Matt Zanchelli <mattt@apple.com>
2020-12-20 22:33:10 -0500

    Fix typos
    becuase -> because
    preceeds -> precedes
    initalizer -> initializer
    intialize -> initialize
    libary -> library
    notfication -> notification
    reciever -> receiver
    collecton -> collection
    exlcusive -> exclusive
    techincal -> technical
    compatability -> compatibility
    setps -> steps
    accomodate -> accommodate
    brakcet -> bracket
    fraciton -> fraction
    programm -> program
    concequently -> consequently
    ecoding -> encoding
    timeIntervalforSelfEnd -> timeIntervalForSelfEnd

--- stdlib/public/core/ArraySlice.swift
+++ stdlib/public/core/ArraySlice.swift
@@ -867,7 +867,7 @@ extension ArraySlice: RangeReplaceableCollection {
   internal mutating func _reserveCapacityAssumingUniqueBuffer(oldCount: Int) {
     // Due to make_mutable hoisting the situation can arise where we hoist
     // _makeMutableAndUnique out of loop and use it to replace
-    // _makeUniqueAndReserveCapacityIfNotUnique that preceeds this call. If the
+    // _makeUniqueAndReserveCapacityIfNotUnique that precedes this call. If the
     // array was empty _makeMutableAndUnique does not replace the empty array
     // buffer by a unique buffer (it just replaces it by the empty array
     // singleton).
