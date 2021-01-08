swiftlang/swift

commit 5d6cf5cd96deab6f78263a09a5dd9e94cb63fe0b

Sema: Fix failure to diagnose throwing expressions inside string interpolations
We need to preserve the DiagnoseErrorOnTry bit stored in the Context when
exiting a ContextScope. Otherwise, we fail to produce a diagnostic if the
'try' expression pushes an intertwining Context, such as a string interpolation
or 'await'.

Fixes <https://bugs.swift.org/browse/SR-13654>, <rdar://problem/69958774>.

lib/Sema/TypeCheckEffects.cpp
@@ -878,6 +878,13 @@ private:
       HandlesErrors(handlesErrors), HandlesAsync(handlesAsync) { }
 
 public:
+  bool shouldDiagnoseErrorOnTry() const {
+    return DiagnoseErrorOnTry;
+  }
+  void setDiagnoseErrorOnTry(bool b) {
+    DiagnoseErrorOnTry = b;
+  }
+
   /// Whether this is a function that rethrows.
   bool isRethrows() const {
     if (!HandlesErrors)
@@ -1375,6 +1382,7 @@ class CheckEffectsCoverage : public EffectsHandlingWalker<CheckEffectsCoverage> 
     DeclContext *OldRethrowsDC;
     ContextFlags OldFlags;
     ThrowingKind OldMaxThrowingKind;
+
   public:
     ContextScope(CheckEffectsCoverage &self, Optional<Context> newContext)
       : Self(self), OldContext(self.CurContext),
@@ -1468,7 +1476,15 @@ class CheckEffectsCoverage : public EffectsHandlingWalker<CheckEffectsCoverage> 
     }
 
     ~ContextScope() {
+      // The "DiagnoseErrorOnTry" flag is a bit of mutable state
+      // in the Context itself, used to postpone diagnostic emission
+      // to a parent "try" expression. If something was diagnosed
+      // during this ContextScope, the flag may have been set, and
+      // we need to preseve its value when restoring the old Context.
+      bool DiagnoseErrorOnTry = Self.CurContext.shouldDiagnoseErrorOnTry();
       Self.CurContext = OldContext;
+      Self.CurContext.setDiagnoseErrorOnTry(DiagnoseErrorOnTry);
+
       Self.RethrowsDC = OldRethrowsDC;
       Self.Flags = OldFlags;
       Self.MaxThrowingKind = OldMaxThrowingKind;

test/stmt/errors.swift
@@ -251,3 +251,16 @@ func sr_11402_func2(_ x: SR_11402_P) {
     print(y)
   }
 }
+
+// https://bugs.swift.org/browse/SR-13654
+
+func sr_13654_func() throws -> String {}
+
+func sr_13654_invalid_interpolation() {
+  _ = try "\(sr_13654_func())" // expected-error {{errors thrown from here are not handled}}
+  _ = "\(try sr_13654_func())" // expected-error {{errors thrown from here are not handled}}
+}
+func sr_13654_valid_interpolation() throws {
+  _ = try "\(sr_13654_func())"
+  _ = "\(try sr_13654_func())"
+}

test/expr/unary/async_await.swift
@@ -133,3 +133,12 @@ func testStringInterpolation() async throws {
   _ = "Eventually produces \(await getInt())"
   _ = await "Eventually produces \(getInt())"
 }
+
+// Make sure try await works too
+func invalidAsyncFunction() async {
+  _ = try await throwingAndAsync() // expected-error {{errors thrown from here are not handled}}
+}
+
+func validAsyncFunction() async throws {
+  _ = try await throwingAndAsync()
+}

commit 99269ddfda2aed1023f44dbae3a96195100ac04d

Sema: Fix preservation of DiagnoseErrorOnTry flag
This fixes a regression from some over-eager logic introduced by my commit
5d6cf5cd96deab6f78263a09a5dd9e94cb63fe0b.

We need to be careful to preserve this flag only in cases where the
throwing-ness bubbles up from the nested ContextScope, namely AwaitExpr
and InterpolatedStringLiteralExpr.

Fixes <rdar://problem/72748150>.

lib/Sema/TypeCheckEffects.cpp
@@ -1579,10 +1579,22 @@ class CheckEffectsCoverage : public EffectsHandlingWalker<CheckEffectsCoverage> 
       OldMaxThrowingKind = std::max(OldMaxThrowingKind, Self.MaxThrowingKind);
     }
 
+    void preserveDiagnoseErrorOnTryFlag() {
+      // The "DiagnoseErrorOnTry" flag is a bit of mutable state
+      // in the Context itself, used to postpone diagnostic emission
+      // to a parent "try" expression. If something was diagnosed
+      // during this ContextScope, the flag may have been set, and
+      // we need to preseve its value when restoring the old Context.
+      bool DiagnoseErrorOnTry = Self.CurContext.shouldDiagnoseErrorOnTry();
+      OldContext.setDiagnoseErrorOnTry(DiagnoseErrorOnTry);
+    }
+
     void preserveCoverageFromAwaitOperand() {
       OldFlags.mergeFrom(ContextFlags::HasAnyAwait, Self.Flags);
       OldFlags.mergeFrom(ContextFlags::throwFlags(), Self.Flags);
       OldMaxThrowingKind = std::max(OldMaxThrowingKind, Self.MaxThrowingKind);
+
+      preserveDiagnoseErrorOnTryFlag();
     }
 
     void preserveCoverageFromTryOperand() {
@@ -1601,6 +1613,8 @@ class CheckEffectsCoverage : public EffectsHandlingWalker<CheckEffectsCoverage> 
       OldFlags.mergeFrom(ContextFlags::HasAnyAsyncSite, Self.Flags);
       OldFlags.mergeFrom(ContextFlags::HasAnyAwait, Self.Flags);
       OldMaxThrowingKind = std::max(OldMaxThrowingKind, Self.MaxThrowingKind);
+
+      preserveDiagnoseErrorOnTryFlag();
     }
     
     bool wasTopLevelDebuggerFunction() const {
@@ -1608,15 +1622,7 @@ class CheckEffectsCoverage : public EffectsHandlingWalker<CheckEffectsCoverage> 
     }
 
     ~ContextScope() {
-      // The "DiagnoseErrorOnTry" flag is a bit of mutable state
-      // in the Context itself, used to postpone diagnostic emission
-      // to a parent "try" expression. If something was diagnosed
-      // during this ContextScope, the flag may have been set, and
-      // we need to preseve its value when restoring the old Context.
-      bool DiagnoseErrorOnTry = Self.CurContext.shouldDiagnoseErrorOnTry();
       Self.CurContext = OldContext;
-      Self.CurContext.setDiagnoseErrorOnTry(DiagnoseErrorOnTry);
-
       Self.RethrowsDC = OldRethrowsDC;
       Self.Flags = OldFlags;
       Self.MaxThrowingKind = OldMaxThrowingKind;

test/Concurrency/async_tasks.swift
@@ -53,7 +53,7 @@ func test_unsafeContinuations() async {
   }
 }
 
-func test_unsafeThrowingContinuations() async {
+func test_unsafeThrowingContinuations() async throws {
   let _: String = await try withUnsafeThrowingContinuation { continuation in
     continuation.resume(returning: "")
   }

test/stmt/errors.swift
@@ -264,3 +264,10 @@ func sr_13654_valid_interpolation() throws {
   _ = try "\(sr_13654_func())"
   _ = "\(try sr_13654_func())"
 }
+
+// rdar://problem/72748150
+func takesClosure(_: (() -> ())) throws -> Int {}
+
+func passesClosure() {
+    _ = try takesClosure { } // expected-error {{errors thrown from here are not handled}}
+}

test/stmt/errors_async.swift
@@ -1,0 +1,14 @@
+// RUN: %target-swift-frontend -typecheck -verify %s -enable-experimental-concurrency
+
+// REQUIRES: concurrency
+
+enum MyError : Error {
+  case bad
+}
+
+func shouldThrow() async {
+  // expected-error@+1 {{errors thrown from here are not handled}}
+  let _: Int = await try withUnsafeThrowingContinuation { continuation in
+    continuation.resume(throwing: MyError.bad)
+  }
+}