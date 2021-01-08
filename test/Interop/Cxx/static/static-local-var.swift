9ba7bf78db72f02c126f6868d363911acbed43ad
Saleem Abdulrasool <compnerd@compnerd.org>
2021-01-08 10:08:03 -0800

    Interop: use new `target-clangxx` for building C++ code
    This enables control over the C++ flags used during testing for the C++
    interop from a single location rather than having to alter all the
    tests.

--- test/Interop/Cxx/static/static-local-var.swift
+++ test/Interop/Cxx/static/static-local-var.swift
@@ -1,5 +1,5 @@
 // RUN: %empty-directory(%t)
-// RUN: %target-clang -c %S/Inputs/static-local-var.cpp -I %S/Inputs -o %t/static-local-var.o
+// RUN: %target-clangxx -c %S/Inputs/static-local-var.cpp -I %S/Inputs -o %t/static-local-var.o
 // RUN: %target-build-swift %s -I %S/Inputs -o %t/statics %t/static-local-var.o -Xfrontend -enable-cxx-interop
 // RUN: %target-codesign %t/statics
 // RUN: %target-run %t/statics
