c5b7e7fb9c2fe0694200015db87395617ae26633
martinboehme <mboehme@google.com>
2021-01-11 14:25:41 +0100

    Fix two issues with the SwiftGlibc module map (#32404)
    * Fix two issues with the SwiftGlibc module map.
    
    The issues are:
    
    - Today, some submodules in `SwiftGlibc` fail to provide definitions that
      they should contain. As a consequence, Swift fails to import some code
      that compiles correctly with standalone Clang. As just one example,
      including `signal.h` should make the type `pid_t` available, but it
      currently does not.
    
    - `SwiftGlibc` is not compatible with the libc++ module map. Trying to
      include libc++ headers in a C++ module imported into Swift results in an
      error message about cyclic dependencies.
    
    This change fixes both of these issues by making it so that `SwiftGlibc`
    no actually longer defines a module map for the glibc headers but merely makes
    all of the symbols from those headers available in a module that can be
    imported into Swift. C / Objective-C / C++ code, on the other hand, will now
    include the glibc headers texually.
    
    For more context on the two issues and this fix, see this forum
    discussion:
    
    https://forums.swift.org/t/problems-with-swiftglibc-and-proposed-fix/37594
    
    This change only modifies `glibc.modulemap.gyb` for the time being but
    leaves `bionic.modulemap.gyb` and `libc-openbsd.modulemap.gyb` unchanged. The
    intent is to fix these in the same way, but it will be easier to do this
    in separate PRs that can be tested individually.
    
    Co-authored-by: zoecarver <z.zoelec2@gmail.com>
    Co-authored-by: Marcel Hlopko <hlopko@google.com>

---
+++ test/Interop/C/libc/include-signal.swift
@@ -0,0 +1,33 @@
+// RUN: %target-typecheck-verify-swift -I %S/Inputs
+
+// Some types such as pid_t that glibc defines are defined using the following
+// construct:
+//
+// #ifndef __pid_t_defined
+// typedef __pid_t pid_t;
+// #define __pid_t_defined
+// #endif
+//
+// glibc defines pid_t in this way in multiple header files, as required by
+// POSIX (e.g. signal.h, unistd.h, and sys/types.h). A number of other types
+// use the same construct (e.g. ssize_t, intptr_t, uid_t, gid_t).
+//
+// Because Swift does not set the -fmodules-local-submodule-visibility flag,
+// the __pid_t_defined macro can leak from the first submodule that defines it
+// into all submodules that follow.
+//
+// As a consequence, a C header file may fail to compile when imported into
+// Swift, even though it compiles cleanly outside Swift with the same system
+// headers.
+//
+// This is a regression test for a bug that was once present in SwiftGlibc
+// module layout. Originally SwiftGlibc defined one submodule per header, and
+// types such as pid_t were defined by the first module that encountered the
+// ifndef construct.
+//
+// See https://forums.swift.org/t/problems-with-swiftglibc-and-proposed-fix/37594
+// for further details.
+
+// REQUIRES: OS=linux-gnu
+
+import IncludeSignal
