f6637276fd9bd96fad02f429f73d45e6da47804d
Pavel Yaskevich <pyaskevich@apple.com>
2021-01-12 14:02:33 -0800

    [ConstraintSystem] Increase score only if members found on `Optional` and its unwrapped type
    Unresolved member lookup is allowed to perform implicit optional
    unwrap of a base type to find members. Previously if there were
    any members directly on `Optional`, lookup would stop there. But
    since SR-13815 it became possible for solver to attempt members
    found on unwrapped type even if there are viable ones on
    `Optional` as well.
    
    New score kind has been introduced to guard against possible ambiguities
    with new scheme - `SK_UnresolvedMemberViaOptional`. It's used very
    time member found via base type unwrap is attempted. Unfortunately,
    doing so can lead to behavior changes in existing code because it's
    possible that base was wrapped into optional implicitly based on
    context e.g. unresolved member passed in as an argument to a parameter
    of optional type.
    
    To fix situations like that, `SK_UnresolvedMemberViaOptional` should
    only be increased if there is a mix of members to attempt - both directly
    on `Optional` and on unwrapped type, in all other cases score should stay
    the same because there could be no ambiguity.
    
    Resolves: rdar://73027153

--- lib/Sema/ConstraintSystem.cpp
+++ lib/Sema/ConstraintSystem.cpp
@@ -2799,8 +2799,7 @@ void ConstraintSystem::resolveOverload(ConstraintLocator *locator,
     increaseScore(SK_DisfavoredOverload);
   }
 
-  if (choice.getKind() == OverloadChoiceKind::DeclViaUnwrappedOptional &&
-      locator->isLastElement<LocatorPathElt::UnresolvedMember>()) {
+  if (choice.isFallbackMemberOnUnwrappedBase()) {
     increaseScore(SK_UnresolvedMemberViaOptional);
   }
 }
