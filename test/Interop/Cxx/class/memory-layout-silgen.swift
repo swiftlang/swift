255eac0264f6e1f2ff2f63d58d7642ddbef7bac3
Saleem Abdulrasool <compnerd@compnerd.org>
2021-01-12 11:43:53 -0800

    test: introduce new `target-swiftxx-frontend` for C++ interop
    Add an convert to the new `target-swiftxx-frontend` substitution in lit
    to control the C++ interop enabling in Swift.  This allows for a single
    site which will enable control of both an overridden standard (for
    testing multiple C++ standards) and simplify writing tests.

--- test/Interop/Cxx/class/memory-layout-silgen.swift
+++ test/Interop/Cxx/class/memory-layout-silgen.swift
@@ -1,4 +1,4 @@
-// RUN: %target-swift-frontend -I %S/Inputs -enable-cxx-interop -emit-ir -o - %s | %FileCheck %s
+// RUN: %target-swiftxx-frontend -I %S/Inputs -emit-ir -o - %s | %FileCheck %s
 
 import MemoryLayout
 
