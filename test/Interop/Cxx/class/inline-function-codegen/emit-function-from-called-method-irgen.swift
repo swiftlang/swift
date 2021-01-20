3ff1362b744999d1efbf85065cca47d894034f60
Rosica Dejanovska <rosica@google.com>
2021-01-20 13:43:53 +0100

    Remove wrongly named tests

--- test/Interop/Cxx/class/inline-function-codegen/emit-function-from-called-method-irgen.swift
+++
@@ -1,9 +0,0 @@
-// RUN: %target-swift-emit-ir %s -I %S/Inputs -enable-cxx-interop | %FileCheck %s
-
-import MethodCallsFunction
-
-public func getValueFromMethod() -> CInt {
-  return callMethod(41)
-}
-
-// CHECK: define linkonce_odr{{( dso_local)?}} i32 @{{_Z9incrementi|"\?increment@@YAHH@Z"}}(i32 %t)
