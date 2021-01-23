8a5810712351f797cc729b5555b89901464e3c09
Rintaro Ishizaki <rishizaki@apple.com>
2021-01-22 17:08:54 -0800

    [Parse] Diagnose default argument for subscript in protocols
    Protocol requirements don't support default arguments. Although this is
    a "semantic" diagnostics, we currently do this for 'func' and 'init' in
    Parser. So for fixing a crash, let's to it for 'subscript' in Parser
    too.
    
    rdar://problem/73159041

--- test/decl/protocol/protocol_with_default_args.swift
+++ test/decl/protocol/protocol_with_default_args.swift
@@ -11,4 +11,5 @@ struct X : P {
 
 protocol Q {
   init(truth: Bool = false) // expected-error{{default argument not permitted in a protocol initializer}}
+  subscript(x: Int, default: Int = 0) -> Self { get } // expected-error {{default argument not permitted in a protocol subscript}}
 }
