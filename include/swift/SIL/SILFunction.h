2f890dcbbfd26e95f0720e08aee50d23a9314cb5
Erik Eckstein <eeckstein@apple.com>
2021-01-25 13:11:33 +0100

    SIL: some improvements to the BasicBlockBitfield utilities
    * add a BasicBlockSetVector class
    * add a second argument to BasicBlockFlag::set, for the set value.
    * rename BasicBlockSet::remove -> BasicBlockSet::erase.
    * add a MaxBitfieldID statistics value in SILFunction.cpp

--- include/swift/SIL/SILFunction.h
+++ include/swift/SIL/SILFunction.h
@@ -199,7 +199,7 @@ private:
 
   /// A monotonically increasing ID which is incremented whenever a
   /// BasicBlockBitfield is constructed.
-  /// Usually this stays below 1000, so a 32-bit unsigned is more than
+  /// Usually this stays below 100000, so a 32-bit unsigned is more than
   /// sufficient.
   /// For details see BasicBlockBitfield::bitfieldID;
   unsigned currentBitfieldID = 1;
