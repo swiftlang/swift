435a7964d07eab15449be8cb8552fc600e894d44
Marcel Hlopko <hlopko@bazel.build>
2021-01-26 13:45:05 +0100

    Update test/Interop/Cxx/templates/class-template-instantiation-existing-specialization.swift
    Co-authored-by: Dmitri Gribenko <gribozavr@gmail.com>

--- test/Interop/Cxx/templates/class-template-instantiation-existing-specialization.swift
+++ test/Interop/Cxx/templates/class-template-instantiation-existing-specialization.swift
@@ -10,7 +10,7 @@ var TemplatesTestSuite = TestSuite("TemplatesTestSuite")
 
 // This test covers the case where Clang has to create specialization eagerly
 // because the code in the header forces it to. Swift has to reuse this
-// specialization, it must not create it's own.
+// specialization, it must not create its own.
 TemplatesTestSuite.test("existing-specialization") {
   let myInt = IntWrapper(value: 18)
   var magicInt = MagicWrapper<IntWrapper>(t: myInt)
