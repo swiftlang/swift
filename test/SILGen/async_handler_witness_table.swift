921cc5fe633099579f435c6da454298232514bff
Doug Gregor <dgregor@apple.com>
2021-02-01 22:46:49 -0800

    Fix a stray FileCheck in new test

--- test/SILGen/async_handler_witness_table.swift
+++ test/SILGen/async_handler_witness_table.swift
@@ -1,6 +1,5 @@
 // RUN: %target-swift-emit-silgen -sdk %S/Inputs -I %S/Inputs -enable-source-import %s -enable-experimental-concurrency > %t.out
 // RUN: %FileCheck -check-prefix=CHECK -check-prefix=CHECK-%target-ptrsize %s < %t.out
-// RUN: %FileCheck -check-prefix=NEGATIVE %s < %t.out
 
 // REQUIRES: objc_interop
 // REQUIRES: concurrency
