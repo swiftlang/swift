28d3491d3d356a4cafdc5f6de8c683df77dea33a
Slava Pestov <spestov@apple.com>
2021-02-06 22:06:12 -0500

    Sema: Reject non-stored local variables in result builders
    We would previously reject properties without an initializer expression,
    but this is not quite sufficient, because of 'lazy', property wrappers,
    and observers.
    
    We could probably support all three in result builder contexts, but
    for now, let's diagnose them like other computed properties, instead of
    crashing.
    
    Fixes part of <rdar://problem/73545981>.

---
+++ test/Constraints/result_builder_invalid_vars.swift
@@ -0,0 +1,45 @@
+// RUN: %target-typecheck-verify-swift
+
+@resultBuilder
+struct DummyBuilder { // expected-note 5 {{struct 'DummyBuilder' declared here}}
+  static func buildBlock<T>(_ t: T) -> T {
+    return t
+  }
+}
+
+func dummy<T>(@DummyBuilder _: () -> T) {}
+
+dummy {
+  var computedVar: Int { return 123 } // expected-error {{closure containing a declaration cannot be used with result builder 'DummyBuilder'}}
+  ()
+}
+
+dummy {
+  lazy var lazyVar: Int = 123 // expected-error {{closure containing a declaration cannot be used with result builder 'DummyBuilder'}}
+  ()
+}
+
+dummy {
+  var observedVar: Int = 123 { // expected-error {{closure containing a declaration cannot be used with result builder 'DummyBuilder'}}
+    didSet {}
+  }
+
+  ()
+}
+
+dummy {
+  var observedVar: Int = 123 { // expected-error {{closure containing a declaration cannot be used with result builder 'DummyBuilder'}}
+    willSet {}
+  }
+
+  ()
+}
+
+@propertyWrapper struct Wrapper {
+  var wrappedValue: Int
+}
+
+dummy {
+  @Wrapper var wrappedVar: Int = 123 // expected-error {{closure containing a declaration cannot be used with result builder 'DummyBuilder'}}
+  ()
+}
