8578584f6126ed31df944d50aa49e29579ca93c9
Philippe Hausler <phausler@apple.com>
2021-02-12 14:43:59 -0800

    Implementations for operators and async functions on AsyncSequence per SE-0298 (#35740)
    * Implementations for operators and async functions on AsyncSequence per SE-0298
    
    * Add trailing new lines to AsyncSequence adopter files
    
    * Remove extra // from preambles
    
    * Transition sequence properties and sequence initializers to internal
    
    * Rename Upstream to Base
    
    * Rename "Failable" asynchronous sequences to "Throwing"
    
    * Make iteration consistent with closure lifetimes and track failure via state
    
    * Use for await syntax for async function extensions on AsyncSequence
    
    * Work-around rethrows calculations being incorrect for conformance + closure rethrowing
    
    * Update testing names to reflect Throwing versus Failable
    
    * Incorperate runAsyncAndBlock into the test function for suite calls
    
    * Remove @frozen from AsyncSequence operators
    
    * Back out reduce family functions for now due to IRGen bug
    
    * Additional corrections for throwing cases and fix up potential linux crasher
    
    * Add a upstream and throwing tests and correct flatMap's throwing behavior
    
    * Unify parameter names to count
    
    * Add a more helpful precondition on prefix
    
    * Add tests for throwing cases of non closure async functions on AsyncSequence
    
    * Correct missing count transition in dropFirst
    
    * Correct missing , in assertion
    
    * [NFC] add commentary on optimization in dropFirst on dropFirst
    
    * Disable collect tests for non macOS platforms for now
    
    * Disable all async sequence tests for non macOS platforms for now

---
+++ stdlib/public/Concurrency/AsyncDropWhileSequence.swift
@@ -0,0 +1,80 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2021 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+import Swift
+
+extension AsyncSequence {
+  @inlinable
+  public __consuming func drop(
+    while predicate: @escaping (Element) async -> Bool
+  ) -> AsyncDropWhileSequence<Self> {
+    AsyncDropWhileSequence(self, predicate: predicate)
+  }
+}
+
+public struct AsyncDropWhileSequence<Base: AsyncSequence> {
+  @usableFromInline
+  let base: Base
+
+  @usableFromInline
+  let predicate: (Base.Element) async -> Bool
+
+  @usableFromInline
+  init(
+    _ base: Base, 
+    predicate: @escaping (Base.Element) async -> Bool
+  ) {
+    self.base = base
+    self.predicate = predicate
+  }
+}
+
+extension AsyncDropWhileSequence: AsyncSequence {
+  public typealias Element = Base.Element
+  public typealias AsyncIterator = Iterator
+
+  public struct Iterator: AsyncIteratorProtocol {
+    @usableFromInline
+    var baseIterator: Base.AsyncIterator
+
+    @usableFromInline
+    var predicate: ((Base.Element) async -> Bool)?
+
+    @usableFromInline
+    init(
+      _ baseIterator: Base.AsyncIterator, 
+      predicate: @escaping (Base.Element) async -> Bool
+    ) {
+      self.baseIterator = baseIterator
+      self.predicate = predicate
+    }
+
+    @inlinable
+    public mutating func next() async rethrows -> Base.Element? {
+      while let predicate = self.predicate {
+        guard let element = try await baseIterator.next() else {
+          return nil
+        }
+        if await predicate(element) == false {
+          self.predicate = nil
+          return element
+        }
+      }
+      return try await baseIterator.next()
+    }
+  }
+
+  @inlinable
+  public __consuming func makeAsyncIterator() -> Iterator {
+    return Iterator(base.makeAsyncIterator(), predicate: predicate)
+  }
+}
---
+++ stdlib/public/Concurrency/AsyncMapSequence.swift
@@ -0,0 +1,74 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2021 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+import Swift
+
+extension AsyncSequence {
+  @inlinable
+  public __consuming func map<Transformed>(
+    _ transform: @escaping (Element) async -> Transformed
+  ) -> AsyncMapSequence<Self, Transformed> {
+    return AsyncMapSequence(self, transform: transform)
+  }
+}
+
+public struct AsyncMapSequence<Base: AsyncSequence, Transformed> {
+  @usableFromInline
+  let base: Base
+
+  @usableFromInline
+  let transform: (Base.Element) async -> Transformed
+
+  @usableFromInline
+  init(
+    _ base: Base, 
+    transform: @escaping (Base.Element) async -> Transformed
+  ) {
+    self.base = base
+    self.transform = transform
+  }
+}
+
+extension AsyncMapSequence: AsyncSequence {
+  public typealias Element = Transformed
+  public typealias AsyncIterator = Iterator
+
+  public struct Iterator: AsyncIteratorProtocol {
+    @usableFromInline
+    var baseIterator: Base.AsyncIterator
+
+    @usableFromInline
+    let transform: (Base.Element) async -> Transformed
+
+    @usableFromInline
+    init(
+      _ baseIterator: Base.AsyncIterator, 
+      transform: @escaping (Base.Element) async -> Transformed
+    ) {
+      self.baseIterator = baseIterator
+      self.transform = transform
+    }
+
+    @inlinable
+    public mutating func next() async rethrows -> Transformed? {
+      guard let element = try await baseIterator.next() else {
+        return nil
+      }
+      return await transform(element)
+    }
+  }
+
+  @inlinable
+  public __consuming func makeAsyncIterator() -> Iterator {
+    return Iterator(base.makeAsyncIterator(), transform: transform)
+  }
+}
