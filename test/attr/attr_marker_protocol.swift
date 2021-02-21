87ac6ce6a310835a06296962766d7a957c1b3ed2
Doug Gregor <dgregor@apple.com>
2021-02-20 21:07:36 -0800

    Ban use of marker protocols in conditional conformances to non-marker protocols
    These conditional conformances could not be properly evaluated at
    runtime, so ban them. We still allow conditional conformances to
    marker protocols.

--- test/attr/attr_marker_protocol.swift
+++ test/attr/attr_marker_protocol.swift
@@ -58,3 +58,8 @@ protocol P9: P8 { }
 // Implied conditional conformance to P8 is okay because P8 is a marker
 // protocol.
 extension Array: P9 where Element: P9 { }
+
+protocol P10 { }
+
+extension Array: P10 where Element: P10, Element: P8 { }
+// expected-error@-1{{conditional conformance to non-marker protocol 'P10' cannot depend on conformance of 'Element' to non-marker protocol 'P8'}}
