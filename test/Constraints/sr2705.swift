1c861330417ccde6bdcc9bfa1bd9a1d307fb1194
Luciano Almeida <passos.luciano@outlook.com>
2021-02-23 00:49:56 -0300

    [tests] Add regression tests for SR-2705

---
+++ test/Constraints/sr2705.swift
@@ -0,0 +1,28 @@
+// RUN: %target-typecheck-verify-swift
+
+func f<T>(_: () -> T) {}
+func f<T>(_: @autoclosure () -> T) {}
+
+f { } // OK
+
+func f1<T>(_: () -> T, _: () -> T) {}
+func f1<T>(_: @autoclosure () -> T, _: @autoclosure () -> T) {}
+
+f1({}, {}) // OK
+
+func f2<T>(_: () -> T, _: () -> T) { }
+func f2<T>(_: () -> T, _: @autoclosure () -> T) { }
+
+f2({}, {}) // OK
+
+func f3(_: () -> Int) {}
+func f3(_: @autoclosure () -> Int) {}
+
+f3 { 0 } // OK
+ 
+func autoclosure(f: () -> Int) { }
+func autoclosure(f: @autoclosure () -> Int) { }
+func autoclosure(f: Int) { }
+
+autoclosure(f: { 0 }) // OK
+let _ = autoclosure as (() -> (Int)) -> () // OK
