ea5bb777e269170d54c56412f6844bc1920d2a22
Julian Lettner <julian.lettner@apple.com>
2021-03-01 16:08:34 -0800

    Fix TSan symbol lookup on Windows

--- stdlib/public/Concurrency/ThreadSanitizer.cpp
+++ stdlib/public/Concurrency/ThreadSanitizer.cpp
@@ -16,19 +16,34 @@
 
 #include "TaskPrivate.h"
 
+#if defined(_WIN32)
+#define NOMINMAX
+#include <windows.h>
+#else
 #include <dlfcn.h>
+#endif
 
 using TSanFunc = void(void *);
 
+namespace {
+static TSanFunc *loadSymbol(const char *name) {
+#if defined(_WIN32)
+  return (TSanFunc *)GetProcAddress(GetModuleHandle(NULL), name);
+#else
+  return (TSanFunc *)dlsym(RTLD_DEFAULT, name);
+#endif
+}
+}
+
 void swift::_swift_tsan_acquire(void *addr) {
-  static auto ptr = (TSanFunc *)dlsym(RTLD_DEFAULT, "__tsan_acquire");
+  static auto ptr = loadSymbol("__tsan_acquire");
   if (ptr) {
     ptr(addr);
   }
 }
 
 void swift::_swift_tsan_release(void *addr) {
-  static auto ptr = (TSanFunc *)dlsym(RTLD_DEFAULT, "__tsan_release");
+  static auto ptr = loadSymbol("__tsan_release");
   if (ptr) {
     ptr(addr);
   }
