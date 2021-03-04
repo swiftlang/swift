07275a39a7ad161d6d12c27eb21001b319d9916f
Julian Lettner <julian.lettner@apple.com>
2021-03-04 13:51:32 -0800

    Teach TSan about Swift Task and Actor model
    I have identified the following conceptual synchronization points at
    which task data and computation can cross thread boundaries.  We need to
    model these in TSan to avoid false positives:
    
    Awaiting an async task (`AsyncTask::waitFuture`), which has two cases:
    1) The task has completed (`AsyncTask::completeFuture`).  Everything
       that happened during task execution "happened before" before the
       point where we access its result.  We synchronize on the *awaited*
       task.
    2) The task is still executing: the current execution is suspended and
       the waiting task is put into the list of "waiters".  Once the awaited
       task completes, the waiters will be scheduled.  In this case, we
       synchronize on the *waiting* task.
    
    Note: there is a similar relationship for task groups which I still have
    to investigate.  I will follow-up with an additional patch and tests.
    
    Actor job execution (`swift::runJobInExecutorContext`): Job scheduling
    (`swift::swift_task_enqueue`) precedes/happens before job execution.
    Also all job executions (switching actors or suspend/resume) are
    serially ordered.
    
    Note: the happens-before edge for schedule->execute isn't strictly
    needed in most cases since scheduling calls through to libdispatch's
    `dispatch_async_f`, which we already intercept and model in TSan.
    However, I am trying to model Swift Task semantics to increase the
    chance of things to continue to work in case the "task backend" is
    switched out.
    
    Radar-Id: rdar://74256733

--- stdlib/public/Concurrency/TaskHooks.cpp
+++ stdlib/public/Concurrency/TaskHooks.cpp
@@ -67,4 +67,61 @@ void swift_task_set_hooks(const TaskHooks *hooks, size_t structSize) {
   memcpy(&tmp, hooks, structSize);
   hookList.push_back(tmp);
 }
+
+// All of the below and ThreadSanitizer.cpp will go away.
+// Setting the hooks will be moved to TSan runtime.
+namespace {
+void tsan_cb_enqueue(Job *job, ExecutorRef executor) {
+  _swift_tsan_release(job);
+}
+
+void tsan_cb_runJobBegin(Job *job, ExecutorRef executor) {
+  _swift_tsan_acquire(job);
+}
+
+void tsan_cb_runJobEnd(Job *job, ExecutorRef executor) {
+  _swift_tsan_release(job);
+}
+
+void tsan_cb_completeFuture(AsyncTask *task) {
+  _swift_tsan_release(static_cast<Job *>(task));
+}
+
+void tsan_cb_completeFutureWaiter(AsyncTask *waitingTask,
+                                  AsyncTask *waitedOnTask) {
+  _swift_tsan_acquire(static_cast<Job *>(waitingTask));
+}
+
+void tsan_cb_waitFuture(AsyncTask *waitingTask, AsyncTask *waitedOnTask,
+                        AsyncTask::FutureFragment::Status status) {
+  using Status = AsyncTask::FutureFragment::Status;
+  switch (status) {
+  case Status::Error:
+  case Status::Success:
+    _swift_tsan_acquire(static_cast<Job *>(waitedOnTask));
+    break;
+  case Status::Executing:
+    _swift_tsan_release(static_cast<Job *>(waitingTask));
+    break;
+  }
+}
+
+bool initTsan() {
+  TaskHooks tsanHooks = {
+      .hook_enqueueGlobal = nullptr,
+      .cb_enqueue = tsan_cb_enqueue,
+      .cb_runJobBegin = tsan_cb_runJobBegin,
+      .cb_runJobEnd = tsan_cb_runJobEnd,
+      .cb_completeFuture = tsan_cb_completeFuture,
+      .cb_completeFutureWaiter = tsan_cb_completeFutureWaiter,
+      .cb_waitFuture = tsan_cb_waitFuture,
+  };
+  swift_task_set_hooks(&tsanHooks, sizeof(TaskHooks));
+  return true;
+}
+
+// Note: global initialization order *within* a tranlsation unit is
+// well-defined, i.e., source order.
+bool initialized = initTsan();
+} // anonymous namespace
 } // namespace swift
