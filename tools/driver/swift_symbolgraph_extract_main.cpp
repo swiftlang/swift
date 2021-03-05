1a9e9501e80b729293eaf00839ae30ff35c90a2a
Victoria Mitchell <victoria_m@apple.com>
2021-03-04 19:39:22 -0700

    don't scan every available module for cross-import overlays
    rdar://74928853

--- tools/driver/swift_symbolgraph_extract_main.cpp
+++ tools/driver/swift_symbolgraph_extract_main.cpp
@@ -260,16 +260,16 @@ int swift_symbolgraph_extract_main(ArrayRef<const char *> Args, const char *Argv
   // don't need to print these errors.
   CI.removeDiagnosticConsumer(&DiagPrinter);
   
-  for (const auto &ModuleName : VisibleModuleNames) {
-    if (ModuleName.str().startswith("_")) {
-      auto CIM = CI.getASTContext().getModuleByName(ModuleName.str());
-      if (CIM && CIM->isCrossImportOverlayOf(M)) {
-        const auto &CIMainFile = CIM->getMainFile(FileUnitKind::SerializedAST);
-        llvm::errs() << "Emitting symbol graph for cross-import overlay module file: "
-          << CIMainFile.getModuleDefiningPath() << '\n';
-        
-        Success |= symbolgraphgen::emitSymbolGraphForModule(CIM, Options);
-      }
+  SmallVector<ModuleDecl *, 2> Overlays;
+  M->findDeclaredCrossImportOverlaysTransitive(Overlays);
+  for (const auto *OM : Overlays) {
+    auto CIM = CI.getASTContext().getModuleByName(OM->getNameStr());
+    if (CIM) {
+      const auto &CIMainFile = CIM->getMainFile(FileUnitKind::SerializedAST);
+      llvm::errs() << "Emitting symbol graph for cross-import overlay module file: "
+        << CIMainFile.getModuleDefiningPath() << '\n';
+      
+      Success |= symbolgraphgen::emitSymbolGraphForModule(CIM, Options);
     }
   }
 
