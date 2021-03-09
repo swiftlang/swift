7c4cab355c5791b2b7909bb6a383567a97af3acd
Kavon Farvardin <kfarvardin@apple.com>
2021-03-08 16:09:41 -0800

    adding one XFAIL'ed task & actor lifetime test
    The `executor_deinit1` test fails 100% of the time
    (from what I've seen) so I thought we could track
    and see when/if someone happens to fix this bug.
    
    Also, added extra coverage for #36298 via `executor_deinit2`

---
+++ test/Concurrency/Runtime/executor_deinit1.swift
@@ -0,0 +1,32 @@
+// RUN: %target-run-simple-swift(-parse-as-library -Xfrontend -enable-experimental-concurrency %import-libdispatch) | %FileCheck %s
+
+// REQUIRES: executable_test
+// REQUIRES: concurrency
+// REQUIRES: libdispatch
+
+// FIXME: this should pass! from rdar://73266050
+// XFAIL: *
+
+// doesn't matter that it's bool identity function or not
+func boolIdentityFn(_ x : Bool) -> Bool { return x }
+
+actor FirstActor {
+    func startTest() { // whether startTest is async or sync doesn't matter
+
+        // do not remove this call or if-statement.
+        if boolIdentityFn(true) {}
+
+    }
+
+    deinit() {
+        // CHECK: called deinit
+        print("called deinit")
+    }
+}
+
+@main struct RunIt {
+    static func main() async {
+        let actor = FirstActor()
+        await actor.startTest() // do not remove this call
+    }
+}
