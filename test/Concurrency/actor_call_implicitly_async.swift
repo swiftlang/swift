f3be40e7372491f65ec900dca73e95d59eb7ba87
Kavon Farvardin <kfarvardin@apple.com>
2021-03-18 17:47:14 -0700

    [test] eff. prop + actor isolation

--- test/Concurrency/actor_call_implicitly_async.swift
+++ test/Concurrency/actor_call_implicitly_async.swift
@@ -1,6 +1,22 @@
 // RUN: %target-typecheck-verify-swift -enable-experimental-concurrency -warn-concurrency
 // REQUIRES: concurrency
 
+
+// some utilities
+func thrower() throws {}
+func asyncer() async {}
+
+func rethrower(_ f : @autoclosure () throws -> Any) rethrows -> Any {
+  return try f()
+}
+
+func asAutoclosure(_ f : @autoclosure () -> Any) -> Any { return f() }
+
+// not a concurrency-safe type
+class Box {
+  var counter : Int = 0
+}
+
 actor BankAccount {
 
   private var curBalance : Int
@@ -61,6 +77,73 @@ actor BankAccount {
     _ = deposit(withdraw(deposit(withdraw(balance()))))
   }
 
+  func testThrowing() throws {}
+
+  var effPropA : Box {
+    get async {
+      await asyncer()
+      return Box()
+    }
+  }
+
+  var effPropT : Box {
+    get throws {
+      try thrower()
+      return Box()
+    }
+  }
+
+  var effPropAT : Int {
+    get async throws {
+      await asyncer()
+      try thrower()
+      return 0
+    }
+  }
+
+  // expected-note@+1 2 {{add 'async' to function 'effPropertiesFromInsideActorInstance()' to make it asynchronous}}
+  func effPropertiesFromInsideActorInstance() throws {
+    // expected-error@+1{{'async' property access in a function that does not support concurrency}}
+    _ = effPropA
+
+    // expected-note@+4{{did you mean to handle error as optional value?}}
+    // expected-note@+3{{did you mean to use 'try'?}}
+    // expected-note@+2{{did you mean to disable error propagation?}}
+    // expected-error@+1{{property access can throw but is not marked with 'try'}}
+    _ = effPropT
+
+    _ = try effPropT
+
+    // expected-note@+6 {{did you mean to handle error as optional value?}}
+    // expected-note@+5 {{did you mean to use 'try'?}}
+    // expected-note@+4 {{did you mean to disable error propagation?}}
+    // expected-error@+3 {{property access can throw but is not marked with 'try'}}
+    // expected-note@+2 {{call is to 'rethrows' function, but argument function can throw}}
+    // expected-error@+1 {{call can throw but is not marked with 'try'}}
+    _ = rethrower(effPropT)
+
+    // expected-note@+2 {{call is to 'rethrows' function, but argument function can throw}}
+    // expected-error@+1 {{call can throw but is not marked with 'try'}}
+    _ = rethrower(try effPropT)
+
+    _ = try rethrower(effPropT)
+    _ = try rethrower(thrower())
+
+    _ = try rethrower(try effPropT)
+    _ = try rethrower(try thrower())
+
+    _ = rethrower(effPropA) // expected-error{{'async' property access in an autoclosure that does not support concurrency}}
+
+    _ = asAutoclosure(effPropT) // expected-error{{property access can throw, but it is not marked with 'try' and it is executed in a non-throwing autoclosure}}
+
+    // expected-note@+5{{did you mean to handle error as optional value?}}
+    // expected-note@+4{{did you mean to use 'try'?}}
+    // expected-note@+3{{did you mean to disable error propagation?}}
+    // expected-error@+2{{property access can throw but is not marked with 'try'}}
+    // expected-error@+1{{'async' property access in a function that does not support concurrency}}
+    _ = effPropAT
+  }
+
 } // end actor
 
 func someAsyncFunc() async {
@@ -87,6 +170,29 @@ func someAsyncFunc() async {
   _ = await a.deposit(b.withdraw(a.deposit(b.withdraw(b.balance()))))
 
   a.testSelfBalance() // expected-error {{call is 'async' but is not marked with 'await'}}
+
+  await a.testThrowing() // expected-error {{call can throw, but it is not marked with 'try' and the error is not handled}}
+
+  ////////////
+  // effectful properties from outside the actor instance
+
+  // expected-warning@+2 {{cannot use property 'effPropA' with a non-sendable type 'Box' across actors}}
+  // expected-error@+1{{property access is 'async' but is not marked with 'await'}} {{7-7=await }}
+  _ = a.effPropA
+
+  // expected-warning@+3 {{cannot use property 'effPropT' with a non-sendable type 'Box' across actors}}
+  // expected-error@+2{{property access can throw, but it is not marked with 'try' and the error is not handled}}
+  // expected-error@+1{{property access is 'async' but is not marked with 'await'}} {{7-7=await }}
+  _ = a.effPropT
+
+  // expected-error@+2{{property access can throw, but it is not marked with 'try' and the error is not handled}}
+    // expected-error@+1{{property access is 'async' but is not marked with 'await'}} {{7-7=await }}
+  _ = a.effPropAT
+
+  // (mostly) corrected ones
+  _ = await a.effPropA  // expected-warning {{cannot use property 'effPropA' with a non-sendable type 'Box' across actors}}
+  _ = try! await a.effPropT // expected-warning {{cannot use property 'effPropT' with a non-sendable type 'Box' across actors}}
+  _ = try? await a.effPropAT
 
   print("ok!")
 }
@@ -254,4 +360,200 @@ actor Calculator {
   let plusOne = await calc.addCurried(await calc.add(0, 1))
   // expected-warning@-1{{cannot call function returning non-sendable type}}
   let _ = plusOne(2)
+}
+
+///////
+// Effectful properties isolated to a global actor
+
+@BananaActor
+var effPropA : Int {
+  get async {
+    await asyncer()
+    try thrower() // expected-error{{errors thrown from here are not handled}}
+    return 0
+  }
+}
+
+@BananaActor
+var effPropT : Int { // expected-note{{var declared here}}
+  get throws {
+    await asyncer()  // expected-error{{'async' call in a function that does not support concurrency}}
+    try thrower()
+    return 0
+  }
+}
+
+@BananaActor
+var effPropAT : Int {
+  get async throws {
+    await asyncer()
+    try thrower()
+    return 0
+  }
+}
+
+// expected-note@+1 2 {{add 'async' to function 'tryEffPropsFromBanana()' to make it asynchronous}}
+@BananaActor func tryEffPropsFromBanana() throws {
+  // expected-error@+1{{'async' property access in a function that does not support concurrency}}
+  _ = effPropA
+
+  // expected-note@+4{{did you mean to handle error as optional value?}}
+  // expected-note@+3{{did you mean to use 'try'?}}
+  // expected-note@+2{{did you mean to disable error propagation?}}
+  // expected-error@+1{{property access can throw but is not marked with 'try'}}
+  _ = effPropT
+
+  _ = try effPropT
+
+  // expected-note@+6 {{did you mean to handle error as optional value?}}
+  // expected-note@+5 {{did you mean to use 'try'?}}
+  // expected-note@+4 {{did you mean to disable error propagation?}}
+  // expected-error@+3 {{property access can throw but is not marked with 'try'}}
+  // expected-note@+2 {{call is to 'rethrows' function, but argument function can throw}}
+  // expected-error@+1 {{call can throw but is not marked with 'try'}}
+  _ = rethrower(effPropT)
+
+  // expected-note@+2 {{call is to 'rethrows' function, but argument function can throw}}
+  // expected-error@+1 {{call can throw but is not marked with 'try'}}
+  _ = rethrower(try effPropT)
+
+  _ = try rethrower(effPropT)
+  _ = try rethrower(thrower())
+
+  _ = try rethrower(try effPropT)
+  _ = try rethrower(try thrower())
+
+  _ = rethrower(effPropA) // expected-error{{'async' property access in an autoclosure that does not support concurrency}}
+
+  _ = asAutoclosure(effPropT) // expected-error{{property access can throw, but it is not marked with 'try' and it is executed in a non-throwing autoclosure}}
+
+  // expected-note@+5{{did you mean to handle error as optional value?}}
+  // expected-note@+4{{did you mean to use 'try'?}}
+  // expected-note@+3{{did you mean to disable error propagation?}}
+  // expected-error@+2{{property access can throw but is not marked with 'try'}}
+  // expected-error@+1{{'async' property access in a function that does not support concurrency}}
+  _ = effPropAT
+}
+
+
+// expected-note@+2 {{add '@BananaActor' to make global function 'tryEffPropsFromSync()' part of global actor 'BananaActor'}}
+// expected-note@+1 2 {{add 'async' to function 'tryEffPropsFromSync()' to make it asynchronous}}
+func tryEffPropsFromSync() {
+  _ = effPropA // expected-error{{'async' property access in a function that does not support concurrency}}
+
+  // expected-error@+1 {{property access can throw, but it is not marked with 'try' and the error is not handled}}
+  _ = effPropT // expected-error{{var 'effPropT' isolated to global actor 'BananaActor' can not be referenced from this synchronous context}}
+  // NOTE: that we don't complain about async access on `effPropT` because it's not declared async, and we're not in an async context!
+
+  // expected-error@+1 {{property access can throw, but it is not marked with 'try' and the error is not handled}}
+  _ = effPropAT // expected-error{{'async' property access in a function that does not support concurrency}}
+}
+
+@OrangeActor func tryEffPropertiesFromGlobalActor() async throws {
+  // expected-error@+1{{property access is 'async' but is not marked with 'await'}} {{7-7=await }}
+  _ = effPropA
+
+  // expected-note@+5{{did you mean to handle error as optional value?}}
+  // expected-note@+4{{did you mean to use 'try'?}}
+  // expected-note@+3{{did you mean to disable error propagation?}}
+  // expected-error@+2{{property access can throw but is not marked with 'try'}}
+  // expected-error@+1{{property access is 'async' but is not marked with 'await'}} {{7-7=await }}
+  _ = effPropT
+
+  // expected-note@+5{{did you mean to handle error as optional value?}}
+  // expected-note@+4{{did you mean to use 'try'?}}
+  // expected-note@+3{{did you mean to disable error propagation?}}
+  // expected-error@+2{{property access can throw but is not marked with 'try'}}
+  // expected-error@+1{{property access is 'async' but is not marked with 'await'}} {{7-7=await }}
+  _ = effPropAT
+
+  _ = await effPropA
+  _ = try? await effPropT
+  _ = try! await effPropAT
+}
+
+/////////////
+// check subscripts in actors
+
+actor SubscriptA {
+  subscript(_ i : Int) -> Int {
+     get async {
+        try thrower() // expected-error{{errors thrown from here are not handled}}
+        await asyncer()
+        return 0
+     }
+  }
+
+  func f() async {
+    _ = self[0] // expected-error{{subscript access is 'async' but is not marked with 'await'}}
+  }
+}
+
+actor SubscriptT {
+  subscript(_ i : Int) -> Int {
+     get throws {
+        try thrower()
+        await asyncer() // expected-error{{'async' call in a function that does not support concurrency}}
+        return 0
+     }
+  }
+
+  func f() throws {
+    _ = try self[0]
+
+    // expected-note@+6 {{did you mean to handle error as optional value?}}
+    // expected-note@+5 {{did you mean to use 'try'?}}
+    // expected-note@+4 {{did you mean to disable error propagation?}}
+    // expected-error@+3 {{subscript access can throw but is not marked with 'try'}}
+    // expected-note@+2 {{call is to 'rethrows' function, but argument function can throw}}
+    // expected-error@+1 {{call can throw but is not marked with 'try'}}
+    _ = rethrower(self[1])
+
+    // expected-note@+2 {{call is to 'rethrows' function, but argument function can throw}}
+    // expected-error@+1 {{call can throw but is not marked with 'try'}}
+    _ = rethrower(try self[1])
+
+    _ = try rethrower(self[1])
+    _ = try rethrower(try self[1])
+  }
+}
+
+actor SubscriptAT {
+  subscript(_ i : Int) -> Int {
+     get async throws {
+        try thrower()
+        await asyncer()
+        return 0
+     }
+  }
+
+  func f() async throws {
+    _ = try await self[0]
+  }
+}
+
+func tryTheActorSubscripts(a : SubscriptA, t : SubscriptT, at : SubscriptAT) async throws {
+  _ = a[0] // expected-error{{subscript access is 'async' but is not marked with 'await'}}
+
+  _ = await a[0]
+
+  // expected-note@+5{{did you mean to handle error as optional value?}}
+  // expected-note@+4{{did you mean to use 'try'?}}
+  // expected-note@+3{{did you mean to disable error propagation?}}
+  // expected-error@+2{{subscript access can throw but is not marked with 'try'}}
+  // expected-error@+1 {{subscript access is 'async' but is not marked with 'await'}}
+  _ = t[0]
+
+  _ = try await t[0]
+  _ = try! await t[0]
+  _ = try? await t[0]
+
+  // expected-note@+5{{did you mean to handle error as optional value?}}
+  // expected-note@+4{{did you mean to use 'try'?}}
+  // expected-note@+3{{did you mean to disable error propagation?}}
+  // expected-error@+2{{subscript access can throw but is not marked with 'try'}}
+  // expected-error@+1 {{subscript access is 'async' but is not marked with 'await'}}
+  _ = at[0]
+
+  _ = try await at[0]
 }
