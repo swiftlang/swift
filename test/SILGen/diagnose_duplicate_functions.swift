dccaa8f01f24e0bb9c1635a76797f1fc30784a05
Erik Eckstein <eeckstein@apple.com>
2021-03-29 14:54:27 +0200

    diagnose duplicated @_cdecl and @_silgen_name function names
    Prints a regular error instead of crashing.
    The check is done in SILGen, because it's simple. We could also do it earlier, but I don't see a strong reason for this.
    
    rdar://75950093

---
+++ test/SILGen/diagnose_duplicate_functions.swift
@@ -0,0 +1,21 @@
+// RUN: %target-swift-emit-silgen %s -o /dev/null -verify
+
+@_silgen_name("foo")
+func a(_ x: Int) -> Int {
+  return x
+}
+
+@_silgen_name("foo")
+func b(_ x: Int) -> Int { // expected-error {{multiple definitions of symbol 'foo'}}
+  return x
+}
+
+@_cdecl("bar")
+func c(_ x: Int) -> Int {
+  return x
+}
+
+@_cdecl("bar")
+func d(_ x: Int) -> Int { // expected-error {{multiple definitions of symbol 'bar'}}
+  return x
+}
