352f1ab85e21f1e1b1e1ae4e805923344299571c
Alexis Laferrière <alaf@apple.com>
2021-03-30 16:33:50 -0700

    [SwiftShims] Make the void* arguments of memcmp _Nullable on Darwin
    This should correspond to the definition of memcmp in
    usr/include/string.h and keep it from being a source of confusion for
    the compiler.
    
    rdar://69876253
    rdar://78270152

--- stdlib/public/SwiftShims/LibcShims.h
+++ stdlib/public/SwiftShims/LibcShims.h
@@ -70,7 +70,11 @@ static inline __swift_size_t _swift_stdlib_strlen_unsigned(const unsigned char *
 SWIFT_READONLY
 static inline int _swift_stdlib_memcmp(const void *s1, const void *s2,
                                        __swift_size_t n) {
+#if defined(__APPLE__)
+  extern int memcmp(const void * _Nullable, const void * _Nullable, __swift_size_t);
+#else
   extern int memcmp(const void *, const void *, __swift_size_t);
+#endif
   return memcmp(s1, s2, n);
 }
 
