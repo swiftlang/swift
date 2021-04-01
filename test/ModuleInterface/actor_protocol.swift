6d2fc9e9bdd63b251e2d29e5c13833989e58eddb
Mike Ash <mikeash@apple.com>
2021-04-01 15:34:33 -0400

    Add @availability to some concurrency tests that were missing it.

--- test/ModuleInterface/actor_protocol.swift
+++ test/ModuleInterface/actor_protocol.swift
@@ -12,38 +12,46 @@
 // CHECK-EXTENSION-NOT: extension {{.+}} : _Concurrency.Actor
 
 // CHECK: public actor PlainActorClass {
+@available(macOS 9999, iOS 9999, watchOS 9999, tvOS 9999, *)
 public actor PlainActorClass {
   @actorIndependent public func enqueue(partialTask: PartialAsyncTask) { }
 }
 
 // CHECK: public actor ExplicitActorClass : _Concurrency.Actor {
+@available(macOS 9999, iOS 9999, watchOS 9999, tvOS 9999, *)
 public actor ExplicitActorClass : Actor {
   @actorIndependent public func enqueue(partialTask: PartialAsyncTask) { }
 }
 
 // CHECK: public actor EmptyActor {
+@available(macOS 9999, iOS 9999, watchOS 9999, tvOS 9999, *)
 public actor EmptyActor {}
 
 // CHECK: actor public class EmptyActorClass {
+@available(macOS 9999, iOS 9999, watchOS 9999, tvOS 9999, *)
 public actor class EmptyActorClass {}
 
 // CHECK: public protocol Cat : _Concurrency.Actor {
+@available(macOS 9999, iOS 9999, watchOS 9999, tvOS 9999, *)
 public protocol Cat : Actor {
   func mew()
 }
 
 // CHECK: public actor HouseCat : Library.Cat {
+@available(macOS 9999, iOS 9999, watchOS 9999, tvOS 9999, *)
 public actor HouseCat : Cat {
   @asyncHandler public func mew() {}
   @actorIndependent public func enqueue(partialTask: PartialAsyncTask) { }
 }
 
 // CHECK: public protocol ToothyMouth {
+@available(macOS 9999, iOS 9999, watchOS 9999, tvOS 9999, *)
 public protocol ToothyMouth {
   func chew()
 }
 
 // CHECK: public actor Lion : Library.ToothyMouth, _Concurrency.Actor {
+@available(macOS 9999, iOS 9999, watchOS 9999, tvOS 9999, *)
 public actor Lion : ToothyMouth, Actor {
   @asyncHandler public func chew() {}
   @actorIndependent public func enqueue(partialTask: PartialAsyncTask) { }
