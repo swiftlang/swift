95fd3ede096a88de0d985d88903baa847609cac8
Doug Gregor <dgregor@apple.com>
2021-04-05 21:29:17 -0700

    Update tests and testing tools for implicit _Concurrency import

--- test/Index/Store/unit-one-file-multi-file-invocation.swift
+++ test/Index/Store/unit-one-file-multi-file-invocation.swift
@@ -8,8 +8,9 @@
 // CHECK: 00-output_for_index
 // CHECK: DEPEND START
 // CHECK: Unit | system | Swift | [[MODULE:.*[/\\]Swift[.]swiftmodule([/\\].+[.]swiftmodule)?]] | [[SWIFT:.+[.]swiftmodule-[A-Z0-9]*]]
+// CHECK: Unit | system | _Concurrency | {{.*}}
 // CHECK: Record | user | {{.*}}{{/|\\}}unit-one-file-multi-file-invocation.swift |
-// CHECK: DEPEND END (2)
+// CHECK: DEPEND END (3)
 
 // CHECK: [[SWIFT]]
 // CHECK: DEPEND START
