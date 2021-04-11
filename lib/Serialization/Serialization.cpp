f4154d601979728c1a4b8f7e0661fe00416db347
Victoria Mitchell <victoria_m@apple.com>
2021-04-11 17:34:51 -0500

    add flag to skip docs on synthesized symbols

--- lib/Serialization/Serialization.cpp
+++ lib/Serialization/Serialization.cpp
@@ -5634,6 +5634,7 @@ void swift::serialize(ModuleOrSourceFile DC,
         AccessLevel::Public,
         /*EmitSynthesizedMembers*/true,
         /*PrintMessages*/false,
+        /*EmitInheritedDocs*/options.SkipSymbolGraphInheritedDocs,
       };
       symbolgraphgen::emitSymbolGraphForModule(M, SGOpts);
     }
--- test/SymbolGraph/Relationships/Synthesized/InheritedDocs.swift
+++ test/SymbolGraph/Relationships/Synthesized/InheritedDocs.swift
@@ -1,7 +1,18 @@
 // RUN: %empty-directory(%t)
-// RUN: %target-build-swift %s -module-name InheritedDocs -emit-module -emit-module-path %t/
+// RUN: %target-swift-frontend %s -module-name InheritedDocs -emit-module -emit-module-path %t/InheritedDocs.swiftmodule -emit-module-source-info-path %t/InheritedDocs.swiftsourceinfo -emit-module-doc-path %t/InheritedDocs.swiftdoc
+
 // RUN: %target-swift-symbolgraph-extract -module-name InheritedDocs -I %t -pretty-print -output-dir %t
-// RUN: %FileCheck %s --input-file %t/InheritedDocs.symbols.json
+// RUN: %FileCheck %s --input-file %t/InheritedDocs.symbols.json --check-prefixes CHECK,DOCS
+
+// RUN: %target-swift-symbolgraph-extract -module-name InheritedDocs -I %t -pretty-print -output-dir %t -skip-inherited-docs
+// RUN: %FileCheck %s --input-file %t/InheritedDocs.symbols.json --check-prefixes CHECK,SKIP
+
+// RUN: %empty-directory(%t)
+// RUN: %target-build-swift %s -module-name InheritedDocs -emit-module -emit-module-path %t/InheritedDocs.swiftmodule -emit-symbol-graph -emit-symbol-graph-dir %t/ -skip-inherited-docs
+// RUN: %FileCheck %s --input-file %t/InheritedDocs.symbols.json --check-prefixes SKIP
+
+// DOCS-COUNT-3: Some Function
+// SKIP-COUNT-2: Some Function
 
 // CHECK:            "source": "s:13InheritedDocs1PPAAE8someFuncyyF::SYNTHESIZED::s:13InheritedDocs1SV"
 // CHECK-NEXT:       "target": "s:13InheritedDocs1SV"
