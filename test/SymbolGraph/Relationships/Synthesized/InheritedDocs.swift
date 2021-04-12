f09fff4d345baf308f51cf2592df6ddd1eb09fd2
Victoria Mitchell <victoria_m@apple.com>
2021-04-12 18:26:08 -0500

    non-synthesized symbols can inherit docs too

--- test/SymbolGraph/Relationships/Synthesized/InheritedDocs.swift
+++ test/SymbolGraph/Relationships/Synthesized/InheritedDocs.swift
@@ -3,23 +3,30 @@
 
 // RUN: %target-swift-symbolgraph-extract -module-name InheritedDocs -I %t -pretty-print -output-dir %t
 // RUN: %FileCheck %s --input-file %t/InheritedDocs.symbols.json --check-prefixes CHECK,DOCS
+// RUN: %FileCheck %s --input-file %t/InheritedDocs.symbols.json --check-prefixes IMPL
 
 // RUN: %target-swift-symbolgraph-extract -module-name InheritedDocs -I %t -pretty-print -output-dir %t -skip-inherited-docs
 // RUN: %FileCheck %s --input-file %t/InheritedDocs.symbols.json --check-prefixes CHECK,SKIP
+// RUN: %FileCheck %s --input-file %t/InheritedDocs.symbols.json --check-prefixes IMPL
 
 // RUN: %empty-directory(%t)
 // RUN: %target-build-swift %s -module-name InheritedDocs -emit-module -emit-module-path %t/InheritedDocs.swiftmodule -emit-symbol-graph -emit-symbol-graph-dir %t/ -skip-inherited-docs
 // RUN: %FileCheck %s --input-file %t/InheritedDocs.symbols.json --check-prefixes SKIP
 
 // DOCS-COUNT-3: Some Function
-// SKIP-COUNT-2: Some Function
+// SKIP-COUNT-1: Some Function
 
 // CHECK:            "source": "s:13InheritedDocs1PPAAE8someFuncyyF::SYNTHESIZED::s:13InheritedDocs1SV"
 // CHECK-NEXT:       "target": "s:13InheritedDocs1SV"
-// CHECK-NEXT:       "sourceOrigin": {
-// CHECK-NEXT:         "identifier": "s:13InheritedDocs1PPAAE8someFuncyyF"
+// CHECK-NEXT:       "sourceOrigin"
+// CHECK-NEXT:         "identifier": "s:13InheritedDocs1PP8someFuncyyF"
 // CHECK-NEXT:         "displayName": "P.someFunc()"
-// CHECK-NEXT:       }
+
+// IMPL:             "source": "s:13InheritedDocs1PPAAE8someFuncyyF"
+// IMPL-NEXT:        "target": "s:13InheritedDocs1PP8someFuncyyF"
+// IMPL-NEXT:        "sourceOrigin"
+// IMPL-NEXT:          "identifier": "s:13InheritedDocs1PP8someFuncyyF"
+// IMPL-NEXT:          "displayName": "P.someFunc()"
 
 /// Protocol P
 public protocol P {
