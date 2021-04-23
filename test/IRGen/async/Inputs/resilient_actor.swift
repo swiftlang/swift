b88e6786945ff9be43502ed9cf77e134accf35c5
Doug Gregor <dgregor@apple.com>
2021-04-23 15:08:57 -0700

    [SE-0306] Disable actor inheritance.
    Actor inheritance was removed in the second revision of SE-0306. Remove
    the ability to inherit actors.
    
    Note that this doesn't fully eliminate all vestigates of inheritance
    from actors. There are simplifications that need to be performed
    still, e.g., there's no need to distinguish
    designated/convenience/required initializers. That will follow.

--- test/IRGen/async/Inputs/resilient_actor.swift
+++ test/IRGen/async/Inputs/resilient_actor.swift
@@ -3,11 +3,6 @@ open actor ResilientBaseActor {
 }
 
 @_fixed_layout
-open actor FixedSubclassOfResilientBaseActor : ResilientBaseActor {
-  public override init() {}
-}
-
-@_fixed_layout
 open actor FixedBaseActor {
   public init() {}
 }
