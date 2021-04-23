bc7488363702b1ac8b7707ad58d4e0d233b89832
Erik Eckstein <eeckstein@apple.com>
2021-04-23 20:15:20 +0200

    libswift: add the SIL Builder
    And two example instruction building functions.

--- include/swift/SIL/SILBridging.h
+++ include/swift/SIL/SILBridging.h
@@ -162,4 +162,11 @@ BridgedMultiValueResult
 BridgedStringRef CondFailInst_getMessage(BridgedInstruction cfi);
 BridgedGlobalVar GlobalAccessInst_getGlobal(BridgedInstruction globalInst);
 
+BridgedInstruction SILBuilder_createBuiltinBinaryFunction(
+          BridgedInstruction insertionPoint,
+          BridgedLocation loc, BridgedStringRef name,
+          BridgedType operandType, BridgedType resultType, BridgedValueArray arguments);
+BridgedInstruction SILBuilder_createCondFail(BridgedInstruction insertionPoint,
+          BridgedLocation loc, BridgedValue condition, BridgedStringRef messge);
+
 #endif
