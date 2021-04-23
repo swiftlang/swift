619226b480dc8fd8bdcead7976c07b441d01a0ba
Yuta Saito <kateinoigakukun@gmail.com>
2021-04-23 17:35:37 +0900

    Fix calling convention mismatch on `Builtin.getCurrentAsyncTask` (#37008)
    * Add explicit calling convention on builtin GetCurrentTask
    
    This builtin function emits a call of swift_task_getCurrent, and user
    code can declare the same name function with slightly different
    signature at LLVM level (data size should be same) using @_silgen_name.
    In that case, IRGen insert cast inst to cast the function to the
    expected signature. But this cast inst drops calling convention info, so
    call inst was emitted without swiftcc.
    
    This patch changed to emit a call of swift_task_getCurrent with the
    explicit calling convention.
    
    * Add test case to ensure builtin function cc when conflict

--- lib/IRGen/GenCall.cpp
+++ lib/IRGen/GenCall.cpp
@@ -230,6 +230,7 @@ void IRGenFunction::setupAsync(unsigned asyncContextIndex) {
 llvm::Value *IRGenFunction::getAsyncTask() {
   auto call = Builder.CreateCall(IGM.getGetCurrentTaskFn(), {});
   call->setDoesNotThrow();
+  call->setCallingConv(IGM.SwiftCC);
   return call;
 }
 
