f059713a7906d5b7c245fb84307ae56b7175bc57
Hamish Knight <hamish_github@mediocremail.com>
2021-04-30 11:14:50 +0100

    [test] Add -print-implicit-attrs to a couple of tests
    Now that @completionHandlerAsync is implicit for
    imported decls, we need to enable implicit attrs in
    a couple of tests that are testing its presence.

--- test/IDE/print_clang_objc_async.swift
+++ test/IDE/print_clang_objc_async.swift
@@ -1,6 +1,6 @@
 // RUN: %empty-directory(%t)
 
-// RUN: %target-swift-ide-test(mock-sdk: %clang-importer-sdk) -print-module -source-filename %s -module-to-print=ObjCConcurrency -function-definitions=false -enable-experimental-concurrency > %t/ObjCConcurrency.printed.txt
+// RUN: %target-swift-ide-test(mock-sdk: %clang-importer-sdk) -print-module -print-implicit-attrs -source-filename %s -module-to-print=ObjCConcurrency -function-definitions=false -enable-experimental-concurrency > %t/ObjCConcurrency.printed.txt
 // RUN: %FileCheck -input-file %t/ObjCConcurrency.printed.txt %s
 
 // REQUIRES: objc_interop
@@ -11,42 +11,52 @@ import _Concurrency
 
 // CHECK: @completionHandlerAsync("doSomethingSlow(_:)", completionHandleIndex: 1)
 // CHECK-NEXT: func doSomethingSlow(_ operation: String, completionHandler handler: @escaping (Int) -> Void)
+// CHECK-NEXT: @discardableResult
 // CHECK-NEXT: func doSomethingSlow(_ operation: String) async -> Int
 
 // CHECK: @completionHandlerAsync("doSomethingDangerous(_:)", completionHandleIndex: 1)
 // CHECK-NEXT: func doSomethingDangerous(_ operation: String, completionHandler handler: ((String?, Error?) -> Void)? = nil)
+// CHECK-NEXT: @discardableResult
 // CHECK-NEXT: func doSomethingDangerous(_ operation: String) async throws -> String
 
 // CHECK: @completionHandlerAsync("checkAvailability()", completionHandleIndex: 0)
 // CHECK-NEXT: func checkAvailability(completionHandler: @escaping (Bool) -> Void)
+// CHECK-NEXT: @discardableResult
 // CHECK-NEXT: func checkAvailability() async -> Bool
 
 // CHECK: @completionHandlerAsync("anotherExample()", completionHandleIndex: 0)
 // CHECK-NEXT: func anotherExample(completionBlock block: @escaping (String) -> Void)
+// CHECK-NEXT: @discardableResult
 // CHECK-NEXT: func anotherExample() async -> String
 
 // CHECK: @completionHandlerAsync("finalExample()", completionHandleIndex: 0)
 // CHECK-NEXT: func finalExampleWithReply(to block: @escaping (String) -> Void)
+// CHECK-NEXT: @discardableResult
 // CHECK-NEXT: func finalExample() async -> String
 
 // CHECK: @completionHandlerAsync("replyingOperation(_:)", completionHandleIndex: 1)
 // CHECK-NEXT: func replyingOperation(_ operation: String, replyTo block: @escaping (String) -> Void)
+// CHECK-NEXT: @discardableResult
 // CHECK-NEXT: func replyingOperation(_ operation: String) async -> String
 
 // CHECK: @completionHandlerAsync("findAnswer()", completionHandleIndex: 0)
 // CHECK-NEXT: func findAnswer(completionHandler handler: @escaping (String?, Error?) -> Void)
+// CHECK-NEXT: @discardableResult
 // CHECK-NEXT: func findAnswer() async throws -> String
 
 // CHECK: @completionHandlerAsync("findAnswerFailingly()", completionHandleIndex: 0)
 // CHECK-NEXT: func findAnswerFailingly(completionHandler handler: @escaping (String?, Error?) -> Void) throws
+// CHECK-NEXT: @discardableResult
 // CHECK-NEXT: func findAnswerFailingly() async throws -> String
 
 // CHECK: @completionHandlerAsync("findQAndA()", completionHandleIndex: 0)
 // CHECK-NEXT: func findQAndA(completionHandler handler: @escaping (String?, String?, Error?) -> Void)
+// CHECK-NEXT: @discardableResult
 // CHECK-NEXT: func findQAndA() async throws -> (String?, String)
 
 // CHECK: @completionHandlerAsync("findQuestionableAnswers()", completionHandleIndex: 0)
 // CHECK-NEXT: func findQuestionableAnswers(completionHandler handler: @escaping CompletionHandler)
+// CHECK-NEXT: @discardableResult
 // CHECK-NEXT: func findQuestionableAnswers() async throws -> (String, String?)
 
 // CHECK: @completionHandlerAsync("doSomethingFun(_:)", completionHandleIndex: 1)
@@ -55,7 +65,9 @@ import _Concurrency
 
 // CHECK: @completionHandlerAsync("doSomethingConflicted(_:)", completionHandleIndex: 1)
 // CHECK-NEXT: func doSomethingConflicted(_ operation: String, completionHandler handler: @escaping (Int) -> Void)
+// CHECK-NEXT: @discardableResult
 // CHECK-NEXT: func doSomethingConflicted(_ operation: String) async -> Int
+// CHECK-NEXT: @discardableResult
 // CHECK-NEXT: func doSomethingConflicted(_ operation: String) -> Int
 
 // CHECK: func dance(_ step: String) async -> String
@@ -63,10 +75,12 @@ import _Concurrency
 
 // CHECK: @completionHandlerAsync("runOnMainThread()", completionHandleIndex: 0)
 // CHECK-NEXT: func runOnMainThread(completionHandler completion: (@MainActor (String) -> Void)? = nil)
+// CHECK-NEXT: @discardableResult
 // CHECK-NEXT: func runOnMainThread() async -> String
 
 // CHECK: @completionHandlerAsync("asyncImportSame(_:)", completionHandleIndex: 1)
 // CHECK-NEXT: func asyncImportSame(_ operation: String, completionHandler handler: @escaping (Int) -> Void)
+// CHECK-NEXT: @discardableResult
 // CHECK-NEXT: func asyncImportSame(_ operation: String) async -> Int
 // CHECK-NEXT: func asyncImportSame(_ operation: String, replyTo handler: @escaping (Int) -> Void)
 // CHECK-NOT: func asyncImportSame(_ operation: String) async -> Int
@@ -76,20 +90,21 @@ import _Concurrency
 // CHECK-LABEL: protocol RefrigeratorDelegate
 // CHECK-NEXT: func someoneDidOpenRefrigerator(_ fridge: Any)
 // CHECK-NEXT: func refrigerator(_ fridge: Any, didGetFilledWithItems items: [Any])
-// CHECK-NEXT: {{^}}  func refrigerator(_ fridge: Any, didGetFilledWithIntegers items: UnsafeMutablePointer<Int>, count: Int)
-// CHECK-NEXT: {{^}}  func refrigerator(_ fridge: Any, willAddItem item: Any)
-// CHECK-NEXT: {{^}}  func refrigerator(_ fridge: Any, didRemoveItem item: Any) -> Bool
+// CHECK-NEXT: {{^}} @objc func refrigerator(_ fridge: Any, didGetFilledWithIntegers items: UnsafeMutablePointer<Int>, count: Int)
+// CHECK-NEXT: {{^}} @objc func refrigerator(_ fridge: Any, willAddItem item: Any)
+// CHECK-NEXT: @discardableResult
+// CHECK-NEXT: {{^}} @objc func refrigerator(_ fridge: Any, didRemoveItem item: Any) -> Bool
 // CHECK-NEXT: {{^[}]$}}
 
 // CHECK-LABEL: protocol ProtocolWithSwiftAttributes {
 // CHECK-NEXT: @actorIndependent func independentMethod()
 // CHECK-NEXT: func asyncHandlerMethod()
-// CHECK-NEXT: {{^}}  @MainActor func mainActorMethod()
-// CHECK-NEXT: {{^}}  @MainActor func uiActorMethod()
-// CHECK-NEXT: {{^}}  optional func missingAtAttributeMethod()
+// CHECK-NEXT: {{^}} @objc @MainActor func mainActorMethod()
+// CHECK-NEXT: {{^}} @objc @MainActor func uiActorMethod()
+// CHECK-NEXT: {{^}} @objc optional func missingAtAttributeMethod()
 // CHECK-NEXT: {{^[}]$}}
 
-// CHECK: {{^}}var MAGIC_NUMBER: Int32 { get }
+// CHECK: {{^}}@actorIndependent(unsafe) var MAGIC_NUMBER: Int32 { get }
 
 // CHECK: func doSomethingConcurrently(_ block: @Sendable () -> Void)
 
--- test/IDE/print_clang_objc_effectful_properties.swift
+++ test/IDE/print_clang_objc_effectful_properties.swift
@@ -1,6 +1,6 @@
 // RUN: %empty-directory(%t)
 
-// RUN: %target-swift-ide-test(mock-sdk: %clang-importer-sdk) -print-module -source-filename %s -module-to-print=EffectfulProperties -function-definitions=false -enable-experimental-concurrency > %t/EffectfulProperties.printed.txt
+// RUN: %target-swift-ide-test(mock-sdk: %clang-importer-sdk) -print-module -print-implicit-attrs -source-filename %s -module-to-print=EffectfulProperties -function-definitions=false -enable-experimental-concurrency > %t/EffectfulProperties.printed.txt
 // RUN: %FileCheck -input-file %t/EffectfulProperties.printed.txt %s
 
 // REQUIRES: objc_interop
@@ -31,6 +31,7 @@
 
 // CHECK:  @completionHandlerAsync("regularMainDog()", completionHandleIndex: 0)
 // CHECK-NEXT:  func regularMainDog(_ completion: @escaping @MainActor (String) -> Void)
+// CHECK-NEXT:  @discardableResult
 // CHECK-NEXT:  func regularMainDog() async -> String
 // CHECK: }
 
