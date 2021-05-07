b8b5edbf5c73dc29bd0ffceedefac2cdefd3f8c5
Arnold Schwaighofer <aschwaighofer@apple.com>
2021-05-07 08:09:34 -0700

    Disable test type_checker_perf/slow/rdar19737632.swift
    It failed on a bot.
    
    rdar://77656775

--- validation-test/Sema/type_checker_perf/slow/rdar19737632.swift
+++ validation-test/Sema/type_checker_perf/slow/rdar19737632.swift
@@ -1,6 +1,9 @@
 // RUN: %target-typecheck-verify-swift -solver-expression-time-threshold=1
 // REQUIRES: tools-release,no_asan
 
+// rdar://77656775
+// UNSUPPORTED: CPU=arm64 && OS=macosx
+
 let a = "a"
 let b = "b"
 let c = 42
