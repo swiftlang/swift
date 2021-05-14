762337cc9b2138c588bef48c11ea3a8210f45aba
Ben Barham <ben_barham@apple.com>
2021-05-14 14:42:08 +1000

    [Refactoring] Add @completionHandlerAsync to sync function
    When adding an async alternative, add the @completionHandlerAsync
    attribute to the sync function. Check for this attribute in addition to
    the name check, ie. convert a call if the callee has either
    @completionHandlerAsync or a name that is completion-handler-like name.
    
    The addition of the attribute is currently gated behind the experimental
    concurrency flag.
    
    Resolves rdar://77486504

--- utils/refactor-check-compiles.py
+++ utils/refactor-check-compiles.py
@@ -21,16 +21,17 @@ def parse_args():
         formatter_class=argparse.RawDescriptionHelpFormatter,
         description="""
         A drop-in replacement for a 'swift-refactor -dump-text' call that
-        1. Checkes that the file still compiles after the refactoring by doing
+        1. Checks that the file still compiles after the refactoring by doing
            'swift-refactor -dump-rewritten' and feeding the result to
            'swift-frontend -typecheck'
         2. Outputting the result of the 'swift-refactor -dump-text' call
 
-        All arguments other than the following will be forwarded to :
+        All arguments other than the following will be forwarded to
         'swift-refactor':
          - swift-frontend
          - swift-refactor
          - temp-dir
+         - enable-experimental-concurrency (sent to both)
         """)
 
     parser.add_argument(
@@ -61,15 +62,29 @@ def parse_args():
         '-pos',
         help='The position to invoke the refactoring at'
     )
+    parser.add_argument(
+        '-enable-experimental-concurrency',
+        action='store_true',
+        help='''
+        Whether to enable experimental concurrency in both swift-refactor and
+        swift-frontend
+        '''
+    )
 
     return parser.parse_known_args()
 
 
 def main():
-    (args, unknown_args) = parse_args()
+    (args, extra_refactor_args) = parse_args()
     temp_file_name = os.path.split(args.source_filename)[-1] + '.' + \
         args.pos.replace(':', '.')
     temp_file_path = os.path.join(args.temp_dir, temp_file_name)
+
+    extra_frontend_args = []
+    if args.enable_experimental_concurrency:
+        extra_refactor_args.append('-enable-experimental-concurrency')
+        extra_frontend_args.append('-enable-experimental-concurrency')
+
     # FIXME: `refactor-check-compiles` should generate both `-dump-text` and
     # `dump-rewritten` from a single `swift-refactor` invocation (SR-14587).
     dump_text_output = run_cmd([
@@ -77,14 +92,14 @@ def main():
         '-dump-text',
         '-source-filename', args.source_filename,
         '-pos', args.pos
-    ] + unknown_args, desc='producing edit').decode("utf-8")
+    ] + extra_refactor_args, desc='producing edit').decode("utf-8")
 
     dump_rewritten_output = run_cmd([
         args.swift_refactor,
         '-dump-rewritten',
         '-source-filename', args.source_filename,
         '-pos', args.pos
-    ] + unknown_args, desc='producing rewritten file')
+    ] + extra_refactor_args, desc='producing rewritten file')
     with open(temp_file_path, 'wb') as f:
         f.write(dump_rewritten_output)
 
@@ -93,7 +108,7 @@ def main():
         '-typecheck',
         temp_file_path,
         '-disable-availability-checking'
-    ], desc='checking that rewritten file compiles')
+    ] + extra_frontend_args, desc='checking that rewritten file compiles')
     sys.stdout.write(dump_text_output)
 
 
