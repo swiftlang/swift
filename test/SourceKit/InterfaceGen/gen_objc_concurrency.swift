0be6a4d9bdd7b3aa55787bce39c2a11ceb9d2b28
Hamish Knight <hamish_github@mediocremail.com>
2021-05-20 16:35:43 +0100

    [test] Add additional test case for rdar://76685011
    Make sure we don't print the @completionHandlerAsync
    attr in the doc info.

--- test/SourceKit/InterfaceGen/gen_objc_concurrency.swift
+++ test/SourceKit/InterfaceGen/gen_objc_concurrency.swift
@@ -3,7 +3,7 @@
 
 // RUN: %empty-directory(%t)
 
-// RUN: %sourcekitd-test -req=interface-gen %S/Inputs/gen_concurrency.swift -- %S/Inputs/gen_concurrency.swift -target %target-triple -I %t -Xfrontend -enable-experimental-concurrency | %FileCheck %s --check-prefix=SWIFT-GEN-INTERFACE
+// RUN: %sourcekitd-test -req=interface-gen %S/../Inputs/concurrency/gen_concurrency.swift -- %S/../Inputs/concurrency/gen_concurrency.swift -target %target-triple -I %t -Xfrontend -enable-experimental-concurrency | %FileCheck %s --check-prefix=SWIFT-GEN-INTERFACE
 
 // Make sure we print @completionHandlerAsync when it was explicitly written by the user.
 // SWIFT-GEN-INTERFACE-LABEL: class ClassWithAsyncAndHandler {
@@ -11,7 +11,7 @@
 // SWIFT-GEN-INTERFACE-NEXT:    internal func foo(_ operation: String, completionHandler handler: @escaping (Int) -> Void)
 // SWIFT-GEN-INTERFACE:         internal func foo(_ operation: String) async -> Int
 
-// RUN: %sourcekitd-test -req=interface-gen -using-swift-args -header %S/Inputs/header_concurrency.h -- %s -Xfrontend -enable-objc-interop -Xfrontend -enable-experimental-concurrency -import-objc-header %S/Inputs/header_concurrency.h -sdk %clang-importer-sdk | %FileCheck %s --check-prefix=OBJC-GEN-INTERFACE
+// RUN: %sourcekitd-test -req=interface-gen -using-swift-args -header %S/../Inputs/concurrency/header_concurrency.h -- %s -Xfrontend -enable-objc-interop -Xfrontend -enable-experimental-concurrency -import-objc-header %S/../Inputs/concurrency/header_concurrency.h -sdk %clang-importer-sdk | %FileCheck %s --check-prefix=OBJC-GEN-INTERFACE
 
 // But don't print @completionHandlerAsync if it was implicitly added to an imported Clang decl (rdar://76685011).
 // OBJC-GEN-INTERFACE-LABEL: class ClassWithHandlerMethod {
