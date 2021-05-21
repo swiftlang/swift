5ab3708895764da5ab051c705922fa4636796926
Varun Gandhi <varun_gandhi@apple.com>
2021-05-21 15:32:28 -0700

    [IRGen] Fix missing musttail for dynamic replacement calls.
    Fixes rdar://78284346.
    
    (Found when trying to land musttail verification. :D)

--- test/IRGen/async_dynamic_replacement.swift
+++ test/IRGen/async_dynamic_replacement.swift
@@ -14,8 +14,7 @@ internal func _replacement_number() async -> Int {
 // rdar://78284346 - Dynamic replacement should use musttail
 //                   for tail calls from swifttailcc to swifttailcc
 // CHECK-LABEL: define {{.*}} swifttailcc void @"$s25async_dynamic_replacement01_C7_numberSiyYaFTI"
-// CHECK-NOT: musttail
-// CHECK: call swifttailcc void
+// CHECK: musttail call swifttailcc void
 // CHECK-NEXT: ret void
 
 public func calls_number() async -> Int {
