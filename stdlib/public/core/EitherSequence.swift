2bb19ab07c305930f5b63d6e8003821de3f1a868
Chris Adamson <cadamson@apple.com>
2021-05-25 11:23:44 -0400

    Remove extraneous /-es from license comments.

--- stdlib/public/core/EitherSequence.swift
+++ stdlib/public/core/EitherSequence.swift
@@ -1,14 +1,14 @@
-////===--- _EitherSequence.swift - A sequence type-erasing two sequences -----===//
-////
-//// This source file is part of the Swift.org open source project
-////
-//// Copyright (c) 2014 - 2017 Apple Inc. and the Swift project authors
-//// Licensed under Apache License v2.0 with Runtime Library Exception
-////
-//// See https://swift.org/LICENSE.txt for license information
-//// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
-////
-////===----------------------------------------------------------------------===//
+//===--- _EitherSequence.swift - A sequence type-erasing two sequences -----===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2017 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
 
 // Not public stdlib API, currently used in Mirror.children implementation.
 
