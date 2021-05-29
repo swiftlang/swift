ef5c4db7ed211169e0590a5bed44664e9f6be971
Pavel Yaskevich <pyaskevich@apple.com>
2021-05-28 17:41:47 -0700

    [ConstraintSystem] Simplify relational constraints with the same dependent member type on both sides
    If relational constraint has the same dependent member type on both
    sides e.g. `$T1.Element == $T1.Element` allow its simplification,
    since inference of `$T1` results in dependent member being resolved
    to the same concrete type. Otherwise constraint system would be left
    with this constraint in inactive state if `$T1` couldn't be resolved
    which results in a crash.
    
    Resolves: rdar://78623338
    (cherry picked from commit 1c3d685fd8b7cb2bf49437e7a8583e9e677b06d4)

--- test/Constraints/generics.swift
+++ test/Constraints/generics.swift
@@ -858,3 +858,19 @@ func rdar56212087() {
 
   setValue(foo("", ""), forKey: "") // Ok (T is inferred as a `String` instead of `Any?`)
 }
+
+// rdar://78623338 - crash due to leftover inactive constraints
+func rdar78623338() {
+  func any<T : Sequence>(_ sequence: T) -> AnySequence<T.Element> {
+    // expected-note@-1 {{required by local function 'any' where 'T' = '() -> ReversedCollection<(ClosedRange<Int>)>'}}
+    AnySequence(sequence.makeIterator)
+  }
+
+  let _ = [
+    any(0...3),
+    // TODO: It should be possible to suggest making a call to `reserved` here but we don't have machinery to do so
+    //       at the moment because there is no way to go from a requirement to the underlying argument/parameter location.
+    any((1...3).reversed) // expected-error {{type '() -> ReversedCollection<(ClosedRange<Int>)>' cannot conform to 'Sequence'}}
+    // expected-note@-1 {{only concrete types such as structs, enums and classes can conform to protocols}}
+  ]
+}
