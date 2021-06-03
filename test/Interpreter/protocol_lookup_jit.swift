524c5e0e14c65f415e921f3a866f37c137e010f0
Michael Gottesman <mgottesman@apple.com>
2021-06-03 12:41:39 -0700

    When running with the interpreter on Darwin, force the usage of the just built, stdlib when running the frontend.
    This is because we currently JIT in process resulting in weirdness around
    swift-frontend wanting to link against the host stdlib and the exec swift code
    wanting to link against the target stdlib. For now, we work around this by just
    saying in that case we will force swift-frontend to use the just built runtime
    so we are consistent. The only potential problem is that a bug in the just built
    runtime may now cause these tests to fail. But overall, that would suggest a
    problem we may want to catch in it of itself, so the work around I think makes
    sense until JITing is done in a separate process (which I think is the right fix).
    
    rdar://78768013

--- test/Interpreter/protocol_lookup_jit.swift
+++ test/Interpreter/protocol_lookup_jit.swift
@@ -1,5 +1,5 @@
 // Test protocol_lookup.swift in JIT mode.
-// RUN: %swift -interpret %S/protocol_lookup.swift | %FileCheck %S/protocol_lookup.swift
+// RUN: %target-jit-run %S/protocol_lookup.swift | %FileCheck %S/protocol_lookup.swift
 // REQUIRES: executable_test
 
 // REQUIRES: swift_interpreter
