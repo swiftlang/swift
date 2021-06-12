c90d64d4feb4f9b9baeaa28cfcd960997d46d536
Slava Pestov <spestov@apple.com>
2021-06-11 23:06:58 -0400

    Sema: Reject generic arguments applied to 'Self'
    Previously these were simply ignored.
    
    Fixes https://bugs.swift.org/browse/SR-14731 / rdar://problem/79051797.

--- lib/Sema/TypeCheckType.cpp
+++ lib/Sema/TypeCheckType.cpp
@@ -1342,6 +1342,25 @@ static SelfTypeKind getSelfTypeKind(DeclContext *dc,
   }
 }
 
+static void diagnoseGenericArgumentsOnSelf(TypeResolution resolution,
+                                           ComponentIdentTypeRepr *comp,
+                                           DeclContext *typeDC) {
+  ASTContext &ctx = resolution.getASTContext();
+  auto &diags = ctx.Diags;
+
+  auto *selfNominal = typeDC->getSelfNominalTypeDecl();
+  auto declaredType = selfNominal->getDeclaredType();
+
+  diags.diagnose(comp->getNameLoc(), diag::cannot_specialize_self);
+
+  if (selfNominal->isGeneric() && !isa<ProtocolDecl>(selfNominal)) {
+    diags.diagnose(comp->getNameLoc(), diag::specialize_explicit_type_instead,
+                   declaredType)
+        .fixItReplace(comp->getNameLoc().getSourceRange(),
+                      declaredType.getString());
+  }
+}
+
 /// Resolve the given identifier type representation as an unqualified type,
 /// returning the type it references.
 ///
@@ -1452,6 +1471,13 @@ static Type resolveTopLevelIdentTypeComponent(TypeResolution resolution,
 
       // Check if we can reference 'Self' here, and if so, what kind of Self it is.
       auto selfTypeKind = getSelfTypeKind(DC, options);
+
+      // We don't allow generic arguments on 'Self'.
+      if (selfTypeKind != SelfTypeKind::InvalidSelf &&
+          isa<GenericIdentTypeRepr>(comp)) {
+        diagnoseGenericArgumentsOnSelf(resolution, comp, typeDC);
+      }
+
       switch (selfTypeKind) {
       case SelfTypeKind::StaticSelf:
         return selfType;
