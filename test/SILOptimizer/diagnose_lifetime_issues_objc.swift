c63eb59c562b2ffe80e161c8a3535cdcb2685780
Andrew Trick <atrick@apple.com>
2021-06-15 16:26:17 -0700

    Disable weak reference lifetime warnings by default.
    Only issue weak lifetime warnings for users who select object lifetime
    optimization. The risk of spurious warnings outweighs the benefits.
    
    Although the warnings are generally useful regardless of the level of
    optimization, it isn't really critical to issue them unless the optimizer
    aggressively shrinks reference lifetimes.
    
    Fixes rdar://79146338 Xcode warns that "referenced object is
    deallocated here" but that object was passed into a method that causes
    strong retention)
    
    (cherry picked from commit 369ecb68f6a4bb31865d5d8d3ed549bc068a4a45)

--- test/SILOptimizer/diagnose_lifetime_issues_objc.swift
+++ test/SILOptimizer/diagnose_lifetime_issues_objc.swift
@@ -1,4 +1,4 @@
-// RUN: %target-swift-frontend -emit-sil %s -o /dev/null -verify
+// RUN: %target-swift-frontend -emit-sil %s -enable-copy-propagation -o /dev/null -verify
 // REQUIRES: objc_interop
 
 import Foundation
