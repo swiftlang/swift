9c9fef1cf55cba82f1f6dc4625f358ef5665d543
Doug Gregor <dgregor@apple.com>
2021-06-15 09:30:39 -0700

    Disable standard substitutions when building with "older" _Concurrency module
    To smooth over an ABI transition, disable the standard substitutions
    for the _Concurrency module within the AST mangler when the
    _Concurrency module we are using predates custom executors. This is a
    short-term hack that should be removed when we settle the ABI.
    
    Addresses rdar://79298287.
    
    (cherry picked from commit de8c8f5537d67344949d6bdce2d5815a71dbd8e8)

--- lib/AST/Module.cpp
+++ lib/AST/Module.cpp
@@ -1427,9 +1427,19 @@ bool ModuleDecl::isStdlibModule() const {
 }
 
 bool ModuleDecl::hasStandardSubstitutions() const {
-  return !getParent() &&
-      (getName() == getASTContext().StdlibModuleName ||
-       getName() == getASTContext().Id_Concurrency);
+  if (getParent())
+    return false;
+
+  if (getName() == getASTContext().StdlibModuleName)
+    return true;
+
+  // The _Concurrency module gets standard substitutions with "new enough"
+  // versions of the module.
+  if (getName() == getASTContext().Id_Concurrency &&
+      getASTContext().getProtocol(KnownProtocolKind::SerialExecutor))
+    return true;
+
+  return false;
 }
 
 bool ModuleDecl::isSwiftShimsModule() const {
