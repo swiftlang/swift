48e06b79d8bc1c92d93c6c3ad7a46284b596849b
Minhyuk Kim <kimminhyuk1004@gmail.com>
2021-06-21 00:30:45 +0900

    [Sema] Add diagnostic note when using nonexistent carrot operator

--- test/decl/operator/power_operator_imported.swift
+++ test/decl/operator/power_operator_imported.swift
@@ -8,4 +8,5 @@ func testNonexistentPowerOperatorWithPowFunctionInScope() {
   let y: Double = 3.0
   let z: Double = x**y // expected-error {{no operator '**' is defined; did you mean 'pow(_:_:)'?}}
   let w: Double = a(x**2.0) // expected-error {{no operator '**' is defined; did you mean 'pow(_:_:)'?}}
+  let _: Double = x^y // expected-error {{binary operator '^' cannot be applied to two 'Double' operands}} expected-note {{did you mean to use 'pow(_:_:)'?}}
 }
