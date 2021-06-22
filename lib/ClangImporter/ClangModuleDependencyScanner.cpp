450b78f48dc4b97a44442bafe124a21c76267102
Evan Wilde <ewilde@apple.com>
2021-06-22 10:53:43 -0700

    NonPathCommandLine -> getAdditionalArgsWithoutModulePaths()
    This value was replaced with a call to 'getNonPathCommandLine()' in
    llvm-project commit 2b73565210ef0b3b29e0f067eef150a32adbb967. The API
    was later renamed 'getAdditionalArgsWithoutModulePaths()' in
    llvm-project commit 77fc4d1feef5f3041f614206447b4461992054bb.

--- lib/ClangImporter/ClangModuleDependencyScanner.cpp
+++ lib/ClangImporter/ClangModuleDependencyScanner.cpp
@@ -247,7 +247,8 @@ void ClangImporter::recordModuleDependencies(
 
     // Add all args the non-path arguments required to be passed in, according
     // to the Clang scanner
-    for (const auto &clangArg : clangModuleDep.NonPathCommandLine) {
+    for (const auto &clangArg :
+         clangModuleDep.getAdditionalArgsWithoutModulePaths()) {
       swiftArgs.push_back("-Xcc");
       swiftArgs.push_back("-Xclang");
       swiftArgs.push_back("-Xcc");
