287bbebbeb6cb09d756210cd230399f7513a3495
Artem Chikin <achikin@apple.com>
2021-06-24 13:11:09 -0700

    Update the ScanDependencies test to use %target-cpu And create arch-specific inputs for it.

--- test/ScanDependencies/clang-target.swift
+++ test/ScanDependencies/clang-target.swift
@@ -1,21 +1,22 @@
 // REQUIRES: VENDOR=apple
+// REQUIRES: OS=macosx
+
 // RUN: %empty-directory(%t.module-cache)
-// RUN: %target-swift-frontend -emit-module -o %t.foo.swiftmodule -module-cache-path %t.module-cache -I %S/Inputs/CHeaders -I %S/Inputs/Swift %s -target x86_64-apple-macosx10.14
+// RUN: %target-swift-frontend -emit-module -o %t.foo.swiftmodule -module-cache-path %t.module-cache -I %S/Inputs/CHeaders -I %S/Inputs/Swift %s -target %target-cpu-apple-macosx10.14
 
 // Without -clang-target, we build two X.pcm
 // RUN: find %t.module-cache -name "X-*.pcm" | count 2
 
 // RUN: %empty-directory(%t.module-cache)
-// RUN: %target-swift-frontend -emit-module -o %t.foo.swiftmodule -module-cache-path %t.module-cache -I %S/Inputs/CHeaders -I %S/Inputs/Swift %s -target x86_64-apple-macosx10.14 -clang-target x86_64-apple-macosx10.14
+// RUN: %target-swift-frontend -emit-module -o %t.foo.swiftmodule -module-cache-path %t.module-cache -I %S/Inputs/CHeaders -I %S/Inputs/Swift %s -target %target-cpu-apple-macosx10.14 -clang-target %target-cpu-apple-macosx10.14
 
 // With -clang-target, we build one X.pcm
 // RUN: find %t.module-cache -name "X-*.pcm" | count 1
+// RUN: %target-swift-frontend -scan-dependencies -module-cache-path %t.module-cache %s -o %t.deps.json -I %S/Inputs/CHeaders -I %S/Inputs/Swift -target %target-cpu-apple-macosx10.14 -clang-target %target-cpu-apple-macosx10.14
 
-// RUN: %target-swift-frontend -scan-dependencies -module-cache-path %t.module-cache %s -o %t.deps.json -I %S/Inputs/CHeaders -I %S/Inputs/Swift -target x86_64-apple-macosx10.14 -clang-target x86_64-apple-macosx10.14
 // RUN: %FileCheck %s < %t.deps.json
 
 // CHECK: "-clang-target"
-// CHECK-NEXT: "x86_64-apple-macosx10.14"
-
+// CHECK-NEXT: "{{.*}}-apple-macosx10.14"
 import X
 import XWithTarget
