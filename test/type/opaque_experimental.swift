c5e307dcdea3a63f7356e578d09b4b82d7b449ed
Benjamin Driscoll <benjamin_driscoll@apple.com>
2021-06-24 16:51:44 -0400

    Add named opaque type test cases for patterns and subscripts

--- test/type/opaque_experimental.swift
+++ test/type/opaque_experimental.swift
@@ -10,3 +10,10 @@ func f4() async -> <T> () { }
 func g0() -> <T> { } // expected-error{{expected type for function result}}
 func g1() -> async <T> () { } // expected-error{{'async' may only occur before '->'}}
 func g2() -> <T> () async { } // expected-error{{'async' may only occur before '->'}}
+
+let x0: <T> Int = 1
+var x1: <T> (Int, Int) = (1, 1)
+var x2: <T> (<U> Int, Int) = (1, 1) // expected-error{{expected type}} expected-error{{cannot convert value of type '(Int, Int)' to specified type 'Int'}}
+for _: <T> Int in [1, 2, 3] { }
+
+struct S0 { subscript(i: Int) -> <T> Int { 1 } }
