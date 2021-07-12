f494bfc6030e06e76cc253bc9b9b9578754e0678
Alex Hoppen <ahoppen@apple.com>
2021-07-12 20:25:28 +0200

    Revert "[Sema] Check completion key path component to determine if completion node was type checked in multi expression closure"
    This reverts commit ffd0072b4e4bfd66cbced8b48cea5cb2b05eec98.

--- lib/Sema/CSApply.cpp
+++ lib/Sema/CSApply.cpp
@@ -8920,11 +8920,6 @@ bool Solution::hasType(ASTNode node) const {
   return cs.hasType(node);
 }
 
-bool Solution::hasType(const KeyPathExpr *KP, unsigned ComponentIndex) const {
-  auto &cs = getConstraintSystem();
-  return cs.hasType(KP, ComponentIndex);
-}
-
 Type Solution::getType(ASTNode node) const {
   auto result = nodeTypes.find(node);
   if (result != nodeTypes.end())
