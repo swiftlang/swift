e223a4f9557f1f8dcd14db09d09449adc33f3461
John McCall <rjmccall@apple.com>
2021-07-14 15:58:03 -0400

    Greatly increase the amount of private storage in a task.
    It's already nearly full, and there's plenty of stuff we're not
    tracking properly yet.

--- include/swift/ABI/Task.h
+++ include/swift/ABI/Task.h
@@ -203,7 +203,7 @@ public:
 
   /// Private storage for the use of the runtime.
   struct alignas(2 * alignof(void*)) OpaquePrivateStorage {
-    void *Storage[6];
+    void *Storage[14];
 
     /// Initialize this storage during the creation of a task.
     void initialize(AsyncTask *task);
