c5ad28779db4a361ed21bec438634e79216cccb7
Luciano Almeida <passos.luciano@outlook.com>
2021-07-19 20:20:56 -0300

    [tests] Run module interface build alternative interface test in dynamic target cpu

--- test/ModuleInterface/build-alternative-interface-framework.swift
+++ test/ModuleInterface/build-alternative-interface-framework.swift
@@ -1,6 +1,11 @@
 // REQUIRES: VENDOR=apple
+
+// Required DummyFramework imported is only built for those two CPU architectures.
+// REQUIRES: CPU=arm64 || CPU=x86_64
+// REQUIRES: OS=macosx
+
 // RUN: %empty-directory(%t/module-cache)
-// RUN: not %target-swift-frontend-typecheck -disable-implicit-concurrency-module-import -target arm64-apple-macosx13.0 -F %S/Inputs %s -module-cache-path %t/module-cache
-// RUN: %target-swift-frontend-typecheck -disable-implicit-concurrency-module-import -target arm64-apple-macosx13.0 -F %S/Inputs %s -module-cache-path %t/module-cache -backup-module-interface-path %S/Inputs/alternative-interfaces
+// RUN: not %target-swift-frontend-typecheck -disable-implicit-concurrency-module-import -target %target-cpu-apple-macosx13.0 -F %S/Inputs %s -module-cache-path %t/module-cache
+// RUN: %target-swift-frontend-typecheck -disable-implicit-concurrency-module-import -target %target-cpu-apple-macosx13.0 -F %S/Inputs %s -module-cache-path %t/module-cache -backup-module-interface-path %S/Inputs/alternative-interfaces
 
 import DummyFramework
