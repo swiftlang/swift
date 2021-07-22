d369fcb130e2a3c44b6f90edb343a30aac4d8953
Ted Kremenek <kremenek@apple.com>
2021-07-22 12:51:52 -0700

    Update Swift version to 5.6

--- test/IRGen/objc.swift
+++ test/IRGen/objc.swift
@@ -145,7 +145,7 @@ class WeakObjC {
 // CHECK:  i32 1, !"Objective-C Version", i32 2}
 // CHECK:  i32 1, !"Objective-C Image Info Version", i32 0}
 // CHECK:  i32 1, !"Objective-C Image Info Section", !"__DATA,__objc_imageinfo,regular,no_dead_strip"}
-//   84215552 == (5 << 24) | (5 << 16) | (7 << 8).
-//     5 and 5 is the current major.minor version. 7 is the Swift ABI version.
-// CHECK:  i32 4, !"Objective-C Garbage Collection", i32 84215552}
+//   84281088 == (5 << 24) | (6 << 16) | (7 << 8).
+//     5 and 6 is the current major.minor version. 7 is the Swift ABI version.
+// CHECK:  i32 4, !"Objective-C Garbage Collection", i32 84281088}
 // CHECK:  i32 1, !"Swift Version", i32 7}
