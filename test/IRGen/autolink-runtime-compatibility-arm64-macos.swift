18aa9874f53ef88be1209c991a25c37dd37553be
Michael Gottesman <mgottesman@apple.com>
2021-07-28 14:59:53 -0700

    Fix test.
    This test got through CI since we do not require Apple Silicon right now.
    
    rdar://81115750

--- test/IRGen/autolink-runtime-compatibility-arm64-macos.swift
+++ test/IRGen/autolink-runtime-compatibility-arm64-macos.swift
@@ -1,14 +1,27 @@
-// REQUIRES: rdar81115750
 // REQUIRES: CPU=arm64,OS=macosx
 
-// Doesn't autolink compatibility library because target OS doesn't need it
-// RUN: %target-swift-frontend -target arm64-apple-macosx10.14  -emit-ir -parse-stdlib %s | %FileCheck -check-prefix=NO-FORCE-LOAD %s
+// Only autolink compatibility libraries before 5.5 since target OS doesn't need
+// it.
+
+// RUN: %target-swift-frontend -target arm64-apple-macosx10.14  -emit-ir -parse-stdlib %s | %FileCheck %s
 
 public func foo() {}
 
-// NO-FORCE-LOAD-NOT: FORCE_LOAD
-// NO-FORCE-LOAD-NOT: !{!"-lswiftCompatibility50"}
-// NO-FORCE-LOAD-NOT: !{!"-lswiftCompatibility51"}
-// NO-FORCE-LOAD-NOT: !{!"-lswiftCompatibility52"}
-// NO-FORCE-LOAD-NOT: !{!"-lswiftCompatibility53"}
-// NO-FORCE-LOAD-NOT: !{!"-lswiftCompatibilityDynamicReplacements"}
+// CHECK-NOT: @"_swift_FORCE_LOAD_$_swiftCompatibility50"
+// CHECK-NOT: @"_swift_FORCE_LOAD_$_swiftCompatibility51"
+// CHECK-NOT: @"_swift_FORCE_LOAD_$_swiftCompatibility52"
+// CHECK-NOT: @"_swift_FORCE_LOAD_$_swiftCompatibility53"
+// CHECK-NOT: @"_swift_FORCE_LOAD_$_swiftCompatibilityDynamicReplacements"
+// CHECK-DAG: declare {{.*}} @"_swift_FORCE_LOAD_$_swiftCompatibilityConcurrency"
+// CHECK-NOT: @"_swift_FORCE_LOAD_$_swiftCompatibility50"
+// CHECK-NOT: @"_swift_FORCE_LOAD_$_swiftCompatibility51"
+// CHECK-NOT: @"_swift_FORCE_LOAD_$_swiftCompatibility52"
+// CHECK-NOT: @"_swift_FORCE_LOAD_$_swiftCompatibility53"
+// CHECK-NOT: @"_swift_FORCE_LOAD_$_swiftCompatibilityDynamicReplacements"
+
+// CHECK-NOT: !{!"-lswiftCompatibility50"}
+// CHECK-NOT: !{!"-lswiftCompatibility51"}
+// CHECK-NOT: !{!"-lswiftCompatibility52"}
+// CHECK-NOT: !{!"-lswiftCompatibility53"}
+// CHECK-NOT: !{!"-lswiftCompatibilityDynamicReplacements"}
+// CHECK-DAG: !{!"-lswiftCompatibilityConcurrency"}
