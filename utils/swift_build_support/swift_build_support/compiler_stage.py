d0ad980931e450db6d042f6f5373d1d78e84ae7d
Michael Gottesman <mgottesman@apple.com>
2021-08-05 14:08:54 -0700

    [build-script] Implement setattr on StageArgs.
    In case people want to override staged args in funky ways.

--- utils/swift_build_support/swift_build_support/compiler_stage.py
+++ utils/swift_build_support/swift_build_support/compiler_stage.py
@@ -12,15 +12,25 @@
 
 class StageArgs(object):
     def __init__(self, stage, args):
-        self.stage = stage
-        self.args = args
+        self.__dict__['postfix'] = stage.postfix
+        self.__dict__['stage'] = stage
+        self.__dict__['args'] = args
         assert(not isinstance(self.args, StageArgs))
 
+    def _get_stage_prefix(self):
+        return self.__dict__['postfix']
+
     def __getattr__(self, key):
-        real_key = '{}{}'.format(key, self.stage.postfix)
-        if not hasattr(self.args, real_key):
+        real_key = '{}{}'.format(key, self._get_stage_prefix())
+        args = self.__dict__['args']
+        if not hasattr(args, real_key):
             return None
-        return getattr(self.args, real_key)
+        return getattr(args, real_key)
+
+    def __setattr__(self, key, value):
+        real_key = '{}{}'.format(key, self._get_stage_prefix())
+        args = self.__dict__['args']
+        setattr(args, real_key, value)
 
 
 class Stage(object):
