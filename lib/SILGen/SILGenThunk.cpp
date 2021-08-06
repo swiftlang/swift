cef64c9c44ecdf6ee95866bbbb8d4c72611973c2
Nate Chandler <nate_chandler@apple.com>
2021-08-06 08:08:58 -0700

    [SILGen] Fixed handler formal arg type indexing.
    In the synthesized completion handler that is passed to ObjC methods
    that are called as async, the formal type of each argument is the
    corresponding parameter of the formal type of the block.  The non-error,
    non-index arguments need to be prepared so that they can be used to
    fulfill the continuation; the lambda which does that preparation for
    each such argument takes the formal type of that argument.  As such, the
    call to that lambda needs to pass the type of the corresponding
    parameter of the formal type of the block to that lambda.  Doing so
    entails skipping over the error and flag parameters if they appear
    before some of the non-error, non-index arguments.
    
    Previously, no parameters were skipped over.  Consequently, when an
    error or flag argument preceded one of the non-error, non-index
    arguments, the wrong formal type was passed to the preparation lambda.
    
    Here, that is fixed by passing the correct index.  The to-be-used
    indices for the formal block parameters are the same as the to-be-used
    indices for the lowered block parameters minus one reflecting the fact
    that the lowered block always has an initial block_storage parameter as
    the first argument which the formal type never has.
    
    rdar://81617749

--- lib/SILGen/SILGenThunk.cpp
+++ lib/SILGen/SILGenThunk.cpp
@@ -408,6 +408,15 @@ SILFunction *SILGenModule::getOrCreateForeignAsyncCompletionHandlerImplFunction(
             continue;
           paramIndices.push_back(index);
         }
+        auto blockParamIndex = [paramIndices](unsigned long i) {
+          // The non-error, non-flag block parameter (formal types of the
+          // completion handler's arguments) indices are the same as the the
+          // parameter (lowered types of the completion handler's arguments)
+          // indices but shifted by 1 corresponding to the fact that the lowered
+          // completion handler has a block_storage argument but the formal type
+          // does not.
+          return paramIndices[i] - 1;
+        };
         if (auto resumeTuple = dyn_cast<TupleType>(resumeType)) {
           assert(paramIndices.size() == resumeTuple->getNumElements());
           assert(params.size() == resumeTuple->getNumElements()
@@ -421,7 +430,8 @@ SILFunction *SILGenModule::getOrCreateForeignAsyncCompletionHandlerImplFunction(
                 F->mapTypeIntoContext(resumeTuple.getElementTypes()[i])
                     ->getCanonicalType(),
                 /*arg*/ params[paramIndices[i]],
-                /*argFormalType*/ blockParams[i].getParameterType());
+                /*argFormalType*/
+                blockParams[blockParamIndex(i)].getParameterType());
           }
         } else {
           assert(paramIndices.size() == 1);
@@ -430,7 +440,8 @@ SILFunction *SILGenModule::getOrCreateForeignAsyncCompletionHandlerImplFunction(
                           /*destFormalType*/
                           F->mapTypeIntoContext(resumeType)->getCanonicalType(),
                           /*arg*/ params[paramIndices[0]],
-                          /*argFormalType*/ blockParams[0].getParameterType());
+                          /*argFormalType*/
+                          blockParams[blockParamIndex(0)].getParameterType());
         }
         
         // Resume the continuation with the composed bridged result.
