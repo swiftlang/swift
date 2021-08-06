624043532c7eb56a6429e90a833874f0f095d15f
Ben Barham <ben_barham@apple.com>
2021-08-06 13:00:47 +1000

    [Serialization] Properly skip invalid destructors when allowing errors
    7856f2d83da7fc213fb3c16676cd953b8e9ff369 only partially skipped writing
    out destructors when they were invalid, ie. it skipped writing the decl
    itself but not the records common to all decls. This would cause any
    records on the destructor to be applied on the next serialized decl.
    
    Make sure to skip serializing anything to do with the destructor when
    it's invalid and does not have a class context.

---
+++ test/Serialization/AllowErrors/invalid-deinit.swift
@@ -0,0 +1,21 @@
+// RUN: %empty-directory(%t)
+
+// Serialize and deserialize a deinit with SourceFile context to make sure we
+// don't crash
+// RUN: %target-swift-frontend -verify -module-name errors -emit-module -o %t/errors.swiftmodule -experimental-allow-module-with-compiler-errors %s
+// RUN: %target-swift-ide-test -print-module -module-to-print=errors -source-filename=x -I %t -allow-compiler-errors
+
+// Also check it wasn't serialized
+// RUN: llvm-bcanalyzer -dump %t/errors.swiftmodule | %FileCheck %s
+// CHECK-NOT: DESTRUCTOR_DECL
+
+struct Foo {}
+
+@discardableResult // expected-error{{'@discardableResult' attribute cannot be applied to this declaration}}
+deinit {} // expected-error{{deinitializers may only be declared within a class}}
+
+func foo() -> Foo { return Foo() }
+
+// Make sure @discardableResult isn't added to `foo`, which could be possible
+// if the deinit is partially serialized
+foo() // expected-warning{{result of call to 'foo()' is unused}}
