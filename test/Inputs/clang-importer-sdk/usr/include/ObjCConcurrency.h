03e175a13ecf13771c9a0b63bf93eb319693a61d
Nate Chandler <nate_chandler@apple.com>
2021-08-06 12:45:31 -0700

    [Test] Added for call-as-async Optional<NS_EXTENSIBLE_STRING_ENUM>.
    The underlying problem was fixed by the change for rdar://81590807 .
    Add tests for the specific case that was originally reported.
    
    rdar://80704382

--- test/Inputs/clang-importer-sdk/usr/include/ObjCConcurrency.h
+++ test/Inputs/clang-importer-sdk/usr/include/ObjCConcurrency.h
@@ -6,6 +6,9 @@
 #define MAIN_ACTOR __attribute__((__swift_attr__("@MainActor")))
 #define MAIN_ACTOR_UNSAFE __attribute__((__swift_attr__("@_unsafeMainActor")))
 
+#define NS_EXTENSIBLE_STRING_ENUM __attribute__((swift_wrapper(struct)));
+typedef NSString *Flavor NS_EXTENSIBLE_STRING_ENUM;
+
 @protocol ServiceProvider
 @property(readonly) NSArray<NSString *> *allOperations;
 -(void)allOperationsWithCompletionHandler:(void (^)(NSArray<NSString *> *))completion;
@@ -88,6 +91,8 @@ typedef void (^CompletionHandler)(NSString * _Nullable, NSString * _Nullable_res
                                                      NSError *_Nullable,
                                                      BOOL))completionHandler
     __attribute__((swift_async_error(zero_argument, 3)));
+- (void)getIceCreamFlavorWithCompletionHandler:
+    (void (^)(Flavor flavor, NSError *__nullable error))completionHandler;
 @end
 
 @protocol RefrigeratorDelegate<NSObject>
