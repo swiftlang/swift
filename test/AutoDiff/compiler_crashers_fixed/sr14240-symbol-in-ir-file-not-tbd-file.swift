9cbf8c939dcabd74cce0f7749565d022e47823c0
Brad Larson <larson@sunsetlakesoftware.com>
2021-08-11 16:46:20 -0600

    Adding fix for handling Clang-imported names, along with test case.

---
+++ test/AutoDiff/compiler_crashers_fixed/sr14240-symbol-in-ir-file-not-tbd-file.swift
@@ -0,0 +1,24 @@
+// RUN: %target-run-simple-swift(-Xfrontend -requirement-machine=off)
+
+// SR-14240: Error: symbol 'powTJfSSpSr' (powTJfSSpSr) is in generated IR file,
+// but not in TBD file.
+
+import _Differentiation
+
+#if canImport(Darwin)
+  import Darwin
+#elseif canImport(Glibc)
+  import Glibc
+#elseif os(Windows)
+  import CRT
+#else
+#error("Unsupported platform")
+#endif
+
+@inlinable
+@derivative(of: pow)
+func powVJP(
+  _ base: Double, _ exponent: Double
+) -> (value: Double, pullback: (Double) -> (Double, Double)) {
+  fatalError()
+}
