af05377afafc8aaaae00bbaa2d64ae14978069e1
Slava Pestov <spestov@apple.com>
2021-08-12 21:45:49 -0400

    RequirementMachine: Use trie to find overlapping rules

--- test/Generics/unify_associated_types.swift
+++ test/Generics/unify_associated_types.swift
@@ -24,8 +24,8 @@ struct MergeTest<G : P1a & P2a> {}
 // CHECK-LABEL: Rewrite system: {
 // CHECK: - τ_0_0.[P2a:T] => τ_0_0.[P1a&P2a:T]
 // CHECK: - τ_0_0.[P1a:T] => τ_0_0.[P1a&P2a:T]
-// CHECK: - τ_0_0.[P1a&P2a:T].[P1:X] => τ_0_0.[P1a&P2a:T].[P1&P2:X]
 // CHECK: - [P1a&P2a:T].[P2:X] => [P1a&P2a:T].[P1&P2:X]
+// CHECK: - τ_0_0.[P1a&P2a:T].[P1:X] => τ_0_0.[P1a&P2a:T].[P1&P2:X]
 // CHECK: - [P1a&P2a:T].[P1:X] => [P1a&P2a:T].[P1&P2:X]
 // CHECK: }
 // CHECK: Property map: {
