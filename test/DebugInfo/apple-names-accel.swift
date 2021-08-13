4445bc6fcfe47802636dba233b493373db964178
Adrian Prantl <aprantl@apple.com>
2021-08-13 14:35:04 -0700

    Update test to use dwarfdump instead of readobj

--- test/DebugInfo/apple-names-accel.swift
+++ test/DebugInfo/apple-names-accel.swift
@@ -1,7 +1,7 @@
 // Check that the apple-names section is emitted on Darwin.
 // Adapted from llvm/test/DebugInfo/X86/debugger-tune.ll
 // RUN: %target-swiftc_driver -emit-object -g %s -o %t
-// RUN: llvm-readobj -sections %t | %FileCheck --check-prefix=CHECK-LLDB %s
+// RUN: %llvm-dwarfdump -a %t | %FileCheck --check-prefix=CHECK-LLDB %s
 
 // CHECK-LLDB-NOT: debug_pubnames
 // CHECK-LLDB:     apple_names
