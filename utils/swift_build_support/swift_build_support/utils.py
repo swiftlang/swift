c86e3af4546d3376d8b79c3eb709c8386d040f09
Michael Gottesman <mgottesman@apple.com>
2021-08-15 13:58:57 -0700

    [build-script] Refactor out BuildScriptInvocation into its own file and out of the top level build-script file.
    This shrinks the number of lines in build-script by 50% and just makes it easier
    to read. It will also enable me to refactor parts of it into pieces without
    touching the main build-script file. This is really library code... build-script
    itself doesn't care about how BuildScriptInvocation is actually implemented
    under the hood.

---
+++ utils/swift_build_support/swift_build_support/utils.py
@@ -0,0 +1,32 @@
+# ===-- utils.py ---------------------------------------------------------===#
+#
+# This source file is part of the Swift.org open source project
+#
+# Copyright (c) 2014 - 2021 Apple Inc. and the Swift project authors
+# Licensed under Apache License v2.0 with Runtime Library Exception
+#
+# See https:#swift.org/LICENSE.txt for license information
+# See https:#swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+#
+# ===---------------------------------------------------------------------===#
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+import sys
+
+
+def fatal_error(message, stream=sys.stderr):
+    """Writes a message to the given stream and exits. By default this
+    function outputs to stderr.
+    """
+
+    stream.write('[{}] ERROR: {}\n'.format(sys.argv[0], message))
+    stream.flush()
+    sys.exit(1)
+
+
+def exit_rejecting_arguments(message, parser=None):
+    print(message, file=sys.stderr)
+    if parser:
+        parser.print_usage(sys.stderr)
+    sys.exit(2)  # 2 is the same as `argparse` error exit code.
