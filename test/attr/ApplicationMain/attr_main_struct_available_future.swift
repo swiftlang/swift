4a3fbb01850d0140e8406e7adc8675d18608df12
Minhyuk Kim <kimminhyuk1004@gmail.com>
2021-08-16 15:58:31 +0900

    Modify diagnostics to suggest updating existing available attribute

--- test/attr/ApplicationMain/attr_main_struct_available_future.swift
+++ test/attr/ApplicationMain/attr_main_struct_available_future.swift
@@ -3,8 +3,7 @@
 // REQUIRES: OS=macosx
 
 @main // expected-error {{'main()' is only available in macOS 10.99 or newer}}
-@available(OSX 10.0, *)
-struct EntryPoint {
+@available(OSX 10.0, *) struct EntryPoint { // expected-note {{update existing @available attribute}} {{16-20=10.99}} 
   @available(OSX 10.99, *)
   static func main() {
   }
