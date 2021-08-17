232b538082f17f1ddb7e3b61a7b7847bec67a9e5
Alastair Houghton <ahoughton@apple.com>
2021-08-17 14:26:09 +0100

    [Tests] Fix opaque return type test to work on iOS et al.
    Needed to expand the availability checks for other platforms.

--- test/TypeRoundTrip/Inputs/testcases/opaque_return_type.swift
+++ test/TypeRoundTrip/Inputs/testcases/opaque_return_type.swift
@@ -3,24 +3,24 @@ import RoundTrip
 protocol P {}
 extension Int: P {}
 
-@available(macOS 10.15, *)
+@available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
 func foo() -> some P { return 0 }
 
-@available(macOS 10.15, *)
+@available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
 var prop: some P { return 0 }
 
-@available(macOS 10.15, *)
+@available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
 func bar() -> some Sequence { return [] }
 
 struct G<T> {}
 
 extension G where T == Int {
-  @available(macOS 10.15, *)
+  @available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
   var baz: some P { return 0 }
 }
 
 public func test() {
-  if #available(macOS 10.15, *) {
+  if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *) {
     roundTripType(type(of:foo))
     roundTripType(type(of:prop))
     roundTripType(type(of:bar))
