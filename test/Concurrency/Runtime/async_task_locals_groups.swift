323fbdbbab73219f1361b9ee4b89d12b7d3db3db
Hamish Knight <hamish_github@mediocremail.com>
2021-08-18 21:44:13 +0100

    [test] Disable async_task_locals_groups.swift
    rdar://82092187

--- test/Concurrency/Runtime/async_task_locals_groups.swift
+++ test/Concurrency/Runtime/async_task_locals_groups.swift
@@ -1,5 +1,6 @@
 // RUN: %target-run-simple-swift( -Xfrontend -disable-availability-checking -parse-as-library %import-libdispatch) | %FileCheck %s
 
+// REQUIRES: rdar82092187
 // REQUIRES: executable_test
 // REQUIRES: concurrency
 // REQUIRES: libdispatch
