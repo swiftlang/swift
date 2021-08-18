3d8fcf20f3214d6ad3dd154da437aa8eae940b47
Xi Ge <xi_ge@apple.com>
2021-08-18 15:54:56 -0700

    ABIChecker: include import decls in the intermediate JSON file
    Removing an import statement can be potentially source-breaking. We should
    prepare for diagnosing such case.

--- lib/DriverTool/swift_api_digester_main.cpp
+++ lib/DriverTool/swift_api_digester_main.cpp
@@ -1105,6 +1105,7 @@ public:
     case SDKNodeKind::DeclAccessor:
     case SDKNodeKind::DeclConstructor:
     case SDKNodeKind::DeclTypeAlias:
+    case SDKNodeKind::DeclImport:
     case SDKNodeKind::TypeFunc:
     case SDKNodeKind::TypeNominal:
     case SDKNodeKind::TypeAlias: {
