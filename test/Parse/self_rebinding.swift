c928d095bb90a811307f0f506e4245112c2c569b
Jack March <jackgeorgemarch@Gmail.com>
2021-08-20 22:14:21 +0100

    test cases for SR-4559 (unapplied reference to method named "self" can be used without qualification)
    https://bugs.swift.org/browse/SR-4559

--- test/Parse/self_rebinding.swift
+++ test/Parse/self_rebinding.swift
@@ -58,3 +58,61 @@ class MyCls {
         something() // this should still refer `MyCls.something`.
     }
 }
+
+// MARK: fix method called 'self' can be confused with regular 'self' https://bugs.swift.org/browse/SR-4559
+
+func funcThatReturnsSomething(_ any: Any) -> Any {
+    any
+}
+
+struct TypeWithSelfMethod {
+    
+    let property = self // expected-warning {{'self' refers to the method 'TypeWithSelfMethod.self', which may be unexpected}} expected-note{{use 'TypeWithSelfMethod.self' to silence this warning}} {{20-20=TypeWithSelfMethod.}}
+    
+    // Existing warning expected, not confusable
+    let property2 = self() // expected-error {{cannot use instance member 'self' within property initializer; property initializers run before 'self' is available}}
+    
+    let propertyFromClosure: () = {
+        print(self) // expected-warning {{'self' refers to the method 'TypeWithSelfMethod.self', which may be unexpected}} expected-note{{use 'TypeWithSelfMethod.self' to silence this warning}} {{15-15=TypeWithSelfMethod.}}
+    }()
+    
+    let propertyFromFunc = funcThatReturnsSomething(self) // expected-warning {{'self' refers to the method 'TypeWithSelfMethod.self', which may be unexpected}} expected-note{{use 'TypeWithSelfMethod.self' to silence this warning}} {{53-53=TypeWithSelfMethod.}}
+    
+    let propertyFromFunc2 = funcThatReturnsSomething(TypeWithSelfMethod.self) // OK
+    
+    func `self`() {
+        
+    }
+}
+
+/// Test fix_unqualified_access_member_named_self doesn't appear for computed var called `self`
+/// it can't currently be referenced as a static member -- unlike a method with the same name
+struct TypeWithSelfComputedVar {
+    
+    let property = self // expected-error {{cannot use instance member 'self' within property initializer; property initializers run before 'self' is available}}
+    
+    let propertyFromClosure: () = {
+        print(self) // expected-error {{cannot use instance member 'self' within property initializer; property initializers run before 'self' is available}}
+    }()
+    
+    let propertyFromFunc = funcThatReturnsSomething(self) // expected-error {{cannot use instance member 'self' within property initializer; property initializers run before 'self' is available}}
+    
+    var `self`: () {
+        ()
+    }
+}
+
+/// Test fix_unqualified_access_member_named_self doesn't appear appear for property called `self`
+/// it can't currently be referenced as a static member -- unlike a method with the same name
+struct TypeWithSelfProperty {
+    
+    let property = self // expected-error {{cannot use instance member 'self' within property initializer; property initializers run before 'self' is available}}
+    
+    let propertyFromClosure: () = {
+        print(self) // expected-error {{cannot use instance member 'self' within property initializer; property initializers run before 'self' is available}}
+    }()
+    
+    let propertyFromFunc = funcThatReturnsSomething(self) // expected-error {{cannot use instance member 'self' within property initializer; property initializers run before 'self' is available}}
+    
+    let `self`: () = ()
+}
