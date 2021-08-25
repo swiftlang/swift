3e9395829e8ce1fc66b0d847e5f2c1f6990c6b27
Alastair Houghton <ahoughton@apple.com>
2021-08-25 16:38:43 +0100

    [Tests] Disable RemoteMirror/interop on use_os_stdlib builds.
    Even after the previous fix, this still isn't working on use_os_stdlib
    builds.  Just mark those as unsupported for now.
    
    rdar://82124292

--- test/RemoteMirror/interop.swift
+++ test/RemoteMirror/interop.swift
@@ -1,5 +1,7 @@
 // REQUIRES: OS=macosx
 // REQUIRES: executable_test
+// UNSUPPORTED: use_os_stdlib
+// UNSUPPORTED: back_deployment_runtime
 
 // RUN: %empty-directory(%t)
 // RUN: %target-build-swift %S/Inputs/interop.swift -emit-library -module-name InteropTest -o %t/%target-library-name(InteropTest)
