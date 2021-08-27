4996857929d40c9c24ba1b3338bc56be37290c21
Yuta Saito <kateinoigakukun@gmail.com>
2021-08-27 08:50:07 -0700

    Concurrency: fix calling-conv mismatch violation
    This is a temporary solution until cdecl can be allowed on function
    declaration without body.

--- stdlib/public/Concurrency/AsyncStream.cpp
+++ stdlib/public/Concurrency/AsyncStream.cpp
@@ -10,28 +10,29 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include "swift/Runtime/Config.h"
 #include "swift/Runtime/Mutex.h"
 
 namespace swift {
 // return the size in words for the given mutex primitive
-extern "C"
+extern "C" SWIFT_CC(swift)
 size_t _swift_async_stream_lock_size() {
   size_t words = sizeof(MutexHandle) / sizeof(void *);
   if (words < 1) { return 1; }
   return words;
 }
 
-extern "C"
+extern "C" SWIFT_CC(swift)
 void _swift_async_stream_lock_init(MutexHandle &lock) {
   MutexPlatformHelper::init(lock);
 }
 
-extern "C"
+extern "C" SWIFT_CC(swift)
 void _swift_async_stream_lock_lock(MutexHandle &lock) {
   MutexPlatformHelper::lock(lock);
 }
 
-extern "C"
+extern "C" SWIFT_CC(swift)
 void _swift_async_stream_lock_unlock(MutexHandle &lock) {
   MutexPlatformHelper::unlock(lock);
 }
