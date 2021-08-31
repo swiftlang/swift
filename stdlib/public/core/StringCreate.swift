52f9c77560b5e95e86c6642413aaf16cbe2f10dd
David Smith <david_smith@apple.com>
2021-08-31 08:50:16 -0700

    Revert "Remove unnecessary _fixLifetime"
    This reverts commit ce17877d50dcd5aefcaba7a84bb630daa92ba955.

--- stdlib/public/core/StringCreate.swift
+++ stdlib/public/core/StringCreate.swift
@@ -124,6 +124,7 @@ extension String {
       // we use it directly. This could be a little faster, but this error path
       // shouldn't be perf-sensitive anyway.
       let temporaryCodeUnits = Array(result.codeUnits)
+      defer { _fixLifetime(temporaryCodeUnits) }
       return temporaryCodeUnits.withContiguousStorageIfAvailable {
         //This could be optimized to use excess tail capacity
         return repairUTF8($0, firstKnownBrokenRange: initialRange)
