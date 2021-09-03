675e70b093efc203344da2daa3f0762fb196c150
Xi Ge <xi_ge@apple.com>
2021-09-03 16:14:59 -0700

    ABIChecker: don't fail when hitting EnumCaseDecl
    All enum elments should have been handled separately so we don't need to anything special
    for EnumCaseDecl except consuming them.

--- test/ModuleInterface/emit-abi-descriptor.swift
+++ test/ModuleInterface/emit-abi-descriptor.swift
@@ -2,6 +2,7 @@
 // RUN: %empty-directory(%t/Foo.swiftmodule)
 // RUN: %empty-directory(%t/ResourceDir/%target-sdk-name/prebuilt-modules/Foo.swiftmodule)
 // RUN: echo "public func foo() {}" > %t/Foo.swift
+// RUN: echo "public enum DisplayStyle { case tuple, optional, collection }" > %t/Foo.swift
 
 // RUN: %target-swift-frontend -emit-module %t/Foo.swift -module-name Foo -emit-module-interface-path %t/Foo.swiftinterface
 // RUN: %target-swift-frontend -compile-module-from-interface %t/Foo.swiftinterface -o %t/Foo.swiftmodule -module-name Foo -emit-abi-descriptor-path %t/Foo.json
