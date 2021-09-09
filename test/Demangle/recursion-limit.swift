bd31af0902fb3364e04c1306017b4c884f46c240
Alastair Houghton <ahoughton@apple.com>
2021-09-09 09:57:07 +0100

    [Demangling] Use basic syntax regex for sed.
    Sed on our Windows builders appears to not support extended regex syntax.
    Attempt fix by using basic syntax instead.  Also, add a '-e'.
    
    rdar://79725187

--- test/Demangle/recursion-limit.swift
+++ test/Demangle/recursion-limit.swift
@@ -3,8 +3,8 @@
 RUN: swift-demangle < %S/Inputs/bigtype.txt 2>&1 > %t.check
 RUN: %diff -u %S/Inputs/bigtype-demangle.txt %t.check
 
-RUN: swift-demangle -remangle-new < %S/Inputs/bigtype.txt 2>&1 | sed -E 's/\(([0-9]+):[0-9]+\)/(\1:xxx)/g' > %t.check 2>&1 || true
+RUN: swift-demangle -remangle-new < %S/Inputs/bigtype.txt 2>&1 | sed -e 's/(\([0-9]\{1,\}\):[0-9]\{1,\})/(\1:xxx)/g' > %t.check 2>&1 || true
 RUN: %diff -u %S/Inputs/bigtype-remangle.txt %t.check
 
-RUN: swift-demangle -remangle-objc-rt < %S/Inputs/bigtype.txt 2>&1 | sed -E 's/\(([0-9]+):[0-9]+\)/(\1:xxx)/g' > %t.check || true
+RUN: swift-demangle -remangle-objc-rt < %S/Inputs/bigtype.txt 2>&1 | sed -e 's/(\([0-9]\{1,\}\):[0-9]\{1,\})/(\1:xxx)/g' > %t.check || true
 RUN: %diff -u %S/Inputs/bigtype-objcrt.txt %t.check
