0309cfa3d6405847ff6c5d4cb94101cc09ed9b5b
Slava Pestov <spestov@apple.com>
2021-09-09 23:39:20 -0400

    RequirementMachine: Reduction order on symbols should compare associated type name before protocols
    The canonical order on associated types compares the name before the
    protocol, so for example T.[P2:A] < T.[P1:B]. Make sure the reduction
    order does the same so that we correctly compute canonical types in
    this case.

---
+++ test/Generics/associated_type_order.swift
@@ -0,0 +1,17 @@
+// RUN: %target-swift-emit-silgen %s -requirement-machine=on | %FileCheck %s
+
+protocol P1 {
+  associatedtype B
+}
+
+protocol P2 {
+  associatedtype A
+}
+
+// Make sure that T.[P1:A] < T.[P2:B].
+
+struct G<T : P1 & P2> where T.A == T.B {
+  // CHECK-LABEL: sil hidden [ossa] @$s21associated_type_order1GV3fooyy1AAA2P2PQzF : $@convention(method) <T where T : P1, T : P2, T.A == T.B> (@in_guaranteed T.A, G<T>) -> () {
+  func foo(_: T.A) {}
+}
+
