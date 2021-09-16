948e36fd6b94c312c916c816265b33cea75832ae
Kuba Mracek <mracek@apple.com>
2021-09-16 11:50:17 -0700

    Split the NO_DYLD CMake flag into separate HAS_DLADDR and STATIC_IMAGE_INSPECTION flags

--- stdlib/public/runtime/MetadataLookup.cpp
+++ stdlib/public/runtime/MetadataLookup.cpp
@@ -1020,7 +1020,7 @@ _gatherGenericParameters(const ContextDescriptor *context,
 
       str += "_gatherGenericParameters: context: ";
 
-#if !defined(SWIFT_RUNTIME_MACHO_NO_DYLD)
+#if SWIFT_STDLIB_HAS_DLADDR
       SymbolInfo contextInfo;
       if (lookupSymbol(context, &contextInfo)) {
         str += contextInfo.symbolName.get();
