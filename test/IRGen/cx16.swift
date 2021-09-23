872f9712d292e5e2544a17b63b277b0e871b03f1
Karoy Lorentey <klorentey@apple.com>
2021-09-22 21:24:16 -0700

    [test] IRGen/cx16: Don't load legacy type info

--- test/IRGen/cx16.swift
+++ test/IRGen/cx16.swift
@@ -1,7 +1,7 @@
-// RUN: %target-swift-frontend -target x86_64-unknown-linux-gnu -parse-stdlib -disable-objc-interop %s -module-name main -emit-ir -o - | %FileCheck %s
-// RUN: %target-swift-frontend -target x86_64-unknown-windows-msvc -parse-stdlib -disable-objc-interop %s -module-name main -emit-ir -o - | %FileCheck %s
-// RUN: %target-swift-frontend -target x86_64-unknown-freebsd -parse-stdlib -disable-objc-interop %s -module-name main -emit-ir -o - | %FileCheck %s
-// RUN: %target-swift-frontend -target x86_64-apple-macosx10.9 -parse-stdlib -module-name main %s -emit-ir -o - | %FileCheck %s
+// RUN: %target-swift-frontend -target x86_64-unknown-linux-gnu -disable-legacy-type-info -parse-stdlib -disable-objc-interop %s -module-name main -emit-ir -o - | %FileCheck %s
+// RUN: %target-swift-frontend -target x86_64-unknown-windows-msvc -disable-legacy-type-info -parse-stdlib -disable-objc-interop %s -module-name main -emit-ir -o - | %FileCheck %s
+// RUN: %target-swift-frontend -target x86_64-unknown-freebsd -disable-legacy-type-info -parse-stdlib -disable-objc-interop %s -module-name main -emit-ir -o - | %FileCheck %s
+// RUN: %target-swift-frontend -target x86_64-apple-macosx10.9 -disable-legacy-type-info -parse-stdlib -module-name main %s -emit-ir -o - | %FileCheck %s
 
 // REQUIRES: CODEGENERATOR=X86
 
