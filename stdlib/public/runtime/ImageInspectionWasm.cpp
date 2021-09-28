a221ab18f5878a9db22650b8d6c53f03172e8782
Max Desiatov <max@desiatov.com>
2021-09-28 14:43:55 +0100

    runtime, WebAssembly: add ImageInspectionWasm.cpp
    This change adds a stub for `swift::lookupSymbol` for the WebAssembly platform.
    
    <!-- If this pull request resolves any bugs in the Swift bug tracker, provide a link: -->
    Related to SR-9307.

---
+++ stdlib/public/runtime/ImageInspectionWasm.cpp
@@ -0,0 +1,30 @@
+//===--- ImageInspectionStatic.cpp - image inspection for static stdlib ---===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2021 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+///
+/// Implementation of ImageInspection for WebAssembly.
+///
+//===----------------------------------------------------------------------===//
+
+#if defined(__wasm__)
+
+#include "../SwiftShims/MetadataSections.h"
+#include "ImageInspection.h"
+
+using namespace swift;
+
+int swift::lookupSymbol(const void *address, SymbolInfo *info) {
+  return 0;
+}
+
+#endif // defined(__wasm__)
