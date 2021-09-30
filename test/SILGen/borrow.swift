b63cc13ebb02c3e7d62b2a8b2b4a9b11192aec8a
Nate Chandler <nate_chandler@apple.com>
2021-09-30 16:49:08 -0700

    [SILGen] Test: Moved tests for lexical lifetimes.
    Previously, tests for the generation of lexical borrow scopes when
    -enable-experimental-lexical-lifetimes is passed had been dumped into
    the file testing the creation of all borrow scopes.  Here, the file
    containing tests on general borrow scopes is reverted to its original
    state and a new file dedicated to lexical lifetimes is added.
    Subsequent tests for SILGen's creation of lexical lifetimes will go into
    that new file.

--- test/SILGen/borrow.swift
+++ test/SILGen/borrow.swift
@@ -1,5 +1,5 @@
 
-// RUN: %target-swift-emit-silgen -enable-experimental-lexical-lifetimes -module-name borrow -parse-stdlib %s | %FileCheck %s
+// RUN: %target-swift-emit-silgen -module-name borrow -parse-stdlib %s | %FileCheck %s
 
 import Swift
 
@@ -13,7 +13,6 @@ final class C {
   var d: D = D()
 }
 
-func use<T>(_ t: T) {}
 func useD(_ d: D) {}
 
 // CHECK-LABEL: sil hidden [ossa] @$s6borrow44lvalueBorrowShouldBeAtEndOfFormalAccessScope{{.*}} : $@convention(thin) () -> () {
@@ -35,59 +34,4 @@ func useD(_ d: D) {}
 func lvalueBorrowShouldBeAtEndOfFormalAccessScope() {
   var c = C()
   useD(c.d)
-}
-
-// CHECK-LABEL: sil hidden [ossa] @lexical_borrow_let_class
-// CHECK:   [[INIT_C:%[^,]+]] = function_ref @$s6borrow1CCACycfC
-// CHECK:   [[INSTANCE:%[^,]+]] = apply [[INIT_C]]({{%[0-9]+}})
-// CHECK:   [[BORROW:%[^,]+]] = begin_borrow [lexical] [[INSTANCE]] : $C
-// CHECK:   end_borrow [[BORROW:%[^,]+]]
-// CHECK-LABEL: } // end sil function 'lexical_borrow_let_class'
-@_silgen_name("lexical_borrow_let_class")
-func lexical_borrow_let_class() {
-  let c = C()
-}
-
-// CHECK-LABEL: sil hidden [ossa] @lexical_borrow_if_let_class
-// CHECK:   [[INIT_C:%[^,]+]] = function_ref @$s6borrow1CC8failablyACSgyt_tcfC
-// CHECK:   [[INSTANCE:%[^,]+]] = apply [[INIT_C]]({{%[^,]+}})
-// CHECK:   switch_enum [[INSTANCE]] : $Optional<C>, case #Optional.some!enumelt: [[BASIC_BLOCK2:bb[^,]+]], case #Optional.none!enumelt: {{bb[^,]+}}
-// CHECK: [[BASIC_BLOCK2]]([[INSTANCE:%[^,]+]] : @owned $C):
-// CHECK:   [[BORROW:%[^,]+]] = begin_borrow [lexical] [[INSTANCE]] : $C
-// CHECK:   end_borrow [[BORROW]] : $C
-// CHECK-LABEL: // end sil function 'lexical_borrow_if_let_class'
-@_silgen_name("lexical_borrow_if_let_class")
-func lexical_borrow_if_let_class() {
-  if let c = C(failably: ()) {
-    use(())
-  }
-}
-
-struct S {
-  let c: C
-}
-
-// CHECK-LABEL: sil hidden [ossa] @lexical_borrow_let_class_in_struct
-// CHECK:   [[INIT_S:%[^,]+]] = function_ref @$s6borrow1SV1cAcA1CC_tcfC
-// CHECK:   [[INSTANCE:%[^,]+]] = apply [[INIT_S]]({{%[0-9]+}}, {{%[0-9]+}})
-// CHECK:   [[BORROW:%[^,]+]] = begin_borrow [lexical] [[INSTANCE]] : $S
-// CHECK:   end_borrow [[BORROW:%[^,]+]]
-// CHECK-LABEL: } // end sil function 'lexical_borrow_let_class_in_struct'
-@_silgen_name("lexical_borrow_let_class_in_struct")
-func lexical_borrow_let_class_in_struct() {
-  let s = S(c: C())
-}
-
-enum E {
-  case e(C)
-}
-
-// CHECK-LABEL: sil hidden [ossa] @lexical_borrow_let_class_in_enum
-// CHECK:   [[INSTANCE:%[^,]+]] = enum $E, #E.e!enumelt, {{%[0-9]+}} : $C
-// CHECK:   [[BORROW:%[^,]+]] = begin_borrow [lexical] [[INSTANCE]] : $E
-// CHECK:   end_borrow [[BORROW:%[^,]+]]
-// CHECK-LABEL: } // end sil function 'lexical_borrow_let_class_in_enum'
-@_silgen_name("lexical_borrow_let_class_in_enum")
-func lexical_borrow_let_class_in_enum() {
-  let s = E.e(C())
 }
