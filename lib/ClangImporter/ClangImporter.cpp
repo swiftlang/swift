ad03343f6d3f04c3ad05a485ae84a067c627f4ef
Zoe Carver <z.zoelec2@gmail.com>
2021-10-08 11:26:53 -0700

    Revert "[cxx-interop] Print decls in the __ObjC module."

--- lib/ClangImporter/ClangImporter.cpp
+++ lib/ClangImporter/ClangImporter.cpp
@@ -2621,10 +2621,7 @@ public:
 
   void foundDecl(ValueDecl *VD, DeclVisibilityKind Reason,
                  DynamicLookupInfo dynamicLookupInfo) override {
-    if (isDeclaredInModule(ModuleFilter, VD) ||
-        // Sometimes imported decls get put into the clang header module. If we
-        // found one of these decls, don't filter it out.
-        VD->getModuleContext()->getName().str() == CLANG_HEADER_MODULE_NAME)
+    if (isDeclaredInModule(ModuleFilter, VD))
       NextConsumer.foundDecl(VD, Reason, dynamicLookupInfo);
   }
 };
