e130fde807f72cd2686b023129feb52cc162e975
elsh <ellie_shin@apple.com>
2021-10-08 16:23:43 -0700

    Fix test for linux

--- test/Frontend/load-module-with-alias-mangling.swift
+++ test/Frontend/load-module-with-alias-mangling.swift
@@ -14,12 +14,14 @@
 // RUN: echo 'public func meow() -> Cat.Klass? { return nil }' >> %t/FileFoo.swift
 // RUN: %target-swift-frontend -module-name Foo %t/FileFoo.swift -module-alias Cat=Bar -I %t -emit-module -emit-module-path %t/Foo.swiftmodule -c -o %t/ResultFoo.o
 
-/// Check Foo.swiftmodule is created and Bar.swiftmodule is loaded
+/// Check Foo.swiftmodule is created
 // RUN: test -f %t/Foo.swiftmodule
 
+/// Check the mangled name for func meow contains the real module name Bar
 // RUN: llvm-objdump -t %t/ResultFoo.o | %FileCheck %s -check-prefix=CHECK-A
-// CHECK-A: _$s3Foo4meow3Bar5KlassCSgyF
+// CHECK-A: s3Foo4meow3Bar5KlassCSgyF
 
+/// Check demangling shows the real module name Bar
 // RUN: llvm-objdump -t %t/ResultFoo.o | swift-demangle | %FileCheck %s -check-prefix=CHECK-B
 // CHECK-B: Foo.meow() -> Bar.Klass?
 
