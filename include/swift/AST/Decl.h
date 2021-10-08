eeeb27d66e520eb0dc532724f9fd17281d601302
zoecarver <z.zoelec2@gmail.com>
2021-10-08 13:17:35 -0700

    [cxx-interop] Add members to the LookupTable where possible.
    If possible, add imported members to the StructDecl's LookupTable rather than adding them directly as members. This will fix the issues with ordering that #39436 poorly attempted to solve during IRGen.
    
    This also allows us to break out most of the test changes from #39436.

--- include/swift/AST/Decl.h
+++ include/swift/AST/Decl.h
@@ -3073,6 +3073,10 @@ class NominalTypeDecl : public GenericTypeDecl, public IterableDeclContext {
   /// Prepare to traverse the list of extensions.
   void prepareExtensions();
 
+  /// Add loaded members from all extensions. Eagerly load any members that we
+  /// can't lazily load.
+  void addLoadedExtensions();
+
   /// Retrieve the conformance loader (if any), and removing it in the
   /// same operation. The caller is responsible for loading the
   /// conformances.
@@ -3194,6 +3198,11 @@ public:
 
   /// Add a new extension to this nominal type.
   void addExtension(ExtensionDecl *extension);
+
+  /// Add a member to this decl's lookup table.
+  ///
+  /// Calls "prepareLookupTable" as a side effect.
+  void addMemberToLookupTable(Decl *member);
 
   /// Retrieve the set of extensions of this type.
   ExtensionRange getExtensions();
