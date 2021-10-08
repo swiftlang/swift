0acf7df90ba4f9d3302b146d571bcdb208d9a669
Shubham Sandeep Rastogi <srastogi22@apple.com>
2021-10-07 17:47:12 -0700

    Enabled top_level_code.swift and fixed issue with prologue.swift
    (cherry picked from commit 548598c8d51a8dffc6c4a49d607fec1221cbae08)

--- test/DebugInfo/prologue.swift
+++ test/DebugInfo/prologue.swift
@@ -7,9 +7,9 @@ func markUsed<T>(_ t: T) {}
 // CHECK: .file [[F:[0-9]+]] "{{.*}}prologue.swift"
 func bar<T, U>(_ x: T, y: U) { markUsed("bar") }
 // CHECK: $s8prologue3bar_1yyx_q_tr0_lF:
-// CHECK: .loc	[[F]] 0 0 prologue_end
+// CHECK: .loc	[[F]] 0 0 is_stmt {{[0-9]+$}}
 // Make sure there is no allocation happening between the end of
 // prologue and the beginning of the function body.
 // CHECK-NOT: callq	*
-// CHECK: .loc	[[F]] [[@LINE-6]] {{.}}
+// CHECK: .loc	[[F]] [[@LINE-6]] {{[0-9]+}} prologue_end is_stmt {{[0-9]+$}}
 // CHECK: {{callq	.*builtinStringLiteral|movq __imp_.*builtinStringLiteral}}
