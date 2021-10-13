ecbf257a96cad60852115f760b2d3a17d277dc2a
Konrad `ktoso` Malawski <konrad_malawski@apple.com>
2021-10-13 10:03:04 +0900

    [Concurrency] SR-15309: Fix instance task.isCancelled impl to use _task

--- stdlib/public/Concurrency/TaskCancellation.swift
+++ stdlib/public/Concurrency/TaskCancellation.swift
@@ -46,13 +46,7 @@ extension Task {
   ///
   /// - SeeAlso: `checkCancellation()`
   public var isCancelled: Bool {
-    withUnsafeCurrentTask { task in
-      guard let task = task else {
-        return false
-      }
-
-      return _taskIsCancelled(task._task)
-    }
+    _taskIsCancelled(_task)
   }
 }
 
