0651acea90de9c74535ae3e97e71f543e0ce1989
Kavon Farvardin <kfarvardin@apple.com>
2021-10-21 15:58:19 -0700

    update dist actor deinit SIL tests
    I've combined the coverage of two tests
    into one file.

--- test/SIL/Distributed/distributed_actor_default_deinit_sil.swift
+++ test/SIL/Distributed/distributed_actor_default_deinit_sil.swift
@@ -1,46 +1,119 @@
-// RUN: %target-swift-frontend -O -primary-file %s -emit-sil -enable-experimental-distributed | %FileCheck %s --dump-input=fail
+// RUN: %target-swift-frontend -module-name default_deinit -primary-file %s -emit-sil -enable-experimental-distributed | %FileCheck %s --enable-var-scope --dump-input=fail
 // REQUIRES: concurrency
 // REQUIRES: distributed
 
 import _Distributed
 
-@available(SwiftStdlib 5.5, *)
-distributed actor SimpleEmptyDistributedActor {
+class SomeClass {}
+
+@available(macOS 12, *)
+distributed actor MyDistActor {
+  let localOnlyField: SomeClass
+
+  init(transport: ActorTransport) {
+    self.localOnlyField = SomeClass()
+  }
 }
 
-// ==== ------------------------------------------------------------------------
-// ==== deinit must invoke transport.resignIdentity() when it is local
+// MARK: distributed actor check
 
-// CHECK: // SimpleEmptyDistributedActor.deinit
-// sil hidden [available 12.0] @$s36distributed_actor_default_deinit_sil27SimpleEmptyDistributedActorCfd : $@convention(method) (@guaranteed SimpleEmptyDistributedActor) -> @owned Builtin.NativeObject {
-// CHECK: [[SELF:%[0-9]+]] "self"
-// CHECK: bb0(%0 : $SimpleEmptyDistributedActor):
-// CHECK:  debug_value %0 : $SimpleEmptyDistributedActor, let, name "self", argno 1 
-// CHECK:  [[ID_ADDR:%[0-9]+]] = ref_element_addr %0 : $SimpleEmptyDistributedActor, #SimpleEmptyDistributedActor.id
-// CHECK:  [[TRANSPORT_ADDR:%[0-9]+]] = ref_element_addr %0 : $SimpleEmptyDistributedActor, #SimpleEmptyDistributedActor.actorTransport
-// CHECK:  %4 = init_existential_ref %0 : $SimpleEmptyDistributedActor : $SimpleEmptyDistributedActor, $AnyObject
-// CHECK:  // function_ref swift_distributed_actor_is_remote
-// CHECK:  %5 = function_ref @swift_distributed_actor_is_remote : $@convention(thin) (@guaranteed AnyObject) -> Bool
-// CHECK:  %6 = apply %5(%4) : $@convention(thin) (@guaranteed AnyObject) -> Bool
-// CHECK:  %7 = struct_extract %6 : $Bool, #Bool._value
-// CHECK:  cond_br %7, [[REMOTE_BB:bb[0-9]+]], [[LOCAL_BB:bb[0-9]+]]
+// This test checks that we resign the identity for local deallocations,
+// destroy only the correct stored properties whether remote or local, and also
+// destroy the executor.
 
-// If local...
+// CHECK-LABEL: sil hidden {{.*}} @$s14default_deinit11MyDistActorCfd : $@convention(method) (@guaranteed MyDistActor) -> @owned Builtin.NativeObject {
+// CHECK: bb0([[SELF:%[0-9]+]] : $MyDistActor):
+// CHECK:   [[EXI_SELF:%[0-9]+]] = init_existential_ref [[SELF]] : $MyDistActor
+// CHECK:   [[IS_REMOTE_FN:%[0-9]+]] = function_ref @swift_distributed_actor_is_remote
+// CHECK:   [[IS_REMOTE:%[0-9]+]] = apply [[IS_REMOTE_FN]]([[EXI_SELF]])
+// CHECK:   [[RAW_BOOL:%[0-9]+]] = struct_extract [[IS_REMOTE]] : $Bool, #Bool._value
+// CHECK:   cond_br [[RAW_BOOL]], [[REMOTE_BB:bb[0-9]+]], [[LOCAL_BB:bb[0-9]+]]
+
+// *** If local... invoke transport.resignIdentity()
 // CHECK: [[LOCAL_BB]]:
-// CHECK:  %9 = open_existential_addr immutable_access %3 : $*ActorTransport to $*@opened({{.*}}) ActorTransport
-// CHECK:  %10 = witness_method $@opened({{.*}}) ActorTransport, #ActorTransport.resignIdentity : <Self where Self : ActorTransport> (Self) -> (AnyActorIdentity) -> (), %9 : $*@opened({{.*}}) ActorTransport : $@convention(witness_method: ActorTransport) <τ_0_0 where τ_0_0 : ActorTransport> (@in_guaranteed AnyActorIdentity, @in_guaranteed τ_0_0) -> ()
-// CHECK:  %11 = apply %10<@opened({{.*}}) ActorTransport>(%2, %9) : $@convention(witness_method: ActorTransport) <τ_0_0 where τ_0_0 : ActorTransport> (@in_guaranteed AnyActorIdentity, @in_guaranteed τ_0_0) -> () 
-// CHECK:  br bb3
+// CHECK:   [[ID_REF:%[0-9]+]] = ref_element_addr [[SELF]] : $MyDistActor, #MyDistActor.id
+// CHECK:   [[TPORT_REF:%[0-9]+]] = ref_element_addr [[SELF]] : $MyDistActor, #MyDistActor.actorTransport
+// CHECK:   [[TPORT_OPENED:%[0-9]+]] = open_existential_addr immutable_access [[TPORT_REF]]
+// CHECK:   [[RESIGN:%[0-9]+]] = witness_method $@opened({{.*}}) ActorTransport, #ActorTransport.resignIdentity : <Self where Self : ActorTransport> (Self) -> (AnyActorIdentity) -> (), [[TPORT_OPENED]]
+// CHECK:   apply [[RESIGN]]<@opened({{.*}}) ActorTransport>([[ID_REF]], [[TPORT_OPENED]])
+// CHECK:   br [[CONTINUE:bb[0-9]+]]
 
-// If remote...
+// *** If remote...
 // CHECK: [[REMOTE_BB]]:
-// CHECK:  br bb3
+// CHECK:  br [[CONTINUE]]
 
-// Finish up the destroying...
-// CHECK: bb3:
-// CHECK:  destroy_addr [[TRANSPORT_ADDR]] : $*ActorTransport
-// CHECK:  destroy_addr [[ID_ADDR]] : $*AnyActorIdentity
-// CHECK:  [[_:%[0-9]+]] = builtin "destroyDefaultActor"(%0 : $SimpleEmptyDistributedActor) : $()
-// CHECK:  [[SELF:%[0-9]+]] = unchecked_ref_cast %0 : $SimpleEmptyDistributedActor to $Builtin.NativeObject
-// CHECK:  return [[SELF]] : $Builtin.NativeObject
-// CHECK: } // end sil function '$s36distributed_actor_default_deinit_sil27SimpleEmptyDistributedActorCfd'
+// Now we deallocate stored properties, and how we do that depends again on
+// being remote or local. Default code emission does another is_remote test,
+// so we check for that here and leave tail-merging to the optimizer, for now.
+// CHECK: [[CONTINUE]]:
+            // *** this is entirely copy-pasted from the first check in bb0 ***
+// CHECK:   [[EXI_SELF:%[0-9]+]] = init_existential_ref [[SELF]] : $MyDistActor
+// CHECK:   [[IS_REMOTE_FN:%[0-9]+]] = function_ref @swift_distributed_actor_is_remote
+// CHECK:   [[IS_REMOTE:%[0-9]+]] = apply [[IS_REMOTE_FN]]([[EXI_SELF]])
+// CHECK:   [[RAW_BOOL:%[0-9]+]] = struct_extract [[IS_REMOTE]] : $Bool, #Bool._value
+// CHECK:   cond_br [[RAW_BOOL]], [[REMOTE_BB_DEALLOC:bb[0-9]+]], [[LOCAL_BB_DEALLOC:bb[0-9]+]]
+
+// *** only destroy the id and transport if remote ***
+// CHECK: [[REMOTE_BB_DEALLOC]]:
+            // *** destroy transport ***
+// CHECK:   [[REF:%[0-9]+]] = ref_element_addr [[SELF]] : $MyDistActor, #MyDistActor.actorTransport
+// CHECK:   [[ACCESS:%[0-9]+]] = begin_access [deinit] [static] [[REF]]
+// CHECK:   destroy_addr [[ACCESS]] : $*ActorTransport
+// CHECK:   end_access [[ACCESS]]
+            // *** destroy identity ***
+// CHECK:   [[REF:%[0-9]+]] = ref_element_addr [[SELF]] : $MyDistActor, #MyDistActor.id
+// CHECK:   [[ACCESS:%[0-9]+]] = begin_access [deinit] [static] [[REF]]
+// CHECK:   destroy_addr [[ACCESS]] : $*AnyActorIdentity
+// CHECK:   end_access [[ACCESS]]
+// CHECK:   br [[AFTER_DEALLOC:bb[0-9]+]]
+
+// *** destroy everything if local ***
+// CHECK: [[LOCAL_BB_DEALLOC]]:
+            // *** destroy the user-defined field ***
+// CHECK:   [[REF:%[0-9]+]] = ref_element_addr [[SELF]] : $MyDistActor, #MyDistActor.localOnlyField
+// CHECK:   [[ACCESS:%[0-9]+]] = begin_access [deinit] [static] [[REF]]
+// CHECK:   destroy_addr [[ACCESS]] : $*SomeClass
+// CHECK:   end_access [[ACCESS]]
+            // *** the rest of this part is identical to the remote case ***
+// CHECK:   [[REF:%[0-9]+]] = ref_element_addr [[SELF]] : $MyDistActor, #MyDistActor.actorTransport
+// CHECK:   [[ACCESS:%[0-9]+]] = begin_access [deinit] [static] [[REF]]
+// CHECK:   destroy_addr [[ACCESS]] : $*ActorTransport
+// CHECK:   end_access [[ACCESS]]
+// CHECK:   [[REF:%[0-9]+]] = ref_element_addr [[SELF]] : $MyDistActor, #MyDistActor.id
+// CHECK:   [[ACCESS:%[0-9]+]] = begin_access [deinit] [static] [[REF]]
+// CHECK:   destroy_addr [[ACCESS]] : $*AnyActorIdentity
+// CHECK:   end_access [[ACCESS]]
+// CHECK:   br [[AFTER_DEALLOC]]
+
+// CHECK: [[AFTER_DEALLOC]]:
+// CHECK:   builtin "destroyDefaultActor"([[SELF]] : $MyDistActor)
+// CHECK:   [[CAST:%[0-9]+]] = unchecked_ref_cast [[SELF]]
+// CHECK:   return [[CAST]] : $Builtin.NativeObject
+// CHECK: } // end sil function '$s14default_deinit11MyDistActorCfd'
+
+
+// MARK: local actor check
+
+@available(macOS 12, *)
+actor SimpleActor {
+  let someField: SomeClass
+  init() {
+    self.someField = SomeClass()
+  }
+}
+
+// additionally, we add basic coverage for a non-distributed actor's deinit
+
+
+// CHECK-LABEL: sil hidden {{.*}} @$s14default_deinit11SimpleActorCfd : $@convention(method) (@guaranteed SimpleActor) -> @owned Builtin.NativeObject {
+// CHECK: bb0([[SELF:%[0-9]+]] : $SimpleActor):
+// CHECK:   [[REF:%[0-9]+]] = ref_element_addr [[SELF]] : $SimpleActor, #SimpleActor.someField
+// CHECK:   [[ACCESS:%[0-9]+]] = begin_access [deinit] [static] [[REF]]
+// CHECK:   destroy_addr [[ACCESS]] : $*SomeClass
+// CHECK:   end_access [[ACCESS]]
+// CHECK:   builtin "destroyDefaultActor"([[SELF]] : $SimpleActor)
+// CHECK:   [[CAST:%[0-9]+]] = unchecked_ref_cast [[SELF]]
+// CHECK:   return [[CAST]] : $Builtin.NativeObject
+// CHECK: } // end sil function '$s14default_deinit11SimpleActorCfd'
+
+
