b1c680db028ad55fc3cd1b9cc20a9a44ba390bd4
Nate Chandler <nate_chandler@apple.com>
2021-10-22 09:41:29 -0700

    [DNM] Enable verify all by default.
    For testing.

--- include/swift/AST/SILOptions.h
+++ include/swift/AST/SILOptions.h
@@ -110,7 +110,7 @@ public:
   bool EnablePerformanceAnnotations = false;
 
   /// Controls whether or not paranoid verification checks are run.
-  bool VerifyAll = false;
+  bool VerifyAll = true;
 
   /// If true, no SIL verification is done at all.
   bool VerifyNone = false;
