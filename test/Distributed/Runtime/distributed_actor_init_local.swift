9a76323b83d111a780f5c9910173f4363c2a7fca
Kavon Farvardin <kfarvardin@apple.com>
2021-10-21 21:14:22 -0700

    throwing sync inits are also not fully working

--- test/Distributed/Runtime/distributed_actor_init_local.swift
+++ test/Distributed/Runtime/distributed_actor_init_local.swift
@@ -10,6 +10,10 @@
 
 import _Distributed
 
+enum MyError: Error {
+  case test
+}
+
 @available(SwiftStdlib 5.5, *)
 distributed actor PickATransport1 {
   init(kappa transport: ActorTransport, other: Int) {}
@@ -33,6 +37,26 @@ distributed actor Bug_CallsReadyTwice {
       self.x = 1
     }
     self.x = 2
+  }
+}
+
+@available(SwiftStdlib 5.5, *)
+distributed actor Throwy {
+  init(transport: ActorTransport, doThrow: Bool) throws {
+    if doThrow {
+      throw MyError.test
+    }
+  }
+}
+
+@available(SwiftStdlib 5.5, *)
+distributed actor ThrowBeforeFullyInit {
+  var x: Int
+  init(transport: ActorTransport, doThrow: Bool) throws {
+    if doThrow {
+      throw MyError.test
+    }
+    self.x = 0
   }
 }
 
@@ -89,10 +113,27 @@ func test() async {
   // CHECK: ready actor:main.PickATransport1, id:AnyActorIdentity(ActorAddress(address: "[[ID]]"))
   // CHECK: resign id:AnyActorIdentity(ActorAddress(address: "[[ID]]"))
 
+  _ = try? Throwy(transport: transport, doThrow: false)
+  // CHECK: assign type:Throwy, id:ActorAddress(address: "[[ID:.*]]")
+  // CHECK: ready actor:main.Throwy, id:AnyActorIdentity(ActorAddress(address: "[[ID]]"))
+  // CHECK: resign id:AnyActorIdentity(ActorAddress(address: "[[ID]]"))
+
+  _ = try? Throwy(transport: transport, doThrow: true)
+  // CHECK: assign type:Throwy, id:ActorAddress(address: "[[ID:.*]]")
+  // CHECK-NOT: ready
+  // CHECK: resign id:AnyActorIdentity(ActorAddress(address: "[[ID]]"))
+
+  _ = try? ThrowBeforeFullyInit(transport: transport, doThrow: true)
+  // CHECK: assign type:ThrowBeforeFullyInit, id:ActorAddress(address: "[[ID:.*]]")
+  // FIXME: The two checks below should work, but do not currently, so they're disabled (rdar://84533820).
+  // MISSING-CHECK-NOT: ready actor:main.ThrowBeforeFullyInit
+  // MISSING-CHECK: resign id:AnyActorIdentity(ActorAddress(address: "[[ID]]"))
+
   _ = await PickATransport2(other: 1, theTransport: transport)
   // CHECK: assign type:PickATransport2, id:ActorAddress(address: "[[ID:.*]]")
   // CHECK: ready actor:main.PickATransport2, id:AnyActorIdentity(ActorAddress(address: "[[ID]]"))
 
+  // FIXME: The checks for this initializer should NOT pass, but currently do. (rdar://84533820)
   _ = await Bug_CallsReadyTwice(transport: transport, wantBug: true)
   // CHECK: assign type:Bug_CallsReadyTwice, id:ActorAddress(address: "[[ID:.*]]")
   // CHECK:      ready actor:main.Bug_CallsReadyTwice, id:AnyActorIdentity(ActorAddress(address: "[[ID]]"))
