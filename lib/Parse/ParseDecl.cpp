470fbc7223a76f401fb78786beb1bd0eed8af87f
Ellie Shin <ellie_shin@apple.com>
2021-10-28 23:03:33 -0700

    Update lib/Parse/ParseDecl.cpp
    Co-authored-by: Becca Royal-Gordon <beccadax@apple.com>

--- lib/Parse/ParseDecl.cpp
+++ lib/Parse/ParseDecl.cpp
@@ -4823,12 +4823,13 @@ ParserResult<ImportDecl> Parser::parseDeclImport(ParseDeclOptions Flags,
   // and check that the module alias appeared in source files instead of
   // its corresponding real name
   auto parsedModuleID = importPath.get().front().Item;
-  auto checkResult = Context.getRealModuleNameOrAlias(parsedModuleID);
-  if (!checkResult.second) {
+  if (Context.getRealModuleName(parsedModuleID).empty()) {
     // If reached here, it means the parsed module name is a real module name
     // which appeared in the source file; only a module alias should be allowed
-    diagnose(importPath.front().Loc, diag::expected_module_alias)
-      .fixItReplace(importPath.front().Loc, checkResult.first.str());
+    auto aliasName = Context.getRealModuleName(parsedModuleID, /*lookupAliasFromReal=*/true);
+    diagnose(importPath.front().Loc, diag::expected_module_alias,
+                     parsedModuleID, aliasName)
+      .fixItReplace(importPath.front().Loc, aliasName.str());
     return nullptr;
   }
 
