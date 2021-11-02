725ee55fbc884880be623bd4a7853f7c1f7aaa28
Karoy Lorentey <klorentey@apple.com>
2021-11-01 19:30:20 -0700

    [stdlib] Optimize `Set.isSuperset<S>(of: S)`
    Call into the specialized overload if the argument happens to be a `Set`.

--- stdlib/public/core/Set.swift
+++ stdlib/public/core/Set.swift
@@ -763,7 +763,10 @@ extension Set: SetAlgebra {
   ///   otherwise, `false`.
   @inlinable
   public func isSuperset<S: Sequence>(of possibleSubset: __owned S) -> Bool
-    where S.Element == Element {
+  where S.Element == Element {
+    if let s = possibleSubset as? Set<Element> {
+      return isSuperset(of: s)
+    }
     for member in possibleSubset {
       if !contains(member) {
         return false
