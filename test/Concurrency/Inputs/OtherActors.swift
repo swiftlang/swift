9bff2b43a60ca577c5c215b08b39dad8aea939d6
Kavon Farvardin <kfarvardin@apple.com>
2021-11-08 14:09:52 -0800

    fix SILGen bug for cross-module actor let accesses
    When trying to access a let-bound property of an actor across
    a moudule boundary, the operation should be treated as async.
    But, upon reaching SILGen, the compiler would crash when trying
    to emit the needed hop, because the base of the element reference
    was missing. This commit fixes that and adds regression coverage.
    
    resolves rdar://81812013

--- test/Concurrency/Inputs/OtherActors.swift
+++ test/Concurrency/Inputs/OtherActors.swift
@@ -1,7 +1,9 @@
 public class SomeClass { }
+public final class SomeSendableClass: Sendable { }
 
 public actor OtherModuleActor {
   public let a: Int = 1
   public nonisolated let b: Int = 2
   public let c: SomeClass = SomeClass()
+  public let d: SomeSendableClass = SomeSendableClass()
 }
