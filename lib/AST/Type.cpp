a15628b8b1a13c44d665a24a6664eedbaf2fa5bb
Slava Pestov <spestov@apple.com>
2021-11-08 16:31:31 -0500

    AST: Fix ReplaceOpaqueTypesWithUnderlyingTypes to handle nested types of opaque result types
    Fixes rdar://problem/85137685.

--- lib/AST/Type.cpp
+++ lib/AST/Type.cpp
@@ -3215,7 +3215,14 @@ static bool canSubstituteTypeInto(Type ty, const DeclContext *dc,
                                   bool isContextWholeModule) {
   TypeDecl *typeDecl = ty->getAnyNominal();
   if (!typeDecl) {
-    // We also need to check that the opaque type descriptor is accessible.
+    // The referenced type might be a different opaque result type.
+
+    // First, unwrap any nested associated types to get the root archetype.
+    while (auto nestedTy = ty->getAs<NestedArchetypeType>())
+      ty = nestedTy->getParent();
+
+    // If the root archetype is an opaque result type, check that its
+    // descriptor is accessible.
     if (auto opaqueTy = ty->getAs<OpaqueTypeArchetypeType>())
       typeDecl = opaqueTy->getDecl();
   }
