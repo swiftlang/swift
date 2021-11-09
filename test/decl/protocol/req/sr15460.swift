swiftlang/swift

commit 1cf9622f3206bad5bbd7db21275e639a80685a90

GenericEnvironment: Replace 'substDependentTypesWithErrorTypes' with an assertion

lib/Sema/TypeCheckProtocolInference.cpp
@@ -1199,7 +1199,23 @@ AssociatedTypeDecl *AssociatedTypeInference::completeSolution(
         // If the substitution produced an error, we're done.
         if (type->hasError())
           return witness.getAssocType();
+
+        // FIXME: If we still have a type parameter and it isn't a generic
+        // parameter of the conforming nominal, it's either a cycle or a
+        // solution that is beyond the current algorithm, i.e.
+        //
+        // protocol P {
+        //   associatedtype A = B
+        //   associatedtype B = C
+        //   associatedtype C = Int
+        // }
+        // struct Conformer: P {}
+        if (type->hasTypeParameter() &&
+            !adoptee->getAnyNominal()->isGeneric()) {
+          return witness.getAssocType();
+        }
       }
+
       type = dc->mapTypeIntoContext(type);
     }
 

include/swift/AST/Type.h
@@ -317,9 +317,6 @@ public:
              LookupConformanceFn conformances,
              SubstOptions options=None) const;
 
-  /// Replace references to substitutable types with error types.
-  Type substDependentTypesWithErrorTypes() const;
-  
   bool isPrivateStdlibType(bool treatNonBuiltinProtocolsAsPublic = true) const;
 
   SWIFT_DEBUG_DUMP;

lib/AST/GenericEnvironment.cpp
@@ -87,9 +87,12 @@ Optional<Type> GenericEnvironment::getMappingIfPresent(
 Type GenericEnvironment::mapTypeIntoContext(GenericEnvironment *env,
                                             Type type) {
   assert(!type->hasArchetype() && "already have a contextual type");
+  assert((env || !type->hasTypeParameter()) &&
+         "no generic environment provided for type with type parameters");
 
-  if (!env)
-    return type.substDependentTypesWithErrorTypes();
+  if (!env) {
+    return type;
+  }
 
   return env->mapTypeIntoContext(type);
 }

lib/AST/Type.cpp
@@ -4151,13 +4151,6 @@ Type Type::subst(TypeSubstitutionFn substitutions,
   return substType(*this, substitutions, conformances, options);
 }
 
-Type Type::substDependentTypesWithErrorTypes() const {
-  return substType(*this,
-                   [](SubstitutableType *t) -> Type { return Type(); },
-                   MakeAbstractConformanceForGenericType(),
-                   SubstFlags::AllowLoweredTypes);
-}
-
 const DependentMemberType *TypeBase::findUnresolvedDependentMemberType() {
   if (!hasTypeParameter()) return nullptr;
 

lib/Sema/DerivedConformanceEquatableHashable.cpp
@@ -1011,7 +1011,7 @@ ValueDecl *DerivedConformance::deriveHashable(ValueDecl *requirement) {
         // Hashable because DerivedConformance::canDeriveHashable returns true
         // even if the conformance can't be derived. See the note there for
         // details.
-        auto *dc = ConformanceDecl->getDeclContext();
+        auto *dc = cast<DeclContext>(ConformanceDecl);
         tryDiagnoseFailedHashableDerivation(dc, Nominal);
         return nullptr;
       }

test/Sema/enum_conformance_synthesis.swift
@@ -173,6 +173,12 @@ func genericNotHashable() {
   GenericNotHashable<String>.A("a").hash(into: &hasher) // expected-error {{value of type 'GenericNotHashable<String>' has no member 'hash'}}
 }
 
+enum GenericNotHashable2<T: Equatable, U: Hashable>: Hashable {
+// expected-error@-1 {{type 'GenericNotHashable2' does not conform to protocol 'Hashable'}}
+  case A(U, T) // expected-note {{associated value type 'T' does not conform to protocol 'Hashable', preventing synthesized conformance of 'GenericNotHashable2<T, U>' to 'Hashable'}}
+  case B
+}
+
 // An enum with no cases should also derive conformance.
 enum NoCases: Hashable {}
 

commit 3282b86d7692def79ee0fb4d0a0f6e5da2fbdd09

AssociatedTypeInference: Fix regression introduced in 1cf9622

lib/Sema/TypeCheckProtocolInference.cpp
@@ -1185,7 +1185,9 @@ AssociatedTypeDecl *AssociatedTypeInference::completeSolution(
   for (const auto &witness : abstractTypeWitnesses) {
     Type type = witness.getType();
     if (type->hasTypeParameter()) {
-      if (witness.getKind() != AbstractTypeWitnessKind::GenericParam) {
+      if (witness.getKind() == AbstractTypeWitnessKind::GenericParam) {
+        type = type = dc->mapTypeIntoContext(type);
+      } else {
         // Replace type parameters with other known or tentative type witnesses.
         type = type.subst(
             [&](SubstitutableType *type) {
@@ -1200,23 +1202,26 @@ AssociatedTypeDecl *AssociatedTypeInference::completeSolution(
         if (type->hasError())
           return witness.getAssocType();
 
-        // FIXME: If we still have a type parameter and it isn't a generic
-        // parameter of the conforming nominal, it's either a cycle or a
-        // solution that is beyond the current algorithm, i.e.
-        //
+        // FIXME: If mapping into context yields an error, or we still have a
+        // type parameter despite not having a generic environment, then a type
+        // parameter was sent to a tentative type witness that itself is a type
+        // parameter, and the solution is cyclic, e.g. { A := B.A, B := A.B },
+        // or beyond the current algorithm, e.g.
         // protocol P {
         //   associatedtype A = B
         //   associatedtype B = C
         //   associatedtype C = Int
         // }
         // struct Conformer: P {}
-        if (type->hasTypeParameter() &&
-            !adoptee->getAnyNominal()->isGeneric()) {
+        if (dc->getGenericEnvironmentOfContext()) {
+          type = dc->mapTypeIntoContext(type);
+
+          if (type->hasError())
+            return witness.getAssocType();
+        } else if (type->hasTypeParameter()) {
           return witness.getAssocType();
         }
       }
-
-      type = dc->mapTypeIntoContext(type);
     }
 
     if (const auto &failed = checkTypeWitness(type, witness.getAssocType(),

test/decl/protocol/req/associated_type_inference_fixed_type.swift
@@ -120,3 +120,12 @@ protocol P13c: P13a, P13b where A == B {}
 struct S13: P13c { // OK, A == B == Never
   func foo(arg: Never) {}
 }
+
+protocol P14 {
+  associatedtype A = Array<Self>
+}
+do {
+  struct Outer<Element> {
+    struct Conformer: P14 {}
+  }
+}

test/decl/protocol/req/sr15460.swift
@@ -1,0 +1,31 @@
+// RUN: %target-typecheck-verify-swift
+
+public protocol P {}
+
+extension Array {
+  public struct Inner {}
+}
+
+extension Array.Inner:
+  BidirectionalCollection,
+  Collection,
+  MutableCollection,
+  RandomAccessCollection,
+  Sequence
+where Element: P {
+  public typealias Element = Array<Element>.Element
+  public typealias Index = Array<Element>.Index
+  public typealias Indices = Array<Element>.Indices
+  public typealias SubSequence = Array<Element>.SubSequence
+
+  public subscript(position: Array<Element>.Index) -> Element {
+    get {} set {}
+  }
+
+  public subscript(bounds: Range<Array<Element>.Index>) -> SubSequence {
+    get {} set {}
+  }
+
+  public var startIndex: Index {0}
+  public var endIndex: Index {0}
+}