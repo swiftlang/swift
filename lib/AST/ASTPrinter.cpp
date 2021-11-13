035d492a3a82802f8dce9af52b5d9fef1d78f268
Doug Gregor <dgregor@apple.com>
2021-11-12 21:45:44 -0800

    Fix printing of 'isolated' parameters.
    Fixes rdar://83316058.

--- lib/AST/ASTPrinter.cpp
+++ lib/AST/ASTPrinter.cpp
@@ -3414,9 +3414,6 @@ void PrintAST::printOneParameter(const ParamDecl *param,
 
   printAttributes(param);
 
-  if (param->isIsolated())
-    Printer << "isolated ";
-
   printArgName();
 
   TypeLoc TheTypeLoc;
