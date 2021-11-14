f4c323ba0adb2af6a598aa8ebcacf5276ac13f7f
Michael Ilseman <michael.ilseman@gmail.com>
2021-11-13 17:17:08 -0700

    WIP: nonnull

--- include/swift/Parse/ExperimentalRegexBridging.h
+++ include/swift/Parse/ExperimentalRegexBridging.h
@@ -1,6 +1,15 @@
 #ifndef EXPERIMENTAL_REGEX_BRIDGING
 #define EXPERIMENTAL_REGEX_BRIDGING
 
+// Clang will be used to import, so we'll get the non-null declarations, but
+// this allows the Swift parser in C++ to be built by compilers without
+// nullability support.
+//
+// TODO: See if we can just include Visibility.h instead.
+#ifndef _Nonnull
+#define _Nonnull
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
