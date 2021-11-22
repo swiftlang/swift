19bf160ed0eb783c96a86bc00e16de1e202cdaa4
Pavel Yaskevich <pyaskevich@apple.com>
2021-11-22 11:44:34 -0800

    [Distributed] IRGen: Add argument loading from `UnsafeRawPointer` into an explosion
    `DistributedAccessor::computeArguments` handles loading of the argument
    values from the provided `UnsafeRawPointer` (managed by the caller),
    coercing retrieved arguments to appropriate platform-native representation,
    and exploding object to form correct argument sequence for distributed
    method call.

--- lib/IRGen/GenDistributed.cpp
+++ lib/IRGen/GenDistributed.cpp
@@ -61,6 +61,7 @@ llvm::Value *irgen::emitDistributedActorInitializeRemote(
 namespace {
 
 class DistributedAccessor {
+  IRGenModule &IGM;
   IRGenFunction &IGF;
 
   /// Underlying distributed method for this accessor.
@@ -70,6 +71,9 @@ public:
   DistributedAccessor(IRGenFunction &IGF, SILFunction *method);
 
   void emit();
+
+private:
+  Explosion computeArguments(llvm::Value *argumentBuffer);
 };
 
 } // end namespace
@@ -130,6 +134,97 @@ void IRGenModule::emitDistributedMethodAccessor(SILFunction *method) {
 
 DistributedAccessor::DistributedAccessor(IRGenFunction &IGF,
                                          SILFunction *method)
-    : IGF(IGF), Method(method) {}
+    : IGM(IGF.IGM), IGF(IGF), Method(method) {}
 
-void DistributedAccessor::emit() { IGF.Builder.CreateRetVoid(); }
+Explosion DistributedAccessor::computeArguments(llvm::Value *argumentBuffer) {
+  auto fnType = Method->getLoweredFunctionType();
+
+  Explosion arguments;
+
+  auto offset =
+      IGF.createAlloca(IGM.Int8PtrTy, IGM.getPointerAlignment(), "offset");
+  IGF.Builder.CreateLifetimeStart(offset, IGM.getPointerSize());
+
+  // Initialize "offset" with the address of the base of the argument buffer.
+  IGF.Builder.CreateStore(argumentBuffer, offset);
+
+  // Cover all of the arguments except to `self` of the actor.
+  for (auto &param : fnType->getParameters().drop_back()) {
+    auto paramTy = param.getSILStorageInterfaceType();
+    const TypeInfo &typeInfo = IGF.getTypeInfo(paramTy);
+
+    // 1. Load current offset.
+    llvm::Value *currentOffset = IGF.Builder.CreateLoad(offset, "elt_offset");
+
+    // 2. Cast the pointer to the type of the element.
+    Address eltPtr = IGF.Builder.CreateBitCast(
+        Address(currentOffset, IGM.getPointerAlignment()),
+        IGM.getStoragePointerType(paramTy));
+
+    // 3. Adjust typed pointer to the alignement of the type.
+    auto alignedOffset = typeInfo.roundUpToTypeAlignment(IGF, eltPtr, paramTy);
+
+    // 4. Load argument value from the element pointer.
+    auto *argValue = IGF.Builder.CreateLoad(alignedOffset, "argval");
+
+    // 5. Create an exploded version of the type to pass as an
+    //    argument to distributed method.
+
+    if (paramTy.isObject()) {
+      auto &nativeSchema = typeInfo.nativeParameterValueSchema(IGM);
+      auto expandedTy = nativeSchema.getExpandedType(IGM);
+
+      if (nativeSchema.requiresIndirect()) {
+        llvm_unreachable("indirect parameters are not supported");
+      }
+
+      Explosion nonNativeParam;
+      nonNativeParam.add(argValue);
+
+      // Convert SIL type into a native representation.
+      auto nativeParam = nativeSchema.mapIntoNative(
+          IGM, IGF, nonNativeParam, paramTy, /*isOutlined=*/false);
+
+      // If expanded type is a struct we need to explode it to match
+      // expected argument schema.
+      if (auto *ST = dyn_cast<llvm::StructType>(expandedTy)) {
+        IGF.emitAllExtractValues(nativeParam.claimNext(), ST, arguments);
+      } else {
+        arguments.add(nativeParam.claimNext());
+      }
+    } else {
+      arguments.add(argValue);
+    }
+
+    // 6. Move the offset to the beginning of the next element.
+    {
+      llvm::Value *typeSize = typeInfo.getSize(IGF, paramTy);
+
+      llvm::Value *addr = alignedOffset.getAddress();
+      addr = IGF.Builder.CreatePtrToInt(addr, IGM.IntPtrTy);
+      llvm::Value *nextOffset = IGF.Builder.CreateIntToPtr(
+          IGF.Builder.CreateAdd(addr, typeSize), IGM.Int8PtrTy);
+
+      IGF.Builder.CreateStore(nextOffset, offset);
+    }
+  }
+
+  IGF.Builder.CreateLifetimeEnd(offset, IGM.getPointerSize());
+
+  return arguments;
+}
+
+void DistributedAccessor::emit() {
+  auto params = IGF.collectParameters();
+
+  // UnsafeRawPointer that holds all of the argument values
+  auto *argBuffer = params.claimNext();
+  // Reference to a `self` of the actor to be called.
+  auto *actorSelf = params.claimNext();
+
+  // Step one is to load all of the data from argument buffer,
+  // so it could be forwarded to the distributed method.
+  auto arguments = computeArguments(argBuffer);
+
+  IGF.Builder.CreateRetVoid();
+}
