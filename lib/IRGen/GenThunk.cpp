64d2d39f32ee767c3897e19e0fd3f68a2b92e7ee
Saleem Abdulrasool <compnerd@compnerd.org>
2021-11-24 12:47:19 -0800

    IRGen: do not mark AFP as const
    We were not marking the async function pointer as constant but adding it
    to the `.rdata` section which the Windows linker helped identify as a
    problem: we were adding data with "rw" to a section that is expected to
    be "rd".  Marking the data as constant results in error propagation
    breaking (presumably due to failure to materialize parameters properly
    as bindiff'ing the Concurrency library indicates that the functions are
    nearly identical with some argument loads elided).  This repairs the
    emission of read/write data into the readonly section.

--- lib/IRGen/GenThunk.cpp
+++ lib/IRGen/GenThunk.cpp
@@ -386,7 +386,6 @@ llvm::Constant *IRGenModule::defineAsyncFunctionPointer(LinkEntity entity,
   auto asyncEntity = LinkEntity::forAsyncFunctionPointer(entity);
   auto *var = cast<llvm::GlobalVariable>(
       getAddrOfLLVMVariable(asyncEntity, init, DebugTypeInfo()));
-  setTrueConstGlobal(var);
   return var;
 }
 
