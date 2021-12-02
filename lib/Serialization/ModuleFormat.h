34fc8cd8566e1a5ed05a1da90af76d30c81ca1af
Evan Wilde <etceterawilde@gmail.com>
2021-12-02 14:10:10 -0800

    update module format number for message

--- lib/Serialization/ModuleFormat.h
+++ lib/Serialization/ModuleFormat.h
@@ -56,7 +56,7 @@ const uint16_t SWIFTMODULE_VERSION_MAJOR = 0;
 /// describe what change you made. The content of this comment isn't important;
 /// it just ensures a conflict if two people change the module format.
 /// Don't worry about adhering to the 80-column limit for this line.
-const uint16_t SWIFTMODULE_VERSION_MINOR = 647; // _unavailableFromAsync attr
+const uint16_t SWIFTMODULE_VERSION_MINOR = 648; // _unavailableFromAsync message
 
 /// A standard hash seed used for all string hashes in a serialized module.
 ///
