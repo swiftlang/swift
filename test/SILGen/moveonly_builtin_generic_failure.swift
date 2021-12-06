6f7f23514355eeebe6acbf565f04a8d998687d01
Michael Gottesman <mgottesman@apple.com>
2021-12-06 10:44:33 -0800

    Remove test that doesnt have a purpose anymore
    Previously, I wasn't supporting address only values at all. We support it now in
    certain cases. So remove this test.

--- test/SILGen/moveonly_builtin_generic_failure.swift
+++
@@ -1,7 +0,0 @@
-// RUN: %target-swift-emit-sil -enable-experimental-move-only -parse-stdlib %s -disable-access-control -disable-objc-attr-requires-foundation-module -verify
-
-import Swift
-
-func addressOnlyMove<T>(t: T) -> T {
-    _move(t) // expected-error {{_move applied to value that the compiler does not know how to check. Please file a bug or an enhancement request!}}
-}
