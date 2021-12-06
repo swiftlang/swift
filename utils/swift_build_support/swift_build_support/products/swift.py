194fb216227e5cb27b1215dc201f1c2925a8c861
Eric Miotto <1094986+edymtt@users.noreply.github.com>
2021-12-06 08:43:29 -0800

    Reimplement test for new parameter as Python test
    Also take the chance to rename a function for clarity

--- utils/swift_build_support/swift_build_support/products/swift.py
+++ utils/swift_build_support/swift_build_support/products/swift.py
@@ -61,9 +61,9 @@ class Swift(product.Product):
 
         # Add experimental string processing flag.
         self.cmake_options.extend(self._enable_experimental_string_processing)
-        
+
         # Add freestanding related flags.
-        self.cmake_options.extend(self._enable_freestanding_is_darwin)
+        self.cmake_options.extend(self._freestanding_is_darwin)
 
     @classmethod
     def is_build_script_impl_product(cls):
@@ -173,9 +173,9 @@ updated without updating swift.py?")
     def _enable_experimental_string_processing(self):
         return [('SWIFT_ENABLE_EXPERIMENTAL_STRING_PROCESSING:BOOL',
                  self.args.enable_experimental_string_processing)]
-    
+
     @property
-    def _enable_freestanding_is_darwin(self):
+    def _freestanding_is_darwin(self):
         return [('SWIFT_FREESTANDING_IS_DARWIN:BOOL',
                  self.args.swift_freestanding_is_darwin)]
 
