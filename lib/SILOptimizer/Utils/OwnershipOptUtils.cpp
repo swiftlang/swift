7235aef07ea94214473abfb03b26a2d329a28d4a
Andrew Trick <atrick@apple.com>
2021-12-09 19:54:21 -0800

    SemanticARCOpts fix - remove deadEndBlocks checks
    SemanticARCOptVisitor::performGuaranteedCopyValueOptimization was
    converting this SIL
    
        %borrow = begin_borrow %copiedValue
        %copy = copy_value %borrow
        %borrowCopy = begin_borrow %copy
        end_borrow %borrow
        end_borrow %borrowCopy
        destroy_value %copy
        // something something
        unreachable
    
    into
    
        %borrow = begin_borrow %copiedValue
        %innerBorrow = begin_borrow %borrow
        end_borrow %borrow
        end_borrow %innerBorrow
        // something something
        unreachable
    
    Dead-end blocks are simply irrelevant for this
    optimization. Unfortunately, there were multiple layers of attempted
    workarounds that were hiding the real problem, except in rare cases.
    
    Thanks Nate Chandler for reducing the test.

--- lib/SILOptimizer/Utils/OwnershipOptUtils.cpp
+++ lib/SILOptimizer/Utils/OwnershipOptUtils.cpp
@@ -176,7 +176,7 @@ GuaranteedOwnershipExtension::checkBorrowExtension(
   assert(guaranteedLiveness.empty());
   borrow.computeLiveness(guaranteedLiveness);
 
-  if (guaranteedLiveness.areUsesWithinBoundary(newUses, deBlocks))
+  if (guaranteedLiveness.areUsesWithinBoundary(newUses, &deBlocks))
     return Valid; // reuse the borrow scope as-is
 
   beginBorrow = dyn_cast<BeginBorrowInst>(borrow.value);
@@ -224,7 +224,7 @@ GuaranteedOwnershipExtension::checkLifetimeExtension(
       ownedConsumeBlocks.push_back(user->getParent());
     }
   }
-  if (ownedLifetime.areUsesWithinBoundary(newUses, deBlocks))
+  if (ownedLifetime.areUsesWithinBoundary(newUses, &deBlocks))
     return Valid;
 
   return ExtendLifetime; // Can't cover newUses without destroy sinking.
