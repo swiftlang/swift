86ccc3643e4b7a5bb38f96429c4ec5eac040c2d5
Michael Gottesman <mgottesman@apple.com>
2021-12-10 22:59:58 -0800

    [move-function] Allow for move to be applied to inout function arguments.
    These are verified like a var except that the parameter will have an implicit
    read use on all function exit terminators. This ensures that if a programmer
    moves a value out of the inout parameter, a new value must be assigned before
    function exit. This is important since otherwise the convention would be
    invalidated. Example:
    
    ```
    func f(_ x: inout T) {
         // Move a value out of x. x does not have a value within it anymore.
         let value = _move(x)
    } // So we emit an error saying there is a use here since an inout must have
      // a valid object within it upon function exit due to convention guarantees.
    ```
    
    As an added side-effect of this, one can now use move as on self in mutating
    contexts. Thus one can move the self value out of the self inout binding using
    _move and if one does not replace self with a new value by end of function, you
    will get a compile time error, e.x.:
    
    ```
    struct S {
      var buffer: Klass
    
      mutating func doSomething() {
        let b = move(self).buffer
        // ... do some stuff, maybe get a different buffer ...
        let maybeDifferentBuffer = maybeNewBuffer(b)
    
        // If we do not re-initialize S with a new value by uncommenting
        // the following line, we will get a compile time error.
        // self = S(differentBuffer)
      }
    }
    ```
    
    (cherry picked from commit e6faa3048855741db1744ed1ced26d83c3a2cf90)

--- test/SILOptimizer/move_function_kills_copyable_loadable_vars.swift
+++ test/SILOptimizer/move_function_kills_copyable_loadable_vars.swift
@@ -14,6 +14,7 @@ func consumingUse(_ k: __owned Klass) {}
 var booleanValue: Bool { false }
 var booleanValue2: Bool { false }
 func nonConsumingUse(_ k: Klass) {}
+func exchangeUse(_ k: Klass) -> Klass { k }
 
 ///////////
 // Klassests //
@@ -164,4 +165,108 @@ public func performMoveOnLaterDefinedInit2(_ p: Klass) {
     }
     nonConsumingUse(x)
     let _ = _move(x)
+}
+
+public func performMoveOnInOut(_ p: inout Klass) { // expected-error {{'p' used after being moved}}
+    let buf = _move(p) // expected-note {{move here}}
+    let _ = buf
+} // expected-note {{use here}}
+
+public func performMoveOnInOut2(_ p: inout Klass, _ p2: Klass) {
+    let buf = _move(p)
+    p = p2
+    let _ = buf
+}
+
+// Mutating self is an inout argument.
+struct S {
+    var buffer: Klass?
+
+    mutating func appendNoError() {
+        let b = _move(self).buffer!
+        let maybeNewB = exchangeUse(b)
+        self = .init(buffer: maybeNewB)
+    }
+
+    mutating func appendError() { // expected-error {{'self' used after being moved}}
+        let b = _move(self).buffer // expected-note {{move here}}
+        let _ = b
+    } // expected-note {{use here}}
+
+    mutating func appendThrowingNoError1(_ f: () throws -> ()) throws {
+        let b = _move(self).buffer!
+        let maybeNewB = exchangeUse(b)
+        // We have to initialize self before we call try since otherwise we will
+        // not initialize self along the throws path.
+        self = .init(buffer: maybeNewB)
+        try f()
+    }
+
+    mutating func appendThrowingNoError2(_ f: () throws -> ()) {
+        do {
+            let b = _move(self).buffer!
+            try f()
+            let maybeNewB = exchangeUse(b)
+            self = .init(buffer: maybeNewB)
+        } catch {
+            self = .init(buffer: nil)
+        }
+    }
+
+    // In this case, since we initialize self before the try point, we will have
+    // re-initialized self before hitting either the code after the try that is
+    // inline or the catch block.
+    mutating func appendThrowingNoError3(_ f: () throws -> ()) {
+        do {
+            let b = _move(self).buffer!
+            let maybeNewB = exchangeUse(b)
+            self = .init(buffer: maybeNewB)
+            try f()
+        } catch {
+        }
+    }
+
+    mutating func appendThrowingError0(_ f: () throws -> ()) throws { // expected-error {{'self' used after being moved}}
+        let b = _move(self).buffer! // expected-note {{move here}}
+        let maybeNewB = exchangeUse(b)
+        try f() // expected-note {{use here}}
+        self = .init(buffer: maybeNewB)
+    }
+
+
+    mutating func appendThrowingError1(_ f: () throws -> ()) throws { // expected-error {{'self' used after being moved}}
+        let b = _move(self).buffer! // expected-note {{move here}}
+        let maybeNewB = exchangeUse(b)
+        let _ = maybeNewB
+        try f() // expected-note {{use here}}
+    }
+
+    mutating func appendThrowingError2(_ f: () throws -> ()) { // expected-error {{'self' used after being moved}}
+        do {
+            let b = _move(self).buffer // expected-note {{move here}}
+            let _ = b
+            try f()
+        } catch {
+            self = .init(buffer: nil)
+        }
+    } // expected-note {{use here}}
+
+    mutating func appendThrowingError3(_ f: () throws -> ()) { // expected-error {{'self' used after being moved}}
+        do {
+            let b = _move(self).buffer! // expected-note {{move here}}
+            try f()
+            let maybeNewB = exchangeUse(b)
+            self = .init(buffer: maybeNewB)
+        } catch {
+        }
+    } // expected-note {{use here}}
+
+    mutating func appendThrowingError4(_ f: () throws -> ()) { // expected-error {{'self' used after being moved}}
+        do {
+            let b = _move(self).buffer // expected-note {{move here}}
+            let _ = b
+            try f()
+        } catch {
+        }
+    } // expected-note {{use here}}
 }
