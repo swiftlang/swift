c36d19e219606621011f89bab40bc3d765fd0d16
Holly Borla <hborla@apple.com>
2021-12-13 19:56:33 -0800

    [ClangImporter] When importing ObjC pointer types, wrap protocol compositions in ExistentialType when explicit existentials are enabled.

--- lib/ClangImporter/ImportType.cpp
+++ lib/ClangImporter/ImportType.cpp
@@ -1022,6 +1022,9 @@ namespace {
               Type importedTypeArg = ProtocolCompositionType::get(
                   Impl.SwiftContext, memberTypes,
                   hasExplicitAnyObject);
+              if (Impl.SwiftContext.LangOpts.EnableExplicitExistentialTypes) {
+                importedTypeArg = ExistentialType::get(importedTypeArg);
+              }
               importedTypeArgs.push_back(importedTypeArg);
             }
           }
@@ -1173,6 +1176,10 @@ namespace {
         importedType = ProtocolCompositionType::get(Impl.SwiftContext,
                                                     members,
                                                     /*HasExplicitAnyObject=*/false);
+
+        if (Impl.SwiftContext.LangOpts.EnableExplicitExistentialTypes) {
+          importedType = ExistentialType::get(importedType);
+        }
       }
 
       // Class or Class<P> maps to an existential metatype.
