719af6cb96bbc244fbaa2a723123485b460687b2
Rokhini Prabhu <rokhinip@apple.com>
2021-12-16 19:28:11 -0800

    Change the logic for adding new task status records to a task
    This change has two parts to it:
    
    1. Add in a new interface (addStatusRecordWithChecks) for adding task
    status records that also takes in a function ref. This function ref will
    be used to evaluate if current state of the parent task has any changes
    that need to be propagated to the child task that has been created.
    
    This is necessary to prevent the following race between task creation
    and concurrent cancellation and escalation:
    
    a. Parent task create child task. It does lazy relaxed loads on its own
       state while doing so and propagates this state to the child.
    b. Child task is created but has not been attached to the parent
       task/task group.
    c. Parent task gets cancelled by another thread.
    d. Child task gets linked into the parent’s task status records but no
       reevaluation has happened to account for changes that might have happened to
       the parent after (a).
    
    2. Move status record management functions from the
    Runtime/Concurrency.h to TaskPrivate.h. Remove any corresponding
    overrides that are no longer needed. Remove unused tryAddStatusRecord
    method whose functionality is provided by addStatusRecordWithChecks.
    
    Radar-Id: rdar://problem/86347801

--- stdlib/public/Concurrency/TaskGroup.cpp
+++ stdlib/public/Concurrency/TaskGroup.cpp
@@ -462,11 +462,14 @@ static void swift_taskGroup_initializeImpl(TaskGroup *group, const Metadata *T) 
   assert(impl == record && "the group IS the task record");
 
   // ok, now that the group actually is initialized: attach it to the task
-  bool notCancelled = swift_task_addStatusRecord(record);
-
-  // If the task has already been cancelled, reflect that immediately in
-  // the group status.
-  if (!notCancelled) impl->statusCancel();
+  addStatusRecord(record, [&](ActiveTaskStatus parentStatus) {
+    // If the task has already been cancelled, reflect that immediately in
+    // the group's status.
+    if (parentStatus.isCancelled()) {
+      impl->statusCancel();
+    }
+    return true;
+  });
 }
 
 // =============================================================================
@@ -493,7 +496,7 @@ void TaskGroupImpl::destroy() {
   SWIFT_TASK_DEBUG_LOG("destroying task group = %p", this);
 
   // First, remove the group from the task and deallocate the record
-  swift_task_removeStatusRecord(getTaskRecord());
+  removeStatusRecord(getTaskRecord());
 
   // No need to drain our queue here, as by the time we call destroy,
   // all tasks inside the group must have been awaited on already.
