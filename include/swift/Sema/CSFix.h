d5b11a61362ca709cec08ea7afe3e49a825d351d
Pavel Yaskevich <pyaskevich@apple.com>
2021-12-20 15:23:03 -0800

    [CSFix] Make it possible to diagnose calls to non-functions in ambiguity contexts
    Resolves: rdar://86611718

--- include/swift/Sema/CSFix.h
+++ include/swift/Sema/CSFix.h
@@ -2235,6 +2235,10 @@ public:
 
   bool diagnose(const Solution &solution, bool asNote = false) const override;
 
+  bool diagnoseForAmbiguity(CommonFixesArray commonFixes) const override {
+    return diagnose(*commonFixes.front().first);
+  }
+
   static RemoveInvalidCall *create(ConstraintSystem &cs,
                                    ConstraintLocator *locator);
 
