1f198bcd93d3b3b0e20353f03fc6d3e397e76db0
Evan Wilde <etceterawilde@gmail.com>
2021-12-21 10:43:26 -0800

    Cleaning up _runAsyncMain a bit
    I'm making two cleanups here. First, the closure going into
    `_runAsyncMain` needs to be `@Sendable` or passing it to the task is not
    safe. This will also result in a warning being emitted.
    Second, I'm making this @usableFromInline and `internal`. This function
    is around for legacy reasons, but it's part of the ABI, so we can't pull
    it out entirely, but we don't want folks using it.

--- stdlib/public/Concurrency/Task.swift
+++ stdlib/public/Concurrency/Task.swift
@@ -807,7 +807,9 @@ internal func _asyncMainDrainQueue() -> Never
 internal func _getMainExecutor() -> Builtin.Executor
 
 @available(SwiftStdlib 5.1, *)
-public func _runAsyncMain(_ asyncFun: @escaping () async throws -> ()) {
+@usableFromInline
+@_predatesConcurrency
+internal func _runAsyncMain(_ asyncFun: @Sendable @escaping () async throws -> ()) {
   Task.detached {
     do {
 #if !os(Windows)
