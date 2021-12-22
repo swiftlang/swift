3011a3cfcadcbfc5d50f34b3ca8d3665de137843
Victoria Mitchell <victoria_m@apple.com>
2021-12-22 16:34:49 -0700

    [SymbolGraphGen] don't emit access control attributes in declarations
    rdar://85280786

--- test/SourceKit/CursorInfo/cursor_symbol_graph_referenced.swift
+++ test/SourceKit/CursorInfo/cursor_symbol_graph_referenced.swift
@@ -237,14 +237,6 @@ extension Parent {
 // PRIVATE:        "declarationFragments": [
 // PRIVATE-NEXT:     {
 // PRIVATE-NEXT:       "kind": "keyword",
-// PRIVATE-NEXT:       "spelling": "private"
-// PRIVATE-NEXT:     },
-// PRIVATE-NEXT:     {
-// PRIVATE-NEXT:       "kind": "text",
-// PRIVATE-NEXT:       "spelling": " "
-// PRIVATE-NEXT:     },
-// PRIVATE-NEXT:     {
-// PRIVATE-NEXT:       "kind": "keyword",
 // PRIVATE-NEXT:       "spelling": "func"
 // PRIVATE-NEXT:     },
 // PRIVATE-NEXT:     {
@@ -300,14 +292,6 @@ extension Parent {
 //
 // SPI:      SYMBOL GRAPH BEGIN
 // SPI:        "declarationFragments": [
-// SPI-NEXT:     {
-// SPI-NEXT:       "kind": "keyword",
-// SPI-NEXT:       "spelling": "internal"
-// SPI-NEXT:     },
-// SPI-NEXT:     {
-// SPI-NEXT:       "kind": "text",
-// SPI-NEXT:       "spelling": " "
-// SPI-NEXT:     },
 // SPI-NEXT:     {
 // SPI-NEXT:       "kind": "keyword",
 // SPI-NEXT:       "spelling": "func"
