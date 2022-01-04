d8bf9495827b47dcfa4cf1871027eb77a17d6a88
Erik Eckstein <eeckstein@apple.com>
2022-01-04 20:59:54 +0100

    BasicCalleeAnalysis: improve finding the actual called deinits of a destroy/release instruction
    * C++: add a function `getDestructors(SILType type, bool isExactType)’: if the type is a final class or `isExactType` is true, then return the one and only destructor of that class.
    * swift: add `getDestructor(ofExactType type: Type)` and `getIncompleteCallees`
    * swift: remove `getDestructor` from the PassContext. The API of the `calleeAnalysis` can be used instead.

--- lib/SILOptimizer/Analysis/FunctionOrder.cpp
+++ lib/SILOptimizer/Analysis/FunctionOrder.cpp
@@ -43,7 +43,9 @@ void BottomUpFunctionOrder::DFS(SILFunction *Start) {
       if (!FAS && !isa<StrongReleaseInst>(&I) && !isa<ReleaseValueInst>(&I))
         continue;
 
-      auto Callees = FAS ? BCA->getCalleeList(FAS) : BCA->getCalleeList(&I);
+      auto Callees = FAS ? BCA->getCalleeList(FAS)
+                         : BCA->getDestructors(I.getOperand(0)->getType(),
+                                               /*isExactType*/ false);
       for (auto *CalleeFn : Callees) {
         // If not yet visited, visit the callee.
         if (DFSNum.find(CalleeFn) == DFSNum.end()) {
