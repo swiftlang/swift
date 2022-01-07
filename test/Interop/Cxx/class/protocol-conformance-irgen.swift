0b4641d1fd3f729a1a4080cf8b257cae006bcc41
Robert Widmann <devteam.codafi@gmail.com>
2022-01-07 11:14:38 -0800

    Fixup Tests

--- test/Interop/Cxx/class/protocol-conformance-irgen.swift
+++ test/Interop/Cxx/class/protocol-conformance-irgen.swift
@@ -11,9 +11,9 @@ protocol HasReturn42 {
 // CHECK: [[OUT:%.*]] = call i32 @{{_ZN18ConformsToProtocol8return42Ev|"\?return42@ConformsToProtocol@@QEAAHXZ"}}(%struct.ConformsToProtocol*
 // CHECK: ret i32 [[OUT]]
 
-// CHECK: define {{.*}}%swift.metadata_response @"$sSo18ConformsToProtocolVMa"(i64 [[ARG:%.*]])
+// CHECK: define {{.*}}%swift.metadata_response @"$sSo18ConformsToProtocolVMa"({{i64|i32}} [[ARG:%.*]])
 // CHECK: load %swift.type*, %swift.type** @"$sSo18ConformsToProtocolVML"
-// CHECK: call swiftcc %swift.metadata_response @swift_getForeignTypeMetadata(i64 [[ARG]], %swift.type* getelementptr inbounds (%swift.full_type, %swift.full_type* bitcast (<{ i8**, i64, <{ i32, i32, i32, i32, i32, i32, i32, i32 }>* }>* @"$sSo18ConformsToProtocolVMf" to %swift.full_type*), i32 0, i32 1))
+// CHECK: call swiftcc %swift.metadata_response @swift_getForeignTypeMetadata({{i64|i32}} [[ARG]]
 // CHECK: ret %swift.metadata_response
 
 extension ConformsToProtocol : HasReturn42 {}
