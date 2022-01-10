b761e7701283a05fdeb3e6cc54844ba742b69c24
Pofat Tseng <tjazzter@gmail.com>
2022-01-10 19:17:12 +0000

    Add a test case for windows

--- test/DebugInfo/debug_compilation_dir_windows.swift
+++ test/DebugInfo/debug_compilation_dir_windows.swift
@@ -1,13 +1,13 @@
-// UNSUPPORTED: OS=windows-msvc
+// REQUIRES: OS=windows-msvc
 // RUN: %target-swiftc_driver -g \
-// RUN:   -c -debug-compilation-dir /path/to \
+// RUN:   -c -debug-compilation-dir Z:\path\to \
 // RUN:   %s -o - -emit-ir | %FileCheck --check-prefix=CHECK-ABS %s
 // RUN: %empty-directory(%t)
 // RUN: mkdir -p %t
 // RUN: cd %t
 // RUN: cp %s .
 // RUN: %target-swiftc_driver -g \
-// RUN:   -c -debug-compilation-dir /path/to \
+// RUN:   -c -debug-compilation-dir Z:\path\to \
 // RUN:   debug_compilation_dir.swift -o - -emit-ir | %FileCheck --check-prefix=CHECK-REL %s
 // RUN: %target-swiftc_driver -g \
 // RUN:   -c -debug-compilation-dir . \
@@ -15,6 +15,6 @@
 
 func foo() {}
 
-// CHECK-ABS: !DIFile(filename: "{{.*}}/debug_compilation_dir.swift", directory: "/path/to")
-// CHECK-REL: !DIFile(filename: "debug_compilation_dir.swift", directory: "/path/to")
+// CHECK-ABS: !DIFile(filename: "{{[a-zA-Z]:\\.*\\}}debug_compilation_dir.swift", directory: "Z:\path\to")
+// CHECK-REL: !DIFile(filename: "debug_compilation_dir.swift", directory: "Z:\path\to")
 // CHECK-REL-CWD: !DIFile(filename: "debug_compilation_dir.swift", directory: ".")
