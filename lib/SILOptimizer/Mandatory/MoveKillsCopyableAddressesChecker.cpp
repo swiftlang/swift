43021673f7002936d09bf68a422691acddb3d093
Michael Gottesman <mgottesman@apple.com>
2022-01-11 11:32:07 -0800

    [5.6] IsNotDistributed isn't on 5.6 and isn't an arg to SILFunction's constructor. Remove it

--- lib/SILOptimizer/Mandatory/MoveKillsCopyableAddressesChecker.cpp
+++ lib/SILOptimizer/Mandatory/MoveKillsCopyableAddressesChecker.cpp
@@ -1160,9 +1160,9 @@ SILFunction *ClosureArgumentInOutToOutCloner::initCloned(
       swift::getSpecializedLinkage(orig, orig->getLinkage()), clonedName,
       clonedTy, orig->getGenericEnvironment(), orig->getLocation(),
       orig->isBare(), orig->isTransparent(), serialized, IsNotDynamic,
-      IsNotDistributed, orig->getEntryCount(), orig->isThunk(),
-      orig->getClassSubclassScope(), orig->getInlineStrategy(),
-      orig->getEffectsKind(), orig, orig->getDebugScope());
+      orig->getEntryCount(), orig->isThunk(), orig->getClassSubclassScope(),
+      orig->getInlineStrategy(), orig->getEffectsKind(), orig,
+      orig->getDebugScope());
   for (auto &Attr : orig->getSemanticsAttrs()) {
     Fn->addSemanticsAttr(Attr);
   }
