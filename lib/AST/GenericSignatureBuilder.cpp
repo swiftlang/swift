f33eb17b752c571751416e8c98430a192e94efb4
Slava Pestov <spestov@apple.com>
2022-01-25 21:18:45 -0500

    GSB: Remove unsound cycle-breaking hack

--- lib/AST/GenericSignatureBuilder.cpp
+++ lib/AST/GenericSignatureBuilder.cpp
@@ -5209,12 +5209,6 @@ public:
     // Infer from generic nominal types.
     auto decl = ty->getAnyNominal();
     if (!decl) return Action::Continue;
-
-    // FIXME: The GSB and the request evaluator both detect a cycle here if we
-    // force a recursive generic signature.  We should look into moving cycle
-    // detection into the generic signature request(s) - see rdar://55263708
-    if (!decl->hasComputedGenericSignature())
-      return Action::Continue;
     
     auto genericSig = decl->getGenericSignature();
     if (!genericSig)
