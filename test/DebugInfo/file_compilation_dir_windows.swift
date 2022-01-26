5dc556ffee183a87b51ff7c299e67235bda00168
Pofat Tseng <tjazzter@gmail.com>
2022-01-26 23:01:02 +0000

    Fix windows test case

--- test/DebugInfo/file_compilation_dir_windows.swift
+++ test/DebugInfo/file_compilation_dir_windows.swift
@@ -5,16 +5,16 @@
 // RUN: %empty-directory(%t)
 // RUN: mkdir -p %t
 // RUN: cd %t
-// RUN: cp %s .
+// RUN: xcopy %s .
 // RUN: %target-swiftc_driver -g \
 // RUN:   -c -file-compilation-dir Z:\path\to \
-// RUN:   file_compilation_dir.swift -o - -emit-ir | %FileCheck --check-prefix=CHECK-REL %s
+// RUN:   file_compilation_dir_windows.swift -o - -emit-ir | %FileCheck --check-prefix=CHECK-REL %s
 // RUN: %target-swiftc_driver -g \
 // RUN:   -c -file-compilation-dir . \
-// RUN:   file_compilation_dir.swift -o - -emit-ir | %FileCheck --check-prefix=CHECK-REL-CWD %s
+// RUN:   file_compilation_dir_windows.swift -o - -emit-ir | %FileCheck --check-prefix=CHECK-REL-CWD %s
 
 func foo() {}
 
-// CHECK-ABS: !DIFile(filename: "{{[a-zA-Z]:\\\\.*\\\\}}file_compilation_dir.swift", directory: "Z:\\path\\to")
-// CHECK-REL: !DIFile(filename: "file_compilation_dir.swift", directory: "Z:\\path\\to")
-// CHECK-REL-CWD: !DIFile(filename: "file_compilation_dir.swift", directory: ".")
+// CHECK-ABS: !DIFile(filename: "{{[a-zA-Z]:\\\\.*\\\\}}file_compilation_dir_windows.swift", directory: "Z:\\path\\to")
+// CHECK-REL: !DIFile(filename: "file_compilation_dir_windows.swift", directory: "Z:\\path\\to")
+// CHECK-REL-CWD: !DIFile(filename: "file_compilation_dir_windows.swift", directory: ".")
