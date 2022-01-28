e2c61d37ba1a17926979367d2575f137c018b85c
Konrad `ktoso` Malawski <konrad_malawski@apple.com>
2022-01-28 10:57:39 +0900

    [Distributed] Move dist funcdecl getters to ASTContext

--- include/swift/AST/Decl.h
+++ include/swift/AST/Decl.h
@@ -3370,9 +3370,6 @@ public:
   /// Find, or potentially synthesize, the implicit 'id' property of this actor.
   VarDecl *getDistributedActorIDProperty() const;
 
-  /// Find the 'makeInvocation' function.
-  AbstractFunctionDecl* getDistributedActorSystemMakeInvocationEncoderFunction() const;
-
   /// Find the 'RemoteCallTarget.init(_mangledName:)' initializer function
   ConstructorDecl* getDistributedRemoteCallTargetInitFunction() const;
 
