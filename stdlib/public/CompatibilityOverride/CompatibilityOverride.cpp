44728a98eb6a3fc669c9a8b17b01fe2ed69db7bb
Joey <59270018+joey-gm@users.noreply.github.com>
2022-02-01 00:34:54 +0800

    Update CompatibilityOverride.cpp

--- stdlib/public/CompatibilityOverride/CompatibilityOverride.cpp
+++ stdlib/public/CompatibilityOverride/CompatibilityOverride.cpp
@@ -1,4 +1,4 @@
-//===--- CompatibilityOverride.cpp - Back-deploying compatibility files ---s-===//
+//===--- CompatibilityOverride.cpp - Back-deploying compatibility fixes ---s-===//
 //
 // This source file is part of the Swift.org open source project
 //
