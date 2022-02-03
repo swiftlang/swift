2a65210ab0e1a08f6afa736fefbe3b32deb412e3
Konrad `ktoso` Malawski <ktoso@apple.com>
2022-02-03 20:22:20 +0900

    Check if not emitting message helps in IR issue

--- lib/IRGen/GenDistributed.cpp
+++ lib/IRGen/GenDistributed.cpp
@@ -483,7 +483,7 @@ void DistributedAccessor::lookupWitnessTables(
     // then accessor should trap.
     {
       IGF.Builder.emitBlock(failBB);
-      IGF.emitTrap("missing witness table", /*EmitUnreachable=*/true);
+      IGF.emitTrap("", /*EmitUnreachable=*/true);
     }
 
     IGF.Builder.emitBlock(contBB);
