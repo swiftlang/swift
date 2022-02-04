66b62e9c79cd161b965d9167175f87877e4b5166
Saleem Abdulrasool <compnerd@compnerd.org>
2022-02-04 13:49:16 -0800

    Reflection: adjust type definitions for `ConcurrentHashMap`
    Adjust the types of `ConcurrentHashMap` to match the implementation.  This field is always a `uint32_t`, which would be misread previously.  Thanks to @mikeash for helping identify this.

--- include/swift/Reflection/RuntimeInternals.h
+++ include/swift/Reflection/RuntimeInternals.h
@@ -49,8 +49,8 @@ template <typename Runtime> struct MetadataCacheNode {
 };
 
 template <typename Runtime> struct ConcurrentHashMap {
-  typename Runtime::StoredSize ReaderCount;
-  typename Runtime::StoredSize ElementCount;
+  typename uint32_t ReaderCount;
+  typename uint32_t ElementCount;
   typename Runtime::StoredPointer Elements;
   typename Runtime::StoredPointer Indices;
   // We'll ignore the remaining fields for now....
