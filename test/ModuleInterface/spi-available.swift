1395af1c9c59e0ec70778eebe0e88bdaecd7df4e
Xi Ge <xi_ge@apple.com>
2022-03-05 12:00:39 -0800

    test: more module interface tests

--- test/ModuleInterface/spi-available.swift
+++ test/ModuleInterface/spi-available.swift
@@ -4,7 +4,7 @@
 // RUN: %target-swift-frontend -compile-module-from-interface %t/Foo.swiftinterface -o %t/Foo.public.swiftmodule -module-name Foo
 // RUN: %target-swift-frontend -compile-module-from-interface %t/Foo.private.swiftinterface -o %t/Foo.private.swiftmodule -module-name Foo
 
-@_spi_available(macOS 10.10, *)
+@_spi_available(macOS 10.10, tvOS 14.0, *)
 @available(iOS 8.0, *)
 public class SPIClass {}
 
@@ -12,4 +12,7 @@ public class SPIClass {}
 // RUN: %FileCheck %s -check-prefix CHECK-PRIVATE < %t/Foo.private.swiftinterface
 
 // CHECK-PUBLIC: @available(macOS, unavailable)
+// CHECK-PUBLIC: @available(tvOS, unavailable)
+
 // CHECK-PRIVATE: @_spi_available(macOS, introduced: 10.10)
+// CHECK-PRIVATE: @_spi_available(tvOS, introduced: 14.0)
