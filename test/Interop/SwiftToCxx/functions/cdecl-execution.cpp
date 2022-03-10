846fdb53073d26ffba585bd364788d76bf9d0d0c
Alex Lorenz <arphaman@gmail.com>
2022-03-10 08:05:52 -0800

    [interop] add target-build-swift-link-cxx option for windows to link correctly

--- test/Interop/SwiftToCxx/functions/cdecl-execution.cpp
+++ test/Interop/SwiftToCxx/functions/cdecl-execution.cpp
@@ -3,7 +3,7 @@
 // RUN: %target-swift-frontend %S/cdecl.swift -typecheck -module-name CdeclFunctions -emit-cxx-header-path %t/functions.h
 
 // RUN: %target-clangxx -c %s -I %t -o %t/cdecl-execution.o
-// RUN: %target-build-swift %S/cdecl.swift -o %t/cdecl-execution -Xlinker %t/cdecl-execution.o -module-name Functions -Xfrontend -entry-point-function-name -Xfrontend swiftMain -Xfrontend -enable-cxx-interop
+// RUN: %target-build-swift-link-cxx %S/cdecl.swift -o %t/cdecl-execution -Xlinker %t/cdecl-execution.o -module-name Functions -Xfrontend -entry-point-function-name -Xfrontend swiftMain
 
 // RUN: %target-codesign %t/cdecl-execution
 // RUN: %target-run %t/cdecl-execution
--- test/Interop/SwiftToCxx/functions/swift-functions-execution.cpp
+++ test/Interop/SwiftToCxx/functions/swift-functions-execution.cpp
@@ -3,7 +3,7 @@
 // RUN: %target-swift-frontend %S/swift-functions.swift -typecheck -module-name Functions -emit-cxx-header-path %t/functions.h
 
 // RUN: %target-clangxx -c %s -I %t -o %t/swift-functions-execution.o
-// RUN: %target-build-swift %S/swift-functions.swift -o %t/swift-functions-execution -Xlinker %t/swift-functions-execution.o -module-name Functions -Xfrontend -entry-point-function-name -Xfrontend swiftMain -Xfrontend -enable-cxx-interop
+// RUN: %target-build-swift-link-cxx %S/swift-functions.swift -o %t/swift-functions-execution -Xlinker %t/swift-functions-execution.o -module-name Functions -Xfrontend -entry-point-function-name -Xfrontend swiftMain
 
 // RUN: %target-codesign %t/swift-functions-execution
 // RUN: %target-run %t/swift-functions-execution | %FileCheck %s
