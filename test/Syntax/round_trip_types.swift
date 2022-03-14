7a1792ab4e902008799168f3bca78f060a85aefa
Alex Hoppen <ahoppen@apple.com>
2022-03-14 15:23:17 +0100

    [Parser] Support 'any' type in SwiftSyntax
    Previously, SwiftSyntax wasn’t able to parse 'any' types. Add support for them now.
    
    rdar://90077430

--- test/Syntax/round_trip_types.swift
+++ test/Syntax/round_trip_types.swift
@@ -4,3 +4,9 @@ typealias TwoInts = (Int, Int)
 typealias TwoNamedInts = (a: Int, b: Int)
 typealias VoidTuple = ()
 typealias TupleWithTrivia = (   Int ,  b  :Int   )
+
+func testAnyType() {
+  protocol MyProto {}
+
+  let foo: [Int: any MyProto]
+}
