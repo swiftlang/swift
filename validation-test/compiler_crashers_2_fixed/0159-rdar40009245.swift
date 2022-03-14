6a74ad7065ca1f45a0860099fcb4c55d13a894c8
Slava Pestov <spestov@apple.com>
2022-03-13 22:53:32 -0400

    RequirementMachine: Drop GSB compatibility hack involving ErrorTypes
    Another hack we can remove now that 'verify' mode skips checking if
    there was a conflict.

--- validation-test/compiler_crashers_2_fixed/0159-rdar40009245.swift
+++ validation-test/compiler_crashers_2_fixed/0159-rdar40009245.swift
@@ -2,6 +2,7 @@
 
 protocol P {
     associatedtype A : P where A.X == Self
+    // expected-error@-1{{'X' is not a member type of type 'Self.A'}}
     associatedtype X : P where P.A == Self
     // expected-error@-1{{associated type 'A' can only be used with a concrete type or generic parameter base}}
 }
