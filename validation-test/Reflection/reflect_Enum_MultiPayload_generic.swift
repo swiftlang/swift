bab89516e0de67b468b9b40b7bb5bbe277d3b0b4
Tim Kientzle <tkientzle@apple.com>
2022-03-18 16:00:29 -0700

    RemoteMirror generic multi-payload-enum fixes
    Apparently, RemoteMirror chokes on certain MPEs with generic payload types.
    It does not recognize the generic payload type so ends up defaulting to a
    zero size.  This causes the overall enum size to be miscalculated unless there
    is another non-generic payload that's at least as large.
    
    Step 1: Test case

---
+++ validation-test/Reflection/reflect_Enum_MultiPayload_generic.swift
@@ -0,0 +1,108 @@
+// RUN: %empty-directory(%t)
+// RUN: %target-build-swift -lswiftSwiftReflectionTest %s -o %t/reflect_Enum_MultiPayload_generic
+// RUN: %target-codesign %t/reflect_Enum_MultiPayload_generic
+
+// RUN: %target-run %target-swift-reflection-test %t/reflect_Enum_MultiPayload_generic | tee /dev/stderr | %FileCheck %s --check-prefix=CHECK --check-prefix=X%target-ptrsize --dump-input=fail
+
+// REQUIRES: reflection_test_support
+// REQUIRES: objc_interop
+// REQUIRES: executable_test
+// UNSUPPORTED: use_os_stdlib
+
+import SwiftReflectionTest
+
+struct S {
+  var a: Int
+  var b: Int
+  var c: Int
+}
+
+class ClassWithNonGenericEnum<T> {
+  enum E {
+  case t(T)
+  case u(Int)
+  }
+  var e: E?
+}
+
+reflect(object: ClassWithNonGenericEnum<S>())
+
+// CHECK: Reflecting an object.
+// CHECK-NEXT: Instance pointer in child address space: 0x{{[0-9a-fA-F]+}}
+// CHECK-NEXT: Type reference:
+// CHECK-NEXT: (bound_generic_class reflect_Enum_MultiPayload_generic.ClassWithNonGenericEnum
+// CHECK-NEXT:   (struct reflect_Enum_MultiPayload_generic.S))
+
+// CHECK: Type info:
+// CHECK-NEXT: (class_instance size=41 alignment=8 stride=48 num_extra_inhabitants=0 bitwise_takable=1
+// CHECK-NEXT:   (field name=e offset=16
+// CHECK-NEXT:     (single_payload_enum size=25 alignment=8 stride=32 num_extra_inhabitants=253 bitwise_takable=1
+// CHECK-NEXT:       (case name=some index=0 offset=0
+// CHECK-NEXT:         (multi_payload_enum size=25 alignment=8 stride=32 num_extra_inhabitants=254 bitwise_takable=1
+// CHECK-NEXT:           (case name=t index=0 offset=0
+// CHECK-NEXT:             (struct size=24 alignment=8 stride=24 num_extra_inhabitants=0 bitwise_takable=1
+// CHECK-NEXT:               (field name=a offset=0
+// CHECK-NEXT:                 (struct size=8 alignment=8 stride=8 num_extra_inhabitants=0 bitwise_takable=1
+// CHECK-NEXT:                   (field name=_value offset=0
+// CHECK-NEXT:                     (builtin size=8 alignment=8 stride=8 num_extra_inhabitants=0 bitwise_takable=1))))
+// CHECK-NEXT:               (field name=b offset=8
+// CHECK-NEXT:                 (struct size=8 alignment=8 stride=8 num_extra_inhabitants=0 bitwise_takable=1
+// CHECK-NEXT:                   (field name=_value offset=0
+// CHECK-NEXT:                     (builtin size=8 alignment=8 stride=8 num_extra_inhabitants=0 bitwise_takable=1))))
+// CHECK-NEXT:               (field name=c offset=16
+// CHECK-NEXT:                 (struct size=8 alignment=8 stride=8 num_extra_inhabitants=0 bitwise_takable=1
+// CHECK-NEXT:                   (field name=_value offset=0
+// CHECK-NEXT:                     (builtin size=8 alignment=8 stride=8 num_extra_inhabitants=0 bitwise_takable=1))))))
+// CHECK-NEXT:           (case name=u index=1 offset=0
+// CHECK-NEXT:             (struct size=8 alignment=8 stride=8 num_extra_inhabitants=0 bitwise_takable=1
+// CHECK-NEXT:               (field name=_value offset=0
+// CHECK-NEXT:                 (builtin size=8 alignment=8 stride=8 num_extra_inhabitants=0 bitwise_takable=1))))))
+// CHECK-NEXT:       (case name=none index=1))))
+
+
+class ClassWithGenericEnum<T> {
+  enum E<T> {
+  case t(T)
+  case u(Int)
+  }
+  var e: E<T>?
+}
+
+reflect(object: ClassWithGenericEnum<S>())
+
+// CHECK: Reflecting an object.
+// CHECK-NEXT: Instance pointer in child address space: 0x{{[0-9a-fA-F]+}}
+// CHECK-NEXT: Type reference:
+// CHECK-NEXT: (bound_generic_class reflect_Enum_MultiPayload_generic.ClassWithGenericEnum
+// CHECK-NEXT:   (struct reflect_Enum_MultiPayload_generic.S))
+
+// CHECK: Type info:
+// CHECK-NEXT: (class_instance size=41 alignment=8 stride=48 num_extra_inhabitants=0 bitwise_takable=1
+// CHECK-NEXT:   (field name=e offset=16
+// CHECK-NEXT:     (single_payload_enum size=25 alignment=8 stride=32 num_extra_inhabitants=253 bitwise_takable=1
+// CHECK-NEXT:       (case name=some index=0 offset=0
+// CHECK-NEXT:         (multi_payload_enum size=25 alignment=8 stride=32 num_extra_inhabitants=254 bitwise_takable=1
+// CHECK-NEXT:           (case name=t index=0 offset=0
+// CHECK-NEXT:             (struct size=24 alignment=8 stride=24 num_extra_inhabitants=0 bitwise_takable=1
+// CHECK-NEXT:               (field name=a offset=0
+// CHECK-NEXT:                 (struct size=8 alignment=8 stride=8 num_extra_inhabitants=0 bitwise_takable=1
+// CHECK-NEXT:                   (field name=_value offset=0
+// CHECK-NEXT:                     (builtin size=8 alignment=8 stride=8 num_extra_inhabitants=0 bitwise_takable=1))))
+// CHECK-NEXT:               (field name=b offset=8
+// CHECK-NEXT:                 (struct size=8 alignment=8 stride=8 num_extra_inhabitants=0 bitwise_takable=1
+// CHECK-NEXT:                   (field name=_value offset=0
+// CHECK-NEXT:                     (builtin size=8 alignment=8 stride=8 num_extra_inhabitants=0 bitwise_takable=1))))
+// CHECK-NEXT:               (field name=c offset=16
+// CHECK-NEXT:                 (struct size=8 alignment=8 stride=8 num_extra_inhabitants=0 bitwise_takable=1
+// CHECK-NEXT:                   (field name=_value offset=0
+// CHECK-NEXT:                     (builtin size=8 alignment=8 stride=8 num_extra_inhabitants=0 bitwise_takable=1))))))
+// CHECK-NEXT:           (case name=u index=1 offset=0
+// CHECK-NEXT:             (struct size=8 alignment=8 stride=8 num_extra_inhabitants=0 bitwise_takable=1
+// CHECK-NEXT:               (field name=_value offset=0
+// CHECK-NEXT:                 (builtin size=8 alignment=8 stride=8 num_extra_inhabitants=0 bitwise_takable=1))))))
+// CHECK-NEXT:       (case name=none index=1))))
+
+doneReflecting()
+
+// CHECK: Done.
+
