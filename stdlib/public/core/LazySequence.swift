c5a5a33e313b2824b59681fe92aac5b58c5b9599
Karoy Lorentey <klorentey@apple.com>
2022-04-04 17:18:14 -0700

    [stdlib] LazySequenceProtocol: Take out duplicate where clause
    (cherry picked from commit 655f085c87b4ffef1674c2b85f13ea48a97903a4)

--- stdlib/public/core/LazySequence.swift
+++ stdlib/public/core/LazySequence.swift
@@ -128,8 +128,7 @@
 /// and discards the resulting array. Instead, use `reduce` for summing 
 /// operations, or `forEach` or a `for`-`in` loop for operations with side 
 /// effects.
-public protocol LazySequenceProtocol<Elements>: Sequence
-where Elements.Element == Element {
+public protocol LazySequenceProtocol<Elements>: Sequence {
   /// A `Sequence` that can contain the same elements as this one,
   /// possibly with a simpler type.
   ///
