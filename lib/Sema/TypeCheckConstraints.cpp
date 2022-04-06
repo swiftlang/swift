1abd15ba23c2a6bf978a8608c24101f1aabf2d81
Alex Hoppen <ahoppen@apple.com>
2022-04-06 14:44:53 +0200

    [Sema] Print whether a type variable allows binding to a hole
    Helped me while debugging and might help others as well.

--- lib/Sema/TypeCheckConstraints.cpp
+++ lib/Sema/TypeCheckConstraints.cpp
@@ -1410,6 +1410,8 @@ void ConstraintSystem::print(raw_ostream &out) const {
       out << " [inout allowed]";
     if (tv->getImpl().canBindToNoEscape())
       out << " [noescape allowed]";
+    if (tv->getImpl().canBindToHole())
+      out << " [hole allowed]";
     auto rep = getRepresentative(tv);
     if (rep == tv) {
       if (auto fixed = getFixedType(tv)) {
