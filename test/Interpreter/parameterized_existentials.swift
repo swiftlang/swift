5783b0c943d1548b92625bf095365da4ced1daac
John McCall <rjmccall@apple.com>
2022-04-11 19:29:15 -0400

    Add a testcase to verify basic execution for constrained existentials
    Disabled for now; there's apparently a bug.

---
+++ test/Interpreter/parameterized_existentials.swift
@@ -0,0 +1,43 @@
+// RUN: %target-run-simple-swift(-Xfrontend -enable-parameterized-existential-types)
+// REQUIRES: executable_test
+
+// REQUIRES: disabled
+
+import StdlibUnittest
+
+var ParameterizedProtocolsTestSuite = TestSuite("ParameterizedProtocols")
+
+protocol Holder<T> {
+  associatedtype T
+  var value: T { get }
+}
+
+struct IntHolder: Holder {
+  var value: Int
+}
+
+struct GenericHolder<T>: Holder {
+  var value: T
+}
+
+ParameterizedProtocolsTestSuite.test("basic") {
+  let x: any Holder<Int> = IntHolder(value: 5)
+  expectEqual(5, x.value)
+}
+
+func staticType<T>(of value: inout T) -> Any.Type {
+  return T.self
+}
+
+func staticTypeForHolders<T>(of value: inout any Holder<T>) -> Any.Type {
+  return (any Holder<T>).self
+}
+
+ParameterizedProtocolsTestSuite.test("metadataEquality") {
+  var x: any Holder<Int> = IntHolder(value: 5)
+  var typeOne = staticType(of: &x)
+  var typeTwo = staticTypeForHolders(of: &x)
+  expectEqual(typeOne, typeTwo)
+}
+
+runAllTests()
