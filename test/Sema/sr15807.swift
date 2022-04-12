1dabc67b5f628583c91ad3119809f10cbe7ad4ca
hank121314 <hank121314@gmail.com>
2022-04-12 22:57:47 +0800

    [Sema] TypeCheckProtocolInference: using trailingWhereClause to get self bounds in source file.

--- test/Sema/sr15807.swift
+++
@@ -1,9 +0,0 @@
-// RUN: %empty-directory(%t)
-// RUN: %target-swift-frontend -emit-module -o %t/sr15807_a.swiftmodule %S/Inputs/sr15807_a.swift
-// RUN: %target-swift-frontend -I %t -emit-sil -verify %s
-
-import Foundation
-import sr15807_a
-
-struct ModuleBFoo: Codable, DefaultsSerializable {
-}
