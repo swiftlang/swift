3ab7ead438cec566e321733c6bcb6d09cc48a986
Hamish Knight <hamish_github@mediocremail.com>
2022-04-12 16:03:48 +0100

    [Lexer] Remove `r'...'` lexing logic
    This spelling is no longer used.

--- lib/Parse/Lexer.cpp
+++ lib/Parse/Lexer.cpp
@@ -2541,20 +2541,13 @@ void Lexer::lexImpl() {
   case '&': case '|':  case '^': case '~': case '.':
     return lexOperatorIdentifier();
 
-  case 'r':
-    // If we have experimental string processing enabled, try lex a regex
-    // literal.
-    if (tryLexRegexLiteral(TokStart))
-      return;
-    LLVM_FALLTHROUGH;
-
   case 'A': case 'B': case 'C': case 'D': case 'E': case 'F': case 'G':
   case 'H': case 'I': case 'J': case 'K': case 'L': case 'M': case 'N':
   case 'O': case 'P': case 'Q': case 'R': case 'S': case 'T': case 'U':
   case 'V': case 'W': case 'X': case 'Y': case 'Z':
   case 'a': case 'b': case 'c': case 'd': case 'e': case 'f': case 'g':
   case 'h': case 'i': case 'j': case 'k': case 'l': case 'm': case 'n':
-  case 'o': case 'p': case 'q': /*r above*/ case 's': case 't': case 'u':
+  case 'o': case 'p': case 'q': case 'r': case 's': case 't': case 'u':
   case 'v': case 'w': case 'x': case 'y': case 'z':
   case '_':
     return lexIdentifier();
