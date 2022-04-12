05fff59bbbabd764ecd569b779ba4495cb5a47bc
hank121314 <hank121314@gmail.com>
2022-04-12 22:57:47 +0800

    [Sema] TypeCheckProtocolInference: using trailingWhereClause to get self bounds in source file.

---
+++ test/Sema/where_clause_across_module_boundaries.swift
@@ -0,0 +1,17 @@
+// RUN: %empty-directory(%t)
+// RUN: %target-swift-frontend -emit-module -o %t/ModuleA.swiftmodule %S/Inputs/where_clause_across_module_boundaries_module.swift
+// RUN: %target-swift-frontend -I %t -emit-sil -verify %s
+
+// SR-15807:
+// Associated Type Inference fails across module boundaries
+// Self bounds from where clause cannot be accessed across modules.
+// This test is intended to test whether it can use generic signature to get self bounds.
+import Foundation
+import ModuleA
+
+struct ModuleBFoo: Codable, DefaultsSerializable {
+}
+
+enum ModuleBBar: Int, Codable, DefaultsSerializable { // expected-error {{type 'ModuleBBar' does not conform to protocol 'DefaultsSerializable'}}
+  case foo, bar
+}
