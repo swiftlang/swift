48eab595691e466f4db65174740534014f13c3f7
Josh Soref <jsoref@users.noreply.github.com>
2022-04-17 15:27:59 -0400

    spelling: entity
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- lib/IDE/ModuleInterfacePrinting.cpp
+++ lib/IDE/ModuleInterfacePrinting.cpp
@@ -1105,11 +1105,11 @@ bool ClangCommentPrinter::shouldPrintNewLineBefore(ClangNode Node) const {
   if (FID.isInvalid())
     return false;
 
-  unsigned LastEntiyLine = 0;
+  unsigned LastEntityLine = 0;
   auto It = LastEntityLines.find(FID);
   if (It != LastEntityLines.end())
-    LastEntiyLine = It->second;
-  return (NodeLineNo > LastEntiyLine) && NodeLineNo - LastEntiyLine > 1;
+    LastEntityLine = It->second;
+  return (NodeLineNo > LastEntityLine) && NodeLineNo - LastEntityLine > 1;
 }
 
 void ClangCommentPrinter::updateLastEntityLine(clang::SourceLocation Loc) {
@@ -1130,7 +1130,7 @@ void ClangCommentPrinter::updateLastEntityLine(clang::SourceLocation Loc) {
 void ClangCommentPrinter::updateLastEntityLine(clang::FileID FID,
                                                unsigned LineNo) {
   assert(!FID.isInvalid());
-  unsigned &LastEntiyLine = LastEntityLines[FID];
-  if (LineNo > LastEntiyLine)
-    LastEntiyLine = LineNo;
+  unsigned &LastEntityLine = LastEntityLines[FID];
+  if (LineNo > LastEntityLine)
+    LastEntityLine = LineNo;
 }
