7bf805e0c09848c18ae7740ba6fb8de5cc9d78ba
Josh Soref <jsoref@users.noreply.github.com>
2022-04-17 15:54:10 -0400

    spelling: unknown
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- test/IDE/complete_attributes.swift
+++ test/IDE/complete_attributes.swift
@@ -24,9 +24,9 @@ struct MyValue {
 // MEMBER_MyValue-DAG: Decl[StaticVar]/CurrNominal:        val[#Int#];
 // MEMBER_MyValue: End completions
 
-class TestUnkownDanglingAttr1 {
-  @UknownAttr(arg: MyValue.#^ATTRARG_MEMBER^#)
+class TestUnknownDanglingAttr1 {
+  @UnknownAttr(arg: MyValue.#^ATTRARG_MEMBER^#)
 }
-class TestUnkownDanglingAttr2 {
-  @UknownAttr(arg: { MyValue.#^ATTRARG_MEMBER_IN_CLOSURE^# })
+class TestUnknownDanglingAttr2 {
+  @UnknownAttr(arg: { MyValue.#^ATTRARG_MEMBER_IN_CLOSURE^# })
 }
