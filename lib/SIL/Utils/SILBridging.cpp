86e319e978f9c4ae2e6ad5a7c99dbe2d9debefbb
Josh Soref <jsoref@users.noreply.github.com>
2022-04-17 15:37:28 -0400

    spelling: message
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- lib/SIL/Utils/SILBridging.cpp
+++ lib/SIL/Utils/SILBridging.cpp
@@ -694,10 +694,10 @@ BridgedInstruction SILBuilder_createBuiltinBinaryFunction(
 }
 
 BridgedInstruction SILBuilder_createCondFail(BridgedInstruction insertionPoint,
-          BridgedLocation loc, BridgedValue condition, BridgedStringRef messge) {
+          BridgedLocation loc, BridgedValue condition, BridgedStringRef message) {
   SILBuilder builder(castToInst(insertionPoint), getSILDebugScope(loc));
   return {builder.createCondFail(getRegularLocation(loc),
-    castToSILValue(condition), getStringRef(messge))};
+    castToSILValue(condition), getStringRef(message))};
 }
 
 BridgedInstruction SILBuilder_createIntegerLiteral(BridgedInstruction insertionPoint,
