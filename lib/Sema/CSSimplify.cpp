973be1dba91488d8fcd3274724f06364d1cc7197
Josh Soref <jsoref@users.noreply.github.com>
2022-04-17 15:36:04 -0400

    spelling: introducing
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- lib/Sema/CSSimplify.cpp
+++ lib/Sema/CSSimplify.cpp
@@ -1851,7 +1851,7 @@ static ConstraintSystem::TypeMatchResult matchCallArguments(
           }
         } else {
           // Matching @autoclosure argument to @autoclosure parameter
-          // directly would mean introducting a function conversion
+          // directly would mean introducing a function conversion
           // in Swift <= 4 mode.
           cs.increaseScore(SK_FunctionConversion);
           matchingAutoClosureResult = false;
