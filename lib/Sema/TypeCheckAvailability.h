7af8cceb6866596eeab39c69c8911962c9ff0005
Josh Soref <jsoref@users.noreply.github.com>
2022-04-17 15:32:14 -0400

    spelling: identical
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- lib/Sema/TypeCheckAvailability.h
+++ lib/Sema/TypeCheckAvailability.h
@@ -142,7 +142,7 @@ public:
   /// Produce a new context with the same properties as this one, except
   /// that if 'exported' is false, the resulting context can reference
   /// declarations that are not exported. If 'exported' is true, the
-  /// resulting context is indentical to this one.
+  /// resulting context is identical to this one.
   ///
   /// That is, this will perform a 'bitwise and' on the 'exported' bit.
   ExportContext withExported(bool exported) const;
