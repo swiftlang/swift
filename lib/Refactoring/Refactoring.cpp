3772736a581b75db212782118c14bef07b99f3c0
Josh Soref <jsoref@users.noreply.github.com>
2022-04-17 15:22:35 -0400

    spelling: continuation
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- lib/Refactoring/Refactoring.cpp
+++ lib/Refactoring/Refactoring.cpp
@@ -6120,7 +6120,7 @@ class AsyncConverter : private SourceEntityWalker {
         : Names(), ContinuationName(ContinuationName) {}
 
     /// Whether this scope is wrapped in a \c withChecked(Throwing)Continuation.
-    bool isWrappedInContination() const { return !ContinuationName.empty(); }
+    bool isWrappedInContinuation() const { return !ContinuationName.empty(); }
   };
   SourceFile *SF;
   SourceManager &SM;
@@ -6571,12 +6571,12 @@ private:
   ///    have an async alternative by our heuristics (e.g. because of a
   ///    completion handler name mismatch or because it also returns a value
   ///    synchronously).
-  void wrapScopeInContinationIfNecessary(ASTNode Node) {
+  void wrapScopeInContinuationIfNecessary(ASTNode Node) {
     if (NestedExprCount != 0) {
       // We can't start a continuation in the middle of an expression
       return;
     }
-    if (Scopes.back().isWrappedInContination()) {
+    if (Scopes.back().isWrappedInContinuation()) {
       // We are already in a continuation. No need to add another one.
       return;
     }
@@ -6629,7 +6629,7 @@ private:
     if (isa<PatternBindingDecl>(D)) {
       // We can't hoist a closure inside a PatternBindingDecl. If it contains
       // a call to the completion handler, wrap it in a continuation.
-      wrapScopeInContinationIfNecessary(D);
+      wrapScopeInContinuationIfNecessary(D);
       NestedExprCount++;
       return true;
     }
@@ -6703,7 +6703,7 @@ private:
                            [&]() { OS << newNameFor(D, true); });
       }
     } else if (CallExpr *CE = TopHandler.getAsHandlerCall(E)) {
-      if (Scopes.back().isWrappedInContination()) {
+      if (Scopes.back().isWrappedInContinuation()) {
         return addCustom(E->getSourceRange(),
                          [&]() { convertHandlerToContinuationResume(CE); });
       } else if (NestedExprCount == 0) {
@@ -6716,7 +6716,7 @@ private:
       //    middle of an expression)
       //  - the current scope is wrapped in a continuation (we can't have await
       //    calls in the continuation block)
-      if (NestedExprCount == 0 && !Scopes.back().isWrappedInContination()) {
+      if (NestedExprCount == 0 && !Scopes.back().isWrappedInContinuation()) {
         // If the refactoring is on the call itself, do not require the callee
         // to have the @available attribute or a completion-like name.
         auto HandlerDesc = AsyncHandlerParamDesc::find(
@@ -6731,7 +6731,7 @@ private:
 
     // We didn't do any special conversion for this expression. If needed, wrap
     // it in a continuation.
-    wrapScopeInContinationIfNecessary(E);
+    wrapScopeInContinuationIfNecessary(E);
 
     NestedExprCount++;
     return true;
@@ -6798,10 +6798,10 @@ private:
   bool walkToStmtPost(Stmt *S) override {
     if (startsNewScope(S)) {
       bool ClosedScopeWasWrappedInContinuation =
-          Scopes.back().isWrappedInContination();
+          Scopes.back().isWrappedInContinuation();
       Scopes.pop_back();
       if (ClosedScopeWasWrappedInContinuation &&
-          !Scopes.back().isWrappedInContination()) {
+          !Scopes.back().isWrappedInContinuation()) {
         // The nested scope was wrapped in a continuation but the current one
         // isn't anymore. Add the '}' that corresponds to the the call to
         // withChecked(Throwing)Continuation.
@@ -7193,7 +7193,7 @@ private:
   /// or error call.
   void convertHandlerToContinuationResumeImpl(const CallExpr *CE,
                                               HandlerResult Result) {
-    assert(Scopes.back().isWrappedInContination());
+    assert(Scopes.back().isWrappedInContinuation());
 
     std::vector<Argument> Args;
     StringRef ResumeArgumentLabel;
