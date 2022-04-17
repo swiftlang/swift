956605e01012535e47ea19cb0e48079e98eb262d
Josh Soref <jsoref@users.noreply.github.com>
2022-04-17 15:18:57 -0400

    spelling: cacheable
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- lib/SIL/IR/TypeLowering.cpp
+++ lib/SIL/IR/TypeLowering.cpp
@@ -2115,7 +2115,7 @@ TypeConverter::getTypeLowering(AbstractionPattern origType,
   auto loweredSubstType =
       computeLoweredRValueType(forExpansion, origType, substType);
 
-  // If that didn't change the type and the key is cachable, there's no
+  // If that didn't change the type and the key is cacheable, there's no
   // point in re-checking the table, so just construct a type lowering
   // and cache it.
   if (loweredSubstType == substType && key.isCacheable()) {
