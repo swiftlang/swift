ea92ae687405b730c4f8919eca0d0b619263710c
Josh Soref <jsoref@users.noreply.github.com>
2022-04-17 15:35:25 -0400

    spelling: intentionally
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- cmake/modules/AddSwift.cmake
+++ cmake/modules/AddSwift.cmake
@@ -718,7 +718,7 @@ function(add_swift_host_tool executable)
 
       elseif(BOOTSTRAPPING_MODE STREQUAL "CROSSCOMPILE-WITH-HOSTLIBS")
 
-        # Intentinally don't add the lib dir of the cross-compiled compiler, so that
+        # Intentionally don't add the lib dir of the cross-compiled compiler, so that
         # the stdlib is not picked up from there, but from the SDK.
         # This requires to explicitly add all the needed compatibility libraries. We
         # can take them from the current build.
