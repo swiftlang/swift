d905b4b0f9dcca2bb321a11f19dcfab91dd927fc
Josh Soref <jsoref@users.noreply.github.com>
2022-04-17 15:24:03 -0400

    spelling: default
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- include/swift/Runtime/Atomic.h
+++ include/swift/Runtime/Atomic.h
@@ -234,7 +234,7 @@ public:
 } // end namespace swift::impl
 
 /// A simple wrapper for std::atomic that provides the most important
-/// interfaces and fixes the API bug where all of the orderings dafault
+/// interfaces and fixes the API bug where all of the orderings default
 /// to sequentially-consistent.
 ///
 /// It also sometimes uses a different implementation in cases where
