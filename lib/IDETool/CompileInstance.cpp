c587885ff165913c1ed2bf9b1df120aa0f50d92e
Josh Soref <jsoref@users.noreply.github.com>
2022-04-17 15:18:59 -0400

    spelling: cached
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- lib/IDETool/CompileInstance.cpp
+++ lib/IDETool/CompileInstance.cpp
@@ -308,7 +308,7 @@ bool CompileInstance::performSema(
       CachedReuseCount < Opts.MaxASTReuseCount) {
     CI->getASTContext().CancellationFlag = CancellationFlag;
     if (!performCachedSemaIfPossible(DiagC)) {
-      // If we compileted cacehd Sema operation. We're done.
+      // If we compileted cached Sema operation. We're done.
       ++CachedReuseCount;
       return CI->getDiags().hadAnyError();
     }
