6a580c566bea2345bfca80774e6f15450b235bd9
Josh Soref <jsoref@users.noreply.github.com>
2022-04-17 15:37:23 -0400

    spelling: member
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- test/Interop/Cxx/templates/function-template-module-interface.swift
+++ test/Interop/Cxx/templates/function-template-module-interface.swift
@@ -7,7 +7,7 @@
 // CHECK: func templateParameterReturnType<R, T, U>(_ a: T, _ b: U) -> R
 // CHECK: func cannotInferTemplate<T>(T: T.Type)
 
-// CHECK: struct HasVariadicMemeber {
+// CHECK: struct HasVariadicMember {
 // CHECK:   @available(*, unavailable, message: "Variadic function is unavailable")
 // CHECK:   mutating func test1(_ varargs: Any...)
 // CHECK:   @available(*, unavailable, message: "Variadic function is unavailable")
