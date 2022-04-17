11c9839f935e201b5bcb6ec489dcae74c41e19b2
Josh Soref <jsoref@users.noreply.github.com>
2022-04-17 15:40:02 -0400

    spelling: optimization
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- include/swift/Basic/LangOptions.h
+++ include/swift/Basic/LangOptions.h
@@ -798,7 +798,7 @@ namespace swift {
     /// header, place it in this directory.
     std::string PrecompiledHeaderOutputDir;
 
-    /// The optimizaton setting.  This doesn't typically matter for
+    /// The optimization setting.  This doesn't typically matter for
     /// import, but it can affect Clang's IR generation of static functions.
     std::string Optimization;
 
