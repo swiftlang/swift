0d9fdf8c169aec8767238d11de9b8a104fb3a1c4
Josh Soref <jsoref@users.noreply.github.com>
2022-04-17 15:46:13 -0400

    spelling: requirement
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- lib/IDE/CompletionOverrideLookup.cpp
+++ lib/IDE/CompletionOverrideLookup.cpp
@@ -112,7 +112,7 @@ Type CompletionOverrideLookup::getOpaqueResultType(
   auto genericSig = VD->getDeclContext()->getGenericSignatureOfContext();
 
   if (genericSig->isConcreteType(ResultT))
-    // If it has same type requrement, we will emit the concrete type.
+    // If it has same type requirement, we will emit the concrete type.
     return nullptr;
 
   // Collect requirements on the associatedtype.
