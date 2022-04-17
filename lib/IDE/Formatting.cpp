72c99937d2cfb17816d82401f41de8f6fbe866b8
Josh Soref <jsoref@users.noreply.github.com>
2022-04-17 17:07:46 -0400

    spelling: preceding
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- lib/IDE/Formatting.cpp
+++ lib/IDE/Formatting.cpp
@@ -1462,7 +1462,7 @@ private:
         ISL->forEachSegment(SF.getASTContext(),
                             [&](bool IsInterpolation, CallExpr *CE) {
           if (IsInterpolation) {
-            // Handle the precedeing string segment.
+            // Handle the preceding string segment.
             CharSourceRange StringRange(SM, PrevStringStart, CE->getStartLoc());
             if (StringRange.contains(TargetLocation)) {
               StringLiteralRange =
