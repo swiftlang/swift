25de614fa4cb48db8f73fbf122dfb1f99b440fd4
Josh Soref <jsoref@users.noreply.github.com>
2022-04-17 15:45:51 -0400

    spelling: replacement
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- validation-test/stdlib/StringViews.swift
+++ validation-test/stdlib/StringViews.swift
@@ -184,7 +184,7 @@ tests.test("index-mapping/utf16-to-utf8/\(id)") {
       [0xf0, 0x9f, 0x8f],
 
       // Prior to UTF-8 String, this tested for empty array in "legacy mode" or
-      // the replacemnet character otherwise. However, SE-0180 (String Index
+      // the replacement character otherwise. However, SE-0180 (String Index
       // Overhaul) dictates subscript behavior should treat it as emergent
       // behavior from its encoded offset, hence we should get the same 3 code
       // units as prior for non-scalar-aligned UTF-16 offsets applied to the
@@ -455,7 +455,7 @@ tests.test("index-mapping/utf16-to-unicode-scalar/\(id)") {
     UnicodeScalar(0x1f3c2),
 
   // Prior to UTF-8 String, this tested for empty array in "legacy mode" or
-  // the replacemnet character otherwise. However, SE-0180 (String Index
+  // the replacement character otherwise. However, SE-0180 (String Index
   // Overhaul) dictates subscript behavior should treat it as emergent
   // behavior from its encoded offset, hence we should get the same 3 code
   // units as prior for non-scalar-aligned UTF-16 offsets applied to the
@@ -589,7 +589,7 @@ tests.test("index-mapping/utf16-to-character/\(id)") {
       "🏂",
 
       // Prior to UTF-8 String, this tested for empty array in "legacy mode" or
-      // the replacemnet character otherwise. However, SE-0180 (String Index
+      // the replacement character otherwise. However, SE-0180 (String Index
       // Overhaul) dictates subscript behavior should treat it as emergent
       // behavior from its encoded offset, hence we should get the same 3 code
       // units as prior for non-scalar-aligned UTF-16 offsets applied to the
