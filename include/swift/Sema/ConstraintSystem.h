5b5c44c63309a39e4dabdb14f5a4c6a2cd403a0f
Evan Wilde <etceterawilde@gmail.com>
2022-04-20 11:03:12 -0700

    Ignore async/sync mismatch on main
    The main function is different from other function resolutions. It isn't
    being called from a synchronous or asynchronous context, but defines
    whether the program starts in a synchronous or async context. As a
    result, we should not prefer one over the other, so the scoring
    mechanism shouldn't involve the async/sync score when resolving the main
    function.
    
    This patch adds a constraint solver flag to ignore async/sync context
    mismatches so that we do not favor one over the other, but otherwise use
    the normal resolution behavior.

--- include/swift/Sema/ConstraintSystem.h
+++ include/swift/Sema/ConstraintSystem.h
@@ -1515,6 +1515,9 @@ enum class ConstraintSystemFlags {
   /// calling conventions, say due to Clang attributes such as
   /// `__attribute__((ns_consumed))`.
   UseClangFunctionTypes = 0x80,
+
+  /// When set, ignore async/sync mismatches
+  IgnoreAsyncSyncMismatch = 0x100,
 };
 
 /// Options that affect the constraint system as a whole.
