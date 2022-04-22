af2af525c46a0a99bb79c02e970c6866a04783f1
Hamish Knight <hamish_github@mediocremail.com>
2022-04-22 14:01:15 +0100

    [Sema] Diagnose regex literals if `Regex<Output>` is unavailable
    Because we don't form a type-checked call to the
    Regex initializer in the AST, we need to explicitly
    handle the availability checking for `Regex<Output>`
    and the initializer we're implicitly calling.
    
    rdar://92156542

--- test/StringProcessing/Frontend/enable-flag.swift
+++ test/StringProcessing/Frontend/enable-flag.swift
@@ -1,6 +1,6 @@
-// RUN: %target-typecheck-verify-swift -enable-bare-slash-regex -enable-experimental-string-processing
-// RUN: %target-typecheck-verify-swift -enable-experimental-string-processing -enable-bare-slash-regex
-// RUN: %target-typecheck-verify-swift -disable-experimental-string-processing -enable-experimental-string-processing -enable-bare-slash-regex
+// RUN: %target-typecheck-verify-swift -disable-availability-checking -enable-bare-slash-regex -enable-experimental-string-processing
+// RUN: %target-typecheck-verify-swift -disable-availability-checking -enable-experimental-string-processing -enable-bare-slash-regex
+// RUN: %target-typecheck-verify-swift -disable-availability-checking -disable-experimental-string-processing -enable-experimental-string-processing -enable-bare-slash-regex
 
 // REQUIRES: swift_in_compiler
 
