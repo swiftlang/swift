40a930e6faa6a23238c880ade1a0340525b72851
Philippe Hausler <phausler@apple.com>
2022-05-03 10:51:32 -0700

    Ensure clocks properly respect leeways and use raw time for calculations for continuous clocks (#58396) (#58472)
    * Ensure clocks properly respect leeways and use raw time for calculations for continuous clocks
    
    * slurp up time calcualtions to inline forms
    
    * ensure the tolerance codepaths get tested as well
    
    * Use task local storage for task sources in leeway based calculations
    
    * use comparison assertions for leeway based calculations
    
    * Whitespace cleanup
    
    * Use the CLOCK_MONOTONIC value for linux to be compatible with linux scheduling for dispatch
    
    * remove incorrect paren in macro check for OpenBSD
    
    * Address feedback and remove stray ;

--- test/Concurrency/Runtime/clock.swift
+++ test/Concurrency/Runtime/clock.swift
@@ -17,8 +17,26 @@ var tests = TestSuite("Time")
         try! await clock.sleep(until: .now + .milliseconds(100))
       }
       // give a reasonable range of expected elapsed time
-      expectTrue(elapsed > .milliseconds(90))
-      expectTrue(elapsed < .milliseconds(200))
+      expectGT(elapsed, .milliseconds(90))
+      expectLT(elapsed, .milliseconds(200))
+    }
+
+    tests.test("ContinuousClock sleep with tolerance") {
+      let clock = ContinuousClock()
+      let elapsed = await clock.measure {
+        try! await clock.sleep(until: .now + .milliseconds(100), tolerance: .milliseconds(100))
+      }
+      // give a reasonable range of expected elapsed time
+      expectGT(elapsed, .milliseconds(90))
+      expectLT(elapsed, .milliseconds(300))
+    }
+
+    tests.test("ContinuousClock sleep longer") {
+      let elapsed = await ContinuousClock().measure {
+        try! await Task.sleep(until: .now + .seconds(1), clock: .continuous)
+      }
+      expectGT(elapsed, .seconds(1) - .milliseconds(90))
+      expectLT(elapsed, .seconds(1) + .milliseconds(200))
     }
 
     tests.test("SuspendingClock sleep") {
@@ -27,8 +45,26 @@ var tests = TestSuite("Time")
         try! await clock.sleep(until: .now + .milliseconds(100))
       }
       // give a reasonable range of expected elapsed time
-      expectTrue(elapsed > .milliseconds(90))
-      expectTrue(elapsed < .milliseconds(200))
+      expectGT(elapsed, .milliseconds(90))
+      expectLT(elapsed, .milliseconds(200))
+    }
+
+    tests.test("SuspendingClock sleep with tolerance") {
+      let clock = SuspendingClock()
+      let elapsed = await clock.measure {
+        try! await clock.sleep(until: .now + .milliseconds(100), tolerance: .milliseconds(100))
+      }
+      // give a reasonable range of expected elapsed time
+      expectGT(elapsed, .milliseconds(90))
+      expectLT(elapsed, .milliseconds(300))
+    }
+
+    tests.test("SuspendingClock sleep longer") {
+      let elapsed = await SuspendingClock().measure {
+        try! await Task.sleep(until: .now + .seconds(1), clock: .suspending)
+      }
+      expectGT(elapsed, .seconds(1) - .milliseconds(90))
+      expectLT(elapsed, .seconds(1) + .milliseconds(200))
     }
 
     tests.test("duration addition") {
