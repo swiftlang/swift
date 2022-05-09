d840642b25e23a6464c534c27bb6ef7e8c1390c9
Victoria Mitchell <victoria_m@apple.com>
2022-05-09 15:52:50 -0600

    make switch non-exhaustive

--- lib/AST/TypeRepr.cpp
+++ lib/AST/TypeRepr.cpp
@@ -537,6 +537,11 @@ void NamedOpaqueReturnTypeRepr::printImpl(ASTPrinter &Printer,
 void SpecifierTypeRepr::printImpl(ASTPrinter &Printer,
                                   const PrintOptions &Opts) const {
   switch (getKind()) {
+#define TYPEREPR(CLASS, PARENT) case TypeReprKind::CLASS:
+#define SPECIFIER_TYPEREPR(CLASS, PARENT)
+#include "swift/AST/TypeReprNodes.def"
+    llvm_unreachable("invalid repr kind");
+    break;
   case TypeReprKind::InOut:
     Printer.printKeyword("inout", Opts, " ");
     break;
@@ -551,9 +556,6 @@ void SpecifierTypeRepr::printImpl(ASTPrinter &Printer,
     break;
   case TypeReprKind::CompileTimeConst:
     Printer.printKeyword("_const", Opts, " ");
-    break;
-  default:
-    llvm_unreachable("unknown specifier type repr");
     break;
   }
   printTypeRepr(Base, Printer, Opts);
