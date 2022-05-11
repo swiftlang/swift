7206bc33ba3b4e9363a00232c6bd2f5e50a998b9
Robert Widmann <devteam.codafi@gmail.com>
2022-05-11 04:13:56 -0700

    Remove Unused Type Nodes

--- lib/Serialization/ModuleFormat.h
+++ lib/Serialization/ModuleFormat.h
@@ -58,7 +58,7 @@ const uint16_t SWIFTMODULE_VERSION_MAJOR = 0;
 /// describe what change you made. The content of this comment isn't important;
 /// it just ensures a conflict if two people change the module format.
 /// Don't worry about adhering to the 80-column limit for this line.
-const uint16_t SWIFTMODULE_VERSION_MINOR = 689; // cast ownership serialization
+const uint16_t SWIFTMODULE_VERSION_MINOR = 690; // remove unused type nodes
 
 /// A standard hash seed used for all string hashes in a serialized module.
 ///
