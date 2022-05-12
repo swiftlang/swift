ec4f51ce175d566c5bc8f3ca325ebb9abcc9f4ff
Serena <osama.id78@gmail.com>
2022-05-13 00:18:24 +0300

    change string_interpolation_unclosed to sound more natural

--- test/Parse/multiline_errors.swift
+++ test/Parse/multiline_errors.swift
@@ -126,14 +126,14 @@ _ = "hello\("""
             world
             """
             )!"
-            // expected-error@-4 {{string interpolation needs to be closed by a )}}
+            // expected-error@-4 {{expected ')' at end of string interpolation}}
             // expected-error@-2 {{unterminated string literal}}
 
 _ = "hello\(
             """
             world
             """)!"
-            // expected-error@-4 {{string interpolation needs to be closed by a )}}
+            // expected-error@-4 {{expected ')' at end of string interpolation}}
             // expected-error@-2 {{unterminated string literal}}
 
 _ = """
@@ -190,4 +190,4 @@ let _ = """
   \("bar
   baz
   """
-  // expected-error@-3 {{string interpolation needs to be closed by a )}}
+  // expected-error@-3 {{expected ')' at end of string interpolation}}
--- test/Parse/string_literal_eof7.swift
+++ test/Parse/string_literal_eof7.swift
@@ -1,6 +1,6 @@
 // RUN: %target-typecheck-verify-swift
 
-// expected-error@+4 {{string interpolation needs to be closed by a )}}
+// expected-error@+4 {{expected ')' at end of string interpolation}}
 // expected-error@+1 {{unterminated string literal}}
 _ = """
     foo
