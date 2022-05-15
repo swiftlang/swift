4ef5c99e8ba9b3f93773d27650f4c69af7114301
Serena <osama.id78@gmail.com>
2022-05-16 00:54:42 +0300

    Trigger string_interpolation_unclosed only when CurPtr ends in a \r or \n

--- lib/Parse/Lexer.cpp
+++ lib/Parse/Lexer.cpp
@@ -1884,20 +1884,25 @@ void Lexer::lexStringLiteral(unsigned CustomDelimiterLen) {
         // Successfully scanned the body of the expression literal.
         ++CurPtr;
         continue;
-      } else if ((*CurPtr == '\r' || *CurPtr == '\n') && IsMultilineString) {
+      } else if (*CurPtr == '\r' || *CurPtr == '\n') {
+        if (IsMultilineString) {
         // The only case we reach here is unterminated single line string in the
         // interpolation. For better recovery, go on after emitting an error.
         diagnose(CurPtr, diag::string_interpolation_unclosed);
         diagnose(CurPtr, diag::lex_unterminated_string);
         wasErroneous = true;
         continue;
-      } else {
+        } else {
         const char *fixItLoc = CurPtr - 1;
         if (*fixItLoc == QuoteChar) {
           fixItLoc -= CustomDelimiterLen;
         }
         diagnose(fixItLoc, diag::string_interpolation_unclosed);
             .fixItInsert(Lexer::getSourceLoc(fixItLoc), ")");
+        return formToken(tok::unknown, TokStart);
+        }
+      } else {
+        diagnose(TokStart, diag::lex_unterminated_string);
         return formToken(tok::unknown, TokStart);
       }
     }
