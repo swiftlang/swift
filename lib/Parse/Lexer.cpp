caeffb91812bdc91314a00df8d767280d79cda0b
Serena <osama.id78@gmail.com>
2022-05-19 23:29:28 +0300

    shouldn't have been there

--- lib/Parse/Lexer.cpp
+++ lib/Parse/Lexer.cpp
@@ -1905,7 +1905,6 @@ void Lexer::lexStringLiteral(unsigned CustomDelimiterLen) {
         wasErroneous = true;
 
         continue;
-        return formToken(tok::unknown, TokStart);
       }
     }
 
