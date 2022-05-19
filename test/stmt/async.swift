d461eab3f0bb0262fd72ef8c624861d317777986
Pavel Yaskevich <pyaskevich@apple.com>
2022-05-19 14:47:01 -0700

    [TypeChecker] Effects: Don't diagnose implicit AST nodes
    Implicitly generated code requires checking but it doesn't have
    to produce a diagnostic into the void.

--- test/stmt/async.swift
+++ test/stmt/async.swift
@@ -7,9 +7,8 @@ func omnom(_ x: Int)
 
 func f() async -> Int { 0 }
 
-func syncContext() {        // expected-note 4 {{add 'async' to function 'syncContext()' to make it asynchronous}}
+func syncContext() {        // expected-note 3 {{add 'async' to function 'syncContext()' to make it asynchronous}}
     _ = await f()           // expected-error{{'async' call in a function that does not support concurrency}}
     async let y = await f() // expected-error{{'async let' in a function that does not support concurrency}}
-                            // expected-error@-1{{'async' call in a function that does not support concurrency}}
     await omnom(y)          // expected-error{{'async let' in a function that does not support concurrency}}
 }
