2422c0c111f8081ce75899209b08e1eec7a000de
Ehud Adler <easports96@gmail.com>
2022-05-27 15:14:52 -0400

    Keep the XFAIL on windows for now

--- test/Interop/Cxx/operators/member-inline-irgen.swift
+++ test/Interop/Cxx/operators/member-inline-irgen.swift
@@ -1,12 +1,13 @@
 // RUN: %target-swift-emit-ir %s -I %S/Inputs -enable-experimental-cxx-interop | %FileCheck %s
 //
-// We can't yet call member functions correctly on Windows (SR-13129).
+// We should be able to support windows now. We will remove XFAIL in follow up
+// XFAIL: windows
 
 import MemberInline
 
 public func sub(_ lhs: inout LoadableIntWrapper, _ rhs: LoadableIntWrapper) -> LoadableIntWrapper { lhs - rhs }
 
-// CHECK: call [[RESA:i32|i64]] [[NAMEA:@(_ZN18LoadableIntWrappermiES_|"\?\?__Ec@@YAXXZ")]](%struct.LoadableIntWrapper* {{%[0-9]+}}, {{i32|\[1 x i32\]|i64|%struct.LoadableIntWrapper\* byval\(.*\) align 4}} {{%[0-9]+}})
+// CHECK: call [[RESA:i32|i64]] [[NAMEA:@(_ZN18LoadableIntWrappermiES_|"\?\?GLoadableIntWrapper@@QEAA\?AU0@U0@@Z")]](%struct.LoadableIntWrapper* {{%[0-9]+}}, {{i32|\[1 x i32\]|i64|%struct.LoadableIntWrapper\* byval\(.*\) align 4}} {{%[0-9]+}})
 
 public func call(_ wrapper: inout LoadableIntWrapper, _ arg: Int32) -> Int32 { wrapper(arg) }
 
