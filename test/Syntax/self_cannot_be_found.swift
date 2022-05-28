721a788ed20d1b4800472f7d286bc96a182edacc
Serena <osama.id78@gmail.com>
2022-05-28 15:57:17 +0300

    Update text in test errors to reflect previous changes

--- test/Syntax/self_cannot_be_found.swift
+++ test/Syntax/self_cannot_be_found.swift
@@ -1,8 +1,8 @@
 // RUN: %target-typecheck-verify-swift
 
-let _ = self // expected-error {{cannot find `self` in scope, did you mean to declare or extend a type?}} 
-let s = self.b // // expected-error {{cannot find `self` in scope, did you mean to declare or extend a type?}} 
-let w = self.method() // expected-error {{cannot find `self` in scope, did you mean to declare or extend a type?}} 
+let _ = self // expected-error {{cannot find 'self' in scope, did you mean to declare or extend a type?}} 
+let s = self.b // // expected-error {{cannot find 'self' in scope, did you mean to declare or extend a type?}} 
+let w = self.method() // expected-error {{cannot find 'self' in scope, did you mean to declare or extend a type?}} 
 
 struct aStruct {
     let b = "a"
