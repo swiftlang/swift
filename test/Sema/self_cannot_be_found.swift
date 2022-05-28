0bd1e841483efa6bf3e631be6a88d522ffad1755
Serena <osama.id78@gmail.com>
2022-05-29 00:59:15 +0300

    within -> in

--- test/Sema/self_cannot_be_found.swift
+++ test/Sema/self_cannot_be_found.swift
@@ -1,8 +1,8 @@
 // RUN: %target-typecheck-verify-swift
 
-let _ = self // expected-error {{cannot find 'self' in scope; did you mean to use it within a type or extension context?}} 
-let s = self.b // // expected-error {{cannot find 'self' in scope; did you mean to use it within a type or extension context?}} 
-let w = self.method() // expected-error {{cannot find 'self' in scope; did you mean to use it within a type or extension context?}} 
+let _ = self // expected-error {{cannot find 'self' in scope; did you mean to use it in a type or extension context?}} 
+let s = self.b // // expected-error {{cannot find 'self' in scope; did you mean to use it in a type or extension context?}} 
+let w = self.method() // expected-error {{cannot find 'self' in scope; did you mean to use it in a type or extension context?}} 
 
 struct aStruct {
     let b = "a"
