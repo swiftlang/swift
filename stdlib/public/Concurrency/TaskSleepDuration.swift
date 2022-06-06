354520b4a9147af7f85af32b9d568650deff0e62
Stephen Celis <stephen@stephencelis.com>
2022-06-06 07:42:21 -0400

    Update stdlib/public/Concurrency/TaskSleepDuration.swift
    Co-authored-by: Xiaodi Wu <13952+xwu@users.noreply.github.com>

--- stdlib/public/Concurrency/TaskSleepDuration.swift
+++ stdlib/public/Concurrency/TaskSleepDuration.swift
@@ -154,7 +154,7 @@ extension Task where Success == Never, Failure == Never {
   ///
   /// - Parameter duration: The duration to wait.
   @available(SwiftStdlib 5.7, *)
-  public static func sleep(
+  public static func sleep(for duration: Duration) async throws {
     for duration: Duration
   ) async throws {
     try await sleep(until: .now + duration, clock: .continuous)
