753b68acbe84b50502f7226cc7ae4e5e8f1b3a27
Pavel Yaskevich <pyaskevich@apple.com>
2022-06-10 16:28:23 -0700

    [Distributed] Add a new access strategy - DispatchToDistributedThunk
    This strategy is used to dispatch accesses to 'distributed' computed
    property to distributed thunk accessor instead of a regular getter
    when access happen outside actor isolation context.

--- lib/AST/Decl.cpp
+++ lib/AST/Decl.cpp
@@ -2259,10 +2259,8 @@ AbstractStorageDecl::getAccessStrategy(AccessSemantics semantics,
     assert(hasStorage());
     return AccessStrategy::getStorage();
 
-  // FIXME: For now `DistributedThunk` behaves just like `Ordinary` but it
-  //        needs a new strategy to be useful.
   case AccessSemantics::DistributedThunk:
-    LLVM_FALLTHROUGH;
+    return AccessStrategy::getDistributedThunkDispatchStrategy();
 
   case AccessSemantics::Ordinary:
     // Skip these checks for local variables, both because they're unnecessary
