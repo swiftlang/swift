3efb7a816d7c09585f0a143405254cebdcb7368b
Serena <osama.id78@gmail.com>
2022-06-14 14:33:47 +0300

    Edit test invokation to include all (current) darwin platforms

--- test/Parse/ConditionalCompilation/if_darwin.swift
+++ test/Parse/ConditionalCompilation/if_darwin.swift
@@ -1,6 +1,7 @@
-// RUN: %swift -typecheck %s -verify -target arm64-apple-ios8.0
-
-// REQUIRES: OS=ios
+// RUN: %swift -typecheck %s -verify -target arm64-apple-ios8.0 -parse-stdlib
+// RUN: %swift -typecheck %s -verify -target arm64-apple-tvos9 -parse-stdlib
+// RUN: %swift -typecheck %s -verify -target i386-apple-watchos4.0 -parse-stdlib
+// RUN: %swift -typecheck %s -verify -target x86_64-apple-macos11.0 -parse-stdlib
 
 #if os(Darwin)
 func xDarwinFunc() {}
@@ -8,5 +9,13 @@ func xDarwinFunc() {}
 func someOtherPlatformFunc() {}
 #endif
 
+#if !os(Darwin) 
+let someNum = 3
+#endif
+
 xDarwinFunc()
 someOtherPlatformFunc() // expected-error {{cannot find 'someOtherPlatformFunc' in scope}}
+
+func anotherFunc() -> Int {
+    return someNum // expected-error {{cannot find 'someNum' in scope}}
+}
