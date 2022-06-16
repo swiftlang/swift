ca2fe59b2f0c8443f58e5c7d693bd0a6b3663267
Andrew Breckenridge <asbreckenridge@me.com>
2022-06-15 18:31:08 -0700

    legal reasons are ostensibly not silly

--- validation-test/Python/copyright-conformance.swift
+++ validation-test/Python/copyright-conformance.swift
@@ -1,3 +1,3 @@
 // Checks to make sure all copyright headers conform with their
-// git creation/last modification date for silly legal reasons.
+// git creation/last modification date.
 // RUN: python3 %utils/copyright-conformance.py -rf %swift_obj_root
