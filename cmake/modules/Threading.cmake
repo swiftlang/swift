4f538f185f09b0a98ca4796e98616074e98fafa3
Alastair Houghton <ahoughton@apple.com>
2022-06-20 14:09:09 +0100

    [Threading] Fix threading package defaults.
    These were *supposed* to work for cross-compilation, but because of
    an error in `Threading.cmake`, they didn't.  Also, the test rig should
    be using `threading_package_name()` rather than `threading_package_default()`,
    so that any user override takes precedence.

--- cmake/modules/Threading.cmake
+++ cmake/modules/Threading.cmake
@@ -1,5 +1,7 @@
 # Get the default threading package for the platform
 function(threading_package_default sdk out_var)
+    precondition(SWIFT_DARWIN_PLATFORMS)
+
     if(sdk IN_LIST SWIFT_DARWIN_PLATFORMS)
       set("${out_var}" "darwin" PARENT_SCOPE)
     elseif(sdk STREQUAL "LINUX")
@@ -17,12 +19,9 @@ endfunction()
 # define that we need to make.  Also deals with the default platform
 # setting.
 function(threading_package_name sdk out_var)
-  precondition(SWIFT_HOST_VARIANT_SDK)
-  precondition(SWIFT_DARWIN_PLATFORMS)
-
   string(TOUPPER "${SWIFT_THREADING_PACKAGE}" package)
   if(package STREQUAL "")
-    threading_package_default(package)
+    threading_package_default(sdk package)
     string(TOUPPER "${package}" package)
   endif()
   set("${out_var}" "${package}" PARENT_SCOPE)
