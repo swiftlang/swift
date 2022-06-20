019a86be9d02dac7ea0f6c5b981b6be25a5db82c
Pavel Yaskevich <pyaskevich@apple.com>
2022-06-20 12:56:08 -0700

    [ResultBuilder] Don't run syntactic diagnostics on transformed do statements
    Visiting of transformed `do` shouldn't run syntatic diagnostics because
    they are going to be performed by `visit` that called `visitDoStmt`.

--- lib/Sema/CSClosure.cpp
+++ lib/Sema/CSClosure.cpp
@@ -1223,13 +1223,16 @@ private:
     return nullptr;
   }
 
-  ASTNode visit(Stmt *S) {
+  ASTNode visit(Stmt *S, bool performSyntacticDiagnostics = true) {
     auto rewritten = ASTVisitor::visit(S);
     if (!rewritten)
       return {};
 
-    if (auto *stmt = getAsStmt(rewritten))
-      performStmtDiagnostics(stmt, context.getAsDeclContext());
+    if (performSyntacticDiagnostics) {
+      if (auto *stmt = getAsStmt(rewritten)) {
+        performStmtDiagnostics(stmt, context.getAsDeclContext());
+      }
+    }
 
     return rewritten;
   }
@@ -1755,8 +1758,9 @@ public:
 
 private:
   ASTNode visitDoStmt(DoStmt *doStmt) override {
-    if (auto transformed = transformDo(doStmt))
-      return visit(transformed.get());
+    if (auto transformed = transformDo(doStmt)) {
+      return visit(transformed.get(), /*performSyntacticDiagnostics=*/false);
+    }
 
     auto newBody = visit(doStmt->getBody());
     if (!newBody)
