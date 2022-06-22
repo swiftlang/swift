e78a7cbacb98b3ef614bbde22fe6970f01b0b2ce
Konrad `ktoso` Malawski <konrad_malawski@apple.com>
2022-06-22 15:35:55 +0900

    [Decl] remove not used and bad-idea isIsolated function

--- lib/AST/Decl.cpp
+++ lib/AST/Decl.cpp
@@ -7781,10 +7781,6 @@ bool AbstractFunctionDecl::isSendable() const {
   return getAttrs().hasAttribute<SendableAttr>();
 }
 
-bool AbstractFunctionDecl::isNonisolated() const {
-  return getAttrs().hasAttribute<NonisolatedAttr>();
-}
-
 bool AbstractFunctionDecl::isBackDeployed() const {
   if (getAttrs().hasAttribute<BackDeployAttr>())
     return true;
