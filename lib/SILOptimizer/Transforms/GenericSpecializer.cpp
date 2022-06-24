9ef9c154f6a509ecbc297322d85f6b88ec4a35bb
Erik Eckstein <eeckstein@apple.com>
2022-06-24 14:43:03 +0200

    MandatoryGenericSpecializer: run optimizations in a loop
    To avoid phase ordering problems of the involved optimizations, iterate until we reach a fixed point.

--- lib/SILOptimizer/Transforms/GenericSpecializer.cpp
+++ lib/SILOptimizer/Transforms/GenericSpecializer.cpp
@@ -226,10 +226,20 @@ void MandatoryGenericSpecializer::run() {
       continue;
 
     // Perform generic specialization and other related optimzations.
-    bool changed = optimize(func, cha);
 
-    if (changed)
-      invalidateAnalysis(func, SILAnalysis::InvalidationKind::Everything);
+    // To avoid phase ordering problems of the involved optimizations, iterate
+    // until we reach a fixed point.
+    // This should always happen, but to be on the save side, limit the number
+    // of iterations to 10 (which is more than enough - usually the loop runs
+    // 1 to 3 times).
+    for (int i = 0; i < 10; i++) {
+      bool changed = optimize(func, cha);
+      if (changed) {
+        invalidateAnalysis(func, SILAnalysis::InvalidationKind::Everything);
+      } else {
+        break;
+      }
+    }
 
     // Continue specializing called functions.
     for (SILBasicBlock &block : *func) {
