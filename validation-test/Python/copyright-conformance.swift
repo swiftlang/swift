ff70edea883fb8568505fa14f1e7f034faf15da3
Andrew Breckenridge <asbreckenridge@me.com>
2022-06-27 18:09:42 -0700

    validation-test/copyright: update invocation to use python argument

--- validation-test/Python/copyright-conformance.swift
+++ validation-test/Python/copyright-conformance.swift
@@ -1,3 +1,3 @@
 // Checks to make sure all copyright headers conform with their
 // git creation/last modification date.
-// RUN: python3 %utils/copyright-conformance.py -rf %swift_obj_root
+// RUN: %{python} %utils/copyright-conformance.py -rf %swift_obj_root
