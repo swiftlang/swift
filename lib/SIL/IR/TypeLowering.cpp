5adb94ca9f1bb61d5e9c66a54fca5d54ca05de48
Michael Gottesman <mgottesman@apple.com>
2022-06-28 17:02:52 -0700

    [no-implicit-copy] Remove the move only bit in type lowering for now.
    We currently do not need it so I am removing it. We /do/ use move only as a
    TypeLowering itself though. Just not as a property.

--- lib/SIL/IR/TypeLowering.cpp
+++ lib/SIL/IR/TypeLowering.cpp
@@ -244,22 +244,10 @@ namespace {
                                            RecursiveProperties::forReference());
     }
 
-    RecursiveProperties getMoveOnlyReferenceRecursiveProperties(
-        IsTypeExpansionSensitive_t isSensitive) {
-      return mergeIsTypeExpansionSensitive(isSensitive,
-                                           RecursiveProperties::forReference());
-    }
-
     RecursiveProperties
     getOpaqueRecursiveProperties(IsTypeExpansionSensitive_t isSensitive) {
       return mergeIsTypeExpansionSensitive(isSensitive,
                                            RecursiveProperties::forOpaque());
-    }
-
-    RecursiveProperties getMoveOnlyOpaqueRecursiveProperties(
-        IsTypeExpansionSensitive_t isSensitive) {
-      return mergeIsTypeExpansionSensitive(
-          isSensitive, RecursiveProperties::forMoveOnlyOpaque());
     }
 
 #define IMPL(TYPE, LOWERING)                                                 \
@@ -704,12 +692,12 @@ namespace {
       if (lowering.isAddressOnly()) {
         return asImpl().handleMoveOnlyAddressOnly(
             type->getCanonicalType(),
-            getMoveOnlyOpaqueRecursiveProperties(isSensitive));
+            getOpaqueRecursiveProperties(isSensitive));
       }
 
       return asImpl().handleMoveOnlyReference(
           type->getCanonicalType(),
-          getMoveOnlyReferenceRecursiveProperties(isSensitive));
+          getReferenceRecursiveProperties(isSensitive));
     }
 
     RetTy handleAggregateByProperties(CanType type, RecursiveProperties props) {
