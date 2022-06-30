f63800b169a299f65cafc864795689c797acce29
Allan Shortlidge <shortlidge@apple.com>
2022-06-30 09:10:46 -0700

    NFC: Test that `@_spi` decls are emitted in ObjC compatibility headers.
    Some projects depend on `@_spi` decls being emitted this way so we should have a test that asserts this is expected behavior.

---
+++ test/PrintAsObjC/spi.swift
@@ -0,0 +1,22 @@
+// RUN: %empty-directory(%t)
+// RUN: %target-swift-frontend(mock-sdk: %clang-importer-sdk) %s -typecheck -emit-objc-header-path %t/out.h
+// RUN: %FileCheck %s < %t/out.h
+
+// REQUIRES: objc_interop
+
+import Foundation
+
+// CHECK-LABEL: @interface PublicClass : NSObject{{$}}
+public class PublicClass: NSObject {
+  // CHECK-NEXT: - (void)spiMember;
+  @_spi(A) @objc public func spiMember() {}
+  // CHECK-NEXT: init
+} // CHECK-NEXT: @end
+
+// CHECK-LABEL: @interface SPIClass : NSObject{{$}}
+@_spi(B)
+public class SPIClass: NSObject {
+  // CHECK-NEXT: - (void)publicMember;
+  @objc public func publicMember() {}
+  // CHECK-NEXT: init
+} // CHECK-NEXT: @end
