4dac1088f812915af60a79651bad49027f6b2085
Kavon Farvardin <kfarvardin@apple.com>
2022-06-30 15:48:19 -0700

    add objc coverage to actor_init_abi test

--- test/Concurrency/Runtime/Inputs/MysteryInit.swift
+++ test/Concurrency/Runtime/Inputs/MysteryInit.swift
@@ -1,19 +1,43 @@
+import Foundation
 
 // This input is useful to ensure the delegation status of
 // an actor's initializer does not affect ABI stability
-public actor BigFoot {
 
-  public let name: String
+public protocol NamedEntity: Actor {
+  nonisolated var name: String? { get }
+}
+
+public actor BigFoot: NamedEntity {
+
+  public nonisolated let name: String?
 
   private init(withName name: String) {
     self.name = name
   }
 
-  public init?() {
+  public init() {
   #if DELEGATES
     self.init(withName: "Sasquatch")
   #else
-    return nil
+    self.name = nil
+  #endif
+  }
+}
+
+
+@objc public actor BigFootObjC: NSObject, NamedEntity {
+
+  public nonisolated let name: String?
+
+  private init(withName name: String) {
+    self.name = name
+  }
+
+  @objc public override init() {
+  #if DELEGATES
+    self.init(withName: "Sasquatch")
+  #else
+    self.name = nil
   #endif
   }
 }
