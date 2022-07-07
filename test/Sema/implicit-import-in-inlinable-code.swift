55d7e6d8fdffb1af943fedab161573d45606b588
Egor Zhdan <e_zhdan@apple.com>
2022-07-07 15:18:49 +0100

    Revert "[Sema] Make "not-imported" diagnostic more direct"
    This reverts commit 8fc80f96d5fa5e3e24b1aa179f9f3319d7ca100d.

--- test/Sema/implicit-import-in-inlinable-code.swift
+++ test/Sema/implicit-import-in-inlinable-code.swift
@@ -38,7 +38,7 @@ import libA
 
 @inlinable public func bar() {
   let a = ImportedType()
-  a.implicitlyImportedMethod() // expected-warning {{instance method 'implicitlyImportedMethod()' cannot be used in an '@inlinable' function because 'libB' was not imported by this file; this is an error in Swift 6}}
+  a.implicitlyImportedMethod() // expected-warning {{instance method 'implicitlyImportedMethod()' cannot be used in an '@inlinable' function because 'libB' was implicitly imported; this is an error in Swift 6}}
 
   // Expected implicit imports are still fine
   a.localModuleMethod()
@@ -50,7 +50,7 @@ import libA
 
 @inlinable public func bar() {
   let a = ImportedType()
-  a.implicitlyImportedMethod() // expected-error {{instance method 'implicitlyImportedMethod()' cannot be used in an '@inlinable' function because 'libB' was not imported by this file}}
+  a.implicitlyImportedMethod() // expected-error {{instance method 'implicitlyImportedMethod()' cannot be used in an '@inlinable' function because 'libB' was implicitly imported}}
 
   // Expected implicit imports are still fine
   a.localModuleMethod()
@@ -61,7 +61,7 @@ import libA
 
 @inlinable public func bar() {
   let a = ImportedType()
-  a.implicitlyImportedMethod() // expected-error {{instance method 'implicitlyImportedMethod()' cannot be used in an '@inlinable' function because 'libB' was not imported by this file}}
+  a.implicitlyImportedMethod() // expected-error {{instance method 'implicitlyImportedMethod()' cannot be used in an '@inlinable' function because 'libB' was implicitly imported}}
 
   // Expected implicit imports are still fine
   a.localModuleMethod()
