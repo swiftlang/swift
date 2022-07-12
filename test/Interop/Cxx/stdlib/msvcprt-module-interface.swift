b94f4a5662ae2c13ab992b7b7aa7c5a1f34d2160
Ehud Adler <easports96@gmail.com>
2022-07-12 14:06:38 -0400

    Added test for constexpr issue

---
+++ test/Interop/Cxx/stdlib/msvcprt-module-interface.swift
@@ -0,0 +1,23 @@
+// RUN: %empty-directory(%t)
+// RUN: %target-swift-ide-test -print-module -module-to-print std -source-filename none -enable-experimental-cxx-interop -module-cache-path %t | %FileCheck %s -check-prefix CHECK-STD
+// RUN: %target-swift-ide-test -print-module -module-to-print=std.string -source-filename=x -enable-experimental-cxx-interop -module-cache-path %t | %FileCheck %s -check-prefix CHECK-STRING
+
+// This test is specific to msvcprt and therefore only runs on Windows.
+// REQUIRES: OS=windows-msvc
+
+// CHECK-STD: import std.iosfwd
+// CHECK-STD: import std.string
+
+// CHECK-STRING: enum std {
+// CHECK-STRING:   struct __CxxTemplateInstSs {
+// CHECK-STRING:     typealias value_type = CChar
+// CHECK-STRING:   }
+// CHECK-STRING:   struct __CxxTemplateInstSbIwSt11char_traitsIwESaIwEE {
+// CHECK-STRING:     typealias value_type = CWideChar
+// CHECK-STRING:   }
+// CHECK-STRING:   static func to_string(_ _Val: Int32) -> std.string
+// CHECK-STRING:   static func to_wstring(_ _Val: Int32) -> std.wstring
+// CHECK-STRING:   typealias size_t = size_t
+// CHECK-STRING:   typealias string = __CxxTemplateInstSs
+// CHECK-STRING:   typealias wstring = __CxxTemplateInstSbIwSt11char_traitsIwESaIwEE
+// CHECK-STRING: }
