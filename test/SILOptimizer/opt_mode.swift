d44b7927bea33af43f23d819dc226d957545203d
Arnold Schwaighofer <aschwaighofer@apple.com>
2022-07-15 10:32:55 -0700

    Fix tests

--- test/SILOptimizer/opt_mode.swift
+++ test/SILOptimizer/opt_mode.swift
@@ -47,5 +47,5 @@ func test_ospeed(_ a: A) -> Int {
 }
 
 
-// CHECK-IR: attributes [[SIZE_ATTR]] = { minsize optsize "
-// CHECK-IR: attributes [[NOSIZE_ATTR]] = { "
+// CHECK-IR-DAG: attributes [[SIZE_ATTR]] = { minsize optsize "
+// CHECK-IR-DAG: attributes [[NOSIZE_ATTR]] = { "
