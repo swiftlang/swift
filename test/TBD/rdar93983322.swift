d3abce1e0fd0be54abee4352ff091045940edc57
Zixu Wang <zixu_wang@apple.com>
2022-07-19 12:14:33 -0700

    [TBDGen] Simplify test case TBD/rdar93983322.swift
    Framework model is not related to the issue. Remove framework layouts
    and search paths.

--- test/TBD/rdar93983322.swift
+++ test/TBD/rdar93983322.swift
@@ -1,27 +1,26 @@
 // REQUIRES: OS=macosx
 // RUN: %empty-directory(%t)
-// RUN: mkdir -p %t/Frameworks/MyModule.framework/Modules/MyModule.swiftmodule
-// RUN: mkdir -p %t/ProjectInternal/ProjectInternal.framework/Modules/ProjectInternal.swiftmodule
+// RUN: mkdir -p %t/Modules
 // RUN: split-file %s %t
 
 // Build the project internal module
 // RUN: %target-swift-frontend %t/ProjectInternal.swift \
 // RUN:   -emit-module -module-name ProjectInternal \
-// RUN:   -o %t/ProjectInternal/ProjectInternal.framework/Modules/ProjectInternal.swiftmodule/%module-target-triple.swiftmodule \
+// RUN:   -o %t/Modules/ProjectInternal.swiftmodule \
 // RUN:   -disable-experimental-string-processing
 
 // Build MyModule with search path to the project internal module
 // RUN: %target-swift-frontend %t/MyModule.swift \
 // RUN:   -emit-module -module-name MyModule \
-// RUN:   -o %t/Frameworks/MyModule.framework/Modules/MyModule.swiftmodule/%module-target-triple.swiftmodule \
-// RUN:   -Fsystem %t/ProjectInternal -disable-experimental-string-processing
+// RUN:   -o %t/Modules/MyModule.swiftmodule \
+// RUN:   -I %t/Modules -disable-experimental-string-processing
 
 // Remove the project internal module as it's not available to clients of MyModule
-// RUN: rm -rf %t/ProjectInternal
+// RUN: rm %t/Modules/ProjectInternal.swiftmodule
 
 // Use swift-api-extract to load MyModule without ProjectInternal
 // RUN: %target-swift-api-extract -o - -pretty-print \
-// RUN:   -module-name MyModule -Fsystem %t/Frameworks
+// RUN:   -module-name MyModule -I %t/Modules
 
 //--- ProjectInternal.swift
 public class ProjectInternalClass {}
