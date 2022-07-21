0f83b65f9b4f49a91ac66e7952cd56e3dac35782
Ehud Adler <Huddie@users.noreply.github.com>
2022-07-21 15:41:17 -0400

    [cxx-interop] Remove impl for constexpr members (#60154)
    * Remove impl for constexpr members
    
    * Readd some tets
    
    * Only remove import of static constexpr

--- lib/ClangImporter/ImportDecl.cpp
+++ lib/ClangImporter/ImportDecl.cpp
@@ -3273,49 +3273,8 @@ namespace {
       return nullptr;
     }
 
-    AccessorDecl *tryCreateConstexprAccessor(const clang::VarDecl *clangVar,
-                                             VarDecl *swiftVar) {
-      assert(clangVar->isConstexpr());
-      clangVar->evaluateValue();
-      auto evaluated = clangVar->getEvaluatedValue();
-      if (!evaluated)
-        return nullptr;
-
-      // If we have a constexpr var with an evaluated value, try to create an
-      // accessor for it. If we can remove all references to the global (which
-      // we should be able to do for constexprs) then we can remove the global
-      // entirely.
-      auto accessor = AccessorDecl::create(
-          Impl.SwiftContext, SourceLoc(), SourceLoc(), AccessorKind::Get,
-          swiftVar, SourceLoc(), StaticSpellingKind::KeywordStatic,
-          /*Async=*/false, /*AsyncLoc=*/SourceLoc(),
-          /*throws=*/false, SourceLoc(), /*genericParams*/ nullptr,
-          ParameterList::createEmpty(Impl.SwiftContext), swiftVar->getType(),
-          swiftVar->getDeclContext());
-      Expr *value = nullptr;
-      // TODO: add non-numeric types.
-      if (evaluated->isInt()) {
-        value = IntegerLiteralExpr::createFromUnsigned(
-            Impl.SwiftContext,
-            static_cast<unsigned>(
-                clangVar->getEvaluatedValue()->getInt().getZExtValue()));
-      } else if (evaluated->isFloat()) {
-        auto floatStr = evaluated->getAsString(Impl.getClangASTContext(),
-                                               clangVar->getType());
-        auto floatIdent = Impl.SwiftContext.getIdentifier(floatStr);
-        value = new (Impl.SwiftContext)
-            FloatLiteralExpr(floatIdent.str(), SourceLoc());
-      }
-      if (!value)
-        return nullptr;
-      auto returnStmt = new (Impl.SwiftContext) ReturnStmt(SourceLoc(), value);
-      auto body = BraceStmt::create(Impl.SwiftContext, SourceLoc(),
-                                    {returnStmt}, SourceLoc());
-      accessor->setBodyParsed(body);
-      return accessor;
-    }
-
     Decl *VisitVarDecl(const clang::VarDecl *decl) {
+
       // Variables are imported as... variables.
       ImportedName importedName;
       Optional<ImportedName> correctSwiftName;
@@ -3362,6 +3321,10 @@ namespace {
       if (dc->isTypeContext())
         isStatic = true;
 
+      // For now we don't import static constexpr
+      if (isStatic && decl->isConstexpr())
+        return nullptr;
+
       auto introducer = Impl.shouldImportGlobalAsLet(decl->getType())
                         ? VarDecl::Introducer::Let
                         : VarDecl::Introducer::Var;
@@ -3384,11 +3347,6 @@ namespace {
       // If this is a compatibility stub, mark it as such.
       if (correctSwiftName)
         markAsVariant(result, *correctSwiftName);
-
-      // For constexpr vars, we can create an accessor with a numeric literal.
-      if (decl->isConstexpr())
-        if (auto acc = tryCreateConstexprAccessor(decl, result))
-          result->setAccessors(SourceLoc(), {acc}, SourceLoc());
 
       return result;
     }
