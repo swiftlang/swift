d858187db8d2742b2c021edbd57785a94ef8f274
Duo Wang <duowang@Duos-MacBook-Pro.local>
2022-07-28 13:44:01 -0700

    [tests] fix missing codesign in test files

--- test/Distributed/Runtime/distributed_actor_remoteCall_roundtrip.swift
+++ test/Distributed/Runtime/distributed_actor_remoteCall_roundtrip.swift
@@ -1,6 +1,7 @@
 // RUN: %empty-directory(%t)
 // RUN: %target-swift-frontend-emit-module -emit-module-path %t/FakeDistributedActorSystems.swiftmodule -module-name FakeDistributedActorSystems -disable-availability-checking %S/../Inputs/FakeDistributedActorSystems.swift
 // RUN: %target-build-swift -module-name main -Xfrontend -disable-availability-checking -j2 -parse-as-library -I %t %s %S/../Inputs/FakeDistributedActorSystems.swift -o %t/a.out
+// RUN: %target-codesign %t/a.out
 // RUN: %target-run %t/a.out | %FileCheck %s --enable-var-scope --color
 
 // X: %target-run-simple-swift( -Xfrontend -module-name=main -Xfrontend -disable-availability-checking  -parse-as-library -Xfrontend -I -Xfrontend %t ) | %FileCheck %s
