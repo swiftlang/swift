72e0ac28199eee059ca405a1e03de8732183dd69
Duo Wang <duowang@Duos-MacBook-Pro.local>
2022-07-28 13:44:01 -0700

    [tests] fix missing codesign in test files

--- test/stdlib/builtin-ifdef.swift
+++ test/stdlib/builtin-ifdef.swift
@@ -1,8 +1,8 @@
 // RUN: %empty-directory(%t)
 // RUN: %target-build-swift %s -parse-stdlib -whole-module-optimization -D LIBRARY -emit-module -o %t/Library.swiftmodule
 // RUN: %target-build-swift %s -parse-stdlib -whole-module-optimization -D LIBRARY -emit-bc -o %t/Library.bc
-// RUN: %target-build-swift %s -parse-as-library -D CLIENT -I %t -o %t/R1 -O            && %target-run %t/R1 | %FileCheck %s
-// RUN: %target-build-swift %s -parse-as-library -D CLIENT -I %t -o %t/R2 -O -D FOO_BAR && %target-run %t/R2 | %FileCheck %s --check-prefix CHECK-FOO-BAR
+// RUN: %target-build-swift %s -parse-as-library -D CLIENT -I %t -o %t/R1 -O            && %target-codesign %t/R1 && %target-run %t/R1 | %FileCheck %s
+// RUN: %target-build-swift %s -parse-as-library -D CLIENT -I %t -o %t/R2 -O -D FOO_BAR && %target-codesign %t/R2 && %target-run %t/R2 | %FileCheck %s --check-prefix CHECK-FOO-BAR
 
 // REQUIRES: executable_test
 
