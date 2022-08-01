26d55a2b83bf9c1d03316a399d86d55957401b9c
Alex Lorenz <arphaman@gmail.com>
2022-08-01 20:03:56 +0100

    [interop][SwiftToCxx] start emitting bindings for Swift class types
    This includes release on destruction, and correctly returning class values from Swift to C++.

---
+++ lib/PrintAsClang/PrintClangClassType.cpp
@@ -0,0 +1,91 @@
+//===--- PrintClangClassType.cpp - Print class types in C/C++ ---*- C++ -*-===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2022 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+#include "PrintClangClassType.h"
+#include "ClangSyntaxPrinter.h"
+#include "PrintClangValueType.h"
+#include "swift/AST/Decl.h"
+
+using namespace swift;
+
+void ClangClassTypePrinter::printClassTypeDecl(
+    const ClassDecl *typeDecl, llvm::function_ref<void(void)> bodyPrinter) {
+  auto printCxxImplClassName = ClangValueTypePrinter::printCxxImplClassName;
+
+  ClangSyntaxPrinter printer(os);
+
+  // Print out a forward declaration of the "hidden" _impl class.
+  printer.printNamespace(cxx_synthesis::getCxxImplNamespaceName(),
+                         [&](raw_ostream &os) {
+                           os << "class ";
+                           printCxxImplClassName(os, typeDecl);
+                           os << ";\n";
+                         });
+
+  os << "class ";
+  printer.printBaseName(typeDecl);
+  // FIXME: Add support for inherintance.
+  os << " final";
+  os << " {\n";
+  os << "public:\n";
+
+  // Destructor releases the object.
+  os << "  inline ~";
+  printer.printBaseName(typeDecl);
+  os << "() { swift::" << cxx_synthesis::getCxxImplNamespaceName()
+     << "::swift_release(_opaquePointer); }\n";
+
+  // FIXME: move semantics should be restricted?
+  os << "  inline ";
+  printer.printBaseName(typeDecl);
+  os << "(";
+  printer.printBaseName(typeDecl);
+  os << "&&) noexcept = default;\n";
+
+  os << "private:\n";
+  os << "  inline ";
+  printer.printBaseName(typeDecl);
+  os << "(void * _Nonnull ptr) noexcept : _opaquePointer(ptr) {}\n";
+  os << "\n  void * _Nonnull _opaquePointer;\n";
+  os << "  friend class " << cxx_synthesis::getCxxImplNamespaceName() << "::";
+  printCxxImplClassName(os, typeDecl);
+  os << ";\n";
+  os << "};\n\n";
+
+  // Print out the "hidden" _impl class.
+  printer.printNamespace(
+      cxx_synthesis::getCxxImplNamespaceName(), [&](raw_ostream &os) {
+        os << "class ";
+        printCxxImplClassName(os, typeDecl);
+        os << " {\n";
+        os << "public:\n";
+        os << "static inline ";
+        printer.printBaseName(typeDecl);
+        os << " fromUnretained(void * _Nonnull ptr) noexcept { return ";
+        printer.printBaseName(typeDecl);
+        os << "(ptr); }\n";
+        os << "};\n";
+      });
+}
+
+void ClangClassTypePrinter::printClassTypeReturnScaffold(
+    raw_ostream &os, const ClassDecl *type, const ModuleDecl *moduleContext,
+    llvm::function_ref<void(void)> bodyPrinter) {
+  os << "  return ";
+  ClangSyntaxPrinter(os).printModuleNamespaceQualifiersIfNeeded(
+      type->getModuleContext(), moduleContext);
+  os << cxx_synthesis::getCxxImplNamespaceName() << "::";
+  ClangValueTypePrinter::printCxxImplClassName(os, type);
+  os << "::fromUnretained(";
+  bodyPrinter();
+  os << ");\n";
+}
---
+++ lib/PrintAsClang/PrintClangClassType.h
@@ -0,0 +1,46 @@
+//===--- PrintClangClassType.h - Print class types in C/C++ -----*- C++ -*-===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2022 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef SWIFT_PRINTASCLANG_PRINTCLANGCLASSTYPE_H
+#define SWIFT_PRINTASCLANG_PRINTCLANGCLASSTYPE_H
+
+#include "swift/Basic/LLVM.h"
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/Support/raw_ostream.h"
+
+namespace swift {
+
+class ClassDecl;
+class ModuleDecl;
+
+/// Responsible for printing a Swift class decl or in C or C++ mode, to
+/// be included in a Swift module's generated clang header.
+class ClangClassTypePrinter {
+public:
+  ClangClassTypePrinter(raw_ostream &os) : os(os) {}
+
+  /// Print the C++ class definition that corresponds to the given Swift class.
+  void printClassTypeDecl(const ClassDecl *typeDecl,
+                          llvm::function_ref<void(void)> bodyPrinter);
+
+  static void
+  printClassTypeReturnScaffold(raw_ostream &os, const ClassDecl *type,
+                               const ModuleDecl *moduleContext,
+                               llvm::function_ref<void(void)> bodyPrinter);
+
+private:
+  raw_ostream &os;
+};
+
+} // end namespace swift
+
+#endif
