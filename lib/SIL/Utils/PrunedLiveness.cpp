7c452ca400d172832e7da361f5fd151941181032
Michael Gottesman <mgottesman@apple.com>
2022-08-20 20:27:16 -0700

    [pruned-liveness] Implement FieldSensitiveAddressPrunedLiveness.
    This is the same algorithm as pruned liveness but assumes that one is tracking
    liveness from an address and uses the same scheme as DI to track liveness as a
    bit vector.

--- lib/SIL/Utils/PrunedLiveness.cpp
+++ lib/SIL/Utils/PrunedLiveness.cpp
@@ -371,3 +371,245 @@ void PrunedLivenessBoundary::compute(const PrunedLiveness &liveness,
     }
   }
 }
+
+//===----------------------------------------------------------------------===//
+//                       Field Sensitive PrunedLiveness
+//===----------------------------------------------------------------------===//
+
+// We can only analyze components of structs whose storage is fully accessible
+// from Swift.
+static StructDecl *getFullyReferenceableStruct(SILType ktypeTy) {
+  auto structDecl = ktypeTy.getStructOrBoundGenericStruct();
+  if (!structDecl || structDecl->hasUnreferenceableStorage())
+    return nullptr;
+  return structDecl;
+}
+
+TypeSubElementCount::TypeSubElementCount(SILType type, SILModule &mod,
+                                         TypeExpansionContext context)
+    : number(1) {
+  if (auto tupleType = type.getAs<TupleType>()) {
+    unsigned numElements = 0;
+    for (auto index : indices(tupleType.getElementTypes()))
+      numElements +=
+          TypeSubElementCount(type.getTupleElementType(index), mod, context);
+    number = numElements;
+    return;
+  }
+
+  if (auto *structDecl = getFullyReferenceableStruct(type)) {
+    unsigned numElements = 0;
+    for (auto *fieldDecl : structDecl->getStoredProperties())
+      numElements += TypeSubElementCount(
+          type.getFieldType(fieldDecl, mod, context), mod, context);
+    number = numElements;
+    return;
+  }
+
+  // If this isn't a tuple or struct, it is a single element. This was our
+  // default value, so we can just return.
+}
+
+Optional<SubElementNumber>
+SubElementNumber::compute(SILValue projectionDerivedFromRoot,
+                          SILValue rootAddress) {
+  unsigned finalSubElementNumber = 0;
+  SILModule &mod = *rootAddress->getModule();
+
+  while (1) {
+    // If we got to the root, we're done.
+    if (rootAddress == projectionDerivedFromRoot)
+      return {SubElementNumber(finalSubElementNumber)};
+
+    if (auto *pbi = dyn_cast<ProjectBoxInst>(projectionDerivedFromRoot)) {
+      projectionDerivedFromRoot = pbi->getOperand();
+      continue;
+    }
+
+    if (auto *bai = dyn_cast<BeginAccessInst>(projectionDerivedFromRoot)) {
+      projectionDerivedFromRoot = bai->getSource();
+      continue;
+    }
+
+    if (auto *teai =
+            dyn_cast<TupleElementAddrInst>(projectionDerivedFromRoot)) {
+      SILType tupleType = teai->getOperand()->getType();
+
+      // Keep track of what subelement is being referenced.
+      for (unsigned i : range(teai->getFieldIndex())) {
+        finalSubElementNumber += TypeSubElementCount(
+            tupleType.getTupleElementType(i), mod,
+            TypeExpansionContext(*rootAddress->getFunction()));
+      }
+      projectionDerivedFromRoot = teai->getOperand();
+      continue;
+    }
+
+    if (auto *seai =
+            dyn_cast<StructElementAddrInst>(projectionDerivedFromRoot)) {
+      SILType type = seai->getOperand()->getType();
+
+      // Keep track of what subelement is being referenced.
+      StructDecl *structDecl = seai->getStructDecl();
+      for (auto *fieldDecl : structDecl->getStoredProperties()) {
+        if (fieldDecl == seai->getField())
+          break;
+        auto context = TypeExpansionContext(*rootAddress->getFunction());
+        finalSubElementNumber += TypeSubElementCount(
+            type.getFieldType(fieldDecl, mod, context), mod, context);
+      }
+
+      projectionDerivedFromRoot = seai->getOperand();
+      continue;
+    }
+
+    // This fails when we visit unchecked_take_enum_data_addr. We should just
+    // add support for enums.
+#ifndef NDEBUG
+    if (!isa<InitExistentialAddrInst>(projectionDerivedFromRoot)) {
+      llvm::errs() << "Unknown access path instruction!\n";
+      llvm::errs() << "Value: " << *projectionDerivedFromRoot;
+      llvm_unreachable("standard error");
+    }
+#endif
+    // Cannot promote loads and stores from within an existential projection.
+    return None;
+  }
+}
+
+void FieldSensitiveAddressPrunedLiveness::updateForUse(
+    SILInstruction *user, TypeTreeLeafTypeRange range, bool lifetimeEnding) {
+  SmallVector<PrunedLiveBlocks::IsLive, 8> resultingLiveness;
+  liveBlocks.updateForUse(user, range.startEltOffset, range.endEltOffset,
+                          resultingLiveness);
+
+  // Note that a user may use the current value from multiple operands. If any
+  // of the uses are non-lifetime-ending, then we must consider the user
+  // itself non-lifetime-ending; it cannot be a final destroy point because
+  // the value of the non-lifetime-ending operand must be kept alive until the
+  // end of the user. Consider a call that takes the same value using
+  // different conventions:
+  //
+  //   apply %f(%val, %val) : $(@guaranteed, @owned) -> ()
+  //
+  // This call is not considered the end of %val's lifetime. The @owned
+  // argument must be copied.
+  auto iterAndSuccess =
+      users.insert({user, InterestingUser(range, lifetimeEnding)});
+  if (!iterAndSuccess.second)
+    iterAndSuccess.first->second &= lifetimeEnding;
+}
+
+void FieldSensitiveAddressPrunedLiveness::isWithinBoundary(
+    SILInstruction *inst, SmallBitVector &outVector) const {
+  SILBasicBlock *block = inst->getParent();
+
+  SmallVector<PrunedLiveBlocks::IsLive, 8> fieldLiveness;
+  getBlockLiveness(block, fieldLiveness);
+  outVector.resize(fieldLiveness.size());
+
+  for (auto pair : llvm::enumerate(fieldLiveness)) {
+    auto isLive = pair.value();
+    unsigned subEltNumber = pair.index();
+    switch (isLive) {
+    case PrunedLiveBlocks::Dead:
+      outVector[subEltNumber] = false;
+      continue;
+    case PrunedLiveBlocks::LiveOut:
+      outVector[subEltNumber] = true;
+      continue;
+    case PrunedLiveBlocks::LiveWithin:
+      // The boundary is within this block. This instruction is before the
+      // boundary iff any interesting uses occur after it.
+      bool foundValue = false;
+      for (SILInstruction &it :
+           make_range(std::next(inst->getIterator()), block->end())) {
+        auto interestingUser = isInterestingUser(&it);
+        switch (interestingUser.first) {
+        case FieldSensitiveAddressPrunedLiveness::NonUser:
+          break;
+        case FieldSensitiveAddressPrunedLiveness::NonLifetimeEndingUse:
+        case FieldSensitiveAddressPrunedLiveness::LifetimeEndingUse:
+          // Check the overlap in between the sub element number and
+          // interestingUser.second. If we don't overlap, just break. We aren't
+          // effected by this.
+          //
+          // TODO: Hoist this out! We should only be visited blocks like this
+          // once!
+          if (!interestingUser.second->contains(subEltNumber))
+            break;
+          outVector[subEltNumber] = true;
+          foundValue = true;
+          break;
+        }
+      }
+      if (foundValue)
+        continue;
+      outVector[subEltNumber] = false;
+    }
+  }
+}
+
+// Use \p liveness to find the last use in \p bb and add it to \p
+// boundary.lastUsers.
+void FieldSensitiveAddressPrunedLivenessBoundary::findLastUserInBlock(
+    SILBasicBlock *bb, FieldSensitiveAddressPrunedLivenessBoundary &boundary,
+    const FieldSensitiveAddressPrunedLiveness &liveness,
+    unsigned subElementNumber) {
+  // TODO: We should move this loop into the caller and only visit a block once
+  // for each sub-element of a type.
+  for (auto &inst : llvm::reverse(*bb)) {
+    auto pair = liveness.isInterestingUser(&inst);
+    if (pair.first == FieldSensitiveAddressPrunedLiveness::NonUser)
+      continue;
+
+    // Do an intersection in between the range associated with this address and
+    // the sub-element number we are checking for.
+    auto &range = *pair.second;
+    if (!range.contains(subElementNumber))
+      continue;
+    boundary.lastUsers.push_back({&inst, range});
+    return;
+  }
+  llvm_unreachable("No user in LiveWithin block");
+}
+
+void FieldSensitiveAddressPrunedLivenessBoundary::compute(
+    const FieldSensitiveAddressPrunedLiveness &liveness) {
+  using IsLive = PrunedLiveBlocks::IsLive;
+  SmallVector<IsLive, 8> perSubElementblockLivenessInfo;
+  SmallVector<IsLive, 8> boundaryBlockLiveness;
+
+  for (SILBasicBlock *bb : liveness.getDiscoveredBlocks()) {
+    SWIFT_DEFER { perSubElementblockLivenessInfo.clear(); };
+
+    // Process each block that has not been visited and is not LiveOut.
+    liveness.getBlockLiveness(bb, perSubElementblockLivenessInfo);
+
+    // TODO: We should do this for all sub-element LiveWithin at the same time
+    // so that we can avoid iterating over the block multiple times.
+    for (auto pair : llvm::enumerate(perSubElementblockLivenessInfo)) {
+      switch (pair.value()) {
+      case PrunedLiveBlocks::LiveOut:
+        for (SILBasicBlock *succBB : bb->getSuccessors()) {
+          liveness.getBlockLiveness(succBB, boundaryBlockLiveness);
+          if (llvm::all_of(boundaryBlockLiveness, [](IsLive isDead) {
+                return isDead == PrunedLiveBlocks::Dead;
+              })) {
+            boundaryEdges.push_back(succBB);
+          }
+        }
+        break;
+      case PrunedLiveBlocks::LiveWithin: {
+        // The liveness boundary is inside this block. Find the last user. This
+        // is where we would insert a destroy to end the values lifetime for the
+        // specific subelementnumber
+        findLastUserInBlock(bb, *this, liveness, pair.index());
+        break;
+      }
+      case PrunedLiveBlocks::Dead:
+        break;
+      }
+    }
+  }
+}
