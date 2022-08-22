0c749b2651d5d36447466cfc38ca814530ee4c61
Anthony Latsis <aqamoss3fan2010@gmail.com>
2022-08-23 01:02:28 +0300

    Gardening: Migrate test suite to GH issues: decl/ext

--- test/decl/ext/sr_12460.swift
+++ test/decl/ext/sr_12460.swift
@@ -1,6 +1,9 @@
 // RUN: %target-swift-ide-test -print-indexed-symbols -source-filename %s 2>&1 | %FileCheck -check-prefix CHECK %s
 
-// Test that we don't crash when validating members inside an extension with no type name.
+// https://github.com/apple/swift/issues/54900
+//
+// Test that we don't crash when validating members inside an extension with no
+// type name.
 
 // CHECK: :[[@LINE+1]]:11: error: expected type name in extension declaration
 extension {
