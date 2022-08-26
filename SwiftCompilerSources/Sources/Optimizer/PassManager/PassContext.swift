f6e6b2ecf3efb0db736cb6308905aa56b4124ab3
Erik Eckstein <eeckstein@apple.com>
2022-08-26 17:56:35 +0200

    Swift SIL: add the possibility to pass a custom location to `Builder` initializers.
    And add the static property `Location.autoGeneratedLocation`

--- SwiftCompilerSources/Sources/Optimizer/PassManager/PassContext.swift
+++ SwiftCompilerSources/Sources/Optimizer/PassManager/PassContext.swift
@@ -157,18 +157,29 @@ extension Builder {
     self.init(insertAt: .before(insPnt), location: insPnt.location, passContext: context._bridged)
   }
 
+  /// Creates a builder which inserts _after_ `insPnt`, using a custom `location`.
+  init(after insPnt: Instruction, location: Location, _ context: PassContext) {
+    if let nextInst = insPnt.next {
+      self.init(insertAt: .before(nextInst), location: location, passContext: context._bridged)
+    } else {
+      self.init(insertAt: .atEndOf(insPnt.block), location: location, passContext: context._bridged)
+    }
+  }
+
   /// Creates a builder which inserts _after_ `insPnt`, using the location of `insPnt`.
   init(after insPnt: Instruction, _ context: PassContext) {
-    if let nextInst = insPnt.next {
-      self.init(insertAt: .before(nextInst), location: insPnt.location, passContext: context._bridged)
-    } else {
-      self.init(insertAt: .atEndOf(insPnt.block), location: insPnt.location, passContext: context._bridged)
-    }
+    self.init(after: insPnt, location: insPnt.location, context)
   }
 
   /// Creates a builder which inserts at the end of `block`, using a custom `location`.
   init(atEndOf block: BasicBlock, location: Location, _ context: PassContext) {
     self.init(insertAt: .atEndOf(block), location: location, passContext: context._bridged)
+  }
+
+  /// Creates a builder which inserts at the begin of `block`, using a custom `location`.
+  init(atBeginOf block: BasicBlock, location: Location, _ context: PassContext) {
+    let firstInst = block.instructions.first!
+    self.init(insertAt: .before(firstInst), location: location, passContext: context._bridged)
   }
 
   /// Creates a builder which inserts at the begin of `block`, using the location of the first
