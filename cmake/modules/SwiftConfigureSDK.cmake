3740a207bdd794e76b35527efbce7ba4dc8f07bb
3405691582 <dsk@google.com>
2022-08-29 10:47:42 -0400

    Apply suggestions from code review
    Co-authored-by: Saleem Abdulrasool <compnerd@compnerd.org>

--- cmake/modules/SwiftConfigureSDK.cmake
+++ cmake/modules/SwiftConfigureSDK.cmake
@@ -405,7 +405,7 @@ macro(configure_sdk_unix name architectures)
 
         set(SWIFT_SDK_OPENBSD_ARCH_amd64_TRIPLE "amd64-unknown-openbsd${openbsd_system_version}")
 
-	if(NOT CMAKE_SYSROOT STREQUAL "")
+	if(CMAKE_SYSROOT)
 	  set(SWIFT_SDK_OPENBSD_ARCH_${arch}_PATH "${CMAKE_SYSROOT}${SWIFT_SDK_OPENBSD_ARCH_${arch}_PATH}" CACHE INTERNAL "sysroot path" FORCE)
 	  set(SWIFT_SDK_OPENBSD_ARCH_${arch}_LIBC_INCLUDE_DIRECTORY "${CMAKE_SYSROOT}${SWIFT_SDK_OPENBSD_ARCH_${arch}_LIBC_INCLUDE_DIRECTORY}" CACHE INTERNAL "sysroot libc path" FORCE)
 	endif()
