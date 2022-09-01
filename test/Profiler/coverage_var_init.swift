7e141f4b1c1137cfd9d2ae863ab80670cc65a6dd
Hamish Knight <hamish_github@mediocremail.com>
2022-09-01 19:29:17 +0100

    [Profiler] Emit coverage for literal initializers
    It's not clear why these should be excluded when
    other initializers receive coverage.

--- test/Profiler/coverage_var_init.swift
+++ test/Profiler/coverage_var_init.swift
@@ -1,6 +1,36 @@
 // RUN: %target-swift-frontend -Xllvm -sil-full-demangle -profile-generate -profile-coverage-mapping -emit-sorted-sil -emit-sil -module-name coverage_var_init %s | %FileCheck %s
 // RUN: %target-swift-frontend -profile-generate -profile-coverage-mapping -emit-ir %s
 
+struct S {
+  // CHECK-LABEL: sil_coverage_map {{.*}} "$s17coverage_var_init1SV1iSivpfi" {{.*}} // variable initialization expression of coverage_var_init.S.i
+  // CHECK-NEXT:  [[@LINE+1]]:11 -> [[@LINE+1]]:12 : 0
+  var i = 0
+
+  // CHECK-LABEL: sil_coverage_map {{.*}} "$s17coverage_var_init1SV1jSivpfi" {{.*}} // variable initialization expression of coverage_var_init.S.j
+  // CHECK-NEXT:  [[@LINE+1]]:11 -> [[@LINE+1]]:16 : 0
+  var j = 1 + 2
+
+  // CHECK-LABEL: sil_coverage_map {{.*}} "$s17coverage_var_init1SV1kSiycvpfi" {{.*}} // variable initialization expression of coverage_var_init.S.k
+  // CHECK-NEXT:  [[@LINE+3]]:11 -> [[@LINE+3]]:20 : 0
+  // CHECK-LABEL: sil_coverage_map {{.*}} "$s17coverage_var_init1SV1kSiycvpfiSiycfU_" {{.*}} // closure #1 () -> Swift.Int in variable initialization expression of coverage_var_init.S.k
+  // CHECK-NEXT: [[@LINE+1]]:11 -> [[@LINE+1]]:20 : 0
+  var k = { 1 + 2 }
+
+  // CHECK-LABEL: sil_coverage_map {{.*}} "$s17coverage_var_init1SV1lSivpfi" {{.*}} // variable initialization expression of coverage_var_init.S.l
+  // CHECK-NEXT:  [[@LINE+1]]:11 -> [[@LINE+1]]:16 : 0
+  var l = #line
+
+  // CHECK-LABEL: sil_coverage_map {{.*}} "$s17coverage_var_init1SV1mSaySiGvpfi" {{.*}} // variable initialization expression of coverage_var_init.S.m
+  // CHECK-NEXT:  [[@LINE+1]]:11 -> [[@LINE+1]]:20 : 0
+  var m = [1, 2, 3]
+
+  // CHECK-LABEL: sil_coverage_map {{.*}} "$s17coverage_var_init1SV1nSSvpfi" {{.*}} // variable initialization expression of coverage_var_init.S.n
+  // CHECK-NEXT:  [[@LINE+3]]:11 -> [[@LINE+3]]:33 : 0
+  // CHECK-NEXT:  [[@LINE+2]]:26 -> [[@LINE+2]]:27 : 1
+  // CHECK-NEXT:  [[@LINE+1]]:30 -> [[@LINE+1]]:31 : (0 - 1)
+  var n = "\(.random() ? 1 : 2)"
+}
+
 final class VarInit {
   // CHECK: sil_coverage_map {{.*}} "$s17coverage_var_init7VarInitC018initializedWrapperE0SivpfP"
   // CHECK-NEXT: [[@LINE+1]]:4 -> [[@LINE+1]]:42 : 0
