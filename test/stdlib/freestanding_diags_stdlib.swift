b6325c1696f9ccb04c8b0f875653da93ecb2564d
Nate Chandler <nate_chandler@apple.com>
2022-09-01 15:34:53 -0700

    [Freestanding] Disable child task priority spec.
    Under the task-to-thread model, specifying a priority doesn't make
    sense.
    
    Here, variations of addTask and addTaskUnlessCancelled are introduced
    which do not take a priority.  Additionally, the original functions are
    made unavailable.

--- test/stdlib/freestanding_diags_stdlib.swift
+++ test/stdlib/freestanding_diags_stdlib.swift
@@ -11,6 +11,7 @@ func chowMein() async {
 @MainActor // expected-error{{not permitted within task-to-thread concurrency model}}
 class ChowMein {}
 
+@available(SwiftStdlib 5.1, *)
 func foo() async {
   Task<Void, Never> {} // expected-error{{Unavailable in task-to-thread concurrency model}}
   Task<Void, Error> {} // expected-error{{Unavailable in task-to-thread concurrency model}}
@@ -31,6 +32,50 @@ func foo() async {
     _ = error
   }
   _ = CheckedContinuation<Int, Never>.self // expected-error{{Unavailable in task-to-thread concurrency model}}
+  func withTaskGroup(_ tg: inout TaskGroup<Int>) async throws {
+    tg.addTask(priority: .low) { return 1 } // expected-error{{Unavailable in task-to-thread concurrency model}}
+    tg.addTask { return 1 } // ok
+    _ = tg.addTaskUnlessCancelled(priority: .low) { return 1 } // expected-error{{Unavailable in task-to-thread concurrency model}}
+    _ = tg.addTaskUnlessCancelled { return 1 } // ok
+
+    _ = await tg.add(priority: .low) { return 1 } // expected-error{{Unavailable in task-to-thread concurrency model}}
+    _ = await tg.add { return 1 } // expected-warning{{'add(operation:)' is deprecated: renamed to 'addTaskUnlessCancelled(operation:)'}}
+                                  // expected-note@-1{{use 'addTaskUnlessCancelled(operation:)' instead}}
+    tg.spawn(priority: .low) { return 1 } // expected-error{{Unavailable in task-to-thread concurrency model}}
+    tg.spawn { return 1 } // expected-warning{{'spawn(operation:)' is deprecated: renamed to 'addTask(operation:)'}}
+                          // expected-note@-1{{use 'addTask(operation:)' instead}}
+    _ = tg.spawnUnlessCancelled(priority: .low) { return 1 } // expected-error{{Unavailable in task-to-thread concurrency model}}
+    _ = tg.spawnUnlessCancelled { return 1 } // expected-warning{{'spawnUnlessCancelled(operation:)' is deprecated: renamed to 'addTaskUnlessCancelled(operation:)'}}
+                                             // expected-note@-1{{use 'addTaskUnlessCancelled(operation:)' instead}}
+    tg.async(priority: .low) { return 1 } // expected-error{{Unavailable in task-to-thread concurrency model}}
+    tg.async { return 1 } // expected-warning{{'async(operation:)' is deprecated: renamed to 'addTask(operation:)'}}
+                          // expected-note@-1{{use 'addTask(operation:)' instead}}
+    _ = tg.asyncUnlessCancelled(priority: .low) { return 1 } // expected-error{{Unavailable in task-to-thread concurrency model}}
+    _ = tg.asyncUnlessCancelled { return 1 } // expected-warning{{'asyncUnlessCancelled(operation:)' is deprecated: renamed to 'addTaskUnlessCancelled(operation:)'}}
+                                             // expected-note@-1{{use 'addTaskUnlessCancelled(operation:)' instead}}
+  }
+  func withThrowingTaskGroup(_ tg: inout ThrowingTaskGroup<Int, Error>) async throws {
+    tg.addTask(priority: .low) { return 1 } // expected-error{{Unavailable in task-to-thread concurrency model}}
+    tg.addTask { return 1 } // ok
+    _ = tg.addTaskUnlessCancelled(priority: .low) { return 1 } // expected-error{{Unavailable in task-to-thread concurrency model}}
+    _ = tg.addTaskUnlessCancelled { return 1 } // ok
+
+    _ = await tg.add(priority: .low) { return 1 } // expected-error{{Unavailable in task-to-thread concurrency model}}
+    _ = await tg.add { return 1 } // expected-warning{{'add(operation:)' is deprecated: renamed to 'addTaskUnlessCancelled(operation:)'}}
+                                  // expected-note@-1{{use 'addTaskUnlessCancelled(operation:)' instead}}
+    tg.spawn(priority: .low) { return 1 } // expected-error{{Unavailable in task-to-thread concurrency model}}
+    tg.spawn { return 1 } // expected-warning{{'spawn(operation:)' is deprecated: renamed to 'addTask(operation:)'}}
+                          // expected-note@-1{{use 'addTask(operation:)' instead}}
+    _ = tg.spawnUnlessCancelled(priority: .low) { return 1 } // expected-error{{Unavailable in task-to-thread concurrency model}}
+    _ = tg.spawnUnlessCancelled { return 1 } // expected-warning{{'spawnUnlessCancelled(operation:)' is deprecated: renamed to 'addTaskUnlessCancelled(operation:)'}}
+                                             // expected-note@-1{{use 'addTaskUnlessCancelled(operation:)' instead}}
+    tg.async(priority: .low) { return 1 } // expected-error{{Unavailable in task-to-thread concurrency model}}
+    tg.async { return 1 } // expected-warning{{'async(operation:)' is deprecated: renamed to 'addTask(operation:)'}}
+                          // expected-note@-1{{use 'addTask(operation:)' instead}}
+    _ = tg.asyncUnlessCancelled(priority: .low) { return 1 } // expected-error{{Unavailable in task-to-thread concurrency model}}
+    _ = tg.asyncUnlessCancelled { return 1 } // expected-warning{{'asyncUnlessCancelled(operation:)' is deprecated: renamed to 'addTaskUnlessCancelled(operation:)'}}
+                                             // expected-note@-1{{use 'addTaskUnlessCancelled(operation:)' instead}}
+  }
 }
 
 func foo2(
