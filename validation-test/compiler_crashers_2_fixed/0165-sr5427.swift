2577af7967c3546fd1fa441cf42904a3d587d15f
Anthony Latsis <aqamoss3fan2010@gmail.com>
2022-09-15 16:09:51 +0300

    Gardening: Migrate test suite to GH issues: validation-test/compiler_crashers_2_fixed (1/4)

--- validation-test/compiler_crashers_2_fixed/0165-sr5427.swift
+++ validation-test/compiler_crashers_2_fixed/0165-sr5427.swift
@@ -1,7 +1,9 @@
 // RUN: %target-typecheck-verify-swift
 
-// Used to crash with: apply expression is not marked as throwing or
-// non-throwing
-struct SR5427 : Error {}
-func sr5427(op: (() throws -> Void)?) rethrows { try op?() }
-try? sr5427(op: { throw SR5427() })
+// https://github.com/apple/swift/issues/48001
+// Used to crash with "apply expression is not marked as throwing or
+// non-throwing".
+
+struct S : Error {}
+func f(op: (() throws -> Void)?) rethrows { try op?() }
+try? f(op: { throw S() })
---
+++ validation-test/compiler_crashers_2_fixed/0189-issue-52436.swift
@@ -0,0 +1,25 @@
+// RUN: %target-swift-frontend -emit-ir -verify %s
+// RUN: %target-swift-frontend -emit-ir -verify -disable-requirement-machine-concrete-contraction %s
+
+// https://github.com/apple/swift/issues/52436
+
+protocol P1 {
+  associatedtype A2 : P2 where A2.A1 == Self
+}
+
+protocol P2 {
+  associatedtype A1 : P1 where A1.A2 == Self
+  var property: Int { get }
+}
+
+extension P2 {
+  var property: Int { return 0 }
+}
+
+class C1 : P1 {
+// expected-warning@-1 {{non-final class 'C1' cannot safely conform to protocol 'P1', which requires that 'Self.A2.A1' is exactly equal to 'Self'; this is an error in Swift 6}}
+  class A2 : P2 {
+  // expected-warning@-1 {{non-final class 'C1.A2' cannot safely conform to protocol 'P2', which requires that 'Self.A1.A2' is exactly equal to 'Self'; this is an error in Swift 6}}
+    typealias A1 = C1
+  }
+}
