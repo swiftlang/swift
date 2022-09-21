90bb343d7e960b2a7709704e6a8f9ad0916ffff3
Evan Wilde <etceterawilde@gmail.com>
2022-09-21 11:41:17 -0700

    Backdeploy swift_task_future_wait_throwing
    Drop the remaining pieces in for adding
    `swift_task_future_wait_throwing`.

--- stdlib/toolchain/Compatibility56/Concurrency/Task.cpp
+++ stdlib/toolchain/Compatibility56/Concurrency/Task.cpp
@@ -135,6 +135,31 @@ void SWIFT_CC(swiftasync) swift::swift56override_swift_task_future_wait(
 
 //===--- swift_task_future_wait_throwing ----------------------------------===//
 
+SWIFT_CC(swiftasync)
+static void task_wait_throwing_resume_adapter(SWIFT_ASYNC_CONTEXT AsyncContext *_context) {
+
+  auto context = static_cast<TaskFutureWaitAsyncContext *>(_context);
+  auto resumeWithError =
+      reinterpret_cast<AsyncVoidClosureEntryPoint *>(context->ResumeParent);
+  return resumeWithError(context->Parent, context->errorResult);
+}
+
+#ifdef __ARM_ARCH_7K__
+__attribute__((noinline))
+SWIFT_CC(swiftasync) static void workaround_function_swift_task_future_wait_throwingImpl(
+    OpaqueValue *result, SWIFT_ASYNC_CONTEXT AsyncContext *callerContext,
+    AsyncTask *task, ThrowingTaskFutureWaitContinuationFunction resumeFunction,
+    AsyncContext *callContext) {
+  // Make sure we don't eliminate calls to this function.
+  asm volatile("" // Do nothing.
+               :  // Output list, empty.
+               : "r"(result), "r"(callerContext), "r"(task) // Input list.
+               : // Clobber list, empty.
+  );
+  return;
+}
+#endif
+
 void SWIFT_CC(swiftasync) swift::swift56override_swift_task_future_wait_throwing(
                                             OpaqueValue *result,
                                             SWIFT_ASYNC_CONTEXT AsyncContext *callerContext,
@@ -142,5 +167,40 @@ void SWIFT_CC(swiftasync) swift::swift56override_swift_task_future_wait_throwing
                                             ThrowingTaskFutureWaitContinuationFunction *resumeFunction,
                                             AsyncContext *callContext,
                                             TaskFutureWaitThrowing_t *original) {
-  original(result, callerContext, task, resumeFunction, callContext);
+  auto waitingTask = swift_task_getCurrent();
+  // Suspend the waiting task.
+  waitingTask->ResumeTask = task_wait_throwing_resume_adapter;
+  waitingTask->ResumeContext = callContext;
+
+  auto resumeFn = reinterpret_cast<TaskContinuationFunction *>(resumeFunction);
+
+  // Wait on the future.
+  assert(task->isFuture());
+
+  switch (task->waitFuture(waitingTask, callContext, resumeFn, callerContext,
+                           result)) {
+  case FutureFragment::Status::Executing:
+    // The waiting task has been queued on the future.
+#ifdef __ARM_ARCH_7K__
+    return workaround_function_swift_task_future_wait_throwingImpl(
+        result, callerContext, task, resumeFunction, callContext);
+#else
+    return;
+#endif
+
+  case FutureFragment::Status::Success: {
+    auto future = task->futureFragment();
+    future->getResultType()->vw_initializeWithCopy(result,
+                                                   future->getStoragePtr());
+    return resumeFunction(callerContext, nullptr /*error*/);
+  }
+
+  case FutureFragment::Status::Error: {
+    // Run the task with an error result.
+    auto future = task->futureFragment();
+    auto error = future->getError();
+    swift_errorRetain(error);
+    return resumeFunction(callerContext, error);
+  }
+  }
 }
