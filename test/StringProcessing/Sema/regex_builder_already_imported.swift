e17b60d30947d8f69538248876a05df242124efb
Apollo Zhu <zhuzhiyu@apple.com>
2022-09-21 15:42:15 -0700

    Update to new RegexBuilder buildExpression's error message

--- test/StringProcessing/Sema/regex_builder_already_imported.swift
+++ test/StringProcessing/Sema/regex_builder_already_imported.swift
@@ -3,14 +3,11 @@
 import RegexBuilder
 
 extension Regex where Output == Substring {
-  init(_ x: String) {} // expected-note {{'init(_:)' declared here}}
+  init(_ x: String) {}
 }
 
 func foo() {
-  // FIXME: This diagnostic could probably be better, it's not clear we should
-  // be resolving to init(_ x: String) vs the result builder API and diagnosing
-  // the fact that Int isn't a RegexComponent.
-  _ = Regex { // expected-error {{trailing closure passed to parameter of type 'String' that does not accept a closure}}
-    0
+  _ = Regex {
+    0  // expected-error {{static method 'buildExpression' requires that 'Int' conform to 'RegexComponent'}}
   }
 }
