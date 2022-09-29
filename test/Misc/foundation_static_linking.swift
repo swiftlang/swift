501b0080421f893f86ea29ade0922ca5b2860227
Max Desiatov <max@desiatov.com>
2022-09-29 22:48:22 +0100

    test: check static linking of FoundationNetworking
    This was fixed in https://github.com/apple/swift-corelibs-foundation/pull/3135 for Swift 5.5, but regressed in Swift 5.6 and Swift 5.7

---
+++ test/Misc/foundation_static_linking.swift
@@ -0,0 +1,6 @@
+// RUN: %target-swiftc_driver -static-stdlib -o %t/foundation_static_linking %s 
+// RUN: not --crash  %t/foundation_static_linking
+
+import Foundation
+
+_ = try! Data(contentsOf: URL(string: "http://httpbin.org/get")!)
