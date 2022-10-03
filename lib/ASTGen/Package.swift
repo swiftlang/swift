8e6628616d043b5251d0e05dd3b4b813f3104f60
Doug Gregor <dgregor@apple.com>
2022-10-03 15:57:51 -0700

    Fix the package that builds swiftASTGen.

--- lib/ASTGen/Package.swift
+++ lib/ASTGen/Package.swift
@@ -4,15 +4,29 @@ import PackageDescription
 
 let package = Package(
     name: "ASTGen",
+    platforms: [
+        .macOS(.v10_15)
+    ],
     products: [
-        .library(name: "ASTGen", targets: ["ASTGen"]),
+        .library(name: "swiftASTGen", targets: ["swiftASTGen"]),
     ],
     dependencies: [
          .package(path: "../../../swift-syntax"),
     ],
     targets: [
         .target(
-            name: "ASTGen",
-            dependencies: ["SwiftSyntax"]),
+            name: "swiftASTGen",
+            dependencies: [
+              .product(name: "SwiftSyntax", package: "swift-syntax"),
+              .product(name: "SwiftParser", package: "swift-syntax")
+            ],
+            path: ".",
+            exclude: ["CMakeLists.txt"],
+        swiftSettings: [
+          .unsafeFlags([
+            "-I", "../../include/swift/",
+            "-I", "../../include"
+          ])
+        ]),
     ]
 )
