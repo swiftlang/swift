08a9e7aa63280e9c8b07a80ca39039a3465a052d
Suyash Srijan <suyashsrijan@outlook.com>
2022-10-05 22:19:58 +0100

    [CSGen] Add source location to synthesized makeIterator().next() call

--- test/Concurrency/async_sequence_syntax.swift
+++ test/Concurrency/async_sequence_syntax.swift
@@ -9,11 +9,16 @@ func missingAsync<T : AsyncSequence>(_ seq: T) throws {
 
 @available(SwiftStdlib 5.1, *)
 func missingThrows<T : AsyncSequence>(_ seq: T) async {
-  for try await _ in seq { } // expected-error{{error is not handled because the enclosing function is not declared 'throws'}}
+  for try await _ in seq { } 
+  // expected-error@-1 {{error is not handled because the enclosing function is not declared 'throws'}}
+  // expected-error@-2 {{call can throw, but the error is not handled}}
+  // expected-note@-3 {{call is to 'rethrows' function, but a conformance has a throwing witness}}
 }
 
 @available(SwiftStdlib 5.1, *)
 func executeAsync(_ work: () async -> Void) { }
+@available(SwiftStdlib 5.1, *)
+func executeAsyncThrows(_ work: () async throws -> Void) { }
 @available(SwiftStdlib 5.1, *)
 func execute(_ work: () -> Void) { }
 
@@ -27,7 +32,9 @@ func missingThrowingInBlock<T : AsyncSequence>(_ seq: T) {
 @available(SwiftStdlib 5.1, *)
 func missingTryInBlock<T : AsyncSequence>(_ seq: T) { 
   executeAsync { 
-    for await _ in seq { } // expected-error{{call can throw, but the error is not handled}}
+    for await _ in seq { } 
+    // expected-error@-1 2{{call can throw, but the error is not handled}}
+    // expected-note@-2 {{call is to 'rethrows' function, but a conformance has a throwing witness}}
   }
 }
 
