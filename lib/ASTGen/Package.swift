5a0d775fa5c476791708c7793b1b6f4c9166330a
Doug Gregor <dgregor@apple.com>
2022-10-06 10:09:34 -0700

    [ASTGen] Add a comment about the purporse of its Package.swift

--- lib/ASTGen/Package.swift
+++ lib/ASTGen/Package.swift
@@ -1,5 +1,12 @@
 // swift-tools-version: 5.6
 
+// The CMake build system is the only one that's able to produce a working
+// compiler. This Package.swift makes it easier to build and work with the
+// swiftASTGen library within IDEs, but it's mainly there for editing---it
+// won't create something that can be meaningfully executed. Most things with
+// the new Swift parser are better implemented/tested within or on top of the
+// swift-syntax package.
+
 import PackageDescription
 
 let package = Package(
