7f466227d7f78a815b146b6761fa710db6df305a
Karl <5254025+karwa@users.noreply.github.com>
2022-10-22 07:16:17 +0200

    [stdlib] Remove unnecessary ASCII check

--- stdlib/public/core/StringUTF8Validation.swift
+++ stdlib/public/core/StringUTF8Validation.swift
@@ -101,10 +101,11 @@ internal func validateUTF8(_ buf: UnsafeBufferPointer<UInt8>) -> UTF8ValidationR
   }
 
   do {
-    var isASCII = true
     while let cu = iter.next() {
-      if UTF8.isASCII(cu) { lastValidIndex &+= 1; continue }
-      isASCII = false
+      if UTF8.isASCII(cu) { 
+        lastValidIndex &+= 1
+        continue 
+      }
       if _slowPath(!_isUTF8MultiByteLeading(cu)) {
         throw UTF8ValidationError()
       }
@@ -147,7 +148,7 @@ internal func validateUTF8(_ buf: UnsafeBufferPointer<UInt8>) -> UTF8ValidationR
         Builtin.unreachable()
       }
     }
-    return .success(UTF8ExtraInfo(isASCII: isASCII))
+    return .success(UTF8ExtraInfo(isASCII: false))
   } catch {
     return .error(toBeReplaced: findInvalidRange(buf[lastValidIndex...]))
   }
