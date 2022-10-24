2af6670fe399decd437bebd1140fbc5440e04e32
Egor Zhdan <e_zhdan@apple.com>
2022-10-24 15:27:05 +0100

    [cxx-interop] Enable a few C++ stdlib tests on Windows

--- test/Interop/Cxx/stdlib/use-std-string.swift
+++ test/Interop/Cxx/stdlib/use-std-string.swift
@@ -2,9 +2,6 @@
 // RUN: %target-run-simple-swift(-D USE_CXXSTDLIB_SPELLING -I %S/Inputs -Xfrontend -enable-experimental-cxx-interop)
 //
 // REQUIRES: executable_test
-//
-// Enable this everywhere once we have a solution for modularizing other C++ stdlibs: rdar://87654514
-// REQUIRES: OS=macosx || OS=linux-gnu
 
 import StdlibUnittest
 import StdString
