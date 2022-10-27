4932c021f46541970d820e0b74fc79a01e3fd661
Doug Gregor <dgregor@apple.com>
2022-10-26 21:43:25 -0700

    Avoid concurrency-related adjustments during witness matching
    We were only applying this logic for some declaration types, not all,
    which is silly. Fixes https://github.com/apple/swift/issues/61602 /
    rdar://101243966.

--- test/Concurrency/actor_isolation_cycle.swift
+++ test/Concurrency/actor_isolation_cycle.swift
@@ -10,3 +10,20 @@ public struct S : P {
   public func g(_: Int) {}
   public func f(_: T) {}
 }
+
+// https://github.com/apple/swift/issues/61602
+@available(SwiftStdlib 5.1, *)
+@MainActor protocol ProtocolWithAssociatedTypes {
+    associatedtype ID: Hashable
+    associatedtype Value
+
+    subscript(_ id: ID) -> Value { get set }
+}
+
+@available(SwiftStdlib 5.1, *)
+final class ClassConforming<ID: Hashable,Value>: ProtocolWithAssociatedTypes {
+    subscript(id: ID) -> Value {
+        get { fatalError() }
+        set { fatalError() }
+    }
+}
