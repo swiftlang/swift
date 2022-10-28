b9452f7a9a96f12f548eb4c722c771af6afc66ee
Erik Eckstein <eeckstein@apple.com>
2022-10-28 17:16:51 +0200

    fix wrong attribute ordering in the runtime
    This fix might be replaced by something better, e.g. https://github.com/apple/swift/pull/61476

--- stdlib/public/SwiftShims/swift/shims/Visibility.h
+++ stdlib/public/SwiftShims/swift/shims/Visibility.h
@@ -211,8 +211,10 @@
 // TODO: use this in shims headers in overlays.
 #if defined(__cplusplus)
 #define SWIFT_EXPORT_FROM(LIBRARY) extern "C" SWIFT_EXPORT_FROM_ATTRIBUTE(LIBRARY)
+#define SWIFT_EXTERN_C extern "C" 
 #else
 #define SWIFT_EXPORT_FROM(LIBRARY) SWIFT_EXPORT_FROM_ATTRIBUTE(LIBRARY)
+#define SWIFT_EXTERN_C
 #endif
 #define SWIFT_RUNTIME_EXPORT SWIFT_EXPORT_FROM(swiftCore)
 
