5d1c99c0d6e1af2c9397a9bc5f8e9534deee8d28
Nate Chandler <nate_chandler@apple.com>
2022-10-28 08:35:55 -0700

    [AddressLowering] CallArgRewriter takes ApplySite.
    Rather than being able to handle only to FullApplySites, it now handles
    PartialApplyInsts as well.

--- lib/SILOptimizer/Mandatory/AddressLowering.cpp
+++ lib/SILOptimizer/Mandatory/AddressLowering.cpp
@@ -1714,13 +1714,13 @@ namespace {
 /// object arguments with address-type arguments.
 class CallArgRewriter {
   AddressLoweringState &pass;
-  FullApplySite apply;
+  ApplySite apply;
   SILLocation callLoc;
   SILBuilder argBuilder;
   AddressMaterialization addrMat;
 
 public:
-  CallArgRewriter(FullApplySite apply, AddressLoweringState &pass)
+  CallArgRewriter(ApplySite apply, AddressLoweringState &pass)
       : pass(pass), apply(apply), callLoc(apply.getLoc()),
         argBuilder(pass.getBuilder(apply.getInstruction()->getIterator())),
         addrMat(pass, argBuilder) {}
