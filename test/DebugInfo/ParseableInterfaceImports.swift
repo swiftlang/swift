4f2431fec0c1023935e7e1d783ec753eb52aae54
Alexis Laferrière <alaf@apple.com>
2022-10-31 16:54:15 -0700

    [Test] Disable some checks on non-resilient swiftinterfaces
    Define SWIFT_ACCEPT_NON_RESILIENT_INTERFACES in tests intentionnally
    building against a non-resilient swiftinterface.

--- test/DebugInfo/ParseableInterfaceImports.swift
+++ test/DebugInfo/ParseableInterfaceImports.swift
@@ -1,9 +1,11 @@
 // RUN: %empty-directory(%t)
 // RUN: %target-swift-frontend-typecheck %S/basic.swift \
 // RUN:    -emit-module-interface-path %t/basic.swiftinterface
-// RUN: %target-swift-frontend -emit-ir -module-name Foo %s -I %t -g -o - \
+// RUN: env SWIFT_ACCEPT_NON_RESILIENT_INTERFACES=1 \
+// RUN:    %target-swift-frontend -emit-ir -module-name Foo %s -I %t -g -o - \
 // RUN:    | %FileCheck %s
-// RUN: %target-swift-frontend -emit-ir -module-name Foo %s -I %t -g -o - \
+// RUN: env SWIFT_ACCEPT_NON_RESILIENT_INTERFACES=1 \
+// RUN:     %target-swift-frontend -emit-ir -module-name Foo %s -I %t -g -o - \
 // RUN:    -sdk %t | %FileCheck %s --check-prefix=SDK
 
 import basic
