3b3bdaf8d15e3d0dac4d3f709c34a5e11b7526d3
Kavon Farvardin <kfarvardin@apple.com>
2022-11-01 18:52:52 -0700

    enforce correctness rules about `isolated` parameters
    This covers function types, closures, and function declarations:
    
    - only one `isolated` parameter
    - no global-actor + `isolated` parameter
    - no `nonisolated` + `isolated`.
    
    all diagnostics are warnings until Swift 6

--- lib/Sema/TypeCheckConcurrency.cpp
+++ lib/Sema/TypeCheckConcurrency.cpp
@@ -3086,30 +3086,6 @@ void swift::checkTopLevelActorIsolation(TopLevelCodeDecl *decl) {
     body->walk(checker);
 }
 
-static bool checkDuplicateIsolatedParameter(AbstractFunctionDecl *decl) {
-  // If we have already seen at least one 'isolated' parameter,
-  // and we see another one, this is invalid as only one 'isolated' param
-  // per function is allowed.
-  // See:
-  // https://github.com/apple/swift-evolution/blob/main/proposals/0313-actor-isolation-control.md#multiple-isolated-parameters
-  ParamDecl* previousIsolatedParam = nullptr;
-  for (size_t i = 0; i < decl->getParameters()->size(); ++i) {
-    auto param = decl->getParameters()->get(i);
-    if (param->isIsolated()) {
-      if (auto previous = previousIsolatedParam) {
-        decl->diagnose(diag::isolated_parameter_second_isolated_param,
-                       previous->getParameterName(),
-                       param->getParameterName());
-        return true;
-      }
-
-      previousIsolatedParam = const_cast<ParamDecl*>(param);
-    }
-  }
-
-  return false;
-}
-
 void swift::checkFunctionActorIsolation(AbstractFunctionDecl *decl) {
   // Disable this check for @LLDBDebuggerFunction functions.
   if (decl->getAttrs().hasAttribute<LLDBDebuggerFunctionAttr>())
@@ -3124,7 +3100,6 @@ void swift::checkFunctionActorIsolation(AbstractFunctionDecl *decl) {
       superInit->walk(checker);
   }
 
-  checkDuplicateIsolatedParameter(decl);
   if (decl->getAttrs().hasAttribute<DistributedActorAttr>()) {
     if (auto func = dyn_cast<FuncDecl>(decl)) {
       checkDistributedFunction(func);
@@ -3734,6 +3709,39 @@ static Optional<unsigned> getIsolatedParamIndex(ValueDecl *value) {
   return None;
 }
 
+/// Verifies rules about `isolated` parameters for the given decl. There is more
+/// checking about these in TypeChecker::checkParameterList.
+///
+/// This function is focused on rules that apply when it's a declaration with
+/// an isolated parameter, rather than some generic parameter list in a
+/// DeclContext.
+///
+/// This function assumes the value already contains an isolated parameter.
+static void checkDeclWithIsolatedParameter(ValueDecl *value) {
+  // assume there is an isolated parameter.
+  assert(getIsolatedParamIndex(value));
+
+  // Suggest removing global-actor attributes written on it, as its ignored.
+  if (auto attr = value->getGlobalActorAttr()) {
+    if (!attr->first->isImplicit()) {
+      value->diagnose(diag::isolated_parameter_combined_global_actor_attr,
+                      value->getDescriptiveKind())
+          .fixItRemove(attr->first->getRangeWithAt())
+          .warnUntilSwiftVersion(6);
+    }
+  }
+
+  // Suggest removing `nonisolated` as it is also ignored
+  if (auto attr = value->getAttrs().getAttribute<NonisolatedAttr>()) {
+    if (!attr->isImplicit()) {
+      value->diagnose(diag::isolated_parameter_combined_nonisolated,
+                      value->getDescriptiveKind())
+          .fixItRemove(attr->getRangeWithAt())
+          .warnUntilSwiftVersion(6);
+    }
+  }
+}
+
 ActorIsolation ActorIsolationRequest::evaluate(
     Evaluator &evaluator, ValueDecl *value) const {
   // If this declaration has actor-isolated "self", it's isolated to that
@@ -3747,6 +3755,8 @@ ActorIsolation ActorIsolationRequest::evaluate(
   // If this declaration has an isolated parameter, it's isolated to that
   // parameter.
   if (auto paramIdx = getIsolatedParamIndex(value)) {
+    checkDeclWithIsolatedParameter(value);
+
     // FIXME: This doesn't allow us to find an Actor or DistributedActor
     // bound on the parameter type effectively.
     auto param = getParameterList(value)->get(*paramIdx);
