ce2f37507d04ece4c1bb6fd219162ad0f337b2ee
Allan Shortlidge <shortlidge@apple.com>
2022-11-08 09:33:41 -0800

    IRGen: Add `#_hasSymbol` support for async declarations.
    Actor isolated methods are still not supported, though, because typechecking doesn't allow a partial application of those.
    
    Resolves rdar://102094672

--- test/IRGen/Inputs/has_symbol_helper.swift
+++ test/IRGen/Inputs/has_symbol_helper.swift
@@ -1,11 +1,23 @@
 public let global: Int = 0
 
+#if CONCURRENCY
+@globalActor
+public struct GA {
+  public actor A {}
+  public static let shared = A()
+}
+#endif
+
 public func function(with argument: Int) {}
 public func throwingFunc() throws {}
 public func genericFunc<T: P>(_ t: T) {}
 public func funcWithOpaqueResult() -> some P { return S(member: 0) }
 @_cdecl("cdecl_func") public func cdeclFunc() {}
 @_silgen_name("forward_declared_func") public func forwardDeclaredFunc()
+#if CONCURRENCY
+@GA public func isolatedFunc() {}
+public func asyncFunc() async {}
+#endif
 
 public protocol P {
   func requirement()
@@ -33,3 +45,10 @@ public enum E {
   case basicCase
   case payloadCase(_: S)
 }
+
+#if CONCURRENCY
+public actor A {
+  public init() {}
+  public func asyncMethod() async {}
+}
+#endif
