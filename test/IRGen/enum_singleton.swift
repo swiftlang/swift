87432bdbe01384686c1f7f984e7194f428920785
Dario Rexin <drexin@apple.com>
2022-11-10 08:55:48 -0800

    [IRGen] Generate proper TypeLayout for singleton enums (#61966)
    * [IRGen] Generate proper TypeLayout for singleton enums
    
    rdar://101587387
    
    Instead of creating a proper TypeLayout, we returned the layout for the singleton case, which caused the tags for the nested cases cases to be returned, instead of the expected `0` for singleton enums.
    
    * Forward destroy, copy calls
    
    * Add some missing forwarding cases

---
+++ test/IRGen/enum_singleton.swift
@@ -0,0 +1,54 @@
+// RUN: %target-swift-frontend -emit-ir %s | %FileCheck %s --check-prefix=CHECK
+// RUN: %target-swift-frontend -emit-ir -O %s | %FileCheck %s --check-prefix=CHECK --check-prefix=CHECK-OPT
+
+public enum Payload {
+  case c1(Bool)
+  case c2(Bool)
+  case c3(Any)
+}
+
+public enum SingletonEnum {
+  case c1(Payload)
+}
+
+// CHECK: define internal void @"$s14enum_singleton13SingletonEnumOwxx"
+// CHECK-OPT:   tail call void @"{{.*}}OwxxTm"
+// CHECK:   ret void
+// CHECK: }
+
+// CHECK: define internal %swift.opaque* @"$s14enum_singleton13SingletonEnumOwcp"
+// CHECK-OPT:   [[R0:%.*]] = tail call %swift.opaque* @"{{.*}}OwcpTm"
+// CHECK-OPT:   ret %swift.opaque* [[R0]]
+// CHECK: }
+
+// CHECK: define internal %swift.opaque* @"$s14enum_singleton13SingletonEnumOwca"
+// CHECK-OPT:   [[R1:%.*]] = tail call %swift.opaque* @"{{.*}}OwcaTm"
+// CHECK-OPT:   ret %swift.opaque* [[R1]]
+// CHECK: }
+
+// CHECK: define internal %swift.opaque* @"$s14enum_singleton13SingletonEnumOwta"
+// CHECK-OPT:   [[R2:%.*]] = tail call %swift.opaque* @"{{.*}}OwtaTm"
+// CHECK-OPT:   ret %swift.opaque* [[R2]]
+// CHECK: }
+
+// CHECK: define internal i32 @"$s14enum_singleton13SingletonEnumOwet"
+// CHECK-OPT:   [[R2:%.*]] = tail call i32 @"{{.*}}OwetTm"
+// CHECK-OPT:   ret i32 [[R2]]
+// CHECK: }
+
+// CHECK: define internal void @"$s14enum_singleton13SingletonEnumOwst"
+// CHECK-OPT:   tail call void @"{{.*}}OwstTm"
+// CHECK-OPT:   ret void
+// CHECK: }
+
+// CHECK: define internal i32 @"$s14enum_singleton13SingletonEnumOwug"
+// CHECK:   ret i32 0
+// CHECK: }
+
+// CHECK: define internal void @"$s14enum_singleton13SingletonEnumOwup"
+// CHECK:   ret void
+// CHECK: }
+
+// CHECK: define internal void @"$s14enum_singleton13SingletonEnumOwui"
+// CHECK:   ret void
+// CHECK: }
