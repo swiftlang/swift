980d40951ff557047dce833d1d092b400bbc20db
Robert Widmann <devteam.codafi@gmail.com>
2022-11-16 13:29:00 -0800

    Delete libSyntax Tests

--- test/Syntax/round_trip_stdlib.swift
+++
@@ -1,3 +0,0 @@
-// <rdar://problem/64269925> test/Syntax/round_trip_stdlib.swift is very slow on ASAN build
-// REQUIRES: no_asan
-// RUN: %round-trip-syntax-test -d %swift_src_root/stdlib --swift-syntax-test %swift-syntax-test
