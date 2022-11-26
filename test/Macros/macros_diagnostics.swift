4f823c60be5e84d1fb0b492126091a79f39a409c
Doug Gregor <dgregor@apple.com>
2022-11-25 21:50:20 -0800

    [Macros] Ensure that macros cannot return opaque result types

--- test/Macros/macros_diagnostics.swift
+++ test/Macros/macros_diagnostics.swift
@@ -3,7 +3,14 @@
 
 macro stringify<T>(_ value: T) -> (T, String) = BuiltinMacros.StringifyMacro
 
+protocol P { }
+
+macro tryToHide<T: P>(_: P) -> some P = BuiltinMacros.Blah
+// expected-error@-1{{some' types are only permitted in properties, subscripts, and functions}}
+
+
 func test(a: Int, b: Int) {
   // FIXME: Bad diagnostic.
   let s = #stringify<Int, Int>(a + b) // expected-error{{type of expression is ambiguous without more context}}
 }
+
