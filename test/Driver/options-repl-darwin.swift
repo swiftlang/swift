5462c0a8ef8beea3b0d62e4fd77b67ad07fa00e8
Doug Gregor <dgregor@apple.com>
2022-11-30 13:53:08 -0800

    Ensure that the host library path is provided to tests.
    Eliminate the "copy the plugin library" step for these tests, which was
    always a hack and doesn't scale when we add more shared libraries.

--- test/Driver/options-repl-darwin.swift
+++ test/Driver/options-repl-darwin.swift
@@ -7,20 +7,18 @@
 // RUN: %empty-directory(%t/usr/bin/)
 // RUN: %empty-directory(%t/usr/lib/)
 // RUN: %hardlink-or-copy(from: %swift_driver_plain, to: %t/usr/bin/swift)
-// RUN: %copy-plugin-support-library(%t/usr/lib/)
 
-// RUN: %t/usr/bin/swift -repl -### | %FileCheck -check-prefix=INTEGRATED %s
-// RUN: %t/usr/bin/swift -### | %FileCheck -check-prefix=INTEGRATED %s
+// RUN: %host-library-env %t/usr/bin/swift -repl -### | %FileCheck -check-prefix=INTEGRATED %s
+// RUN: %host-library-env %t/usr/bin/swift -### | %FileCheck -check-prefix=INTEGRATED %s
 
 // RUN: touch %t/usr/bin/lldb
 // RUN: chmod +x %t/usr/bin/lldb
-// RUN: %t/usr/bin/swift -repl -### | %FileCheck -check-prefix=LLDB %s
-// RUN: %t/usr/bin/swift -### | %FileCheck -check-prefix=LLDB %s
+// RUN: %host-library-env %t/usr/bin/swift -repl -### | %FileCheck -check-prefix=LLDB %s
+// RUN: %host-library-env %t/usr/bin/swift -### | %FileCheck -check-prefix=LLDB %s
 
 // RUN: %empty-directory(%t/Toolchains/Test.xctoolchain/usr/bin)
 // RUN: %empty-directory(%t/Toolchains/Test.xctoolchain/usr/lib)
 // RUN: mv %t/usr/bin/swift %t/Toolchains/Test.xctoolchain/usr/bin/swift
-// RUN: %copy-plugin-support-library(%t/Toolchains/Test.xctoolchain/usr/lib/)
 // RUN: %t/Toolchains/Test.xctoolchain/usr/bin/swift -repl -### | %FileCheck -check-prefix=LLDB %s
 
 // Clean up the test executable because hard links are expensive.
