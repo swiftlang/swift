40cb06dd8521328c4cf010351f1a4ecd750ee538
Matt Jacobson <mhjacobson@me.com>
2022-11-30 17:50:53 -0500

    build: on FreeBSD, set the rpath of built stdlib dylibs to $ORIGIN
    This matches OpenBSD and Linux, and it allows, for example,
    libswift_StringProcessing.so to find libswiftGlibc.so at dynamic link time.

--- stdlib/cmake/modules/AddSwiftStdlib.cmake
+++ stdlib/cmake/modules/AddSwiftStdlib.cmake
@@ -1115,6 +1115,10 @@ function(add_swift_target_library_single target name)
     set_target_properties("${target}"
       PROPERTIES
       INSTALL_RPATH "$ORIGIN")
+  elseif("${SWIFTLIB_SINGLE_SDK}" STREQUAL "FREEBSD")
+    set_target_properties("${target}"
+      PROPERTIES
+      INSTALL_RPATH "$ORIGIN")
   endif()
 
   set_target_properties("${target}" PROPERTIES BUILD_WITH_INSTALL_RPATH YES)
