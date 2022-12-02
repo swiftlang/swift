91d00ac1705c218185467c014fa215c4b2cf49ed
Erik Eckstein <eeckstein@apple.com>
2022-12-02 11:12:23 +0100

    tests: re-enable SILOptimizer/set.swift
    And make it more robust.
    
    rdar://102865253

--- test/SILOptimizer/set.swift
+++ test/SILOptimizer/set.swift
@@ -1,13 +1,11 @@
-// REQUIRES: rdar102865253
-
-// RUN: %target-swift-frontend  -parse-as-library -primary-file %s -O -sil-verify-all -module-name=test -emit-sil | %FileCheck %s
-// RUN: %target-swift-frontend  -parse-as-library -primary-file %s -Osize -sil-verify-all -module-name=test -emit-sil | %FileCheck %s
+// RUN: %target-swift-frontend  -parse-as-library -primary-file %s -O -sil-verify-all -module-name=test -Xllvm -sil-disable-pass=function-signature-opts -emit-sil | %FileCheck %s
+// RUN: %target-swift-frontend  -parse-as-library -primary-file %s -Osize -sil-verify-all -module-name=test -Xllvm -sil-disable-pass=function-signature-opts -emit-sil | %FileCheck %s
 // REQUIRES: swift_stdlib_no_asserts,optimized_stdlib
 // REQUIRES: swift_in_compiler
 
 // Test optimal code generation for creating empty sets.
 
-// CHECK-LABEL: sil @$s4test30createEmptySetFromArrayLiteralShySiGyF
+// CHECK-LABEL: sil {{.*}}@$s4test30createEmptySetFromArrayLiteralShySiGyF
 // CHECK:         global_addr @_swiftEmptySetSingleton
 // CHECK-NOT:     apply
 // CHECK:       } // end sil function '$s4test30createEmptySetFromArrayLiteralShySiGyF'
@@ -15,7 +13,7 @@ public func createEmptySetFromArrayLiteral() -> Set<Int> {
     return []
 }
 
-// CHECK-LABEL: sil @$s4test29createEmptySetWithInitializerShySiGyF
+// CHECK-LABEL: sil {{.*}}@$s4test29createEmptySetWithInitializerShySiGyF
 // CHECK:         global_addr @_swiftEmptySetSingleton
 // CHECK-NOT:     apply
 // CHECK:       } // end sil function '$s4test29createEmptySetWithInitializerShySiGyF'
@@ -23,7 +21,7 @@ public func createEmptySetWithInitializer() -> Set<Int> {
     return Set<Int>()
 }
 
-// CHECK-LABEL: sil @$s4test17createNonEmptySetShySiGyF
+// CHECK-LABEL: sil {{.*}}@$s4test17createNonEmptySetShySiGyF
 // CHECK:         global_value
 // CHECK:         [[F:%[0-9]+]] = function_ref @$sSh21_nonEmptyArrayLiteralShyxGSayxG_tcfCSi_Tg5
 // CHECK:         apply [[F]]
