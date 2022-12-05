f9141e1aa196057d872e3b37f3ccb8b5dc6a0f9c
Slava Pestov <spestov@apple.com>
2022-12-05 14:09:32 -0500

    SIL: Stub out type lowering of function types containing pack expansions

--- test/SILGen/pack_expansion_type.swift
+++ test/SILGen/pack_expansion_type.swift
@@ -11,4 +11,8 @@ public struct VariadicType<T...> {
   // CHECK-LABEL: sil [ossa] @$s19pack_expansion_type12VariadicTypeV14variadicMethod1t1ux_qd__txQp_txxQp_qd__qd__Qptqd__RhzlF : $@convention(method) <T...><U... where ((T, U)...) : Any> (@in_guaranteed T..., @in_guaranteed U..., VariadicType<T...>) -> @out (T, U)... {
   // CHECK: bb0(%0 : $*(T, U)..., %1 : $*T..., %2 : $*U..., %3 : $VariadicType<T...>):
   public func variadicMethod<U...>(t: T..., u: U...) -> ((T, U)...) {}
+
+  // CHECK-LABEL: sil [ossa] @$s19pack_expansion_type12VariadicTypeV13takesFunction1tyqd__qd__Qp_txxQpXE_tlF : $@convention(method) <T...><U...> (@noescape @callee_guaranteed @substituted <τ_0_0..., τ_0_1..., τ_0_2..., τ_0_3...> (@in_guaranteed τ_0_0...) -> @out τ_0_2... for <T, T, U, U>, VariadicType<T...>) -> () {
+  // CHECK: bb0(%0 : $@noescape @callee_guaranteed @substituted <τ_0_0..., τ_0_1..., τ_0_2..., τ_0_3...> (@in_guaranteed τ_0_0...) -> @out τ_0_2... for <T, T, U, U>, %1 : $VariadicType<T...>):
+  public func takesFunction<U...>(t: (T...) -> (U...)) {}
 }
