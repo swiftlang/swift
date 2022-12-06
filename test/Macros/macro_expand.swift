a2dfd90516ef173a0317ee72bf2bead1785c67ef
Doug Gregor <dgregor@apple.com>
2022-12-05 18:18:02 -0800

    [Macros] Fix test case after rename

--- test/Macros/macro_expand.swift
+++ test/Macros/macro_expand.swift
@@ -1,6 +1,6 @@
 // RUN: %empty-directory(%t)
 // RUN: %target-build-swift -I %swift-host-lib-dir -L %swift-host-lib-dir -emit-library -o %t/%target-library-name(MacroDefinition) -module-name=MacroDefinition %S/Inputs/syntax_macro_definitions.swift -g -no-toolchain-stdlib-rpath
-// RUNx: %target-swift-frontend -dump-ast -enable-experimental-feature Macros -load-plugin-library %t/%target-library-name(MacroDefinition) -I %swift-host-lib-dir %s -module-name MacroUser 2>&1 | %FileCheck --check-prefix CHECK-AST %s
+// RUNx: %target-swift-frontend -dump-ast -enable-experimental-feature Macros -enable-experimental-feature Macros -load-plugin-library %t/%target-library-name(MacroDefinition) -I %swift-host-lib-dir %s -module-name MacroUser 2>&1 | %FileCheck --check-prefix CHECK-AST %s
 // RUN: %target-typecheck-verify-swift -enable-experimental-feature Macros -load-plugin-library %t/%target-library-name(MacroDefinition) -I %swift-host-lib-dir -module-name MacroUser -DTEST_DIAGNOSTICS
 // RUN: %target-build-swift -enable-experimental-feature Macros -enable-experimental-feature Macros -load-plugin-library %t/%target-library-name(MacroDefinition) -I %swift-host-lib-dir -L %swift-host-lib-dir %s -o %t/main -module-name MacroUser
 // RUN: %target-run %t/main | %FileCheck %s
@@ -14,10 +14,10 @@ macro stringify<T>(_ value: T) -> (T, String) = MacroDefinition.StringifyMacro
 macro fileID<T: _ExpressibleByStringLitera>: T = MacroDefinition.FileIDMacro
 
 func testFileID(a: Int, b: Int) {
-  // CHECK: MacroUser/macro_external_exec.swift
+  // CHECK: MacroUser/macro_expand.swift
   print("Result is \(#customFileID)")
 
-  // CHECK: Builtin result is MacroUser/macro_external_exec.swift
+  // CHECK: Builtin result is MacroUser/macro_expand.swift
   // CHECK-AST: macro_expansion_expr type='String'{{.*}}name=line
   print("Builtin result is \(#fileID)")
 }
