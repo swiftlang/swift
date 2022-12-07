4253131adc322f0cddb469c956d42836041514c8
Alex Hoppen <ahoppen@apple.com>
2022-12-07 22:27:24 +0100

    [SwiftSyntax] Verify files generated using CodeGeneration in CI

--- utils/swift_build_support/swift_build_support/products/swiftsyntax.py
+++ utils/swift_build_support/swift_build_support/products/swiftsyntax.py
@@ -79,13 +79,6 @@ class SwiftSyntax(product.Product):
                 os.path.join(self.source_dir, 'build-script.py'),
                 'verify-source-code',
                 '--toolchain', self.install_toolchain_path(host_target),
-                # Verifying the files generated using SwiftSyntaxBuilder requires
-                # internet access to pull the pinned SwiftSyntaxBuilder version. Since
-                # we don't have internet access in CI, don't verify these files.
-                # This is not a huge deal because only SwiftSyntaxBuilder is generated
-                # in terms of itself and it will most likely fail to compile if it isn't
-                # re-generated after gyb files have been updated.
-                '--gyb-only'
             ]
             if self.args.verbose_build:
                 build_cmd.append('--verbose')
