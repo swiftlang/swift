a5af302173a28258d50d2f077a670fbd02b55b29
Pavel Yaskevich <pyaskevich@apple.com>
2022-12-08 14:34:05 -0800

    [stdlib] RuntimeMetadata: Register newly added "runtime attributes" section

--- stdlib/public/runtime/SwiftRT-ELF.cpp
+++ stdlib/public/runtime/SwiftRT-ELF.cpp
@@ -42,6 +42,7 @@ DECLARE_SWIFT_SECTION(swift5_builtin)
 DECLARE_SWIFT_SECTION(swift5_capture)
 DECLARE_SWIFT_SECTION(swift5_mpenum)
 DECLARE_SWIFT_SECTION(swift5_accessible_functions)
+DECLARE_SWIFT_SECTION(swift5_runtime_attributes)
 }
 
 #undef DECLARE_SWIFT_SECTION
@@ -77,6 +78,7 @@ static void swift_image_constructor() {
       SWIFT_SECTION_RANGE(swift5_capture),
       SWIFT_SECTION_RANGE(swift5_mpenum),
       SWIFT_SECTION_RANGE(swift5_accessible_functions),
+      SWIFT_SECTION_RANGE(swift5_runtime_attributes),
   };
 
 #undef SWIFT_SECTION_RANGE
