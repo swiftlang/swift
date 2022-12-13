387d36242d0759ff9de732aec878bdcaa7552e83
Konrad `ktoso` Malawski <ktoso@apple.com>
2022-12-13 21:45:11 +0900

    [Concurrency] Fix missing inherit executor on withTaskCancellationHandler (#62546)

---
+++ test/Concurrency/Runtime/async_task_cancellation_taskGroup.swift
@@ -0,0 +1,40 @@
+// RUN: %target-run-simple-swift( -Xfrontend -disable-availability-checking %import-libdispatch -parse-as-library) | %FileCheck %s --dump-input=always
+
+// REQUIRES: executable_test
+// REQUIRES: concurrency
+// REQUIRES: libdispatch
+
+// rdar://76038845
+// REQUIRES: concurrency_runtime
+
+import Dispatch
+
+@available(SwiftStdlib 5.1, *)
+func test_detach_cancel_taskGroup() async {
+  print(#function) // CHECK: test_detach_cancel_taskGroup
+
+  await withTaskGroup(of: Void.self) { group in
+    group.cancelAll() // immediately cancel the group
+    print("group.cancel()") // CHECK: group.cancel()
+
+    group.addTask {
+      // immediately cancelled child task...
+      await withTaskCancellationHandler {
+        print("child: operation, was cancelled: \(Task.isCancelled)")
+      } onCancel: {
+        print("child: onCancel, was cancelled: \(Task.isCancelled)")
+      }
+    }
+    // CHECK: child: onCancel, was cancelled: true
+    // CHECK: child: operation, was cancelled: true
+  }
+
+  print("done") // CHECK: done
+}
+
+@available(SwiftStdlib 5.1, *)
+@main struct Main {
+  static func main() async {
+    await test_detach_cancel_taskGroup()
+  }
+}
