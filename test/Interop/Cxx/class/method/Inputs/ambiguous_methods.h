168ef490af2863fe2d8c50881abad8cd4bfedb94
Egor Zhdan <e_zhdan@apple.com>
2022-12-14 12:29:02 +0000

    [cxx-interop] Disambiguate const and non-const methods consistently
    When importing a C++ struct that contains two methods that only differ in const-ness, we append `Mutating` to the name of the non-const method to make it possible to call from Swift unambiguously.
    
    Unfortunately that logic was dependent on the order in which we import methods of a class: the `Mutating` suffix was added when another method with the same name was already imported.
    
    This caused lookup failures, and the behavior was incorrect when the pair of methods return instances of an unsafe type: the const overload was renamed as `Unsafe` properly, but the non-const overload was not renamed.

--- test/Interop/Cxx/class/method/Inputs/ambiguous_methods.h
+++ test/Interop/Cxx/class/method/Inputs/ambiguous_methods.h
@@ -47,4 +47,13 @@ struct HasAmbiguousMethods2 {
   }
 };
 
+struct Unsafe {
+  int *ptr;
+};
+
+struct HasAmbiguousUnsafeMethods {
+  Unsafe getUnsafe() const { return Unsafe(); }
+  Unsafe getUnsafe() { return Unsafe(); }
+};
+
 #endif // TEST_INTEROP_CXX_CLASS_AMBIGUOUS_METHOD_METHODS_H
