6725f365ba79d62f5bf187edfb8e9fce511ccf06
Holly Borla <hborla@apple.com>
2022-12-22 17:06:51 -0500

    [Variadic Generics] Update tests for syntax changes.

--- test/ModuleInterface/pack_expansion_type.swift
+++ test/ModuleInterface/pack_expansion_type.swift
@@ -6,12 +6,12 @@
 // REQUIRES: asserts
 
 // CHECK: public func variadicFunction<T..., U...>(t: T..., u: U...) -> ((T, U)...) where ((T, U)...) : Any
-public func variadicFunction<T..., U...>(t: T..., u: U...) -> ((T, U)...) {}
+public func variadicFunction<T..., U...>(t: repeat each T, u: repeat each  U) -> (repeat (each T, each U)) {}
 
 // CHECK: public struct VariadicType<T...> {
 public struct VariadicType<T...> {
   // CHECK: public func variadicMethod<U...>(t: T..., u: U...) -> ((T, U)...) where ((T, U)...) : Any
-  public func variadicMethod<U...>(t: T..., u: U...) -> ((T, U)...) {}
+  public func variadicMethod<U...>(t: repeat each T, u: repeat each  U) -> (repeat (each T, each U)) {}
 
   // CHECK: public func returnsSelf() -> PackExpansionType.VariadicType<T...>
   public func returnsSelf() -> Self {}
@@ -25,10 +25,10 @@ public func returnsVariadicType() -> VariadicType< > {}
 public func returnsVariadicType() -> VariadicType<Int, String, Float> {}
 
 // CHECK: public func returnsVariadicType<T...>() -> PackExpansionType.VariadicType<T...>
-public func returnsVariadicType<T...>() -> VariadicType<T... > {}
+public func returnsVariadicType<T...>() -> VariadicType<repeat each T> {}
 
 // CHECK: public typealias VariadicAlias<T...> = PackExpansionType.VariadicType<Swift.Array<T>...>
-public typealias VariadicAlias<T...> = VariadicType<Array<T>... >
+public typealias VariadicAlias<T...> = VariadicType<repeat Array<each T>>
 
 // CHECK: public func returnsVariadicAlias() -> PackExpansionType.VariadicAlias<>
 public func returnsVariadicAlias() -> VariadicAlias< > {}
@@ -37,4 +37,4 @@ public func returnsVariadicAlias() -> VariadicAlias< > {}
 public func returnsVariadicAlias() -> VariadicAlias<Int, String, Float> {}
 
 // CHECK: public func returnsVariadicAlias<T...>() -> PackExpansionType.VariadicAlias<T...>
-public func returnsVariadicAlias<T...>() -> VariadicAlias<T... > {}
+public func returnsVariadicAlias<T...>() -> VariadicAlias<repeat each T> {}
