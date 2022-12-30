bf8e1b4264bc8c4c71fbafea7494b04b4f9bb0a2
Yuta Saito <kateinoigakukun@gmail.com>
2022-12-30 08:30:29 +0000

    [ci] cleanup ci.sh for unnecessary workarounds

--- utils/webassembly/ci.sh
+++ utils/webassembly/ci.sh
@@ -2,26 +2,10 @@
 
 set -ex
 
-SOURCE_PATH="$( cd "$(dirname $0)/../../.." && pwd  )" 
+SOURCE_PATH="$(cd "$(dirname $0)/../../.." && pwd)"
 SWIFT_PATH=$SOURCE_PATH/swift
 UTILS_PATH=$SWIFT_PATH/utils/webassembly
-
-case $(uname -s) in
-  Darwin)
-    HOST_SUFFIX=macosx-$(uname -m)
-  ;;
-  Linux)
-    HOST_SUFFIX=linux-$(uname -m)
-  ;;
-  *)
-    echo "Unrecognised platform $(uname -s)"
-    exit 1
-  ;;
-esac
-
 BUILD_SCRIPT=$UTILS_PATH/build-toolchain.sh
-RUN_TEST_BIN=$SWIFT_PATH/utils/run-test
-TARGET_STDLIB_BUILD_DIR=$SOURCE_PATH/target-build/swift-stdlib-wasi-wasm32
 BUILD_SDK_PATH="$SOURCE_PATH/build-sdk"
 
 export WASMER_DIR="$BUILD_SDK_PATH/wasmer"
@@ -33,7 +17,4 @@ export PATH="$BUILD_SDK_PATH/bin:$PATH"
 export SCCACHE_CACHE_SIZE="50G"
 export SCCACHE_DIR="$SOURCE_PATH/build-cache"
 
-$BUILD_SCRIPT
-
-# workaround: host target test directory is necessary to use run-test
-mkdir -p "$TARGET_STDLIB_BUILD_DIR/test-$HOST_SUFFIX"
+$UTILS_PATH/build-toolchain.sh
