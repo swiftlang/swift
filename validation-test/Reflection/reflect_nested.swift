893d83ba0ad8ae279d2f7c792a237618b54476bd
Augusto Noronha <augusto2112@me.com>
2023-01-04 14:17:44 -0800

    Fix wrong calculation of generic params per level when building decl
    Fix computation of generic params per level when a generic type is
    nested in  a non-generic one. For example, for the following code:
    
    ```
    class A {
      class B<T, U> {
      }
    }
    ```
    Fix the array of generic params per level from [2] to [0, 2].
    
    rdar://103457629

--- validation-test/Reflection/reflect_nested.swift
+++ validation-test/Reflection/reflect_nested.swift
@@ -43,6 +43,37 @@ reflect(object: obj)
 // CHECK-32-NEXT:   (bound_generic_class reflect_nested.OuterGeneric.Inner
 // CHECK-32-NEXT:     (bound_generic_class reflect_nested.OuterGeneric
 // CHECK-32-NEXT:       (struct Swift.Int))))
+  
+class OuterNonGeneric {
+  class InnerGeneric<T, U> {
+    var x: T
+    var y: U
+
+    init(x: T, y: U) {
+      self.x = x
+      self.y = y
+    }
+  }
+}
+
+var obj2 = OuterNonGeneric.InnerGeneric(x: 17, y: "hello")
+reflect(object: obj2)
+
+// CHECK-64: Reflecting an object.
+// CHECK-64: Instance pointer in child address space: 0x{{[0-9a-fA-F]+}}
+// CHECK-64: Type reference:
+// CHECK-64: (bound_generic_class reflect_nested.OuterNonGeneric.InnerGeneric
+// CHECK-64-NEXT:   (struct Swift.Int)
+// CHECK-64-NEXT:   (struct Swift.String)
+// CHECK-64-NEXT:     (bound_generic_class reflect_nested.OuterNonGeneric))
+
+// CHECK-32: Reflecting an object.
+// CHECK-32: Instance pointer in child address space: 0x{{[0-9a-fA-F]+}}
+// CHECK-32: Type reference:
+// CHECK-32: (bound_generic_class reflect_nested.OuterNonGeneric.InnerGeneric
+// CHECK-32-NEXT:   (struct Swift.Int)
+// CHECK-32-NEXT:   (struct Swift.String)
+// CHECK-32-NEXT:     (bound_generic_class reflect_nested.OuterNonGeneric))
 
 doneReflecting()
 
