1358b4cbeea3a8d64f6b670d1bdc56a4fc49b808
Holly Borla <hborla@apple.com>
2023-01-04 19:00:45 -0800

    [Variadic Generics] Update pack expansion type mangling in tests.
    Previously, pack expansion types of plain parameter packs whose reduced
    shape is another parameter pack (i.e. as a result of parallel pack expansion
    somewhere else in the signature) was previously incorrect. Fixing that "broke"
    the incorrect mangling in these tests.

--- test/IRGen/variadic_generic_functions.swift
+++ test/IRGen/variadic_generic_functions.swift
@@ -11,5 +11,5 @@ func f1<T...>(t: repeat each T) {}
 // CHECK-LABEL: define hidden swiftcc void @"$s26variadic_generic_functions2f21t1uyxxQp_q_q_Qptr0_lF"(i8** noalias nocapture dereferenceable(8) %0, i8** noalias nocapture dereferenceable(8) %1, i64 %2, i64 %3, %swift.type* %T, %swift.type* %U)
 func f2<T..., U...>(t: repeat each T, u: repeat each U) {}
 
-// CHECK-LABEL: define hidden swiftcc void @"$s26variadic_generic_functions2f31t1uyxxQp_q_q_Qptq_Rhzr0_lF"(i8** noalias nocapture dereferenceable(8) %0, i8** noalias nocapture dereferenceable(8) %1, i64 %2, %swift.type* %T, %swift.type* %U)
+// CHECK-LABEL: define hidden swiftcc void @"$s26variadic_generic_functions2f31t1uyxxQp_q_xQptq_Rhzr0_lF"(i8** noalias nocapture dereferenceable(8) %0, i8** noalias nocapture dereferenceable(8) %1, i64 %2, %swift.type* %T, %swift.type* %U)
 func f3<T..., U...>(t: repeat each T, u: repeat each U) where (repeat (each T, each U)): Any {}
--- test/SILGen/pack_expansion_type.swift
+++ test/SILGen/pack_expansion_type.swift
@@ -5,12 +5,12 @@
 
 // XFAIL: OS=windows-msvc
 
-// CHECK-LABEL: sil [ossa] @$s19pack_expansion_type16variadicFunction1t1ux_q_txQp_txxQp_q_q_Qptq_Rhzr0_lF : $@convention(thin) <T..., U... where ((T, U)...) : Any> (@in_guaranteed T..., @in_guaranteed U...) -> @out (T, U)... {
+// CHECK-LABEL: sil [ossa] @$s19pack_expansion_type16variadicFunction1t1ux_q_txQp_txxQp_q_xQptq_Rhzr0_lF : $@convention(thin) <T..., U... where ((T, U)...) : Any> (@in_guaranteed T..., @in_guaranteed U...) -> @out (T, U)... {
 // CHECK: bb0(%0 : $*(T, U)..., %1 : $*T..., %2 : $*U...):
 public func variadicFunction<T..., U...>(t: repeat each T, u: repeat each U) -> (repeat (each T, each U)) {}
 
 public struct VariadicType<T...> {
-  // CHECK-LABEL: sil [ossa] @$s19pack_expansion_type12VariadicTypeV14variadicMethod1t1ux_qd__txQp_txxQp_qd__qd__Qptqd__RhzlF : $@convention(method) <T...><U... where ((T, U)...) : Any> (@in_guaranteed T..., @in_guaranteed U..., VariadicType<T...>) -> @out (T, U)... {
+  // CHECK-LABEL: sil [ossa] @$s19pack_expansion_type12VariadicTypeV14variadicMethod1t1ux_qd__txQp_txxQp_qd__xQptqd__RhzlF : $@convention(method) <T...><U... where ((T, U)...) : Any> (@in_guaranteed T..., @in_guaranteed U..., VariadicType<T...>) -> @out (T, U)... {
   // CHECK: bb0(%0 : $*(T, U)..., %1 : $*T..., %2 : $*U..., %3 : $VariadicType<T...>):
   public func variadicMethod<U...>(t: repeat each T, u: repeat each U) -> (repeat (each T, each U)) {}
 
