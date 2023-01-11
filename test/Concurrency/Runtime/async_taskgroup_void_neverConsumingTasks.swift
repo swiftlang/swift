1d1cda737731d10a73f11294a3cbbe8c254ef3c9
Konrad `ktoso` Malawski <konrad_malawski@apple.com>
2023-01-11 15:50:07 +0900

    remove import-darwin from test

--- test/Concurrency/Runtime/async_taskgroup_void_neverConsumingTasks.swift
+++ test/Concurrency/Runtime/async_taskgroup_void_neverConsumingTasks.swift
@@ -4,7 +4,6 @@
 // REQUIRES: concurrency_runtime
 // UNSUPPORTED: back_deployment_runtime
 // UNSUPPORTED: OS=linux-gnu
-import Darwin
 
 actor Waiter {
   let until: Int
@@ -19,7 +18,6 @@ actor Waiter {
 
   func increment() {
     self.count += 1
-    fputs("> increment (\(self.count)/\(self.until))\n", stderr);
     if self.until <= self.count {
       if let cc = self.cc {
         cc.resume(returning: self.count)
@@ -29,12 +27,10 @@ actor Waiter {
 
   func wait() async -> Int {
     if self.until <= self.count {
-      fputs("> RETURN in Waiter\n", stderr);
       return self.count
     }
 
     return await withCheckedContinuation { cc in
-      fputs("> WAIT in Waiter\n", stderr);
       self.cc = cc
     }
   }
@@ -48,9 +44,7 @@ func test_taskGroup_void_neverConsume() async {
   print("Start tasks: \(until)")
   let allTasks = await withDiscardingTaskGroup() { group in
     for n in 1...until {
-    fputs("> enqueue: \(n)\n", stderr);
       group.addTask {
-        fputs("> run: \(n)\n", stderr);
         try? await Task.sleep(until: .now + .milliseconds(100), clock: .continuous)
         await waiter.increment()
       }
@@ -71,9 +65,7 @@ func test_taskGroup_void_neverConsume(sleepBeforeGroupWaitAll: Duration) async {
   print("Start tasks: \(until)")
   let allTasks = await withDiscardingTaskGroup() { group in
     for n in 1...until {
-    fputs("> enqueue: \(n)\n", stderr);
       group.addTask {
-        fputs("> run: \(n)\n", stderr);
         try? await Task.sleep(until: .now + .milliseconds(100), clock: .continuous)
         await waiter.increment()
       }
