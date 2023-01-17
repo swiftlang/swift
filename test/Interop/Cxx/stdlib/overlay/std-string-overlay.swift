9818312da417c880b8a300001297675038ba8765
Egor Zhdan <e_zhdan@apple.com>
2023-01-17 11:51:13 +0000

    Revert "Revert "[cxx-interop] Add conversions between `std::u16string` and `Swift.String`""
    This re-lands https://github.com/apple/swift/pull/61695 since the linker error on CentOS has been fixed in https://github.com/apple/swift/pull/63058.
    
    This reverts commit e97b1c8e

--- test/Interop/Cxx/stdlib/overlay/std-string-overlay.swift
+++ test/Interop/Cxx/stdlib/overlay/std-string-overlay.swift
@@ -45,6 +45,36 @@ StdStringOverlayTestSuite.test("std::string <=> Swift.String") {
   expectEqual(swift7, "���")
 }
 
+StdStringOverlayTestSuite.test("std::u16string <=> Swift.String") {
+  let cxx1 = std.u16string()
+  let swift1 = String(cxx1)
+  expectEqual(swift1, "")
+
+  let cxx2 = std.u16string("something123")
+  expectEqual(cxx2.size(), 12)
+  let swift2 = String(cxx2)
+  expectEqual(swift2, "something123")
+
+  let cxx3: std.u16string = "literal"
+  expectEqual(cxx3.size(), 7)
+
+  let cxx4: std.u16string = "тест"
+  expectEqual(cxx4.size(), 4)
+  let swift4 = String(cxx4)
+  expectEqual(swift4, "тест")
+
+  // Emojis are represented by more than one CWideChar.
+  let cxx5: std.u16string = "emoji_🤖"
+  expectEqual(cxx5.size(), 8)
+  let swift5 = String(cxx5)
+  expectEqual(swift5, "emoji_🤖")
+
+  let cxx6 = std.u16string("xyz\0abc")
+  expectEqual(cxx6.size(), 7)
+  let swift6 = String(cxx6)
+  expectEqual(swift6, "xyz\0abc")
+}
+
 StdStringOverlayTestSuite.test("std::string as Swift.CustomDebugStringConvertible") {
   let cxx1 = std.string()
   expectEqual(cxx1.debugDescription, "std.string()")
