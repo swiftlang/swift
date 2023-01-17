ae1f8248b3e5629b2635f0fa4ac6f81f64debf55
Holly Borla <hborla@apple.com>
2023-01-17 08:32:37 -0800

    [Serialization] Don't serialize macro custom attributes.

--- test/Serialization/Inputs/def_macros.swift
+++ test/Serialization/Inputs/def_macros.swift
@@ -1,5 +1,13 @@
-@expression public macro publicStringify<T>(_ value: T) -> (T, String) = SomeModule.StringifyMacro
+@expression public macro publicStringify<T>(_ value: T) -> (T, String) = #externalMacro(module: "MacroDefinition", type: "StringifyMacro")
 
-@expression macro internalStringify<T>(_ value: T) -> (T, String) = SomeModule.StringifyMacro
+@expression macro internalStringify<T>(_ value: T) -> (T, String) = #externalMacro(module: "MacroDefinition", type: "StringifyMacro")
 
-@attached(accessor) public macro myWrapper: Void = SomeModule.MyWrapperMacro
+@attached(accessor) public macro myWrapper: Void = #externalMacro(module: "MacroDefinition", type: "MyWrapperMacro")
+
+@attached(memberAttributes) public macro wrapAllProperties() = #externalMacro(module: "MacroDefinition", type: "WrapAllProperties")
+
+// Make sure that macro custom attributes are not serialized.
+@wrapAllProperties
+public struct S {
+  public var value: Int
+}
