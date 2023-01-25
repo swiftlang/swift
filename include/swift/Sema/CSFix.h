9b688e9ca6b2080485749978292fe09757e0649c
Pavel Yaskevich <pyaskevich@apple.com>
2023-01-25 13:01:14 -0800

    [CSFix] Move `diagnoseForAmbiguity` to RequirementFix
    The logic is common for all types of fixes that represent
    a requirement failure.

--- include/swift/Sema/CSFix.h
+++ include/swift/Sema/CSFix.h
@@ -612,6 +612,8 @@ public:
   Type lhsType() const { return LHS; }
   Type rhsType() const { return RHS; }
 
+  bool diagnoseForAmbiguity(CommonFixesArray commonFixes) const override;
+
   bool diagnose(const Solution &solution,
                 bool asNote = false) const override = 0;
 };
@@ -634,8 +636,6 @@ public:
   }
 
   bool diagnose(const Solution &solution, bool asNote = false) const override;
-
-  bool diagnoseForAmbiguity(CommonFixesArray commonFixes) const override;
 
   static MissingConformance *forRequirement(ConstraintSystem &cs, Type type,
                                             Type protocolType,
