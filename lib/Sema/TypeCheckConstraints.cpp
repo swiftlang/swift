007399f0d082e60be8c8927265c07c9544ccb6d1
Kavon Farvardin <kfarvardin@apple.com>
2023-01-27 19:11:34 -0800

    Ban explicit casts of move-only types unless its the identity cast
    Since structs and enums only participate in subtyping via protocols,
    move-only types that are structs and enums effectively are only
    subtypes of themselves.

--- lib/Sema/TypeCheckConstraints.cpp
+++ lib/Sema/TypeCheckConstraints.cpp
@@ -1659,6 +1659,27 @@ TypeChecker::typeCheckCheckedCast(Type fromType, Type toType,
        !unwrappedIUO)) {
     return CheckedCastKind::Coercion;
   }
+
+  // Since move-only types currently cannot conform to protocols, nor be a class
+  // type, the subtyping hierarchy is a bit bizarre as of now:
+  //
+  //              noncopyable
+  //           structs and enums
+  //                   |
+  //       +--------- Any
+  //       |           |
+  //   AnyObject    protocol
+  //       |       existentials
+  //       |            |   \
+  //       +---------+  |    +-- structs/enums
+  //                 |  |
+  //                classes
+  //           (and their subtyping)
+  //
+  //
+  // Thus, right now, a move-only type is only a subtype of itself.
+  if (fromType->isPureMoveOnly())
+    return CheckedCastKind::Unresolved;
   
   // Check for a bridging conversion.
   // Anything bridges to AnyObject.
