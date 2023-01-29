0eca9f7388a21aced54e26b67e40e93796e03a55
Alex Lorenz <arphaman@gmail.com>
2023-01-28 16:39:08 -0800

    update import-foundation-with-indexing.swift to check that we actually indexed Foundation

--- test/Interop/Cxx/stdlib/import-foundation-with-indexing.swift
+++ test/Interop/Cxx/stdlib/import-foundation-with-indexing.swift
@@ -1,7 +1,11 @@
+// RUN: %empty-directory(%t)
 // RUN: %target-swift-frontend %s -c -index-system-modules -index-store-path %t -enable-experimental-cxx-interop
-//
+// RUN: ls %t/v5/units | %FileCheck %s
+
 // REQUIRES: OS=macosx
 
 import Foundation
 
 func test(d: Date) {}
+
+// CHECK: Foundation-{{.*}}.pcm
