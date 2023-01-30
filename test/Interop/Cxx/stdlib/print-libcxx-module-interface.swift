da30593e93ded6d04d3b7333ecd16cff25f3ed3d
Alex Lorenz <arphaman@gmail.com>
2023-01-30 09:19:55 -0800

    [interop] test - update to use CxxStdlib module name for printing libc++ interface

--- test/Interop/Cxx/stdlib/print-libcxx-module-interface.swift
+++ test/Interop/Cxx/stdlib/print-libcxx-module-interface.swift
@@ -1,4 +1,4 @@
-// RUN: %target-swift-ide-test -print-module -module-to-print=std -I %libcxx-in-sdk-path -source-filename=x -enable-experimental-cxx-interop -enable-objc-interop -module-print-submodules | %FileCheck %s
+// RUN: %target-swift-ide-test -print-module -module-to-print=CxxStdlib -source-filename=x -enable-experimental-cxx-interop -enable-objc-interop -module-print-submodules | %FileCheck %s
 
 // REQUIRES: OS=macosx
 // REQUIRES: libcxx-in-sdk
