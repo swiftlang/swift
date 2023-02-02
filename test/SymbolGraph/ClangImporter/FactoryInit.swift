d208dd99937549b146bc0956dc0feaf76a01a9e0
Victoria Mitchell <victoria_m@apple.com>
2023-02-02 14:53:03 -0700

    don't print the "not inherited" mark in SGF declarations
    rdar://104917487

---
+++ test/SymbolGraph/ClangImporter/FactoryInit.swift
@@ -0,0 +1,6 @@
+// RUN: %empty-directory(%t)
+// RUN: %target-swift-symbolgraph-extract -sdk %clang-importer-sdk -module-name FactoryInit -F %S/Inputs/FactoryInit -output-dir %t -pretty-print -v
+
+// RUN: %FileCheck %s --input-file %t/FactoryInit.symbols.json
+
+// CHECK-NOT: not inherited
