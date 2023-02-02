a98f7d91efb67de822e07a35ad2ba5e121b6d65a
Steven Wu <stevenwu@apple.com>
2023-02-02 10:54:15 -0800

    Virtualize depscan output to allow deterministic check
    Batch mode depscan also gets diagnostics for output errors.

--- lib/DependencyScan/ScanDependencies.cpp
+++ lib/DependencyScan/ScanDependencies.cpp
@@ -20,6 +20,7 @@
 #include "swift/AST/DiagnosticsFrontend.h"
 #include "swift/AST/DiagnosticsSema.h"
 #include "swift/AST/DiagnosticsDriver.h"
+#include "swift/AST/FileSystem.h"
 #include "swift/AST/Module.h"
 #include "swift/AST/ModuleDependencies.h"
 #include "swift/AST/ModuleLoader.h"
@@ -45,6 +46,7 @@
 #include "llvm/Support/StringSaver.h"
 #include "llvm/Support/YAMLParser.h"
 #include "llvm/Support/YAMLTraits.h"
+#include "llvm/Support/raw_ostream.h"
 #include <set>
 #include <string>
 #include <sstream>
@@ -1393,15 +1395,6 @@ bool swift::dependencies::scanDependencies(CompilerInstance &instance) {
   ASTContext &Context = instance.getASTContext();
   const FrontendOptions &opts = instance.getInvocation().getFrontendOptions();
   std::string path = opts.InputsAndOutputs.getSingleOutputFilename();
-  std::error_code EC;
-  llvm::raw_fd_ostream out(path, EC, llvm::sys::fs::OF_None);
-  if (out.has_error() || EC) {
-    Context.Diags.diagnose(SourceLoc(), diag::error_opening_output, path,
-                           EC.message());
-    out.clear_error();
-    return true;
-  }
-
   // `-scan-dependencies` invocations use a single new instance
   // of a module cache
   SwiftDependencyScanningService service;
@@ -1427,8 +1420,13 @@ bool swift::dependencies::scanDependencies(CompilerInstance &instance) {
     return true;
   auto dependencies = std::move(*dependenciesOrErr);
 
-  // Write out the JSON description.
-  writeJSON(out, dependencies);
+  if (withOutputFile(Context.Diags, instance.getOutputBackend(), path,
+                     [&](llvm::raw_pwrite_stream &os) {
+                       writeJSON(os, dependencies);
+                       return false;
+                     }))
+    return true;
+
   // This process succeeds regardless of whether any errors occurred.
   // FIXME: We shouldn't need this, but it's masking bugs in our scanning
   // logic where we don't create a fresh context when scanning Swift interfaces
@@ -1441,20 +1439,12 @@ bool swift::dependencies::prescanDependencies(CompilerInstance &instance) {
   ASTContext &Context = instance.getASTContext();
   const FrontendOptions &opts = instance.getInvocation().getFrontendOptions();
   std::string path = opts.InputsAndOutputs.getSingleOutputFilename();
-  std::error_code EC;
-  llvm::raw_fd_ostream out(path, EC, llvm::sys::fs::OF_None);
   // `-scan-dependencies` invocations use a single new instance
   // of a module cache
   SwiftDependencyScanningService singleUseService;
   ModuleDependenciesCache cache(singleUseService,
                                 instance.getMainModule()->getNameStr().str(),
                                 instance.getInvocation().getModuleScanningHash());
-  if (out.has_error() || EC) {
-    Context.Diags.diagnose(SourceLoc(), diag::error_opening_output, path,
-                           EC.message());
-    out.clear_error();
-    return true;
-  }
 
   // Execute import prescan, and write JSON output to the output stream
   auto importSetOrErr = performModulePrescan(instance);
@@ -1463,7 +1453,13 @@ bool swift::dependencies::prescanDependencies(CompilerInstance &instance) {
   auto importSet = std::move(*importSetOrErr);
 
   // Serialize and output main module dependencies only and exit.
-  writePrescanJSON(out, importSet);
+  if (withOutputFile(Context.Diags, instance.getOutputBackend(), path,
+                     [&](llvm::raw_pwrite_stream &os) {
+                       writePrescanJSON(os, importSet);
+                       return false;
+                     }))
+    return true;
+
   // This process succeeds regardless of whether any errors occurred.
   // FIXME: We shouldn't need this, but it's masking bugs in our scanning
   // logic where we don't create a fresh context when scanning Swift interfaces
@@ -1499,12 +1495,16 @@ bool swift::dependencies::batchScanDependencies(
   auto iresults = batchScanResults.cbegin();
   for (; ientries != batchInput->end() and iresults != batchScanResults.end();
        ++ientries, ++iresults) {
-    std::error_code EC;
-    llvm::raw_fd_ostream out((*ientries).outputPath, EC, llvm::sys::fs::OF_None);
     if ((*iresults).getError())
       return true;
 
-    writeJSON(out, **iresults);
+    if (withOutputFile(instance.getASTContext().Diags,
+                       instance.getOutputBackend(), (*ientries).outputPath,
+                       [&](llvm::raw_pwrite_stream &os) {
+                         writeJSON(os, **iresults);
+                         return false;
+                       }))
+      return true;
   }
   return false;
 }
@@ -1534,12 +1534,16 @@ bool swift::dependencies::batchPrescanDependencies(
   for (;
        ientries != batchInput->end() and iresults != batchPrescanResults.end();
        ++ientries, ++iresults) {
-    std::error_code EC;
-    llvm::raw_fd_ostream out((*ientries).outputPath, EC, llvm::sys::fs::OF_None);
     if ((*iresults).getError())
       return true;
 
-    writePrescanJSON(out, **iresults);
+    if (withOutputFile(instance.getASTContext().Diags,
+                       instance.getOutputBackend(), (*ientries).outputPath,
+                       [&](llvm::raw_pwrite_stream &os) {
+                         writePrescanJSON(os, **iresults);
+                         return false;
+                       }))
+      return true;
   }
   return false;
 }
--- test/Frontend/output_determinism_check.swift
+++ test/Frontend/output_determinism_check.swift
@@ -8,6 +8,7 @@
 // RUN: %target-swift-frontend -module-name test -c -o %t/test.o -primary-file %s -enable-swift-deterministic-check -always-compile-output-files 2>&1 | %FileCheck %s --check-prefix=OBJECT_OUTPUT
 
 /// Explicit module build. Check building swiftmodule from interface file.
+// RUN: %target-swift-frontend -scan-dependencies -module-name test -o %t/test.json %s -enable-swift-deterministic-check 2>&1 | %FileCheck %s --check-prefix=DEPSCAN_OUTPUT
 /// TODO: Implicit module build use a different compiler instance so it doesn't support checking yet.
 // RUN: %target-swift-frontend -typecheck -emit-module-interface-path %t/test.swiftinterface %s -O -enable-swift-deterministic-check 2>&1 | %FileCheck %s --check-prefix=INTERFACE_OUTPUT
 /// Hit cache and not emit the second time.
@@ -20,6 +21,7 @@
 // SIB_OUTPUT: remark: produced matching output file '{{.*}}{{/|\\}}test.sib'
 // OBJECT_OUTPUT: remark: produced matching output file '{{.*}}{{/|\\}}test.o'
 // OBJECT_MISMATCH: error: output file '{{.*}}{{/|\\}}test.o' is missing from second compilation for deterministic check
+// DEPSCAN_OUTPUT: remark: produced matching output file '{{.*}}{{/|\\}}test.json'
 // INTERFACE_OUTPUT: remark: produced matching output file '{{.*}}{{/|\\}}test.swiftinterface'
 // MODULE_MISMATCH: error: output file '{{.*}}{{/|\\}}test.swiftmodule' is missing from second compilation for deterministic check
 
