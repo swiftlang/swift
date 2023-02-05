f2b513415f14edf6c7383aa706a033d78218b864
Pavel Yaskevich <pyaskevich@apple.com>
2023-02-04 18:48:19 -0800

    [CSGen] Remove ConstraintOptimizer and all favoring logic

--- include/swift/Sema/ConstraintSystem.h
+++ include/swift/Sema/ConstraintSystem.h
@@ -5403,11 +5403,6 @@ public:
   bool applySolutionToBody(TapExpr *tapExpr,
                            SyntacticElementTargetRewriter &rewriter);
 
-  /// Reorder the disjunctive clauses for a given expression to
-  /// increase the likelihood that a favored constraint will be successfully
-  /// resolved before any others.
-  void optimizeConstraints(Expr *e);
-
   void startExpressionTimer(ExpressionTimer::AnchorType anchor);
 
   /// Determine if we've already explored too many paths in an
