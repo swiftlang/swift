dfe8af0ec3f4dd0fd4ed0380e5b44bc71aec97e3
Pavel Yaskevich <pyaskevich@apple.com>
2023-02-07 15:39:44 -0800

    [CSGen] Handle recursive use of variable declarations
    It's possible for out-of-scope type variable to be the type of
    declaration if such declaration is recursively referenced in
    the body of a closure located in its initializer expression.
    In such cases type of the variable declaration cannot be connected
    to the closure because its not known in advance (determined by the
    initializer itself).
    
    Resolves: https://github.com/apple/swift/issues/63455
    (cherry picked from commit 83dd93ad2e902972eee58a3344f69a554e98e661)

--- lib/Sema/CSGen.cpp
+++ lib/Sema/CSGen.cpp
@@ -1351,6 +1351,13 @@ namespace {
     Type visitDeclRefExpr(DeclRefExpr *E) {
       auto locator = CS.getConstraintLocator(E);
 
+      auto invalidateReference = [&]() -> Type {
+        auto *hole = CS.createTypeVariable(locator, TVO_CanBindToHole);
+        (void)CS.recordFix(AllowRefToInvalidDecl::create(CS, locator));
+        CS.setType(E, hole);
+        return hole;
+      };
+
       Type knownType;
       if (auto *VD = dyn_cast<VarDecl>(E->getDecl())) {
         knownType = CS.getTypeIfAvailable(VD);
@@ -1358,13 +1365,19 @@ namespace {
           knownType = CS.getVarType(VD);
 
         if (knownType) {
+          // An out-of-scope type variable could be a type of a declaration
+          // only in diagnostic mode when invalid variable declaration is
+          // recursively referenced inside of a multi-statement closure
+          // located somewhere within its initializer e.g.:
+          // `let x = [<call>] { ... print(x) }`
+          if (auto *typeVar = knownType->getAs<TypeVariableType>()) {
+            if (!CS.isActiveTypeVariable(typeVar))
+              return invalidateReference();
+          }
+
           // If the known type has an error, bail out.
           if (knownType->hasError()) {
-            auto *hole = CS.createTypeVariable(locator, TVO_CanBindToHole);
-            (void)CS.recordFix(AllowRefToInvalidDecl::create(CS, locator));
-            if (!CS.hasType(E))
-              CS.setType(E, hole);
-            return hole;
+            return invalidateReference();
           }
 
           if (!knownType->hasPlaceholder()) {
@@ -1381,10 +1394,7 @@ namespace {
       // (in getTypeOfReference) so we can match non-error param types.
       if (!knownType && E->getDecl()->isInvalid() &&
           !CS.isForCodeCompletion()) {
-        auto *hole = CS.createTypeVariable(locator, TVO_CanBindToHole);
-        (void)CS.recordFix(AllowRefToInvalidDecl::create(CS, locator));
-        CS.setType(E, hole);
-        return hole;
+        return invalidateReference();
       }
 
       // Create an overload choice referencing this declaration and immediately
