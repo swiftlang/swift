83dd93ad2e902972eee58a3344f69a554e98e661
Pavel Yaskevich <pyaskevich@apple.com>
2023-02-07 15:39:44 -0800

    [CSGen] Handle recursive use of variable declarations
    It's possible for out-of-scope type variable to be the type of
    declaration if such declaration is recursively referenced in
    the body of a closure located in its initializer expression.
    In such cases type of the variable declaration cannot be connected
    to the closure because its not known in advance (determined by the
    initializer itself).
    
    Resolves: https://github.com/apple/swift/issues/63455

--- lib/Sema/CSSyntacticElement.cpp
+++ lib/Sema/CSSyntacticElement.cpp
@@ -74,7 +74,7 @@ public:
     if (auto *DRE = dyn_cast<DeclRefExpr>(expr)) {
       auto *decl = DRE->getDecl();
 
-      if (auto type = CS.getTypeIfAvailable(DRE->getDecl())) {
+      if (auto type = CS.getTypeIfAvailable(decl)) {
         auto &ctx = CS.getASTContext();
         // If this is not one of the closure parameters which
         // is inferrable from the body, let's replace type
