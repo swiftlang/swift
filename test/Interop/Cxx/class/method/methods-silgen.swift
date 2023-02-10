c00c76214636afbc0ab91c9db65bc17823cd5c75
zoecarver <z.zoelec2@gmail.com>
2023-02-10 09:27:11 -0800

    [tests] Fix SILGen tests to have Windows mangling

--- test/Interop/Cxx/class/method/methods-silgen.swift
+++ test/Interop/Cxx/class/method/methods-silgen.swift
@@ -3,10 +3,10 @@
 import Methods
 
 // clang name: ReferenceParams::ReferenceParams
-// CHECK: sil [clang ReferenceParams.init] @_ZN15ReferenceParamsC1ERKiS1_ : $@convention(c) (@in_guaranteed Int32, @in_guaranteed Int32) -> @out ReferenceParams
+// CHECK: sil [clang ReferenceParams.init] @{{_ZN15ReferenceParamsC1ERKiS1_|\?\?0ReferenceParams@@QEAA@AEBH0@Z}} : $@convention(c) (@in_guaranteed Int32, @in_guaranteed Int32) -> @out ReferenceParams
 
 // clang name: ReferenceParams::staticMethod
-// CHECK: sil [clang ReferenceParams.staticMethod] @_ZN15ReferenceParams12staticMethodERKiS1_ : $@convention(c) (@in_guaranteed Int32, @in_guaranteed Int32) -> ()
+// CHECK: sil [clang ReferenceParams.staticMethod] @{{_ZN15ReferenceParams12staticMethodERKiS1_|\?staticMethod@ReferenceParams@@SAXAEBH0@Z}} : $@convention(c) (@in_guaranteed Int32, @in_guaranteed Int32) -> ()
 
 public func use() {
   let a = CInt(42)
