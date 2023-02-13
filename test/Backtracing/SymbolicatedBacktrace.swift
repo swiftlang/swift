186d4865df67774e230094e587d3530a0f81f600
Alastair Houghton <ahoughton@apple.com>
2023-02-13 15:35:57 +0000

    [Backtracing] Add `swift-backtrace` to the build.
    This is Swift's external backtracer.  It's written in Swift, and it's
    responsible for generating nice backtraces when Swift programs crash.
    It makes use of the new `_Backtracing` library, which is also (mostly,
    aside from some assembly language) implemented in Swift.
    
    rdar://103442000

---
+++ test/Backtracing/SymbolicatedBacktrace.swift
@@ -0,0 +1,50 @@
+// RUN: %empty-directory(%t)
+// RUN: %target-build-swift %s -parse-as-library -g -Onone -o %t/SymbolicatedBacktrace
+// RUN: %target-codesign %t/SymbolicatedBacktrace
+// RUN: %target-run %t/SymbolicatedBacktrace | %FileCheck %s
+
+// REQUIRES: executable_test
+// REQUIRES: OS=macosx || OS=ios || OS=watchOS || OS=tvOS
+
+import _Backtracing
+
+func kablam() {
+  kerpow()
+}
+
+func kerpow() {
+  whap()
+}
+
+func whap() {
+  zonk()
+}
+
+func zonk() {
+  splat()
+}
+
+func splat() {
+  pow()
+}
+
+func pow() {
+  let backtrace = try! Backtrace.capture().symbolicated()!
+
+  // CHECK:      0{{[ \t]+}}0x{{[0-9a-f]+}} [ra] [0] SymbolicatedBacktrace pow()
+  // CHECK:      1{{[ \t]+}}0x{{[0-9a-f]+}} [ra] [0] SymbolicatedBacktrace splat()
+  // CHECK:      2{{[ \t]+}}0x{{[0-9a-f]+}} [ra] [0] SymbolicatedBacktrace zonk()
+  // CHECK:      3{{[ \t]+}}0x{{[0-9a-f]+}} [ra] [0] SymbolicatedBacktrace whap()
+  // CHECK:      4{{[ \t]+}}0x{{[0-9a-f]+}} [ra] [0] SymbolicatedBacktrace kerpow()
+  // CHECK:      5{{[ \t]+}}0x{{[0-9a-f]+}} [ra] [0] SymbolicatedBacktrace kablam()
+  // CHECK:      6{{[ \t]+}}0x{{[0-9a-f]+}} [ra] [0] SymbolicatedBacktrace static SymbolicatedBacktrace.main()
+
+  print(backtrace)
+}
+
+@main
+struct SymbolicatedBacktrace {
+  static func main() {
+    kablam()
+  }
+}
