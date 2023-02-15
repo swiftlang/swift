989b69fc1def23e2ac902aeee0826d6f2292af0e
Alex Hoppen <ahoppen@apple.com>
2023-02-15 15:02:05 +0100

    [IDE] Add debug output if we use fallback type checking for code completion

--- lib/IDE/CodeCompletion.cpp
+++ lib/IDE/CodeCompletion.cpp
@@ -1441,8 +1441,12 @@ bool CodeCompletionCallbacksImpl::trySolverCompletion(bool MaybeFuncBody) {
     // switch case where there control expression is invalid). Having normal
     // typechecking still resolve even these cases would be beneficial for
     // tooling in general though.
-    if (!Lookup.gotCallback())
+    if (!Lookup.gotCallback()) {
+      if (Context.TypeCheckerOpts.DebugConstraintSolver) {
+        llvm::errs() << "--- Fallback typecheck for code completion ---\n";
+      }
       Lookup.fallbackTypeCheck(CurDeclContext);
+    }
   };
 
   switch (Kind) {
