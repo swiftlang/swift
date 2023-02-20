fd749888eb0006ec5959cdb09d6e0bb44b1dd8e4
Konrad `ktoso` Malawski <konrad_malawski@apple.com>
2023-02-20 20:05:08 +0900

    [Distributed] correct take semantics for synthesized ID assignments
    cleanup: no need to dump input always

--- test/Distributed/distributed_actor_accessor_section_elf.swift
+++ test/Distributed/distributed_actor_accessor_section_elf.swift
@@ -1,6 +1,6 @@
 // RUN: %empty-directory(%t)
 // RUN: %target-swift-frontend-emit-module -emit-module-path %t/FakeDistributedActorSystems.swiftmodule -module-name FakeDistributedActorSystems -disable-availability-checking %S/Inputs/FakeDistributedActorSystems.swift
-// RUN: %target-swift-frontend -emit-irgen -module-name distributed_actor_accessors -disable-availability-checking -I %t 2>&1 %s | %IRGenFileCheck %s --dump-input=always
+// RUN: %target-swift-frontend -emit-irgen -module-name distributed_actor_accessors -disable-availability-checking -I %t 2>&1 %s | %IRGenFileCheck %s
 
 // UNSUPPORTED: back_deploy_concurrency
 // REQUIRES: concurrency
