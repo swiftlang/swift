57b8712fd32c62aa75f27317b3705a129cc6eb17
Michael Gottesman <mgottesman@apple.com>
2023-02-21 17:04:26 -0800

    [borrow-operator] Make it an error to try to _borrow a let. One can only _borrow a var.
    I also changed the error message to explicitly say one can only apply it to var
    bindings.

--- test/Sema/borrow_expr.swift
+++ test/Sema/borrow_expr.swift
@@ -1,87 +1,114 @@
 // RUN: %target-typecheck-verify-swift  -disable-availability-checking -enable-experimental-move-only
 
 class Klass {
-    var k: Klass? = nil
+  var k: Klass? = nil
 }
 
 func useString(_ x: String) {}
 
 var global: String = "5"
 func testGlobal() {
-    useString(_borrow global)
+  useString(_borrow global)
 }
 
 func testLet() {
-    let t = String()
-    useString(_borrow t)
+  let t = String()
+  useString(_borrow t) // expected-error {{'borrow' can only be applied to var bindings}}
 }
 
 func testVar() {
-    var t = String()
-    t = String()
-    useString(_borrow t)
+  var t = String()
+  t = String()
+  useString(_borrow t)
 }
 
 struct S {
-    var k = Klass()
+  var k = Klass()
 
-    func test() {}
+  func test() {}
 
-    func test2() {
-        (_borrow self).test()
-    }
+  func test2() {
+    (_borrow self).test() // expected-error {{'borrow' can only be applied to var bindings}}
+  }
+
+  mutating func test3() {
+    (_borrow self).test()
+  }
 }
 
 func useApply() {
-    var s = S()
-    s = S()
-    (_borrow s).test()
+  var s = S()
+  s = S()
+  (_borrow s).test()
 }
 
 func testExprFailureLet() {
-    let t = 5
-    // Next line is parsed as move(t) + t
-    let _ = _borrow t + t
-    // Next line is parsed as move(t+t)
-    let _ = _borrow (t+t) // expected-error {{'borrow' can only be applied to lvalues}}
+  let t = 5
+  // Next line is parsed as move(t) + t
+  let _ = _borrow t + t // expected-error {{'borrow' can only be applied to var bindings}}
+  // Next line is parsed as move(t+t)
+  let _ = _borrow (t+t) // expected-error {{'borrow' can only be applied to var bindings}}
 }
 
 func testExprFailureVar() {
-    var t = 5
-    t = 5
-    // Next line is parsed as move(t) + t
-    let _ = _borrow t + t
-    // Next line is parsed as move(t+t)
-    let _ = _borrow (t+t) // expected-error {{'borrow' can only be applied to lvalues}}
+  var t = 5
+  t = 5
+  // Next line is parsed as move(t) + t
+  let _ = _borrow t + t
+  // Next line is parsed as move(t+t)
+  let _ = _borrow (t+t) // expected-error {{'borrow' can only be applied to var bindings}}
 }
 
 func letAddressOnly<T>(_ v: T) {
-    let t = v
-    let _ = _borrow t
+  let t = v
+  let _ = _borrow t // expected-error {{'borrow' can only be applied to var bindings}}
 }
 
 struct StructWithField {
-    var k: Klass? = nil
+  var k: Klass = Klass()
+
+  var computedK : Klass {
+    return k
+  }
+
+  var computedK2 : Klass {
+    _read {
+      yield k
+    }
+  }
 }
 
 func testLetStructAccessField() {
-    let t = StructWithField()
-    let _ = _borrow t.k
+  let t = StructWithField()
+  let _ = _borrow t.k // expected-error {{'borrow' can only be applied to var bindings}}
+  let _ = _borrow t.computedK // expected-error {{'borrow' can only be applied to var bindings}}
+  let _ = _borrow t.computedK2 // expected-error {{'borrow' can only be applied to var bindings}}
+  let _ = _borrow (_borrow t).computedK
+  // expected-error @-1:11 {{'borrow' can only be applied to var bindings}}
+  // expected-error @-2:20 {{'borrow' can only be applied to var bindings}}
+  let _ = _borrow (_borrow t).computedK2
+  // expected-error @-1:11 {{'borrow' can only be applied to var bindings}}
+  // expected-error @-2:20 {{'borrow' can only be applied to var bindings}}
 }
 
 func testVarStructAccessField() {
-    var t = StructWithField()
-    t = StructWithField()
-    let _ = _borrow t.k
+  var t = StructWithField()
+  t = StructWithField()
+  let _ = _borrow t.k
+  let _ = _borrow t.computedK // expected-error {{'borrow' can only be applied to var bindings}}
+  let _ = _borrow t.computedK2 // expected-error {{'borrow' can only be applied to var bindings}}
+  let _ = _borrow (_borrow t).computedK // expected-error {{'borrow' can only be applied to var bindings}}
+  let _ = _borrow (_borrow t).computedK2 // expected-error {{'borrow' can only be applied to var bindings}}
+
 }
 
 func testLetClassAccessField() {
-    let t = Klass()
-    let _ = _borrow t.k
+  let t = Klass()
+  let _ = _borrow t.k // expected-error {{'borrow' can only be applied to var bindings}}
 }
 
 func testVarClassAccessField() {
-    var t = Klass()
-    t = Klass()
-    let _ = _borrow t.k // expected-error {{'borrow' can only be applied to lvalues}}
+  var t = Klass()
+  t = Klass()
+  let _ = _borrow t.k // expected-error {{'borrow' can only be applied to var bindings}}
 }
