5b6b8f495024952d29e8f3510dbca7985d6c48c6
Philippe Hausler <phausler@apple.com>
2023-03-03 09:27:01 -0800

    [Observation] Add names for attached member names for Observable macro

--- stdlib/public/Observation/Sources/Observation/Macros.swift
+++ stdlib/public/Observation/Sources/Observation/Macros.swift
@@ -12,7 +12,7 @@
 #if SWIFT_OBSERVATION_MACROS
 #if compiler(>=5.8) && hasAttribute(attached)
 @available(SwiftStdlib 5.9, *)
-@attached(member)
+@attached(member, names: named(_registrar), named(transactions), named(changes), named(_Storage), named(_storage))
 @attached(memberAttribute)
 @attached(conformance)
 public macro Observable() = 
