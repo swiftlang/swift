b09071ed80bb6d6d5b8d8b4f67c2c6ad21edb108
Saleem Abdulrasool <compnerd@compnerd.org>
2023-03-04 09:09:04 -0800

    Update class-fingerprint.swift

--- test/Incremental/Fingerprints/class-fingerprint.swift
+++ test/Incremental/Fingerprints/class-fingerprint.swift
@@ -10,6 +10,7 @@
 // Seeing weird failure on CI, so set the mod times
 // RUN: touch -t 200101010101 %t/*.swift
 
+// RUN: echo "running initial swiftc"
 // RUN: cd %t && %swiftc_driver-stdlib-target -enable-batch-mode -j2 -incremental -driver-show-incremental main.swift definesAB.swift usesA.swift usesB.swift -module-name main -output-file-map ofm.json >&output3
 
 // only-run-for-debugging: cp %t/usesB.swiftdeps %t/usesB3.swiftdeps
@@ -17,6 +18,7 @@
 
 // Change one type, only uses of that type get recompiled
 
+// RUN: echo "copying stuffs around ..."
 // RUN: cp %t/definesAB{-after,}.swift
 
 // Seeing weird failure on CI, so ensure that definesAB.swift is newer
@@ -24,10 +26,12 @@
 // RUN: touch -t 200101010101 %t/*.swift
 // RUN: touch -t 200301010101 %t/definesAB.swift
 
+// RUN: echo "running swiftc"
 // RUN: cd %t && %swiftc_driver-stdlib-target -enable-batch-mode -j2 -incremental -driver-show-incremental main.swift definesAB.swift usesA.swift usesB.swift -module-name main -output-file-map ofm.json >&output4
 
 // only-run-for-debugging: cp %t/usesB.swiftdeps %t/usesB4.swiftdeps
 
+// RUN: echo "displaying output"
 // RUN: cat %t/output4
 // RUN: %FileCheck -check-prefix=CHECK-MAINAB-RECOMPILED %s < %t/output4
 
