dabf0cee1762de9bca20101475bbd9f2ed2c0970
Allan Shortlidge <shortlidge@apple.com>
2023-03-14 09:52:31 -0700

    IRGen: Weakly link symbols for unavailable declarations.
    When computing linkage, the compiler would treat unavailable declarations as if
    they were "always available" when they lack an `introduced:` version:
    
    ```
    // Library
    @available(macOS, unavailable)
    public func foo() {
      // …
    }
    
    // Client
    import Library
    
    @available(macOS, unavailable)
    func bar() {
      // Even though foo() and bar() are unavalable on macOS the compiler still
      // strongly links foo().
      foo()
    }
    ```
    
    This created an unnecessary dependency between libraries and their clients and
    also can interfere with back deployment, since unavailable declarations may not
    be present in a library on all OS versions. Developers could work around these
    issues by conditionally compiling the code that references an unavailable
    declaration, but they shouldn't have to given that unavailable code is meant to
    be provably unreachable at runtime. Additionally, it could improve library code
    size if we allowed the compiler to strip unavailable declarations from a binary
    completely.
    
    Resolves rdar://106673713

--- test/IRGen/Inputs/weak_import_availability_helper.swift
+++ test/IRGen/Inputs/weak_import_availability_helper.swift
@@ -1,8 +1,17 @@
 @available(macOS 10.50, *)
 public func conditionallyAvailableFunction() {}
 
+@available(macOS, unavailable)
+public func unavailableFunction() {}
+
 @available(macOS 10.50, *)
 public var conditionallyAvailableGlobal: Int {
+  get {return 0}
+  set {}
+}
+
+@available(macOS, unavailable)
+public var unavailableGlobal: Int {
   get {return 0}
   set {}
 }
@@ -12,12 +21,27 @@ public struct ConditionallyAvailableStruct {
   public func conditionallyAvailableMethod() {}
 }
 
+extension ConditionallyAvailableStruct {
+  public struct NestedStruct {}
+}
+
+@available(macOS, unavailable)
+public struct UnvailableStruct {
+  public func unavailableMethod() {}
+}
+
 public protocol AlwaysAvailableProtocol {}
 
 public struct AlwaysAvailableStruct {}
 
 @available(macOS 10.50, *)
 extension AlwaysAvailableStruct : AlwaysAvailableProtocol {}
+
+@available(macOS, unavailable)
+public protocol UnavailableProtocol {}
+
+@available(macOS, unavailable)
+extension AlwaysAvailableStruct : UnavailableProtocol {}
 
 public enum AlwaysAvailableEnum {
   case alwaysAvailableCase
