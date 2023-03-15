d0961140f281d25c7dd5660a11aaf72c3eeddec0
Victoria Mitchell <victoria_m@apple.com>
2023-03-15 13:47:47 -0600

    review: remove check for cursor info

--- test/SourceKit/CursorInfo/doc_attr.swift
+++ test/SourceKit/CursorInfo/doc_attr.swift
@@ -6,7 +6,4 @@ enum E
 // The @_documentation attribute caused sourcekit-lsp to crash
 // cf. https://github.com/apple/swift/issues/64309
 
-// RUN: %sourcekitd-test -req=cursor -pos=2:6 %s -- %s | %FileCheck %s
-
-// CHECK: <Declaration>@_documentation(visibility: public) enum E</Declaration>
-// CHECK: <decl.enum><syntaxtype.attribute.builtin><syntaxtype.attribute.name>@_documentation</syntaxtype.attribute.name>(visibility: public)</syntaxtype.attribute.builtin> <syntaxtype.keyword>enum</syntaxtype.keyword> <decl.name>E</decl.name></decl.enum>
+// RUN: %sourcekitd-test -req=cursor -pos=2:6 %s -- %s
