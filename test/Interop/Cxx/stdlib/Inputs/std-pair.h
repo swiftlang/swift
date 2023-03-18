861d99fa25427b8c440eb81eb2b95277d249cbe8
Alex Lorenz <arphaman@gmail.com>
2023-03-17 17:01:56 -0700

    [interop] expand std.pair tests after non-trivial record fix

--- test/Interop/Cxx/stdlib/Inputs/std-pair.h
+++ test/Interop/Cxx/stdlib/Inputs/std-pair.h
@@ -4,8 +4,22 @@
 
 using PairInts = std::pair<int, int>;
 
-// FIXME: return pair by value, but it causes IRGen crash atm.
-inline const PairInts &getIntPair() {
-    static PairInts value = { -5, 12 };
+inline const PairInts &getIntPairPointer() {
+    static PairInts value = { 4, 9 };
     return value;
+}
+
+inline PairInts getIntPair() {
+    return { -5, 12 };
+}
+
+struct StructInPair {
+    int x;
+    int y;
+};
+
+using PairStructInt = std::pair<StructInPair, int>;
+
+inline PairStructInt getPairStructInt(int x) {
+    return { { x * 2, -x}, x };
 }
