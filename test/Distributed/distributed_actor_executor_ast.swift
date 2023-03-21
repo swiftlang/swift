acff77292c51e471c9b3cac81bce514ce6622700
Konrad `ktoso` Malawski <konrad_malawski@apple.com>
2023-03-21 16:27:50 +0900

    adjust ast test for new contidion

--- test/Distributed/distributed_actor_executor_ast.swift
+++ test/Distributed/distributed_actor_executor_ast.swift
@@ -23,12 +23,26 @@ distributed actor DefaultWorker {
   // empty on purpose, default executor
 }
 
+// Check DefaultWorker, the DefaultActor version of the synthesis:
 // CHECK:  (class_decl range=[{{.*}}] "DefaultWorker" interface type='DefaultWorker.Type' access=internal non-resilient actor
+// The unowned executor property:
 // CHECK:    (var_decl implicit "unownedExecutor" type='Optional<UnownedSerialExecutor>' interface type='Optional<UnownedSerialExecutor>' access=internal final readImpl=getter immutable
 // CHECK:     (accessor_decl implicit 'anonname={{.*}}' interface type='(DefaultWorker) -> () -> Optional<UnownedSerialExecutor>' access=internal final get_for=unownedExecutor
 // CHECK:       (parameter "self" type='DefaultWorker' interface type='DefaultWorker')
 // CHECK:       (parameter_list)
 // CHECK:       (brace_stmt implicit
+// We guard the rest of the body; we only return a default executor if the actor is local:
+// CHECK:       (guard_stmt implicit
+// CHECK:         (call_expr implicit type='Bool' nothrow
+// CHECK:           (declref_expr implicit type='@_NO_EXTINFO (AnyObject) -> Bool' decl=Distributed.(file).__isLocalActor function_ref=unapplied)
+// CHECK:           (argument_list implicit
+// CHECK:             (argument
+// CHECK:               (erasure_expr implicit type='AnyObject'
+// CHECK:                 (declref_expr implicit type='DefaultWorker' decl=main.(file).DefaultWorker.<anonymous>.self function_ref=unapplied)))))
+// CHECK:           (brace_stmt implicit
+// CHECK:             (return_stmt implicit
+// CHECK:               (nil_literal_expr implicit type='Optional<UnownedSerialExecutor>' initializer=**NULL**))))
+// If the actor is not local, we return a default executor for it, same as normal actors:
 // CHECK:         (return_stmt implicit
 // CHECK:           (inject_into_optional implicit type='Optional<UnownedSerialExecutor>'
 // CHECK:             (call_expr implicit type='UnownedSerialExecutor' nothrow
