620462ddf60ddeafb41a834091a50fa72ceac683
Allan Shortlidge <shortlidge@apple.com>
2023-03-21 12:33:56 -0700

    Sema: Don't diagnose declarations more available than unavailable container.
    It turns out that we must allow declarations with `introduced:` availability
    nested inside of other declarations that are `unavailable` in order to
    influence weak linking. Stop diagnosing declarations as being more available
    than their unavailable containers and revert some changes to availability
    inference that were designed to avoid creating these nestings but caused
    regressions for declarations marked `@_spi_available.`
    
    Reverts parts of https://github.com/apple/swift/pull/64310,
    https://github.com/apple/swift/pull/64015, and
    https://github.com/apple/swift/pull/62900.

--- test/ModuleInterface/actor_availability.swift
+++ test/ModuleInterface/actor_availability.swift
@@ -1,7 +1,9 @@
 // RUN: %empty-directory(%t)
-// RUN: %target-swift-emit-module-interface(%t/Library.swiftinterface) %s -module-name Library -target %target-swift-abi-5.3-triple
+// RUN: %target-swift-emit-module-interfaces(%t/Library.swiftinterface, %t/Library.private.swiftinterface) %s -module-name Library -target %target-swift-abi-5.3-triple
 // RUN: %target-swift-typecheck-module-from-interface(%t/Library.swiftinterface) -module-name Library
-// RUN: %FileCheck %s < %t/Library.swiftinterface
+// RUN: %target-swift-typecheck-module-from-interface(%t/Library.private.swiftinterface) -module-name Library
+// RUN: %FileCheck %s --check-prefixes=CHECK,CHECK-PUBLIC < %t/Library.swiftinterface
+// RUN: %FileCheck %s --check-prefixes=CHECK,CHECK-PRIVATE < %t/Library.private.swiftinterface
 
 // REQUIRES: VENDOR=apple
 
@@ -33,7 +35,7 @@ public actor ActorWithExplicitAvailability {
 @available(macOS, unavailable)
 public actor UnavailableActor {
   // CHECK: @available(iOS 13.0, tvOS 13.0, watchOS 6.0, *)
-  // CHECK-NEXT: @available(macOS, unavailable)
+  // CHECK-NEXT: @available(macOS, unavailable, introduced: 10.15)
   // CHECK-NEXT: @_semantics("defaultActor") nonisolated final public var unownedExecutor: _Concurrency.UnownedSerialExecutor {
   // CHECK-NEXT:   get
   // CHECK-NEXT: }
@@ -72,9 +74,39 @@ extension Enum {
   @available(macOS, unavailable)
   public actor UnavailableExtensionNestedActor {
     // CHECK: @available(iOS 13.4, tvOS 13.4, watchOS 6.2, *)
-    // CHECK-NEXT: @available(macOS, unavailable)
+    // CHECK-NEXT: @available(macOS, unavailable, introduced: 10.15.4)
     // CHECK-NEXT: @_semantics("defaultActor") nonisolated final public var unownedExecutor: _Concurrency.UnownedSerialExecutor {
     // CHECK-NEXT:   get
     // CHECK-NEXT: }
+  }
+}
+
+// CHECK-PUBLIC: @available(macOS, unavailable)
+// CHECK-PUBLIC-NEXT: @available(iOS, unavailable)
+// CHECK-PUBLIC-NEXT: @available(watchOS, unavailable)
+// CHECK-PUBLIC-NEXT: @available(tvOS, unavailable)
+// CHECK-PUBLIC-NEXT: public struct SPIAvailableStruct
+// CHECK-PRIVATE: @_spi_available(macOS, introduced: 10.15.4)
+// CHECK-PRIVATE-NEXT: @_spi_available(iOS, introduced: 13.4)
+// CHECK-PRIVATE-NEXT: @_spi_available(watchOS, introduced: 6.2)
+// CHECK-PRIVATE-NEXT: @_spi_available(tvOS, introduced: 13.4)
+// CHECK-PRIVATE-NEXT: public struct SPIAvailableStruct
+@_spi_available(SwiftStdlib 5.2, *)
+public struct SPIAvailableStruct {
+  // CHECK: #if compiler(>=5.3) && $Actors
+  // CHECK-NEXT: @_hasMissingDesignatedInitializers @available(macOS, unavailable)
+  // CHECK-NEXT: public actor UnavailableNestedActor
+  @available(macOS, unavailable)
+  public actor UnavailableNestedActor {
+    // CHECK-PUBLIC: @available(iOS, unavailable)
+    // CHECK-PUBLIC-NEXT: @available(tvOS, unavailable)
+    // CHECK-PUBLIC-NEXT: @available(watchOS, unavailable)
+    // CHECK-PUBLIC-NEXT: @available(macOS, unavailable)
+    // CHECK-PUBLIC-NEXT: @_semantics("defaultActor") nonisolated final public var unownedExecutor: _Concurrency.UnownedSerialExecutor
+    // CHECK-PRIVATE: @_spi_available(iOS, introduced: 13.4)
+    // CHECK-PRIVATE-NEXT: @_spi_available(tvOS, introduced: 13.4)
+    // CHECK-PRIVATE-NEXT: @_spi_available(watchOS, introduced: 6.2)
+    // CHECK-PRIVATE-NEXT: @_spi_available(macOS, unavailable, introduced: 10.15.4)
+    // CHECK-PRIVATE-NEXT: @_semantics("defaultActor") nonisolated final public var unownedExecutor: _Concurrency.UnownedSerialExecutor
   }
 }
