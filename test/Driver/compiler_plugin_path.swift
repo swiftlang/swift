b1788529aeffd0e79e8fe3d0dfafd93759c39250
Doug Gregor <dgregor@apple.com>
2023-03-22 00:41:47 -0700

    Cope with Windows paths

--- test/Driver/compiler_plugin_path.swift
+++ test/Driver/compiler_plugin_path.swift
@@ -1,7 +1,7 @@
 // RUN: %swiftc_driver -driver-print-jobs -target x86_64-apple-macosx10.9 %s 2>^1 | %FileCheck %s
 
 // CHECK: -plugin-path
-// CHECK-SAME: BUILD_DIR/lib/swift/host/plugins
+// CHECK-SAME: BUILD_DIR{{(\.\/)?}}lib{{(\.\/)}}swift{{(\.\/)}}host{{(\.\/)}}plugins
 
 // CHECK-SAME: -plugin-path
-// CHECK-SAME: BUILD_DIR/local/lib/swift/host/plugins
+// CHECK-SAME: BUILD_DIR{{(\.\/)?}}local{{(\.\/)}}lib{{(\.\/)}}swift{{(\.\/)}}host{{(\.\/)}}plugins
