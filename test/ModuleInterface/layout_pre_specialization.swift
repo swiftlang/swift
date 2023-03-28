134c9f3992576bee8606a9905bca25b5d335a2ab
Dario Rexin <drexin@apple.com>
2023-03-28 14:23:32 -0700

    Incorporate feedback

--- test/ModuleInterface/layout_pre_specialization.swift
+++ test/ModuleInterface/layout_pre_specialization.swift
@@ -1,9 +1,9 @@
 // RUN: %empty-directory(%t)
 
-// RUN: %target-swift-frontend -enable-experimental-feature LayoutPrespecialization -typecheck -module-name LayoutPrespec -emit-module-interface-path %t/LayoutPrespec.swiftinterface %s
-// RUN: %FileCheck %s < %t/LayoutPrespec.swiftinterface --check-prefix CHECK
-// RUN: %target-swift-frontend -compile-module-from-interface %t/LayoutPrespec.swiftinterface -o %t/LayoutPrespec.swiftmodule
-// RUN: %target-swift-frontend -enable-experimental-feature LayoutPrespecialization -compile-module-from-interface %t/LayoutPrespec.swiftinterface -o %t/LayoutPrespec.swiftmodule
+// RUN: %target-swift-emit-module-interface(%t/LayoutPrespec.swiftinterface) %s -enable-experimental-feature LayoutPrespecialization -module-name LayoutPrespec -disable-availability-checking
+// RUN: %target-swift-typecheck-module-from-interface(%t/LayoutPrespec.swiftinterface) -module-name LayoutPrespec -disable-availability-checking
+// RUN: %target-swift-typecheck-module-from-interface(%t/LayoutPrespec.swiftinterface) -module-name LayoutPrespec -enable-experimental-feature LayoutPrespecialization -disable-availability-checking
+// RUN: %FileCheck %s < %t/LayoutPrespec.swiftinterface
 
 // CHECK: #if compiler(>=5.3) && $LayoutPrespecialization
 // CHECK-NEXT: @_specialize(exported: true, kind: full, where @_noMetadata A : _Class)
