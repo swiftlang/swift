84d643fc8534fd5229f163ef76422e8d96a1a95b
Meghana Gupta <meghanavgupta@gmail.com>
2023-03-28 20:45:01 -0700

    Fix infinite loop in the new hasPointerEscape(SILValue) api

--- lib/SIL/Utils/OwnershipUtils.cpp
+++ lib/SIL/Utils/OwnershipUtils.cpp
@@ -97,7 +97,7 @@ bool swift::hasPointerEscape(SILValue original) {
       return true;
     });
   }
-  while (auto value = worklist.popAndForget()) {
+  while (auto value = worklist.pop()) {
     for (auto use : value->getUses()) {
       switch (use->getOperandOwnership()) {
       case OperandOwnership::PointerEscape:
