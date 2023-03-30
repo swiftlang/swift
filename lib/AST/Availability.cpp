176d97247cf6183cb0fefdc39ba3f786902eab12
Allan Shortlidge <shortlidge@apple.com>
2023-03-30 10:41:02 -0700

    IRGen: Skip lowering unavailable opaque type descriptors.
    Part of rdar://107425181

--- lib/AST/Availability.cpp
+++ lib/AST/Availability.cpp
@@ -184,6 +184,9 @@ AvailabilityInference::parentDeclForInferredAvailability(const Decl *D) {
   if (auto *PBD = dyn_cast<PatternBindingDecl>(D))
     return PBD->getAnchoringVarDecl(0);
 
+  if (auto *OTD = dyn_cast<OpaqueTypeDecl>(D))
+    return OTD->getNamingDecl();
+
   // Clang decls may be inaccurately parented rdar://53956555
   if (D->hasClangNode())
     return nullptr;
