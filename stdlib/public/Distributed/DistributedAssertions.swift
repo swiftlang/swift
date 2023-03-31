762cc389042d987cae31e59f4edd7acaf1f4bc70
Konrad `ktoso` Malawski <konrad_malawski@apple.com>
2023-03-31 17:01:20 +0900

    [Executors] Move assert/assume APIs onto actors; assumeIsolated()

--- stdlib/public/Distributed/DistributedAssertions.swift
+++ stdlib/public/Distributed/DistributedAssertions.swift
@@ -16,98 +16,97 @@ import _Concurrency
 // ==== -----------------------------------------------------------------------
 // MARK: Precondition APIs
 
-/// Unconditionally if the current task is executing on the serial executor of the passed in `actor`,
-/// and if not crash the program offering information about the executor mismatch.
-///
-/// This function's effect varies depending on the build flag used:
-///
-/// * In playgrounds and `-Onone` builds (the default for Xcode's Debug
-///   configuration), stops program execution in a debuggable state after
-///   printing `message`.
-///
-/// * In `-O` builds (the default for Xcode's Release configuration), stops
-///   program execution.
-///
-/// * In `-Ounchecked` builds, the optimizer may assume that this function is
-///   never called. Failure to satisfy that assumption is a serious
-///   programming error.
-///
-/// - Parameter actor: the actor whose serial executor we expect to be the current executor
 @available(SwiftStdlib 5.9, *)
-public func preconditionOnExecutor(
-    of actor: some DistributedActor,
-    _ message: @autoclosure () -> String = String(),
-    file: StaticString = #fileID, line: UInt = #line
-) {
-  guard _isDebugAssertConfiguration() || _isReleaseAssertConfiguration() else {
-    return
-  }
+extension DistributedActor {
+  /// Unconditionally if the current task is executing on the serial executor of the passed in `actor`,
+  /// and if not crash the program offering information about the executor mismatch.
+  ///
+  /// This function's effect varies depending on the build flag used:
+  ///
+  /// * In playgrounds and `-Onone` builds (the default for Xcode's Debug
+  ///   configuration), stops program execution in a debuggable state after
+  ///   printing `message`.
+  ///
+  /// * In `-O` builds (the default for Xcode's Release configuration), stops
+  ///   program execution.
+  ///
+  /// * In `-Ounchecked` builds, the optimizer may assume that this function is
+  ///   never called. Failure to satisfy that assumption is a serious
+  ///   programming error.
+  @available(SwiftStdlib 5.9, *)
+  public nonisolated func preconditionIsolated(
+      _ message: @autoclosure () -> String = String(),
+      file: StaticString = #fileID, line: UInt = #line
+  ) {
+    guard _isDebugAssertConfiguration() || _isReleaseAssertConfiguration() else {
+      return
+    }
 
-  guard __isLocalActor(actor) else {
-    return
-  }
+    guard __isLocalActor(self) else {
+      return
+    }
 
-  guard let unownedExecutor = actor.localUnownedExecutor else {
-    preconditionFailure(
-        "Incorrect actor executor assumption; Distributed actor \(actor) is 'local' but has no executor!",
+    guard let unownedExecutor = self.localUnownedExecutor else {
+      preconditionFailure(
+          "Incorrect actor executor assumption; Distributed actor \(self) is 'local' but has no executor!",
+          file: file, line: line)
+    }
+
+    let expectationCheck = _taskIsCurrentExecutor(unownedExecutor._executor)
+
+    // TODO: offer information which executor we actually got
+    precondition(expectationCheck,
+        // TODO: figure out a way to get the typed repr out of the unowned executor
+        "Incorrect actor executor assumption; Expected '\(unownedExecutor)' executor. \(message())",
         file: file, line: line)
   }
-
-  let expectationCheck = _taskIsCurrentExecutor(unownedExecutor._executor)
-
-  // TODO: offer information which executor we actually got
-  precondition(expectationCheck,
-      // TODO: figure out a way to get the typed repr out of the unowned executor
-      "Incorrect actor executor assumption; Expected '\(unownedExecutor)' executor. \(message())",
-      file: file, line: line)
 }
 
 // ==== -----------------------------------------------------------------------
 // MARK: Assert APIs
 
-/// Performs an executor check in debug builds.
-///
-/// * In playgrounds and `-Onone` builds (the default for Xcode's Debug
-///   configuration): If `condition` evaluates to `false`, stop program
-///   execution in a debuggable state after printing `message`.
-///
-/// * In `-O` builds (the default for Xcode's Release configuration),
-///   `condition` is not evaluated, and there are no effects.
-///
-/// * In `-Ounchecked` builds, `condition` is not evaluated, but the optimizer
-///   may assume that it *always* evaluates to `true`. Failure to satisfy that
-///   assumption is a serious programming error.
-///
-///
-/// - Parameter actor: the actor whose serial executor we expect to be the current executor
 @available(SwiftStdlib 5.9, *)
-@_transparent
-public func assertOnExecutor(
-    of actor: some DistributedActor,
-    _ message: @autoclosure () -> String = String(),
-    file: StaticString = #fileID, line: UInt = #line
-) {
-  guard _isDebugAssertConfiguration() else {
-    return
-  }
+extension DistributedActor {
+  /// Performs an executor check in debug builds.
+  ///
+  /// * In playgrounds and `-Onone` builds (the default for Xcode's Debug
+  ///   configuration): If `condition` evaluates to `false`, stop program
+  ///   execution in a debuggable state after printing `message`.
+  ///
+  /// * In `-O` builds (the default for Xcode's Release configuration),
+  ///   `condition` is not evaluated, and there are no effects.
+  ///
+  /// * In `-Ounchecked` builds, `condition` is not evaluated, but the optimizer
+  ///   may assume that it *always* evaluates to `true`. Failure to satisfy that
+  ///   assumption is a serious programming error.
+  @available(SwiftStdlib 5.9, *)
+  @_transparent
+  public nonisolated func assertIsolated(
+      _ message: @autoclosure () -> String = String(),
+      file: StaticString = #fileID, line: UInt = #line
+  ) {
+    guard _isDebugAssertConfiguration() else {
+      return
+    }
 
-  guard __isLocalActor(actor) else {
-    return
-  }
+    guard __isLocalActor(self) else {
+      return
+    }
 
-  guard let unownedExecutor = actor.localUnownedExecutor else {
-    preconditionFailure(
-        "Incorrect actor executor assumption; Distributed actor \(actor) is 'local' but has no executor!",
-        file: file, line: line)
-  }
+    guard let unownedExecutor = self.localUnownedExecutor else {
+      preconditionFailure(
+          "Incorrect actor executor assumption; Distributed actor \(self) is 'local' but has no executor!",
+          file: file, line: line)
+    }
 
-  guard _taskIsCurrentExecutor(unownedExecutor._executor) else {
-    // TODO: offer information which executor we actually got
-    // TODO: figure out a way to get the typed repr out of the unowned executor
-    let msg = "Incorrect actor executor assumption; Expected '\(unownedExecutor)' executor. \(message())"
-    /// TODO: implement the logic in-place perhaps rather than delegating to precondition()?
-    assertionFailure(msg, file: file, line: line) // short-cut so we get the exact same failure reporting semantics
-    return
+    guard _taskIsCurrentExecutor(unownedExecutor._executor) else {
+      // TODO: offer information which executor we actually got
+      // TODO: figure out a way to get the typed repr out of the unowned executor
+      let msg = "Incorrect actor executor assumption; Expected '\(unownedExecutor)' executor. \(message())"
+      /// TODO: implement the logic in-place perhaps rather than delegating to precondition()?
+      assertionFailure(msg, file: file, line: line) // short-cut so we get the exact same failure reporting semantics
+      return
+    }
   }
 }
 
@@ -116,34 +115,58 @@ public func assertOnExecutor(
 // MARK: Assume APIs
 
 @available(SwiftStdlib 5.9, *)
-@_unavailableFromAsync(message: "express the closure as an explicit function declared on the specified 'distributed actor' instead")
-public func assumeOnLocalDistributedActorExecutor<Act: DistributedActor, T>(
-    of actor: Act,
-    _ operation: (isolated Act) throws -> T,
-    file: StaticString = #fileID, line: UInt = #line
-) rethrows -> T {
-  typealias YesActor = (isolated Act) throws -> T
-  typealias NoActor = (Act) throws -> T
+extension DistributedActor {
 
-  guard __isLocalActor(actor) else {
-    fatalError("Cannot assume to be 'isolated \(Act.self)' since distributed actor '\(actor)' is remote.")
-  }
+  /// Assume that the current actor is a local distributed actor and that the currently executing context is the same as that actors
+  /// serial executor, or crash.
+  ///
+  /// This method allows developers to *assume and verify* that the currently executing synchronous function
+  /// is actually executing on the serial executor that this distributed (local) actor is using.
+  ///
+  /// If that is the case, the operation is invoked with an `isolated` version of the actoe,
+  /// allowing synchronous access to actor local state without hopping through asynchronous boundaries.
+  ///
+  /// If the current context is not running on the actor's serial executor, or if the actor is a reference to a remote actor,
+  /// this method will crash with a fatalError (similar to ``preconditionIsolated()``).
+  ///
+  /// This method can only be used from synchronous functions, as asynchronous ones should instead
+  /// perform normal method call to the actor.
+  ///
+  /// - Parameters:
+  ///   - operation: the operation that will be executed if the current context is executing on the actors serial executor, and the actor is a local reference
+  ///   - file: source location where the assume call is made
+  ///   - file: source location where the assume call is made
+  /// - Returns: the return value of the `operation`
+  /// - Throws: rethrows the `Error` thrown by the operation if it threw
+  @available(SwiftStdlib 5.9, *)
+  @_unavailableFromAsync(message: "express the closure as an explicit function declared on the specified 'distributed actor' instead")
+  public nonisolated func assumeIsolated<T>(
+      _ operation: (isolated Self) throws -> T,
+      file: StaticString = #fileID, line: UInt = #line
+  ) rethrows -> T {
+    typealias YesActor = (isolated Self) throws -> T
+    typealias NoActor = (Self) throws -> T
 
-  /// This is guaranteed to be fatal if the check fails,
-  /// as this is our "safe" version of this API.
-  guard let executor = actor.localUnownedExecutor else {
-    fatalError("Distributed local actor MUST have executor, but was nil")
-  }
-  guard _taskIsCurrentExecutor(executor._executor) else {
-    // TODO: offer information which executor we actually got when
-    fatalError("Incorrect actor executor assumption; Expected same executor as \(actor).", file: file, line: line)
-  }
+      guard __isLocalActor(self) else {
+        fatalError("Cannot assume to be 'isolated \(Self.self)' since distributed actor '\(self)' is a remote actor reference.")
+      }
 
-  // To do the unsafe cast, we have to pretend it's @escaping.
-  return try withoutActuallyEscaping(operation) {
-    (_ fn: @escaping YesActor) throws -> T in
-    let rawFn = unsafeBitCast(fn, to: NoActor.self)
-    return try rawFn(actor)
+    /// This is guaranteed to be fatal if the check fails,
+    /// as this is our "safe" version of this API.
+    guard let executor = self.localUnownedExecutor else {
+      fatalError("Distributed local actor MUST have executor, but was nil")
+    }
+    guard _taskIsCurrentExecutor(executor._executor) else {
+      // TODO: offer information which executor we actually got when
+      fatalError("Incorrect actor executor assumption; Expected same executor as \(self).", file: file, line: line)
+    }
+
+    // To do the unsafe cast, we have to pretend it's @escaping.
+    return try withoutActuallyEscaping(operation) {
+      (_ fn: @escaping YesActor) throws -> T in
+      let rawFn = unsafeBitCast(fn, to: NoActor.self)
+      return try rawFn(self)
+    }
   }
 }
 
