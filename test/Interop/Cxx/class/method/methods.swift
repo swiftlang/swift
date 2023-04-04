7f10efad1273a449d26e104a626347bdddb7f122
zoecarver <z.zoelec2@gmail.com>
2023-04-04 11:39:50 -0700

    [cxx-interop][nfc][no-merge] See how broken methods are.

--- test/Interop/Cxx/class/method/methods.swift
+++ test/Interop/Cxx/class/method/methods.swift
@@ -1,9 +1,6 @@
 // RUN: %target-run-simple-swift(-I %S/Inputs/ -Xfrontend -enable-experimental-cxx-interop)
 //
 // REQUIRES: executable_test
-//
-// Crash when running on windows: rdar://88391102
-// XFAIL: OS=windows-msvc
 
 import StdlibUnittest
 import Methods
