02a604e7e782aadb8bff68f164cf729da315f92a
Rintaro Ishizaki <rishizaki@apple.com>
2023-04-05 12:48:09 -0700

    [Tests/Macros] Add %target-codesign before %target-run
    rdar://107658689

--- test/Macros/macro_expand_conformances.swift
+++ test/Macros/macro_expand_conformances.swift
@@ -6,6 +6,7 @@
 // RUN: %FileCheck -check-prefix=CHECK-DUMP %s < %t/expansions-dump.txt
 // RUN: %target-typecheck-verify-swift -swift-version 5 -load-plugin-library %t/%target-library-name(MacroDefinition) -module-name MacroUser -DTEST_DIAGNOSTICS -swift-version 5
 // RUN: %target-build-swift -swift-version 5 -load-plugin-library %t/%target-library-name(MacroDefinition) %s -o %t/main -module-name MacroUser -swift-version 5
+// RUN: %target-codesign %t/main
 // RUN: %target-run %t/main | %FileCheck %s
 
 @attached(conformance)
