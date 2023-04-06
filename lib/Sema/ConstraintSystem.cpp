341395fb897e51c2a235cff7c4c6e1a036ad8f0f
Pavel Yaskevich <pyaskevich@apple.com>
2023-04-06 00:06:11 -0700

    [ConstraintSystem] Add a new locator element to identify pack expansion types
    (cherry picked from commit 6769e39ed76fa34a71aed1418d6b012ec629de36)

--- lib/Sema/ConstraintSystem.cpp
+++ lib/Sema/ConstraintSystem.cpp
@@ -5745,6 +5745,7 @@ void constraints::simplifyLocator(ASTNode &anchor,
 
     case ConstraintLocator::PackElement:
     case ConstraintLocator::PackShape:
+    case ConstraintLocator::PackExpansionType:
       break;
 
     case ConstraintLocator::PackExpansionPattern: {
