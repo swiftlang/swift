39bb7bfe440d8958ac5acb6667826c972ee36fd6
Dave Lee <davelee.com@gmail.com>
2023-04-11 16:23:20 -0600

    [AST] Convert ASTContext::getSwiftName to a free function
    The only state `getSwiftName` uses is its argument, and can be made a free function. Of
    note the inverse operation, `getKnownFoundationEntity`, is also a free function. This
    removes the requirement that callers have an `ASTContext` instance.
    
    (cherry-picked from commit fb524c0b865659cd81af31241667abf808677006)

--- lib/Sema/TypeCheckType.cpp
+++ lib/Sema/TypeCheckType.cpp
@@ -1374,7 +1374,7 @@ static Type diagnoseUnknownType(TypeResolution resolution,
       repr->overwriteNameRef(DeclNameRef(ctx.getIdentifier(RemappedTy)));
 
       // HACK: 'NSUInteger' suggests both 'UInt' and 'Int'.
-      if (TypeName == ctx.getSwiftName(KnownFoundationEntity::NSUInteger)) {
+      if (TypeName == swift::getSwiftName(KnownFoundationEntity::NSUInteger)) {
         diags.diagnose(L, diag::note_remapped_type, "UInt")
           .fixItReplace(R, "UInt");
       }
