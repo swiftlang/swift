5080898f9b66c2bd039603e6edbaf193bf6fbd39
Alexis Laferrière <alaf@apple.com>
2023-04-12 16:20:12 -0700

    [Serialization] Disable deserialization safety along with access control
    Disabling access control is fundamentally incompatible with
    deserialization safety. Let's turn off safety automatically when in use.
    
    This was reported by a few tests:
    
    stdlib/Dictionary.swift
    stdlib/Set.swift
    stdlib/SetOperations.swift.gyb
    stdlib/SwiftNativeNSBase.swift

--- test/Serialization/Safety/skip-reading-internal-details.swift
+++ test/Serialization/Safety/skip-reading-internal-details.swift
@@ -20,6 +20,11 @@
 
 // RUN: %target-swift-frontend -typecheck %t/Client.swift -I %t \
 // RUN:   -verify -Xllvm -debug-only=Serialization \
+// RUN:   -disable-access-control 2>&1 \
+// RUN:   | %FileCheck --check-prefixes=NEEDED,UNSAFE %s
+
+// RUN: %target-swift-frontend -typecheck %t/Client.swift -I %t \
+// RUN:   -verify -Xllvm -debug-only=Serialization \
 // RUN:   -enable-deserialization-safety 2>&1 \
 // RUN:   | %FileCheck --check-prefixes=NEEDED,CLEAN,SAFE %s
 
