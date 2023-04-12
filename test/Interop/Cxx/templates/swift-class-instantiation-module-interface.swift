045fcf3ff59a1a8894fc30021a24698b45c2ed26
Alex Lorenz <arphaman@gmail.com>
2023-04-12 11:25:30 -0700

    [interop] Prohibit use of C++ APIs in public interfaces that opt-in into library evolution
    The CxxStdlib overlay now has to be built without library evolution enabled.

--- test/Interop/Cxx/templates/swift-class-instantiation-module-interface.swift
+++ test/Interop/Cxx/templates/swift-class-instantiation-module-interface.swift
@@ -1,5 +1,5 @@
 // RUN: %empty-directory(%t)
-// RUN: %target-swiftxx-frontend -emit-module -o %t/SwiftClassTemplateModule.swiftmodule %S/Inputs/SwiftClassInstantiationModule.swift -I %S/Inputs -enable-library-evolution -swift-version 5 -disable-availability-checking
+// RUN: %target-swiftxx-frontend -emit-module -o %t/SwiftClassTemplateModule.swiftmodule %S/Inputs/SwiftClassInstantiationModule.swift -I %S/Inputs -swift-version 5 -disable-availability-checking
 // RUN: %target-swift-ide-test -print-module -module-to-print=SwiftClassTemplateModule -I %t/ -source-filename=x -enable-experimental-cxx-interop | %FileCheck %s
 
 
