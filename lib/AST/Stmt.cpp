4791804e14525bd1d006f79daecf9d148257449d
Cal Stephens <cal@calstephens.tech>
2023-04-20 13:11:54 -0700

    Consolidate LabeledConditionalStmt::rebindsSelf logic into shared helper

--- lib/AST/Stmt.cpp
+++ lib/AST/Stmt.cpp
@@ -440,6 +440,71 @@ void LabeledConditionalStmt::setCond(StmtCondition e) {
   Cond = e;
 }
 
+/// Whether or not this conditional stmt rebinds self with a `let self`
+/// or `let self = self` condition. If `requireLoadExpr` is `true`,
+/// additionally requires that the RHS of the self condition is a `LoadExpr`.
+bool LabeledConditionalStmt::rebindsSelf(ASTContext &Ctx,
+                                         bool requireLoadExpr) const {
+  return llvm::any_of(getCond(), [&Ctx, requireLoadExpr](const auto &cond) {
+    return cond.rebindsSelf(Ctx, requireLoadExpr);
+  });
+}
+
+/// Whether or not this conditional stmt rebinds self with a `let self`
+/// or `let self = self` condition. If `requireLoadExpr` is `true`,
+/// additionally requires that the RHS of the self condition is a `LoadExpr`.
+bool StmtConditionElement::rebindsSelf(ASTContext &Ctx,
+                                       bool requireLoadExpr) const {
+  auto pattern = getPatternOrNull();
+  if (!pattern) {
+    return false;
+  }
+
+  // Check whether or not this pattern defines a new `self` decl
+  bool isSelfRebinding = false;
+  if (pattern->getBoundName() == Ctx.Id_self) {
+    isSelfRebinding = true;
+  }
+
+  else if (auto OSP = dyn_cast<OptionalSomePattern>(pattern)) {
+    if (auto subPattern = OSP->getSubPattern()) {
+      isSelfRebinding = subPattern->getBoundName() == Ctx.Id_self;
+    }
+  }
+
+  if (!isSelfRebinding) {
+    return false;
+  }
+
+  // Check that the RHS expr is exactly `self` and not something else
+  Expr *exprToCheckForDRE = getInitializerOrNull();
+  if (!exprToCheckForDRE) {
+    return false;
+  }
+
+  // The RHS could be a `LoadExpr` or `DeclRefExpr`
+  Expr *loadExprSubExpr = nullptr;
+  if (auto LE = dyn_cast<LoadExpr>(exprToCheckForDRE)) {
+    loadExprSubExpr = LE->getSubExpr();
+  }
+
+  if (requireLoadExpr) {
+    exprToCheckForDRE = loadExprSubExpr;
+  } else {
+    if (loadExprSubExpr) {
+      exprToCheckForDRE = loadExprSubExpr;
+    }
+    exprToCheckForDRE = exprToCheckForDRE->getSemanticsProvidingExpr();
+  }
+
+  DeclRefExpr *condDRE = dyn_cast_or_null<DeclRefExpr>(exprToCheckForDRE);
+  if (!condDRE || !condDRE->getDecl()->hasName()) {
+    return false;
+  }
+
+  return condDRE->getDecl()->getName().isSimpleName(Ctx.Id_self);
+}
+
 PoundAvailableInfo *
 PoundAvailableInfo::create(ASTContext &ctx, SourceLoc PoundLoc,
                            SourceLoc LParenLoc,
