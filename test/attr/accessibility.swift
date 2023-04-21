3cabe08d29c7637a84a4ffecea8d11f85ce66e30
stevenwong <stevenhuang12@outlook.com>
2023-04-21 23:38:57 +0800

    [Parser] Make parser aware of the unexpected attributes

--- test/attr/accessibility.swift
+++ test/attr/accessibility.swift
@@ -102,7 +102,7 @@ duplicateAttr(1) // expected-error{{argument passed to call that takes no argume
 
 // CHECK ALLOWED DECLS
 private import Swift // expected-error {{Access level on imports require '-enable-experimental-feature AccessLevelOnImport}}
-private(set) infix operator ~~~ // expected-error {{'private' modifier cannot be applied to this declaration}} {{1-14=}}
+private(set) infix operator ~~~ // expected-error {{unexpected attribute private in operator declaration}} {{1-14=}}
 
 private typealias MyInt = Int
 
