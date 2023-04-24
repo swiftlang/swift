b13da727b52434ab1e2fe2722a15dce27df13547
Mykola Pokhylets <mykola.pokhylets@gmail.com>
2023-04-24 12:24:18 +0200

    Removed declaration of the AdHocJob from the ABI headers

--- stdlib/public/Concurrency/Task.cpp
+++ stdlib/public/Concurrency/Task.cpp
@@ -206,14 +206,6 @@ void NullaryContinuationJob::process(Job *_job) {
   swift_continuation_resume(continuation);
 }
 
-void AdHocJob::process(Job *_job) {
-  auto *job = cast<AdHocJob>(_job);
-  void *ctx = job->Context;
-  AdHocWorkFunction *work = job->Work;
-  delete job;
-  return work(ctx);
-}
-
 void AsyncTask::completeFuture(AsyncContext *context) {
   using Status = FutureFragment::Status;
   using WaitQueueItem = FutureFragment::WaitQueueItem;
@@ -1681,6 +1673,32 @@ swift_task_createNullaryContinuationJobImpl(
 
   return job;
 }
+
+/// Job that allows to use executor API to schedule a block of task-less
+/// synchronous code.
+class AdHocJob : public Job {
+private:
+  void *Context;
+  AdHocWorkFunction *Work;
+
+public:
+  AdHocJob(JobPriority priority, void *context, AdHocWorkFunction *work)
+      : Job({JobKind::AdHoc, priority}, &process), Context(context),
+        Work(work) {}
+
+  SWIFT_CC(swiftasync)
+  static void process(Job *_job) {
+    auto *job = cast<AdHocJob>(_job);
+    void *ctx = job->Context;
+    AdHocWorkFunction *work = job->Work;
+    delete job;
+    return work(ctx);
+  }
+
+  static bool classof(const Job *job) {
+    return job->Flags.getKind() == JobKind::AdHoc;
+  }
+};
 
 SWIFT_CC(swift)
 static void swift_task_performOnExecutorImpl(void *context,
