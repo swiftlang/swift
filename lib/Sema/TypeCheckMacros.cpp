4f6ab4b9dc1e6e3f374130bc84335a75df430e81
Doug Gregor <dgregor@apple.com>
2023-04-24 21:17:55 -0700

    Anchor the macro-resolution request on the declaration to which the macro is attached
    The macro-resolution request for an attached macro was expressed in
    terms of the custom attribute and the declaration context enclosing the
    attribute. While the declaration context is the correct one for
    resolving the types and arguments of the custom attribute, the
    declaration provides a better anchor for cases where the same
    attribute applies to multiple declarations, e.g., with
    member-attribute macros, leading to false cyclic references.

--- lib/Sema/TypeCheckMacros.cpp
+++ lib/Sema/TypeCheckMacros.cpp
@@ -1478,7 +1478,9 @@ swift::expandConformances(CustomAttr *attr, MacroDecl *macro,
 ConcreteDeclRef
 ResolveMacroRequest::evaluate(Evaluator &evaluator,
                               UnresolvedMacroReference macroRef,
-                              DeclContext *dc) const {
+                              const Decl *decl) const {
+  auto dc = decl->getDeclContext();
+
   // Macro expressions and declarations have their own stored macro
   // reference. Use it if it's there.
   if (auto *expr = macroRef.getExpr()) {
