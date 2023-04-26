0e9b8a1e0a09aaa03f302c3cea81bb9515604fc7
Allan Shortlidge <shortlidge@apple.com>
2023-04-25 23:49:05 -0700

    Sema: Relax distributed actor typechecking for swiftinterfaces.
    Some decls that are expected to be synthesized for distributed actors are
    printed explicitly in swiftinterfaces so diagnostics and assertions need to
    take that possibility into account.
    
    Resolves rdar://108533918

--- lib/Sema/TypeCheckAttr.cpp
+++ lib/Sema/TypeCheckAttr.cpp
@@ -6676,9 +6676,8 @@ void AttributeChecker::visitNonisolatedAttr(NonisolatedAttr *attr) {
         // The synthesized "id" and "actorSystem" are the only exceptions,
         // because the implementation mirrors them.
         if (nominal->isDistributedActor() &&
-            !(var->isImplicit() &&
-              (var->getName() == Ctx.Id_id ||
-               var->getName() == Ctx.Id_actorSystem))) {
+            !(var->getName() == Ctx.Id_id ||
+              var->getName() == Ctx.Id_actorSystem)) {
           diagnoseAndRemoveAttr(attr,
                                 diag::nonisolated_distributed_actor_storage);
           return;
