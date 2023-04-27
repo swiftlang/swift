879a10b8605fce5c1e496681c76a22e40ec00263
Doug Gregor <dgregor@apple.com>
2023-04-26 18:27:10 -0700

    [Macros] Ensure that the type refinement context accounts for macros
    Make sure that we include all of the AST nodes from a macro expansion
    buffer when building the type refinement context, not just the
    declarations.

--- test/Macros/macro_availability_macosx.swift
+++ test/Macros/macro_availability_macosx.swift
@@ -1,6 +1,9 @@
-// REQUIRES: swift_swift_parser, OS=macosx
+// REQUIRES: swift_swift_parser, executable_test, OS=macosx
 
-// RUN: %target-typecheck-verify-swift -swift-version 5 -module-name MacrosTest -target %target-cpu-apple-macosx11
+// RUN: %empty-directory(%t)
+// RUN: %host-build-swift -swift-version 5 -emit-library -o %t/%target-library-name(MacroDefinition) -module-name=MacroDefinition %S/Inputs/syntax_macro_definitions.swift -g -no-toolchain-stdlib-rpath -swift-version 5
+
+// RUN: %target-typecheck-verify-swift -swift-version 5 -module-name MacrosTest -target %target-cpu-apple-macosx11  -load-plugin-library %t/%target-library-name(MacroDefinition)
 
 @available(macOS 12.0, *)
 struct X { }
@@ -11,3 +14,16 @@ struct X { }
 @available(macOS 12.0, *)
 @freestanding(expression) macro m2() -> X = #externalMacro(module: "A", type: "B")
 // expected-warning@-1{{external macro implementation type 'A.B' could not be found for macro 'm2()'}}
+
+@freestanding(expression) macro stringify<T>(_ value: T) -> (T, String) = #externalMacro(module: "MacroDefinition", type: "StringifyMacro")
+
+@available(macOS 12.0, *)
+func onlyInMacOS12() { }
+
+func test() {
+  _ = #stringify({
+      if #available(macOS 12.0, *) {
+        onlyInMacOS12()
+      }
+    })
+}
