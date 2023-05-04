e09e222e6af2dfa38081ad6e0900db62918e5570
Dinesh Sharma <65244911+dk-talks@users.noreply.github.com>
2023-05-04 10:15:49 +0530

    Update inherit.swift

--- test/decl/inherit/inherit.swift
+++ test/decl/inherit/inherit.swift
@@ -30,13 +30,13 @@ class D3 : Any, A { } // expected-error{{superclass 'A' must appear first in the
 class D4 : P & P1, A { } // expected-error{{superclass 'A' must appear first in the inheritance clause}}{{18-21=}}{{12-12=A, }}
 
 // Struct inheriting a class
-struct S : A { } // expected-error{{class can only inherit from protocol in extension declaration 'A'}}
+struct S : A { } // expected-error{{cannot inherit from non-protocol type 'A'}}
 
 // Protocol inheriting a class
 protocol Q : A { }
 
 // Extension inheriting a class
-extension C : A { } // expected-error{{class can only inherit from protocol in extension declaration 'A'}}
+extension C : A { } // expected-error{{cannot inherit from non-protocol type 'A'}}
 
 // Keywords in inheritance clauses
 struct S2 : struct { } // expected-error{{expected type}}
