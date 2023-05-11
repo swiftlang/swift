b17ade939186b7dc2354f245032823f2a26acad6
Alex Lorenz <arphaman@gmail.com>
2023-05-11 15:16:25 -0700

    [cxx-interop] implicitly imported CxxShim module should not force client modules to enable C++ interoperability
    Fixes https://github.com/apple/swift/issues/65831

---
+++ test/Interop/Cxx/implementation-only-imports/import-implementation-only-cxxstdlib-interop-module-without-interop.swift
@@ -0,0 +1,13 @@
+// RUN: %empty-directory(%t)
+
+// RUN: %target-swift-frontend %S/Inputs/use-cxx-stdlib-impl-only.swift -I %S/Inputs/ -module-name UseCxxStdlibImplOnly -emit-module -emit-module-path %t/UseCxxStdlibImplOnly.swiftmodule -cxx-interoperability-mode=default
+
+// RUN: %target-swift-frontend %s -typecheck -module-name TestMod -I %t -I %S/Inputs
+
+// REQUIRES: OS=macosx || OS=linux-gnu
+
+import UseCxxStdlibImplOnly
+
+public func testCallsAPI() {
+    testUsesCxxStdlib()
+}
