30dc160b21f402fd3159909edfcfbb15516a2d94
Victoria Mitchell <victoria_m@apple.com>
2023-05-12 11:32:20 -0600

    review: replace SwiftAttrExtension test skeleton with split-file

--- test/SymbolGraph/ClangImporter/Inputs/SwiftAttrExtension/SwiftAttrExtension.framework/Headers/SwiftAttrExtension.h
+++
@@ -1,5 +0,0 @@
-@import Foundation;
-
-__attribute__((swift_attr("@_nonSendable(_assumed)")))
-@interface MyObjcClass : NSObject
-@end
--- test/SymbolGraph/ClangImporter/SwiftAttrExtension.swift
+++ test/SymbolGraph/ClangImporter/SwiftAttrExtension.swift
@@ -1,15 +1,26 @@
-// RUN: %empty-directory(%t)
-// RUN: cp -r %S/Inputs/SwiftAttrExtension/SwiftAttrExtension.framework %t
-// RUN: %target-swift-frontend(mock-sdk: %clang-importer-sdk) -enable-objc-interop -emit-module-path %t/SwiftAttrExtension.framework/Modules/SwiftAttrExtension.swiftmodule/%target-swiftmodule-name -import-underlying-module -F %t -module-name SwiftAttrExtension -disable-objc-attr-requires-foundation-module %s -emit-symbol-graph -emit-symbol-graph-dir %t
-// RUN: %FileCheck %s --input-file %t/SwiftAttrExtension.symbols.json
-
-// RUN: %target-swift-symbolgraph-extract -sdk %clang-importer-sdk -module-name SwiftAttrExtension -F %t -output-dir %t -pretty-print -v
-// RUN: %FileCheck %s --input-file %t/SwiftAttrExtension.symbols.json
-
 // REQUIRES: objc_interop
 
-extension MyObjcClass {
-  public func someFunc() {}
+// RUN: %empty-directory(%t)
+// RUN: %empty-directory(%t/SwiftAttrExtension)
+// RUN: split-file %s %t
+
+// RUN: %target-swift-frontend(mock-sdk: %clang-importer-sdk) -emit-module -emit-module-path %t/SwiftAttrExtension.swiftmodule -enable-objc-interop -I %t/SwiftAttrExtension -module-name SwiftAttrExtension -import-underlying-module -emit-symbol-graph -emit-symbol-graph-dir %t %t/Extension.swift
+// RUN: %FileCheck %s --input-file %t/SwiftAttrExtension.symbols.json
+
+//--- SwiftAttrExtension/module.modulemap
+module SwiftAttrExtension {
+  header "SwiftAttrExtension.h"
 }
 
-// CHECK: s:So11MyObjcClassC18SwiftAttrExtensionE8someFuncyyF
+//--- SwiftAttrExtension/SwiftAttrExtension.h
+@import Foundation;
+
+__attribute__((swift_attr("@_nonSendable(_assumed)")))
+@interface MyObjcClass : NSObject
+@end
+
+//--- Extension.swift
+extension MyObjcClass {
+  // CHECK: s:So11MyObjcClassC18SwiftAttrExtensionE8someFuncyyF
+  public func someFunc() {}
+}
