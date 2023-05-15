82b24ba1654aab16e1e98c4e08a0acff8558592c
Erik Eckstein <eeckstein@apple.com>
2023-05-15 22:47:09 +0200

    IRGen: fix async vtable stubs
    For deleted async vtable methods we need to create a async function pointer.
    
    Fixes a missing-symbol linker error
    rdar://108924001

--- test/IRGen/report_dead_method_call.swift
+++ test/IRGen/report_dead_method_call.swift
@@ -2,25 +2,31 @@
 
 // We compile with -O (optimizations) and -disable-access-control (which
 // allows use to "call" methods that are removed by dead code elimination).
-// RUN: %target-build-swift %S/Inputs/report_dead_method_call/main.swift %s -O -Xfrontend -disable-access-control -o %t/report_dead_method_call
+// RUN: %target-build-swift -parse-as-library %S/Inputs/report_dead_method_call/main.swift %s -O -Xfrontend -disable-access-control -Xfrontend -disable-availability-checking -o %t/report_dead_method_call
 
 // The private, unused methods are optimized away. The test calls these
 // methods anyway (since it has overridden the access control), so we
 // expect them to produce "Fatal error: Call of deleted method" when run.
 // RUN: %target-codesign %t/report_dead_method_call
 // RUN: %target-run %t/report_dead_method_call
+
 // REQUIRES: executable_test
+// REQUIRES: concurrency
+// UNSUPPORTED: freestanding
+// REQUIRES: concurrency_runtime
+// UNSUPPORTED: back_deployment_runtime
 
 // UNSUPPORTED: swift_test_mode_optimize_none_with_implicit_dynamic
 // UNSUPPORTED: swift_test_mode_optimize_with_implicit_dynamic
 
 private protocol PrivateProto {
 	func abc()
+	func abcAsync() async
 }
 
 struct PrivateStructC : PrivateProto {
-	func abc() {
-	}
+	func abc() {}
+	func abcAsync() async {}
 }
 
 struct Container {
@@ -33,14 +39,19 @@ func callProto() {
 	testProto(Container())
 }
 
+@inline(never)
+func callProtoAsync() async {
+	await testProtoAsync(Container())
+}
+
 private class Base {
-	func def() {
-	}
+	func def() {}
+	func defAsync() async {}
 }
 
 private class Derived : Base {
-	override func def() {
-	}
+	override func def() {}
+	override func defAsync() async {}
 }
 
 struct ClassContainer {
@@ -53,8 +64,14 @@ func callClass() {
 	testClass(ClassContainer())
 }
 
+@inline(never)
+func callClassAsync() async {
+	await testClassAsync(ClassContainer())
+}
+
 public class PublicBase {
-	private func ghi() {
-	}
+	private func ghi() { }
+
+  private func ghiAsync() async {}
 }
 
