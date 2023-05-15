b0dab0b48c77ac25058788c4a1fbe81bc53a90e7
Alex Lorenz <arphaman@gmail.com>
2023-05-15 13:05:15 -0700

    [cxx-interop] evaluate default constructor's unevaluated exception spec if needed when emitting C++ constructor call
    Fixes https://github.com/apple/swift/issues/65891

--- lib/IRGen/IRGenModule.h
+++ lib/IRGen/IRGenModule.h
@@ -1826,6 +1826,9 @@ public:
 
   bool isForeignExceptionHandlingEnabled() const;
 
+  /// Returns true if the given Clang function does not throw exceptions.
+  bool isCxxNoThrow(clang::FunctionDecl *fd, bool defaultNoThrow = false);
+
 private:
   llvm::Constant *
   getAddrOfSharedContextDescriptor(LinkEntity entity,
