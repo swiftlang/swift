b5d7d70fde601c165938a4f53c1091998c1ed3a5
Michael Gottesman <mgottesman@apple.com>
2023-05-15 11:18:17 -0700

    Update some allocbox to stack tests given I removed early allocbox to stack.
    (cherry picked from commit 0b38bbaba0ab17ccef53bed9d1707d9f420f4e41)

--- test/SILOptimizer/allocboxtostack_escape.swift
+++ test/SILOptimizer/allocboxtostack_escape.swift
@@ -12,9 +12,7 @@ func testError() -> (() -> ()) {
   // We emit this error twice since we run allocbox to stack twice in the pipeline for now. This is not an official feature, so it is ok for now.
   @_semantics("boxtostack.mustbeonstack")
   var x = Box<Klass>(value: Klass()) // expected-error {{Can not promote value from heap to stack due to value escaping}}
-  // expected-error @-1 {{Can not promote value from heap to stack due to value escaping}}
   let result = { // expected-note {{value escapes here}}
-      // expected-note @-1 {{value escapes here}}
     myPrint(&x)
   }
   return result
