dd3bc822fcd88f3c94c9036ca771b2c3d7daadc6
Victoria Mitchell <victoria_m@apple.com>
2023-05-16 10:08:53 -0600

    review: change the test to import a separate objc module

--- test/SymbolGraph/ClangImporter/SwiftAttrExtension.swift
+++ test/SymbolGraph/ClangImporter/SwiftAttrExtension.swift
@@ -1,18 +1,18 @@
 // REQUIRES: objc_interop
 
 // RUN: %empty-directory(%t)
-// RUN: %empty-directory(%t/SwiftAttrExtension)
+// RUN: %empty-directory(%t/SwiftAttr)
 // RUN: split-file %s %t
 
-// RUN: %target-swift-frontend(mock-sdk: %clang-importer-sdk) -emit-module -emit-module-path %t/SwiftAttrExtension.swiftmodule -enable-objc-interop -I %t/SwiftAttrExtension -module-name SwiftAttrExtension -import-underlying-module -emit-symbol-graph -emit-symbol-graph-dir %t %t/Extension.swift
-// RUN: %FileCheck %s --input-file %t/SwiftAttrExtension.symbols.json
+// RUN: %target-swift-frontend(mock-sdk: %clang-importer-sdk) -emit-module -emit-module-path %t/SwiftAttrExtension.swiftmodule -enable-objc-interop -I %t/SwiftAttr -module-name SwiftAttrExtension -emit-symbol-graph -emit-symbol-graph-dir %t %t/Extension.swift
+// RUN: %FileCheck %s --input-file %t/SwiftAttrExtension@SwiftAttr.symbols.json
 
-//--- SwiftAttrExtension/module.modulemap
-module SwiftAttrExtension {
-  header "SwiftAttrExtension.h"
+//--- SwiftAttr/module.modulemap
+module SwiftAttr {
+  header "SwiftAttr.h"
 }
 
-//--- SwiftAttrExtension/SwiftAttrExtension.h
+//--- SwiftAttr/SwiftAttr.h
 @import Foundation;
 
 __attribute__((swift_attr("@_nonSendable(_assumed)")))
@@ -20,6 +20,8 @@ __attribute__((swift_attr("@_nonSendable(_assumed)")))
 @end
 
 //--- Extension.swift
+import SwiftAttr
+
 extension MyObjcClass {
   // CHECK: s:So11MyObjcClassC18SwiftAttrExtensionE8someFuncyyF
   public func someFunc() {}
