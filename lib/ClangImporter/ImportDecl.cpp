83eb3c2dff23535545fdd7f2ae9127211dd1f1cf
QuietMisdreavus <QuietMisdreavus@users.noreply.github.com>
2023-05-17 09:10:13 -0600

    [ClangImporter] don't add swift_attr files to a module's auxiliary files (#65867)
    rdar://107624995

--- lib/ClangImporter/ImportDecl.cpp
+++ lib/ClangImporter/ImportDecl.cpp
@@ -7561,7 +7561,6 @@ SourceFile &ClangImporter::Implementation::getClangSwiftAttrSourceFile(
   auto sourceFile = new (SwiftContext) SourceFile(
       module, SourceFileKind::Library, None);
   ClangSwiftAttrSourceFiles.insert({&module, sourceFile});
-  module.addAuxiliaryFile(*sourceFile);
   return *sourceFile;
 }
 
