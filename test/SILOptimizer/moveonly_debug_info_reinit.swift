01c8e52baf5eaad5ed4527396714ea1cf7734cb2
Joe Groff <jgroff@apple.com>
2023-05-17 20:39:54 -0700

    MoveOnlyAddressChecker: Reintroduce debug info for variables after reassignment.
    After a value is consumed, we emit a `debug_value undef` to indicate that the
    variable value is no longer valid to the debugger. However, once a value is
    reassigned, it becomes valid again, so emit a `debug_value %original_address` to
    reassociate the variable with the valid memory location. rdar://109218404

---
+++ test/SILOptimizer/moveonly_debug_info_reinit.swift
@@ -0,0 +1,41 @@
+// RUN: %target-swift-frontend -emit-sil -g %s | %FileCheck %s
+// RUN: %target-swift-frontend -DADDRESS_ONLY -emit-sil -g %s | %FileCheck %s
+
+struct Foo: ~Copyable {
+#if ADDRESS_ONLY
+    private var x: Any
+#else
+    private var x: Int
+#endif
+}
+
+@_silgen_name("use")
+func use(_: inout Foo)
+
+// CHECK-LABEL: sil {{.*}} @${{.*}}3bar
+func bar(_ x: consuming Foo, y: consuming Foo, z: consuming Foo) {
+    // CHECK: [[X:%.*]] = alloc_stack{{.*}} $Foo, var, name "x"
+
+    // CHECK: [[USE:%.*]] = function_ref @use
+    // CHECK: apply [[USE]]
+    // CHECK: debug_value undef : $*Foo, var, name "x"
+    use(&x)
+    let _ = x
+
+    // CHECK: debug_value undef : $*Foo, var, name "y"
+    // CHECK: debug_value [[X]] : $*Foo, var, name "x"
+    x = y
+    // CHECK: [[USE:%.*]] = function_ref @use
+    // CHECK: apply [[USE]]
+    // CHECK: debug_value undef : $*Foo, var, name "x"
+    use(&x)
+    let _ = x
+
+    // CHECK: debug_value undef : $*Foo, var, name "z"
+    // CHECK: debug_value [[X]] : $*Foo, var, name "x"
+    x = z
+    // CHECK: [[USE:%.*]] = function_ref @use
+    // CHECK: apply [[USE]]
+    // CHECK: debug_value undef : $*Foo, var, name "x"
+    use(&x)
+}
