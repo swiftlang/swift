4a18a44d2b3d19b4a91e845cfa6564eb7d8cb32c
Hamish Knight <hamish_github@mediocremail.com>
2023-05-20 00:32:21 +0100

    [IRGen] Update coverage mapping for version 6
    The coverage format version we use is automatically
    set to whatever the latest version LLVM has. This
    resulting in us outputting version 6 as our
    format without having made the changes necessary
    to reflect the new format. Update the emission
    logic to take account for [the new changes in
    version 6][1] (we need to include the current
    working directory as the first element of the list
    of files we output). Additionally, add a
    `static_assert` so we don't get caught out by
    version bumps in the future. Note we can't pin our
    version, as it must stay in sync with the version
    Clang is using.
    
    [1]: https://llvm.org/docs/CoverageMappingFormat.html#llvm-ir-representation

--- test/Profiler/coverage_relative_path.swift
+++ test/Profiler/coverage_relative_path.swift
@@ -1,15 +1,15 @@
 // To make sure this test is resilient to directory changes, we create nested directories inside of the
 // temporary test directory and assert those exist, or don't exist, in the emitted ir
 //
-// RUN: rm -rf %t
+// RUN: %empty-directory(%t)
 // RUN: mkdir -p %t/root/nested
 // RUN: echo "func coverage() {}" > %t/root/nested/coverage_relative_path.swift
 // RUN: cd %t/root
 
 // RUN: %target-swift-frontend -profile-generate -profile-coverage-mapping -Xllvm -enable-name-compression=false -emit-ir nested/coverage_relative_path.swift | %FileCheck -check-prefix=ABSOLUTE %s
 //
-// ABSOLUTE: @__llvm_coverage_mapping = {{.*"\\01.*root.*nested.*coverage_relative_path\.swift}}
+// ABSOLUTE: @__llvm_coverage_mapping = {{.*"\\02.*root[^/\\]*nested[/\\]*coverage_relative_path\.swift}}
 
 // RUN: %target-swift-frontend -profile-generate -profile-coverage-mapping -Xllvm -enable-name-compression=false -coverage-prefix-map %/t/root=. -emit-ir %/t/root/nested/coverage_relative_path.swift | %FileCheck -check-prefix=RELATIVE %s
 //
-// RELATIVE: @__llvm_coverage_mapping = {{.*"\\01[^/]*}}.{{/|\\}}nested{{.*coverage_relative_path\.swift}}
+// RELATIVE: @__llvm_coverage_mapping = {{.*"\\02.*\\01[^/]*\.[/\\]*nested[/\\]*coverage_relative_path\.swift}}
