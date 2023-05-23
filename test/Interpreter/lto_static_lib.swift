8f7e433c71065b686763b0356e5b11c55dcb43dd
Arnold Schwaighofer <aschwaighofer@apple.com>
2023-05-23 15:21:23 -0700

    Fix LTO test to use just use libLTO.dylib
    This is necessary until the Xcode toolchain's libLTO.dylib supports opaque pointers

--- test/Interpreter/lto_static_lib.swift
+++ test/Interpreter/lto_static_lib.swift
@@ -1,7 +1,7 @@
 // RUN: %empty-directory(%t)
 // RUN: %target-build-swift -parse-as-library -lto=llvm-thin %s -emit-bc -o %t/a.o %lto_flags
-// RUN: ar -r -s %t/archive.a %t/a.o
-// RUN: %target-clang %t/archive.a -isysroot %sdk -L%swift-lib-dir/swift/%target-sdk-name -flto -o %t/main
+// RUN: %use_just_built_liblto ar -r -s %t/archive.a %t/a.o
+// RUN: %use_just_built_liblto %target-clang %t/archive.a -isysroot %sdk -L%swift-lib-dir/swift/%target-sdk-name -flto -o %t/main
 // RUN: %target-run %t/main | %FileCheck %s
 
 // REQUIRES: executable_test
