db41d89fae87c72273476765beec3e3e71c4c6d3
Nate Chandler <nate_chandler@apple.com>
2023-05-24 16:50:36 -0700

    [StackNesting] Gardening: Fixed typos.

--- lib/SILOptimizer/Utils/StackNesting.cpp
+++ lib/SILOptimizer/Utils/StackNesting.cpp
@@ -95,7 +95,7 @@ bool StackNesting::solve() {
   // unreachable instructions (otherwise the backward data flow would be
   // sufficient). The special thing about unreachable-blocks is that it's
   // okay to have alive locations at that point, i.e. locations which are never
-  // dealloced. We cannot get such locations with a purly backward dataflow.
+  // dealloced. We cannot get such locations with a purely backward dataflow.
   do {
     changed = false;
 
@@ -120,7 +120,7 @@ bool StackNesting::solve() {
   } while (changed);
 
   // Second step: do a backward dataflow analysis to extend the lifetimes of
-  // no properly nested allocations.
+  // not properly nested allocations.
   do {
     changed = false;
 
@@ -151,7 +151,7 @@ bool StackNesting::solve() {
           int BitNr = bitNumberForAlloc(StackInst);
           if (Bits != StackLocs[BitNr].AliveLocs) {
             // More locations are alive around the StackInst's location.
-            // Update the AlivaLocs bitset, which contains all those alive
+            // Update the AliveLocs bitset, which contains all those alive
             // locations.
             assert(Bits.test(BitNr) && "no dealloc found for alloc stack");
             StackLocs[BitNr].AliveLocs = Bits;
@@ -317,7 +317,7 @@ StackNesting::Changes StackNesting::fixNesting(SILFunction *F) {
       return Changes::None;
 
     // Insert deallocs at block boundaries. This might be necessary in CFG sub
-    // graphs which don't  reach a function exit, but only an unreachable.
+    // graphs which don't reach a function exit, but only an unreachable.
     changes = SN.insertDeallocsAtBlockBoundaries();
     if (changes == Changes::None) {
       // Do the real work: extend lifetimes by moving deallocs.
