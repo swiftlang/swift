2ba7ca369b41575922083c8d3f053478679c271c
Nate Chandler <nate_chandler@apple.com>
2023-05-25 13:05:50 -0700

    [IRGen] Disable on-stack pack metadata via attr.
    When lowering a SIL function, if it bears the no_onstack_pack_metadata
    annotation, disable on-stack pack metadata.

--- lib/IRGen/IRGenSIL.cpp
+++ lib/IRGen/IRGenSIL.cpp
@@ -2355,6 +2355,9 @@ void IRGenSILFunction::emitSILFunction() {
   if (IGM.DebugInfo)
     IGM.DebugInfo->emitFunction(*CurSILFn, CurFn);
 
+  if (!CurSILFn->useStackForPackMetadata())
+    packMetadataStackPromotionDisabled = true;
+
   // Map the entry bb.
   LoweredBBs[&*CurSILFn->begin()] = LoweredBB(&CurFn->back(), {});
   // Create LLVM basic blocks for the other bbs.
