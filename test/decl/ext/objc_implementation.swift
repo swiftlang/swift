16853f869a7920c5ec667b38f9525bb498bf06f3
Becca Royal-Gordon <beccadax@apple.com>
2023-05-25 13:54:12 -0700

    [NFC] Correct new @objcImpl tests for 5.9

--- test/decl/ext/objc_implementation.swift
+++ test/decl/ext/objc_implementation.swift
@@ -39,10 +39,10 @@
   }
 
   var methodFromHeader5: CInt
-  // expected-error@-1 {{property 'methodFromHeader5' does not match the instance method declared by the header}}
+  // expected-warning@-1 {{property 'methodFromHeader5' does not match the instance method declared by the header}}
 
   func method(fromHeader6: Double) {
-    // expected-error@-1 {{instance method 'method(fromHeader6:)' of type '(Double) -> ()' does not match type '(Int32) -> Void' declared by the header}}
+    // expected-warning@-1 {{instance method 'method(fromHeader6:)' of type '(Double) -> ()' does not match type '(Int32) -> Void' declared by the header}}
   }
 
   var propertyFromHeader1: CInt
@@ -64,10 +64,10 @@
   }
 
   @objc let propertyFromHeader5: CInt
-  // expected-error@-1 {{property 'propertyFromHeader5' should be settable to match the settable property declared by the header}}
+  // expected-warning@-1 {{property 'propertyFromHeader5' should be settable to match the settable property declared by the header}}
 
   @objc var propertyFromHeader6: CInt {
-    // expected-error@-1 {{property 'propertyFromHeader6' should be settable to match the settable property declared by the header}}
+    // expected-warning@-1 {{property 'propertyFromHeader6' should be settable to match the settable property declared by the header}}
     get { return 1 }
   }
 
@@ -75,12 +75,12 @@
   // FIXME: Should complain about final not fulfilling the @objc requirement
 
   func propertyFromHeader10() -> CInt {
-    // expected-error@-1 {{instance method 'propertyFromHeader10()' does not match the property declared by the header}}
+    // expected-warning@-1 {{instance method 'propertyFromHeader10()' does not match the property declared by the header}}
     return 1
   }
 
   var propertyFromHeader11: Float
-  // expected-error@-1 {{property 'propertyFromHeader11' of type 'Float' does not match type 'Int32' declared by the header}}
+  // expected-warning@-1 {{property 'propertyFromHeader11' of type 'Float' does not match type 'Int32' declared by the header}}
 
   var readonlyPropertyFromHeader1: CInt
   // OK, provides an implementation with a stored property that's nonpublicly settable
@@ -154,7 +154,7 @@
   }
 
   class func classMethod3(_: Float) {
-    // expected-error@-1 {{class method 'classMethod3' of type '(Float) -> ()' does not match type '(Int32) -> Void' declared by the header}}
+    // expected-warning@-1 {{class method 'classMethod3' of type '(Float) -> ()' does not match type '(Int32) -> Void' declared by the header}}
   }
 
   func instanceMethod1(_: CInt) {
@@ -325,15 +325,15 @@
   func nullUnspecifiedResultAndArg(_: Any!) -> Any! { fatalError() } // no-error
 
   func nonnullResult1() -> Any { fatalError() } // no-error
-  func nonnullResult2() -> Any? { fatalError() } // expected-error {{instance method 'nonnullResult2()' of type '() -> Any?' does not match type '() -> Any' declared by the header}}
+  func nonnullResult2() -> Any? { fatalError() } // expected-warning {{instance method 'nonnullResult2()' of type '() -> Any?' does not match type '() -> Any' declared by the header}}
   func nonnullResult3() -> Any! { fatalError() } // no-error (special case)
 
   func nonnullArgument1(_: Any) {} // no-error
-  func nonnullArgument2(_: Any?) {} // expected-error {{instance method 'nonnullArgument2' of type '(Any?) -> ()' does not match type '(Any) -> Void' declared by the header}}
+  func nonnullArgument2(_: Any?) {} // expected-warning {{instance method 'nonnullArgument2' of type '(Any?) -> ()' does not match type '(Any) -> Void' declared by the header}}
   func nonnullArgument3(_: Any!) {} // no-error (special case)
 
-  func nullableResult() -> Any { fatalError() } // expected-error {{instance method 'nullableResult()' of type '() -> Any' does not match type '() -> Any?' declared by the header}}
-  func nullableArgument(_: Any) {} // expected-error {{instance method 'nullableArgument' of type '(Any) -> ()' does not match type '(Any?) -> Void' declared by the header}}
+  func nullableResult() -> Any { fatalError() } // expected-warning {{instance method 'nullableResult()' of type '() -> Any' does not match type '() -> Any?' declared by the header}}
+  func nullableArgument(_: Any) {} // expected-warning {{instance method 'nullableArgument' of type '(Any) -> ()' does not match type '(Any?) -> Void' declared by the header}}
 
   func nonPointerResult() -> CInt! { fatalError() } // expected-error{{method cannot be implicitly @objc because its result type cannot be represented in Objective-C}}
   func nonPointerArgument(_: CInt!) {} // expected-error {{method cannot be implicitly @objc because the type of the parameter cannot be represented in Objective-C}}
