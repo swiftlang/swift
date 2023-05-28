a52302d3845c8e81cd4c6e5e759b0968c9939abf
Michael Gottesman <mgottesman@apple.com>
2023-05-27 23:28:41 -0700

    Some misc fixes for tests.

--- test/Interpreter/moveonly_split_module_source_deinit.swift
+++ test/Interpreter/moveonly_split_module_source_deinit.swift
@@ -2,7 +2,7 @@
 
 // RUN: %empty-directory(%t)
 // RUN: %target-swiftc_driver -emit-module -module-name server -emit-module-path %t/server.swiftmodule %s %S/Inputs/moveonly_split_module_source_input.swift
-// RUN: %target-swiftc_driver  -emit-executable -module-name server -emit-module-path %t/server.swiftmodule %s %S/Inputs/moveonly_split_module_source_input.swift -o %t/server -Xlinker -add_ast_path -Xlinker %t/server.swiftmodule -Xlinker -alias -Xlinker _server_main -Xlinker _main
+// RUN: %target-swiftc_driver  -emit-executable -module-name server -emit-module-path %t/server.swiftmodule %s %S/Inputs/moveonly_split_module_source_input.swift -o %t/server
 // RUN: %target-codesign %t/server
 // RUN: %target-run %t/server | %FileCheck %s
 
