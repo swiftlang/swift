8123bfff545665ad0b44147c04c6f5296a0f610b
Alexis Laferrière <alaf@apple.com>
2023-06-02 13:20:54 -0700

    [Serialization] Fix serializing CompilerPluginExecutablePaths values
    rdar://110068048

--- lib/Serialization/Serialization.cpp
+++ lib/Serialization/Serialization.cpp
@@ -1150,7 +1150,7 @@ void Serializer::writeHeader(const SerializationOptions &options) {
 
         options_block::CompilerPluginExecutablePathLayout
           CompilerPluginExecutablePath(Out);
-        for (auto Arg : options.CompilerPluginLibraryPaths) {
+        for (auto Arg : options.CompilerPluginExecutablePaths) {
           CompilerPluginExecutablePath.emit(ScratchRecord, Arg);
         }
       }
