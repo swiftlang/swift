a890ec2c0f4859d777c7b2e589207519309c6779
Alastair Houghton <ahoughton@apple.com>
2023-06-06 11:04:06 +0100

    [Backtracing][Linux][Tests] Fix verify_all_overlays test.
    The `_Backtracing` module has a number of private implementation only
    imports that aren't used outside of the module and that don't require
    any additional libraries (hence they aren't relevant to the outside
    world).  `verify_all_overlays.py` needs to know about these when it
    does its test, because it loadas the module as the main module, which
    results in implementation only imports being required instead of
    ignored.
    
    rdar://110261712

--- validation-test/SIL/verify_all_overlays.py
+++ validation-test/SIL/verify_all_overlays.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # RUN: ${python} %s %target-swiftmodule-name %platform-sdk-overlay-dir \
+# RUN:     %swift_src_root \
 # RUN:     %target-sil-opt -sdk %sdk -enable-sil-verify-all \
 # RUN:       -F %sdk/System/Library/PrivateFrameworks \
 # RUN:       -F "%xcode-extra-frameworks-dir"
@@ -14,9 +15,11 @@ import sys
 
 target_swiftmodule_name = sys.argv[1]
 sdk_overlay_dir = sys.argv[2]
-sil_opt_invocation = sys.argv[3:]
+source_dir = sys.argv[3]
+sil_opt_invocation = sys.argv[4:]
 
 for module_file in os.listdir(sdk_overlay_dir):
+    extra_args = []
     module_name, ext = os.path.splitext(module_file)
     if ext != ".swiftmodule":
         continue
@@ -33,6 +36,12 @@ for module_file in os.listdir(sdk_overlay_dir):
     # TODO: fix the DifferentiationUnittest module.
     if module_name == "DifferentiationUnittest":
         continue
+    # Backtracing needs its own additional modules in the module path
+    if module_name == "_Backtracing":
+        extra_args = ["-I", os.path.join(source_dir, "stdlib",
+                                         "public", "Backtracing", "modules"),
+                      "-I", os.path.join(source_dir, "include")]
+
     print("# " + module_name)
 
     module_path = os.path.join(sdk_overlay_dir, module_file)
@@ -50,4 +59,5 @@ for module_file in os.listdir(sdk_overlay_dir):
     # We are deliberately discarding the output here; we're just making sure
     # it can be generated.
     subprocess.check_output(sil_opt_invocation +
-                            [module_path, "-module-name", module_name])
+                            [module_path, "-module-name", module_name] +
+                            extra_args)
