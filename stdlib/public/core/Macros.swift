2051971f66ac81f81774311ad7ecbb0d76e3505f
Doug Gregor <dgregor@apple.com>
2023-06-07 05:11:25 -0700

    Remove OptionSet macro from the standard library. (#66404)

--- stdlib/public/core/Macros.swift
+++ stdlib/public/core/Macros.swift
@@ -63,36 +63,6 @@ public macro column<T: ExpressibleByIntegerLiteral>() -> T =
 @freestanding(expression)
 public macro dsohandle() -> UnsafeRawPointer = Builtin.DSOHandleMacro
 
-/// Create an option set from a struct that contains a nested `Options` enum.
-///
-/// Attach this macro to a struct that contains a nested `Options` enum
-/// with an integer raw value. The struct will be transformed to conform to
-/// `OptionSet` by
-///   1. Introducing a `rawValue` stored property to track which options are set,
-///    along with the necessary `RawType` typealias and initializers to satisfy
-///    the `OptionSet` protocol. The raw type is specified after `@OptionSet`,
-///    e.g., `@OptionSet<UInt8>`.
-///   2. Introducing static properties for each of the cases within the `Options`
-///    enum, of the type of the struct.
-///
-/// The `Options` enum must have a raw value, where its case elements
-/// each indicate a different option in the resulting option set. For example,
-/// the struct and its nested `Options` enum could look like this:
-///
-///     @OptionSet<UInt8>
-///     struct ShippingOptions {
-///       private enum Options: Int {
-///         case nextDay
-///         case secondDay
-///         case priority
-///         case standard
-///       }
-///     }
-@attached(member, names: named(RawValue), named(rawValue), named(`init`), arbitrary)
-@attached(conformance)
-public macro OptionSet<RawType>() =
-  #externalMacro(module: "SwiftMacros", type: "OptionSetMacro")
-
 /// Produce the given warning message during compilation.
 @freestanding(declaration)
 public macro warning(_ message: String) = Builtin.WarningMacro
