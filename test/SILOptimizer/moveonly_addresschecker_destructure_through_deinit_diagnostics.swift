5ef0b316050c643bdd967c0f2826f7e73d6469a6
Michael Gottesman <mgottesman@apple.com>
2023-06-26 16:15:52 -0700

    [move-only] Emit an error if we /ever/ partially consume a noncopyable type.
    The reason why I am doing this is that this was not part of the original
    evolution proposal (it was called an extension) and after some discussion it was
    realized that partial consumption would benefit from discussion on the forums.
    
    rdar://111353459
    (cherry picked from commit 3dde9df468a7d3a318adc0b9079fe002588b4999)

--- test/SILOptimizer/moveonly_addresschecker_destructure_through_deinit_diagnostics.swift
+++ test/SILOptimizer/moveonly_addresschecker_destructure_through_deinit_diagnostics.swift
@@ -1,4 +1,4 @@
-// RUN: %target-swift-emit-sil -sil-verify-all -verify -enable-experimental-feature MoveOnlyClasses -enable-experimental-feature MoveOnlyTuples %s
+// RUN: %target-swift-emit-sil -enable-experimental-feature MoveOnlyPartialConsumption -sil-verify-all -verify -enable-experimental-feature MoveOnlyClasses -enable-experimental-feature MoveOnlyTuples %s
 
 // This test validates that we properly emit errors if we partially invalidate
 // through a type with a deinit.
