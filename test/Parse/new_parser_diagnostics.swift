8e73c16aa58ae7e0f8f017c7ef4c422c68ff61e6
Alex Hoppen <ahoppen@apple.com>
2023-06-27 09:13:43 +0200

    [Parse] Fix a typo in the new parser diagnostics

--- test/Parse/new_parser_diagnostics.swift
+++ test/Parse/new_parser_diagnostics.swift
@@ -5,5 +5,5 @@
 // REQUIRES: asserts
 
 _ = [(Int) -> async throws Int]()
-// expected-error@-1{{'async throws' must preceed '->'}}
+// expected-error@-1{{'async throws' must precede '->'}}
 // expected-note@-2{{move 'async throws' in front of '->'}}{{15-21=}} {{21-28=}} {{20-21= }} {{12-12=async }} {{12-12=throws }}
