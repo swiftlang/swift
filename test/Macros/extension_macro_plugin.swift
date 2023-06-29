d8e0f3cbcd8be560a4638da0da6e9f4d5222030e
Holly Borla <hborla@apple.com>
2023-06-28 23:14:20 -0700

    [Macros] Add a macro plugin test for extension macros.
    (cherry picked from commit 7e85ebdac81c8798aa0b761bacb1951977641b46)

---
+++ test/Macros/extension_macro_plugin.swift
@@ -0,0 +1,57 @@
+// FIXME: Swift parser is not enabled on Linux CI yet.
+// REQUIRES: OS=macosx
+
+// RUN: %empty-directory(%t)
+// RUN: %empty-directory(%t/plugins)
+//
+//== Build the plugin library
+// RUN: %host-build-swift \
+// RUN:   -swift-version 5 \
+// RUN:   -emit-library \
+// RUN:   -o %t/plugins/%target-library-name(MacroDefinition) \
+// RUN:   -module-name=MacroDefinition \
+// RUN:   %S/Inputs/syntax_macro_definitions.swift \
+// RUN:   -g -no-toolchain-stdlib-rpath
+
+// RUN: SWIFT_DUMP_PLUGIN_MESSAGING=1 %swift-target-frontend \
+// RUN:   -typecheck -verify \
+// RUN:   -swift-version 5 -enable-experimental-feature ExtensionMacros \
+// RUN:   -external-plugin-path %t/plugins#%swift-plugin-server \
+// RUN:   -module-name MyApp \
+// RUN:   %s \
+// RUN:   2>&1 | tee %t/macro-expansions.txt
+
+// RUN: %FileCheck %s < %t/macro-expansions.txt
+
+@attached(extension, conformances: P, names: named(requirement))
+macro DelegatedConformance() = #externalMacro(module: "MacroDefinition", type: "DelegatedConformanceMacro")
+
+protocol P {
+  static func requirement()
+}
+
+struct Wrapped: P {
+  static func requirement() {
+    print("Wrapped.requirement")
+  }
+}
+
+@DelegatedConformance
+struct Generic<Element> {}
+
+// CHECK: {"expandMacroResult":{"diagnostics":[],"expandedSource":"extension Generic: P where Element: P {\n  static func requirement() {\n    Element.requirement()\n  }\n}"}}
+
+func requiresP(_ value: (some P).Type) {
+  value.requirement()
+}
+
+requiresP(Generic<Wrapped>.self)
+
+struct Outer {
+  @DelegatedConformance
+  struct Nested<Element> {}
+}
+
+// CHECK: {"expandMacroResult":{"diagnostics":[],"expandedSource":"extension Outer.Nested: P where Element: P {\n  static func requirement() {\n    Element.requirement()\n  }\n}"}}
+
+requiresP(Outer.Nested<Wrapped>.self)
