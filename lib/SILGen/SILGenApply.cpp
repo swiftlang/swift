e14f2bc0c7a3fec0188dbbe5722c43590cf5f36b
John McCall <rjmccall@apple.com>
2023-06-29 14:49:59 -0400

    [NFC] Add a method to just ask if a tuple AP vanishes under substitution

--- lib/SILGen/SILGenApply.cpp
+++ lib/SILGen/SILGenApply.cpp
@@ -3417,8 +3417,7 @@ private:
     // If the original parameter type is a vanishing tuple, we want to emit
     // this as if the argument source was wrapped in an extra level of
     // tuple literal.
-    bool origTupleVanishes =
-      origParamType.getVanishingTupleElementPatternType().has_value();
+    bool origTupleVanishes = origParamType.doesTupleVanish();
 
     auto substType = arg.getSubstRValueType();
 
