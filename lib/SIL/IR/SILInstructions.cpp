0718217993148dabced9df1a316383a42111230b
Pavel Yaskevich <pyaskevich@apple.com>
2023-07-04 00:06:27 -0700

    [SIL] InitAccessors: Add a way to retrieve a name of the property referenced by `assign_or_init` instruction
    This is going to be used by DI diagnostics.
    
    (cherry picked from commit a736e183c40b5bd8630748bb7d6b5c71430ff20e)

--- lib/SIL/IR/SILInstructions.cpp
+++ lib/SIL/IR/SILInstructions.cpp
@@ -1283,6 +1283,12 @@ bool AssignOrInitInst::isPropertyAlreadyInitialized(unsigned propertyIdx) {
   return Assignments.test(propertyIdx);
 }
 
+StringRef AssignOrInitInst::getPropertyName() const {
+  auto *accessor = getReferencedInitAccessor();
+  assert(accessor);
+  return cast<VarDecl>(accessor->getStorage())->getNameStr();
+}
+
 AccessorDecl *AssignOrInitInst::getReferencedInitAccessor() const {
   SILValue initRef = getInitializer();
   SILFunction *accessorFn = nullptr;
