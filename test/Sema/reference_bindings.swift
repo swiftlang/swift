f2bfa1cbac9aef926cfc0d5cb30e8ea2151d346c
Michael Gottesman <mgottesman@apple.com>
2023-07-10 11:26:26 -0700

    [reference-bindings] Add Sema checks for inout reference bindings to make sure we can only attach to lvalues
    rdar://112029192

---
+++ test/Sema/reference_bindings.swift
@@ -0,0 +1,54 @@
+// RUN: %target-typecheck-verify-swift -enable-experimental-feature ReferenceBindings
+
+var globalValue = String()
+class Klass {
+    var sStored: String = ""
+    var sGetter: String { fatalError() }
+    var sModify: String {
+        _read {
+            fatalError()
+        }
+        _modify {
+            fatalError()
+        }
+    }
+}
+
+struct S : ~Copyable {
+    var sStored: String = ""
+    var sGetter: String { fatalError() }
+    var sModify: String {
+        _read {
+            fatalError()
+        }
+        _modify {
+            fatalError()
+        }
+    }    
+}
+
+func foo(_ arg: String, _ consumingArg: consuming String, _ borrowingArg: borrowing String, _ inoutArg: inout String) {
+    let letValue = 5
+    var varValue = 5
+
+    inout noInitialValue: String // expected-error {{inout bindings must have an initial value}}
+    inout literalX = 5 // expected-error {{inout bindings must be bound to an lvalue}}
+    inout consumingArgX = consumingArg
+    inout borrowingArgX = borrowingArg // expected-error {{inout bindings must be bound to an lvalue}}
+    inout inoutArgX = inoutArg
+    inout letValueX = letValue // expected-error {{inout bindings must be bound to an lvalue}}
+    inout varValueX = varValue
+    inout globalValueX = globalValue
+    let k = Klass()
+    inout storedKlassFieldX = k.sStored
+    inout getterKlassFieldX = k.sGetter // expected-error {{inout bindings must be bound to an lvalue}}
+    inout modifyKlassFieldX = k.sModify
+    let letStruct = S()
+    inout storedStructLetFieldX = letStruct.sStored // expected-error {{inout bindings must be bound to an lvalue}}
+    inout getterStructLetFieldX = letStruct.sGetter // expected-error {{inout bindings must be bound to an lvalue}}
+    inout modifyStructLetFieldX = letStruct.sModify // expected-error {{inout bindings must be bound to an lvalue}}
+    var varStruct = S()
+    inout storedStructVarFieldX = varStruct.sStored
+    inout getterStructVarFieldX = varStruct.sGetter // expected-error {{inout bindings must be bound to an lvalue}}
+    inout modifyStructVarFieldX = varStruct.sModify
+}
