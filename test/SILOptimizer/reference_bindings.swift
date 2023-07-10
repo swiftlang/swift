adeae2f63e5d8c0109d9929c1b4d92fb620ae823
Michael Gottesman <mgottesman@apple.com>
2023-07-10 11:27:30 -0700

    [reference-bindings] Inout Binding ensure that we handle fields correctly
    I also:
    
    1. Added some basic SILGen/SILOptimizer/Interpreter tests.
    2. Added some logging and cleaned up the pass a little bit.
    
    rdar://112033023

---
+++ test/SILOptimizer/reference_bindings.swift
@@ -0,0 +1,31 @@
+// RUN: %target-swift-frontend -verify -sil-verify-all -enable-experimental-feature ReferenceBindings -emit-sil %s
+
+func varBindingTest() {
+    var x = "1"
+    x = "1"
+    inout x1 = x // expected-warning {{variable 'x1' was never mutated; consider changing to 'let' constant}}
+    // expected-error @-1 {{overlapping accesses to 'x', but modification requires exclusive access; consider copying to a local variable}}
+    let _ = x1
+    inout x2 = x // expected-note {{conflicting access is here}}
+    // expected-warning @-1 {{variable 'x2' was never mutated; consider changing to 'let' constant}}
+    let _ = x2
+}
+
+func varBindingTest2() {
+    var x = "1"
+    x = "1"
+    inout x1 = x // expected-warning {{variable 'x1' was never mutated; consider changing to 'let' constant}}
+    // expected-error @-1 {{overlapping accesses to 'x', but modification requires exclusive access; consider copying to a local variable}}
+    let _ = x1
+    x = "2" // expected-note {{conflicting access is here}}
+}
+
+func varBindingTest3() {
+    var x = "1"
+    x = "1"
+    do {
+      inout x1 = x // expected-warning {{variable 'x1' was never mutated; consider changing to 'let' constant}}
+      let _ = x1
+    }
+    x = "2"
+}
