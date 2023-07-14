80d6b0c49efeb9b478ee5dac47cdd146fbf1a778
Joe Groff <jgroff@apple.com>
2023-07-14 14:21:57 -0700

    SILGen: Don't reuse the `Initialization` across branches of an `if` or `switch` expression.
    `Initialization` is stateful and not meant to be emitted into multiple times across different contexts.
    If emitting into an initialization causes it to be split or aborted, that will carry over into
    further uses of the initialization. This was happening during `if` and `switch` expression
    emission, leading to miscompiles or compiler crashes. Fix this by saving only the buffer when
    we prepare emission for a statement expression, and creating the initialization in the scope
    where the expression for a branch actually gets emitted. Fixes rdar://112213253.

--- lib/SILGen/SILGenFunction.cpp
+++ lib/SILGen/SILGenFunction.cpp
@@ -1650,7 +1650,7 @@ void SILGenFunction::emitGeneratorFunction(
   mergeCleanupBlocks();
 }
 
-Initialization *SILGenFunction::getSingleValueStmtInit(Expr *E) {
+std::unique_ptr<Initialization> SILGenFunction::getSingleValueStmtInit(Expr *E) {
   if (SingleValueStmtInitStack.empty())
     return nullptr;
 
@@ -1659,7 +1659,15 @@ Initialization *SILGenFunction::getSingleValueStmtInit(Expr *E) {
   if (!SingleValueStmtInitStack.back().Exprs.contains(E))
     return nullptr;
 
-  return SingleValueStmtInitStack.back().Init;
+  // This won't give us a useful diagnostic if the result doesn't end up
+  // initialized ("variable '<unknown>' used before being initialized"), but it
+  // will at least catch a potential miscompile when the SIL verifier is
+  // disabled.
+  auto resultAddr = SingleValueStmtInitStack.back().InitializationBuffer;
+  resultAddr = B.createMarkUninitialized(
+      E, resultAddr, MarkUninitializedInst::Kind::Var);
+      
+  return std::make_unique<KnownAddressInitialization>(resultAddr);
 }
 
 void SILGenFunction::emitProfilerIncrement(ASTNode Node) {
