64401aeeeaf2985b3c048f84edf36405048644a8
Egor Zhdan <e_zhdan@apple.com>
2023-07-20 19:37:58 +0100

    [ClangImporter] NFC: Remove unused definitions

--- lib/ClangImporter/SwiftLookupTable.h
+++ lib/ClangImporter/SwiftLookupTable.h
@@ -527,9 +527,6 @@ public:
   /// Retrieve the set of base names that are stored in the lookup table.
   SmallVector<SerializedSwiftName, 4> allBaseNames();
 
-  /// Retrieve the set of base names that are stored in the globals-as-members lookup table.
-  SmallVector<SerializedSwiftName, 4> allGlobalsAsMembersBaseNames();
-
   /// Lookup Objective-C members with the given base name, regardless
   /// of context.
   SmallVector<clang::NamedDecl *, 4>
@@ -560,11 +557,6 @@ public:
   /// Retrieve the set of global declarations that are going to be
   /// imported as members.
   SmallVector<SingleEntry, 4> allGlobalsAsMembers();
-
-  /// Retrieve the set of base names that are going to be imported as members in the
-  /// given context.
-  SmallVector<SerializedSwiftName, 4>
-  globalsAsMembersBaseNames(EffectiveClangContext context);
 
   /// Deserialize all entries.
   void deserializeAll();
