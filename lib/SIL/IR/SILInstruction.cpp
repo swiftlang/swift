bd9daaecb03e7c70c833168af044eaf1108ffa5a
Nate Chandler <nate_chandler@apple.com>
2023-07-24 13:47:08 -0700

    [IRGen] NFC: Unified unary instructions.
    In preparation for adding addition unary instructions which
    `mayRequirePackMetadata`, group the instructions which already may
    produce pack metadata depending on their single operand's type together.

--- lib/SIL/IR/SILInstruction.cpp
+++ lib/SIL/IR/SILInstruction.cpp
@@ -1314,17 +1314,15 @@ bool SILInstruction::mayRequirePackMetadata() const {
     }
     return false;
   }
-  case SILInstructionKind::DebugValueInst: {
-    auto *dvi = cast<DebugValueInst>(this);
-    return dvi->getOperand()->getType().hasPack();
+  case SILInstructionKind::ClassMethodInst:
+  case SILInstructionKind::DebugValueInst: 
+  // Unary instructions.
+  {
+    return getOperand(0)->getType().hasPack();
   }
   case SILInstructionKind::MetatypeInst: {
     auto *mi = cast<MetatypeInst>(this);
     return mi->getType().hasPack();
-  }
-  case SILInstructionKind::ClassMethodInst: {
-    auto *cmi = cast<ClassMethodInst>(this);
-    return cmi->getOperand()->getType().hasPack();
   }
   case SILInstructionKind::WitnessMethodInst: {
     auto *wmi = cast<WitnessMethodInst>(this);
