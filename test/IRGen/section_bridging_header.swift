7b36b22378f51a3a07a1081c373cb570ef09105e
Kuba Mracek <mracek@apple.com>
2023-07-27 10:30:22 -0700

    Add a test exercising placing a struct with C function pointers into a custom section

---
+++ test/IRGen/section_bridging_header.swift
@@ -0,0 +1,23 @@
+// RUN: %empty-directory(%t)
+// RUN: %{python} %utils/split_file.py -o %t %s
+// RUN: %target-swift-frontend -enable-experimental-feature SymbolLinkageMarkers -enforce-exclusivity=unchecked -parse-as-library -emit-sil -import-objc-header %t/bridge.h %t/file.swift -o /dev/null
+
+// REQUIRES: swift_in_compiler
+
+// BEGIN bridge.h
+struct MyStruct1 {
+	int x;
+	void (*fptr1)();
+	void (*fptr2)(int);
+	void (*fptr3)(int, char, void *);
+};
+
+// BEGIN file.swift
+
+func foo() { }
+@_section("__TEXT,__mysection") var my_global1 = MyStruct1(
+	x: 42,
+	fptr1: foo,
+	fptr2: nil,
+	fptr3: { arg1, arg2, arg3 in print(arg1, arg2, arg3) }
+	)
