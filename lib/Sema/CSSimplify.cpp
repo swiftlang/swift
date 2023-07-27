3140c12691100e2b27d7cb40321672aba05ab7bd
Amritpan Kaur <you@example.com>
2023-07-26 18:36:34 -0700

    [CSSimplify] If keypath is malformed, do not attempt to

--- lib/Sema/CSSimplify.cpp
+++ lib/Sema/CSSimplify.cpp
@@ -11550,14 +11550,17 @@ bool ConstraintSystem::resolveClosure(TypeVariableType *typeVar,
 bool ConstraintSystem::resolveKeyPath(TypeVariableType *typeVar,
                                       Type contextualType,
                                       ConstraintLocatorBuilder locator) {
-
+  auto *keyPathLocator = typeVar->getImpl().getLocator();
+  auto *keyPath = castToExpr<KeyPathExpr>(keyPathLocator->getAnchor());
+  if (keyPath->hasSingleInvalidComponent()) {
+    assignFixedType(typeVar, contextualType);
+    return true;
+  }
   if (auto *BGT = contextualType->getAs<BoundGenericType>()) {
     auto args = BGT->getGenericArgs();
     if (isKnownKeyPathType(contextualType) && args.size() >= 1) {
       auto root = BGT->getGenericArgs()[0];
 
-      auto *keyPathLocator = typeVar->getImpl().getLocator();
-      auto *keyPath = castToExpr<KeyPathExpr>(keyPathLocator->getAnchor());
       auto *keyPathValueTV = getKeyPathValueType(keyPath);
       contextualType = BoundGenericType::get(
           args.size() == 1 ? getASTContext().getKeyPathDecl() : BGT->getDecl(),
