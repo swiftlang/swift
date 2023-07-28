9c22a7a5302c96ed82d712caac06b2e07c2e2f67
Egor Zhdan <e_zhdan@apple.com>
2023-07-28 19:53:59 +0100

    [cxx-interop] Memberwise init is not synthesized for C++ type with templated using decl
    When Swift fails to import a member of a struct, it checks to see if this member could affect the memory layout of the struct, and if it can, Swift doesn't synthesize the memberwise initializer for this struct. This logic was overly restrictive and treated templated using-decls as potentially affecting the memory layout of the struct.
    
    rdar://113044949

--- test/Interop/Cxx/class/Inputs/memberwise-initializer.h
+++ test/Interop/Cxx/class/Inputs/memberwise-initializer.h
@@ -1,6 +1,10 @@
 #ifndef TEST_INTEROP_CXX_CLASS_INPUTS_MEMBERWISE_INITIALIZER_H
 #define TEST_INTEROP_CXX_CLASS_INPUTS_MEMBERWISE_INITIALIZER_H
 
+template <typename T>
+struct TemplatedType {};
+
+
 struct StructPrivateOnly {
 private:
   int varPrivate;
@@ -50,6 +54,22 @@ struct ClassWithUnimportedMemberFunction {
 public:
   int varPublic;
   int ClassWithUnimportedMemberFunction::* unimportedMemberFunction();
+};
+
+struct ClassWithTemplatedFunction {
+public:
+  int varPublic;
+
+  template <int I>
+  void foo();
+};
+
+struct ClassWithTemplatedUsingDecl {
+public:
+  int varPublic;
+
+  template <typename T>
+  using MyUsing = TemplatedType<T>;
 };
 
 #endif
