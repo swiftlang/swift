a445cf29bf2360b1ae391e421b1fd77d5a0c6723
Alejandro Alonso <alejandro_alonso@apple.com>
2023-08-03 15:17:41 -0700

    Use a simpler interpreter test

--- test/Interpreter/raw_layout.swift
+++ test/Interpreter/raw_layout.swift
@@ -1,69 +1,20 @@
-// RUN: %target-run-simple-swift(-enable-experimental-feature RawLayout -enable-builtin-module) | %FileCheck %s
+// RUN: %target-run-simple-swift(-enable-experimental-feature RawLayout) | %FileCheck %s
 // REQUIRES: executable_test
 
-import Builtin
-
 @_rawLayout(like: T)
-struct Cell<T>: ~Copyable {
-  var address: UnsafeMutablePointer<T> {
-    UnsafeMutablePointer<T>(Builtin.unprotectedAddressOfBorrow(self))
-  }
+struct Cell<T>: ~Copyable {}
 
-  init(_ value: consuming T) {
-    address.initialize(to: value)
-  }
-
-  deinit {
-    // FIXME: discard self should work with rawLayout types
-    //address.deinitialize(count: 1)
-
-    // Note: We don't need to deallocate the address here because the memory it
-    // points to is being destroyed within this deinit.
-  }
-
-  borrowing func replace(with replacement: consuming T) -> T {
-    let previous = address.move()
-    address.initialize(to: replacement)
-    return previous
-  }
-
-  borrowing func set(_ value: consuming T) {
-    let previous = replace(with: value)
-    _ = consume previous
-  }
-
-  consuming func get() -> T {
-    // Move the value out of self and don't call our deinitializer
-    let previous = address.move()
-    discard self
-    return previous
-  }
+struct Foo<T>: ~Copyable {
+  let cell = Cell<T>()
+  let myValue = 123
 }
 
-@_eagerMove
-class SpecialInt {
-  var value: Int
-
-  init(_ value: Int) {
-    self.value = value
-  }
-
-  deinit {
-    print("Deinitializing \(value)!")
-  }
+func something<T>(_ x: borrowing Foo<T>) -> Int {
+  x.myValue
 }
 
-do {
-  let specialInt0 = SpecialInt(128)
-  let cell0 = Cell<SpecialInt>(specialInt0)
+// CHECK: 123
+print(something(Foo<Bool>()))
 
-  let specialInt0Again = cell0.replace(with: SpecialInt(316))
-
-  // CHECK: Deinitializing 316!
-  cell0.set(SpecialInt(592))
-
-  let specialInt1 = cell0.get()
-
-  // CHECK: Deinitializing 592!
-  // CHECK: Deinitializing 128!
-}
+// CHECK: 123
+print(something(Foo<Int>()))
