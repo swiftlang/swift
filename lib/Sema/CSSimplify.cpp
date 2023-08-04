35fdbd049aa2e2c8203a4012c71200866ca42ed1
Pavel Yaskevich <pyaskevich@apple.com>
2023-08-04 11:20:29 -0700

    [CSSimplify] Avoid eager matching between pack expansion and its contextual type
    This is effectively a premature optimization. Contextual type might
    not be fully resolved yet, so let's give solver a chance to do it for us.
    
    (cherry picked from commit 3dff3e5a49ebfa1caa6f520e23a78b43a09ea7a0)

--- lib/Sema/CSSimplify.cpp
+++ lib/Sema/CSSimplify.cpp
@@ -11501,17 +11501,6 @@ bool ConstraintSystem::resolvePackExpansion(TypeVariableType *typeVar,
           .getOpenedType();
 
   assignFixedType(typeVar, openedExpansionType, locator);
-
-  // We have a fully resolved contextual pack expansion type, let's
-  // apply it right away.
-  if (!contextualType->isEqual(openedExpansionType)) {
-    assert(contextualType->is<PackExpansionType>() &&
-           !contextualType->hasTypeVariable());
-    auto result = matchTypes(openedExpansionType, contextualType,
-                             ConstraintKind::Equal, {}, locator);
-    return !result.isFailure();
-  }
-
   return true;
 }
 
