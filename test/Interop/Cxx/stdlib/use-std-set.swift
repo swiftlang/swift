8fd4e98e8acf44951f83a78c75af68e6c1cc73a3
Alex Lorenz <aleksei_lorenz@apple.com>
2023-08-10 12:42:57 -0700

    [cxx-interop] enable use-std-set test on windows

--- test/Interop/Cxx/stdlib/use-std-set.swift
+++ test/Interop/Cxx/stdlib/use-std-set.swift
@@ -1,9 +1,6 @@
 // RUN: %target-run-simple-swift(-I %S/Inputs -Xfrontend -enable-experimental-cxx-interop)
 //
 // REQUIRES: executable_test
-//
-// Enable this everywhere once we have a solution for modularizing other C++ stdlibs: rdar://87654514
-// REQUIRES: OS=macosx || OS=linux-gnu
 
 import StdlibUnittest
 import StdSet
