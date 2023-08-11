d915cf37cb9e2bee13eec043fd859ad2c7d40737
Michael Gottesman <mgottesman@apple.com>
2023-08-11 16:07:15 -0700

    [reference-bindings] Add some more tests.

--- test/SILGen/reference_bindings.swift
+++ test/SILGen/reference_bindings.swift
@@ -1,6 +1,11 @@
 // RUN: %target-swift-frontend -emit-silgen -enable-experimental-feature ReferenceBindings -o - %s | %FileCheck %s
 // RUN: %target-swift-frontend -emit-sil -sil-verify-all -enable-experimental-feature ReferenceBindings -o - %s | %FileCheck -check-prefix=SIL %s
 
+class Klass {}
+struct S {
+  var k = Klass()
+}
+
 func doSomething() {}
 
 // Before the transformation, our access scope is not to end of scope... so
@@ -36,4 +41,33 @@ func testBindToVar() {
     var x = 5
     inout x2 = x
     doSomething()
+}
+
+// CHECK-LABEL: sil hidden [ossa] @$s18reference_bindings15testBindToInOutyySSzF : $@convention(thin) (@inout String) -> () {
+// CHECK: bb0([[ARG:%.*]] : $*String):
+// CHECK:   [[BOX:%.*]] = alloc_box ${ var String }
+// CHECK:   [[MARK:%.*]] = mark_unresolved_reference_binding [inout] [[BOX]]
+// CHECK:   [[PROJECT:%.*]] = project_box [[MARK]]
+// CHECK:   [[ACCESS:%.*]] = begin_access [read] [unknown] [[ARG]]
+// CHECK:   copy_addr [[ACCESS]] to [init] [[PROJECT]]
+// CHECK:   end_access [[ACCESS]]
+// CHECK:   apply {{%.*}}()
+// CHECK:   destroy_value [[MARK]]
+// CHECK: } // end sil function '$s18reference_bindings15testBindToInOutyySSzF'
+
+// SIL-LABEL: sil hidden @$s18reference_bindings15testBindToInOutyySSzF : $@convention(thin) (@inout String) -> () {
+// SIL: bb0([[ARG:%.*]] : $*String):
+// SIL:   [[STACK:%.*]] = alloc_stack $String
+// SIL:   [[ACCESS:%.*]] = begin_access [modify] [static] [[ARG]]
+// SIL:   [[VAL:%.*]] = load [[ACCESS]]
+// SIL:   store [[VAL]] to [[STACK]]
+// SIL:   apply {{%.*}}
+// SIL:   [[VAL:%.*]] = load [[STACK]]
+// SIL:   store [[VAL]] to [[ACCESS]]
+// SIL:   end_access [[ACCESS]]
+// SIL:   dealloc_stack [[STACK]]
+// SIL: } // end sil function '$s18reference_bindings15testBindToInOutyySSzF'
+func testBindToInOut(_ x: inout String) {
+  inout y = x
+  doSomething()
 }
