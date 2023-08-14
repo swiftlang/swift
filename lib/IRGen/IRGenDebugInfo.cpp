af6599ff312dd1fe55b152b54cec3312758f9a38
Felipe de Azevedo Piovezan <fpiovezan@apple.com>
2023-08-14 10:02:22 -0400

    [IRGen] Delete no-op code
    Commit 5c3ccf505065 added a number of blocks of code which were partially
    removed by 39af79f491b. This partial remove left lines of code doing nothin; we
    remove them here.

--- lib/IRGen/IRGenDebugInfo.cpp
+++ lib/IRGen/IRGenDebugInfo.cpp
@@ -2987,11 +2987,6 @@ void IRGenDebugInfoImpl::emitGlobalVariableDeclaration(
   if (Opts.DebugInfoLevel <= IRGenDebugInfoLevel::LineTables)
     return;
 
-  if (swift::TypeBase *ty = DbgTy.getType()) {
-    if (MetatypeType *metaTy = dyn_cast<MetatypeType>(ty))
-      ty = metaTy->getInstanceType().getPointer();
-  }
-
   llvm::DIType *DITy = getOrCreateType(DbgTy);
   VarDecl *VD = nullptr;
   if (Loc)
