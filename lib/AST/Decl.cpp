e2db4d221cfc7caf635042b029e5b22c9ba17543
Joe Groff <jgroff@apple.com>
2023-08-16 14:19:34 -0700

    Revert "AST: Spot fix for AbstractStorageDecl::isResilient()"
    This reverts commit d5b354fd5f93a2ce9fce9e55f24d993b1bef5381. It causes miscompiles when
    accessing properties declared with `@_originallyDefinedIn` that are now defined in modules
    with library evolution enabled from the module that the property was originally defined in.
    Just because the property used to be declared in the current module doesn't mean it can
    bypass the stable ABI of the module that the property now belongs to.
    
    It looks like the logic that this PR replaced is also faulty, since `@_originallyDefinedIn`
    really oughtn't factor into the resilience computation at any level, but let's unwind one
    level of brokenness at a time.
    
    Fixes rdar://113935401.

--- lib/AST/Decl.cpp
+++ lib/AST/Decl.cpp
@@ -2810,26 +2810,13 @@ bool AbstractStorageDecl::isResilient() const {
   return getModuleContext()->isResilient();
 }
 
-static bool isOriginallyDefinedIn(const Decl *D, const ModuleDecl* MD) {
-  if (!MD)
-    return false;
-  if (D->getAlternateModuleName().empty())
-    return false;
-  return D->getAlternateModuleName() == MD->getName().str();
-}
-
 bool AbstractStorageDecl::isResilient(ModuleDecl *M,
                                       ResilienceExpansion expansion) const {
   switch (expansion) {
   case ResilienceExpansion::Minimal:
     return isResilient();
   case ResilienceExpansion::Maximal:
-    // We consider this decl belongs to the module either it's currently
-    // defined in this module or it's originally defined in this module, which
-    // is specified by @_originallyDefinedIn
-    return (M != getModuleContext() &&
-            !isOriginallyDefinedIn(this, M) &&
-            isResilient());
+    return M != getModuleContext() && isResilient();
   }
   llvm_unreachable("bad resilience expansion");
 }
@@ -4782,6 +4769,14 @@ DestructorDecl *NominalTypeDecl::getValueTypeDestructor() {
   return cast<DestructorDecl>(found[0]);
 }
 
+static bool isOriginallyDefinedIn(const Decl *D, const ModuleDecl* MD) {
+  if (!MD)
+    return false;
+  if (D->getAlternateModuleName().empty())
+    return false;
+  return D->getAlternateModuleName() == MD->getName().str();
+}
+
 bool NominalTypeDecl::isResilient(ModuleDecl *M,
                                   ResilienceExpansion expansion) const {
   switch (expansion) {
@@ -4791,9 +4786,8 @@ bool NominalTypeDecl::isResilient(ModuleDecl *M,
     // We consider this decl belongs to the module either it's currently
     // defined in this module or it's originally defined in this module, which
     // is specified by @_originallyDefinedIn
-    return (M != getModuleContext() &&
-            !isOriginallyDefinedIn(this, M) &&
-            isResilient());
+    return M != getModuleContext() && !isOriginallyDefinedIn(this, M) &&
+      isResilient();
   }
   llvm_unreachable("bad resilience expansion");
 }
