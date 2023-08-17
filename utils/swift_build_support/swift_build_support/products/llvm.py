efce129650d503820f3ba0e21924e2020d4dda9e
Eric Miotto <1094986+edymtt@users.noreply.github.com>
2023-08-17 07:36:41 -0700

    Use LLVM runtimes build
    LLVM plans to remove the legacy methods for building compiler-rt. This change updates build-script to use LLVM_ENABLE_RUNTIMES to build compiler-rt with the just-built clang instead of the legacy methods.
    
    This does not update the llvm-install-components in
    build-presets.ini -- this is done on purpose to stress more the
    migration logic, some of the presets will be updated with a subsequent PRs
    
    Addresses rdar://113972453

--- utils/swift_build_support/swift_build_support/products/llvm.py
+++ utils/swift_build_support/swift_build_support/products/llvm.py
@@ -217,6 +217,22 @@ class LLVM(cmake_product.CMakeProduct):
         # LLVM will always be built in part
         return True
 
+    @classmethod
+    def compute_cmake_args_for_runtimes(cls, host_target):
+        # Swift expects the old layout for the runtime directory
+        args_for_runtime = '-DLLVM_ENABLE_PER_TARGET_RUNTIME_DIR:BOOL=OFF'
+
+        if host_target.startswith('linux'):
+            # This preserves the behaviour we had when using
+            # LLVM_BUILD_EXTERNAL COMPILER_RT --
+            # that is, having the linker not complaing if symbols used
+            # by TSan are undefined (namely the ones for Blocks Runtime)
+            # In the long term, we want to remove this and
+            # build Blocks Runtime before LLVM
+            args_for_runtime += ';-DSANITIZER_COMMON_LINK_FLAGS:STRING=-Wl,-z,undefs'
+
+        return args_for_runtime
+
     def build(self, host_target):
         """build() -> void
 
@@ -291,6 +307,9 @@ class LLVM(cmake_product.CMakeProduct):
         llvm_cmake_options.define('LLVM_INCLUDE_DOCS:BOOL', 'TRUE')
         llvm_cmake_options.define('LLVM_ENABLE_LTO:STRING', self.args.lto_type)
         llvm_cmake_options.define('COMPILER_RT_INTERCEPT_LIBDISPATCH', 'ON')
+        llvm_cmake_options.define(
+            'RUNTIMES_CMAKE_ARGS',
+            LLVM.compute_cmake_args_for_runtimes(host_target))
 
         if self.args.build_embedded_stdlib and system() == "Darwin":
             # Ask for Mach-O cross-compilation builtins (for Embedded Swift)
@@ -299,10 +318,12 @@ class LLVM(cmake_product.CMakeProduct):
                 'armv6 armv6m armv7 armv7m armv7em')
 
         llvm_enable_projects = ['clang']
+        llvm_enable_runtimes = []
 
         if self.args.build_compiler_rt and \
                 not self.is_cross_compile_target(host_target):
-            llvm_enable_projects.append('compiler-rt')
+            llvm_enable_runtimes.append('compiler-rt')
+            build_targets.append("runtimes")
 
         if self.args.build_clang_tools_extra:
             llvm_enable_projects.append('clang-tools-extra')
@@ -316,18 +337,20 @@ class LLVM(cmake_product.CMakeProduct):
         if self.args.build_lld:
             llvm_enable_projects.append('lld')
 
+        if self.args.test:
+            # LLVMTestingSupport is not built at part of `all`
+            # and is required by some Swift tests
+            build_targets.append('LLVMTestingSupport')
+
         llvm_cmake_options.define('LLVM_ENABLE_PROJECTS',
                                   ';'.join(llvm_enable_projects))
+        llvm_cmake_options.define('LLVM_ENABLE_RUNTIMES',
+                                  ';'.join(llvm_enable_runtimes))
 
-        # In the near future we are aiming to build compiler-rt with
-        # LLVM_ENABLE_RUNTIMES
-        # Until that happens, we need to unset this variable from
-        # LLVM CMakeCache.txt for two reasons
-        # * prevent PRs testing this variable to affect other runs landing
-        #   unrelated features
-        # * avoid fallouts should we land such change and then have to revert
-        #   it to account for unforeseen regressions
-        llvm_cmake_options.undefine('LLVM_ENABLE_RUNTIMES')
+        # This accounts for previous incremental runs that may have set
+        # those in the LLVM CMakeCache.txt
+        llvm_cmake_options.undefine('LLVM_TOOL_COMPILER_RT_BUILD')
+        llvm_cmake_options.undefine('LLVM_BUILD_EXTERNAL_COMPILER_RT')
 
         # NOTE: This is not a dead option! It is relied upon for certain
         # bots/build-configs!
@@ -491,9 +514,19 @@ class LLVM(cmake_product.CMakeProduct):
            self.args.llvm_install_components != 'all':
             install_targets = []
             components = self.args.llvm_install_components.split(';')
+            if 'compiler-rt' in components:
+                # This is a courtesy fallback to avoid breaking downstream presets
+                # we are not aware of
+                components.remove('compiler-rt')
+                components.append('builtins')
+                components.append('runtimes')
+                print('warning: replaced legacy LLVM component compiler-rt '
+                      'with builtins;runtimes -- consider updating your preset',
+                      flush=True)
+
             for component in components:
                 if self.is_cross_compile_target(host_target):
-                    if component == 'compiler-rt':
+                    if component in ['builtins', 'runtimes']:
                         continue
                 install_targets.append('install-{}'.format(component))
 
