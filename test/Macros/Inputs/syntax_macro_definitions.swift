ab576b31a75a09e84a132fde437d664f89d292c3
Doug Gregor <dgregor@apple.com>
2023-08-20 09:21:08 -0700

    Always supersede conformances implied by pre-macro-expansion conformances
    Pre-macro-expansion conformances are introduced at the point where an
    attached extension macro is attached to a particular nominal type, and
    can imply other conformances. Once the macro is expanded, they are
    expected to be replaced by the real conformance from the extension
    produced by the macro. This includes any other conformances that are
    implied by that conformances. Ensure that the real conformance---and
    every conformances it implies---are considered "better" than the
    pre-expansion conformances.
    
    Fixes a bug where we would pick the wrong (pre-expansion)
    conformances, which would then fail to get fully type-checked prior to
    serialization. This could accept invalid code that then crashed the
    compiler, as in rdar://112916159.

--- test/Macros/Inputs/syntax_macro_definitions.swift
+++ test/Macros/Inputs/syntax_macro_definitions.swift
@@ -1360,6 +1360,19 @@ public struct HashableMacro: ExtensionMacro {
   }
 }
 
+public struct ImpliesHashableMacro: ExtensionMacro {
+  public static func expansion(
+    of node: AttributeSyntax,
+    attachedTo: some DeclGroupSyntax,
+    providingExtensionsOf type: some TypeSyntaxProtocol,
+    conformingTo protocols: [TypeSyntax],
+    in context: some MacroExpansionContext
+  ) throws -> [ExtensionDeclSyntax] {
+    let ext: DeclSyntax = "extension \(type.trimmed): ImpliesHashable {}"
+    return [ext.cast(ExtensionDeclSyntax.self)]
+  }
+}
+
 public struct DelegatedConformanceMacro: ExtensionMacro, MemberMacro {
   public static func expansion(
     of node: AttributeSyntax,
