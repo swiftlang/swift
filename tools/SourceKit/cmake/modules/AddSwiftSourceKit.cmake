914a6fdf7aad9927018940b2c806f1492a4fc938
Rintaro Ishizaki <rishizaki@apple.com>
2023-08-25 22:37:34 +0000

    [CMake] Fix weiredness in swift runtime linking for SourceKit libraries
    SourceKit components are never built while bootstrapping stages. So it
    doen't need to have stage 0 or 1 handling.

--- tools/SourceKit/cmake/modules/AddSwiftSourceKit.cmake
+++ tools/SourceKit/cmake/modules/AddSwiftSourceKit.cmake
@@ -154,9 +154,6 @@ function(add_sourcekit_swift_runtime_link_flags target path HAS_SWIFT_MODULES)
   endif()
 
   if(SWIFT_SWIFT_PARSER)
-    # Make sure we can find the early SwiftSyntax libraries.
-    target_link_directories(${target} PRIVATE "${SWIFT_PATH_TO_EARLYSWIFTSYNTAX_BUILD_DIR}/lib/swift/host")
-
     # Add rpath to the host Swift libraries.
     if (NOT SWIFT_HOST_VARIANT_SDK IN_LIST SWIFT_DARWIN_PLATFORMS)
       file(RELATIVE_PATH relative_hostlib_path "${path}" "${SWIFTLIB_DIR}/host")
@@ -166,25 +163,16 @@ function(add_sourcekit_swift_runtime_link_flags target path HAS_SWIFT_MODULES)
     # For the "end step" of bootstrapping configurations on Darwin, need to be
     # able to fall back to the SDK directory for libswiftCore et al.
     if (BOOTSTRAPPING_MODE MATCHES "BOOTSTRAPPING.*")
-      if (NOT "${bootstrapping}" STREQUAL "1")
-        if(SWIFT_HOST_VARIANT_SDK IN_LIST SWIFT_DARWIN_PLATFORMS)
-          target_link_directories(${target} PRIVATE "${sdk_dir}")
+      if(SWIFT_HOST_VARIANT_SDK IN_LIST SWIFT_DARWIN_PLATFORMS)
+        target_link_directories(${target} PRIVATE "${sdk_dir}")
 
-          # Include the abi stable system stdlib in our rpath.
-          set(swift_runtime_rpath "/usr/lib/swift")
+        # Include the abi stable system stdlib in our rpath.
+        set(swift_runtime_rpath "/usr/lib/swift")
 
-          # Add in the toolchain directory so we can grab compatibility libraries
-          get_filename_component(TOOLCHAIN_BIN_DIR ${SWIFT_EXEC_FOR_SWIFT_MODULES} DIRECTORY)
-          get_filename_component(TOOLCHAIN_LIB_DIR "${TOOLCHAIN_BIN_DIR}/../lib/swift/${SWIFT_SDK_${SWIFT_HOST_VARIANT_SDK}_LIB_SUBDIR}" ABSOLUTE)
-          target_link_directories(${target} PUBLIC ${TOOLCHAIN_LIB_DIR})
-        else()
-          get_filename_component(swift_bin_dir ${SWIFT_EXEC_FOR_SWIFT_MODULES} DIRECTORY)
-          get_filename_component(swift_dir ${swift_bin_dir} DIRECTORY)
-          set(host_lib_dir "${swift_dir}/lib/swift/${SWIFT_SDK_${SWIFT_HOST_VARIANT_SDK}_LIB_SUBDIR}")
-          target_link_directories(${target} PUBLIC ${host_lib_dir})
-
-          list(APPEND RPATH_LIST "${host_lib_dir}")
-        endif()
+        # Add in the toolchain directory so we can grab compatibility libraries
+        get_filename_component(TOOLCHAIN_BIN_DIR ${SWIFT_EXEC_FOR_SWIFT_MODULES} DIRECTORY)
+        get_filename_component(TOOLCHAIN_LIB_DIR "${TOOLCHAIN_BIN_DIR}/../lib/swift/${SWIFT_SDK_${SWIFT_HOST_VARIANT_SDK}_LIB_SUBDIR}" ABSOLUTE)
+        target_link_directories(${target} PUBLIC ${TOOLCHAIN_LIB_DIR})
       endif()
     endif()
   endif()
