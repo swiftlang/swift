56d0e6587a5ba82da97ba562f26c907809f7a92c
Rintaro Ishizaki <rishizaki@apple.com>
2023-08-29 01:06:51 +0000

    [CMake] Use RPATH_CHANGE instead RPATH_SET
    RPATH_SET is not available until cmake 3.21.0. Use RPATH_CHANGE instead.
    
    (cherry picked from commit f64506976aac4e611641415fb1c43e7f4e632d3e)
    (cherry picked from commit c39f95bcaa8b0498d0c34d9124663313f7a446fb)

--- cmake/modules/SetRPATH.cmake
+++ cmake/modules/SetRPATH.cmake
@@ -4,10 +4,18 @@ function(swift_get_set_rpath_script_file out_var)
   set(${out_var} "${SWIFT_SET_RPATH_SCRIPT_FILE}" PARENT_SCOPE)
 endfunction()
 
-# Actual RPATH_SET operation to the file.
+# Actual RPATH_CHANGE operation to the file.
 function(_swift_set_rpath_impl file new_rpath)
-  # NOTE: RPATH_SET is not documented, and works only for ELF and XCOFF. 
-  file(RPATH_SET FILE "${file}" NEW_RPATH "${new_rpath}")
+  # FIXME: Handle non-ELF files. We can't use RPATH_SET because it's only available CMake 3.21.0
+  execute_process(
+    COMMAND readelf -Wd "${file}"
+    COMMAND grep -Po "R(UN)?PATH.*\\[\\K[^\\]]*"
+    OUTPUT_VARIABLE current_rpath
+  )
+  string(STRIP "${current_rpath}" current_rpath)
+
+  # NOTE: RPATH_CHANGE is not documented, and works only for ELF and XCOFF.
+  file(RPATH_CHANGE FILE "${file}" OLD_RPATH "${current_rpath}" NEW_RPATH "${new_rpath}")
 endfunction()
 
 # For 'cmake -P <scirpt>'.
