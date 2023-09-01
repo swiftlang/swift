7be242a7373bc93b7a0a0b66c6f43b06af652043
Rajveer <rajveer.developer@icloud.com>
2023-09-01 16:38:49 +0530

    [Sema] Improves diagnostic when passing an argument as property of an optional class instance (#57437)

--- lib/Sema/CSSimplify.cpp
+++ lib/Sema/CSSimplify.cpp
@@ -6534,6 +6534,15 @@ bool ConstraintSystem::repairFailures(
     // diagnostics.
     if (flags.contains(TMF_MatchingGenericArguments))
       break;
+    
+    if (!path.empty()) {
+      if (path.back().is<LocatorPathElt::ApplyArgToParam>())
+        conversionsOrFixes.push_back(AllowArgumentMismatch::create(
+            *this, lhs, rhs, getConstraintLocator(anchor, path)));
+      else if (path.back().is<LocatorPathElt::ContextualType>())
+        conversionsOrFixes.push_back(IgnoreContextualType::create(
+            *this, lhs, rhs, getConstraintLocator(anchor, path)));
+    }
 
     Type fromType;
     Type toType;
