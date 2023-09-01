61b2503fc06a072308005ec0cbb5c5600c713b42
Holly Borla <hborla@apple.com>
2023-08-31 20:19:40 -0700

    [Requirement Machine] Stricter verification conditions for element symbols.

--- lib/AST/RequirementMachine/RewriteSystem.cpp
+++ lib/AST/RequirementMachine/RewriteSystem.cpp
@@ -570,7 +570,8 @@ void RewriteSystem::verifyRewriteRules(ValidityPolicy policy) const {
 
       if (index != 0) {
         ASSERT_RULE(symbol.getKind() != Symbol::Kind::GenericParam ||
-                    lhs[index - 1].getKind() == Symbol::Kind::PackElement);
+                    (index == 1 &&
+                     lhs[index - 1].getKind() == Symbol::Kind::PackElement));
       }
 
       if (!rule.isLHSSimplified() &&
@@ -626,7 +627,8 @@ void RewriteSystem::verifyRewriteRules(ValidityPolicy policy) const {
 
       if (index != 0) {
         ASSERT_RULE(symbol.getKind() != Symbol::Kind::GenericParam ||
-                    lhs[index - 1].getKind() == Symbol::Kind::PackElement);
+                    (index == 1 &&
+                     lhs[index - 1].getKind() == Symbol::Kind::PackElement));
       }
 
       if (!rule.isRHSSimplified() &&
