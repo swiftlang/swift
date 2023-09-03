2c0ab081921260749e5f6d34eb3d6b002d1f4d88
Holly Borla <hborla@apple.com>
2023-09-02 17:29:37 -0700

    [NFC] Add -dump-input=fail to a FileCheck test.

--- test/Generics/parameter-pack-same-element-requirements.swift
+++ test/Generics/parameter-pack-same-element-requirements.swift
@@ -1,4 +1,4 @@
-// RUN: %target-swift-frontend -typecheck %s -debug-generic-signatures -enable-experimental-feature SameElementRequirements -disable-availability-checking 2>&1 | %FileCheck %s
+// RUN: %target-swift-frontend -typecheck %s -debug-generic-signatures -enable-experimental-feature SameElementRequirements -disable-availability-checking 2>&1 | %FileCheck %s -dump-input=fail
 
 // REQUIRES: asserts
 
