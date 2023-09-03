0b1a8ffc7971eaa235bb66dbb1adb98b06dedb82
Saleem Abdulrasool <abdulras@thebrowser.company>
2023-09-03 11:41:03 -0700

    tests: replace shell with python for portability
    Replace the process_fine_grained_swiftdeps.sh with a python equivalent
    (which also preserves the horrendous handling of YAML and even
    "faithfully" replicates the horrible global variables).  This enables a
    number of tests on Windows although the instigating macro test is not
    yet enabled due to the need for further tweaks to the tests.

--- test/Frontend/dependencies-preservation-fine.swift
+++ test/Frontend/dependencies-preservation-fine.swift
@@ -1,7 +1,3 @@
-// REQUIRES: shell
-// Also uses awk:
-// XFAIL OS=windows
-
 // This test verifies that copies of dependency files are preserved after a
 // compilation. For example, if the first compilation produces 'foo.swiftdeps',
 // a second compilation should move 'foo.swiftdeps' to 'foo.swiftdeps~', then
@@ -11,7 +7,7 @@
 
 // First, produce the dependency files and verify their contents.
 // RUN: %target-swift-frontend -emit-reference-dependencies-path %t.swiftdeps -typecheck -primary-file "%S/../Inputs/empty file.swift"
-// RUN: %S/../Inputs/process_fine_grained_swiftdeps.sh %swift-dependency-tool %t.swiftdeps %t-processed.swiftdeps
+// RUN: %{python} %S/../Inputs/process_fine_grained_swiftdeps.py %swift-dependency-tool %t.swiftdeps > %t-processed.swiftdeps
 // RUN: %FileCheck -check-prefix=CHECK %s < %t-processed.swiftdeps
 
 // CHECK-NOT: topLevel{{.*}}EmptyStruct{{.*}}true
@@ -22,7 +18,7 @@
 // file.
 // RUN: %target-swift-frontend -emit-reference-dependencies-path %t.swiftdeps -typecheck -primary-file %S/../Inputs/global_resilience.swift
 // RUN: %FileCheck -check-prefix=CHECK %s < %t.swiftdeps~
-// RUN: %S/../Inputs/process_fine_grained_swiftdeps.sh %swift-dependency-tool %t.swiftdeps %t-processed.swiftdeps
+// RUN: %{python} %S/../Inputs/process_fine_grained_swiftdeps.py %swift-dependency-tool %t.swiftdeps > %t-processed.swiftdeps
 // RUN: %FileCheck -check-prefix=CHECK-OVERWRITTEN %s < %t-processed.swiftdeps
 
 // CHECK-OVERWRITTEN:topLevel{{.*}}EmptyStruct{{.*}}true
