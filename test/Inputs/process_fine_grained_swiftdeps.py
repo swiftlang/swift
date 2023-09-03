8ae31b646aca287906c6d0978bf3bd6536f1a657
Saleem Abdulrasool <compnerd@compnerd.org>
2023-09-03 16:47:31 -0700

    Update process_fine_grained_swiftdeps.py
    `python3 -m black %s`

--- test/Inputs/process_fine_grained_swiftdeps.py
+++ test/Inputs/process_fine_grained_swiftdeps.py
@@ -1,6 +1,4 @@
 
-import os
-import re
 import subprocess
 import sys
 
@@ -9,27 +7,30 @@ import sys
 # <kind> <aspect> <context> <name> <isProvides>
 # Also sort for consistency, since the node order can vary.
 
-output = subprocess.run([
+output = subprocess.run(
+    [
         sys.argv[1],
         "--to-yaml",
         "--input-filename={}".format(sys.argv[2]),
-        "--output-filename=-"
-    ], stdout=subprocess.PIPE)
+        "--output-filename=-",
+    ],
+    stdout=subprocess.PIPE,
+)
 entries = []
-k = a = c = n = s = p = ''
-for line in output.stdout.decode('utf-8').split('\n'):
-    if 'kind:' in line:
+k = a = c = n = s = p = ""
+for line in output.stdout.decode("utf-8").split("\n"):
+    if "kind:" in line:
         k = line.split()[1]
-    if 'aspect:' in line:
+    if "aspect:" in line:
         a = line.split()[1]
-    if 'context:' in line:
+    if "context:" in line:
         c = line.split()[1]
-    if 'name:' in line:
+    if "name:" in line:
         n = ' '.join(line.split()[1:])
-    if 'sequenceNumber:' in line:
+    if "sequenceNumber:" in line:
         s = line.split()[1]
-    if 'isProvides:' in line:
+    if "isProvides:" in line:
         p = line.split()[1]
         entries.append(' '.join([k, a, c, n, p]))
 entries.sort()
-print('\n'.join(entries))
+print("\n".join(entries))
