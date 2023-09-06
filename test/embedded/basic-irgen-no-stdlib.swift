26d223aedf542b4d9ade0ba84a8a329c7b9d0e86
zoecarver <z.zoelec2@gmail.com>
2023-09-06 14:38:49 -0700

    [embedded] Mark test as unsupported on Windows.

--- test/embedded/basic-irgen-no-stdlib.swift
+++ test/embedded/basic-irgen-no-stdlib.swift
@@ -1,5 +1,8 @@
 // RUN: %target-swift-emit-ir %s -parse-stdlib -enable-experimental-feature Embedded -target arm64e-apple-none | %FileCheck %s
 
+// TODO: investigate why windows is generating more metadata.
+// XFAIL: OS=windows-msvc
+
 struct Bool {}
 
 protocol Player {
