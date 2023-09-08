d239a14f124fdf5cd048c46b837392b0f8d32a23
Nate Chandler <nate_chandler@apple.com>
2023-09-08 07:08:59 -0700

    [Test] Required executable_test in executing test.
    rdar://115169839

--- validation-test/SILOptimizer/gh68328.swift
+++ validation-test/SILOptimizer/gh68328.swift
@@ -3,6 +3,8 @@
 // RUN: %target-run %t/bin 2> %t/out.txt || true
 // RUN: %FileCheck %s < %t/out.txt
 
+// REQUIRES: executable_test
+
 struct Example: ~Copyable {
     private var failureString: String { "Goodbye." }
     deinit { fatalError("FATAL ERROR: \(failureString)") }
