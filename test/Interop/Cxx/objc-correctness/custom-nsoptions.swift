4e197ba63497d0cf7904cda189a7c29973a8077f
zoecarver <z.zoelec2@gmail.com>
2023-09-11 11:50:39 -0700

    Revert "[cxx-interop] Import custom `NS_OPTIONS` correctly"
    This reverts commit 46f86f0870cf64e649034851d149e735dfb5a14b.

--- test/Interop/Cxx/objc-correctness/custom-nsoptions.swift
+++
@@ -1,7 +0,0 @@
-// RUN: %target-typecheck-verify-swift -verify-ignore-unknown -I %S/Inputs -enable-objc-interop -enable-experimental-cxx-interop
-// REQUIRES: objc_interop
-
-import CustomNSOptions
-
-let flags1: MyControlFlags = []
-let flags2: MyControlFlags = [.first]
