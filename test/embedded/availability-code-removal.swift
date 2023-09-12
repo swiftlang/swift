f4258dd1256e0858fda2e947e17077ef47286176
Kuba Mracek <mracek@apple.com>
2023-09-11 21:33:00 -0700

    [embedded] expand description and checks in availability-code-removal.swift

--- test/embedded/availability-code-removal.swift
+++ test/embedded/availability-code-removal.swift
@@ -1,4 +1,14 @@
-// RUN: %target-swift-frontend -emit-ir %s -parse-stdlib -enable-experimental-feature Embedded
+// This test is checking that usage of an existential (which is normally
+// disallowed in embedded Swift and flagged in IRGen) is left undiagnosed
+// because the context is @_unavailableInEmbedded.
+//
+// The breakdown of that is
+// - (1) @_unavailableInEmbedded makes the declaration unavailable,
+// - (2) unavailable function bodies is removed in embedded Swift,
+// - (3) the test() function is not reported by the existential checker.
+
+// RUN: %target-swift-frontend -emit-ir %s -parse-stdlib | %FileCheck %s --check-prefix CHECK-A
+// RUN: %target-swift-frontend -emit-ir %s -parse-stdlib -enable-experimental-feature Embedded | %FileCheck %s --check-prefix CHECK-B
 
 public protocol Player {}
 struct Concrete: Player {}
@@ -7,3 +17,6 @@ struct Concrete: Player {}
 public func test() -> any Player {
   Concrete() // no error because we're in unavailable-in-embedded context
 }
+
+// CHECK-A: $s4main4testAA6Player_pyF
+// CHECK-B-NOT: $s4main4testAA6Player_pyF
