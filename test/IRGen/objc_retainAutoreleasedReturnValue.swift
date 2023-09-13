44698731cd77d73a008a4859ebc16bd82727b0b7
Meghana Gupta <meghanavgupta@gmail.com>
2023-09-12 23:37:22 -0700

    Revert "Revert "Use clang.arc.attachedcall for emission of objc_retainAutoreleasedReturnValue""
    This reverts commit c3b809181574bfc32d9e490e05491c0aeaeaaef7.

--- test/IRGen/objc_retainAutoreleasedReturnValue.swift
+++ test/IRGen/objc_retainAutoreleasedReturnValue.swift
@@ -1,5 +1,5 @@
 // RUN: %target-swift-frontend -module-name objc_retainAutoreleasedReturnValue -import-objc-header %S/Inputs/StaticInline.h %s -emit-ir | %FileCheck %s
-// RUN: %target-swift-frontend -module-name objc_retainAutoreleasedReturnValue -O -import-objc-header %S/Inputs/StaticInline.h %s -emit-ir | %FileCheck %s --check-prefix=OPT
+// RUN: %target-swift-frontend -module-name objc_retainAutoreleasedReturnValue -O -import-objc-header %S/Inputs/StaticInline.h %s -emit-ir -disable-llvm-optzns | %FileCheck %s --check-prefix=OPT
 
 // REQUIRES: objc_interop
 // REQUIRES: CPU=x86_64
@@ -32,6 +32,6 @@ public func test(_ dict: NSDictionary) {
 
 // OPT-LABEL: define {{.*}}swiftcc void @"$s34objc_retainAutoreleasedReturnValue10useClosureyySo12NSDictionaryC_yADXEtF09$s34objc_bcd16Value4testyySo12H10CFyADXEfU_Tf1nc_n"(ptr %0)
 // OPT: entry:
-// OPT:   call {{.*}}@objc_msgSend
-// OPT:   notail call ptr @llvm.objc.retainAutoreleasedReturnValue
+// OPT:   [[R:%.*]] = call ptr @objc_msgSend({{.*}}) [ "clang.arc.attachedcall"(ptr @llvm.objc.retainAutoreleasedReturnValue) 
+// OPT:   call void (...) @llvm.objc.clang.arc.noop.use(ptr [[R]])
 // OPT:   ret void
