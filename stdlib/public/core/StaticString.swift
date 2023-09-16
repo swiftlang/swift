65a57c7811f028719b1f1e2751cb22fc032bd767
Kuba Mracek <mracek@apple.com>
2023-09-16 13:40:43 -0700

    [embedded] Add an explaining message into StaticString's fatalError

--- stdlib/public/core/StaticString.swift
+++ stdlib/public/core/StaticString.swift
@@ -206,7 +206,7 @@ public struct StaticString: Sendable {
         start: utf8Start, count: utf8CodeUnitCount))
     } else {
       #if $Embedded
-      fatalError()
+      fatalError("non-pointer representation not support in embedded Swift")
       #else
       return unicodeScalar.withUTF8CodeUnits { body($0) }
       #endif
