c0735eee75c4b92fd2a70803fce3798e21553e5c
Kuba Mracek <mracek@apple.com>
2023-09-16 13:43:43 -0700

    [embedded] Drop unnecessary #if in _makeCollectionDescription

--- stdlib/public/core/ArrayShared.swift
+++ stdlib/public/core/ArrayShared.swift
@@ -102,7 +102,7 @@ extension Collection {
   internal func _makeCollectionDescription(
     withTypeName type: String? = nil
   ) -> String {
-#if !SWIFT_STDLIB_STATIC_PRINT && !$Embedded
+#if !SWIFT_STDLIB_STATIC_PRINT
     var result = ""
     if let type = type {
       result += "\(type)(["
