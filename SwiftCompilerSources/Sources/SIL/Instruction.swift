d7c1c50d9bf6530db6d50cee801e0071cc212e5c
Andrew Trick <atrick@apple.com>
2023-09-21 14:53:54 -0700

    SwiftCompilerSources: VarDecl bridging for SIL.

--- SwiftCompilerSources/Sources/SIL/Instruction.swift
+++ SwiftCompilerSources/Sources/SIL/Instruction.swift
@@ -353,7 +353,36 @@ final public class HopToExecutorInst : Instruction, UnaryInstruction {}
 
 final public class FixLifetimeInst : Instruction, UnaryInstruction {}
 
-final public class DebugValueInst : Instruction, UnaryInstruction {}
+// VarDecl is a struct wrapper around a C++ VarDecl pointer. This insulates the Swift interface from the C++ interface and avoids the need to fake a Swift class. When the AST exposes VarDecl, then this can be replaced whenever it is convenient.
+public struct VarDecl {
+  var bridged: BridgedVarDecl
+  
+  public init?(bridged: OptionalBridgedVarDecl) {
+    guard let decl = bridged.decl else { return nil }
+    self.bridged = BridgedVarDecl(decl: decl)
+  }
+  
+  public var userFacingName: String { String(bridged.getUserFacingName()) }
+}
+
+// See C++ VarDeclCarryingInst
+public protocol VarDeclInst {
+  var varDecl: VarDecl? { get }
+}
+
+public protocol DebugVariableInst : VarDeclInst {
+  var debugVariable: OptionalBridgedSILDebugVariable { get }
+}
+
+final public class DebugValueInst : Instruction, UnaryInstruction, DebugVariableInst {
+  public var varDecl: VarDecl? {
+    VarDecl(bridged: bridged.DebugValue_getDecl())
+  }
+
+  public var debugVariable: OptionalBridgedSILDebugVariable {
+    return bridged.DebugValue_getVarInfo()
+  }
+}
 
 final public class DebugStepInst : Instruction {}
 
@@ -608,7 +637,11 @@ final public class DynamicFunctionRefInst : FunctionRefBaseInst {
 final public class PreviousDynamicFunctionRefInst : FunctionRefBaseInst {
 }
 
-final public class GlobalAddrInst : GlobalAccessInst {}
+final public class GlobalAddrInst : GlobalAccessInst, VarDeclInst {
+  public var varDecl: VarDecl? {
+    VarDecl(bridged: bridged.GlobalAddr_getDecl())
+  }
+}
 
 final public class GlobalValueInst : GlobalAccessInst {
   public var isBare: Bool { bridged.GlobalValueInst_isBare() }
@@ -711,13 +744,17 @@ final public class SelectEnumInst : SingleValueInstruction {
   public var enumOperand: Operand { operands[0] }
 }
 
-final public class RefElementAddrInst : SingleValueInstruction, UnaryInstruction {
+final public class RefElementAddrInst : SingleValueInstruction, UnaryInstruction, VarDeclInst {
   public var instance: Value { operand.value }
   public var fieldIndex: Int { bridged.RefElementAddrInst_fieldIndex() }
 
   public var fieldIsLet: Bool { bridged.RefElementAddrInst_fieldIsLet() }
 
   public var isImmutable: Bool { bridged.RefElementAddrInst_isImmutable() }
+  
+  public var varDecl: VarDecl? {
+    VarDecl(bridged: bridged.RefElementAddr_getDecl())
+  }
 }
 
 final public class RefTailAddrInst : SingleValueInstruction, UnaryInstruction {
@@ -942,8 +979,16 @@ final public class LinearFunctionInst: SingleValueInstruction, ForwardingInstruc
 
 public protocol Allocation : SingleValueInstruction { }
 
-final public class AllocStackInst : SingleValueInstruction, Allocation {
+final public class AllocStackInst : SingleValueInstruction, Allocation, DebugVariableInst {
   public var hasDynamicLifetime: Bool { bridged.AllocStackInst_hasDynamicLifetime() }
+
+  public var varDecl: VarDecl? {
+    VarDecl(bridged: bridged.AllocStack_getDecl())
+  }
+
+  public var debugVariable: OptionalBridgedSILDebugVariable {
+    return bridged.AllocStack_getVarInfo()
+  }
 }
 
 public class AllocRefInstBase : SingleValueInstruction, Allocation {
@@ -973,7 +1018,15 @@ final public class AllocRefDynamicInst : AllocRefInstBase {
   }
 }
 
-final public class AllocBoxInst : SingleValueInstruction, Allocation {
+final public class AllocBoxInst : SingleValueInstruction, Allocation, DebugVariableInst {
+
+  public var varDecl: VarDecl? {
+    VarDecl(bridged: bridged.AllocBox_getDecl())
+  }
+
+  public var debugVariable: OptionalBridgedSILDebugVariable {
+    return bridged.AllocBox_getVarInfo()
+  }
 }
 
 final public class AllocExistentialBoxInst : SingleValueInstruction, Allocation {
