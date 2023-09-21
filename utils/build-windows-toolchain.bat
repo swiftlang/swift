5582a9314f30acfe3d5667b5decb42c540e93974
Saleem Abdulrasool <compnerd@compnerd.org>
2023-09-21 09:09:53 -0700

    Update build-windows-toolchain.bat
    debug

--- utils/build-windows-toolchain.bat
+++ utils/build-windows-toolchain.bat
@@ -882,6 +882,7 @@ setlocal enableextensions enabledelayedexpansion
 :: Test Swift
 :: TODO(compnerd) make lit adjust the path properly
 path %BuildRoot%\3;%BuildRoot%\1\bin;%PATH%;%SystemDrive%\Program Files\Git\usr\bin
+tree %BuildRoot%\1\tools\swift\unittests\SourceKit\SwiftLang\CMakeFiles\SourceKitSwiftLangTests.dir
 cmake --build %BuildRoot%\1 --target check-swift || (exit /b)
 
 goto :eof
