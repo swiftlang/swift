9006482fc61394d8caefe5043741f48e1d1c51ec
Kuba Mracek <mracek@apple.com>
2023-09-25 10:18:29 -0700

    [embedded] Mark ptr-auth-irgen-verify-no-stdlib.swift test as REQUIRES: swift_in_compiler

--- test/embedded/ptr-auth-irgen-verify-no-stdlib.swift
+++ test/embedded/ptr-auth-irgen-verify-no-stdlib.swift
@@ -1,7 +1,6 @@
 // RUN: %target-swift-emit-ir %s -parse-stdlib -enable-experimental-feature Embedded -target arm64e-apple-none -Xcc -fptrauth-calls -module-name Swift | %FileCheck %s
 
-// Windows does not have SwiftCompilerSources.
-// XFAIL: OS=windows-msvc
+// REQUIRES: swift_in_compiler
 
 // Some verification is blocked on string lookup succeeding.
 struct String {}
