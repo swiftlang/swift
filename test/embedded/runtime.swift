497b810942aad5a83451edc6b4a7b2ac82073154
Kuba Mracek <mracek@apple.com>
2023-09-25 14:15:30 -0700

    [embedded] Use -runtime-compatibility-version none in a test until we fix the driver to avoid linking compat libraries

--- test/embedded/runtime.swift
+++ test/embedded/runtime.swift
@@ -1,4 +1,4 @@
-// RUN: %target-run-simple-swift(-enable-experimental-feature Embedded -parse-as-library -wmo -Xfrontend -disable-objc-interop) | %FileCheck %s
+// RUN: %target-run-simple-swift(-enable-experimental-feature Embedded -parse-as-library -runtime-compatibility-version none -wmo -Xfrontend -disable-objc-interop) | %FileCheck %s
 
 // REQUIRES: executable_test
 // REQUIRES: optimized_stdlib
