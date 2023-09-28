1f8388c0b19eb49c46ae7494704d07ab25b462b7
Viranchee Lotia <viranchee@outlook.com>
2023-09-28 15:13:05 -0400

    enable all tests, to check on CI

--- test/AutoDiff/compiler_crashers_fixed/issue-57869-vardecl-adjoint-values.swift
+++ test/AutoDiff/compiler_crashers_fixed/issue-57869-vardecl-adjoint-values.swift
@@ -2,7 +2,6 @@
 // RUN: %target-swift-frontend -c -g -Xllvm -verify-di-holes=true %s
 
 // Every so often this test crashes the linker on Linux
-// REQUIRES: rdar87254800
 
 // https://github.com/apple/swift/issues/57869
 // Differentiable functions with control flow yield an assertion failure
