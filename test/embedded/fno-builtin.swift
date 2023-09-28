0687da396922f8619377401efdb9a06a7a3cf0f5
Kuba Mracek <mracek@apple.com>
2023-09-27 21:03:11 -0700

    [embedded] Fix embedded Swift builds with -Xcc -ffreestanding and -fno-builtin

---
+++ test/embedded/fno-builtin.swift
@@ -0,0 +1,11 @@
+// RUN: %target-swift-emit-ir %s %S/Inputs/print.swift -module-name main -Xcc -fno-builtin -enable-experimental-feature Embedded | %FileCheck %s
+// RUN: %target-swift-emit-ir %s %S/Inputs/print.swift -module-name main -Xcc -ffreestanding -enable-experimental-feature Embedded | %FileCheck %s
+
+public func foo() -> [Int] {
+	var a = [1, 2, 3]
+	a.append(4)
+	let b = a.sorted()
+	return b
+}
+
+// CHECK: define {{.*}}@"$s4main3fooSaySiGyF"()
