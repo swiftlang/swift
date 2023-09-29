f5bc6068cd771b056cced64d2ae8e6b3b76b8fdd
Viranchee Lotia <viranchee@outlook.com>
2023-09-29 06:13:09 -0400

    Revert a line from commit "enable all tests, to check on CI"

--- test/AutoDiff/compiler_crashers_fixed/rdar74087329-debug-scope-trampoline-blocks.swift
+++ test/AutoDiff/compiler_crashers_fixed/rdar74087329-debug-scope-trampoline-blocks.swift
@@ -1,6 +1,7 @@
 // RUN: %target-build-swift %s
 // RUN: %target-swift-frontend -c -g -Xllvm -verify-di-holes=true %s
 
+// rdar://74087329 (DI verification failure with trampoline blocks in VJP)
 
 import _Differentiation
 
