e68faffe3d53d8e53938b92dea90856bae28d8d5
Kuba Mracek <mracek@apple.com>
2023-10-02 17:40:26 -0700

    [embedded] Add IR checks into embedded/concurrency-builtins.swift

--- test/embedded/concurrency-builtins.swift
+++ test/embedded/concurrency-builtins.swift
@@ -1,4 +1,4 @@
-// RUN: %target-swift-frontend -emit-ir %s -enable-experimental-feature Embedded -enable-builtin-module
+// RUN: %target-swift-frontend -emit-irgen %s -enable-experimental-feature Embedded -enable-builtin-module | %FileCheck %s
 
 // REQUIRES: swift_in_compiler
 // REQUIRES: optimized_stdlib
@@ -12,3 +12,18 @@ public func test() async {
       return 42
     }
 }
+
+// CHECK: define {{.*}}@"$s4main4testyyYaF"(ptr swiftasync %0)
+// CHECK: entry:
+// CHECK:   %result_type_info = alloca %swift.result_type_info_task_option
+// CHECK:   call {{.*}}@llvm.coro.id.async
+// CHECK:   call {{.*}}@llvm.coro.begin
+// CHECK:   call {{.*}}@llvm.coro.async.resume
+// CHECK:   call {{.*}}@llvm.coro.suspend.async.sl_p0s
+// CHECK:   call {{.*}}@__swift_async_resume_get_context
+// CHECK:   call {{.*}}@swift_allocObject
+// CHECK:   call {{.*}}%swift.async_task_and_context @swift_task_create
+// CHECK:   call {{.*}}@swift_release
+// CHECK:   call {{.*}}@llvm.coro.end.async
+// CHECK:   unreachable
+// CHECK: }
