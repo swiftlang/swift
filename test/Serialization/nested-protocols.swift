f71d7b1587aa07596bc86469814c972dae97951d
Karl Wagner <5254025+karwa@users.noreply.github.com>
2023-10-07 05:33:18 +0200

    [Test] Fix use of 'FileCheck'

--- test/Serialization/nested-protocols.swift
+++ test/Serialization/nested-protocols.swift
@@ -1,6 +1,6 @@
 // RUN: %empty-directory(%t)
 // RUN: %target-swift-frontend -emit-module -module-name nestedprotocolsource -emit-module-path %t/nestedprotocolsource.swiftmodule -primary-file %S/Inputs/nested-protocols.swift
-// RUN: %target-swift-frontend -emit-sil %s -I %t | Filecheck %s
+// RUN: %target-swift-frontend -emit-sil %s -I %t | %FileCheck %s
 import nestedprotocolsource
 
 // CHECK: usesNested<A>(_:)
