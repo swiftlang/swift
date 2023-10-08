89197527a8e94f267fd962d63181b7788f757aa3
Carl Peto <carl.peto@me.com>
2023-10-08 11:49:43 +0100

    For some reason, the way optimisation is done has perhaps changed a bit in 5.9. The upshot is that compiling our standard library crashes during the write bitcode phase for AVR without this patch to IRGen.cpp. Its possible that this problem would crash other non apple platforms such as CortexARM and Linux too without this patch.

--- lib/IRGen/IRGen.cpp
+++ lib/IRGen/IRGen.cpp
@@ -89,6 +89,7 @@
 #include "llvm/Transforms/Instrumentation/ThreadSanitizer.h"
 #include "llvm/Transforms/ObjCARC.h"
 #include "llvm/Transforms/Scalar.h"
+#include "llvm/Transforms/Utils/NameAnonGlobals.h"
 
 #include <thread>
 
@@ -393,6 +394,7 @@ void swift::performLLVMOptimizations(const IRGenOptions &Opts,
         // lto summary.)
         Module->addModuleFlag(llvm::Module::Error, "EnableSplitLTOUnit",
                               uint32_t(1));
+        MPM.addPass(NameAnonGlobalPass());
       }
       MPM.addPass(BitcodeWriterPass(
           *out, /*ShouldPreserveUseListOrder*/ false, EmitRegularLTOSummary));
