a7daf13bf60b36645100532d762390410ebff179
Allan Shortlidge <shortlidge@apple.com>
2023-10-11 16:40:22 -0700

    AST: Introduce and adopt Decl::isExposedToClients().
    This method is a more ergonomic utility wrapping DeclExportabilityVisitor.

--- lib/Serialization/Serialization.cpp
+++ lib/Serialization/Serialization.cpp
@@ -16,7 +16,6 @@
 #include "swift/AST/ASTMangler.h"
 #include "swift/AST/ASTVisitor.h"
 #include "swift/AST/AutoDiff.h"
-#include "swift/AST/DeclExportabilityVisitor.h"
 #include "swift/AST/DiagnosticsCommon.h"
 #include "swift/AST/Expr.h"
 #include "swift/AST/FileSystem.h"
@@ -3306,7 +3305,7 @@ public:
   /// it, but at the same time keep the safety checks precise to avoid
   /// XRef errors and such.
   static bool isDeserializationSafe(const Decl *decl) {
-    return DeclExportabilityVisitor().visit(decl);
+    return decl->isExposedToClients();
   }
 
 private:
