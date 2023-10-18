cc83a7d414a608951842d72d1916fe0208f30666
Erik Eckstein <eeckstein@apple.com>
2023-10-18 11:56:54 +0200

    add the "array.copy_into_vector" array-semantic

--- lib/SILOptimizer/Analysis/ArraySemantic.cpp
+++ lib/SILOptimizer/Analysis/ArraySemantic.cpp
@@ -52,6 +52,7 @@ ArrayCallKind swift::getArraySemanticsKind(SILFunction *f) {
                   ArrayCallKind::kWithUnsafeMutableBufferPointer)
             .Case("array.append_contentsOf", ArrayCallKind::kAppendContentsOf)
             .Case("array.append_element", ArrayCallKind::kAppendElement)
+            .Case("array.copy_into_vector", ArrayCallKind::kCopyIntoVector)
             .Default(ArrayCallKind::kNone);
     if (Tmp != ArrayCallKind::kNone) {
       assert(Kind == ArrayCallKind::kNone && "Multiple array semantic "
