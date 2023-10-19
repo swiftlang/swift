943e05254ff607d570341aa52395d635a580c9d3
Kuba Mracek <mracek@apple.com>
2023-10-18 22:00:14 -0700

    [embedded] Fix test build command in embedded/dynamic-self.swift

--- test/embedded/dynamic-self.swift
+++ test/embedded/dynamic-self.swift
@@ -1,4 +1,4 @@
-// RUN: %target-swift-emit-ir %s -enable-experimental-feature Embedded -parse-as-library | %FileCheck %s
+// RUN: %target-swift-emit-ir %s -enable-experimental-feature Embedded -parse-as-library -module-name main | %FileCheck %s
 
 // REQUIRES: swift_in_compiler
 // REQUIRES: VENDOR=apple
