f96ae2f2db0a94d09a0ec52cfe37ef6f13c5f2eb
Kyle <kyle201817146@gmail.com>
2023-10-20 10:54:44 +0800

    [Documentation] Fix UnsafeMutablePointer's documentation

--- stdlib/public/core/UnsafePointer.swift
+++ stdlib/public/core/UnsafePointer.swift
@@ -612,7 +612,7 @@ public struct UnsafeMutablePointer<Pointee>: _Pointer {
     self.init(mutating: unwrapped)
   }
   
-  /// Creates an immutable typed pointer referencing the same memory as the
+  /// Creates a mutable typed pointer referencing the same memory as the
   /// given mutable pointer.
   ///
   /// - Parameter other: The pointer to convert.
@@ -621,7 +621,7 @@ public struct UnsafeMutablePointer<Pointee>: _Pointer {
    self._rawValue = other._rawValue
   }
 
-  /// Creates an immutable typed pointer referencing the same memory as the
+  /// Creates a mutable typed pointer referencing the same memory as the
   /// given mutable pointer.
   ///
   /// - Parameter other: The pointer to convert. If `other` is `nil`, the
