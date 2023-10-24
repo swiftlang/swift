232259ce99e5ac55b33e9c1b7e49ffd607abcb85
Egor Zhdan <e_zhdan@apple.com>
2023-10-24 13:16:41 +0100

    [cxx-interop] Enable an overlay test on Linux
    Resolves https://github.com/apple/swift/issues/66363

--- test/Interop/Cxx/stdlib/overlay/custom-convertible-to-collection.swift
+++ test/Interop/Cxx/stdlib/overlay/custom-convertible-to-collection.swift
@@ -14,7 +14,6 @@ CxxSequenceTestSuite.test("SimpleSequence to Swift.Array") {
   expectEqual([1, 2, 3, 4] as [Int32], array)
 }
 
-#if !os(Linux) // this test crashes on Linux (https://github.com/apple/swift/issues/66363)
 CxxSequenceTestSuite.test("SimpleCopyAwareSequence to Swift.Array") {
   copiesCount = 0
 
@@ -23,7 +22,6 @@ CxxSequenceTestSuite.test("SimpleCopyAwareSequence to Swift.Array") {
 
   expectEqual(0, copiesCount) // make sure we don't copy the C++ sequence value unnecessarily
 }
-#endif
 
 CxxSequenceTestSuite.test("SimpleSequenceWithOutOfLineEqualEqual to Swift.Array") {
   let seq = SimpleSequenceWithOutOfLineEqualEqual()
