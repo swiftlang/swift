984870e49881c172149bf7391428592cc78e5e45
Max Desiatov <m_desiatov@apple.com>
2023-10-24 21:25:09 +0100

    test/Documentation: create `Info.plist` in `docs.docc` for testing
    Looks like older DocC versions require `Info.plist` to be present, especially in directories with `.docc` extension.

--- test/Documentation/docc.swift
+++ test/Documentation/docc.swift
@@ -1,7 +1,10 @@
 // REQUIRES: OS=macosx
 
 // RUN: %empty-directory(%t)
-// RUN: mv $(dirname %utils)/docs $(dirname %utils)/docs.docc
+// RUN: mv "$(dirname %utils)/docs" "$(dirname %utils)/docs.docc"
+// RUN: plutil -create xml1 "$(dirname %utils)/docs.docc/Info.plist"
+// RUN: plutil -insert CFBundleDisplayName -string "Swift Compiler Documentation" "$(dirname %utils)/docs.docc/Info.plist"
+// RUN: plutil -insert CFBundleIdentifier -string "org.swift.compiler-documentation" "$(dirname %utils)/docs.docc/Info.plist"
 // RUN: xcrun docc convert --output-path %t $(dirname %utils)/docs.docc
 // RUN: mv $(dirname %utils)/docs.docc $(dirname %utils)/docs
 
