cfd00e842fefdcabaed210c265783f2a604c6488
Michael Gottesman <mgottesman@apple.com>
2023-10-25 13:14:47 -0700

    [region-isolation] Fix header in PartitionUtils.h
    NFC.

--- include/swift/SILOptimizer/Utils/PartitionUtils.h
+++ include/swift/SILOptimizer/Utils/PartitionUtils.h
@@ -1,5 +1,17 @@
-#ifndef SWIFT_PARTITIONUTILS_H
-#define SWIFT_PARTITIONUTILS_H
+//===--- PartitionUtils.h -------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2023 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef SWIFT_SILOPTIMIZER_UTILS_PARTITIONUTILS_H
+#define SWIFT_SILOPTIMIZER_UTILS_PARTITIONUTILS_H
 
 #include "swift/Basic/Defer.h"
 #include "swift/Basic/LLVM.h"
