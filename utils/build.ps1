97849f6ec88bde49f8dd0cd7a40c08826dac73d5
Saleem Abdulrasool <compnerd@compnerd.org>
2023-10-30 13:18:37 -0700

    Update build.ps1

--- utils/build.ps1
+++ utils/build.ps1
@@ -1582,6 +1582,8 @@ function Build-Installer() {
     Copy-File "$($HostArch.BinaryCache)\installer\Release\$($HostArch.VSName)\*.msi" "$Stage\"
     Copy-File "$($HostArch.BinaryCache)\installer\Release\$($HostArch.VSName)\*.msm" "$Stage\"
     Copy-File "$($HostArch.BinaryCache)\installer\Release\$($HostArch.VSName)\installer.exe" "$Stage\"
+    # Extract installer engine to ease code-signing on swift.org CI
+    Invoke-Program "$BinaryCache\wix-4.0.1\tools\net6.0\any\wix.exe" -- burn detach "$($HostArch.BinaryCache)\installer\Release\$($HostArch.VSName)\installer.exe" -engine "$Stage\installer-engine.exe" -intermediateFolder "$($HostArch.BinaryCache)\installer\$($HostArch.VSName)\"
   }
 }
 
