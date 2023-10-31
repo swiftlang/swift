d0fae407f39dec4579650471cffb5d2f8fe4c27e
Pavel Yaskevich <pyaskevich@apple.com>
2023-10-31 11:39:32 -0700

    [CSSimplify] Increase score if conformance constraint finds missing conformances
    Although the solver is allowed to use types with missing synthesizable
    conformances we need to note their presence in the score in order
    to rank overloads without such types higher.

---
+++ test/Concurrency/sendable_disambiguation.swift
@@ -0,0 +1,30 @@
+// RUN: %target-swift-frontend %s -emit-sil -o /dev/null -verify -strict-concurrency=complete
+
+// REQUIRES: concurrency
+// REQUIRES: asserts
+// REQUIRES: OS=macosx
+
+do {
+  struct Test {
+    init<T: Sendable>(value: T) {}
+    // expected-note@-1 {{found this candidate}}
+    init<T>(value: T) {}
+    // expected-note@-1 {{found this candidate}}
+  }
+
+  struct SendableOnly {
+    init<T: Sendable>(value: T) {}
+  }
+
+  func testNonSendable<T>(v: T) { // expected-note {{consider making generic parameter 'T' conform to the 'Sendable' protocol}}
+    _ = Test(value: v) // Ok (non-Sendable overload)
+    _ = SendableOnly(value: v)
+    // expected-warning@-1 {{type 'T' does not conform to the 'Sendable' protocol}}
+  }
+
+  func testSendable<T: Sendable>(v: T) {
+    _ = Test(value: v)
+    // expected-error@-1 {{ambiguous use of 'init(value:)'}}
+    _ = SendableOnly(value: v) // Ok
+  }
+}
