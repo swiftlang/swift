a03f6e5b3ae6d835c9ee962d4b8c7792a03a622d
Finagolfin <repo@butta.fastem.com>
2023-11-01 07:27:01 +0530

    [Build] Enable building and CI testing the early Swift Driver on linux with the prebuilt host toolchain

--- test/Driver/Dependencies/only-skip-once.swift
+++ test/Driver/Dependencies/only-skip-once.swift
@@ -1,4 +1,4 @@
-// XFAIL: OS=linux-gnu, OS=openbsd, OS=windows-msvc, OS=linux-android, OS=linux-androideabi
+// XFAIL: OS=openbsd, OS=windows-msvc
 
 // RUN: %empty-directory(%t)
 // RUN: cp -r %S/Inputs/only-skip-once/* %t
@@ -9,7 +9,7 @@
 // CHECK-INITIAL: {{(Job finished: {compile: main.o <= main.swift}|Finished Compiling main.swift)}}
 // CHECK-INITIAL: {{(Job finished: {compile: file1.o <= file1.swift}|Finished Compiling file1.swift)}}
 // CHECK-INITIAL: {{(Job finished: {compile: file2.o <= file2.swift}|Finished Compiling file2.swift)}}
-// CHECK-INITIAL: {{(Job finished: {link: main <= main.o file1.o file2.o}|Finished Linking main)}}
+// CHECK-INITIAL: {{(Job finished: {link: main <= main.o file1.o file2.o|Finished Linking main)}}
 
 // RUN: touch -t 201401240006 %t/file2.swift
 // RUN: cd %t && %target-swiftc_driver -driver-show-job-lifecycle -output-file-map %t/output-file-map.json -incremental main.swift file1.swift file2.swift -j1 2>&1 |  %FileCheck -check-prefix=CHECK-REBUILD %s
@@ -18,5 +18,5 @@
 // CHECK-REBUILD-DAG: {{(Job finished: {compile: file2.o <= file2.swift}|Finished Compiling file2.swift)}}
 // CHECK-REBUILD-DAG: {{(Job skipped: {compile: main.o <= main.swift}|Skipped Compiling main.swift)}}
 // CHECK-REBUILD-DAG: {{(Job skipped: {compile: file1.o <= file1.swift}|Skipped Compiling file1.swift)}}
-// CHECK-REBUILD-DAG: {{(Job finished: {link: main <= main.o file1.o file2.o}|Finished Linking main)}}
+// CHECK-REBUILD-DAG: {{(Job finished: {link: main <= main.o file1.o file2.o|Finished Linking main)}}
 // CHECK-REBUILD-NOT: {{(Job skipped:|Skipped)}}
