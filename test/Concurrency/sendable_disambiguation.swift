dff578638a4724db7a7d2df445acb2f9100e56ef
Pavel Yaskevich <pyaskevich@apple.com>
2023-11-01 10:44:32 -0700

    [ConstraintSystem] Move `SK_MissingSynthesizableConformance` before `SK_UnappliedFunction`
    This would make sure that if property is non-Sendable we'd pick
    a method if it's Sendable instead.

--- test/Concurrency/sendable_disambiguation.swift
+++ test/Concurrency/sendable_disambiguation.swift
@@ -25,3 +25,24 @@ do {
     _ = SendableOnly(value: v) // Ok
   }
 }
+
+do {
+  class K {
+    func value() {}
+  }
+
+  struct X {
+    var fn: (Int) -> K
+    func fn(_: Int) -> Int { 42 }
+  }
+
+  func sendable<T>(_ fn: (Int) -> T) -> T { fn(42) }
+  func sendable<T: Sendable>(_ fn: (Int) -> T) -> T { fn(0) }
+
+  func test(x: X) {
+    let res = sendable(x.fn) // Ok (non-ambiguous and non-Sendable overload)
+    res.value() // To make sure that previous expression picks a property
+    let _: K = sendable(x.fn) // Ok (picks `sendable<T>` with a property)
+    let _: Int = sendable(x.fn) // Ok (picks `sendable<T: Sendable>` with a method)
+  }
+}
