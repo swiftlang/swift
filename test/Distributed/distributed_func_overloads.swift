faf287015f94c7c6e44dbc94f37371e2f7ac3ca6
Konrad `ktoso` Malawski <konrad_malawski@apple.com>
2023-11-02 11:58:08 +0900

    [Distributed] Allow overloads with different types; mangling can handle it

--- test/Distributed/distributed_func_overloads.swift
+++ test/Distributed/distributed_func_overloads.swift
@@ -16,25 +16,34 @@ distributed actor Overloader {
   func overloaded() {}
   func overloaded() async {}
 
-  distributed func overloadedDistA() {} // expected-note{{ambiguous distributed func 'overloadedDistA()' declared here}}
-  distributed func overloadedDistA() async {} // expected-error{{ambiguous distributed func declaration 'overloadedDistA()', cannot overload distributed methods on effect only}}
+  distributed func overloadedDistA() {} // expected-error{{ambiguous distributed func declaration 'overloadedDistA()', cannot overload distributed methods on effect only}}
+  distributed func overloadedDistA() async {} // expected-note{{ambiguous distributed func 'overloadedDistA()' declared here}}
 
-  distributed func overloadedDistT() throws {} // expected-note{{ambiguous distributed func 'overloadedDistT()' declared here}}
-  distributed func overloadedDistT() async throws {} // expected-error{{ambiguous distributed func declaration 'overloadedDistT()', cannot overload distributed methods on effect only}}
+  distributed func overloadedDistT() throws {} // expected-error{{ambiguous distributed func declaration 'overloadedDistT()', cannot overload distributed methods on effect only}}
+  distributed func overloadedDistT() async throws {} // expected-note{{ambiguous distributed func 'overloadedDistT()' declared here}}
 
   // Throws overloads are not legal anyway, but let's check for them here too:
   distributed func overloadedDistThrows() {}
-  // expected-note@-1{{ambiguous distributed func 'overloadedDistThrows()' declared here}}
-  // expected-note@-2{{'overloadedDistThrows()' previously declared here}}
+  // expected-note@-1{{'overloadedDistThrows()' previously declared here}}
+  // expected-error@-2{{ambiguous distributed func declaration 'overloadedDistThrows()', cannot overload distributed methods on effect only}}
   distributed func overloadedDistThrows() throws {}
-  // expected-error@-1{{ambiguous distributed func declaration 'overloadedDistThrows()', cannot overload distributed methods on effect only}}
-  // expected-error@-2{{invalid redeclaration of 'overloadedDistThrows()'}}
+  // expected-error@-1{{invalid redeclaration of 'overloadedDistThrows()'}}
+  // expected-note@-2{{ambiguous distributed func 'overloadedDistThrows()' declared here}}
 
   distributed func overloadedDistAsync() async {}
-  // expected-note@-1{{ambiguous distributed func 'overloadedDistAsync()' declared here}}
-  // expected-note@-2{{'overloadedDistAsync()' previously declared here}}
+  // expected-note@-1{{'overloadedDistAsync()' previously declared here}}
+  // expected-error@-2{{ambiguous distributed func declaration 'overloadedDistAsync()', cannot overload distributed methods on effect only}}
   distributed func overloadedDistAsync() async throws {}
-  // expected-error@-1{{ambiguous distributed func declaration 'overloadedDistAsync()', cannot overload distributed methods on effect only}}
-  // expected-error@-2{{invalid redeclaration of 'overloadedDistAsync()'}}
+  // expected-error@-1{{invalid redeclaration of 'overloadedDistAsync()'}}
+  // expected-note@-2{{ambiguous distributed func 'overloadedDistAsync()' declared here}}
+
+  // overloads differing by parameter type are allowed,
+  // since the mangled identifier includes full type information:
+  distributed func overloadedDistParams(param: String) async {}
+  distributed func overloadedDistParams(param: Int) async {}
+
+  distributed func overloadedDistParams() async {} // also ok
+
+  distributed func overloadedDistParams<A: Sendable & Codable>(param: A) async {}
 }
 
