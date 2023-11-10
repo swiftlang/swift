0d15d4edbeb410c1b396908afbd6e75793272da9
Doug Gregor <dgregor@apple.com>
2023-11-09 22:22:52 -0800

    Replace rethrowing `map` with generic typed throws
    Adopt typed throws for the `map` operation to propagate thrown error
    types through the `map` API. This is done in a manner that is backward
    compatible for almost all cases:
    
    * The new typed-throws entrypoint is `@_alwaysEmitIntoClient` so it
    can back-deploy all the way back.
    * The old `rethrows` entrypoint is left in place, using
    `@usableFromInline` with `internal` so it is part of the ABI but not
    the public interface, and `@_disfavoredOverload` so the type checker
    avoids it while building the standard library itself. The old
    entrypoint is implemented in terms of the new one.
    
    Note that the implementation details for the existential collection
    "box" classes rely on method overriding of `_map` operations, and the
    types are frozen, so  we don't get to change their signatures. However,
    these are only implementations, not API: the actual API `map`
    functions can be upgraded to typed throws.
    
    Note that this code makes use of a known hole in `rethrows` checking
    to allow calling between `rethrows` and typed throws. We'll need to do
    something about this for source-compatibility reasons, but I'll follow
    up with that separately.

--- stdlib/public/core/ExistentialCollection.swift
+++ stdlib/public/core/ExistentialCollection.swift
@@ -526,7 +526,12 @@ internal final class _SequenceBox<S: Sequence>: _AnySequenceBox<S.Element> {
   internal override func _map<T>(
     _ transform: (Element) throws -> T
   ) rethrows -> [T] {
-    return try _base.map(transform)
+    do {
+      return try _base.map(transform)
+    } catch {
+      try _rethrowsViaClosure { throw error }
+      Builtin.unreachable()
+    }
   }
   @inlinable
   internal override func _filter(
@@ -619,7 +624,12 @@ internal final class _CollectionBox<S: Collection>: _AnyCollectionBox<S.Element>
   internal override func _map<T>(
     _ transform: (Element) throws -> T
   ) rethrows -> [T] {
-    return try _base.map(transform)
+    do {
+      return try _base.map(transform)
+    } catch {
+      try _rethrowsViaClosure { throw error }
+      Builtin.unreachable()
+    }
   }
   @inlinable
   internal override func _filter(
@@ -814,7 +824,12 @@ internal final class _BidirectionalCollectionBox<S: BidirectionalCollection>
   internal override func _map<T>(
     _ transform: (Element) throws -> T
   ) rethrows -> [T] {
-    return try _base.map(transform)
+    do {
+      return try _base.map(transform)
+    } catch {
+      try _rethrowsViaClosure { throw error }
+      Builtin.unreachable()
+    }
   }
   @inlinable
   internal override func _filter(
@@ -1027,7 +1042,12 @@ internal final class _RandomAccessCollectionBox<S: RandomAccessCollection>
   internal override func _map<T>(
     _ transform: (Element) throws -> T
   ) rethrows -> [T] {
-    return try _base.map(transform)
+    do {
+      return try _base.map(transform)
+    } catch {
+      try _rethrowsViaClosure { throw error }
+      Builtin.unreachable()
+    }
   }
   @inlinable
   internal override func _filter(
@@ -1303,10 +1323,29 @@ extension AnySequence {
   }
 
   @inlinable
-  public func map<T>(
+  @_alwaysEmitIntoClient
+  public func map<T, E>(
+    _ transform: (Element) throws(E) -> T
+  ) throws(E) -> [T] {
+    do {
+      return try _box._map(transform)
+    } catch {
+      throw error as! E
+    }
+  }
+
+  // ABI-only entrypoint
+  @usableFromInline
+  @_disfavoredOverload
+  func map<T>(
     _ transform: (Element) throws -> T
   ) rethrows -> [T] {
-    return try _box._map(transform)
+    do {
+      return try map(transform)
+    } catch {
+      try _rethrowsViaClosure { throw error }
+      Builtin.unreachable()
+    }
   }
 
   @inlinable
@@ -1390,10 +1429,29 @@ extension AnyCollection {
   }
 
   @inlinable
-  public func map<T>(
+  @_alwaysEmitIntoClient
+  public func map<T, E>(
+    _ transform: (Element) throws(E) -> T
+  ) throws(E) -> [T] {
+    do {
+      return try _box._map(transform)
+    } catch {
+      throw error as! E
+    }
+  }
+
+  // ABI-only entrypoint
+  @usableFromInline
+  @_disfavoredOverload
+  func map<T>(
     _ transform: (Element) throws -> T
   ) rethrows -> [T] {
-    return try _box._map(transform)
+    do {
+      return try map(transform)
+    } catch {
+      try _rethrowsViaClosure { throw error }
+      Builtin.unreachable()
+    }
   }
 
   @inlinable
@@ -1483,10 +1541,29 @@ extension AnyBidirectionalCollection {
   }
 
   @inlinable
-  public func map<T>(
+  @_alwaysEmitIntoClient
+  public func map<T, E>(
+    _ transform: (Element) throws(E) -> T
+  ) throws(E) -> [T] {
+    do {
+      return try _box._map(transform)
+    } catch {
+      throw error as! E
+    }
+  }
+
+  // ABI-only entrypoint
+  @usableFromInline
+  @_disfavoredOverload
+  func map<T>(
     _ transform: (Element) throws -> T
   ) rethrows -> [T] {
-    return try _box._map(transform)
+    do {
+      return try map(transform)
+    } catch {
+      try _rethrowsViaClosure { throw error }
+      Builtin.unreachable()
+    }
   }
 
   @inlinable
@@ -1578,10 +1655,29 @@ extension AnyRandomAccessCollection {
   }
 
   @inlinable
-  public func map<T>(
+  @_alwaysEmitIntoClient
+  public func map<T, E>(
+    _ transform: (Element) throws(E) -> T
+  ) throws(E) -> [T] {
+    do {
+      return try _box._map(transform)
+    } catch {
+      throw error as! E
+    }
+  }
+
+  // ABI-only entrypoint
+  @usableFromInline
+  @_disfavoredOverload
+  func map<T>(
     _ transform: (Element) throws -> T
   ) rethrows -> [T] {
-    return try _box._map(transform)
+    do {
+      return try map(transform)
+    } catch {
+      try _rethrowsViaClosure { throw error }
+      Builtin.unreachable()
+    }
   }
 
   @inlinable
