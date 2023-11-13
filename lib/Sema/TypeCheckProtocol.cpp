6082d1c05bcbe4fcbabad961c10e4d574b714481
Pavel Yaskevich <pyaskevich@apple.com>
2023-11-13 14:56:13 -0800

    [Sema] Don't propagate `allowsMissing` to existential superclass check
    `TypeChecker::containsProtocol` shouldn't propagate user-provided
    `allowMissing` to existential superclass check because the type
    could be `& Sendable` and `allowMissing` propagation would result
    in invalid builtin conformance returned for a non-conforming
    superclass.

--- lib/Sema/TypeCheckProtocol.cpp
+++ lib/Sema/TypeCheckProtocol.cpp
@@ -5777,12 +5777,16 @@ TypeChecker::containsProtocol(Type T, ProtocolDecl *Proto, ModuleDecl *M,
 
     // First, if we have a superclass constraint, the class may conform
     // concretely.
+    //
+    // Note that `allowMissing` is not propagated here because it
+    // would result in a missing conformance if type is `& Sendable`
+    // protocol composition. It's handled for type as a whole below.
     if (auto superclass = layout.getSuperclass()) {
       auto result =
           (skipConditionalRequirements
-           ? M->lookupConformance(superclass, Proto, allowMissing)
-           : TypeChecker::conformsToProtocol(
-               superclass, Proto, M, allowMissing));
+               ? M->lookupConformance(superclass, Proto, /*allowMissing=*/false)
+               : TypeChecker::conformsToProtocol(superclass, Proto, M,
+                                                 /*allowMissing=*/false));
       if (result) {
         return result;
       }
