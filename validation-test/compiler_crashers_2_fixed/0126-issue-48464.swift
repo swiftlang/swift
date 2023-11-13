e65290c2bd0b754e73e0e1131469dd23b2b18d95
Slava Pestov <spestov@apple.com>
2023-11-13 13:54:32 -0500

    Sema: Associated type inference skips witnesses that might trigger a request cycle
    This implements a structural walk over the TypeRepr to catch
    situations where we attempt to infer `A` from `func f(_: A)`,
    which references the concrete `A` that will be synthesized
    in the conforming type.
    
    Fixes:
    - rdar://34956654 / https://github.com/apple/swift/issues/48680
    - rdar://38913692 / https://github.com/apple/swift/issues/49066
    - rdar://56672411
    - https://github.com/apple/swift/issues/50010
    - rdar://81587765 / https://github.com/apple/swift/issues/57355
    - rdar://117442510

--- validation-test/compiler_crashers_2_fixed/0126-issue-48464.swift
+++ validation-test/compiler_crashers_2_fixed/0126-issue-48464.swift
@@ -1,31 +1,30 @@
-// RUN: %target-typecheck-verify-swift
+// RUN: %target-swift-frontend -emit-ir %s
 
 // https://github.com/apple/swift/issues/48464
 
-protocol VectorIndex {
+public protocol VectorIndex {
     associatedtype Vector8 : Vector where Vector8.Index == Self, Vector8.Element == UInt8
 }
-enum VectorIndex1 : VectorIndex {
+public enum VectorIndex1 : VectorIndex {
     case i0
-    typealias Vector8 = Vector1<UInt8>
+    public typealias Vector8 = Vector1<UInt8>
 }
-protocol Vector {
+public protocol Vector {
     associatedtype Index: VectorIndex
     associatedtype Element
     init(elementForIndex: (Index) -> Element)
     subscript(index: Index) -> Element { get set }
 }
-struct Vector1<Element> : Vector {
-    //typealias Index = VectorIndex1 // Uncomment this line to workaround bug.
-    var e0: Element
-    init(elementForIndex: (VectorIndex1) -> Element) {
+public struct Vector1<Element> : Vector {
+    public var e0: Element
+    public init(elementForIndex: (VectorIndex1) -> Element) {
         e0 = elementForIndex(.i0)
     }
-    subscript(index: Index) -> Element { // expected-error {{reference to invalid associated type 'Index' of type 'Vector1<Element>'}}
+    public subscript(index: Index) -> Element {
         get { return e0 }
         set { e0 = newValue }
     }
 }
 extension Vector where Index == VectorIndex1 {
-    init(_ e0: Element) { fatalError() }
+    public init(_ e0: Element) { fatalError() }
 }
