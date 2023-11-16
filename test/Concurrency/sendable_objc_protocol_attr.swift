f3cd1cfccc8a75fa6b773759ebcc6f8d696dc535
Pavel Yaskevich <pyaskevich@apple.com>
2023-11-16 13:40:41 -0800

    [Concurrency] Downgrade error about inherited Sendable classes inheriting from non-NSObject to warning

--- test/Concurrency/sendable_objc_protocol_attr.swift
+++ test/Concurrency/sendable_objc_protocol_attr.swift
@@ -60,6 +60,42 @@ class KUncheckedRefined : NSObject, MyRefinedObjCProtocol, @unchecked Sendable {
   let test: String = "refined unchecked"
 }
 
+@preconcurrency
+protocol P : Sendable {
+}
+
+protocol Q : Sendable {
+}
+
+do {
+  class A : NSObject {}
+
+  final class B : A, P {
+    // expected-warning@-1 {{'Sendable' class 'B' cannot inherit from another class other than 'NSObject'}}
+  }
+
+  final class UncheckedB : A, P, @unchecked Sendable { // Ok
+  }
+
+  class C : A, MyObjCProtocol {
+    // expected-warning@-1 {{non-final class 'C' cannot conform to 'Sendable'; use '@unchecked Sendable'}}
+    // expected-warning@-2 {{'Sendable' class 'C' cannot inherit from another class other than 'NSObject'}}
+    let test: String = "c"
+  }
+
+  class UncheckedC : A, MyObjCProtocol, @unchecked Sendable { // Ok
+    let test: String = "c"
+  }
+
+  // A warning until `-swift-version 6`
+  final class D : A, Q {
+    // expected-warning@-1 {{'Sendable' class 'D' cannot inherit from another class other than 'NSObject'}}
+  }
+
+  final class UncheckedD : A, Q, @unchecked Sendable { // Ok
+  }
+}
+
 do {
   func testSendable<T: Sendable>(_: T) {}
 
