6b000bdec68475cfe9d9197f7044df3ecccaa700
Apollo Zhu <zhuzhiyu@apple.com>
2023-11-19 15:30:30 -0800

    Members from other files appear as runtime value kind

--- test/ConstExtraction/ExtractFromExtension.swift
+++ test/ConstExtraction/ExtractFromExtension.swift
@@ -8,49 +8,36 @@ extension MyType {
     static let myValue = MyType("it is doable")
 }
 
-// CHECK: [
-// CHECK-NEXT:  {
-// CHECK-NEXT:    "typeName": "ProtocolConformances.MyType",
-// CHECK-NEXT:    "mangledTypeName": "20ProtocolConformances6MyTypeV",
-// CHECK-NEXT:    "kind": "struct",
-// CHECK-NEXT:    "file": "{{.*}}test{{/|\\\\}}ConstExtraction{{/|\\\\}}Inputs{{/|\\\\}}ProtocolConformances.swift",
-// CHECK-NEXT:    "line": 3,
-// CHECK-NEXT:    "conformances": [
-// CHECK-NEXT:      "ProtocolConformances.MyProto"
-// CHECK-NEXT:    ],
-// CHECK-NEXT:    "associatedTypeAliases": [],
-// CHECK-NEXT:    "properties": [
-// CHECK-NEXT:      {
-// CHECK-NEXT:        "label": "value",
-// CHECK-NEXT:        "type": "Swift.String",
-// CHECK-NEXT:        "mangledTypeName": "n/a - deprecated",
-// CHECK-NEXT:        "isStatic": "false",
-// CHECK-NEXT:        "isComputed": "false",
-// CHECK-NEXT:        "file": "{{.*}}test{{/|\\\\}}ConstExtraction{{/|\\\\}}Inputs{{/|\\\\}}ProtocolConformances.swift",
-// CHECK-NEXT:        "line": 4,
-// CHECK-NEXT:        "valueKind": "Runtime"
-// CHECK-NEXT:      },
-// CHECK-NEXT:      {
-// CHECK-NEXT:        "label": "myValue",
-// CHECK-NEXT:        "type": "ProtocolConformances.MyType",
-// CHECK-NEXT:        "mangledTypeName": "n/a - deprecated",
-// CHECK-NEXT:        "isStatic": "true",
-// CHECK-NEXT:        "isComputed": "false",
-// CHECK-NEXT:        "file": "{{.*}}test{{/|\\\\}}ConstExtraction{{/|\\\\}}ExtractFromExtension.swift",
-// CHECK-NEXT:        "line": 8,
-// CHECK-NEXT:        "valueKind": "InitCall",
-// CHECK-NEXT:        "value": {
-// CHECK-NEXT:          "type": "ProtocolConformances.MyType",
-// CHECK-NEXT:          "arguments": [
-// CHECK-NEXT:            {
-// CHECK-NEXT:              "label": "",
-// CHECK-NEXT:              "type": "Swift.String",
-// CHECK-NEXT:              "valueKind": "RawLiteral",
-// CHECK-NEXT:              "value": "it is doable"
-// CHECK-NEXT:            }
-// CHECK-NEXT:          ]
+// CHECK:       "typeName": "ProtocolConformances.MyType",
+// CHECK:       "kind": "struct",
+// CHECK:       "conformances": [
+// CHECK-NEXT:    "ProtocolConformances.MyProto"
+// CHECK-NEXT:  ],
+// CHECK:       "properties": [
+
+// CHECK:         "label": "value",
+// CHECK:         "type": "Swift.String",
+// CHECK:         "valueKind": "Runtime"
+
+// CHECK:         "label": "defaultValue",
+// CHECK:         "type": "ProtocolConformances.MyType",
+// CHECK:         "valueKind": "Runtime"
+
+// CHECK:         "label": "anotherValue",
+// CHECK:         "type": "ProtocolConformances.MyType",
+// CHECK:         "valueKind": "Runtime"
+
+// CHECK:         "label": "myValue",
+// CHECK:         "type": "ProtocolConformances.MyType",
+// CHECK:         "valueKind": "InitCall",
+// CHECK-NEXT:    "value": {
+// CHECK-NEXT:      "type": "ProtocolConformances.MyType",
+// CHECK-NEXT:      "arguments": [
+// CHECK-NEXT:        {
+// CHECK-NEXT:          "label": "",
+// CHECK-NEXT:          "type": "Swift.String",
+// CHECK-NEXT:          "valueKind": "RawLiteral",
+// CHECK-NEXT:          "value": "it is doable"
 // CHECK-NEXT:        }
-// CHECK-NEXT:      }
-// CHECK-NEXT:    ]
-// CHECK-NEXT:  }
-// CHECK-NEXT:]
+// CHECK-NEXT:      ]
+// CHECK-NEXT:    }
--- test/ConstExtraction/Inputs/ProtocolConformances.swift
+++ test/ConstExtraction/Inputs/ProtocolConformances.swift
@@ -6,6 +6,10 @@ public struct MyType {
     public init(_ value: String) {
         self.value = value
     }
+    
+    private static let defaultValue = MyType("not me")
 }
 
-extension MyType: MyProto { }
+extension MyType: MyProto {
+    static let anotherValue = MyType("also not me")
+}
