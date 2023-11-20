b789e7867a4c794fba4c16a897b9ea4545cfaede
Konrad `ktoso` Malawski <konrad_malawski@apple.com>
2023-11-20 13:19:39 +0900

    undo changes to test/Concurrency/Runtime/async_task_withUnsafeCurrentTask.swift

--- test/Concurrency/Runtime/async_task_withUnsafeCurrentTask.swift
+++ test/Concurrency/Runtime/async_task_withUnsafeCurrentTask.swift
@@ -1,6 +1,7 @@
 // RUN: %target-run-simple-swift( -Xfrontend -disable-availability-checking -parse-as-library) 2>&1 | %FileCheck %s --dump-input=always
 // REQUIRES: executable_test
 // REQUIRES: concurrency
+// REQUIRES: swift_task_debug_log
 
 // REQUIRES: concurrency_runtime
 // UNSUPPORTED: back_deployment_runtime
@@ -15,15 +16,19 @@ import Darwin
 
 func test_withUnsafeCurrentTask() async {
   // The task we're running in ("main")
+  // CHECK: creating task [[MAIN_TASK:0x.*]] with parent 0x0
+
+  // CHECK: creating task [[TASK:0x.*]] with parent 0x0
   let t = Task.detached {
-    _ = withUnsafeCurrentTask { task in
-      fputs("OK: \(task!)\n", stderr)
+    withUnsafeCurrentTask { task in
+      fputs("OK: \(task!)", stderr)
     }
     fputs("DONE", stderr)
   }
 
   // CHECK: OK: UnsafeCurrentTask(_task: (Opaque Value))
   // CHECK: DONE
+  // CHECK: destroy task [[TASK]]
   await t.value
 }
 
