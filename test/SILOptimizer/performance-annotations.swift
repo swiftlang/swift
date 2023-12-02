93187836be6ee890141ba021fff9188524b716e0
Becca Royal-Gordon <beccadax@apple.com>
2023-12-02 09:47:15 -0800

    XFAIL SILOptimizer/performance-annotations.swift
    Blocking several bots:
    
    https://ci.swift.org/job/oss-swift-package-macos/2456/
    https://ci.swift.org/job/oss-swift_tools-R_stdlib-RD_test-simulator/4344/
    https://ci.swift.org/job/oss-swift_tools-RA_stdlib-DA_test-device-non_executable/4949/
    https://ci.swift.org/job/oss-swift_tools-RA_stdlib-RD_test-simulator/3362/
    
    rdar://119075334

--- test/SILOptimizer/performance-annotations.swift
+++ test/SILOptimizer/performance-annotations.swift
@@ -1,3 +1,5 @@
+// REQUIRES: rdar119075334
+
 // RUN: %target-swift-frontend -parse-as-library -emit-sil %s -o /dev/null -verify
 // REQUIRES: swift_stdlib_no_asserts,optimized_stdlib
 // REQUIRES: swift_in_compiler
