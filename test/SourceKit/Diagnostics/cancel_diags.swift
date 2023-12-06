406d5434c1df349a0781b1c79a65e48078d7cd5d
Rintaro Ishizaki <rishizaki@apple.com>
2023-12-06 09:45:24 -0800

    [SourceKit] Alway dump the input for FileCheck in a cancellation test
    To get more information when failed.
    
    rdar://116486638

--- test/SourceKit/Diagnostics/cancel_diags.swift
+++ test/SourceKit/Diagnostics/cancel_diags.swift
@@ -1,4 +1,4 @@
-// RUN: not %sourcekitd-test -req=diags %s -print-raw-response -id=diag -async -- %s == -cancel=diag 2>&1 | %FileCheck %s
+// RUN: not %sourcekitd-test -req=diags %s -print-raw-response -id=diag -async -- %s == -cancel=diag 2>&1 | %FileCheck --dump-input=always %s
 
 func foo(x: Invalid1, y: Invalid2) {
     x / y / x / y / x / y / x / y
