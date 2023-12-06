5ef93af269613be63ca1f70afbfab011e72b5772
Adrian Prantl <aprantl@apple.com>
2023-12-06 10:57:24 -0800

    Revert "Don't require a strict revision match in LLDB."
    This reverts commit 3cc2831608a22c87df24220348ac1695aa476276.
    
    The compiler's revision check has been relaxed since the feature was introduced
    and so it's nos better to reduce the number of special code paths for LLDB in
    the compiler to facilitate reasoning about it.
    
    rdar://117824367

--- lib/Frontend/CompilerInvocation.cpp
+++ lib/Frontend/CompilerInvocation.cpp
@@ -3224,8 +3224,7 @@ serialization::Status
 CompilerInvocation::loadFromSerializedAST(StringRef data) {
   serialization::ExtendedValidationInfo extendedInfo;
   serialization::ValidationInfo info = serialization::validateSerializedAST(
-      data, getSILOptions().EnableOSSAModules, LangOpts.SDKName,
-      !LangOpts.DebuggerSupport, &extendedInfo);
+      data, getSILOptions().EnableOSSAModules, LangOpts.SDKName, &extendedInfo);
 
   if (info.status != serialization::Status::Valid)
     return info.status;
@@ -3261,7 +3260,7 @@ CompilerInvocation::setUpInputForSILTool(
 
   auto result = serialization::validateSerializedAST(
       fileBufOrErr.get()->getBuffer(), getSILOptions().EnableOSSAModules,
-      LangOpts.SDKName, !LangOpts.DebuggerSupport, &extendedInfo);
+      LangOpts.SDKName, &extendedInfo);
   bool hasSerializedAST = result.status == serialization::Status::Valid;
 
   if (hasSerializedAST) {
