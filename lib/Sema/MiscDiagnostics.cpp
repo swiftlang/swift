29dac4380eca1db80ab451a1394a0bc07ba6d549
Hamish Knight <hamish_knight@apple.com>
2023-12-08 14:38:40 +0000

    [Sema] Correctly re-contextualize `if`/`switch` exprs in lazy vars
    With `if`/`switch` expressions, we may now have
    local bindings within lazy initializers, and
    therefore need to ensure we correctly
    re-contextualize them. Adjust the walker to set
    the DeclContext for all decls it encounters,
    and make sure it handles some cases that the
    ASTWalker does not currently visit.
    
    rdar://119158202

--- lib/Sema/MiscDiagnostics.cpp
+++ lib/Sema/MiscDiagnostics.cpp
@@ -3912,7 +3912,8 @@ private:
   PreWalkResult<Expr *> walkToExprPre(Expr *E) override {
     if (auto *SVE = dyn_cast<SingleValueStmtExpr>(E)) {
       // Diagnose a SingleValueStmtExpr in a context that we do not currently
-      // support.
+      // support. If we start allowing these in arbitrary places, we'll need
+      // to ensure that autoclosures correctly contextualize them.
       if (!ValidSingleValueStmtExprs.contains(SVE)) {
         Diags.diagnose(SVE->getLoc(), diag::single_value_stmt_out_of_place,
                        SVE->getStmt()->getKind());
