097c556b44dbd0c059258fd48e287490eba66d88
Stefan Gränitz <stefan.graenitz@gmail.com>
2023-12-11 12:03:39 +0100

    [Driver] Match full path in options.swift

--- test/Driver/options.swift
+++ test/Driver/options.swift
@@ -125,11 +125,11 @@
 // INVALID_DWARF_VERSION: invalid value '{{1|6}}' in '-dwarf-version={{1|6}}'
 
 // RUN: %swiftc_driver -### -g -dwarf-fission -o dwarf-fission.o %s 2>&1 | %FileCheck -check-prefix DWARF_FISSION %s
-// DWARF_FISSION: -split-dwarf-output options{{.*}}.dwo
+// DWARF_FISSION: -split-dwarf-output {{.*}}options{{.*}}.dwo
 
 // RUN: %swiftc_driver -### -g -dwarf-fission -emit-object -module-name test -output-file-map Inputs/fission-ofm.json Inputs/main.swift Inputs/lib.swift %s 2>&1 | %FileCheck -check-prefix DWARF_FISSION_MULTI %s
-// DWARF_FISSION_MULTI: -primary-file main.swift {{.*}} -split-dwarf-output main{{.*}}.dwo {{.*}} -o main.o
-// DWARF_FISSION_MULTI: -primary-file lib.swift {{.*}} -split-dwarf-output lib{{.*}}.dwo {{.*}} -o lib.o
+// DWARF_FISSION_MULTI: -primary-file main.swift {{.*}} -split-dwarf-output {{.*}}main{{.*}}.dwo {{.*}} -o main.o
+// DWARF_FISSION_MULTI: -primary-file lib.swift {{.*}} -split-dwarf-output {{.*}}lib{{.*}}.dwo {{.*}} -o lib.o
 
 // RUN: not %swift_driver -gline-tables-only -debug-info-format=codeview %s 2>&1 | %FileCheck -check-prefix BAD_DEBUG_LEVEL_ERROR %s
 // RUN: not %swift_driver -gdwarf-types -debug-info-format=codeview %s 2>&1 | %FileCheck -check-prefix BAD_DEBUG_LEVEL_ERROR %s
