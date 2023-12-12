bbd182bd41142d2a3d08d31b89c744c692769549
Pavel Yaskevich <pyaskevich@apple.com>
2023-12-12 13:40:33 -0800

    [Concurrency] Teach `Type::stripConcurrency` to handle sugared array and dictionary types

--- test/Concurrency/sendable_preconcurrency_erasure.swift
+++ test/Concurrency/sendable_preconcurrency_erasure.swift
@@ -15,6 +15,23 @@ do {
   // CHECK-LABEL: sil private [ossa] @$s31sendable_preconcurrency_erasure4testL1_yySayyp_yyctGF
 }
 
+// Test erase in sugared types
+do {
+  @preconcurrency func testArray1(_: [any Sendable]) {}
+  // CHECK-LABEL: sil private [ossa] @$s31sendable_preconcurrency_erasure10testArray1L2_yySayypGF
+  @preconcurrency func testArray2(_: [@Sendable () -> Void]) {}
+  // CHECK-LABEL: sil private [ossa] @$s31sendable_preconcurrency_erasure10testArray2L2_yySayyycGF
+
+  @preconcurrency func testDictionary(_: [Int: (any Sendable, (String, @Sendable (any Sendable) -> Void))]) {}
+  // CHECK-LABEL: sil private [ossa] @$s31sendable_preconcurrency_erasure14testDictionaryL2_yySDySiyp_SS_yypcttGF
+
+  @preconcurrency func testDesugaredDict(_: Dictionary<Int, (any Sendable, (String, @Sendable (any Sendable) -> Void))>) {}
+  // CHECK-LABEL: sil private [ossa] @$s31sendable_preconcurrency_erasure17testDesugaredDictL2_yySDySiyp_SS_yypcttGF
+
+  @preconcurrency func testVariadic(_: (any Sendable)...) {}
+  // CHECK-LABEL: sil private [ossa] @$s31sendable_preconcurrency_erasure12testVariadicL2_yyypd_tF
+}
+
 public struct Data {
   @preconcurrency var test: (any Sendable, Array<(Int, any Sendable)>)? = nil
   // CHECK-LABEL: sil [transparent] [ossa] @$s31sendable_preconcurrency_erasure4DataV4testyp_SaySi_yptGtSgvpfi
