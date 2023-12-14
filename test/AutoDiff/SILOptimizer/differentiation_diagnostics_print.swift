71f7126c439ee1935bb45114a1e5384ffe26c748
Viranchee Lotia <viranchee@outlook.com>
2023-12-14 18:26:03 -0500

    Add code for covering lines DifferentiationInvoker.cpp:47-50

---
+++ test/AutoDiff/SILOptimizer/differentiation_diagnostics_print.swift
@@ -0,0 +1,16 @@
+// RUN: %target-swift-frontend -emit-sil -verify -Xllvm --sil-print-after=differentiation -Xllvm --debug-only=differentiation %s 2>&1 | %FileCheck %s
+
+// Needed for '--debug-only'
+
+import _Differentiation
+
+// Helps increase code coverage for DifferentiationInvoker.cpp:47-50
+// Copy of differentiation_diagnostics.swift:fragileFuncWithGradient, but 
+public func mirror(_ x: Float) -> Float { x }
+
+@inlinable
+func useInlineFunc() {
+  // expected-error @+2 {{function is not differentiable}}
+  // expected-note @+1 {{differentiated functions in '@inlinable' functions must be marked '@differentiable' or have a public '@derivative'}}
+  _ = gradient(at: 0, of: mirror)
+}
