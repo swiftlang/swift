b39d6a5c2fa779a2bf7402aae3ad7d38dd8be1a0
Stefan Gränitz <stefan.graenitz@gmail.com>
2023-12-15 14:39:29 +0100

    [Driver] Fix multi-unit test in options.swift

--- test/Driver/options.swift
+++ test/Driver/options.swift
@@ -127,9 +127,9 @@
 // RUN: %swiftc_driver -### -g -dwarf-fission -o dwarf-fission.o %s 2>&1 | %FileCheck -check-prefix DWARF_FISSION %s
 // DWARF_FISSION: -split-dwarf-output {{.*}}options{{.*}}.dwo
 
-// RUN: %swiftc_driver -### -g -dwarf-fission -emit-object -module-name test -output-file-map Inputs/fission-ofm.json Inputs/main.swift Inputs/lib.swift %s 2>&1 | %FileCheck -check-prefix DWARF_FISSION_MULTI %s
-// DWARF_FISSION_MULTI: -primary-file main.swift {{.*}} -split-dwarf-output {{.*}}main{{.*}}.dwo {{.*}} -o main.o
-// DWARF_FISSION_MULTI: -primary-file lib.swift {{.*}} -split-dwarf-output {{.*}}lib{{.*}}.dwo {{.*}} -o lib.o
+// RUN: %swiftc_driver -### -g -dwarf-fission -emit-object -module-name test -output-file-map %S/Inputs/fission-ofm.json %S/Inputs/main.swift %S/Inputs/lib.swift %s 2>&1 | %FileCheck -check-prefix DWARF_FISSION_MULTI %s
+// DWARF_FISSION_MULTI: -primary-file {{.*}}main.swift {{.*}} -split-dwarf-output {{.*}}main{{.*}}.dwo {{.*}}
+// DWARF_FISSION_MULTI: -primary-file {{.*}}lib.swift {{.*}} -split-dwarf-output {{.*}}lib{{.*}}.dwo {{.*}}
 
 // RUN: not %swift_driver -gline-tables-only -debug-info-format=codeview %s 2>&1 | %FileCheck -check-prefix BAD_DEBUG_LEVEL_ERROR %s
 // RUN: not %swift_driver -gdwarf-types -debug-info-format=codeview %s 2>&1 | %FileCheck -check-prefix BAD_DEBUG_LEVEL_ERROR %s
