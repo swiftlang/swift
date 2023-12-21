8b7e720b52cc3139dc0e1faa9a78467e2b80926b
Rintaro Ishizaki <rishizaki@apple.com>
2023-12-21 09:49:33 -0800

    [Tests] IRGen/section_structs.swift requires 'optimized_stdlib'
    rdar://119899895

--- test/IRGen/section_structs.swift
+++ test/IRGen/section_structs.swift
@@ -2,6 +2,9 @@
 
 // REQUIRES: swift_in_compiler
 
+// Fails without optimized stdlib (rdar://119899895)
+// REQUIRES: optimized_stdlib
+
 // The `StaticString("hello").utf8Start` test fails on 32 bit
 // UNSUPPORTED: PTRSIZE=32
 
