1bd8763bda831689452088fa95e405d4df9d2aab
David Smith <david_smith@apple.com>
2023-12-20 16:15:26 -0800

    Try disabling size correction entirely just to see what happens

--- stdlib/public/core/StringStorage.swift
+++ stdlib/public/core/StringStorage.swift
@@ -186,7 +186,7 @@ fileprivate func _allocate<T: AnyObject>(
   let linearBucketThreshold = 128
   if _fastPath(numBytes < linearBucketThreshold) {
     // Allocate up to the nearest bucket of 16
-    let realNumBytes = _mallocGoodSize(for: numBytes)
+    let realNumBytes = numBytes //_mallocGoodSize(for: numBytes)
     let realNumTailBytes = realNumBytes - numHeaderBytes
     _internalInvariant(realNumTailBytes >= numTailBytes)
     let object = tailAllocator(realNumTailBytes)
@@ -200,8 +200,8 @@ fileprivate func _allocate<T: AnyObject>(
     growTailBytes = numTailBytes
   }
 
-  let total = _mallocGoodSize(for: numHeaderBytes + growTailBytes)
-  let totalTailBytes = total - numHeaderBytes
+  let total = numHeaderBytes + growTailBytes //_mallocGoodSize(for: numHeaderBytes + growTailBytes)
+  let totalTailBytes = growTailBytes //total - numHeaderBytes
 
   return (tailAllocator(totalTailBytes), totalTailBytes)
 }
