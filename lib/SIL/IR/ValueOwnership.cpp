fa3a9595249343db2072903a6b3e60fff2c6eda0
Erik Eckstein <eeckstein@apple.com>
2024-01-03 12:15:19 +0100

    ObjectOutliner: support outlining of classes in global let variables
    ```
      let c = SomeClass()
    ```
    
    is turned into
    
    ```
      private let outlinedVariable = SomeClass()  // statically initialized and allocated in the data section
      let c = outlinedVariable
    ```
    
    rdar://111021230
    rdar://115502043
    
    Also, make the ObjectOutliner work for OSSA. Though, it currently doesn't run in the OSSA pipeline.

--- lib/SIL/IR/ValueOwnership.cpp
+++ lib/SIL/IR/ValueOwnership.cpp
@@ -71,6 +71,7 @@ CONSTANT_OWNERSHIP_INST(Owned, WeakCopyValue)
 
 CONSTANT_OWNERSHIP_INST(Guaranteed, BeginBorrow)
 CONSTANT_OWNERSHIP_INST(Guaranteed, LoadBorrow)
+CONSTANT_OWNERSHIP_INST(None, GlobalValue)
 CONSTANT_OWNERSHIP_INST(Owned, AllocBox)
 CONSTANT_OWNERSHIP_INST(Owned, AllocExistentialBox)
 CONSTANT_OWNERSHIP_INST(Owned, AllocRef)
@@ -85,7 +86,6 @@ CONSTANT_OWNERSHIP_INST(Owned, EndInitLetRef)
 CONSTANT_OWNERSHIP_INST(Owned, BeginDeallocRef)
 CONSTANT_OWNERSHIP_INST(Owned, KeyPath)
 CONSTANT_OWNERSHIP_INST(Owned, InitExistentialValue)
-CONSTANT_OWNERSHIP_INST(Owned, GlobalValue) // TODO: is this correct?
 
 // One would think that these /should/ be unowned. In truth they are owned since
 // objc metatypes do not go through the retain/release fast path. In their
