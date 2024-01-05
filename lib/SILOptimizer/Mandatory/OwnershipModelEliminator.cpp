1b0817b234177d5faa7d49dbd4f49bbaf5b23688
Meghana Gupta <meghanavgupta@gmail.com>
2024-01-05 13:38:27 -0800

    Convert OME to a module pass

--- lib/SILOptimizer/Mandatory/OwnershipModelEliminator.cpp
+++ lib/SILOptimizer/Mandatory/OwnershipModelEliminator.cpp
@@ -788,7 +788,7 @@ static void prepareSILFunctionForOptimization(ModuleDecl *, SILFunction *f) {
 
 namespace {
 
-struct OwnershipModelEliminator : SILFunctionTransform {
+struct OwnershipModelEliminator : SILModuleTransform {
   bool skipTransparent;
   bool skipStdlibModule;
 
@@ -796,53 +796,44 @@ struct OwnershipModelEliminator : SILFunctionTransform {
       : skipTransparent(skipTransparent), skipStdlibModule(skipStdlibModule) {}
 
   void run() override {
-    if (DumpBefore.size()) {
-      getFunction()->dump(DumpBefore.c_str());
+    auto *mod = getModule();
+
+    for (auto &f : *mod) {
+      if (!f.hasOwnership())
+        continue;;
+
+      // If we were asked to not strip ownership from transparent functions in
+      // /our/ module, return.
+      if (skipTransparent && f.isTransparent())
+        continue;
+
+      // Verify here to make sure ownership is correct before we strip.
+      {
+        // Add a pretty stack trace entry to tell users who see a verification
+        // failure triggered by this verification check that they need to re-run
+        // with -sil-verify-all to actually find the pass that introduced the
+        // verification error.
+        //
+        // DISCUSSION: This occurs due to the crash from the verification
+        // failure happening in the pass itself. This causes us to dump the
+        // SILFunction and emit a msg that this pass (OME) is the culprit. This
+        // is generally correct for most passes, but not for OME since we are
+        // verifying before we have even modified the function to ensure that
+        // all ownership invariants have been respected before we lower
+        // ownership from the function.
+        llvm::PrettyStackTraceString silVerifyAllMsgOnFailure(
+            "Found verification error when verifying before lowering "
+            "ownership. Please re-run with -sil-verify-all to identify the "
+            "actual pass that introduced the verification error.");
+        f.verify(getAnalysis<BasicCalleeAnalysis>()->getCalleeCache());
+      }
+
+      if (stripOwnership(f)) {
+        auto InvalidKind =
+            SILAnalysis::InvalidationKind::BranchesAndInstructions;
+        invalidateAnalysis(&f, InvalidKind);
+      }
     }
-
-    auto *f = getFunction();
-    auto &mod = getFunction()->getModule();
-
-    // If we are supposed to skip the stdlib module and we are in the stdlib
-    // module bail.
-    if (skipStdlibModule && mod.isStdlibModule()) {
-      return;
-    }
-
-    if (!f->hasOwnership())
-      return;
-
-    // If we were asked to not strip ownership from transparent functions in
-    // /our/ module, return.
-    if (skipTransparent && f->isTransparent())
-      return;
-
-    // Verify here to make sure ownership is correct before we strip.
-    {
-      // Add a pretty stack trace entry to tell users who see a verification
-      // failure triggered by this verification check that they need to re-run
-      // with -sil-verify-all to actually find the pass that introduced the
-      // verification error.
-      //
-      // DISCUSSION: This occurs due to the crash from the verification
-      // failure happening in the pass itself. This causes us to dump the
-      // SILFunction and emit a msg that this pass (OME) is the culprit. This
-      // is generally correct for most passes, but not for OME since we are
-      // verifying before we have even modified the function to ensure that
-      // all ownership invariants have been respected before we lower
-      // ownership from the function.
-      llvm::PrettyStackTraceString silVerifyAllMsgOnFailure(
-          "Found verification error when verifying before lowering "
-          "ownership. Please re-run with -sil-verify-all to identify the "
-          "actual pass that introduced the verification error.");
-      f->verify(getAnalysis<BasicCalleeAnalysis>()->getCalleeCache());
-    }
-
-    if (stripOwnership(*f)) {
-      auto InvalidKind = SILAnalysis::InvalidationKind::BranchesAndInstructions;
-      invalidateAnalysis(InvalidKind);
-    }
-
     // If we were asked to strip transparent, we are at the beginning of the
     // performance pipeline. In such a case, we register a handler so that all
     // future things we deserialize have ownership stripped.
@@ -850,17 +841,17 @@ struct OwnershipModelEliminator : SILFunctionTransform {
         FunctionBodyDeserializationNotificationHandler;
     std::unique_ptr<DeserializationNotificationHandler> ptr;
     if (skipTransparent) {
-      if (!mod.hasRegisteredDeserializationNotificationHandlerForNonTransparentFuncOME()) {
+      if (!mod->hasRegisteredDeserializationNotificationHandlerForNonTransparentFuncOME()) {
         ptr.reset(new NotificationHandlerTy(
             prepareNonTransparentSILFunctionForOptimization));
-        mod.registerDeserializationNotificationHandler(std::move(ptr));
-        mod.setRegisteredDeserializationNotificationHandlerForNonTransparentFuncOME();
+        mod->registerDeserializationNotificationHandler(std::move(ptr));
+        mod->setRegisteredDeserializationNotificationHandlerForNonTransparentFuncOME();
       }
     } else {
-      if (!mod.hasRegisteredDeserializationNotificationHandlerForAllFuncOME()) {
+      if (!mod->hasRegisteredDeserializationNotificationHandlerForAllFuncOME()) {
         ptr.reset(new NotificationHandlerTy(prepareSILFunctionForOptimization));
-        mod.registerDeserializationNotificationHandler(std::move(ptr));
-        mod.setRegisteredDeserializationNotificationHandlerForAllFuncOME();
+        mod->registerDeserializationNotificationHandler(std::move(ptr));
+        mod->setRegisteredDeserializationNotificationHandlerForAllFuncOME();
       }
     }
   }
