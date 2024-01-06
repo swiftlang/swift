e69662449834551667e2f157af10bbe5e96c2a33
Holly Borla <hborla@apple.com>
2024-01-05 22:02:14 -0800

    [NFC] Don't run nonisolated(unsafe) test with SendNonSendable on the 5.10 branch.

--- test/Concurrency/experimental_feature_strictconcurrency.swift
+++ test/Concurrency/experimental_feature_strictconcurrency.swift
@@ -1,6 +1,5 @@
 // RUN: %target-swift-frontend -disable-availability-checking -parse-as-library -enable-experimental-feature StrictConcurrency -enable-experimental-feature GlobalConcurrency -emit-sil -o /dev/null -verify %s
 // RUN: %target-swift-frontend -disable-availability-checking -parse-as-library -enable-experimental-feature StrictConcurrency=complete -enable-experimental-feature GlobalConcurrency -emit-sil -o /dev/null -verify %s
-// RUN: %target-swift-frontend -disable-availability-checking -parse-as-library -enable-experimental-feature StrictConcurrency=complete -enable-experimental-feature GlobalConcurrency -emit-sil -o /dev/null -verify -enable-experimental-feature SendNonSendable %s
 
 // REQUIRES: concurrency
 // REQUIRES: asserts
