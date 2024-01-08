53979a2fcc31d5f6baf8472f55c0cb35b4def55f
Egor Zhdan <e_zhdan@apple.com>
2024-01-08 15:46:34 +0000

    [cxx-interop] More tests for move-only C++ types

--- test/Interop/Cxx/class/type-classification-typechecker.swift
+++ test/Interop/Cxx/class/type-classification-typechecker.swift
@@ -3,9 +3,23 @@
 import TypeClassification
 
 func test() {
+  let x = StructWithPointerNonCopyableTriviallyMovable()
+  let v = copy x // expected-error {{'copy' cannot be applied to noncopyable types}}
+  _ = v
+}
+
+func testField() {
+  let x = StructWithPointerNonCopyableTriviallyMovableField()
+  let v = copy x // expected-error {{'copy' cannot be applied to noncopyable types}}
+  _ = v
+}
+
+func testAnnotated() {
   let x = StructCopyableMovableAnnotatedNonCopyable()
   let v = copy x // expected-error {{'copy' cannot be applied to noncopyable types}}
   _ = v
 }
 
 test()
+testField()
+testAnnotated()
