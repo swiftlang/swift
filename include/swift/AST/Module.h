30a4740962caee4f078ec09649261aecd6000be7
Kavon Farvardin <kfarvardin@apple.com>
2024-01-11 14:52:20 -0800

    [NFC] clean-up collectExistentialConformances

--- include/swift/AST/Module.h
+++ include/swift/AST/Module.h
@@ -853,8 +853,10 @@ public:
   ProtocolConformanceRef conformsToProtocol(Type sourceTy,
                                             ProtocolDecl *targetProtocol);
 
-  /// Collect the conformances of \c fromType each of the protocols of an
+  /// Collect the conformances of \c fromType to each of the protocols of an
   /// existential type's layout.
+  ///
+  /// See `TypeChecker::containsProtocol` for details on the boolean arguments.
   ArrayRef<ProtocolConformanceRef>
   collectExistentialConformances(CanType fromType, CanType existential,
                                  bool skipConditionalRequirements = true,
