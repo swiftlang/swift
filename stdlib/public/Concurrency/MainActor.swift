70b1c0e43cb584fef30840d29a0a7732793715f8
Konrad `ktoso` Malawski <ktoso@apple.com>
2024-01-11 16:48:32 +0900

    [Concurrency] Major cleanup of assert/precondition/assume isolated docs (#70800)
    Co-authored-by: Alex Martini <amartini@apple.com>

--- stdlib/public/Concurrency/MainActor.swift
+++ stdlib/public/Concurrency/MainActor.swift
@@ -101,19 +101,41 @@ extension MainActor {
 
 @available(SwiftStdlib 5.1, *)
 extension MainActor {
-  /// A safe way to synchronously assume that the current execution context belongs to the MainActor.
+  /// Assume that the current task is executing on the main actor's
+  /// serial executor, or stop program execution.
   ///
-  /// This API should only be used as last resort, when it is not possible to express the current
-  /// execution context definitely belongs to the main actor in other ways. E.g. one may need to use
-  /// this in a delegate style API, where a synchronous method is guaranteed to be called by the
-  /// main actor, however it is not possible to annotate this legacy API with `@MainActor`.
+  /// This method allows to *assume and verify* that the currently
+  /// executing synchronous function is actually executing on the serial
+  /// executor of the MainActor.
   ///
-  /// - Warning: If the current executor is *not* the MainActor's serial executor, this function will crash.
+  /// If that is the case, the operation is invoked with an `isolated` version
+  /// of the actor, / allowing synchronous access to actor local state without
+  /// hopping through asynchronous boundaries.
   ///
-  /// Note that this check is performed against the MainActor's serial executor, meaning that
-  /// if another actor uses the same serial executor--by using ``MainActor/sharedUnownedExecutor``
-  /// as its own ``Actor/unownedExecutor``--this check will succeed, as from a concurrency safety
-  /// perspective, the serial executor guarantees mutual exclusion of those two actors.
+  /// If the current context is not running on the actor's serial executor, or
+  /// if the actor is a reference to a remote actor, this method will crash
+  /// with a fatal error (similar to ``preconditionIsolated()``).
+  ///
+  /// This method can only be used from synchronous functions, as asynchronous
+  /// functions should instead perform a normal method call to the actor, which
+  /// will hop task execution to the target actor if necessary.
+  ///
+  /// - Note: This check is performed against the MainActor's serial executor,
+  ///   meaning that / if another actor uses the same serial executor--by using
+  ///   ``MainActor/sharedUnownedExecutor`` as its own
+  ///   ``Actor/unownedExecutor``--this check will succeed , as from a concurrency
+  ///   safety perspective, the serial executor guarantees mutual exclusion of
+  ///   those two actors.
+  ///
+  /// - Parameters:
+  ///   - operation: the operation that will be executed if the current context
+  ///                is executing on the MainActor's serial executor.
+  ///   - file: The file name to print if the assertion fails. The default is
+  ///           where this method was called.
+  ///   - line: The line number to print if the assertion fails The default is
+  ///           where this method was called.
+  /// - Returns: the return value of the `operation`
+  /// - Throws: rethrows the `Error` thrown by the operation if it threw
   @available(SwiftStdlib 5.1, *)
   @backDeployed(before: SwiftStdlib 5.9)
   @_unavailableFromAsync(message: "await the call to the @MainActor closure directly")
