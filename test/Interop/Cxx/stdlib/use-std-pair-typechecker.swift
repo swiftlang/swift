e9dd9109e743f0978a6cd944fe171825067d4ef7
Egor Zhdan <e_zhdan@apple.com>
2024-01-12 16:21:43 +0000

    [cxx-interop] Update the `std::pair` safety test
    The test was passing by accident: we didn't properly check `std::pair` type for completeness, and assumed that it is incomplete.
    
    This updates the test to actually verify the behavior that is specified in the vision document.

--- test/Interop/Cxx/stdlib/use-std-pair-typechecker.swift
+++ test/Interop/Cxx/stdlib/use-std-pair-typechecker.swift
@@ -1,7 +1,12 @@
-// RUN: %target-typecheck-verify-swift -I %S/Inputs -enable-experimental-cxx-interop
+// RUN: not %target-swift-frontend %s -typecheck -I %S/Inputs -enable-experimental-cxx-interop 2>&1 | %FileCheck %s
 
 import StdPair
 
 let u = HasMethodThatReturnsUnsafePair()
-u.getUnsafePair() // expected-error {{value of type 'HasMethodThatReturnsUnsafePair' has no member 'getUnsafePair'}}
-u.getIteratorPair() // expected-error {{value of type 'HasMethodThatReturnsUnsafePair' has no member 'getIteratorPair'}}
+u.getUnsafePair()
+// CHECK: error: value of type 'HasMethodThatReturnsUnsafePair' has no member 'getUnsafePair'
+// CHECK: note: C++ method 'getUnsafePair' may return an interior pointer
+
+u.getIteratorPair()
+// CHECK: error: value of type 'HasMethodThatReturnsUnsafePair' has no member 'getIteratorPair'
+// CHECK: note: C++ method 'getIteratorPair' may return an interior pointer
