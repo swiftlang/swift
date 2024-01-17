6f82fd3ea7e2e100631f3fc9c55699be62b26187
Sophia Poirier <spoirier@apple.com>
2024-01-16 18:00:10 -0800

    [Concurrency] bypass global variable static checking for @TaskLocal property wrapper declarations

--- test/Concurrency/task_local.swift
+++ test/Concurrency/task_local.swift
@@ -8,18 +8,14 @@
 @available(SwiftStdlib 5.1, *)
 struct TL {
   @TaskLocal
-  static var number: Int = 0 // expected-complete-warning {{static property 'number' is not concurrency-safe because it is non-isolated global shared mutable state; this is an error in Swift 6}}
-  // expected-complete-note@-1 {{isolate 'number' to a global actor, or convert it to a 'let' constant and conform it to 'Sendable'}}
+  static var number: Int = 0
 
   @TaskLocal
-  static var someNil: Int? // expected-complete-warning {{static property 'someNil' is not concurrency-safe because it is non-isolated global shared mutable state; this is an error in Swift 6}}
-  // expected-complete-note@-1 {{isolate 'someNil' to a global actor, or convert it to a 'let' constant and conform it to 'Sendable'}}
+  static var someNil: Int?
 
   @TaskLocal
   static var noValue: Int // expected-error{{'static var' declaration requires an initializer expression or an explicitly stated getter}}
   // expected-note@-1{{add an initializer to silence this error}}
-  // expected-complete-warning@-2 {{static property 'noValue' is not concurrency-safe because it is non-isolated global shared mutable state; this is an error in Swift 6}}
-  // expected-complete-note@-3 {{isolate 'noValue' to a global actor, or convert it to a 'let' constant and conform it to 'Sendable'}}
 
   @TaskLocal
   var notStatic: String? // expected-error{{property 'notStatic', must be static because property wrapper 'TaskLocal<String?>' can only be applied to static properties}}
