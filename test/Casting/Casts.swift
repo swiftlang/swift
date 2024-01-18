4ea5ed1309c784f02d83536de1644842c93e616b
Tim Kientzle <tkientzle@apple.com>
2024-01-17 18:14:57 -0800

    Revert #68952 [Casting] Make more casts look inside __SwiftValue
    Turns out that our Obj-C bridging relies on this inconsistent behavior,
    so it can quite likely never be actually fixed.

--- test/Casting/Casts.swift
+++ test/Casting/Casts.swift
@@ -1065,13 +1065,24 @@ CastsTests.test("Don't put AnyHashable inside AnyObject") {
 }
 
 #if _runtime(_ObjC)
-CastsTests.test("__SwiftValue should not be obvious to `is`") {
+// We currently (as of Jan 2024) bridge NSSet to Swift with `x as!
+// Set<NSObject>`, which in turn demands that __SwiftValue successfully cast to
+// NSObject.
+// So this nonsensical behavior can probably never be fixed.
+// (It's nonsense because it implies that every Swift object is derived
+// from NSObject.)  See PR #68952 for an early attempt to change it which
+// had to be reverted.
+CastsTests.test("__SwiftValue should not be obvious to `is`")
+.xfail(.always("Probably can never be fixed"))
+.code {
   struct S {}
   let s = S() as AnyObject
   expectFalse(s is NSObject)
 }
 #endif
 
+// See above for reasons why this might need to remain broken forever,
+// though I do have some hope for it.
 CastsTests.test("type(of:) should look through __SwiftValue")
 .xfail(.always("Known to be broken"))
 .code {
