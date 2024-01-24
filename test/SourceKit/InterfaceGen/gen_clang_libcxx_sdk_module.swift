63a71c9599a553b378f0ea7491212a510c8d1111
Pavel Yaskevich <xedin@apache.org>
2024-01-24 13:37:58 -0800

    [cxx-interop] Temporarily disable tests failing because of a libc++ modulemap change
    rdar://119869070

--- test/SourceKit/InterfaceGen/gen_clang_libcxx_sdk_module.swift
+++ test/SourceKit/InterfaceGen/gen_clang_libcxx_sdk_module.swift
@@ -1,6 +1,7 @@
 // RUN: %sourcekitd-test -req=interface-gen -module CxxStdlib -- -Xfrontend -disable-implicit-concurrency-module-import -Xfrontend -disable-implicit-string-processing-module-import -cxx-interoperability-mode=default -target %target-triple -sdk %sdk | %FileCheck %s
 
 // REQUIRES: OS=macosx
+// REQUIRES: rdar119869070
 
 // CHECK: import CxxStdlib.vector
 // CHECK: extension std.basic_string<CChar, char_traits<CChar>, allocator<CChar>> {
