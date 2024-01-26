633d5bc45a7c0a7b0e481701cf3f8fc2025f8f51
Max Desiatov <m_desiatov@apple.com>
2024-01-26 21:39:17 +0000

    Enable `x86_64` and `i686` ELF triples for embedded Swift (#70642)
    This is just as useful for x86 bare metal as the already enabled `arm*-none-none-eabi` triples.

--- stdlib/public/core/VarArgs.swift
+++ stdlib/public/core/VarArgs.swift
@@ -413,7 +413,7 @@ extension Double: _CVarArgPassedAsDouble, _CVarArgAligned {
   }
 }
 
-#if !(os(Windows) || os(Android)) && (arch(i386) || arch(x86_64))
+#if !(os(Windows) || os(Android) || ($Embedded && !os(Linux) && !(os(macOS) || os(iOS) || os(watchOS) || os(tvOS)))) && (arch(i386) || arch(x86_64))
 extension Float80: CVarArg, _CVarArgAligned {
   /// Transform `self` into a series of machine words that can be
   /// appropriately interpreted by C varargs.
