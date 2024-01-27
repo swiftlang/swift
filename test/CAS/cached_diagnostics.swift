52671e1a6a223eb877787a666c7a3615fb832e13
Steven Wu <stevenwu@apple.com>
2024-01-26 17:13:26 -0800

    [Caching] Improve swift caching related tests
    Improve caching related tests to make them faster and test the situation
    closer to what actually happens during explicit module build. Most
    noticable changes are:
    * Avoid swift stdlib dependency during the tests to save time
    * Use dependency scanner output to construct test cases
    * Update old test cases that try to simulate caching from file system
      inputs to using only CAS inputs.

--- test/CAS/cached_diagnostics.swift
+++ test/CAS/cached_diagnostics.swift
@@ -1,28 +1,41 @@
 // RUN: %empty-directory(%t)
 
-// RUN: %target-swift-frontend -c -cache-compile-job -cas-path %t/cas -allow-unstable-cache-key-for-testing %s \
-// RUN:   -import-objc-header %S/Inputs/objc.h -emit-module -emit-module-path %t/test.swiftmodule 2>&1 | %FileCheck %s
-// RUN: %cache-tool -cas-path %t/cas -cache-tool-action print-output-keys -- %target-swift-frontend -c -cache-compile-job -cas-path %t/cas -allow-unstable-cache-key-for-testing %s \
-// RUN:   -import-objc-header %S/Inputs/objc.h -emit-module -emit-module-path %t/test.swiftmodule > %t/cache_key.json
-// RUN: %cache-tool -cas-path %t/cas -cache-tool-action render-diags %t/cache_key.json -- %target-swift-frontend -c -cache-compile-job -cas-path %t/cas -allow-unstable-cache-key-for-testing %s \
-// RUN:   -import-objc-header %S/Inputs/objc.h -emit-module -emit-module-path %t/test.swiftmodule 2>&1 | %FileCheck %s
+// RUN: %target-swift-frontend -scan-dependencies -module-name Test -O -import-objc-header %S/Inputs/objc.h \
+// RUN:   -disable-implicit-string-processing-module-import -disable-implicit-concurrency-module-import -parse-stdlib \
+// RUN:   %s -o %t/deps.json -cache-compile-job -cas-path %t/cas
+
+// RUN: %{python} %S/Inputs/BuildCommandExtractor.py %t/deps.json bridgingHeader | tail -n +2  > %t/header.cmd
+// RUN: %target-swift-frontend @%t/header.cmd -disable-implicit-swift-modules %S/Inputs/objc.h -O -o %t/objc.pch 2>&1 | %FileCheck %s -check-prefix CHECK-BRIDGE
+// RUN: %cache-tool -cas-path %t/cas -cache-tool-action print-output-keys -- \
+// RUN:   %target-swift-frontend @%t/header.cmd -disable-implicit-swift-modules %S/Inputs/objc.h -O -o %t/objc.pch > %t/keys.json
+// RUN: %{python} %S/Inputs/ExtractOutputKey.py %t/keys.json %S/Inputs/objc.h > %t/key
+
+// RUN: %{python} %S/Inputs/BuildCommandExtractor.py %t/deps.json Test > %t/MyApp.cmd
+// RUN: echo "\"-disable-implicit-string-processing-module-import\"" >> %t/MyApp.cmd
+// RUN: echo "\"-disable-implicit-concurrency-module-import\"" >> %t/MyApp.cmd
+// RUN: echo "\"-disable-implicit-swift-modules\"" >> %t/MyApp.cmd
+// RUN: echo "\"-parse-stdlib\"" >> %t/MyApp.cmd
+// RUN: echo "\"-import-objc-header\"" >> %t/MyApp.cmd
+// RUN: echo "\"%t/objc.pch\"" >> %t/MyApp.cmd
+// RUN: echo "\"-bridging-header-pch-key\"" >> %t/MyApp.cmd
+// RUN: echo "\"@%t/key\"" >> %t/MyApp.cmd
+
+// RUN: %target-swift-frontend  -cache-compile-job -module-name Test -O -cas-path %t/cas @%t/MyApp.cmd %s \
+// RUN:   -emit-module -o %t/test.swiftmodule 2>&1 | %FileCheck %s
+// RUN: %cache-tool -cas-path %t/cas -cache-tool-action print-output-keys -- %target-swift-frontend -cache-compile-job -module-name Test -O -cas-path %t/cas @%t/MyApp.cmd %s \
+// RUN:   -emit-module -o %t/test.swiftmodule > %t/cache_key.json
+// RUN: %cache-tool -cas-path %t/cas -cache-tool-action render-diags %t/cache_key.json -- %target-swift-frontend -cache-compile-job -module-name Test -O -cas-path %t/cas @%t/MyApp.cmd %s \
+// RUN:   -emit-module -o %t/test.swiftmodule 2>&1 | %FileCheck %s
 
 #warning("this is a warning")  // expected-warning {{this is a warning}}
 
-// CHECK: warning: warning in bridging header
+// CHECK-BRIDGE: warning: warning in bridging header
 // CHECK: warning: this is a warning
 
 /// Check other DiagnosticConsumers.
-// RUN: %target-swift-frontend -c -cache-compile-job -cas-path %t/cas -allow-unstable-cache-key-for-testing %s \
+// RUN: %target-swift-frontend -c -cache-compile-job -module-name Test -O -cas-path %t/cas @%t/MyApp.cmd %s \
 // RUN:   -typecheck -serialize-diagnostics -serialize-diagnostics-path %t/test.diag -verify
 // RUN: %FileCheck %s -check-prefix CHECK-SERIALIZED <%t/test.diag
 
-// Verify the serialized diags have the right magic at the top.
+/// Verify the serialized diags have the right magic at the top.
 // CHECK-SERIALIZED: DIA
-
-/// Check path remapping.
-// RUN: %cache-tool -cas-path %t/cas -cache-tool-action render-diags %t/cache_key.json -- %target-swift-frontend -c -cache-compile-job -cas-path %t/cas -allow-unstable-cache-key-for-testing %s \
-// RUN:   -import-objc-header %S/Inputs/objc.h -emit-module -emit-module-path %t/test.swiftmodule -cache-replay-prefix-map %S=/^test  2>&1 | %FileCheck %s --check-prefix REMAP
-
-// REMAP: /^test/Inputs/objc.h:3:2: warning: warning in bridging header
-// REMAP: /^test/cached_diagnostics.swift:10:10: warning: this is a warning
--- test/CAS/cas-explicit-module-map.swift
+++ test/CAS/cas-explicit-module-map.swift
@@ -1,140 +1,51 @@
 // RUN: %empty-directory(%t)
 // RUN: mkdir -p %t/cas
 // RUN: split-file %s %t
-// RUN: %target-swift-emit-pcm -Xfrontend -cache-compile-job -Xfrontend -allow-unstable-cache-key-for-testing -Xfrontend -cas-path -Xfrontend %t/cas -module-cache-path %t/clang-module-cache -module-name SwiftShims %swift-lib-dir/swift/shims/module.modulemap -o %t/SwiftShims.pcm
-// RUN: %target-swift-emit-pcm -Xfrontend -cache-compile-job -Xfrontend -allow-unstable-cache-key-for-testing -Xfrontend -cas-path -Xfrontend %t/cas -module-cache-path %t/clang-module-cache -module-name SwiftShims %swift-lib-dir/swift/shims/module.modulemap -o %t/SwiftShims.pcm -### > %t/Shims.cmd
-// RUN: %cache-tool -cas-path %t/cas -cache-tool-action print-output-keys -- @%t/Shims.cmd > %t/Shims.key.json
-// RUN: %{python} %S/Inputs/ExtractOutputKey.py %t/Shims.key.json %t/SwiftShims.pcm | tr -d '\n' > %t/Shims.key
 
-// RUN: %target-swift-emit-pcm -Xfrontend -cache-compile-job -Xfrontend -allow-unstable-cache-key-for-testing -module-cache-path %t/clang-module-cache -Xfrontend -cas-path -Xfrontend %t/cas -module-name _SwiftConcurrencyShims %swift-lib-dir/swift/shims/module.modulemap -o %t/_SwiftConcurrencyShims.pcm
-// RUN: %target-swift-emit-pcm -Xfrontend -cache-compile-job -Xfrontend -allow-unstable-cache-key-for-testing -module-cache-path %t/clang-module-cache -Xfrontend -cas-path -Xfrontend %t/cas -module-name _SwiftConcurrencyShims %swift-lib-dir/swift/shims/module.modulemap -o %t/_SwiftConcurrencyShims.pcm -### > %t/ConcurrencyShims.cmd
-// RUN: %cache-tool -cas-path %t/cas -cache-tool-action print-output-keys -- @%t/ConcurrencyShims.cmd > %t/ConcurrencyShims.key.json
-// RUN: %{python} %S/Inputs/ExtractOutputKey.py %t/ConcurrencyShims.key.json %t/_SwiftConcurrencyShims.pcm | tr -d '\n' > %t/ConcurrencyShims.key
+// RUN: %target-swift-frontend -emit-module -module-cache-path %t/clang-module-cache %t/A.swift -disable-implicit-string-processing-module-import -disable-implicit-concurrency-module-import -parse-stdlib -o %t/A.swiftmodule -swift-version 5
+// RUN: %target-swift-frontend -emit-module -module-cache-path %t/clang-module-cache %t/B.swift -disable-implicit-string-processing-module-import -disable-implicit-concurrency-module-import -parse-stdlib -o %t/B.swiftmodule -I %t -swift-version 5
 
-// RUN: %target-swift-frontend -emit-module -module-cache-path %t/clang-module-cache %t/A.swift -o %t/A.swiftmodule -swift-version 5
-// RUN: %target-swift-frontend -emit-module -module-cache-path %t/clang-module-cache %t/B.swift -o %t/B.swiftmodule -I %t -swift-version 5
-// RUN: %target-swift-frontend -scan-dependencies -module-cache-path %t/clang-module-cache %t/Test.swift -o %t/deps.json -I %t -swift-version 5 -cache-compile-job -cas-path %t/cas
+// RUN: %target-swift-frontend -scan-dependencies -module-cache-path %t/clang-module-cache %t/Test.swift -o %t/deps.json -I %t -module-name Test \
+// RUN:    -swift-version 5 -cache-compile-job -cas-path %t/cas -disable-implicit-string-processing-module-import -disable-implicit-concurrency-module-import -parse-stdlib
 // RUN: %validate-json %t/deps.json &>/dev/null
 
-// RUN: %{python} %S/Inputs/SwiftDepsExtractor.py %t/deps.json swiftPrebuiltExternal:A moduleCacheKey | tr -d '\n' > %t/A.key
-// RUN: %{python} %S/Inputs/SwiftDepsExtractor.py %t/deps.json swiftPrebuiltExternal:B moduleCacheKey | tr -d '\n' > %t/B.key
-
-/// Prepare the cas objects that can be used to construct CompileJobResultSchema object.
-// RUN: llvm-cas --cas %t/cas --get-cache-result @%t/A.key > %t/A.result
-// RUN: llvm-cas --cas %t/cas --ls-node-refs @%t/A.result | tail -n 1 > %t/schema.casid
-// RUN: llvm-cas --cas %t/cas --cat-blob @%t/A.result > %t/kind.blob
-
-/// Make keys for module loads. The result casid construction is tied with the actual structure of CompilerJobResultSchema.
-// RUN: llvm-cas --cas %t/cas --make-blob --data %stdlib_module | tr -d '\n' > %t/Swift.key
-// RUN: llvm-cas --cas %t/cas --make-node --data %t/kind.blob @%t/Swift.key @%t/schema.casid  > %t/Swift.casid
-// RUN: llvm-cas --cas %t/cas --put-cache-key @%t/Swift.key @%t/Swift.casid
-
-// RUN: llvm-cas --cas %t/cas --make-blob --data %ononesupport_module | tr -d '\n' > %t/ONone.key
-// RUN: llvm-cas --cas %t/cas --make-node --data %t/kind.blob @%t/ONone.key @%t/schema.casid > %t/ONone.casid
-// RUN: llvm-cas --cas %t/cas --put-cache-key @%t/ONone.key @%t/ONone.casid
-
-// RUN: llvm-cas --cas %t/cas --make-blob --data %concurrency_module | tr -d '\n' > %t/Concurrency.key
-// RUN: llvm-cas --cas %t/cas --make-node --data %t/kind.blob @%t/Concurrency.key @%t/schema.casid > %t/Concurrency.casid
-// RUN: llvm-cas --cas %t/cas --put-cache-key @%t/Concurrency.key @%t/Concurrency.casid
-
-// RUN: llvm-cas --cas %t/cas --make-blob --data %string_processing_module | tr -d '\n' > %t/String.key
-// RUN: llvm-cas --cas %t/cas --make-node --data %t/kind.blob @%t/String.key @%t/schema.casid > %t/String.casid
-// RUN: llvm-cas --cas %t/cas --put-cache-key @%t/String.key @%t/String.casid
-
-// RUN: echo "[{" > %/t/map.json
-// RUN: echo "\"moduleName\": \"A\"," >> %/t/map.json
-// RUN: echo "\"modulePath\": \"A.swiftmodule\"," >> %/t/map.json
-// RUN: echo -n "\"moduleCacheKey\": " >> %/t/map.json
-// RUN: cat %t/A.key >> %/t/map.json
-// RUN: echo "," >> %/t/map.json
-// RUN: echo "\"isFramework\": false" >> %/t/map.json
-// RUN: echo "}," >> %/t/map.json
-// RUN: echo "{" >> %/t/map.json
-// RUN: echo "\"moduleName\": \"B\"," >> %/t/map.json
-// RUN: echo "\"modulePath\": \"B.swiftmodule\"," >> %/t/map.json
-// RUN: echo -n "\"moduleCacheKey\": " >> %/t/map.json
-// RUN: cat %t/B.key >> %/t/map.json
-// RUN: echo "," >> %/t/map.json
-// RUN: echo "\"isFramework\": false" >> %/t/map.json
-// RUN: echo "}," >> %/t/map.json
-// RUN: echo "{" >> %/t/map.json
-// RUN: echo "\"moduleName\": \"Swift\"," >> %/t/map.json
-// RUN: echo "\"modulePath\": \"Swift.swiftmodule\"," >> %/t/map.json
-// RUN: echo -n "\"moduleCacheKey\": \"" >> %/t/map.json
-// RUN: cat %t/Swift.key >> %/t/map.json
-// RUN: echo "\"," >> %/t/map.json
-// RUN: echo "\"isFramework\": false" >> %/t/map.json
-// RUN: echo "}," >> %/t/map.json
-// RUN: echo "{" >> %/t/map.json
-// RUN: echo "\"moduleName\": \"SwiftOnoneSupport\"," >> %/t/map.json
-// RUN: echo "\"modulePath\": \"SwiftOnoneSupport.swiftmodule\"," >> %/t/map.json
-// RUN: echo -n "\"moduleCacheKey\": \"" >> %/t/map.json
-// RUN: cat %t/ONone.key >> %/t/map.json
-// RUN: echo "\"," >> %/t/map.json
-// RUN: echo "\"isFramework\": false" >> %/t/map.json
-// RUN: echo "}," >> %/t/map.json
-// RUN: echo "{" >> %/t/map.json
-// RUN: echo "\"moduleName\": \"_Concurrency\"," >> %/t/map.json
-// RUN: echo "\"modulePath\": \"_Concurrency.swiftmodule\"," >> %/t/map.json
-// RUN: echo -n "\"moduleCacheKey\": \"" >> %/t/map.json
-// RUN: cat %t/Concurrency.key >> %/t/map.json
-// RUN: echo "\"," >> %/t/map.json
-// RUN: echo "\"isFramework\": false" >> %/t/map.json
-// RUN: echo "}," >> %/t/map.json
-// RUN: echo "{" >> %/t/map.json
-// RUN: echo "\"moduleName\": \"_StringProcessing\"," >> %/t/map.json
-// RUN: echo "\"modulePath\": \"_StringProcessing.swiftmodule\"," >> %/t/map.json
-// RUN: echo -n "\"moduleCacheKey\": \"" >> %/t/map.json
-// RUN: cat %t/String.key >> %/t/map.json
-// RUN: echo "\"," >> %/t/map.json
-// RUN: echo "\"isFramework\": false" >> %/t/map.json
-// RUN: echo "}," >> %/t/map.json
-// RUN: echo "{" >> %/t/map.json
-// RUN: echo "\"moduleName\": \"SwiftShims\"," >> %/t/map.json
-// RUN: echo "\"clangModulePath\": \"SwiftShims.pcm\"," >> %/t/map.json
-// RUN: echo -n "\"clangModuleCacheKey\": \"" >> %/t/map.json
-// RUN: cat %t/Shims.key >> %/t/map.json
-// RUN: echo "\"," >> %/t/map.json
-// RUN: echo "\"isFramework\": false" >> %/t/map.json
-// RUN: echo "}," >> %/t/map.json
-// RUN: echo "{" >> %/t/map.json
-// RUN: echo "\"moduleName\": \"_SwiftConcurrencyShims\"," >> %/t/map.json
-// RUN: echo "\"clangModulePath\": \"_SwiftConcurrency.pcm\"," >> %/t/map.json
-// RUN: echo -n "\"clangModuleCacheKey\": \"" >> %/t/map.json
-// RUN: cat %t/ConcurrencyShims.key >> %/t/map.json
-// RUN: echo "\"," >> %/t/map.json
-// RUN: echo "\"isFramework\": false" >> %/t/map.json
-// RUN: echo "}]" >> %/t/map.json
-
+// RUN: %{python} %S/Inputs/GenerateExplicitModuleMap.py %t/deps.json > %t/map.json
 // RUN: llvm-cas --cas %t/cas --make-blob --data %t/map.json > %t/map.casid
 
-// RUN: %target-swift-frontend -emit-module -emit-module-path %t/Foo.swiftmodule -disable-implicit-swift-modules -module-cache-path %t.module-cache -explicit-swift-module-map-file @%t/map.casid -Rmodule-loading -Xcc -Rmodule-import %s -cache-compile-job -cas-path %t/cas -allow-unstable-cache-key-for-testing 2>&1 | %FileCheck %s
+// RUN: %{python} %S/Inputs/BuildCommandExtractor.py %t/deps.json Test > %t/MyApp.cmd
+// RUN: %target-swift-frontend \
+// RUN:   -typecheck -cache-compile-job -cas-path %t/cas \
+// RUN:   -swift-version 5 -disable-implicit-swift-modules \
+// RUN:   -disable-implicit-string-processing-module-import -disable-implicit-concurrency-module-import -parse-stdlib \
+// RUN:   -module-name Test -explicit-swift-module-map-file @%t/map.casid \
+// RUN:   %t/Test.swift @%t/MyApp.cmd
 
-// RUN: %target-swift-frontend -typecheck -emit-module-interface-path %t/Foo.swiftinterface -disable-implicit-swift-modules -module-cache-path %t.module-cache -explicit-swift-module-map-file @%t/map.casid %s -cache-compile-job -cas-path %t/cas -allow-unstable-cache-key-for-testing -swift-version 5 -enable-library-evolution -o %t/Foo.swiftmodule
+// RUN: %target-swift-frontend -emit-module -emit-module-path %t/Foo.swiftmodule -emit-module-interface-path %t/Foo.swiftinterface -disable-implicit-swift-modules \
+// RUN:   -module-cache-path %t.module-cache -explicit-swift-module-map-file @%t/map.casid %t/Test.swift -cache-compile-job \
+// RUN:   -cas-path %t/cas -swift-version 5 -enable-library-evolution -o %t/Foo.swiftmodule \
+// RUN:   -disable-implicit-string-processing-module-import -disable-implicit-concurrency-module-import -parse-stdlib @%t/MyApp.cmd
 // RUN: %cache-tool -cas-path %t/cas -cache-tool-action print-output-keys -- \
-// RUN:   %target-swift-frontend -typecheck -emit-module-interface-path %t/Foo.swiftinterface -disable-implicit-swift-modules \
-// RUN:   -module-cache-path %t.module-cache -explicit-swift-module-map-file @%t/map.casid %s -cache-compile-job \
-// RUN:   -cas-path %t/cas -allow-unstable-cache-key-for-testing  -swift-version 5 -enable-library-evolution > %t/keys.json
+// RUN:   %target-swift-frontend -emit-module -emit-module-path %t/Foo.swiftmodule -emit-module-interface-path %t/Foo.swiftinterface -disable-implicit-swift-modules \
+// RUN:   -module-cache-path %t.module-cache -explicit-swift-module-map-file @%t/map.casid %t/Test.swift -cache-compile-job \
+// RUN:   -cas-path %t/cas -swift-version 5 -enable-library-evolution -o %t/Foo.swiftmodule \
+// RUN:   -disable-implicit-string-processing-module-import -disable-implicit-concurrency-module-import -parse-stdlib @%t/MyApp.cmd > %t/keys.json
+// RUN: %{python} %S/Inputs/ExtractOutputKey.py %t/keys.json %t/Test.swift > %t/key
 
-// RUN: %{python} %S/Inputs/ExtractOutputKey.py %t/keys.json %t/Foo.swiftinterface > %t/interface.casid
 // RUN: %target-swift-frontend -typecheck-module-from-interface %t/Foo.swiftinterface -disable-implicit-swift-modules \
 // RUN:   -module-cache-path %t.module-cache -explicit-swift-module-map-file @%t/map.casid  \
-// RUN:   -cache-compile-job -cas-path %t/cas -allow-unstable-cache-key-for-testing -swift-version 5 -enable-library-evolution \
-// RUN:   -explicit-interface-module-build -Rcache-compile-job 2>&1 | %FileCheck %s --check-prefix=VERIFY-OUTPUT --check-prefix=CACHE-MISS
+// RUN:   -cache-compile-job -cas-path %t/cas -swift-version 5 -enable-library-evolution \
+// RUN:   -disable-implicit-string-processing-module-import -disable-implicit-concurrency-module-import -parse-stdlib \
+// RUN:   -explicit-interface-module-build -Rcache-compile-job @%t/MyApp.cmd -input-file-key @%t/key 2>&1 \
+// RUN:   | %FileCheck %s --check-prefix=VERIFY-OUTPUT --check-prefix=CACHE-MISS
 // RUN: %target-swift-frontend -typecheck-module-from-interface %t/Foo.swiftinterface -disable-implicit-swift-modules \
 // RUN:   -module-cache-path %t.module-cache -explicit-swift-module-map-file @%t/map.casid  \
-// RUN:   -cache-compile-job -cas-path %t/cas -allow-unstable-cache-key-for-testing -swift-version 5 -enable-library-evolution \
-// RUN:   -explicit-interface-module-build -Rcache-compile-job 2>&1 | %FileCheck %s --check-prefix=VERIFY-OUTPUT --check-prefix=CACHE-HIT
-
-// CHECK-DAG: loaded module 'A'
-// CHECK-DAG: loaded module 'B'
-// CHECK-DAG: loaded module 'Swift'
-// CHECK-DAG: loaded module '_StringProcessing'
-// CHECK-DAG: loaded module '_Concurrency'
-// CHECK-DAG: loaded module 'SwiftOnoneSupport'
+// RUN:   -cache-compile-job -cas-path %t/cas -swift-version 5 -enable-library-evolution \
+// RUN:   -disable-implicit-string-processing-module-import -disable-implicit-concurrency-module-import -parse-stdlib \
+// RUN:   -explicit-interface-module-build -Rcache-compile-job @%t/MyApp.cmd -input-file-key @%t/key 2>&1 \
+// RUN:   | %FileCheck %s --check-prefix=VERIFY-OUTPUT --check-prefix=CACHE-HIT
 
 // CACHE-MISS: remark: cache miss for input
-// VERIFY-OUTPUT: warning: module 'A' was not compiled with library evolution support
+// VERIFY-OUTPUT: warning: module 'B' was not compiled with library evolution support
 // CACHE-HIT: remark: replay output file
 
 //--- A.swift
