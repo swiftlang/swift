c8119e3c01d2d3ccbf523262e170d7ad3f67ac50
Nate Chandler <nate_chandler@apple.com>
2024-01-30 11:20:49 -0800

    [stdlib] FixedWidthInteger is BitwiseCopyable.

--- stdlib/public/core/Integers.swift
+++ stdlib/public/core/Integers.swift
@@ -1904,7 +1904,8 @@ public protocol _LosslessStringConvertibleOrNone {}
 /// customization points for arithmetic operations. When you provide just those
 /// methods, the standard library provides default implementations for all
 /// other arithmetic methods and operators.
-public protocol FixedWidthInteger: BinaryInteger, _LosslessStringConvertibleOrNone
+public protocol FixedWidthInteger
+: BinaryInteger, _LosslessStringConvertibleOrNone, _BitwiseCopyable
 where Magnitude: FixedWidthInteger & UnsignedInteger,
       Stride: FixedWidthInteger & SignedInteger {
   /// The number of bits used for the underlying binary representation of
