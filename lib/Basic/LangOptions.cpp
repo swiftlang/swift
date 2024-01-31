df7a65770471e6b7993d4616f591d29119310c15
Hamish Knight <hamish_knight@apple.com>
2024-01-31 17:32:42 +0000

    Enable `ImplicitLastExprResults` by default

--- lib/Basic/LangOptions.cpp
+++ lib/Basic/LangOptions.cpp
@@ -32,6 +32,7 @@
 using namespace swift;
 
 LangOptions::LangOptions() {
+  Features.insert(Feature::ImplicitLastExprResults);
   // Add all promoted language features
 #define LANGUAGE_FEATURE(FeatureName, SENumber, Description)                   \
   Features.insert(Feature::FeatureName);
