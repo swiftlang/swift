9895a54f70d1888736bd73e17ae6c2daaa27a49d
Slava Pestov <spestov@apple.com>
2024-01-31 22:54:22 -0500

    sil-opt: Enable Feature::NoncopyableGenerics when built with SWIFT_ENABLE_EXPERIMENTAL_NONCOPYABLE_GENERICS

--- lib/DriverTool/sil_opt_main.cpp
+++ lib/DriverTool/sil_opt_main.cpp
@@ -652,6 +652,9 @@ int sil_opt_main(ArrayRef<const char *> argv, void *MainAddr) {
     Invocation.getLangOptions().enableFeature(
         Feature::OldOwnershipOperatorSpellings);
   }
+  if (SWIFT_ENABLE_EXPERIMENTAL_NONCOPYABLE_GENERICS)
+    Invocation.getLangOptions().enableFeature(Feature::NoncopyableGenerics);
+
   Invocation.getLangOptions().BypassResilienceChecks =
       options.BypassResilienceChecks;
   Invocation.getDiagnosticOptions().PrintDiagnosticNames =
