312ac1be556c0ec0918872a29410196bcd5b78c7
Kavon Farvardin <kfarvardin@apple.com>
2024-02-04 23:03:20 -0800

    NCGenerics: handle MoveOnlyClasses correctly

---
+++ test/Generics/inverse_classes1.swift
@@ -0,0 +1,19 @@
+// RUN: %target-typecheck-verify-swift \
+// RUN:   -parse-stdlib -module-name Swift \
+// RUN:   -enable-experimental-feature MoveOnlyClasses \
+// RUN:   -enable-experimental-feature NoncopyableGenerics
+
+// NOTE: -parse-stdlib is a transitional workaround and should not be required.
+
+@_moveOnly
+class KlassLegacy {}
+
+class KlassModern: ~Copyable {}
+
+class Konditional<T: ~Copyable> {}
+
+func checks<T: ~Copyable, C>(
+          _ a: KlassLegacy, // expected-error {{parameter of noncopyable type 'KlassLegacy' must specify ownership}} // expected-note 3{{add}}
+          _ b: KlassModern, // expected-error {{parameter of noncopyable type 'KlassModern' must specify ownership}} // expected-note 3{{add}}
+          _ c: Konditional<T>,
+          _ d: Konditional<C>) {}
