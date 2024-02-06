5f0a9c475b7d945a712322c5ca26bb8b858bb988
Pavel Yaskevich <pyaskevich@apple.com>
2024-02-06 11:33:09 -0800

    [ClangImporter] Import NSNotificationName constants as `nonisolated`
    These constants could be used with observer APIs from a different isolation
    context, so it's more convenient to import them as `nonisolated` unless
    they are explicitly isolated to a MainActor.
    
    Resolves: rdar://114052705

---
+++ test/Concurrency/import_nsnotificationname_constants_as_nonisolated.swift
@@ -0,0 +1,45 @@
+// RUN: %empty-directory(%t/src)
+// RUN: %empty-directory(%t/sdk)
+// RUN: split-file %s %t/src
+
+// RUN: %target-swift-frontend(mock-sdk: %clang-importer-sdk) -typecheck %t/src/main.swift \
+// RUN:   -import-objc-header %t/src/Test.h \
+// RUN:   -strict-concurrency=complete \
+// RUN:   -disable-availability-checking \
+// RUN:   -module-name main -I %t -verify
+
+// REQUIRES: objc_interop
+// REQUIRES: concurrency
+
+//--- Test.h
+#define SWIFT_MAIN_ACTOR __attribute__((swift_attr("@MainActor")))
+
+#pragma clang assume_nonnull begin
+
+@import Foundation;
+
+SWIFT_MAIN_ACTOR
+@interface Test : NSObject
+@end
+
+extern NSNotificationName const TestDidTrigger __attribute__((swift_name("Test.didTrigger")));
+
+SWIFT_MAIN_ACTOR
+extern NSNotificationName const TestIsolatedTrigger __attribute__((swift_name("Test.isolatedTrigger")));
+
+#pragma clang assume_nonnull end
+
+//--- main.swift
+
+func testAsync() async {
+  print(Test.didTrigger) // Ok (property is nonisolated)
+  print(Test.isolatedTrigger)
+  // expected-warning@-1 {{expression is 'async' but is not marked with 'await'; this is an error in Swift 6}}
+  // expected-note@-2 {{property access is 'async'}}
+}
+
+@MainActor
+func testMainActor() {
+  print(Test.didTrigger) // Ok
+  print(Test.isolatedTrigger) // Ok
+}
