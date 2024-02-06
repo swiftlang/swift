ee12fd6655c1d81d77caaff14b2846f396bded7e
Kavon Farvardin <kfarvardin@apple.com>
2024-02-05 20:55:18 -0800

    NCGenerics: reenable round-trip for mangling

--- stdlib/cmake/modules/SwiftSource.cmake
+++ stdlib/cmake/modules/SwiftSource.cmake
@@ -610,7 +610,6 @@ function(_compile_swift_files
   if(SWIFT_ENABLE_EXPERIMENTAL_NONCOPYABLE_GENERICS)
     list(APPEND swift_flags "-enable-experimental-feature" "NoncopyableGenerics")
     list(APPEND swift_flags "-Xfrontend" "-enable-experimental-associated-type-inference")
-    list(APPEND swift_flags "-Xfrontend" "-disable-round-trip-debug-types") # NOTE: temporary until we fix mangling!
   endif()
 
   if (SWIFT_STDLIB_ENABLE_STRICT_CONCURRENCY_COMPLETE)
