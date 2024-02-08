ea49d13a12758ae66ad93e910a6c42dca9c1397d
Robert Widmann <devteam.codafi@gmail.com>
2024-02-08 03:27:29 -0700

    Availability

--- test/ModuleInterface/Inputs/opaque-type-abi-name/test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-/Users/rwidmann/SwiftInternal/build/Ninja-Release/swift-macosx-arm64/bin/swift-frontend -sdk $(xcrun --show-sdk-path) -compile-module-from-interface -module-name Bottom -o Bottom.swiftmodule Bottom.swiftinterface -verify 
