757c50cd4b56723afaad048a06ad0141a51847dc
Jan Svoboda <jan_svoboda@apple.com>
2024-02-09 05:17:14 +0100

    [ClangImporter] Align callback with Clang change (#71475)

--- lib/ClangImporter/ClangImporter.cpp
+++ lib/ClangImporter/ClangImporter.cpp
@@ -117,9 +117,10 @@ namespace {
         clang::SourceLocation HashLoc, const clang::Token &IncludeTok,
         StringRef FileName, bool IsAngled, clang::CharSourceRange FilenameRange,
         clang::OptionalFileEntryRef File, StringRef SearchPath,
-        StringRef RelativePath, const clang::Module *Imported,
+        StringRef RelativePath, const clang::Module *SuggestedModule,
+        bool ModuleImported,
         clang::SrcMgr::CharacteristicKind FileType) override {
-      handleImport(Imported);
+      handleImport(ModuleImported ? SuggestedModule : nullptr);
     }
 
     void moduleImport(clang::SourceLocation ImportLoc,
@@ -293,18 +294,19 @@ private:
                           bool IsAngled, clang::CharSourceRange FilenameRange,
                           clang::OptionalFileEntryRef File,
                           StringRef SearchPath, StringRef RelativePath,
-                          const clang::Module *Imported,
+                          const clang::Module *SuggestedModule,
+                          bool ModuleImported,
                           clang::SrcMgr::CharacteristicKind FileType) override {
-    if (!Imported) {
+    if (!ModuleImported) {
       if (File)
         Impl.BridgeHeaderFiles.insert(*File);
       return;
     }
     // Synthesize identifier locations.
     SmallVector<clang::SourceLocation, 4> IdLocs;
-    for (unsigned I = 0, E = getNumModuleIdentifiers(Imported); I != E; ++I)
+    for (unsigned I = 0, E = getNumModuleIdentifiers(SuggestedModule); I != E; ++I)
       IdLocs.push_back(HashLoc);
-    handleImport(HashLoc, IdLocs, Imported);
+    handleImport(HashLoc, IdLocs, SuggestedModule);
   }
 
   void moduleImport(clang::SourceLocation ImportLoc,
