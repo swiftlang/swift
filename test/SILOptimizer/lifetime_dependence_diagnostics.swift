ade2ed6ddbe73cbd345f38b72ff6908fa7a342e4
Andrew Trick <atrick@apple.com>
2024-02-12 15:35:45 -0800

    LifetimeDependenceInsertion: do not emit inherited dependencies.
    These were always redundant. And there is no way to emit them
    correctly for valid OSA when the original dependence scope is in the
    caller.
    
    NFC without:
    -enable-experimental-feature NonescapableTypes
    -enable-lifetime-dependence-diagnostics

--- test/SILOptimizer/lifetime_dependence_diagnostics.swift
+++ test/SILOptimizer/lifetime_dependence_diagnostics.swift
@@ -18,14 +18,26 @@ func bv_copy(_ bv: borrowing BV) -> _copy(bv) BV {
   copy bv
 }
 
-// Diagnostics resolves mark_dependence [nonescaping].
+// No mark_dependence is needed for a inherited scope.
 //
-// CHECK-LABEL: sil hidden @$s4test14bv_borrow_copy0B0AA2BVVAE_tF : $@convention(thin) (@guaranteed BV) -> _scope(1) @owned BV {
-// CHECK: bb0(%0 : @noImplicitCopy $BV):
-// CHECK:   [[R:%.*]] = apply %{{.*}}(%0) : $@convention(thin) (@guaranteed BV) -> _inherit(1) @owned BV
-// CHECK:   [[MD:%.*]] = mark_dependence [nonescaping] [[R]] : $BV on %0 : $BV
-// CHECK:   return [[MD]] : $BV
-// CHECK-LABEL: } // end sil function '$s4test14bv_borrow_copy0B0AA2BVVAE_tF'
-func bv_borrow_copy(bv: borrowing BV) -> _borrow(bv) BV {
+// CHECK-LABEL: sil hidden @$s4test14bv_borrow_copyyAA2BVVADF : $@convention(thin) (@guaranteed BV) -> _scope(1) @owned BV {
+// CHECK:      bb0(%0 : @noImplicitCopy $BV):
+// CHECK:        apply %{{.*}}(%0) : $@convention(thin) (@guaranteed BV) -> _inherit(1) @owned BV // user: %4
+// CHECK-NEXT:   return %3 : $BV
+// CHECK-LABEL: } // end sil function '$s4test14bv_borrow_copyyAA2BVVADF'
+func bv_borrow_copy(_ bv: borrowing BV) -> _borrow(bv) BV {
   bv_copy(bv) 
+}
+
+// The mark_dependence for the borrow scope should be marked
+// [nonescaping] after diagnostics.
+//
+// CHECK-LABEL: sil hidden @$s4test010bv_borrow_C00B0AA2BVVAE_tF : $@convention(thin) (@guaranteed BV) -> _scope(1) @owned BV {
+// CHECK:       bb0(%0 : @noImplicitCopy $BV):
+// CHECK:         [[R:%.*]] = apply %{{.*}}(%0) : $@convention(thin) (@guaranteed BV) -> _scope(1) @owned BV
+// CHECK:         %{{.*}} = mark_dependence [nonescaping] [[R]] : $BV on %0 : $BV
+// CHECK-NEXT:    return %{{.*}} : $BV
+// CHECK-LABEL: } // end sil function '$s4test010bv_borrow_C00B0AA2BVVAE_tF'
+func bv_borrow_borrow(bv: borrowing BV) -> _borrow(bv) BV {
+  bv_borrow_copy(bv)
 }
