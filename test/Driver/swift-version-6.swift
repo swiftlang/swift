6a3bacdf45cbb26ff1ebe3e00a137811f2de9789
Holly Borla <hborla@apple.com>
2024-02-13 08:32:52 -0800

    [Test] Fix expected output in test/Driver/swift-version-6.swift.

--- test/Driver/swift-version-6.swift
+++ test/Driver/swift-version-6.swift
@@ -44,5 +44,4 @@ jkl
 #endif
 
 // ERROR_7: <unknown>:0: error: invalid value '7' in '-swift-version 7'
-// ERROR_7: <unknown>:0: note: valid arguments to '-swift-version'
-// ERROR_7: valid arguments to '-swift-version' are '4', '4.2', '5', '6'
+// ERROR_7: <unknown>:0: note: valid arguments to '-swift-version' are '4', '4.2', '5', '6'
