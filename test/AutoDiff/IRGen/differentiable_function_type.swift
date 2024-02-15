cefe3129e1761c745006a00d9f3a474b36d78913
Andrew Trick <atrick@apple.com>
2024-02-15 12:20:14 -0800

    Added -disable-round-trip-debug-types to differentiable_function_type.swift
    Incorrect reconstructed type for $sq_xIelgnr_D
    
    Tracked by: rdar://123029365
    (AutoDiff/IRGen/differentiable_function_type.swift fails sporadically
    on Windows; blocks PR testing)

--- test/AutoDiff/IRGen/differentiable_function_type.swift
+++ test/AutoDiff/IRGen/differentiable_function_type.swift
@@ -1,4 +1,11 @@
-// RUN: %target-swift-frontend -emit-ir -g %s
+// RUN: %target-swift-frontend -emit-ir -g %s \
+// RUN: -disable-round-trip-debug-types
+
+// FIXME: round-trip-debug-types is disabled for this test because it fails on Windows:
+// Incorrect reconstructed type for $sq_xIelgnr_D
+//
+// rdar://123029365 (AutoDiff/IRGen/differentiable_function_type.swift fails sporadically on Windows;
+// blocks PR testing)
 
 import _Differentiation
 
