fb1a480a39de39672df4ec917e5b279babfa582e
Allan Shortlidge <shortlidge@apple.com>
2024-02-15 17:52:30 -0800

    Disable IRGen/lto_autolink.swift on iOS and arm64e.
    Resolves rdar://122672371

--- test/IRGen/lto_autolink.swift
+++ test/IRGen/lto_autolink.swift
@@ -44,6 +44,7 @@ import AutolinkModuleMapLink
 #endif
 
 // UNSUPPORTED: OS=macosx && CPU=arm64
+// UNSUPPORTED: OS=iphoneos && CPU=arm64e
 // UNSUPPORTED: OS=watchos && CPU=arm64_32
 // UNSUPPORTED: OS=linux-gnu && CPU=aarch64
 // UNSUPPORTED: CPU=wasm32
