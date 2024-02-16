b643cd8fb60e61d9063b22b949a2ffe01472b752
Pavel Yaskevich <pyaskevich@apple.com>
2024-02-15 21:34:42 -0800

    [AST/Sema] NonCopyableGenerics: Remove all but one direct use of `getMarking(...)`

--- lib/Sema/TypeCheckDeclPrimary.cpp
+++ lib/Sema/TypeCheckDeclPrimary.cpp
@@ -3219,11 +3219,11 @@ public:
     auto &ctx = decl->getASTContext();
 
     for (auto ip : InvertibleProtocolSet::full()) {
-      auto marking = decl->getMarking(ip);
+      auto inverseMarking = decl->hasInverseMarking(ip);
 
       // Inferred inverses are already ignored for classes.
       // FIXME: we can also diagnose @_moveOnly here if we use `isAnyExplicit`
-      if (!marking.getInverse().is(InverseMarking::Kind::Explicit))
+      if (!inverseMarking.is(InverseMarking::Kind::Explicit))
         continue;
 
       // Allow ~Copyable when MoveOnlyClasses is enabled
@@ -3232,7 +3232,7 @@ public:
         continue;
 
 
-      ctx.Diags.diagnose(marking.getInverse().getLoc(),
+      ctx.Diags.diagnose(inverseMarking.getLoc(),
                          diag::inverse_on_class,
                          getProtocolName(getKnownProtocolKind(ip)));
     }
