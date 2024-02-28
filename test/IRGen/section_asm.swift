3c029ebd3fa16e1364f06fab310752d703c69c49
Alastair Houghton <ahoughton@apple.com>
2024-02-28 17:00:51 +0000

    [IRGen] Use llvm.used on ELF instead of llvm.compiler.used.
    We changed to `llvm.compiler.used` because of the behaviour of `gold`,
    which refuses to coalesce sections that have different `SHF_GNU_RETAIN`
    flags, which causes problems with metadata.
    
    Originally I thought we were going to have to generate two sections
    with distinct names and have the runtime look for both of them, but
    it turns out that the runtime only wants to see sections that have
    `SHF_GNU_RETAIN` in any case.  It's really the reflection code that
    is interested in being able to see non-retained sections.  The upshot
    is that we don't need to use `llvm.compiler.used`; it's just fine if
    we have duplicate sections, as long as the reflection code looks for
    them when it's inspecting an ELF image.
    
    This also means we no longer need to pass `-z nostart-stop-gc` to the
    linker if we're using `lld`.
    
    rdar://123504095

--- test/IRGen/section_asm.swift
+++ test/IRGen/section_asm.swift
@@ -2,24 +2,24 @@
 // REQUIRES: swift_in_compiler
 // UNSUPPORTED: CPU=wasm32
 
-// CHECK: .section{{.*}}__TEXT,__mysection
+// CHECK: .section{{.*}}"__TEXT,__mysection","ax"
 // CHECK-NOT: .section
 // CHECK: $s7section3fooyyF:
 
-// CHECK: .section{{.*}}__TEXT,__mysection
+// CHECK: .section{{.*}}"__TEXT,__mysection","ax"
 // CHECK-NOT: .section
 // CHECK: $s7section8MyStructV3fooyyF:
 
-// CHECK: .section{{.*}}__DATA,__mysection
+// CHECK: .section{{.*}}"__DATA,__mysection","aw"
 // CHECK-NOT: .section
 // CHECK: $s7section2g0Sivp:
 // CHECK-NOT: .section
 // CHECK: $s7section2g1Si_Sitvp:
 // CHECK-NOT: .section
 // CHECK: $s7section2g2Sbvp:
-// CHECK-NOT: .section
+// CHECK: .section{{.*}}"__DATA,__mysection","awR"
 // CHECK: $s7section2g3Sbvp:
-// CHECK-NOT: .section
+// CHECK: .section{{.*}}"__DATA,__mysection","aw"
 // CHECK: $s7section2g4SpySiGSgvp:
 // CHECK-NOT: .section
 // CHECK: $s7section2g5SpySiGSgvp:
