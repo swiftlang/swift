0d7c575177b576347000bcee01033c5241b7b7f8
Alastair Houghton <ahoughton@apple.com>
2024-03-01 15:36:59 +0000

    [IRGen][Test] Update tests for non-Linux platform support.
    Tweak the tests slightly for WASM, Windows and Darwin.
    
    rdar://23335318

--- test/IRGen/cf_objc_retainAutoreleasedReturnValue.swift
+++ test/IRGen/cf_objc_retainAutoreleasedReturnValue.swift
@@ -1,5 +1,9 @@
 // RUN: %target-swift-frontend -module-name cf_objc_retainAutoreleasedReturnValue -I %S/Inputs %s -enable-objc-interop -emit-ir | %FileCheck %s
-// RUN: %target-swift-frontend -module-name cf_objc_retainAutoreleasedReturnValue -I %S/Inputs %s -emit-ir | %FileCheck %s --check-prefix=NO_INTEROP
+
+// We need to require objc_codegen to avoid this test on WASM.
+// (That's why the other half of this test is in a separate file.)
+
+// REQUIRES: objc_codegen
 
 import CFBridgedType
 
@@ -10,15 +14,7 @@ public func foo() {
 
 // With interop enabled, this should use objc_retainAutoreleasedReturnValue()
 
-// CHECK-LABEL: define protected swiftcc void @"$s37cf_objc_retainAutoreleasedReturnValue3fooyyF"()
+// CHECK-LABEL: define {{.*}}swiftcc void @"$s37cf_objc_retainAutoreleasedReturnValue3fooyyF"()
 // CHECK: entry:
 // CHECK:   %0 = call {{.*}}@returnsACFBridgedType()
 // CHECK:   %1 = notail call ptr @llvm.objc.retainAutoreleasedReturnValue(ptr %0)
-
-// Without interop, it should call swift_retain() instead.
-
-// NO_INTEROP-LABEL: define protected swiftcc void @"$s37cf_objc_retainAutoreleasedReturnValue3fooyyF"()
-// NO_INTEROP: entry:
-// NO_INTEROP:   %0 = call {{.*}}@returnsACFBridgedType()
-// NO_INTEROP:   %1 = call ptr @swift_retain(ptr returned %0)
-
