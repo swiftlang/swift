f3e8d8bd243397cb5a378d96509dcc4ea73f0e00
Michael Gottesman <mgottesman@apple.com>
2024-03-02 15:21:56 -0800

    Disable availability checking on a test

--- test/SILGen/isolated_self_capture.swift
+++ test/SILGen/isolated_self_capture.swift
@@ -1,4 +1,4 @@
-// RUN: %target-swift-frontend -emit-silgen %s -module-name test -swift-version 5 | %FileCheck %s
+// RUN: %target-swift-frontend -emit-silgen %s -module-name test -disable-availability-checking -sil-verify-all -swift-version 5 | %FileCheck %s
 
 // REQUIRES: concurrency
 
