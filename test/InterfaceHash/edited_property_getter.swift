383455e6cc5d0c5dfdfeb79002f8fa54c236a563
Mishal Shah <mishal_shah@apple.com>
2024-03-01 17:26:59 -0800

    [Disable] InterfaceHash test: edited_property_getter and edited_method_body

--- test/InterfaceHash/edited_property_getter.swift
+++ test/InterfaceHash/edited_property_getter.swift
@@ -2,6 +2,9 @@
 // Also uses awk:
 // XFAIL OS=windows
 
+// rdar://115209834
+// UNSUPPORTED: OS=linux-gnu
+
 // When adding a private protocol method, the interface hash should stay the same
 // The per-type fingerprint should change
 
