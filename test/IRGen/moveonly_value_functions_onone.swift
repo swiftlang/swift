fa570fbf9d087e3000796be62b0a5ed5a251a783
Nate Chandler <nate_chandler@apple.com>
2024-03-02 22:34:31 -0800

    [Test] Removed a disabled feature flag.

--- test/IRGen/moveonly_value_functions_onone.swift
+++ test/IRGen/moveonly_value_functions_onone.swift
@@ -1,6 +1,5 @@
 // RUN: %target-swift-emit-irgen -Onone                      \
 // RUN:     -enable-experimental-feature NoncopyableGenerics \
-// RUN:     -disable-type-layout                             \
 // RUN:     %s                                               \
 // RUN: |                                                    \
 // RUN: %IRGenFileCheck %s
