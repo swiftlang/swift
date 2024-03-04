3d34871ec6c6686ddeba86479af87c2f4da16437
Sofia Rodriguez <sofia_rodriguez@apple.com>
2024-03-04 19:40:21 +0100

    Fix tests

--- test/SymbolGraph/ClangImporter/ImportForwardDeclarationsTest.swift
+++ test/SymbolGraph/ClangImporter/ImportForwardDeclarationsTest.swift
@@ -1,6 +1,6 @@
 // RUN: %empty-directory(%t)
 // RUN: cp -r %S/Inputs/ForwardDeclarations/ForwardDeclarations.framework %t
-// RUN: %target-swift-frontend(mock-sdk: %clang-importer-sdk) -enable-objc-interop -emit-module-path %t/ForwardDeclarations.framework/Modules/ForwardDeclarations.swiftmodule/%target-swiftmodule-name -import-underlying-module -F %t -module-name ForwardDeclarations -disable-objc-attr-requires-foundation-module %s -emit-symbol-graph -emit-symbol-graph-dir %t
+// RUN: %target-swift-frontend(mock-sdk: %clang-importer-sdk) -enable-objc-interop -emit-module-path %t/ForwardDeclarations.framework/Modules/ForwardDeclarations.swiftmodule/%target-swiftmodule-name -import-underlying-module -F %t -module-name ForwardDeclarations -disable-objc-attr-requires-foundation-module %s
 // RUN: %target-swift-symbolgraph-extract -sdk %clang-importer-sdk -module-name ForwardDeclarations -F %t -output-dir %t -pretty-print -v
 // RUN: %FileCheck %s --input-file %t/ForwardDeclarations.symbols.json
 
