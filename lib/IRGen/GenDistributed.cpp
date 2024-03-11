1b1c1c8353fa8c9b1474afb1fe19c039c81ec2d1
Konrad `ktoso` Malawski <konrad_malawski@apple.com>
2024-03-11 15:24:27 +0900

    [Distributed] remove un-necessary getThunk in GenDistributed

--- lib/IRGen/GenDistributed.cpp
+++ lib/IRGen/GenDistributed.cpp
@@ -858,9 +858,6 @@ FunctionPointer AccessorTarget::getPointerToTarget(llvm::Value *actorSelf) {
   }
 
   auto *requirementDecl = Target.get<AbstractFunctionDecl *>();
-  if (requirementDecl->isDistributed()) {
-    requirementDecl = requirementDecl->getDistributedThunk();
-  }
   auto *protocol = requirementDecl->getDeclContext()->getSelfProtocolDecl();
   SILDeclRef requirementRef = SILDeclRef(requirementDecl).asDistributed();
 
