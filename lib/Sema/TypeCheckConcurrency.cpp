f3c7d607b2fccae10c6b5659373a094339d3065f
Michael Gottesman <mgottesman@apple.com>
2024-03-13 00:10:42 -0700

    ignore preconcurrency context

--- lib/Sema/TypeCheckConcurrency.cpp
+++ lib/Sema/TypeCheckConcurrency.cpp
@@ -4050,7 +4050,7 @@ namespace {
               decl, useKind,
               refKind + 1, refGlobalActor,
               result.isolation)
-          .warnUntilSwiftVersionIf(preconcurrencyContext, 6);
+          .warnUntilSwiftVersion(6);
 
           if (derivedConformanceType) {
             auto *decl = dyn_cast<ValueDecl>(getDeclContext()->getAsDecl());
