72abd6ce9d62dfe3a4e5877d551e2523dc66c94d
Karoy Lorentey <klorentey@apple.com>
2024-03-13 17:44:40 -0700

    [IRGen] Temporarily disable round-tripping debug types

--- lib/Frontend/CompilerInvocation.cpp
+++ lib/Frontend/CompilerInvocation.cpp
@@ -2728,11 +2728,13 @@ static bool ParseIRGenArgs(IRGenOptions &Opts, ArgList &Args,
   if (Args.hasArg(OPT_no_clang_module_breadcrumbs))
     Opts.DisableClangModuleSkeletonCUs = true;
 
-  if (auto A = Args.getLastArg(OPT_enable_round_trip_debug_types,
-                               OPT_disable_round_trip_debug_types)) {
-    Opts.DisableRoundTripDebugTypes =
-        Args.hasArg(OPT_disable_round_trip_debug_types);
-  }
+  // FIXME(NCG): Temporarily force-disabled, it keeps tripping up on noncopyables (rdar://124564447)
+  // if (auto A = Args.getLastArg(OPT_enable_round_trip_debug_types,
+  //                              OPT_disable_round_trip_debug_types)) {
+  //   Opts.DisableRoundTripDebugTypes =
+  //       Args.hasArg(OPT_disable_round_trip_debug_types);
+  // }
+  Opts.DisableRoundTripDebugTypes = true;
 
   if (Args.hasArg(OPT_disable_debugger_shadow_copies))
     Opts.DisableDebuggerShadowCopies = true;
