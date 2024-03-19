9188fbb745cafb24065c3c592cf3713d6160b3ed
Ben Barham <ben_barham@apple.com>
2024-03-19 10:50:46 -0700

    [Test] Disable flakey test
    This seems to be failing more regularly now and is often blocking CI.
    Disable for now while we investigate.

--- test/SourceKit/Diagnostics/cancel_diags.swift
+++ test/SourceKit/Diagnostics/cancel_diags.swift
@@ -1,3 +1,5 @@
+// REQUIRES: rdar116486638
+
 // RUN: not %sourcekitd-test -req=diags %s -print-raw-response -id=diag -async -- %s == -cancel=diag 2>&1 | %FileCheck --dump-input=always %s
 
 func foo(x: Invalid1, y: Invalid2) {
