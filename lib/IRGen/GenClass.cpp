9c95d6af8c2ffa8d62e67c683b802c78b8522a82
Konrad `ktoso` Malawski <konrad_malawski@apple.com>
2024-03-22 10:56:32 +0900

    prevent handling DistributedGet in places where it should never appear

--- lib/IRGen/GenClass.cpp
+++ lib/IRGen/GenClass.cpp
@@ -1855,7 +1855,6 @@ namespace {
 
       switch (accessor->getAccessorKind()) {
       case AccessorKind::Get:
-      case AccessorKind::DistributedGet: // TODO: maybe dont do this?
         return emitObjCGetterDescriptor(IGM, descriptors,
                                         accessor->getStorage());
 
@@ -1866,6 +1865,7 @@ namespace {
 #define OBJC_ACCESSOR(ID, KEYWORD)
 #define ACCESSOR(ID) \
       case AccessorKind::ID:
+      case AccessorKind::DistributedGet:
 #include "swift/AST/AccessorKinds.def"
         llvm_unreachable("shouldn't be trying to build this accessor");
       }
