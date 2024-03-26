a3aa827c909b7e55f956597b1e35f4d55e2b5c6d
Becca Royal-Gordon <beccadax@apple.com>
2024-03-25 21:17:59 -0700

    Warn that @objcImpl extension is deprecated
    …only when the ObjCImplementation experimental language feature is enabled. (`@implementation extension` is the preferred form.)

--- test/decl/ext/objc_implementation_features.swift
+++ test/decl/ext/objc_implementation_features.swift
@@ -3,6 +3,8 @@
 // RUN: not %target-swift-frontend(mock-sdk: %clang-importer-sdk-nosource) -typecheck %s -import-objc-header %S/Inputs/objc_implementation.h 2>&1 | %FileCheck --check-prefixes NO,CHECK %s
 // RUN: not %target-swift-frontend(mock-sdk: %clang-importer-sdk-nosource) -typecheck %s -import-objc-header %S/Inputs/objc_implementation.h -enable-experimental-feature ObjCImplementation 2>&1 | %FileCheck --check-prefixes YES,CHECK %s
 
+// NO-NOT: objc_implementation_features.swift:[[@LINE+2]]:{{[0-9]+}}: warning: warning-only '@_objcImplementation' spelling is deprecated; use '@implementation' instead
+// YES-DAG: objc_implementation_features.swift:[[@LINE+1]]:{{[0-9]+}}: warning: warning-only '@_objcImplementation' spelling is deprecated; use '@implementation' instead
 @_objcImplementation extension ObjCSubclass {}
 
 // NO-DAG: objc_implementation_features.swift:[[@LINE+2]]:{{[0-9]+}}: error: 'implementation' attribute is only valid when experimental feature ObjCImplementation is enabled
