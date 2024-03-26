492d4ad49d6accac1aeb748e7991f4f31935268b
Becca Royal-Gordon <beccadax@apple.com>
2024-03-25 21:15:02 -0700

    Add experimental feature flags for @implementation
    • ObjCImplementation controls @implementation on extensions
    • CImplementation controls @implementation and @_objcImplementation on cdecl functions
    
    Why the difference between them? Because `@_objcImplementation extension` has already been adopted pretty widely, while `@_objcImplementation @_cdecl` is very new.

--- lib/AST/FeatureSet.cpp
+++ lib/AST/FeatureSet.cpp
@@ -680,6 +680,9 @@ static bool usesFeatureGlobalActorIsolatedTypesUsability(Decl *decl) {
   return false;
 }
 
+UNINTERESTING_FEATURE(ObjCImplementation)
+UNINTERESTING_FEATURE(CImplementation)
+
 // ----------------------------------------------------------------------------
 // MARK: - FeatureSet
 // ----------------------------------------------------------------------------
