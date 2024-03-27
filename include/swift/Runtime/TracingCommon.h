swiftlang/swift

commit b8cb005f99194e8478f6fa1e604b0b02c2a4f00b

Disable tracing in xpcproxy
Add xpcproxy to the list of processes that handle tracing
requests and therefore cannot themselves use tracing without
deadlocking.

rdar://124996590

include/swift/Runtime/TracingCommon.h
@@ -33,7 +33,8 @@ static inline bool shouldEnableTracing() {
     return false;
   if (__progname && (strcmp(__progname, "logd") == 0 ||
                      strcmp(__progname, "diagnosticd") == 0 ||
-                     strcmp(__progname, "notifyd") == 0))
+                     strcmp(__progname, "notifyd") == 0) ||
+                     strcmp(__progname, "xpcproxy") == 0)
     return false;
   return true;
 }

commit 346a12556e152d92a45ed136ac7eb3e573f54064

Fix logical issue introduced in b8cb005.

include/swift/Runtime/TracingCommon.h
@@ -33,8 +33,8 @@ static inline bool shouldEnableTracing() {
     return false;
   if (__progname && (strcmp(__progname, "logd") == 0 ||
                      strcmp(__progname, "diagnosticd") == 0 ||
-                     strcmp(__progname, "notifyd") == 0) ||
-                     strcmp(__progname, "xpcproxy") == 0)
+                     strcmp(__progname, "notifyd") == 0 ||
+                     strcmp(__progname, "xpcproxy") == 0))
     return false;
   return true;
 }