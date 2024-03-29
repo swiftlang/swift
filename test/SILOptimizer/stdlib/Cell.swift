15c5df4a6e8bd19aca75654a1d9da250090dacfd
Rintaro Ishizaki <rishizaki@apple.com>
2024-03-29 12:07:16 -0700

    Merge pull request #72700 from Azoy/fix-cell-test-mangling
    [test] Update Cell.swift to use new mangling

--- test/SILOptimizer/stdlib/Cell.swift
+++ test/SILOptimizer/stdlib/Cell.swift
@@ -5,25 +5,25 @@ import Builtin
 @frozen
 @_rawLayout(like: T)
 public struct Cell<T: ~Copyable>: ~Copyable {
-  // CHECK-LABEL: sil {{.*}} @$s4CellAAVAARiczrlE7addressSpyxGvg : $@convention(method) <T where T : ~Copyable> (@in_guaranteed Cell<T>) -> UnsafeMutablePointer<T> {
+  // CHECK-LABEL: sil {{.*}} @$s4CellAAVAARi_zrlE7addressSpyxGvg : $@convention(method) <T where T : ~Copyable> (@in_guaranteed Cell<T>) -> UnsafeMutablePointer<T> {
   // CHECK:       bb0([[SELF:%.*]] : $*Cell<T>):
   // CHECK:         [[RAW_LAYOUT_ADDR:%.*]] = builtin "addressOfRawLayout"<Cell<T>>([[SELF]] : $*Cell<T>) : $Builtin.RawPointer
   // CHECK-NEXT:    [[POINTER:%.*]] = struct $UnsafeMutablePointer<T> ([[RAW_LAYOUT_ADDR]] : $Builtin.RawPointer)
   // CHECK-NEXT:    return [[POINTER]] : $UnsafeMutablePointer<T>
-  // CHECK-LABEL: } // end sil function '$s4CellAAVAARiczrlE7addressSpyxGvg'
+  // CHECK-LABEL: } // end sil function '$s4CellAAVAARi_zrlE7addressSpyxGvg'
   @_transparent
   public var address: UnsafeMutablePointer<T> {
     .init(Builtin.addressOfRawLayout(self))
   }
 
-  // CHECK-LABEL: sil {{.*}} @$s4CellAAVAARiczrlEyAByxGxcfC : $@convention(method) <T where T : ~Copyable> (@in T, @thin Cell<T>.Type) -> @out Cell<T> {
+  // CHECK-LABEL: sil {{.*}} @$s4CellAAVAARi_zrlEyAByxGxcfC : $@convention(method) <T where T : ~Copyable> (@in T, @thin Cell<T>.Type) -> @out Cell<T> {
   // CHECK:       bb0({{%.*}} : $*Cell<T>, [[VALUE:%.*]] : $*T, {{%.*}} : $@thin Cell<T>.Type):
   // CHECK:         {{%.*}} = builtin "zeroInitializer"<Cell<T>>([[SELF:%.*]] : $*Cell<T>) : $()
   // CHECK-NEXT:    [[RAW_LAYOUT_ADDR:%.*]] = builtin "addressOfRawLayout"<Cell<T>>([[SELF]] : $*Cell<T>) : $Builtin.RawPointer
   // CHECK-NEXT:    [[POINTER:%.*]] = struct $UnsafeMutablePointer<T> ([[RAW_LAYOUT_ADDR]] : $Builtin.RawPointer)
   //                Calling 'UnsafeMutablePointer<T>.initialize(to:)'
   // CHECK:         {{%.*}} = apply {{%.*}}<T>([[VALUE]], [[POINTER]])
-  // CHECK-LABEL: } // end sil function '$s4CellAAVAARiczrlEyAByxGxcfC'
+  // CHECK-LABEL: } // end sil function '$s4CellAAVAARi_zrlEyAByxGxcfC'
   @_transparent
   public init(_ value: consuming T) {
     address.initialize(to: value)
