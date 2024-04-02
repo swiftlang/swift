swiftlang/swift

commit 6027bf46a6915aa976a30cf29ef37c74f74b09fd

AST: Simplify collectExistentialConformances()

lib/Sema/CSApply.cpp
@@ -6671,12 +6671,6 @@ Expr *ExprRewriter::coerceExistential(Expr *expr, Type toType,
   /// because most protocols do not conform to themselves -- however we still
   /// allow the conversion here, except the ErasureExpr ends up with trivial
   /// conformances.
-  auto conformances =
-      dc->getParentModule()
-        ->collectExistentialConformances(fromInstanceType->getCanonicalType(),
-                                         toInstanceType->getCanonicalType(),
-                                         /*skipConditionalRequirements=*/false,
-                                         /*allowMissing=*/true);
 
   // Use the requirements of any parameterized protocols to build out fake
   // argument conversions that can be used to infer opaque types.
@@ -6738,6 +6732,16 @@ Expr *ExprRewriter::coerceExistential(Expr *expr, Type toType,
     auto *archetypeVal = cs.cacheType(
         new (ctx) OpaqueValueExpr(expr->getSourceRange(), fromType));
 
+    fromInstanceType = fromType;
+    while (auto *metatypeType = fromInstanceType->getAs<MetatypeType>())
+      fromInstanceType = metatypeType->getInstanceType();
+
+    auto conformances =
+        dc->getParentModule()
+          ->collectExistentialConformances(fromInstanceType->getCanonicalType(),
+                                           toInstanceType->getCanonicalType(),
+                                           /*allowMissing=*/true);
+
     auto *result = cs.cacheType(ErasureExpr::create(ctx, archetypeVal, toType,
                                                     conformances,
                                                     argConversions));
@@ -6752,6 +6756,12 @@ Expr *ExprRewriter::coerceExistential(Expr *expr, Type toType,
       expr = cs.coerceToRValue(expr);
     }
   }
+
+  auto conformances =
+      dc->getParentModule()
+        ->collectExistentialConformances(fromInstanceType->getCanonicalType(),
+                                         toInstanceType->getCanonicalType(),
+                                         /*allowMissing=*/true);
 
   return cs.cacheType(ErasureExpr::create(ctx, expr, toType,
                                           conformances, argConversions));

include/swift/AST/Module.h
@@ -862,11 +862,8 @@ public:
 
   /// Collect the conformances of \c fromType to each of the protocols of an
   /// existential type's layout.
-  ///
-  /// See `TypeChecker::containsProtocol` for details on the boolean arguments.
   ArrayRef<ProtocolConformanceRef>
   collectExistentialConformances(CanType fromType, CanType existential,
-                                 bool skipConditionalRequirements = true,
                                  bool allowMissing = false);
 
   /// Find a member named \p name in \p container that was declared in this

include/swift/AST/TypeCheckRequests.h
@@ -323,30 +323,6 @@ public:
   void cacheResult(bool value) const;
 };
 
-class CollectExistentialConformancesRequest
-    : public SimpleRequest<CollectExistentialConformancesRequest,
-                           ArrayRef<ProtocolConformanceRef>(ModuleDecl*,
-                                                            CanType,
-                                                            CanType,
-                                                            bool,
-                                                            bool),
-                           RequestFlags::Uncached> { // TODO: maybe cache this?
-public:
-  using SimpleRequest::SimpleRequest;
-
-private:
-  friend SimpleRequest;
-
-  // Evaluation.
-  ArrayRef<ProtocolConformanceRef>
-      evaluate(Evaluator &evaluator,
-               ModuleDecl *module,
-               CanType fromType,
-               CanType existential,
-               bool skipConditionalRequirements,
-               bool allowMissing) const;
-};
-
 /// Determine whether an existential type conforming to this protocol
 /// requires the \c any syntax.
 class HasSelfOrAssociatedTypeRequirementsRequest :

include/swift/AST/TypeCheckerTypeIDZone.def
@@ -101,13 +101,6 @@ SWIFT_REQUEST(TypeChecker, EnumRawTypeRequest,
               Type(EnumDecl *), Cached, NoLocationInfo)
 SWIFT_REQUEST(TypeChecker, ExistentialConformsToSelfRequest,
               bool(ProtocolDecl *), SeparatelyCached, NoLocationInfo)
-SWIFT_REQUEST(TypeChecker, CollectExistentialConformancesRequest,
-            ArrayRef<ProtocolConformanceRef>(ModuleDecl*,
-                                             CanType,
-                                             CanType,
-                                             bool,
-                                             bool),
-            Uncached, NoLocationInfo)
 SWIFT_REQUEST(TypeChecker, HasSelfOrAssociatedTypeRequirementsRequest,
               bool(ProtocolDecl *), SeparatelyCached, NoLocationInfo)
 SWIFT_REQUEST(TypeChecker, ExtendedTypeRequest, Type(ExtensionDecl *), Cached,

lib/AST/ConformanceLookup.cpp
@@ -47,14 +47,20 @@ using namespace swift;
 ArrayRef<ProtocolConformanceRef>
 ModuleDecl::collectExistentialConformances(CanType fromType,
                                            CanType existential,
-                                           bool skipConditionalRequirements,
                                            bool allowMissing) {
-  CollectExistentialConformancesRequest request{this,
-                                                fromType,
-                                                existential,
-                                                skipConditionalRequirements,
-                                                allowMissing};
-  return evaluateOrDefault(getASTContext().evaluator, request, /*default=*/{});
+  assert(existential.isAnyExistentialType());
+
+  auto layout = existential.getExistentialLayout();
+  auto protocols = layout.getProtocols();
+
+  SmallVector<ProtocolConformanceRef, 4> conformances;
+  for (auto *proto : protocols) {
+    auto conformance = lookupConformance(fromType, proto, allowMissing);
+    assert(conformance);
+    conformances.push_back(conformance);
+  }
+
+  return getASTContext().AllocateCopy(conformances);
 }
 
 ProtocolConformanceRef

lib/SILGen/SILGenPoly.cpp
@@ -279,7 +279,6 @@ static ManagedValue emitTransformExistential(SILGenFunction &SGF,
       SGF.SGM.M.getSwiftModule()->collectExistentialConformances(
                                      fromInstanceType,
                                      toInstanceType,
-                                     /*skipConditionalRequirements=*/true,
                                      /*allowMissing=*/true);
 
   // Build result existential

lib/Sema/CSSimplify.cpp
@@ -8550,7 +8550,6 @@ ConstraintSystem::SolutionKind ConstraintSystem::simplifyConformsToConstraint(
   case ConstraintKind::SelfObjectOfProtocol: {
     auto conformance = TypeChecker::containsProtocol(
         type, protocol, DC->getParentModule(),
-        /*skipConditionalRequirements=*/true,
         /*allowMissing=*/true);
     if (conformance) {
       return recordConformance(conformance);

lib/Sema/TypeCheckProtocol.cpp
@@ -5280,7 +5280,6 @@ void swift::diagnoseConformanceFailure(Type T,
 
 ProtocolConformanceRef
 TypeChecker::containsProtocol(Type T, ProtocolDecl *Proto, ModuleDecl *M,
-                              bool skipConditionalRequirements,
                               bool allowMissing) {
   // Existential types don't need to conform, i.e., they only need to
   // contain the protocol.
@@ -5305,10 +5304,8 @@ TypeChecker::containsProtocol(Type T, ProtocolDecl *Proto, ModuleDecl *M,
     // would result in a missing conformance if type is `& Sendable`
     // protocol composition. It's handled for type as a whole below.
     if (auto superclass = layout.getSuperclass()) {
-      auto result =
-          (skipConditionalRequirements
-               ? M->lookupConformance(superclass, Proto, /*allowMissing=*/false)
-               : M->checkConformance(superclass, Proto, /*allowMissing=*/false));
+      auto result = M->lookupConformance(superclass, Proto,
+                                         /*allowMissing=*/false);
       if (result) {
         return result;
       }
@@ -5331,9 +5328,7 @@ TypeChecker::containsProtocol(Type T, ProtocolDecl *Proto, ModuleDecl *M,
   }
 
   // For non-existential types, this is equivalent to checking conformance.
-  return (skipConditionalRequirements
-          ? M->lookupConformance(T, Proto, allowMissing)
-          : M->checkConformance(T, Proto, allowMissing));
+  return M->lookupConformance(T, Proto, allowMissing);
 }
 
 bool TypeChecker::conformsToKnownProtocol(

commit e7131cfbe242e1151b083ce67fc4aa72636366c2

Sema: Fix existential-metatype-to-Any conversion
We were incorrectly unwrapping too many levels of metatype, and
our ErasureExpr would end up with conformances for the instance
type and not the metatype itself.

In the old universe, this was not a problem, but now Any has two
protocol conformance members, so suddently this violated invariants.

This was a regression, introduced in 6027bf46a6915aa976a30cf29ef37c74f74b09fd.

Fixes rdar://125460667.

lib/Sema/CSApply.cpp
@@ -6688,13 +6688,24 @@ Expr *ExprRewriter::coerceExistential(Expr *expr, Type toType,
   Type fromInstanceType = fromType;
   Type toInstanceType = toType;
 
-  // Look through metatypes
+  // For existential-to-existential coercions, open the source existential.
+  Type openedFromType;
+  if (fromType->isAnyExistentialType()) {
+    openedFromType = OpenedArchetypeType::getAny(fromType->getCanonicalType(),
+                                                 dc->getGenericSignatureOfContext());
+  }
+
+  Type openedFromInstanceType = openedFromType;
+
+  // Look through metatypes.
   while ((fromInstanceType->is<UnresolvedType>() ||
           fromInstanceType->is<AnyMetatypeType>()) &&
          toInstanceType->is<ExistentialMetatypeType>()) {
     if (!fromInstanceType->is<UnresolvedType>())
-      fromInstanceType = fromInstanceType->castTo<AnyMetatypeType>()->getInstanceType();
-    toInstanceType = toInstanceType->castTo<ExistentialMetatypeType>()->getExistentialInstanceType();
+      fromInstanceType = fromInstanceType->getMetatypeInstanceType();
+    if (openedFromInstanceType && !openedFromInstanceType->is<UnresolvedType>())
+      openedFromInstanceType = openedFromInstanceType->getMetatypeInstanceType();
+    toInstanceType = toInstanceType->getMetatypeInstanceType();
   }
 
   ASTContext &ctx = cs.getASTContext();
@@ -6758,20 +6769,13 @@ Expr *ExprRewriter::coerceExistential(Expr *expr, Type toType,
   }
 
   // For existential-to-existential coercions, open the source existential.
-  if (fromType->isAnyExistentialType()) {
-    fromType = OpenedArchetypeType::getAny(fromType->getCanonicalType(),
-                                           dc->getGenericSignatureOfContext());
-
+  if (openedFromType) {
     auto *archetypeVal = cs.cacheType(
-        new (ctx) OpaqueValueExpr(expr->getSourceRange(), fromType));
-
-    fromInstanceType = fromType;
-    while (auto *metatypeType = fromInstanceType->getAs<MetatypeType>())
-      fromInstanceType = metatypeType->getInstanceType();
+        new (ctx) OpaqueValueExpr(expr->getSourceRange(), openedFromType));
 
     auto conformances =
         dc->getParentModule()
-          ->collectExistentialConformances(fromInstanceType->getCanonicalType(),
+          ->collectExistentialConformances(openedFromInstanceType->getCanonicalType(),
                                            toInstanceType->getCanonicalType(),
                                            /*allowMissing=*/true);
 

test/SILOptimizer/rdar125460667.swift
@@ -1,0 +1,9 @@
+// RUN: %target-swift-frontend -emit-sil %s
+
+@_transparent func f(a: Any.Type) -> Any {
+  return a
+}
+
+func g(a: Any.Type) {
+  f(a: a)
+}