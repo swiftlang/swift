4e274ce0a5b0d7aaafc9a2326503ffbbe4c587b7
Philippe Hausler <phausler@apple.com>
2024-04-04 14:37:22 -0700

    Move the tests for package scopes to the module interface tests

--- test/stdlib/Observation/Observable.swift
+++ test/stdlib/Observation/Observable.swift
@@ -32,11 +32,6 @@ public class PublicContainsWeak {
 }
 
 @Observable
-class ContainsPackage {
-  package var contents: Int = 0
-}
-
-@Observable
 class ContainsUnowned {
   unowned var obj: AnyObject? = nil
 }
