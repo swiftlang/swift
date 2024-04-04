dcb60ab2d65849703f241b9a209ef76649641562
Kuba Mracek <mracek@apple.com>
2024-04-04 15:37:55 -0700

    [embedded] Build even -macos Embedded Concurrency as THREADING=none, update dependency list

--- test/embedded/dependencies-concurrency.swift
+++ test/embedded/dependencies-concurrency.swift
@@ -9,7 +9,6 @@
 // Fail if there is any entry in actual-dependencies.txt that's not in allowed-dependencies.txt
 // RUN: test -z "`comm -13 %t/allowed-dependencies.txt %t/actual-dependencies.txt`"
 
-// DEP: __ZNSt3__111this_thread9sleep_forERKNS_6chrono8durationIxNS_5ratioILl1ELl1000000000EEEEE
 // DEP: __ZNSt3__112basic_stringIcNS_11char_traitsIcEENS_9allocatorIcEEE6appendEPKc
 // DEP: __ZNSt3__112basic_stringIcNS_11char_traitsIcEENS_9allocatorIcEEE6appendEPKcm
 // DEP: __ZNSt3__112basic_stringIcNS_11char_traitsIcEENS_9allocatorIcEEE6insertEmPKc
@@ -20,12 +19,12 @@
 // DEP: __ZdlPv
 // DEP: __Znwm
 // DEP: ___assert_rtn
+// DEP: ___error
 // DEP: ___stack_chk_fail
 // DEP: ___stack_chk_guard
 // DEP: ___stderrp
 // DEP: __availability_version_check
 // DEP: _abort
-// DEP: _asl_log
 // DEP: _dispatch_once_f
 // DEP: _dlsym
 // DEP: _exit
@@ -37,15 +36,9 @@
 // DEP: _fseek
 // DEP: _ftell
 // DEP: _malloc
-// DEP: _memcpy
 // DEP: _memset_s
-// DEP: _os_unfair_lock_lock
-// DEP: _os_unfair_lock_unlock
+// DEP: _nanosleep
 // DEP: _posix_memalign
-// DEP: _pthread_equal
-// DEP: _pthread_getspecific
-// DEP: _pthread_self
-// DEP: _pthread_setspecific
 // DEP: _putchar
 // DEP: _rewind
 // DEP: _sscanf
