78852338de821c56469e1f7d6e850561e77b8ba4
Artem Chikin <achikin@apple.com>
2024-04-05 11:25:47 -0700

    Revert "On 'canImport' of Clang submodules of modules with an umbrella header, attempt to infer the submodule"

--- lib/ClangImporter/ClangImporter.cpp
+++ lib/ClangImporter/ClangImporter.cpp
@@ -2083,7 +2083,7 @@ bool ClangImporter::canImportModule(ImportPath::Module modulePath,
 
   if (modulePath.hasSubmodule()) {
     for (auto &component : modulePath.getSubmodulePath()) {
-      clangModule = clangModule->findOrInferSubmodule(component.Item.str());
+      clangModule = clangModule->findSubmodule(component.Item.str());
 
       // Special case: a submodule named "Foo.Private" can be moved to a
       // top-level module named "Foo_Private". Clang has special support for
