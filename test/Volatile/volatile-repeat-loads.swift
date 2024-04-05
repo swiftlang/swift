81f8b2a8cd0ee08fa4d8a24a3efa5f6498f30b91
Kuba Mracek <mracek@apple.com>
2024-04-05 12:09:37 -0700

    [Volatile] Remove extensions on UnsafeMutablePointer

--- test/Volatile/volatile-repeat-loads.swift
+++ test/Volatile/volatile-repeat-loads.swift
@@ -21,20 +21,3 @@ public func test_volatilepointer() -> UInt8 {
 // CHECK:   [[RET:%.*]] = load atomic volatile i8, ptr {{.*}} monotonic, align 1
 // CHECK:   ret i8 [[RET]]
 // CHECK: }
-
-public func test_unsafepointer() -> UInt8 {
-  let p = UnsafeMutablePointer<UInt8>(bitPattern: 0xf000baaa)!
-  p.volatileStore(42)
-  let a = p.volatileLoad()
-  let b = p.volatileLoad()
-  let c = p.volatileLoad()
-  return c
-}
-
-// CHECK: define {{.*}}i8 @"$s4main18test_unsafepointers5UInt8VyF"()
-// CHECK:   store atomic volatile i8 42, ptr {{.*}} monotonic, align 1
-// CHECK:   load atomic volatile i8, ptr {{.*}} monotonic, align 1
-// CHECK:   load atomic volatile i8, ptr {{.*}} monotonic, align 1
-// CHECK:   [[RET:%.*]] = load atomic volatile i8, ptr {{.*}} monotonic, align 1
-// CHECK:   ret i8 [[RET]]
-// CHECK: }
