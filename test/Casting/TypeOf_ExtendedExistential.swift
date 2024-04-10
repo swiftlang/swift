810792843b508c8ebfe681abcb430ee3bb388a68
Tim Kientzle <tkientzle@apple.com>
2024-04-09 17:37:57 -0700

    Fill in extended existential support for type(of:) and mirror
    type(of:) now returns the dynamic type of the contents of
    an extended existential (just like it does for a regular existential)
    
    Mirror can now reflect fields of a value stored inside an extended
    existential (just like it can with a regular existential).  This
    requires type(of:) support, since Mirror internals use that to
    determine how to reflect a value.
    
    Resolves rdar://102906195

---
+++ test/Casting/TypeOf_ExtendedExistential.swift
@@ -0,0 +1,84 @@
+// RUN: %empty-directory(%t)
+//
+// RUN: %target-build-swift -swift-version 5 -g -Onone  -module-name a %s -o %t/a.swift5.Onone.out
+// RUN: %target-codesign %t/a.swift5.Onone.out
+// RUN: %target-run %t/a.swift5.Onone.out
+//
+// RUN: %target-build-swift -swift-version 6 -g -Onone  -module-name a %s -o %t/a.swift6.Onone.out
+// RUN: %target-codesign %t/a.swift6.Onone.out
+// RUN: %target-run %t/a.swift6.Onone.out
+//
+// RUN: %target-build-swift -swift-version 5 -g -O  -module-name a %s -o %t/a.swift5.O.out
+// RUN: %target-codesign %t/a.swift5.O.out
+// RUN: %target-run %t/a.swift5.O.out
+//
+// RUN: %target-build-swift -swift-version 6 -g -O  -module-name a %s -o %t/a.swift6.O.out
+// RUN: %target-codesign %t/a.swift6.O.out
+// RUN: %target-run %t/a.swift6.O.out
+//
+// REQUIRES: executable_test
+// REQUIRES: concurrency
+// UNSUPPORTED: use_os_stdlib
+// UNSUPPORTED: back_deployment_runtime
+
+import StdlibUnittest
+
+func blackhole<T>(_ t: T) { }
+
+private func runtimeCast<T,U>(_ from: T, to: U.Type) -> U? {
+  return from as? U
+}
+
+let CastsTests = TestSuite("Casts")
+
+protocol Box<T> {
+  associatedtype T
+  var t: T { get }
+}
+
+CastsTests.test("type(of:) should look through extended existentials (none)") {
+  struct C<T>: Box { var t: T }
+  func genericErase<T>(_ value: T) -> Any { value }
+  let c: any Box<Int> = C(t: 42)
+  if #available(macOS 13.0, *) {
+    let x = genericErase(c)
+    expectEqual("C<Int>", "\(type(of:x))")
+  } else {
+    expectEqual(0, 1)
+  }
+}
+
+protocol OBox<T> : AnyObject {
+  associatedtype T
+  var t: T { get }
+}
+
+CastsTests.test("type(of:) should look through extended existentials (class)") {
+  class C<T>: OBox {
+    var t: T
+    init(t: T) { self.t = t }
+  }
+  func genericErase<T>(_ value: T) -> Any { value }
+  let c: any OBox<Int> = C(t: 42)
+  if #available(macOS 13.0, *) {
+    let x = genericErase(c)
+    expectEqual("C<Int>", "\(type(of:x))")
+  } else {
+    expectEqual(0, 1)
+  }
+}
+
+
+CastsTests.test("type(of:) should look through extended existentials (metatype)") {
+  struct C<T>: Box { var t: T }
+  func genericErase<T>(_ value: T) -> Any { value }
+  let t: any Box<Int>.Type = C<Int>.self
+  if #available(macOS 13.0, *) {
+    let x = genericErase(t)
+    expectEqual("C<Int>.Type", "\(type(of:x))")
+  } else {
+    expectEqual(0, 1)
+  }
+}
+
+runAllTests()
