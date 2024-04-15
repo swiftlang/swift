99cd01face38957f3ff98b6d887581b63d88c0a0
Kabir Oberai <oberai.kabir@gmail.com>
2024-04-15 00:10:07 -0400

    wip

--- tools/swift-plugin-server/Package.swift
+++ tools/swift-plugin-server/Package.swift
@@ -7,6 +7,10 @@ let package = Package(
   platforms: [
     .macOS(.v10_15)
   ],
+  products: [
+    .library(name: "swift-plugin-server", targets: ["swift-plugin-server"]),
+    .library(name: "swift-wasm-plugin-server", targets: ["swift-wasm-plugin-server"]),
+  ],
   dependencies: [
     .package(path: "../../../swift-syntax"),
     .package(path: "../../lib/ASTGen"),
@@ -34,6 +38,15 @@ let package = Package(
         .product(name: "SwiftOperators", package: "swift-syntax"),
         .product(name: "SwiftParser", package: "swift-syntax"),
         .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+        "CSwiftPluginServer"
+      ],
+      swiftSettings: [.interoperabilityMode(.Cxx)]
+    ),
+    .target(
+      name: "swift-wasm-plugin-server",
+      dependencies: [
+        .product(name: "swiftLLVMJSON", package: "ASTGen"),
+        .product(name: "SwiftCompilerPluginMessageHandling", package: "swift-syntax"),
         "CSwiftPluginServer"
       ],
       swiftSettings: [.interoperabilityMode(.Cxx)]
