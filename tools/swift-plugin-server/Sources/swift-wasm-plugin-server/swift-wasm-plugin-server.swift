7c984158d699f3de12c298c89e785f0d2f817d2f
Kabir Oberai <oberai.kabir@gmail.com>
2024-04-15 02:23:12 -0400

    the continuation just... works now?

--- tools/swift-plugin-server/Sources/swift-wasm-plugin-server/swift-wasm-plugin-server.swift
+++ tools/swift-plugin-server/Sources/swift-wasm-plugin-server/swift-wasm-plugin-server.swift
@@ -2,7 +2,7 @@
 //
 // This source file is part of the Swift open source project
 //
-// Copyright (c) 2023 Apple Inc. and the Swift project authors
+// Copyright (c) 2024 Apple Inc. and the Swift project authors
 // Licensed under Apache License v2.0 with Runtime Library Exception
 //
 // See http://swift.org/LICENSE.txt for license information
@@ -10,11 +10,12 @@
 //
 //===----------------------------------------------------------------------===//
 
-@_spi(PluginMessage) import SwiftCompilerPluginMessageHandling
+import SwiftCompilerPluginMessageHandling
 import swiftLLVMJSON
 import Foundation
 import CSwiftPluginServer
 
+@available(macOS 10.15, *)
 @main
 final class SwiftPluginServer {
   let connection: PluginHostConnection
@@ -32,7 +33,7 @@ final class SwiftPluginServer {
   @MainActor private func loadPluginLibrary(path: String, moduleName: String) async throws -> WasmPlugin {
     guard path.hasSuffix(".wasm") else { throw PluginServerError(message: "swift-wasm-plugin-server can only load wasm") }
     let wasm = try Data(contentsOf: URL(fileURLWithPath: path))
-    return try JSCWasmPlugin(wasm: wasm)
+    return try await JSCWasmPlugin(wasm: wasm)
   }
 
   private func expandMacro(
