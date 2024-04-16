27a3034ca29e9d95239ff9b30058b9d4f678bdf3
Kabir Oberai <oberai.kabir@gmail.com>
2024-04-16 02:44:32 -0400

    safety, tweaks

--- tools/swift-plugin-server/Sources/swift-wasm-plugin-server/WasmKitPlugin.swift
+++ tools/swift-plugin-server/Sources/swift-wasm-plugin-server/WasmKitPlugin.swift
@@ -21,6 +21,7 @@ typealias DefaultWasmPlugin = WasmKitPlugin
 struct WasmKitPlugin: WasmPlugin {
     let instance: ModuleInstance
     let runtime: Runtime
+    let memory: GuestMemory
 
     init(wasm: Data) throws {
         let module = try parseWasm(bytes: Array(wasm))
@@ -29,30 +30,37 @@ struct WasmKitPlugin: WasmPlugin {
         runtime = Runtime(hostModules: bridge.hostModules)
         instance = try runtime.instantiate(module: module)
         _ = try bridge.start(instance, runtime: runtime)
+
+        let exports = instance.exports
+        guard case let .memory(memoryAddr) = exports["memory"] else {
+            throw WasmKitPluginError(message: "Wasm plugin does not export a valid memory.")
+        }
+        self.memory = GuestMemory(store: runtime.store, address: memoryAddr)
     }
 
     func handleMessage(_ json: Data) throws -> Data {
-        let exports = instance.exports
-        guard case let .memory(memoryAddr) = exports["memory"] else { fatalError("bad memory") }
-        guard case let .function(malloc) = exports["wacro_malloc"] else { fatalError("bad wacro_malloc") }
-        guard case let .function(parse) = exports["wacro_parse"] else { fatalError("bad wacro_parse") }
-        guard case let .function(free) = exports["wacro_free"] else { fatalError("bad wacro_free") }
-        let memory = GuestMemory(store: runtime.store, address: memoryAddr)
-
         let jsonLen = UInt32(json.count)
-        let inAddr = try malloc.invoke([.i32(jsonLen)], runtime: runtime)[0].i32
+        let inAddr = try runtime.invoke(instance, function: "wacro_malloc", with: [.i32(jsonLen)])[0].i32
         let rawInAddr = UnsafeGuestPointer<UInt8>(memorySpace: memory, offset: inAddr)
         _ = UnsafeGuestBufferPointer(baseAddress: rawInAddr, count: jsonLen)
             .withHostPointer { $0.initialize(from: json) }
 
-        let outAddr = try parse.invoke([.i32(inAddr), .i32(jsonLen)], runtime: runtime)[0].i32
+        let outAddr = try runtime.invoke(instance, function: "wacro_parse", with: [.i32(inAddr), .i32(jsonLen)])[0].i32
         let outLen = UnsafeGuestPointer<UInt32>(memorySpace: memory, offset: outAddr).pointee
         let outBase = UnsafeGuestPointer<UInt8>(memorySpace: memory, offset: outAddr + 4)
-        let out = UnsafeGuestBufferPointer(baseAddress: outBase, count: outLen).withHostPointer { Data($0) }
+        let out = UnsafeGuestBufferPointer(baseAddress: outBase, count: outLen)
+            .withHostPointer { Data($0) }
 
-        _ = try free.invoke([.i32(outAddr)], runtime: runtime)
+        _ = try runtime.invoke(instance, function: "wacro_free", with: [.i32(outAddr)])
 
         return out
+    }
+}
+
+struct WasmKitPluginError: Error, CustomStringConvertible {
+    let description: String
+    init(message: String) {
+        self.description = message
     }
 }
 
