5f41b76f3af1033909de54afe5831d8cc6a97e56
Stephen Canon <scanon@apple.com>
2024-04-19 15:32:12 -0400

    Move Float16 print tests to their own file and test exhaustively (#72859) (#73106)
    We can easily test all 2**16 values, so let's do it. Also now _Float16 is properly supported in clang, so we can pass arguments to CPP that way, which lets us get snan right on more platforms.

--- stdlib/public/core/Runtime.swift
+++ stdlib/public/core/Runtime.swift
@@ -350,13 +350,22 @@ internal struct _Buffer72 {
 }
 
 #if !((os(macOS) || targetEnvironment(macCatalyst)) && arch(x86_64))
+#if arch(wasm32)
 // Note that this takes a Float32 argument instead of Float16, because clang
 // doesn't have _Float16 on all platforms yet.
+@available(SwiftStdlib 5.3, *)
+typealias _CFloat16Argument = Float32
+#else
+@available(SwiftStdlib 5.3, *)
+typealias _CFloat16Argument = Float16
+#endif
+
+@available(SwiftStdlib 5.3, *)
 @_silgen_name("swift_float16ToString")
 internal func _float16ToStringImpl(
   _ buffer: UnsafeMutablePointer<UTF8.CodeUnit>,
   _ bufferLength: UInt,
-  _ value: Float32,
+  _ value: _CFloat16Argument,
   _ debug: Bool
 ) -> Int
 
@@ -369,7 +378,7 @@ internal func _float16ToString(
   _internalInvariant(MemoryLayout<_Buffer32>.size == 32)
   var buffer = _Buffer32()
   let length = buffer.withBytes { (bufferPtr) in
-    _float16ToStringImpl(bufferPtr, 32, Float(value), debug)
+    _float16ToStringImpl(bufferPtr, 32, _CFloat16Argument(value), debug)
   }
   return (buffer, length)
 }
