0a25248e4bceaaacace65d8fae2df44c8176c69a
Alexis Laferrière <alaf@apple.com>
2024-04-22 10:19:14 -0700

    Test: Expect warning on deprecated `@_implementationOnly` in tests

--- test/Concurrency/preconcurrency_implementationonly_override.swift
+++ test/Concurrency/preconcurrency_implementationonly_override.swift
@@ -11,6 +11,7 @@
 // REQUIRES: asserts
 
 @_implementationOnly import ImplementationOnlyDefs
+// expected-warning @-1 {{'@_implementationOnly' is deprecated, use 'internal import' and family instead}}
 
 class D: C {
   @_implementationOnly
