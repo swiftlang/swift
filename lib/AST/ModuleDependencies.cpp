fed1feab9395d5df24301d86cdc0c73dd22b98b7
Artem Chikin <achikin@apple.com>
2024-04-24 15:08:23 -0700

    Revert "[6.0🍒][Dependency Scanning] Use a standalone `DependencyScanningFilesystem` on the scanner, sharing a common `status` cache from Clang's dependency service cache"

--- lib/AST/ModuleDependencies.cpp
+++ lib/AST/ModuleDependencies.cpp
@@ -427,6 +427,7 @@ SwiftDependencyScanningService::SwiftDependencyScanningService() {
       /* CAS (llvm::cas::ObjectStore) */ nullptr,
       /* Cache (llvm::cas::ActionCache) */ nullptr,
       /* SharedFS */ nullptr);
+  SharedFilesystemCache.emplace();
 }
 
 bool
