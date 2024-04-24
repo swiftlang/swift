5f71553147fa0515cc357b26779971eb79d5dd07
Steven Wu <stevenwu@apple.com>
2024-04-24 11:31:48 -0700

    [ScanDependencies] Get accurate macro dependency
    Build an accurate macro dependency for swift caching. Specifically, do
    not include not used macro plugins into the dependency, which might
    cause false negatives for cache hits.
    
    This also builds the foundation for future improvement when dependency
    scanning will determine the macro plugin to load and swift-frontend do
    not need to redo the work.
    
    rdar://127116512
    (cherry picked from commit 4125e8c3cdc4ff0287ed49d24a03c87a4884542a)

--- lib/DependencyScan/ScanDependencies.cpp
+++ lib/DependencyScan/ScanDependencies.cpp
@@ -243,6 +243,7 @@ static llvm::Error resolveExplicitModuleInputs(
     return E;
 
   std::vector<std::string> commandLine = resolvingDepInfo.getCommandline();
+  auto dependencyInfoCopy = resolvingDepInfo;
   for (const auto &depModuleID : dependencies) {
     const auto &depInfo = cache.findKnownDependency(depModuleID);
     switch (depModuleID.Kind) {
@@ -254,6 +255,14 @@ static llvm::Error resolveExplicitModuleInputs(
                        : interfaceDepDetails->moduleCacheKey;
       commandLine.push_back("-swift-module-file=" + depModuleID.ModuleName + "=" +
                             path);
+      // Add the exported macro from interface into current module.
+      llvm::for_each(
+          interfaceDepDetails->textualModuleDetails.macroDependencies,
+          [&](const auto &entry) {
+            dependencyInfoCopy.addMacroDependency(entry.first(),
+                                                  entry.second.LibraryPath,
+                                                  entry.second.ExecutablePath);
+          });
     } break;
     case swift::ModuleDependencyKind::SwiftBinary: {
       auto binaryDepDetails = depInfo.getAsSwiftBinaryModule();
@@ -318,7 +327,6 @@ static llvm::Error resolveExplicitModuleInputs(
   }
 
   // Update the dependency in the cache with the modified command-line.
-  auto dependencyInfoCopy = resolvingDepInfo;
   if (resolvingDepInfo.isSwiftInterfaceModule() ||
       resolvingDepInfo.isClangModule()) {
     if (service.hasPathMapping())
@@ -344,6 +352,10 @@ static llvm::Error resolveExplicitModuleInputs(
       llvm::for_each(
           sourceDep->auxiliaryFiles,
           [&tracker](const std::string &file) { tracker->trackFile(file); });
+      llvm::for_each(sourceDep->textualModuleDetails.macroDependencies,
+                     [&tracker](const auto &entry) {
+                       tracker->trackFile(entry.second.LibraryPath);
+                     });
       auto root = tracker->createTreeFromDependencies();
       if (!root)
         return root.takeError();
