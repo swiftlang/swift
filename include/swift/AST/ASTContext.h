f6eba5af1e8cf9ff045c419253b7ef465c016953
Augusto Noronha <anoronha@apple.com>
2024-04-25 12:19:23 -0700

    Account for different module ABI name when reconstructing a type
    A type's mangled name will store the module's ABI name, not the module's
    regular name. When reconstructing a type from a mangled name, the
    demangler needs to take that into account.
    
    rdar://126953614
    (cherry picked from commit 04034d5b80e173bf8639aba0fc27340d238a4ecf)

--- include/swift/AST/ASTContext.h
+++ include/swift/AST/ASTContext.h
@@ -1181,6 +1181,11 @@ public:
 
   ModuleDecl *getModuleByIdentifier(Identifier ModuleID);
 
+  /// Looks up an already loaded module by its ABI name.
+  ///
+  /// \returns The module if found, nullptr otherwise.
+  ModuleDecl *getLoadedModuleByABIName(StringRef ModuleName);
+
   /// Returns the standard library module, or null if the library isn't present.
   ///
   /// If \p loadIfAbsent is true, the ASTContext will attempt to load the module
