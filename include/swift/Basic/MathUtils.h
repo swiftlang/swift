852ef0bc01d3b203eb94aa1bbeb9cc43ae9ed69f
Alejandro Alonso <alejandro_alonso@apple.com>
2024-04-26 09:45:30 -0700

    Define a popcount util
    add include
    
    Define a popcount util
    
    Update MathUtils.h

--- include/swift/Basic/MathUtils.h
+++ include/swift/Basic/MathUtils.h
@@ -19,6 +19,10 @@
 
 #include <cstddef>
 
+#if SWIFT_COMPILER_IS_MSVC
+#include <intrin.h>
+#endif
+
 namespace swift {
 
 /// Round the given value up to the given alignment, as a power of two.
@@ -31,6 +35,15 @@ static inline constexpr T roundUpToAlignment(T offset, T alignment) {
 /// power of two minus one).
 static inline size_t roundUpToAlignMask(size_t size, size_t alignMask) {
   return (size + alignMask) & ~alignMask;
+}
+
+static inline unsigned popcount(unsigned value) {
+#if SWIFT_COMPILER_IS_MSVC
+  return __popcnt(value);
+#else
+  // Assume we have a compiler with this intrinsic.
+  return __builtin_popcount(value);
+#endif
 }
 
 } // namespace swift
