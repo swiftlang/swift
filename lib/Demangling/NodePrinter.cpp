aeb9039b3ab0b31b5b41c5b020cc7d3aa425159e
Dave Lee <davelee.com@gmail.com>
2024-04-30 08:11:29 -0700

    Document shouldShowEntityType

--- lib/Demangling/NodePrinter.cpp
+++ lib/Demangling/NodePrinter.cpp
@@ -1349,11 +1349,17 @@ static bool needSpaceBeforeType(NodePointer Type) {
   }
 }
 
+/// Determine whether to print an entity's type.
 static bool shouldShowEntityType(Node::Kind EntityKind,
                                  const DemangleOptions &Options) {
   switch (EntityKind) {
   case Node::Kind::ExplicitClosure:
   case Node::Kind::ImplicitClosure:
+    /// The signature of a closure (its `Type` node) can optionally be omitted.
+    /// Unlike functions which can have overloads, the signature of a closure is
+    /// not needed to be uniquely identified. A closure is uniquely identified
+    /// by its index and parent. Omitting the signature improves the readability
+    /// when long type names are in use.
     return Options.ShowClosureSignature;
   default:
     return true;
