10ae9db27a6b86d6926f263e9d2cf0a6969f8e6d
Becca Royal-Gordon <beccadax@apple.com>
2024-05-01 17:13:31 -0700

    Fix objcImpl resilience tests

--- test/Interpreter/objc_implementation_resilience.swift
+++ test/Interpreter/objc_implementation_resilience.swift
@@ -2,6 +2,12 @@
 // Will not execute correctly without ObjC runtime support.
 // REQUIRES: rdar109171643
 
-// RUN: %target-run-simple-swift(-import-objc-header %S/Inputs/objc_implementation.h -D TOP_LEVEL_CODE -D RESILIENCE -swift-version 5 -enable-experimental-feature CImplementation -enable-experimental-feature ObjCImplementationWithResilientStorage -target %target-future-triple) %S/objc_implementation.swift | %FileCheck %S/objc_implementation.swift --check-prefixes CHECK,CHECK-RESILIENCE
+// RUN: %target-run-simple-swift(-import-objc-header %S/Inputs/objc_implementation.h -D RESILIENCE -swift-version 5 -enable-experimental-feature CImplementation -enable-experimental-feature ObjCImplementationWithResilientStorage -target %target-future-triple %S/objc_implementation.swift) | %FileCheck %S/objc_implementation.swift --check-prefixes CHECK,CHECK-RESILIENCE
 // REQUIRES: executable_test
 // REQUIRES: objc_interop
+
+@main struct Main {
+  static func main() {
+    ImplClass.runTests()
+  }
+}
