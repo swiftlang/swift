faf1d2b72442b533bb33f65b4e82b365809050bf
li3zhen1 <craig.lizhen@outlook.com>
2024-05-04 08:24:37 -0700

    [Decl] Add tests for invalid extension and multi file access of init accessors

--- test/decl/var/init_accessors.swift
+++ test/decl/var/init_accessors.swift
@@ -690,3 +690,19 @@ do {
   _ = Test2(q: "some question") // Ok `a` is default initialized to `nil`
   _ = Test2(q: "ultimate question", a: 42)
 }
+
+struct Test2ForExtension {
+  var _x: Int
+}
+
+extension Test2ForExtension {
+  var extendedX: Int {
+    @storageRestrictions(initializes: _x)
+    init {
+    // expected-error@-1 {{init accessors could only be declared in the primary declaration}}
+      self._x = newValue
+    }
+    get { _x }
+    set { _x = newValue }
+  }
+}
