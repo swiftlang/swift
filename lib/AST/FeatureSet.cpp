35be4a15467cc5989f3bf2be41bde438fcb2ce8e
Kavon Farvardin <kfarvardin@apple.com>
2024-05-07 14:30:10 -0700

    NCGenerics: avoid feature-guarding in some cases
    With the generalization of Optional to support noncopyable types, our
    feature-guarding in swiftinterface files would double-print functions
    that simply refer to the Optional type.
    
    Since NoncopyableGenerics is a suppressible feature, by default
    a second version of Optional and UnsafePointer are emitted into
    swiftinterface files, where the ~Copyable generalization is stripped
    away.
    
    We can rely on that to avoid double-printing the function, if the types
    substituted for the generic parameters are all Copyable.
    
    We need a bit more checking for when
    `@_disallowFeatureSuppression(NoncopyableGenerics)` is used, since this
    trick relies on there always being a definition of the type we refer to,
    whether the feature is enabled or not.
    
    resolves rdar://127389991
    
    (cherry picked from commit f0f91eedc9aa692dc3b72b984e353bc58278ff53)

--- lib/AST/FeatureSet.cpp
+++ lib/AST/FeatureSet.cpp
@@ -504,6 +504,21 @@ static bool usesFeatureRawLayout(Decl *decl) {
 UNINTERESTING_FEATURE(Embedded)
 UNINTERESTING_FEATURE(SuppressedAssociatedTypes)
 
+static bool disallowFeatureSuppression(StringRef featureName, Decl *decl);
+
+static bool allBoundTypesAreCopyable(Type type, DeclContext *context) {
+  assert(type->getAnyNominal());
+  auto bgt = type->getAs<BoundGenericType>();
+  if (!bgt)
+    return false;  // nothing is bound.
+
+  for (auto argInterfaceTy : bgt->getGenericArgs())
+    if (context->mapTypeIntoContext(argInterfaceTy)->isNoncopyable())
+      return false;
+
+  return true;
+}
+
 static bool usesFeatureNoncopyableGenerics(Decl *decl) {
   if (decl->getAttrs().hasAttribute<PreInverseGenericsAttr>())
     return true;
@@ -521,15 +536,29 @@ static bool usesFeatureNoncopyableGenerics(Decl *decl) {
 
     if (isa<AbstractFunctionDecl>(valueDecl) ||
         isa<AbstractStorageDecl>(valueDecl)) {
-      if (valueDecl->getInterfaceType().findIf([&](Type type) -> bool {
-            if (auto *nominalDecl = type->getAnyNominal()) {
-              if (isa<StructDecl, EnumDecl, ClassDecl>(nominalDecl))
-                return usesFeatureNoncopyableGenerics(nominalDecl);
-            }
-            return false;
-          })) {
+      auto *context = decl->getInnermostDeclContext();
+      auto usesFeature = valueDecl->getInterfaceType().findIf(
+          [&](Type type) -> bool {
+        auto *nominalDecl = type->getAnyNominal();
+        if (!nominalDecl || !isa<StructDecl, EnumDecl, ClassDecl>(nominalDecl))
+          return false;
+
+        if (!usesFeatureNoncopyableGenerics(nominalDecl))
+          return false;
+
+        // If we only _refer_ to a TypeDecl that uses NoncopyableGenerics,
+        // and a suppressed version of that decl is in the interface, then we're
+        // only referring to the un-suppressed version if any of the bound types
+        // are noncopyable. (rdar://127389991)
+        if (!disallowFeatureSuppression("NoncopyableGenerics", nominalDecl)
+            && allBoundTypesAreCopyable(type, context)) {
+          return false;
+        }
+
         return true;
-      }
+      });
+      if (usesFeature)
+        return true;
     }
   }
 
