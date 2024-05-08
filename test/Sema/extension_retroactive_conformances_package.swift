9d3c63a05a327766733bad25bd8c4275628c78f4
Harlan Haskins <harlan@apple.com>
2024-05-08 12:32:28 -0600

    Add package decls to the test too

--- test/Sema/extension_retroactive_conformances_package.swift
+++ test/Sema/extension_retroactive_conformances_package.swift
@@ -6,23 +6,36 @@
 
 //--- Library.swift
 
-public class LibraryClass {
-}
-
-public protocol LibraryProtocol {
-}
+public class LibraryClass {}
+public protocol LibraryProtocol {}
+package class PackageLibraryClass {}
+package protocol PackageLibraryProtocol {}
 
 //--- OtherLibrary.swift
 public class OtherLibraryClass {}
-public class OtherLibraryProtocol {}
+public protocol OtherLibraryProtocol {}
+package class PackageOtherLibraryClass {}
+package protocol PackageOtherLibraryProtocol {}
 
 //--- Client.swift
 
-import Library
-import OtherLibrary
+// package import Library
+// package import OtherLibrary
 
-// These are all fine because all 3 of these libraries are in the same package.
+// These are all fine because all 3 of these modules are in the same package.
 
 extension LibraryClass: LibraryProtocol {}
 extension OtherLibraryClass: LibraryProtocol {}
 extension LibraryClass: OtherLibraryProtocol {}
+
+extension PackageLibraryClass: LibraryProtocol {}
+extension PackageOtherLibraryClass: LibraryProtocol {}
+extension PackageLibraryClass: OtherLibraryProtocol {}
+
+extension LibraryClass: PackageLibraryProtocol {}
+extension OtherLibraryClass: PackageLibraryProtocol {}
+extension LibraryClass: PackageOtherLibraryProtocol {}
+
+extension PackageLibraryClass: PackageLibraryProtocol {}
+extension PackageOtherLibraryClass: PackageLibraryProtocol {}
+extension PackageLibraryClass: PackageOtherLibraryProtocol {}
