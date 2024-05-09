531d91b894a4b87ad641abaffec56c8512aefabf
Slava Pestov <spestov@apple.com>
2024-05-09 17:01:08 -0400

    AST: Use hasPrimaryArchetype() in a few places

--- lib/AST/Decl.cpp
+++ lib/AST/Decl.cpp
@@ -8180,7 +8180,7 @@ Type VarDecl::getPropertyWrapperInitValueInterfaceType() const {
     return Type();
 
   Type valueInterfaceTy = initInfo.getWrappedValuePlaceholder()->getType();
-  if (valueInterfaceTy->hasArchetype())
+  if (valueInterfaceTy->hasPrimaryArchetype())
     valueInterfaceTy = valueInterfaceTy->mapTypeOutOfContext();
 
   return valueInterfaceTy;
