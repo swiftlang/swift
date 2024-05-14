29da947e508a73ab99de64ab16f3ae802e70fb50
Arnold Schwaighofer <aschwaighofer@apple.com>
2024-05-13 19:21:24 -0700

    [6.0] Revert "Merge pull request #72708 from aschwaighofer/c_array_heuristic_6.0"
    This reverts commit 0e350d8d2c0252e3391e71b3eeda83e551ad93bc, reversing
    changes made to 1e5659ed00e2491f39f50fd263746e3787a3526b.
    
    It triggered a bug that needs to be tracked down.
    
    rdar://128019015

--- lib/IRGen/LoadableByAddress.cpp
+++ lib/IRGen/LoadableByAddress.cpp
@@ -17,7 +17,6 @@
 //===----------------------------------------------------------------------===//
 
 #define DEBUG_TYPE "loadable-address"
-#include "Explosion.h"
 #include "FixedTypeInfo.h"
 #include "IRGenMangler.h"
 #include "IRGenModule.h"
@@ -3432,29 +3431,6 @@ public:
     toDeleteBlockArg.push_back(std::make_pair(b, argIdx));
   }
 
-  bool isPotentiallyCArray(SILType ty) {
-    if (ty.isAddress() || ty.isClassOrClassMetatype()) {
-      return false;
-    }
-
-    auto canType = ty.getASTType();
-    if (canType->hasTypeParameter()) {
-      assert(genEnv && "Expected a GenericEnv");
-      canType = genEnv->mapTypeIntoContext(canType)->getCanonicalType();
-    }
-
-    if (canType.getAnyGeneric() || isa<TupleType>(canType)) {
-      assert(ty.isObject() &&
-             "Expected only two categories: address and object");
-      assert(!canType->hasTypeParameter());
-      const TypeInfo &TI = irgenModule->getTypeInfoForLowered(canType);
-      auto explosionSchema = TI.getSchema();
-      if (explosionSchema.size() > 15)
-        return true;
-    }
-    return false;
-  }
-
   bool isLargeLoadableType(SILType ty) {
     if (ty.isAddress() || ty.isClassOrClassMetatype()) {
       return false;
@@ -3472,11 +3448,7 @@ public:
       assert(!canType->hasTypeParameter());
       const TypeInfo &TI = irgenModule->getTypeInfoForLowered(canType);
       auto &nativeSchemaOrigParam = TI.nativeParameterValueSchema(*irgenModule);
-      if (nativeSchemaOrigParam.size() > 15)
-        return true;
-      auto explosionSchema = TI.getSchema();
-      if (explosionSchema.size() > 15)
-        return true;
+      return nativeSchemaOrigParam.size() > 15;
     }
     return false;
   }
@@ -3793,7 +3765,7 @@ protected:
       builder.createStore(bc->getLoc(), bc->getOperand(), opdAddr,
                           StoreOwnershipQualifier::Unqualified);
       assignment.mapValueToAddress(origValue, addr);
-      assignment.markForDeletion(bc);
+
       return;
     }
     auto opdAddr = assignment.getAddressForValue(bc->getOperand());
@@ -3955,9 +3927,7 @@ protected:
   }
 
   void visitDebugValueInst(DebugValueInst *dbg) {
-    if (!dbg->hasAddrVal() &&
-        (assignment.isPotentiallyCArray(dbg->getOperand()->getType()) ||
-         overlapsWithOnStackDebugLoc(dbg->getOperand()))) {
+    if (!dbg->hasAddrVal() && overlapsWithOnStackDebugLoc(dbg->getOperand())) {
       assignment.markForDeletion(dbg);
       return;
     }
