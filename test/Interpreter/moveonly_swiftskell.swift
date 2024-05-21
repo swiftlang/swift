9c89d76dcbd5c9ed4decc5570d262bf8708bdcf9
Kavon Farvardin <kfarvardin@apple.com>
2024-05-20 17:42:51 -0700

    Test: get to "hello world"
    (cherry picked from commit a361eabbb15ec4227f04af6199e836f421a475b7)

--- test/Interpreter/moveonly_swiftskell.swift
+++ test/Interpreter/moveonly_swiftskell.swift
@@ -1,14 +1,16 @@
 // RUN: %empty-directory(%t)
 
 // RUN: %target-build-swift \
+// RUN:    -emit-module      \
 // RUN:    -emit-module-path %t \
 // RUN:    -enable-library-evolution \
 // RUN:    -module-name Swiftskell \
 // RUN:    -parse-as-library \
-// RUN:    %S/../Inputs/Swiftskell.swift -c -o %t/Swiftskell.o \
+// RUN:    %S/../Inputs/Swiftskell.swift \
+// RUN:   -enable-experimental-feature SuppressedAssociatedTypes \
 // RUN:   -enable-experimental-feature NonescapableTypes
 
-// RUN: %target-build-swift -o %t/a.out %s -I %t %t/Swiftskell.o
+// RUN: %target-build-swift %s -I %t -o %t/a.out
 // RUN: %target-codesign %t/a.out
 // RUN: %target-run %t/a.out | %FileCheck %s
 
