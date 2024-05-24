52faf8e1633bbd56bc8d4733f4fcddd9eb30e811
Arnold Schwaighofer <aschwaighofer@apple.com>
2024-05-23 18:53:15 -0700

    [6.0] Merge pull request #73794 from aschwaighofer/package_cmo_opaque_type_subst
    Fix opaque type substitution in the package cmo resilience domain
    
    We need to serialize the underlying type if we want to see the underlying type accross the package resilience domain.
    
    rdar://128482863
    
    (cherry picked from commit 58cc2a4090e6816f69183d189f106b0cf6f4595a)
    
    Risk: Low, should only affect package CMO mode
    
    rdar://128647016

--- lib/AST/TypeSubstitution.cpp
+++ lib/AST/TypeSubstitution.cpp
@@ -1003,6 +1003,14 @@ ReplaceOpaqueTypesWithUnderlyingTypes::shouldPerformSubstitution(
       module == contextModule)
     return OpaqueSubstitutionKind::SubstituteSameModuleMaximalResilience;
 
+  // Allow replacement of opaque result types in the context of maximal
+  // resilient expansion if the context's and the opaque type's module are in
+  // the same package.
+  if (contextExpansion == ResilienceExpansion::Maximal &&
+      module->isResilient() && module->serializePackageEnabled() &&
+      module->inSamePackage(contextModule))
+    return OpaqueSubstitutionKind::SubstituteSamePackageMaximalResilience;
+
   // Allow general replacement from non resilient modules. Otherwise, disallow.
   if (module->isResilient())
     return OpaqueSubstitutionKind::DontSubstitute;
@@ -1065,6 +1073,10 @@ static bool canSubstituteTypeInto(Type ty, const DeclContext *dc,
       return true;
 
     return typeDecl->getEffectiveAccess() > AccessLevel::FilePrivate;
+
+  case OpaqueSubstitutionKind::SubstituteSamePackageMaximalResilience: {
+    return typeDecl->getEffectiveAccess() >= AccessLevel::Package;
+  }
 
   case OpaqueSubstitutionKind::SubstituteNonResilientModule:
     // Can't access types that are not public from a different module.
