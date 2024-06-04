6c9ffafc20f567215be5d8fcd032eba469a2888f
Jeremy Schonfeld <jschonfeld@apple.com>
2024-06-04 14:55:00 -0700

    [Windows] Prepare for swift-foundation in the toolchain

--- utils/build.ps1
+++ utils/build.ps1
@@ -1414,6 +1414,7 @@ function Build-Dispatch([Platform]$Platform, $Arch, [switch]$Test = $false) {
 
 function Build-Foundation([Platform]$Platform, $Arch, [switch]$Test = $false) {
   $DispatchBinaryCache = Get-TargetProjectBinaryCache $Arch Dispatch
+  $SwiftSyntaxBinaryCache = Get-TargetProjectBinaryCache $Arch SwiftSyntax
   $FoundationBinaryCache = Get-TargetProjectBinaryCache $Arch Foundation
   $ShortArch = $Arch.LLVMName
 
@@ -1458,6 +1459,10 @@ function Build-Foundation([Platform]$Platform, $Arch, [switch]$Test = $false) {
         ZLIB_LIBRARY = "$LibraryRoot\zlib-1.3.1\usr\lib\$Platform\$ShortArch\zlibstatic.lib";
         ZLIB_INCLUDE_DIR = "$LibraryRoot\zlib-1.3.1\usr\include";
         dispatch_DIR = "$DispatchBinaryCache\cmake\modules";
+        SwiftSyntax_DIR = "$SwiftSyntaxBinaryCache\cmake\modules";
+        _SwiftFoundation_SourceDIR = "$SourceCache\swift-foundation";
+        _SwiftFoundationICU_SourceDIR = "$SourceCache\swift-foundation-icu";
+        _SwiftCollections_SourceDIR = "$SourceCache\swift-collections"
       } + $TestingDefines)
   }
 }
