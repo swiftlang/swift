74aaf8869789337c230341be2473443fa99f489b
Joe Groff <jgroff@apple.com>
2024-06-05 14:12:00 -0700

    Turn mishandled reinitialize-in-`defer`-after-`consume` cases into errors.
    The handling of multi-basic-block control flow in `defer` blocks looks like it
    was left incomplete and completely untested; I fixed a few obvious problems but
    it still completely lacks any analysis of conditional reinitializations. For now,
    change it to treat attempted reinitializations as uses-after-consumes so we raise
    reliable errors now instead of emitting code that causes memory corruption at
    runtime. Fixes rdar://129303198.

--- lib/AST/FeatureSet.cpp
+++ lib/AST/FeatureSet.cpp
@@ -767,6 +767,7 @@ static bool usesFeatureSensitive(Decl *decl) {
 }
 
 UNINTERESTING_FEATURE(DebugDescriptionMacro)
+UNINTERESTING_FEATURE(ReinitializeConsumeInMultiBlockDefer)
 
 // ----------------------------------------------------------------------------
 // MARK: - FeatureSet
