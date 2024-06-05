b3a98d69a8dc0b5279bfcb9a81350be51c4519fc
Alejandro Alonso <alejandro_alonso@apple.com>
2024-06-04 18:38:50 -0700

    Merge pull request #71383 from Azoy/mutex
    [stdlib] Implement Mutex in Synchronization

--- stdlib/public/Synchronization/Atomics/Atomic.swift
+++ stdlib/public/Synchronization/Atomics/Atomic.swift
@@ -22,14 +22,14 @@ public struct Atomic<Value: AtomicRepresentable> {
   @available(SwiftStdlib 6.0, *)
   @_alwaysEmitIntoClient
   @_transparent
-  var address: UnsafeMutablePointer<Value.AtomicRepresentation> {
-    UnsafeMutablePointer<Value.AtomicRepresentation>(rawAddress)
+  var _address: UnsafeMutablePointer<Value.AtomicRepresentation> {
+    UnsafeMutablePointer<Value.AtomicRepresentation>(_rawAddress)
   }
 
   @available(SwiftStdlib 6.0, *)
   @_alwaysEmitIntoClient
   @_transparent
-  var rawAddress: Builtin.RawPointer {
+  var _rawAddress: Builtin.RawPointer {
     Builtin.unprotectedAddressOfBorrow(self)
   }
 
@@ -40,7 +40,7 @@ public struct Atomic<Value: AtomicRepresentable> {
   @_alwaysEmitIntoClient
   @_transparent
   public init(_ initialValue: consuming Value) {
-    address.initialize(to: Value.encodeAtomicRepresentation(initialValue))
+    _address.initialize(to: Value.encodeAtomicRepresentation(initialValue))
   }
 
   // Deinit's can't be marked @_transparent. Do these things need all of these
@@ -49,10 +49,10 @@ public struct Atomic<Value: AtomicRepresentable> {
   @_alwaysEmitIntoClient
   @inlinable
   deinit {
-    let oldValue = Value.decodeAtomicRepresentation(address.pointee)
+    let oldValue = Value.decodeAtomicRepresentation(_address.pointee)
     _ = consume oldValue
 
-    address.deinitialize(count: 1)
+    _address.deinitialize(count: 1)
   }
 }
 
