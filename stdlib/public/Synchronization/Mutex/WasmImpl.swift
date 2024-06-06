2dbbbcf64656072d5b43d7a5139c5efd3a4e9296
Yuta Saito <kateinoigakukun@gmail.com>
2024-06-06 04:14:46 +0000

    [Synchronization] Skip atomic operations in single-threaded mode on WebAssembly
    Use of atomics instructions requires the support of threads proposal and
    it's not widely supported yet. So we should enable actual atomic
    operations only when targeting wasm32-uknown-wasip1-threads.

--- stdlib/public/Synchronization/Mutex/WasmImpl.swift
+++ stdlib/public/Synchronization/Mutex/WasmImpl.swift
@@ -25,6 +25,7 @@ internal func _swift_stdlib_wake(on: UnsafePointer<UInt32>, count: UInt32) -> UI
 
 extension Atomic where Value == _MutexHandle.State {
   internal borrowing func _wait(expected: _MutexHandle.State) {
+    #if _runtime(_multithreaded)
     _ = _swift_stdlib_wait(
       on: .init(_rawAddress),
       expected: expected.rawValue,
@@ -32,11 +33,14 @@ extension Atomic where Value == _MutexHandle.State {
       // A timeout of < 0 means indefinitely.
       timeout: -1
     )
+    #endif
   }
 
   internal borrowing func _wake() {
+    #if _runtime(_multithreaded)
     // Only wake up 1 thread
     _ = _swift_stdlib_wake(on: .init(_rawAddress), count: 1)
+    #endif
   }
 }
 
