d7354b6f279b522b6ef75e1dbaeb73042ff62862
Nate Chandler <nate_chandler@apple.com>
2024-06-11 07:56:52 -0700

    [NFC] MOWTE: Deduplicated code.
    Replaced copy-pasted code with a twice-invoked closure.  In preparation
    to add a third invocation.

--- lib/SILOptimizer/Mandatory/MoveOnlyWrappedTypeEliminator.cpp
+++ lib/SILOptimizer/Mandatory/MoveOnlyWrappedTypeEliminator.cpp
@@ -299,45 +299,55 @@ bool SILMoveOnlyWrappedTypeEliminator::process() {
   llvm::SmallSetVector<SILArgument *, 8> touchedArgs;
   llvm::SmallSetVector<SILInstruction *, 8> touchedInsts;
 
+  // For each value whose type is move-only wrapped:
+  // - rewrite the value's type
+  // - record its users for later visitation
+  auto visitValue = [&touchedInsts, fn = fn,
+                     trivialOnly = trivialOnly](SILValue value) -> bool {
+    if (!value->getType().hasAnyMoveOnlyWrapping(fn))
+      return false;
+
+    // If we are looking at trivial only, skip non-trivial function args.
+    if (trivialOnly && !isMoveOnlyWrappedTrivial(value))
+      return false;
+
+    for (auto *use : value->getNonTypeDependentUses())
+      touchedInsts.insert(use->getUser());
+
+    value->unsafelyEliminateMoveOnlyWrapper(fn);
+
+    return true;
+  };
+
   for (auto &bb : *fn) {
     for (auto *arg : bb.getArguments()) {
-      if (!arg->getType().hasAnyMoveOnlyWrapping(fn))
+      bool relevant = visitValue(arg);
+      if (!relevant)
         continue;
-
-      // If we are looking at trivial only, skip non-trivial function args.
-      if (trivialOnly &&
-          !arg->getType().removingMoveOnlyWrapper().isTrivial(*fn))
-        continue;
-
-      arg->unsafelyEliminateMoveOnlyWrapper(fn);
 
       // If our new type is trivial, convert the arguments ownership to
       // None. Otherwise, preserve the ownership kind of the argument.
       if (arg->getType().isTrivial(*fn))
         arg->setOwnershipKind(OwnershipKind::None);
       touchedArgs.insert(arg);
-      for (auto *use : arg->getNonTypeDependentUses())
-        touchedInsts.insert(use->getUser());
+
+      madeChange = true;
     }
 
     for (auto &ii : bb) {
-      for (SILValue v : ii.getResults()) {
-        if (!v->getType().hasAnyMoveOnlyWrapping(fn))
+      bool touched = false;
+      for (SILValue value : ii.getResults()) {
+        bool relevant = visitValue(value);
+        if (!relevant)
           continue;
 
-        if (trivialOnly &&
-            !isMoveOnlyWrappedTrivial(v))
-          continue;
-
-        v->unsafelyEliminateMoveOnlyWrapper(fn);
-        touchedInsts.insert(&ii);
-
-        // Add all users as well. This ensures we visit things like
-        // destroy_value and end_borrow.
-        for (auto *use : v->getNonTypeDependentUses())
-          touchedInsts.insert(use->getUser());
-        madeChange = true;
+        touched = true;
       }
+      if (!touched)
+        continue;
+      touchedInsts.insert(&ii);
+
+      madeChange = true;
     }
   }
 
