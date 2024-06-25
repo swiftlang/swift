8518c719608d3b373951dfccc3b7d2b0736b2bea
Joe Groff <jgroff@apple.com>
2024-06-25 14:10:02 -0700

    MoveOnlyAddressChecker: More robust checking for consume-during-borrow.
    - While an opaque borrow access occurs to part of a value, the entire scope of
      the access needs to be treated as a liveness range, so add the `EndAccess`es
      to the liveness range.
    - The SIL verifier may crash the compiler on SILGen-generated code when the
      developer's source contains consume-during-borrow code patterns. Allow
      `load_borrow` instructions to be marked `[unchecked]`, which suppresses
      verifier checks until the move checker runs and gets a chance to properly
      diagnose these errors.
    
    Fixes rdar://124360175.

--- lib/SILOptimizer/Mandatory/MoveOnlyAddressCheckerUtils.cpp
+++ lib/SILOptimizer/Mandatory/MoveOnlyAddressCheckerUtils.cpp
@@ -1257,23 +1257,29 @@ void UseState::initializeLiveness(
                             << *livenessInstAndValue.first;
                liveness.print(llvm::dbgs()));
   }
+  
+  auto updateForLivenessAccess = [&](BeginAccessInst *beginAccess,
+                                     const SmallBitVector &livenessMask) {
+    for (auto *endAccess : beginAccess->getEndAccesses()) {
+      liveness.updateForUse(endAccess, livenessMask, false /*lifetime ending*/);
+    }
+  };
 
   for (auto livenessInstAndValue : nonconsumingUses) {
     if (auto *lbi = dyn_cast<LoadBorrowInst>(livenessInstAndValue.first)) {
       auto accessPathWithBase =
           AccessPathWithBase::computeInScope(lbi->getOperand());
       if (auto *beginAccess =
-              dyn_cast<BeginAccessInst>(accessPathWithBase.base)) {
-        for (auto *endAccess : beginAccess->getEndAccesses()) {
-          liveness.updateForUse(endAccess, livenessInstAndValue.second,
-                                false /*lifetime ending*/);
-        }
+              dyn_cast_or_null<BeginAccessInst>(accessPathWithBase.base)) {
+        updateForLivenessAccess(beginAccess, livenessInstAndValue.second);
       } else {
         for (auto *ebi : lbi->getEndBorrows()) {
           liveness.updateForUse(ebi, livenessInstAndValue.second,
                                 false /*lifetime ending*/);
         }
       }
+    } else if (auto *bai = dyn_cast<BeginAccessInst>(livenessInstAndValue.first)) {
+      updateForLivenessAccess(bai, livenessInstAndValue.second);
     } else {
       liveness.updateForUse(livenessInstAndValue.first,
                             livenessInstAndValue.second,
