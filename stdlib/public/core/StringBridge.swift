fa74b763c1f0b2e39e9b3b53e4c5ade4bc2f1f6a
David Smith <david_smith@apple.com>
2024-06-26 14:41:13 -0700

    Remove now-redundant thing

--- stdlib/public/core/StringBridge.swift
+++ stdlib/public/core/StringBridge.swift
@@ -696,7 +696,7 @@ extension String {
         _internalInvariant(!copy._guts.isSmall)
         return copy._bridgeToObjectiveCImpl()
     }
-    if _guts._object.isImmortal && !_guts._object.hasObjCBridgeableObject {
+    if _guts._object.isImmortal {
       // TODO: We'd rather emit a valid ObjC object statically than create a
       // shared string class instance.
       let gutsCountAndFlags = _guts._object._countAndFlags
