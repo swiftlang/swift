52821946824b2497cced8f31cf062074233672f9
Meghana Gupta <meghanavgupta@gmail.com>
2024-06-28 19:41:44 -0700

    Fix isTriviallyDuplicatable to handle open_pack_element
    SILInstruction::clone doesn't know how to clone instructions that produce
    the archetype uuid. SILCloner is equipped to handle such instructions.
    
    Optimizations like LoopRotate use  SILInstruction::clone and will be
    incorrect for such instructions.
    
    rdar://130047619

--- lib/SIL/IR/SILInstruction.cpp
+++ lib/SIL/IR/SILInstruction.cpp
@@ -1418,8 +1418,10 @@ bool SILInstruction::isTriviallyDuplicatable() const {
 
   if (isa<OpenExistentialAddrInst>(this) || isa<OpenExistentialRefInst>(this) ||
       isa<OpenExistentialMetatypeInst>(this) ||
-      isa<OpenExistentialValueInst>(this) || isa<OpenExistentialBoxInst>(this) ||
-      isa<OpenExistentialBoxValueInst>(this)) {
+      isa<OpenExistentialValueInst>(this) ||
+      isa<OpenExistentialBoxInst>(this) ||
+      isa<OpenExistentialBoxValueInst>(this) ||
+      isa<OpenPackElementInst>(this)) {
     // Don't know how to duplicate these properly yet. Inst.clone() per
     // instruction does not work. Because the follow-up instructions need to
     // reuse the same archetype uuid which would only work if we used a
