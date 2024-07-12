a3c614d924b10cde933f594f94be62d0867063a6
Jeremy Schonfeld <jschonfeld@apple.com>
2024-07-12 09:54:46 -0700

    Fix build failure in _SynchronizationShims module

--- stdlib/public/SwiftShims/swift/shims/_SynchronizationShims.h
+++ stdlib/public/SwiftShims/swift/shims/_SynchronizationShims.h
@@ -23,7 +23,7 @@
 #include <unistd.h>
 
 static inline __swift_uint32_t _swift_stdlib_gettid() {
-  static __thread tid = 0;
+  static __thread auto tid = 0;
 
   if (tid == 0) {
     tid = syscall(SYS_gettid);
