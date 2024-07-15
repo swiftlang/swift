651092e54f971fefe64ce8c7b29aa59aeff77d71
Steven Wu <stevenwu@apple.com>
2024-07-15 14:02:28 -0700

    [Interface] Always build swiftinterface when explicitly requested
    When explicitly requested to build a swiftinterface file to swiftmodule,
    alway perform the entire compilation, instead of immitating implicit
    build to do a up-to-date check in order to skip the compilation.
    
    This can cause different build behavior depending on the order of the
    task, turning a failed compilation into a successful one. The next step
    might be using a stricter module hash to avoid output path collision all
    together.
    
    rdar://131780720

--- test/Frontend/output_determinism_check.swift
+++ test/Frontend/output_determinism_check.swift
@@ -17,9 +17,8 @@
 // RUN: %target-swift-frontend -typecheck -emit-module-interface-path %t/test.swiftinterface %s -O -enable-deterministic-check 2>&1 | %FileCheck %s --check-prefix=INTERFACE_OUTPUT
 /// Hit cache and not emit the second time.
 // RUN: rm %t/test.swiftmodule
-// RUN: not %target-swift-frontend -compile-module-from-interface %t/test.swiftinterface -explicit-interface-module-build -o %t/test.swiftmodule -enable-deterministic-check 2>&1 | %FileCheck --check-prefix=MODULE_MISMATCH %s
 /// Force swiftmodule generation.
-// RUN: %target-swift-frontend -compile-module-from-interface %t/test.swiftinterface -explicit-interface-module-build -o %t/test.swiftmodule -enable-deterministic-check -always-compile-output-files 2>&1 | %FileCheck --check-prefix=MODULE_OUTPUT %s
+// RUN: %target-swift-frontend -compile-module-from-interface %t/test.swiftinterface -explicit-interface-module-build -o %t/test.swiftmodule -enable-deterministic-check 2>&1 | %FileCheck --check-prefix=MODULE_OUTPUT %s
 
 // RUN: %target-swift-frontend -emit-pcm -module-name UserClangModule -o %t/test.pcm %S/Inputs/dependencies/module.modulemap -enable-deterministic-check 2>&1 | %FileCheck %s --check-prefix=PCM_OUTPUT
 
@@ -35,7 +34,6 @@
 // DEPSCAN_CACHE_OUTPUT: remark: produced matching output file '{{.*}}{{/|\\}}deps-cache'
 // DEPSCAN_OUTPUT: remark: produced matching output file '{{.*}}{{/|\\}}test.json'
 // INTERFACE_OUTPUT: remark: produced matching output file '{{.*}}{{/|\\}}test.swiftinterface'
-// MODULE_MISMATCH: error: output file '{{.*}}{{/|\\}}test.swiftmodule' is missing from second compilation for deterministic check
 // PCM_OUTPUT: remark: produced matching output file '{{.*}}{{/|\\}}test.pcm'
 
 public var x = 1
