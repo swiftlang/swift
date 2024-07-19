e195093aeea6199684134c344987d378a42c9a25
Gabor Horvath <gaborh@apple.com>
2024-07-19 17:00:10 +0100

    [cxx-interop] Fix the printing of types with generic arguments
    Previously the code got the declaration for types with generic
    arguments and the printer used the declaration. This was a lossy
    operation, we printed the type with generic parameters instead of the
    arguments. This patch makes sure we print the type with the arguments.
    Unfortunately, the code structure is not the most clear, type printing
    is currently inherently part of the function signature printing. This
    code path needs to be factored out in the future to make the code easier
    to understand.
    
    rdar://130679337

--- test/Interop/SwiftToCxx/enums/enum-associated-value-class-type-cxx.swift
+++ test/Interop/SwiftToCxx/enums/enum-associated-value-class-type-cxx.swift
@@ -29,6 +29,16 @@ extension E {
   }
 }
 
+public enum F {
+  case a(Int)
+  case b([C])
+}
+
+public enum G<T> {
+  case a(Int)
+  case b([T])
+}
+
 // CHECK:      SWIFT_INLINE_THUNK E E::_impl_c::operator()(const C& val) const {
 // CHECK-NEXT:   auto result = E::_make();
 // CHECK-NEXT:   auto op = swift::_impl::_impl_RefCountedClass::copyOpaquePointer(val);
@@ -47,3 +57,23 @@ extension E {
 
 // CHECK: SWIFT_INLINE_THUNK bool E::matchesIntValue(swift::Int value) const {
 // CHECK-NEXT: return _impl::$s5Enums1EO15matchesIntValueySbSiF(value, _impl::swift_interop_passDirect_Enums_uint64_t_0_8_uint8_t_8_9(_getOpaquePointer()));
+
+// CHECK: SWIFT_INLINE_THUNK swift::Array<C> F::getB() const {
+// CHECK-NEXT:    if (!isB()) abort();
+// CHECK-NEXT:    alignas(F) unsigned char buffer[sizeof(F)];
+// CHECK-NEXT:    auto *thisCopy = new(buffer) F(*this);
+// CHECK-NEXT:    char * _Nonnull payloadFromDestruction = thisCopy->_destructiveProjectEnumData();
+// CHECK-NEXT:    return swift::_impl::implClassFor<swift::Array<C>>::type::returnNewValue([&](char * _Nonnull result) SWIFT_INLINE_THUNK_ATTRIBUTES {
+// CHECK-NEXT:      swift::_impl::implClassFor<swift::Array<C>>::type::initializeWithTake(result, payloadFromDestruction);
+// CHECK-NEXT:    });
+// CHECK-NEXT:  }
+
+// CHECK:   SWIFT_INLINE_THUNK swift::Array<T_0_0> G<T_0_0>::getB() const {
+// CHECK-NEXT:     if (!isB()) abort();
+// CHECK-NEXT:     alignas(G) unsigned char buffer[sizeof(G)];
+// CHECK-NEXT:     auto *thisCopy = new(buffer) G(*this);
+// CHECK-NEXT:     char * _Nonnull payloadFromDestruction = thisCopy->_destructiveProjectEnumData();
+// CHECK-NEXT:     return swift::_impl::implClassFor<swift::Array<T_0_0>>::type::returnNewValue([&](char * _Nonnull result) SWIFT_INLINE_THUNK_ATTRIBUTES {
+// CHECK-NEXT:       swift::_impl::implClassFor<swift::Array<T_0_0>>::type::initializeWithTake(result, payloadFromDestruction);
+// CHECK-NEXT:     });
+// CHECK-NEXT:   }
