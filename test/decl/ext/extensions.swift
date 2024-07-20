ad524c163ab73910e2f40882efea46685778c5f0
iMostafa <mostafa.essam@wallapop.com>
2024-07-21 00:52:49 +0200

    Fixed tests

--- test/decl/ext/extensions.swift
+++ test/decl/ext/extensions.swift
@@ -206,7 +206,7 @@ extension DoesNotImposeClassReq_2 where Self : AnyObject {
   var wrappingProperty2: String {
     get { property }
     nonmutating set { property = newValue } // expected-error {{cannot assign to property: 'self' is immutable}}
-    // expected-note@-1 {{mark accessor 'mutating' to make 'self' mutable}}{{5-16=mutating}}
+    // expected-note@-1 {{mark setter 'mutating' to make 'self' mutable}}{{5-16=mutating}}
   }
   
   var wrappingProperty3: String {
