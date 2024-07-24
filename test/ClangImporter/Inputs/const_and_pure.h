d2184688aae95c86d2873efe3cefc4a51f64a8a4
Becca Royal-Gordon <beccadax@apple.com>
2024-07-24 13:07:13 -0700

    Fix ClangImporter/const_and_pure.swift
    In the rebranch compiler, `void` functions cannot be const or pure.
    
    Fixes rdar://127262449.

--- test/ClangImporter/Inputs/const_and_pure.h
+++ test/ClangImporter/Inputs/const_and_pure.h
@@ -1,7 +1,7 @@
 
-__attribute__((const)) void const_function();
+__attribute__((const)) int const_function();
 
-__attribute__((pure)) void pure_function();
+__attribute__((pure)) int pure_function();
 
-void normal_function();
+int normal_function();
 
