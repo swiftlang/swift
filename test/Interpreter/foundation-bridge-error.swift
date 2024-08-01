03c25c467ca2e5cdaed9e8fddf631ff07bb72da4
Ben Langmuir <blangmuir@apple.com>
2024-08-01 09:45:48 -0700

    Test only

---
+++ test/Interpreter/foundation-bridge-error.swift
@@ -0,0 +1,21 @@
+// Check that we can access localizedDescription, which crashes in the runtime
+// if Foundation is loaded after the runtime is already initialized on Darwin.
+
+// REQUIRES: executable_test
+// REQUIRES: objc_interop
+// REQUIRES: OS=macosx
+
+// RUN: %target-jit-run %s
+// RUN: DYLD_INSERT_LIBRARIES=/System/Library/Frameworks/Foundation.framework/Foundation %target-jit-run %s
+
+import Foundation
+
+print("Insert Libraries: \(ProcessInfo.processInfo.environment["DYLD_INSERT_LIBRARIES"] ?? "<nil>")")
+
+enum SomeError: LocalizedError {
+ case fail
+}
+
+let err = SomeError.fail
+let path = (#file as NSString).lastPathComponent
+let desc = err.localizedDescription
