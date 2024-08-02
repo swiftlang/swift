fd41d2601f97999c506b08dae1bd06bc4e2a6e0c
Andrew Kaster <andrew@ladybird.org>
2024-08-02 17:04:20 -0600

    [cxx-interop] Disable c++ execution header with libstdcxx versions >= 11
    Workaround for https://github.com/swiftlang/swift/issues/75661

--- stdlib/public/Cxx/libstdcxx/libstdcxx.h
+++ stdlib/public/Cxx/libstdcxx/libstdcxx.h
@@ -1,7 +1,9 @@
+#include "version"
+
 // C++17 and newer:
 
 // <execution> includes tbb headers, which might not be installed.
 // Only include <execution> if tbb is installed.
-#if __has_include("execution") && __has_include(<tbb/blocked_range.h>)
+#if __has_include("execution") && __has_include(<tbb/blocked_range.h>) && (_GLIBCXX_RELEASE < 11)
 #include "execution"
 #endif
