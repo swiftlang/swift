96bfaafb14f81eadc9664c2579d9c5d0c0bd7695
Alejandro Alonso <alejandro_alonso@apple.com>
2024-08-08 21:26:56 -0700

    Update ASTDumperTests.cpp

--- unittests/AST/ASTDumperTests.cpp
+++ unittests/AST/ASTDumperTests.cpp
@@ -53,5 +53,5 @@ TEST(ASTDumper, ArchetypeType) {
 
   EXPECT_EQ(str,
             " name=\"\\xCF\\x84_0_0\"\n"
-            "  (interface_type=generic_type_param_type depth=0 index=0))\n");
+            "  (interface_type=generic_type_param_type depth=0 index=0 param_kind=type))\n");
 }
