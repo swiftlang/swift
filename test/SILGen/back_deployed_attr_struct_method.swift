adf058f88e6f4130a80c872e63d16216a81019ec
Ben Langmuir <blangmuir@apple.com>
2024-08-14 16:39:27 -0700

    [test] Move availability tests to later fake OS versions
    10.50 was once greater than any real macOS version, but now it compares
    less than real released versions, which makes these tests depend on the
    deployment target unnecessarily. Update these tests to use even larger
    numbers to hopefully keep them independent a little longer.
    
    (cherry picked from commit fd1875dcfbb752a770c64a478b27a79b8b01267e)
    
     Conflicts:
    	test/Interpreter/Inputs/availability_zippered.swift
    	test/Parse/availability_query_unavailability.swift
    	test/decl/enum/derived_hashable_equatable_macos.swift
    
    (cherry picked from commit 3e76bb53896ac6df15d991dc5fc9e9f2fa5ec8b0)

--- test/SILGen/back_deployed_attr_struct_method.swift
+++ test/SILGen/back_deployed_attr_struct_method.swift
@@ -14,8 +14,8 @@ public struct TopLevelStruct {
   // -- Back deployment thunk for TopLevelStruct.trivialMethod()
   // CHECK-LABEL: sil non_abi [serialized] [thunk] [ossa] @$s11back_deploy14TopLevelStructV13trivialMethodyyFTwb : $@convention(method) (TopLevelStruct) -> ()
   // CHECK: bb0([[BB0_ARG:%.*]] : $TopLevelStruct):
-  // CHECK:   [[MAJOR:%.*]] = integer_literal $Builtin.Word, 10
-  // CHECK:   [[MINOR:%.*]] = integer_literal $Builtin.Word, 52
+  // CHECK:   [[MAJOR:%.*]] = integer_literal $Builtin.Word, 52
+  // CHECK:   [[MINOR:%.*]] = integer_literal $Builtin.Word, 1
   // CHECK:   [[PATCH:%.*]] = integer_literal $Builtin.Word, 0
   // CHECK:   [[OSVFN:%.*]] = function_ref @$ss26_stdlib_isOSVersionAtLeastyBi1_Bw_BwBwtF : $@convention(thin) (Builtin.Word, Builtin.Word, Builtin.Word) -> Builtin.Int1
   // CHECK:   [[AVAIL:%.*]] = apply [[OSVFN]]([[MAJOR]], [[MINOR]], [[PATCH]]) : $@convention(thin) (Builtin.Word, Builtin.Word, Builtin.Word) -> Builtin.Int1
@@ -36,8 +36,8 @@ public struct TopLevelStruct {
   // CHECK:   return [[RESULT]] : $()
 
   // -- Original definition of TopLevelStruct.trivialMethod()
-  // CHECK-LABEL: sil [available 10.52] [ossa] @$s11back_deploy14TopLevelStructV13trivialMethodyyF : $@convention(method) (TopLevelStruct) -> ()
-  @backDeployed(before: macOS 10.52)
+  // CHECK-LABEL: sil [available 52.1] [ossa] @$s11back_deploy14TopLevelStructV13trivialMethodyyF : $@convention(method) (TopLevelStruct) -> ()
+  @backDeployed(before: macOS 52.1)
   public func trivialMethod() {}
 }
 
