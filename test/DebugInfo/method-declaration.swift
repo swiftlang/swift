7c19ca2fbc0c804ea15d47d7249e60acd4a47e94
Ellis Hoag <ellishoag@meta.com>
2024-08-15 16:35:18 -0700

    [IRGen][DebugInfo] Emit declarations for CFunctionPointer reps

--- test/DebugInfo/method-declaration.swift
+++ test/DebugInfo/method-declaration.swift
@@ -1,4 +1,7 @@
-// RUN: %target-swift-frontend -primary-file %s -emit-ir -gdwarf-types -o - | %FileCheck %s
+// RUN: %empty-directory(%t)
+// RUN: split-file %s %t
+
+// RUN: %target-swift-frontend -primary-file %t/a.swift -import-objc-header %t/objc.h -enable-objc-interop -emit-ir -gdwarf-types -o - | %FileCheck %s
 
 // Verify that we added a declaration for a method.
 
@@ -8,6 +11,7 @@
 // CHECK: define{{.*}}bar_method{{.*}} !dbg ![[BAR_METHOD_DEF_DBG:[0-9]+]]
 // CHECK: define{{.*}}a_function{{.*}} !dbg ![[FUNC_DEF_DBG:[0-9]+]]
 
+//--- a.swift
 // CHECK-DAG: ![[FOO_DBG:[0-9]+]] = !DICompositeType(tag: {{.*}} name: "Foo", {{.*}} identifier:
 public struct Foo {
 // CHECK-DAG: ![[FOO_STATIC_METHOD_DEF_DBG]] = distinct !DISubprogram(name: "foo_static_method"{{.*}}, scope: ![[FOO_DBG]]{{.*}}DISPFlagDefinition{{.*}}, declaration: ![[FOO_STATIC_METHOD_DECL_DBG:[0-9]+]]
@@ -16,6 +20,13 @@ public struct Foo {
 // CHECK-DAG: ![[FOO_METHOD_DEF_DBG]] = distinct !DISubprogram(name: "foo_method"{{.*}}, scope: ![[FOO_DBG]]{{.*}}DISPFlagDefinition{{.*}}, declaration: ![[FOO_METHOD_DECL_DBG:[0-9]+]]
 // CHECK-DAG: ![[FOO_METHOD_DECL_DBG]] = !DISubprogram(name: "foo_method"{{.*}}, scope: ![[FOO_DBG]]
     func foo_method() {}
+
+// CHECK-DAG: !DISubprogram(linkageName: "$s1a3FooVACycfcyycfU_To"
+// CHECK-SAME: scope: ![[FOO_DBG]]
+// CHECK-SAME: declaration: !
+    init() {
+        let _ = ObjCGoo(myVal:{})
+    }
 }
 
 // CHECK-DAG: ![[BAR_DBG:[0-9]+]] = !DICompositeType(tag: {{.*}} name: "Bar", {{.*}} identifier:
@@ -35,3 +46,7 @@ public class Bar {
 // CHECK-SAME: )
 func a_function() {}
 
+//--- objc.h
+@interface ObjCGoo
+- (instancetype)initWithMyVal:(void (*)())myVal;
+@end
