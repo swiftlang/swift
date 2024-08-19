ad1acd7f4ac06c1a7409c015a2b934bd6ccba4a5
Allan Shortlidge <shortlidge@apple.com>
2024-08-19 12:17:24 -0700

    Frontend: Allow -enable-experimental-feature to specify upcoming features.
    During the lifecycle of a feature, it may start as an experimental feature and
    then graduate to become an upcoming feature. To preserve compatibility with
    projects that adopted the feature when it was experimental,
    `-enable-experimental-feature` ought to be able to enable upcoming features,
    too.
    
    Since projects may use `-enable-experimental-feature` for compatibility with an
    older toolchain that does not have the feature as an upcoming feature, there is
    no warning when the flag is used to enable an upcoming feature.
    
    Note that if the semantics of a feature change when it graduates from
    experimental to upcoming, then the feature must be renamed so that projects
    using the experimental feature have an opportunity opt-in to the new semantics
    of the upcoming feature.
    
    Resolves rdar://134276783.

--- test/Frontend/upcoming_feature.swift
+++ test/Frontend/upcoming_feature.swift
@@ -12,9 +12,13 @@
 // RUN: %target-swift-frontend -typecheck -enable-upcoming-feature ConciseMagicFile -enable-upcoming-feature UnknownFeature %s
 // RUN: %target-swift-frontend -typecheck -enable-upcoming-feature UnknownFeature -enable-upcoming-feature ConciseMagicFile %s
 
+// For compatibility when a feature graduates, it's fine to refer to an
+// upcoming feature as an experimental feature.
+// RUN: %target-swift-frontend -typecheck -enable-experimental-feature ConciseMagicFile %s
 
 // It's not fine to provide a feature that's in the specified language version.
 // RUN: not %target-swift-frontend -typecheck -enable-upcoming-feature ConciseMagicFile -swift-version 6 %s 2>&1 | %FileCheck %s
+// RUN: not %target-swift-frontend -typecheck -enable-experimental-feature ConciseMagicFile -swift-version 6 %s 2>&1 | %FileCheck %s
 
 // CHECK: error: upcoming feature 'ConciseMagicFile' is already enabled as of Swift version 6
 
