swiftlang/swift

commit cbaef38968b5f5ae5201a3ef4135e8380d1db5d6

[c++-interop] Improve performance of creating a C++ `std::string` from a contiguous UTF-8 `Swift.String`

stdlib/public/Cxx/std/String.swift
@@ -20,25 +20,16 @@ extension std.string {
   /// - Complexity: O(*n*), where *n* is the number of UTF-8 code units in the
   ///   Swift string.
   public init(_ string: String) {
-    self.init()
-    let utf8 = string.utf8
-    self.reserve(utf8.count)
-    for char in utf8 {
-      self.push_back(value_type(bitPattern: char))
+    self = string.withCString(encodedAs: UTF8.self) { buffer in
+      std.string(buffer, string.utf8.count, .init())
     }
   }
 
   public init(_ string: UnsafePointer<CChar>?) {
-    self.init()
-
-    guard let str = string else {
-      return
-    }
-
-    let len = UTF8._nullCodeUnitOffset(in: str)
-    for i in 0..<len {
-      let char = UInt8(str[i])
-      self.push_back(value_type(bitPattern: char))
+    if let str = string {
+      self.init(str, UTF8._nullCodeUnitOffset(in: str), .init())
+    } else {
+      self.init()
     }
   }
 }

commit c8fa166e5879eca200f2d6fddb7a5f858406ae29

[cxx][windows] fix the CxxStdlib build for recent MSVC versions after recent regression
The build regressed after cbaef38968b5f5ae5201a3ef4135e8380d1db5d6 as newer versions of MSVC that aren't used in the OSS CI yet fail to build CxxStdlib now

stdlib/public/Cxx/std/String.swift
@@ -21,13 +21,27 @@ extension std.string {
   ///   Swift string.
   public init(_ string: String) {
     self = string.withCString(encodedAs: UTF8.self) { buffer in
+#if os(Windows)
+      // Use the 2 parameter constructor.
+      // The MSVC standard library has a enable_if template guard
+      // on the 3 parameter constructor, and thus it's not imported into Swift.
+      std.string(buffer, string.utf8.count)
+#else
       std.string(buffer, string.utf8.count, .init())
+#endif
     }
   }
 
   public init(_ string: UnsafePointer<CChar>?) {
     if let str = string {
+#if os(Windows)
+      // Use the 2 parameter constructor.
+      // The MSVC standard library has a enable_if template guard
+      // on the 3 parameter constructor, and thus it's not imported into Swift.
+      self.init(str, UTF8._nullCodeUnitOffset(in: str))
+#else
       self.init(str, UTF8._nullCodeUnitOffset(in: str), .init())
+#endif
     } else {
       self.init()
     }