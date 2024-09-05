78903412fe69ffdae5a1880a1aad21a67ddc25b6
Amritpan Kaur <amritpankaur@gmail.com>
2024-09-05 12:33:32 -0700

    [Expr/AST] Add apply component to handle resolved method arguments.

--- lib/AST/ASTDumper.cpp
+++ lib/AST/ASTDumper.cpp
@@ -4322,6 +4322,9 @@ public:
           case KeyPathExpr::Component::Kind::UnresolvedApply:
             printHead("unresolved_apply", ASTNodeColor, label);
             break;
+          case KeyPathExpr::Component::Kind::Apply:
+            printHead("apply", ASTNodeColor, label);
+            break;
           }
           printTypeField(GetTypeOfKeyPathComponent(E, i), Label::always("type"));
           if (auto *args = component.getArgs()) {
