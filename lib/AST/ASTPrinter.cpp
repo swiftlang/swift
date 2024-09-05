26b697e96abc69e675c772fcb6ac747588119192
Amritpan Kaur <amritpankaur@gmail.com>
2024-09-04 18:17:55 -0700

    [Expr/AST] Add unresolvedApply component to handle method arguments.

--- lib/AST/ASTPrinter.cpp
+++ lib/AST/ASTPrinter.cpp
@@ -5070,6 +5070,11 @@ void PrintAST::printKeyPathComponents(KeyPathExpr *expr, ArrayRef<KeyPathExpr::C
         case ComponentKind::CodeCompletion: {
           break;
         }
+        case ComponentKind::UnresolvedApply: {
+          auto args = component.getArgs();
+          printArgumentList(args, /*forSubscript*/ false);
+          break;
+        }
       }
     }
   } else {
