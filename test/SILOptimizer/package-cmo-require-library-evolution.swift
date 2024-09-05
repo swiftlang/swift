98595fcb74b90ca02648b6244fd23d7a8d629742
elsh <ellie_shin@apple.com>
2024-09-05 16:03:06 -0700

    s/warning/error Update tests

--- test/SILOptimizer/package-cmo-require-library-evolution.swift
+++ test/SILOptimizer/package-cmo-require-library-evolution.swift
@@ -1,17 +1,12 @@
 // RUN: %empty-directory(%t)
 
-// RUN: %target-build-swift %s \
+// RUN: not %target-build-swift %s \
 // RUN: -module-name=Lib -package-name Pkg \
 // RUN: -emit-module -o %t/Lib.swiftmodule -I%t \
 // RUN: -Xfrontend -package-cmo \
 // RUN: -Xfrontend -allow-non-resilient-access \
-// RUN: -O -wmo 2>&1 | %FileCheck %s --check-prefix CHECK-WARNING
-// CHECK-WARNING: warning: Library evolution must be enabled for Package CMO
-
-// RUN: llvm-bcanalyzer %t/Lib.swiftmodule | %FileCheck %s -check-prefix=CHECK-BC
-// CHECK-BC-NOT: SERIALIZE_PACKAGE_ENABLED
-
-// RUN: rm -rf %t/Lib.swiftmodule
+// RUN: -O -wmo 2>&1 | %FileCheck %s --check-prefix CHECK-DIAGS
+// CHECK-DIAGS: error: Library evolution must be enabled for Package CMO
 
 // RUN: %target-build-swift %s \
 // RUN: -module-name=Lib -package-name Pkg \
@@ -21,8 +16,8 @@
 // RUN: -enable-library-evolution \
 // RUN: -O -wmo
 
-// RUN: llvm-bcanalyzer %t/Lib.swiftmodule | %FileCheck %s -check-prefix=CHECK-OK
-// CHECK-OK: SERIALIZE_PACKAGE_ENABLED
+// RUN: llvm-bcanalyzer %t/Lib.swiftmodule | %FileCheck %s -check-prefix=CHECK-BC
+// CHECK-BC: SERIALIZE_PACKAGE_ENABLED
 
 // REQUIRES: swift_in_compiler
 
