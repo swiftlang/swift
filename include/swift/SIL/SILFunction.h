a0088327d455bbea32a41c9c5bbd38bc89d61a0d
Michael Gottesman <mgottesman@apple.com>
2024-09-11 16:04:37 -0700

    [concurrency] Represent a SILFunction without isolation as std::optional<ActorIsolation> instead of ActorIsolation::Unspecified.
    The reason why is that we want to distinguish inbetween SILFunction's that are
    marked as unspecified by SILGen and those that are parsed from textual SIL that
    do not have any specified isolation. This will make it easier to write nice
    FileCheck tests against SILGen output on what is the inferred isolation for
    various items.
    
    NFCI.

--- include/swift/SIL/SILFunction.h
+++ include/swift/SIL/SILFunction.h
@@ -353,7 +353,7 @@ private:
   unsigned BlockListChangeIdx = 0;
 
   /// The isolation of this function.
-  ActorIsolation actorIsolation = ActorIsolation::forUnspecified();
+  std::optional<ActorIsolation> actorIsolation;
 
   /// The function's bare attribute. Bare means that the function is SIL-only
   /// and does not require debug info.
@@ -1453,7 +1453,9 @@ public:
     actorIsolation = newActorIsolation;
   }
 
-  ActorIsolation getActorIsolation() const { return actorIsolation; }
+  std::optional<ActorIsolation> getActorIsolation() const {
+    return actorIsolation;
+  }
 
   /// Return the source file that this SILFunction belongs to if it exists.
   SourceFile *getSourceFile() const;
