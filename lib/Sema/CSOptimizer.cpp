6f8379eb3a20bb471e64d64ed3fea93d8dd8b4bb
Pavel Yaskevich <pyaskevich@apple.com>
2024-09-11 14:57:44 -0700

    [CSOptimizer] Average score should reflect number of defaulted parameters

--- lib/Sema/CSOptimizer.cpp
+++ lib/Sema/CSOptimizer.cpp
@@ -441,6 +441,7 @@ static Constraint *determineBestChoicesInContext(
           }
 
           double score = 0.0;
+          unsigned numDefaulted = 0;
           for (unsigned paramIdx = 0, n = overloadType->getNumParams();
                paramIdx != n; ++paramIdx) {
             const auto &param = overloadType->getParams()[paramIdx];
@@ -448,7 +449,8 @@ static Constraint *determineBestChoicesInContext(
             auto argIndices = matchings->parameterBindings[paramIdx];
             switch (argIndices.size()) {
             case 0:
-              // Current parameter is defaulted.
+              // Current parameter is defaulted, mark and continue.
+              ++numDefaulted;
               continue;
 
             case 1:
@@ -542,9 +544,14 @@ static Constraint *determineBestChoicesInContext(
             score += bestCandidateScore;
           }
 
+          // An overload whether all of the parameters are defaulted
+          // that's called without arguments.
+          if (numDefaulted == overloadType->getNumParams())
+            return;
+
           // Average the score to avoid disfavoring disjunctions with fewer
           // parameters.
-          score /= overloadType->getNumParams();
+          score /= (overloadType->getNumParams() - numDefaulted);
 
           // If one of the result types matches exactly, that's a good
           // indication that overload choice should be favored.
