f9b143b67340369f464dd8ba1788d27b353c3da6
Akira Hatanaka <ahatanaka@apple.com>
2024-09-11 18:25:38 -0700

    Relax check strings
    rdar://135165028

--- test/Interop/Cxx/class/inheritance/functions-objc-irgen.swift
+++ test/Interop/Cxx/class/inheritance/functions-objc-irgen.swift
@@ -1,25 +1,25 @@
-// RUN: %target-swift-emit-irgen %s -I %S/Inputs -cxx-interoperability-mode=default -Xcc -fno-exceptions -Xcc -fno-objc-exceptions | %FileCheck --dump-input-filter=all %s
+// RUN: %target-swift-emit-irgen %s -I %S/Inputs -cxx-interoperability-mode=default -Xcc -fno-exceptions -Xcc -fno-objc-exceptions | %FileCheck %s
 // REQUIRES: objc_interop
 
 // UNSUPPORTED: CPU=arm64e
 
 import FunctionsObjC
 
-// CHECK: define linkonce_odr noundef ptr @_ZNK7Derived36__synthesizedBaseCall_virtual_methodEv(ptr noundef nonnull align {{4|8}} dereferenceable({{8|12}}) %[[THIS:.*]])
+// CHECK: define linkonce_odr {{.*}}ptr @_ZNK7Derived36__synthesizedBaseCall_virtual_methodEv(ptr {{.*}}%[[THIS:.*]])
 // CHECK: %[[THIS_ADDR:.*]] = alloca ptr,
 // CHECK: store ptr %[[THIS]], ptr %[[THIS_ADDR]],
 // CHECK: %[[THIS1:.*]] = load ptr, ptr %[[THIS_ADDR]],
 // CHECK: %[[VTABLE:.*]] = load ptr, ptr %[[THIS1]],
 // CHECK: %[[VFN:.*]] = getelementptr inbounds ptr, ptr %[[VTABLE]], i64 0
 // CHECK: %[[V0:.*]] = load ptr, ptr %[[VFN]],
-// CHECK: %[[CALL:.*]] = call noundef ptr %[[V0]](ptr noundef nonnull align {{4|8}} dereferenceable({{8|12}}) %[[THIS1]])
+// CHECK: %[[CALL:.*]] = call {{.*}}ptr %[[V0]](ptr {{.*}}%[[THIS1]])
 // CHECK: ret ptr %[[CALL]]
 
-// CHECK: define linkonce_odr noundef ptr @_ZNK7Derived40__synthesizedBaseCall_non_virtual_methodEv(ptr noundef nonnull align {{4|8}} dereferenceable({{8|12}}) %[[THIS:.*]])
+// CHECK: define linkonce_odr {{.*}}ptr @_ZNK7Derived40__synthesizedBaseCall_non_virtual_methodEv(ptr {{.*}}%[[THIS:.*]])
 // CHECK: %[[THIS_ADDR:.*]] = alloca ptr,
 // CHECK: store ptr %[[THIS]], ptr %[[THIS_ADDR]],
 // CHECK: %[[THIS1:.*]] = load ptr, ptr %[[THIS_ADDR]],
-// CHECK: %[[CALL:.*]] = call noundef ptr @_ZNK4Base18non_virtual_methodEv(ptr noundef nonnull align {{4|8}} dereferenceable({{8|12}}) %[[THIS1]])
+// CHECK: %[[CALL:.*]] = call {{.*}}ptr @_ZNK4Base18non_virtual_methodEv(ptr {{.*}}%[[THIS1]])
 // CHECK: ret ptr %[[CALL]]
 
 func testBaseMethodCall() -> C {
