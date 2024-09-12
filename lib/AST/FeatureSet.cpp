929c0ca7d8287f9ea88bbdde0e73972b7abc3de7
Gabor Horvath <gaborh@apple.com>
2024-09-12 14:01:13 +0100

    [cxx-interop] Introduce a safe C++ interop mode
    In this mode all C++ types are imported as unsafe by default. Users
    explicitly marking types are escapable or not escapable can make them
    imported as safe. In the future, we also want to import unannotated
    functions as unsafe and add more logic to infer types that are actually
    safe, like agregates of escapable types.

--- lib/AST/FeatureSet.cpp
+++ lib/AST/FeatureSet.cpp
@@ -300,6 +300,7 @@ static bool usesFeatureAllowUnsafeAttribute(Decl *decl) {
 }
 
 UNINTERESTING_FEATURE(WarnUnsafe)
+UNINTERESTING_FEATURE(SafeInterop)
 
 static bool usesFeatureValueGenerics(Decl *decl) {
   auto genericContext = decl->getAsGenericContext();
