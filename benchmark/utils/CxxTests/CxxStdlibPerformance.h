b3d6e38ab16215b66739aca87063deffe56f40cd
smonteiro2 <s.moreno.monteiro@tecnico.ulisboa.pt>
2024-09-13 16:24:20 +0100

    [cxx-interop] Benchmarks for std::span in Swift
    * std::span is not supported in swift-ci linux
    * check in Swift that C++ >= 20

--- benchmark/utils/CxxTests/CxxStdlibPerformance.h
+++ benchmark/utils/CxxTests/CxxStdlibPerformance.h
@@ -4,11 +4,29 @@
 #include <vector>
 #include <set>
 
+// FIXME swift-ci linux tests do not support std::span
+#if defined(__cplusplus) && __cplusplus >= 202002L
+#include <span>
+#define SPAN_BENCHMARKS 1
+#else
+#define SPAN_BENCHMARKS 0
+#endif
+
+static const size_t spanSize = 50000;
+
 using VectorOfU32 = std::vector<uint32_t>;
 using SetOfU32 = std::set<uint32_t>;
+#if SPAN_BENCHMARKS
+using ArrayOfU32 = uint32_t[spanSize];
+using SpanOfU32 = std::span<uint32_t>;
+#endif
 
 static inline VectorOfU32 vec;
 static inline SetOfU32 set;
+#if SPAN_BENCHMARKS
+static inline ArrayOfU32 array;
+static inline SpanOfU32 span;
+#endif
 
 inline void initVector(size_t size) {
     if (!vec.empty()) {
@@ -28,6 +46,18 @@ inline void initSet(size_t size) {
         set.insert(uint32_t(i));
     }
 }
+
+#if SPAN_BENCHMARKS
+inline void initSpan() {
+    if (!span.empty()) {
+        return;
+    }
+    for (size_t i = 0; i < spanSize; ++i) {
+        array[i] = uint32_t(i);
+    }
+    span = SpanOfU32(array);
+}
+#endif
 
 inline VectorOfU32 makeVector32(size_t size) {
     initVector(size);
