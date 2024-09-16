4e1af3b6b7478be077629dbda3b463d7e37fb831
Doug Gregor <dgregor@apple.com>
2024-09-16 14:53:25 -0700

    Test printing of declarations created by macros on imported decls

--- test/Inputs/clang-importer-sdk/usr/include/completion_handler_globals.h
+++ test/Inputs/clang-importer-sdk/usr/include/completion_handler_globals.h
@@ -1,2 +1,2 @@
 void async_divide(double x, double y, void (* _Nonnull completionHandler)(double x))
-  __attribute__((swift_attr("@ModuleUser.AddAsync")));
+  __attribute__((swift_attr("@macro_library.AddAsync")));
