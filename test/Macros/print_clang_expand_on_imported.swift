70980b95f8e78a90f44f2436e63a530dd7c4be4d
Doug Gregor <dgregor@apple.com>
2024-09-16 16:30:00 -0700

    Add experimental feature MacrosOnImporters for applying macro to imported decl
    This is still very much experimental and we don't want to accidentally
    enable it everywhere.

--- test/Macros/print_clang_expand_on_imported.swift
+++ test/Macros/print_clang_expand_on_imported.swift
@@ -6,7 +6,7 @@
 // Build the macro library to give us access to AddAsync.
 // RUN: %target-swift-frontend -swift-version 5 -emit-module -o %t/macro_library.swiftmodule %S/Inputs/macro_library.swift -module-name macro_library -load-plugin-library %t/%target-library-name(MacroDefinition)
 
-// RUN: %target-swift-ide-test(mock-sdk: %clang-importer-sdk) -print-module -print-implicit-attrs -source-filename %s -module-to-print=CompletionHandlerGlobals -I %t -function-definitions=false -load-plugin-library %t/%target-library-name(MacroDefinition) -import-module macro_library > %t/imported.printed.txt
+// RUN: %target-swift-ide-test(mock-sdk: %clang-importer-sdk) -print-module -print-implicit-attrs -source-filename %s -module-to-print=CompletionHandlerGlobals -I %t -function-definitions=false -load-plugin-library %t/%target-library-name(MacroDefinition) -import-module macro_library -enable-experimental-feature MacrosOnImports > %t/imported.printed.txt
 // RUN: %FileCheck -input-file %t/imported.printed.txt %s
 
 import CompletionHandlerGlobals
