a5e26fdb124880e068ed553770331737b554715c
Alejandro Alonso <alejandro_alonso@apple.com>
2024-09-18 10:30:10 -0700

    Restrict parsing integer types in certain contexts

--- include/swift/Parse/Parser.h
+++ include/swift/Parse/Parser.h
@@ -1435,6 +1435,11 @@ public:
   ParserResult<TypeRepr> parseTypeSimple(
       Diag<> MessageID, ParseTypeReason reason);
 
+  ParserResult<TypeRepr> parseTypeOrValue();
+  ParserResult<TypeRepr> parseTypeOrValue(Diag<> MessageID,
+                          ParseTypeReason reason = ParseTypeReason::Unspecified,
+                          bool fromASTGen = false);
+
   /// Parse layout constraint.
   LayoutConstraint parseLayoutConstraint(Identifier LayoutConstraintID);
 
