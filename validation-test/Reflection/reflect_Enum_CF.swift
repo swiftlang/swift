e62560c9ca6cbffc6ac098f58f41711520d4b3bd
Tim Kientzle <tkientzle@apple.com>
2024-09-19 14:04:11 -0700

    Limit this part to only 64-bit targets

--- validation-test/Reflection/reflect_Enum_CF.swift
+++ validation-test/Reflection/reflect_Enum_CF.swift
@@ -62,11 +62,11 @@ reflect(enum: StructA().field1)
 // CHECK-NEXT:   (foreign name=So11CFNumberRefa))
 
 // CHECK: Type info:
-// CHECK-NEXT: (multi_payload_enum size=9 alignment=8 stride=16 num_extra_inhabitants=254 bitwise_takable=1
-// CHECK-NEXT:   (case name=a index=0 offset=0
-// CHECK-NEXT:     (tuple size=0 alignment=1 stride=1 num_extra_inhabitants=0 bitwise_takable=1))
-// CHECK-NEXT:   (case name=b index=1 offset=0
-// CHECK-NEXT:     (reference kind=strong refcounting=unknown)))
+// CHECK64-NEXT: (multi_payload_enum size=9 alignment=8 stride=16 num_extra_inhabitants=254 bitwise_takable=1
+// CHECK64-NEXT:   (case name=a index=0 offset=0
+// CHECK64-NEXT:     (tuple size=0 alignment=1 stride=1 num_extra_inhabitants=0 bitwise_takable=1))
+// CHECK64-NEXT:   (case name=b index=1 offset=0
+// CHECK64-NEXT:     (reference kind=strong refcounting=unknown)))
 
 // CHECK: Mangled name: $s15reflect_Enum_CF6MyPairOyytSo11CFNumberRefaG
 // CHECK-NEXT: Demangled name: reflect_Enum_CF.MyPair<(), __C.CFNumberRef>
@@ -84,17 +84,17 @@ reflect(enum: T.a)
 // CHECK-NEXT: (enum reflect_Enum_CF.T)
 
 // CHECK: Type info:
-// CHECK-NEXT: (single_payload_enum size=9 alignment=8 stride=16 num_extra_inhabitants=253 bitwise_takable=1
-// CHECK-NEXT:   (case name=b index=0 offset=0
-// CHECK-NEXT:     (multi_payload_enum size=9 alignment=8 stride=16 num_extra_inhabitants=254 bitwise_takable=1
-// CHECK-NEXT:       (case name=a index=0 offset=0
-// CHECK-NEXT:         (tuple size=0 alignment=1 stride=1 num_extra_inhabitants=0 bitwise_takable=1))
-// CHECK-NEXT:       (case name=b index=1 offset=0
-// CHECK-NEXT:         (reference kind=strong refcounting=unknown))))
-// CHECK-NEXT:   (case name=a index=1))
-// CHECK-NEXT: Mangled name: $s15reflect_Enum_CF1TO
-// CHECK-NEXT: Demangled name: reflect_Enum_CF.T
+// CHECK64-NEXT: (single_payload_enum size=9 alignment=8 stride=16 num_extra_inhabitants=253 bitwise_takable=1
+// CHECK64-NEXT:   (case name=b index=0 offset=0
+// CHECK64-NEXT:     (multi_payload_enum size=9 alignment=8 stride=16 num_extra_inhabitants=254 bitwise_takable=1
+// CHECK64-NEXT:       (case name=a index=0 offset=0
+// CHECK64-NEXT:         (tuple size=0 alignment=1 stride=1 num_extra_inhabitants=0 bitwise_takable=1))
+// CHECK64-NEXT:       (case name=b index=1 offset=0
+// CHECK64-NEXT:         (reference kind=strong refcounting=unknown))))
+// CHECK64-NEXT:   (case name=a index=1))
 
+// CHECK: Mangled name: $s15reflect_Enum_CF1TO
+// CHECK-NEXT: Demangled name: reflect_Enum_CF.T
 
 // CHECK: Enum value:
 // CHECK-NEXT: (enum_value name=a index=1)
