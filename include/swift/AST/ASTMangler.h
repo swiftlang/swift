77ed0146905165f152b94155ae798514a371b581
Doug Gregor <dgregor@apple.com>
2024-09-19 16:34:27 -0700

    Remove unused ASTMangler::mangleGlobalGetterEntity

--- include/swift/AST/ASTMangler.h
+++ include/swift/AST/ASTMangler.h
@@ -217,9 +217,6 @@ public:
                                    bool isStatic,
                                    SymbolKind SKind);
 
-  std::string mangleGlobalGetterEntity(const ValueDecl *decl,
-                                       SymbolKind SKind = SymbolKind::Default);
-
   std::string mangleDefaultArgumentEntity(const DeclContext *func,
                                           unsigned index,
                                           SymbolKind SKind = SymbolKind::Default);
