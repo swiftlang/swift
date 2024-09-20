cf6a16ab2b822ced432af5382f310a1b48198c34
Dario Rexin <drexin@apple.com>
2024-09-19 23:51:54 -0700

    [IRGen] Add synchronous typed throws implementation to typed_throws_abi.swift test
    Adding some tests cases for direct return of typed errors in synchronous functions.

--- test/IRGen/typed_throws_abi.swift
+++ test/IRGen/typed_throws_abi.swift
@@ -8,6 +8,239 @@ struct OneWord: Error {
     let x = 1
 }
 
+struct Impl: P {
+    // CHECK: define hidden swiftcc void @"$s16typed_throws_abi4ImplV2f0yySbAA5EmptyVYKF"(i1 %0, ptr swiftself %1, ptr noalias nocapture swifterror dereferenceable(8) %2)
+    // CHECK:   br i1 %0, label %[[SUCCESS:.*]], label %[[FAIL:.*]]
+    // CHECK: [[SUCCESS]]:
+    // CHECK:   ret void
+    // CHECK: [[FAIL]]:
+    // CHECK:   call swiftcc void @"$s16typed_throws_abi5EmptyVACycfC"()
+    // CHECK:   [[ERROR_WITNESS:%.*]] = call ptr @"$s16typed_throws_abi5EmptyVACs5ErrorAAWl"() #12
+    // CHECK:   call swiftcc void @swift_willThrowTyped(ptr noalias undef, ptr getelementptr inbounds (<{ ptr, ptr, i64, ptr }>, ptr @"$s16typed_throws_abi5EmptyVMf", i32 0, i32 2), ptr [[ERROR_WITNESS]])
+    // CHECK:   store ptr inttoptr (i64 1 to ptr), ptr %2, align 8
+    // CHECK:   ret void
+    // CHECK: }
+    func f0(_ b: Bool) throws(Empty) {
+        guard b else {
+            throw Empty()
+        }
+    }
+
+    // CHECK: define hidden swiftcc i64 @"$s16typed_throws_abi4ImplV2f1ySiSbAA5EmptyVYKF"(i1 %0, ptr swiftself %1, ptr noalias nocapture swifterror dereferenceable(8) %2)
+    // CHECK:   br i1 %0, label %[[SUCCESS:.*]], label %[[FAIL:.*]]
+    // CHECK: [[SUCCESS]]:
+    // CHECK:   ret i64 1
+    // CHECK: [[FAIL]]:
+    // CHECK:   call swiftcc void @"$s16typed_throws_abi5EmptyVACycfC"()
+    // CHECK:   [[ERROR_WITNESS:%.*]] = call ptr @"$s16typed_throws_abi5EmptyVACs5ErrorAAWl"() #12
+    // CHECK:   call swiftcc void @swift_willThrowTyped(ptr noalias undef, ptr getelementptr inbounds (<{ ptr, ptr, i64, ptr }>, ptr @"$s16typed_throws_abi5EmptyVMf", i32 0, i32 2), ptr [[ERROR_WITNESS]])
+    // CHECK:   store ptr inttoptr (i64 1 to ptr), ptr %2, align 8
+    // CHECK:   ret i64 undef
+    // CHECK: }
+    func f1(_ b: Bool) throws(Empty) -> Int {
+        guard b else {
+            throw Empty()
+        }
+        return 1
+    }
+
+    // CHECK: define hidden swiftcc { i64, i64 } @"$s16typed_throws_abi4ImplV2f2ySi_SitSbAA5EmptyVYKF"(i1 %0, ptr swiftself %1, ptr noalias nocapture swifterror dereferenceable(8) %2)
+    // CHECK:   br i1 %0, label %[[SUCCESS:.*]], label %[[FAIL:.*]]
+    // CHECK: [[SUCCESS]]:
+    // CHECK:   ret { i64, i64 } { i64 1, i64 2 }
+    // CHECK: [[FAIL]]:
+    // CHECK:   call swiftcc void @"$s16typed_throws_abi5EmptyVACycfC"()
+    // CHECK:   [[ERROR_WITNESS:%.*]] = call ptr @"$s16typed_throws_abi5EmptyVACs5ErrorAAWl"() #12
+    // CHECK:   call swiftcc void @swift_willThrowTyped(ptr noalias undef, ptr getelementptr inbounds (<{ ptr, ptr, i64, ptr }>, ptr @"$s16typed_throws_abi5EmptyVMf", i32 0, i32 2), ptr [[ERROR_WITNESS]])
+    // CHECK:   store ptr inttoptr (i64 1 to ptr), ptr %2, align 8
+    // CHECK:   ret { i64, i64 } undef
+    // CHECK: }
+    func f2(_ b: Bool) throws(Empty) -> (Int, Int) {
+        guard b else {
+            throw Empty()
+        }
+        return (1, 2)
+    }
+
+    // CHECK: define hidden swiftcc { i64, i64, i64 } @"$s16typed_throws_abi4ImplV2f3ySi_S2itSbAA5EmptyVYKF"(i1 %0, ptr swiftself %1, ptr noalias nocapture swifterror dereferenceable(8) %2)
+    // CHECK:   br i1 %0, label %[[SUCCESS:.*]], label %[[FAIL:.*]]
+    // CHECK: [[SUCCESS]]:
+    // CHECK:   ret { i64, i64, i64 } { i64 1, i64 2, i64 3 }
+    // CHECK: [[FAIL]]:
+    // CHECK:   call swiftcc void @"$s16typed_throws_abi5EmptyVACycfC"()
+    // CHECK:   [[ERROR_WITNESS:%.*]] = call ptr @"$s16typed_throws_abi5EmptyVACs5ErrorAAWl"() #12
+    // CHECK:   call swiftcc void @swift_willThrowTyped(ptr noalias undef, ptr getelementptr inbounds (<{ ptr, ptr, i64, ptr }>, ptr @"$s16typed_throws_abi5EmptyVMf", i32 0, i32 2), ptr [[ERROR_WITNESS]])
+    // CHECK:   store ptr inttoptr (i64 1 to ptr), ptr %2, align 8
+    // CHECK:   ret { i64, i64, i64 } undef
+    // CHECK: }
+    func f3(_ b: Bool) throws(Empty) -> (Int, Int, Int) {
+        guard b else {
+            throw Empty()
+        }
+        return (1, 2, 3)
+    }
+
+    // CHECK: define hidden swiftcc { i64, i64, i64, i64 } @"$s16typed_throws_abi4ImplV2f4ySi_S3itSbAA5EmptyVYKF"(i1 %0, ptr swiftself %1, ptr noalias nocapture swifterror dereferenceable(8) %2)
+    // CHECK:   br i1 %0, label %[[SUCCESS:.*]], label %[[FAIL:.*]]
+    // CHECK: [[SUCCESS]]:
+    // CHECK:   ret { i64, i64, i64, i64 } { i64 1, i64 2, i64 3, i64 4 }
+    // CHECK: [[FAIL]]:
+    // CHECK:   call swiftcc void @"$s16typed_throws_abi5EmptyVACycfC"()
+    // CHECK:   [[ERROR_WITNESS:%.*]] = call ptr @"$s16typed_throws_abi5EmptyVACs5ErrorAAWl"() #12
+    // CHECK:   call swiftcc void @swift_willThrowTyped(ptr noalias undef, ptr getelementptr inbounds (<{ ptr, ptr, i64, ptr }>, ptr @"$s16typed_throws_abi5EmptyVMf", i32 0, i32 2), ptr [[ERROR_WITNESS]])
+    // CHECK:   store ptr inttoptr (i64 1 to ptr), ptr %2, align 8
+    // CHECK:   ret { i64, i64, i64, i64 } undef
+    // CHECK: }
+    func f4(_ b: Bool) throws(Empty) -> (Int, Int, Int, Int) {
+        guard b else {
+            throw Empty()
+        }
+        return (1, 2, 3, 4)
+    }
+
+    // CHECK: define hidden swiftcc void @"$s16typed_throws_abi4ImplV2f5ySi_S4itSbAA5EmptyVYKF"(ptr noalias nocapture sret(<{ %TSi, %TSi, %TSi, %TSi, %TSi }>) %0, i1 %1, ptr swiftself %2, ptr noalias nocapture swifterror dereferenceable(8) %3, ptr %4)
+    // CHECK:   br i1 %1, label %[[SUCCESS:.*]], label %[[FAIL:.*]]
+    // CHECK: [[SUCCESS]]:
+    // CHECK:   store i64 1, ptr %.elt._value, align 8
+    // CHECK:   store i64 2, ptr %.elt1._value, align 8
+    // CHECK:   store i64 3, ptr %.elt2._value, align 8
+    // CHECK:   store i64 4, ptr %.elt3._value, align 8
+    // CHECK:   store i64 5, ptr %.elt4._value, align 8
+    // CHECK:   ret void
+    // CHECK: [[FAIL]]:
+    // CHECK:   call swiftcc void @"$s16typed_throws_abi5EmptyVACycfC"()
+    // CHECK:   [[ERROR_WITNESS:%.*]] = call ptr @"$s16typed_throws_abi5EmptyVACs5ErrorAAWl"() #12
+    // CHECK:   call swiftcc void @swift_willThrowTyped(ptr noalias undef, ptr getelementptr inbounds (<{ ptr, ptr, i64, ptr }>, ptr @"$s16typed_throws_abi5EmptyVMf", i32 0, i32 2), ptr [[ERROR_WITNESS]])
+    // CHECK:   store ptr inttoptr (i64 1 to ptr), ptr %3, align 8
+    // CHECK:   ret void
+    // CHECK: }
+    func f5(_ b: Bool) throws(Empty) -> (Int, Int, Int, Int, Int) {
+        guard b else {
+            throw Empty()
+        }
+        return (1, 2, 3, 4, 5)
+    }
+
+    // CHECK: define hidden swiftcc i64 @"$s16typed_throws_abi4ImplV2g0yySbAA7OneWordVYKF"(i1 %0, ptr swiftself %1, ptr noalias nocapture swifterror dereferenceable(8) %2)
+    // CHECK:   [[ERROR:%.*]] = alloca %T16typed_throws_abi7OneWordV
+    // CHECK:   br i1 %0, label %[[SUCCESS:.*]], label %[[FAIL:.*]]
+    // CHECK: [[SUCCESS]]:
+    // CHECK:   ret i64 undef
+    // CHECK: [[FAIL]]:
+    // CHECK:   call swiftcc i64 @"$s16typed_throws_abi7OneWordVACycfC"()
+    // CHECK:   [[ERROR_WITNESS:%.*]] = call ptr @"$s16typed_throws_abi7OneWordVACs5ErrorAAWl"()
+    // CHECK:   call swiftcc void @swift_willThrowTyped(ptr noalias [[ERROR]], ptr getelementptr inbounds (<{ ptr, ptr, i64, ptr, i32, [4 x i8] }>, ptr @"$s16typed_throws_abi7OneWordVMf", i32 0, i32 2), ptr [[ERROR_WITNESS]])
+    // CHECK:   store ptr inttoptr (i64 1 to ptr), ptr %2, align 8
+    // CHECK:   ret i64 {{%.*}}
+    // CHECK: }
+    func g0(_ b: Bool) throws(OneWord) {
+        guard b else {
+            throw OneWord()
+        }
+    }
+
+    // CHECK: define hidden swiftcc i64 @"$s16typed_throws_abi4ImplV2g1ySiSbAA7OneWordVYKF"(i1 %0, ptr swiftself %1, ptr noalias nocapture swifterror dereferenceable(8) %2)
+    // CHECK:   [[ERROR:%.*]] = alloca %T16typed_throws_abi7OneWordV
+    // CHECK:   br i1 %0, label %[[SUCCESS:.*]], label %[[FAIL:.*]]
+    // CHECK: [[SUCCESS]]:
+    // CHECK:   ret i64 1
+    // CHECK: [[FAIL]]:
+    // CHECK:   call swiftcc i64 @"$s16typed_throws_abi7OneWordVACycfC"()
+    // CHECK:   [[ERROR_WITNESS:%.*]] = call ptr @"$s16typed_throws_abi7OneWordVACs5ErrorAAWl"()
+    // CHECK:   call swiftcc void @swift_willThrowTyped(ptr noalias [[ERROR]], ptr getelementptr inbounds (<{ ptr, ptr, i64, ptr, i32, [4 x i8] }>, ptr @"$s16typed_throws_abi7OneWordVMf", i32 0, i32 2), ptr [[ERROR_WITNESS]])
+    // CHECK:   store ptr inttoptr (i64 1 to ptr), ptr %2, align 8
+    // CHECK:   ret i64 {{%.*}}
+    // CHECK: }
+    func g1(_ b: Bool) throws(OneWord) -> Int {
+        guard b else {
+            throw OneWord()
+        }
+        return 1
+    }
+
+    // CHECK: define hidden swiftcc { i64, i64 } @"$s16typed_throws_abi4ImplV2g2ySi_SitSbAA7OneWordVYKF"(i1 %0, ptr swiftself %1, ptr noalias nocapture swifterror dereferenceable(8) %2)
+    // CHECK:   [[ERROR:%.*]] = alloca %T16typed_throws_abi7OneWordV
+    // CHECK:   br i1 %0, label %[[SUCCESS:.*]], label %[[FAIL:.*]]
+    // CHECK: [[SUCCESS]]:
+    // CHECK:   ret { i64, i64 } { i64 1, i64 2 }
+    // CHECK: [[FAIL]]:
+    // CHECK:   call swiftcc i64 @"$s16typed_throws_abi7OneWordVACycfC"()
+    // CHECK:   [[ERROR_WITNESS:%.*]] = call ptr @"$s16typed_throws_abi7OneWordVACs5ErrorAAWl"()
+    // CHECK:   call swiftcc void @swift_willThrowTyped(ptr noalias [[ERROR]], ptr getelementptr inbounds (<{ ptr, ptr, i64, ptr, i32, [4 x i8] }>, ptr @"$s16typed_throws_abi7OneWordVMf", i32 0, i32 2), ptr [[ERROR_WITNESS]])
+    // CHECK:   store ptr inttoptr (i64 1 to ptr), ptr %2, align 8
+    // CHECK:   ret { i64, i64 } {{%.*}}
+    // CHECK: }
+    func g2(_ b: Bool) throws(OneWord) -> (Int, Int) {
+        guard b else {
+            throw OneWord()
+        }
+        return (1, 2)
+    }
+
+    // CHECK: define hidden swiftcc { i64, i64, i64 } @"$s16typed_throws_abi4ImplV2g3ySi_S2itSbAA7OneWordVYKF"(i1 %0, ptr swiftself %1, ptr noalias nocapture swifterror dereferenceable(8) %2)
+    // CHECK:   [[ERROR:%.*]] = alloca %T16typed_throws_abi7OneWordV
+    // CHECK:   br i1 %0, label %[[SUCCESS:.*]], label %[[FAIL:.*]]
+    // CHECK: [[SUCCESS]]:
+    // CHECK:   ret { i64, i64, i64 } { i64 1, i64 2, i64 3 }
+    // CHECK: [[FAIL]]:
+    // CHECK:   call swiftcc i64 @"$s16typed_throws_abi7OneWordVACycfC"()
+    // CHECK:   [[ERROR_WITNESS:%.*]] = call ptr @"$s16typed_throws_abi7OneWordVACs5ErrorAAWl"()
+    // CHECK:   call swiftcc void @swift_willThrowTyped(ptr noalias [[ERROR]], ptr getelementptr inbounds (<{ ptr, ptr, i64, ptr, i32, [4 x i8] }>, ptr @"$s16typed_throws_abi7OneWordVMf", i32 0, i32 2), ptr [[ERROR_WITNESS]])
+    // CHECK:   store ptr inttoptr (i64 1 to ptr), ptr %2, align 8
+    // CHECK:   ret { i64, i64, i64 } {{%.*}}
+    // CHECK: }
+    func g3(_ b: Bool) throws(OneWord) -> (Int, Int, Int) {
+        guard b else {
+            throw OneWord()
+        }
+        return (1, 2, 3)
+    }
+
+    // CHECK: define hidden swiftcc { i64, i64, i64, i64 } @"$s16typed_throws_abi4ImplV2g4ySi_S3itSbAA7OneWordVYKF"(i1 %0, ptr swiftself %1, ptr noalias nocapture swifterror dereferenceable(8) %2)
+    // CHECK:   [[ERROR:%.*]] = alloca %T16typed_throws_abi7OneWordV
+    // CHECK:   br i1 %0, label %[[SUCCESS:.*]], label %[[FAIL:.*]]
+    // CHECK: [[SUCCESS]]:
+    // CHECK:   ret { i64, i64, i64, i64 } { i64 1, i64 2, i64 3, i64 4 }
+    // CHECK: [[FAIL]]:
+    // CHECK:   call swiftcc i64 @"$s16typed_throws_abi7OneWordVACycfC"()
+    // CHECK:   [[ERROR_WITNESS:%.*]] = call ptr @"$s16typed_throws_abi7OneWordVACs5ErrorAAWl"()
+    // CHECK:   call swiftcc void @swift_willThrowTyped(ptr noalias [[ERROR]], ptr getelementptr inbounds (<{ ptr, ptr, i64, ptr, i32, [4 x i8] }>, ptr @"$s16typed_throws_abi7OneWordVMf", i32 0, i32 2), ptr [[ERROR_WITNESS]])
+    // CHECK:   store ptr inttoptr (i64 1 to ptr), ptr %2, align 8
+    // CHECK:   ret { i64, i64, i64, i64 } {{%.*}}
+    // CHECK: }
+    func g4(_ b: Bool) throws(OneWord) -> (Int, Int, Int, Int) {
+        guard b else {
+            throw OneWord()
+        }
+        return (1, 2, 3, 4)
+    }
+
+    // CHECK: define hidden swiftcc void @"$s16typed_throws_abi4ImplV2g5ySi_S4itSbAA7OneWordVYKF"(ptr noalias nocapture sret(<{ %TSi, %TSi, %TSi, %TSi, %TSi }>) %0, i1 %1, ptr swiftself %2, ptr noalias nocapture swifterror dereferenceable(8) %3, ptr %4) #0 {
+    // CHECK: entry:
+    // CHECK:   [[ERROR:%.*]] = alloca %T16typed_throws_abi7OneWordV, align 8
+    // CHECK:   br i1 %1, label %[[SUCCESS:.*]], label %[[FAIL:.*]]
+    // CHECK: [[SUCCESS]]:
+    // CHECK:   store i64 1, ptr {{%.*}}, align 8
+    // CHECK:   store i64 2, ptr {{%.*}}, align 8
+    // CHECK:   store i64 3, ptr {{%.*}}, align 8
+    // CHECK:   store i64 4, ptr {{%.*}}, align 8
+    // CHECK:   store i64 5, ptr {{%.*}}, align 8
+    // CHECK:   ret void
+    // CHECK: [[FAIL]]:
+    // CHECK:   call swiftcc i64 @"$s16typed_throws_abi7OneWordVACycfC"()
+    // CHECK:   [[ERROR_WITNESS:%.*]] = call ptr @"$s16typed_throws_abi7OneWordVACs5ErrorAAWl"()
+    // CHECK:   call swiftcc void @swift_willThrowTyped(ptr noalias [[ERROR]], ptr getelementptr inbounds (<{ ptr, ptr, i64, ptr, i32, [4 x i8] }>, ptr @"$s16typed_throws_abi7OneWordVMf", i32 0, i32 2), ptr [[ERROR_WITNESS]])
+    // CHECK:   store ptr inttoptr (i64 1 to ptr), ptr %3, align 8
+    // CHECK:   ret void
+    // CHECK: }
+    func g5(_ b: Bool) throws(OneWord) -> (Int, Int, Int, Int, Int) {
+        guard b else {
+            throw OneWord()
+        }
+        return (1, 2, 3, 4, 5)
+    }
+}
+
 protocol P {
     // CHECK: define hidden swiftcc void @"$s16typed_throws_abi1PP2f0yySbAA5EmptyVYKFTj"(i1 %0, ptr noalias swiftself %1, ptr noalias nocapture swifterror dereferenceable(8) %2, ptr %3, ptr %4)
     // CHECK:   [[ERROR:%.*]] = load ptr, ptr %2
