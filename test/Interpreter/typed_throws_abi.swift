9d15445a1415e49157c95f12371a944a9bd40253
Dario Rexin <drexin@apple.com>
2024-09-21 11:40:12 -0700

    [Test] Add executable tests for typed throws ABI

--- test/Interpreter/typed_throws_abi.swift
+++ test/Interpreter/typed_throws_abi.swift
@@ -77,16 +77,6 @@ func checkSync() async {
     await invoke { try impl.g5(true) }
     // CHECK: Error: OneWord(x: 0)
     await invoke { try impl.g5(false) }
-
-    // CHECK: Success: (1.0, 2.0)
-    await invoke { try impl.nonMatching_f0(true) }
-    // CHECK: Error: OneWord(x: 0)
-    await invoke { try impl.nonMatching_f0(false) }
-
-    // CHECK: Success: (1.0, true, 2.0)
-    await invoke { try impl.nonMatching_f1(true) }
-    // CHECK: Error: OneWord(x: 0)
-    await invoke { try impl.nonMatching_f1(false) }
 }
 
 func checkAsync() async {
@@ -150,16 +140,6 @@ func checkAsync() async {
     await invoke { try await impl.g5(true) }
     // CHECK: Error: OneWord(x: 0)
     await invoke { try await impl.g5(false) }
-
-    // CHECK: Success: (1.0, 2.0)
-    await invoke { try await impl.nonMatching_f0(true) }
-    // CHECK: Error: OneWord(x: 0)
-    await invoke { try await impl.nonMatching_f0(false) }
-
-    // CHECK: Success: (1.0, true, 2.0)
-    await invoke { try await impl.nonMatching_f1(true) }
-    // CHECK: Error: OneWord(x: 0)
-    await invoke { try await impl.nonMatching_f1(false) }
 }
 
 @main
