f985b0ee033d4b475738dc7009e93ea0748b878c
Michael Gottesman <mgottesman@apple.com>
2024-09-23 20:24:56 -0700

    [thunk-lowering] Add a pass that performs lowering of ThunkInsts.
    Right now it just handles the "identity" case so we can validate the
    functionality.

--- include/swift/SIL/SILModule.h
+++ include/swift/SIL/SILModule.h
@@ -644,6 +644,13 @@ public:
     return {functions.begin(), functions.end()};
   }
 
+  /// Move \p fn to be in the function list before \p moveBefore.
+  void moveBefore(SILModule::iterator moveBefore, SILFunction *fn);
+
+  /// Move \p fn to be in the function list after \p moveAfter. It is assumed
+  /// that \p moveAfter is not end.
+  void moveAfter(SILModule::iterator moveAfter, SILFunction *fn);
+
   const_iterator zombies_begin() const { return zombieFunctions.begin(); }
   const_iterator zombies_end() const { return zombieFunctions.end(); }
 
