49a2831d3f8931aa74e78bd74ef5810e47b5658e
Nate Chandler <nate_chandler@apple.com>
2024-10-01 15:09:16 -0700

    [CoroutineAccessors] Old ABI may be used via flag.
    Add a setting to IRGenOptions and key off of it to emit yield_once_2
    coroutines using either (1) the same code-path as yield_once coroutines
    or (2) a new, not-yet implemented code-path.
    
    Add flags to set the value in both directions.  During bringup, by
    default, use the existing caller-allocated ABI.

---
+++ test/IRGen/coroutine_accessors.swift
@@ -0,0 +1,53 @@
+// RUN: %target-swift-emit-irgen                             \
+// RUN:     %s                                               \
+// RUN:     -enable-experimental-feature CoroutineAccessors  \
+// RUN: | %IRGenFileCheck %s --check-prefix=CHECK-OLD
+
+// For now, a crash is expected when attempting to use the callee-allocated ABI: 
+// it's not implemented.
+// RUN: not --crash                                             \
+// RUN:     %target-swift-emit-irgen                            \
+// RUN:         %s                                              \
+// RUN:         -enable-experimental-feature CoroutineAccessors \
+// RUN:         -enable-experimental-feature CoroutineAccessorsAllocateInCallee
+
+// REQUIRES: asserts
+
+public struct S {
+public var o: any AnyObject
+public var _i: Int = 0
+
+public var irm: Int {
+// CHECK-OLD-LABEL: define{{.*}} { ptr, i64 } @"$s19coroutine_accessors1SV3irmSivy"(
+// CHECK-OLD-SAME:      ptr noalias dereferenceable(32) %0,
+// CHECK-OLD-SAME:      ptr %1,
+// CHECK-OLD-SAME:      i64 %2
+// CHECK-OLD-SAME:  )
+// CHECK-OLD-SAME:  {
+// CHECK-OLD:       }
+  read {
+    yield _i
+  }
+// CHECK-OLD-LABEL: define{{.*}} { ptr, ptr } @"$s19coroutine_accessors1SV3irmSivx"(
+// CHECK-OLD-SAME:      ptr noalias dereferenceable(32) %0, 
+// CHECK-OLD-SAME:      ptr nocapture swiftself dereferenceable(16) %1
+// CHECK-OLD-SAME:  )
+// CHECK-OLD-SAME:  {
+// CHECK-OLD:       }
+  modify {
+    yield &_i
+  }
+// CHECK-OLD-LABEL: define{{.*}} i64 @"$s19coroutine_accessors1SV3irmSivg"(
+// CHECK-OLD-SAME:      ptr %0,
+// CHECK-OLD-SAME:      i64 %1
+// CHECK-OLD-SAME:  ) #0
+// CHECK-OLD-SAME:  {
+// CHECK-OLD:       }
+// CHECK-OLD-LABEL: define{{.*}} void @"$s19coroutine_accessors1SV3irmSivs"(
+// CHECK-OLD-SAME:      i64 %0, 
+// CHECK-OLD-SAME:      ptr nocapture swiftself dereferenceable(16) %1
+// CHECK-OLD-SAME:  )
+// CHECK-OLD-SAME:  {
+// CHECK-OLD:       }
+} // public var irm
+}
