85b4785a3afed780703350de962ac5998e0acac2
Vera Mitchell <vgm@apple.com>
2024-10-01 12:47:42 -0600

    gate UmbrellaSubmodules test on objc_interop

--- test/SymbolGraph/ClangImporter/UmbrellaSubmodules.swift
+++ test/SymbolGraph/ClangImporter/UmbrellaSubmodules.swift
@@ -6,6 +6,8 @@
 // RUN: %target-swift-symbolgraph-extract -sdk %clang-importer-sdk -module-name UmbrellaSubmodules -I %t/UmbrellaSubmodules -output-dir %t -pretty-print -v
 // RUN: %FileCheck %s --input-file %t/UmbrellaSubmodules.symbols.json
 
+// REQUIRES: objc_interop
+
 //--- UmbrellaSubmodules/module.modulemap
 module UmbrellaSubmodules {
     umbrella header "UmbrellaSubmodules.h"
