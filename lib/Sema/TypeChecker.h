969cb6e5de6f846cb09c9ba2aa16cda402d6f84d
Becca Royal-Gordon <beccadax@apple.com>
2024-10-04 14:35:56 -0700

    [NFC] Improve lookup flag printing facilities

--- lib/Sema/TypeChecker.h
+++ lib/Sema/TypeChecker.h
@@ -161,10 +161,14 @@ enum class NameLookupFlags {
   /// Whether to include members that would otherwise be filtered out because
   /// they come from a module that has not been imported.
   IgnoreMissingImports = 1 << 4,
+
+  // Reminder: If you add a flag, make sure you update simple_display() below
 };
 
 /// A set of options that control name lookup.
 using NameLookupOptions = OptionSet<NameLookupFlags>;
+
+void simple_display(llvm::raw_ostream &out, NameLookupOptions opts);
 
 inline NameLookupOptions operator|(NameLookupFlags flag1,
                                    NameLookupFlags flag2) {
