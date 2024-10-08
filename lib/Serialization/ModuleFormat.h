71239d6357da52b0ab1d7c027989a07366da940f
Nate Chandler <nate_chandler@apple.com>
2024-10-08 15:49:53 -0700

    [CoroutineAccessors] SIL represents callee alloc.
    When its operand has coroutine kind `yield_once_2`, a `begin_apply`
    instruction produces an additional value representing the storage
    allocated by the callee.  This storage must be deallocated by a
    `dealloc_stack` on every path out of the function.  Like any other stack
    allocation, it must obey stack discipline.

--- lib/Serialization/ModuleFormat.h
+++ lib/Serialization/ModuleFormat.h
@@ -58,7 +58,7 @@ const uint16_t SWIFTMODULE_VERSION_MAJOR = 0;
 /// describe what change you made. The content of this comment isn't important;
 /// it just ensures a conflict if two people change the module format.
 /// Don't worry about adhering to the 80-column limit for this line.
-const uint16_t SWIFTMODULE_VERSION_MINOR = 895; // @yield_once_2
+const uint16_t SWIFTMODULE_VERSION_MINOR = 896; // @yield_once_2 produces address
 
 /// A standard hash seed used for all string hashes in a serialized module.
 ///
