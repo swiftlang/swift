89c3b32b4b58dead56b8ba55cdd322f0d1c2e701
Nate Chandler <nate_chandler@apple.com>
2024-10-08 17:01:56 -0700

    [Test] Fix whitespace.

--- test/SILGen/coroutine_accessors.swift
+++ test/SILGen/coroutine_accessors.swift
@@ -1,5 +1,5 @@
-// RUN: %target-swift-emit-silgen \
-// RUN:     %s \
+// RUN: %target-swift-emit-silgen                           \
+// RUN:     %s                                              \
 // RUN:     -enable-experimental-feature CoroutineAccessors \
 // RUN: | %FileCheck %s
 
@@ -10,7 +10,7 @@ public var o: any AnyObject
 public var _i: Int = 0
 
 public var irm: Int {
-// CHECK-LABEL: sil [ossa] @$s19coroutine_accessors1SV3irmSivy : 
+// CHECK-LABEL: sil [ossa] @$s19coroutine_accessors1SV3irmSivy :
 // CHECK-SAME:      $@yield_once
 // CHECK-SAME:      @convention(method)
 // CHECK-SAME:      (@guaranteed S)
@@ -21,7 +21,7 @@ public var irm: Int {
   read {
     yield _i
   }
-// CHECK-LABEL: sil [ossa] @$s19coroutine_accessors1SV3irmSivx : 
+// CHECK-LABEL: sil [ossa] @$s19coroutine_accessors1SV3irmSivx :
 // CHECK-SAME:      $@yield_once
 // CHECK-SAME:      @convention(method)
 // CHECK-SAME:      (@inout S)
@@ -46,6 +46,7 @@ public var irm: Int {
 // CHECK:         end_apply [[TOKEN]]
 // CHECK:         return [[VALUE:%[^,]+]]
 // CHECK-LABEL: } // end sil function '$s19coroutine_accessors1SV3irmSivg'
+
 // CHECK-LABEL: sil {{.*}}[ossa] @$s19coroutine_accessors1SV3irmSivs :
 // CHECK-SAME:      $@convention(method)
 // CHECK-SAME:      (Int, @inout S)
@@ -55,14 +56,14 @@ public var irm: Int {
 // CHECK:       bb0(
 // CHECK-SAME:      [[NEW_VALUE:%[^,]+]] :
 // CHECK-SAME:      [[SELF:%[^,]+]] :
-// CHECK-SAME: ):
-// CHECK:        [[SELF_ACCESS:%[^,]+]] = begin_access [modify] [unknown] [[SELF]]
-// CHECK:        [[MODIFY_ACCESSOR:%[^,]+]] = function_ref @$s19coroutine_accessors1SV3irmSivx
-// CHECK:        ([[VALUE_ADDRESS:%[^,]+]], [[TOKEN:%[^,]+]]) = begin_apply [[MODIFY_ACCESSOR]]([[SELF_ACCESS]])
-// CHECK:        assign [[NEW_VALUE:%[^,]+]] to [[VALUE_ADDRESS]]
-// CHECK:        end_apply [[TOKEN]]
-// CHECK:        end_access [[SELF_ACCESS]]
-// CHECK-LABEL:  } // end sil function '$s19coroutine_accessors1SV3irmSivs'
+// CHECK-SAME:  ):
+// CHECK:         [[SELF_ACCESS:%[^,]+]] = begin_access [modify] [unknown] [[SELF]]
+// CHECK:         [[MODIFY_ACCESSOR:%[^,]+]] = function_ref @$s19coroutine_accessors1SV3irmSivx
+// CHECK:         ([[VALUE_ADDRESS:%[^,]+]], [[TOKEN:%[^,]+]]) = begin_apply [[MODIFY_ACCESSOR]]([[SELF_ACCESS]])
+// CHECK:         assign [[NEW_VALUE:%[^,]+]] to [[VALUE_ADDRESS]]
+// CHECK:         end_apply [[TOKEN]]
+// CHECK:         end_access [[SELF_ACCESS]]
+// CHECK-LABEL:} // end sil function '$s19coroutine_accessors1SV3irmSivs'
 } // public var irm
 
 } // public struct S
