f0b28e6336cfdefaa826cebe2ffa36267a09aad5
Nate Chandler <nate_chandler@apple.com>
2024-10-09 14:56:16 -0700

    [Test] Add caller of modify.

--- test/SILGen/coroutine_accessors.swift
+++ test/SILGen/coroutine_accessors.swift
@@ -89,4 +89,53 @@ public var irm: Int {
 // CHECK-LABEL: } // end sil function '$s19coroutine_accessors1SV3irmSivM'
 } // public var irm
 
+// CHECK-LABEL: sil {{.*}}[ossa] @$s19coroutine_accessors1SV6update3irmS2i_tKF :
+// CHECK-SAME:      $@convention(method)
+// CHECK-SAME:      (Int, @inout S)
+// CHECK-SAME:      ->
+// CHECK-SAME:      (Int, @error any Error)
+// CHECK-SAME: {
+// CHECK:      bb0(
+// CHECK-SAME:      [[NEW_VALUE:%[^,]+]] :
+// CHECK-SAME:      [[SELF:%[^,]+]] :
+// CHECK-SAME: ):
+// CHECK:      [[OLD_VALUE_ADDR:%[^,]+]] = alloc_stack $Int
+// CHECK:      [[NEW_VALUE_ADDR:%[^,]+]] = alloc_stack $Int
+// CHECK:      store [[NEW_VALUE:%[^,]+]] to [trivial] [[NEW_VALUE_ADDR]]
+// CHECK:      [[SELF_ACCESS:%[^,]+]] = begin_access [modify] [unknown] [[SELF]]
+// CHECK:      [[MODIFY_ACCESSOR:%[^,]+]] = function_ref @$s19coroutine_accessors1SV3irmSivx
+// CHECK:      ([[VALUE_ADDR:%[^,]+]], [[TOKEN:%[^,]+]]) = begin_apply [[MODIFY_ACCESSOR]]([[SELF_ACCESS]])
+// CHECK:      [[UPDATE:%[^,]+]] = function_ref @$s19coroutine_accessors6update2at2toxxz_xtKSQRzlF
+// CHECK:      try_apply [[UPDATE:%[^,]+]]<Int>([[OLD_VALUE_ADDR]], [[VALUE_ADDR]], [[NEW_VALUE_ADDR]])
+// CHECK:    bb1
+// CHECK:      end_apply [[TOKEN]] as $()
+// CHECK:      end_access [[SELF_ACCESS]]
+// CHECK:      dealloc_stack [[NEW_VALUE_ADDR]]
+// CHECK:      [[OLD_VALUE:%[^,]+]] = load [trivial] [[OLD_VALUE_ADDR]]
+// CHECK:      dealloc_stack [[OLD_VALUE_ADDR]]
+// CHECK:      return [[OLD_VALUE]]
+// CHECK:    bb2([[ERROR:%[^,]+]] : @owned $any Error):
+// CHECK:      abort_apply [[TOKEN]]
+// CHECK:      end_access [[SELF_ACCESS]]
+// CHECK:      dealloc_stack [[NEW_VALUE_ADDR]]
+// CHECK:      dealloc_stack [[OLD_VALUE_ADDR]]
+// CHECK:      throw [[ERROR]]
+// CHECK-LABEL: } // end sil function '$s19coroutine_accessors1SV6update3irmS2i_tKF'
+mutating func update(irm newValue: Int) throws -> Int {
+  try coroutine_accessors.update(at: &irm, to: newValue)
+}
+
 } // public struct S
+
+enum E : Error {
+  case e
+}
+
+func update<T : Equatable>(at location: inout T, to newValue: T) throws -> T {
+  let oldValue = location
+  if oldValue == newValue {
+    throw E.e
+  }
+  location = newValue
+  return oldValue
+}
