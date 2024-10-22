38b56423fa0190269a5f70ac9697b7f29df46f10
usama <u_hameed@apple.com>
2024-10-22 13:16:44 -0700

    Remove full paths from test

--- test/Serialization/debug-scope.swift
+++ test/Serialization/debug-scope.swift
@@ -8,21 +8,21 @@ import def_debug
 
 // SIL-NOT: UnknownCode
 
-// CHECK: sil_scope [[SCOPE_ONE:[0-9]+]] { loc "SOURCE_DIR/test/Serialization/Inputs/def_debug.swift":2:13 parent @$s9def_debug3foo1xs6UInt64VAE_tF : $@convention(thin) (UInt64) -> UInt64 inlined_at [[INLINE_SITE_ONE:[0-9]+]] }
-// CHECK: sil_scope [[SCOPE_TWO:[0-9]+]] { loc "SOURCE_DIR/test/Serialization/Inputs/def_debug.swift":3:5 parent [[SCOPE_ONE]] inlined_at [[INLINE_SITE_ONE]] }
-// CHECK: sil_scope {{[0-9]+}} { loc "SOURCE_DIR/test/Serialization/Inputs/def_debug.swift":3:18 parent [[SCOPE_TWO]] inlined_at [[INLINE_SITE_ONE]] }
+// CHECK: sil_scope [[SCOPE_ONE:[0-9]+]] { loc "{{.*}}def_debug.swift":2:13 parent @$s9def_debug3foo1xs6UInt64VAE_tF : $@convention(thin) (UInt64) -> UInt64 inlined_at [[INLINE_SITE_ONE:[0-9]+]] }
+// CHECK: sil_scope [[SCOPE_TWO:[0-9]+]] { loc "{{.*}}def_debug.swift":3:5 parent [[SCOPE_ONE]] inlined_at [[INLINE_SITE_ONE]] }
+// CHECK: sil_scope {{[0-9]+}} { loc "{{.*}}def_debug.swift":3:18 parent [[SCOPE_TWO]] inlined_at [[INLINE_SITE_ONE]] }
 let simpleFunc = foo(x: 1)
 
 // _transparent with Onone
-// NOOPT: sil_scope [[SCOPE_GENERIC_ONE:[0-9]+]] { loc "SOURCE_DIR/test/Serialization/Inputs/def_debug.swift":10:13 parent @$s9def_debug25specializedGenericInlinedSiyF : $@convention(thin) () -> Int }
+// NOOPT: sil_scope [[SCOPE_GENERIC_ONE:[0-9]+]] { loc "{{.*}}def_debug.swift":10:13 parent @$s9def_debug25specializedGenericInlinedSiyF : $@convention(thin) () -> Int }
 var funcReferringInlinedGeneric = specializedGenericInlined()
 
 // _alwaysEmitIntoClient with Onone
-// NOOPT: sil_scope [[SCOPE_ONE:[0-9]+]] { loc "SOURCE_DIR/test/Serialization/Inputs/def_debug.swift":21:13 parent @$s9def_debug10barGeneric_3sumxSayxG_xtSjRzlF : $@convention(thin) <τ_0_0 where τ_0_0 : Numeric> (@guaranteed Array<τ_0_0>, @in_guaranteed τ_0_0) -> @out τ_0_0 }
-// NOOPT: sil_scope [[SCOPE_TWO:[0-9]+]] { loc "SOURCE_DIR/test/Serialization/Inputs/def_debug.swift":22:9 parent [[SCOPE_ONE]] }
-// NOOPT: sil_scope {{[0-9]+}} { loc "SOURCE_DIR/test/Serialization/Inputs/def_debug.swift":22:16 parent [[SCOPE_ONE]] }
-// NOOPT: sil_scope [[SCOPE_THREE:[0-9]+]] { loc "SOURCE_DIR/test/Serialization/Inputs/def_debug.swift":23:5 parent [[SCOPE_TWO]] }
-// NOOPT: sil_scope {{[0-9]+}} { loc "SOURCE_DIR/test/Serialization/Inputs/def_debug.swift":23:14 parent [[SCOPE_THREE]] }
+// NOOPT: sil_scope [[SCOPE_ONE:[0-9]+]] { loc "{{.*}}def_debug.swift":21:13 parent @$s9def_debug10barGeneric_3sumxSayxG_xtSjRzlF : $@convention(thin) <τ_0_0 where τ_0_0 : Numeric> (@guaranteed Array<τ_0_0>, @in_guaranteed τ_0_0) -> @out τ_0_0 }
+// NOOPT: sil_scope [[SCOPE_TWO:[0-9]+]] { loc "{{.*}}def_debug.swift":22:9 parent [[SCOPE_ONE]] }
+// NOOPT: sil_scope {{[0-9]+}} { loc "{{.*}}def_debug.swift":22:16 parent [[SCOPE_ONE]] }
+// NOOPT: sil_scope [[SCOPE_THREE:[0-9]+]] { loc "{{.*}}def_debug.swift":23:5 parent [[SCOPE_TWO]] }
+// NOOPT: sil_scope {{[0-9]+}} { loc "{{.*}}def_debug.swift":23:14 parent [[SCOPE_THREE]] }
 let x = barGeneric([1,2], sum: 0)
 
 //Use inlinable so that the generic inliner later tries to lookup an inlinable
