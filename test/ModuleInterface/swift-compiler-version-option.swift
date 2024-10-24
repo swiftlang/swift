b696f5afdcb9d91922de44782f36687a69f42542
Pavel Yaskevich <pyaskevich@apple.com>
2024-10-24 16:59:42 -0700

    [Frontend] Always print current compiler version in swiftinterface flags
    Swiftinterface files are always built with "current" compiler version,
    which means that `-swift-compiler-version` has no effect in this context.

--- test/ModuleInterface/swift-compiler-version-option.swift
+++ test/ModuleInterface/swift-compiler-version-option.swift
@@ -4,20 +4,20 @@
 // RUN: %target-swift-frontend %s \
 // RUN:   -emit-module-path %t/Lib.swiftmodule \
 // RUN:   -emit-module-interface-path %t/Lib.swiftinterface \
-// RUN:   -enable-library-evolution -swift-version 6 \
-// RUN:   -swift-compiler-version 6.0.0.1
+// RUN:   -enable-library-evolution \
+// RUN:   -swift-version 6
 
 // RUN: %target-swift-typecheck-module-from-interface(%t/Lib.swiftinterface)
 
 /// Check option in swiftinterface
 // RUN: cat %t/Lib.swiftinterface | %FileCheck --check-prefix=CHECK-OPTION %s
-// CHECK-OPTION: swift-module-flags:
-// CHECK-SAME-OPTION: -swift-compiler-version 6.0.0.1
+// CHECK-OPTION: swift-module-flags-ignorable:
+// CHECK-SAME-OPTION: -swift-compiler-version {{.*}}
 
 /// Check option in swiftmodule
 // RUN: llvm-bcanalyzer --dump %t/Lib.swiftmodule | %FileCheck --check-prefix=CHECK-MODULE-OPTION %s
 // CHECK-MODULE-OPTION: <OPTIONS_BLOCK
-// CHECK-MODULE-OPTION: <SWIFT_COMPILER_VERSION abbrevid={{.*}}/> blob data = '6.0.0.1'
+// CHECK-MODULE-OPTION: <SWIFT_COMPILER_VERSION abbrevid={{.*}}/> blob data = '{{.*}}'
 // CHECK-MODULE-OPTION: </OPTIONS_BLOCK>
 
 public struct S {
