b1ea058302c13f209ada6609214d4a26914a9064
QuietMisdreavus <QuietMisdreavus@users.noreply.github.com>
2024-10-31 14:18:37 -0600

    Revert "fix nondeterminism in OptionalRequirementOf test (#77276)"
    This reverts commit 108ad6f1157aae08edb68f7d4683c93f92ccc56a.

--- test/SymbolGraph/Relationships/OptionalRequirementOf.swift
+++ test/SymbolGraph/Relationships/OptionalRequirementOf.swift
@@ -8,12 +8,12 @@
 // RUN: %target-swift-symbolgraph-extract -module-name OptionalRequirementOf -F %t/frameworks -pretty-print -output-dir %t
 // RUN: %FileCheck %s --input-file %t/OptionalRequirementOf.symbols.json
 
-// CHECK-NOT: "kind": "requirementOf"
-
 // ObjCProto.objcReq -> ObjCProto
+// CHECK-NOT: "kind": "requirementOf",{{[[:space:]]*}}"source": "c:@M@OptionalRequirementOf@objc(pl)SwiftProto(im)swiftReq",{{[[:space:]]*}}"target": "c:@M@OptionalRequirementOf@objc(pl)SwiftProto"
 // CHECK-DAG: "kind": "optionalRequirementOf",{{[[:space:]]*}}"source": "c:@M@OptionalRequirementOf@objc(pl)SwiftProto(im)swiftReq",{{[[:space:]]*}}"target": "c:@M@OptionalRequirementOf@objc(pl)SwiftProto"
 
 // SwiftProto.swiftReq -> SwiftProto
+// CHECK-NOT: "kind": "requirementOf",{{[[:space:]]*}}"source": "c:objc(pl)ObjCProto(im)objcReq",{{[[:space:]]*}}"target": "c:objc(pl)ObjCProto"
 // CHECK-DAG: "kind": "optionalRequirementOf",{{[[:space:]]*}}"source": "c:objc(pl)ObjCProto(im)objcReq",{{[[:space:]]*}}"target": "c:objc(pl)ObjCProto"
 
 //--- reqs.swift
