12fb294cce862ba8abe0a1aaa12d6cb120d833d7
Evan Wilde <etceterawilde@gmail.com>
2024-11-07 00:11:08 -0800

    Remove SwiftCore_SWIFTC_CLANGIMPORTER_RESOURCE_DIR
    The SwiftCore_SWIFTC_CLANGIMPORTER_RESOURCE_DIR is not being used so I'm
    deleting it for now. This means we only grab the target info once.

--- Runtimes/Core/cmake/modules/PlatformInfo.cmake
+++ Runtimes/Core/cmake/modules/PlatformInfo.cmake
@@ -15,28 +15,6 @@ if(NOT SwiftCore_MODULE_TRIPLE)
   "${target_info_json}")
   string(JSON module_triple GET "${target_info_json}" "target" "moduleTriple")
   set(SwiftCore_MODULE_TRIPLE "${module_triple}" CACHE STRING "swift module triple used for installed swiftmodule and swiftinterface files")
+  message(CONFIGURE_LOG "Swift module triple: ${module_triple}")
   mark_as_advanced(SwiftCore_MODULE_TRIPLE)
-endif()
-
-if(NOT SwiftCore_SWIFTC_CLANGIMPORTER_RESOURCE_DIR)
-  # TODO: We need to separate the concept of compiler resources and the stdlib.
-  #       Compiler-resources in the compiler-resource directory are specific to
-  #       a given compiler. The headers in `lib/clang/<version>/include` and
-  #       `lib/swift/clang/include` correspond with that specific copy clang and
-  #       should not be mixed. This won't cause modularization issues because
-  #       the one copy of clang should never be looking in the other's resource
-  #       directory. If there are issues here, something has gone horribly wrong
-  #       and you're looking in the wrong place.
-  set(module_triple_command "${CMAKE_Swift_COMPILER}" -print-target-info)
-  if(CMAKE_Swift_COMPILER_TARGET)
-    list(APPEND module_triple_command -target ${CMAKE_Swift_COMPILER_TARGET})
-  endif()
-  execute_process(COMMAND ${module_triple_command} OUTPUT_VARIABLE target_info_json)
-  string(JSON resource_dir GET "${target_info_json}" "paths" "runtimeResourcePath")
-  cmake_path(APPEND resource_dir "clang")
-  message(CONFIGURE_LOG "Using Swift clang-importer headers from toolchain Swift\n"
-    "clang-importer header directory: ${resource_dir}")
-  set(SwiftCore_SWIFTC_CLANGIMPORTER_RESOURCE_DIR "${resource_dir}"
-    CACHE FILEPATH "Swift clang-importer resource directory")
-  mark_as_advanced(SwiftCore_SWIFTC_CLANGIMPORTER_RESOURCE_DIR)
 endif()
