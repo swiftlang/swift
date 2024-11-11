ae716bcb339beb91c10f7cafb3f1bba99a9ccd17
Doug Gregor <dgregor@apple.com>
2024-11-11 09:49:32 -0800

    XFAIL tests that can't work without more library support

--- test/Macros/PointerBounds/SizedBy/MutableRawSpan.swift
+++ test/Macros/PointerBounds/SizedBy/MutableRawSpan.swift
@@ -1,6 +1,6 @@
 // REQUIRES: swift_swift_parser
 // REQUIRES: pointer_bounds
-
+// XFAIL: OS=windows-msvc
 // RUN: %target-swift-frontend %s -swift-version 5 -module-name main -disable-availability-checking -typecheck -plugin-path %swift-plugin-dir -dump-macro-expansions 2>&1 | %FileCheck --match-full-lines %s
 
 @PointerBounds(.sizedBy(pointer: 1, size: "size"), .nonescaping(pointer: 1))
