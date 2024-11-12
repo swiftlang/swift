964ba731bbd548e1770da68f9c22a201ac29954c
Hamish Knight <hamish_knight@apple.com>
2024-11-12 18:26:53 +0000

    [Sema] Inherit ExprAvailabilityWalker from BaseDiagnosticWalker
    Simplifies things a tiny amount, and ensures we
    don't attempt to walk into non-PatternBindingDecls,
    same as other MiscDiagnostic passes. I don't think
    this currently makes a difference since I don't
    believe we have any interesting cases where a Decl
    is nested in an Expr without an intermediate Stmt,
    but it's the right thing to do regardless.

--- lib/Sema/TypeCheckAvailability.cpp
+++ lib/Sema/TypeCheckAvailability.cpp
@@ -3561,7 +3561,7 @@ bool diagnoseExplicitUnavailability(
 }
 
 namespace {
-class ExprAvailabilityWalker : public ASTWalker {
+class ExprAvailabilityWalker : public BaseDiagnosticWalker {
   /// Models how member references will translate to accessor usage. This is
   /// used to diagnose the availability of individual accessors that may be
   /// called by the expression being checked.
@@ -3595,11 +3595,6 @@ class ExprAvailabilityWalker : public ASTWalker {
 public:
   explicit ExprAvailabilityWalker(const ExportContext &Where)
     : Context(Where.getDeclContext()->getASTContext()), Where(Where) {}
-
-  MacroWalking getMacroWalkingBehavior() const override {
-    // Expanded source should be type checked and diagnosed separately.
-    return MacroWalking::Arguments;
-  }
 
   PreWalkAction walkToArgumentPre(const Argument &Arg) override {
     // Arguments should be walked in their own member access context which
