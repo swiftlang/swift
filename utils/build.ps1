85db85638e2ecb9a0342b0d166c1e929643171c6
Saleem Abdulrasool <abdulras@thebrowser.company>
2024-11-15 09:17:46 -0800

    utils: wire up the CMake based build of DocC SymbolKit
    This is in preparation to build DocC as a shared library via CMake to
    allow linking into SourceKit-LSP.

--- utils/build.ps1
+++ utils/build.ps1
@@ -2479,6 +2479,16 @@ function Build-SourceKitLSP($Arch) {
     }
 }
 
+function Build-SymbolKit($Arch) {
+  Build-CMakeProject `
+    -Src $SourceCache\swift-docc-symbolkit `
+    -Bin (Get-HostProjectBinaryCache SymbolKit) `
+    -Arch $Arch `
+    -UseBuiltCompilers Swift `
+    -SwiftSDK (Get-HostSwiftSDK) `
+    -BuildTargets default
+}
+
 function Test-SourceKitLSP {
   $SwiftPMArguments = @(
     # swift-syntax
@@ -2839,6 +2849,7 @@ if (-not $SkipBuild) {
   Invoke-BuildStep Build-Format $HostArch
   Invoke-BuildStep Build-IndexStoreDB $HostArch
   Invoke-BuildStep Build-SourceKitLSP $HostArch
+  Invoke-BuildStep Build-SymbolKit $HostArch
 }
 
 Install-HostToolchain
