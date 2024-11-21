751bcfd9529713212465e1c645e347158221dd10
Gabor Horvath <gaborh@apple.com>
2024-11-21 11:59:04 +0000

    [cxx-interop] Support parameter packs in escapability annotations
    rdar://140271527

--- test/Interop/Cxx/class/nonescapable-errors.swift
+++ test/Interop/Cxx/class/nonescapable-errors.swift
@@ -77,6 +77,13 @@ Outer<View>::NonTemplated::Inner<Owner> j1();
 Outer<Owner>::NonTemplated::Inner<View> j2();
 Outer<Owner>::NonTemplated::Inner<Owner> j3();
 
+template<typename... Ts>
+struct SWIFT_ESCAPABLE_IF(Ts) MyTuple {};
+
+MyTuple<View> k1();
+MyTuple<Owner, View> k2();
+MyTuple<Owner, Owner> k3();
+
 //--- test.swift
 import Test
 
@@ -113,6 +120,13 @@ public func noAnnotations() -> View {
     // CHECK: nonescapable.h:63:41: error: cannot infer lifetime dependence, no parameters found that are either ~Escapable or Escapable with a borrowing ownership
     // CHECK-NO-LIFETIMES: nonescapable.h:63:41: error: returning ~Escapable type requires '-enable-experimental-feature LifetimeDependence'
     j3()
+    k1();
+    // CHECK: nonescapable.h:69:15: error: cannot infer lifetime dependence, no parameters found that are either ~Escapable or Escapable with a borrowing ownership
+    // CHECK-NO-LIFETIMES: nonescapable.h:69:15: error: returning ~Escapable type requires '-enable-experimental-feature LifetimeDependence'
+    k2();
+    // CHECK: nonescapable.h:70:22: error: cannot infer lifetime dependence, no parameters found that are either ~Escapable or Escapable with a borrowing ownership
+    // CHECK-NO-LIFETIMES: nonescapable.h:70:22: error: returning ~Escapable type requires '-enable-experimental-feature LifetimeDependence'
+    k3();
     // CHECK-NOT: error
     // CHECK-NOT: warning
     return View()
