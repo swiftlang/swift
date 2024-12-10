f3c687db98dae96f3b2a616ecc7ecb1b1cf0e591
Konrad `ktoso` Malawski <konrad_malawski@apple.com>
2024-12-10 13:33:22 +0900

    Add pointer auth for isolated deinit work function

--- stdlib/public/Concurrency/Actor.cpp
+++ stdlib/public/Concurrency/Actor.cpp
@@ -18,6 +18,7 @@
 #include "swift/Runtime/Concurrency.h"
 #include <atomic>
 #include <new>
+#include <ptrauth.h>
 
 #include "../CompatibilityOverride/CompatibilityOverride.h"
 #include "swift/ABI/Actor.h"
@@ -2351,6 +2352,9 @@ static void swift_task_deinitOnExecutorImpl(void *object,
                                             DeinitWorkFunction *work,
                                             SerialExecutorRef newExecutor,
                                             size_t rawFlags) {
+  // Sign the function pointer
+  work = swift_auth_code_function(
+      work, SpecialPointerAuthDiscriminators::DeinitWorkFunction);
   // If the current executor is compatible with running the new executor,
   // we can just immediately continue running with the resume function
   // we were passed in.
