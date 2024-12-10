d7e8c7461680aa0bcfff40da46c856d75d295e6c
Andrew Rogers <andrurogerz@gmail.com>
2024-12-09 16:56:31 -0800

    [swift-inspect] add ptrace retries

--- tools/swift-inspect/Sources/SwiftInspectLinux/SystemHeaders/ptrace.h
+++ tools/swift-inspect/Sources/SwiftInspectLinux/SystemHeaders/ptrace.h
@@ -18,36 +18,46 @@
 #include <linux/ptrace.h>
 
 static inline
+int ptrace_wrapper(int op, pid_t pid, void *addr, void *data) {
+  int retries = 3;
+  int result;
+  do {
+    result = ptrace(op, pid, addr, data);
+  } while (result < 0 && (errno == EAGAIN || errno == EBUSY) && retries-- > 0);
+  return result;
+}
+
+static inline
 int ptrace_attach(pid_t pid) {
-  return ptrace(PTRACE_ATTACH, pid, 0, 0);
+  return ptrace_wrapper(PTRACE_ATTACH, pid, 0, 0);
 }
 
 static inline
 int ptrace_detach(pid_t pid) {
-  return ptrace(PTRACE_DETACH, pid, 0, 0);
+  return ptrace_wrapper(PTRACE_DETACH, pid, 0, 0);
 }
 
 static inline
 int ptrace_continue(pid_t pid) {
-  return ptrace(PTRACE_CONT, pid, 0, 0);
+  return ptrace_wrapper(PTRACE_CONT, pid, 0, 0);
 }
 
 static inline
 int ptrace_getsiginfo(pid_t pid, siginfo_t *siginfo) {
-  return ptrace(PTRACE_GETSIGINFO, pid, 0, siginfo);
+  return ptrace_wrapper(PTRACE_GETSIGINFO, pid, 0, siginfo);
 }
 
 static inline
 int ptrace_pokedata(pid_t pid, unsigned long addr, unsigned long value) {
-  return ptrace(PTRACE_POKEDATA, pid, addr, value);
+  return ptrace_wrapper(PTRACE_POKEDATA, pid, (void*)(uintptr_t)addr, (void*)(uintptr_t)value);
 }
 
 static inline
 int ptrace_getregset(pid_t pid, int type, struct iovec *regs) {
-  return ptrace(PTRACE_GETREGSET, pid, type, regs);
+  return ptrace_wrapper(PTRACE_GETREGSET, pid, (void*)(uintptr_t)type, regs);
 }
 
 static inline
 int ptrace_setregset(pid_t pid, int type, struct iovec *regs) {
-  return ptrace(PTRACE_SETREGSET, pid, type, regs);
+  return ptrace_wrapper(PTRACE_SETREGSET, pid, (void*)(uintptr_t)type, regs);
 }
