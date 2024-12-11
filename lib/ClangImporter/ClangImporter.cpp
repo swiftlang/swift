ec8ea94fb90f6ae4fc5a831295fadb4547a0fc7f
Henrik G. Olsson <h_olsson@apple.com>
2024-12-11 13:36:56 -0800

    Infer @PointerBounds macro from clang __counted_by parameters (#77387)
    This results in an automatic wrapper function with safe pointer types
    when the imported function has bounds attributes. This exercises similar
    pathways as the recently added functionality for specifying macros from
    swift_attr. The new functionality is guarded by the experimental
    language feature SafeInteropWrappers.
    
    rdar://97942270

--- lib/ClangImporter/ClangImporter.cpp
+++ lib/ClangImporter/ClangImporter.cpp
@@ -579,6 +579,9 @@ void importer::getNormalInvocationArguments(
     }
   }
 
+  if (LangOpts.hasFeature(Feature::SafeInteropWrappers))
+    invocationArgStrs.push_back("-fexperimental-bounds-safety-attributes");
+
   // Set C language options.
   if (triple.isOSDarwin()) {
     invocationArgStrs.insert(invocationArgStrs.end(), {
