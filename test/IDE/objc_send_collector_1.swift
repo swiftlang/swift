3f69aed0f8efc360f2e1116e0b439da4db9f5499
Xi Ge <xi_ge@apple.com>
2024-12-10 22:06:56 -0800

    FineModuleTracing: use requested action type to further gate whether emitting fine-grained module trace is supported.
    Emitting fine-grained module tracing files during full compilation should be sufficient for the use case in design. Other
    swift-frontend invocations, such as emitting modules, doesn't need fully type-checked AST to perform the action, therefore
    requesting/walking the fully typechecked AST to emit those tracing files may introduce performance overhead.

--- test/IDE/objc_send_collector_1.swift
+++ test/IDE/objc_send_collector_1.swift
@@ -7,7 +7,7 @@
 // RUN: SWIFT_COMPILER_FINE_GRAINED_TRACE_PATH=%t/given_trace.json %target-swift-frontend  -I %t/lib/swift -typecheck %s %S/Inputs/objc_send_collector_2.swift -module-name main -swift-version 5 -F %S/Inputs/mock-sdk -emit-loaded-module-trace-path %t/.MODULE_TRACE
 // RUN: cat %t/given_trace.json | %{python} -c 'import json, sys; json.dump(json.loads(sys.stdin.read()), sys.stdout, sort_keys=True, indent=2)' | %FileCheck %s
 
-// RUN: SWIFT_COMPILER_FINE_GRAINED_TRACE_PATH=%t/given_trace_2.json %target-swift-frontend  -I %t/lib/swift -typecheck %s %S/Inputs/objc_send_collector_2.swift -module-name main -swift-version 5 -F %S/Inputs/mock-sdk -emit-loaded-module-trace-path %t/.MODULE_TRACE -enable-library-evolution -experimental-lazy-typecheck
+// RUN: SWIFT_COMPILER_FINE_GRAINED_TRACE_PATH=%t/given_trace_2.json %target-swift-frontend  -I %t/lib/swift -emit-module %s %S/Inputs/objc_send_collector_2.swift -module-name main -swift-version 5 -F %S/Inputs/mock-sdk -emit-loaded-module-trace-path %t/.MODULE_TRACE -enable-library-evolution
 // RUN: not ls %t/given_trace_2.json
 
 // REQUIRES: objc_interop
