85a4f18dcd259b4ccd9ca78f25e5c0dc7084161f
Kuba Mracek <mracek@apple.com>
2024-12-12 12:17:55 -0800

    [embedded] Add test/embedded/cxxshim.swift testcase

---
+++ test/embedded/cxxshim.swift
@@ -0,0 +1,27 @@
+// RUN: %empty-directory(%t)
+// RUN: %{python} %utils/split_file.py -o %t %s
+
+// RUN: %target-swift-frontend -I %t %t/Main.swift -enable-experimental-feature Embedded -cxx-interoperability-mode=default -c -o %t/a.o -Rmodule-loading
+
+// BEGIN header.h
+
+// C++
+struct Base { int field; };
+struct Derived : Base {};
+
+// BEGIN module.modulemap
+
+module MyModule {
+  header "header.h"
+}
+
+// BEGIN Main.swift
+
+import MyModule
+
+public func foo() {
+  var d = Derived()
+  d.field = 123
+}
+
+foo()
