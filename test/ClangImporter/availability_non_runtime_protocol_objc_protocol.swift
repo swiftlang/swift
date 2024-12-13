9d8fa7e2be34d579f5badecb24652dc41979a955
Sharon Xu <sharonxu@fb.com>
2024-12-13 15:34:11 -0800

    add more test cases

--- test/ClangImporter/availability_non_runtime_protocol_objc_protocol.swift
+++ test/ClangImporter/availability_non_runtime_protocol_objc_protocol.swift
@@ -7,8 +7,42 @@
 import Foundation
 
 @objc class Wrapper : NSObject {
-  @objc public func runMutations(_ mutations: [ObjCNonRuntimeProtocolUsedinSwift, ObjCRuntimeProtocolUsedinSwift]) { // expected-error {{Objective-C protocol 'ObjCNonRuntimeProtocolUsedinSwift' has the non_runtime_protocol attribute and its metadata is not available in Swift}}
+  @objc public func runMutations(_ mutations: [ObjCNonRuntimeProtocolUsedinSwift]) { // expected-error {{Objective-C protocol 'ObjCNonRuntimeProtocolUsedinSwift' has the non_runtime_protocol attribute and its metadata is not available in Swift}}
     for _ in mutations {
     }
+  }
+
+  @objc public func runMutations2(_ mutations: [ObjCRuntimeProtocolUsedinSwift]) {
+    for _ in mutations {
+    }
+  }
+
+  public func iterate(collection mutations: some Collection<any ObjCNonRuntimeProtocolUsedinSwift>) { // expected-error {{Objective-C protocol 'ObjCNonRuntimeProtocolUsedinSwift' has the non_runtime_protocol attribute and its metadata is not available in Swift}}
+      for m in mutations {
+          print(typeOf(anyNRP: m))
+          print(typeOf(someNRP: m))
+      }
+  }
+
+  public func typeOf(anyNRP nrp: any ObjCNonRuntimeProtocolUsedinSwift) -> Any { // expected-error {{Objective-C protocol 'ObjCNonRuntimeProtocolUsedinSwift' has the non_runtime_protocol attribute and its metadata is not available in Swift}}
+      return type(of: nrp)
+  }
+
+  public func typeOf(someNRP nrp: some ObjCNonRuntimeProtocolUsedinSwift) -> Any { // expected-error {{Objective-C protocol 'ObjCNonRuntimeProtocolUsedinSwift' has the non_runtime_protocol attribute and its metadata is not available in Swift}}
+      return type(of: nrp)
+  }
+
+  public func dynamicIsNRP(_ thing: Any) -> Bool {
+      return thing is ObjCNonRuntimeProtocolUsedinSwift // expected-error {{Objective-C protocol 'ObjCNonRuntimeProtocolUsedinSwift' has the non_runtime_protocol attribute and its metadata is not available in Swift}}
+  }
+
+  @objc protocol ExtendsNRP: ObjCNonRuntimeProtocolUsedinSwift {} // expected-error {{Objective-C protocol 'ObjCNonRuntimeProtocolUsedinSwift' has the non_runtime_protocol attribute and its metadata is not available in Swift}}
+
+  public func dynamicAsNRP(_ thing: ExtendsNRP) -> any ObjCNonRuntimeProtocolUsedinSwift { // expected-error {{Objective-C protocol 'ObjCNonRuntimeProtocolUsedinSwift' has the non_runtime_protocol attribute and its metadata is not available in Swift}}
+      return thing as ObjCNonRuntimeProtocolUsedinSwift // expected-error {{Objective-C protocol 'ObjCNonRuntimeProtocolUsedinSwift' has the non_runtime_protocol attribute and its metadata is not available in Swift}}
+  }
+
+  public func dynamicAsNRP(_ thing: Any) -> (any ObjCNonRuntimeProtocolUsedinSwift)? { // expected-error {{Objective-C protocol 'ObjCNonRuntimeProtocolUsedinSwift' has the non_runtime_protocol attribute and its metadata is not available in Swift}}
+      return thing as? ObjCNonRuntimeProtocolUsedinSwift // expected-error {{Objective-C protocol 'ObjCNonRuntimeProtocolUsedinSwift' has the non_runtime_protocol attribute and its metadata is not available in Swift}}
   }
 }
