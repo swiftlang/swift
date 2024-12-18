c24f01a368c89c9e33301dc7aae1564ac042c706
elsh <ellie_shin@apple.com>
2024-12-17 19:11:09 -0800

    Formatting

--- include/swift/AST/Decl.h
+++ include/swift/AST/Decl.h
@@ -2828,7 +2828,6 @@ private:
 
   friend class Decl;
   SourceLoc getLocFromSource() const { return NameLoc; }
-
 protected:
   ValueDecl(DeclKind K,
             llvm::PointerUnion<DeclContext *, ASTContext *> context,
