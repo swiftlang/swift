4ebfa25525f55ac225e14624f82e091d7990f752
Sharon Xu <sharonxu@fb.com>
2024-12-20 14:46:37 -0800

    delete unnecessary tests

--- test/ClangImporter/availability_non_runtime_protocol_objc_protocol2.swift
+++
@@ -1,8 +0,0 @@
- // REQUIRES: objc_interop
-// REQUIRES: foundation
-
-// RUN: %target-swift-frontend -enable-objc-interop -typecheck -verify  -import-objc-header %S/Inputs/availability_non_runtime_protocol_objc_protocol.h %s
-
- 
- 
-    let _ : ObjCNonRuntimeProtocolUsedinSwift
