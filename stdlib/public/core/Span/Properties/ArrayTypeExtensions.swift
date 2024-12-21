995bd45949b20875629fd982f3c244cd90b24add
Guillaume Lessard <guillaume.lessard@apple.com>
2024-12-20 18:04:31 -0800

    [stdlib] add some `storage` and `bytes` properties

---
+++ stdlib/public/core/Span/Properties/ArrayTypeExtensions.swift
@@ -0,0 +1,35 @@
+//===--- ArrayTypeExtensions.swift ----------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2024 - 2025 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+extension ContiguousArray {
+
+  @available(SwiftStdlib 6.1, *)
+  public var storage: Span<Element> {
+    get {
+      let (pointer, count) = (_buffer.firstElementAddress, _buffer.count)
+      let span = Span(_unsafeStart: pointer, count: count)
+      return _overrideLifetime(span, borrowing: self)
+    }
+  }
+}
+
+extension ArraySlice {
+
+  @available(SwiftStdlib 6.1, *)
+  public var storage: Span<Element> {
+    get {
+      let (pointer, count) = (_buffer.firstElementAddress, _buffer.count)
+      let span = Span(_unsafeStart: pointer, count: count)
+      return _overrideLifetime(span, borrowing: self)
+    }
+  }
+}
