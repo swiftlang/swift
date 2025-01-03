1a94e558cab97204b2e2b26847509adbf36d94c5
Henrik G. Olsson <h_olsson@apple.com>
2025-01-03 12:40:00 +0100

    use swift-ide-test for checking interop signatures

--- test/Interop/C/swiftify-import/sized-by.swift
+++ test/Interop/C/swiftify-import/sized-by.swift
@@ -1,52 +1,20 @@
 // REQUIRES: swift_feature_SafeInteropWrappers
 
+// RUN: %target-swift-ide-test -print-module -module-to-print=SizedByClang -plugin-path %swift-plugin-dir -I %S/Inputs -source-filename=x -enable-experimental-feature SafeInteropWrappers | %FileCheck %s
+
+// swift-ide-test doesn't currently typecheck the macro expansions, so run the compiler as well
 // RUN: %empty-directory(%t)
-// RUN: %target-swift-frontend -emit-module -disable-availability-checking -plugin-path %swift-plugin-dir -o %t/CountedBy.swiftmodule -I %S/Inputs -enable-experimental-feature SafeInteropWrappers %s
+// RUN: %target-swift-frontend -emit-module -plugin-path %swift-plugin-dir -o %t/CountedBy.swiftmodule -I %S/Inputs -enable-experimental-feature SafeInteropWrappers %s
 
 // Check that ClangImporter correctly infers and expands @_SwiftifyImport macros for functions with __sized_by parameters.
-
 import SizedByClang
 
-@inlinable
-public func callSimple(_ p: UnsafeMutableRawBufferPointer) {
-  simple(p)
-}
-
-// Check that macros from swift_attr work the same as inferred macros.
-@inlinable
-public func callSwiftAttr(_ p: UnsafeMutableRawBufferPointer) {
-  swiftAttr(p)
-}
-
-@inlinable
-public func callShared(_ len: CInt, _ p1: UnsafeMutableRawBufferPointer, _ p2: UnsafeMutableRawBufferPointer) {
-  shared(len, p1, p2)
-}
-
-@inlinable
-public func callComplexExpr(_ len: CInt, _ offset: CInt, _ p: UnsafeMutableRawBufferPointer) {
-  complexExpr(len, offset, p)
-}
-
-
-@inlinable
-public func callNullUnspecified(_ p: UnsafeMutableRawBufferPointer) {
-  nullUnspecified(p)
-}
-
-@inlinable
-public func callNonnull(_ p: UnsafeMutableRawBufferPointer) {
-  nonnull(p)
-}
-
-@inlinable
-public func callNullable(_ p: UnsafeMutableRawBufferPointer?) {
-  nullable(p)
-}
-
-@inlinable
-public func callOpaque(_ p: UnsafeRawBufferPointer) {
-  opaque(p)
-}
-
+// CHECK:      @_alwaysEmitIntoClient public func complexExpr(_ len: Int{{.*}}, _ offset: Int{{.*}}, _ p: UnsafeMutableRawBufferPointer)
+// CHECK-NEXT: @_alwaysEmitIntoClient public func nonnull(_  p: UnsafeMutableRawBufferPointer)
+// CHECK-NEXT: @_alwaysEmitIntoClient public func nullUnspecified(_  p: UnsafeMutableRawBufferPointer)
+// CHECK-NEXT: @_alwaysEmitIntoClient public func nullable(_  p: UnsafeMutableRawBufferPointer?)
+// CHECK-NEXT: @_alwaysEmitIntoClient public func opaque(_  p: UnsafeRawBufferPointer)
+// CHECK-NEXT: @_alwaysEmitIntoClient public func shared(_ len: Int{{.*}}, _ p1: UnsafeMutableRawBufferPointer, _ p2: UnsafeMutableRawBufferPointer)
+// CHECK-NEXT: @_alwaysEmitIntoClient public func simple(_  p: UnsafeMutableRawBufferPointer)
+// CHECK-NEXT: @_alwaysEmitIntoClient public func swiftAttr(_  p: UnsafeMutableRawBufferPointer)
 
