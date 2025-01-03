8cee7003691b4b7ea70a914afb3b64c81731c3c0
Henrik G. Olsson <h_olsson@apple.com>
2025-01-03 15:20:11 +0100

    fix NullableSpan test

--- test/Macros/SwiftifyImport/MacroErrors/NullableSpan.swift
+++ test/Macros/SwiftifyImport/MacroErrors/NullableSpan.swift
@@ -4,7 +4,7 @@
 // RUN: %target-typecheck-verify-swift -swift-version 5 -module-name main -disable-availability-checking -typecheck -plugin-path %swift-plugin-dir -verify -enable-experimental-feature Span
 
 // XFAIL: *
-// expanded form errors with "type 'RawSpan' does not conform to protocol 'Escapable'" because Optional doesn't support ~Escapable yet 
+// expanded form errors with "type 'Span' does not conform to protocol 'Escapable'" because Optional doesn't support ~Escapable yet 
 @_SwiftifyImport(.countedBy(pointer: 1, count: "len"), .nonescaping(pointer: 1))
-func nullableSpan(_ ptr: UnsafePointer<CInt>, _ len: CInt) {
+func nullableSpan(_ ptr: UnsafePointer<CInt>?, _ len: CInt) {
 }
