83731afcb9b6953d52578e326ebe61dc21b00be2
Stefan Gränitz <stefan.graenitz@gmail.com>
2025-01-08 15:06:35 +0100

    Reland "[windows][toolchain] Enable builtins and sanitizers"

--- utils/build.ps1
+++ utils/build.ps1
@@ -1794,6 +1794,21 @@ function Build-CURL([Platform]$Platform, $Arch) {
     })
 }
 
+function Build-Sanitizers([Platform]$Platform, $Arch, $InstallTo) {
+  Isolate-EnvVars {
+    # Use configured compilers
+    Build-CMakeProject `
+      -Src $SourceCache\llvm-project\runtimes `
+      -Bin "$(Get-HostProjectBinaryCache Compilers)\runtimes\runtimes-$($Arch.LLVMTarget)-bins" `
+      -InstallTo $InstallTo `
+      -Arch $Arch `
+      -Platform $Platform `
+      -Defines (@{
+        COMPILER_RT_BUILD_SANITIZERS = "YES"
+      })
+  }
+}
+
 function Build-Runtime([Platform]$Platform, $Arch) {
   $PlatformDefines = @{}
   if ($Platform -eq "Android") {
@@ -2799,6 +2814,11 @@ if (-not $SkipBuild) {
   Invoke-BuildStep Build-CMark $HostArch
   Invoke-BuildStep Build-XML2 Windows $HostArch
   Invoke-BuildStep Build-Compilers $HostArch
+
+  $InstallTo = "$($HostArch.ToolchainInstallRoot)\usr"
+  foreach ($Arch in $WindowsSDKArchs) {
+    Invoke-BuildStep Build-Sanitizers Windows $Arch $InstallTo
+  }
 }
 
 if ($Clean) {
