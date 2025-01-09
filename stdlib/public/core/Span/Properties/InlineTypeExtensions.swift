b36e0761423b5b4790fe1032be35dc9a0d878502
Guillaume Lessard <guillaume.lessard@apple.com>
2025-01-08 17:51:40 -0800

    [stdlib] add storage properties to SIMD_N_

--- stdlib/public/core/Span/Properties/InlineTypeExtensions.swift
+++ stdlib/public/core/Span/Properties/InlineTypeExtensions.swift
@@ -14,9 +14,109 @@ extension CollectionOfOne {
 
   @available(SwiftStdlib 6.1, *)
   public var storage: Span<Element> {
-    get {
+    @_alwaysEmitIntoClient
+    @lifetime(borrow self)
+    borrowing get {
       let pointer = UnsafePointer<Element>(Builtin.addressOfBorrow(self))
       let span = Span(_unsafeStart: pointer, count: 1)
+      return _overrideLifetime(span, borrowing: self)
+    }
+  }
+}
+
+extension SIMD2 {
+
+  @available(SwiftStdlib 6.1, *)
+  public var storage: Span<Scalar> {
+    @_alwaysEmitIntoClient
+    @lifetime(borrow self)
+    borrowing get {
+      let pointer = UnsafePointer<Scalar>(Builtin.addressOfBorrow(self))
+      let span = Span(_unsafeStart: pointer, count: 2)
+      return _overrideLifetime(span, borrowing: self)
+    }
+  }
+}
+
+extension SIMD3 {
+
+  @available(SwiftStdlib 6.1, *)
+  public var storage: Span<Scalar> {
+    @_alwaysEmitIntoClient
+    @lifetime(borrow self)
+    borrowing get {
+      let pointer = UnsafePointer<Scalar>(Builtin.addressOfBorrow(self))
+      let span = Span(_unsafeStart: pointer, count: 3)
+      return _overrideLifetime(span, borrowing: self)
+    }
+  }
+}
+
+extension SIMD4 {
+
+  @available(SwiftStdlib 6.1, *)
+  public var storage: Span<Scalar> {
+    @_alwaysEmitIntoClient
+    @lifetime(borrow self)
+    borrowing get {
+      let pointer = UnsafePointer<Scalar>(Builtin.addressOfBorrow(self))
+      let span = Span(_unsafeStart: pointer, count: 4)
+      return _overrideLifetime(span, borrowing: self)
+    }
+  }
+}
+
+extension SIMD8 {
+
+  @available(SwiftStdlib 6.1, *)
+  public var storage: Span<Scalar> {
+    @_alwaysEmitIntoClient
+    @lifetime(borrow self)
+    borrowing get {
+      let pointer = UnsafePointer<Scalar>(Builtin.addressOfBorrow(self))
+      let span = Span(_unsafeStart: pointer, count: 8)
+      return _overrideLifetime(span, borrowing: self)
+    }
+  }
+}
+
+extension SIMD16 {
+
+  @available(SwiftStdlib 6.1, *)
+  public var storage: Span<Scalar> {
+    @_alwaysEmitIntoClient
+    @lifetime(borrow self)
+    borrowing get {
+      let pointer = UnsafePointer<Scalar>(Builtin.addressOfBorrow(self))
+      let span = Span(_unsafeStart: pointer, count: 16)
+      return _overrideLifetime(span, borrowing: self)
+    }
+  }
+}
+
+extension SIMD32 {
+
+  @available(SwiftStdlib 6.1, *)
+  public var storage: Span<Scalar> {
+    @_alwaysEmitIntoClient
+    @lifetime(borrow self)
+    borrowing get {
+      let pointer = UnsafePointer<Scalar>(Builtin.addressOfBorrow(self))
+      let span = Span(_unsafeStart: pointer, count: 32)
+      return _overrideLifetime(span, borrowing: self)
+    }
+  }
+}
+
+extension SIMD64 {
+
+  @available(SwiftStdlib 6.1, *)
+  public var storage: Span<Scalar> {
+    @_alwaysEmitIntoClient
+    @lifetime(borrow self)
+    borrowing get {
+      let pointer = UnsafePointer<Scalar>(Builtin.addressOfBorrow(self))
+      let span = Span(_unsafeStart: pointer, count: 64)
       return _overrideLifetime(span, borrowing: self)
     }
   }
