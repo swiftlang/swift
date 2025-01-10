077de53def08ac0b95a95ad09000ca68c9e02c4c
Guillaume Lessard <guillaume.lessard@apple.com>
2025-01-09 16:24:07 -0800

    [stdlib] add storage property to `Substring.UTF8View`

--- test/stdlib/Span/StringUTF8StorageProperty.swift
+++ test/stdlib/Span/StringUTF8StorageProperty.swift
@@ -58,3 +58,45 @@ suite.test("Span from Large Native String")
     expectEqual(span[i], u[i])
   }
 }
+
+suite.test("Span from Small String's Substring")
+.skip(.custom(
+  { if #available(SwiftStdlib 6.1, *) { false } else { true } },
+  reason: "Requires Swift 6.1's standard library"
+))
+.code {
+  guard #available(SwiftStdlib 6.1, *) else { return }
+
+  let s = "A small string.".dropFirst(8).utf8
+  let u = Array("string.".utf8)
+  let span = s.storage
+
+  let count = span.count
+  expectEqual(count, s.count)
+
+  for i in span.indices {
+    expectEqual(span[i], u[i])
+  }
+}
+
+suite.test("Span from Large Native String's Substring")
+.skip(.custom(
+  { if #available(SwiftStdlib 6.1, *) { false } else { true } },
+  reason: "Requires Swift 6.1's standard library"
+))
+.code {
+  guard #available(SwiftStdlib 6.1, *) else { return }
+
+  let t = "A long string that is altogether not smol."
+  let s = t.dropFirst(22).prefix(10).utf8
+  let u = Array("altogether".utf8)
+  let span = s.storage
+
+  let count = span.count
+  expectEqual(count, s.count)
+
+  for i in span.indices {
+    expectEqual(span[i], u[i])
+  }
+}
+
