3a8ed89dfa816b4bd71051948fa004a2f2c9d88a
Egor Zhdan <e_zhdan@apple.com>
2025-01-15 18:14:31 +0000

    [cxx-interop] Fix a failing test
    The API that is being used is not available on all versions of all platforms.
    
    rdar://142973844

--- test/Interop/Cxx/objc-correctness/os-logger.swift
+++ test/Interop/Cxx/objc-correctness/os-logger.swift
@@ -1,7 +1,7 @@
 // RUN: %target-swift-frontend -typecheck -verify -I %S/Inputs -cxx-interoperability-mode=default %s
 
 // REQUIRES: objc_interop
-// REQUIRES: VENDOR=apple
+// REQUIRES: OS=macosx
 
 import os
 
