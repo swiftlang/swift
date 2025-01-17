174f0e4c686c9113d22487a642768972f4a2c012
Konrad `ktoso` Malawski <konrad_malawski@apple.com>
2025-01-17 14:27:01 +0900

    [Distributed] Fix IRGen accessor emitting for distributed properties with protocols
    resolves rdar://142761605

--- lib/AST/ASTMangler.cpp
+++ lib/AST/ASTMangler.cpp
@@ -4408,8 +4408,6 @@ void ASTMangler::appendDistributedThunk(
                                                        false);
   // TODO: add a flag to skip class/struct information from parameter types
 
-  BaseEntitySignature base(thunk);
-
   // Since computed property SILDeclRef's refer to the "originator"
   // of the thunk, we need to mangle distributed thunks of accessors
   // specially.
@@ -4431,6 +4429,8 @@ void ASTMangler::appendDistributedThunk(
     thunk = storage->getDistributedThunk();
     assert(thunk);
   }
+
+  BaseEntitySignature base(thunk);
   assert(isa<AbstractFunctionDecl>(thunk) &&
          "distributed thunk to mangle must be function decl");
   assert(thunk->getContextKind() == DeclContextKind::AbstractFunctionDecl);
