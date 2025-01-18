720240254bf10b2aef362d811791ea113664a16a
Doug Gregor <dgregor@apple.com>
2025-01-18 13:11:15 -0800

    Enusre that we cannot infinite-loop when finding expression parent

--- lib/Sema/TypeCheckEffects.cpp
+++ lib/Sema/TypeCheckEffects.cpp
@@ -3168,11 +3168,15 @@ class CheckEffectsCoverage : public EffectsHandlingWalker<CheckEffectsCoverage> 
   /// Find the top location where we should put the await
   static Expr *walkToAnchor(Expr *e, llvm::DenseMap<Expr *, Expr *> &parentMap,
                             bool isInterpolatedString) {
+    llvm::SmallPtrSet<Expr *, 4> visited;
     Expr *parent = e;
     Expr *lastParent = e;
     while (parent && !isEffectAnchor(parent)) {
       lastParent = parent;
       parent = parentMap[parent];
+
+      if (!visited.insert(parent).second)
+        break;
     }
 
     if (parent && !isAnchorTooEarly(parent)) {
