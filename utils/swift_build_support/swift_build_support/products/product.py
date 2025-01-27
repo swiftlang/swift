07f3af120fb25fe5157fce9231fe7c67fd51d5bb
Max Desiatov <m_desiatov@apple.com>
2025-01-27 20:20:01 +0000

    Merge pull request #78909 from xtremekforever/#75341-fix-swift-build-support-typo
    Fix typo in get_linux_sysroot that makes cross-compilation fail

--- utils/swift_build_support/swift_build_support/products/product.py
+++ utils/swift_build_support/swift_build_support/products/product.py
@@ -381,7 +381,7 @@ class Product(object):
             return self.args.cross_compile_sysroot
         if not self.is_cross_compile_target('{}-{}'.format(platform, arch)):
             return None
-        sysroot_arch, abi = self.get_linux_target_components(arch)
+        sysroot_arch, _, abi = self.get_linux_target_components(arch)
         # $ARCH-$PLATFORM-$ABI
         # E.x.: aarch64-linux-gnu
         sysroot_dirname = '{}-{}-{}'.format(sysroot_arch, platform, abi)
