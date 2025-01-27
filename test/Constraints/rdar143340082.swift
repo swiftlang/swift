swiftlang/swift

commit 0c128e5db78068573bf9ca382ab51c8cad79280a

Sema: Optimize ConstraintGraph::computeConnectedComponents()
Instead of starting a depth-first search from each type variable
and marking all type variables that haven't been marked yet,
we can implement this as a union-find.

We can also store the temporary state directly inside the
TypeVariableType::Implementation, instead of creating large
DenseMaps whose keys range over all type variables.

lib/Sema/ConstraintGraph.cpp
@@ -721,19 +721,9 @@ namespace {
     ConstraintGraph &cg;
     ArrayRef<TypeVariableType *> typeVars;
 
-    /// A mapping from each type variable to its representative in a union-find
-    /// data structure, excluding entries where the type variable is its own
-    /// representative.
-    mutable llvm::SmallDenseMap<TypeVariableType *, TypeVariableType *>
-        representatives;
-
-    // Figure out which components have unbound type variables and/or
-    // constraints. These are the only components we want to report.
-    llvm::SmallDenseSet<TypeVariableType *> validComponents;
-
-    /// The complete set of constraints that were visited while computing
-    /// connected components.
-    llvm::SmallPtrSet<Constraint *, 8> visitedConstraints;
+    /// The number of connected components discovered so far. Decremented when
+    /// we merge equivalence classes.
+    unsigned validComponentCount = 0;
 
     /// Describes the one-way incoming and outcoming adjacencies of
     /// a component within the directed graph of one-way constraints.
@@ -776,10 +766,9 @@ namespace {
       // The final return value.
       SmallVector<Component, 1> flatComponents;
 
-
       // We don't actually need to partition the graph into components if
       // there are fewer than 2.
-      if (validComponents.size() < 2 && cg.getOrphanedConstraints().empty())
+      if (validComponentCount < 2 && cg.getOrphanedConstraints().empty())
         return flatComponents;
 
       // Mapping from representatives to components.
@@ -790,8 +779,8 @@ namespace {
       for (auto typeVar : typeVars) {
         // Find the representative. If we aren't creating a type variable
         // for this component, skip it.
-        auto rep = findRepresentative(typeVar);
-        if (validComponents.count(rep) == 0)
+        auto rep = typeVar->getImpl().getComponent();
+        if (!rep->getImpl().isValidComponent())
           continue;
 
         auto pair = components.insert({rep, Component(components.size())});
@@ -829,7 +818,7 @@ namespace {
           typeVar = constraintTypeVars.front();
         }
 
-        auto rep = findRepresentative(typeVar);
+        auto rep = typeVar->getImpl().getComponent();
         getComponent(rep).addConstraint(&constraint);
       }
 
@@ -857,7 +846,7 @@ namespace {
           auto &oneWayComponent = knownOneWayComponent->second;
           auto &component = getComponent(typeVar);
           for (auto inAdj : oneWayComponent.inAdjacencies) {
-            if (validComponents.count(inAdj) == 0)
+            if (!inAdj->getImpl().isValidComponent())
               continue;
 
             component.recordDependency(getComponent(inAdj));
@@ -899,23 +888,6 @@ namespace {
       return flatComponents;
     }
 
-    /// Find the representative for the given type variable within the set
-    /// of representatives in a union-find data structure.
-    TypeVariableType *findRepresentative(TypeVariableType *typeVar) const {
-      // If we don't have a record of this type variable, it is it's own
-      // representative.
-      auto known = representatives.find(typeVar);
-      if (known == representatives.end() || known->second == typeVar)
-        return typeVar;
-
-      // Find the representative of the parent.
-      auto parent = known->second;
-      auto rep = findRepresentative(parent);
-      representatives[typeVar] = rep;
-
-      return rep;
-    }
-
   private:
     /// Perform the union of two type variables in a union-find data structure
     /// used for connected components.
@@ -923,20 +895,27 @@ namespace {
     /// \returns true if the two components were separate and have now been
     /// joined, \c false if they were already in the same set.
     bool unionSets(TypeVariableType *typeVar1, TypeVariableType *typeVar2) {
-      auto rep1 = findRepresentative(typeVar1);
-      auto rep2 = findRepresentative(typeVar2);
+      auto rep1 = typeVar1->getImpl().getComponent();
+      auto rep2 = typeVar2->getImpl().getComponent();
       if (rep1 == rep2)
         return false;
 
       // Reparent the type variable with the higher ID. The actual choice doesn't
       // matter, but this makes debugging easier.
-      if (rep1->getID() < rep2->getID()) {
-        validComponents.erase(rep2);
-        representatives[rep2] = rep1;
-      } else {
-        validComponents.erase(rep1);
-        representatives[rep1] = rep2;
+      if (rep1->getID() > rep2->getID())
+        std::swap(rep1, rep2);
+
+      if (rep2->getImpl().isValidComponent()) {
+        // If both are valid components, decrement the valid component counter
+        // by one. Otherwise, propagate the valid component flag.
+        if (!rep1->getImpl().markValidComponent()) {
+          ASSERT(validComponentCount > 0);
+          --validComponentCount;
+        }
       }
+
+      rep2->getImpl().setComponent(rep1);
+
       return true;
     }
 
@@ -949,51 +928,49 @@ namespace {
 
       auto &cs = cg.getConstraintSystem();
 
-      // Perform a depth-first search from each type variable to identify
-      // what component it is in.
       for (auto typeVar : typeVars) {
-        // If we've already assigned a representative to this type variable,
-        // we're done.
-        if (representatives.count(typeVar) > 0)
-          continue;
+        auto &impl = typeVar->getImpl();
+        if (auto *rep = impl.getRepresentativeOrFixed().dyn_cast<TypeVariableType *>()) {
+          impl.setComponent(rep);
+          if (typeVar == rep) {
+            if (impl.markValidComponent())
+              ++validComponentCount;
+          }
+        } else {
+          impl.setComponent(typeVar);
+        }
+      }
 
-        // Perform a depth-first search to mark those type variables that are
-        // in the same component as this type variable.
-        depthFirstSearch(
-            cg, typeVar,
-            [&](TypeVariableType *found) {
-              // If we have already seen this node, we're done.
-              auto inserted = representatives.insert({found, typeVar});
-              assert((inserted.second || inserted.first->second == typeVar) &&
-                     "Wrong component?");
-
-              if (inserted.second)
-                if (!cs.getFixedType(found))
-                  validComponents.insert(typeVar);
-
-              return inserted.second;
-            },
-            [&](Constraint *constraint) {
-              // Record and skip one-way constraints.
-              if (constraint->isOneWayConstraint()) {
-                oneWayConstraints.push_back(constraint);
-                return false;
-              }
-
-              return true;
-            },
-            visitedConstraints);
+      for (auto typeVar : typeVars) {
+        auto &impl = typeVar->getImpl();
+        if (auto fixedType = impl.getRepresentativeOrFixed().dyn_cast<TypeBase *>()) {
+          auto &node = cg[typeVar];
+          for (auto otherTypeVar : node.getReferencedVars()) {
+            unionSets(typeVar, otherTypeVar);
+          }
+        }
       }
 
       for (auto &constraint : cs.getConstraints()) {
-        if (constraint.getKind() == ConstraintKind::Disjunction ||
-            constraint.getKind() == ConstraintKind::Conjunction) {
-          for (auto typeVar : constraint.getTypeVariables()) {
-            auto rep = findRepresentative(typeVar);
-            if (validComponents.insert(rep).second)
-              ASSERT(cs.getFixedType(typeVar));
-          }
+        if (constraint.isOneWayConstraint()) {
+          oneWayConstraints.push_back(&constraint);
+          auto *typeVar = constraint.getFirstType()->castTo<TypeVariableType>();
+          typeVar = typeVar->getImpl().getComponent();
+          if (typeVar->getImpl().markValidComponent())
+            ++validComponentCount;
+          continue;
         }
+
+        auto typeVars = constraint.getTypeVariables();
+        if (typeVars.empty())
+          continue;
+
+        auto *firstTypeVar = typeVars[0]->getImpl().getComponent();
+        if (firstTypeVar->getImpl().markValidComponent())
+          ++validComponentCount;
+
+        for (auto *otherTypeVar : typeVars.slice(1))
+          unionSets(firstTypeVar, otherTypeVar);
       }
 
       return oneWayConstraints;
@@ -1016,7 +993,7 @@ namespace {
       SmallPtrSet<TypeVariableType *, 2> typeVars;
       type->getTypeVariables(typeVars);
       for (auto typeVar : typeVars) {
-        auto rep = findRepresentative(typeVar);
+        auto rep = typeVar->getImpl().getComponent();
         insertIfUnique(results, rep);
       }
 
@@ -1226,7 +1203,7 @@ namespace {
             rep,
             [&](TypeVariableType *typeVar) -> ArrayRef<TypeVariableType *> {
               // Traverse the outgoing adjacencies for the subcomponent
-              assert(typeVar == findRepresentative(typeVar));
+              assert(typeVar == typeVar->getImpl().getComponent());
               auto oneWayComponent = oneWayDigraph.find(typeVar);
               if (oneWayComponent == oneWayDigraph.end()) {
                 return { };
@@ -1240,7 +1217,7 @@ namespace {
             [&](TypeVariableType *typeVar) {
               // Record this type variable, if it's one of the representative
               // type variables.
-              if (validComponents.count(typeVar) > 0)
+              if (typeVar->getImpl().isValidComponent())
                 orderedReps.push_back(typeVar);
             });
       }

include/swift/Sema/ConstraintSystem.h
@@ -39,6 +39,7 @@
 #include "swift/Sema/SolutionResult.h"
 #include "swift/Sema/SyntacticElementTarget.h"
 #include "llvm/ADT/MapVector.h"
+#include "llvm/ADT/PointerIntPair.h"
 #include "llvm/ADT/PointerUnion.h"
 #include "llvm/ADT/STLExtras.h"
 #include "llvm/ADT/SetOperations.h"
@@ -339,17 +340,17 @@ class TypeVariableType::Implementation {
   /// The corresponding node in the constraint graph.
   constraints::ConstraintGraphNode *GraphNode = nullptr;
 
+  /// Temporary state for ConstraintGraph::computeConnectedComponents(),
+  /// stored inline for performance.
+  llvm::PointerIntPair<TypeVariableType *, 1, unsigned> Component;
+
   friend class constraints::SolverTrail;
 
 public:
   /// Retrieve the type variable associated with this implementation.
-  TypeVariableType *getTypeVariable() {
-    return reinterpret_cast<TypeVariableType *>(this) - 1;
-  }
-
-  /// Retrieve the type variable associated with this implementation.
-  const TypeVariableType *getTypeVariable() const {
-    return reinterpret_cast<const TypeVariableType *>(this) - 1;
+  TypeVariableType *getTypeVariable() const {
+    return reinterpret_cast<TypeVariableType *>(
+        const_cast<Implementation *>(this)) - 1;
   }
 
   explicit Implementation(constraints::ConstraintLocator *locator,
@@ -413,6 +414,12 @@ public:
     // Check whether the representative is different from our own type
     // variable.
     return ParentOrFixed.get<TypeVariableType *>() != getTypeVariable();
+  }
+
+  /// Low-level accessor; use getRepresentative() or getFixedType() instead.
+  llvm::PointerUnion<TypeVariableType *, TypeBase *>
+  getRepresentativeOrFixed() const {
+    return ParentOrFixed;
   }
 
   /// Record the current type-variable binding.
@@ -630,6 +637,37 @@ public:
       impl.recordBinding(*trail);
 
     impl.getTypeVariable()->Bits.TypeVariableType.Options |= TVO_CanBindToHole;
+  }
+
+  void setComponent(TypeVariableType *parent) {
+    Component.setPointerAndInt(parent, /*valid=*/false);
+  }
+
+  TypeVariableType *getComponent() const {
+    auto *rep = getTypeVariable();
+    while (rep != rep->getImpl().Component.getPointer())
+      rep = rep->getImpl().Component.getPointer();
+
+    // Path compression
+    if (rep != getTypeVariable()) {
+      const_cast<TypeVariableType::Implementation *>(this)
+          ->Component.setPointer(rep);
+    }
+
+    return rep;
+  }
+
+  bool isValidComponent() const {
+    ASSERT(Component.getPointer() == getTypeVariable());
+    return Component.getInt();
+  }
+
+  bool markValidComponent() {
+    if (Component.getInt())
+      return false;
+    ASSERT(Component.getPointer() == getTypeVariable());
+    Component.setInt(1);
+    return true;
   }
 
   /// Print the type variable to the given output stream.

commit 881c4f775b75a5150e6ce3f3d6dcf7ac54859142

Sema: Ignore inactive type variables in addTypeVariableConstraintsToWorkList()
Fixes a regression from commit 0c128e5db78068573bf9ca382ab51c8cad79280a.

The old depthFirstSearch() walked all adjacencies via the constraint graph,
and thus it would visit type variables that are currently inactive because
we're solving a conjunction element.

This was inconsistent with the new union-find which only formed the
connected components from the currently active type variables; adjacencies
involving inactive type variables are no longer considered.

Fix the inconsistency by changing gatherConstraints(), which used from
addTypeVariableConstraintsToWorkList(), to also skip inactive type
variables.

Fixes rdar://problem/143340082.

lib/Sema/CSSolver.cpp
@@ -1946,6 +1946,17 @@ bool ConjunctionElement::attempt(ConstraintSystem &cs) const {
     llvm::SmallPtrSet<TypeVariableType *, 4> referencedVars;
     findReferencedVariables(cs, referencedVars);
 
+    if (cs.isDebugMode()) {
+      auto indent = cs.solverState->getCurrentIndent();
+      auto &log = llvm::errs().indent(indent);
+      log << "(Element type variables in scope: ";
+      interleave(
+        referencedVars,
+        [&](TypeVariableType *typeVar) { log << "$T" << typeVar->getID(); },
+        [&] { log << ", "; });
+      log << ")\n";
+    }
+
     for (auto *typeVar : referencedVars)
       cs.addTypeVariable(typeVar);
   }

lib/Sema/ConstraintGraph.cpp
@@ -565,6 +565,11 @@ static void depthFirstSearch(
     llvm::function_ref<void(Constraint *)> visitConstraint,
     llvm::SmallPtrSet<TypeVariableType *, 4> &typeVars,
     llvm::SmallPtrSet<Constraint *, 8> &visitedConstraints) {
+  // If we're not looking at this type variable right now because we're
+  // solving a conjunction element, don't consider its adjacencies.
+  if (!cg.getConstraintSystem().isActiveTypeVariable(typeVar))
+    return;
+
   // Visit this node. If we've already seen it, bail out.
   if (!typeVars.insert(typeVar).second)
     return;

test/Constraints/rdar143340082.swift
@@ -1,0 +1,13 @@
+// RUN: %target-typecheck-verify-swift
+
+struct Test {
+    var v: String
+    var i: Int
+}
+
+do {
+    let _ = Array(1 ... 20).map { i in
+        _ = 0
+        return Test(v: "\(i * 1000)", i: 1)
+    }
+}