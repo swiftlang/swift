29aaec872a4c6d96a518b39f553b846f73799355
Allan Shortlidge <shortlidge@apple.com>
2025-01-26 18:47:05 -0800

    Sema: Always allow method overrides to be as available as the context.
    When a method override is as available as the class it's a member of, then it
    can't be any more available. It doesn't make sense to diagnose such a method as
    less available than the method it overrides. This regressed recently for
    methods belonging to classes that are nested inside extensions. The
    availability of the derived class may be defined by its context, but the
    compiler was only checking the availability attributes directly on the class.
    
    Resolves rdar://143600638.

--- lib/Sema/TypeCheckAvailability.cpp
+++ lib/Sema/TypeCheckAvailability.cpp
@@ -1537,6 +1537,12 @@ TypeChecker::availabilityAtLocation(SourceLoc loc, const DeclContext *DC,
   return availability;
 }
 
+AvailabilityContext
+TypeChecker::availabilityForDeclSignature(const Decl *decl) {
+  return TypeChecker::availabilityAtLocation(decl->getLoc(),
+                                             decl->getInnermostDeclContext());
+}
+
 AvailabilityRange TypeChecker::overApproximateAvailabilityAtLocation(
     SourceLoc loc, const DeclContext *DC,
     const AvailabilityScope **MostRefined) {
