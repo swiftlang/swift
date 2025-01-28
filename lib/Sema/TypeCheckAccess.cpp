6ca1547bbc985a44587b694ea73b299505594963
Egor Zhdan <e_zhdan@apple.com>
2025-01-28 16:22:17 +0000

    [cxx-interop] Diagnose resilience for C++ types declared within namespaces correctly
    C++ types that are declared within namespaces are imported into a fake __ObjC module in Swift. This is meant to be a workaround for namespaces spanning across multiple Clang modules, which Swift decls normally cannot do.
    
    This change fixes two issues:
    1. If a C++ type is declared within a namespace, the type isn't compatible with resilience, and it is used in a public resilient Swift interface, the compiler would emit an incorrect warning that mentioned the fake __ObjC module instead of the actual Clang module.
    2. If the declaration is in an overlay for another module, the compiler would emit an error, while we actually consider this kind of usage to be safe.
    
    rdar://143631944

--- lib/Sema/TypeCheckAccess.cpp
+++ lib/Sema/TypeCheckAccess.cpp
@@ -1939,7 +1939,7 @@ swift::getDisallowedOriginKind(const Decl *decl,
                                const ExportContext &where,
                                DowngradeToWarning &downgradeToWarning) {
   downgradeToWarning = DowngradeToWarning::No;
-  ModuleDecl *M = decl->getModuleContext();
+  ModuleDecl *M = decl->getModuleContextForNameLookup();
   auto *SF = where.getDeclContext()->getParentSourceFile();
 
   RestrictedImportKind howImported = SF->getRestrictedImportKind(M);
@@ -2040,7 +2040,7 @@ swift::getDisallowedOriginKind(const Decl *decl,
       where.getDeclContext()->getAsDecl()->getModuleContext()->isResilient() &&
       !where.getDeclContext()
            ->getAsDecl()
-           ->getModuleContext()
+           ->getModuleContextForNameLookup()
            ->getUnderlyingModuleIfOverlay() &&
       decl->hasClangNode() && !decl->getModuleContext()->isSwiftShimsModule() &&
       isFragileClangNode(decl->getClangNode()) &&
