65ac75fae5d70c3ca482148e7905e6142539335d
stzn <sztk1209@gmail.com>
2025-01-30 19:48:27 +0900

    [Sema]Set IsSending flag to subscript.getter if needed

--- lib/AST/Decl.cpp
+++ lib/AST/Decl.cpp
@@ -10607,6 +10607,9 @@ AccessorDecl *AccessorDecl::createParsed(
       // The cloned parameter is implicit.
       param->setImplicit();
 
+      if (subscriptParam->isSending())
+        param->setSending();
+
       newParams.push_back(param);
     }
   }
