45d4648bdb9e1a9f1e3205556e653e203c55c06a
Adrian Prantl <aprantl@apple.com>
2025-01-30 17:25:23 -0800

    Revert "[Debug info] Emit bound generic class type parameters when emitting AST types"
    This reverts commit f2132890db531a91864564b6a51c7b0810a89094
    it triggers the debug info sanity check when running the source compatibility testsuite.

--- test/DebugInfo/typealias_indirect.swift
+++
@@ -1,8 +0,0 @@
-// RUN: %target-swift-frontend %s -emit-ir -parse-as-library -module-name a -g -o - | %FileCheck %s
-// CHECK: !DIDerivedType(tag: DW_TAG_typedef, name: "$s1a10LocalAliasaD", {{.*}}baseType: ![[BASETY:[0-9]+]]
-// CHECK: ![[BASETY]]{{.*}}$sSbD
-public class MyClass<A, B> {}
-public typealias LocalAlias = Bool
-public typealias ClassAlias = MyClass<LocalAlias, Bool>
-public func use(cls: ClassAlias?) {}
-
