fa73be241531253ac9ae6a6892a8b8df5b379e56
Slava Pestov <spestov@apple.com>
2025-02-01 23:55:16 -0500

    Revert "Sema: Remove one-way constraint handling from computeConnectedComponents()"
    This reverts commit dd3e49c3ac29701713b5e6a74571cba94c3add43.

--- include/swift/Sema/Constraint.h
+++ include/swift/Sema/Constraint.h
@@ -820,6 +820,11 @@ public:
   /// from the rest of the constraint system.
   bool isIsolated() const { return IsIsolated; }
 
+  /// Whether this is a one-way constraint.
+  bool isOneWayConstraint() const {
+    return false;
+  }
+
   /// Retrieve the overload choice for an overload-binding constraint.
   OverloadChoice getOverloadChoice() const {
     assert(Kind == ConstraintKind::BindOverload);
