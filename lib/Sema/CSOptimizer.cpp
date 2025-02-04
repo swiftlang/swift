71f282aa658c34b8af39b6d5c8de94afbe64ea9b
Pavel Yaskevich <pyaskevich@apple.com>
2025-02-04 00:21:48 -0800

    Revert "[CSOptimizer] Keep track of mismatches while evaluating candidates"
    This reverts commit a094c3ebb061ba25e501ade2e6fd010043666a39.

--- lib/Sema/CSOptimizer.cpp
+++ lib/Sema/CSOptimizer.cpp
@@ -16,7 +16,6 @@
 
 #include "swift/Sema/ConstraintGraph.h"
 #include "swift/Sema/ConstraintSystem.h"
-#include "llvm/ADT/BitVector.h"
 #include "llvm/ADT/DenseMap.h"
 #include "llvm/ADT/SmallVector.h"
 #include "llvm/ADT/TinyPtrVector.h"
@@ -232,45 +231,20 @@ static void determineBestChoicesInContext(
             if (paramType->is<FunctionType>())
               continue;
 
-            if (candidateArgumentTypes[i].empty())
-              continue;
-
-            // The idea here is to match the parameter type against
-            // all of the argument candidate types and pick the best
-            // match (i.e. exact equality one).
-            //
-            // If none of the candidates match exactly and they are
-            // all bound concrete types, we consider this is mismatch
-            // at this parameter position and remove the overload choice
-            // from consideration.
-
-            double bestCandidateScore = 0;
-            llvm::BitVector mismatches(candidateArgumentTypes[i].size());
-
-            for (unsigned candidateIdx : indices(candidateArgumentTypes[i])) {
-              // If one of the candidates matched exactly there is no reason
-              // to continue checking.
-              if (bestCandidateScore == 1)
-                break;
-
-              Type candidateType;
-              bool isLiteralDefault;
-
-              std::tie(candidateType, isLiteralDefault) =
-                  candidateArgumentTypes[i][candidateIdx];
+            double argScore = 0.0;
+            for (auto const &candidate : candidateArgumentTypes[i]) {
+              auto candidateType = candidate.first;
 
               // `inout` parameter accepts only l-value argument.
-              if (paramFlags.isInOut() && !candidateType->is<LValueType>()) {
-                mismatches.set(candidateIdx);
+              if (paramFlags.isInOut() && !candidateType->is<LValueType>())
                 continue;
-              }
 
               // The specifier only matters for `inout` check.
               candidateType = candidateType->getWithoutSpecifierType();
               // Exact match on one of the candidate bindings.
               if (candidateType->isEqual(paramType)) {
-                bestCandidateScore =
-                    std::max(bestCandidateScore, isLiteralDefault ? 0.3 : 1.0);
+                argScore = std::max(
+                    argScore, /*fromLiteral=*/candidate.second ? 0.3 : 1.0);
                 continue;
               }
 
@@ -279,23 +253,12 @@ static void determineBestChoicesInContext(
               // and expect a particular `Scalar` if it's known.
               if (isSIMDType(candidateType) &&
                   isArithmeticOperatorOnSIMDProtocol(decl)) {
-                bestCandidateScore = 1.0;
+                argScore = std::max(argScore, 1.0);
                 continue;
               }
-
-              // Only established arguments could be considered mismatches,
-              // literal default types should be regarded as holes if they
-              // didn't match.
-              if (!isLiteralDefault && !candidateType->hasTypeVariable())
-                mismatches.set(candidateIdx);
             }
 
-            // If none of the candidates for this parameter matched, let's
-            // drop this overload from any further consideration.
-            if (mismatches.all())
-              return;
-
-            score += bestCandidateScore;
+            score += argScore;
           }
 
           // Average the score to avoid disfavoring disjunctions with fewer
