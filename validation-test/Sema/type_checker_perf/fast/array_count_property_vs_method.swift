51ab75d00fd079c5dee99e50bf4948ff7ea76373
Pavel Yaskevich <pyaskevich@apple.com>
2025-02-04 00:06:37 -0800

    Revert "[CSOptimizer] Extend candidate/parameter matching to support array literals"
    This reverts commit 8a304f88c6cf5ace86975297c885725362032517.

--- validation-test/Sema/type_checker_perf/fast/array_count_property_vs_method.swift
+++ validation-test/Sema/type_checker_perf/fast/array_count_property_vs_method.swift
@@ -1,6 +1,5 @@
-// RUN: %target-typecheck-verify-swift -solver-scope-threshold=11000
+// RUN: %target-typecheck-verify-swift -solver-expression-time-threshold=5
 // REQUIRES: tools-release,no_asan
-// REQUIRES: OS=macosx
 
 func f(n: Int, a: [Int]) {
   let _ = [(0 ..< n + a.count).map { Int8($0) }] +
