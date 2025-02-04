21bb23612ba0bbaa8ddf1da109c79d56d965b154
Pavel Yaskevich <pyaskevich@apple.com>
2025-02-04 00:20:37 -0800

    Revert "[CSOptimizer] Limit "old" behavior compatibility to unlabeled unary arguments"
    This reverts commit 9fb73143f6e6462bc9664a00fc00c72e9f70a241.

--- lib/Sema/CSOptimizer.cpp
+++ lib/Sema/CSOptimizer.cpp
@@ -181,15 +181,6 @@ static OverloadedDeclRefExpr *isOverloadedDeclRef(Constraint *disjunction) {
   return nullptr;
 }
 
-static unsigned numOverloadChoicesMatchingOnArity(OverloadedDeclRefExpr *ODRE,
-                                                  ArgumentList *arguments) {
-  return llvm::count_if(ODRE->getDecls(), [&arguments](auto *choice) {
-    if (auto *paramList = getParameterList(choice))
-      return arguments->size() == paramList->size();
-    return false;
-  });
-}
-
 /// This maintains an "old hack" behavior where overloads  of some
 /// `OverloadedDeclRef` calls were favored purely based on number of
 /// argument and (non-defaulted) parameters matching.
@@ -200,7 +191,11 @@ static void findFavoredChoicesBasedOnArity(
   if (!ODRE)
     return;
 
-  if (numOverloadChoicesMatchingOnArity(ODRE, argumentList) > 1)
+  if (llvm::count_if(ODRE->getDecls(), [&argumentList](auto *choice) {
+        if (auto *paramList = getParameterList(choice))
+          return argumentList->size() == paramList->size();
+        return false;
+      }) > 1)
     return;
 
   auto isVariadicGenericOverload = [&](ValueDecl *choice) {
@@ -636,16 +631,7 @@ static Constraint *determineBestChoicesInContext(
     double bestScore = 0.0;
     SmallVector<std::pair<Constraint *, double>, 2> favoredChoices;
 
-    // Preserves old behavior where, for unary calls, the solver
-    // would not consider choices that didn't match on the number
-    // of parameters (regardless of defaults) and only exact
-    // matches were favored.
-    bool preserveFavoringOfUnlabeledUnaryArgument = false;
-    if (argumentList->isUnlabeledUnary()) {
-      auto ODRE = isOverloadedDeclRef(disjunction);
-      preserveFavoringOfUnlabeledUnaryArgument =
-          !ODRE || numOverloadChoicesMatchingOnArity(ODRE, argumentList) < 2;
-    }
+    bool isOverloadedDeclRefDisjunction = isOverloadedDeclRef(disjunction);
 
     forEachDisjunctionChoice(
         cs, disjunction,
@@ -668,8 +654,11 @@ static Constraint *determineBestChoicesInContext(
           // matches to filter out non-default literal bindings which otherwise
           // could cause "over-favoring".
           bool favorExactMatchesOnly = onlyLiteralCandidates;
-
-          if (preserveFavoringOfUnlabeledUnaryArgument) {
+          // Preserves old behavior where for unary calls to members
+          // the solver would not consider choices that didn't match on
+          // the number of parameters (regardless of defaults) and only
+          // exact matches were favored.
+          if (!isOverloadedDeclRefDisjunction && argumentList->size() == 1) {
             // Old behavior completely disregarded the fact that some of
             // the parameters could be defaulted.
             if (overloadType->getNumParams() != 1)
