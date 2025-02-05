7a5c9573946c31c316e25488f0559dbdfc925e9c
Rintaro Ishizaki <rishizaki@apple.com>
2025-02-05 10:42:37 -0800

    [ASTGen] Fix several header comments

--- lib/ASTGen/Sources/ASTGen/Regex.swift
+++ lib/ASTGen/Sources/ASTGen/Regex.swift
@@ -1,4 +1,4 @@
-//===--- Regex.swift - SourceLoc bridiging utilities ------------------===//
+//===--- Regex.swift ------------------------------------------------------===//
 //
 // This source file is part of the Swift.org open source project
 //
