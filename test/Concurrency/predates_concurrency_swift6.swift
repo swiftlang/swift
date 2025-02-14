aa0b3561474fea3b424d31b7c70aae549c04d46b
Pavel Yaskevich <pyaskevich@apple.com>
2025-02-13 18:04:16 -0800

    [TypeChecker] Make sure that @preconcurrency use is detected in base expressions
    This expands https://github.com/swiftlang/swift/pull/77510 to base
    expressions of calls, subscripts and members to make sure that if
    the base has `@preconcurrency` declaration at some level. Otherwise,
    the availability checker won't appropriately downgrade unavailable
    `Sendable` conformances for `@preconcurrency` declarations.

--- test/Concurrency/predates_concurrency_swift6.swift
+++ test/Concurrency/predates_concurrency_swift6.swift
@@ -289,3 +289,22 @@ func testErasureDowngrade(ns: NotSendable, us: UnavailableSendable, c: C) {
     // expected-error@-1 {{conformance of 'UnavailableSendable' to 'Sendable' is unavailable}}
   }
 }
+
+// The member itself could be non-preconcurrency but the base could be.
+do {
+  @preconcurrency var d: [String: any Sendable] = [:]
+
+  let data: [String: Any] = [:]
+  d.merge(data, uniquingKeysWith: { _, rhs in rhs})
+  // expected-warning@-1 {{type 'Any' does not conform to the 'Sendable' protocol}}
+
+  struct Test {
+    @preconcurrency var info: [String: any Sendable] = [:]
+  }
+
+  func test(s: inout Test) {
+    s.info["hello"] = { }
+    // expected-warning@-1 {{type '() -> ()' does not conform to the 'Sendable' protocol}}
+    // expected-note@-2 {{a function type must be marked '@Sendable' to conform to 'Sendable'}}
+  }
+}
