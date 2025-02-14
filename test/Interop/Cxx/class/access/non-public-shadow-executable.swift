8652f1b45a74bb5e69cd3082aeee398b7cd66ef9
John Hui <j-hui@apple.com>
2025-02-13 22:13:31 -0800

    Add test case for shadowing of non-public members

---
+++ test/Interop/Cxx/class/access/non-public-shadow-executable.swift
@@ -0,0 +1,22 @@
+// Check that we are resolving the correct member for all unambiguous members
+// in the Shadow struct.
+
+// RUN: %target-run-simple-swift(-I %S/Inputs/ -Xfrontend -cxx-interoperability-mode=default)
+// REQUIRES: executable_test
+
+import StdlibUnittest
+import NonPublicShadow
+
+var Tests = TestSuite("NonPublicShadow")
+
+Tests.test("Check return values") {
+  var s = Shadow()
+
+  expectEqual(s.publOrPriv(), Return.Publ_publOrPriv)
+
+  expectEqual(s.publPublShadowed(), Return.Shadow_publPublShadowed)
+  expectEqual(s.protPublShadowed(), Return.Shadow_protPublShadowed)
+  expectEqual(s.privPublShadowed(), Return.Shadow_privPublShadowed)
+}
+
+runAllTests()
