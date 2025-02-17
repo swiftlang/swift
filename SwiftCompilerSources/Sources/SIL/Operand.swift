6874a61f87e9e0e97f2bbd3dfcf83fed76bf0e7d
Erik Eckstein <eeckstein@apple.com>
2025-02-17 09:14:31 +0100

    SIL: be more tolerant when a user is deleted during use-list iteration.
    Check if an operand's instruction has been deleted in `UseList.next()`.
    This allows to delete an instruction, which has two uses of a value, during use-list iteration.

--- SwiftCompilerSources/Sources/SIL/Operand.swift
+++ SwiftCompilerSources/Sources/SIL/Operand.swift
@@ -105,8 +105,16 @@ public struct UseList : CollectionLikeSequence {
     var currentOpPtr: OptionalBridgedOperand
     
     public mutating func next() -> Operand? {
-      if let op = currentOpPtr.operand {
-        currentOpPtr = op.getNextUse()
+      if let bridgedOp = currentOpPtr.operand {
+        var op = bridgedOp
+        // Skip operands of deleted instructions.
+        while op.isDeleted() {
+          guard let nextOp = op.getNextUse().operand else {
+            return nil
+          }
+          op = nextOp
+        }
+        currentOpPtr = op.getNextUse();
         return Operand(bridged: op)
       }
       return nil
