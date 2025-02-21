40244a2935ba024dcc0c37af89fa47f37c41721f
Alexis Laferrière <alaf@apple.com>
2025-02-21 11:43:00 -0800

    Frontend: Ignore resilient binary swiftmodules under usr/lib/swift
    Most SDKs use only swiftinterfaces under usr/lib/swift. Let's make sure
    we standardize this behavior and use only swiftinterface when they are
    present, even if there are also binary swiftmodule files available.
    
    Apply the same logic to SubFrameworks as well while we're at it.
    
    rdar://145316821

--- test/ModuleInterface/ignore-adjacent-swiftmodules.swift
+++ test/ModuleInterface/ignore-adjacent-swiftmodules.swift
@@ -3,21 +3,40 @@
 // REQUIRES: VENDOR=apple
 
 /// Prepare the SDK.
+//// Public framework
 // RUN: cp -r %S/../Sema/Inputs/public-private-sdk %t/sdk
 // RUN: %target-swift-frontend -emit-module -module-name PublicSwift -enable-library-evolution -swift-version 5 \
 // RUN:   %t/sdk/System/Library/Frameworks/PublicSwift.framework/Modules/PublicSwift.swiftmodule/source.swift \
 // RUN:   -o %t/sdk/System/Library/Frameworks/PublicSwift.framework/Modules/PublicSwift.swiftmodule/%target-swiftmodule-name \
 // RUN:   -emit-module-interface-path %t/sdk/System/Library/Frameworks/PublicSwift.framework/Modules/PublicSwift.swiftmodule/%target-swiftinterface-name
 // RUN: %target-swift-typecheck-module-from-interface(%t/sdk/System/Library/Frameworks/PublicSwift.framework/Modules/PublicSwift.swiftmodule/%target-swiftinterface-name) -module-name PublicSwift
+
+//// Private framework
 // RUN: %target-swift-frontend -emit-module -module-name PrivateSwift -enable-library-evolution -swift-version 5 \
 // RUN:   %t/sdk/System/Library/PrivateFrameworks/PrivateSwift.framework/Modules/PrivateSwift.swiftmodule/source.swift \
 // RUN:   -o %t/sdk/System/Library/PrivateFrameworks/PrivateSwift.framework/Modules/PrivateSwift.swiftmodule/%target-swiftmodule-name \
 // RUN:   -emit-module-interface-path %t/sdk/System/Library/PrivateFrameworks/PrivateSwift.framework/Modules/PrivateSwift.swiftmodule/%target-swiftinterface-name
 // RUN: %target-swift-typecheck-module-from-interface(%t/sdk/System/Library/PrivateFrameworks/PrivateSwift.framework/Modules/PrivateSwift.swiftmodule/%target-swiftinterface-name) -module-name PrivateSwift
 
+//// Public library
+// RUN: %target-swift-frontend -emit-module -module-name PublicSwiftLibrary -enable-library-evolution -swift-version 5 \
+// RUN:   %t/sdk/usr/lib/swift/PublicSwiftLibrary.swiftmodule/source.swift \
+// RUN:   -o %t/sdk/usr/lib/swift/PublicSwiftLibrary.swiftmodule/%target-swiftmodule-name \
+// RUN:   -emit-module-interface-path %t/sdk/usr/lib/swift/PublicSwiftLibrary.swiftmodule/%target-swiftinterface-name
+// RUN: %target-swift-typecheck-module-from-interface(%t/sdk/usr/lib/swift/PublicSwiftLibrary.swiftmodule/%target-swiftinterface-name) -module-name PublicSwiftLibrary
+
+//// Public subframework
+// RUN: %target-swift-frontend -emit-module -module-name SubSwift -enable-library-evolution -swift-version 5 \
+// RUN:   %t/sdk/System/Library/SubFrameworks/SubSwift.framework/Modules/SubSwift.swiftmodule/source.swift \
+// RUN:   -o %t/sdk/System/Library/SubFrameworks/SubSwift.framework/Modules/SubSwift.swiftmodule/%target-swiftmodule-name \
+// RUN:   -emit-module-interface-path %t/sdk/System/Library/SubFrameworks/SubSwift.framework/Modules/SubSwift.swiftmodule/%target-swiftinterface-name
+// RUN: %target-swift-typecheck-module-from-interface(%t/sdk/System/Library/SubFrameworks/SubSwift.framework/Modules/SubSwift.swiftmodule/%target-swiftinterface-name) -module-name SubSwift
+
 /// Break the swiftmodules.
 // RUN: echo "This is a malformed swiftmodule" > %t/sdk/System/Library/Frameworks/PublicSwift.framework/Modules/PublicSwift.swiftmodule/%target-swiftmodule-name
 // RUN: echo "This is a malformed swiftmodule" > %t/sdk/System/Library/PrivateFrameworks/PrivateSwift.framework/Modules/PrivateSwift.swiftmodule/%target-swiftmodule-name
+// RUN: echo "This is a malformed swiftmodule" > %t/sdk/System/Library/SubFrameworks/SubSwift.framework/Modules/SubSwift.swiftmodule/%target-swiftmodule-name
+// RUN: echo "This is a malformed swiftmodule" > %t/sdk/usr/lib/swift/PublicSwiftLibrary.swiftmodule/%target-swiftmodule-name
 
 /// There should be no attempt at loading the malformed PublicSwift swiftmodule.
 /// This means no notes about:
@@ -36,3 +55,9 @@ import PrivateSwift
 // expected-remark @-1 {{rebuilding module 'PrivateSwift' from interface}}
 // expected-note @-2 {{compiled module is out of date}}
 // expected-note @-3 {{: malformed}}
+
+import PublicSwiftLibrary // expected-remark {{rebuilding module 'PublicSwiftLibrary' from interface}}
+// expected-note @-1 {{was ignored because it's a library module in the SDK}}
+
+import SubSwift // expected-remark {{rebuilding module 'SubSwift' from interface}}
+// expected-note @-1 {{was ignored because it belongs to a framework in the SDK}}
