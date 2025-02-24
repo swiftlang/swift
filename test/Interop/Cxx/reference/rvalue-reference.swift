3e2bf788940dfe6c42dbc14de7e07e435e752446
Gabor Horvath <gaborh@apple.com>
2025-02-24 15:50:07 +0000

    [cxx-interop] Fix calling convention for rvalue reference params
    In C++, we always expected to invoke the dtor for moved-from objects.
    This is not the case for swift. Fortunately, @inCxx calling convention
    is already expressing that the caller supposed to destroy the object.
    This fixes the missing dtor calls when calling C++ functions taking
    rvalue references. Fixes #77894.
    
    rdar://140786022

---
+++ test/Interop/Cxx/reference/rvalue-reference.swift
@@ -0,0 +1,54 @@
+// RUN: %target-run-simple-swift(-I %S/Inputs/ -Xfrontend -enable-experimental-cxx-interop -Onone) | %FileCheck %s
+// RUN: %target-run-simple-swift(-I %S/Inputs/ -Xfrontend -enable-experimental-cxx-interop -O) | %FileCheck -check-prefix=CHECK-OPT %s
+//
+// REQUIRES: executable_test
+
+import SpecialMembers
+
+func consume(_ x: consuming MoveOnly) {}
+func consume(_ x: consuming Copyable) {}
+
+func main() {
+    let x = MoveOnly()
+// CHECK: MoveOnly 0 created
+// CHECK-OPT: MoveOnly 0 created
+    byRValueRef(consuming: x)
+// CHECK-NEXT: MoveOnly 1 move-created
+// CHECK-OPT-NEXT: MoveOnly 1 move-created
+// CHECK-NEXT: MoveOnly 0 destroyed
+// CHECK-OPT-NEXT: MoveOnly 0 destroyed
+// CHECK-NEXT: MoveOnly 1 destroyed
+// CHECK-OPT-NEXT: MoveOnly 1 destroyed
+    let x2 = MoveOnly()
+// CHECK-NEXT: MoveOnly 0 created
+// CHECK-OPT-NEXT: MoveOnly 0 created
+    consume(x2)
+// CHECK-NEXT: MoveOnly 1 move-created
+// CHECK-NEXT: MoveOnly 0 destroyed
+// CHECK-OPT-NEXT: MoveOnly 0 destroyed
+// CHECK-NEXT: MoveOnly 2 move-created
+// CHECK-NEXT: MoveOnly 1 destroyed
+// CHECK-NEXT: MoveOnly 2 destroyed
+    let x3 = Copyable()
+// CHECK-NEXT: Copyable 0 created
+// CHECK-OPT-NEXT: Copyable 0 created
+    byRValueRef(consuming: x3)
+// CHECK-NEXT: Copyable 1 copy-created
+// CHECK-OPT-NEXT: Copyable 1 copy-created
+// CHECK-NEXT: Copyable 1 destroyed
+// CHECK-OPT-NEXT: Copyable 1 destroyed
+    let x4 = Copyable()
+// CHECK-NEXT: Copyable 0 created
+// CHECK-OPT-NEXT: Copyable 0 created
+    consume(x4)
+// CHECK-NEXT: Copyable 1 copy-created
+// CHECK-NEXT: Copyable 2 move-created
+// CHECK-NEXT: Copyable 1 destroyed
+// CHECK-NEXT: Copyable 2 destroyed
+// CHECK-NEXT: Copyable 0 destroyed
+// CHECK-OPT-NEXT: Copyable 0 destroyed
+// CHECK-NEXT: Copyable 0 destroyed
+// CHECK-OPT-NEXT: Copyable 0 destroyed
+}
+
+main()
