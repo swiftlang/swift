a34fb387eb398120a374be8a143608ade986c587
Alejandro Alonso <alejandro_alonso@apple.com>
2025-02-25 09:28:34 -0800

    Always use an opaque AP for inline array elements

--- lib/SIL/IR/TypeLowering.cpp
+++ lib/SIL/IR/TypeLowering.cpp
@@ -3067,14 +3067,8 @@ bool TypeConverter::visitAggregateLeaves(
                            origTy.getPackExpansionPatternType(),
                            field, index);
       } else if (auto array = dyn_cast<BuiltinFixedArrayType>(ty)) {
-        auto origEltTy = AbstractionPattern::getOpaque();
-
-        if (auto origBFA = origTy.getAs<BuiltinFixedArrayType>()) {
-          origEltTy = AbstractionPattern(origTy.getGenericSignatureOrNull(),
-                                         origBFA->getElementType());
-        }
-
-        insertIntoWorklist(array->getElementType(), origEltTy, field, index);
+        insertIntoWorklist(array->getElementType(),
+                           AbstractionPattern::getOpaque(), field, index);
       } else if (auto *decl = ty.getStructOrBoundGenericStruct()) {
         for (auto *structField : decl->getStoredProperties()) {
           auto subMap = ty->getContextSubstitutionMap();
