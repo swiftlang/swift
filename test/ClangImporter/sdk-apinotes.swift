f0062ee5ae9ed6a09173504d78aaee4376b04ec7
Finagolfin <finagolfin@tuta.io>
2025-02-26 04:33:08 +0530

    Have the frontend and new swift-driver look in an external `-sdk` for non-Darwin platform runtime libraries and modules too
    as done originally in #25990 with the legacy C++ Driver, but since lost in the new swift-driver. Only difference
    is this checks if a Swift resource directory exists in `-sdk` and falls back to the default if not.

--- test/ClangImporter/sdk-apinotes.swift
+++ test/ClangImporter/sdk-apinotes.swift
@@ -1,4 +1,11 @@
-// RUN: %target-swift-frontend(mock-sdk: %clang-importer-sdk) -typecheck %s -verify
+// RUN: %empty-directory(%t/sdk)
+// RUN: %empty-directory(%t/sdk/usr/lib/swift/apinotes)
+// RUN: %empty-directory(%t/sdk/usr/lib/swift/%target-sdk-name)
+// RUN: cp -r %clang-importer-sdk-path/usr/include %t/sdk/usr
+// RUN: cp -r %test-resource-dir/shims %t/sdk/usr/lib/swift
+// RUN: cp %S/Inputs/cfuncs.apinotes %t/sdk/usr/lib/swift/apinotes
+// RUN: cp -r %platform-module-dir/Swift.swiftmodule %t/sdk/usr/lib/swift/%target-sdk-name/
+// RUN: %target-swift-frontend(mock-sdk: -sdk %t/sdk) -typecheck %s -verify -disable-implicit-concurrency-module-import -disable-implicit-string-processing-module-import
 
 import cfuncs
 
