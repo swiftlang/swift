c096bac9d61661aa6abb04d3b884c2fafba60a99
Henrik G. Olsson <h_olsson@apple.com>
2025-02-28 19:14:11 -0800

    [Swiftify][StrictMemorySafety] Test `unsafe` warnings in wrappers (NFC)
    As _SwiftifyImport now emits the `unsafe` keyword to prevent warnings
    about unsafe code in the macro expansions, we should make sure that our
    tests do not emit any warnings. It turns out that calls to
    RawSpan::withUnsafeRawPointer are not recognised as unsafe, so we
    sometimes get warnings about using `unsafe` on a safe expression. This
    issue is tracked under rdar://145899513.

--- test/Macros/SwiftifyImport/SizedBy/Nullable.swift
+++ test/Macros/SwiftifyImport/SizedBy/Nullable.swift
@@ -1,6 +1,6 @@
 // REQUIRES: swift_swift_parser
 
-// RUN: %target-swift-frontend %s -swift-version 5 -module-name main -disable-availability-checking -typecheck -plugin-path %swift-plugin-dir -dump-macro-expansions 2>&1 | %FileCheck --match-full-lines %s
+// RUN: %target-swift-frontend %s -swift-version 5 -module-name main -disable-availability-checking -typecheck -plugin-path %swift-plugin-dir -strict-memory-safety -warnings-as-errors -dump-macro-expansions 2>&1 | %FileCheck --match-full-lines %s
 
 @_SwiftifyImport(.sizedBy(pointer: .param(1), size: "size"))
 func myFunc(_ ptr: UnsafeRawPointer?, _ size: CInt) {
