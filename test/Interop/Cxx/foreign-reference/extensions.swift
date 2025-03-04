8f58ad67cf9be734af9b2840c8ea252ee93d2078
Gábor Horváth <xazax.hun@gmail.com>
2025-03-04 10:30:47 +0000

    Update test/Interop/Cxx/foreign-reference/extensions.swift
    Co-authored-by: John Hui <j-hui@apple.com>

--- test/Interop/Cxx/foreign-reference/extensions.swift
+++ test/Interop/Cxx/foreign-reference/extensions.swift
@@ -1,26 +1,50 @@
-// RUN: %target-run-simple-swift(-I %S/Inputs/ -Xfrontend -enable-experimental-cxx-interop -Xfrontend -disable-availability-checking -Onone)
+// RUN: %target-run-simple-swift(-I %S/Inputs/ -Xfrontend -cxx-interoperability-mode=default -Xfrontend -disable-availability-checking -Onone) | %FileCheck %s
 // REQUIRES: executable_test
+
+// Metadata for foreign reference types is not supported on Windows.
+// UNSUPPORTED: OS=windows-msvc
 
 import ReferenceCounted
 
 protocol MyProto {
+    static func g()
     func foo() -> Self
+    func bar(_ x: Self)
+    func baz()
+}
+
+extension NS.LocalCount: MyProto {
+    static func g() {
+        print("Static method g called")
+    }
+
+    public func foo() -> Self {
+        Self.g()
+        return self
+    }
+
+    public func bar(_ x: Self) {
+    }
+}
+
+extension MyProto {
+    func baz() {
+        Self.g()
+    }
 }
 
 extension NS.LocalCount {
-    static func g() {}
-
     public func f() {
         Self.g()
     }
 }
 
-extension NS.LocalCount: MyProto {
-    public func foo() -> Self {
-        return self
-    }
-}
 
 let x = NS.LocalCount.create()
 x.f()
+// CHECK: Static method g called
 let _ = x.foo()
+// CHECK-NEXT: Static method g called
+let _ = x.baz()
+// CHECK-NEXT: Static method g called
+x.bar(x)
