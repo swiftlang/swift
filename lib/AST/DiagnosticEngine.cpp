db2fa7bcdab6facc0e8f44f06dfbaa4bbd3cf3d0
Doug Gregor <dgregor@apple.com>
2025-03-04 09:14:48 -0800

    [Diagnostics] Use the educational note name for the category name

--- lib/AST/DiagnosticEngine.cpp
+++ lib/AST/DiagnosticEngine.cpp
@@ -1337,6 +1337,7 @@ DiagnosticEngine::diagnosticInfoForDiagnostic(const Diagnostic &diagnostic,
 
   auto groupID = diagnostic.getGroupID();
   StringRef Category;
+  const char * const *associatedNotes = nullptr;
   if (isAPIDigesterBreakageDiagnostic(diagnostic.getID()))
     Category = "api-digester-breaking-change";
   else if (isNoUsageDiagnostic(diagnostic.getID()))
@@ -1345,6 +1346,10 @@ DiagnosticEngine::diagnosticInfoForDiagnostic(const Diagnostic &diagnostic,
     Category = getDiagGroupInfoByID(groupID).name;
   else if (isDeprecationDiagnostic(diagnostic.getID()))
     Category = "deprecation";
+  else if ((associatedNotes = educationalNotes[(uint32_t)diagnostic.getID()]) &&
+           *associatedNotes) {
+    Category = llvm::sys::path::stem(*associatedNotes);
+  }
 
   auto fixIts = diagnostic.getFixIts();
   if (loc.isValid()) {
