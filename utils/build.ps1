be9dd8e599ab6f78f0a30c315c120ca0b1f9c3ad
Saleem Abdulrasool <compnerd@compnerd.org>
2025-03-07 09:06:53 -0800

    utils: remove swiftinterfaces from the build image
    These cannot be reserialized due to a bug in the serialization logic
    where we do not inject the VFS Ovelay for the system modules.
    
    Bug: #79839

--- utils/build.ps1
+++ utils/build.ps1
@@ -3207,6 +3207,10 @@ if (-not $SkipBuild) {
 
     # Build platform: SDK, Redist and XCTest
     Invoke-BuildStep Build-Runtime Windows $Arch
+
+    # FIXME(#79839)
+    Get-ChildItem -Path "$($Arch.SDKRoot)\usr\lib\swift\android" -Filter "*.swiftinterface" -Recurse
+
     Invoke-BuildStep Build-Dispatch Windows $Arch
     # FIXME(compnerd) ensure that the _build_ is the first arch and don't rebuild on each arch
     Invoke-BuildStep Build-FoundationMacros -Build Windows $BuildArch
@@ -3232,6 +3236,10 @@ if (-not $SkipBuild) {
 
     # Build platform: SDK, Redist and XCTest
     Invoke-BuildStep Build-Runtime Android $Arch
+
+    # FIXME(#79839)
+    Get-ChildItem -Path "$($Arch.SDKRoot)\usr\lib\swift\android" -Filter "*.swiftinterface" -Recurse
+
     Invoke-BuildStep Build-Dispatch Android $Arch
     Invoke-BuildStep Build-Foundation Android $Arch
     Invoke-BuildStep Build-Sanitizers Android $Arch
