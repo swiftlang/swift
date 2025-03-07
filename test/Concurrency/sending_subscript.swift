98209cdea51213e473f55d1aa6aec56a8ca3ce5b
stzn <sztk1209@gmail.com>
2025-03-08 07:09:08 +0900

    Add a test case with a tuple result

--- test/Concurrency/sending_subscript.swift
+++ test/Concurrency/sending_subscript.swift
@@ -4,10 +4,19 @@
 
 class NonSendableKlass {}
 
-// CHECK: subscript(_: sending NonSendableKlass) -> sending NonSendableKlass { get }
+// CHECK-DAG: subscript(_: sending NonSendableKlass) -> sending NonSendableKlass { get }
 
-// CHECK: sil hidden [ossa] @$s17sending_subscript1SVyAA16NonSendableKlassCAEncig : $@convention(method) (@sil_sending @owned NonSendableKlass, S) -> @sil_sending @owned NonSendableKlass {
+// CHECK-DAG: sil hidden [ossa] @$s17sending_subscript1SVyAA16NonSendableKlassCAEncig : $@convention(method) (@sil_sending @owned NonSendableKlass, S) -> @sil_sending @owned NonSendableKlass {
 struct S {
   subscript(_: sending NonSendableKlass) -> sending NonSendableKlass { NonSendableKlass() }
+}
+
+// CHECK-DAG: subscript(_: sending NonSendableKlass) -> sending (NonSendableKlass, NonSendableKlass) { get }
+
+// CHECK-DAG: sil hidden [ossa] @$s17sending_subscript2S2VyAA16NonSendableKlassC_AEtAEncig : $@convention(method) (@sil_sending @owned NonSendableKlass, S2) -> (@sil_sending @owned NonSendableKlass, @sil_sending @owned NonSendableKlass) {
+struct S2 {
+  subscript(_: sending NonSendableKlass) -> sending (NonSendableKlass, NonSendableKlass) { 
+    (NonSendableKlass(), NonSendableKlass()) 
+  }
 }
 
