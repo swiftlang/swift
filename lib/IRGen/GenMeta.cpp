d861a9f81aaa94f67910127d689e9239a1d4892e
Alastair Houghton <ahoughton@apple.com>
2025-03-07 20:09:53 +0000

    [Concurrency] More work on the custom executor implementation.
    Added an `-executor-factory` argument to the compiler to let you safely
    specify the executors you wish to use (by naming a type that returns
    them).
    
    Also added some tests of the new functionality.
    
    rdar://141348916

--- lib/IRGen/GenMeta.cpp
+++ lib/IRGen/GenMeta.cpp
@@ -7016,6 +7016,7 @@ SpecialProtocol irgen::getSpecialProtocolID(ProtocolDecl *P) {
   case KnownProtocolKind::Executor:
   case KnownProtocolKind::SerialExecutor:
   case KnownProtocolKind::TaskExecutor:
+  case KnownProtocolKind::ExecutorFactory:
   case KnownProtocolKind::Sendable:
   case KnownProtocolKind::UnsafeSendable:
   case KnownProtocolKind::RangeReplaceableCollection:
--- stdlib/public/Concurrency/PlatformExecutorDarwin.swift
+++ stdlib/public/Concurrency/PlatformExecutorDarwin.swift
@@ -14,108 +14,22 @@
 
 import Swift
 
-// .. Platform Main Executor ...................................................
-
-/// `PlatformMainExecutor` is used during program start-up and also for any
-/// `@MainActor` code.  It implements `SerialExecutor`, `RunLoopExecutor` and
-/// `EventableExecutor`.
 @available(SwiftStdlib 6.2, *)
-public class PlatformMainExecutor: MainExecutor, @unchecked Sendable {
-  var executor: any MainExecutor
-
-  init() {
+public struct PlatformExecutorFactory: ExecutorFactory {
+  public static var mainExecutor: any MainExecutor {
     if CoreFoundation.isPresent {
-      executor = CFMainExecutor()
+      return CFMainExecutor()
     } else {
-      executor = DispatchMainExecutor()
+      return DispatchMainExecutor()
     }
   }
 
-  public var supportsScheduling: Bool { executor.supportsScheduling }
-
-  public func enqueue(_ job: consuming ExecutorJob) {
-    executor.enqueue(job)
-  }
-
-  public func enqueue<C: Clock>(_ job: consuming ExecutorJob,
-                                after delay: C.Duration,
-                                tolerance: C.Duration? = nil,
-                                clock: C) {
-    executor.enqueue(job, after: delay, tolerance: tolerance, clock: clock)
-  }
-
-  public func enqueue<C: Clock>(_ job: consuming ExecutorJob,
-                                at instant: C.Instant,
-                                tolerance: C.Duration? = nil,
-                                clock: C) {
-    executor.enqueue(job, at: instant, tolerance: tolerance, clock: clock)
-  }
-
-  public func run() throws {
-    try executor.run()
-  }
-
-  public func run(until condition: () -> Bool) throws {
-    try executor.run(until: condition)
-  }
-
-  public func stop() {
-    executor.stop()
-  }
-
-  public func registerEvent(handler: @escaping () -> ()) -> ExecutorEvent {
-    return executor.registerEvent(handler: handler)
-  }
-
-  public func deregister(event: ExecutorEvent) {
-    executor.deregister(event: event)
-  }
-
-  public func notify(event: ExecutorEvent) {
-    executor.notify(event: event)
-  }
-
-  public func checkIsolated() {
-    executor.checkIsolated()
-  }
-}
-
-// .. Platform Task Executor ...................................................
-
-/// `PlatformDefaultExecutor` is the default executor for non-`@MainActor`
-/// tasks.  It implements `TaskExecutor` only.
-@available(SwiftStdlib 6.2, *)
-public class PlatformDefaultExecutor: TaskExecutor, @unchecked Sendable {
-  var executor: any TaskExecutor
-
-  init() {
+  public static var defaultExecutor: any TaskExecutor {
     if CoreFoundation.isPresent {
-      executor = CFTaskExecutor()
+      return CFTaskExecutor()
     } else {
-      executor = DispatchTaskExecutor()
+      return DispatchTaskExecutor()
     }
-  }
-
-  public var isMainExecutor: Bool { executor.isMainExecutor }
-
-  public var supportsScheduling: Bool { executor.supportsScheduling }
-
-  public func enqueue(_ job: consuming ExecutorJob) {
-    executor.enqueue(job)
-  }
-
-  public func enqueue<C: Clock>(_ job: consuming ExecutorJob,
-                                after delay: C.Duration,
-                                tolerance: C.Duration? = nil,
-                                clock: C) {
-    executor.enqueue(job, after: delay, tolerance: tolerance, clock: clock)
-  }
-
-  public func enqueue<C: Clock>(_ job: consuming ExecutorJob,
-                                at instant: C.Instant,
-                                tolerance: C.Duration? = nil,
-                                clock: C) {
-    executor.enqueue(job, at: instant, tolerance: tolerance, clock: clock)
   }
 }
 
