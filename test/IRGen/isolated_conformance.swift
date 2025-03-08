904d0cffb2ae816dddd5445c6002f9dc1d75b9f6
Doug Gregor <dgregor@apple.com>
2025-03-07 23:49:40 -0800

    Generalize FileCheck'ing of IRGen testcase slightly

--- test/IRGen/isolated_conformance.swift
+++ test/IRGen/isolated_conformance.swift
@@ -14,8 +14,8 @@ protocol P {
 // CHECK-SAME: ptr @"$s20isolated_conformance1XVMn"
 // CHECK-SAME: ptr @"$s20isolated_conformance1XVyxGAA1PAAWP
 // CHECK-SAME: i32 524288
-// CHECK-SAME: @"symbolic ScM"
-// CHECK-SAME: ptr @"got.$sScMs11GlobalActorsMc"
+// CHECK-SAME: symbolic ScM"
+// CHECK-SAME: $sScMs11GlobalActorsMc"
 @MainActor
 struct X<T>: isolated P {
   func f() { }
