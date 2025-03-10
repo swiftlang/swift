d25f1fa86f2851609c26635c1051402ad810b72f
Kuba Mracek <mracek@apple.com>
2025-03-10 15:01:23 -0700

    [embedded] Emit weak_odr instead of linkonce_odr symbols under -Xfrontend -mergeable-symbols

---
+++ test/embedded/linkage-public-exports.swift
@@ -0,0 +1,13 @@
+// RUN: %target-swift-frontend %s -mergeable-symbols    -enable-experimental-feature Embedded -emit-ir | %FileCheck %s
+// RUN: %target-swift-frontend %s -mergeable-symbols -O -enable-experimental-feature Embedded -emit-ir | %FileCheck %s
+
+// REQUIRES: swift_in_compiler
+// REQUIRES: OS=macosx || OS=linux-gnu
+// REQUIRES: swift_feature_Embedded
+
+public func foo1() { }
+
+func foo2() { }
+
+// CHECK: foo1
+// CHECK-NOT: foo2
