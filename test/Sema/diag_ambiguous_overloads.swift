77e136d04b8876b71d7eb8c23164e87c737c37d5
Rajveer <rajveer.developer@icloud.com>
2025-03-16 17:33:18 +0530

    [Sema] Produce expected diagnostic for invalid operator usage in loop
    Resolves #79999
    
    Snippet:
    
    ```
    func foo(_ a: Int) -> [Int] { [] }
    for _ in foo(-) {}
    ```

--- test/Sema/diag_ambiguous_overloads.swift
+++ test/Sema/diag_ambiguous_overloads.swift
@@ -52,9 +52,12 @@ func testDiagnoseForAmbiguityCrash(schools: [School]) {
 class DefaultValue {
   static func foo(_ a: Int) {}
   static func foo(_ a: Int, _ b: Int = 1) {}
+  // expected-note@-1 {{candidate expects value of type 'Int' for parameter #1 (got 'Double')}}
   static func foo(_ a: Int, _ b: Int = 1, _ c: Int = 2) {}
+  // expected-note@-1 {{candidate expects value of type 'Int' for parameter #1 (got 'Double')}}
 }
-DefaultValue.foo(1.0, 1) // expected-error {{cannot convert value of type 'Double' to expected argument type 'Int'}}
+DefaultValue.foo(1.0, 1)
+// expected-error@-1 {{no exact matches in call to static method 'foo'}}
 
 
 class Variadic {
@@ -152,7 +155,11 @@ do {
   func f1(_ u: String) -> Double {} // expected-note 2 {{candidate expects value of type 'String' for parameter #1 (got 'Double')}}
   func f2(_ u: Int) {}
 
-  f2(f1(1 as Double)) // expected-error {{no exact matches in call to local function 'f1'}}
+  f2(f1(1 as Double))
+  // expected-error@-1 {{no exact matches in call to local function 'f1'}}
+  // expected-error@-2 {{ambiguous use of 'f1'}}
+  // expected-note@-3 {{cannot convert result type 'String' to expected argument type 'Int'}}
+  // expected-note@-4 {{cannot convert result type 'Double' to expected argument type 'Int'}}
   f1(1 as Double) as Int // expected-error {{no exact matches in call to local function 'f1'}}
 }
 do {
@@ -188,4 +195,39 @@ do {
 
   let _ = i16 -- i16
   // expected-error@-1 {{ambiguous use of operator '--'}}
+}
+
+// https://github.com/swiftlang/swift/issues/79999
+do {
+  func foo(_ a: Int) -> [Int] { [] }
+  
+  let _ = foo(-)
+  // expected-error@-1 {{ambiguous use of '-'}}
+  // expected-note@-2 {{no candidates found for specified argument '-'}}
+
+  for _ in foo(-) {}
+  // expected-error@-1 {{ambiguous use of '-'}}
+  // expected-note@-2 {{no candidates found for specified argument '-'}}
+  
+  func test() -> Int {}
+  func test() -> String {}
+  
+  func other(_: Int32) {}
+  
+  other(test())
+  // expected-error@-1 {{ambiguous use of 'test'}}
+  // expected-note@-2 {{cannot convert result type 'Int' to expected argument type 'Int32'}}
+  // expected-note@-3 {{cannot convert result type 'String' to expected argument type 'Int32'}}
+}
+
+do {
+  func test(_: Int) {}
+  // expected-note@-1 {{candidate expects value of type 'Int' for parameter #1 (got 'Double')}}
+  func test(_: String) {}
+  // expected-note@-1 {{candidate expects value of type 'String' for parameter #1 (got 'Double')}}
+  
+  func compute() -> Double { 0 }
+  
+  test(compute())
+  // expected-error@-1 {{no exact matches in call to local function 'test'}}
 }
