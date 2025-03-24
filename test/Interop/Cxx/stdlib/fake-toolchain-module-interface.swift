ec4171100e654217cf98a4d2fdbc0ffde40b3105
Michael Chiu <nyan@myuji.xyz>
2025-03-24 18:55:01 -0400

    disable fake toolchain test on freebsd

--- test/Interop/Cxx/stdlib/fake-toolchain-module-interface.swift
+++ test/Interop/Cxx/stdlib/fake-toolchain-module-interface.swift
@@ -1,10 +1,11 @@
-// RUN: %target-swift-ide-test -print-module -module-to-print=FakeToolchain -tools-directory %S/Inputs/fake-toolchain/bin -source-filename=x -enable-experimental-cxx-interop -Xcc -stdlib=libc++ -Xcc -I%S/Inputs/fake-toolchain/include/c++/v1 | %FileCheck %s
+// RUN: %target-swift-ide-test -print-module -module-to-print=FakeToolchain -tools-directory %S/Inputs/fake-toolchain/bin -source-filename=x -enable-experimental-cxx-interop -Xcc -stdlib=libc++ | %FileCheck %s
 
 // Clang driver on Windows doesn't support -stdlib=libc++
 // XFAIL: OS=windows-msvc
 // Android NDK layout might need more flags to find libc++
 // XFAIL: OS=linux-androideabi
 // XFAIL: OS=linux-android
+// XFAIL: OS=freebsd
 
 // CHECK: enum FakeNamespace {
 // CHECK:   static func foo(_ x: Int32)
