d1737b9c2024ac8559beb254b033f981e7a58240
Henrik G. Olsson <hnrklssn@gmail.com>
2025-03-27 16:37:31 -0700

    [Swiftify] Add MutableSpan support for std::span, and disable it (#80315)
    __counted_by already had MutableSpan support, so add it for std::span
    for parity. But since MutableSpan hasn't landed in the standard library
    yet, disable emitting it to prevent compilation errors in expansions.
    
    rdar://147882736

--- test/Macros/SwiftifyImport/CountedBy/MutableSpan.swift
+++ test/Macros/SwiftifyImport/CountedBy/MutableSpan.swift
@@ -1,16 +1,15 @@
 // REQUIRES: swift_swift_parser
 
-// RUN: not %target-swift-frontend %s -swift-version 5 -module-name main -disable-availability-checking -typecheck -plugin-path %swift-plugin-dir -strict-memory-safety -warnings-as-errors -dump-macro-expansions > %t.log 2>&1
+// RUN: %target-swift-frontend %s -swift-version 5 -module-name main -disable-availability-checking -typecheck -plugin-path %swift-plugin-dir -strict-memory-safety -warnings-as-errors -dump-macro-expansions > %t.log 2>&1
 // RUN: %FileCheck --match-full-lines %s < %t.log
 
 @_SwiftifyImport(.countedBy(pointer: .param(1), count: "len"), .nonescaping(pointer: .param(1)))
 func myFunc(_ ptr: UnsafeMutablePointer<CInt>, _ len: CInt) {
 }
 
-// CHECK:      @_alwaysEmitIntoClient
-// CHECK-NEXT: func myFunc(_ ptr: MutableSpan<CInt>) {
-// CHECK-NEXT:     return unsafe ptr.withUnsafeBufferPointer { _ptrPtr in
-// CHECK-NEXT:         return unsafe myFunc(_ptrPtr.baseAddress!, CInt(exactly: ptr.count)!)
-// CHECK-NEXT:     }
-// CHECK-NEXT: }
+// Emits UnsafeMutableBufferPointer until MutableSpan has landed
 
+// CHECK:      @_alwaysEmitIntoClient
+// CHECK-NEXT: func myFunc(_ ptr: UnsafeMutableBufferPointer<CInt>) {
+// CHECK-NEXT:     return unsafe myFunc(ptr.baseAddress!, CInt(exactly: ptr.count)!)
+// CHECK-NEXT: }
