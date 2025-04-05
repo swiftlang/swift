873da73044de84f60dea67721cf3438537185ef1
Allan Shortlidge <shortlidge@apple.com>
2025-04-05 08:49:35 -0700

    Parse: Improve MemberImportVisibility diags for appendLiteral(_:) calls.
    Ensure compiler generated calls to `appendLiteral(_:)` have source locations for
    diagnostics.
    
    Partially resolves rdar://144535697.

--- test/NameLookup/members_transitive_compiler_protocols.swift
+++ test/NameLookup/members_transitive_compiler_protocols.swift
@@ -9,11 +9,16 @@
 //--- main.swift
 
 import Swift
-// expected-note 2 {{add import of module 'lib'}}
+// expected-note 6 {{add import of module 'lib'}}
 
 for _ in makeSequence() { }
 // expected-error@-1 {{instance method 'makeIterator()' is not available due to missing import of defining module 'lib'}}
 // expected-error@-2 {{instance method 'next()' is not available due to missing import of defining module 'lib'}}
+
+takesMessage("\(1)")
+// expected-error@-1 {{initializer 'init(stringInterpolation:)' is not available due to missing import of defining module 'lib'}}
+// expected-error@-2 {{instance method 'appendInterpolation' is not available due to missing import of defining module 'lib'}}
+// expected-error@-3 2 {{instance method 'appendLiteral' is not available due to missing import of defining module 'lib'}}
 
 //--- other.swift
 
@@ -22,6 +27,8 @@ import lib
 func makeSequence() -> EmptySequence {
   return MySequence()
 }
+
+func takesMessage(_ x: Message) { }
 
 //--- lib.swift
 
@@ -32,4 +39,15 @@ public struct EmptySequence: Sequence {
 
   public func makeIterator() -> Iterator { Iterator() }
   public init() { }
+}
+
+public struct MessageInterpolation: StringInterpolationProtocol {
+  public init(literalCapacity: Int, interpolationCount: Int) { }
+  public mutating func appendInterpolation(_ value: @autoclosure () -> Int) { }
+  public mutating func appendLiteral(_ literal: String) { }
+}
+
+public struct Message: ExpressibleByStringInterpolation {
+  public init(stringInterpolation: MessageInterpolation) { }
+  public init(stringLiteral: String) { }
 }
