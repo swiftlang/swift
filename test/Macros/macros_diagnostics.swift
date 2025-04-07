9a414c4de4a08d4e353d73c6ee53566492a40867
Anthony Latsis <alatsis@apple.com>
2025-04-07 13:43:11 +0100

    AST: Quote attributes more consistently in `DiagnosticsParse.def`

--- test/Macros/macros_diagnostics.swift
+++ test/Macros/macros_diagnostics.swift
@@ -6,7 +6,7 @@
 @expression macro stringify<T>(_ value: T) -> (T, String) = #externalMacro(module: "MacroDefinition", type: "StringifyMacro")
 // expected-note@-1 3{{'stringify' declared here}}
 // expected-warning@-2{{external macro implementation type}}
-// expected-warning@-3{{@expression has been removed in favor of @freestanding(expression)}}{{1-12=@freestanding(expression)}}
+// expected-warning@-3{{'@expression' has been removed in favor of '@freestanding(expression)'}}{{1-12=@freestanding(expression)}}
 @freestanding(expression) macro missingMacro1(_: Any) = MissingModule.MissingType // expected-note{{'missingMacro1' declared here}}
 // expected-warning@-1{{external macro definitions are now written using #externalMacro}}
 // expected-warning@-2{{external macro implementation type}}
--- test/Parse/diagnose_dynamicReplacement.swift
+++ test/Parse/diagnose_dynamicReplacement.swift
@@ -16,5 +16,5 @@ func test_dynamic_replacement_for2() {
 
 @_dynamicReplacement(for: dynamically_replaceable() // expected-note {{to match this opening '('}}
 func test_dynamic_replacement_for3() {
-// expected-error@-1 {{expected ')' after function name for @_dynamicReplacement}}
+// expected-error@-1 {{expected ')' after function name for '@_dynamicReplacement'}}
 }
