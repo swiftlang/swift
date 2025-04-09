swiftlang/swift

commit a00157ec431f312bb85ed9c3ddd166a659a07784

IRGen: Refactor LocalTypeData a bit
The "abstract conformance is just a ProtocolDecl" assumption is pretty
fundamental here, so we have to fudge a bit at the API boundary for now.

Eventually, LocalTypeDataKind should just contain a ProtocolConformanceRef
instead of duplicating the representation, however this would require
fixing various calls to LocalTypeDataKind::forAbstractProtocolWitnessTable()
which pass in a ProtocolDecl to pass in a ProtocolConformanceRef instead.

lib/IRGen/GenProto.cpp
@@ -3402,8 +3402,7 @@ MetadataResponse MetadataPath::followComponent(IRGenFunction &IGF,
   }
 
   case Component::Kind::OutOfLineBaseProtocol: {
-    auto conformance = sourceKey.Kind.getProtocolConformance();
-    auto protocol = conformance.getRequirement();
+    auto protocol = sourceKey.Kind.getConformedProtocol();
     auto &pi = IGF.IGM.getProtocolInfo(protocol,
                                        ProtocolInfoKind::RequirementSignature);
 
@@ -3413,9 +3412,10 @@ MetadataResponse MetadataPath::followComponent(IRGenFunction &IGF,
 
     sourceKey.Kind =
       LocalTypeDataKind::forAbstractProtocolWitnessTable(inheritedProtocol);
-    if (conformance.isConcrete()) {
+    if (sourceKey.Kind.isConcreteProtocolConformance()) {
       auto inheritedConformance =
-        conformance.getConcrete()->getInheritedConformance(inheritedProtocol);
+        sourceKey.Kind.getConcreteProtocolConformance()
+          ->getInheritedConformance(inheritedProtocol);
       if (inheritedConformance) {
         sourceKey.Kind = LocalTypeDataKind::forConcreteProtocolWitnessTable(
                                                           inheritedConformance);
@@ -3438,8 +3438,8 @@ MetadataResponse MetadataPath::followComponent(IRGenFunction &IGF,
 
   case Component::Kind::AssociatedConformance: {
     auto sourceType = sourceKey.Type;
-    auto sourceConformance = sourceKey.Kind.getProtocolConformance();
-    auto sourceProtocol = sourceConformance.getRequirement();
+    auto sourceConformance = sourceKey.Kind.getProtocolConformance(sourceType);
+    auto sourceProtocol = sourceKey.Kind.getConformedProtocol();
     auto &pi = IGF.IGM.getProtocolInfo(sourceProtocol,
                                        ProtocolInfoKind::RequirementSignature);
 
@@ -3473,7 +3473,8 @@ MetadataResponse MetadataPath::followComponent(IRGenFunction &IGF,
       // In Embedded Swift associated-conformance entries simply point to the witness table
       // of the associated conformance.
       llvm::Value *sourceWTable = source.getMetadata();
-      llvm::Value *associatedWTable = emitAssociatedConformanceValue(IGF, sourceWTable, associatedConformanceRef);
+      llvm::Value *associatedWTable = emitAssociatedConformanceValue(
+          IGF, sourceWTable, associatedConformanceRef);
       return MetadataResponse::forComplete(associatedWTable);
     }
 
@@ -3562,13 +3563,13 @@ MetadataResponse MetadataPath::followComponent(IRGenFunction &IGF,
   }
 
   case Component::Kind::ConditionalConformance: {
-    auto sourceConformance = sourceKey.Kind.getProtocolConformance();
+    auto sourceConformance = sourceKey.Kind.getConcreteProtocolConformance();
 
     auto reqtIndex = component.getPrimaryIndex();
 
     ProtocolDecl *conformingProto;
     auto found = SILWitnessTable::enumerateWitnessTableConditionalConformances(
-        sourceConformance.getConcrete(),
+        sourceConformance,
         [&](unsigned index, CanType type, ProtocolDecl *proto) {
           if (reqtIndex == index) {
             conformingProto = proto;

lib/IRGen/LocalTypeDataKind.h
@@ -19,6 +19,8 @@
 #ifndef SWIFT_IRGEN_LOCALTYPEDATAKIND_H
 #define SWIFT_IRGEN_LOCALTYPEDATAKIND_H
 
+#include "swift/AST/PackConformance.h"
+#include "swift/AST/ProtocolConformance.h"
 #include "swift/AST/ProtocolConformanceRef.h"
 #include "swift/AST/Type.h"
 #include "swift/IRGen/ValueWitness.h"
@@ -183,13 +185,23 @@ public:
     return reinterpret_cast<PackConformance*>(Value - Kind_PackConformance);
   }
 
-  ProtocolConformanceRef getProtocolConformance() const {
+  ProtocolDecl *getConformedProtocol() const {
     assert(!isSingletonKind());
     if ((Value & KindMask) == Kind_Decl) {
-      // FIXME: Passing an empty Type() here temporarily while staging in
-      // new representation for abstract conformances
+      return getAbstractProtocolConformance();
+    } else if ((Value & KindMask) == Kind_PackConformance) {
+      return getPackProtocolConformance()->getProtocol();
+    } else {
+      assert((Value & KindMask) == Kind_Conformance);
+      return getConcreteProtocolConformance()->getProtocol();
+    }
+  }
+
+  ProtocolConformanceRef getProtocolConformance(Type conformingType) const {
+    assert(!isSingletonKind());
+    if ((Value & KindMask) == Kind_Decl) {
       return ProtocolConformanceRef::forAbstract(
-          Type(), getAbstractProtocolConformance());
+          conformingType, getAbstractProtocolConformance());
     } else if ((Value & KindMask) == Kind_PackConformance) {
       return ProtocolConformanceRef(getPackProtocolConformance());
     } else {

commit 5a39c81d8a82ab5dda283b0c897a5034d2f47bcb

IRGen: Fix silly mistake in MetadataPath::followComponent()
This fixes a regression from a00157ec431f312bb85ed9c3ddd166a659a07784.

My change made it so that sourceKey.Kind was checked after being
overwritten with an abstract conformance, so we would never take
the if statement. Incredibly, it almost worked.

Fixes rdar://problem/148698142.

lib/IRGen/GenProto.cpp
@@ -3390,16 +3390,15 @@ MetadataResponse MetadataPath::followComponent(IRGenFunction &IGF,
     assert(entry.isOutOfLineBase());
     auto inheritedProtocol = entry.getBase();
 
-    sourceKey.Kind =
-      LocalTypeDataKind::forAbstractProtocolWitnessTable(inheritedProtocol);
     if (sourceKey.Kind.isConcreteProtocolConformance()) {
       auto inheritedConformance =
         sourceKey.Kind.getConcreteProtocolConformance()
           ->getInheritedConformance(inheritedProtocol);
-      if (inheritedConformance) {
-        sourceKey.Kind = LocalTypeDataKind::forConcreteProtocolWitnessTable(
-                                                          inheritedConformance);
-      }
+      sourceKey.Kind = LocalTypeDataKind::forConcreteProtocolWitnessTable(
+                                                        inheritedConformance);
+    } else {
+      sourceKey.Kind =
+        LocalTypeDataKind::forAbstractProtocolWitnessTable(inheritedProtocol);
     }
 
     if (!source) return MetadataResponse();

test/IRGen/conformance_path_with_concrete_steps.swift
@@ -1,0 +1,29 @@
+// RUN: %target-swift-frontend -emit-ir %s
+
+public protocol P1 {
+  associatedtype A: P3
+}
+
+public protocol P2: P1 where A.B == G<C> {
+  associatedtype C where C == A.B.C
+}
+
+public protocol P3 {
+  associatedtype B: P4
+}
+
+public protocol P4: P5 {}
+
+public protocol P5 {
+  associatedtype C: P6
+}
+
+public protocol P6 {
+  func foo()
+}
+
+public struct G<C: P6>: P4 {}
+
+public func f<T: P2>(_: T, c: T.C) {
+  return c.foo()
+}