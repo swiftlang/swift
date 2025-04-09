96903979a92bda8981e37d956bd7111642b214a7
Nate Chandler <nate_chandler@apple.com>
2025-04-09 12:56:44 -0700

    [CoroutineAccessors] Unwind based on feature.
    Now that coroutine kind (and consequently ABI) for the accessors is
    keyed off a SIL option, it's no longer possible to read whether a given
    SILFunction arose from a read/modify coroutine just by checking its
    coroutine kind.  Regardless of ABI, read/modify coroutines may only
    unwind (i.e. are only permitted not to "run to completion") if the
    relevant experimental (soon to be deleted) feature is enabled.

--- lib/SILGen/SILGenFunction.h
+++ lib/SILGen/SILGenFunction.h
@@ -2034,6 +2034,7 @@ public:
       PreparedArguments &&optionalSubscripts,
       SILType addressType, bool isOnSelfParameter);
 
+  bool canUnwindAccessorDeclRef(SILDeclRef accessorRef);
   CleanupHandle emitCoroutineAccessor(SILLocation loc, SILDeclRef accessor,
                                       SubstitutionMap substitutions,
                                       ArgumentSource &&optionalSelfValue,
@@ -2301,8 +2302,9 @@ public:
                                         PreparedArguments &&args, Type overriddenSelfType,
                                         SGFContext ctx);
 
-  CleanupHandle emitBeginApply(SILLocation loc, ManagedValue fn,
-                               SubstitutionMap subs, ArrayRef<ManagedValue> args,
+  CleanupHandle emitBeginApply(SILLocation loc, ManagedValue fn, bool canUnwind,
+                               SubstitutionMap subs,
+                               ArrayRef<ManagedValue> args,
                                CanSILFunctionType substFnType,
                                ApplyOptions options,
                                SmallVectorImpl<ManagedValue> &yields);
@@ -2315,7 +2317,8 @@ public:
   std::tuple<MultipleValueInstructionResult *, CleanupHandle, SILValue,
              CleanupHandle>
   emitBeginApplyWithRethrow(SILLocation loc, SILValue fn, SILType substFnType,
-                            SubstitutionMap subs, ArrayRef<SILValue> args,
+                            bool canUnwind, SubstitutionMap subs,
+                            ArrayRef<SILValue> args,
                             SmallVectorImpl<SILValue> &yields);
   void emitEndApplyWithRethrow(SILLocation loc,
                                MultipleValueInstructionResult *token,
