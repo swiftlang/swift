534fb0f0b0c1dc4888655e7ca7fcae7e709cc0a5
Philippe Hausler <phausler@apple.com>
2025-04-10 10:04:57 -0700

    Integrate feedback for making the initializer non-nullable and split off an `untilFinished` version that allows developers to specify when the iteration should terminate (including Optional elements)

--- stdlib/public/Observation/Sources/Observation/Observed.swift
+++ stdlib/public/Observation/Sources/Observation/Observed.swift
@@ -19,6 +19,16 @@ import _Concurrency
 /// starting from the willSet of the first mutation to the next suspension point of the safe access.
 @available(SwiftStdlib 9999, *)
 public struct Observed<Element: Sendable, Failure: Error>: AsyncSequence, Sendable {
+  /// An indication type for specifying if an element is the next element or a finishing of iteration
+  ///
+  /// This is used in conjunction with `Observed.untilFinished` to emit values until a `.finish` is 
+  /// returned. All other elements in the observation emit closure for `untilFinished` should return
+  /// `.next(element)` to indicate a properly formed next element from the observation closure.
+  public enum Iteration: Sendable {
+    case next(Element)
+    case finish
+  }
+  
   struct State {
     enum Continuation {
       case cancelled
@@ -109,28 +119,67 @@ public struct Observed<Element: Sendable, Failure: Error>: AsyncSequence, Sendab
     }
   }
   
+  // @isolated(any) closures cannot be composed and retain or forward their isolation
+  // this basically would be replaced with `{ .next(elementProducer()) }` if that
+  // were to become possible.
+  enum Emit {
+    case iteration(@isolated(any) @Sendable () throws(Failure) -> Iteration)
+    case element(@isolated(any) @Sendable () throws(Failure) -> Element)
+    
+    var isolation: (any Actor)? {
+      switch self {
+      case .iteration(let closure): closure.isolation
+      case .element(let closure): closure.isolation
+      }
+    }
+  }
+  
   let state: _ManagedCriticalState<State>
-  let emit: @isolated(any) @Sendable () throws(Failure) -> Element?
+  let emit: Emit
+  
+  // internal funnel method for initialziation
+  internal init(emit: Emit) {
+    self.emit = emit
+    self.state = _ManagedCriticalState(State())
+  }
   
   /// Constructs an asynchronous sequence for a given closure by tracking changes of `@Observable` types.
   ///
-  /// The emit closure is responsible for extracting a value out of a single or many `@Observable` types. When `nil` is
-  /// returned the sequence terminates and no more values will be produced. Likewise when an error is thrown, iterations will
-  /// catch that error and the sequence will be in a terminal state, no longer producing values.
+  /// The emit closure is responsible for extracting a value out of a single or many `@Observable` types. When
+  /// this initializer is invoked the closure inherits the current actor isolation and subseqent calls made 
+  /// internally by `Observed` re-invoke the closure on that isolation if present. This means that if the 
+  /// `Observed` is constructed on the `@MainActor`, all following calls to the emit closure will also be 
+  /// isolated to the `@MainActor` and likewise for other isolations.
   ///
-  /// When constructing an `Observed` asynchronous sequence it is strongly suggested that the isolation parameter is left
-  /// to the default value. Other values specified are only for advanced and unique use cases and may result in unexpected
-  /// behavior.
+  /// In the case that this method is used in a `nonisolated` context it then means that the usage point
+  /// must maintain rules pertaining to the `Sendable` nature of captured types. This method and other 
+  /// parts of `Observed` do not add additional concurrency protection for these cases; so types must 
+  /// be safe to maintain the safe construction and usage of `Observed` when called in an explicitly 
+  /// `nonisolated` isolation domain.
   ///
   /// - Parameters:
   ///     - isolation:  The concurrency isolation domain of the caller.
   ///     - emit: A closure to generate an element for the sequence.
-  ///
   public init(
-    @_inheritActorContext _ emit: @escaping @isolated(any) @Sendable () throws(Failure) -> Element?
+    @_inheritActorContext _ emit: @escaping @isolated(any) @Sendable () throws(Failure) -> Element
   ) {
-    self.emit = emit
-    self.state = _ManagedCriticalState(State())
+    self.init(emit: .element(emit))
+  }
+  
+  /// Constructs an asynchronous sequence for a given closure by tracking changes of `@Observable` types.
+  ///
+  /// The emit closure is responsible for extracting a value out of a single or many `@Observable` types. This method
+  /// continues to be invoked until the .finished option is returned or an error is thrown. Additionally the emit
+  /// closure follows the same isolation rules as the initializer form; where isolation is preserved from the 
+  /// initial invocation and restored if present to ensure the closure is always isolated to the initial construction
+  /// isolation domain.
+  ///
+  /// - Parameters:
+  ///     - emit: A closure to generate an element for the sequence.
+  public static func untilFinished(
+    @_inheritActorContext _ emit: @escaping @isolated(any) @Sendable () throws(Failure) -> Iteration
+  ) -> Observed<Element, Failure> {
+    .init(emit: .iteration(emit))
   }
   
   public struct Iterator: AsyncIteratorProtocol {
@@ -138,16 +187,21 @@ public struct Observed<Element: Sendable, Failure: Error>: AsyncSequence, Sendab
     // 1) to store a critical region of state of the mutations
     // 2) to idenitify the termination of _this_ sequence
     var state: _ManagedCriticalState<State>?
-    let emit: @isolated(any) @Sendable () throws(Failure) -> Element?
+    let emit: Emit
     
     // this is the primary implementation of the tracking
     // it is bound to be called on the specified isolation of the construction
-    fileprivate static func trackEmission(isolation trackingIsolation: isolated (any Actor)?, state: _ManagedCriticalState<State>, emit: @escaping @isolated(any) @Sendable () throws(Failure) -> Element?) throws(Failure) -> Element? {
+    fileprivate static func trackEmission(isolation trackingIsolation: isolated (any Actor)?, state: _ManagedCriticalState<State>, emit: Emit) throws(Failure) -> Iteration {
       // this ferries in an intermediate form with Result to skip over `withObservationTracking` not handling errors being thrown
       // particularly this case is that the error is also an iteration state transition data point (it terminates the sequence)
       // so we need to hold that to get a chance to catch and clean-up
       let result = withObservationTracking {
-        Result(catching: emit)
+        switch emit {
+        case .element(let element):
+          Result(catching: element).map { Iteration.next($0) }
+        case .iteration(let iteration):
+          Result(catching: iteration)
+        }
       } onChange: { [state] in
         // resume all cases where the awaiting continuations are awaiting a willSet
         State.emitWillChange(state)
@@ -175,11 +229,10 @@ public struct Observed<Element: Sendable, Failure: Error>: AsyncSequence, Sendab
         return try terminate(id: id)
       }
       // start by directly tracking the emission via a withObservation tracking on the isolation specified fro mthe init
-      guard let element = try await Iterator.trackEmission(isolation: emit.isolation, state: state, emit: emit) else {
-        // the user returned a nil from the closure so terminate the sequence
-        return try terminate(id: id)
+      switch try await Iterator.trackEmission(isolation: emit.isolation, state: state, emit: emit) {
+      case .finish: return try terminate(id: id)
+      case .next(let element): return element
       }
-      return element
     }
     
     public mutating func next(isolation iterationIsolation: isolated (any Actor)? = #isolation) async throws(Failure) -> Element? {
