c51eaa336ad5e453e9d97919b28d46796ae8ad81
Max Desiatov <m_desiatov@apple.com>
2025-04-10 13:35:28 +0100

    Revert "Revert "Concurrency: Move code between `Executor{Bridge,Impl}.cpp`"" (#80692)
    * Revert "Revert "Concurrency: Move code between `Executor{Bridge,Impl}.cpp`""
    
    * Update CMakeLists.txt

--- stdlib/public/Concurrency/ExecutorBridge.cpp
+++ stdlib/public/Concurrency/ExecutorBridge.cpp
@@ -35,81 +35,9 @@ SerialExecutorRef swift_getMainExecutor() {
 #endif
 
 extern "C" SWIFT_CC(swift)
-void _swift_task_checkIsolatedSwift(
-  HeapObject *executor,
-  const Metadata *executorType,
-  const SerialExecutorWitnessTable *witnessTable
-);
-
-extern "C" SWIFT_CC(swift)
 void _swift_task_checkIsolatedSwift(HeapObject *executor,
                                     const Metadata *executorType,
                                     const SerialExecutorWitnessTable *witnessTable);
-
-extern "C" SWIFT_CC(swift)
-bool _swift_task_isIsolatingCurrentContextSwift(
-  HeapObject *executor,
-  const Metadata *executorType,
-  const SerialExecutorWitnessTable *witnessTable
-);
-
-extern "C" SWIFT_CC(swift)
-bool _swift_task_isMainExecutorSwift(
-  HeapObject *executor,
-  const Metadata *executorType,
-  const SerialExecutorWitnessTable *witnessTable
-);
-
-extern "C" SWIFT_CC(swift)
-void swift_task_checkIsolatedImpl(SerialExecutorRef executor) {
-  HeapObject *identity = executor.getIdentity();
-
-  // We might be being called with an actor rather than a "proper"
-  // SerialExecutor; in that case, we won't have a SerialExecutor witness
-  // table.
-  if (executor.hasSerialExecutorWitnessTable()) {
-    _swift_task_checkIsolatedSwift(identity,
-                                   swift_getObjectType(identity),
-                                   executor.getSerialExecutorWitnessTable());
-  } else {
-    const Metadata *objectType = swift_getObjectType(executor.getIdentity());
-    auto typeName = swift_getTypeName(objectType, true);
-
-    swift_Concurrency_fatalError(
-      0, "Incorrect actor executor assumption; expected '%.*s' executor.\n",
-      (int)typeName.length, typeName.data);
-  }
-}
-
-extern "C" SWIFT_CC(swift)
-bool swift_task_isIsolatingCurrentContextImpl(SerialExecutorRef executor) {
-  HeapObject *identity = executor.getIdentity();
-
-  // We might be being called with an actor rather than a "proper"
-  // SerialExecutor; in that case, we won't have a SerialExecutor witness
-  // table.
-  if (executor.hasSerialExecutorWitnessTable()) {
-    return _swift_task_isIsolatingCurrentContextSwift(identity,
-                                   swift_getObjectType(identity),
-                                   executor.getSerialExecutorWitnessTable());
-  } else {
-    const Metadata *objectType = swift_getObjectType(executor.getIdentity());
-    auto typeName = swift_getTypeName(objectType, true);
-
-    swift_Concurrency_fatalError(
-      0, "Incorrect actor executor assumption; expected '%.*s' executor.\n",
-      (int)typeName.length, typeName.data);
-  }
-}
-
-extern "C" SWIFT_CC(swift)
-bool swift_task_isMainExecutorImpl(SerialExecutorRef executor) {
-  HeapObject *identity = executor.getIdentity();
-  return executor.hasSerialExecutorWitnessTable()
-    && _swift_task_isMainExecutorSwift(identity,
-                                       swift_getObjectType(identity),
-                                       executor.getSerialExecutorWitnessTable());
-}
 
 extern "C" SWIFT_CC(swift)
 uint8_t swift_job_getPriority(Job *job) {
