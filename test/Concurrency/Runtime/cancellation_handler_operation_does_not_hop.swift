be2b2ad3a4ff64ec6263445655f3ae1c730d91e7
Konrad 'ktoso' Malawski <ktoso@apple.com>
2025-04-11 11:47:09 +0900

    [Concurrency] Use caller execution for withTaskCannelation handler
    The problem is that even with the #isolation parameter the non-Sendable
    async closure operation _still_ would potentially hop off the caller
    isolation. We introduced this change because the plan was the
    non-Sendable closure would run on the isolated parameter's isolation,
    but that's not actually the case:
    
    Instead, we can use the @execution(caller) on the function and closure,
    in order to guarantee there is no hop between those at all, and
    developers can trust that adding this cancellation handler will not
    cause any unexpected isolation changes and hops. The API was always
    documented to not hop as we execute the operation, so this brings the
    correct and expected behavior.
    
    resolves rdar://140110775
    
    move to nonisolated(nonsending)
    
    [Concurrency] latest cancellation handler is emit into client; no ABI entry

---
+++ test/Concurrency/Runtime/cancellation_handler_operation_does_not_hop.swift
@@ -0,0 +1,71 @@
+// RUN: %target-run-simple-swift( -target %target-swift-5.1-abi-triple %import-libdispatch) | %FileCheck %s
+// REQUIRES: concurrency
+// REQUIRES: executable_test
+
+// REQUIRES: concurrency_runtime
+// UNSUPPORTED: back_deployment_runtime
+// UNSUPPORTED: freestanding
+
+//public func test<T>(_ block: nonisolated(nonsending) () async -> T ) async  {
+//  await block()
+//}
+
+actor Canceller {
+  var hello: String = "checking..."
+
+  func testFunc() async {
+    await withTaskCancellationHandler {
+      self.assertIsolated("wat in \(#function)!")
+      print("testFunc.withTaskCancellationHandler") // CHECK: testFunc.withTaskCancellationHandler
+      self.hello = "done!"
+    } onCancel: {
+      // noop
+    }
+
+    // just a simple check to see we executed the closure
+
+    await globalTestFunc()
+  }
+}
+func globalTestFunc(isolation: isolated (any Actor)? = #isolation) async {
+  isolation!.assertIsolated("wat in \(#function)!")
+  await withTaskCancellationHandler {
+     isolation!.assertIsolated("wat in \(#function)!")
+     print("globalTestFunc.withTaskCancellationHandler") // CHECK: globalTestFunc.withTaskCancellationHandler
+  } onCancel: {
+    // noop
+  }
+}
+
+@MainActor
+func testMainActor() async {
+  MainActor.preconditionIsolated("Expected main actor")
+  await withTaskCancellationHandler {
+    MainActor.preconditionIsolated("expected MainActor")
+  } onCancel: {
+    // noop
+  }
+}
+
+func testMainActorIsolated(isolation: isolated (any Actor)? = #isolation) async {
+  isolation!.preconditionIsolated("Expected main actor")
+  MainActor.preconditionIsolated("Expected main actor")
+  await withTaskCancellationHandler {
+    print("_unsafeInheritExecutor_withTaskCancellationHandler")
+    MainActor.preconditionIsolated("expected MainActor")
+  } onCancel: {
+    // noop
+  }
+}
+
+_ = await Canceller().testFunc()
+
+_ = await Task { @MainActor in
+  await testMainActor()
+}.value
+
+_ = await Task { @MainActor in
+  await testMainActorIsolated()
+}.value
+
+print("done") // CHECK: done
