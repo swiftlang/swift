24d168b1e4b11da7c82de21a18002c7eed40c740
Pavel Yaskevich <pyaskevich@apple.com>
2025-04-15 15:02:54 -0700

    [AST] ASTPrinter: Don't print '@escaping' on 'inout' parameters
    All of the `inout` types are implicitly `@escaping`.
    
    (cherry picked from commit ee38182ae22fc8b2d7f26be66c7e1382bd8be7b3)

--- test/ModuleInterface/execution_behavior_attrs.swift
+++ test/ModuleInterface/execution_behavior_attrs.swift
@@ -10,6 +10,11 @@ public struct TestWithAttrs {
   // CHECK-NEXT: public func test(_: nonisolated(nonsending) @escaping () async -> Swift.Void)
   // CHECK-NEXT: #endif
   public func test(_: nonisolated(nonsending) @escaping () async -> Void) {}
+
+  // CHECK: #if compiler(>=5.3) && $AsyncExecutionBehaviorAttributes
+  // CHECK-NEXT: public func withInOut(fn: nonisolated(nonsending) inout () async -> Swift.Void)
+  // CHECK-NEXT: #endif
+  public func withInOut(fn: nonisolated(nonsending) inout () async -> Void) {}
 }
 
 public struct Test {
