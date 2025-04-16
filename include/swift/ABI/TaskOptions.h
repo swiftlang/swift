fb06f683fb5440bc1f91d338fa804dc5a91dc230
Max Desiatov <m_desiatov@apple.com>
2025-04-16 21:33:08 +0100

    Embedded: fix arguments mismatch in `ResultTypeInfo`
    Mismatched number of arguments traps on Wasm. Second argument of `destroy` in VW seems unused, so let's pass `nullptr` as a workaround.

--- include/swift/ABI/TaskOptions.h
+++ include/swift/ABI/TaskOptions.h
@@ -199,7 +199,7 @@ public:
   AsyncLet *getAsyncLet() const {
     return asyncLet;
   }
-  
+
   void *getResultBuffer() const {
     return resultBuffer;
   }
@@ -224,7 +224,7 @@ class ResultTypeInfoTaskOptionRecord : public TaskOptionRecord {
             storeEnumTagSinglePayload)(OpaqueValue *, unsigned, unsigned);
 
   void (*__ptrauth_swift_value_witness_function_pointer(
-      SpecialPointerAuthDiscriminators::Destroy) destroy)(OpaqueValue *);
+      SpecialPointerAuthDiscriminators::Destroy) destroy)(OpaqueValue *, void *);
 
   static bool classof(const TaskOptionRecord *record) {
     return record->getKind() == TaskOptionRecordKind::ResultTypeInfo;
