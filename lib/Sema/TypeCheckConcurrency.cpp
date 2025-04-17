3de72c54528405c7ed738c11fd662a52f6ba7051
Pavel Yaskevich <pyaskevich@apple.com>
2025-04-16 17:11:48 -0700

    [TypeChecker] Allow closures to assume `nonisolated(nonsending)`
    Always infer `nonisolated(nonsending)` from context directly on
    a closure unless the closure is marked as `@concurrent`, otherwise
    the closure is not going to get correct isolation and going to hop
    to the wrong executor in its preamble.
    
    Resolves: rdar://149107104

--- lib/Sema/TypeCheckConcurrency.cpp
+++ lib/Sema/TypeCheckConcurrency.cpp
@@ -4696,6 +4696,13 @@ ActorIsolation ActorIsolationChecker::determineClosureIsolation(
       }
     }
 
+    // `nonisolated(nonsending)` inferred from the context makes
+    // the closure caller isolated.
+    if (auto *closureTy = getType(closure)->getAs<FunctionType>()) {
+      if (closureTy->getIsolation().isNonIsolatedCaller())
+        return ActorIsolation::forCallerIsolationInheriting();
+    }
+
     // If a closure has an isolated parameter, it is isolated to that
     // parameter.
     for (auto param : *closure->getParameters()) {
