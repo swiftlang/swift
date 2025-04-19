af6b4ad7a1a62744b288774156aeec4fbbf9bb2f
kntkymt <kntkc0de@gmail.com>
2025-04-19 14:36:11 +0900

    [Parser] fix parse trailing commna in string interpolation at the end of parseListItem flow instead

--- lib/Parse/Parser.cpp
+++ lib/Parse/Parser.cpp
@@ -1057,11 +1057,18 @@ Parser::parseListItem(ParserStatus &Status, tok RightK, SourceLoc LeftLoc,
       return ParseListItemResult::Finished;
   }
   if (consumeIf(tok::comma)) {
-    if (Tok.isNot(RightK))
+    if (Tok.isNot(RightK) && !tokIsStringInterpolationEOF(Tok, RightK))
       return ParseListItemResult::Continue;
     if (!AllowSepAfterLast) {
       diagnose(Tok, diag::unexpected_separator, ",").fixItRemove(PreviousLoc);
     }
+    
+    // Enable trailing comma in string literal interpolation
+    if (tokIsStringInterpolationEOF(Tok, RightK)) {
+      RightLoc = Tok.getLoc();
+      return ParseListItemResult::FinishedInStringInterpolation;
+    }
+    
     return ParseListItemResult::Finished;
   }
   // If we're in a comma-separated list, the next token is at the
