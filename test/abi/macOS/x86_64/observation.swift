39df5db3c3f7acbeb110d2d712e90d33ff692f55
Philippe Hausler <phausler@apple.com>
2025-04-22 16:40:29 -0700

    Correct the abi assertions to be the tests instead of altering the baselines

--- test/abi/macOS/x86_64/observation.swift
+++ test/abi/macOS/x86_64/observation.swift
@@ -41,3 +41,48 @@ Added: _$s11Observation0A8TrackingV7changeds10AnyKeyPathCSgvg
 
 // property descriptor for Observation.ObservationTracking.changed : Swift.AnyKeyPath?
 Added: _$s11Observation0A8TrackingV7changeds10AnyKeyPathCSgvpMV
+
+// static Observation.Observed.untilFinished(@isolated(any) @Sendable () throws(B) -> Observation.Observed<A, B>.Iteration) -> Observation.Observed<A, B>
+Added: _$s11Observation8ObservedV13untilFinishedyACyxq_GAC9IterationOyxq__GyYbq_YKYAcFZ
+
+// Observation.Observed.makeAsyncIterator() -> Observation.Observed<A, B>.Iterator
+Added: _$s11Observation8ObservedV17makeAsyncIteratorAC0E0Vyxq__GyF
+
+// Observation.Observed.Iterator.next(isolation: isolated Swift.Actor?) async throws(B) -> A?
+Added: _$s11Observation8ObservedV8IteratorV4next9isolationxSgScA_pSgYi_tYaq_YKF
+
+// async function pointer to Observation.Observed.Iterator.next(isolation: isolated Swift.Actor?) async throws(B) -> A?
+Added: _$s11Observation8ObservedV8IteratorV4next9isolationxSgScA_pSgYi_tYaq_YKFTu
+
+// type metadata accessor for Observation.Observed.Iterator
+Added: _$s11Observation8ObservedV8IteratorVMa
+
+// nominal type descriptor for Observation.Observed.Iterator
+Added: _$s11Observation8ObservedV8IteratorVMn
+
+// protocol conformance descriptor for Observation.Observed<A, B>.Iterator : Swift.AsyncIteratorProtocol in Observation
+Added: _$s11Observation8ObservedV8IteratorVyxq__GScIAAMc
+
+// enum case for Observation.Observed.Iteration.next<A, B where A: Swift.Sendable, B: Swift.Error>(Observation.Observed<A, B>.Iteration.Type) -> (A) -> Observation.Observed<A, B>.Iteration
+Added: _$s11Observation8ObservedV9IterationO4nextyAEyxq__GxcAGms8SendableRzs5ErrorR_r0_lFWC
+
+// enum case for Observation.Observed.Iteration.finish<A, B where A: Swift.Sendable, B: Swift.Error>(Observation.Observed<A, B>.Iteration.Type) -> Observation.Observed<A, B>.Iteration
+Added: _$s11Observation8ObservedV9IterationO6finishyAEyxq__GAGms8SendableRzs5ErrorR_r0_lFWC
+
+// type metadata accessor for Observation.Observed.Iteration
+Added: _$s11Observation8ObservedV9IterationOMa
+
+// nominal type descriptor for Observation.Observed.Iteration
+Added: _$s11Observation8ObservedV9IterationOMn
+
+// type metadata accessor for Observation.Observed
+Added: _$s11Observation8ObservedVMa
+
+// nominal type descriptor for Observation.Observed
+Added: _$s11Observation8ObservedVMn
+
+// Observation.Observed.init(@isolated(any) @Sendable () throws(B) -> A) -> Observation.Observed<A, B>
+Added: _$s11Observation8ObservedVyACyxq_GxyYbq_YKYAccfC
+
+// protocol conformance descriptor for Observation.Observed<A, B> : Swift.AsyncSequence in Observation
+Added: _$s11Observation8ObservedVyxq_GSciAAMc
