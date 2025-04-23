54e0607091e1b7818cfa9b3b09b8dadee65c1f8c
Hamish Knight <hamish_knight@apple.com>
2025-04-23 12:54:17 +0100

    [Sema] Add missing null check for Type
    `getType` here can return a null type if the queried expression isn't
    part of the solution, which can currently happen for code completion.
    I'm working on a more principled fix for this, but until then this is
    a low-risk fix that will unblock the stress tester and can be
    cherry-picked to 6.2.
    
    rdar://149759542

--- lib/Sema/TypeCheckConcurrency.cpp
+++ lib/Sema/TypeCheckConcurrency.cpp
@@ -4700,9 +4700,11 @@ ActorIsolation ActorIsolationChecker::determineClosureIsolation(
 
     // `nonisolated(nonsending)` inferred from the context makes
     // the closure caller isolated.
-    if (auto *closureTy = getType(closure)->getAs<FunctionType>()) {
-      if (closureTy->getIsolation().isNonIsolatedCaller())
-        return ActorIsolation::forCallerIsolationInheriting();
+    if (auto closureTy = getType(closure)) {
+      if (auto *closureFnTy = closureTy->getAs<FunctionType>()) {
+        if (closureFnTy->getIsolation().isNonIsolatedCaller())
+          return ActorIsolation::forCallerIsolationInheriting();
+      }
     }
 
     // If a closure has an isolated parameter, it is isolated to that
