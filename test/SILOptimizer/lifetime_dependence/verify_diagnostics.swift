bb6b5b364591720644e39b7a5025f56f6f072948
Andrew Trick <atrick@apple.com>
2025-04-22 22:15:39 -0700

    Fix LifetimeDependenceDiagnostics: handle drop_deinit unsafeAddress
    This adds support to handle unsafe addressors, which generate mark_dependence
    instructions with an address base. This case does not arise with lifetime
    dependencies. Nonetheless, the lifetime dependence diagnostics kick in to
    attempt to promote the unsafe addressor's mark_dependence to noescape, so we
    need to handle it.
    
    rdar://149784450 (Compiler crash with non-escapable deinit dereferencing)
    (cherry picked from commit 3b08592ed21210a451c2e82a3f8ef38b011ae7b9)

--- test/SILOptimizer/lifetime_dependence/verify_diagnostics.swift
+++ test/SILOptimizer/lifetime_dependence/verify_diagnostics.swift
@@ -2,15 +2,39 @@
 // RUN:   -o /dev/null \
 // RUN:   -verify \
 // RUN:   -sil-verify-all \
+// RUN:   -enable-builtin-module \
 // RUN:   -module-name test \
 // RUN:   -define-availability "Span 0.1:macOS 9999, iOS 9999, watchOS 9999, tvOS 9999, visionOS 9999" \
-// RUN:   -enable-experimental-feature LifetimeDependence
+// RUN:   -enable-experimental-feature LifetimeDependence \
+// RUN:   -enable-experimental-feature AddressableParameters
 
 // REQUIRES: swift_in_compiler
 // REQUIRES: swift_feature_LifetimeDependence
+// REQUIRES: swift_feature_AddressableParameters
 
 // Test diagnostic output for interesting corner cases. Similar to semantics.swift, but this tests corner cases in the
 // implementation as opposed to basic language rules.
+
+import Builtin
+
+struct Borrow<T: ~Copyable>: Copyable, ~Escapable {
+  let pointer: UnsafePointer<T>
+
+  @lifetime(borrow value)
+  init(_ value: borrowing @_addressable T) {
+    pointer = UnsafePointer(Builtin.unprotectedAddressOfBorrow(value))
+  }
+
+  subscript() -> T {
+    unsafeAddress {
+      pointer
+    }
+  }
+}
+
+struct A {}
+
+func useA(_:A){}
 
 // Test that conditionally returning an Optional succeeds.
 //
@@ -34,4 +58,12 @@ extension Array {
 @lifetime(&array)
 func getImmutableSpan(_ array: inout [Int]) -> Span<Int> {
  return array.span
+}
+
+struct TestDeinitCallsAddressor: ~Copyable, ~Escapable {
+  let a: Borrow<A>
+
+  deinit {
+    useA(a[])
+  }
 }
