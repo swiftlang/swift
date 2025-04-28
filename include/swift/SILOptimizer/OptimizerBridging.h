5885421c06c540d418378748f7337317757ad9a8
Meghana Gupta <meghana_gupta@apple.com>
2025-04-28 16:37:20 -0700

    Bridge getSwiftMutableSpanDecl() and isBuiltinType()

--- include/swift/SILOptimizer/OptimizerBridging.h
+++ include/swift/SILOptimizer/OptimizerBridging.h
@@ -215,6 +215,7 @@ struct BridgedPassContext {
   SWIFT_IMPORT_UNSAFE BRIDGED_INLINE BridgedDomTree getDomTree() const;
   SWIFT_IMPORT_UNSAFE BRIDGED_INLINE BridgedPostDomTree getPostDomTree() const;
   SWIFT_IMPORT_UNSAFE BRIDGED_INLINE BridgedDeclObj getSwiftArrayDecl() const;
+  SWIFT_IMPORT_UNSAFE BRIDGED_INLINE BridgedDeclObj getSwiftMutableSpanDecl() const;
 
   // AST
 
