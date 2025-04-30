2d7b7a73f8f1a84f09269f661c375ad4e1a3d1d8
Yeoul Na <yeoul_na@apple.com>
2025-04-30 08:53:22 -0700

    [Swiftify] SafeInteropWrapper crashes with 'cgImage.width' (#80948)
    * [Swiftify] Fix crash calling cgImage.width
    
    cgImage.width calls the C function, CGImageGetWidth(CGImageRef).
    Swift representation and Clang representation of this function
    seem to have a parameter number mismatch, causing swiftify function
    to crash.
    
    rdar://149691207
    
    * [NFC] Fix test/Interop/ObjC/swiftify-import/getter.swift

--- lib/ClangImporter/ImportDecl.cpp
+++ lib/ClangImporter/ImportDecl.cpp
@@ -4745,7 +4745,7 @@ namespace {
       auto loc = Impl.importSourceLoc(decl->getLocation());
       auto dc = Impl.importDeclContextOf(
           decl, importedName.getEffectiveContext());
-      
+
       SmallVector<GenericTypeParamDecl *, 4> genericParams;
       for (auto &param : *decl->getTemplateParameters()) {
         auto genericParamDecl =
@@ -5528,7 +5528,7 @@ namespace {
                                               objcClass->getDeclaredType());
       Impl.SwiftContext.evaluator.cacheOutput(ExtendedNominalRequest{result},
                                               std::move(objcClass));
-      
+
       Identifier categoryName;
       if (!decl->getName().empty())
         categoryName = Impl.SwiftContext.getIdentifier(decl->getName());
@@ -9155,6 +9155,9 @@ void ClangImporter::Implementation::swiftify(FuncDecl *MappedDecl) {
   if (!ClangDecl)
     return;
 
+  if (ClangDecl->getNumParams() != MappedDecl->getParameters()->size())
+    return;
+
   llvm::SmallString<128> MacroString;
   // We only attach the macro if it will produce an overload. Any __counted_by
   // will produce an overload, since UnsafeBufferPointer is still an improvement
@@ -9570,7 +9573,7 @@ ClangImporter::Implementation::importDeclImpl(const clang::NamedDecl *ClangDecl,
     Result = importDecl(UnderlyingDecl, version);
     SkippedOverTypedef = true;
   }
-  
+
   if (!Result) {
     SwiftDeclConverter converter(*this, version);
     Result = converter.Visit(ClangDecl);
