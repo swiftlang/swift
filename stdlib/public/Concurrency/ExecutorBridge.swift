7f5f45ced239eb8889d479b830708fea779776cd
Alastair Houghton <ahoughton@apple.com>
2025-04-30 14:18:42 +0100

    [Concurrency] Fix issue with using Dispatch queues as executors.
    We were failing to switch executors to Dispatch queues, where those
    were being used as executors, which caused a variety of unusual
    symptoms.
    
    rdar://150310927

--- stdlib/public/Concurrency/ExecutorBridge.swift
+++ stdlib/public/Concurrency/ExecutorBridge.swift
@@ -119,3 +119,8 @@ internal func _dispatchEnqueueWithDeadline(_ global: CBool,
 @available(SwiftStdlib 6.2, *)
 @_silgen_name("swift_dispatchAssertMainQueue")
 internal func _dispatchAssertMainQueue()
+
+@_silgen_name("swift_getDispatchQueueForExecutor")
+internal func _getDispatchQueueForExecutor(
+  _ executor: UnownedSerialExecutor
+) -> OpaquePointer?
