a11f234ab690892e3623040b26974eed7904d4e7
Henrik G. Olsson <hnrklssn@gmail.com>
2025-05-04 01:52:05 -0700

    [Swiftify] Don't use count from Span inside withUnsafeBufferPointer call (#81267)
    (cherry picked from commit bd233ea26e25df62cc42b2a3597bbfc5831489b2)

--- test/Interop/C/swiftify-import/sized-by-noescape.swift
+++ test/Interop/C/swiftify-import/sized-by-noescape.swift
@@ -12,8 +12,50 @@ import SizedByNoEscapeClang
 // CHECK:      @_alwaysEmitIntoClient public func complexExpr(_ len: Int{{.*}}, _ offset: Int{{.*}}, _ p: RawSpan)
 // CHECK-NEXT: @_alwaysEmitIntoClient public func nonnull(_ p: RawSpan)
 // CHECK-NEXT: @_alwaysEmitIntoClient public func nullUnspecified(_  p: RawSpan)
+// CHECK-NEXT: @_alwaysEmitIntoClient public func nullable(_  p: RawSpan?)
 // CHECK-NEXT: @_alwaysEmitIntoClient public func opaque(_  p: RawSpan)
 // CHECK-NEXT: @_alwaysEmitIntoClient @_disfavoredOverload public func returnPointer(_ len: Int{{.*}}) -> UnsafeRawBufferPointer
 // CHECK-NEXT: @_alwaysEmitIntoClient public func shared(_ len: Int{{.*}}, _ p1: RawSpan, _ p2: RawSpan)
 // CHECK-NEXT: @_alwaysEmitIntoClient public func simple(_  p: RawSpan)
 // CHECK-NEXT: @_alwaysEmitIntoClient public func swiftAttr(_  p: RawSpan)
+
+@inlinable
+public func callComplexExpr(_ p: RawSpan) {
+  complexExpr(CInt(p.byteCount), 1, p)
+}
+
+@inlinable
+public func callNonnull(_ p: RawSpan) {
+  nonnull(p)
+}
+
+@inlinable
+public func callNullUnspecified(_ p: RawSpan) {
+  nullUnspecified(p)
+}
+
+@inlinable
+public func callNullable(_ p: RawSpan?) {
+  nullable(p)
+}
+
+@inlinable
+public func callReturnPointer() {
+  let a: UnsafeRawBufferPointer? = returnPointer(4) // call wrapper
+  let b: UnsafeRawPointer? = returnPointer(4) // call unsafe interop
+}
+
+@inlinable
+public func callShared(_ p: RawSpan, _ p2: RawSpan) {
+  shared(CInt(p.byteCount), p, p2)
+}
+
+@inlinable
+public func callSimple(_ p: RawSpan) {
+  simple(p)
+}
+
+@inlinable
+public func callSwiftAttr(_ p: RawSpan) {
+  swiftAttr(p)
+}
