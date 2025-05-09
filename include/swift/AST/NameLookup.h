d72afad86f9847ddb5bc7fe866323e86285553ce
Becca Royal-Gordon <beccadax@apple.com>
2025-05-08 19:37:58 -0700

    Handle module selectors in qualified lookup

--- include/swift/AST/NameLookup.h
+++ include/swift/AST/NameLookup.h
@@ -486,6 +486,14 @@ public:
 /// \returns true if any declarations were removed, false otherwise.
 bool removeOverriddenDecls(SmallVectorImpl<ValueDecl*> &decls);
 
+/// Remove any declarations in the given set that do not match the
+/// module selector, if it is not empty.
+///
+/// \returns true if any declarations were removed, false otherwise.
+bool removeOutOfModuleDecls(SmallVectorImpl<ValueDecl*> &decls,
+                            Identifier moduleSelector,
+                            const DeclContext *dc);
+
 /// Remove any declarations in the given set that are shadowed by
 /// other declarations in that set.
 ///
@@ -561,6 +569,7 @@ void tryExtractDirectlyReferencedNominalTypes(
 /// Once name lookup has gathered a set of results, perform any necessary
 /// steps to prune the result set before returning it to the caller.
 void pruneLookupResultSet(const DeclContext *dc, NLOptions options,
+                          Identifier moduleSelector,
                           SmallVectorImpl<ValueDecl *> &decls);
 
 /// Do nothing if debugClient is null.
