39006ccebc690fed0ae9f84c336b5daa542faa87
Erik Eckstein <eeckstein@apple.com>
2025-05-09 19:14:03 +0200

    Optimizer: simplify `unchecked_addr_cast` for vectors
    Reimplement the simplification in swift and add a new transformation:
    ```
      %1 = unchecked_addr_cast %0 : $*Builtin.FixedArray<N, Element> to $*Element
    ```
    ->
    ```
      %1 = vector_base_addr %0 : $*Builtin.FixedArray<N, Element>
    ```

--- SwiftCompilerSources/Sources/Optimizer/PassManager/PassRegistration.swift
+++ SwiftCompilerSources/Sources/Optimizer/PassManager/PassRegistration.swift
@@ -125,6 +125,7 @@ private func registerSwiftPasses() {
   registerForSILCombine(PointerToAddressInst.self,  { run(PointerToAddressInst.self, $0) })
   registerForSILCombine(UncheckedEnumDataInst.self, { run(UncheckedEnumDataInst.self, $0) })
   registerForSILCombine(WitnessMethodInst.self,     { run(WitnessMethodInst.self, $0) })
+  registerForSILCombine(UncheckedAddrCastInst.self, { run(UncheckedAddrCastInst.self, $0) })
   registerForSILCombine(UnconditionalCheckedCastInst.self, { run(UnconditionalCheckedCastInst.self, $0) })
   registerForSILCombine(AllocStackInst.self,        { run(AllocStackInst.self, $0) })
   registerForSILCombine(ApplyInst.self,             { run(ApplyInst.self, $0) })
