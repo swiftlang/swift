d10fd7118c2cbc8a692f71707022f780f523b4a2
Becca Royal-Gordon <beccadax@apple.com>
2025-05-08 18:39:39 -0700

    [NFC] Give withoutArgumentLabels() a context arg
    It will soon need to allocate sometimes.

--- include/swift/AST/Identifier.h
+++ include/swift/AST/Identifier.h
@@ -791,7 +791,7 @@ public:
     return lhs.compare(rhs) >= 0;
   }
 
-  DeclNameRef withoutArgumentLabels() const;
+  DeclNameRef withoutArgumentLabels(ASTContext &C) const;
   DeclNameRef withArgumentLabels(ASTContext &C,
                                  ArrayRef<Identifier> argumentNames) const;
 
@@ -824,7 +824,7 @@ inline DeclNameRef DeclNameRef::getFromOpaqueValue(void *p) {
   return DeclNameRef(DeclName::getFromOpaqueValue(p));
 }
 
-inline DeclNameRef DeclNameRef::withoutArgumentLabels() const {
+inline DeclNameRef DeclNameRef::withoutArgumentLabels(ASTContext &C) const {
   return DeclNameRef(getBaseName());
 }
 
@@ -832,7 +832,6 @@ inline DeclNameRef DeclNameRef::withArgumentLabels(
     ASTContext &C, ArrayRef<Identifier> argumentNames) const {
   return DeclNameRef(DeclName(C, getBaseName(), argumentNames));
 }
-
 
 inline DeclNameRef DeclNameRef::createSubscript() {
   return DeclNameRef(DeclBaseName::createSubscript());
