cc4d9daea92436c9be7fd2f4f703cde1c66b6764
Hamish Knight <hamish_knight@apple.com>
2025-05-09 14:47:58 +0100

    [AST] Remove unparsed case from `FuncDecl::getSourceRange`
    This wasn't really sound since it could result in source ranges that
    have different buffers for the start and end loc. Instead, adjust
    the parser logic to look at the brace range.

--- lib/AST/Decl.cpp
+++ lib/AST/Decl.cpp
@@ -11453,9 +11453,6 @@ SourceRange FuncDecl::getSourceRange() const {
   if (startLoc.isInvalid())
     return SourceRange();
 
-  if (getBodyKind() == BodyKind::Unparsed)
-    return { startLoc, BodyRange.End };
-
   SourceLoc endLoc = getOriginalBodySourceRange().End;
   if (endLoc.isInvalid()) {
     if (isa<AccessorDecl>(this))
