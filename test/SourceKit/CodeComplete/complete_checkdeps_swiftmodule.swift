de1a430f64c8d16069c0fd870772a250c514be96
Anthony Latsis <alatsis@apple.com>
2025-05-10 01:42:34 +0100

    [test] Fix misspelled FileCheck directives

--- test/SourceKit/CodeComplete/complete_checkdeps_swiftmodule.swift
+++ test/SourceKit/CodeComplete/complete_checkdeps_swiftmodule.swift
@@ -37,7 +37,7 @@ func foo() {
 
 // RUN:   -shell -- echo '### Modify framework (s)' == \
 // RUN:   -shell --  %target-swift-frontend -emit-module -module-name SwiftFW -o %t/Frameworks/SwiftFW.framework/Modules/SwiftFW.swiftmodule/%target-swiftmodule-name $INPUT_DIR/SwiftFW_src_mod/Funcs.swift == \
-// RUN    -shell -- touch -t 210001010101 %t/Frameworks/SwiftFW.framework/Modules/SwiftFW.swiftmodule/%target-swiftmodule-name == \
+// RUN:    -shell -- touch -t 210001010101 %t/Frameworks/SwiftFW.framework/Modules/SwiftFW.swiftmodule/%target-swiftmodule-name == \
 // RUN:   -req=complete -pos=5:3 %s -- ${COMPILER_ARGS[@]} == \
 
 // RUN:   -shell -- echo '### Fast completion' == \
