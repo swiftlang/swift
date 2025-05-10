7e813325a85aa5857260307b8fe146406f706081
Becca Royal-Gordon <beccadax@apple.com>
2025-05-09 18:13:29 -0700

    [NFC] Add representations for DeclNameRef/Loc with module selector

--- include/swift/AST/ASTBridging.h
+++ include/swift/AST/ASTBridging.h
@@ -198,10 +198,13 @@ BridgedDeclNameRef_createParsed(BridgedDeclBaseName cBaseName);
 
 class BridgedDeclNameLoc {
   const void *_Nullable LocationInfo;
-  size_t NumArgumentLabels;
+  uint32_t NumArgumentLabels;
+  bool HasModuleSelectorLoc;
 
 public:
-  BridgedDeclNameLoc() : LocationInfo(nullptr), NumArgumentLabels(0) {}
+  BridgedDeclNameLoc()
+    : LocationInfo(nullptr), NumArgumentLabels(0), HasModuleSelectorLoc(false)
+  {}
 
   BRIDGED_INLINE BridgedDeclNameLoc(swift::DeclNameLoc loc);
 
