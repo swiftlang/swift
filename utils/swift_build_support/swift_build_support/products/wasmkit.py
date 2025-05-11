06bee27b9ebc2469182cdf253a40f637a1a5ad23
Max Desiatov <m_desiatov@apple.com>
2025-05-11 17:36:28 +0100

    Set rpath correctly for installed `wasmkit` binary

--- utils/swift_build_support/swift_build_support/products/wasmkit.py
+++ utils/swift_build_support/swift_build_support/products/wasmkit.py
@@ -56,33 +56,44 @@ class WasmKit(product.Product):
         """
         install_destdir = self.host_install_destdir(host_target)
         build_toolchain_path = install_destdir + self.args.install_prefix + '/bin'
-        shutil.copy(self.bin_path, build_toolchain_path + '/wasmkit')
+        bin_path = run_swift_build(host_target, self, 'wasmkit-cli', set_installation_rpath=True)
+        shutil.copy(bin_path, build_toolchain_path + '/wasmkit')
 
     def build(self, host_target):
-        self.bin_path = run_swift_build(host_target, self, 'wasmkit-cli')
-        print("Built wasmkit-cli at: " + self.bin_path)
+        bin_path = run_swift_build(host_target, self, 'wasmkit-cli')
+        print("Built wasmkit-cli at: " + bin_path)
         # Copy the built binary to ./bin
         dest_bin_path = self.__class__.cli_file_path(self.build_dir)
         print("Copying wasmkit-cli to: " + dest_bin_path)
         os.makedirs(os.path.dirname(dest_bin_path), exist_ok=True)
-        shutil.copy(self.bin_path, dest_bin_path)
+        shutil.copy(bin_path, dest_bin_path)
 
     @classmethod
     def cli_file_path(cls, build_dir):
         return os.path.join(build_dir, 'bin', 'wasmkit-cli')
 
 
-def run_swift_build(host_target, product, swpft_package_product_name):
+def run_swift_build(host_target, product, swiftpm_package_product_name, set_installation_rpath=False):
     # Building with the freshly-built SwiftPM
     swift_build = os.path.join(product.install_toolchain_path(host_target), "bin", "swift-build")
 
+    build_os = host_target.split('-')[0]
+    if set_installation_rpath and not host_target.startswith('macos'):
+        # Library rpath for swift, dispatch, Foundation, etc. when installing
+        rpath_args = [
+            '--disable-local-rpath', '-Xswiftc', '-no-toolchain-stdlib-rpath',
+            '-Xlinker', '-rpath', '-Xlinker', '$ORIGIN/../lib/swift/' + build_os
+        ]
+    else:
+        rpath_args = []
+
     build_args = [
         swift_build,
-        '--product', swpft_package_product_name,
+        '--product', swiftpm_package_product_name,
         '--package-path', os.path.join(product.source_dir),
         '--build-path', product.build_dir,
         '--configuration', 'release',
-    ]
+    ] + rpath_args
 
     if product.args.verbose_build:
         build_args.append('--verbose')
@@ -94,4 +105,4 @@ def run_swift_build(host_target, product, swpft_package_product_name):
 
     bin_dir_path = shell.capture(
         build_args + ['--show-bin-path'], dry_run=False, echo=False).rstrip()
-    return os.path.join(bin_dir_path, swpft_package_product_name)
+    return os.path.join(bin_dir_path, swiftpm_package_product_name)
