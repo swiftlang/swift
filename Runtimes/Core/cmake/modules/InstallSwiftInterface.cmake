283577721561f2e02ed1005dbec06f31de553780
Saleem Abdulrasool <abdulras@thebrowser.company>
2025-05-12 17:36:18 -0700

    Runtimes: install the `.swiftdoc` interface as well
    This is part of the development component for the module and is part of
    the interface definition. This content is consumed by the user during
    development.

--- Runtimes/Core/cmake/modules/InstallSwiftInterface.cmake
+++ Runtimes/Core/cmake/modules/InstallSwiftInterface.cmake
@@ -36,4 +36,16 @@ function(install_swift_interface target)
         COMPONENT SwiftCore_development)
     endif()
   endif()
+
+  # Install Swift documentation interface files.
+  install(FILES "${CMAKE_CURRENT_BINARY_DIR}/$<TARGET_PROPERTY:${target},Swift_MODULE_NAME>.swiftdoc"
+    RENAME "${SwiftCore_MODULE_TRIPLE}.swiftdoc"
+    DESTINATION "${SwiftCore_INSTALL_SWIFTMODULEDIR}/$<TARGET_PROPERTY:${target},Swift_MODULE_NAME>.swiftmodule"
+    COMPONENT SwiftCore_development)
+  if(SwiftCore_VARIANT_MODULE_TRIPLE)
+    install(FILES "${CMAKE_CURRENT_BINARY_DIR}/${target}-${SwiftCore_VARIANT_MODULE_TRIPLE}/${target}.swiftdoc"
+      RENAME "${SwiftCore_VARIANT_MODULE_TRIPLE}.swiftdoc"
+      DESTINATION "${SwiftCore_INSTALL_SWIFTMODULEDIR}/$<TARGET_PROPERTY:${target},Swift_MODULE_NAME>.swiftmodule"
+      COMPONENT SwiftCore_development)
+  endif()
 endfunction()
