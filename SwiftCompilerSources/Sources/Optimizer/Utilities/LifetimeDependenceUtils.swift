22ac782bb31b014a6481ba0fc54849e05f60e05a
Andrew Trick <atrick@apple.com>
2025-05-15 23:54:24 -0700

    Improve LifetimeDependenceDefUseWalker: consult Type.hasCustomDeinit
    Fixes rdar://150828355 (Overlapping access error after the last use
    of a lifetime-dependent span variable)

--- SwiftCompilerSources/Sources/Optimizer/Utilities/LifetimeDependenceUtils.swift
+++ SwiftCompilerSources/Sources/Optimizer/Utilities/LifetimeDependenceUtils.swift
@@ -658,13 +658,19 @@ extension LifetimeDependenceDefUseWalker {
       // Catch .instantaneousUse operations that are dependence leaf uses.
       return leafUse(of: operand)
 
-    case is DestroyValueInst, is EndLifetimeInst, is DeallocRefInst,
-         is DeallocBoxInst, is DeallocExistentialBoxInst,
-         is BeginCOWMutationInst, is EndCOWMutationInst,
-         is EndInitLetRefInst, is DeallocPartialRefInst, is BeginDeallocRefInst:
-      // Catch .destroyingConsume operations that are dependence leaf
-      // uses.
-      return leafUse(of: operand)
+    case is DestroyValueInst:
+      // Handles dependent value destroys.
+      switch operand.value.type.hasCustomDeinit(in: function) {
+      case .HasOnlyDefaultDeinit:
+        // Ignore destroys on a type whose members recursively only have default deinits.
+        return .continueWalk
+      case .MayHaveCustomDeinit:
+        return leafUse(of: operand)
+      }
+
+    case is DeallocBoxInst, is DeallocExistentialBoxInst, is DeallocPartialRefInst, is DeallocRefInst,
+         is EndLifetimeInst:
+      return .continueWalk
 
     case let si as StoringInstruction where si.sourceOperand == operand:
       return visitStoredUses(of: operand, into: si.destinationOperand.value)
