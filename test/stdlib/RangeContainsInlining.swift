ac58408733cdaf2ff1d5eda7385ac4f29e0659bb
Karoy Lorentey <klorentey@apple.com>
2025-05-16 18:42:27 -0700

    [test] Add rudimentary test for transparent bounds checking helpers

--- test/stdlib/RangeContainsInlining.swift
+++ test/stdlib/RangeContainsInlining.swift
@@ -2,38 +2,30 @@
 // RUN: %target-swift-emit-ir -primary-file %s -O 2>&1 | %FileCheck %s
 // RUN: %target-swift-emit-ir -primary-file %s -Osize 2>&1 | %FileCheck %s
 
-// CHECK-LABEL: define hidden swiftcc i1 @"$s17RangeContainsPerf08halfOpenB0ySbSnySiG_SitF"
+
+// CHECK-LABEL: define swiftcc i1 @"$s21RangeContainsInlining08halfOpenB0ySbSnySiG_SitF"
 // CHECK-NOT: call swiftcc
 // CHECK: icmp
 // CHECK-NOT: call swiftcc
 // CHECK-LABEL: }
-func halfOpenContains(_ r: Range<Int>, _ i: Int) -> Bool {
+public func halfOpenContains(_ r: Range<Int>, _ i: Int) -> Bool {
   r.contains(i)
 }
 
-// CHECK-LABEL: define hidden swiftcc i1 @"$s17RangeContainsPerf06closedB0ySbSNySiG_SitF"
+// CHECK-LABEL: define swiftcc i1 @"$s21RangeContainsInlining06closedB0ySbSNySiG_SitF"
 // CHECK-NOT: call swiftcc
 // CHECK: icmp
 // CHECK-NOT: call swiftcc
 // CHECK-LABEL: }
-func closedContains(_ r: ClosedRange<Int>, _ i: Int) -> Bool {
+public func closedContains(_ r: ClosedRange<Int>, _ i: Int) -> Bool {
   r.contains(i)
 }
 
-// CHECK-LABEL: define hidden swiftcc i1 @"$s17RangeContainsPerf20halfOpenPatternMatchySbSnySiG_SitF"
+// CHECK-LABEL: define swiftcc i1 @"$s21RangeContainsInlining20halfOpenPatternMatchySbSnySiG_SitF"
 // CHECK-NOT: call swiftcc
 // CHECK: icmp
 // CHECK-NOT: call swiftcc
 // CHECK-LABEL: }
-func halfOpenPatternMatch(_ r: Range<Int>, _ i: Int) -> Bool {
-  r ~= i
-}
-
-// CHECK-LABEL: define hidden swiftcc i1 @"$s17RangeContainsPerf18closedPatternMatchySbSNySiG_SitF"
-// CHECK-NOT: call swiftcc
-// CHECK: icmp
-// CHECK-NOT: call swiftcc
-// CHECK-LABEL: }
-func closedPatternMatch(_ r: ClosedRange<Int>, _ i: Int) -> Bool {
+public func halfOpenPatternMatch(_ r: Range<Int>, _ i: Int) -> Bool {
   r ~= i
 }
