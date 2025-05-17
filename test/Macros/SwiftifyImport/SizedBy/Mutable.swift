9e943fd37ec2b4aff8ab011ec15c96faef86e0c5
Henrik G. Olsson <h_olsson@apple.com>
2025-05-17 02:25:30 -0700

    [Swiftify] Always remove count parameters when possible
    Previously we did not remove count parameters if any count parameters
    were shared between count expressions, or if any count expression
    contained operations. Buffer sizes were also just checked to be larger
    than or equal than the given count.
    
    We now extract the count from Spans/BufferPointers whenever possible,
    and store that value in a variable at the start of the function. If
    multiple parameters share the same count, a bounds check is emitted to
    make sure that they have the same size. Subspans can be used if one span
    is larger than necessary.
    
    The message in the bounds check is changed so that it includes the
    expected and actual value, to aid in debugging.
    
    This patch also fixes some incorrect indentation, and adds the
    Whitespace.swift test case to act as a regression test in case the
    indentation changes, since the other test cases don't use significant
    whitespace.
    
    rdar://151488820
    rdar://151511090
    rdar://146333006
    rdar://147715799

--- test/Macros/SwiftifyImport/SizedBy/Mutable.swift
+++ test/Macros/SwiftifyImport/SizedBy/Mutable.swift
@@ -8,5 +8,6 @@ func myFunc(_ ptr: UnsafeMutableRawPointer, _ size: CInt) {
 
 // CHECK:      @_alwaysEmitIntoClient @_disfavoredOverload
 // CHECK-NEXT: func myFunc(_ ptr: UnsafeMutableRawBufferPointer) {
-// CHECK-NEXT:     return unsafe myFunc(ptr.baseAddress!, CInt(exactly: ptr.count)!)
+// CHECK-NEXT:     let size = CInt(exactly: unsafe ptr.count)!
+// CHECK-NEXT:     return unsafe myFunc(ptr.baseAddress!, size)
 // CHECK-NEXT: }
