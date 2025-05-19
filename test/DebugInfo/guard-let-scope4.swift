04166e21c968d193b5bf9b3aa9501e34fab4765e
Mykola Pokhylets <mykola.pokhylets@gmail.com>
2025-05-19 17:03:59 +0200

    Fixed some tests
    Remaining failures:
    
    SILGen/unowned-class-bound-generic-parameter.swift

--- test/DebugInfo/guard-let-scope4.swift
+++ test/DebugInfo/guard-let-scope4.swift
@@ -8,6 +8,7 @@ open class C {
       // CHECK: sil_scope [[LAMBDA:[0-9]+]] { loc "{{.*}}":6:5
       // CHECK: sil_scope [[BODY:[0-9]+]] { loc "{{.*}}":6:19 parent [[LAMBDA]]
       // CHECK: sil_scope [[LET:[0-9]+]] { loc "{{.*}}":7:7 parent [[BODY]]
+      // CHECK: sil_scope [[TMP:[0-9]+]] { loc "{{.*}}":7:17 parent [[LET]]
       // CHECK: sil_scope [[GUARD:[0-9]+]] { loc "{{.*}}":7:17 parent [[LET]]
       // CHECK: debug_value {{.*}} : $C, let, name "self", {{.*}}, scope [[GUARD]]
       // CHECK: function_ref {{.*}}3fun{{.*}}, scope [[GUARD]]
