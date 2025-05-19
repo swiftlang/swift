28eceb4c2c4ea7c190bef47a18de46407958e95d
Egor Zhdan <e_zhdan@apple.com>
2025-05-19 16:00:39 +0100

    [cxx-interop] Temporarily disable a test for `std::function` on UBI 9
    Similar to 0a4eeabd.
    
    rdar://151476434
    (cherry picked from commit 30649f239187cf6161cfbfda76feb77226cadc9c)

--- test/Interop/Cxx/stdlib/use-std-function.swift
+++ test/Interop/Cxx/stdlib/use-std-function.swift
@@ -15,6 +15,7 @@
 // XFAIL: LinuxDistribution=rhel-9.3
 // XFAIL: LinuxDistribution=rhel-9.4
 // XFAIL: LinuxDistribution=rhel-9.5
+// XFAIL: LinuxDistribution=rhel-9.6
 // XFAIL: LinuxDistribution=fedora-39
 // XFAIL: LinuxDistribution=debian-12
 
