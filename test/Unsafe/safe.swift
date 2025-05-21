d794c95d4419f96c9d6a698d49929929991be98b
Doug Gregor <dgregor@apple.com>
2025-05-21 13:30:43 +0100

    Improve diagnostic for unsafe call arguments
    Use the argument type rather than the (potentially generic) parameter type.

--- test/Unsafe/safe.swift
+++ test/Unsafe/safe.swift
@@ -334,8 +334,8 @@ func testSwitch(se: SomeEnum) {
   switch unsafe se {
   case someEnumValue: break
     // expected-warning@-1{{expression uses unsafe constructs but is not marked with 'unsafe'}}{{8-8=unsafe }}
-    // expected-note@-2{{argument #0 in call to operator function '~=' has unsafe type 'T'}}
-    // expected-note@-3{{argument #1 in call to operator function '~=' has unsafe type 'T'}}
+    // expected-note@-2{{argument #0 in call to operator function '~=' has unsafe type 'SomeEnum'}}
+    // expected-note@-3{{argument #1 in call to operator function '~=' has unsafe type 'SomeEnum'}}
     // expected-note@-4{{reference to unsafe type 'SomeEnum'}}
     // expected-note@-5{{reference to unsafe var 'someEnumValue'}}
     // expected-note@-6{{reference to let '$match' involves unsafe type 'SomeEnum'}}
