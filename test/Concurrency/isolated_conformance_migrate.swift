534c204eaff64485e4802b875ba4f34736f106cb
Doug Gregor <dgregor@apple.com>
2025-05-22 12:13:39 +0100

    Add unnecessary REQUIRES: line for a feature
    (cherry picked from commit 2eeb3946e3a25d31df7ca9b7afe3b0aeb1e0a4c0)

--- test/Concurrency/isolated_conformance_migrate.swift
+++ test/Concurrency/isolated_conformance_migrate.swift
@@ -1,7 +1,7 @@
 // RUN: %target-swift-frontend -typecheck -verify -target %target-swift-5.1-abi-triple -swift-version 6 -enable-upcoming-feature InferIsolatedConformances:migrate %s
 
 // REQUIRES: concurrency
-
+// REQUIRES: swift_feature_InferIsolatedConformances
 
 protocol P { }
 protocol Q: P { }
