67edfeaf47a4216fee5c748cc5179b4ddaeb118a
Slava Pestov <spestov@apple.com>
2025-05-28 23:42:52 -0400

    Add tests suggested by @AnthonyLatsis

--- test/DebugInfo/parameterized_existential_composition.swift
+++ test/DebugInfo/parameterized_existential_composition.swift
@@ -1,5 +1,8 @@
 // RUN: %target-swift-frontend -emit-ir %s -g -target %target-swift-5.9-abi-triple
 
+// Note: The goal of this test is to exercise the mangling/demangling via
+// the -g flag.
+
 public protocol P<A, B> {
   associatedtype A
   associatedtype B
@@ -30,6 +33,17 @@ public func foo(_ a: (any P<Int, Float> & Q<String>).Type) {}
 public func foo(_ a: (any P<Int, Float> & Q<String> & R).Type) {}
 public func foo(_ a: (any P<Int, Float> & Q<String> & R & C<Bool>).Type) {}
 public func foo(_ a: (any P<Int, Float> & Q<String> & R & AnyObject).Type) {}
+
+public func foo(_ a: (any P & R).Type) {}
+public func foo(_ a: (any P & Q<String>).Type) {}
+public func foo(_ a: (any P & Q<String> & R).Type) {}
+public func foo(_ a: (any P & Q<String> & R & C<Bool>).Type) {}
+public func foo(_ a: (any P & Q<String> & R & AnyObject).Type) {}
+
+public func foo(_ a: (any P<Int, Float> & Q).Type) {}
+public func foo(_ a: (any P<Int, Float> & Q & R).Type) {}
+public func foo(_ a: (any P<Int, Float> & Q & R & C<Bool>).Type) {}
+public func foo(_ a: (any P<Int, Float> & Q & R & AnyObject).Type) {}
 
 public struct Foo<each T, U> {
   public var a1: (repeat any P<each T, U> & R)
