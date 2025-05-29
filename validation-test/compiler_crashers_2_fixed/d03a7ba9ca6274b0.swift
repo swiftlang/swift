0a0019ba7692b25dd490337b8746cfabc7f073bc
Slava Pestov <spestov@apple.com>
2025-05-29 14:39:14 -0400

    Sema: Fix null pointer dereference with invalid ParamDecl

--- validation-test/compiler_crashers_2_fixed/d03a7ba9ca6274b0.swift
+++ validation-test/compiler_crashers_2_fixed/d03a7ba9ca6274b0.swift
@@ -1,3 +1,3 @@
 // {"signature":"swift::ParamSpecifierRequest::evaluate(swift::Evaluator&, swift::ParamDecl*) const"}
-// RUN: not --crash %target-swift-frontend -typecheck %s
+// RUN: not %target-swift-frontend -typecheck %s
 func a([0])
