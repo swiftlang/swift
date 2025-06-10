6d0da8d5ccad91a98193245978e4b54f0aa4fcb7
Hamish Knight <hamish_knight@apple.com>
2025-06-10 15:44:09 +0100

    [CS] Avoid solver-allocated inputs with `typesSatisfyConstraint`
    Escaping solver-allocated types into a nested allocation arena is
    problematic since we can e.g lazily compute the `ContextSubMap` for a
    `NominalOrBoundGenericNominalType`, which is then destroyed when we
    exit the nested arena. Ensure we don't pass any types with type
    variables or placeholders to `typesSatisfyConstraint`.
    
    rdar://152763265

--- validation-test/compiler_crashers_2_fixed/dfd09450bb7a1.swift
+++ validation-test/compiler_crashers_2_fixed/dfd09450bb7a1.swift
@@ -1,6 +1,5 @@
 // {"signature":"swift::ProtocolConformanceRef::forAbstract(swift::Type, swift::ProtocolDecl*)"}
-// RUN: not --crash %target-swift-frontend -typecheck %s
-// REQUIRES: rdar152763265
+// RUN: not %target-swift-frontend -typecheck %s
 @resultBuilder struct a {
   static buildBlock<b, c, d, e>(b, c, d, e) func f<h>(_ : Bool @a Bool->h) {                         f(true {
     cond in var g : Int g 2 30\ g
