1112cf3d2de966f19f5da6b5bdda68f1e7fcf733
Stephen Canon <scanon@apple.com>
2025-06-12 20:10:18 -0400

    Fixup test copypasta.

--- test/Prototypes/builtin-vector-via-extern-c.swift
+++ test/Prototypes/builtin-vector-via-extern-c.swift
@@ -1,4 +1,4 @@
-//===--- SIMDFloatComparisons.swift.gyb -----------------------*- swift -*-===//
+//===----------------------------------------------------------*- swift -*-===//
 //
 // This source file is part of the Swift.org open source project
 //
@@ -9,9 +9,9 @@
 // See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
 //
 //===----------------------------------------------------------------------===//
-// RUN: %empty-directory(%t)
-// RUN: %gyb %s -o %t/SIMDFloatComparisons.swift
-// RUN: %target-swift-frontend -primary-file %t/SIMDFloatComparisons.swift -emit-ir -enable-experimental-feature BuiltinModule -enable-experimental-feature Extern | %FileCheck %t/SIMDFloatComparisons.swift --check-prefix=CHECK
+// REQUIRES: swift_feature_Extern
+// REQUIRES: swift_feature_BuiltinModule
+// RUN: %target-swift-frontend -primary-file %s -enable-experimental-feature BuiltinModule -enable-experimental-feature Extern -emit-ir | %FileCheck %s --check-prefix=CHECK
 
 import Builtin
 
@@ -23,7 +23,7 @@ public func saturatingAdd(_ a: SIMD16<UInt8>, _ b: SIMD16<UInt8>) -> SIMD16<UInt
   // Hack around init from Builtin type being stdlib-internal using unsafeBitCast.
   unsafeBitCast(_uaddSat(a._storage._value, b._storage._value), to: SIMD16<UInt8>.self)
 }
-// CHECK: s20SIMDFloatComparisons13saturatingAddys6SIMD16Vys5UInt8VGAG_AGtF{{.*}} {
+// CHECK: saturatingAdd{{.*}} {
 // CHECK: entry:
 // CHECK: call <16 x i8> @llvm.uadd.sat.v16i8
 
