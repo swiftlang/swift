6570da30e0915a83b21fe234ab260206aa8e69b3
Hamish Knight <hamish_knight@apple.com>
2025-06-16 11:08:38 +0100

    [test] Clean up some crasher test cases

---
+++ validation-test/compiler_crashers_2/486a70c43d10bda5.swift
@@ -0,0 +1,4 @@
+// {"signature":"(anonymous namespace)::ConnectedComponents::unionSets(swift::TypeVariableType*, swift::TypeVariableType*)"}
+// RUN: not --crash %target-swift-frontend -typecheck %s
+let b = ( c , d a {
+e b
--- validation-test/compiler_crashers_2/fea44854b27673b7.swift
+++
@@ -1,3 +0,0 @@
-// {"signature":"deriveBodyRawRepresentable_init(swift::AbstractFunctionDecl*, void*)"}
-// RUN: not --crash %target-swift-frontend -typecheck %s
-a enum a : Int {              case = #/
