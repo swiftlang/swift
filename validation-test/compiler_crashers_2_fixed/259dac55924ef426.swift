eec924b5052c23bf11b48526b1e8cb3f4f2c52e1
Slava Pestov <spestov@apple.com>
2025-06-16 14:47:35 -0400

    Sema: Fix crash in diagnoseIfSynthesisUnsupportedForDecl() with tuple extension

--- validation-test/compiler_crashers_2_fixed/259dac55924ef426.swift
+++ validation-test/compiler_crashers_2_fixed/259dac55924ef426.swift
@@ -1,3 +1,3 @@
 // {"signature":"swift::GenericContext::getGenericParams() const"}
-// RUN: not --crash %target-swift-frontend -typecheck %s
+// RUN: not %target-swift-frontend -typecheck %s
 typealias a = () extension a : Comparable
