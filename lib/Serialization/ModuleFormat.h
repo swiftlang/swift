9604081c46a04ae0dd03e99ab3672688bec8ee86
Meghana Gupta <meghana_gupta@apple.com>
2025-06-19 05:27:04 -0700

    Serialize/deserialize lifetime dependencies on enum elements

--- lib/Serialization/ModuleFormat.h
+++ lib/Serialization/ModuleFormat.h
@@ -58,7 +58,7 @@ const uint16_t SWIFTMODULE_VERSION_MAJOR = 0;
 /// describe what change you made. The content of this comment isn't important;
 /// it just ensures a conflict if two people change the module format.
 /// Don't worry about adhering to the 80-column limit for this line.
-const uint16_t SWIFTMODULE_VERSION_MINOR = 942; // update LifetimeDependenceLayout
+const uint16_t SWIFTMODULE_VERSION_MINOR = 943; // Lifetime dependencies on enum element
 
 /// A standard hash seed used for all string hashes in a serialized module.
 ///
