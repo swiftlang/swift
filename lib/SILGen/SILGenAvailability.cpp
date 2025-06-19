b1b1e00865920ef8278fe63f8bb3d7bfb76b54f0
Allan Shortlidge <shortlidge@apple.com>
2025-06-19 15:37:37 -0700

    SILGen: Rename SILGenBackDeploy.cpp to SILGenAvailability.cpp.
    NFC.

--- lib/SILGen/SILGenAvailability.cpp
+++ lib/SILGen/SILGenAvailability.cpp
@@ -1,8 +1,8 @@
-//===--- SILGenBackDeploy.cpp - SILGen for back deployment ----------------===//
+//===--- SILGenAvailability.cpp - SILGen for availability queries ---------===//
 //
 // This source file is part of the Swift.org open source project
 //
-// Copyright (c) 2022 Apple Inc. and the Swift project authors
+// Copyright (c) 2022 - 2025 Apple Inc. and the Swift project authors
 // Licensed under Apache License v2.0 with Runtime Library Exception
 //
 // See https://swift.org/LICENSE.txt for license information
