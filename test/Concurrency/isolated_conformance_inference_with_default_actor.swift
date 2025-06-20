a15fa70318cbdedd870506c123568a817cf47e94
Slava Pestov <spestov@apple.com>
2025-06-20 14:26:42 -0400

    Sema: Expand isolated conformance isolation to consider conformances to inherited protocols
    Otherwise, if PDerived inherits from P and P's requirements are
    witnessed by @MainActor-isolated members, we fail to infer
    @MainActor isolation on the conformance to PDerived.
    
    - Fixes https://github.com/swiftlang/swift/issues/82222.
    - Fixes rdar://153219831.

---
+++ test/Concurrency/isolated_conformance_inference_with_default_actor.swift
@@ -0,0 +1,8 @@
+// RUN: %empty-directory(%t)
+// RUN: %target-swift-frontend -emit-module -emit-module-path %t/isolated_conformance_other.swiftmodule %S/Inputs/isolated_conformance_other.swift -swift-version 6
+// RUN: %target-typecheck-verify-swift -I %t -swift-version 6 -enable-upcoming-feature InferIsolatedConformances -default-isolation MainActor -swift-version 6
+
+import isolated_conformance_other
+
+struct S1: P { func f() {} }
+struct S2: PDerived { func f() {} }
