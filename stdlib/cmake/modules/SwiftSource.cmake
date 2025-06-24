a6210bba38b24738b253655733d4a0f81aa99ad1
Guillaume Lessard <guillaume.lessard@apple.com>
2025-06-24 11:09:03 -0700

    [cmake] enable NonescapableAccessorOnTrivial
    Enable the NonescapableAccessorOnTrivial feature flag in swiftinterface files

--- stdlib/cmake/modules/SwiftSource.cmake
+++ stdlib/cmake/modules/SwiftSource.cmake
@@ -640,6 +640,7 @@ function(_compile_swift_files
   list(APPEND swift_flags "-enable-experimental-feature" "LifetimeDependence")
   list(APPEND swift_flags "-enable-experimental-feature" "InoutLifetimeDependence")
   list(APPEND swift_flags "-enable-experimental-feature" "LifetimeDependenceMutableAccessors")
+  list(APPEND swift_flags "-enable-experimental-feature" "NonescapableAccessorOnTrivial")
 
   list(APPEND swift_flags "-enable-upcoming-feature" "MemberImportVisibility")
 
