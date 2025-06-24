7c84eeb9d41a7f679bab2b75dd184db1c28a8287
Fahad Nayyar <f_nayyar@apple.com>
2025-06-24 16:12:32 -0700

    Revert "[cxx-interop] Disabling should be annotated with SWIFT_RETURNS_(UN)RETAINED warning (#81411)"
    This reverts commit 49aea31e411c2ae9a9e812a2e18974c3e9e2f03f.

--- test/Interop/Cxx/objc-correctness/Inputs/cxx-frt.h
+++ test/Interop/Cxx/objc-correctness/Inputs/cxx-frt.h
@@ -16,7 +16,7 @@ void releaseCxxRefType(CxxRefType *_Nonnull b) {}
 
 @interface Bridge : NSObject
 
-+ (struct CxxRefType *)objCMethodReturningFRTUnannotated;
++ (struct CxxRefType *)objCMethodReturningFRTUnannotated; // expected-warning {{'objCMethodReturningFRTUnannotated' should be annotated with either SWIFT_RETURNS_RETAINED or SWIFT_RETURNS_UNRETAINED as it is returning a SWIFT_SHARED_REFERENCE}}
 + (struct CxxRefType *)objCMethodReturningFRTUnowned
     __attribute__((swift_attr("returns_unretained")));
 + (struct CxxRefType *)objCMethodReturningFRTOwned
