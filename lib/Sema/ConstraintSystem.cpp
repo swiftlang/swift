017be57e9b51a576b2bf0e642f5fb8a28b39dc95
Slava Pestov <spestov@apple.com>
2025-06-25 18:55:45 -0400

    Sema: Fix -warn-long-expression-type-checking when expression timer is turned off
    My change 983b75e1cf645be5133812094daaba2caca6ce36 broke
    -warn-long-expression-type-checking because now the
    ExpressionTimer is not instantiated by default and that
    entire code path is skipped.
    
    Change it so that if -warn-long-expression-type-checking
    is passed in, we still start the timer, we just don't
    ever consider it to have 'expired'.
    
    Fixes rdar://problem/152998878.

--- lib/Sema/ConstraintSystem.cpp
+++ lib/Sema/ConstraintSystem.cpp
@@ -141,9 +141,18 @@ ConstraintSystem::~ConstraintSystem() {
 void ConstraintSystem::startExpressionTimer(ExpressionTimer::AnchorType anchor) {
   ASSERT(!Timer);
 
-  unsigned timeout = getASTContext().TypeCheckerOpts.ExpressionTimeoutThreshold;
-  if (timeout == 0)
-    return;
+  const auto &opts = getASTContext().TypeCheckerOpts;
+  unsigned timeout = opts.ExpressionTimeoutThreshold;
+
+  // If either the timeout is set, or we're asked to emit warnings,
+  // start the timer. Otherwise, don't start the timer, it's needless
+  // overhead.
+  if (timeout == 0) {
+    if (opts.WarnLongExpressionTypeChecking == 0)
+      return;
+
+    timeout = ExpressionTimer::NoLimit;
+  }
 
   Timer.emplace(anchor, *this, timeout);
 }
