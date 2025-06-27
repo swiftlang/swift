c23ac7e399e704560a5a9e3d532a0125b85fb144
David Smith <david_smith@apple.com>
2025-06-26 17:59:39 -0700

    Fix mismerge

--- test/abi/macOS/arm64/stdlib.swift
+++ test/abi/macOS/arm64/stdlib.swift
@@ -1092,6 +1092,8 @@ Added: _$ss6MirrorV12DisplayStyleO16foreignReferenceyA2DmFWC
 
 // var InlineArray._protectedBuffer
 // var InlineArray._protectedAddress
+Added: _$ss11InlineArrayVsRi__rlE16_protectedBufferSRyq_GvpMV
+Added: _$ss11InlineArrayVsRi__rlE17_protectedAddressSPyq_GvpMV
 
 // lengthOfBytes(using:)
 Added: __swift_stdlib_NSStringLengthOfBytesInEncodingTrampoline
