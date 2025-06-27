1b1e9d4d80e1474a9f00d3bfc282a6e593af8a50
Ben Rimmington <me@benrimmington.com>
2025-06-27 11:14:24 +0100

    [stdlib] Update `InlineArray` documentation (#82363)
    Remove misleading big O notation from initializers.

--- stdlib/public/core/InlineArray.swift
+++ stdlib/public/core/InlineArray.swift
@@ -212,8 +212,6 @@ extension InlineArray where Element: ~Copyable {
   ///
   /// - Parameter body: A closure that returns an owned `Element` to emplace at
   ///   the passed in index.
-  ///
-  /// - Complexity: O(*n*), where *n* is the number of elements in the array.
   @available(SwiftStdlib 6.2, *)
   @_alwaysEmitIntoClient
   public init<E: Error>(_ body: (Index) throws(E) -> Element) throws(E) {
@@ -260,8 +258,6 @@ extension InlineArray where Element: ~Copyable {
   ///   - next: A closure that takes an immutable borrow reference to the
   ///     preceding element, and returns an owned `Element` instance to emplace
   ///     into the array.
-  ///
-  /// - Complexity: O(*n*), where *n* is the number of elements in the array.
   @available(SwiftStdlib 6.2, *)
   @_alwaysEmitIntoClient
   public init<E: Error>(
@@ -313,8 +309,6 @@ extension InlineArray where Element: Copyable {
   /// Initializes every element in this array to a copy of the given value.
   ///
   /// - Parameter value: The instance to initialize this array with.
-  ///
-  /// - Complexity: O(*n*), where *n* is the number of elements in the array.
   @available(SwiftStdlib 6.2, *)
   @_alwaysEmitIntoClient
   public init(repeating value: Element) {
@@ -525,7 +519,7 @@ extension InlineArray where Element: ~Copyable {
 }
 
 //===----------------------------------------------------------------------===//
-// MARK: Span
+// MARK: - Span APIs
 //===----------------------------------------------------------------------===//
 
 @available(SwiftStdlib 6.2, *)
