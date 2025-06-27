d6c8812bca5d4f72511f3b2b70cc16ebd13f709a
Doug Gregor <dgregor@apple.com>
2025-06-27 13:54:06 -0700

    Add tests for SendableProhibitsMainActorInference

---
+++ test/Macros/default_main_actor_nonisolated.swift
@@ -0,0 +1,26 @@
+// REQUIRES: swift_swift_parser, executable_test
+
+// RUN: %empty-directory(%t)
+// RUN: %host-build-swift -swift-version 5 -emit-library -o %t/%target-library-name(MacroDefinition) -module-name=MacroDefinition %S/Inputs/syntax_macro_definitions.swift -g -no-toolchain-stdlib-rpath
+
+// Check for errors
+// RUN: %target-swift-frontend -swift-version 5 -typecheck -load-plugin-library %t/%target-library-name(MacroDefinition) %s -I %t -disable-availability-checking -swift-version 6 -default-isolation MainActor -enable-experimental-feature SendableProhibitsMainActorInference
+
+
+@attached(extension, conformances: Sendable)
+macro AddSendable() = #externalMacro(module: "MacroDefinition", type: "SendableMacro")
+
+@AddSendable
+final class SendableClass {
+  var property: Int { 0 }
+
+  func f() { }
+}
+
+nonisolated func acceptSendable<T: Sendable>(_: T) { }
+
+@concurrent func test(sc: SendableClass) async {
+  acceptSendable(sc) // SendableClass is Sendable
+  acceptSendable(\SendableClass.property) // so is its property
+  sc.f() // not on the main actor, so this is okay
+}
