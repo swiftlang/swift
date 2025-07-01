a26bf2f95c3c53351169a1406cf16fedcccf8170
Hamish Knight <hamish_knight@apple.com>
2025-07-01 15:55:43 +0100

    [test] Mark a couple of IDE crashers as fixed

--- validation-test/IDE/crashers_fixed/70921c2dbc55979.swift
+++ validation-test/IDE/crashers_fixed/70921c2dbc55979.swift
@@ -1,6 +1,6 @@
 // {"kind":"complete","signature":"swift::irgen::IRGenModule::emitLazyObjCProtocolDefinition(swift::ProtocolDecl*)"}
 // Actual signature: matchCallArguments
-// RUN: not --crash %target-swift-ide-test -code-completion --code-completion-token=COMPLETE -source-filename %s
+// RUN: %target-swift-ide-test -code-completion --code-completion-token=COMPLETE -source-filename %s
 a(
 #^COMPLETE^#[
 ""!
