62115791c0a9720795b797688189874e9a0849c8
Guillaume Lessard <guillaume.lessard@apple.com>
2025-07-01 18:05:14 -0700

    [stdlib] make `makeContiguousUTF8` stricter

--- stdlib/public/core/StringCreate.swift
+++ stdlib/public/core/StringCreate.swift
@@ -312,9 +312,17 @@ extension String {
   @inline(never) // slow-path
   internal static func _copying(_ str: Substring) -> String {
     if _fastPath(str._wholeGuts.isFastUTF8) {
-      return unsafe str._wholeGuts.withFastUTF8(range: str._offsetRange) {
+      var new = unsafe str._wholeGuts.withFastUTF8(range: str._offsetRange) {
         unsafe String._uncheckedFromUTF8($0)
       }
+#if os(watchOS) && _pointerBitWidth(_32)
+      if str._wholeGuts.isSmall,
+         str._wholeGuts.count > _SmallString.contiguousCapacity() {
+        new.reserveCapacity(_SmallString.capacity + 1)
+        return new
+      }
+#endif
+      return new
     }
     return unsafe Array(str.utf8).withUnsafeBufferPointer {
       unsafe String._uncheckedFromUTF8($0)
