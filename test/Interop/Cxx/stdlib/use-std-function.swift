cd58e657c794c32309aa1c10cd9e9efd94f879c6
Egor Zhdan <e_zhdan@apple.com>
2025-07-08 16:45:13 +0100

    [cxx-interop] Unconditionally apply `@_addressable` for C++ functions
    ClangImporter was importing C++ decls inconsistently when the experimental AddressableParameters feature is enabled vs disabled. This caused deserialization issues if a dependency module was build without AddressableParameters, but a client module enables them.
    
    rdar://155319311
    
    Co-authored-by: Gabor Horvath <gaborh@apple.com>

--- test/Interop/Cxx/stdlib/use-std-function.swift
+++ test/Interop/Cxx/stdlib/use-std-function.swift
@@ -10,8 +10,6 @@
 // libstdc++11 declares a templated constructor of std::function with an rvalue-reference parameter,
 // which aren't yet supported in Swift. Therefore initializing a std::function from Swift closures
 // will not work on the platforms that are shipped with this version of libstdc++ (rdar://125816354).
-// XFAIL: LinuxDistribution=ubuntu-24.04
-// XFAIL: LinuxDistribution=ubuntu-22.04
 // XFAIL: LinuxDistribution=rhel-9.3
 // XFAIL: LinuxDistribution=rhel-9.4
 // XFAIL: LinuxDistribution=rhel-9.5
