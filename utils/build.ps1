413f5ba2a0e30ab7bbfe75e55cba2f47f6134677
Ben Barham <ben_barham@apple.com>
2025-07-08 13:48:09 -0700

    Only run foundation tests for faster builds

--- utils/build.ps1
+++ utils/build.ps1
@@ -195,10 +195,11 @@ if ($AndroidSDKs.Length -gt 0) {
   $Android = $true
 }
 
-if ($Test -contains "*") {
-  # Explicitly don't include llbuild yet since tests are known to fail on Windows
-  $Test = @("lld", "lldb", "swift", "dispatch", "foundation", "xctest", "swift-format", "sourcekit-lsp")
-}
+#if ($Test -contains "*") {
+#  # Explicitly don't include llbuild yet since tests are known to fail on Windows
+#  $Test = @("lld", "lldb", "swift", "dispatch", "foundation", "xctest", "swift-format", "sourcekit-lsp")
+#}
+$Test = @("foundation")
 
 ## Declare static build and build tool parameters.
 
