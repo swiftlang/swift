934197370051ea593c2acf034ae00e6c7522d229
Guillaume Lessard <guillaume.lessard@apple.com>
2025-07-08 14:29:21 -0700

    [stdlib] update doc-comments

--- stdlib/public/core/StringProtocol.swift
+++ stdlib/public/core/StringProtocol.swift
@@ -182,9 +182,8 @@ extension String {
   ///
   /// Contiguous strings always operate in O(1) time for withUTF8, always give
   /// a result for String.UTF8View.withContiguousStorageIfAvailable, and always
-  /// return a non-nil value from `String._utf8Span` and `String.utf8._span`.
+  /// return a non-nil value from `String._utf8Span` and `String.UTF8View._span`.
   /// Contiguous strings also benefit from fast-paths and better optimizations.
-  ///
   @_alwaysEmitIntoClient
   public var isContiguousUTF8: Bool {
     if _guts.isFastUTF8 {
@@ -236,13 +235,14 @@ extension String {
 
 // Contiguous UTF-8 strings
 extension Substring {
-  /// Returns whether this string is capable of providing access to
-  /// validly-encoded UTF-8 contents in contiguous memory in O(1) time.
+  /// Returns whether this string's storage contains
+  /// validly-encoded UTF-8 contents in contiguous memory.
   ///
-  /// Contiguous strings always operate in O(1) time for withUTF8 and always
-  /// give a result for String.UTF8View.withContiguousStorageIfAvailable.
+  /// Contiguous strings always operate in O(1) time for withUTF8, always give
+  /// a result for Substring.UTF8View.withContiguousStorageIfAvailable, and
+  /// always return a non-nil value from `Substring._utf8Span` and
+  /// `Substring.UTF8View._span`.
   /// Contiguous strings also benefit from fast-paths and better optimizations.
-  ///
   @_alwaysEmitIntoClient
   public var isContiguousUTF8: Bool { return self.base.isContiguousUTF8 }
 
