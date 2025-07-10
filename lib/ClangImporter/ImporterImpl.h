039d86873499b2efd3ae20368bcc937fabae287b
Fahad Nayyar <f_nayyar@apple.com>
2025-07-09 22:22:48 -0700

    Using addImportDiagnostic

--- lib/ClangImporter/ImporterImpl.h
+++ lib/ClangImporter/ImporterImpl.h
@@ -260,6 +260,7 @@ struct ImportDiagnostic {
                    clang::SourceLocation loc)
       : target(target), diag(diag), loc(loc) {}
 
+  // IMP
   bool operator==(const ImportDiagnostic &other) const {
     return target == other.target && loc == other.loc &&
            diag.getID() == other.diag.getID();
