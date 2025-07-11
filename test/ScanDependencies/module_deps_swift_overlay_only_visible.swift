15593e5ca68b7e3fba358fe617fd38fc66bdf879
Artem Chikin <achikin@apple.com>
2025-07-11 16:59:34 -0700

    [Dependency Scanning] Restrict Swift overlay lookup to "visible" Clang modules only
    Previously Swift overlay lookup was performed for every directly and transitively-imported Clang module.
    
    https://github.com/llvm/llvm-project/pull/147969 introduced the concept of "visible" Clang modules from a given named Clang dependency scanner query which closely maps to the set of modules for which Swift will attempt to load a Swift overlay. This change switches overlay querying to apply only to the set of such visible modules.
    
    Resolves rdar://144797648

---
+++ test/ScanDependencies/module_deps_swift_overlay_only_visible.swift
@@ -0,0 +1,72 @@
+// RUN: %empty-directory(%t)
+// RUN: %empty-directory(%t/clang-module-cache)
+// RUN: %empty-directory(%t/swiftDeps)
+// RUN: %empty-directory(%t/clangDeps)
+// RUN: split-file %s %t
+
+// RUN: %target-swift-frontend -scan-dependencies -module-cache-path %t/clang-module-cache %s -o %t/deps.json -I %t/swiftDeps -I %t/clangDeps
+// RUN: %validate-json %t/deps.json | %FileCheck %s
+
+// Ensure Swift module 'E' has a Swift overlay dependency on
+// 'G', because Clang module 'G' is a visible dependency of Clang module 'E'
+//
+// CHECK-LABEL:        "modulePath": "{{.*}}E-{{.*}}.swiftmodule"
+// CHECK:              "swiftOverlayDependencies": [
+// CHECK-NEXT:            {
+// CHECK-NEXT:              "swift": "G"
+// CHECK-NEXT:            }
+// CHECK-NEXT:          ]
+
+// Ensure Swift module 'A' does not have a Swift overlay dependency on
+// 'G', because although 'A' depends on Clang module 'G', it does not export it
+// and therefore it is not visible
+//
+// CHECK:        "modulePath": "{{.*}}A-{{.*}}.swiftmodule"
+// CHECK-NOT:              "swiftOverlayDependencies": [
+
+//--- swiftDeps/A.swiftinterface
+// swift-interface-format-version: 1.0
+// swift-module-flags: -module-name A -enable-library-evolution
+@_exported import A
+public func overlayFuncA() {}
+
+//--- swiftDeps/E.swiftinterface
+// swift-interface-format-version: 1.0
+// swift-module-flags: -module-name E -enable-library-evolution
+@_exported import E
+public func overlayFuncE() {}
+
+//--- swiftDeps/G.swiftinterface
+// swift-interface-format-version: 1.0
+// swift-module-flags: -module-name G -enable-library-evolution
+@_exported import G
+public func overlayFuncG() {}
+
+//--- clangDeps/module.modulemap
+module A {
+  header "A.h"
+  // No export *
+}
+module E {
+  header "E.h"
+  export *
+}
+module G {
+  header "G.h"
+  export *
+}
+
+//--- clangDeps/A.h
+#include "G.h"
+void funcA(void);
+
+//--- clangDeps/E.h
+#include "G.h"
+void funcE(void);
+
+//--- clangDeps/G.h
+void funcG(void);
+
+//--- client.swift
+import A
+import E
