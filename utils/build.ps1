1ac84912d934236e20355c89896161f3e54e6b07
Ben Barham <b.n.barham@gmail.com>
2025-07-11 12:27:52 -0700

    Revert "[Build] Temporarily add -j 1 to swift-corelibs-foundation Windows build"

--- utils/build.ps1
+++ utils/build.ps1
@@ -2588,8 +2588,7 @@ function Test-Foundation {
       -Src $SourceCache\swift-corelibs-foundation `
       -Bin "$BinaryCache\$($BuildPlatform.Triple)\FoundationTests" `
       -Platform $BuildPlatform `
-      -Configuration $FoundationTestConfiguration `
-      -j 1
+      -Configuration $FoundationTestConfiguration
   }
 }
 
