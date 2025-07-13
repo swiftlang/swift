2e236e58e0fd8b8bffb268a283570e8e20c1563c
Fahad Nayyar <f_nayyar@apple.com>
2025-07-13 16:17:45 -0700

    Remove diagnostic for SWIFT_RETURNS_(UN)RETAINED on non-FRT return types

--- lib/ClangImporter/ImportDecl.cpp
+++ lib/ClangImporter/ImportDecl.cpp
@@ -3661,19 +3661,6 @@ namespace {
                 decl);
           }
         }
-      } else {
-        if (returnsRetainedAttrIsPresent || returnsUnretainedAttrIsPresent) {
-          if (const auto *functionDecl = dyn_cast<clang::FunctionDecl>(decl)) {
-            if (functionDecl->isTemplateInstantiation()) {
-              return;
-            }
-          }
-          Impl.diagnose(
-              loc,
-              diag::
-                  returns_retained_or_returns_unretained_for_non_cxx_frt_values,
-              decl);
-        }
       }
     }
 
