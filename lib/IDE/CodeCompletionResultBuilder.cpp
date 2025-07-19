180cc0c007e79bb3e1ef3ed2b65dc2a89f38441e
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2025-07-19 12:17:21 +0300

    [IDE] Verify decls match in verify USR to Decl reconstruction

--- lib/IDE/CodeCompletionResultBuilder.cpp
+++ lib/IDE/CodeCompletionResultBuilder.cpp
@@ -112,6 +112,15 @@ static void verifyUSRToDeclReconstruction(const Decl *D) {
 
     assert(false && "Reconstructed declaration shouldn't be null");
   }
+
+  if (Reconstructed != VD) {
+    llvm::errs() << "Swift USR is " << SwiftUSR << ", reconstructed declaration is:\n";
+    Reconstructed->dump(llvm::errs());
+    llvm::errs() << "Original declaration is:\n";
+    VD->dump(llvm::errs());
+
+    assert(false && "Reconstructed declaration should match the original one");
+  }
 }
 
 CodeCompletionResult *CodeCompletionResultBuilder::takeResult() {
@@ -219,13 +228,6 @@ CodeCompletionResult *CodeCompletionResultBuilder::takeResult() {
         ContextFreeResult->calculateContextualNotRecommendedReason(
             ContextualNotRecReason, CanCurrDeclContextHandleAsync);
 
-    // TODO(a7medev): Move to completion options.
-    bool verifyUSRToDecl = true;
-    if (verifyUSRToDecl) {
-      auto &Ctx = DC->getASTContext();
-      verifyUSRToDeclReconstruction(Ctx, AssociatedDecl);
-    }
-    
     return new (Allocator) CodeCompletionResult(
         *ContextFreeResult, SemanticContext, Flair, NumBytesToErase,
         typeRelation, notRecommendedReason);
