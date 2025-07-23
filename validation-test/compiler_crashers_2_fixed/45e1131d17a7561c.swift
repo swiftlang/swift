f81ac999bbec92d449081cb91317cc886458df76
Pavel Yaskevich <pyaskevich@apple.com>
2025-07-23 00:49:51 -0700

    [Tests] NFC: Adjust tests improved by the changes to ambiguity diagnostics

--- validation-test/compiler_crashers_2_fixed/45e1131d17a7561c.swift
+++ validation-test/compiler_crashers_2_fixed/45e1131d17a7561c.swift
@@ -1,5 +1,5 @@
 // {"signature":"swift::constraints::simplifyLocator(swift::ASTNode&, llvm::ArrayRef<swift::constraints::ConstraintLocator::PathElement>&, swift::SourceRange&)"}
-// RUN: not --crash %target-swift-frontend -typecheck %s
+// RUN: not %target-swift-frontend -typecheck %s
 struct a { eq = "" b "Self ecuador1 Self) > Bool {
     let getProperties = (
       \.eq as Self -> _
