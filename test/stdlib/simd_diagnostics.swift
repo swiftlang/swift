676ea9e46c90035490f1da921ead61a0631b64a2
Max Desiatov <m_desiatov@apple.com>
2025-07-24 16:06:04 +0100

    simd_diagnostics.swift: XFAIL non-Darwin platforms

--- test/stdlib/simd_diagnostics.swift
+++ test/stdlib/simd_diagnostics.swift
@@ -1,6 +1,7 @@
 // RUN: %target-typecheck-verify-swift
 
 // FIXME: No simd module on linux rdar://problem/20795411
+// XFAIL: OS=linux-gnu, OS=windows-msvc, OS=openbsd, OS=linux-android, OS=linux-androideabi
 // XFAIL: CPU=wasm32
 
 import simd
