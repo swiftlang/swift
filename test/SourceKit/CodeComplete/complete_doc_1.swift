d450e1ab0339b7ac494b72dee00f8a76796a3869
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2025-07-25 17:20:22 +0300

    [Test] Check full doc in completion with swift-ide-test

--- test/SourceKit/CodeComplete/complete_doc_1.swift
+++ test/SourceKit/CodeComplete/complete_doc_1.swift
@@ -19,39 +19,16 @@ struct S: P {
 }
 
 func test() {
-  S().
+  S().#^DOC^#
 }
 
 // All in main module.
-// RUN: %sourcekitd-test -req=complete -req-opts=includefulldocumentation=1 -pos=22:7 %s -- %s -module-name DocBriefTest | %FileCheck %s -check-prefix=CHECK
+// RUN: %target-swift-ide-test -code-completion -source-filename %s -module-name DocBriefTest -code-completion-token=DOC -code-completion-comments=true | %FileCheck %s -check-prefix=CHECK
 
-// CHECK: {
-// CHECK:   key.results: [
-// CHECK-NEXT:     {
-// CHECK-NEXT:       key.kind: source.lang.swift.decl.function.method.instance,
-// CHECK-NEXT:       key.name: "bar()",
-// CHECK-NEXT:       key.doc.full_as_xml: "<Function file=\"{{.*}}\" line=\"{{.*}}\" column=\"{{.*}}\"><Name>bar()</Name><USR>s:12DocBriefTest1PP3baryyF</USR><Declaration>func bar()</Declaration><CommentParts><Abstract><Para>This is a doc comment of P.bar</Para></Abstract><Discussion><Para>May have default information.</Para><Note><Para>This documentation comment was inherited from <codeVoice>P</codeVoice>.</Para></Note></Discussion></CommentParts></Function>",
-// CHECK-NEXT:       key.description: "bar()",
-// CHECK-NEXT:       key.typename: "Void",
-// CHECK-NEXT:       key.doc.brief: "This is a doc comment of P.bar",
-// CHECK-NEXT:       key.context: source.codecompletion.context.superclass,
-// CHECK-NEXT:       key.typerelation: source.codecompletion.typerelation.unknown,
-// CHECK-NEXT:       key.num_bytes_to_erase: 0,
-// CHECK-NEXT:       key.associated_usrs: "s:12DocBriefTest1PPAAE3baryyF",
-// CHECK-NEXT:       key.modulename: "DocBriefTest",
-// CHECK-NEXT:       key.sourcetext: "bar()"
-// CHECK-NEXT:     },
-// CHECK-NEXT:     {
-// CHECK-NEXT:       key.kind: source.lang.swift.decl.function.method.instance,
-// CHECK-NEXT:       key.name: "foo()",
-// CHECK-NEXT:       key.doc.full_as_xml: "<Function file=\"{{.*}}\" line=\"{{.*}}\" column=\"{{.*}}\"><Name>foo()</Name><USR>s:12DocBriefTest1PP3fooyyF</USR><Declaration>func foo()</Declaration><CommentParts><Abstract><Para>This is a doc comment of P.foo</Para></Abstract><Discussion><Para>Do whatever.</Para><Note><Para>This documentation comment was inherited from <codeVoice>P</codeVoice>.</Para></Note></Discussion></CommentParts></Function>",
-// CHECK-NEXT:       key.description: "foo()",
-// CHECK-NEXT:       key.typename: "Void",
-// CHECK-NEXT:       key.doc.brief: "This is a doc comment of P.foo",
-// CHECK-NEXT:       key.context: source.codecompletion.context.thisclass,
-// CHECK-NEXT:       key.typerelation: source.codecompletion.typerelation.unknown,
-// CHECK-NEXT:       key.num_bytes_to_erase: 0,
-// CHECK-NEXT:       key.associated_usrs: "s:12DocBriefTest1SV3fooyyF s:12DocBriefTest1PP3fooyyF",
-// CHECK-NEXT:       key.modulename: "DocBriefTest",
-// CHECK-NEXT:       key.sourcetext: "foo()"
-// CHECK-NEXT:     }
+// CHECK:      Decl[InstanceMethod]/CurrNominal:  foo()[#Void#]; name=foo();
+// CHECK-SAME: briefcomment=This is a doc comment of P.foo;
+// CHECK-SAME: fullcomment=<Function file="{{.*}}" line="5" column="8"><Name>foo()</Name><USR>s:12DocBriefTest1PP3fooyyF</USR><Declaration>func foo()</Declaration><CommentParts><Abstract><Para>This is a doc comment of P.foo</Para></Abstract><Discussion><Para>Do whatever.</Para><Note><Para>This documentation comment was inherited from <codeVoice>P</codeVoice>.</Para></Note></Discussion></CommentParts></Function>
+
+// CHECK:      Decl[InstanceMethod]/Super:        bar()[#Void#]; name=bar();
+// CHECK-SAME: briefcomment=This is a doc comment of P.bar;
+// CHECK-SAME: fullcomment=<Function file="{{.*}}" line="10" column="8"><Name>bar()</Name><USR>s:12DocBriefTest1PP3baryyF</USR><Declaration>func bar()</Declaration><CommentParts><Abstract><Para>This is a doc comment of P.bar</Para></Abstract><Discussion><Para>May have default information.</Para><Note><Para>This documentation comment was inherited from <codeVoice>P</codeVoice>.</Para></Note></Discussion></CommentParts></Function>
--- test/SourceKit/CodeComplete/complete_from_clang_module.swift
+++ test/SourceKit/CodeComplete/complete_from_clang_module.swift
@@ -1,18 +1,11 @@
 import Foo
 
+#^COMPLETE^#
+
 // REQUIRES: objc_interop
-// RUN: %sourcekitd-test -req=complete -req-opts=includefulldocumentation=1 -pos=2:1 %s -- -F %S/../Inputs/libIDE-mock-sdk %s | %FileCheck %s
+// RUN: %target-swift-ide-test -code-completion -source-filename %s -F %S/../Inputs/libIDE-mock-sdk -code-completion-token=COMPLETE -code-completion-comments=true | %FileCheck %s -check-prefix=CHECK
 
-// CHECK-LABEL:      key.name: "fooIntVar",
-// CHECK-NEXT:       key.doc.full_as_xml: "<Variable file=\"{{.*}}\" line=\"{{.*}}\" column=\"{{.*}}\"><Name>fooIntVar</Name><USR>c:@fooIntVar</USR><Declaration>var fooIntVar: Int32</Declaration><Abstract><Para> Aaa. fooIntVar. Bbb.</Para></Abstract></Variable>",
-// CHECK-NEXT:       key.description: "fooIntVar",
-// CHECK-NEXT:       key.typename: "Int32",
-// CHECK-NEXT:       key.doc.brief: "Aaa.  fooIntVar.  Bbb.",
-// CHECK-NEXT:       key.context: source.codecompletion.context.othermodule,
-// CHECK-NEXT:       key.typerelation: source.codecompletion.typerelation.unknown,
-// CHECK-NEXT:       key.num_bytes_to_erase: 0,
-// CHECK-NEXT:       key.associated_usrs: "c:@fooIntVar",
-// CHECK-NEXT:       key.modulename: "Foo",
-// CHECK-NEXT:       key.sourcetext: "fooIntVar"
-// CHECK-NEXT:     },
-
+// CHECK:            Decl[GlobalVar]/OtherModule[Foo]:   fooIntVar[#Int32#];
+// CHECK-SAME-LABEL: name=fooIntVar;
+// CHECK-SAME:       briefcomment=Aaa. fooIntVar. Bbb.;
+// CHECK-SAME:       fullcomment=<Variable file="{{.*}}" line="63" column="12"><Name>fooIntVar</Name><USR>c:@fooIntVar</USR><Declaration>var fooIntVar: Int32</Declaration><Abstract><Para> Aaa.  fooIntVar.  Bbb.</Para></Abstract></Variable>
