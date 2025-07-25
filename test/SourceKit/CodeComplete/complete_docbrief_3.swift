21b3e66b7b817ac6342d8c7235b3eb36b695ec69
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2025-07-25 18:53:33 +0300

    [Test] Revert changes to SourceKit documentation tests

---
+++ test/SourceKit/CodeComplete/complete_docbrief_3.swift
@@ -0,0 +1,46 @@
+// RUN: %empty-directory(%t)
+// RUN: %empty-directory(%t/Modules)
+// RUN: split-file --leading-lines %s %t
+
+// RUN: %target-swift-frontend \
+// RUN:     -emit-module \
+// RUN:     -module-name DocBriefTest \
+// RUN:     -emit-module-path %t/Modules/DocBriefTest.swiftmodule \
+// RUN:     -emit-module-doc-path %t/Modules/DocBriefTest.swiftdoc \
+// RUN:     %t/Module.swift
+
+//--- Module.swift
+public protocol P {
+  /// This is a doc comment of P.foo
+  ///
+  /// Do whatever.
+  func foo()
+}
+
+public struct S: P {
+  public init() {}
+  public func foo() {}
+}
+
+//--- User.swift
+import DocBriefTest
+func test() {
+  // RUN: %sourcekitd-test -req=complete -pos=%(line+1):7 %t/User.swift -- %t/User.swift -I %t/Modules -target %target-triple -module-name DocBriefUser | %FileCheck %s -check-prefix=CHECK
+  S().foo()
+
+  // CHECK: {
+  // CHECK:   key.results: [
+  // CHECK-NEXT:     {
+  // CHECK-NEXT:       key.kind: source.lang.swift.decl.function.method.instance,
+  // CHECK-NEXT:       key.name: "foo()",
+  // CHECK-NEXT:       key.description: "foo()",
+  // CHECK-NEXT:       key.typename: "Void",
+  // CHECK-NEXT:       key.doc.brief: "This is a doc comment of P.foo",
+  // CHECK-NEXT:       key.context: source.codecompletion.context.thisclass,
+  // CHECK-NEXT:       key.typerelation: source.codecompletion.typerelation.unknown,
+  // CHECK-NEXT:       key.num_bytes_to_erase: 0,
+  // CHECK-NEXT:       key.associated_usrs: "s:12DocBriefTest1SV3fooyyF s:12DocBriefTest1PP3fooyyF",
+  // CHECK-NEXT:       key.modulename: "DocBriefTest",
+  // CHECK-NEXT:       key.sourcetext: "foo()"
+  // CHECK-NEXT:     }
+}
--- test/SourceKit/CodeComplete/complete_from_clang_module.swift
+++ test/SourceKit/CodeComplete/complete_from_clang_module.swift
@@ -1,11 +1,17 @@
 import Foo
 
-#^COMPLETE^#
-
 // REQUIRES: objc_interop
-// RUN: %target-swift-ide-test -code-completion -source-filename %s -F %S/../Inputs/libIDE-mock-sdk -code-completion-token=COMPLETE -code-completion-comments=true | %FileCheck %s -check-prefix=CHECK
+// RUN: %sourcekitd-test -req=complete -pos=2:1 %s -- -F %S/../Inputs/libIDE-mock-sdk %s | %FileCheck %s
 
-// CHECK:            Decl[GlobalVar]/OtherModule[Foo]:   fooIntVar[#Int32#];
-// CHECK-SAME-LABEL: name=fooIntVar;
-// CHECK-SAME:       briefcomment=Aaa. fooIntVar. Bbb.;
-// CHECK-SAME:       fullcomment=<Variable file="{{.*}}" line="63" column="12"><Name>fooIntVar</Name><USR>c:@fooIntVar</USR><Declaration>var fooIntVar: Int32</Declaration><Abstract><Para> Aaa.  fooIntVar.  Bbb.</Para></Abstract></Variable>
+// CHECK-LABEL:      key.name: "fooIntVar",
+// CHECK-NEXT:       key.description: "fooIntVar",
+// CHECK-NEXT:       key.typename: "Int32",
+// CHECK-NEXT:       key.doc.brief: "Aaa.  fooIntVar.  Bbb.",
+// CHECK-NEXT:       key.context: source.codecompletion.context.othermodule,
+// CHECK-NEXT:       key.typerelation: source.codecompletion.typerelation.unknown,
+// CHECK-NEXT:       key.num_bytes_to_erase: 0,
+// CHECK-NEXT:       key.associated_usrs: "c:@fooIntVar",
+// CHECK-NEXT:       key.modulename: "Foo",
+// CHECK-NEXT:       key.sourcetext: "fooIntVar"
+// CHECK-NEXT:     },
+
