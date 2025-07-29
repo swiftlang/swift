6c147368044e82058e8aec1f0287e104358efebb
Ben Rimmington <me@benrimmington.com>
2025-07-29 23:28:01 +0100

    Add code headers
    <https://www.swift.org/contributing/#code-templates>

--- stdlib/public/core/UTF8Span.swift
+++ stdlib/public/core/UTF8Span.swift
@@ -1,5 +1,14 @@
-// TODO: comment header
-
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2025 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
 
 /// A borrowed view into contiguous memory that contains validly-encoded UTF-8 code units.
 @frozen
