c866fa074a5cd6b2b0f8b8c7aa1f90f4077174af
Alastair Houghton <ahoughton@apple.com>
2025-07-30 17:00:26 +0100

    [Tests][Concurrency] Remove clock canonicalization from test.
    We've removed this code, so the test was failing.

--- test/Concurrency/Runtime/sleep_executor.swift
+++ test/Concurrency/Runtime/sleep_executor.swift
@@ -39,15 +39,6 @@ final class TestExecutor: TaskExecutor, SchedulingExecutor, @unchecked Sendable 
                                 after delay: C.Duration,
                                 tolerance: C.Duration? = nil,
                                 clock: C) {
-    // Convert the clock to its canonical equivalent, if any
-    if let canonical = clock.canonicalClock {
-      enqueue(_job,
-              after: clock.convertToCanonical(duration: delay),
-              tolerance: clock.maybeConvertToCanonical(duration: tolerance),
-              clock: canonical)
-      return
-    }
-
     // Convert to `Swift.Duration`
     let duration = delay as! Swift.Duration
 
