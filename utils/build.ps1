d29fad8d4743ffc6ad0ee66735e501480e95277e
Saleem Abdulrasool <abdulras@thebrowser.company>
2025-08-01 15:51:58 -0700

    build.ps1: make `-Android` explicitly required
    Default values for the Android environment should be the complete set.
    The top-level `-Android` flag controls whether the Android SDKs are
    built.

--- utils/build.ps1
+++ utils/build.ps1
@@ -127,7 +127,7 @@ param
   [ValidateRange(1, 36)]
   [int] $AndroidAPILevel = 28,
   [string[]] $AndroidSDKVersions = @("Android", "AndroidExperimental"),
-  [string[]] $AndroidSDKArchitectures = @(),
+  [string[]] $AndroidSDKArchitectures = @("aarch64", "armv7", "i686", "x86_64"),
   [string[]] $WindowsSDKVersions = @("Windows", "WindowsExperimental"),
   [string[]] $WindowsSDKArchitectures = @("X64","X86","Arm64"),
   [string] $ProductVersion = "0.0.0",
@@ -190,11 +190,6 @@ if (($PinnedBuild -or $PinnedSHA256 -or $PinnedVersion) -and -not ($PinnedBuild 
   throw "If any of PinnedBuild, PinnedSHA256, or PinnedVersion is set, all three must be set."
 }
 
-if ($Android -and ($AndroidSDKArchitectures.Length -eq 0)) {
-  # Enable all android SDKs by default.
-  $AndroidSDKArchitectures = @("aarch64","armv7","i686","x86_64")
-}
-
 # Work around limitations of cmd passing in array arguments via powershell.exe -File
 if ($AndroidSDKVersions.Length -eq 1) { $AndroidSDKVersions = $AndroidSDKVersions[0].Split(",") }
 if ($AndroidSDKArchitectures.Length -eq 1) { $AndroidSDKArchitectures = $AndroidSDKArchitectures[0].Split(",") }
@@ -203,11 +198,6 @@ if ($WindowsSDKVersions.Length -eq 1) { $WindowsSDKVersions = $WindowsSDKVersion
 if ($WindowsSDKArchitectures.Length -eq 1) { $WindowsSDKArchitectures = $WindowsSDKArchitectures[0].Split(",") }
 
 if ($Test.Length -eq 1) { $Test = $Test[0].Split(",") }
-
-if ($AndroidSDKArchitectures.Length -gt 0) {
-  # Always enable android when one of the SDKs is specified.
-  $Android = $true
-}
 
 if ($Test -contains "*") {
   # Explicitly don't include llbuild yet since tests are known to fail on Windows
