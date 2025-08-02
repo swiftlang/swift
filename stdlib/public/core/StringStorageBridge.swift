4b51a751ae5bf66f701c5deef6048ce4bcb70247
David Smith <david_smith@apple.com>
2025-08-01 17:47:15 -0700

    Override -_fastUTF8StringContents:utf8Length: to give bridged Strings O(1) access to their UTF8 contents

--- stdlib/public/core/StringStorageBridge.swift
+++ stdlib/public/core/StringStorageBridge.swift
@@ -228,6 +228,16 @@ extension __StringStorage {
     }
     return nil
   }
+  
+  @objc(_fastUTF8StringContents:utf8Length:)
+  @_effects(readonly)
+  final internal func _fastUTF8StringContents(
+    _ requiresNulTermination: Int8
+    _ outUTF8Length: UnsafeMutablePointer<UInt>
+  ) -> UnsafePointer<UInt8>? {
+    outUTF8Length.pointee = UInt(count)
+    return unsafe start
+  }
 
   @objc(UTF8String)
   @_effects(readonly)
@@ -345,6 +355,16 @@ extension __SharedStringStorage {
       return unsafe start._asCChar
     }
     return nil
+  }
+  
+  @objc(_fastUTF8StringContents:utf8Length:)
+  @_effects(readonly)
+  final internal func _fastUTF8StringContents(
+    _ requiresNulTermination: Int8
+    _ outUTF8Length: UnsafeMutablePointer<UInt>
+  ) -> UnsafePointer<UInt8>? {
+    outUTF8Length.pointee = UInt(count)
+    return unsafe start
   }
 
   @objc(UTF8String)
