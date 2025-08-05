e913d673f5333bf93c78f243456388bd1b22caf7
Zhenchao Li <leezhenchao@gmail.com>
2025-08-05 12:59:35 -0700

    use path computation helper

--- utils/build.ps1
+++ utils/build.ps1
@@ -697,6 +697,7 @@ enum Project {
   SourceKitLSP
   SymbolKit
   DocC
+  brotli
 
   LLVM
   Runtime
@@ -2175,7 +2176,7 @@ function Build-Sanitizers([Hashtable] $Platform) {
 function Build-Brotli([Hashtable] $Platform) {
   Build-CMakeProject `
     -Src $SourceCache\brotli `
-    -Bin "$BinaryCache\$($Platform.Triple)\brotli" `
+    -Bin "$(Get-ProjectBinaryCache $Platform brotli)" `
     -InstallTo "$BinaryCache\$($Platform.Triple)\usr" `
     -Platform $Platform `
     -UseMSVCCompilers C `
