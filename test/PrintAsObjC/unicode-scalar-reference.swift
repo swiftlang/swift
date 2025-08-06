d46655e538b968c607f4ac74b67e3e4558c7f3c2
Alexis Laferrière <alaf@apple.com>
2025-08-06 14:31:37 -0700

    PrintAsClang: Disable Float16 check on platforms where it's unsupported

--- test/PrintAsObjC/unicode-scalar-reference.swift
+++ test/PrintAsObjC/unicode-scalar-reference.swift
@@ -5,7 +5,7 @@
 // or actually be printed using a C / Objective-C compatible type.
 
 // RUN: %target-swift-frontend(mock-sdk: %clang-importer-sdk) \
-// RUN:   %s -emit-module -verify -o %t \
+// RUN:   %s -emit-module -verify -o %t -emit-module-doc \
 // RUN:   -emit-clang-header-path %t/compat.h
 // RUN: %FileCheck %s --input-file %t/compat.h
 
@@ -17,6 +17,15 @@ func referencesScalar() -> Unicode.Scalar { fatalError() }
 func x_referencesRelated(a: CChar32, b: CWideChar) { }
 // CHECK: SWIFT_EXTERN void referencesRelated(char32_t a, wchar_t b)
 
-@_cdecl("referencesFloat16")
-func y_referencesFloat16(a: Float16, b: CFloat16) { }
+#if !((os(macOS) || targetEnvironment(macCatalyst)) && arch(x86_64))
+    // Actual test of Float16, on supported platforms.
+    @_cdecl("referencesFloat16")
+    func y_referencesFloat16(a: Float16, b: CFloat16) { }
+#else
+    // Just print the expected string on macOS&x86_64 where there's no Float16.
+    // This disables this one check on unsupported platforms.
+    /// SWIFT_EXTERN void referencesFloat16(_Float16 a, _Float16 b)
+    @_cdecl("printTheDoc")
+    public func y_printTheDoc() { }
+#endif
 // CHECK: SWIFT_EXTERN void referencesFloat16(_Float16 a, _Float16 b)
