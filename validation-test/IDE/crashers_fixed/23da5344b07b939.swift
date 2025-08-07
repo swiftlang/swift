0bd1ff05728435753ffacd2695d8c1962f00d771
Hamish Knight <hamish_knight@apple.com>
2025-08-07 12:13:44 +0100

    Avoid dropping ExtInfo in `replaceParamErrorTypeByPlaceholder`
    Make sure we preserve the info to avoid hitting an assert.

--- validation-test/IDE/crashers_fixed/23da5344b07b939.swift
+++ validation-test/IDE/crashers_fixed/23da5344b07b939.swift
@@ -1,3 +1,3 @@
 // {"kind":"complete","signature":"swift::AnyFunctionType::getExtInfo() const","signatureAssert":"Assertion failed: (hasExtInfo()), function getExtInfo"}
-// RUN: not --crash %target-swift-ide-test -code-completion -batch-code-completion -skip-filecheck -code-completion-diagnostics -source-filename %s
+// RUN: %target-swift-ide-test -code-completion -batch-code-completion -skip-filecheck -code-completion-diagnostics -source-filename %s
 func a(b) a#^COMPLETE^#
