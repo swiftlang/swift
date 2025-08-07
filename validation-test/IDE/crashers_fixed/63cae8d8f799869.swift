d7fea157a122d4d6f5225ce9bff6e4653894bb87
Hamish Knight <hamish_knight@apple.com>
2025-08-07 15:09:47 +0100

    [IDE] Remove fallback type-checking logic
    This should no longer be necessary, rip it out.

--- validation-test/IDE/crashers_fixed/63cae8d8f799869.swift
+++ validation-test/IDE/crashers_fixed/63cae8d8f799869.swift
@@ -1,5 +1,5 @@
 // {"kind":"complete","original":"8d3b43e5","signature":"(anonymous namespace)::ConstraintWalker::walkToExprPost(swift::Expr*)"}
-// RUN: not --crash %target-swift-ide-test -code-completion -batch-code-completion -skip-filecheck -code-completion-diagnostics -source-filename %s
+// RUN: %target-swift-ide-test -code-completion -batch-code-completion -skip-filecheck -code-completion-diagnostics -source-filename %s
 [
   switch  {
   case ((\.a) #^^#
