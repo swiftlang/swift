7822efdfae11e1d4f3b91ab8bdbe7fabf464879d
Gabor Horvath <gaborh@apple.com>
2025-08-08 18:05:11 +0100

    [cxx-interop] Export empty enums as namespaces
    It is a common idiom in Swift to use empty enums as namespaces.
    Previously, we just exported all zero sized types as unavailable
    declarations so users had no way to access anything in these
    "namespaces". This PR changes reverse interop to treat empty enums as
    namespaces.
    
    rdar://147499583

--- lib/PrintAsClang/PrintClangFunction.cpp
+++ lib/PrintAsClang/PrintClangFunction.cpp
@@ -1625,7 +1625,8 @@ void DeclAndTypeClangFunctionPrinter::printCxxMethod(
   bool isMutating =
       isa<FuncDecl>(FD) ? cast<FuncDecl>(FD)->isMutating() : false;
   modifiers.isConst = !isa<ClassDecl>(typeDeclContext) && !isMutating &&
-                      !isConstructor && !isStatic;
+                      !isConstructor && !isStatic &&
+                      !declAndTypePrinter.isEmptyEnum(typeDeclContext);
   modifiers.hasSymbolUSR = !isDefinition;
   auto result = printFunctionSignature(
       FD, signature, cxx_translation::getNameForCxx(FD), resultTy,
