a7d57d75a2fe098f876e62450edae27a915f8d46
Michael Gottesman <mgottesman@apple.com>
2025-08-08 13:50:02 -0700

    [silgen] Change two places we used Builtin.Executor to instead use Optional<any Actor> as an expected executor.
    We want SILGen to have a simplified view of its executor and know that whenever
    one sees an Actor, it is an actual actor instead of a Builtin.Executor. This
    just simplifies code. Also, we should eventually have an invariant that
    Builtin.Executor should only be allowed in LoweredSIL after LowerHopToExecutor
    has run. But that is a change for another day.

--- test/SILGen/objc_async.swift
+++ test/SILGen/objc_async.swift
@@ -255,7 +255,7 @@ func testThrowingMethodFromMain(slowServer: SlowServer) async -> String {
 // rdar://91502776
 // CHECK-LABEL: sil hidden [ossa] @$s{{.*}}21checkCostcoMembershipSbyYaF : $@convention(thin) @async () -> Bool {
 // CHECK:    bb0:
-// CHECK:        hop_to_executor {{%.*}} : $Optional<Builtin.Executor>
+// CHECK:        hop_to_executor {{%.*}} : $Optional<any Actor>
 // CHECK:        [[FINAL_BUF:%.*]] = alloc_stack $Bool
 // CHECK:        [[RESULT_BUF:%.*]] = alloc_stack $NSObject
 // CHECK:        [[METH:%.*]] = objc_method {{%.*}} : $@objc_metatype Person.Type, #Person.asCustomer!foreign
@@ -264,7 +264,7 @@ func testThrowingMethodFromMain(slowServer: SlowServer) async -> String {
 // CHECK:        dealloc_stack {{%.*}} : $*@block_storage
 // CHECK:        await_async_continuation {{%.*}} : $Builtin.RawUnsafeContinuation, resume bb1
 // CHECK:    bb1:
-// CHECK:        hop_to_executor {{%.*}} : $Optional<Builtin.Executor>
+// CHECK:        hop_to_executor {{%.*}} : $Optional<any Actor>
 // CHECK:        [[RESULT:%.*]] = load [take] [[RESULT_BUF]] : $*NSObject
 // CHECK:        objc_method {{%.*}} : $CostcoManager, #CostcoManager.isCustomerEnrolled!foreign
 // CHECK:        get_async_continuation_addr Bool, [[FINAL_BUF]] : $*Bool
@@ -275,7 +275,7 @@ func testThrowingMethodFromMain(slowServer: SlowServer) async -> String {
 // CHECK:        dealloc_stack [[BLOCK_STORAGE]] : $*@block_storage
 // CHECK:        await_async_continuation {{%.*}} : $Builtin.RawUnsafeContinuation, resume bb2
 // CHECK:    bb2:
-// CHECK:        hop_to_executor {{%.*}} : $Optional<Builtin.Executor>
+// CHECK:        hop_to_executor {{%.*}} : $Optional<any Actor>
 // CHECK:        [[ANSWER:%.*]] = load [trivial] [[FINAL_BUF]] : $*Bool
 // CHECK:        fix_lifetime [[EXTEND2]] : $CostcoManager
 // CHECK:        destroy_value [[EXTEND2]] : $CostcoManager
@@ -325,7 +325,7 @@ extension OptionalMemberLookups {
 // CHECK: destroy_value {{.*}} : $MainActor
 // CHECK: dealloc_stack {{.*}} : $*AutoreleasingUnsafeMutablePointer<Optional<NSError>>
 // CHECK: dealloc_stack {{.*}} : $*@sil_unmanaged Optional<NSError>
-// CHECK: hop_to_executor {{.*}} : $Optional<Builtin.Executor>
+// CHECK: hop_to_executor {{.*}} : $Optional<any Actor>
 // CHECK: switch_enum
 func checkHotdogs(_ v: some HotdogCompetitor, _ timeLimit: NSObject) async throws -> String? {
     return try await v.pileOfHotdogsToEat(withLimit: timeLimit)
