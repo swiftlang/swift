0d5e8c9c781a00b6937d4868e1c4538ac0d9759d
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2025-08-09 13:20:48 +0300

    [IDE] Erase archetypes with no declaration generic signature
    This generalization enables curried functions with generic parameters coming from the initial declaration to be printed with the archetype's upperbound rather than '_' unresolved type.
    
    As an added benefit, T.self and T.Type for generic parameters now get shown as the upperbound of the generic parameter provided

--- test/IDE/complete_keypath_member_lookup.swift
+++ test/IDE/complete_keypath_member_lookup.swift
@@ -125,7 +125,7 @@ func testGenericUnderconstrained1<G: P>(r: G) {
   r.#^testGenericUnderconstrained1^#
 }
 // testGenericUnderconstrained1-NOT: CurrNominal
-// testGenericUnderconstrained1: Keyword[self]/CurrNominal:          self[#{{any P|G}}#];
+// testGenericUnderconstrained1: Keyword[self]/CurrNominal:          self[#{{any P|P}}#];
 // testGenericUnderconstrained1-NOT: CurrNominal
 
 func testExistential1(r: P) {
