b48d052f64a13ca07552aeed2a9bd85bb8594218
Alex Hoppen <ahoppen@apple.com>
2025-08-13 13:06:32 +0200

    Revert "[Windows] Use a multiroot data file to test (corelibs-)foundation on Windows"
    This reverts commit 61f833c04c6ef9521b59d9ba7749cb1d862c0ed2.

--- utils/build.ps1
+++ utils/build.ps1
@@ -2834,17 +2834,12 @@ function Build-Foundation {
 }
 
 function Test-Foundation {
-  $ScratchPath = "$BinaryCache\$($BuildPlatform.Triple)\FoundationTests"
-
   # Foundation tests build via swiftpm rather than CMake
   Build-SPMProject `
     -Action Test `
     -Src $SourceCache\swift-foundation `
-    -Bin "$ScratchPath" `
-    -Platform $BuildPlatform `
-    -Configuration $FoundationTestConfiguration `
-    --multiroot-data-file "$SourceCache\swift\utils\build_swift\resources\SwiftPM-Unified-Build.xcworkspace" `
-    --test-product swift-foundationPackageTests
+    -Bin "$BinaryCache\$($BuildPlatform.Triple)\CoreFoundationTests" `
+    -Platform $BuildPlatform
 
   Invoke-IsolatingEnvVars {
     $env:DISPATCH_INCLUDE_PATH="$(Get-SwiftSDK $BuildPlatform.OS)/usr/include"
@@ -2856,12 +2851,10 @@ function Test-Foundation {
     Build-SPMProject `
       -Action Test `
       -Src $SourceCache\swift-corelibs-foundation `
-      -Bin "$ScratchPath" `
+      -Bin "$BinaryCache\$($BuildPlatform.Triple)\FoundationTests" `
       -Platform $BuildPlatform `
       -Configuration $FoundationTestConfiguration `
-      --multiroot-data-file "$SourceCache\swift\utils\build_swift\resources\SwiftPM-Unified-Build.xcworkspace" `
-      --test-product swift-corelibs-foundationPackageTests `
-      -j 1 # Running parallel causes a non-deterministic crash in CI only, see https://github.com/swiftlang/swift/issues/83606
+      -j 1
   }
 }
 
