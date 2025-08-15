8d8045459113895c8901bd47097bbff0b4d34a9c
Hamish Knight <hamish_knight@apple.com>
2025-08-15 17:09:33 +0100

    [test] Record whether a crasher is a stack overflow

--- validation-test/compiler_crashers_2/275b15e1edc79b4f.swift
+++ validation-test/compiler_crashers_2/275b15e1edc79b4f.swift
@@ -1,4 +1,4 @@
-// {"kind":"typecheck","signature":"(anonymous namespace)::TypeSubstituter::transformDependentMemberType(swift::DependentMemberType*, swift::TypePosition)"}
+// {"kind":"typecheck","signature":"(anonymous namespace)::TypeSubstituter::transformDependentMemberType(swift::DependentMemberType*, swift::TypePosition)","stackOverflow":true}
 // RUN: not --crash %target-swift-frontend -typecheck %s
 protocol a: Collection where Iterator == Self
   struct b<c: a>: IteratorProtocol {next -> c.Element? struct c: a {
