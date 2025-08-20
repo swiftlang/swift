a7ecd309d4577b7974a2c912db3f3b23f83a61be
Pavel Yaskevich <pyaskevich@apple.com>
2025-08-20 00:13:34 -0700

    [AST] SE-0487: Promote `NonexhaustiveAttribute` to a language feature
    The proposal [has been accepted](https://forums.swift.org/t/accepted-se-0487-nonexhaustive-enums/81508)
    which makes the feature experimental no longer.

--- test/IDE/complete_decl_attribute_feature_requirement.swift
+++ test/IDE/complete_decl_attribute_feature_requirement.swift
@@ -3,18 +3,15 @@
 // it's enabled. When a feature becomes non-experimental, move its test cases
 // into the normal complete_decl_attribute.swift test file.
 
-// REQUIRES: swift_feature_NonexhaustiveAttribute
-
 // RUN: %batch-code-completion -filecheck-additional-suffix _DISABLED
-// RUN: %batch-code-completion -filecheck-additional-suffix _ENABLED \
-// RUN:        -enable-experimental-feature NonexhaustiveAttribute
+// RUN: %batch-code-completion -filecheck-additional-suffix _ENABLED
+
+// NOTE: There are currently no experimental features that need code completion
+// testing, but this test file is being left in place for when it's needed
+// again. At that time, please remove the ABIAttribute tests.
+// REQUIRES: new_use_case
 
 // NOTE: Please do not include the ", N items" after "Begin completions". The
 // item count creates needless merge conflicts given that an "End completions"
 // line exists for each test.
 
-@#^KEYWORD4^# enum E {}
-// KEYWORD4:              Begin completions
-// KEYWORD4_ENABLED-DAG:  Keyword/None:              nonexhaustive[#{{.*}} Attribute#]; name=nonexhaustive
-// KEYWORD4_DISABLED-NOT: Keyword/None:              nonexhaustive[#{{.*}} Attribute#]; name=nonexhaustive
-// KEYWORD4:              End completions
