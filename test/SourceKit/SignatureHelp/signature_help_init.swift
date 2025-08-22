4928d076ad5200e6935106cb4158f819c47cbea9
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2025-08-22 19:33:00 +0300

    [Test] Change signature help tests to use split-file

--- test/SourceKit/SignatureHelp/signature_help_init.swift
+++ test/SourceKit/SignatureHelp/signature_help_init.swift
@@ -1,31 +1,36 @@
+// RUN: %empty-directory(%t)
+// RUN: split-file %s %t
+// RUN: %sourcekitd-test -req=signaturehelp -pos=5:27 %t/input.swift -- %t/input.swift > %t/actual.result
+// RUN: diff -u %t/expected.result %t/actual.result
+
+//--- input.swift
 struct Person {
   init(name: String, age: Int, profession job: String) { }
 }
 
 Person(name: "John", age: )
 
-// RUN: %sourcekitd-test -req=signaturehelp -pos=5:27 %s -- %s | %FileCheck -check-prefix=CHECK %s
-
-// CHECK:      {
-// CHECK-NEXT:   key.signatures: [
-// CHECK-NEXT:     {
-// CHECK-NEXT:       key.name: "init(name: String, age: Int, profession: String)",
-// CHECK-NEXT:       key.parameters: [
-// CHECK-NEXT:         {
-// CHECK-NEXT:           key.nameoffset: 5,
-// CHECK-NEXT:           key.namelength: 12
-// CHECK-NEXT:         },
-// CHECK-NEXT:         {
-// CHECK-NEXT:           key.nameoffset: 19,
-// CHECK-NEXT:           key.namelength: 8
-// CHECK-NEXT:         },
-// CHECK-NEXT:         {
-// CHECK-NEXT:           key.nameoffset: 29,
-// CHECK-NEXT:           key.namelength: 18
-// CHECK-NEXT:         }
-// CHECK-NEXT:       ],
-// CHECK-NEXT:       key.active_parameter: 1
-// CHECK-NEXT:     }
-// CHECK-NEXT:   ],
-// CHECK-NEXT:   key.active_signature: 0
-// CHECK-NEXT: }
+//--- expected.result
+{
+  key.signatures: [
+    {
+      key.name: "init(name: String, age: Int, profession: String)",
+      key.parameters: [
+        {
+          key.nameoffset: 5,
+          key.namelength: 12
+        },
+        {
+          key.nameoffset: 19,
+          key.namelength: 8
+        },
+        {
+          key.nameoffset: 29,
+          key.namelength: 18
+        }
+      ],
+      key.active_parameter: 1
+    }
+  ],
+  key.active_signature: 0
+}
