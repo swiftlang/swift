0999643e7743b467ac3c91b9bfee158072e577f3
Hamish Knight <hamish_knight@apple.com>
2025-08-22 10:47:10 +0100

    [Sema] Resolve interface type in `ExtendedTypeRequest`
    Resolving only a structural type meant we weren't checking generic
    constraints, allowing invalid extensions to get past the type-checker.
    Change to resolve an interface type.

--- lib/Sema/TypeCheckDecl.cpp
+++ lib/Sema/TypeCheckDecl.cpp
@@ -3028,7 +3028,7 @@ ExtendedTypeRequest::evaluate(Evaluator &eval, ExtensionDecl *ext) const {
   TypeResolutionOptions options(TypeResolverContext::ExtensionBinding);
   if (ext->isInSpecializeExtensionContext())
     options |= TypeResolutionFlags::AllowUsableFromInline;
-  const auto resolution = TypeResolution::forStructural(
+  const auto resolution = TypeResolution::forInterface(
       ext->getDeclContext(), options, nullptr,
       // FIXME: Don't let placeholder types escape type resolution.
       // For now, just return the placeholder type.
