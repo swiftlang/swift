e78ce6165f51078a233f4da17eab87c3a74f0cac
Egor Zhdan <e_zhdan@apple.com>
2025-08-22 18:18:14 +0100

    [cxx-interop] Allow retain/release operations to be methods
    Some foreign reference types such as IUnknown define retain/release operations as methods of the type.
    
    Previously Swift only supported retain/release operations as standalone functions.
    
    The syntax for member functions would be `SWIFT_SHARED_REFERENCE(.doRetain, .doRelease)`.
    
    rdar://160696723

--- lib/ClangImporter/SwiftDeclSynthesizer.cpp
+++ lib/ClangImporter/SwiftDeclSynthesizer.cpp
@@ -2882,8 +2882,7 @@ FuncDecl *SwiftDeclSynthesizer::findExplicitDestroy(
     if (!destroyFuncName.consume_front("destroy:"))
       continue;
 
-    auto decls = getValueDeclsForName(
-        clangType, nominal->getASTContext(), destroyFuncName);
+    auto decls = getValueDeclsForName(nominal, destroyFuncName);
     for (auto decl : decls) {
       auto func = dyn_cast<FuncDecl>(decl);
       if (!func)
