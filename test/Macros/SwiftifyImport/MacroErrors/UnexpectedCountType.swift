70ec916d176e071e55a753ca19c97d531d4a55a9
Henrik G. Olsson <h_olsson@apple.com>
2025-08-27 15:33:24 -0700

    [Swiftify] Remove XFAIL from UnexpectedPointerType.swift test
    Now that the DiagnosticsVerifier supports verifying diagnostics in
    macro expansions it's time to enable this test case.

--- test/Macros/SwiftifyImport/MacroErrors/UnexpectedCountType.swift
+++ test/Macros/SwiftifyImport/MacroErrors/UnexpectedCountType.swift
@@ -1,17 +1,25 @@
-// REQUIRES: swift_swift_parser
-
-// XFAIL: *
-
-// RUN: not %target-swift-frontend %s -swift-version 5 -module-name main -disable-availability-checking -typecheck -plugin-path %swift-plugin-dir -dump-macro-expansions 2>&1 | %FileCheck --match-full-lines %s
-// RUN: %target-typecheck-verify-swift %s -swift-version 5 -module-name main -disable-availability-checking -typecheck -plugin-path %swift-plugin-dir -verify
-
+//--- test.swift
 @_SwiftifyImport(.countedBy(pointer: .param(1), count: "len"))
 func myFunc(_ ptr: UnsafePointer<CInt>, _ len: String) {
 }
+// expected-note@-3 2{{in expansion of macro '_SwiftifyImport' on global function 'myFunc' here}}
 
-// CHECK:      @_alwaysEmitIntoClient @_disfavoredOverload
-// CHECK-NEXT: func myFunc(_ ptr: UnsafeBufferPointer<CInt>) {
-// CHECK-NEXT:     myFunc(ptr.baseAddress!, String(exactly: ptr.count)!)
-// CHECK-NEXT: }
+// expected-error@@__swiftmacro_4main6myFunc15_SwiftifyImportfMp_.swift:4:15{{no exact matches in call to initializer}}
+// expected-error@@__swiftmacro_4main6myFunc15_SwiftifyImportfMp_.swift:4:48{{cannot force unwrap value of non-optional type 'String'}}
 
-// expected-error@_SwiftifyImport:2{{no exact matches in call to initializer}}
+// REQUIRES: swift_swift_parser
+// RUN: %empty-directory(%t)
+// RUN: split-file %s %t
+// RUN: %target-swift-frontend %t/test.swift -swift-version 5 -module-name main -disable-availability-checking -typecheck -plugin-path %swift-plugin-dir -dump-macro-expansions -verify 2> %t/dump.txt
+// RUN: diff %t/myFunc.expected %t/dump.txt
+
+//--- myFunc.expected
+@__swiftmacro_4main6myFunc15_SwiftifyImportfMp_.swift
+------------------------------
+/// This is an auto-generated wrapper for safer interop
+@_alwaysEmitIntoClient @_disfavoredOverload
+func myFunc(_ ptr: UnsafeBufferPointer<CInt>) {
+    let len = String(exactly: unsafe ptr.count)!
+    return unsafe myFunc(ptr.baseAddress!, len)
+}
+------------------------------
