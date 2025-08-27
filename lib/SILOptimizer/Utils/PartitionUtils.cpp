ee5faaf4d2f5bd684950c87d5b32d26734ae97cb
Michael Gottesman <mgottesman@apple.com>
2025-08-27 11:53:26 -0700

    [rbi] Change IsolationHistory::Node to use next as its pointer name instead of parent.
    Just makes it easier to think of it in code as a true linked list since it fits
    the normal nomenclature.

--- lib/SILOptimizer/Utils/PartitionUtils.cpp
+++ lib/SILOptimizer/Utils/PartitionUtils.cpp
@@ -775,7 +775,7 @@ void Partition::printHistory(llvm::raw_ostream &os) const {
     }
     os << "\n";
 
-  } while ((head = head->getParent()));
+  } while ((head = head->getNext()));
 }
 
 bool Partition::is_canonical_correct() {
@@ -1086,6 +1086,6 @@ IsolationHistory::Node *IsolationHistory::pop() {
     return nullptr;
 
   auto *result = head;
-  head = head->parent;
+  head = head->next;
   return result;
 }
