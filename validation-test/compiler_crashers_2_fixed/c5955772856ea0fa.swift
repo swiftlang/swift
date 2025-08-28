627f41ccbcdaeb43c11c911e0a3e132831bc5e55
Hamish Knight <hamish_knight@apple.com>
2025-08-28 23:48:14 +0100

    [CS] Fix an overly strict assert
    `matchExistentialTypes` can handle existential metatypes, fix the
    assert to handle that.

--- validation-test/compiler_crashers_2_fixed/c5955772856ea0fa.swift
+++ validation-test/compiler_crashers_2_fixed/c5955772856ea0fa.swift
@@ -1,3 +1,3 @@
 // {"kind":"typecheck","signature":"swift::constraints::Constraint::create(swift::constraints::ConstraintSystem&, swift::constraints::ConstraintKind, swift::Type, swift::Type, swift::constraints::ConstraintLocator*, llvm::ArrayRef<swift::TypeVariableType*>)","signatureAssert":"Assertion failed: ((kind != ConstraintKind::ConformsTo && kind != ConstraintKind::NonisolatedConformsTo && kind != ConstraintKind::TransitivelyConformsTo) || second->isExistentialType()), function create"}
-// RUN: not --crash %target-swift-frontend -typecheck %s
+// RUN: not %target-swift-frontend -typecheck %s
 protocol a let : a.Type.Type = b->c
