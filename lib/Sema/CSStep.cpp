beb9eb75673b03dbead116529724bf001b9f8546
Hamish Knight <hamish_knight@apple.com>
2025-09-01 14:36:25 +0100

    [DNM] Experiment: Try skipping unavailable/disfavored choices for completion

--- lib/Sema/CSStep.cpp
+++ lib/Sema/CSStep.cpp
@@ -615,6 +615,18 @@ bool TypeChecker::isDeclRefinementOf(ValueDecl *declA, ValueDecl *declB) {
                            false);
 }
 
+bool DisjunctionStep::isLastChoiceBetterThan(ScoreKind kind) const {
+  if (!LastSolvedChoice)
+    return false;
+
+  auto &lastScore = LastSolvedChoice->second;
+  for (unsigned i = 0, n = unsigned(kind) + 1; i != n; ++i) {
+    if (lastScore.Data[i] > 0)
+      return false;
+  }
+  return true;
+}
+
 bool DisjunctionStep::shouldSkip(const DisjunctionChoice &choice) const {
   auto &ctx = CS.getASTContext();
 
@@ -635,27 +647,21 @@ bool DisjunctionStep::shouldSkip(const DisjunctionChoice &choice) const {
   if (choice.isDisabled())
     return skip("disabled");
 
-  if (!CS.shouldAttemptFixes()) {
+  if (!CS.shouldAttemptFixes() || CS.isForCodeCompletion()) {
     // Skip unavailable overloads.
-    if (choice.isUnavailable())
-      return skip("unavailable");
+    if (choice.isUnavailable()) {
+      // For code completion, we only want to do this if we've already seen
+      // a better choice.
+      if (!CS.isForCodeCompletion() || isLastChoiceBetterThan(SK_Unavailable))
+        return skip("unavailable");
+    }
 
     // Since the disfavored overloads are always located at the end of
     // the partition they could be skipped if there was at least one
     // valid solution for this partition already, because the solution
     // they produce would always be worse.
-    if (choice.isDisfavored() && LastSolvedChoice) {
-      bool canSkipDisfavored = true;
-      auto &lastScore = LastSolvedChoice->second;
-      for (unsigned i = 0, n = unsigned(SK_DisfavoredOverload) + 1; i != n;
-           ++i) {
-        if (lastScore.Data[i] > 0) {
-          canSkipDisfavored = false;
-          break;
-        }
-      }
-
-      if (canSkipDisfavored)
+    if (choice.isDisfavored()) {
+      if (isLastChoiceBetterThan(SK_DisfavoredOverload))
         return skip("disfavored");
     }
   }
