45dcb68af047cdbd1d0467b82b07578cf2d981ae
Egor Zhdan <e_zhdan@apple.com>
2025-09-03 16:21:55 +0100

    [cxx-interop] Test initializing foreign reference type when passed by parameter
    This only adds a test.

--- test/Interop/Cxx/foreign-reference/pass-as-parameter-module-interface.swift
+++ test/Interop/Cxx/foreign-reference/pass-as-parameter-module-interface.swift
@@ -7,3 +7,5 @@
 // CHECK: func extractValueFromRefToConstPtr(_ b: inout IntBox!) -> Int32
 // CHECK: func extractValueFromConstRefToPtr(_ b: IntBox!) -> Int32
 // CHECK: func extractValueFromConstRefToConstPtr(_ b: IntBox!) -> Int32
+
+// CHECK: func initializeByPtr(_ value: Int32, _ b: UnsafeMutablePointer<IntBox?>!)
