7b6b576d454e54ed7a7f02a119d05ae063cbc4bc
Egor Zhdan <e_zhdan@apple.com>
2025-09-04 15:08:21 +0100

    [cxx-interop] Avoid trying to instantiate copy constructor of `std::optional<NonCopyable>`
    In libc++, `std::optional` inherits from several mixin base types. Some of those base types do not declare a deleted copy constructor for non-copyable value types, which works fine because clients are not supposed to use those base types directly from C++.
    
    Swift, however, imports all of the transitive base types when importing a C++ type. As part of this, ClangImporter will attempt to instantiate e.g. `std::__optional_copy_assign_base<NonCopyable>`, and will fail with a hard error while doing so.
    
    rdar://152718041
    
    (cherry picked from commit 124055a51a1e5c0900b2c38896264fde8d1c397b)

--- test/Interop/Cxx/stdlib/Inputs/std-optional.h
+++ test/Interop/Cxx/stdlib/Inputs/std-optional.h
@@ -7,11 +7,24 @@
 using StdOptionalInt = std::optional<int>;
 using StdOptionalString = std::optional<std::string>;
 
+struct HasDeletedCopyCtor {
+  int value;
+  HasDeletedCopyCtor(int value) : value(value) {}
+  HasDeletedCopyCtor(const HasDeletedCopyCtor &other) = delete;
+  HasDeletedCopyCtor(HasDeletedCopyCtor &&other) = default;
+};
+using StdOptionalHasDeletedCopyCtor = std::optional<HasDeletedCopyCtor>;
+
 inline StdOptionalInt getNonNilOptional() { return {123}; }
 
 inline StdOptionalInt getNilOptional() { return {std::nullopt}; }
 
+inline StdOptionalHasDeletedCopyCtor getNonNilOptionalHasDeletedCopyCtor() {
+  return StdOptionalHasDeletedCopyCtor(HasDeletedCopyCtor(654));
+}
+
 inline bool takesOptionalInt(std::optional<int> arg) { return (bool)arg; }
 inline bool takesOptionalString(std::optional<std::string> arg) { return (bool)arg; }
+inline bool takesOptionalHasDeletedCopyCtor(std::optional<HasDeletedCopyCtor> arg) { return (bool)arg; }
 
 #endif // TEST_INTEROP_CXX_STDLIB_INPUTS_STD_OPTIONAL_H
