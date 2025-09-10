95ff51c739597558ae105c40ade45c21b52c0b8b
Gábor Horváth <xazax.hun@gmail.com>
2025-09-10 20:40:03 +0100

    Merge pull request #84199 from Xazax-hun/no-reflection-anon-types
    [cxx-interop] Restrict the uses of anonymous types

--- lib/ClangImporter/ImportDecl.cpp
+++ lib/ClangImporter/ImportDecl.cpp
@@ -4598,6 +4598,10 @@ namespace {
         // FIXME: Temporarily unreachable because of check above.
         markAsVariant(result, *correctSwiftName);
 
+      if (decl->isAnonymousStructOrUnion())
+        Impl.markUnavailable(
+            result, "refer to the members of the anonymous type instead");
+
       return result;
     }
 
