7688401b4a415035b21b2ccec0c0e50026ed1755
Fahad Nayyar <f_nayyar@apple.com>
2025-09-11 13:59:47 -0700

    added more tests

--- test/Interop/Cxx/templates/Inputs/ForwardDeclaredSpecialization.h
+++ test/Interop/Cxx/templates/Inputs/ForwardDeclaredSpecialization.h
@@ -11,9 +11,26 @@ struct MyTemplate<int>;
 typedef MyTemplate<int> MyIntTemplate;
 
 template <> 
+struct MyTemplate<int>;
+typedef MyTemplate<int> MyIntTemplate2;
+
+template <> 
 struct MyTemplate<double> {
   double value;
 };
-typedef MyTemplate<double> MyCompleteIntTemplate;
+typedef MyTemplate<double> MyCompleteDoubleTemplate;
 
-#endif // FORWARD_DECLARED_SPECIALIZATION_H
+typedef MyTemplate<float> MyFloatTemplate;
+typedef MyTemplate<bool> MyCompleteBoolTemplate;
+
+template <>
+struct MyTemplate<char>;
+typedef MyTemplate<char> MyCharTemplate;
+template <>
+struct MyTemplate<char> {
+  char value;
+};
+
+typedef MyTemplate<char> MyCharTemplate2;
+
+#endif
--- test/Interop/Cxx/templates/forward-declared-specialization.swift
+++ test/Interop/Cxx/templates/forward-declared-specialization.swift
@@ -2,8 +2,21 @@
 
 import ForwardDeclaredSpecialization
 
-func testForwardDeclaredSpecialization(_ param: MyIntTemplate) { // expected-error {{cannot find type 'MyIntTemplate' in scope}}
+func testForwardDeclaredSpecialization(_ param: MyIntTemplate) { 
+  // expected-error@-1 {{cannot find type 'MyIntTemplate' in scope}}
 }
 
-func testCompleteSpecialization(_ param: MyCompleteIntTemplate) {
+func testCompleteSpecialization(_ param: MyCompleteDoubleTemplate) {
+}
+
+func testTypedefWithoutSpecialization(_ param: MyFloatTemplate) {
+}
+
+func testTypedefWithoutSpecialization2(_ param: MyCompleteBoolTemplate) {
+}
+
+func testLaterDefinedSpecialization(_ param: MyCharTemplate) {
+}
+
+func testLaterDefinedSpecialization2(_ param: MyCharTemplate2) {
 }
