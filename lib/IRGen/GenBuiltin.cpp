7c49111f8e5f51895cd66a4eceacf350bcdc89d3
John McCall <rjmccall@apple.com>
2025-09-12 16:25:53 -0400

    Remove the deprecated and unused endAsyncLet builtin.

--- lib/IRGen/GenBuiltin.cpp
+++ lib/IRGen/GenBuiltin.cpp
@@ -250,14 +250,6 @@ void irgen::emitBuiltinCall(IRGenFunction &IGF, const BuiltinInfo &Builtin,
     return;
   }
 
-  if (Builtin.ID == BuiltinValueKind::EndAsyncLet) {
-    emitEndAsyncLet(IGF, args.claimNext());
-    // Ignore a second operand which is inserted by ClosureLifetimeFixup and
-    // only used for dependency tracking.
-    (void)args.claimAll();
-    return;
-  }
-
   if (Builtin.ID == BuiltinValueKind::EndAsyncLetLifetime) {
     IGF.Builder.CreateLifetimeEnd(args.claimNext());
     // Ignore a second operand which is inserted by ClosureLifetimeFixup and
--- lib/SIL/Utils/MemAccessUtils.cpp
+++ lib/SIL/Utils/MemAccessUtils.cpp
@@ -2644,7 +2644,6 @@ static void visitBuiltinAddress(BuiltinInst *builtin,
     case BuiltinValueKind::DestroyDefaultActor:
     case BuiltinValueKind::GetCurrentExecutor:
     case BuiltinValueKind::StartAsyncLetWithLocalBuffer:
-    case BuiltinValueKind::EndAsyncLet:
     case BuiltinValueKind::EndAsyncLetLifetime:
     case BuiltinValueKind::CreateTaskGroup:
     case BuiltinValueKind::CreateTaskGroupWithFlags:
