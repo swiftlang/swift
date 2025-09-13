888e6d659a46fe6db8a610941821c4413fd52df8
Henrik G. Olsson <h_olsson@apple.com>
2025-09-12 22:32:52 -0700

    Revert "[SourceKit] Add regression test for submodule import printing behaviour"
    This reverts commit 0cf53ae7f5fbb27f79d70e4997d3b2f3b27c8028.

--- test/SourceKit/InterfaceGen/gen_foundation.swift
+++
@@ -1,9 +0,0 @@
-RUN: %sourcekitd-test -req=interface-gen -module Foundation -- -target %target-triple -sdk %sdk | %FileCheck %s
-RUN: %sourcekitd-test -req=interface-gen -module Foundation.NSArray -- -target %target-triple -sdk %sdk | %FileCheck %s --check-prefix=CHECK-NSARRAY
-
-REQUIRES: OS=macosx
-
-This tests that printing the interface of a submodule does not print imports of other submodules of the same TLM.
-
-CHECK: import Foundation.NSArray
-CHECK-NSARRAY-NOT: import
