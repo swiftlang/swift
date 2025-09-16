0712a0effb11dd618b43b155655f2f70da242e36
Andrew Trick <atrick@apple.com>
2025-09-16 00:36:43 -0700

    LifetimeDependenceDiagnostics: skip implicit Unsafe*Pointer methods.
    An implicit setter is generated for the unsafe Pointee addressor. To make
    diagnostics pass on the setter, it would need
        _overrideLifetime(newValue, borrowing: ()).
    
    But since the setter itself is compiler-generated, treat the override as
    implicit.

--- SwiftCompilerSources/Sources/Optimizer/FunctionPasses/LifetimeDependenceDiagnostics.swift
+++ SwiftCompilerSources/Sources/Optimizer/FunctionPasses/LifetimeDependenceDiagnostics.swift
@@ -43,6 +43,9 @@ let lifetimeDependenceDiagnosticsPass = FunctionPass(
   log("\(function)")
   log("\(function.convention)")
 
+  if skipStdlibFunction(function) {
+    return
+  }
   for argument in function.arguments
       where !argument.type.isEscapable(in: function)
   {
@@ -88,6 +91,22 @@ let lifetimeDependenceDiagnosticsPass = FunctionPass(
       continue
     }
   }
+}
+
+/// Do not diagnose implicit unsafe pointer methods. They are effectively builtins. Manually overriding lifetimes is
+/// undesirable and impossible because of rules regarding which combination of accessors can be user-defined.
+private func skipStdlibFunction(_ function: Function) -> Bool {
+  if !function.isImplicit {
+    return false
+  }
+  guard let selfType = function.selfArgument?.type else {
+    return false
+  }
+  if selfType.isAnyUnsafePointer || selfType.isAnyUnsafeBufferPointer {
+    log("Skipping implicit unsafe pointer method... They are considered builtins.")
+    return true
+  }
+  return false
 }
 
 /// Analyze a single Lifetime dependence and trigger diagnostics.
