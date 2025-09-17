99895359ed19b28e1108faa104de5bc2be236196
Anthony Latsis <alatsis@apple.com>
2025-09-17 15:48:29 +0100

    [build-windows-toolchain.bat] Fix environment variable name

--- utils/build-windows-toolchain.bat
+++ utils/build-windows-toolchain.bat
@@ -81,7 +81,7 @@ call :CloneRepositories || (exit /b 1)
 
 :: We only have write access to BuildRoot, so use that as the image root.
 powershell.exe -ExecutionPolicy RemoteSigned -File %~dp0build.ps1 ^
-  %HostArchName% ^
+  %HostArchNameArg% ^
   -SourceCache %SourceRoot% ^
   -BinaryCache %BuildRoot% ^
   -ImageRoot %BuildRoot% ^
