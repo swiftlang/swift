68c9c3ca233b1e0f4b774e58d84155ffcbd3db9a
Henrik G. Olsson <h_olsson@apple.com>
2025-09-19 16:37:07 -0700

    Ignore file newline differences between platforms in diff

--- test/Interop/C/swiftify-import/counted-by.swift
+++ test/Interop/C/swiftify-import/counted-by.swift
@@ -5,7 +5,7 @@
 // RUN: %generate-callers(module:CountedByClang) > %t/test.swift
 // RUN: %verify-safe-wrappers -enable-experimental-feature SafeInteropWrappers %t/test.swift
 // RUN: %dump-safe-wrappers -enable-experimental-feature SafeInteropWrappers %t/test.swift 2> %t/expansions.out
-// RUN: diff %t/expansions.out %t/expansions.expected
+// RUN: diff --strip-trailing-cr %t/expansions.out %t/expansions.expected
 
 //--- expansions.expected
 @__swiftmacro_So6simple15_SwiftifyImportfMp_.swift
