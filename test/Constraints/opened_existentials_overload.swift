swiftlang/swift

commit 9e3d0e0a8c78076a70a9869d10acc6189cb7bb6b

Sema: Move weird ConstructorDecl hack from getEffectiveOverloadType() to simplifyAppliedOverloadsImpl()

lib/Sema/CSSimplify.cpp
@@ -13210,18 +13210,14 @@ bool ConstraintSystem::simplifyAppliedOverloadsImpl(
 
   /// The common result type amongst all function overloads.
   Type commonResultType;
-  auto updateCommonResultType = [&](Type choiceType) {
-    auto markFailure = [&] {
-      commonResultType = ErrorType::get(getASTContext());
-    };
 
-    auto choiceFnType = choiceType->getAs<FunctionType>();
-    if (!choiceFnType)
-      return markFailure();
+  auto markFailure = [&] {
+    commonResultType = ErrorType::get(getASTContext());
+  };
 
+  auto updateCommonResultType = [&](Type choiceResultType) {
     // For now, don't attempt to establish a common result type when there
     // are type parameters.
-    Type choiceResultType = choiceFnType->getResult();
     if (choiceResultType->hasTypeParameter())
       return markFailure();
 
@@ -13359,8 +13355,28 @@ retry_after_fail:
             choiceType = objectType;
         }
 
-        // If we have a function type, we can compute a common result type.
-        updateCommonResultType(choiceType);
+        if (auto *choiceFnType = choiceType->getAs<FunctionType>()) {
+          if (isa<ConstructorDecl>(choice.getDecl())) {
+            auto choiceResultType = choice.getBaseType()
+                                        ->getRValueType()
+                                        ->getMetatypeInstanceType();
+
+            if (choiceResultType->getOptionalObjectType()) {
+              hasUnhandledConstraints = true;
+              return true;
+            }
+
+            if (choiceFnType->getResult()->getOptionalObjectType())
+              choiceResultType = OptionalType::get(choiceResultType);
+
+            updateCommonResultType(choiceResultType);
+          } else {
+            updateCommonResultType(choiceFnType->getResult());
+          }
+        } else {
+          markFailure();
+        }
+
         return true;
       });
 

lib/Sema/TypeOfReference.cpp
@@ -2008,25 +2008,9 @@ Type ConstraintSystem::getEffectiveOverloadType(ConstraintLocator *locator,
           return Type();
       }
 
-      // Hack for constructors.
       if (isa<ConstructorDecl>(decl) &&
-          !decl->getDeclContext()->getSelfProtocolDecl()) {
-        if (!overload.getBaseType())
-          return Type();
-
-        if (!overload.getBaseType()->getOptionalObjectType()) {
-          // `Int??(0)` if we look through all optional types for `Self`
-          // we'll end up with incorrect type `Int?` for result because
-          // the actual result type is `Int??`.
-          if (overload.getBaseType()
-                  ->getRValueType()
-                  ->getMetatypeInstanceType()
-                  ->getOptionalObjectType())
-            return Type();
-
-          type = type->replaceCovariantResultType(
-              getBaseObjectType(), /*uncurryLevel=*/2);
-        }
+          decl->getDeclContext()->getSelfClassDecl()) {
+        type = type->withCovariantResultType();
       }
 
       // Cope with 'Self' returns.

commit 40948d7abd05f92a670f6120be1df1f6f55b5458

Sema: Workaround for broken existential opening behavior
This is a narrow workaround for a regression from
9e3d0e0a8c78076a70a9869d10acc6189cb7bb6b.

There is no reason to skip this logic for protocol extension
members, except that doing so happens to break existential
opening in an expression that involves a call to Array.init
elsewhere.

However there is an underlying issue here with existential opening,
which doesn't seem to work right in the presence of overloading.

The test case demonstrates the fixed problem, together with an
existing bug that points to the underlying problem.

Fixes rdar://160389221.

lib/Sema/CSSimplify.cpp
@@ -13394,8 +13394,12 @@ retry_after_fail:
             choiceType = objectType;
         }
 
+        // FIXME: The !getSelfProtocolDecl() check is load-bearing, because
+        // this optimization interacts poorly with existential opening
+        // somehow. It should all be removed.
         if (auto *choiceFnType = choiceType->getAs<FunctionType>()) {
-          if (isa<ConstructorDecl>(choice.getDecl())) {
+          if (isa<ConstructorDecl>(choice.getDecl()) &&
+              !choice.getDecl()->getDeclContext()->getSelfProtocolDecl()) {
             auto choiceResultType = choice.getBaseType()
                                         ->getRValueType()
                                         ->getMetatypeInstanceType();

test/Constraints/opened_existentials_overload.swift
@@ -1,0 +1,30 @@
+// RUN: %target-typecheck-verify-swift
+
+protocol P {}
+
+func g(_: some P) {}
+// expected-note@-1 {{required by global function 'g' where 'some P' = 'any P'}}
+
+// rdar://problem/160389221
+func good(_ x: Array<any P>) {
+  Array(x).forEach { y in g(y) }
+}
+
+extension Array {
+  var ffirst: Element? { fatalError() }
+  func ffirst(wwhere: (Element) -> Bool) -> Element { fatalError() }
+}
+
+func bad(_ x: Array<any P>) {
+  let y = x.ffirst!
+  g(y) // ok
+
+  let yy = x.ffirst
+  g(yy!) // ok
+
+  // FIXME: This is broken
+
+  g(x.ffirst!)
+  // expected-error@-1 {{type 'any P' cannot conform to 'P'}}
+  // expected-note@-2 {{only concrete types such as structs, enums and classes can conform to protocols}}
+}