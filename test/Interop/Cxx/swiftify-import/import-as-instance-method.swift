a287803a3ba9ba9ea5c1b2628db27de14e10a77f
Henrik G. Olsson <h_olsson@apple.com>
2025-09-25 14:46:59 -0700

    try to debug CI diff failure

--- test/Interop/Cxx/swiftify-import/import-as-instance-method.swift
+++ test/Interop/Cxx/swiftify-import/import-as-instance-method.swift
@@ -4,7 +4,7 @@
 // RUN: split-file %s %t
 
 // RUN: env SWIFT_BACKTRACE="" %target-swift-frontend -emit-module -plugin-path %swift-plugin-dir -o %t/Test.swiftmodule -I %t/Inputs -enable-experimental-feature SafeInteropWrappers -strict-memory-safety -warnings-as-errors -Xcc -Werror %t/test.swift -cxx-interoperability-mode=default -Xcc -std=c++20 -dump-macro-expansions 2> %t/out.txt
-// RUN: diff --strip-trailing-cr %t/out.txt %t/out.expected
+// RUN: diff -u --strip-trailing-cr %t/out.txt %t/out.expected
 // RUN: %target-swift-frontend -emit-module -plugin-path %swift-plugin-dir -o %t/Test.swiftmodule -I %t/Inputs -enable-experimental-feature SafeInteropWrappers -strict-memory-safety -warnings-as-errors -Xcc -Werror %t/test.swift  -cxx-interoperability-mode=default -Xcc -std=c++20 -verify -verify-additional-file %t/Inputs/instance.h %t/test.swift -DVERIFY
 
 //--- test.swift
