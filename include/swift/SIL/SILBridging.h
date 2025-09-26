a84ca01adc203913e0beeace89706d4e9cca9c8b
Aidan Hall <ahall28@apple.com>
2025-09-26 12:24:38 +0100

    SILBridging: unsafe PackLengthInst_getPackType()

--- include/swift/SIL/SILBridging.h
+++ include/swift/SIL/SILBridging.h
@@ -928,7 +928,7 @@ struct BridgedInstruction {
   BRIDGED_INLINE SwiftInt FullApplySite_numIndirectResultArguments() const;
   BRIDGED_INLINE bool ConvertFunctionInst_withoutActuallyEscaping() const;
   SWIFT_IMPORT_UNSAFE BRIDGED_INLINE BridgedCanType TypeValueInst_getParamType() const;
-  BRIDGED_INLINE BridgedCanType PackLengthInst_getPackType() const;
+  SWIFT_IMPORT_UNSAFE BRIDGED_INLINE BridgedCanType PackLengthInst_getPackType() const;
   BRIDGED_INLINE SwiftInt ScalarPackIndexInst_getComponentIndex() const;
   SWIFT_IMPORT_UNSAFE BRIDGED_INLINE BridgedCanType AnyPackIndexInst_getIndexedPackType() const;
 
