2c919e138e4be6ecaff1560e3f6b582d7b4e18e5
Ellis Hoag <ellishoag@meta.com>
2025-09-30 08:42:11 -0700

    [ObjC][Gen] Emit ObjC strings to respective sections (#84300)
    Clang emits ObjC strings into special sections
    
    https://github.com/llvm/llvm-project/blob/d5e7c27d53887e6ae490d8e26193a54987728458/clang/lib/CodeGen/CGObjCMac.cpp#L4056-L4072
    
    As far as I can tell from `CGObjCMac.cpp`:
    * types go into `__objc_methtype`
    * class, category, and protocol names go into `__objc_classname`
    * method names, property names, and property types go into
    `__objc_methname`
    
    See also https://github.com/swiftlang/swift/pull/84236.

--- lib/IRGen/GenConstant.cpp
+++ lib/IRGen/GenConstant.cpp
@@ -79,7 +79,9 @@ llvm::Constant *irgen::emitAddrOfConstantString(IRGenModule &IGM,
   case StringLiteralInst::Encoding::Bytes:
   case StringLiteralInst::Encoding::UTF8:
   case StringLiteralInst::Encoding::UTF8_OSLOG:
-    return IGM.getAddrOfGlobalString(SLI->getValue(), false, useOSLogEncoding);
+    return IGM.getAddrOfGlobalString(
+        SLI->getValue(), useOSLogEncoding ? CStringSectionType::OSLogString
+                                          : CStringSectionType::Default);
 
   case StringLiteralInst::Encoding::ObjCSelector:
     llvm_unreachable("cannot get the address of an Objective-C selector");
