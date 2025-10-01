3f805366d01ac8c092cc74d06506a93faf844a0a
Max Desiatov <m_desiatov@apple.com>
2025-10-01 09:18:48 +0100

    Add missing `// REQUIRES: swift_feature_ForExpressions`

--- test/stmt/for-expr.swift
+++ test/stmt/for-expr.swift
@@ -1,5 +1,7 @@
 // RUN: %target-run-simple-swift(-enable-experimental-feature ForExpressions) | %FileCheck %s
 
+// REQUIRES: swift_feature_ForExpressions
+
 func f() -> String {
   for (i, x) in "hello".enumerated() {
     if i % 2 == 0 {
