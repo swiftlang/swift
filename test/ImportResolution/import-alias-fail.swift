a17ad02f77cfaadcfaa0b44ce42d49746bd5d037
Henrik G. Olsson <h_olsson@apple.com>
2025-10-02 11:32:42 -0700

    fix non-fatal import error

--- test/ImportResolution/import-alias-fail.swift
+++ test/ImportResolution/import-alias-fail.swift
@@ -1,4 +1,4 @@
-// RUN: %target-typecheck-verify-swift -module-alias Foo=Bar -verify-ignore-unknown
+// RUN: %target-typecheck-verify-swift -module-alias Foo=Bar
 
 import Foo // expected-error{{no such module 'Bar'}}
 // expected-note@-1{{module name 'Foo' is aliased to 'Bar'}}
