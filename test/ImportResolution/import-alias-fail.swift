b01a7821ac22a43fc055fcadf0923a91b79f76ce
Henrik G. Olsson <h_olsson@apple.com>
2025-10-01 23:55:05 -0700

    remove unnecessary c++ interop

--- test/ImportResolution/import-alias-fail.swift
+++ test/ImportResolution/import-alias-fail.swift
@@ -1,4 +1,4 @@
-// RUN: %target-typecheck-verify-swift -cxx-interoperability-mode=default -module-alias Foo=Bar -verify-ignore-unknown
+// RUN: %target-typecheck-verify-swift -module-alias Foo=Bar -verify-ignore-unknown
 
 import Foo // expected-error{{no such module 'Bar'}}
 // expected-note@-1{{module name 'Foo' is aliased to 'Bar'}}
