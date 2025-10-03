2136b2219e24eb2ad16f32825f43c7f9c65796e6
Mike Ash <mikeash@apple.com>
2025-10-03 14:39:24 -0400

    Disable clientRR in JIT mode.

--- lib/IRGen/SwiftTargetInfo.cpp
+++ lib/IRGen/SwiftTargetInfo.cpp
@@ -71,10 +71,14 @@ static void configureARM64(IRGenModule &IGM, const llvm::Triple &triple,
   // half for the kernel.
   target.SwiftRetainIgnoresNegativeValues = true;
 
-  // ARM64 has swiftClientRetainRelease, except for Android and Embedded.
+  // ARM64 has swiftClientRetainRelease, except for:
+  // * Android
+  // * Embedded
+  // * JIT
   if (!triple.isAndroid() &&
       !IGM.getSwiftModule()->getASTContext().LangOpts.hasFeature(
-          Feature::Embedded))
+          Feature::Embedded) &&
+      !IGM.getOptions().UseJIT)
     target.HasSwiftClientRRLibrary = true;
 
   target.UsableSwiftAsyncContextAddrIntrinsic = true;
