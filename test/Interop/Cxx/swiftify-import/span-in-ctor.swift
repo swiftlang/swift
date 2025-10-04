6a9ac22cd280b426f6c1e1454f2e3b5c2769086f
Henrik G. Olsson <hnrklssn@gmail.com>
2025-10-04 00:40:00 -0700

    Merge pull request #84507 from hnrklssn/swiftify-import-as-method
    [Swiftify] Add support for free functions imported as instance methods
    
    (cherry picked from commit ed4f058c01734dcc596d8589825e4d365803cd13)

--- test/Interop/Cxx/swiftify-import/span-in-ctor.swift
+++ test/Interop/Cxx/swiftify-import/span-in-ctor.swift
@@ -1,8 +1,5 @@
 // REQUIRES: swift_feature_SafeInteropWrappers
 
-// FIXME swift-ci linux tests do not support std::span
-// UNSUPPORTED: OS=linux-gnu, OS=linux-android, OS=linux-androideabi
-
 // RUN: rm -rf %t
 // RUN: split-file %s %t
 // RUN: %target-swift-frontend -c -plugin-path %swift-plugin-dir -I %t/Inputs -Xcc -std=c++20 -cxx-interoperability-mode=default -enable-experimental-feature SafeInteropWrappers %t/method.swift -dump-macro-expansions -verify 2>&1 | %FileCheck %s
