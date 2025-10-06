dd4221d2bd322c90f6232c68542dbe8a282f5e7b
Alexis Laferrière <alaf@apple.com>
2025-10-06 10:28:31 -0700

    Embedded: Allow implementation-only-import-build to build wider

--- test/embedded/implementation-only-import-build.swift
+++ test/embedded/implementation-only-import-build.swift
@@ -1,15 +1,12 @@
 // RUN: %empty-directory(%t)
 // RUN: %target-swift-frontend -emit-module -o %t/indirects.swiftmodule \
 // RUN:   %S/../Sema/Inputs/implementation-only-imports/indirects.swift \
-// RUN:   -swift-version 5 -target arm64-apple-none-macho \
 // RUN:   -enable-experimental-feature Embedded
 // RUN: %target-swift-frontend -emit-module -o %t/directs.swiftmodule -I %t\
 // RUN:    %S/../Sema/Inputs/implementation-only-imports/directs.swift \
-// RUN:   -swift-version 5 -target arm64-apple-none-macho \
 // RUN:   -enable-experimental-feature Embedded
 
 // RUN: %target-swift-frontend -emit-sil %s -I %t \
-// RUN:   -swift-version 5 -target arm64-apple-none-macho \
 // RUN:   -enable-experimental-feature Embedded
 
 // REQUIRES: swift_feature_Embedded
