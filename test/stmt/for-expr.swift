2388e0bde356c499ce8cd5467bcaf8dfaf171eb0
Max Desiatov <m_desiatov@apple.com>
2025-10-07 23:07:08 +0100

    Add `REQUIRES: executable_test` to `for-expr.swift`
    The lack of this condition made some jobs fail, e.g. https://ci.swift.org/job/oss-swift_tools-RA_stdlib-DA_test-device-non_executable/9749
    
    rdar://162140584

--- test/stmt/for-expr.swift
+++ test/stmt/for-expr.swift
@@ -1,6 +1,7 @@
 // RUN: %target-run-simple-swift(-enable-experimental-feature ForExpressions) | %FileCheck %s
 
 // REQUIRES: swift_feature_ForExpressions
+// REQUIRES: executable_test
 
 func f() -> String {
   for (i, x) in "hello".enumerated() {
