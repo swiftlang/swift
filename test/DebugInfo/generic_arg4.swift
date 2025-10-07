b8f2d33deda58b79628f9b95af8a7446aa76592f
Adrian Prantl <aprantl@apple.com>
2025-10-06 17:54:57 -0700

    [DebugInfo] Use the human-readable name as name of FwdDecls of unused structs
    rdar://162080220

--- test/DebugInfo/generic_arg4.swift
+++ test/DebugInfo/generic_arg4.swift
@@ -9,7 +9,7 @@ public struct Q<T> {
 // CHECK-SAME:       ![[ARG:.*]], !DIExpression()
 // CHECK: store ptr %0, ptr %[[ALLOCA]], align
 // No deref here: the array argument is passed by value.
-// CHECK: ![[DITY:.*]] = !DICompositeType({{.*}}name: "$sSay12generic_arg41QVyxGGD"
+// CHECK: ![[DITY:.*]] = !DICompositeType({{.*}}identifier: "$sSay12generic_arg41QVyxGGD"
 public func foo<T>(_ arg: [Q<T>]) {
 // CHECK: ![[ARG]] = !DILocalVariable(name: "arg", arg: 1,
 // CHECK-SAME:                        line: [[@LINE-2]], type: ![[DITY:.*]])
