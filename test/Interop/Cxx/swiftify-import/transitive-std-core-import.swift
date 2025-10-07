a6c0778c7fbf037159b2f03c4aa6f8614e224b35
Henrik G. Olsson <h_olsson@apple.com>
2025-10-06 18:02:26 -0700

    only run transitive-std-core-import.swift on new libc++

--- test/Interop/Cxx/swiftify-import/transitive-std-core-import.swift
+++ test/Interop/Cxx/swiftify-import/transitive-std-core-import.swift
@@ -1,8 +1,14 @@
 // REQUIRES: swift_feature_SafeInteropWrappers
+// REQUIRES: OS=macosx
 
+// Don't run this test with libc++ versions 17-19, when the top-level std module was split into multiple top-level modules.
 // RUN: %empty-directory(%t)
-// RUN: split-file %s %t
-// RUN: %target-swift-frontend -emit-module -plugin-path %swift-plugin-dir -o %t/Test.swiftmodule -I %t/Inputs -enable-experimental-feature SafeInteropWrappers -strict-memory-safety -warnings-as-errors -Xcc -Werror %t/test.swift  -cxx-interoperability-mode=default -Xcc -std=c++20
+// RUN: %target-clangxx %S/../stdlib/Inputs/check-libcxx-version.cpp -o %t/check-libcxx-version
+// RUN: %target-codesign %t/check-libcxx-version
+
+// RUN: %target-run %t/check-libcxx-version || %empty-directory(%t)
+// RUN: %target-run %t/check-libcxx-version || split-file %s %t
+// RUN: %target-run %t/check-libcxx-version || %target-swift-frontend -emit-module -plugin-path %swift-plugin-dir -o %t/Test.swiftmodule -I %t/Inputs -enable-experimental-feature SafeInteropWrappers -strict-memory-safety -warnings-as-errors -Xcc -Werror %t/test.swift  -cxx-interoperability-mode=default -Xcc -std=c++20
 
 // Test that implicit imports can refer to std_core etc., even though Swift source code may not.
 
