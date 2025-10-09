973913f8221391bfa7ff985519d7a2916a91234e
Mads Odgaard <mads.odgaard@frameo.com>
2025-10-09 17:19:57 +0200

    use parse-stdlib for test and disable unversioned reflection test

--- test/IRGen/availability_android.swift
+++ test/IRGen/availability_android.swift
@@ -1,13 +1,9 @@
-// RUN: %target-swift-frontend -target aarch64-unknown-linux-android24 -primary-file %s -emit-ir | %FileCheck %s
-// RUN: %target-swift-frontend -target aarch64-unknown-linux-android24 -primary-file %s -O -emit-ir | %FileCheck %s --check-prefix=OPT
-
-// REQUIRES: OS=linux-android || OS=linux-androideabi
+// RUN: %target-swift-frontend -target aarch64-unknown-linux-android24 -primary-file %s -emit-ir -parse-stdlib | %FileCheck %s
 
 // CHECK-LABEL: define{{.*}}$s20availability_android0A5CheckyyF
 // CHECK: call swiftcc i1 @"$ss26_stdlib_isOSVersionAtLeastyBi1_Bw_BwBwtF"(
 
-// OPT-LABEL: define{{.*}}$s20availability_android0A5CheckyyF
-// OPT: call {{.*}} @"$ss26_stdlib_isOSVersionAtLeastyBi1_Bw_BwBwtF"(
+import Swift
 
 public func availabilityCheck() {
   if #available(Android 28, *) {
