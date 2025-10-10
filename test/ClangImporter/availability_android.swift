cf03af940ea3dd08b0b781566cf52b14404c611e
Mads Odgaard <mads.odgaard@frameo.com>
2025-10-10 13:55:21 +0200

    add bionic clang importer test

--- test/ClangImporter/availability_android.swift
+++ test/ClangImporter/availability_android.swift
@@ -3,9 +3,24 @@
 // REQUIRES: OS=linux-android || OS=linux-androideabi
 
 import AndroidVersioning
+import Android
 
 FunctionIntroducedIn24()
 
 FunctionIntroducedIn28()
 // expected-error@-1 {{'FunctionIntroducedIn28()' is only available in Android 28 or newer}}
 // expected-note@-2 {{add 'if #available' version check}}
+
+func test_ifaddrs_introduced_in_24() {
+    var ifaddr_ptr: UnsafeMutablePointer<ifaddrs>? = nil
+    if getifaddrs(&ifaddr_ptr) == 0 {
+        freeifaddrs(ifaddr_ptr)
+    }
+}
+
+func test_getentropy_introduced_in_28() {
+    var buffer: [UInt8] = .init(repeating: 0, count: 16)
+    getentropy(&buffer, buffer.count)
+    // expected-error@-1 {{'getentropy' is only available in Android 28 or newer}}
+    // expected-note@-2 {{add 'if #available' version check}}
+}
