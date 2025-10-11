7d8314211dcb0c96b8c5ff540b340ba7bcf5a3b4
Mads Odgaard <mads@madsodgaard.com>
2025-10-11 15:31:17 +0200

    fix IRGen test

--- test/IRGen/availability_android.swift
+++ test/IRGen/availability_android.swift
@@ -1,9 +1,14 @@
 // RUN: %target-swift-frontend -target aarch64-unknown-linux-android24 -primary-file %s -emit-ir -parse-stdlib | %FileCheck %s
 
-// CHECK-LABEL: define{{.*}}$s20availability_android0A5CheckyyF
+// CHECK-LABEL: define{{.*}}$s20availability_android0A5CheckSSyF
 // CHECK: call swiftcc i1 @"$ss26_stdlib_isOSVersionAtLeastyBi1_Bw_BwBwtF"(
 
-@inlinable
-public func availabilityCheck() {
-  if #available(Android 28, *) {}
+import Swift
+
+public func availabilityCheck() -> String {
+  if #available(Android 28, *) {
+    return "28"
+  } else {
+    return "not 28"
+  }
 }
