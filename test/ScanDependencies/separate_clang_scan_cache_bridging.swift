b22e4d2bc47ab8642aa8562b8b2e97f96d26f7e3
Henrik G. Olsson <h_olsson@apple.com>
2025-10-12 23:53:13 -0700

    Enable YAML compatibility for some tests (NFC)
    This makes sure TMP_DIR is sanitized correctly on Windows despite output
    using `\\` as path separator.

--- test/ScanDependencies/separate_clang_scan_cache_bridging.swift
+++ test/ScanDependencies/separate_clang_scan_cache_bridging.swift
@@ -1,8 +1,8 @@
 // RUN: %empty-directory(%t.module-cache)
 // RUN: %empty-directory(%t.scanner-cache)
 
-// RUN: %target-swift-frontend -scan-dependencies -module-cache-path %t.module-cache -clang-scanner-module-cache-path %t.scanner-cache %s -o %t.deps.json -I %S/Inputs/SwiftDifferent -import-objc-header %S/Inputs/CHeaders/Bridging.h -dump-clang-diagnostics 2>&1 | %FileCheck %s --check-prefix=CHECK-CLANG-COMMAND
-// RUN: %validate-json %t.deps.json | %FileCheck %s --check-prefix=CHECK-DEPS
+// RUN: %target-swift-frontend -scan-dependencies -module-cache-path %t.module-cache -clang-scanner-module-cache-path %t.scanner-cache %s -o %t.deps.json -I %S/Inputs/SwiftDifferent -import-objc-header %S/Inputs/CHeaders/Bridging.h -dump-clang-diagnostics 2>&1 | %FileCheck %s --check-prefix=CHECK-CLANG-COMMAND --enable-yaml-compatibility
+// RUN: %validate-json %t.deps.json | %FileCheck %s --check-prefix=CHECK-DEPS --enable-yaml-compatibility
 
 // Ensure we prefer clang scanner module cache path
 // CHECK-CLANG-COMMAND: '-fmodules-cache-path={{.*}}.scanner-cache'
