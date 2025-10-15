ad81d1a45ee5afbc6a91f4a410b8ee7f29e46fca
Konrad Malawski <ktoso@apple.com>
2025-10-15 22:46:59 +0900

    withContinuation APIs should not hop/re-enqueue un-necessarily

--- test/Concurrency/Runtime/must_not_hop_withContinuation.swift
+++ test/Concurrency/Runtime/must_not_hop_withContinuation.swift
@@ -10,33 +10,6 @@
 
 import Synchronization
 
-@available(SwiftStdlib 6.0, *)
-final class AssertExactEnqueueCountExecutor: SerialExecutor {
-  let expectedEnqueueCount: Int
-  let enqueueCount: Atomic<Int>
-
-  let name: String
-  
-  init(expectedEnqueueCount: Int, name: String) {
-    self.expectedEnqueueCount = expectedEnqueueCount
-    self.enqueueCount = .init(0)
-    self.name = name
-  }
-
-  public func enqueue(_ job: consuming ExecutorJob) {
-    let newEnqueueValue = self.enqueueCount.add(1, ordering: .relaxed).newValue
-    if newEnqueueValue > self.expectedEnqueueCount {
-      fatalError("Got unexpected enqueue (\(newEnqueueValue)), in: \(self.name)")
-    }
-    print("[executor][\(self.name)] Enqueue (\(newEnqueueValue))")
-    job.runSynchronously(on: self.asUnownedSerialExecutor())
-  }
-
-  public func asUnownedSerialExecutor() -> UnownedSerialExecutor {
-    UnownedSerialExecutor(ordinary: self)
-  }
-}
-
 
 if #available(SwiftStdlib 6.0, *) {
   print("=== foo() async")
@@ -47,7 +20,7 @@ if #available(SwiftStdlib 6.0, *) {
 // CHECK: === foo() async
 // CHECK-NEXT: ---------------------------------------
 // We hop to the task executor:
-// CHECK-NEXT: Executor(task-executor): enqueue (1)
+// CHECK-NEXT: [executor][task-executor] Enqueue (1)
 
 // CHECK-NEXT: foo - withTaskExecutorPreference
 
@@ -63,21 +36,15 @@ if #available(SwiftStdlib 6.0, *) {
 // CHECK: foo - withTaskExecutorPreference - withUnsafeThrowingContinuation
 // CHECK-NEXT: foo - withTaskExecutorPreference - withUnsafeThrowingContinuation done
 
-// CHECK: foo - withTaskExecutorPreference - TL.withValue
-// CHECK-NEXT: foo - withTaskExecutorPreference - TL.withValue done
-
-// CHECK: foo - withTaskExecutorPreference - TL.withValue throwing
-// CHECK-NEXT: foo - withTaskExecutorPreference - TL.withValue throwing done
-
 // By checking that this is the second enqueue here,
 // we check that there was no stray enqueues between with... invocations:
-// CHECK-NEXT: Executor(task-executor): enqueue (2)
+// CHECK-NEXT: [executor][task-executor] Enqueue (2)
 
 // CHECK-NEXT: foo - withTaskExecutorPreference done
 
 // CHECK-NEXT: == Make: actor Foo
 // CHECK-NEXT: ---------------------------------------
-// CHECK-NEXT: Executor(actor-executor): enqueue (1)
+// CHECK-NEXT: [executor][actor-executor] Enqueue (1)
 // CHECK-NEXT: actor.foo
 
 // CHECK: actor.foo - withCheckedContinuation
@@ -94,55 +61,36 @@ if #available(SwiftStdlib 6.0, *) {
 // CHECK-NEXT: actor.foo done
 
 // No more enqueues are expected afterwards
-// CHECK-NOT: Executor
-
-@TaskLocal
-let myTaskLocal: String = ""
-
-nonisolated(nonsending) func someFunc() async throws {
-  print("nonisolated(nonsending) someFunc() async")
-}
+// CHECK-NOT: [executor]
 
 @available(SwiftStdlib 6.0, *)
 @concurrent
 func foo() async {
-  await withTaskExecutorPreference(MyExec(name: "task-executor")) {
+  await withTaskExecutorPreference(AssertExactEnqueueCountExecutor(maxEnqueues: 2, name: "task-executor")) {
     print("foo - withTaskExecutorPreference")
     await withCheckedContinuation { cont in
       print("foo - withTaskExecutorPreference - withCheckedContinuation")
       cont.resume()
     }
-    print("foo - withTaskExecutorPreference - withCheckedContinuation done\n")
+    print("foo - withTaskExecutorPreference - withCheckedContinuation done")
 
     await withUnsafeContinuation { cont in
       print("foo - withTaskExecutorPreference - withUnsafeContinuation")
       cont.resume()
     }
-    print("foo - withTaskExecutorPreference - withUnsafeContinuation done\n")
+    print("foo - withTaskExecutorPreference - withUnsafeContinuation done")
 
     try! await withCheckedThrowingContinuation { cont in
       print("foo - withTaskExecutorPreference - withCheckedThrowingContinuation")
       cont.resume()
     }
-    print("foo - withTaskExecutorPreference - withCheckedThrowingContinuation done\n")
+    print("foo - withTaskExecutorPreference - withCheckedThrowingContinuation done")
 
     try! await withUnsafeThrowingContinuation { cont in
       print("foo - withTaskExecutorPreference - withUnsafeThrowingContinuation")
       cont.resume()
     }
-    print("foo - withTaskExecutorPreference - withUnsafeThrowingContinuation done\n")
-
-    await $myTaskLocal.withValue("value") {
-      print("foo - withTaskExecutorPreference - TL.withValue")
-      try? await someFunc()
-    }
-    print("foo - withTaskExecutorPreference - TL.withValue done\n")
-
-    try! await $myTaskLocal.withValue("value") {
-      print("foo - withTaskExecutorPreference - TL.withValue throwing")
-      try await someFunc()
-    }
-    print("foo - withTaskExecutorPreference - TL.withValue throwing done\n")
+    print("foo - withTaskExecutorPreference - withUnsafeThrowingContinuation done")
   }
   print("foo - withTaskExecutorPreference done")
 
@@ -153,12 +101,12 @@ func foo() async {
 
 @available(SwiftStdlib 6.0, *)
 actor Foo {
-  let exec = MyExec(name: "actor-executor")
-        
+  let exec = AssertExactEnqueueCountExecutor(maxEnqueues: 2, name: "actor-executor")
+
   nonisolated var unownedExecutor: UnownedSerialExecutor {
     self.exec.asUnownedSerialExecutor()
   }
-          
+
   func foo() async {
     print("actor.foo")
 
@@ -166,48 +114,54 @@ actor Foo {
       print("actor.foo - withCheckedContinuation")
       cont.resume()
     }
-    print("actor.foo - withCheckedContinuation done\n")
+    print("actor.foo - withCheckedContinuation done")
 
     await withUnsafeContinuation { cont in
       print("actor.foo - withUnsafeContinuation")
       cont.resume()
     }
-    print("actor.foo - withUnsafeContinuation done\n")
-    
+    print("actor.foo - withUnsafeContinuation done")
+
     try! await withCheckedThrowingContinuation { cont in
       print("actor.foo - withCheckedThrowingContinuation")
       cont.resume()
     }
-    print("actor.foo - withCheckedThrowingContinuation done\n")
-    
+    print("actor.foo - withCheckedThrowingContinuation done")
+
     try! await withUnsafeThrowingContinuation { cont in
       print("actor.foo - withUnsafeThrowingContinuation")
       cont.resume()
     }
-    print("actor.foo - withUnsafeThrowingContinuation done\n")
+    print("actor.foo - withUnsafeThrowingContinuation done")
 
     print("actor.foo done")
   }
 }
 
 @available(SwiftStdlib 6.0, *)
-final class MyExec: TaskExecutor, SerialExecutor {
-  let name: String
-  let enqueueCount: Atomic<Int> = .init(0)
+final class AssertExactEnqueueCountExecutor: TaskExecutor, SerialExecutor {
+  let maxEnqueues: Int
+  let enqueueCount: Atomic<Int>
 
-  init(name: String) {
+  let name: String
+
+  init(maxEnqueues: Int, name: String) {
+    self.maxEnqueues = maxEnqueues
+    self.enqueueCount = .init(0)
     self.name = name
   }
 
-  func enqueue(_ job: UnownedJob) {
-    let count = self.enqueueCount.add(1, ordering: .relaxed)
-    print("Executor(\(name)): enqueue (\(count.newValue))")
+  public func enqueue(_ job: consuming ExecutorJob) {
+    let newEnqueueValue = self.enqueueCount.add(1, ordering: .relaxed).newValue
+    if newEnqueueValue > self.maxEnqueues {
+      fatalError("Got unexpected enqueue (\(newEnqueueValue)), in: \(self.name)")
+    }
+    print("[executor][\(self.name)] Enqueue (\(newEnqueueValue))")
+    job.runSynchronously(on: self.asUnownedSerialExecutor())
+  }
 
-    job
-    .runSynchronously(
-      isolatedTo: self.asUnownedSerialExecutor(),
-      taskExecutor: self.asUnownedTaskExecutor()
-    )
+  public func asUnownedSerialExecutor() -> UnownedSerialExecutor {
+    UnownedSerialExecutor(ordinary: self)
   }
 }
 
