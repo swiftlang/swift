0770adf8345d1ca1026066f556a44ce655cb1b7d
Mads Odgaard <mads.odgaard@frameo.com>
2025-10-15 21:50:58 +0200

    Add test to make sure runtime check is omitted for target version

--- test/IRGen/availability_android.swift
+++ test/IRGen/availability_android.swift
@@ -1,7 +1,11 @@
-// RUN: %target-swift-frontend -target aarch64-unknown-linux-android24 -primary-file %s -emit-ir | %FileCheck %s
+// RUN: %target-swift-frontend -target aarch64-unknown-linux-android24 -primary-file %s -emit-ir | %FileCheck %s --check-prefix=CHECK-24
+// RUN: %target-swift-frontend -target aarch64-unknown-linux-android28 -primary-file %s -emit-ir | %FileCheck %s --check-prefix=CHECK-28
 
-// CHECK-LABEL: define{{.*}}$s20availability_android0A5CheckyyF
-// CHECK: call swiftcc i1 @"$ss26_stdlib_isOSVersionAtLeastyBi1_Bw_BwBwtF"(
+// CHECK-24-LABEL: define{{.*}}$s20availability_android0A5CheckyyF
+// CHECK-24: call swiftcc i1 @"$ss26_stdlib_isOSVersionAtLeastyBi1_Bw_BwBwtF"(
+
+// CHECK-28-LABEL: define{{.*}}$s20availability_android0A5CheckyyF
+// CHECK-28-NOT: call swiftcc i1 @"$ss26_stdlib_isOSVersionAtLeastyBi1_Bw_BwBwtF"(
 
 // REQUIRES: OS=linux-android || OS=linux-androideabi
 
