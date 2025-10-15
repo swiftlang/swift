6377edc7917d51e5c2afc9e8d5a3f8b69568e793
Hamish Knight <hamish_knight@apple.com>
2025-10-15 23:22:03 +0100

    [CS] Re-order subscript constraint generation
    Add the application constraint before the member constraint, which
    allows us to get rid of the special-cased delaying logic for dynamic
    member subscripts. The diagnostic change here is due to the fact that
    we no longer have a simplified type for the result in CSGen, which
    would also be the case if we had a disjunction for the member.

--- lib/Sema/CSSolver.cpp
+++ lib/Sema/CSSolver.cpp
@@ -1798,12 +1798,6 @@ ConstraintSystem::filterDisjunction(
     // constraint, so instead let's keep the disjunction, but disable all
     // unviable choices.
     if (choice->getOverloadChoice().isKeyPathDynamicMemberLookup()) {
-      // Early simplification of the "keypath dynamic member lookup" choice
-      // is impossible because it requires constraints associated with
-      // subscript index expression to be present.
-      if (Phase == ConstraintSystemPhase::ConstraintGeneration)
-        return SolutionKind::Unsolved;
-
       for (auto *currentChoice : disjunction->getNestedConstraints()) {
         if (currentChoice->isDisabled())
           continue;
