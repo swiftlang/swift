38f91e2dbd77927f4f176dae7160d3fc2c909ab9
Alastair Houghton <ahoughton@apple.com>
2025-10-15 13:25:22 +0100

    [Backtracing] Fix Linux and macOS builds.
    There was a case-sensitivity problem, and a missing source file.
    
    rdar://101623384

---
+++ stdlib/public/libexec/swift-backtrace/TargetWindows.swift
@@ -0,0 +1,18 @@
+//===--- TargetLinux.swift - Represents a process we are inspecting -------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2022-2025 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+//
+//  Defines `Target`, which represents the process we are inspecting.
+//  This is the Windows version.
+//
+//===----------------------------------------------------------------------===//
+
+// ###TODO
