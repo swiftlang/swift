62371b908b6aa95d1c4c183ece57ed9c566dadee
Allan Shortlidge <shortlidge@apple.com>
2025-10-16 15:49:10 -0700

    Tests: Upstream missing contents of ClangImporter/availability_ios.swift.

--- test/ClangImporter/availability_ios.swift
+++ test/ClangImporter/availability_ios.swift
@@ -3,6 +3,7 @@
 // RUN: %target-swift-frontend(mock-sdk: %clang-importer-sdk) -typecheck -verify -verify-ignore-unrelated -I %S/Inputs/custom-modules -application-extension-library %s
 
 // REQUIRES: OS=ios
+// UNSUPPORTED: OS=maccatalyst
 
 import Foundation
 import AvailabilityExtras
@@ -13,4 +14,26 @@ func test_unavailable_because_deprecated() {
 
 func test_swift_unavailable_wins() {
   unavailableWithOS() // expected-error {{'unavailableWithOS()' is unavailable in Swift}}
+}
+
+
+@available(iOS, introduced: 1.0)
+@available(macCatalyst, introduced: 1.0, obsoleted: 2.0)
+func obsoletedOnMacCatalystButNotIOS() { }
+
+obsoletedOnMacCatalystButNotIOS() // no-error
+
+@available(iOS, introduced: 1.0)
+@available(macCatalyst, introduced: 1.0, deprecated: 2.0)
+func deprecatedOnMacCatalystButNotIOS() { }
+
+@available(iOS, introduced: 8.0)
+func maccatalyst_tests() {
+  deprecatedOnMacCatalystButNotIOS() // no-warning
+
+  availableOnIOSButUnavailableOniOSAppExtension() // no-error
+  availableOnIOSAppExtensionButUnavailableOnmacCatalystAppExtension() // no-error
+
+  availableOnIOSButDeprecatedOniOSAppExtension() // no-warning
+  availableOnIOSAppExtensionButDeprecatedOnmacCatalystAppExtension() // no-warning
 }
