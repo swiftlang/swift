484cf6850858c8068f6fa00f9db09cf3300ff1c2
Henrik G. Olsson <h_olsson@apple.com>
2025-10-20 10:46:29 -0700

    disable use-std-span.swift for macOS

--- test/Interop/Cxx/stdlib/use-std-span.swift
+++ test/Interop/Cxx/stdlib/use-std-span.swift
@@ -4,6 +4,9 @@
 // TODO: test failed in Windows PR testing: rdar://144384453
 // UNSUPPORTED: OS=windows-msvc
 
+// TODO: test failed in macOS PR testing but passes locally: rdar://163049442
+// UNSUPPORTED: OS=macosx
+
 // REQUIRES: executable_test
 // REQUIRES: std_span
 
