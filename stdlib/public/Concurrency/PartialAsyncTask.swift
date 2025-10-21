13c453b9c0683032f8ed0fcc62db48330b6a0f7b
Konrad Malawski <ktoso@apple.com>
2025-10-21 16:46:15 +0900

    [Concurrency] Prevent task re-enqueues in continuation APIs
    This explicitly adopts nonisolated nonsending funcs for the with
    continuation APIs. This guarantees there are not spurious enqueues
    between the caller and the closure executing, which is a requirement for
    these methods.
    
    This takes a risk on removing the bad #isolation based API from source,
    however explicitly passing the isolation as something else than the
    actual isolation would always have been incorrect, so we should try to
    get this through.
    
    There are further re-enqueue fixes to be done, but this specific API
    should adopt the nonisolated nonsending approach.
    
    Resolves rdar://162192512

--- stdlib/public/Concurrency/PartialAsyncTask.swift
+++ stdlib/public/Concurrency/PartialAsyncTask.swift
@@ -889,8 +889,7 @@ internal func _resumeUnsafeThrowingContinuationWithError<T>(
 @available(SwiftStdlib 5.1, *)
 @_alwaysEmitIntoClient
 @unsafe
-public func withUnsafeContinuation<T>(
-  isolation: isolated (any Actor)? = #isolation,
+public nonisolated(nonsending) func withUnsafeContinuation<T>(
   _ fn: (UnsafeContinuation<T, Never>) -> Void
 ) async -> sending T {
   return await Builtin.withUnsafeContinuation {
@@ -926,8 +925,7 @@ public func withUnsafeContinuation<T>(
 @available(SwiftStdlib 5.1, *)
 @_alwaysEmitIntoClient
 @unsafe
-public func withUnsafeThrowingContinuation<T>(
-  isolation: isolated (any Actor)? = #isolation,
+public nonisolated(nonsending) func withUnsafeThrowingContinuation<T>(
   _ fn: (UnsafeContinuation<T, Error>) -> Void
 ) async throws -> sending T {
   return try await Builtin.withUnsafeThrowingContinuation {
