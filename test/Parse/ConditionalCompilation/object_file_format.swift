4923db8761b5528d6130888d421428bba2319784
Kuba Mracek <mracek@apple.com>
2025-10-22 10:38:52 -0700

    Adjust PlatformConditionKinds.def + target #objectFormat test more precisely

--- test/Parse/ConditionalCompilation/object_file_format.swift
+++ test/Parse/ConditionalCompilation/object_file_format.swift
@@ -1,11 +1,48 @@
-// RUN: %swift -typecheck %s -verify -target arm64-apple-none-macho -parse-stdlib
-// RUN: %swift -typecheck %s -verify -target arm64-apple-none-elf -parse-stdlib
-// RUN: %swift -typecheck %s -verify -target wasm32-unknown-wasi -parse-stdlib
-// RUN: %swift -typecheck %s -verify -target x86_64-unknown-windows-msvc -parse-stdlib
-// RUN: %swift-ide-test -test-input-complete -source-filename=%s -target arm64-apple-macos
+// RUN: %swift -typecheck %s -target arm64-apple-none-macho -parse-stdlib 2>&1 | %FileCheck -check-prefix CHECK-MACHO %s
+// RUN: %swift -typecheck %s -target arm64-apple-none-elf -parse-stdlib 2>&1 | %FileCheck -check-prefix CHECK-ELF %s
+// RUN: %swift -typecheck %s -target wasm32-unknown-wasi -parse-stdlib 2>&1 | %FileCheck -check-prefix CHECK-WASM %s
+// RUN: %swift -typecheck %s -target x86_64-unknown-windows-msvc -parse-stdlib 2>&1 | %FileCheck -check-prefix CHECK-COFF %s
 
-#if objectFormat(MachO) || objectFormat(ELF) || objectFormat(Wasm) || objectFormat(COFF)
-class C {}
-var x = C()
+#if objectFormat(MachO)
+#warning("I'm MachO")
+#else
+#warning("I'm not MachO")
 #endif
-var y = x
+
+#if objectFormat(ELF)
+#warning("I'm ELF")
+#else
+#warning("I'm not ELF")
+#endif
+
+#if objectFormat(Wasm)
+#warning("I'm Wasm")
+#else
+#warning("I'm not Wasm")
+#endif
+
+#if objectFormat(COFF)
+#warning("I'm COFF")
+#else
+#warning("I'm not COFF")
+#endif
+
+// CHECK-MACHO: I'm MachO
+// CHECK-MACHO: I'm not ELF
+// CHECK-MACHO: I'm not Wasm
+// CHECK-MACHO: I'm not COFF
+
+// CHECK-ELF: I'm not MachO
+// CHECK-ELF: I'm ELF
+// CHECK-ELF: I'm not Wasm
+// CHECK-ELF: I'm not COFF
+
+// CHECK-WASM: I'm not MachO
+// CHECK-WASM: I'm not ELF
+// CHECK-WASM: I'm Wasm
+// CHECK-WASM: I'm not COFF
+
+// CHECK-COFF: I'm not MachO
+// CHECK-COFF: I'm not ELF
+// CHECK-COFF: I'm not Wasm
+// CHECK-COFF: I'm COFF
