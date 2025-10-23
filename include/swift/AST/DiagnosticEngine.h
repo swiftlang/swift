83eea39b30898a88955f2e06b3c6533875c3cef2
Artem Chikin <achikin@apple.com>
2025-10-23 14:43:09 -0700

    [Diagnostics] Replace diagnostics' 'DefaultIgnore' option with a corresponding option on diagnostic groups
    This brings this control in line with other diagnostic controls we have which operate on a per-group level.
    'DefaultIgnoreWarnings' diagnostic group option applies to all warnings belonging to a certain diagnostic group.
    
    The inheritance rules are:
    - Marking a diagnostic group as 'DefaultIgnoreWarnings' means warnings belonging to this group will not be emitted by-default
      - Warnings belonging to sub-groups of this group will also not be emitted by-default
    - Enabling a 'DefaultIgnoreWarnings' group (with '-Werror','-Wwarning', etc.) means warnings belonging to this group will be emitted.
      - Warnings belonging to sub-groups of this group will also be emitted.
      - Warnings belonging to super-groups of this group will not be affected.

--- include/swift/AST/DiagnosticEngine.h
+++ include/swift/AST/DiagnosticEngine.h
@@ -57,7 +57,7 @@ namespace swift {
   /// this enumeration type that uniquely identifies it.
   enum class DiagID : uint32_t;
 
-  enum class DiagGroupID : uint16_t;
+  enum class DiagGroupID : uint32_t;
 
   /// Describes a diagnostic along with its argument types.
   ///
@@ -633,6 +633,9 @@ namespace swift {
     /// escalated to errors.
     llvm::BitVector warningsAsErrors;
 
+    /// Track which diagnostic group (`DiagGroupID`) warnings should be ignored.
+    llvm::BitVector ignoredDiagnosticGroups;
+
     /// Whether a fatal error has occurred
     bool fatalErrorOccurred = false;
 
@@ -641,9 +644,6 @@ namespace swift {
 
     /// Track the previous emitted Behavior, useful for notes
     DiagnosticBehavior previousBehavior = DiagnosticBehavior::Unspecified;
-
-    /// Track which diagnostics should be ignored.
-    llvm::BitVector ignoredDiagnostics;
 
     friend class DiagnosticStateRAII;
 
@@ -708,17 +708,23 @@ namespace swift {
       fatalErrorOccurred = false;
     }
 
-    /// Set whether a diagnostic should be ignored.
-    void setIgnoredDiagnostic(DiagID id, bool ignored) {
-      ignoredDiagnostics[(unsigned)id] = ignored;
+    /// Set whether a diagnostic group should be ignored.
+    void setIgnoredDiagnosticGroup(DiagGroupID id, bool ignored) {
+      ignoredDiagnosticGroups[(unsigned)id] = ignored;
     }
 
-    bool isIgnoredDiagnostic(DiagID id) const {
-      return ignoredDiagnostics[(unsigned)id];
+    /// Query whether a specific diagnostic group is ignored.
+    bool isIgnoredDiagnosticGroup(DiagGroupID id) const {
+      return ignoredDiagnosticGroups[(unsigned)id];
     }
+    
+    /// Query whether a specific diagnostic group and *all*
+    /// of its subgroups are ignored.
+    bool isIgnoredDiagnosticGroupTree(DiagGroupID id) const;
 
     void swap(DiagnosticState &other) {
-      std::swap(showDiagnosticsAfterFatalError, other.showDiagnosticsAfterFatalError);
+      std::swap(showDiagnosticsAfterFatalError,
+                other.showDiagnosticsAfterFatalError);
       std::swap(suppressWarnings, other.suppressWarnings);
       std::swap(suppressNotes, other.suppressNotes);
       std::swap(suppressRemarks, other.suppressRemarks);
@@ -726,7 +732,7 @@ namespace swift {
       std::swap(fatalErrorOccurred, other.fatalErrorOccurred);
       std::swap(anyErrorOccurred, other.anyErrorOccurred);
       std::swap(previousBehavior, other.previousBehavior);
-      std::swap(ignoredDiagnostics, other.ignoredDiagnostics);
+      std::swap(ignoredDiagnosticGroups, other.ignoredDiagnosticGroups);
     }
 
   private:
@@ -966,12 +972,12 @@ namespace swift {
       localization = diag::LocalizationProducer::producerFor(locale, path);
     }
 
-    void ignoreDiagnostic(DiagID id) {
-      state.setIgnoredDiagnostic(id, true);
+    bool isIgnoredDiagnosticGroup(DiagGroupID id) const {
+      return state.isIgnoredDiagnosticGroup(id);
     }
-
-    bool isIgnoredDiagnostic(DiagID id) const {
-      return state.isIgnoredDiagnostic(id);
+    
+    bool isIgnoredDiagnosticGroupTree(DiagGroupID id) const {
+      return state.isIgnoredDiagnosticGroupTree(id);
     }
 
     void resetHadAnyError() {
