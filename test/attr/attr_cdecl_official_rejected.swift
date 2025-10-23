467bc11ab0286033bc8a1697bff7c6019cc23f56
Alexis Laferrière <alaf@apple.com>
2025-10-23 13:57:45 -0700

    SE-495: Make @c an official feature

--- test/attr/attr_cdecl_official_rejected.swift
+++ test/attr/attr_cdecl_official_rejected.swift
@@ -1,12 +1,9 @@
 // RUN: %target-typecheck-verify-swift
-// RUN: %target-swift-frontend -typecheck %s \
-// RUN:   -enable-experimental-feature CDecl
+// RUN: %target-swift-frontend -typecheck %s
 
-// REQUIRES: swift_feature_CDecl
-
-@c(cdecl_foo) func foo() { } // expected-error {{'@c' requires '-enable-experimental-feature CDecl'}}
+@c(cdecl_foo) func foo() { }
 
 var computed: Int {
-  @c(get_computed) get { return 0 } // expected-error {{'@c' requires '-enable-experimental-feature CDecl'}}
-  @c(set_computed) set { } // expected-error {{'@c' requires '-enable-experimental-feature CDecl'}}
+  @c(get_computed) get { return 0 }
+  @c(set_computed) set { }
 }
