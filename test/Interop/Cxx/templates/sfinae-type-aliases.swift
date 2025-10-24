348cbf37cf3fc21a8deab248ac75ef9a94b1e0d4
John Hui <j-hui@apple.com>
2025-10-24 16:26:15 -0700

    [NFC] Use -verify-additional-prefix in sfinae-type-aliases.swift

--- test/Interop/Cxx/templates/sfinae-type-aliases.swift
+++ test/Interop/Cxx/templates/sfinae-type-aliases.swift
@@ -11,33 +11,31 @@
 //
 // Check for presence of errors when we do try to use invalid types
 // RUN: %target-swift-frontend -typecheck -verify -suppress-notes \
+// RUN:   -verify-additional-prefix poke- \
 // RUN:   -cxx-interoperability-mode=default \
 // RUN:   -I %t%{fs-sep}Inputs \
-// RUN:   -verify-additional-file %t%{fs-sep}Inputs%{fs-sep}CxxErrHeader.h \
+// RUN:   -verify-additional-file %t%{fs-sep}Inputs%{fs-sep}CxxHeader.h \
 // RUN:   %t%{fs-sep}poke.swift
 //
-// Check that module interfaces look reasonable and identical
-// RUN: %target-swift-ide-test -print-module -module-to-print=CxxModule    -I %t/Inputs -source-filename=x -cxx-interoperability-mode=default | %FileCheck %t/Inputs/CxxHeader.h
-// RUN: %target-swift-ide-test -print-module -module-to-print=CxxErrModule -I %t/Inputs -source-filename=x -cxx-interoperability-mode=default | %FileCheck %t/Inputs/CxxHeader.h
+// RUN: %target-swift-ide-test -print-module -source-filename=x \
+// RUN:   -module-to-print=CxxModule -I %t/Inputs \
+// RUN:   -cxx-interoperability-mode=default \
+// RUN: | %FileCheck %t/Inputs/CxxHeader.h
 
 //--- Inputs/module.modulemap
 module CxxModule {
     requires cplusplus
     header "CxxHeader.h"
 }
-module CxxErrModule {
-    requires cplusplus
-    header "CxxErrHeader.h"
-}
 
 //--- Inputs/CxxHeader.h
 #pragma once
 
-struct Empty {};
+struct Empty {}; // expected-poke-error@+1 {{no type named 'Missing' in 'Empty'}}
 template <typename T> struct MissingMember { typename T::Missing member; };
 using SUB = MissingMember<Empty>;
 
-struct Incomplete;
+struct Incomplete; // expected-poke-error@+1 {{field has incomplete type 'Incomplete'}}
 template <typename T> struct IncompleteField { T member; };
 using INC = IncompleteField<Incomplete>;
 
@@ -54,27 +52,6 @@ struct Aliaser {
 // CHECK-NEXT:   typealias TypedefBool = Bool
 // CHECK-NEXT:   typealias UsingBool = Bool
 // CHECK-NEXT: }
-
-//--- Inputs/CxxErrHeader.h
-// The exact same as CxxHeader.h, except this header contains expected-diagnostics comments
-#pragma once
-
-struct Empty {}; // expected-error@+1 {{no type named 'Missing' in 'Empty'}}
-template <typename T> struct MissingMember { typename T::Missing member; };
-using SUB = MissingMember<Empty>;
-
-struct Incomplete; // expected-error@+1 {{field has incomplete type 'Incomplete'}}
-template <typename T> struct IncompleteField { T member; };
-using INC = IncompleteField<Incomplete>;
-
-struct Aliaser {
-  typedef SUB  TypedefSUB;
-  typedef INC  TypedefINC;
-  typedef bool TypedefBool;
-  using UsingSUB  = SUB;
-  using UsingINC  = INC;
-  using UsingBool = bool;
-};
 
 //--- cxx.cpp
 // expected-no-diagnostics
@@ -94,6 +71,6 @@ func make() {
 }
 
 //--- poke.swift
-import CxxErrModule
-func make(_ _: Aliaser.TypedefSUB,  // expected-error {{'TypedefSUB' is not a member type of struct}}
-          _ _: Aliaser.UsingINC) {} // expected-error {{'UsingINC' is not a member type of struct}}
+import CxxModule
+func make(_ _: Aliaser.TypedefSUB,  // expected-poke-error {{'TypedefSUB' is not a member type of struct}}
+          _ _: Aliaser.UsingINC) {} // expected-poke-error {{'UsingINC' is not a member type of struct}}
