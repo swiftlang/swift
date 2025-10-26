3f812f792e8878a98a6a3a6474e341eca072fd33
Ben Cohen <ben_cohen@apple.com>
2025-10-26 13:40:45 -0700

    Allow Comparable: ~Escapable

--- test/stdlib/NoncopyableComparable.swift
+++ test/stdlib/NoncopyableComparable.swift
@@ -9,30 +9,44 @@
 // See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
 //
 //===----------------------------------------------------------------------===//
-// RUN: %target-run-simple-swift
+// RUN: %target-run-simple-swift(-enable-experimental-feature Lifetimes)
 // REQUIRES: executable_test
+// REQUIRES: swift_feature_Lifetimes
 
 import StdlibUnittest
 
 let NoncopyableComparableTests = TestSuite("NoncopyableComparable")
 
-struct Noncopyable<Wrapped: ~Copyable>: ~Copyable {
+struct Noncopyable<Wrapped: ~Copyable & ~Escapable>: ~Copyable, ~Escapable {
   var wrapped: Wrapped
 }
 
-extension Noncopyable: Equatable where Wrapped: Equatable & ~Copyable { }
+extension Noncopyable: Equatable where Wrapped: Equatable & ~Copyable & ~Escapable { }
 
-extension Noncopyable: Comparable where Wrapped: Comparable & ~Copyable {
+extension Noncopyable: Comparable where Wrapped: Comparable & ~Copyable & ~Escapable {
   static func < (lhs: borrowing Self, rhs: borrowing Self) -> Bool { lhs.wrapped < rhs.wrapped }
 }
 
-extension Comparable where Self: ~Copyable {
+extension Noncopyable: Escapable where Wrapped: Escapable & ~Copyable { }
+
+struct Nonescapable: ~Escapable {
+  let wrapped: Int
+}
+
+extension Nonescapable: Equatable { }
+
+extension Nonescapable: Comparable {
+  static func <(lhs: Self, rhs: Self) -> Bool { lhs.wrapped < rhs.wrapped }
+
+}
+
+extension Comparable where Self: ~Copyable & ~Escapable {
   func isLessThan(_ other: borrowing Self) -> Bool {
     self < other
   }
 }
 
-func isLessOrEqual<T: Comparable & ~Copyable>(_ lhs: borrowing T, _ rhs: borrowing T) -> Bool {
+func isLessOrEqual<T: Comparable & ~Copyable & ~Escapable>(_ lhs: borrowing T, _ rhs: borrowing T) -> Bool {
   lhs <= rhs
 }
 
@@ -80,6 +94,24 @@ NoncopyableComparableTests.test("comparing noncopyables") {
   expectTrue(array.inReverseOrder())
   array.swapAt(1, 2)
   expectFalse(array.inReverseOrder())
- }
+}
+
+NoncopyableComparableTests.test("comparing nonescapables") {
+  let a = Noncopyable<Nonescapable>(wrapped: .init(wrapped: 0))
+  let b = Noncopyable<Nonescapable>(wrapped: .init(wrapped: 1))
+  let c = Noncopyable<Nonescapable>(wrapped: .init(wrapped: 2))
+  
+  expectTrue(a < b)
+  expectTrue(c > a)
+  expectFalse(a < a)
+  expectFalse(a > c)
+  expectFalse(c <= a)
+  expectTrue(a <= a)
+  expectTrue(a < b && b < c)
+  
+  expectTrue(a.isLessThan(b))
+  expectTrue(isLessOrEqual(a,b))
+  expectFalse(b.isLessThan(a))
+}  
 
 runAllTests()
