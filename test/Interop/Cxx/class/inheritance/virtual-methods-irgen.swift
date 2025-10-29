34e47290f16d3c056d86c75e91df698669d2b72d
Doug Gregor <dgregor@apple.com>
2025-10-29 14:40:13 -0700

    [C++ interop] Fix manual mangling of C++ base method thunks
    When creating a C++ method that thunks from a particular C++ derived
    class to call a method on one of its base classes, we do some manual
    name mangling. This name mangling did not properly capture all of the
    aspects of the thunk, so it was prone to collisions.
    
    In moving to SIL asmname, we got a different set of collisions from
    the ones that existed previously. Expanding the mangling to account
    for small differences (const or not) as well as the base class type
    eliminates these collisions. Most of the test changes account for the
    new mangling, but the member-inheritance test indicates that this
    change fixes an existing miscompile as well.

--- test/Interop/Cxx/class/inheritance/virtual-methods-irgen.swift
+++ test/Interop/Cxx/class/inheritance/virtual-methods-irgen.swift
@@ -23,7 +23,7 @@ d4.f()
 // CHECK: call swiftcc {{.*}} @"$sSo8Derived4V1fs5Int32VyF"
 
 // CHECK: define {{.*}} @"$sSo8Derived4V1fs5Int32VyF"(ptr swiftself dereferenceable
-// CHECK: call {{.*}}  @{{_ZN8Derived423__synthesizedBaseCall_fEv|"\?__synthesizedBaseCall_f@Derived4@@QEAAHXZ"}}
+// CHECK: call {{.*}}  @{{_ZN8Derived434__synthesizedBaseCall_f__ZTS5Base3Ev|"\?__synthesizedBaseCall_ZTS5Base3Ev@@QEAAHXZ"}}
 
 // CHECK: define {{.*}}void @{{_ZN7DerivedIiE3fooEv|"\?foo@\?\$Derived@H@@UEAAXXZ"}}
 // CHECK:   call void @{{_Z21testFunctionCollectedv|"\?testFunctionCollected@@YAXXZ"}}
