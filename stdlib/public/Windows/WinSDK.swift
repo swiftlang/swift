2d5daed5890a0bde657ae517a94ba8ac634f29d7
Mohamed Hegazy <mhegazy@thebrowser.company>
2025-10-30 09:47:21 -0700

    Switft to __equals function

--- stdlib/public/Windows/WinSDK.swift
+++ stdlib/public/Windows/WinSDK.swift
@@ -335,18 +335,13 @@ extension GUID {
 
 // These conformances are marked @retroactive because the GUID type nominally
 // comes from the _GUIDDef clang module rather than the WinSDK clang module.
-
-// The C++ overlay already vends `==` when interop is enabled; avoid a redeclaration.
-#if canImport(Cxx)
-extension GUID: @retroactive Equatable {}
-#else
 extension GUID: @retroactive Equatable {
   @_transparent
-  public static func ==(lhs: Self, rhs: Self) -> Bool {
+  @_implements(Equatable, ==(_:_:))
+  public static func __equals(lhs: Self, rhs: Self) -> Bool {
     lhs.uint128Value == rhs.uint128Value
   }
 }
-#endif
 
 extension GUID: @retroactive Hashable {
   @_transparent
