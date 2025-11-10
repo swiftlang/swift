1e7a858d7bc247b6c55f1f2c86bdad93d4c268c6
Erik Eckstein <eeckstein@apple.com>
2025-11-10 18:43:11 +0100

    Optimizer: simplify `move_value`
    The `move_value` instruction is only used to specify flags.
    Remove a `move_value` which either doesn't specify any flags or which flags are not relevant outside the mandatory pipeline, like `[lexical]`.

--- test/SILOptimizer/lexical_lifetime_elim.swift
+++ test/SILOptimizer/lexical_lifetime_elim.swift
@@ -1,50 +1,22 @@
-// RUN: %target-swift-frontend -emit-sil -O -parse-as-library -enable-copy-propagation=false -Xllvm -sil-print-all -module-name=main %s 2>&1 | %FileCheck %s --check-prefixes CHECK,CHECK-NOCOPYPROP
-// RUN: %target-swift-frontend -emit-sil -O -parse-as-library -enable-lexical-lifetimes=false -Xllvm -sil-print-all -module-name=main %s 2>&1 | %FileCheck %s --check-prefixes CHECK,CHECK-COPYPROP
+// RUN: %target-swift-frontend -emit-sil -O -parse-as-library -enable-copy-propagation=false -module-name=main %s | %FileCheck %s
+// RUN: %target-swift-frontend -emit-sil -O -parse-as-library -enable-lexical-lifetimes=false -module-name=main %s | %FileCheck %s
 
 // REQUIRES: swift_in_compiler
 
+@_silgen_name("takeGuaranteed")
 @inline(never)
-func takeGuaranteed(_ a: AnyObject) -> AnyObject {
-  return a
-}
+func takeGuaranteed(_ a: AnyObject) -> AnyObject
 
 @_silgen_name("getOwned")
 @inline(never)
 func getOwned() -> AnyObject
 
-// CHECK-LABEL: // testLexical()
-// CHECK: [[A:%.*]] = apply %{{.*}}()
-// CHECK: [[B:%.*]] = move_value [lexical] [var_decl] [[A]]
-// CHECK: [[BB:%.*]] = begin_borrow [[B]]
-// CHECK: apply %{{.*}}([[BB]])
-// CHECK: end_borrow [[BB]]
-// CHECK: apply
-// CHECK: destroy_value [[B]]
-// CHECK-LABEL: } // end sil function
-
-// LexicalLifetimeEliminator must strip the [lexical] flag
-// before the first round of SemanticARCOpts.
-
-// CHECK-NOT: *** SIL function after {{.*}} (semantic-arc-opts)
-
-// CHECK-LABEL: *** SIL function after {{.*}} (sil-lexical-lifetime-eliminator)
-// CHECK-LABEL: // testLexical()
-// CHECK: [[A:%.*]] = apply %{{.*}}()
-// CHECK: [[B:%.*]] = move_value [var_decl] [[A]]
-// CHECK: apply %{{.*}}([[B]])
-// CHECK: apply
-// CHECK: destroy_value [[B]]
-// CHECK-LABEL: } // end sil function
-
-// The first round of SemanticARCOpts/CopyPropagation must eliminate the
-// redundant move_value that was only needed for a lexical lifetime.
-
-// CHECK-NOCOPYPROP-LABEL: *** SIL function after {{.*}} (semantic-arc-opts)
-// CHECK-COPYPROP-LABEL: *** SIL function after {{.*}} (copy-propagation)
-// CHECK-LABEL: // testLexical()
-// CHECK: [[A:%.*]] = apply %{{.*}}()
-// CHECK: apply %{{.*}}([[A]])
-// CHECK-LABEL: } // end sil function
+// CHECK-LABEL: sil @$s4main11testLexicalyXlyF :
+// CHECK:         [[A:%.*]] = apply
+// CHECK:         [[B:%.*]] = apply
+// CHECK-DAG:     strong_release [[B]]
+// CHECK-DAG:     strong_release [[A]]
+// CHECK-LABEL: } // end sil function '$s4main11testLexicalyXlyF'
 public func testLexical() -> AnyObject {
   let a = getOwned()
   // Without lexical lifetimes, the lifetime of 'a' ends in between the two calls:
