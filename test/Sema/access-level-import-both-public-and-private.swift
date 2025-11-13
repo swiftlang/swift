ba7b9b0d6b24010bbffe76e20fcae598aca7ee7e
Henrik G. Olsson <h_olsson@apple.com>
2025-11-12 16:07:50 -0800

    Revert "Sema: Prefer the submodule import for access-levels diagnostics"
    This reverts commit ca8792ca37fb3e8d8db55a6ce2032875ebb4d2db.
    
    Revert "[ImportResolution] Gracefully handle importing broken clang module"
    
    This reverts commit edb48dff8c6d345b6b66299b35b5887a8352850f.
    
    Revert "[ImportResolution] Don't deduplicate scoped imports"
    
    This reverts commit da96079eb0460f8f981f05d205dc2ec92e7e88b9.
    
    Revert "[ImportResolution] Deduplicate top-level clang modules from import list"
    
    This reverts commit 0ea5677aa193ae48db762dc47967acc1d566657f.
    
    Reverting due to namelookup errors.
    
    rdar://164588082

--- test/Sema/access-level-import-both-public-and-private.swift
+++ test/Sema/access-level-import-both-public-and-private.swift
@@ -6,12 +6,9 @@
 // RUN: %target-swift-frontend -verify -verify-additional-file %t%{fs-sep}bar.h -typecheck %t/test.swift -I %t
 
 //--- test.swift
-// expected-note@+1{{struct 'Baz' imported as 'private' from 'Bar' here}}
 private import Foo.Bar
 public import Foo
 
-// expected-error@+2{{function cannot be declared public because its parameter uses a private type}}
-// expected-note@+1{{struct 'Baz' is imported by this file as 'private' from 'Bar'}}
 public func test(x: Baz) {}
 
 //--- foo.h
@@ -20,7 +17,6 @@ public func test(x: Baz) {}
 
 //--- bar.h
 #pragma once
-// expected-note@+1{{type declared here}}
 struct Baz {};
 
 //--- module.modulemap
