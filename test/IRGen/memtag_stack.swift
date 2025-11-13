ad6e856c2697564f2619f125721dbeaa75a81686
Dan Blackwell <dan_blackwell@apple.com>
2025-11-13 12:12:25 +0000

    [Sanitizers] Add support for -sanitize=memtag-stack
    This sanitizer adds MTE (memory tagging extension) checks to stack variable accesses. Enablement requires setting an attribute on function definitions, and the instrumentation is added by LLVM.

---
+++ test/IRGen/memtag_stack.swift
@@ -0,0 +1,11 @@
+// RUN: %target-swift-frontend -emit-ir -sanitize=memtag-stack -parse-as-library -target arm64-apple-macosx10.9 %s | %FileCheck %s
+
+// Check that functions have the sanitize_memtag attribute when -sanitize=memtag-stack is enabled
+
+// CHECK-LABEL: define {{.*}} @"${{.*}}testFunction{{.*}}"
+// CHECK-SAME: [[ATTRS:#[0-9]+]]
+// CHECK: attributes [[ATTRS]] = {{.*}} sanitize_memtag
+
+func testFunction() -> Int {
+  return 42
+}
