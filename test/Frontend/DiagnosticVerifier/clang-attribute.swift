bbcba6a8f96da86c002db88c57d95030b63fa669
Henrik G. Olsson <h_olsson@apple.com>
2025-11-15 14:23:08 -0800

    [DiagnosticVerifier] add test with diagnostics in unparsed clang attr

---
+++ test/Frontend/DiagnosticVerifier/clang-attribute.swift
@@ -0,0 +1,82 @@
+// RUN: %empty-directory(%t)
+// RUN: split-file %s %t
+
+// RUN: not %target-swift-frontend -typecheck -verify %t/test.swift -I %t -plugin-path %swift-plugin-dir -Rmacro-expansions 2>&1 | \
+// RUN: %FileCheck %s --implicit-check-not error: --implicit-check-not note: --implicit-check-not warning: --implicit-check-not remark: --match-full-lines --sanitize TEST_H=%t%{fs-sep}test.h
+
+//--- test.h
+void foo(int len, int *p) __attribute__((
+    swift_attr("@_SwiftifyImport(.countedBy(pointer: .param(2), count: \"len\"))")));
+
+//--- test.swift
+import TestClang
+
+func bar(x: UnsafeMutableBufferPointer<CInt>) {
+  foo(x) // trigger macro expansion
+}
+
+//--- module.modulemap
+module TestClang {
+  header "test.h"
+  export *
+}
+
+// CHECK: @__swiftmacro_So3foo15_SwiftifyImportfMp_.swift:1:1: error: unexpected remark produced: macro content: |/// This is an auto-generated wrapper for safer interop|
+// CHECK: TEST_H:1:25: note: in expansion from here
+// CHECK: @__swiftmacro_So3foo15_SwiftifyImportfMp_.swift:1:1: note: file '@__swiftmacro_So3foo15_SwiftifyImportfMp_.swift' is not parsed for 'expected' statements. Use '-verify-additional-file @__swiftmacro_So3foo15_SwiftifyImportfMp_.swift' to enable, or '-verify-ignore-unrelated' to ignore diagnostics in this file
+// CHECK: TEST_H:1:25: note: in expansion from here
+// CHECK: error: unexpected note produced: in expansion of macro '_SwiftifyImport' on global function 'foo' here
+  // CHECK-NEXT: @_SwiftifyImport(.countedBy(pointer: .param(2), count: "len"))
+// CHECK: note: file '' is not parsed for 'expected' statements. Use '-verify-additional-file ' to enable, or '-verify-ignore-unrelated' to ignore diagnostics in this file
+
+// CHECK: @__swiftmacro_So3foo15_SwiftifyImportfMp_.swift:2:1: error: unexpected remark produced: macro content: |@_alwaysEmitIntoClient @_disfavoredOverload public func foo(_ p: UnsafeMutableBufferPointer<Int32>) {|
+// CHECK: TEST_H:1:25: note: in expansion from here
+// CHECK: @__swiftmacro_So3foo15_SwiftifyImportfMp_.swift:2:1: note: file '@__swiftmacro_So3foo15_SwiftifyImportfMp_.swift' is not parsed for 'expected' statements. Use '-verify-additional-file @__swiftmacro_So3foo15_SwiftifyImportfMp_.swift' to enable, or '-verify-ignore-unrelated' to ignore diagnostics in this file
+// CHECK: TEST_H:1:25: note: in expansion from here
+// CHECK: error: unexpected note produced: in expansion of macro '_SwiftifyImport' on global function 'foo' here
+  // CHECK-NEXT: @_SwiftifyImport(.countedBy(pointer: .param(2), count: "len"))
+// CHECK: note: file '' is not parsed for 'expected' statements. Use '-verify-additional-file ' to enable, or '-verify-ignore-unrelated' to ignore diagnostics in this file
+
+// CHECK: @__swiftmacro_So3foo15_SwiftifyImportfMp_.swift:3:1: error: unexpected remark produced: macro content: |    let len = Int32(exactly: p.count)!|
+// CHECK: TEST_H:1:25: note: in expansion from here
+// CHECK: @__swiftmacro_So3foo15_SwiftifyImportfMp_.swift:3:1: note: file '@__swiftmacro_So3foo15_SwiftifyImportfMp_.swift' is not parsed for 'expected' statements. Use '-verify-additional-file @__swiftmacro_So3foo15_SwiftifyImportfMp_.swift' to enable, or '-verify-ignore-unrelated' to ignore diagnostics in this file
+// CHECK: TEST_H:1:25: note: in expansion from here
+// CHECK: error: unexpected note produced: in expansion of macro '_SwiftifyImport' on global function 'foo' here
+  // CHECK-NEXT: @_SwiftifyImport(.countedBy(pointer: .param(2), count: "len"))
+// CHECK: note: file '' is not parsed for 'expected' statements. Use '-verify-additional-file ' to enable, or '-verify-ignore-unrelated' to ignore diagnostics in this file
+
+// CHECK: @__swiftmacro_So3foo15_SwiftifyImportfMp_.swift:4:1: error: unexpected remark produced: macro content: |    return unsafe foo(len, p.baseAddress!)|
+// CHECK: TEST_H:1:25: note: in expansion from here
+// CHECK: @__swiftmacro_So3foo15_SwiftifyImportfMp_.swift:4:1: note: file '@__swiftmacro_So3foo15_SwiftifyImportfMp_.swift' is not parsed for 'expected' statements. Use '-verify-additional-file @__swiftmacro_So3foo15_SwiftifyImportfMp_.swift' to enable, or '-verify-ignore-unrelated' to ignore diagnostics in this file
+// CHECK: TEST_H:1:25: note: in expansion from here
+// CHECK: error: unexpected note produced: in expansion of macro '_SwiftifyImport' on global function 'foo' here
+  // CHECK-NEXT: @_SwiftifyImport(.countedBy(pointer: .param(2), count: "len"))
+// CHECK: note: file '' is not parsed for 'expected' statements. Use '-verify-additional-file ' to enable, or '-verify-ignore-unrelated' to ignore diagnostics in this file
+
+// CHECK: @__swiftmacro_So3foo15_SwiftifyImportfMp_.swift:5:1: error: unexpected remark produced: macro content: |}|
+// CHECK: TEST_H:1:25: note: in expansion from here
+// CHECK: @__swiftmacro_So3foo15_SwiftifyImportfMp_.swift:5:1: note: file '@__swiftmacro_So3foo15_SwiftifyImportfMp_.swift' is not parsed for 'expected' statements. Use '-verify-additional-file @__swiftmacro_So3foo15_SwiftifyImportfMp_.swift' to enable, or '-verify-ignore-unrelated' to ignore diagnostics in this file
+// CHECK: TEST_H:1:25: note: in expansion from here
+// CHECK: error: unexpected note produced: in expansion of macro '_SwiftifyImport' on global function 'foo' here
+  // CHECK-NEXT: @_SwiftifyImport(.countedBy(pointer: .param(2), count: "len"))
+// CHECK: note: file '' is not parsed for 'expected' statements. Use '-verify-additional-file ' to enable, or '-verify-ignore-unrelated' to ignore diagnostics in this file
+
+// CHECK:  @__swiftmacro_So3foo15_SwiftifyImportfMp_.swift:1:1: remark: diagnostic produced elsewhere: macro content: |/// This is an auto-generated wrapper for safer interop|
+// CHECK: TEST_H:1:25: note: in expansion from here
+// CHECK: note: diagnostic produced elsewhere: in expansion of macro '_SwiftifyImport' on global function 'foo' here
+
+// CHECK:  @__swiftmacro_So3foo15_SwiftifyImportfMp_.swift:2:1: remark: diagnostic produced elsewhere: macro content: |@_alwaysEmitIntoClient @_disfavoredOverload public func foo(_ p: UnsafeMutableBufferPointer<Int32>) {|
+// CHECK: TEST_H:1:25: note: in expansion from here
+// CHECK: note: diagnostic produced elsewhere: in expansion of macro '_SwiftifyImport' on global function 'foo' here
+
+// CHECK:  @__swiftmacro_So3foo15_SwiftifyImportfMp_.swift:3:1: remark: diagnostic produced elsewhere: macro content: |    let len = Int32(exactly: p.count)!|
+// CHECK: TEST_H:1:25: note: in expansion from here
+// CHECK: note: diagnostic produced elsewhere: in expansion of macro '_SwiftifyImport' on global function 'foo' here
+
+// CHECK:  @__swiftmacro_So3foo15_SwiftifyImportfMp_.swift:4:1: remark: diagnostic produced elsewhere: macro content: |    return unsafe foo(len, p.baseAddress!)|
+// CHECK: TEST_H:1:25: note: in expansion from here
+// CHECK: note: diagnostic produced elsewhere: in expansion of macro '_SwiftifyImport' on global function 'foo' here
+
+// CHECK:  @__swiftmacro_So3foo15_SwiftifyImportfMp_.swift:5:1: remark: diagnostic produced elsewhere: macro content: |}|
+// CHECK: TEST_H:1:25: note: in expansion from here
+// CHECK: note: diagnostic produced elsewhere: in expansion of macro '_SwiftifyImport' on global function 'foo' here
