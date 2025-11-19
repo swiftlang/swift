bac0856f27ae90d326b688ea907d8f50cffc0a61
Hamish Knight <hamish_knight@apple.com>
2025-11-19 09:07:23 +0000

    [test] Add a fixed stress tester issue
    This is no longer too complex.

---
+++ validation-test/IDE/stress_tester_issues_fixed/issue-66785.swift
@@ -0,0 +1,5 @@
+// RUN: %batch-code-completion
+// https://github.com/swiftlang/swift/issues/66785
+
+0...31127...159768...8791155...11591160...11611425...14691471...14711473...14741476...14771479...#^COMPLETE^#
+// COMPLETE: Literal[Integer]/None: 0[#Int#]; name=0
