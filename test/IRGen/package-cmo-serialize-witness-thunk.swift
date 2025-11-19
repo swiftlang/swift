0d0595da43c93c24a72e2fe203025ebc3a8a3ed9
Yuta Saito <yuta@goodnotes.com>
2025-11-19 03:26:02 +0000

    [wasm] Adjust IRGen tests not to assume every arch support returning two ptr directly

--- test/IRGen/package-cmo-serialize-witness-thunk.swift
+++ test/IRGen/package-cmo-serialize-witness-thunk.swift
@@ -21,7 +21,6 @@
 // RUN: %FileCheck %s --check-prefix=CHECK-IR < %t/Lib-ir.ll
 // RUN: %FileCheck %s --check-prefix=CHECK-TBD < %t/Lib-tbd.tbd
 
-
 /// TEST that a public (or package) protocol witness thunk gets a shared linkage if Package CMO is enabled.
 /// It also gets [serialized] in emit-silgen.
 ///
@@ -33,8 +32,8 @@
 // CHECK-NO-PCMO-SIL-DAG: sil private [transparent] [thunk] @$s3Lib3PubVAA5ProtoA2aDP3fooSSvgTW : $@convention(witness_method: Proto) (@in_guaranteed Pub) -> @owned String {
 // CHECK-SIL-DAG:         sil shared [transparent] [thunk] @$s3Lib3PubVAA5ProtoA2aDP3fooSSvgTW : $@convention(witness_method: Proto) (@in_guaranteed Pub) -> @owned String {
 
-// CHECK-NO-PCMO-IR-DAG: define internal swiftcc { {{i64, ptr|i32, i32, i32}} } @"$s3Lib3PubVAA5ProtoA2aDP3fooSSvgTW"
-// CHECK-IR-DAG:         define linkonce_odr hidden swiftcc { {{i64, ptr|i32, i32, i32}} } @"$s3Lib3PubVAA5ProtoA2aDP3fooSSvgTW"
+// CHECK-NO-PCMO-IR-DAG: define internal swiftcc {{{ i64, ptr }|{ i32, i32, i32 }|void}} @"$s3Lib3PubVAA5ProtoA2aDP3fooSSvgTW"
+// CHECK-IR-DAG:         define linkonce_odr hidden swiftcc {{{ i64, ptr }|{ i32, i32, i32 }|void}} @"$s3Lib3PubVAA5ProtoA2aDP3fooSSvgTW"
 
 // protocol witness for Proto.bar(_:) in conformance Pub
 
