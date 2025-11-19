e400e707823b33bd5f046e6e3a81ef7863aeba41
Dan Blackwell <dan_blackwell@apple.com>
2025-11-19 11:39:10 +0000

    Address review comments

--- test/IRGen/memtag_stack.swift
+++ test/IRGen/memtag_stack.swift
@@ -1,4 +1,7 @@
-// RUN: %target-swift-frontend -emit-ir -sanitize=memtag-stack -parse-as-library -target arm64-apple-macosx10.9 %s | %FileCheck %s
+// REQUIRES: CODEGENERATOR=AArch64
+// REQUIRES: OS=macosx
+
+// RUN: %swift -emit-ir -sanitize=memtag-stack -parse-as-library -target arm64-apple-macosx10.9 %s | %FileCheck %s
 
 // Check that functions have the sanitize_memtag attribute when -sanitize=memtag-stack is enabled
 
