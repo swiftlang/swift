f7aae22e43e38abd4fd619cd6ba916213e3f5bda
Arnold Schwaighofer <aschwaighofer@apple.com>
2025-11-20 07:34:33 -0800

    [embedded] A few more test cases for existential casting

--- test/embedded/existential.swift
+++ test/embedded/existential.swift
@@ -216,6 +216,48 @@ func test7(_ p: any Any) {
   c.a()
 }
 
+class BaseClass {
+  func foo() { print("BaseClass.foo") }
+  deinit {
+    print("BaseClass.deinit")
+  }
+}
+
+class SubClass : BaseClass {
+  override func foo() { print("SubClass.foo") }
+}
+
+func test8(_ p: any Any) {
+  print("test any as? SubClass")
+  if let c = p as? SubClass {
+    print("success")
+    c.foo()
+  } else {
+    print("cast failed")
+  }
+}
+
+func test9(_ p: any Any) {
+  print("test any as? BaseClass")
+  if let c = p as? BaseClass {
+    print("success")
+    c.foo()
+  } else {
+    print("cast failed")
+  }
+}
+
+func test10(_ p: any Any) {
+  print("test any as! BaseClass")
+  let c = p as! BaseClass
+  c.foo()
+}
+
+func test11(_ p: any Any) {
+  print("test any as! SubClass")
+  let c = p as! SubClass
+  c.foo()
+}
 @main
 struct Main {
   static func main() {
@@ -262,7 +304,7 @@ struct Main {
 // OUTPUT:  deinit called
 // OUTPUT: cast failed
 // OUTPUT-NOT:  deinit called
-   test5(GenericStructWithClass<Int>())
+    test5(GenericStructWithClass<Int>())
 // OUTPUT: test any as? MyStruct
 // OUTPUT: cast failed
 // OUTPUT:  deinit called
@@ -289,5 +331,46 @@ struct Main {
 // OUTPUT: a LargeMyStruct 5
 // OUTPUT:  deinit called
 // OUTPUT-NOT:  deinit called
+    test8(SubClass())
+// OUTPUT:  success
+// OUTPUT:  SubClass.foo
+// OUTPUT:  BaseClass.deinit
+// OUTPUT-NOT:  deinit
+    test8(BaseClass())
+// OUTPUT: test any as? SubClass
+// OUTPUT: cast failed
+// OUTPUT: BaseClass.deinit
+// OUTPUT-NOT:  deinit
+    test9(SubClass())
+// OUTPUT: test any as? BaseClass
+// OUTPUT:  success
+// OUTPUT:  SubClass.foo
+// OUTPUT:  BaseClass.deinit
+// OUTPUT-NOT:  deinit
+    test9(BaseClass())
+// OUTPUT: test any as? BaseClass
+// OUTPUT:  success
+// OUTPUT:  BaseClass.foo
+// OUTPUT:  BaseClass.deinit
+// OUTPUT-NOT:  deinit
+    test9(C())
+// OUTPUT: test any as? BaseClass
+// OUTPUT: cast failed
+// OUTPUT-NOT:  deinit
+    test10(BaseClass())
+// OUTPUT: test any as! BaseClass
+// OUTPUT:  BaseClass.foo
+// OUTPUT:  BaseClass.deinit
+// OUTPUT-NOT:  deinit
+    test10(SubClass())
+// OUTPUT: test any as! BaseClass
+// OUTPUT:  SubClass.foo
+// OUTPUT:  BaseClass.deinit
+// OUTPUT-NOT:  deinit
+    test11(SubClass())
+// OUTPUT: test any as! SubClass
+// OUTPUT:  SubClass.foo
+// OUTPUT:  BaseClass.deinit
+// OUTPUT-NOT:  deinit
   }
 }
