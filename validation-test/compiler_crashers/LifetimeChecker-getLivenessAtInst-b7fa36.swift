b8a8a84615198505b58bcd4c9d927efa9ff612cc
Hamish Knight <hamish_knight@apple.com>
2025-12-06 10:21:17 +0000

    [test] Remove a duplicate test

--- validation-test/compiler_crashers/LifetimeChecker-getLivenessAtInst-b7fa36.swift
+++ validation-test/compiler_crashers/LifetimeChecker-getLivenessAtInst-b7fa36.swift
@@ -1,4 +1,4 @@
-// {"kind":"emit-sil","original":"96451f18","signature":"(anonymous namespace)::LifetimeChecker::getLivenessAtInst(swift::SILInstruction*, unsigned int, unsigned int)","signatureAssert":"Assertion failed: (E <= size() && \"Attempted to set out-of-bounds range!\"), function set"}
+// {"kind":"emit-sil","original":"af5c6219","signature":"(anonymous namespace)::LifetimeChecker::getLivenessAtInst(swift::SILInstruction*, unsigned int, unsigned int)","signatureAssert":"Assertion failed: (E <= size() && \"Attempted to set out-of-bounds range!\"), function set"}
 // RUN: not --crash %target-swift-frontend -emit-sil %s
 struct a<b> {
   let c: String
