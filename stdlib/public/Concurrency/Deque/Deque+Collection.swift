2cbe719fd68cf6848afba811bdcbd7b47971269a
Nate Cook <natecook@apple.com>
2025-12-08 13:46:32 -0600

    Back out new type aliases for existing sequences

--- stdlib/public/Concurrency/Deque/Deque+Collection.swift
+++ stdlib/public/Concurrency/Deque/Deque+Collection.swift
@@ -19,9 +19,6 @@ extension _Deque: Sequence {
   // This custom implementation performs direct storage access to eliminate any
   // and all index validation overhead. It also optimizes away repeated
   // conversions from indices to storage slots.
-  @available(macOS 9999, iOS 9999, watchOS 9999, tvOS 9999, *)
-  internal typealias BorrowingIterator = BorrowingIteratorAdapter<Iterator>
-
   /// An iterator over the members of a deque.
   struct Iterator: IteratorProtocol {
     internal var _storage: _Deque._Storage
