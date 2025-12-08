ee14e1fe6d94629c8b00eba4047352e48816a6b4
Karoy Lorentey <klorentey@apple.com>
2025-12-08 13:18:42 -0800

    Update stdlib/public/core/Array.swift

--- stdlib/public/core/Array.swift
+++ stdlib/public/core/Array.swift
@@ -2177,8 +2177,7 @@ extension Array {
   /// are both `true`, then `a.isTriviallyIdentical(to: c)` is also `true`.
   /// (Transitivity)
   /// - If `a` and `b` are `Equatable`, then `a.isTriviallyIdentical(b)` implies
-  /// `a == b`
-  ///   - `a == b` does not imply `a.isTriviallyIdentical(b)`
+  /// `a == b`. `a == b` does not imply `a.isTriviallyIdentical(b)`
   ///
   /// Values produced by copying the same value, with no intervening mutations,
   /// compare as identical:
