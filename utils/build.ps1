6a9c26329aada3f9c49ff8415b048b7b6669a664
John Bute <johnbute@Johns-MacBook-Pro-4.local>
2025-12-10 13:05:53 -0500

    using msvc compilers instead
    (cherry picked from commit 90aaa0fe3e2474cf33c75c5bc5d243de91cc0596)

--- utils/build.ps1
+++ utils/build.ps1
@@ -3627,7 +3627,8 @@ function Build-ArgumentParser([Hashtable] $Platform) {
     -Bin (Get-ProjectBinaryCache $Platform ArgumentParser) `
     -InstallTo "$($Platform.ToolchainInstallRoot)\usr" `
     -Platform $Platform `
-    -UseBuiltCompilers Swift C`
+    -UseBuiltCompilers Swift `
+    -UseMSVCCompilers C `
     -SwiftSDK (Get-SwiftSDK -OS $Platform.OS -Identifier $Platform.DefaultSDK) `
     -Defines @{
       BUILD_SHARED_LIBS = "YES";
