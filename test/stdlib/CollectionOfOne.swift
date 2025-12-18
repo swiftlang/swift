63e4bfbd89a1492a031ed6932df25ff47cb36f2d
Clinton Nkwocha <ci_nkwocha@apple.com>
2025-12-18 11:37:42 +0000

    Add Hashable conformance to CollectionOfOne

--- test/stdlib/CollectionOfOne.swift
+++ test/stdlib/CollectionOfOne.swift
@@ -18,4 +18,16 @@ OneTests.test("Basic tests") {
   }
 }
 
+if #available(SwiftStdlib 6.3, *) {
+  OneTests.test("Equatable") {
+    checkEquatable(true, CollectionOfOne(1), CollectionOfOne(1))
+    checkEquatable(false, CollectionOfOne("a"), CollectionOfOne("b"))
+  }
+
+  OneTests.test("Hashable") {
+    let collections = [CollectionOfOne(1), CollectionOfOne(2)]
+    checkHashable(collections, equalityOracle: { collections[$0] == collections[$1] })
+  }
+}
+
 runAllTests()
