6c5cc0c2a6ef538ad8991114e8bd4ba7a7811928
Anton Korobeynikov <anton@korobeynikov.info>
2025-12-24 09:53:23 +0300

    Fix serialization / deserialization of non-accessor coroutines

--- lib/Serialization/ModuleFormat.h
+++ lib/Serialization/ModuleFormat.h
@@ -1435,6 +1435,12 @@ namespace decls_block {
                      BCFixed<1>               // addressable
                      >;
 
+  using FunctionYieldLayout =
+      BCRecordLayout<FUNCTION_YIELD,
+                     TypeIDField,             // type
+                     ParamDeclSpecifierField // inout, shared or owned?
+                     >;
+
   TYPE_LAYOUT(MetatypeTypeLayout,
     METATYPE_TYPE,
     TypeIDField,                // instance type
@@ -1840,6 +1846,8 @@ namespace decls_block {
     BCFixed<1>,   // async?
     BCFixed<1>,   // throws?
     TypeIDField,  // thrown error
+    TypeIDField,  // yield value type
+    BCFixed<1>,   // inout yield?
     GenericSignatureIDField, // generic environment
     TypeIDField,  // result interface type
     BCFixed<1>,   // IUO result?
