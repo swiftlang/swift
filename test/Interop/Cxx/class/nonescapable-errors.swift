3bb9593e626b8adb789be378e7c78d5b6c8b76c0
Gabor Horvath <gaborh@apple.com>
2025-12-29 10:08:24 +0100

    [cxx-interop] Add conditional escapability to standard smart pointers
    The smart pointers in the standard library should be conditionally
    escapable based on the escapability of the pointee type. They, however,
    should not be conditionally copyable. This patch adds the missing
    conditional conformance for these types.

--- test/Interop/Cxx/class/nonescapable-errors.swift
+++ test/Interop/Cxx/class/nonescapable-errors.swift
@@ -1,6 +1,6 @@
 // RUN: rm -rf %t
 // RUN: split-file %s %t
-//
+
 // RUN: %target-swift-frontend -typecheck -verify %t%{fs-sep}test.swift \
 // RUN:   -I %swift_src_root%{fs-sep}lib%{fs-sep}ClangImporter%{fs-sep}SwiftBridging -I %t%{fs-sep}Inputs \
 // RUN:   -cxx-interoperability-mode=default \
@@ -8,14 +8,14 @@
 // RUN:   -verify-additional-file %t%{fs-sep}Inputs%{fs-sep}nonescapable.h \
 // RUN:   -verify-additional-prefix LIFETIMES- \
 // RUN:   -enable-experimental-feature LifetimeDependence
-//
+
 // RUN: %target-swift-frontend -typecheck -verify %t%{fs-sep}test.swift \
 // RUN:   -I %swift_src_root%{fs-sep}lib%{fs-sep}ClangImporter%{fs-sep}SwiftBridging -I %t%{fs-sep}Inputs \
 // RUN:   -cxx-interoperability-mode=default \
 // RUN:   -verify-ignore-unrelated \
 // RUN:   -verify-additional-file %t%{fs-sep}Inputs%{fs-sep}nonescapable.h \
 // RUN:   -verify-additional-prefix NO-LIFETIMES-
-//
+
 // REQUIRES: swift_feature_LifetimeDependence
 
 //--- Inputs/module.modulemap
@@ -319,7 +319,7 @@ public func noAnnotations() -> View {
     k2()
     k3()
     l1()
-    l2();
+    l2()
     return View()
 }
 
