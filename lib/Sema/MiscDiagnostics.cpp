fcffabf7161a85ce8a39e2b1661fe5b061f12309
Elsa Keirouz <e_keirouz@apple.com>
2025-12-31 18:00:08 +0100

    fix: remove obsolete comment

--- lib/Sema/MiscDiagnostics.cpp
+++ lib/Sema/MiscDiagnostics.cpp
@@ -4200,7 +4200,6 @@ VarDeclUsageChecker::~VarDeclUsageChecker() {
           // Don't try to suggest 'var' -> 'let' conversion
           // in case of 'for' loop because it's an implicitly
           // immutable context.
-          // FIXME: need to find out if the stmt is part of a foreach's
           // desugared
           suggestLet = !isa<ForEachStmt>(stmt);
         }
