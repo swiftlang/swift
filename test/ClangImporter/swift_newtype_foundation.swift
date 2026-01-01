e12d0fad1b01005e40e9b31bed31ad3adcd57247
Kyle <kyle201817146@gmail.com>
2026-01-01 10:12:04 +0800

    Update to use split-file

--- test/ClangImporter/swift_newtype_foundation.swift
+++ test/ClangImporter/swift_newtype_foundation.swift
@@ -1,57 +1,41 @@
 // RUN: %empty-directory(%t)
+// RUN: split-file %s %t
 
-// Test that swift_newtype works on all platforms when Foundation is imported.
-// RUN: %target-swift-frontend(mock-sdk: %clang-importer-sdk) -typecheck -I %S/Inputs/custom-modules %s -verify
+// Build the FoundationStub module first
+// RUN: %target-swift-frontend -emit-module -emit-module-path %t/FoundationStub.swiftmodule -module-name FoundationStub %t/FoundationStub.swift
 
-// REQUIRES: OS=linux-gnu || OS=windows-msvc
+// Test that swift_newtype works on all platforms when _ObjectiveCBridgeable is available.
+// RUN: %target-swift-frontend(mock-sdk: %clang-importer-sdk) -typecheck -I %S/Inputs/custom-modules -I %t %t/main.swift -verify
 
 // This test verifies the fix for https://github.com/swiftlang/swift/issues/71086
-// Previously, importing swift_newtype types with Foundation would crash on non-ObjC
-// platforms because the _ObjectiveCBridgeable extension in NewtypeWrapper.swift was
-// incorrectly guarded by #if _runtime(_ObjC), preventing it from being available on
-// Linux even though _ObjectiveCBridgeable is available via swift-corelibs-foundation.
+// Previously, importing swift_newtype types would crash on non-ObjC platforms
+// because the _ObjectiveCBridgeable extension in NewtypeWrapper.swift was
+// incorrectly guarded by #if _runtime(_ObjC), preventing it from being available
+// on Linux even though _ObjectiveCBridgeable is available via swift-corelibs-foundation.
 
-import NewtypeFoundation
-
-// Stub Foundation types for non-Darwin platforms since the mock SDK doesn't include them
-// Minimal NSObject stub
-class NSObject {
-  init() {}
-
-  func isEqual(_ object: Any?) -> Bool {
-    return false
-  }
-}
+//--- FoundationStub.swift
 
 // Minimal NSNumber stub for testing
-class NSNumber: NSObject {
+public class NSNumber {
   private var _value: UInt64
 
-  init(value: UInt32) {
+  public init(value: UInt32) {
     self._value = UInt64(value)
-    super.init()
   }
 
-  var uint32Value: UInt32 {
+  public var uint32Value: UInt32 {
     return UInt32(truncatingIfNeeded: _value)
-  }
-
-  override func isEqual(_ object: Any?) -> Bool {
-    guard let other = object as? NSNumber else { return false }
-    return self._value == other._value
   }
 }
 
-// UInt32 bridging conformance for non-Darwin platforms
-extension UInt32: _ObjectiveCBridgeable {
+// UInt32 bridging conformance
+extension UInt32: @retroactive _ObjectiveCBridgeable {
   public func _bridgeToObjectiveC() -> NSNumber {
     return NSNumber(value: self)
   }
 
   public static func _forceBridgeFromObjectiveC(_ x: NSNumber, result: inout UInt32?) {
-    if !_conditionallyBridgeFromObjectiveC(x, result: &result) {
-      fatalError("Unable to bridge \(type(of: x)) to \(self)")
-    }
+    result = x.uint32Value
   }
 
   public static func _conditionallyBridgeFromObjectiveC(_ x: NSNumber, result: inout UInt32?) -> Bool {
@@ -65,15 +49,20 @@ extension UInt32: _ObjectiveCBridgeable {
   }
 }
 
+//--- main.swift
+
+import NewtypeFoundation
+import FoundationStub
+
 // Test that the newtype wrapper conforms to _ObjectiveCBridgeable when the
 // underlying type does
 func acceptObjectiveCBridgeable<T: _ObjectiveCBridgeable>(_ t: T) {}
 
 func testUInt32Newtype(attr: MyUInt32Newtype) {
-  // UInt32 conforms to _ObjectiveCBridgeable on all platforms (via Foundation)
+  // UInt32 conforms to _ObjectiveCBridgeable (via FoundationStub)
   // so MyUInt32Newtype should also conform
   acceptObjectiveCBridgeable(attr)
-  
+
   // Test that we can use the newtype
   let _ = attr.rawValue
   let copy = MyUInt32Newtype(rawValue: 42)
@@ -85,7 +74,7 @@ func testEquatableHashable(a: MyUInt32Newtype, b: MyUInt32Newtype) {
   let _ = a == b
   let _ = a != b
   let _ = a.hashValue
-  
+
   var dict: [MyUInt32Newtype: String] = [:]
   dict[a] = "test"
   let _ = dict[b]
@@ -99,7 +88,7 @@ func testCollections() {
     MyUInt32Newtype(rawValue: 3)
   ]
   let _ = array.count
-  
+
   let set: Set<MyUInt32Newtype> = [
     MyUInt32Newtype(rawValue: 1),
     MyUInt32Newtype(rawValue: 2)
@@ -115,4 +104,3 @@ struct Container: Equatable {
 func testContainerEquatable(a: Container, b: Container) {
   let _ = a == b
 }
-
