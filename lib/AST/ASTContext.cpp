bcc31df1a9c1af8ced527e33cbb8a443842f3470
Michael Gottesman <mgottesman@apple.com>
2026-01-06 09:30:52 -0800

    [sil] Add convenience APIs to SILBoxType for generics safe field access
    Before this patch it was easy to try to work with the types in a SILBoxType by
    accessing the layout of the SILBoxType... especially if one was working with
    mutability since there wasn't any comment/APIs on SILBoxType that pointed one at
    the appropriate API on SILType.
    
    To make this easier to use and self document, I added some new helper APIs onto
    SILBoxType that:
    
    1. Let one grab the fields without touching the layout. The layout is an
    internal detail of SILBoxType that shouldn't be touched unless it is necessary.
    
    2. Get a specific properly specialized SILType of a SILType for a given
    SILFunction.
    
    3. Access the number of SILFIelds and also whether or not a specific SILField is
    mutable.
    
    4. Yields a transform range that transform the SILFields in the SILBoxType into
    a range of properly specialized SILTypes.
    
    This should prevent these sorts of mistakes from happening in the future.
    
    (cherry picked from commit f10ca1c949629ae331c77d9bbacdd09147c86bdd)

--- lib/AST/ASTContext.cpp
+++ lib/AST/ASTContext.cpp
@@ -7036,6 +7036,14 @@ SILBoxType::withMutable(ASTContext &ctx,
       getSubstitutions());
 }
 
+ArrayRef<SILField> SILBoxType::getFields() const {
+  return getLayout()->getFields();
+}
+
+bool SILBoxType::isFieldMutable(unsigned index) const {
+  return getFields()[index].isMutable();
+}
+
 LayoutConstraint
 LayoutConstraint::getLayoutConstraint(LayoutConstraintKind Kind,
                                       ASTContext &C) {
