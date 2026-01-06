f79c1307090ec3a008e9ae0e07c2bc8620878d02
Doug Gregor <dgregor@apple.com>
2026-01-05 17:53:29 -0800

    Use pure C headers with newtype

--- test/ClangImporter/newtype_conformance_objc.swift
+++ test/ClangImporter/newtype_conformance_objc.swift
@@ -15,6 +15,23 @@ func acceptEquatable<T: Equatable>(_: T) {}
 func acceptHashable<T: Hashable>(_: T) {}
 // expected-note@-1 {{where 'T' = 'WrappedRef'}}
 // expected-note@-2 {{where 'T' = 'WrappedValue'}}
+func acceptComparable<T: Comparable>(_: T) {}
+// expected-note@-1 {{where 'T' = 'NSNotification.Name'}}
+
+func testNewTypeWrapper(x: NSNotification.Name, y: NSNotification.Name, z: NSFileAttributeKey) {
+  acceptEquatable(x)
+  acceptHashable(x)
+  acceptComparable(x) // expected-error {{global function 'acceptComparable' requires that 'NSNotification.Name' conform to 'Comparable'}}
+  // expected-note@-1 {{did you mean to use '.rawValue'?}} {{19-19=.rawValue}}
+
+  _ = x == y
+  _ = x != y
+  _ = x.hashValue
+
+  _ = x as NSString
+
+  _ = z as NSString
+}
 
 func testCustomWrappers(wrappedRef: WrappedRef, wrappedValue: WrappedValue) {
   acceptEquatable(wrappedRef)
