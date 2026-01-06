25e970415e76364e1671bedbd494b1a3305fc33e
Doug Gregor <dgregor@apple.com>
2026-01-05 17:53:29 -0800

    Use pure C headers with newtype

--- test/ClangImporter/newtype_conformance.swift
+++ test/ClangImporter/newtype_conformance.swift
@@ -1,25 +1,19 @@
 // RUN: %target-swift-frontend(mock-sdk: %clang-importer-sdk) -typecheck -I %S/Inputs/custom-modules %s -verify
 
-import Foundation
+import SimpleSwiftNewtypes
 
 func acceptEquatable<T: Equatable>(_: T) {}
 func acceptHashable<T: Hashable>(_: T) {}
 func acceptComparable<T: Comparable>(_: T) {}
-// expected-note@-1 {{where 'T' = 'NSNotification.Name'}}
+// expected-note@-1{{where 'T' = 'Radians'}}
 
-func testNewTypeWrapper(x: NSNotification.Name, y: NSNotification.Name, z: NSFileAttributeKey) {
+func testNewTypeWrapper(x: Radians, y: Radians, z: Float) {
   acceptEquatable(x)
   acceptHashable(x)
-  acceptComparable(x) // expected-error {{global function 'acceptComparable' requires that 'NSNotification.Name' conform to 'Comparable'}}
+  acceptComparable(x) // expected-error {{global function 'acceptComparable' requires that 'Radians' conform to 'Comparable'}}
   // expected-note@-1 {{did you mean to use '.rawValue'?}} {{19-19=.rawValue}}
 
   _ = x == y
   _ = x != y
   _ = x.hashValue
-
-#if _runtime(_ObjC)
-  _ = x as NSString
-
-  _ = z as NSString
-#endif
 }
