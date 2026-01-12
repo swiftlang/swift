87dc1c5d8b7a13c99cc6075b1d0ed0cb87f32e65
Erik Eckstein <eeckstein@apple.com>
2026-01-12 08:56:15 +0100

    SIL verifier: accept "owned" ownership for borrowed-from's enclosing values
    "none" is like "owned" and can happen e.g. if a non-trivial enum is constructed with a trivial case:
    ```
      %1 = enum $Optional<AnyObject>, #Optional.none!enumelt   // ownership: none
      ...
      %3 = borrowed %phi from (%1)
    ```
    
    Fix a false verification error
    https://github.com/swiftlang/swift/issues/86407
    rdar://167833469

--- SwiftCompilerSources/Sources/SIL/Utilities/Verifier.swift
+++ SwiftCompilerSources/Sources/SIL/Utilities/Verifier.swift
@@ -150,11 +150,19 @@ private extension Value {
     switch ownership {
     case .owned:
       return true
+    case .none:
+      // "none" is like "owned" and can happen e.g. if a non-trivial enum is constructed with a trivial case:
+      // ```
+      //   %1 = enum $Optional<AnyObject>, #Optional.none!enumelt   // ownership: none
+      //   ...
+      //   %3 = borrowed %phi from (%1)
+      // ```
+      return true
     case .guaranteed:
       return BeginBorrowValue(self) != nil ||
              self is BorrowedFromInst ||
              forwardingInstruction != nil
-    case .none, .unowned:
+    case .unowned:
       return false
     }
   }
