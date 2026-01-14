3e94ef5c1e2d73cd00a1f3d1c6b4d723841d2458
Ramon Asuncion <asuncionbatista@gmail.com>
2026-01-14 14:23:01 -0500

    [Test][SourceKit] Add UNSUPPORTED directive for legacy driver

--- test/Driver/LegacyDriver/legacy-driver-propagates-response-file-to-new-driver.swift
+++ test/Driver/LegacyDriver/legacy-driver-propagates-response-file-to-new-driver.swift
@@ -3,8 +3,10 @@
 // preserving response files) instead of trying to spawn the process with the
 // expansion, which may exceed `ARG_MAX`.
 
+// UNSUPPORTED: OS=windows-msvc
+
 // RUN: %{python} -c 'for i in range(500001): print("-DTEST5_" + str(i))' > %t.resp
-// RUN: %{python} -c "import shutil; shutil.copy(r'%S/Inputs/print-args.sh', r'%swift-bin-dir/legacy-driver-propagates-response-file.sh')"
+// RUN: cp %S/Inputs/print-args.sh %swift-bin-dir/legacy-driver-propagates-response-file.sh
 // RUN: env SWIFT_OVERLOAD_DRIVER=legacy-driver-propagates-response-file.sh %swiftc_driver_plain -disallow-use-new-driver %s @%t.resp | %FileCheck %s
 // RUN: rm %swift-bin-dir/legacy-driver-propagates-response-file.sh
 
