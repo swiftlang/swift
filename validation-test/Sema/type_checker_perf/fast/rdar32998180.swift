6d00491a13ad95a1e372e0e1ad7c0f1e33d25a0e
Slava Pestov <spestov@apple.com>
2026-01-15 11:05:05 -0500

    Sema: Use -solver-scope-threshold instead of -solver-expression-time-threshold in type_checker_perf/fast/ tests
    Also, re-enable rdar21398466.swift, which was disabled in 2020 and
    never re-enabled again.
    
    Most tests finish with 1000 scopes; for a few I had to bump it a bit.
    The maximum valid test is rdar32998180.swift with 16000 scopes, which
    takes 27ms wall time on my machine.
    
    The maximum invalid test is rdar21398466.swift, which takes 50ms
    on my machine.

--- validation-test/Sema/type_checker_perf/fast/rdar32998180.swift
+++ validation-test/Sema/type_checker_perf/fast/rdar32998180.swift
@@ -1,9 +1,9 @@
-// RUN: %target-typecheck-verify-swift -solver-expression-time-threshold=1
+// RUN: %target-typecheck-verify-swift -solver-scope-threshold=12000
 // REQUIRES: tools-release,no_asan
 // REQUIRES: OS=macosx
 
 func rdar32998180(value: UInt16) -> UInt16 {
   let result = ((((value >> 1) ^ (value >> 1) ^ (value >> 1) ^ (value >> 1)) & 1) << 1)
-  | (((((value >> 1) ^ (value >> 1) ^ (value >> 1) ^ (value >> 1)) & 1) << 1) << 1)
+             | (((((value >> 1) ^ (value >> 1) ^ (value >> 1) ^ (value >> 1)) & 1) << 1) << 1)
   return result
 }
