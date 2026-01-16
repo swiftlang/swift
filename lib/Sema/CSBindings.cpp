86e88f50e68e27f5994608eb25dae84b9090a780
Pavel Yaskevich <pyaskevich@apple.com>
2026-01-16 15:15:18 -0800

    [CSBindings] Don't use iterator after calling `erase`
    There are few places where the binding is found, removed
    and then re-inserted with a different type. The new
    binding should be formed before `erase` is called and
    iterator is mutated otherwise using the old iterator
    point could lead to corruption.
    
    In the added test-case `determineLiteralCoverage`
    would find a "supertypes of" binding and attempt to
    remove + re-insert it but it ends up as "subtypes of"
    after that because calling `.erase` moves the iterator.

--- lib/Sema/CSBindings.cpp
+++ lib/Sema/CSBindings.cpp
@@ -959,8 +959,9 @@ void BindingSet::addBinding(PotentialBinding binding) {
           });
 
       if (inferredCGFloat != Bindings.end()) {
-        Bindings.erase(inferredCGFloat);
-        Bindings.insert(inferredCGFloat->withType(type));
+        auto newBinding = inferredCGFloat->withType(type);
+        (void)Bindings.erase(inferredCGFloat);
+        Bindings.insert(newBinding);
         return;
       }
     }
@@ -1048,8 +1049,9 @@ void BindingSet::determineLiteralCoverage() {
       literal.setCoveredBy(binding->getSource());
 
       if (adjustedTy) {
-        Bindings.erase(binding);
-        Bindings.insert(binding->withType(adjustedTy));
+        auto newBinding = binding->withType(adjustedTy);
+        (void)Bindings.erase(binding);
+        Bindings.insert(newBinding);
       }
 
       break;
