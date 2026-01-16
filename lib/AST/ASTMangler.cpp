f8f48d173c9f281ef3383986566d17c67f8fcbab
Slava Pestov <spestov@apple.com>
2026-01-15 21:29:50 -0500

    ASTMangler: Fix incorrect mangling of retroactive conformances to marker protocols
    We turn off the AllowMarkerProtocols mangler flag when
    mangling the types that are passed into the runtime demangler.
    
    The logic in appendAnyProtocolConformance() was wrong when
    this flag was set; we would early exit from this function,
    but then appendRetroactiveConformances() would still call
    appendListSeparator(). The would result in an invalid mangled
    string which could not be parsed by the demangler.
    
    In particular, this meant that the recent changes to generalize
    Equatable to allow non-Copyable and non-Escapable conformers
    could introduce runtime crashes, because a mangled string for
    a retroactive conformance to Equatable could be incorrect.
    
    The fix is to handle AllowMarkerProtocols further up in
    forEachConditionalConformance().
    
    Note that this change should not directly change ABI, because
    AllowMarkerProtocols is on for symbol names.
    
    When AllowMarkerProtocols is on, we still always mangle
    conformances to Copyable and Escapable in this code path.
    
    This means that even with this change, mangling a symbol name
    that contains a retroactive conformance to Equatable can output
    a different string than in Swift 6.3, which means we have an ABI
    break. That problem requires a separate fix.
    
    - Fixes rdar://problem/168023786.

--- lib/AST/ASTMangler.cpp
+++ lib/AST/ASTMangler.cpp
@@ -2112,6 +2112,7 @@ static bool isRetroactiveConformance(const RootProtocolConformance *root) {
 
 template<typename Fn>
 static bool forEachConditionalConformance(const ProtocolConformance *conformance,
+                                          bool allowMarkerProtocols,
                                           Fn fn) {
   auto *rootConformance = conformance->getRootConformance();
 
@@ -2139,6 +2140,10 @@ static bool forEachConditionalConformance(const ProtocolConformance *conformance
       continue;
 
     ProtocolDecl *proto = req.getProtocolDecl();
+
+    if (!allowMarkerProtocols && proto->isMarkerProtocol())
+      continue;
+
     auto conformance = subMap.lookupConformance(
         req.getFirstType()->getCanonicalType(), proto);
     if (fn(req.getFirstType().subst(subMap), conformance))
@@ -2175,7 +2180,8 @@ static bool containsRetroactiveConformance(
 
   // If the conformance is conditional and any of the substitutions used to
   // satisfy the conditions are retroactive, it's retroactive.
-  return forEachConditionalConformance(conformance,
+  return forEachConditionalConformance(
+    conformance, /*allowMarkerProtocols=*/false,
     [&](Type substType, ProtocolConformanceRef substConf) -> bool {
       return containsRetroactiveConformance(substConf);
     });
@@ -4587,10 +4593,10 @@ void ASTMangler::appendAnyProtocolConformance(
                                            CanType conformingType,
                                            ProtocolConformanceRef conformance) {
   // If we have a conformance to a marker protocol but we aren't allowed to
-  // emit marker protocols, skip it.
-  if (!AllowMarkerProtocols &&
-      conformance.getProtocol()->isMarkerProtocol())
-    return;
+  // emit marker protocols, it's too late. The caller should have handled this,
+  // because otherwise they don't know if they should emit a list separator or
+  // not.
+  ASSERT(AllowMarkerProtocols || !conformance.getProtocol()->isMarkerProtocol());
 
   // While all invertible protocols are marker protocols, do not mangle them
   // as a dependent conformance. See `conformanceRequirementIndex` which skips
@@ -4651,7 +4657,7 @@ void ASTMangler::appendConcreteProtocolConformance(
 
   // Conditional conformance requirements.
   bool firstRequirement = true;
-  forEachConditionalConformance(conformance,
+  forEachConditionalConformance(conformance, AllowMarkerProtocols,
     [&](Type substType, ProtocolConformanceRef substConf) -> bool {
       if (substType->hasArchetype())
         substType = substType->mapTypeOutOfEnvironment();
