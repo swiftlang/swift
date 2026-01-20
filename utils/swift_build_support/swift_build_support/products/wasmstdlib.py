7a19572701d574b4b1a21691277dda195eff6796
Max Desiatov <m_desiatov@apple.com>
2026-01-20 15:46:26 +0000

    wasmstdlib.py: add 'AutoDiff' to lit test paths
    Follow up to https://github.com/swiftlang/swift/pull/86552 enabling tests for the newly enabled experimental feature.

--- utils/swift_build_support/swift_build_support/products/wasmstdlib.py
+++ utils/swift_build_support/swift_build_support/products/wasmstdlib.py
@@ -193,7 +193,7 @@ class WasmStdlib(cmake_product.CMakeProduct):
         self.cmake_options.define('SWIFT_INCLUDE_TESTS:BOOL', 'TRUE')
         self.cmake_options.define('SWIFT_ENABLE_SOURCEKIT_TESTS:BOOL', 'FALSE')
         lit_test_paths = [
-            'IRGen', 'stdlib', 'Concurrency/Runtime', 'embedded',
+            'IRGen', 'stdlib', 'Concurrency/Runtime', 'embedded', 'AutoDiff',
             # TODO(katei): Enable all interpreter tests
             'Interpreter/enum.swift',
         ]
