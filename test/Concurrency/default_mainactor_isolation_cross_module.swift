be4408a38b259f99ada1ebef4df839612b718adb
Pavel Yaskevich <pyaskevich@apple.com>
2026-01-20 17:47:43 -0800

    [Concurrency] Mark a type as `nonisolated` if a conformance requires it
    When default isolation is set to `MainActor` a type should be
    considered `nonisolated` if it's primary definition is stated
    to conform to a `Sendable` or `SendableMetatype` protocol. But
    default isolation computation currently produces "unspecified"
    instead which cannot be used as a condition during conformance
    isolation inference and so some implied conformances are
    wrongly considered to be `@MainActor`.
    
    The change adjusts the default isolation computation to produce
    `nonisolated` isolation in the case described above and adds
    a condition to the conformance isolation checking to prevent
    inferring `@MainActor` from an extension if a type itself is
    `nonisolated`.
    
    Resolves: rdar://166542727
    (cherry picked from commit 4646ca13859b6f09d3594d07e2127de0ee775bea)

---
+++ test/Concurrency/default_mainactor_isolation_cross_module.swift
@@ -0,0 +1,86 @@
+// RUN: %empty-directory(%t/src)
+// RUN: split-file %s %t/src
+
+// Build macros
+// RUN: %host-build-swift -swift-version 5 -emit-library -o %t/%target-library-name(MacroDefinition) -module-name=MacroDefinition %t/src/macro_definitions.swift -g -no-toolchain-stdlib-rpath
+
+/// Build the library
+// RUN: %target-swift-frontend -emit-module %t/src/Lib.swift \
+// RUN:   -module-name Lib \
+// RUN:   -enable-library-evolution \
+// RUN:   -emit-module-path %t/Lib.swiftmodule \
+// RUN:   -emit-module-interface-path %t/Lib.swiftinterface
+
+// RUN: %target-swift-typecheck-module-from-interface(%t/Lib.swiftinterface) -module-name Lib
+
+// Build the client using module
+// RUN: %target-swift-frontend -typecheck -verify -load-plugin-library %t/%target-library-name(MacroDefinition) -default-isolation MainActor -module-name Client -I %t %t/src/Client.swift
+// RUN: %target-swift-frontend -typecheck -verify -load-plugin-library %t/%target-library-name(MacroDefinition) -default-isolation MainActor -module-name ClientWithMacro -I %t %t/src/ClientWithMacro.swift
+
+// RUN: rm %t/Lib.swiftmodule
+
+// Re-build the client using interface
+// RUN: %target-swift-frontend -typecheck -verify -load-plugin-library %t/%target-library-name(MacroDefinition) -default-isolation MainActor -module-name Client -I %t %t/src/Client.swift
+// RUN: %target-swift-frontend -typecheck -verify -load-plugin-library %t/%target-library-name(MacroDefinition) -default-isolation MainActor -module-name ClientWithMacro -I %t %t/src/ClientWithMacro.swift
+
+// REQUIRES: swift_swift_parser, executable_test
+
+//--- macro_definitions.swift
+import SwiftOperators
+@_spi(ExperimentalLanguageFeatures) import SwiftSyntax
+import SwiftSyntaxBuilder
+@_spi(ExperimentalLanguageFeature) import SwiftSyntaxMacros
+
+public struct AddConformanceMacro: ExtensionMacro {
+  public static func expansion(
+    of node: AttributeSyntax,
+    attachedTo: some DeclGroupSyntax,
+    providingExtensionsOf type: some TypeSyntaxProtocol,
+    conformingTo protocols: [TypeSyntax],
+    in context: some MacroExpansionContext
+  ) throws -> [ExtensionDeclSyntax] {
+    let conformance: DeclSyntax =
+      """
+      extension \(type.trimmed): Q {}
+      """
+
+    guard let extensionDecl = conformance.as(ExtensionDeclSyntax.self) else {
+      return []
+    }
+
+    return [extensionDecl]
+  }
+}
+
+//--- Lib.swift
+public protocol Other {
+}
+
+public protocol P: Sendable {
+}
+
+public protocol Q: P, Other {
+}
+
+public protocol W: Q {
+}
+
+@attached(extension, conformances: Q)
+public macro AddQ() = #externalMacro(module: "MacroDefinition", type: "AddConformanceMacro")
+
+//--- ClientWithMacro.swift
+import Lib
+
+@AddQ
+struct S: W {
+  var x: Int // Ok (no errors about conformance isolation)
+}
+
+//--- Client.swift
+import Lib
+
+struct S: P {
+}
+
+extension S: Q { // Ok (no errors about conformance isolation)
+}
