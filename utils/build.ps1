371f2778a0f0acd1420d083aae737d90a5a1c552
Finagolfin <finagolfin@tuta.io>
2026-01-22 00:16:28 +0530

    [test] Fix building Android tests on Windows toolchain CI
    Ever since #84574 added Android availability versioning, the Reflection test stopped
    building for Android unless the API level was in the target triple, so we hacked that
    into CMake in that pull, but the Windows CI does not pass in `SWIFT_ANDROID_API_LEVEL`,
    like `build-script` does, so pass it in here when testing Android.

--- utils/build.ps1
+++ utils/build.ps1
@@ -2811,6 +2811,13 @@ function Test-Runtime([Hashtable] $Platform) {
     throw "LIT test utilities not found in $CompilersBinaryCache\bin"
   }
 
+  $PlatformDefines = @{}
+  if ($Platform.OS -eq [OS]::Android) {
+    $PlatformDefines += @{
+      SWIFT_ANDROID_API_LEVEL = "$AndroidAPILevel";
+    }
+  }
+
   Invoke-IsolatingEnvVars {
     # Filter known issues when testing on Windows
     Load-LitTestOverrides $PSScriptRoot/windows-swift-android-lit-test-overrides.txt
@@ -2822,13 +2829,13 @@ function Test-Runtime([Hashtable] $Platform) {
       -UseBuiltCompilers C,CXX,Swift `
       -SwiftSDK $null `
       -BuildTargets check-swift-validation-only_non_executable `
-      -Defines @{
+      -Defines ($PlatformDefines + @{
         SWIFT_INCLUDE_TESTS = "YES";
         SWIFT_INCLUDE_TEST_BINARIES = "YES";
         SWIFT_BUILD_TEST_SUPPORT_MODULES = "YES";
         SWIFT_NATIVE_LLVM_TOOLS_PATH = Join-Path -Path $CompilersBinaryCache -ChildPath "bin";
         LLVM_LIT_ARGS = "-vv";
-      }
+      })
   }
 }
 
