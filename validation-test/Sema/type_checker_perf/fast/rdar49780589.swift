4d6ff92fbad3088e0cb0feca867fa1515e3942ec
Slava Pestov <spestov@apple.com>
2026-01-22 10:14:35 -0500

    Sema: Add more fast and slow type checker performance test cases

---
+++ validation-test/Sema/type_checker_perf/fast/rdar49780589.swift
@@ -0,0 +1,13 @@
+// RUN: %target-typecheck-verify-swift -solver-scope-threshold=1000
+
+// REQUIRES: objc_interop
+
+import simd
+
+var decelerationFactor: Float = 0.998
+
+func initialVelocity_compilesSlowly(forTargetOffset targetOffset: float3, fromCurrentOffset currentOffset: float3) -> float3 {
+    let k = decelerationFactor
+    let v = (targetOffset - currentOffset) * (Float(1) - k) / k * Float(1000)
+    return v
+}
