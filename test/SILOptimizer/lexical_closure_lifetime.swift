384946d30907af70a67d63177625da8300347032
Erik Eckstein <eeckstein@apple.com>
2026-01-22 11:33:50 +0100

    MandatoryInlining: fix handling of inlined lexical closures
    If the result of the `partial_apply` is lexical we must make sure to make its captured arguments lexical.
    Otherwise the argument lifetimes may be shortened after inlining because the (lexical) `partial_apply` is not there anymore to keep the arguments alive.
    
    Fixes a mis-compile
    rdar://168337959

---
+++ test/SILOptimizer/lexical_closure_lifetime.swift
@@ -0,0 +1,35 @@
+// RUN: %target-run-simple-swift | %FileCheck %s
+
+// REQUIRES: executable_test
+
+class C {}
+
+func runit<T>(_ c: () -> T) -> T {
+  return c()
+}
+
+func testit() -> C? {
+    weak var weakRef: C? = nil
+
+    let resultsHandler = runit {
+        let c = C()
+        weakRef = c
+
+        let x: () -> () = {
+            _ = c
+        }
+        return x
+    }
+
+    resultsHandler()
+
+    return weakRef
+}
+
+if testit() == nil {
+  fatalError()
+}
+
+// CHECK: success
+print("success")
+
