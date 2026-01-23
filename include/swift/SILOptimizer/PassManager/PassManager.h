58c46dc025c671afbec218c321678d72e454d5fa
Erik Eckstein <eeckstein@apple.com>
2026-01-23 19:19:14 +0100

    PassManager: add an option `-sil-print-pass-md5` to print MD5 hashes of SIL after each pass
    With this option an MD5 hash of the SIL is printed after each function or module pass - if the pass has changed the SIL.
    This is useful for finding non-determinisms in the optimizer.

--- include/swift/SILOptimizer/PassManager/PassManager.h
+++ include/swift/SILOptimizer/PassManager/PassManager.h
@@ -454,11 +454,11 @@ private:
 
   /// Dumps information about the pass with index \p TransIdx to llvm::dbgs().
   void dumpPassInfo(const char *Title, SILTransform *Tr, SILFunction *F,
-                    int passIdx = -1);
+                    int passIdx = -1, bool skipNewline = false);
 
   /// Dumps information about the pass with index \p TransIdx to llvm::dbgs().
   void dumpPassInfo(const char *Title, unsigned TransIdx,
-                    SILFunction *F = nullptr);
+                    SILFunction *F = nullptr, bool skipNewline = false);
 
   /// Displays the call graph in an external dot-viewer.
   /// This function is meant for use from the debugger.
