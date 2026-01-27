623f36b472b1319e759d58d74bb30f5d737de746
Slava Pestov <spestov@apple.com>
2026-01-27 00:07:41 +0000

    Fix test/Generics/Inputs/conditional_conformances_invisible_witness_other.swift
    - Fixes rdar://168942655

--- test/Generics/Inputs/conditional_conformances_invisible_witness_other.swift
+++ test/Generics/Inputs/conditional_conformances_invisible_witness_other.swift
@@ -6,10 +6,8 @@ internal protocol Q {}
 
 extension Q {
   public var foo: String { "" }
-  // expected-note@-1 {{mark the property as 'public' to satisfy the requirement}}
 }
 
 extension Optional: Q {}
 
 extension Optional: P where Wrapped: P {}
-// expected-warning@-1 {{property 'foo' must be as accessible as its enclosing type because it matches a requirement in protocol 'P'}}
