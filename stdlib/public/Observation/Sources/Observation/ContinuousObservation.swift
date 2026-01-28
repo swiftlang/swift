6793d97c0dfb4ef144fa060a7b7e9eb8b88c53dd
Philippe Hausler <phausler@gmail.com>
2026-01-28 10:51:15 -0800

    Add missing import and preamble to continuous observation

--- stdlib/public/Observation/Sources/Observation/ContinuousObservation.swift
+++ stdlib/public/Observation/Sources/Observation/ContinuousObservation.swift
@@ -1,3 +1,16 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2026 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+//
+//===----------------------------------------------------------------------===//
+
+import _Concurrency
+
 @available(SwiftStdlib 6.4, *)
 public func withContinuousObservation(
   options: ObservationTracking.Options,
