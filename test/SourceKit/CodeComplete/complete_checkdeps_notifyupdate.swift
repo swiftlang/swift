5dea047d5e64e1790c91376a90ecfed9928a16a8
Ramon Asuncion <asuncionbatista@gmail.com>
2026-02-02 11:21:20 -0500

    [Test][SourceKit] Use lit DEFINE directive for shared compiler args

--- test/SourceKit/CodeComplete/complete_checkdeps_notifyupdate.swift
+++ test/SourceKit/CodeComplete/complete_checkdeps_notifyupdate.swift
@@ -11,34 +11,32 @@ func foo() {
 // RUN: %empty-directory(%t/Frameworks)
 // RUN: %empty-directory(%t/MyProject)
 
-// RUN: COMPILER_ARGS=( \
-// RUN:   -target %target-triple \
-// RUN:   -module-name MyProject \
-// RUN:   -F %t/Frameworks \
-// RUN:   -I %t/MyProject \
-// RUN:   -import-objc-header %t/MyProject/Bridging.h \
-// RUN:   %t/MyProject/Library.swift \
-// RUN:   %s \
-// RUN: )
-// RUN: INPUT_DIR=%S/Inputs/checkdeps
+// DEFINE: %{args} = \
+// DEFINE:   -target %target-triple \
+// DEFINE:   -module-name MyProject \
+// DEFINE:   -F %t/Frameworks \
+// DEFINE:   -I %t/MyProject \
+// DEFINE:   -import-objc-header %t/MyProject/Bridging.h \
+// DEFINE:   %t/MyProject/Library.swift \
+// DEFINE:   %s
 
-// RUN: cp -R $INPUT_DIR/MyProject %t/
-// RUN: cp -R $INPUT_DIR/ClangFW.framework %t/Frameworks/
+// RUN: cp -R %S/Inputs/checkdeps/MyProject %t/
+// RUN: cp -R %S/Inputs/checkdeps/ClangFW.framework %t/Frameworks/
 // RUN: %empty-directory(%t/Frameworks/SwiftFW.framework/Modules/SwiftFW.swiftmodule)
-// RUN: %target-swift-frontend -emit-module -module-name SwiftFW -o %t/Frameworks/SwiftFW.framework/Modules/SwiftFW.swiftmodule/%target-swiftmodule-name $INPUT_DIR/SwiftFW_src/Funcs.swift
+// RUN: %target-swift-frontend -emit-module -module-name SwiftFW -o %t/Frameworks/SwiftFW.framework/Modules/SwiftFW.swiftmodule/%target-swiftmodule-name %S/Inputs/checkdeps/SwiftFW_src/Funcs.swift
 
 // RUN: %sourcekitd-test \
 // RUN:   -shell -- echo "### Initial" == \
-// RUN:   -req=complete -pos=5:3 %s -- ${COMPILER_ARGS[@]} == \
+// RUN:   -req=complete -pos=5:3 %s -- %{args} == \
 
 // RUN:   -shell -- echo '### Modify framework' == \
-// RUN:   -shell -- %target-swift-frontend -emit-module -module-name SwiftFW -o %t/Frameworks/SwiftFW.framework/Modules/SwiftFW.swiftmodule/%target-swiftmodule-name $INPUT_DIR/SwiftFW_src_mod/Funcs.swift == \
-// RUN:   -req=dependency-updated -- ${COMPILER_ARGS[@]} == \
-// RUN:   -req=complete -pos=5:3 %s -- ${COMPILER_ARGS[@]} == \
+// RUN:   -shell -- %target-swift-frontend -emit-module -module-name SwiftFW -o %t/Frameworks/SwiftFW.framework/Modules/SwiftFW.swiftmodule/%target-swiftmodule-name %S/Inputs/checkdeps/SwiftFW_src_mod/Funcs.swift == \
+// RUN:   -req=dependency-updated -- %{args} == \
+// RUN:   -req=complete -pos=5:3 %s -- %{args} == \
 
 // RUN:   -shell -- echo '### Notify without modifying' == \
-// RUN:   -req=dependency-updated -- ${COMPILER_ARGS[@]} == \
-// RUN:   -req=complete -pos=5:3 %s -- ${COMPILER_ARGS[@]} \
+// RUN:   -req=dependency-updated -- %{args} == \
+// RUN:   -req=complete -pos=5:3 %s -- %{args} \
 
 // RUN:   | %FileCheck %s
 
--- test/SourceKit/CodeComplete/complete_checkdeps_swiftmodule.swift
+++ test/SourceKit/CodeComplete/complete_checkdeps_swiftmodule.swift
@@ -12,37 +12,35 @@ func foo() {
 // RUN: %empty-directory(%t/Frameworks)
 // RUN: %empty-directory(%t/MyProject)
 
-// RUN: COMPILER_ARGS=( \
-// RUN:   -target %target-triple \
-// RUN:   -module-name MyProject \
-// RUN:   -F %t/Frameworks \
-// RUN:   -I %t/MyProject \
-// RUN:   -import-objc-header %t/MyProject/Bridging.h \
-// RUN:   %t/MyProject/Library.swift \
-// RUN:   %s \
-// RUN: )
-// RUN: INPUT_DIR=%S/Inputs/checkdeps
+// DEFINE: %{args} = \
+// DEFINE:   -target %target-triple \
+// DEFINE:   -module-name MyProject \
+// DEFINE:   -F %t/Frameworks \
+// DEFINE:   -I %t/MyProject \
+// DEFINE:   -import-objc-header %t/MyProject/Bridging.h \
+// DEFINE:   %t/MyProject/Library.swift \
+// DEFINE:   %s
 
-// RUN: cp -R $INPUT_DIR/MyProject %t/
-// RUN: cp -R $INPUT_DIR/ClangFW.framework %t/Frameworks/
+// RUN: cp -R %S/Inputs/checkdeps/MyProject %t/
+// RUN: cp -R %S/Inputs/checkdeps/ClangFW.framework %t/Frameworks/
 // RUN: %empty-directory(%t/Frameworks/SwiftFW.framework/Modules/SwiftFW.swiftmodule)
-// RUN: %target-swift-frontend -emit-module -module-name SwiftFW -o %t/Frameworks/SwiftFW.framework/Modules/SwiftFW.swiftmodule/%target-swiftmodule-name $INPUT_DIR/SwiftFW_src/Funcs.swift
+// RUN: %target-swift-frontend -emit-module -module-name SwiftFW -o %t/Frameworks/SwiftFW.framework/Modules/SwiftFW.swiftmodule/%target-swiftmodule-name %S/Inputs/checkdeps/SwiftFW_src/Funcs.swift
 // RUN: touch -t 202001010101 %t/Frameworks/SwiftFW.framework/Modules/SwiftFW.swiftmodule/%target-swiftmodule-name
 
 // RUN: %sourcekitd-test \
 // RUN:   -req=global-config -req-opts=completion_check_dependency_interval=0 == \
 
 // RUN:   -shell -- echo "### Initial" == \
-// RUN:   -req=complete -pos=5:3 %s -- ${COMPILER_ARGS[@]} == \
+// RUN:   -req=complete -pos=5:3 %s -- %{args} == \
 
 // RUN:   -shell -- echo '### Modify framework (s)' == \
-// RUN:   -shell --  %target-swift-frontend -emit-module -module-name SwiftFW -o %t/Frameworks/SwiftFW.framework/Modules/SwiftFW.swiftmodule/%target-swiftmodule-name $INPUT_DIR/SwiftFW_src_mod/Funcs.swift == \
+// RUN:   -shell --  %target-swift-frontend -emit-module -module-name SwiftFW -o %t/Frameworks/SwiftFW.framework/Modules/SwiftFW.swiftmodule/%target-swiftmodule-name %S/Inputs/checkdeps/SwiftFW_src_mod/Funcs.swift == \
 // RUN:    -shell -- touch -t 210001010101 %t/Frameworks/SwiftFW.framework/Modules/SwiftFW.swiftmodule/%target-swiftmodule-name == \
-// RUN:   -req=complete -pos=5:3 %s -- ${COMPILER_ARGS[@]} == \
+// RUN:   -req=complete -pos=5:3 %s -- %{args} == \
 
 // RUN:   -shell -- echo '### Fast completion' == \
 // RUN:   -shell -- touch -t 202001010101 %t/Frameworks/SwiftFW.framework/Modules/SwiftFW.swiftmodule/%target-swiftmodule-name == \
-// RUN:   -req=complete -pos=5:3 %s -- ${COMPILER_ARGS[@]} \
+// RUN:   -req=complete -pos=5:3 %s -- %{args} \
 
 // RUN:   | %FileCheck %s
 
