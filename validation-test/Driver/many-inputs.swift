eef2ac5b099763f2b156c0b70a312bec3c7f02c9
Ramon Asuncion <asuncionbatista@gmail.com>
2026-02-02 22:41:09 -0500

    [Test][Driver] Replace bash for loop with Python for internal shell

--- validation-test/Driver/many-inputs.swift
+++ validation-test/Driver/many-inputs.swift
@@ -1,20 +1,19 @@
-// REQUIRES: shell
 // RUN: %empty-directory(%t)
 
 // This limit was chosen because multi-threaded compilation broke here on OS X
 // at one point.
-// RUN: for i in {1..1100}; do echo "public func foo$i() {}" > %t/$i.swift; echo "CHECK: foo$i" >> %t/check.txt; done
+// RUN: %{python} -c "for i in range(1, 1101): open(f'%t/{i}.swift', 'w').write(f'public func foo{i}() {{}}\n'); open(f'%t/check.txt', 'a').write(f'CHECK: foo{i}\n')"
 
-// RUN: (cd %t && %target-build-swift -whole-module-optimization -emit-library ./*.swift -o ./libWMO)
+// RUN: cd %t && %target-build-swift -whole-module-optimization -emit-library ./*.swift -o ./libWMO
 // RUN: nm %t/libWMO | %FileCheck %t/check.txt
 
-// RUN: (cd %t && %target-build-swift -whole-module-optimization -num-threads 1 -emit-library ./*.swift -o ./libWMOThreaded)
+// RUN: cd %t && %target-build-swift -whole-module-optimization -num-threads 1 -emit-library ./*.swift -o ./libWMOThreaded
 // RUN: nm %t/libWMOThreaded | %FileCheck %t/check.txt
 
 // This is very slow due to process overhead. It's also doing one file at a time
 // because we don't have a good way for lit tests to claim more than one thread.
 // But it's still important to check.
-// RUN: (cd %t && %target-build-swift -emit-library ./*.swift -o ./libMultiFile)
+// RUN: cd %t && %target-build-swift -emit-library ./*.swift -o ./libMultiFile
 // RUN: nm %t/libMultiFile | %FileCheck %t/check.txt
 
 // REQUIRES: long_test
