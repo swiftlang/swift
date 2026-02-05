swiftlang/swift

commit c2952b77aa6771ecc26d3753805b10000bba04c9

[cxx-interop] Synthesize .pointee and .successor() on-demand
.pointee is a computed variable whose getter and setter are synthesized
from operator*() (dereference).

.successor() is a method that is synthesized from operator++()
(prefix increment).

They are currently created at the end of importing each StructDecl.
That relies on importing operators eagerly, which can lead to template
over-instantiation issues, and is also extra work that we should try to
avoid.

Also, .pointee picks getters and setters based on the last-seen
operator*() overload, which is an unintuitive programming model and also
especially error-prone when matters of inheritance are involved.

We should teach ClangImporter to look up and synthesize these members
on-demand, rather than relying on synthesizing these eagerly.

rdar://167360692

lib/ClangImporter/ImportDecl.cpp
@@ -70,6 +70,7 @@
 #include "clang/AST/RecursiveASTVisitor.h"
 #include "clang/AST/StmtVisitor.h"
 #include "clang/AST/Type.h"
+#include "clang/Basic/OperatorKinds.h"
 #include "clang/Basic/Specifiers.h"
 #include "clang/Basic/TargetInfo.h"
 #include "clang/Lex/Preprocessor.h"
@@ -86,6 +87,7 @@
 #include "llvm/ADT/StringSwitch.h"
 #include "llvm/ADT/TinyPtrVector.h"
 #include "llvm/Support/Casting.h"
+#include "llvm/Support/ErrorHandling.h"
 #include "llvm/Support/Path.h"
 
 #include <algorithm>
@@ -2808,27 +2810,6 @@ namespace {
             Impl.addAlternateDecl(subscriptImpl, subscript);
           }
         }
-
-        auto getterAndSetterIt = Impl.cxxDereferenceOperators.find(result);
-        if (getterAndSetterIt != Impl.cxxDereferenceOperators.end()) {
-          // If this type has a dereference operator, synthesize a computed
-          // property called `pointee` for it.
-          auto getterAndSetter = getterAndSetterIt->second;
-
-          VarDecl *pointeeProperty =
-              synthesizer.makeDereferencedPointeeProperty(
-                  getterAndSetter.first, getterAndSetter.second);
-
-          // Import the attributes from clang decl of dereference operator to
-          // synthesized pointee property.
-          FuncDecl *getterOrSetterImpl = getterAndSetter.first
-                                             ? getterAndSetter.first
-                                             : getterAndSetter.second;
-          Impl.importAttributesFromClangDeclToSynthesizedSwiftDecl(
-              getterOrSetterImpl, pointeeProperty);
-
-          result->addMember(pointeeProperty);
-        }
       }
 
       if (auto classDecl = dyn_cast<ClassDecl>(result)) {
@@ -3742,9 +3723,9 @@ namespace {
     }
 
     /// Handles special functions such as subscripts and dereference operators.
-    bool
-    processSpecialImportedFunc(FuncDecl *func, ImportedName importedName,
-                               clang::OverloadedOperatorKind cxxOperatorKind) {
+    bool processSpecialImportedFunc(FuncDecl *func, ImportedName importedName,
+                                    const clang::FunctionDecl *clangFunc) {
+      auto cxxOperatorKind = clangFunc->getOverloadedOperator();
       if (cxxOperatorKind == clang::OverloadedOperatorKind::OO_None)
         return true;
       // If this operator was renamed via swift_name attribute, the imported
@@ -3795,50 +3776,36 @@ namespace {
         return true;
       }
 
-      if (importedName.isDereferenceAccessor()) {
-        auto &getterAndSetter = Impl.cxxDereferenceOperators[typeDecl];
-
-        switch (importedName.getAccessorKind()) {
-        case ImportedAccessorKind::DereferenceGetter:
-          getterAndSetter.first = func;
-          break;
-        case ImportedAccessorKind::DereferenceSetter:
-          getterAndSetter.second = func;
-          break;
-        default:
-          llvm_unreachable("invalid dereference operator kind");
-        }
-
-        Impl.markUnavailable(func, "use .pointee property");
-        return true;
-      }
-
-      if (cxxOperatorKind == clang::OverloadedOperatorKind::OO_PlusPlus) {
-        // Make sure the type is not a foreign reference type.
-        // We cannot handle `operator++` for those types, since the
-        // current implementation creates a new instance of the type.
-        if (func->getParameters()->size() == 0 && !isa<ClassDecl>(typeDecl)) {
-          // This is a pre-increment operator. We synthesize a
-          // non-mutating function called `successor() -> Self`.
-          FuncDecl *successorFunc = synthesizer.makeSuccessorFunc(func);
-
-          // Import the clang decl attributes to synthesized successor function.
-          Impl.importAttributesFromClangDeclToSynthesizedSwiftDecl(func, successorFunc);
-
-          typeDecl->addMember(successorFunc);
-
-          Impl.markUnavailable(func, "use .successor()");
-        } else {
-          Impl.markUnavailable(func, "unable to create .successor() func");
-        }
-        func->overwriteAccess(AccessLevel::Private);
-        return true;
-      }
-
       // Check if this method _is_ an overloaded operator but is not a
       // call / subscript / dereference / increment. Those
       // operators do not need static versions.
-      if (cxxOperatorKind != clang::OverloadedOperatorKind::OO_Call) {
+      switch (cxxOperatorKind) {
+      case clang::OverloadedOperatorKind::OO_None:
+        llvm_unreachable("should only be handling operators at this point");
+      case clang::OverloadedOperatorKind::OO_PlusPlus:
+        if (clangFunc->getMinRequiredArguments() != 0)
+          // Do not allow post-increment to be used from Swift
+          Impl.markUnavailable(func, "unable to create .successor() func");
+        else if (auto *method = dyn_cast<clang::CXXMethodDecl>(clangFunc);
+                 method && method->getRefQualifier() == clang::RQ_RValue)
+          // TODO: we shouldn't have to handle this case. We only mark it as
+          // unavailable for now to preserve old behavior, where r-value-this
+          // operator ++ overloads are always unavailable.
+          Impl.markUnavailable(func, "use .successor()");
+        break;
+
+      case clang::OverloadedOperatorKind::OO_Star:
+        if (auto *method = dyn_cast<clang::CXXMethodDecl>(clangFunc);
+            method && method->getRefQualifier() == clang::RQ_RValue)
+          // TODO: we shouldn't have to handle this case. We only mark it as
+          // unavailable for now to preserve old behavior, where r-value-this
+          // operator * overloads are always unavailable.
+          Impl.markUnavailable(func, "use .pointee property");
+        break;
+      case clang::OverloadedOperatorKind::OO_Call:
+      case clang::OverloadedOperatorKind::OO_Subscript:
+        break;
+      default:
         auto opFuncDecl = synthesizer.makeOperator(func, cxxOperatorKind);
         Impl.addAlternateDecl(func, opFuncDecl);
 
@@ -4186,8 +4153,7 @@ namespace {
         }
         func->setAccess(importer::convertClangAccess(decl->getAccess()));
 
-        bool success = processSpecialImportedFunc(
-            func, importedName, decl->getOverloadedOperator());
+        bool success = processSpecialImportedFunc(func, importedName, decl);
         if (!success)
           return nullptr;
       }
@@ -5022,8 +4988,8 @@ namespace {
         clonedMethod->overwriteAccess(
             importer::convertClangAccess(decl->getAccess()));
 
-        bool success = processSpecialImportedFunc(
-            clonedMethod, importedName, targetMethod->getOverloadedOperator());
+        bool success = processSpecialImportedFunc(clonedMethod, importedName,
+                                                  targetMethod);
         if (!success)
           return nullptr;
 
@@ -9423,17 +9389,6 @@ static void filterUsableVersionedAttrs(
   }
 }
 
-void ClangImporter::Implementation::importAttributesFromClangDeclToSynthesizedSwiftDecl(Decl *sourceDecl, Decl* synthesizedDecl)
-{
-  // sourceDecl->getClangDecl() can be null because some lazily instantiated cases like C++ members that were instantiated from using-shadow-decls have no corresponding Clang decl.
-  // FIXME: Need to include the cases where correspondoing clang decl is not present.
-  if (auto clangDeclForSource =
-          dyn_cast_or_null<clang::NamedDecl>(
-              sourceDecl->getClangDecl())) {
-    importAttributes(clangDeclForSource, synthesizedDecl);
-  }
-}
-
 /// Import Clang attributes as Swift attributes.
 void ClangImporter::Implementation::importAttributes(
     const clang::NamedDecl *ClangDecl,
@@ -10652,6 +10607,11 @@ void ClangImporter::Implementation::loadAllMembersOfRecordDecl(
       auto baseInheritance = ClangInheritanceInfo(inheritance, base);
       loadAllMembersOfRecordDecl(swiftDecl, baseRecord, baseInheritance);
     }
+  }
+
+  if ((isa<clang::CXXRecordDecl>(swiftDecl->getClangDecl())) && !inheritance) {
+    lookupAndImportPointee(swiftDecl);
+    lookupAndImportSuccessor(swiftDecl);
   }
 }
 

test/Interop/Cxx/operators/Inputs/member-inline.h
@@ -536,7 +536,7 @@ struct ClassWithOperatorStarAvailable {
   int value;
 
 public:
-  int &operator*() { return value; }
+  const int &operator*() const { return value; }
 };
 
 struct DerivedClassWithOperatorStarAvailable : ClassWithOperatorStarAvailable {
@@ -546,7 +546,8 @@ struct ClassWithOperatorStarUnavailable {
   int value;
 
 public:
-  int &operator*() __attribute__((availability(swift, unavailable))) {
+  const int &operator*() const
+      __attribute__((availability(swift, unavailable))) {
     return value;
   }
 };

test/Interop/Cxx/operators/member-inline-module-interface.swift
@@ -1,14 +1,12 @@
-// RUN: %target-swift-ide-test -print-module -module-to-print=MemberInline -I %S/Inputs -source-filename=x -cxx-interoperability-mode=swift-5.9 -Xcc -std=c++23 | %FileCheck %s
-// RUN: %target-swift-ide-test -print-module -module-to-print=MemberInline -I %S/Inputs -source-filename=x -cxx-interoperability-mode=swift-6 -Xcc -std=c++23 | %FileCheck %s
-// RUN: %target-swift-ide-test -print-module -module-to-print=MemberInline -I %S/Inputs -source-filename=x -cxx-interoperability-mode=upcoming-swift -Xcc -std=c++23 | %FileCheck %s
+// RUN: %target-swift-ide-test -print-module -module-to-print=MemberInline -I %S/Inputs -source-filename=x -cxx-interoperability-mode=default -Xcc -std=c++23 | %FileCheck %s
 
 // CHECK: struct LoadableIntWrapper {
-// CHECK:   func successor() -> LoadableIntWrapper
 // CHECK:   static func - (lhs: inout LoadableIntWrapper, rhs: LoadableIntWrapper) -> LoadableIntWrapper
 // CHECK:   static func += (lhs: inout LoadableIntWrapper, rhs: LoadableIntWrapper)
 // CHECK:   mutating func callAsFunction() -> Int32
 // CHECK:   mutating func callAsFunction(_ x: Int32) -> Int32
 // CHECK:   mutating func callAsFunction(_ x: Int32, _ y: Int32) -> Int32
+// CHECK:   func successor() -> LoadableIntWrapper
 // CHECK: }
 // CHECK: func == (lhs: LoadableIntWrapper, rhs: LoadableIntWrapper) -> Bool
 // CHECK: func -= (lhs: inout LoadableIntWrapper, rhs: LoadableIntWrapper)
@@ -234,36 +232,35 @@
 // CHECK: }
 
 // CHECK: struct Iterator {
-// CHECK:   var pointee: Int32 { mutating get set }
 // CHECK:   @available(*, unavailable, message: "use .pointee property")
 // CHECK:   mutating func __operatorStar() -> UnsafeMutablePointer<Int32>
+// CHECK:   var pointee: Int32 { mutating get set }
 // CHECK: }
 
 // CHECK: struct ConstIterator {
-// CHECK:   var pointee: Int32 { get }
 // CHECK:   @available(*, unavailable, message: "use .pointee property")
 // CHECK:   func __operatorStar() -> UnsafePointer<Int32>
+// CHECK:   var pointee: Int32 { get }
 // CHECK: }
 
 // CHECK: struct ConstIteratorByVal {
-// CHECK:   var pointee: Int32 { get }
 // CHECK:   @available(*, unavailable, message: "use .pointee property")
 // CHECK:   func __operatorStar() -> Int32
+// CHECK:   var pointee: Int32 { get }
 // CHECK: }
 
 // CHECK: struct AmbiguousOperatorStar {
 // CHECK-NEXT:   init()
-// CHECK-NEXT:   var pointee: Int32
 // CHECK-NEXT:   @available(*, unavailable, message: "use .pointee property")
 // CHECK-NEXT:   mutating func __operatorStar() -> UnsafeMutablePointer<Int32>
 // CHECK-NEXT:   @available(*, unavailable, message: "use .pointee property")
 // CHECK-NEXT:   func __operatorStar() -> UnsafePointer<Int32>
 // CHECK-NEXT:   var value: Int32
+// CHECK-NEXT:   var pointee: Int32
 // CHECK-NEXT: }
 
 // CHECK: struct AmbiguousOperatorStar2 {
 // CHECK-NEXT:   init()
-// CHECK-NEXT:   var pointee: Int32
 // CHECK-NEXT:   @available(*, unavailable, message: "use .pointee property")
 // CHECK-NEXT:   mutating func __operatorStar() -> UnsafeMutablePointer<Int32>
 // CHECK-NEXT:   @available(*, unavailable, message: "use .pointee property")
@@ -271,29 +268,30 @@
 // CHECK-NEXT:   @available(*, unavailable, message: "use .pointee property")
 // CHECK-NEXT:   func __operatorStar() -> UnsafePointer<Int32>
 // CHECK-NEXT:   var value: Int32
+// CHECK-NEXT:   var pointee: Int32
 // CHECK-NEXT: }
 
 // CHECK: struct DerivedFromConstIterator {
 // CHECK-NEXT:   init()
-// TODO:   @available(*, unavailable, message: "use .pointee property")
+// CHECK-NEXT:   @available(*, unavailable, message: "use .pointee property")
 // CHECK-NEXT:   func __operatorStar() -> UnsafePointer<Int32>
-// TODO: `var pointee` should be printed here
-// CHECK: }
+// CHECK-NEXT:   var pointee: Int32 { get }
+// CHECK-NEXT: }
 
 // CHECK: struct DerivedFromConstIteratorPrivatelyWithUsingDecl {
 // CHECK-NEXT:   init()
-// CHECK-NEXT:   var pointee: Int32 { get }
 // CHECK-NEXT:   @available(*, unavailable, message: "use .pointee property")
 // CHECK-NEXT:   func __operatorStar() -> UnsafePointer<Int32>
+// CHECK-NEXT:   var pointee: Int32 { get }
 // CHECK: }
 
 // CHECK: struct DerivedFromAmbiguousOperatorStarPrivatelyWithUsingDecl {
 // CHECK-NEXT:   init()
-// CHECK-NEXT:   var pointee: Int32
 // CHECK-NEXT:   @available(*, unavailable, message: "use .pointee property")
 // CHECK-NEXT:   mutating func __operatorStar() -> UnsafeMutablePointer<Int32>
 // CHECK-NEXT:   @available(*, unavailable, message: "use .pointee property")
 // CHECK-NEXT:   func __operatorStar() -> UnsafePointer<Int32>
+// CHECK-NEXT:   var pointee: Int32
 // CHECK: }
 
 // CHECK: struct DerivedFromLoadableIntWrapperWithUsingDecl {

test/Interop/Cxx/operators/member-inline-typechecker.swift
@@ -71,8 +71,7 @@ let voidReturnTypeResult: HasPreIncrementOperatorWithVoidReturnType = voidReturn
 let immortalIncrement = myCounter.successor() // expected-error {{value of type 'ImmortalCounter' has no member 'successor'}}
 
 let derivedConstIter = DerivedFromConstIteratorPrivately()
-derivedConstIter.pointee // expected-error {{value of type 'DerivedFromConstIteratorPrivately' has no member 'pointee'}}
-// FIXME: inheriting operators is currently flaky. the error should be {{'pointee' is inaccessible due to 'private' protection level}}
+derivedConstIter.pointee // expected-error {{'pointee' is inaccessible due to 'private' protection level}}
 
 let derivedConstIterWithUD = DerivedFromConstIteratorPrivatelyWithUsingDecl()
 let _ = derivedConstIterWithUD.pointee

include/swift/ClangImporter/ClangImporter.h
@@ -959,6 +959,9 @@ public:
   ClangInheritanceInfo(ClangInheritanceInfo prev, clang::CXXBaseSpecifier base)
       : access(computeAccess(prev, base)) {}
 
+  /// Initialize an instance of this class at a particular given access level.
+  ClangInheritanceInfo(clang::AccessSpecifier access) : access(access) {}
+
   /// Whether this info represents a case of nested private inheritance.
   bool isNestedPrivate() const { return !access.has_value(); }
 

lib/ClangImporter/ClangDerivedConformances.cpp
@@ -545,10 +545,9 @@ conformToCxxIteratorIfNeeded(ClangImporter::Implementation &impl,
     }
   }
 
-  // Check if present: `var pointee: Pointee { get }`
-  auto pointeeId = ctx.getIdentifier("pointee");
-  auto pointee = lookupDirectSingleWithoutExtensions<VarDecl>(decl, pointeeId);
-  if (!pointee || pointee->isGetterMutating() || pointee->getTypeInContext()->hasError())
+  auto *pointee = impl.lookupAndImportPointee(decl);
+  if (!pointee || pointee->isGetterMutating() ||
+      pointee->getTypeInContext()->hasError())
     return;
 
   // Check if `var pointee: Pointee` is settable. This is required for the
@@ -556,10 +555,7 @@ conformToCxxIteratorIfNeeded(ClangImporter::Implementation &impl,
   // UnsafeCxxInputIterator.
   bool pointeeSettable = pointee->isSettable(nullptr);
 
-  // Check if present: `func successor() -> Self`
-  auto successorId = ctx.getIdentifier("successor");
-  auto successor =
-      lookupDirectSingleWithoutExtensions<FuncDecl>(decl, successorId);
+  auto *successor = impl.lookupAndImportSuccessor(decl);
   if (!successor || successor->isMutating())
     return;
   auto successorTy = successor->getResultInterfaceType();
@@ -714,6 +710,9 @@ static void conformToCxxOptional(ClangImporter::Implementation &impl,
   auto *Wrapped = dyn_cast_or_null<TypeAliasDecl>(
       impl.importDecl(value_type, impl.CurrentVersion));
   if (!Wrapped)
+    return;
+
+  if (!impl.lookupAndImportPointee(decl))
     return;
 
   impl.addSynthesizedTypealias(decl, ctx.getIdentifier("Wrapped"),

commit 5b124bc54de39a0244866964c0392eb0666021f6

[cxx-interop] Fix confusion between multiply and dereference operators
c2952b7 mistakenly treated the binary C++ operator*() ("multiply") as
the unary operator*() ("dereference"), preventing ClangImporter from
synthesizing the Swift func *(_:_) operator. This patch fixes the issue
and adds some tests to exercise the scenario.

rdar://169448888

lib/ClangImporter/ImportDecl.cpp
@@ -3780,6 +3780,10 @@ namespace {
       switch (cxxOperatorKind) {
       case clang::OverloadedOperatorKind::OO_None:
         llvm_unreachable("should only be handling operators at this point");
+      case clang::OverloadedOperatorKind::OO_Call:
+      case clang::OverloadedOperatorKind::OO_Subscript:
+        break;
+
       case clang::OverloadedOperatorKind::OO_PlusPlus:
         if (clangFunc->getMinRequiredArguments() != 0)
           // Do not allow post-increment to be used from Swift
@@ -3793,16 +3797,20 @@ namespace {
         break;
 
       case clang::OverloadedOperatorKind::OO_Star:
-        if (auto *method = dyn_cast<clang::CXXMethodDecl>(clangFunc);
-            method && method->getRefQualifier() == clang::RQ_RValue)
-          // TODO: we shouldn't have to handle this case. We only mark it as
-          // unavailable for now to preserve old behavior, where r-value-this
-          // operator * overloads are always unavailable.
-          Impl.markUnavailable(func, "use .pointee property");
-        break;
-      case clang::OverloadedOperatorKind::OO_Call:
-      case clang::OverloadedOperatorKind::OO_Subscript:
-        break;
+        if (clangFunc->param_empty()) {
+          // Dereference operator, T operator*()
+          if (auto *method = dyn_cast<clang::CXXMethodDecl>(clangFunc);
+              method && method->getRefQualifier() == clang::RQ_RValue)
+            // TODO: we shouldn't have to handle this case. We only mark it as
+            // unavailable for now to preserve old behavior, where r-value-this
+            // operator * overloads are always unavailable.
+            Impl.markUnavailable(func, "use .pointee property");
+          break;
+        }
+
+        // Multiply operator, T operator*(R rhs) or T operator*(L lhs, R rhs)
+        // which we handle like most other operators
+        LLVM_FALLTHROUGH;
       default:
         auto opFuncDecl = synthesizer.makeOperator(func, cxxOperatorKind);
         Impl.addAlternateDecl(func, opFuncDecl);

test/Interop/Cxx/operators/Inputs/member-inline.h
@@ -431,6 +431,24 @@ public:
   int operator*() const { return value; }
 };
 
+class AllStar {
+private:
+  long L = 111;
+
+public:
+  // The unary variant (i.e., dereference) should be accessible
+  // via the synthesized .pointee member
+  long &operator*() { return L; }
+
+  // The binary variant (i.e., multiply) should be accessible
+  // via the synthesized static func *(lhs, rhs) operator
+  AllStar operator*(const AllStar &rhs) const {
+    AllStar a;
+    a.L = this->L * rhs.L;
+    return a;
+  }
+};
+
 struct AmbiguousOperatorStar {
 private:
   int value = 567;

test/Interop/Cxx/operators/member-inline-module-interface.swift
@@ -249,6 +249,17 @@
 // CHECK:   var pointee: Int32 { get }
 // CHECK: }
 
+// CHECK: struct AllStar {
+// CHECK-NEXT:   init()
+// CHECK-NEXT:   @available(*, unavailable, message: "use .pointee property")
+// CHECK-NEXT:   mutating func __operatorStar() -> UnsafeMutablePointer<Int>
+// CHECK-NEXT:   static func * (lhs: AllStar, rhs: AllStar) -> AllStar
+// CHECK-NEXT:   @available(*, unavailable, message: "use * instead")
+// CHECK-NEXT:   func __operatorStar(_ rhs: AllStar) -> AllStar
+// CHECK-NEXT:   var L: Int
+// CHECK-NEXT:   var pointee: Int { mutating get set }
+// CHECK-NEXT: }
+
 // CHECK: struct AmbiguousOperatorStar {
 // CHECK-NEXT:   init()
 // CHECK-NEXT:   @available(*, unavailable, message: "use .pointee property")

test/Interop/Cxx/operators/member-inline-typechecker.swift
@@ -84,6 +84,13 @@ let _ = classWithSuccessorAvailable.successor();
 let classWithSuccessorUnavailable = ClassWithSuccessorUnavailable()
 let _ = classWithSuccessorUnavailable.successor(); // expected-error {{'successor()' is unavailable in Swift}}
 
+let allStar = AllStar() // expected-note {{change 'let' to 'var' to make it mutable}}
+let _ = allStar.pointee // expected-error {{cannot use mutating getter on immutable value}}
+var mutAllStar = AllStar()
+let _ = mutAllStar.pointee
+var _: AllStar  = mutAllStar * allStar
+var _: AllStar = allStar * mutAllStar
+
 var classWithOperatorStarAvailable = ClassWithOperatorStarAvailable()
 let _ = classWithOperatorStarAvailable.pointee
 let derivedClassWithOperatorStarAvailable = DerivedClassWithOperatorStarAvailable()

test/Interop/Cxx/operators/member-inline.swift
@@ -385,6 +385,22 @@ OperatorsTestSuite.test("ConstIteratorByVal.pointee") {
   expectEqual(456, res)
 }
 
+OperatorsTestSuite.test("AllStar.*") {
+  var a = AllStar()
+  var b = a
+  b.pointee = 9
+
+  expectEqual(111, a.pointee)
+  expectEqual(9, b.pointee)
+
+  var c = a * b
+  c.pointee += 1000
+
+  expectEqual(111, a.pointee)
+  expectEqual(9, b.pointee)
+  expectEqual(1999, c.pointee)
+}
+
 OperatorsTestSuite.test("AmbiguousOperatorStar.pointee") {
   let stars = AmbiguousOperatorStar()
   let res = stars.pointee