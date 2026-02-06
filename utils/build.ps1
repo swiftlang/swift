808fdf649c61fb5b1f8582bbbf2ba8e23db68ec9
Charles Zablit <c_zablit@apple.com>
2026-02-06 15:06:45 +0000

    [windows] add required steps to enable Swift lldb tests

--- utils/build.ps1
+++ utils/build.ps1
@@ -2373,13 +2373,13 @@ function Test-Compilers([Hashtable] $Platform, [string] $Variant, [switch] $Test
       Copy-Item `
         -Path "$RuntimeBinaryCache\bin\swiftCore.dll" `
         -Destination "$(Get-ProjectBinaryCache $BuildPlatform Compilers)\bin"
-
       $TestingDefines += @{
         LLDB_INCLUDE_TESTS = "YES";
         # Check for required Python modules in CMake
         LLDB_ENFORCE_STRICT_TEST_REQUIREMENTS = "YES";
         # No watchpoint support on windows: https://github.com/llvm/llvm-project/issues/24820
-        LLDB_TEST_USER_ARGS = "--skip-category=watchpoint";
+        LLDB_TEST_USER_ARGS = "--skip-category=watchpoint;--sysroot=$(Get-SwiftSDK -OS $Platform.OS -Identifier $Platform.DefaultSDK)";
+        LLDB_TEST_SWIFT_DRIVER_EXTRA_FLAGS = "-sdk $(Get-SwiftSDK -OS $Platform.OS -Identifier $Platform.DefaultSDK)"
         # gtest sharding breaks llvm-lit's --xfail and LIT_XFAIL inputs: https://github.com/llvm/llvm-project/issues/102264
         LLVM_LIT_ARGS = "-v --no-gtest-sharding --time-tests";
         # LLDB Unit tests link against this library
