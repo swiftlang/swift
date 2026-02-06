7acf2b4cdeb79d5d9a39e1b30a2c0c7de0474c35
Anton Korobeynikov <anton@korobeynikov.info>
2026-02-06 12:00:21 -0800

    Factor out coroutine synthesizer from function one to simplify things

--- lib/AST/Builtins.cpp
+++ lib/AST/Builtins.cpp
@@ -1665,7 +1665,7 @@ static ValueDecl *getCreateTask(ASTContext &ctx, Identifier id) {
           _label("taskName", _defaulted(_optional(_rawPointer), _nil)),
           _label("operation",
                  _sending(_function(_async(_throws(_thick)), _typeparam(0),
-                                    _parameters(), _yields())))),
+                                    _parameters())))),
       _tuple(_nativeObject, _rawPointer));
 }
 
@@ -1689,7 +1689,7 @@ static ValueDecl *getCreateDiscardingTask(ASTContext &ctx, Identifier id) {
                             _nil)),
           _label("taskName", _defaulted(_optional(_rawPointer), _nil)),
           _label("operation", _sending(_function(_async(_throws(_thick)), _void,
-                                                 _parameters(), _yields())))),
+                                                 _parameters())))),
       _tuple(_nativeObject, _rawPointer));
 }
 
@@ -1734,7 +1734,7 @@ static ValueDecl *getTaskRunInline(ASTContext &ctx, Identifier id) {
   return getBuiltinFunction(
       ctx, id, _thin, _generics(_unrestricted, _conformsToDefaults(0)),
       _parameters(_function(_async(_noescape(_thick)), _typeparam(0),
-                            _parameters(), _yields())),
+                            _parameters())),
       _typeparam(0));
 }
 
