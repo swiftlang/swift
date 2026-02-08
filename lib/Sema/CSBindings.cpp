afa6496de96d92983c30ae8673b03ffd6f1a9541
Slava Pestov <spestov@apple.com>
2026-02-08 16:02:08 -0500

    Sema: Narrow down BindingSet::isDelayedByDisjunction() to lvalues only

--- lib/Sema/CSBindings.cpp
+++ lib/Sema/CSBindings.cpp
@@ -1711,6 +1711,10 @@ bool swift::constraints::inference::checkTypeOfBinding(
 }
 
 bool BindingSet::isDelayedByDisjunction() const {
+  if (!TypeVar->getImpl().canBindToLValue())
+    return false;
+
+  // Result type of subscript could be l-value so we can't bind it early.
   if (TypeVar->getImpl().isSubscriptResultType())
     return true;
 
@@ -1726,7 +1730,6 @@ bool BindingSet::favoredOverDisjunction(Constraint *disjunction) const {
     return false;
 
   if (!CS.shouldAttemptFixes()) {
-    // Result type of subscript could be l-value so we can't bind it early.
     if (!isDelayedByDisjunction()) {
       if (getNumExactBindings() > 0)
         return true;
