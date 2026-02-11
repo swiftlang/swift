04eec3aba51af2fdcd25a5ae72faf9068c14d23a
Saleem Abdulrasool <compnerd@compnerd.org>
2026-02-11 10:17:58 -0800

    Threading: rename `Win32.cpp` to `WindowsThreading.cpp` (NFC)
    This renames the source file to accomodate static linking of the
    runtime. When `libswiftCore.lib` is linked against, we would encounter
    a linker warning due to the replicated filename:
    ```
    libswiftCore.lib(Win32.cpp.obj) : warning LNK4255: library contain multiple objects of the same name; linking object as if no debug info
    ```

--- stdlib/public/runtime/WindowsUnicode.cpp
+++ stdlib/public/runtime/WindowsUnicode.cpp
@@ -1,4 +1,4 @@
-//===--- Win32.cpp - Win32 utility functions --------------------*- C++ -*-===//
+//===--- WindowsUnicode.cpp - Windows Unicode utility functions -*- C++ -*-===//
 //
 // This source file is part of the Swift.org open source project
 //
