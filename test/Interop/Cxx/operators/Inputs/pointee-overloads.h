swiftlang/swift

commit c2952b77aa6771ecc26d3753805b10000bba04c9

[cxx-interop] Synthesize .pointee and .successor() on-demand
.pointee is a computed variable whose getter and setter are synthesized
from operator*() (dereference).

.successor() is a method that is synthesized from operator++()
(prefix increment).

They are currently created at the end of importing each StructDecl.
That relies on importing operators eagerly, which can lead to template
over-instantiation issues, and is also extra work that we should try to
avoid.

Also, .pointee picks getters and setters based on the last-seen
operator*() overload, which is an unintuitive programming model and also
especially error-prone when matters of inheritance are involved.

We should teach ClangImporter to look up and synthesize these members
on-demand, rather than relying on synthesizing these eagerly.

rdar://167360692

lib/ClangImporter/ClangLookup.cpp
@@ -15,6 +15,7 @@
 //===----------------------------------------------------------------------===//
 
 #include "ImporterImpl.h"
+#include "SwiftDeclSynthesizer.h"
 #include "swift/AST/ASTContext.h"
 #include "swift/AST/Builtins.h"
 #include "swift/AST/ClangModuleLoader.h"
@@ -32,22 +33,30 @@
 #include "swift/AST/Type.h"
 #include "swift/AST/TypeCheckRequests.h"
 #include "swift/AST/Types.h"
+#include "swift/Basic/Defer.h"
 #include "swift/Basic/LLVM.h"
 #include "swift/ClangImporter/ClangImporter.h"
 #include "swift/ClangImporter/ClangImporterRequests.h"
 #include "swift/ClangImporter/ClangModule.h"
 #include "swift/Subsystems.h"
 #include "clang/AST/ASTContext.h"
+#include "clang/AST/CXXInheritance.h"
 #include "clang/AST/Decl.h"
 #include "clang/AST/DeclBase.h"
+#include "clang/AST/DeclCXX.h"
 #include "clang/AST/DeclTemplate.h"
+#include "clang/AST/DeclarationName.h"
 #include "clang/AST/Mangle.h"
 #include "clang/AST/Type.h"
+#include "clang/Basic/Specifiers.h"
 #include "clang/Index/IndexingAction.h"
+#include "clang/Sema/Lookup.h"
+#include "clang/Sema/Sema.h"
 #include "clang/Serialization/ASTReader.h"
 #include "clang/Serialization/ASTWriter.h"
 #include "llvm/ADT/STLExtras.h"
 #include "llvm/ADT/SmallVector.h"
+#include "llvm/ADT/iterator_range.h"
 #include <utility>
 
 using namespace swift;
@@ -224,6 +233,7 @@ TinyPtrVector<ValueDecl *> ClangRecordMemberLookup::evaluate(
   ClangInheritanceInfo inheritance = desc.inheritance;
 
   auto &ctx = recordDecl->getASTContext();
+  auto &Importer = *static_cast<ClangImporter *>(ctx.getClangModuleLoader());
 
   // Whether to skip non-public members. Feature::ImportNonPublicCxxMembers says
   // to import all non-public members by default; if that is disabled, we only
@@ -370,5 +380,272 @@ TinyPtrVector<ValueDecl *> ClangRecordMemberLookup::evaluate(
     }
   }
 
+  if (result.empty() && !inheritance) {
+    if (name.isSimpleName("pointee")) {
+      if (auto *pointee = Importer.Impl.lookupAndImportPointee(inheritingDecl))
+        result.push_back(pointee);
+    } else if (name.isSimpleName("successor")) {
+      if (auto *succ = Importer.Impl.lookupAndImportSuccessor(inheritingDecl))
+        result.push_back(succ);
+    }
+  }
+
   return result;
+}
+
+/// Perform a qualified lookup for \a name in \a Record, suppressing diagnostics
+/// and returning std::nullopt if the lookup was unsuccessful.
+static std::optional<clang::LookupResult>
+lookupCXXMember(clang::Sema &Sema, clang::DeclarationName name,
+                const clang::CXXRecordDecl *Record) {
+  auto R = clang::LookupResult(Sema, name, clang::SourceLocation(),
+                               clang::Sema::LookupMemberName);
+  R.suppressDiagnostics();
+  // NOTE: this will not find free-standing operator decl, i.e. not a member
+  Sema.LookupQualifiedName(R, const_cast<clang::CXXRecordDecl *>(Record));
+
+  switch (R.getResultKind()) {
+  case clang::LookupResultKind::Found:
+  case clang::LookupResultKind::FoundOverloaded:
+    return R;
+  default:
+    return std::nullopt; // Lookup didn't yield any results
+  }
+}
+
+namespace {
+/// Convenience wrapper around clang::LookupResult::iterator that yields
+/// std::pair<NamedDecl *, AccessSpecifier> rather than just NamedDecl *.
+struct ResultIterator
+    : llvm::iterator_adaptor_base<
+          ResultIterator, clang::LookupResult::iterator,
+          clang::LookupResult::iterator::iterator_category,
+          std::pair<const clang::NamedDecl *, clang::AccessSpecifier>> {
+  explicit ResultIterator(const clang::LookupResult::iterator &it) { I = it; }
+  value_type operator*() const {
+    return std::make_pair(I.getDecl(), I.getAccess());
+  }
+};
+} // namespace
+
+/// Creates an iterable range of method decl-access pairs for the results found
+/// in \a R, originating from a qualified lookup in \a Origin.
+///
+/// This helper for the lookupAndImport* functions encapsulates casting the
+/// decls to clang::CXXMethodDecl *s and "looking through" using decls. It also
+/// ensures the access is set to clang::AS_none if the method is not inherited.
+static auto filterMethodOverloads(clang::LookupResult &R,
+                                  const clang::CXXRecordDecl *Origin) {
+  return llvm::make_filter_range(
+      llvm::map_range(
+          llvm::make_range(ResultIterator{R.begin()},
+                           ResultIterator{R.end()}),
+          [=](std::pair<const clang::NamedDecl *, clang::AccessSpecifier> da) {
+            auto [nd, access] = da;
+
+            if (auto *usd = dyn_cast<clang::UsingShadowDecl>(nd))
+              nd = usd->getTargetDecl();
+
+            if (nd->getDeclContext() == Origin)
+              access = clang::AS_none; // not inherited
+
+            auto *md = dyn_cast<clang::CXXMethodDecl>(nd);
+            return std::make_pair(md, access);
+          }),
+      [](std::pair<const clang::CXXMethodDecl *, clang::AccessSpecifier> da) {
+        return da.first;
+      });
+}
+
+/// Imports a C++ \a method to a Swift \a struct as a non-inherited member when
+/// \a access is AS_none, or an inherited member with effective \a access
+/// otherwise. Marks the method as unavailable with \a unavailabilityMsg.
+///
+/// Helper for the lookupAndImport* functions.
+static FuncDecl *importUnavailableMethod(ClangImporter::Implementation &Impl,
+                                         const clang::CXXMethodDecl *method,
+                                         clang::AccessSpecifier access,
+                                         NominalTypeDecl *Struct,
+                                         StringRef unavailabilityMsg) {
+  auto *func =
+      dyn_cast_or_null<FuncDecl>(Impl.importDecl(method, Impl.CurrentVersion));
+  if (!func)
+    return nullptr;
+  if (auto inheritance = ClangInheritanceInfo(access))
+    func = dyn_cast_or_null<FuncDecl>(
+        Impl.importBaseMemberDecl(func, Struct, inheritance));
+  if (!func)
+    return nullptr;
+  Impl.markUnavailable(func, unavailabilityMsg);
+  return func;
+}
+
+VarDecl *
+ClangImporter::Implementation::lookupAndImportPointee(NominalTypeDecl *Struct) {
+  const auto *CXXRecord =
+      dyn_cast<clang::CXXRecordDecl>(Struct->getClangDecl());
+
+  if (!CXXRecord || !isa<StructDecl>(Struct))
+    // Do not synthesize successor() if this is not a C++ record, or if it is
+    // a foreign reference type (successor() needs to copy values of this type),
+    // which would be a ClassDecl rather than a StructDecl.
+    return nullptr;
+
+  if (auto [it, inserted] = importedPointeeCache.try_emplace(Struct, nullptr);
+      !inserted)
+    return it->second;
+
+  // From this point onward, if we encounter some error and return, future calls
+  // to this function will pick up the nullptr we cached using try_emplace. Note
+  // that this may silently suppress unintentional cycles.
+
+  auto &Ctx = getClangASTContext();
+  auto &Sema = getClangSema();
+
+  auto name = Ctx.DeclarationNames.getCXXOperatorName(
+      clang::OverloadedOperatorKind::OO_Star);
+  auto R = lookupCXXMember(Sema, name, CXXRecord);
+  if (!R.has_value())
+    return nullptr;
+
+  auto overloads = filterMethodOverloads(R.value(), CXXRecord);
+
+  const clang::CXXMethodDecl *CXXGetter = nullptr, *CXXSetter = nullptr;
+  clang::AccessSpecifier CXXGetterAccess, CXXSetterAccess;
+
+  for (auto [method, access] : overloads) {
+    if (method->isStatic() || method->isVolatile() ||
+        method->getMinRequiredArguments() != 0 ||
+        method->getRefQualifier() == clang::RQ_RValue)
+      continue;
+
+    // A setter is anything that returns a mutable reference; anything else is
+    // a getter.
+    auto retTy = method->getReturnType();
+    bool isSetter =
+        retTy->isReferenceType() && !retTy->getPointeeType().isConstQualified();
+
+    if (isSetter) {
+      if (!CXXSetter) {
+        CXXSetter = method;
+        CXXSetterAccess = access;
+      } else if (!CXXSetter->isConst() && method->isConst()) {
+        // Previously found a non-const setter, but prefer a const setter
+        CXXSetter = method;
+        CXXSetterAccess = access;
+      }
+    }
+
+    // NOTE: a setter can also be used as a getter, so we don't check !isSetter
+    if (!CXXGetter) {
+      CXXGetter = method;
+      CXXGetterAccess = access;
+    } else if (!CXXGetter->isConst() && method->isConst()) {
+      // Previously found a non-const getter, but prefer a const getter
+      CXXGetter = method;
+      CXXGetterAccess = access;
+    }
+  }
+
+  if (!CXXGetter && !CXXSetter)
+    return nullptr;
+
+  FuncDecl *getter = nullptr, *setter = nullptr;
+
+  if (CXXSetter) {
+    setter = importUnavailableMethod(*this, CXXSetter, CXXSetterAccess, Struct,
+                                     "use .pointee property");
+    if (!setter)
+      return nullptr;
+  }
+
+  if (CXXGetter == CXXSetter) {
+    getter = setter;
+  } else if (CXXGetter) {
+    getter = importUnavailableMethod(*this, CXXGetter, CXXGetterAccess, Struct,
+                                     "use .pointee property");
+    if (!getter)
+      return nullptr;
+  }
+
+  SwiftDeclSynthesizer synth{*this};
+  auto *pointee = synth.makeDereferencedPointeeProperty(getter, setter);
+  if (!pointee)
+    return nullptr;
+
+  importAttributes(CXXGetter ? CXXGetter : CXXSetter, pointee);
+
+  Struct->addMember(pointee);
+  importedPointeeCache[Struct] = pointee;
+  return pointee;
+}
+
+FuncDecl *ClangImporter::Implementation::lookupAndImportSuccessor(
+    NominalTypeDecl *Struct) {
+  const auto *CXXRecord =
+      dyn_cast<clang::CXXRecordDecl>(Struct->getClangDecl());
+
+  if (!CXXRecord || !isa<StructDecl>(Struct))
+    // Do not synthesize successor() if this is not a C++ record, or if it is
+    // a foreign reference type (successor() needs to copy values of this type),
+    // which would be a ClassDecl rather than a StructDecl.
+    return nullptr;
+
+  if (auto [it, inserted] = importedSuccessorCache.try_emplace(Struct, nullptr);
+      !inserted)
+    return it->second;
+
+  // From this point onward, if we encounter some error and return, future calls
+  // to this function will pick up the nullptr we cached using try_emplace. Note
+  // that this may silently suppress unintentional cycles.
+
+  auto &Ctx = getClangASTContext();
+  auto &Sema = getClangSema();
+
+  auto name = Ctx.DeclarationNames.getCXXOperatorName(
+      clang::OverloadedOperatorKind::OO_PlusPlus);
+  auto R = lookupCXXMember(Sema, name, CXXRecord);
+  if (!R.has_value())
+    return nullptr;
+
+  auto overloads = filterMethodOverloads(R.value(), CXXRecord);
+
+  const clang::CXXMethodDecl *CXXMethod = nullptr;
+  clang::AccessSpecifier CXXMethodAccess;
+
+  for (auto [method, access] : overloads) {
+    if (method->isStatic() || method->isVolatile() ||
+        method->getRefQualifier() == clang::RQ_RValue ||
+        method->getMinRequiredArguments() != 0)
+      continue;
+
+    // FIXME: we currently only support non-const overloads of operator++,
+    // so skip const overloads for now. Synthesizing a nonmutating .successor()
+    // from a const overload should be possible but currently causes a crash.
+
+    if (method->isConst())
+      continue;
+
+    CXXMethod = method;
+    CXXMethodAccess = access;
+  }
+
+  if (!CXXMethod)
+    return nullptr;
+
+  auto *incr = importUnavailableMethod(*this, CXXMethod, CXXMethodAccess,
+                                       Struct, "use .pointee property");
+  if (!incr)
+    return nullptr;
+
+  SwiftDeclSynthesizer synth{*this};
+  auto *succ = synth.makeSuccessorFunc(incr);
+  if (!succ)
+    return nullptr;
+
+  importAttributes(CXXMethod, succ);
+
+  Struct->addMember(succ);
+  importedSuccessorCache[Struct] = succ;
+  return succ;
 }

test/Interop/Cxx/operators/Inputs/pointee-overloads.h
@@ -1,0 +1,97 @@
+#pragma once
+
+struct Pointee_Const {
+  int x = 111;
+  const int &operator*() const { return x; }
+};
+
+struct Pointee_NonConst {
+  int x = 222;
+  int &operator*() { return x; }
+};
+
+struct Pointee_Const_NonConst {
+  int x = 333;
+  int y = 444;
+  const int &operator*() const { return x; }
+  int &operator*() { return y; }
+};
+
+struct Pointee_NonConst_Const {
+  int x = 333;
+  int y = 444;
+  int &operator*() { return y; }
+  const int &operator*() const { return x; }
+};
+
+struct Pointee_NonConst_NonConst {
+  int x = 333;
+  int y = 444;
+  const int &operator*() const { return x; }
+  const int &operator*() { return y; }
+};
+
+struct Pointee_Volatile {
+  int x = 404;
+  volatile int &operator*() volatile { return x; }
+};
+
+struct Pointee_ConstVolatile {
+  int x = 404;
+  const volatile int &operator*() const volatile { return x; }
+};
+
+struct Pointee_Volatile_Const {
+  int x = 404;
+  int y = 555;
+  volatile int &operator*() volatile { return x; }
+  const int &operator*() const { return y; }
+};
+
+struct Pointee_NonConstGetter {
+  int x = 666;
+  const int &operator*() { return x; }
+};
+
+struct Pointee_MutableConst {
+  mutable int x = 666;
+  int &operator*() const { return x; }
+};
+
+struct Pointee_LConst {
+  int x = 1111;
+  const int &operator*() const & { return x; }
+};
+
+struct Pointee_LNonConst {
+  int x = 2222;
+  int &operator*() & { return x; }
+};
+
+struct Pointee_LConst_LNonConst {
+  int x = 3333;
+  int y = 4444;
+  const int &operator*() const & { return x; }
+  int &operator*() & { return y; }
+};
+
+struct Pointee_LNonConst_LConst {
+  int x = 3333;
+  int y = 4444;
+  int &operator*() & { return y; }
+  const int &operator*() const & { return x; }
+};
+
+struct Pointee_LConst_RConst {
+  int x = 5555;
+  int y = 6666;
+  const int &operator*() const & { return x; }
+  const int &operator*() const && { return y; }
+};
+
+struct Pointee_LNonConst_RNonConst {
+  int x = 5555;
+  int y = 6666;
+  int &operator*() & { return x; }
+  int &operator*() && { return y; }
+};

test/Interop/Cxx/operators/pointee-overloads-typechecker.swift
@@ -1,0 +1,114 @@
+// RUN: %target-typecheck-verify-swift -I %S/Inputs -cxx-interoperability-mode=default -suppress-notes
+import PointeeOverloads
+
+let lPointee_Const = Pointee_Const()
+let _ = lPointee_Const.pointee
+
+var vPointee_Const = Pointee_Const()
+let _ = vPointee_Const.pointee
+vPointee_Const.pointee = 42  // expected-error {{'pointee' is a get-only property}}
+
+let lPointee_NonConst = Pointee_NonConst()
+let _ = lPointee_NonConst.pointee // expected-error {{cannot use mutating getter on immutable value}}
+
+var vPointee_NonConst = Pointee_NonConst()
+let _ = vPointee_NonConst.pointee
+vPointee_NonConst.pointee = 42
+
+let lPointee_Const_NonConst = Pointee_Const_NonConst()
+let _ = lPointee_Const_NonConst.pointee
+
+var vPointee_Const_NonConst = Pointee_Const_NonConst()
+let _ = vPointee_Const_NonConst.pointee
+vPointee_Const_NonConst.pointee = 42
+
+let lPointee_NonConst_Const = Pointee_NonConst_Const()
+let _ = lPointee_NonConst_Const.pointee
+
+var vPointee_NonConst_Const = Pointee_NonConst_Const()
+let _ = vPointee_NonConst_Const.pointee
+vPointee_NonConst_Const.pointee = 42
+
+let lPointee_NonConst_NonConst = Pointee_NonConst_NonConst()
+let _ = lPointee_NonConst_NonConst.pointee
+
+var vPointee_NonConst_NonConst = Pointee_NonConst_NonConst()
+let _ = vPointee_NonConst_NonConst.pointee
+vPointee_NonConst_NonConst.pointee = 42 // expected-error {{'pointee' is a get-only property}}
+
+let lPointee_Volatile = Pointee_Volatile()
+let _ = lPointee_Volatile.pointee // expected-error {{has no member 'pointee'}}
+
+var vPointee_Volatile = Pointee_Volatile()
+let _ = vPointee_Volatile.pointee // expected-error {{has no member 'pointee'}}
+vPointee_Volatile.pointee = 42 // expected-error {{has no member 'pointee'}}
+
+let lPointee_ConstVolatile = Pointee_ConstVolatile()
+let _ = lPointee_ConstVolatile.pointee // expected-error {{has no member 'pointee'}}
+
+var vPointee_ConstVolatile = Pointee_ConstVolatile()
+let _ = vPointee_ConstVolatile.pointee // expected-error {{has no member 'pointee'}}
+vPointee_ConstVolatile.pointee = 42 // expected-error {{has no member 'pointee'}}
+
+let lPointee_Volatile_Const = Pointee_Volatile_Const()
+let _ = lPointee_Volatile_Const.pointee
+
+var vPointee_Volatile_Const = Pointee_Volatile_Const()
+let _ = vPointee_Volatile_Const.pointee
+vPointee_Volatile_Const.pointee = 42 // expected-error {{'pointee' is a get-only property}}
+
+let lPointee_NonConstGetter = Pointee_NonConstGetter()
+let _ = lPointee_NonConstGetter.pointee // expected-error {{cannot use mutating getter on immutable value}}
+
+var vPointee_NonConstGetter = Pointee_NonConstGetter()
+let _ = vPointee_NonConstGetter.pointee
+vPointee_NonConstGetter.pointee = 42 // expected-error {{'pointee' is a get-only property}}
+
+let lPointee_MutableConst = Pointee_MutableConst()
+let _ = lPointee_MutableConst.pointee
+
+var vPointee_MutableConst = Pointee_MutableConst()
+let _ = vPointee_MutableConst.pointee
+vPointee_MutableConst.pointee = 42
+
+let lPointee_LConst = Pointee_LConst()
+let _ = lPointee_LConst.pointee
+
+var vPointee_LConst = Pointee_LConst()
+let _ = vPointee_LConst.pointee
+vPointee_LConst.pointee = 42 // expected-error {{'pointee' is a get-only property}}
+
+let lPointee_LNonConst = Pointee_LNonConst()
+let _ = lPointee_LNonConst.pointee // expected-error {{cannot use mutating getter on immutable value}}
+
+var vPointee_LNonConst = Pointee_LNonConst()
+let _ = vPointee_LNonConst.pointee
+vPointee_LNonConst.pointee = 42
+
+let lPointee_LConst_LNonConst = Pointee_LConst_LNonConst()
+let _ = lPointee_LConst_LNonConst.pointee
+
+var vPointee_LConst_LNonConst = Pointee_LConst_LNonConst()
+let _ = vPointee_LConst_LNonConst.pointee
+vPointee_LConst_LNonConst.pointee = 42
+
+let lPointee_LNonConst_LConst = Pointee_LNonConst_LConst()
+let _ = lPointee_LNonConst_LConst.pointee
+
+var vPointee_LNonConst_LConst = Pointee_LNonConst_LConst()
+let _ = vPointee_LNonConst_LConst.pointee
+vPointee_LNonConst_LConst.pointee = 42
+
+let lPointee_LConst_RConst = Pointee_LConst_RConst()
+let _ = lPointee_LConst_RConst.pointee
+
+var vPointee_LConst_RConst = Pointee_LConst_RConst()
+let _ = vPointee_LConst_RConst.pointee
+vPointee_LConst_RConst.pointee = 42 // expected-error {{'pointee' is a get-only property}}
+
+let lPointee_LNonConst_RNonConst = Pointee_LNonConst_RNonConst()
+let _ = lPointee_LNonConst_RNonConst.pointee // expected-error {{cannot use mutating getter on immutable value}}
+
+var vPointee_LNonConst_RNonConst = Pointee_LNonConst_RNonConst()
+let _ = vPointee_LNonConst_RNonConst.pointee
+vPointee_LNonConst_RNonConst.pointee = 42

test/Interop/Cxx/operators/pointee-overloads.swift
@@ -1,0 +1,156 @@
+// RUN: %target-run-simple-swift(-I %S/Inputs -cxx-interoperability-mode=default)
+//
+// REQUIRES: executable_test
+
+import PointeeOverloads
+import StdlibUnittest
+
+var PointeeOverloadTestSuite = TestSuite("OperatorOverload")
+
+PointeeOverloadTestSuite.test("Pointee_Const") {
+  let a = Pointee_Const()
+  expectEqual(a.pointee, 111)
+
+  var b = Pointee_Const()
+  expectEqual(b.pointee, 111)
+  b.x = 100
+  expectEqual(b.pointee, 100)
+}
+
+PointeeOverloadTestSuite.test("Pointee_NonConst") {
+  var a = Pointee_NonConst()
+  expectEqual(a.pointee, 222)
+  a.pointee = 200
+  expectEqual(a.x, 200)
+  a.x = 202
+  expectEqual(a.pointee, 202)
+}
+
+PointeeOverloadTestSuite.test("Pointee_Const_NonConst") {
+  let a = Pointee_Const_NonConst()
+  expectEqual(a.pointee, 333)
+
+  var b = Pointee_Const_NonConst()
+  expectEqual(b.pointee, 333)
+  b.pointee = 400
+  expectEqual(b.pointee, 333)
+  expectEqual(b.x, 333)
+  expectEqual(b.y, 400)
+  b.y = 440
+  expectEqual(b.pointee, 333)
+  expectEqual(b.x, 333)
+  expectEqual(b.y, 440)
+}
+
+PointeeOverloadTestSuite.test("Pointee_NonConst_Const") {
+  let a = Pointee_NonConst_Const()
+  expectEqual(a.pointee, 333)
+
+  var b = Pointee_NonConst_Const()
+  expectEqual(b.pointee, 333)
+  b.pointee = 400
+  expectEqual(b.pointee, 333)
+  expectEqual(b.x, 333)
+  expectEqual(b.y, 400)
+}
+
+PointeeOverloadTestSuite.test("Pointee_NonConst_NonConst") {
+  let a = Pointee_NonConst_NonConst()
+  expectEqual(a.pointee, 333)
+
+  var b = Pointee_NonConst_NonConst()
+  expectEqual(b.pointee, 333)
+}
+
+PointeeOverloadTestSuite.test("Pointee_Volatile_Const") {
+  var a = Pointee_Volatile_Const()
+  expectEqual(a.pointee, 555)
+}
+
+PointeeOverloadTestSuite.test("Pointee_NonConstGetter") {
+  var a = Pointee_NonConstGetter()
+  expectEqual(a.pointee, 666)
+  // a.pointee = 6466 // get-only property
+}
+
+PointeeOverloadTestSuite.test("Pointee_MutableConst") {
+  let a = Pointee_MutableConst() // for some reason let is ok here
+  expectEqual(a.pointee, 666)
+
+  a.pointee = 600 // FIXME: assigning to mutable doesn't seem to work
+  expectNotEqual(a.pointee, 600) // FIXME: this should be expectEqual
+  expectNotEqual(a.x, 600) // FIXME: this should be expectEqual
+}
+
+PointeeOverloadTestSuite.test("Pointee_LConst") {
+  let a = Pointee_LConst()
+  expectEqual(a.pointee, 1111)
+
+  var b = Pointee_LConst()
+  expectEqual(b.pointee, 1111)
+  b.x = 1000
+  expectEqual(b.pointee, 1000)
+}
+
+PointeeOverloadTestSuite.test("Pointee_LNonConst") {
+  var a = Pointee_LNonConst()
+  expectEqual(a.pointee, 2222)
+  a.pointee = 2000
+  expectEqual(a.x, 2000)
+  a.x = 2020
+  expectEqual(a.pointee, 2020)
+}
+
+PointeeOverloadTestSuite.test("Pointee_LConst_LNonConst") {
+  let a = Pointee_LConst_LNonConst()
+  expectEqual(a.pointee, 3333)
+
+  var b = Pointee_LConst_LNonConst()
+  expectEqual(b.pointee, 3333)
+  b.pointee = 4000
+  expectEqual(b.pointee, 3333)
+  expectEqual(b.x, 3333)
+  expectEqual(b.y, 4000)
+}
+
+PointeeOverloadTestSuite.test("Pointee_LNonConst_LConst") {
+  let a = Pointee_LConst_LNonConst()
+  expectEqual(a.pointee, 3333)
+
+  var b = Pointee_LConst_LNonConst()
+  expectEqual(b.pointee, 3333)
+  b.pointee = 4000
+  expectEqual(b.pointee, 3333)
+  expectEqual(b.x, 3333)
+  expectEqual(b.y, 4000)
+  b.y = 4400
+  expectEqual(b.pointee, 3333)
+  expectEqual(b.x, 3333)
+  expectEqual(b.y, 4400)
+}
+
+PointeeOverloadTestSuite.test("Pointee_LConst_RConst") {
+  let a = Pointee_LConst_RConst()
+  expectEqual(a.pointee, 5555)
+
+  var b = Pointee_LConst_RConst()
+  expectEqual(b.pointee, 5555)
+  b.x = 5000
+  b.y = 6000
+  expectEqual(b.pointee, 5000)
+}
+
+PointeeOverloadTestSuite.test("Pointee_LNonConst_RNonConst") {
+  var a = Pointee_LNonConst_RNonConst()
+  expectEqual(a.pointee, 5555)
+  a.pointee = 5000
+  expectEqual(a.x, 5000)
+  expectEqual(a.y, 6666)
+  a.x = 5050
+  a.y = 6060
+  expectEqual(a.pointee, 5050)
+  expectEqual(a.x, 5050)
+  expectEqual(a.y, 6060)
+}
+
+runAllTests()

include/swift/ClangImporter/ClangImporter.h
@@ -959,6 +959,9 @@ public:
   ClangInheritanceInfo(ClangInheritanceInfo prev, clang::CXXBaseSpecifier base)
       : access(computeAccess(prev, base)) {}
 
+  /// Initialize an instance of this class at a particular given access level.
+  ClangInheritanceInfo(clang::AccessSpecifier access) : access(access) {}
+
   /// Whether this info represents a case of nested private inheritance.
   bool isNestedPrivate() const { return !access.has_value(); }
 

commit 6de8f3c5cc4e2e4cd58c8793be3dd6206d50fffc

[cxx-interop] Fix .pointee lookup for foreign reference types
This regression was introduced in c2952b77aa6771ecc26d3753805b10000bba04c9

rdar://170233903

lib/ClangImporter/ClangLookup.cpp
@@ -486,10 +486,7 @@ ClangImporter::Implementation::lookupAndImportPointee(NominalTypeDecl *Struct) {
   const auto *CXXRecord =
       dyn_cast<clang::CXXRecordDecl>(Struct->getClangDecl());
 
-  if (!CXXRecord || !isa<StructDecl>(Struct))
-    // Do not synthesize successor() if this is not a C++ record, or if it is
-    // a foreign reference type (successor() needs to copy values of this type),
-    // which would be a ClassDecl rather than a StructDecl.
+  if (!CXXRecord)
     return nullptr;
 
   if (auto [it, inserted] = importedPointeeCache.try_emplace(Struct, nullptr);

test/Interop/Cxx/operators/Inputs/pointee-overloads.h
@@ -95,3 +95,51 @@ struct Pointee_LNonConst_RNonConst {
   int &operator*() & { return x; }
   int &operator*() && { return y; }
 };
+
+struct FRT_Const_NonConst {
+  void ref() const { ++count; }
+  void deref() const { if (--count <= 0) delete this; }
+
+  const int& operator*() const { return value; }
+  int& operator*() { return value; }
+
+  FRT_Const_NonConst(int value) : value{value}, count{1} {}
+
+  int value;
+  mutable int count;
+} __attribute__((swift_attr("import_reference")))
+  __attribute__((swift_attr("retain:.ref")))
+  __attribute__((swift_attr("release:.deref")));
+
+__attribute__((swift_attr("returns_retained")))
+inline FRT_Const_NonConst * _Nonnull new_FRT_Const_NonConst(int value) {
+  return new FRT_Const_NonConst{value};
+}
+
+template<typename T>
+struct Template_FRT_Const_NonConst {
+  void ref() const { ++count; }
+  void deref() const { if (--count <= 0) delete this; }
+
+  const T& operator*() const { return value; }
+  T& operator*() { return value; }
+
+  Template_FRT_Const_NonConst(T value) : value{value}, count{1} {}
+
+  T value;
+  mutable int count;
+} __attribute__((swift_attr("import_reference")))
+  __attribute__((swift_attr("retain:.ref")))
+  __attribute__((swift_attr("release:.deref")));
+
+__attribute__((swift_attr("returns_retained")))
+inline Template_FRT_Const_NonConst<int> * _Nonnull
+Int_Template_FRT_Const_NonConst(int value) {
+  return new Template_FRT_Const_NonConst{value};
+}
+
+__attribute__((swift_attr("returns_retained")))
+inline Template_FRT_Const_NonConst<double> * _Nonnull
+Double_Template_FRT_Const_NonConst(double value) {
+  return new Template_FRT_Const_NonConst{value};
+}

test/Interop/Cxx/operators/pointee-overloads-typechecker.swift
@@ -112,3 +112,19 @@ let _ = lPointee_LNonConst_RNonConst.pointee // expected-error {{cannot use muta
 var vPointee_LNonConst_RNonConst = Pointee_LNonConst_RNonConst()
 let _ = vPointee_LNonConst_RNonConst.pointee
 vPointee_LNonConst_RNonConst.pointee = 42
+
+let vFRT_Const_NonConst = new_FRT_Const_NonConst(42)
+let _ = vFRT_Const_NonConst.pointee
+vFRT_Const_NonConst.pointee = 67
+
+let vInt_FRT_Const_NonConst = Int_Template_FRT_Const_NonConst(42)
+let _: Int32 = vInt_FRT_Const_NonConst.pointee
+vInt_FRT_Const_NonConst.pointee = 4
+vInt_FRT_Const_NonConst.pointee = false // expected-error {{cannot assign value of type 'Bool' to type 'Int32'}}
+vInt_FRT_Const_NonConst.pointee = 4.2 // expected-error {{cannot assign value of type 'Double' to type 'Int32'}}
+
+let vFloat_FRT_Const_NonConst = Double_Template_FRT_Const_NonConst(6.7)
+let _: Double = vFloat_FRT_Const_NonConst.pointee
+vFloat_FRT_Const_NonConst.pointee = 4
+vFloat_FRT_Const_NonConst.pointee = false // expected-error {{cannot assign value of type 'Bool' to type 'Double'}}
+vFloat_FRT_Const_NonConst.pointee = 4.2

test/Interop/Cxx/operators/pointee-overloads.swift
@@ -153,4 +153,46 @@ PointeeOverloadTestSuite.test("Pointee_LNonConst_RNonConst") {
   expectEqual(a.y, 6060)
 }
 
+PointeeOverloadTestSuite.test("FRT_Const_NonConst") {
+  let a = new_FRT_Const_NonConst(8888)
+  expectEqual(a.pointee, 8888)
+
+  let b = a
+  a.pointee = 8866
+  expectEqual(a.pointee, 8866)
+  expectEqual(b.pointee, 8866)
+
+  b.pointee = 6688
+  expectEqual(a.pointee, 6688)
+  expectEqual(b.pointee, 6688)
+}
+
+PointeeOverloadTestSuite.test("Int_Template_FRT_Const_NonConst") {
+  let a = Int_Template_FRT_Const_NonConst(8888)
+  expectEqual(a.pointee, 8888)
+
+  let b = a
+  a.pointee = 8866
+  expectEqual(a.pointee, 8866)
+  expectEqual(b.pointee, 8866)
+
+  b.pointee = 6688
+  expectEqual(a.pointee, 6688)
+  expectEqual(b.pointee, 6688)
+}
+
+PointeeOverloadTestSuite.test("Double_Template_FRT_Const_NonConst") {
+  let a = Double_Template_FRT_Const_NonConst(88.88)
+  expectEqual(a.pointee, 88.88)
+
+  let b = a
+  a.pointee = 88.66
+  expectEqual(a.pointee, 88.66)
+  expectEqual(b.pointee, 88.66)
+
+  b.pointee = 66.88
+  expectEqual(a.pointee, 66.88)
+  expectEqual(b.pointee, 66.88)
+}
+
 runAllTests()