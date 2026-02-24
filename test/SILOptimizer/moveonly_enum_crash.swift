83d42a66ddcef9ad871a9d449b0b0c30724b6473
AasishDairelSahayaGrinspan <aasishdairel@gmail.com>
2026-02-24 21:08:58 +0530

    Fix tests to use consuming values on normal enums instead of invalid ~Copyable indirect enums

--- test/SILOptimizer/moveonly_enum_crash.swift
+++ test/SILOptimizer/moveonly_enum_crash.swift
@@ -21,28 +21,17 @@ func testListMethodAndProperty(_ x: consuming List<Int>) {
       _ = x.prop
 }
 
-public enum MoveOnlyList<Element>: ~Copyable {
-      case empty
-      indirect case list(Element, MoveOnlyList)
-
-      consuming func performAction() {}
-      
-      var prop: Int {
-          consuming get { return 0 }
-      }
-}
-
-func testMoveOnlyListMethod(_ tail: consuming MoveOnlyList<Int>) {
+func testListMethodMultipleConsume(_ tail: consuming List<Int>) {
       tail.performAction() // expected-note {{consumed here}}
       tail.performAction() // expected-error {{'tail' used after consume}}
 }
 
-func testMoveOnlyListProperty(_ tail: consuming MoveOnlyList<Int>) {
+func testListPropertyMultipleConsume(_ tail: consuming List<Int>) {
       let _ = tail.prop // expected-note {{consumed here}}
       let _ = tail.prop // expected-error {{'tail' used after consume}}
 }
 
-func testMoveOnlyListMultipleConsume(_ value: Int, _ tail: consuming MoveOnlyList<Int>) -> MoveOnlyList<Int> {
+func testListMultipleConsume(_ value: Int, _ tail: consuming List<Int>) -> List<Int> {
       let _ = tail.prop // expected-note {{consumed here}}
       return .list(value, tail) // expected-error {{'tail' used after consume}}
 }
