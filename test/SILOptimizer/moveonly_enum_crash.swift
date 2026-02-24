8de8bb89bddda7fc537ffb343f903fa04e249534
AasishDairelSahayaGrinspan <aasishdairel@gmail.com>
2026-02-24 20:08:17 +0530

    Add tests for consuming methods/getters and moveonly diagnostics

--- test/SILOptimizer/moveonly_enum_crash.swift
+++ test/SILOptimizer/moveonly_enum_crash.swift
@@ -4,8 +4,45 @@
 public enum List<Element> {
       case empty
       indirect case list(Element, List)
+      
+      consuming func performAction() {}
+      
+      var prop: Int {
+          consuming get { return 0 }
+      }
 }
 
 func cons(_ value: Int, _ tail: consuming List<Int>) -> List<Int> {
       return .list(value, tail)
+}
+
+func testListMethodAndProperty(_ x: consuming List<Int>) {
+      x.performAction()
+      _ = x.prop
+}
+
+public enum MoveOnlyList<Element>: ~Copyable {
+      case empty
+      indirect case list(Element, MoveOnlyList)
+
+      consuming func performAction() {}
+      
+      var prop: Int {
+          consuming get { return 0 }
+      }
+}
+
+func testMoveOnlyListMethod(_ tail: consuming MoveOnlyList<Int>) {
+      tail.performAction() // expected-note {{consumed here}}
+      tail.performAction() // expected-error {{'tail' used after consume}}
+}
+
+func testMoveOnlyListProperty(_ tail: consuming MoveOnlyList<Int>) {
+      let _ = tail.prop // expected-note {{consumed here}}
+      let _ = tail.prop // expected-error {{'tail' used after consume}}
+}
+
+func testMoveOnlyListMultipleConsume(_ value: Int, _ tail: consuming MoveOnlyList<Int>) -> MoveOnlyList<Int> {
+      let _ = tail.prop // expected-note {{consumed here}}
+      return .list(value, tail) // expected-error {{'tail' used after consume}}
 }
