635acfac65e866455abe016b2083b90a34f2d2e3
Mishal Shah <shahmishal@users.noreply.github.com>
2026-02-24 20:05:36 -0800

    Update XFAIL conditions for rhel-9.7
    Remove XFAIL for LinuxDistribution rhel-9.7.

--- test/Interop/Cxx/stdlib/use-std-function.swift
+++ test/Interop/Cxx/stdlib/use-std-function.swift
@@ -16,6 +16,7 @@
 // XFAIL: LinuxDistribution=rhel-9.4
 // XFAIL: LinuxDistribution=rhel-9.5
 // XFAIL: LinuxDistribution=rhel-9.6
+// XFAIL: LinuxDistribution=rhel-9.7
 // XFAIL: LinuxDistribution=fedora-39
 // XFAIL: LinuxDistribution=fedora-41
 // XFAIL: LinuxDistribution=debian-12
