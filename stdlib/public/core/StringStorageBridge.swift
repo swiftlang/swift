af2c03844393335a866b768c657588891694e82c
David Smith <david_smith@apple.com>
2026-02-25 22:45:07 -0800

    Refactor a bit

--- stdlib/public/core/StringStorageBridge.swift
+++ stdlib/public/core/StringStorageBridge.swift
@@ -458,6 +458,223 @@ extension __SharedStringStorage {
   }
 }
 
+fileprivate extension RawSpan {
+  @inline(__always)
+  func decodeUTF8(ofWidth width: Int, at index: Int) -> UInt32? {
+    guard index <= self.byteOffsets.upperBound &- width else {
+      return nil
+    }
+    var l1 = unsafe UInt32(self.unsafeLoadUnaligned(fromUncheckedByteOffset: index &+ 0, as: UInt8.self))
+    switch width {
+    case 1:
+      break
+    case 2:
+      let l2 = unsafe UInt32(self.unsafeLoadUnaligned(fromUncheckedByteOffset: index &+ 1, as: UInt8.self))
+      l1 =  (l1 & 0b00011111) << 6
+      l1 |= (l2 & 0b00111111)
+    case 3:
+      let l2 = unsafe UInt32(self.unsafeLoadUnaligned(fromUncheckedByteOffset: index &+ 1, as: UInt8.self))
+      let l3 = unsafe UInt32(self.unsafeLoadUnaligned(fromUncheckedByteOffset: index &+ 2, as: UInt8.self))
+      l1 =  (l1 & 0b00001111) << 12
+      l1 |= (l2 & 0b00111111) << 6
+      l1 |= (l3 & 0b00111111)
+    default:
+      let l2 = unsafe UInt32(self.unsafeLoadUnaligned(fromUncheckedByteOffset: index &+ 1, as: UInt8.self))
+      let l3 = unsafe UInt32(self.unsafeLoadUnaligned(fromUncheckedByteOffset: index &+ 2, as: UInt8.self))
+      let l4 = unsafe UInt32(self.unsafeLoadUnaligned(fromUncheckedByteOffset: index &+ 3, as: UInt8.self))
+      l1 =  (l1 & 0b00000111) << 18
+      l1 |= (l2 & 0b00111111) << 12
+      l1 |= (l3 & 0b00111111) << 6
+      l1 |= (l4 & 0b00111111)
+    }
+    return l1
+  }
+  
+  @inline(__always)
+  func decodeUTF16(ofWidth width: Int, at index: Int) -> UInt32? {
+    _debugPrecondition(width == 2 || width == 4)
+    guard index <= self.byteOffsets.upperBound &- width else {
+      return nil
+    }
+    let r1 = unsafe self.unsafeLoadUnaligned(fromUncheckedByteOffset: index, as: UInt16.self)
+    if width == 2 {
+      return UInt32(r1)
+    }
+    if !Unicode.UTF16.isLeadSurrogate(r1) {
+      return nil
+    }
+    let r2 = unsafe self.unsafeLoadUnaligned(fromUncheckedByteOffset: index &+ 2, as: UInt16.self)
+    if !Unicode.UTF16.isTrailSurrogate(r2) {
+      return nil
+    }
+    return Unicode.UTF16._decodeSurrogates(r1, r2).value
+  }
+  
+  @inline(__always)
+  func contentsAreTriviallyIdentical(to rhs: RawSpan, possibleUTF16EdgeCase: Bool) -> Bool? {
+    if self.byteCount == 0 {
+      return rhs.byteCount == 0
+    }
+    if rhs.byteCount == 0 {
+      return false //already covered self.byteCount == 0 above
+    }
+    /*
+     Bizarre edge case: an ASCII buffer with embedded null bytes could be
+     bitwise-equal to a UTF16 buffer without embedded null bytes while not
+     being semantically equal
+     */
+    if possibleUTF16EdgeCase {
+      return nil
+    }
+    if self.isIdentical(to: rhs) {
+      return true
+    }
+    return nil //can't decide trivially
+  }
+}
+
+@_effects(releasenone)
+fileprivate func isEqual(
+  utf8Bytes lhs: RawSpan,
+  utf16Bytes rhs: RawSpan
+) -> Bool {
+    // If the UTF8 buffer is entirely 3 byte characters, that will be 2 bytes in UTF16
+    let minimumUTF16CountForUTF8 = (lhs.byteCount * 2) / 3
+    if rhs.byteCount < minimumUTF16CountForUTF8 {
+      return false
+    }
+    // On the other hand if it's entirely 1 byte characters, that'll double in size
+    let maximumUTF16CountForUTF8 = lhs.byteCount * 2
+    if rhs.byteCount > maximumUTF16CountForUTF8 {
+      return false
+    }
+    
+    var lhsIdx = 0
+    var rhsIdx = 0
+    
+    while true {
+      guard let firstUTF8Byte = lhs.decodeUTF8(ofWidth: 1, at: lhsIdx) else {
+        // If we've consumed everything in both buffers without finding an inequality, we're equal
+        return lhsIdx == lhs.byteOffsets.upperBound && rhsIdx == rhs.byteOffsets.upperBound
+      }
+      let utf8Width: Int
+      let utf16Width: Int
+      if firstUTF8Byte < 128 {
+        utf8Width = 1
+        utf16Width = 2
+        guard let r = rhs.decodeUTF16(ofWidth: utf16Width, at: rhsIdx) else {
+          return false
+        }
+        if r != firstUTF8Byte {
+          return false
+        }
+      } else {
+        utf8Width = _utf8ScalarLength(UInt8(truncatingIfNeeded: firstUTF8Byte))
+        utf16Width = utf8Width == 4 ? 4 : 2
+        guard
+          let l = lhs.decodeUTF8(ofWidth: utf8Width, at: lhsIdx),
+          let r = rhs.decodeUTF16(ofWidth: utf16Width, at: rhsIdx) else {
+          return false
+        }
+        if (l != r) {
+          return false
+        }
+      }
+      lhsIdx &+= utf8Width
+      rhsIdx &+= utf16Width
+    }
+}
+
+@inline(__always)
+fileprivate func isEqual(
+  bytes lhs: RawSpan,
+  bytes rhs: RawSpan
+) -> Bool {
+  _debugPrecondition(lhs.byteCount > 0)
+  if lhs.byteCount != rhs.byteCount {
+    return false
+  }
+  return unsafe lhs.withUnsafeBytes { lhsBuffer in
+    return unsafe rhs.withUnsafeBytes { rhsBuffer in
+      return unsafe 0 == _swift_stdlib_memcmp(
+        lhsBuffer.baseAddress.unsafelyUnwrapped,
+        rhsBuffer.baseAddress.unsafelyUnwrapped,
+        lhs.byteCount
+      )
+    }
+  }
+}
+
+@_effects(releasenone)
+fileprivate func isEqual(
+  asciiBytes lhs: RawSpan,
+  utf16Bytes rhs: RawSpan
+) -> Bool {
+    if lhs.byteCount * 2 != rhs.byteCount {
+      return false
+    }
+    for lhsIdx in lhs.byteOffsets {
+      /*
+       Promote ascii to 2 byte rather than truncating utf16 to 1 byte to match
+       NSString's behavior, which guards against invalid utf16 contents.
+       
+       Bounds checking handled by looping over `byteOffsets`
+       */
+      let l = unsafe UInt16(lhs.unsafeLoadUnaligned(
+        fromUncheckedByteOffset: lhsIdx,
+        as: UInt8.self
+      ))
+      // Bounds checking handled by the count * 2 verification earlier
+      let r = unsafe rhs.unsafeLoadUnaligned(
+        fromUncheckedByteOffset: lhsIdx &* 2,
+        as: UInt16.self
+      )
+      if l != r {
+        return false
+      }
+    }
+    return true
+}
+
+@_effects(releasenone)
+@c @_spi(Foundation) public func _swift_unicodeBuffersEqual_nonNormalizing(
+  bytes rawLHS: UnsafeRawPointer,
+  count lhsCount: Int,
+  encoding lhsEnc: UInt,
+  bytes rawRHS: UnsafeRawPointer,
+  count rhsCount: Int,
+  encoding rhsEnc: UInt
+) -> Bool {
+  let lhs = unsafe RawSpan(_unsafeStart: rawLHS, byteCount: lhsCount)
+  let rhs = unsafe RawSpan(_unsafeStart: rawRHS, byteCount: rhsCount)
+  let possibleUTF16EdgeCase = lhsEnc != rhsEnc &&
+      (lhsEnc == _cocoaUTF16Encoding || rhsEnc == _cocoaUTF16Encoding)
+  if let trivialCheck = lhs.contentsAreTriviallyIdentical(
+    to: rhs,
+    possibleUTF16EdgeCase: possibleUTF16EdgeCase) {
+    return trivialCheck
+  }
+  // ASCII == UTF8 can just use memcmp
+  if (lhsEnc == rhsEnc) ||
+     (lhsEnc == _cocoaASCIIEncoding && rhsEnc == _cocoaUTF8Encoding) ||
+     (lhsEnc == _cocoaUTF8Encoding && rhsEnc == _cocoaASCIIEncoding) {
+    return isEqual(bytes: lhs, bytes: rhs)
+  }
+  if lhsEnc == _cocoaUTF8Encoding && rhsEnc == _cocoaUTF16Encoding {
+    return isEqual(utf8Bytes: lhs, utf16Bytes: rhs)
+  }
+  if lhsEnc == _cocoaUTF16Encoding && rhsEnc == _cocoaUTF8Encoding {
+    return isEqual(utf8Bytes: rhs, utf16Bytes: lhs)
+  }
+  if lhsEnc == _cocoaASCIIEncoding && rhsEnc == _cocoaUTF16Encoding {
+    return isEqual(asciiBytes: lhs, utf16Bytes: rhs)
+  }
+  if lhsEnc == _cocoaUTF16Encoding && rhsEnc == _cocoaASCIIEncoding {
+    return isEqual(asciiBytes: rhs, utf16Bytes: lhs)
+  }
+  fatalError("Unsupported combination of encodings")
+}
+
 // See swift_stdlib_connectNSBaseClasses. This method is installed onto
 // NSString in Foundation via ObjC runtime shenanigans
 @_effects(releasenone)
@@ -472,77 +689,53 @@ extension __SharedStringStorage {
   defer { _fixLifetime(ns) }
   
   let rhsCount = _stdlib_binary_CFStringGetLength(ns)
-    
-  let tryASCIIRHS = { () -> Int8? in
-    return unsafe withCocoaASCIIPointer(ns) { (rhsPtr) -> Int8? in
-      return unsafe _swift_unicodeBuffersEqual_nonNormalizing(
-        bytes: lhsPtr,
-        count: lhsByteCount,
-        encoding: lhsEncoding,
-        bytes: rhsPtr,
-        count: rhsCount,
-        encoding: _cocoaASCIIEncoding
-      ) ? 1 : 0
+  
+  if lhsByteCount == 0 {
+    return rhsCount == 0 ? 1 : 0
+  }
+  if rhsCount == 0 {
+    return 0
+  }
+  
+  let result = unsafe withCocoaASCIIPointer(ns) { (rhsPtr) -> Int8? in
+    //We know RHS is ASCII at this point
+    let lhs = unsafe RawSpan(_unsafeStart: lhsPtr, byteCount: lhsByteCount)
+    let rhs = unsafe Span(_unsafeStart: rhsPtr, count: rhsCount).bytes
+    switch lhsEncoding {
+    case _cocoaASCIIEncoding, _cocoaUTF8Encoding:
+      if lhs.isIdentical(to: rhs) {
+        return 1
+      }
+      return isEqual(bytes: lhs, bytes: rhs) ? 1 : 0
+    case _cocoaUTF16Encoding:
+      return isEqual(asciiBytes: rhs, utf16Bytes: lhs) ? 1 : 0
+    default:
+      fatalError("Unsupported combination of encodings")
+    }
+  }
+  if let result {
+    return result
+  }
+  
+  if let rhsPtr = unsafe _stdlib_binary_CFStringGetCharactersPtr(ns) {
+    //We know RHS is UTF16 at this point
+    let lhs = unsafe RawSpan(_unsafeStart: lhsPtr, byteCount: lhsByteCount)
+    let rhs = unsafe Span(_unsafeStart: rhsPtr, count: rhsCount).bytes
+    switch lhsEncoding {
+    case _cocoaASCIIEncoding:
+      return isEqual(asciiBytes: lhs, utf16Bytes: rhs) ? 1 : 0
+    case _cocoaUTF8Encoding:
+      return isEqual(utf8Bytes: lhs, utf16Bytes: rhs) ? 1 : 0
+    case _cocoaUTF16Encoding:
+      if lhs.isIdentical(to: rhs) {
+        return 1
+      }
+      return isEqual(bytes: lhs, bytes: rhs) ? 1 : 0
+    default:
+      fatalError("Unsupported combination of encodings")
     }
   }
   
-  let tryUTF16RHS = { () -> Int8? in
-    if let rhsPtr = unsafe _stdlib_binary_CFStringGetCharactersPtr(ns) {
-      return unsafe _swift_unicodeBuffersEqual_nonNormalizing(
-        bytes: lhsPtr,
-        count: lhsByteCount,
-        encoding: lhsEncoding,
-        bytes: rhsPtr,
-        count: rhsCount,
-        encoding: _cocoaUTF16Encoding
-      ) ? 1 : 0
-    }
-    return nil
-  }
-  
-  //TODO: figure out how to de-duplicate these length checks with the ones in StringComparison.swift
-  switch lhsEncoding {
-  case _cocoaASCIIEncoding:
-    // One LHS byte per UTF16 code unit
-    if rhsCount != lhsByteCount {
-      return 0
-    }
-    if let result = tryASCIIRHS() {
-      return result
-    }
-    if let result = tryUTF16RHS() {
-      return result
-    }
-  case _cocoaUTF8Encoding:
-    // Best case size change for UTF8 -> UTF16 is 3 bytes -> 1 code unit
-    if rhsCount < lhsByteCount / 3 {
-      return 0
-    }
-    // Worst case size change for UTF8 -> UTF16 is 1 byte -> 1 code unit
-    if rhsCount > lhsByteCount * 2 {
-      return 0
-    }
-    if let result = tryASCIIRHS() {
-      return result
-    }
-    if let result = tryUTF16RHS() {
-      return result
-    }
-  case _cocoaUTF16Encoding:
-    // Two LHS bytes per UTF16 code unit
-    if rhsCount != lhsByteCount * 2 {
-      return 0
-    }
-    if let result = tryUTF16RHS() {
-      return result
-    }
-    if let result = tryASCIIRHS() {
-      return result
-    }
-  default:
-    fatalError("Unsupported encoding")
-  }
-    
   var remainingLHSByteCount = lhsByteCount
   var offset = 0
   
