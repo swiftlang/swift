093f0fa4a4af31ab96315970891a4f5c6b4cf923
Flamki <9833ayush@gmail.com>
2026-03-01 20:47:53 +0530

    [Availability] Update existing @available fix-it for #57378

--- test/attr/ApplicationMain/attr_main_struct_available_future.swift
+++ test/attr/ApplicationMain/attr_main_struct_available_future.swift
@@ -5,6 +5,7 @@
 @main // expected-error {{'main()' is only available in macOS 99 or newer}}
 @available(OSX 10.0, *)
 struct EntryPoint {
+// expected-note@-1 {{update '@available' attribute on enclosing}}
   @available(OSX 99, *)
   static func main() {
   }
