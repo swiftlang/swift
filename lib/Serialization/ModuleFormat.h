187cec62e77bc680b5466393c9c0a9ac2479b541
Aidan Hall <ahall28@apple.com>
2026-03-03 12:25:17 +0000

    [Serialization] Get lifetimes with LifetimeDependenceInfoRequest
    Fixes: rdar://171315715

--- lib/Serialization/ModuleFormat.h
+++ lib/Serialization/ModuleFormat.h
@@ -58,7 +58,7 @@ const uint16_t SWIFTMODULE_VERSION_MAJOR = 0;
 /// describe what change you made. The content of this comment isn't important;
 /// it just ensures a conflict if two people change the module format.
 /// Don't worry about adhering to the 80-column limit for this line.
-const uint16_t SWIFTMODULE_VERSION_MINOR = 986; // explicit module map
+const uint16_t SWIFTMODULE_VERSION_MINOR = 987; // Serialize decl lifetimes instead of their their function types' lifetimes
 
 /// A standard hash seed used for all string hashes in a serialized module.
 ///
