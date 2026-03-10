890772e4c1e617bfd74af8eb9a599e0c52e51b1a
Anton Korobeynikov <anton@korobeynikov.info>
2026-03-09 23:16:42 -0700

    Add basic codegen for coroutine function types

--- lib/SIL/IR/SILFunctionType.cpp
+++ lib/SIL/IR/SILFunctionType.cpp
@@ -2843,7 +2843,10 @@ static CanSILFunctionType getSILFunctionType(
 
     // Coroutines are always native, so fetch the native abstraction pattern.
     auto sig = origFd->getGenericSignatureOfContext().getCanonicalSignature();
-    auto origYieldType = origFd->getYieldsInterfaceType();
+    const auto *origYL = origFd->getYields();
+    assert(origYL->size() == 1 && "only support a single yield type");
+    
+    auto origYieldType = origYL->front().getInterfaceType(origFd);
     if (auto inoutTy = origYieldType->getAs<InOutType>()) {
       isInOutYield = true;
       origYieldType = inoutTy->getObjectType();
@@ -2851,7 +2854,7 @@ static CanSILFunctionType getSILFunctionType(
     auto reducedYieldType = sig.getReducedType(origYieldType);
     coroutineOrigYieldType = AbstractionPattern(sig, reducedYieldType);
 
-    Type valueType = fd->getYieldsInterfaceType();
+    Type valueType = fd->getYields()->front().getInterfaceType(fd);
     if (isInOutYield)
       valueType = valueType->castTo<InOutType>()->getObjectType();
 
@@ -2865,7 +2868,25 @@ static CanSILFunctionType getSILFunctionType(
     }
   } else if (substFnInterfaceType
                  ->isCoroutine()) { // Derive yield type for function type
-    assert(0 && "Does not support coroutine function types yet");
+    coroutineKind = SILCoroutineKind::YieldOnce;
+    assert(substFnInterfaceType->getYields().size() == 1 &&
+           "only support a single yield type");
+
+    auto sig = origType.hasGenericSignature() ? origType.getGenericSignature()
+                                              : genericSig;
+    auto origYieldType = origType.getFunctionYieldType(0);
+    auto reducedYieldType = sig.getReducedType(origYieldType.getType());
+    coroutineOrigYieldType = AbstractionPattern(sig, reducedYieldType);
+
+    auto yieldType = substFnInterfaceType->getYields().front();
+    auto valueType = yieldType.getType();
+    isInOutYield = yieldType.isInOut();
+    assert(isInOutYield == origType.getFunctionYieldFlags(0).isInOut());
+
+    if (reqtSubs)
+      valueType = valueType.subst(*reqtSubs);
+
+    coroutineSubstYieldType = valueType->getReducedType(genericSig);
   }
 
   bool shouldBuildSubstFunctionType = [&]{
