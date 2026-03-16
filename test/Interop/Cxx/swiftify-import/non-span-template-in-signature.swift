8d2e854c5881bf5d31991124a4ca7277e77e639e
Ray Wong <hlchbzz@gmail.com>
2026-03-17 05:10:44 +0900

    [ClangImporter] Add -Rmacro-expansions verify coverage to test
    Verify the exact macro expansion output for all three wrapper functions:
    - Foo.process: std::optional return with Span/MutableSpan params
    - Bar.readData: class-member ConstSpan alias
    - Bar.writeData: class-member MutSpan alias

--- test/Interop/Cxx/swiftify-import/non-span-template-in-signature.swift
+++ test/Interop/Cxx/swiftify-import/non-span-template-in-signature.swift
@@ -13,6 +13,8 @@
 // RUN:   -enable-experimental-feature SafeInteropWrappers \
 // RUN:   -enable-experimental-feature Lifetimes \
 // RUN:   -cxx-interoperability-mode=default \
+// RUN:   -Rmacro-expansions -verify -verify-additional-file %t%{fs-sep}Inputs%{fs-sep}api.h \
+// RUN:   -suppress-notes \
 // RUN:   %t/test.swift
 
 //--- Inputs/module.modulemap
@@ -32,8 +34,16 @@ using MutableByteSpan = std::span<uint8_t>;
 
 // Non-span templates in the return type must not block wrapper generation.
 struct Foo {
-  std::optional<size_t> process(ByteSpan input __noescape,
-                                MutableByteSpan output __noescape) const;
+  // expected-expansion@+9:82{{
+  //   expected-remark@1{{macro content: |/// This is an auto-generated wrapper for safer interop|}}
+  //   expected-remark@2{{macro content: |@_alwaysEmitIntoClient @available(visionOS 1.0, tvOS 12.2, watchOS 5.2, iOS 12.2, macOS 10.14.4, *) @_lifetime(output: copy output) @_disfavoredOverload|}}
+  //   expected-remark@3{{macro content: |public func process(_ input: Span<CUnsignedChar>, _ output: inout MutableSpan<CUnsignedChar>) -> std.__1.optional<CUnsignedLong> {|}}
+  //   expected-remark@4{{macro content: |    return unsafe output.withUnsafeMutableBufferPointer { _outputPtr in|}}
+  //   expected-remark@5{{macro content: |      return unsafe process(ByteSpan(input), MutableByteSpan(_outputPtr))|}}
+  //   expected-remark@6{{macro content: |    }|}}
+  //   expected-remark@7{{macro content: |}|}}
+  // }}
+  std::optional<size_t> process(ByteSpan input __noescape, MutableByteSpan output __noescape) const;
 };
 
 // Class-member type aliases for std::span must also work.
@@ -41,7 +51,24 @@ struct Bar {
   using ConstSpan = std::span<const uint8_t>;
   using MutSpan = std::span<uint8_t>;
 
+  // expected-expansion@+7:31{{
+  //   expected-remark@1{{macro content: |/// This is an auto-generated wrapper for safer interop|}}
+  //   expected-remark@2{{macro content: |@_alwaysEmitIntoClient @available(visionOS 1.0, tvOS 12.2, watchOS 5.2, iOS 12.2, macOS 10.14.4, *) @_disfavoredOverload|}}
+  //   expected-remark@3{{macro content: |public func readData(_ data: Span<CUnsignedChar>) {|}}
+  //   expected-remark@4{{macro content: |    return unsafe readData(Bar.ConstSpan(data))|}}
+  //   expected-remark@5{{macro content: |}|}}
+  // }}
   void readData(ConstSpan data __noescape) const;
+
+  // expected-expansion@+9:32{{
+  //   expected-remark@1{{macro content: |/// This is an auto-generated wrapper for safer interop|}}
+  //   expected-remark@2{{macro content: |@_alwaysEmitIntoClient @available(visionOS 1.0, tvOS 12.2, watchOS 5.2, iOS 12.2, macOS 10.14.4, *) @_lifetime(output: copy output) @_disfavoredOverload|}}
+  //   expected-remark@3{{macro content: |public func writeData(_ output: inout MutableSpan<CUnsignedChar>) {|}}
+  //   expected-remark@4{{macro content: |    return unsafe output.withUnsafeMutableBufferPointer { _outputPtr in|}}
+  //   expected-remark@5{{macro content: |      return unsafe writeData(Bar.MutSpan(_outputPtr))|}}
+  //   expected-remark@6{{macro content: |    }|}}
+  //   expected-remark@7{{macro content: |}|}}
+  // }}
   void writeData(MutSpan output __noescape) const;
 };
 
