fcd96640ba0f744c6414ca0c92c2a174b57701f1
Konrad Malawski <ktoso@apple.com>
2026-03-26 13:43:24 +0900

    Revert "[Concurrency] nonsending+typed-throw: with(Checked/Unsafe)(Throwing)Continuation (#84944)"
    This reverts commit 19c914ef3fb4c76eff1ab8c07ebdfb1dc2257135.

--- stdlib/public/Concurrency/PartialAsyncTask.swift
+++ stdlib/public/Concurrency/PartialAsyncTask.swift
@@ -895,23 +895,8 @@ internal func _resumeUnsafeThrowingContinuationWithError<T>(
 @available(SwiftStdlib 5.1, *)
 @_alwaysEmitIntoClient
 @unsafe
-public nonisolated(nonsending) func withUnsafeContinuation<T>(
-  _ fn: (UnsafeContinuation<T, Never>) -> Void
-) async -> sending T {
-  return await Builtin.withUnsafeContinuation {
-    unsafe fn(UnsafeContinuation<T, Never>($0))
-  }
-}
-
-/// Source-compatibility overload; replaced by
-/// ``withUnsafeContinuation(_:)``.
-@available(SwiftStdlib 5.1, *)
-@_alwaysEmitIntoClient
-@unsafe
-@_disfavoredOverload
-@available(*, deprecated, message: "Replaced by nonisolated(nonsending) overload")
-public func withUnsafeContinuation<T>( // source-compatibility overload
-  isolation: isolated (any Actor)?,
+public func withUnsafeContinuation<T>(
+  isolation: isolated (any Actor)? = #isolation,
   _ fn: (UnsafeContinuation<T, Never>) -> Void
 ) async -> sending T {
   return await Builtin.withUnsafeContinuation {
@@ -947,38 +932,8 @@ public func withUnsafeContinuation<T>( // source-compatibility overload
 @available(SwiftStdlib 5.1, *)
 @_alwaysEmitIntoClient
 @unsafe
-public nonisolated(nonsending) func withUnsafeThrowingContinuation<T, E>(
-  _ fn: (UnsafeContinuation<T, E>) -> Void
-) async throws(E) -> sending T {
-  do {
-    return try await Builtin.withUnsafeThrowingContinuation {
-      unsafe fn(UnsafeContinuation<T, E>($0))
-    }
-  } catch {
-    throw error as! E
-  }
-}
-
-@available(SwiftStdlib 5.1, *)
-@_alwaysEmitIntoClient
-@unsafe
-public nonisolated(nonsending) func withUnsafeThrowingContinuation<T>(
-  _ fn: (UnsafeContinuation<T, Error>) -> Void
-) async throws(Error) -> sending T {
-  return try await Builtin.withUnsafeThrowingContinuation {
-    unsafe fn(UnsafeContinuation<T, Error>($0))
-  }
-}
-
-/// Source-compatibility overload; replaced by
-/// ``withUnsafeThrowingContinuation(_:)``.
-@available(SwiftStdlib 5.1, *)
-@_alwaysEmitIntoClient
-@unsafe
-@_disfavoredOverload
-@available(*, deprecated, message: "Replaced by nonisolated(nonsending) overload")
-public func withUnsafeThrowingContinuation<T>( // source-compatibility overload
-  isolation: isolated (any Actor)?,
+public func withUnsafeThrowingContinuation<T>(
+  isolation: isolated (any Actor)? = #isolation,
   _ fn: (UnsafeContinuation<T, Error>) -> Void
 ) async throws -> sending T {
   return try await Builtin.withUnsafeThrowingContinuation {
