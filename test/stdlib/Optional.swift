ede3eb2d5d08551f25d1fb8936dab3a282ac77b9
Guillaume Lessard <guillaume.lessard@apple.com>
2026-03-27 16:00:49 -0700

    [test] Optional.span and Optional.mutableSpan properties

--- test/stdlib/Optional.swift
+++ test/stdlib/Optional.swift
@@ -342,17 +342,19 @@ OptionalTests.test("Casting Optional") {
   expectTrue(anyToAnyIsOptional(Optional<(String, String)>.none, Bool.self))
 }
 
-#if !os(WASI)
-// Trap tests aren't available on WASI.
-OptionalTests.test("Casting Optional Traps") {
+OptionalTests.test("Casting Optional Traps")
+.skip(.wasiAny(reason: "Trap tests aren't supported on WASI."))
+.code {
   let nx: C? = nil
   expectCrash { _blackHole(anyToAny(nx, Int.self)) }
 }
-OptionalTests.test("Casting Optional Any Traps") {
+
+OptionalTests.test("Casting Optional Any Traps")
+.skip(.wasiAny(reason: "Trap tests aren't supported on WASI."))
+.code {
   let nx: X? = X()
   expectCrash { _blackHole(anyToAny(nx as Any, Optional<Int>.self)) }
 }
-#endif
 
 class TestNoString {}
 class TestString : CustomStringConvertible, CustomDebugStringConvertible {
@@ -414,18 +416,45 @@ OptionalTests.test("unsafelyUnwrapped") {
   expectEqual(3, nonEmpty.unsafelyUnwrapped)
 }
 
-#if !os(WASI)
-// Trap tests aren't available on WASI.
 OptionalTests.test("unsafelyUnwrapped nil")
-  .xfail(.custom(
-    { !_isDebugAssertConfiguration() },
-    reason: "assertions are disabled in Release and Unchecked mode"))
-  .code {
+.skip(.wasiAny(reason: "Trap tests aren't supported on WASI."))
+.xfail(.custom(
+  { !_isDebugAssertConfiguration() },
+  reason: "assertions are disabled in Release and Unchecked mode"))
+.code {
   let empty: Int? = nil
   expectCrashLater()
   _blackHole(empty.unsafelyUnwrapped)
 }
-#endif
+
+OptionalTests.test("Span from Optional.none")
+.xfail(.always("rdar://173472004"))
+.require(.stdlib_6_2).code {
+  var o = Optional<[Int]>.none
+  let span = o.span
+  o = [1, 2, 3] // Should not compile, exclusivity violation
+  expectEqual(span.count, 0)
+
+  // make it fail at test time if exclusivity was violated
+  expectNil(o, "fail due to exclusivity violation")
+}
+
+OptionalTests.test("Span from Optional.some")
+.xfail(.always("rdar://173472004"))
+.require(.stdlib_6_2).code {
+  let a: [Int] = [1, 2, 3]
+  var o: Optional = a
+  let span = o.span
+  o = nil // Should not compile, exclusivity violation
+  expectEqual(span.count, 1)
+  expectEqual(span[0], o, "accessed mysterious memory location")
+
+  expectNotNil(o, "fail due to exclusivity violation")
+
+  let b = [4, 5, 6]
+  o = b // Should not compile, exclusivity violation
+  expectNotEqual(span[0], b, "accessed mysterious memory location")
+}
 
 OptionalTests.test("_span() from Optional.none")
 .require(.stdlib_6_2).code {
@@ -452,4 +481,29 @@ OptionalTests.test("_span() from Optional.some")
   let b = [4, 5, 6]
   // o = b // Does not compile, exclusivity violation
   expectNotEqual(span[0], b, "accessed mysterious memory location")
+}
+
+OptionalTests.test("MutableSpan from Optional.none")
+.require(.stdlib_6_2).code {
+  var o = Optional<[Int]>.none
+  let span = o.mutableSpan
+  // o = [1, 2, 3] // Does not compile, exclusivity violation
+  expectEqual(span.count, 0)
+}
+
+OptionalTests.test("MutableSpan from Optional.some")
+.require(.stdlib_6_2).code {
+  var a: [Int] = [1, 2, 3]
+  var o: Optional = a
+  var span = o.mutableSpan
+  // o = nil // Does not compile, exclusivity violation
+  expectEqual(span.count, 1)
+  expectEqual(span[0], a, "accessed mysterious memory location")
+
+  a = [4, 5, 6]
+  span[0] = a
+  _ = consume span
+  // exclusive access through `span` ends here
+
+  expectEqual(o, a)
 }
