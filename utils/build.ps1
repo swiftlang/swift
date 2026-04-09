ed4b0288642103261f35c958eeaa2b448198de1c
Charles Zablit <c_zablit@apple.com>
2026-04-09 11:00:47 +0100

    [do not merge] shorten lldb CI testing time

--- utils/build.ps1
+++ utils/build.ps1
@@ -223,6 +223,11 @@ param
 )
 
 ## Prepare the build environment.
+$Test = @("lldb")
+$SkipPackaging = $true
+$WindowsSDKArchitectures = @("X64")
+$Windows = $true
+$WindowsSDKVersions = @("WindowsExperimental")
 
 $ErrorActionPreference = "Stop"
 Set-StrictMode -Version 3.0
@@ -4539,13 +4544,13 @@ if (-not $SkipBuild) {
   Invoke-BuildStep Build-Subprocess $HostPlatform
   Invoke-BuildStep Build-ToolsProtocols $HostPlatform
   Invoke-BuildStep Build-Build $HostPlatform
-  Invoke-BuildStep Build-PackageManager $HostPlatform
-  Invoke-BuildStep Build-Markdown $HostPlatform
-  Invoke-BuildStep Build-Format $HostPlatform
-  Invoke-BuildStep Build-LMDB $HostPlatform
-  Invoke-BuildStep Build-IndexStoreDB $HostPlatform
-  Invoke-BuildStep Build-SourceKitLSP $HostPlatform
-  Invoke-BuildStep Build-Inspect $HostPlatform
+  # Invoke-BuildStep Build-PackageManager $HostPlatform
+  # Invoke-BuildStep Build-Markdown $HostPlatform
+  # Invoke-BuildStep Build-Format $HostPlatform
+  # Invoke-BuildStep Build-LMDB $HostPlatform
+  # Invoke-BuildStep Build-IndexStoreDB $HostPlatform
+  # Invoke-BuildStep Build-SourceKitLSP $HostPlatform
+  # Invoke-BuildStep Build-Inspect $HostPlatform
 }
 
 Install-HostToolchain
