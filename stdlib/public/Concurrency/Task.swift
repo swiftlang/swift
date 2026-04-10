52fe1c43ac8d7acf389b28d500e7ddc997d4e047
Konrad Malawski <ktoso@apple.com>
2026-04-10 19:13:25 +0900

    [Concurrency] Allow getting unsafe task from Job and name as well
    Don't add unsafeCurrentTask for the time being
    
    adjust comments and make @inlinable the property names

--- stdlib/public/Concurrency/Task.swift
+++ stdlib/public/Concurrency/Task.swift
@@ -630,6 +630,21 @@ extension Task where Success == Never, Failure == Never {
   }
 }
 
+@available(SwiftStdlib 5.1, *)
+extension Task {
+
+  /// Return the task's name, if it was set during its creation.
+  @inlinable
+  @available(SwiftStdlib 6.4, *)
+  public var name: String? {
+    if let name = unsafe _getTaskName(self._task) {
+      unsafe String(cString: name)
+    } else {
+      nil
+    }
+  }
+}
+
 // ==== Voluntary Suspension -----------------------------------------------------
 
 @available(SwiftStdlib 5.1, *)
@@ -771,11 +786,11 @@ public struct UnsafeCurrentTask {
   ///
   /// ### Instance property isCancelled ignores Task Cancellation Shields
   ///
-  /// Instance properties `task.isCancelled` and `unsafeCurrentTask.isCancelled`
-  /// are not contextual and therefore do not respect cancellation shields. If a task
+  /// The instance property `task.isCancelled`
+  /// is not contextual and therefore does not respect cancellation shields. If a task
   /// was cancelled and is executing
-  /// with an active cancellation shield, these properties will return the _actual_
-  /// cancellation status of the task.
+  /// with an active cancellation shield, this property will return the _actual_
+  /// cancellation status of the specific task.
   ///
   /// It is possible to determine if a shield is active and then actively determine
   /// that the cancelled status should be temporarily ignored by using this pair of APIs:
@@ -874,6 +889,17 @@ public struct UnsafeCurrentTask {
     @_alwaysEmitIntoClient
     get {
       unsafe _taskHasActiveCancellationShield(_task)
+    }
+  }
+
+  /// Return the task's name, if it was set during its creation.
+  @inlinable
+  @available(SwiftStdlib 6.4, *)
+  public var name: String? {
+    if let name = unsafe _getTaskName(self._task) {
+      unsafe String(cString: name)
+    } else {
+      nil
     }
   }
 }
@@ -1081,6 +1107,11 @@ func _getCurrentThreadPriority() -> Int
 @available(SwiftStdlib 6.2, *)
 @_silgen_name("swift_task_getCurrentTaskName")
 internal func _getCurrentTaskName() -> UnsafePointer<UInt8>?
+
+@available(StdlibDeploymentTarget 6.4, *)
+@usableFromInline
+@_silgen_name("swift_task_getTaskName")
+internal func _getTaskName(_ job: Builtin.NativeObject) -> UnsafePointer<UInt8>?
 
 @available(SwiftStdlib 6.2, *)
 internal func _getCurrentTaskNameString() -> String? {
