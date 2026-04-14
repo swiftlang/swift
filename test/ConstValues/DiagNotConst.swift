1570140d650741926c1f66a7d27c2e0b1713e153
Doug Gregor <dgregor@apple.com>
2026-04-14 09:26:12 -0700

    Revert "[6.3] [Section] Do not diagnose static-initialization failure on an already-erroneous context"

--- test/ConstValues/DiagNotConst.swift
+++ test/ConstValues/DiagNotConst.swift
@@ -5,6 +5,7 @@
 
 @const let a: Bool = Bool.random()
 // expected-error@-1 {{'@const' value should be initialized with a compile-time value}}
+// expected-error@-2 {{global variable must be a compile-time constant}} // Remove this once we common out the diagnostics
 
 func foo() -> Int {
 	return 42 * Int.random(in: 0 ..< 10)
@@ -12,3 +13,4 @@ func foo() -> Int {
 
 @const let b: Int = foo()
 // expected-error@-1 {{'@const' value should be initialized with a compile-time value}}
+// expected-error@-2 {{global variable must be a compile-time constant}} // Remove this once we common out the diagnostics
