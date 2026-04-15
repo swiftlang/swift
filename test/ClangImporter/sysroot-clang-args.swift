cc819751d373c99fcb1cf06a11dbe3efe09bb6bb
fridolin.karger <fridolin.karger@gmail.com>
2026-04-15 12:50:06 +0200

    update tests to use new flag

--- test/ClangImporter/sysroot-clang-args.swift
+++ test/ClangImporter/sysroot-clang-args.swift
@@ -2,13 +2,13 @@
 // RUN: mkdir -p %t/mysdk %t/mysysroot
 
 // -sdk alone is forwarded as --sysroot=<sdk>.
-// RUN: %swift -target x86_64-unknown-linux-gnu -sdk %t/mysdk -parse-stdlib -typecheck %s -dump-clang-diagnostics 2>&1 | %FileCheck -check-prefix CHECK-SDK %s
+// RUN: %swift -target x86_64-unknown-linux-gnu -sdk %t/mysdk -parse-stdlib -typecheck %s -Rclang-importer 2>&1 | %FileCheck -check-prefix CHECK-SDK %s
 
 // -sysroot alone is forwarded as --sysroot=<sysroot>.
-// RUN: %swift -target x86_64-unknown-linux-gnu -sysroot %t/mysysroot -parse-stdlib -typecheck %s -dump-clang-diagnostics 2>&1 | %FileCheck -check-prefix CHECK-SYSROOT %s
+// RUN: %swift -target x86_64-unknown-linux-gnu -sysroot %t/mysysroot -parse-stdlib -typecheck %s -Rclang-importer 2>&1 | %FileCheck -check-prefix CHECK-SYSROOT %s
 
 // when both -sdk and -sysroot are given, -sysroot takes precedence.
-// RUN: %swift -target x86_64-unknown-linux-gnu -sdk %t/mysdk -sysroot %t/mysysroot -parse-stdlib -typecheck %s -dump-clang-diagnostics 2>&1 | %FileCheck -check-prefix CHECK-BOTH %s
+// RUN: %swift -target x86_64-unknown-linux-gnu -sdk %t/mysdk -sysroot %t/mysysroot -parse-stdlib -typecheck %s -Rclang-importer 2>&1 | %FileCheck -check-prefix CHECK-BOTH %s
 
 // CHECK-SDK: clang importer driver args:
 // CHECK-SDK: '--sysroot={{.*}}{{/|\\}}mysdk'
--- test/ClangImporter/windows-sdk-macros.swift
+++ test/ClangImporter/windows-sdk-macros.swift
@@ -2,10 +2,10 @@
 // REQUIRES: CODEGENERATOR=ARM
 // REQUIRES: CODEGENERATOR=X86
 
-// RUN: %swift -target i686-unknown-windows-msvc -typecheck %s -parse-stdlib -dump-clang-diagnostics 2>&1 | %FileCheck %s -check-prefix CHECK-X86
-// RUN: %swift -target x86_64-unknown-windows-msvc -typecheck %s -parse-stdlib -dump-clang-diagnostics 2>&1 | %FileCheck %s -check-prefix CHECK-X64
-// RUN: %swift -target thumbv7-unknown-windows-msvc -typecheck %s -parse-stdlib -dump-clang-diagnostics 2>&1 | %FileCheck %s -check-prefix CHECK-ARM
-// RUN: %swift -target aarch64-unknown-windows-msvc -typecheck %s -parse-stdlib -dump-clang-diagnostics 2>&1 | %FileCheck %s -check-prefix CHECK-ARM64
+// RUN: %swift -target i686-unknown-windows-msvc -typecheck %s -parse-stdlib -Rclang-importer 2>&1 | %FileCheck %s -check-prefix CHECK-X86
+// RUN: %swift -target x86_64-unknown-windows-msvc -typecheck %s -parse-stdlib -Rclang-importer 2>&1 | %FileCheck %s -check-prefix CHECK-X64
+// RUN: %swift -target thumbv7-unknown-windows-msvc -typecheck %s -parse-stdlib -Rclang-importer 2>&1 | %FileCheck %s -check-prefix CHECK-ARM
+// RUN: %swift -target aarch64-unknown-windows-msvc -typecheck %s -parse-stdlib -Rclang-importer 2>&1 | %FileCheck %s -check-prefix CHECK-ARM64
 
 // CHECK-X86: -D_X86_
 // CHECK-X64: -D_AMD64_
