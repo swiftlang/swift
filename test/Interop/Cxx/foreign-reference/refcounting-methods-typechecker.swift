0c7416afa1cb3adb5b96408975366cd63d7ccb79
John Hui <j-hui@apple.com>
2026-04-29 23:01:36 -0700

    [cxx-interop] Move swift/bridging header to clang resource dir
    The swift/bridging header provides annotation macros (e.g.,
    SWIFT_SHARED_REFERENCE, SWIFT_COMPUTED_PROPERTY) for C++ code that
    interoperate with Swift. Previously, this header was shipped in the
    Swift toolchain's usr/include, which is not part of Clang's default
    search paths in the OSS toolchain, which forced users to manually pass
    -I flags to find the header.
    
    By placing it in Clang's resource directory, the header is automatically
    discoverable by both clang and swift (via its embedded Clang instance),
    since both already search <clang-resource-dir>/include by default.
    
    In addition to removing lib/ClangImporter/SwiftBridging/, where the
    swift/bridging lived, this patch also removes build settings related to
    the bridging header (SWIFT_ENABLE_CXX_INTEROP_SWIFT_BRIDGING_HEADER and
    the --enable-cxx-interop-swift-bridging-header build-script flag), as
    well as the explicit -I flags in interop tests pointing to the old
    SwiftBridging directory.
    
    rdar://136716355
    (cherry picked from commit e39632ff377d9936ee5a6547c34c7be18cee4522)

--- test/Interop/Cxx/foreign-reference/refcounting-methods-typechecker.swift
+++ test/Interop/Cxx/foreign-reference/refcounting-methods-typechecker.swift
@@ -1,4 +1,4 @@
-// RUN: %target-typecheck-verify-swift -Xcc -DINCORRECT -I %S%{fs-sep}Inputs -I %swift_src_root/lib/ClangImporter/SwiftBridging -verify-additional-file %S%{fs-sep}Inputs%{fs-sep}refcounting-methods.h -cxx-interoperability-mode=upcoming-swift -disable-availability-checking
+// RUN: %target-typecheck-verify-swift -Xcc -DINCORRECT -I %S%{fs-sep}Inputs -verify-additional-file %S%{fs-sep}Inputs%{fs-sep}refcounting-methods.h -cxx-interoperability-mode=upcoming-swift -disable-availability-checking
 
 import RefCountingMethods
 
