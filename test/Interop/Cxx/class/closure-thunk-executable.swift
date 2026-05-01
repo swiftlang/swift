1590f24d4c81fe71ffa97ca8425dc1d1c880a8e7
Gabor Horvath <gaborh@apple.com>
2026-05-01 16:49:48 +0100

    [cxx-interop] Fix incorrect type lowering for closures in some scenarios
    When a closure is passed as a function pointer to C++, we rely on an
    initializing conversion to plumb the clang type through so we can lower
    the closure's foreign entry point with the right conventions (lowering
    without the clang type results in the wrong convention for const T&
    parameters). Unfortunately, when we assign to a field, we did not have
    these initializing conversions. This patch creates a new way to handle
    OrigToSubstComponent for l-values that have a Clang function pointer
    type. When these values are written, we peel the path component we set
    up a conversion to get a converted RValue. When the l-value is read we
    just do the peeling.
    
    rdar://156635576

--- test/Interop/Cxx/class/closure-thunk-executable.swift
+++ test/Interop/Cxx/class/closure-thunk-executable.swift
@@ -15,4 +15,57 @@ ClosureTestSuite.test("Pass FRT to function pointer") {
   cppGo({N in })
 }
 
+ClosureTestSuite.test("AssignClosureToStructMemberConstRefNonTrivial") {
+  var s = ConstRefNonTrivialFPStruct()
+  s.fp = { nt in }
+  s.callFp(NonTrivial())
+  // Read back and call through the function pointer directly.
+  let f = s.fp
+  f(NonTrivial())
+}
+
+ClosureTestSuite.test("AssignClosureToStructMemberConstRefTrivial") {
+  var s = ConstRefTrivialFPStruct()
+  var t = Trivial()
+  t.i = 42
+  s.fp = { t in }
+  s.callFp(t)
+  let f = s.fp
+  f(t)
+}
+
+ClosureTestSuite.test("AssignClosureToStructMemberNullableConstRef") {
+  var s = NullableConstRefNonTrivialFPStruct()
+  s.fp = { nt in }
+  s.callFp(NonTrivial())
+  let f = s.fp
+  f?(NonTrivial())
+}
+
+ClosureTestSuite.test("LValueCopyConstRefNonTrivial") {
+  var s1 = ConstRefNonTrivialFPStruct()
+  var s2 = ConstRefNonTrivialFPStruct()
+  s1.fp = { _ in }
+  s2.fp = s1.fp
+  s2.callFp(NonTrivial())
+}
+
+ClosureTestSuite.test("LValueCopyConstRefTrivial") {
+  var s1 = ConstRefTrivialFPStruct()
+  var s2 = ConstRefTrivialFPStruct()
+  var t = Trivial()
+  t.i = 42
+  s1.fp = { _ in }
+  s2.fp = s1.fp
+  s2.callFp(t)
+}
+
+ClosureTestSuite.test("LValueCopyNullableConstRef") {
+  var s1 = NullableConstRefNonTrivialFPStruct()
+  var s2 = NullableConstRefNonTrivialFPStruct()
+  s1.fp = { _ in }
+  s2.fp = s1.fp
+  s2.callFp(NonTrivial())
+}
+
 runAllTests()
