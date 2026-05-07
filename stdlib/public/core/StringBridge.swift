506758a904d53de380ae89583da8c2073c3335e4
David Smith <david_smith@apple.com>
2026-05-06 17:04:21 -0700

    Review fixes

--- stdlib/public/core/StringBridge.swift
+++ stdlib/public/core/StringBridge.swift
@@ -631,12 +631,12 @@ extension String {
       return _guts.asSmall.withUTF8 { bufPtr in
         // Smol ASCII a) may bridge to tagged pointers, b) can't contain a BOM
         if _guts.isSmallASCII,
-           _guts.count <= 11, //tagged pointers can't fit >11 characters
+           bufPtr.count <= 11, //tagged pointers can't fit >11 characters
            let result = unsafe _createCFString(
-          bufPtr.baseAddress._unsafelyUnwrappedUnchecked,
-          bufPtr.count,
-          kCFStringEncodingUTF8
-        ) {
+            bufPtr.baseAddress._unsafelyUnwrappedUnchecked,
+            bufPtr.count,
+            kCFStringEncodingUTF8
+           ) {
           return result
         }
         // We can't form a tagged pointer String, so make a non-small String,
