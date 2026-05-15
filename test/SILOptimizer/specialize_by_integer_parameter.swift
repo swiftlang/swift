b787c69bc8d8fa37c8dfbfc97314ead5bdc0ffb6
Joe Groff <jgroff@apple.com>
2026-05-15 09:26:17 -0700

    Remove hardcoded integer bit width from test/SILOptimizer/specialize_by_integer_parameter.swift

--- test/SILOptimizer/specialize_by_integer_parameter.swift
+++ test/SILOptimizer/specialize_by_integer_parameter.swift
@@ -5,7 +5,7 @@ public struct Foo<let count: Int> {
 	// CHECK:         [[PARAM_VAL:%.*]] = type_value $Int for count
 	// CHECK:         [[SPEC_INT:%.*]] = integer_literal ${{.*}}, 3
 	// CHECK:         [[PARAM_INT:%.*]] = struct_extract [[PARAM_VAL]], #Int._value
-	// CHECK:         builtin "cmp_eq_Int64"([[PARAM_INT]], [[SPEC_INT]])
+	// CHECK:         builtin "cmp_eq_Int{{.*}}"([[PARAM_INT]], [[SPEC_INT]])
     @specialized(where count == 3)
     public func bar() -> Int {
 		return count
