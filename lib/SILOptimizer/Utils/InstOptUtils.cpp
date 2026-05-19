1311b5c03ac4f7b047d2098b77c0dbc11b8c2bf1
Emil Pedersen <emil_pedersen2@apple.com>
2026-05-19 15:38:52 +0100

    [DebugInfo] Use prependDeref in salvageLoadDebugInfo
    Replace the use of createDebugValueAddr in salvageLoadDebugInfo, that
    was used to rewrite debug value instructions with a prepended deref, to
    use the prependDeref function to prepend it in place, and move it to the
    correct place.

--- lib/SILOptimizer/Utils/InstOptUtils.cpp
+++ lib/SILOptimizer/Utils/InstOptUtils.cpp
@@ -2039,19 +2039,16 @@ void swift::salvageDebugInfo(SILInstruction *I) {
 }
 
 void swift::salvageLoadDebugInfo(LoadOperation load) {
-  for (Operand *debugUse : getDebugUses(load.getLoadInst())) {
-    // Create a new debug_value rather than reusing the old one so the
-    // SILBuilder adds 'expr(deref)' to account for the indirection.
+  // The use list is mutated during iteration.
+  SmallVector<Operand *, 4> debugUses(getDebugUses(load.getLoadInst()));
+  for (Operand *debugUse : debugUses) {
+    // Update the debug_value to use the loaded address.
     auto *debugInst = cast<DebugValueInst>(debugUse->getUser());
-    auto varInfo = debugInst->getVarInfo();
-    if (!varInfo)
-      continue;
-
-    // The new debug_value must be "hoisted" to the load to ensure that the
+    // The debug_value must be "hoisted" to the load to ensure that the
     // address is still valid.
-    SILBuilder(load.getLoadInst(), debugInst->getDebugScope())
-      .createDebugValueAddr(debugInst->getLoc(), load.getOperand(),
-                            varInfo.value());
+    debugInst->moveBefore(load.getLoadInst());
+    debugInst->setOperand(load.getOperand());
+    debugInst->prependDeref();
   }
 }
 
