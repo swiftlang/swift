2a65d590ba463abd6026c441af1da6b5fa072aab
Max Desiatov <m_desiatov@apple.com>
2026-05-20 21:23:35 +0100

    IRGenDebugInfo: work around `LifetimeDependenceInfo` handling (#89266)
    `ASTDemangler` drops LifetimeDependenceInfo (see `ASTDemangler.cpp`'s "Handle LifetimeDependenceInfo here" TODO); until that TODO is closed, types containing function types with lifetime dependencies cannot round-trip.
    
    Resolves https://github.com/swiftlang/swift/issues/89257
    
    rdar://177437498

---
+++ test/embedded/debuginfo-noescape-closure-crash.swift
@@ -0,0 +1,20 @@
+// RUN: %target-swift-frontend -g -emit-ir %s -enable-experimental-feature Embedded -enable-experimental-feature Lifetimes -wmo
+
+// REQUIRES: swift_in_compiler
+// REQUIRES: swift_feature_Embedded
+// REQUIRES: swift_feature_Lifetimes
+
+// Verify IRGenDebugInfo emits debug info for a stored closure of type
+// (inout T) -> U where T is ~Escapable, without crashing the round-trip
+// type-reconstruction self-check in getMangledName.
+
+public struct B: ~Copyable, ~Escapable { @_lifetime(immortal) init() {} }
+public final class C {}
+
+public final class K {
+    var fn: ((inout B) -> C)?
+    init() { self.fn = self.method }
+    func method(b: inout B) -> C { C() }
+}
+
+_ = K()
