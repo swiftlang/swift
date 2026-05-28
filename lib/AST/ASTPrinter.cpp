78f593efdb8a4888bae81d36576d8f3ae02df514
Anton Korobeynikov <anton@korobeynikov.info>
2026-05-27 23:39:05 -0700

    Correctly print coroutine AST yield types

--- lib/AST/ASTPrinter.cpp
+++ lib/AST/ASTPrinter.cpp
@@ -4826,7 +4826,7 @@ void PrintAST::visitFuncDecl(FuncDecl *decl) {
       SWIFT_DEFER {
         Printer.printStructurePost(PrintStructureKind::CoroutineYieldsTypes);
       };
-      Printer << " " << tok::kw_yield << " (";
+      Printer << " yields (";
 
       for (auto [idx, yield] : llvm::enumerate(yields)) {
         if (idx > 0)
@@ -4846,6 +4846,7 @@ void PrintAST::visitFuncDecl(FuncDecl *decl) {
 
         printTypeLoc(TheTypeLoc, getNonRecursiveOptions(decl));
       }
+      Printer << ")";
     }
 
     Type ResultTy = decl->getResultInterfaceType();
