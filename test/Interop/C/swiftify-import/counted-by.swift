1677f13714cccbf746320e93b7a82f612e4ba93a
Henrik G. Olsson <h_olsson@apple.com>
2026-05-28 11:00:26 -0700

    fix windows portability for test

--- test/Interop/C/swiftify-import/counted-by.swift
+++ test/Interop/C/swiftify-import/counted-by.swift
@@ -172,27 +172,27 @@ void constInt(int * __counted_by(42 * 10) p);
 // }}
 void constFloatCastedToInt(int * __counted_by((int) (4.2 / 12)) p);
 
-// expected-expansion@+9:52{{
+// expected-expansion@+9:140{{
 //   expected-remark@1{{macro content: |/// This is an auto-generated wrapper for safer interop|}}
 //   expected-remark@2{{macro content: |@_alwaysEmitIntoClient @_disfavoredOverload public func sizeofType(_ p: UnsafeMutableBufferPointer<Int32>) {|}}
-//   expected-remark@3{{macro content: |    if p.count != CUnsignedLong(8) {|}}
-//   expected-remark@4{{macro content: |      fatalError("bounds check failure in sizeofType: expected \\(CUnsignedLong(8)) but got \\(p.count)")|}}
+//   expected-remark@3{{macro content: |    if p.count != CUnsignedLongLong(8) {|}}
+//   expected-remark@4{{macro content: |      fatalError("bounds check failure in sizeofType: expected \\(CUnsignedLongLong(8)) but got \\(p.count)")|}}
 //   expected-remark@5{{macro content: |    }|}}
 //   expected-remark@6{{macro content: |    return unsafe sizeofType(p.baseAddress)|}}
 //   expected-remark@7{{macro content: |}|}}
 // }}
-void sizeofType(int * __counted_by(sizeof(int *)) p);
+void sizeofType(int * __counted_by((unsigned long long /*cast to long long to avoid size_t differences between platforms*/)sizeof(int *)) p);
 
-// expected-expansion@+9:49{{
+// expected-expansion@+9:137{{
 //   expected-remark@1{{macro content: |/// This is an auto-generated wrapper for safer interop|}}
 //   expected-remark@2{{macro content: |@_alwaysEmitIntoClient @_disfavoredOverload public func sizeofParam(_ p: UnsafeMutableBufferPointer<Int32>) {|}}
-//   expected-remark@3{{macro content: |    if p.count != CUnsignedLong(8) {|}}
-//   expected-remark@4{{macro content: |      fatalError("bounds check failure in sizeofParam: expected \\(CUnsignedLong(8)) but got \\(p.count)")|}}
+//   expected-remark@3{{macro content: |    if p.count != CUnsignedLongLong(8) {|}}
+//   expected-remark@4{{macro content: |      fatalError("bounds check failure in sizeofParam: expected \\(CUnsignedLongLong(8)) but got \\(p.count)")|}}
 //   expected-remark@5{{macro content: |    }|}}
 //   expected-remark@6{{macro content: |    return unsafe sizeofParam(p.baseAddress)|}}
 //   expected-remark@7{{macro content: |}|}}
 // }}
-void sizeofParam(int * __counted_by(sizeof(p)) p);
+void sizeofParam(int * __counted_by((unsigned long long /*cast to long long to avoid size_t differences between platforms*/)sizeof(p)) p);
 
 void derefLen(int * len, int * __counted_by(*len) p);
 
@@ -218,10 +218,10 @@ void pointerCastToInt(int *square, int * __counted_by((int) square) p);
 // expected-expansion@+11:50{{
 //   expected-remark@1{{macro content: |/// This is an auto-generated wrapper for safer interop|}}
 //   expected-remark@2{{macro content: |@_alwaysEmitIntoClient @_disfavoredOverload public func nanAsInt(_ p: UnsafeMutableBufferPointer<Int32>) {|}}
-//   expected-remark@3{{macro content: |    if p.count != ((0 / 0)) {|}}
 //   expected-error@3{{division by zero}}
-//   expected-remark@4{{macro content: |      fatalError("bounds check failure in nanAsInt: expected \\(((0 / 0))) but got \\(p.count)")|}}
+//   expected-remark@3{{macro content: |    if p.count != ((0 / 0)) {|}}
 //   expected-error@4 2{{division by zero}}
+//   expected-remark@4{{macro content: |      fatalError("bounds check failure in nanAsInt: expected \\(((0 / 0))) but got \\(p.count)")|}}
 //   expected-remark@5{{macro content: |    }|}}
 //   expected-remark@6{{macro content: |    return unsafe nanAsInt(p.baseAddress)|}}
 //   expected-remark@7{{macro content: |}|}}
@@ -286,16 +286,16 @@ void octalLiteral(int * __counted_by(0777) p);
 // Regression test: by printing the count expression with C syntax,
 // this example would fail to typecheck since it relies on implicit
 // casts present in C but not in Swift.
-// expected-expansion@+9:78{{
+// expected-expansion@+9:83{{
 //   expected-remark@1{{macro content: |/// This is an auto-generated wrapper for safer interop|}}
-//   expected-remark@2{{macro content: |@_alwaysEmitIntoClient @_disfavoredOverload public func implicitIntCast(_ offset: Int, _ len: Int32, _ p: UnsafeMutableBufferPointer<Int32>) {|}}
-//   expected-remark@3{{macro content: |    if p.count != (offset + CLong(len)) {|}}
-//   expected-remark@4{{macro content: |      fatalError("bounds check failure in implicitIntCast: expected \\((offset + CLong(len))) but got \\(p.count)")|}}
+//   expected-remark@2{{macro content: |@_alwaysEmitIntoClient @_disfavoredOverload public func implicitIntCast(_ offset: Int64, _ len: Int32, _ p: UnsafeMutableBufferPointer<Int32>) {|}}
+//   expected-remark@3{{macro content: |    if p.count != (offset + CLongLong(len)) {|}}
+//   expected-remark@4{{macro content: |      fatalError("bounds check failure in implicitIntCast: expected \\((offset + CLongLong(len))) but got \\(p.count)")|}}
 //   expected-remark@5{{macro content: |    }|}}
 //   expected-remark@6{{macro content: |    return unsafe implicitIntCast(offset, len, p.baseAddress)|}}
 //   expected-remark@7{{macro content: |}|}}
 // }}
-void implicitIntCast(long offset, int len, int * __counted_by(offset + len) p);
+void implicitIntCast(long long offset, int len, int * __counted_by(offset + len) p);
 
 void variadic(int len, int * __counted_by(len) p, ...);
 
@@ -306,7 +306,7 @@ module Test {
 
 //--- test.swift
 // GENERATED-BY: %target-swift-ide-test -print-module -module-to-print=Test -plugin-path %swift-plugin-dir -I %t -source-filename=x -Xcc -Wno-nullability-completeness -Xcc -Wno-div-by-zero -Xcc -Wno-pointer-to-int-cast > %t/Test-interface.swift && %swift-function-caller-generator Test %t/Test-interface.swift
-// GENERATED-HASH: 16b1fb4738c5e431b23930b84f738e5195935c2730f1ce04d3b5cbdf3f6d13f8
+// GENERATED-HASH: 53e42d566d4da72b941ab8adce364b9438badc1fbdc6054d986984d3ba2fc23b
 import Test
 
 func call_simple(_ len: Int32, _ p: UnsafeMutablePointer<Int32>!) {
@@ -429,7 +429,7 @@ func call_octalLiteral(_ p: UnsafeMutablePointer<Int32>!) {
   return unsafe octalLiteral(p)
 }
 
-func call_implicitIntCast(_ offset: Int, _ len: Int32, _ p: UnsafeMutablePointer<Int32>!) {
+func call_implicitIntCast(_ offset: Int64, _ len: Int32, _ p: UnsafeMutablePointer<Int32>!) {
   return unsafe implicitIntCast(offset, len, p)
 }
 
@@ -465,7 +465,7 @@ func call_implicitIntCast(_ offset: Int, _ len: Int32, _ p: UnsafeMutablePointer
   return unsafe hexLiteral(p)
 }
 
-@_alwaysEmitIntoClient @_disfavoredOverload public func call_implicitIntCast(_ offset: Int, _ len: Int32, _ p: UnsafeMutableBufferPointer<Int32>) {
+@_alwaysEmitIntoClient @_disfavoredOverload public func call_implicitIntCast(_ offset: Int64, _ len: Int32, _ p: UnsafeMutableBufferPointer<Int32>) {
   return unsafe implicitIntCast(offset, len, p)
 }
 
