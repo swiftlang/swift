8399d175a994240ba01974d9b0eef5e6dee63869
Henrik G. Olsson <hnrklssn@gmail.com>
2026-05-29 10:39:33 -0700

    Merge pull request #89436 from hnrklssn/swiftify-trap-source-location
    [SILGen] emit correct line number for macro source location
    
    (cherry picked from commit c223659788e85f8769d196d97ad2702cea66b94f)

---
+++ test/Interop/C/swiftify-import/trap-source-location.swift
@@ -0,0 +1,34 @@
+// REQUIRES: executable_test
+// REQUIRES: swift_test_mode_optimize_none
+
+// RUN: %empty-directory(%t)
+// RUN: split-file %s %t
+
+// RUN: %target-build-swift %t/test.swift -plugin-path %swift-plugin-dir \
+// RUN:   -import-bridging-header %t/bridging.h \
+// RUN:   -Xfrontend -validate-tbd-against-ir=none -o %t/test -target %target-swift-6.2-abi-triple
+// RUN: %target-codesign %t/test
+
+// RUN: not --crash env SWIFT_BACKTRACE=enable=no %target-run %t/test interop 2>&1 | %FileCheck %s --check-prefix CLANG
+// RUN: not --crash env SWIFT_BACKTRACE=enable=no %target-run %t/test         2>&1 | %FileCheck %s --check-prefix SWIFT
+
+// Trigger Optional unwrapping inside a macro by passing a default Span to a safe wrapper
+// where the underlying function does not accept null pointers - the base pointer of the
+// empty Span is null, and triggers a runtime exception. This is a regression test to make
+// sure the source location of the trap is correct.
+
+//--- test.swift
+@_SwiftifyImport(.countedBy(pointer: .param(1), count: "len"), .nonescaping(pointer: .param(1)))
+public func invokeMacroInSwiftModule(_ p: UnsafePointer<Int32>, _ len: Int32) {}
+
+if CommandLine.arguments.dropFirst().first == "interop" {
+  invokeMacroInClangModule(Span()) // CLANG: bridging.h:3: Fatal error: Unexpectedly found nil while unwrapping an Optional value
+} else {
+  invokeMacroInSwiftModule(Span()) // SWIFT: test.swift:2: Fatal error: Unexpectedly found nil while unwrapping an Optional value
+}
+//--- bridging.h
+#include <ptrcheck.h>
+
+static inline void invokeMacroInClangModule(
+    const int* __counted_by(count) __attribute__((__noescape__)) arr,
+    int count) {}
