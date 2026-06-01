8513077e55f0aec2548cff903850dc0263b15793
Egor Zhdan <e_zhdan@apple.com>
2026-06-01 16:33:16 +0100

    [cxx-interop] Add a test for `super` with inherited foreign reference type
    Swift allows calling a method from a superclass by doing `super.foo()` within an extension for a derived class. This adds a test to ensure it works correctly for FRTs too.
    
    rdar://177619427

---
+++ test/Interop/Cxx/foreign-reference/super-static-dispatch-silgen.swift
@@ -0,0 +1,18 @@
+// RUN: %target-swift-emit-silgen %s -I %S/Inputs -cxx-interoperability-mode=default -disable-availability-checking | %FileCheck %s
+
+import SuperStaticDispatch
+
+extension Derived {
+  public func callSuper() -> Int32 {
+    return super.nonVirtualMethod()
+  }
+}
+
+// CHECK-LABEL: sil {{.*}}callSuper{{.*}}
+// CHECK-NOT: objc_super_method
+// CHECK: Base.nonVirtualMethod()
+// CHECK-NOT: Derived.nonVirtualMethod()
+// CHECK: end sil function
+
+// CHECK: [clang Base.nonVirtualMethod]
+// CHECK-NOT: [clang Derived.nonVirtualMethod]
