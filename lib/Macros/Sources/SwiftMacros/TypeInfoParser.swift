f2a66a269711e416f23e3768c789ccf2944576fd
Paul Passeron <paul_passeron@apple.com>
2026-06-02 12:13:30 +0100

    [SwiftMacros] Added documenting comments to type info parsing utilities.

--- lib/Macros/Sources/SwiftMacros/TypeInfoParser.swift
+++ lib/Macros/Sources/SwiftMacros/TypeInfoParser.swift
@@ -10,56 +10,101 @@
 //
 //===----------------------------------------------------------------------===//
 // Utilities to handle type information passed as arguments to macros         //
-// conformances.                                                              //
+// deriving conformances.                                                     //
 //===----------------------------------------------------------------------===//
 
 import SwiftSyntax
 import SwiftSyntaxBuilder
 
+/// Represents information on a type for which a macro derives a conformance
+/// to a protocol.
 public struct NominalTypeInfo {
   var name: String
   var kind: NominalTypeKind
   var isUnsafe: Bool
 }
 
+/// Represents the kind of nominal type this is, for the moment only structs
+/// and enums are supported.
 public enum NominalTypeKind {
   case enumLike(EnumTypeInfo)
   case structLike(StructTypeInfo)
 }
 
 public struct EnumTypeInfo {
+  /// Does this enum have the `@objc` attribute ?
   var isObjC: Bool
+
+  /// Information on all its cases
   var cases: [CaseInfo]
 }
 
+/// Represents information on a single case of an enumeration.
 public struct CaseInfo {
   var name: String
+
+  /// For each associated value, we have the label's name and `nil` if there
+  /// isn't one
   var associatedValues: [String?]
 }
 
 public struct StructTypeInfo {
+  /// Information on all the struct's properties
   var properties: [StoredProperty]
 }
 
 public struct StoredProperty {
+  /// name of the stored property
   var name: String
+
+  /// Textual representation of the property's type.
   var typeName: String
+
+  /// Wether the property was introduced with `var`
   var isVar: Bool
+
+  /// Wether the property is static
   var isStatic: Bool
 }
 
+/// Error type thrown by the various parsign functions in case of ill-formed
+/// input
 public enum TypeInfoParseError: Error {
+  /// Inside a function call, we expected a specific label if `expected is a
+  /// string or no label if it is `nil` but we got the `got` syntax node
+  /// instead.
   case badArgName(expected: String?, got: LabeledExprSyntax)
+
+  /// Inside a function call, we expected `expected` arguments but got the ones
+  /// in the `args` syntax node.
   case argCountMismatch(expected: Int, args: LabeledExprListSyntax)
+
+  /// We were expecting to parse a string literal but found the `got` syntax
+  /// node instead.
   case expectedStringLiteral(got: ExprSyntax)
+
+  /// We were expecting to parse a boolean literal but found the `got` syntax
+  /// node instead.
   case expectedBoolLiteral(got: ExprSyntax)
+
+  /// We were expecting to parse an array literal but found the `got` syntax
+  /// node instead.
   case expectedArrayLiteral(got: ExprSyntax)
+
+  /// We were expecting to parse a function call expression but found the `got`
+  /// syntax node instead.
   case expectedFunctionCall(got: ExprSyntax)
+
+  /// We expected a function call which caller's name was in `names` but found
+  /// the `got` syntax node instead.
   case expectedFunctionCallNames(names: [String], got: ExprSyntax)
 
+  /// We expected a string literal carrying Swift syntax as payload but found
+  /// the `got` syntax node instead.
   case expectedStrLitAsInput(got: ExprSyntax)
 }
 
+/// Parses a string literal and returns its contents. Throws in case of error.
 func parseString(node: ExprSyntax) throws -> String {
   guard let res = node.as(StringLiteralExprSyntax.self)?.representedLiteralValue else {
     throw TypeInfoParseError.expectedStringLiteral(got: node)
@@ -67,6 +112,7 @@ func parseString(node: ExprSyntax) throws -> String {
   return res
 }
 
+/// Parses a bool literal and returns its contents. Throws in case of error.
 func parseBool(node: ExprSyntax) throws -> Bool {
   guard let lit = node.as(BooleanLiteralExprSyntax.self) else {
     throw TypeInfoParseError.expectedBoolLiteral(got: node)
@@ -74,34 +120,47 @@ func parseBool(node: ExprSyntax) throws -> Bool {
   return lit.trimmedDescription == "true"
 }
 
-func parseOpt<T>(node: ExprSyntax, parser f: (ExprSyntax) throws -> T) throws -> T? {
+/// Parses either `nil` or an expression with the `parser` parsing function.
+/// Throws if `parser` throws
+func parseOpt<T>(node: ExprSyntax, parser: (ExprSyntax) throws -> T) throws -> T? {
   // Eagerly checks if it is nil. This means that T?? can never be some(nil)
   // parsed this way as we just expect either nil or a value.
   if node.is(NilLiteralExprSyntax.self) {
     return nil
   }
-  return try f(node)
+  return try parser(node)
 }
 
-func parseArr<T>(node: ExprSyntax, parser f: (ExprSyntax) throws -> T) throws -> [T] {
+/// Parses an array literal containing elements parsed with the `parse` function
+/// and returns them all. Will throw if it is not an array literal or if one
+/// of the elements throws during parsing.
+func parseArr<T>(node: ExprSyntax, parser: (ExprSyntax) throws -> T) throws -> [T] {
   guard let arr = node.as(ArrayExprSyntax.self) else {
     throw TypeInfoParseError.expectedArrayLiteral(got: node)
   }
-  return try arr.elements.map({ try f($0.expression) })
+  return try arr.elements.map({ try parser($0.expression) })
 }
 
+/// Helper struct to represent an function call argument to be parsed.
 struct ArgInfo<T> {
+  /// The label of the argument, `nil` if there is none.
   var name: String?
+
+  /// The parsing function
   var parser: (ExprSyntax) throws -> T
 
+  /// Returns a string argument with the `name` label.
   static func stringArg(_ name: String?) -> ArgInfo<String> {
     .init(name: name, parser: parseString)
   }
 
+  /// Returns a boolean argument with the `name` label.
   static func boolArg(_ name: String?) -> ArgInfo<Bool> {
     .init(name: name, parser: parseBool)
   }
 
+  /// Returns an argument with the `name` label of type `U?` where `U`
+  /// expressions can be parsed using the `parser` function.
   static func optArg<U>(
     _ name: String?, parser: @escaping (ExprSyntax) throws -> U
   ) -> ArgInfo<U?> {
@@ -110,6 +169,8 @@ struct ArgInfo<T> {
       parser: { node in try parseOpt(node: node, parser: parser) })
   }
 
+  /// Returns an argument with the `name` label of type `[U]` where `U`
+  /// expressions can be parsed using the `parser` function.
   static func arrArg<U>(_ name: String?, parser: @escaping (ExprSyntax) throws -> U) -> ArgInfo<
     [U]
   > {
@@ -118,14 +179,20 @@ struct ArgInfo<T> {
       parser: { node in try parseArr(node: node, parser: parser) })
   }
 
+  /// Returns a new argument with the same name but expecting an array of the
+  /// elements expected by `self`. This is meant to be used like a builder.
   func toArr() -> ArgInfo<[T]> {
     .arrArg(name, parser: parser)
   }
 
+  /// Returns a new argument with the same name but expecting an optional of the
+  /// elements expected by `self`. This is meant to be used like a builder.
   func toOpt() -> ArgInfo<T?> {
     .optArg(name, parser: parser)
   }
 
+  /// Returns the parsed argument from `arg` and throws if the name is
+  /// mismatched or if the parsing function fails.
   func expect(arg: LabeledExprSyntax) throws -> T {
     let lbl = arg.label?.text
     if lbl != name {
@@ -136,22 +203,32 @@ struct ArgInfo<T> {
 }
 
 extension LabeledExprListSyntax {
+
+  /// Given a LabeledExprListSyntax and a variable number of argument infos,
+  /// try to parse the argument list as expected by the infos. Throws if one
+  /// of the arguments fails to parse.
   func expect<each ArgType>(
     _ infos: repeat ArgInfo<each ArgType>
   ) throws -> (repeat each ArgType) {
+    // Building the list of arguments syntax
     var lst = self.map { $0 }
 
+    // How many arguments to expect
     var count = 0
     for _ in repeat each infos {
       count += 1
     }
 
+    // Throw if there is a count mismatch, no need to try parsing anything.
     if count != lst.count {
       throw TypeInfoParseError.argCountMismatch(expected: count, args: self)
     }
 
     var idx = 0
 
+    /// Helper function to parse a single argument and increment the count to
+    /// be able to parse the next one. This could not be a closure as it is
+    /// generic in the type of the parsed expression `T`.
     func makeArg<T>(
       _ info: ArgInfo<T>, _ elems: inout [LabeledExprSyntax], _ idx: inout Int
     ) throws -> T {
@@ -160,8 +237,15 @@ extension LabeledExprListSyntax {
       return val
     }
 
+    // For each argument, try and parse it. If all goes well a tuple containing
+    // all args is returned.
     return (repeat try makeArg(each infos, &lst, &idx))
   }
+}
+
+public protocol TypeInfoSyntax: Equatable {
+  static func fromSyntax(node: ExprSyntax) throws -> Self
+  var syntax: ExprSyntax { get }
 }
 
 extension NominalTypeInfo: TypeInfoSyntax {
@@ -325,11 +409,6 @@ extension CaseInfo: TypeInfoSyntax {
     CaseInfo(name: \(stringlit(name)), associatedValues: \(arrSyntax(associatedValues, {optSyntax($0, stringlit)})))
     """
   }
-}
-
-public protocol TypeInfoSyntax: Equatable {
-  static func fromSyntax(node: ExprSyntax) throws -> Self
-  var syntax: ExprSyntax { get }
 }
 
 extension TypeInfoSyntax {
