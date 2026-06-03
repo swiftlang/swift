51719734236ee10e6c72442fcd7d0f553617b71f
Emil Pedersen <emil_pedersen2@apple.com>
2026-06-03 16:11:38 +0100

    [DebugInfo] Implement enum salvage
    Salvage `enum` instruction using a debug reconstruction block.

--- lib/SILOptimizer/Utils/InstOptUtils.cpp
+++ lib/SILOptimizer/Utils/InstOptUtils.cpp
@@ -2177,6 +2177,13 @@ void swift::salvageDebugInfo(SILInstruction *I) {
     }
   }
 
+  if (auto *EI = dyn_cast<EnumInst>(I)) {
+    if (EI->hasOperand())
+      salvageUnaryInst(EI);
+    else
+      salvageNullaryInst(EI);
+  }
+
   if (isa<IntegerLiteralInst>(I) || isa<FloatLiteralInst>(I))
     salvageNullaryInst(cast<SingleValueInstruction>(I));
 
