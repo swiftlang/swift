f6f046fec6ea9879fa186517f3fef5357fb1a1af
Anthony Latsis <alatsis@apple.com>
2026-06-04 13:11:07 +0100

    Revert "IRGen: Switch LLVM pipeline optimization level from Os to O2"
    This reverts commit 2cb0f2ce60d12b03191c96f72855656fc496581a.

--- lib/IRGen/IRGen.cpp
+++ lib/IRGen/IRGen.cpp
@@ -389,7 +389,7 @@ void swift::performLLVMOptimizations(
     PTO.MergeFunctions = !Opts.DisableLLVMMergeFunctions;
     // Splitting trades code size to enhance memory locality, avoid in -Osize.
     DoHotColdSplit = Opts.EnableHotColdSplit && !Opts.optimizeForSize();
-    level = llvm::OptimizationLevel::O2;
+    level = llvm::OptimizationLevel::Os;
   } else {
     level = llvm::OptimizationLevel::O0;
   }
