b4e7d7c60c553990fc9a666e828cffc61adab6cb
Slava Pestov <spestov@apple.com>
2026-06-05 23:32:37 -0400

    AST: Simplify RequirementEnvironment::RequirementEnvironment()
    Past me thought this code was clever because it avoided a redundant lookup
    of the conformance we're checking. But present me thinks this is
    over-engineered, and deleting it is the easiest way to fix a crash.
    
    This actually doesn't fix the root cause of the crash, which is
    that getSuperclass() and getSuperclassDecl() might return different
    results, with the former failing to resolve even when the latter
    returns a valid Decl, because the latter goes via a simpler lookup
    mechanism.
    
    However, I think simplifying this code is good regardless.
    
    Fixes rdar://177691260.

--- validation-test/compiler_crashers_fixed/SubstitutionMap-lookupConformance-ebecdd.swift
+++ validation-test/compiler_crashers_fixed/SubstitutionMap-lookupConformance-ebecdd.swift
@@ -1,5 +1,5 @@
 // {"kind":"typecheck","original":"fba90dae","signature":"swift::SubstitutionMap::lookupConformance(swift::CanType, swift::ProtocolDecl*) const","signatureAssert":"Assertion failed: (result && \"Subject type must be exactly equal to left-hand side of a\" \"conformance requirement in protocol requirement signature\"), function getAssociatedConformance","signatureNext":"LookUpConformanceInSubstitutionMap"}
-// RUN: not --crash %target-swift-frontend -typecheck %s
+// RUN: not %target-swift-frontend -typecheck %s
 protocol a {
   associatedtype b
   func c < d where Self : e, b == <#type#>>()
