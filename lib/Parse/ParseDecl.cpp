7530396d1aa37649d1cf18c4c21977f922b56e25
Meghana Gupta <meghana_gupta@apple.com>
2026-06-10 09:01:18 +0530

    Eliminate unnecessary error while defining macros with borrow/mutate accessors
    There were unnecessary errors when parsing accessor macros that generate borrow and mutate accessors.
    This was happening because the parser was using default declaration options instead of using
    appropriate ones from the DeclContext.
    
    Fixes rdar://179180822

--- lib/Parse/ParseDecl.cpp
+++ lib/Parse/ParseDecl.cpp
@@ -8547,9 +8547,9 @@ void Parser::parseTopLevelAccessors(
     if (accessorStatus.isError())
       break;
 
-    (void)parseAccessorAfterIntroducer(loc, kind, accessors, hasEffectfulGet,
-                                       parsingLimitedSyntax, attributes,
-                                       PD_Default, storage, status);
+    (void)parseAccessorAfterIntroducer(
+        loc, kind, accessors, hasEffectfulGet, parsingLimitedSyntax, attributes,
+        getParseDeclOptions(storage->getDeclContext()), storage, status);
     if (IsFirstAccessor) {
       IsFirstAccessor = false;
     }
