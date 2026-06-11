1a7b3ec8ddea8313b5ef01a7994b7c49037817ed
Hamish Knight <hamish_knight@apple.com>
2026-06-11 11:44:13 +0100

    [Sema] Allow member on opaque parameter type
    We recently started rejecting members on opaque types, which is correct
    for opaque archetypes, but this also meant that we started rejecting it
    for opaque parameters. Since these are just sugar for generic
    parameters, refine the condition here such that we only reject cases
    where we have an actual opaque archetype.

--- test/Sema/opaque_member_type.swift
+++ test/Sema/opaque_member_type.swift
@@ -10,3 +10,23 @@ struct S: P {
 }
 
 let spt: (some P).T = S().t // expected-error {{cannot access member type}}
+
+struct S2<T> {}
+
+protocol Q {}
+
+extension Q {
+  typealias X = S2<Self>
+}
+
+protocol R {
+  associatedtype X
+}
+
+// These are fine, `some Q` is implicitly a generic parameter.
+func asParameter1(_: (some Q).X) {}
+func asParameter2(_: [(some Q).X]) {}
+
+// This is not fine, there is no way to infer the generic parameter.
+// FIXME: We ought to have a better diagnostic for this.
+func asParameter3(_: (some R).X) {} // expected-error {{generic parameter '_' is not used in function signature}}
