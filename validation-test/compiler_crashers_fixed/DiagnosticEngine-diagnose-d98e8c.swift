11acde7d8a775b0c0487f22a9b31890b50e7a41f
Hamish Knight <hamish_knight@apple.com>
2026-06-12 19:24:51 +0100

    [AST] Improve `diag::invalid_diagnostic` behavior
    Add `SWIFT_BUG_REPORT_MESSAGE` to it, and ensure `Diag` has a default
    constructor so it doesn't end up being uninitialized.

--- validation-test/compiler_crashers_fixed/DiagnosticEngine-diagnose-d98e8c.swift
+++ validation-test/compiler_crashers_fixed/DiagnosticEngine-diagnose-d98e8c.swift
@@ -1,9 +1,5 @@
 // {"aliases":["swift::DiagnosticState::determineBehavior(swift::Diagnostic const&, swift::SourceManager&) const"],"extraArgs":["-language-mode","6"],"kind":"typecheck","original":"ede44338","signature":"swift::InFlightDiagnostic swift::DiagnosticEngine::diagnose<swift::Type, swift::Type>(swift::SourceLoc, swift::Diag<swift::Type, swift::Type>, swift::detail::PassArgument<swift::Type>::type, swift::detail::PassArgument<swift::Type>::type)","signatureNext":"ContextualFailure::diagnoseAsError"}
-// RUN: not --crash %target-swift-frontend -typecheck -language-mode 6 %s
-// REQUIRES: OS=macosx
-
-// Temporarily disabled while we investigate why it's suddenly not crashing.
-// REQUIRES: rdar179474757
+// RUN: not %target-swift-frontend -typecheck -language-mode 6 %s
 class a {
   func
     b(c: a) -> a
