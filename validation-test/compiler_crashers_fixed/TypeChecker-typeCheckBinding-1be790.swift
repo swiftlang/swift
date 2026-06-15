2a31f57e6ac62033cc8f7004593231adef6b99ec
Hamish Knight <hamish_knight@apple.com>
2026-06-15 23:09:03 +0100

    [Sema] Remove an `ABORT` for 6.4
    The proper fix for this is likely too risky for 6.4, instead let's
    just remove `ABORT`.
    
    rdar://176933373

--- validation-test/compiler_crashers_fixed/TypeChecker-typeCheckBinding-1be790.swift
+++ validation-test/compiler_crashers_fixed/TypeChecker-typeCheckBinding-1be790.swift
@@ -1,5 +1,5 @@
 // {"kind":"typecheck","original":"16a71758","signature":"swift::TypeChecker::typeCheckBinding(swift::Pattern*&, swift::Expr*&, swift::DeclContext*, swift::Type, swift::PatternBindingDecl*, unsigned int, swift::optionset::OptionSet<swift::TypeCheckExprFlags, unsigned int>)","signatureNext":"TypeChecker::typeCheckPatternBinding"}
-// RUN: not --crash %target-swift-frontend -typecheck %s
+// RUN: not %target-swift-frontend -typecheck %s
 func a(
     = {
       let b = c {
