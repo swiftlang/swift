50d4d2ad9ee412d4d2a49d3957971b363d7897e1
Konrad Malawski <ktoso@apple.com>
2026-06-24 15:39:31 +0900

    [Concurrency] TaskLocal synthesized decl must always be nonisolated
    In main actor isolated by default mode the synthesized property would
    end up main actor isolated by accident; this is nonsensical of course
    because accessing the task local variable means accessing "the current
    task" so there's no reason to isolate it to any actor like that.
    
    The only change here is to automatically syntheisze the 'nonisolated'
    keyword on the $variable
    
    rdar://166570154

--- test/Concurrency/async_task_locals_basic_warnings.swift
+++ test/Concurrency/async_task_locals_basic_warnings.swift
@@ -4,6 +4,10 @@
 // RUN: %target-swift-frontend -I %t -plugin-path %swift-plugin-dir -target %target-swift-5.1-abi-triple -strict-concurrency=complete -parse-as-library %s -emit-sil -o /dev/null -verify
 // RUN: %target-swift-frontend -I %t -plugin-path %swift-plugin-dir -disable-availability-checking -swift-version 6 -parse-as-library %s -emit-sil -o /dev/null -verify
 
+// In MainActor isolation by default mode, the synthesized $variable
+// must not become MainActor isolated as that would be nonsensical
+// RUN: %target-swift-frontend -I %t -plugin-path %swift-plugin-dir -disable-availability-checking -swift-version 6 -default-isolation MainActor -parse-as-library %s -emit-sil -o /dev/null -verify
+
 // REQUIRES: concurrency
 
 actor Test {
@@ -20,4 +24,18 @@ actor Test {
   func work() async {
     print("Hello \(Self.local ?? 0)")
   }
+}
+
+@globalActor
+struct OtherGlobalActor {
+  static let shared = Test()
+}
+
+class TaskLocalContainer {
+  @TaskLocal nonisolated static var number: Int = 42
+
+  // It is allowed to @GlobalActor the variable although not very useful... 
+  // Accesses may be performed from child tasks anyway,
+  // so this doesn't guarantee only accesses from the specified actor.
+  @TaskLocal @OtherGlobalActor static var other: Int = 7
 }
