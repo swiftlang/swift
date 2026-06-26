56bf6134160e9b671f26f3b78365c613bf9e427c
Erik Eckstein <eeckstein@apple.com>
2026-06-26 16:14:42 +0200

    ASTBridging: add `BridgedCOMThreadingModel` for `COMThreadingModel`
    Fixes a compiler error when building a debug compiler (which uses pure bridging mode).
    In pure bridging mode it's not possible to include swift header files.
    Therefore we cannot directly use `swift::COMThreadingModel`.

--- lib/AST/Bridging/DeclAttributeBridging.cpp
+++ lib/AST/Bridging/DeclAttributeBridging.cpp
@@ -198,18 +198,35 @@ BridgedBackDeployedAttr BridgedBackDeployedAttr_createParsed(
       atLoc, range, platform, cVersion.unbridged(), /*Implicit=*/false);
 }
 
+static swift::COMThreadingModel unbridge(BridgedCOMThreadingModel value) {
+  switch (value) {
+    case BridgedCOMThreadingModelSingle:
+      return swift::COMThreadingModel::Single;
+    case BridgedCOMThreadingModelApartment:
+      return swift::COMThreadingModel::Apartment;
+    case BridgedCOMThreadingModelFree:
+      return swift::COMThreadingModel::Free;
+    case BridgedCOMThreadingModelBoth:
+      return swift::COMThreadingModel::Both;
+    case BridgedCOMThreadingModelNeutral:
+      return swift::COMThreadingModel::Neutral;
+  }
+  llvm_unreachable("unhandled enum value");
+}
+
+
 BridgedCOMAttr BridgedCOMAttr_createParsed(BridgedASTContext context,
                                            swift::SourceLoc location,
                                            swift::SourceRange range,
                                            BridgedStringRef interface,
                                            BridgedStringRef implementation,
-                                           swift::COMThreadingModel threading) {
+                                           BridgedCOMThreadingModel threading) {
   std::optional<StringRef> CLSID;
   if (implementation.unbridged().data())
     CLSID = implementation.unbridged();
   return new (context.unbridged()) COMAttr(location, range,
                                            interface.unbridged(), CLSID,
-                                           threading);
+                                           unbridge(threading));
 }
 
 BridgedCDeclAttr BridgedCDeclAttr_createParsed(BridgedASTContext cContext,
