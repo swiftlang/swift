e4cfc85ffec854b5b81306c4e6cf36ba94d0152b
Hiroshi Yamauchi <56735936+hjyamauchi@users.noreply.github.com>
2026-06-29 08:32:21 -0700

    Support ObjC protocols in the SIL metatype instruction (#90238)
    This fixes the assertion failure in the anyobject-cas-crash test with
    optimizations enabled.
    
    rdar://180590931

--- test/IRGen/anyobject-cast-crash.swift
+++ test/IRGen/anyobject-cast-crash.swift
@@ -2,6 +2,9 @@
 // RUN: %target-build-swift %s -target %module-target-future -o %t/anyobject-cast-crash
 // RUN: %target-codesign %t/anyobject-cast-crash
 // RUN: %target-run %t/anyobject-cast-crash
+// RUN: %target-build-swift %s -target %module-target-future -O -o %t/anyobject-cast-crash-opt
+// RUN: %target-codesign %t/anyobject-cast-crash-opt
+// RUN: %target-run %t/anyobject-cast-crash-opt
 // REQUIRES: executable_test
 // REQUIRES: objc_interop
 
