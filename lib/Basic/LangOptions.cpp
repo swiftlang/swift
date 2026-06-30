a87bdb5412abc746e60648bca7f04a1fd18bb13c
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-06-30 14:37:46 -0400

    [stdlib] Drop big-endian MIPS variants

--- lib/Basic/LangOptions.cpp
+++ lib/Basic/LangOptions.cpp
@@ -100,9 +100,7 @@ static const SupportedConditionalValue SupportedConditionalCompilationArches[] =
   "powerpc",
   "powerpc64",
   "powerpc64le",
-  "mips",
   "mipsel",
-  "mips64",
   "mips64el",
   "s390x",
   "wasm32",
@@ -625,14 +623,8 @@ std::pair<bool, bool> LangOptions::setTarget(llvm::Triple triple) {
   case llvm::Triple::ArchType::ppc64le:
     addPlatformConditionValue(PlatformConditionKind::Arch, "powerpc64le");
     break;
-  case llvm::Triple::ArchType::mips:
-    addPlatformConditionValue(PlatformConditionKind::Arch, "mips");
-    break;
   case llvm::Triple::ArchType::mipsel:
     addPlatformConditionValue(PlatformConditionKind::Arch, "mipsel");
-    break;
-  case llvm::Triple::ArchType::mips64:
-    addPlatformConditionValue(PlatformConditionKind::Arch, "mips64");
     break;
   case llvm::Triple::ArchType::mips64el:
     addPlatformConditionValue(PlatformConditionKind::Arch, "mips64el");
