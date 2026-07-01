7df312ba490b65978c0c14780cf405b233b07120
Alastair Houghton <ahoughton@apple.com>
2026-07-01 14:58:48 +0100

    [Sema] Test `shouldAttemptFixes()` and invalidate the result.
    We should only skip the requirement here if `shouldAttemptFixes()` is
    true, and in that case we should increase the hole count to make certain
    the result is invalid.
    
    rdar://178854703

--- lib/Sema/CSSimplify.cpp
+++ lib/Sema/CSSimplify.cpp
@@ -4485,8 +4485,15 @@ ConstraintSystem::matchExistentialTypes(Type type1, Type type2,
         parameterizedType->getRequirements(type1, reqs);
         for (const auto &req : reqs) {
           assert(req.getKind() == RequirementKind::SameType);
-          if (req.hasError())
-            continue;
+          if (req.hasError()) {
+            if (shouldAttemptFixes()) {
+              // Increase SK_Hole just to ensure the solution is marked invalid.
+              increaseScore(SK_Hole, locator);
+              continue;
+            }
+
+            return TypeMatchResult::failure();
+          }
 
           auto result = matchTypes(req.getFirstType(), req.getSecondType(),
                                    ConstraintKind::Bind,
