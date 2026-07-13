fe9c1191dcbe6c3db51f0c51e8807944f7315539
Pavel Yaskevich <pyaskevich@apple.com>
2026-07-13 14:13:40 -0700

    [CSSimplify] Passing a closure with captures to `@autoclosure` parameter should incur score increase
    The score should be increased both for regular closures and the
    ones with captures uniformly.
    
    Resolves: https://github.com/swiftlang/swift/issues/90628
    Resolves: rdar://182121110
    (cherry picked from commit c911c20be48e11b9d9252d08fcd8a5d312a994e6)

--- lib/Sema/CSSimplify.cpp
+++ lib/Sema/CSSimplify.cpp
@@ -1692,7 +1692,8 @@ static ConstraintSystem::TypeMatchResult matchCallArguments(
         //    func f<T>(_: @autoclosure () -> T) {}
         //
         //    f { } // OK
-        if (isExpr<ClosureExpr>(argExpr)) {
+        //    f { [v] in } // OK
+        if (isExpr<ClosureExpr>(argExpr) || isExpr<CaptureListExpr>(argExpr)) {
           cs.increaseScore(SK_FunctionToAutoClosureConversion, loc);
         }
 
