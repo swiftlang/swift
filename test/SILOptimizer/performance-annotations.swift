5ab2f214115c4f99faa160205e8ce6e763d79fc2
Luiz Seibel <luizfelipedasilvaseibel@gmail.com>
2026-07-14 20:16:18 -0300

    [Diagnostics] Fix indefinite article in performance_deallocating diagnostic

--- test/SILOptimizer/performance-annotations.swift
+++ test/SILOptimizer/performance-annotations.swift
@@ -604,3 +604,11 @@ extension G where T == Int {
     }
   }
 }
+
+@_noAllocation
+
+func testUntypedDeallocation(_ p: UnsafeMutableRawPointer) {
+
+    p.deallocate() // expected-error {{this code pattern can cause a deallocation}}
+
+}
