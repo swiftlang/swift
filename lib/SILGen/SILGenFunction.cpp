48b3f04993b0192bbd30f3d8321b72acaeaba997
Kavon Farvardin <kfarvardin@apple.com>
2026-07-23 17:44:22 -0700

    SILGen: avoid isAddressOnly() in more places
    It's more robust to ask if it `isLoadableOrOpaque()` when
    deciding whether to create an allocation in SILGen.
    
    Eventually, under SIL opaque values, SILGen shouldn't
    need to ask whether the lowered type is "address-only".

--- lib/SILGen/SILGenFunction.cpp
+++ lib/SILGen/SILGenFunction.cpp
@@ -738,7 +738,7 @@ void SILGenFunction::emitCaptures(SILLocation loc,
                          valueType, TypeExpansionContext::
                                         noOpaqueTypeArchetypesSubstitution(
                                             expansion.getResilienceExpansion()))
-                     .isAddressOnly() &&
+                     .getRecursiveProperties().isAddressOnly() &&
                  !entryValue->getType().isAddress()) {
 
         assert(!isPack);
