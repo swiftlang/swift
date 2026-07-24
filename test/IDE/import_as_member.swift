8e7afa5d3d41a240b1eb586a3a2cc248e9f78c1c
Carl Peto <carlpeto@Carls-MacBook-Pro.local>
2026-07-24 11:17:41 +0100

    [Backtracing] [Testing] Reinstate old unit tests
    These tests appear to have been accidentally (?) left disabled though the underlying issue
    they were waiting to have fixed had been fixed. Seems to run locally. If restoring any of
    them causes flakiness then we can create a new GitHub Issue and gate the test on that being
    completed.
    
    For now it's worth trying getting them working again.
    
    Fixes #90914

--- test/IDE/import_as_member.swift
+++ test/IDE/import_as_member.swift
@@ -7,105 +7,102 @@
 // RUN: %target-swift-ide-test(mock-sdk: %clang-importer-sdk) -I %t -I %S/Inputs/custom-modules -print-module -source-filename %s -module-to-print=ImportAsMember.APINotes -swift-version 4 -always-argument-labels | %FileCheck %s -check-prefix=PRINT-APINOTES-3 -strict-whitespace
 // RUN: %target-swift-ide-test(mock-sdk: %clang-importer-sdk) -I %t -I %S/Inputs/custom-modules -print-module -source-filename %s -module-to-print=ImportAsMember.APINotes -swift-version 5 -always-argument-labels | %FileCheck %s -check-prefix=PRINT-APINOTES-4 -strict-whitespace
 
-// RUN: %target-typecheck-verify-swift -I %S/Inputs/custom-modules
-
-// Assertion failed: (I != F.TypeRemap.end() && "Invalid index into type index remap"))
-// REQUIRES: rdar70691386
+// RUN: %target-typecheck-verify-swift -I %S/Inputs/custom-modules -verify-ignore-unrelated
 
 // PRINT: struct Struct1 {
-// PRINT-NEXT:   var x: Double
-// PRINT-NEXT:   var y: Double
-// PRINT-NEXT:   var z: Double
 // PRINT-NEXT:   init()
-// PRINT-NEXT:   init(x x: Double, y y: Double, z z: Double)
+// PRINT-NEXT:   init(x x: CDouble, y y: CDouble, z z: CDouble)
+// PRINT-NEXT:   var x: CDouble
+// PRINT-NEXT:   var y: CDouble
+// PRINT-NEXT:   var z: CDouble
 // PRINT-NEXT: }
 
 // Make sure the other extension isn't here.
-// PRINT-NOT: static var static1: Double
+// PRINT-NOT: static var static1: {{C?}}Double
 
 // PRINT:      extension Struct1 {
-// PRINT-NEXT:   static var globalVar: Double
-// PRINT-NEXT:   init(value value: Double)
+// PRINT-NEXT:   static var globalVar: {{C?}}Double
+// PRINT-NEXT:   init(value value: {{C?}}Double)
 // PRINT-NEXT:   init(specialLabel specialLabel: ())
 // PRINT-NEXT:   func inverted() -> Struct1
 // PRINT-NEXT:   mutating func invert()
-// PRINT-NEXT:   func translate(radians radians: Double) -> Struct1
-// PRINT-NEXT:   func scale(_ radians: Double) -> Struct1
-// PRINT-NEXT:   var radius: Double { get nonmutating set }
-// PRINT-NEXT:   var altitude: Double{{$}}
-// PRINT-NEXT:   var magnitude: Double { get }
-// PRINT-NEXT:   static func staticMethod() -> Int32
-// PRINT-NEXT:   static var property: Int32
-// PRINT-NEXT:   static var getOnlyProperty: Int32 { get }
-// PRINT-NEXT:   func selfComesLast(x x: Double)
-// PRINT-NEXT:   func selfComesThird(a a: Int32, b b: Float, x x: Double)
+// PRINT-NEXT:   func translate(radians radians: {{C?}}Double) -> Struct1
+// PRINT-NEXT:   func scale(_ radians: {{C?}}Double) -> Struct1
+// PRINT-NEXT:   var radius: {{C?}}Double { get nonmutating set }
+// PRINT-NEXT:   var altitude: {{C?}}Double{{$}}
+// PRINT-NEXT:   var magnitude: {{C?}}Double { get }
+// PRINT-NEXT:   static func staticMethod() -> {{(CInt|Int32)}}
+// PRINT-NEXT:   static var property: {{(CInt|Int32)}}
+// PRINT-NEXT:   static var getOnlyProperty: {{(CInt|Int32)}} { get }
+// PRINT-NEXT:   func selfComesLast(x x: {{C?}}Double)
+// PRINT-NEXT:   func selfComesThird(a a: {{(CInt|Int32)}}, b b: {{C?}}Float, x x: {{C?}}Double)
 // PRINT-NEXT: }
-// PRINT-NOT: static var static1: Double
+// PRINT-NOT: static var static1: {{C?}}Double
 
 
 // Make sure the other extension isn't here.
-// PRINTB-NOT: static var globalVar: Double
+// PRINTB-NOT: static var globalVar: {{C?}}Double
 
 // PRINTB:      extension Struct1 {
-// PRINTB:        static var static1: Double
-// PRINTB-NEXT:   static var static2: Float
-// PRINTB-NEXT:   init(float value: Float)
+// PRINTB:        static var static1: {{C?}}Double
+// PRINTB-NEXT:   static var static2: {{C?}}Float
+// PRINTB-NEXT:   init(float value: {{C?}}Float)
 // PRINTB-NEXT:   static var zero: Struct1 { get }
 // PRINTB-NEXT: }
 
 // PRINTB: var currentStruct1: Struct1
 
-// PRINTB-NOT: static var globalVar: Double
+// PRINTB-NOT: static var globalVar: {{C?}}Double
 
 // PRINT-APINOTES-3:      @available(swift, obsoleted: 3, renamed: "Struct1.oldApiNoteVar")
-// PRINT-APINOTES-3-NEXT: var IAMStruct1APINoteVar: Double
+// PRINT-APINOTES-3-NEXT: var IAMStruct1APINoteVar: {{C?}}Double
 // PRINT-APINOTES-3:      extension Struct1 {
-// PRINT-APINOTES-3-NEXT:   var oldApiNoteVar: Double
+// PRINT-APINOTES-3-NEXT:   var oldApiNoteVar: {{C?}}Double
 // PRINT-APINOTES-3-NEXT:   @available(swift, introduced: 4.2, renamed: "Struct1.oldApiNoteVar")
-// PRINT-APINOTES-3-NEXT:   var newApiNoteVar: Double
+// PRINT-APINOTES-3-NEXT:   var newApiNoteVar: {{C?}}Double
 // PRINT-APINOTES-3-NEXT:   @available(swift, introduced: 4.2, renamed: "IAMStruct1APINoteVarInSwift4")
-// PRINT-APINOTES-3-NEXT:   var apiNoteVarInSwift4: Double
+// PRINT-APINOTES-3-NEXT:   var apiNoteVarInSwift4: {{C?}}Double
 // PRINT-APINOTES-3-NEXT:   static func oldApiNoteMethod()
 // PRINT-APINOTES-3-NEXT:   @available(swift, introduced: 4.2, renamed: "Struct1.oldApiNoteMethod()")
 // PRINT-APINOTES-3-NEXT:   static func newApiNoteMethod()
-// PRINT-APINOTES-3-NEXT:   init(oldLabel _: Int32)
+// PRINT-APINOTES-3-NEXT:   init(oldLabel _: {{(CInt|Int32)}})
 // PRINT-APINOTES-3-NEXT:   @available(swift, introduced: 4.2, renamed: "Struct1.init(oldLabel:)")
-// PRINT-APINOTES-3-NEXT:   init(newLabel _: Int32)
+// PRINT-APINOTES-3-NEXT:   init(newLabel _: {{(CInt|Int32)}})
 // PRINT-APINOTES-3-NEXT:   typealias OldApiNoteType = Struct1.NewApiNoteType
-// PRINT-APINOTES-3-NEXT:   typealias NewApiNoteType = Double
+// PRINT-APINOTES-3-NEXT:   typealias NewApiNoteType = {{C?}}Double
 // PRINT-APINOTES-3-NEXT: }
 // PRINT-APINOTES-3-NOT: @available
-// PRINT-APINOTES-3:     var IAMStruct1APINoteVarInSwift4: Double
+// PRINT-APINOTES-3:     var IAMStruct1APINoteVarInSwift4: {{C?}}Double
 // PRINT-APINOTES-3:     @available(swift, obsoleted: 3, renamed: "Struct1.oldApiNoteMethod()")
 // PRINT-APINOTES-3-NEXT: func IAMStruct1APINoteFunction()
 // PRINT-APINOTES-3:     @available(swift, obsoleted: 3, renamed: "Struct1.init(oldLabel:)")
-// PRINT-APINOTES-3-NEXT: func IAMStruct1APINoteCreateFunction(_ _: Int32) -> Struct1
+// PRINT-APINOTES-3-NEXT: func IAMStruct1APINoteCreateFunction(_ _: {{(CInt|Int32)}}) -> Struct1
 // PRINT-APINOTES-3:      @available(swift, obsoleted: 3, renamed: "Struct1.OldApiNoteType")
 // PRINT-APINOTES-3-NEXT: typealias IAMStruct1APINoteType = Struct1.OldApiNoteType
 
 // PRINT-APINOTES-4:      @available(swift, obsoleted: 3, renamed: "Struct1.newApiNoteVar")
-// PRINT-APINOTES-4-NEXT: var IAMStruct1APINoteVar: Double
+// PRINT-APINOTES-4-NEXT: var IAMStruct1APINoteVar: {{C?}}Double
 // PRINT-APINOTES-4:      extension Struct1 {
-// PRINT-APINOTES-4-NEXT:   var newApiNoteVar: Double
+// PRINT-APINOTES-4-NEXT:   var newApiNoteVar: {{C?}}Double
 // PRINT-APINOTES-4-NEXT:   @available(swift, obsoleted: 4.2, renamed: "Struct1.newApiNoteVar")
-// PRINT-APINOTES-4-NEXT:   var oldApiNoteVar: Double
-// PRINT-APINOTES-4-NEXT:   var apiNoteVarInSwift4: Double
+// PRINT-APINOTES-4-NEXT:   var oldApiNoteVar: {{C?}}Double
+// PRINT-APINOTES-4-NEXT:   var apiNoteVarInSwift4: {{C?}}Double
 // PRINT-APINOTES-4-NEXT:   static func newApiNoteMethod()
 // PRINT-APINOTES-4-NEXT:   @available(swift, obsoleted: 4.2, renamed: "Struct1.newApiNoteMethod()")
 // PRINT-APINOTES-4-NEXT:   static func oldApiNoteMethod()
-// PRINT-APINOTES-4-NEXT:   init(newLabel _: Int32)
+// PRINT-APINOTES-4-NEXT:   init(newLabel _: {{(CInt|Int32)}})
 // PRINT-APINOTES-4-NEXT:   @available(swift, obsoleted: 4.2, renamed: "Struct1.init(newLabel:)")
-// PRINT-APINOTES-4-NEXT:   init(oldLabel _: Int32)
-// PRINT-APINOTES-4-NEXT:   typealias NewApiNoteType = Double
+// PRINT-APINOTES-4-NEXT:   init(oldLabel _: {{(CInt|Int32)}})
+// PRINT-APINOTES-4-NEXT:   typealias NewApiNoteType = {{C?}}Double
 // PRINT-APINOTES-4-NEXT:   @available(swift, obsoleted: 4.2, renamed: "Struct1.NewApiNoteType")
 // PRINT-APINOTES-4-NEXT:   typealias OldApiNoteType = Struct1.NewApiNoteType
 // PRINT-APINOTES-4-NEXT: }
 // PRINT-APINOTES-4:      @available(swift, obsoleted: 4.2, renamed: "Struct1.apiNoteVarInSwift4")
-// PRINT-APINOTES-4-NEXT: var IAMStruct1APINoteVarInSwift4: Double
+// PRINT-APINOTES-4-NEXT: var IAMStruct1APINoteVarInSwift4: {{C?}}Double
 // PRINT-APINOTES-4:     @available(swift, obsoleted: 3, renamed: "Struct1.newApiNoteMethod()")
 // PRINT-APINOTES-4-NEXT: func IAMStruct1APINoteFunction()
 // PRINT-APINOTES-4:     @available(swift, obsoleted: 3, renamed: "Struct1.init(newLabel:)")
-// PRINT-APINOTES-4-NEXT: func IAMStruct1APINoteCreateFunction(_ _: Int32) -> Struct1
+// PRINT-APINOTES-4-NEXT: func IAMStruct1APINoteCreateFunction(_ _: {{(CInt|Int32)}}) -> Struct1
 // PRINT-APINOTES-4:      @available(swift, obsoleted: 3, renamed: "Struct1.NewApiNoteType")
 // PRINT-APINOTES-4-NEXT: typealias IAMStruct1APINoteType = Struct1.NewApiNoteType
 
