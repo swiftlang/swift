d0b66ee70e3ee2119dea6c617f822a7b52109043
Hamish Knight <hamish_knight@apple.com>
2026-07-26 15:46:58 +0100

    [Sema] Remove `DiagnosticTransaction` from `CallerSideDefaultArgExprRequest`
    2019 me had good intentions with this, but it's a fundamentally
    unsound hack and isn't worth the tradeoff. Let's just remove it.
    
    rdar://141047299

--- test/multifile/default-arguments/one-module/invalid-magic-literals.swift
+++ test/multifile/default-arguments/one-module/invalid-magic-literals.swift
@@ -1,6 +1,6 @@
 // RUN: %target-swift-frontend -emit-sil -verify -module-name main -primary-file %s %S/Inputs/invalid-magic-literals-other.swift
 
 func bar() {
-  badMagicLiteral()
-  let _: Int = badGenericMagicLiteral()
+  badMagicLiteral() // expected-error {{default argument value of type 'Int' cannot be converted to type 'String'}}
+  let _: Int = badGenericMagicLiteral() // expected-error {{default argument value of type 'String' cannot be converted to type 'Int'}}
 }
--- validation-test/compiler_crashers_fixed/CallerSideDefaultArgExprRequest-evaluate-372b93.swift
+++ validation-test/compiler_crashers_fixed/CallerSideDefaultArgExprRequest-evaluate-372b93.swift
@@ -1,5 +1,5 @@
 // {"kind":"typecheck","signature":"swift::CallerSideDefaultArgExprRequest::evaluate(swift::Evaluator&, swift::DefaultArgumentExpr*) const","signatureAssert":"Assertion failed: (ctx.Diags.hadAnyError()), function evaluate","signatureNext":"CallerSideDefaultArgExprRequest::OutputType"}
-// RUN: not --crash %target-swift-frontend -typecheck %s
+// RUN: not %target-swift-frontend -typecheck %s
 @propertyWrapper
 struct a {
   var wrappedValue: Int?
