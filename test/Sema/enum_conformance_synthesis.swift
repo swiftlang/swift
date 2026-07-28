8517f7bdc30100334c1f313433f25cfb40cda6a3
Paul Passeron <paul_passeron@apple.com>
2026-07-28 16:38:06 +0100

    [test] Merge macros/non-macros Sema/... `Equatable` derivation tests

--- test/Sema/enum_conformance_synthesis.swift
+++ test/Sema/enum_conformance_synthesis.swift
@@ -1,4 +1,7 @@
 // RUN: %target-swift-frontend -typecheck -verify -verify-ignore-unrelated -primary-file %s %S/Inputs/enum_conformance_synthesis_other.swift -verify-ignore-unknown -swift-version 4
+// RUN: %target-swift-frontend -typecheck -verify -verify-ignore-unrelated -primary-file %s %S/Inputs/enum_conformance_synthesis_other.swift -verify-ignore-unknown -swift-version 4 -enable-experimental-feature DeriveConformancesViaMacros -load-plugin-library %swift-plugin-dir/%target-library-name(SwiftMacros)
+
+// REQUIRES: swift_feature_DeriveConformancesViaMacros
 
 var hasher = Hasher()
 
