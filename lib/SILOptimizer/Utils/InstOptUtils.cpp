f12487f9bb3319cc5e1dfeffff3df62b0b9f06dc
Emil Pedersen <emil_pedersen2@apple.com>
2026-07-29 13:53:09 +0100

    [DebugInfo] Salvage convert_function transitive users
    The debug uses of the convert_function instruction being replaced with
    values of a different type is a problem, as the debug value incorrectly
    changes type.
    
    The salvageDebugInfo function must be called on all transitive uses,
    including begin_borrow and copy_value, which also need to be salvaged.
    
    rdar://183435195

--- lib/SILOptimizer/Utils/InstOptUtils.cpp
+++ lib/SILOptimizer/Utils/InstOptUtils.cpp
@@ -1829,6 +1829,7 @@ bool swift::tryEliminateOnlyOwnershipUsedForwardingInst(
   }
 
   SmallVector<Operand *, 32> worklist(getNonDebugUses(forwardingInst));
+  SmallVector<SILInstruction *, 4> forwardingUsers;
   while (!worklist.empty()) {
     auto *use = worklist.pop_back_val();
     auto *user = use->getUser();
@@ -1838,6 +1839,7 @@ bool swift::tryEliminateOnlyOwnershipUsedForwardingInst(
       continue;
 
     if (isa<CopyValueInst>(user) || isa<BeginBorrowInst>(user)) {
+      forwardingUsers.push_back(user);
       for (auto result : user->getResults())
         for (auto *resultUse : getNonDebugUses(result))
           worklist.push_back(resultUse);
@@ -1847,9 +1849,12 @@ bool swift::tryEliminateOnlyOwnershipUsedForwardingInst(
     return false;
   }
 
-  // Rewrite debug uses separately (they might need salvaging logic).
-  // Note: We know that `forwardingInst` is salvageable as this function is
-  // only called with convert_function instructions.
+  // Rewrite all debug uses separately (they might need salvaging logic).
+  // Note: We know that `forwardingInst` and its users are salvageable as this
+  // function is only called with convert_function instructions, and
+  // begin_borrow and copy_value are salvageable as well.
+  for (auto *inst : llvm::reverse(forwardingUsers))
+    salvageDebugInfo(inst);
   salvageDebugInfo(forwardingInst);
 
   // Now that we know we can perform our transform, set all uses of
