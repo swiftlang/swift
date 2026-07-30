0f730bda26261e44ffd6e66c28e7ab2ece2f61a1
Carl Peto <carlpeto@Carls-MacBook-Pro.local>
2026-07-30 15:39:18 +0100

    [Android] [Testing] Tighten up partially-pre-defined-class-template-irgen.swift
    Fixes: #91101

--- test/IDE/import_as_member.swift
+++ test/IDE/import_as_member.swift
@@ -18,91 +18,91 @@
 // PRINT-NEXT: }
 
 // Make sure the other extension isn't here.
-// PRINT-NOT: static var static1: {{C?}}Double
+// PRINT-NOT: static var static1: CDouble
 
 // PRINT:      extension Struct1 {
-// PRINT-NEXT:   static var globalVar: {{C?}}Double
-// PRINT-NEXT:   init(value value: {{C?}}Double)
+// PRINT-NEXT:   static var globalVar: CDouble
+// PRINT-NEXT:   init(value value: CDouble)
 // PRINT-NEXT:   init(specialLabel specialLabel: ())
 // PRINT-NEXT:   func inverted() -> Struct1
 // PRINT-NEXT:   mutating func invert()
-// PRINT-NEXT:   func translate(radians radians: {{C?}}Double) -> Struct1
-// PRINT-NEXT:   func scale(_ radians: {{C?}}Double) -> Struct1
-// PRINT-NEXT:   var radius: {{C?}}Double { get nonmutating set }
-// PRINT-NEXT:   var altitude: {{C?}}Double{{$}}
-// PRINT-NEXT:   var magnitude: {{C?}}Double { get }
-// PRINT-NEXT:   static func staticMethod() -> {{(CInt|Int32)}}
-// PRINT-NEXT:   static var property: {{(CInt|Int32)}}
-// PRINT-NEXT:   static var getOnlyProperty: {{(CInt|Int32)}} { get }
-// PRINT-NEXT:   func selfComesLast(x x: {{C?}}Double)
-// PRINT-NEXT:   func selfComesThird(a a: {{(CInt|Int32)}}, b b: {{C?}}Float, x x: {{C?}}Double)
+// PRINT-NEXT:   func translate(radians radians: CDouble) -> Struct1
+// PRINT-NEXT:   func scale(_ radians: CDouble) -> Struct1
+// PRINT-NEXT:   var radius: CDouble { get nonmutating set }
+// PRINT-NEXT:   var altitude: CDouble{{$}}
+// PRINT-NEXT:   var magnitude: CDouble { get }
+// PRINT-NEXT:   static func staticMethod() -> CInt
+// PRINT-NEXT:   static var property: CInt
+// PRINT-NEXT:   static var getOnlyProperty: CInt { get }
+// PRINT-NEXT:   func selfComesLast(x x: CDouble)
+// PRINT-NEXT:   func selfComesThird(a a: CInt, b b: CFloat, x x: CDouble)
 // PRINT-NEXT: }
-// PRINT-NOT: static var static1: {{C?}}Double
+// PRINT-NOT: static var static1: CDouble
 
 
 // Make sure the other extension isn't here.
-// PRINTB-NOT: static var globalVar: {{C?}}Double
+// PRINTB-NOT: static var globalVar: CDouble
 
 // PRINTB:      extension Struct1 {
-// PRINTB:        static var static1: {{C?}}Double
-// PRINTB-NEXT:   static var static2: {{C?}}Float
-// PRINTB-NEXT:   init(float value: {{C?}}Float)
+// PRINTB:        static var static1: CDouble
+// PRINTB-NEXT:   static var static2: CFloat
+// PRINTB-NEXT:   init(float value: CFloat)
 // PRINTB-NEXT:   static var zero: Struct1 { get }
 // PRINTB-NEXT: }
 
 // PRINTB: var currentStruct1: Struct1
 
-// PRINTB-NOT: static var globalVar: {{C?}}Double
+// PRINTB-NOT: static var globalVar: CDouble
 
 // PRINT-APINOTES-3:      @available(swift, obsoleted: 3, renamed: "Struct1.oldApiNoteVar")
-// PRINT-APINOTES-3-NEXT: var IAMStruct1APINoteVar: {{C?}}Double
+// PRINT-APINOTES-3-NEXT: var IAMStruct1APINoteVar: CDouble
 // PRINT-APINOTES-3:      extension Struct1 {
-// PRINT-APINOTES-3-NEXT:   var oldApiNoteVar: {{C?}}Double
+// PRINT-APINOTES-3-NEXT:   var oldApiNoteVar: CDouble
 // PRINT-APINOTES-3-NEXT:   @available(swift, introduced: 4.2, renamed: "Struct1.oldApiNoteVar")
-// PRINT-APINOTES-3-NEXT:   var newApiNoteVar: {{C?}}Double
+// PRINT-APINOTES-3-NEXT:   var newApiNoteVar: CDouble
 // PRINT-APINOTES-3-NEXT:   @available(swift, introduced: 4.2, renamed: "IAMStruct1APINoteVarInSwift4")
-// PRINT-APINOTES-3-NEXT:   var apiNoteVarInSwift4: {{C?}}Double
+// PRINT-APINOTES-3-NEXT:   var apiNoteVarInSwift4: CDouble
 // PRINT-APINOTES-3-NEXT:   static func oldApiNoteMethod()
 // PRINT-APINOTES-3-NEXT:   @available(swift, introduced: 4.2, renamed: "Struct1.oldApiNoteMethod()")
 // PRINT-APINOTES-3-NEXT:   static func newApiNoteMethod()
-// PRINT-APINOTES-3-NEXT:   init(oldLabel _: {{(CInt|Int32)}})
+// PRINT-APINOTES-3-NEXT:   init(oldLabel _: CInt)
 // PRINT-APINOTES-3-NEXT:   @available(swift, introduced: 4.2, renamed: "Struct1.init(oldLabel:)")
-// PRINT-APINOTES-3-NEXT:   init(newLabel _: {{(CInt|Int32)}})
+// PRINT-APINOTES-3-NEXT:   init(newLabel _: CInt)
 // PRINT-APINOTES-3-NEXT:   typealias OldApiNoteType = Struct1.NewApiNoteType
-// PRINT-APINOTES-3-NEXT:   typealias NewApiNoteType = {{C?}}Double
+// PRINT-APINOTES-3-NEXT:   typealias NewApiNoteType = CDouble
 // PRINT-APINOTES-3-NEXT: }
 // PRINT-APINOTES-3-NOT: @available
-// PRINT-APINOTES-3:     var IAMStruct1APINoteVarInSwift4: {{C?}}Double
+// PRINT-APINOTES-3:     var IAMStruct1APINoteVarInSwift4: CDouble
 // PRINT-APINOTES-3:     @available(swift, obsoleted: 3, renamed: "Struct1.oldApiNoteMethod()")
 // PRINT-APINOTES-3-NEXT: func IAMStruct1APINoteFunction()
 // PRINT-APINOTES-3:     @available(swift, obsoleted: 3, renamed: "Struct1.init(oldLabel:)")
-// PRINT-APINOTES-3-NEXT: func IAMStruct1APINoteCreateFunction(_ _: {{(CInt|Int32)}}) -> Struct1
+// PRINT-APINOTES-3-NEXT: func IAMStruct1APINoteCreateFunction(_ _: CInt) -> Struct1
 // PRINT-APINOTES-3:      @available(swift, obsoleted: 3, renamed: "Struct1.OldApiNoteType")
 // PRINT-APINOTES-3-NEXT: typealias IAMStruct1APINoteType = Struct1.OldApiNoteType
 
 // PRINT-APINOTES-4:      @available(swift, obsoleted: 3, renamed: "Struct1.newApiNoteVar")
-// PRINT-APINOTES-4-NEXT: var IAMStruct1APINoteVar: {{C?}}Double
+// PRINT-APINOTES-4-NEXT: var IAMStruct1APINoteVar: CDouble
 // PRINT-APINOTES-4:      extension Struct1 {
-// PRINT-APINOTES-4-NEXT:   var newApiNoteVar: {{C?}}Double
+// PRINT-APINOTES-4-NEXT:   var newApiNoteVar: CDouble
 // PRINT-APINOTES-4-NEXT:   @available(swift, obsoleted: 4.2, renamed: "Struct1.newApiNoteVar")
-// PRINT-APINOTES-4-NEXT:   var oldApiNoteVar: {{C?}}Double
-// PRINT-APINOTES-4-NEXT:   var apiNoteVarInSwift4: {{C?}}Double
+// PRINT-APINOTES-4-NEXT:   var oldApiNoteVar: CDouble
+// PRINT-APINOTES-4-NEXT:   var apiNoteVarInSwift4: CDouble
 // PRINT-APINOTES-4-NEXT:   static func newApiNoteMethod()
 // PRINT-APINOTES-4-NEXT:   @available(swift, obsoleted: 4.2, renamed: "Struct1.newApiNoteMethod()")
 // PRINT-APINOTES-4-NEXT:   static func oldApiNoteMethod()
-// PRINT-APINOTES-4-NEXT:   init(newLabel _: {{(CInt|Int32)}})
+// PRINT-APINOTES-4-NEXT:   init(newLabel _: CInt)
 // PRINT-APINOTES-4-NEXT:   @available(swift, obsoleted: 4.2, renamed: "Struct1.init(newLabel:)")
-// PRINT-APINOTES-4-NEXT:   init(oldLabel _: {{(CInt|Int32)}})
-// PRINT-APINOTES-4-NEXT:   typealias NewApiNoteType = {{C?}}Double
+// PRINT-APINOTES-4-NEXT:   init(oldLabel _: CInt)
+// PRINT-APINOTES-4-NEXT:   typealias NewApiNoteType = CDouble
 // PRINT-APINOTES-4-NEXT:   @available(swift, obsoleted: 4.2, renamed: "Struct1.NewApiNoteType")
 // PRINT-APINOTES-4-NEXT:   typealias OldApiNoteType = Struct1.NewApiNoteType
 // PRINT-APINOTES-4-NEXT: }
 // PRINT-APINOTES-4:      @available(swift, obsoleted: 4.2, renamed: "Struct1.apiNoteVarInSwift4")
-// PRINT-APINOTES-4-NEXT: var IAMStruct1APINoteVarInSwift4: {{C?}}Double
+// PRINT-APINOTES-4-NEXT: var IAMStruct1APINoteVarInSwift4: CDouble
 // PRINT-APINOTES-4:     @available(swift, obsoleted: 3, renamed: "Struct1.newApiNoteMethod()")
 // PRINT-APINOTES-4-NEXT: func IAMStruct1APINoteFunction()
 // PRINT-APINOTES-4:     @available(swift, obsoleted: 3, renamed: "Struct1.init(newLabel:)")
-// PRINT-APINOTES-4-NEXT: func IAMStruct1APINoteCreateFunction(_ _: {{(CInt|Int32)}}) -> Struct1
+// PRINT-APINOTES-4-NEXT: func IAMStruct1APINoteCreateFunction(_ _: CInt) -> Struct1
 // PRINT-APINOTES-4:      @available(swift, obsoleted: 3, renamed: "Struct1.NewApiNoteType")
 // PRINT-APINOTES-4-NEXT: typealias IAMStruct1APINoteType = Struct1.NewApiNoteType
 
--- test/Interop/Cxx/templates/partially-pre-defined-class-template-irgen.swift
+++ test/Interop/Cxx/templates/partially-pre-defined-class-template-irgen.swift
@@ -1,4 +1,4 @@
-// RUN: %target-swift-emit-ir %s -I %S/Inputs -enable-experimental-cxx-interop | %FileCheck %s
+// RUN: %target-swift-emit-ir %s -I %S/Inputs -cxx-interoperability-mode=default | %FileCheck %s
 import PartiallyPreDefinedClassTemplate
 
 public func getWrappedMagicInt() -> CInt {
@@ -7,25 +7,7 @@ public func getWrappedMagicInt() -> CInt {
   return magicInt.getValuePlusArg(13)
 }
 
-// CHECK: define {{(protected |dllexport )?}}swiftcc i32 @"$s4main18getWrappedMagicInts5Int32VyF"()
-// CHECK: %magicInt = alloca %TSo0030MagicWrapperIntWrapper_bHAFhxbV, align 4
-// CHECK: %magicInt.t = getelementptr inbounds nuw %TSo0030MagicWrapperIntWrapper_bHAFhxbV, ptr %magicInt, i32 0, i32 0
-// CHECK: {{(invoke|call)}} {{(noundef )?}}i32 @{{_ZNK12MagicWrapperI10IntWrapperE15getValuePlusArgEi|"\?getValuePlusArg@\?\$MagicWrapper@UIntWrapper@@@@QEBAHH@Z"}}(ptr {{[^,]*}}%magicInt, i32 {{(noundef )?}}13)
-
-// CHECK: define {{(weak_odr|linkonce_odr)}}{{( dso_local)?}} {{(noundef )?}}i32 @{{_ZNK12MagicWrapperI10IntWrapperE15getValuePlusArgEi|"\?getValuePlusArg@\?\$MagicWrapper@UIntWrapper@@@@QEBAHH@Z"}}(ptr {{[^,]*}} %this, i32 {{(noundef )?}}%arg)
-// CHECK: %this.addr = alloca ptr, align {{4|8}}
-// CHECK: store ptr %this, ptr %this.addr, align {{4|8}}
-// CHECK: %this1 = load ptr, ptr %this.addr, align {{4|8}}
-// CHECK: %t = getelementptr inbounds nuw %struct.MagicWrapper, ptr %this1, i32 0, i32 0
-// CHECK: %call = call {{(noundef )?}}i32 @{{_ZNK10IntWrapper8getValueEv|"\?getValue@IntWrapper@@QEBAHXZ"}}(ptr {{[^,]*}} %t)
-// CHECK: [[ARG:%.*]] = load i32, ptr %arg.addr, align 4
-// CHECK: %add = add nsw i32 %call, [[ARG]]
-// CHECK: ret i32 %add
-
-// CHECK: define linkonce_odr{{( dso_local)?}} {{(noundef )?}}i32 @{{_ZNK10IntWrapper8getValueEv|"\?getValue@IntWrapper@@QEBAHXZ"}}(ptr {{[^,]*}} %this)
-// CHECK: %this.addr = alloca ptr, align {{4|8}}
-// CHECK: store ptr %this, ptr %this.addr, align {{4|8}}
-// CHECK: %this1 = load ptr, ptr %this.addr, align {{4|8}}
-// CHECK: %value = getelementptr inbounds nuw %struct.IntWrapper, ptr %this1, i32 0, i32 0
-// CHECK: [[VALUE:%.*]] = load i32, ptr %value, align 4
-// CHECK: ret i32 [[VALUE]]
+// CHECK-LABEL: define {{.*}}swiftcc i32 @"$s4main18getWrappedMagicInts5Int32VyF"()
+// CHECK: i32 @{{_ZNK12MagicWrapperI10IntWrapperE15getValuePlusArgEi|"\?getValuePlusArg@\?\$MagicWrapper@UIntWrapper@@@@QEBAHH@Z"}}(ptr {{[^,]*}}, i32 {{(noundef )?}}13)
+// CHECK: define {{.*}}i32 @{{_ZNK12MagicWrapperI10IntWrapperE15getValuePlusArgEi|"\?getValuePlusArg@\?\$MagicWrapper@UIntWrapper@@@@QEBAHH@Z"}}(
+// CHECK: define {{.*}}i32 @{{_ZNK10IntWrapper8getValueEv|"\?getValue@IntWrapper@@QEBAHXZ"}}(
