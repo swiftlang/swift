0b964e7776ea60b9b4b3690605e10a4ca471c7d0
Anton Korobeynikov <anton@korobeynikov.info>
2026-08-04 15:22:13 -0700

    Fix address lowering of unchecking cast: pass original formal type, not the one derived from the fully abstracted substituted type.

--- test/Interpreter/function_metatypes.swift
+++ test/Interpreter/function_metatypes.swift
@@ -1,6 +1,5 @@
 // RUN: %target-run-simple-swift | %FileCheck %s
 // REQUIRES: executable_test
-// XFAIL: swift_test_mode_optimize_none_with_opaque_values
 
 protocol ProtocolHasInOut {
   associatedtype Input
