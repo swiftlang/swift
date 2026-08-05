3fb26b9cdbbeaca6060c659b64b73b318a43e333
Guillaume Lessard <guillaume.lessard@apple.com>
2026-08-05 13:46:31 -0700

    [test] encode expectations
    -  assertionFailure(“”) and assert(false, “”) unexpectedly behaved differently, and they should not. A previous fix caused a regression in compilation behaviour.
    - These tests probe the behaviour of assertionFailure(“”) for all four combinations of (-O, -Onone) and (-assert-config Debug, -assert-config Release).
    - The compilation regression is guarded by a -verify test.

--- validation-test/stdlib/AssertDiagnosticsSIL.swift
+++ validation-test/stdlib/AssertDiagnosticsSIL.swift
@@ -1,7 +1,17 @@
-// RUN: %target-swift-frontend %s -emit-sil -verify
+// RUN: %target-swift-frontend -emit-sil -verify -O -assert-config Debug       %s -o /dev/null
+// RUN: %target-swift-frontend -emit-sil -verify -O -assert-config Release     %s -o /dev/null
+// RUN: %target-swift-frontend -emit-sil -verify -Onone -assert-config Debug   %s -o /dev/null
+// RUN: %target-swift-frontend -emit-sil -verify -Onone -assert-config Release %s -o /dev/null
 
 func assertionFailure_isNotNoreturn() -> Int {
   _ = 0 // Don't implicitly return the assertionFailure call.
   assertionFailure("")
 } // expected-error {{missing return in global function expected to return 'Int'}}
 
+func rdar183643880Reproducer(item: String?) {
+  guard let item else {
+    assertionFailure("")
+  } // expected-error {{'guard' body must not fall through}}
+
+  print(".some: \(item)")
+}
