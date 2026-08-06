439db879bde9cbf5abe2aa44c46ceb4ba9258e6d
Max Desiatov <m_desiatov@apple.com>
2026-08-06 18:18:55 +0100

    Mark `InFlightSubstitution-lookupConformance-577ddb.swift` as UNSUPPORTED
    rdar://184244867

--- validation-test/compiler_crashers/CxxInterop/InFlightSubstitution-lookupConformance-577ddb.swift
+++ validation-test/compiler_crashers/CxxInterop/InFlightSubstitution-lookupConformance-577ddb.swift
@@ -3,6 +3,7 @@
 // RUN: not %{python} %swift_src_root/test/Inputs/timeout.py 60 \
 // RUN:             %target-swift-frontend -typecheck -experimental-allow-module-with-compiler-errors -cxx-interoperability-mode=default -emit-clang-header-min-access internal -emit-clang-header-path /dev/null %s || \
 // RUN: not --crash %target-swift-frontend -typecheck -experimental-allow-module-with-compiler-errors -cxx-interoperability-mode=default -emit-clang-header-min-access internal -emit-clang-header-path /dev/null %s
+// UNSUPPORTED: OS=linux-gnu
 enum a<b: Hashable> {
   case (a<[b]>)
 }
