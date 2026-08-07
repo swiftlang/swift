09973583fe6ef13549bf07d2adba78726d254974
Carl Peto <carlpeto@Carls-MacBook-Pro.local>
2026-08-07 15:15:30 +0100

    [Backtracing] [Runtime] Improve some error checking to improve stability.

--- stdlib/public/RuntimeModule/Dwarf.swift
+++ stdlib/public/RuntimeModule/Dwarf.swift
@@ -1813,7 +1813,9 @@ class DwarfReader<S: DwarfSource & AnyObject> {
       if case let .address(highPCAddr) = highPCVal {
         highPC = highPCAddr
       } else if let highPCOffset = highPCVal.uint64Value() {
-        highPC = lowPC + highPCOffset
+        let (sum, overflowed) = lowPC.addingReportingOverflow(highPCOffset)
+        guard !overflowed else { return }
+        highPC = sum
       } else {
         return
       }
@@ -1827,8 +1829,51 @@ class DwarfReader<S: DwarfSource & AnyObject> {
            filename: filename,
            line: Int(callLine),
            column: Int(callColumn)))
-    } else if let rangeVal = attributes[.DW_AT_ranges] {
-      try forEachRange(of: rangeVal, unit: unit) { lowPC, highPC in
+    } else if let rangeVal = attributes[.DW_AT_ranges],
+              let rangesSection = rangesSection,
+              case let .sectionOffset(offset) = rangeVal,
+              unit.version < 5 {
+      // We don't support .debug_rnglists at present (which is what we'd
+      // have if unit.version is 5 or higher).
+      var rangeCursor = ImageSourceCursor(source: rangesSection,
+                                          offset: offset)
+      var rangeBase: Address = unit.lowPC ?? 0
+
+      while true {
+        let beginning: Address
+        let ending: Address
+
+        switch unit.addressSize {
+          case 4:
+            beginning = UInt64(maybeSwap(try rangeCursor.read(as: UInt32.self)))
+            ending = UInt64(maybeSwap(try rangeCursor.read(as: UInt32.self)))
+            if beginning == 0xffffffff {
+              rangeBase = ending
+              continue
+            }
+          case 8:
+            beginning = maybeSwap(try rangeCursor.read(as: UInt64.self))
+            ending = maybeSwap(try rangeCursor.read(as: UInt64.self))
+            if beginning == 0xffffffffffffffff {
+              rangeBase = ending
+              continue
+            }
+          default:
+            throw DwarfError.badAddressSize(unit.addressSize)
+        }
+
+        if beginning == 0 && ending == 0 {
+          break
+        }
+
+        let (lowPC, lowOverflowed)
+          = beginning.addingReportingOverflow(rangeBase)
+        let (highPC, highOverflowed)
+          = ending.addingReportingOverflow(rangeBase)
+        if lowOverflowed || highOverflowed || highPC < lowPC {
+          continue
+        }
+
         fn(CallSiteInfo(
              depth: depth,
              rawName: rawName,
@@ -1906,7 +1951,9 @@ class DwarfReader<S: DwarfSource & AnyObject> {
       if case let .address(highPCAddr) = highPCVal {
         highPC = highPCAddr
       } else if let highPCOffset = highPCVal.uint64Value() {
-        highPC = lowPC + highPCOffset
+        let (sum, overflowed) = lowPC.addingReportingOverflow(highPCOffset)
+        guard !overflowed else { return }
+        highPC = sum
       } else {
         return
       }
