121426748b01e96caf825b841eebe7f94472c1c4
cuishuang <imcusg@gmail.com>
2026-08-14 22:21:45 +0800

    [Diagnostics] Report the required parameter type for contextual members
    Signed-off-by: cuishuang <imcusg@gmail.com>

--- lib/Sema/CSDiagnostics.cpp
+++ lib/Sema/CSDiagnostics.cpp
@@ -2847,6 +2847,10 @@ bool ContextualFailure::diagnoseAsError() {
 
     ParameterListInfo info(
         params, choice, hasAppliedSelf(solution, overload->choice));
+    auto missingParamIdx = llvm::find_if(
+        indices(params), [&info](const unsigned paramIdx) -> bool {
+          return !info.hasDefaultArgument(paramIdx);
+        });
     auto numMissingArgs = llvm::count_if(
         indices(params), [&info](const unsigned paramIdx) -> bool {
           return !info.hasDefaultArgument(paramIdx);
@@ -2871,7 +2875,7 @@ bool ContextualFailure::diagnoseAsError() {
       }
     } else {
       emitDiagnostic(diag::expected_argument_in_contextual_member,
-                     choice, params.front().getPlainType());
+                     choice, params[*missingParamIdx].getPlainType());
     }
 
     return true;
