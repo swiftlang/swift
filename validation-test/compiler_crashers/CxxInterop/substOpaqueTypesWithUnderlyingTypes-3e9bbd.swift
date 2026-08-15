fdd36cb29af0604328922ea93ceab591a6d478ad
Finagolfin <finagolfin@tuta.io>
2026-08-15 13:52:12 +0530

    [test][linux] Disable C++ Interop crasher that keeps timing out on toolchain CI

--- validation-test/compiler_crashers/CxxInterop/substOpaqueTypesWithUnderlyingTypes-3e9bbd.swift
+++ validation-test/compiler_crashers/CxxInterop/substOpaqueTypesWithUnderlyingTypes-3e9bbd.swift
@@ -3,6 +3,10 @@
 // RUN: not %{python} %swift_src_root/test/Inputs/timeout.py 60 \
 // RUN:             %target-swift-frontend -typecheck -experimental-allow-module-with-compiler-errors -cxx-interoperability-mode=default -emit-clang-header-min-access internal -emit-clang-header-path /dev/null %s || \
 // RUN: not --crash %target-swift-frontend -typecheck -experimental-allow-module-with-compiler-errors -cxx-interoperability-mode=default -emit-clang-header-min-access internal -emit-clang-header-path /dev/null %s
+
+// This test sometimes still times out on linux, #91374
+// UNSUPPORTED: OS=linux-gnu
+
 protocol a
   func b -> some a {
     struct c<d, e>: a {
