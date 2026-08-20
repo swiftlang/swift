30b2d3e732651bb0db043ffe9628c93b33883757
Mykola Pokhylets <mykola.pokhylets@gmail.com>
2026-08-20 21:26:55 +0200

    Revert "Using ClosureExpr instead of ConversionPair in CollectionUpcastConversionExpr"
    This reverts commit 551a2cec6cb14821351437dcad747117d5fa356d.
    
    # Conflicts:
    #	include/swift/AST/Expr.h
    #	include/swift/Sema/ConstraintSystem.h
    #	lib/Sema/CSApply.cpp
    #	lib/Sema/MiscDiagnostics.cpp

--- lib/Sema/TypeCheckAvailability.cpp
+++ lib/Sema/TypeCheckAvailability.cpp
@@ -2312,13 +2312,8 @@ class ExprAvailabilityWalker : public BaseDiagnosticWalker {
   }
 
 public:
-  explicit ExprAvailabilityWalker(const ExportContext &Where,
-                                  bool preconcurrency = false)
-      : Context(Where.getDeclContext()->getASTContext()), Where(Where) {
-    if (preconcurrency) {
-      PreconcurrencyCalleeStack.push_back(true);
-    }
-  }
+  explicit ExprAvailabilityWalker(const ExportContext &Where)
+    : Context(Where.getDeclContext()->getASTContext()), Where(Where) {}
 
   PreWalkAction walkToArgumentPre(const Argument &Arg) override {
     // Arguments should be walked in their own member access context which
@@ -2473,11 +2468,15 @@ public:
                                               EE->getLoc(),
                                               Where.getDeclContext());
 
+      bool preconcurrency = false;
+      if (!PreconcurrencyCalleeStack.empty()) {
+        preconcurrency = PreconcurrencyCalleeStack.back();
+      }
+
       for (ProtocolConformanceRef C : EE->getConformances()) {
-        diagnoseConformanceAvailability(
-            E->getLoc(), C, Where, Type(), Type(),
-            /*useConformanceAvailabilityErrorsOpt=*/true,
-            /*preconcurrency=*/preconcurrency());
+        diagnoseConformanceAvailability(E->getLoc(), C, Where, Type(), Type(),
+                                        /*useConformanceAvailabilityErrorsOpt=*/true,
+                                        /*preconcurrency=*/preconcurrency);
       }
     }
 
@@ -2516,25 +2515,18 @@ public:
     // differ, e.g for things like `guard #available(...)`.
     class StmtRecurseWalker : public BaseDiagnosticWalker {
       DeclContext *DC;
-      bool preconcurrency;
 
     public:
-      StmtRecurseWalker(DeclContext *DC, bool inPreconcurrency)
-          : DC(DC), preconcurrency(inPreconcurrency) {}
+      StmtRecurseWalker(DeclContext *DC) : DC(DC) {}
 
       PreWalkResult<Expr *> walkToExprPre(Expr *E) override {
-        diagnoseExprAvailability(E, DC, preconcurrency);
+        diagnoseExprAvailability(E, DC);
         return Action::SkipNode(E);
       }
     };
-    StmtRecurseWalker W(Where.getDeclContext(), preconcurrency());
+    StmtRecurseWalker W(Where.getDeclContext());
     S->walk(W);
     return Action::SkipNode(S);
-  }
-
-  bool preconcurrency() const {
-    return PreconcurrencyCalleeStack.empty() ? false
-                                             : PreconcurrencyCalleeStack.back();
   }
 
   bool
@@ -2737,15 +2729,7 @@ private:
     auto where = ExportContext::forFunctionBody(closure, closure->getStartLoc());
     if (where.isImplicit())
       return;
-
-    bool preconcurrency = false;
-    if (auto closureExpr = dyn_cast<ClosureExpr>(closure)) {
-      if (closureExpr->isConversionClosure()) {
-        preconcurrency = this->preconcurrency();
-      }
-    }
-
-    ExprAvailabilityWalker walker(where, preconcurrency);
+    ExprAvailabilityWalker walker(where);
 
     // Manually dive into the body
     closure->getBody()->walk(walker);
@@ -3120,8 +3104,7 @@ bool swift::diagnoseDeclAvailability(const ValueDecl *D, SourceRange R,
 }
 
 /// Diagnose uses of unavailable declarations.
-void swift::diagnoseExprAvailability(const Expr *E, DeclContext *DC,
-                                     bool preconcurrency) {
+void swift::diagnoseExprAvailability(const Expr *E, DeclContext *DC) {
   auto where = ExportContext::forFunctionBody(DC, E->getStartLoc());
   // FIXME: We skip availability checking for expressions in synthesized
   // derivation macros to match the behavior of the old built-in conformance
@@ -3132,7 +3115,7 @@ void swift::diagnoseExprAvailability(const Expr *E, DeclContext *DC,
   // https://github.com/swiftlang/swift/issues/77589.
   if (where.isImplicit() || E->isFromSyntheticMacroExpansion(DC))
     return;
-  ExprAvailabilityWalker walker(where, preconcurrency);
+  ExprAvailabilityWalker walker(where);
   const_cast<Expr *>(E)->walk(walker);
 }
 
