a63c00c2003e0a6e215f2a4d2c8fa943b4551cd8
John Hui <j-hui@apple.com>
2026-08-20 23:35:23 -0700

    [cxx-interop] Enable ImportCxxMembersLazily by default
    Also, only skip eagerly imported constructors and subscripts during
    member loading. This ensures that non-constructor member functions that
    are imported with SWIFT_NAME(init(...)) don't get skipped.
    
    Also, make function template decls explicitly eager. These were always
    eagerly imported, but this patch makes that explicit.
    
    rdar://185444478

--- test/Interop/Cxx/class/method/Inputs/swift-name-different-type.h
+++ test/Interop/Cxx/class/method/Inputs/swift-name-different-type.h
@@ -5,15 +5,11 @@ struct B {
   // expected-to-note @+1 {{while importing 'renamedFrom0'}}
   void renamedFrom0(A &a) const
     __attribute__((swift_name("A.renamedFrom0(self:)")));
-  // expected-from-warning@-2 {{swift_name cannot be used to import a non-static C++ method as a member of a different type}}
-  // expected-from-note @-3 {{while importing 'renamedFrom0'}}
 
   // expected-to-warning@+2 {{swift_name cannot be used to import a non-static C++ method as a member of a different type}}
   // expected-to-note @+1 {{while importing 'renamedFrom1'}}
   void renamedFrom1(A &a, int i) const
     __attribute__((swift_name("A.renamedFrom1(self:j:)")));
-  // expected-from-warning@-2 {{swift_name cannot be used to import a non-static C++ method as a member of a different type}}
-  // expected-from-note @-3 {{while importing 'renamedFrom1'}}
 
   void other() const;
 };
