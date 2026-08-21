c5a2823c7dc086fa9aeb486ffa1a85a5e182516a
John Hui <j-hui@apple.com>
2026-08-21 15:34:07 -0700

    [cxx-interop][test] Make swift-name-different-type resilient to lazy imports

--- test/Interop/Cxx/class/method/Inputs/swift-name-different-type.h
+++ test/Interop/Cxx/class/method/Inputs/swift-name-different-type.h
@@ -1,7 +1,19 @@
 struct A {};
 
 struct B {
-  void b(A &a, int e) const // expected-warning {{swift_name cannot be used to import a non-static C++ method as a member of a different type}} expected-note {{while importing 'b'}}
-    __attribute__((swift_name("A.c(self:d:)")));
+  // expected-to-warning@+2 {{swift_name cannot be used to import a non-static C++ method as a member of a different type}}
+  // expected-to-note @+1 {{while importing 'renamedFrom0'}}
+  void renamedFrom0(A &a) const
+    __attribute__((swift_name("A.renamedFrom0(self:)")));
+  // expected-from-warning@-2 {{swift_name cannot be used to import a non-static C++ method as a member of a different type}}
+  // expected-from-note @-3 {{while importing 'renamedFrom0'}}
+
+  // expected-to-warning@+2 {{swift_name cannot be used to import a non-static C++ method as a member of a different type}}
+  // expected-to-note @+1 {{while importing 'renamedFrom1'}}
+  void renamedFrom1(A &a, int i) const
+    __attribute__((swift_name("A.renamedFrom1(self:j:)")));
+  // expected-from-warning@-2 {{swift_name cannot be used to import a non-static C++ method as a member of a different type}}
+  // expected-from-note @-3 {{while importing 'renamedFrom1'}}
+
   void other() const;
 };
