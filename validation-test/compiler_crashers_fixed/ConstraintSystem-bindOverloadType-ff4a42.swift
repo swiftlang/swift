9a3362896da07d062f977c137c2562c195f076a0
Hamish Knight <hamish_knight@apple.com>
2026-08-24 21:40:35 +0100

    [CS] Bail from holed keypath dynamic member constraint
    If we have a dynamic member lookup and the member type has already been
    bound to a hole, bail without attempting to solve the member constraint.
    Keypath dynamic member subscripts expect the applicable function
    constraint to be present, which is may not be for a hole since we don't
    attempt to solve them when all arguments are bound to holes.
    
    rdar://185642635

--- validation-test/compiler_crashers_fixed/ConstraintSystem-bindOverloadType-ff4a42.swift
+++ validation-test/compiler_crashers_fixed/ConstraintSystem-bindOverloadType-ff4a42.swift
@@ -1,5 +1,5 @@
 // {"kind":"typecheck","signature":"swift::constraints::ConstraintSystem::bindOverloadType(swift::constraints::SelectedOverload const&, swift::Type, swift::constraints::ConstraintLocator*, swift::DeclContext*)","signatureAssert":"Assertion failed: (constraints.size() == 1), function bindOverloadType","signatureNext":"ConstraintSystem::resolveOverload"}
-// RUN: not --crash %target-swift-frontend -typecheck %s
+// RUN: not %target-swift-frontend -typecheck %s
 @dynamicMemberLookup struct a {
   subscript(dynamicMember c: KeyPath<Int, Int>) -> Int
   func d(b: a) {
