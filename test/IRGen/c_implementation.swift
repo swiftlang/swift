655f247c21eae98baaca80dac5b3d913213ba907
Saleem Abdulrasool <compnerd@compnerd.org>
2026-08-23 18:57:13 -0700

    SIL: preserve Clang types for C implementations
    A Swift function matched to an imported C declaration discovers its Clang
    declaration after the initial function ext-info is formed. Lowering used that
    declaration for abstraction conventions but did not retain its function type.
    Attributes such as a non-default calling convention were therefore discarded
    before SIL.
    
    Store the matched Clang function pointer type in the SIL function ext-info, as
    lowering already does for ordinary imported C declarations. Verify that an
    @implementation @c function retains a preserve_most convention in SIL.

--- test/IRGen/c_implementation.swift
+++ test/IRGen/c_implementation.swift
@@ -2,8 +2,16 @@
 // RUN:   -disable-objc-interop \
 // RUN:   -F %clang-importer-sdk-path/frameworks %s \
 // RUN:   -import-objc-header %S/Inputs/objc_implementation.h -emit-ir \
+// RUN:   -use-clang-function-types \
 // RUN:   -target %target-future-triple > %t.ir
 // RUN: %FileCheck --input-file %t.ir %s
+// RUN: %target-swift-frontend(mock-sdk: %clang-importer-sdk) \
+// RUN:   -disable-objc-interop \
+// RUN:   -F %clang-importer-sdk-path/frameworks %s \
+// RUN:   -import-objc-header %S/Inputs/objc_implementation.h -emit-sil \
+// RUN:   -use-clang-function-types \
+// RUN:   -experimental-print-full-convention \
+// RUN:   -target %target-future-triple | %FileCheck %s --check-prefix=SIL
 
 @implementation @c
 public func implFunc(_ param: Int32) {}
@@ -14,10 +22,16 @@ public func implFuncCName(_ param: Int32) {}
 @implementation @c(implFuncRenamed_C)
 public func implFuncRenamed_Swift(param: Int32) {}
 
+@implementation @c
+public func implFuncPreserveMost(_ param: Int32) -> Int32 {
+  return param
+}
+
 public func fn() {
   implFunc(2)
   implFuncCName(3)
   implFuncRenamed_Swift(param: 4)
+  _ = implFuncPreserveMost(5)
 }
 
 /// implFunc(_:)
@@ -30,6 +44,11 @@ public func fn() {
 
 // CHECK-NOT: define{{.*}} swiftcc void @"$s16c_implementation13implFuncCNameyys5Int32VF"
 
+/// implFuncPreserveMost(_:)
+// SIL-LABEL: sil [asmname "implFuncPreserveMost"]
+// SIL-SAME: @convention(c, cType:
+// SIL-SAME: preserve_most
+
 /// fn()
 // CHECK-LABEL: define{{.*}} swiftcc void @"$s16c_implementation2fnyyF"
 // CHECK:   call void @implFunc
@@ -37,4 +56,3 @@ public func fn() {
 // CHECK:   call void @implFuncRenamed_C
 // CHECK:   ret void
 // CHECK: }
-
