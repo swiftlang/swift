950dcbffa0f3f5f8a01af1ee44938aa9005038fd
Stephen Canon <scanon@apple.com>
2026-08-26 12:23:07 -0400

    Bugfix for simd floating-point negation.

---
+++ test/stdlib/SIMDGenericFP.swift
@@ -0,0 +1,26 @@
+// RUN: %target-run-simple-swift
+// REQUIRES: executable_test
+
+import Foundation
+import StdlibUnittest
+
+let SIMDGenericFPTests = TestSuite("SIMD Generic FP")
+
+func getNegativeZero<T>(of: T.Type) -> T
+where T: SIMD, T.Scalar: BinaryFloatingPoint {
+  -T()
+}
+
+SIMDGenericFPTests.test("negation") {
+  let negZero = -getNegativeZero(of: SIMD4<Float>)
+  expectEqual(negZero[0].sign, .minus)
+  expectEqual(negZero[1].sign, .minus)
+  expectEqual(negZero[2].sign, .minus)
+  expectEqual(negZero[3].sign, .minus)
+  expectEqual(negZero[0], 0)
+  expectEqual(negZero[1], 0)
+  expectEqual(negZero[2], 0)
+  expectEqual(negZero[3], 0)
+}
+
+runAllTests()
