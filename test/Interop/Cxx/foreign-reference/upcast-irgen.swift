41b54caf8ee520e81eb0b799dd8dd8d633d314ab
Egor Zhdan <e_zhdan@apple.com>
2026-08-26 15:33:49 +0100

    Reapply "[cxx-interop] Enable ForeignReferenceTypeInheritance by default"
    This reverts commit 18bfe390bc2da90cfd9cbbbd4a6b80cac17b04df.
    
    rdar://182098358

--- test/Interop/Cxx/foreign-reference/upcast-irgen.swift
+++ test/Interop/Cxx/foreign-reference/upcast-irgen.swift
@@ -1,6 +1,4 @@
-// RUN: %target-swift-emit-irgen %s -I %S/Inputs -cxx-interoperability-mode=default -enable-experimental-feature ForeignReferenceTypeInheritance -validate-tbd-against-ir=none -disable-llvm-verify -Xcc -fignore-exceptions -target %target-swift-5.8-abi-triple | %FileCheck %s
-
-// REQUIRES: swift_feature_ForeignReferenceTypeInheritance
+// RUN: %target-swift-emit-irgen %s -I %S/Inputs -cxx-interoperability-mode=default -validate-tbd-against-ir=none -disable-llvm-verify -Xcc -fignore-exceptions -target %target-swift-5.8-abi-triple | %FileCheck %s
 
 import Upcast
 
