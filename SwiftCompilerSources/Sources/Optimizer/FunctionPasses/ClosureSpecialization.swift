7cab38f45ef8f058a7072704ed415249f3ba7f4c
Daniil Kovalev <dkovalev@accesssoftek.com>
2026-08-27 17:57:05 +0300

    [SILOptimizer] Fix compiler performance in closure specialization pass
    After #91103, compiling code with certain closure chains takes enormous
    amount of time: see report
    https://github.com/swiftlang/swift/pull/91103#issuecomment-5430613745
    
    The root cause of the issue is unbounded amount of triggers for iterative
    specialization of just-specialized callees.
    
    This patch resolves the issue by allowing iterative specialization call
    only from root pass invocation. This way, iteration depth does not
    exceed 1, while expected optimizations are still performed.

--- SwiftCompilerSources/Sources/Optimizer/FunctionPasses/ClosureSpecialization.swift
+++ SwiftCompilerSources/Sources/Optimizer/FunctionPasses/ClosureSpecialization.swift
@@ -58,10 +58,10 @@ private func log(prefix: Bool = true, _ message: @autoclosure () -> String) {
 let closureSpecialization = FunctionPass(name: "closure-specialization") {
   (function: Function, context: FunctionPassContext) in
 
-  runClosureSpecialization(function: function, context: context)
+  runClosureSpecialization(function: function, isRootInvocation: true, context: context)
 }
 
-func runClosureSpecialization(function: Function, context: FunctionPassContext) {
+func runClosureSpecialization(function: Function, isRootInvocation: Bool, context: FunctionPassContext) {
   guard function.hasOwnership else {
     return
   }
@@ -73,7 +73,7 @@ func runClosureSpecialization(function: Function, context: FunctionPassContext) 
 
     for inst in function.instructions {
       if let apply = inst as? FullApplySite {
-        if trySpecialize(apply: apply, context) {
+        if trySpecialize(apply: apply, isRootInvocation: isRootInvocation, context) {
           changed = true
         }
       }
@@ -200,7 +200,7 @@ let autodiffClosureSpecialization = FunctionPass(name: "autodiff-closure-special
       break
     }
 
-    if trySpecialize(apply: partialApplyInst, context) {
+    if trySpecialize(apply: partialApplyInst, isRootInvocation: true, context) {
       changed = true
     }
 
@@ -217,7 +217,7 @@ let autodiffClosureSpecialization = FunctionPass(name: "autodiff-closure-special
 
 // ===================== Utility functions and extensions ===================== //
 
-private func trySpecialize(apply: ApplySite, _ context: FunctionPassContext) -> Bool {
+private func trySpecialize(apply: ApplySite, isRootInvocation: Bool, _ context: FunctionPassContext) -> Bool {
   guard isCalleeSpecializable(of: apply),
         let specialization = analyzeArguments(of: apply, context)
   else {
@@ -255,7 +255,7 @@ private func trySpecialize(apply: ApplySite, _ context: FunctionPassContext) -> 
 
   specialization.deleteDeadClosures(context)
 
-  if isNewFunction {
+  if isNewFunction && isRootInvocation {
     /// Further specialization rounds can leave the cloned callee holding a specializable closure and an `apply` which
     /// takes this closure as an argument. Consider the case when this specializable closure captures an argument from
     /// the cloned callee, and this argument by itself is a specializable closure constructed in caller and passed to
@@ -321,7 +321,7 @@ private func trySpecialize(apply: ApplySite, _ context: FunctionPassContext) -> 
 
     context.buildSpecializedFunction(specializedFunction: specializedFunction) {
       (specializedFunction, specializedContext) in
-      runClosureSpecialization(function: specializedFunction, context: specializedContext)
+      runClosureSpecialization(function: specializedFunction, isRootInvocation: false, context: specializedContext)
     }
   }
 
