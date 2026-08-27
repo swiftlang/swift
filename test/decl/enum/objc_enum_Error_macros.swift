906ef20379aa9951f08c72f7b4617e61abbbfc2d
Paul Passeron <paul_passeron@apple.com>
2026-08-27 16:43:28 +0100

    [test] Add tests for objc enum `Error` using macros

---
+++ test/decl/enum/objc_enum_Error_macros.swift
@@ -0,0 +1,22 @@
+// RUN: %target-typecheck-verify-swift -load-plugin-library %swift-plugin-dir/%target-library-name(SwiftMacros) -enable-experimental-feature DeriveConformancesViaMacros 
+
+// REQUIRES: objc_interop
+// REQUIRES: swift_feature_DeriveConformancesViaMacros
+
+import Foundation
+
+func acceptBridgeableNSError<E : _ObjectiveCBridgeableError>(_ e: E) { } // expected-note {{where 'E' = 'E3'}}
+
+@objc enum E2 : Int, Error {
+  case A = 1
+}
+
+acceptBridgeableNSError(E2.A)
+
+
+@objc enum E3 : Int {
+  case A = 1
+}
+
+acceptBridgeableNSError(E3.A)
+// expected-error@-1{{global function 'acceptBridgeableNSError' requires that 'E3' conform to '_ObjectiveCBridgeableError'}}
