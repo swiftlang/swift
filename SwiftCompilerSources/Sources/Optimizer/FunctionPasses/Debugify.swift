ff8110e7fa6bb8fc029a222f1679802c913d4b4d
Emil Pedersen <emil_pedersen2@apple.com>
2026-08-28 12:49:29 +0100

    Debugify: Create a Debugify Pass
    This pass adds a debug_value with a debug reconstruction block to all
    instruction results in all functions. When it is enabled, with -Xllvm
    -sil-enable-debugify, it can catch mishandled debug info with verifier
    assertions or paired with lost variables statistics.

---
+++ SwiftCompilerSources/Sources/Optimizer/FunctionPasses/Debugify.swift
@@ -0,0 +1,67 @@
+//===--- Debugify.swift ---------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2026 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+import SIL
+
+/// Attaches a `debug_value` with a no-op debug reconstruction block to every
+/// instruction result.
+///
+/// This is a stress tool for the debug info representation. It allows detecting
+/// passes that mishandle `debug_value` instructions, by increasing the chances
+/// of running into a type-chain verifier failure.
+///
+/// Enabled with `-Xllvm -sil-enable-debugify`.
+let debugifyPass = FunctionPass(name: "debugify") {
+  (function: Function, context: FunctionPassContext) in
+  
+  // TODO: We could also handle phi nodes.
+
+  for inst in function.instructions {
+    // Existing debug_values all get a new no-op reconstruction block.
+    if let debugValue = inst as? DebugValueInst {
+      if debugValue.debugReconstructionBlock == nil, debugValue.operands.count == 1,
+         !(debugValue.operands[0].value is Undef) {
+        _ = debugValue.getOrCreateDebugReconstructionBlock()
+      }
+      continue
+    }
+    // No result, nothing to do.
+    if inst.results.isEmpty {
+      continue
+    }
+
+    // Some instructions don't support debug_values on their result.
+    if inst is DropDeinitInst {
+      continue
+    }
+
+    let builder = Builder(after: inst, context)
+    for result in inst.results {
+      // Ignore empty tuples.
+      if result.type.isVoid {
+        continue
+      }
+      // Don't add a new debug_value if there's already one.
+      if result.uses.contains(where: { $0.instruction is DebugValueInst }) {
+        continue
+      }
+      // Store borrow doesn't allow debug values on its destination.
+      if result.uses.contains(where: { use in
+        use.instruction is StoreBorrowInst && use.index == 1
+      }) {
+        continue
+      }
+      // Returns nil for values that cannot carry a reconstruction block.
+      builder.createDebugifyDebugValue(value: result)
+    }
+  }
+}
