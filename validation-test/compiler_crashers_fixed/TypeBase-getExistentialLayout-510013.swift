56d9489a6323e48b51c164e8b8b20100387781e2
Konrad `ktoso` Malawski <ktoso@apple.com>
2026-08-30 11:01:54 +0900

    Remove crash info line from fixed test

--- validation-test/compiler_crashers_fixed/TypeBase-getExistentialLayout-510013.swift
+++ validation-test/compiler_crashers_fixed/TypeBase-getExistentialLayout-510013.swift
@@ -1,4 +1,3 @@
-// {"kind":"typecheck","original":"66895c35","signature":"swift::TypeBase::getExistentialLayout()","signatureAssert":"Assertion failed: (isa<To>(Val) && \"cast<Ty>() argument of incompatible type!\"), function cast","signatureNext":"checkDistributedSerializationRequirementIsExactlyCodable"}
 // RUN: not %target-swift-frontend -typecheck %s
 // REQUIRES: OS=macosx
 import Distributed
