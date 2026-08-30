6d94a3e25e3753a2a849cf1bfb8834e78ce6cb99
Konrad Malawski <ktoso@apple.com>
2026-08-30 10:58:48 +0900

    [Distributed] Move two fixed compiler crashers to _fixed

--- validation-test/compiler_crashers_fixed/TypeBase-getExistentialLayout-92011e.swift
+++ validation-test/compiler_crashers_fixed/TypeBase-getExistentialLayout-92011e.swift
@@ -1,5 +1,5 @@
 // {"kind":"typecheck","original":"8d81c2b7","signature":"swift::TypeBase::getExistentialLayout()","signatureAssert":"Assertion failed: (isa<To>(Val) && \"cast<Ty>() argument of incompatible type!\"), function cast","signatureNext":"checkDistributedTargetResultType"}
-// RUN: not --crash %target-swift-frontend -typecheck %s
+// RUN: not %target-swift-frontend -typecheck %s
 // REQUIRES: OS=macosx
 import Distributed
 distributed actor a<d: DistributedActorSystem> {
