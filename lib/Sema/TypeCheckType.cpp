96970c23cbee119031bc347c642e46c9d1d5264a
Developer <dev@example.com>
2026-09-09 18:40:19 +0000

    [Sema] Reject isolated in inheritance clauses

--- lib/Sema/TypeCheckType.cpp
+++ lib/Sema/TypeCheckType.cpp
@@ -5749,10 +5749,9 @@ TypeResolver::resolveOwnershipTypeRepr(OwnershipTypeRepr *repr,
 NeverNullType
 TypeResolver::resolveIsolatedTypeRepr(IsolatedTypeRepr *repr,
                                       TypeResolutionOptions options) {
-  // isolated is only value for non-EnumCaseDecl parameters.
-  if ((!options.is(TypeResolverContext::FunctionInput) ||
-       options.hasBase(TypeResolverContext::EnumElementDecl)) &&
-      !options.is(TypeResolverContext::Inherited)) {
+  // isolated is only valid for non-EnumCaseDecl parameters.
+  if (!options.is(TypeResolverContext::FunctionInput) ||
+      options.hasBase(TypeResolverContext::EnumElementDecl)) {
     diagnoseInvalid(
         repr, repr->getSpecifierLoc(), diag::attr_only_on_parameters,
         "isolated");
