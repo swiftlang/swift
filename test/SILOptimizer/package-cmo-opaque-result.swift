c192f0e1af71bf1ac70ff24df8011f8c3999806c
Allan Shortlidge <shortlidge@apple.com>
2026-09-10 13:11:24 -0700

    test: Stop disabling availability checking in opaque result type tests.
    These tests pass -disable-availability-checking to silence diagnostics about
    'some' return types, which require the Swift 5.1 runtime. Pass
    -target %target-swift-5.1-abi-triple instead.

--- test/SILOptimizer/package-cmo-opaque-result.swift
+++ test/SILOptimizer/package-cmo-opaque-result.swift
@@ -5,7 +5,7 @@
 // RUN: -module-name=Lib -package-name Pkg \
 // RUN: -parse-as-library -emit-module -emit-module-path %t/Lib.swiftmodule -I%t \
 // RUN: -Xfrontend -experimental-package-cmo -Xfrontend -experimental-allow-non-resilient-access \
-// RUN: -O -wmo -enable-library-evolution -Xfrontend -disable-availability-checking
+// RUN: -O -wmo -enable-library-evolution -target %target-swift-5.1-abi-triple
 
 // RUN: %target-build-swift -module-name=Main -package-name Pkg -I%t -emit-sil -O %t/main.swift -o %t/Main-res.sil
 // RUN: %FileCheck %s --check-prefixes=CHECK-OPAQUE < %t/Main-res.sil
