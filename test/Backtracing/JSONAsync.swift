79ea54ecce742e3fcf4a0b7a04757686d0c72517
Carl Peto <carlpeto@Carls-MacBook-Pro.local>
2026-09-10 17:23:20 +0100

    [Backtracing] [Testing] Add extra debug logging for async bt tests

--- test/Backtracing/JSONAsync.swift
+++ test/Backtracing/JSONAsync.swift
@@ -3,7 +3,7 @@
 // RUN: %target-codesign %t/JSONAsync
 
 // RUN: env %env-SWIFT_BACKTRACE=enable=yes,demangle=no,cache=no,format=json,output-to=%t/crash.json %target-run %t/JSONAsync %t/JSONAsync.dSYM 2>&1 || true
-// RUN: %validate-json %t/crash.json | %FileCheck %s
+// RUN: %validate-json %t/crash.json | %FileCheck %s -dump-input-filter=all
 
 // UNSUPPORTED: use_os_stdlib
 // UNSUPPORTED: back_deployment_runtime
