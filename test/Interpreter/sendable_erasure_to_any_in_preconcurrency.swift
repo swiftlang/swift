057ad074144d135fd5d3922adf616e5c7b8cd2db
Slava Pestov <spestov@apple.com>
2026-09-11 16:52:16 -0400

    Build all execution tests with -solver-enable-diagnose-valid-salvage too
    This uncovered one more valid-in-salvage expression, which I extracted
    into its own test case.

--- test/Interpreter/sendable_erasure_to_any_in_preconcurrency.swift
+++ test/Interpreter/sendable_erasure_to_any_in_preconcurrency.swift
@@ -1,4 +1,4 @@
-// RUN: %target-run-simple-swift | %FileCheck %s
+// RUN: %target-run-simple-swift(-Xfrontend -solver-disable-diagnose-valid-salvage) | %FileCheck %s
 
 // REQUIRES: executable_test
 
