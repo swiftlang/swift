863fc112b950ef3c489e5bc69c49e4afb49be7c3
Meghana Gupta <meghana_gupta@apple.com>
2026-09-14 13:04:31 -0700

    Emit align attribute on indirect params
    rdar://186672118

--- test/IRGen/addressable.swift
+++ test/IRGen/addressable.swift
@@ -21,14 +21,14 @@ func getMutNE(_ holder: inout Holder) -> NE
 
 // The parameter cannot be 'captures(none)'.
 //
-// CHECK-LABEL: define{{.*}} swiftcc void @"$s1A17testAddressableInyAA2NEVAA6HolderVF"(ptr noalias %0)
+// CHECK-LABEL: define{{.*}} swiftcc void @"$s1A17testAddressableInyAA2NEVAA6HolderVF"(ptr noalias align 1 %0)
 public func testAddressableIn(_ holder: Holder) -> NE {
   getNE(holder)
 }
 
 // The parameter cannot be 'captures(none)'.
 //
-// CHECK-LABEL: define{{.*}} swiftcc void @"$s1A20testAddressableInoutyAA2NEVAA6HolderVzF"(ptr noalias %0)
+// CHECK-LABEL: define{{.*}} swiftcc void @"$s1A20testAddressableInoutyAA2NEVAA6HolderVzF"(ptr noalias align 1 %0)
 public func testAddressableInout(_ holder: inout Holder) -> NE {
   getMutNE(&holder)
 }
