981574f7dd2c3b11b466cc1dffd509d1f0e4c6bd
Anton Korobeynikov <anton@korobeynikov.info>
2026-09-17 09:07:51 -0700

    Fix opaque-value-related issues exposed by autodiff (#91243)
    Fixes #91131

--- lib/SILOptimizer/Differentiation/VJPCloner.cpp
+++ lib/SILOptimizer/Differentiation/VJPCloner.cpp
@@ -1726,9 +1726,9 @@ bool VJPCloner::Implementation::run() {
 
   // Generate pullback code.
   PullbackCloner PullbackCloner(cloner);
-  if (PullbackCloner.run()) {
+  if (PullbackCloner.run())
     errorOccurred = true;
-  }
+
   if (!errorOccurred) {
     auto *pm = &context.getPassManager();
     pm->getSwiftPassInvocation()->initializeNestedSwiftPassInvocation(vjp);
