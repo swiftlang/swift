2f5b713665bec5500f6dff801c7f24c82ad8147d
Konrad `ktoso` Malawski <ktoso@apple.com>
2026-09-18 07:11:21 +0900

    [Distributed][Embedded] Tests require optimized_stdlib
    Resolves rdar://187318455

--- test/Distributed/Embedded/distributed_embedded_any_some_param_diag.swift
+++ test/Distributed/Embedded/distributed_embedded_any_some_param_diag.swift
@@ -3,6 +3,7 @@
 // REQUIRES: OS=macosx
 // REQUIRES: swift_feature_Embedded
 // REQUIRES: swift_feature_EmbeddedDistributed
+// REQUIRES: optimized_stdlib
 
 // Embedded `@Resolvable` support mirrors the shapes non-embedded accepts:
 // `any P` / `some P` parameters and `any P` returns where `P` is `@Resolvable`
