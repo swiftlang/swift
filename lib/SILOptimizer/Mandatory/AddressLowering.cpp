0c9f414921220bc3ac03f438fa9b31ef8bcf8bb2
Jakub Florek <j_florek@apple.com>
2026-09-18 11:53:05 +0100

    Opaque Values: Handle the AddressOfRawLayout builtin in AddressLowering.
    AddressLowering hit its "Unimplemented builtin opaque value use" fatal error on `Builtin.addressOfRawLayout` with an address-only raw-layout operand. The fix rewrites the builtin's operand to the value's storage address, matching `getEnumTag`. Removes the opaque-values XFAIL from `test/Interpreter/raw_layout.swift`.
    
    Fixes part of rdar://182880837

--- lib/SILOptimizer/Mandatory/AddressLowering.cpp
+++ lib/SILOptimizer/Mandatory/AddressLowering.cpp
@@ -3718,7 +3718,8 @@ protected:
     case BuiltinValueKind::ResumeThrowingContinuationReturning:
     case BuiltinValueKind::AddTaskLocalValue:
     case BuiltinValueKind::TaskLocalValuePush:
-    case BuiltinValueKind::GetEnumTag: {
+    case BuiltinValueKind::GetEnumTag:
+    case BuiltinValueKind::AddressOfRawLayout: {
       SILValue opAddr = addrMat.materializeAddress(use->get());
       bi->setOperand(use->getOperandNumber(), opAddr);
       break;
