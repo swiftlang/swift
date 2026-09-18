2f9d7a6727aed889c05d815a93c98d9227227415
Guillaume Lessard <guillaume.lessard@apple.com>
2026-09-18 16:25:24 -0700

    [test] MutableSpan bulk copying operations

--- stdlib/public/core/Span/MutableSpan.swift
+++ stdlib/public/core/Span/MutableSpan.swift
@@ -705,7 +705,7 @@ extension MutableSpan {
   }
 
 #if !SPAN_COMPATIBILITY_STUB
-  /// Copies every element of source into this span, starting at index.
+  /// Copies every element of the source into this span, starting at index.
   ///
   /// This span must have enough space between `index` and its end for every
   /// element `source` provides.
@@ -737,7 +737,7 @@ extension MutableSpan {
     _precondition(next.isEmpty)
   }
 
-  /// Copies every element of source into this span, starting at index.
+  /// Copies every element of the source into this span, starting at index.
   ///
   /// This span must have space between `index` and its end for every element
   /// `source` provides.
