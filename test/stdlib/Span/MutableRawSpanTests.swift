384a4b73690d4531c8682caf2afd6c1b70e742b3
Guillaume Lessard <guillaume.lessard@apple.com>
2026-09-19 22:45:09 -0700

    [gardening] eliminate warnings in test

--- test/stdlib/Span/MutableRawSpanTests.swift
+++ test/stdlib/Span/MutableRawSpanTests.swift
@@ -465,7 +465,7 @@ suite.test("updateAll(moving:)")
     byteCount: byteCount, alignment: 8
   )
   defer { storage.deallocate() }
-  storage.initializeMemory(as: UInt8.self, fromContentsOf: 0..<8)
+  _ = storage.initializeMemory(as: UInt8.self, fromContentsOf: 0..<8)
   var source = OutputRawSpan(buffer: storage, initializedCount: 8)
   expectEqual(source.isFull, true)
 
@@ -652,7 +652,7 @@ suite.test("_consumingExtracting() bounds checking")
 .crashOutputMatches("Byte offset range out of bounds", when: _isDebugAssertConfiguration())
 .code {
   var b: ContiguousArray<Int> = [1, 2, 3, 4]
-  var span = MutableRawSpan(elements: b.mutableSpan)
+  let span = MutableRawSpan(elements: b.mutableSpan)
   expectCrashLater()
   _ = span._consumingExtracting(2 ..< .max)
 }
@@ -802,7 +802,7 @@ suite.test("_consumingExtracting(first:) bound checking")
 .crashOutputMatches("Can't have a prefix of negative length", when: _isDebugAssertConfiguration())
 .code {
   var b: ContiguousArray<Int> = [1, 2, 3, 4]
-  var span = MutableRawSpan(elements: b.mutableSpan)
+  let span = MutableRawSpan(elements: b.mutableSpan)
   expectCrashLater()
   _ = span._consumingExtracting(first: -1)
 }
@@ -813,7 +813,7 @@ suite.test("_consumingExtracting(droppingLast:) bound checking")
 .crashOutputMatches("Can't drop a negative number of bytes", when: _isDebugAssertConfiguration())
 .code {
   var b: ContiguousArray<Int> = [1, 2, 3, 4]
-  var span = MutableRawSpan(elements: b.mutableSpan)
+  let span = MutableRawSpan(elements: b.mutableSpan)
   expectCrashLater()
   _ = span._consumingExtracting(droppingLast: -1)
 }
@@ -929,7 +929,7 @@ suite.test("_consumingExtracting(last:) bound checking")
 .crashOutputMatches("Can't have a suffix of negative length", when: _isDebugAssertConfiguration())
 .code {
   var b: ContiguousArray<Int> = [1, 2, 3, 4]
-  var span = MutableRawSpan(elements: b.mutableSpan)
+  let span = MutableRawSpan(elements: b.mutableSpan)
   expectCrashLater()
   _ = span._consumingExtracting(last: -1)
 }
@@ -940,7 +940,7 @@ suite.test("_consumingExtracting(droppingFirst:) bound checking")
 .crashOutputMatches("Can't drop a negative number of bytes", when: _isDebugAssertConfiguration())
 .code {
   var b: ContiguousArray<Int> = [1, 2, 3, 4]
-  var span = MutableRawSpan(elements: b.mutableSpan)
+  let span = MutableRawSpan(elements: b.mutableSpan)
   expectCrashLater()
   _ = span._consumingExtracting(droppingFirst: -1)
 }
