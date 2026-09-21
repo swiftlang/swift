8af629c33189f7d44477933c4a5a142f570aee3a
Pavel Yaskevich <pyaskevich@apple.com>
2026-09-21 11:59:04 -0700

    [IRGen] Give non-escaping `@called(once)` closure a destructor
    Non-escaping `@called(once)` values aren't trivial because they
    can have consuming captures and so they require a destructor that
    is going to destroy all of their owned non-trivial fields.

--- lib/IRGen/GenFunc.cpp
+++ lib/IRGen/GenFunc.cpp
@@ -2252,7 +2252,8 @@ static llvm::Value *emitPartialApplicationForwarder(
     // so we can tail call. The safety of this assumes that neither this release
     // nor any of the loads can throw.
     if (consumesContext && !dependsOnContextLifetime && rawData) {
-      assert(!outType->isNoEscape() && "Trivial context must not be released");
+      assert(!outType->isTrivialNoEscape() &&
+             "Trivial closure context must not be released");
       destroyClosureContext(IGM, subIGF, layout, rawData, data, consumedFields);
     }
 
@@ -2373,7 +2374,8 @@ static llvm::Value *emitPartialApplicationForwarder(
   
   // If the parameters depended on the context, consume the context now.
   if (rawData && consumesContext && dependsOnContextLifetime) {
-    assert(!outType->isNoEscape() && "Trivial context must not be released");
+    assert(!outType->isTrivialNoEscape() &&
+           "Trivial closure context must not be released");
     destroyClosureContext(IGM, subIGF, layout, rawData, data, consumedFields);
   }
 
@@ -2674,6 +2676,33 @@ std::optional<StackAddress> irgen::emitFunctionPartialApplication(
       stackAddr = stackAddr->withAddress(IGF.Builder.CreateElementBitCast(
           stackAddr->getAddress(), IGF.IGM.OpaqueTy));
       data = stackAddr->getAddress().getAddress();
+
+      // @called(once) closures always get a context even if they are
+      // stack promoted because they can have consuming captures.
+      if (outType->isCalledOnce()) {
+        assert(!outType->isTrivialNoEscape());
+
+        // Borrowed `~Copyable` captures aren't owned by stack-promoted closures
+        // and so they cannot be destroyed.
+        llvm::BitVector unownedFields(argConventions.size());
+        for (unsigned i : indices(argConventions)) {
+          auto fieldTy = layout.getElementTypes()[i];
+          if (fieldTy &&
+              argConventions[i] == ParameterConvention::Direct_Guaranteed &&
+              fieldTy.isMoveOnly())
+            unownedFields.set(i);
+        }
+
+        auto descriptor = IGF.IGM.getAddrOfCaptureDescriptor(SILFn, origType,
+                                                       substType, subs,
+                                                       layout);
+        auto maybeDescriptor = layout.computeTypedMallocTypeDescriptor(IGF.IGM);
+        auto metadata = layout.getPrivateMetadata(
+            IGF.IGM, descriptor, maybeDescriptor, "closure", unownedFields);
+        auto object = IGF.Builder.CreateBitCast(data, IGF.IGM.RefCountedPtrTy);
+        object = IGF.emitInitStackObjectCall(metadata, object, "closure");
+        data = IGF.Builder.CreateBitCast(object, IGF.IGM.OpaquePtrTy);
+      }
     } else {
         auto descriptor = IGF.IGM.getAddrOfCaptureDescriptor(SILFn, origType,
                                                        substType, subs,
