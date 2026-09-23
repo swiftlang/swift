64e51d34974e895cf775b03ad59d2f390801c00b
Artem Chikin <achikin@apple.com>
2026-09-23 13:25:12 +0100

    [Tests] Switch `swift-ide-test` to use `swift_getSingleFrontendInvocationFromDriverArgumentsV3`
    This is the most recent version of the API.

--- tools/swift-ide-test/swift-ide-test.cpp
+++ tools/swift-ide-test/swift-ide-test.cpp
@@ -4446,6 +4446,7 @@ static int doPrintUSRs(const CompilerInvocation &InitInvok,
   return 0;
 }
 
+#if SWIFT_IDE_TEST_HAS_SWIFT_DRIVER
 extern "C" {
   typedef enum {
     SWIFTDRIVER_TOOLING_DIAGNOSTIC_ERROR = 0,
@@ -4453,20 +4454,51 @@ extern "C" {
     SWIFTDRIVER_TOOLING_DIAGNOSTIC_REMARK = 2,
     SWIFTDRIVER_TOOLING_DIAGNOSTIC_NOTE = 3
   } swiftdriver_tooling_diagnostic_kind;
-  bool swift_getSingleFrontendInvocationFromDriverArgumentsV2(const char *, int, const char**, bool(int, const char**),
-							void(swiftdriver_tooling_diagnostic_kind, const char*), bool);
+  bool swift_getSingleFrontendInvocationFromDriverArgumentsV3(
+      const char *, int, const char **, bool(int, const char **),
+      void(swiftdriver_tooling_diagnostic_kind, const char *), bool, bool);
 }
-bool displayFrontendArgs(int FrontendArgC, const char ** FrontendArgV) {
+
+static bool displayFrontendArgs(int FrontendArgC, const char **FrontendArgV) {
   llvm::outs() << "SwiftDriver (new) Frontend Arguments BEGIN\n";
   for (int i = 0; i < FrontendArgC; ++i) {
     llvm::outs() << FrontendArgV[i] << "\n";
   }
   llvm::outs() << "SwiftDriver (new) Frontend Arguments END\n";
-  return false;
+
+  ArrayRef<const char *> SubArgs(FrontendArgV, FrontendArgC);
+  if (!SubArgs.empty() &&
+      llvm::sys::path::stem(SubArgs.front()) == "swift-frontend")
+    SubArgs = SubArgs.drop_front();
+  if (!SubArgs.empty() && StringRef(SubArgs.front()) == "-frontend")
+    SubArgs = SubArgs.drop_front();
+
+  CompilerInvocation CI;
+  PrintingDiagnosticConsumer PDC;
+  SourceManager SM;
+  DiagnosticEngine Diags(SM);
+  Diags.addConsumer(PDC);
+  return CI.parseArgs(SubArgs, Diags);
 }
-void handleDiagnostic(swiftdriver_tooling_diagnostic_kind diagKind, const char * diagMessage) {
-  return;
+
+static void handleDiagnostic(swiftdriver_tooling_diagnostic_kind diagKind,
+                             const char *diagMessage) {
+  switch (diagKind) {
+  case SWIFTDRIVER_TOOLING_DIAGNOSTIC_ERROR:
+    llvm::errs() << "<swift-driver>: error: " << diagMessage << "\n";
+    break;
+  case SWIFTDRIVER_TOOLING_DIAGNOSTIC_WARNING:
+    llvm::errs() << "<swift-driver>: warning: " << diagMessage << "\n";
+    break;
+  case SWIFTDRIVER_TOOLING_DIAGNOSTIC_REMARK:
+    llvm::errs() << "<swift-driver>: remark: " << diagMessage << "\n";
+    break;
+  case SWIFTDRIVER_TOOLING_DIAGNOSTIC_NOTE:
+    llvm::errs() << "<swift-driver>: note: " << diagMessage << "\n";
+    break;
+  }
 }
+#endif // SWIFT_IDE_TEST_HAS_SWIFT_DRIVER
 
 static int doTestCreateCompilerInvocation(StringRef DriverPath,
                                           ArrayRef<const char *> Args,
@@ -4489,22 +4521,27 @@ static int doTestCreateCompilerInvocation(StringRef DriverPath,
       },
       ForceNoOutputs);
 
-  {
-    const char **argList = (const char**)malloc(sizeof(const char*) * Args.size());
-    for (size_t i = 0; i < Args.size(); ++i) {
-      argList[i] = Args[i];
-    }
-    bool HadError = swift_getSingleFrontendInvocationFromDriverArgumentsV2(
-        DriverPath.str().c_str(), Args.size(), argList,
-        &displayFrontendArgs,
-        &handleDiagnostic,
-        ForceNoOutputs);
-  }
-
   if (HadError) {
     llvm::errs() << "error: unable to create a CompilerInvocation\n";
-    return 1;
   }
+
+#if SWIFT_IDE_TEST_HAS_SWIFT_DRIVER
+  SmallVector<const char *, 16> argList(Args.begin(), Args.end());
+  std::string driverPathStr = DriverPath.str();
+  bool NewHadError = swift_getSingleFrontendInvocationFromDriverArgumentsV3(
+      driverPathStr.c_str(), argList.size(), argList.data(),
+      &displayFrontendArgs, &handleDiagnostic,
+      /*compilerIntegratedTooling=*/true, ForceNoOutputs);
+  if (NewHadError) {
+    llvm::errs()
+        << "error: unable to create a CompilerInvocation (new driver)\n";
+  }
+#else
+  const bool NewHadError = false;
+#endif
+
+  if (HadError || NewHadError)
+    return 1;
 
   return 0;
 }
