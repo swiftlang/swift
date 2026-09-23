669413db2e4e7fcf2dadd0b118475e3ac8b82cdb
Daniel Rodríguez <danielrodriguez@meta.com>
2026-09-23 10:08:42 -0700

    [Macros] Avoid assert when declaring prefix/suffix macros on special functions
    When a macro is declared with prefix or suffix "names", the compiler
    tries to create identifiers by prefixing or suffixing the given literal
    onto the base name. If the base name is a special name (like
    a constructor, destructor or subscript), the compiler was hitting an
    assert in `getIdentifier` because of the special name.
    
    Bail out before calling `getIdentifier`. The generated name will not be
    added to the list of names that the macro derives, but the names were
    invalid anyway (in the non-assert compiler, that is).
    
    It is not clear what those names are used for. It might be for better
    IDE integration, but it is unclear.
    
    Three crashing tests found that seemingly hit the assert. Moved to fixed
    and changed them to not crash.

--- validation-test/compiler_crashers_fixed/Decl-getResolvedCustomAttrType-4c3b60.swift
+++ validation-test/compiler_crashers_fixed/Decl-getResolvedCustomAttrType-4c3b60.swift
@@ -1,5 +1,5 @@
 // {"kind":"typecheck","original":"000f0a50","signature":"swift::Decl::getResolvedCustomAttrType(swift::CustomAttr*) const","signatureAssert":"Assertion failed: (!isSpecial() && \"Cannot retrieve identifier from special names\"), function getIdentifier"}
-// RUN: not --crash %target-swift-frontend -typecheck %s
+// RUN: not %target-swift-frontend -typecheck %s
 @attached(peer prefixed(a)) macro b
 struct c {
   @d config = @b
