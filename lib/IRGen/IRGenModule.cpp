4bac7ceda94bbee525278a3016ea8562c85d68d2
Doug Gregor <dgregor@apple.com>
2026-09-23 20:22:38 -0700

    Add proper availability for newly-introduced borrowing runtime functions
    This ensures that we get weak references to these symbols when building
    for older deployment targets.
    
    Fixes rdar://188247062
    
    (cherry picked from commit 4ae13d1c1ce68a5d19464eaea04cc042d6559403)

--- lib/IRGen/IRGenModule.cpp
+++ lib/IRGen/IRGenModule.cpp
@@ -995,6 +995,14 @@ namespace RuntimeConstants {
     return RuntimeAvailability::AlwaysAvailable;
   }
 
+  RuntimeAvailability BorrowingAvailability(ASTContext &Context) {
+    auto featureAvailability = Context.getBorrowingAvailability();
+    if (!isDeploymentAvailabilityContainedIn(Context, featureAvailability)) {
+      return RuntimeAvailability::ConditionallyAvailable;
+    }
+    return RuntimeAvailability::AlwaysAvailable;
+  }
+
   RuntimeAvailability InitRawStructMetadataAvailability(ASTContext &Context) {
     auto featureAvailability = Context.getInitRawStructMetadataAvailability();
     if (!isDeploymentAvailabilityContainedIn(Context, featureAvailability)) {
