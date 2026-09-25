e90f3e1071c5e9eff149c4c097c28b0a72527801
Anthony Latsis <alatsis@apple.com>
2026-09-25 12:35:51 +0100

    Revert "Merge pull request #91338 from gonzalolarralde/gonzalolarralde/embedded-dispatch-executor-tests"
    This reverts commit 6995d5c0a1e86d50b8c1ce5aaf94aa790a77978f, reversing
    changes made to b9f0356769ba53c26ca34eebdc97ce365cc07531.
    
    rdar://188371415

--- test/Concurrency/Runtime/async_let_throw_completion_order.swift
+++ test/Concurrency/Runtime/async_let_throw_completion_order.swift
@@ -9,11 +9,6 @@
 // REQUIRES: concurrency_runtime
 // UNSUPPORTED: back_deployment_runtime
 
-// RUN: %if embedded_cooperative_executor %{ %target-run-embedded-cooperative-swift() | %FileCheck %s %}
-// RUN: %if embedded_dispatch_executor %{ %target-run-embedded-dispatch-swift() | %FileCheck %s %}
-
-import _Concurrency
-
 struct Bad: Error {}
 
 class Foo { init() async throws {}; deinit { print("Foo down") } }
